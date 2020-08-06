06cf96962d24b5ec5f4193b9820a2ad9dfaaa6bd
andrew <andrew@expensify.com>
2020-08-06 08:46:13 -0700

    Configure webpack

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-env'],
+  plugins: [
+    'transform-class-properties',
+    [
+      'module-resolver',
+      {
+        alias: {
+          '^react-native$': 'react-native-web',
+        },
+      },
+    ],
+  ],
 };
