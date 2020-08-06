4df2e339069139022a6d12c00c3f6593163f2cd8
andrew <andrew@expensify.com>
2020-08-05 18:36:01 -0700

    Hello World

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './js/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
