ab2f8c6000c9df03d30364d5f21eec67c3b808f6
andrew <andrew@expensify.com>
2020-08-08 07:13:42 -0700

    Create a new login when needed

--- js/store/actions/SessionActions.js
+++ js/store/actions/SessionActions.js
@@ -4,30 +4,66 @@ import ROUTES from '../../ROUTES.js';
 import STOREKEYS from '../STOREKEYS.js';
 import * as PersistentStorage from '../../lib/PersistentStorage.js';
 
+// TODO: Figure out how to determine prod/dev on mobile, etc.
+const IS_IN_PRODUCTION = false;
+const partnerName = IS_IN_PRODUCTION ? 'chat-expensify-com' : 'android';
+const partnerPassword = IS_IN_PRODUCTION
+  ? 'e21965746fd75f82bb66'
+  : 'c3a9ac418ea3f152aae2';
+
 /**
  * Sign in with the API
  * @param {string} login
  * @param {string} password
+ * @param {boolean} useExpensifyLogin
  */
-function signIn(login, password) {
+function signIn(login, password, useExpensifyLogin = false) {
   Store.set(STOREKEYS.CREDENTIALS, {login, password});
   Store.set(STOREKEYS.SESSION, {});
 
-  request('Authenticate', {
-    useExpensifyLogin: true,
-    partnerName: 'expensify.com',
-    partnerPassword: 'MkgLvVAyaTlmw',
+  return request('Authenticate', {
+    useExpensifyLogin: useExpensifyLogin,
+    partnerName: partnerName,
+    partnerPassword: partnerPassword,
     partnerUserID: login,
     partnerUserSecret: password,
   })
     .then((data) => {
+      // 404 We need to create a login
+      if (data.jsonCode === 404 && !useExpensifyLogin) {
+        signIn(login, password, true).then((expensifyLoginData) => {
+          createLogin(expensifyLoginData.authToken, login, password);
+        });
+        return;
+      }
+
       Store.set(STOREKEYS.SESSION, data);
       Store.set(STOREKEYS.APP_REDIRECT_TO, ROUTES.HOME);
+
+      return data;
     })
     .catch((err) => {
       console.warn(err);
       Store.set(STOREKEYS.SESSION, {error: err});
     });
+}
+
+/**
+ * Create login
+ * @param {string} authToken
+ * @param {string} login
+ * @param {string} password
+ */
+function createLogin(authToken, login, password) {
+  request('CreateLogin', {
+    authToken: authToken,
+    partnerName,
+    partnerPassword,
+    partnerUserID: login,
+    partnerUserSecret: password,
+  }).catch((err) => {
+    Store.set(STOREKEYS.SESSION, {error: err});
+  });
 }
 
 /**
