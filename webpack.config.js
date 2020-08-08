f977bc09ab874501a45800286fc9cfe9baeff8b5
Tim Golen <tgolen@expensify.com>
2020-08-08 09:36:14 -0600

    Correct spacing

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,42 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/index.js',
-  },
-  output: {
-    filename: '[name]-[hash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      // With the dev server, we need to put this file in the dist folder
-      filename: 'index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      // Transpiles ES6 and JSX
-      {
-        test: /\.js$/,
-        exclude: /node_modules|\.native.js$/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      'react-native$': 'react-native-web',
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
     },
-  },
+    output: {
+        filename: '[name]-[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            // With the dev server, we need to put this file in the dist folder
+            filename: 'index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            // Transpiles ES6 and JSX
+            {
+                test: /\.js$/,
+                exclude: /node_modules|\.native.js$/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            'react-native$': 'react-native-web',
+        },
+    },
 };
