69d5dbea62dd5dd0eaf70aa37025d3369c5a86c3
Tim Golen <tgolen@expensify.com>
2020-08-08 16:23:56 -0600

    Add the personal details action

---
+++ src/CONST.js
@@ -0,0 +1,5 @@
+const CONST = {
+    CLOUDFRONT_URL: 'https://d2k5nsl2zxldvw.cloudfront.net',
+};
+
+export default CONST;
---
+++ src/store/actions/PersonalDetailsActions.js
@@ -0,0 +1,82 @@
+import _ from 'underscore';
+import * as Store from '../Store';
+import {request} from '../../lib/Network';
+import STOREKEYS from '../STOREKEYS';
+import md5 from '../../lib/md5';
+import CONST from '../../CONST';
+
+/**
+ * Returns the URL for a user's avatar and handles someone not having any avatar at all
+ *
+ * @param {object} personalDetails
+ * @param {string} login
+ * @returns {string}
+ */
+function getAvatar(personalDetails, login) {
+    if (personalDetails.detailJSON && personalDetails.detailJSON.avatar) {
+        return personalDetails.detailJSON.avatar.replace(/&d=404$/, '');
+    }
+
+    // There are 8 possible default avatars, so we choose which one this user has based
+    // on a simple hash of their login (which is converted from HEX to INT)
+    const loginHashBucket = (parseInt(md5(login).substring(0, 4), 16) % 8) + 1;
+    return `${CONST.CLOUDFRONT_URL}/images/avatars/avatar_${loginHashBucket}.png`;
+}
+
+/**
+ * Get the personal details for our organization
+ *
+ * return {Promise}
+ */
+function fetch() {
+    const requestPromise = request('Get', {
+        returnValueList: 'personalDetailsList',
+    })
+        .then(data => Store.set(STOREKEYS.PERSONAL_DETAILS, _.reduce(data.personalDetailsList, (finalPersonalDetailObject, personalDetails, login) => {
+            // Form the details into something that has all the data in an easy to use format.
+            const avatarURL = getAvatar(personalDetails, login);
+            const firstName = personalDetails.firstName || '';
+            const lastName = personalDetails.lastName || '';
+            const fullName = `${firstName} ${lastName}`.trim();
+            const displayName = fullName === '' ? login : fullName;
+            const displayNameWithEmail = fullName === '' ? login : `${fullName} (${login})`;
+            return {
+                ...finalPersonalDetailObject,
+                [login]: {
+                    login,
+                    avatarURL,
+                    firstName,
+                    lastName,
+                    fullName,
+                    displayName,
+                    displayNameWithEmail,
+                }
+            };
+        }, {})));
+
+    // Refresh the personal details every 30 minutes
+    setTimeout(fetch, 1000 * 60 * 30);
+    return requestPromise;
+}
+
+/**
+ * Get the timezone of the logged in user
+ *
+ * @returns {Promise}
+ */
+function fetchTimezone() {
+    const requestPromise = request('Get', {
+        returnValueList: 'nameValuePairs',
+        name: 'timeZone',
+    })
+        .then(data => Store.set(STOREKEYS.TIMEZONE, data.nameValuePairs.timeZone.selected || 'America/Los_Angeles'));
+
+    // Refresh the timezone every 30 minutes
+    setTimeout(fetchTimezone, 1000 * 60 * 30);
+    return requestPromise;
+}
+
+export {
+    fetch,
+    fetchTimezone,
+};
