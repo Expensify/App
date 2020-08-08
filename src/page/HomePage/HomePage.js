b7e73154afce49c81e64c395491acef0f6a5cf96
Tim Golen <tgolen@expensify.com>
2020-08-08 16:23:46 -0600

    Add styles and display a header

--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import {
     SafeAreaView,
-    Text,
     StatusBar,
     View,
-    Button
 } from 'react-native';
-import {signOut} from '../../store/actions/SessionActions';
+import Header from './Header';
 
 const App = () => (
     <>
         <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={{padding: 20}}>
+        <SafeAreaView>
             <View>
-                <Text
-                    style={{
-                        fontSize: 20,
-                        margin: 20,
-                        textAlign: 'center',
-                        fontWeight: 'bold',
-                    }}
-                >
-                    React Native Chat Homepage!
-                </Text>
-                <Button onPress={signOut} title="Sign Out" />
+                <Header />
             </View>
         </SafeAreaView>
     </>
---
+++ src/style/StyleSheet.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native-web';
+
+const styles = StyleSheet.create({
+    nav: {
+        flexDirection: 'row',
+        backgroundColor: '#efefef',
+        padding: 20,
+    },
+    brand: {
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    flex1: {
+        flex: 1,
+    }
+});
+
+export default styles;
