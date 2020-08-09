d5092a54905fc50bad78e9cf16e04627000120d7
Tim Golen <tgolen@expensify.com>
2020-08-09 11:18:46 -0600

    Remvoe subscribe and unsubscribe for now
    Fixes https://github.com/AndrewGable/ReactNativeChat/issues/12

--- src/Expensify.js
+++ src/Expensify.js
@@ -6,6 +6,7 @@ import * as Store from './store/Store';
 import * as ActiveClientManager from './lib/ActiveClientManager';
 import {verifyAuthToken} from './store/actions/SessionActions';
 import STOREKEYS from './store/STOREKEYS';
+import WithStoreSubscribeToState from './components/WithStoreSubscribeToState';
 import {
     Route,
     Router,
@@ -16,34 +17,13 @@ import {
 // Initialize the store when the app loads for the first time
 Store.init();
 
-export default class Expensify extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            redirectTo: null,
-        };
-    }
-
-    componentDidMount() {
-        // Listen for when the app wants to redirect to a specific URL
-        Store.subscribe(STOREKEYS.APP_REDIRECT_TO, (redirectTo) => {
-            this.setState({redirectTo});
-        });
-
-        // Verify that our authToken is OK to use
-        verifyAuthToken();
-
-        // Initialize this client as being an active client
-        ActiveClientManager.init();
-    }
-
+class Expensify extends Component {
     render() {
         return (
             <Beforeunload onBeforeunload={ActiveClientManager.removeClient}>
                 <Router>
                     {/* If there is ever a property for redirecting, we do the redirect here */}
-                    {this.state.redirectTo && <Redirect to={this.state.redirectTo} />}
+                    {this.state && this.state.redirectTo && <Redirect to={this.state.redirectTo} />}
 
                     <Switch>
                         <Route path="/signin" component={SignInPage} />
@@ -54,3 +34,16 @@ export default class Expensify extends Component {
         );
     }
 }
+
+export default WithStoreSubscribeToState({
+    redirectTo: {
+        key: STOREKEYS.APP_REDIRECT_TO,
+        loader: () => {
+            // Verify that our authToken is OK to use
+            verifyAuthToken();
+
+            // Initialize this client as being an active client
+            ActiveClientManager.init();
+        }
+    },
+})(Expensify);
