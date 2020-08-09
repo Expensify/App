7a058563ca386c66a7e61b0904858f44bc86bc17
Tim Golen <tgolen@expensify.com>
2020-08-09 10:14:55 -0600

    Move the sidebar link into it's own component

--- src/page/HomePage/SidebarView.js
+++ src/page/HomePage/SidebarView.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Link} from '../../lib/Router';
 import _ from 'underscore';
 import styles from '../../style/StyleSheet';
 import WithStoreSubscribeToState from '../../components/WithStoreSubscribeToState';
 import STOREKEYS from '../../store/STOREKEYS';
 import {fetchAll} from '../../store/actions/ReportActions';
+import SidebarLink from './SidebarLink';
 
 class SidebarView extends React.Component {
     render() {
         return (
             <View style={[styles.flexGrow1, styles.p1]}>
                 {this.state && this.state.reports && this.state.reports.length > 0 && _.map(this.state.reports, (report) => (
-                    <View key={report.reportID}>
-                        <Link to={`/${report.reportID}`}>{report.reportName}</Link>
-                    </View>
+                    <SidebarLink key={report.reportID} reportID={report.reportID} reportName={report.reportName} />
                 ))}
             </View>
         );
