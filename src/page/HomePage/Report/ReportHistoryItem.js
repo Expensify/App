dc3adaf8dd9c15aa6a28a10f2d2e09e7f0c3bb2e
Tim Golen <tgolen@expensify.com>
2020-08-09 15:26:17 -0600

    Add a view for displaying history items

---
+++ src/page/HomePage/Report/ReportHistoryItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const propTypes = {
+};
+
+class ReportHistoryItem extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Report History Item</Text>
+            </View>
+        );
+    }
+}
+ReportHistoryItem.propTypes = propTypes;
+
+export default ReportHistoryItem;
--- src/page/HomePage/Report/ReportHistoryView.js
+++ src/page/HomePage/Report/ReportHistoryView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import PropTypes from 'prop-types';
+import _ from 'underscore';
 import styles from '../../../style/StyleSheet';
 import {fetchHistory} from '../../../store/actions/ReportActions';
 import WithStore from '../../../components/WithStore';
 import STOREKEYS from '../../../store/STOREKEYS';
+import ReportHistoryItem from './ReportHistoryItem';
 
 const propTypes = {
     // The ID of the report being looked at
@@ -42,7 +44,15 @@ class ReportHistoryView extends React.Component {
     render() {
         return (
             <View style={styles.flexColumn}>
-                <Text>Report History View</Text>
+                {this.state && this.state.reportHistory && this.state.reportHistory.length === 0 && (
+                    <Text>Be the first person to comment!</Text>
+                )}
+                {this.state && this.state.reportHistory && this.state.reportHistory.length > 0 && _.map(this.state.reportHistory, reportHistoryItem => (
+                    <ReportHistoryItem
+                        key={reportHistoryItem.sequenceNumber}
+                        data={reportHistoryItem}
+                    />
+                ))}
             </View>
         );
     }
