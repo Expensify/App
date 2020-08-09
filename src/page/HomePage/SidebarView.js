a5cbde69031b9e99cd988075769fda7d0de1eb23
Tim Golen <tgolen@expensify.com>
2020-08-08 22:24:18 -0600

    List reports in the sidebar

--- src/page/HomePage/SidebarView.js
+++ src/page/HomePage/SidebarView.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {NavLink} from '../../lib/Router';
+import _ from 'underscore';
 import styles from '../../style/StyleSheet';
+import WithStoreSubscribeToState from '../../components/WithStoreSubscribeToState';
+import STOREKEYS from '../../store/STOREKEYS';
+import {fetchAll} from '../../store/actions/ReportActions';
 
-function SidebarView() {
-    return (
-        <View style={[styles.flexGrow1, styles.p1]}>
-            <Text>Sidebar View</Text>
-        </View>
-    );
+class SidebarView extends React.Component {
+    render() {
+        return (
+            <View style={[styles.flexGrow1, styles.p1]}>
+                {this.state && this.state.reports && this.state.reports.length > 0 && _.map(this.state.reports, (report) => (
+                    <View key={report.reportID}>
+                        <NavLink to={`/${report.reportID}`}>{report.reportName}</NavLink>
+                    </View>
+                ))}
+            </View>
+        );
+    }
 }
 
-export default SidebarView;
+export default WithStoreSubscribeToState({
+    reports: {key: STOREKEYS.REPORTS, loader: fetchAll},
+})(SidebarView);
