bd033282ad084cfc8dea0ba6ed4ee4ac417b940a
Tim Golen <tgolen@expensify.com>
2020-08-09 15:33:09 -0600

    Add some more views for displaying history items

--- src/page/HomePage/Report/ReportHistoryItemSingle.js
+++ src/page/HomePage/Report/ReportHistoryItemSingle.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import PropTypes from 'prop-types';
-import ReportHistoryItemSingle from './ReportHistoryItemSingle';
 import ReportHistoryPropsTypes from './ReportHistoryPropsTypes';
 
 const propTypes = {
@@ -9,15 +8,15 @@ const propTypes = {
     historyItem: PropTypes.shape(ReportHistoryPropsTypes).isRequired,
 };
 
-class ReportHistoryItem extends React.Component {
+class ReportHistoryItemSingle extends React.Component {
     render() {
         return (
             <View>
-                <ReportHistoryItemSingle historyItem={this.props.historyItem} />
+                <Text>Single history item</Text>
             </View>
         );
     }
 }
-ReportHistoryItem.propTypes = propTypes;
+ReportHistoryItemSingle.propTypes = propTypes;
 
-export default ReportHistoryItem;
+export default ReportHistoryItemSingle;
