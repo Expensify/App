c078bb3c3694e8989481a4689e3e81b3dea52ebe
andrew <andrew@expensify.com>
2020-08-09 20:50:11 -0700

    Get V1 pusher working

--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -9,22 +9,43 @@ import styles from '../../style/StyleSheet';
 import Header from './HeaderView';
 import Sidebar from './SidebarView';
 import Main from './MainView';
+import * as Store from '../../store/Store';
+import STOREKEYS from '../../store/STOREKEYS';
+import {initPusher} from '../../store/actions/ReportActions';
+import * as pusher from '../../lib/Pusher/pusher';
 
-const App = () => (
-    <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={[styles.flex1, styles.h100p]}>
-            <View style={[styles.flexColumn, styles.h100p]}>
-                <Route path="/:reportID?">
-                    <Header />
-                    <View style={[styles.flex1, styles.flexRow]}>
-                        <Sidebar />
-                        <Main />
+export default class App extends React.Component {
+    componentDidMount() {
+        Store.get(STOREKEYS.SESSION, 'authToken').then((authToken) => {
+            if (authToken) {
+                // Initialize the pusher connection
+                pusher.init(null, {
+                    authToken,
+                });
+
+                // Setup the report action handler to subscribe to pusher
+                initPusher();
+            }
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView style={[styles.flex1, styles.h100p]}>
+                    <View style={[styles.flexColumn, styles.h100p]}>
+                        <Route path="/:reportID?">
+                            <Header />
+                            <View style={[styles.flex1, styles.flexRow]}>
+                                <Sidebar />
+                                <Main />
+                            </View>
+                        </Route>
                     </View>
-                </Route>
-            </View>
-        </SafeAreaView>
-    </>
-);
+                </SafeAreaView>
+            </>
+        );
+    }
+}
 App.displayName = 'App';
-export default App;
