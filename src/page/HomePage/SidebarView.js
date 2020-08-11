be241a12bea5cb3b4228fc2e32f7c921adbd726a
Tim Golen <tgolen@expensify.com>
2020-08-11 16:35:58 -0600

    Add an unread chat indicator

--- src/page/HomePage/SidebarView.js
+++ src/page/HomePage/SidebarView.js
@@ -7,10 +7,24 @@ import STOREKEYS from '../../store/STOREKEYS';
 import {fetchAll} from '../../store/actions/ReportActions';
 import SidebarLink from './SidebarLink';
 import logo from '../../images/expensify-logo_reversed.png';
+import PageTitleUpdater from '../../lib/PageTitleUpdater';
 
 class SidebarView extends React.Component {
+
+    /**
+     * Updates the page title to indicate there are unread reports
+     */
+    updateUnreadReportIndicator() {
+        if (this.state) {
+            const hasUnreadReports = _.any(this.state.individualReports, report => report.hasUnread);
+            console.log('has unread', hasUnreadReports)
+            PageTitleUpdater(hasUnreadReports);
+        }
+    }
+
     render() {
         const reports = this.state && this.state.reports;
+        this.updateUnreadReportIndicator();
         return (
             <View style={[styles.flex1, styles.p1]}>
                 <Image
@@ -30,5 +44,10 @@ export default WithStore({
         key: STOREKEYS.REPORTS,
         loader: fetchAll,
         prefillWithKey: STOREKEYS.REPORTS,
+    },
+    individualReports: {
+        key: `${STOREKEYS.REPORT}_[0-9]+$`,
+        addAsCollection: true,
+        collectionId: 'reportID',
     },
 })(SidebarView);
