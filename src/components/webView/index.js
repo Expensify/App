dc24727cb6930cd0aab83eb15ed3d7dcf998d007
Tim Golen <tgolen@expensify.com>
2020-08-11 11:46:03 -0600

    Styled comments on web

--- src/components/webView/index.js
+++ src/components/webView/index.js
@@ -1,3 +1,25 @@
-import {WebView} from 'react-native-web-webview';
+import {Text} from 'react-native';
+import React from 'react';
+import PropTypes from 'prop-types';
+import ExpensiMark from '../../lib/ExpensiMark';
+
+const parser = new ExpensiMark();
+const propTypes = {
+    html: PropTypes.string,
+};
+const defaultProps = {
+    html: '',
+};
+
+const WebView = ({html}) => (
+    <Text>
+        {/* eslint-disable-next-line react/no-danger */}
+        <span dangerouslySetInnerHTML={{__html: parser.replace(html)}} />
+    </Text>
+);
+
+WebView.propTypes = propTypes;
+WebView.defaultProps = defaultProps;
+WebView.displayName = 'WebView';
 
 export default WebView;
--- src/components/webView/index.native.js
+++ src/components/webView/index.native.js
@@ -1,3 +1,22 @@
-import {WebView} from 'react-native-webview';
+import React from 'react';
+import {Text} from 'react-native';
+import Str from '../../lib/Str';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    html: PropTypes.string,
+};
+const defaultProps = {
+    html: '',
+};
+
+// @TODO native clients don't support directly injecting HTML so a method needs to be found to do this reliably
+const WebView = ({html}) => (
+    <Text>{Str.htmlDecode(html)}</Text>
+);
+
+WebView.propTypes = propTypes;
+WebView.defaultProps = defaultProps;
+WebView.displayName = 'WebView';
 
 export default WebView;
