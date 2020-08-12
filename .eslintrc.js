f2defda5d6d98a2f4d0f8e4cfe8c3666e408fa3e
andrew <andrew@expensify.com>
2020-08-12 15:32:01 -0700

    Auto fixables

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
     extends: 'expensify',
     parser: 'babel-eslint',
     rules: {
-        "react/jsx-filename-extension": [1, {"extensions": [".js"]}],
+        'react/jsx-filename-extension': [1, {extensions: ['.js']}],
     },
 };
