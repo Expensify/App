d19c4187c275cb269cecdf13169bc8f5893c5108
Tim Golen <tgolen@expensify.com>
2020-08-12 16:03:30 -0600

    Access local config options

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -1,4 +1,5 @@
 import {Platform} from 'react-native';
+import devConfig from './CONFIG.DEV';
 
 // eslint-disable-next-line no-undef
 const IS_IN_PRODUCTION = Platform.OS === 'web' ? process.env.NODE_ENV === 'production' : !__DEV__;
@@ -16,5 +17,5 @@ export default {
         PARTNER_PASSWORD: IS_IN_PRODUCTION ? 'e21965746fd75f82bb66' : 'c3a9ac418ea3f152aae2',
         API_ROOT: IS_IN_PRODUCTION ? 'https://www.expensify.com/api?' : 'https://www.expensify.com.dev/api?',
     },
-    REPORT_IDS: IS_IN_PRODUCTION ? '63212778,63212795,63212764,63212607,63699490' : '35,29,123123123',
+    REPORT_IDS: IS_IN_PRODUCTION ? '63212778,63212795,63212764,63212607,63699490' : devConfig.REPORT_IDS,
 };
