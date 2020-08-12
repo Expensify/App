460b94b762c6c0f2dc42d54105d85f5aca03592d
Tim Golen <tgolen@expensify.com>
2020-08-12 09:12:49 -0600

    Move the store to lib

--- src/Expensify.js
+++ src/Expensify.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 // import {Beforeunload} from 'react-beforeunload';
 import SignInPage from './page/SignInPage';
 import HomePage from './page/HomePage/HomePage';
-import * as Store from './store/Store';
+import * as Store from './lib/Store';
 import * as ActiveClientManager from './lib/ActiveClientManager';
 import {verifyAuthToken} from './store/actions/SessionActions';
 import STOREKEYS from './store/STOREKEYS';
