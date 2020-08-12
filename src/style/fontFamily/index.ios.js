a7a72011d32f3000da2f7c6426f8260c6cc83be2
Tim Golen <tgolen@expensify.com>
2020-08-12 13:39:42 -0600

    Add the files for typography

---
+++ src/style/fontFamily/index.ios.js
@@ -0,0 +1,16 @@
+const fontFamily = {
+    fabriga: {
+        fontFamily: 'Fabriga',
+    },
+    fabrigaBlack: {
+        fontFamily: 'Fabriga-Black',
+    },
+    americaMedium: {
+        fontFamily: 'GTAmericaExp-Medium',
+    },
+    americaRegular: {
+        fontFamily: 'GTAmericaExp-Regular',
+    },
+};
+
+export default fontFamily;
