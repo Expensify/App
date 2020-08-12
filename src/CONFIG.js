4c44c28b4674624fcd0981c2e72df1087f7c6795
Joe Gambino <joe@expensify.com>
2020-08-12 13:40:50 -0700

    Move AUTH_TOKEN_EXPIRATION_TIME to CONFIG.js

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -6,6 +6,7 @@ const IS_IN_PRODUCTION = Platform.OS === 'web' ? process.env.NODE_ENV === 'produ
 export default {
     IS_IN_PRODUCTION,
     SITE_TITLE: 'Chat',
+    AUTH_TOKEN_EXPIRATION_TIME: 1000 * 60 * 90,
     PUSHER: {
         APP_KEY: IS_IN_PRODUCTION ? '268df511a204fbb60884' : 'ac6d22b891daae55283a',
         AUTH_URL: IS_IN_PRODUCTION ? 'https://www.expensify.com' : 'https://www.expensify.com.dev',
