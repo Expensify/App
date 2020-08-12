4dac1be1333b602e8f4dbcf1ef40d991552eda1c
andrew <andrew@expensify.com>
2020-08-12 15:14:33 -0700

    Run npm install

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -8,12 +8,12 @@ export default {
     SITE_TITLE: 'Chat',
     PUSHER: {
         APP_KEY: IS_IN_PRODUCTION ? '268df511a204fbb60884' : 'ac6d22b891daae55283a',
-        AUTH_URL: IS_IN_PRODUCTION ? 'https://www.expensify.com' : 'https://www.expensify.com.dev',
+        AUTH_URL: IS_IN_PRODUCTION ? 'https://www.expensify.com' : 'https://3181268f341d.ngrok.io',
         CLUSTER: 'mt1',
     },
     EXPENSIFY: {
         PARTNER_NAME: IS_IN_PRODUCTION ? 'chat-expensify-com' : 'android',
         PARTNER_PASSWORD: IS_IN_PRODUCTION ? 'e21965746fd75f82bb66' : 'c3a9ac418ea3f152aae2',
-        API_ROOT: IS_IN_PRODUCTION ? 'https://www.expensify.com/api?' : 'https://www.expensify.com.dev/api?',
+        API_ROOT: IS_IN_PRODUCTION ? 'https://www.expensify.com/api?' : 'https://3181268f341d.ngrok.io/api?',
     }
 };
