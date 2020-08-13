2db79ab06e995343d324d315cbcb69a3b2bc271e
Joe Gambino <joe@expensify.com>
2020-08-13 16:30:18 -0700

    remove unnecessary config.dev

--- src/CONFIG.DEV.example.js
+++
@@ -1,8 +0,0 @@
-/**
- * Rename this file to `CONFIG.DEV.js` and put your local config in here
- */
-// Use module.exports because this file is loaded in webpack which uses commonJS modules
-module.exports = {
-    // Add a comma separated list of report IDs here to load into the LHN
-    REPORT_IDS: '',
-};
--- src/CONFIG.js
+++ src/CONFIG.js
@@ -7,14 +7,14 @@ const IS_IN_PRODUCTION = Platform.OS === 'web' ? process.env.NODE_ENV === 'produ
 export default {
     AUTH_TOKEN_EXPIRATION_TIME: 1000 * 60 * 90,
     EXPENSIFY: {
-        API_ROOT: IS_IN_PRODUCTION ? 'https://www.expensify.com/api?' : 'https://expensify-joe.ngrok.io/api?',
+        API_ROOT: IS_IN_PRODUCTION ? 'https://www.expensify.com/api?' : 'https://www.expensify.com.dev/api?',
         PARTNER_NAME: IS_IN_PRODUCTION ? 'chat-expensify-com' : 'android',
         PARTNER_PASSWORD: IS_IN_PRODUCTION ? 'e21965746fd75f82bb66' : 'c3a9ac418ea3f152aae2',
     },
     IS_IN_PRODUCTION,
     PUSHER: {
         APP_KEY: IS_IN_PRODUCTION ? '268df511a204fbb60884' : 'ac6d22b891daae55283a',
-        AUTH_URL: IS_IN_PRODUCTION ? 'https://www.expensify.com' : 'https://expensify-joe.ngrok.io',
+        AUTH_URL: IS_IN_PRODUCTION ? 'https://www.expensify.com' : 'https://www.expensify.com.dev',
         CLUSTER: 'mt1',
     },
     REPORT_IDS: Config.REPORT_IDS,
