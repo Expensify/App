e8332def2ea9a5ff9ba5fd62a64ae3289c8f6436
andrew <andrew@expensify.com>
2020-08-13 13:11:07 -0700

    Add comments for native/web specific files

--- src/components/KeyboardSpacer/index.ios.js
+++ src/components/KeyboardSpacer/index.ios.js
@@ -1,3 +1,7 @@
+/**
+ * On iOS the keyboard covers the input fields on the bottom of the view. This component moves the view up with the
+ * keyboard allowing the user to see what they are typing.
+ */
 import ReactNativeKeyboardSpacer from 'react-native-keyboard-spacer';
 import React from 'react';
 
