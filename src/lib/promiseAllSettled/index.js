48ef2bb2a838cdbda9aadae7c2a9806efc3b7aa5
Tim Golen <tgolen@expensify.com>
2020-08-13 15:34:16 -0600

    Lint

--- src/lib/promiseAllSettled/index.js
+++ src/lib/promiseAllSettled/index.js
@@ -1,10 +1,14 @@
-import _ from 'underscore';
-
 /**
  * Native devices don't support Promise.allSettled, but all modern browsers do so a reference directly to the native
  * Promise implementation is fine.
  */
-const promiseAllSettled = arrayOfPromises => Promise.allSettled(arrayOfPromises);
 
+/**
+ * Returns a promise that is resolved when all provided promises are either resolved or rejected
+ *
+ * @param {Promise[]} arrayOfPromises
+ * @returns {Promise}
+ */
+const promiseAllSettled = arrayOfPromises => Promise.allSettled(arrayOfPromises);
 
 export default promiseAllSettled;
