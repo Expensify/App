ae567ce91e26a426cde9c18cfc2df60ff863a19e
Joe Gambino <joe@expensify.com>
2020-08-13 15:39:14 -0700

    eslint

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -1,5 +1,5 @@
 import {Platform} from 'react-native';
-import Config from "react-native-config";
+import Config from 'react-native-config';
 
 // eslint-disable-next-line no-undef
 const IS_IN_PRODUCTION = Platform.OS === 'web' ? process.env.NODE_ENV === 'production' : !__DEV__;
