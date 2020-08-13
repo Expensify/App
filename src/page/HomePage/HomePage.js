00caca6bfe0d1d178048a0e02ae4e8ac305a1cf8
andrew <andrew@expensify.com>
2020-08-12 17:39:12 -0700

    V1 Hamburger menu

--- src/page/HomePage/HomePage.js
+++ src/page/HomePage/HomePage.js
@@ -13,8 +13,19 @@ import Ion from '../../lib/Ion';
 import IONKEYS from '../../IONKEYS';
 import {initPusher} from '../../lib/actions/ActionsReport';
 import * as pusher from '../../lib/Pusher/pusher';
+import ROUTES from '../../ROUTES';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hamburgerShown: false
+        };
+
+        this.toggleHamburger = this.toggleHamburger.bind(this);
+    }
+
     componentDidMount() {
         Ion.get(IONKEYS.SESSION, 'authToken').then((authToken) => {
             if (authToken) {
@@ -25,8 +36,24 @@ export default class App extends React.Component {
 
                 // Setup the report action handler to subscribe to pusher
                 initPusher();
+
+                // TODO: Remove, debugging
+                // Ion.set(IONKEYS.APP_REDIRECT_TO, '/36');
+
+                Ion.get(IONKEYS.REPORTS).then((reports) => {
+                    const firstReportID = reports[0].reportID ?? '';
+                    if (firstReportID) {
+                        console.warn(`Report ID ${firstReportID}`)
+                        Ion.set(IONKEYS.APP_REDIRECT_TO, `/${firstReportID}`);
+                    }
+                });
             }
         });
+    }
+
+    toggleHamburger() {
+        const currentValue = this.state.hamburgerShown;
+        this.setState({hamburgerShown: !currentValue});
     }
 
     render() {
@@ -36,11 +63,13 @@ export default class App extends React.Component {
                 <SafeAreaView style={[styles.flex1, styles.h100p]}>
                     <View style={[styles.appContentWrapper, styles.flexRow, styles.h100p]}>
                         <Route path="/:reportID?">
-                            <View style={{width: 300}}>
-                                <Sidebar />
+                            {this.state.hamburgerShown && (
+                            <View style={{position: 'absolute', left: 0, top: 0, bottom:0, width: 300, zIndex: 20}}>
+                                <Sidebar toggleHamburger={this.toggleHamburger} />
                             </View>
+                            )}
                             <View style={[styles.appContent, styles.flex1, styles.flexColumn]}>
-                                <Header />
+                                <Header toggleHamburger={this.toggleHamburger} />
                                 <Main />
                             </View>
                         </Route>
