7acdfc1dd86043c2c5af40e7a7be80ea1d5f215b
andrew <andrew@expensify.com>
2020-08-14 10:16:27 -0700

    Add TextInputFocusable

---
+++ src/components/TextInputFocusable/index.native.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {TextInput} from 'react-native';
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const TextInputFocusable = props => (<TextInput {...props} />);
+export default TextInputFocusable;
