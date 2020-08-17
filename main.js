cacc1efa600b951c8cb8456b9f375c9133ab4a34
andrew <andrew@expensify.com>
2020-08-17 16:33:24 -0700

    Get production build electron working

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const {app, BrowserWindow} = require('electron');
 const serve = require('electron-serve');
 
+// TODO: Turn this off, use CORS after alpha launch
+app.commandLine.appendSwitch('disable-web-security');
+
 const mainWindow = (async () => {
     const loadURL = serve({directory: 'dist'});
 
     await app.whenReady();
 
     const browserWindow = new BrowserWindow({
+        // TODO: Grab from StyleSheet.js
+        backgroundColor: '#FAFAFA',
         width: 1200,
         height: 900,
         webPreferences: {
