5c735d16a59fa818707348eceee41cfa5cc30312
andrew <andrew@expensify.com>
2020-08-17 18:15:50 -0700

    Add comment

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const {app, BrowserWindow} = require('electron');
 const serve = require('electron-serve');
 
+/**
+ * Electron main process that handles wrapping the web application.
+ *
+ * @see: https://www.electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
+ */
+
 // TODO: Turn this off, use CORS after alpha launch
 app.commandLine.appendSwitch('disable-web-security');
 
