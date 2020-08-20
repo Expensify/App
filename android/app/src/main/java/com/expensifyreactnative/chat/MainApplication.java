4efa3c64dc28922c3129843dc723057857c4f5bd
Marc Glasser <marc@expensify.com>
2020-08-19 17:29:48 -0700

    undo things

--- android/app/src/main/java/com/expensifyreactnative/chat/MainApplication.java
+++ android/app/src/main/java/com/expensifyreactnative/chat/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
