2634e0478e52a94f45efb623a68fd61236448bca
Rory Abraham <rory@expensify.com>
2020-08-20 13:28:32 -0700

    save current git hash in local storage, and create logic to determine if it needs to be updated

--- src/lib/VisibilityManager/CONST.js
+++ src/lib/VisibilityManager/CONST.js
@@ -6,4 +6,12 @@ export default CONST = {
     EVENT: {
         VISIBILITY_CHANGE: 'visibilitychange',
     },
+
+    // AsyncStorage key for git version hash
+    KEY_VERSION_HASH: 'gitVersionHash',
+
+    COMMAND: {
+        // TODO: get correct URL
+        GET_VERSION_HASH: '',
+    },
 };
--- src/lib/VisibilityManager/index.js
+++ src/lib/VisibilityManager/index.js
@@ -5,7 +5,10 @@
  *  - Prompts the user to refresh the page if it's currently visible
  */
 import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+
 import CONST from './CONST';
+import {queueRequest} from '../Network';
 
 const getDisplayName = component => component.displayName || component.name || 'Component';
 
@@ -13,26 +16,66 @@ export default function (WrappedComponent) {
     class withBackgroundRefresh extends React.Component {
         constructor() {
             super();
-            this.state = {
-                pageShouldRefresh: false,
-            };
-
             this.onVisibilityChange.bind(this);
         }
 
         componentDidMount() {
             window.addEventListener(CONST.EVENT.VISIBILITY_CHANGE, this.onVisibilityChange());
+
+            // When the page first loads, get the current version hash
+            this.getStoredVersionAsync();
         }
 
-        onVisibilityChange() {
-            if (this.state.pageShouldRefresh) {
+        async onVisibilityChange() {
+            const pageShouldRefresh = await this.pageShouldRefreshAsync();
+            if (pageShouldRefresh) {
                 if (document.visibilityState === CONST.STATE.HIDDEN) {
                     // Refresh page w/o browser cache
                     window.location.reload(true);
                 } else if (document.visibilityState === CONST.STATE.VISIBLE) {
-                    // Notify user that they should refresh the page
+                    // TODO: Notify user that they should refresh the page
                 }
             }
+        }
+
+        /**
+         * Get stored git hash, or if there is none then fetch the remote git hash and save it to LocalStorage.
+         */
+        async getStoredVersionAsync() {
+            const storedVersion = await AsyncStorage.getItem(CONST.KEY_VERSION_HASH);
+            if (!storedVersion) {
+                // only get the remote version if there is no version locally stored
+                const remoteVersion = await queueRequest(CONST.COMMAND.GET_VERSION_HASH);
+                AsyncStorage.setItem(CONST.KEY_VERSION_HASH, remoteVersion);
+            }
+        }
+
+        /**
+         * Fetch the remote git hash, and compare it to the one stored in LocalStorage.
+         *
+         * If they are the same:
+         *  - save the updated version in LocalStorage
+         *  - return false
+         *
+         * Else return true
+         *
+         * @returns {boolean}
+         */
+        async pageShouldRefreshAsync() {
+            const storedVersion = await AsyncStorage.getItem(CONST.KEY_VERSION_HASH);
+
+            // If the app is offline, this request will hang indefinitely.
+            // But that's okay, because it couldn't possibly refresh anyways.
+            const remoteVersion = await queueRequest(CONST.COMMAND.GET_VERSION_HASH);
+
+            if (storedVersion === remoteVersion) {
+                if (!storedVersion) {
+                    await AsyncStorage.setItem(CONST.KEY_VERSION_HASH, remoteVersion);
+                }
+                return false;
+            }
+
+            return true;
         }
 
         render() {
