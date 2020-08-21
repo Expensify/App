1d04aade560b4286436ec8572107310b75230c87
Tim Golen <tgolen@expensify.com>
2020-08-21 13:28:20 -0600

    Get the search options to appear

--- src/page/home/sidebar/ChatSwitcherView.js
+++ src/page/home/sidebar/ChatSwitcherView.js
@@ -6,9 +6,11 @@ import {
     Text,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import _ from 'underscore';
 import styles, {colors} from '../../../style/StyleSheet';
 import WithIon from '../../../components/WithIon';
 import IONKEYS from '../../../IONKEYS';
+import Str from '../../../lib/Str';
 
 const propTypes = {
     // A method that is triggered when the TextInput gets focus
@@ -33,43 +35,112 @@ class ChatSwitcherView extends React.Component {
     constructor(props) {
         super(props);
 
+        this.maxSearchResults = 10;
+
         this.updateSearch = this.updateSearch.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
 
         this.state = {
             search: '',
+            options: [],
         };
     }
 
     /**
      * Every time the text changes in the TextInput, update the search value in the state
      *
-     * @param {string} text
+     * @param {string} value
      */
-    updateSearch(text) {
-        this.setState({search: text});
+    updateSearch(value) {
+        this.setState({search: value});
+
+        // Search our full list of options. We want:
+        // 1) Exact matches first
+        // 2) beginning-of-string matches second
+        // 3) middle-of-string matches last
+        const matchRegexes = [
+            new RegExp(`^${Str.escapeForRegExp(value)}$`, 'i'),
+            new RegExp(`^${Str.escapeForRegExp(value)}`, 'i'),
+            new RegExp(Str.escapeForRegExp(value), 'i'),
+        ];
+
+        // Use a Set so that duplicate values are automatically removed
+        let matches = new Set();
+        const searchOptions = _.chain(this.props.personalDetails)
+            .values()
+            .map(p => ({
+                text: p.displayName,
+                value: p.login,
+                valueToSearch: p.displayNameWithEmail.replace(new RegExp(/&nbsp;/g), ''),
+            }))
+            .value();
+
+        for (let i = 0; i < matchRegexes.length; i++) {
+            if (matches.size < this.maxSearchResults) {
+                for (let j = 0; j < searchOptions.length; j++) {
+                    const option = searchOptions[j];
+                    const isMatch = matchRegexes[i].test(option.valueToSearch);
+
+                    // Don't include the disabled options that match the regex unless we specified we want them to show.
+                    // If we want them to show then add them whether they match the regex or not.
+                    // Make sure we don't include the same option twice
+                    if (isMatch) {
+                        matches.add(option);
+                    }
+
+                    if (matches.size === this.maxSearchResults) {
+                        break;
+                    }
+                }
+            } else {
+                break;
+            }
+        }
+
+        matches = Array.from(matches);
+
+        const options = _(matches).map(option => ({
+            focused: false,
+            ...option
+        }));
+
+        this.setState({
+            options,
+        });
     }
 
     render() {
-        console.log(this.props.personalDetails);
         return (
-            <View style={[styles.flexRow]}>
-                <TextInput
-                    style={[styles.textInput, styles.textInputReversed, styles.flex1]}
-                    value={this.state.search}
-                    onBlur={this.props.onBlur}
-                    onChangeText={this.updateSearch}
-                    onFocus={this.props.onFocus}
-                    placeholder="Find or start a chat"
-                    placeholderTextColor={colors.textSupporting}
-                />
-                <TouchableOpacity
-                    style={[styles.chatItemSubmitButton, styles.buttonSuccess]}
-                    onPress={() => this.setState({search: ''})}
-                    underlayColor="#fff"
-                >
-                    <Text>X</Text>
-                </TouchableOpacity>
-            </View>
+            <>
+                <View style={[styles.flexRow]}>
+                    <TextInput
+                        style={[styles.textInput, styles.textInputReversed, styles.flex1]}
+                        value={this.state.search}
+                        onBlur={this.props.onBlur}
+                        onChangeText={this.updateSearch}
+                        onFocus={this.props.onFocus}
+                        onKeyDown={this.handleKeyDown}
+                        placeholder="Find or start a chat"
+                        placeholderTextColor={colors.textSupporting}
+                    />
+                    <TouchableOpacity
+                        style={[styles.chatItemSubmitButton, styles.buttonSuccess]}
+                        onPress={() => this.setState({search: ''})}
+                        underlayColor="#fff"
+                    >
+                        <Text>X</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {this.state.options.length > 0 && _.map(this.state.options, option => (
+                    <TouchableOpacity key={option.value}>
+                        <Text>
+                            {option.focused && '> '}
+                            {option.text}
+                        </Text>
+                    </TouchableOpacity>
+                ))}
+            </>
         );
     }
 }
