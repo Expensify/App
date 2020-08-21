b404d40d249cdc406fe58fdb61ef10b6d157249b
Tim Golen <tgolen@expensify.com>
2020-08-21 10:33:20 -0600

    Refactor the sidebar into separate components

---
+++ src/page/home/sidebar/SidebarBottom.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Image, View} from 'react-native';
+import PropTypes from 'prop-types';
+import styles, {getSafeAreaMargins} from '../../../style/StyleSheet';
+import Text from '../../../components/Text';
+import AppLinksView from './AppLinksView';
+import {signOut} from '../../../lib/actions/ActionsSession';
+import IONKEYS from '../../../IONKEYS';
+import {fetch as getPersonalDetails} from '../../../lib/actions/ActionsPersonalDetails';
+import WithIon from '../../../components/WithIon';
+
+const propTypes = {
+    // Safe area insets required for mobile devices margins
+    // eslint-disable-next-line react/forbid-prop-types
+    insets: PropTypes.object.isRequired,
+
+    /* Ion Props */
+
+    // The personal details of the person who is logged in
+    myPersonalDetails: PropTypes.shape({
+        // Display name of the current user from their personal details
+        userDisplayName: PropTypes.string,
+
+        // Avatar URL of the current user from their personal details
+        avatarURL: PropTypes.string,
+    }),
+
+    // Is this person offline?
+    isOffline: PropTypes.bool,
+};
+
+const defaultProps = {
+    myPersonalDetails: {},
+    isOffline: false,
+};
+
+const SidebarBottom = ({myPersonalDetails, isOffline, insets}) => (
+    <View style={[styles.sidebarFooter, getSafeAreaMargins(insets)]}>
+        <View style={[styles.sidebarFooterAvatar]}>
+            <Image
+                source={{uri: myPersonalDetails.avatarURL}}
+                style={[styles.historyItemAvatar]}
+            />
+            {isOffline && (
+                <View style={[styles.statusIndicator]} />
+            )}
+        </View>
+        <View style={[styles.flexColumn]}>
+            {myPersonalDetails.userDisplayName && (
+                <Text style={[styles.sidebarFooterUsername]}>
+                    {myPersonalDetails.userDisplayName}
+                </Text>
+            )}
+            <View style={[styles.flexRow]}>
+                <AppLinksView />
+                <Text style={[styles.sidebarFooterLink]} onPress={signOut}>Sign Out</Text>
+            </View>
+        </View>
+    </View>
+);
+
+SidebarBottom.propTypes = propTypes;
+SidebarBottom.defaultProps = defaultProps;
+SidebarBottom.displayName = 'SidebarBottom';
+
+export default WithIon({
+    // Map this.props.userDisplayName to the personal details key in the store and bind it to the displayName property
+    // and load it with data from getPersonalDetails()
+    myPersonalDetails: {
+        key: IONKEYS.MY_PERSONAL_DETAILS,
+        loader: getPersonalDetails,
+    },
+    isOffline: {
+        key: IONKEYS.NETWORK,
+        path: 'isOffline',
+        defaultValue: false,
+    },
+})(SidebarBottom);
---
+++ src/page/home/sidebar/SidebarLinks.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {View} from 'react-native';
+import _ from 'underscore';
+import PropTypes from 'prop-types';
+import styles from '../../../style/StyleSheet';
+import Text from '../../../components/Text';
+import SidebarLink from './SidebarLink';
+import WithIon from '../../../components/WithIon';
+import IONKEYS from '../../../IONKEYS';
+import {fetchAll} from '../../../lib/actions/ActionsReport';
+import Ion from '../../../lib/Ion';
+import PageTitleUpdater from '../../../lib/PageTitleUpdater/index.native';
+
+const propTypes = {
+    // Toggles the hamburger menu open and closed
+    onLinkClick: PropTypes.func.isRequired,
+
+    /* Ion Props */
+
+    // List of reports
+    reports: PropTypes.objectOf(PropTypes.shape({
+        reportID: PropTypes.number,
+        reportName: PropTypes.string,
+        hasUnread: PropTypes.bool,
+    })),
+};
+const defaultProps = {
+    reports: {},
+};
+
+const SidebarLinks = ({reports, onLinkClick}) => {
+    // Updates the page title to indicate there are unread reports
+    const hasUnreadReports = _.any(reports, report => report.hasUnread);
+    PageTitleUpdater(hasUnreadReports);
+
+    return (
+        <View style={[styles.sidebarListContainer]}>
+            <View style={[styles.sidebarListItem]}>
+                <Text style={[styles.sidebarListHeader]}>
+                    Chats
+                </Text>
+            </View>
+            {_.map(reports, report => (
+                <SidebarLink
+                    key={report.reportID}
+                    reportID={report.reportID}
+                    reportName={report.reportName}
+                    onLinkClick={onLinkClick}
+                />
+            ))}
+        </View>
+    );
+};
+
+SidebarLinks.propTypes = propTypes;
+SidebarLinks.defaultProps = defaultProps;
+SidebarLinks.displayName = 'SidebarLinks';
+
+export default WithIon({
+    reports: {
+        key: `${IONKEYS.REPORT}_[0-9]+$`,
+        addAsCollection: true,
+        collectionID: 'reportID',
+        loader: () => fetchAll().then(() => {
+            // After the reports are loaded for the first time, redirect to the first reportID in the list
+            Ion.multiGet([IONKEYS.CURRENT_URL, IONKEYS.FIRST_REPORT_ID]).then((values) => {
+                const currentURL = values[IONKEYS.CURRENT_URL] || '';
+                const firstReportID = values[IONKEYS.FIRST_REPORT_ID] || 0;
+
+                // If we're on the home page, then redirect to the first report ID
+                if (currentURL === '/' && firstReportID) {
+                    Ion.set(IONKEYS.APP_REDIRECT_TO, `/${firstReportID}`);
+                }
+            });
+        }),
+    },
+})(SidebarLinks);
