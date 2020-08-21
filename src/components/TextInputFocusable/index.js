cf979c2c414761fe210de2f0ddd7419f932d457b
Joe Gambino <joe@expensify.com>
2020-08-21 15:30:58 -0700

    use textarea rows for height

--- src/components/TextInputFocusable/index.js
+++ src/components/TextInputFocusable/index.js
@@ -5,6 +5,15 @@ import {TextInput} from 'react-native';
  * On web we like to have the Text Input field always focused so the user can easily type a new chat
  */
 class TextInputFocusable extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            numberOfLines: 1,
+        };
+        this.updateNumberOfLines = this.updateNumberOfLines.bind(this);
+    }
+
     componentDidMount() {
         this.focusInput();
     }
@@ -13,24 +22,34 @@ class TextInputFocusable extends React.Component {
         this.focusInput();
     }
 
+    updateNumberOfLines(event) {
+        const target = event.nativeEvent.target;
+        const computedStyle = window.getComputedStyle(target);
+        const lineHeight = parseInt(computedStyle.lineHeight, 10) || 20;
+        const paddingTopAndBottom = parseInt(computedStyle.paddingBottom, 10) + parseInt(computedStyle.paddingTop, 10);
+
+        // We have to reset the rows back to the minimum before updating so that the scrollHeight is not
+        // affected by the previous row setting. If we don't rows will be added, but not removed on backspace/delete.
+        this.setState({numberOfLines: 1}, () => {
+            this.setState({numberOfLines: Math.ceil((target.scrollHeight - paddingTopAndBottom) / lineHeight)});
+        });
+    }
+
     focusInput() {
         this.textInput.focus();
     }
 
     render() {
-        const scrollHeight = this.state && this.state.height ? this.state.height : 40;
+        const numberOfLines = this.state && this.state.numberOfLines ? this.state.numberOfLines : 1;
         return (
             <TextInput
                 ref={el => this.textInput = el}
                 onChange={(event) => {
-                    // eslint-disable-next-line react/no-unused-state
-                    this.setState({height: event.nativeEvent.srcElement.scrollHeight});
+                    this.updateNumberOfLines(event);
                 }}
-                minHeight={scrollHeight}
-                style={[...this.props.style, {minHeight: scrollHeight}]}
+                numberOfLines={numberOfLines}
                 /* eslint-disable-next-line react/jsx-props-no-spreading */
                 {...this.props}
-                container
             />
         );
     }
