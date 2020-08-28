d44c411f85639eb57dcec05d6aeaf61f6fed06a6
Tim Golen <tgolen@expensify.com>
2020-08-28 10:27:33 -0600

    Prevent crashing if there is no previous or current action

--- src/page/home/report/ReportHistoryView.js
+++ src/page/home/report/ReportHistoryView.js
@@ -71,6 +71,10 @@ class ReportHistoryView extends React.Component {
         const previousAction = reportHistory[historyItemIndex - 1];
         const currentAction = reportHistory[historyItemIndex];
 
+        if (!currentAction || !previousAction) {
+            return false;
+        }
+
         // Comments are only grouped if they happen within 5 minutes of each other
         if (currentAction.timestamp - previousAction.timestamp > 300) {
             return false;
