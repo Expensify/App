3e791acdbb6fbce9d0d414bd8d49c842beba627f
Andre Fonseca <afonseca@expensify.com>
2020-08-31 16:47:14 +0100

    fixing iOS

--- src/components/TextInputFocusable/index.native.js
+++ src/components/TextInputFocusable/index.native.js
@@ -5,9 +5,20 @@ import {TextInput} from 'react-native';
  * On native layers we like to have the Text Input not focused so the user can read new chats without they keyboard in
  * the way of the view
  */
-// eslint-disable-next-line react/jsx-props-no-spreading
-const TextInputFocusable = props => (<TextInput {...props} />);
+class TextInputFocusable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'TextInputFocusable';
+    }
 
-TextInputFocusable.displayName = 'TextInputFocusable';
+    clearLines() {}
+
+    render() {
+        return (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <TextInput {...this.props} />
+        );
+    }
+}
 
 export default TextInputFocusable;
