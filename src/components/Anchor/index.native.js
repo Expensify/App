cab2706b7190d7fe4707d5cac125408f59c4eb3b
Brandon Stites <bstites+1@expensify.com>
2020-08-31 16:38:15 -0600

    Separate the anchor component between web and native so that we can open linksin new tabs better on web/desktop

---
+++ src/components/Anchor/index.native.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'underscore';
+import {Linking, Text} from 'react-native';
+
+/**
+ * Text based component that is passed a URL to open onPress
+ */
+
+const propTypes = {
+    // The URL to open
+    href: PropTypes.string.isRequired,
+
+    // Any children to display
+    children: PropTypes.node,
+
+    // Any additional styles to apply
+    // eslint-disable-next-line react/forbid-prop-types
+    style: PropTypes.any,
+};
+
+const defaultProps = {
+    children: null,
+    style: {},
+};
+
+const Anchor = ({
+                   href,
+                   children,
+                   style,
+                   ...props
+               }) => {
+    // If the style prop is an array of styles, we need to mix them all together
+    const mergedStyles = !_.isArray(style) ? style : _.reduce(style, (finalStyles, s) => ({
+        ...finalStyles,
+        ...s
+    }), {});
+
+    return (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <Text style={mergedStyles} onPress={() => Linking.openURL(href)} {...props}>
+            {children}
+        </Text>
+    );
+};
+
+Anchor.propTypes = propTypes;
+Anchor.defaultProps = defaultProps;
+Anchor.displayName = 'Anchor';
+
+export default Anchor;
