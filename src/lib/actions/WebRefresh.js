5abf22201c640a009a8a4634d63dc074dc85fc4f
Rory Abraham <rory@expensify.com>
2020-09-02 09:59:32 -0700

    clean up promise chaining

--- src/lib/actions/WebRefresh.js
+++ src/lib/actions/WebRefresh.js
@@ -31,23 +31,22 @@ const appShouldRefresh = (localVersion) => {
  * 3) If the app's visibility changes or 30 minutes passes without it changing,  check if the app should refresh.
  */
 const init = () => {
-    Ion.set(IONKEYS.APP.SHOULD_REFRESH, false);
+    Ion.set(IONKEYS.APP.SHOULD_REFRESH, false)
+        .then(() => request(COMMAND_GET_VERSION)) // When the page first loads, get the current version hash
+        .then((currentVersion) => {
+            // Check periodically if we should refresh the app
+            let timer = setInterval(() => appShouldRefresh(currentVersion), REFRESH_TIMEOUT);
 
-    // When the page first loads, get the current version hash
-    request(COMMAND_GET_VERSION).then((currentVersion) => {
-        // Check periodically if we should refresh the app
-        let timer = setInterval(() => appShouldRefresh(currentVersion), REFRESH_TIMEOUT);
+            // Also check if the app's visibility changes
+            window.addEventListener('visibilitychange', () => {
+                // Reset timeout
+                clearInterval(timer);
+                timer = setInterval(() => appShouldRefresh(currentVersion), REFRESH_TIMEOUT);
 
-        // Also check if the app's visibility changes
-        window.addEventListener('visibilitychange', () => {
-            // Reset timeout
-            clearInterval(timer);
-            timer = setInterval(() => appShouldRefresh(currentVersion), REFRESH_TIMEOUT);
-
-            // Compare hashes and update Ion app_shouldRefresh
-            appShouldRefresh(currentVersion);
+                // Compare hashes and update Ion app_shouldRefresh
+                appShouldRefresh(currentVersion);
+            });
         });
-    });
 };
 
 export default init;
