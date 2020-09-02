afb156f53002aa9586e7a10b0ea05fd40e19f040
Tim Golen <tgolen@expensify.com>
2020-09-02 13:47:17 -0600

    Remove null

--- src/components/withIon.js
+++ src/components/withIon.js
@@ -116,7 +116,7 @@ export default function (mapIonToState) {
                 // Pre-fill the state with any data already in the store
                 if (mapping.initWithStoredValues !== false) {
                     Ion.getInitialStateFromConnectionID(connectionID)
-                        .then(data => this.setState({[statePropertyName]: data || null}));
+                        .then(data => this.setState({[statePropertyName]: data}));
                 }
 
                 // Load the data from an API request if necessary
