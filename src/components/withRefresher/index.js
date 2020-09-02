d95845d1447c85476fd5e0319d0fc8d228476ba1
Rory Abraham <rory@expensify.com>
2020-09-02 11:36:43 -0700

    refresh only if online

--- src/components/withRefresher/index.js
+++ src/components/withRefresher/index.js
@@ -9,6 +9,7 @@ import PropTypes from 'prop-types';
 
 import getDisplayName from '../../lib/getDisplayName';
 import InitRefresher from '../../lib/actions/WebRefresh';
+import Ion from '../Ion';
 import IONKEYS from '../../IONKEYS';
 import withIon from '../withIon';
 
@@ -26,17 +27,28 @@ export default function (WrappedComponent) {
             if (!prevProps.appShouldRefresh && this.props.appShouldRefresh) {
                 if (document.visibilityState === 'hidden') {
                     // Page is hidden, refresh immediately
-                    window.location.reload(true);
+                    this.refreshIfOnline();
                 } else if (document.visibilityState === 'visible') {
                     // TODO: Notify user in a less invasive way that they should refresh the page (i.e: Growl)
                     // Prompt user to refresh the page
                     if (window.confirm('Refresh the page to get the latest updates!')) {
-                        window.location.reload(true);
+                        this.refreshIfOnline();
                     } else {
                         this.props.appShouldRefresh = false;
                     }
                 }
             }
+        }
+
+        refreshIfOnline() {
+            Ion.get(IONKEYS.NETWORK, 'isOffline')
+                .then((isOffline) => {
+                    if (!isOffline) {
+                        window.location.reload(true);
+                    } else {
+                        this.props.appShouldRefresh = false;
+                    }
+                });
         }
 
         render() {
