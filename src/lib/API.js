7d75273aaee6c737b373c440cd6d89ccdb5d8e39
Carlos Alvarez <carlos@expensify.com>
2020-09-02 17:43:12 -0700

    Refactor

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,9 +1,28 @@
 import Ion from './Ion';
 import IONKEYS from '../IONKEYS';
 import {request} from './Network';
+import CONFIG from '../CONFIG';
 
+/**
+ * @param {object} parameters
+ * @param {string} parameters.partnerUserID
+ * @param {string} parameters.partnerUserSecret
+ * @param {string} parameters.twoFactorAuthCode
+ * @param {string} [parameters.exitTo]
+ * @returns {Promise}
+ */
 function authenticate(parameters) {
-    request('Authenticate', parameters)
+    return request('Authenticate', {
+        // When authenticating for the first time, we pass useExpensifyLogin as true so we check for credentials for
+        // the expensify partnerID to let users authenticate with their expensify user and password.
+        useExpensifyLogin: true,
+        partnerName: CONFIG.EXPENSIFY.PARTNER_NAME,
+        partnerPassword: CONFIG.EXPENSIFY.PARTNER_PASSWORD,
+        partnerUserID: parameters.partnerUserID,
+        partnerUserSecret: parameters.partnerUserSecret,
+        twoFactorAuthCode: parameters.twoFactorAuthCode,
+        exitTo: parameters.exitTo,
+    })
         .catch((err) => {
             console.error(err);
             console.debug('[SIGNIN] Request error');
@@ -11,8 +30,19 @@ function authenticate(parameters) {
         });
 }
 
+/**
+ * @param {object} parameters
+ * @param {string} parameters.authToken
+ * @param {string} parameters.partnerUserID
+ * @returns {Promise}
+ */
 function deleteLogin(parameters) {
-    return request('DeleteLogin', parameters)
+    return request('DeleteLogin', {
+        authToken: parameters.authToken,
+        partnerUserID: parameters.partnerUserID,
+        partnerName: CONFIG.EXPENSIFY.PARTNER_NAME,
+        partnerPassword: CONFIG.EXPENSIFY.PARTNER_PASSWORD,
+    })
         .catch(err => Ion.merge(IONKEYS.SESSION, {error: err.message}));
 }
 
--- src/lib/actions/Session.js
+++ src/lib/actions/Session.js
@@ -1,58 +1,28 @@
 import Ion from '../Ion';
 import * as API from '../API';
 import IONKEYS from '../../IONKEYS';
-import CONFIG from '../../CONFIG';
 import redirectToSignIn from './SignInRedirect';
 
-/**
- * Sign in with the API
- *
- * @param {string} login
- * @param {string} password
- * @param {string} twoFactorAuthCode
- * @param {string} exitTo
- * @param {boolean} useExpensifyLogin
- *
- * @returns {Promise}
- */
-function signIn(login, password, twoFactorAuthCode = '', exitTo) {
+function signIn(partnerUserID, partnerUserSecret, twoFactorAuthCode = '', exitTo) {
     return API.authenticate({
-        // When authenticating for the first time, we pass useExpensifyLogin as true so we check for credentials for
-        // the expensify partnerID to let users authenticate with their expensify user and password.
-        useExpensifyLogin: true,
-        partnerName: CONFIG.EXPENSIFY.PARTNER_NAME,
-        partnerPassword: CONFIG.EXPENSIFY.PARTNER_PASSWORD,
-        partnerUserID: login,
-        partnerUserSecret: password,
+        partnerUserID,
+        partnerUserSecret,
         twoFactorAuthCode,
         exitTo
     });
 }
 
 /**
- * Delete login
- * @param {string} authToken
- * @param {string} login
- * @returns {Promise}
- */
-function deleteLogin(authToken, login) {
-    return API.deleteLogin({
-        authToken,
-        partnerName: CONFIG.EXPENSIFY.PARTNER_NAME,
-        partnerPassword: CONFIG.EXPENSIFY.PARTNER_PASSWORD,
-        partnerUserID: login,
-    });
-}
-
-/**
- * Sign out of our application
- *
+ * Clears the Ion store and redirects user to the sign in page
  * @returns {Promise}
  */
 function signOut() {
     return redirectToSignIn()
         .then(() => Ion.multiGet([IONKEYS.SESSION, IONKEYS.CREDENTIALS]))
-        .then(data => deleteLogin(data.session.authToken, data.credentials.login))
+        .then(data => API.deleteLogin({
+            authToken: data.session.authToken,
+            partnerUserID: data.credentials.login
+        }))
         .then(Ion.clear)
         .catch(err => Ion.merge(IONKEYS.SESSION, {error: err.message}));
 }
