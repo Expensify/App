40e55200b8091824ca68cddf9e6f373edbfa39c1
Tim Golen <tgolen@expensify.com>
2020-09-03 14:17:58 -0600

    history renamed to action

--- src/page/home/report/ReportActionItemSingle.js
+++ src/page/home/report/ReportActionItemSingle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Image} from 'react-native';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
-import ReportHistoryPropsTypes from './ReportActionPropsTypes';
+import ReportActionPropTypes from './ReportActionPropTypes';
 import ReportActionItemMessage from './ReportActionItemMessage';
 import ReportActionItemFragment from './ReportActionItemFragment';
 import styles from '../../../style/StyleSheet';
@@ -10,39 +10,39 @@ import CONST from '../../../CONST';
 import ReportActionItemDate from './ReportActionItemDate';
 
 const propTypes = {
-    // All the data of the history item
-    historyItem: PropTypes.shape(ReportHistoryPropsTypes).isRequired,
+    // All the data of the action
+    action: PropTypes.shape(ReportActionPropTypes).isRequired,
 };
 
 class ReportActionItemSingle extends React.PureComponent {
     render() {
-        const {historyItem} = this.props;
-        const avatarUrl = historyItem.automatic
+        const {action} = this.props;
+        const avatarUrl = action.automatic
             ? `${CONST.CLOUDFRONT_URL}/images/icons/concierge_2019.svg`
-            : historyItem.avatar;
+            : action.avatar;
         return (
             <View style={[styles.chatItem]}>
                 <View style={[styles.chatItemLeft]}>
-                    <View style={[styles.historyItemAvatarWrapper]}>
+                    <View style={[styles.actionAvatarWrapper]}>
                         <Image
                             source={{uri: avatarUrl}}
-                            style={[styles.historyItemAvatar]}
+                            style={[styles.actionAvatar]}
                         />
                     </View>
                 </View>
                 <View style={[styles.chatItemRight]}>
                     <View style={[styles.chatItemMessageHeader]}>
-                        {historyItem.person.map(fragment => (
-                            <View key={_.uniqueId('person-', historyItem.sequenceNumber)}>
+                        {action.person.map(fragment => (
+                            <View key={_.uniqueId('person-', action.sequenceNumber)}>
                                 <ReportActionItemFragment fragment={fragment} />
                             </View>
                         ))}
                         <View>
-                            <ReportActionItemDate timestamp={historyItem.timestamp} />
+                            <ReportActionItemDate timestamp={action.timestamp} />
                         </View>
                     </View>
                     <View style={[styles.chatItemMessage]}>
-                        <ReportActionItemMessage historyItem={historyItem} />
+                        <ReportActionItemMessage action={action} />
                     </View>
                 </View>
             </View>
