884f9ba17632360cff1195b32fe3667a0a41a0a5
Tim Golen <tgolen@expensify.com>
2020-09-04 12:43:52 -0600

    camelcase the keys

--- src/IONKEYS.js
+++ src/IONKEYS.js
@@ -2,15 +2,15 @@
  * This is a file containing constants for all the top level keys in our store
  */
 export default {
-    ACTIVE_CLIENTS: 'active_clients',
-    APP_REDIRECT_TO: 'app_redirectTo',
-    CURRENT_URL: 'current_url',
+    ACTIVE_CLIENTS: 'activeClients',
+    APP_REDIRECT_TO: 'appRedirectTo',
+    CURRENT_URL: 'currentURL',
     CREDENTIALS: 'credentials',
-    FIRST_REPORT_ID: 'first_report_id',
-    MY_PERSONAL_DETAILS: 'my_personal_details',
+    FIRST_REPORT_ID: 'firstReportID',
+    MY_PERSONAL_DETAILS: 'myPersonalDetails',
     NETWORK: 'network',
-    PERSONAL_DETAILS: 'personal_details',
+    PERSONAL_DETAILS: 'personalDetails',
     REPORT: 'report',
-    REPORT_ACTIONS: 'report_actions',
+    REPORT_ACTIONS: 'reportActions',
     SESSION: 'session',
 };
