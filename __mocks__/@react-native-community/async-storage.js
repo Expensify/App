48a9931150e683cdba84e9f63fde72c718d8583d
andrew <andrew@expensify.com>
2020-09-04 16:49:53 -0700

    Tests are noisy, but are passing locally!

---
+++ __mocks__/@react-native-community/async-storage.js
@@ -0,0 +1,1 @@
+export {default} from '@react-native-community/async-storage/jest/async-storage-mock';
---
+++ __mocks__/react-native-config.js
@@ -0,0 +1,10 @@
+export default {
+    REPORT_IDS: '1,2',
+    EXPENSIFY_API_ROOT: 'https://www.expensify.com.dev/api?',
+    EXPENSIFY_PARTNER_NAME: 'android',
+    EXPENSIFY_PARTNER_PASSWORD: 'c3a9ac418ea3f152aae2',
+    PUSHER_APP_KEY: 'ac6d22b891daae55283a',
+    PUSHER_AUTH_URL: 'https://www.expensify.com.dev',
+    PARTNER_USER_ID: '',
+    PARTNER_USER_SECRET: ''
+};
