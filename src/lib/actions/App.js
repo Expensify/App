02fb2b0f7c3ce4b24c1bc24dbe87e39ca1770d4a
Brandon Stites <bstites+1@expensify.com>
2020-09-08 17:46:57 -0600

    Connect to Ion correctly

--- src/lib/actions/App.js
+++ src/lib/actions/App.js
@@ -1,14 +1,19 @@
 import Ion from '../Ion';
 import IONKEYS from '../../IONKEYS';
 
+let currentRedirectTo;
+Ion.connect({
+    key: IONKEYS.APP_REDIRECT_TO,
+    callback: val => currentRedirectTo = val,
+});
+
 /**
  * Keep the current route match stored in Ion so other libs can access it
  * Also reset the app_redirect_to in Ion so that if we go back to the current url the state will update
  *
  * @param {object} match
- * @param {string} currentRedirectTo
  */
-function recordCurrentRoute(match, currentRedirectTo) {
+function recordCurrentRoute({match}) {
     Ion.set(IONKEYS.CURRENT_URL, match.url);
     if (match.url === currentRedirectTo) {
         Ion.set(IONKEYS.APP_REDIRECT_TO, '');
