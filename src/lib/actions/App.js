87384f2b48259870b7e1069ece4011f4ed4d9907
Brandon Stites <bstites+1@expensify.com>
2020-09-08 15:03:17 -0600

    Pass currentRedirectTo and check for string before checking first character

--- src/lib/actions/App.js
+++ src/lib/actions/App.js
@@ -5,17 +5,24 @@ import IONKEYS from '../../IONKEYS';
  * Keep the current route match stored in Ion so other libs can access it
  * Also reset the app_redirect_to in Ion so that if we go back to the current url the state will update
  *
- * @param {object} params.match
+ * @param {object} match
+ * @param {string} currentRedirectTo
  */
-function recordCurrentRoute({match}) {
+function recordCurrentRoute(match, currentRedirectTo) {
     Ion.set(IONKEYS.CURRENT_URL, match.url);
-    if (match.url === this.props.redirectTo) {
+    if (match.url ===  currentRedirectTo) {
         Ion.set(IONKEYS.APP_REDIRECT_TO, '');
     }
 }
 
+
+/**
+ * Redirect the app to a new page by updating the state in Ion
+ *
+ * @param url
+ */
 function redirect(url) {
-    const formattedURL = url.charAt(0) === '/' ? url : `/${url}`;
+    const formattedURL = (typeof url === 'string' && url.startsWith('/')) ? url : `/${url}`;
     Ion.set(IONKEYS.APP_REDIRECT_TO, formattedURL);
 }
 
