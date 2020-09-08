2c330a5095a0e270c2d07f744f2dbfa02bd9748d
Rory Abraham <rory@expensify.com>
2020-09-08 14:35:46 -0700

    isClientTheLeader is a promise

--- src/lib/Notification/index.js
+++ src/lib/Notification/index.js
@@ -102,23 +102,26 @@ function push({
  * @param {Function} params.onClick
  */
 function pushReportCommentNotification({reportAction, onClick}) {
-    if (!ActiveClientManager.isClientTheLeader()) {
-        return;
-    }
+    ActiveClientManager.isClientTheLeader()
+        .then((isClientTheLeader) => {
+            if (!isClientTheLeader) {
+                return;
+            }
 
-    const {person, message} = reportAction;
+            const {person, message} = reportAction;
 
-    const plainTextPerson = Str.htmlDecode(person.map(f => f.text).join());
+            const plainTextPerson = Str.htmlDecode(person.map(f => f.text).join());
 
-    // Specifically target the comment part of the message
-    const plainTextMessage = Str.htmlDecode((message.find(f => f.type === 'COMMENT') || {}).text);
+            // Specifically target the comment part of the message
+            const plainTextMessage = Str.htmlDecode((message.find(f => f.type === 'COMMENT') || {}).text);
 
-    push({
-        title: `New message from ${plainTextPerson}`,
-        body: plainTextMessage,
-        delay: 0,
-        onClick,
-    });
+            push({
+                title: `New message from ${plainTextPerson}`,
+                body: plainTextMessage,
+                delay: 0,
+                onClick,
+            });
+        });
 }
 
 /* ====== Public Functions (signatures must match index.native.js) ====== */
