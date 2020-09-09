23aaadde52fbe581e57093e5b5b0523609afc37e
Rory Abraham <rory@expensify.com>
2020-09-08 18:49:00 -0700

    remove wrapper function for network connection check

--- src/lib/checkForUpdates.js
+++ src/lib/checkForUpdates.js
@@ -4,23 +4,21 @@ import {isOnline} from './Network';
 const UPDATE_INTERVAL = 1000 * 60 * 60;
 
 export default function (platformSpecificUpdate) {
-    const updateIfOnline = () => {
+    // Check for updates every hour
+    setInterval(() => {
         // We only want to attempt updates if we're online
         if (isOnline()) {
             platformSpecificUpdate();
         }
-    };
-
-    // Check for updates every hour
-    setInterval(updateIfOnline, UPDATE_INTERVAL);
+    }, UPDATE_INTERVAL);
 
     // On Web, we want to check for updates and refresh the page if necessary when the app is backgrounded.
     // That way, it will auto-update silently when they minimize the page,
     // and we don't bug the user any more than necessary :)
     if (Platform.OS === 'web') {
         AppState.addEventListener('change', () => {
-            if (AppState.currentState === 'background') {
-                updateIfOnline();
+            if (AppState.currentState === 'background' && isOnline()) {
+                platformSpecificUpdate();
             }
         });
     }
