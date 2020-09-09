76cd89909bc3eb248b75bcdc9c7318777404f08e
Rory Abraham <rory@expensify.com>
2020-09-08 19:13:13 -0700

    add doc and remove web code from shared library

--- index.js
+++ index.js
@@ -6,26 +6,39 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 import checkForUpdates from './src/lib/checkForUpdates';
-import {download} from './src/lib/Network';
+import {download, isOnline} from './src/lib/Network';
 
 AppRegistry.registerComponent(appName, () => App);
+
+function webUpdate(currentVersion) {
+    download('version.json')
+        .then((newVersion) => {
+            if (newVersion !== currentVersion) {
+                if (window.visibilityState === 'hidden') {
+                    // Page is hidden, refresh immediately
+                    window.location.reload(true);
+                } else if (window.confirm('Refresh the page to get the latest updates!')) {
+                    // TODO: Notify user in a less invasive way that they should refresh the page (i.e: Growl)
+                    // Prompt user to refresh the page
+                    window.location.reload(true);
+                }
+            }
+        });
+}
 
 // When app loads, get current version
 download('version.json')
     .then((currentVersion) => {
         checkForUpdates(() => {
-            download('version.json')
-                .then((newVersion) => {
-                    if (newVersion !== currentVersion) {
-                        if (window.visibilityState === 'hidden') {
-                            // Page is hidden, refresh immediately
-                            window.location.reload(true);
-                        } else if (window.confirm('Refresh the page to get the latest updates!')) {
-                            // TODO: Notify user in a less invasive way that they should refresh the page (i.e: Growl)
-                            // Prompt user to refresh the page
-                            window.location.reload(true);
-                        }
-                    }
-                });
+            webUpdate(currentVersion);
+        }, () => {
+            // We want to check for updates and refresh the page if necessary when the app is backgrounded.
+            // That way, it will auto-update silently when they minimize the page,
+            // and we don't bug the user any more than necessary :)
+            window.addEventListener('visibilitychange', () => {
+                if (window.visibilityState === 'hidden' && isOnline()) {
+                    webUpdate(currentVersion);
+                }
+            });
         });
     });
--- src/lib/checkForUpdates.js
+++ src/lib/checkForUpdates.js
@@ -1,9 +1,16 @@
-import {AppState, Platform} from 'react-native';
 import {isOnline} from './Network';
 
 const UPDATE_INTERVAL = 1000 * 60 * 60;
 
-export default function (platformSpecificUpdate) {
+/**
+ * Check for updates every hour, and perform and platform-specific update if there is a network connection.
+ *
+ * @param {Function} platformSpecificUpdate
+ * @param {Function} platformSpecificInitializer
+ */
+export default function (platformSpecificUpdate, platformSpecificInitializer = () => {}) {
+    platformSpecificInitializer();
+
     // Check for updates every hour
     setInterval(() => {
         // We only want to attempt updates if we're online
@@ -11,15 +18,4 @@ export default function (platformSpecificUpdate) {
             platformSpecificUpdate();
         }
     }, UPDATE_INTERVAL);
-
-    // On Web, we want to check for updates and refresh the page if necessary when the app is backgrounded.
-    // That way, it will auto-update silently when they minimize the page,
-    // and we don't bug the user any more than necessary :)
-    if (Platform.OS === 'web') {
-        AppState.addEventListener('change', () => {
-            if (AppState.currentState === 'background' && isOnline()) {
-                platformSpecificUpdate();
-            }
-        });
-    }
 }
