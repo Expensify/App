6b92a1f6dc0d54aa467a0d971c2bdd7f452b621d
andrew <andrew@expensify.com>
2020-09-09 10:46:12 -0700

    Continue to explore unit tests

--- tests/App-test.js
+++ tests/App-test.js
@@ -5,9 +5,14 @@
 import 'react-native';
 import React from 'react';
 
-// Note: test renderer must be required after react-native.
+// Note: `react-test-renderer` renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import {render, fireEvent} from '@testing-library/react-native';
+import {MemoryRouter} from 'react-router-dom';
+
+
 import App from '../src/App';
+import SignInPage from '../src/page/SignInPage';
 
 jest.mock('../node_modules/@react-native-community/async-storage',
     () => require('./mocks/@react-native-community/async-storage'));
@@ -18,4 +23,18 @@ jest.mock('../node_modules/react-native-config',
 
 it('renders correctly', () => {
     renderer.create(<App />);
+});
+
+describe('SampleComponent', () => {
+    test('should render', () => {
+        const component = renderer
+            .create(
+                <MemoryRouter>
+                    <SignInPage />
+                </MemoryRouter>
+            )
+            .toJSON();
+
+        expect(component).toMatchSnapshot();
+    });
 });
