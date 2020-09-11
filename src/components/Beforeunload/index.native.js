61aaa85ff2f84051106401365c778ac1305d6f0d
Tim Golen <tgolen@expensify.com>
2020-09-11 09:45:44 -0600

    Fix mobile

--- src/components/Beforeunload/index.native.js
+++ src/components/Beforeunload/index.native.js
@@ -1,6 +1,4 @@
-/**
- * Since Beforeunload is only used for the ActiveClientManager, which
- * is only necessary for web when there are multiple tabs open, this component
- * is a no-op for native devices.
- */
-export default () => {};
+// Since Beforeunload is only used for the ActiveClientManager, which
+// is only necessary for web when there are multiple tabs open, this component
+// is a no-op for native devices.
+export default ({children}) => children;
