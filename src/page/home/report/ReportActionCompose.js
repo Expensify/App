5dd10169f9d036e3f6eef1499561418fcbb913ac
chiragsalian <chirag@expensify.com>
2020-09-11 17:18:40 -0700

    comment correction

--- src/page/home/report/ReportActionCompose.js
+++ src/page/home/report/ReportActionCompose.js
@@ -41,7 +41,7 @@ class ReportActionCompose extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
-        // The first time the component loads the props is emptry and the next time it may contain value.
+        // The first time the component loads the props is empty and the next time it may contain value.
         // If it does let's update this.comment so that it matches the defaultValue that we show in textInput.
         if (this.props.comment && prevProps.comment === '' && prevProps.comment !== this.props.comment) {
             this.comment = this.props.comment;
