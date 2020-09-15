845460de1680ba937032fe7835fe88727746ad6c
Tim Golen <tgolen@expensify.com>
2020-09-15 11:13:19 -0600

    Optimize how many reports render at one time

--- src/page/home/MainView.js
+++ src/page/home/MainView.js
@@ -26,24 +26,66 @@ const defaultProps = {
     reports: {},
 };
 
-class MainView extends React.Component {
+class MainView extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.areFirstReportsRendered = false;
+        this.areAllReportsRendered = false;
+        this.reportsToRender = null;
+    }
+
+    /**
+     * This is an optimized method for rendering reports so that not too many
+     * things are rendered on the first load of the page
+     */
+    componentDidUpdate() {
+        // If all reports have been rendered, that's good, there is nothing additional to do
+        if (this.areAllReportsRendered) {
+            return;
+        }
+
+        // The first reports that get rendered are the ones that are unread, pinned, or matching the URL
+        if (!this.areFirstReportsRendered) {
+            this.areFirstReportsRendered = true;
+            this.reportsToRender = _.filter(this.props.reports, report => report.isUnread || report.pinnedReport || this.isReportIDMatchingURL(report.reportID));
+            this.forceUpdate();
+            return;
+        }
+
+        // After the first reports are rendered, then the rest of the reports are rendered
+        // after a brief delay to give the UI thread some breathing room
+        if (!this.areAllReportsRendered) {
+            this.areAllReportsRendered = true;
+            this.reportsToRender = this.props.reports;
+            setTimeout(() => this.forceUpdate(), 5000);
+        }
+    }
+
+    /**
+     * Looks to see if the reportID matches the report ID in the URL because that
+     * report needs to be the one that is visible while all the other reports are hidden
+     *
+     * @param {number} reportID
+     *
+     * @returns {boolean}
+     */
+    isReportIDMatchingURL(reportID) {
+        const reportIDInURL = parseInt(this.props.match.params.reportID, 10);
+        return reportID === reportIDInURL;
+    }
+
     render() {
-        if (!_.size(this.props.reports)) {
+        if (!_.size(this.reportsToRender)) {
             return null;
         }
 
-        const reportIDInURL = parseInt(this.props.match.params.reportID, 10);
-
-        // The styles for each of our reports. Basically, they are all hidden except for the one matching the
+        // The styles for each of our report views. Basically, they are all hidden except for the one matching the
         // reportID in the URL
-        let activeReportID;
-        const reportStyles = _.reduce(this.props.reports, (memo, report) => {
-            const isActiveReport = reportIDInURL === report.reportID;
+        const reportStyles = _.reduce(this.reportsToRender, (memo, report) => {
             const finalData = {...memo};
             let reportStyle;
 
-            if (isActiveReport) {
-                activeReportID = report.reportID;
+            if (this.isReportIDMatchingURL(report.reportID)) {
                 reportStyle = [styles.dFlex, styles.flex1];
             } else {
                 reportStyle = [styles.dNone];
@@ -55,14 +97,14 @@ class MainView extends React.Component {
 
         return (
             <>
-                {_.map(this.props.reports, report => (
+                {_.map(this.reportsToRender, report => report.reportName && (
                     <View
                         key={report.reportID}
                         style={reportStyles[report.reportID]}
                     >
                         <ReportView
                             reportID={report.reportID}
-                            isActiveReport={report.reportID === activeReportID}
+                            isActiveReport={this.isReportIDMatchingURL(report.reportID)}
                         />
                     </View>
                 ))}
