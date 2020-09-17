7ee502cbcd43bfb82f01eaf7278b4346ff8f8760
andrew <andrew@expensify.com>
2020-09-17 11:52:39 -0700

    Tweak configs

--- detox.config.js
+++ detox.config.js
@@ -2,7 +2,7 @@ module.exports = {
     configurations: {
         'ios.sim.debug': {
             binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/ReactNativeChat.app',
-            build: 'xcodebuild -project ios/ReactNativeChat.xcodeproj -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
+            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build | xcpretty',
             type: 'ios.simulator',
             device: {
                 type: 'iPhone 11'
