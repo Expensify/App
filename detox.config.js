6ebca57b9d0994835447bdc0f8e8d4c98f6434b1
andrew <andrew@expensify.com>
2020-09-17 12:59:00 -0700

    Continue config of detox

--- detox.config.js
+++ detox.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+    testRunner: 'jest',
+    runnerConfig: 'e2e/config.json',
     configurations: {
         'ios.sim.debug': {
-            binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/ReactNativeChat.app',
-            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build | xcpretty',
+            binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/Chat.app',
+            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -destination "platform=iOS Simulator,name=iPhone 11,OS=14.0" | xcpretty',
             type: 'ios.simulator',
             device: {
                 type: 'iPhone 11'
