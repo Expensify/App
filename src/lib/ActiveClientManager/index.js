be25856b776cf4eba6c2d0ecf9e842e9cbaeed8b
Tim Golen <tgolen@expensify.com>
2020-09-16 20:04:48 -0600

    Remove onbeforeunload event

--- src/lib/ActiveClientManager/index.js
+++ src/lib/ActiveClientManager/index.js
@@ -4,36 +4,35 @@ import Ion from '../Ion';
 import IONKEYS from '../../IONKEYS';
 
 const clientID = Guid();
+const maxClients = 20;
 
 let activeClients;
 Ion.connect({
     key: IONKEYS.ACTIVE_CLIENTS,
 
-    callback: val => activeClients = val,
+    callback: (val) => {
+        activeClients = val;
+        if (activeClients.length >= maxClients) {
+            activeClients.shift();
+            Ion.set(IONKEYS.ACTIVE_CLIENTS, activeClients);
+        }
+    },
 });
-
-/**
- * Remove this client ID from the list of active client IDs when this client is exited
- */
-function removeClient() {
-    Ion.set(IONKEYS.ACTIVE_CLIENTS, _.without(activeClients, clientID));
-}
 
 /**
  * Add our client ID to the list of active IDs
  */
 function init() {
     Ion.merge(IONKEYS.ACTIVE_CLIENTS, [clientID]);
-    window.addEventListener('beforeunload', removeClient);
 }
 
 /**
- * Checks if the current client is the leader (the first one in the list of active clients)
+ * The last GUID is the most recent GUID, so that should be the leader
  *
  * @returns {boolean}
  */
 function isClientTheLeader() {
-    return _.first(activeClients) === clientID;
+    return _.last(activeClients) === clientID;
 }
 
 export {
