1e248460a62e135fbb3bbd8011ed9a714786e4c8
Joe Gambino <joe@expensify.com>
2020-09-17 19:23:56 -0700

    simplify web image picker

--- src/lib/ImagePicker/index.js
+++ src/lib/ImagePicker/index.js
@@ -1,51 +1,16 @@
 // Implementation adapted from https://github.com/QuantumBA/foqum-react-native-document-picker/blob/master/web/index.js
 
-import _ from 'underscore';
-
-const {body} = document;
-const input = document.createElement('input');
-input.type = 'file';
-
-/**
- * Sets the URI value on the file
- *
- * @param {object} file
- * @returns {object}
- */
-function addUri(file) {
-    const newFile = file;
-    newFile.uri = URL.createObjectURL(file);
-
-    return newFile;
-}
-
 const ImagePicker = {
     showImagePicker(options, callback) {
-        const {files} = input;
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = function () {
+            const file = input.files[0];
+            file.uri = URL.createObjectURL(file);
 
-        // Clear previously saved files
-        if (files) {
-            input.value = '';
-        }
+            callback(file);
+        };
 
-        function onfocus() {
-            body.removeEventListener('focus', onfocus, true);
-
-            // Add a 500 millisecond delay here to ensure input.files is set before checking it
-            _.delay(() => {
-                const inputFiles = input.files;
-                if (!inputFiles.length) {
-                    callback({
-                        didCancel: true
-                    });
-                    return;
-                }
-
-                callback(addUri(inputFiles[0]));
-            }, 500);
-        }
-
-        body.addEventListener('focus', onfocus, true);
         input.click();
     },
 
