28993fd774119f807809267a02778b831ac58471
andrew <andrew@expensify.com>
2020-09-18 09:02:15 -0700

    Tweak sim used

--- detox.config.js
+++ detox.config.js
@@ -4,7 +4,7 @@ module.exports = {
     configurations: {
         'ios.sim.debug': {
             binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/Chat.app',
-            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -destination "platform=iOS Simulator,name=iPhone 11,OS=14.0" | xcpretty',
+            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -destination "platform=iOS Simulator,name=iPhone 11,OS=13.7" | xcpretty',
             type: 'ios.simulator',
             device: {
                 type: 'iPhone 11'
