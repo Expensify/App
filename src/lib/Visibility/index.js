951e0ba9df7ca43233b8d1ee383b329669371526
Marc Glasser <marc@expensify.com>
2020-09-21 13:11:10 -0700

    add docs

--- src/lib/Visibility/index.js
+++ src/lib/Visibility/index.js
@@ -1,11 +1,19 @@
+// We conditionally import the ipcRenderer here so that we can
+// communicate with the main Electron process in main.js
 const ipcRenderer = window.require ? window.require('electron').ipcRenderer : null;
 
+/**
+ * Detects whether the app is visible or not. Electron supports
+ * document.visibilityState, but switching to another app does not
+ * trigger a state of hidden so we ask the main process synchronously
+ * whether the BrowserWindow.isFocused()
+ *
+ * @returns {Boolean}
+ */
 function isVisible() {
-    if (ipcRenderer) {
-        return ipcRenderer.sendSync('request-visibility');
-    }
-
-    return document.visibilityState === 'visible';
+    return ipcRenderer
+        ? ipcRenderer.sendSync('request-visibility')
+        : document.visibilityState === 'visible';
 }
 
 export default {
