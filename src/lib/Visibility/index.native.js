2df98130bd9d05bb249640687bff5840f5fe93b9
Marc Glasser <marc@expensify.com>
2020-09-21 13:11:10 -0700

    add docs

--- src/lib/Visibility/index.native.js
+++ src/lib/Visibility/index.native.js
@@ -1,3 +1,9 @@
+// Mobile apps do not require this check for visibility as
+// they do not use the Notification lib.
+
+/**
+ * @return {Boolean}
+ */
 const isVisible = () => true;
 
 export default {
