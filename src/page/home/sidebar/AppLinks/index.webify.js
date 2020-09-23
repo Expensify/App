2273b6ff91cc59d4210d55e7061365eadea67e05
Rory Abraham <rory@expensify.com>
2020-09-23 10:45:12 -0700

    move platform-specific links to their own file

--- src/page/home/sidebar/AppLinks/index.webify.js
+++ src/page/home/sidebar/AppLinks/index.webify.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Platform} from 'react-native';
 
 import styles from '../../../../style/StyleSheet';
 import openURLInNewTab from '../../../../lib/openURLInNewTab';
@@ -7,22 +6,12 @@ import Text from '../../../../components/Text';
 
 const AppLinks = () => (
     <>
-        {Platform.OS === 'web' && (
-            <Text
-                style={[styles.sidebarFooterLink, styles.mr2]}
-                onPress={() => openURLInNewTab('https://chat.expensify.com/Chat.dmg')}
-            >
-                Desktop
-            </Text>
-        )}
-        {Platform.OS !== 'web' && (
-            <Text
-                style={[styles.sidebarFooterLink, styles.mr2]}
-                onPress={() => openURLInNewTab('https://chat.expensify.com')}
-            >
-                Web
-            </Text>
-        )}
+        <Text
+            style={[styles.sidebarFooterLink, styles.mr2]}
+            onPress={() => openURLInNewTab('https://chat.expensify.com/Chat.dmg')}
+        >
+            Desktop
+        </Text>
         <Text
             style={[styles.sidebarFooterLink, styles.mr2]}
             onPress={() => openURLInNewTab('https://testflight.apple.com/join/ucuXr4g5')}
