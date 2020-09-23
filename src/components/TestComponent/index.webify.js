1e40b31142c4836aa3ccaca1138d702a79a57f82
Rory Abraham <rory@expensify.com>
2020-09-23 10:45:12 -0700

    move platform-specific links to their own file

--- src/components/TestComponent/index.webify.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import Text from '../Text';
-
-const TestComponent = () => <View><Text color="#fff">The platform is Web!</Text></View>;
-
-export default TestComponent;
