9a38513e02cd49c7b2e06522b5bd95ad71b6df92
andrew <andrew@expensify.com>
2020-09-23 18:33:43 -0700

    Add macOS support

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -1,27 +1,26 @@
 import {Platform} from 'react-native';
-import Config from 'react-native-config';
 
 export default {
     AUTH_TOKEN_EXPIRATION_TIME: 1000 * 60 * 90,
     EXPENSIFY: {
-        API_ROOT: Config.EXPENSIFY_API_ROOT,
-        PARTNER_NAME: Config.EXPENSIFY_PARTNER_NAME,
-        PARTNER_PASSWORD: Config.EXPENSIFY_PARTNER_PASSWORD,
+        API_ROOT: 'https://www.expensify.com/api?',
+        PARTNER_NAME: 'chat-expensify-com',
+        PARTNER_PASSWORD: 'e21965746fd75f82bb66',
     },
     // eslint-disable-next-line no-undef
     IS_IN_PRODUCTION: Platform.OS === 'web' ? process.env.NODE_ENV === 'production' : !__DEV__,
     PUSHER: {
-        APP_KEY: Config.PUSHER_APP_KEY,
+        APP_KEY: '268df511a204fbb60884',
         CLUSTER: 'mt1',
     },
-    REPORT_IDS: Config.REPORT_IDS,
+    REPORT_IDS: '63212778,63212795,63212764,63212607,63699490',
     SITE_TITLE: 'Chat',
     FAVICON: {
         DEFAULT: 'favicon.png',
         UNREAD: 'favicon-unread.png'
     },
     LOGIN: {
-        PARTNER_USER_ID: Config.PARTNER_USER_ID,
-        PARTNER_USER_SECRET: Config.PARTNER_USER_SECRET,
+        PARTNER_USER_ID: '',
+        PARTNER_USER_SECRET: '',
     }
 };
