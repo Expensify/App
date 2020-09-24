8b8e20f7309a63785e56d8e90b225a165c43194b
Rory Abraham <rory@expensify.com>
2020-09-24 11:38:03 -0700

    setup push notifications in separate module

---
+++ src/lib/Notification/PushNotification/index.native.js
@@ -0,0 +1,21 @@
+import {UrbanAirship, NotificationOptionsIOS} from 'urbanairship-react-native';
+import _ from 'underscore';
+
+// By default, we set all foreground push notification options to true (only relevant to iOS)
+const foregroundPresentationOptions = _.mapObject(_.invert(NotificationOptionsIOS), () => true);
+
+/* ====== Public Functions (signatures must match index.js) ====== */
+
+/**
+ * See if user notification permissions are set, and ask for permission if not set.
+ *
+ * @returns {Promise}
+ */
+function enableUserNotifications() {
+    UrbanAirship.setForegroundPresentationOptions(foregroundPresentationOptions);
+    return UrbanAirship.enableUserPushNotifications();
+}
+
+export default {
+    enableUserNotifications,
+};
