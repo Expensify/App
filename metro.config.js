21a950b37fe3aae3c76b531fd23ea070330de435
Rory Abraham <rory@expensify.com>
2020-09-28 10:05:39 -0700

    fix index.website.js bundler for cross platform  and fix import

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,12 @@
  * @format
  */
 
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
+    resolver: {
+        blacklistRE: blacklist([new RegExp(/.*\.website\.js$/), new RegExp(/.*\.desktop\.js$/)]),
+    },
     transformer: {
         getTransformOptions: async () => ({
             transform: {
