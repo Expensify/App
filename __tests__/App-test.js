fb7406ef00549b515b6891d7497eb51a5e5ed741
Marc Glasser <marc@expensify.com>
2020-09-30 17:33:06 -0700

    undo a few things

---
+++ __tests__/App-test.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../src/App';
+
+// Note: test renderer must be required after react-native.
+
+it('renders correctly', () => {
+    renderer.create(<App />);
+});
