1d22125f59aeefa547135199cb6721c5e81381b6
Marc Glasser <marc@expensify.com>
2020-10-02 18:29:30 -0700

    fix style

--- src/components/InlineCodeBlock/index.ios.js
+++ src/components/InlineCodeBlock/index.ios.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import {webViewStyles, styles} from '../../style/StyleSheet';
+import styles, {webViewStyles} from '../../style/StyleSheet';
 
 export default ({children}) => (
     <View style={[webViewStyles.codeTagStyle, styles.mbn5]}>
