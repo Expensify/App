3890a6c7a1b89c836f971669ea91609eb9fc3552
Rory Abraham <rory@expensify.com>
2020-10-02 17:51:38 -0700

    update exports in index.js as well as index.native.js

--- src/lib/Notification/PushNotification/index.js
+++ src/lib/Notification/PushNotification/index.js
@@ -1,5 +1,5 @@
 // Push notifications are only supported on mobile, so we'll just noop here
 export default {
-    enablePushNotifications: () => {},
-    bindActionToPushNotification: () => {},
+    enable: () => {},
+    bind: () => {},
 };
