a930ae324274dc196e7cdbe13547b23244b95cda
andrew <andrew@expensify.com>
2020-10-05 14:40:34 -0700

    Try to use release configs

--- detox.config.js
+++ detox.config.js
@@ -9,6 +9,14 @@ module.exports = {
             device: {
                 type: 'iPhone 11'
             }
+        },
+        'ios.sim.release': {
+            binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/Chat.app',
+            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Release -sdk iphonesimulator -derivedDataPath ios/build -destination "platform=iOS Simulator,name=iPhone 11,OS=13.7" | xcpretty',
+            type: 'ios.simulator',
+            device: {
+                type: 'iPhone 11'
+            }
         }
     }
 };
