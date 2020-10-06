9a2841b9287c0bf6efc62d3ace1a912b66342b3e
Marc Glasser <marc@expensify.com>
2020-10-06 09:43:11 -0700

    add propTypes

--- src/components/InlineCodeBlock/index.ios.js
+++ src/components/InlineCodeBlock/index.ios.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {View} from 'react-native';
 import styles, {webViewStyles} from '../../style/StyleSheet';
 
-export default ({children}) => (
+const propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]).isRequired
+};
+
+const InlineCodeBlock = ({children}) => (
     <View style={[webViewStyles.codeTagStyle, styles.mbn5]}>
         {children}
     </View>
 );
+
+InlineCodeBlock.propTypes = propTypes;
+InlineCodeBlock.displayName = 'InlineCodeBlock';
+export default InlineCodeBlock;
