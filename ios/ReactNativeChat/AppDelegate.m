1b85d7891730c0c5842779251d6a34ed8ebbc0da
Joe Gambino <joe@expensify.com>
2020-10-06 14:48:55 -0700

    changes to get iOS reporting crashes

--- ios/ReactNativeChat/AppDelegate.m
+++ ios/ReactNativeChat/AppDelegate.m
@@ -28,13 +28,13 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  //if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  //}
+  
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-
-  if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-  }
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
