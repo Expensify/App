19ac0681a43936b810ad18b50fd7d83f4834ff5b
Marc Glasser <marc@expensify.com>
2020-10-06 16:20:44 -0700

    add resolver sourceExts to metro config to fix CONST.jsx

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,9 @@
  */
 
 module.exports = {
+    resolver: {
+       sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json'],
+    },
     transformer: {
         getTransformOptions: async () => ({
             transform: {
