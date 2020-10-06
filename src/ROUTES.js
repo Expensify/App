8fd7ee5fcd0a366ac5858cbe26130210a6efed7a
Tim Golen <tgolen@expensify.com>
2020-10-06 14:35:19 -0600

    Add routes for root and distinguish it from home

--- src/ROUTES.js
+++ src/ROUTES.js
@@ -2,8 +2,9 @@
  * This is a file containing constants for all of the routes we want to be able to go to
  */
 export default {
+    HOME: '/home',
+    REPORT: reportID => `/${reportID}`,
+    ROOT: '/',
     SIGNIN: '/signin',
     SIGNIN_WITH_EXITTO: exitTo => `/signin/exitTo${exitTo}`,
-    HOME: '/',
-    REPORT: reportID => `/${reportID}`,
 };
