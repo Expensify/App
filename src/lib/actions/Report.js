fa93402ba88add3514be02da803fb020a5ed29ae
Marc Glasser <marc@expensify.com>
2020-10-07 15:12:06 -0700

    Fix notifications sending for every comment

--- src/lib/actions/Report.js
+++ src/lib/actions/Report.js
@@ -205,10 +205,8 @@ function updateReportWithNewAction(reportID, reportAction) {
         return;
     }
 
-    const currentReportID = Number(lodashGet(currentURL.split('/'), [1], 0));
-
     // If we are currently viewing this report do not show a notification.
-    if (reportID === currentReportID && Visibility.isVisible()) {
+    if (reportID === lastViewedReportID && Visibility.isVisible()) {
         console.debug('[NOTIFICATION] No notification because it was a comment for the current report');
         return;
     }
