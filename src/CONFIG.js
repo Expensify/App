fd4ae9e9de540722d73b16c02b394fc0e60b7eae
andrew <andrew@expensify.com>
2020-10-06 18:08:48 -0700

    Test not using config, will need to revert this

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -4,17 +4,17 @@ import Config from 'react-native-config';
 export default {
     AUTH_TOKEN_EXPIRATION_TIME: 1000 * 60 * 90,
     EXPENSIFY: {
-        API_ROOT: Config.EXPENSIFY_API_ROOT,
-        PARTNER_NAME: Config.EXPENSIFY_PARTNER_NAME,
-        PARTNER_PASSWORD: Config.EXPENSIFY_PARTNER_PASSWORD,
+        API_ROOT: 'https://www.expensify.com.dev/api?',
+        PARTNER_NAME: 'android',
+        PARTNER_PASSWORD: 'c3a9ac418ea3f152aae2',
     },
     // eslint-disable-next-line no-undef
     IS_IN_PRODUCTION: Platform.OS === 'web' ? process.env.NODE_ENV === 'production' : !__DEV__,
     PUSHER: {
-        APP_KEY: Config.PUSHER_APP_KEY,
+        APP_KEY: 'ac6d22b891daae55283a',
         CLUSTER: 'mt1',
     },
-    REPORT_IDS: Config.REPORT_IDS,
+    REPORT_IDS: '1,2',
     SITE_TITLE: 'Chat',
     FAVICON: {
         DEFAULT: 'favicon.png',
