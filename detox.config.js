aef961d6dd3ced734f2dd21dd0c3d6ecbc3bdcec
andrew <andrew@expensify.com>
2020-10-06 17:22:56 -0700

    Fix lint

--- detox.config.js
+++ detox.config.js
@@ -4,7 +4,9 @@ module.exports = {
     configurations: {
         'ios.sim.debug': {
             binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/Chat.app',
-            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build -destination "platform=iOS Simulator,name=iPhone 11,OS=13.7" | xcpretty',
+            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration '
+                + 'Debug -sdk iphonesimulator -derivedDataPath ios/build -destination '
+                + '"platform=iOS Simulator,name=iPhone 11,OS=13.7" | xcpretty',
             type: 'ios.simulator',
             device: {
                 type: 'iPhone 11'
@@ -12,7 +14,8 @@ module.exports = {
         },
         'ios.sim.release': {
             binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/Chat.app',
-            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty',
+            build: 'xcodebuild -workspace ios/ReactNativeChat.xcworkspace -scheme ReactNativeChat -configuration '
+                + 'Release -sdk iphonesimulator -derivedDataPath ios/build | xcpretty',
             type: 'ios.simulator',
             name: 'iPhone 11'
         },
