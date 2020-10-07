8c2454a22164f2a8099d1345cc4acfcbca320393
andrew <andrew@expensify.com>
2020-10-06 18:08:44 -0700

    Remove mocks

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -2,13 +2,6 @@
 
 jest.setTimeout(120000 * 10);
 
-jest.mock('../node_modules/@react-native-community/async-storage',
-    () => require('./mocks/@react-native-community/async-storage'));
-jest.mock('../node_modules/@react-native-community/netinfo',
-    () => require('./mocks/@react-native-community/netinfo'));
-jest.mock('../node_modules/react-native-config',
-    () => require('./mocks/react-native-config'));
-
 describe('Example', () => {
     beforeEach(async () => {
         await device.reloadReactNative();
