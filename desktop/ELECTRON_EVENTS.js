aa1f1bd5ea1ed4b05f2270938802b7c84b20a515
Marc Glasser <marc@expensify.com>
2020-10-08 20:09:03 -0700

    make these constants consistent

--- desktop/ELECTRON_EVENTS.js
+++ desktop/ELECTRON_EVENTS.js
@@ -1,6 +1,6 @@
 const ELECTRON_EVENTS = {
-    REQUEST_UPDATE_BADGE_COUNT: 'request-update-badge-count',
-    REQUEST_VISIBILITY: 'request_visibility',
+    REQUEST_UPDATE_BADGE_COUNT: 'requestUpdateBadgeCount',
+    REQUEST_VISIBILITY: 'requestVisibility',
 };
 
 module.exports = ELECTRON_EVENTS;
