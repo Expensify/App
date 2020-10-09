e8bc374c9aaac9d513589e1110481a99a1139dfb
Rory Abraham <rory@expensify.com>
2020-10-09 16:03:32 -0700

    create CJS/ES6 library module

---
+++ src/lib/checkForUpdates/index.website.js
@@ -0,0 +1,32 @@
+import Ion from '../Ion';
+import IONKEYS from '../../IONKEYS';
+
+const UPDATE_INTERVAL = 1000 * 60 * 60;
+
+// Subscribe to network status, because we only want to attempt updates when we have a network connection.
+let isOnline;
+Ion.connect({
+    key: IONKEYS.NETWORK,
+    callback: network => isOnline = !network.isOffline,
+});
+
+/**
+ * Check for updates every hour, and perform and platform-specific update if there is a network connection.
+ *
+ * @param {Object} platformSpecificUpdater
+ * @param {Function} platformSpecificUpdater.update
+ * @param {?Function} platformSpecificUpdater.init
+ */
+export default function (platformSpecificUpdater) {
+    if (platformSpecificUpdater.init) {
+        platformSpecificUpdater.init();
+    }
+
+    // Check for updates every hour
+    setInterval(() => {
+        // We only want to attempt updates if we're online
+        if (isOnline) {
+            platformSpecificUpdater.update();
+        }
+    }, UPDATE_INTERVAL);
+}
