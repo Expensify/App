3be66e10134c404730bc999b1a38abab4344fd3d
Rory Abraham <rory@expensify.com>
2020-10-09 15:58:56 -0700

    create custom version file webpack plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CustomVersionFilePlugin = require('./CustomVersionFilePlugin');
 
 // Check for a --platform command line argument (default to 'web')
 // If it is 'web', we want to ignore .desktop.js files, and if it is 'desktop', we want to ignore .website.js files.
@@ -31,6 +32,8 @@ module.exports = {
                 {from: 'web/favicon-unread.png'},
             ],
         }),
+
+        new CustomVersionFilePlugin(),
     ],
     module: {
         rules: [
