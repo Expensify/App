331b40730ac1b0c991d334ffc58767b2e0548395
Rory Abraham <rory@expensify.com>
2020-10-09 16:57:04 -0700

    add some useful comments

--- desktop/index.js
+++ desktop/index.js
@@ -1,3 +1,7 @@
+/**
+ * This file is serving as the webpack entry point for the desktop webpack build.
+ * The main process run by electron lives in desktop/main.js.
+ */
 import {AppRegistry} from 'react-native';
 import App from '../src/App';
 
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /**
+ * Note: This file is the entry point used by React Native for the native platforms.
+ * Webpack uses different entry points for web and desktop, located at web/index.js and desktop/index.js, respectively.
+ */
+/**
  * @format
  */
 
