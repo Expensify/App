a49b5ee86e1df5a907623705d24d3479c2628767
Joe Gambino <joe@expensify.com>
2020-10-13 14:57:52 -0700

    add analytics, fix Android build issues

--- ios/ReactNativeChat/AppDelegate.m
+++ ios/ReactNativeChat/AppDelegate.m
@@ -28,9 +28,8 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  //if ([FIRApp defaultApp] == nil) {
-    [FIRApp configure];
-  //}
+  // Configure firebase
+  [FIRApp configure];
   
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
--- src/page/home/report/ReportActionCompose.js
+++ src/page/home/report/ReportActionCompose.js
@@ -10,7 +10,6 @@ import paperClipIcon from '../../../../assets/images/icon-paper-clip.png';
 import ImagePicker from '../../../lib/ImagePicker';
 import withIon from '../../../components/withIon';
 import {addAction, saveReportComment} from '../../../lib/actions/Report';
-import crashlytics from '@react-native-firebase/crashlytics';
 
 const propTypes = {
     // A method to call when the form is submitted
@@ -120,9 +119,6 @@ class ReportActionCompose extends React.Component {
      * @param {SyntheticEvent} e
      */
     showAttachmentPicker(e) {
-        crashlytics().log('joe testing log');
-        crashlytics().crash();
-
         e.preventDefault();
 
         /**
