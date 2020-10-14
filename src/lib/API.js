bba6c36aec3bee3e30049808dd9c683af34d02a2
Carlos Alvarez <carlos@expensify.com>
2020-10-14 12:40:15 -0700

    Buggie

--- src/lib/API.js
+++ src/lib/API.js
@@ -249,7 +249,8 @@ Ion.connect({
 
         // If we have an authToken but no login, it's the users first time signing in and we need to
         // create a login for the user, so when the authToken expires we can get a new one with said login
-        if (authToken && !credentials.login && !creatingLogin) {
+        var hasLogin = credentials && credentials.login;
+        if (authToken && !hasLogin && !creatingLogin) {
             creatingLogin = true;
             createLogin(Str.generateDeviceLoginID(), Guid())
                 .then(() => creatingLogin = false);
