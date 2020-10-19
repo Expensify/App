e680db4b1b652e141a735ae320098e6d9a684b36
Andre Fonseca <afonseca@expensify.com>
2020-10-19 17:19:33 +0100

    _ not used

--- src/components/TextInputFocusable/index.native.js
+++ src/components/TextInputFocusable/index.native.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {TextInput} from 'react-native';
 import PropTypes from 'prop-types';
-import _ from 'underscore';
 
 const propTypes = {
     // A ref to forward to the text input
