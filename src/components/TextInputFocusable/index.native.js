8adc46974e46f8c3edddf26415f6fad7b957065e
Andre Fonseca <afonseca@expensify.com>
2020-10-27 17:23:43 +0000

    the proptypes need to be in the native side as well

--- src/components/TextInputFocusable/index.native.js
+++ src/components/TextInputFocusable/index.native.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import {TextInput} from 'react-native';
+import PropTypes from 'prop-types';
 
 /**
  * On native layers we like to have the Text Input not focused so the user can read new chats without they keyboard in
  * the way of the view
  */
+
+const propTypes = {
+    // If the input should clear, it actually gets intercepted instead of .clear()
+    shouldClear: PropTypes.bool,
+
+    // When the input has cleared whoever owns this input should know about it
+    onClear: PropTypes.func,
+};
+
+const defaultProps = {
+    shouldClear: false,
+    onClear: null,
+};
+
 class TextInputFocusable extends React.Component {
+    componentDidUpdate(prevProps) {
+        if (!prevProps.shouldClear && this.props.shouldClear) {
+            this.textInput.clear();
+            if (this.props.onClear) {
+                this.props.onClear();
+            }
+        }
+    }
+
     render() {
         return (
             <TextInput
@@ -19,4 +43,6 @@ class TextInputFocusable extends React.Component {
 }
 
 TextInputFocusable.displayName = 'TextInputFocusable';
+TextInputFocusable.propTypes = propTypes;
+TextInputFocusable.defaultProps = defaultProps;
 export default TextInputFocusable;
