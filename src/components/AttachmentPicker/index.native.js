e27da2b389fce344a048605c624d6ddf3c301464
Marc Glasser <marc@expensify.com>
2020-10-29 08:42:28 -0700

    Use a function component

--- src/components/AttachmentPicker/index.native.js
+++ src/components/AttachmentPicker/index.native.js
@@ -1,4 +1,3 @@
-import React, {createRef} from 'react';
 import PropTypes from 'prop-types';
 import AttachmentPickerNative from '../../libs/AttachmentPickerNative';
 
@@ -6,22 +5,13 @@ const propTypes = {
     children: PropTypes.func.isRequired,
 };
 
-class AttachmentPicker extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onChangeCallback = createRef();
-    }
-
-    render() {
-        return this.props.children({
-            show: (callback) => {
-                AttachmentPickerNative.show((response) => {
-                    callback(AttachmentPickerNative.getDataForUpload(response));
-                });
-            },
+const AttachmentPicker = ({children}) => children({
+    show: (callback) => {
+        AttachmentPickerNative.show((response) => {
+            callback(AttachmentPickerNative.getDataForUpload(response));
         });
-    }
-}
+    },
+});
 
 AttachmentPicker.propTypes = propTypes;
 export default AttachmentPicker;
