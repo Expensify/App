55dc466c97618af90a4dc7759a0d82ef9d64a0eb
andrew <andrew@expensify.com>
2020-11-05 10:51:10 -0800

    Apply peer review changes

--- tests/e2e/environment.js
+++ tests/e2e/environment.js
@@ -8,11 +8,7 @@ class CustomDetoxEnvironment extends DetoxCircusEnvironment {
     constructor(config) {
         super(config);
 
-        // Can be safely removed, if you are content with the default value (=300000ms)
-        this.initTimeout = 300000;
-
         // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
-        // This is strictly optional.
         this.registerListeners({
             SpecReporter,
             WorkerAssignReporter,
