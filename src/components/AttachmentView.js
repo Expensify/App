ff8c70b4ce88c0fdaa9e33988ddea0145f54bd61
Jack Nam <jack@expensify.com>
2020-11-09 15:47:39 -0800

    DRY it up

---
+++ src/components/AttachmentView.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../styles/StyleSheet';
+import Str from '../libs/Str';
+import PDFView from './PDFView';
+import ImageView from './ImageView';
+
+const propTypes = {
+    // URL to full-sized attachment
+    sourceURL: PropTypes.string.isRequired,
+
+    // Height of image
+    imageHeight: PropTypes.number,
+
+    // Width of image
+    imageWidth: PropTypes.number,
+};
+
+const defaultProps = {
+    imageHeight: 200,
+    imageWidth: 200,
+};
+
+const AttachmentView = props => (
+    <>
+        {(Str.isPDF(props.sourceURL)) ? (
+            <PDFView
+                sourceURL={props.sourceURL}
+                style={styles.imageModalPDF}
+            />
+        ) : (
+            <ImageView
+                imageWidth={props.imageWidth}
+                imageHeight={props.imageHeight}
+                sourceURL={props.sourceURL}
+            />
+        )}
+    </>
+);
+
+AttachmentView.propTypes = propTypes;
+AttachmentView.defaultProps = defaultProps;
+AttachmentView.displayName = 'AttachmentView';
+
+export default AttachmentView;
