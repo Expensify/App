a76d49f6d9e86df39a7a6dc3f5f155bdc67c6360
Tim Golen <tgolen@expensify.com>
2020-11-12 14:24:21 -0700

    Move get report history to new API

--- src/libs/API.js
+++ src/libs/API.js
@@ -412,22 +412,9 @@ function setLastReadActionID(parameters) {
     });
 }
 
-/**
- * @param {object} parameters
- * @param {number} parameters.reportID
- * @returns {Promise}
- */
-function getReportHistory(parameters) {
-    return queueRequest('Report_GetHistory', {
-        authToken,
-        reportID: parameters.reportID,
-    });
-}
-
 export {
     authenticate,
     deleteLogin,
     getAuthToken,
-    getReportHistory,
     setLastReadActionID,
 };
