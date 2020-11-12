38617412c258932324fd2edccc8142f115af90d2
Tim Golen <tgolen@expensify.com>
2020-11-12 14:06:13 -0700

    Add queued network requests

--- src/libs/Network.js
+++ src/libs/Network.js
@@ -1,25 +1,76 @@
+import _ from 'underscore';
+import Onyx from 'react-native-onyx';
 import HttpUtils from './HttpUtils';
+import NetworkConnection from './NetworkConnection';
+import ONYXKEYS from '../ONYXKEYS';
+
+// Queue for network requests so we don't lose actions done by the user while offline
+let networkRequestQueue = [];
+
+// We subscribe to changes to the online/offline status of the network to determine when we should fire off API calls
+// vs queueing them for later.
+let isOffline;
+Onyx.connect({
+    key: ONYXKEYS.NETWORK,
+    callback: val => isOffline = val && val.isOffline,
+});
 
 /**
- * Adds our API command to the URL so the API call is more easily identified in the
- * network tab of the JS console
- *
- * @param {string} command
- * @param {string} url
- * @returns {string}
+ * Process the networkRequestQueue by looping through the queue and attempting to make the requests
  */
-function addCommandToUrl(command, url) {
-    let newUrl = url;
+function processNetworkRequestQueue() {
+    if (isOffline) {
+        // Two things will bring the app online again...
+        // 1. Pusher reconnecting (see registerSocketEventCallback in this file)
+        // 2. Getting a 200 response back from the API (happens right below)
 
-    if (command) {
-        // Add a ? to the end of the URL if there isn't one already
-        if (newUrl.indexOf('?') === -1) {
-            newUrl = `${newUrl}?`;
-        }
-        newUrl = `${newUrl}&command=${command}`;
+        // Make a simple request every second to see if the API is online again
+        HttpUtils.xhr('Get', {doNotRetry: true})
+            .then(() => NetworkConnection.setOfflineStatus(false));
+        return;
     }
 
-    return newUrl;
+    // @TODO figure out how reauthenticating will work
+    // // Don't make any requests until we're done re-authenticating since we'll use the new authToken
+    // // from that response for the subsequent network requests
+    // if (reauthenticating || networkRequestQueue.length === 0) {
+    //     return;
+    // }
+
+    _.each(networkRequestQueue, (queuedRequest) => {
+        HttpUtils.xhr(queuedRequest.command, queuedRequest.data, queuedRequest.type)
+            .then(queuedRequest.resolve)
+            .catch(queuedRequest.reject);
+    });
+
+    networkRequestQueue = [];
+}
+
+// Process our write queue very often
+setInterval(processNetworkRequestQueue, 1000);
+
+/**
+ * Adds a request to networkRequestQueue
+ *
+ * @param {string} command
+ * @param {mixed} data
+ * @param {string} type
+ * @returns {Promise}
+ */
+function queueRequest(command, data, type) {
+    return new Promise((resolve, reject) => {
+        // Add the write request to a queue of actions to perform
+        networkRequestQueue.push({
+            command,
+            data,
+            type,
+            resolve,
+            reject,
+        });
+
+        // Try to fire off the request as soon as it's queued so we don't add a delay to every queued command
+        processNetworkRequestQueue();
+    });
 }
 
 /**
@@ -33,8 +84,8 @@ export default function Network(endpoint) {
     }
 
     return {
-        post(command, parameters, type) {
-            HttpUtils.xhr(command, parameters, type);
+        post(command, data, type) {
+            queueRequest(command, data, type);
         },
     };
 }
