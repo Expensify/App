7bf0f9c10e744374c16e56cba399e65d358936a6
alberto@expensify.com <alberto@expensify.com>
2020-11-20 10:47:31 +0100

    resulve warnings

--- src/pages/home/sidebar/SidebarLinks.js
+++ src/pages/home/sidebar/SidebarLinks.js
@@ -36,7 +36,11 @@ const propTypes = {
 
     isChatSwitcherActive: PropTypes.bool,
 
-    personalDetails: PropTypes.object,
+    personalDetails: PropTypes.objectOf(PropTypes.shape({
+        login: PropTypes.string.isRequired,
+        avatarURL: PropTypes.string.isRequired,
+        displayName: PropTypes.string.isRequired,
+    })),
 };
 const defaultProps = {
     reports: {},
@@ -85,6 +89,8 @@ const SidebarLinks = (props) => {
                 {/* A report will not have a report name if it hasn't been fetched from the server yet */}
                 {/* so nothing is rendered */}
                 {_.map(reportsToDisplay, (report) => {
+
+                    // eslint-disable-next-line max-len
                     const participantDetails = get(report, 'participants.length', 0) === 1 ? get(props.personalDetails, report.participants[0], '') : '';
                     return report.reportName && (
                         <ChatLinkRow
