bb8ed24098056cb86e5b25492b32969d35d82fdb
Tim Golen <tgolen@expensify.com>
2020-11-24 09:52:03 -0700

    Fix warning about setBackgroundColor only working on Android

---
+++ src/components/CustomStatusBar/index.android.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+export default class CustomStatusBar extends React.Component {
+    componentDidMount() {
+        StatusBar.setBarStyle('dark-content', true);
+        StatusBar.setBackgroundColor('transparent', true);
+        StatusBar.setTranslucent(true);
+    }
+
+    render() {
+        return <StatusBar />;
+    }
+}
