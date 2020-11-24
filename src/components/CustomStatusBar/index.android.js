3c493ab3e500cc9c81e7131b11668359624c3b2c
Tim Golen <tgolen@expensify.com>
2020-11-24 10:14:35 -0700

    Adding a comment

--- src/components/CustomStatusBar/index.android.js
+++ src/components/CustomStatusBar/index.android.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 
+/**
+ * Only the Android platform supports "setBackgroundColor" and "setTranslucent"
+ */
+
 export default class CustomStatusBar extends React.Component {
     componentDidMount() {
         StatusBar.setBarStyle('dark-content', true);
