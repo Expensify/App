e44589afea8fb0af608c53b164b1fdb8a63ce74f
Tim Golen <tgolen@expensify.com>
2020-11-24 10:01:19 -0700

    Fix another platform specific warning

--- src/components/CustomStatusBar/index.js
+++ src/components/CustomStatusBar/index.js
@@ -4,7 +4,6 @@ import {StatusBar} from 'react-native';
 export default class CustomStatusBar extends React.Component {
     componentDidMount() {
         StatusBar.setBarStyle('dark-content', true);
-        StatusBar.setTranslucent(true);
     }
 
     render() {
