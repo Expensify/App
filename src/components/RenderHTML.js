e0af40051abd8956dd709bcea9ffb250f62bdb0f
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-11-27 15:43:30 -0300

    Move HTML rendering logic from ReportActionItemFragment to RenderHTML
    A new RenderHTML component is available in src/components/RenderHTML.js.
    It lightens the aforementioned component, and help refine HTML logic.
    The new blockquoteRenderer has been dropped since it only inject styles
    to the renderer.

--- src/components/RenderHTML.js
+++ src/components/RenderHTML.js
@@ -1,17 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Platform, View, useWindowDimensions} from 'react-native';
+import {useWindowDimensions} from 'react-native';
 import HTML, {
     defaultHTMLElementModels,
     TNodeChildrenRenderer,
-    splitBoxModelStyle
+    splitBoxModelStyle,
 } from 'react-native-render-html';
 import Config from '../CONFIG';
 import {getAuthToken} from '../libs/API';
-import styles, {webViewStyles} from '../styles/StyleSheet';
+import {webViewStyles} from '../styles/StyleSheet';
 import fontFamily from '../styles/fontFamily';
 import AnchorForCommentsOnly from './AnchorForCommentsOnly';
 import ImageThumbnailWithModal from './ImageThumbnailWithModal';
+import InlineCodeBlock from './InlineCodeBlock';
 
 const MAX_IMG_DIMENSIONS = 512;
 
@@ -27,19 +29,12 @@ function computeImagesMaxWidth(contentWidth) {
     return Math.min(MAX_IMG_DIMENSIONS, contentWidth);
 }
 
-function PreRenderer({key, tnode}) {
-    return (
-        <View key={key} style={webViewStyles.preTagStyle}>
-            <TNodeChildrenRenderer tnode={tnode} />
-        </View>
-    );
-}
-
 function AnchorRenderer({tnode, key, style}) {
     const htmlAttribs = tnode.attributes;
     return (
         <AnchorForCommentsOnly
             href={htmlAttribs.href}
+
             // Unless otherwise specified open all links in
             // a new window. On Desktop this means that we will
             // skip the default Save As... download prompt
@@ -48,31 +43,35 @@ function AnchorRenderer({tnode, key, style}) {
             target={htmlAttribs.target || '_blank'}
             rel={htmlAttribs.rel || 'noopener noreferrer'}
             style={style}
-            key={key}>
+            key={key}
+        >
             <TNodeChildrenRenderer tnode={tnode} />
         </AnchorForCommentsOnly>
     );
 }
 
-function CodeRenderer({key, style, TDefaultRenderer, ...defaultProps}) {
+function CodeRenderer({
+    key,
+    style,
+    TDefaultRenderer, ...defaultRendererProps
+}) {
     // We split wrapper and inner styles
-    // "boxModel" corresponds to border, margin, padding and backgroundColor
-    const { boxModel, rest } = splitBoxModelStyle(style);
+    // "boxModelStyle" corresponds to border, margin, padding and backgroundColor
+    const {boxModelStyle, otherStyle: textStyle} = splitBoxModelStyle(style);
     return (
-        <View style={boxModel} key={key}>
-            <TDefaultRenderer
-                style={[
-                    rest,
-                    Platform.select({ios: styles.mbn5, default: undefined}),
-                ]}
-                {...defaultProps}
-            />
-        </View>
+        <InlineCodeBlock
+            TDefaultRenderer={TDefaultRenderer}
+            boxModelStyle={boxModelStyle}
+            textStyle={textStyle}
+            defaultRendererProps={defaultRendererProps}
+            key={key}
+        />
     );
 }
 
 function ImgRenderer({key, tnode}) {
     const htmlAttribs = tnode.attributes;
+
     // Attaches authTokens as a URL parameter to load image attachments
     let previewSource = htmlAttribs['data-expensify-source']
         ? `${htmlAttribs.src}?authToken=${getAuthToken()}`
@@ -102,21 +101,19 @@ function ImgRenderer({key, tnode}) {
 }
 
 AnchorRenderer.model = defaultHTMLElementModels.a;
-PreRenderer.model = defaultHTMLElementModels.pre;
 CodeRenderer.model = defaultHTMLElementModels.code;
 ImgRenderer.model = defaultHTMLElementModels.img;
 
 // Define the custom render methods
 const renderers = {
     a: AnchorRenderer,
-    pre: PreRenderer,
     code: CodeRenderer,
     img: ImgRenderer,
 };
 
 const propTypes = {
     html: PropTypes.string.isRequired,
-    debug: PropTypes.bool
+    debug: PropTypes.bool,
 };
 
 const RenderHTML = ({html, debug = false}) => {
@@ -144,5 +141,8 @@ const RenderHTML = ({html, debug = false}) => {
 
 RenderHTML.displayName = 'RenderHTML';
 RenderHTML.propTypes = propTypes;
+RenderHTML.defaultProps = {
+    debug: false,
+};
 
 export default RenderHTML;
