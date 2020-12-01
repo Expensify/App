ff9e0ec7c36aea4dda1cdcde762294ce31cfc94c
Marc Glasser <marc.aaron.glasser@gmail.com>
2020-12-01 08:36:55 -1000

    make proxy work for files

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -6,16 +6,27 @@ const common = require('./webpack.common.js');
 
 const env = dotenv.config({path: path.resolve(__dirname, '../../.env')}).parsed;
 
-module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, '../dist'),
-        hot: true,
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            __REACT_WEB_CONFIG__: JSON.stringify(env),
-        })
-    ]
-});
+module.exports = ({proxy}) => {
+    const proxySettings = {};
+    if (proxy) {
+        proxySettings.proxy = {
+            '/api': 'http://localhost:9000',
+            '/chat-attachments': 'http://localhost:9000',
+        };
+    }
+
+    return merge(common, {
+        mode: 'development',
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, '../dist'),
+            hot: true,
+            ...proxySettings,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                __REACT_WEB_CONFIG__: JSON.stringify(env),
+            })
+        ]
+    });
+}
