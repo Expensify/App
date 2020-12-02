d22079a6e7c3da48643de1ad0008fa290d487da2
Andrew Gable <andrewgable@gmail.com>
2020-12-02 14:51:57 -0700

    Revert "React Native Render HTML: whitespace collapsing"

--- src/components/AnchorForCommentsOnly/index.js
+++ src/components/AnchorForCommentsOnly/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {StyleSheet} from 'react-native';
-import anchorForCommentsOnlyPropTypes from './anchorForCommentsOnlyPropTypes';
+
+/**
+ * Text based component that is passed a URL to open onPress
+ */
+
+const propTypes = {
+    // The URL to open
+    href: PropTypes.string,
+
+    // What headers to send to the linked page (usually noopener and noreferrer)
+    rel: PropTypes.string,
+
+    // Used to determine where to open a link ("_blank" is passed for a new tab)
+    target: PropTypes.string,
+
+    // Any children to display
+    children: PropTypes.node,
+
+    // Any additional styles to apply
+    // eslint-disable-next-line react/forbid-prop-types
+    style: PropTypes.any,
+};
 
 const defaultProps = {
     href: '',
@@ -30,7 +52,7 @@ const AnchorForCommentsOnly = ({
     </a>
 );
 
-AnchorForCommentsOnly.propTypes = anchorForCommentsOnlyPropTypes;
+AnchorForCommentsOnly.propTypes = propTypes;
 AnchorForCommentsOnly.defaultProps = defaultProps;
 AnchorForCommentsOnly.displayName = 'AnchorForCommentsOnly';
 
