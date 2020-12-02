e188ad4787b5fdf9d802aa5e92410c158e4a154d
Marc Glasser <marc.aaron.glasser@gmail.com>
2020-12-02 12:46:35 -1000

    fix const again

--- src/CONST.js
+++ src/CONST.js
@@ -1,7 +1,10 @@
 import CONFIG from './CONFIG';
+const CLOUDFRONT_URL = 'https://d2k5nsl2zxldvw.cloudfront.net';
 
-export default {
-    CLOUDFRONT_URL: 'https://d2k5nsl2zxldvw.cloudfront.net',
+const CONST = {
+    CLOUDFRONT_URL,
     PDF_VIEWER_URL: `${CONFIG.EXPENSIFY.URL_EXPENSIFY_CASH}/pdf-viewer/web/viewer.html`,
     EXPENSIFY_ICON_URL: `${CLOUDFRONT_URL}/images/favicon-2019.png`,
 };
+
+export default CONST;
