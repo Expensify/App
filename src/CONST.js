03a8d454d0f69c70d8822438547b98adaa19a4de
Marc Glasser <marc.aaron.glasser@gmail.com>
2020-12-02 12:42:14 -1000

    fix const

--- src/CONST.js
+++ src/CONST.js
@@ -1,10 +1,7 @@
-const CLOUDFRONT_URL = 'https://d2k5nsl2zxldvw.cloudfront.net';
-const PDF_VIEWER_URL = '/pdf-viewer/web/viewer.html';
+import CONFIG from './CONFIG';
 
-const CONST = {
-    CLOUDFRONT_URL,
-    PDF_VIEWER_URL,
+export default {
+    CLOUDFRONT_URL: 'https://d2k5nsl2zxldvw.cloudfront.net',
+    PDF_VIEWER_URL: `${CONFIG.EXPENSIFY.URL_EXPENSIFY_CASH}/pdf-viewer/web/viewer.html`,
     EXPENSIFY_ICON_URL: `${CLOUDFRONT_URL}/images/favicon-2019.png`,
 };
-
-export default CONST;
