5c255ccd5dcc38c026b0d425a51a8dd72ae08528
Marc Glasser <marc.aaron.glasser@gmail.com>
2020-12-07 13:33:50 -1000

    add the notfound page - needs styling

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const NotFound = props => (
+    <View>
+        <Text>Report Not Found</Text>
+    </View>
+);
+
+export default NotFound;
