413cf6c51e23ea7e607325d1551343f969ce5abc
Rory Abraham <rory@expensify.com>
2020-12-08 10:22:32 -0800

    make utility function for matchMedia and make ipad regex case-insensitive

--- src/libs/InterfaceType/index.js
+++ src/libs/InterfaceType/index.js
@@ -1,24 +1,34 @@
 import _ from 'underscore';
 import INTERFACE_TYPES from './INTERFACE_TYPES';
 
+/**
+ * Does a window media query.
+ *
+ * @param {String} query
+ * @returns {boolean}
+ */
+function matchMedia(query) {
+    return window.matchMedia(query).matches;
+}
+
 const userAgent = window.navigator.userAgent;
 const hasTouchStartInWindow = _.has(window, 'ontouchstart');
 const supportsTouchEvents = hasTouchStartInWindow
-    || (_.has(window, 'TouchEvent') && window.matchMedia('(any-pointer: coarse)').matches);
+    || (_.has(window, 'TouchEvent') && matchMedia('(any-pointer: coarse)'));
 const hasTouch = (window.navigator.maxTouchPoints || 0) > 0 || supportsTouchEvents;
 
 // iPads now support a mouse that can hover, however the media query interaction
 // feature results always say iPads only have a coarse pointer that can't hover
 // even when a mouse is connected
-const isIPad = window.matchMedia('(pointer: coarse)').matches && /Ipad/.test(userAgent);
+const isIPad = matchMedia('(pointer: coarse)') && /ipad/i.test(userAgent);
 
 // Assume we're dealing with a mouse-only interface
 let interfaceType = INTERFACE_TYPES.MOUSE_ONLY;
 if (hasTouch) {
     if (!hasTouchStartInWindow
-        || !window.matchMedia('(pointer: coarse)').matches
-        || window.matchMedia('(any-pointer: fine)').matches
-        || window.matchMedia('(any-hover: hover)').matches
+        || !matchMedia('(pointer: coarse)')
+        || matchMedia('(any-pointer: fine)')
+        || matchMedia('(any-hover: hover)')
         || isIPad) {
         interfaceType = INTERFACE_TYPES.HYBRID;
     } else {
