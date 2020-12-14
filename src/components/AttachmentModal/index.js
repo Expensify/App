f9df8f01f42d76dbd6ad55f82af309ec87690570
Rory Abraham <rory@expensify.com>
2020-12-14 08:52:21 -0800

    revert changes to AttachmentModal

--- src/components/AttachmentModal/index.js
+++ src/components/AttachmentModal/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import AttachmentModalBase from './AttachmentModalBase';
 import propTypes from './attachmentModalPropTypes';
 
@@ -7,46 +7,14 @@ const defaultProps = {
     isAuthTokenRequired: false,
 };
 
-class AttachmentModal extends Component {
-    constructor(props) {
-        super(props);
-        this.modalRef = React.createRef();
-
-        this.handleKeyboardEvent = this.handleKeyboardEvent.bind(this);
-    }
-
-    componentDidMount() {
-        document.onkeyup = this.handleKeyboardEvent;
-    }
-
-    handleKeyboardEvent(e) {
-        if (!e) {
-            return;
-        }
-
-        e.preventDefault();
-
-        if (e.key === 'Enter') {
-            this.modalRef.current.confirmAndClose();
-        }
-
-        if (e.key === 'Escape') {
-            this.modalRef.current.close();
-        }
-    }
-
-    render() {
-        return (
-            <AttachmentModalBase
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...this.props}
-                ref={this.modalRef}
-            >
-                {this.props.children}
-            </AttachmentModalBase>
-        );
-    }
-}
+const AttachmentModal = props => (
+    <AttachmentModalBase
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+    >
+        {props.children}
+    </AttachmentModalBase>
+);
 
 AttachmentModal.propTypes = propTypes;
 AttachmentModal.defaultProps = defaultProps;
