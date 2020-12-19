152ffa8322bfa8a66cd3d77228d0ef2429024d8f
Tim Golen <tgolen@expensify.com>
2020-12-19 08:13:29 -0700

    Add the skeleton for a submit button

---
+++ src/pages/signin/SubmitButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const SubmitButton = () => (
+    <>
+    </>
+);
+
+SubmitButton.propTypes = propTypes;
+SubmitButton.displayName = 'SubmitButton';
+
+export default SubmitButton;
