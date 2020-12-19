aca6301c61e094f19d9a816fb85bf55befb2df72
Tim Golen <tgolen@expensify.com>
2020-12-19 09:06:14 -0700

    Add a form for resending the validation link

---
+++ src/pages/signin/ResendValidationForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../../styles/styles';
+import SubmitButton from './SubmitButton';
+import openURLInNewTab from '../../libs/openURLInNewTab';
+
+const propTypes = {
+    // A function that is called when the form is submitted
+    onSubmit: PropTypes.func.isRequired,
+};
+
+class ResendValidationForm extends React.Component {
+    constructor() {
+        super();
+
+        this.validateAndSubmitForm = this.validateAndSubmitForm.bind(this);
+
+        this.state = {
+            isLoading: false,
+        };
+    }
+
+    /**
+     * Check that all the form fields are valid, then trigger the submit callback
+     */
+    validateAndSubmitForm() {
+        this.setState({
+            isLoading: true,
+        });
+        this.props.onSubmit({
+            login: this.state.login,
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <View>
+                    <Text style={[styles.textP]}>
+                        Please validate your account by clicking on the link we just sent you.
+                    </Text>
+                </View>
+                <View style={[styles.mt4]}>
+                    <SubmitButton
+                        text="Resend Link"
+                        isLoading={this.state.isLoading}
+                        onClick={this.validateAndSubmitForm}
+                    />
+                </View>
+            </>
+        );
+    }
+}
+
+ResendValidationForm.propTypes = propTypes;
+
+export default ResendValidationForm;
