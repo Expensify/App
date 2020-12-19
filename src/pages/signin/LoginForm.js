a989f421d84f03db9075c597a19224362ec2004a
Tim Golen <tgolen@expensify.com>
2020-12-19 08:11:44 -0700

    Add a form field to the form

--- src/pages/signin/LoginForm.js
+++ src/pages/signin/LoginForm.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../../styles/styles';
 
-const propTypes = {};
+const propTypes = {
+    // A function that is called when the form is submitted
+    onSubmit: PropTypes.function.isRequired,
+};
 
-const LoginForm = () => (
-    <>
-    </>
-);
+class LoginForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            login: '',
+        };
+    }
+
+    render() {
+        return (
+            <View style={[styles.mb4]}>
+                <Text style={[styles.formLabel]}>Login</Text>
+                <TextInput
+                    style={[styles.textInput]}
+                    value={this.state.login}
+                    autoCompleteType="email"
+                    textContentType="username"
+                    onChangeText={text => this.setState({login: text})}
+                    onSubmitEditing={this.props.submitForm}
+                    autoCapitalize="none"
+                    placeholder="Email or phone"
+                />
+            </View>
+        );
+    }
+}
 
 LoginForm.propTypes = propTypes;
-LoginForm.displayName = 'LoginForm';
 
 export default LoginForm;
