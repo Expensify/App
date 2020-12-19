01fcd645384e06b8155339a9c18030abd43f6738
Tim Golen <tgolen@expensify.com>
2020-12-19 08:07:50 -0700

    Add the skeleton for the new login form

---
+++ src/pages/signin/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const propTypes = {};
+
+const LoginForm = () => (
+    <>
+    </>
+);
+
+LoginForm.propTypes = propTypes;
+LoginForm.displayName = 'LoginForm';
+
+export default LoginForm;
