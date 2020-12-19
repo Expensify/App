9fe4c4b3a2a2d062cecb7e47db54e18314b1fe40
Tim Golen <tgolen@expensify.com>
2020-12-19 08:16:44 -0700

    Add elements and props to the submit button

--- src/pages/signin/SubmitButton.js
+++ src/pages/signin/SubmitButton.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+    ActivityIndicator, Text, TouchableOpacity, View
+} from 'react-native';
+import styles from '../../styles/styles';
+import themeColors from '../../styles/themes/default';
 
-const propTypes = {};
+const propTypes = {
+    // The text for the button label
+    text: PropTypes.string.isRequired,
 
-const SubmitButton = () => (
-    <>
-    </>
+    // Indicates whether the button should be disabled and in the loading state
+    isLoading: PropTypes.bool.isRequired,
+
+    // A function that is called when the button is clicked on
+    onClick: PropTypes.function.isRequired,
+};
+
+const SubmitButton = props => (
+    <TouchableOpacity
+        style={[styles.button, styles.buttonSuccess, styles.mb4]}
+        onPress={props.onClick}
+        underlayColor={themeColors.componentBG}
+        disabled={props.isLoading}
+    >
+        {props.isLoading ? (
+            <ActivityIndicator color={themeColors.textReversed} />
+        ) : (
+            <Text style={[styles.buttonText, styles.buttonSuccessText]}>
+                {props.text}
+            </Text>
+        )}
+    </TouchableOpacity>
 );
 
 SubmitButton.propTypes = propTypes;
