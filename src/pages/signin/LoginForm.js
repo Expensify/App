6c40910da8ba7240dbf20129fb1022af190bdac7
Tim Golen <tgolen@expensify.com>
2020-12-19 08:21:45 -0700

    Add form validation and submit form

--- src/pages/signin/LoginForm.js
+++ src/pages/signin/LoginForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, TextInput, View} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../styles/styles';
+import SubmitButton from './SubmitButton';
 
 const propTypes = {
     // A function that is called when the form is submitted
@@ -11,26 +12,63 @@ const propTypes = {
 class LoginForm extends React.Component {
     constructor() {
         super();
+
+        this.validateAndSubmitForm = this.validateAndSubmitForm.bind(this);
+
         this.state = {
+            formError: false,
             login: '',
+            isLoading: false,
         };
+    }
+
+    /**
+     * Check that all the form fields are valid, then trigger the submit callback
+     */
+    validateAndSubmitForm() {
+        if (!this.state.login.trim()) {
+            this.setState({formError: 'Please fill out all fields'});
+            return;
+        }
+
+        this.setState({
+            formError: null,
+            isLoading: true,
+        });
+        this.props.onSubmit({
+            login: this.state.login,
+        });
     }
 
     render() {
         return (
-            <View style={[styles.mb4]}>
-                <Text style={[styles.formLabel]}>Login</Text>
-                <TextInput
-                    style={[styles.textInput]}
-                    value={this.state.login}
-                    autoCompleteType="email"
-                    textContentType="username"
-                    onChangeText={text => this.setState({login: text})}
-                    onSubmitEditing={this.props.submitForm}
-                    autoCapitalize="none"
-                    placeholder="Email or phone"
-                />
-            </View>
+            <>
+                <View style={[styles.mb4]}>
+                    <Text style={[styles.formLabel]}>Login</Text>
+                    <TextInput
+                        style={[styles.textInput]}
+                        value={this.state.login}
+                        autoCompleteType="email"
+                        textContentType="username"
+                        onChangeText={text => this.setState({login: text})}
+                        onSubmitEditing={this.props.validateAndSubmitForm}
+                        autoCapitalize="none"
+                        placeholder="Email or phone"
+                    />
+                </View>
+                <View>
+                    <SubmitButton
+                        text="Next"
+                        isLoading={this.state.isLoading}
+                        onClick={this.props.validateAndSubmitForm}
+                    />
+                </View>
+                {this.state.formError && (
+                    <Text style={[styles.formError]}>
+                        {this.state.formError}
+                    </Text>
+                )}
+            </>
         );
     }
 }
