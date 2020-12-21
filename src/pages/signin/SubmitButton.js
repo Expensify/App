4d8fda013f3be501f6f6d692d8e6f475238ed309
Tim Golen <tgolen@expensify.com>
2020-12-20 17:08:13 -0700

    Have the button listen to the session error to determine if it's disabled

--- src/pages/signin/SubmitButton.js
+++ src/pages/signin/SubmitButton.js
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import {
     ActivityIndicator, Text, TouchableOpacity
 } from 'react-native';
+import {withOnyx} from 'react-native-onyx';
 import styles from '../../styles/styles';
 import themeColors from '../../styles/themes/default';
+import ONYXKEYS from '../../ONYXKEYS';
 
 const propTypes = {
     // The text for the button label
@@ -17,24 +19,31 @@ const propTypes = {
     onClick: PropTypes.func.isRequired,
 };
 
-const SubmitButton = props => (
-    <TouchableOpacity
-        style={[styles.button, styles.buttonSuccess, styles.mb4]}
-        onPress={props.onClick}
-        underlayColor={themeColors.componentBG}
-        disabled={props.isLoading}
-    >
-        {props.isLoading ? (
-            <ActivityIndicator color={themeColors.textReversed} />
-        ) : (
-            <Text style={[styles.buttonText, styles.buttonSuccessText]}>
-                {props.text}
-            </Text>
-        )}
-    </TouchableOpacity>
-);
+const SubmitButton = (props) => {
+    // When there is an error in the session (a sign on error) then the button should be
+    // enabled so the form can be submitted again
+    const isLoading = props.isLoading && !props.session.error;
+    return (
+        <TouchableOpacity
+            style={[styles.button, styles.buttonSuccess, styles.mb4]}
+            onPress={props.onClick}
+            underlayColor={themeColors.componentBG}
+            disabled={isLoading}
+        >
+            {isLoading ? (
+                <ActivityIndicator color={themeColors.textReversed} />
+            ) : (
+                <Text style={[styles.buttonText, styles.buttonSuccessText]}>
+                    {props.text}
+                </Text>
+            )}
+        </TouchableOpacity>
+    );
+};
 
 SubmitButton.propTypes = propTypes;
 SubmitButton.displayName = 'SubmitButton';
 
-export default SubmitButton;
+export default withOnyx({
+    session: {key: ONYXKEYS.SESSION},
+}, SubmitButton);
