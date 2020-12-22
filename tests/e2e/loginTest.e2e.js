79184999a50066e8b0ef766f16d129db79202adc
Tim Golen <tgolen@expensify.com>
2020-12-22 13:22:37 -0700

    Fix test (too many branches)

--- tests/e2e/loginTest.e2e.js
+++ tests/e2e/loginTest.e2e.js
@@ -5,5 +5,5 @@ jest.setTimeout(120000 * 10);
 describe('Test login page', () => {
     beforeEach(() => device.reloadReactNative());
 
-    it('should have a Log In button visible', () => expect(element(by.text('Continue'))).toBeVisible());
+    it('should have a Log In button visible', () => expect(element(by.text('Next'))).toBeVisible());
 });
