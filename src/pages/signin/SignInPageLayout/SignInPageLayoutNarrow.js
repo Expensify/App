3fabd3803d8b67e748122717732d5ebdb74011be
Tim Golen <tgolen@expensify.com>
2020-12-22 15:43:05 -0700

    Scroll the entire page instead of just the text and image

--- src/pages/signin/SignInPageLayout/SignInPageLayoutNarrow.js
+++ src/pages/signin/SignInPageLayout/SignInPageLayoutNarrow.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {
+    Image, ScrollView, Text, View
+} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../../styles/styles';
 import logo from '../../../../assets/images/expensify-logo-coin-2x.png';
@@ -10,23 +12,27 @@ const propTypes = {
 };
 
 const SignInPageLayoutNarrow = ({children}) => (
-    <View style={[styles.signInPageInnerNative]}>
-        <View style={[styles.signInPageLogoNative]}>
-            <Image
-                resizeMode="contain"
-                style={[styles.signinLogo]}
-                source={logo}
-            />
-        </View>
+    <ScrollView>
+        <View>
+            <View style={[styles.signInPageInnerNative]}>
+                <View style={[styles.signInPageLogoNative]}>
+                    <Image
+                        resizeMode="contain"
+                        style={[styles.signinLogo]}
+                        source={logo}
+                    />
+                </View>
 
-        <View style={[styles.mb6, styles.alignItemsCenter]}>
-            <Text style={[styles.h1]}>
-                Expensify.cash
-            </Text>
-        </View>
+                <View style={[styles.mb6, styles.alignItemsCenter]}>
+                    <Text style={[styles.h1]}>
+                        Expensify.cash
+                    </Text>
+                </View>
 
-        {children}
-    </View>
+                {children}
+            </View>
+        </View>
+    </ScrollView>
 );
 
 SignInPageLayoutNarrow.propTypes = propTypes;
