50a755badd6e5d2d2c4623cec786bab64054102c
Marc Glasser <marc.aaron.glasser@gmail.com>
2020-12-25 07:59:14 -1000

    move spacing utilities to seprate file and standardize on multiples of 4

--- src/pages/home/sidebar/ChatSwitcherList.js
+++ src/pages/home/sidebar/ChatSwitcherList.js
@@ -47,7 +47,7 @@ const ChatSwitcherList = ({
                 )}
                 extraData={focusedIndex}
                 ListFooterComponent={View}
-                ListFooterComponentStyle={[styles.p1]}
+                ListFooterComponentStyle={[styles.p3]}
             />
             <KeyboardSpacer />
         </View>
---
+++ src/styles/utilities/spacing.js
@@ -0,0 +1,116 @@
+// Reusable spacing utility styles loosely based on Bootstrap.
+// All styles should be incremented in units of 4
+// https://getbootstrap.com/docs/5.0/utilities/spacing
+export default {
+    m0: {
+        margin: 0,
+    },
+
+    mr0: {
+        marginRight: 0,
+    },
+
+    mr1: {
+        marginRight: 4,
+    },
+
+    mr2: {
+        marginRight: 8,
+    },
+
+    mr3: {
+        marginRight: 12,
+    },
+
+    ml1: {
+        marginLeft: 4,
+    },
+
+    ml2: {
+        marginLeft: 8,
+    },
+
+    mt1: {
+        marginTop: 4,
+    },
+
+    mt2: {
+        marginTop: 8,
+    },
+
+    mt3: {
+        marginTop: 12,
+    },
+
+    mt4: {
+        marginTop: 16,
+    },
+
+    mt5: {
+        marginTop: 20,
+    },
+
+    mt6: {
+        marginTop: 24,
+    },
+
+    mb1: {
+        marginBottom: 4,
+    },
+
+    mb2: {
+        marginBottom: 8,
+    },
+
+    mb3: {
+        marginBottom: 12,
+    },
+
+    mb4: {
+        marginBottom: 16,
+    },
+
+    mb5: {
+        marginBottom: 20,
+    },
+
+    mb6: {
+        marginBottom: 24,
+    },
+
+    mbn1: {
+        marginBottom: -4,
+    },
+
+    p0: {
+        padding: 0,
+    },
+
+    p1: {
+        padding: 4,
+    },
+
+    p2: {
+        padding: 8,
+    },
+
+    p3: {
+        padding: 12,
+    },
+
+    p4: {
+        padding: 16,
+    },
+
+    p5: {
+        padding: 20,
+    },
+
+    pr1: {
+        paddingRight: 4,
+    },
+
+    pr2: {
+        paddingRight: 8,
+    },
+};
