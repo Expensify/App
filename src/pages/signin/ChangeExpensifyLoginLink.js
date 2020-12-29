853d23e0c63c1bd051502563be6c44b63f6fef5e
Tim Golen <tgolen@expensify.com>
2020-12-29 14:39:27 -0700

    Import react

--- src/pages/signin/ChangeExpensifyLoginLink.js
+++ src/pages/signin/ChangeExpensifyLoginLink.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import styles from '../../styles/styles';
 import {restartSignin} from '../../libs/actions/Session';
