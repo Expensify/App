1cf6b8ed8970ab06b27499ac87167cf97fc199cf
Rory Abraham <rory@expensify.com>
2020-12-29 11:16:38 -0800

    install and configure react-native-svg-web

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -114,6 +114,7 @@ module.exports = {
             'react-native$': 'react-native-web',
             'react-native-webview': 'react-native-web-webview',
             'react-native-modal': 'modal-enhanced-react-native-web',
+            'react-native-svg': 'react-native-svg-web',
         },
 
         // React Native libraries may have web-specific module implementations that appear with the extension `.web.js`
