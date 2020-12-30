259a8ccf5a3d0d4ea76dca47a0aeb12a1369cc78
UpworkBartkoski <upworkbartkoski@gmail.com>
2020-12-29 20:34:40 -0600

    change style

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -220,7 +220,7 @@ class App extends React.Component {
     render() {
         const hamburgerStyle = this.state.isHamburgerEnabled && this.props.isSidebarShown ? styles.hamburgerOpenAbsolute
                 : styles.hamburgerOpen;
-        const visibility = !this.state.isHamburgerEnabled || this.props.isSidebarShown ? styles.width300px : styles.width0px;
+        const visibility = !this.state.isHamburgerEnabled || this.props.isSidebarShown ? styles.w300 : styles.w0;
         const appContentWrapperStyle = !this.state.isHamburgerEnabled ? styles.appContentWrapperLarge : null;
         return (
             <SafeAreaProvider>
--- src/styles/utilities/sizing.js
+++ src/styles/utilities/sizing.js
@@ -15,4 +15,12 @@ export default {
     w50: {
         width: '50%',
     },
+
+    w300: {
+        width: 300,
+    },
+
+    w0: {
+        width: 0,
+    },
 };
