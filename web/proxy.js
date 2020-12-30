b99dfffeee73c168f51391aaf7be6154cb88cc03
andrew <andrew@expensify.com>
2020-12-30 14:27:19 -0700

    Tweak helper checks

--- web/proxy.js
+++ web/proxy.js
@@ -2,8 +2,8 @@ const http = require('http');
 const https = require('https');
 require('dotenv').config();
 
-if (process.env.USE_WEB_PROXY !== 'true') {
-    process.stdout.write('Skipping proxy as USE_WEB_PROXY was set to false.');
+if (process.env.USE_WEB_PROXY === 'false') {
+    process.stdout.write('Skipping proxy as USE_WEB_PROXY was set to false.\n');
     process.exit();
 }
 
