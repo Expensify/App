d6e63a6e44b5e4b29d33eb048f6aa9bd25736100
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-01-01 12:37:21 -1000

    make utils

--- tests/actions/SessionTest.js
+++ tests/actions/SessionTest.js
@@ -1,15 +1,7 @@
 import {fetchAccountDetails, signIn} from '../../src/libs/actions/Session';
 import * as API from '../../src/libs/API';
 import HttpUtils from '../../src/libs/HttpUtils';
-
-/**
- * Method which waits for all asynchronous JS to stop executing before proceeding. This helps test things like actions
- * that expect some Onyx value to be available. This way we do not have to explicitly wait for an action to finish
- * (e.g. by making it a promise and waiting for it to resolve).
- *
- * @returns {Promise}
- */
-const waitForPromisesToResolve = () => new Promise(setImmediate);
+import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
 
 // Set up manual mocks for methods used in the actions so our test does not fail.
 jest.mock('../../src/libs/Notification/PushNotification', () => ({
