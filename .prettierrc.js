edec8a434cc3c4afa33fc87db147f3f0599b9300
Ron <laniado.ron@gmail.com>
2021-01-04 11:30:01 -0500

    fix coding style

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 4,
+  arrowParens: 'avoid',
+  useTabs: true,
+};
