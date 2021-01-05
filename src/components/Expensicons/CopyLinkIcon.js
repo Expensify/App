894aa353efcd119550cca7a856702527ea6bd6be
Rory Abraham <rory@expensify.com>
2021-01-04 18:02:11 -0800

    add fill color to icons

--- src/components/Expensicons/CopyLinkIcon.js
+++ src/components/Expensicons/CopyLinkIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CopyLinkSVG from '../../../assets/images/link-copy.svg';
+import themeColors from '../../styles/themes/default';
 
 const propTypes = {
     height: PropTypes.number.isRequired,
@@ -11,6 +12,7 @@ const CopyLinkIcon = props => (
     <CopyLinkSVG
         height={props.height}
         width={props.width}
+        fill={themeColors.icon}
     />
 );
 
--- src/components/Expensicons/TrashIcon.js
+++ src/components/Expensicons/TrashIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TrashSVG from '../../../assets/images/trashcan.svg';
+import themeColors from '../../styles/themes/default';
 
 const propTypes = {
     height: PropTypes.number.isRequired,
@@ -11,6 +12,7 @@ const TrashIcon = props => (
     <TrashSVG
         height={props.height}
         width={props.width}
+        fill={themeColors.icon}
     />
 );
 
