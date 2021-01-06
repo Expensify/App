2611883d40980786b1801b8ef54e3ef04681560f
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-01-06 13:08:20 -1000

    resolve the same promise that we start off with

--- src/libs/API.js
+++ src/libs/API.js
@@ -104,12 +104,13 @@ function requireParameters(parameterNames, parameters, commandName) {
  * Function used to handle expired auth tokens. It re-authenticates with the API and
  * then replays the original request
  *
- * @param {Object} originalResponse
  * @param {String} originalCommand
  * @param {Object} [originalParameters]
  * @param {String} [originalType]
+ *
+ * @returns {Promise}
  */
-function handleExpiredAuthToken(originalResponse, originalCommand, originalParameters, originalType) {
+function handleExpiredAuthToken(originalCommand, originalParameters, originalType) {
     // There are some API requests that should not be retried when there is an auth failure
     // like creating and deleting logins
     if (originalParameters.doNotRetry) {
@@ -119,8 +120,7 @@ function handleExpiredAuthToken(originalResponse, originalCommand, originalParam
     // When the authentication process is running, and more API requests will be requeued and they will
     // be performed after authentication is done.
     if (isAuthenticating) {
-        Network.post(originalCommand, originalParameters, originalType);
-        return;
+        return Network.post(originalCommand, originalParameters, originalType);
     }
 
     // Prevent any more requests from being processed while authentication happens
@@ -128,7 +128,7 @@ function handleExpiredAuthToken(originalResponse, originalCommand, originalParam
     isAuthenticating = true;
 
     // eslint-disable-next-line no-use-before-define
-    Authenticate({
+    return Authenticate({
         useExpensifyLogin: false,
         partnerName: CONFIG.EXPENSIFY.PARTNER_NAME,
         partnerPassword: CONFIG.EXPENSIFY.PARTNER_PASSWORD,
@@ -154,7 +154,7 @@ function handleExpiredAuthToken(originalResponse, originalCommand, originalParam
 
             // Now that the API is authenticated, make the original request again with the new authToken
             const params = addAuthTokenToParameters(originalCommand, originalParameters);
-            Network.post(originalCommand, params, originalType);
+            return Network.post(originalCommand, params, originalType);
         })
 
         .catch((error) => {
@@ -176,22 +176,22 @@ function handleExpiredAuthToken(originalResponse, originalCommand, originalParam
  * @returns {Promise}
  */
 function request(command, parameters, type = 'post') {
-    const networkPromise = Network.post(command, parameters, type);
+    return new Promise((resolve, reject) => {
+        Network.post(command, parameters, type)
+            .then((response) => {
+                // Handle expired auth tokens properly
+                if (response.jsonCode === 407) {
+                    console.log('auth token expired');
+                    handleExpiredAuthToken(command, parameters, type)
+                        .then(resolve)
+                        .catch(reject);
+                    return;
+                }
 
-    // Setup the default handlers to work with different response codes
-    networkPromise.then((response) => {
-        // Handle expired auth tokens properly
-        if (response.jsonCode === 407) {
-            handleExpiredAuthToken(response, command, parameters, type);
-
-            // Throw an error to prevent other handlers from being triggered on this promise
-            throw new Error('A default handler was used for this request');
-        }
-
-        return response;
+                resolve(response);
+            })
+            .catch(reject);
     });
-
-    return networkPromise;
 }
 
 /**
