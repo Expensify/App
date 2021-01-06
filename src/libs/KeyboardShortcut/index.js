b325b1d51f7eeea317aeaa7551511725b8efd23e
Frank <codewithfrank@gmail.com>
2021-01-06 22:47:49 +0000

    patch: linting

--- src/libs/KeyboardShortcut/index.js
+++ src/libs/KeyboardShortcut/index.js
@@ -7,46 +7,45 @@ const events = {};
  * @param {Event} event
  */
 function bindHandlerToKeyupEvent(event) {
-    if (events[event.key] === undefined) {
-        return;
+  if (events[event.key] === undefined) {
+    return;
+  }
+
+  // The active callback is the last element in the array
+  const eventCallbacks = events[event.key];
+  const callback = eventCallbacks[eventCallbacks.length - 1];
+
+  const pressedModifiers = _.all(callback.modifiers, (modifier) => {
+    if (modifier === 'shift' && !event.shiftKey) {
+      return false;
     }
-
-    // The active callback is the last element in the array
-    const eventCallbacks = events[event.key];
-    const callback = eventCallbacks[eventCallbacks.length - 1];
-
-    const pressedModifiers = _.all(callback.modifiers, (modifier) => {
-
-        if (modifier === 'shift' && !event.shiftKey) {
-            return false;
-        }
-        if (modifier === 'control' && !event.ctrlKey) {
-            return false;
-        }
-        if (modifier === 'alt' && !event.altKey) {
-            return false;
-        }
-        return !(modifier === 'meta' && !event.metaKey);
-    });
-
-
-    if (!pressedModifiers) {
-        return;
+    if (modifier === 'control' && !event.ctrlKey) {
+      return false;
     }
-
-    // If configured to do so, prevent input text control to trigger this event
-    if (!callback.captureOnInputs && (
-        event.target.nodeName === 'INPUT'
-        || event.target.nodeName === 'TEXTAREA'
-        || event.target.contentEditable === 'true'
-    )) {
-        return;
+    if (modifier === 'alt' && !event.altKey) {
+      return false;
     }
+    return !(modifier === 'meta' && !event.metaKey);
+  });
 
-    if (_.isFunction(callback.callback)) {
-        callback.callback(event);
-    }
-    event.preventDefault();
+  if (!pressedModifiers) {
+    return;
+  }
+
+  // If configured to do so, prevent input text control to trigger this event
+  if (
+    !callback.captureOnInputs &&
+    (event.target.nodeName === 'INPUT' ||
+      event.target.nodeName === 'TEXTAREA' ||
+      event.target.contentEditable === 'true')
+  ) {
+    return;
+  }
+
+  if (_.isFunction(callback.callback)) {
+    callback.callback(event);
+  }
+  event.preventDefault();
 }
 
 // Make sure we don't add multiple listeners
@@ -65,31 +64,34 @@ document.addEventListener('keydown', bindHandlerToKeyupEvent);
  * The "subClass" is used by pages to bind /unbind with no worries
  */
 const KeyboardShortcut = {
-    /**
-     * Subscribes to a keyboard event.
-     * @param {String} key The key code to watch
-     * @param {Function} callback The callback to call
-     * @param {String|Array} modifiers Can either be shift or control
-     * @param {Boolean} captureOnInputs Should we capture the event on inputs too?
-     */
-    subscribe(key, callback, modifiers = 'shift', captureOnInputs = false) {
+  /**
+   * Subscribes to a keyboard event.
+   * @param {String} key The key code to watch
+   * @param {Function} callback The callback to call
+   * @param {String|Array} modifiers Can either be shift or control
+   * @param {Boolean} captureOnInputs Should we capture the event on inputs too?
+   */
+  subscribe(key, callback, modifiers = 'shift', captureOnInputs = false) {
+    //enable support for special keys like Escape
+    //const keyCode =  key.charCodeAt(0);
 
-        //enable support for special keys like Escape
-        //const keyCode =  key.charCodeAt(0);
-
-        if (events[key] === undefined) {
-            events[key] = [];
-        }
-        events[key].push({callback, modifiers: _.isArray(modifiers) ? modifiers : [modifiers], captureOnInputs});
-    },
-
-    /**
-     * Unsubscribes to a keyboard event.
-     * @param {String} key The key to stop watching
-     */
-    unsubscribe(key) {
-        delete events[key];
+    if (events[key] === undefined) {
+      events[key] = [];
     }
+    events[key].push({
+      callback,
+      modifiers: _.isArray(modifiers) ? modifiers : [modifiers],
+      captureOnInputs,
+    });
+  },
+
+  /**
+   * Unsubscribes to a keyboard event.
+   * @param {String} key The key to stop watching
+   */
+  unsubscribe(key) {
+    delete events[key];
+  },
 };
 
 export default KeyboardShortcut;
