30ad0159e16d850539e3d6f383a53d752d92f1e8
Kashan Z <dev.kashan@gmail.com>
2021-01-08 07:49:38 +0500

    Fixed a couple of spaces

--- src/libs/ErrorMessage/index.js
+++ src/libs/ErrorMessage/index.js
@@ -11,10 +11,10 @@ import messages from './messages';
 function getErrorMessage(type, error) {
     const code = error.split(' ')[0];
     if (!_.isEmpty(code)) {
-      const message = _.filter(messages, {type: type, errorCode: +code.trim()});
-      if (!_.isEmpty(message)) {
-        return message[0].message;
-      }
+        const message = _.filter(messages, {type: type, errorCode: +code.trim()});
+        if (!_.isEmpty(message)) {
+            return message[0].message;
+        }
     }
     return error;
 }
--- src/libs/ErrorMessage/messages.js
+++ src/libs/ErrorMessage/messages.js
@@ -1,37 +1,38 @@
 const messages = [
     {
-      type: 'Login',
-      errorCode: 401,
-      message: 'Incorrect login or password. Please try again.',
+        type: 'Login',
+        errorCode: 401,
+        message: 'Incorrect login or password. Please try again.',
     },
     {
-      type: 'Login',
-      errorCode: 402,
-      message:
-        'You have 2FA enabled on this account. Please sign in using your email or phone number.',
+        type: 'Login',
+        errorCode: 402,
+        message:
+          'You have 2FA enabled on this account. Please sign in using your email or phone number.',
     },
     {
-      type: 'Login',
-      errorCode: 403,
-      message:
-        'Invalid login or password. Please try again or reset your password.',
+        type: 'Login',
+        errorCode: 403,
+        message:
+          'Invalid login or password. Please try again or reset your password.',
     },
     {
-      type: 'Login',
-      errorCode: 404,
-      message:
-        'We were unable to change your password. This is likely due to an expired password reset link in an old password reset email. We have emailed you a new link so you can try again. Check your Inbox and your Spam folder; it should arrive in just a few minutes.',
+        type: 'Login',
+        errorCode: 404,
+        message:
+          'We were unable to change your password. This is likely due to an expired password reset link in an old password reset email. We have emailed you a new link so you can try again. Check your Inbox and your Spam folder; it should arrive in just a few minutes.',
     },
     {
-      type: 'Login',
-      errorCode: 405,
-      message:
-        'You do not have access to this application. Please add your GitHub username for access.',
+        type: 'Login',
+        errorCode: 405,
+        message:
+          'You do not have access to this application. Please add your GitHub username for access.',
     },
     {
-      type: 'Login',
-      errorCode: 413,
-      message: `Your account has been locked.`,
+        type: 'Login',
+        errorCode: 413,
+        message: 'Your account has been locked.',
     },
 ];
+
 export default messages;
