151718c4d63a1fec80b91186b1d7d296120de81b
Rory Abraham <rory@expensify.com>
2021-01-12 11:02:09 -0800

    Commit bundled GH action

--- .github/actions/bumpVersion/index.js
+++ .github/actions/bumpVersion/index.js
@@ -1,6 +1,15 @@
-const {exec} = require('child_process');
-const core = require('@actions/core');
-const github = require('@actions/github');
+// THIS IS A COMPILED FILE. DO NOT MODIFY.
+
+module.exports =
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 959:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const {exec} = __nccwpck_require__(129);
+const core = __nccwpck_require__(80);
+const github = __nccwpck_require__(928);
 
 // Use Github Actions' default environment variables to get repo information
 // https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables#default-environment-variables
@@ -25,7 +34,7 @@ do {
                     `retryCount: ${++errCount}`,
                 );
                 shouldRetry = true;
-                const currentPatchVersion = require('../../../package.json').version.slice(0, -4);
+                const currentPatchVersion = __nccwpck_require__(719)/* .version.slice */ .i8.slice(0, -4);
 
                 // Get the highest build version git tag from the repo
                 console.log('Fetching tags from github...');
@@ -55,3 +64,80 @@ do {
         }
     });
 } while (shouldRetry);
+
+
+/***/ }),
+
+/***/ 80:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 928:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ }),
+
+/***/ 719:
+/***/ ((module) => {
+
+"use strict";
+module.exports = {"i8":"1.0.1-332"};
+
+/***/ }),
+
+/***/ 129:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("child_process");;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/
+/******/ 	__nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+/******/ 	// module exports must be returned from runtime so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	return __nccwpck_require__(959);
+/******/ })()
+;
