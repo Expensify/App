d0501e5a1ec9c4b62f7d36b178ba57536d72aaa7
Sergey Storchay <r8@r8.com.ua>
2021-01-14 11:47:57 +0200

    Revert "Unpause network queue after redirect because of the missing Auth Token"
    This reverts commit 15f5a249

--- src/libs/API.js
+++ src/libs/API.js
@@ -54,15 +54,11 @@ function addAuthTokenToParameters(command, parameters) {
         // If we end up here with no authToken it means we are trying to make an API request before we are signed in.
         // In this case, we should cancel the current request by pausing the queue and clearing the remaining requests.
         if (!authToken) {
+            redirectToSignIn();
+
             console.debug('A request was made without an authToken', {command, parameters});
             Network.pauseRequestQueue();
             Network.clearRequestQueue();
-
-            redirectToSignIn().finally(() => {
-                // We use `finally` to catch both `resolve` and `reject`
-                Network.unpauseRequestQueue();
-            });
-
             return;
         }
 
