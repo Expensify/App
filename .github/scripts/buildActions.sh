11437a433636f91fceea116c8dc97ddde8984040
Rory Abraham <rory@expensify.com>
2021-01-15 14:45:33 -0800

    Move warning note to separate file for reuse

--- .github/scripts/buildActions.sh
+++ .github/scripts/buildActions.sh
@@ -3,14 +3,15 @@
 # Used to precompile all Github Action node.js scripts using ncc.
 # This bundles them with their dependencies into a single executable node.js script.
 
+# Get a reference to the parent directory
+# In order for the script to be runnable from anywhere, don't use relative path like '../'
+declare -r PARENT_DIR=$(dirname "$(realpath "$0")")
+
 # This will be inserted at the top of all compiled files as a warning to devs.
-declare -r NOTE_DONT_EDIT='/**
- * NOTE: This is a compiled file. DO NOT directly edit this file.
- */
-'
+NOTE_DONT_EDIT=$("$PARENT_DIR/generateWarningNote.sh")
 
 # Get the list of Github Actions using ./listActions.sh
-GITHUB_ACTIONS=( "$("$(dirname "$(realpath "$0")")/listActions.sh")" )
+GITHUB_ACTIONS=( "$("$PARENT_DIR/listActions.sh")" )
 
 for ACTION in "${GITHUB_ACTIONS[@]}"; do
     # Build the action
