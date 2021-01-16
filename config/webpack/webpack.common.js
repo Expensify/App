61d299634759d9769a28e1e97ddf9d42690271a0
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-01-15 15:16:27 -1000

    move fonts to css

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -30,6 +30,7 @@ module.exports = {
             patterns: [
                 {from: 'web/favicon.png'},
                 {from: 'web/favicon-unread.png'},
+                {from: 'assets/css', to: 'css'},
 
                 // These files are copied over as per instructions here
                 // https://github.com/mozilla/pdf.js/wiki/Setup-pdf.js-in-a-website#examples
