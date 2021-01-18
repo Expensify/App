be1f1c25a436a3aa1836fb4ad72c9dc3a8f3f0f2
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-01-19 01:47:00 +0530

    chg: refactor AppState to pusher:subscription_succeeded

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -119,6 +119,7 @@ class App extends React.Component {
     componentWillUnmount() {
         Dimensions.removeEventListener('change', this.toggleHamburgerBasedOnDimensions);
         KeyboardShortcut.unsubscribe('K');
+        NetworkConnection.stopListeningForReconnect();
     }
 
     /**
