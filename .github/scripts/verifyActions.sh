6b19aa89498b529f1c1b44b7f69d0f1972879264
Rory Abraham <rory@expensify.com>
2021-01-19 15:28:40 -0800

    Attempt to use local script to verify

---
+++ .github/scripts/verifyActions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Re-compiles all Github Actions and verifies that there is no diff,
+# because that would indicate that the PR author forgot to run `npm run gh-actions-build`
+# and commit the re-bundled the javascript sources.
+
+declare -r GREEN='\033[0;32m'
+delcare -r RED='\033[0;31m'
+declare -r NC='\033[0m'
+
+if [[ $(git diff --exit-code) ]]; then
+    echo "${GREEN}Github Actions are up to date!${NC}"
+    exit 0
+else
+    echo "${RED}Error: Diff found when Github Actions were rebuilt. Did you forget to run ${NC}\`npm run gh-actions-build\`${RED}?${NC}"
+    exit 1
+fi
