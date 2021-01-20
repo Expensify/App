3369b8de6213ae9c9f522f5c196069ce30cf56a9
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-01-20 12:31:52 -1000

    add PropTypes for options

--- src/components/ReportList.js
+++ src/components/ReportList.js
@@ -23,7 +23,35 @@ const propTypes = {
 
     /** Array of already selected options */
     selectedOptions: PropTypes.arrayOf(PropTypes.shape({
-        // Report and Personal Detail option
+        /** Text to display */
+        text: PropTypes.string,
+
+        /** Alternate text to display */
+        alternateText: PropTypes.string,
+
+        /** Array of icon urls */
+        icons: PropTypes.arrayOf(PropTypes.string),
+
+        /** Login (only present when there is a single participant) */
+        login: PropTypes.string,
+
+        /** reportID (only present when there is a matching report) */
+        reportID: PropTypes.number,
+
+        /** Whether the report has read or not */
+        isUnread: PropTypes.bool,
+
+        /** Whether the report has a draft comment or not */
+        hasDraftComment: PropTypes.bool,
+
+        /** Key used internally by React */
+        keyForList: PropTypes.string,
+
+        /** Search text we use to filter options */
+        searchText: PropTypes.string,
+
+        /** Whether the report is pinned or not */
+        isPinned: PropTypes.bool,
     })),
 
     /** Whether we can select multiple options or not */
