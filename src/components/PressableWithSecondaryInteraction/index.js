388c118302d1f4fcb3638164ce0cade94d84554f
Rory Abraham <rory@expensify.com>
2021-01-22 15:32:39 -0800

    Update comments

--- src/components/PressableWithSecondaryInteraction/index.js
+++ src/components/PressableWithSecondaryInteraction/index.js
@@ -1,16 +1,19 @@
-/**
- * This is a special Pressable that calls onSecondaryInteraction when LongPressed, or right-clicked.
- */
 import _ from 'underscore';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Pressable} from 'react-native';
 
 const propTypes = {
+    // The function that should be called when this pressable is LongPressed or right-clicked.
     onSecondaryInteraction: PropTypes.func.isRequired,
+
+    // The children which should be contained in this wrapper component.
     children: PropTypes.node.isRequired,
 };
 
+/**
+ * This is a special Pressable that calls onSecondaryInteraction when LongPressed, or right-clicked.
+ */
 class PressableWithSecondaryInteraction extends Component {
     constructor(props) {
         super(props);
