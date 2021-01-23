4c1c9dff3ca7e92b6a78d71d4163a0fa3195eceb
Rory Abraham <rory@expensify.com>
2021-01-22 16:27:35 -0800

    Update tooltip style

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -4,5 +4,7 @@
  * https://material-ui.com/components/tooltips/
  */
 import Tooltip from '@material-ui/core/Tooltip';
+import withStyles from '@material-ui/core/styles/withStyles';
+import styles from '../../styles/styles';
 
-export default Tooltip;
+export default withStyles(styles.tooltip)(Tooltip);
