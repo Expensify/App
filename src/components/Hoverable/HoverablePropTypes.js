3b10ab3d9cffd0c82903e18301be2b84d4d704a0
Rory Abraham <rory@expensify.com>
2021-01-25 13:40:54 -0800

    Fix props comments

--- src/components/Hoverable/HoverablePropTypes.js
+++ src/components/Hoverable/HoverablePropTypes.js
@@ -2,17 +2,17 @@ import {Component} from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-    /* Child to wrap with Hoverable. Must be only child. */
+    // Child to wrap with Hoverable. Must be only child.
     children: PropTypes.oneOfType([
         PropTypes.func,
         PropTypes.element,
         PropTypes.instanceOf(Component),
     ]).isRequired,
 
-    /* Function that executes when the mouse moves over the child element. */
+    // Function that executes when the mouse moves over the child element.
     onHoverIn: PropTypes.func,
 
-    /* Function that executes when the mouse leaves the child element. */
+    // Function that executes when the mouse leaves the child element.
     onHoverOut: PropTypes.func,
 };
 
