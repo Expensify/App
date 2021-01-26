642a749ea3cf0c9a0cf05fb32da3178913bf84ad
Yuwen Memon <yuwen@expensify.com>
2021-01-25 16:04:22 -0800

    Get the version number showing at the very bottom

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -956,8 +956,7 @@ const styles = {
     settingsPageBackground: {
         flexDirection: 'column',
         width: '100%',
-        height: '100%',
-        position: 'relative',
+        flexGrow: 1,
         justifyContent: 'space-between',
         alignItems: 'center',
     },
