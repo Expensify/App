3e8d073daecf7ff2965bfc0549d30070bf8a45f9
Rory Abraham <rory@expensify.com>
2021-01-26 14:42:46 -0800

    Clean up horizontal shifting logic

--- src/styles/getTooltipStyles.js
+++ src/styles/getTooltipStyles.js
@@ -5,6 +5,8 @@ import themeColors from './themes/default';
 import fontFamily from './fontFamily';
 import variables from './variables';
 
+// This defines the proximity with the edge of the window in which tooltips should not be displayed.
+// If a tooltip is too close to the edge of the screen, we'll shift it towards the center.
 const GUTTER_WIDTH = 16;
 
 /**
@@ -28,6 +30,36 @@ function roundToNearestMultipleOfFour(n) {
 }
 
 /**
+ * Compute the amount the tooltip needs to be horizontally shifted in order to keep it from displaying in the gutters.
+ *
+ * @param {Number} windowWidth - The width of the window.
+ * @param {Number} xOffset - The distance between the left edge of the window
+ *                           and the left edge of the wrapped component.
+ * @param {Number} componentWidth - The width of the wrapped component.
+ * @param {Number} tooltipWidth - The width of the tooltip itself.
+ * @returns {Number}
+ */
+function computeHorizontalShift(windowWidth, xOffset, componentWidth, tooltipWidth) {
+    // First find the left and right edges of the tooltip.
+    const componentCenter = xOffset + (componentWidth / 2);
+    const tooltipLeftEdge = componentCenter - (tooltipWidth / 2);
+    const tooltipRightEdge = componentCenter + (tooltipWidth / 2);
+
+    if (tooltipLeftEdge < GUTTER_WIDTH) {
+        // Tooltip is in left gutter, shift right by a multiple of four.
+        return roundToNearestMultipleOfFour(GUTTER_WIDTH - tooltipLeftEdge);
+    }
+
+    if (tooltipRightEdge > (windowWidth - GUTTER_WIDTH)) {
+        // Tooltip is in right gutter, shift left by a multiple of four.
+        return roundToNearestMultipleOfFour(windowWidth - GUTTER_WIDTH - tooltipRightEdge);
+    }
+
+    // Tooltip is not in the gutter, so no need to shift it horizontally
+    return 0;
+}
+
+/**
  * Generate styles for the tooltip component.
  *
  * @param {Number} interpolatedSize - The current size of the tooltip used in the scaling animation.
@@ -35,38 +67,30 @@ function roundToNearestMultipleOfFour(n) {
  *                           and the left edge of the wrapped component.
  * @param {Number} yOffset - The distance between the top edge of the window
  *                           and the top edge of the wrapped component.
- * @param {Number} width - The width of the wrapped component.
- * @param {Number} height - The height of the wrapped component.
+ * @param {Number} componentWidth - The width of the wrapped component.
+ * @param {Number} componentHeight - The height of the wrapped component.
  * @param {Number} tooltipWidth - The width of the tooltip itself.
  * @param {Number} tooltipHeight - The height of the tooltip itself.
  * @returns {Object}
  */
 export default function getTooltipStyles(
-    interpolatedSize,
-    xOffset,
-    yOffset,
-    width,
-    height,
-    tooltipWidth,
-    tooltipHeight,
+    interpolatedSize: Number,
+    xOffset: Number,
+    yOffset: Number,
+    componentWidth: Number,
+    componentHeight: Number,
+    tooltipWidth: Number,
+    tooltipHeight: Number,
 ) {
     // Determine if the tooltip should display below the wrapped component.
+    // If a tooltip will try to render within GUTTER_WIDTH logical pixels of the top of the screen,
+    // we'll display it beneath its wrapped component rather than above it as usual.
     const windowWidth = Dimensions.get('window').width;
     const shouldShowBelow = (yOffset - tooltipHeight) < GUTTER_WIDTH;
 
     // Determine if we need to shift the tooltip horizontally to prevent it
     // from displaying too near to the edge of the screen.
-    let horizontalShift = 0;
-    const tooltipLeftEdge = (xOffset + (width / 2)) - (tooltipWidth / 2);
-    const tooltipRightEdge = (xOffset + (width / 2)) + (tooltipWidth / 2);
-    if (tooltipLeftEdge < GUTTER_WIDTH) {
-        // Tooltip is in left gutter, shift right
-        horizontalShift = GUTTER_WIDTH - tooltipLeftEdge;
-    } else if (tooltipRightEdge > (windowWidth - GUTTER_WIDTH)) {
-        // Tooltip is in right gutter, shift left
-        horizontalShift = (windowWidth - GUTTER_WIDTH) - tooltipRightEdge;
-    }
-    horizontalShift = roundToNearestMultipleOfFour(horizontalShift);
+    const horizontalShift = computeHorizontalShift(windowWidth, xOffset, componentWidth, tooltipWidth);
 
     return {
         animationStyle: {
@@ -91,13 +115,13 @@ export default function getTooltipStyles(
             // OR
             // Shift the tooltip down by...
             // (component height) + (pointer height - 1)
-            top: shouldShowBelow ? height + 3 : -26,
+            top: shouldShowBelow ? componentHeight + 3 : -26,
 
             // Shift the tooltip to the left by...
             //   half the component's width
             // + half the tooltip's width
             // + any horizontal shift
-            left: (tooltipWidth / -2) + (width / 2) + horizontalShift,
+            left: (tooltipWidth / -2) + (componentWidth / 2) + horizontalShift,
         },
         tooltipTextStyle: {
             color: themeColors.textReversed,
@@ -110,10 +134,10 @@ export default function getTooltipStyles(
             // Shift the pointer up by its height
             // OR
             // Down by the component's height - its height
-            top: shouldShowBelow ? height - 4 : -4,
+            top: shouldShowBelow ? componentHeight - 4 : -4,
 
             // Shift the pointer to the right (to the middle of the wrapped element)
-            left: (width / 2) - 4,
+            left: (componentWidth / 2) - 4,
         },
         pointerStyle: {
             width: 0,
