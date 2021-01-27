f5d5f05046b9ae1f854bcf8a0ba0d25a81f9708b
Luke Donahue <luke@expensify.com>
2021-01-27 13:29:05 -0800

    added a plus sign to group avatars

--- src/components/MultipleAvatars.js
+++ src/components/MultipleAvatars.js
@@ -52,7 +52,7 @@ const MultipleAvatars = props => (
                                 ]}
                             >
                                 <Text style={styles.avatarInnerText}>
-                                    {props.avatarImageURLs.length - 1}
+                                    +{props.avatarImageURLs.length - 1}
                                 </Text>
                             </View>
                         )}
