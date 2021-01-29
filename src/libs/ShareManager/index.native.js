244f38b293d61da5502b9561c1bf43a612b2b5a1
Sergey Storchay <r8@r8.com.ua>
2021-01-29 02:06:42 +0200

    Post shared item to selected report

--- src/libs/ShareManager/index.native.js
+++ src/libs/ShareManager/index.native.js
@@ -4,17 +4,37 @@ import {set as setSharedItem} from '../actions/SharedItem';
 import ROUTES from '../../ROUTES';
 import ShareType from './ShareType';
 
+const TEXT_MIME_TYPE = /^text\//i;
+
 // Share event listener
 let listener;
 
 /**
- * Prepares shared item and redirect to Share Page.
+ * Prepare shared item for posting
+ *
+ * @param {Object} sharedItem Shared item object
+ * @returns {Object} Prepared shared item structure
+ */
+function prepareSharedItem(sharedItem) {
+    const type = (sharedItem.mimeType.match(TEXT_MIME_TYPE))
+        ? ShareType.TEXT
+        : ShareType.FILE;
+
+    return {
+        type,
+        data: sharedItem.data,
+    };
+}
+
+/**
+ * Saves shared item and redirects to Share Page.
  *
  * @param {Object} sharedItem
  */
 function handleShare(sharedItem) {
     if (sharedItem) {
-        setSharedItem(sharedItem);
+        const preparedSharedItem = prepareSharedItem(sharedItem);
+        setSharedItem(preparedSharedItem);
         redirect(ROUTES.SHARE);
     }
 }
