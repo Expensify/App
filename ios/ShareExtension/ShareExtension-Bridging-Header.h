a906a9e8ee205fd891f2130bd73a017248b4ca06
Sergey Storchay <r8@r8.com.ua>
2021-01-29 03:19:48 +0200

    Fix ShareExtension target configuration

--- ios/ShareExtension/ShareExtension-Bridging-Header.h
+++ ios/ShareExtension/ShareExtension-Bridging-Header.h
@@ -1,4 +1,4 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTBridgeDelegate.h>
+#import <React/RCTRootView.h>
