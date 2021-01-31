73c3b9496ca033754371d803d18591702c67c6ea
Sergey Storchay <r8@r8.com.ua>
2021-01-31 22:01:41 +0200

    Create proper data object for shared file

--- src/pages/SharePage.js
+++ src/pages/SharePage.js
@@ -24,8 +24,26 @@ const propTypes = {
 
     // Shared item object
     sharedItem: PropTypes.shape({
+        // Shared item type (one of the `ShareType.*`)
         type: PropTypes.string.isRequired,
-        data: PropTypes.string.isRequired,
+
+        // Shared item data. Differs depending on type
+        data: PropTypes.oneOfType([
+            // Text or HTML for `ShareType.TEXT`
+            PropTypes.string,
+
+            // File data structure for `ShareType.FILE`
+            PropTypes.shape({
+                // File name
+                name: PropTypes.string.isRequired,
+
+                // File MIME type
+                type: PropTypes.string.isRequired,
+
+                // File URI
+                uri: PropTypes.string.isRequired,
+            }),
+        ]).isRequired,
     }),
 };
 
