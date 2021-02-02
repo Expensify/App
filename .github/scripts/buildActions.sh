39985731ca30d72b63a33caa6703bc170c6acf4d
Rory Abraham <rory@expensify.com>
2021-02-02 09:08:58 -0800

    Make polyfill for realpath

--- .github/scripts/buildActions.sh
+++ .github/scripts/buildActions.sh
@@ -3,8 +3,15 @@
 # Used to precompile all Github Action node.js scripts using ncc.
 # This bundles them with their dependencies into a single executable node.js script.
 
+# This function is a polyfill for GNU's realpath that (hopefully) works on (almost) any shell
+# By default, realpath is not available on macOS w/o first installing coreutils. :(
+realpath_polyfill() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 # In order for this script to be safely run from anywhere, we cannot use the raw relative path '../actions'
-declare -r ACTIONS_DIR="$(dirname "$(dirname "$(realpath "$0")")")/actions"
+declare ACTIONS_DIR
+ACTIONS_DIR="$(dirname "$(dirname "$(realpath_polyfill "$0")")")/actions"
 
 # List of paths to all JS files that implement our GH Actions
 declare -r GITHUB_ACTIONS=(
