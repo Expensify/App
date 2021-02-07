063b53dc73840a14fad2cc648e1325ae944a2896
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-02-07 12:15:30 -1000

    add bundle analyzer

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const {merge} = require('webpack-merge');
-const dotenv = require('dotenv');
 // eslint-disable-next-line no-unused-vars
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const dotenv = require('dotenv');
+
+// eslint-disable-next-line no-unused-vars
 const common = require('./webpack.common.js');
 
 const env = dotenv.config({path: path.resolve(__dirname, '../../.env.production')}).parsed;
@@ -12,7 +14,8 @@ module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-        new BundleAnalyzerPlugin(),
+        // Uncomment this and run `npm run build` to interactively inspect JS bundle contents
+        // new BundleAnalyzerPlugin(),
         new webpack.DefinePlugin({
             __REACT_WEB_CONFIG__: JSON.stringify(env),
 
