6decb5f4349ac818f8af16be2a30f21e36ca1f35
Rory Abraham <rory@expensify.com>
2021-02-08 14:30:24 -0800

    Fix hoverable to ensure state change before callback functions are executed

--- src/components/Hoverable/index.js
+++ src/components/Hoverable/index.js
@@ -22,11 +22,9 @@ class Hoverable extends Component {
      */
     toggleHoverState() {
         if (this.state.isHovered) {
-            this.props.onHoverOut();
-            this.setState({isHovered: false});
+            this.setState({isHovered: false}, this.props.onHoverOut());
         } else {
-            this.props.onHoverIn();
-            this.setState({isHovered: true});
+            this.setState({isHovered: true}, this.props.onHoverIn());
         }
     }
 
