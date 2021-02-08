7f74bdf0d3de6567784145c6998fa3edf2f820a6
Rory Abraham <rory@expensify.com>
2021-02-08 14:24:17 -0800

    Revert "Save draft state - measureInWindow happening from ReportView"
    This reverts commit eb38ba40aa4990f5f653208f2aa9d35cf9e580a1.

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -5,33 +5,42 @@ import ReportActionItemSingle from './ReportActionItemSingle';
 import ReportActionPropTypes from './ReportActionPropTypes';
 import ReportActionItemGrouped from './ReportActionItemGrouped';
 import getReportActionItemContainerStyles from '../../../styles/getReportActionItemContainerStyles';
+import styles from '../../../styles/styles';
+import ReportActionContextMenu from './ReportActionContextMenu/ReportActionContextMenu';
 import Hoverable from '../../../components/Hoverable';
 
 const propTypes = {
-    // All the data of the action item
+    /* The ID of the report this action is on. */
+    reportID: PropTypes.number.isRequired,
+
+    /* All the data of the action item */
     action: PropTypes.shape(ReportActionPropTypes).isRequired,
 
-    // Should the comment have the appearance of being grouped with the previous comment?
+    /* Should the comment have the appearance of being grouped with the previous comment? */
     displayAsGroup: PropTypes.bool.isRequired,
-
-    // This this action as the active reportAction for the report.
-    setIsActive: PropTypes.func.isRequired,
 };
 
-const ReportActionItem = React.forwardRef((props, ref) => (
-    <Hoverable
-        onHoverIn={() => props.setIsActive(true)}
-        onHoverOut={() => props.setIsActive(false)}
-    >
+const ReportActionItem = props => (
+    <Hoverable>
         {hovered => (
-            <View ref={ref} style={getReportActionItemContainerStyles(hovered)}>
-                {!props.displayAsGroup
-                    ? <ReportActionItemSingle action={props.action} />
-                    : <ReportActionItemGrouped action={props.action} />}
+            <View>
+                <View style={getReportActionItemContainerStyles(hovered)}>
+                    {!props.displayAsGroup
+                        ? <ReportActionItemSingle action={props.action} />
+                        : <ReportActionItemGrouped action={props.action} />}
+                </View>
+                <View style={styles.miniReportActionContextMenuWrapperStyle}>
+                    <ReportActionContextMenu
+                        reportID={props.reportID}
+                        reportActionID={props.action.sequenceNumber}
+                        shouldShow={hovered}
+                        isMini
+                    />
+                </View>
             </View>
         )}
     </Hoverable>
-));
+);
 
 ReportActionItem.propTypes = propTypes;
 
