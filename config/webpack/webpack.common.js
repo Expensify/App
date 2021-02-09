6be8442596f366e40b29e7867ace64f37c7accd0
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-02-09 07:49:20 -1000

    remove textinputfocusable changes

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const {IgnorePlugin} = require('webpack');
 const CustomVersionFilePlugin = require('./CustomVersionFilePlugin');
 
 // Check for a --platform command line argument (default to 'web')
@@ -41,7 +40,6 @@ module.exports = {
             ],
         }),
 
-        new IgnorePlugin(/^\.\/locale$/, /moment$/),
         new CustomVersionFilePlugin(),
     ],
     module: {
