b6e5279be03b4fb06f5f886c7aa1d4e92576b693
Rory Abraham <rory@expensify.com>
2021-02-10 17:51:47 -0800

    Remove cat

--- .github/scripts/verifyActions.sh
+++ .github/scripts/verifyActions.sh
@@ -12,8 +12,6 @@ declare -r NC='\033[0m'
 printf '\nRebuilding GitHub Actions...\n'
 npm run gh-actions-build
 
-cat .github/actions/bumpVersion/index.js
-
 # Check for a diff
 printf '\nChecking for a diff...\n'
 if [[ $(git diff --color-words --exit-code) -eq 0 ]]; then
