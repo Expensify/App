e00d81dc41dadee804443abfda01ca4b5cea0d63
Brandon Stites <bstites+1@expensify.com>
2021-02-12 12:57:46 -0700

    Fix tests

--- tests/unit/OptionsListUtilsTest.js
+++ tests/unit/OptionsListUtilsTest.js
@@ -139,8 +139,8 @@ describe('OptionsListUtils', () => {
         // When we filter in the Search view without providing a searchValue
         let results = OptionsListUtils.getSearchOptions(REPORTS, PERSONAL_DETAILS, '');
 
-        // Then all options returned should be recentReports and none should be personalDetails
-        expect(results.personalDetails.length).toBe(0);
+        // Then all recentReports and personalDetails should be returned
+        expect(results.personalDetails.length).toBe(_.size(PERSONAL_DETAILS));
 
         // Then all of the reports should be shown, including the one that has no message on them.
         expect(results.recentReports.length).toBe(_.size(REPORTS));
