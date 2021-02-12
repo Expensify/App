7ef72b14db018166c69aeba6c2b13dd0627a2aad
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-02-11 15:18:57 -1000

    Clean up comments

--- src/Navigator/RootNavigator.js
+++ src/Navigator/RootNavigator.js
@@ -1,13 +1,3 @@
-// We should still use Onyx as the single source of truth to set a route and we can still use react router for this as
-// well. This way the web code does not have to change to drastically and will just be cleaned up a bit. but we will
-// move things to a more programattic navigation style.
-
-// If we are on web then we should not need to use react-navigation at all and in fact probably don't want to, but
-// instead will use a split pane navigation that essentially have a main view and a side bar view. This will probably
-// also be used by iPad
-
-// Mobile web could maybe use react-navigation... so we can still base this on Dimensions
-
 import _ from 'underscore';
 import React from 'react';
 import Onyx, {withOnyx} from 'react-native-onyx';
