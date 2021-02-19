bfc815229db2110bde318e9b6bb24f0fba164029
Rory Abraham <rory@expensify.com>
2021-02-19 10:58:52 -0800

    Fix popover positioning

--- src/components/Modal/BaseModal.js
+++ src/components/Modal/BaseModal.js
@@ -33,7 +33,7 @@ const BaseModal = (props) => {
             windowHeight: props.windowHeight,
             isSmallScreenWidth: props.isSmallScreenWidth,
         },
-        props.anchorPosition,
+        props.popoverAnchorPosition,
     );
 
     return (
