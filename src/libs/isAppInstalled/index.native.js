951b589eae3e7bba753a7bb3a4a465e9d248a55e
Sameera Madushan <sameeradelpechithra@gmail.com>
2021-02-24 21:58:38 +0530

    Converting async await to promise

--- src/libs/isAppInstalled/index.native.js
+++ src/libs/isAppInstalled/index.native.js
@@ -4,12 +4,16 @@ import {Linking} from 'react-native';
  * Gets the URLSchema and check the existence of that application
  *
  * @param {String} urlScheme
- * @returns {Promise<Boolean>}
+ * @returns {Promise}
  */
 export default async function isAppInstalled(urlScheme) {
-    try {
-        return await Linking.canOpenURL(`${urlScheme}://`);
-    } catch {
-        return false;
-    }
+    return new Promise((resolve, reject) => {
+        Linking.canOpenURL(`${urlScheme}://`)
+            .then((isInstalled) => {
+                resolve(isInstalled);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
