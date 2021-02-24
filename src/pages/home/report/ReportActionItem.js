cccaab8c3a25da118a26f51302c0a9d3b1d1104c
Rory Abraham <rory@expensify.com>
2021-02-24 01:03:28 -0800

    Eliminate unnecessary ReportActionItemRow component

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -1,11 +1,21 @@
 import _ from 'underscore';
-import React, {memo} from 'react';
+import React, {Component} from 'react';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
+import ReportActionPropTypes from './ReportActionPropTypes';
+import styles from '../../../styles/styles';
+import getReportActionItemStyles from '../../../styles/getReportActionItemStyles';
+import PressableWithSecondaryInteraction from '../../../components/PressableWithSecondaryInteraction';
+import Hoverable from '../../../components/Hoverable';
+import PopoverWithMeasuredContent from '../../../components/PopoverWithMeasuredContent';
 import ReportActionItemSingle from './ReportActionItemSingle';
 import ReportActionItemGrouped from './ReportActionItemGrouped';
-import ReportActionPropTypes from './ReportActionPropTypes';
+import ReportActionContextMenu from './ReportActionContextMenu';
 
 const propTypes = {
+    // The ID of the report this action is on.
+    reportID: PropTypes.number.isRequired,
+
     // All the data of the action item
     action: PropTypes.shape(ReportActionPropTypes).isRequired,
 
@@ -13,17 +23,106 @@ const propTypes = {
     displayAsGroup: PropTypes.bool.isRequired,
 };
 
-const ReportActionItem = props => (
-    <>
-        {!props.displayAsGroup
-            ? <ReportActionItemSingle action={props.action} />
-            : <ReportActionItemGrouped action={props.action} />}
-    </>
-);
+class ReportActionItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isPopoverVisible: false,
+        };
+
+        // The horizontal and vertical position (relative to the screen) where the popover will display.
+        this.popoverAnchorPosition = {
+            horizontal: 0,
+            vertical: 0,
+        };
+
+        this.showPopover = this.showPopover.bind(this);
+        this.hidePopover = this.hidePopover.bind(this);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // Use underscore's isEqual to do a deep comparison of props.
+        // We can't use PureComponent because its shallow comparison wouldn't detect changes in the `action` prop.
+        return !_.isEqual(this.props, nextProps) || !_.isEqual(this.state, nextState);
+    }
+
+    /**
+     * Save the location of a native press event.
+     *
+     * @param {Object} nativeEvent
+     */
+    capturePressLocation(nativeEvent) {
+        this.popoverAnchorPosition = {
+            horizontal: nativeEvent.pageX,
+            vertical: nativeEvent.pageY,
+        };
+    }
+
+    /**
+     * Show the ReportActionContextMenu modal popover.
+     *
+     * @param {Object} [event] - A press event.
+     */
+    showPopover(event) {
+        const nativeEvent = event.nativeEvent || {};
+        this.capturePressLocation(nativeEvent);
+        this.setState({isPopoverVisible: true});
+    }
+
+    /**
+     * Hide the ReportActionContextMenu modal popover.
+     */
+    hidePopover() {
+        this.setState({isPopoverVisible: false});
+    }
+
+    render() {
+        return (
+            <PressableWithSecondaryInteraction onSecondaryInteraction={this.showPopover}>
+                <Hoverable>
+                    {hovered => (
+                        <View>
+                            <View style={getReportActionItemStyles(hovered)}>
+                                {!this.props.displayAsGroup
+                                    ? <ReportActionItemSingle action={this.props.action} />
+                                    : <ReportActionItemGrouped action={this.props.action} />}
+                            </View>
+                            <View style={styles.miniReportActionContextMenuWrapperStyle}>
+                                <ReportActionContextMenu
+                                    reportID={this.props.reportID}
+                                    reportActionID={this.props.action.sequenceNumber}
+                                    isVisible={hovered && !this.state.isPopoverVisible}
+                                    isMini
+                                />
+                            </View>
+                            <PopoverWithMeasuredContent
+                                isVisible={this.state.isPopoverVisible}
+                                onClose={this.hidePopover}
+                                anchorPosition={this.popoverAnchorPosition}
+                                animationIn="bounceIn"
+                                measureContent={() => (
+                                    <ReportActionContextMenu
+                                        isVisible
+                                        reportID={-1}
+                                        reportActionID={-1}
+                                    />
+                                )}
+                            >
+                                <ReportActionContextMenu
+                                    isVisible={this.state.isPopoverVisible}
+                                    reportID={this.props.reportID}
+                                    reportActionID={this.props.action.sequenceNumber}
+                                />
+                            </PopoverWithMeasuredContent>
+                        </View>
+                    )}
+                </Hoverable>
+            </PressableWithSecondaryInteraction>
+        );
+    }
+}
 
 ReportActionItem.propTypes = propTypes;
-ReportActionItem.displayName = 'ReportActionItem';
 
-export default memo(ReportActionItem, (prevProps, nextProps) => (
-    prevProps.displayAsGroup === nextProps.displayAsGroup && _.isEqual(prevProps.action, nextProps.action)
-));
+export default ReportActionItem;
