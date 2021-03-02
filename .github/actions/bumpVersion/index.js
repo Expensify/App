677e39847c0728c9df0d07c90fdd94276dae913a
Rory Abraham <rory@expensify.com>
2021-03-02 12:53:06 -0800

    Adjust promise chain to always fetch tags first

--- .github/actions/bumpVersion/index.js
+++ .github/actions/bumpVersion/index.js
@@ -57,71 +57,63 @@ let shouldRetry;
 
 do {
     shouldRetry = false;
-    exec('npm version prerelease -m "Update version to %s"')
-        .then(({stdout}) => {
-            postVersionUpdateNative(stdout);
-        })
-        // eslint-disable-next-line no-loop-func
-        .catch((err) => {
-            console.log(err.stdout);
-            console.log(err.stderr);
+    if (errCount > 0) {
+        console.log(
+            'Err: npm version conflict, attempting to automatically resolve',
+            `retryCount: ${++errCount}`,
+        );
+    }
 
-            // It's possible that two PRs were merged in rapid succession.
-            // In this case, both PRs will attempt to update to the same npm version.
-            // This will cause the deploy to fail with an exit code 128,
-            // saying the git tag for that version already exists.
-            if (errCount < MAX_RETRIES) {
-                console.log(
-                    'Err: npm version conflict, attempting to automatically resolve',
-                    `retryCount: ${++errCount}`,
+    if (errCount < MAX_RETRIES) {
+        // Determine current patch version
+        const {version} = JSON.parse(fs.readFileSync('./package.json'));
+        const currentPatchVersion = `v${version.split('-')[0]}`;
+        console.log('Current patch version:', currentPatchVersion);
+
+        let newVersion;
+
+        // Fetch tags
+        console.log('Fetching tags from github...');
+        const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN', {required: true}));
+        return octokit.repos.listTags({
+            owner: repoOwner,
+            repo: repoName,
+        })
+            .catch(githubError => core.setFailed(githubError))
+            .then((githubResponse) => {
+                // Find the highest build version git tag
+                const tags = githubResponse.data.map(tag => tag.name);
+                console.log('Tags: ', tags);
+                const highestBuildNumber = Math.max(
+                    ...(tags
+                        .filter(tag => tag.startsWith(currentPatchVersion))
+                        .map(tag => tag.split('-')[1])
+                    ),
                 );
+                console.log('Highest build number from current patch version:', highestBuildNumber);
+
+                // Bump the build number again
+                newVersion = `${currentPatchVersion}-${highestBuildNumber + 1}`;
+                console.log(`Setting npm version for this PR to ${newVersion}`);
+                return exec(`npm version ${newVersion} -m "Update version to ${newVersion}"`);
+            })
+            // eslint-disable-next-line no-loop-func
+            .then(({stdout}) => {
+                // NPM version successfully updated, update native versions - don't retry.
+                console.log(stdout);
+                postVersionUpdateNative(newVersion);
+            })
+            // eslint-disable-next-line no-loop-func
+            .catch(({stdout, stderr}) => {
+                // Log errors and retry
+                console.log(stdout);
+                console.error(stderr);
                 shouldRetry = true;
-                const {version} = JSON.parse(fs.readFileSync('./package.json'));
-                const currentPatchVersion = `v${version.split('-')[0]}`;
-                console.log('Current patch version:', currentPatchVersion);
+            });
+    }
 
-                // Fetch tags
-                console.log('Fetching tags from github...');
-                const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN', {required: true}));
-                return octokit.repos.listTags({
-                    owner: repoOwner,
-                    repo: repoName,
-                })
-                    .then(githubResponse => ({currentPatchVersion, githubResponse}))
-                    .catch(githubError => core.setFailed(githubError));
-            }
-
-            // Maximum retries reached, fail this action.
-            core.setFailed(err);
-        })
-        .then(({currentPatchVersion, githubResponse}) => {
-            // Find the highest build version git tag
-            const tags = githubResponse.data.map(tag => tag.name);
-            console.log('Tags: ', tags);
-            const highestBuildNumber = Math.max(
-                ...(tags
-                    .filter(tag => tag.startsWith(currentPatchVersion))
-                    .map(tag => tag.split('-')[1])
-                ),
-            );
-            console.log('Highest build number from current patch version:', highestBuildNumber);
-
-            // Bump the build number again
-            const newBuildNumber = `${currentPatchVersion}-${highestBuildNumber + 1}`;
-            console.log(`Setting npm version for this PR to ${newBuildNumber}`);
-            return exec(`npm version ${newBuildNumber} -m "Update version to ${newBuildNumber}"`);
-        })
-        // eslint-disable-next-line no-loop-func
-        .then(({stdout}) => {
-            // NPM version successfully updated, update native versions - don't retry.
-            postVersionUpdateNative(stdout);
-            shouldRetry = false;
-        })
-        .catch(({stdout, stderr}) => {
-            // Log errors and retry
-            console.log(stdout);
-            console.error(stderr);
-        });
+    // Maximum retries reached, fail this action.
+    core.setFailed('Maximum retries reached, something is wrong with this action.');
 } while (shouldRetry);
 
 
