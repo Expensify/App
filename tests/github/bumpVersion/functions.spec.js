88e47d33afc3e69d0869b9c8f98d15d614967a20
Frederico Gauz <frgauz@gmail.com>
2021-03-03 03:20:52 -0800

    fix missing github folder in test and long lines

--- tests/github/bumpVersion/functions.spec.js
+++ tests/github/bumpVersion/functions.spec.js
@@ -1,5 +1,5 @@
 const {describe} = require('jest-circus');
-const functions = require('/.github/actions/bumpVersion/functions.js');
+const functions = require('../../../.github/actions/bumpVersion/functions.js');
 
 const version = 'v2.3.9-80';
 const versionNumber = [2, 3, 9, 80];
@@ -65,18 +65,32 @@ describe('incrementPatch', () => {
 
 describe('incrementVersion', () => {
     it('should increment major', () => {
-        expect(functions.incrementVersion(version, functions.semanticVersionLevels.major)).toStrictEqual('v3.0.0');
+        expect(
+            functions.incrementVersion(
+                version, functions.semanticVersionLevels.major,
+            ),
+        ).toStrictEqual('v3.0.0');
     });
     it('should increment major even above max level', () => {
-        expect(functions.incrementVersion(`v${functions.maxIncrements}.5.1-80`, functions.semanticVersionLevels.major)).toStrictEqual(`v${functions.maxIncrements + 1}.0.0`);
+        expect(
+            functions.incrementVersion(
+                `v${functions.maxIncrements}.5.1-80`, functions.semanticVersionLevels.major,
+            ),
+        ).toStrictEqual(`v${functions.maxIncrements + 1}.0.0`);
     });
     it('should increment build number', () => {
-        expect(functions.incrementVersion(version, functions.semanticVersionLevels.build)).toStrictEqual('v2.3.9-81');
+        expect(functions.incrementVersion(
+            version, functions.semanticVersionLevels.build,
+        )).toStrictEqual('v2.3.9-81');
     });
     it('should add build number if there is no build number', () => {
-        expect(functions.incrementVersion(version.split('-')[0], functions.semanticVersionLevels.build)).toStrictEqual('v2.3.9-1');
+        expect(
+            functions.incrementVersion(version.split('-')[0], functions.semanticVersionLevels.build),
+        ).toStrictEqual('v2.3.9-1');
     });
     it('should increment patch if minor is above max level', () => {
-        expect(functions.incrementVersion(`v2.3.9-${functions.maxIncrements}`, functions.semanticVersionLevels.build)).toStrictEqual('v2.3.10');
+        expect(
+            functions.incrementVersion(`v2.3.9-${functions.maxIncrements}`, functions.semanticVersionLevels.build),
+        ).toStrictEqual('v2.3.10');
     });
 });
