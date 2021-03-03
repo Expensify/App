8e40767c794faac879a9b68aba470a9e06c3ecba
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-03-02 18:15:05 -1000

    remove active report things

--- src/pages/home/report/ReportView.js
+++ src/pages/home/report/ReportView.js
@@ -3,10 +3,8 @@ import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import ReportActionView from './ReportActionsView';
 import ReportActionCompose from './ReportActionCompose';
-import {addAction, subscribeToReportTypingEvents, unsubscribeFromReportChannel} from '../../../libs/actions/Report';
+import {addAction} from '../../../libs/actions/Report';
 import KeyboardSpacer from '../../../components/KeyboardSpacer';
-import Timing from '../../../libs/actions/Timing';
-import CONST from '../../../CONST';
 import styles from '../../../styles/styles';
 
 const propTypes = {
@@ -17,26 +15,8 @@ const propTypes = {
 // This is a PureComponent so that it only re-renders when the reportID changes or when the report changes from
 // active to inactive (or vice versa). This should greatly reduce how often comments are re-rendered.
 class ReportView extends React.Component {
-    componentDidMount() {
-        console.log('ReportView mounted');
-        subscribeToReportTypingEvents(this.props.reportID);
-    }
-
     shouldComponentUpdate(prevProps) {
         return this.props.reportID !== prevProps.reportID;
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.reportID !== this.props.reportID) {
-            unsubscribeFromReportChannel(prevProps.reportID);
-            subscribeToReportTypingEvents(this.props.reportID);
-            Timing.end(CONST.TIMING.SWITCH_REPORT, CONST.TIMING.COLD);
-        }
-    }
-
-    componentWillUnmount() {
-        unsubscribeFromReportChannel(this.props.reportID);
-        console.log('ReportView unmounted');
     }
 
     render() {
@@ -44,7 +24,6 @@ class ReportView extends React.Component {
             <View style={[styles.chatContent]}>
                 <ReportActionView
                     reportID={this.props.reportID}
-                    isActiveReport
                 />
 
                 <ReportActionCompose
@@ -58,5 +37,4 @@ class ReportView extends React.Component {
 }
 
 ReportView.propTypes = propTypes;
-
 export default ReportView;
