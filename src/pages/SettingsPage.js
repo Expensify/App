8559638bf0c13d8d49d025467e700aa2d8f58eff
Puneet Lath <puneet@expensify.com>
2021-03-07 16:26:36 -0500

    Add settings and privacy links to settings page

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -16,6 +16,8 @@ import AvatarWithIndicator from '../components/AvatarWithIndicator';
 import HeaderWithCloseButton from '../components/HeaderWithCloseButton';
 import {redirectToLastReport} from '../libs/actions/App';
 import HeaderGap from '../components/HeaderGap';
+import CONST from '../CONST';
+ import openURLInNewTab from '../libs/openURLInNewTab';
 
 const propTypes = {
     /* Onyx Props */
@@ -97,10 +99,32 @@ const SettingsPage = ({
                         </Text>
                     </TouchableOpacity>
                 </View>
-                <Text style={[styles.chatItemMessageHeaderTimestamp]} numberOfLines={1}>
-                    v
-                    {version}
-                </Text>
+                <View  style={[styles.sidebarFooter]}>
+                    <Text style={[styles.chatItemMessageHeaderTimestamp]} numberOfLines={1}>
+                        v
+                        {version}
+                    </Text>
+                    <Text style={[styles.chatItemMessageHeaderTimestamp]} numberOfLines={1}>
+                        Read the
+                        {' '}
+                        <Text
+                            style={[styles.chatItemMessageHeaderTimestamp, styles.link]}
+                            onPress={() => openURLInNewTab(CONST.TERMS_URL)}
+                        >
+                            terms of service
+                        </Text>
+                        {' '}
+                        and
+                        {' '}
+                        <Text
+                            style={[styles.chatItemMessageHeaderTimestamp, styles.link]}
+                            onPress={() => openURLInNewTab(CONST.PRIVACY_URL)}
+                        >
+                        privacy policy
+                        </Text>
+                        . 
+                    </Text>
+                </View>
             </View>
         </>
     );
