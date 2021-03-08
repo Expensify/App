57bf1ba7cc37e39883f9f68c58065819cbb7abcc
Tim Golen <tgolen@expensify.com>
2021-03-08 08:30:47 -0700

    Add some debug logs and timing

--- src/libs/MigrateOnyx.js
+++ src/libs/MigrateOnyx.js
@@ -1,5 +1,9 @@
 function run() {
+    const startTime = Date.now();
+    console.debug('[Migrate Onyx] start');
     return new Promise((resolve) => {
+        const timeElapsed = Date.now() - startTime;
+        console.debug(`[Migrate Onyx] finished in ${timeElapsed}ms`);
         resolve();
     });
 }
