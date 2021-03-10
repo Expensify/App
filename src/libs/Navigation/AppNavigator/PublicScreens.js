68f83201509ede906ebef253b3478086112965bd
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-03-09 15:04:26 -1000

    Fix up auth routes

--- src/libs/Navigation/AppNavigator/PublicScreens.js
+++ src/libs/Navigation/AppNavigator/PublicScreens.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import SignInPage from '../../../pages/signin/SignInPage';
 import SetPasswordPage from '../../../pages/SetPasswordPage';
