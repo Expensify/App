b0393f15b0880782984fa92be4b5e486811d267a
Frederico Gauz <frgauz@gmail.com>
2021-03-11 04:24:19 -0800

    add diff-so-fancy to script

--- .github/scripts/verifyActions.sh
+++ .github/scripts/verifyActions.sh
@@ -8,13 +8,27 @@ declare -r GREEN='\033[0;32m'
 declare -r RED='\033[0;31m'
 declare -r NC='\033[0m'
 
+# In order for this script to be safely run from anywhere, we cannot use the raw relative path '../scripts'
+declare SCRIPTS_DIR
+SCRIPTS_DIR="$(dirname "$(dirname "$0")")/scripts"
+
+declare LIB_PATH
+LIB_PATH="/root/expensify/bin/diff-so-fancy"
+
+# Clone diff-so-fancy repository
+git clone https://github.com/so-fancy/diff-so-fancy.git $LIB_PATH
+
+# Expose diff-so-fancy to path
+PATH=$LIB_PATH:$PATH
+
 # Rebuild all the Github Actions
 printf '\nRebuilding GitHub Actions...\n'
 npm run gh-actions-build
 
 # Check for a diff
 printf '\nChecking for a diff...\n'
-if [[ $(git diff --exit-code) -eq 0 ]]; then
+git diff | diff-so-fancy | less --tabs=4 -RFX
+if [[ $(git diff --quiet --exit-code) -eq 0 ]]; then
     echo -e "${GREEN}Github Actions are up to date!${NC}"
     exit 0
 else
