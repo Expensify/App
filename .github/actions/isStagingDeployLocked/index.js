4412e1d2f8e3ae77e8c0dbe247c5c51c80eb69bf
Rory Abraham <rory@expensify.com>
2021-03-11 16:01:32 -0800

    Rebuild GH actions

--- .github/actions/isStagingDeployLocked/index.js
+++ .github/actions/isStagingDeployLocked/index.js
@@ -357,6 +357,8 @@ class GithubUtils {
                     });
                 }
 
+                issueBody += '\r\ncc @Expensify/applauseleads\r\n';
+
                 return issueBody;
             })
             // eslint-disable-next-line no-console
