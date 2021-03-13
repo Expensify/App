a7a8896c46f2692418a3726fd8f911d873eaec1d
andrew <andrew@expensify.com>
2021-03-12 17:12:19 -0700

    Undo tests for now

--- tests/unit/createOrUpdateStagingDeploy.js
+++
@@ -1,9 +0,0 @@
-const createOrUpdateStagingDeploy = require('../../.github/actions/createOrUpdateStagingDeploy/createOrUpdateStagingDeploy').default();
-
-describe('createOrUpdateStagingDeploy.js', () => {
-    test('', () => {
-        createOrUpdateStagingDeploy().then((issueBody) => {
-            expect(issueBody).toBe('andrew');
-        });
-    });
-});
