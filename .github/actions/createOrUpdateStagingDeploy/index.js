25229e129685219bec8b299be8e68e9fc561f1c4
Rory Abraham <rory@expensify.com>
2021-03-17 15:53:04 -0700

    Add emojis in dummy PR to retest another

--- .github/actions/createOrUpdateStagingDeploy/index.js
+++ .github/actions/createOrUpdateStagingDeploy/index.js
@@ -32,7 +32,7 @@ githubUtils.getStagingDeployCash()
         ),
     ))
     .then(({data}) => {
-        console.log('Successfully updated StagingDeployCash!', data.html_url);
+        console.log('Successfully updated StagingDeployCash! 🎉', data.html_url);
         process.exit(0);
     })
     .catch((err) => {
@@ -74,7 +74,7 @@ githubUtils.getStagingDeployCash()
         newVersion,
         _.map(PRNumbers, GithubUtils.getPullRequestURLFromNumber),
     ))
-    .then(({data}) => console.log('Successfully created new StagingDeployCash!', data.html_url))
+    .then(({data}) => console.log('Successfully created new StagingDeployCash! 🎉', data.html_url))
     .catch((err) => {
         console.error('An error occurred!', err);
         core.setFailed(err);
