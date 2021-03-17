d027da0922a37e16f9378f61c743ae95319eeaa6
Nikki Wines <nikkiwines@expensify.com>
2021-03-17 12:48:04 -0700

    merge master, update AuthScreens.js to not call fetchTimeZone()

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -23,7 +23,7 @@ import CONFIG from '../../../CONFIG';
 import {fetchCountryCodeByRequestIP} from '../../actions/GeoLocation';
 import KeyboardShortcut from '../../KeyboardShortcut';
 import Navigation from '../Navigation';
-import {getBetas} from '../../actions/User';
+import * as User from '../../actions/User';
 import NameValuePair from '../../actions/NameValuePair';
 
 // Main drawer navigator
@@ -71,8 +71,8 @@ class AuthScreens extends React.Component {
         // Fetch some data we need on initialization
         NameValuePair.get(CONST.NVP.PRIORITY_MODE, ONYXKEYS.PRIORITY_MODE, 'default');
         PersonalDetails.fetch();
-        PersonalDetails.fetchTimezone();
-        getBetas();
+        User.fetch()
+        User.getBetas();
         fetchAllReports(true, true);
         fetchCountryCodeByRequestIP();
         UnreadIndicatorUpdater.listenForReportChanges();
@@ -85,8 +85,8 @@ class AuthScreens extends React.Component {
                 return;
             }
             PersonalDetails.fetch();
-            PersonalDetails.fetchTimezone();
-            getBetas();
+            User.fetch()
+            User.getBetas();
         }, 1000 * 60 * 30);
 
         Timing.end(CONST.TIMING.HOMEPAGE_INITIAL_RENDER);
