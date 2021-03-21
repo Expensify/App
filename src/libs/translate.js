879b6c9c1e82db958c81fa9dab75df720be89db1
tugbadogan <tugbadogan92@gmail.com>
2021-03-21 18:09:04 +0000

    Introduce translations and translate library for internationalization

---
+++ src/libs/translate.js
@@ -0,0 +1,57 @@
+import lodashGet from 'lodash.get';
+import Str from 'expensify-common/lib/str';
+import Log from './Log';
+import Config from '../CONFIG';
+import translations from '../languages/translations';
+
+/**
+ * Return translated string for given locale and key
+ *
+ * @param {String} locale
+ * @param {String|Array} key
+ * @param {Object} variables
+ * @returns {string}
+ */
+function translate(locale, key, variables = {}) {
+    const localeLanguage = locale.substring(0, 2);
+    const fullLocale = lodashGet(translations, locale, {});
+    const language = lodashGet(translations, localeLanguage, {});
+    const defaultLanguage = lodashGet(translations, 'en', {});
+
+    let translationValue;
+
+    // Search key in full locale
+    translationValue = lodashGet(fullLocale, key);
+    if (translationValue) {
+        return Str.result(translationValue, variables);
+    }
+
+    // Key is not found in full locale, search it in language
+    translationValue = lodashGet(language, key);
+    if (translationValue) {
+        return Str.result(translationValue, variables);
+    }
+    if (localeLanguage !== 'en') {
+        Log.alert(`${key} was not found in the ${localeLanguage} locale`, 0, {}, false);
+    }
+
+    // Key is not translated, search it in default language (en)
+    translationValue = lodashGet(defaultLanguage, key);
+    if (translationValue) {
+        return Str.result(translationValue, variables);
+    }
+
+    // Key is not found in default language, on production log an alert to server
+    // on development throw an error
+    if (Config.IS_IN_PRODUCTION) {
+        const keyString = Array.isArray(key) ? key.join('.') : key;
+        Log.alert(`${keyString} was not found in the en locale`, 0, {}, false);
+        return keyString;
+    }
+    throw new Error(`${key} was not found in the default language`);
+}
+
+export {
+    // eslint-disable-next-line import/prefer-default-export
+    translate,
+};
---
+++ tests/unit/TranslateTest.js
@@ -0,0 +1,55 @@
+const translations = require('../../src/languages/translations');
+const CONFIG = require('../../src/CONFIG');
+
+translations.default = {
+  'en': {
+    testkey1: 'English',
+    testKey2: 'Test Word 2',
+    testKey3: 'Test Word 3',
+    testKeyGroup: {
+      testFunction: ({ testVariable }) => `With variable ${testVariable}`,
+    },
+  },
+  'es': {
+    testkey1: 'Spanish',
+    testKey2: 'Spanish Word 2',
+  },
+  'es-ES': { testkey1: 'Spanish ES' },
+};
+
+import { translate } from '../../src/libs/translate';
+
+describe('translate', () => {
+  it('Test present key in full locale', () => {
+    expect(translate('es-ES', 'testkey1')).toBe('Spanish ES');
+  });
+
+  it('Test when key is not found in full locale, but present in language', () => {
+    expect(translate('es-ES', 'testKey2')).toBe('Spanish Word 2');
+    expect(translate('es', 'testKey2')).toBe('Spanish Word 2');
+  });
+
+  it('Test when key is not found in full locale and language, but present in default', () => {
+    expect(translate('es-ES', 'testKey3')).toBe('Test Word 3');
+  });
+
+  test('Test when key is not found in default', () => {
+    expect(() => translate('es-ES', 'testKey4')).toThrow(Error);
+    expect(() => translate('es-ES', ['a', 'b', 'c'])).toThrow(Error);
+  });
+
+  test('Test when key is not found in default (Production Mode)', () => {
+    CONFIG.default = {
+      IS_IN_PRODUCTION: true,
+    }
+    expect(translate('es-ES', 'testKey4')).toBe('testKey4');
+    expect(translate('es-ES', ['a', 'b', 'c'])).toBe('a.b.c');
+  });
+
+  it('Test when translation value is a function', () => {
+    const expectedValue = 'With variable Test Variable';
+    const testVariable = 'Test Variable';
+    expect(translate('en', 'testKeyGroup.testFunction', { testVariable })).toBe(expectedValue);
+    expect(translate('en', ['testKeyGroup', 'testFunction'], { testVariable })).toBe(expectedValue);
+  });
+});
