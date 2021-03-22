f7a1a6475bb30c586cf9b9775c5897be7c98fa36
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-03-22 06:57:14 -1000

    make app state only run on native and sleep timer only run on web/desktop

---
+++ src/libs/AppStateMonitor/shouldReportActivity/index.js
@@ -0,0 +1,3 @@
+// We only need to report when the app becomes active on native since web maintains most of it's network functions while
+// in the "background" and the concept is not quite the same on mobile.
+export default false;
