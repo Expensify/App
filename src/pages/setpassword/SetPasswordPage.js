e6f5062bc842352c264d606b24995ba534949b3d
Jasper Huang <jasper@expensify.com>
2021-03-22 12:15:31 +0800

    fix eslint

--- src/pages/setpassword/SetPasswordPage.js
+++ src/pages/setpassword/SetPasswordPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView,} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import styles from '../../styles/styles';
 import SignInPageLayout from '../signin/SignInPageLayout';
 import SetPasswordForm from './SetPasswordForm';
