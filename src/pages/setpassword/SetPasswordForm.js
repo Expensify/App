db30d9313175dcf982ea491727246a522dbc193a
Jasper Huang <jasper@expensify.com>
2021-03-23 19:46:58 +0800

    dry up and reduce the number of components

--- src/pages/setpassword/SetPasswordForm.js
+++ src/pages/setpassword/SetPasswordForm.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import {
-    Text, TextInput, View,
-} from 'react-native';
-import {withOnyx} from 'react-native-onyx';
+import {Text, TextInput, View} from 'react-native';
 import _ from 'underscore';
+import React from 'react';
 import lodashGet from 'lodash.get';
-import styles from '../../../styles/styles';
-import ButtonWithLoader from '../../../components/ButtonWithLoader';
-import {setPassword} from '../../../libs/actions/Session';
-import ONYXKEYS from '../../../ONYXKEYS';
-import SetPasswordPageProps from '../SetPasswordPageProps';
+import styles from '../../styles/styles';
+import ButtonWithLoader from '../../components/ButtonWithLoader';
+import {setPassword} from '../../libs/actions/Session';
+import SetPasswordPageProps from './SetPasswordPageProps';
 
-class SetPasswordFormWide extends React.Component {
+class SetPasswordForm extends React.Component {
     constructor(props) {
         super(props);
-
         this.submitForm = this.submitForm.bind(this);
 
         this.state = {
@@ -42,7 +37,7 @@ class SetPasswordFormWide extends React.Component {
 
     render() {
         return (
-            <View style={[styles.loginFormContainer]}>
+            <>
                 <View style={[styles.mb4]}>
                     <Text style={[styles.formLabel]}>Enter a password:</Text>
                     <TextInput
@@ -71,28 +66,12 @@ class SetPasswordFormWide extends React.Component {
                         {this.props.account.error}
                     </Text>
                 )}
-
-                <View style={[styles.mt6]}>
-                    <View style={[styles.mb6]}>
-                        <Text style={[styles.textP, styles.textStrong, styles.mb1]}>
-                            With Expensify.cash, chat and payments are the same thing.
-                        </Text>
-                        <Text style={[styles.textP]}>
-                            Money talks. And now that chat and payments are in one place, it&apos;s also easy.
-                            {' '}
-                            Your payments get to you as fast as you can get your point across.
-                        </Text>
-                    </View>
-                </View>
-            </View>
+            </>
         );
     }
 }
 
-SetPasswordFormWide.propTypes = SetPasswordPageProps.propTypes;
-SetPasswordFormWide.defaultProps = SetPasswordPageProps.defaultProps;
+SetPasswordForm.propTypes = SetPasswordPageProps.propTypes;
+SetPasswordForm.defaultProps = SetPasswordPageProps.defaultProps;
 
-export default withOnyx({
-    credentials: {key: ONYXKEYS.CREDENTIALS},
-    account: {key: ONYXKEYS.ACCOUNT},
-})(SetPasswordFormWide);
+export default SetPasswordForm;
--- src/pages/setpassword/SetPasswordForm/index.native.js
+++
@@ -1,3 +0,0 @@
-import SetPasswordFormNarrow from './SetPasswordFormNarrow';
-
-export default SetPasswordFormNarrow;
