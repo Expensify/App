f6b1ef689ab5d7616fa47f4749a28e8bad344205
Jasper Huang <jasper@expensify.com>
2021-03-23 19:46:58 +0800

    dry up and reduce the number of components

--- src/pages/setpassword/SetPasswordPage.js
+++ src/pages/setpassword/SetPasswordPage.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text, View, Image} from 'react-native';
 import styles from '../../styles/styles';
 import SignInPageLayout from '../signin/SignInPageLayout';
-import SetPasswordForm from './SetPasswordForm';
 import SetPasswordPageProps from './SetPasswordPageProps';
+import welcomeScreenshot from '../../../assets/images/welcome-screenshot.png';
+import withWindowDimensions from '../../components/withWindowDimensions';
+import SetPasswordForm from './SetPasswordForm';
 
-const SetPasswordPage = props => (
-    <SafeAreaView style={[styles.signInPage]}>
-        <SignInPageLayout>
-            <SetPasswordForm
-                /* eslint-disable-next-line react/jsx-props-no-spreading */
-                {...props}
-            />
-        </SignInPageLayout>
-    </SafeAreaView>
-);
+const SetPasswordPage = (props) => {
+    const welcomeText = (
+        <View style={props.isSmallScreenWidth ? [] : [styles.mb6]}>
+            <Text style={[styles.textLabel, styles.textStrong, styles.mb1]}>
+                With Expensify.cash, chat and payments are the same thing.
+            </Text>
+            <Text style={[props.isSmallScreenWidth ? styles.textLabel : styles.textP]}>
+                Money talks. And now that chat and payments are in one place, it&apos;s also easy.
+                {' '}
+                Your payments get to you as fast as you can get your point across.
+            </Text>
+        </View>
+    );
+    return (
+        <SafeAreaView style={[styles.signInPage]}>
+            <SignInPageLayout>
+                <View style={[styles.loginFormContainer]}>
+                    <SetPasswordForm
+                        /* eslint-disable-next-line react/jsx-props-no-spreading */
+                        {...props}
+                    />
+                    {props.isSmallScreenWidth && (
+                        <View style={[styles.mt5, styles.mb5]}>
+                            <Image
+                                resizeMode="contain"
+                                style={[styles.signinWelcomeScreenshot]}
+                                source={welcomeScreenshot}
+                            />
+                        </View>
+                    )}
+                    {welcomeText}
+                </View>
+            </SignInPageLayout>
+        </SafeAreaView>
+    );
+};
 
 SetPasswordPage.propTypes = SetPasswordPageProps.propTypes;
 SetPasswordPage.defaultProps = SetPasswordPageProps.defaultProps;
 
-export default SetPasswordPage;
+export default withWindowDimensions(SetPasswordPage);
