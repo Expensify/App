47cf1986341e5248c63f46d345ecffc7802ab729
Jasper Huang <jasper@expensify.com>
2021-03-24 18:29:31 +0800

    refactor based on PR comments

---
+++ src/components/WelcomeText.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../styles/styles';
+import withWindowDimensions from './withWindowDimensions';
+
+const propTypes = {
+    isSmallScreenWidth: PropTypes.bool.isRequired
+};
+
+const WelcomeText = props => (
+    <View style={props.isSmallScreenWidth ? [] : [styles.mb6, styles.mt6]}>
+        <Text style={[props.isSmallScreenWidth ? styles.textLabel : styles.textP, styles.textStrong, styles.mb1]}>
+            With Expensify.cash, chat and payments are the same thing.
+        </Text>
+        <Text style={[props.isSmallScreenWidth ? styles.textLabel : styles.textP]}>
+            Money talks. And now that chat and payments are in one place, it&apos;s also easy.
+            {' '}
+            Your payments get to you as fast as you can get your point across.
+        </Text>
+    </View>
+);
+
+WelcomeText.propTypes = propTypes;
+
+export default withWindowDimensions(WelcomeText);
