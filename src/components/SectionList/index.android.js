f3a75859201886602dd348e6777525258bebbdf7
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-03-25 17:11:24 -1000

    kinda want to remove the clipped sub views on main flat list...

--- src/components/SectionList/index.android.js
+++ src/components/SectionList/index.android.js
@@ -1,13 +1,15 @@
-import SectionList from 'react-native';
+import React, {forwardRef} from 'react';
+import {SectionList} from 'react-native';
 
-export default props => (
+export default forwardRef((props, ref) => (
     <SectionList
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
+        ref={ref}
 
-        // For Android we want to use removeClippedSubviews since it helps manage memory consumption. When we run out
-        // memory images stop loading and appear as grey circles
+        // For Android we want to use removeClippedSubviews since it helps manage memory consumption. When we
+        // run out memory images stop loading and appear as grey circles
         // eslint-disable-next-line react/jsx-props-no-multi-spaces
         removeClippedSubviews
     />
-);
+));
