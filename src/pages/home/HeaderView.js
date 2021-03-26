66b9aa64ce9eff5d5b6fe047ee1979460a0ff5ca
Luke Donahue <luke@expensify.com>
2021-03-26 13:57:01 -0700

    refactored headerview to class component

--- src/pages/home/HeaderView.js
+++ src/pages/home/HeaderView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Pressable} from 'react-native';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
@@ -18,16 +18,11 @@ import {getReportParticipantsTitle} from '../../libs/reportUtils';
 import OptionRowTitle from './sidebar/OptionRowTitle';
 import {getPersonalDetailsForLogins} from '../../libs/OptionsListUtils';
 import {participantPropTypes} from './sidebar/optionPropTypes';
+import VideoChatMenu from '../../components/VideoChatMenu';
 
 const propTypes = {
     // Toggles the navigationMenu open and closed
     onNavigationMenuButtonClicked: PropTypes.func.isRequired,
-
-    // Toggles the videoChatMenu open and closed
-    onVideoChatMenuButtonClicked: PropTypes.func.isRequired,
-
-    // Indicates whether the video chat menu is active
-    isVideoChatMenuActive: PropTypes.bool.isRequired,
 
     /* Onyx Props */
     // The report currently being looked at
@@ -55,78 +50,121 @@ const defaultProps = {
     report: null,
 };
 
-const HeaderView = (props) => {
-    const participants = lodashGet(props.report, 'participants', []);
-    const reportOption = {
-        text: lodashGet(props.report, 'reportName', ''),
-        tooltipText: getReportParticipantsTitle(participants),
-        participantsList: getPersonalDetailsForLogins(participants, props.personalDetails),
-    };
+class HeaderView extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <View style={[styles.appContentHeader]} nativeID="drag-area">
-            <View style={[styles.appContentHeaderTitle, !props.isSmallScreenWidth && styles.pl5]}>
-                {props.isSmallScreenWidth && (
-                    <Pressable
-                        onPress={props.onNavigationMenuButtonClicked}
-                        style={[styles.LHNToggle]}
-                    >
-                        <Icon src={BackArrow} />
-                    </Pressable>
-                )}
-                {props.report && props.report.reportName && (
-                    <View
-                        style={[
-                            styles.flex1,
-                            styles.flexRow,
-                            styles.alignItemsCenter,
-                            styles.justifyContentBetween,
-                        ]}
-                    >
+        this.participants = lodashGet(this.props.report, 'participants', []);
+        this.reportOption = {
+            text: lodashGet(this.props.report, 'reportName', ''),
+            tooltipText: getReportParticipantsTitle(this.participants),
+            participantsList: getPersonalDetailsForLogins(this.participants, this.props.personalDetails),
+        };
+        this.toggleVideoChatMenu = this.toggleVideoChatMenu.bind(this);
+        this.measureVideoChatIconPosition = this.measureVideoChatIconPosition.bind(this);
+        this.videoChatIconWrapper = null;
+
+        this.state = {
+            isVideoChatMenuActive: false,
+            videoChatIconPosition: {x: 0, y: 0},
+        };
+    }
+
+    toggleVideoChatMenu() {
+        this.setState(prevState => ({
+            isVideoChatMenuActive: !prevState.isVideoChatMenuActive,
+        }));
+    }
+
+    measureVideoChatIconPosition() {
+        if (this.videoChatIconWrapper) {
+            this.videoChatIconWrapper.measureInWindow((x, y) => this.setState({
+                videoChatIconPosition: {x, y},
+            }));
+        }
+    }
+
+    render() {
+        return (
+            <View style={[styles.appContentHeader]} nativeID="drag-area">
+                <View style={[styles.appContentHeaderTitle, !this.props.isSmallScreenWidth && styles.pl5]}>
+                    {this.props.isSmallScreenWidth && (
                         <Pressable
-                            onPress={() => {
-                                if (participants.length === 1) {
-                                    Navigation.navigate(ROUTES.getDetailsRoute(participants[0]));
-                                }
-                            }}
-                            style={[styles.flexRow, styles.alignItemsCenter, styles.flex1]}
+                            onPress={this.props.onNavigationMenuButtonClicked}
+                            style={[styles.LHNToggle]}
                         >
-                            <MultipleAvatars avatarImageURLs={props.report.icons} />
-                            <View style={[styles.flex1, styles.flexRow]}>
-                                <OptionRowTitle
-                                    option={reportOption}
-                                    tooltipEnabled
-                                    numberOfLines={2}
-                                    style={[styles.headerText]}
+                            <Icon src={BackArrow} />
+                        </Pressable>
+                    )}
+                    {this.props.report && this.props.report.reportName && (
+                        <View
+                            style={[
+                                styles.flex1,
+                                styles.flexRow,
+                                styles.alignItemsCenter,
+                                styles.justifyContentBetween,
+                            ]}
+                        >
+                            <Pressable
+                                onPress={() => {
+                                    if (this.participants.length === 1) {
+                                        Navigation.navigate(ROUTES.getDetailsRoute(this.participants[0]));
+                                    }
+                                }}
+                                style={[styles.flexRow, styles.alignItemsCenter, styles.flex1]}
+                            >
+                                <MultipleAvatars avatarImageURLs={this.props.report.icons} />
+                                <View style={[styles.flex1, styles.flexRow]}>
+                                    <OptionRowTitle
+                                        option={this.reportOption}
+                                        tooltipEnabled
+                                        numberOfLines={2}
+                                        style={[styles.headerText]}
+                                    />
+                                </View>
+                            </Pressable>
+                            <View style={[styles.reportOptions, styles.flexRow]}>
+                                <View
+                                    ref={el => this.videoChatIconWrapper = el}
+                                    onLayout={this.measureVideoChatIconPosition}
+                                >
+                                    <Pressable
+                                        onPress={() => {
+                                            this.toggleVideoChatMenu();
+                                        }}
+                                        style={[styles.touchableButtonImage, styles.mr0]}
+                                    >
+                                        <Icon
+                                            src={Phone}
+                                            fill={this.state.isVideoChatMenuActive
+                                                ? themeColors.heading
+                                                : themeColors.icon}
+                                        />
+                                    </Pressable>
+                                </View>
+                                <Pressable
+                                    onPress={() => togglePinnedState(this.props.report)}
+                                    style={[styles.touchableButtonImage, styles.mr0]}
+                                >
+                                    <Icon
+                                        src={Pin}
+                                        fill={this.props.report.isPinned ? themeColors.heading : themeColors.icon}
+                                    />
+                                </Pressable>
+                                <VideoChatMenu
+                                    onClose={this.toggleVideoChatMenu}
+                                    isVisible={this.state.isVideoChatMenuActive}
+                                    anchorPosition={this.state.videoChatIconPosition}
                                 />
                             </View>
-                        </Pressable>
-                        <View style={[styles.reportOptions, styles.flexRow]}>
-                            <Pressable
-                                onPress={() => props.onVideoChatMenuButtonClicked()}
-                                style={[styles.touchableButtonImage, styles.mr0]}
-                            >
-                                <Icon
-                                    src={Phone}
-                                    fill={props.isVideoChatMenuActive ? themeColors.heading : themeColors.icon}
-                                />
-                            </Pressable>
-                            <Pressable
-                                onPress={() => togglePinnedState(props.report)}
-                                style={[styles.touchableButtonImage, styles.mr0]}
-                            >
-                                <Icon
-                                    src={Pin}
-                                    fill={props.report.isPinned ? themeColors.heading : themeColors.icon}
-                                />
-                            </Pressable>
                         </View>
-                    </View>
-                )}
+                    )}
+                </View>
             </View>
-        </View>
-    );
-};
+        );
+    }
+}
+
 HeaderView.propTypes = propTypes;
 HeaderView.displayName = 'HeaderView';
 HeaderView.defaultProps = defaultProps;
