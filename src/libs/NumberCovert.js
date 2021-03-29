76c7feee457858f1b82ee329c1bf2241cafcfd79
Ahtazaz Ahmed Khan <ahmedahtazaz@gmail.com>
2021-03-30 00:00:12 +0500

    Suggested changes have been incorporated.

--- src/libs/NumberCovert.js
+++ src/libs/NumberCovert.js
@@ -1,87 +1,54 @@
 import lodashGet from 'lodash/get';
 import loadashTrim from 'lodash/trim';
 import loadashTrimStart from 'lodash/trimStart';
-import loadashToString from 'lodash/toString';
 import loadashIncludes from 'lodash/includes';
 import loadashStartsWith from 'lodash/startsWith';
 import loadashPadStart from 'lodash/padStart';
-import Log from './Log';
-import Config from '../CONFIG';
 import translations from '../languages/translations';
-
-function showErrorAndReturn(locale) {
-    // When a locale is entered which is not available, on production log an alert to server
-
-    if (Config.IS_IN_PRODUCTION) {
-        Log.alert(`${locale} was not found.`, 0, {}, false);
-    }
-
-    return `${locale} was not found.`;
-}
 
 /**
  * Returns a locally converted phone number without the country code
  *
  * @param {String} locale eg 'en', 'es-ES'
- * @param {String|Array} number
+ * @param {String} number
  * @returns {string}
  */
 function toLocalPhone(locale, number) {
-    const numString = loadashTrim(loadashToString(number));
-
-    let withoutPlusNum;
-
-    if (loadashIncludes(numString, '+')) {
-        withoutPlusNum = loadashTrimStart(numString, '+');
-    } else {
-        withoutPlusNum = numString;
-    }
-
+    const numString = loadashTrim(number);
+    const withoutPlusNum = loadashIncludes(numString, '+') ? loadashTrimStart(numString, '+') : numString;
     const fullLocale = lodashGet(translations, locale, {});
-    const phoneCountryCode = fullLocale.phoneCountryCode;
+    const country = fullLocale.phoneCountryCode;
 
-    if (phoneCountryCode) {
-        if (loadashStartsWith(withoutPlusNum, phoneCountryCode)) {
-            return loadashTrimStart(withoutPlusNum, phoneCountryCode);
+    if (country) {
+        if (loadashStartsWith(withoutPlusNum, country)) {
+            return loadashTrimStart(withoutPlusNum, country);
         }
         return numString;
     }
-    return showErrorAndReturn(locale);
+    return `${locale} was not found.`;
 }
 
 /**
  * Returns an internationally converted phone number with the country code
  *
  * @param {String} locale eg 'en', 'es-ES'
- * @param {String|Array} number
+ * @param {String} number
  * @returns {string}
  */
 function fromLocalPhone(locale, number) {
-    const numString = loadashTrim(loadashToString(number));
-    let withoutPlusNum;
-
-    if (loadashIncludes(numString, '+')) {
-        withoutPlusNum = loadashTrimStart(numString, '+');
-    } else {
-        withoutPlusNum = numString;
-    }
-
+    const numString = loadashTrim(number);
+    const withoutPlusNum = loadashIncludes(numString, '+') ? loadashTrimStart(numString, '+') : numString;
     const fullLocale = lodashGet(translations, locale, {});
-    const phoneCountryCode = fullLocale.phoneCountryCode;
+    const country = fullLocale.phoneCountryCode;
+    const paddedConCode = country ? loadashPadStart(country, country.length + 1, '+') : '';
 
-    let paddedConCode = '';
-
-    if (phoneCountryCode) {
-        paddedConCode = loadashPadStart(phoneCountryCode, phoneCountryCode.length + 1, '+');
-    }
-
-    if (phoneCountryCode) {
-        if (loadashStartsWith(withoutPlusNum, phoneCountryCode)) {
+    if (country) {
+        if (loadashStartsWith(withoutPlusNum, country)) {
             return loadashPadStart(withoutPlusNum, withoutPlusNum.length + 1, '+');
         }
         return loadashPadStart(withoutPlusNum, paddedConCode.length + withoutPlusNum.length, paddedConCode);
     }
-    return showErrorAndReturn(locale);
+    return `${locale} was not found.`;
 }
 
 export {
