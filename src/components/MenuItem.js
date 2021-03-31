2f4872abf832a32a3971a4d871175b6e857de485
Luke Donahue <luke@expensify.com>
2021-03-31 14:07:39 -0700

    changed wrapperStyle type to object

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    View, Text, Pressable, StyleSheet,
+    View, Text, Pressable,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -12,7 +12,7 @@ import {ArrowRight} from './Icon/Expensicons';
 const propTypes = {
     // Any additional styles to apply
     // eslint-disable-next-line react/forbid-prop-types
-    wrapperStyle: PropTypes.any,
+    wrapperStyle: PropTypes.object,
 
     // Function to fire when component is pressed
     onPress: PropTypes.func.isRequired,
@@ -29,7 +29,7 @@ const propTypes = {
 
 const defaultProps = {
     shouldShowRightArrow: false,
-    wrapperStyle: null,
+    wrapperStyle: {},
 };
 
 const MenuItem = ({
@@ -38,37 +38,33 @@ const MenuItem = ({
     title,
     shouldShowRightArrow,
     wrapperStyle,
-}) => {
-    // Flatten the wrapperStyle in case it's an array of styles
-    const flatWrapperStyle = StyleSheet.flatten(wrapperStyle);
-
-    return (
-        <Pressable
-            onPress={onPress}
-            style={({hovered}) => ([
-                styles.createMenuItem,
-                hovered && {backgroundColor: themeColors.buttonHoveredBG},
-                flatWrapperStyle,
-            ])}
-        >
-            <View style={styles.flexRow}>
-                <View style={styles.createMenuIcon}>
-                    <Icon src={icon} />
-                </View>
-                <View style={styles.justifyContentCenter}>
-                    <Text style={[styles.createMenuText, styles.ml3]}>
-                        {title}
-                    </Text>
-                </View>
+}) => (
+    <Pressable
+        onPress={onPress}
+        style={({hovered}) => ([
+            styles.createMenuItem,
+            hovered && {backgroundColor: themeColors.buttonHoveredBG},
+            wrapperStyle,
+        ])}
+    >
+        <View style={styles.flexRow}>
+            <View style={styles.createMenuIcon}>
+                <Icon src={icon} />
             </View>
-            {shouldShowRightArrow && (
-                <View style={styles.createMenuIcon}>
-                    <Icon src={ArrowRight} />
-                </View>
-            )}
-        </Pressable>
-    );
-};
+            <View style={styles.justifyContentCenter}>
+                <Text style={[styles.createMenuText, styles.ml3]}>
+                    {title}
+                </Text>
+            </View>
+        </View>
+        {shouldShowRightArrow && (
+            <View style={styles.createMenuIcon}>
+                <Icon src={ArrowRight} />
+            </View>
+        )}
+    </Pressable>
+);
+
 
 MenuItem.propTypes = propTypes;
 MenuItem.defaultProps = defaultProps;
