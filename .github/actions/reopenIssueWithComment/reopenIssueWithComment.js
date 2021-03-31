28b41594d4712c1fb4eca1a7653b0128778d7259
Scott Deeter <scott@expensify.com>
2021-03-30 18:13:16 -0700

    Modularize how GithubUtils gets used

--- .github/actions/reopenIssueWithComment/reopenIssueWithComment.js
+++ .github/actions/reopenIssueWithComment/reopenIssueWithComment.js
@@ -1,20 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const GithubUtils = require('../../libs/GithubUtils');
+const {GITHUB_OWNER, EXPENSIFY_CASH_REPO} = require('../../libs/GithubUtils');
 
 const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN', {required: true}));
 const issueNumber = core.getInput('ISSUE_NUMBER', {required: true});
 
 octokit.rest.issues.update({
-    owner: GithubUtils.GITHUB_OWNER,
-    repo: GithubUtils.EXPENSIFY_CASH_REPO,
+    owner: GITHUB_OWNER,
+    repo: EXPENSIFY_CASH_REPO,
     issue_number: issueNumber,
     state: 'open',
 });
 
 octokit.rest.issues.createComment({
-    owner: GithubUtils.GITHUB_OWNER,
-    repo: GithubUtils.EXPENSIFY_CASH_REPO,
+    owner: GITHUB_OWNER,
+    repo: EXPENSIFY_CASH_REPO,
     issue_number: issueNumber,
     body: core.getInput('COMMENT', {required: true}),
 });
