338753630a20ed5e4f9f2504f3e2aaeee9b794fd
maftalion <aftalion.matt@gmail.com>
2021-03-31 01:57:23 -0700

    Add note to login fields

--- src/pages/settings/ProfilePage/LoginField.js
+++ src/pages/settings/ProfilePage/LoginField.js
@@ -26,46 +26,73 @@ const LoginField = ({
     label,
     login,
     type,
-}) => (
-    <View style={styles.mb6}>
-        <Text style={[styles.mb1, styles.formLabel]}>{label}</Text>
-        {!login.partnerUserID ? (
-            <Pressable
-                style={[styles.createMenuItem, styles.ph0]}
-                onPress={() => Navigation.navigate(type === 'phone'
-                    ? ROUTES.SETTINGS_ADD_PHONE
-                    : ROUTES.SETTINGS_ADD_EMAIL)}
-            >
-                <View style={styles.flexRow}>
-                    <View style={styles.createMenuIcon}>
-                        <Icon src={Plus} />
+}) => {
+    let note;
+    if (type === 'phone') {
+        // No phone number
+        if (!login.partnerUserID) {
+            note = 'Add your phone number to settle up via Venmo.';
+
+            // Has unvalidated phone number
+        } else if (!login.validatedDate) {
+            note = 'The number has not yet been validated. Click the button to resend the validation link via text.';
+
+            // Has verified phone number
+        } else {
+            note = 'Use your phone number to settle up via Venmo.';
+        }
+
+        // Has unvalidated email
+    } else if (login.partnerUserID && !login.validatedDate) {
+        note = 'The email has not yet been validated. Click the button to resend the validation link via text.';
+    }
+
+    return (
+        <View style={styles.mb6}>
+            <Text style={styles.formLabel}>{label}</Text>
+            {!login.partnerUserID ? (
+                <Pressable
+                    style={[styles.createMenuItem, styles.ph0]}
+                    onPress={() => Navigation.navigate(type === 'phone'
+                        ? ROUTES.SETTINGS_ADD_PHONE
+                        : ROUTES.SETTINGS_ADD_EMAIL)}
+                >
+                    <View style={styles.flexRow}>
+                        <View style={styles.createMenuIcon}>
+                            <Icon src={Plus} />
+                        </View>
+                        <View style={styles.justifyContentCenter}>
+                            <Text style={[styles.createMenuText, styles.ml3]}>
+                                {`Add ${label}`}
+                            </Text>
+                        </View>
                     </View>
-                    <View style={styles.justifyContentCenter}>
-                        <Text style={[styles.createMenuText, styles.ml3]}>
-                            {`Add ${label}`}
-                        </Text>
-                    </View>
+                </Pressable>
+            ) : (
+                <View style={[styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
+                    <Text style={[styles.textP]} numberOfLines={1}>
+                        {login.partnerUserID}
+                    </Text>
+                    {!login.validatedDate && (
+                        <Pressable
+                            style={styles.button}
+                            onPress={() => resendValidateCode(login.partnerUserID)}
+                        >
+                            <Text style={styles.createMenuText}>
+                                Resend
+                            </Text>
+                        </Pressable>
+                    )}
                 </View>
-            </Pressable>
-        ) : (
-            <View style={[styles.flexRow, styles.justifyContentBetween]}>
-                <Text style={[styles.textP]} numberOfLines={1}>
-                    {login.partnerUserID}
+            )}
+            {note && (
+                <Text style={[styles.textLabel, styles.colorMuted]}>
+                    {note}
                 </Text>
-                {!login.validatedDate && (
-                    <Pressable
-                        style={styles.button}
-                        onPress={() => resendValidateCode(login.partnerUserID)}
-                    >
-                        <Text style={styles.createMenuText}>
-                            Resend
-                        </Text>
-                    </Pressable>
-                )}
-            </View>
-        )}
-    </View>
-);
+            )}
+        </View>
+    );
+};
 
 LoginField.propTypes = propTypes;
 LoginField.displayName = 'LoginField';
