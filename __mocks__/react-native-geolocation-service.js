ecd3e4a7b97b7af67d846534782b71b2be89134c
rameshhpathak <rameshh.pathak2@gmail.com>
2021-04-01 12:30:23 +0545

    Add mockups for jest

---
+++ __mocks__/react-native-geolocation-service.js
@@ -0,0 +1,9 @@
+export default {
+    addListener: jest.fn(),
+    getCurrentPosition: jest.fn(),
+    removeListeners: jest.fn(),
+    requestAuthorization: jest.fn(),
+    setConfiguration: jest.fn(),
+    startObserving: jest.fn(),
+    stopObserving: jest.fn(),
+};
