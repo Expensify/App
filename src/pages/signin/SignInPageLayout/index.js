19684a4b8c8edd783e41c761c920457a63753528
Jasper Huang <jasper@expensify.com>
2021-04-01 10:00:46 +0800

    Fix indent

--- src/pages/signin/SignInPageLayout/index.js
+++ src/pages/signin/SignInPageLayout/index.js
@@ -10,10 +10,10 @@ const propTypes = {
 
 const SignInPageLayout = props => (
     !props.isSmallScreenWidth
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    ? <SignInPageLayoutWide {...props} />
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    : <SignInPageLayoutNarrow {...props} />
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        ? <SignInPageLayoutWide {...props} />
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        : <SignInPageLayoutNarrow {...props} />
 );
 
 SignInPageLayout.propTypes = propTypes;
