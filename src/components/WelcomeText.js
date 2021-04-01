1d3cd1769f44b89dd4b2833a2df43f72d8d4dff0
Jasper Huang <jasper@expensify.com>
2021-04-01 12:14:10 +0800

    Remove windowDimensions from WelcomeText

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {Text} from 'react-native';
+import PropTypes from 'prop-types';
 import styles from '../styles/styles';
+
+const propTypes = {
+    isSmallText: PropTypes.bool,
+};
+
+const defaultProps = {
+    isSmallText: false,
+};
 
 const WelcomeText = props => (
     <>
-        <Text style={[props.isSmallScreenWidth ? styles.textLabel : styles.textP, styles.textStrong, styles.mb1]}>
+        <Text style={[props.isSmallText ? styles.textLabel : styles.textP, styles.textStrong, styles.mb1]}>
             With Expensify.cash, chat and payments are the same thing.
         </Text>
-        <Text style={[props.isSmallScreenWidth ? styles.textLabel : styles.textP]}>
+        <Text style={[props.isSmallText ? styles.textLabel : styles.textP]}>
             Money talks. And now that chat and payments are in one place, it&apos;s also easy.
             {' '}
             Your payments get to you as fast as you can get your point across.
@@ -16,5 +25,7 @@ const WelcomeText = props => (
 );
 
 WelcomeText.displayName = 'WelcomeText';
+WelcomeText.propTypes = propTypes;
+WelcomeText.defaultProps = defaultProps;
 
 export default WelcomeText;
