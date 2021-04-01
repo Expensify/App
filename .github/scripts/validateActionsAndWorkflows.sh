c2895982ec799977a233f0cd84925e3246f46710
Rory Abraham <rory@expensify.com>
2021-04-01 00:04:17 -0700

    Improve comment and add more early exits

--- .github/scripts/validateActionsAndWorkflows.sh
+++ .github/scripts/validateActionsAndWorkflows.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-# 1) Lints the yaml style
-# 2) Validates the Github Actions and workflows using the json schemas provided by https://www.schemastore.org/json/
+# Validates the Github Actions and workflows using the json schemas provided by https://www.schemastore.org/json/
 
-# Track exit codes so we can run a full lint, report errors, and exit with the correct code
+# Track exit codes separately so we can run a full validation, report errors, and exit with the correct code
 declare EXIT_CODE=0
 
 # Download the up-to-date json schemas for github actions and workflows
 cd ./.github && mkdir ./tempSchemas || exit 1
-curl https://json.schemastore.org/github-action.json --output ./tempSchemas/github-action.json --silent
-curl https://json.schemastore.org/github-workflow.json --output ./tempSchemas/github-workflow.json --silent
+curl https://json.schemastore.org/github-action.json --output ./tempSchemas/github-action.json --silent || exit 1
+curl https://json.schemastore.org/github-workflow.json --output ./tempSchemas/github-workflow.json --silent || exit 1
 
 # Validate the actions and workflows using the JSON schemas and ajv https://github.com/ajv-validator/ajv-cli
 find ./actions/ -type f -name "*.yml" -print0 | xargs -0 -I file ajv -s ./tempSchemas/github-action.json -d file --strict=false || EXIT_CODE=1
