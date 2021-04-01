c6c68fa91e9ad3d07fc9e4ddefe0959f9c5cff20
Jasper Huang <jasper@expensify.com>
2021-04-01 13:15:56 +0800

    Change to use convention

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -4,19 +4,21 @@ import PropTypes from 'prop-types';
 import styles from '../styles/styles';
 
 const propTypes = {
-    isSmallText: PropTypes.bool,
+
+    // Fontsize
+    textSize: PropTypes.oneOf(['default', 'large']),
 };
 
 const defaultProps = {
-    isSmallText: false,
+    textSize: 'default',
 };
 
 const WelcomeText = props => (
     <>
-        <Text style={[props.isSmallText ? styles.textLabel : styles.textP, styles.textStrong, styles.mb1]}>
+        <Text style={[props.textSize === 'large' ? styles.textP : styles.textLabel, styles.textStrong, styles.mb1]}>
             With Expensify.cash, chat and payments are the same thing.
         </Text>
-        <Text style={[props.isSmallText ? styles.textLabel : styles.textP]}>
+        <Text style={[props.textSize === 'large' ? styles.textP : styles.textLabel]}>
             Money talks. And now that chat and payments are in one place, it&apos;s also easy.
             {' '}
             Your payments get to you as fast as you can get your point across.
