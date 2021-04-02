9f4c97b1529f0728fdf41d3e69427b830504edd9
Scott Deeter <scott@expensify.com>
2021-04-01 17:18:35 -0700

    Get rid of async/await in code checkDeployBlockers

--- .github/actions/checkDeployBlockers/checkDeployBlockers.js
+++ .github/actions/checkDeployBlockers/checkDeployBlockers.js
@@ -6,30 +6,26 @@ const token = core.getInput('GITHUB_TOKEN', {required: true});
 const issueNumber = core.getInput('ISSUE_NUMBER', {required: true});
 const octokit = github.getOctokit(token, {required: true});
 
-function getIssue() {
+function checkDeployBlockers() {
     return octokit.rest.issues.get({
         owner: GITHUB_OWNER,
         repo: EXPENSIFY_CASH_REPO,
         issue_number: issueNumber,
-    });
+    })
+        .then((issue) => {
+            const body = issue.data.body || '';
+            const pattern = /-\s\[\s]/g;
+            const matches = pattern.exec(body);
+            core.setOutput('HAS_DEPLOY_BLOCKERS', matches !== null);
+        });
 }
 
-async function checkUnfinishedTasks() {
-    const issue = await getIssue();
-    const body = issue.data.body || '';
-
-    const pattern = /-\s\[\s]/g;
-    const matches = pattern.exec(body);
-
-    console.log({
-        token,
-        issueNumber,
-        issue,
-        body,
-        matches,
+checkDeployBlockers()
+    .then(() => {
+        console.log('The checkDeployBlockers action ran successfully');
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error('Something went wrong. The checkDeployBlockers action did not run successfully', err);
+        core.setFailed(err);
     });
-
-    core.setOutput('HAS_DEPLOY_BLOCKERS', matches !== null);
-}
-
-checkUnfinishedTasks();
