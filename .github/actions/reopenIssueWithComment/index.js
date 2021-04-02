89709787abf819e961bb0202520d55598881e1bf
Scott Deeter <scott@expensify.com>
2021-04-01 17:21:20 -0700

    Update compiled index files

--- .github/actions/reopenIssueWithComment/index.js
+++ .github/actions/reopenIssueWithComment/index.js
@@ -15,19 +15,30 @@ const {GITHUB_OWNER, EXPENSIFY_CASH_REPO} = __nccwpck_require__(7999);
 const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN', {required: true}));
 const issueNumber = core.getInput('ISSUE_NUMBER', {required: true});
 
-octokit.rest.issues.update({
-    owner: GITHUB_OWNER,
-    repo: EXPENSIFY_CASH_REPO,
-    issue_number: issueNumber,
-    state: 'open',
-});
+function reopenIssueWithComment() {
+    return octokit.rest.issues.update({
+        owner: GITHUB_OWNER,
+        repo: EXPENSIFY_CASH_REPO,
+        issue_number: issueNumber,
+        state: 'open',
+    })
+        .then(() => octokit.rest.issues.createComment({
+            owner: GITHUB_OWNER,
+            repo: EXPENSIFY_CASH_REPO,
+            issue_number: issueNumber,
+            body: core.getInput('COMMENT', {required: true}),
+        }));
+}
 
-octokit.rest.issues.createComment({
-    owner: GITHUB_OWNER,
-    repo: EXPENSIFY_CASH_REPO,
-    issue_number: issueNumber,
-    body: core.getInput('COMMENT', {required: true}),
-});
+reopenIssueWithComment()
+    .then(() => {
+        console.log('Issue successfully reopened and commented.');
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error('Something went wrong. The issue was not successfully reopened', err);
+        core.setFailed(err);
+    });
 
 
 /***/ }),
