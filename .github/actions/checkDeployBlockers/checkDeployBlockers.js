0ec8bd93afe1b0ab75418e6cb75c18488f2e9a80
Scott Deeter <scott@expensify.com>
2021-04-05 12:15:57 -0700

    Encapsulate action in a run function

--- .github/actions/checkDeployBlockers/checkDeployBlockers.js
+++ .github/actions/checkDeployBlockers/checkDeployBlockers.js
@@ -2,11 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const {GITHUB_OWNER, EXPENSIFY_CASH_REPO} = require('../../libs/GithubUtils');
 
-const token = core.getInput('GITHUB_TOKEN', {required: true});
-const issueNumber = core.getInput('ISSUE_NUMBER', {required: true});
-const octokit = github.getOctokit(token, {required: true});
+const run = function () {
+    const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN', {required: true}));
+    const issueNumber = Number(core.getInput('ISSUE_NUMBER', {required: true}));
 
-function checkDeployBlockers() {
     return octokit.rest.issues.get({
         owner: GITHUB_OWNER,
         repo: EXPENSIFY_CASH_REPO,
@@ -17,15 +16,13 @@ function checkDeployBlockers() {
             const pattern = /-\s\[\s]/g;
             const matches = pattern.exec(body);
             core.setOutput('HAS_DEPLOY_BLOCKERS', matches !== null);
+
+            return issue.data;
         });
+};
+
+if (require.main === module) {
+    run();
 }
 
-checkDeployBlockers()
-    .then(() => {
-        console.log('The checkDeployBlockers action ran successfully');
-        process.exit(0);
-    })
-    .catch((err) => {
-        console.error('Something went wrong. The checkDeployBlockers action did not run successfully', err);
-        core.setFailed(err);
-    });
+module.exports = run;
