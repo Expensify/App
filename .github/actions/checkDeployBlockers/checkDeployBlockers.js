de64ad4537842e624fbcd0cc2e06fc587d615aa8
Scott Deeter <scott@expensify.com>
2021-04-05 16:12:17 -0700

    Check last comment for :shipit:

--- .github/actions/checkDeployBlockers/checkDeployBlockers.js
+++ .github/actions/checkDeployBlockers/checkDeployBlockers.js
@@ -5,6 +5,7 @@ const {GITHUB_OWNER, EXPENSIFY_CASH_REPO} = require('../../libs/GithubUtils');
 const run = function () {
     const octokit = github.getOctokit(core.getInput('GITHUB_TOKEN', {required: true}));
     const issueNumber = Number(core.getInput('ISSUE_NUMBER', {required: true}));
+    let hasDeployBlockers = false;
 
     return octokit.rest.issues.get({
         owner: GITHUB_OWNER,
@@ -15,9 +16,26 @@ const run = function () {
             const body = issue.data.body || '';
             const pattern = /-\s\[\s]/g;
             const matches = pattern.exec(body);
-            core.setOutput('HAS_DEPLOY_BLOCKERS', matches !== null);
+            hasDeployBlockers = matches !== null;
 
             return issue.data;
+        })
+        .then(() => {
+            if (!hasDeployBlockers) {
+                return octokit.rest.issues.listComments({
+                    owner: GITHUB_OWNER,
+                    repo: EXPENSIFY_CASH_REPO,
+                    issue_number: issueNumber,
+                })
+                    .then((comments) => {
+                        const lastComment = comments.data[comments.data.length - 1];
+                        const shipItRegex = /:shipit:/g;
+                        hasDeployBlockers = shipItRegex.exec(lastComment.body) === null;
+                    });
+            }
+        })
+        .then(() => {
+            core.setOutput('HAS_DEPLOY_BLOCKERS', hasDeployBlockers);
         });
 };
 
