8cd0909a8abb3b3db1bc0c13a48570a36d05e866
Peter Velkov <kidroca@gmail.com>
2021-04-07 15:07:53 +0300

    refactor: extract FullscreenLoading styles

--- src/components/Loading/FullscreenLoading.js
+++ src/components/Loading/FullscreenLoading.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import LoadingIndicator from './LoadingIndicator';
+import styles from '../../styles/styles';
 
 const FullScreenLoadingIndicator = () => (
-    <View style={styles.container}>
+    <View style={[StyleSheet.absoluteFillObject, styles.fullScreenLoading]}>
         <LoadingIndicator size={88} />
     </View>
 );
-
-const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#eeeeeecc',
-        zIndex: 10,
-    },
-});
 
 export default FullScreenLoadingIndicator;
