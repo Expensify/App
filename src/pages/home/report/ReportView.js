8184e0f4792535a0e76d11c5c3efa4306d20f383
Peter Velkov <kidroca@gmail.com>
2021-04-07 15:19:56 +0300

    Drop: custom LoadingIndicator and use the built in ActivityIndicator

--- src/pages/home/report/ReportView.js
+++ src/pages/home/report/ReportView.js
@@ -8,7 +8,7 @@ import compose from '../../../libs/compose';
 import KeyboardSpacer from '../../../components/KeyboardSpacer';
 import styles from '../../../styles/styles';
 import SwipeableView from '../../../components/SwipeableView';
-import FullScreenLoadingIndicator from '../../../components/Loading/FullscreenLoading';
+import FullScreenLoadingIndicator from '../../../components/FullscreenLoading';
 import withDrawerState from '../../../components/withDrawerState';
 import withWindowDimensions from '../../../components/withWindowDimensions';
 
