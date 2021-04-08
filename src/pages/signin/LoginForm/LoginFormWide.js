48c162e5a1430454728faba1b313fb72619d3261
Nikki Wines <nikkiwines@expensify.com>
2021-04-08 15:02:47 -0700

    don't try to log the user in after validating secondary login

--- src/pages/signin/LoginForm/LoginFormWide.js
+++ src/pages/signin/LoginForm/LoginFormWide.js
@@ -17,6 +17,9 @@ const propTypes = {
         // An error message to display to the user
         error: PropTypes.string,
 
+        // Success message to display when necessary
+        success: PropTypes.string,
+
         // Whether or not a sign on form is loading (being submitted)
         loading: PropTypes.bool,
     }),
@@ -90,6 +93,11 @@ class LoginFormWide extends React.Component {
                     {!_.isEmpty(this.props.account.error) && (
                         <Text style={[styles.formError]}>
                             {this.props.account.error}
+                        </Text>
+                    )}
+                    {!_.isEmpty(this.props.account.success) && (
+                        <Text style={[styles.formSuccess]}>
+                            {this.props.account.success}
                         </Text>
                     )}
                 </View>
