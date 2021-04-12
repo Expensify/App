de0bcb100c3e8535f7f6302586703f9145aacbdd
rameshhpathak <rameshh.pathak2@gmail.com>
2021-04-12 12:47:24 +0545

    Fix android importerror

--- android/app/src/main/java/com/expensify/chat/MainApplication.java
+++ android/app/src/main/java/com/expensify/chat/MainApplication.java
@@ -5,7 +5,6 @@ import android.database.CursorWindow;
 import androidx.multidex.MultiDexApplication;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
