21a6f448d3fcfa64e1161b33e6dea7e2740b4338
Peter Velkov <kidroca@gmail.com>
2021-04-12 17:17:47 +0300

    refactor: NavigationRoot remove Linking handling
    With the current configuration it should be possible for the
    navigation to resolve initial state entirely through `linkingConfig`

--- src/libs/Navigation/NavigationRoot.js
+++ src/libs/Navigation/NavigationRoot.js
@@ -1,18 +1,10 @@
 import _ from 'underscore';
 import React, {Component} from 'react';
-import {Linking} from 'react-native';
 import PropTypes from 'prop-types';
-import {
-    getStateFromPath,
-    getPathFromState,
-    NavigationContainer,
-} from '@react-navigation/native';
-import {withOnyx} from 'react-native-onyx';
+import {getPathFromState, NavigationContainer} from '@react-navigation/native';
 import {navigationRef} from './Navigation';
 import linkingConfig from './linkingConfig';
 import AppNavigator from './AppNavigator';
-import getPathName from './getPathName';
-import ONYXKEYS from '../../ONYXKEYS';
 import ROUTES from '../../ROUTES';
 import {updateCurrentlyViewedReportID} from '../actions/Report';
 import {setCurrentURL} from '../actions/App';
@@ -21,66 +13,13 @@ import FullScreenLoadingIndicator from '../../components/FullscreenLoadingIndica
 const propTypes = {
     // Whether the current user is logged in with an authToken
     authenticated: PropTypes.bool.isRequired,
-
-    // The current reportID that we are navigated to or should show in the ReportScreen
-    currentlyViewedReportID: PropTypes.string,
-};
-
-const defaultProps = {
-    currentlyViewedReportID: null,
 };
 
 class NavigationRoot extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            loading: true,
-            initialState: undefined,
-        };
-
         this.handleStateChange = this.handleStateChange.bind(this);
-    }
-
-    componentDidMount() {
-        Linking.getInitialURL()
-            .then((initialUrl) => {
-                // On web we should be able to parse this. It will be null on native for now until deep links are
-                // hooked up
-                const path = getPathName(initialUrl);
-                let initialState = getStateFromPath(path, linkingConfig.config);
-                setCurrentURL(path);
-
-                // If we are landing on something other than the report screen or site root then we MUST set the
-                // initial route to the currently viewed report so there some history to navigate back from
-                if (path !== `/${ROUTES.HOME}` && !path.includes(`/${ROUTES.REPORT}`)) {
-                    const homeRoute = {
-                        name: 'Home',
-                    };
-
-                    if (this.props.currentlyViewedReportID) {
-                        homeRoute.params = {
-                            screen: 'Report',
-                            params: {
-                                reportID: this.props.currentlyViewedReportID,
-                            },
-                        };
-                    }
-
-                    if (!initialState) {
-                        initialState = {
-                            routes: [],
-                        };
-                    }
-
-                    initialState.routes = [
-                        homeRoute,
-                        ...initialState.routes,
-                    ];
-                }
-
-                this.setState({loading: false, initialState});
-            });
     }
 
     /**
@@ -103,23 +42,10 @@ class NavigationRoot extends Component {
         setCurrentURL(path);
     }
 
-    /**
-     * Render some fallback content until the navigation is ready
-     * @returns {JSX.Element}
-     */
-    renderFallback() {
-        return <FullScreenLoadingIndicator visible />;
-    }
-
     render() {
-        if (this.state.loading) {
-            return this.renderFallback();
-        }
-
-        // If we are on web, desktop, or a widescreen width we will use our custom navigator to create the wider layout
         return (
             <NavigationContainer
-                initialState={this.state.initialState}
+                fallback={<FullScreenLoadingIndicator visible />}
                 onStateChange={this.handleStateChange}
                 ref={navigationRef}
                 linking={linkingConfig}
@@ -134,9 +60,4 @@ class NavigationRoot extends Component {
 }
 
 NavigationRoot.propTypes = propTypes;
-NavigationRoot.defaultProps = defaultProps;
-export default withOnyx({
-    currentlyViewedReportID: {
-        key: ONYXKEYS.CURRENTLY_VIEWED_REPORTID,
-    },
-})(NavigationRoot);
+export default NavigationRoot;
