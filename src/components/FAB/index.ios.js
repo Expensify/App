1bd334b600b529e114be944ed6fcfaa5a0217784
Ali Abbas Malik <aliabbasmalik8@gmail.com>
2021-04-13 20:39:07 -0400

    [IS-2151] make FAB main component simple

--- src/components/FAB/index.ios.js
+++ src/components/FAB/index.ios.js
@@ -6,6 +6,7 @@ import {
 import FAB from './FAB';
 import fabPropTypes from './fabPropTypes';
 
+// KeyboardAvoidingView only need in IOS so that's the reason make platform specific FAB component.
 function Fab({onPress, isActive}) {
     return (
         <KeyboardAvoidingView behavior="position">
--- src/components/FAB/index.js
+++ src/components/FAB/index.js
@@ -1,11 +1,3 @@
-import React from 'react';
-
 import FAB from './FAB';
-import fabPropTypes from './fabPropTypes';
 
-function Fab({onPress, isActive}) {
-    return <FAB onPress={onPress} isActive={isActive} />;
-}
-
-Fab.propTypes = fabPropTypes;
-export default Fab;
+export default FAB;
