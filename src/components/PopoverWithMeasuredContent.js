f580b03c18ee5d5b2542f57c7e01139fc9220758
Brandon Stites <bstites+1@expensify.com>
2021-04-14 18:46:27 -0600

    More scuffed merging

--- src/components/PopoverWithMeasuredContent.js
+++ src/components/PopoverWithMeasuredContent.js
@@ -126,7 +126,7 @@ class PopoverWithMeasuredContent extends Component {
                     {...this.props}
                     anchorPosition={this.calculateAdjustedAnchorPosition()}
                 >
-                    {this.props.children}
+                    {this.props.measureContent()}
                 </Popover>
             ) : (
 
