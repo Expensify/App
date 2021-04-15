cf067dfebea669c2de7ab5c48b787d02d2c8920f
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-04-14 14:17:39 -1000

    fix typo

--- src/libs/Navigation/canPushReportScreen/index.website.js
+++ src/libs/Navigation/canPushReportScreen/index.website.js
@@ -2,6 +2,6 @@ import {Dimensions} from 'react-native';
 import variables from '../../../styles/variables';
 
 // On mobile web the default "back behavior" is going to be opening the drawer again so we'll change this behavior
-// depending on what size scree we are dealing with
+// depending on what size screen we are dealing with
 const screenWidth = Dimensions.get('window').width;
 export default () => screenWidth > variables.mobileResponsiveWidthBreakpoint;
