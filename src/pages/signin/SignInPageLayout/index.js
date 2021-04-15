e6017836013e54bb0845b40b5f1547d5532d0561
Jasper Huang <jasper@expensify.com>
2021-04-15 15:39:52 +0800

    Remove autofocus, add back children prop

--- src/pages/signin/SignInPageLayout/index.js
+++ src/pages/signin/SignInPageLayout/index.js
@@ -10,9 +10,9 @@ const propTypes = {
 const SignInPageLayout = props => (
     !props.isSmallScreenWidth
         // eslint-disable-next-line react/jsx-props-no-spreading
-        ? <SignInPageLayoutWide />
+        ? <SignInPageLayoutWide>{props.children}</SignInPageLayoutWide>
         // eslint-disable-next-line react/jsx-props-no-spreading
-        : <SignInPageLayoutNarrow />
+        : <SignInPageLayoutNarrow>{props.children}</SignInPageLayoutNarrow>
 );
 
 SignInPageLayout.propTypes = propTypes;
