a4a0bd236a4b8d3ea35d20664ee248b6a72c3f83
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-04-20 07:53:14 -1000

    use set() update key name and add comments

--- src/ONYXKEYS.js
+++ src/ONYXKEYS.js
@@ -9,8 +9,9 @@ export default {
     // which tab is the leader, and which ones are the followers
     ACTIVE_CLIENTS: 'activeClients',
 
-    // A key that is set when we are still waiting for the initial round of reports to load
-    APP_DATA_LOADED: 'appDataLoaded',
+    // A key that is set while we are still waiting for the initial round of reports to load. Once set it should not be
+    // false unless we sign out. If there are reports in storage when the app inits this will be `true`.
+    INITIAL_REPORT_DATA_LOADED: 'initialReportDataLoaded',
 
     NETWORK_REQUEST_QUEUE: 'networkRequestQueue',
 
