c6790397bedd148d8d48109bd869a3d6048fa57c
Joe Gambino <joe@expensify.com>
2021-04-20 16:32:21 -0700

    Add environment variable and set logo dynamically

--- src/components/ExpensifyCashLogo.js
+++ src/components/ExpensifyCashLogo.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ProductionLogo from '../../assets/images/expensify-cash-dev.svg';
+import ProductionLogo from '../../assets/images/expensify-cash.svg';
+import DevLogo from '../../assets/images/expensify-cash-dev.svg';
+import StagingLogo from '../../assets/images/expensify-cash-stg.svg';
+import CONFIG from '../CONFIG';
+import CONST from '../CONST';
 
 const propTypes = {
     width: PropTypes.number.isRequired,
     height: PropTypes.number.isRequired,
 };
 
-const ExpensifyCashLogo = props => (
-    <ProductionLogo width={props.width} height={props.height} />
-);
+export const ExpensifyCashLogo = (props) => {
+    if (CONFIG.EXPENSIFY.ENVIRONMENT === CONST.ENVIRONMENT.PRODUCTION) {
+        return <ProductionLogo width={props.width} height={props.height} />;
+    }
+
+    if (CONFIG.EXPENSIFY.ENVIRONMENT === CONST.ENVIRONMENT.STAGING) {
+        return <StagingLogo width={props.width} height={props.height} />;
+    }
+
+    return <DevLogo width={props.width} height={props.height} />;
+};
 
 ExpensifyCashLogo.propTypes = propTypes;
 ExpensifyCashLogo.displayName = 'ExpensifyCashLogo';
