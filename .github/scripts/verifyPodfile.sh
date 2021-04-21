59bbfc46d84fff519516e5ddbaf4110b93985df0
andrew <andrew@expensify.com>
2021-04-21 10:12:20 -0600

    Fix pod install command

--- .github/scripts/verifyPodfile.sh
+++ .github/scripts/verifyPodfile.sh
@@ -4,20 +4,23 @@ declare -r GREEN='\033[0;32m'
 declare -r RED='\033[0;31m'
 declare -r NC='\033[0m'
 
+declare LIB_PATH
+LIB_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../ && pwd)/node_modules/diff-so-fancy"
+
 podfileSha=$(openssl sha1 ios/Podfile | awk '{print $2}')
 podfileLockSha=$(awk '/PODFILE CHECKSUM: /{print $3}' ios/Podfile.lock)
 
 echo "Podfile: $podfileSha"
 echo "Podfile.lock: $podfileLockSha"
 
-if [ $podfileSha == $podfileLockSha ]; then
-    echo -e "${GREEN}Podfile verified!${NC}"
+if [ "$podfileSha" == "$podfileLockSha" ]; then
+    echo -e "${GREEN}Podfile sha is up to date!${NC}"
 else
     echo -e "${RED}Error: Podfile.lock out of date with Podfile. Did you forget to run \`npx pod-install\`?${NC}"
     exit 1
 fi
 
-pod install
+cd ios && pod install --repo-update
 
 DIFF_OUTPUT=$(git diff --exit-code)
 EXIT_CODE=$?
@@ -27,6 +30,6 @@ if [[ EXIT_CODE -eq 0 ]]; then
     exit 0
 else
     echo -e "${RED}Error: Diff found when pods were rebuilt. Did you forget to run \`pod install\`? Do you need to merge main?${NC}"
-    echo "$DIFF_OUTPUT" | $LIB_PATH/diff-so-fancy | less --tabs=4 -RFX
+    echo "$DIFF_OUTPUT" | "$LIB_PATH"/diff-so-fancy | less --tabs=4 -RFX
     exit 1
 fi
