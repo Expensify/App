b5cc52cfbfd5ab81d438fbf96d3223f9a63c60ea
Alex Beaman <alexbeaman@expensify.com>
2021-04-21 17:36:26 +0200

    Add togglePin test + refactor a bit

--- tests/actions/ReportTest.js
+++ tests/actions/ReportTest.js
@@ -4,7 +4,9 @@ import ONYXKEYS from '../../src/ONYXKEYS';
 import * as Pusher from '../../src/libs/Pusher/pusher';
 import PusherConnectionManager from '../../src/libs/PusherConnectionManager';
 import CONFIG from '../../src/CONFIG';
-import {addAction, subscribeToReportCommentAndTogglePinnedEvents} from '../../src/libs/actions/Report';
+import {
+    addAction, togglePinnedState, subscribeToReportCommentAndTogglePinnedEvents
+} from '../../src/libs/actions/Report';
 import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
 import PushNotification from '../../src/libs/Notification/PushNotification';
 import {signInWithTestUser, fetchPersonalDetailsForTestUser} from '../utils/TestHelper';
@@ -13,7 +15,30 @@ PushNotification.register = () => {};
 PushNotification.deregister = () => {};
 
 describe('actions/Report', () => {
-    it('should store a new report action in Onyx when one is handled via Pusher', () => {
+    beforeAll(() => {
+        // When using the Pusher mock the act of calling Pusher.isSubscribed will create a
+        // channel already in a subscribed state. These methods are normally used to prevent
+        // duplicated subscriptions, but we don't need them for this test so forcing them to
+        // return false will make the testing less complex.
+        Pusher.isSubscribed = jest.fn().mockReturnValue(false);
+        Pusher.isAlreadySubscribing = jest.fn().mockReturnValue(false);
+
+        // Connect to Pusher
+        PusherConnectionManager.init();
+        Pusher.init({
+            appKey: CONFIG.PUSHER.APP_KEY,
+            cluster: CONFIG.PUSHER.CLUSTER,
+            authEndpoint: `${CONFIG.EXPENSIFY.URL_API_ROOT}api?command=Push_Authenticate`,
+        });
+    });
+
+    afterEach(() => {
+        // Unsubscribe from account channel after each test since we subscribe in the function
+        // subscribeToReportCommentAndTogglePinnedEvents and we don't want duplicate event subscriptions.
+        Pusher.unsubscribe('private-user-accountID-1');
+    });
+
+    it('should store a new report action in Onyx when reportComment event is handled via Pusher', () => {
         const TEST_USER_ACCOUNT_ID = 1;
         const TEST_USER_LOGIN = 'test@test.com';
         const REPORT_ID = 1;
@@ -29,21 +54,6 @@ describe('actions/Report', () => {
             sequenceNumber: ACTION_ID,
             shouldShow: true,
         };
-
-        // When using the Pusher mock the act of calling Pusher.isSubscribed will create a
-        // channel already in a subscribed state. These methods are normally used to prevent
-        // duplicated subscriptions, but we don't need them for this test so forcing them to
-        // return false will make the testing less complex.
-        Pusher.isSubscribed = jest.fn().mockReturnValue(false);
-        Pusher.isAlreadySubscribing = jest.fn().mockReturnValue(false);
-
-        // Connect to Pusher
-        PusherConnectionManager.init();
-        Pusher.init({
-            appKey: CONFIG.PUSHER.APP_KEY,
-            cluster: CONFIG.PUSHER.CLUSTER,
-            authEndpoint: `${CONFIG.EXPENSIFY.URL_API_ROOT}api?command=Push_Authenticate`,
-        });
 
         let reportActions;
         Onyx.connect({
@@ -108,5 +118,52 @@ describe('actions/Report', () => {
             });
     });
 
-    // TODO: it('should update pins when togglePinned event is handled via Pusher');
+    it('should update pins in Onyx when togglePinned event is handled via Pusher', () => {
+        const TEST_USER_ACCOUNT_ID = 1;
+        const TEST_USER_LOGIN = 'test@test.com';
+        const REPORT_ID = 1;
+        const REPORT = {
+            reportID: REPORT_ID,
+            isPinned: false,
+        };
+
+        let reportIsPinned;
+        Onyx.connect({
+            key: `${ONYXKEYS.COLLECTION.REPORT}${REPORT_ID}`,
+            callback: val => reportIsPinned = val.isPinned
+        });
+
+        // Set up Onyx with some test user data
+        return signInWithTestUser(TEST_USER_ACCOUNT_ID, TEST_USER_LOGIN)
+            .then(() => {
+                subscribeToReportCommentAndTogglePinnedEvents();
+                return waitForPromisesToResolve();
+            })
+            .then(() => {
+                togglePinnedState(REPORT);
+                return waitForPromisesToResolve();
+            })
+            .then(() => {
+                // Before pusher event gets sent back, test that Onyx immediately updated the report pin state.
+                expect(reportIsPinned).toEqual(true);
+            })
+            .then(() => {
+                // We subscribed to the Pusher channel above and now we need to simulate a reportTogglePinned
+                // Pusher event so we can verify that pinning was handled correctly and merged into the report.
+                const channel = Pusher.getChannel('private-user-accountID-1');
+                channel.emit(Pusher.TYPE.REPORT_TOGGLE_PINNED, {
+                    reportID: REPORT_ID,
+                    isPinned: true,
+                });
+
+                // Once an event is emitted to the Pusher channel we should see the report pin get updated
+                // by the Pusher callback and added to the storage so we must wait for promises to resolve again and
+                // then verify the data is in Onyx.
+                return waitForPromisesToResolve();
+            })
+            .then(() => {
+                // Make sure the pin state gets from the Pusher callback into Onyx.
+                expect(reportIsPinned).toEqual(true);
+            });
+    });
 });
