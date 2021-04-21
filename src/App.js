737857270ec15d8ffb414c40f7a18a2444783ac6
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-04-21 06:53:05 -1000

    ignore logbox error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {LogBox} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import CustomStatusBar from './components/CustomStatusBar';
 import Expensify from './Expensify';
+
+LogBox.ignoreLogs([
+    'Setting a timer',
+]);
 
 const App = () => (
     <SafeAreaProvider>
