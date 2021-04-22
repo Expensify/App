d2ba95f98cfc99689b4f829722f3c8e83d05668a
Jasper Huang <jasper@expensify.com>
2021-04-22 12:31:21 +0800

    Revert unneeded change

--- src/pages/signin/SignInPageLayout/SignInPageLayoutWide.js
+++ src/pages/signin/SignInPageLayout/SignInPageLayoutWide.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
-    Image,
-    Text,
-    View,
+    Image, Text, View,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../../styles/styles';
