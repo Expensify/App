fef2e146aafcb8d85f86802d5847f59dc346b7bb
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-04-22 06:45:25 +0530

    fix: IOS Inline code block

---
+++ src/styles/codeStyles/index.android.js
@@ -0,0 +1,23 @@
+const codeWordWrapper = {
+    height: 10,
+};
+const codeWordStyle = {
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    flexBasis: 'auto',
+    paddingLeft: 0,
+    paddingRight: 0,
+    justifyContent: 'center',
+    marginVertical: -2,
+    top: -1,
+};
+
+const codeBlock = {
+    lineHeight: 18,
+};
+
+export default {codeWordWrapper, codeWordStyle, codeBlock};
