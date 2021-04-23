1415c5b4386eb9c2eb3c11bfdcf789b6090155dc
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-04-23 12:06:54 +0530

    fix: inline code block in IOS and refactor code

--- src/styles/codeStyles/index.ios.js
+++ src/styles/codeStyles/index.ios.js
@@ -1,25 +1,16 @@
 const codeWordWrapper = {
-    height: 20,
+    height: 18,
     justifyContent: 'center',
-    marginTop: 4,
+    marginTop: 2,
 };
+
 const codeWordStyle = {
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopLeftRadius: 0,
-    borderBottomLeftRadius: 0,
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    justifyContent: 'center',
-    marginVertical: -2,
-    height: 20,
+    height: 16,
     top: 4,
 };
 
-const codeBlock = {
-    lineHeight: 13,
+const codeTextStyle = {
+    lineHeight: 15,
 };
 
-export default {codeWordWrapper, codeWordStyle, codeBlock};
+export default {codeWordWrapper, codeWordStyle, codeTextStyle};
--- src/styles/codeStyles/index.js
+++ src/styles/codeStyles/index.js
@@ -1,4 +1,5 @@
+// We do not need these on Web/Desktop as their implementation defer from Native devices so just noop them
 const codeWordWrapper = {};
 const codeWordStyle = {};
-const codeBlock = {};
-export default {codeWordWrapper, codeWordStyle, codeBlock};
+const codeTextStyle = {};
+export default {codeWordWrapper, codeWordStyle, codeTextStyle};
