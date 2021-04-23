e6450ff2015b888cd47da192b411f2de3866706d
Jasper Huang <jasper@expensify.com>
2021-04-23 13:52:11 +0800

    Change to use authToken

--- src/libs/Navigation/Navigation.js
+++ src/libs/Navigation/Navigation.js
@@ -13,7 +13,7 @@ import ONYXKEYS from '../../ONYXKEYS';
 let isLoggedIn = false;
 Onyx.connect({
     key: ONYXKEYS.SESSION,
-    callback: val => isLoggedIn = !_.isEmpty(val),
+    callback: val => isLoggedIn = !!val.authToken,
 });
 
 export const navigationRef = React.createRef();
