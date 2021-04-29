7976ec82bb013e674861bc86232f5887b7ef11cd
Joe Gambino <joe@expensify.com>
2021-04-29 16:23:43 -0700

    add environment checker on ios

---
+++ ios/EnvironmentChecker.h
@@ -0,0 +1,12 @@
+//
+//  EnvironmentChecker.h
+//  ExpensifyCash
+//
+//  Created by Joe Gambino on 4/29/21.
+//
+
+#ifndef EnvironmentChecker_h
+#define EnvironmentChecker_h
+
+
+#endif /* EnvironmentChecker_h */
