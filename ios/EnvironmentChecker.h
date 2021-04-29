18dd2bd19cb09f757c32d2f8691f062c8095c6bf
Joe Gambino <joe@expensify.com>
2021-04-29 16:44:09 -0700

    add native module

--- ios/EnvironmentChecker.h
+++ ios/EnvironmentChecker.h
@@ -2,11 +2,14 @@
 //  EnvironmentChecker.h
 //  ExpensifyCash
 //
-//  Created by Joe Gambino on 4/29/21.
-//
 
 #ifndef EnvironmentChecker_h
 #define EnvironmentChecker_h
+
+#import <React/RCTBridgeModule.h>
+
+@interface EnvironmentChecker : NSObject <RCTBridgeModule>
+@end
 
 
 #endif /* EnvironmentChecker_h */
--- ios/EnvironmentChecker.m
+++ ios/EnvironmentChecker.m
@@ -2,7 +2,24 @@
 //  EnvironmentChecker.m
 //  ExpensifyCash
 //
-//  Created by Joe Gambino on 4/29/21.
-//
+// Source:
+// https://github.com/react-native-device-info/react-native-device-info/issues/228#issuecomment-420703355
 
-#import <Foundation/Foundation.h>
+#import "EnvironmentChecker.h"
+
+@implementation EnvironmentChecker
+
+RCT_EXPORT_MODULE();
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getEnvironment)
+{
+  NSURL *receiptURL = [[NSBundle mainBundle] appStoreReceiptURL];
+  NSString *receiptURLString = [receiptURL path];
+  BOOL isRunningTestFlightBeta =  ([receiptURLString rangeOfString:@"sandboxReceipt"].location != NSNotFound);
+  if(isRunningTestFlightBeta) {
+    return @"TESTFLIGHT";
+  }
+
+  return @"PRODUCTION";
+}
+
+@end
