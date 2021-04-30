364698fcf84bbe2df6f9ad47020cc2967d2b9db6
rameshhpathak <rameshh.pathak2@gmail.com>
2021-05-01 00:36:35 +0545

    Revert sidescreen route typo

--- src/pages/home/sidebar/SidebarScreen.js
+++ src/pages/home/sidebar/SidebarScreen.js
@@ -114,7 +114,7 @@ class SidebarScreen extends Component {
                                 {
                                     icon: Users,
                                     text: 'New Group',
-                                    onSelected: () => Navigation.navigate(ROUTES.IOU_BILL),
+                                    onSelected: () => Navigation.navigate(ROUTES.NEW_GROUP),
                                 },
                                 ...(Permissions.canUseIOU() ? [
                                     {
