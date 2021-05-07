e76444c0059d66fb3151a8c7d49fccb9bb56cafd
Joe Gambino <joe@expensify.com>
2021-05-07 16:44:14 -0700

    add poc testing for android state update

---
+++ src/components/ExpensifyCashLogo/index.android.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProductionLogo from '../../../assets/images/expensify-cash.svg';
+import DevLogo from '../../../assets/images/expensify-cash-dev.svg';
+import StagingLogo from '../../../assets/images/expensify-cash-stg.svg';
+import CONST from '../../CONST';
+import Environment from '../../libs/Environment/index';
+
+const propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+};
+
+class ExpensifyCashLogo extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.updateBadgeState = this.updateBadgeState.bind(this);
+
+        this.state = {
+            environment: Environment.getEnvironment(this.updateBadgeState),
+        };
+    }
+
+    updateBadgeState(environment) {
+        this.setState({
+            environment,
+        });
+    }
+
+    render() {
+        switch (this.state.environment) {
+            case CONST.ENVIRONMENT.PRODUCTION:
+                return <ProductionLogo width={this.props.width} height={this.props.height} />;
+            case CONST.ENVIRONMENT.STAGING:
+                return <StagingLogo width={this.props.width} height={this.props.height} />;
+            default:
+                return <DevLogo width={this.props.width} height={this.props.height} />;
+        }
+    }
+}
+
+ExpensifyCashLogo.propTypes = propTypes;
+ExpensifyCashLogo.displayName = 'ExpensifyCashLogo';
+
+export default ExpensifyCashLogo;
--- src/libs/Environment/index.android.js
+++ src/libs/Environment/index.android.js
@@ -4,8 +4,14 @@ import CONST from '../../CONST';
 import {version} from '../../../package.json';
 
 let environment = CONST.ENVIRONMENT.PRODUCTION;
+let isLoading = false;
+let callback = null;
 
-function getEnvironment() {
+function getEnvironment(updateEnvCallback) {
+    if (isLoading) {
+        callback = updateEnvCallback;
+    }
+
     return environment;
 }
 
@@ -18,29 +24,43 @@ function setEnvironment() {
     // Since we promote staging builds to production without creating a new build, check the google play listing to
     // see if the current version is greater than the production one
     try {
+        isLoading = true;
         fetch(CONST.PLAY_STORE_URL)
             .then(res => res.text())
             .then((text) => {
-                const match = text.match(/<span[^>]+class="htlgb"[^>]*>([-\d.]+)<\/span>/);
-                if (!match) {
-                    environment = CONST.ENVIRONMENT.PRODUCTION;
-                    return;
-                }
+                console.log('start sleep');
+                setTimeout(() => {
+                    console.log('done sleep');
+                    const match = text.match(/<span[^>]+class="htlgb"[^>]*>([-\d.]+)<\/span>/);
+                    if (!match) {
+                        environment = CONST.ENVIRONMENT.PRODUCTION;
+                        isLoading = false;
+                        return;
+                    }
 
-                const storeVersion = match[1].trim();
-                if (storeVersion === version) {
-                    environment = CONST.ENVIRONMENT.PRODUCTION;
-                    return;
-                }
+                    const storeVersion = match[1].trim();
+                    if (storeVersion === version) {
+                        environment = CONST.ENVIRONMENT.PRODUCTION;
+                        isLoading = false;
+                        return;
+                    }
 
-                // If the version isn't the same as the store version, and we aren't on dev, this is a beta build
-                environment = CONST.ENVIRONMENT.STAGING;
+                    // If the version isn't the same as the store version, and we aren't on dev, this is a beta build
+                    environment = CONST.ENVIRONMENT.STAGING;
+                    isLoading = false;
+
+                    if (callback) {
+                        callback(environment);
+                    }
+                }, 10000);
             })
             .catch(() => {
                 environment = CONST.ENVIRONMENT.PRODUCTION;
+                isLoading = false;
             });
     } catch (e) {
         environment = CONST.ENVIRONMENT.PRODUCTION;
+        isLoading = false;
     }
 }
 
