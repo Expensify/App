f4313eb0cbdbaebf7cbc5cc10b36f7d708a5ba0c
Joe Gambino <joe@expensify.com>
2021-05-07 16:44:14 -0700

    add poc testing for android state update

--- src/components/ExpensifyCashLogo/index.js
+++ src/components/ExpensifyCashLogo/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ProductionLogo from '../../assets/images/expensify-cash.svg';
-import DevLogo from '../../assets/images/expensify-cash-dev.svg';
-import StagingLogo from '../../assets/images/expensify-cash-stg.svg';
-import CONST from '../CONST';
-import Environment from '../libs/Environment';
+import ProductionLogo from '../../../assets/images/expensify-cash.svg';
+import DevLogo from '../../../assets/images/expensify-cash-dev.svg';
+import StagingLogo from '../../../assets/images/expensify-cash-stg.svg';
+import CONST from '../../CONST';
+import Environment from '../../libs/Environment';
 
 const propTypes = {
     width: PropTypes.number.isRequired,
@@ -26,5 +26,4 @@ const ExpensifyCashLogo = (props) => {
 
 ExpensifyCashLogo.propTypes = propTypes;
 ExpensifyCashLogo.displayName = 'ExpensifyCashLogo';
-
 export default ExpensifyCashLogo;
