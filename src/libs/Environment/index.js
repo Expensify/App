3eac39a12e3e32c29a3dd96288fd3e194884111a
Joe Gambino <joe@expensify.com>
2021-05-11 12:29:32 -0700

    add promise on web and remove platform-specific code

--- src/libs/Environment/index.js
+++ src/libs/Environment/index.js
@@ -3,7 +3,7 @@ import lodashGet from 'lodash/get';
 import CONST from '../../CONST';
 
 function getEnvironment() {
-    return lodashGet(Config, 'ENVIRONMENT', CONST.ENVIRONMENT.DEV);
+    return new Promise(resolve => resolve(lodashGet(Config, 'ENVIRONMENT', CONST.ENVIRONMENT.DEV)));
 }
 
 export default {
