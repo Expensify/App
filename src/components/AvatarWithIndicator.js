185d271eb22651c674f35b3214a5aaadd2012352
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-05-14 05:03:29 +0530

    Update src/components/AvatarWithIndicator.js
    Co-authored-by: Amal Nazeem <amalnazeem@gmail.com>

--- src/components/AvatarWithIndicator.js
+++ src/components/AvatarWithIndicator.js
@@ -19,7 +19,7 @@ const propTypes = {
     // avatar size
     size: PropTypes.string,
 
-    // When true, shows sync indicator
+    // Whether we show the sync indicator
     isSyncing: PropTypes.bool,
 };
 
