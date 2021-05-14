2ebbf5835b93e3fdddde680883f5eb494d61c2e1
Joe Gambino <joe@expensify.com>
2021-05-14 11:36:32 -0700

    remove unnecessary catch and bool

--- src/libs/Environment/index.android.js
+++ src/libs/Environment/index.android.js
@@ -3,8 +3,7 @@ import Config from 'react-native-config';
 import CONST from '../../CONST';
 import {version} from '../../../package.json';
 
-let environment = CONST.ENVIRONMENT.PRODUCTION;
-let hasSetEnvironment = false;
+let environment = null;
 
 /**
  * Returns a promise that resolves with the current environment string value
@@ -14,41 +13,34 @@ let hasSetEnvironment = false;
 function getEnvironment() {
     return new Promise((resolve) => {
         // If we've already set the environment, use the current value
-        if (hasSetEnvironment) {
+        if (environment) {
             return resolve(environment);
         }
 
         if (lodashGet(Config, 'ENVIRONMENT', CONST.ENVIRONMENT.DEV) === CONST.ENVIRONMENT.DEV) {
-            hasSetEnvironment = true;
             environment = CONST.ENVIRONMENT.DEV;
             return resolve(environment);
         }
 
         // If we haven't set the environment yet and we aren't on dev, check the play store listing to see if the
         // current version matches production
-        try {
-            fetch(CONST.PLAY_STORE_URL)
-                .then(res => res.text())
-                .then((text) => {
-                    hasSetEnvironment = true;
-                    const productionVersionMatch = text.match(/<span[^>]+class="htlgb"[^>]*>([-\d.]+)<\/span>/);
+        fetch(CONST.PLAY_STORE_URL)
+            .then(res => res.text())
+            .then((text) => {
+                const productionVersionMatch = text.match(/<span[^>]+class="htlgb"[^>]*>([-\d.]+)<\/span>/);
 
-                    // If we have a match for the production version regex and the current version is not the same
-                    // as the production version, we are on a beta build
-                    if (productionVersionMatch && productionVersionMatch[1].trim() !== version) {
-                        environment = CONST.ENVIRONMENT.STAGING;
-                    }
+                // If we have a match for the production version regex and the current version is not the same
+                // as the production version, we are on a beta build
+                environment = productionVersionMatch && productionVersionMatch[1].trim() !== version
+                    ? CONST.ENVIRONMENT.STAGING
+                    : CONST.ENVIRONMENT.PRODUCTION;
 
-                    resolve(environment);
-                })
-                .catch(() => {
-                    hasSetEnvironment = true;
-                    resolve(environment);
-                });
-        } catch (e) {
-            hasSetEnvironment = true;
-            resolve(environment);
-        }
+                resolve(environment);
+            })
+            .catch(() => {
+                environment = CONST.ENVIRONMENT.PRODUCTION;
+                resolve(environment);
+            });
     });
 }
 
