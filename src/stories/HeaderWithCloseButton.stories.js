1b65d7ed42937ae73172825f965e0cc96e52937a
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-05-14 08:46:11 -1000

    add a few stories

--- src/stories/HeaderWithCloseButton.stories.js
+++ src/stories/HeaderWithCloseButton.stories.js
@@ -17,7 +17,16 @@ const Template = args => <HeaderWithCloseButton {...args} />;
 // Arguments can be passed to the component by binding
 // See: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Default = Template.bind({});
+export const Attachment = Template.bind({});
+export const Profile = Template.bind({});
 Default.args = {
+    title: 'Settings',
+};
+Attachment.args = {
     title: 'Attachment',
     shouldShowDownloadButton: true,
+};
+Profile.args = {
+    title: 'Profile',
+    shouldShowBackButton: true,
 };
