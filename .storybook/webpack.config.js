fbfba0170dad97f1704fd05c9258ffde668e6dd5
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-05-13 14:02:05 -1000

    Use staging config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,19 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-param-reassign */
-/* eslint-disable @lwc/lwc/no-async-await */
 const path = require('path');
-module.exports = async ({config}) => {
+const dotenv = require('dotenv');
+
+const env = dotenv.config({path: path.resolve(__dirname, '../.env.staging')}).parsed;
+
+module.exports = ({config}) => {
     config.resolve.alias = {
         'react-native-config': 'react-web-config',
         'react-native$': 'react-native-web',
     };
 
+    // Necessary to overwrite the values in the existing DefinePlugin hardcoded to the Config staging values
     const definePluginId = config.plugins.findIndex(p => p.constructor.name === 'DefinePlugin');
-    config.plugins[definePluginId].definitions.__REACT_WEB_CONFIG__ = JSON.stringify({});
-
+    config.plugins[definePluginId].definitions.__REACT_WEB_CONFIG__ = JSON.stringify(env);
     config.resolve.extensions.push('.web.js', '.website.js');
     return config;
 };
