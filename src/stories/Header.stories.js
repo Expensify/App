e3a3ddf32f55be12afe15c7be1952c3a1b2ab12c
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-05-13 15:23:05 -1000

    add some comments

--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -1,5 +1,10 @@
 import Header from '../components/Header';
 
+/**
+ * We use the Component Story Format for writing stories. Follow the docs here:
+ *
+ * https://storybook.js.org/docs/react/writing-stories/introduction#component-story-format
+ */
 export default {
     title: 'Components/Header',
     component: Header,
@@ -8,8 +13,9 @@ export default {
 // eslint-disable-next-line react/jsx-props-no-spreading
 const Template = args => <Header {...args} />;
 
+// Arguments can be passed to the component by binding
+// See: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Default = Template.bind({});
-
 Default.args = {
     textSize: 'large',
     title: 'Chats',
