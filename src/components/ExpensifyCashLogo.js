3f7aa7776acccbf09d467a022ee2614956696e39
Joe Gambino <joe@expensify.com>
2021-05-17 14:16:40 -0700

    pass props in constructor, remove unnecessary index

--- src/components/ExpensifyCashLogo.js
+++ src/components/ExpensifyCashLogo.js
@@ -4,7 +4,7 @@ import ProductionLogo from '../../assets/images/expensify-cash.svg';
 import DevLogo from '../../assets/images/expensify-cash-dev.svg';
 import StagingLogo from '../../assets/images/expensify-cash-stg.svg';
 import CONST from '../CONST';
-import Environment from '../libs/Environment/index';
+import Environment from '../libs/Environment';
 
 const propTypes = {
     width: PropTypes.number.isRequired,
