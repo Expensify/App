27416b7a0f6886d676d0abfc4cb4d49355833010
Joe Gambino <joe@expensify.com>
2021-05-17 15:20:17 -0700

    use props instead of state

--- src/components/EnvironmentBadge.js
+++ src/components/EnvironmentBadge.js
@@ -2,27 +2,32 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import styles from '../styles/styles';
 import CONST from '../CONST';
-import withEnvironment from './withEnvironment';
+import withEnvironment, {environmentPropTypes} from './withEnvironment';
+
+const propTypes = {
+    ...environmentPropTypes,
+};
 
 class EnvironmentBadge extends React.Component {
     render() {
         // If we are on production, don't show any badge
-        if (this.state.environment === CONST.ENVIRONMENT.PRODUCTION) {
+        if (this.props.environment === CONST.ENVIRONMENT.PRODUCTION) {
             return null;
         }
 
-        const backgroundColorStyle = this.state.environment === CONST.ENVIRONMENT.STAGING
+        const backgroundColorStyle = this.props.environment === CONST.ENVIRONMENT.STAGING
             ? styles.badgeSuccess
             : styles.badgeDanger;
 
         return (
             <View style={[styles.badge, backgroundColorStyle, styles.ml2]}>
                 <Text style={styles.badgeText}>
-                    {this.state.environment}
+                    {this.props.environment}
                 </Text>
             </View>
         );
     }
 }
 
+EnvironmentBadge.propTypes = propTypes;
 export default withEnvironment(EnvironmentBadge);
