f4a5e79ad6241adc3cfed0248d9d498bff5b36dd
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-05-18 18:07:52 -1000

    Start to add the venmo check

--- src/CONST.js
+++ src/CONST.js
@@ -12,6 +12,9 @@ const CONST = {
         PRESSED: 'pressed',
         COMPLETE: 'complete',
     },
+    CURRENCY: {
+        USD: 'USD',
+    },
     CLOUDFRONT_URL,
     NEW_ZOOM_MEETING_URL: 'https://zoom.us/start/videomeeting',
     NEW_GOOGLE_MEET_MEETING_URL: 'https://meet.google.com/new',
