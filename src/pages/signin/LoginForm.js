48438e09a787f5bce7d664920078199ccf0bf6a3
Dharam <dhasing5@in.ibm.com>
2021-05-20 21:38:21 +0530

    Fix - Login screen use email keyboard type, disable auto complete

--- src/pages/signin/LoginForm.js
+++ src/pages/signin/LoginForm.js
@@ -83,6 +83,7 @@ class LoginForm extends React.Component {
                         onChangeText={text => this.setState({login: text})}
                         onSubmitEditing={this.validateAndSubmitForm}
                         autoCapitalize="none"
+                        //Fix autoCorrect
                         autoCorrect="off"
                         keyboardType="email-address"
                         placeholder={this.props.translate('loginForm.phoneOrEmail')}
