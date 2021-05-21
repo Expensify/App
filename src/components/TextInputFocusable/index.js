ce7ddc5e1797ea8701eef606250211170a555a7a
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-05-22 01:31:47 +0530

    chg: refactor growl to the top so that it does not show statically on native

--- src/components/TextInputFocusable/index.js
+++ src/components/TextInputFocusable/index.js
@@ -170,7 +170,6 @@ class TextInputFocusable extends React.Component {
      */
     setCursorPosition({nativeEvent: {selection}}) {
         this.selection = selection;
-        console.debug(this.selection);
     }
 
     /**
