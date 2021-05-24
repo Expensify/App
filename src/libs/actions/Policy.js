1c5edf28cca6399031965de3c2ea80f7ed37fb48
Amal Nazeem <amal@expensify.com>
2021-05-24 18:10:16 -0400

    initial addition for chatType info

---
+++ src/libs/actions/Policy.js
@@ -0,0 +1,14 @@
+import Onyx from 'react-native-onyx';
+import ONYXKEYS from '../../ONYXKEYS';
+
+
+/**
+ *
+ * @param {String} policyID
+ */
+function getPolicySummary(policyID) {
+}
+
+export {
+    getPolicySummary,
+}
--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -163,6 +163,11 @@ function getSimplifiedReportObject(report) {
     return {
         reportID: report.reportID,
         reportName,
+        chatType: lodashGet(report, [
+            'reportNameValuePairs.chatType'
+        ]),
+        ownerEmail: report.ownerEmail,
+        policyName: getPolicyName(report.),
         unreadActionCount: getUnreadActionCount(report),
         maxSequenceNumber: report.reportActionList.length,
         participants: getParticipantEmailsFromReport(report),
