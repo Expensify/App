7546c040018fb2a7db7e04cfa453992553138712
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-05-24 12:34:21 -1000

    Fix styles

--- src/pages/EnablePayments/TermsStep.js
+++ src/pages/EnablePayments/TermsStep.js
@@ -65,7 +65,7 @@ class TermsStep extends React.Component {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </Text>
                     </ScrollView>
-                    <View style={[styles.flexRow, styles.mb4]}>
+                    <View style={[styles.flexRow, styles.mb4, styles.flex1]}>
                         <Checkbox
                             isChecked={this.state.hasAcceptedDisclosure}
                             onClick={this.toggleDisclosure}
@@ -82,14 +82,13 @@ class TermsStep extends React.Component {
                             </Text>
                         </TouchableOpacity>
                     </View>
-                    <View style={[styles.flexRow, styles.mb4]}>
+                    <View style={[styles.flexRow, styles.mb4, styles.flex1]}>
                         <Checkbox
                             isChecked={this.state.hasAcceptedPrivacyPolicyAndWalletAgreement}
                             onClick={this.togglePrivacyPolicy}
                         />
                         <TouchableOpacity
                             onPress={this.togglePrivacyPolicy}
-                            style={[styles.w100]}
                         >
                             <Text style={[styles.ml2, styles.textP, styles.flex1]}>
                                 {`${this.props.translate('termsStep.agreeToThe')} `}
