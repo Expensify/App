11ec73c055d0937b6a9e7bbf300261b41bb92d99
Jasper Huang <jasper@expensify.com>
2021-06-02 11:30:25 +0800

    Re-add stuff lost in merge

--- src/CONST.js
+++ src/CONST.js
@@ -168,6 +168,7 @@ const CONST = {
         // not be changed.
         PAYMENT_TYPE: {
             ELSEWHERE: 'Elsewhere',
+            EXPENSIFY: 'Expensify',
             PAYPAL_ME: 'PayPal.me',
             VENMO: 'Venmo',
         },
