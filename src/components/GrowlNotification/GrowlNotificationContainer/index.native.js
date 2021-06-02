97fb81675c2f19097d74f49ad7540bdf1b35382a
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-06-02 05:40:34 +0530

    fix Prop types

--- src/components/GrowlNotification/GrowlNotificationContainer/index.native.js
+++ src/components/GrowlNotification/GrowlNotificationContainer/index.native.js
@@ -1,8 +1,14 @@
+import _ from 'underscore';
 import React from 'react';
 import {Animated} from 'react-native';
 import {SafeAreaInsetsContext} from 'react-native-safe-area-context';
 import styles, {getSafeAreaPadding} from '../../../styles/styles';
-import propTypes from './GrowlNotificationContainerPropTypes';
+import {windowDimensionsPropTypes} from '../../withWindowDimensions';
+import GrowlNotificationContainerPropTypes from './GrowlNotificationContainerPropTypes';
+
+const propTypes = {
+    ..._.omit(GrowlNotificationContainerPropTypes, _.keys(windowDimensionsPropTypes)),
+};
 
 const GrowlNotificationContainer = ({children, translateY}) => (
     <SafeAreaInsetsContext.Consumer>
