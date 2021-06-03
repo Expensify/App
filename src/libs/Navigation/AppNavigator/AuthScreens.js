3a7cf351b9f95232231a5786509c77c8ce1de774
Tim Golen <tgolen@expensify.com>
2021-06-03 14:32:08 -0600

    Added a new action file for getting policy summaries

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -27,6 +27,7 @@ import Navigation from '../Navigation';
 import * as User from '../../actions/User';
 import {setModalVisibility} from '../../actions/Modal';
 import NameValuePair from '../../actions/NameValuePair';
+import {getPolicySummaries} from '../../actions/Policy';
 import modalCardStyleInterpolator from './modalCardStyleInterpolator';
 import createCustomModalStackNavigator from './createCustomModalStackNavigator';
 
@@ -122,6 +123,7 @@ class AuthScreens extends React.Component {
         fetchAllReports(true, true, true);
         fetchCountryCodeByRequestIP();
         UnreadIndicatorUpdater.listenForReportChanges();
+        getPolicySummaries();
 
         // Refresh the personal details, timezone and betas every 30 minutes
         // There is no pusher event that sends updated personal details data yet
