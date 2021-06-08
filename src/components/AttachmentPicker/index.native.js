6adbb0fbb07dcba4f24bd4c62c147b34a616bda5
Yevhenii Voloshchak <copyreading@gmail.com>
2021-06-08 19:23:02 +0300

    Do not translate showImagePicker error description

--- src/components/AttachmentPicker/index.native.js
+++ src/components/AttachmentPicker/index.native.js
@@ -145,8 +145,7 @@ class AttachmentPicker extends Component {
                             this.showGeneralAlert(response.error);
                             break;
                     }
-                    const errorDescription = this.props.translate('attachmentPicker.errorDuringAttachmentSelection');
-                    reject(new Error(`${errorDescription}: ${response.error}`));
+                    reject(new Error(`Error during attachment selection: ${response.error}`));
                 }
 
                 resolve(response);
