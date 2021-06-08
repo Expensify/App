c128cfe3d5c7e5d38f5f501cf42059b2caa5a0c3
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-06-08 12:41:12 -1000

    add create manual form

--- src/pages/BusinessBankAccount/NewPage.js
+++ src/pages/BusinessBankAccount/NewPage.js
@@ -1,6 +1,6 @@
 import _ from 'underscore';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
 import MenuItem from '../../components/MenuItem';
@@ -12,11 +12,35 @@ import Icon from '../../components/Icon';
 import colors from '../../styles/colors';
 import Navigation from '../../libs/Navigation/Navigation';
 import Permissions from '../../libs/Permissions';
+import CONST from '../../CONST';
+import TextInputWithLabel from '../../components/TextInputWithLabel';
+import Checkbox from '../../components/Checkbox';
 
 class BusinessBankAccountNewPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+
+        this.toggleTerms = this.toggleTerms.bind(this);
+
+        this.state = {
+            addMethodSelection: undefined,
+            hasAcceptedTerms: false,
+            routingNumber: undefined,
+            accountNumber: undefined,
+        };
+    }
+
+    toggleTerms() {
+        this.setState(prevState => ({
+            hasAcceptedTerms: !prevState.hasAcceptedTerms,
+        }));
+    }
+
+    canSubmitManually() {
+        // @TODO add better validation
+        return this.state.hasAcceptedTerms
+            && this.state.accountNumber
+            && this.state.routingNumber;
     }
 
     render() {
@@ -27,54 +51,118 @@ class BusinessBankAccountNewPage extends React.Component {
             return null;
         }
 
+        // @TODO - these are duplicated from TermsStep for the KYC flow and the checkboxes there should be refactored
+        // into a new component as a follow up.
+        const toggleStyles = [
+            styles.ml2,
+            styles.pr2,
+            styles.w100,
+            styles.flexRow,
+            styles.flexWrap,
+            styles.alignItemsCenter,
+        ];
+
         return (
             <ScreenWrapper>
                 <View style={[styles.flex1, styles.justifyContentBetween]}>
                     <HeaderWithCloseButton
                         title="Add Bank Account"
                         onCloseButtonPress={Navigation.dismissModal}
+                        onBackButtonPress={() => this.setState({addMethodSelection: undefined})}
+                        shouldShowBackButton={!_.isUndefined(this.state.addMethodSelection)}
                     />
-                    <View style={[styles.flex1]}>
-                        <Text style={[styles.mh5, styles.mb5]}>
-                            To get started with the Expensify Card, you first need to add a bank account.
-                        </Text>
-                        <MenuItem
-                            icon={Bug}
-                            title="Log Into Your Bank"
-                            onPress={() => {
-                                console.log('launching Plaid flow');
-                            }}
-                            shouldShowRightIcon
-                        />
-                        <MenuItem
-                            icon={Bug}
-                            title="Connect Manually"
-                            onPress={() => {
-                                console.log('connect manually');
-                            }}
-                            shouldShowRightIcon
-                        />
-                        <View style={[styles.m5, styles.flexRow, styles.justifyContentBetween]}>
-                            <TextLink href="">
-                                Privacy
-                            </TextLink>
-                            <TextLink href="" style={[styles.dFlex, styles.justifyContentCenter]}>
-                                Your data is secure
-                                <View style={[styles.ml1]}>
-                                    <Icon src={Lock} fill={colors.blue} />
+                    {!this.state.addMethodSelection && (
+                        <>
+                            <View style={[styles.flex1]}>
+                                <Text style={[styles.mh5, styles.mb5]}>
+                                    To get started with the Expensify Card, you first need to add a bank account.
+                                </Text>
+                                <MenuItem
+                                    icon={Bug}
+                                    title="Log Into Your Bank"
+                                    onPress={() => {
+                                        this.setState({addMethodSelection: CONST.BANK_ACCOUNT.ADD_METHOD.PLAID});
+                                    }}
+                                    shouldShowRightIcon
+                                />
+                                <MenuItem
+                                    icon={Bug}
+                                    title="Connect Manually"
+                                    onPress={() => {
+                                        this.setState({addMethodSelection: CONST.BANK_ACCOUNT.ADD_METHOD.MANUAL});
+                                    }}
+                                    shouldShowRightIcon
+                                />
+                                <View style={[styles.m5, styles.flexRow, styles.justifyContentBetween]}>
+                                    <TextLink href="https://use.expensify.com/privacy">
+                                        Privacy
+                                    </TextLink>
+                                    <TextLink
+                                        // eslint-disable-next-line max-len
+                                        href="https://community.expensify.com/discussion/5677/deep-dive-how-expensify-protects-your-information/"
+                                        style={[styles.dFlex, styles.justifyContentCenter]}>
+                                        Your data is secure
+                                        <View style={[styles.ml1]}>
+                                            <Icon src={Lock} fill={colors.blue} />
+                                        </View>
+                                    </TextLink>
                                 </View>
-                            </TextLink>
+                            </View>
+                        </>
+                    )}
+                    {this.state.addMethodSelection === CONST.BANK_ACCOUNT.ADD_METHOD.PLAID && (
+                        <View>
+                            <Text>Launch Plaid Flow whoosh</Text>
                         </View>
-                    </View>
-                    <View style={[styles.m5]}>
-                        <Button
-                            success
-                            text="Save & Continue"
-                            onPress={() => {
-                                console.log('lehgo');
-                            }}
-                        />
-                    </View>
+                    )}
+                    {this.state.addMethodSelection === CONST.BANK_ACCOUNT.ADD_METHOD.MANUAL && (
+                        <>
+                            <View style={[styles.m5, styles.flex1]}>
+                                <Text style={[styles.mb5]}>
+                                    Your routing number and account number can be found on a check for the account.
+                                </Text>
+                                <TextInputWithLabel
+                                    label="Routing Number"
+                                    placeholder="9 digits"
+                                    keyboardType="numeric"
+                                    value={this.state.routingNumber}
+                                    onChangeText={routingNumber => this.setState({routingNumber})}
+                                />
+                                <TextInputWithLabel
+                                    label="Account Number"
+                                    keyboardType="numeric"
+                                    value={this.state.accountNumber}
+                                    onChangeText={accountNumber => this.setState({accountNumber})}
+                                />
+                                <View style={[styles.flexRow, styles.mb4, styles.mt5]}>
+                                    <Checkbox
+                                        isChecked={this.state.hasAcceptedTerms}
+                                        onClick={this.toggleTerms}
+                                    />
+                                    <TouchableOpacity
+                                        onPress={this.toggleTerms}
+                                        style={toggleStyles}
+                                    >
+                                        <Text>
+                                            {'I accept the '}
+                                            <TextLink href="https://use.expensify.com/terms">
+                                                Expensify Terms of Service
+                                            </TextLink>
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            <Button
+                                success
+                                text="Save & Continue"
+                                style={[styles.m5]}
+                                isDisabled={!this.canSubmitManually()}
+                                onPress={() => {
+                                    console.log('lehgo');
+                                }}
+                            />
+                        </>
+                    )}
                 </View>
             </ScreenWrapper>
         );
