539cf49e5d10ef5eae65dcdf8d283e4ad4760260
Rafael <rdjuric@pm.me>
2021-06-09 02:51:35 -0300

    makes PDF attachment modals scrollable

--- src/CONST.js
+++ src/CONST.js
@@ -70,6 +70,7 @@ const CONST = {
         MODAL_TYPE: {
             CONFIRM: 'confirm',
             CENTERED: 'centered',
+            CENTERED_UNSWIPEABLE: 'centered_unswipeable',
             BOTTOM_DOCKED: 'bottom_docked',
             POPOVER: 'popover',
             RIGHT_DOCKED: 'right_docked',
