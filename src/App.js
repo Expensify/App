52102f0a958552a35d09084d0ba5f9dd01b25fa7
Peter Velkov <kidroca@gmail.com>
2021-06-10 23:21:09 +0300

    feat: wrap the root component with an error boundary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+
 import CustomStatusBar from './components/CustomStatusBar';
 import Expensify from './Expensify';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App = () => (
     <SafeAreaProvider>
         <CustomStatusBar />
-        <Expensify />
+        <ErrorBoundary errorMessage="[App] A Crash was intercepted by the main error boundary">
+            <Expensify />
+        </ErrorBoundary>
     </SafeAreaProvider>
 );
 
