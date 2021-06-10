cd343d112812905ca44d5ae852f30cd2b5210e88
Peter Velkov <kidroca@gmail.com>
2021-06-11 01:16:09 +0300

    refactor: update parameters because the error message is not logging
    The error logs as "{}" in the log, probably because it's a special object
    Updated to pass the error message instead

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -3,7 +3,7 @@ import Log from '../../libs/Log';
 
 BaseErrorBoundary.defaultProps.logError = (errorMessage, error, errorInfo) => {
     // Log the error to the server
-    Log.alert(errorMessage, 10, {error, errorInfo});
+    Log.alert(errorMessage, 10, {error: error.message, errorInfo});
 };
 
 export default BaseErrorBoundary;
