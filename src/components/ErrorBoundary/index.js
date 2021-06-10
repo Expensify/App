f5a4ea7888c9e08885ea4c9d45a51f33e5c4eadb
Peter Velkov <kidroca@gmail.com>
2021-06-11 00:35:46 +0300

    refactor: use Log.alert instead of Log.info

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -3,7 +3,7 @@ import Log from '../../libs/Log';
 
 BaseErrorBoundary.defaultProps.logError = (errorMessage, error, errorInfo) => {
     // Log the error to the server
-    Log.info(errorMessage, true, {error, errorInfo});
+    Log.alert(errorMessage, 10, {error, errorInfo});
 };
 
 export default BaseErrorBoundary;
