46989c4a8513097306f75652a66514123a7d7677
Peter Velkov <kidroca@gmail.com>
2021-06-11 11:54:56 +0300

    refactor: address PR comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-
 import CustomStatusBar from './components/CustomStatusBar';
-import Expensify from './Expensify';
 import ErrorBoundary from './components/ErrorBoundary';
+import Expensify from './Expensify';
 
 const App = () => (
     <SafeAreaProvider>
         <CustomStatusBar />
-        <ErrorBoundary errorMessage="[App] A Crash was intercepted by the main error boundary">
+        <ErrorBoundary errorMessage="E.cash crash caught by error boundary">
             <Expensify />
         </ErrorBoundary>
     </SafeAreaProvider>
