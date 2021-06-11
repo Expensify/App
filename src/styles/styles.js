4ad470f25b78bcc71252e9982523e6a66f9af13a
Horus Lugo López <horus@expensify.com>
2021-06-11 14:01:27 +0200

    Add AttachmentPicker and use the workspace default avatar

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1521,13 +1521,6 @@ const styles = {
     cursorDisabled: {
         cursor: 'not-allowed',
     },
-
-    newWorkspacePill: {
-        height: 28,
-        width: 80,
-        borderRadius: 4,
-        backgroundColor: colors.gray1,
-    },
 };
 
 const baseCodeTagStyles = {
