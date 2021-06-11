d9603602eb5ceb401080a132447477a5052151e3
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-06-10 20:04:00 -1000

    move setupWithdrawalAccount into an action so that our views can purely respond to Onyx setting the achData

--- src/pages/ReimbursementAccountPage.js
+++ src/pages/ReimbursementAccountPage.js
@@ -1,9 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable react/no-unused-state */
-import _ from 'underscore';
 import moment from 'moment';
 import lodashGet from 'lodash/get';
-import lodashHas from 'lodash/has';
 import React from 'react';
 import {withOnyx} from 'react-native-onyx';
 import Str from 'expensify-common/lib/str';
@@ -12,50 +10,31 @@ import PropTypes from 'prop-types';
 import ScreenWrapper from '../components/ScreenWrapper';
 import {fetchFreePlanVerifiedBankAccount} from '../libs/actions/BankAccounts';
 import ONYXKEYS from '../ONYXKEYS';
-import BankAccount from '../libs/models/BankAccount';
-import * as API from '../libs/API';
-
-const WITHDRAWAL_ACCOUNT_STEPS = [
-    {
-        id: 'BankAccountStep',
-        title: 'Bank Account',
-    },
-    {
-        id: 'CompanyStep',
-        title: 'Company Information',
-    },
-    {
-        id: 'RequestorStep',
-        title: 'Requestor Information',
-    },
-    {
-        id: 'ACHContractStep',
-        title: 'Beneficial Owners',
-    },
-    {
-        id: 'ValidationStep',
-        title: 'Validate',
-    },
-    {
-        id: 'EnableStep',
-        title: 'Enable',
-    },
-];
 
 const propTypes = {
-    skipOnfido: PropTypes.bool,
     freePlanBankAccount: PropTypes.shape({
-        bankAccount: PropTypes.shape({}),
         loading: PropTypes.bool,
         throttledDate: PropTypes.string,
+        achData: PropTypes.shape({
+            country: PropTypes.string,
+        }),
     }),
+
+    personalDetails: PropTypes.shape({
+        firstName: PropTypes.string,
+        lastName: PropTypes.string,
+    }),
+
+    session: PropTypes.shape({
+        email: PropTypes.string,
+    }).isRequired,
 };
 
 const defaultProps = {
-    skipOnfido: false,
     freePlanBankAccount: {
         loading: true,
     },
+    personalDetails: {},
 };
 
 class ReimbursementAccountPage extends React.Component {
@@ -65,11 +44,6 @@ class ReimbursementAccountPage extends React.Component {
         this.state = {
             bankAccountID: undefined,
             isLoading: true,
-
-            // In Web-Secure Plaid is disabled if there is no window.Plaid global (SDK script didn't load) or if we explicitly disable it because the user made too many calls to BankAccount_Get
-            // See: https://github.com/Expensify/Web-Secure/blob/044c82affb78812a58b881a6d5ba026d91dace3b/site/app/settings/reimbursement/PlaidBankForm.jsx#L171-L176
-            // @TODO we should handle the too many calls to Plaid situation, but for now it will remain enabled
-            isPlaidDisabled: false,
             achData: {},
             isWithdrawal: true,
             lastDataWithError: {},
@@ -81,247 +55,17 @@ class ReimbursementAccountPage extends React.Component {
         fetchFreePlanVerifiedBankAccount();
     }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.freePlanBankAccount.loading && !this.props.freePlanBankAccount.loading) {
-            this.init();
-        }
-    }
-
-    /**
-     * Get step position in the array
-     * @param {String} stepID
-     * @return {Number}
-     */
-    getIndexByStepID(stepID) {
-        return _.findIndex(WITHDRAWAL_ACCOUNT_STEPS, step => step.id === stepID);
-    }
-
-    /**
-     * Get next step ID
-     * @return {String}
-     */
-    getNextStepID() {
-        const nextStepIndex = Math.min(this.getIndexByStepID(this.state.achData.currentStep) + 1, WITHDRAWAL_ACCOUNT_STEPS.length - 1);
-        return lodashGet(WITHDRAWAL_ACCOUNT_STEPS, [nextStepIndex, 'id'], 'BankAccountStep');
-    }
-
-    /**
-     * Create or update the bank account in db with the updated data.
-     *
-     * @param {Object} [data]
-     */
-    setupWithdrawalAccount(data) {
-        // @TODO trigger loading
-
-        // Create a shallow copy of the data and overwrite new values
-        const newAchData = {...this.state.achData};
-        _.extend(newAchData, data);
-        if (data && data.isSavings !== undefined) {
-            newAchData.isSavings = Boolean(data.isSavings);
-        }
-        if (!newAchData.setupType) {
-            newAchData.setupType = newAchData.plaidAccountID ? 'plaid' : 'manual';
-        }
-
-        let nextStep = newAchData.currentStep;
-
-        // @TODO move this to an action instead
-        API.BankAccount_SetupWithdrawal(newAchData)
-            .finally((response) => {
-                // @TODO hide the loader
-
-                const currentStep = newAchData.currentStep;
-                let achData = response.achData;
-                let error = lodashGet(achData, 'verifications.errorMessage');
-
-                if (response.jsonCode === 200 && !error) {
-                    // Show warning if another account already set up this bank account and promote share
-                    if (response.existingOwners) {
-                        // @TODO Show error about existing owners
-                        return;
-                    }
-
-                    if (currentStep === 'RequestorStep') {
-                        const requestorResponse = lodashGet(achData, 'verifications.externalApiResponses.requestorIdentityID');
-                        if (newAchData.useOnfido) {
-                            const onfidoRes = lodashGet(achData, 'verifications.externalApiResponses.requestorIdentityOnfido');
-                            const sdkToken = lodashGet(onfidoRes, 'apiResult.sdkToken');
-                            if (sdkToken && !newAchData.isOnfidoSetupComplete && onfidoRes.status !== 'pass') {
-                                // Requestor Step still needs to run Onfido
-                                achData.sdkToken = sdkToken;
-                                this.setState({achData: newAchData}, () => {
-                                    this.goToStepID('RequestorStep', achData);
-                                });
-                                return;
-                            }
-                        } else if (requestorResponse) {
-                            // Don't go to next step if Requestor Step needs to ask some questions
-                            let questions = lodashGet(requestorResponse, 'apiResult.questions.question') || [];
-                            if (_.isEmpty(questions)) {
-                                const differentiatorQuestion = lodashGet(requestorResponse, 'apiResult.differentiator-question');
-                                if (differentiatorQuestion) {
-                                    questions = [differentiatorQuestion];
-                                }
-                            }
-                            if (!_.isEmpty(questions)) {
-                                achData.questions = questions;
-                                this.setState({achData: newAchData}, () => {
-                                    this.goToStepID('RequestorStep', achData);
-                                });
-                                return;
-                            }
-                        }
-                    }
-                    if (currentStep === 'ACHContractStep') {
-                        // const promise = $.Deferred(); // Not sure what is awaiting this promise yet but doesn't seem like something we will want to do...
-
-                        // We want to make a task completion so we can pay guides, but we don't want to close the iframe so mark completeSetup as false
-                        // this.triggerParentSuccessMessage(response, false); - Not really sure if this applies in E.cash
-
-                        // Get an up-to-date bank account list so that we can allow the user to validate their newly generated bank account
-                        return API.get({returnValueList: 'bankAccountList'})
-                            .done((json) => {
-                                const bankAccount = new BankAccount(_.findWhere(json.bankAccountList, {bankAccountID: newAchData.bankAccountID}));
-                                achData = bankAccount.toACHData();
-                                const needsToPassLatestChecks = achData.state === BankAccount.STATE.OPEN && achData.needsToPassLatestChecks;
-                                achData.bankAccountInReview = needsToPassLatestChecks || achData.state === BankAccount.STATE.VERIFYING;
-                                this.setState({achData: newAchData}, () => {
-                                    this.goToStepID('ValidationStep', achData);
-                                });
-                            });
-                    }
-                    if ((currentStep === 'ValidationStep' && newAchData.bankAccountInReview) || currentStep === 'EnableStep') {
-                        // Setup done! We can close the modal - @TODO - not sure what this is doing or if we need it
-                        // this.triggerParentSuccessMessage(response);
-                    } else {
-                        nextStep = this.getNextStepID();
-                    }
-                } else {
-                    if (response.jsonCode === 666) {
-                        error = response.message;
-                    }
-                    if (lodashGet(achData, 'verifications.throttled')) {
-                        achData.disableFields = true;
-                    }
-                }
-
-                // Go to next step
-                this.setState({achData: newAchData}, () => {
-                    this.goToStepID(nextStep, achData);
-                });
-
-                if (error) {
-                    // @TODO - Show the error
-                }
-            });
-    }
-
-    getDefaultCountry() {
-        const defaultCountry = this.state.achData.country || 'US'; // @TODO - In Web-Expensify this fallback refers to User.getIpCountry()
-
-        // In Web-Secure we check the policy to find out the defaultCountry. The policy is passed in here:
-        // https://github.com/Expensify/Web-Expensify/blob/896941794f68d7dce64466d83a3e86a5f8122e45/site/app/policyEditor/policyEditorPage.jsx#L2169-L2171
-        // @TODO figure out whether we need to check the policy or not - not sure if it's necessary for V1 of Free Plan
-        return defaultCountry;
-    }
-
-    /**
-     * Go to a specific step id.
-     * @param {String} stepID
-     * @param {Object} [achData]
-     */
-    goToStepID(stepID, achData) {
-        // Setting state again will refresh the view and progress us to the next step
-        this.setState((prevState) => {
-            const newAchData = {...prevState.achData};
-
-            // If we go back to Requestor Step, reset any validation and previously answered questions from expectID.
-            if (!newAchData.useOnfido && stepID === 'RequestorStep') {
-                delete newAchData.questions;
-                delete newAchData.answers;
-                if (lodashHas(newAchData, 'verifications.externalApiResponses')) {
-                    delete newAchData.verifications.externalApiResponses.requestorIdentityID;
-                    delete newAchData.verifications.externalApiResponses.requestorIdentityKBA;
-                }
-            }
-
-            // When going from companyStep to bankAccountStep, show the manual form instead of Plaid
-            if (newAchData.currentStep === 'CompanyStep' && stepID === 'BankAccountStep') {
-                newAchData.subStep = 'manual';
-            }
-
-            _.extend(newAchData, achData, {currentStep: stepID});
-            return ({achData: newAchData});
-        });
-    }
-
-    init() {
-        // If the user already is already setting up a bank account we will want to continue the flow for them
-        let currentStep;
-        const bankAccountJSON = parseInt(lodashGet(this.props, 'freePlanBankAccount.bankAccount.bankAccountID', '0'), 10) || 0;
-        const bankAccount = bankAccountJSON ? new BankAccount(bankAccountJSON) : null;
-        const achData = bankAccount ? bankAccount.toACHData() : {};
-        achData.useOnfido = !this.props.skipOnfido;
-        achData.policyID = this.props.policyId || '';
-        achData.plaidLinkToken = this.props.plaidLinkToken; // I think probably we won't have this until we start the Plaid flow but maybe...?
-        achData.isInSetup = !bankAccount || bankAccount.isInSetup();
-        achData.bankAccountInReview = bankAccount && bankAccount.isVerifying();
-        achData.domainLimi = 0;
-        achData.isDomainUsingExpensifyCard = false; // Maybe also needs to be a prop... not too sure.
-
-        // Unsure what the substeps are used for so far... pretty sure it's just to advance to the "login" step here and not used anywhere else
-        // https://github.com/Expensify/Web-Expensify/blob/896941794f68d7dce64466d83a3e86a5f8122e45/site/app/settings/reimbursement/bankAccountView.jsx#L356-L357
-        achData.subStep = this.props.subStep;
-
-        // If we're not in setup, it means we already have a withdrawal account and we're upgrading it to a business bank account.
-        // So let the user review all steps with all info prefilled and editable, unless a specific step was passed.
-        if (!achData.isInSetup) {
-            currentStep = this.props.currentStep; // Not sure if we need this as user's won't be upgrading these accounts...
-        }
-
-        // Temporary fix for Onfido flow. Can be removed by nkuoch after Sept 1 2020. - not sure if we need this or what this is about...
-        if (currentStep === 'ACHContractStep' && achData.useOnfido) {
-            const onfidoRes = lodashGet(achData, 'verifications.externalApiResponses.requestorIdentityOnfido');
-            const sdkToken = lodashGet(onfidoRes, 'apiResult.sdkToken');
-            if (sdkToken && !achData.isOnfidoSetupComplete && onfidoRes.status !== 'pass') {
-                currentStep = 'RequestorStep';
-            }
-        }
-
-        // Ensure we route the user to the correct step based on the status of their bank account
-        if (bankAccount && !currentStep) {
-            currentStep = bankAccount.isPending() || bankAccount.isVerifying() ? 'ValidationStep' : 'BankAccountStep';
-
-            // Again, not sure how much of this logic is needed right now as we shouldn't be handling any open accounts in E.cash yet...
-            if (bankAccount.isOpen()) {
-                if (bankAccount.needsToPassLatestChecks()) {
-                    // const hasTriedToUpgrade = bankAccount.getDateSigned() > (NVP.get('expensify_migration_2020_04_28_RunKycVerifications') || '2020-01-13');
-                    // currentStep = hasTriedToUpgrade ? 'ValidationStep' : 'CompanyStep';
-                    // achData.bankAccountInReview = hasTriedToUpgrade;
-                } else {
-                    // Not handling the EnableStep...
-                    // currentStep = 'EnableStep';
-                }
-            }
-        }
-
-        // If at this point we still don't have a current step, default to the BankAccountStep
-        if (!currentStep) {
-            currentStep = 'BankAccountStep';
-        }
-
-        this.setState({isLoading: false, currentStep, achData}, () => {
-            // @TODO this isn't really ideal - mostly doing this so we don't have to deviate too far from what is being migrated from Web-Secure for now.
-            this.goToStepID(currentStep, achData);
-        });
-    }
-
     render() {
         if (this.props.freePlanBankAccount.loading) {
             return null;
         }
 
-        const defaultCountry = this.getDefaultCountry();
+        const {achData} = this.props.freePlanBankAccount;
+
+        // In Web-Secure we check the policy to find out the defaultCountry. The policy is passed in here:
+        // https://github.com/Expensify/Web-Expensify/blob/896941794f68d7dce64466d83a3e86a5f8122e45/site/app/policyEditor/policyEditorPage.jsx#L2169-L2171
+        // @TODO figure out whether we need to check the policy or not - not sure if it's necessary for V1 of Free Plan
+        const defaultCountry = achData.country || 'US'; // @TODO - In Web-Expensify this fallback refers to User.getIpCountry()
         const personalDetails = {firstName: this.props.personalDetails.firstName, lastName: this.props.personalDetails.lastName};
         const userHasPhonePrimaryEmail = Str.endsWith(this.props.session.email, '@expensify.sms');
 
@@ -330,15 +74,13 @@ class ReimbursementAccountPage extends React.Component {
             defaultCountry,
             personalDetails,
             userHasPhonePrimaryEmail,
-            achData: this.state.achData,
-            steps: WITHDRAWAL_ACCOUNT_STEPS,
-            isPlaidDisabled: this.state.isPlaidDisabled,
+            achData,
         });
 
         // React.v.AddWithdrawalAccountForm mostly exists to
-        // 1. show a message about being throttled
-        // 2. show the step progress bar (that we're not using here)
-        // 3. block people who have primary phone logins from adding VBA
+        // 1. Show a message about being throttled
+        // 2. Show the step progress bar (that we're not using here)
+        // 3. Block people who have primary phone logins from adding VBA
         // https://github.com/Expensify/Web-Secure/blob/044c82affb78812a58b881a6d5ba026d91dace3b/site/app/dialogs/reimbursementAccount/addWithdrawalAccountForm.jsx#L56-L73
 
         if (userHasPhonePrimaryEmail) {
@@ -358,14 +100,15 @@ class ReimbursementAccountPage extends React.Component {
 
         // If we made it this far then we will render React.v.AddBankAccountForm with the isWithdrawal passed and the following params
         console.debug({
-            achData: this.state.achData,
+            achData,
             defaultCountry,
             preventCountryEdit: false, // Maybe we don't need to worry about this yet since there is no country selection?
             personalDetails,
             userHasPhonePrimaryEmail,
-            steps: WITHDRAWAL_ACCOUNT_STEPS,
-            isPlaidDisabled: this.state.isPlaidDisabled,
         });
+
+        // Everything we need to display UI-wise is pretty much in the achData at this point. We just need to call the correct actions in the right places when
+        // submitting a form or navigating back or forward.
 
         // This is where stuff gets fun... this form is used for both withdrawal and deposit accounts and displays the "step views" there are also some controls for
         // navigating to next and previous steps, error, and loading states etc. Errors + loaders are set with PubSub events. We will likely want to use Onyx for that
