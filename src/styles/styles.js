1e2c3459f86fdb64a2ac9b196970998a3cbb4f6f
Nikki Wines <nikkiwines@expensify.com>
2021-06-14 18:07:03 -0700

    add textMicroSupporting

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -454,6 +454,13 @@ const styles = {
         lineHeight: 16,
     },
 
+    textMicroSupporting: {
+        color: themeColors.textSupporting,
+        fontFamily: fontFamily.GTA,
+        fontSize: variables.fontSizeSmall,
+        lineHeight: 14,
+    },
+
     // Sidebar Styles
     sidebar: {
         backgroundColor: themeColors.sidebar,
