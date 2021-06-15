68d98296e46a748567d76df2e27bc4a9e78bcaad
Kosuke Tseng <ctseng@expensify.com>
2021-06-14 21:53:08 -0700

    get props and state

--- src/pages/ReimbursementAccount/ValidationStep.js
+++ src/pages/ReimbursementAccount/ValidationStep.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import {View, TextInput} from 'react-native';
-import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
-import Navigation from '../../libs/Navigation/Navigation';
+import {View} from 'react-native';
 import styles from '../../styles/styles';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
+
+import Button from '../../components/Button';
+import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
+import Navigation from '../../libs/Navigation/Navigation';
+import TextInputWithLabel from '../../components/TextInputWithLabel';
 
 const propTypes = {
     ...withLocalizePropTypes,
 };
 
 class ValidationStep extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            amount1: 0,
+        };
+    }
+
     render() {
         return (
             <View style={[styles.flex1, styles.justifyContentBetween]}>
@@ -17,9 +28,23 @@ class ValidationStep extends React.Component {
                     title="Validation Step"
                     onCloseButtonPress={Navigation.dismissModal}
                 />
+                <TextInputWithLabel
+                    placeholder="1.01"
+                    keyboardType="number-pad"
+                    value={this.state.amount1}
+                    onChangeText={amount1 => this.setState({amount1})}
+                />
+                <Button
+                    success
+                    text={this.props.translate('common.saveAndContinue')}
+                    style={[styles.m5]}
+                    onPress={this.submit}
+                />
             </View>
         );
     }
 }
+
+ValidationStep.propTypes = propTypes;
 
 export default withLocalize(ValidationStep);
