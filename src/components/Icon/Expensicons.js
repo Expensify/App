1d012601432524fd7c41ae9725912768593a8353
Horus Lugo López <horus@expensify.com>
2021-06-16 17:04:18 +0200

    Add NewWorkspace icon

--- src/components/Icon/Expensicons.js
+++ src/components/Icon/Expensicons.js
@@ -43,6 +43,7 @@ import Trashcan from '../../../assets/images/trashcan.svg';
 import Users from '../../../assets/images/users.svg';
 import Upload from '../../../assets/images/upload.svg';
 import Wallet from '../../../assets/images/wallet.svg';
+import NewWorkspace from '../../../assets/images/new-workspace.svg';
 
 export {
     Android,
@@ -90,4 +91,5 @@ export {
     Upload,
     Users,
     Wallet,
+    NewWorkspace,
 };
