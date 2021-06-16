143e7b8aa382fc7abdd6505b4f83daba347136e2
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-06-16 07:56:34 -1000

    add check for isUsingExpensifyCard

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -123,7 +123,7 @@ class AuthScreens extends React.Component {
         PersonalDetails.fetchPersonalDetails();
         User.getUserDetails();
         User.getBetas();
-        User.getPublicDomainInfo();
+        User.getDomainInfo();
         PersonalDetails.fetchCurrencyPreferences();
         fetchAllReports(true, true);
         fetchCountryCodeByRequestIP();
