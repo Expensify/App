42f52ee0c0a2dd56c7d64f9d7bfb7303277c1ca4
Rory Abraham <rory@expensify.com>
2021-06-16 14:50:58 -0700

    Put header and footer outside ScrollView

--- src/pages/ReimbursementAccount/CompanyStep.js
+++ src/pages/ReimbursementAccount/CompanyStep.js
@@ -17,79 +17,81 @@ import StatePicker from '../../components/StatePicker';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 
 const CompanyStep = ({translate}) => (
-    <ScrollView style={[styles.flex1, styles.w100]}>
+    <>
         <HeaderWithCloseButton
             title={translate('companyStep.headerTitle')}
             shouldShowBackButton
             onBackButtonPress={() => goToWithdrawalAccountSetupStep(CONST.BANK_ACCOUNT.STEP.BANK_ACCOUNT)}
             onCloseButtonPress={Navigation.dismissModal}
         />
-        <View style={[styles.p4]}>
-            <View style={[styles.alignItemsCenter]}>
-                <Text>{translate('companyStep.subtitle')}</Text>
+        <ScrollView style={[styles.flex1, styles.w100]}>
+            <View style={[styles.p4]}>
+                <View style={[styles.alignItemsCenter]}>
+                    <Text>{translate('companyStep.subtitle')}</Text>
+                </View>
+                <TextInputWithLabel label={translate('companyStep.legalBusinessName')} containerStyles={[styles.mt4]} />
+                <TextInputWithLabel label={translate('common.companyAddressNoPO')} containerStyles={[styles.mt4]} />
+                <View style={[styles.flexRow, styles.mt4]}>
+                    <View style={[styles.flex2, styles.mr2]}>
+                        <TextInputWithLabel label={translate('common.city')} />
+                    </View>
+                    <View style={[styles.flex1]}>
+                        <Text style={[styles.formLabel]}>{translate('common.state')}</Text>
+                        <StatePicker onChange={() => {}} />
+                    </View>
+                </View>
+                <TextInputWithLabel label={translate('common.zip')} containerStyles={[styles.mt4]} />
+                <TextInputWithLabel
+                    label={translate('common.phoneNumber')}
+                    containerStyles={[styles.mt4]}
+                    keyboardType={CONST.KEYBOARD_TYPE.PHONE_PAD}
+                />
+                <TextInputWithLabel label={translate('companyStep.companyWebsite')} containerStyles={[styles.mt4]} />
+                <TextInputWithLabel label={translate('companyStep.taxIDNumber')} containerStyles={[styles.mt4]} />
+                <Text style={[styles.formLabel, styles.mt4]}>{translate('companyStep.companyType')}</Text>
+                <Picker
+                    items={_.map(CONST.INCORPORATION_TYPES, (label, value) => ({value, label}))}
+                    onChange={() => {}}
+                    placeholder={{value: '', label: 'Type'}}
+                />
+                <View style={[styles.flexRow, styles.mt4]}>
+                    <View style={[styles.flex2, styles.mr2]}>
+                        <TextInputWithLabel label={translate('companyStep.incorporationDate')} />
+                    </View>
+                    <View style={[styles.flex1]}>
+                        <Text style={[styles.formLabel]}>{translate('common.state')}</Text>
+                        <StatePicker onChange={() => {}} />
+                    </View>
+                </View>
+                {/* TODO: incorporation date picker */}
+                <TextInputWithLabel
+                    label={translate('companyStep.industryClassificationCode')}
+                    helpLinkText={translate('common.whatThis')}
+                    helpLinkURL="https://www.naics.com/search/"
+                    containerStyles={[styles.mt4]}
+                />
+                <TextInputWithLabel
+                    label={`Expensify ${translate('common.password')}`}
+                    containerStyles={[styles.mt4]}
+                    secureTextEntry
+                    autoCompleteType="password"
+                    textContentType="password"
+                />
+                <CheckboxWithLabel
+                    isChecked={false}
+                    onPress={() => {}}
+                    LabelComponent={() => (
+                        <>
+                            <Text>{`${translate('companyStep.confirmCompanyIsNot')} `}</Text>
+                            <TextLink href="https://community.expensify.com/discussion/6191/list-of-restricted-businesses">
+                                {`${translate('companyStep.listOfRestrictedBusinesses')}.`}
+                            </TextLink>
+                        </>
+                    )}
+                    style={[styles.mt4]}
+                />
             </View>
-            <TextInputWithLabel label={translate('companyStep.legalBusinessName')} containerStyles={[styles.mt4]} />
-            <TextInputWithLabel label={translate('common.companyAddressNoPO')} containerStyles={[styles.mt4]} />
-            <View style={[styles.flexRow, styles.mt4]}>
-                <View style={[styles.flex2, styles.mr2]}>
-                    <TextInputWithLabel label={translate('common.city')} />
-                </View>
-                <View style={[styles.flex1]}>
-                    <Text style={[styles.formLabel]}>{translate('common.state')}</Text>
-                    <StatePicker onChange={() => {}} />
-                </View>
-            </View>
-            <TextInputWithLabel label={translate('common.zip')} containerStyles={[styles.mt4]} />
-            <TextInputWithLabel
-                label={translate('common.phoneNumber')}
-                containerStyles={[styles.mt4]}
-                keyboardType={CONST.KEYBOARD_TYPE.PHONE_PAD}
-            />
-            <TextInputWithLabel label={translate('companyStep.companyWebsite')} containerStyles={[styles.mt4]} />
-            <TextInputWithLabel label={translate('companyStep.taxIDNumber')} containerStyles={[styles.mt4]} />
-            <Text style={[styles.formLabel, styles.mt4]}>{translate('companyStep.companyType')}</Text>
-            <Picker
-                items={_.map(CONST.INCORPORATION_TYPES, (label, value) => ({value, label}))}
-                onChange={() => {}}
-                placeholder={{value: '', label: 'Type'}}
-            />
-            <View style={[styles.flexRow, styles.mt4]}>
-                <View style={[styles.flex2, styles.mr2]}>
-                    <TextInputWithLabel label={translate('companyStep.incorporationDate')} />
-                </View>
-                <View style={[styles.flex1]}>
-                    <Text style={[styles.formLabel]}>{translate('common.state')}</Text>
-                    <StatePicker onChange={() => {}} />
-                </View>
-            </View>
-            {/* TODO: incorporation date picker */}
-            <TextInputWithLabel
-                label={translate('companyStep.industryClassificationCode')}
-                helpLinkText={translate('common.whatThis')}
-                helpLinkURL="https://www.naics.com/search/"
-                containerStyles={[styles.mt4]}
-            />
-            <TextInputWithLabel
-                label={`Expensify ${translate('common.password')}`}
-                containerStyles={[styles.mt4]}
-                secureTextEntry
-                autoCompleteType="password"
-                textContentType="password"
-            />
-            <CheckboxWithLabel
-                isChecked={false}
-                onPress={() => {}}
-                LabelComponent={() => (
-                    <>
-                        <Text>{`${translate('companyStep.confirmCompanyIsNot')} `}</Text>
-                        <TextLink href="https://community.expensify.com/discussion/6191/list-of-restricted-businesses">
-                            {`${translate('companyStep.listOfRestrictedBusinesses')}.`}
-                        </TextLink>
-                    </>
-                )}
-                style={[styles.mt4]}
-            />
-        </View>
+        </ScrollView>
         <FixedFooter style={[styles.mt5]}>
             <Button
                 success
@@ -99,7 +101,7 @@ const CompanyStep = ({translate}) => (
                 text={translate('common.saveAndContinue')}
             />
         </FixedFooter>
-    </ScrollView>
+    </>
 );
 
 CompanyStep.propTypes = withLocalizePropTypes;
