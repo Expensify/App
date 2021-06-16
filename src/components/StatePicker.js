0ce9827840b32c50a4c41afc2a45a7cca4c87e78
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-06-16 13:49:47 -1000

    Implement submit

--- src/components/StatePicker.js
+++ src/components/StatePicker.js
@@ -10,12 +10,21 @@ const STATES = _.map(CONST.STATES, ({stateISO}) => ({
 }));
 
 const propTypes = {
+    /** A callback method that is called when the value changes and it received the selected value as an argument */
     onChange: PropTypes.func.isRequired,
+
+    /** The value that needs to be selected */
+    value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
-const StatePicker = props => <Picker items={STATES} onChange={props.onChange} />;
+const defaultProps = {
+    value: '',
+};
+
+const StatePicker = props => <Picker items={STATES} onChange={props.onChange} value={props.value} />;
 
 StatePicker.propTypes = propTypes;
+StatePicker.defaultProps = defaultProps;
 StatePicker.displayName = 'StatePicker';
 
 export default StatePicker;
--- src/pages/ReimbursementAccount/CompanyStep.js
+++ src/pages/ReimbursementAccount/CompanyStep.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {View, ScrollView} from 'react-native';
 import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
 import CONST from '../../CONST';
-import {goToWithdrawalAccountSetupStep} from '../../libs/actions/BankAccounts';
+import {goToWithdrawalAccountSetupStep, setupWithdrawalAccount} from '../../libs/actions/BankAccounts';
 import Navigation from '../../libs/Navigation/Navigation';
 import Text from '../../components/Text';
 import TextInputWithLabel from '../../components/TextInputWithLabel';
@@ -16,97 +16,188 @@ import Picker from '../../components/Picker';
 import StatePicker from '../../components/StatePicker';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 
-const CompanyStep = ({translate}) => (
-    <>
-        <HeaderWithCloseButton
-            title={translate('companyStep.headerTitle')}
-            shouldShowBackButton
-            onBackButtonPress={() => goToWithdrawalAccountSetupStep(CONST.BANK_ACCOUNT.STEP.BANK_ACCOUNT)}
-            onCloseButtonPress={Navigation.dismissModal}
-        />
-        <ScrollView style={[styles.flex1, styles.w100]}>
-            <View style={[styles.p4]}>
-                <View style={[styles.alignItemsCenter]}>
-                    <Text>{translate('companyStep.subtitle')}</Text>
-                </View>
-                <TextInputWithLabel label={translate('companyStep.legalBusinessName')} containerStyles={[styles.mt4]} />
-                <TextInputWithLabel label={translate('common.companyAddressNoPO')} containerStyles={[styles.mt4]} />
-                <View style={[styles.flexRow, styles.mt4]}>
-                    <View style={[styles.flex2, styles.mr2]}>
-                        <TextInputWithLabel label={translate('common.city')} />
+class CompanyStep extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.submit = this.submit.bind(this);
+
+        this.state = {
+            companyName: '',
+            addressStreet: '',
+            addressCity: '',
+            addressState: '',
+            addressZipCode: '',
+            companyPhone: '',
+            website: '',
+            companyTaxID: '',
+            incorporationType: '',
+            incorporationDate: '',
+            incorporationState: '',
+            industryCode: '',
+            password: '',
+            hasNoConnectionToCannabis: false,
+        };
+    }
+
+    validate() {
+
+    }
+
+    submit() {
+        if (!this.validate()) {
+            // @Todo implement and show any errors
+            return;
+        }
+
+        setupWithdrawalAccount({...this.state});
+    }
+
+    render() {
+        return (
+            <>
+                <HeaderWithCloseButton
+                    title={this.props.translate('companyStep.headerTitle')}
+                    shouldShowBackButton
+                    onBackButtonPress={() => goToWithdrawalAccountSetupStep(CONST.BANK_ACCOUNT.STEP.BANK_ACCOUNT)}
+                    onCloseButtonPress={Navigation.dismissModal}
+                />
+                <ScrollView style={[styles.flex1, styles.w100]}>
+                    <View style={[styles.p4]}>
+                        <View style={[styles.alignItemsCenter]}>
+                            <Text>{this.props.translate('companyStep.subtitle')}</Text>
+                        </View>
+                        <TextInputWithLabel
+                            label={this.props.translate('companyStep.legalBusinessName')}
+                            containerStyles={[styles.mt4]}
+                            onChange={companyName => this.setState({companyName})}
+                            value={this.state.companyName}
+                        />
+                        <TextInputWithLabel
+                            label={this.props.translate('common.companyAddressNoPO')}
+                            containerStyles={[styles.mt4]}
+                            onChange={addressStreet => this.setState({addressStreet})}
+                            value={this.state.addressStreet}
+                        />
+                        <View style={[styles.flexRow, styles.mt4]}>
+                            <View style={[styles.flex2, styles.mr2]}>
+                                <TextInputWithLabel
+                                    label={this.props.translate('common.city')}
+                                    onChange={addressCity => this.setState({addressCity})}
+                                    value={this.state.addressCity}
+                                />
+                            </View>
+                            <View style={[styles.flex1]}>
+                                <Text style={[styles.formLabel]}>{this.props.translate('common.state')}</Text>
+                                <StatePicker
+                                    onChange={addressState => this.setState({addressState})}
+                                    value={this.state.addressState}
+                                />
+                            </View>
+                        </View>
+                        <TextInputWithLabel
+                            label={this.props.translate('common.zip')}
+                            containerStyles={[styles.mt4]}
+                            onChange={addressZipCode => this.setState({addressZipCode})}
+                            value={this.state.addressZipCode}
+                        />
+                        <TextInputWithLabel
+                            label={this.props.translate('common.phoneNumber')}
+                            containerStyles={[styles.mt4]}
+                            keyboardType={CONST.KEYBOARD_TYPE.PHONE_PAD}
+                            onChange={companyPhone => this.setState({companyPhone})}
+                            value={this.state.companyPhone}
+                        />
+                        <TextInputWithLabel
+                            label={this.props.translate('companyStep.companyWebsite')}
+                            containerStyles={[styles.mt4]}
+                            onChange={website => this.setState({website})}
+                            value={this.state.website}
+                        />
+                        <TextInputWithLabel
+                            label={this.props.translate('companyStep.taxIDNumber')}
+                            containerStyles={[styles.mt4]}
+                            keyboardType={CONST.KEYBOARD_TYPE.PHONE_PAD}
+                            onChange={companyTaxID => this.setState({companyTaxID})}
+                            value={this.state.companyTaxID}
+                        />
+                        <Text style={[styles.formLabel, styles.mt4]}>
+                            {this.props.translate('companyStep.companyType')}
+                        </Text>
+                        <Picker
+                            items={_.map(CONST.INCORPORATION_TYPES, (label, value) => ({value, label}))}
+                            onChange={incorporationType => this.setState({incorporationType})}
+                            value={this.state.incorporationType}
+                            placeholder={{value: '', label: 'Type'}}
+                        />
+                        <View style={[styles.flexRow, styles.mt4]}>
+                            <View style={[styles.flex2, styles.mr2]}>
+                                {/* TODO: Replace with date picker */}
+                                <TextInputWithLabel
+                                    label={this.props.translate('companyStep.incorporationDate')}
+                                    onChange={incorporationDate => this.setState({incorporationDate})}
+                                    value={this.state.incorporationDate}
+                                />
+                            </View>
+                            <View style={[styles.flex1]}>
+                                <Text style={[styles.formLabel]}>{this.props.translate('common.state')}</Text>
+                                <StatePicker
+                                    onChange={incorporationState => this.setState({incorporationState})}
+                                    value={this.state.incorporationState}
+                                />
+                            </View>
+                        </View>
+                        {/* TODO: Replace with NAICS picker */}
+                        <TextInputWithLabel
+                            label={this.props.translate('companyStep.industryClassificationCode')}
+                            helpLinkText={this.props.translate('common.whatThis')}
+                            helpLinkURL="https://www.naics.com/search/"
+                            containerStyles={[styles.mt4]}
+                            onChange={industryCode => this.setState({industryCode})}
+                            value={this.state.industryCode}
+                        />
+                        <TextInputWithLabel
+                            label={`Expensify ${this.props.translate('common.password')}`}
+                            containerStyles={[styles.mt4]}
+                            secureTextEntry
+                            autoCompleteType="password"
+                            textContentType="password"
+                            onChange={password => this.setState({password})}
+                            value={this.state.password}
+                        />
+                        <CheckboxWithLabel
+                            isChecked={this.state.hasNoConnectionToCannabis}
+                            onPress={() => this.setState(prevState => ({
+                                hasNoConnectionToCannabis: !prevState.hasNoConnectionToCannabis,
+                            }))}
+                            LabelComponent={() => (
+                                <>
+                                    <Text>{`${this.props.translate('companyStep.confirmCompanyIsNot')} `}</Text>
+                                    <TextLink
+                                        // eslint-disable-next-line max-len
+                                        href="https://community.expensify.com/discussion/6191/list-of-restricted-businesses"
+                                    >
+                                        {`${this.props.translate('companyStep.listOfRestrictedBusinesses')}.`}
+                                    </TextLink>
+                                </>
+                            )}
+                            style={[styles.mt4]}
+                        />
                     </View>
-                    <View style={[styles.flex1]}>
-                        <Text style={[styles.formLabel]}>{translate('common.state')}</Text>
-                        <StatePicker onChange={() => {}} />
-                    </View>
-                </View>
-                <TextInputWithLabel label={translate('common.zip')} containerStyles={[styles.mt4]} />
-                <TextInputWithLabel
-                    label={translate('common.phoneNumber')}
-                    containerStyles={[styles.mt4]}
-                    keyboardType={CONST.KEYBOARD_TYPE.PHONE_PAD}
-                />
-                <TextInputWithLabel label={translate('companyStep.companyWebsite')} containerStyles={[styles.mt4]} />
-                <TextInputWithLabel label={translate('companyStep.taxIDNumber')} containerStyles={[styles.mt4]} />
-                <Text style={[styles.formLabel, styles.mt4]}>{translate('companyStep.companyType')}</Text>
-                <Picker
-                    items={_.map(CONST.INCORPORATION_TYPES, (label, value) => ({value, label}))}
-                    onChange={() => {}}
-                    placeholder={{value: '', label: 'Type'}}
-                />
-                <View style={[styles.flexRow, styles.mt4]}>
-                    <View style={[styles.flex2, styles.mr2]}>
-                        <TextInputWithLabel label={translate('companyStep.incorporationDate')} />
-                    </View>
-                    <View style={[styles.flex1]}>
-                        <Text style={[styles.formLabel]}>{translate('common.state')}</Text>
-                        <StatePicker onChange={() => {}} />
-                    </View>
-                </View>
-                {/* TODO: incorporation date picker */}
-                <TextInputWithLabel
-                    label={translate('companyStep.industryClassificationCode')}
-                    helpLinkText={translate('common.whatThis')}
-                    helpLinkURL="https://www.naics.com/search/"
-                    containerStyles={[styles.mt4]}
-                />
-                <TextInputWithLabel
-                    label={`Expensify ${translate('common.password')}`}
-                    containerStyles={[styles.mt4]}
-                    secureTextEntry
-                    autoCompleteType="password"
-                    textContentType="password"
-                />
-                <CheckboxWithLabel
-                    isChecked={false}
-                    onPress={() => {}}
-                    LabelComponent={() => (
-                        <>
-                            <Text>{`${translate('companyStep.confirmCompanyIsNot')} `}</Text>
-                            <TextLink
-                                href="https://community.expensify.com/discussion/6191/list-of-restricted-businesses"
-                            >
-                                {`${translate('companyStep.listOfRestrictedBusinesses')}.`}
-                            </TextLink>
-                        </>
-                    )}
-                    style={[styles.mt4]}
-                />
-            </View>
-        </ScrollView>
-        <FixedFooter style={[styles.mt5]}>
-            <Button
-                success
-                onPress={() => {
-                }}
-                style={[styles.w100]}
-                text={translate('common.saveAndContinue')}
-            />
-        </FixedFooter>
-    </>
-);
+                </ScrollView>
+                <FixedFooter style={[styles.mt5]}>
+                    <Button
+                        success
+                        onPress={this.submit}
+                        style={[styles.w100]}
+                        text={this.props.translate('common.saveAndContinue')}
+                    />
+                </FixedFooter>
+            </>
+        );
+    }
+}
 
 CompanyStep.propTypes = withLocalizePropTypes;
-CompanyStep.displayName = 'CompanyStep';
 
 export default withLocalize(CompanyStep);
