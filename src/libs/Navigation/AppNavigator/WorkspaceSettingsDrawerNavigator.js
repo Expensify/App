7f2dd52345ef24462731436d4f3aaaa23e6b7f74
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-06-17 03:30:54 +0530

    refactor Drawer options

--- src/libs/Navigation/AppNavigator/WorkspaceSettingsDrawerNavigator.js
+++ src/libs/Navigation/AppNavigator/WorkspaceSettingsDrawerNavigator.js
@@ -1,42 +1,23 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
-import styles, {getNavigationDrawerStyle, getWorkspaceSettingsDrawerType} from '../../../styles/styles';
+
+// Screens
+import BaseDrawerNavigator from './baseDrawerNavigator';
 import WorkspaceCard from '../../../pages/workspace/WorkspaceCard';
 import WorkspaceSidebar from '../../../pages/workspace/WorkspaceSidebar';
 
-const propTypes = {
-    ...windowDimensionsPropTypes,
-};
-const defaultProps = {};
-const Drawer = createDrawerNavigator();
-
-const WorkspaceSettingsDrawerNavigator = props => (
-    <Drawer.Navigator
-        defaultStatus={props.isSmallScreenWidth ? 'open' : 'closed'}
-        sceneContainerStyle={styles.navigationSceneContainer}
+const WorkspaceSettingsDrawerNavigator = () => (
+    <BaseDrawerNavigator
         drawerContent={() => <WorkspaceSidebar />}
-        screenOptions={{
-            cardStyle: styles.navigationScreenCardStyle,
-            headerShown: false,
-            drawerType: getWorkspaceSettingsDrawerType(props.isSmallScreenWidth),
-            drawerStyle: getNavigationDrawerStyle(
-                props.windowWidth,
-                props.windowHeight,
-                props.isSmallScreenWidth,
-            ),
-            swipeEdgeWidth: 500,
-        }}
-    >
-        <Drawer.Screen
-            name="WorkspaceCard"
-            component={WorkspaceCard}
-        />
-    </Drawer.Navigator>
+        screens={[
+            {
+                name: 'WorkspaceCard',
+                component: WorkspaceCard,
+                initialParams: {},
+            },
+        ]}
+    />
 );
 
-WorkspaceSettingsDrawerNavigator.propTypes = propTypes;
-WorkspaceSettingsDrawerNavigator.defaultProps = defaultProps;
 WorkspaceSettingsDrawerNavigator.displayName = 'WorkspaceSettingsDrawerNavigator';
 
-export default withWindowDimensions(WorkspaceSettingsDrawerNavigator);
+export default WorkspaceSettingsDrawerNavigator;
