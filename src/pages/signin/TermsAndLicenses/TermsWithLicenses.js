8304251f8074a0d9952f5ec6a6dbe228643f5e80
Pranshu Chittora <pranshuchittora17@gmail.com>
2021-06-17 23:53:48 +0530

    fix: Styling fixes

--- src/pages/signin/TermsAndLicenses/TermsWithLicenses.js
+++ src/pages/signin/TermsAndLicenses/TermsWithLicenses.js
@@ -11,30 +11,28 @@ const TermsWithLicenses = ({translate}) => (
         <View style={[styles.mt1, styles.alignItemsCenter, styles.mb3]}>
             <LogoWordmark height={30} width={80} />
         </View>
-        <View style={[styles.dFlex, styles.flexWrap]}>
-            <Text style={[styles.flexColumn, styles.textAlignCenter, styles.loginTermsText]}>
+        <Text style={[styles.textAlignCenter, styles.loginTermsText]}>
+            {translate('termsOfUse.phrase1')}
+            {' '}
+            <TextLink style={[styles.loginTermsText, styles.termsLink]} href={CONST.TERMS_URL}>
+                {translate('termsOfUse.phrase2')}
+            </TextLink>
+            {' '}
+            {translate('termsOfUse.phrase3')}
+            {' '}
+            <TextLink style={[styles.loginTermsText, styles.termsLink]} href={CONST.PRIVACY_URL}>
+                {translate('termsOfUse.phrase4')}
+            </TextLink>
+            .
+            {' '}
+            {translate('termsOfUse.phrase5')}
+            {' '}
+            <TextLink style={[styles.loginTermsText, styles.termsLink]} href={CONST.LICENSES_URL}>
+                {translate('termsOfUse.phrase6')}
+            </TextLink>
+            .
+        </Text>
 
-                {translate('termsOfUse.phrase1')}
-                {' '}
-                <TextLink style={[styles.loginTermsText, styles.termsLink]} href={CONST.TERMS_URL}>
-                    {translate('termsOfUse.phrase2')}
-                </TextLink>
-                {' '}
-                {translate('termsOfUse.phrase3')}
-                {' '}
-                <TextLink style={[styles.loginTermsText, styles.termsLink]} href={CONST.PRIVACY_URL}>
-                    {translate('termsOfUse.phrase4')}
-                </TextLink>
-                .
-                {' '}
-                {translate('termsOfUse.phrase5')}
-                {' '}
-                <TextLink style={[styles.loginTermsText, styles.termsLink]} href={CONST.LICENSES_URL}>
-                    {translate('termsOfUse.phrase6')}
-                </TextLink>
-                .
-            </Text>
-        </View>
     </View>
 );
 
