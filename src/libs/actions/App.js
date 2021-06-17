8b8d661ec4589564d1e0a9fce25a62bbc295f634
Rory Abraham <rory@expensify.com>
2021-06-17 08:37:57 -0700

    Fix language selector

--- src/libs/actions/App.js
+++ src/libs/actions/App.js
@@ -8,7 +8,15 @@ function setCurrentURL(url) {
     Onyx.set(ONYXKEYS.CURRENT_URL, url);
 }
 
+/**
+ * @param {String} locale
+ */
+function setLocale(locale) {
+    console.log('RORY_DEBUG setting locale to:', locale);
+    Onyx.set(ONYXKEYS.PREFERRED_LOCALE, locale);
+}
+
 export {
-    // eslint-disable-next-line import/prefer-default-export
     setCurrentURL,
+    setLocale,
 };
