0ff478ea554e3733db01d95b826dcdc4250354bd
Amal Nazeem <amal@expensify.com>
2021-06-21 10:33:39 -0700

    fix comment typos and accomodate recent policy id name change

--- src/components/optionPropTypes.js
+++ src/components/optionPropTypes.js
@@ -31,6 +31,6 @@ export default PropTypes.shape({
     // Whether the report is pinned or not
     isPinned: PropTypes.bool,
 
-    // Whether the report is corresponds to a default room
+    // Whether the report corresponds to a default room
     isDefaultChatRoom: PropTypes.bool,
 });
