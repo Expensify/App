ba72db343d4faf874527ca25c657e68c199881aa
Jasper Huang <jasper@expensify.com>
2021-06-21 18:46:14 +0800

    Update src/languages/es.js
    Co-authored-by: Horus Lugo <horus@expensify.com>

--- src/languages/es.js
+++ src/languages/es.js
@@ -356,7 +356,7 @@ export default {
         requestACall: 'Llámame por teléfono',
         description: 'Necesitas ayuda configurando tu cuenta? Nuestro equipo de guías te pueden ayudar.',
         instructions: 'Escribe tu nombre y número de teléfono y te llamaremos.',
-        availabilityText: '*Nuestros guías están disponibles de domingo desde las 17.00 CT a viernes hasta las 17.00 CT. Si solicitas una llamada fuera de este horario, te llamaremos de lunes a viernes de 9.00 a 17.00 en tu hora local. El orden de llamada corresponde con el orden en el que las llamadas son recibidas.',
+        availabilityText: '*Nuestros guías están disponibles de domingo desde las 17.00 CT a viernes hasta las 17.00 CT. Si solicitas una llamada fuera de este horario, te llamaremos de lunes a viernes de 9.00 a 17.00 en tu hora local. El orden de llamada corresponde con el orden de solicitud.',
         callMe: 'Llámame',
         growlMessageOnSave: 'Llamada perdida.',
         growlMessageInvalidPhone: 'El teléfono no es valido. Intenta nuevamente agregando el código de país. Eg: +15005550006',
