c5f7418e838a33f2f8e221ac234c8fef165210a5
Brandon Stites <bstites+1@expensify.com>
2021-06-21 19:47:38 -0600

    Move getting the paypal.me nvp to getPaymentMethods

--- src/libs/actions/PaymentMethods.js
+++ src/libs/actions/PaymentMethods.js
@@ -2,16 +2,19 @@ import lodashGet from 'lodash/get';
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 import * as API from '../API';
+import CONST from '../../CONST';
 
 function getPaymentMethods() {
     API.Get({
-        returnValueList: 'bankAccountList, cardList, userWallet',
+        returnValueList: 'bankAccountList, cardList, userWallet, nameValuePairs',
+        name: 'paypalMeAddress',
     })
         .then((response) => {
             Onyx.multiSet({
                 [ONYXKEYS.USER_WALLET]: lodashGet(response, 'userWallet', null),
                 [ONYXKEYS.BANK_ACCOUNT_LIST]: lodashGet(response, 'bankAccountList', []),
                 [ONYXKEYS.CARD_LIST]: lodashGet(response, 'cardList', []),
+                [ONYXKEYS.NVP_PAYPAL_ME_ADDRESS]: lodashGet(response, `nameValuePairs.${CONST.NVP.PAYPAL_ME_ADDRESS}`, ''),
             });
         });
 }
--- src/libs/actions/User.js
+++ src/libs/actions/User.js
@@ -62,18 +62,13 @@ function getBetas() {
  */
 function getUserDetails() {
     API.Get({
-        returnValueList: 'account, loginList, nameValuePairs',
-        name: CONST.NVP.PAYPAL_ME_ADDRESS,
+        returnValueList: 'account, loginList',
     })
         .then((response) => {
             // Update the User onyx key
             const loginList = _.where(response.loginList, {partnerName: 'expensify.com'});
             const expensifyNewsStatus = lodashGet(response, 'account.subscribed', true);
             Onyx.merge(ONYXKEYS.USER, {loginList, expensifyNewsStatus});
-
-            // Update the nvp_payPalMeAddress NVP
-            const payPalMeAddress = lodashGet(response, `nameValuePairs.${CONST.NVP.PAYPAL_ME_ADDRESS}`, '');
-            Onyx.merge(ONYXKEYS.NVP_PAYPAL_ME_ADDRESS, payPalMeAddress);
         });
 }
 
