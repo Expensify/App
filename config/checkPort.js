434ffaf2b809903c312566fb71b995d2983d198f
Horus Lugo López <horus@expensify.com>
2021-06-22 17:48:52 +0200

    Fix code style

--- config/checkPort.js
+++ config/checkPort.js
@@ -3,29 +3,26 @@ const net = require('net');
 function checkPort(port, host) {
     return new Promise((resolve, reject) => {
         const server = net.createServer();
-
         server.once('error', (err) => {
-            if (err.code !== 'EADDRINUSE') { return reject(err); }
+            if (err.code !== 'EADDRINUSE') {
+                return reject(err);
+            }
 
             reject(err);
         });
-
         server.once('listening', () => {
-            server.once('close', () => { resolve(false); });
+            server.once('close', () => resolve(false));
             server.close();
         });
-
         server.listen(port, host);
     });
 }
 
 const inputPort = Number(process.argv.slice(2)[0]);
-
 if (Number.isNaN(inputPort) || inputPort < 0 || inputPort > 65535) {
     console.error('This command requires an argument that must be a number [0-65535]\n');
     process.exit(1);
 }
-
 
 Promise.all([
     checkPort(inputPort, 'localhost'),
@@ -38,6 +35,5 @@ Promise.all([
             `The port ${inputPort} is currently in use.`,
             `You can run \`lsof -i :${inputPort}\` to see which program is using it.\n`,
         );
-
         process.exit(1);
     });
