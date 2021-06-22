dc913f217e9ffe86185760de68d298a5df8a3771
Nikki Wines <nikkiwines@expensify.com>
2021-06-21 17:13:28 -0700

    add onfido portal

--- src/pages/ReimbursementAccount/RequestorStep.js
+++ src/pages/ReimbursementAccount/RequestorStep.js
@@ -14,6 +14,7 @@ import Button from '../../components/Button';
 import FixedFooter from '../../components/FixedFooter';
 import IdentityForm from './IdentityForm';
 import Growl from '../../libs/Growl';
+import Onfido from '../../components/Onfido';
 
 class RequestorStep extends React.Component {
     constructor(props) {
@@ -31,6 +32,8 @@ class RequestorStep extends React.Component {
             dob: lodashGet(props, ['achData', 'dob'], ''),
             ssnLast4: lodashGet(props, ['achData', 'ssnLast4'], ''),
             isControllingOfficer: lodashGet(props, ['achData', 'isControllingOfficer'], false),
+            onfidoData: lodashGet(props, ['achData', 'onfidoData'], ''),
+            isOnfidoSetupComplete: lodashGet(props, ['achData', 'isOnfidoSetupComplete'], false),
         };
     }
 
@@ -64,92 +67,111 @@ class RequestorStep extends React.Component {
     render() {
         return (
             <>
-                <HeaderWithCloseButton
-                    title={this.props.translate('requestorStep.headerTitle')}
-                    shouldShowBackButton
-                    onBackButtonPress={() => goToWithdrawalAccountSetupStep(CONST.BANK_ACCOUNT.STEP.COMPANY_STEP)}
-                    onCloseButtonPress={Navigation.dismissModal}
+                {!this.props.achData.useOnfido && !this.props.achData.sdkToken}
+                ? (
+                <Onfido
+                    sdkToken={this.props.achData.sdkToken}
+                    onUserExit={() => {
+                        goToWithdrawalAccountSetupStep(CONST.BANK_ACCOUNT.STEP.REQUESTOR);
+                    }}
+                    onSuccess={(onfidoData) => {
+                        this.setState({
+                            onFidoData: JSON.stringify(onfidoData),
+                            isOnfidoSetupComplete: true,
+                        });
+                        this.submit();
+                    }}
                 />
-                <ScrollView style={[styles.flex1, styles.w100]}>
-                    <View style={[styles.p4]}>
-                        <IdentityForm
-                            onFieldChange={(field, value) => this.onFieldChange(field, value)}
-                            values={{
-                                firstName: this.state.firstName,
-                                lastName: this.state.lastName,
-                                street: this.state.street,
-                                city: this.state.city,
-                                state: this.state.state,
-                                zipCode: this.state.zipCode,
-                                dob: this.state.dob,
-                                ssnLast4: this.state.ssnLast4,
-                            }}
-                        />
-                        <CheckboxWithLabel
-                            isChecked={this.state.isControllingOfficer}
-                            onPress={() => this.setState(prevState => ({
-                                isControllingOfficer: !prevState.isControllingOfficer,
-                            }))}
-                            LabelComponent={() => (
-                                <View style={[styles.flex1, styles.pr1]}>
-                                    <Text>
-                                        {this.props.translate('requestorStep.isControllingOfficer')}
-                                    </Text>
-                                </View>
-                            )}
-                            style={[styles.mt4]}
-                        />
-                        <Text style={[styles.textMicroSupporting, styles.mt5]}>
-                            {this.props.translate('requestorStep.financialRegulations')}
-                            <TextLink
-                                style={styles.textMicro}
-                                // eslint-disable-next-line max-len
-                                href="https://community.expensify.com/discussion/6983/faq-why-do-i-need-to-provide-personal-documentation-when-setting-up-updating-my-bank-account"
-                            >
-                                {`${this.props.translate('requestorStep.learnMore')}`}
-                            </TextLink>
-                            {' | '}
-                            <TextLink
-                                style={styles.textMicro}
-                                // eslint-disable-next-line max-len
-                                href="https://community.expensify.com/discussion/5677/deep-dive-security-how-expensify-protects-your-information"
-                            >
-                                {`${this.props.translate('requestorStep.isMyDataSafe')}`}
-                            </TextLink>
-                        </Text>
-                        <Text style={[styles.mt3, styles.textMicroSupporting]}>
-                            {this.props.translate('requestorStep.onFidoConditions')}
-                            <TextLink
-                                style={styles.textMicro}
-                                href="https://onfido.com/facial-scan-policy-and-release/"
-                            >
-                                {`${this.props.translate('requestorStep.onFidoFacialScan')}`}
-                            </TextLink>
-                            {', '}
-                            <TextLink
-                                style={styles.textMicro}
-                                href="https://onfido.com/privacy/"
-                            >
-                                {`${this.props.translate('common.privacyPolicy')}`}
-                            </TextLink>
-                            {` ${this.props.translate('common.and')} `}
-                            <TextLink
-                                style={styles.textMicro}
-                                href="https://onfido.com/terms-of-service/"
-                            >
-                                {`${this.props.translate('common.termsOfService')}`}
-                            </TextLink>
-                        </Text>
-                    </View>
-                </ScrollView>
-                <FixedFooter style={[styles.mt5]}>
-                    <Button
-                        success
-                        onPress={this.submit}
-                        style={[styles.w100]}
-                        text={this.props.translate('common.saveAndContinue')}
+                ) : (
+                <>
+                    <HeaderWithCloseButton
+                        title={this.props.translate('requestorStep.headerTitle')}
+                        shouldShowBackButton
+                        onBackButtonPress={() => goToWithdrawalAccountSetupStep(CONST.BANK_ACCOUNT.STEP.COMPANY_STEP)}
+                        onCloseButtonPress={Navigation.dismissModal}
                     />
-                </FixedFooter>
+                    <ScrollView style={[styles.flex1, styles.w100]}>
+                        <View style={[styles.p4]}>
+                            <IdentityForm
+                                onFieldChange={(field, value) => this.onFieldChange(field, value)}
+                                values={{
+                                    firstName: this.state.firstName,
+                                    lastName: this.state.lastName,
+                                    street: this.state.street,
+                                    city: this.state.city,
+                                    state: this.state.state,
+                                    zipCode: this.state.zipCode,
+                                    dob: this.state.dob,
+                                    ssnLast4: this.state.ssnLast4,
+                                }}
+                            />
+                            <CheckboxWithLabel
+                                isChecked={this.state.isControllingOfficer}
+                                onPress={() => this.setState(prevState => ({
+                                    isControllingOfficer: !prevState.isControllingOfficer,
+                                }))}
+                                LabelComponent={() => (
+                                    <View style={[styles.flex1, styles.pr1]}>
+                                        <Text>
+                                            {this.props.translate('requestorStep.isControllingOfficer')}
+                                        </Text>
+                                    </View>
+                                )}
+                                style={[styles.mt4]}
+                            />
+                            <Text style={[styles.textMicroSupporting, styles.mt5]}>
+                                {this.props.translate('requestorStep.financialRegulations')}
+                                <TextLink
+                                    style={styles.textMicro}
+                                    // eslint-disable-next-line max-len
+                                    href="https://community.expensify.com/discussion/6983/faq-why-do-i-need-to-provide-personal-documentation-when-setting-up-updating-my-bank-account"
+                                >
+                                    {`${this.props.translate('requestorStep.learnMore')}`}
+                                </TextLink>
+                                {' | '}
+                                <TextLink
+                                    style={styles.textMicro}
+                                    // eslint-disable-next-line max-len
+                                    href="https://community.expensify.com/discussion/5677/deep-dive-security-how-expensify-protects-your-information"
+                                >
+                                    {`${this.props.translate('requestorStep.isMyDataSafe')}`}
+                                </TextLink>
+                            </Text>
+                            <Text style={[styles.mt3, styles.textMicroSupporting]}>
+                                {this.props.translate('requestorStep.onFidoConditions')}
+                                <TextLink
+                                    style={styles.textMicro}
+                                    href="https://onfido.com/facial-scan-policy-and-release/"
+                                >
+                                    {`${this.props.translate('requestorStep.onFidoFacialScan')}`}
+                                </TextLink>
+                                {', '}
+                                <TextLink
+                                    style={styles.textMicro}
+                                    href="https://onfido.com/privacy/"
+                                >
+                                    {`${this.props.translate('common.privacyPolicy')}`}
+                                </TextLink>
+                                {` ${this.props.translate('common.and')} `}
+                                <TextLink
+                                    style={styles.textMicro}
+                                    href="https://onfido.com/terms-of-service/"
+                                >
+                                    {`${this.props.translate('common.termsOfService')}`}
+                                </TextLink>
+                            </Text>
+                        </View>
+                    </ScrollView>
+                    <FixedFooter style={[styles.mt5]}>
+                        <Button
+                            success
+                            onPress={this.submit}
+                            style={[styles.w100]}
+                            text={this.props.translate('common.saveAndContinue')}
+                        />
+                    </FixedFooter>
+                </>
+                )
             </>
         );
     }
