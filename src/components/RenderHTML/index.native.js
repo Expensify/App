e2fc7ec8eab854f078e34536c7e73554fe3fd52f
Viacheslav Surzhikov <syva80@yandex.ru>
2021-06-24 13:41:57 +0300

    fix errors

--- src/components/RenderHTML/index.native.js
+++ src/components/RenderHTML/index.native.js
@@ -1,12 +1,19 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import HTML from './BaseRenderHTML';
+import BaseRenderHTML from './BaseRenderHTML';
+import {
+    propTypes,
+    defaultProps,
+} from './renderHTMLPropTypes';
 
-const RenderHTML = props => (
-    <HTML
-        {...props}
+const RenderHTML = ({html, debag}) => (
+    <BaseRenderHTML
+        html={html}
+        debag={debag}
     />
 );
+
+RenderHTML.displayName = 'RenderHTML';
+RenderHTML.propTypes = propTypes;
+RenderHTML.defaultProps = defaultProps;
 
 export default RenderHTML;
