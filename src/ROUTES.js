1075f4b6c713eb287555c8b8975ed96c48f8e12c
Brandon Stites <bstites+1@expensify.com>
2021-06-29 13:50:42 -0600

    Kebab case

--- src/ROUTES.js
+++ src/ROUTES.js
@@ -19,7 +19,7 @@ export default {
     SETTINGS_ABOUT: 'settings/about',
     SETTINGS_APP_DOWNLOAD_LINKS: 'settings/about/app-download-links',
     SETTINGS_PAYMENTS: 'settings/payments',
-    SETTINGS_ADD_PAYPAL_ME: 'settings/payments/addPayPalMe',
+    SETTINGS_ADD_PAYPAL_ME: 'settings/payments/add-paypal-me',
     SETTINGS_ADD_LOGIN: 'settings/addlogin/:type',
     getSettingsAddLoginRoute: type => `settings/addlogin/${type}`,
     NEW_GROUP: 'new/group',
