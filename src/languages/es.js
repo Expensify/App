df2ef38320dbff8eb51f513585ab1629fee2ade1
Vit Horacek <vit@expensify.com>
2021-06-30 15:08:12 +0100

    Update finalized Spanish translations

--- src/languages/es.js
+++ src/languages/es.js
@@ -369,10 +369,10 @@ export default {
             genericFailureMessage: 'Se ha producido un error al intentar crear el Workspace. Por favor, inténtalo de nuevo.',
         },
         people: {
-            assignee: 'Assignee',
-            genericFailureMessage: 'An error occurred removing an user from the workspace, please try again.',
-            removeMembersPrompt: 'Are you sure you want to remove the selected people from your workspace?',
-            removeMembersTitle: 'Remove Members',
+            assignee: 'Persona asignada',
+            genericFailureMessage: 'Se ha producido un error al intentar eliminar a un usuario del espacio de trabajo. Por favor inténtalo más tarde.',
+            removeMembersPrompt: '¿Estás seguro que quieres eliminar a las personas seleccionadas de tu espacio de trabajo?',
+            removeMembersTitle: 'Eliminar miembros',
         },
         card: {
             addEmail: 'Agregar correo electrónico',
