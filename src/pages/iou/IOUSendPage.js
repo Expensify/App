2cbcc79c3653102d04711fdf69c74e0f65414297
Nicholas Murray <nmurray@expensify.com>
2021-07-01 16:54:38 -0700

    Add in IOUModal steps and new prop type

--- src/pages/iou/IOUSendPage.js
+++ src/pages/iou/IOUSendPage.js
@@ -2,4 +2,4 @@ import React from 'react';
 import IOUModal from './IOUModal';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-export default props => <IOUModal {...props} />;
+export default props => <IOUModal {...props} iouType="send" />;
