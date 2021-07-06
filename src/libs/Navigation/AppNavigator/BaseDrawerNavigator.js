415b816c408e84d0f8c6dfc814e1daf4202347d0
Horus Lugo López <horus@expensify.com>
2021-07-06 16:57:13 +0200

    Inset the full screen modal used for workspaces on large devices

--- src/libs/Navigation/AppNavigator/BaseDrawerNavigator.js
+++ src/libs/Navigation/AppNavigator/BaseDrawerNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import _ from 'underscore';
 import PropTypes from 'prop-types';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {View} from 'react-native';
 import styles, {getNavigationDrawerStyle, getNavigationDrawerType} from '../../../styles/styles';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
 
@@ -27,33 +28,45 @@ const propTypes = {
 };
 const Drawer = createDrawerNavigator();
 
-const BaseDrawerNavigator = props => (
-    <Drawer.Navigator
-        defaultStatus={props.isSmallScreenWidth ? 'open' : 'closed'}
-        sceneContainerStyle={styles.navigationSceneContainer}
-        drawerContent={props.drawerContent}
-        screenOptions={{
-            cardStyle: styles.navigationScreenCardStyle,
-            headerShown: false,
-            drawerType: getNavigationDrawerType(props.isSmallScreenWidth),
-            drawerStyle: getNavigationDrawerStyle(
-                props.windowWidth,
-                props.windowHeight,
-                props.isSmallScreenWidth,
-            ),
-            swipeEdgeWidth: 500,
-        }}
-    >
-        {_.map(props.screens, screen => (
-            <Drawer.Screen
-                key={screen.name}
-                name={screen.name}
-                component={screen.component}
-                initialParams={screen.initialParams}
-            />
-        ))}
-    </Drawer.Navigator>
-);
+const BaseDrawerNavigator = (props) => {
+    const content = (
+        <Drawer.Navigator
+            defaultStatus={props.isSmallScreenWidth ? 'open' : 'closed'}
+            sceneContainerStyle={styles.navigationSceneContainer}
+            drawerContent={props.drawerContent}
+            screenOptions={{
+                cardStyle: styles.navigationScreenCardStyle,
+                headerShown: false,
+                drawerType: getNavigationDrawerType(props.isSmallScreenWidth),
+                drawerStyle: getNavigationDrawerStyle(
+                    props.windowWidth,
+                    props.windowHeight,
+                    props.isSmallScreenWidth,
+                ),
+                swipeEdgeWidth: 500,
+            }}
+        >
+            {_.map(props.screens, screen => (
+                <Drawer.Screen
+                    key={screen.name}
+                    name={screen.name}
+                    component={screen.component}
+                    initialParams={screen.initialParams}
+                />
+            ))}
+        </Drawer.Navigator>
+    );
+
+    if (!props.isSmallScreenWidth) {
+        return (
+            <View style={styles.navigationSceneFullScreenWrapper}>
+                {content}
+            </View>
+        );
+    }
+
+    return content;
+};
 
 BaseDrawerNavigator.propTypes = propTypes;
 BaseDrawerNavigator.displayName = 'BaseDrawerNavigator';
