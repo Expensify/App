6315c42b3c3765acdba54a50a7fc26f07bcf08ea
Horus Lugo López <horus@expensify.com>
2021-07-07 12:18:55 +0200

    Move CardOverlay styles to the styles.js file

--- src/components/CardOverlay.js
+++ src/components/CardOverlay.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
-import themeColors from '../styles/themes/default';
+import styles from '../styles/styles';
 
-const CardOverlay = () => (
-    <View
-        style={{
-            backgroundColor: themeColors.modalBackdrop,
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            opacity: 0.5,
-        }}
-    />
-);
+const CardOverlay = () => <View style={styles.cardOverlay} />;
 
 export default CardOverlay;
