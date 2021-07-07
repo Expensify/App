d3d6dc5fe270045dc4772814599157633539e863
Horus Lugo López <horus@expensify.com>
2021-07-07 12:18:55 +0200

    Move CardOverlay styles to the styles.js file

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1746,6 +1746,16 @@ const styles = {
         lineHeight: 16,
         ...whiteSpace.noWrap,
     },
+
+    cardOverlay: {
+        backgroundColor: themeColors.modalBackdrop,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        opacity: 0.5,
+    },
 };
 
 const baseCodeTagStyles = {
