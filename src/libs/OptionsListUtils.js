adb4ffd10288cba837847c4266b5a2ceee9c417b
Santhoshkumar Sellavel <Santhoshsellavel@gmail.com>
2021-07-13 02:53:08 +0530

    Restricted requesting Payment to self & Split with self.

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -23,6 +23,12 @@ Onyx.connect({
     callback: val => currentUserLogin = val && val.email,
 });
 
+let currentUser;
+Onyx.connect({
+    key: ONYXKEYS.USER,
+    callback: val => currentUser = val,
+});
+
 let countryCodeByIP;
 Onyx.connect({
     key: ONYXKEYS.COUNTRY_CODE,
@@ -217,7 +223,8 @@ function createOption(personalDetailList, report, draftComments, {
 
         // It doesn't make sense to provide a login in the case of a report with multiple participants since
         // there isn't any one single login to refer to for a report.
-        login: !hasMultipleParticipants ? personalDetail.login : null,
+        // If single login is a mobile number, appending SMS domain
+        login: !hasMultipleParticipants ? addSMSDomainIfPhoneNumber(personalDetail.login) : null,
         reportID: report ? report.reportID : null,
         isUnread: report ? report.unreadActionCount > 0 : null,
         hasDraftComment: _.size(reportDraftComment) > 0,
@@ -673,6 +680,36 @@ function getReportIcons(report, personalDetails) {
         .map(item => item.avatar);
 }
 
+/**
+ * Returns the given userToInvite is currentUser or not.
+ * @param {Object} userToInvite
+ * @returns {Bool}
+ */
+
+ function isCurrentUser(userToInvite) {
+    if (!userToInvite) {
+        // If userToInvite is null or undefined
+        return false;
+    }
+
+    // If user login is mobile number, append sms domain if not appended already just a fail safe.
+    const userToInviteLogin = addSMSDomainIfPhoneNumber(userToInvite.login);
+
+    // Initial check with currentUserLogin
+    let result = currentUserLogin.toLowerCase() === userToInviteLogin.toLowerCase();
+    const {loginList} = currentUser;
+    let index = 0;
+
+    // Checking userToInviteLogin against to current user login options.
+    while (index < loginList.length && !result) {
+        if (loginList[index].partnerUserID.toLowerCase() === userToInviteLogin.toLowerCase()) {
+            result = true;
+        }
+        index++;
+    }
+    return result;
+}
+
 export {
     getSearchOptions,
     getNewChatOptions,
@@ -685,4 +722,5 @@ export {
     getIOUConfirmationOptionsFromParticipants,
     getDefaultAvatar,
     getReportIcons,
+    isCurrentUser,
 };
