c3ad5fb845523a80b30d303b38176d602517eda4
Rushat Gabhane <rushatgabhane@gmail.com>
2021-07-12 08:36:50 +0300

    convert to class comp and use KeyboardShortcut

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,44 +1,54 @@
 import _ from 'underscore';
-import React, {useEffect} from 'react';
+import React, {Component} from 'react';
 import {
-    Pressable, ActivityIndicator,
+    Pressable, ActivityIndicator, InteractionManager
 } from 'react-native';
 import {propTypes, defaultProps} from './ButtonPropTypes';
 import styles from '../../styles/styles';
 import themeColors from '../../styles/themes/default';
 import OpacityView from '../OpacityView';
 import Text from '../Text';
+import KeyboardShortcut from '../../libs/KeyboardShortcut';
 
-const Button = (props) => {
-    const additionalStyles = _.isArray(props.style) ? props.style : [props.style];
+class Button extends Component {
+    constructor(props) {
+        super(props);
+        this.additionalStyles = _.isArray(this.props.style) ? this.props.style : [this.props.style];
 
-    /**
-     * Call props.onPress when Enter key is pressed
-     *
-     * @param {Object} event
-     */
-    function handleKeydown(event) {
-        if (event.key === 'Enter') {
-            props.onPress();
-        }
+        this.renderContent = this.renderContent.bind(this);
     }
 
-    useEffect(() => {
-        if (props.pressOnEnter && props.onPress && !props.isDisabled && !props.isLoading) {
-            document.addEventListener('keydown', handleKeydown);
-            return () => {
-                document.removeEventListener('keydown', handleKeydown);
-            };
+    componentDidMount() {
+        if (!this.props.pressOnEnter) {
+            return;
         }
-    }, [props.isDisabled, props.isLoading]);
+        // Component is not initialized yet due to navigation transitions
+        // Wait until interactions are complete before trying to attach listener
+        InteractionManager.runAfterInteractions(() => {
+            // Setup and attach keypress handler for pressing the button with Enter key
+            this.unsubscribe = KeyboardShortcut.subscribe('Enter', () => {
+                if (this.props.pressOnEnter && !this.props.isDisabled && !this.props.isLoading) {
+                    this.props.onPress();
+                }
+            }, [], true);
+        });
+    }
 
-    function renderContent() {
-        const {ContentComponent} = props;
+    componentWillUnmount() {
+        // Cleanup event listeners
+        if (!this.unsubscribe) {
+            return;
+        }
+        this.unsubscribe();
+    }
+
+    renderContent() {
+        const {ContentComponent} = this.props;
         if (ContentComponent) {
             return <ContentComponent />;
         }
 
-        return props.isLoading
+        return this.props.isLoading
             ? (
                 <ActivityIndicator color={themeColors.textReversed} />
             ) : (
@@ -46,49 +56,51 @@ const Button = (props) => {
                     selectable={false}
                     style={[
                         styles.buttonText,
-                        props.small && styles.buttonSmallText,
-                        props.large && styles.buttonLargeText,
-                        props.success && styles.buttonSuccessText,
-                        props.danger && styles.buttonDangerText,
-                        ...props.textStyles,
+                        this.props.small && styles.buttonSmallText,
+                        this.props.large && styles.buttonLargeText,
+                        this.props.success && styles.buttonSuccessText,
+                        this.props.danger && styles.buttonDangerText,
+                        ...this.props.textStyles,
                     ]}
                 >
-                    {props.text}
+                    {this.props.text}
                 </Text>
             );
     }
 
-    return (
-        <Pressable
-            onPress={props.onPress}
-            disabled={props.isLoading || props.isDisabled}
-            style={[
-                ...additionalStyles,
-            ]}
-        >
-            {({pressed, hovered}) => (
-                <OpacityView
-                    shouldDim={pressed}
-                    style={[
-                        styles.button,
-                        props.small ? styles.buttonSmall : undefined,
-                        props.large ? styles.buttonLarge : undefined,
-                        props.success ? styles.buttonSuccess : undefined,
-                        props.danger ? styles.buttonDanger : undefined,
-                        (props.isDisabled && props.danger) ? styles.buttonDangerDisabled : undefined,
-                        (props.isDisabled && !props.danger) ? styles.buttonDisable : undefined,
-                        (props.success && hovered) ? styles.buttonSuccessHovered : undefined,
-                        (props.danger && hovered) ? styles.buttonDangerHovered : undefined,
-                        props.shouldRemoveRightBorderRadius ? styles.noRightBorderRadius : undefined,
-                        props.shouldRemoveLeftBorderRadius ? styles.noLeftBorderRadius : undefined,
-                    ]}
-                >
-                    {renderContent()}
-                </OpacityView>
-            )}
-        </Pressable>
-    );
-};
+    render() {
+        return (
+            <Pressable
+                onPress={this.props.onPress}
+                disabled={this.props.isLoading || this.props.isDisabled}
+                style={[
+                    ...this.additionalStyles,
+                ]}
+            >
+                {({pressed, hovered}) => (
+                    <OpacityView
+                        shouldDim={pressed}
+                        style={[
+                            styles.button,
+                            this.props.small ? styles.buttonSmall : undefined,
+                            this.props.large ? styles.buttonLarge : undefined,
+                            this.props.success ? styles.buttonSuccess : undefined,
+                            this.props.danger ? styles.buttonDanger : undefined,
+                            (this.props.isDisabled && this.props.danger) ? styles.buttonDangerDisabled : undefined,
+                            (this.props.isDisabled && !this.props.danger) ? styles.buttonDisable : undefined,
+                            (this.props.success && hovered) ? styles.buttonSuccessHovered : undefined,
+                            (this.props.danger && hovered) ? styles.buttonDangerHovered : undefined,
+                            this.props.shouldRemoveRightBorderRadius ? styles.noRightBorderRadius : undefined,
+                            this.props.shouldRemoveLeftBorderRadius ? styles.noLeftBorderRadius : undefined,
+                        ]}
+                    >
+                        {this.renderContent()}
+                    </OpacityView>
+                )}
+            </Pressable>
+        );
+    }
+}
 
 Button.propTypes = propTypes;
 Button.defaultProps = defaultProps;
