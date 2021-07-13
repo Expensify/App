f3e07850787b416b026c38e5231f0e407c639e4a
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-07-13 21:39:15 +0530

    fix: scrolling issues

--- src/pages/workspace/WorkspaceInvitePage.js
+++ src/pages/workspace/WorkspaceInvitePage.js
@@ -91,44 +91,42 @@ class WorkspaceInvitePage extends React.Component {
                         onCloseButtonPress={Navigation.dismissModal}
                     />
                     <ScrollView style={styles.flex1} contentContainerStyle={styles.p5}>
-                        <View style={styles.flexGrow1}>
-                            <Text style={[styles.mb6]}>
-                                {this.props.translate('workspace.invite.invitePeoplePrompt')}
+                        <Text style={[styles.mb6]}>
+                            {this.props.translate('workspace.invite.invitePeoplePrompt')}
+                        </Text>
+                        <View style={styles.mb6}>
+                            <Text style={[styles.mb2]}>
+                                {this.props.translate('workspace.invite.enterEmailOrPhone')}
                             </Text>
-                            <View style={styles.mb6}>
-                                <Text style={[styles.mb2]}>
-                                    {this.props.translate('workspace.invite.enterEmailOrPhone')}
-                                </Text>
-                                <TextInput
-                                    autoCompleteType="email"
-                                    autoCorrect={false}
-                                    autoCapitalize="none"
-                                    style={[styles.textInput]}
-                                    value={this.state.emailOrPhone}
-                                    keyboardType={getEmailKeyboardType()}
-                                    onChangeText={text => this.setState({emailOrPhone: text})}
-                                />
-                            </View>
-                            <View style={styles.mb6}>
-                                <Text style={[styles.mb2]}>
-                                    {this.props.translate('workspace.invite.personalMessagePrompt')}
-                                </Text>
-                                <TextInput
-                                    autoCompleteType="off"
-                                    autoCorrect={false}
-                                    style={[styles.textInput, styles.workspaceInviteWelcome, styles.mb6]}
-                                    numberOfLines={10}
-                                    textAlignVertical="top"
-                                    multiline
-                                    value={this.state.welcomeNote}
-                                    placeholder={this.getWelcomeNotePlaceholder()}
-                                    placeholderTextColor={themeColors.placeholderText}
-                                    onChangeText={text => this.setState({welcomeNote: text})}
-                                />
-                                <TextLink href="https://use.expensify.com/privacy">
-                                    {this.props.translate('common.privacy')}
-                                </TextLink>
-                            </View>
+                            <TextInput
+                                autoCompleteType="email"
+                                autoCorrect={false}
+                                autoCapitalize="none"
+                                style={[styles.textInput]}
+                                value={this.state.emailOrPhone}
+                                keyboardType={getEmailKeyboardType()}
+                                onChangeText={text => this.setState({emailOrPhone: text})}
+                            />
+                        </View>
+                        <View style={styles.mb6}>
+                            <Text style={[styles.mb2]}>
+                                {this.props.translate('workspace.invite.personalMessagePrompt')}
+                            </Text>
+                            <TextInput
+                                autoCompleteType="off"
+                                autoCorrect={false}
+                                style={[styles.textInput, styles.workspaceInviteWelcome, styles.mb6]}
+                                numberOfLines={10}
+                                textAlignVertical="top"
+                                multiline
+                                value={this.state.welcomeNote}
+                                placeholder={this.getWelcomeNotePlaceholder()}
+                                placeholderTextColor={themeColors.placeholderText}
+                                onChangeText={text => this.setState({welcomeNote: text})}
+                            />
+                            <TextLink href="https://use.expensify.com/privacy">
+                                {this.props.translate('common.privacy')}
+                            </TextLink>
                         </View>
                     </ScrollView>
                     <FixedFooter style={[styles.flexGrow0]}>
