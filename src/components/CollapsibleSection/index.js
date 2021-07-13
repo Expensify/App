067ca47b25b9d03a749a3c17737d58120ff7c537
Joe Gambino <joe@expensify.com>
2021-07-12 21:38:42 -0700

    show open and close arrow for sections

--- src/components/CollapsibleSection/index.js
+++ src/components/CollapsibleSection/index.js
@@ -4,6 +4,8 @@ import Collapse from 'react-collapse';
 import Text from '../Text';
 import {propTypes, defaultProps} from './CollapsibleSectionPropTypes';
 import styles from '../../styles/styles';
+import Icon from '../Icon';
+import {Close, DownArrow} from '../Icon/Expensicons';
 
 class CollapsibleSection extends React.Component {
     constructor(props) {
@@ -24,10 +26,15 @@ class CollapsibleSection extends React.Component {
     }
 
     render() {
+        const src = this.state.isExpanded ? Close : DownArrow;
+
         return (
             <View style={[styles.mt4, styles.termsSection]}>
-                <TouchableOpacity onPress={this.toggleExpanded} style={[styles.pb4, styles.pl4]}>
-                    <Text>{this.props.title}</Text>
+                <TouchableOpacity onPress={this.toggleExpanded} style={[styles.pb4, styles.pl4, styles.flexRow]}>
+                    <Text style={styles.flex1}>{this.props.title}</Text>
+                    <View style={[styles.mr4]}>
+                        <Icon src={src} />
+                    </View>
                 </TouchableOpacity>
 
                 <Collapse isOpened={this.state.isExpanded}>
