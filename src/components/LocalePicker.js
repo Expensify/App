307c4e31078d5e7a26dea649ff0d8373a832e9aa
Dmytro Klymenko <dmytro.klymenko@protonmail.com>
2021-07-22 21:50:39 +0300

    #3770 move LocalePicker/index.js to LocalePicker.js

--- src/components/LocalePicker.js
+++ src/components/LocalePicker.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {withOnyx} from 'react-native-onyx';
 import PropTypes from 'prop-types';
-import styles from '../../styles/styles';
-import Picker from '../Picker';
-import Text from '../Text';
-import compose from '../../libs/compose';
-import {setLocale} from '../../libs/actions/App';
-import withLocalize, {withLocalizePropTypes} from '../withLocalize';
-import ONYXKEYS from '../../ONYXKEYS';
-import CONST from '../../CONST';
-import Permissions from '../../libs/Permissions';
+import styles from '../styles/styles';
+import Picker from './Picker';
+import Text from './Text';
+import compose from '../libs/compose';
+import {setLocale} from '../libs/actions/App';
+import withLocalize, {withLocalizePropTypes} from './withLocalize';
+import ONYXKEYS from '../ONYXKEYS';
+import CONST from '../CONST';
+import Permissions from '../libs/Permissions';
 
 const propTypes = {
     /** Indicates which locale the user currently has selected */
