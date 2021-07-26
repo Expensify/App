e0451f60f06a90873a525fdcc27bfde867b69a44
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-07-26 17:42:12 +0530

    refactor Context Menu

--- src/pages/home/report/ContextMenu/MiniReportActionContextMenu/index.js
+++ src/pages/home/report/ContextMenu/MiniReportActionContextMenu/index.js
@@ -1,124 +1,33 @@
 import _ from 'underscore';
 import React from 'react';
 import {View} from 'react-native';
-import lodashGet from 'lodash/get';
-import Str from 'expensify-common/lib/str';
+import PropTypes from 'prop-types';
 import {
-    Clipboard as ClipboardIcon, LinkCopy, Mail, Pencil, Trashcan, Checkmark,
-} from '../../../../../components/Icon/Expensicons';
-import getReportActionContextMenuStyles from '../../../../../styles/getReportActionContextMenuStyles';
-import {
-    setNewMarkerPosition, updateLastReadActionID, saveReportActionDraft,
-} from '../../../../../libs/actions/Report';
-import ContextMenuItem from '../../../../../components/ContextMenuItem';
-import {propTypes, defaultProps} from './MiniReportActionContextMenuPropsTypes';
-import Clipboard from '../../../../../libs/Clipboard';
-import {isReportMessageAttachment, canEditReportAction, canDeleteReportAction} from '../../../../../libs/reportUtils';
+    propTypes as ReportActionContextMenuPropsTypes,
+    defaultProps as ReportActionContextMenuDefaultProps,
+} from '../ReportActionContextMenuPropsTypes';
 import withLocalize from '../../../../../components/withLocalize';
+import {getMiniReportActionContextMenuWrapperStyle} from '../../../../../styles/getReportActionItemStyles';
+import ReportActionContextMenu from '../ReportActionContextMenu';
 
-class MiniReportActionContextMenu extends React.Component {
-    constructor(props) {
-        super(props);
+const propTypes = {
+    ..._.omit(ReportActionContextMenuPropsTypes, ['isMini']),
 
-        this.getActionText = this.getActionText.bind(this);
+    /** Should the comment have the appearance of being grouped with the previous comment? */
+    displayAsGroup: PropTypes.bool,
+};
 
-        // A list of all the context actions in this menu.
-        this.contextActions = [
-            // Copy to clipboard
-            {
-                text: this.props.translate('contextMenuItem.copyToClipboard'),
-                icon: ClipboardIcon,
-                successText: this.props.translate('contextMenuItem.copied'),
-                successIcon: Checkmark,
-                shouldShow: true,
+const defaultProps = {
+    ..._.omit(ReportActionContextMenuDefaultProps, ['isMini']),
+    displayAsGroup: false,
+};
 
-                // If return value is true, we switch the `text` and `icon` on
-                // `ContextMenuItem` with `successText` and `successIcon` which will fallback to
-                // the `text` and `icon`
-                onPress: () => {
-                    const message = _.last(lodashGet(this.props.reportAction, 'message', null));
-                    const html = lodashGet(message, 'html', '');
-                    const text = Str.htmlDecode(props.selection || lodashGet(message, 'text', ''));
-                    const isAttachment = _.has(this.props.reportAction, 'isAttachment')
-                        ? this.props.reportAction.isAttachment
-                        : isReportMessageAttachment(text);
-                    if (!isAttachment) {
-                        Clipboard.setString(text);
-                    } else {
-                        Clipboard.setString(html);
-                    }
-                },
-            },
-
-            {
-                text: this.props.translate('reportActionContextMenu.copyLink'),
-                icon: LinkCopy,
-                shouldShow: false,
-                onPress: () => {},
-            },
-
-            {
-                text: this.props.translate('reportActionContextMenu.markAsUnread'),
-                icon: Mail,
-                successIcon: Checkmark,
-                shouldShow: true,
-                onPress: () => {
-                    updateLastReadActionID(this.props.reportID, this.props.reportAction.sequenceNumber);
-                    setNewMarkerPosition(this.props.reportID, this.props.reportAction.sequenceNumber);
-                },
-            },
-
-            {
-                text: this.props.translate('reportActionContextMenu.editComment'),
-                icon: Pencil,
-                shouldShow: () => canEditReportAction(this.props.reportAction),
-                onPress: () => {
-                    saveReportActionDraft(
-                        this.props.reportID,
-                        this.props.reportAction.reportActionID,
-                        _.isEmpty(this.props.draftMessage) ? this.getActionText() : '',
-                    );
-                },
-            },
-            {
-                text: this.props.translate('reportActionContextMenu.deleteComment'),
-                icon: Trashcan,
-                shouldShow: () => canDeleteReportAction(this.props.reportAction),
-                onPress: () => {
-                    this.props.showDeleteConfirmModal(this.props.reportID, this.props.reportAction);
-                },
-            },
-        ];
-    }
-
-    /**
-     * Gets the markdown version of the message in an action.
-     *
-     * @return {String}
-     */
-    getActionText() {
-        const message = _.last(lodashGet(this.props.reportAction, 'message', null));
-        return lodashGet(message, 'html', '');
-    }
-
-    render() {
-        return this.props.isVisible && (
-            <View style={getReportActionContextMenuStyles(true)}>
-                {this.contextActions.map(contextAction => _.result(contextAction, 'shouldShow', false) && (
-                    <ContextMenuItem
-                        icon={contextAction.icon}
-                        text={contextAction.text}
-                        successIcon={contextAction.successIcon}
-                        successText={contextAction.successText}
-                        isMini
-                        key={contextAction.text}
-                        onPress={() => contextAction.onPress(this.props.reportAction)}
-                    />
-                ))}
-            </View>
-        );
-    }
-}
+const MiniReportActionContextMenu = props => (
+    <View style={getMiniReportActionContextMenuWrapperStyle(props.displayAsGroup)}>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <ReportActionContextMenu isMini {...props} />
+    </View>
+);
 
 MiniReportActionContextMenu.propTypes = propTypes;
 MiniReportActionContextMenu.defaultProps = defaultProps;
--- src/pages/home/report/ContextMenu/ReportActionContextMenuPropsTypes.js
+++ src/pages/home/report/ContextMenu/ReportActionContextMenuPropsTypes.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import {withLocalizePropTypes} from '../../../../../components/withLocalize';
-import ReportActionPropTypes from '../../ReportActionPropTypes';
+import {withLocalizePropTypes} from '../../../../components/withLocalize';
+import ReportActionPropTypes from '../ReportActionPropTypes';
 
 const propTypes = {
     /** The ID of the report this report action is attached to. */
@@ -9,6 +9,10 @@ const propTypes = {
 
     /** The report action this context menu is attached to. */
     reportAction: PropTypes.shape(ReportActionPropTypes).isRequired,
+
+    /** If true, this component will be a small, row-oriented menu that displays icons but not text.
+    If false, this component will be a larger, column-oriented menu that displays icons alongside text in each row. */
+    isMini: PropTypes.bool,
 
     /** Controls the visibility of this component. */
     isVisible: PropTypes.bool,
@@ -22,16 +26,17 @@ const propTypes = {
     /** Function to show the delete Action confirmation modal */
     showDeleteConfirmModal: PropTypes.func.isRequired,
 
+    /** Function to dismiss the popover containing this menu */
+    hidePopover: PropTypes.func.isRequired,
+
     ...withLocalizePropTypes,
 };
 
 const defaultProps = {
+    isMini: false,
     isVisible: false,
     selection: '',
     draftMessage: '',
 };
 
-export {
-    propTypes,
-    defaultProps,
-};
+export {propTypes, defaultProps};
