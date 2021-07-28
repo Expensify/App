259c99303d8df1cb2eca1cc856931eb259417d34
Horus Lugo López <horus@expensify.com>
2021-07-28 14:03:06 +0200

    Clear cache on notification dismiss

---
+++ android/app/src/main/java/com/expensify/chat/CustomNotificationListener.java
@@ -0,0 +1,48 @@
+package com.expensify.chat;
+
+import androidx.annotation.NonNull;
+
+import com.urbanairship.push.NotificationActionButtonInfo;
+import com.urbanairship.push.NotificationInfo;
+import com.urbanairship.push.NotificationListener;
+import com.urbanairship.push.PushMessage;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CustomNotificationListener implements NotificationListener {
+    private final NotificationListener parent;
+    private final CustomNotificationProvider provider;
+
+    CustomNotificationListener(NotificationListener parent, CustomNotificationProvider provider) {
+        this.parent = parent;
+        this.provider = provider;
+    }
+
+    @Override
+    public void onNotificationPosted(@NonNull @NotNull NotificationInfo notificationInfo) {
+        parent.onNotificationPosted(notificationInfo);
+    }
+
+    @Override
+    public boolean onNotificationOpened(@NonNull @NotNull NotificationInfo notificationInfo) {
+        return parent.onNotificationOpened(notificationInfo);
+    }
+
+    @Override
+    public boolean onNotificationForegroundAction(@NonNull @NotNull NotificationInfo notificationInfo, @NonNull @NotNull NotificationActionButtonInfo actionButtonInfo) {
+        return parent.onNotificationForegroundAction(notificationInfo, actionButtonInfo);
+    }
+
+    @Override
+    public void onNotificationBackgroundAction(@NonNull @NotNull NotificationInfo notificationInfo, @NonNull @NotNull NotificationActionButtonInfo actionButtonInfo) {
+        parent.onNotificationBackgroundAction(notificationInfo, actionButtonInfo);
+    }
+
+    @Override
+    public void onNotificationDismissed(@NonNull @NotNull NotificationInfo notificationInfo) {
+        parent.onNotificationDismissed(notificationInfo);
+
+        PushMessage message = notificationInfo.getMessage();
+        provider.onDismissNotification(message);
+    }
+}
--- android/app/src/main/java/com/expensify/chat/CustomNotificationProvider.java
+++ android/app/src/main/java/com/expensify/chat/CustomNotificationProvider.java
@@ -1,11 +1,8 @@
 package com.expensify.chat;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Build;
-import android.service.notification.StatusBarNotification;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
@@ -13,15 +10,12 @@ import android.view.WindowManager;
 
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.app.Person;
 import androidx.core.graphics.drawable.IconCompat;
 
 import com.urbanairship.AirshipConfigOptions;
-import com.urbanairship.json.JsonException;
-import com.urbanairship.json.JsonList;
 import com.urbanairship.json.JsonMap;
 import com.urbanairship.json.JsonValue;
 import com.urbanairship.push.PushMessage;
@@ -32,10 +26,8 @@ import com.urbanairship.util.ImageUtils;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -57,25 +49,12 @@ public class CustomNotificationProvider extends ReactNotificationProvider {
     private static final String TAG = "NotificationProvider";
 
     // Conversation JSON keys
-    private static final String CONVERSATION_KEY = "conversation";
-    private static final String CONVERSATION_OWNER_KEY = "owner";
-    private static final String CONVERSATION_TITLE_KEY = "title";
-    private static final String PEOPLE_KEY = "people";
-    private static final String PERSON_ID_KEY = "id";
-    private static final String PERSON_ICON_KEY = "icon";
-    private static final String PERSON_NAME_KEY = "name";
-    private static final String MESSAGES_KEY = "messages";
-    private static final String MESSAGE_TEXT_KEY = "text";
-    private static final String MESSAGE_TIME_KEY = "time";
-    private static final String MESSAGE_PERSON_KEY = "person";
-
-    // Expensify Conversation JSON keys
     private static final String PAYLOAD_KEY = "payload";
     private static final String TYPE_KEY = "type";
     private static final String REPORT_COMMENT_TYPE = "reportComment";
 
     private final ExecutorService executorService = Executors.newCachedThreadPool();
-    private final HashMap<Integer, NotificationCache> cache = new HashMap<>();
+    public final HashMap<Integer, NotificationCache> cache = new HashMap<>();
 
     public CustomNotificationProvider(@NonNull Context context, @NonNull AirshipConfigOptions configOptions) {
         super(context, configOptions);
@@ -91,16 +70,12 @@ public class CustomNotificationProvider extends ReactNotificationProvider {
             return builder;
         }
 
-        if (message.containsKey(CONVERSATION_KEY)) {
-            applyMessageStyle(message, builder);
-        }
-
         if (message.containsKey(PAYLOAD_KEY)) {
             try {
                 JsonMap payload = JsonValue.parseString(message.getExtra(PAYLOAD_KEY)).optMap();
 
                 if (REPORT_COMMENT_TYPE.equals(payload.get(TYPE_KEY).getString())) {
-                        applyExpensifyMessageStyle(builder, payload, arguments);
+                        applyMessageStyle(builder, payload, arguments);
                 }
             } catch (Exception e) {
                 Log.e(TAG, "Failed to parse conversation", e);
@@ -110,15 +85,13 @@ public class CustomNotificationProvider extends ReactNotificationProvider {
         return builder;
     }
 
-    private void applyExpensifyMessageStyle(NotificationCompat.Builder builder, JsonMap payload, NotificationArguments arguments) {
+    private void applyMessageStyle(NotificationCompat.Builder builder, JsonMap payload, NotificationArguments arguments) {
         int reportID = payload.get("reportID").getInt(-1);
         NotificationCache notificationCache = findOrCreateNotificationCache(reportID);
-
         JsonMap reportAction = payload.get("reportAction").getMap();
         String name = reportAction.get("person").getList().get(0).getMap().get("text").getString();
         String avatar = reportAction.get("avatar").getString();
         String accountID = Integer.toString(reportAction.get("actorAccountID").getInt(-1));
-
         String message = reportAction.get("message").getList().get(0).getMap().get("text").getString();
         long time = reportAction.get("timestamp").getLong(0);
 
@@ -162,81 +135,19 @@ public class CustomNotificationProvider extends ReactNotificationProvider {
         return notificationCache;
     }
 
-
-    private void applyMessageStyle(PushMessage message, NotificationCompat.Builder builder) {
-        JsonMap conversation = null;
+    public void onDismissNotification(PushMessage message) {
         try {
-            conversation = JsonValue.parseString(message.getExtra(CONVERSATION_KEY)).optMap();
-        } catch (JsonException e) {
-            Log.e(TAG, "Failed to parse conversation", e);
-        }
+            JsonMap payload = JsonValue.parseString(message.getExtra(PAYLOAD_KEY)).optMap();
+            int reportID = payload.get("reportID").getInt(-1);
 
-        if (conversation == null) {
-            return;
-        }
-
-        Map<String, Person> people = resolvePeople(conversation.opt(PEOPLE_KEY).optList());
-        if (people.isEmpty()) {
-            Log.e(TAG, "Missing people.");
-            return;
-        }
-
-        String ownerKey = conversation.get(CONVERSATION_OWNER_KEY).getString();
-        if (!people.containsKey(ownerKey)) {
-            Log.e(TAG, "Missing owner.");
-            return;
-        }
-
-        NotificationCompat.MessagingStyle messagingStyle = new NotificationCompat.MessagingStyle(people.get(ownerKey))
-                .setGroupConversation(people.size() > 2)
-                .setConversationTitle(conversation.opt(CONVERSATION_TITLE_KEY).optString());
-
-        for (JsonValue messageJson : conversation.opt(MESSAGES_KEY).optList()) {
-            String personKey = messageJson.optMap().opt(MESSAGE_PERSON_KEY).getString();
-            String text = messageJson.optMap().opt(MESSAGE_TEXT_KEY).getString();
-            long time = messageJson.optMap().opt(MESSAGE_TIME_KEY).getLong(0);
-
-            if (people.containsKey(personKey) && text != null && time > 0) {
-                messagingStyle.addMessage(text, time, people.get(personKey));
+            if (reportID == -1) {
+                return;
             }
+
+            cache.remove(reportID);
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to delete conversation cache");
         }
-
-        builder.setStyle(messagingStyle);
-    }
-
-    private Map<String, Person> resolvePeople(JsonList peopleJson) {
-        Map<String, Person> people = Collections.synchronizedMap(new HashMap<>());
-        CountDownLatch countDownLatch = new CountDownLatch(peopleJson.size());
-
-        for (JsonValue personJson : peopleJson) {
-            executorService.execute(() -> {
-                String id = personJson.optMap().opt(PERSON_ID_KEY).optString();
-                String name = personJson.optMap().opt(PERSON_NAME_KEY).optString();
-                String icon = personJson.optMap().opt(PERSON_ICON_KEY).optString();
-
-                if (id != null) {
-                    IconCompat iconCompat = fetchIcon(icon, FALLBACK_ICON_ID);
-                    Person person = new Person.Builder()
-                            .setIcon(iconCompat)
-                            .setKey(id)
-                            .setName(name)
-                            .build();
-
-                    people.put(id, person);
-                }
-
-                countDownLatch.countDown();
-            });
-        }
-
-        try {
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            Log.e(TAG, "Failed to resolve people", e);
-            Thread.currentThread().interrupt();
-        }
-
-        return people;
     }
 
     @NonNull
