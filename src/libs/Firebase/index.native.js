a058756f88d1f6de73a15ec3706a3a2f93984690
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-07-27 14:52:46 -1000

    undo logs

--- src/libs/Firebase/index.native.js
+++ src/libs/Firebase/index.native.js
@@ -1,18 +1,21 @@
 import perf from '@react-native-firebase/perf';
 import {isDevelopment} from '../Environment/Environment';
+import Log from '../Log';
 
 const traceMap = {};
+
+let start, stop;
 
 /**
  * @param {String} customEventName
  */
 function startTrace(customEventName) {
+    start = global.performance.now();
     if (isDevelopment()) {
         return;
     }
 
     if (traceMap[customEventName]) {
-        console.debug('[Firebase] Trace with this name has already started');
         return;
     }
 
@@ -24,16 +27,20 @@ function startTrace(customEventName) {
  * @param {String} customEventName
  */
 function stopTrace(customEventName) {
+    stop = global.performance.now();
+
     if (isDevelopment()) {
         return;
     }
 
     const trace = traceMap[customEventName];
     if (!trace) {
-        console.debug('[Firebase] Could not find trace');
+        return;
     }
 
     trace.stop();
+
+    Log.info(`@marcaaron sidebar_loaded took: ${stop - start} ms`, true);
     delete traceMap[customEventName];
 }
 
