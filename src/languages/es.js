0277089d973bd1eec5ddfa51fd153e0e203c6cb3
Rushat Gabhane <rushatgabhane@gmail.com>
2021-07-28 12:40:59 +0300

    show confirm modal for large attachment

--- src/languages/es.js
+++ src/languages/es.js
@@ -65,6 +65,7 @@ export default {
         takePhoto: 'Hacer una Foto',
         chooseFromGallery: 'Elegir de la galería',
         chooseDocument: 'Elegir Documento',
+        attachmentTooLarge: 'Archivo adjunto demasiado grandes',
         sizeExceeded: 'El archivo adjunto supera el límite de 50 MB',
     },
     textInputFocusable: {
