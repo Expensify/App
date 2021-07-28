62b400e07bc0675f20990262355723ddc47dc279
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-07-28 18:52:18 +0530

    refactor and minor changes

--- src/pages/home/report/ContextMenu/GenericReportActionContextMenuPropTypes.js
+++ src/pages/home/report/ContextMenu/GenericReportActionContextMenuPropTypes.js
@@ -1,10 +1,8 @@
 import PropTypes from 'prop-types';
-import {withLocalizePropTypes} from '../../../../components/withLocalize';
 import ReportActionPropTypes from '../ReportActionPropTypes';
 
 const propTypes = {
     /** The ID of the report this report action is attached to. */
-    // eslint-disable-next-line react/no-unused-prop-types
     reportID: PropTypes.number.isRequired,
 
     /** The report action this context menu is attached to. */
@@ -22,8 +20,6 @@ const propTypes = {
 
     /** Draft message - if this is set the comment is in 'edit' mode */
     draftMessage: PropTypes.string,
-
-    ...withLocalizePropTypes,
 };
 
 const defaultProps = {
