e756696437c7dcc59a2f0ded5053b3b615b788ec
Carlos Martins <cmartins@expensify.com>
2021-07-28 11:44:45 -0600

    remove env check

--- wdyr.js
+++ wdyr.js
@@ -4,9 +4,9 @@ import React from 'react';
 import Config from 'react-native-config';
 import lodashGet from 'lodash/get';
 
-const useWDYR = lodashGet(Config, 'USE_WDYR', 'false') === 'true';
+const useWDYR = lodashGet(Config, 'USE_WDYR') === 'true';
 
-if (useWDYR && process.env.NODE_ENV === 'development') {
+if (useWDYR) {
     const whyDidYouRender = require('@welldone-software/why-did-you-render');
     whyDidYouRender(React, {
         // Include and exclude components to be tracked by their displayName here
