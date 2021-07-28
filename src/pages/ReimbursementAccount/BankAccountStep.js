b1c6da32d59b31a8b4986d49fb2c70084e532248
Jasper Huang <jasper@expensify.com>
2021-07-28 11:40:32 -0700

    remove flex

--- src/pages/ReimbursementAccount/BankAccountStep.js
+++ src/pages/ReimbursementAccount/BankAccountStep.js
@@ -253,7 +253,7 @@ class BankAccountStep extends React.Component {
                     onConfirm={hideExistingOwnersError}
                     shouldShowCancelButton={false}
                     prompt={(
-                        <View style={[styles.flex1]}>
+                        <View>
                             <Text style={[styles.mb4]}>
                                 <Text>
                                     {this.props.translate('bankAccount.error.existingOwners.alreadyInUse')}
