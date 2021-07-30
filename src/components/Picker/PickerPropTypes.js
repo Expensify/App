f3c1c79e43e6f41f07baecf8c70462d556ac77a5
Donnie Darko <im@kakajann.com>
2021-07-30 08:50:14 +0500

    more refactors on ExpensiPicker

--- src/components/Picker/PickerPropTypes.js
+++ src/components/Picker/PickerPropTypes.js
@@ -9,7 +9,7 @@ const propTypes = {
     onChange: PropTypes.func.isRequired,
 
     /** Whether or not to show the disabled styles */
-    useDisabledStyles: PropTypes.bool,
+    isDisabled: PropTypes.bool,
 
     /** Disables interaction with the component */
     disabled: PropTypes.bool,
@@ -42,7 +42,7 @@ const propTypes = {
     size: PropTypes.oneOf(['normal', 'small']),
 };
 const defaultProps = {
-    useDisabledStyles: false,
+    isDisabled: false,
     disabled: false,
     placeholder: {},
     value: null,
