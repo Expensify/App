5169b3e23d54eb83ee4a5458abe035b4edc843f2
Donnie Darko <im@kakajann.com>
2021-07-30 09:02:59 +0500

    refactor ExpensiTextInputWrapper

--- src/components/ExpensiTextInput/ExpensiTextInputWrapper.js
+++ src/components/ExpensiTextInput/ExpensiTextInputWrapper.js
@@ -1,7 +1,31 @@
 import React, {PureComponent} from 'react';
 import {TouchableWithoutFeedback, View} from 'react-native';
-import {propTypes, defaultProps} from './propTypes';
-import styles from '../../../styles/styles';
+import PropTypes from 'prop-types';
+import styles from '../../styles/styles';
+
+const propTypes = {
+    /** Input label */
+    children: PropTypes.node.isRequired,
+
+    /** Has Label */
+    hasLabel: PropTypes.bool.isRequired,
+
+    /** On press to wrapper */
+    onPress: PropTypes.func.isRequired,
+
+    /** Is input in focus? */
+    isFocused: PropTypes.bool.isRequired,
+
+    /** Has error? */
+    error: PropTypes.bool.isRequired,
+
+    /** Styles for the outermost container for this component. */
+    containerStyles: PropTypes.arrayOf(PropTypes.object),
+};
+
+const defaultProps = {
+    containerStyles: [],
+};
 
 class ExpensiTextInputWrapper extends PureComponent {
     render() {
