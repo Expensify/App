0b0c49cd0b45aa57df96b5b3aa1327baa68a61be
Joe Gambino <joe@expensify.com>
2021-07-30 13:09:34 -0700

    Update execSync command to run pre-defined string

--- .github/libs/GitUtils.js
+++ .github/libs/GitUtils.js
@@ -9,8 +9,9 @@ const {execSync} = require('child_process');
  * @returns {Array}
  */
 function getPullRequestsMergedBetween(fromRef, toRef) {
+    const command = `git log --format="%s" ${fromRef}...${toRef}`;
     console.log('Getting pull requests merged between the following refs:', fromRef, toRef);
-    const localGitLogs = execSync(`git log --format="%s" ${fromRef}...${toRef}`).toString();
+    const localGitLogs = execSync(command).toString();
     return _.map(
         [...localGitLogs.matchAll(/Merge pull request #(\d{1,6}) from (?!Expensify\/(?:master|main|version-))/g)],
         match => match[1],
