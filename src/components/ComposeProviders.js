3be6b85cfc5578a9d3f21e75f74b8ed63919e93f
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-07-30 13:17:35 -1000

    fix propTypes and displayName

--- src/components/ComposeProviders.js
+++ src/components/ComposeProviders.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const propTypes = {
     /** Provider components go here */
-    components: PropTypes.arrayOf(PropTypes.object).isRequired,
+    components: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.func])).isRequired,
 
     /** Rendered child component */
     children: PropTypes.node.isRequired,
