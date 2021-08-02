9905d36b6315b6d3bf985e909e17410908d419a0
Rafael <rdjuric@pm.me>
2021-08-02 19:34:00 -0300

    revert some changes, removes hardcoded prop

--- src/components/InvertedFlatList/index.ios.js
+++ src/components/InvertedFlatList/index.ios.js
@@ -6,10 +6,5 @@ export default forwardRef((props, ref) => (
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
         ref={ref}
-
-        // Remove clipped subviews helps prevent avatars and attachments from eating up excess memory on Android. When
-        // we run out of memory images stop appearing without any warning.
-        // eslint-disable-next-line react/jsx-props-no-multi-spaces
-        shouldRemoveClippedSubviews
     />
 ));
