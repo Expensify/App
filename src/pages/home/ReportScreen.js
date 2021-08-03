6c3ee083fe5c5f3a93f7fd9bf9d9749b640c7a07
Aman Ansar <amanansari.a2@gmail.com>
2021-08-03 18:16:32 +0530

    more readable & usable

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -58,13 +58,15 @@ class ReportScreen extends React.Component {
 
     /**
      * Get the currently viewed report ID as number
-     *
+     * If the shouldRedirectIfNan flag is set, we redirect to the Concierge chat
+     * when the ID is not a number
+     * @param {Boolean} shouldRedirectIfNan
      * @returns {Number}
      */
-    getReportID() {
+    getReportID(shouldRedirectIfNan = false) {
         const params = this.props.route.params;
         const id = Number.parseInt(params.reportID, 10);
-        if (_.isNaN(id)) {
+        if (shouldRedirectIfNan && _.isNaN(id)) {
             handleInaccessibleReport();
         }
         return id;
@@ -93,7 +95,7 @@ class ReportScreen extends React.Component {
      * Persists the currently viewed report id
      */
     storeCurrentlyViewedReport() {
-        const reportID = this.getReportID();
+        const reportID = this.getReportID(true);
         updateCurrentlyViewedReportID(reportID);
     }
 
