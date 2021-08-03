4bd4f9a5b725aee31c6df0e8548f0a673b7ea6f6
Vit Horacek <vit@expensify.com>
2021-08-03 19:18:12 +0100

    Change the IOU type string to use global constants

--- src/pages/iou/IOUSendPage.js
+++ src/pages/iou/IOUSendPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IOUModal from './IOUModal';
+import CONST from '../../CONST';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-export default props => <IOUModal {...props} iouType="send" />;
+export default props => <IOUModal {...props} iouType={CONST.IOU_TYPE.SEND} />;
