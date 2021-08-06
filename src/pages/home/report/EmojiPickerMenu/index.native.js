968195ecd68fd95c5db31302b48f7d15743d1fa1
Manan Jadhav <manan.jadhav@gmail.com>
2021-08-07 00:49:40 +0530

    fix(emoji-skintone): Added skinPicker to native

--- src/pages/home/report/EmojiPickerMenu/index.native.js
+++ src/pages/home/report/EmojiPickerMenu/index.native.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
-import {View, FlatList} from 'react-native';
+import {View, FlatList, Pressable} from 'react-native';
 import PropTypes from 'prop-types';
 import compose from '../../../../libs/compose';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../../../components/withWindowDimensions';
 import CONST from '../../../../CONST';
 import styles from '../../../../styles/styles';
-import emojis from '../../../../../assets/emojis';
+import emojis, {skinTones} from '../../../../../assets/emojis';
 import EmojiPickerMenuItem from '../EmojiPickerMenuItem';
 import Text from '../../../../components/Text';
 import dynamicEmojiSize from './dynamicEmojiSize';
+import getSkinToneEmojiCode from './getSkinToneEmojiCode';
+import withLocalize, {withLocalizePropTypes} from '../../../../components/withLocalize';
 
 const propTypes = {
     /** Function to add the selected emoji to the main compose text input */
     onEmojiSelected: PropTypes.func.isRequired,
 
+    /** Stores user's preferred skin tone */
+    preferredSkinTone: PropTypes.number,
+
+    /** Function to sync the selected skin tone with parent, onyx and nvp */
+    updatePreferredSkinTone: PropTypes.func,
+
     /** Props related to the dimensions of the window */
     ...windowDimensionsPropTypes,
+
+    /** Props related to translation */
+    ...withLocalizePropTypes,
 };
 
 class EmojiPickerMenu extends Component {
@@ -40,7 +51,90 @@ class EmojiPickerMenu extends Component {
         this.emojiSize = {
             fontSize: dynamicEmojiSize(this.props.windowWidth),
         };
+
+        this.state = {
+            showSkinToneList: false,
+        };
     }
+
+    /**
+     * Update the preferredSkinTone and call the parent function if available
+     * @param {Number} skinTone
+     */
+    setPreferredSkinTone(skinTone) {
+        this.setState({showSkinToneList: false});
+        if (this.props.updatePreferredSkinTone) {
+            this.props.updatePreferredSkinTone(skinTone);
+        }
+    }
+
+    /**
+     * Returns view for skin tone picker
+     * @returns {*}
+     */
+    renderSkinTonePicker() {
+        return (
+            <View style={[styles.flexRow, styles.p1]}>
+                {
+                  !this.state.showSkinToneList
+                        && (
+                        <Pressable
+                            onPress={
+                              () => this.setState(prevState => ({showSkinToneList: !prevState.showSkinToneList}))
+                            }
+                            style={[
+                                styles.pv1,
+                                styles.flex1,
+                                styles.flexRow,
+                                styles.alignSelfCenter,
+                                styles.justifyContentStart,
+                            ]}
+                        >
+                            <Text style={[styles.emojiText, this.emojiSize]}>
+                                {`${getSkinToneEmojiCode(this.props.preferredSkinTone)}\uFE0F`}
+                            </Text>
+                            <Text style={[styles.emojiHeaderStyle]}>
+                                {this.props.translate('emojiPicker.skinTonePickerLabel')}
+                            </Text>
+                        </Pressable>
+                        )
+                }
+                {
+                        this.state.showSkinToneList
+                        && (
+                        <View>
+                            <View style={[styles.flexRow]}>
+                                {
+                              skinTones.map(skinToneEmoji => (
+                                  <EmojiPickerMenuItem
+                                      onPress={() => this.setPreferredSkinTone(skinToneEmoji.skinTone)}
+                                      onHover={() => this.setState({
+                                          highlightedSkinToneIndex: skinToneEmoji.skinTone,
+                                      })}
+                                      key={skinToneEmoji.code}
+                                      emojiItemStyle={[styles.emojiSkinToneItem, styles.emojiSkinToneItem]}
+                                      emoji={`${skinToneEmoji.code}\uFE0F`}
+                                      isHighlighted={skinToneEmoji.skinTone === this.state.highlightedSkinToneIndex}
+                                      emojiSize={this.emojiSize}
+                                  />
+                              ))
+                            }
+                            </View>
+                            <Text style={
+                              [styles.emojiHeaderStyle, styles.emojiExtraSmall, styles.textAlignCenter, styles.pv0]
+                              }
+                            >
+                                {this.props.translate('emojiPicker.setPreferredSkinTone')}
+                            </Text>
+                        </View>
+                        )
+                    }
+
+            </View>
+
+        );
+    }
+
 
     /**
      * Given an emoji item object, render a component based on its type.
@@ -51,6 +145,7 @@ class EmojiPickerMenu extends Component {
      * @returns {*}
      */
     renderItem({item}) {
+        const {code, types} = item;
         if (item.code === CONST.EMOJI_SPACER) {
             return null;
         }
@@ -63,10 +158,16 @@ class EmojiPickerMenu extends Component {
             );
         }
 
+        let emojiCode = code;
+        if (types && typeof this.props.preferredSkinTone === 'number' && types[this.props.preferredSkinTone]) {
+            emojiCode = types[this.props.preferredSkinTone];
+        }
+
+
         return (
             <EmojiPickerMenuItem
                 onPress={this.props.onEmojiSelected}
-                emoji={`${item.code}\uFE0F`}
+                emoji={`${emojiCode}\uFE0F`}
                 emojiSize={this.emojiSize}
             />
         );
@@ -83,15 +184,21 @@ class EmojiPickerMenu extends Component {
                     style={styles.emojiPickerList}
                     stickyHeaderIndices={this.unfilteredHeaderIndices}
                 />
+                {this.renderSkinTonePicker()}
             </View>
         );
     }
 }
 
 EmojiPickerMenu.propTypes = propTypes;
+EmojiPickerMenu.defaultProps = {
+    preferredSkinTone: undefined,
+    updatePreferredSkinTone: undefined,
+};
 
 export default compose(
     withWindowDimensions,
+    withLocalize,
 )(React.forwardRef((props, ref) => (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <EmojiPickerMenu {...props} forwardedRef={ref} />
