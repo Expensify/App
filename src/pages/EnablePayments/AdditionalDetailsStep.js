1410064f343ee26145efd4f46e8fd373a222c40d
Luke Donahue <luke@expensify.com>
2021-08-12 17:36:37 -0700

    imported fragment

--- src/pages/EnablePayments/AdditionalDetailsStep.js
+++ src/pages/EnablePayments/AdditionalDetailsStep.js
@@ -1,5 +1,5 @@
 import _ from 'underscore';
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import {
