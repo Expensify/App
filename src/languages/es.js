a841dac3a4e393844eb271a86f85dc9f1c1761f0
Manan Jadhav <manan.jadhav@gmail.com>
2021-08-17 18:24:09 +0530

    refactor(tooltip-icon-button): Changed translation for Actions

--- src/languages/es.js
+++ src/languages/es.js
@@ -118,7 +118,7 @@ export default {
         phrase4: '¡Bienvenido de vuelta al Nuevo Expensify! Por favor, introduce tu contraseña.',
     },
     reportActionCompose: {
-        addAction: 'Actions',
+        addAction: 'Acción',
         sendAttachment: 'Enviar adjunto',
         addAttachment: 'Agregar Archivo Adjunto',
         writeSomething: 'Escribe algo...',
