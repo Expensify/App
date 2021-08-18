9b6706cbb414e3ba030908994f9ae1bd8aa3e988
Akshaya Salvi <akshayasalvi12@gmail.com>
2021-08-18 23:10:27 +0530

    replaced hyperlink with attachmentview based on condition

--- src/components/AnchorForCommentsOnly/BaseAnchorForCommentsOnly/index.js
+++ src/components/AnchorForCommentsOnly/BaseAnchorForCommentsOnly/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Pressable, StyleSheet} from 'react-native';
 import lodashGet from 'lodash/get';
 import Text from '../../Text';
+import Icon from '../../Icon';
 import {propTypes, defaultProps} from '../anchorForCommentsOnlyPropTypes';
 import PressableWithSecondaryInteraction from '../../PressableWithSecondaryInteraction';
 import {showContextMenu} from '../../../pages/home/report/ContextMenu/ReportActionContextMenu';
 import {CONTEXT_MENU_TYPES} from '../../../pages/home/report/ContextMenu/ContextMenuActions';
+import {Download} from '../../Icon/Expensicons';
+import AttachmentView from '../../AttachmentView';
+import fileDownload from '../../../libs/fileDownload';
 
 
 /*
@@ -17,34 +21,54 @@ const BaseAnchorForCommentsOnly = ({
     target,
     children,
     style,
+    text,
     ...props
 }) => {
     let linkRef;
     return (
-        <PressableWithSecondaryInteraction
-            onSecondaryInteraction={
-                (event) => {
-                    showContextMenu(
-                        CONTEXT_MENU_TYPES.LINK,
-                        event,
-                        href,
-                        lodashGet(linkRef, 'current'),
-                    );
-                }
-            }
-        >
-            <Text
-                ref={el => linkRef = el}
-                style={StyleSheet.flatten(style)}
-                accessibilityRole="link"
-                href={href}
-                hrefAttrs={{rel, target}}
+
+        props.shouldDownloadFile
+            ? (
+                <Pressable onPress={() => {
+                    fileDownload(href);
+                }}
+                >
+                    <AttachmentView
+                        sourceURL={href}
+                        file={{name: text}}
+                        rightElement={(
+                            <Icon src={Download} />
+                        )}
+                    />
+                </Pressable>
+            )
+            : (
+                <PressableWithSecondaryInteraction
+                    onSecondaryInteraction={
+                            (event) => {
+                                showContextMenu(
+                                    CONTEXT_MENU_TYPES.LINK,
+                                    event,
+                                    href,
+                                    lodashGet(linkRef, 'current'),
+                                );
+                            }
+                        }
+                >
+                    <Text
+                        ref={el => linkRef = el}
+                        style={StyleSheet.flatten(style)}
+                        accessibilityRole="link"
+                        href={href}
+                        hrefAttrs={{rel, target}}
                 // eslint-disable-next-line react/jsx-props-no-spreading
-                {...props}
-            >
-                {children}
-            </Text>
-        </PressableWithSecondaryInteraction>
+                        {...props}
+                    >
+                        {children}
+                    </Text>
+                </PressableWithSecondaryInteraction>
+            )
+
     );
 };
 
