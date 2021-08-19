8d1524a2962c651784f2358af35a610133b68e01
Peter Velkov <kidroca@gmail.com>
2021-08-19 23:19:30 +0300

    Add performance capturing sugar

--- src/libs/Performance.js
+++ src/libs/Performance.js
@@ -1,6 +1,14 @@
 import _ from 'underscore';
 import lodashTransform from 'lodash/transform';
+import React, {Profiler, forwardRef} from 'react';
+import {Alert} from 'react-native';
+
 import canCapturePerformanceMetrics from './canCapturePerformanceMetrics';
+import getComponentDisplayName from './getComponentDisplayName';
+import CONST from '../CONST';
+
+/** @type {import('react-native-performance').Performance} */
+let performance;
 
 /**
  * Deep diff between two objects. Useful for figuring out what changed about an object from one render to the next so
@@ -25,43 +33,145 @@ function diffObject(object, base) {
     return changes(object, base);
 }
 
-/**
- * Sets up an observer to capture events recorded in the native layer before the app fully initializes.
- */
-function setupPerformanceObserver() {
-    if (!canCapturePerformanceMetrics()) {
-        return;
-    }
+// When performance monitoring is disabled the implementations are blank
+/* eslint-disable import/no-mutable-exports */
+let setupPerformanceObserver = () => {};
+let printPerformanceMetrics = () => {};
+let markStart = () => {};
+let markEnd = () => {};
+let traceRender = () => {};
+let withRenderTrace = () => Component => Component;
+/* eslint-enable import/no-mutable-exports */
 
-    const performance = require('react-native-performance').default;
-    const PerformanceObserver = require('react-native-performance').PerformanceObserver;
-    new PerformanceObserver((list) => {
-        if (list.getEntries().find(entry => entry.name === 'nativeLaunchEnd')) {
-            performance.measure('nativeLaunch', 'nativeLaunchStart', 'nativeLaunchEnd');
+if (canCapturePerformanceMetrics()) {
+    /**
+     * Sets up an observer to capture events recorded in the native layer before the app fully initializes.
+     */
+    setupPerformanceObserver = () => {
+        const performanceReported = require('react-native-performance-flipper-reporter');
+        performanceReported.setupDefaultFlipperReporter();
 
-            // eslint-disable-next-line no-undef
-            if (__DEV__) {
-                performance.measure('jsBundleDownload', 'downloadStart', 'downloadEnd');
-            } else {
-                performance.measure('runJsBundle', 'runJsBundleStart', 'runJsBundleEnd');
-            }
-        }
-    }).observe({type: 'react-native-mark', buffered: true});
-}
+        const perfModule = require('react-native-performance');
+        perfModule.setResourceLoggingEnabled(true);
+        performance = perfModule.default;
 
-/**
- * Outputs performance stats. We alert these so that they are easy to access in release builds.
- */
-function printPerformanceMetrics() {
-    const performance = require('react-native-performance').default;
-    const entries = _.map(performance.getEntriesByType('measure'), entry => ({
-        name: entry.name, duration: Math.floor(entry.duration),
-    }));
-    alert(JSON.stringify(entries, null, 4));
+        // Monitor some native marks that we want to put on the timeline
+        new perfModule.PerformanceObserver((list, observer) => {
+            list.getEntries()
+                .forEach((entry) => {
+                    if (entry.name === 'nativeLaunchEnd') {
+                        performance.measure('nativeLaunch', 'nativeLaunchStart', 'nativeLaunchEnd');
+                    }
+                    if (entry.name === 'downloadEnd') {
+                        performance.measure('jsBundleDownload', 'downloadStart', 'downloadEnd');
+
+                        // We don't need to keep the observer past this point
+                        observer.disconnect();
+                    }
+                });
+        }).observe({type: 'react-native-mark', buffered: true});
+
+        new perfModule.PerformanceObserver((list) => {
+            list.getEntriesByType('mark')
+                .forEach((mark) => {
+                    if (mark.name.endsWith('_end')) {
+                        const end = mark.name;
+                        const name = end.replace(/_end$/, '');
+                        const start = `${name}_start`;
+                        performance.measure(name, start, end);
+                    }
+
+                    if (mark.name === `${CONST.TIMING.SIDEBAR_LOADED}_end`) {
+                        performance.measure('TTI', 'nativeLaunchStart', mark.name);
+                        printPerformanceMetrics();
+                    }
+                });
+        }).observe({type: 'mark', buffered: true});
+    };
+
+    /**
+     * Outputs performance stats. We alert these so that they are easy to access in release builds.
+     */
+    printPerformanceMetrics = () => {
+        const stats = [...performance.getEntriesByType('measure'), ...performance.getEntriesByType('react-native-mark')]
+            .filter(entry => entry.duration > 0)
+            .map(entry => `\u2022 ${entry.name}: ${entry.value || entry.duration.toFixed(1)}ms`);
+
+        Alert.alert('Performance', stats.join('\n'));
+    };
+
+    /**
+     * Adds a start mark to the performance entries
+     * @param {string} name
+     * @param {Object} [detail]
+     * @returns {PerformanceMark}
+     */
+    markStart = (name, detail) => performance.mark(`${name}_start`, {detail});
+
+    /**
+     * Adds an end mark to the performance entries
+     * This would also automatically create a measure between start and end
+     * @param {string} name
+     * @param {Object} [detail]
+     * @returns {PerformanceMark}
+     */
+    markEnd = (name, detail) => performance.mark(`${name}_end`, {detail});
+
+    /**
+     * Put data emitted by Profiler components on the timeline
+     * @param {string} id the "id" prop of the Profiler tree that has just committed
+     * @param {'mount'|'update'} phase either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+     * @param {number} actualDuration time spent rendering the committed update
+     * @param {number} baseDuration estimated time to render the entire subtree without memoization
+     * @param {number} startTime when React began rendering this update
+     * @param {number} commitTime when React committed this update
+     * @param {Set} interactions the Set of interactions belonging to this update
+     * @returns {PerformanceMeasure}
+     */
+    traceRender = (
+        id,
+        phase,
+        actualDuration,
+        baseDuration,
+        startTime,
+        commitTime,
+        interactions,
+    ) => performance.measure(id, {
+        start: startTime,
+        duration: actualDuration,
+        detail: {
+            phase,
+            baseDuration,
+            commitTime,
+            interactions,
+        },
+    });
+
+    /**
+     * A HOC that captures render timings of the Wrapped component
+     * @param {object} config
+     * @param {string} config.id
+     * @returns {function(React.Component): React.FunctionComponent}
+     */
+    withRenderTrace = ({id}) => (WrappedComponent) => {
+        const WithRenderTrace = forwardRef((props, ref) => (
+            <Profiler id={id} onRender={traceRender}>
+                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                <WrappedComponent {...props} ref={ref} />
+            </Profiler>
+        ));
+
+        WithRenderTrace.displayName = `withRenderTrace(${getComponentDisplayName(WrappedComponent)})`;
+        return WithRenderTrace;
+    };
 }
 
 export {
     diffObject,
     printPerformanceMetrics,
     setupPerformanceObserver,
+    markStart,
+    markEnd,
+    traceRender,
+    withRenderTrace,
 };
