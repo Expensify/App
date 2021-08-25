4e76e51270c920be21779121863f0ad3675a577c
Santhoshkumar Sellavel <Santhoshsellavel@gmail.com>
2021-08-26 01:05:57 +0530

    Styles h3/h4 missing bold fixed for web, desktop, iOS

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -60,11 +60,13 @@ const styles = {
     h3: {
         fontFamily: fontFamily.GTA_BOLD,
         fontSize: variables.fontSizeNormal,
+        fontWeight: fontWeightBold,
     },
 
     h4: {
         fontFamily: fontFamily.GTA_BOLD,
         fontSize: variables.fontSizeLabel,
+        fontWeight: fontWeightBold,
     },
 
     textAlignCenter: {
