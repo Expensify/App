0942e49de945fa3dece7029b8bc6177eda6f2246
Carlos Martins <cmartins@expensify.com>
2021-08-26 11:10:18 -0600

    fix RN bug in conditional rendering on mobile devices

--- src/pages/ReimbursementAccount/ValidationStep.js
+++ src/pages/ReimbursementAccount/ValidationStep.js
@@ -151,7 +151,7 @@ class ValidationStep extends React.Component {
                                 value={this.state.amount3}
                                 onChangeText={amount3 => this.setState({amount3})}
                             />
-                            {errorMessage && (
+                            {Boolean(errorMessage) && (
                                 <Text style={[styles.mb5, styles.textDanger]}>
                                     {errorMessage}
                                 </Text>
