2b7fade13ea1267ce903923190514a05253ee516
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-08-27 04:49:27 +0530

    fix: count when LHN is open on mobile

--- src/pages/home/report/ReportActionsView.js
+++ src/pages/home/report/ReportActionsView.js
@@ -209,7 +209,7 @@ class ReportActionsView extends React.Component {
                 // Only update the unread count when MarkerBadge is visible
                 // Otherwise marker will be shown on scrolling up from the bottom even if user have read those messages
                 if (this.state.isMarkerActive) {
-                    this.updateLocalUnreadActionCount();
+                    this.updateLocalUnreadActionCount(shouldRecordMaxAction);
                 }
 
                 // show new MarkerBadge when there is a new message
@@ -389,9 +389,14 @@ class ReportActionsView extends React.Component {
 
     /**
      * Update the unread messages count to show in the MarkerBadge
+     * @param {Boolean} [incrementCount=true] Whether count should increment or reset
      */
-    updateLocalUnreadActionCount() {
-        this.setState(prevState => ({localUnreadActionCount: prevState.localUnreadActionCount + this.props.report.unreadActionCount}));
+    updateLocalUnreadActionCount(incrementCount = true) {
+        this.setState(prevState => ({
+            localUnreadActionCount: incrementCount
+                ? prevState.localUnreadActionCount + this.props.report.unreadActionCount
+                : this.props.report.unreadActionCount,
+        }));
     }
 
     /**
