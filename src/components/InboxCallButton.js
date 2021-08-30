5be672a2a01f335994c9e91e4d0154b2a6ebfd81
Alexander <amechler@expensify.com>
2021-08-30 09:53:22 -0700

    Add icons to buttons, fix inboxcallbutton text size

--- src/components/InboxCallButton.js
+++ src/components/InboxCallButton.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import {
-    View, Pressable,
-} from 'react-native';
 import PropTypes from 'prop-types';
-import Icon from './Icon';
-import {Phone} from './Icon/Expensicons';
 import styles from '../styles/styles';
-import themeColors from '../styles/themes/default';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 import compose from '../libs/compose';
 import Navigation from '../libs/Navigation/Navigation';
 import ROUTES from '../ROUTES';
-import Text from './Text';
 import Tooltip from './Tooltip';
+import Button from './Button';
+import {Phone} from './Icon/Expensicons';
 
 const propTypes = {
     ...withLocalizePropTypes,
@@ -30,27 +25,14 @@ const InboxCallButton = props => (
         text={props.translate('requestCallPage.needHelpTooltip')}
         containerStyles={[styles.justifyContentCenter, styles.alignItemsCenter]}
     >
-        <Pressable
+        <Button
             onPress={() => {
                 Navigation.navigate(ROUTES.getRequestCallRoute(props.taskID));
             }}
-            style={[styles.button, styles.buttonSmall]}
-        >
-            <View style={styles.flexRow}>
-                <View style={styles.mr1}>
-                    <Icon
-                        src={Phone}
-                        fill={themeColors.heading}
-                        small
-                    />
-                </View>
-                <View>
-                    <Text style={styles.buttonText}>
-                        {props.translate('requestCallPage.needHelp')}
-                    </Text>
-                </View>
-            </View>
-        </Pressable>
+            text={props.translate('requestCallPage.needHelp')}
+            small
+            icon={Phone}
+        />
     </Tooltip>
 );
 
@@ -58,3 +40,19 @@ InboxCallButton.propTypes = propTypes;
 InboxCallButton.defaultProps = defaultProps;
 InboxCallButton.displayName = 'InboxCallButton';
 export default compose(withLocalize)(InboxCallButton);
+
+// <View style={[styles.flexRow, styles.alignItemsCenter]}>
+//                 <View style={styles.mr1}>
+//                     <Icon
+//                         src={Phone}
+//                         fill={themeColors.heading}
+//                         width={12}
+//                         height={12}
+//                     />
+//                 </View>
+//                 <View>
+//                     <Text style={styles.buttonSmallText}>
+//                         {props.translate('requestCallPage.needHelp')}
+//                     </Text>
+//                 </View>
+//             </View>
