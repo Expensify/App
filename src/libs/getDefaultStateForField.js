e02bd3bfcbf9368366e250fb0ee0d8b944c1ee66
Carlos Martins <cmartins@expensify.com>
2021-09-09 14:08:21 -0600

    change jsdocs

--- src/libs/getDefaultStateForField.js
+++ src/libs/getDefaultStateForField.js
@@ -1,7 +1,8 @@
 import lodashGet from 'lodash/get';
 
 /**
- * Add / to the end of any URL if not present
+ * Get the default state for input fields in the VBA flow
+ *
  * @param {Object} props
  * @param {String} fieldName
  * @param {*} defaultValue
