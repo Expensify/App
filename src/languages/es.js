67360cfcff49ac18455c97d897c7b8626f7c2217
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-09-15 13:05:28 -1000

    fix espanol again

--- src/languages/es.js
+++ src/languages/es.js
@@ -402,8 +402,8 @@ export default {
             dob: 'Ingresa una fecha de nacimiento válida',
             age: 'Los solicitantes deben ser mayores de 18 años',
             ssnLast4: 'Ingresa los últimos 4 dígitos del número de seguro social',
-            firstName: 'Ingresa una nombre válido',
-            lastName: 'Ingresa una apellido válido',
+            firstName: 'Ingresa un nombre válido',
+            lastName: 'Ingresa un apellido válido',
             noDefaultDepositAccountOrDebitCardAvailable: 'Por favor agrega una cuenta bancaria para depósitos o una tarjeta de débito',
             existingOwners: {
                 alreadyInUse: 'La cuenta bancaria ya se encuentra en uso por ',
