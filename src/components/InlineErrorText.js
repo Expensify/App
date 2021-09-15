daaa74869e6e87d161f1c77408a88d7f3a7bb7e0
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-09-15 09:38:18 -1000

    add missing component

---
+++ src/components/InlineErrorText.js
@@ -0,0 +1,28 @@
+import _ from 'underscore';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../styles/styles';
+import Text from './Text';
+
+const propTypes = {
+    /** Text to display */
+    children: PropTypes.string,
+};
+
+const defaultProps = {
+    children: 'Error',
+};
+
+const InlineErrorText = (props) => {
+    if (_.isEmpty(props.children)) {
+        return null;
+    }
+
+    return (
+        <Text style={[styles.formError, styles.mt1]}>{props.children}</Text>
+    );
+};
+
+InlineErrorText.propTypes = propTypes;
+InlineErrorText.defaultProps = defaultProps;
+export default InlineErrorText;
