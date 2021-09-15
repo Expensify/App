4669f5d33946bbdff701d3488db0b3ab81048eb2
Aman Ansari <amanansari.a2@gmail.com>
2021-09-15 23:21:57 +0530

    Update src/libs/actions/Policy.js
    Co-authored-by: Alex Beaman <dabeamanator@gmail.com>

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -265,7 +265,7 @@ function update(policyID, values) {
 
             const updatedValues = {...values, ...{isPolicyUpdating: false}};
             Onyx.merge(`${ONYXKEYS.COLLECTION.POLICY}${policyID}`, updatedValues);
-            Growl.show(translateLocal('workspace.editor.growlMessageOnSave'), CONST.GROWL.SUCCESS, 5000);
+            Growl.show(translateLocal('workspace.editor.growlMessageOnSave'), CONST.GROWL.SUCCESS, 3000);
         }).catch(() => {
             const errorMessage = translateLocal('workspace.editor.genericFailureMessage');
             Growl.error(errorMessage, 5000);
