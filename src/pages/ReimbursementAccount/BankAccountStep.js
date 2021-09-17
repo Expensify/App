c8a22e2fbc803f563d247f30bd2eb466b308c24b
Marco Chávez <marco@expensify.com>
2021-09-17 12:13:14 -0500

    remove step counter in the 1st part of BankAccount

--- src/pages/ReimbursementAccount/BankAccountStep.js
+++ src/pages/ReimbursementAccount/BankAccountStep.js
@@ -169,7 +169,7 @@ class BankAccountStep extends React.Component {
             <View style={[styles.flex1, styles.justifyContentBetween]}>
                 <HeaderWithCloseButton
                     title={this.props.translate('bankAccount.addBankAccount')}
-                    stepCounter={{step: 1, total: 5}}
+                    stepCounter={this.state.bankAccountAddMethod && {step: 1, total: 5}}
                     onCloseButtonPress={Navigation.dismissModal}
                     onBackButtonPress={() => this.setState({bankAccountAddMethod: undefined})}
                     shouldShowBackButton={!_.isUndefined(this.state.bankAccountAddMethod)}
