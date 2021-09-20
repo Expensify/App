028aad67e3ef93642e18e2bee00abefdca96d2ee
Vit Horacek <vit@expensify.com>
2021-09-20 19:37:50 +0100

    Use infinitive form

--- src/languages/es.js
+++ src/languages/es.js
@@ -600,7 +600,7 @@ export default {
             genericFailureMessage: 'Se ha producido un error al intentar eliminar a un usuario del espacio de trabajo. Por favor inténtalo más tarde.',
             removeMembersPrompt: '¿Estás seguro que quieres eliminar a las personas seleccionadas de tu espacio de trabajo?',
             removeMembersTitle: 'Eliminar miembros',
-            selectAll: 'Selecciona todo',
+            selectAll: 'Seleccionar todo',
         },
         card: {
             addEmail: 'Agregar correo electrónico',
