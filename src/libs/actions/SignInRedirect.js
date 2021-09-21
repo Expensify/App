3a55a943132e7c6840259b0f44705da3103d6677
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-09-21 13:29:55 -1000

    remove newline

--- src/libs/actions/SignInRedirect.js
+++ src/libs/actions/SignInRedirect.js
@@ -37,7 +37,6 @@ Onyx.connect({
     },
 });
 
-
 /**
  * Clears the Onyx store and redirects to the sign in page.
  * Normally this method would live in Session.js, but that would cause a circular dependency with Network.js.
