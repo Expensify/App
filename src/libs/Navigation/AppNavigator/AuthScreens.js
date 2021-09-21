8ea5a2e6ac5cdc7cbcc00ac963633de8f8c025a3
Amal Nazeem <amal@expensify.com>
2021-09-21 19:53:10 -0400

    Use Screens Const

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -320,7 +320,7 @@ class AuthScreens extends React.Component {
                     component={ValidateLoginPage}
                 />
                 <RootStack.Screen
-                    name="LogInWithShortLivedToken"
+                    name={SCREENS.LOG_IN_WITH_SHORT_LIVED_TOKEN}
                     options={defaultScreenOptions}
                     component={LogInWithShortLivedTokenPage}
                 />
