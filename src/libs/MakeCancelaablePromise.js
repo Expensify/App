ec77b4bd9069e3f211bf5991195b1e60dee21f86
Nicholas Murray <nmurray@expensify.com>
2021-09-27 18:35:57 -0700

    JSX styling

--- src/libs/MakeCancelaablePromise.js
+++ src/libs/MakeCancelaablePromise.js
@@ -9,12 +9,8 @@ export default function makeCancelablePromise(promise) {
     let hasCanceled_ = false;
 
     const wrappedPromise = new Promise((resolve, reject) => {
-        promise.then((val) =>
-            hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
-        );
-        promise.catch((error) =>
-            hasCanceled_ ? reject({isCanceled: true}) : reject(error)
-        );
+        promise.then((val) => hasCanceled_ ? reject({isCanceled: true}) : resolve(val));
+        promise.catch((error) => hasCanceled_ ? reject({isCanceled: true}) : reject(error));
     });
 
     return {
