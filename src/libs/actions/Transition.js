1ada4f055e3705575907d4f743f895c8e0f7f957
Rory Abraham <rory@expensify.com>
2021-09-28 20:10:53 -0700

    Get rid of navigation in action

--- src/libs/actions/Transition.js
+++ src/libs/actions/Transition.js
@@ -1,5 +1,3 @@
-import ROUTES from '../../ROUTES';
-import Navigation from '../Navigation/Navigation';
 import {getBetas} from './User';
 import Permissions from '../Permissions';
 import {create as createPolicy, getPolicyList, getPolicySummaries} from './Policy';
@@ -18,17 +16,7 @@ function loadBetasAndFetchPolicies(shouldCreateNewWorkspace = false) {
             }
             return Promise.resolve();
         })
-        .then(() => (shouldCreateNewWorkspace ? createPolicy() : Promise.resolve()))
-        .then(({policyID}) => {
-            // In order to navigate to a modal, we first have to dismiss the current modal. But there is no current
-            // modal you say? I know, it confuses me too. Without dismissing the current modal, if the user cancels out
-            // of the workspace modal, then they will be routed back to
-            // /transition/<accountID>/<email>/<authToken>/workspace/<policyID>/card and we don't want that. We want them to go back to `/`
-            // and by calling dismissModal(), the /transition/... route is removed from history so the user will get taken to `/`
-            // if they cancel out of the new workspace modal.
-            Navigation.dismissModal();
-            Navigation.navigate(policyID ? ROUTES.getWorkspaceCardRoute(policyID) : ROUTES.HOME);
-        });
+        .then(() => (shouldCreateNewWorkspace ? createPolicy() : Promise.resolve()));
 }
 
 export {
