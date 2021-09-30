11bfd928bf888cd77f4f2c66eb32cfdebf31b641
Tim Golen <tgolen@expensify.com>
2021-09-30 10:30:35 -0600

    Add skeleton component for address search

---
+++ src/components/AddressSearch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const propTypes = {};
+const defaultProps = {};
+
+class AddressSearch extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<Text>Address Search</Text>);
+    }
+};
+
+AddressSearch.propTypes = propTypes;
+AddressSearch.defaultProps = defaultProps;
+
+export default AddressSearch;
