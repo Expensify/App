79dc6fa3ca2449b7ea4b65ef22e9d6c18a9b221f
Brandon Stites <bstites+1@expensify.com>
2021-09-30 13:47:02 -0600

    Fix permission parens

--- src/libs/Permissions.js
+++ src/libs/Permissions.js
@@ -64,7 +64,7 @@ function canUseInternationalization(betas) {
  * @returns {Boolean}
  */
 function canUseWallet(betas) {
-    return _.contains(betas, CONST.BETAS.BETA_EXPENSIFY_WALLET || canUseAllBetas(betas));
+    return _.contains(betas, CONST.BETAS.BETA_EXPENSIFY_WALLET) || canUseAllBetas(betas);
 }
 
 export default {
