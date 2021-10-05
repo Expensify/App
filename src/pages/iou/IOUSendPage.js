5dee09f13642f4eaa0d4678bf35477cdcfe3f15f
Vit Horacek <vit@expensify.com>
2021-10-05 17:18:45 +0300

    Remove double line

--- src/pages/iou/IOUSendPage.js
+++ src/pages/iou/IOUSendPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import CONST from '../../CONST';
 import IOUModal from './IOUModal';
-import CONST from '../../CONST';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 export default props => <IOUModal {...props} iouType={CONST.IOU.IOU_TYPE.SEND} />;
