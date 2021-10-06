4f15e2aec6eda27ea5683ae8176a653838a0d6fb
Tim Golen <tgolen@expensify.com>
2021-10-06 17:48:52 -0400

    DRY up the reimburse page

--- src/pages/workspace/reimburse/WorkspaceReimbursePage.js
+++ src/pages/workspace/reimburse/WorkspaceReimbursePage.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'underscore';
-import {View, ScrollView} from 'react-native';
-import {withOnyx} from 'react-native-onyx';
-import styles from '../../../styles/styles';
-import Navigation from '../../../libs/Navigation/Navigation';
 import compose from '../../../libs/compose';
-import ROUTES from '../../../ROUTES';
-import HeaderWithCloseButton from '../../../components/HeaderWithCloseButton';
-import ScreenWrapper from '../../../components/ScreenWrapper';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
-import ONYXKEYS from '../../../ONYXKEYS';
-import {fetchFreePlanVerifiedBankAccount} from '../../../libs/actions/BankAccounts';
-import BankAccount from '../../../libs/models/BankAccount';
 import WorkspaceReimburseNoVBAView from './WorkspaceReimburseNoVBAView';
 import WorkspaceReimburseVBAView from './WorkspaceReimburseVBAView';
+import WorkspacePageWithSections from '../WorkspacePageWithSections';
 
 const propTypes = {
     /** The route object passed to this page from the navigator */
@@ -29,49 +19,27 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-class WorkspaceReimbursePage extends React.Component {
-    componentDidMount() {
-        fetchFreePlanVerifiedBankAccount();
-    }
+const WorkspaceReimbursePage = ({translate, route}) => (
+    <WorkspacePageWithSections
+        headerText={translate('workspace.common.reimburse')}
+        route={route}
+    >
+        {(hasVBA, policyID) => (
+            <>
+                {!hasVBA && (
+                    <WorkspaceReimburseNoVBAView policyID={policyID} />
+                )}
 
-    render() {
-        const achState = _.get(this.props.reimbursementAccount, ['achData', 'state'], '');
-        const hasVBA = achState === BankAccount.STATE.OPEN;
-        const policyID = _.get(this.props.route, ['params', 'policyID']);
-
-        return (
-            <ScreenWrapper>
-                <HeaderWithCloseButton
-                    title={this.props.translate('workspace.common.reimburse')}
-                    shouldShowBackButton
-                    onBackButtonPress={() => Navigation.navigate(ROUTES.getWorkspaceInitialRoute(policyID))}
-                    onCloseButtonPress={() => Navigation.dismissModal()}
-                />
-                <ScrollView style={[styles.settingsPageBackground]}>
-                    <View style={styles.w100}>
-
-                        {!hasVBA && (
-                            <WorkspaceReimburseNoVBAView policyID={policyID} />
-                        )}
-
-                        {hasVBA && (
-                            <WorkspaceReimburseVBAView policyID={policyID} />
-                        )}
-
-                    </View>
-                </ScrollView>
-            </ScreenWrapper>
-        );
-    }
-}
+                {hasVBA && (
+                    <WorkspaceReimburseVBAView policyID={policyID} />
+                )}
+            </>
+        )}
+    </WorkspacePageWithSections>
+);
 
 WorkspaceReimbursePage.propTypes = propTypes;
 
 export default compose(
     withLocalize,
-    withOnyx({
-        reimbursementAccount: {
-            key: ONYXKEYS.REIMBURSEMENT_ACCOUNT,
-        },
-    }),
 )(WorkspaceReimbursePage);
