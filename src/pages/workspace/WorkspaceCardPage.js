a15c8c835d5750e27f7083466b72e19813f23c66
Tim Golen <tgolen@expensify.com>
2021-10-06 14:47:46 -0400

    Add logic for detecting the state of the VBA

--- src/pages/workspace/WorkspaceCardPage.js
+++ src/pages/workspace/WorkspaceCardPage.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
-import {View, ScrollView, Text} from 'react-native';
+import {View, ScrollView} from 'react-native';
+import {withOnyx} from 'react-native-onyx';
 import styles from '../../styles/styles';
 import Navigation from '../../libs/Navigation/Navigation';
+import compose from '../../libs/compose';
 import ROUTES from '../../ROUTES';
 import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 import WorkspaceCardNoVBAView from './WorkspaceCardNoVBAView';
 import WorkspaceCardVBANoECardView from './WorkspaceCardVBANoECardView';
-import Icon from '../../components/Icon';
-import {Apple} from '../../components/Icon/Expensicons';
 import WorkspaceCardVBAWithECardView from './WorkspaceCardVBAWithECardView';
+import ONYXKEYS from '../../ONYXKEYS';
+import {fetchFreePlanVerifiedBankAccount} from '../../libs/actions/BankAccounts';
+import VBALoadingIndicator from '../../components/VBALoadingIndicator';
+import lodashGet from 'lodash/get';
+import BankAccount from '../../libs/models/BankAccount';
 
 const propTypes = {
     /** The route object passed to this page from the navigator */
@@ -27,27 +32,61 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const WorkspaceCardPage = ({translate, route}) => (
-    <ScreenWrapper>
-        <HeaderWithCloseButton
-            title={translate('workspace.common.card')}
-            shouldShowBackButton
-            onBackButtonPress={() => Navigation.navigate(ROUTES.getWorkspaceInitialRoute(_.get(route, ['params', 'policyID'])))}
-            onCloseButtonPress={() => Navigation.dismissModal()}
-        />
-        <ScrollView style={[styles.settingsPageBackground]}>
-            <View style={styles.w100}>
+class WorkspaceCardPage extends React.Component {
+    componentDidMount() {
+        fetchFreePlanVerifiedBankAccount();
+    }
 
-                <WorkspaceCardVBAWithECardView />
-                <WorkspaceCardNoVBAView policyID={_.get(route, ['params', 'policyID'])} />
-                <WorkspaceCardVBANoECardView />
+    render() {
+        if (this.props.reimbursementAccount.loading) {
+            return (<VBALoadingIndicator />);
+        }
 
-            </View>
-        </ScrollView>
-    </ScreenWrapper>
-);
+        const achState = lodashGet(this.props.reimbursementAccount, 'achData.state', '');
+        const hasVBA = achState === BankAccount.STATE.OPEN;
+        const isUsingECard = _.get(this.props.user, 'isUsingExpensifyCard');
+        const policyID = _.get(this.props.route, ['params', 'policyID']);
+
+        return (
+            <ScreenWrapper>
+                <HeaderWithCloseButton
+                    title={this.props.translate('workspace.common.card')}
+                    shouldShowBackButton
+                    onBackButtonPress={() => Navigation.navigate(ROUTES.getWorkspaceInitialRoute(policyID))}
+                    onCloseButtonPress={() => Navigation.dismissModal()}
+                />
+                <ScrollView style={[styles.settingsPageBackground]}>
+                    <View style={styles.w100}>
+
+                        {!hasVBA && (
+                            <WorkspaceCardNoVBAView policyID={policyID} />
+                        )}
+
+                        {hasVBA && !isUsingECard && (
+                            <WorkspaceCardVBANoECardView />
+                        )}
+
+                        {hasVBA && isUsingECard && (
+                            <WorkspaceCardVBAWithECardView />
+                        )}
+
+                    </View>
+                </ScrollView>
+            </ScreenWrapper>
+        );
+    }
+}
 
 WorkspaceCardPage.propTypes = propTypes;
-WorkspaceCardPage.displayName = 'WorkspaceCardPage';
 
-export default withLocalize(WorkspaceCardPage);
+export default compose(
+    withLocalize,
+    withOnyx({
+        user: {
+            key: ONYXKEYS.USER,
+        },
+        reimbursementAccount: {
+            key: ONYXKEYS.REIMBURSEMENT_ACCOUNT,
+        },
+    }),
+)(WorkspaceCardPage);
--- src/pages/workspace/WorkspaceCardPage2.js
+++ src/pages/workspace/WorkspaceCardPage2.js
@@ -1,3 +1,4 @@
+/* TODO: Delete this file! */
 import React from 'react';
 import {
     View, ScrollView, StyleSheet,
