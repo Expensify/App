191273d9d271e23f4d7ab145023f76a016e3ed1d
Tim Golen <tgolen@expensify.com>
2021-10-06 17:22:48 -0400

    Parse user's email domain

--- src/pages/workspace/bills/WorkspaceBillsNoVBAView.js
+++ src/pages/workspace/bills/WorkspaceBillsNoVBAView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import PropTypes from 'prop-types';
+import {withOnyx} from 'react-native-onyx';
 import styles from '../../../styles/styles';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
 import {
@@ -13,8 +14,11 @@ import {
 import WorkspaceSection from '../WorkspaceSection';
 import CopyTextToClipboard from '../../../components/CopyTextToClipboard';
 import {openSignedInLink} from '../../../libs/actions/App';
+import compose from '../../../libs/compose';
 import Navigation from '../../../libs/Navigation/Navigation';
 import ROUTES from '../../../ROUTES';
+import Str from 'expensify-common/lib/str';
+import ONYXKEYS from '../../../ONYXKEYS';
 
 const propTypes = {
     /** The policy ID currently being configured */
@@ -23,55 +27,65 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const WorkspaceBillsNoVBAView = ({translate, policyID}) => (
-    <>
-        <WorkspaceSection
-            title={translate('workspace.bills.manageYourBills')}
-            icon={Apple} // TODO: Replace this with the proper icon
-            menuItems={[
-                {
-                    title: translate('workspace.bills.viewAllBills'),
-                    // eslint-disable-next-line max-len
-                    onPress: () => openSignedInLink(`https://www.expensify.com/reports?param={"startDate":"","endDate":"","reportName":"","policyID":"${policyID}","from":"all","type":"bill","states":{"Open":true,"Processing":true,"Approved":true,"Reimbursed":true,"Archived":true},"isAdvancedFilterMode":true}`),
-                    icon: Receipt, // TODO: use the bill icon once it's added to this repo
-                    shouldShowRightIcon: true,
-                    iconRight: NewWindow,
-                },
-            ]}
-        >
-            <View style={[styles.mv4]}>
-                <Text>
-                    {translate('workspace.bills.askYourVendorsBeforeEmail')}
-                    <CopyTextToClipboard
-                        text="your.domain@expensify.com"
-                        textStyles={[styles.textBlue]}
-                    />
-                    <Text>{translate('workspace.bills.askYourVendorsAfterEmail')}</Text>
-                </Text>
-            </View>
-        </WorkspaceSection>
+const WorkspaceBillsNoVBAView = ({translate, policyID, session}) => {
+    const emailDomain = Str.extractEmailDomain(session.email);
+    return (
+        <>
+            <WorkspaceSection
+                title={translate('workspace.bills.manageYourBills')}
+                icon={Apple} // TODO: Replace this with the proper icon
+                menuItems={[
+                    {
+                        title: translate('workspace.bills.viewAllBills'),
+                        // eslint-disable-next-line max-len
+                        onPress: () => openSignedInLink(`https://www.expensify.com/reports?param={"startDate":"","endDate":"","reportName":"","policyID":"${policyID}","from":"all","type":"bill","states":{"Open":true,"Processing":true,"Approved":true,"Reimbursed":true,"Archived":true},"isAdvancedFilterMode":true}`),
+                        icon: Receipt, // TODO: use the bill icon once it's added to this repo
+                        shouldShowRightIcon: true,
+                        iconRight: NewWindow,
+                    },
+                ]}
+            >
+                <View style={[styles.mv4]}>
+                    <Text>
+                        {translate('workspace.bills.askYourVendorsBeforeEmail')}
+                        <CopyTextToClipboard
+                            text={`${emailDomain}@expensify.cash`}
+                            textStyles={[styles.textBlue]}
+                        />
+                        <Text>{translate('workspace.bills.askYourVendorsAfterEmail')}</Text>
+                    </Text>
+                </View>
+            </WorkspaceSection>
 
-        <WorkspaceSection
-            title={translate('workspace.bills.unlockOnlineBillPayment')}
-            icon={Apple} // TODO: Replace this with the proper icon
-            menuItems={[
-                {
-                    title: translate('workspace.common.bankAccount'),
-                    onPress: () => Navigation.navigate(ROUTES.getWorkspaceBankAccountRoute(policyID)),
-                    icon: Bank,
-                    shouldShowRightIcon: true,
-                    iconRight: ArrowRight,
-                },
-            ]}
-        >
-            <View style={[styles.mv4]}>
-                <Text>{translate('workspace.bills.unlockNoVBACopy')}</Text>
-            </View>
-        </WorkspaceSection>
-    </>
-);
+            <WorkspaceSection
+                title={translate('workspace.bills.unlockOnlineBillPayment')}
+                icon={Apple} // TODO: Replace this with the proper icon
+                menuItems={[
+                    {
+                        title: translate('workspace.common.bankAccount'),
+                        onPress: () => Navigation.navigate(ROUTES.getWorkspaceBankAccountRoute(policyID)),
+                        icon: Bank,
+                        shouldShowRightIcon: true,
+                        iconRight: ArrowRight,
+                    },
+                ]}
+            >
+                <View style={[styles.mv4]}>
+                    <Text>{translate('workspace.bills.unlockNoVBACopy')}</Text>
+                </View>
+            </WorkspaceSection>
+        </>
+    );
+};
 
 WorkspaceBillsNoVBAView.propTypes = propTypes;
 WorkspaceBillsNoVBAView.displayName = 'WorkspaceBillsNoVBAView';
 
-export default withLocalize(WorkspaceBillsNoVBAView);
+export default compose(
+    withLocalize,
+    withOnyx({
+        session: {
+            key: ONYXKEYS.SESSION,
+        },
+    }),
+)(WorkspaceBillsNoVBAView);
