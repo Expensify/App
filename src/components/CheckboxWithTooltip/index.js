f5b06e5e891a325cbea6d446481283ac3caa5423
Rocio Perez <rocio@expensify.com>
2021-10-08 12:10:44 +0300

    Check for dimensions

--- src/components/CheckboxWithTooltip/index.js
+++ src/components/CheckboxWithTooltip/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {View} from 'react-native';
+import CheckboxWithTooltipNative from 'index.native';
 import Checkbox from '../Checkbox';
 import {propTypes, defaultProps} from './CheckboxWithTooltipPropTypes';
 import Tooltip from '../Tooltip';
+import withWindowDimensions from '../withWindowDimensions';
+import compose from '../../libs/compose';
 
 /**
  * @param {propTypes} props
  * @returns {ReactNodeLike}
  */
 const CheckboxWithTooltip = (props) => {
+    if (props.isSmallScreenWidth || props.isMediumScreenWidth) {
+        return (
+            <CheckboxWithTooltipNative
+                style={props.style}
+                isChecked={props.isChecked}
+                onPress={props.onPress}
+                text={props.text}
+                disabled={props.disabled}
+                toggleTooltip={props.toggleTooltip}
+            />
+        );
+    }
     const checkbox = (
         <Checkbox
             isChecked={props.isChecked}
@@ -32,4 +47,7 @@ const CheckboxWithTooltip = (props) => {
 CheckboxWithTooltip.propTypes = propTypes;
 CheckboxWithTooltip.defaultProps = defaultProps;
 CheckboxWithTooltip.displayName = 'CheckboxWithTooltip';
-export default CheckboxWithTooltip;
+
+export default compose(
+    withWindowDimensions,
+)(CheckboxWithTooltip);
