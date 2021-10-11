fe815727b79b5d4417d062379ce9304c6fb90555
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-10-11 11:51:24 +0530

    cleanup

--- src/components/Tooltip/TooltipSense.js
+++ src/components/Tooltip/TooltipSense.js
@@ -2,6 +2,10 @@ import _ from 'underscore';
 import CONST from '../../CONST';
 
 let active = false;
+
+/**
+ * Debounced function to deactive the TooltipSense after a specific time
+*/
 const debouncedDeactivate = _.debounce(() => {
     active = false;
 }, CONST.TIMING.TOOLTIP_SENSE);
@@ -11,12 +15,12 @@ function activate() {
     debouncedDeactivate.cancel();
 }
 
-function isActive() {
-    return active === true;
-}
-
 function deactivate() {
     return debouncedDeactivate();
+}
+
+function isActive() {
+    return active === true;
 }
 
 export default {
