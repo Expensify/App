9332c92adb017ec76bc49a1419cc2ad514af6c7e
Tim Golen <tgolen@expensify.com>
2021-10-12 10:39:47 -0600

    Add new action

---
+++ src/libs/actions/Link.js
@@ -0,0 +1,50 @@
+import Onyx from 'react-native-onyx';
+import lodashGet from 'lodash/get';
+import {Linking} from 'react-native';
+import ONYXKEYS from '../../ONYXKEYS';
+import Growl from '../Growl';
+import {translateLocal} from '../translate';
+import CONST from '../../CONST';
+import * as API from '../API';
+import CONFIG from '../../CONFIG';
+
+let isNetworkOffline = false;
+Onyx.connect({
+    key: ONYXKEYS.NETWORK,
+    callback: val => isNetworkOffline = lodashGet(val, 'isOffline', false),
+});
+
+let currentUserEmail;
+Onyx.connect({
+    key: ONYXKEYS.SESSION,
+    callback: val => currentUserEmail = lodashGet(val, 'email', ''),
+});
+
+/**
+ * @returns {Boolean}
+ */
+function showGrowlIfOffline() {
+    if (isNetworkOffline) {
+        Growl.show(translateLocal('session.offlineMessageRetry'), CONST.GROWL.WARNING);
+    }
+    return isNetworkOffline;
+}
+
+function openOldDotLink(url) {
+    if (!showGrowlIfOffline()) {
+        API.GetShortLivedAuthToken().then(({shortLivedAuthToken}) => {
+            Linking.openURL(`${CONFIG.EXPENSIFY.URL_EXPENSIFY_COM}${url}${url.indexOf('?') === -1 ? '?' : '&'}authToken=${shortLivedAuthToken}&email=${encodeURIComponent(currentUserEmail)}`);
+        });
+    }
+}
+
+function openExternalLink(url) {
+    if (!showGrowlIfOffline()) {
+        Linking.openURL(url);
+    }
+}
+
+export {
+    openOldDotLink,
+    openExternalLink,
+};
