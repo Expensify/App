868873e6ed872db4d10768155660dbb2e6b240d9
Amal Nazeem <amal@expensify.com>
2021-10-13 18:56:30 -0400

    Fix max-len issue

--- src/pages/workspace/reimburse/WorkspaceReimburseNoVBAView.js
+++ src/pages/workspace/reimburse/WorkspaceReimburseNoVBAView.js
@@ -31,6 +31,7 @@ const WorkspaceReimburseNoVBAView = ({translate, policyID}) => (
             menuItems={[
                 {
                     title: translate('workspace.reimburse.viewAllReceipts'),
+                    // eslint-disable-next-line max-len
                     onPress: () => openOldDotLink(`reports?param={"startDate":"","endDate":"","reportName":"","policyID":"${policyID}","from":"all","type":"expense","showStates":"Archived","isAdvancedFilterMode":"true"}`),
                     icon: Receipt,
                     shouldShowRightIcon: true,
