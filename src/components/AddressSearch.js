d51e03dfc15ca153514a082be136277ae151182c
Luke Donahue <luke@expensify.com>
2021-10-12 19:41:10 -0700

    refactor to funcitonal component

--- src/components/AddressSearch.js
+++ src/components/AddressSearch.js
@@ -1,5 +1,5 @@
 import _ from 'underscore';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {LogBox} from 'react-native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
@@ -33,55 +33,49 @@ const defaultProps = {
     containerStyles: null,
 };
 
-class AddressSearch extends React.Component {
-    constructor(props) {
-        super(props);
-        this.googlePlacesRef = React.createRef();
-    }
+const AddressSearch = (props) => {
+    const googlePlacesRef = useRef();
+    useEffect(() => {
+        googlePlacesRef.current?.setAddressText(props.value);
+      }, []);
 
-    componentDidMount() {
-        this.googlePlacesRef.current?.setAddressText(this.props.value);
-    }
-
-    getAddressComponent(object, field, nameType) {
+    const getAddressComponent = (object, field, nameType) => {
         return _.chain(object.address_components)
             .find(component => _.contains(component.types, field))
             .get(nameType)
             .value();
     }
 
-    /**
-     * @param {Object} details See https://developers.google.com/maps/documentation/places/web-service/details#PlaceDetailsResponses
-     */
-    saveLocationDetails = (details) => {
+
+    const saveLocationDetails = (details) => {
         if (details.address_components) {
             // Gather the values from the Google details
-            const streetNumber = this.getAddressComponent(details, 'street_number', 'long_name');
-            const streetName = this.getAddressComponent(details, 'route', 'long_name');
-            const city = this.getAddressComponent(details, 'locality', 'long_name');
-            const state = this.getAddressComponent(details, 'administrative_area_level_1', 'short_name');
-            const zipCode = this.getAddressComponent(details, 'postal_code', 'long_name');
+            const streetNumber = getAddressComponent(details, 'street_number', 'long_name');
+            const streetName = getAddressComponent(details, 'route', 'long_name');
+            const city = getAddressComponent(details, 'locality', 'long_name');
+            const state = getAddressComponent(details, 'administrative_area_level_1', 'short_name');
+            const zipCode = getAddressComponent(details, 'postal_code', 'long_name');
 
             // Trigger text change events for each of the individual fields being saved on the server
-            this.props.onChangeText('addressStreet', `${streetNumber} ${streetName}`);
-            this.props.onChangeText('addressCity', city);
-            this.props.onChangeText('addressState', state);
-            this.props.onChangeText('addressZipCode', zipCode);
+            props.onChangeText('addressStreet', `${streetNumber} ${streetName}`);
+            props.onChangeText('addressCity', city);
+            props.onChangeText('addressState', state);
+            props.onChangeText('addressZipCode', zipCode);
         }
     }
 
-    render() {
-        return (
+    return (
+        <>
             <GooglePlacesAutocomplete
-                ref={this.googlePlacesRef}
+                ref={googlePlacesRef}
                 fetchDetails
                 keepResultsAfterBlur
                 suppressDefaultStyles
                 enablePoweredByContainer={false}
-                onPress={(data, details) => this.saveLocationDetails(details)}
+                onPress={(data, details) => saveLocationDetails(details)}
                 query={{
                     key: 'AIzaSyC4axhhXtpiS-WozJEsmlL3Kg3kXucbZus',
-                    language: this.props.preferredLocale,
+                    language: props.preferredLocale,
                 }}
                 requestUrl={{
                     useOnPlatform: 'web',
@@ -89,8 +83,8 @@ class AddressSearch extends React.Component {
                 }}
                 textInputProps={{
                     InputComp: ExpensiTextInput,
-                    label: this.props.label,
-                    containerStyles: this.props.containerStyles,
+                    label: props.label,
+                    containerStyles: props.containerStyles,
                 }}
                 styles={{
                     textInputContainer: [styles.flexColumn],
@@ -111,8 +105,9 @@ class AddressSearch extends React.Component {
                     separator: [styles.googleSearchSeparator],
                 }}
             />
-        );
-    }
+        </>
+    );
+
 }
 
 AddressSearch.propTypes = propTypes;
