2b235686468e52c5bc4005a2e98ee051bb99e220
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-10-15 22:57:38 +0530

    remove comment

--- src/components/Modal/index.ios.js
+++ src/components/Modal/index.ios.js
@@ -3,8 +3,6 @@ import withWindowDimensions from '../withWindowDimensions';
 import BaseModal from './BaseModal';
 import {propTypes, defaultProps} from './ModalPropTypes';
 
-// Only want to use <SafeAreaView> on iOS. Avoids ScrollBar in the middle of the modal.
-// https://github.com/facebook/react-native/issues/26610
 const Modal = props => (
     <BaseModal
             // eslint-disable-next-line react/jsx-props-no-spreading
