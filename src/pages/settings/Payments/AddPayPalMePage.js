23961abcab68577c50b63c84523bf0a81e810d89
Alfredo Alcantara <alfredo.alcantara.r@gmail.com>
2021-10-20 16:44:26 -0700

    Removes textinput ref

--- src/pages/settings/Payments/AddPayPalMePage.js
+++ src/pages/settings/Payments/AddPayPalMePage.js
@@ -41,7 +41,6 @@ class AddPayPalMePage extends React.Component {
             payPalMeUsernameError: false,
         };
         this.setPayPalMeUsername = this.setPayPalMeUsername.bind(this);
-        this.paypalUsernameInputRef = null;
     }
 
     componentDidMount() {
@@ -88,7 +87,6 @@ class AddPayPalMePage extends React.Component {
                             </Text>
                             <ExpensiTextInput
                                 label={this.props.translate('addPayPalMePage.payPalMe')}
-                                ref={el => this.paypalUsernameInputRef = el}
                                 autoCompleteType="off"
                                 autoCorrect={false}
                                 value={this.state.payPalMeUsername}
