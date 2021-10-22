23ad37fe0c28ecbb4ed1a3b2cb2bf41f952a2f58
Aldo Canepa <aldo@expensify.com>
2021-10-22 16:30:09 -0700

    Add documentation to GooglePlacesUtils.js

--- src/libs/GooglePlacesUtils.js
+++ src/libs/GooglePlacesUtils.js
@@ -1,12 +1,32 @@
 import _ from 'underscore';
 
-function getAddressComponent(addressComponents, type, field) {
+/**
+ * Finds an address component by type, and returns the value associated to key. Each address component object
+ * inside the addressComponents array has the following structure:
+ * {
+ *   long_name: "New York",
+ *   short_name: "New York",
+ *   types: [ "locality", "political" ]
+ * }
+ *
+ * @param {Array} addressComponents
+ * @param {String} type
+ * @param {String} key
+ * @returns {String|undefined}
+ */
+function getAddressComponent(addressComponents, type, key) {
     return _.chain(addressComponents)
         .find(component => _.contains(component.types, type))
-        .get(field)
+        .get(key)
         .value();
 }
 
+/**
+ * Validates this contains the minimum address components
+ *
+ * @param {Array} addressComponents
+ * @returns {Boolean}
+ */
 function validateAddressComponents(addressComponents) {
     if (!addressComponents) {
         return false;
