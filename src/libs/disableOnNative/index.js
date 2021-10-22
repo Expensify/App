6340532059db63ce4ddc4401edd600b76d1cf328
Rajat Parashar <parasharrajat@users.noreply.github.com>
2021-10-23 03:20:39 +0530

    comments

--- src/libs/disableOnNative/index.js
+++ src/libs/disableOnNative/index.js
@@ -1,1 +1,6 @@
+/**
+ * On non-native devices, just pass the boolean value straight on through.
+ * @param {Boolean} value
+ * @returns {Boolean}
+ */
 export default value => Boolean(value);
--- src/libs/disableOnNative/index.native.js
+++ src/libs/disableOnNative/index.native.js
@@ -1,1 +1,7 @@
+/**
+ * On native devices, always return false.
+ * This is needed so we can always disable some feature on native devices that should be
+ * conditionally enabled / disabled on web & desktop.
+ * @returns {Boolean} false always
+ */
 export default () => false;
