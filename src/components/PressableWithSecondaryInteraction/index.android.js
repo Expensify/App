0129d55077930687ec6c99c89a2aa2baf51e74e6
Rushat Gabhane <rushatgabhane@gmail.com>
2021-10-28 15:16:12 +0300

    add comment explaining android version

--- src/components/PressableWithSecondaryInteraction/index.android.js
+++ src/components/PressableWithSecondaryInteraction/index.android.js
@@ -14,6 +14,8 @@ function handleLongPress(event, props) {
     event.preventDefault();
     props.onSecondaryInteraction(event);
 
+    // The constant effectHeavyClick is added in API level 29. Docs: https://developer.android.com/reference/android/os/VibrationEffect#EFFECT_HEAVY_CLICK 
+    // We use keyboardTap added in API level 8 as a fallback. Docs: https://developer.android.com/reference/android/view/HapticFeedbackConstants#KEYBOARD_TAP
     if (Platform.Version >= 29) {
         ReactNativeHapticFeedback.trigger('effectHeavyClick');
         return;
