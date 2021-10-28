1bb3d17c2672451616fa598204df095cd0a4babb
Rushat Gabhane <rushatgabhane@gmail.com>
2021-10-29 02:42:39 +0300

    revert ios config for unimodules

--- ios/NewExpensify/AppDelegate.h
+++ ios/NewExpensify/AppDelegate.h
@@ -2,9 +2,7 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-#import <UMCore/UMAppDelegateWrapper.h>
-
-@interface AppDelegate : UMAppDelegateWrapper <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
