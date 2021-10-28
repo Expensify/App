eddab69b07746b86739b9699af9761c200056c04
Brandon Stites <bstites+1@expensify.com>
2021-10-28 17:54:51 -0600

    keep types consistent

--- src/libs/actions/BankAccounts.js
+++ src/libs/actions/BankAccounts.js
@@ -876,7 +876,7 @@ function resetFreePlanBankAccount() {
             // Clear reimbursement account, draft user input, and the bank account list
             Onyx.set(ONYXKEYS.REIMBURSEMENT_ACCOUNT, {});
             Onyx.set(ONYXKEYS.REIMBURSEMENT_ACCOUNT_DRAFT, null);
-            Onyx.set(ONYXKEYS.BANK_ACCOUNT_LIST, []);
+            Onyx.set(ONYXKEYS.BANK_ACCOUNT_LIST, {});
 
             // Clear the NVP for the bank account so the user can add a new one
             API.SetNameValuePair({name: CONST.NVP.FREE_PLAN_BANK_ACCOUNT_ID, value: ''});
