4664a8a672d7633af4bfef12dbaa8ba47dbed3af
Vit Horacek <vit@expensify.com>
2021-11-01 19:32:27 +0000

    Add comment

--- .github/libs/GitUtils.js
+++ .github/libs/GitUtils.js
@@ -13,6 +13,8 @@ function getPullRequestsMergedBetween(fromRef, toRef) {
     console.log('Getting pull requests merged between the following refs:', fromRef, toRef);
     console.log('Running command: ', command);
     const localGitLogs = execSync(command).toString();
+
+    // Filter out merge commits which are introduced by cherry-picking
     return _.map(
         _.filter(
             [...localGitLogs.matchAll(/Merge pull request #(\d{1,6}) from (?!Expensify\/(?:master|main|version-))/g)],
