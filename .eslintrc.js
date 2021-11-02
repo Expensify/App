4cc724e9fdb0c17dda1bf866724b507dbdbfce24
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-11-01 21:20:31 -1000

    move various rules to eslint-config-expensify

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,34 +2,8 @@ module.exports = {
     extends: 'expensify',
     parser: 'babel-eslint',
     ignorePatterns: ['!.*', 'src/vendor', '.github/actions/**/index.js'],
-    rules: {
-        'react/jsx-filename-extension': [1, {extensions: ['.js']}],
-        'comma-dangle': ['error', 'always-multiline'],
-        'import/group-exports': 'error',
-        'import/prefer-default-export': 0,
-        'es/no-nullish-coalescing-operators': 'error',
-        'es/no-optional-chaining': 'error',
-        'react/destructuring-assignment': [2, 'never'],
-
-        /**
-         * Custom rules from the eslint-local-rules directory
-         */
-        'local-rules/no-negated-variables': 'error',
-        'local-rules/no-api-in-views': 'error',
-        'local-rules/prefer-actions-set-data': 'error',
-        'local-rules/prefer-onyx-connect-in-libs': 'error',
-        'local-rules/no-thenable-actions-in-views': 'error',
-        'local-rules/prefer-early-return': 'error',
-        'local-rules/no-inline-named-export': 'error',
-        'local-rules/prefer-import-module-contents': 'error',
-        'local-rules/prefer-underscore-method': 'error',
-        'local-rules/no-useless-compose': 'error',
-    },
     plugins: [
         'detox',
-        'eslint-plugin-local-rules',
-        'import',
-        'eslint-plugin-es',
     ],
     env: {
         jest: true,
