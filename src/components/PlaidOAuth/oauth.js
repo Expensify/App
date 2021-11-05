b9df3006334485809e08c55ff3717abf869c5c8d
Nicholas Murray <nmurray@expensify.com>
2021-11-04 17:16:58 -0700

    Hook up OAuth redirect with onyx plaidLinkToken

--- src/components/PlaidOAuth/oauth.js
+++ src/components/PlaidOAuth/oauth.js
@@ -1,8 +1,16 @@
 import React, { useEffect } from 'react';
 import { usePlaidLink } from 'react-plaid-link';
-const OAuthLink = (callback, deps) => {
+import compose from "../../libs/compose";
+import withLocalize from "../withLocalize";
+import {withOnyx} from "react-native-onyx";
+import ONYXKEYS from "../../ONYXKEYS";
+import {plaidLinkDefaultProps, plaidLinkPropTypes} from "../PlaidLink/plaidLinkPropTypes";
+import PlaidLink from "../PlaidLink";
+
+const OAuthLink = (props, callback, deps) => {
     // The Link token from the first Link initialization
-    const linkToken = localStorage.getItem('link_token');
+    // const linkToken = localStorage.getItem('link_token');
+    const linkToken = props.plaidLinkToken;
     const onSuccess = React.useCallback((public_token) => {
         // send public_token to server, retrieve access_token and item_id
         // return to "https://example.com" upon completion
@@ -25,4 +33,20 @@ const OAuthLink = (callback, deps) => {
     }, [ready, open]);
     return <></>;
 };
-export default OAuthLink;
+OAuthLink.propTypes = plaidLinkPropTypes;
+OAuthLink.defaultProps = plaidLinkDefaultProps;
+OAuthLink.displayName = 'OAuthLink';
+
+export default compose(
+    withOnyx({
+        plaidLinkToken: {
+            key: ONYXKEYS.PLAID_LINK_TOKEN,
+
+            // We always fetch a new token to call Plaid. If we don't then it's possible to open multiple Plaid Link instances. In particular, this can cause issues for Android e.g.
+            // inability to hand off to React Native once the bank connection is made. This is because an old stashed token will mount the PlaidLink component then it gets set again
+            // which will mount another PlaidLink component.
+            initWithStoredValues: false,
+        }
+    })
+)(OAuthLink);
+
