969c75147a47b573bb758442ef8452642c92bb5b
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-11-08 11:40:03 -0500

    fix bad merge

--- src/libs/API.js
+++ src/libs/API.js
@@ -84,38 +84,6 @@ function addDefaultValuesToParameters(command, parameters) {
 Network.registerParameterEnhancer(addDefaultValuesToParameters);
 
 /**
-<<<<<<< HEAD
-=======
- * @throws {Error} If the "parameters" object has a null or undefined value for any of the given parameterNames
- *
- * @param {String[]} parameterNames Array of the required parameter names
- * @param {Object} parameters A map from available parameter names to their values
- * @param {String} commandName The name of the API command
- */
-function requireParameters(parameterNames, parameters, commandName) {
-    parameterNames.forEach((parameterName) => {
-        if (_(parameters).has(parameterName)
-            && parameters[parameterName] !== null
-            && parameters[parameterName] !== undefined
-        ) {
-            return;
-        }
-
-        const propertiesToRedact = ['authToken', 'password', 'partnerUserSecret', 'twoFactorAuthCode'];
-        const parametersCopy = _.chain(parameters)
-            .clone()
-            .mapObject((val, key) => (_.contains(propertiesToRedact, key) ? '<redacted>' : val))
-            .value();
-        const keys = _(parametersCopy).keys().join(', ') || 'none';
-
-        let error = `Parameter ${parameterName} is required for "${commandName}". `;
-        error += `Supplied parameters: ${keys}`;
-        throw new Error(error);
-    });
-}
-
-/**
->>>>>>> origin
  * Function used to handle expired auth tokens. It re-authenticates with the API and
  * then replays the original request
  *
