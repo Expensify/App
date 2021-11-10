37024573d6a566df15c353b3ff0c807f5f89ae72
Nicholas Murray <nmurray@expensify.com>
2021-11-10 14:43:44 -0500

    Delete onyx key

--- src/ONYXKEYS.js
+++ src/ONYXKEYS.js
@@ -147,7 +147,4 @@ export default {
 
     // Are report actions loading?
     IS_LOADING_REPORT_ACTIONS: 'isLoadingReportActions',
-
-    // Stores which bank account flow, personal or business
-    IS_ADDING_BBA: 'isAddingBBA',
 };
