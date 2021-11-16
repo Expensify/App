5ff5cd7f4c529e04659233e61d5567b8f23b982d
Jasper Huang <jasper@expensify.com>
2021-11-15 18:29:33 -0800

    WorkspaceNewRoomPage WIP

--- src/pages/workspace/WorkspaceNewRoomPage.js
+++ src/pages/workspace/WorkspaceNewRoomPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {withOnyx} from 'react-native-onyx';
 import withFullPolicy, {fullPolicyDefaultProps, fullPolicyPropTypes} from './withFullPolicy';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 import ONYXKEYS from '../../CONST';
 import compose from '../../libs/compose';
+import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
+import Navigation from '../../libs/Navigation/Navigation';
+import ScreenWrapper from '../../components/ScreenWrapper';
+import styles from '../../styles/styles';
+import TextInputWithLabel from '../../components/TextInputWithLabel';
 
 const propTypes = {
     ...fullPolicyPropTypes,
@@ -19,13 +24,39 @@ const defaultProps = {
 class WorkspaceNewRoomPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            roomName: '',
+        };
+    }
+
+    onSubmit() {
+
     }
 
     render() {
         return (
-            <View>
-                <Text>Hi</Text>
-            </View>
+            <ScreenWrapper>
+                <HeaderWithCloseButton
+                    title={this.props.translate('newRoomPage.newRoom')}
+                    onCloseButtonPress={() => Navigation.dismissModal()}
+                />
+                <View style={[styles.flex1, styles.w100, styles.pRelative, styles.p5]}>
+                    <TextInputWithLabel
+                        label={this.props.translate('newRoomPage.roomName')}
+                        prefixCharacter="#"
+                        placeholder={this.props.translate('newRoomPage.social')}
+                        containerStyles={[styles.mb5]}
+                        onChangeText={roomName => {
+                            this.setState({roomName});
+                        }}
+                    />
+                    {/*<ExpensiPicker*/}
+                    {/*    label={this.props.translate('workspace.common.workspace')}*/}
+                    {/*/>*/}
+                </View>
+
+            </ScreenWrapper>
         );
     }
 }
