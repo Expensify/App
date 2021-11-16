2b4063fbd9952ce28f28aa0bd748a8c1c489c589
Joe Gambino <joe@expensify.com>
2021-11-15 16:04:43 -0800

    add fallback

--- .github/actions/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
+++ .github/actions/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
@@ -5,7 +5,7 @@ const {PLIST_PATH} = require('../../libs/nativeVersionUpdater');
 const bundleVersion = execSync(`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" ${PLIST_PATH}`);
 const shortBundleVersion = execSync(`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ${PLIST_PATH}`);
 
-if (shortBundleVersion !== bundleVersion.split('-')[0]) {
+if (shortBundleVersion !== (bundleVersion.split('-') || [''])[0]) {
     console.log('Bundle Versions do not match');
     core.setOutput('BUNDLE_VERSIONS_MATCH', false);
 } else {
