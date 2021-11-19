843bb9174d567577a176148cc21d0aa7ba15b3da
Amal Nazeem <amal@expensify.com>
2021-11-19 15:30:38 -0700

    Update Componet to be functional to use useDeepLinkRedirector

--- src/components/PlaidLink/index.native.js
+++ src/components/PlaidLink/index.native.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import {NativeEventEmitter} from 'react-native';
 import {openLink, useDeepLinkRedirector} from 'react-native-plaid-link-sdk';
 import Log from '../../libs/Log';
@@ -6,53 +6,41 @@ import CONST from '../../CONST';
 import nativeModule from './nativeModule';
 import {plaidLinkPropTypes, plaidLinkDefaultProps} from './plaidLinkPropTypes';
 
-class PlaidLink extends React.Component {
-    constructor(props) {
-        super(props);
-        this.listener = undefined;
-    }
 
-    componentDidMount() {
+const PlaidLink  = (props) => {
+    let listener = undefined;
+    useDeepLinkRedirector();
+    useEffect(() => {
         const emitter = new NativeEventEmitter(nativeModule);
-        this.listener = emitter.addListener('onEvent', this.onEvent.bind(this));
-
-        // Use this hook so that deep linking works for plaid on iOS
-        useDeepLinkRedirector();
+        listener = emitter.addListener('onEvent', (event) => {
+            Log.info('[PlaidLink] Handled Plaid Event: ', false, event);
+            if (event.eventName === CONST.PLAID.EVENT.ERROR) {
+                props.onError(event.metadata);
+            } else if (event.eventName === CONST.PLAID.EVENT.EXIT) {
+                props.onExit();
+            }
+        });
 
         openLink({
             tokenConfig: {
-                token: this.props.token,
+                token: props.token,
             },
             onSuccess: ({publicToken, metadata}) => {
-                this.props.onSuccess({publicToken, metadata});
+                props.onSuccess({publicToken, metadata});
             },
         });
-    }
 
-    componentWillUnmount() {
-        if (!this.listener) {
-            return;
+        return () => {
+            if (listener) {
+                return;
+            }
+
+            listener.remove();
         }
+    }, []);
 
-        this.listener.remove();
-    }
-
-    /**
-     * @param {*} event
-     */
-    onEvent(event) {
-        Log.info('[PlaidLink] Handled Plaid Event: ', false, event);
-        if (event.eventName === CONST.PLAID.EVENT.ERROR) {
-            this.props.onError(event.metadata);
-        } else if (event.eventName === CONST.PLAID.EVENT.EXIT) {
-            this.props.onExit();
-        }
-    }
-
-    render() {
-        return null;
-    }
-}
+    return null;
+};
 
 PlaidLink.propTypes = plaidLinkPropTypes;
 PlaidLink.defaultProps = plaidLinkDefaultProps;
