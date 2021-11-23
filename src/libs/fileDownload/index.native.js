9ab6e430984547433da3adad9a91eecf71cee4e7
Manan Jadhav <manan.jadhav@gmail.com>
2021-11-24 03:00:34 +0530

    fix(loading-indicator-for-attachment): Return Promise from all download functions

--- src/libs/fileDownload/index.native.js
+++ src/libs/fileDownload/index.native.js
@@ -53,52 +53,57 @@ function showAlert(content) {
  * Handling the download
  * @param {String} url
  * @param {String} fileName
+ * @returns {Promise}
  */
 function handleDownload(url, fileName) {
-    const dirs = RNFetchBlob.fs.dirs;
+    return new Promise((resolve) => {
+        const dirs = RNFetchBlob.fs.dirs;
 
-    // android files will download to Download directory
-    // ios files will download to documents directory
-    const path = getPlatform() === 'android' ? dirs.DownloadDir : dirs.DocumentDir;
-    const attachmentName = fileName || getAttachmentName(url);
+        // android files will download to Download directory
+        // ios files will download to documents directory
+        const path = getPlatform() === 'android' ? dirs.DownloadDir : dirs.DocumentDir;
+        const attachmentName = fileName || getAttachmentName(url);
 
-    // fetching the attachment
-    const fetchedAttachment = RNFetchBlob.config({
-        fileCache: true,
-        path: `${path}/${attachmentName}`,
-        addAndroidDownloads: {
-            useDownloadManager: true,
-            notification: true,
-            path: `${path}/Expensify/${attachmentName}`,
-        },
-    }).fetch('GET', url);
+        // fetching the attachment
+        const fetchedAttachment = RNFetchBlob.config({
+            fileCache: true,
+            path: `${path}/${attachmentName}`,
+            addAndroidDownloads: {
+                useDownloadManager: true,
+                notification: true,
+                path: `${path}/Expensify/${attachmentName}`,
+            },
+        }).fetch('GET', url);
 
-    // resolving the fetched attachment
-    fetchedAttachment.then((attachment) => {
-        if (!attachment || !attachment.info()) {
-            return;
-        }
+        // resolving the fetched attachment
+        fetchedAttachment.then((attachment) => {
+            if (!attachment || !attachment.info()) {
+                return;
+            }
 
-        showAlert({
-            title: 'Downloaded!',
-            message: 'Attachment successfully downloaded',
-            options: [
-                {
-                    text: 'OK',
-                    style: 'cancel',
-                },
-            ],
-        });
-    }).catch(() => {
-        showAlert({
-            title: 'Attachment Error',
-            message: 'Attachment cannot be downloaded',
-            options: [
-                {
-                    text: 'Cancel',
-                    style: 'cancel',
-                },
-            ],
+            showAlert({
+                title: 'Downloaded!',
+                message: 'Attachment successfully downloaded',
+                options: [
+                    {
+                        text: 'OK',
+                        style: 'cancel',
+                    },
+                ],
+            });
+            return resolve();
+        }).catch(() => {
+            showAlert({
+                title: 'Attachment Error',
+                message: 'Attachment cannot be downloaded',
+                options: [
+                    {
+                        text: 'Cancel',
+                        style: 'cancel',
+                    },
+                ],
+            });
+            return resolve();
         });
     });
 }
@@ -108,6 +113,7 @@ function handleDownload(url, fileName) {
  * Platform specifically check download
  * @param {String} url
  * @param {String} fileName
+ * @returns {Promise} fileName
  */
 export default function fileDownload(url, fileName) {
     const permissionError = {
@@ -130,15 +136,16 @@ export default function fileDownload(url, fileName) {
     if (getPlatform() === 'android') {
         hasAndroidPermission().then((hasPermission) => {
             if (hasPermission) {
-                handleDownload(url, fileName);
-                return;
+                return handleDownload(url, fileName);
             }
 
             showAlert(permissionError);
+            return Promise.resolve();
         }).catch(() => {
             showAlert(permissionError);
+            return Promise.resolve();
         });
     } else {
-        handleDownload(url, fileName);
+        return handleDownload(url, fileName);
     }
 }
