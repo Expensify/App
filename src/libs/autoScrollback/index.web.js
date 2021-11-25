df1f19aad8f5014aecef0bac1944cd21a6677f07
Sid Ferreira <143615+sidferreira@users.noreply.github.com>
2021-11-25 15:37:48 -0800

    Mobile Web Safari - Keyboard Visible Overscroll - review 2

--- src/libs/autoScrollback/index.web.js
+++ src/libs/autoScrollback/index.web.js
@@ -1,7 +1,4 @@
 /* autoScrollBack address Mobile Safari-specific issues when the user overscrolls the window while the keyboard is visible */
-
-/* allows smooth scroll when programmatic scrolling */
-import smoothscroll from 'smoothscroll-polyfill';
 import Str from 'expensify-common/lib/str';
 
 import getBrowser from '../getBrowser';
@@ -13,76 +10,22 @@ const init = () => {
     const userAgent = navigator.userAgent.toLowerCase();
 
     if (Str.contains(userAgent, 'iphone os 1')) {
-        const baseInnerHeight = window.innerHeight;
+        const innterHeightWithoutKeyboard = window.innerHeight;
 
-        /* minimum acceptable innerHeight, less than that, scroll back */
-        let minimumInnerHeight = 0;
+        let waitingForScrollAfterFocus = false;
+
+        let maxScrollY = 0;
 
         const isIos15 = Str.contains(userAgent, 'iphone os 15_');
 
-        switch (window.outerHeight) {
-            case 926: { // iPhone 12/13 Pro Max
-                minimumInnerHeight = 440;
-                break;
-            }
-            case 896: {
-                if (window.devicePixelRatio === 3) { //  iPhone 11 Pro Max
-                    minimumInnerHeight = 425;
-                } else { // iPhone 11
-                    minimumInnerHeight = isIos15 ? 460 : 420;
-                }
-                break;
-            }
-            case 844: {
-                if (window.devicePixelRatio === 3) { // iPhone 13 / iPhone 12/13 Pro
-                    minimumInnerHeight = 370;
-                } else { //  iPhone 12
-                    minimumInnerHeight = 412;
-                }
-                break;
-            }
-            case 812: {
-                if (window.devicePixelRatio === 3) { // iPhone 11 Pro / iPhone 12/13 Mini
-                    minimumInnerHeight = 340;
-                } else { //  iPhone 11
-                    minimumInnerHeight = 420;
-                }
-                break;
-            }
-            default: {
-                //
-            }
-        }
-
-        if (!minimumInnerHeight) {
-            return;
-        }
-
         let isTouching = false;
-        let timeout;
-        smoothscroll.polyfill();
 
-        const scrollBack = () => {
-            window.requestAnimationFrame(() => {
-                window.scrollTo({
-                    top: baseInnerHeight - minimumInnerHeight,
-                    behavior: 'smooth',
-                });
-            });
-        };
-
-        const clearTimeoutIfNeeded = () => {
-            if (!timeout) {
+        const scrollback = () => {
+            if (!maxScrollY) {
                 return;
             }
-            clearTimeout(timeout);
-            timeout = undefined;
-        };
-
-        const scheduleScrollback = () => {
-            clearTimeoutIfNeeded();
-            if (!isTouching && minimumInnerHeight > window.innerHeight) {
-                timeout = setTimeout(scrollBack, 34);
+            if (!isTouching && window.scrollY > maxScrollY) {
+                window.scrollTo({top: maxScrollY, behavior: 'smooth'});
             }
         };
 
@@ -91,10 +34,21 @@ const init = () => {
         });
         document.addEventListener('touchend', () => {
             isTouching = false;
-            scheduleScrollback();
+            scrollback();
         });
         document.addEventListener('scroll', () => {
-            scheduleScrollback();
+            if (waitingForScrollAfterFocus && !maxScrollY) {
+                waitingForScrollAfterFocus = false;
+                const keyboardHeight = innterHeightWithoutKeyboard - window.visualViewport.height;
+                maxScrollY = keyboardHeight + (isIos15 ? 52 : 0);
+            }
+            scrollback();
+        });
+        document.addEventListener('focusin', () => {
+            waitingForScrollAfterFocus = true;
+        });
+        document.addEventListener('focusout', () => {
+            waitingForScrollAfterFocus = false;
         });
     }
 };
