dfd9d7a0a4171dc48ec62bd7f46100fd5e94d7bc
Nicholas Murray <nmurray@expensify.com>
2021-11-26 13:26:40 -0500

    Test on web, desktop with complete uri

--- src/libs/getPlaidLinkTokenParameters/index.js
+++ src/libs/getPlaidLinkTokenParameters/index.js
@@ -1,1 +1,8 @@
-export default () => ({});
+import ROUTES from "../../ROUTES";
+import {removeTrailingForwardSlash} from "../Url";
+import CONST from "../../CONST";
+
+export default () => {
+    const bankAccountRoute = window.location.href.includes('personal') ? ROUTES.BANK_ACCOUNT_PERSONAL : removeTrailingForwardSlash(ROUTES.getBankAccountRoute());
+    return {redirect_uri: `${CONST.NEW_EXPENSIFY_URL}/${bankAccountRoute}`};
+};
