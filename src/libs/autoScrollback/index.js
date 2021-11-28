4c3817155a149dc44abe86668d25edeb046d325b
Sid Ferreira <143615+sidferreira@users.noreply.github.com>
2021-11-27 20:10:18 -0800

    Mobile Web Safari - Keyboard Visible Overscroll - review 3

--- src/libs/autoScrollback/index.js
+++ src/libs/autoScrollback/index.js
@@ -1,4 +1,2 @@
-/* autoScrollBack address Mobile Safari-specific issues when the user overscrolls the window while the keyboard is visible */
-/* it has no effect to other platforms */
-
-export default () => {};
+/* The autoScrollBack address Mobile Safari-specific issues when the user overscrolls the window while the keyboard is visible */
+/* It has no effect to other platforms */
--- src/libs/autoScrollback/index.web.js
+++ src/libs/autoScrollback/index.web.js
@@ -1,56 +1,64 @@
-/* autoScrollBack address Mobile Safari-specific issues when the user overscrolls the window while the keyboard is visible */
+/* The autoScrollBack address Mobile Safari-specific issues when the user overscrolls the window while the keyboard is visible */
 import Str from 'expensify-common/lib/str';
-
+import CONST from '../../CONST';
 import getBrowser from '../getBrowser';
 
-const init = () => {
-    if (getBrowser() !== 'safari') {
+const userAgent = navigator.userAgent.toLowerCase();
+
+// The innerHeight when the keyboard is not visible
+const baseInnerHeight = window.innerHeight;
+
+// Control flag if an input/text area was focused and we're waiting the "focus scroll" to identify the screen size
+let isWaitingForScroll = false;
+
+// Calculated value of the maximum value to the screen to scroll when keyboard is visible
+let maxScrollY = 0;
+
+let isTouching = false;
+
+const isIos15 = Str.contains(userAgent, 'iphone os 15_');
+
+function scrollback() {
+    if (!maxScrollY) {
         return;
     }
-    const userAgent = navigator.userAgent.toLowerCase();
-
-    if (Str.contains(userAgent, 'iphone os 1')) {
-        const innterHeightWithoutKeyboard = window.innerHeight;
-
-        let waitingForScrollAfterFocus = false;
-
-        let maxScrollY = 0;
-
-        const isIos15 = Str.contains(userAgent, 'iphone os 15_');
-
-        let isTouching = false;
-
-        const scrollback = () => {
-            if (!maxScrollY) {
-                return;
-            }
-            if (!isTouching && window.scrollY > maxScrollY) {
-                window.scrollTo({top: maxScrollY, behavior: 'smooth'});
-            }
-        };
-
-        document.addEventListener('touchstart', () => {
-            isTouching = true;
-        });
-        document.addEventListener('touchend', () => {
-            isTouching = false;
-            scrollback();
-        });
-        document.addEventListener('scroll', () => {
-            if (waitingForScrollAfterFocus && !maxScrollY) {
-                waitingForScrollAfterFocus = false;
-                const keyboardHeight = innterHeightWithoutKeyboard - window.visualViewport.height;
-                maxScrollY = keyboardHeight + (isIos15 ? 52 : 0);
-            }
-            scrollback();
-        });
-        document.addEventListener('focusin', () => {
-            waitingForScrollAfterFocus = true;
-        });
-        document.addEventListener('focusout', () => {
-            waitingForScrollAfterFocus = false;
-        });
+    if (!isTouching && window.scrollY > maxScrollY) {
+        window.scrollTo({top: maxScrollY, behavior: 'smooth'});
     }
-};
+}
 
-init();
+function touchStarted() {
+    isTouching = true;
+}
+
+function scrollbackAfterTouch() {
+    isTouching = false;
+    scrollback();
+}
+
+function scrollbackAfterScroll() {
+    if (isWaitingForScroll && !maxScrollY) {
+        isWaitingForScroll = false;
+        const keyboardHeight = baseInnerHeight - window.visualViewport.height;
+
+        // The iOS 15 Safari has a 52 pixel tall address label that must be manually added
+        maxScrollY = keyboardHeight + (isIos15 ? 52 : 0);
+    }
+    scrollback();
+}
+
+function startsWaitingForScroll() {
+    isWaitingForScroll = true;
+}
+
+function stopsWaitingForScroll() {
+    isWaitingForScroll = false;
+}
+
+if (getBrowser() === CONST.BROWSER.SAFARI && Str.contains(userAgent, 'iphone os 1')) {
+    document.addEventListener('touchstart', touchStarted);
+    document.addEventListener('touchend', scrollbackAfterTouch);
+    document.addEventListener('scroll', scrollbackAfterScroll);
+    document.addEventListener('focusin', startsWaitingForScroll);
+    document.addEventListener('focusout', stopsWaitingForScroll);
+}
