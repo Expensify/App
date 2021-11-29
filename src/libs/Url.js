3a8ac3aa2863c110530649d3900616487c27f7da
Nicholas Murray <nmurray@expensify.com>
2021-11-29 16:19:46 -0500

    Refactor ROUTE

--- src/libs/Url.js
+++ src/libs/Url.js
@@ -10,21 +10,7 @@ function addTrailingForwardSlash(url) {
     return url;
 }
 
-/**
- * Remove / to the end of any URL if present
- * @param {String} url
- * @returns {String}
- */
-function removeTrailingForwardSlash(url) {
-    if (url.endsWith('/')) {
-        return url.slice(0, -1);
-    }
-    return url;
-}
-
-
 export {
     // eslint-disable-next-line import/prefer-default-export
     addTrailingForwardSlash,
-    removeTrailingForwardSlash,
 };
--- src/libs/getPlaidLinkTokenParameters/index.js
+++ src/libs/getPlaidLinkTokenParameters/index.js
@@ -1,8 +1,7 @@
 import ROUTES from '../../ROUTES';
-import * as Url from '../Url';
 import CONST from '../../CONST';
 
 export default () => {
-    const bankAccountRoute = window.location.href.includes('personal') ? ROUTES.BANK_ACCOUNT_PERSONAL : Url.removeTrailingForwardSlash(ROUTES.getBankAccountRoute());
+    const bankAccountRoute = window.location.href.includes('personal') ? ROUTES.BANK_ACCOUNT_PERSONAL : ROUTES.BANK_ACCOUNT;
     return {redirect_uri: `${CONST.NEW_EXPENSIFY_URL}/${bankAccountRoute}`};
 };
