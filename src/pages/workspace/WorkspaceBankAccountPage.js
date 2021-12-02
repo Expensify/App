60a3ad87b3d0738f47bf5a92782190a4fc848a2e
Puneet Lath <puneet@expensify.com>
2021-12-02 18:16:25 -0500

    fix import of ExpensifyText in WorkspaceBankAccountPage

--- src/pages/workspace/WorkspaceBankAccountPage.js
+++ src/pages/workspace/WorkspaceBankAccountPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {withOnyx} from 'react-native-onyx';
 import * as Expensicons from '../../components/Icon/Expensicons';
 import * as Illustrations from '../../components/Icon/Illustrations';
-import Text from '../../components/ExpensifyText';
+import ExpensifyText from '../../components/ExpensifyText';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 import * as BankAccounts from '../../libs/actions/BankAccounts';
 import compose from '../../libs/compose';
