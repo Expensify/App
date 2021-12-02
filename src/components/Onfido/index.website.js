c37def14af00e992bf82e9553db34d0d6f21d9e2
Carlos Martins <cmartins@expensify.com>
2021-12-02 16:26:42 -0700

    create web onfido component

---
+++ src/components/Onfido/index.website.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import lodashGet from 'lodash/get';
+import BaseOnfido from './BaseOnfido';
+import onfidoPropTypes from './onfidoPropTypes';
+
+class Onfido extends Component {
+    constructor(props) {
+        super(props);
+        this.baseOnfido = null;
+    }
+    
+    componentWillUnmount() {
+        const onfidoOut = lodashGet(this, 'baseOnfido.onfidoOut');
+        if (!onfidoOut) {
+            return;
+        }
+        
+        onfidoOut.tearDown();
+    }
+    
+    render() {
+        return (
+            <BaseOnfido
+                ref={e => this.baseOnfido = e}
+                
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...this.props}
+            />
+        );
+    }
+}
+
+Onfido.propTypes = onfidoPropTypes;
+
+export default Onfido;
