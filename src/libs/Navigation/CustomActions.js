6c71c6665217a755ea95ac7db9ad7ec1206e305c
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-12-03 09:41:59 -1000

    try to improve comments

--- src/libs/Navigation/CustomActions.js
+++ src/libs/Navigation/CustomActions.js
@@ -51,48 +51,70 @@ function getParamsFromState(state) {
 }
 
 /**
+ * @param {Object} state
+ * @returns {String}
+ */
+function getScreenNameFromState(state) {
+    return lodashGet(state, 'routes[0].state.routes[0].name', '');
+}
+
+/**
+ * @returns {Object}
+ */
+function getActiveState() {
+    // We use our RootState as the dispatch's state is relative to the active navigator and might not contain our active screen.
+    return navigationRef.current.getRootState();
+}
+
+/**
  * Special accomodation must be made for navigating to a screen inside a DrawerNavigator (e.g. our ReportScreen). The web/mWeb default behavior when
  * calling "navigate()" does not give us the browser history we would expect for a typical web paradigm (e.g. that navigating from one screen another
  * should allow us to navigate back to the screen we were on previously). This custom action helps us get around these problems.
  *
  * More context here: https://github.com/react-navigation/react-navigation/issues/9744
  *
- * @param {String} path
+ * @param {String} route
  * @returns {Function}
  */
-function pushDrawerRoute(path) {
+function pushDrawerRoute(route) {
     return (currentState) => {
-        let state = currentState;
+        // Parse the state, name, and params from the new route we want to navigate to
+        const newStateFromRoute = getStateFromPath(route, linkingConfig.config);
+        const newScreenName = getScreenNameFromState(newStateFromRoute);
+        const newScreenParams = getParamsFromState(newStateFromRoute);
 
-        const stateFromPath = getStateFromPath(path, linkingConfig.config);
-        const newScreenName = lodashGet(stateFromPath, 'routes[0].state.routes[0].name');
-        const newScreenParams = getParamsFromState(stateFromPath);
-
-        // Avoid the navigation and refocus the report if we're trying to navigate to our active report
-        // We use our RootState as the dispatch's state is relative to the active navigator and might
-        // not contain our active report.
-        const rootState = navigationRef.current.getRootState();
-        const activeScreenParams = getParamsFromState(rootState);
-        if (state.type !== 'drawer') {
+        // When we are navigating away from a non-drawer navigator we need to dismiss any screens pushed onto the main stack.
+        if (currentState.type !== 'drawer') {
             navigateBackToRootDrawer();
         }
-        if (_.isEqual(activeScreenParams, newScreenParams)) {
+
+        // If we're trying to navigate to the same screen that is already active there's nothing more to do except close the drawer.
+        // This prevents unnecessary re-rendering the screen and adding duplicate items to the browser history.
+        const activeState = getActiveState();
+        const activeScreenName = getScreenNameFromState(activeState);
+        const activeScreenParams = getParamsFromState(activeState);
+        if (newScreenName === activeScreenName && _.isEqual(activeScreenParams, newScreenParams)) {
             return DrawerActions.closeDrawer();
         }
 
-        // When navigating from non Drawer navigator, get new state for the report and reset the navigation state.
-        if (state.type !== 'drawer') {
-            state = getStateFromPath(path, linkingConfig.config);
+        let state = currentState;
+
+        // When navigating from non-Drawer navigator we switch to using the new state generated from the provided route. If we are navigating away from a non-Drawer navigator the
+        // currentState will not have a history field to use. By using the state from the route we create a "fresh state" that we can use to setup the browser history again.
+        // Note: A current limitation with this is that navigating "back" won't display the routes we have cleared out e.g. SearchPage and the history effectively gets "reset".
+        if (currentState.type !== 'drawer') {
+            state = newStateFromRoute;
         }
 
         const screenRoute = {type: 'route', name: newScreenName};
-        const history = _.map([...(state.history || [screenRoute])], () => screenRoute);
+        const history = _.map(state.history ? [...state.history] : [screenRoute], () => screenRoute);
 
-        // Force drawer to close and show the report screen
+        // Force drawer to close and show
         history.push({
             type: 'drawer',
             status: 'closed',
         });
+
         return CommonActions.reset({
             ...state,
             routes: [{
