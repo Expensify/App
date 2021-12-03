87e88b9b99b4d775de07096d0dac4a5af024a907
Marc Glasser <marc.aaron.glasser@gmail.com>
2021-12-03 08:46:38 -1000

    clean stuff up

--- src/libs/Navigation/CustomActions.js
+++ src/libs/Navigation/CustomActions.js
@@ -4,12 +4,13 @@ import {
 } from '@react-navigation/native';
 import lodashGet from 'lodash/get';
 import linkingConfig from './linkingConfig';
+import navigationRef from './navigationRef';
 
 /**
  * Go back to the Main Drawer
  * @param {Object} navigationRef
  */
-function navigateBackToRootDrawer(navigationRef) {
+function navigateBackToRootDrawer() {
     let isLeavingNestedDrawerNavigator = false;
 
     // This should take us to the first view of the modal's stack navigator
@@ -42,43 +43,49 @@ function navigateBackToRootDrawer(navigationRef) {
 }
 
 /**
- * In order to create the desired browser navigation behavior on web and mobile web we need to replace any
- * type: 'drawer' routes with a type: 'route' so that when pushing to a report screen we never show the
- * drawer. We don't want to remove these since we always want the history length to increase by 1 whenever
- * we are moving to a new report screen or back to a previous one. This is a workaround since
- * react-navigation default behavior for a drawer is to skip pushing history states when navigating to the
- * current route
+ * @param {Object} state
+ * @returns {Object}
+ */
+function getParamsFromState(state) {
+    return lodashGet(state, 'routes[0].state.routes[0].params', {});
+}
+
+/**
+ * Special accomodation must be made for navigating to a screen inside a DrawerNavigator (e.g. our ReportScreen). The web/mWeb default behavior when
+ * calling "navigate()" does not give us the browser history we would expect for a typical web paradigm (e.g. that navigating from one screen another
+ * should allow us to navigate back to the screen we were on previously). This custom action helps us get around these problems.
  *
- * @param {String} screenName
- * @param {Object} params
+ * More context here: https://github.com/react-navigation/react-navigation/issues/9744
+ *
  * @param {String} path
- * @param {Object} navigationRef
  * @returns {Function}
  */
-function pushDrawerRoute(screenName, params, path, navigationRef) {
+function pushDrawerRoute(path) {
     return (currentState) => {
         let state = currentState;
+
+        const stateFromPath = getStateFromPath(path, linkingConfig.config);
+        const newScreenName = lodashGet(stateFromPath, 'routes[0].state.routes[0].name');
+        const newScreenParams = getParamsFromState(stateFromPath);
 
         // Avoid the navigation and refocus the report if we're trying to navigate to our active report
         // We use our RootState as the dispatch's state is relative to the active navigator and might
         // not contain our active report.
         const rootState = navigationRef.current.getRootState();
-        const activeReportID = lodashGet(rootState, 'routes[0].state.routes[0].params.reportID', '');
+        const activeScreenParams = getParamsFromState(rootState);
         if (state.type !== 'drawer') {
-            navigateBackToRootDrawer(navigationRef);
+            navigateBackToRootDrawer();
         }
-        if (activeReportID === params.reportID) {
+        if (_.isEqual(activeScreenParams, newScreenParams)) {
             return DrawerActions.closeDrawer();
         }
 
         // When navigating from non Drawer navigator, get new state for the report and reset the navigation state.
         if (state.type !== 'drawer') {
-            state = linkingConfig.getStateFromPath
-                ? linkingConfig.getStateFromPath(path, linkingConfig.config)
-                : getStateFromPath(path, linkingConfig.config);
+            state = getStateFromPath(path, linkingConfig.config);
         }
 
-        const screenRoute = {type: 'route', name: screenName};
+        const screenRoute = {type: 'route', name: newScreenName};
         const history = _.map([...(state.history || [screenRoute])], () => screenRoute);
 
         // Force drawer to close and show the report screen
@@ -89,8 +96,8 @@ function pushDrawerRoute(screenName, params, path, navigationRef) {
         return CommonActions.reset({
             ...state,
             routes: [{
-                name: screenName,
-                params,
+                name: newScreenName,
+                params: newScreenParams,
             }],
             history,
         });
