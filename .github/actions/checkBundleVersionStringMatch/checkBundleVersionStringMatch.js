25a7f3db1b844be52d601bb75f88a2c1b542abda
Joe Gambino <joe@expensify.com>
2021-12-06 19:21:28 -0800

    use grep/sed

--- .github/actions/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
+++ .github/actions/checkBundleVersionStringMatch/checkBundleVersionStringMatch.js
@@ -2,12 +2,14 @@ const core = require('@actions/core');
 const {execSync} = require('child_process');
 const {PLIST_PATH} = require('../../libs/nativeVersionUpdater');
 
-const bundleVersion = execSync(`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" ${PLIST_PATH}`);
-const shortBundleVersion = execSync(`/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" ${PLIST_PATH}`);
+const bundleVersion = execSync(`grep -A1 'CFBundleVersion' ${PLIST_PATH} | grep -v 'CFBundleVersion' | sed 's|[</string>,]||g')`).toString().trim();
+const shortBundleVersion = execSync(`grep -A1 'CFBundleShortVersionString' ${PLIST_PATH} | grep -v 'CFBundleShortVersionString' | sed 's|[</string>,]||g')`).toString().trim();
 
 console.log(`Bundle Version: ${bundleVersion}`);
 console.log(`Short Bundle Version: ${shortBundleVersion}`);
-if (shortBundleVersion !== (bundleVersion.split('-') || [''])[0]) {
+
+const hasValue = shortBundleVersion && bundleVersion;
+if (!hasValue || (shortBundleVersion !== (bundleVersion.split('-') || [''])[0])) {
     console.log('Bundle Versions do not match');
     core.setOutput('BUNDLE_VERSIONS_MATCH', false);
 } else {
