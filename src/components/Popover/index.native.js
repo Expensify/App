d07d3d0506c5b1c776331d164cad11f3fabba87f
Alfredo Alcántara <alfredo@lunadna.com>
2021-12-09 19:30:51 -0800

    Sends correct animations

--- src/components/Popover/index.native.js
+++ src/components/Popover/index.native.js
@@ -13,17 +13,21 @@ const propTypes = {
  * This is a convenience wrapper around the Modal component for a responsive Popover.
  * On small screen widths, it uses BottomDocked modal type, and a Popover type on wide screen widths.
  */
-const Popover = props => (
-    <Modal
-        type={props.fromSidebarMediumScreen ? CONST.MODAL.MODAL_TYPE.POPOVER : CONST.MODAL.MODAL_TYPE.BOTTOM_DOCKED}
-        popoverAnchorPosition={props.isSmallScreenWidth ? undefined : props.anchorPosition}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...props}
-        fullscreen={props.isSmallScreenWidth ? true : props.fullscreen}
-        animationIn={props.isSmallScreenWidth ? undefined : props.animationIn}
-        animationOut={props.isSmallScreenWidth ? undefined : props.animationOut}
-    />
-);
+const Popover = (props) => {
+    const propsWithoutAnimation = _.omit(props, ['animationIn', 'animationOut', 'popoverAnchorPosition', 'disableAnimation']);
+    return (
+        <Modal
+            type={props.fromSidebarMediumScreen ? CONST.MODAL.MODAL_TYPE.POPOVER : CONST.MODAL.MODAL_TYPE.BOTTOM_DOCKED}
+            popoverAnchorPosition={props.fromSidebarMediumScreen ? props.anchorPosition : undefined}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...propsWithoutAnimation}
+
+            // Mobile will always has fullscreen menu
+            // eslint-disable-next-line react/jsx-props-no-multi-spaces
+            fullscreen
+        />
+    );
+};
 
 Popover.propTypes = propTypes;
 Popover.defaultProps = defaultProps;
