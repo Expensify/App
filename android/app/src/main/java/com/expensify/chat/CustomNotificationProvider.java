6be374da5348951d3f1ad032e859a85ca543e01d
Rafay Saleem <rafaysaleem80@gmail.com>
2021-12-14 21:50:35 +0500

    Icon rounding issue resolved

--- android/app/src/main/java/com/expensify/chat/CustomNotificationProvider.java
+++ android/app/src/main/java/com/expensify/chat/CustomNotificationProvider.java
@@ -2,6 +2,12 @@ package com.expensify.chat;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Bitmap.Config;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
@@ -72,6 +78,25 @@ public class CustomNotificationProvider extends ReactNotificationProvider {
         }
 
         return builder;
+    }
+
+    public Bitmap getCroppedBitmap(Bitmap bitmap) {
+       Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+            bitmap.getHeight(), Config.ARGB_8888);
+       Canvas canvas = new Canvas(output);
+
+       final int color = 0xff424242;
+       final Paint paint = new Paint();
+       final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+       paint.setAntiAlias(true);
+       canvas.drawARGB(0, 0, 0, 0);
+       paint.setColor(color);
+       canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+            bitmap.getWidth() / 2, paint);
+       paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+       canvas.drawBitmap(bitmap, rect, rect, paint);
+       return output;
     }
 
     /**
@@ -208,7 +233,7 @@ public class CustomNotificationProvider extends ReactNotificationProvider {
 
         try {
             Bitmap bitmap = future.get(MAX_ICON_FETCH_WAIT_TIME_SECONDS, TimeUnit.SECONDS);
-            return IconCompat.createWithBitmap(bitmap);
+            return IconCompat.createWithBitmap(getCroppedBitmap(bitmap));
         } catch (InterruptedException e) {
             Log.e(TAG,"Failed to fetch icon", e);
             Thread.currentThread().interrupt();
