63dacc562bc37228117ff37fafdad0b49344308d
Yuwen Memon <yuwen@expensify.com>
2021-12-13 18:30:34 -0800

    Make sure that we reduce the unread message count when we delete an unread message

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -538,7 +538,12 @@ function setNewMarkerPosition(reportID, sequenceNumber) {
 function updateReportActionMessage(reportID, sequenceNumber, message) {
     const actionToMerge = {};
     actionToMerge[sequenceNumber] = {message: [message]};
-    Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`, actionToMerge);
+    Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`, actionToMerge).then(() => {
+        // If the message is deleted, update the last read in case the deleted message is being counted in the unreadActionCount
+        if (!message.text) {
+            setLocalLastRead(reportID, lastReadSequenceNumbers[reportID]);
+        }
+    });
 
     // If this is the most recent message, update the lastMessageText in the report object as well
     if (sequenceNumber === reportMaxSequenceNumbers[reportID]) {
