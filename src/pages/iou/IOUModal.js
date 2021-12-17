8994349b86818239464eff3925373e6629009a42
Monil Bhavsar <monil@expensify.com>
2021-12-17 12:45:34 +0530

    Revert to use default currency

--- src/pages/iou/IOUModal.js
+++ src/pages/iou/IOUModal.js
@@ -145,9 +145,7 @@ class IOUModal extends Component {
 
     componentDidMount() {
         PersonalDetails.fetchLocalCurrency();
-        if (!this.props.iou.selectedCurrencyCode) {
-            IOU.setIOUSelectedCurrency(this.props.myPersonalDetails.localCurrencyCode);
-        }
+        IOU.setIOUSelectedCurrency(this.props.myPersonalDetails.localCurrencyCode);
     }
 
     componentDidUpdate(prevProps) {
