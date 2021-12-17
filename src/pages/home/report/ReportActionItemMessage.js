5d84848118eb5f7a7f8fa428f60d387e89fd419d
Aswin S <mr.aswin@gmail.com>
2021-12-18 01:09:15 +0530

    Render placeholder text for deleted comment

--- src/pages/home/report/ReportActionItemMessage.js
+++ src/pages/home/report/ReportActionItemMessage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
+import lodashGet from 'lodash/get';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
 import styles from '../../../styles/styles';
 import ReportActionItemFragment from './ReportActionItemFragment';
 import reportActionPropTypes from './reportActionPropTypes';
 import {withNetwork} from '../../../components/OnyxProvider';
+import ExpensifyText from '../../../components/ExpensifyText';
+import * as Localize from '../../../libs/Localize';
 
 const propTypes = {
     /** The report action */
@@ -24,16 +27,21 @@ const defaultProps = {
 
 const ReportActionItemMessage = (props) => {
     const isUnsent = props.network.isOffline && props.action.loading;
+
+    // A deleted comment has either an empty array or an object with html field with empty string as value
+    const isDeleted = lodashGet(props.action, 'message[0].html', '') === '';
     return (
         <View style={[styles.chatItemMessage, isUnsent && styles.chatItemUnsentMessage]}>
-            {_.map(_.compact(props.action.message), (fragment, index) => (
-                <ReportActionItemFragment
-                    key={`actionFragment-${props.action.sequenceNumber}-${index}`}
-                    fragment={fragment}
-                    isAttachment={props.action.isAttachment}
-                    loading={props.action.loading}
-                />
-            ))}
+            {isDeleted
+                ? <ExpensifyText>{`[${Localize.translateLocal('common.deletedCommentMessage')}]`}</ExpensifyText>
+                : _.map(_.compact(props.action.message), (fragment, index) => (
+                    <ReportActionItemFragment
+                        key={`actionFragment-${props.action.sequenceNumber}-${index}`}
+                        fragment={fragment}
+                        isAttachment={props.action.isAttachment}
+                        loading={props.action.loading}
+                    />
+                ))}
         </View>
     );
 };
