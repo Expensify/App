e6d95aaa54d2afb5405138b6ad1522e9a522b99f
Akshaya Salvi <akshayasalvi12@gmail.com>
2021-12-18 09:42:50 +0530

    Added handling for mobile web

--- src/pages/iou/steps/IOUAmountPage.js
+++ src/pages/iou/steps/IOUAmountPage.js
@@ -220,6 +220,7 @@ class IOUAmountPage extends React.Component {
                         placeholder="0"
                         keyboardType={CONST.KEYBOARD_TYPE.NUMERIC}
                         showSoftInputOnFocus={false}
+                        inputMode="none"
                     />
 
                 </View>
