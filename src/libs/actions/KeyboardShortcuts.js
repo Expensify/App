de1745b98dfca48f4ab51e9dea9f68ace4b44cf6
Akshaya Salvi <akshayasalvi12@gmail.com>
2021-12-18 11:43:39 +0530

    Added jsdoc for the functions

--- src/libs/actions/KeyboardShortcuts.js
+++ src/libs/actions/KeyboardShortcuts.js
@@ -5,10 +5,16 @@ Onyx.connect({
     key: ONYXKEYS.IS_SHORTCUTS_MODAL_OPEN,
 });
 
+/**
+ * Set keyboard shortcuts flag to show modal
+ */
 function showKeyboardShortcutModal() {
     Onyx.set(ONYXKEYS.IS_SHORTCUTS_MODAL_OPEN, true);
 }
 
+/**
+ * Unset keyboard shortcuts flag to hide modal
+ */
 function hideKeyboardShortcutModal() {
     Onyx.set(ONYXKEYS.IS_SHORTCUTS_MODAL_OPEN, false);
 }
