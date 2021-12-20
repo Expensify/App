e82bd001c0788c2fe0e2740f34aa4b097af788a5
Marco Chávez <marco@expensify.com>
2021-12-20 16:57:35 -0600

    add maxLength to cvv and expiry date

--- src/pages/settings/Payments/AddDebitCardPage.js
+++ src/pages/settings/Payments/AddDebitCardPage.js
@@ -209,6 +209,7 @@ class DebitCardPage extends Component {
                                         placeholder={this.props.translate('addDebitCardPage.expirationDate')}
                                         onChangeText={expirationDate => this.clearErrorAndSetValue('expirationDate', expirationDate)}
                                         value={this.state.expirationDate}
+                                        maxLength="7"
                                         errorText={this.getErrorText('expirationDate')}
                                         keyboardType={CONST.KEYBOARD_TYPE.PHONE_PAD}
                                     />
@@ -218,6 +219,8 @@ class DebitCardPage extends Component {
                                         label={this.props.translate('addDebitCardPage.cvv')}
                                         onChangeText={securityCode => this.clearErrorAndSetValue('securityCode', securityCode)}
                                         value={this.state.securityCode}
+                                        maxLength="4"
+                                        pattern="\d*"
                                         errorText={this.getErrorText('securityCode')}
                                         keyboardType={CONST.KEYBOARD_TYPE.NUMBER_PAD}
                                     />
