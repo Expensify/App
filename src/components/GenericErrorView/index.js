a9df0267321e4091bc4a01544b92de505058ea8d
Manan Jadhav <manan.jadhav@gmail.com>
2021-12-21 22:48:51 +0530

    feat(error-page): Additional styling and alignment

--- src/components/GenericErrorView/index.js
+++ src/components/GenericErrorView/index.js
@@ -20,42 +20,45 @@ const GenericErrorView = (props) => {
     };
 
     return (
-        <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
+        <View style={[styles.flex1, styles.m5]}>
+            <View style={[styles.flexColumn, styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
+                <View style={[styles.flexColumn, styles.flexColumn, styles.alignItemsStart]}>
+                    <View style={[styles.mb5]}>
+                        <Icon
+                            src={Expensicons.Bug}
+                            height={variables.componentSizeNormal}
+                            width={variables.componentSizeNormal}
+                            fill={defaultTheme.iconSuccessFill}
+                        />
+                    </View>
+                    <View style={[styles.mb5]}>
+                        <ExpensifyText style={[styles.headerText, styles.textXXLarge]}>
+                            {props.translate('genericErrorView.title')}
+                        </ExpensifyText>
+                    </View>
+                    <View style={[styles.mb5]}>
+                        <ExpensifyText>
+                            {props.translate('genericErrorView.body.helpTextMobile')}
+                        </ExpensifyText>
 
-            <View style={[styles.flexColumn, styles.alignItemsStart]}>
-                <View style={[styles.mb5]}>
-                    <Icon
-                        src={Expensicons.Bug}
-                        height={variables.componentSizeNormal}
-                        width={variables.componentSizeNormal}
-                        fill={defaultTheme.iconSuccessFill}
-                    />
-                </View>
-                <View style={[styles.mb5]}>
-                    <ExpensifyText style={[styles.headerText, styles.textXXLarge]}>
-                        {props.translate('genericErrorView.title')}
-                    </ExpensifyText>
-                </View>
-                <ExpensifyText>
-                    {props.translate('genericErrorView.body.helpTextMobile')}
-                </ExpensifyText>
-
-                <ExpensifyText>
-                    {props.translate('genericErrorView.body.helpTextConcierge')}
-                </ExpensifyText>
-
-                <View style={styles.flexWrap}>
-                    <Button
-                        success
-                        onPress={refreshPage}
-                        text={props.translate('genericErrorView.refresh')}
-                    />
+                        <ExpensifyText>
+                            {props.translate('genericErrorView.body.helpTextConcierge')}
+                        </ExpensifyText>
+                    </View>
+                    <View style={styles.flexWrap}>
+                        <Button
+                            success
+                            onPress={refreshPage}
+                            text={props.translate('genericErrorView.refresh')}
+                        />
+                    </View>
                 </View>
             </View>
             <View styles={[styles.alignSelfEnd]}>
-                <LogoWordmark height={30} width={80} />
+                <View style={[styles.flex1, styles.flexRow, styles.justifyContentCenter]}>
+                    <LogoWordmark height={30} width={80} />
+                </View>
             </View>
-
         </View>
     );
 };
