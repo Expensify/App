60ea2e02255b7213af169a758b9a2d076c15b0f0
Puneet Lath <puneet@expensify.com>
2021-12-25 23:41:59 -0800

    Add menu items for chat and phone to GetAssistancePage

--- src/pages/GetAssistancePage.js
+++ src/pages/GetAssistancePage.js
@@ -5,10 +5,12 @@ import withLocalize, {withLocalizePropTypes} from '../components/withLocalize';
 import compose from '../libs/compose';
 import KeyboardAvoidingView from '../components/KeyboardAvoidingView';
 import HeaderWithCloseButton from '../components/HeaderWithCloseButton';
+import MenuItem from '../components/MenuItem';
 import Navigation from '../libs/Navigation/Navigation';
 import styles from '../styles/styles';
 import Text from '../components/Text';
 import RequestCallIcon from '../../assets/images/request-call.svg';
+import * as Expensicons from '../components/Icon/Expensicons';
 
 const propTypes = {
     ...withLocalizePropTypes,
@@ -29,16 +31,32 @@ class GetAssistancePage extends Component {
                         shouldShowBackButton
                         onBackButtonPress={() => Navigation.goBack()}
                     />
-                    <ScrollView style={styles.flex1} contentContainerStyle={[styles.p5, styles.pt0]}>
-                        <View style={[styles.flex1, styles.flexRow, styles.alignItemsCenter]}>
-                            <Text style={[styles.h1, styles.flex1]}>
-                                {this.props.translate('getAssistancePage.subtitle')}
+                    <ScrollView style={styles.flex1} contentContainerStyle={[styles.pt0]}>
+                        <View style={[styles.ph5]}>
+                            <View style={[styles.flex1, styles.flexRow, styles.alignItemsCenter]}>
+                                <Text style={[styles.h1, styles.flex1]}>
+                                    {this.props.translate('getAssistancePage.subtitle')}
+                                </Text>
+                                <RequestCallIcon width={160} height={100} style={styles.flex1} />
+                            </View>
+                            <Text style={[styles.mb4]}>
+                                {this.props.translate('getAssistancePage.description')}
                             </Text>
-                            <RequestCallIcon width={160} height={100} style={styles.flex1} />
                         </View>
-                        <Text style={[styles.mb4]}>
-                            {this.props.translate('getAssistancePage.description')}
-                        </Text>
+                        <MenuItem
+                            key="chatWithConcierge"
+                            title={this.props.translate('getAssistancePage.chatWithConcierge')}
+                            icon={Expensicons.ChatBubble}
+                            onPress={() => alert('chat')}
+                            shouldShowRightIcon
+                        />
+                        <MenuItem
+                            key="requestSetupCall"
+                            title={this.props.translate('getAssistancePage.requestSetupCall')}
+                            icon={Expensicons.Phone}
+                            onPress={() => alert('phone')}
+                            shouldShowRightIcon
+                        />
                     </ScrollView>
                 </KeyboardAvoidingView>
             </ScreenWrapper>
