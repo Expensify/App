ef1693595f0ce77830b0d9e5335fc34bcf508b2e
Puneet Lath <puneet@expensify.com>
2021-12-25 23:06:25 -0800

    Add withLocalize to GetAssistancePage

--- src/pages/GetAssistancePage.js
+++ src/pages/GetAssistancePage.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import {View, ScrollView} from 'react-native';
 import ScreenWrapper from '../components/ScreenWrapper';
+import withLocalize, {withLocalizePropTypes} from '../components/withLocalize';
+import compose from '../libs/compose';
 import KeyboardAvoidingView from '../components/KeyboardAvoidingView';
 import HeaderWithCloseButton from '../components/HeaderWithCloseButton';
 import Navigation from '../libs/Navigation/Navigation';
 import styles from '../styles/styles';
 import Text from '../components/Text';
 import RequestCallIcon from '../../assets/images/request-call.svg';
+
+const propTypes = {
+    ...withLocalizePropTypes,
+};
 
 class GetAssistancePage extends Component {
     constructor(props) {
@@ -18,18 +24,20 @@ class GetAssistancePage extends Component {
             <ScreenWrapper>
                 <KeyboardAvoidingView>
                     <HeaderWithCloseButton
-                        title={"Get assistance"}
+                        title={this.props.translate('getAssistancePage.title')}
                         onCloseButtonPress={() => Navigation.dismissModal(true)}
                         shouldShowBackButton
                         onBackButtonPress={() => Navigation.goBack()}
                     />
                     <ScrollView style={styles.flex1} contentContainerStyle={[styles.p5, styles.pt0]}>
                         <View style={[styles.flex1, styles.flexRow, styles.alignItemsCenter]}>
-                            <Text style={[styles.h1, styles.flex1]}>{"this is a subtitle"}</Text>
+                            <Text style={[styles.h1, styles.flex1]}>
+                                {this.props.translate('getAssistancePage.subtitle')}
+                            </Text>
                             <RequestCallIcon width={160} height={100} style={styles.flex1} />
                         </View>
                         <Text style={[styles.mb4]}>
-                            {"blah blah blah"}
+                            {this.props.translate('getAssistancePage.description')}
                         </Text>
                     </ScrollView>
                 </KeyboardAvoidingView>
@@ -38,4 +46,8 @@ class GetAssistancePage extends Component {
     }
 }
 
-export default GetAssistancePage;
+GetAssistancePage.propTypes = propTypes;
+
+export default compose(
+    withLocalize,
+)(GetAssistancePage);
