2539f474483cddbc10a8944a0e27c11fdabd12ad
Peter Velkov <kidroca@gmail.com>
2021-12-28 20:08:04 +0200

    Refactor: remove setShouldSignOut unused leftover

--- src/libs/actions/Session/setShouldSignOut.js
+++
@@ -1,9 +0,0 @@
-import Onyx from 'react-native-onyx';
-import ONYXKEYS from '../../../ONYXKEYS';
-
-/**
- * @param {Boolean} shouldSignOut
- */
-export default function setShouldSignOut(shouldSignOut) {
-    Onyx.set(ONYXKEYS.SHOULD_SIGN_OUT, shouldSignOut);
-}
