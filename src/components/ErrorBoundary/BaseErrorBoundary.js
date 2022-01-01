65599762b71de5f20236614cba9f6ecfb8245ce5
Manan Jadhav <manan.jadhav@gmail.com>
2022-01-01 09:15:25 +0530

    fix: Corrected component name and moved Signout to props

--- src/components/ErrorBoundary/BaseErrorBoundary.js
+++ src/components/ErrorBoundary/BaseErrorBoundary.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BootSplash from '../../libs/BootSplash';
 import GenericErrorPage from '../../pages/GenericErrorPage';
+import * as Session from '../../libs/actions/Session';
 
 const propTypes = {
     /* A message posted to `logError` (along with error data) when this component intercepts an error */
@@ -28,6 +29,7 @@ class BaseErrorBoundary extends React.Component {
         super(props);
         this.state = {hasError: false};
         this.onRefresh = this.onRefresh.bind(this);
+        this.onSignOut = this.onSignOut.bind(this);
     }
 
     static getDerivedStateFromError() {
@@ -46,10 +48,15 @@ class BaseErrorBoundary extends React.Component {
         this.setState({hasError: false});
     }
 
+    onSignOut() {
+        Session.signOut();
+        this.onRefresh();
+    }
+
     render() {
         if (this.state.hasError) {
             // For the moment we've decided not to render any fallback UI
-            return <GenericErrorPage onRefresh={this.onRefresh} />;
+            return <GenericErrorPage onRefresh={this.onRefresh} onSignOut={this.onSignOut} />;
         }
 
         return this.props.children;
--- src/pages/GenericErrorPage/index.js
+++ src/pages/GenericErrorPage/index.js
@@ -10,7 +10,6 @@ import LogoWordmark from '../../../assets/images/expensify-wordmark.svg';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../components/withWindowDimensions';
 import compose from '../../libs/compose';
-import * as Session from '../../libs/actions/Session';
 import variables from '../../styles/variables';
 import styles from '../../styles/styles';
 import ErrorBodyText from './ErrorBodyText';
@@ -21,71 +20,68 @@ const propTypes = {
     ...withLocalizePropTypes,
     ...windowDimensionsPropTypes,
 
-    /** Function call on Refresh button click */
+    /** Callback to call on refresh button click */
     onRefresh: PropTypes.func.isRequired,
+
+    /** Callback to call on signOut button click */
+    onSignOut: PropTypes.func.isRequired,
 };
 
-const genericErrorPage = (props) => {
-    const onSignOutClick = () => {
-        Session.signOut();
-        props.onRefresh();
-    };
-
-    return (
-        <View style={[styles.flex1, styles.pv10, styles.ph5, styles.errorPageContainer]}>
-            <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
-                <View style={styles.alignItemsStart}>
-                    <View style={styles.mb5}>
-                        <Icon
-                            src={Expensicons.Bug}
-                            height={variables.componentSizeNormal}
-                            width={variables.componentSizeNormal}
-                            fill={defaultTheme.iconSuccessFill}
+const GenericErrorPage = props => (
+    <View style={[styles.flex1, styles.pv10, styles.ph5, styles.errorPageContainer]}>
+        <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
+            <View style={styles.alignItemsStart}>
+                <View style={styles.mb5}>
+                    <Icon
+                        src={Expensicons.Bug}
+                        height={variables.componentSizeNormal}
+                        width={variables.componentSizeNormal}
+                        fill={defaultTheme.iconSuccessFill}
+                    />
+                </View>
+                <View style={styles.mb5}>
+                    <Text style={[styles.headerText, styles.textXXLarge]}>
+                        {props.translate('genericErrorPage.title')}
+                    </Text>
+                </View>
+                <View style={styles.mb5}>
+                    <ErrorBodyText />
+                    <Text>
+                        {props.translate('genericErrorPage.body.helpTextConcierge')}
+                        <TextLink href={`mailto:${CONST.EMAIL.CONCIERGE}`} style={[styles.link]}>
+                            {CONST.EMAIL.CONCIERGE}
+                        </TextLink>
+                    </Text>
+                </View>
+                <View style={[styles.dFlex, styles.flexRow, styles.w100]}>
+                    <View style={[styles.flex1, styles.flexRow, styles.justifyContentBetween]}>
+                        <Button
+                            success
+                            small={props.isSmallScreenWidth}
+                            onPress={props.onRefresh}
+                            text={props.translate('genericErrorPage.refresh')}
+                        />
+                        <Button
+                            small={props.isSmallScreenWidth}
+                            onPress={props.onSignOut}
+                            text={props.translate('initialSettingsPage.signOut')}
                         />
                     </View>
-                    <View style={styles.mb5}>
-                        <Text style={[styles.headerText, styles.textXXLarge]}>
-                            {props.translate('genericErrorPage.title')}
-                        </Text>
-                    </View>
-                    <View style={styles.mb5}>
-                        <ErrorBodyText />
-                        <Text>
-                            {props.translate('genericErrorPage.body.helpTextConcierge')}
-                            <TextLink href={`mailto:${CONST.EMAIL.CONCIERGE}`} style={[styles.link]}>
-                                {CONST.EMAIL.CONCIERGE}
-                            </TextLink>
-                        </Text>
-                    </View>
-                    <View style={[styles.dFlex, styles.flexRow, styles.w100]}>
-                        <View style={[styles.flex1, styles.flexRow, styles.justifyContentBetween]}>
-                            <Button
-                                success
-                                small={props.isSmallScreenWidth}
-                                onPress={props.onRefresh}
-                                text={props.translate('genericErrorPage.refresh')}
-                            />
-                            <Button
-                                small={props.isSmallScreenWidth}
-                                onPress={onSignOutClick}
-                                text={props.translate('initialSettingsPage.signOut')}
-                            />
-                        </View>
-                    </View>
-                </View>
-            </View>
-            <View styles={styles.alignSelfEnd}>
-                <View style={[styles.flex1, styles.flexRow, styles.justifyContentCenter]}>
-                    <LogoWordmark height={30} width={80} />
                 </View>
             </View>
         </View>
-    );
-};
+        <View styles={styles.alignSelfEnd}>
+            <View style={[styles.flex1, styles.flexRow, styles.justifyContentCenter]}>
+                <LogoWordmark height={30} width={80} />
+            </View>
+        </View>
+    </View>
+);
 
-genericErrorPage.propTypes = propTypes;
+GenericErrorPage.propTypes = propTypes;
+GenericErrorPage.displayName = 'GenericErrorPage';
 
 export default compose(
     withWindowDimensions,
     withLocalize,
-)(genericErrorPage);
+)(GenericErrorPage);
