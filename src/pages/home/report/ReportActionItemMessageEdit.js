2c411acb865702c96017c2570c784b2c65abaa75
Manan Jadhav <manan.jadhav@gmail.com>
2022-01-03 00:45:31 +0530

    fix: Added EmojiPicker to the edit comment

--- src/pages/home/report/ReportActionItemMessageEdit.js
+++ src/pages/home/report/ReportActionItemMessageEdit.js
@@ -12,8 +12,12 @@ import toggleReportActionComposeView from '../../../libs/toggleReportActionCompo
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
 import Button from '../../../components/Button';
+import EmojiPicker from '../../../components/EmojiPicker';
 import ReportActionComposeFocusManager from '../../../libs/ReportActionComposeFocusManager';
 import compose from '../../../libs/compose';
+import * as User from '../../../libs/actions/User';
+import * as ReportUtils from '../../../libs/reportUtils';
+import CONST from '../../../CONST';
 
 const propTypes = {
     /** All the data of the action */
@@ -31,6 +35,19 @@ const propTypes = {
     /** A ref to forward to the text input */
     forwardedRef: PropTypes.func,
 
+    /** The report currently being looked at */
+    report: PropTypes.shape({
+
+        /** participants associated with current report */
+        participants: PropTypes.arrayOf(PropTypes.string),
+    }),
+
+    // The NVP describing a user's block status
+    blockedFromConcierge: PropTypes.shape({
+        // The date that the user will be unblocked
+        expiresAt: PropTypes.string,
+    }),
+
     /** Window Dimensions Props */
     ...windowDimensionsPropTypes,
 
@@ -40,6 +57,8 @@ const propTypes = {
 
 const defaultProps = {
     forwardedRef: () => {},
+    report: {},
+    blockedFromConcierge: {},
 };
 
 class ReportActionItemMessageEdit extends React.Component {
@@ -48,6 +67,7 @@ class ReportActionItemMessageEdit extends React.Component {
         this.updateDraft = this.updateDraft.bind(this);
         this.deleteDraft = this.deleteDraft.bind(this);
         this.debouncedSaveDraft = _.debounce(this.debouncedSaveDraft.bind(this), 1000);
+        this.addEmojiToTextBox = this.addEmojiToTextBox.bind(this);
         this.publishDraft = this.publishDraft.bind(this);
         this.triggerSaveOrCancel = this.triggerSaveOrCancel.bind(this);
         this.onSelectionChange = this.onSelectionChange.bind(this);
@@ -72,6 +92,24 @@ class ReportActionItemMessageEdit extends React.Component {
     onSelectionChange(e) {
         this.setState({selection: e.nativeEvent.selection});
     }
+
+    /**
+     * Callback for the emoji picker to add whatever emoji is chosen into the main input
+     *
+     * @param {String} emoji
+     */
+    addEmojiToTextBox(emoji) {
+        const newComment = this.state.draft.slice(0, this.state.selection.start)
+            + emoji + this.state.draft.slice(this.state.selection.end, this.state.draft.length);
+        this.setState(prevState => ({
+            selection: {
+                start: prevState.selection.start + emoji.length,
+                end: prevState.selection.start + emoji.length,
+            },
+        }));
+        this.updateDraft(newComment);
+    }
+
 
     /**
      * Update the value of the draft in Onyx
@@ -140,6 +178,16 @@ class ReportActionItemMessageEdit extends React.Component {
     }
 
     render() {
+        const isConciergeChat = this.props.report.participants
+            && _.contains(this.props.report.participants, CONST.EMAIL.CONCIERGE);
+
+        let isBlockedFromConcierge = false;
+        if (isConciergeChat && !_.isEmpty(this.props.blockedFromConcierge)) {
+            isBlockedFromConcierge = User.isBlockedFromConcierge(this.props.blockedFromConcierge.expiresAt);
+        }
+
+        const isArchivedChatRoom = ReportUtils.isArchivedRoom(this.props.report);
+
         return (
             <View style={styles.chatItemMessage}>
                 <View style={[styles.chatItemComposeBox, styles.flexRow, styles.chatItemComposeBoxColor]}>
@@ -160,6 +208,10 @@ class ReportActionItemMessageEdit extends React.Component {
                         }}
                         selection={this.state.selection}
                         onSelectionChange={this.onSelectionChange}
+                    />
+                    <EmojiPicker
+                        isDisabled={isBlockedFromConcierge || isArchivedChatRoom}
+                        onEmojiSelected={this.addEmojiToTextBox}
                     />
                 </View>
                 <View style={[styles.flexRow, styles.mt1]}>
