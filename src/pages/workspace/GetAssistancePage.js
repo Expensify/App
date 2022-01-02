270d1a4a4361628a750438dc93fbe38cf6057bfc
Puneet Lath <puneet@expensify.com>
2022-01-01 17:25:56 -0800

    Refactor GetAssistancePage into stateless functional component

--- src/pages/workspace/GetAssistancePage.js
+++ src/pages/workspace/GetAssistancePage.js
@@ -1,9 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, ScrollView} from 'react-native';
 import PropTypes from 'prop-types';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
-import compose from '../../libs/compose';
 import KeyboardAvoidingView from '../../components/KeyboardAvoidingView';
 import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
 import MenuItem from '../../components/MenuItem';
@@ -27,56 +26,47 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-class GetAssistancePage extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <ScreenWrapper>
-                <KeyboardAvoidingView>
-                    <HeaderWithCloseButton
-                        title={this.props.translate('getAssistancePage.title')}
-                        onCloseButtonPress={() => Navigation.dismissModal(true)}
-                        shouldShowBackButton
-                        onBackButtonPress={() => Navigation.goBack()}
-                    />
-                    <ScrollView style={styles.flex1} contentContainerStyle={[styles.pt0]}>
-                        <View style={[styles.ph5]}>
-                            <View style={[styles.flex1, styles.flexRow, styles.alignItemsCenter]}>
-                                <Text style={[styles.h1, styles.flex1]}>
-                                    {this.props.translate('getAssistancePage.subtitle')}
-                                </Text>
-                                <RequestCallIcon width={160} height={100} style={styles.flex1} />
-                            </View>
-                            <Text style={[styles.mb4]}>
-                                {this.props.translate('getAssistancePage.description')}
-                            </Text>
-                        </View>
-                        <MenuItem
-                            key="chatWithConcierge"
-                            title={this.props.translate('getAssistancePage.chatWithConcierge')}
-                            icon={Expensicons.ChatBubble}
-                            onPress={() => Report.navigateToConciergeChat()}
-                            shouldShowRightIcon
-                        />
-                        <MenuItem
-                            key="requestSetupCall"
-                            title={this.props.translate('getAssistancePage.requestSetupCall')}
-                            icon={Expensicons.Phone}
-                            onPress={() => Navigation.navigate(ROUTES.getRequestCallRoute(this.props.route.params.taskID))}
-                            shouldShowRightIcon
-                        />
-                    </ScrollView>
-                </KeyboardAvoidingView>
-            </ScreenWrapper>
-        );
-    }
-}
+const GetAssistancePage = props => (
+    <ScreenWrapper>
+        <KeyboardAvoidingView>
+            <HeaderWithCloseButton
+                title={props.translate('getAssistancePage.title')}
+                onCloseButtonPress={() => Navigation.dismissModal(true)}
+                shouldShowBackButton
+                onBackButtonPress={() => Navigation.goBack()}
+            />
+            <ScrollView style={styles.flex1} contentContainerStyle={[styles.pt0]}>
+                <View style={[styles.ph5]}>
+                    <View style={[styles.flex1, styles.flexRow, styles.alignItemsCenter]}>
+                        <Text style={[styles.h1, styles.flex1]}>
+                            {props.translate('getAssistancePage.subtitle')}
+                        </Text>
+                        <RequestCallIcon width={160} height={100} style={styles.flex1} />
+                    </View>
+                    <Text style={[styles.mb4]}>
+                        {props.translate('getAssistancePage.description')}
+                    </Text>
+                </View>
+                <MenuItem
+                    key="chatWithConcierge"
+                    title={props.translate('getAssistancePage.chatWithConcierge')}
+                    icon={Expensicons.ChatBubble}
+                    onPress={() => Report.navigateToConciergeChat()}
+                    shouldShowRightIcon
+                />
+                <MenuItem
+                    key="requestSetupCall"
+                    title={props.translate('getAssistancePage.requestSetupCall')}
+                    icon={Expensicons.Phone}
+                    onPress={() => Navigation.navigate(ROUTES.getRequestCallRoute(props.route.params.taskID))}
+                    shouldShowRightIcon
+                />
+            </ScrollView>
+        </KeyboardAvoidingView>
+    </ScreenWrapper>
+);
 
 GetAssistancePage.propTypes = propTypes;
+GetAssistancePage.displayName = 'GetAssistancePage';
 
-export default compose(
-    withLocalize,
-)(GetAssistancePage);
+export default withLocalize(GetAssistancePage);
