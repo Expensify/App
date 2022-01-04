c38d79e2a4fe24dbca8909166e323142597f94b9
Rory Abraham <rory@expensify.com>
2022-01-04 16:32:46 -0500

    Fix TDefaultRenderer propType

--- src/components/HTMLEngineProvider/HTMLRenderers/htmlRendererPropTypes.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/htmlRendererPropTypes.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export default {
     tnode: PropTypes.object,
-    TDefaultRenderer: PropTypes.object,
+    TDefaultRenderer: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
     key: PropTypes.string,
     style: PropTypes.object,
 };
