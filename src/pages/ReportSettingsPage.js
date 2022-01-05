bddeb9ead4124d388830a44962c45d69b5370139
Amal Nazeem <amal@expensify.com>
2022-01-05 16:04:05 -0500

    Create skeleton SettingsPage

---
+++ src/pages/ReportSettingsPage.js
@@ -0,0 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { compose } from 'underscore';
+import HeaderWithCloseButton from '../components/HeaderWithCloseButton';
+import ScreenWrapper from '../components/ScreenWrapper';
+import withLocalize from '../components/withLocalize';
+import Navigation from '../libs/Navigation/Navigation';
+import ONYXKEYS from '../ONYXKEYS';
+
+const propTypes = {
+};
+
+
+const ReportSettingsPage = (props) => {
+  return (
+    <ScreenWrapper>
+      <HeaderWithCloseButton
+        title={this.props.translate('common.settings')}
+        shouldShowBackButton
+        onBackButtonPress={() => Navigation.goBack()}
+        onCloseButtonPress={() => NavigationContainer.dismissModal(true)}
+      />
+    </ScreenWrapper>
+  );
+}
+
+ReportSettingsPage.propTypes = propTypes;
+ReportSettingsPage.displayName = 'ReportSettingsPage';
+
+export default compose(
+  withLocalize,
+  withOnyx({
+  }),
+)(ReportSettingsPage);
