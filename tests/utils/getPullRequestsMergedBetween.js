2aa5460df7466ef268111b2d30de121e92d72802
Rory Abraham <rory@expensify.com>
2022-01-05 12:29:07 -0500

    Simplify output to only include pull request numbers

--- tests/utils/getPullRequestsMergedBetween.js
+++ tests/utils/getPullRequestsMergedBetween.js
@@ -2,4 +2,11 @@
 
 const GitUtils = require('../../.github/libs/GitUtils');
 
-GitUtils.getPullRequestsMergedBetween(process.argv[2], process.argv[3]);
+/* eslint-disable no-console */
+const realConsoleLog = console.log;
+console.log = () => {};
+
+const output = GitUtils.getPullRequestsMergedBetween(process.argv[2], process.argv[3]);
+
+console.log = realConsoleLog;
+console.log(output);
