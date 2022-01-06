84da563d294f67247cef9a788a68ec8aaeadda83
Amal Nazeem <amal@expensify.com>
2022-01-06 12:12:46 -0500

    Add title into component

--- src/components/NotificationPreferences.js
+++ src/components/NotificationPreferences.js
@@ -7,6 +7,7 @@ import styles from '../styles/styles';
 import * as Report from '../libs/actions/Report';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 import Picker from './Picker';
+import Text from './Text';
 
 const propTypes = {
     /** The report whose notification preferences to show */
@@ -38,6 +39,11 @@ const NotificationPreferences = (props) => {
     };
     return (
         <View>
+            <View style={styles.mt4}>
+                <Text style={[styles.formLabel]} numberOfLines={1}>
+                    {props.translate('common.notifications')}
+                </Text>
+            </View>
             <View style={[styles.mb5]}>
                 <Picker
                     label={props.translate('reportDetailsPage.notificationPreferencesDescription')}
