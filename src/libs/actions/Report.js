877c7339f6e73f4887cdd7ee9a86c23c4d846506
Amal Nazeem <amal@expensify.com>
2022-01-07 17:06:57 -0500

    Add context to how report visibility is being used

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -194,7 +194,7 @@ function getSimplifiedReportObject(report) {
     // Used for archived rooms, will store the policy name that the room used to belong to.
     const oldPolicyName = lodashGet(report, ['reportNameValuePairs', 'oldPolicyName'], '');
 
-    // Used for User Created Policy Rooms
+    // Used for User Created Policy Rooms, will denote how access to a chat room is given among workspace members
     const visibility = lodashGet(report, ['reportNameValuePairs', 'visibility']);
 
     return {
