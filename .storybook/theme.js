4acc21462e979d92e18e5342577e049a33bb882d
Rory Abraham <rory@expensify.com>
2022-01-12 01:40:44 -0800

    Implement custom theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,7 +1,13 @@
 import {create} from '@storybook/theming';
+import colors from '../src/styles/colors';
 
 export default create({
     base: 'light',
     brandTitle: 'Expensify UI Docs',
     brandImage: 'logomark.svg',
+    appBg: colors.dark,
+    textColor: 'rgba(255,255,255,0.9)',
+    textInverseColor: colors.black,
+    fontBase: 'GTAmericaExp-Regular',
+    inputBg: colors.gray1,
 });
