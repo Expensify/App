35318796ee940673526f4e635c86c1b5d08b94fe
sig5 <singhalsakar@gmail.com>
2022-01-12 17:19:23 +0530

    fixed linting issue

--- src/pages/iou/steps/IOUAmountPage.js
+++ src/pages/iou/steps/IOUAmountPage.js
@@ -81,7 +81,7 @@ class IOUAmountPage extends React.Component {
         this.stripCommaFromAmount = this.stripCommaFromAmount.bind(this);
         this.focusTextInput = this.focusTextInput.bind(this);
         this.dismissKeyboardWhenBackgrounded = this.dismissKeyboardWhenBackgrounded.bind(this);
-        
+
         this.state = {
             amount: props.selectedAmount,
         };
