440c85fd030029b7f038483e73e3a51e93ec354c
sahil <thesahindia@gmail.com>
2022-01-19 21:54:58 +0530

    use lodashGet

--- src/pages/home/report/EmojiPickerMenu/index.js
+++ src/pages/home/report/EmojiPickerMenu/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {View, FlatList} from 'react-native';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
+import lodashGet from 'lodash/get';
 import CONST from '../../../../CONST';
 import styles from '../../../../styles/styles';
 import * as StyleUtils from '../../../../styles/StyleUtils';
@@ -143,9 +144,7 @@ class EmojiPickerMenu extends Component {
             // Select the currently highlighted emoji if enter is pressed
             if (keyBoardEvent.key === 'Enter' && this.state.highlightedIndex !== -1) {
                 const item = this.state.filteredEmojis[this.state.highlightedIndex];
-                const emoji = item.types && item.types[this.props.preferredSkinTone]
-                    ? item.types[this.props.preferredSkinTone]
-                    : item.code;
+                const emoji = lodashGet(item, ['types', this.props.preferredSkinTone], item.code);
                 this.props.onEmojiSelected(emoji, item);
                 return;
             }
