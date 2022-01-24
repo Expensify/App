fe47ea619850b5037d978b58748c3ebdffe99a8f
Md Neyaz Ahmad <mdneyazahmad66@gmail.com>
2022-01-24 20:54:26 +0530

    fix: scroll to bottom on large paragraph text

--- src/pages/home/report/ReportActionCompose.js
+++ src/pages/home/report/ReportActionCompose.js
@@ -351,6 +351,8 @@ class ReportActionCompose extends React.Component {
         if (newComment) {
             this.debouncedBroadcastUserIsTyping();
         }
+
+        this.textInput.scrollTop = this.textInput.scrollHeight;
     }
 
     /**
