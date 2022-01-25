8e19bf3efc8891d9676c8e7fa9e381766ee5ecdd
Sobit Neupane <073bct543.sobit@pcampus.edu.np>
2022-01-25 12:02:22 +0545

    Maintain consistent interface

--- src/libs/actions/PersonalDetails.js
+++ src/libs/actions/PersonalDetails.js
@@ -155,7 +155,7 @@ function getFromReportParticipants(reports) {
         .value();
 
     if (participantEmails.length === 0) {
-        return Promise.resolve([]);
+        return Promise.resolve({});
     }
 
     return API.PersonalDetails_GetForEmails({emailList: participantEmails.join(',')})
