6a2f52a4213c18e0eb6ea86d51cf99cae4d1a5a4
Amal Nazeem <amal@expensify.com>
2022-01-26 10:43:57 -0500

    Use const because this is always policy room

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1581,8 +1581,7 @@ function createPolicyRoom(policyID, reportName, visibility) {
             // Make sure the report has its icons set
             const report = allReports[reportID];
             const icons = OptionsListUtils.getReportIcons(report, {});
-            const chatType = lodashGet(report, ['reportNameValuePairs', 'chatType'], '');
-            const reportName = ReportUtils.getChatReportName(report, chatType);
+            const reportName = ReportUtils.getChatReportName(report, CONST.REPORT.CHAT_TYPE.POLICY_ROOM);
             Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {icons, reportName});
             Navigation.navigate(ROUTES.getReportRoute(reportID));
         })
