f5fc6ade0a13d6ecab600862afad08af06fd4f32
Rory Abraham <rory@expensify.com>
2022-01-31 14:09:18 -0800

    Rebuild GH actions

--- .github/actions/awaitStagingDeploys/index.js
+++ .github/actions/awaitStagingDeploys/index.js
@@ -17,21 +17,36 @@ function run() {
     return promiseDoWhile(
         () => !_.isEmpty(currentStagingDeploys),
         _.throttle(
-            () => GitHubUtils.octokit.actions.listWorkflowRuns({
-                owner: GitHubUtils.GITHUB_OWNER,
-                repo: GitHubUtils.APP_REPO,
-                workflow_id: 'platformDeploy.yml',
-                event: 'push',
-            })
-                .then(response => currentStagingDeploys = _.filter(response.data.workflow_runs, workflowRun => workflowRun.status !== 'completed'))
+            () => Promise.all([
+                // These are active deploys
+                GitHubUtils.octokit.actions.listWorkflowRuns({
+                    owner: GitHubUtils.GITHUB_OWNER,
+                    repo: GitHubUtils.APP_REPO,
+                    workflow_id: 'platformDeploy.yml',
+                    event: 'push',
+                }),
+
+                // These have the potential to become active deploys, so we need to wait for them to finish as well
+                // In this context, we'll refer to unresolved preDeploy workflow runs as staging deploys as well
+                GitHubUtils.octokit.actions.listWorkflowRuns({
+                    owner: GitHubUtils.GITHUB_OWNER,
+                    repo: GitHubUtils.APP_REPO,
+                    workflow_id: 'preDeploy.yml',
+                }),
+            ])
+                .then(responses => [
+                    ...responses[0].data.workflow_runs,
+                    ...responses[1].data.workflow_runs,
+                ])
+                .then(workflowRuns => currentStagingDeploys = _.filter(workflowRuns, workflowRun => workflowRun.status !== 'completed'))
                 .then(() => console.log(
                     _.isEmpty(currentStagingDeploys)
                         ? 'No current staging deploys found'
                         : `Found ${currentStagingDeploys.length} staging deploy${currentStagingDeploys.length > 1 ? 's' : ''} still running...`,
                 )),
 
-            // Poll every 90 seconds instead of every 10 seconds
-            GitHubUtils.POLL_RATE * 9,
+            // Poll every 60 seconds instead of every 10 seconds
+            GitHubUtils.POLL_RATE * 6,
         ),
     );
 }
