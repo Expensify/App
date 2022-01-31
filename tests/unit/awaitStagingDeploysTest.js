0ced2f621a9a499ae0990b548c9e5af4521b4af8
Rory Abraham <rory@expensify.com>
2022-01-31 14:02:54 -0800

    Address race condition for pull requests merged right before lock label is added

--- tests/unit/awaitStagingDeploysTest.js
+++ tests/unit/awaitStagingDeploysTest.js
@@ -1,6 +1,7 @@
 /**
  * @jest-environment node
  */
+const _ = require('underscore');
 const run = require('../../.github/actions/awaitStagingDeploys/awaitStagingDeploys');
 const GitHubUtils = require('../../.github/libs/GithubUtils');
 
@@ -9,7 +10,25 @@ const TEST_POLL_RATE = 1;
 const COMPLETED_WORKFLOW = {status: 'completed'};
 const INCOMPLETE_WORKFLOW = {status: 'in_progress'};
 
-const mockListWorkflowRuns = jest.fn();
+const mockListPlatformDeploys = jest.fn();
+const mockListPreDeploys = jest.fn();
+const mockListWorkflowRuns = jest.fn().mockImplementation((args) => {
+    const defaultReturn = Promise.resolve({data: {workflow_runs: []}});
+
+    if (!_.has(args, 'workflow_id')) {
+        return defaultReturn;
+    }
+
+    if (args.workflow_id === 'platformDeploy.yml') {
+        return mockListPlatformDeploys();
+    }
+
+    if (args.workflow_id === 'preDeploy.yml') {
+        return mockListPreDeploys();
+    }
+
+    return defaultReturn;
+});
 
 beforeAll(() => {
     // Mock octokit module
@@ -24,7 +43,7 @@ beforeAll(() => {
 
 describe('awaitStagingDeploys', () => {
     test('Should wait for all running staging deploys to finish', () => {
-        mockListWorkflowRuns.mockResolvedValueOnce({
+        mockListPlatformDeploys.mockResolvedValueOnce({
             data: {
                 workflow_runs: [
                     COMPLETED_WORKFLOW,
@@ -33,7 +52,12 @@ describe('awaitStagingDeploys', () => {
                 ],
             },
         });
-        mockListWorkflowRuns.mockResolvedValueOnce({
+        mockListPreDeploys.mockResolvedValueOnce({
+            data: {
+                workflow_runs: [],
+            },
+        });
+        mockListPlatformDeploys.mockResolvedValueOnce({
             data: {
                 workflow_runs: [
                     COMPLETED_WORKFLOW,
@@ -42,11 +66,39 @@ describe('awaitStagingDeploys', () => {
                 ],
             },
         });
-        mockListWorkflowRuns.mockResolvedValueOnce({
+        mockListPreDeploys.mockResolvedValueOnce({
+            data: {
+                workflow_runs: [],
+            },
+        });
+        mockListPlatformDeploys.mockResolvedValueOnce({
             data: {
                 workflow_runs: [
                     COMPLETED_WORKFLOW,
                     COMPLETED_WORKFLOW,
+                    COMPLETED_WORKFLOW,
+                ],
+            },
+        });
+        mockListPreDeploys.mockResolvedValueOnce({
+            data: {
+                workflow_runs: [
+                    INCOMPLETE_WORKFLOW,
+                ],
+            },
+        });
+        mockListPlatformDeploys.mockResolvedValueOnce({
+            data: {
+                workflow_runs: [
+                    COMPLETED_WORKFLOW,
+                    COMPLETED_WORKFLOW,
+                    COMPLETED_WORKFLOW,
+                ],
+            },
+        });
+        mockListPreDeploys.mockResolvedValueOnce({
+            data: {
+                workflow_runs: [
                     COMPLETED_WORKFLOW,
                 ],
             },
@@ -55,9 +107,10 @@ describe('awaitStagingDeploys', () => {
         const consoleSpy = jest.spyOn(console, 'log');
         return run()
             .then(() => {
-                expect(consoleSpy).toHaveBeenCalledTimes(3);
+                expect(consoleSpy).toHaveBeenCalledTimes(4);
                 expect(consoleSpy).toHaveBeenNthCalledWith(1, 'Found 2 staging deploys still running...');
                 expect(consoleSpy).toHaveBeenNthCalledWith(2, 'Found 1 staging deploy still running...');
+                expect(consoleSpy).toHaveBeenNthCalledWith(3, 'Found 1 staging deploy still running...');
                 expect(consoleSpy).toHaveBeenLastCalledWith('No current staging deploys found');
             });
     });
