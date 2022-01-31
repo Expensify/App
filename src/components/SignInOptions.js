98e19c3856162b26d7ecfce2a6a6f26fa6d96958
Marco Chávez <marco@expensify.com>
2022-01-30 18:05:25 -0600

    add onEmailOrPhoneNumberPress callback to SignInOptions

--- src/components/SignInOptions.js
+++ src/components/SignInOptions.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
 import styles from '../styles/styles';
 import Button from './Button';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 import useGoogleLogin from '../libs/useGoogleLogin';
 
 const propTypes = {
+    /** Callback to trigger when the button "Email or Phone Number" is pressed */
+    onEmailOrPhoneNumberPress: PropTypes.func.isRequired,
     ...withLocalizePropTypes,
 };
 
-const defaultProps = {
-    anchorPosition: {},
-    payPalMeUsername: '',
-    shouldShowPaypal: true,
-    betas: [],
-};
-const SignInOptions = (props) => {
+// const defaultProps = {
+// };
+function SignInOptions(props) {
     const {
         googleAuthLoaded, signIn, isSigningIn, res, err,
     } = useGoogleLogin();
@@ -27,22 +27,40 @@ const SignInOptions = (props) => {
         debugger;
         console.log(err);
     }
+    if (props.asdfFunc) {
+        props.asdfFunc();
+    }
     return (
         <>
             <View style={[styles.mt5]}>
                 <Button
                     success
-                    text={props.translate('signInPage.google')}
+                    text={props.translate('signInPage.emailOrPhoneNumber')}
+                    onPress={props.onEmailOrPhoneNumberPress}
+                />
+            </View>
+            <View style={[styles.mt3]}>
+                <Button
+                    success
+                    text={props.translate('signInPage.googleButton', {name: lodashGet(res, '')})}
                     isLoading={isSigningIn}
                     isDisabled={!googleAuthLoaded}
                     onPress={signIn}
                 />
             </View>
+            <View style={[styles.mt3]}>
+                <Button
+                    success
+                    text={props.translate('signInPage.appleButton')}
+                />
+            </View>
         </>
     );
-};
+}
 
 SignInOptions.propTypes = propTypes;
-SignInOptions.defaultProps = defaultProps;
+
+// SignInOptions.defaultProps = defaultProps;
+
 
 export default withLocalize(SignInOptions);
