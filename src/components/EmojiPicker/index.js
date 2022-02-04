4d0857363cbf289edd45166758d04b3dc6526e3d
Manan Jadhav <manan.jadhav@gmail.com>
2022-02-05 03:50:35 +0530

    feat: Removed withWindowDimensions and withOnyx and moved to EmojiPickerMenu

--- src/components/EmojiPicker/index.js
+++ src/components/EmojiPicker/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import {Dimensions} from 'react-native';
-import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
-import compose from '../../libs/compose';
 import Popover from '../Popover';
-import ONYXKEYS from '../../ONYXKEYS';
 import EmojiPickerMenu from './EmojiPickerMenu';
 import * as User from '../../libs/actions/User';
-import * as EmojiUtils from '../../libs/EmojiUtils';
 import CONST from '../../CONST';
-import withWindowDimensions, {windowDimensionsPropTypes} from '../withWindowDimensions';
-
-const propTypes = {
-    ...windowDimensionsPropTypes,
-};
 
 class EmojiPicker extends React.Component {
     constructor(props) {
@@ -23,7 +14,6 @@ class EmojiPicker extends React.Component {
         this.showEmojiPicker = this.showEmojiPicker.bind(this);
         this.selectEmoji = this.selectEmoji.bind(this);
         this.measureEmojiPopoverAnchorPosition = this.measureEmojiPopoverAnchorPosition.bind(this);
-        this.setPreferredSkinTone = this.setPreferredSkinTone.bind(this);
         this.focusEmojiSearchInput = this.focusEmojiSearchInput.bind(this);
         this.onModalHide = () => {};
         this.onEmojiSelected = () => {};
@@ -51,25 +41,11 @@ class EmojiPicker extends React.Component {
     }
 
     /**
-     * Update preferredSkinTone and sync with Onyx, NVP.
-     * @param {Number|String} skinTone
-     */
-    setPreferredSkinTone(skinTone) {
-        if (skinTone === this.props.preferredSkinTone) {
-            return;
-        }
-
-        User.setPreferredSkinTone(skinTone);
-    }
-
-    /**
      * Callback for the emoji picker to add whatever emoji is chosen into the main input
      *
      * @param {String} emoji
-     * @param {Object} emojiObject
      */
-    selectEmoji(emoji, emojiObject) {
-        EmojiUtils.addToFrequentlyUsedEmojis(this.props.frequentlyUsedEmojis, emojiObject);
+    selectEmoji(emoji) {
         this.hideEmojiPicker();
         if (_.isFunction(this.onEmojiSelected)) {
             this.onEmojiSelected(emoji);
@@ -122,49 +98,30 @@ class EmojiPicker extends React.Component {
     }
 
     render() {
+        // There is no way to disable animations and they are really laggy, because there are so many
+        // emojis. The best alternative is to set it to 1ms so it just "pops" in and out
         return (
-            <>
-                {
-
-                    // There is no way to disable animations and they are really laggy, because there are so many
-                    // emojis. The best alternative is to set it to 1ms so it just "pops" in and out
-                }
-                <Popover
-                    isVisible={this.state.isEmojiPickerVisible}
-                    onClose={this.hideEmojiPicker}
-                    onModalShow={this.focusEmojiSearchInput}
-                    onModalHide={this.onModalHide}
-                    hideModalContentWhileAnimating
-                    animationInTiming={1}
-                    animationOutTiming={1}
-                    anchorPosition={{
-                        bottom: this.props.windowHeight - this.state.emojiPopoverAnchorPosition.vertical,
-                        left: this.state.emojiPopoverAnchorPosition.horizontal - CONST.EMOJI_PICKER_SIZE,
-                    }}
-                >
-                    <EmojiPickerMenu
-                        onEmojiSelected={this.selectEmoji}
-                        ref={el => this.emojiSearchInput = el}
-                        preferredSkinTone={this.props.preferredSkinTone}
-                        updatePreferredSkinTone={this.setPreferredSkinTone}
-                        frequentlyUsedEmojis={this.props.frequentlyUsedEmojis}
-                    />
-                </Popover>
-            </>
+            <Popover
+                isVisible={this.state.isEmojiPickerVisible}
+                onClose={this.hideEmojiPicker}
+                onModalShow={this.focusEmojiSearchInput}
+                onModalHide={this.onModalHide}
+                hideModalContentWhileAnimating
+                animationInTiming={1}
+                animationOutTiming={1}
+                anchorPosition={{
+                    bottom: 10, // 500 - this.state.emojiPopoverAnchorPosition.vertical,
+                    left: this.state.emojiPopoverAnchorPosition.horizontal - CONST.EMOJI_PICKER_SIZE,
+                }}
+            >
+                <EmojiPickerMenu
+                    onEmojiSelected={this.selectEmoji}
+                    ref={el => this.emojiSearchInput = el}
+                    updatePreferredSkinTone={User.setPreferredSkinTone}
+                />
+            </Popover>
         );
     }
 }
 
-EmojiPicker.propTypes = propTypes;
-
-export default compose(
-    withWindowDimensions,
-    withOnyx({
-        preferredSkinTone: {
-            key: ONYXKEYS.PREFERRED_EMOJI_SKIN_TONE,
-        },
-        frequentlyUsedEmojis: {
-            key: ONYXKEYS.FREQUENTLY_USED_EMOJIS,
-        },
-    }),
-)(EmojiPicker);
+export default EmojiPicker;
