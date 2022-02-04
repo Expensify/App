c3e298b200eb87fccd19db4ae9e65d77b8e051c7
sig5 <singhalsakar@gmail.com>
2022-02-04 09:03:39 +0530

    modified datepicker props

--- src/components/DatePicker/datepickerPropTypes.js
+++ src/components/DatePicker/datepickerPropTypes.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import * as FormUtils from '../../libs/FormUtils';
 import {
     propTypes as fieldPropTypes,
     defaultProps as defaultFieldPropTypes,
@@ -15,12 +16,33 @@ const propTypes = {
 
     /* Restricts for selectable max date range for the picker */
     maximumDate: PropTypes.instanceOf(Date),
+
+    /** Indicates that the input is being used with the Form component */
+    isFormInput: PropTypes.bool,
+
+    /**
+      * The ID used to uniquely identify the input
+      *
+      * @param {Object} props - props passed to the input
+      * @returns {Object} - returns an Error object if isFormInput is supplied but inputID is falsey or not a string
+      */
+    inputID: props => FormUtils.getInputIDPropTypes(props),
+
+    /** Saves a draft of the input value when used in a form */
+    shouldSaveDraft: PropTypes.bool,
+
+    /** Error text to appear */
+    errorText: PropTypes.string,
+
 };
 
 const defaultProps = {
     ...defaultFieldPropTypes,
     value: undefined,
     maximumDate: undefined,
+    isFormInput: false,
+    shouldSaveDraft: false,
+
 };
 
 export {propTypes, defaultProps};
