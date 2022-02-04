f972b3e71ab70dd3c79a82a9771601f55dfea350
Manan Jadhav <manan.jadhav@gmail.com>
2022-02-05 01:49:18 +0530

    feat: Added an independent resuable emoji picker button

---
+++ src/components/EmojiPicker/EmojiPickerButton.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Pressable} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../../styles/styles';
+import * as StyleUtils from '../../styles/StyleUtils';
+import getButtonState from '../../libs/getButtonState';
+import * as Expensicons from '../Icon/Expensicons';
+import Tooltip from '../Tooltip';
+import Icon from '../Icon';
+import withLocalize, {withLocalizePropTypes} from '../withLocalize';
+import * as EmojiPickerAction from '../../libs/actions/EmojiPickerAction';
+
+const propTypes = {
+    /** Flag to disable the emoji picker button */
+    isDisabled: PropTypes.bool,
+
+    ...withLocalizePropTypes,
+};
+
+const defaultProps = {
+    isDisabled: false,
+};
+
+//
+
+class EmojiPickerButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.measureEmojiPopoverAnchorPosition = this.measureEmojiPopoverAnchorPosition.bind(this);
+        this.showEmojiPicker = this.showEmojiPicker.bind(this);
+    }
+
+    showEmojiPicker() {
+        EmojiPickerAction.showEmojiPicker(this.props.onModalHide, this.props.onEmojiSelected, this.props.onBeforeShowEmojiPicker);
+    }
+
+    measureEmojiPopoverAnchorPosition() {
+        if (!EmojiPickerAction.emojiPickerRef || !EmojiPickerAction.emojiPickerRef.current) {
+            return;
+        }
+        EmojiPickerAction.emojiPickerRef.current.measureEmojiPopoverAnchorPosition(this.emojiPopoverAnchor);
+    }
+
+    render() {
+        return (
+            <Pressable
+                ref={el => this.emojiPopoverAnchor = el}
+                style={({hovered, pressed}) => ([
+                    styles.chatItemEmojiButton,
+                    StyleUtils.getButtonBackgroundColorStyle(getButtonState(hovered, pressed)),
+                ])}
+                disabled={this.props.isDisabled}
+                onPress={this.showEmojiPicker}
+                onLayout={this.measureEmojiPopoverAnchorPosition}
+            >
+                {({hovered, pressed}) => (
+                    <Tooltip text={this.props.translate('reportActionCompose.emoji')}>
+                        <Icon
+                            src={Expensicons.Emoji}
+                            fill={StyleUtils.getIconFillColor(getButtonState(hovered, pressed))}
+                        />
+                    </Tooltip>
+                )}
+            </Pressable>
+        );
+    }
+}
+
+EmojiPickerButton.propTypes = propTypes;
+EmojiPickerButton.defaultProps = defaultProps;
+export default withLocalize(EmojiPickerButton);
