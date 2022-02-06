62c3a028ad50e4c44cf0a6d88b48d8149b83c039
sig5 <singhalsakar@gmail.com>
2022-02-06 18:34:09 +0530

    aggregated proptypes

---
+++ src/pages/iou/steps/IOUAmountPage/IOUAmountPropTypes.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import {windowDimensionsPropTypes} from '../../../../components/withWindowDimensions';
+import {withLocalizePropTypes} from '../../../../components/withLocalize';
+import CONST from '../../../../CONST';
+
+const propTypes = {
+    /** Whether or not this IOU has multiple participants */
+    hasMultipleParticipants: PropTypes.bool.isRequired,
+
+    /** The ID of the report this screen should display */
+    reportID: PropTypes.string.isRequired,
+
+    /** Callback to inform parent modal of success */
+    onStepComplete: PropTypes.func.isRequired,
+
+    /** The currency list constant object from Onyx */
+    currencyList: PropTypes.objectOf(PropTypes.shape({
+        /** Symbol for the currency */
+        symbol: PropTypes.string,
+
+        /** Name of the currency */
+        name: PropTypes.string,
+
+        /** ISO4217 Code for the currency */
+        ISO4217: PropTypes.string,
+    })).isRequired,
+
+    /** Previously selected amount to show if the user comes back to this screen */
+    selectedAmount: PropTypes.string.isRequired,
+
+    /** Window Dimensions Props */
+    ...windowDimensionsPropTypes,
+
+    /* Onyx Props */
+
+    /** Holds data related to IOU view state, rather than the underlying IOU data. */
+    iou: PropTypes.shape({
+
+        /** Whether or not the IOU step is loading (retrieving users preferred currency) */
+        loading: PropTypes.bool,
+
+        /** Selected Currency Code of the current IOU */
+        selectedCurrencyCode: PropTypes.string,
+    }),
+
+    ...withLocalizePropTypes,
+};
+
+const defaultProps = {
+    iou: {
+        selectedCurrencyCode: CONST.CURRENCY.USD,
+    },
+};
+export {
+    defaultProps,
+    propTypes,
+};
--- src/pages/iou/steps/IOUAmountPage/index.js
+++ src/pages/iou/steps/IOUAmountPage/index.js
@@ -4,71 +4,22 @@ import {
     TouchableOpacity,
     InteractionManager,
 } from 'react-native';
-import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import lodashGet from 'lodash/get';
 import _ from 'underscore';
 import ONYXKEYS from '../../../../ONYXKEYS';
 import styles from '../../../../styles/styles';
 import BigNumberPad from '../../../../components/BigNumberPad';
-import withWindowDimensions, {windowDimensionsPropTypes} from '../../../../components/withWindowDimensions';
+import withWindowDimensions from '../../../../components/withWindowDimensions';
 import Navigation from '../../../../libs/Navigation/Navigation';
 import ROUTES from '../../../../ROUTES';
-import withLocalize, {withLocalizePropTypes} from '../../../../components/withLocalize';
+import withLocalize from '../../../../components/withLocalize';
 import compose from '../../../../libs/compose';
 import Button from '../../../../components/Button';
 import Text from '../../../../components/Text';
 import CONST from '../../../../CONST';
 import TextInputAutoWidthWithoutKeyboard from '../../../../components/TextInputAutoWidthWithoutKeyboard';
-
-const propTypes = {
-    /** Whether or not this IOU has multiple participants */
-    hasMultipleParticipants: PropTypes.bool.isRequired,
-
-    /** The ID of the report this screen should display */
-    reportID: PropTypes.string.isRequired,
-
-    /** Callback to inform parent modal of success */
-    onStepComplete: PropTypes.func.isRequired,
-
-    /** The currency list constant object from Onyx */
-    currencyList: PropTypes.objectOf(PropTypes.shape({
-        /** Symbol for the currency */
-        symbol: PropTypes.string,
-
-        /** Name of the currency */
-        name: PropTypes.string,
-
-        /** ISO4217 Code for the currency */
-        ISO4217: PropTypes.string,
-    })).isRequired,
-
-    /** Previously selected amount to show if the user comes back to this screen */
-    selectedAmount: PropTypes.string.isRequired,
-
-    /** Window Dimensions Props */
-    ...windowDimensionsPropTypes,
-
-    /* Onyx Props */
-
-    /** Holds data related to IOU view state, rather than the underlying IOU data. */
-    iou: PropTypes.shape({
-
-        /** Whether or not the IOU step is loading (retrieving users preferred currency) */
-        loading: PropTypes.bool,
-
-        /** Selected Currency Code of the current IOU */
-        selectedCurrencyCode: PropTypes.string,
-    }),
-
-    ...withLocalizePropTypes,
-};
-
-const defaultProps = {
-    iou: {
-        selectedCurrencyCode: CONST.CURRENCY.USD,
-    },
-};
+import {propTypes, defaultProps} from './IOUAmountPropTypes';
 
 class IOUAmountPage extends React.Component {
     constructor(props) {
