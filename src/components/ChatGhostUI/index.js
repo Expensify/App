d98a67c9bc23c95fe92d025f7d6a19ed0fcdeccc
Manan Jadhav <manan.jadhav@gmail.com>
2022-02-07 11:08:50 +0530

    feat: Updated chat ghost ui with dynamic loaders

--- src/components/ChatGhostUI/index.js
+++ src/components/ChatGhostUI/index.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import {Dimensions} from 'react-native';
-import ContentLoader from 'react-content-loader';
 import _ from 'underscore';
+import ContentLoadingOneLine from './ContentLoadingElements/ContentLoadingOneLine';
+import ContentLoadingTwoLines from './ContentLoadingElements/ContentLoadingTwoLines';
+import ContentLoadingThreeLines from './ContentLoadingElements/ContentLoadingThreeLines';
 
 
-const ChatList = (props) => {
+const ChatList = () => {
     const windowHeight = Dimensions.get('window').height;
 
-    const height = 140;
+    const height = 80;
     const numberOfRows = Math.floor(windowHeight / height);
     const contentItems = Array.from({length: numberOfRows}, (v, i) => i);
 
     return (
         <>
             {
-                _.map(contentItems, item => (
-                    <ContentLoader height={height} width="100%" key={item} {...props}>
-                        <circle cx="50" cy="70" r="30" />
-                        <rect x="100" y="64.7" width="30%" height="17" />
-                        <rect x="100" y="29.5" width="85%" height="17" />
-                        <rect x="100" y="97.8" width="60%" height="16" />
-                    </ContentLoader>
-                ))
+                _.map(contentItems, (item, index) => {
+                    const iconIndex = (index + 1) % 4;
+                    switch (iconIndex) {
+                        case 2:
+                            return <ContentLoadingTwoLines key={item} />;
+                        case 0:
+                            return <ContentLoadingThreeLines key={item} />;
+                        default:
+                            return <ContentLoadingOneLine key={item} />;
+                    }
+                })
             }
+
         </>
     );
 };
