92dd9a1870955940e8120e611eebf12e39ecce4e
Manan Jadhav <manan.jadhav@gmail.com>
2022-02-07 15:55:00 +0530

    feat: Added native files for content loading elements

---
+++ src/components/ChatGhostUI/ContentLoadingElements/ContentLoadingThreeLines/index.native.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ContentLoader, {Circle, Rect} from 'react-content-loader/native';
+
+const ContentLoadingThreeLines = () => (
+    <ContentLoader
+        height={100}
+        width="100%"
+        title=""
+    >
+        <Circle cx="50" cy="26" r="20" />
+        <Rect x="90" y="11" width="20%" height="8" />
+        <Rect x="90" y="31" width="90%" height="8" />
+        <Rect x="90" y="51" width="90%" height="8" />
+        <Rect x="90" y="71" width="50%" height="8" />
+    </ContentLoader>
+);
+
+ContentLoadingThreeLines.displayName = 'ContentLoadingThreeLines';
+export default ContentLoadingThreeLines;
