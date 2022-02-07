248dc37a7b86d4ee141702c55dd642eca9bb83e0
Aswin S <mr.aswin@gmail.com>
2022-02-08 01:08:20 +0530

    Revert removal of extra blank line

--- src/components/TouchableWithoutFocus.js
+++ src/components/TouchableWithoutFocus.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
+
 const propTypes = {
     /** Element that should be clickable  */
     children: PropTypes.oneOfType([
