91f354b1e839ab5b8daa7ed64d25dfb70e71cb39
AHMED <a.s1171111@gmail.com>
2022-02-09 20:19:06 +0200

    fix / android need more config .

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, I18nManager} from 'react-native';
 import App from './src/App';
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
+
+// force app layout to work left to right.
+I18nManager.allowRTL(false);
+I18nManager.forceRTL(false);
+I18nManager.swapLeftAndRightInRTL(false);
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
 additionalAppSetup();
