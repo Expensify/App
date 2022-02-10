7aba2363e3fedc6428b27559ae37254481c93ec7
Mark Toman <marktomandev@gmail.com>
2022-02-10 16:50:21 +0100

    Read and write rate as cents

--- src/pages/workspace/reimburse/WorkspaceReimburseNoVBAView.js
+++ src/pages/workspace/reimburse/WorkspaceReimburseNoVBAView.js
@@ -64,7 +64,7 @@ class WorkspaceReimburseNoVBAView extends React.Component {
         Policy.setCustomUnitRate(this.props.policyID, this.state.unitID, {
             customUnitRateID: this.state.rateID,
             name: this.state.rateName,
-            rate: value,
+            rate: value * 100,
         }, null);
     }, 3000, {leading: false, trailing: true});
 
@@ -76,7 +76,7 @@ class WorkspaceReimburseNoVBAView extends React.Component {
             unitValue: lodashGet(props, 'policy.customUnit.value', 'mi'),
             rateID: lodashGet(props, 'policy.customUnit.rate.id', ''),
             rateName: lodashGet(props, 'policy.customUnit.rate.name', ''),
-            rateValue: this.getRateDisplayValue(lodashGet(props, 'policy.customUnit.rate.value', '')),
+            rateValue: this.getRateDisplayValue(lodashGet(props, 'policy.customUnit.rate.value', 0) / 100),
             rateCurrency: lodashGet(props, 'policy.customUnit.rate.currency', ''),
         };
     }
