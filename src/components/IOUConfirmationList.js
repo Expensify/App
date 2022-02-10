3ea199fb5b38e8fdfcef72a526601bafb1878942
Rory Abraham <rory@expensify.com>
2022-02-10 01:08:46 -0800

    Clean up SettlementButton onPress

--- src/components/IOUConfirmationList.js
+++ src/components/IOUConfirmationList.js
@@ -138,7 +138,7 @@ class IOUConfirmationList extends Component {
         };
 
         this.toggleOption = this.toggleOption.bind(this);
-        this.onPress = this.onPress.bind(this);
+        this.confirm = this.confirm.bind(this);
     }
 
     componentDidMount() {
@@ -150,7 +150,7 @@ class IOUConfirmationList extends Component {
     /**
      * @param {String} value
      */
-    onPress(value) {
+    confirm(value) {
         if (this.props.iouType === CONST.IOU.IOU_TYPE.SEND) {
             Log.info(`[IOU] Sending money via: ${value}`);
             this.props.onSendMoney(value);
@@ -388,7 +388,7 @@ class IOUConfirmationList extends Component {
                         <SettlementButton
                             isDisabled={shouldDisableButton}
                             isLoading={this.props.iou.loading && !this.props.network.isOffline}
-                            onPress={this.onPress}
+                            onPress={this.confirm}
                             shouldShowPaypal={Boolean(recipient.payPalMeAddress)}
                             recipientPhoneNumber={recipient.phoneNumber}
                             enablePaymentsRoute={ROUTES.IOU_SEND_ENABLE_PAYMENTS}
@@ -400,7 +400,7 @@ class IOUConfirmationList extends Component {
                         <ButtonWithMenu
                             isDisabled={shouldDisableButton}
                             isLoading={isLoading}
-                            onPress={(_event, value) => this.onPress(value)}
+                            onPress={(_event, value) => this.confirm(value)}
                             options={this.splitOrRequestOptions}
                         />
                     )}
