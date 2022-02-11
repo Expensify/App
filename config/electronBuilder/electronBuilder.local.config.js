f3b03aca2503d6800313aca1a63976a2fd93bd26
Peter Velkov <kidroca@gmail.com>
2022-02-11 19:08:35 +0200

    Change file structure - files from parent folders cannot be included

--- config/electronBuilder/electronBuilder.local.config.js
+++ config/electronBuilder/electronBuilder.local.config.js
@@ -30,12 +30,13 @@ module.exports = {
         internetEnabled: true,
     },
     files: [
-        '../dist/**/*',
-        '*.js',
-        '../src/libs/checkForUpdates.js',
-        '../src/CONST/ENVIRONMENT.js',
+        './dist/**/*',
+        './desktop/*.js',
+        './src/libs/checkForUpdates.js',
+        './src/CONST/ENVIRONMENT.js',
     ],
     directories: {
-        output: '../desktop-build',
+        app: 'desktop',
+        output: 'desktop-build',
     },
 };
