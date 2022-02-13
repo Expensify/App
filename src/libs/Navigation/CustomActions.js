d42410d5670107dd70a0ead3266bf945e9a3a725
Hitesh Agja <hiteshagja24@gmail.com>
2022-02-13 23:53:03 +0530

    Improvement in navigateBackToRootDrawer method in CustomActions

--- src/libs/Navigation/CustomActions.js
+++ src/libs/Navigation/CustomActions.js
@@ -7,39 +7,23 @@ import linkingConfig from './linkingConfig';
 import navigationRef from './navigationRef';
 
 /**
+ * @returns {Object}
+ */
+function getActiveState() {
+    // We use our RootState as the dispatch's state is relative to the active navigator and might not contain our active screen.
+    return navigationRef.current.getRootState();
+}
+
+/**
  * Go back to the Main Drawer
  * @param {Object} navigationRef
  */
 function navigateBackToRootDrawer() {
-    let isLeavingNestedDrawerNavigator = false;
-
-    // This should take us to the first view of the modal's stack navigator
-    navigationRef.current.dispatch((state) => {
-        // If this is a nested drawer navigator then we pop the screen and
-        // prevent calling goBack() as it's default behavior is to toggle open the active drawer
-        if (state.type === 'drawer') {
-            isLeavingNestedDrawerNavigator = true;
-            return StackActions.pop();
-        }
-
-        // If there are multiple routes then we can pop back to the first route
-        if (state.routes.length > 1) {
-            return StackActions.popToTop();
-        }
-
-        // Otherwise, we are already on the last page of a modal so just do nothing here as goBack() will navigate us
-        // back to the screen we were on before we opened the modal.
-        return StackActions.pop(0);
+    const activeState = getActiveState();
+    navigationRef.current.dispatch({
+        ...StackActions.popToTop(),
+        target: activeState.key,
     });
-
-    if (isLeavingNestedDrawerNavigator) {
-        return;
-    }
-
-    // Navigate back to where we were before we launched the modal
-    if (navigationRef.current.canGoBack()) {
-        navigationRef.current.goBack();
-    }
 }
 
 /**
