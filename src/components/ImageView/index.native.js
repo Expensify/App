aedcaafedfd55c867830d48c96a9fdfffabaf8d6
AHMED SHERIF <a.s1171111@gmail.com>
2022-02-14 21:18:02 +0200

    Update src/components/ImageView/index.native.js
    Co-authored-by: Rushat Gabhane <rushatgabhane@gmail.com>

--- src/components/ImageView/index.native.js
+++ src/components/ImageView/index.native.js
@@ -66,7 +66,7 @@ class ImageView extends PureComponent {
             let imageHeight = height;
             const aspectRatio = (imageHeight / imageWidth);
 
-            //  Fit the image to windowWidth
+            // Fit the image to windowWidth
             imageWidth = Math.round(this.props.windowWidth);
             imageHeight = Math.round(imageWidth * aspectRatio);
 
