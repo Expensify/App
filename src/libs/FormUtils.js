303ee05ec8ab3daf39e3bef36f2b4bc8e065574e
Carlos Martins <cmartins@expensify.com>
2022-02-15 13:02:54 -0300

    update getInputIDPropTypes docs

--- src/libs/FormUtils.js
+++ src/libs/FormUtils.js
@@ -1,5 +1,5 @@
 /**
- * Gets the prop type for inputID
+ * Custom prop validator that enforces inputID as a required string if isFormInput is true
  *
  * @param {Object} props - props passed to the input component
  * @returns {Object} returns an Error object if isFormInput is supplied but inputID is falsey or not a string
@@ -9,7 +9,7 @@ function getInputIDPropTypes(props) {
         return;
     }
     if (!props.inputID) {
-        return new Error('InputID is required if isFormInput prop is supplied.');
+        return new Error('InputID is required if isFormInput is true');
     }
     if (typeof props.inputID !== 'string') {
         return new Error(`Invalid prop type ${typeof props.inputID} supplied to inputID. Expecting string.`);
