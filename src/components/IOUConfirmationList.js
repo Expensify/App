438fa77644ecfe3659c32a44f79713603e0a5699
Rory Abraham <rory@expensify.com>
2022-02-15 15:22:10 -0800

    Get rid of unnecessary arrow function in render

--- src/components/IOUConfirmationList.js
+++ src/components/IOUConfirmationList.js
@@ -142,6 +142,7 @@ class IOUConfirmationList extends Component {
         this.toggleOption = this.toggleOption.bind(this);
         this.confirm = this.confirm.bind(this);
         this.scrollToIndex = this.scrollToIndex.bind(this);
+        this.maybeToggleIndex = this.maybeToggleIndex.bind(this);
     }
 
     componentDidMount() {
@@ -367,6 +368,21 @@ class IOUConfirmationList extends Component {
         }
     }
 
+    /**
+     * @param {Number} index
+     */
+    maybeToggleIndex(index) {
+        if (!this.allOptions[index]) {
+            return;
+        }
+
+        if (!this.props.hasMultipleParticipants) {
+            return;
+        }
+
+        this.toggleOption(this.allOptions[index]);
+    }
+
     render() {
         const hoverStyle = this.props.hasMultipleParticipants ? styles.hoveredComponentBG : {};
         const toggleOption = this.props.hasMultipleParticipants ? this.toggleOption : undefined;
@@ -378,16 +394,8 @@ class IOUConfirmationList extends Component {
                 initialFocusedIndex={this.allOptions.length}
                 listLength={this.allOptions.length + 1}
                 onFocusedIndexChanged={this.scrollToIndex}
-                onEnterKeyPressed={(focusedIndex) => {
-                    if (focusedIndex === this.allOptions.length) {
-                        return;
-                    }
-
-                    if (this.props.hasMultipleParticipants) {
-                        this.toggleOption(this.allOptions[focusedIndex]);
-                    }
-                }}
-                shouldEnterKeyEventBubble={focusedIndex => focusedIndex === this.allOptions.length}
+                onEnterKeyPressed={this.maybeToggleIndex}
+                shouldEnterKeyEventBubble={focusedIndex => !this.allOptions[focusedIndex]}
             >
                 {({focusedIndex}) => (
                     <>
