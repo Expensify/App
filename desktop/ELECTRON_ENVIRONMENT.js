b8cf68efaa4825913611f7d9180001ff6b1c62f4
Peter Velkov <kidroca@gmail.com>
2022-02-15 22:49:58 +0200

    Fix ELECTRON_ENVIRONMENT.js
    This script is now getting bundled into `main.js` before `package.json` is modified by electron-builder
    But the build process sets the `process.env.ELECTRON_ENV` so it's not a problem to just remove these lines
    
    Note: updated return docs - the returned values are the lowercase names and not ['PROD', 'STG', 'DEV']

--- desktop/ELECTRON_ENVIRONMENT.js
+++ desktop/ELECTRON_ENVIRONMENT.js
@@ -1,19 +1,10 @@
-// This variable is injected into package.json by electron-builder via the extraMetadata field (specified in electronBuilder.config.js)
-// It will be `PROD` on production, `STG` on staging, and `undefined` on dev (because dev doesn't use electron-builder)
-const {electronEnvironment} = require('../package.json');
 const ENVIRONMENT = require('../src/CONST/ENVIRONMENT');
 
 /**
- * @returns {String} – One of ['PROD', 'STG', 'DEV']
+ * @returns {'development'|'staging'|'production'}
  */
 function getEnvironment() {
-    // If we are on dev, then the ELECTRON_ENVIRONMENT environment variable will be present (set in package.json desktop script `npm run desktop`)
-    if (process.env.ELECTRON_ENV === ENVIRONMENT.DEV) {
-        return ENVIRONMENT.DEV;
-    }
-
-    // Otherwise, use the environment injected into package.json by electron-builder
-    return electronEnvironment;
+    return process.env.ELECTRON_ENV;
 }
 
 function isDev() {
