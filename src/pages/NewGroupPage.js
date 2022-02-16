1fb3422023c4f45446e71e8e9ce544a4ef109d78
Rory Abraham <rory@expensify.com>
2022-02-15 17:00:03 -0800

    Make CMD+Enter create a group

--- src/pages/NewGroupPage.js
+++ src/pages/NewGroupPage.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import NewChatPage from './NewChatPage';
+import CONST from '../CONST';
+import KeyboardShortcut from '../libs/KeyboardShortcut';
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-const NewGroupPage = props => <NewChatPage {...props} isGroupChat />;
+class NewGroupPage extends Component {
+    componentDidMount() {
+        const enterConfig = CONST.KEYBOARD_SHORTCUTS.ENTER;
+        const modifiers = KeyboardShortcut.getShortcutModifiers(['CTRL']);
+        this.unsubscribeCTRLEnter = KeyboardShortcut.subscribe(
+            enterConfig.shortcutKey,
+            () => {
+                if (!this.newChatPage) {
+                    return;
+                }
+                this.newChatPage.createGroup();
+            },
+            enterConfig.descriptionKey,
+            modifiers,
+            true,
+        );
+    }
 
-NewGroupPage.displayName = 'NewGroupPage';
+    componentWillUnmount() {
+        if (!this.unsubscribeCTRLEnter) {
+            return;
+        }
+        this.unsubscribeCTRLEnter();
+    }
+
+    render() {
+        return (
+            <NewChatPage
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...this.props}
+                isGroupChat
+                ref={el => this.newChatPage = el}
+            />
+        );
+    }
+}
 
 export default NewGroupPage;
