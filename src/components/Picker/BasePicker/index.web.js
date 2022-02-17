addc22a10f5b3000dd51412df96184c4ca896cb6
LucioChavezFuentes <luciobertinchavez@gmail.com>
2022-02-17 17:10:35 -0600

    update comment about scroll to Picker on Web

--- src/components/Picker/BasePicker/index.web.js
+++ src/components/Picker/BasePicker/index.web.js
@@ -26,10 +26,11 @@ class BasePicker extends React.Component {
             return;
         }
 
-        // ! If RNPickerSelect pickerProps returns the <select> tag's ref, the implementation of this file's Web version would be no longer neccesary,
-        // ! check for any update on https://github.com/lawnstarter/react-native-picker-select or https://github.com/react-native-picker/picker
-        // View ref will return a <div>, apply 'focus()' on it will not scroll to it.
-        // Applying 'scrollIntoView(false)' will scroll to element.
+        // ! Picker from https://github.com/react-native-picker/picker is being used inside RNPickerSelect. Picker 'ref' prop does not work on Web
+        // ! therefore it can not get '<select>' ref that Picker renders, so a pull request https://github.com/react-native-picker/picker/pull/376
+        // ! has been made with a potential fix to this issue , if Picker returns a 'ref' the implementation to this file would not be neccesary.
+        // An alternative is getting a View container ref. View ref will return a <div>,
+        // apply 'focus()' on it will not scroll to it. Applying 'scrollIntoView(false)' will scroll to element. https://developer.mozilla.org/es/docs/Web/API/Element/scrollIntoView
         const originalFocus = viewRef.focus;
         viewRef.focus = function () {
             // Execute original focus() using apply on ref context to prevent infinite focus() call stack and preserve original focus() functionality
