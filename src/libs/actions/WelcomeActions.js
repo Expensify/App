68ff8d070ba9ca8d2320236f6f44d1404ea9bed0
Carlos Martins <cmartins@expensify.com>
2022-02-22 18:03:46 -0300

    move function inside timeout

--- src/libs/actions/WelcomeActions.js
+++ src/libs/actions/WelcomeActions.js
@@ -48,34 +48,34 @@ Onyx.connect({
  * @param {Function} params.toggleCreateMenu
  */
 function show({routes, toggleCreateMenu}) {
-    if (!isFirstTimeNewExpensifyUser) {
-        return;
-    }
+    // NOTE: This setTimeout is required due to a bug in react-navigation where modals do not display properly in a drawerContent
+    // This is a short-term workaround, see this issue for updates on a long-term solution: https://github.com/Expensify/App/issues/5296
+    setTimeout(() => {
+        if (!isFirstTimeNewExpensifyUser) {
+            return;
+        }
 
-    // Set the NVP back to false so we don't automatically run welcome actions again
-    NameValuePair.set(CONST.NVP.IS_FIRST_TIME_NEW_EXPENSIFY_USER, false, ONYXKEYS.NVP_IS_FIRST_TIME_NEW_EXPENSIFY_USER);
+        // Set the NVP back to false so we don't automatically run welcome actions again
+        NameValuePair.set(CONST.NVP.IS_FIRST_TIME_NEW_EXPENSIFY_USER, false, ONYXKEYS.NVP_IS_FIRST_TIME_NEW_EXPENSIFY_USER);
 
-    // We want to display the Workspace chat first since that means a user is already in a Workspace and doesn't need to create another one
-    const workspaceChatReport = _.find(allReports, report => ReportUtils.isPolicyExpenseChat(report));
-    if (workspaceChatReport) {
-        Navigation.navigate(ROUTES.getReportRoute(workspaceChatReport.reportID));
-        return;
-    }
+        // We want to display the Workspace chat first since that means a user is already in a Workspace and doesn't need to create another one
+        const workspaceChatReport = _.find(allReports, report => ReportUtils.isPolicyExpenseChat(report));
+        if (workspaceChatReport) {
+            Navigation.navigate(ROUTES.getReportRoute(workspaceChatReport.reportID));
+            return;
+        }
 
-    // If we are rendering the SidebarScreen at the same time as a workspace route that means we've already created a workspace via workspace/new and should not open the global
-    // create menu right now.
-    const topRouteName = lodashGet(_.last(routes), 'name', '');
-    const isDisplayingWorkspaceRoute = topRouteName.toLowerCase().includes('workspace');
+        // If we are rendering the SidebarScreen at the same time as a workspace route that means we've already created a workspace via workspace/new and should not open the global
+        // create menu right now.
+        const topRouteName = lodashGet(_.last(routes), 'name', '');
+        const isDisplayingWorkspaceRoute = topRouteName.toLowerCase().includes('workspace');
 
-    // It's also possible that we already have a workspace policy. In either case we will not toggle the menu but do still want to set the NVP in this case
-    // since the user does not need to create a workspace.
-    if (!Policy.isAdminOfFreePolicy(allPolicies) && !isDisplayingWorkspaceRoute) {
-        // NOTE: This setTimeout is required due to a bug in react-navigation where modals do not display properly in a drawerContent
-        // This is a short-term workaround, see this issue for updates on a long-term solution: https://github.com/Expensify/App/issues/5296
-        setTimeout(() => {
+        // It's also possible that we already have a workspace policy. In either case we will not toggle the menu but do still want to set the NVP in this case
+        // since the user does not need to create a workspace.
+        if (!Policy.isAdminOfFreePolicy(allPolicies) && !isDisplayingWorkspaceRoute) {
             toggleCreateMenu();
-        }, 1500);
-    }
+        }
+    }, 1500);
 }
 
 export {
