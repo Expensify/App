20715e26e89b11d8f9541bc2981488e7eb3db607
Rory Abraham <rory@expensify.com>
2022-02-22 16:23:39 -0800

    Simplify component to only include the one hoverable element

--- src/components/ArchivedReportFooter.js
+++ src/components/ArchivedReportFooter.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Pressable, View} from 'react-native';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from './Icon';
 import * as Expensicons from './Icon/Expensicons';
+import Hoverable from './Hoverable';
 import Text from './Text';
-import Tooltip from './Tooltip';
 import styles from '../styles/styles';
 import * as StyleUtils from '../styles/StyleUtils';
 import getButtonState from '../libs/getButtonState';
@@ -12,32 +12,27 @@ import withLocalize, {withLocalizePropTypes} from './withLocalize';
 
 const propTypes = {
     /** The reason this report was archived. */
-    archivedReason: PropTypes.string.isRequired,
+    archiveReason: PropTypes.string.isRequired,
 
     ...withLocalizePropTypes,
 };
 
 const ArchivedReportFooter = (props) => {
-    // const getTranslationKeyForArchivedReason = () => {};
-    // const message = props.translate(getTranslationKeyForArchivedReason(props.archivedReason));
+    // const getTranslationKeyForArchiveReason = () => {};
+    // const message = props.translate(getTranslationKeyForArchiveReason(props.archiveReason));
     const message = props.translate('reportActionCompose.roomIsArchived');
     return (
-        <View style={[styles.p5, styles.hoveredComponentBG]}>
-            <View style={[styles.flexRow]}>
-                {/* TODO: translate archivedReason */}
-                <Text>{message}</Text>
-                <Tooltip text="TODO: get translated message for archived chat info icon">
-                    <Pressable onPress={() => { /* TODO: What does clicking the ! icon do? */ }}>
-                        {({pressed, hovered}) => (
-                            <Icon
-                                src={Expensicons.Exclamation}
-                                fill={StyleUtils.getIconFillColor(getButtonState(hovered, pressed))}
-                            />
-                        )}
-                    </Pressable>
-                </Tooltip>
-            </View>
-        </View>
+        <Hoverable containerStyles={[styles.flexRow, styles.p5]}>
+            {isHovered => (
+                <View style={isHovered ? styles.activeComponentBG : styles.hoveredComponentBG}>
+                    <Text>{message}</Text>
+                    <Icon
+                        src={Expensicons.Exclamation}
+                        fill={StyleUtils.getIconFillColor(getButtonState(isHovered))}
+                    />
+                </View>
+            )}
+        </Hoverable>
     );
 };
 
