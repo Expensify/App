94edab2420a166abd32a175703b70de1ccbf8101
Rory Abraham <rory@expensify.com>
2022-02-24 14:24:01 -0800

    Get rid of NumberUtils library

--- src/libs/PropTypeUtils.js
+++ src/libs/PropTypeUtils.js
@@ -1,5 +1,3 @@
-import * as NumberUtils from './NumberUtils';
-
 function integerPropType(props, propName, componentName) {
     const propValue = props[propName];
     if (!Number.isInteger(propValue)) {
@@ -9,7 +7,7 @@ function integerPropType(props, propName, componentName) {
 
 function wholeNumberPropType(props, propName, componentName) {
     const propValue = props[propName];
-    if (!NumberUtils.isWholeNumber(propValue)) {
+    if (!Number.isInteger(propValue) || propValue < 0) {
         return new Error(`Invalid prop ${propName} supplied to ${componentName}. Must be a whole number, but got ${propValue} instead`);
     }
 }
