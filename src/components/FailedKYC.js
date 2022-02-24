ffd9b60f1aa670c13f9181ff435c0636682c31e1
Kosuke Tseng <ctseng@expensify.com>
2022-02-23 22:40:48 -0800

    fix lint

--- src/components/FailedKYC.js
+++ src/components/FailedKYC.js
@@ -3,23 +3,24 @@ import {
     View,
 } from 'react-native';
 import Text from './Text';
-import PropTypes from 'prop-types';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 import styles from '../styles/styles';
 
+const propTypes = {
+    ...withLocalizePropTypes,
+};
 
-class FailedKYC extends React.Component {
-    render() {
-        return (
-            <View style={[styles.flex1]}>
-                <View style={[styles.ph5]}>
-                    <Text style={styles.mb3}>
-                        {this.props.translate('additionalDetailsStep.failedKYCText')}
-                    </Text>
-                </View>
+const FailedKYC = (props) => (
+        <View style={[styles.flex1]}>
+            <View style={[styles.ph5]}>
+                <Text style={styles.mb3}>
+                    {props.translate('additionalDetailsStep.failedKYCText')}
+                </Text>
             </View>
-        );
-    }
-}
+        </View>
+);
+
+FailedKYC.propTypes = propTypes;
+FailedKYC.displayName = 'FailedKYC';
 
 export default withLocalize(FailedKYC);
