0a84faec5222aa49b1eaf03cd5db49351186f939
Marco Chávez <marco@expensify.com>
2022-02-25 14:31:10 -0600

    remove size from SubscriptAvatar

--- src/components/SubscriptAvatar.js
+++ src/components/SubscriptAvatar.js
@@ -17,44 +17,34 @@ const propTypes = {
 
     /** Default icon if the subscript image is not set */
     defaultSubscriptIcon: PropTypes.func.isRequired,
-
-    /** Set the sie of avatars */
-    size: PropTypes.oneOf(['default', 'large']),
-};
-
-const defaultProps = {
-    size: 'default',
 };
 
 const SubscriptAvatar = props => (
-    <View style={props.size === 'large' ? {} : styles.emptyAvatar}>
-        <View style={props.size === 'large' ? [styles.secondAvatarHovered, styles.rightSideLargeAvatar] : []}>
+    <View style={styles.emptyAvatar}>
+        <View>
             <Tooltip text={props.avatarTooltips[0]} absolute>
                 <Avatar
                     source={props.avatarImageURLs[0]}
-                    imageStyles={props.size === 'large' ? [styles.avatarLarge] : []}
                 />
             </Tooltip>
         </View>
         <View
-            style={
-                props.size === 'large' ? styles.secondAvatarLarge : [styles.secondAvatarSubscript, styles.secondAvatarHovered]
-            }
+            style={[styles.secondAvatarSubscript, styles.secondAvatarHovered]}
         >
             <Tooltip text={props.avatarTooltips[1]} absolute>
                 { props.avatarImageURLs[1] === ''
                     ? (
                         <Icon
                             src={props.defaultSubscriptIcon()}
-                            height={props.size === 'large' ? variables.avatarSizeLarge : variables.iconSizeNormal}
-                            width={props.size === 'large' ? variables.avatarSizeLarge : variables.iconSizeNormal}
+                            height={variables.iconSizeNormal}
+                            width={variables.iconSizeNormal}
                             style={styles.singleSubscript}
                             fill={themedefault.iconSuccessFill}
                         />
                     ) : (
                         <Avatar
                             source={props.avatarImageURLs[1]}
-                            imageStyles={[props.size === 'large' ? styles.avatarLarge : styles.singleSubscript]}
+                            imageStyles={[styles.singleSubscript]}
                         />
                     )}
             </Tooltip>
@@ -62,6 +52,5 @@ const SubscriptAvatar = props => (
     </View>
 );
 
-SubscriptAvatar.defaultProps = defaultProps;
 SubscriptAvatar.propTypes = propTypes;
 export default memo(SubscriptAvatar);
