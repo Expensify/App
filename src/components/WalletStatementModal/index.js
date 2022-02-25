c0249d7e0c3c1a429962c17f2766d376608e48af
Nicholas Murray <nmurray@expensify.com>
2022-02-25 10:44:36 -0800

    Refactor to class component with isLoading state

--- src/components/WalletStatementModal/index.js
+++ src/components/WalletStatementModal/index.js
@@ -5,21 +5,45 @@ import compose from '../../libs/compose';
 import withLocalize from '../withLocalize';
 import ONYXKEYS from '../../ONYXKEYS';
 import {walletStatementPropTypes, walletStatementDefaultProps} from './WalletStatementModalPropTypes';
+import {ActivityIndicator} from 'react-native';
+import styles from '../../styles/styles';
+import themeColors from '../../styles/themes/default';
 
-const WalletStatementModal = (props) => {
-    const authToken = lodashGet(props, 'session.authToken', null);
-    return (
-        <iframe
-            src={`${props.statementPageURL}&authToken=${authToken}`}
-            title="Statements"
-            height="100%"
-            width="100%"
-            seamless="seamless"
-            frameBorder="0"
-            onLoad={() => console.log("content loaded")}
-        />
-    );
-};
+class WalletStatementModal extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true,
+        };
+    }
+
+    render() {
+        const authToken = lodashGet(this.props, 'session.authToken', null);
+        return (
+            <>
+                {this.state.isLoading && (
+                    <ActivityIndicator
+                        size="large"
+                        style={[styles.flex1]}
+                        color={themeColors.textSupporting}
+                    />
+                 )}
+                {!this.state.isLoading && (
+                    <iframe
+                        src={`${this.props.statementPageURL}&authToken=${authToken}`}
+                        title="Statements"
+                        height="100%"
+                        width="100%"
+                        seamless="seamless"
+                        frameBorder="0"
+                        onLoad={() => this.setState({isLoading: false})}
+                    />
+                )}
+            </>
+        );
+    }
+}
 
 WalletStatementModal.propTypes = walletStatementPropTypes;
 WalletStatementModal.defaultProps = walletStatementDefaultProps;
