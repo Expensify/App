d5790567c5e539d6073e63ddef7e3c80f105c845
Nicholas Murray <nmurray@expensify.com>
2022-02-25 12:56:20 -0800

    Add class component for wallet statement

--- src/components/WalletStatementModal/index.native.js
+++ src/components/WalletStatementModal/index.native.js
@@ -6,22 +6,48 @@ import withLocalize from '../withLocalize';
 import ONYXKEYS from '../../ONYXKEYS';
 import compose from '../../libs/compose';
 import {walletStatementPropTypes, walletStatementDefaultProps} from './WalletStatementModalPropTypes';
+import {ActivityIndicator, View} from 'react-native';
+import styles from '../../styles/styles';
+import themeColors from '../../styles/themes/default';
 
-const WalletStatementModal = (props) => {
-    const authToken = lodashGet(props, 'session.authToken', null);
-    return (
-        <WebView
-            originWhitelist={['https://*']}
-            source={{
-                uri: props.statementPageURL,
-                headers: {
-                    Cookie: `authToken=${authToken}`,
-                },
-            }}
-            incognito // 'incognito' prop required for Android, issue here https://github.com/react-native-webview/react-native-webview/issues/1352
-        />
-    );
-};
+class WalletStatementModal extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: false,
+        };
+    }
+
+    render() {
+        const authToken = lodashGet(this.props, 'session.authToken', null);
+        return (
+            <>
+                {this.state.isLoading && (
+                    <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
+                        <ActivityIndicator
+                            color={themeColors.spinner}
+                            size="large"
+                        />
+                    </View>
+                )}
+                <View style={[styles.flex1]}>
+                    <WebView
+                        originWhitelist={['https://*']}
+                        source={{
+                            uri: this.props.statementPageURL,
+                            headers: {
+                                Cookie: `authToken=${authToken}`,
+                            },
+                        }}
+                        incognito // 'incognito' prop required for Android, issue here https://github.com/react-native-webview/react-native-webview/issues/1352
+                        onLoad={() => this.setState({isLoading: false})}
+                    />
+                </View>
+            </>
+        );
+    }
+}
 
 WalletStatementModal.propTypes = walletStatementPropTypes;
 WalletStatementModal.defaultProps = walletStatementDefaultProps;
