d07b33cfffc2d8708457f4e312e8085272b80cf6
Kosuke Tseng <ctseng@expensify.com>
2022-03-01 15:55:01 -0800

    fix paths

--- src/pages/EnablePayments/FailedKYC.js
+++ src/pages/EnablePayments/FailedKYC.js
@@ -2,11 +2,11 @@ import React from 'react';
 import {
     View,
 } from 'react-native';
-import CONST from '../CONST';
-import Text from './Text';
-import TextLink from './TextLink';
-import withLocalize, {withLocalizePropTypes} from './withLocalize';
-import styles from '../styles/styles';
+import CONST from '../../CONST';
+import Text from '../../components/Text';
+import TextLink from '../../components/TextLink';
+import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
+import styles from '../../styles/styles';
 
 const propTypes = {
     ...withLocalizePropTypes,
