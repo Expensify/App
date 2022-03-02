906e969816761e4ed9dfb477fbd14e092ce20cbc
Rory Abraham <rory@expensify.com>
2022-03-01 17:14:54 -0800

    Use reusable Banner component in ArchivedReportFooter

--- src/components/ArchivedReportFooter.js
+++ src/components/ArchivedReportFooter.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import CONST from '../CONST';
-import Icon from './Icon';
-import * as Expensicons from './Icon/Expensicons';
-import Hoverable from './Hoverable';
-import RenderHTML from './RenderHTML';
-import Text from './Text';
-import styles from '../styles/styles';
-import * as StyleUtils from '../styles/StyleUtils';
-import getButtonState from '../libs/getButtonState';
+import Banner from './Banner';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 
 const propTypes = {
@@ -23,33 +15,12 @@ const defaultProps = {
     archiveReason: CONST.REPORT.ARCHIVE_REASON.DEFAULT,
 };
 
+// TODO: pass displayName, policyName, and other parameters to translate()
 const ArchivedReportFooter = props => (
-    <Hoverable>
-        {isHovered => (
-            <View style={[
-                styles.flexRow,
-                styles.alignItemsCenter,
-                styles.p5,
-                styles.borderRadiusNormal,
-                isHovered ? styles.activeComponentBG : styles.hoveredComponentBG,
-            ]}
-            >
-                <View style={[styles.mr3]}>
-                    <Icon
-                        src={Expensicons.Exclamation}
-                        fill={StyleUtils.getIconFillColor(getButtonState(isHovered))}
-                    />
-                </View>
-                {
-                    props.archiveReason === CONST.REPORT.ARCHIVE_REASON.DEFAULT
-                        ? <Text>{props.translate(`reportArchiveReasons.${props.archiveReason}`)}</Text>
-
-                        // TODO: pass displayName, policyName, and other parameters to translate()
-                        : <RenderHTML html={props.translate(`reportArchiveReasons.${props.archiveReason}`)} />
-                }
-            </View>
-        )}
-    </Hoverable>
+    <Banner
+        text={props.translate(`reportArchiveReasons.${props.archiveReason}`)}
+        shouldRenderHTML={props.archiveReason === CONST.REPORT.ARCHIVE_REASON.DEFAULT}
+    />
 );
 
 ArchivedReportFooter.propTypes = propTypes;
