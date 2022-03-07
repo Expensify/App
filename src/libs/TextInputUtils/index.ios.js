a6f8698b6d1df1d1f856dd2907877616f14075aa
Katsuki <1313124+K4tsuki@users.noreply.github.com>
2022-03-07 23:58:28 +0700

    set ios settimeout

--- src/libs/TextInputUtils/index.ios.js
+++ src/libs/TextInputUtils/index.ios.js
@@ -6,15 +6,12 @@
 * @param {Object} selection: {start: number, end: number}
 */
 function setTextAndSelection(textInputElement, {text, selection}) {
+    textInputElement.setNativeProps({text});
+
     // In IOS setNativeProps({text}) (line above) will change selection of textInputElement. Add selection properties in parameter will result in unexpected behaviour.
     // To get the expected behaviour need to split setNativeProps for text and selection
-    // Let previous setNativeProps finish and then set selection properly
-    new Promise(() => {
-        textInputElement.setNativeProps({text});
-    }).then(() => {
-        // This line used to prevent cursor/selection coming back to previous selection set by line above.
-        textInputElement.setNativeProps({selection});
-    });
+    // Let previous setNativeProps finish first and then set selection properly
+    setTimeout(() => textInputElement.setNativeProps({selection}), 0);
 }
 
 export default {
