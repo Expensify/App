c32a5a04e5ecd8f31db9bdc92564bb7fd3597d47
Manan Jadhav <manan.jadhav@gmail.com>
2022-03-09 00:07:17 +0530

    fix: changed width for native devices

--- src/components/ChatGhostUI/ContentLoadingElements/ContentLoadingOneLine/index.native.js
+++ src/components/ChatGhostUI/ContentLoadingElements/ContentLoadingOneLine/index.native.js
@@ -9,7 +9,7 @@ const ContentLoadingOneLine = () => (
     >
         <Circle cx="50" cy="26" r="20" />
         <Rect x="90" y="11" width="20%" height="8" />
-        <Rect x="90" y="31" width="90%" height="8" />
+        <Rect x="90" y="31" width="75%" height="8" />
     </ContentLoader>
 );
 
