56007296b10e51f751bbd3df38e01b2f5dec921e
Marco Chávez <marco@expensify.com>
2022-03-12 16:02:07 -0600

    display icons in Storybook

--- src/stories/SubscriptAvatar.stories.js
+++ src/stories/SubscriptAvatar.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SubscriptAvatar from '../components/SubscriptAvatar';
+import * as Expensicons from '../components/Icon/Expensicons';
 
 /**
  * We use the Component Story Format for writing stories. Follow the docs here:
@@ -9,7 +10,10 @@ import SubscriptAvatar from '../components/SubscriptAvatar';
 export default {
     title: 'Components/SubscriptAvatar',
     component: SubscriptAvatar,
-    args: {},
+    args: {
+        mainAvatar: Expensicons.Profile,
+        secondaryAvatar: Expensicons.Workspace,
+    },
 };
 
 // eslint-disable-next-line react/jsx-props-no-spreading
