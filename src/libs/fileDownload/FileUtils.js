f23b626da4822982dd0ca557f1b5c0344ca45d88
Manan Jadhav <manan.jadhav@gmail.com>
2022-03-14 12:55:51 +0530

    fix: addd file type handling

--- src/libs/fileDownload/FileUtils.js
+++ src/libs/fileDownload/FileUtils.js
@@ -1,5 +1,7 @@
 import {Alert} from 'react-native';
 import moment from 'moment';
+import * as mime from 'react-native-mime-types';
+import CONST from '../../CONST';
 
 /**
  * Re useable alert function
@@ -19,14 +21,35 @@ function showAlert(content) {
  * @param {String} url
  * @returns {String}
  */
-export default function getAttachmentName(url) {
+function getAttachmentName(url) {
     if (!url) {
         return '';
     }
     return `${moment().format('DDMMYYYYHHmmss')}.${url.split(/[#?]/)[0].split('.').pop().trim()}`;
 }
 
+/**
+ * Returns file type based on the filename/uri
+ * @param {String} fileName
+ * @returns {String}
+ */
+
+function getFileType(fileName) {
+    if (!fileName) {
+        return;
+    }
+    const contentType = mime.contentType(fileName);
+    if (contentType.startsWith('image')) {
+        return CONST.ATTACHMENT_FILE_TYPE.IMAGE;
+    }
+    if (contentType.startsWith('video')) {
+        return CONST.ATTACHMENT_FILE_TYPE.VIDEO;
+    }
+    return CONST.ATTACHMENT_FILE_TYPE.FILE;
+}
+
 export {
     showAlert,
     getAttachmentName,
+    getFileType,
 };
