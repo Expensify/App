60188588faa89e6cce7c68d637cce3cab0e22fa7
mateusbra <suehtamzarb@gmail.com>
2022-03-18 19:43:56 -0300

    Change comment

--- src/components/AnchorForCommentsOnly/anchorForCommentsOnlyPropTypes.js
+++ src/components/AnchorForCommentsOnly/anchorForCommentsOnlyPropTypes.js
@@ -21,7 +21,7 @@ const propTypes = {
     /** Any children to display */
     children: PropTypes.node,
 
-    /** Display label in case of attachments. Text to display in case of anchors */
+    /** Filename in case of attachments, anchor text in case of URLs or emails. */
     displayName: PropTypes.string,
 
     /** Any additional styles to apply */
