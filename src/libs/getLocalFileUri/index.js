383d55806ba6596c56dd88475ca9bfa1b0f8fbd5
Barun Pandey <barunn.pandey@gmail.com>
2022-03-20 07:12:19 +0545

    Add @param in JSDoc for localFileUri

--- src/libs/getLocalFileUri/index.js
+++ src/libs/getLocalFileUri/index.js
@@ -1,5 +1,6 @@
 /**
  * Returns the URI string. For non-ios devices, we don't need to change anything
+ *  @param {bool} localFileUri which is fileUri coming in by default - from react native picker
  * @return {String}
  */
 
