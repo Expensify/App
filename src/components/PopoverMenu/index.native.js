ada317766f2cc3f3243e80386dda185915ec72c7
Manan Jadhav <manan.jadhav@gmail.com>
2022-03-21 21:11:18 +0530

    fix: removed keyboard flag from native popover

--- src/components/PopoverMenu/index.native.js
+++ src/components/PopoverMenu/index.native.js
@@ -28,7 +28,6 @@ class PopoverMenu extends Component {
             <BasePopoverMenu
                 // eslint-disable-next-line react/jsx-props-no-spreading
                 {...this.props}
-                allowKeyboardNavigation={false}
                 onMenuHide={this.onMenuHide}
                 onItemSelected={item => this.selectItem(item)}
             />
