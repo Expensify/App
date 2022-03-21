dea92cd3208ce9c94c1dcd3a0eb3543ffc33a62f
Md Neyaz Ahmad <mdneyazahmad66@gmail.com>
2022-03-21 18:36:07 +0530

    fix: add PureComponent to ReportScreen

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -90,7 +90,7 @@ function getReportID(route) {
     return Number.parseInt(params.reportID, 10);
 }
 
-class ReportScreen extends React.Component {
+class ReportScreen extends React.PureComponent {
     constructor(props) {
         super(props);
 
