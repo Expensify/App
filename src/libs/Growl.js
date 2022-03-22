191c02f6ca1281ae0979ad99792b492efd7927ea
Neil Marcellini <neil@expensify.com>
2022-03-22 10:58:13 -0700

    Retry growls when the ref is not available

--- src/libs/Growl.js
+++ src/libs/Growl.js
@@ -4,13 +4,17 @@ import CONST from '../CONST';
 const growlRef = React.createRef();
 
 /**
- * Show the growl notification
+ * Show the growl notification. If the ref is not available, retry until it is.
  *
  * @param {String} bodyText
  * @param {String} type
  * @param {Number} [duration]
 */
 function show(bodyText, type, duration = CONST.GROWL.DURATION) {
+    if (!growlRef.current) {
+        setTimeout(() => show(bodyText, type, duration), 100);
+        return;
+    }
     growlRef.current.show(bodyText, type, duration);
 }
 
