32a94f03d26666863a5769b41b19af974fb6c800
Tim Golen <tgolen@expensify.com>
2022-03-25 05:20:47 -0600

    Correct comment reference

--- src/libs/API/commands/BaseAPICommandBlocking.js
+++ src/libs/API/commands/BaseAPICommandBlocking.js
@@ -5,14 +5,14 @@ export default class extends BaseAPICommand {
      * An abstract method to define what should happen before the network request is made
      */
     startBlocking() {
-        throw new Error('This abstract method needs defined in the class extending BaseAPICommand');
+        throw new Error('This abstract method needs defined in the class extending BaseAPICommandBlocking');
     }
 
     /**
      * An abstract method to define what should happen after the network request is made
      */
     finishBlocking() {
-        throw new Error('This abstract method needs defined in the class extending BaseAPICommand');
+        throw new Error('This abstract method needs defined in the class extending BaseAPICommandBlocking');
     }
 
     /**
