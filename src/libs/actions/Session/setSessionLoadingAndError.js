618b34d19c1471966a04a9753d2992f6260fd859
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-03-25 14:15:03 -1000

    remove setSessionError

--- src/libs/actions/Session/setSessionLoadingAndError.js
+++
@@ -1,9 +0,0 @@
-import Onyx from 'react-native-onyx';
-import ONYXKEYS from '../../../ONYXKEYS';
-
-/**
- * @param {String} error
- */
-export default function setSessionError(error) {
-    Onyx.merge(ONYXKEYS.SESSION, {error});
-}
