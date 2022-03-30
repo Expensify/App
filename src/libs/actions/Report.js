6f27b7ab290218128b4084a33e1f8d5addb8c878
Amal Nazeem <amal@expensify.com>
2022-03-30 14:51:12 -0400

    Make case for archived rooms

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -155,9 +155,18 @@ function getChatReportName(fullReport, chatType) {
             : '')}`;
     }
 
-    // For a basic policy room or a Policy Expense chat, return its original name
-    if (ReportUtils.isUserCreatedPolicyRoom({chatType}) || ReportUtils.isPolicyExpenseChat({chatType})) {
+    // For a basic policy room, return its original name
+    if (ReportUtils.isUserCreatedPolicyRoom({chatType})) {
         return LoginUtils.getEmailWithoutMergedAccountPrefix(fullReport.reportName);
+    }
+
+    // For a Policy Expense Chat, return its original name and append (archived)' if it is archived
+    if (ReportUtils.isPolicyExpenseChat({chatType})) {
+        let name = LoginUtils.getEmailWithoutMergedAccountPrefix(fullReport.reportName);
+        if (ReportUtils.isArchivedRoom({stateNum: fullReport.state, statusNum: fullReport.status})) {
+            name += " (archived)";
+        }
+        return name;
     }
 
     const {sharedReportList} = fullReport;
