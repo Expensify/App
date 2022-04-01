d63976079f331eb3fe9d0f276be22186ec028f92
Tim Golen <tgolen@expensify.com>
2022-04-01 10:21:20 -0600

    Add initial code

---
+++ src/libs/DataChanged.js
@@ -0,0 +1,23 @@
+const callbacksByAPICommandName = {};
+
+function publish(apiCommandName, data) {
+    const callbacksToTrigger = callbacksByAPICommandName[apiCommandName];
+    if (!callbacksToTrigger) {
+        return;
+    }
+    for (let i = 0; i < callbacksToTrigger.length; i++) {
+        callbacksToTrigger[i](data);
+    }
+}
+
+function subscribe(apiCommandName, callback) {
+    if (!callbacksByAPICommandName[apiCommandName]) {
+        callbacksByAPICommandName[apiCommandName] = [];
+    }
+    callbacksByAPICommandName[apiCommandName].push(callback);
+}
+
+export {
+    publish,
+    subscribe,
+};
