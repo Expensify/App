696b7d5dccb627df9a5f98d2658fb53f468089d3
Md Neyaz Ahmad <mdneyazahmad66@gmail.com>
2022-04-01 15:35:30 +0530

    test: refactor names

--- tests/unit/CurrencySymbolUtilsTest.js
+++ tests/unit/CurrencySymbolUtilsTest.js
@@ -3,7 +3,6 @@ import * as CurrencySymbolUtils from '../../src/libs/CurrencySymbolUtils';
 import currencyList from './currencyList.json';
 
 const currencyCodeList = _.keys(currencyList);
-
 const AVAILABLE_LOCALES = ['en', 'es'];
 
 // Contains item [isLeft, locale, currencyCode]
@@ -14,18 +13,17 @@ const symbolPositions = [
 
 describe('CurrencySymbolUtils', () => {
     describe('getLocalizedCurrencySymbol', () => {
-        test.each(AVAILABLE_LOCALES)('returns non empty string for all currencyCode with preferredLocale %s', (prefrredLocale) => {
+        test.each(AVAILABLE_LOCALES)('Returns non empty string for all currencyCode with preferredLocale %s', (prefrredLocale) => {
             _.forEach(currencyCodeList, (currencyCode) => {
                 const localizedSymbol = CurrencySymbolUtils.getLocalizedCurrencySymbol(prefrredLocale, currencyCode);
 
-                expect(typeof localizedSymbol).toBe('string');
-                expect(localizedSymbol.length).toBeGreaterThan(0);
+                expect(localizedSymbol).toBeTruthy();
             });
         });
     });
 
     describe('isCurrencySymbolLTR', () => {
-        test.each(symbolPositions)('returns %s for prefrredLocale %s and currencyCode %s', (isLeft, locale, currencyCode) => {
+        test.each(symbolPositions)('Returns %s for prefrredLocale %s and currencyCode %s', (isLeft, locale, currencyCode) => {
             const isSymbolLeft = CurrencySymbolUtils.isCurrencySymbolLTR(locale, currencyCode);
             expect(isSymbolLeft).toBe(isLeft);
         });
