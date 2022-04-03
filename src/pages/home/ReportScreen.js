634ff43d5e0725b77982e1b660665c79e112a98e
Md Neyaz Ahmad <mdneyazahmad66@gmail.com>
2022-04-03 20:34:20 +0530

    fix: remove pure component

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -90,7 +90,7 @@ function getReportID(route) {
     return Number.parseInt(params.reportID, 10);
 }
 
-class ReportScreen extends React.PureComponent {
+class ReportScreen extends React.Component {
     constructor(props) {
         super(props);
 
