e7404de235c38a914b5ba779ec89497152355da3
Marco Chávez <marco@expensify.com>
2022-04-06 19:48:57 -0500

    pass personalDetails for getReportIcons for ReportActionItemCreated

--- src/pages/home/report/ReportActionItemCreated.js
+++ src/pages/home/report/ReportActionItemCreated.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import ONYXKEYS from '../../../ONYXKEYS';
 import RoomHeaderAvatars from '../../../components/RoomHeaderAvatars';
 import ReportWelcomeText from '../../../components/ReportWelcomeText';
+import participantPropTypes from '../../../components/participantPropTypes';
 import * as ReportUtils from '../../../libs/reportUtils';
 import styles from '../../../styles/styles';
 import * as OptionsListUtils from '../../../libs/OptionsListUtils';
@@ -17,15 +18,20 @@ const propTypes = {
 
         /** Whether the user is not an admin of policyExpenseChat chat */
         isOwnPolicyExpenseChat: PropTypes.bool,
+
     }),
+
+    /** Personal details of all the users */
+    personalDetails: PropTypes.objectOf(participantPropTypes),
 };
 const defaultProps = {
     report: {},
+    personalDetails: {},
 };
 
 const ReportActionItemCreated = (props) => {
     const isPolicyExpenseChat = ReportUtils.isPolicyExpenseChat(props.report);
-    const avatarIcons = OptionsListUtils.getReportIcons(props.report);
+    const avatarIcons = OptionsListUtils.getReportIcons(props.report, props.personalDetails);
     return (
         <View style={[
             styles.chatContent,
@@ -51,5 +57,8 @@ ReportActionItemCreated.displayName = 'ReportActionItemCreated';
 export default withOnyx({
     report: {
         key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT}${reportID}`,
+    },
+    personalDetails: {
+        key: ONYXKEYS.PERSONAL_DETAILS,
     },
 })(ReportActionItemCreated);
