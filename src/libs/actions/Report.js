efe4f0c7f1aa36d06d373905371704380003140c
Brandon Stites <bstites+1@expensify.com>
2022-04-07 13:58:05 -0600

    Fix docs

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1254,7 +1254,7 @@ function deleteReportComment(reportID, reportAction) {
  *
  * @param {Number} reportID
  * @param {Number} [sequenceNumber] This can be used to set the last read actionID to a specific
- * @param {Boolean} manuallyMarked If the user manually marked this as unread, we need to tell the API
+ * @param {Boolean} [manuallyMarked] If the user manually marked this as unread, we need to tell the API
  *  spot (eg. mark-as-unread). Otherwise, when this param is omitted, the highest sequence number becomes the one that
  *  is last read (meaning that the entire report history has been read)
  */
