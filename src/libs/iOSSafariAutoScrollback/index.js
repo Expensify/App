5f2691668762a3278a4c147abe16ec379551fc8e
Rory Abraham <rory@expensify.com>
2022-04-08 14:39:29 -0700

    Revert iOS Safari auto scrollback

--- src/libs/iOSSafariAutoScrollback/index.js
+++
@@ -1,3 +0,0 @@
-/* The autoScrollBack address Mobile Safari-specific issues when the user overscrolls the window while the keyboard is visible */
-/* It has no effect to other platforms */
-export default function () { }
