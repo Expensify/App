db7f3436a8e77b2a2aa7f6ca1beb37c2a72ea892
eVoloshchak <copyreading@gmail.com>
2022-04-09 10:20:56 +0300

    Update comment

--- src/components/RoomNameInput.js
+++ src/components/RoomNameInput.js
@@ -96,7 +96,7 @@ class RoomNameInput extends Component {
                 prefixCharacter={CONST.POLICY.ROOM_PREFIX}
                 placeholder={this.props.translate('newRoomPage.social')}
                 onChange={this.setModifiedRoomName}
-                defaultValue={this.props.initialValue.substring(1)} // Since room name always starts with a prefix, we omit the first character
+                defaultValue={this.props.initialValue.substring(1)} // Since the room name always starts with a prefix, we omit the first character to avoid displaying it twice.
                 errorText={this.props.errorText}
                 autoCapitalize="none"
             />
