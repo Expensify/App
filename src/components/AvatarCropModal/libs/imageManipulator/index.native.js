680b8fc8722330f6aaa06d99d7407afc0fa1b370
Vitalii Tabakharniuk <44551710+Tabakharniuk@users.noreply.github.com>
2022-04-10 18:39:47 +0200

    Organize imports

--- src/components/AvatarCropModal/libs/imageManipulator/index.native.js
+++ src/components/AvatarCropModal/libs/imageManipulator/index.native.js
@@ -1,9 +1,9 @@
 import RNImageManipulator from '@oguzhnatly/react-native-image-manipulator';
 import RNFetchBlob from 'rn-fetch-blob';
 
-function imageManipulator(uri, actions, a) {
+function imageManipulator(uri, actions, options) {
     return new Promise((resolve) => {
-        RNImageManipulator.manipulate(uri, actions, a).then((result) => {
+        RNImageManipulator.manipulate(uri, actions, options).then((result) => {
             RNFetchBlob.fs.stat(result.uri.replace('file://', '')).then(({size}) => {
                 resolve({...result, size});
             });
