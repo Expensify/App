9b766793f3b6ee7f3bfd331a5357c78a05ed5c47
mateusbra <suehtamzarb@gmail.com>
2022-04-13 17:58:54 -0300

    change comment

--- src/libs/VirtualKeyboard/index.js
+++ src/libs/VirtualKeyboard/index.js
@@ -15,7 +15,7 @@ function isOpen() {
 
 /**
  * As of January 2022, the VirtualKeyboard web API is not available in all browsers yet
- * If it is unavailable, we default to assuming that the virtual keyboard is open mobile devices.
+ * If it is unavailable, we default to assuming that the virtual keyboard is open on mobile devices.
  * See https://github.com/Expensify/App/issues/6767 for additional context.
  *
  * @returns {Boolean}
