e248f7a640dfdd54a858048388e60a5a2edf961c
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-04-13 11:33:33 -1000

    add some comment about why the custom error class exists

--- src/libs/Errors/HttpsError.js
+++ src/libs/Errors/HttpsError.js
@@ -1,3 +1,6 @@
+/**
+ * Custom error class useful for re-throwing fetch errors with status code or valid error responses with status 200 but non 200 jsonCode
+ */
 export default class HttpsError extends Error {
     constructor({
         message,
