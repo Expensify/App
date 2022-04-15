4158a3bef7c1ff08fec19c2c5f47c0dda593fe00
Amal Nazeem <amal@expensify.com>
2022-04-15 12:56:23 -0400

    DRY variables

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -482,7 +482,7 @@ function getOptions(reports, personalDetails, activeReportID, {
         // Save the report in the map if this is a single participant so we can associate the reportID with the
         // personal detail option later. Individuals should not be associated with single participant
         // policyExpenseChats or chatRooms since those are not people.
-        if (logins.length <= 1 && !ReportUtils.isPolicyExpenseChat(report) && !ReportUtils.isChatRoom(report)) {
+        if (logins.length <= 1 && !isPolicyExpenseChat && !isChatRoom) {
             reportMapForLogins[logins[0]] = report;
         }
         const isSearchingSomeonesPolicyExpenseChat = !report.isOwnPolicyExpenseChat && searchValue !== '';
