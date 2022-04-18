1e1c600a9f48a9ce24a461b9a9c225bf93543484
Rory Abraham <rory@expensify.com>
2022-04-18 09:41:01 -0700

    Create general-purpose scoped EventBus

---
+++ src/libs/EventBus.js
@@ -0,0 +1,45 @@
+import _ from 'underscore';
+import EventEmitter from 'events';
+
+/**
+ * A general-purpose set of event emitters.
+ */
+export default class EventBus {
+    /**
+     * @param {String} name – the name of the event scope
+     * @param {Array<String>} events – all the event names within this event bus
+     */
+    constructor(name, events) {
+        this.name = name;
+        this.events = events;
+        this.eventEmitters = _.reduce(events, (memo, event) => ({...memo, [event]: new EventEmitter()}), {});
+    }
+
+    /**
+     * Trigger an event.
+     *
+     * @param {String} eventName – the event to trigger
+     * @param {*} [eventData] – arbitrary arguments to pass to event handler(s) for this event
+     */
+    emit(eventName, ...eventData) {
+        const emitter = this.eventEmitters[eventName];
+        if (!emitter) {
+            throw new Error(`The ${eventName} network event does not exist in the ${this.name} EventBus`);
+        }
+        emitter.emit(eventName, ...eventData);
+    }
+
+    /**
+     * Setup a handler for an event.
+     *
+     * @param {String} eventName
+     * @param {Function} callback
+     */
+    on(eventName, callback) {
+        const emitter = this.eventEmitters[eventName];
+        if (!emitter) {
+            throw new Error(`The ${eventName} network event does not exist in the ${this.name} EventBus`);
+        }
+        emitter.on(eventName, callback);
+    }
+}
