50caa1310299acfe19f429b61f6fefbf95b2edb3
Yevhenii Voloshchak <copyreading@gmail.com>
2022-04-25 11:51:58 +0300

    Strip all non-numeric characters in companyTaxID on form submission

--- src/pages/ReimbursementAccount/CompanyStep.js
+++ src/pages/ReimbursementAccount/CompanyStep.js
@@ -176,7 +176,7 @@ class CompanyStep extends React.Component {
         }
 
         const incorporationDate = moment(this.state.incorporationDate).format(CONST.DATE.MOMENT_FORMAT_STRING);
-        BankAccounts.setupWithdrawalAccount({...this.state, incorporationDate});
+        BankAccounts.setupWithdrawalAccount({...this.state, incorporationDate, companyTaxID: this.state.companyTaxID.replace(/\D/g,'')});
     }
 
     render() {
