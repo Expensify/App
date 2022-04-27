bfed4accd4a478f7630adf7bc0722f92203283b9
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-04-27 07:01:34 -1000

    update propTypes

--- src/components/networkPropTypes.js
+++ src/components/networkPropTypes.js
@@ -3,4 +3,7 @@ import PropTypes from 'prop-types';
 export default PropTypes.shape({
     /** Is the network currently offline or not */
     isOffline: PropTypes.bool,
+
+    /** Whether we should fail all network requests */
+    shouldFailAllRequests: PropTypes.bool,
 });
