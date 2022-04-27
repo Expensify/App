17e974e8aa6f24d34bd2276a3be582057287dd06
Rajat Parashar <parasharrajat@users.noreply.github.com>
2022-04-27 23:27:49 +0530

    Add PressOnEnter story

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import {View} from 'react-native';
 import Button from '../components/Button';
+import Text from '../components/Text';
 
 /**
  * We use the Component Story Format for writing stories. Follow the docs here:
@@ -18,6 +20,23 @@ const Template = args => <Button {...args} />;
 // See: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Default = Template.bind({});
 const Loading = Template.bind({});
+const PressOnEnter = (props) => {
+    const [text, setText] = useState('');
+    const onPress = useCallback(() => {
+        setText('Button Pressed!');
+        setTimeout(() => setText(''), 500);
+    });
+    return (
+        <Button
+        // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+        // eslint-disable-next-line react/prop-types
+            text={text || props.text}
+            onPress={onPress}
+        />
+    );
+};
+
 Default.args = {
     text: 'Save & Continue',
     success: true,
@@ -28,8 +47,15 @@ Loading.args = {
     success: true,
 };
 
+PressOnEnter.args = {
+    text: 'Press Enter',
+    pressOnEnter: true,
+    success: true,
+};
+
 export default story;
 export {
     Default,
     Loading,
+    PressOnEnter,
 };
