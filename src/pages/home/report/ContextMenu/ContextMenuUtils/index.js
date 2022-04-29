3fa69cbc3dfc9c1a72fa7e5db3e66c9d1d57a817
Yevhenii Voloshchak <copyreading@gmail.com>
2022-04-29 11:04:17 +0300

    Fix typo

--- src/pages/home/report/ContextMenu/ContextMenuUtils/index.js
+++ src/pages/home/report/ContextMenu/ContextMenuUtils/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
 /**
- * The popover description will be an empty string on anything but mobile web 
+ * The popover description will be an empty string on anything but mobile web
  * because we show link in tooltip instead of popover on web
  *
  * @param {String} selection
