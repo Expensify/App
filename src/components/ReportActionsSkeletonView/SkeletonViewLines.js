d49b3cb9fd5571dfee2884da37eeff7011c6e8a2
Manan Jadhav <manan.jadhav@gmail.com>
2022-05-03 00:32:40 +0530

    feat: updated component structure for skeleton views

---
+++ src/components/ReportActionsSkeletonView/SkeletonViewLines.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Rect, Circle} from 'react-native-svg';
+import SkeletonViewContentLoader from 'react-content-loader';
+
+const propTypes = {
+    numberOfRows: PropTypes.number.isRequired,
+};
+
+const SkeletonViewLines = (props) => {
+    const renderOneLine = () => props.numberOfRows;
+
+    return (
+        <SkeletonViewContentLoader
+            height={renderOneLine()}
+        >
+            <Circle cx="50" cy="26" r="20" />
+            <Rect x="90" y="11" width="20%" height="8" />
+            <Rect x="90" y="31" width="90%" height="8" />
+        </SkeletonViewContentLoader>
+    );
+};
+
+SkeletonViewLines.displayName = 'SkeletonViewLines';
+SkeletonViewLines.propTypes = propTypes;
+export default SkeletonViewLines;
+
+// <ContentLoadingWrapper
+//         height={100}
+//     >
+//         <Circle cx="50" cy="26" r="20" />
+//         <Rect x="90" y="11" width="20%" height="8" />
+//         <Rect x="90" y="31" width="90%" height="8" />
+//         <Rect x="90" y="51" width="90%" height="8" />
+//         <Rect x="90" y="71" width="50%" height="8" />
+//     </ContentLoadingWrapper>
+
+//     <ContentLoadingWrapper
+//         height={80}
+//     >
+//         <Circle cx="50" cy="26" r="20" />
+//         <Rect x="90" y="11" width="20%" height="8" />
+//         <Rect x="90" y="31" width="75%" height="8" />
+//         <Rect x="90" y="51" width="50%" height="8" />
+//     </ContentLoadingWrapper>
