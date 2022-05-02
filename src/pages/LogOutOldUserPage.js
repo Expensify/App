dbf3610afa6547018b810e6176efe47a2ae77940
Neil Marcellini <neil@expensify.com>
2022-05-02 15:38:25 -0700

    Revert "Navigate in transition page so navigation is ready"
    This reverts commit d1800a56659a62b3cb6c93c2fb68be4aa4e9fbb1.

--- src/pages/LogOutOldUserPage.js
+++ src/pages/LogOutOldUserPage.js
@@ -6,8 +6,6 @@ import ONYXKEYS from '../ONYXKEYS';
 import * as Session from '../libs/actions/Session';
 import FullScreenLoadingIndicator from '../components/FullscreenLoadingIndicator';
 import Log from '../libs/Log';
-import Navigation from '../libs/Navigation/Navigation';
-import ROUTES from '../ROUTES';
 
 const propTypes = {
     /** The parameters needed to authenticate with a short lived token are in the URL */
@@ -54,21 +52,7 @@ class LogOutOldUserPage extends Component {
         if (this.props.session && this.props.session.email !== email) {
             Log.info('[LogOutOldUserPage] Different user signed in - signing out');
             Session.signOutAndRedirectToSignIn();
-            return;
         }
-
-        // exitTo is URI encoded because it could contain a variable number of slashes (i.e. "workspace/new" vs "workspace/<ID>/card")
-        const exitTo = decodeURIComponent(lodashGet(this.props.route.params, 'exitTo', ''));
-        if (exitTo === ROUTES.WORKSPACE_NEW) {
-            // New workspace creation is handled in AuthScreens, not in its own screen
-            Log.info('[LoginWithShortLivedTokenPage] exitTo is workspace/new - handling new workspace creation in AuthScreens');
-            return;
-        }
-
-        // We must call dismissModal() to remove the /transition route from history
-        Log.info('[LoginWithShortLivedTokenPage] Dismissing LoginWithShortLivedTokenPage and navigating to exitTo');
-        Navigation.dismissModal();
-        Navigation.navigate(exitTo);
     }
 
     render() {
