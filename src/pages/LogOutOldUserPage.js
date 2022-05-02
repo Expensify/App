9ac316a7d8a6e6b96cd9e24414071ffd97481873
Neil Marcellini <neil@expensify.com>
2022-05-02 15:04:25 -0700

    Simplify the comment about dismissing the modal

--- src/pages/LogOutOldUserPage.js
+++ src/pages/LogOutOldUserPage.js
@@ -65,9 +65,7 @@ class LogOutOldUserPage extends Component {
             return;
         }
 
-        // In order to navigate to a modal, we first have to dismiss the current modal. Without dismissing the current modal, if the user cancels out of the workspace modal,
-        // then they will be routed back to /transition/<accountID>/<email>/<authToken>/workspace/<policyID>/card and we don't want that. We want them to go back to `/`
-        // and by calling dismissModal(), the /transition/... route is removed from history so the user will get taken to `/` if they cancel out of the new workspace modal.
+        // We must call dismissModal() to remove the /transition route from history
         Log.info('[LoginWithShortLivedTokenPage] Dismissing LoginWithShortLivedTokenPage and navigating to exitTo');
         Navigation.dismissModal();
         Navigation.navigate(exitTo);
