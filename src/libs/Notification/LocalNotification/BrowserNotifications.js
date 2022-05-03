d3e1840a3bfa61599062442f7aae3e007e3893cf
Carlos Martins <cmartins@expensify.com>
2022-05-03 17:44:18 +0200

    rename Update to AppUpdate

--- src/libs/Notification/LocalNotification/BrowserNotifications.js
+++ src/libs/Notification/LocalNotification/BrowserNotifications.js
@@ -2,7 +2,7 @@
 import _ from 'underscore';
 import Str from 'expensify-common/lib/str';
 import focusApp from './focusApp';
-import * as Update from '../../actions/Update';
+import * as AppUpdate from '../../actions/AppUpdate';
 import EXPENSIFY_ICON_URL from '../../../../assets/images/expensify-logo-round-clearspace.png';
 
 const DEFAULT_DELAY = 4000;
@@ -128,7 +128,7 @@ export default {
             body: 'A new version of this app is available!',
             delay: 0,
             onClick: () => {
-                Update.triggerUpdateAvailable();
+                AppUpdate.triggerUpdateAvailable();
             },
         });
     },
