41efea383e798812dba51f257ca949858ca1388a
Tushu17 <tusharshrivastav1711@gmail.com>
2022-05-04 01:10:43 +0530

    remove view

--- src/components/Tooltip/index.native.js
+++ src/components/Tooltip/index.native.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import {View} from 'react-native';
 import PropTypes from 'prop-types';
 
 // We can't use the common component for the Tooltip as Web implementation uses DOM specific method to
 // render the View which is not present on the Mobile.
 const propTypes = {
-    /** Styles to be assigned to the Tooltip wrapper views */
-    containerStyles: PropTypes.arrayOf(PropTypes.object),
-
     /** Children to wrap with Tooltip. */
     children: PropTypes.node.isRequired,
-};
-
-const defaultProps = {
-    containerStyles: [],
 };
 
 /**
@@ -21,12 +12,10 @@ const defaultProps = {
  * @returns {ReactNodeLike}
  */
 const Tooltip = props => (
-    <View style={props.containerStyles}>
-        {props.children}
-    </View>
+    props.children
 );
 
 Tooltip.propTypes = propTypes;
-Tooltip.defaultProps = defaultProps;
 Tooltip.displayName = 'Tooltip';
+
 export default Tooltip;
