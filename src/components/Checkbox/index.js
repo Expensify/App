9e908cd8ef666a4d7b66e8f499eea5490157d288
Md Neyaz Ahmad <mdneyazahmad66@gmail.com>
2022-05-04 19:51:52 +0530

    fix: linting errors

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Pressable} from 'react-native';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../../styles/styles';
 import Icon from '../Icon';
