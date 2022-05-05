0a9db4ad4862cbb69a312c3dc1211a0ab44f149a
Carlos Martins <cmartins@expensify.com>
2022-05-05 23:37:53 +0200

    rm FormUtils.validateInputIDProps

--- src/components/Picker/index.js
+++ src/components/Picker/index.js
@@ -24,13 +24,8 @@ const propTypes = {
     /** Customize the Picker container */
     containerStyles: PropTypes.arrayOf(PropTypes.object),
 
-    /**
-     * The ID used to uniquely identify the input
-     *
-     * @param {Object} props - props passed to the input
-     * @returns {Object} - returns an Error object if isFormInput is supplied but inputID is falsey or not a string
-     */
-    inputID: props => FormUtils.validateInputIDProps(props),
+    /** The ID used to uniquely identify the input in a Form */
+    inputID: PropTypes.string,
 
     /** Saves a draft of the input value when used in a form */
     shouldSaveDraft: PropTypes.bool,
--- src/components/StatePicker.js
+++ src/components/StatePicker.js
@@ -21,13 +21,8 @@ const propTypes = {
     /** The value that needs to be selected */
     value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 
-    /**
-    * The ID used to uniquely identify the input
-    *
-    * @param {Object} props - props passed to the input
-    * @returns {Object} - returns an Error object if isFormInput is supplied but inputID is falsey or not a string
-    */
-    inputID: props => FormUtils.validateInputIDProps(props),
+    /** The ID used to uniquely identify the input in a Form */
+    inputID: PropTypes.string,
 
     /** Saves a draft of the input value when used in a form */
     shouldSaveDraft: PropTypes.bool,
