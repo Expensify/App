c9cb4159f94eb029a391b381f1032fb72b29c2f0
Mohammad Luthfi Fathur Rahman <luthfi.ufi14@gmail.com>
2022-05-05 10:23:43 +0800

    revert back the getModalStyles and use getShouldAddTopSafeAreaPadding function

--- src/styles/getModalStyles/index.android.js
+++
@@ -1,8 +0,0 @@
-import CONST from '../../CONST';
-import getModalStyles from './getModalStyles';
-
-export default getModalStyles({
-    shouldModalAddTopSafeAreaPadding: {
-        [CONST.MODAL.MODAL_TYPE.RIGHT_DOCKED]: true,
-    },
-});
