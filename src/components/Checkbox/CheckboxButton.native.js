e64cbc9fd058e2cb26713add2fab99e0051d8fe4
Md Neyaz Ahmad <mdneyazahmad66@gmail.com>
2022-05-06 12:42:13 +0530

    fix: error on native checkbox

--- src/components/Checkbox/CheckboxButton.native.js
+++ src/components/Checkbox/CheckboxButton.native.js
@@ -36,7 +36,7 @@ const CheckboxButton = props => (
         disabled={props.disabled}
         onPress={props.onPress}
         ref={props.forwardedRef}
-        style={this.props.style}
+        style={props.style}
     >
         {props.children}
     </Pressable>
