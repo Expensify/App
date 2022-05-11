17e1d3ff970e491f485708db0509520826e8603c
Mohammad Luthfi Fathur Rahman <luthfi.ufi14@gmail.com>
2022-05-11 22:06:47 +0700

    add default value

--- src/libs/getModalStyles/index.android.js
+++ src/libs/getModalStyles/index.android.js
@@ -1,6 +1,6 @@
 import getModalStyles from './getModalStyles';
 
-export default (type, windowDimensions, popoverAnchorPosition, containerStyle) => ({
+export default (type, windowDimensions, popoverAnchorPosition = {}, containerStyle = {}) => ({
     ...getModalStyles(type, windowDimensions, popoverAnchorPosition, containerStyle),
     shouldAddTopSafeAreaPadding: false,
 });
