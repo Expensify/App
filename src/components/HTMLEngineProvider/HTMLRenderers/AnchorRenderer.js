051719a811d93cad800dc3450ce89cd343c89442
Joel Bettner <joelbettner@expensify.com>
2022-05-12 16:39:12 -0600

    Merge pull request #8952 from Expensify/revert-8769-openOldDotLinks
    Revert "Open old dot links with openOldDotLink() function"
    
    (cherry picked from commit 6afed2adc04986c875708651d2e926e7d691c037)

--- src/components/HTMLEngineProvider/HTMLRenderers/AnchorRenderer.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/AnchorRenderer.js
@@ -8,11 +8,9 @@ import htmlRendererPropTypes from './htmlRendererPropTypes';
 import * as HTMLEngineUtils from '../htmlEngineUtils';
 import Text from '../../Text';
 import CONST from '../../../CONST';
-import CONFIG from '../../../CONFIG';
 import styles from '../../../styles/styles';
 import Navigation from '../../../libs/Navigation/Navigation';
 import AnchorForCommentsOnly from '../../AnchorForCommentsOnly';
-import * as Link from '../../../libs/actions/Link';
 
 const AnchorRenderer = (props) => {
     const htmlAttribs = props.tnode.attributes;
@@ -22,29 +20,16 @@ const AnchorRenderer = (props) => {
     const displayName = lodashGet(props.tnode, 'domNode.children[0].data', '');
     const parentStyle = lodashGet(props.tnode, 'parent.styles.nativeTextRet', {});
     const attrHref = htmlAttribs.href || '';
-    const internalNewExpensifyPath = (attrHref.startsWith(CONST.NEW_EXPENSIFY_URL) && attrHref.replace(CONST.NEW_EXPENSIFY_URL, ''))
+    const internalExpensifyPath = (attrHref.startsWith(CONST.NEW_EXPENSIFY_URL) && attrHref.replace(CONST.NEW_EXPENSIFY_URL, ''))
         || (attrHref.startsWith(CONST.STAGING_NEW_EXPENSIFY_URL) && attrHref.replace(CONST.STAGING_NEW_EXPENSIFY_URL, ''));
-    const internalExpensifyPath = attrHref.startsWith(CONFIG.EXPENSIFY.EXPENSIFY_URL) && attrHref.replace(CONFIG.EXPENSIFY.EXPENSIFY_URL, '');
 
     // If we are handling a New Expensify link then we will assume this should be opened by the app internally. This ensures that the links are opened internally via react-navigation
     // instead of in a new tab or with a page refresh (which is the default behavior of an anchor tag)
-    if (internalNewExpensifyPath) {
-        return (
-            <Text
-                style={styles.link}
-                onPress={() => Navigation.navigate(internalNewExpensifyPath)}
-            >
-                <TNodeChildrenRenderer tnode={props.tnode} />
-            </Text>
-        );
-    }
-
-    // If we are handling an old dot Expensify link we need to open it with openOldDotLink() so we can navigate to it with the user already logged in.
     if (internalExpensifyPath) {
         return (
             <Text
                 style={styles.link}
-                onPress={() => Link.openOldDotLink(internalExpensifyPath)}
+                onPress={() => Navigation.navigate(internalExpensifyPath)}
             >
                 <TNodeChildrenRenderer tnode={props.tnode} />
             </Text>
