4669e7331199c830ef8b5f71ef38332793cddeca
Yuwen Memon <yuwen@expensify.com>
2022-05-12 22:18:40 -0700

    Use pusher with encryption

--- src/libs/Pusher/library/index.js
+++ src/libs/Pusher/library/index.js
@@ -2,6 +2,6 @@
  * We use the standard pusher-js module to support pusher on web environments.
  * @see: https://github.com/pusher/pusher-js
  */
-import Pusher from 'pusher-js';
+import Pusher from 'pusher-js/with-encryption';
 
 export default Pusher;
