ffe38ad74110f56546323bacb09b780c2d710cd8
mollfpr <luthfi.ufi14@gmail.com>
2022-05-16 07:44:35 +0700

    Update src/styles/getModalStyles/index.android.js
    Co-authored-by: Santhoshkumar Sellavel <85645967+Santhosh-Sellavel@users.noreply.github.com>

--- src/styles/getModalStyles/index.android.js
+++ src/styles/getModalStyles/index.android.js
@@ -1,6 +1,5 @@
 import getBaseModalStyles from './getBaseModalStyles';
 
-// Overrides the value of shouldAddTopSafeAreaPadding to false
 // Only apply top padding on iOS since it's the only platform using SafeAreaView
 export default (type, windowDimensions, popoverAnchorPosition = {}, containerStyle = {}) => ({
     ...getBaseModalStyles(type, windowDimensions, popoverAnchorPosition, containerStyle),
