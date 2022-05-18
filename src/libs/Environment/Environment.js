9ca9cf636cd0f76ebce67bdc524fe58a03d9c148
chiragsalian <chirag@expensify.com>
2022-05-18 15:24:55 -0700

    build dev url using dynamic port logic

--- src/libs/Environment/Environment.js
+++ src/libs/Environment/Environment.js
@@ -2,9 +2,10 @@ import Config from 'react-native-config';
 import lodashGet from 'lodash/get';
 import CONST from '../../CONST';
 import getEnvironment from './getEnvironment';
+import CONFIG from '../../CONFIG';
 
 const ENVIRONMENT_URLS = {
-    [CONST.ENVIRONMENT.DEV]: CONST.DEV_NEW_EXPENSIFY_URL,
+    [CONST.ENVIRONMENT.DEV]: CONST.DEV_NEW_EXPENSIFY_URL + CONFIG.DEV_PORT,
     [CONST.ENVIRONMENT.STAGING]: CONST.STAGING_NEW_EXPENSIFY_URL,
     [CONST.ENVIRONMENT.PRODUCTION]: CONST.NEW_EXPENSIFY_URL,
 };
