66baf1823dbe17ac30f2c988b9ab7e0f02ee050d
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-05-17 15:55:36 -1000

    remove logs

--- src/libs/EmojiUtils.js
+++ src/libs/EmojiUtils.js
@@ -1,6 +1,7 @@
 import _ from 'underscore';
 import lodashOrderBy from 'lodash/orderBy';
 import moment from 'moment';
+import replaceAll from 'string.prototype.replaceall';
 import CONST from '../CONST';
 import * as User from './actions/User';
 
@@ -77,7 +78,7 @@ function isSingleEmoji(message) {
  * @returns {Boolean}
  */
 function containsOnlyEmojis(message) {
-    const trimmedMessage = message.replace(/ /g, '').replaceAll('\n', '');
+    const trimmedMessage = replaceAll(message.replace(/ /g, ''), '\n', '');
     const match = trimmedMessage.match(CONST.REGEX.EMOJIS);
 
     if (!match) {
