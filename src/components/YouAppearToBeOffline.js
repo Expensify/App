63b5fb4a6632171ce5184a9ec369ebf717f7f90d
Tim Golen <tgolen@expensify.com>
2022-05-19 09:06:05 -0600

    Add new component for appearing to be offline

---
+++ src/components/YouAppearToBeOffline.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {withNetwork} from './OnyxProvider';
+import networkPropTypes from './networkPropTypes';
+import Icon from './Icon';
+import * as Expensicons from './Icon/Expensicons';
+import variables from '../styles/variables';
+import Text from './Text';
+import styles from '../styles/styles';
+import compose from '../libs/compose';
+import withLocalize, {withLocalizePropTypes} from './withLocalize';
+
+const propTypes = {
+    /** Information about the network */
+    network: networkPropTypes.isRequired,
+
+    ...withLocalizePropTypes,
+};
+
+const YouAppearToBeOffline = (props) => {
+    if (!props.network.isOffline) {
+        return null;
+    }
+    return (
+        <>
+            <Icon
+                src={Expensicons.Offline}
+                width={variables.iconSizeExtraSmall}
+                height={variables.iconSizeExtraSmall}
+            />
+            <Text style={[styles.ml2, styles.chatItemComposeSecondaryRowSubText]}>
+                {props.translate('reportActionCompose.youAppearToBeOffline')}
+            </Text>
+        </>
+    );
+};
+
+YouAppearToBeOffline.propTypes = propTypes;
+YouAppearToBeOffline.displayName = 'YouAppearToBeOffline';
+
+export default compose(
+    withLocalize,
+    withNetwork(),
+)(YouAppearToBeOffline);
