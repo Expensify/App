6aa89a000888ac1075ced1fdd518dc7309286911
Tim Golen <tgolen@expensify.com>
2022-05-19 09:24:43 -0600

    Add new component for exceeding comment length

---
+++ src/components/ExceededCommentLength.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CONST from '../CONST';
+import Text from './Text';
+import styles from '../styles/styles';
+
+const propTypes = {
+    /** The current length of the comment */
+    commentLength: PropTypes.number.isRequired,
+};
+
+const ExceededCommentLength = (props) => {
+    const hasExceededMaxCommentLength = props.commentLength > CONST.MAX_COMMENT_LENGTH;
+    if (!hasExceededMaxCommentLength) {
+        return null;
+    }
+
+    return (
+        <Text style={[styles.textMicro, styles.textDanger]}>
+            {`${props.commentLength}/${CONST.MAX_COMMENT_LENGTH}`}
+        </Text>
+    );
+};
+
+ExceededCommentLength.propTypes = propTypes;
+ExceededCommentLength.displayName = 'ExceededCommentLength';
+
+export default ExceededCommentLength;
