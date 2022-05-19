cd6b497f2bee68b3541aa3a3e349368030a48bd5
Neil Marcellini <neil@expensify.com>
2022-05-19 14:57:08 -0700

    Give isComposerFullSize a default prop

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -58,7 +58,7 @@ const propTypes = {
     /** Array of report actions for this report */
     reportActions: PropTypes.objectOf(PropTypes.shape(reportActionPropTypes)),
 
-    isComposerFullSize: PropTypes.bool.isRequired,
+    isComposerFullSize: PropTypes.bool,
 
     /** Beta features list */
     betas: PropTypes.arrayOf(PropTypes.string),
@@ -75,6 +75,7 @@ const defaultProps = {
         maxSequenceNumber: 0,
         hasOutstandingIOU: false,
     },
+    isComposerFullSize: false,
     betas: [],
 };
 
