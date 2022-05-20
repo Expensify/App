511cc68dfc2ab3fd2ca29ea5ca6cbf43770fc5e2
Neil Marcellini <neil@expensify.com>
2022-05-20 11:52:17 -0700

    Don't navigate to the workspace chat on transition

--- src/libs/actions/Welcome.js
+++ src/libs/actions/Welcome.js
@@ -100,19 +100,19 @@ function show({routes, showCreateMenu}) {
         // Set the NVP back to false so we don't automatically run welcome actions again
         NameValuePair.set(CONST.NVP.IS_FIRST_TIME_NEW_EXPENSIFY_USER, false, ONYXKEYS.NVP_IS_FIRST_TIME_NEW_EXPENSIFY_USER);
 
-        // We want to display the Workspace chat first since that means a user is already in a Workspace and doesn't need to create another one
-        const workspaceChatReport = _.find(allReports, report => ReportUtils.isPolicyExpenseChat(report));
-        if (workspaceChatReport) {
-            Navigation.navigate(ROUTES.getReportRoute(workspaceChatReport.reportID));
-            return;
-        }
-
         // If we are rendering the SidebarScreen at the same time as a workspace route that means we've already created a workspace via workspace/new and should not open the global
-        // create menu right now.
+        // create menu right now. We should also stay on the workspace page if that is our destination.
         const topRouteName = lodashGet(_.last(routes), 'name', '');
         const transitionRoute = _.find(routes, route => route.name === SCREENS.TRANSITION);
         const exitingToWorkspaceRoute = lodashGet(transitionRoute, 'params.exitTo', '') === 'workspace/new';
         const isDisplayingWorkspaceRoute = topRouteName.toLowerCase().includes('workspace') || exitingToWorkspaceRoute;
+
+        // We want to display the Workspace chat first since that means a user is already in a Workspace and doesn't need to create another one
+        const workspaceChatReport = _.find(allReports, report => ReportUtils.isPolicyExpenseChat(report));
+        if (workspaceChatReport && !isDisplayingWorkspaceRoute) {
+            Navigation.navigate(ROUTES.getReportRoute(workspaceChatReport.reportID));
+            return;
+        }
 
         // If user is not already an admin of a free policy and we are not navigating them to their workspace or creating a new workspace via workspace/new then
         // we will show the create menu.
