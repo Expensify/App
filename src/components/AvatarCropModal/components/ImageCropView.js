a0be8cd90dade6313ed5ba5cec73566566fa1902
Vitalii Tabakharniuk <44551710+Tabakharniuk@users.noreply.github.com>
2022-05-20 20:33:16 +0200

    Move styles to useAnimatedStyle

--- src/components/AvatarCropModal/components/ImageCropView.js
+++ src/components/AvatarCropModal/components/ImageCropView.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {Image, View} from 'react-native';
 import {PanGestureHandler} from 'react-native-gesture-handler';
-import Animated from 'react-native-reanimated';
+import Animated, {useAnimatedStyle} from 'react-native-reanimated';
 import styles from '../../../styles/styles';
 import Icon from '../../Icon';
 import * as Expensicons from '../../Icon/Expensicons';
@@ -34,27 +34,29 @@ const defaultProps = {
     panGestureEventHandler: () => { },
 };
 
-const ImageCropView = props => (
-    <PanGestureHandler onGestureEvent={props.panGestureEventHandler}>
-        <Animated.View>
-            <View style={[{
-                height: props.containerSize,
-                width: props.containerSize,
-            }, styles.imageCropContainer]}
-            >
-                <AnimatedImage style={props.imageStyle} onLayout={props.onLayout} source={{uri: props.imageUri}} resizeMode="contain" />
-                <View style={[{
-                    width: props.containerSize,
-                    height: props.containerSize,
-                }, styles.l0, styles.b0, styles.pAbsolute]}
-                >
-                    <Icon src={Expensicons.ImageCropMask} width={props.containerSize} height={props.containerSize} />
-                </View>
-            </View>
-        </Animated.View>
-    </PanGestureHandler>
+const ImageCropView = (props) => {
+    const containerStyles = useAnimatedStyle(() => ({
+        height: props.containerSize,
+        width: props.containerSize,
+    }), [props.containerSize]);
 
-);
+    return (
+        <PanGestureHandler onGestureEvent={props.panGestureEventHandler}>
+            <Animated.View>
+                <View style={[containerStyles, styles.imageCropContainer]}>
+                    <AnimatedImage style={props.imageStyle} onLayout={props.onLayout} source={{uri: props.imageUri}} resizeMode="contain" />
+                    <View style={[{
+                        width: props.containerSize,
+                        height: props.containerSize,
+                    }, styles.l0, styles.b0, styles.pAbsolute]}
+                    >
+                        <Icon src={Expensicons.ImageCropMask} width={props.containerSize} height={props.containerSize} />
+                    </View>
+                </View>
+            </Animated.View>
+        </PanGestureHandler>
+    );
+};
 
 ImageCropView.displayName = 'ImageCropView';
 ImageCropView.propTypes = propTypes;
