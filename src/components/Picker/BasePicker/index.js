89a8eab148bc67b0d908d431e68464eb65e7b572
Carlos Martins <cmartins@expensify.com>
2022-05-20 15:49:09 -0600

    rm state, expose setNativeProps, pass correct props

--- src/components/Picker/BasePicker/index.js
+++ src/components/Picker/BasePicker/index.js
@@ -10,17 +10,20 @@ class BasePicker extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            selectedValue: this.props.defaultValue,
-        };
+        this.pickerValue = this.props.defaultValue;
 
         this.updateSelectedValueAndExecuteOnChange = this.updateSelectedValueAndExecuteOnChange.bind(this);
         this.executeOnCloseAndOnBlur = this.executeOnCloseAndOnBlur.bind(this);
+        this.setNativeProps = this.setNativeProps.bind(this);
+    }
+
+    setNativeProps({value}) {
+        this.pickerValue = value;
     }
 
     updateSelectedValueAndExecuteOnChange(value) {
         this.props.onInputChange(value);
-        this.setState({selectedValue: value});
+        this.pickerValue = value;
     }
 
     executeOnCloseAndOnBlur() {
@@ -38,7 +41,7 @@ class BasePicker extends React.Component {
                 style={this.props.size === 'normal' ? basePickerStyles(this.props.disabled, hasError, this.props.focused) : styles.pickerSmall}
                 useNativeAndroidPickerStyle={false}
                 placeholder={this.props.placeholder}
-                value={this.props.value || this.state.selectedValue}
+                value={this.props.value || this.pickerValue}
                 Icon={() => this.props.icon(this.props.size)}
                 disabled={this.props.disabled}
                 fixAndroidTouchableBug
@@ -47,7 +50,16 @@ class BasePicker extends React.Component {
                 pickerProps={{
                     onFocus: this.props.onOpen,
                     onBlur: this.executeOnCloseAndOnBlur,
-                    ref: this.props.innerRef,
+                }}
+                ref={(node) => {
+                    if (!node || !_.isFunction(this.props.innerRef)) {
+                        return;
+                    }
+
+                    this.props.innerRef(node);
+
+                    // eslint-disable-next-line no-param-reassign
+                    node.setNativeProps = this.setNativeProps;
                 }}
             />
         );
