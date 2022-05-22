7a0b368c688705e90d1f24b5fed60ce09ae2533c
David Frenkiel <david.frenkiel@yahoo.com>
2022-05-22 09:50:41 -1000

    feat: added pdf password form for ios/android

--- src/components/PDFView/index.native.js
+++ src/components/PDFView/index.native.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {TouchableWithoutFeedback} from 'react-native';
 import PDF from 'react-native-pdf';
@@ -6,6 +6,7 @@ import styles from '../../styles/styles';
 import * as StyleUtils from '../../styles/StyleUtils';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../withWindowDimensions';
 import FullScreenLoadingIndicator from '../FullscreenLoadingIndicator';
+import PDFPasswordForm from './PDFPasswordForm';
 
 const propTypes = {
     /** URL to full-sized image */
@@ -26,22 +27,72 @@ const defaultProps = {
 /**
  * On the native layer, we use a pdf library to display PDFs
  *
- * @param props
+ * @param {Object} props
  * @returns {JSX.Element}
  */
+const PDFView = (props) => {
+    const [requestPassword, setRequestPassword] = useState(false);
+    const [passwordInvalid, setPasswordInvalid] = useState(false);
+    const [password, setPassword] = useState('');
 
-const PDFView = props => (
-    <TouchableWithoutFeedback style={[styles.flex1, props.style]}>
-        <PDF
-            activityIndicator={<FullScreenLoadingIndicator />}
-            source={{uri: props.sourceURL}}
-            style={[
-                styles.imageModalPDF,
-                StyleUtils.getWidthAndHeightStyle(props.windowWidth, props.windowHeight),
-            ]}
-        />
-    </TouchableWithoutFeedback>
-);
+    /**
+     * The react-native-pdf/PDF calls this handler when a password is required,
+     * or if the specified password is invalid.
+     *
+     * The message is "Password required or incorrect password." Note that the message
+     * doesn't specify whether the password is simply empty or rather invalid.
+     *
+     * @param {Object} error
+     */
+    const onError = (error) => {
+        if (!error.message.match(/password/i)) {
+            return;
+        }
+
+        setRequestPassword(true);
+
+        // The error message provided by react-native-pdf doesn't indicate whether this
+        // is an initial password request or if the password is invalid. So we just assume
+        // that if a password was already entered then it's an invalid password error.
+        if (password) {
+            setPasswordInvalid(true);
+        }
+    };
+
+    /**
+     * Handler called by PDFPasswordForm when the password form is submitted.
+     *
+     * @param {string} submittedPassword Password submitted in PDFPasswordForm
+     */
+    const onPasswordFormSubmit = (submittedPassword) => {
+        setPassword(submittedPassword);
+        setRequestPassword(false);
+    };
+
+    return (
+        <TouchableWithoutFeedback style={[styles.flex1, props.style]}>
+
+            {requestPassword ? (
+                <PDFPasswordForm
+                    onSubmit={onPasswordFormSubmit}
+                    passwordInvalid={passwordInvalid}
+                />
+            ) : (
+                <PDF
+                    activityIndicator={<FullScreenLoadingIndicator />}
+                    source={{uri: props.sourceURL}}
+                    style={[
+                        styles.imageModalPDF,
+                        StyleUtils.getWidthAndHeightStyle(props.windowWidth, props.windowHeight),
+                    ]}
+                    onError={error => onError(error)}
+                    password={password}
+                />
+            )}
+
+        </TouchableWithoutFeedback>
+    );
+};
 
 PDFView.propTypes = propTypes;
 PDFView.defaultProps = defaultProps;
