11a2151aee7ecda2f56457c7e18eb951d619c7cf
David Frenkiel <david.frenkiel@yahoo.com>
2022-05-22 14:02:49 -1000

    feat: pdf password form - converted native PDFView to pure component

--- src/components/PDFView/index.native.js
+++ src/components/PDFView/index.native.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import {TouchableWithoutFeedback} from 'react-native';
+import {TouchableWithoutFeedback, View} from 'react-native';
 import PDF from 'react-native-pdf';
 import styles from '../../styles/styles';
 import * as StyleUtils from '../../styles/StyleUtils';
@@ -25,15 +25,33 @@ const defaultProps = {
 };
 
 /**
- * On the native layer, we use a pdf library to display PDFs
+ * On the native layer, we use a pdf library to display PDFs. If a PDF is
+ * password-protected we render a PDFPasswordForm to request a password
+ * from the user.
  *
- * @param {Object} props
- * @returns {JSX.Element}
+ * In order to render things nicely during a password challenge we need
+ * to keep track of a bunch of additional state. In particular, the
+ * react-native-pdf/PDF component is both conditionally rendered and hidden
+ * depending upon the situation. It needs to be rerendered on each password
+ * submission because it doesn't dynamically handle updates to it's
+ * password property. And we need to hide it during password challenges
+ * so that PDFPasswordForm doesn't bounce when react-native-pdf/PDF
+ * is (temporarily) rendered.
  */
-const PDFView = (props) => {
-    const [requestPassword, setRequestPassword] = useState(false);
-    const [passwordInvalid, setPasswordInvalid] = useState(false);
-    const [password, setPassword] = useState('');
+class PDFView extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            requestPassword: false,
+            passwordInvalid: false,
+            password: '',
+            attemptPdfLoad: true,
+            pdfLoaded: false,
+        };
+        this.onError = this.onError.bind(this);
+        this.onPasswordFormSubmit = this.onPasswordFormSubmit.bind(this);
+        this.onLoadComplete = this.onLoadComplete.bind(this);
+    }
 
     /**
      * The react-native-pdf/PDF calls this handler when a password is required,
@@ -44,55 +62,76 @@ const PDFView = (props) => {
      *
      * @param {Object} error
      */
-    const onError = (error) => {
+    onError(error) {
         if (!error.message.match(/password/i)) {
             return;
         }
 
-        setRequestPassword(true);
+        // Render password form, and don't render PDF.
+        this.setState({requestPassword: true, attemptPdfLoad: false});
 
         // The error message provided by react-native-pdf doesn't indicate whether this
         // is an initial password request or if the password is invalid. So we just assume
         // that if a password was already entered then it's an invalid password error.
-        if (password) {
-            setPasswordInvalid(true);
+        if (this.state.password) {
+            this.setState({passwordInvalid: true});
         }
-    };
+    }
 
     /**
      * Handler called by PDFPasswordForm when the password form is submitted.
      *
      * @param {string} submittedPassword Password submitted in PDFPasswordForm
      */
-    const onPasswordFormSubmit = (submittedPassword) => {
-        setPassword(submittedPassword);
-        setRequestPassword(false);
-    };
+    onPasswordFormSubmit(submittedPassword) {
+        // Render PDF in invisible state. It's invisible because at this
+        // point in the password challenge process pdfLoaded is false.
+        this.setState({password: submittedPassword, attemptPdfLoad: true});
+    }
 
-    return (
-        <TouchableWithoutFeedback style={[styles.flex1, props.style]}>
+    onLoadComplete() {
+        // Don't render PDFPasswordForm, and do render PDF in visible state.
+        this.setState({requestPassword: false, pdfLoaded: true});
+    }
 
-            {requestPassword ? (
-                <PDFPasswordForm
-                    onSubmit={onPasswordFormSubmit}
-                    passwordInvalid={passwordInvalid}
-                />
-            ) : (
-                <PDF
-                    activityIndicator={<FullScreenLoadingIndicator />}
-                    source={{uri: props.sourceURL}}
-                    style={[
-                        styles.imageModalPDF,
-                        StyleUtils.getWidthAndHeightStyle(props.windowWidth, props.windowHeight),
-                    ]}
-                    onError={error => onError(error)}
-                    password={password}
-                />
-            )}
+    render() {
+        const pdfStyles = [
+            styles.imageModalPDF,
+            StyleUtils.getWidthAndHeightStyle(this.props.windowWidth, this.props.windowHeight),
+        ];
 
-        </TouchableWithoutFeedback>
-    );
-};
+        if (!this.state.pdfLoaded) {
+            pdfStyles.push(styles.invisible);
+        }
+
+        return (
+            <TouchableWithoutFeedback style={[styles.flex1, this.props.style]}>
+
+                <View>
+
+                    {this.state.attemptPdfLoad && (
+                        <PDF
+                            activityIndicator={<FullScreenLoadingIndicator />}
+                            source={{uri: this.props.sourceURL}}
+                            style={pdfStyles}
+                            onError={error => this.onError(error)}
+                            password={this.state.password}
+                            onLoadComplete={this.onLoadComplete}
+                        />
+                    )}
+
+                    {this.state.requestPassword && (
+                        <PDFPasswordForm
+                            onSubmit={this.onPasswordFormSubmit}
+                            passwordInvalid={this.state.passwordInvalid}
+                        />
+                    )}
+
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
 
 PDFView.propTypes = propTypes;
 PDFView.defaultProps = defaultProps;
