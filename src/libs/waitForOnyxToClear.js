ecdbe41e82afca73112d1a550403ecb23544ae4f
Neil Marcellini <neil@expensify.com>
2022-05-23 09:22:53 -0700

    Add JSDoc to waitForOnyxToClear()

--- src/libs/waitForOnyxToClear.js
+++ src/libs/waitForOnyxToClear.js
@@ -3,6 +3,9 @@ import createOnReadyTask from './createOnReadyTask';
 const onyxClearReadyTask = createOnReadyTask();
 onyxClearReadyTask.setIsReady();
 
+/**
+ * @returns {Promise}
+ */
 function waitForOnyxToClear() {
     return onyxClearReadyTask.isReady();
 }
