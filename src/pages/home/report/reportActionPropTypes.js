0e9192dfa9decdceeb6600d851d6deccf867ce42
Luke Donahue <luke@expensify.com>
2022-05-26 13:44:24 -0700

    jsdoc style

--- src/pages/home/report/reportActionPropTypes.js
+++ src/pages/home/report/reportActionPropTypes.js
@@ -3,31 +3,31 @@ import PropTypes from 'prop-types';
 import reportActionFragmentPropTypes from './reportActionFragmentPropTypes';
 
 export default {
-    // Name of the action e.g. ADDCOMMENT
+    /** Name of the action e.g. ADDCOMMENT */
     actionName: PropTypes.string,
 
-    // Person who created the action
+    /** Person who created the action */
     person: PropTypes.arrayOf(reportActionFragmentPropTypes),
 
-    // ID of the report action
+    /** ID of the report action */
     sequenceNumber: PropTypes.number,
 
-    // Unix timestamp
+    /** Unix timestamp */
     timestamp: PropTypes.number,
 
-    // report action message
+    /** report action message */
     message: PropTypes.arrayOf(reportActionFragmentPropTypes),
 
-    // Original message associated with this action
+    /** Original message associated with this action */
     originalMessage: PropTypes.shape({
         // The ID of the iou transaction
         IOUTransactionID: PropTypes.string,
     }),
 
-    // If the reportAction is pending, that means we have not yet sent the Report_AddComment command to the server.
-    // This should most often occur when the client is offline.
+    /** If the reportAction is pending, that means we have not yet sent the Report_AddComment command to the server.
+    This should most often occur when the client is offline. */
     isPending: PropTypes.bool,
 
-    // Error message that's come back from the server.
+    /** Error message that's come back from the server. */
     error: PropTypes.string,
 };
