1edd08e8ef4cb515d4b55bb361878d53364605fa
Luke Donahue <luke@expensify.com>
2022-05-26 17:06:44 -0700

    getting everything lined up

--- src/components/FullPageOfflineView.js
+++ src/components/FullPageOfflineView.js
@@ -22,8 +22,16 @@ const propTypes = {
 const FullPageOfflineView = (props) => {
     if (ONYXKEYS.NETWORK.isOffline) {
         return (
-            <View>
-                <Text>{props.translate('reportActionCompose.youAppearToBeOffline')}</Text>
+            <View
+                style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}
+            >
+                <Icon
+                    src={Expensicons.Offline}
+                    fill={themeColors.offline}
+                    width={30}
+                    height={30}
+                />
+                <Text style={[styles.h1]}>{props.translate('reportActionCompose.youAppearToBeOffline')}</Text>
             </View>
         );
     }
