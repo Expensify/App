db958fc8299f46a876a944b7967e2f75feecbb4a
Luke Donahue <luke@expensify.com>
2022-05-27 16:15:09 -0700

    added args

--- src/stories/FormAlertWithSubmitButton.stories.js
+++ src/stories/FormAlertWithSubmitButton.stories.js
@@ -17,6 +17,14 @@ const Template = args => <FormAlertWithSubmitButton {...args} />;
 // Arguments can be passed to the component by binding
 // See: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Default = Template.bind({});
+Default.args = {
+    isAlertVisible: true,
+    onSubmit: () => {},
+    buttonText: 'Submit',
+    network: {
+        isOffline: true,
+    },
+};
 
 export default story;
 export {
