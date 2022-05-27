abe70faaad04415ff4ac41cf380190b608435ee4
Amal Nazeem <amal@expensify.com>
2022-05-27 13:04:14 -0400

    Use getReportName when building options

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -220,7 +220,8 @@ function createOption(logins, personalDetails, report, {
 }) {
     const isChatRoom = ReportUtils.isChatRoom(report);
     const isPolicyExpenseChat = ReportUtils.isPolicyExpenseChat(report);
-    const personalDetailList = _.values(getPersonalDetailsForLogins(logins, personalDetails));
+    const personalDetailMap = getPersonalDetailsForLogins(logins, personalDetails);
+    const personalDetailList = _.values(personalDetailMap);
     const isArchivedRoom = ReportUtils.isArchivedRoom(report);
     const hasMultipleParticipants = personalDetailList.length > 1 || isChatRoom || isPolicyExpenseChat;
     const personalDetail = personalDetailList[0];
@@ -252,8 +253,7 @@ function createOption(logins, personalDetails, report, {
     let text;
     let alternateText;
     if (isChatRoom || isPolicyExpenseChat) {
-        text = lodashGet(report, 'reportName')
-            || lodashGet(report, 'oldPolicyName', '');
+        text = ReportUtils.getReportName(report, personalDetailMap, policies);
         alternateText = (showChatPreviewLine && !forcePolicyNamePreview && lastMessageText)
             ? lastMessageText
             : subtitle;
