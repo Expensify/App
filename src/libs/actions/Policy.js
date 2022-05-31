78a7bdc603f2bd7f5a84a0bd6c252190158e1963
Neil Marcellini <neil@expensify.com>
2022-05-31 14:15:18 -0700

    Wait for navigation just before navigating
    Policy.getPolicyList() was being called after the previous
    login had been deleted since it wasn't called until the
    navigation was ready.

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -238,6 +238,7 @@ function createAndGetPolicyList() {
             newPolicyID = policyID;
             return getPolicyList();
         })
+        .then(Navigation.isNavigationReady)
         .then(() => {
             Navigation.dismissModal();
             navigateToPolicy(newPolicyID);
