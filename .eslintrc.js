207e26871711b59abf336f4097e92c9bbe14037c
Rory Abraham <rory@expensify.com>
2022-05-30 20:03:22 -0700

    Enable react-native eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     ignorePatterns: ['!.*', 'src/vendor', '.github/actions/**/index.js'],
     env: {
         jest: true,
+        'react-native/react-native': true,
     },
     settings: {
         'import/resolver': {
@@ -22,5 +23,13 @@ module.exports = {
     },
     globals: {
         __DEV__: 'readonly',
+    },
+    plugins: ['react-native'],
+    rules: {
+        'react-native/no-unused-styles': ['error'],
+        'react-native/split-platform-components': ['error'],
+        'react-native/no-inline-styles': ['error'],
+        'react-native/no-color-literals': ['error'],
+        'react-native/no-raw-text': ['error'],
     },
 };
