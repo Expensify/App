65071acbdf0991357793786cc8581e7768829ffc
Neil Marcellini <neil@expensify.com>
2022-06-01 12:29:07 -0700

    Keep input focus on mobile web when expanding

--- src/pages/home/report/ReportActionCompose.js
+++ src/pages/home/report/ReportActionCompose.js
@@ -535,6 +535,7 @@ class ReportActionCompose extends React.Component {
                                                             onPress={(e) => {
                                                                 e.preventDefault();
                                                                 Report.setIsComposerFullSize(this.props.reportID, true);
+                                                                this.focus(false);
                                                             }}
                                                             style={styles.composerSizeButton}
                                                             underlayColor={themeColors.componentBG}
