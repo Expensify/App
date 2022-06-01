6ba0c1d5fb0cd46d21d52d1a88bf83eb589556f1
sobitneupane <073bct543.sobit@pcampus.edu.np>
2022-06-01 21:47:20 +0545

    Syntactic Change

--- src/pages/iou/steps/IOUAmountPage.js
+++ src/pages/iou/steps/IOUAmountPage.js
@@ -218,8 +218,12 @@ class IOUAmountPage extends React.Component {
                 ]}
                 >
                     <TouchableOpacity onPress={() => {
-                        if (this.props.hasMultipleParticipants) { return Navigation.navigate(ROUTES.getIouBillCurrencyRoute(this.props.reportID)); }
-                        if (this.props.iouType === CONST.IOU.IOU_TYPE.SEND) { return Navigation.navigate(ROUTES.getIouSendCurrencyRoute(this.props.reportID)); }
+                        if (this.props.hasMultipleParticipants) {
+                            return Navigation.navigate(ROUTES.getIouBillCurrencyRoute(this.props.reportID));
+                        }
+                        if (this.props.iouType === CONST.IOU.IOU_TYPE.SEND) {
+                            return Navigation.navigate(ROUTES.getIouSendCurrencyRoute(this.props.reportID));
+                        }
                         return Navigation.navigate(ROUTES.getIouRequestCurrencyRoute(this.props.reportID));
                     }}
                     >
