a71f2369d01a425290a3c6a318ac2ff18c132a5e
Vit Horacek <vit@expensify.com>
2022-06-02 12:30:58 +0100

    Update the stylings and finish refactoring

--- src/pages/home/report/ReportTypingIndicator.js
+++ src/pages/home/report/ReportTypingIndicator.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
 import {withOnyx} from 'react-native-onyx';
+import {withNetwork} from '../../../components/OnyxProvider';
+import networkPropTypes from '../../../components/networkPropTypes';
 import compose from '../../../libs/compose';
 import ONYXKEYS from '../../../ONYXKEYS';
 import styles from '../../../styles/styles';
@@ -13,6 +15,9 @@ import TextWithEllipsis from '../../../components/TextWithEllipsis';
 const propTypes = {
     /** Key-value pairs of user logins and whether or not they are typing. Keys are logins. */
     userTypingStatuses: PropTypes.objectOf(PropTypes.bool),
+
+    /** Information about the network */
+    network: networkPropTypes.isRequired,
 
     ...withLocalizePropTypes,
 };
@@ -45,6 +50,11 @@ class ReportTypingIndicator extends React.Component {
 
     render() {
         const numUsersTyping = _.size(this.state.usersTyping);
+
+        // If we are offline, the user typing statuses are not up-to-date so do not show them
+        if (this.props.network.isOffline) {
+            return null;
+        }
 
         // Decide on the Text element that will hold the display based on the number of users that are typing.
         switch (numUsersTyping) {
@@ -84,6 +94,7 @@ ReportTypingIndicator.defaultProps = defaultProps;
 
 export default compose(
     withLocalize,
+    withNetwork(),
     withOnyx({
         userTypingStatuses: {
             key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT_USER_IS_TYPING}${reportID}`,
