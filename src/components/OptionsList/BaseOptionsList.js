aa2ec217cff62c5904c5c22eb6c724cdcf42c179
Rory Abraham <rory@expensify.com>
2022-06-02 14:50:56 -0700

    Remove onScrollToIndexFailed

--- src/components/OptionsList/BaseOptionsList.js
+++ src/components/OptionsList/BaseOptionsList.js
@@ -3,7 +3,6 @@ import React, {forwardRef, Component} from 'react';
 import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import CONST from '../../CONST';
-import Log from '../../libs/Log';
 import styles from '../../styles/styles';
 import variables from '../../styles/variables';
 import OptionRow from '../OptionRow';
@@ -35,7 +34,6 @@ class BaseOptionsList extends Component {
         this.renderSectionHeader = this.renderSectionHeader.bind(this);
         this.getItemLayout = this.getItemLayout.bind(this);
         this.extractKey = this.extractKey.bind(this);
-        this.onScrollToIndexFailed = this.onScrollToIndexFailed.bind(this);
         this.onViewableItemsChanged = this.onViewableItemsChanged.bind(this);
         this.viewabilityConfig = {viewAreaCoveragePercentThreshold: 95};
         this.didLayout = false;
@@ -68,16 +66,6 @@ class BaseOptionsList extends Component {
 
         this.didLayout = true;
         this.props.onLayout();
-    }
-
-    /**
-     * We must implement this method in order to use the ref.scrollToLocation() method.
-     * See: https://reactnative.dev/docs/sectionlist#scrolltolocation
-     *
-     * @param {Object} info
-     */
-    onScrollToIndexFailed(info) {
-        Log.hmmm('[OptionsList] scrollToIndex failed', info);
     }
 
     /**
@@ -219,7 +207,6 @@ class BaseOptionsList extends Component {
                     showsVerticalScrollIndicator={false}
                     sections={_.filter(this.props.sections, section => section.data.length !== 0)}
                     keyExtractor={this.extractKey}
-                    onScrollToIndexFailed={this.onScrollToIndexFailed}
                     stickySectionHeadersEnabled={false}
                     renderItem={this.renderItem}
                     renderSectionHeader={this.renderSectionHeader}
