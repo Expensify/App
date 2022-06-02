78849d5cd75da0bee115e062bdcbe804e6956e78
Rory Abraham <rory@expensify.com>
2022-06-02 14:49:42 -0700

    Implement getItemLayout

--- src/components/OptionsList/BaseOptionsList.js
+++ src/components/OptionsList/BaseOptionsList.js
@@ -2,8 +2,10 @@ import _ from 'underscore';
 import React, {forwardRef, Component} from 'react';
 import {View} from 'react-native';
 import PropTypes from 'prop-types';
+import CONST from '../../CONST';
 import Log from '../../libs/Log';
 import styles from '../../styles/styles';
+import variables from '../../styles/variables';
 import OptionRow from '../OptionRow';
 import SectionList from '../SectionList';
 import Text from '../Text';
@@ -31,6 +33,7 @@ class BaseOptionsList extends Component {
 
         this.renderItem = this.renderItem.bind(this);
         this.renderSectionHeader = this.renderSectionHeader.bind(this);
+        this.getItemLayout = this.getItemLayout.bind(this);
         this.extractKey = this.extractKey.bind(this);
         this.onScrollToIndexFailed = this.onScrollToIndexFailed.bind(this);
         this.onViewableItemsChanged = this.onViewableItemsChanged.bind(this);
@@ -75,6 +78,59 @@ class BaseOptionsList extends Component {
      */
     onScrollToIndexFailed(info) {
         Log.hmmm('[OptionsList] scrollToIndex failed', info);
+    }
+
+    /**
+     * This function is used to deterministically compute the layout of any given item in our list.
+     * We need to implement it so that we can programmatically scroll to any item in the list.
+     * If we don't, then we can only scroll to items in the virtual render window of the SectionList.
+     *
+     * @param {Array} data - This is the same as the data we pass into the component, i.e: all the non-empty sections
+     * @param {Number} index - This index is provided by React Native, and refers to a flat array with data from all the sections. This flat array has some quirks:
+     *
+     *                         1. It ALWAYS includes a list header and a list footer, even if we don't provide/render those.
+     *                         2. Each section includes a header, even if we don't provide/render one.
+     *
+     *                         For example, given a list with two sections, two items in each section, no header, no footer, and no section headers,
+     *                         the flat array might look something like this:
+     *
+     *                         [{header}, {sectionHeader}, {item}, {item}, {sectionHeader}, {item}, {item}, {footer}]
+     *
+     * @returns {Object}
+     */
+    getItemLayout(data, index) {
+        const optionHeight = this.props.optionMode === CONST.OPTION_MODE.COMPACT ? variables.optionRowHeightCompact : variables.optionRowHeight;
+
+        let offset = 0;
+        let length = 0;
+        for (let i = 0; i < data.length; i++) {
+            const section = data[i];
+            const sectionHeaderHeight = section.title && !this.props.hideSectionHeaders ? variables.optionsListSectionHeaderHeight : 0;
+
+            // The indexOffset of our section is the sum of the lengths of previous sections' data arrays.
+            // To correct it, we need to account for the section headers of all previous sections, and the list header.
+            const correctedIndexOffset = section.indexOffset + i + 1;
+
+            if (correctedIndexOffset > index) {
+                break;
+            }
+
+            if (correctedIndexOffset === index) {
+                // This means that the item we're looking for a section header
+                length = sectionHeaderHeight;
+            }
+
+            offset += sectionHeaderHeight;
+            offset += (Math.min(correctedIndexOffset + section.data.length, index) - correctedIndexOffset) * optionHeight;
+        }
+
+        length = length || optionHeight;
+
+        return {
+            length,
+            offset,
+            index,
+        };
     }
 
     /**
@@ -128,8 +184,11 @@ class BaseOptionsList extends Component {
      */
     renderSectionHeader({section: {title, shouldShow}}) {
         if (title && shouldShow && !this.props.hideSectionHeaders) {
+            // Warning: this optionsListSectionHeader style is brittle – it's computed manually from the dynamic styles in the text node below.
+            // We do this only so that we can reference the height in getItemLayout
+            // 11pt font + 20px padding renders to 54px height
             return (
-                <View>
+                <View style={styles.optionsListSectionHeader}>
                     <Text style={[styles.p5, styles.textMicroBold, styles.colorHeading, styles.textUppercase]}>
                         {title}
                     </Text>
@@ -164,6 +223,7 @@ class BaseOptionsList extends Component {
                     stickySectionHeadersEnabled={false}
                     renderItem={this.renderItem}
                     renderSectionHeader={this.renderSectionHeader}
+                    getItemLayout={this.getItemLayout}
                     extraData={this.props.focusedIndex}
                     initialNumToRender={5}
                     maxToRenderPerBatch={5}
