5ce457ddd12bb298c4af2a7e6047977db855617a
Vitalii Tabakharniuk <44551710+Tabakharniuk@users.noreply.github.com>
2022-06-07 21:17:46 +0200

    Bind this to updateAvatarImage & openAvatarCropModal

--- src/components/AvatarWithImagePicker.js
+++ src/components/AvatarWithImagePicker.js
@@ -74,6 +74,8 @@ class AvatarWithImagePicker extends React.Component {
         this.animation = new SpinningIndicatorAnimation();
         this.setUploadLimitModalVisibility = this.setUploadLimitModalVisibility.bind(this);
         this.isValidSize = this.isValidSize.bind(this);
+        this.updateAvatarImage = this.updateAvatarImage.bind(this);
+        this.openAvatarCropModal = this.openAvatarCropModal.bind(this);
         this.state = {
             isMenuVisible: false,
             isMaxUploadSizeModalOpen: false,
@@ -151,7 +153,7 @@ class AvatarWithImagePicker extends React.Component {
                 text: this.props.translate('avatarWithImagePicker.uploadPhoto'),
                 onSelected: () => {
                     openPicker({
-                        onPicked: avatar => this.openAvatarCropModal(avatar),
+                        onPicked: this.openAvatarCropModal,
                     });
                 },
             },
@@ -257,7 +259,7 @@ class AvatarWithImagePicker extends React.Component {
                 <AvatarCropModal
                     onClose={() => this.setState({isAvatarCropModalOpen: false})}
                     isVisible={this.state.isAvatarCropModalOpen}
-                    onSave={image => this.updateAvatarImage(image)}
+                    onSave={this.updateAvatarImage}
                     imageUri={lodashGet(this.state.image, 'uri')}
                 />
             </View>
