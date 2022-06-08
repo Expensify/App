de17d17ebd5cc59db4a3fdceaf0d297eb34a81be
David Frenkiel <david.frenkiel@yahoo.com>
2022-06-08 12:58:11 -1000

    feat: pdf password form - some cleanup/refactoring in native code

--- src/components/PDFView/index.native.js
+++ src/components/PDFView/index.native.js
@@ -9,15 +9,15 @@ import PDFPasswordForm from './PDFPasswordForm';
 import {propTypes, defaultProps} from './pdfViewPropTypes';
 
 /**
- * On the native layer, we use a pdf library to display PDFs. If a PDF is
+ * On the native layer, we use react-native-pdf/PDF to display PDFs. If a PDF is
  * password-protected we render a PDFPasswordForm to request a password
  * from the user.
  *
  * In order to render things nicely during a password challenge we need
- * to keep track of a bunch of additional state. In particular, the
+ * to keep track of additional state. In particular, the
  * react-native-pdf/PDF component is both conditionally rendered and hidden
  * depending upon the situation. It needs to be rerendered on each password
- * submission because it doesn't dynamically handle updates to it's
+ * submission because it doesn't dynamically handle updates to its
  * password property. And we need to hide it during password challenges
  * so that PDFPasswordForm doesn't bounce when react-native-pdf/PDF
  * is (temporarily) rendered.
@@ -27,10 +27,9 @@ class PDFView extends Component {
         super(props);
         this.state = {
             shouldRequestPassword: false,
+            shouldAttemptPdfLoad: true,
             isPasswordInvalid: false,
             password: '',
-            shouldAttemptPdfLoad: true,
-            isPdfLoaded: false,
         };
         this.initiatePasswordChallenge = this.initiatePasswordChallenge.bind(this);
         this.attemptPdfLoadWithPassword = this.attemptPdfLoadWithPassword.bind(this);
@@ -39,11 +38,11 @@ class PDFView extends Component {
 
     /**
      * Initiate password challenge if message received from react-native-pdf/PDF
-     * indicates that a password is required is invalid.
+     * indicates that a password is required or invalid.
      *
      * For a password challenge the message is "Password required or incorrect password."
      * Note that the message doesn't specify whether the password is simply empty or
-     * rather invalid.
+     * invalid.
      *
      * @param {String} message
      */
@@ -70,18 +69,19 @@ class PDFView extends Component {
      * @param {String} password Password submitted via PDFPasswordForm
      */
     attemptPdfLoadWithPassword(password) {
-        // Render PDF in invisible state. It's invisible because at this
-        // stage of the password challenge process isPdfLoaded is false.
+        // Render react-native-pdf/PDF so that it can validate the password.
+        // Note that at this point in the password challenge, shouldRequestPassword is true.
+        // Thus react-native-pdf/PDF will be rendered - but not visible.
         this.setState({password, shouldAttemptPdfLoad: true});
     }
 
     /**
-     * When the PDF is successfully loaded, terminate password challenge by hiding
-     * PDFPasswordForm and making the PDF component visible.
+     * When the PDF is loaded after a successful password challenge,
+     * hide PDFPasswordForm - and thus also make the react-native-pdf/PDF visible.
      */
     terminatePasswordChallenge() {
-        // Don't render PDFPasswordForm, and do render PDF in visible state.
-        this.setState({shouldRequestPassword: false, isPdfLoaded: true});
+        // Don't render PDFPasswordForm, and thus also render the PDF in visible state.
+        this.setState({shouldRequestPassword: false});
     }
 
     render() {
@@ -90,16 +90,16 @@ class PDFView extends Component {
             StyleUtils.getWidthAndHeightStyle(this.props.windowWidth, this.props.windowHeight),
         ];
 
-        // If we haven't yet successfully loaded the PDF, then we need to hide the
-        // react-native-pdf/PDF component so that PDFPasswordForm is positioned
-        // nicely. We're specifically hiding it because we still need to render the PDF so
-        // that it can validate the password.
-        if (!this.state.isPdfLoaded) {
+        // If we haven't yet successfully validated the password and loaded the PDF,
+        // then we need to hide the react-native-pdf/PDF component so that PDFPasswordForm
+        // is positioned nicely. We're specifically hiding it because we still need to render
+        // the PDF so that it can validate the password.
+        if (this.state.shouldRequestPassword && this.state.shouldAttemptPdfLoad) {
             pdfStyles.push(styles.invisible);
         }
 
-        // For small screens (and Android specifically) force the container view
-        // to take up the full width when displaying the password form.
+        // For small screens force the container view to take up the full width when
+        // displaying the password form.
         const containerStyles = this.state.shouldRequestPassword && this.props.isSmallScreenWidth
             ? styles.pdfPasswordForm.nativeNarrowContainer : {};
 
