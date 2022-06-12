89d8661281e04347414dbb1b1be51b488cf9732e
David Frenkiel <david.frenkiel@yahoo.com>
2022-06-12 08:41:55 -1000

    feat: pdf password form - form validation

--- src/components/PDFView/PDFPasswordForm.js
+++ src/components/PDFView/PDFPasswordForm.js
@@ -1,3 +1,4 @@
+import _ from 'underscore';
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {View} from 'react-native';
@@ -26,8 +27,11 @@ class PDFPasswordForm extends Component {
         super(props);
         this.state = {
             password: '',
+            validationErrorText: '',
         };
         this.submitPassword = this.submitPassword.bind(this);
+        this.validateOnBlur = this.validateOnBlur.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
     }
 
     submitPassword() {
@@ -35,6 +39,22 @@ class PDFPasswordForm extends Component {
             return;
         }
         this.props.onSubmit(this.state.password);
+    }
+
+    updatePassword(password) {
+        if (!_.isEmpty(password)) {
+            this.setState({validationErrorText: ''});
+        }
+        this.setState({password});
+    }
+
+    validateOnBlur() {
+        if (!_.isEmpty(this.state.password)) {
+            return;
+        }
+        this.setState({
+            validationErrorText: this.props.translate('attachmentView.passwordRequired'),
+        });
     }
 
     render() {
@@ -46,21 +66,23 @@ class PDFPasswordForm extends Component {
         return (
             <View style={containerStyles}>
                 <Text style={styles.mb4}>
-                    {this.props.translate('attachmentView.pdfPasswordRequired')}
+                    {this.props.translate('attachmentView.pdfPasswordFormLabel')}
                 </Text>
                 <TextInput
                     label={this.props.translate('common.password')}
                     autoCompleteType="off"
                     textContentType="password"
-                    onChangeText={password => this.setState({password})}
+                    onInputChange={this.updatePassword}
                     returnKeyType="done"
                     onSubmitEditing={this.submitPassword}
+                    errorText={this.state.validationErrorText}
+                    onBlur={this.validateOnBlur}
                     secureTextEntry
                     autoFocus
                 />
                 {this.props.isPasswordInvalid && (
                     <Text style={[styles.formError]}>
-                        {this.props.translate('attachmentView.incorrectPDFPassword')}
+                        {this.props.translate('attachmentView.passwordIncorrect')}
                     </Text>
                 )}
                 <Button
