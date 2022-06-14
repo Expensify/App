5a8c3e5fb2a5d85fa6962053e3e2f1eb1928e420
Manan Jadhav <manan.jadhav@gmail.com>
2022-06-14 13:09:06 +0530

    fix: report actions loader

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -106,7 +106,6 @@ class ReportScreen extends React.Component {
     }
 
     componentDidMount() {
-        this.prepareTransition();
         this.storeCurrentlyViewedReport();
     }
 
@@ -114,8 +113,6 @@ class ReportScreen extends React.Component {
         if (this.props.route.params.reportID === prevProps.route.params.reportID) {
             return;
         }
-
-        this.prepareTransition();
         this.storeCurrentlyViewedReport();
     }
 
