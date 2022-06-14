479e056701d69256f7cc054ad8a2cd0996b1bbe5
Manan Jadhav <manan.jadhav@gmail.com>
2022-06-14 15:44:18 +0530

    fix: alignment of skeleton view

--- src/components/ReportActionsSkeletonView/SkeletonViewLines.js
+++ src/components/ReportActionsSkeletonView/SkeletonViewLines.js
@@ -13,11 +13,11 @@ const SkeletonViewLines = props => (
     <SkeletonViewContentLoader
         height={CONST.CHAT_SKELETON_VIEW.HEIGHT_FOR_ROW_COUNT[props.numberOfRows]}
     >
-        <Circle cx="50" cy="26" r="20" />
-        <Rect x="90" y="11" width="20%" height="8" />
-        <Rect x="90" y="31" width="90%" height="8" />
-        {props.numberOfRows > 1 && <Rect x="90" y="51" width="50%" height="8" />}
-        {props.numberOfRows > 2 && <Rect x="90" y="71" width="50%" height="8" />}
+        <Circle cx="40" cy="26" r="20" />
+        <Rect x="70" y="11" width="20%" height="8" />
+        <Rect x="70" y="31" width="90%" height="8" />
+        {props.numberOfRows > 1 && <Rect x="70" y="51" width="50%" height="8" />}
+        {props.numberOfRows > 2 && <Rect x="70" y="71" width="50%" height="8" />}
     </SkeletonViewContentLoader>
 );
 
