21faefe9c0710a6d21da7e82e1b5141a2c58d4e6
Neil Marcellini <neil@expensify.com>
2022-06-15 12:48:36 -0400

    Create isLogginInAsNewUser util function

---
+++ src/libs/SessionUtils.js
@@ -0,0 +1,29 @@
+import lodashGet from 'lodash/get';
+
+/**
+ * Determine if the transitioning user is logging in as a new user.
+ *
+ * @param {String} transitionURL
+ * @param {String} sessionEmail
+ * @returns {Boolean}
+ */
+function isLoggingInAsNewUser(transitionURL, sessionEmail) {
+    // The OldDot mobile app does not URL encode the parameters, but OldDot web
+    // does. We don't want to deploy OldDot mobile again, so as a work around we
+    // compare both the raw and decoded email from the transition link. If both don't match
+    // the session email, then we are logging in as a new user.
+    // See https://github.com/Expensify/Mobile-Expensify/pull/12280#issuecomment-1155633503
+    const params = new URLSearchParams(transitionURL);
+    const paramsEmail = params.get('email');
+
+    // Capture the un-encoded text in the email param
+    const emailParamRegex = /[?&]email=([^&]*)/g;
+    const matches = emailParamRegex.exec(transitionURL);
+    const linkedEmail = lodashGet(matches, 1, null);
+    return paramsEmail !== sessionEmail && linkedEmail !== sessionEmail;
+}
+
+export {
+    // eslint-disable-next-line import/prefer-default-export
+    isLoggingInAsNewUser,
+};
