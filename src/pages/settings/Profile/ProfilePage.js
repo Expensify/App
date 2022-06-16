26d331a040530fc87794473a8daeda5464dc2095
Mahendra Liya <liyamahendra4@gmail.com>
2022-06-16 15:47:26 +0530

    Solved automatic timezone checkbox not being clickable

--- src/pages/settings/Profile/ProfilePage.js
+++ src/pages/settings/Profile/ProfilePage.js
@@ -346,11 +346,9 @@ class ProfilePage extends Component {
                             />
                         </View>
                         <CheckboxWithLabel
-                            inputID="shouldSetTimezoneAutomatically"
                             label={this.props.translate('profilePage.setMyTimezoneAutomatically')}
                             isChecked={this.state.isAutomaticTimezone}
                             onInputChange={this.setAutomaticTimezone}
-                            shouldSaveDraft
                         />
                     </Form>
                 </KeyboardAvoidingView>
