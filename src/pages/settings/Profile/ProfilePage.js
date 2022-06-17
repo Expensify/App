b3ce0f4751c1c2c0c5e264119e2b81ec2eed9561
Mahendra Liya <liyamahendra4@gmail.com>
2022-06-17 23:06:52 +0530

    Making CheckboxWithLabel compatible with the Form

--- src/pages/settings/Profile/ProfilePage.js
+++ src/pages/settings/Profile/ProfilePage.js
@@ -334,9 +334,11 @@ class ProfilePage extends Component {
                             />
                         </View>
                         <CheckboxWithLabel
+                            inputID="shouldSetTimezoneAutomatically"
                             label={this.props.translate('profilePage.setMyTimezoneAutomatically')}
                             isChecked={this.state.isAutomaticTimezone}
                             onInputChange={this.setAutomaticTimezone}
+                            shouldSaveDraft
                         />
                     </Form>
                 </KeyboardAvoidingView>
