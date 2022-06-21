fdc36060123df4f7e8df2232929d903b741ef164
chiragsalian <chirag@expensify.com>
2022-06-20 17:09:34 -0700

    support for getEnvironmentURL

--- src/libs/Environment/Environment.js
+++ src/libs/Environment/Environment.js
@@ -2,6 +2,13 @@ import Config from 'react-native-config';
 import lodashGet from 'lodash/get';
 import CONST from '../../CONST';
 import getEnvironment from './getEnvironment';
+import CONFIG from '../../CONFIG';
+
+const ENVIRONMENT_URLS = {
+    [CONST.ENVIRONMENT.DEV]: CONST.DEV_NEW_EXPENSIFY_URL + CONFIG.DEV_PORT,
+    [CONST.ENVIRONMENT.STAGING]: CONST.STAGING_NEW_EXPENSIFY_URL,
+    [CONST.ENVIRONMENT.PRODUCTION]: CONST.NEW_EXPENSIFY_URL,
+};
 
 /**
  * Are we running the app in development?
@@ -12,7 +19,20 @@ function isDevelopment() {
     return lodashGet(Config, 'ENVIRONMENT', CONST.ENVIRONMENT.DEV) === CONST.ENVIRONMENT.DEV;
 }
 
+/**
+ * Get the URL based on the environment we are in
+ *
+ * @returns {Promise<unknown>}
+ */
+function getEnvironmentURL() {
+    return new Promise((resolve) => {
+        getEnvironment()
+            .then(environment => resolve(ENVIRONMENT_URLS[environment]));
+    });
+}
+
 export {
     getEnvironment,
     isDevelopment,
+    getEnvironmentURL,
 };
