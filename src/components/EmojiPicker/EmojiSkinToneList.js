e5e56648bf41dcadb38667c9791e1b17632ff4c6
sahil <thesahindia@gmail.com>
2022-06-21 23:49:58 +0530

    fix the broken skin tone emoji for safari

--- src/components/EmojiPicker/EmojiSkinToneList.js
+++ src/components/EmojiPicker/EmojiSkinToneList.js
@@ -67,7 +67,7 @@ class EmojiSkinToneList extends Component {
                                 styles.alignItemsCenter,
                             ]}
                         >
-                            <Text style={[styles.emojiText, styles.ph2, styles.emojiItem]}>
+                            <Text style={[styles.emojiText, styles.ph2, styles.emojiItem, styles.textNoWrap]}>
                                 {selectedEmoji.code}
                             </Text>
                             <Text style={[styles.emojiSkinToneTitle]}>
