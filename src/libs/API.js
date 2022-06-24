a0ddf1738129c4be739e07021be8c46f28059194
Marco Chávez <marcochavezf@gmail.com>
2022-06-24 11:03:31 -0500

    Update src/libs/API.js
    Co-authored-by: Tim Golen <tgolen@gmail.com>

--- src/libs/API.js
+++ src/libs/API.js
@@ -5,7 +5,7 @@ import * as SequentialQueue from './Network/SequentialQueue';
 import {version} from '../../package.json';
 
 /**
- * All calls to API.write() will be persisted to disk as a JSON object.
+ * All calls to API.write() will be persisted to disk as JSON with the params, successData, and failureData.
  * That object will have all the data needed to make the WRITE request later. In other words, we'll queue the WRITE
  * request so that if the network is unavailable or the app is closed, we can send the WRITE request whenever the
  * network comes back.
