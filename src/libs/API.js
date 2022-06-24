b64fbae6a9747222bae43b490a0a44acc0b66d4e
Marco Chávez <marco@expensify.com>
2022-06-24 11:53:14 -0500

    update apiCommandParameters JSDoc description

--- src/libs/API.js
+++ src/libs/API.js
@@ -8,14 +8,14 @@ import {version} from '../../package.json';
  * All calls to API.write() will be persisted to disk as JSON with the params, successData, and failureData.
  * This is so that if the network is unavailable or the app is closed, we can send the WRITE request later.
  *
- * @param {String} command - Name of API command to call
- * @param {Object} apiCommandParameters - Name value pairs of parameters to send to the API
+ * @param {String} command - Name of API command to call.
+ * @param {Object} apiCommandParameters - Name value pairs of parameters to send to the API.
  * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data that will be merged
  *                             into Onyx before and after a request is made. Each nested object will be formatted in
  *                             the same way as an API response.
  * @param {Object} [onyxData.optimisticData] - Onyx instructions that will be passed to Onyx.update() before the request is made.
- * @param {Object} [onyxData.successData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode === 200
- * @param {Object} [onyxData.failureData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode !== 200
+ * @param {Object} [onyxData.successData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode === 200.
+ * @param {Object} [onyxData.failureData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode !== 200.
  */
 function write(command, apiCommandParameters = {}, onyxData = {}) {
     // Optimistically update Onyx
@@ -54,8 +54,8 @@ function write(command, apiCommandParameters = {}, onyxData = {}) {
  * Using this method is discouraged and will throw an ESLint error. Use it sparingly and only when all other alternatives have been exhausted.
  * It is best to discuss it in Slack anytime you are tempted to use this method.
  *
- * @param {String} command - Name of API command to call
- * @param {Object} apiCommandParameters - The object of parameters to send to the API
+ * @param {String} command - Name of API command to call.
+ * @param {Object} apiCommandParameters - Name value pairs of parameters to send to the API.
  * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data that will be merged
  *                             into Onyx before and after a request is made. Each nested object will be formatted in
  *                             the same way as an API response.
@@ -90,8 +90,8 @@ function makeRequestWithSideEffects(command, apiCommandParameters = {}, onyxData
 /**
  * Requests made with this method are not be persisted to disk. If there is no network connectivity, the request is ignored and discarded.
  *
- * @param {String} command - Name of API command to call
- * @param {Object} apiCommandParameters - The object of parameters to send to the API
+ * @param {String} command - Name of API command to call.
+ * @param {Object} apiCommandParameters - Name value pairs of parameters to send to the API.
  * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data that will be merged
  *                             into Onyx before and after a request is made. Each nested object will be formatted in
  *                             the same way as an API response.
