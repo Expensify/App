a9b9c00c7496f01ea70a7398102f9feeeb5933d2
Marco Chávez <marco@expensify.com>
2022-06-24 07:47:19 -0500

    add description to the API.write command

--- src/libs/API.js
+++ src/libs/API.js
@@ -5,10 +5,15 @@ import * as SequentialQueue from './Network/SequentialQueue';
 import {version} from '../../package.json';
 
 /**
+ * API.write() is for WRITE commands. It will not return a promise or any value to the caller.
+ * All calls to API.write() will be persisted to disk as a JSON object.
+ * That object will have all the data needed to make the WRITE request later. In other words, we'll queue the WRITE
+ * request so that if the network is unavailable or the app is closed, we can send the WRITE request whenever the
+ * network comes back.
  *
  * @param {String} command - Name of API command to call
  * @param {Object} apiCommandParameters - The object of parameters to send to the API
- * @param {Object} onyxData
+ * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data
  * @param {Object} [onyxData.optimisticData] - An object of data that will be merged into Onyx before the request is made
  * @param {Object} [onyxData.successData] - An object of data that will be merged into Onyx when the request succeeds.
  * @param {Object} [onyxData.failureData] - An object of data that will be merged into Onyx when the request fails.
