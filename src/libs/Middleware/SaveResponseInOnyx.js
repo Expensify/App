76629f323dcfc1a224d2bb0ac1a871cddd11810c
Yuwen Memon <yuwen@expensify.com>
2022-06-27 20:24:29 -0700

    Don't neccesistate the presence of onyxData in a response in order to update with success or fail data

--- src/libs/Middleware/SaveResponseInOnyx.js
+++ src/libs/Middleware/SaveResponseInOnyx.js
@@ -9,21 +9,26 @@ import _ from 'underscore';
 function SaveResponseInOnyx(response, request) {
     return response
         .then((responseData) => {
-            // We'll only save the onyxData, successData and failureData for the refactored commands
-            if (_.has(responseData, 'onyxData')) {
-                const data = [];
-                if (responseData.jsonCode === 200) {
-                    if (request.successData) {
-                        data.push(...request.successData);
-                    }
-                } else if (request.failureData) {
-                    data.push(...request.failureData);
+            const data = [];
+
+            // Handle the request's success/failure data (client-side data)
+            if (responseData.jsonCode === 200) {
+                if (request.successData) {
+                    data.push(...request.successData);
                 }
-                if (responseData.onyxData) {
-                    data.push(...responseData.onyxData);
-                }
+            } else if (request.failureData) {
+                data.push(...request.failureData);
+            }
+
+            // Add any onyx updates that are being sent back from the API
+            if (responseData.onyxData) {
+                data.push(...responseData.onyxData);
+            }
+
+            if (!_.isEmpty(data)) {
                 Onyx.update(data);
             }
+
             return responseData;
         });
 }
