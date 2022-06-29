880007dc4349b123444fcee33d4fa80be485115b
Yuwen Memon <yuwen@expensify.com>
2022-06-29 10:39:16 -0700

    More spanish

--- src/languages/es.js
+++ src/languages/es.js
@@ -926,7 +926,7 @@ export default {
     },
     requestCallConfirmationScreen: {
         callRequested: '¡Llamada solicitada con éxito!',
-        allSet: 'Listo! Pronto recibirás una llamada nuestra.',
+        allSet: 'Todo listo! Pronto recibirás una llamada nuestra.',
         gotIt: 'Entendido',
     },
     emojiPicker: {
