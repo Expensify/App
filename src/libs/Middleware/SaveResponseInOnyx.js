1b496acd8f7fd656a9a0df46a1b96369e6bb8f28
Yuwen Memon <yuwen@expensify.com>
2022-06-28 20:38:08 -0700

    Fix tests

--- src/libs/Middleware/SaveResponseInOnyx.js
+++ src/libs/Middleware/SaveResponseInOnyx.js
@@ -11,22 +11,23 @@ function SaveResponseInOnyx(response, request) {
         .then((responseData) => {
             const data = [];
 
-            // Handle the request's success/failure data (client-side data)
-            if (responseData.jsonCode === 200) {
-                if (request.successData) {
+            // Make sure we have a response (i.e. this isn't a promise being passed down to us by a failed retry request and responseData is undefined)
+            if (responseData) {
+                // Handle the request's success/failure data (client-side data)
+                if (responseData.jsonCode === 200 && request.successData) {
                     data.push(...request.successData);
+                } else if (request.failureData) {
+                    data.push(...request.failureData);
                 }
-            } else if (request.failureData) {
-                data.push(...request.failureData);
-            }
 
-            // Add any onyx updates that are being sent back from the API
-            if (responseData.onyxData) {
-                data.push(...responseData.onyxData);
-            }
+                // Add any onyx updates that are being sent back from the API
+                if (responseData.onyxData) {
+                    data.push(...responseData.onyxData);
+                }
 
-            if (!_.isEmpty(data)) {
-                Onyx.update(data);
+                if (!_.isEmpty(data)) {
+                    Onyx.update(data);
+                }
             }
 
             return responseData;
