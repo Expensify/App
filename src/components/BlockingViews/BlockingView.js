8ed54ffa542c130ec79f16e6ec6524420f7f0e32
Andrew Rosiclair <arosiclair@expensify.com>
2022-06-30 14:49:12 -0400

    added shared BlockingView

---
+++ src/components/BlockingViews/BlockingView.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from '../../styles/styles';
+import variables from '../../styles/variables';
+import Icon from '../Icon';
+import Text from '../Text';
+
+const propTypes = {
+    /** Src url for the icon (should be from Expensicons) */
+    icon: PropTypes.string.isRequired,
+
+    /** Color for the icon (should be from theme) */
+    iconColor: PropTypes.string.isRequired,
+
+    /** Title message below the icon */
+    title: PropTypes.string.isRequired,
+
+    /** Subtitle message below the title */
+    subtitle: PropTypes.string.isRequired,
+};
+
+const BlockingView = props => (
+    <View
+        style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}
+    >
+        <Icon
+            src={props.icon}
+            fill={props.iconColor}
+            width={variables.iconSizeSuperLarge}
+            height={variables.iconSizeSuperLarge}
+        />
+        <Text style={[styles.headerText, styles.textLarge, styles.mt5]}>{props.title}</Text>
+        <Text style={[styles.w70, styles.textAlignCenter]}>{props.subtitle}</Text>
+    </View>
+);
+
+BlockingView.propTypes = propTypes;
+BlockingView.displayName = 'BlockingView';
+
+export default BlockingView;
