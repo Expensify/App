217c07164442dcdb34e2cb0d45e3a085a12e45a5
Maria D'Costa <maria@expensify.com>
2022-06-30 13:17:36 +0400

    Remove unused confirm modal

--- src/pages/settings/Payments/PaymentsPage/BasePaymentsPage.js
+++ src/pages/settings/Payments/PaymentsPage/BasePaymentsPage.js
@@ -27,7 +27,6 @@ import ConfirmPopover from '../../../../components/ConfirmPopover';
 import AddPaymentMethodMenu from '../../../../components/AddPaymentMethodMenu';
 import CONST from '../../../../CONST';
 import * as Expensicons from '../../../../components/Icon/Expensicons';
-import ConfirmModal from '../../../../components/ConfirmModal';
 import KYCWall from '../../../../components/KYCWall';
 import {propTypes, defaultProps} from './paymentsPagePropTypes';
 import {withNetwork} from '../../../../components/OnyxProvider';
@@ -419,19 +418,6 @@ class BasePaymentsPage extends React.Component {
                         }}
                         shouldShowCancelButton
                         danger
-                    />
-                    <ConfirmModal
-                        title={this.props.translate('paymentsPage.allSet')}
-                        onConfirm={PaymentMethods.dismissWalletConfirmModal}
-                        isVisible={this.props.walletTransfer.shouldShowConfirmModal}
-                        prompt={this.props.translate('paymentsPage.transferConfirmText', {
-                            amount: this.props.numberFormat(
-                                this.props.walletTransfer.transferAmount / 100,
-                                {style: 'currency', currency: 'USD'},
-                            ),
-                        })}
-                        confirmText={this.props.translate('paymentsPage.gotIt')}
-                        shouldShowCancelButton={false}
                     />
                 </KeyboardAvoidingView>
             </ScreenWrapper>
