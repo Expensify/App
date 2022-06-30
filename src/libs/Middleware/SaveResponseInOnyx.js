7877efa963854d428ffcc3569f3f27a76a8136fb
Maria D'Costa <maria@expensify.com>
2022-06-30 11:46:52 +0400

    Allow success and failure data to be set for refactored commands

--- src/libs/Middleware/SaveResponseInOnyx.js
+++ src/libs/Middleware/SaveResponseInOnyx.js
@@ -10,20 +10,18 @@ function SaveResponseInOnyx(response, request) {
     return response
         .then((responseData) => {
             // We'll only save the onyxData, successData and failureData for the refactored commands
-            if (_.has(responseData, 'onyxData')) {
-                const data = [];
-                if (responseData.jsonCode === 200) {
-                    if (request.successData) {
-                        data.push(...request.successData);
-                    }
-                } else if (request.failureData) {
-                    data.push(...request.failureData);
+            const data = [];
+            if (responseData.jsonCode == 200) {
+                if (request.successData) {
+                    data.push(...request.successData);
                 }
-                if (responseData.onyxData) {
-                    data.push(...responseData.onyxData);
-                }
-                Onyx.update(data);
+            } else if (request.failureData) {
+                data.push(...request.failureData);
             }
+            if (responseData.onyxData) {
+                data.push(...responseData.onyxData);
+            }
+            Onyx.update(data);
             return responseData;
         });
 }
