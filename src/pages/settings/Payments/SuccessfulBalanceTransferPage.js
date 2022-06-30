55e3783471bc2375cc78f631228d40e79c5f87b6
Maria D'Costa <maria@expensify.com>
2022-06-30 11:22:34 +0400

    Fixes for successful transfer balance page

--- src/pages/settings/Payments/SuccessfulBalanceTransferPage.js
+++ src/pages/settings/Payments/SuccessfulBalanceTransferPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {
     View,
 } from 'react-native';
-import CONST from '../../CONST';
-import Text from '../../components/Text';
-import TextLink from '../../components/TextLink';
-import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
-import styles from '../../styles/styles';
+import Button from '../../../components/Button';
+import CONST from '../../../CONST';
+import Text from '../../../components/Text';
+import TextLink from '../../../components/TextLink';
+import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
+import styles from '../../../styles/styles';
+import * as PaymentMethods from '../../../libs/actions/PaymentMethods';
 
 const propTypes = {
     ...withLocalizePropTypes,
@@ -16,18 +18,17 @@ const SuccessfulBalanceTransferPage = props => (
     <View style={[styles.flex1]}>
         <View style={[styles.ph5]}>
             <Text style={styles.mb3}>
-                Your wallet balance has been successfully transferred! Please reach out to
+                {"Your wallet balance has been successfully transferred! Please reach out to "}
                 <TextLink href={`mailto:${CONST.EMAIL.CONCIERGE}`} style={[styles.link]}>
                     {CONST.EMAIL.CONCIERGE}
-                </TextLink>
-                if you have any questions.
+                </TextLink> 
+                {" if you have any questions."}
             </Text>
             <Button
                 text={props.translate('paymentsPage.allSet')}
-                onPress={() => Navigation.navigate(ROUTES.SETTINGS_PAYMENTS)}
+                onPress={() => PaymentMethods.dismissSuccessfulTransferBalancePage()}
                 style={[styles.mt4]}
                 iconStyles={[styles.mr5]}
-                extraLarge
                 success
             />
         </View>
