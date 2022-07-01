d616b01e0dd9365840f93d9942ea76f1cb8f5583
Rory Abraham <rory@expensify.com>
2022-07-01 15:59:33 -0700

    Move focusedOption check

--- src/components/OptionsSelector.js
+++ src/components/OptionsSelector.js
@@ -190,12 +190,12 @@ class OptionsSelector extends Component {
         this.unsubscribeCTRLEnter = KeyboardShortcut.subscribe(
             CTRLEnterConfig.shortcutKey,
             () => {
-                const focusedOption = this.state.allOptions[this.state.focusedIndex];
                 if (this.props.canSelectMultipleOptions) {
                     this.props.onConfirmSelection();
                     return;
                 }
 
+                const focusedOption = this.state.allOptions[this.state.focusedIndex];
                 if (!focusedOption) {
                     return;
                 }
