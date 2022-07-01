9232edceb12ad8c7e7189c92ce559123006ac779
sahil <thesahindia@gmail.com>
2022-07-01 23:30:18 +0530

    add comment

--- src/styles/fontFamily/emoji/index.js
+++ src/styles/fontFamily/emoji/index.js
@@ -1,3 +1,5 @@
+// In windows and ubuntu, we need these system fonts for emojis to work properly
+// otherwise few of them will appear as black and white
 const EMOJI_TEXT_FONT = 'Segoe UI Emoji, Noto Color Emoji';
 
 export default EMOJI_TEXT_FONT;
