b1edcb0280cfbb6e11d0ef836fe84f10a936a528
Marco Chávez <marco@expensify.com>
2022-07-06 11:35:38 -0500

    add reponsiveness to the LHN

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -15,3 +15,20 @@ if (window.tocbot) {
 function openArticle(path) { // eslint-disable-line no-unused-vars
     window.location.href = path;
 }
+
+function toggleHeaderMenu() { // eslint-disable-line no-unused-vars
+    const lhn = document.getElementById('lhn');
+    const anguleUpIcon = document.getElementById('angle-up-icon');
+    const barsIcon = document.getElementById('bars-icon');
+    if (lhn.className === 'expanded') {
+        // Collapse the LHN in mobile
+        lhn.className = '';
+        barsIcon.classList.remove('hide');
+        anguleUpIcon.classList.add('hide');
+    } else {
+        // Expand the LHN in mobile
+        lhn.className = 'expanded';
+        barsIcon.classList.add('hide');
+        anguleUpIcon.classList.remove('hide');
+    }
+}
