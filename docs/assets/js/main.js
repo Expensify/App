d2bdf78c3939f9a397eb6f8fc13711b7238e8907
Marco Chávez <marco@expensify.com>
2022-07-07 15:09:21 -0500

    fix toc in lhn styles

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -9,6 +9,16 @@ if (window.tocbot) {
         // Disable the collapsible functionality of the library by
         // setting the maximum number of heading levels (6)
         collapseDepth: 6,
+
+        // Main class to add to lists.
+        listClass: 'lhn-items',
+
+        // Main class to add to links.
+        linkClass: 'link',
+
+        // Class to add to active links,
+        // the link corresponding to the top most heading on the page.
+        activeLinkClass: 'selected-hub',
     });
 }
 
