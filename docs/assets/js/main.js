fecd619aa8eb40dd70325eadb6c7c19f1a0ee1e8
Marco Chávez <marco@expensify.com>
2022-07-07 15:48:09 -0500

    fix scroll in lhn

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,3 +1,25 @@
+
+function openArticle(path) { // eslint-disable-line no-unused-vars
+    window.location.href = path;
+}
+
+function toggleHeaderMenu() { // eslint-disable-line no-unused-vars
+    const lhn = document.getElementById('lhn');
+    const anguleUpIcon = document.getElementById('angle-up-icon');
+    const barsIcon = document.getElementById('bars-icon');
+    if (lhn.className === 'expanded') {
+        // Collapse the LHN in mobile
+        lhn.className = '';
+        barsIcon.classList.remove('hide');
+        anguleUpIcon.classList.add('hide');
+    } else {
+        // Expand the LHN in mobile
+        lhn.className = 'expanded';
+        barsIcon.classList.add('hide');
+        anguleUpIcon.classList.remove('hide');
+    }
+}
+
 if (window.tocbot) {
     window.tocbot.init({
         // Where to render the table of contents.
@@ -19,26 +41,16 @@ if (window.tocbot) {
         // Class to add to active links,
         // the link corresponding to the top most heading on the page.
         activeLinkClass: 'selected-hub',
+
+        // Only takes affect when `tocSelector` is scrolling,
+        // keep the toc scroll position in sync with the content.
+        disableTocScrollSync: true,
+
+        // onclick function to apply to all links in toc. will be called with
+        // the event as the first parameter, and this can be used to stop,
+        // propagation, prevent default or perform action
+        onClick() {
+            toggleHeaderMenu();
+        },
     });
-}
-
-function openArticle(path) { // eslint-disable-line no-unused-vars
-    window.location.href = path;
-}
-
-function toggleHeaderMenu() { // eslint-disable-line no-unused-vars
-    const lhn = document.getElementById('lhn');
-    const anguleUpIcon = document.getElementById('angle-up-icon');
-    const barsIcon = document.getElementById('bars-icon');
-    if (lhn.className === 'expanded') {
-        // Collapse the LHN in mobile
-        lhn.className = '';
-        barsIcon.classList.remove('hide');
-        anguleUpIcon.classList.add('hide');
-    } else {
-        // Expand the LHN in mobile
-        lhn.className = 'expanded';
-        barsIcon.classList.add('hide');
-        anguleUpIcon.classList.remove('hide');
-    }
 }
