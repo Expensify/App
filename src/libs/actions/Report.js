b09b88d466cf70c217bfe29e7dd9f46556bf1b46
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-07-08 15:22:28 -1000

    Use valueOf() not unix() for lastVisitedTimestamp

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1071,7 +1071,7 @@ function updateLastReadActionID(reportID, sequenceNumber, manuallyMarked = false
     const lastReadSequenceNumber = _.isNumber(sequenceNumber) ? (sequenceNumber - 1) : reportMaxSequenceNumber;
     Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {
         unreadActionCount: calculateUnreadActionCount(reportID, lastReadSequenceNumber, reportMaxSequenceNumber),
-        lastVisitedTimestamp: moment().unix(),
+        lastVisitedTimestamp: moment().valueOf(),
         lastReadSequenceNumber,
     });
 
@@ -1486,7 +1486,7 @@ function viewNewReportAction(reportID, action) {
     // unreadActionCount if the incoming sequenceNumber is higher than the last read for the user.
     if (isFromCurrentUser) {
         updatedReportObject.unreadActionCount = 0;
-        updatedReportObject.lastVisitedTimestamp = moment().unix();
+        updatedReportObject.lastVisitedTimestamp = moment().valueOf();
         updatedReportObject.lastReadSequenceNumber = action.sequenceNumber;
     } else if (incomingSequenceNumber > lastReadSequenceNumber) {
         updatedReportObject.unreadActionCount = getUnreadActionCount(reportID) + 1;
