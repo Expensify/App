be8a030a977a154d3f74e91bcc0d6ee483c645d6
Rory Abraham <rory@expensify.com>
2022-07-09 01:27:08 -0700

    Make schema validations async

--- .github/scripts/validateActionsAndWorkflows.sh
+++ .github/scripts/validateActionsAndWorkflows.sh
@@ -2,24 +2,47 @@
 
 echo 'Validates the Github Actions and workflows using the json schemas provided by (https://www.schemastore.org/json/)'
 
-# Track exit codes separately so we can run a full validation, report errors, and exit with the correct code
-declare EXIT_CODE=0
-
 # Download the up-to-date json schemas for github actions and workflows
 cd ./.github && mkdir ./tempSchemas || exit 1
 curl https://json.schemastore.org/github-action.json --output ./tempSchemas/github-action.json --silent || exit 1
 curl https://json.schemastore.org/github-workflow.json --output ./tempSchemas/github-workflow.json --silent || exit 1
 
+# Track exit codes separately so we can run a full validation, report errors, and exit with the correct code
+declare EXIT_CODE=0
+
+# This stores all the process IDs of the ajv commands so they can run in parallel
+declare ASYNC_PROCESSES
+
+# Arrays of actions and workflows
+declare -r ACTIONS=(./actions/*/*/action.yml)
+declare -r WORKFLOWS=(./workflows/*.yml)
+
 # Validate the actions and workflows using the JSON schemas and ajv https://github.com/ajv-validator/ajv-cli
-find ./actions -type f -name "*.yml" -print0 | xargs -0 -I file ajv -s ./tempSchemas/github-action.json -d file --strict=false || EXIT_CODE=1
-find ./workflows -type f -name "*.yml" -print0 | xargs -0 -I file ajv -s ./tempSchemas/github-workflow.json -d file --strict=false || EXIT_CODE=1
+for ((i=0; i < ${#ACTIONS[@]}; i++)); do
+  ACTION=${ACTIONS[$i]}
+  ajv -s ./tempSchemas/github-action.json -d "$ACTION" --strict=false &
+  ASYNC_PROCESSES[$i]=$!
+done
+
+for ((i=0; i < ${#WORKFLOWS[@]}; i++)); do
+  WORKFLOW=${WORKFLOWS[$i]}
+  ajv -s ./tempSchemas/github-workflow.json -d "$WORKFLOW" --strict=false &
+  ASYNC_PROCESSES[${#ACTIONS[@]} + $i]=$!
+done
+
+# Wait for the background builds to finish
+for PID in ${ASYNC_PROCESSES[*]}; do
+  if ! wait $PID; then
+    EXIT_CODE=$?
+  fi
+done
+
+# Cleanup after ourselves and delete the schemas
+rm -rf ./tempSchemas
 
 if (( "$EXIT_CODE" != 0 )); then
   exit $EXIT_CODE
 fi
-
-# Cleanup after ourselves and delete the schemas
-rm -rf ./tempSchemas
 
 echo
 echo 'Lint Github Actions via actionlint (https://github.com/rhysd/actionlint)'
