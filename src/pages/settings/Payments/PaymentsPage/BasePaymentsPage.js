f58e3f4082465946cd3231d7458b4ffa7714a216
Josue Echandia <josueechandiasto@gmail.com>
2022-07-11 14:57:12 -0500

    Lint fix

--- src/pages/settings/Payments/PaymentsPage/BasePaymentsPage.js
+++ src/pages/settings/Payments/PaymentsPage/BasePaymentsPage.js
@@ -304,7 +304,7 @@ class BasePaymentsPage extends React.Component {
                         onClose={this.hideAddPaymentMenu}
                         anchorPosition={{
                             bottom: this.state.anchorPositionBottom,
-                            right: this.state.anchorPositionRight-30,
+                            right: this.state.anchorPositionRight - 30,
                         }}
                         onItemSelected={method => this.addPaymentMethodTypePressed(method)}
                     />
