89c366edc76b3dd71b0f43cff8fb1ca098a8ae18
Vit Horacek <vit@expensify.com>
2022-07-14 18:48:42 +0100

    Remove call from app start

--- src/libs/actions/App.js
+++ src/libs/actions/App.js
@@ -95,7 +95,6 @@ function getAppData(shouldSyncPolicyList = true) {
     User.getUserDetails();
     User.getBetas();
     User.getDomainInfo();
-    PersonalDetails.openIOUModalPage();
     GeoLocation.fetchCountryCodeByRequestIP();
     BankAccounts.fetchUserWallet();
 
