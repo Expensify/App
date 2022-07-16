c1efce47d5c16677a90deff75e547313302a379d
Vitalii Tabakharniuk <44551710+Tabakharniuk@users.noreply.github.com>
2022-07-16 18:21:05 +0200

    Update gestureHandlerPropTypes

--- src/components/AvatarCropModal/gestureHandlerPropTypes.js
+++ src/components/AvatarCropModal/gestureHandlerPropTypes.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-export default PropTypes.shape({
-    current: PropTypes.shape({
-        eventNames: PropTypes.arrayOf(PropTypes.string),
-        reattachNeeded: PropTypes.bool,
-        registrations: PropTypes.arrayOf(PropTypes.number),
-        viewTag: PropTypes.number,
-        worklet: PropTypes.func,
+export default PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({
+        current: PropTypes.shape({
+            eventNames: PropTypes.arrayOf(PropTypes.string),
+            reattachNeeded: PropTypes.bool,
+            registrations: PropTypes.arrayOf(PropTypes.number),
+            viewTag: PropTypes.number,
+            worklet: PropTypes.func,
+        }),
     }),
-});
+]);
