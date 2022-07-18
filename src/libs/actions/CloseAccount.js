7a1604a9ab9d9338a5431fb2c3a7a71b6b91bb03
Alex Beaman <alexbeaman@expensify.com>
2022-07-18 17:01:39 +0200

    Don't worry about closing already closed modal

--- src/libs/actions/CloseAccount.js
+++ src/libs/actions/CloseAccount.js
@@ -1,19 +1,10 @@
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 
-let isCloseAccountErrorModalOpen;
-Onyx.connect({
-    key: ONYXKEYS.CLOSE_ACCOUNT,
-    callback: data => isCloseAccountErrorModalOpen = Boolean(data && data.error),
-});
-
 /**
  * Unset CloseAccount error message to hide modal
  */
 function hideCloseAccountErrorModal() {
-    if (!isCloseAccountErrorModalOpen) {
-        return;
-    }
     Onyx.merge(ONYXKEYS.CLOSE_ACCOUNT, {error: ''});
 }
 
