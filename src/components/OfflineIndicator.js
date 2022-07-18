ed74e954a6c279f534ff2c8ff0a1d18cae90b800
Nikki Wines <nikkiwines@expensify.com>
2022-07-17 21:55:44 -0600

    update comment

--- src/components/OfflineIndicator.js
+++ src/components/OfflineIndicator.js
@@ -16,7 +16,7 @@ const propTypes = {
     /** Information about the network */
     network: networkPropTypes.isRequired,
 
-    /** Additional styles to add after local styles. Applied to Pressable portion of button */
+    /** Additional styles to add after local styles.
     style: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.object),
         PropTypes.object,
