438392490e54f62d143df8bd92c40e0d8cf1bc70
Josue Echandia <josueechandiasto@gmail.com>
2022-07-21 23:22:56 -0500

    Merge improvement

--- src/pages/settings/Payments/PaymentMethodList.js
+++ src/pages/settings/Payments/PaymentMethodList.js
@@ -238,7 +238,7 @@ class PaymentMethodList extends Component {
                                         icon={Expensicons.CreditCard}
                                         onPress={e => this.props.onPress(e)}
                                         isDisabled={this.props.isLoadingPayments || isOffline}
-                                        style={[styles.mh4]}
+                                        style={[styles.mh4, styles.mb4]}
                                         iconStyles={[styles.mr4]}
                                         success
                                         shouldShowRightIcon
