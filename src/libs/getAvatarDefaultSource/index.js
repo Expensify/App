121667e868d47bfadcd63cf4e4013a2a79cb07f1
b1tjoy <103875612+b1tjoy@users.noreply.github.com>
2022-07-25 22:48:30 +0800

    explain why instead of what in comment

--- src/libs/getAvatarDefaultSource/index.js
+++ src/libs/getAvatarDefaultSource/index.js
@@ -1,5 +1,6 @@
 /**
- * Return the source as default source of image when running on Web/Desktop
+ * Avatar icon flickers when message is sent for the first time, return and set the source as
+ * defaultSource prop of image to prevent avatar icon from flicker when running on Web/Desktop
  * @param {String|Function} source The source of avatar image
  * @return {Object} The image source
  */
