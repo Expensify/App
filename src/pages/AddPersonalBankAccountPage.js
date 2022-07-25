672f90df4a503230937f8e705a4fec8491bfdf56
Maria D'Costa <maria@expensify.com>
2022-07-25 15:15:00 +0400

    Add styling for success screen

--- src/pages/AddPersonalBankAccountPage.js
+++ src/pages/AddPersonalBankAccountPage.js
@@ -15,7 +15,11 @@ import compose from '../libs/compose';
 import ONYXKEYS from '../ONYXKEYS';
 import Text from '../components/Text';
 import styles from '../styles/styles';
+import * as Illustrations from '../components/Icon/Illustrations';
+import Icon from '../components/Icon';
+import defaultTheme from '../styles/themes/default';
 import Button from '../components/Button';
+import FixedFooter from '../components/FixedFooter';
 import FormScrollView from '../components/FormScrollView';
 import FormAlertWithSubmitButton from '../components/FormAlertWithSubmitButton';
 import FormHelper from '../libs/FormHelper';
@@ -132,16 +136,31 @@ class AddPersonalBankAccountPage extends React.Component {
                 />
                 {shouldShowSuccess ? (
                     <>
-                        <Text style={[styles.formSuccess, styles.mh5]}>
-                            {success}
-                        </Text>
-                        <View style={[styles.mh5, styles.mb5, styles.flex1, styles.justifyContentEnd]}>
+                        <View style={[styles.pageWrapper, styles.flex1, styles.flexColumn, styles.alignItemsCenter, styles.justifyContentCenter]}>
+                            <Icon
+                                src={Illustrations.TadaBlue}
+                                height={100}
+                                width={100}
+                                fill={defaultTheme.iconSuccessFill}
+                            />
+                            <View style={[styles.ph5]}>
+                                <Text style={[styles.mt5, styles.h1, styles.textAlignCenter]}>
+                                    {this.props.translate('addPersonalBankAccountPage.successTitle')}
+                                </Text>
+                                <Text style={[styles.mt3, styles.textAlignCenter]}>
+                                    {this.props.translate('addPersonalBankAccountPage.successMessage')}
+                                </Text>
+                            </View>
+                        </View>
+                        <FixedFooter>
                             <Button
-                                success
                                 text={this.props.translate('common.continue')}
                                 onPress={() => Navigation.navigate(ROUTES.SETTINGS_PAYMENTS)}
+                                style={[styles.mt4]}
+                                iconStyles={[styles.mr5]}
+                                success
                             />
-                        </View>
+                        </FixedFooter>
                     </>
                 ) : (
                     <FormScrollView>
