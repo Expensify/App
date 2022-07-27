161c5344eea1f546f4989c982cfe4b4a540cfd98
Jasper Huang <jasper@expensify.com>
2022-07-27 12:30:38 -0700

    fix newchatpage white gap on android

--- src/pages/NewChatPage.js
+++ src/pages/NewChatPage.js
@@ -214,7 +214,7 @@ class NewChatPage extends Component {
             maxParticipantsReached,
         );
         return (
-            <ScreenWrapper>
+            <ScreenWrapper keyboardAvoidingViewBehavior="height">
                 {({didScreenTransitionEnd}) => (
                     <>
                         <HeaderWithCloseButton
