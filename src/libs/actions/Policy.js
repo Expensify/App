09de6664aa5ba7625e0e4b73ee689a3fd7870fc6
Jack Nam <jack@expensify.com>
2022-07-27 16:13:24 -0700

    remove chainz

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -546,7 +546,7 @@ function subscribeToPolicyEvents() {
  * @returns {Boolean}
  */
 function hasPolicyMemberError(policyMemberList) {
-    return _.chain(policyMemberList).some(member => !_.isEmpty(member.errors)).value();
+    return policyMemberList.some(member => !_.isEmpty(member.errors)).value();
 }
 
 export {
