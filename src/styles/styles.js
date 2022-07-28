181650567a10790abd2a03ac5e6b7391ffd080cb
sahil <thesahindia@gmail.com>
2022-07-29 01:56:53 +0530

    use EMOJI_TEXT_FONT at the composer

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1385,7 +1385,7 @@ const styles = {
         backgroundColor: themeColors.componentBG,
         borderColor: themeColors.border,
         color: themeColors.text,
-        fontFamily: fontFamily.GTA,
+        fontFamily: fontFamily.EMOJI_TEXT_FONT,
         fontSize: variables.fontSizeNormal,
         borderWidth: 0,
         borderRadius: 0,
