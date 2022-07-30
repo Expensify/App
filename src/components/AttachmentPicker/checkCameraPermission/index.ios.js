a094ceb97e7b6774457a0dcf40e600d1ad3d8de3
dharmik <dharmikisback@gmail.com>
2022-07-30 16:42:25 +0530

    Minor changes.

--- src/components/AttachmentPicker/checkCameraPermission/index.ios.js
+++ src/components/AttachmentPicker/checkCameraPermission/index.ios.js
@@ -4,10 +4,5 @@ export default function checkCameraPermission() {
     /**
     * Checks to see if the iOS device has camera permission or not
     */
-    request(PERMISSIONS.IOS.CAMERA).then((result) => {
-        if (result === RESULTS.GRANTED) {
-            return true;
-        }
-        return false;
-    });
+    request(PERMISSIONS.IOS.CAMERA).then(result => result === RESULTS.GRANTED);
 }
