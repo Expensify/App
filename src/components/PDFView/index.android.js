a135ccfcaee82894acd8e6273186f68b265837e1
David Frenkiel <david.frenkiel@yahoo.com>
2022-08-01 11:36:16 -1000

    feat: pdf password form - removed unnecessary keyboard closure on android

--- src/components/PDFView/index.android.js
+++ src/components/PDFView/index.android.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
 import BasePDFViewNative from './BasePDFViewNative';
 import * as pdfViewPropTypes from './pdfViewPropTypes';
 import compose from '../../libs/compose';
@@ -7,24 +6,10 @@ import withWindowDimensions from '../withWindowDimensions';
 import withKeyboardState from '../withKeyboardState';
 
 class PDFView extends Component {
-    constructor(props) {
-        super(props);
-        this.hideKeyboard = this.hideKeyboard.bind(this);
-    }
-
     componentDidUpdate() {
         // Alert the parent component that it may be necessary to adjust the UI
         // to accommodate the keyboard.
         this.props.onAvoidKeyboard(this.props.isShown);
-    }
-
-    /**
-     * Hide keyboard before rendering PDF. On Android we need to close the keyboard
-     * before rendering the PDF. If the keyboard is open during PDF rendering, then
-     * react-native-pdf doesn't correctly scale the PDF view.
-     */
-    hideKeyboard() {
-        Keyboard.dismiss();
     }
 
     render() {
@@ -32,7 +17,6 @@ class PDFView extends Component {
             <BasePDFViewNative
                 sourceURL={this.props.sourceURL}
                 style={this.props.style}
-                onAttemptPdfLoad={this.hideKeyboard}
             />
         );
     }
