b26cd9f1af17f29fb406827a9b109610ff043988
neil-marcellini <neil@expensify.com>
2022-08-02 10:41:10 -0700

    Remove unused onViewportResize on native platforms

--- src/libs/VisualViewport/index.native.js
+++ src/libs/VisualViewport/index.native.js
@@ -2,11 +2,9 @@
 /**
  * Visual Viewport is not available on native, so return an empty function.
  *
- * @param {Function} onViewportResize
  * @returns {Function}
  */
-// eslint-disable-next-line no-unused-vars
-function addViewportResizeListener(onViewportResize) {
+function addViewportResizeListener() {
     return () => {};
 }
 
