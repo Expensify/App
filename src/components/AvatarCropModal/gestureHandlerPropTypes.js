6400d68e104f402d6f93dee8eea3dea92d6fb9af
Vitalii Tabakharniuk <44551710+Tabakharniuk@users.noreply.github.com>
2022-08-03 22:11:54 +0200

    Add comments to gestureHandlerPropTypes

--- src/components/AvatarCropModal/gestureHandlerPropTypes.js
+++ src/components/AvatarCropModal/gestureHandlerPropTypes.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 
-// PropTypes of react-native-gesture-handler's onGestureEvent prop
 export default PropTypes.oneOfType([
+    // Executes once a gesture is triggered
     PropTypes.func,
     PropTypes.shape({
         current: PropTypes.shape({
+            // Array of event names that will be handled by animation handler
             eventNames: PropTypes.arrayOf(PropTypes.string),
-            reattachNeeded: PropTypes.bool,
+
+            // Array of registered event handlers ids
             registrations: PropTypes.arrayOf(PropTypes.number),
+
+            // React tag of the node we want to manage
             viewTag: PropTypes.number,
+
+            // Executes once a gesture is triggered
             worklet: PropTypes.func,
         }),
     }),
