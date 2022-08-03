f80b2e75f7785678765c76b781e8d604d89db31b
Rajat Parashar <parasharrajat@users.noreply.github.com>
2022-08-03 18:31:46 +0530

    Upgrade jest to latest version

---
+++ jest/setup.env.js
@@ -0,0 +1,1 @@
+global.setImmediate = jest.useRealTimers;
--- tests/utils/waitForPromisesToResolve.js
+++ tests/utils/waitForPromisesToResolve.js
@@ -1,3 +1,5 @@
+import {setImmediate} from 'timers';
+
 /**
  * Method which waits for all asynchronous JS to stop executing before proceeding. This helps test things like actions
  * that expect some Onyx value to be available. This way we do not have to explicitly wait for an action to finish
