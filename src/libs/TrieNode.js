f214eddaf61b12d66e95c0a3d19c5fff0ac8cb0b
karim <108357004+Karim-30@users.noreply.github.com>
2022-08-05 08:09:50 +0200

    remove emoji keyword from TrieNode file and fix return typo

---
+++ src/libs/TrieNode.js
@@ -0,0 +1,44 @@
+/** Class representing a Trie node. */
+class TrieNode {
+    /**
+     * Reset all attributes to default values.
+     */
+    constructor() {
+        this.keys = new Map();
+        this.end = false;
+        this.code = '';
+    }
+
+    /**
+     * Make the current node an end node.
+    */
+    setEnd() {
+        this.end = true;
+    }
+
+    /**
+    * Check if the current node is an end node.
+    * @returns {Boolean}
+    */
+    isEnd() {
+        return this.end;
+    }
+
+    /**
+    * Set the node code, code represent a word payload
+    * @param {String} code
+    */
+    setCode(code) {
+        this.code = code;
+    }
+
+    /**
+    * Get the node code, code represent a word payload
+    * @return {String}
+    */
+    getCode() {
+        return this.code;
+    }
+}
+
+export default TrieNode;
