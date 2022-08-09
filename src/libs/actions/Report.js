15fbbf84737b90822d1f4a11ac5d531d0801dd83
neil-marcellini <neil@expensify.com>
2022-08-08 17:05:16 -0700

    Use rbr pending action update constant

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1482,7 +1482,7 @@ function updatePolicyRoomName(policyRoomReport, reportName) {
             value: {
                 reportName,
                 pendingFields: {
-                    reportName: 'update',
+                    reportName: CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE,
                 },
             },
         },
