3595918c070e3a90ab1f0140fb3d33c9a64d9a74
Maria D'Costa <maria@expensify.com>
2022-08-10 16:21:25 +0400

    Fix prop type

--- src/components/AvatarWithIndicator.js
+++ src/components/AvatarWithIndicator.js
@@ -25,14 +25,15 @@ const propTypes = {
     policiesMemberList: PropTypes.objectOf(policyMemberPropType),
 
     /** The user's wallet (coming from Onyx) */
-    userwallet: PropTypes.objectOf(userWalletPropTypes),
+    ...userWalletPropTypes,
 };
 
 const defaultProps = {
     size: 'default',
     tooltipText: '',
     policiesMemberList: {},
-    userwallet: {},
+    // eslint-disable-next-line react/default-props-match-prop-types
+    userWallet: {},
 };
 
 const AvatarWithIndicator = (props) => {
