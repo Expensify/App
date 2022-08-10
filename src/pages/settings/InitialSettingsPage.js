7208f5f5787e5b6b294e7dff1448b893b5269037
Jack Nam <jack@expensify.com>
2022-08-09 18:05:31 -0700

    add styles for spacing and make it an error dot

--- src/pages/settings/InitialSettingsPage.js
+++ src/pages/settings/InitialSettingsPage.js
@@ -120,7 +120,7 @@ const InitialSettingsPage = (props) => {
             translationKey: 'common.payments',
             icon: Expensicons.Wallet,
             action: () => { Navigation.navigate(ROUTES.SETTINGS_PAYMENTS); },
-            brickRoadIndicator: PaymentMethods.hasPaymentMethodError(props.bankAccountList, props.cardList)
+            brickRoadIndicator: PaymentMethods.hasPaymentMethodError(props.bankAccountList, props.cardList) ? 'error' : null,
         },
         {
             translationKey: 'initialSettingsPage.about',
