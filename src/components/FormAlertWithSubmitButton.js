e445cf7884990d9bc1cbc379ff95f060a7bb2f19
Carlos Martins <cmartins@expensify.com>
2022-08-10 11:59:44 -0600

    revert offlineindicator

--- src/components/FormAlertWithSubmitButton.js
+++ src/components/FormAlertWithSubmitButton.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import styles from '../styles/styles';
 import Button from './Button';
 import FormAlertWrapper from './FormAlertWrapper';
-import OfflineIndicator from './OfflineIndicator';
 
 const propTypes = {
     /** Text for the button */
@@ -45,21 +44,19 @@ const defaultProps = {
 
 const FormAlertWithSubmitButton = props => (
     <FormAlertWrapper
-        containerStyles={[styles.mh5, styles.flex1, styles.justifyContentEnd, ...props.containerStyles]}
+        containerStyles={[styles.mh5, styles.mb5, styles.flex1, styles.justifyContentEnd, ...props.containerStyles]}
         isAlertVisible={props.isAlertVisible}
         isMessageHtml={props.isMessageHtml}
         message={props.message}
         onFixTheErrorsPressed={props.onFixTheErrorsPressed}
     >
         {isOffline => (isOffline ? (
-            <>
-                <Button
-                    success
-                    isDisabled
-                    text={props.buttonText}
-                />
-                <OfflineIndicator containerStyles={[styles.mv1]} />
-            </>
+            <Button
+                success
+                isDisabled
+                text={props.buttonText}
+                style={[styles.mb3]}
+            />
         ) : (
             <Button
                 success
