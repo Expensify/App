b64af1f70ec0a2e60e32f1f801cd97ba9b183683
Amy Evans <amy@expensify.com>
2022-08-12 13:23:01 -0400

    Update data structure to be consistent

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -548,7 +548,7 @@ function updateWorkspaceCustomUnit(policyID, currentCustomUnits, values) {
             value: {
                 customUnits: {
                     distance: {
-                        // customUnitID: values.id, // @TODO reconcile customUnitID vs id
+                        // customUnitID: values.customUnitID,
                         customUnitID: '628e77618f113',
                         // name: values.name,
                         name: 'Distance',
@@ -581,11 +581,11 @@ function updateWorkspaceCustomUnit(policyID, currentCustomUnits, values) {
             value: {
                 customUnits: {
                     distance: {
-                        // id: currentCustomUnits.id,
-                        id: '628e77618f113',
+                        // customUnitID: currentCustomUnits.customUnitID,
+                        customUnitID: '628e77618f113',
                         // name: currentCustomUnits.name,
                         name: 'Distance',
-                        value: currentCustomUnits.value,
+                        attributes: currentCustomUnits.attributes,
                         errors: {
                             // <current_microtime>: '<some generic error>',
                         },
@@ -597,7 +597,7 @@ function updateWorkspaceCustomUnit(policyID, currentCustomUnits, values) {
 
     API.write('UpdateWorkspaceCustomUnit', {
         policyID,
-        customUnit: JSON.stringify(values),
+        customUnits: JSON.stringify(values),
     }, {optimisticData, successData, failureData});
 }
 
