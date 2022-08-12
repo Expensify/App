60c468b8f163f7fb572b36d6261c5744b5059d55
Mahendra Liya <liyamahendra4@gmail.com>
2022-08-12 11:36:20 +0530

    Removed trim
    Co-authored-by: Rushat Gabhane <rushatgabhane@gmail.com>

--- src/pages/RequestCallPage.js
+++ src/pages/RequestCallPage.js
@@ -269,7 +269,7 @@ class RequestCallPage extends Component {
         const phoneNumberError = this.getPhoneNumberError();
         const phoneExtensionError = this.getPhoneExtensionError();
 
-        const [hasFirstNameError, hasLastNameError] = ValidationUtils.doesFailCharacterLimit(CONST.FORM_CHARACTER_LIMIT, [this.state.firstName.trim(), this.state.lastName.trim()]);
+        const [hasFirstNameError, hasLastNameError] = ValidationUtils.doesFailCharacterLimit(CONST.FORM_CHARACTER_LIMIT, [this.state.firstName, this.state.lastName]);
         this.setState({
             hasFirstNameError,
             hasLastNameError,
