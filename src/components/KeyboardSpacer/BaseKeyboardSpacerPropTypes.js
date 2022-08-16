54798eaf1913a9878e5a4df196676a88acd5be12
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2022-08-16 12:32:12 +0530

    Add reference links and improve comments

--- src/components/KeyboardSpacer/BaseKeyboardSpacerPropTypes.js
+++ src/components/KeyboardSpacer/BaseKeyboardSpacerPropTypes.js
@@ -4,12 +4,13 @@ const propTypes = {
     /** Top Spacing is used when there requirement of additional height to view. */
     topSpacing: PropTypes.number,
 
-    /** Callback to update the value of keybaord status along with current height. */
+    /** Callback to update the value of keyboard status along with keyboard height + top spacing. */
     onToggle: PropTypes.func,
 
     /** iOS uses LayoutAnimation.Types[event.easing]. */
     iOSAnimated: PropTypes.bool,
 
+    /** Keyboard event as per platform specific and also for different event to show/hide keyboard https://reactnative.dev/docs/keyboard#addlistener */
     /** Pass event name for keyboardShow since iOS and android both has different event to show keyboard. */
     keyboardShowMethod: PropTypes.string,
 
