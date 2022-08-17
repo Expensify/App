5cb48a37f8472b869795d34025c7e51276727823
Andrew Rosiclair <arosiclair@expensify.com>
2022-08-17 13:10:12 -0400

    remove unnecessary fragment

--- src/pages/workspace/WorkspacePageWithSections.js
+++ src/pages/workspace/WorkspacePageWithSections.js
@@ -103,18 +103,16 @@ class WorkspacePageWithSections extends React.Component {
                     onBackButtonPress={() => Navigation.navigate(ROUTES.getWorkspaceInitialRoute(policyID))}
                     onCloseButtonPress={() => Navigation.dismissModal()}
                 />
-                <>
-                    <ScrollView
-                        style={[styles.settingsPageBackground, styles.flex1, styles.w100]}
-                    >
-                        <View style={[styles.w100, styles.flex1]}>
+                <ScrollView
+                    style={[styles.settingsPageBackground, styles.flex1, styles.w100]}
+                >
+                    <View style={[styles.w100, styles.flex1]}>
 
-                            {this.props.children(hasVBA, policyID, isUsingECard)}
+                        {this.props.children(hasVBA, policyID, isUsingECard)}
 
-                        </View>
-                    </ScrollView>
-                    {this.props.footer}
-                </>
+                    </View>
+                </ScrollView>
+                {this.props.footer}
             </ScreenWrapper>
         );
     }
