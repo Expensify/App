dbf0af785b387f34c174c6e3212c1b82436618dc
Srikar Parsi <srikar.parsi@expensify.com>
2022-08-18 14:47:16 -0400

    style fixes

--- src/libs/NumberUtils.js
+++ src/libs/NumberUtils.js
@@ -1,4 +1,4 @@
-import CONST from "../CONST";
+import CONST from '../CONST';
 
 /**
  * Generates a random positive 64 bit numeric string by randomly generating the left half and right half and concatenating them. Used to generate client-side ids.
@@ -8,21 +8,20 @@ function rand64() {
     // Max 64-bit signed:
     // 9,223,372,036,854,775,807
     // The left part of the max 64-bit number *+1* because we're flooring it
-    let left = Math.floor(Math.random() * CONST.MAX_64BIT_LEFT_HALF + 1);
-  
+    const left = Math.floor(Math.random() * CONST.MAX_64BIT_LEFT_HALF + 1);
+
     // The right part of the max 64-bit number *+1* for the same reason
     let right;
-  
+
     // If the top is any number but the highest one, we can actually have any value for the rest
-    if (left != CONST.MAX_64BIT_LEFT_HALF) {
+    if (left !== CONST.MAX_64BIT_LEFT_HALF) {
         right = Math.floor(Math.random() * 1000000000);
     } else {
         right = Math.floor(Math.random() * CONST.MAX_64BIT_RIGHT_HALF + 1);
     }
-  
+
     // Pad the right with zeros
-    let rightString = right.toString().padStart(9, '0');
+    const rightString = right.toString().padStart(9, '0');
   
     return left + rightString;
-  }
-  
+}
