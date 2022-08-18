6d56628da15547b01e0e9cdf60982c33fa0f082a
Srikar Parsi <srikar.parsi@expensify.com>
2022-08-18 13:41:22 -0400

    variable names

--- src/libs/NumberUtils.js
+++ src/libs/NumberUtils.js
@@ -11,16 +11,18 @@ function rand64() {
     let left = Math.floor(Math.random() * CONST.MAX_64BIT_LEFT_HALF + 1);
   
     // The right part of the max 64-bit number *+1* for the same reason
-    let rest = Math.floor(Math.random() * CONST.MAX_64BIT_RIGHT_HALF + 1);
+    let right;
   
     // If the top is any number but the highest one, we can actually have any value for the rest
     if (left != CONST.MAX_64BIT_LEFT_HALF) {
-      rest = Math.floor(Math.random() * 1000000000);
+        right = Math.floor(Math.random() * 1000000000);
+    } else {
+        right = Math.floor(Math.random() * CONST.MAX_64BIT_RIGHT_HALF + 1);
     }
   
     // Pad the right with zeros
-    let restString = rest.toString().padStart(9, '0');
+    let rightString = right.toString().padStart(9, '0');
   
-    return left + restString;
+    return left + rightString;
   }
   
