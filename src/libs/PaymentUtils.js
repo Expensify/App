443c3e24ea1b510e5f349c0706ac18736211cf8f
Justice Arthur <justice@expensify.com>
2022-08-19 09:26:58 +0000

    remove usages of formatPaymentMethod

--- src/libs/PaymentUtils.js
+++ src/libs/PaymentUtils.js
@@ -4,7 +4,6 @@ import BankAccount from './models/BankAccount';
 import * as Expensicons from '../components/Icon/Expensicons';
 import getBankIcon from '../components/Icon/BankIcons';
 import CONST from '../CONST';
-import * as Localize from './Localize';
 
 /**
  * Check to see if user has either a debit card or personal bank account added
@@ -30,7 +29,7 @@ function hasExpensifyPaymentMethod(cardList = [], bankAccountList = []) {
  * @param {Array} paymentMethodList
  * @returns {Array<PaymentMethod>}
  */
-function formatPaymentMethods2(paymentMethodList) {
+function formatPaymentMethods(paymentMethodList) {
     const paymentMethods = [...paymentMethodList];
     _.each(paymentMethods, (paymentMethod, index) => {
         switch (paymentMethod.accountType) {
@@ -61,83 +60,6 @@ function formatPaymentMethods2(paymentMethodList) {
 }
 
 /**
- * Get the PaymentMethods list
- * @param {Array} bankAccountList
- * @param {Array} cardList
- * @param {String} [payPalMeUsername='']
- * @param {Object} userWallet
- * @returns {Array<PaymentMethod>}
- */
-function formatPaymentMethods(bankAccountList, cardList, payPalMeUsername = '', userWallet) {
-    const combinedPaymentMethods = [];
-
-    _.each(bankAccountList, (bankAccount) => {
-        // Add all bank accounts besides the wallet
-        if (bankAccount.type === CONST.BANK_ACCOUNT_TYPES.WALLET) {
-            return;
-        }
-
-        const formattedBankAccountNumber = bankAccount.accountNumber
-            ? `${Localize.translateLocal('paymentMethodList.accountLastFour')} ${bankAccount.accountNumber.slice(-4)
-            }`
-            : null;
-        const isDefault = userWallet.walletLinkedAccountType === CONST.PAYMENT_METHODS.BANK_ACCOUNT && userWallet.walletLinkedAccountID === bankAccount.bankAccountID;
-        const {icon, iconSize} = getBankIcon(lodashGet(bankAccount, 'additionalData.bankName', ''));
-        combinedPaymentMethods.push({
-            title: bankAccount.addressName,
-            description: formattedBankAccountNumber,
-            methodID: bankAccount.bankAccountID,
-            icon,
-            iconSize,
-            key: `bankAccount-${bankAccount.bankAccountID}`,
-            accountType: CONST.PAYMENT_METHODS.BANK_ACCOUNT,
-            accountData: _.extend({}, bankAccount, {icon}),
-            isDefault,
-            errors: bankAccount.errors,
-            pendingAction: bankAccount.pendingAction,
-        });
-    });
-
-    _.each(cardList, (card) => {
-        const formattedCardNumber = card.cardNumber
-            ? `${Localize.translateLocal('paymentMethodList.cardLastFour')} ${card.cardNumber.slice(-4)}`
-            : null;
-        const isDefault = userWallet.walletLinkedAccountType === CONST.PAYMENT_METHODS.DEBIT_CARD && userWallet.walletLinkedAccountID === card.fundID;
-        const {icon, iconSize} = getBankIcon(card.bank, true);
-        combinedPaymentMethods.push({
-            title: card.addressName,
-            description: formattedCardNumber,
-            methodID: card.fundID,
-            icon,
-            iconSize,
-            key: `card-${card.cardNumber}`,
-            accountType: CONST.PAYMENT_METHODS.DEBIT_CARD,
-            accountData: _.extend({}, card, {icon}),
-            isDefault,
-            errors: card.errors,
-            pendingAction: card.pendingAction,
-        });
-    });
-
-    if (payPalMeUsername) {
-        combinedPaymentMethods.push({
-            title: 'PayPal.me',
-            methodID: CONST.PAYMENT_METHODS.PAYPAL,
-            description: payPalMeUsername,
-            icon: Expensicons.PayPal,
-            key: 'payPalMePaymentMethod',
-            accountType: CONST.PAYMENT_METHODS.PAYPAL,
-            accountData: {
-                username: payPalMeUsername,
-                icon: Expensicons.PayPal,
-            },
-        });
-    }
-
-    return combinedPaymentMethods;
-}
-
-/**
  * @param {Number} currentBalance, in cents
  * @param {String} methodType
  * @returns {Number} the fee, in cents
@@ -154,5 +76,4 @@ export {
     hasExpensifyPaymentMethod,
     formatPaymentMethods,
     calculateWalletTransferBalanceFee,
-    formatPaymentMethods2,
 };
