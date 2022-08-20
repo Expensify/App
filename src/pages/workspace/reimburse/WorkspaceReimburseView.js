802272cdc7fe5c74aeaf611b6fb780b347d019f3
Eugene Voloshchak <copyreading@gmail.com>
2022-08-20 19:40:09 +0300

    Localize decimal separator for the rate field

--- src/pages/workspace/reimburse/WorkspaceReimburseView.js
+++ src/pages/workspace/reimburse/WorkspaceReimburseView.js
@@ -20,12 +20,7 @@ import * as Policy from '../../../libs/actions/Policy';
 import withFullPolicy from '../withFullPolicy';
 import CONST from '../../../CONST';
 import Button from '../../../components/Button';
-import {withNetwork} from '../../../components/OnyxProvider';
-import FullPageNotFoundView from '../../../components/BlockingViews/FullPageNotFoundView';
-import OfflineWithFeedback from '../../../components/OfflineWithFeedback';
-import * as ReimbursementAccount from '../../../libs/actions/ReimbursementAccount';
-import networkPropTypes from '../../../components/networkPropTypes';
-import Log from '../../../libs/Log';
+import getPermittedDecimalSeparator from '../../../libs/getPermittedDecimalSeparator';
 
 const propTypes = {
     /** The policy ID currently being configured */
@@ -124,19 +119,29 @@ class WorkspaceReimburseView extends React.Component {
     }
 
     getRateDisplayValue(value) {
-        const numValue = parseFloat(value);
+        const numValue = this.getNumericValue(value);
         if (Number.isNaN(numValue)) {
             return '';
+        }
+        return numValue.toString().replace('.', this.props.toLocaleDigit('.')).substring(0, value.length);
+    }
+
+    getNumericValue(value) {
+        const numValue = parseFloat(value.toString().replace(this.props.toLocaleDigit('.'), '.'));
+        if (Number.isNaN(numValue)) {
+            return NaN;
         }
 
         return numValue.toFixed(3);
     }
 
     setRate(value) {
-        const isInvalidRateValue = value !== '' && !CONST.REGEX.RATE_VALUE.test(value);
+        const decimalSeparator = this.props.toLocaleDigit('.');
+        const rateValueRegex = RegExp(String.raw`^\d{1,8}([${getPermittedDecimalSeparator(decimalSeparator)}]\d{0,3})?$`, 'i');
+        const isInvalidRateValue = value !== '' && !rateValueRegex.test(value);
 
         this.setState(prevState => ({
-            unitRateValue: !isInvalidRateValue ? value : prevState.unitRateValue,
+            rateValue: !isInvalidRateValue ? this.getRateDisplayValue(value) : prevState.rateValue,
         }), () => {
             // Set the corrected value with a delay and sync to the server
             this.updateRateValueDebounced(this.state.unitRateValue);
@@ -170,22 +175,17 @@ class WorkspaceReimburseView extends React.Component {
     }
 
     updateRateValue(value) {
-        const numValue = parseFloat(value);
+        const numValue = this.getNumericValue(value);
 
         if (_.isNaN(numValue)) {
             return;
         }
 
-        this.setState({
-            unitRateValue: numValue.toFixed(3),
-        });
-
-        const distanceCustomUnit = _.find(lodashGet(this.props, 'policy.customUnits', {}), unit => unit.name === 'Distance');
-        const currentCustomUnitRate = lodashGet(distanceCustomUnit, ['rates', this.state.unitRateID], {});
-        Policy.updateCustomUnitRate(this.props.policyID, currentCustomUnitRate, this.state.unitID, {
-            ...currentCustomUnitRate,
-            rate: numValue.toFixed(3) * CONST.POLICY.CUSTOM_UNIT_RATE_BASE_OFFSET,
-        });
+        Policy.setCustomUnitRate(this.props.policyID, this.state.unitID, {
+            customUnitRateID: this.state.rateID,
+            name: this.state.rateName,
+            rate: numValue * 100,
+        }, null);
     }
 
     render() {
