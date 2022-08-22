612d0977e72e07c00a499f2bca39860711b6cefa
Yuwen Memon <yuwen@expensify.com>
2022-08-22 08:10:20 +0800

    Update copy

--- src/languages/es.js
+++ src/languages/es.js
@@ -485,7 +485,7 @@ export default {
         error: {
             invalidFormatEmailLogin: 'El email introducido no es válido. Corrígelo e inténtalo de nuevo.',
         },
-        cannotGetAccountDetails: 'No se pueden obtener los detalles de la cuenta. Por favor inténtalo de nuevo.',
+        cannotGetAccountDetails: 'No se pudo obtener el detalle de la cuenta, por favor inténtalo de nuevo.',
     },
     personalDetails: {
         error: {
