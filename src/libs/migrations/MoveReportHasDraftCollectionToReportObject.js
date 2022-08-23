9de95684bbdfadfe5565c527143a001a682666dd
Tim Golen <tgolen@gmail.com>
2022-08-23 16:28:46 +0200

    Write initial migration of data

--- src/libs/migrations/MoveReportHasDraftCollectionToReportObject.js
+++ src/libs/migrations/MoveReportHasDraftCollectionToReportObject.js
@@ -5,6 +5,46 @@ import ONYXKEYS from '../../ONYXKEYS';
 
 export default function () {
     return new Promise((resolve) => {
-        return resolve();
+        // Connect to the USER key in Onyx to get the value of expensifyNewsStatus
+        // then set that value as isSubscribedToNewsletter
+        // finally remove expensifyNewsStatus by setting the value to null
+        const connectionID = Onyx.connect({
+            key: ONYXKEYS.COLLECTION.REPORTS_WITH_DRAFT,
+            waitForCollectionCallback: true,
+            callback: (reportsWithDraftCollection) => {
+                Onyx.disconnect(connectionID);
+
+                if (!_.size(reportsWithDraftCollection)) {
+                    Log.info('[Migrate Onyx] No reports in the old draft collection');
+                    return resolve();
+                }
+
+                const reportIDsWithDrafts = [];
+                _.each(reportsWithDraftCollection, (reportHasDraft, onyxKey) => {
+                    if (reportHasDraft) {
+                        reportIDsWithDrafts.push(onyxKey.replace(ONYXKEYS.COLLECTION.REPORTS_WITH_DRAFT, ''));
+                    }
+                });
+
+                if (!reportIDsWithDrafts.length) {
+                    Log.info('[Migrate Onyx] No old reports have drafts');
+                    return resolve();
+                }
+
+                const mergeObject = _.reduce(reportIDsWithDrafts, (finalMergeObject, reportID) => {
+                    return {
+                        [`${ONYXKEYS.COLLECTION.REPORT}${reportID}`]: {hasDraft: true},
+                        ...finalMergeObject,
+                    };
+                }, {});
+
+                // eslint-disable-next-line rulesdir/prefer-actions-set-data
+                Onyx.mergeCollection(ONYXKEYS.COLLECTION.REPORT, mergeObject)
+                    .then(() => {
+                        Log.info('[Migrate Onyx] Ran migration MoveReportHasDraftCollectionToReportObject');
+                        resolve();
+                    });
+            },
+        });
     });
 }
