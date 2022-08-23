f52a5f3472741d2158a945a38a352761394fc731
Tim Golen <tgolen@gmail.com>
2022-08-23 16:08:40 +0200

    Add skeleton migration

---
+++ src/libs/migrations/MoveReportHasDraftCollectionToReportObject.js
@@ -0,0 +1,10 @@
+import Onyx from 'react-native-onyx';
+import _ from 'underscore';
+import Log from '../Log';
+import ONYXKEYS from '../../ONYXKEYS';
+
+export default function () {
+    return new Promise((resolve) => {
+        return resolve();
+    });
+}
