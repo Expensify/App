fc516d8077865545f24bbeb4ad49bfc86e635583
Robert Chen <robert@expensify.com>
2022-08-23 12:42:36 +0200

    Proper navigation when container is not ready

--- src/libs/actions/User.js
+++ src/libs/actions/User.js
@@ -215,6 +215,7 @@ function setSecondaryLoginAndNavigate(login, password) {
 function validateLogin(accountID, validateCode) {
     const isLoggedIn = !_.isEmpty(sessionAuthToken);
     const redirectRoute = isLoggedIn ? ROUTES.getReportRoute(currentlyViewedReportID) : ROUTES.HOME;
+
     Onyx.merge(ONYXKEYS.ACCOUNT, {...CONST.DEFAULT_ACCOUNT_DATA, isLoading: true});
 
     API.write('ValidateLogin', {
@@ -226,7 +227,7 @@ function validateLogin(accountID, validateCode) {
                 onyxMethod: CONST.ONYX.METHOD.MERGE,
                 key: ONYXKEYS.ACCOUNT,
                 value: {
-                    isLoading: true,
+                    isLoading: false,
                 },
             },
         ],
