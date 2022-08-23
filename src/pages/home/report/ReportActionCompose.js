3bbd0b565a7fcbc2896cbd0e283893807acf96d8
Manan Jadhav <manan.jadhav@gmail.com>
2022-08-24 04:20:51 +0530

    refactor: rename the flag to enable key actions

--- src/pages/home/report/ReportActionCompose.js
+++ src/pages/home/report/ReportActionCompose.js
@@ -573,7 +573,7 @@ class ReportActionCompose extends React.Component {
                                                 </Tooltip>
                                             </View>
                                             <PopoverMenu
-                                                enableArrowKeysActions
+                                                shouldEnableArrowKeysActions
                                                 isVisible={this.state.isMenuVisible}
                                                 onClose={() => this.setMenuVisibility(false)}
                                                 onItemSelected={() => this.setMenuVisibility(false)}
