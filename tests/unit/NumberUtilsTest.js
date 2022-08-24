2cdbd9631e5cbcf0549d84b911b89456f4d77ee4
Brandon Stites <bstites+1@expensify.com>
2022-08-24 16:56:57 +0200

    Check for the type instead of whether it is a number or not

--- tests/unit/NumberUtilsTest.js
+++ tests/unit/NumberUtilsTest.js
@@ -3,7 +3,7 @@ const NumberUtils = require('../../src/libs/NumberUtils');
 describe('libs/NumberUtils', () => {
     it('should generate a random 64-bit numeric string', () => {
         const id = NumberUtils.rand64();
-        expect(Number.isNaN(id));
+        expect(typeof id).toBe('string');
         // eslint-disable-next-line no-undef
         expect(BigInt(id)).toBeLessThan(BigInt(9223372036854775807));
         // eslint-disable-next-line no-undef
