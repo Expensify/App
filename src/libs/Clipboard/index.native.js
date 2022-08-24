23ecd49e789b09b1ca501154054855572ba49bc8
Andrew Gable <andrew@expensify.com>
2022-08-24 19:23:54 +0200

    Wrap setString instead of using Clipboard object

--- src/libs/Clipboard/index.native.js
+++ src/libs/Clipboard/index.native.js
@@ -1,7 +1,16 @@
 import Clipboard from '@react-native-community/clipboard';
 
+/**
+ * Sets a string on the Clipboard object via @react-native-community/clipboard
+ *
+ * @param {String} text
+ */
+const setString = (text) => {
+    Clipboard.setString(text);
+};
+
 export default {
-    ...Clipboard,
+    setString,
 
     // We don't want to set HTML on native platforms so noop them.
     canSetHtml: () => false,
