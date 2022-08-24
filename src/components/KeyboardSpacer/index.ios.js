be26fc349feb994373af2ddee0986c186121e6f7
Luke Donahue <luke@expensify.com>
2022-08-24 19:59:59 +0200

    missed the underscore import

--- src/components/KeyboardSpacer/index.ios.js
+++ src/components/KeyboardSpacer/index.ios.js
@@ -2,6 +2,7 @@
  * On iOS the keyboard covers the input fields on the bottom of the view. This component moves the view up with the
  * keyboard allowing the user to see what they are typing.
  */
+import _ from 'underscore';
 import React from 'react';
 import ReactNativeKeyboardSpacer from './KeyboardSpacer';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../withWindowDimensions';
