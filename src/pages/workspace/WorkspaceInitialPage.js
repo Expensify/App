6ece7b7efc762c5c91ffb4cdcb145dc6e5f16674
neil-marcellini <neil@expensify.com>
2022-08-24 13:32:09 +0200

    Style tweak

--- src/pages/workspace/WorkspaceInitialPage.js
+++ src/pages/workspace/WorkspaceInitialPage.js
@@ -84,7 +84,7 @@ class WorkspaceInitialPage extends React.Component {
 
         const hasMembersError = PolicyUtils.hasPolicyMemberError(this.props.policyMemberList);
         const hasGeneralSettingsError = !_.isEmpty(lodashGet(this.props.policy, 'errorFields.generalSettings', {}))
-        || !_.isEmpty(lodashGet(this.props.policy, 'errorFields.avatarURL', {}));
+            || !_.isEmpty(lodashGet(this.props.policy, 'errorFields.avatarURL', {}));
         const menuItems = [
             {
                 translationKey: 'workspace.common.settings',
