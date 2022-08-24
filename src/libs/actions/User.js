deb7af8fbb3d3489a3cc080b36ab8658a25f8c99
Robert Chen <robert@expensify.com>
2022-08-24 18:17:02 +0200

    errant space

--- src/libs/actions/User.js
+++ src/libs/actions/User.js
@@ -229,7 +229,7 @@ function validateLogin(accountID, validateCode) {
     API.write('ValidateLogin', {
         accountID,
         validateCode,
-    }, { defaultData, defaultData, defaultData});
+    }, {defaultData, defaultData, defaultData});
     Navigation.navigate(redirectRoute);
 }
 
