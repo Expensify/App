34ca065d3b5833b0d6b7a75372a1775a5bdf7107
Rory Abraham <rory@expensify.com>
2022-08-25 16:38:34 +0200

    Add empty string as option

--- src/components/ScreenWrapper/BaseScreenWrapper.js
+++ src/components/ScreenWrapper/BaseScreenWrapper.js
@@ -20,7 +20,7 @@ import ONYXKEYS from '../../ONYXKEYS';
 import {withNetwork} from '../OnyxProvider';
 import {propTypes, defaultProps} from './propTypes';
 
-propTypes.keyboardAvoidingViewBehavior = PropTypes.oneOf(['padding', 'height', 'position']).isRequired;
+propTypes.keyboardAvoidingViewBehavior = PropTypes.oneOf(['padding', 'height', 'position', '']).isRequired;
 
 class BaseScreenWrapper extends React.Component {
     constructor(props) {
