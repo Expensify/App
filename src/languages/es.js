607e5c5c2771cbc5a2959ebece4c616dad6210e4
Jasper Huang <jasper@expensify.com>
2022-08-25 14:05:05 +0800

    add copy

--- src/languages/es.js
+++ src/languages/es.js
@@ -832,6 +832,7 @@ export default {
             captureNoVBACopyAfterEmail: ' y descarga la App de Expensify para controlar tus gastos en efectivo sobre la marcha.',
             unlockNoVBACopy: 'Conecta una cuenta bancaria para reembolsar online a los miembros de tu espacio de trabajo.',
             fastReimbursementsVBACopy: '¡Todo listo para reembolsar recibos desde tu cuenta bancaria!',
+            updateCustomUnitError: 'Los cambios no han podido ser guardados. El espacio de trabajo ha sido modificado mientras estabas desconectado, por favor inténtalo de nuevo.',
         },
         bills: {
             manageYourBills: 'Gestiona tus facturas',
