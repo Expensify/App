44997f0cd44a0386bc9fdc3b1a8ee242ec9df3e2
Luke Donahue <luke@expensify.com>
2022-08-25 16:20:42 +0200

    ok that's better

---
+++ src/components/ScreenWrapper/index.android.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseScreenWrapper from './BaseScreenWrapper';
+import {defaultProps, propTypes} from './propTypes';
+
+const ScreenWrapper = props => (
+    <BaseScreenWrapper
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        keyboardAvoidingViewBehavior="height"
+    >
+        {props.children}
+    </BaseScreenWrapper>
+);
+ScreenWrapper.propTypes = propTypes;
+ScreenWrapper.defaultProps = defaultProps;
+
+export default ScreenWrapper;
