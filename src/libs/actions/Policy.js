6c97f10affbadf2d7da867a82cec64c143ba565e
Jasper Huang <jasper@expensify.com>
2022-08-25 14:21:27 +0800

    add back function doc

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -63,6 +63,8 @@ function getSimplifiedEmployeeList(employeeList) {
  * @param {String} fullPolicyOrPolicySummary.outputCurrency
  * @param {String} [fullPolicyOrPolicySummary.avatar]
  * @param {String} [fullPolicyOrPolicySummary.value.avatar]
+ * @param {String} [fullPolicyOrPolicySummary.avatarURL]
+ * @param {String} [fullPolicyOrPolicySummary.value.avatarURL]
  * @param {Object} [fullPolicyOrPolicySummary.value.employeeList]
  * @param {Object} [fullPolicyOrPolicySummary.value.customUnits]
  * @param {Boolean} isFromFullPolicy,
