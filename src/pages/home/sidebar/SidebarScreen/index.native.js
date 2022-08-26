7cf1017c4f1493652bd658d551f90f3750186f6d
Alberto <alberto@expensify.com>
2022-08-26 15:58:57 +0200

    remove usage

--- src/pages/home/sidebar/SidebarScreen/index.native.js
+++ src/pages/home/sidebar/SidebarScreen/index.native.js
@@ -26,8 +26,5 @@ export default compose(
         betas: {
             key: ONYXKEYS.BETAS,
         },
-        isCreatingWorkspace: {
-            key: ONYXKEYS.IS_CREATING_WORKSPACE,
-        },
     }),
 )(SidebarScreen);
