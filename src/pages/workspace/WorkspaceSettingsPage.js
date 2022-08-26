721aa482757540afb38c0b63343a0b391b2f4773
neil-marcellini <neil@expensify.com>
2022-08-26 17:52:17 +0200

    Make onClose default to a no op

--- src/pages/workspace/WorkspaceSettingsPage.js
+++ src/pages/workspace/WorkspaceSettingsPage.js
@@ -148,7 +148,6 @@ class WorkspaceSettingsPage extends React.Component {
                             </OfflineWithFeedback>
                             <OfflineWithFeedback
                                 pendingAction={lodashGet(this.props.policy, 'pendingFields.generalSettings')}
-                                onClose={() => {}}
                             >
                                 <TextInput
                                     label={this.props.translate('workspace.editor.nameInputLabel')}
