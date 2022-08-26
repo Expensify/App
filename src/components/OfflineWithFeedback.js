01b5bceec4047314b205b506d4104f770a47b48c
Jasper Huang <jasper@expensify.com>
2022-08-26 21:00:09 +0800

    remove isCloseable and store errors in errorFields.avatar

--- src/components/OfflineWithFeedback.js
+++ src/components/OfflineWithFeedback.js
@@ -45,9 +45,6 @@ const propTypes = {
     /** Additional style object for the error row */
     errorRowStyles: stylePropTypes,
 
-    /** Whether we should allow the user to close the error message */
-    isCloseable: PropTypes.bool,
-
     ...withLocalizePropTypes,
 };
 
@@ -57,7 +54,6 @@ const defaultProps = {
     onClose: () => {},
     style: [],
     errorRowStyles: [],
-    isCloseable: true,
 };
 
 /**
@@ -114,18 +110,16 @@ const OfflineWithFeedback = (props) => {
                             <Text key={i} style={styles.offlineFeedback.text}>{error}</Text>
                         ))}
                     </View>
-                    {props.isCloseable && (
-                        <Tooltip text={props.translate('common.close')}>
-                            <Pressable
-                                onPress={props.onClose}
-                                style={[styles.touchableButtonImage, styles.mr0]}
-                                accessibilityRole="button"
-                                accessibilityLabel={props.translate('common.close')}
-                            >
-                                <Icon src={Expensicons.Close} />
-                            </Pressable>
-                        </Tooltip>
-                    )}
+                    <Tooltip text={props.translate('common.close')}>
+                        <Pressable
+                            onPress={props.onClose}
+                            style={[styles.touchableButtonImage, styles.mr0]}
+                            accessibilityRole="button"
+                            accessibilityLabel={props.translate('common.close')}
+                        >
+                            <Icon src={Expensicons.Close} />
+                        </Pressable>
+                    </Tooltip>
                 </View>
             )}
         </View>
