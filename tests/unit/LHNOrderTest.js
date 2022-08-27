ab123385c20ac5e5ad030989e0928d085cef21c4
Tim Golen <tgolen@expensify.com>
2022-08-27 23:17:36 +0200

    Switch test rendering lib

--- tests/unit/LHNOrderTest.js
+++ tests/unit/LHNOrderTest.js
@@ -1,5 +1,51 @@
-describe('LHN Order', () => {
-    test('has no items', () => {
+import 'react-native';
+import React from 'react';
 
+// Note: `react-test-renderer` renderer must be required after react-native.
+import {render} from '@testing-library/react-native';
+import SideBarLinks from '../../src/pages/home/sidebar/SidebarLinks';
+
+const fakeInsets = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+};
+
+describe('Sidebar', () => {
+    test('is not rendered when there are no props passed to it', () => {
+        const sideBarLinks = render(<SideBarLinks />);
+        sideBarLinks.debug();
+        expect(sideBarLinks.toJSON()).toBe(null);
     });
+
+    // test('is not rendered when there are no props passed to it', () => {
+    //     let sideBarLinks;
+    //     act(() => {
+    //         sideBarLinks = create(<SideBarLinks
+    //             onLinkClick={() => {}}
+    //             insets={fakeInsets}
+    //             onAvatarClick={() => {}}
+    //             isSmallScreenWidth
+    //         />);
+    //     });
+    //     expect(sideBarLinks.toJSON()).toBe(null);
+    //     act(() => {
+    //         sideBarLinks.update(<SideBarLinks
+    //             onLinkClick={() => {}}
+    //             insets={fakeInsets}
+    //             onAvatarClick={() => {}}
+    //             isSmallScreenWidth
+    //             personalDetails={{
+    //                 'email1@test.com': {
+    //                     login: 'email1@test.com',
+    //                     displayName: 'Email One',
+    //                     avatar: 'none',
+    //                     firstName: 'Email',
+    //                 },
+    //             }}
+    //         />);
+    //     });
+    //     expect(sideBarLinks.toJSON()).not.toBe(null);
+    // });
 });
