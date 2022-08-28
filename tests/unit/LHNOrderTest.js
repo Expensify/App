b80572b599ca36dca56fac0e4ee0e737ade383de
Tim Golen <tgolen@expensify.com>
2022-08-28 08:06:32 +0200

    Get the tests passing

--- tests/unit/LHNOrderTest.js
+++ tests/unit/LHNOrderTest.js
@@ -1,12 +1,11 @@
-import 'react-native';
 import React from 'react';
 import Onyx from 'react-native-onyx';
-import {View} from 'react-native';
 
 // Note: `react-test-renderer` renderer must be required after react-native.
 import {render} from '@testing-library/react-native';
 import SidebarLinks from '../../src/pages/home/sidebar/SidebarLinks';
 import ONYXKEYS from '../../src/ONYXKEYS';
+import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
 
 const fakeInsets = {
     top: 0,
@@ -18,6 +17,13 @@ const fakeInsets = {
 const ONYX_KEYS = {
     PERSONAL_DETAILS: 'personalDetails',
     NVP_PREFERRED_LOCALE: 'preferredLocale',
+    CURRENTLY_VIEWED_REPORTID: 'currentlyViewedReportID',
+    NVP_PRIORITY_MODE: 'nvp_priorityMode',
+    COLLECTION: {
+        REPORT: 'report_',
+        REPORT_ACTIONS: 'reportActions_',
+        REPORTS_WITH_DRAFT: 'reportWithDraft_',
+    },
 };
 
 Onyx.init({
@@ -25,6 +31,7 @@ Onyx.init({
     registerStorageEventListener: () => {},
 });
 
+// Icons need to be explicitly mocked. The testing library throws an error when trying to render them
 jest.mock('../../src/components/Icon/Expensicons', () => ({MagnifyingGlass: () => ''}));
 
 describe('Sidebar', () => {
@@ -46,7 +53,7 @@ describe('Sidebar', () => {
         expect(sideBarLinks.toJSON()).toBe(null);
     });
 
-    test('is not rendered when there are no props passed to it', () => {
+    test('is rendered with an empty list when personal details exist', () => {
         const sideBarLinks = render(<SidebarLinks
             onLinkClick={() => {}}
             insets={fakeInsets}
@@ -62,17 +69,23 @@ describe('Sidebar', () => {
             translate={thing => thing}
         />);
         expect(sideBarLinks.toJSON()).toBe(null);
-        Onyx.multiSet({
-            [ONYX_KEYS.PERSONAL_DETAILS]: {
-                'email1@test.com': {
-                    login: 'email1@test.com',
-                    displayName: 'Email One',
-                    avatar: 'none',
-                    firstName: 'Email',
-                },
-            },
-            [ONYXKEYS.NVP_PREFERRED_LOCALE]: 'en',
-        });
-        expect(sideBarLinks.toJSON()).not.toBe(null);
+        return waitForPromisesToResolve()
+            .then(() => {
+                Onyx.multiSet({
+                    [ONYX_KEYS.PERSONAL_DETAILS]: {
+                        'email1@test.com': {
+                            login: 'email1@test.com',
+                            displayName: 'Email One',
+                            avatar: 'none',
+                            firstName: 'Email',
+                        },
+                    },
+                    [ONYXKEYS.NVP_PREFERRED_LOCALE]: 'en',
+                    [ONYXKEYS.CURRENTLY_VIEWED_REPORTID]: '1',
+                }).then(() => {
+                    expect(sideBarLinks.toJSON()).not.toBe(null);
+                });
+                return waitForPromisesToResolve();
+            });
     });
 });
