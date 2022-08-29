37389a86add45f1edfef76df376d14d4a3516426
luan <103875612+b1tjoy@users.noreply.github.com>
2022-08-30 07:56:49 +0800

    move isScrollingVertically to local variable of event handler

--- src/components/HTMLEngineProvider/HTMLRenderers/PreRenderer/index.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/PreRenderer/index.js
@@ -9,8 +9,7 @@ class PreRenderer extends React.Component {
         super(props);
 
         this.scrollNode = this.scrollNode.bind(this);
-        this.isVerticalScrolling = false;
-        this.debouncedGetScrollDirection = _.debounce(this.getScrollDirection.bind(this), 100, true);
+        this.debouncedIsScrollingVertically = _.debounce(this.isScrollingVertically.bind(this), 100, true);
     }
 
     componentDidMount() {
@@ -27,14 +26,15 @@ class PreRenderer extends React.Component {
     }
 
     /**
-     * Get scroll direction based on deltaX and deltaY. We debounce this
+     * Check if user is scrolling vertically based on deltaX and deltaY. We debounce this
      * method in the constructor to make sure it's called only for the first event.
      * @param {WheelEvent} event Wheel event
+     * @returns {Boolean} true if user is scrolling vertically
      */
-    getScrollDirection(event) {
+    isScrollingVertically(event) {
         // Mark as vertical scrolling only when absolute value of deltaY is more than the double of absolute
         // value of deltaX, so user can use trackpad scroll on the code block horizontally at a wide angle.
-        this.isVerticalScrolling = Math.abs(event.deltaY) > (Math.abs(event.deltaX) * 2);
+        return Math.abs(event.deltaY) > (Math.abs(event.deltaX) * 2);
     }
 
     /**
@@ -44,8 +44,8 @@ class PreRenderer extends React.Component {
     scrollNode(event) {
         const node = this.ref.getScrollableNode();
         const horizontalOverflow = node.scrollWidth > node.offsetWidth;
-        this.debouncedGetScrollDirection(event);
-        if ((event.currentTarget === node) && horizontalOverflow && !this.isVerticalScrolling) {
+        const isScrollingVertically = this.debouncedIsScrollingVertically(event);
+        if ((event.currentTarget === node) && horizontalOverflow && !isScrollingVertically) {
             node.scrollLeft += event.deltaX;
             event.preventDefault();
             event.stopPropagation();
