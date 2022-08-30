087214f9083ded782cd2cdeaf77f1fa22399aaeb
Alberto <alberto@expensify.com>
2022-08-30 15:02:11 +0200

    style

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -531,8 +531,8 @@ function getOptions(reports, personalDetails, activeReportID, {
             // If the report is pinned and we are using the option to display pinned reports on top then we need to
             // collect the pinned reports so we can sort them alphabetically once they are collected. We want to skip
             // default archived rooms.
-            if (prioritizePinnedReports && reportOption.isPinned &&
-                !(reportOption.isArchivedRoom && reportOption.isDefaultRoom)) {
+            if (prioritizePinnedReports && reportOption.isPinned
+                && !(reportOption.isArchivedRoom && reportOption.isDefaultRoom)) {
                 pinnedReportOptions.push(reportOption);
             } else if (prioritizeIOUDebts && reportOption.hasOutstandingIOU && !reportOption.isIOUReportOwner) {
                 iouDebtReportOptions.push(reportOption);
