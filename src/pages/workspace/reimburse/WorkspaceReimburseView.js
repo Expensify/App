fa1858a2e87491f023d2740a97ef63919536b03a
Yuwen Memon <yuwen@expensify.com>
2022-08-30 19:05:07 +0200

    Fix even more conflicts

--- src/pages/workspace/reimburse/WorkspaceReimburseView.js
+++ src/pages/workspace/reimburse/WorkspaceReimburseView.js
@@ -246,9 +246,9 @@ class WorkspaceReimburseView extends React.Component {
                                 onPress={() => Navigation.navigate(ROUTES.getWorkspaceBankAccountRoute(this.props.policyID))}
                                 icon={Expensicons.Bank}
                                 style={[styles.mt4]}
-                                iconStyles={[styles.mr5]}
+                                iconStyles={[styles.buttonCTAIcon]}
                                 shouldShowRightIcon
-                                extraLarge
+                                large
                                 success
                             />
                         </Section>
