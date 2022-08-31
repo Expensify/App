8e36be90815fe6a3decf6d3ee545ab47763b44ca
Andrew Gable <andrew@expensify.com>
2022-08-31 17:44:44 +0200

    Fix lint issues

--- .github/actions/javascript/contributorChecklist/index.js
+++ .github/actions/javascript/contributorChecklist/index.js
@@ -109,16 +109,16 @@ const completedContributorPlusChecklist = `- [x] I have verified the author chec
 const issue = github.context.payload.issue ? github.context.payload.issue.number : github.context.payload.pull_request.number;
 const combinedData = [];
 
-function printUncheckedItems(result, expected) {
+function printUncheckedItems(result) {
     const checklist = result.split('\n');
 
-    for (const line of checklist) {
+    checklist.forEach((line) => {
         // Provide a search string with the first 30 characters to figure out if the checkbox item is in the checklist
         const lineSearchString = line.replace('- [ ] ', '').slice(0, 30);
         if (line.includes('- [ ]') && (completedContributorChecklist.includes(lineSearchString) || completedContributorPlusChecklist.includes(lineSearchString))) {
             console.log(`Unchecked checklist item: ${line}`);
         }
-    }
+    });
 }
 
 // Get all user text from the pull request, review comments, and pull request comments
