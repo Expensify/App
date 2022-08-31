f192ac2a2c63fbfd261aec2ad3dce44acc41e60a
Marco Chávez <marco@expensify.com>
2022-08-31 14:17:49 +0200

    remove clientID params from addActions function

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -818,8 +818,6 @@ function addActions(reportID, text = '', file) {
         reportActionID: file ? attachmentAction.reportActionID : reportCommentAction.reportActionID,
         commentReportActionID: file && reportCommentAction ? reportCommentAction.reportActionID : null,
         reportComment: reportCommentText,
-        clientID: lastAction.clientID,
-        commentClientID: lodashGet(reportCommentAction, 'clientID', ''),
         file,
     };
 
