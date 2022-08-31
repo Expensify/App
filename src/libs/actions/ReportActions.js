2f9de3d5532c7d4d01f95a863299d14349c4baaf
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-08-31 17:42:29 +0200

    Remove lastVisibleReportAction export

--- src/libs/actions/ReportActions.js
+++ src/libs/actions/ReportActions.js
@@ -124,7 +124,6 @@ function clearReportActionErrors(reportID, sequenceNumber) {
 export {
     getDeletedCommentsCount,
     getLastVisibleMessageText,
-    getLastVisibleReportAction,
     clearReportActionErrors,
     isFromCurrentUser,
     deleteOptimisticReportAction,
