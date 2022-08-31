6ac5899bf405a32a9a2b00ec4601450759eb45eb
Jasper Huang <jasper@expensify.com>
2022-08-31 17:16:16 +0800

    move sorting logic into DotIndicatorMessage

--- src/components/DotIndicatorMessage.js
+++ src/components/DotIndicatorMessage.js
@@ -11,7 +11,10 @@ import Text from './Text';
 
 const propTypes = {
     /** The messages to display  */
-    messages: PropTypes.arrayOf(PropTypes.string),
+    messages: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.string),
+        PropTypes.objectOf(PropTypes.string),
+    ]),
 
     /** The type of message, 'error' shows a red dot, 'success' shows a green dot */
     type: PropTypes.oneOf(['error', 'success']).isRequired,
@@ -31,13 +34,24 @@ const DotIndicatorMessage = (props) => {
     if (_.isEmpty(props.messages)) {
         return null;
     }
+
+    // If the messages are passed as an object (such as our Onyx errors), then grab the sorted values
+    let sortedMessages = props.messages;
+    if (!_.isArray(props.messages)) {
+        sortedMessages = _.chain(props.messages)
+            .keys()
+            .sortBy()
+            .map(key => props.messages[key])
+            .value();
+    }
+
     return (
         <View style={[styles.dotIndicatorMessage, ...props.style]}>
             <View style={styles.offlineFeedback.errorDot}>
                 <Icon src={Expensicons.DotIndicator} fill={props.type === 'error' ? colors.red : colors.green} height={variables.iconSizeSmall} width={variables.iconSizeSmall}/>
             </View>
             <View style={styles.offlineFeedback.textContainer}>
-                {_.map(props.messages, (message, i) => (
+                {_.map(sortedMessages, (message, i) => (
                     <Text key={i} style={styles.offlineFeedback.text}>{message}</Text>
                 ))}
             </View>
