a2cd886a9de28f6e635d98cd26161b75b74cdd55
Francois Laithier <francois@expensify.com>
2022-09-01 18:25:15 +0200

    Clear errors in LHN

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1530,6 +1530,15 @@ function viewNewReportAction(reportID, action) {
     });
 }
 
+/**
+ * Clear the errors associated with the IOUs of a given report.
+ *
+ * @param {Number} reportID
+ */
+function clearIOUError(reportID) {
+    Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {errorFields: {iou: null}});
+}
+
 // We are using this map to ensure actions are only handled once
 const handledReportActions = {};
 Onyx.connect({
@@ -1607,4 +1616,5 @@ export {
     createOptimisticChatReport,
     updatePolicyRoomName,
     clearPolicyRoomNameErrors,
+    clearIOUError,
 };
