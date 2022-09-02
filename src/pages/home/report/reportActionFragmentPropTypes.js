3199d389341282069a281a6f11da7727d7c87ad0
Paulo Vale <paulo@expensify.com>
2022-09-02 12:44:04 +0200

    Add style to reportActionFragment propTypes

--- src/pages/home/report/reportActionFragmentPropTypes.js
+++ src/pages/home/report/reportActionFragmentPropTypes.js
@@ -25,4 +25,10 @@ export default PropTypes.shape({
 
     // An additional avatar url - not the main avatar url but used within a message.
     iconUrl: PropTypes.string,
+
+    // Additional styles to add after local styles
+    style: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.object),
+        PropTypes.object,
+    ]),
 });
