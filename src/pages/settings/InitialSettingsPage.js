2d6c15c62a8d5513d8d0289ef086bc67fd2de0d4
Puneet Lath <puneet@expensify.com>
2022-09-02 12:02:49 +0200

    Remove accidental console statement

--- src/pages/settings/InitialSettingsPage.js
+++ src/pages/settings/InitialSettingsPage.js
@@ -184,7 +184,6 @@ class InitialSettingsPage extends React.Component {
 
     getMenuItem(item, index) {
         const keyTitle = item.translationKey ? this.props.translate(item.translationKey) : item.title;
-        console.log("🚀 ~ file: InitialSettingsPage.js ~ line 187 ~ InitialSettingsPage ~ getMenuItem ~ keyTitle_index", `${keyTitle}_${index}`)
         const isPaymentItem = item.translationKey === 'common.payments';
 
         if (item.isPolicy) {
