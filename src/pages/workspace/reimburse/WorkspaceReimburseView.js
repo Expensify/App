39c5042941e50c8aaebcfb448c95d18938caccd6
Jasper Huang <jasper@expensify.com>
2022-09-05 10:00:08 +0100

    fix stuff messed up in merge

--- src/pages/workspace/reimburse/WorkspaceReimburseView.js
+++ src/pages/workspace/reimburse/WorkspaceReimburseView.js
@@ -98,13 +98,13 @@ class WorkspaceReimburseView extends React.Component {
                 .values()
                 .findWhere({name: CONST.CUSTOM_UNITS.NAME_DISTANCE})
                 .value();
-
+            const customUnitRate = _.find(lodashGet(distanceCustomUnit, 'onyxRates', {}), rate => rate.name === 'Default Rate');
             this.setState({
                 unitID: lodashGet(distanceCustomUnit, 'customUnitID', ''),
                 unitName: lodashGet(distanceCustomUnit, 'name', ''),
                 unitValue: lodashGet(distanceCustomUnit, 'attributes.unit', 'mi'),
-                unitRateID: lodashGet(distanceCustomUnit, 'rates[0].customUnitRateID', ''),
-                rateValue: this.getRateDisplayValue(lodashGet(distanceCustomUnit, 'rates[0].rate', 0) / 100),
+                unitRateID: lodashGet(customUnitRate, 'customUnitRateID'),
+                rateValue: this.getRateDisplayValue(lodashGet(customUnitRate, 'rate', 0) / 100),
             });
         }
 
