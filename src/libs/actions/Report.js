7795cac7006fab9b34d6f45f68929552271ad302
neil-marcellini <neil@expensify.com>
2022-09-07 13:02:22 +0100

    Access the policy id properly

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1445,7 +1445,7 @@ function addWorkspaceRoom(policy, reportName, visibility) {
         policy.emailList,
         reportName,
         CONST.REPORT.CHAT_TYPE.POLICY_ROOM,
-        policy.policyID,
+        policy.id,
         CONST.REPORT.OWNER_EMAIL_FAKE,
         false,
         '',
