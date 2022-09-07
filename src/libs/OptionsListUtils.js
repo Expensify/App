c04ddd4fd9c2b5a7ef44abbdfb4367b654146f6a
Jack Nam <jack@expensify.com>
2022-09-07 10:00:39 +0100

    update locations where this is used

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -444,7 +444,7 @@ function getOptions(reports, personalDetails, activeReportID, {
         }
 
         // We let Free Plan default rooms to be shown in the App - it's the one exception to the beta, otherwise do not show policy rooms in product
-        if (ReportUtils.isDefaultRoom(report) && !Permissions.canUseDefaultRooms(betas) && ReportUtils.getPolicyType(report, policies) !== CONST.POLICY.TYPE.FREE) {
+        if (ReportUtils.isDefaultRoom(report) && !ReportUtils.hasExpensifyGuidesEmails(logins) && !Permissions.canUseDefaultRooms(betas) && ReportUtils.getPolicyType(report, policies) !== CONST.POLICY.TYPE.FREE) {
             return;
         }
 
