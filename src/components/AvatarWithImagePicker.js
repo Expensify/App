689a0ca94069517516f1850201592e0cd817a097
sahil <thesahindia@gmail.com>
2022-09-15 00:49:14 +0530

    create a function to check if attachment extension is allowed for avatar

--- src/components/AvatarWithImagePicker.js
+++ src/components/AvatarWithImagePicker.js
@@ -20,6 +20,7 @@ import CONST from '../CONST';
 import SpinningIndicatorAnimation from '../styles/animation/SpinningIndicatorAnimation';
 import Tooltip from './Tooltip';
 import stylePropTypes from '../styles/stylePropTypes';
+import * as FileUtils from '../libs/fileDownload/FileUtils';
 
 const propTypes = {
     /** Avatar URL to display */
@@ -122,6 +123,17 @@ class AvatarWithImagePicker extends React.Component {
 
     hideErrorModal() {
         this.setState({isErrorModalVisible: false});
+    }
+
+    /**
+     * Check if the attachment extension is allowed.
+     *
+     * @param {Object} image
+     * @returns {Boolean}
+     */
+    isValidExtension(image) {
+        const {fileExtension} = FileUtils.splitExtensionFromFileName(image.name);
+        return _.contains(CONST.AVATAR_ALLOWED_EXTENSIONS, fileExtension);
     }
 
     /**
