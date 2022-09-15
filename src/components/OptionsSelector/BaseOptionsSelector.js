59e5b43d593c792188fcbe96207540a2a77d658f
Rushat Gabhane <rushatgabhane@gmail.com>
2022-09-16 05:08:47 +0530

    move selection to state

--- src/components/OptionsSelector/BaseOptionsSelector.js
+++ src/components/OptionsSelector/BaseOptionsSelector.js
@@ -45,6 +45,10 @@ class BaseOptionsSelector extends Component {
         this.state = {
             allOptions,
             focusedIndex: this.props.shouldTextInputAppearBelowOptions ? allOptions.length : 0,
+            selection: {
+                start: this.props.value.length,
+                end: this.props.value.length,
+            },
         };
     }
 
@@ -206,7 +210,7 @@ class BaseOptionsSelector extends Component {
     selectRow(option, ref) {
         if (this.props.shouldFocusOnSelectRow) {
             // Input is permanently focused on native platforms, so we always highlight the text inside of it
-            this.textInput.setNativeProps({selection: {start: 0, end: this.props.value.length}});
+            this.setState({selection: {start: 0, end: this.props.value.length}});
             if (this.relatedTarget && ref === findNodeHandle(this.relatedTarget)) {
                 this.textInput.focus();
             }
@@ -251,6 +255,8 @@ class BaseOptionsSelector extends Component {
                 }}
                 selectTextOnFocus
                 blurOnSubmit={Boolean(this.state.allOptions.length)}
+                selection={this.state.selection}
+                onSelectionChange={e => this.setState({selection: e.nativeEvent.selection})}
             />
         );
         const optionsList = this.props.shouldShowOptions ? (
