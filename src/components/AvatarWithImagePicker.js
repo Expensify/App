a9767e182292d9c619180a9d974598a31c3452ca
Sahil <thesahindia@gmail.com>
2022-09-16 21:43:00 +0530

    handle null case for the image
    Co-authored-by: Manan <manan.jadhav@gmail.com>

--- src/components/AvatarWithImagePicker.js
+++ src/components/AvatarWithImagePicker.js
@@ -132,7 +132,7 @@ class AvatarWithImagePicker extends React.Component {
      * @returns {Boolean}
      */
     isValidExtension(image) {
-        const fileExtension = lodashGet(FileUtils.splitExtensionFromFileName(image.name), 'fileExtension', '');
+      const {fileExtension} = FileUtils.splitExtensionFromFileName(lodashGet(image, 'name', ''));
         return _.contains(CONST.AVATAR_ALLOWED_EXTENSIONS, fileExtension.toLowerCase());
     }
 
