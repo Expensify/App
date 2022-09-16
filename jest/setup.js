b3ab1ba3c7c0a99d9604f1df0562f7684f468222
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-09-15 20:03:49 -1000

    improve comment and add explanation for icon name setting

--- jest/setup.js
+++ jest/setup.js
@@ -62,11 +62,10 @@ jest.mock('../src/components/Icon/Expensicons', () => {
     const reduce = require('underscore').reduce;
     const Expensicons = jest.requireActual('../src/components/Icon/Expensicons');
     return reduce(Expensicons, (prev, _curr, key) => {
+        // We set the name of the anonymous mock function here so we can dynamically build the list of mocks and access the
+        // "name" property to use in accessibility hints for element querying
         const fn = () => '';
-        Object.defineProperty(fn, 'name', {
-            value: key,
-            configurable: true,
-        });
+        Object.defineProperty(fn, 'name', {value: key});
         return {...prev, [key]: fn};
     }, {});
 });
