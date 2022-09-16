cb43a44a43b36d46bff9343fb40423cbc8bf6fa4
aimane-chnaif <aiman.chnaif@gmail.com>
2022-09-16 03:22:17 -0600

    rename isNewChat to isEmptyChat

--- src/pages/home/report/ReportActionCompose.js
+++ src/pages/home/report/ReportActionCompose.js
@@ -195,7 +195,7 @@ class ReportActionCompose extends React.Component {
         ReportActionComposeFocusManager.clear();
     }
 
-    isNewChat() {
+    isEmptyChat() {
         return _.size(this.props.reportActions) === 1;
     }
 
@@ -255,7 +255,7 @@ class ReportActionCompose extends React.Component {
             return this.props.translate('reportActionCompose.blockedFromConcierge');
         }
 
-        if (this.isNewChat()) {
+        if (this.isEmptyChat()) {
             return this.props.translate('reportActionCompose.sayHello');
         }
 
@@ -603,7 +603,7 @@ class ReportActionCompose extends React.Component {
                                 </AttachmentPicker>
                                 <View style={styles.textInputComposeSpacing}>
                                     <Composer
-                                        autoFocus={!this.props.modal.isVisible && (this.shouldFocusInputOnScreenFocus || this.isNewChat())}
+                                        autoFocus={!this.props.modal.isVisible && (this.shouldFocusInputOnScreenFocus || this.isEmptyChat())}
                                         multiline
                                         ref={this.setTextInputRef}
                                         textAlignVertical="top"
