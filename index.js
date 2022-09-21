0c77d81b6c48a4eb6d54505c91177824dfa1f900
Szymon <szymon20000@gmail.com>
2022-09-21 21:57:15 +0200

    stash work

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
+import _ from 'lodash';
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
 additionalAppSetup();
+
+/*const array = new Array(1e4).fill(null).map(() => ({a: 5}));
+
+function measure(fun) {
+  const before = performance.now();
+  for (let i = 0; i < 100; ++i) {
+    fun();
+  }
+  const after = performance.now();
+  return after - before;
+}
+
+console.log('bbb lodash', measure(() => { 
+  let x = 0;
+  _.each(array, (i) => { x += i.a});
+}));
+
+console.log('bbb forEach', measure(() => { 
+  let x = 0;
+  array.forEach((i) => { x += i.a}); 
+}));*/
