51ac6c490288935085b9f99d1566306f994d2267
Tim Golen <tgolen@expensify.com>
2022-09-21 16:09:53 -0600

    Move test to the proper suite

--- tests/utils/LHNUtils.js
+++ tests/utils/LHNUtils.js
@@ -3,6 +3,82 @@ import {render} from '@testing-library/react-native';
 import {LocaleContextProvider} from '../../src/components/withLocalize';
 import SidebarLinks from '../../src/pages/home/sidebar/SidebarLinks';
 
+const TEST_MAX_SEQUENCE_NUMBER = 10;
+
+const fakePersonalDetails = {
+    'email1@test.com': {
+        login: 'email1@test.com',
+        displayName: 'Email One',
+        avatar: 'none',
+        firstName: 'One',
+    },
+    'email2@test.com': {
+        login: 'email2@test.com',
+        displayName: 'Email Two',
+        avatar: 'none',
+        firstName: 'Two',
+    },
+    'email3@test.com': {
+        login: 'email3@test.com',
+        displayName: 'Email Three',
+        avatar: 'none',
+        firstName: 'Three',
+    },
+    'email4@test.com': {
+        login: 'email4@test.com',
+        displayName: 'Email Four',
+        avatar: 'none',
+        firstName: 'Four',
+    },
+    'email5@test.com': {
+        login: 'email5@test.com',
+        displayName: 'Email Five',
+        avatar: 'none',
+        firstName: 'Five',
+    },
+    'email6@test.com': {
+        login: 'email6@test.com',
+        displayName: 'Email Six',
+        avatar: 'none',
+        firstName: 'Six',
+    },
+    'email7@test.com': {
+        login: 'email7@test.com',
+        displayName: 'Email Seven',
+        avatar: 'none',
+        firstName: 'Seven',
+    },
+    'email8@test.com': {
+        login: 'email8@test.com',
+        displayName: 'Email Eight',
+        avatar: 'none',
+        firstName: 'Eight',
+    },
+    'email9@test.com': {
+        login: 'email9@test.com',
+        displayName: 'Email Nine',
+        avatar: 'none',
+        firstName: 'Nine',
+    },
+};
+
+let lastFakeReportID = 0;
+
+/**
+ * @param {String[]} participants
+ * @returns {Object}
+ */
+function getFakeReport(participants = []) {
+    return {
+        reportID: ++lastFakeReportID,
+        reportName: 'Report',
+        maxSequenceNumber: TEST_MAX_SEQUENCE_NUMBER,
+        lastReadSequenceNumber: TEST_MAX_SEQUENCE_NUMBER,
+        lastMessageTimestamp: Date.now(),
+        participants,
+    };
+}
+
 function getDefaultRenderedSidebarLinks() {
     // An ErrorBoundary needs to be added to the rendering so that any errors that happen while the component
     // renders are logged to the console. Without an error boundary, Jest only reports the error like "The above error
@@ -50,6 +126,8 @@ function getDefaultRenderedSidebarLinks() {
 }
 
 export {
-    // eslint-disable-next-line import/prefer-default-export
+    fakePersonalDetails,
     getDefaultRenderedSidebarLinks,
+    getFakeReport,
+    TEST_MAX_SEQUENCE_NUMBER,
 };
