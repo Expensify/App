a770eeae4cfc1c50a1209b32e81d5242a27c78e0
Youssef Lourayad <youssef@expensify.com>
2022-09-22 22:32:36 +0100

    Fix typo

--- src/pages/ReimbursementAccount/ValidationStep.js
+++ src/pages/ReimbursementAccount/ValidationStep.js
@@ -51,7 +51,7 @@ class ValidationStep extends React.Component {
 
     /**
      * @param {Object} values - form input values passed by the Form component
-     * @returns {Ojbect}
+     * @returns {Object}
      */
     validate(values) {
         const errors = {};
