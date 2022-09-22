a8a8d405416258d4f5db689c67744e3b8f201603
Youssef Lourayad <youssef@expensify.com>
2022-09-23 00:44:54 +0100

    Use new format of server errors

--- src/libs/actions/BankAccounts.js
+++ src/libs/actions/BankAccounts.js
@@ -121,7 +121,7 @@ function addPersonalBankAccount(account, password) {
                 key: ONYXKEYS.PERSONAL_BANK_ACCOUNT,
                 value: {
                     isLoading: true,
-                    error: '',
+                    errors: null,
                 },
             },
         ],
@@ -131,7 +131,7 @@ function addPersonalBankAccount(account, password) {
                 key: ONYXKEYS.PERSONAL_BANK_ACCOUNT,
                 value: {
                     isLoading: false,
-                    error: '',
+                    errors: null,
                     shouldShowSuccess: true,
                 },
             },
@@ -142,7 +142,9 @@ function addPersonalBankAccount(account, password) {
                 key: ONYXKEYS.PERSONAL_BANK_ACCOUNT,
                 value: {
                     isLoading: false,
-                    error: Localize.translateLocal('paymentsPage.addBankAccountFailure'),
+                    errors: {
+                        [DateUtils.getMicroseconds()]: Localize.translateLocal('paymentsPage.addBankAccountFailure'),
+                    },
                 },
             },
         ],
