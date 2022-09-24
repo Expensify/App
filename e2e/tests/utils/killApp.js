5b7bd4199ddf8d7ca7515a731e32b017e81a2bcf
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-09-24 12:50:25 +0200

    fix add desc back
    Signed-off-by: Hanno J. Gödecke <die.drei99@yahoo.de>

---
+++ e2e/tests/utils/killApp.js
@@ -0,0 +1,11 @@
+const {execSync} = require('node:child_process');
+const {APP_PACKAGE} = require('../../config');
+
+module.exports = function (platform = 'android') {
+    if (platform !== 'android') {
+        throw new Error(`killApp() missing implementation for platform: ${platform}`);
+    }
+
+    // use adb to kill the app
+    execSync(`adb shell am force-stop ${APP_PACKAGE}`);
+};
