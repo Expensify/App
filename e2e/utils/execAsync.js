184b3104d1876f80263b274a189a3f6ebc004d81
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-09-26 19:13:13 +0200

    fix: maxBuffer: [ERR_CHILD_PROCESS_STDIO_MAXBUFFER]: stdout maxBuffer length exceeded
    Signed-off-by: Hanno J. Gödecke <die.drei99@yahoo.de>

--- e2e/utils/execAsync.js
+++ e2e/utils/execAsync.js
@@ -5,6 +5,7 @@ module.exports = command => new Promise((resolve, reject) => {
     Logger.log('Output of command:', command);
     exec(command, {
         encoding: 'utf8',
+        maxBuffer: 1024 * 1024 * 10, // increase max buffer to 10MB, to avoid errors
     }, (error, stdout) => {
         if (error) {
             Logger.log(`failed with error: ${error}`);
