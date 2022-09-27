fa91aa99a7b4441e5c4ec8fc19edcd3c54d86e84
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-09-27 18:51:54 +0200

    increase interaction timeout to 2 minutes
    Signed-off-by: Hanno J. Gödecke <die.drei99@yahoo.de>

--- e2e/utils/withFailTimeout.js
+++ e2e/utils/withFailTimeout.js
@@ -1,9 +1,11 @@
 const {INTERACTION_TIMEOUT} = require('../config');
 
+const TIMEOUT = process.env.INTERACTION_TIMEOUT || INTERACTION_TIMEOUT;
+
 const withFailTimeout = (promise, name) => {
     const timeoutId = setTimeout(() => {
         throw new Error(`[${name}] Interaction timed out`);
-    }, INTERACTION_TIMEOUT);
+    }, Number(TIMEOUT));
     return promise.finally(() => clearTimeout(timeoutId));
 };
 
