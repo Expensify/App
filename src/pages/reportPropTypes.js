88bf4d750d4d93f622350da1af2465b6defb46d1
Tim Golen <tgolen@expensify.com>
2022-09-27 15:15:44 -0600

    Use new report prop types in the report actions view

--- src/pages/reportPropTypes.js
+++ src/pages/reportPropTypes.js
@@ -11,5 +11,14 @@ export default PropTypes.shape({
     icons: PropTypes.arrayOf(PropTypes.string),
 
     /** ID of the report */
-    reportID: PropTypes.string,
+    reportID: PropTypes.string.isRequired,
+
+    /** The largest sequenceNumber on this report */
+    maxSequenceNumber: PropTypes.number,
+
+    /** Whether there is an outstanding amount in IOU */
+    hasOutstandingIOU: PropTypes.bool,
+
+    /** Are we loading more report actions? */
+    isLoadingMoreReportActions: PropTypes.bool,
 });
