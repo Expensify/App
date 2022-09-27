f0ee196e4e7cab6308aa45e8d14d139172feb8a8
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-09-27 17:31:39 +0200

    reduce logging
    Signed-off-by: Hanno J. Gödecke <die.drei99@yahoo.de>

--- e2e/utils/logger.js
+++ e2e/utils/logger.js
@@ -6,6 +6,10 @@ const setLogLevelVerbose = (value) => {
     isVerbose = value;
 };
 
+// on CI systems when using .progressInfo, the current line won't reset but a new line gets added
+// which can flood the logs. You can increase this rate to mitigate this effect.
+const LOGGER_PROGRESS_REFRESH_RATE = process.env.LOGGER_PROGRESS_REFRESH_RATE || 250;
+
 const log = (...args) => {
     if (isVerbose) {
         console.debug(...args);
@@ -28,7 +32,7 @@ const progressInfo = (textParam) => {
         process.stdout.write(`\r${getTexts()[i++]}`);
         // eslint-disable-next-line no-bitwise
         i &= 3;
-    }, 250);
+    }, Number(LOGGER_PROGRESS_REFRESH_RATE));
 
     return {
         updateText: (newText) => {
