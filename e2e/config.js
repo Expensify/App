32d7065cb852bdec5637b30fe0ed7acdf87ce58a
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-09-27 15:12:57 +0200

    Revert "wip: UNDO ME"
    This reverts commit e9d2109b84cedfbf94d423700019f2e04bea37ee.

--- e2e/config.js
+++ e2e/config.js
@@ -8,7 +8,6 @@ const OUTPUT_DIR = 'e2e/.results';
 // add your test name here …
 const TEST_NAMES = {
     AppStartTime: 'App start time',
-    AnotherTest: 'Another test',
 };
 
 module.exports = {
@@ -49,9 +48,6 @@ module.exports = {
             name: TEST_NAMES.AppStartTime,
 
             // ... any additional config you might need
-        },
-        [TEST_NAMES.AnotherTest]: {
-            name: TEST_NAMES.AnotherTest,
         },
     },
 };
--- src/libs/E2E/tests/anotherTest.e2e.js
+++
@@ -1,8 +0,0 @@
-import E2EClient from '../client';
-
-const test = () => E2EClient.submitTestResults({
-    name: 'DummyMetrics',
-    duration: '123',
-}).then(E2EClient.submitTestDone);
-
-export default test;
