2dc540ebd4501a860b4c56f0bc6e63cbf7f11b7c
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-09-28 07:39:02 +0200

    increase and log timeout
    Signed-off-by: Hanno J. Gödecke <die.drei99@yahoo.de>

--- e2e/utils/withFailTimeout.js
+++ e2e/utils/withFailTimeout.js
@@ -4,7 +4,7 @@ const TIMEOUT = process.env.INTERACTION_TIMEOUT || INTERACTION_TIMEOUT;
 
 const withFailTimeout = (promise, name) => {
     const timeoutId = setTimeout(() => {
-        throw new Error(`[${name}] Interaction timed out`);
+        throw new Error(`[${name}] Interaction timed out after ${(TIMEOUT / 1000).toFixed(0)}s`);
     }, Number(TIMEOUT));
     return promise.finally(() => clearTimeout(timeoutId));
 };
