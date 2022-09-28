d11ff308cd7542e3ad57f1348d00c51a081d7724
Carlos Martins <cmartins@expensify.com>
2022-09-28 15:24:40 -0600

    add conditional

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -260,7 +260,7 @@ class ReportScreen extends React.Component {
                         onNavigationMenuButtonClicked={() => Navigation.navigate(ROUTES.HOME)}
                     />
                 </OfflineWithFeedback>
-                {ReportUtils.isConciergeChatReport(this.props.report) && this.state.isBannerVisible && (
+                {this.props.accountManagerReportID && ReportUtils.isConciergeChatReport(this.props.report) && this.state.isBannerVisible && (
                     <Banner
                         containerStyles={[styles.mh4, styles.mt4, styles.p4, styles.bgDark]}
                         textStyles={[styles.colorReversed]}
