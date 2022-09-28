9bcdcbaf5bd3101dff3214ba2ad5c33d39ca5f42
Szymon Kapała <szymon20000@gmail.com>
2022-09-28 09:18:09 +0200

    10000

--- tests/ui/UnreadIndicatorsTest.js
+++ tests/ui/UnreadIndicatorsTest.js
@@ -26,7 +26,7 @@ beforeAll(() => {
     // simulate data arriving we will just set it into Onyx directly with Onyx.merge() or Onyx.set() etc.
     global.fetch = TestHelper.getGlobalFetchMock();
 
-    jest.setTimeout(6000);
+    jest.setTimeout(10000);
     Linking.setInitialURL('https://new.expensify.com/r/1');
     appSetup();
 });
