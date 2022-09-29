26448a3b612c89d8b16dd224f368039ee14cb948
tienifr <christianwen18@gmail.com>
2022-09-30 00:05:15 +0700

    fix/11157-Unable-to-save-first-name-last-name-and-time-zone-when-user-is-offline

--- src/components/Form.js
+++ src/components/Form.js
@@ -45,6 +45,8 @@ const propTypes = {
     // eslint-disable-next-line react/forbid-prop-types
     draftValues: PropTypes.object,
 
+    enabledWhenOffline: PropTypes.bool,
+
     ...withLocalizePropTypes,
 };
 
@@ -55,6 +57,7 @@ const defaultProps = {
         errors: null,
     },
     draftValues: {},
+    enabledWhenOffline: false,
 };
 
 class Form extends React.Component {
@@ -206,6 +209,7 @@ class Form extends React.Component {
                                 this.inputRefs[_.first(_.keys(this.state.errors))].focus();
                             }}
                             containerStyles={[styles.mh0, styles.mt5]}
+                            enabledWhenOffline={this.props.enabledWhenOffline}
                         />
                         )}
                     </View>
