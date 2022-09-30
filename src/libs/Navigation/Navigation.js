7c3ed8aa1ff1ef5571031d0779cc9d4b48c43f39
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-09-29 16:13:27 -1000

    Get reportID from route instead of saving it in the drawer

--- src/libs/Navigation/Navigation.js
+++ src/libs/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import _ from 'underscore';
+import lodashGet from 'lodash/get';
 import {Keyboard} from 'react-native';
 import {DrawerActions, getPathFromState, StackActions} from '@react-navigation/native';
 import Onyx from 'react-native-onyx';
@@ -178,6 +179,19 @@ function getActiveRoute() {
 }
 
 /**
+ * @returns {String}
+ */
+function getReportIDFromRoute() {
+    if (!navigationRef.current) {
+        return '';
+    }
+
+    const drawerState = lodashGet(navigationRef.current.getState(), ['routes', 0, 'state']);
+    const reportRoute = lodashGet(drawerState, ['routes', 0]);
+    return lodashGet(reportRoute, ['params', 'reportID'], '');
+}
+
+/**
  * Check whether the passed route is currently Active or not.
  *
  * Building path with getPathFromState since navigationRef.current.getCurrentRoute().path
@@ -214,6 +228,7 @@ export default {
     setDidTapNotification,
     isNavigationReady,
     setIsNavigationReady,
+    getReportIDFromRoute,
 };
 
 export {
