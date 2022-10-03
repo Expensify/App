9393db8d7cce057cbc9e45b6e20d1a73b27eee83
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-10-03 12:49:47 +0200

    removed unused code
    Signed-off-by: Hanno J. Gödecke <die.drei99@yahoo.de>

--- e2e/utils/isAppRunning.js
+++
@@ -1,8 +0,0 @@
-const {APP_PACKAGE} = require('../config');
-const execAsync = require('./execAsync');
-
-module.exports = function () {
-    // adb shell pidof
-    const cmd = `adb shell pidof ${APP_PACKAGE}`;
-    return execAsync(cmd);
-};
