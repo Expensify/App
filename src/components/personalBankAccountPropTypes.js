8704adbe702f372f61d0d50e3be6d4a2e573816c
Kosuke Tseng <ctseng@expensify.com>
2022-10-03 10:06:00 -0700

    update proptypes

--- src/components/personalBankAccountPropTypes.js
+++ src/components/personalBankAccountPropTypes.js
@@ -15,38 +15,8 @@ export default PropTypes.shape({
         plaidSelector: PropTypes.string,
     }),
 
-    /** Data we need for the API command "AddPersonalBankAccount" */
-    selectedBankAccount: PropTypes.shape({
-        /** Account Number */
-        accountNumber: PropTypes.string,
-
-        /** Nickname of bank account */
-        addressName: PropTypes.string,
-
-        /** Bank name given by Plaid */
-        bank: PropTypes.string,
-
-        /** This payment method has not been added, and is pending with and error */
-        isPending: PropTypes.bool,
-
-        /** Is Savings account */
-        isSavings: PropTypes.bool,
-
-        /** Password */
-        password: PropTypes.string,
-
-        /** Plaid token */
-        plaidAccessToken: PropTypes.string,
-
-        /** Plaid bank account ID */
-        plaidAccountID: PropTypes.string,
-
-        /** Routing number */
-        routingNumber: PropTypes.string,
-
-        /** Tells us if this is set up via Plaid or manually */
-        setupType: PropTypes.string,
-    }),
+    /** Plaid account ID of the pending bank account */
+    selectedPlaidAccountID: PropTypes.string,
 
     /** Personal Bank Account successfully added. Show user success modal */
     shouldShowSuccess: PropTypes.bool,
