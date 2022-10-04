44b8a952ce272dc91db64ec529719c471556a93c
Rushat Gabhane <rushatgabhane@gmail.com>
2022-10-05 05:06:07 +0530

    remove console log

--- src/pages/settings/Profile/ProfilePage.js
+++ src/pages/settings/Profile/ProfilePage.js
@@ -196,7 +196,7 @@ class ProfilePage extends Component {
         }));
         const currentUserDetails = this.props.currentUserPersonalDetails || {};
         const pronounsPickerValue = this.state.hasSelfSelectedPronouns ? CONST.PRONOUNS.SELF_SELECT : this.pronouns;
-        console.log(this.state.selectedTimezone)
+
         return (
             <ScreenWrapper>
                 <HeaderWithCloseButton
