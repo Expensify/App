0e5e3d052b185b0d440d715c6551e414d52295dd
Alex Beaman <alexbeaman@expensify.com>
2022-10-04 13:35:33 +0200

    New specific component for top description

---
+++ src/components/MenuItemWithTopDescription.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import menuItemPropTypes from './menuItemPropTypes';
+import MenuItem from './MenuItem';
+
+const propTypes = {
+    ...menuItemPropTypes,
+};
+
+const MenuItemWithTopDescription = (props) => {
+    <MenuItem
+        {...props}
+        shouldShowBasicTitle
+        shouldShowDescriptionOnTop
+    />
+};
+
+MenuItemWithTopDescription.propTypes = propTypes;
+MenuItemWithTopDescription.displayName = 'MenuItemWithTopDescription';
+
+export default MenuItemWithTopDescription;
