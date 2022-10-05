5f4e76a88f427b94d3372cd4a40cd9709c495994
hublot <hublot@aliyun.com>
2022-10-05 11:59:33 +0800

    Reset lastAccessedWorkspacePolicyID when delete the workspace

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -25,6 +25,13 @@ Onyx.connect({
         allPolicies[key] = val;
     },
 });
+
+let lastAccessedWorkspacePolicyID = null
+Onyx.connect({
+    key: ONYXKEYS.LAST_ACCESSED_WORKSPACE_POLICY_ID,
+    callback: value => lastAccessedWorkspacePolicyID = value
+});
+
 let sessionEmail = '';
 Onyx.connect({
     key: ONYXKEYS.SESSION,
@@ -125,6 +132,11 @@ function deleteWorkspace(policyID) {
     const failureData = [];
     const successData = [];
     API.write('DeleteWorkspace', {policyID}, {optimisticData, successData, failureData});
+
+    // Reset the lastAccessedWorkspacePolicyID
+    if (policyID === lastAccessedWorkspacePolicyID) {
+    	updateLastAccessedWorkspace(null);
+    }
 }
 
 /**
@@ -638,6 +650,9 @@ function updateCustomUnitRate(policyID, currentCustomUnitRate, customUnitID, new
  * @param {String} policyID
  */
 function updateLastAccessedWorkspace(policyID) {
+    if (policyID === lastAccessedWorkspacePolicyID) {
+        return;
+    }
     Onyx.set(ONYXKEYS.LAST_ACCESSED_WORKSPACE_POLICY_ID, policyID);
 }
 
