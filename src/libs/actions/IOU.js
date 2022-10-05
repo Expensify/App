c8b3c39ac8313a0a486d8c5e92866ff49d7874b9
Carlos Martins <cmartins@expensify.com>
2022-10-05 17:13:39 -0600

    add pendingActions

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -298,7 +298,8 @@ function requestMoney(report, amount, currency, recipientEmail, debtorEmail, com
  */
 function buildSplitBillOnyxData(participants, amount, comment, currentUserEmail, currency, locale) {
     // getChatByParticipants should be created in this PR https://github.com/Expensify/App/pull/11439/files
-    const groupChatReport = ReportUtils.getChatByParticipants(participants) || ReportUtils.buildOptimisticChatReport(participants);
+    const existingGroupChatReport = ReportUtils.getChatByParticipants(participants);
+    const groupChatReport = existingGroupChatReport || ReportUtils.buildOptimisticChatReport(participants);
     const groupReportAction = ReportUtils.buildOptimisticIOUReportAction(
         lodashGet(groupChatReport, 'maxSequenceNumber', 0) + 1,
         CONST.IOU.REPORT_ACTION_TYPE.CREATE,
@@ -311,13 +312,21 @@ function buildSplitBillOnyxData(participants, amount, comment, currentUserEmail,
         {
             onyxMethod: CONST.ONYX.METHOD.MERGE,
             key: `${ONYXKEYS.COLLECTION.REPORT}${groupChatReport.reportID}`,
-            value: groupChatReport,
+            value: {
+                ...groupChatReport,
+                pendingFields: {
+                    createChat: existingGroupChatReport ? null : CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
+                },
+            },
         },
         {
             onyxMethod: CONST.ONYX.METHOD.MERGE,
             key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${groupChatReport.reportID}`,
             value: {
-                [groupReportAction.sequenceNumber]: groupReportAction,
+                [groupReportAction.sequenceNumber]: {
+                    ...groupReportAction,
+                    pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
+                },
             },
         },
     ];
@@ -330,7 +339,8 @@ function buildSplitBillOnyxData(participants, amount, comment, currentUserEmail,
         }
 
         // getChatByParticipants should be created in this PR https://github.com/Expensify/App/pull/11439/files
-        const oneOnOneChatReport = ReportUtils.getChatByParticipants([currentUserEmail, email]) || ReportUtils.buildOptimisticChatReport([currentUserEmail, email]);
+        const existingOneOnOneChatReport = ReportUtils.getChatByParticipants([currentUserEmail, email]);
+        const oneOnOneChatReport = existingOneOnOneChatReport || ReportUtils.buildOptimisticChatReport([currentUserEmail, email]);
         let oneOnOneIOUReport;
         if (oneOnOneChatReport.iouReportID) {
             oneOnOneIOUReport = iouReports[`${ONYXKEYS.COLLECTION.REPORT_IOUS}${oneOnOneChatReport.iouReportID}`];
@@ -359,7 +369,12 @@ function buildSplitBillOnyxData(participants, amount, comment, currentUserEmail,
             {
                 onyxMethod: CONST.ONYX.METHOD.MERGE,
                 key: `${ONYXKEYS.COLLECTION.REPORT}${oneOnOneChatReport.reportID}`,
-                value: oneOnOneChatReport,
+                value: {
+                    ...oneOnOneChatReport,
+                    pendingFields: {
+                        createChat: existingOneOnOneChatReport ? null : CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
+                    },
+                },
             },
             {
                 onyxMethod: CONST.ONYX.METHOD.MERGE,
@@ -370,7 +385,10 @@ function buildSplitBillOnyxData(participants, amount, comment, currentUserEmail,
                 onyxMethod: CONST.ONYX.METHOD.MERGE,
                 key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${oneOnOneChatReport.reportID}`,
                 value: {
-                    [oneOnOneIOUReportAction.sequenceNumber]: oneOnOneIOUReportAction,
+                    [oneOnOneIOUReportAction.sequenceNumber]: {
+                        ...oneOnOneIOUReportAction,
+                        pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
+                    },
                 },
             },
         );
