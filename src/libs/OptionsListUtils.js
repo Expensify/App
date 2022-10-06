71480712807fc8ae4693496d1e8f5d913c9c679d
Tim Golen <tgolen@expensify.com>
2022-10-06 11:30:45 -0600

    Stop factoring in the view mode when searching reports

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -448,7 +448,6 @@ function getOptions(reports, personalDetails, {
     sortPersonalDetailsByAlphaAsc = true,
     forcePolicyNamePreview = false,
 }) {
-    const isInGSDMode = priorityMode === CONST.PRIORITY_MODE.GSD;
     let recentReportOptions = [];
     let personalDetailsOptions = [];
     const reportMapForLogins = {};
@@ -457,7 +456,7 @@ function getOptions(reports, personalDetails, {
     const filteredReports = _.filter(reports, report => ReportUtils.shouldReportBeInOptionList(
         report,
         Navigation.getReportIDFromRoute(),
-        isInGSDMode,
+        false,
         currentUserLogin,
         iouReports,
         betas,
