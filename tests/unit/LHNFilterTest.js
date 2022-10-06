4f54b6138f9b102ba5149108c3907a9afb51f0d4
John Lee <john@expensify.com>
2022-10-06 16:16:14 -0400

    Fix tests with merged main

--- tests/unit/LHNFilterTest.js
+++ tests/unit/LHNFilterTest.js
@@ -562,10 +562,10 @@ describe('Sidebar', () => {
         const booleansWhichRemovesInactiveReport = [
             JSON.stringify([false, false, false, false, false, false, false]),
             JSON.stringify([false, false, false, true, false, false, false]),
+            JSON.stringify([false, false, false, false, true, false, false]),
+            JSON.stringify([false, false, false, true, true, false, false]),
             JSON.stringify([false, false, true, false, false, false, false]),
-            JSON.stringify([false, false, true, false, true, false, false]),
             JSON.stringify([false, false, true, true, false, false, false]),
-            JSON.stringify([false, false, true, true, true, false, false]),
             JSON.stringify([false, true, false, false, false, false, false]),
             JSON.stringify([false, true, false, false, true, false, false]),
             JSON.stringify([false, true, false, true, false, false, false]),
