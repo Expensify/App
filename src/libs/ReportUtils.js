cf6540410c0994239eb3ddd2f8f97e4b457cab15
Tim Golen <tgolen@expensify.com>
2022-10-06 08:23:07 -0600

    Check for unread reports at the end of the filter logic

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -916,12 +916,6 @@ function shouldReportBeInOptionList(report, reportIDFromRoute, isInGSDMode, curr
         return true;
     }
 
-    // Include unread reports when in GSD mode
-    // GSD mode is specifically for focusing the user on the most relevant chats, primarily, the unread ones
-    if (isInGSDMode) {
-        return isUnread(report);
-    }
-
     // Include reports if they have a draft, are pinned, or have an outstanding IOU
     // These are always relevant to the user no matter what view mode the user prefers
     if (report.hasDraft || report.isPinned || hasOutstandingIOU(report, currentUserLogin, iouReports)) {
@@ -959,6 +953,12 @@ function shouldReportBeInOptionList(report, reportIDFromRoute, isInGSDMode, curr
     // Include policy expense chats if the user isn't in the policy expense chat beta
     if (isPolicyExpenseChat(report) && !Permissions.canUsePolicyExpenseChat(betas)) {
         return false;
+    }
+
+    // Include unread reports when in GSD mode
+    // GSD mode is specifically for focusing the user on the most relevant chats, primarily, the unread ones
+    if (isInGSDMode) {
+        return isUnread(report);
     }
 
     return true;
