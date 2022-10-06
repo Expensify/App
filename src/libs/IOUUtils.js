07c2ec257093319b395f386b06b038d45073a04d
Carlos Martins <cmartins@expensify.com>
2022-10-06 16:46:14 -0600

    move calculateAmount to utils

--- src/libs/IOUUtils.js
+++ src/libs/IOUUtils.js
@@ -1,10 +1,8 @@
-import CONST from '../CONST';
-
 /**
  * Calculates the amount per user given a list of participants
- * @param {Array} participants - List of logins for the participants in the chat. It should not include the current user's login.
- * @param {Number} total - IOU total amount
- * @param {Boolean} isDefaultUser - Whether we are calculating the amount for the current user
+ * @param {Array} participants
+ * @param {Int} total
+ * @param {Boolean} isDefaultUser
  * @returns {Number}
  */
 function calculateAmount(participants, total, isDefaultUser = false) {
@@ -17,9 +15,7 @@ function calculateAmount(participants, total, isDefaultUser = false) {
     const totalParticipants = participants.length + 1;
     const amountPerPerson = Math.round(iouAmount / totalParticipants);
 
-    if (!isDefaultUser) {
-        return amountPerPerson;
-    }
+    if (!isDefaultUser) { return amountPerPerson; }
 
     const sumAmount = amountPerPerson * totalParticipants;
     const difference = iouAmount - sumAmount;
@@ -27,38 +23,7 @@ function calculateAmount(participants, total, isDefaultUser = false) {
     return iouAmount !== sumAmount ? (amountPerPerson + difference) : amountPerPerson;
 }
 
-/**
- * The owner of the IOU report is the account who is owed money and the manager is the one who owes money!
- * In case the owner/manager swap, we need to update the owner of the IOU report and the report total, since it is always positive.
- * For example: if user1 owes user2 $10, then we have: {ownerEmail: user2, managerEmail: user1, total: $10 (a positive amount, owed to user2)}
- * If user1 requests $17 from user2, then we have: {ownerEmail: user1, managerEmail: user2, total: $7 (still a positive amount, but now owed to user1)}
- *
- * @param {Object} iouReport
- * @param {String} actorEmail
- * @param {Number} amount
- * @param {String} type
- * @returns {Object}
- */
-function updateIOUOwnerAndTotal(iouReport, actorEmail, amount, type = CONST.IOU.REPORT_ACTION_TYPE.CREATE) {
-    const iouReportUpdate = {...iouReport};
-
-    if (actorEmail === iouReport.ownerEmail) {
-        iouReportUpdate.total += type === CONST.IOU.REPORT_ACTION_TYPE.CANCEL ? -amount : amount;
-    } else {
-        iouReportUpdate.total += type === CONST.IOU.REPORT_ACTION_TYPE.CANCEL ? amount : -amount;
-    }
-
-    if (iouReportUpdate.total < 0) {
-        // The total sign has changed and hence we need to flip the manager and owner of the report.
-        iouReportUpdate.ownerEmail = iouReport.managerEmail;
-        iouReportUpdate.managerEmail = iouReport.ownerEmail;
-        iouReportUpdate.total = -iouReportUpdate.total;
-    }
-
-    return iouReportUpdate;
-}
-
 export {
+    // eslint-disable-next-line import/prefer-default-export
     calculateAmount,
-    updateIOUOwnerAndTotal,
 };
