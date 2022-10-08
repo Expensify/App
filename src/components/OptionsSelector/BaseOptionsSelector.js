97789926db4e17b5816f665ecc1ae77bdb8dc51a
Oliver Wilks <ollywilks@googlemail.com>
2022-10-08 16:20:57 +0100

    Fix for App crash while navigating back from Contacts

--- src/components/OptionsSelector/BaseOptionsSelector.js
+++ src/components/OptionsSelector/BaseOptionsSelector.js
@@ -85,7 +85,7 @@ class BaseOptionsSelector extends Component {
         );
 
         if (this.props.shouldDelayFocus) {
-            setTimeout(() => this.textInput.focus(), CONST.ANIMATED_TRANSITION);
+            this.focusTimeout = setTimeout(() => this.textInput.focus(), CONST.ANIMATED_TRANSITION);
         } else {
             this.textInput.focus();
         }
@@ -123,6 +123,10 @@ class BaseOptionsSelector extends Component {
     }
 
     componentWillUnmount() {
+        if (this.focusTimeout) {
+            clearTimeout(this.focusTimeout);
+        }
+
         if (this.unsubscribeEnter) {
             this.unsubscribeEnter();
         }
