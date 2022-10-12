44f7c705f85662dfd4bf24998089c7776b843aa8
aimane-chnaif <aiman.chnaif@gmail.com>
2022-10-12 19:55:35 +1000

    add contentContainerStyle propsTypes in OfflineWithFeedback

--- src/components/OfflineWithFeedback.js
+++ src/components/OfflineWithFeedback.js
@@ -40,6 +40,9 @@ const propTypes = {
     /** Additional styles to add after local styles. Applied to the parent container */
     style: stylePropTypes,
 
+    /** Additional styles to add after local styles. Applied to the children wrapper container */
+    contentContainerStyle: stylePropTypes,
+
     /** Additional style object for the error row */
     errorRowStyles: stylePropTypes,
 
@@ -51,6 +54,7 @@ const defaultProps = {
     errors: null,
     onClose: () => {},
     style: [],
+    contentContainerStyle: [],
     errorRowStyles: [],
 };
 
