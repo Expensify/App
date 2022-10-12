89c0c114f99e44db859c2e723b067c397426853c
Nathalie Kuoch <nkuoch@expensify.com>
2022-10-12 17:22:47 +0200

    Trying to fix selectAccount issue

--- src/libs/actions/PaymentMethods.js
+++ src/libs/actions/PaymentMethods.js
@@ -253,7 +253,7 @@ function addPaymentCard(params) {
 function clearDebitCardFormErrorAndSubmit() {
     Onyx.set(ONYXKEYS.FORMS.ADD_DEBIT_CARD_FORM, {
         isLoading: false,
-        error: null,
+        errors: null,
     });
 }
 
