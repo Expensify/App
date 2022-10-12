a08385d925faf25fe2bfb39846801b4cf4af8fba
Carlos Martins <cmartins@expensify.com>
2022-10-12 08:30:43 -0600

    update form to read drafts key

--- src/components/Form.js
+++ src/components/Form.js
@@ -238,7 +238,7 @@ export default compose(
             key: props => props.formID,
         },
         draftValues: {
-            key: props => `${props.formID}DraftValues`,
+            key: props => `${props.formID}Draft`,
         },
     }),
 )(Form);
