54b09d6f57b5b5559c936cdc1675737d717e03e5
Carlos Martins <cmartins@expensify.com>
2022-10-13 17:00:29 -0600

    fix participants login list

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -150,7 +150,8 @@ function createIOUTransaction(params) {
  */
 function createSplitsAndOnyxData(participants, currentUserLogin, amount, comment, currency, locale) {
     const currentUserEmail = OptionsListUtils.addSMSDomainIfPhoneNumber(currentUserLogin);
-    const existingGroupChatReport = ReportUtils.getChatByParticipants(participants);
+    const participantLogins = _.map(participants, participant => OptionsListUtils.addSMSDomainIfPhoneNumber(participant.login));
+    const existingGroupChatReport = ReportUtils.getChatByParticipants(participantLogins);
     const groupChatReport = existingGroupChatReport || ReportUtils.buildOptimisticChatReport(_.pluck(participants, 'login'));
     const groupCreatedReportAction = existingGroupChatReport ? {} : ReportUtils.buildOptimisticCreatedReportAction(currentUserEmail);
     const groupIOUReportAction = ReportUtils.buildOptimisticIOUReportAction(
