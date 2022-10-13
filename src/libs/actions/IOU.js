5a19404816d0bc20d2ae7c8aa0b0905e10ca9145
Carlos Martins <cmartins@expensify.com>
2022-10-13 15:17:17 -0600

    fix iouReport key

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -266,7 +266,7 @@ function createSplitsAndOnyxData(participants, currentUserLogin, amount, comment
             },
             {
                 onyxMethod: CONST.ONYX.METHOD.MERGE,
-                key: `${ONYXKEYS.COLLECTION.REPORT_IOUS}${oneOnOneChatReport.reportID}`,
+                key: `${ONYXKEYS.COLLECTION.REPORT_IOUS}${oneOnOneIOUReport.reportID}`,
                 value: oneOnOneIOUReport,
             },
             {
