d4f2dcee4ce422d1252bca7ceebc62caf585e9c6
Tyler Karaszewski <tyler@expensify.com>
2022-10-17 12:46:16 +0200

    re-change (again) prop names after merge

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -288,13 +288,24 @@ class ReportScreen extends React.Component {
                                     />
                                 )
                                 : (
-                                    <ReportActionsView
-                                        reportActions={this.props.reportActions}
-                                        report={this.props.report}
-                                        session={this.props.session}
-                                        isComposerFullSize={this.props.isComposerFullSize}
-                                        isDrawerOpen={this.props.isDrawerOpen}
-                                    />
+                                    <>
+                                        <ReportActionsView
+                                            reportActions={this.props.reportActions}
+                                            report={this.props.report}
+                                            session={this.props.session}
+                                            isComposerFullSize={this.props.isComposerFullSize}
+                                            isDrawerOpen={this.props.isDrawerOpen}
+                                        />
+                                        <ReportFooter
+                                            errors={addWorkspaceRoomOrChatErrors}
+                                            pendingAction={addWorkspaceRoomOrChatPendingAction}
+                                            isOffline={this.props.network.isOffline}
+                                            reportActions={this.props.reportActions}
+                                            report={this.props.report}
+                                            isComposerFullSize={this.props.isComposerFullSize}
+                                            onSubmitComment={this.onSubmitComment}
+                                        />
+                                    </>
                                 )}
                             <ReportFooter
                                 errors={addWorkspaceRoomOrChatErrors}
