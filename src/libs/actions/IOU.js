72813e2999c18e56fde148142f9e04a201ba2ec1
Carlos Martins <cmartins@expensify.com>
2022-10-19 17:47:53 -0600

    use correct type, use waitForCollectionCallback

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -16,16 +16,11 @@ import * as ReportUtils from '../ReportUtils';
 import * as IOUUtils from '../IOUUtils';
 import * as OptionsListUtils from '../OptionsListUtils';
 
-const iouReports = {};
+let iouReports;
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.REPORT_IOUS,
-    callback: (iouReport, key) => {
-        if (!iouReport || !key || !iouReport.ownerEmail) {
-            return;
-        }
-
-        iouReports[key] = iouReport;
-    },
+    waitForCollectionCallback: true,
+    callback: val => iouReports = val,
 });
 
 /**
@@ -297,7 +292,7 @@ function requestMoney(report, amount, currency, recipientEmail, debtorEmail, com
 /**
  * @param {Array} participants
  * @param {String} currentUserLogin
- * @param {Int} amount
+ * @param {Number} amount
  * @param {String} comment
  * @param {String} currency
  * @param {String} locale
@@ -510,7 +505,7 @@ function createSplitsAndOnyxData(participants, currentUserLogin, amount, comment
 /**
  * @param {Array} participants
  * @param {String} currentUserLogin
- * @param {Int} amount
+ * @param {Number} amount
  * @param {String} comment
  * @param {String} currency
  * @param {String} locale
@@ -535,7 +530,7 @@ function splitBill(participants, currentUserLogin, amount, comment, currency, lo
 /**
  * @param {Array} participants
  * @param {String} currentUserLogin
- * @param {Int} amount
+ * @param {Number} amount
  * @param {String} comment
  * @param {String} currency
  * @param {String} locale
