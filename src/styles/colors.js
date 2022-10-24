cb7b6176c2a0b0c0cfe9f26c05b69397630fa2ee
Georgia Monahan <georgia@expensify.com>
2022-10-24 08:35:52 -0700

    Update colors and add themes for testing

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,21 +1,58 @@
 export default {
-    white: '#FFFFFF',
-    gray1: '#FAFAFA',
-    gray2: '#ECECEC',
-    gray3: '#C6C9CA',
-    gray4: '#7D8B8F',
-    dark: '#0b1b34',
+    white: '#FFFFFF', // App Background
+    gray1: '#FAFAFA', // Highlight Background
+    gray2: '#ECECEC', // Borders
+    gray3: '#C6C9CA', // Icons
+    gray4: '#7D8B8F', // Supporting Text
+    dark: '#0b1b34', // Text
     black: '#000000',
-    blue: '#0185ff',
+    blue: '#0185ff', // Link
     blueHover: '#0063bf',
     floralwhite: '#fffaf0',
-    green: '#03d47c',
+    green: '#03d47c', // Success Button
     greenHover: '#03c775',
     orange: '#ff7101',
     pink: '#f68dfe',
-    red: '#fc3826',
+    red: '#fc3826', // Danger Red
     redHover: '#e13826',
     redDisabled: '#fea29a',
     yellow: '#fed607',
     transparent: 'transparent',
+
+    // Dark Mode Colors vGreen
+    darkAppBackground: '#061B09',
+    darkHighlightBackground: '#002E22',
+    darkBorders: '#1B5744',
+    darkIcons: '#8B9C8F',
+    darkSupportingText: '#C9D3C5',
+    darkText: '#FFFFFF',
+    darkDefaultButton: '#184E3D',
+    darkSuccessButton: '#03D47C',
+    darkDangerButton: '#F25730',
+    darkLink: '#8DC8FF',
+
+    // Dark Mode Colors vBlack
+    darkerAppBackground: '#040B15',
+    darkerHighlightBackground: '#0B1B34',
+    darkerBorders: '#32435E',
+    darkerIcons: '#7D8B8F',
+    darkerSupportingText: '#C6C9CA',
+    darkerText: '#ECECEC',
+    darkerDefaultButton: '#32435E',
+    darkerSuccessButton: '#03D47C',
+    darkerDangerButton: '#FC3826',
+    darkerLink: '#8DC8FF',
+
+    // Light Mode Colors
+    lightAppBackground: '#FFFFFF',
+    lightHighlightBackground: '#FBF8F5',
+    lightBorders: '#EEEBE7',
+    lightIcons: '#A2A9A3',
+    lightSupportingText: '#586A64',
+    lightText: '#002140',
+    lightDefaultButton: '#EEEBE7',
+    lightSuccessButton: '#03D47C',
+    lightDangerButton: '#F25730',
+    lightLink: '#0164BF',
+
 };
