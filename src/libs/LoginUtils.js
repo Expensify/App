ddbbb82330aa92e7fe37edf2254d6d776d75c371
Alex Beaman <alexbeaman@expensify.com>
2022-10-24 15:42:54 +0200

    New temporary util to convert login list to object

--- src/libs/LoginUtils.js
+++ src/libs/LoginUtils.js
@@ -1,3 +1,4 @@
+import _ from 'underscore';
 import CONST from '../CONST';
 
 /**
@@ -12,6 +13,7 @@ function getEmailWithoutMergedAccountPrefix(email) {
 
 /**
  * Remove the special chars from the phone number
+ *
  * @param {String} phone
  * @return {String}
  */
@@ -21,6 +23,7 @@ function getPhoneNumberWithoutSpecialChars(phone) {
 
 /**
  * Remove +1 and special chars from the phone number
+ *
  * @param {String} phone
  * @return {String}
  */
@@ -28,8 +31,28 @@ function getPhoneNumberWithoutUSCountryCodeAndSpecialChars(phone) {
     return getPhoneNumberWithoutSpecialChars(phone.replace(/^\+1/, ''));
 }
 
+/**
+ * Converts a login list to an object, where the login partnerUserID is the key
+ *
+ * TODO: remove this once the server is always sending back the correct format!
+ * https://github.com/Expensify/App/issues/10960
+ *
+ * @param {Array|Object} loginList 
+ */
+function convertLoginListToObject(loginList = {}) {
+    if (!_.isArray(loginList)) {
+        return loginList;
+    }
+
+    return _.reduce(response.loginList, (allLogins, login) => {
+        allLogins[login.partnerUserID] = login;
+        return allLogins;
+    }, {});
+}
+
 export {
     getEmailWithoutMergedAccountPrefix,
     getPhoneNumberWithoutSpecialChars,
     getPhoneNumberWithoutUSCountryCodeAndSpecialChars,
+    convertLoginListToObject,
 };
