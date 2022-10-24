9beca4354b825e8a374d6662ed34c8363af4675e
Alex Beaman <alexbeaman@expensify.com>
2022-10-24 16:30:49 +0200

    Update prop type variable name

--- src/pages/RequestCallPage.js
+++ src/pages/RequestCallPage.js
@@ -31,7 +31,7 @@ import networkPropTypes from '../components/networkPropTypes';
 import RequestCallConfirmationScreen from './RequestCallConfirmationScreen';
 import Form from '../components/Form';
 
-const loginPropType = PropTypes.shape({
+const loginPropTypes = PropTypes.shape({
     /** Phone/Emails associated with user */
     partnerUserID: PropTypes.string,
 });
@@ -42,10 +42,10 @@ const propTypes = {
 
     /** Login list for the user that is signed in */
     loginList: PropTypes.oneOfType([
-        PropTypes.objectOf(loginPropType),
+        PropTypes.objectOf(loginPropTypes),
         // TODO: remove this once this closes:
         // https://github.com/Expensify/App/issues/10960
-        PropTypes.arrayOf(loginPropType),
+        PropTypes.arrayOf(loginPropTypes),
     ]),
 
     /** The policies which the user has access to */
