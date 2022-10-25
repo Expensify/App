8beb910c17068ba0df04bbf6c7a21a847ebb717b
tienifr <christianwen18@gmail.com>
2022-10-25 23:27:45 +0700

    fix: rebase

--- src/libs/ReportScrollManager/index.js
+++ src/libs/ReportScrollManager/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import _ from 'underscore';
 
 // This ref is created using React.createRef here because this function is used by a component that doesn't have access
 // to the original ref.
@@ -15,8 +16,15 @@ function scrollToIndex(index, isEditing) {
     if (isEditing) {
         return;
     }
-
-    flatListRef.current.scrollToIndex(index);
+    const sizeMap = flatListRef.current.props.sizeMap;
+    const clientHeight = flatListRef.current.getScrollableNode().clientHeight;
+    const paddingTop = parseFloat(flatListRef.current.getScrollableNode().childNodes[0].style.paddingTop || '0px');
+    const sizeArray = _.map(_.values(sizeMap), s => s.length);
+    const sumNum = _.reduce(sizeArray.slice(0, (index.index)), (acc, val) => acc + val);
+    flatListRef.current.scrollToOffset({
+        animated: index.animated,
+        offset: (sumNum + paddingTop + (index.viewOffset || 0)) - (clientHeight * (index.viewPosition || 0)),
+    });
 }
 
 /**
