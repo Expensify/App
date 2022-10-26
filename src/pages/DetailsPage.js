e8b3448ba705fba95bb16446dc75bcce919b214c
Brandon Stites <bstites+1@expensify.com>
2022-10-26 13:48:52 -0600

    Remove uses of fetchOrCreateReport

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -193,7 +193,7 @@ class DetailsPage extends React.PureComponent {
                                 <MenuItem
                                     title={`${this.props.translate('common.message')}${details.displayName}`}
                                     icon={Expensicons.ChatBubble}
-                                    onPress={() => Report.fetchOrCreateChatReport([this.props.session.email, details.login])}
+                                    onPress={() => Report.navigateToOrCreateChatReport([details.login])}
                                     wrapperStyle={styles.breakAll}
                                     shouldShowRightIcon
                                 />
