b94e893085e78752d0cf0c154d4d8a2f995265d6
Justice Arthur <justice@expensify.com>
2022-10-27 17:47:43 +0100

    address issues with error on creation

--- src/pages/workspace/WorkspaceInitialPage.js
+++ src/pages/workspace/WorkspaceInitialPage.js
@@ -60,6 +60,8 @@ class WorkspaceInitialPage extends React.Component {
         this.openEditor = this.openEditor.bind(this);
         this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
         this.confirmDeleteAndHideModal = this.confirmDeleteAndHideModal.bind(this);
+        this.disabled = this.disabled.bind(this);
+        this.dismissError = this.dismissError.bind(this);
 
         this.state = {
             isDeleteModalOpen: false,
@@ -88,6 +90,18 @@ class WorkspaceInitialPage extends React.Component {
         PolicyActions.deletePolicy(this.props.policy.id);
         this.toggleDeleteModal(false);
         Navigation.navigate(ROUTES.SETTINGS);
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    disabled() {
+        return Boolean(this.props.policy.pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD && this.props.policy.errors);
+    }
+
+    dismissError() {
+        Navigation.navigate(ROUTES.SETTINGS);
+        Policy.removeWorkspace(this.props.policy.id);
     }
 
     render() {
@@ -169,40 +183,18 @@ class WorkspaceInitialPage extends React.Component {
                             styles.justifyContentBetween,
                         ]}
                     >
-                        <View style={[styles.flex1]}>
-                            <View style={styles.pageWrapper}>
-                                <View style={[styles.settingsPageBody, styles.alignItemsCenter]}>
-                                    <Pressable
-                                        style={[styles.pRelative, styles.avatarLarge]}
-                                        onPress={this.openEditor}
-                                    >
-                                        {this.props.policy.avatar
-                                            ? (
-                                                <Avatar
-                                                    containerStyles={styles.avatarLarge}
-                                                    imageStyles={[styles.avatarLarge, styles.alignSelfCenter]}
-                                                    source={this.props.policy.avatar}
-                                                    fallbackIcon={Expensicons.FallbackWorkspaceAvatar}
-                                                    size={CONST.AVATAR_SIZE.LARGE}
-                                                />
-                                            )
-                                            : (
-                                                <Icon
-                                                    src={Expensicons.Workspace}
-                                                    height={80}
-                                                    width={80}
-                                                    fill={themedefault.iconSuccessFill}
-                                                />
-                                            )}
-                                    </Pressable>
-                                    {!_.isEmpty(this.props.policy.name) && (
+                        <OfflineWithFeedback
+                            pendingAction={this.props.policy.pendingAction}
+                            onClose={() => this.dismissError()}
+                            errors={this.props.policy.errors}
+                            errorRowStyles={[styles.ph6, styles.pv2]}
+                        >
+                            <View style={[styles.flex1]}>
+                                <View style={styles.pageWrapper}>
+                                    <View style={[styles.settingsPageBody, styles.alignItemsCenter]}>
                                         <Pressable
-                                            style={[
-                                                styles.alignSelfCenter,
-                                                styles.mt4,
-                                                styles.mb6,
-                                                styles.w100,
-                                            ]}
+                                            disabled={this.disabled()}
+                                            style={[styles.pRelative, styles.avatarLarge]}
                                             onPress={this.openEditor}
                                         >
                                             <Tooltip text={this.props.policy.name}>
@@ -217,8 +209,45 @@ class WorkspaceInitialPage extends React.Component {
                                                 </Text>
                                             </Tooltip>
                                         </Pressable>
-                                    )}
+                                        {!_.isEmpty(this.props.policy.name) && (
+                                            <Pressable
+                                                disabled={this.disabled()}
+                                                style={[
+                                                    styles.alignSelfCenter,
+                                                    styles.mt4,
+                                                    styles.mb6,
+                                                    styles.w100,
+                                                ]}
+                                                onPress={this.openEditor}
+                                            >
+                                                <Tooltip text={this.props.policy.name}>
+                                                    <Text
+                                                        numberOfLines={1}
+                                                        style={[
+                                                            styles.displayName,
+                                                            styles.alignSelfCenter,
+                                                        ]}
+                                                    >
+                                                        {this.props.policy.name}
+                                                    </Text>
+                                                </Tooltip>
+                                            </Pressable>
+                                        )}
+                                    </View>
                                 </View>
+                                {_.map(menuItems, item => (
+                                    <MenuItem
+                                        key={item.translationKey}
+                                        disabled={this.disabled()}
+                                        interactive={!this.disabled()}
+                                        title={this.props.translate(item.translationKey)}
+                                        icon={item.icon}
+                                        iconRight={item.iconRight}
+                                        onPress={() => item.action()}
+                                        shouldShowRightIcon
+                                        brickRoadIndicator={item.brickRoadIndicator}
+                                    />
+                                ))}
                             </View>
                             {_.map(menuItems, item => (
                                 <MenuItem
