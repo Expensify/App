2221e5692391c257d3c15adf6051e6b649015a7a
Alex Beaman <alexbeaman@expensify.com>
2022-10-28 13:56:20 +0200

    Fill out timezone select page & filtering

--- src/pages/settings/Profile/TimezoneSelectPage.js
+++ src/pages/settings/Profile/TimezoneSelectPage.js
@@ -1,20 +1,18 @@
 import lodashGet from 'lodash/get';
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import _ from 'underscore';
+import moment from 'moment-timezone';
 import withCurrentUserPersonalDetails, {withCurrentUserPersonalDetailsPropTypes, withCurrentUserPersonalDetailsDefaultProps} from '../../../components/withCurrentUserPersonalDetails';
 import ScreenWrapper from '../../../components/ScreenWrapper';
 import HeaderWithCloseButton from '../../../components/HeaderWithCloseButton';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
 import ROUTES from '../../../ROUTES';
 import CONST from '../../../CONST';
-import Text from '../../../components/Text';
 import styles from '../../../styles/styles';
 import Navigation from '../../../libs/Navigation/Navigation';
 import * as PersonalDetails from '../../../libs/actions/PersonalDetails';
 import compose from '../../../libs/compose';
-import MenuItemWithTopDescription from '../../../components/MenuItemWithTopDescription';
-import TextInput from '../../../components/TextInput';
-import OptionsList from '../../../components/OptionsList';
+import OptionsSelector from '../../../components/OptionsSelector';
 
 const propTypes = {
     ...withLocalizePropTypes,
@@ -29,61 +27,63 @@ class TimezoneSelectPage extends Component {
     constructor(props) {
         super(props);
 
-        this.updateSelectedTimezone = this.updateSelectedTimezone.bind(this);
+        this.saveSelectedTimezone = this.saveSelectedTimezone.bind(this);
+        this.filterShownTimezones = this.filterShownTimezones.bind(this);
+
+        this.currentSelectedTimezone = lodashGet(props.currentUserPersonalDetails, 'timezone.selected', CONST.DEFAULT_TIME_ZONE.selected);
+        this.allTimezones = _.chain(moment.tz.names())
+            .filter(timezone => !timezone.startsWith('Etc/GMT'))
+            .map(timezone => ({
+                text: timezone,
+                keyForList: timezone,
+
+                // Add green checkmark icon & bold the timezone text
+                isSelected: timezone === this.currentSelectedTimezone,
+                isUnread: timezone === this.currentSelectedTimezone,
+            }))
+            .value();
 
         this.state = {
-            selectedTimezone: lodashGet(props.currentUserPersonalDetails, 'timezone.selected', CONST.DEFAULT_TIME_ZONE.selected),
-        }
+            timezoneInputText: this.currentSelectedTimezone,
+            timezoneOptions: this.allTimezones,
+        };
     }
-    
-    updateSelectedTimezone(newSelectedTimezone) {
-        PersonalDetails.updateSelectedTimezone(newSelectedTimezone);
-    };
-    
+
+    /**
+     * @param {Object} timezone
+     * @param {String} timezone.label
+     */
+    saveSelectedTimezone({label}) {
+        PersonalDetails.updateSelectedTimezone(label);
+    }
+
+    /**
+     * @param {String} searchText
+     */
+    filterShownTimezones(searchText) {
+        this.setState({
+            timezoneInputText: searchText,
+            timezoneOptions: _.filter(this.allTimezones, (tz => tz.text.toLowerCase().startsWith(searchText.toLowerCase()))),
+        });
+    }
+
     render() {
         return (
             <ScreenWrapper>
                 <HeaderWithCloseButton
                     title={this.props.translate('timezonePage.timezone')}
                     shouldShowBackButton
-                    onBackButtonPress={() => Navigation.navigate(ROUTES.SETTINGS_PROFILE)}
+                    onBackButtonPress={() => Navigation.navigate(ROUTES.SETTINGS_TIMEZONE_INITIAL)}
                     onCloseButtonPress={() => Navigation.dismissModal(true)}
                 />
-                <View style={[styles.ph5]}>
-                    <TextInput
-                        value={this.state.selectedTimezone}
-                        label={this.props.translate('timezonePage.timezone')}
-                    />
-                    <OptionsList
-                        onSelectRow={(something) => console.log('select', something)}
-                        sections={[
-                            {
-                                title: undefined,
-                                data: [
-                                    {
-                                        text: 'text',
-                                        subtitle: 'subtitle',
-                                    },
-                                    {
-                                        text: 'text',
-                                        subtitle: 'subtitle',
-                                    },
-                                    {
-                                        text: 'text',
-                                        subtitle: 'subtitle',
-                                    },
-                                    {
-                                        text: 'text',
-                                        subtitle: 'subtitle',
-                                    },
-                                ],
-                                indexOffset: 0,
-                            },
-                        ]}
-                        // focusedIndex={}
-                        // selectedOptions={}
-                    />
-                </View>
+                <OptionsSelector
+                    textInputLabel={this.props.translate('timezonePage.timezone')}
+                    value={this.state.timezoneInputText}
+                    onChangeText={this.filterShownTimezones}
+                    onSelectRow={this.saveSelectedTimezone}
+                    optionHoveredStyle={styles.hoveredComponentBG}
+                    sections={[{data: this.state.timezoneOptions}]}
+                />
             </ScreenWrapper>
         );
     }
