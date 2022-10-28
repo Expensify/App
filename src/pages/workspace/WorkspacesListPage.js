eac41f2351380504c7c61a951b59443a3f84bef7
Justice Arthur <justice@expensify.com>
2022-10-28 12:16:50 +0100

    include RBR on workspace list

--- src/pages/workspace/WorkspacesListPage.js
+++ src/pages/workspace/WorkspacesListPage.js
@@ -124,7 +124,6 @@ class WorkspacesListPage extends Component {
                 iconStyles: policy.avatar ? [] : [styles.popoverMenuIconEmphasized],
                 iconFill: themeColors.iconReversed,
                 fallbackIcon: Expensicons.FallbackWorkspaceAvatar,
-                brickRoadIndicator: PolicyUtils.getPolicyBrickRoadIndicatorStatus(policy, this.props.policyMembers),
                 pendingAction: policy.pendingAction,
                 isPolicy: true,
                 errors: policy.errors,
