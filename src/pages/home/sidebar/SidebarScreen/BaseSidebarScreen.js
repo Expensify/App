ba3b9bfae3d6940436d71c7f97d434872c4d14ff
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-10-28 15:39:17 +0200

    removed onyx subscribe, use shouldComponentUpdate

--- src/pages/home/sidebar/SidebarScreen/BaseSidebarScreen.js
+++ src/pages/home/sidebar/SidebarScreen/BaseSidebarScreen.js
@@ -58,18 +58,16 @@ class BaseSidebarScreen extends Component {
                 style={[styles.sidebar]}
             >
                 {({insets}) => (
-                    <>
-                        <View style={[styles.flex1]}>
-                            <SidebarLinks
-                                onLinkClick={this.startTimer}
-                                insets={insets}
-                                onAvatarClick={this.navigateToSettings}
-                                isSmallScreenWidth={this.props.isSmallScreenWidth}
-                                isDrawerOpen={this.props.isDrawerOpen}
-                                reportIDFromRoute={this.props.reportIDFromRoute}
-                            />
-                        </View>
-                    </>
+                    <View style={[styles.flex1]}>
+                        <SidebarLinks
+                            onLinkClick={this.startTimer}
+                            insets={insets}
+                            onAvatarClick={this.navigateToSettings}
+                            isSmallScreenWidth={this.props.isSmallScreenWidth}
+                            isDrawerOpen={this.props.isDrawerOpen}
+                            reportIDFromRoute={this.props.reportIDFromRoute}
+                        />
+                    </View>
                 )}
             </ScreenWrapper>
         );
