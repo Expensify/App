04111d4cd5f5541d28b66d06d6dcb183feeaacb8
Justice Arthur <justice@expensify.com>
2022-10-28 12:16:50 +0100

    include RBR on workspace list

--- src/libs/PolicyUtils.js
+++ src/libs/PolicyUtils.js
@@ -48,7 +48,7 @@ function hasCustomUnitsError(policy) {
  */
 function getPolicyBrickRoadIndicatorStatus(policy, policyMembers) {
     const policyMemberList = lodashGet(policyMembers, `${ONYXKEYS.COLLECTION.POLICY_MEMBER_LIST}${policy.id}`, {});
-    if (hasPolicyMemberError(policyMemberList) || hasCustomUnitsError(policy)) {
+    if (hasPolicyMemberError(policyMemberList) || hasCustomUnitsError(policy) || hasPolicyError(policy)) {
         return CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR;
     }
     return '';
