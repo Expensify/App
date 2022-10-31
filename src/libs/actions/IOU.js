c0a3efdece82fdae78eddeb2c8b7fa4dd64d3288
Carlos Martins <cmartins@expensify.com>
2022-10-31 15:27:25 -0600

    use type

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -660,7 +660,7 @@ function cancelMoneyRequest(chatReportID, iouReportID, type, moneyRequestAction)
     );
 
     const currentUserEmail = optimisticReportAction.actorEmail;
-    const updatedIOUReport = IOUUtils.updateIOUOwnerAndTotal(iouReport, currentUserEmail, amount, CONST.IOU.REPORT_ACTION_TYPE.CANCEL);
+    const updatedIOUReport = IOUUtils.updateIOUOwnerAndTotal(iouReport, currentUserEmail, amount, type);
 
     chatReport.maxSequenceNumber = newSequenceNumber;
     chatReport.lastReadSequenceNumber = newSequenceNumber;
@@ -730,6 +730,7 @@ function cancelMoneyRequest(chatReportID, iouReportID, type, moneyRequestAction)
         cancelMoneyRequestReportActionID: optimisticReportAction.reportActionID,
     }, {optimisticData, successData, failureData});
 
+    // @TODO: should we navigate the user? What if they want to cancel multiple requests??
     Navigation.navigate(ROUTES.getReportRoute(chatReportID));
 }
 
