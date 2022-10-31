a0cd4bcd11a849d0ba2455fc6b9bfa01d2709f45
Kosuke Tseng <ctseng@expensify.com>
2022-10-31 16:20:07 -0700

    update workspaceInvoicesPage

--- src/pages/workspace/invoices/WorkspaceInvoicesPage.js
+++ src/pages/workspace/invoices/WorkspaceInvoicesPage.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
+import {ScrollView, View} from 'react-native';
+import {withOnyx} from 'react-native-onyx';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
-import WorkspacePageWithSections from '../WorkspacePageWithSections';
+import * as BankAccounts from '../../../libs/actions/BankAccounts';
+import BankAccount from '../../../libs/models/BankAccount';
+import CONST from '../../../CONST';
+import compose from '../../../libs/compose';
+import HeaderWithCloseButton from '../../../components/HeaderWithCloseButton';
+import Navigation from '../../../libs/Navigation/Navigation';
+import ONYXKEYS from '../../../ONYXKEYS';
+import ROUTES from '../../../ROUTES';
+import ScreenWrapper from '../../../components/ScreenWrapper';
+import styles from '../../../styles/styles';
+import withPolicy from '../withPolicy';
 import WorkspaceInvoicesNoVBAView from './WorkspaceInvoicesNoVBAView';
 import WorkspaceInvoicesVBAView from './WorkspaceInvoicesVBAView';
-import CONST from '../../../CONST';
 
 const propTypes = {
     /** The route object passed to this page from the navigator */
@@ -19,27 +31,53 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const WorkspaceInvoicesPage = props => (
-    <WorkspacePageWithSections
-        shouldUseScrollView
-        headerText={props.translate('workspace.common.invoices')}
-        route={props.route}
-        guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_INVOICES}
-    >
-        {(hasVBA, policyID) => (
-            <>
-                {!hasVBA && (
-                    <WorkspaceInvoicesNoVBAView policyID={policyID} />
-                )}
-                {hasVBA && (
-                    <WorkspaceInvoicesVBAView policyID={policyID} />
-                )}
-            </>
-        )}
-    </WorkspacePageWithSections>
-);
+class WorkspaceInvoicesPage extends React.Component {
+    componentDidMount() {
+        BankAccounts.openWorkspaceView();
+    }
+
+    render() {
+        const achState = lodashGet(this.props.reimbursementAccount, 'achData.state', '');
+        const hasVBBA = achState === BankAccount.STATE.OPEN;
+        const policyName = lodashGet(this.props.policy, 'name');
+        const policyID = lodashGet(this.props.route, 'params.policyID');
+        return (
+            <ScreenWrapper>
+                <HeaderWithCloseButton
+                    title={this.props.translate('workspace.common.invoices')}
+                    subtitle={policyName}
+                    shouldShowGetAssistanceButton
+                    guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_INVOICES}
+                    shouldShowBackButton
+                    onBackButtonPress={() => Navigation.navigate(ROUTES.getWorkspaceInitialRoute(policyID))}
+                    onCloseButtonPress={() => Navigation.dismissModal()}
+                />
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    style={[styles.settingsPageBackground, styles.flex1, styles.w100]}
+                >
+                    <View style={[styles.w100, styles.flex1]}>
+                        {!hasVBBA && (
+                            <WorkspaceInvoicesNoVBAView policyID={policyID} />
+                        )}
+                        {hasVBBA && (
+                            <WorkspaceInvoicesVBAView policyID={policyID} />
+                        )}
+                    </View>
+                </ScrollView>
+            </ScreenWrapper>
+        );
+    }
+}
 
 WorkspaceInvoicesPage.propTypes = propTypes;
-WorkspaceInvoicesPage.displayName = 'WorkspaceInvoicesPage';
 
-export default withLocalize(WorkspaceInvoicesPage);
+export default compose(
+    withPolicy,
+    withLocalize,
+    withOnyx({
+        reimbursementAccount: {
+            key: ONYXKEYS.REIMBURSEMENT_ACCOUNT,
+        },
+    }),
+)(WorkspaceInvoicesPage);
