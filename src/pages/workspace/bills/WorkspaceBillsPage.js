36f942a16e1c2a661fafefb6f159c54017d101a2
Kosuke Tseng <ctseng@expensify.com>
2022-10-31 22:00:48 -0700

    update workspaceBills

--- src/pages/workspace/bills/WorkspaceBillsPage.js
+++ src/pages/workspace/bills/WorkspaceBillsPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
+import {ScrollView, View} from 'react-native';
+import {withOnyx} from 'react-native-onyx';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
+import * as BankAccounts from '../../../libs/actions/BankAccounts';
+import BankAccount from '../../../libs/models/BankAccount';
+import compose from '../../../libs/compose';
+import CONST from '../../../CONST';
+import HeaderWithCloseButton from '../../../components/HeaderWithCloseButton';
+import Navigation from '../../../libs/Navigation/Navigation';
+import ONYXKEYS from '../../../ONYXKEYS';
+import reimbursementAccountPropTypes from '../../ReimbursementAccount/reimbursementAccountPropTypes';
+import ROUTES from '../../../ROUTES';
+import styles from '../../../styles/styles';
+import ScreenWrapper from '../../../components/ScreenWrapper';
+import withPolicy from '../withPolicy';
 import WorkspaceBillsNoVBAView from './WorkspaceBillsNoVBAView';
 import WorkspaceBillsVBAView from './WorkspaceBillsVBAView';
-import WorkspacePageWithSections from '../WorkspacePageWithSections';
-import CONST from '../../../CONST';
 
 const propTypes = {
+    /** From Onyx */
+    /** Bank account currently in setup */
+    reimbursementAccount: reimbursementAccountPropTypes,
+
     /** The route object passed to this page from the navigator */
     route: PropTypes.shape({
         /** Each parameter passed via the URL */
@@ -19,26 +36,58 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const WorkspaceBillsPage = props => (
-    <WorkspacePageWithSections
-        shouldUseScrollView
-        headerText={props.translate('workspace.common.bills')}
-        route={props.route}
-        guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_BILLS}
-    >
-        {(hasVBA, policyID) => (
-            <>
-                {!hasVBA && (
-                    <WorkspaceBillsNoVBAView policyID={policyID} />
-                )}
-                {hasVBA && (
-                    <WorkspaceBillsVBAView policyID={policyID} />
-                )}
-            </>
-        )}
-    </WorkspacePageWithSections>
-);
+const defaultProps = {
+    reimbursementAccount: {},
+};
 
+class WorkspaceBillsPage extends React.Component {
+    componentDidMount() {
+        BankAccounts.openWorkspaceView();
+    }
+
+    render() {
+        const achState = lodashGet(this.props.reimbursementAccount, 'achData.state', '');
+        const hasVBBA = achState === BankAccount.STATE.OPEN;
+        const policyName = lodashGet(this.props.policy, 'name');
+        const policyID = lodashGet(this.props.route, 'params.policyID');
+        return (
+            <ScreenWrapper>
+                <HeaderWithCloseButton
+                    title={this.props.translate('workspace.common.bills')}
+                    subtitle={policyName}
+                    shouldShowGetAssistanceButton
+                    guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_BILLS}
+                    shouldShowBackButton
+                    onBackButtonPress={() => Navigation.navigate(ROUTES.getWorkspaceInitialRoute(policyID))}
+                    onCloseButtonPress={() => Navigation.dismissModal()}
+                />
+                <ScrollView
+                    keyboardShouldPersistTaps="handled"
+                    style={[styles.settingsPageBackground, styles.flex1, styles.w100]}
+                >
+                    <View style={[styles.w100, styles.flex1]}>
+                        {!hasVBBA && (
+                            <WorkspaceBillsNoVBAView policyID={policyID} />
+                        )}
+                        {hasVBBA && (
+                            <WorkspaceBillsVBAView policyID={policyID} />
+                        )}
+                    </View>
+                </ScrollView>
+            </ScreenWrapper>
+        );
+    }
+}
+
+WorkspaceBillsPage.defaultProps = defaultProps;
 WorkspaceBillsPage.propTypes = propTypes;
-WorkspaceBillsPage.displayName = 'WorkspaceBillsPage';
-export default withLocalize(WorkspaceBillsPage);
+
+export default compose(
+    withPolicy,
+    withLocalize,
+    withOnyx({
+        reimbursementAccount: {
+            key: ONYXKEYS.REIMBURSEMENT_ACCOUNT,
+        },
+    }),
+)(WorkspaceBillsPage);
