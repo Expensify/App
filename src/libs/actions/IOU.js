e43b985aa78ccad3ec2f187d4e2edfa063f63f13
Carlos Martins <cmartins@expensify.com>
2022-11-01 09:36:37 -0600

    pass correct transactionID in request money

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -308,7 +308,7 @@ function requestMoney(report, amount, currency, recipientEmail, debtorEmail, deb
         comment,
         iouReportID: iouReport.reportID,
         chatReportID: chatReport.reportID,
-        transactionID: NumberUtils.rand64(),
+        transactionID: optimisticReportAction.originalMessage.IOUTransactionID,
         reportActionID: optimisticReportAction.reportActionID,
         clientID: optimisticReportAction.sequenceNumber,
     }, {optimisticData, successData, failureData});
