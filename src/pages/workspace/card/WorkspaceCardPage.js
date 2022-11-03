e9ea263c8c033310bb3549eedf822a5cd1b00f6c
Kosuke Tseng <ctseng@expensify.com>
2022-11-03 15:42:23 -0700

    update card

--- src/pages/workspace/card/WorkspaceCardPage.js
+++ src/pages/workspace/card/WorkspaceCardPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
+import {withOnyx} from 'react-native-onyx';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
+import * as BankAccounts from '../../../libs/actions/BankAccounts';
+import BankAccount from '../../../libs/models/BankAccount';
+import compose from '../../../libs/compose';
+import CONST from '../../../CONST';
+import ONYXKEYS from '../../../ONYXKEYS';
 import WorkspaceCardNoVBAView from './WorkspaceCardNoVBAView';
 import WorkspaceCardVBANoECardView from './WorkspaceCardVBANoECardView';
 import WorkspaceCardVBAWithECardView from './WorkspaceCardVBAWithECardView';
 import WorkspacePageWithSections from '../WorkspacePageWithSections';
-import CONST from '../../../CONST';
 
 const propTypes = {
     /** The route object passed to this page from the navigator */
@@ -20,32 +26,53 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const WorkspaceCardPage = props => (
-    <WorkspacePageWithSections
-        shouldUseScrollView
-        headerText={props.translate('workspace.common.card')}
-        route={props.route}
-        guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_CARD}
-    >
-        {(hasVBA, policyID, isUsingECard) => (
-            <>
-                {!hasVBA && (
-                    <WorkspaceCardNoVBAView policyID={policyID} />
-                )}
+const defaultProps = {
+    reimbursementAccount: {},
+};
 
-                {hasVBA && !isUsingECard && (
-                    <WorkspaceCardVBANoECardView />
-                )}
+class WorkspaceCardPage extends React.Component {
+    componentDidMount() {
+        BankAccounts.openWorkspaceView();
+    }
 
-                {hasVBA && isUsingECard && (
-                    <WorkspaceCardVBAWithECardView />
+    render() {
+        const achState = lodashGet(this.props.reimbursementAccount, 'achData.state', '');
+        const hasVBBA = achState === BankAccount.STATE.OPEN;
+        return (
+            <WorkspacePageWithSections
+                shouldUseScrollView
+                headerText={this.props.translate('workspace.common.card')}
+                route={this.props.route}
+                guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_CARD}
+            >
+                {(hasVBA, policyID, isUsingECard) => (
+                    <>
+                        {!hasVBBA && (
+                            <WorkspaceCardNoVBAView policyID={policyID} />
+                        )}
+
+                        {hasVBBA && !isUsingECard && (
+                            <WorkspaceCardVBANoECardView />
+                        )}
+
+                        {hasVBBA && isUsingECard && (
+                            <WorkspaceCardVBAWithECardView />
+                        )}
+                    </>
                 )}
-            </>
-        )}
-    </WorkspacePageWithSections>
-);
+            </WorkspacePageWithSections>
+        );
+    }
+}
 
 WorkspaceCardPage.propTypes = propTypes;
-WorkspaceCardPage.displayName = 'WorkspaceCardPage';
+WorkspaceCardPage.defaultProps = defaultProps;
 
-export default withLocalize(WorkspaceCardPage);
+export default compose(
+    withOnyx({
+        reimbursementAccount: {
+            key: ONYXKEYS.REIMBURSEMENT_ACCOUNT,
+        },
+    }),
+    withLocalize,
+)(WorkspaceCardPage);
