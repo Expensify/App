ca234a0d4fa4bddbf3f6893c030bf1c0bdb993a1
Kosuke Tseng <ctseng@expensify.com>
2022-11-03 15:44:58 -0700

    update invoices

--- src/pages/workspace/invoices/WorkspaceInvoicesPage.js
+++ src/pages/workspace/invoices/WorkspaceInvoicesPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
+import {withOnyx} from 'react-native-onyx';
 import withLocalize, {withLocalizePropTypes} from '../../../components/withLocalize';
+import * as BankAccounts from '../../../libs/actions/BankAccounts';
+import BankAccount from '../../../libs/models/BankAccount';
+import compose from '../../../libs/compose';
+import CONST from '../../../CONST';
+import ONYXKEYS from '../../../ONYXKEYS';
 import WorkspacePageWithSections from '../WorkspacePageWithSections';
 import WorkspaceInvoicesNoVBAView from './WorkspaceInvoicesNoVBAView';
 import WorkspaceInvoicesVBAView from './WorkspaceInvoicesVBAView';
-import CONST from '../../../CONST';
 
 const propTypes = {
     /** The route object passed to this page from the navigator */
@@ -19,27 +25,48 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const WorkspaceInvoicesPage = props => (
-    <WorkspacePageWithSections
-        shouldUseScrollView
-        headerText={props.translate('workspace.common.invoices')}
-        route={props.route}
-        guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_INVOICES}
-    >
-        {(hasVBA, policyID) => (
-            <>
-                {!hasVBA && (
-                    <WorkspaceInvoicesNoVBAView policyID={policyID} />
+const defaultProps = {
+    reimbursementAccount: {},
+};
+
+class WorkspaceInvoicesPage extends React.Component {
+    componentDidMount() {
+        BankAccounts.openWorkspaceView();
+    }
+
+    render() {
+        const achState = lodashGet(this.props.reimbursementAccount, 'achData.state', '');
+        const hasVBBA = achState === BankAccount.STATE.OPEN;
+        return (
+            <WorkspacePageWithSections
+                shouldUseScrollView
+                headerText={this.props.translate('workspace.common.invoices')}
+                route={this.props.route}
+                guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_INVOICES}
+            >
+                {(hasVBA, policyID) => (
+                    <>
+                        {!hasVBBA && (
+                            <WorkspaceInvoicesNoVBAView policyID={policyID} />
+                        )}
+                        {hasVBBA && (
+                            <WorkspaceInvoicesVBAView policyID={policyID} />
+                        )}
+                    </>
                 )}
-                {hasVBA && (
-                    <WorkspaceInvoicesVBAView policyID={policyID} />
-                )}
-            </>
-        )}
-    </WorkspacePageWithSections>
-);
+            </WorkspacePageWithSections>
+        );
+    }
+}
 
 WorkspaceInvoicesPage.propTypes = propTypes;
-WorkspaceInvoicesPage.displayName = 'WorkspaceInvoicesPage';
+WorkspaceInvoicesPage.defaultProps = defaultProps;
 
-export default withLocalize(WorkspaceInvoicesPage);
+export default compose(
+    withOnyx({
+        reimbursementAccount: {
+            key: ONYXKEYS.REIMBURSEMENT_ACCOUNT,
+        },
+    }),
+    withLocalize,
+)(WorkspaceInvoicesPage);
