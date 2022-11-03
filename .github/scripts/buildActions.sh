946eb0fee10f83f455eb754dec5e79f51fbfc1ed
Tim Golen <tgolen@expensify.com>
2022-11-03 08:49:44 -0600

    Modify workflows for author and reviewer checklists

--- .github/scripts/buildActions.sh
+++ .github/scripts/buildActions.sh
@@ -24,6 +24,8 @@ declare -r GITHUB_ACTIONS=(
     "$ACTIONS_DIR/triggerWorkflowAndWait/triggerWorkflowAndWait.js"
     "$ACTIONS_DIR/verifySignedCommits/verifySignedCommits.js"
     "$ACTIONS_DIR/contributorChecklist/contributorChecklist.js"
+    "$ACTIONS_DIR/authorChecklist/authorChecklist.js"
+    "$ACTIONS_DIR/reviewerChecklist/reviewerChecklist.js"
 )
 
 # This will be inserted at the top of all compiled files as a warning to devs.
