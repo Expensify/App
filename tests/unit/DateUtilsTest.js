035e95ff31a3cc8e58de4d432e29e2190b85bf90
Carlos Martins <cmartins@expensify.com>
2022-11-07 18:07:04 -0700

    fix test

--- tests/unit/DateUtilsTest.js
+++ tests/unit/DateUtilsTest.js
@@ -9,12 +9,12 @@ describe('DateUtils/getLocalMomentFromTimestamp', () => {
         const timezone = 'America/Los_Angeles';
         const localMoment = DateUtils.getLocalMomentFromTimestamp(locale, datetime, timezone);
         expect(moment.isMoment(localMoment)).toBe(true);
-        expect(moment(localMoment).format()).toEqual('2022-11-06T23:00:00-08:00');
+        expect(moment(localMoment).format()).toEqual('2022-11-06T16:00:00-08:00');
     });
 });
 
 describe('DateUtils/timestampToDateTime', () => {
     it('should return a moment object with the correct date and time', () => {
-        expect(DateUtils.timestampToDateTime(locale, datetime)).toBe('Yesterday at 11:00 PM');
+        expect(DateUtils.timestampToDateTime(locale, datetime)).toBe('Yesterday at 4:00 PM');
     });
 });
