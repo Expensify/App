c8d00b25593e6280d22fdf207da5b23c250c6fca
rory <rory@expensify.com>
2022-11-09 13:40:20 -0800

    Fix JS style (again)

--- src/pages/home/report/ReportActionsList.js
+++ src/pages/home/report/ReportActionsList.js
@@ -166,10 +166,10 @@ class ReportActionsList extends React.Component {
      * @returns {JSX.Element}
      */
     renderCell({
-       item,
-       index,
-       style,
-       ...props
+        item,
+        index,
+        style,
+        ...props
     }) {
         const cellStyle = [
             style,
