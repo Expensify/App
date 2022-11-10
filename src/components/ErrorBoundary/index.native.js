7bc444c4891c82bbb0edccb542dc7d3db56c4306
Marc Glasser <marc.aaron.glasser@gmail.com>
2022-11-09 14:38:53 -1000

    remove the check in error boundary

--- src/components/ErrorBoundary/index.native.js
+++ src/components/ErrorBoundary/index.native.js
@@ -2,15 +2,8 @@ import crashlytics from '@react-native-firebase/crashlytics';
 
 import BaseErrorBoundary from './BaseErrorBoundary';
 import Log from '../../libs/Log';
-import CONFIG from '../../CONFIG';
 
 BaseErrorBoundary.defaultProps.logError = (errorMessage, error, errorInfo) => {
-    // Do not log JS crashes to crashlytics if we are on dev or local release builds.
-    // They should only be sent on official staging or production versions of the app.
-    if (!CONFIG.SEND_CRASH_REPORTS) {
-        return;
-    }
-
     // Log the error to the server
     Log.alert(`${errorMessage} - ${error.message}`, {errorInfo}, false);
 
