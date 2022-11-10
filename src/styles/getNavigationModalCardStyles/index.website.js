aa92e40bb3e5dbda90d163995ca20d9db572260c
VikalpP <vikalppanseriya@gmail.com>
2022-11-10 09:54:08 +0530

    doc: add comments explaining the style changes

--- src/styles/getNavigationModalCardStyles/index.website.js
+++ src/styles/getNavigationModalCardStyles/index.website.js
@@ -2,5 +2,7 @@ import getBaseNavigationModalCardStyles from './getBaseNavigationModalCardStyles
 
 export default isSmallScreenWidth => ({
     ...getBaseNavigationModalCardStyles(isSmallScreenWidth),
+
+    // This makes the modal card take up the full height of the screen on Desktop Safari and iOS Safari avoiding bottom navigation bar.
     height: 'calc(100vh - 100%)',
 });
