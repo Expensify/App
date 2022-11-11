dcbbff0e4d4e8dffe0d9d53e4bab2c38e3539d7f
Daniel Gale-Rosen <dgalerosen@expensify.com>
2022-11-11 16:49:31 -0500

    remove removeclippedsubviews altogether"

--- src/components/InvertedFlatList/BaseInvertedFlatList.js
+++ src/components/InvertedFlatList/BaseInvertedFlatList.js
@@ -145,7 +145,6 @@ class BaseInvertedFlatList extends Component {
                 // We keep this property very low so that chat switching remains fast
                 maxToRenderPerBatch={1}
                 windowSize={15}
-                removeClippedSubviews={false}
                 maintainVisibleContentPosition={{minIndexForVisible: 0, autoscrollToTopThreshold: 0}}
             />
         );
