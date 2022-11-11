3aa9a3b9bcaa3d9b3c4e0abee4a720b4ea1f11ed
Rushat Gabhane <rushatgabhane@gmail.com>
2022-11-11 23:44:40 +0530

    fix: unclickable context menu

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -173,7 +173,7 @@ class ReportActionItem extends Component {
                     event.target.blur();
                 }}
             >
-                <Hoverable resetsOnClickOutside>
+                <Hoverable>
                     {hovered => (
                         <View>
                             {this.props.shouldDisplayNewIndicator && (
