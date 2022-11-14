d9cefe128d5403d591191d06331ea42d8914b4e4
Andre Fonseca <afonseca@expensify.com>
2022-11-14 17:19:56 +0000

    filling the details for the IOUDetailsModal

--- src/pages/iou/IOUDetailsModal.js
+++ src/pages/iou/IOUDetailsModal.js
@@ -102,13 +102,14 @@ class IOUDetailsModal extends Component {
         // If it's paying with the wallet lets call the correct service for this
         if (paymentMethodType === CONST.IOU.PAYMENT_TYPE.EXPENSIFY) {
             IOU.sendMoneyWithWallet(
-                lodashGet(this.props, 'route.params.reportID', ''),
+                this.props.route.params.chatReportID,
+                this.props.route.params.iouReportID,
                 '',
+                this.props.iouReport.total,
+                this.props.iouReport.currency,
                 '',
-                amount,
-                currency,
-                comment,
-                this.state.participants,
+                this.props.iouReport.ownerEmail,
+                this.props.iouReport.managerEmail,
             );
             return;
         }
