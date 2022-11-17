7d76729d3df76d7a9b31394485f14028d40d61d6
Tim Golen <tgolen@expensify.com>
2022-11-17 14:39:38 -0700

    Remove some of the old code

--- src/pages/signin/SignInPageLayout/SignInPageContent.js
+++ src/pages/signin/SignInPageLayout/SignInPageContent.js
@@ -47,6 +47,7 @@ const SignInPageContent = props => (
             ]}
         >
             <SignInPageForm style={[
+                styles.alignSelfStretch,
                 props.isSmallScreenWidth ? styles.ph5 : styles.ph4,
             ]}
             >
@@ -73,78 +74,6 @@ const SignInPageContent = props => (
         </View>
     </View>
 );
-
-// const SignInPageContent = props => (
-//     <KeyboardAvoidingView
-//         behavior="position"
-//         style={[
-//             StyleUtils.getModalPaddingStyles({
-//                 shouldAddBottomSafeAreaPadding: true,
-//                 modalContainerStylePaddingBottom: 20,
-//                 safeAreaPaddingBottom: props.insets.bottom,
-//             }),
-//             // props.isSmallScreenWidth ? styles.signInPageNarrowContentMargin : {},
-//             // !props.isMediumScreenWidth || (props.isMediumScreenWidth && props.windowHeight < variables.minHeightToShowGraphics) ? styles.signInPageWideLeftContentMargin : {},
-//             styles.mb3,
-//             {backgroundColor: 'red'},
-//         ]}
-//     >
-//         <View style={[styles.flex1, styles.h100, {backgroundColor: 'green'}]}>
-//             <View style={[{backgroundColor: 'yellow'}, styles.flexGrow1]}>
-//
-//             </View>
-//             <ScrollView
-//                 keyboardShouldPersistTaps="handled"
-//                 showsVerticalScrollIndicator={false}
-//                 style={[
-//                     styles.h100,
-//                     !props.isSmallScreenWidth && styles.alignSelfCenter,
-//                     !props.isSmallScreenWidth && styles.signInPageWideLeftContainer,
-//                 ]}
-//                 contentContainerStyle={[
-//                     scrollViewContentContainerStyles,
-//                     styles.alignItemsCenter,
-//                     !props.isSmallScreenWidth && styles.ph6,
-//                 ]}
-//             >
-//                 <View style={[styles.flex1, styles.flexRow]}>
-//                     <View style={[
-//                         styles.flex1,
-//                         styles.signInPageNarrowContentContainer,
-//                     ]}
-//                     >
-//                         <SignInPageForm style={[
-//                             styles.flex1,
-//                             styles.alignSelfStretch,
-//                             props.isSmallScreenWidth ? styles.ph5 : styles.ph4,
-//                         ]}
-//                         >
-//                             <View style={[
-//                                 styles.componentHeightLarge,
-//                                 ...(props.isSmallScreenWidth ? [styles.mb2] : [styles.mt6, styles.mb5]),
-//                             ]}
-//                             >
-//                                 <ExpensifyCashLogo
-//                                     width={variables.componentSizeLarge}
-//                                     height={variables.componentSizeLarge}
-//                                 />
-//                             </View>
-//                             {props.shouldShowWelcomeText && (
-//                                 <Text style={[styles.mv5, styles.textLabel, styles.h3]}>
-//                                     {props.welcomeText}
-//                                 </Text>
-//                             )}
-//                             {props.children}
-//                         </SignInPageForm>
-//                         <View style={[styles.mb5, styles.alignSelfCenter, styles.ph5]}>
-//                             <TermsAndLicenses />
-//                         </View>
-//                     </View>
-//                 </View>
-//             </ScrollView>
-//         </View>
-//     </KeyboardAvoidingView>
-// );
 
 SignInPageContent.propTypes = propTypes;
 SignInPageContent.displayName = 'SignInPageContent';
