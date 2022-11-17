f6d9205793af7f16e815ce6378a5f13867c7f453
Tim Golen <tgolen@expensify.com>
2022-11-17 09:24:10 -0700

    Make return types more consistent

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -973,7 +973,7 @@ function hasOutstandingIOU(report, currentUserLogin, iouReports) {
  * @param {Object} report
  * @param {String} report.iouReportID
  * @param {Object} iouReports
- * @returns {null|Number}
+ * @returns {Number}
  */
 function getIOUTotal(report, iouReports) {
     if (report.hasOutstandingIOU) {
@@ -982,7 +982,7 @@ function getIOUTotal(report, iouReports) {
             return iouReport.total;
         }
     }
-    return null;
+    return 0;
 }
 
 /**
@@ -990,7 +990,7 @@ function getIOUTotal(report, iouReports) {
  * @param {String} report.iouReportID
  * @param {String} currentUserLogin
  * @param {Object} iouReports
- * @returns {null|Boolean}
+ * @returns {Boolean}
  */
 function isIOUOwnedByCurrentUser(report, currentUserLogin, iouReports) {
     if (report.hasOutstandingIOU) {
@@ -999,7 +999,7 @@ function isIOUOwnedByCurrentUser(report, currentUserLogin, iouReports) {
             return iouReport.ownerEmail === currentUserLogin;
         }
     }
-    return null;
+    return false;
 }
 
 /**
