f8938cf23e84aa9635ba91d1b9c6d292a80c0bc7
rory <rory@expensify.com>
2022-11-16 17:49:39 -0800

    Nix deprecated babel-eslint and replace with @babel/eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['expensify', 'plugin:storybook/recommended'],
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     ignorePatterns: ['!.*', 'src/vendor', '.github/actions/**/index.js', 'desktop/dist/*.js', 'dist/*.js', 'node_modules/.bin/**', '.git/**'],
     env: {
         jest: true,
