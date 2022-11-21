1a0ee696dbe2b22204fa9a1c216614c370fe5bdc
Jasper Huang <jasper@expensify.com>
2022-11-21 13:24:42 -0800

    revert unused

--- src/pages/signin/SignInPageLayout/SignInPageContent.js
+++ src/pages/signin/SignInPageLayout/SignInPageContent.js
@@ -14,7 +14,6 @@ import scrollViewContentContainerStyles from './signInPageStyles';
 import withKeyboardState from '../../../components/withKeyboardState';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
 import * as StyleUtils from '../../../styles/StyleUtils';
-import PressableDismissKeyboard from '../../../components/PressableDismissKeyboard';
 
 const propTypes = {
     /** The children to show inside the layout */
@@ -32,67 +31,69 @@ const propTypes = {
 };
 
 const SignInPageContent = props => (
-    <PressableDismissKeyboard>
-        <View
-            style={[
-                styles.h100,
-                !props.isSmallScreenWidth && styles.alignSelfCenter,
-                !props.isSmallScreenWidth && styles.signInPageWideLeftContainer,
-                scrollViewContentContainerStyles,
-                styles.alignItemsCenter,
-                !props.isSmallScreenWidth && styles.ph6,
+    <ScrollView
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+        style={[
+            styles.h100,
+            !props.isSmallScreenWidth && styles.alignSelfCenter,
+            !props.isSmallScreenWidth && styles.signInPageWideLeftContainer,
+        ]}
+        contentContainerStyle={[
+            scrollViewContentContainerStyles,
+            styles.alignItemsCenter,
+            !props.isSmallScreenWidth && styles.ph6,
+        ]}
+    >
+        <View style={[styles.flex1, styles.flexRow]}>
+            <View style={[
+                styles.flex1,
+                styles.signInPageNarrowContentContainer,
             ]}
-        >
-            <View style={[styles.flex1, styles.flexRow]}>
-                <View style={[
+            >
+                <SignInPageForm style={[
                     styles.flex1,
-                    styles.signInPageNarrowContentContainer,
+                    styles.alignSelfStretch,
+                    props.isSmallScreenWidth ? styles.ph5 : styles.ph4,
                 ]}
                 >
-                    <SignInPageForm style={[
-                        styles.flex1,
-                        styles.alignSelfStretch,
-                        props.isSmallScreenWidth ? styles.ph5 : styles.ph4,
-                    ]}
+                    <KeyboardAvoidingView
+                        behavior="position"
+                        style={[
+                            StyleUtils.getModalPaddingStyles({
+                                shouldAddBottomSafeAreaPadding: true,
+                                modalContainerStylePaddingBottom: 20,
+                                safeAreaPaddingBottom: props.insets.bottom,
+                            }),
+                            props.isSmallScreenWidth ? styles.signInPageNarrowContentMargin : {},
+                            !props.isMediumScreenWidth || (props.isMediumScreenWidth && props.windowHeight < variables.minHeightToShowGraphics) ? styles.signInPageWideLeftContentMargin : {},
+                            styles.mb3,
+                        ]}
                     >
-                        <KeyboardAvoidingView
-                            behavior="position"
-                            style={[
-                                StyleUtils.getModalPaddingStyles({
-                                    shouldAddBottomSafeAreaPadding: true,
-                                    modalContainerStylePaddingBottom: 20,
-                                    safeAreaPaddingBottom: props.insets.bottom,
-                                }),
-                                props.isSmallScreenWidth ? styles.signInPageNarrowContentMargin : {},
-                                !props.isMediumScreenWidth || (props.isMediumScreenWidth && props.windowHeight < variables.minHeightToShowGraphics) ? styles.signInPageWideLeftContentMargin : {},
-                                styles.mb3,
-                            ]}
+                        <View style={[
+                            styles.componentHeightLarge,
+                            ...(props.isSmallScreenWidth ? [styles.mb2] : [styles.mt6, styles.mb5]),
+                        ]}
                         >
-                            <View style={[
-                                styles.componentHeightLarge,
-                                ...(props.isSmallScreenWidth ? [styles.mb2] : [styles.mt6, styles.mb5]),
-                            ]}
-                            >
-                                <ExpensifyCashLogo
-                                    width={variables.componentSizeLarge}
-                                    height={variables.componentSizeLarge}
-                                />
-                            </View>
-                            {props.shouldShowWelcomeText && (
-                                <Text style={[styles.mv5, styles.textLabel, styles.h3]}>
-                                    {props.welcomeText}
-                                </Text>
-                            )}
-                            {props.children}
-                        </KeyboardAvoidingView>
-                    </SignInPageForm>
-                    <View style={[styles.mb5, styles.alignSelfCenter, styles.ph5]}>
-                        <TermsAndLicenses />
-                    </View>
+                            <ExpensifyCashLogo
+                                width={variables.componentSizeLarge}
+                                height={variables.componentSizeLarge}
+                            />
+                        </View>
+                        {props.shouldShowWelcomeText && (
+                            <Text style={[styles.mv5, styles.textLabel, styles.h3]}>
+                                {props.welcomeText}
+                            </Text>
+                        )}
+                        {props.children}
+                    </KeyboardAvoidingView>
+                </SignInPageForm>
+                <View style={[styles.mb5, styles.alignSelfCenter, styles.ph5]}>
+                    <TermsAndLicenses />
                 </View>
             </View>
         </View>
-    </PressableDismissKeyboard>
+    </ScrollView>
 );
 
 SignInPageContent.propTypes = propTypes;
