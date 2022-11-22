045b759282dccd09a522450202aa4e47ad98b3a5
rory <rory@expensify.com>
2022-11-21 17:41:09 -0800

    Add more test cases

--- tests/unit/MigrationTest.js
+++ tests/unit/MigrationTest.js
@@ -2,6 +2,7 @@ import Onyx from 'react-native-onyx';
 import _ from 'underscore';
 import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
 import CONST from '../../src/CONST';
+import Log from '../../src/libs/Log';
 import getPlatform from '../../src/libs/getPlatform';
 import AddLastActionCreated from '../../src/libs/migrations/AddLastActionCreated';
 import MoveToIndexedDB from '../../src/libs/migrations/MoveToIndexedDB';
@@ -13,20 +14,23 @@ jest.mock('../../src/libs/getPlatform');
 // This seems related: https://github.com/facebook/jest/issues/11876
 jest.useRealTimers();
 
+let LogSpy;
+
 describe('Migrations', () => {
     beforeAll(() => {
         Onyx.init({keys: ONYXKEYS});
+        LogSpy = jest.spyOn(Log, 'info');
         return waitForPromisesToResolve();
     });
 
     beforeEach(() => {
+        jest.clearAllMocks();
         Onyx.clear();
         return waitForPromisesToResolve();
     });
 
     describe('MoveToIndexedDb', () => {
         beforeEach(() => {
-            jest.clearAllMocks();
             getPlatform.mockImplementation(() => CONST.PLATFORM.WEB);
             jest.spyOn(Onyx, 'multiSet').mockImplementation(() => Promise.resolve());
             localStorage.clear();
@@ -99,6 +103,7 @@ describe('Migrations', () => {
             })
                 .then(AddLastActionCreated)
                 .then(() => {
+                    expect(LogSpy).toHaveBeenCalledWith('[Migrate Onyx] Adding lastActionCreated field to 2 reports');
                     const connectionID = Onyx.connect({
                         key: ONYXKEYS.COLLECTION.REPORT,
                         waitForCollectionCallbacks: true,
@@ -115,9 +120,25 @@ describe('Migrations', () => {
                 });
         });
 
+        it('Should skip if the report data already has the correct fields', () => {
+            Onyx.set(ONYXKEYS.COLLECTION.REPORT, {
+                report_1: {
+                    lastActionCreated: '2022-11-16 01:31:13.702',
+                },
+                report_2: {
+                    lastActionCreated: '2022-11-16 01:31:54.821',
+                },
+            })
+                .then(AddLastActionCreated)
+                .then(() => {
+                    expect(LogSpy).toHaveBeenCalledWith('[Migrate Onyx] Skipped migration AddLastActionCreated');
+                });
+        });
+
         it('Should work even if there is no report data', () => {
             AddLastActionCreated()
                 .then(() => {
+                    expect(LogSpy).toHaveBeenCalledWith('[Migrate Onyx] Skipped migration AddLastActionCreated');
                     const connectionID = Onyx.connect({
                         key: ONYXKEYS.COLLECTION.REPORT,
                         waitForCollectionCallbacks: true,
