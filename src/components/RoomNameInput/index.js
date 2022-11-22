f947319ecc4c01a771518dae0e436bc4b9c93d55
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2022-11-22 15:13:39 +0100

    Fix lint PropTypes is defined and never used

--- src/components/RoomNameInput/index.js
+++ src/components/RoomNameInput/index.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import CONST from '../../CONST';
 import withLocalize from '../withLocalize';
 import TextInput from '../TextInput';
