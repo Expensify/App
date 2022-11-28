3104a7e3fbf81523d6b7b3d17f7d715bbcdb6920
neil-marcellini <neil@expensify.com>
2022-11-28 13:22:59 -0800

    Revert "Ignore Push events when offline with devtools"
    This reverts commit e4d0e3f76fd61c522f0a792340f4e2495d320f38.

--- src/libs/Pusher/pusher.js
+++ src/libs/Pusher/pusher.js
@@ -5,14 +5,14 @@ import Pusher from './library';
 import TYPE from './EventType';
 import Log from '../Log';
 
-let isOffline = false;
+let shouldForceOffline = false;
 Onyx.connect({
     key: ONYXKEYS.NETWORK,
     callback: (network) => {
         if (!network) {
             return;
         }
-        isOffline = Boolean(network.shouldForceOffline) || network.isOffline;
+        shouldForceOffline = Boolean(network.shouldForceOffline);
     },
 });
 
@@ -125,8 +125,8 @@ function bindEventToChannel(channel, eventName, eventCallback = () => {}) {
 
     const chunkedDataEvents = {};
     const callback = (eventData) => {
-        if (isOffline) {
-            Log.info('[Pusher] Ignoring Push event because isOffline = true');
+        if (shouldForceOffline) {
+            Log.info('[Pusher] Ignoring a Push event because shouldForceOffline = true');
             return;
         }
 
