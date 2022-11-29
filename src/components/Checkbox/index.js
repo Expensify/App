96c6f0fa66b6ab37c103ac738d5a22726bb686b0
someone-here <eshgupta1217@gmail.com>
2022-11-29 23:59:29 +0530

    Fixed Styling and signed
    Signed-off-by: someone-here <eshgupta1217@gmail.com>

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import BaseCheckbox from "./BaseCheckbox";
+import React from 'react';
+import BaseCheckbox from './BaseCheckbox';
 
 const Checkbox = props => (
     <BaseCheckbox
         onMouseDown={props.onMouseDown ? props.onMouseDown : props.onPress}
         onPress={undefined}
+        // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
     />
 );
 
 Checkbox.propTypes = BaseCheckbox.propTypes;
 Checkbox.defaultProps = BaseCheckbox.defaultProps;
+Checkbox.displayName = 'Checkbox';
 
 export default Checkbox;
