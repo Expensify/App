d612bd97275ad6871779ff3724221de847541ed5
rory <rory@expensify.com>
2022-11-28 16:09:56 -0800

    Add basic migration

---
+++ src/libs/migrations/KeyReportActionsByReportActionID.js
@@ -0,0 +1,55 @@
+import _ from 'underscore';
+import Onyx from 'react-native-onyx';
+import Log from '../Log';
+import ONYXKEYS from '../../ONYXKEYS';
+
+/**
+ * This migration updates reportActions data to be keyed by reportActionID rather than by sequenceNumber.
+ *
+ * @returns {Promise}
+ */
+export default function () {
+    return new Promise((resolve) => {
+        let shouldSkipMigration = false;
+        const connectionID = Onyx.connect({
+            key: ONYXKEYS.COLLECTION.REPORT_ACTIONS,
+            waitForCollectionCallbacks: true,
+            callback: (allReportActions) => {
+                Onyx.disconnect(connectionID);
+                const newReportActions = {};
+
+                for (let i = 0; i < Object.entries(allReportActions).length; i++) {
+                    const [onyxKey, reportActionsForReport] = Object.entries(allReportActions)[i];
+                    const newReportActionsForReport = {};
+                    for (let j = 0; j < Object.entries(reportActionsForReport).length; j++) {
+                        const [reportActionKey, reportAction] = reportActionsForReport[j];
+
+                        // If we find a reportAction that's already keyed by reoprtActionID instead of sequenceNumber,
+                        // then we assume the migration already happened and return early.
+                        if (reportActionKey === reportAction.reportActionID && reportActionKey !== reportAction.sequenceNumber) {
+                            shouldSkipMigration = true;
+                            return;
+                        }
+
+                        // Remove the sequenceNumber key from the reportActions object
+                        newReportActionsForReport[reportActionKey] = null;
+
+                        // Move it to be keyed by reportActionID instead
+                        newReportActionsForReport[reportAction.reportActionID] = reportAction;
+                    }
+                    newReportActions[onyxKey] = newReportActionsForReport;
+                }
+
+                Log.info(`[Migrate Onyx] Re-keying reportActions by reportActionID for ${_.keys(newReportActions).length} reports.`);
+                // eslint-disable-next-line rulesdir/prefer-actions-set-data
+                Onyx.mergeCollection(ONYXKEYS.COLLECTION.REPORT_ACTIONS, newReportActions);
+            },
+        });
+
+        if (shouldSkipMigration) {
+            Log.info('[Migrate Onyx] Skipped migration KeyReportActionsByReportActionID');
+        }
+
+        return resolve();
+    });
+}
