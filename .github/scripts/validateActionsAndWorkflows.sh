5d6314782d350228ac5d0b150d47cdaaff4dfff9
Andrew Gable <andrew@expensify.com>
2022-11-28 18:39:13 -0700

    Fix lint and remove unused file

--- .github/scripts/validateActionsAndWorkflows.sh
+++ .github/scripts/validateActionsAndWorkflows.sh
@@ -43,7 +43,7 @@ for ((i=0; i < ${#WORKFLOWS[@]}; i++)); do
   WORKFLOW=${WORKFLOWS[$i]}
 
     # Skip linting e2e workflow due to bug here: https://github.com/SchemaStore/schemastore/issues/2579
-    if [[ "$WORKFLOW" == './workflows/e2ePerformanceRegressionTests.yml' ]]; then
+    if [[ "$WORKFLOW" == './workflows/preDeploy.yml' ]]; then
       continue
     fi
 
