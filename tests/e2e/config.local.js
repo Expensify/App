95fa715aae9dc94a3815b5733b4ec1544fcf9566
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-11-30 14:11:15 +0100

    fix: results not calculated correctly due to runs not devidable by 4

--- tests/e2e/config.local.js
+++ tests/e2e/config.local.js
@@ -1,6 +1,6 @@
 module.exports = {
     WARM_UP_RUNS: 1,
-    RUNS: 7,
+    RUNS: 8,
     APP_PATHS: {
         baseline: './android/app/build/outputs/apk/e2eRelease/app-e2eRelease.apk',
         compare: './android/app/build/outputs/apk/e2eRelease/app-e2eRelease.apk',
