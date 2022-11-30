d98e7b7b7ba77fe1454d7911f8c695277e03da2f
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-11-30 12:33:00 +0100

    make error handling more resilient

--- tests/e2e/testRunner.js
+++ tests/e2e/testRunner.js
@@ -180,10 +180,14 @@ const runTests = async () => {
         // Write logcat, meminfo, emulator info to file as well:
         require('node:child_process').execSync(`adb logcat -d > ${config.OUTPUT_DIR}/logcat.txt`);
         require('node:child_process').execSync(`adb shell "cat /proc/meminfo" > ${config.OUTPUT_DIR}/meminfo.txt`);
-        require('node:child_process').execSync(`cat ~/.android/avd/${process.env.AVD_NAME || 'test'}.avd/config.ini > ${config.OUTPUT_DIR}/emulator-config.ini`);
         require('node:child_process').execSync(`adb shell "getprop" > ${config.OUTPUT_DIR}/emulator-properties.txt`);
 
         require('node:child_process').execSync(`cat ${config.LOG_FILE}`);
+        try {
+            require('node:child_process').execSync(`cat ~/.android/avd/${process.env.AVD_NAME || 'test'}.avd/config.ini > ${config.OUTPUT_DIR}/emulator-config.ini`);
+        } catch (__) {
+            // the file might not exist
+        }
         process.exit(1);
     }
 };
