a1c38246abf1276708b28e540179e01aef1bf722
Osama <51829206+osama256@users.noreply.github.com>
2022-12-01 07:08:32 +0200

    Update src/styles/variables.js
    Co-authored-by: Carlos Martins <luacmartins@gmail.com>

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -5,7 +5,7 @@ import {PixelRatio} from 'react-native';
  * method always returns the defaultValue (first param). When the device font size increases/decreases, the PixelRatio.getFontScale() value increases/decreases as well. This means that if you have text
  * and its 'fontSize' is 19, the device font size changed to the 5th level on the iOS slider and the actual fontSize is 19 * PixelRatio.getFontScale() = 19 * 1.11 = 21.09. Since we are
  * disallowing font scaling we need to calculate it manually. We calculate it with: PixelRatio.getFontScale() * defaultValue > maxValue ? maxValue : defaultValue * PixelRatio
- * getFontScale() this equation means increases/decreases the fontSize when the device font size increases/decreases but do not increase it if the fontSize value exceed maxValue (second
+ * getFontScale() This means that the fontSize is increased/decreased when the device font size changes up to maxValue (second param)
  * param).
  * @param {Number} defaultValue
  * @param {Number} maxValue
