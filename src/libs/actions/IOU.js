53f5d7dfcb8f95eb691d4ce02b7cb5f75cede6df
Amy Evans <amy@expensify.com>
2022-12-01 13:45:33 -0500

    Move updating of hasOutstandingIOU to utility

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -73,7 +73,6 @@ function requestMoney(report, amount, currency, recipientEmail, participant, com
             amount,
             currency,
         );
-        iouReport.hasOutstandingIOU = iouReport.total !== 0;
     } else {
         iouReport = ReportUtils.buildOptimisticIOUReport(recipientEmail, debtorEmail, amount, chatReport.reportID, currency, preferredLocale);
     }
