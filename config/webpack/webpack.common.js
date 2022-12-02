7f1544d9a9303bdd0b8c7f5bb75e275970cb78a6
rory <rory@expensify.com>
2022-12-02 10:06:05 -0800

    Remove unrelated style-loader change

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -181,6 +181,16 @@ const webpackConfig = ({envFile = '.env', platform = 'web'}) => ({
                 ],
             },
             {
+                test: /splash.css$/i,
+                use: [{
+                    loader: 'style-loader',
+                    options: {
+                        insert: 'head',
+                        injectType: 'singletonStyleTag',
+                    },
+                }],
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
