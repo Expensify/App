f1974f26bc0d2ef26e462139e404e6c2b8aedc24
someone-here <eshgupta1217@gmail.com>
2022-12-05 00:11:38 +0530

    Fixed regression

---
+++ src/components/Checkbox/index.native.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseCheckbox from './BaseCheckbox';
+
+const Checkbox = props => (
+    <BaseCheckbox
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        onPress={props.onPress ? props.onPress : props.onMouseDown}
+        onMouseDown={undefined}
+    />
+);
+
+Checkbox.propTypes = BaseCheckbox.propTypes;
+Checkbox.defaultProps = BaseCheckbox.defaultProps;
+Checkbox.displayName = 'Checkbox';
+
+export default Checkbox;
