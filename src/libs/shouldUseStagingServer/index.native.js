0e3048e7cc433dd50216ca380ae0ede41d6b00d1
Vit Horacek <vit@expensify.com>
2022-12-05 13:31:13 +0000

    Update comment

--- src/libs/shouldUseStagingServer/index.native.js
+++ src/libs/shouldUseStagingServer/index.native.js
@@ -1,6 +1,10 @@
 import * as Environment from '../Environment/Environment';
 
-/*
+/**
+ * Helper method used to decide which API endpoint to call in the Native apps.
+ * We build the staging native apps with production env config so we cannot rely on those values,
+ * hence we will decide solely on the value of the shouldUseStagingServer value (always false in production).
+ *
  * @param {Boolean} stagingServerToggleState
  * @returns {Boolean}
  */
