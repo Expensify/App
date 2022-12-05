2e2f26d9544baecc543113b56748b1dce0890689
Luke Donahue <luke@expensify.com>
2022-12-05 14:18:42 -0800

    set width only

--- src/components/AvatarCropModal/AvatarCropModal.js
+++ src/components/AvatarCropModal/AvatarCropModal.js
@@ -315,7 +315,7 @@ const AvatarCropModal = (props) => {
                                 translateX={translateX}
                                 rotation={rotation}
                             />
-                            <View style={[styles.mt5, styles.justifyContentBetween, styles.alignItemsCenter, styles.flexRow, StyleUtils.getWidthAndHeightStyle(imageContainerSize)]}>
+                            <View style={[styles.mt5, styles.justifyContentBetween, styles.alignItemsCenter, styles.flexRow, {width: imageContainerSize}]}>
                                 <Icon src={Expensicons.Zoom} fill={themeColors.icons} />
                                 <Pressable
                                     style={[styles.mh5, styles.flex1]}
