4c1f22827170a039ce9cf0cb03a11295da3529be
Rocio Perez-Cano <rocio@expensify.com>
2022-12-06 14:29:16 -0500

    Move comment

--- src/libs/actions/App.js
+++ src/libs/actions/App.js
@@ -211,11 +211,11 @@ function setUpPoliciesAndNavigate(session) {
         return;
     }
     if (!isLoggingInAsNewUser && exitTo) {
-        // The drawer navigation is only created after we have fetched reports from the server.
-        // Thus, if we use the standard navigation and try to navigate to a drawer route before
-        // the reports have been fetched, we will fail to navigate.
         Navigation.isNavigationReady()
             .then(() => {
+                // The drawer navigation is only created after we have fetched reports from the server.
+                // Thus, if we use the standard navigation and try to navigate to a drawer route before
+                // the reports have been fetched, we will fail to navigate.
                 Navigation.isDrawerReady()
                     .then(() => {
                         // We must call dismissModal() to remove the /transition route from history
