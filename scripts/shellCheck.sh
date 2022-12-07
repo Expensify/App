ec1f2771eeac6ffa112e500d49996303ac0dfdb8
rory <rory@expensify.com>
2022-12-06 17:03:01 -0800

    Add proper handling of exit code

--- scripts/shellCheck.sh
+++ scripts/shellCheck.sh
@@ -8,14 +8,18 @@ cd "$ROOT_DIR" || exit 1
 # This lists all shell scripts in this repo except those in directories we want to ignore
 SHELL_SCRIPTS=$(find . -type d \( -path ./node_modules -o -path ./vendor -o -path ./ios/Pods \) -prune -o -name '*.sh' -print)
 
-declare ASYNC_PROCESSES=()
+ASYNC_PROCESSES=()
 for SHELL_SCRIPT in $SHELL_SCRIPTS; do
   npx shellcheck "$SHELL_SCRIPT" &
   ASYNC_PROCESSES+=($!)
 done
 
+EXIT_CODE=0
 for PID in "${ASYNC_PROCESSES[@]}"; do
-  wait "$PID"
+  if ! wait "$PID"; then
+    EXIT_CODE=1
+  fi
 done
 
 cd "$CURRENT_DIR" || exit 1
+exit $EXIT_CODE
