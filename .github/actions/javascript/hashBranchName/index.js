45b0acd08c5524bbc4b91718d99f239b9a5da882
staszekscp <mariusz.stanisz@swmansion.com>
2022-12-08 11:18:56 +0100

    fin linter issues

--- .github/actions/javascript/hashBranchName/index.js
+++ .github/actions/javascript/hashBranchName/index.js
@@ -16,11 +16,10 @@ const pullRequestHeadRef = core.getInput('PULL_REQUEST_HEAD_REF');
 const refName = core.getInput('REF_NAME');
 
 const hashBranchName = function () {
-    if (eventName === 'workflow_dispatch') {
-        return btoa(refName).replace(/[^a-zA-Z0-9 ]/g, '');
-    } else {
-        return btoa(pullRequestHeadRef).replace(/[^a-zA-Z0-9 ]/g, '');
-    }
+  if (eventName === 'workflow_dispatch') {
+    return btoa(refName).replace(/[^a-zA-Z0-9 ]/g, '');
+  }
+  return btoa(pullRequestHeadRef).replace(/[^a-zA-Z0-9 ]/g, '');
 };
 
 core.setOutput('BRANCH_NAME_HASH', hashBranchName());
