2f2a2cd3c3e052765153365f44e85639b3f40046
Aldo Canepa <aldo@expensify.com>
2022-12-07 16:54:41 -0800

    Higher initial number to render

--- src/components/OptionsList/BaseOptionsList.js
+++ src/components/OptionsList/BaseOptionsList.js
@@ -229,7 +229,7 @@ class BaseOptionsList extends Component {
                     getItemLayout={this.getItemLayout}
                     renderSectionHeader={this.renderSectionHeader}
                     extraData={this.props.focusedIndex}
-                    initialNumToRender={5}
+                    initialNumToRender={12}
                     maxToRenderPerBatch={5}
                     windowSize={5}
                     viewabilityConfig={this.viewabilityConfig}
