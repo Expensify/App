153a5908f73c7cae956a239e88a7b559ea22a349
Daraksha <daraksha.dk001@gmail.com>
2022-12-09 22:09:18 +0530

    add tooltip for currency picker

--- src/components/CurrencySymbolButton.js
+++ src/components/CurrencySymbolButton.js
@@ -3,6 +3,8 @@ import {TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 import Text from './Text';
 import styles from '../styles/styles';
+import Tooltip from './Tooltip';
+import withLocalize, {withLocalizePropTypes} from './withLocalize';
 
 const propTypes = {
     /** Currency symbol of selected currency */
@@ -10,17 +12,21 @@ const propTypes = {
 
     /** Function to call when currency button is pressed */
     onCurrencyButtonPress: PropTypes.func.isRequired,
+
+    ...withLocalizePropTypes,
 };
 
 function CurrencySymbolButton(props) {
     return (
-        <TouchableOpacity onPress={props.onCurrencyButtonPress}>
-            <Text style={styles.iouAmountText}>{props.currencySymbol}</Text>
-        </TouchableOpacity>
+        <Tooltip text={props.translate('common.currency')}>
+            <TouchableOpacity onPress={props.onCurrencyButtonPress}>
+                <Text style={styles.iouAmountText}>{props.currencySymbol}</Text>
+            </TouchableOpacity>
+        </Tooltip>
     );
 }
 
 CurrencySymbolButton.propTypes = propTypes;
 CurrencySymbolButton.displayName = 'CurrencySymbolButton';
 
-export default CurrencySymbolButton;
+export default withLocalize(CurrencySymbolButton);
