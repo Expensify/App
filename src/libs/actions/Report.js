0e02e78bbdb10595db9e9ee5ac2726a27f92b433
Brandon Stites <bstites+1@expensify.com>
2022-12-12 14:19:21 -0700

    Remove code from wrong PR

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -905,7 +905,6 @@ function editReportComment(reportID, originalReportAction, textForNewComment) {
     // Do not autolink if someone explicitly tries to remove a link from message.
     // https://github.com/Expensify/App/issues/9090
     const htmlForNewComment = parser.replace(textForNewComment, {filterRules: _.filter(_.pluck(parser.rules, 'name'), name => name !== 'autolink')});
-    const originalMessageHTML = parser.replace(originalReportAction.message[0].html, {filterRules: _.filter(_.pluck(parser.rules, 'name'), name => name !== 'autolink')});
 
     //  Delete the comment if it's empty
     if (_.isEmpty(htmlForNewComment)) {
@@ -914,7 +913,7 @@ function editReportComment(reportID, originalReportAction, textForNewComment) {
     }
 
     // Skip the Edit if message is not changed
-    if (originalMessageHTML === htmlForNewComment.trim()) {
+    if (originalReportAction.message[0].html === htmlForNewComment.trim()) {
         return;
     }
 
