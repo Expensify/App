8aed1c7c6ca7d3216fa42ac980ff001b272793cc
Tim Golen <tgolen@expensify.com>
2022-12-12 11:49:53 -0700

    Play around with keyboard detection on iOS

--- src/pages/signin/SignInPageLayout/SignInPageContent.js
+++ src/pages/signin/SignInPageLayout/SignInPageContent.js
@@ -12,6 +12,7 @@ import SignInPageForm from '../../../components/SignInPageForm';
 import compose from '../../../libs/compose';
 import withKeyboardState from '../../../components/withKeyboardState';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
+import KeyboardAvoidingView from '../../../components/KeyboardAvoidingView';
 
 const propTypes = {
     /** The children to show inside the layout */
@@ -29,6 +30,7 @@ const propTypes = {
 };
 
 const SignInPageContent = (props) => {
+    console.log('!!!', props.isShown, props.windowHeight);
     const dismissKeyboardWhenTappedOutsideOfInput = () => {
         // This prop comes from withKeyboardState
         if (!props.isShown) {
@@ -44,38 +46,39 @@ const SignInPageContent = (props) => {
                     styles.flex1,
                     styles.signInPageLeftContainer,
                     !props.isSmallScreenWidth && styles.signInPageLeftContainerWide,
+                    {backgroundColor: 'red'},
                 ]}
             >
-                <View style={[styles.flex1, styles.alignSelfCenter, styles.signInPageWelcomeFormContainer]}>
-                    {/* This empty view creates margin on the top of the sign in form which will shrink and grow depending on if the keyboard is open or not */}
-                    <View style={[styles.flexGrow1, styles.signInPageContentTopSpacer]} />
+                    <KeyboardAvoidingView behavior="padding" style={[styles.flex1, styles.alignSelfCenter, styles.signInPageWelcomeFormContainer]}>
+                        {/* This empty view creates margin on the top of the sign in form which will shrink and grow depending on if the keyboard is open or not */}
+                        <View style={[styles.flexGrow1, styles.signInPageContentTopSpacer, {backgroundColor: 'yellow'}, props.isShown && {backgroundColor: 'pink'}]} />
 
-                    <View style={[styles.flexGrow2]}>
-                        <SignInPageForm style={[styles.alignSelfStretch]}>
-                            <View style={[
-                                styles.componentHeightLarge,
-                                ...(props.isSmallScreenWidth ? [styles.mb2] : [styles.mt6, styles.mb5]),
-                            ]}
-                            >
-                                <ExpensifyCashLogo
-                                    width={variables.componentSizeLarge}
-                                    height={variables.componentSizeLarge}
-                                />
-                            </View>
-                            {props.shouldShowWelcomeText && (
-                                <View style={[styles.signInPageWelcomeTextContainer]}>
-                                    <Text style={[styles.mv5, styles.textLabel, styles.h3]}>
-                                        {props.welcomeText}
-                                    </Text>
+                        <View style={[styles.flexGrow2, {backgroundColor: 'blue'}]}>
+                            <SignInPageForm style={[styles.alignSelfStretch]}>
+                                <View style={[
+                                    styles.componentHeightLarge,
+                                    ...(props.isSmallScreenWidth ? [styles.mb2] : [styles.mt6, styles.mb5]),
+                                ]}
+                                >
+                                    <ExpensifyCashLogo
+                                        width={variables.componentSizeLarge}
+                                        height={variables.componentSizeLarge}
+                                    />
                                 </View>
-                            )}
-                            {props.children}
-                        </SignInPageForm>
-                    </View>
-                    <View style={[styles.mv5]}>
-                        <TermsAndLicenses />
-                    </View>
-                </View>
+                                {props.shouldShowWelcomeText && (
+                                    <View style={[styles.signInPageWelcomeTextContainer]}>
+                                        <Text style={[styles.mv5, styles.textLabel, styles.h3]}>
+                                            {props.windowHeight}{props.welcomeText}
+                                        </Text>
+                                    </View>
+                                )}
+                                {props.children}
+                            </SignInPageForm>
+                        </View>
+                        <View style={[styles.mv5, {backgroundColor: 'green'}]}>
+                            <TermsAndLicenses />
+                        </View>
+                    </KeyboardAvoidingView>
             </View>
         </TouchableWithoutFeedback>
     );
