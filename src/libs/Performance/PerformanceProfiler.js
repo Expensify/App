11b35128a31aa68eb07cb11a2d81fc2b03a837e8
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-12-13 19:46:13 +0100

    disable logging

--- src/libs/Performance/PerformanceProfiler.js
+++ src/libs/Performance/PerformanceProfiler.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import {PerformanceProfiler as RNPerformanceProfiler, LogLevel} from '@shopify/react-native-performance';
+import {PerformanceProfiler as RNPerformanceProfiler} from '@shopify/react-native-performance';
 import PropTypes from 'prop-types';
 
+// eslint-disable-next-line no-unused-vars
 const onReportPrepared = (report) => {
-    console.log(`>>${JSON.stringify(report, null, 2).replaceAll('\n', '\n>>')}`);
+    // console.log(`>>${JSON.stringify(report, null, 2).replaceAll('\n', '\n>>')}`);
 };
 
 const propTypes = {
     children: PropTypes.node.isRequired,
 };
 
+const loggingDisabled = 4;
+
 // TODO: does this implementation work on web as well?
 const PerformanceProfiler = props => (
-    <RNPerformanceProfiler onReportPrepared={onReportPrepared} renderTimeoutMillis={20_000} logLevel={LogLevel.Debug}>
+    <RNPerformanceProfiler onReportPrepared={onReportPrepared} renderTimeoutMillis={20_000} logLevel={loggingDisabled}>
         {props.children}
     </RNPerformanceProfiler>
 );
