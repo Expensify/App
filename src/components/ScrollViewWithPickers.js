38d24f89cb92920d51389af880322ad5c95b25a3
Christoph Pader <christoph@margelo.io>
2022-12-14 22:53:47 +0100

    add comment for ScrollViewWithPickers component

--- src/components/ScrollViewWithPickers.js
+++ src/components/ScrollViewWithPickers.js
@@ -7,6 +7,13 @@ export const ScrollViewWithPickersContext = React.createContext();
 // eslint-disable-next-line react/forbid-foreign-prop-types
 const propTypes = ScrollView.propTypes;
 
+/*
+* <ScrollViewWithPickers /> is a wrapper around <ScrollView /> that provides a ref to the <ScrollView />.
+* <ScrollViewWithPickers /> can be used as a direct replacement for <ScrollView />
+* if it contains one or more <Picker /> / <RNPickerSelect /> components.
+* Using this wrapper will automatically hadnle scrolling to the picker's <TextInput />
+* when the picker modal is opened
+*/
 export default function ScrollViewWithPickers(props) {
     // eslint-disable-next-line react/destructuring-assignment
     const {children, ...restProps} = props;
