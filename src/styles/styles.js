94925f74b3e3a4abc3855d3e3451b944f0540eb8
Luke Donahue <luke@expensify.com>
2022-12-14 15:13:54 -0800

    slight adjustment to dropdown height

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -796,6 +796,7 @@ const styles = {
         borderColor: themeColors.border,
         justifyContent: 'center',
         backgroundColor: 'transparent',
+        height: variables.inputHeight
     },
 
     pickerLabel: {
