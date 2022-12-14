05e466de2d3b13e1dcae986ccdc309c5fa7a5d21
rory <rory@expensify.com>
2022-12-14 11:51:17 -0800

    Finish removing IE support

--- src/libs/DeviceCapabilities/hasHoverSupport/index.js
+++ src/libs/DeviceCapabilities/hasHoverSupport/index.js
@@ -3,15 +3,7 @@
  *
  * @returns {Boolean}
  */
-
-import * as Browser from '../../Browser';
-
 function hasHoverSupport() {
-    // First check internet explorer, because it doesn't support interaction media queries:
-    // https://caniuse.com/?search=media%20features
-    if (Browser.isInternetExplorer()) {
-        return !Browser.isMobile();
-    }
     return !window.matchMedia('(hover: none)').matches;
 }
 
