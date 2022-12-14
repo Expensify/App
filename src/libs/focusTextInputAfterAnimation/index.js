455cb41a0f95370dcbf8532014aba94f4b69abb2
Tim Golen <tgolen@expensify.com>
2022-12-14 12:56:45 -0700

    Fix typo

--- src/libs/focusTextInputAfterAnimation/index.js
+++ src/libs/focusTextInputAfterAnimation/index.js
@@ -1,6 +1,6 @@
 /**
  * This library is a no-op for all platforms except for Android and will immediately focus the given input without any delays. This is important for native iOS clients because
- * text inputs can only be focused from user interactions and wrapping the focus() inside a setTimeout breaks that use case since it's not longer triggered from a user interaction.
+ * text inputs can only be focused from user interactions and wrapping the focus() inside a setTimeout breaks that use case since it's no longer triggered from a user interaction.
  *
  * @param {Object} inputRef
  */
