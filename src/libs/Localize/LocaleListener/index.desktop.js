4e31474652982215073701f40b57d445dedf31a9
Rocio Perez-Cano <pecanoro@users.noreply.github.com>
2022-12-19 17:48:45 +0100

    Update src/libs/Localize/LocaleListener/index.desktop.js
    Co-authored-by: Rory Abraham <47436092+roryabraham@users.noreply.github.com>

--- src/libs/Localize/LocaleListener/index.desktop.js
+++ src/libs/Localize/LocaleListener/index.desktop.js
@@ -1,7 +1,14 @@
 import ELECTRON_EVENTS from '../../../../desktop/ELECTRON_EVENTS';
 import BaseLocaleListener from './BaseLocaleListener';
 
-const listenForLocaleChanges = () => {
+const connect = (callback) => {
+    BaseLocaleListener.connect((val) => {
+    	callback(val);
+
+    	// Send the updated locale to the Electron main process
+    	window.electron.send(ELECTRON_EVENTS.LOCALE_UPDATED, val);
+    });
+};
     // Send the updated locale to the Electron main process
     BaseLocaleListener.connect(val => window.electron.send(ELECTRON_EVENTS.LOCALE_UPDATED, val));
 };
