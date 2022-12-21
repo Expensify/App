980b375185b0a4efa71db7a09607a62b701c01b5
Luke Donahue <luke@expensify.com>
2022-12-21 12:46:09 -0800

    naming

--- src/libs/Localize.js
+++ src/libs/Localize.js
@@ -23,21 +23,21 @@ Onyx.connect({
 /**
  * Return translated string for given locale and phrase
  *
- * @param {String} [locale] eg 'en', 'es-ES'
+ * @param {String} [abreviatedDialect] eg 'en', 'es-ES'
  * @param {String|Array} phrase
  * @param {Object} [variables]
  * @returns {String}
  */
-function translate(locale = CONST.DEFAULT_LOCALE, phrase, variables = {}) {
-    const localeLanguage = locale.substring(0, 2);
-    const fullLocale = lodashGet(translations, locale, {});
-    const language = lodashGet(translations, localeLanguage, {});
+function translate(abreviatedDialect = CONST.DEFAULT_LOCALE, phrase, variables = {}) {
+    const abreviatedLanguage = abreviatedDialect.substring(0, 2);
+    const allTranslatedCopy = lodashGet(translations, abreviatedDialect, {});
+    const language = lodashGet(translations, abreviatedLanguage, {});
     const defaultLanguage = lodashGet(translations, 'en', {});
 
     let translationValue;
 
     // Search phrase in full locale
-    translationValue = lodashGet(fullLocale, phrase);
+    translationValue = lodashGet(allTranslatedCopy, phrase);
     if (translationValue) {
         return Str.result(translationValue, variables);
     }
@@ -47,8 +47,8 @@ function translate(locale = CONST.DEFAULT_LOCALE, phrase, variables = {}) {
     if (translationValue) {
         return Str.result(translationValue, variables);
     }
-    if (localeLanguage !== 'en') {
-        Log.alert(`${phrase} was not found in the ${localeLanguage} locale`);
+    if (abreviatedLanguage !== 'en') {
+        Log.alert(`${phrase} was not found in the ${abreviatedLanguage} locale`);
     }
 
     // Phrase is not translated, search it in default language (en)
