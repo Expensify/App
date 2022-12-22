0a369cc857f9c0ff930ce1127519dc14449305a7
Rocio Perez-Cano <rocio@expensify.com>
2022-12-22 21:00:04 +0100

    Add callback

--- src/libs/Localize/LocaleListener/index.desktop.js
+++ src/libs/Localize/LocaleListener/index.desktop.js
@@ -2,6 +2,11 @@ import ELECTRON_EVENTS from '../../../../desktop/ELECTRON_EVENTS';
 import BaseLocaleListener from './BaseLocaleListener';
 
 export default {
-    // Send the updated locale to the Electron main process
-    connect: () => BaseLocaleListener.connect(val => window.electron.send(ELECTRON_EVENTS.LOCALE_UPDATED, val)),
+    connect: (callbackAfterChange = () => {}) => BaseLocaleListener.connect((val) => {
+        // Send the updated locale to the Electron main process
+        window.electron.send(ELECTRON_EVENTS.LOCALE_UPDATED, val);
+
+        // Then execute the callback provided for the renderer process
+        callbackAfterChange(val);
+    }),
 };
