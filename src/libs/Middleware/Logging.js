3c524a23ef31fc839bdd492bdb9d895b10e6c4e6
neil-marcellini <neil@expensify.com>
2022-12-28 13:57:57 -0800

    Throw service interrupted error to next middleware

--- src/libs/Middleware/Logging.js
+++ src/libs/Middleware/Logging.js
@@ -116,6 +116,7 @@ function Logging(response, request) {
                 // Expensify site is down completely OR
                 // Auth (database connection) is down / bedrock has timed out while making a request. We currently can't tell the difference between Auth down and bedrock timing out.
                 Log.hmmm('[Network] API request error: Expensify service interrupted or timed out', logParams);
+                throw error;
             } else {
                 // If we get any error that is not known log an alert so we can learn more about it and document it here.
                 Log.alert(`${CONST.ERROR.ENSURE_BUGBOT} unknown API request error caught while processing request`, logParams, false);
