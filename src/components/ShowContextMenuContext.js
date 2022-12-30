ca1b3118390a9f1974e4c24545917e7e739069c5
b1tjoy <103875612+b1tjoy@users.noreply.github.com>
2022-12-30 11:16:22 +0800

    provide default property values for context consumer

--- src/components/ShowContextMenuContext.js
+++ src/components/ShowContextMenuContext.js
@@ -2,15 +2,21 @@ import React from 'react';
 import * as ReportActionContextMenu from '../pages/home/report/ContextMenu/ReportActionContextMenu';
 import * as ContextMenuActions from '../pages/home/report/ContextMenu/ContextMenuActions';
 
-const ShowContextMenuContext = React.createContext(null);
+const ShowContextMenuContext = React.createContext({
+    anchor: null,
+    reportID: null,
+    action: undefined,
+    checkIfContextMenuActive: () => {},
+});
+
 ShowContextMenuContext.displayName = 'ShowContextMenuContext';
 
 /**
  * Show the report action context menu.
  *
- * @param {Object} [event] - A press event
+ * @param {Object} event - Press event object
  * @param {Element} anchor - Context menu anchor
- * @param {String} reportID - Active Report Id
+ * @param {String} reportID - Active Report ID
  * @param {Object} action - ReportAction for ContextMenu
  * @param {Function} checkIfContextMenuActive Callback to update context menu active state
  */
