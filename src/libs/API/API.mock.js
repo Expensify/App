5bb084c9f8d6c2a4fdbf92dc087791aa70275464
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-01-03 12:04:09 -0800

    add mock API

---
+++ src/libs/API/API.mock.js
@@ -0,0 +1,67 @@
+/* eslint-disable rulesdir/no-api-in-views */
+import CONST from '../../CONST';
+import * as API from './API';
+
+/**
+ * All calls to API.write() will be persisted to disk as JSON with the params, successData, and failureData.
+ * This is so that if the network is unavailable or the app is closed, we can send the WRITE request later.
+ *
+ * @param {String} command - Name of API command to call.
+ * @param {Object} apiCommandParameters - Parameters to send to the API.
+ * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data that will be merged
+ *                             into Onyx before and after a request is made. Each nested object will be formatted in
+ *                             the same way as an API response.
+ * @param {Object} [onyxData.optimisticData] - Onyx instructions that will be passed to Onyx.update() before the request is made.
+ * @param {Object} [onyxData.successData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode === 200.
+ * @param {Object} [onyxData.failureData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode !== 200.
+ */
+function write(command, apiCommandParameters = {}, onyxData = {}) {
+    throw new Error('API.write() is not implemented in the mock API. Please use API.createTransaction() instead.');
+}
+
+/**
+ * For commands where the network response must be accessed directly or when there is functionality that can only
+ * happen once the request is finished (eg. calling third-party services like Onfido and Plaid, redirecting a user
+ * depending on the response data, etc.).
+ * It works just like API.read(), except that it will return a promise.
+ * Using this method is discouraged and will throw an ESLint error. Use it sparingly and only when all other alternatives have been exhausted.
+ * It is best to discuss it in Slack anytime you are tempted to use this method.
+ *
+ * @param {String} command - Name of API command to call.
+ * @param {Object} apiCommandParameters - Parameters to send to the API.
+ * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data that will be merged
+ *                             into Onyx before and after a request is made. Each nested object will be formatted in
+ *                             the same way as an API response.
+ * @param {Object} [onyxData.optimisticData] - Onyx instructions that will be passed to Onyx.update() before the request is made.
+ * @param {Object} [onyxData.successData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode === 200.
+ * @param {Object} [onyxData.failureData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode !== 200.
+ * @param {String} [apiRequestType] - Can be either 'read', 'write', or 'makeRequestWithSideEffects'. We use this to either return the chained
+ *                                    response back to the caller or to trigger reconnection callbacks when re-authentication is required.
+ * @returns {Promise}
+ */
+function makeRequestWithSideEffects(command, apiCommandParameters = {}, onyxData = {}, apiRequestType = CONST.API_REQUEST_TYPE.MAKE_REQUEST_WITH_SIDE_EFFECTS) {
+    throw new Error('API.makeRequestWithSideEffects() is not implemented in the mock API. Please use API.createTransaction() instead.');
+}
+
+/**
+ * Requests made with this method are not be persisted to disk. If there is no network connectivity, the request is ignored and discarded.
+ *
+ * @param {String} command - Name of API command to call.
+ * @param {Object} apiCommandParameters - Parameters to send to the API.
+ * @param {Object} onyxData  - Object containing errors, loading states, and optimistic UI data that will be merged
+ *                             into Onyx before and after a request is made. Each nested object will be formatted in
+ *                             the same way as an API response.
+ * @param {Object} [onyxData.optimisticData] - Onyx instructions that will be passed to Onyx.update() before the request is made.
+ * @param {Object} [onyxData.successData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode === 200.
+ * @param {Object} [onyxData.failureData] - Onyx instructions that will be passed to Onyx.update() when the response has jsonCode !== 200.
+ */
+function read(command, apiCommandParameters, onyxData) {
+    console.warn('[API.read] for command', command, 'is not mocket yet!');
+    return API.read(command, apiCommandParameters, onyxData);
+}
+
+export {
+    write,
+    makeRequestWithSideEffects,
+    read,
+};
