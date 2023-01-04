ddb53df2125a9cfa2c39e5311a71d88ce63803dc
rory <rory@expensify.com>
2023-01-04 11:07:47 -0800

    Fix reportActions test

--- tests/actions/ReportTest.js
+++ tests/actions/ReportTest.js
@@ -56,7 +56,7 @@ describe('actions/Report', () => {
         const TEST_USER_ACCOUNT_ID = 1;
         const TEST_USER_LOGIN = 'test@test.com';
         const REPORT_ID = 1;
-        const ACTION_ID = 1;
+        let reportActionID;
         const REPORT_ACTION = {
             actionName: CONST.REPORT.ACTIONS.TYPE.ADDCOMMENT,
             actorAccountID: TEST_USER_ACCOUNT_ID,
@@ -65,7 +65,7 @@ describe('actions/Report', () => {
             avatar: 'https://d2k5nsl2zxldvw.cloudfront.net/images/avatars/avatar_3.png',
             message: [{type: 'COMMENT', html: 'Testing a comment', text: 'Testing a comment'}],
             person: [{type: 'TEXT', style: 'strong', text: 'Test User'}],
-            sequenceNumber: ACTION_ID,
+            sequenceNumber: 1,
             shouldShow: true,
         };
 
@@ -74,8 +74,6 @@ describe('actions/Report', () => {
             key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${REPORT_ID}`,
             callback: val => reportActions = val,
         });
-
-        let clientID;
 
         // Set up Onyx with some test user data
         return TestHelper.signInWithTestUser(TEST_USER_ACCOUNT_ID, TEST_USER_LOGIN)
@@ -92,9 +90,8 @@ describe('actions/Report', () => {
             })
             .then(() => {
                 const resultAction = _.first(_.values(reportActions));
+                reportActionID = resultAction.reportActionID;
 
-                // Store the generated clientID so that we can send it with our mock Pusher update
-                clientID = resultAction.clientID;
                 expect(resultAction.message).toEqual(REPORT_ACTION.message);
                 expect(resultAction.person).toEqual(REPORT_ACTION.person);
                 expect(resultAction.pendingAction).toEqual(CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD);
@@ -102,8 +99,6 @@ describe('actions/Report', () => {
                 // We subscribed to the Pusher channel above and now we need to simulate a reportComment action
                 // Pusher event so we can verify that action was handled correctly and merged into the reportActions.
                 const channel = Pusher.getChannel(`${CONST.PUSHER.PRIVATE_USER_CHANNEL_PREFIX}1${CONFIG.PUSHER.SUFFIX}`);
-                const actionWithoutLoading = {...resultAction};
-                delete actionWithoutLoading.pendingAction;
                 channel.emit(Pusher.TYPE.ONYX_API_UPDATE, [
                     {
                         onyxMethod: CONST.ONYX.METHOD.MERGE,
@@ -121,8 +116,7 @@ describe('actions/Report', () => {
                         onyxMethod: CONST.ONYX.METHOD.MERGE,
                         key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${REPORT_ID}`,
                         value: {
-                            [clientID]: null,
-                            [ACTION_ID]: actionWithoutLoading,
+                            [reportActionID]: {pendingAction: null},
                         },
                     },
                 ]);
@@ -136,10 +130,10 @@ describe('actions/Report', () => {
                 // Verify there is only one action and our optimistic comment has been removed
                 expect(_.size(reportActions)).toBe(1);
 
-                const resultAction = reportActions[ACTION_ID];
+                const resultAction = reportActions[reportActionID];
 
                 // Verify that our action is no longer in the loading state
-                expect(resultAction.pendingAction).not.toBeDefined();
+                expect(resultAction.pendingAction).toBeNull();
             });
     });
 
@@ -338,41 +332,37 @@ describe('actions/Report', () => {
                     person: [{type: 'TEXT', style: 'strong', text: 'Test User'}],
                     shouldShow: true,
                     created: DateUtils.getDBTime(Date.now() - 3),
-                    reportActionID: 'derp1',
                 };
 
                 const optimisticReportActions = {
                     onyxMethod: CONST.ONYX.METHOD.MERGE,
                     key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${REPORT_ID}`,
                     value: {
-                        [_.toArray(reportActions)[1].clientID]: null,
-                        [_.toArray(reportActions)[2].clientID]: null,
-                        [_.toArray(reportActions)[3].clientID]: null,
-                        2: {
+                        200: {
                             ...USER_1_BASE_ACTION,
                             message: [{type: 'COMMENT', html: 'Current User Comment 1', text: 'Current User Comment 1'}],
                             created: DateUtils.getDBTime(Date.now() - 2),
                             sequenceNumber: 2,
-                            reportActionID: 'derp2',
+                            reportActionID: '200',
                         },
-                        3: {
+                        300: {
                             ...USER_1_BASE_ACTION,
                             message: [{type: 'COMMENT', html: 'Current User Comment 2', text: 'Current User Comment 2'}],
                             created: DateUtils.getDBTime(Date.now() - 1),
                             sequenceNumber: 3,
-                            reportActionID: 'derp3',
+                            reportActionID: '300',
                         },
-                        4: {
+                        400: {
                             ...USER_1_BASE_ACTION,
                             message: [{type: 'COMMENT', html: 'Current User Comment 3', text: 'Current User Comment 3'}],
                             created: DateUtils.getDBTime(),
                             sequenceNumber: 4,
-                            reportActionID: 'derp4',
+                            reportActionID: '400',
                         },
                     },
                 };
                 reportActionCreatedDate = DateUtils.getDBTime();
-                optimisticReportActions.value[4].created = reportActionCreatedDate;
+                optimisticReportActions.value[400].created = reportActionCreatedDate;
 
                 // When we emit the events for these pending created actions to update them to not pending
                 channel.emit(Pusher.TYPE.ONYX_API_UPDATE, [
@@ -397,7 +387,7 @@ describe('actions/Report', () => {
             })
             .then(() => {
                 // If the user deletes a comment that is before the last read
-                Report.deleteReportComment(REPORT_ID, {...reportActions[2], sequenceNumber: 2, clientID: null});
+                Report.deleteReportComment(REPORT_ID, {...reportActions[200]});
                 return waitForPromisesToResolve();
             })
             .then(() => {
@@ -415,7 +405,7 @@ describe('actions/Report', () => {
                 expect(report.lastReadTime).toBe(DateUtils.subtractMillisecondsFromDateTime(reportActionCreatedDate, 1));
 
                 // If the user deletes the last comment after the lastReadTime the lastMessageText will reflect the new last comment
-                Report.deleteReportComment(REPORT_ID, {...reportActions[4], sequenceNumber: 4, clientID: null});
+                Report.deleteReportComment(REPORT_ID, {...reportActions[400]});
                 return waitForPromisesToResolve();
             })
             .then(() => {
