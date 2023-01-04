6983064ea1c9aff9016256b7f7e2d0b4d3da6639
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-01-04 11:43:27 -0800

    Update FORMS.md doc with description of SafeAreaPadding gotcha. Add doc to SafeAreaConsumer

--- src/components/SafeAreaConsumer.js
+++ src/components/SafeAreaConsumer.js
@@ -11,6 +11,13 @@ const propTypes = {
     ]).isRequired,
 };
 
+/**
+ * This component is a light wrapper around the SafeAreaInsetsContext.Consumer. There are several places where we
+ * may need not just the insets, but the computed styles so we save a few lines of code with this.
+ *
+ * @param {Object} props
+ * @returns {React.Component}
+ */
 const SafeAreaConsumer = props => (
     <SafeAreaInsetsContext.Consumer>
         {(insets) => {
