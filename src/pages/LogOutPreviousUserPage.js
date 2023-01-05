0e318a18e6ad6b9cf90f1fb75a71dfb397da4c5b
Monil Bhavsar <monil@expensify.com>
2023-01-06 00:20:11 +0530

    Force login and refresh authToken

--- src/pages/LogOutPreviousUserPage.js
+++ src/pages/LogOutPreviousUserPage.js
@@ -20,10 +20,23 @@ class LogOutPreviousUserPage extends Component {
     componentDidMount() {
         Linking.getInitialURL()
             .then((transitionURL) => {
+
                 const sessionEmail = lodashGet(this.props.session, 'email', '');
                 const isLoggingInAsNewUser = SessionUtils.isLoggingInAsNewUser(transitionURL, sessionEmail);
+
                 if (isLoggingInAsNewUser) {
                     Session.signOutAndRedirectToSignIn();
+                }
+
+                // We need to signin and fetch a new authToken, if a user was already authenticated in NewDot, and was redirected to OlDot
+                // and their authToken stored in Onyx becomes invalid.
+                // This workflow is triggered while setting up VBBA. User is redirected from NewDot to OldDot to set up 2FA, and then redirected back to NewDot
+                // On Enabling 2FA, authToken stored in Onyx becomes expired and hence we need to fetch new authToken
+                const shouldForceLogin = lodashGet(this.props, 'route.params.shouldForceLogin', '') === 'true';
+                if (shouldForceLogin) {
+                    const email = lodashGet(this.props, 'route.params.email', '');
+                    const shortLivedAuthToken = lodashGet(this.props, 'route.params.shortLivedAuthToken', '');
+                    Session.signInWithShortLivedAuthToken(email, shortLivedAuthToken);
                 }
             });
     }
