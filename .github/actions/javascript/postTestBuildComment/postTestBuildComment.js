9166d101ee500083243189387150be70a7d809da
Michał Czernek <czernekmichal@gmail.com>
2023-01-10 14:11:01 +0100

    Test postTestBuildComment GH action.

--- .github/actions/javascript/postTestBuildComment/postTestBuildComment.js
+++ .github/actions/javascript/postTestBuildComment/postTestBuildComment.js
@@ -2,35 +2,34 @@ const core = require('@actions/core');
 const {context} = require('@actions/github');
 const GithubUtils = require('../../../libs/GithubUtils');
 
-const PR_NUMBER = core.getInput('PR_NUMBER', {required: true});
-
-const androidSuccess = core.getInput('ANDROID', {required: true}) === 'success';
-const desktopSuccess = core.getInput('DESKTOP', {required: true}) === 'success';
-const iOSSuccess = core.getInput('IOS', {required: true}) === 'success';
-const webSuccess = core.getInput('WEB', {required: true}) === 'success';
-
-const androidLink = androidSuccess ? core.getInput('ANDROID_LINK') : '❌ FAILED ❌';
-const desktopLink = desktopSuccess ? core.getInput('DESKTOP_LINK') : '❌ FAILED ❌';
-const iOSLink = iOSSuccess ? core.getInput('IOS_LINK') : '❌ FAILED ❌';
-const webLink = webSuccess ? core.getInput('WEB_LINK') : '❌ FAILED ❌';
-
-const androidQRCode = androidSuccess
-    ? `![Android](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${androidLink})`
-    : "The QR code can't be generated, because the android build failed";
-const desktopQRCode = desktopSuccess
-    ? `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${desktopLink})`
-    : "The QR code can't be generated, because the iOS build failed";
-const iOSQRCode = iOSSuccess
-    ? `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${iOSLink})`
-    : "The QR code can't be generated, because the desktop build failed";
-const webQRCode = webSuccess
-    ? `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${webLink})`
-    : "The QR code can't be generated, because the web build failed";
-
 /**
  * @returns {String}
  */
 function getTestBuildMessage() {
+    console.log('Input for android', core.getInput('ANDROID', {required: true}));
+    const androidSuccess = core.getInput('ANDROID', {required: true}) === 'success';
+    const desktopSuccess = core.getInput('DESKTOP', {required: true}) === 'success';
+    const iOSSuccess = core.getInput('IOS', {required: true}) === 'success';
+    const webSuccess = core.getInput('WEB', {required: true}) === 'success';
+
+    const androidLink = androidSuccess ? core.getInput('ANDROID_LINK') : '❌ FAILED ❌';
+    const desktopLink = desktopSuccess ? core.getInput('DESKTOP_LINK') : '❌ FAILED ❌';
+    const iOSLink = iOSSuccess ? core.getInput('IOS_LINK') : '❌ FAILED ❌';
+    const webLink = webSuccess ? core.getInput('WEB_LINK') : '❌ FAILED ❌';
+
+    const androidQRCode = androidSuccess
+        ? `![Android](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${androidLink})`
+        : "The QR code can't be generated, because the android build failed";
+    const desktopQRCode = desktopSuccess
+        ? `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${desktopLink})`
+        : "The QR code can't be generated, because the iOS build failed";
+    const iOSQRCode = iOSSuccess
+        ? `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${iOSLink})`
+        : "The QR code can't be generated, because the desktop build failed";
+    const webQRCode = webSuccess
+        ? `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${webLink})`
+        : "The QR code can't be generated, because the web build failed";
+
     let message = ':test_tube::test_tube: Use the links below to test this build in android and iOS. Happy testing! :test_tube::test_tube:';
     message += '\n| android :robot:  | iOS :apple: |';
     message += '\n| ------------- | ------------- |';
@@ -60,6 +59,7 @@ function commentPR(PR, message) {
 }
 
 const run = function () {
+    const PR_NUMBER = core.getInput('PR_NUMBER', {required: true});
     return commentPR(PR_NUMBER, getTestBuildMessage()).then(() => Promise.resolve());
 };
 
---
+++ tests/unit/postTestBuildComment.js
@@ -0,0 +1,61 @@
+const {when} = require('jest-when');
+
+const core = require('@actions/core');
+const GithubUtils = require('../../.github/libs/GithubUtils');
+
+const mockGetInput = jest.fn();
+const mockCreateComment = jest.fn();
+
+jest.mock('@actions/github', () => ({
+    context: {
+        repo: {
+            repo: 'repo',
+        },
+    },
+}));
+
+const ghAction = require('../../.github/actions/javascript/postTestBuildComment/postTestBuildComment');
+
+const androidLink = 'https://expensify.app/ANDROID_LINK';
+const iOSLink = 'https://expensify.app/IOS_LINK';
+const webLink = 'https://expensify.app/WEB_LINK';
+const desktopLink = 'https://expensify.app/DESKTOP_LINK';
+
+const androidQRCode = `![Android](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${androidLink})`;
+const desktopQRCode = `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${desktopLink})`;
+const iOSQRCode = `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${iOSLink})`;
+const webQRCode = `![iOS](https://api.qrserver.com/v1/create-qr-code/?size=120x120&data=${webLink})`;
+
+let message = ':test_tube::test_tube: Use the links below to test this build in android and iOS. Happy testing! :test_tube::test_tube:';
+message += '\n| android :robot:  | iOS :apple: |';
+message += '\n| ------------- | ------------- |';
+message += `\n| ${androidLink}  | ${iOSLink}  |`;
+message += `\n| ${androidQRCode}  | ${iOSQRCode}  |`;
+message += '\n| desktop :computer: | web :spider_web: |';
+message += `\n| ${desktopLink}  | ${webLink}  |`;
+message += `\n| ${desktopQRCode}  | ${webQRCode}  |`;
+
+describe('Post test build comments action tests', () => {
+    beforeAll(() => {
+        // Mock core module
+        core.getInput = mockGetInput;
+        GithubUtils.createComment = mockCreateComment;
+    });
+
+    test('Test GH action', () => {
+        when(core.getInput).calledWith('PR_NUMBER', {required: true}).mockReturnValue(12);
+        when(core.getInput).calledWith('ANDROID', {required: true}).mockReturnValue('success');
+        when(core.getInput).calledWith('IOS', {required: true}).mockReturnValue('success');
+        when(core.getInput).calledWith('WEB', {required: true}).mockReturnValue('success');
+        when(core.getInput).calledWith('DESKTOP', {required: true}).mockReturnValue('success');
+        when(core.getInput).calledWith('ANDROID_LINK').mockReturnValue('https://expensify.app/ANDROID_LINK');
+        when(core.getInput).calledWith('IOS_LINK').mockReturnValue('https://expensify.app/IOS_LINK');
+        when(core.getInput).calledWith('WEB_LINK').mockReturnValue('https://expensify.app/WEB_LINK');
+        when(core.getInput).calledWith('DESKTOP_LINK').mockReturnValue('https://expensify.app/DESKTOP_LINK');
+        GithubUtils.createComment.mockResolvedValue(true);
+        ghAction();
+        expect(GithubUtils.createComment).toBeCalledTimes(1);
+        expect(GithubUtils.createComment).toBeCalledWith('repo', 12, message);
+    });
+});
+
