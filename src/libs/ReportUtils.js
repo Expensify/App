cc8ce08f5a2a4b51ed9a49aa614593cb8e8c6cfb
Jack Nam <jack@expensify.com>
2023-01-11 10:23:47 -0800

    add back reportActionID

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -918,6 +918,7 @@ function buildOptimisticCreatedReportAction(ownerEmail) {
     return {
         0: {
             actionName: CONST.REPORT.ACTIONS.TYPE.CREATED,
+            reportActionID: NumberUtils.rand64(),
             pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
             actorAccountID: currentUserAccountID,
             message: [
