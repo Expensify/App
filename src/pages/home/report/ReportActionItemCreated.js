44b28f281f156eb820a565738674f2a87d67b9ec
Daniel Gale-Rosen <dgalerosen@expensify.com>
2023-01-11 15:58:18 -0500

    update image, change to imagebackground

--- src/pages/home/report/ReportActionItemCreated.js
+++ src/pages/home/report/ReportActionItemCreated.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pressable, Image, View} from 'react-native';
+import {Pressable, ImageBackground, View} from 'react-native';
 import lodashGet from 'lodash/get';
 import {withOnyx} from 'react-native-onyx';
 import PropTypes from 'prop-types';
@@ -53,7 +53,7 @@ const ReportActionItemCreated = (props) => {
             onClose={() => Report.navigateToConciergeChatAndDeleteReport(props.report.reportID)}
         >
             <View pointerEvents="none" style={[styles.emptyStateBackgroundContainer, StyleUtils.getReportWelcomeBackgroundImageStyle(props.isSmallScreenWidth)]}>
-                <Image
+                <ImageBackground
                     source={EmptyStateBackgroundImage}
                     style={[styles.emptyStateBackgroundImage]}
                 />
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2884,7 +2884,7 @@ const styles = {
 
     emptyStateBackgroundImage: {
         width: '100%',
-        height: '80%',
+        height: '100%',
         bottom: '-40%',
     },
 };
