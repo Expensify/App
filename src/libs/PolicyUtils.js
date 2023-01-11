059fe50d6d6f1a50e7c734d14572b524454dacfb
Jatin Soni <jatinsonijs@icloud.com>
2023-01-11 15:40:51 +0530

    Change comment

--- src/libs/PolicyUtils.js
+++ src/libs/PolicyUtils.js
@@ -59,7 +59,7 @@ function getPolicyBrickRoadIndicatorStatus(policy, policyMembers) {
  * If offline, always show the policy pending deletion.
  * If online, show the policy pending deletion only if there is an error.
  * Note: Using a local ONYXKEYS.NETWORK subscription will cause a delay in
- * updating the screen pass the offline status from the component.
+ * updating the screen. Passing the offline status from the component.
  * @param {Object} policy
  * @param {boolean} isOffline
  * @returns {boolean}
