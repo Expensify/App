85e25c8b7d8c47c9ae35903907a1d7c7cd7d0373
Kosuke Tseng <ctseng@expensify.com>
2023-01-13 14:23:32 -0800

    remove token

--- src/components/AddPlaidBankAccount.js
+++ src/components/AddPlaidBankAccount.js
@@ -119,8 +119,7 @@ class AddPlaidBankAccount extends React.Component {
         if (!plaidBankAccounts.length) {
             return (
                 <FullPageOfflineBlockingView>
-                    {(this.props.plaidData.isLoading || (!token && !plaidDataErrorMessage))
-                    && (
+                    {this.props.plaidData.isLoading && (
                         <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
                             <ActivityIndicator color={themeColors.spinner} size="large" />
                         </View>
