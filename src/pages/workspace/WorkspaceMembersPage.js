b85e275de98b0e56dc4ba6001093230247f52d13
Carlos Martins <cmartins@expensify.com>
2023-01-17 16:28:16 -0700

    keep select all checked if admin is selected

--- src/pages/workspace/WorkspaceMembersPage.js
+++ src/pages/workspace/WorkspaceMembersPage.js
@@ -346,7 +346,7 @@ class WorkspaceMembersPage extends React.Component {
                             <View style={[styles.peopleRow, styles.ph5, styles.pb3]}>
                                 <View style={[styles.peopleRowCell]}>
                                     <Checkbox
-                                        isChecked={this.state.selectedEmployees.length === removableMembers.length && removableMembers.length !== 0}
+                                        isChecked={this.state.selectedEmployees.length >= removableMembers.length && removableMembers.length !== 0}
                                         onPress={() => this.toggleAllUsers()}
                                     />
                                 </View>
