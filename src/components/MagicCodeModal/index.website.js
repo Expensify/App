27880b17a93b1acf73ebe61e1ae34bec30d2dc58
Cristi Paval <cristi@expensify.com>
2023-01-20 20:57:36 +0200

    Add Expensify wordmark in magic code modal.

--- src/components/MagicCodeModal/index.website.js
+++ src/components/MagicCodeModal/index.website.js
@@ -1,11 +1,27 @@
 import React, { PureComponent } from "react";
-import FullScreenLoadingIndicator from "../FullscreenLoadingIndicator";
+import { View } from "react-native";
+import colors from "../../styles/colors";
+import styles from '../../styles/styles';
+import Icon from "../Icon";
+import withLocalize from "../withLocalize";
+import * as Expensicons from '../Icon/Expensicons';
 
 class MagicCodeModal extends PureComponent {
 
     render() {
-        return <FullScreenLoadingIndicator />;
+        return (
+            <View style={styles.deeplinkWrapperContainer}>
+                <View style={styles.deeplinkWrapperFooter}>
+                    <Icon
+                        width={154}
+                        height={34}
+                        fill={colors.green}
+                        src={Expensicons.ExpensifyWordmark}
+                    />
+                </View>
+            </View>
+        );
     }
 }
 
-export default MagicCodeModal;
+export default withLocalize(MagicCodeModal);
