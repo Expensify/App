f787378306f04e04b9bd593e99de920af5f08847
Marco Chávez <marco@expensify.com>
2023-01-21 09:41:09 -0600

    add comment

--- src/components/ConfirmModal.js
+++ src/components/ConfirmModal.js
@@ -71,6 +71,8 @@ const ConfirmModal = props => (
     >
         <ConfirmContent
             title={props.title}
+
+            /* Disable onConfirm function if the modal is being dismissed */
             onConfirm={() => (props.isVisible ? props.onConfirm() : null)}
             onCancel={props.onCancel}
             confirmText={props.confirmText}
