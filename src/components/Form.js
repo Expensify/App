63ff50a32721a1d46eab5b5b8b623982a2e41c97
Christoph Pader <christoph@margelo.io>
2023-01-24 10:23:52 +0100

    fix: passed invalid ref to measureLayout

--- src/components/Form.js
+++ src/components/Form.js
@@ -292,7 +292,7 @@ class Form extends React.Component {
 
                         // We subtract 10 to scroll slightly above the input
                         if (focusInput.measureLayout && typeof focusInput.measureLayout === 'function') {
-                            focusInput.measureLayout(this.form, (x, y) => this.form.scrollTo({y: y - 10, animated: false}));
+                            focusInput.measureLayout(this.formRef.current, (x, y) => this.formRef.current.scrollTo({y: y - 10, animated: false}));
                         }
                     }}
                     containerStyles={[styles.mh0, styles.mt5, styles.flex1]}
