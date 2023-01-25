2a5ab7d8bea9193b918d1381f8ae2ba65d0088be
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-01-24 21:04:17 -0800

    fix lint

--- src/pages/ReimbursementAccount/BankAccountManualStep.js
+++ src/pages/ReimbursementAccount/BankAccountManualStep.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, View} from 'react-native';
+import {Image} from 'react-native';
 import lodashGet from 'lodash/get';
 import HeaderWithCloseButton from '../../components/HeaderWithCloseButton';
 import CONST from '../../CONST';
