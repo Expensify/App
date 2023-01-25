69dfd4722b7452129571b4d1b1a80c085b04e76d
Cristi Paval <cristi@expensify.com>
2023-01-25 18:39:28 +0200

    Add priority modes page header

--- src/pages/settings/Preferences/PriorityModesPage.js
+++ src/pages/settings/Preferences/PriorityModesPage.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import HeaderWithCloseButton from "../../../components/HeaderWithCloseButton";
 import ScreenWrapper from "../../../components/ScreenWrapper";
+import withLocalize, {withLocalizePropTypes} from "../../../components/withLocalize";
+import Navigation from "../../../libs/Navigation/Navigation";
+import ROUTES from "../../../ROUTES";
+
+const propTypes = {
+    ...withLocalizePropTypes,
+};
 
 class PriorityModesPage extends Component {
 
     render() {
         return <ScreenWrapper includeSafeAreaPaddingBottom={false}>
-            {'This is a text'}
+            <HeaderWithCloseButton
+                title={this.props.translate('preferencesPage.priorityMode')}
+                shouldShowBackButton
+                onBackButtonPress={() => Navigation.navigate(ROUTES.SETTINGS_PREFERENCES)}
+                onCloseButtonPress={() => Navigation.dismissModal(true)}
+            />
         </ScreenWrapper>;
     }
 }
 
-export default PriorityModesPage;
+PriorityModesPage.propTypes = propTypes;
+
+export default withLocalize(PriorityModesPage);
