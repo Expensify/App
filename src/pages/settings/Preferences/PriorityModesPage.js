735f4bdba5033224ef4f3eaa8025a000a23d9615
Cristi Paval <cristi@expensify.com>
2023-01-25 21:25:40 +0200

    Add options list in Priority Modes page

--- src/pages/settings/Preferences/PriorityModesPage.js
+++ src/pages/settings/Preferences/PriorityModesPage.js
@@ -4,23 +4,47 @@ import ScreenWrapper from "../../../components/ScreenWrapper";
 import withLocalize, {withLocalizePropTypes} from "../../../components/withLocalize";
 import Navigation from "../../../libs/Navigation/Navigation";
 import ROUTES from "../../../ROUTES";
+import CONST from '../../../CONST';
+import OptionsList from "../../../components/OptionsList";
+import styles from "../../../styles/styles";
 
 const propTypes = {
     ...withLocalizePropTypes,
 };
 
-class PriorityModesPage extends Component {
+const PriorityModesPage = (props) => {
 
-    render() {
-        return <ScreenWrapper includeSafeAreaPaddingBottom={false}>
+    const priorityModes = [
+        {
+            value: CONST.PRIORITY_MODE.DEFAULT,
+            text: props.translate('preferencesPage.mostRecent'),
+            keyForList: 'default',
+            description: props.translate('preferencesPage.mostRecentModeDescription'),
+        },
+        {
+            value: CONST.PRIORITY_MODE.GSD,
+            text: props.translate('preferencesPage.focus'),
+            keyForList: 'gsd',
+            description: props.translate('preferencesPage.focusModeDescription'),
+        },
+    ];
+
+        return (<ScreenWrapper includeSafeAreaPaddingBottom={false}>
             <HeaderWithCloseButton
-                title={this.props.translate('preferencesPage.priorityMode')}
+                title={props.translate('preferencesPage.priorityMode')}
                 shouldShowBackButton
                 onBackButtonPress={() => Navigation.navigate(ROUTES.SETTINGS_PREFERENCES)}
                 onCloseButtonPress={() => Navigation.dismissModal(true)}
             />
-        </ScreenWrapper>;
-    }
+            <OptionsList
+                sections={[{data: priorityModes}]}
+                // onSelectRow={option => updatePronouns(option.value)}
+                hideSectionHeaders
+                optionHoveredStyle={styles.hoveredComponentBG}
+                shouldHaveOptionSeparator
+                contentContainerStyles={[styles.ph5]}
+            />
+        </ScreenWrapper>);
 }
 
 PriorityModesPage.propTypes = propTypes;
