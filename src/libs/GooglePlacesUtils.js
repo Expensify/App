c94d73c14c2741dc872f15d51a89903bf0108838
David Bondy <dbondy@expensify.com>
2023-01-26 13:35:02 -0700

    splitting code into separate methods to more easily capture performance metrics and trying to stop loops with multiple interators

--- src/libs/GooglePlacesUtils.js
+++ src/libs/GooglePlacesUtils.js
@@ -22,59 +22,67 @@ function getAddressComponent(addressComponents, type, key) {
         .value();
 }
 
+function getAddressComponentsUnderscore(addressComponents, fieldsToExtract) {
+    const startTime = performance.now()
+    const result = _.mapObject(fieldsToExtract, () => '');
+    _.each(addressComponents, (addressComponent) => {
+        _.each(addressComponent.types, (addressType) => {
+            if (!_.has(fieldsToExtract, addressType) || !_.isEmpty(result[addressType])) {
+                return;
+            }
+            result[addressType] = lodashGet(addressComponent, fieldsToExtract[addressType], '');
+        });
+    });
+    const endTime = performance.now()
+    console.log(`Call to getAddressComponentsUnderscore took ${endTime - startTime}ms`);
+    return result;
+}
+
+function getAddressComponentsNested(addressComponents, fieldsToExtract) {
+    const startTime = performance.now()
+
+    const result = {};
+    for(const field in fieldsToExtract) {
+        const typeToFind = field;
+        const nameToFind = fieldsToExtract[field];
+        const addressComponent = addressComponents.find((elm, indx) => {
+            if (_.isArray(elm['types'])) {
+                return elm.types.find((type) => {
+                    return type === typeToFind;
+                })
+            } else {
+                return false;
+            }
+        });
+
+        result[typeToFind] = addressComponent ? addressComponent[nameToFind] : '';
+    }
+    const endTime = performance.now()
+    console.log(`Call to getAddressComponentsNested took ${endTime - startTime}ms`);
+    return result;
+}
+
 function getAddressComponents(addressComponents, fieldsToExtract) {
     const startTime = performance.now()
-    //const result = _.mapObject(fieldsToExtract, () => '');
-    //_.each(addressComponents, (addressComponent) => {
-    //    _.each(addressComponent.types, (addressType) => {
-    //        if (!_.has(fieldsToExtract, addressType) || !_.isEmpty(result[addressType])) {
-    //            return;
-    //        }
-    //        result[addressType] = lodashGet(addressComponent, fieldsToExtract[addressType], '');
-    //    });
-    //});
-
-    //const result = {};
-    //for(const field in fieldsToExtract) {
-    //    const typeToFind = field;
-    //    const nameToFind = fieldsToExtract[field];
-    //    const addressComponent = addressComponents.find((elm, indx) => {
-    //        if (_.isArray(elm['types'])) {
-    //            return elm.types.find((type) => {
-    //                return type === typeToFind;
-    //            })
-    //        } else {
-    //            return false;
-    //        }
-    //    });
-
-    //    result[typeToFind] = addressComponent ? addressComponent[nameToFind] : '';
-    //}
-
     // We want to avoid using a nested loops in conjunction with nested array built-ins so that we avoid
     // large O(n^2) complexity so separate out finding the objects we want from the loop of building an object to return
     const typesToFind = Object.keys(fieldsToExtract);
     const nameToFind = Object.values(fieldsToExtract);
-    console.log({typesToFind, nameToFind});
     const matchedComponents = addressComponents.filter(component => component['types'] && component['types'].some(type => typesToFind.indexOf(type) >= 0));
-    console.log(matchedComponents);
 
     const result = typesToFind.reduce((obj, type, indx) => {
-        console.log(matchedComponents[indx]);
-        console.log(nameToFind[indx]);
-        console.log(obj);
         return {...obj, [type]: (matchedComponents[indx] ? matchedComponents[indx][nameToFind[indx]] : '')};
     }, {});
 
-    console.log(result);
-
     const endTime = performance.now()
-    console.log(`Call to getAddressComponents took ${endTime - startTime} milliseconds`);
+    console.log(`Call to getAddressComponents took ${endTime - startTime}ms`);
     return result;
 }
 
 export {
     // eslint-disable-next-line import/prefer-default-export
     getAddressComponent,
+    getAddressComponentsUnderscore,
+    getAddressComponentsNested,
     getAddressComponents,
 };
