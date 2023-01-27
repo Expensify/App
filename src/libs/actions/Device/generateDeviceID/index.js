6621f1e3516557ad6e24bf052bacc7f1119625d4
rory <rory@expensify.com>
2023-01-26 17:51:32 -0800

    Clarify comment

--- src/libs/actions/Device/generateDeviceID/index.js
+++ src/libs/actions/Device/generateDeviceID/index.js
@@ -13,14 +13,14 @@ const uniqueID = Str.guid(deviceID);
  *
  * Therefore, this deviceID is not truly unique but will be a new GUID each time the app runs.
  *
- * This GUID should stored in Onyx under ONYXKEYS.DEVICE_ID and preserved on logout, such that the deviceID will only change if:
+ * This GUID is stored in Onyx under ONYXKEYS.DEVICE_ID and is preserved on logout, such that the deviceID will only change if:
  *
- * - The user uninstalls and reinstalls the app (Android/desktop)
- * - The user opens the app on a different browser or in an incognito window (web)
+ * - The user uninstalls and reinstalls the app (Android/desktop), OR
+ * - The user opens the app on a different browser or in an incognito window (web), OR
  * - The user manually clears Onyx data
  *
- * While this isn't perfect, it's the best we can do without violating Google Play's App Store guidelines.
- * It's also just as good as any common web solution, such as this one (which is also reset under the same circumstances):
+ * While this isn't perfect, it's the best we can do without violating the Google Play Store guidelines.
+ * It's also just as good as any obvious web solution, such as this one (which is also reset under the same circumstances):
  * https://developer.mozilla.org/en-US/docs/Web/API/MediaDeviceInfo/deviceId
  *
  * @returns {Promise<String>}
