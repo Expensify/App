27d325c20967ebb2867bcb042823e809b085fa18
Georgia Monahan <georgia@expensify.com>
2023-01-31 15:14:45 -0800

    Temporary Block

--- src/pages/signin/SignInPageLayout/Footer.js
+++ src/pages/signin/SignInPageLayout/Footer.js
@@ -1,31 +1,13 @@
-import {Pressable} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
-import _ from 'underscore';
-import styles from '../../../styles/styles';
-import * as StyleUtils from '../../../styles/StyleUtils';
-import * as Link from '../../../libs/actions/Link';
-import SVGImage from '../../../components/SVGImage';
-
-const backgroundStyle = StyleUtils.getLoginPagePromoStyle();
+import Text from '../../../components/Text';
 
 const Footer = () => (
-    <Pressable
-        style={[
-            styles.flex1,
-            StyleUtils.getBackgroundColorStyle(backgroundStyle.backgroundColor),
-        ]}
-        onPress={() => {
-            Link.openExternalLink(backgroundStyle.redirectUri);
-        }}
-        disabled={_.isEmpty(backgroundStyle.redirectUri)}
-    >
-        <SVGImage
-            width="100%"
-            height="100%"
-            src={backgroundStyle.backgroundImageUri}
-            resizeMode="contain"
-        />
-    </Pressable>
+    <View>
+        <Text>
+            HELLO
+        </Text>
+    </View>
 );
 
 Footer.displayName = 'Footer';
