0aabecc02af16a0fde0b6b935aae9eec54415419
Brandon Stites <bstites+1@expensify.com>
2023-01-31 16:28:15 -0700

    SDFOJI

--- src/components/EmojiPicker/CategoryShortcutBar.js
+++ src/components/EmojiPicker/CategoryShortcutBar.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {View} from 'react-native';
 import styles from '../../styles/styles';
+import Recent from '../../../assets/images/history.svg';
+import Smiley from '../../../assets/images/emoji.svg';
 
-const CategoryShortcutBar = () => <View style={[styles.pt2, styles.ph4, styles.flexRow]} />;
 
+const CategoryShortcutBar = () => {
+
+    return (
+        <View style={[styles.pt2, styles.ph4, styles.flexRow]}>
+            <Text>test</Text>
+        </View>
+    );
+};
 CategoryShortcutBar.displayName = 'CategoryShortcutBar';
 
 export default CategoryShortcutBar;
