d211b6b907d08b826640b6603bc127eec8d4379d
Carlos Martins <cmartins@expensify.com>
2023-01-31 09:55:30 -0700

    revert changes to proptypes

--- src/components/TextInput/baseTextInputPropTypes.js
+++ src/components/TextInput/baseTextInputPropTypes.js
@@ -73,9 +73,6 @@ const propTypes = {
 
     /** Indicate whether pressing Enter on multiline input is allowed to submit the form. */
     submitOnEnter: PropTypes.bool,
-
-    /** Whether the input should be disabled. */
-    disabled: PropTypes.bool,
 };
 
 const defaultProps = {
@@ -108,7 +105,6 @@ const defaultProps = {
     onInputChange: () => {},
     shouldDelayFocus: false,
     submitOnEnter: false,
-    disabled: false,
 };
 
 export {propTypes, defaultProps};
