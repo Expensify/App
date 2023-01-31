f17d1d26f3e0d7a6df9c3141fa5cb2c0b5468682
Brandon Stites <bstites+1@expensify.com>
2023-01-31 16:38:08 -0700

    SDOIFJ

--- src/components/EmojiPicker/CategoryShortcutBar.js
+++ src/components/EmojiPicker/CategoryShortcutBar.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {View, Text} from 'react-native';
-import _ from 'underscore/underscore-node.mjs';
 import styles from '../../styles/styles';
-import Recent from '../../../assets/images/history.svg';
-import Smiley from '../../../assets/images/emoji.svg';
-import CategoryShortcutButton from './CategoryShortcutButton';
 
-const propTypes = {
-    /** The function to call when an emoji is selected */
-    onPress: PropTypes.func.isRequired,
-
-    /** The indices that the icons should link to */
-    headerIndices: PropTypes.arrayOf(PropTypes.number).isRequired,
-};
-
-const CategoryShortcutBar = (props) => {
-    const icons = [Recent, Smiley];
+const CategoryShortcutBar = () => {
 
     return (
         <View style={[styles.pt2, styles.ph4, styles.flexRow]}>
@@ -24,7 +10,6 @@ const CategoryShortcutBar = (props) => {
         </View>
     );
 };
-CategoryShortcutBar.propTypes = propTypes;
 CategoryShortcutBar.displayName = 'CategoryShortcutBar';
 
 export default CategoryShortcutBar;
