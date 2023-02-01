1c0b7e9492a9f4c062c5fb10eeef853019ed4756
Cristi Paval <cristi@expensify.com>
2023-02-02 00:07:09 +0200

    Fix more lint errors.

--- src/pages/ValidateLoginPage/index.website.js
+++ src/pages/ValidateLoginPage/index.website.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {withOnyx} from 'react-native-onyx';
+import {compose} from 'underscore';
 import lodashGet from 'lodash/get';
 import {
     propTypes as validateLinkPropTypes,
@@ -6,13 +8,12 @@ import {
 } from './validateLinkPropTypes';
 import * as User from '../../libs/actions/User';
 import FullScreenLoadingIndicator from '../../components/FullscreenLoadingIndicator';
-import { compose } from 'underscore';
-import { withBetas } from '../../components/OnyxProvider';
+import {withBetas} from '../../components/OnyxProvider';
 import CONST from '../../CONST';
 import MagicCodeModal from '../../components/MagicCodeModal';
-import { withOnyx } from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 import * as Session from '../../libs/actions/Session';
+import Permissions from '../../libs/Permissions';
 
 const propTypes = {
     /** The accountID and validateCode are passed via the URL */
@@ -22,21 +23,18 @@ const propTypes = {
 const defaultProps = {
     route: validateLinkDefaultProps,
 };
+
 class ValidateLoginPage extends Component {
-    
     componentDidMount() {
-        if (this.isPasswordlessFlow()) {
+        if (Permissions.canUsePasswordlessLogins(this.props.betas)) {
             if (!this.isAuthenticated()) {
                 Session.signInFromMagicLink(this.accountID(), this.validateCode());
-            } 
+            }
         } else {
             User.validateLogin(this.accountID(), this.validateCode());
         }
     }
 
-    /**
-     * @returns {boolean}
-     */
     isAuthenticated() {
         const authToken = lodashGet(this.props, 'session.authToken', null);
         return Boolean(authToken);
@@ -46,25 +44,15 @@ class ValidateLoginPage extends Component {
 
     validateCode = () => lodashGet(this.props.route.params, 'validateCode', '');
 
-    isPasswordlessFlow = () => this.props.betas &&
-        (
-            this.props.betas.includes(CONST.BETAS.PASSWORDLESS)
-            || this.props.betas.includes(CONST.BETAS.ALL)
-        );
-
     render() {
-        return (this.isPasswordlessFlow() ? <MagicCodeModal code={this.validateCode()}/> : <FullScreenLoadingIndicator />);
+        return (Permissions.canUsePasswordlessLogins(this.props.betas) ? <MagicCodeModal code={this.validateCode()} /> : <FullScreenLoadingIndicator />);
     }
 }
 
 ValidateLoginPage.propTypes = propTypes;
 ValidateLoginPage.defaultProps = defaultProps;
 
-export default compose(
-    withBetas(),
-    withOnyx({
-        session: {
-            key: ONYXKEYS.SESSION,
-        },
-    }),
-)(ValidateLoginPage);
+export default withOnyx({
+    betas: { key: ONYXKEYS.BETAS },
+    session: { key: ONYXKEYS.SESSION },
+})(ValidateLoginPage);
