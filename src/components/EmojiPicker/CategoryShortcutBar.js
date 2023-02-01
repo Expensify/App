4664c891d7f43b147e33732279771c9ce49aaf9b
Brandon Stites <bstites+1@expensify.com>
2023-01-31 17:14:56 -0700

    SDOFIJSODIJFSOIDJF

--- src/components/EmojiPicker/CategoryShortcutBar.js
+++ src/components/EmojiPicker/CategoryShortcutBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {View, Text} from 'react-native';
-import _ from 'underscore/underscore-node.mjs';
 import styles from '../../styles/styles';
 import Recent from '../../../assets/images/history.svg';
 import Smiley from '../../../assets/images/emoji.svg';
