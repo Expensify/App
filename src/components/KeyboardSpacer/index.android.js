a3cc3288eba974af2ea15387ce4dfa4cc2d05a3d
Oliver Wilks <ollywilks@googlemail.com>
2023-02-01 16:57:39 +0000

    Deleted KeyboardSpacer index.android

--- src/components/KeyboardSpacer/index.android.js
+++
@@ -1,9 +0,0 @@
-/**
- * On Android we do not need to implement a keyboard spacer, so we return a null component.
- *
- * @returns {null}
- * @constructor
- */
-const KeyboardSpacer = () => null;
-
-export default KeyboardSpacer;
