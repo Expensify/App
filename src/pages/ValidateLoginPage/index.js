ce8ac369a1777150f94c07067ab3da22f1341918
Cristi Paval <cristi@expensify.com>
2023-02-02 23:46:31 +0200

    Add automatic login for NewDot desktop.

--- src/pages/ValidateLoginPage/index.js
+++ src/pages/ValidateLoginPage/index.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {withOnyx} from 'react-native-onyx';
 import lodashGet from 'lodash/get';
 import {
     propTypes as validateLinkPropTypes,
@@ -6,22 +8,40 @@ import {
 } from './validateLinkPropTypes';
 import * as User from '../../libs/actions/User';
 import FullScreenLoadingIndicator from '../../components/FullscreenLoadingIndicator';
+import ONYXKEYS from '../../ONYXKEYS';
+import * as Session from '../../libs/actions/Session';
+import Permissions from '../../libs/Permissions';
+import Navigation from '../../libs/Navigation/Navigation';
+import ROUTES from '../../ROUTES';
 
 const propTypes = {
     /** The accountID and validateCode are passed via the URL */
     route: validateLinkPropTypes,
+
+    /** List of betas available to current user */
+    betas: PropTypes.arrayOf(PropTypes.string),
 };
 
 const defaultProps = {
     route: validateLinkDefaultProps,
+    betas: [],
 };
+
 class ValidateLoginPage extends Component {
     componentDidMount() {
-        const accountID = lodashGet(this.props.route.params, 'accountID', '');
-        const validateCode = lodashGet(this.props.route.params, 'validateCode', '');
-
-        User.validateLogin(accountID, validateCode);
+        console.log('isAuthenticated: ' + this.isAuthenticated() + '   passwordless: ' + Permissions.canUsePasswordlessLogins(this.props.betas));
+        if (Permissions.canUsePasswordlessLogins(this.props.betas) && !this.isAuthenticated()) {
+            Session.signInFromMagicLink(this.accountID(), this.validateCode());
+        } else {
+            User.validateLogin(this.accountID(), this.validateCode());
+        }
     }
+
+    accountID = () => lodashGet(this.props.route.params, 'accountID', '');
+
+    validateCode = () => lodashGet(this.props.route.params, 'validateCode', '');
+
+    isAuthenticated = () => Boolean(lodashGet(this.props, 'session.authToken', null));
 
     render() {
         return <FullScreenLoadingIndicator />;
@@ -31,4 +51,7 @@ class ValidateLoginPage extends Component {
 ValidateLoginPage.propTypes = propTypes;
 ValidateLoginPage.defaultProps = defaultProps;
 
-export default ValidateLoginPage;
+export default withOnyx({
+    betas: {key: ONYXKEYS.BETAS},
+    session: {key: ONYXKEYS.SESSION},
+})(ValidateLoginPage);
