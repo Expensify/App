24c461be3b5d7c15b7df6e2bf2a7a7c2a566857b
azimgd <me@azimgd.com>
2023-02-06 23:17:49 +0500

    update keycommand import order

--- src/CONST.js
+++ src/CONST.js
@@ -1,7 +1,7 @@
 import lodashGet from 'lodash/get';
 import Config from 'react-native-config';
-import * as Url from './libs/Url';
 import * as KeyCommand from 'react-native-key-command';
+import * as Url from './libs/Url';
 
 const CLOUDFRONT_URL = 'https://d2k5nsl2zxldvw.cloudfront.net';
 const ACTIVE_EXPENSIFY_URL = Url.addTrailingForwardSlash(lodashGet(Config, 'NEW_EXPENSIFY_URL', 'https://new.expensify.com'));
