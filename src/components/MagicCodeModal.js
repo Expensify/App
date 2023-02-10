35970782da336aba7febd46a24c3080d620b09e7
Cristi Paval <cristi@expensify.com>
2023-02-10 18:43:20 +0200

    Update src/components/MagicCodeModal.js
    Co-authored-by: John Lee <johnmlee101@gmail.com>

--- src/components/MagicCodeModal.js
+++ src/components/MagicCodeModal.js
@@ -36,7 +36,7 @@ const defaultProps = {
 
 class MagicCodeModal extends PureComponent {
     render() {
-        const titleKey = this.props.isSuccessfullySignedIn ? 'magicCodeModal.succesfulSignInTitle' : 'magicCodeModal.title';
+        const titleKey = this.props.isSuccessfullySignedIn ? 'magicCodeModal.successfulSignInTitle' : 'magicCodeModal.title';
         const descriptionKey = this.props.isSuccessfullySignedIn ? 'magicCodeModal.succesfulSignInDescription' : 'magicCodeModal.description';
         const illustration = this.props.isSuccessfullySignedIn ? Illustrations.Abracadabra : Illustrations.MagicCode;
         return (
