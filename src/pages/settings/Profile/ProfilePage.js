8fe819d942163cb552d9f4a5464994ce6f74bc92
Alex Beaman <alexbeaman@expensify.com>
2023-02-10 17:53:17 +0200

    Link to new contact methods page & simplify

--- src/pages/settings/Profile/ProfilePage.js
+++ src/pages/settings/Profile/ProfilePage.js
@@ -1,10 +1,8 @@
 import Str from 'expensify-common/lib/str';
 import lodashGet from 'lodash/get';
-import PropTypes from 'prop-types';
-import React, {Component} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
-import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
 import AvatarWithImagePicker from '../../../components/AvatarWithImagePicker';
 import HeaderWithCloseButton from '../../../components/HeaderWithCloseButton';
@@ -19,189 +17,102 @@ import * as PersonalDetails from '../../../libs/actions/PersonalDetails';
 import compose from '../../../libs/compose';
 import Navigation from '../../../libs/Navigation/Navigation';
 import * as ReportUtils from '../../../libs/ReportUtils';
-import ONYXKEYS from '../../../ONYXKEYS';
 import ROUTES from '../../../ROUTES';
 import styles from '../../../styles/styles';
-import LoginField from './LoginField';
 import * as Expensicons from '../../../components/Icon/Expensicons';
 
 const propTypes = {
-    /* Onyx Props */
-
-    /** Login list for the user that is signed in */
-    loginList: PropTypes.shape({
-        /** Value of partner name */
-        partnerName: PropTypes.string,
-
-        /** Phone/Email associated with user */
-        partnerUserID: PropTypes.string,
-
-        /** Date of when login was validated */
-        validatedDate: PropTypes.string,
-    }),
-
     ...withLocalizePropTypes,
     ...withCurrentUserPersonalDetailsPropTypes,
 };
 
 const defaultProps = {
-    loginList: {},
     ...withCurrentUserPersonalDetailsDefaultProps,
 };
 
-class ProfilePage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.defaultAvatar = ReportUtils.getDefaultAvatar(this.props.currentUserPersonalDetails.login);
-        this.avatar = {uri: lodashGet(this.props.currentUserPersonalDetails, 'avatar') || this.defaultAvatar};
-        this.state = {
-            logins: this.getLogins(),
-        };
-
-        this.getLogins = this.getLogins.bind(this);
-    }
-
-    componentDidUpdate(prevProps) {
-        let stateToUpdate = {};
-
-        // Recalculate logins if loginList has changed
-        if (_.keys(this.props.loginList).length !== _.keys(prevProps.loginList).length) {
-            stateToUpdate = {...stateToUpdate, logins: this.getLogins()};
-        }
-
-        if (_.isEmpty(stateToUpdate)) {
-            return;
-        }
-
-        // eslint-disable-next-line react/no-did-update-set-state
-        this.setState(stateToUpdate);
-    }
-
-    getPronouns() {
-        let pronounsKey = lodashGet(this.props.currentUserPersonalDetails, 'pronouns', '');
+const ProfilePage = (props) => {
+    const getPronouns = () => {
+        let pronounsKey = lodashGet(props.currentUserPersonalDetails, 'pronouns', '');
         if (pronounsKey.startsWith(CONST.PRONOUNS.PREFIX)) {
             pronounsKey = pronounsKey.slice(CONST.PRONOUNS.PREFIX.length);
         }
-        return lodashGet(this.props.translate('pronouns'), pronounsKey, this.props.translate('profilePage.selectYourPronouns'));
-    }
+        return lodashGet(props.translate('pronouns'), pronounsKey, props.translate('profilePage.selectYourPronouns'));
+    };
+    const currentUserDetails = props.currentUserPersonalDetails || {};
+    const profileSettingsOptions = [
+        {
+            description: props.translate('displayNamePage.headerTitle'),
+            title: lodashGet(currentUserDetails, 'displayName', ''),
+            pageRoute: ROUTES.SETTINGS_DISPLAY_NAME,
+        },
+        {
+            description: props.translate('contacts.contactMethod'),
+            title: Str.removeSMSDomain(lodashGet(currentUserDetails, 'login', '')),
+            pageRoute: ROUTES.SETTINGS_CONTACT_METHODS,
+        },
+        {
+            description: props.translate('pronounsPage.pronouns'),
+            title: getPronouns(),
+            pageRoute: ROUTES.SETTINGS_PRONOUNS,
+        },
+        {
+            description: props.translate('timezonePage.timezone'),
+            title: `${lodashGet(currentUserDetails, 'timezone.selected', '')}`,
+            pageRoute: ROUTES.SETTINGS_TIMEZONE,
+        },
+    ];
 
-    /**
-     * Get the most validated login of each type
-     *
-     * @returns {Object}
-     */
-    getLogins() {
-        return _.reduce(_.values(this.props.loginList), (logins, currentLogin) => {
-            const type = Str.isSMSLogin(currentLogin.partnerUserID) ? CONST.LOGIN_TYPE.PHONE : CONST.LOGIN_TYPE.EMAIL;
-            const login = Str.removeSMSDomain(currentLogin.partnerUserID);
-
-            // If there's already a login type that's validated and/or currentLogin isn't valid then return early
-            if ((login !== lodashGet(this.props.currentUserPersonalDetails, 'login')) && !_.isEmpty(logins[type])
-                && (logins[type].validatedDate || !currentLogin.validatedDate)) {
-                return logins;
-            }
-            return {
-                ...logins,
-                [type]: {
-                    ...currentLogin,
-                    type,
-                    partnerUserID: Str.removeSMSDomain(currentLogin.partnerUserID),
-                },
-            };
-        }, {
-            phone: {},
-            email: {},
-        });
-    }
-
-    render() {
-        const currentUserDetails = this.props.currentUserPersonalDetails || {};
-        const profileSettingsOptions = [
-            {
-                description: this.props.translate('displayNamePage.headerTitle'),
-                title: lodashGet(currentUserDetails, 'displayName', ''),
-                pageRoute: ROUTES.SETTINGS_DISPLAY_NAME,
-            },
-            {
-                description: this.props.translate('pronounsPage.pronouns'),
-                title: this.getPronouns(),
-                pageRoute: ROUTES.SETTINGS_PRONOUNS,
-            },
-            {
-                description: this.props.translate('timezonePage.timezone'),
-                title: `${lodashGet(currentUserDetails, 'timezone.selected', '')}`,
-                pageRoute: ROUTES.SETTINGS_TIMEZONE,
-            },
-        ];
-        return (
-            <ScreenWrapper includeSafeAreaPaddingBottom={false}>
-                <HeaderWithCloseButton
-                    title={this.props.translate('common.profile')}
-                    shouldShowBackButton
-                    onBackButtonPress={() => Navigation.navigate(ROUTES.SETTINGS)}
-                    onCloseButtonPress={() => Navigation.dismissModal(true)}
-                />
-                <ScrollView>
-                    <OfflineWithFeedback
-                        pendingAction={lodashGet(this.props.currentUserPersonalDetails, 'pendingFields.avatar', null)}
-                        errors={lodashGet(this.props.currentUserPersonalDetails, 'errorFields.avatar', null)}
-                        errorRowStyles={[styles.mt6]}
-                        onClose={PersonalDetails.clearAvatarErrors}
-                    >
-                        <AvatarWithImagePicker
-                            isUsingDefaultAvatar={ReportUtils.isDefaultAvatar(lodashGet(currentUserDetails, 'avatar', ''))}
-                            source={ReportUtils.getAvatar(lodashGet(currentUserDetails, 'avatar', ''), lodashGet(currentUserDetails, 'login', ''))}
-                            onImageSelected={PersonalDetails.updateAvatar}
-                            onImageRemoved={PersonalDetails.deleteAvatar}
-                            anchorPosition={styles.createMenuPositionProfile}
-                            size={CONST.AVATAR_SIZE.LARGE}
+    return (
+        <ScreenWrapper includeSafeAreaPaddingBottom={false}>
+            <HeaderWithCloseButton
+                title={props.translate('common.profile')}
+                shouldShowBackButton
+                onBackButtonPress={() => Navigation.navigate(ROUTES.SETTINGS)}
+                onCloseButtonPress={() => Navigation.dismissModal(true)}
+            />
+            <ScrollView>
+                <OfflineWithFeedback
+                    pendingAction={lodashGet(props.currentUserPersonalDetails, 'pendingFields.avatar', null)}
+                    errors={lodashGet(props.currentUserPersonalDetails, 'errorFields.avatar', null)}
+                    errorRowStyles={[styles.mt6]}
+                    onClose={PersonalDetails.clearAvatarErrors}
+                >
+                    <AvatarWithImagePicker
+                        isUsingDefaultAvatar={ReportUtils.isDefaultAvatar(lodashGet(currentUserDetails, 'avatar', ''))}
+                        source={ReportUtils.getAvatar(lodashGet(currentUserDetails, 'avatar', ''), lodashGet(currentUserDetails, 'login', ''))}
+                        onImageSelected={PersonalDetails.updateAvatar}
+                        onImageRemoved={PersonalDetails.deleteAvatar}
+                        anchorPosition={styles.createMenuPositionProfile}
+                        size={CONST.AVATAR_SIZE.LARGE}
+                    />
+                </OfflineWithFeedback>
+                <View style={[styles.mt4]}>
+                    {_.map(profileSettingsOptions, (detail, index) => (
+                        <MenuItemWithTopDescription
+                            key={`${detail.title}_${index}`}
+                            shouldShowRightIcon
+                            title={detail.title}
+                            description={detail.description}
+                            onPress={() => Navigation.navigate(detail.pageRoute)}
                         />
-                    </OfflineWithFeedback>
-                    <View style={[styles.mt4]}>
-                        {_.map(profileSettingsOptions, (detail, index) => (
-                            <MenuItemWithTopDescription
-                                key={`${detail.title}_${index}`}
-                                shouldShowRightIcon
-                                title={detail.title}
-                                description={detail.description}
-                                onPress={() => Navigation.navigate(detail.pageRoute)}
-                            />
-                        ))}
-                    </View>
-                    <LoginField
-                        label={this.props.translate('profilePage.emailAddress')}
-                        type="email"
-                        login={this.state.logins.email}
-                        defaultValue={this.state.logins.email}
-                    />
-                    <LoginField
-                        label={this.props.translate('common.phoneNumber')}
-                        type="phone"
-                        login={this.state.logins.phone}
-                        defaultValue={this.state.logins.phone}
-                    />
-                    <MenuItem
-                        title={this.props.translate('privatePersonalDetails.personalDetails')}
-                        icon={Expensicons.User}
-                        onPress={() => Navigation.navigate(ROUTES.SETTINGS_PERSONAL_DETAILS)}
-                        shouldShowRightIcon
-                    />
-                </ScrollView>
-            </ScreenWrapper>
-        );
-    }
-}
+                    ))}
+                </View>
+                <MenuItem
+                    title={props.translate('privatePersonalDetails.personalDetails')}
+                    icon={Expensicons.User}
+                    onPress={() => Navigation.navigate(ROUTES.SETTINGS_PERSONAL_DETAILS)}
+                    shouldShowRightIcon
+                />
+            </ScrollView>
+        </ScreenWrapper>
+    );
+};
 
 ProfilePage.propTypes = propTypes;
 ProfilePage.defaultProps = defaultProps;
+ProfilePage.displayName = 'ProfilePage';
 
 export default compose(
     withLocalize,
     withCurrentUserPersonalDetails,
-    withOnyx({
-        loginList: {
-            key: ONYXKEYS.LOGIN_LIST,
-        },
-    }),
 )(ProfilePage);
