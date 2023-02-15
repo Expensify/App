36acf5f3b86cd545b75e96e7e7ded8aba166ffdb
ArekChr <arkadiuszchr@gmail.com>
2023-02-15 15:01:56 +0100

    docs: add code comment to generateMonthMatrix

--- src/components/CalendarPicker/generateMonthMatrix.js
+++ src/components/CalendarPicker/generateMonthMatrix.js
@@ -1,5 +1,11 @@
 import moment from 'moment';
 
+/**
+ * Generates a matrix representation of a month's calendar given the year and month.
+ * @param {number} year - The year for which to generate the month matrix.
+ * @param {number} month - The month (0-indexed) for which to generate the month matrix.
+ * @returns {Array.<Array.<number|null>>} - A 2D array of the month's calendar days, with null values representing days outside the current month.
+ */
 export default function generateMonthMatrix(year, month) {
     const daysInMonth = moment([year, month]).daysInMonth();
     const firstDay = moment([year, month, 1]).startOf('month').locale('en');
