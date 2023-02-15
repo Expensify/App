4c60df789e26d40a4f1a3ad63feeef98eef71cef
Peter Velkov <kidroca@gmail.com>
2023-02-15 19:20:11 +0200

    Update `tryResolveUrlFromApiRoot` to use `ApiUtils.getApiRoot`

--- src/libs/tryResolveUrlFromApiRoot.js
+++ src/libs/tryResolveUrlFromApiRoot.js
@@ -1,9 +1,15 @@
 import Config from '../CONFIG';
+import * as ApiUtils from './ApiUtils';
 
 // Absolute URLs (`/` or `//`) should be resolved from API ROOT
 // Legacy attachments can come from either staging or prod, depending on the env they were uploaded by
 // Both should be replaced and loaded from API ROOT of the current environment
-const ORIGINS_TO_REPLACE = ['/+', Config.EXPENSIFY.EXPENSIFY_URL, Config.EXPENSIFY.STAGING_API_ROOT];
+const ORIGINS_TO_REPLACE = [
+    '/+',
+    Config.EXPENSIFY.EXPENSIFY_URL,
+    Config.EXPENSIFY.STAGING_API_ROOT,
+    Config.EXPENSIFY.PRODUCTION_API_ROOT,
+];
 
 // Anything starting with a match from ORIGINS_TO_REPLACE
 const ORIGIN_PATTERN = new RegExp(`^(${ORIGINS_TO_REPLACE.join('|')})`);
@@ -20,5 +26,6 @@ const ORIGIN_PATTERN = new RegExp(`^(${ORIGINS_TO_REPLACE.join('|')})`);
  * @returns {String}
  */
 export default function tryResolveUrlFromApiRoot(url) {
-    return url.replace(ORIGIN_PATTERN, Config.EXPENSIFY.PRODUCTION_API_ROOT);
+    const apiRoot = ApiUtils.getApiRoot({shouldUseSecure: false});
+    return url.replace(ORIGIN_PATTERN, apiRoot);
 }
