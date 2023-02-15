1881e6c73ea81d52d768408dd8ce86c271063e22
ArekChr <arkadiuszchr@gmail.com>
2023-02-15 15:39:30 +0100

    tests: generateMonthMatrix & error handling

--- src/components/CalendarPicker/generateMonthMatrix.js
+++ src/components/CalendarPicker/generateMonthMatrix.js
@@ -7,6 +7,22 @@ import moment from 'moment';
  * @returns {Array.<Array.<number|null>>} - A 2D array of the month's calendar days, with null values representing days outside the current month.
  */
 export default function generateMonthMatrix(year, month) {
+    if (typeof year !== 'number') {
+        throw new TypeError('Year must be a number');
+    }
+    if (year < 0) {
+        throw new TypeError('Year must be a positive integer');
+    }
+    if (typeof month !== 'number') {
+        throw new TypeError('Month must be a number');
+    }
+    if (month < 0) {
+        throw new TypeError('Month cannot be less than 0');
+    }
+    if (month > 11) {
+        throw new TypeError('Month cannot be greater than 11');
+    }
+
     const daysInMonth = moment([year, month]).daysInMonth();
     const firstDay = moment([year, month, 1]).startOf('month').locale('en');
     const matrix = [];
