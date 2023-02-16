db114f1ba049f3d038833f288462b2e138731643
christianwenifr <christianwen18@gmail.com>
2023-02-16 16:18:09 +0700

    fix: delete redundant

--- src/libs/toggleReportActionComposeViewWhenCloseEditMessage/index.js
+++
@@ -1,9 +0,0 @@
-import * as Composer from '../actions/Composer';
-
-export default (isSmallScreenWidth = true) => {
-    if (!isSmallScreenWidth) {
-        return;
-    }
-
-    Composer.setShouldShowComposeInput(true);
-};
