eddcc2d8ecee1bf784e75bd661c9b91c728943dd
christianwenifr <christianwen18@gmail.com>
2023-02-16 16:26:22 +0700

    fix: mweb bug

--- src/pages/home/report/ReportActionItemMessageEdit.js
+++ src/pages/home/report/ReportActionItemMessageEdit.js
@@ -247,11 +247,17 @@ class ReportActionItemMessageEdit extends React.Component {
                             toggleReportActionComposeView(false, this.props.isSmallScreenWidth);
                         }}
                         onBlur={(event) => {
+                            const relatedTargetId = lodashGet(event, 'nativeEvent.relatedTarget.id');
+
                             // Return to prevent re-render when save/cancel button is pressed which cancels the onPress event by re-rendering
-                            if (_.contains([this.saveButtonID, this.cancelButtonID, this.emojiButtonID, this.messageEditInput], lodashGet(event, 'nativeEvent.relatedTarget.id'))) {
+                            if (_.contains([this.saveButtonID, this.cancelButtonID, this.emojiButtonID], relatedTargetId)) {
                                 return;
                             }
                             this.setState({isFocused: false});
+
+                            if (this.messageEditInput === relatedTargetId) {
+                                return;
+                            }
                             openReportActionComposeViewWhenClosingMessageEdit(this.props.isSmallScreenWidth);
                         }}
                         selection={this.state.selection}
