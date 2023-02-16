ff2c8b2f74b2bb099c1b1b051908518d5eacb69d
tienifr <christianwen18@gmail.com>
2023-02-16 15:18:56 +0700

    fix: toggle composer input only when the keyboard hide completely

--- src/libs/toggleReportActionComposeView/index.native.js
+++ src/libs/toggleReportActionComposeView/index.native.js
@@ -2,10 +2,8 @@ import {Keyboard} from 'react-native';
 import * as Composer from '../actions/Composer';
 
 export default (shouldShowComposeInput) => {
-    this.keyboardDidHideListener = Keyboard.addListener(
-        'keyboardDidHide',
-        () => {
-            Composer.setShouldShowComposeInput(shouldShowComposeInput);
-        },
-    );
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+        Composer.setShouldShowComposeInput(shouldShowComposeInput);
+        keyboardDidHideListener.remove();
+    });
 };
