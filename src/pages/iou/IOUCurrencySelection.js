b9ad34885e766c2ec8c32221745114600f4146c9
Nicolay Arefyeu <n.arefyev91@gmail.com>
2023-02-16 12:45:19 +0200

    Keyboard is visible for a brief moment on request/send money page after getting back from currency search page

--- src/pages/iou/IOUCurrencySelection.js
+++ src/pages/iou/IOUCurrencySelection.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
 import Str from 'expensify-common/lib/str';
+import {Keyboard} from 'react-native';
 import ONYXKEYS from '../../ONYXKEYS';
 import OptionsSelector from '../../components/OptionsSelector';
 import Navigation from '../../libs/Navigation/Navigation';
@@ -13,6 +14,7 @@ import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize
 import * as IOU from '../../libs/actions/IOU';
 import * as CurrencySymbolUtils from '../../libs/CurrencySymbolUtils';
 import {withNetwork} from '../../components/OnyxProvider';
+import withKeyboardState from '../../components/withKeyboardState';
 
 /**
  * IOU Currency selection for selecting currency
@@ -49,6 +51,17 @@ class IOUCurrencySelection extends Component {
         this.getSections = this.getSections.bind(this);
         this.confirmCurrencySelection = this.confirmCurrencySelection.bind(this);
         this.changeSearchValue = this.changeSearchValue.bind(this);
+    }
+
+    componentDidMount() {
+        this.beforeRemoveSubscription = this.props.navigation.addListener('beforeRemove', () => {
+            if (!this.props.isKeyboardShown) { return; }
+            Keyboard.dismiss();
+        });
+    }
+
+    componentWillUnmount() {
+        this.beforeRemoveSubscription();
     }
 
     /**
@@ -140,6 +153,7 @@ IOUCurrencySelection.defaultProps = defaultProps;
 
 export default compose(
     withLocalize,
+    withKeyboardState,
     withOnyx({
         currencyList: {key: ONYXKEYS.CURRENCY_LIST},
     }),
