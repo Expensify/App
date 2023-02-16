7889bae7046def767630b527d312269d95fc94d7
Alex Beaman <alexbeaman@expensify.com>
2023-02-16 13:11:25 +0200

    Add new possible value for brickRoadIndicator

--- src/components/menuItemPropTypes.js
+++ src/components/menuItemPropTypes.js
@@ -80,7 +80,7 @@ const propTypes = {
     floatRightAvatars: PropTypes.arrayOf(PropTypes.string),
 
     /** The type of brick road indicator to show. */
-    brickRoadIndicator: PropTypes.oneOf([CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR, CONST.BRICK_ROAD_INDICATOR_STATUS.INFO, '']),
+    brickRoadIndicator: PropTypes.oneOf([CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR, CONST.BRICK_ROAD_INDICATOR_STATUS.INFO, CONST.BRICK_ROAD_INDICATOR_STATUS.GREEN, '']),
 
     /** Prop to identify if we should load avatars vertically instead of diagonally */
     shouldStackHorizontally: PropTypes.bool,
