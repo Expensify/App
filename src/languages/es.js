18490630f70931e4633feba3f520e048fcf5ce0a
tienifr <christianwen18@gmail.com>
2023-02-20 11:20:12 +0700

    replace acceptedTerms with acceptTerms for consistency

--- src/languages/es.js
+++ src/languages/es.js
@@ -85,7 +85,7 @@ export default {
         genericErrorMessage: 'Ups... algo no ha ido bien y la acción no se pudo completar. Por favor inténtalo más tarde.',
         error: {
             invalidAmount: 'Monto no válido',
-            acceptedTerms: 'Debes aceptar los Términos de servicio para continuar',
+            acceptTerms: 'Debes aceptar los Términos de servicio para continuar',
             phoneNumber: `Ingresa un teléfono válido, incluyendo el código de país (p. ej. ${CONST.EXAMPLE_PHONE_NUMBER})`,
             fieldRequired: 'Este campo es obligatorio.',
             characterLimit: ({limit}) => `Supera el límite de ${limit} caracteres`,
