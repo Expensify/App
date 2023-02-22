88f999f6477df47af922e9917b0605d361a83d2d
Nikki Wines <nikkiwines@expensify.com>
2023-02-22 12:01:32 -0800

    bypass password popover if passwordless

--- src/pages/settings/Payments/PaymentsPage/BasePaymentsPage.js
+++ src/pages/settings/Payments/PaymentsPage/BasePaymentsPage.js
@@ -464,10 +464,14 @@ class BasePaymentsPage extends React.Component {
                                     // InteractionManager fires after the currently running animation is completed.
                                     // https://github.com/Expensify/App/issues/7768#issuecomment-1044879541
                                     InteractionManager.runAfterInteractions(() => {
-                                        this.setState({
-                                            shouldShowPasswordPrompt: !Permissions.canUsePasswordlessLogins(this.props.betas),
-                                            passwordButtonText: this.props.translate('paymentsPage.setDefaultConfirmation'),
-                                        });
+                                        if (Permissions.canUsePasswordlessLogins(this.props.betas)) {
+                                            this.makeDefaultPaymentMethod();
+                                        } else {
+                                            this.setState({
+                                                shouldShowPasswordPrompt: true,
+                                                passwordButtonText: this.props.translate('paymentsPage.setDefaultConfirmation'),
+                                            });
+                                        }
                                     });
                                 }}
                                 text={this.props.translate('paymentsPage.setDefaultConfirmation')}
