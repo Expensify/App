d21f6e2439feb7dcfbfef807d033546ba98b11b2
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-02-23 02:58:08 -0800

    add comment

--- src/pages/settings/Preferences/PriorityModePage.js
+++ src/pages/settings/Preferences/PriorityModePage.js
@@ -27,6 +27,8 @@ const PriorityModePage = (props) => {
                 value: key,
                 text: mode.label,
                 alternateText: mode.description,
+
+                // Set max line to 0 to reset line restriction
                 alternateTextMaxLines: 0,
                 keyForList: key,
 
