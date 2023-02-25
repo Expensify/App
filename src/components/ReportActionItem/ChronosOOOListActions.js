f830a128b1a7027fe9e904b622a77f514c4498f8
Tim Golen <tgolen@expensify.com>
2023-02-25 16:49:40 -0700

    Fix the date display

--- src/components/ReportActionItem/ChronosOOOListActions.js
+++ src/components/ReportActionItem/ChronosOOOListActions.js
@@ -3,13 +3,13 @@ import {View} from 'react-native';
 import _ from 'underscore';
 import PropTypes from 'prop-types';
 import lodashGet from 'lodash/get';
-import moment from 'moment/moment';
 import reportActionPropTypes from '../../pages/home/report/reportActionPropTypes';
 import styles from '../../styles/styles';
 import Text from '../Text';
 import Button from '../Button';
 import * as Chronos from '../../libs/actions/Chronos';
 import withLocalize, {withLocalizePropTypes} from '../withLocalize';
+import DateUtils from '../../libs/DateUtils';
 
 const propTypes = {
     /** The ID of the report */
@@ -35,15 +35,13 @@ const ChronosOOOListActions = (props) => {
     return (
         <View>
             {_.map(events, (event) => {
-                const start = new moment(event.start.date);
-                const end = new moment(event.end.date);
+                const start = DateUtils.getLocalMomentFromDatetime(props.preferredLocale, event.start.date);
+                const end = DateUtils.getLocalMomentFromDatetime(props.preferredLocale, event.end.date);
                 return (
-                    <View style={[styles.flexRow, styles.alignItemsCenter, styles.pt, styles.ml18]}>
+                    <View key={event.id} style={[styles.flexRow, styles.alignItemsCenter, styles.pt, styles.ml18]}>
                         {event.lengthInDays > 0 ? (
                             <Text>
                                 {event.summary}
-                                {' '}
-                                {start.format('dddd MMM Do, YYYY')}
                                 {' for '}
                                 {event.lengthInDays}
                                 {event.lengthInDays === 1 ? ' day' : 'days'}
