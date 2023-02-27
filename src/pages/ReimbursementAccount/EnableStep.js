3e15034b5e768393e56e7b6bf884bd2e3e61b135
Aldo Canepa <aldo@expensify.com>
2023-02-27 15:33:19 -0800

    Remove unused import

--- src/pages/ReimbursementAccount/EnableStep.js
+++ src/pages/ReimbursementAccount/EnableStep.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {ScrollView} from 'react-native';
 import {withOnyx} from 'react-native-onyx';
 import lodashGet from 'lodash/get';
