f59daa3d9e6412e1f6e6630909872ae58ea3b54d
Patrick <patrick.cze@gmail.com>
2023-02-28 20:45:49 -0700

    Adding a clear comment of why we are making this change to the emojiHeaderContainerPlatformStyles

--- src/styles/emojiHeaderContainerPlatformStyles/index.js
+++ src/styles/emojiHeaderContainerPlatformStyles/index.js
@@ -1,1 +1,4 @@
+// This change is only temporary until browser have fixed the issue with position: sticky causing the header to bounce on scroll in non-native apps
+// https://github.com/Expensify/App/issues/15282
+// https://bugs.chromium.org/p/chromium/issues/detail?id=734461
 export default {top: -1};
