b4668918c533f026bb36ca13928f1b38aec390fd
Alex Beaman <alexbeaman@expensify.com>
2023-03-02 20:15:00 +0200

    Add more spacing between verify form and delete btn

--- src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.js
+++ src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.js
@@ -165,7 +165,7 @@ class ContactMethodDetailsPage extends Component {
                         danger
                     />
                     {!loginData.validatedDate && (
-                        <View style={[styles.mh5, styles.mb2]}>
+                        <View style={[styles.mh5, styles.mb7]}>
                             <View style={[styles.flexRow, styles.alignItemsCenter, styles.mb1, styles.mt3]}>
                                 <Icon src={Expensicons.DotIndicator} fill={colors.green} />
                                 <View style={[styles.flex1, styles.ml4]}>
