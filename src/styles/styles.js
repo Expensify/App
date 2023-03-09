217f06d21b345867a88cbe002bda7579a598ed88
Olly <ollywilks@googlemail.com>
2023-03-09 10:32:35 +0000

    Disabled WebkitUserSelect for navigationModalOverlay
    Co-authored-by: Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1589,6 +1589,7 @@ const styles = {
 
     navigationModalOverlay: {
         userSelect: 'none',
+        WebkitUserSelect: 'none',
         position: 'absolute',
         width: '100%',
         height: '100%',
