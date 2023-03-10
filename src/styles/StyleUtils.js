99189050bc0300c2f829a8d068afafeff96993ba
Taras Perun <terry.perun@gmail.com>
2023-03-10 20:37:49 +0100

    resolving a merge conflict

--- src/styles/StyleUtils.js
+++ src/styles/StyleUtils.js
@@ -821,6 +821,63 @@ function getEmojiPickerListHeight(hasAdditionalSpace) {
     };
 }
 
+/**
+ * Gets styles for Emoji Suggestion row
+ *
+ * @param {Number} highlightedEmojiIndex
+ * @param {Number} rowHeight
+ * @param {Boolean} hovered
+ * @param {Number} currentEmojiIndex
+ * @returns {Object}
+ */
+function getEmojiSuggestionItemStyle(
+    highlightedEmojiIndex,
+    rowHeight,
+    hovered,
+    currentEmojiIndex,
+) {
+    return [
+        {
+            height: rowHeight,
+            justifyContent: 'center',
+        },
+        (currentEmojiIndex === highlightedEmojiIndex && !hovered) || hovered
+            ? {
+                backgroundColor: themeColors.highlightBG,
+            }
+            : {},
+    ];
+}
+
+/**
+ * Gets the correct position for emoji suggestion container
+ *
+ * @param {Number} itemsHeight
+ * @param {Boolean} shouldIncludeReportRecipientLocalTimeHeight
+ * @returns {Object}
+ */
+function getEmojiSuggestionContainerStyle(
+    itemsHeight,
+    shouldIncludeReportRecipientLocalTimeHeight,
+) {
+    const optionalPadding = shouldIncludeReportRecipientLocalTimeHeight ? CONST.RECIPIENT_LOCAL_TIME_HEIGHT : 0;
+    const padding = CONST.EMOJI_SUGGESTER.SUGGESTER_PADDING - optionalPadding;
+
+    // The suggester is positioned absolutely within the component that includes the input and RecipientLocalTime view (for non-expanded mode only). To position it correctly,
+    // we need to shift it by the suggester's height plus its padding and, if applicable, the height of the RecipientLocalTime view.
+    return {
+        overflow: 'hidden',
+        top: -(itemsHeight + padding),
+    };
+}
+
+/**
+ * Select the correct color for text.
+ * @param {Boolean} isColored
+ * @returns {String | null}
+ */
+const getColoredBackgroundStyle = isColored => ({backgroundColor: isColored ? colors.blueLink : null});
+
 function getEmojiReactionBubbleStyle(isHovered, hasUserReacted, sizeScale = 1) {
     const sizeStyles = {
         paddingVertical: styles.emojiReactionBubble.paddingVertical * sizeScale,
@@ -901,6 +958,9 @@ export {
     getReportWelcomeTopMarginStyle,
     getReportWelcomeContainerStyle,
     getEmojiPickerListHeight,
+    getEmojiSuggestionItemStyle,
+    getEmojiSuggestionContainerStyle,
+    getColoredBackgroundStyle,
     getDefaultWorspaceAvatarColor,
     getAvatarBorderRadius,
     getEmojiReactionBubbleStyle,
