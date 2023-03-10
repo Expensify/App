d69edced7d97287a055128a1af4645c702fe69ac
Janic Duplessis <janicduplessis@gmail.com>
2023-03-10 09:53:43 -0500

    Remove StatusBar usage on android

--- src/components/CustomStatusBar/index.android.js
+++ src/components/CustomStatusBar/index.android.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-
 /**
- * Only the Android platform supports "setBackgroundColor"
+ * On Android we setup the status bar in native code.
  */
 
-export default class CustomStatusBar extends React.Component {
-    componentDidMount() {
-        StatusBar.setBarStyle('light-content');
-    }
-
-    render() {
-        // Prefer to not render the StatusBar component in Android as it can cause
-        // issues with edge to edge display, which we setup in MainActivity.java.
-        return null;
-    }
+export default function CustomStatusBar() {
+    // Prefer to not render the StatusBar component in Android as it can cause
+    // issues with edge to edge display, which we setup in MainActivity.java.
+    return null;
 }
