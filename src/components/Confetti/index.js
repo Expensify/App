7b87d6d8d48c1af268aff9ebbd734312e984d4c4
Sibtain Ali <allroundexperts@gmail.com>
2023-03-14 14:59:26 +0500

    feat: added confetti sound

--- src/components/Confetti/index.js
+++ src/components/Confetti/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactConfetti from 'react-confetti';
+import {Howl} from 'howler';
+import confettiSound from '../../../assets/sounds/confetti_pop.mp3';
 
 const propTypes = {
     /** Triggers confetti */
@@ -22,6 +24,13 @@ const Confetti = (props) => {
             return props.trigger;
         });
     }, [props.trigger]);
+
+    React.useEffect(() => {
+        if (!toggle) {
+            return;
+        }
+        new Howl({src: confettiSound}).play();
+    }, [toggle]);
 
     return toggle ? <ReactConfetti gravity={0.05} recycle={false} onConfettiComplete={() => setToggle(false)} /> : null;
 };
