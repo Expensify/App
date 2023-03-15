cc5f358d42dc8438097af21255b3c2fc7c01dd8e
Marc Rousavy <me@mrousavy.com>
2023-03-15 14:18:11 -0400

    Update chatgpt.js

--- src/libs/chatgpt.js
+++ src/libs/chatgpt.js
@@ -1,13 +1,11 @@
-import {ChatGPTAPI} from 'chatgpt';
+import {ChatGPT} from 'chatgpt-official';
 
-const OPENAI_API_TOKEN = process.env.OPENAI_API_TOKEN;
-
-const api = new ChatGPTAPI({
-    apiKey: OPENAI_API_TOKEN,
-});
+const bot = new ChatGPT(process.env.OPENAI_API_TOKEN);
 
 // eslint-disable-next-line rulesdir/no-inline-named-export, import/prefer-default-export, @lwc/lwc/no-async-await
 export async function summarize(largeMessage) {
-    const res = await api.sendMessage(largeMessage);
-    return res;
+    console.log('Asking ChatGPT:', largeMessage);
+    const response = await bot.ask(largeMessage);
+    console.log('ChatGPT replied:', response);
+    return response;
 }
