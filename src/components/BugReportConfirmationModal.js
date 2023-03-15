7a04abe54709d564ec770d6dfe128fdb5503a4e6
Georgia Monahan <georgia@expensify.com>
2023-03-15 14:43:57 -0400

    Add bug image to modal

--- src/components/BugReportConfirmationModal.js
+++ src/components/BugReportConfirmationModal.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View} from 'react-native';
 import {withOnyx} from 'react-native-onyx';
-import HeaderWithCloseButton from './HeaderWithCloseButton';
-import Text from './Text';
-import Modal from './Modal';
 import CONST from '../CONST';
-import styles from '../styles/styles';
-import * as StyleUtils from '../styles/StyleUtils';
 import withWindowDimensions, {windowDimensionsPropTypes} from './withWindowDimensions';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 import compose from '../libs/compose';
@@ -16,6 +10,8 @@ import * as BugReportShortcutsActions from '../libs/actions/BugReportShortcuts';
 import * as ModalActions from '../libs/actions/Modal';
 import ONYXKEYS from '../ONYXKEYS';
 import ConfirmModal from './ConfirmModal';
+import Icon from './Icon';
+import * as Illustrations from './Icon/Illustrations';
 
 const propTypes = {
     /** prop to set shortcuts modal visibility */
@@ -68,7 +64,17 @@ class BugReportConfirmationModal extends React.Component {
                     isVisible={this.props.isBugReportModalOpen}
                     onConfirm={this.submitAndClose}
                     onCancel={this.close}
-                    prompt="Would you like to report a bug?"
+                    prompt={(
+                        <Icon
+                            src={Illustrations.Bug}
+                            width={228}
+                            height={236}
+                            fill="#0180FF"
+                            style={[
+                                {width: 200, height: 200},
+                            ]}
+                        />
+                    )}
                     confirmText="Report Bug"
                     cancelText={this.props.translate('common.cancel')}
                 />
