46f6341785a6b903c44b39f5f4d91f5000daec41
Christoph Pader <christoph@margelo.io>
2023-03-15 14:28:47 -0400

    cg

--- src/libs/chatgpt.js
+++ src/libs/chatgpt.js
@@ -1,11 +1,13 @@
-import {ChatGPT} from 'chatgpt-official';
+import {ChatGPTAPI} from 'chatgpt';
 
-const bot = new ChatGPT(process.env.OPENAI_API_TOKEN);
+const api = new ChatGPTAPI({
+    apiKey: process.env.OPENAI_API_TOKEN,
+});
 
 // eslint-disable-next-line rulesdir/no-inline-named-export, import/prefer-default-export, @lwc/lwc/no-async-await
 export async function summarize(largeMessage) {
     console.log('Asking ChatGPT:', largeMessage);
-    const response = await bot.ask(largeMessage);
-    console.log('ChatGPT replied:', response);
-    return response;
+    const response = await api.sendMessage(largeMessage);
+    console.log('ChatGPT replied:', response.text);
+    return response.text;
 }
