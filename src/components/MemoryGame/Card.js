598844a5bce5750b3c1b1e52af345f1469a587ea
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-03-15 14:15:02 -0400

    add card details

--- src/components/MemoryGame/Card.js
+++ src/components/MemoryGame/Card.js
@@ -1,28 +1,45 @@
 import * as React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Pressable} from 'react-native';
 
 import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withRepeat,
-  withTiming,
+    useSharedValue, useAnimatedStyle, withRepeat, withTiming,
 } from 'react-native-reanimated';
 
-export default function Card({index, key, card, onPress}) {
-  const handlePress = () => {
-    console.log('flip');
-    if (onPress) onPress();
-  };
+export default function Card({
+    index, key, card, onPress, isDisabled, isInactive, isFlipped,
+}) {
+    const [isOn, setIsOn] = React.useState(false);
 
-  return (
-    <Pressable style={[styles.container]} onPress={handlePress}></Pressable>
-  );
+    const sv = useSharedValue(0);
+
+    const handlePress = () => {
+        console.log('flip', isOn);
+        setIsOn(!isOn);
+
+        if (onPress) {
+            onPress();
+        }
+    };
+
+    React.useEffect(() => {
+        sv.value = withRepeat(withTiming(90, {duration: 500}), 2, true);
+    }, [isOn]);
+
+    const style = useAnimatedStyle(() => ({
+        transform: [{rotateY: `${-sv.value}deg`}],
+    }));
+
+    return (
+        <Animated.View style={style}>
+            <Pressable disabled={isDisabled} style={[styles.container]} onPress={handlePress} />
+        </Animated.View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'pink',
-  },
+    container: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'green',
+    },
 });
