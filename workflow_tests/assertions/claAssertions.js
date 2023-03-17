a7e8de2ae0162a707fba0b196b760bc5f5a1093b
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-03-17 15:22:32 +0100

    Add tests
    Added test for the `cla.yml` workflow
    
    See: https://github.com/Expensify/App/issues/13604

---
+++ workflow_tests/assertions/claAssertions.js
@@ -0,0 +1,64 @@
+const utils = require('../utils/utils');
+
+const assertCLAJobExecuted = (workflowResult, commentBody = '', githubRepository = '', didExecute = true, runAssitant = true) => {
+    const steps = [
+        utils.getStepAssertion(
+            'CLA comment check',
+            true,
+            null,
+            'CLA',
+            'CLA comment check',
+            [{key: 'text', value: commentBody}, {key: 'regex', value: '\\s*I have read the CLA Document and I hereby sign the CLA\\s*'}],
+            [],
+        ),
+        utils.getStepAssertion(
+            'CLA comment re-check',
+            true,
+            null,
+            'CLA',
+            'CLA comment re-check',
+            [{key: 'text', value: commentBody}, {key: 'regex', value: '\\s*recheck\\s*'}],
+            [],
+        ),
+    ];
+
+    for (const expectedStep of steps) {
+        if (didExecute) {
+            expect(workflowResult).toEqual(expect.arrayContaining([expectedStep]));
+        } else {
+            expect(workflowResult).not.toEqual(expect.arrayContaining([expectedStep]));
+        }
+    }
+
+    const assistantSteps = [
+        utils.getStepAssertion(
+            'CLA Assistant',
+            true,
+            null,
+            'CLA',
+            'CLA Assistant',
+            [
+                {key: 'path-to-signatures', value: `${githubRepository}/cla.json`},
+                {key: 'path-to-document', value: `https://github.com/${githubRepository}/blob/main/contributingGuides/CLA.md`},
+                {key: 'branch', value: 'main'},
+                {key: 'remote-organization-name', value: 'Expensify'},
+                {key: 'remote-repository-name', value: 'CLA'},
+                {key: 'lock-pullrequest-aftermerge', value: false},
+                {key: 'allowlist', value: 'OSBotify,snyk-bot'},
+            ],
+            [{key: 'GITHUB_TOKEN', value: '***'}, {key: 'PERSONAL_ACCESS_TOKEN', value: '***'}],
+        ),
+    ];
+
+    for (const step of assistantSteps) {
+        if (didExecute && runAssitant) {
+            expect(workflowResult).toEqual(expect.arrayContaining([step]));
+        } else {
+            expect(workflowResult).not.toEqual(expect.arrayContaining([step]));
+        }
+    }
+};
+
+module.exports = {
+    assertCLAJobExecuted,
+};
