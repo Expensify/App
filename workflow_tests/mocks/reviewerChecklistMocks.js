0e6e74cf2108fd7333bf43b9008e02952cd2ce47
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-03-24 14:20:51 +0100

    Add tests
    Added tests for `reviewerChecklist` workflow
    
    See: https://github.com/Expensify/App/issues/13604

---
+++ workflow_tests/mocks/reviewerChecklistMocks.js
@@ -0,0 +1,17 @@
+const utils = require('../utils/utils');
+
+// checklist
+const REVIEWERCHECKLIST__CHECKLIST__REVIEWERCHECKLIST_JS__STEP_MOCK = utils.getMockStep(
+    'reviewerChecklist.js',
+    'reviewerChecklist.js',
+    'CHECKLIST',
+    ['GITHUB_TOKEN'],
+    [],
+);
+const REVIEWERCHECKLIST__CHECKLIST__STEP_MOCKS = [
+    REVIEWERCHECKLIST__CHECKLIST__REVIEWERCHECKLIST_JS__STEP_MOCK,
+];
+
+module.exports = {
+    REVIEWERCHECKLIST__CHECKLIST__STEP_MOCKS,
+};
