cc2a6bb74fde2014334aaa5885b227f4313c5f52
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-03-27 12:11:17 +0200

    Add tests
    Added tests for `warnCPLabel` workflow
    
    See: https://github.com/Expensify/App/issues/13604

---
+++ workflow_tests/assertions/warnCPLabelAssertions.js
@@ -0,0 +1,49 @@
+const utils = require('../utils/utils');
+
+const assertWarnCPLabelJobExecuted = (workflowResult, didExecute = true, isSuccessful = true) => {
+    const steps = [
+        utils.getStepAssertion(
+            'Comment on PR to explain the CP Staging label',
+            true,
+            null,
+            'WARNCPLABEL',
+            'Comment on PR to explain the CP Staging label',
+            [{key: 'github_token', value: '***'}],
+            [],
+        ),
+    ];
+
+    for (const expectedStep of steps) {
+        if (didExecute) {
+            if (isSuccessful) {
+                expect(workflowResult).toEqual(expect.arrayContaining([expectedStep]));
+            }
+        } else {
+            expect(workflowResult).not.toEqual(expect.arrayContaining([expectedStep]));
+        }
+    }
+
+    const failedSteps = [
+        utils.getStepAssertion(
+            'Announce failed workflow in Slack',
+            true,
+            null,
+            'WARNCPLABEL',
+            'Announce failed workflow in Slack',
+            [{key: 'SLACK_WEBHOOK', value: '***'}],
+            [],
+        ),
+    ];
+
+    for (const step of failedSteps) {
+        if (didExecute && !isSuccessful) {
+            expect(workflowResult).toEqual(expect.arrayContaining([step]));
+        } else {
+            expect(workflowResult).not.toEqual(expect.arrayContaining([step]));
+        }
+    }
+};
+
+module.exports = {
+    assertWarnCPLabelJobExecuted,
+};
---
+++ workflow_tests/mocks/warnCPLabelMocks.js
@@ -0,0 +1,25 @@
+const utils = require('../utils/utils');
+
+// warncplabel
+const WARNCPLABEL__WARNCPLABEL__COMMENT_ON_PR_TO_EXPLAIN_THE_CP_STAGING_LABEL__STEP_MOCK = utils.getMockStep(
+    'Comment on PR to explain the CP Staging label',
+    'Comment on PR to explain the CP Staging label',
+    'WARNCPLABEL',
+    ['github_token'],
+    [],
+);
+const WARNCPLABEL__WARNCPLABEL__ANNOUNCE_FAILED_WORKFLOW_IN_SLACK__STEP_MOCK = utils.getMockStep(
+    'Announce failed workflow in Slack',
+    'Announce failed workflow in Slack',
+    'WARNCPLABEL',
+    ['SLACK_WEBHOOK'],
+    [],
+);
+const WARNCPLABEL__WARNCPLABEL__STEP_MOCKS = [
+    WARNCPLABEL__WARNCPLABEL__COMMENT_ON_PR_TO_EXPLAIN_THE_CP_STAGING_LABEL__STEP_MOCK,
+    WARNCPLABEL__WARNCPLABEL__ANNOUNCE_FAILED_WORKFLOW_IN_SLACK__STEP_MOCK,
+];
+
+module.exports = {
+    WARNCPLABEL__WARNCPLABEL__STEP_MOCKS,
+};
