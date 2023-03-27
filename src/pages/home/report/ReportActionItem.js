34b9e2b0833eb29c94fffef6d90e502a282dfc3d
Carlos Martins <cmartins@expensify.com>
2023-03-27 14:51:58 +0000

    update color

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -184,7 +184,7 @@ class ReportActionItem extends Component {
                                 action={this.props.action}
                                 style={[
                                     (!this.props.displayAsGroup && isAttachment) ? styles.mt2 : undefined,
-                                    CONST.REPORT.ACTIONS.TYPE.POLICYCHANGELOG_REGEX.test(this.props.action.actionName) ? styles.buttonSuccessDisabled : undefined,
+                                    CONST.REPORT.ACTIONS.TYPE.POLICYCHANGELOG_REGEX.test(this.props.action.actionName) ? styles.colorMuted : undefined,
                                 ]}
                             />
                         ) : (
