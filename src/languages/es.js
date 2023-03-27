ebc49c27b1d279d6708b2e7648ebb98d1d39d5ae
Syed Ayaz Hussain <56740856+ayazhussain79@users.noreply.github.com>
2023-03-28 01:18:45 +0500

    Date of birth in Spanish to lower case

--- src/languages/es.js
+++ src/languages/es.js
@@ -55,7 +55,7 @@ export default {
         invite: 'Invitar',
         here: 'aquí',
         date: 'Fecha',
-        dob: 'Fecha de Nacimiento',
+        dob: 'Fecha de nacimiento',
         currentYear: 'Año actual',
         currentMonth: 'Mes actual',
         ssnLast4: 'Últimos 4 dígitos de su SSN',
