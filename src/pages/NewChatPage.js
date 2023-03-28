ac79773ac5f1b22faa3a3bec52dbc63abc41d84f
Jack Nam <jack@expensify.com>
2023-03-28 15:15:20 +0000

    add back class methods

--- src/pages/NewChatPage.js
+++ src/pages/NewChatPage.js
@@ -45,13 +45,13 @@ const defaultProps = {
 };
 
 const NewChatPage = (props) => {
-    const excludedGroupEmails = _.without(CONST.EXPENSIFY_EMAILS, CONST.EMAIL.CONCIERGE);
-
     const [searchTerm, setSearchTerm] = useState('');
     const [recentReports, setRecentReports] = useState(initialRecentReports);
     const [personalDetails, setPersonalDetails] = useState(initialPersonalDetails);
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [userToInvite, setUserToInvite] = useState(initialUserToInvite);
+
+    const excludedGroupEmails = _.without(CONST.EXPENSIFY_EMAILS, CONST.EMAIL.CONCIERGE);
 
     useEffect(() => {
         const {
@@ -73,7 +73,197 @@ const NewChatPage = (props) => {
     }, []);
 
     
+    /**
+     * Returns the sections needed for the OptionsSelector
+     *
+     * @param {Boolean} maxParticipantsReached
+     * @returns {Array}
+     */
+    function getSections(maxParticipantsReached) {
+        const sections = [];
+        let indexOffset = 0;
 
+        if (props.isGroupChat) {
+            sections.push({
+                title: undefined,
+                data: selectedOptions,
+                shouldShow: !_.isEmpty(selectedOptions),
+                indexOffset,
+            });
+            indexOffset += selectedOptions.length;
+
+            if (maxParticipantsReached) {
+                return sections;
+            }
+        }
+
+        // Filtering out selected users from the search results
+        const filterText = _.reduce(selectedOptions, (str, {login}) => `${str} ${login}`, '');
+        const recentReportsWithoutSelected = _.filter(recentReports, ({login}) => !filterText.includes(login));
+        const personalDetailsWithoutSelected = _.filter(personalDetails, ({login}) => !filterText.includes(login));
+        const hasUnselectedUserToInvite = userToInvite && !filterText.includes(userToInvite.login);
+
+        sections.push({
+            title: this.props.translate('common.recents'),
+            data: recentReportsWithoutSelected,
+            shouldShow: !_.isEmpty(recentReportsWithoutSelected),
+            indexOffset,
+        });
+        indexOffset += recentReportsWithoutSelected.length;
+
+        sections.push({
+            title: this.props.translate('common.contacts'),
+            data: personalDetailsWithoutSelected,
+            shouldShow: !_.isEmpty(personalDetailsWithoutSelected),
+            indexOffset,
+        });
+        indexOffset += personalDetailsWithoutSelected.length;
+
+        if (hasUnselectedUserToInvite) {
+            sections.push(({
+                title: undefined,
+                data: [userToInvite],
+                shouldShow: true,
+                indexOffset,
+            }));
+        }
+
+        return sections;
+    }
+
+    function updateOptionsWithSearchTerm(searchTerm = '') {
+        const {
+            recentReports,
+            personalDetails,
+            userToInvite,
+        } = OptionsListUtils.getNewChatOptions(
+            props.reports,
+            props.personalDetails,
+            props.betas,
+            searchTerm,
+            [],
+            props.isGroupChat ? excludedGroupEmails : [],
+        );
+        this.setState({
+            searchTerm,
+            userToInvite,
+            recentReports,
+            personalDetails,
+        });
+    }
+
+    /**
+     * Removes a selected option from list if already selected. If not already selected add this option to the list.
+     * @param {Object} option
+     */
+    function toggleOption(option) {
+        this.setState((prevState) => {
+            const isOptionInList = _.some(prevState.selectedOptions, selectedOption => (
+                selectedOption.login === option.login
+            ));
+
+            let newSelectedOptions;
+
+            if (isOptionInList) {
+                newSelectedOptions = _.reject(prevState.selectedOptions, selectedOption => (
+                    selectedOption.login === option.login
+                ));
+            } else {
+                newSelectedOptions = [...prevState.selectedOptions, option];
+            }
+
+            const {
+                recentReports,
+                personalDetails,
+                userToInvite,
+            } = OptionsListUtils.getNewChatOptions(
+                this.props.reports,
+                this.props.personalDetails,
+                this.props.betas,
+                prevState.searchTerm,
+                [],
+                this.excludedGroupEmails,
+            );
+
+            return {
+                selectedOptions: newSelectedOptions,
+                recentReports,
+                personalDetails,
+                userToInvite,
+                searchTerm: prevState.searchTerm,
+            };
+        });
+    }
+
+    /**
+     * Creates a new 1:1 chat with the option and the current user,
+     * or navigates to the existing chat if one with those participants already exists.
+     *
+     * @param {Object} option
+     */
+    function createChat(option) {
+        Report.navigateToAndOpenReport([option.login]);
+    }
+
+    /**
+     * Creates a new group chat with all the selected options and the current user,
+     * or navigates to the existing chat if one with those participants already exists.
+     */
+    function createGroup() {
+        if (!this.props.isGroupChat) {
+            return;
+        }
+
+        const userLogins = _.pluck(this.state.selectedOptions, 'login');
+        if (userLogins.length < 1) {
+            return;
+        }
+        Report.navigateToAndOpenReport(userLogins);
+    }
+        const maxParticipantsReached = this.state.selectedOptions.length === CONST.REPORT.MAXIMUM_PARTICIPANTS;
+        const sections = this.getSections(maxParticipantsReached);
+        const headerMessage = OptionsListUtils.getHeaderMessage(
+            (this.state.personalDetails.length + this.state.recentReports.length) !== 0,
+            Boolean(this.state.userToInvite),
+            this.state.searchTerm,
+            maxParticipantsReached,
+        );
+        return (
+            <ScreenWrapper includeSafeAreaPaddingBottom={false}>
+                {({didScreenTransitionEnd, safeAreaPaddingBottomStyle}) => (
+                    <>
+                        <HeaderWithCloseButton
+                            title={this.props.isGroupChat
+                                ? this.props.translate('sidebarScreen.newGroup')
+                                : this.props.translate('sidebarScreen.newChat')}
+                            onCloseButtonPress={() => Navigation.dismissModal(true)}
+                        />
+                        <View style={[styles.flex1, styles.w100, styles.pRelative, this.state.selectedOptions.length > 0 ? safeAreaPaddingBottomStyle : {}]}>
+                            {didScreenTransitionEnd ? (
+                                <OptionsSelector
+                                    canSelectMultipleOptions={this.props.isGroupChat}
+                                    sections={sections}
+                                    selectedOptions={this.state.selectedOptions}
+                                    value={this.state.searchTerm}
+                                    onSelectRow={option => (this.props.isGroupChat ? this.toggleOption(option) : this.createChat(option))}
+                                    onChangeText={this.updateOptionsWithSearchTerm}
+                                    headerMessage={headerMessage}
+                                    boldStyle
+                                    shouldFocusOnSelectRow={this.props.isGroupChat}
+                                    shouldShowConfirmButton={this.props.isGroupChat}
+                                    confirmButtonText={this.props.translate('newChatPage.createGroup')}
+                                    onConfirmSelection={this.createGroup}
+                                    placeholderText={this.props.translate('optionsSelector.nameEmailOrPhoneNumber')}
+                                    safeAreaPaddingBottomStyle={safeAreaPaddingBottomStyle}
+                                />
+                            ) : (
+                                <FullScreenLoadingIndicator />
+                            )}
+                        </View>
+                    </>
+                )}
+            </ScreenWrapper>
+        );
     
 };
 
@@ -108,7 +298,153 @@ class NewChatPage extends Component {
         };
     }
 
+    /**
+     * Returns the sections needed for the OptionsSelector
+     *
+     * @param {Boolean} maxParticipantsReached
+     * @returns {Array}
+     */
+    function getSections(maxParticipantsReached) {
+        const sections = [];
+        let indexOffset = 0;
 
+        if (this.props.isGroupChat) {
+            sections.push({
+                title: undefined,
+                data: this.state.selectedOptions,
+                shouldShow: !_.isEmpty(this.state.selectedOptions),
+                indexOffset,
+            });
+            indexOffset += this.state.selectedOptions.length;
+
+            if (maxParticipantsReached) {
+                return sections;
+            }
+        }
+
+        // Filtering out selected users from the search results
+        const filterText = _.reduce(this.state.selectedOptions, (str, {login}) => `${str} ${login}`, '');
+        const recentReportsWithoutSelected = _.filter(this.state.recentReports, ({login}) => !filterText.includes(login));
+        const personalDetailsWithoutSelected = _.filter(this.state.personalDetails, ({login}) => !filterText.includes(login));
+        const hasUnselectedUserToInvite = this.state.userToInvite && !filterText.includes(this.state.userToInvite.login);
+
+        sections.push({
+            title: this.props.translate('common.recents'),
+            data: recentReportsWithoutSelected,
+            shouldShow: !_.isEmpty(recentReportsWithoutSelected),
+            indexOffset,
+        });
+        indexOffset += recentReportsWithoutSelected.length;
+
+        sections.push({
+            title: this.props.translate('common.contacts'),
+            data: personalDetailsWithoutSelected,
+            shouldShow: !_.isEmpty(personalDetailsWithoutSelected),
+            indexOffset,
+        });
+        indexOffset += personalDetailsWithoutSelected.length;
+
+        if (hasUnselectedUserToInvite) {
+            sections.push(({
+                title: undefined,
+                data: [this.state.userToInvite],
+                shouldShow: true,
+                indexOffset,
+            }));
+        }
+
+        return sections;
+    }
+
+    updateOptionsWithSearchTerm(searchTerm = '') {
+        const {
+            recentReports,
+            personalDetails,
+            userToInvite,
+        } = OptionsListUtils.getNewChatOptions(
+            this.props.reports,
+            this.props.personalDetails,
+            this.props.betas,
+            searchTerm,
+            [],
+            this.props.isGroupChat ? this.excludedGroupEmails : [],
+        );
+        this.setState({
+            searchTerm,
+            userToInvite,
+            recentReports,
+            personalDetails,
+        });
+    }
+
+    /**
+     * Removes a selected option from list if already selected. If not already selected add this option to the list.
+     * @param {Object} option
+     */
+    toggleOption(option) {
+        this.setState((prevState) => {
+            const isOptionInList = _.some(prevState.selectedOptions, selectedOption => (
+                selectedOption.login === option.login
+            ));
+
+            let newSelectedOptions;
+
+            if (isOptionInList) {
+                newSelectedOptions = _.reject(prevState.selectedOptions, selectedOption => (
+                    selectedOption.login === option.login
+                ));
+            } else {
+                newSelectedOptions = [...prevState.selectedOptions, option];
+            }
+
+            const {
+                recentReports,
+                personalDetails,
+                userToInvite,
+            } = OptionsListUtils.getNewChatOptions(
+                this.props.reports,
+                this.props.personalDetails,
+                this.props.betas,
+                prevState.searchTerm,
+                [],
+                this.excludedGroupEmails,
+            );
+
+            return {
+                selectedOptions: newSelectedOptions,
+                recentReports,
+                personalDetails,
+                userToInvite,
+                searchTerm: prevState.searchTerm,
+            };
+        });
+    }
+
+    /**
+     * Creates a new 1:1 chat with the option and the current user,
+     * or navigates to the existing chat if one with those participants already exists.
+     *
+     * @param {Object} option
+     */
+    createChat(option) {
+        Report.navigateToAndOpenReport([option.login]);
+    }
+
+    /**
+     * Creates a new group chat with all the selected options and the current user,
+     * or navigates to the existing chat if one with those participants already exists.
+     */
+    createGroup() {
+        if (!this.props.isGroupChat) {
+            return;
+        }
+
+        const userLogins = _.pluck(this.state.selectedOptions, 'login');
+        if (userLogins.length < 1) {
+            return;
+        }
+        Report.navigateToAndOpenReport(userLogins);
+    }
 
     render() {
         const maxParticipantsReached = this.state.selectedOptions.length === CONST.REPORT.MAXIMUM_PARTICIPANTS;
