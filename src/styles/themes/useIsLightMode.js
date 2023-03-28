288160ea4db4c4cd7e8db332cdb90c848f0d8a8d
Christoph Pader <christoph@margelo.io>
2023-03-28 10:30:05 +0200

    add change listener to useIsLightMode

--- src/styles/themes/useIsLightMode.js
+++ src/styles/themes/useIsLightMode.js
@@ -1,7 +1,16 @@
+import {useState, useEffect} from 'react';
 import {Appearance} from 'react-native';
 
 function useIsLightMode() {
-    return Appearance.getColorScheme() === 'light';
+    const [colorScheme, setColorScheme] = useState(() => Appearance.getColorScheme());
+
+    useEffect(() => {
+        const unsubcribe = Appearance.addChangeListener(({newColorScheme}) => setColorScheme(newColorScheme)).remove;
+
+        return unsubcribe;
+    }, []);
+
+    return colorScheme === 'light';
 }
 
 export default useIsLightMode;
