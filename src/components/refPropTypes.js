5718dbe8b8c8c3c306045616f0189ca343541e99
Jasper Huang <jasper@expensify.com>
2023-03-29 09:30:41 +0000

    fix propTypes

--- src/components/refPropTypes.js
+++ src/components/refPropTypes.js
@@ -2,5 +2,5 @@ import PropTypes from 'prop-types';
 
 export default PropTypes.oneOfType([
     PropTypes.func,
-    PropTypes.shape({current: PropTypes.instanceOf(Element)}),
+    PropTypes.shape({current: PropTypes.elementType}),
 ]);
