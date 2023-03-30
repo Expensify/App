8b08eb3a4f75bb47a9d22e695cb20aeb2bd8bf2a
Andrew Rosiclair <arosiclair@expensify.com>
2023-03-30 14:23:52 +0000

    remove unnecessary docs

--- src/libs/Notification/PushNotification/configureForegroundNotifications/index.android.js
+++ src/libs/Notification/PushNotification/configureForegroundNotifications/index.android.js
@@ -1,9 +1,6 @@
 import Airship from '@ua/react-native-airship';
 import shouldShowPushNotification from '../shouldShowPushNotification';
 
-/**
- * Configures notification handling while in the foreground on iOS and Android. This is a no-op on other platforms.
- */
 export default function configureForegroundNotifications() {
     Airship.push.android.setForegroundDisplayPredicate(pushPayload => Promise.resolve(shouldShowPushNotification(pushPayload)));
 }
--- src/libs/Notification/PushNotification/configureForegroundNotifications/index.ios.js
+++ src/libs/Notification/PushNotification/configureForegroundNotifications/index.ios.js
@@ -1,9 +1,6 @@
 import Airship, {iOS} from '@ua/react-native-airship';
 import shouldShowPushNotification from '../shouldShowPushNotification';
 
-/**
- * Configures notification handling while in the foreground on iOS and Android. This is a no-op on other platforms.
- */
 export default function configureForegroundNotifications() {
     // Set our default iOS foreground presentation to be loud with a banner
     // More info here https://developer.apple.com/documentation/usernotifications/unusernotificationcenterdelegate/1649518-usernotificationcenter
