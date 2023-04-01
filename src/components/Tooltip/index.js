70e694ba90500f978e6c0121dabf01f407957095
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-04-01 15:21:19 +0200

    Use BoundsObserver to keep tooltip over the wrapper

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,12 +1,12 @@
-import _ from 'underscore';
+import _ from 'lodash';
 import React, {PureComponent} from 'react';
 import {Animated, View} from 'react-native';
+import {BoundsObserver} from '@react-ng/bounds-observer';
 import TooltipRenderedOnPageBody from './TooltipRenderedOnPageBody';
 import Hoverable from '../Hoverable';
 import withWindowDimensions from '../withWindowDimensions';
-import {propTypes, defaultProps} from './tooltipPropTypes';
+import {defaultProps, propTypes} from './tooltipPropTypes';
 import TooltipSense from './TooltipSense';
-import makeCancellablePromise from '../../libs/MakeCancellablePromise';
 import * as DeviceCapabilities from '../../libs/DeviceCapabilities';
 
 class Tooltip extends PureComponent {
@@ -30,57 +30,20 @@ class Tooltip extends PureComponent {
 
         // Whether the tooltip is first tooltip to activate the TooltipSense
         this.isTooltipSenseInitiator = false;
-        this.shouldStartShowAnimation = false;
         this.animation = new Animated.Value(0);
         this.hasHoverSupport = DeviceCapabilities.hasHoverSupport();
 
-        this.getWrapperPosition = this.getWrapperPosition.bind(this);
         this.showTooltip = this.showTooltip.bind(this);
         this.hideTooltip = this.hideTooltip.bind(this);
+        this.updateBounds = this.updateBounds.bind(this);
     }
 
-    componentDidUpdate(prevProps) {
-        if (this.props.windowWidth === prevProps.windowWidth && this.props.windowHeight === prevProps.windowHeight) {
-            return;
-        }
-
-        this.getWrapperPositionPromise = makeCancellablePromise(this.getWrapperPosition());
-        this.getWrapperPositionPromise.promise.then(({x, y}) => this.setState({xOffset: x, yOffset: y}));
-    }
-
-    componentWillUnmount() {
-        if (!this.getWrapperPositionPromise) {
-            return;
-        }
-
-        this.getWrapperPositionPromise.cancel();
-    }
-
-    /**
-     * Measure the position of the wrapper view relative to the window.
-     *
-     * @returns {Promise}
-     */
-    getWrapperPosition() {
-        return new Promise((resolve) => {
-            // Make sure the wrapper is mounted before attempting to measure it.
-            if (this.wrapperView && _.isFunction(this.wrapperView.measureInWindow)) {
-                this.wrapperView.measureInWindow((x, y, width, height) =>
-                    resolve({
-                        x,
-                        y,
-                        width,
-                        height,
-                    }),
-                );
-            } else {
-                resolve({
-                    x: 0,
-                    y: 0,
-                    width: 0,
-                    height: 0,
-                });
-            }
+    updateBounds(bounds) {
+        this.setState({
+            wrapperWidth: bounds.width,
+            wrapperHeight: bounds.height,
+            xOffset: bounds.x,
+            yOffset: bounds.y,
         });
     }
 
@@ -91,38 +54,22 @@ class Tooltip extends PureComponent {
         if (!this.state.isRendered) {
             this.setState({isRendered: true});
         }
+
         this.animation.stopAnimation();
-        this.shouldStartShowAnimation = true;
 
-        // We have to dynamically calculate the position here as tooltip could have been rendered on some elments
-        // that has changed its position
-        this.getWrapperPositionPromise = makeCancellablePromise(this.getWrapperPosition());
-        this.getWrapperPositionPromise.promise.then(({x, y, width, height}) => {
-            this.setState({
-                wrapperWidth: width,
-                wrapperHeight: height,
-                xOffset: x,
-                yOffset: y,
-            });
-
-            // We may need this check due to the reason that the animation start will fire async
-            // and hideTooltip could fire before it thus keeping the Tooltip visible
-            if (this.shouldStartShowAnimation) {
-                // When TooltipSense is active, immediately show the tooltip
-                if (TooltipSense.isActive()) {
-                    this.animation.setValue(1);
-                } else {
-                    this.isTooltipSenseInitiator = true;
-                    Animated.timing(this.animation, {
-                        toValue: 1,
-                        duration: 140,
-                        delay: 500,
-                        useNativeDriver: false,
-                    }).start();
-                }
-                TooltipSense.activate();
-            }
-        });
+        // When TooltipSense is active, immediately show the tooltip
+        if (TooltipSense.isActive()) {
+            this.animation.setValue(1);
+        } else {
+            this.isTooltipSenseInitiator = true;
+            Animated.timing(this.animation, {
+                toValue: 1,
+                duration: 140,
+                delay: 500,
+                useNativeDriver: false,
+            }).start();
+        }
+        TooltipSense.activate();
     }
 
     /**
@@ -130,7 +77,7 @@ class Tooltip extends PureComponent {
      */
     hideTooltip() {
         this.animation.stopAnimation();
-        this.shouldStartShowAnimation = false;
+
         if (TooltipSense.isActive() && !this.isTooltipSenseInitiator) {
             this.animation.setValue(0);
         } else {
@@ -142,7 +89,10 @@ class Tooltip extends PureComponent {
                 useNativeDriver: false,
             }).start();
         }
+
         TooltipSense.deactivate();
+
+        this.setState({isRendered: false});
     }
 
     render() {
@@ -205,14 +155,19 @@ class Tooltip extends PureComponent {
                         key={[this.props.text, ...this.props.renderTooltipContentKey]}
                     />
                 )}
-                <Hoverable
-                    absolute={this.props.absolute}
-                    containerStyles={this.props.containerStyles}
-                    onHoverIn={this.showTooltip}
-                    onHoverOut={this.hideTooltip}
+                <BoundsObserver
+                    enabled={this.state.isRendered}
+                    onBoundsChange={this.updateBounds}
                 >
-                    {child}
-                </Hoverable>
+                    <Hoverable
+                        absolute={this.props.absolute}
+                        containerStyles={this.props.containerStyles}
+                        onHoverIn={this.showTooltip}
+                        onHoverOut={this.hideTooltip}
+                    >
+                        {child}
+                    </Hoverable>
+                </BoundsObserver>
             </>
         );
     }
