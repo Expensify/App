62b5df82ebfe00752ca92298fd9b810f2a801763
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-04-07 18:51:23 +0200

    remove drawer related functions from the Navigation.js

--- src/libs/Navigation/Navigation.js
+++ src/libs/Navigation/Navigation.js
@@ -1,16 +1,12 @@
 import _ from 'lodash';
 import lodashGet from 'lodash/get';
-
-/* import {Keyboard} from 'react-native'; */
 import {
     CommonActions, getPathFromState, StackActions,
 } from '@react-navigation/native';
-import Onyx from 'react-native-onyx';
 import Log from '../Log';
 import DomUtils from '../DomUtils';
 import linkTo from './linkTo';
 import ROUTES from '../../ROUTES';
-import ONYXKEYS from '../../ONYXKEYS';
 import linkingConfig from './linkingConfig';
 import navigationRef from './navigationRef';
 import NAVIGATORS from '../../NAVIGATORS';
@@ -21,36 +17,12 @@ const navigationIsReadyPromise = new Promise((resolve) => {
     resolveNavigationIsReadyPromise = resolve;
 });
 
-let resolveDrawerIsReadyPromise;
-let drawerIsReadyPromise = new Promise((resolve) => {
-    resolveDrawerIsReadyPromise = resolve;
-});
-
 let resolveReportScreenIsReadyPromise;
 let reportScreenIsReadyPromise = new Promise((resolve) => {
     resolveReportScreenIsReadyPromise = resolve;
 });
 
-let isLoggedIn = false;
 let pendingRoute = null;
-
-Onyx.connect({
-    key: ONYXKEYS.SESSION,
-    callback: val => isLoggedIn = Boolean(val && val.authToken),
-});
-
-// This flag indicates that we're trying to deeplink to a report when react-navigation is not fully loaded yet.
-// If true, this flag will cause the drawer to start in a closed state (which is not the default for small screens)
-// so it doesn't cover the report we're trying to link to.
-let didTapNotificationBeforeReady = false;
-
-function setDidTapNotification() {
-    if (navigationRef.isReady()) {
-        return;
-    }
-
-    didTapNotificationBeforeReady = true;
-}
 
 /**
  * @param {String} methodName
@@ -79,20 +51,6 @@ function goBack() {
         return;
     }
     navigationRef.current.goBack();
-}
-
-/**
- * We navigate to the certains screens with a custom action so that we can preserve the browser history in web. react-navigation does not handle this well
- * and only offers a "mobile" navigation paradigm e.g. in order to add a history item onto the browser history stack one would need to use the "push" action.
- * However, this is not performant as it would keep stacking ReportScreen instances (which are quite expensive to render).
- * We're also looking to see if we have a participants route since those also have a reportID param, but do not have the problem described above and should not use the custom action.
- *
- * @param {String} route
- * @returns {Boolean}
- */
-function isDrawerRoute(route) {
-    const {reportID, isSubReportPageRoute} = ROUTES.parseReportRouteParams(route);
-    return reportID && !isSubReportPageRoute;
 }
 
 /**
@@ -213,23 +171,6 @@ function resetIsReportScreenReadyPromise() {
     });
 }
 
-/**
- * @returns {Promise}
- */
-function isDrawerReady() {
-    return drawerIsReadyPromise;
-}
-
-function setIsDrawerReady() {
-    resolveDrawerIsReadyPromise();
-}
-
-function resetDrawerIsReadyPromise() {
-    drawerIsReadyPromise = new Promise((resolve) => {
-        resolveDrawerIsReadyPromise = resolve;
-    });
-}
-
 function isReportScreenReady() {
     return reportScreenIsReadyPromise;
 }
@@ -246,15 +187,10 @@ export default {
     isActiveRoute,
     getActiveRoute,
     goBack,
-    setDidTapNotification,
     isNavigationReady,
     setIsNavigationReady,
     getReportIDFromRoute,
-    isDrawerReady,
-    setIsDrawerReady,
-    resetDrawerIsReadyPromise,
     resetIsReportScreenReadyPromise,
-    isDrawerRoute,
     isReportScreenReady,
     setIsReportScreenIsReady,
 
