7608c7e1e334645bf308dd95d63103277982ed76
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-04-07 12:35:38 -0700

    use e.g. instead of Format: and deprecate common.zipCodeExample then use custom formatter

--- src/languages/es.js
+++ src/languages/es.js
@@ -130,7 +130,7 @@ export default {
         yesContinue: 'Sí, Continuar',
         zipCodeExample: 'p. ej. 12345, 12345-1234, 12345 1234',
         websiteExample: 'p. ej. https://www.expensify.com',
-        format: ({zipSampleFormat}) => (zipSampleFormat ? `Formato: ${zipSampleFormat}` : ''),
+        format: ({zipSampleFormat}) => (zipSampleFormat ? `p. ej. ${zipSampleFormat}` : ''),
     },
     attachmentPicker: {
         cameraPermissionRequired: 'Permiso para acceder a la cámara',
