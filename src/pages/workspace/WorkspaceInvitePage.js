77345f19f677932f0d9724df2e34905a6c59cbff
Eduardo <eduardo.graciano@callstack.com>
2023-04-12 14:25:01 +0200

    let the bottom button to move up

--- src/pages/workspace/WorkspaceInvitePage.js
+++ src/pages/workspace/WorkspaceInvitePage.js
@@ -300,7 +300,7 @@ class WorkspaceInvitePage extends React.Component {
         const policyName = lodashGet(this.props.policy, 'name');
 
         return (
-            <ScreenWrapper>
+            <ScreenWrapper shouldEnableMaxHeight>
                 {({didScreenTransitionEnd}) => (
                     <FullPageNotFoundView
                         shouldShow={_.isEmpty(this.props.policy)}
