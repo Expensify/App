8c60699ac4e1d00e1a877698d470f83f1c6b93c6
Robert Kozik <robert.kozik@swmansion.com>
2023-04-12 12:30:40 +0200

    create move focus functions for web and native

---
+++ src/libs/Accessibility/index.js
@@ -0,0 +1,10 @@
+const moveAccessibilityFocus = (ref) => {
+    if (!ref || !ref.current) {
+        return;
+    }
+    ref.current.focus();
+};
+
+export default {
+    moveAccessibilityFocus,
+};
---
+++ src/libs/Accessibility/index.native.js
@@ -0,0 +1,12 @@
+import {AccessibilityInfo} from 'react-native';
+
+const moveAccessibilityFocus = (ref) => {
+    if (!ref) {
+        return;
+    }
+    AccessibilityInfo.sendAccssibilityEvent(ref, 'focus');
+};
+
+export default {
+    moveAccessibilityFocus,
+};
