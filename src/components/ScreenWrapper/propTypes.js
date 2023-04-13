b79e16b30e1bb22c301661e70617ea2ea8b7e5de
Alex Dvoretsky <a@vcc.vc>
2023-04-13 20:15:49 +0500

    Remove unused props

--- src/components/ScreenWrapper/propTypes.js
+++ src/components/ScreenWrapper/propTypes.js
@@ -19,9 +19,6 @@ const propTypes = {
     /** Called when navigated Screen's transition is finished. It does not fire when user exit the page. */
     onEntryTransitionEnd: PropTypes.func,
 
-    /** Is the test tools modal open? */
-    isTestToolsModalOpen: PropTypes.bool,
-
     /** The behavior to pass to the KeyboardAvoidingView, requires some trial and error depending on the layout/devices used.
      *  Search 'switch(behavior)' in ./node_modules/react-native/Libraries/Components/Keyboard/KeyboardAvoidingView.js for more context */
     keyboardAvoidingViewBehavior: PropTypes.oneOf(['padding', 'height', 'position']),
@@ -43,7 +40,6 @@ const defaultProps = {
     includePaddingTop: true,
     onEntryTransitionEnd: () => {},
     modal: {},
-    isTestToolsModalOpen: false,
     keyboardAvoidingViewBehavior: 'padding',
 };
 
