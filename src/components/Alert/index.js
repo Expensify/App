db627d0249da388a5eadf46612159036a7fff92e
Ionatan Wiznia <ionatan@expensify.com>
2023-04-14 13:00:40 -0300

    Add comment

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,5 +1,12 @@
 import _ from 'underscore';
 
+/**
+ * Shows an alert modal with ok and cancel options.
+ *
+ * @param {String} title The title of the alert
+ * @param {String} description The description of the alert
+ * @param {Object[]} options An array of objects with `style` and `onPress` properties
+ */
 export default (title, description, options) => {
     const result = _.filter(window.confirm([title, description], Boolean)).join('\n');
 
