fd21d4e6bca4a3e69c543bb2a097a62878ad3c5e
Robert Kozik <robert.kozik@swmansion.com>
2023-04-17 20:57:28 +0200

    set up PressableWithoutFeedback component

---
+++ src/components/Pressable/PressableWithoutFeedback.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import GenericPressable from './GenericPressable';
+import GenericPressableProps from './GenericPressable/PropTypes';
+
+const omitedProps = [
+    'pressStyle',
+    'hoverStyle',
+    'focusStyle',
+    'activeStyle',
+    'disabledStyle',
+    'screenReaderActiveStyle',
+    'shouldUseHapticsOnPress',
+    'shouldUseHapticsOnLongPress',
+];
+
+const PressableWithoutFeedback = (props) => {
+    const propsWithoutStyling = _.omit(props, omitedProps);
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    return <GenericPressable {...propsWithoutStyling} />;
+};
+
+PressableWithoutFeedback.displayName = 'PressableWithoutFeedback';
+PressableWithoutFeedback.propTypes = _.omit(GenericPressableProps.propTypes, omitedProps);
+PressableWithoutFeedback.defaultProps = _.omit(GenericPressableProps.defaultProps, omitedProps);
+
+export default PressableWithoutFeedback;
