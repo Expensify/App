047fba8a50748d0cc5be606b06dae7ed2cc3cbb0
dukenv0307 <dukenv0307@gmail.com>
2023-04-17 15:20:14 +0700

    fix: merge main

--- src/App.js
+++ src/App.js
@@ -19,40 +19,40 @@ import {KeyboardStateProvider} from './components/withKeyboardState';
 
 // For easier debugging and development, when we are in web we expose Onyx to the window, so you can more easily set data into Onyx
 if (window && Environment.isDevelopment()) {
-    window.Onyx = Onyx;
+  window.Onyx = Onyx;
 }
 
 LogBox.ignoreLogs([
-    // Basically it means that if the app goes in the background and back to foreground on Android,
-    // the timer is lost. Currently Expensify is using a 30 minutes interval to refresh personal details.
-    // More details here: https://git.io/JJYeb
-    'Setting a timer for a long period of time',
+  // Basically it means that if the app goes in the background and back to foreground on Android,
+  // the timer is lost. Currently Expensify is using a 30 minutes interval to refresh personal details.
+  // More details here: https://git.io/JJYeb
+  'Setting a timer for a long period of time',
 ]);
 
 const fill = {flex: 1};
 
 const App = () => (
-    <GestureHandlerRootView style={fill}>
-        <ComposeProviders
-            components={[
-                OnyxProvider,
-                SafeAreaProvider,
-                PortalProvider,
-                SafeArea,
-                LocaleContextProvider,
-                HTMLEngineProvider,
-                WindowDimensionsProvider,
-                KeyboardStateProvider,
-            ]}
-        >
-            <CustomStatusBar />
-            <ErrorBoundary errorMessage="NewExpensify crash caught by error boundary">
-                <Expensify />
-            </ErrorBoundary>
-        </ComposeProviders>
-    </GestureHandlerRootView>
+  <GestureHandlerRootView style={fill}>
+    <ComposeProviders
+      components={[
+        OnyxProvider,
+        SafeAreaProvider,
+        PortalProvider,
+        SafeArea,
+        LocaleContextProvider,
+        HTMLEngineProvider,
+        WindowDimensionsProvider,
+        KeyboardStateProvider,
+      ]}>
+      <CustomStatusBar />
+      <ErrorBoundary errorMessage="NewExpensify crash caught by error boundary">
+        <Expensify />
+      </ErrorBoundary>
+    </ComposeProviders>
+  </GestureHandlerRootView>
 );
 
 App.displayName = 'App';
 
 export default App;
+asdjakbd;
