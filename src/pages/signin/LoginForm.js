3bd8395ec5707c9aa77705a7da3d66802fc77329
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-04-18 15:55:40 -0700

    Back to original port, fixup

--- src/pages/signin/LoginForm.js
+++ src/pages/signin/LoginForm.js
@@ -213,7 +213,7 @@ class LoginForm extends React.Component {
                                 containerStyles={[styles.mh0]}
                             />
                             <Button
-                                onPress={() => window.open('localhost:8082/appleauth')}
+                                onPress={() => window.open('localhost:8080/appleauth')}
                                 title="Sign in with Apple"
                             />
                             <GoogleSignInButton clientId="" onCredentialResponse={() => {}} />
