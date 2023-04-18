39b93f258679484cc0d82e05968b0fa1bc3406c0
ahmedGaber93 <a.gaber0093@gmail.com>
2023-04-18 23:12:19 +0200

    add padding bottom to base modal

--- src/styles/StyleUtils.js
+++ src/styles/StyleUtils.js
@@ -422,20 +422,17 @@ function getModalPaddingStyles({
     modalContainerStylePaddingBottom,
     insets,
 }) {
-    // set fallback value for safeAreaPaddingBottom to keep padding bottom consistent with padding top.
+    // use fallback value for safeAreaPaddingBottom to keep padding bottom consistent with padding top.
     // More info: issue #17376
-    if (insets.bottom === 0) {
-        // eslint-disable-next-line no-param-reassign
-        safeAreaPaddingBottom = modalContainerStylePaddingTop;
-    }
+    const safeAreaPaddingBottomWithFallback = insets.bottom === 0 ? modalContainerStylePaddingTop : safeAreaPaddingBottom;
     return {
         marginTop: (modalContainerStyleMarginTop || 0) + (shouldAddTopSafeAreaMargin ? safeAreaPaddingTop : 0),
-        marginBottom: (modalContainerStyleMarginBottom || 0) + (shouldAddBottomSafeAreaMargin ? safeAreaPaddingBottom : 0),
+        marginBottom: (modalContainerStyleMarginBottom || 0) + (shouldAddBottomSafeAreaMargin ? safeAreaPaddingBottomWithFallback : 0),
         paddingTop: shouldAddTopSafeAreaPadding
             ? (modalContainerStylePaddingTop || 0) + safeAreaPaddingTop
             : modalContainerStylePaddingTop || 0,
         paddingBottom: shouldAddBottomSafeAreaPadding
-            ? (modalContainerStylePaddingBottom || 0) + safeAreaPaddingBottom
+            ? (modalContainerStylePaddingBottom || 0) + safeAreaPaddingBottomWithFallback
             : modalContainerStylePaddingBottom || 0,
         paddingLeft: safeAreaPaddingLeft || 0,
         paddingRight: safeAreaPaddingRight || 0,
