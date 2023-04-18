3dbea099bdaa165e59a0379ef8ffebed6edd44f4
Robert Kozik <robert.kozik@swmansion.com>
2023-04-18 22:40:19 +0200

    comment GenericPressable propTypes

--- src/components/Pressable/GenericPressable/PropTypes.js
+++ src/components/Pressable/GenericPressable/PropTypes.js
@@ -9,22 +9,94 @@ const stylePropTypeWithFunction = PropTypes.oneOfType([
 
 const propTypes = {
     ...PressableProps,
+
+    /**
+     * onPress callback
+     */
     onPress: PropTypes.func.isRequired,
+
+    /**
+     * Specifies keyboard shortcut to trigger onPressHandler
+     * @example {shortcutKey: 'a', modifiers: ['ctrl', 'shift'], descriptionKey: 'keyboardShortcut.description'}
+     */
     keyboardShortcut: PropTypes.shape({
         descriptionKey: PropTypes.string.isRequired,
         shortcutKey: PropTypes.string.isRequired,
         modifiers: PropTypes.arrayOf(PropTypes.string),
     }),
+
+    /**
+     * Specifies if haptic feedback should be used on press
+     * @default false
+     */
     shouldUseHapticsOnPress: PropTypes.bool,
+
+    /**
+     * Specifies if haptic feedback should be used on long press
+     * @default false
+     */
     shouldUseHapticsOnLongPress: PropTypes.bool,
+
+    /**
+     * style for when the component is disabled. Can be a function that receives the component's state (active, disabled, hover, focus, pressed, isScreenReaderActive)
+     * @default {}
+     * @example {backgroundColor: 'red'}
+     * @example state => ({backgroundColor: state.active ? 'red' : 'blue'})
+     */
     disabledStyle: stylePropTypeWithFunction,
+
+    /**
+     * style for when the component is hovered. Can be a function that receives the component's state (active, disabled, hover, focus, pressed, isScreenReaderActive)
+     * @default {}
+     * @example {backgroundColor: 'red'}
+     * @example state => ({backgroundColor: state.active ? 'red' : 'blue'})
+     */
     hoverStyle: stylePropTypeWithFunction,
+
+    /**
+     * style for when the component is focused. Can be a function that receives the component's state (active, disabled, hover, focus, pressed, isScreenReaderActive)
+     * @default {}
+     * @example {backgroundColor: 'red'}
+     * @example state => ({backgroundColor: state.active ? 'red' : 'blue'})
+     */
     focusStyle: stylePropTypeWithFunction,
+
+    /**
+     * style for when the component is pressed. Can be a function that receives the component's state (active, disabled, hover, focus, pressed, isScreenReaderActive)
+     * @default {}
+     * @example {backgroundColor: 'red'}
+     * @example state => ({backgroundColor: state.active ? 'red' : 'blue'})
+     */
     pressedStyle: stylePropTypeWithFunction,
+
+    /**
+     * style for when the component is active and the screen reader is on.
+     * Can be a function that receives the component's state (active, disabled, hover, focus, pressed, isScreenReaderActive)
+     * @default {}
+     * @example {backgroundColor: 'red'}
+     * @example state => ({backgroundColor: state.active ? 'red' : 'blue'})
+     */
     screenReaderActiveStyle: stylePropTypeWithFunction,
+
+    /**
+     * Specifies if the component should be accessible when the screen reader is on
+     * @default 'all'
+     * @example 'all' - the component is accessible regardless of screen reader state
+     * @example 'active' - the component is accessible only when the screen reader is on
+     * @example 'disabled' - the component is not accessible when the screen reader is on
+     */
     enableInScreenReaderStates: PropTypes.oneOf(['all', 'active', 'disabled']),
+
+    /**
+     * Specifies which component should be focused after interacting with this component
+     */
     nextFocusRef: PropTypes.func,
     accessibilityLabel: PropTypes.string.isRequired,
+
+    /**
+     * Specifies if the component should calculate its hitSlop automatically
+     * @default true
+     */
     shouldUseAutoHitSlop: PropTypes.bool,
 };
 
