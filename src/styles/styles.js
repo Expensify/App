627d4e0d62bc490a41f7ad0659fa32ee016eb0c2
Carlos Martins <luacmartins@gmail.com>
2023-04-18 10:00:58 -0600

    Revert "Allow keyboard shortcuts to work on native devices"

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2799,9 +2799,7 @@ const styles = {
 
     keyboardShortcutModalContainer: {
         maxHeight: '100%',
-        flexShrink: 0,
-        flexGrow: 0,
-        flexBasis: 'auto',
+        flex: '0 0 auto',
     },
 
     keyboardShortcutTableWrapper: {
