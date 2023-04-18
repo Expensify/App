04dad6dcf580e6123e3cd87b6e97ba325cf50d42
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-04-18 15:37:39 -0700

    Add screens for testing web redirection

--- src/ROUTES.js
+++ src/ROUTES.js
@@ -64,6 +64,7 @@ export default {
     /** This is a utility route used to go to the user's concierge chat, or the sign-in page if the user's not authenticated */
     CONCIERGE: 'concierge',
     APPLEOAUTH: 'appleauth',
+    APPLEOAUTHRECEIVER: 'appleauthtoken/:token',
 
     IOU_REQUEST,
     IOU_BILL,
--- src/libs/Navigation/AppNavigator/PublicScreens.js
+++ src/libs/Navigation/AppNavigator/PublicScreens.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {View, Platform} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import SignInPage from '../../../pages/signin/SignInPage';
 import SetPasswordPage from '../../../pages/SetPasswordPage';
@@ -6,10 +7,57 @@ import ValidateLoginPage from '../../../pages/ValidateLoginPage';
 import LogInWithShortLivedAuthTokenPage from '../../../pages/LogInWithShortLivedAuthTokenPage';
 import SCREENS from '../../../SCREENS';
 import defaultScreenOptions from './defaultScreenOptions';
+import Text from '../../../components/Text';
+import AppleSignInScript from '../../../pages/signin/AppleSignInScript';
 
 const RootStack = createStackNavigator();
 
-const TestScreen = () => null;
+const AppleAuthScreen = () => {
+    useEffect(() => {
+        const clientId = 'com.infinitered.expensify.test';
+        const redirectURI = 'https://exptest.serveo.net/appleauth';
+        const scope = 'name email';
+        const state = '';
+        const script = document.createElement('script');
+        script.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js';
+        script.async = true;
+        script.onload = () => {
+            window.AppleID.auth.init({
+                clientId,
+                scope,
+                redirectURI,
+                state,
+                usePopup: true,
+            });
+            console.log('😇calling fn');
+            window.AppleID.auth.signIn().then((result) => {
+                console.log('got a result', result);
+                window.open(`new-expensify://signintoken/appleauthtoken/${result.authorization.id_token}`);
+            });
+        };
+
+        document.body.appendChild(script);
+    }, []);
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            {Platform.OS === 'web' && <AppleSignInScript />}
+            <Text>Redirecting you to sign in...</Text>
+        </View>
+    );
+};
+
+const AppleAuthScreenReceiver = ({route}) => {
+    const {params: {token}} = route;
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                TOKEN:
+                {' '}
+                {token}
+            </Text>
+        </View>
+    );
+};
 
 const PublicScreens = () => (
     <RootStack.Navigator>
@@ -41,7 +89,12 @@ const PublicScreens = () => (
         <RootStack.Screen
             name="AppleOAuth"
             options={defaultScreenOptions}
-            component={TestScreen}
+            component={AppleAuthScreen}
+        />
+        <RootStack.Screen
+            name="AppleOAuthReceiver"
+            options={defaultScreenOptions}
+            component={AppleAuthScreenReceiver}
         />
     </RootStack.Navigator>
 );
