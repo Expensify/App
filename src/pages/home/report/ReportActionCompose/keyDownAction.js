3e63196f45fd59be8d4859bd3f91e926d833a2e2
tienifr <christianwen18@gmail.com>
2023-04-19 17:41:29 +0700

    fix: 16098

---
+++ src/pages/home/report/ReportActionCompose/keyDownAction.js
@@ -0,0 +1,9 @@
+function listenKeyDown(callback) {
+    document.addEventListener('keydown', callback);
+}
+
+function removeListenKeyDown(callback) {
+    document.removeEventListener('keydown', callback);
+}
+
+export default {listenKeyDown, removeListenKeyDown};
