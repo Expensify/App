bf777ef954c1a54f33b9f0cfc924d3757f11e593
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-04-19 21:29:13 +0800

    unsubscribe login list and pass it as parameter instead

--- src/libs/actions/User.js
+++ src/libs/actions/User.js
@@ -29,12 +29,6 @@ Onyx.connect({
     },
 });
 
-let loginList;
-Onyx.connect({
-    key: ONYXKEYS.LOGIN_LIST,
-    callback: val => loginList = val,
-});
-
 /**
  * Changes a password for a given account
  *
@@ -191,8 +185,9 @@ function updateNewsletterSubscription(isSubscribed) {
  * Delete a specific contact method
  *
  * @param {String} contactMethod - the contact method being deleted
+ * @param {Array} loginList
  */
-function deleteContactMethod(contactMethod) {
+function deleteContactMethod(contactMethod, loginList) {
     const oldLoginData = loginList[contactMethod];
 
     // If the contact method failed to be added to the account, then it should only be deleted locally.
