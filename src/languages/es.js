66742fe37865ccddadff844d3b86bc5cc41fa2a6
laurenreidexpensify <62073721+laurenreidexpensify@users.noreply.github.com>
2023-04-19 11:08:43 +0100

    Updating translation of DC

--- src/languages/es.js
+++ src/languages/es.js
@@ -1451,7 +1451,7 @@ export default {
         },
         DC: {
             stateISO: 'DC',
-            stateName: 'District Of Columbia',
+            stateName: 'Distrito de Columbia',
         },
     },
     allCountries: {
