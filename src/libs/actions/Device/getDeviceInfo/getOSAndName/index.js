3683ed6a472b5d76e274c8b71c79d33fe2244eef
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-04-20 07:34:54 -1000

    remove ua-parser-

--- src/libs/actions/Device/getDeviceInfo/getOSAndName/index.js
+++ src/libs/actions/Device/getDeviceInfo/getOSAndName/index.js
@@ -1,12 +1,3 @@
-import {UAParser} from 'ua-parser-js';
+import * as Device from 'expensify-common/lib/Device';
 
-export default function getOSAndName() {
-    const parser = new UAParser();
-    const result = parser.getResult();
-    return {
-        os: result.os.name,
-        os_version: result.os.version,
-        device_name: result.browser.name,
-        device_version: result.browser.version,
-    };
-}
+export default Device.getOSAndName;
