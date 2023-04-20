c94e8f4794c557e7bb9a4aca1cee18348893421a
rory <rory@expensify.com>
2023-04-20 08:37:38 -0400

    Create TransactionUtils.buildOptimisticTransaction

---
+++ src/libs/TransactionUtils.js
@@ -0,0 +1,22 @@
+import DateUtils from './DateUtils';
+import * as NumberUtils from './NumberUtils';
+
+/**
+ * Optimistically generate a transaction.
+ *
+ * @param {Number} amount – in cents
+ * @param {String} comment
+ * @param {String} currency
+ */
+function buildOptimisticTransaction(amount, comment, currency) {
+    // transactionIDs are random, positive, 64-bit numbers.
+    // Because JS can only handle 53-bit numbers, transactionIDs are strings in the front-end (just like reportActionID)
+    const transactionID = NumberUtils.rand64();
+    const created = DateUtils.getDBTime();
+    return {
+        transactionID,
+        amount,
+        comment,
+        created,
+    };
+}
