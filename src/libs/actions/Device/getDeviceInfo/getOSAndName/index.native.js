99eae590d602843e9be498f376856a1034d65e65
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-04-20 09:40:31 -1000

    Include whether we are using an emulator

--- src/libs/actions/Device/getDeviceInfo/getOSAndName/index.native.js
+++ src/libs/actions/Device/getDeviceInfo/getOSAndName/index.native.js
@@ -1,14 +1,13 @@
 import Str from 'expensify-common/lib/str';
 import {
-    getDeviceNameSync, getManufacturerSync, getSystemVersion,
+    getDeviceNameSync, getManufacturerSync, getSystemVersion, isEmulatorSync,
 } from 'react-native-device-info';
 
 export default function getOSAndName() {
     const deviceName = getDeviceNameSync();
-    const isEmulator = deviceName.startsWith('sdk');
     const prettyName = `${Str.UCFirst((getManufacturerSync() || ''))} ${deviceName}`;
     return {
-        device_name: isEmulator ? `Emulator - ${prettyName}` : prettyName,
+        device_name: isEmulatorSync() ? `Emulator - ${prettyName}` : prettyName,
         os_version: getSystemVersion(),
     };
 }
