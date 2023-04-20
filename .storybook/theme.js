b5838328213a57ff922363f98f3010b8205d9c68
Ali Toshmatov <alitoshmatov2001@gmail.com>
2023-04-20 10:03:35 +0500

    Changed favicon logo, logomark, and colors

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,14 +2,21 @@ import {create} from '@storybook/theming';
 import colors from '../src/styles/colors';
 
 export default create({
-    appBg: colors.dark,
-    barSelectedColor: colors.blue,
-    base: 'light',
-    brandTitle: 'Expensify UI Docs',
+    brandTitle: 'New Expensify UI Docs',
     brandImage: 'logomark.svg',
-    colorPrimary: colors.dark,
-    colorSecondary: colors.orange,
     fontBase: 'ExpensifyNeue-Regular',
     fontCode: 'monospace',
-    textInverseColor: colors.black,
+    appBg: colors.greenAppBackground,
+    colorPrimary: colors.greenDefaultButton,
+    colorSecondary: colors.green,
+    appContentBg: colors.greenAppBackground,
+    textColor: colors.white,
+    textInverseColor: colors.white,
+    barTextColor: colors.white,
+    barSelectedColor: colors.blueLink,
+    barBg: colors.greenHighlightBackground,
+    appBorderColor: colors.greenBorders,
+    inputBg: colors.greenHighlightBackground,
+    inputBorder: colors.greenBorders,
+
 });
