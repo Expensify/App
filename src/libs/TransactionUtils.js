962009fc71f5684d3959f8c1fb1ca947ca331ee9
rory <rory@expensify.com>
2023-04-20 08:44:14 -0400

    Make comment optional in buildOptimisticTransaction

--- src/libs/TransactionUtils.js
+++ src/libs/TransactionUtils.js
@@ -5,10 +5,10 @@ import * as NumberUtils from './NumberUtils';
  * Optimistically generate a transaction.
  *
  * @param {Number} amount – in cents
- * @param {String} comment
  * @param {String} currency
+ * @param {String} comment
  */
-function buildOptimisticTransaction(amount, comment, currency) {
+function buildOptimisticTransaction(amount, currency, comment = '') {
     // transactionIDs are random, positive, 64-bit numbers.
     // Because JS can only handle 53-bit numbers, transactionIDs are strings in the front-end (just like reportActionID)
     const transactionID = NumberUtils.rand64();
