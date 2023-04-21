68e66c0344f573313ef89bfc1f670fb7ba7e1d22
staszekscp <mariusz.stanisz@swmansion.com>
2023-04-21 11:21:25 +0200

    remove unnecessary isDrawerReady and isDrawerOpen

--- src/pages/home/report/ReportActionsList.js
+++ src/pages/home/report/ReportActionsList.js
@@ -130,7 +130,7 @@ const ReportActionsList = (props) => {
 
     // Native mobile does not render updates flatlist the changes even though component did update called.
     // To notify there something changes we can use extraData prop to flatlist
-    const extraData = (!props.isDrawerOpen && props.isSmallScreenWidth) ? props.newMarkerReportActionID : undefined;
+    const extraData = props.isSmallScreenWidth ? props.newMarkerReportActionID : undefined;
     const shouldShowReportRecipientLocalTime = ReportUtils.canShowReportRecipientLocalTime(props.personalDetails, props.report);
     return (
         <Animated.View style={[animatedStyles, styles.flex1]}>
