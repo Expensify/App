3c2671a45abca866ca1070611055664d0b047d55
Jasper Huang <jasper@expensify.com>
2023-04-25 13:34:14 -0700

    use variable

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -436,9 +436,10 @@ function createOption(logins, personalDetails, report, reportActions = {}, {
         }
         reportName = ReportUtils.getReportName(report, policies);
     } else {
-        reportName = ReportUtils.getDisplayNameForParticipant(logins[0]);
-        result.keyForList = logins[0];
-        result.alternateText = LocalePhoneNumber.formatPhoneNumber(logins[0]);
+        const login = logins[0];
+        reportName = ReportUtils.getDisplayNameForParticipant(login);
+        result.keyForList = login;
+        result.alternateText = LocalePhoneNumber.formatPhoneNumber(login);
     }
 
     result.isIOUReportOwner = ReportUtils.isIOUOwnedByCurrentUser(result, iouReports);
