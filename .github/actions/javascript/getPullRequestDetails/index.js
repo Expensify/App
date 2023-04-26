3373ec76c216f3a8a999e6d3285f68c82ad372f8
jczekalski <j.a.czekalski@gmail.com>
2023-04-26 12:02:03 +0200

    log complete PR data

--- .github/actions/javascript/getPullRequestDetails/index.js
+++ .github/actions/javascript/getPullRequestDetails/index.js
@@ -54,6 +54,7 @@ function outputMergeActor(PR) {
  * @param {Object} PR
  */
 function outputPullRequestData(PR) {
+    console.log('PR data', JSON.stringify(PR));
     core.setOutput('MERGE_COMMIT_SHA', PR.merge_commit_sha);
     core.setOutput('HEAD_COMMIT_SHA', PR.head.sha);
     core.setOutput('IS_MERGED', PR.merged);
