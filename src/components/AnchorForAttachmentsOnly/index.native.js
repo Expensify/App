5a59f158a1f39967079c2284e27778510df369e2
Aswin S <mr.aswin@gmail.com>
2023-04-26 13:15:40 +0530

    fix: use StyleUtils to destructure array type

--- src/components/AnchorForAttachmentsOnly/index.native.js
+++ src/components/AnchorForAttachmentsOnly/index.native.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import * as anchorForAttachmentsOnlyPropTypes from './anchorForAttachmentsOnlyPropTypes';
-import BaseAnchorForAttachmentsOnly from './BaseAnchorForAttachmentsOnly';
+import * as StyleUtils from '../../styles/StyleUtils';
 import styles from '../../styles/styles';
+import BaseAnchorForAttachmentsOnly from './BaseAnchorForAttachmentsOnly';
+import * as anchorForAttachmentsOnlyPropTypes from './anchorForAttachmentsOnlyPropTypes';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-const AnchorForAttachmentsOnly = props => <BaseAnchorForAttachmentsOnly {...props} style={[props.style, styles.mw100]} />;
+const AnchorForAttachmentsOnly = props => <BaseAnchorForAttachmentsOnly {...props} style={[...StyleUtils.parseStyleAsArray(props.style), styles.mw100]} />;
 
 AnchorForAttachmentsOnly.propTypes = anchorForAttachmentsOnlyPropTypes.propTypes;
 AnchorForAttachmentsOnly.defaultProps = anchorForAttachmentsOnlyPropTypes.defaultProps;
