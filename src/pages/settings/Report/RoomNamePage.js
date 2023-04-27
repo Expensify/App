2b12c2051467927afd65f903d77ee7a8b86caded
neil-marcellini <neil@expensify.com>
2023-04-27 14:20:46 -0700

    Convert RoomNamePage to functional component

--- src/pages/settings/Report/RoomNamePage.js
+++ src/pages/settings/Report/RoomNamePage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useCallback} from 'react';
 import _ from 'underscore';
 import {View} from 'react-native';
 import ScreenWrapper from '../../../components/ScreenWrapper';
@@ -20,76 +20,59 @@ import * as Report from '../../../libs/actions/Report';
 const propTypes = {
     ...withLocalizePropTypes,
 
-    /* Onyx Props */
-    /** The active report */
+    /** The room report for which the name is being edited */
     report: reportPropTypes.isRequired,
 };
 
-class RoomNamePage extends Component {
-    constructor(props) {
-        super(props);
-        this.updateRoomName = this.updateRoomName.bind(this);
-        this.validate = this.validate.bind(this);
-    }
+const RoomNamePage = (props) => {
+    const report = props.report;
+    const translate = props.translate;
+    const updateRoomName = useCallback((values) => {
+        Report.updatePolicyRoomName(report, RoomNameInputUtils.modifyRoomName(values.roomName));
+        Navigation.drawerGoBack(ROUTES.getReportSettingsRoute(report.reportID));
+    }, [report]);
 
-    /**
-     * Submit form to update room's name
-     * @param {Object} values
-     * @param {String} values.roomName
-     */
-    updateRoomName(values) {
-        Report.updatePolicyRoomName(this.props.report, RoomNameInputUtils.modifyRoomName(values.roomName));
-        Navigation.drawerGoBack(ROUTES.getReportSettingsRoute(this.props.report.reportID));
-    }
-
-    /**
-     * @param {Object} values
-     * @param {String} values.roomName
-     * @returns {Object} - An object containing the errors for each inputID
-     */
-    validate(values) {
+    const validate = useCallback((values) => {
         const errors = {};
         if (_.isEmpty(values.roomName)) {
-            errors.roomName = this.props.translate('common.error.fieldRequired');
+            errors.roomName = translate('common.error.fieldRequired');
         }
-
         return errors;
-    }
+    }, [translate]);
 
-    render() {
-        return (
-            <ScreenWrapper includeSafeAreaPaddingBottom={false}>
-                <HeaderWithCloseButton
-                    title={this.props.translate('newRoomPage.roomName')}
-                    shouldShowBackButton
-                    onBackButtonPress={() => Navigation.drawerGoBack(ROUTES.getReportSettingsRoute(this.props.report.reportID))}
-                    onCloseButtonPress={() => Navigation.dismissModal(true)}
-                />
-                <Form
-                    style={[styles.flexGrow1, styles.ph5]}
-                    formID={ONYXKEYS.FORMS.ROOM_NAME_FORM}
-                    onSubmit={this.updateRoomName}
-                    validate={this.validate}
-                    submitButtonText={this.props.translate('common.save')}
-                    enabledWhenOffline
-                >
-                    <View style={styles.mb4}>
-                        <TextInput
-                            inputID="roomName"
-                            name="name"
-                            prefixCharacter={CONST.POLICY.ROOM_PREFIX}
-                            label={this.props.translate('newRoomPage.roomName')}
-                            defaultValue={this.props.report.reportName.substring(1)} // Since the room name always starts with a prefix, we omit the first character.
-                            maxLength={CONST.REPORT.MAX_ROOM_NAME_LENGTH}
-                        />
-                    </View>
-                </Form>
-            </ScreenWrapper>
-        );
-    }
-}
+    return (
+        <ScreenWrapper includeSafeAreaPaddingBottom={false}>
+            <HeaderWithCloseButton
+                title={translate('newRoomPage.roomName')}
+                shouldShowBackButton
+                onBackButtonPress={() => Navigation.drawerGoBack(ROUTES.getReportSettingsRoute(report.reportID))}
+                onCloseButtonPress={() => Navigation.dismissModal(true)}
+            />
+            <Form
+                style={[styles.flexGrow1, styles.ph5]}
+                formID={ONYXKEYS.FORMS.ROOM_NAME_FORM}
+                onSubmit={updateRoomName}
+                validate={validate}
+                submitButtonText={translate('common.save')}
+                enabledWhenOffline
+            >
+                <View style={styles.mb4}>
+                    <TextInput
+                        inputID="roomName"
+                        name="name"
+                        prefixCharacter={CONST.POLICY.ROOM_PREFIX}
+                        label={translate('newRoomPage.roomName')}
+                        defaultValue={report.reportName.substring(1)}
+                        maxLength={CONST.REPORT.MAX_ROOM_NAME_LENGTH}
+                    />
+                </View>
+            </Form>
+        </ScreenWrapper>
+    );
+};
 
 RoomNamePage.propTypes = propTypes;
+RoomNamePage.displayName = 'RoomNamePage';
 
 export default compose(
     withLocalize,
