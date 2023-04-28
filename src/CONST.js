15cf4c5e61fead83081f9eeaa8eb66493b8a6408
Cristi Paval <cristi@expensify.com>
2023-04-28 13:23:52 +0100

    New report action type in CONST file.

--- src/CONST.js
+++ src/CONST.js
@@ -400,6 +400,7 @@ const CONST = {
                 IOU: 'IOU',
                 RENAMED: 'RENAMED',
                 CHRONOSOOOLIST: 'CHRONOSOOOLIST',
+                REPORTPREVIEW: 'REPORTPREVIEW',
                 POLICYCHANGELOG: {
                     ADD_APPROVER_RULE: 'POLICYCHANGELOG_ADD_APPROVER_RULE',
                     ADD_CATEGORY: 'POLICYCHANGELOG_ADD_CATEGORY',
