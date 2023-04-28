39119b92bb831878f1b96c7fe5b788fb58eaa472
Tim Golen <tgolen@expensify.com>
2023-04-28 12:16:31 -0600

    Remove console logs

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -374,13 +374,11 @@ function getPolicyName(report) {
         return report.policyName;
     }
 
-    console.log(1, allPolicies, _.size(allPolicies))
     if (!allPolicies || _.size(allPolicies) === 0) {
         return Localize.translateLocal('workspace.common.unavailable');
     }
 
     const policy = allPolicies[`${ONYXKEYS.COLLECTION.POLICY}${report.policyID}`];
-    console.log(2, policy, `${ONYXKEYS.COLLECTION.POLICY}${report.policyID}`)
     if (!policy) {
         return report.oldPolicyName || Localize.translateLocal('workspace.common.unavailable');
     }
