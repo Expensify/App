4c3c9bd99f4651fe96774adbf303c3b59f88db9f
Victor Nyagudi <nyagudivicktor@gmail.com>
2023-05-01 16:43:11 +0300

    Add logic to prevent 'new' word from being copied

--- src/libs/SelectionScraper/index.js
+++ src/libs/SelectionScraper/index.js
@@ -2,6 +2,7 @@ import render from 'dom-serializer';
 import {parseDocument} from 'htmlparser2';
 import _ from 'underscore';
 import Str from 'expensify-common/lib/str';
+import styles from '../../styles/styles';
 
 const elementsWillBeSkipped = ['html', 'body'];
 const tagAttribute = 'data-testid';
@@ -79,6 +80,8 @@ const getHTMLOfSelection = () => {
             div.appendChild(child);
         }
     }
+
+    div.querySelectorAll(`div[style*="user-select: ${styles.userSelectNone.WebkitUserSelect}"]`).forEach(item => item.remove());
 
     return div.innerHTML;
 };
