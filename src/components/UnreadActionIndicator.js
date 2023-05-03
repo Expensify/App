8ec06a175f7ece202b2a05935022b8ccd0de9688
Andrew Gable <andrewgable@gmail.com>
2023-05-03 17:00:35 -0600

    Revert "Fix:  Copying the messages and marking a message in between as unread generates 'new' in the text"

--- src/components/UnreadActionIndicator.js
+++ src/components/UnreadActionIndicator.js
@@ -5,11 +5,7 @@ import Text from './Text';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
 
 const UnreadActionIndicator = props => (
-    <View
-        accessibilityLabel={props.translate('accessibilityHints.newMessageLineIndicator')}
-        data-action-id={props.reportActionID}
-        style={[styles.unreadIndicatorContainer, styles.userSelectNone]}
-    >
+    <View accessibilityLabel={props.translate('accessibilityHints.newMessageLineIndicator')} data-action-id={props.reportActionID} style={styles.unreadIndicatorContainer}>
         <View style={styles.unreadIndicatorLine} />
         <Text style={styles.unreadIndicatorText}>
             {props.translate('common.new')}
