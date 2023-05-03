012d2c3ee0a9b7aef7c2619c8470b874d315d468
romulo114 <romulo.dev114@gmail.com>
2023-05-03 12:02:46 -0500

    fix: border cut in emoji picker

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1570,7 +1570,6 @@ const styles = {
     },
 
     emojiSkinToneTitle: {
-        backgroundColor: themeColors.componentBG,
         width: '100%',
         ...spacing.pv1,
         fontFamily: fontFamily.EXP_NEUE_BOLD,
