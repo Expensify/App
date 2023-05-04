25b74e7d5a99a3c26ba6eb8e81220b9e80812348
Vit Horacek <vit@expensify.com>
2023-05-04 16:25:11 +0200

    Resolve merge conflict

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -4,11 +4,7 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import _ from 'underscore';
 import lodashGet from 'lodash/get';
-<<<<<<< HEAD
-=======
 import Str from 'expensify-common/lib/str';
-import getNavigationModalCardStyle from '../../../styles/getNavigationModalCardStyles';
->>>>>>> main
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../../components/withWindowDimensions';
 import CONST from '../../../CONST';
 import compose from '../../compose';
