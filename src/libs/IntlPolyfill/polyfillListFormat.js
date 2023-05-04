7a9fbbc8771321f58f036a29e929554fdb8d4de2
rory <rory@expensify.com>
2023-05-04 14:48:36 -0700

    Remove debug logs

--- src/libs/IntlPolyfill/polyfillListFormat.js
+++ src/libs/IntlPolyfill/polyfillListFormat.js
@@ -1,11 +1,7 @@
 export default function () {
-    console.log('RORY_DEBUG polyfilling ListFormat?');
     if (Intl && 'ListFormat' in Intl) {
-        console.log('RORY_DEBUG not polyfilling ListFormat');
         return;
     }
-
-    console.log('RORY_DEBUG ListFormat polyfill should be running like now-ish');
 
     require('@formatjs/intl-listformat/polyfill-force');
 
