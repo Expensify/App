ef037a4f890e1d8743177d9efdec3b6a8cd624e8
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-05-08 10:47:51 -0700

    Attempts to get Apple button to render

--- src/pages/signin/AppleSignInScript.js
+++ src/pages/signin/AppleSignInScript.js
@@ -1,60 +1,93 @@
-import React, {useEffect, useState} from 'react';
+import React, {useLayoutEffect, useState, useEffect} from 'react';
 import {View} from 'react-native';
 
-function AppleSignInScript() {
-    const [forceRender, setForceRender] = useState(undefined);
-    useEffect(() => {
-        const clientId = 'com.infinitered.expensify.test';
-        const redirectURI = 'https://exptest.ngrok.io/appleauth';
-        const scope = 'name email';
-        const state = '';
-        const script = document.createElement('script');
-        script.src = 'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js';
-        script.async = true;
-        script.onload = () => {
-            window.AppleID.auth.init({
-                clientId,
-                scope,
-                redirectURI,
-                state,
-                usePopup: true,
+const config = {
+    clientId: 'com.infinitered.expensify.test',
+    scope: 'name email',
+    redirectURI: 'https://exptest.ngrok.io/appleauth',
+    state: '',
+    nonce: '',
+    usePopup: true,
+};
+
+const cachedScripts = [];
+const useScript = (src) => {
+    // Keeping track of script loaded and error state
+    const [state, setState] = useState({
+        loaded: false,
+        error: false,
+    });
+
+    useLayoutEffect(() => {
+    // If cachedScripts array already includes src that means another instance ...
+    // ... of this hook already loaded this script, so no need to load again.
+        if (cachedScripts.includes(src)) {
+            setState({
+                loaded: true,
+                error: false,
             });
-            setForceRender(true);
+            return () => { };
+        }
+        cachedScripts.push(src);
+
+        // Create script
+        const script = document.createElement('script');
+        script.src = src;
+        script.type = 'text/javascript';
+
+        // Script event listener callbacks for load and error
+        const onScriptLoad = () => {
+            console.log("SCRIPT LOADED");
+    window.AppleID.auth.init(config);
+            setState({
+                loaded: true,
+                error: false,
+            });
+            window.AppleID.auth.renderButton();
         };
 
+        const onScriptError = () => {
+        // Remove from cachedScripts we can try loading again
+        console.log("SCRIPT ERRORED")
+            const index = cachedScripts.indexOf(src);
+            if (index >= 0) { cachedScripts.splice(index, 1); }
+            script.remove();
+
+            setState({
+                loaded: false,
+                error: true,
+            });
+        };
+
+        script.addEventListener('load', onScriptLoad);
+        script.addEventListener('error', onScriptError);
+
+        // Add script to document body
         document.body.appendChild(script);
-        document.addEventListener('AppleIDSignInOnSuccess', (event) => {
-            //
-            // Handle successful response.
-            console.log(event.detail.data);
-        });
 
-        // Listen for authorization failures.
-        document.addEventListener('AppleIDSignInOnFailure', (event) => {
-            // Handle error.
-            console.log(event.detail.error);
-        });
-    }, []);
+        // Remove event listeners on cleanup
+        return () => {
+            script.removeEventListener('load', onScriptLoad);
+            script.removeEventListener('error', onScriptError);
+        };
+    }, [src]); // Only re-run effect if script src changes
 
-    useEffect(() => { console.log('forceRender:', forceRender); }, [forceRender]);
+    return [state.loaded, state.error];
+};
 
-    return (
-        <View style={{width: 40, height: 40, marginRight: 20}}>
-            <div
-                style={{fontSize: '0'}}
-                id="appleid-signin"
-                data-mode="logo-only"
-                data-color="white"
-                data-border-radius="50"
-                data-border="false"
-                data-border-color="white"
-                data-width="40"
-                data-height="40"
-                data-type="sign in"
-            />
+function DivOnly() {
+ return <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in"></div>;
+}
 
-        </View>
+function ScriptOnly() {
+    const [loaded] = useScript(
+        `https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/${'en_US'}/appleid.auth.js`,
     );
+
+}
+
+function AppleSignInScript({style = {}}) {
+    return <><ScriptOnly/><DivOnly /></>;
 }
 
 export default AppleSignInScript;
