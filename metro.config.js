31a5993274e313e63b6e011f1f4f567664b0ef56
Fábio Henriques <contact@fabiohenriques.dev>
2023-05-08 17:42:04 +0100

    Fix metro.config.js to use new config loading setup

--- metro.config.js
+++ metro.config.js
@@ -1,46 +1,39 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- */
-
-const {getDefaultConfig} = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const _ = require('underscore');
 require('dotenv').config();
 
-/* eslint arrow-body-style: 0 */
-module.exports = (() => {
-    const isUsingMockAPI = process.env.E2E_TESTING === 'true';
-    if (isUsingMockAPI) {
-        // eslint-disable-next-line no-console
-        console.warn('⚠️ Using mock API');
-    }
+const defaultConfig = getDefaultConfig(__dirname);
 
-    return getDefaultConfig(__dirname)
-        .then((config) => {
-            return {
-                resolver: {
-                    assetExts: _.filter(config.resolver.assetExts, ext => ext !== 'svg'),
-                    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json', 'svg'],
-                    resolveRequest: (context, moduleName, platform) => {
-                        const resolution = context.resolveRequest(context, moduleName, platform);
-                        if (isUsingMockAPI && moduleName.includes('/API')) {
-                            return {
-                                ...resolution,
-                                filePath: resolution.filePath.replace(/src\/libs\/API.js/, 'src/libs/E2E/API.mock.js'),
-                            };
-                        }
-                        return resolution;
-                    },
-                },
-                transformer: {
-                    getTransformOptions: () => ({
-                        transform: {
-                            experimentalImportSupport: false,
-                            inlineRequires: true,
-                        },
-                    }),
-                    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-                },
-            };
-        });
-})();
+const isUsingMockAPI = process.env.E2E_TESTING === 'true';
+if (isUsingMockAPI) {
+    // eslint-disable-next-line no-console
+    console.warn('⚠️ Using mock API');
+}
+
+/**
+* Metro configuration
+* https://facebook.github.io/metro/docs/configuration
+*
+* @type {import('metro-config').MetroConfig}
+*/
+const config = {
+    resolver: {
+        assetExts: _.filter(defaultConfig.resolver.assetExts, ext => ext !== 'svg'),
+        sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json', 'svg'],
+        resolveRequest: (context, moduleName, platform) => {
+            const resolution = context.resolveRequest(context, moduleName, platform);
+            if (isUsingMockAPI && moduleName.includes('/API')) {
+                return {
+                    ...resolution,
+                    filePath: resolution.filePath.replace(/src\/libs\/API.js/, 'src/libs/E2E/API.mock.js'),
+                };
+            }
+            return resolution;
+        },
+    },
+    transformer: {
+        babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
