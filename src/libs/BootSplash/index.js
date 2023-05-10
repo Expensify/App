961f4520ba9ee443484fb3328f63165f269cea33
Mathieu Acthernoene <zoontek@gmail.com>
2023-05-10 15:40:53 +0200

    Remove extra logic

--- src/libs/BootSplash/index.js
+++ src/libs/BootSplash/index.js
@@ -1,33 +1,16 @@
 import Log from '../Log';
 
-let areFontsReady = false;
-document.fonts.ready.then(() => {
-    areFontsReady = true;
-});
+function resolveAfter(delay) {
+    return new Promise((resolve) => setTimeout(resolve, delay));
+}
 
 function hide() {
     Log.info('[BootSplash] hiding splash screen', false);
 
-    return new Promise((resolve) => {
-        const minMilisecondsToWait = 1.5 * 1000;
-        let passedMiliseconds = 0;
-        let isRootMounted = false;
-        const root = document.getElementById('root');
+    return document.fonts.ready.then(() => {
         const splash = document.getElementById('splash');
-
-        const intervalId = setInterval(() => {
-            passedMiliseconds += 250;
-            isRootMounted = root.children.length > 0;
-            if (passedMiliseconds >= minMilisecondsToWait && isRootMounted && areFontsReady) {
-                clearInterval(intervalId);
-                splash.style.opacity = 0;
-
-                setTimeout(() => {
-                    splash.parentNode.removeChild(splash);
-                    resolve();
-                }, 250);
-            }
-        }, 250);
+        splash.style.opacity = 0;
+        return resolveAfter(250).then(() => splash.parentNode.removeChild(splash));
     });
 }
 
