34a305ba2d1d96301569427a80132ee27ef98277
Mathieu Acthernoene <zoontek@gmail.com>
2023-05-10 12:45:15 +0200

    Fix CSP issue by inlining CSS + logo and hide once the bundle is loaded

--- src/libs/BootSplash/index.js
+++ src/libs/BootSplash/index.js
@@ -1,5 +1,42 @@
+import Log from '../Log';
+
+let areFontsReady = false;
+document.fonts.ready.then(() => {
+    areFontsReady = true;
+});
+
+function hide() {
+    Log.info('[BootSplash] hiding splash screen', false);
+
+    return new Promise((resolve) => {
+        const minMilisecondsToWait = 1.5 * 1000;
+        let passedMiliseconds = 0;
+        let isRootMounted = false;
+        const root = document.getElementById('root');
+        const splash = document.getElementById('splash');
+
+        const intervalId = setInterval(() => {
+            passedMiliseconds += 250;
+            isRootMounted = root.children.length > 0;
+            if (passedMiliseconds >= minMilisecondsToWait && isRootMounted && areFontsReady) {
+                clearInterval(intervalId);
+                splash.style.opacity = 0;
+
+                setTimeout(() => {
+                    splash.parentNode.removeChild(splash);
+                    resolve();
+                }, 250);
+            }
+        }, 250);
+    });
+}
+
+function getVisibilityStatus() {
+    return document.getElementById('splash') ? 'visible' : 'hidden';
+}
+
 export default {
-    hide: () => Promise.resolve(),
-    getVisibilityStatus: () => Promise.resolve('hidden'),
+    hide,
+    getVisibilityStatus,
     navigationBarHeight: 0,
 };
