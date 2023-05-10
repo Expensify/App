86f683defc2fe3e035ad8dd01c428e1be8a14bef
Rocio Perez-Cano <rocio@expensify.com>
2023-05-10 13:50:58 +0200

    Add elements to return

--- src/pages/ReportWelcomeMessagePage.js
+++ src/pages/ReportWelcomeMessagePage.js
@@ -1,10 +1,10 @@
 import React, {
-    useCallback, useMemo, useRef, useState,
+    useCallback, useRef, useState,
 } from 'react';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
-import {ScrollView} from 'react-native';
+import {ScrollView, TextInput, View} from 'react-native';
 import compose from '../libs/compose';
 import withLocalize, {withLocalizePropTypes} from '../components/withLocalize';
 import ONYXKEYS from '../ONYXKEYS';
@@ -15,6 +15,8 @@ import styles from '../styles/styles';
 import reportPropTypes from './reportPropTypes';
 import withReportOrNotFound from './home/report/withReportOrNotFound';
 import FullPageNotFoundView from '../components/BlockingViews/FullPageNotFoundView';
+import FixedFooter from '../components/FixedFooter';
+import Button from '../components/Button';
 
 const propTypes = {
     ...withLocalizePropTypes,
@@ -43,6 +45,7 @@ const defaultProps = {
 
 function ReportWelcomeMessagePage(props) {
     const [welcomeMessage, setWelcomeMessage] = useState('');
+    const welcomeMessageInputRef = useRef(null);
 
     const handleWelcomeMessageChange = useCallback((value) => {
         setWelcomeMessage(value);
@@ -53,7 +56,14 @@ function ReportWelcomeMessagePage(props) {
     }, [welcomeMessage]);
 
     return (
-        <ScreenWrapper>
+        <ScreenWrapper
+            onTransitionEnd={() => {
+                if (!welcomeMessageInputRef.current) {
+                    return;
+                }
+                welcomeMessageInputRef.current.focus();
+            }}
+        >
             <FullPageNotFoundView shouldShow={_.isEmpty(this.props.report)}>
                 <HeaderWithCloseButton
                     title={props.translate('welcomeMessagePage.welcomeMessage')}
@@ -64,7 +74,26 @@ function ReportWelcomeMessagePage(props) {
                     <Text style={[styles.ph5, styles.mb5]}>
                         {props.translate('welcomeMessagePage.explainerText')}
                     </Text>
+                    <View style={[styles.ph5, styles.mb6]}>
+                        <TextInput
+                            label={props.translate('welcomeMessagePage.welcomeMessage')}
+                            multiline
+                            numberOfLines={4}
+                            ref={welcomeMessageInputRef}
+                            value={welcomeMessage}
+                            onChangeText={handleWelcomeMessageChange}
+                            autoCapitalize="none"
+                        />
+                    </View>
                 </ScrollView>
+                <FixedFooter style={[styles.flexGrow0]}>
+                    <Button
+                        success
+                        text={props.translate('common.save')}
+                        onPress={submitForm}
+                        pressOnEnter
+                    />
+                </FixedFooter>
             </FullPageNotFoundView>
         </ScreenWrapper>
     );
