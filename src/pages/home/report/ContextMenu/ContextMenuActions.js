9a815ca85c485400c88c99a5a8bbcb19fd5c63d0
Brandon Stites <bstites+1@expensify.com>
2023-05-11 16:24:34 -0600

    Add beta check to threads

--- src/pages/home/report/ContextMenu/ContextMenuActions.js
+++ src/pages/home/report/ContextMenu/ContextMenuActions.js
@@ -110,7 +110,7 @@ export default [
         icon: Expensicons.ChatBubble,
         successTextTranslateKey: '',
         successIcon: null,
-        shouldShow: (type, reportAction) => Environment.isDevelopment() && type === CONTEXT_MENU_TYPES.REPORT_ACTION && reportAction.actionName === CONST.REPORT.ACTIONS.TYPE.ADDCOMMENT,
+        shouldShow: (type, reportAction, betas) => Permissions.canUseThreads(betas) && type === CONTEXT_MENU_TYPES.REPORT_ACTION && reportAction.actionName === CONST.REPORT.ACTIONS.TYPE.ADDCOMMENT,
         onPress: (closePopover, {reportAction, reportID}) => {
             Report.navigateToAndOpenChildReport(lodashGet(reportAction, 'childReportID', '0'), reportAction, reportID);
             if (closePopover) {
