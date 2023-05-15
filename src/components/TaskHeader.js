bc72ce3911e9f9f7691fd0ad6d20aa4a804dcb77
Jack Nam <jack@expensify.com>
2023-05-15 09:02:42 -0700

    clean up task header

--- src/components/TaskHeader.js
+++ src/components/TaskHeader.js
@@ -1,12 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {View, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 import lodashGet from 'lodash/get';
-import {withOnyx} from 'react-native-onyx';
-import HeaderWithCloseButton from './HeaderWithCloseButton';
 import reportPropTypes from '../pages/reportPropTypes';
 import withLocalize, {withLocalizePropTypes} from './withLocalize';
-import ONYXKEYS from '../ONYXKEYS';
 import * as ReportUtils from '../libs/ReportUtils';
 import * as Expensicons from './Icon/Expensicons';
 import Text from './Text';
@@ -20,18 +17,13 @@ import compose from '../libs/compose';
 import Navigation from '../libs/Navigation/Navigation';
 import ROUTES from '../ROUTES';
 import Icon from './Icon';
-import * as CurrencyUtils from '../libs/CurrencyUtils';
 import MenuItemWithTopDescription from './MenuItemWithTopDescription';
 import Button from './Button';
 import * as TaskUtils from '../libs/actions/Task';
-import TaskSelectorLink from './TaskSelectorLink';
 
 const propTypes = {
     /** The report currently being looked at */
     report: reportPropTypes.isRequired,
-
-    /** The parent report of the currently looked at report */
-    parentReport: reportPropTypes,
 
     /** Personal details so we can get the ones for the report participants */
     personalDetails: PropTypes.objectOf(participantPropTypes).isRequired,
@@ -39,28 +31,15 @@ const propTypes = {
     ...withLocalizePropTypes,
 };
 
-const defaultProps = {
-    parentReport: {},
-};
-
 function TaskHeader(props) {
-    const [assignee, setAssignee] = useState({});
     const title = ReportUtils.getReportName(props.report);
     const assigneeName = ReportUtils.getDisplayNameForParticipant(props.report.managerEmail);
     const assigneeAvatar = ReportUtils.getAvatar(lodashGet(props.personalDetails, [props.report.managerEmail, 'avatar']), props.report.managerEmail);
     const isCompleted = props.report.stateNum === CONST.REPORT.STATE_NUM.SUBMITTED && props.report.statusNum === CONST.REPORT.STATUS.APPROVED;
     const parentReportID = props.report.parentReportID;
-    console.log('report prop', props.report);
 
     useEffect(() => {
-        TaskUtils.clearOutTaskInfo();
         TaskUtils.setTaskReport(props.report);
-        if (!props.report.assignee) {
-            return;
-        }
-        const assigneeDetails = lodashGet(props.personalDetails, props.report.assignee);
-        const displayDetails = TaskUtils.getAssignee(assigneeDetails);
-        setAssignee(displayDetails);
     }, [props]);
 
     return (
@@ -131,14 +110,5 @@ function TaskHeader(props) {
 
 TaskHeader.propTypes = propTypes;
 TaskHeader.displayName = 'TaskHeader';
-TaskHeader.defaultProps = defaultProps;
 
-export default compose(
-    withWindowDimensions,
-    withLocalize,
-    withOnyx({
-        parentReport: {
-            key: ({report}) => `${ONYXKEYS.COLLECTION.REPORT}${report.parentReportID}`,
-        },
-    }),
-)(TaskHeader);
+export default compose(withWindowDimensions, withLocalize)(TaskHeader);
