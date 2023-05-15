0af44b3efad7f4b1035a723f30dacb24aea0adae
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2023-05-15 19:23:39 +0530

    lastMessageTextFromReport as alternateText

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -451,7 +451,7 @@ function createOption(logins, personalDetails, report, reportActions = {}, {show
         if (result.isChatRoom || result.isPolicyExpenseChat) {
             result.alternateText = showChatPreviewLine && !forcePolicyNamePreview && lastMessageText ? lastMessageText : subtitle;
         } else if (result.isMoneyRequestReport) {
-            result.alternateText = Localize.translate(preferredLocale, 'footer.expenseReports');
+            result.alternateText = lastMessageTextFromReport || Localize.translate(preferredLocale, 'report.noActivityYet');
         } else {
             result.alternateText = showChatPreviewLine && lastMessageText ? lastMessageText : LocalePhoneNumber.formatPhoneNumber(personalDetail.login);
         }
