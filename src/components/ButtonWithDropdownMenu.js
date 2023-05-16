53cecb7f0f3b99f92a6797d851a4f65dda3a9a33
Ionatan Wiznia <ionatan@expensify.com>
2023-05-16 14:50:22 -0300

    Fix import

--- src/components/ButtonWithDropdownMenu.js
+++ src/components/ButtonWithDropdownMenu.js
@@ -1,6 +1,6 @@
 import React, {useState, useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import View from 'react-native';
+import {View} from 'react-native';
 import _ from 'underscore';
 import useWindowDimensions from '../hooks/useWindowDimensions';
 import styles from '../styles/styles';
