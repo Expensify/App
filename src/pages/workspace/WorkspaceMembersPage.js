9516626d7f3cadbb46ecb8ffa2734423e565d58a
tienifr <christianwen18@gmail.com>
2023-05-17 16:17:44 +0700

    lift logic up

--- src/pages/workspace/WorkspaceMembersPage.js
+++ src/pages/workspace/WorkspaceMembersPage.js
@@ -388,11 +388,10 @@ class WorkspaceMembersPage extends React.Component {
         data = this.getMemberOptions(data, this.state.searchValue.trim().toLowerCase());
 
         // If this policy is owned by Expensify then show all support (expensify.com or team.expensify.com) emails
-        if (policyOwner && currentUserLogin && !PolicyUtils.isExpensifyTeam(policyOwner)) {
-            // We don't want to show guides as policy members unless the user is not a guide. Some customers get confused when they
-            // see random people added to their policy, but guides having access to the policies help set them up.
-            const isCurrentUserExpensifyTeam = PolicyUtils.isExpensifyTeam(currentUserLogin);
-            data = _.reject(data, (member) => !isCurrentUserExpensifyTeam && PolicyUtils.isExpensifyTeam(member.login));
+        // We don't want to show guides as policy members unless the user is not a guide. Some customers get confused when they
+        // see random people added to their policy, but guides having access to the policies help set them up.
+        if (policyOwner && currentUserLogin && !PolicyUtils.isExpensifyTeam(policyOwner) && !PolicyUtils.isExpensifyTeam(currentUserLogin)) {
+            data = _.reject(data, (member) => PolicyUtils.isExpensifyTeam(member.login));
         }
 
         _.each(data, (member) => {
