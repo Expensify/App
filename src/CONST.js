ff6e3e8373133fae612f0804f5ce06897f12619d
Sibtain Ali <allroundexperts@gmail.com>
2023-05-19 03:16:21 +0500

    feat: fix lint errors

--- src/CONST.js
+++ src/CONST.js
@@ -1021,8 +1021,8 @@ const CONST = {
         HAS_AT_MOST_TWO_AT_SIGNS: /^@[^@]*@?[^@]*$/,
 
         SPECIAL_CHAR_OR_EMOJI:
-        // eslint-disable-next-line no-misleading-character-class
-        /[\n\s/?"{}[\]()&^%$#<>!*\p{Extended_Pictographic}\u200d\u{1f1e6}-\u{1f1ff}\u{1f3fb}-\u{1f3ff}\u{e0020}-\u{e007f}\u20E3\uFE0F]|[#*0-9]\uFE0F?\u20E3/gu,
+            // eslint-disable-next-line no-misleading-character-class
+            /[\n\s/?"{}[\]()&^%$#<>!*\p{Extended_Pictographic}\u200d\u{1f1e6}-\u{1f1ff}\u{1f3fb}-\u{1f3ff}\u{e0020}-\u{e007f}\u20E3\uFE0F]|[#*0-9]\uFE0F?\u20E3/gu,
 
         // Define the regular expression pattern to match a string starting with a colon and ending with a space or newline character
         EMOJI_REPLACER: /^:[^\n\r]+?(?=$|\s)/,
