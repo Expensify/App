61b69df46d336d92cfdbd7ed1521431735256f1d
Christoph Pader <christoph@margelo.io>
2023-05-19 13:51:19 +0200

    add prettier as in main

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,40 @@
 module.exports = {
-    extends: ['expensify', 'plugin:storybook/recommended', 'plugin:react-hooks/recommended'],
-    plugins: ['react-hooks'],
-    parser: 'babel-eslint',
-    ignorePatterns: ['!.*', 'src/vendor', '.github/actions/**/index.js', 'desktop/dist/*.js', 'dist/*.js', 'node_modules/.bin/**', '.git/**'],
-    env: {
-        jest: true,
+  extends: [
+    'expensify',
+    'plugin:storybook/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['react-hooks'],
+  parser: 'babel-eslint',
+  ignorePatterns: [
+    '!.*',
+    'src/vendor',
+    '.github/actions/**/index.js',
+    'desktop/dist/*.js',
+    'dist/*.js',
+    'node_modules/.bin/**',
+    '.git/**',
+  ],
+  env: {
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.website.js',
+          '.desktop.js',
+          '.native.js',
+          '.ios.js',
+          '.android.js',
+          '.config.js',
+        ],
+      },
     },
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: [
-                    '.js',
-                    '.website.js',
-                    '.desktop.js',
-                    '.native.js',
-                    '.ios.js',
-                    '.android.js',
-                    '.config.js',
-                ],
-            },
-        },
-    },
-    globals: {
-        __DEV__: 'readonly',
-    },
+  },
+  globals: {
+    __DEV__: 'readonly',
+  },
 };
