54c7a121778a4cfc3e1b5adfef1a4c09dd930ba4
Jasper Huang <jasper@expensify.com>
2023-05-22 06:26:30 +0800

    simplify logic with variable

--- src/styles/getTooltipStyles.js
+++ src/styles/getTooltipStyles.js
@@ -67,9 +67,10 @@ function isOverlappingAtTop(xOffset, yOffset, tooltip) {
     }
 
     const element = document.elementFromPoint(xOffset, yOffset);
+    const tooltipRef = tooltip && tooltip.current || tooltip;
 
     // Ensure it's not the already rendered element of this very tooltip, so the tooltip doesn't try to "avoid" itself
-    if (!element || tooltip && ((tooltip.current && tooltip.current.contains(element)) || tooltip.contains(element))) {
+    if (!element || (tooltipRef && tooltipRef.contains(element))) {
         return false;
     }
 
