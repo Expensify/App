bd80ad9d531d48a0aa29720cc3280cac8fe839b3
David Bondy <dbondy@expensify.com>
2023-05-22 15:13:35 -0600

    remove unused method

--- src/libs/actions/CloseAccount.js
+++ src/libs/actions/CloseAccount.js
@@ -3,13 +3,6 @@ import ONYXKEYS from '../../ONYXKEYS';
 import CONST from '../../CONST';
 
 /**
- * Clear CloseAccount error message to hide modal
- */
-function clearError() {
-    Onyx.merge(ONYXKEYS.FORMS.CLOSE_ACCOUNT_FORM, {errors: null});
-}
-
-/**
  * Set default Onyx data
  */
 function setDefaultData() {
@@ -17,7 +10,5 @@ function setDefaultData() {
 }
 
 export {
-    // eslint-disable-next-line import/prefer-default-export
-    clearError,
     setDefaultData,
 };
