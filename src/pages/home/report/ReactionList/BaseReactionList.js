cab3b1a7dabf49c0bc3ccb0f63b270ae075b10e9
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2023-05-23 13:33:01 +0530

    close popup on opening the user detail

--- src/pages/home/report/ReactionList/BaseReactionList.js
+++ src/pages/home/report/ReactionList/BaseReactionList.js
@@ -34,40 +34,6 @@ const defaultProps = {
 };
 
 /**
- * Given an emoji item object, render a component based on its type.
- * Items with the code "SPACER" return nothing and are used to fill rows up to 8
- * so that the sticky headers function properly
- *
- * @param {Object} params
- * @param {Object} params.item
- * @return {React.Component}
- */
-const renderItem = ({item}) => (
-    <OptionRow
-        item={item}
-        boldStyle
-        style={{maxWidth: variables.mobileResponsiveWidthBreakpoint}}
-        hoverStyle={styles.hoveredComponentBG}
-        onSelectRow={() => {
-            Navigation.navigate(ROUTES.getDetailsRoute(item.login));
-        }}
-        option={{
-            text: Str.removeSMSDomain(item.displayName),
-            alternateText: Str.removeSMSDomain(item.login),
-            participantsList: [item],
-            icons: [
-                {
-                    source: ReportUtils.getAvatar(item.avatar, item.login),
-                    name: item.login,
-                    type: CONST.ICON_TYPE_AVATAR,
-                },
-            ],
-            keyForList: item.login,
-        }}
-    />
-);
-
-/**
  * Create a unique key for each action in the FlatList.
  * @param {Object} item
  * @param {Number} index
@@ -95,6 +61,42 @@ const BaseReactionList = (props) => {
     if (!props.isVisible) {
         return null;
     }
+
+    /**
+     * Given an emoji item object, render a component based on its type.
+     * Items with the code "SPACER" return nothing and are used to fill rows up to 8
+     * so that the sticky headers function properly
+     *
+     * @param {Object} params
+     * @param {Object} params.item
+     * @return {React.Component}
+     */
+    const renderItem = ({item}) => (
+        <OptionRow
+            item={item}
+            boldStyle
+            style={{maxWidth: variables.mobileResponsiveWidthBreakpoint}}
+            hoverStyle={styles.hoveredComponentBG}
+            onSelectRow={() => {
+                props.onClose();
+                Navigation.navigate(ROUTES.getDetailsRoute(item.login));
+            }}
+            option={{
+                text: Str.removeSMSDomain(item.displayName),
+                alternateText: Str.removeSMSDomain(item.login),
+                participantsList: [item],
+                icons: [
+                    {
+                        source: ReportUtils.getAvatar(item.avatar, item.login),
+                        name: item.login,
+                        type: CONST.ICON_TYPE_AVATAR,
+                    },
+                ],
+                keyForList: item.login,
+            }}
+        />
+    );
+
     return (
         <>
             <HeaderReactionList
