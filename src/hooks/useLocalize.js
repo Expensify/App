eacbc0d782fb73869aeec5af6ba9f080030cca8c
rory <rory@expensify.com>
2023-05-23 14:51:12 -0700

    Fix useLocalize

--- src/hooks/useLocalize.js
+++ src/hooks/useLocalize.js
@@ -2,5 +2,5 @@ import {useContext} from 'react';
 import {LocaleContext} from '../components/withLocalize';
 
 export default function useLocalize() {
-    useContext(LocaleContext);
+    return useContext(LocaleContext);
 }
