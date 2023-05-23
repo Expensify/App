d1cdb04f01a625e9a39295512e96e9a833ce5204
azimgd <me@azimgd.com>
2023-05-23 21:12:39 +0500

    refactor to functional component

--- src/components/AttachmentCarousel/CarouselActions/index.native.js
+++ src/components/AttachmentCarousel/CarouselActions/index.native.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import KeyboardShortcut from '../../../libs/KeyboardShortcut';
 import CONST from '../../../CONST';
@@ -8,42 +8,35 @@ const propTypes = {
     onCycleThroughAttachments: PropTypes.func.isRequired,
 };
 
-class Carousel extends React.Component {
-    componentDidMount() {
+const Carousel = (props) => {
+    useEffect(() => {
         const shortcutLeftConfig = CONST.KEYBOARD_SHORTCUTS.ARROW_LEFT;
-        this.unsubscribeLeftKey = KeyboardShortcut.subscribe(
+        const unsubscribeLeftKey = KeyboardShortcut.subscribe(
             shortcutLeftConfig.shortcutKey,
             () => {
-                this.props.onCycleThroughAttachments(-1);
+                props.onCycleThroughAttachments(-1);
             },
             shortcutLeftConfig.descriptionKey,
             shortcutLeftConfig.modifiers,
         );
 
         const shortcutRightConfig = CONST.KEYBOARD_SHORTCUTS.ARROW_RIGHT;
-        this.unsubscribeLeftKey = KeyboardShortcut.subscribe(
+        const unsubscribeRightKey = KeyboardShortcut.subscribe(
             shortcutRightConfig.shortcutKey,
             () => {
-                this.props.onCycleThroughAttachments(1);
+                props.onCycleThroughAttachments(1);
             },
             shortcutRightConfig.descriptionKey,
             shortcutRightConfig.modifiers,
         );
-    }
 
-    componentWillUnmount() {
-        if (this.unsubscribeLeftKey) {
-            this.unsubscribeLeftKey();
-        }
-        if (this.unsubscribeLeftKey) {
-            this.unsubscribeLeftKey();
-        }
-    }
+        return () => {
+            unsubscribeLeftKey();
+            unsubscribeRightKey();
+        };
+    }, []);
 
-    render() {
-        // This component is only used to listen for keyboard events
-        return null;
-    }
+    return null;
 }
 
 Carousel.propTypes = propTypes;
