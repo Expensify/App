9e0cb2f679f87898dc9e751e1ad77c539e80f57b
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2023-05-24 20:37:47 +0530

    Hide mention suggestions while opening file picker

--- src/pages/home/report/ReportActionCompose.js
+++ src/pages/home/report/ReportActionCompose.js
@@ -182,7 +182,7 @@ class ReportActionCompose extends React.Component {
         this.updateNumberOfLines = this.updateNumberOfLines.bind(this);
         this.showPopoverMenu = this.showPopoverMenu.bind(this);
         this.comment = props.comment;
-        this.setShouldBlockEmojiCalcToFalse = this.setShouldBlockEmojiCalcToFalse.bind(this);
+        this.setShouldBlockSuggestionsCalcToFalse = this.setShouldBlockSuggestionsCalcToFalse.bind(this);
 
         // React Native will retain focus on an input for native devices but web/mWeb behave differently so we have some focus management
         // code that will refocus the compose input after a user closes a modal or some other actions, see usage of ReportActionComposeFocusManager
@@ -273,6 +273,10 @@ class ReportActionCompose extends React.Component {
     }
 
     onSelectionChange(e) {
+        if (this.state.shouldBlockSuggestionsCalc) {
+            this.setShouldBlockSuggestionsCalcToFalse();
+            return;
+        }
         LayoutAnimation.configureNext(LayoutAnimation.create(50, LayoutAnimation.Types.easeInEaseOut, LayoutAnimation.Properties.opacity));
         this.setState({selection: e.nativeEvent.selection});
         if (!this.state.value || e.nativeEvent.selection.end < 1) {
@@ -416,9 +420,9 @@ class ReportActionCompose extends React.Component {
     }
 
     // eslint-disable-next-line rulesdir/prefer-early-return
-    setShouldBlockEmojiCalcToFalse() {
-        if (this.state && this.state.shouldBlockEmojiCalc) {
-            this.setState({shouldBlockEmojiCalc: false});
+    setShouldBlockSuggestionsCalcToFalse() {
+        if (this.state && this.state.shouldBlockSuggestionsCalc) {
+            this.setState({shouldBlockSuggestionsCalc: false});
         }
     }
 
@@ -505,10 +509,6 @@ class ReportActionCompose extends React.Component {
      * Calculates and cares about the content of an Emoji Suggester
      */
     calculateEmojiSuggestion() {
-        if (this.state.shouldBlockEmojiCalc) {
-            this.setState({shouldBlockEmojiCalc: false});
-            return;
-        }
         const leftString = this.state.value.substring(0, this.state.selection.end);
         const colonIndex = leftString.lastIndexOf(':');
         const isCurrentlyShowingEmojiSuggestion = this.isEmojiCode(this.state.value, this.state.selection.end);
@@ -936,7 +936,7 @@ class ReportActionCompose extends React.Component {
                             onConfirm={this.addAttachment}
                             onModalShow={() => this.setState({isAttachmentPreviewActive: true})}
                             onModalHide={() => {
-                                this.setShouldBlockEmojiCalcToFalse();
+                                this.setShouldBlockSuggestionsCalcToFalse();
                                 this.setState({isAttachmentPreviewActive: false});
                             }}
                         >
@@ -1020,7 +1020,7 @@ class ReportActionCompose extends React.Component {
                                                                 // Set a flag to block emoji calculation until we're finished using the file picker,
                                                                 // which will stop any flickering as the file picker opens on non-native devices.
                                                                 if (this.willBlurTextInputOnTapOutside) {
-                                                                    this.setState({shouldBlockEmojiCalc: true});
+                                                                    this.setState({shouldBlockSuggestionsCalc: true});
                                                                 }
 
                                                                 openPicker({
@@ -1074,7 +1074,7 @@ class ReportActionCompose extends React.Component {
                                                     this.setIsFocused(false);
                                                     this.resetSuggestions();
                                                 }}
-                                                onClick={this.setShouldBlockEmojiCalcToFalse}
+                                                onClick={this.setShouldBlockSuggestionsCalcToFalse}
                                                 onPasteFile={displayFileInModal}
                                                 shouldClear={this.state.textInputShouldClear}
                                                 onClear={() => this.setTextInputShouldClear(false)}
