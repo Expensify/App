720730c58efba0fee258cb0f7136ddba3e1c5cb2
ahmedGaber93 <a.gaber0093@gmail.com>
2023-05-26 01:03:21 +0200

    update QRCode logo propType

--- src/components/QRCode/index.js
+++ src/components/QRCode/index.js
@@ -10,7 +10,7 @@ const propTypes = {
     url: PropTypes.string.isRequired,
     /**
      * The logo which will be displayed in the middle of the QR code.
-     * Follows ImageProps href from react-native-svg that used by react-native-qrcode-svg.
+     * Follows ImageProps href from react-native-svg that is used by react-native-qrcode-svg.
      */
     logo: PropTypes.oneOfType([PropTypes.shape({uri: PropTypes.string}), PropTypes.number, PropTypes.string]),
     /**
