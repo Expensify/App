24ca23f150c2844bdbb49554fc97d0f46248f05f
dukenv0307 <129500732+dukenv0307@users.noreply.github.com>
2023-05-25 17:36:36 +0700

    Update src/styles/getContainerComposeStyles/index.js
    Co-authored-by: Tim Golen <tgolen@gmail.com>

--- src/styles/getContainerComposeStyles/index.js
+++ src/styles/getContainerComposeStyles/index.js
@@ -1,3 +1,3 @@
 import styles from '../styles';
-// we need to set paddingVertical = 0 on the web to avoid App displays normal pointer on some parts of compose box when not in focus
+// We need to set paddingVertical = 0 on web to avoid displaying a normal pointer on some parts of compose box when not in focus
 export default () => [styles.textInputComposeSpacing, styles.textInputComposeBorder, {paddingVertical: 0}];
