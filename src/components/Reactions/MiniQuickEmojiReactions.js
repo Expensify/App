93012d78a7c087efe5619763873b64485b75e3a7
Tim Golen <tgolen@expensify.com>
2023-05-25 11:19:42 +0100

    Move ESLint disable to a better line

--- src/components/Reactions/MiniQuickEmojiReactions.js
+++ src/components/Reactions/MiniQuickEmojiReactions.js
@@ -92,11 +92,11 @@ const MiniQuickEmojiReactions = (props) => {
 MiniQuickEmojiReactions.displayName = 'MiniQuickEmojiReactions';
 MiniQuickEmojiReactions.propTypes = propTypes;
 MiniQuickEmojiReactions.defaultProps = defaultProps;
-// ESLint throws an error because it can't see that emojiReactions is defined in props. It is defined in props, but
-// because of a couple spread operators, I think that's why ESLint struggles to see it
-// eslint-disable-next-line rulesdir/onyx-props-must-have-default
 export default compose(
     withLocalize,
+    // ESLint throws an error because it can't see that emojiReactions is defined in props. It is defined in props, but
+    // because of a couple spread operators, I think that's why ESLint struggles to see it
+    // eslint-disable-next-line rulesdir/onyx-props-must-have-default
     withOnyx({
         preferredSkinTone: {
             key: ONYXKEYS.PREFERRED_EMOJI_SKIN_TONE,
