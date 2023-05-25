20a9bfda9c211e914de251d4df676375f93e7cf3
Vinh Hoang <hoangzinhvn@gmail.com>
2023-05-25 15:55:32 +0700

    Update method JSDoc
    Co-authored-by: Tim Golen <tgolen@gmail.com>

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -757,8 +757,8 @@ function getAvatar(avatarURL, login) {
  * Provided an avatar URL, if avatar is a default avatar, return NewDot default avatar URL.
  * Otherwise, return the URL pointing to a user-uploaded avatar.
  *
- * @param {String} [avatarURL] - the avatar source from user's personalDetails
- * @param {String} [login] - the email of the user
+ * @param {String} avatarURL - the avatar source from user's personalDetails
+ * @param {String} login - the email of the user
  * @returns {String|Function}
  */
 function getAvatarUrl(avatarURL, login) {
