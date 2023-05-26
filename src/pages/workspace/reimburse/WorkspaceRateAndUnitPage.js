bddae4a41774e90011c2b7eeacbd29cb582dd4ff
Pierre Michel <pmiche04@gmail.com>
2023-05-26 16:43:05 -0600

    Fix: user could enter a number and a point and save successfully
    Signed-off-by: Pierre Michel <pmiche04@gmail.com>

--- src/pages/workspace/reimburse/WorkspaceRateAndUnitPage.js
+++ src/pages/workspace/reimburse/WorkspaceRateAndUnitPage.js
@@ -93,7 +93,7 @@ class WorkspaceRateAndUnitPage extends React.Component {
     validate(values) {
         const errors = {};
         const decimalSeparator = this.props.toLocaleDigit('.');
-        const rateValueRegex = RegExp(String.raw`^\d{1,8}([${getPermittedDecimalSeparator(decimalSeparator)}]\d{0,3})?$`, 'i');
+        const rateValueRegex = RegExp(String.raw`^\d{1,8}([${getPermittedDecimalSeparator(decimalSeparator)}]\d{1,3})?$`, 'i');
         if (!rateValueRegex.test(values.rate)) {
             errors.rate = this.props.translate('workspace.reimburse.invalidRateError');
         }
