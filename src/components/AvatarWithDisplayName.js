4fbb346e9e6fa54372da486126c9b9676008bba0
Sibtain Ali <allroundexperts@gmail.com>
2023-05-27 02:36:23 +0500

    feat: update comment for isAnonymous prop

--- src/components/AvatarWithDisplayName.js
+++ src/components/AvatarWithDisplayName.js
@@ -33,7 +33,7 @@ const propTypes = {
     /** Personal details of all the users */
     personalDetails: PropTypes.objectOf(participantPropTypes),
 
-    /** Skip getting the report  */
+    /** Uses the reports display name as title with relying on policies and personalDetails object */
     isAnonymous: PropTypes.bool,
 
     ...windowDimensionsPropTypes,
