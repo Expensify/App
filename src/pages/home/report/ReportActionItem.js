ff847f5bc7148eb5e454f8145cdb4f169feb39ac
Wojciech Stanisz <wojciech.stanisz@swmansion.com>
2023-05-29 12:06:45 +0200

    Use size instead of length

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -285,7 +285,7 @@ function ReportActionItem(props) {
         return (
             <>
                 {children}
-                {!_.isEmpty(props.action.linkMetadata) && props.action.linkMetadata.length > 0 && <LinkPreviewer linkMetadata={props.action.linkMetadata} />}
+                {!_.isEmpty(props.action.linkMetadata) && _.size(props.action.linkMetadata) > 0 && <LinkPreviewer linkMetadata={props.action.linkMetadata} />}
                 {hasReactions && (
                     <View style={props.draftMessage ? styles.chatItemReactionsDraftRight : {}}>
                         <ReportActionItemReactions
