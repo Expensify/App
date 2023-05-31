9abfe1b99cc5bbed7e507f7a1ccaaed6a2083967
Daniel Edwards <cdaniel.edwards@gmail.com>
2023-05-30 21:01:40 -0400

    Fixes langague test and lint issue

--- src/components/SignInButtons/ButtonBase.js
+++ src/components/SignInButtons/ButtonBase.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import {Pressable} from 'react-native';
+import { Pressable } from 'react-native';
+import PropTypes from 'prop-types';
 
 const style = {
     margin: 10,
     padding: 2,
 };
+
+const propTypes = {
+    
+    /** The on press method */
+    onPress: PropTypes.func,
+
+    /** The icon to render */
+    icon: PropTypes.element,
+};
+
+const defaultProps = {
+    onPress: () => { },
+    icon: null,
+};
+
 
 const ButtonBase = ({onPress, icon}) => (
     <Pressable
@@ -16,5 +32,7 @@ const ButtonBase = ({onPress, icon}) => (
 );
 
 ButtonBase.displayName = 'ButtonBase';
+ButtonBase.propTypes = propTypes;
+ButtonBase.defaultProps = defaultProps;
 
 export default ButtonBase;
