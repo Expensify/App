38b0c1017ec6098be57c51a2edd5f9499cb9b514
burczu <b.dybowski@gmail.com>
2023-06-01 14:28:50 +0200

    updated helper methods used in the ReportWelocmeText component

--- src/CONST.js
+++ src/CONST.js
@@ -1174,6 +1174,11 @@ const CONST = {
             this.EMAIL.ADMIN,
         ];
     },
+    get EXPENSIFY_ACCOUNT_IDS() {
+        return [
+            this.ACCOUNT_ID.CONCIERGE,
+        ];
+    },
 
     // Auth limit is 60k for the column but we store edits and other metadata along the html so let's use a lower limit to accommodate for it.
     MAX_COMMENT_LENGTH: 15000,
