1cdc46b64dbbc44572c026687ea8c8d5b607e0ad
Nikki Wines <nikkiwines@expensify.com>
2023-06-02 12:52:31 -0700

    update styling for emaildeliveryfailure view

--- src/pages/signin/EmailDeliveryFailurePage.js
+++ src/pages/signin/EmailDeliveryFailurePage.js
@@ -5,18 +5,15 @@ import {withOnyx} from 'react-native-onyx';
 import PropTypes from 'prop-types';
 import Str from 'expensify-common/lib/str';
 import styles from '../../styles/styles';
-import Button from '../../components/Button';
 import Text from '../../components/Text';
-import * as Session from '../../libs/actions/Session';
+import TextLink from '../../components/TextLink';
 import ONYXKEYS from '../../ONYXKEYS';
 import withLocalize, {withLocalizePropTypes} from '../../components/withLocalize';
 import compose from '../../libs/compose';
 import redirectToSignIn from '../../libs/actions/SignInRedirect';
-import Avatar from '../../components/Avatar';
-import * as ReportUtils from '../../libs/ReportUtils';
 import networkPropTypes from '../../components/networkPropTypes';
 import {withNetwork} from '../../components/OnyxProvider';
-import DotIndicatorMessage from '../../components/DotIndicatorMessage';
+import CONST from '../../CONST';
 
 const propTypes = {
     /* Onyx Props */
@@ -38,46 +35,47 @@ const defaultProps = {
 };
 
 const EmailDeliveryFailurePage = (props) => {
-    const isSMSLogin = Str.isSMSLogin(props.credentials.login);
-
-    // replacing spaces with "hard spaces" to prevent breaking the number
-    const login = isSMSLogin ? props.formatPhoneNumber(props.credentials.login).replace(/ /g, '\u00A0') : props.credentials.login;
-
-    const loginType = (isSMSLogin ? props.translate('common.phone') : props.translate('common.email')).toLowerCase();
-
+    // Replace spaces with "hard spaces" to prevent breaking the number
+    const login = Str.isSMSLogin(props.credentials.login) ? Str.removeSMSDomain(props.credentials.login) : props.credentials.login;
+    console.log("login: " + login);
     return (
         <>
-            <View style={[styles.mt3, styles.flexRow, styles.alignItemsCenter, styles.justifyContentStart]}>
+            <View style={[styles.mv3, styles.flexRow, styles.justifyContentetween]}>
                 <View style={[styles.flex1]}>
-                    <Text textBreakStrategy="simple">
-                        {props.translate('emailDeliveryFailurePage.ourEmailProvider', props.credentials.login)}
+                    <Text>
+                        {props.translate('emailDeliveryFailurePage.ourEmailProvider', {login})}
                     </Text>
-                    <Text
-                        textBreakStrategy="simple"
-                        style={[styles.textStrong]}
-                    >
-                        {props.translate('emailDeliveryFailurePage.confirmThat', props.credentials.login)}
-                    </Text>
-                    <Text textBreakStrategy="simple">
+                    <Text style={[styles.mt5]}>
+                        <Text style={[styles.textStrong]}>
+                            {props.translate('emailDeliveryFailurePage.confirmThat', {login})}
+                        </Text>
                         {props.translate('emailDeliveryFailurePage.emailAliases')}
                     </Text>
-                    <Text
-                        textBreakStrategy="simple"
-                        style={[styles.textStrong]}
-                    >
-                        {props.translate('emailDeliveryFailurePage.whitelistExpensify', props.credentials.login)}
-                    </Text>
-                    <Text textBreakStrategy="simple">
+                    <Text style={[styles.mt5]}>
+                        <Text style={[styles.textStrong]}>
+                            {props.translate('emailDeliveryFailurePage.whitelistExpensify')}
+                        </Text>
                         {props.translate('emailDeliveryFailurePage.youCanFindDirections')}
-                        <a href="">{props.translate('common.here')}</a>
+                        <TextLink href="https://community.expensify.com/discussion/5651/deep-dive-best-practices-when-youre-running-into-trouble-receiving-emails-from-expensify/p1?new=1"
+                            style={[styles.link]}
+                        >
+                            {props.translate('common.here')}
+                        </TextLink>
                         {props.translate('emailDeliveryFailurePage.helpConfigure')}
                     </Text>
-                    <Text textBreakStrategy="simple">
+                    <Text style={styles.mt5}>
                         {props.translate('emailDeliveryFailurePage.onceTheAbove')}
+                        <TextLink
+                            href={`mailto:${CONST.EMAIL.CONCIERGE}`}
+                            style={[styles.link]}
+                        >
+                            {CONST.EMAIL.CONCIERGE}
+                        </TextLink>
+                        {props.translate('emailDeliveryFailurePage.toUnblock')}
                     </Text>
                 </View>
             </View>
-            <View style={[styles.mb4, styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
+            <View style={[styles.mv4, styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
                 <TouchableOpacity onPress={() => redirectToSignIn()}>
                     <Text style={[styles.link]}>{props.translate('common.back')}</Text>
                 </TouchableOpacity>
