2ded5358ebf41a701d00bdf74cb53d2d5c1a249f
Robert Kozik <robert.kozik@swmansion.com>
2023-06-02 20:53:15 +0200

    migrate UnlinkLoginForm to PressableWithFeedback

--- src/pages/signin/UnlinkLoginForm.js
+++ src/pages/signin/UnlinkLoginForm.js
@@ -78,7 +78,7 @@ const UnlinkLoginForm = (props) => {
                 />
             )}
             <View style={[styles.mb4, styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
-                <PressableWithFeedback shouldUseAutoHitSlop={false} onPress={() => redirectToSignIn()}>
+                <PressableWithFeedback accessibilityLabel={props.translate('common.back')} onPress={() => redirectToSignIn()}>
                     <Text style={[styles.link]}>{props.translate('common.back')}</Text>
                 </PressableWithFeedback>
                 <Button
