8e667db40c8e20dff4142927f04a081d0434a0be
Brandon Stites <bstites+1@expensify.com>
2023-06-02 12:32:50 -0600

    Add new key for reactions

--- src/ONYXKEYS.js
+++ src/ONYXKEYS.js
@@ -118,6 +118,7 @@ export default {
         REPORT: 'report_',
         REPORT_ACTIONS: 'reportActions_',
         REPORT_ACTIONS_DRAFTS: 'reportActionsDrafts_',
+        REPORT_ACTIONS_REACTIONS: 'reportActionsReactions_',
         REPORT_DRAFT_COMMENT: 'reportDraftComment_',
         REPORT_DRAFT_COMMENT_NUMBER_OF_LINES: 'reportDraftCommentNumberOfLines_',
         REPORT_IS_COMPOSER_FULL_SIZE: 'reportIsComposerFullSize_',
