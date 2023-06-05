f5a0029df1082bd048e9a7059caaecfddcac92bb
rory <rory@expensify.com>
2023-06-05 16:03:30 -0700

    Revert PERSONAL_DETAILS_LIST Onyx changes

--- src/libs/PersonalDetailsUtils.js
+++ src/libs/PersonalDetailsUtils.js
@@ -5,14 +5,14 @@ import * as Localize from './Localize';
 
 let personalDetails = [];
 Onyx.connect({
-    key: ONYXKEYS.PERSONAL_DETAILS_LIST,
+    key: ONYXKEYS.PERSONAL_DETAILS,
     callback: (val) => (personalDetails = _.values(val)),
 });
 
 /**
  * Given a list of account IDs (as string) it will return an array of personal details objects.
  * @param {Array<string>} accountIDs  - Array of accountIDs
- * @param {String} currentUserAccountID
+ * @param {Number} currentUserAccountID
  * @param {Boolean} shouldChangeUserDisplayName - It will replace the current user's personal detail object's displayName with 'You'.
  * @returns {Array} - Array of personal detail objects
  */
@@ -21,7 +21,7 @@ function getPersonalDetailsByIDs(accountIDs, currentUserAccountID, shouldChangeU
     _.each(
         _.filter(personalDetails, (detail) => accountIDs.includes(detail.accountID)),
         (detail) => {
-            if (shouldChangeUserDisplayName && currentUserAccountID === detail.accountID) {
+            if (shouldChangeUserDisplayName && currentUserAccountID.toString() === detail.accountID) {
                 result.push({
                     ...detail,
                     displayName: Localize.translateLocal('common.you'),
