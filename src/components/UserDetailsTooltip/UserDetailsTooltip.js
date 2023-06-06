9840b18ed39c6a0008653fc0e68438f747b5d4aa
Luiz Ozorio <luiz@cloudpresser.com>
2023-06-06 14:30:38 -0400

    fix(userDetailsTooltip): useCallback

--- src/components/UserDetailsTooltip/UserDetailsTooltip.js
+++ src/components/UserDetailsTooltip/UserDetailsTooltip.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { View, Text } from 'react-native';
 import Avatar from '../Avatar';
 import Tooltip from '../Tooltip';
@@ -6,19 +6,22 @@ import { propTypes, defaultProps } from './userDetailsTooltipPropTypes';
 import styles from '../../styles/styles';
 
 function UserDetailsTooltip(props){
+    
+    const renderTooltipContent = useCallback(() => {
+        return (
+            <View style={styles.alignItemsCenter}>
+                <Avatar
+                    containerStyles={[styles.actionAvatar]}
+                    source={props.avatarSource}
+                />
+                {Boolean(props.name.trim()) ? <Text>{props.name}</Text> : ''}
+                {Boolean(props.handle.trim()) ? <Text>@{props.handle}</Text> : ''}
+            </View>
+        );
+      }, [props.avatarSource, props.name, props.handle]);
+
     return (
-        <Tooltip
-            renderTooltipContent={()=> (
-                    <View style={styles.alignItemsCenter}>
-                        <Avatar
-                            containerStyles={[styles.actionAvatar]}
-                            source={props.avatarSource}
-                        />
-                        {Boolean(props.name.trim()) ? <Text>{props.name}</Text> : ''}
-                        {Boolean(props.handle.trim()) ? <Text>@{props.handle}</Text> : ''}
-                    </View>
-                )}
-        >{props.children}</Tooltip>
+        <Tooltip renderTooltipContent={renderTooltipContent}>{props.children}</Tooltip>
     )
 }
 
