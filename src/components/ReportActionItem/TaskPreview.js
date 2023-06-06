75899891c195bf8acdb960c7f2e444aed3913ecf
Jack Nam <jack@expensify.com>
2023-06-06 10:24:41 -0400

    fix preview not updating

--- src/components/ReportActionItem/TaskPreview.js
+++ src/components/ReportActionItem/TaskPreview.js
@@ -53,9 +53,7 @@ const TaskPreview = (props) => {
     // The reportAction might not contain details regarding the taskReport
     // Only the direct parent reportAction will contain details about the taskReport
     // Other linked reportActions will only contain the taskReportID and we will grab the details from there
-    const isTaskCompleted =
-        (props.taskReport.stateNum === CONST.REPORT.STATE_NUM.SUBMITTED && props.taskReport.statusNum === CONST.REPORT.STATUS.APPROVED) ||
-        (props.action.childStateNum === CONST.REPORT.STATE_NUM.SUBMITTED && props.action.childStatusNum === CONST.REPORT.STATUS.APPROVED);
+    const isTaskCompleted = props.taskReport ? (props.taskReport.stateNum === CONST.REPORT.STATE_NUM.SUBMITTED && props.taskReport.statusNum === CONST.REPORT.STATUS.APPROVED) : (props.action.childStateNum === CONST.REPORT.STATE_NUM.SUBMITTED && props.action.childStatusNum === CONST.REPORT.STATUS.APPROVED);
     const taskTitle = props.taskReport.reportName || props.action.childReportName;
     const taskAssignee = props.taskReport.managerEmail || props.action.childManagerEmail;
     const htmlForTaskPreview = taskAssignee ? `<comment><mention-user>@${taskAssignee}</mention-user> ${taskTitle}</comment>` : `<comment>${taskTitle}</comment>`;
