cfea01e2d1e3ed4c8ed89f042676edab5783cd2c
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-06-06 11:41:49 +0800

    Revert "simplify money request participants"
    This reverts commit c69d294c252f3eece2deaf2f54d7bd7866159068.

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -263,8 +263,7 @@ function buildOnyxDataForMoneyRequest(
  */
 function requestMoney(report, amount, currency, payeeEmail, participant, comment) {
     const payerEmail = OptionsListUtils.addSMSDomainIfPhoneNumber(participant.login);
-    // reportID exist if we request money in a policyExpenseChat
-    const isPolicyExpenseChat = Boolean(participant.reportID);
+    const isPolicyExpenseChat = participant.isPolicyExpenseChat || participant.isOwnPolicyExpenseChat;
 
     // STEP 1: Get existing chat report OR build a new optimistic one
     let isNewChatReport = false;
