f3398df3095d7bd615533742f9ebea53dbba9df3
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-06-06 16:52:08 +0200

    fix: FreezeWrapper

--- src/libs/Navigation/FreezeWrapper.js
+++ src/libs/Navigation/FreezeWrapper.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
+import lodashFindIndex from 'lodash/findIndex';
 import PropTypes from 'prop-types';
-import {useIsFocused, useNavigation} from '@react-navigation/native';
+import {useIsFocused, useNavigation, useRoute} from '@react-navigation/native';
 import {Freeze} from 'react-freeze';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../../components/withWindowDimensions';
 
@@ -16,30 +17,30 @@ const defaultProps = {
 function FreezeWrapper(props) {
     const [isScreenBlurred, setIsScreenBlurred] = useState(false);
     // we need to know the screen index to determine if the screen can be frozen
-    const [screenIndex, setScreenIndex] = useState(null);
+    const screenIndexRef = useRef(null);
     const isFocused = useIsFocused();
     const navigation = useNavigation();
+    const route = useRoute();
 
     useEffect(() => {
-        if (screenIndex !== null) {
-            return;
-        }
-        setScreenIndex(navigation.getState().index);
-    }, [navigation, screenIndex]);
+        const index = lodashFindIndex(navigation.getState().routes, (o) => o.key === route.key);
+        screenIndexRef.current = index;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     useEffect(() => {
         const unsubscribe = navigation.addListener('state', () => {
             // if the screen is more than 1 screen away from the current screen, freeze it,
             // we don't want to freeze the screen if it's the previous screen because the freeze placeholder
             // would be visible at the beginning of the back animation then
-            if (navigation.getState().index - screenIndex > 1 && screenIndex !== null) {
+            if (navigation.getState().index - screenIndexRef.current > 1) {
                 setIsScreenBlurred(true);
             } else {
                 setIsScreenBlurred(false);
             }
         });
         return () => unsubscribe();
-    }, [isFocused, isScreenBlurred, navigation, screenIndex]);
+    }, [isFocused, isScreenBlurred, navigation]);
 
     return <Freeze freeze={!isFocused && isScreenBlurred && !props.keepVisible}>{props.children}</Freeze>;
 }
