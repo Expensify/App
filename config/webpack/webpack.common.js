0efb8a70edbba37c5e2baec177d81d0bb9bfd93f
Blazej Kustra <blazej.kustra@swmansion.com>
2023-06-06 11:46:23 +0200

    Include ts files in weback config

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -192,7 +192,7 @@ const webpackConfig = ({envFile = '.env', platform = 'web'}) => ({
         // This is also why we have to use .website.js for our own web-specific files...
         // Because desktop also relies on "web-specific" module implementations
         // This also skips packing web only dependencies to desktop and vice versa
-        extensions: ['.web.js', platform === 'web' ? '.website.js' : '.desktop.js', '.js', '.jsx'],
+        extensions: ['.web.js', platform === 'web' ? '.website.js' : '.desktop.js', '.ts', '.tsx', '.js', '.jsx', '.json'],
         fallback: {
             'process/browser': require.resolve('process/browser'),
         },
