809437d1b4263efe0f5d40c760e0a418dafe3c37
Arkadiusz Chrabąszczewski <Arkadiuszchr@gmail.com>
2023-06-06 11:26:04 +0200

    fix: build action

--- .github/actions/javascript/validateReassureOutput/index.js
+++ .github/actions/javascript/validateReassureOutput/index.js
@@ -11,35 +11,26 @@ module.exports =
 const core = __nccwpck_require__(186);
 
 const run = () => {
-    const regressionOutput = JSON.parse(core.getInput('REGRESSION_OUTPUT', {required: true}));
-    const countDeviation = core.getInput('COUNT_DEVIATION', {required: true});
-    const durationDeviation = core.getInput('DURATION_DEVIATION_PERCENTAGE', {required: true});
+    const regressionOutput = JSON.parse(core.getInput('REGRESSION_OUTPUT', { required: true }));
+    const countDeviation = core.getInput('COUNT_DEVIATION', { required: true });
+    const durationDeviation = core.getInput('DURATION_DEVIATION_PERCENTAGE', { required: true });
 
-    console.log('Input values:');
-    console.log('REGRESSION_OUTPUT:', regressionOutput);
-    console.log('COUNT_DEVIATION:', countDeviation);
-    console.log('DURATION_DEVIATION_PERCENTAGE:', durationDeviation);
-
-    if (regressionOutput.countChanged === undefined) {
+    if (regressionOutput.countChanged === undefined || regressionOutput.countChanged.length === 0) {
         console.log('No countChanged data available. Exiting...');
         return true;
-    }
+    } 
+
+    console.log(`Processing ${regressionOutput.countChanged.length} measurements...`);
 
     for (let i = 0; i < regressionOutput.countChanged.length; i++) {
         const measurement = regressionOutput.countChanged[i];
         const baseline = measurement.baseline;
         const current = measurement.current;
 
-        console.log(`Processing measurement ${i + 1}:`);
-        console.log('Measurement:', measurement);
-        console.log('Baseline:', baseline);
-        console.log('Current:', current);
+        console.log(`Processing measurement ${i + 1}: ${measurement.name}`);
 
         const renderCountDiff = current.meanCount - baseline.meanCount;
-        console.log('Render count difference:', renderCountDiff);
-
         if (renderCountDiff >= countDeviation) {
-            console.log(`Render count difference exceeded the allowed deviation of ${countDeviation}. Current difference: ${renderCountDiff}`);
             core.setFailed(`Render count difference exceeded the allowed deviation of ${countDeviation}. Current difference: ${renderCountDiff}`);
             break;
         } else {
@@ -47,10 +38,7 @@ const run = () => {
         }
 
         const increasePercentage = ((current.meanDuration - baseline.meanDuration) / baseline.meanDuration) * 100;
-        console.log('Duration increase percentage:', increasePercentage);
-
         if (increasePercentage > durationDeviation) {
-            console.log(`Duration increase percentage exceeded the allowed deviation of ${durationDeviation}%. Current percentage: ${increasePercentage}%`);
             core.setFailed(`Duration increase percentage exceeded the allowed deviation of ${durationDeviation}%. Current percentage: ${increasePercentage}%`);
             break;
         } else {
