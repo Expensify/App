18810b6948ed0ea98cf9250b5b99dd3c4b6883fb
Michał Skałka <michal.skalka@swmansion.com>
2023-06-06 16:17:50 +0200

    migrated ImageView to PressableWithoutFeedback

--- src/components/ImageView/index.js
+++ src/components/ImageView/index.js
@@ -1,12 +1,13 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
-import {View, Pressable} from 'react-native';
+import {View} from 'react-native';
 import Image from '../Image';
 import styles from '../../styles/styles';
 import * as StyleUtils from '../../styles/StyleUtils';
 import * as DeviceCapabilities from '../../libs/DeviceCapabilities';
 import withWindowDimensions, {windowDimensionsPropTypes} from '../withWindowDimensions';
 import FullscreenLoadingIndicator from '../FullscreenLoadingIndicator';
+import PressableWithoutFeedback from '../Pressable/PressableWithoutFeedback';
 
 const propTypes = {
     /** Whether source url requires authentication */
@@ -266,7 +267,8 @@ class ImageView extends PureComponent {
                 onLayout={this.onContainerLayoutChanged}
                 style={[styles.imageViewContainer, styles.overflowAuto, styles.pRelative]}
             >
-                <Pressable
+                <PressableWithoutFeedback
+                    accessible={false}
                     style={{
                         ...StyleUtils.getZoomSizingStyle(
                             this.state.isZoomed,
@@ -292,7 +294,7 @@ class ImageView extends PureComponent {
                         onLoadStart={this.imageLoadingStart}
                         onLoad={this.imageLoad}
                     />
-                </Pressable>
+                </PressableWithoutFeedback>
 
                 {this.state.isLoading && <FullscreenLoadingIndicator style={[styles.opacity1, styles.bgTransparent]} />}
             </View>
