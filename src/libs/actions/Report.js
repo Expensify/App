9365efb5a0c5b7111c1ca469fe265c54ded9c7ec
Alberto <alberto@expensify.com>
2023-06-07 11:24:02 +0200

    use lodashget

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -668,7 +668,7 @@ function readNewestAction(reportID) {
  */
 function markCommentAsUnread(reportID, reportActionCreated) {
     // If no action created date is provided, use the last action's
-    const actionCreationTime = reportActionCreated || allReports[reportID].lastVisibleActionCreated;
+    const actionCreationTime = reportActionCreated || lodashGet(allReports, [reportID, 'lastVisibleActionCreated'], '0');
 
     // We subtract 1 millisecond so that the lastReadTime is updated to just before a given reportAction's created date
     // For example, if we want to mark a report action with ID 100 and created date '2014-04-01 16:07:02.999' unread, we set the lastReadTime to '2014-04-01 16:07:02.998'
