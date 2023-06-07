736a620f8dcf86d21e18bba08087b976310cd19d
Duc Nguyen <nguyenhoangminhduc@gmail.com>
2023-06-07 22:06:29 +0300

    Run prettier

--- src/components/CopyTextToClipboard.js
+++ src/components/CopyTextToClipboard.js
@@ -35,7 +35,7 @@ const CopyTextToClipboard = (props) => {
             onPress={copyToClipboard}
         />
     );
-}
+};
 
 CopyTextToClipboard.propTypes = propTypes;
 CopyTextToClipboard.defaultProps = defaultProps;
