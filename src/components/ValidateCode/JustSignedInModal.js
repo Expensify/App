859f3bab8cf33fbd90bbe2d54fabd93cebf29241
Cristi Paval <cristi@expensify.com>
2023-06-08 00:00:21 +0300

    Make the new component a function component.

--- src/components/ValidateCode/JustSignedInModal.js
+++ src/components/ValidateCode/JustSignedInModal.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {View} from 'react-native';
 import colors from '../../styles/colors';
@@ -17,38 +17,36 @@ const propTypes = {
     is2FARequired: PropTypes.bool.isRequired,
 };
 
-class JustSignedInModal extends PureComponent {
-    render() {
-        return (
-            <View style={styles.deeplinkWrapperContainer}>
-                <View style={styles.deeplinkWrapperMessage}>
-                    <View style={styles.mb2}>
-                        <Icon
-                            width={variables.modalTopIconWidth}
-                            height={this.props.is2FARequired ? variables.modalTopIconHeight : variables.modalTopBigIconHeight}
-                            src={this.props.is2FARequired ? Illustrations.SafeBlue : Illustrations.Abracadabra}
-                        />
-                    </View>
-                    <Text style={[styles.textHeadline, styles.textXXLarge, styles.textAlignCenter]}>
-                        {this.props.translate(this.props.is2FARequired ? 'validateCodeModal.tfaRequiredTitle' : 'validateCodeModal.successfulSignInTitle')}
-                    </Text>
-                    <View style={[styles.mt2, styles.mb2]}>
-                        <Text style={[styles.fontSizeNormal, styles.textAlignCenter]}>
-                            {this.props.translate(this.props.is2FARequired ? 'validateCodeModal.tfaRequiredDescription' : 'validateCodeModal.successfulSignInDescription')}
-                        </Text>
-                    </View>
-                </View>
-                <View style={styles.deeplinkWrapperFooter}>
+function JustSignedInModal(props) {
+    return (
+        <View style={styles.deeplinkWrapperContainer}>
+            <View style={styles.deeplinkWrapperMessage}>
+                <View style={styles.mb2}>
                     <Icon
-                        width={variables.modalWordmarkWidth}
-                        height={variables.modalWordmarkHeight}
-                        fill={colors.green}
-                        src={Expensicons.ExpensifyWordmark}
+                        width={variables.modalTopIconWidth}
+                        height={props.is2FARequired ? variables.modalTopIconHeight : variables.modalTopBigIconHeight}
+                        src={props.is2FARequired ? Illustrations.SafeBlue : Illustrations.Abracadabra}
                     />
                 </View>
+                <Text style={[styles.textHeadline, styles.textXXLarge, styles.textAlignCenter]}>
+                    {props.translate(props.is2FARequired ? 'validateCodeModal.tfaRequiredTitle' : 'validateCodeModal.successfulSignInTitle')}
+                </Text>
+                <View style={[styles.mt2, styles.mb2]}>
+                    <Text style={[styles.fontSizeNormal, styles.textAlignCenter]}>
+                        {props.translate(props.is2FARequired ? 'validateCodeModal.tfaRequiredDescription' : 'validateCodeModal.successfulSignInDescription')}
+                    </Text>
+                </View>
             </View>
-        );
-    }
+            <View style={styles.deeplinkWrapperFooter}>
+                <Icon
+                    width={variables.modalWordmarkWidth}
+                    height={variables.modalWordmarkHeight}
+                    fill={colors.green}
+                    src={Expensicons.ExpensifyWordmark}
+                />
+            </View>
+        </View>
+    );
 }
 
 JustSignedInModal.propTypes = propTypes;
