53e2de6504827c9bf87f3dde1783d5d42321f518
Sibtain Ali <allroundexperts@gmail.com>
2023-06-09 04:45:09 +0500

    fix: lint errors

--- src/components/EmojiPicker/EmojiPickerButton.js
+++ src/components/EmojiPicker/EmojiPickerButton.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
 import styles from '../../styles/styles';
 import * as StyleUtils from '../../styles/StyleUtils';
