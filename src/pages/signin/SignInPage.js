3c5873708441d2d280d54dcb66648cbad11e5c93
rory <rory@expensify.com>
2023-06-08 10:14:09 -0700

    Remove unused import

--- src/pages/signin/SignInPage.js
+++ src/pages/signin/SignInPage.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo} from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
