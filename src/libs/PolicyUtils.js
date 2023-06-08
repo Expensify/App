a609cd8d72626d24379ce7637746280c20e86cb4
neil-marcellini <neil@expensify.com>
2023-06-08 15:45:49 -0700

    Fix member emails to accountIDs

--- src/libs/PolicyUtils.js
+++ src/libs/PolicyUtils.js
@@ -111,20 +111,19 @@ const isPolicyAdmin = (policy) => lodashGet(policy, 'role') === CONST.POLICY.ROL
  * We only return members without errors. Otherwise, the members with errors would immediately be removed before the user has a chance to read the error.
  */
 function getClientPolicyMemberEmailsToAccountIDs(policyMembers, personalDetails) {
-    return _.chain(policyMembers)
-        .filter((member) => _.isEmpty(member.errors))
-        .keys()
-        .reduce((result, accountID) => {
-            const personalDetail = personalDetails[accountID];
-            if (personalDetail && personalDetail.login) {
-                return {
-                    ...result,
-                    [personalDetail.login]: accountID,
-                };
-            }
-            return result;
-        }, {})
-        .value();
+    const memberEmailsToAccountIDs = {};
+    _.each(policyMembers, (member, accountID) => {
+        if (!_.isEmpty(member.errors)) {
+            return;
+        }
+        const personalDetail = personalDetails[accountID];
+        if (!personalDetail || !personalDetail.login) {
+            return;
+        }
+        memberEmailsToAccountIDs[personalDetail.login] = accountID;
+
+    });
+    return memberEmailsToAccountIDs;
 }
 
 export {
