ed089f9c55d41421877f07b05cf33c42ad6de9b4
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-06-09 09:16:53 -0700

    Pulling in app doesn't work. This does. Going to work my way up to see where it fails.

--- index.share.js
+++ index.share.js
@@ -1,10 +1,26 @@
-import { AppRegistry, View, Text } from "react-native";
+// import { AppRegistry, View, Text } from "react-native";
+// 
+// function MyShareComponent() {
+//   return <View><Text>Rendered share component!</Text></View>
+// }
+// 
+// AppRegistry.registerComponent(
+//   "ShareMenuModuleComponent",
+//   () => MyShareComponent
+// );
+import 'react-native-gesture-handler';
+import {AppRegistry} from 'react-native';
+import DotIndicatorMessage from './src/components/DotIndicatorMessage'
+import styles from './src/styles/styles'
 
-function MyShareComponent() {
-  return <View><Text>Rendered share component!</Text></View>
+function TestEntry () {
+  return (
+                    <DotIndicatorMessage
+                        style={[styles.mv2]}
+                        type="success"
+                        messages={{0: 'hello'}}
+                    />
+  )
 }
 
-AppRegistry.registerComponent(
-  "ShareMenuModuleComponent",
-  () => MyShareComponent
-);
+AppRegistry.registerComponent("ShareMenuModuleComponent", () => TestEntry);
