489d83b168fab738c80c2c4a849f746154d34731
rory <rory@expensify.com>
2023-06-08 17:11:28 -0700

    Implement custom changes in CustomStatusBar

--- src/components/CustomStatusBar/index.android.js
+++ src/components/CustomStatusBar/index.android.js
@@ -1,7 +1,6 @@
 /**
  * On Android we setup the status bar in native code.
  */
-
 export default function CustomStatusBar() {
     // Prefer to not render the StatusBar component in Android as it can cause
     // issues with edge to edge display. We setup the status bar appearance in
