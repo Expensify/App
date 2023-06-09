e914b930af757a6e4623328f4efbbb3c73761ab1
dukenv0307 <dukenv0307@gmail.com>
2023-06-09 17:08:38 +0700

    fix header view display

--- src/pages/home/HeaderView.js
+++ src/pages/home/HeaderView.js
@@ -76,7 +76,7 @@ const HeaderView = (props) => {
     const isChatRoom = ReportUtils.isChatRoom(props.report);
     const isPolicyExpenseChat = ReportUtils.isPolicyExpenseChat(props.report);
     const isTaskReport = ReportUtils.isTaskReport(props.report);
-    const reportHeaderData = !isThread && props.report.parentReportID ? props.parentReport : props.report;
+    const reportHeaderData = !isTaskReport && !isThread && props.report.parentReportID ? props.parentReport : props.report;
     const title = ReportUtils.getReportName(reportHeaderData);
     const subtitle = ReportUtils.getChatRoomSubtitle(reportHeaderData, props.parentReport);
     const isConcierge = participants.length === 1 && _.contains(participants, CONST.EMAIL.CONCIERGE);
