e7684a5cc763b2676b86eda3003e9624a69121ba
VH <hoangzinhvn@gmail.com>
2023-06-09 21:44:29 +0700

    Use same logic for checking should show subscript for a report

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -2124,7 +2124,7 @@ function shouldReportShowSubscript(report) {
         return false;
     }
 
-    if (isPolicyExpenseChat(report) && !isThread(report) && !report.isOwnPolicyExpenseChat) {
+    if (isPolicyExpenseChat(report) && !isThread(report) && !isTaskReport(report) && !report.isOwnPolicyExpenseChat) {
         return true;
     }
 
