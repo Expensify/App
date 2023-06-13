ef3b464161f150a98c36a6874d9b0cecd9825803
Hezekiel Tamire <hezekiel.feyissa@gmail.com>
2023-06-13 23:14:56 +0300

    added props to PressableWithFeedback

--- src/components/Pressable/PressableWithFeedback.js
+++ src/components/Pressable/PressableWithFeedback.js
@@ -10,7 +10,7 @@ import * as StyleUtils from '../../styles/StyleUtils';
 
 const omittedProps = ['style', 'pressStyle', 'hoverStyle', 'focusStyle', 'wrapperStyle'];
 
-const PressableWithFeedbackPropTypes = {
+constDPressableWithFeedbackPropTypes = {
     ..._.omit(GenericPressablePropTypes.pressablePropTypes, omittedProps),
     /**
      * Determines what opacity value should be applied to the underlaying view when Pressable is pressed.
@@ -44,6 +44,7 @@ const PressableWithFeedback = forwardRef((props, ref) => {
     return (
         <GenericPressable
             ref={ref}
+            nativeID={props.nativeID}
             style={props.wrapperStyle}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...propsWithoutStyling}
