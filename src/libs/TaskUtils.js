0a7cb1ee5bb2e83e1f5980d85d1deadfc0410a92
VH <hoangzinhvn@gmail.com>
2023-06-13 22:48:30 +0700

    Add JSdoc

--- src/libs/TaskUtils.js
+++ src/libs/TaskUtils.js
@@ -3,7 +3,7 @@ import * as ReportActionsUtils from './ReportActionsUtils';
 import CONST from '../CONST';
 
 /**
- * Returns the parentReportAction if the given report is a thread/task.
+ * Returns Task assignee email
  *
  * @param {Object} taskReport
  * @returns {String|null}
@@ -21,6 +21,12 @@ function getTaskAssigneeEmail(taskReport) {
     return lodashGet(reportAction, 'childManagerEmail', null);
 }
 
+/**
+ * Returns Task owner email
+ *
+ * @param {Object} taskReport
+ * @returns {String|null}
+ */
 function getTaskOwnerEmail(taskReport) {
     return lodashGet(taskReport, 'ownerEmail', null);
 }
@@ -29,6 +35,16 @@ function isTaskCanceled(taskReport) {
     return taskReport.stateNum === CONST.REPORT.STATE_NUM.SUBMITTED && taskReport.statusNum === CONST.REPORT.STATUS.CLOSED;
 }
 
+/**
+ * Can only Mark Task Complete if:
+ *
+ * - Current user is either the assignee or the owner
+ * - And Task is not canceled
+ *
+ * @param {Object} taskReport
+ * @param {String} sessionEmail
+ * @returns {Boolean}
+ */
 function canMarkTaskComplete(taskReport, sessionEmail) {
     const taskAssigneeEmail = getTaskAssigneeEmail(taskReport);
     const taskOwnerEmail = getTaskOwnerEmail(taskReport);
