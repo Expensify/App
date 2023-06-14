ba7f24e3ef7e766aa380e8ee1d70aced72a7a8e5
Nikki Wines <nikkiwines@expensify.com>
2023-06-14 15:36:24 -0400

    remove unused useState import

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, Pressable} from 'react-native';
 import PropTypes from 'prop-types';
 import styles from '../styles/styles';
