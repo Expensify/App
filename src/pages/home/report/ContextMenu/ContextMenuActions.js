36437140a2742ece5faa49c4361e73bdd2b13d8d
Oliver Wilks <ollywilks@googlemail.com>
2023-06-14 19:06:39 +0100

    Check if is money request when showing pin option

--- src/pages/home/report/ContextMenu/ContextMenuActions.js
+++ src/pages/home/report/ContextMenu/ContextMenuActions.js
@@ -272,7 +272,7 @@ export default [
     {
         textTranslateKey: 'common.pin',
         icon: Expensicons.Pin,
-        shouldShow: (type, reportAction, isArchivedRoom, betas, anchor, isChronosReport, reportID, isPinnedChat) => type === CONTEXT_MENU_TYPES.REPORT && !isPinnedChat,
+        shouldShow: (type, reportAction, isArchivedRoom, betas, anchor, isChronosReport, reportID, isPinnedChat) => !ReportUtils.isMoneyRequestReport(undefined, reportID) && type === CONTEXT_MENU_TYPES.REPORT && !isPinnedChat,
         onPress: (closePopover, {reportID}) => {
             Report.togglePinnedState(reportID, false);
             if (closePopover) {
@@ -284,7 +284,7 @@ export default [
     {
         textTranslateKey: 'common.unPin',
         icon: Expensicons.Pin,
-        shouldShow: (type, reportAction, isArchivedRoom, betas, anchor, isChronosReport, reportID, isPinnedChat) => type === CONTEXT_MENU_TYPES.REPORT && isPinnedChat,
+        shouldShow: (type, reportAction, isArchivedRoom, betas, anchor, isChronosReport, reportID, isPinnedChat) => !ReportUtils.isMoneyRequestReport(undefined, reportID) && type === CONTEXT_MENU_TYPES.REPORT && isPinnedChat,
         onPress: (closePopover, {reportID}) => {
             Report.togglePinnedState(reportID, true);
             if (closePopover) {
