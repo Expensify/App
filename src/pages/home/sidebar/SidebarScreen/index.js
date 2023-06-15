9280c5b213dc56335de32938d38be1abd23e97af
Rahul kushwaha <rahulkushwaha931@gmail.com>
2023-06-15 17:09:44 +0530

    set statusbar color to sidebar in useFocusEffect

--- src/pages/home/sidebar/SidebarScreen/index.js
+++ src/pages/home/sidebar/SidebarScreen/index.js
@@ -1,12 +1,19 @@
 import React, {useRef} from 'react';
+import {useFocusEffect} from '@react-navigation/native';
 import sidebarPropTypes from './sidebarPropTypes';
 import BaseSidebarScreen from './BaseSidebarScreen';
 import FloatingActionButtonAndPopover from './FloatingActionButtonAndPopover';
 import FreezeWrapper from '../../../../libs/Navigation/FreezeWrapper';
 import withWindowDimensions from '../../../../components/withWindowDimensions';
+import StatusBar from '../../../../libs/StatusBar';
+import themeColors from '../../../../styles/themes/default';
 
 function SidebarScreen(props) {
     const popoverModal = useRef(null);
+
+    useFocusEffect(() => {
+        StatusBar.setBackgroundColor(themeColors.sidebar);
+    });
 
     /**
      * Method create event listener
