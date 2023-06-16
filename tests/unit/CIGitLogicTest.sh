6cb914b755d20163cce37f23af1e4a70e2907f9a
rory <rory@expensify.com>
2023-06-16 11:23:28 -0700

    Create utility function for bumping version

--- tests/unit/CIGitLogicTest.sh
+++ tests/unit/CIGitLogicTest.sh
@@ -10,10 +10,6 @@ getPullRequestsMergedBetween="$TEST_DIR/utils/getPullRequestsMergedBetween.mjs"
 
 source "$SCRIPTS_DIR/shellUtils.sh"
 
-function print_version {
-  < package.json jq -r .version
-}
-
 function setup_git_as_human {
   info "Switching to human git user"
   git config user.name test
@@ -24,6 +20,23 @@ function setup_git_as_osbotify {
   info "Switching to OSBotify git user"
   git config user.name OSBotify
   git config user.email infra+osbotify@expensify.com
+}
+
+function print_version {
+  < package.json jq -r .version
+}
+
+function bump_version {
+  info "Bumping version..."
+  setup_git_as_osbotify
+  if [[ $1 == 'minor' ]]; then
+    npm --no-git-tag-version version minor
+  else
+    npm --no-git-tag-version version patch
+  fi
+  git add package.json package-lock.json
+  git commit -m "Update version to $(print_version)"
+  success "Version bumped to $(print_version) on main"
 }
 
 ### Phase 0: Verify necessary tools are installed (all tools should be pre-installed on all GitHub Actions runners)
@@ -87,25 +100,19 @@ success "Setup complete!"
 title "Scenario #1: Merge a pull request while the checklist is unlocked"
 
 info "Creating PR #1 and merging it into main..."
-git switch -c "pr-1"
-echo "Changes from PR #1" >> "PR1.txt"
-git add "PR1.txt"
+git switch -c pr-1
+echo "Changes from PR #1" >> PR1.txt
+git add PR1.txt
 git commit -m "Changes from PR #1"
 success "Created PR #1 in branch pr-1"
 
 info "Merging PR #1 to main"
 git switch main
-git merge "pr-1" --no-ff -m "Merge pull request #1 from Expensify/pr-1"
-git branch -d "pr-1"
+git merge pr-1 --no-ff -m "Merge pull request #1 from Expensify/pr-1"
+git branch -d pr-1
 success "Merged PR #1 to main"
 
-# Bump the version to 1.0.1
-info "Bumping version to 1.0.1"
-setup_git_as_osbotify
-npm --no-git-tag-version version 1.0.1
-git add package.json package-lock.json
-git commit -m "Update version to $(print_version)"
-success "Version bumped to $(print_version) on main"
+bump_version patch
 
 # Recreate staging from main
 info "Recreating staging from main..."
@@ -131,14 +138,14 @@ title "Scenario #2: Merge a pull request with the checklist locked, but don't CP
 
 info "Creating PR #2 and merging it into main..."
 setup_git_as_human
-git switch -c "pr-2"
-echo "Changes from PR #2" >> "PR2.txt"
-git add "PR2.txt"
+git switch -c pr-2
+echo "Changes from PR #2" >> PR2.txt
+git add PR2.txt
 git commit -m "Changes from PR #2"
 git switch main
-git merge "pr-2" --no-ff -m "Merge pull request #2 from Expensify/pr-2"
+git merge pr-2 --no-ff -m "Merge pull request #2 from Expensify/pr-2"
 info "Merged PR #2 into main"
-git branch -d "pr-2"
+git branch -d pr-2
 success "Created PR #2 and merged it to main!"
 
 success "Scenario #2 completed successfully!"
@@ -146,24 +153,18 @@ success "Scenario #2 completed successfully!"
 title "Scenario #3: Merge a pull request with the checklist locked and CP it to staging"
 
 info "Creating PR #3 and merging it into main..."
-git switch -c "pr-3"
-echo "Changes from PR #3" >> "PR3.txt"
-git add "PR3.txt"
+git switch -c pr-3
+echo "Changes from PR #3" >> PR3.txt
+git add PR3.txt
 git commit -m "Changes from PR #3"
 git switch main
-git merge "pr-3" --no-ff -m "Merge pull request #3 from Expensify/pr-3"
+git merge pr-3 --no-ff -m "Merge pull request #3 from Expensify/pr-3"
 PR_MERGE_COMMIT="$(git log -1 --format='%H')"
 info "Merged PR #3 into main"
-git branch -d "pr-3"
+git branch -d pr-3
 success "Created PR #3 and merged it to main!"
 
-info "Bumping version to 1.0.2 on main..."
-setup_git_as_osbotify
-npm --no-git-tag-version version 1.0.2 -m "Update version to 1.0.2"
-git add package.json package-lock.json
-git commit -m "Update version to $(print_version)"
-VERSION_BUMP_COMMIT="$(git log -1 --format='%H')"
-success "Bumped version to 1.0.2 on main!"
+bump_version patch
 
 info "Cherry picking PR #3 and the version bump to staging..."
 git switch staging
@@ -210,12 +211,7 @@ success "Scenario #4A completed successfully!"
 
 title "Scenario #4B: Run the staging deploy and create a new checklist"
 
-info "Bumping version to 1.1.0 on main..."
-git switch main
-npm --no-git-tag-version version 1.1.0
-git add package.json package-lock.json
-git commit -m "Update version to $(print_version)"
-success "Successfully updated version to 1.1.0 on main!"
+bump_version minor
 
 info "Re-creating staging from main..."
 git branch -D staging
@@ -239,22 +235,17 @@ title "Scenario #5: Merging another pull request when the checklist is unlocked"
 info "Creating PR #5 and merging it to main..."
 setup_git_as_human
 git switch main
-git switch -c "pr-5"
-echo "Changes from PR #5" >> "PR5.txt"
-git add "PR5.txt"
+git switch -c pr-5
+echo "Changes from PR #5" >> PR5.txt
+git add PR5.txt
 git commit -m "Changes from PR #5"
 git switch main
-git merge "pr-5" --no-ff -m "Merge pull request #5 from Expensify/pr-5"
+git merge pr-5 --no-ff -m "Merge pull request #5 from Expensify/pr-5"
 info "Merged PR #5 into main"
-git branch -d "pr-5"
+git branch -d pr-5
 success "Created PR #5 and merged it into main!"
 
-info "Bumping version to 1.1.1 on main..."
-setup_git_as_osbotify
-npm --no-git-tag-version version 1.1.1 -m "Update version to 1.1.1"
-git add package.json package-lock.json
-git commit -m "Update version to $(print_version)"
-success "Bumped version to 1.1.1 on main!"
+bump_version patch
 
 info "Recreating staging from main..."
 git branch -D staging
@@ -275,7 +266,7 @@ info "Checking output of getPullRequestsMergedBetween 1.1.0 1.1.1"
 output=$(node "$getPullRequestsMergedBetween" '1.1.0' '1.1.1')
 assert_equal "$output" "[ '5' ]"
 
-success "Scenario #6 completed successfully!"
+success "Scenario #5 completed successfully!"
 
 ### Cleanup
 title "Cleaning up..."
