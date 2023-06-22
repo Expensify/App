a1f26ce13923afce75837a929bab5fef2d86158d
Ryan Linton <ryanlntn@gmail.com>
2023-06-21 18:46:25 -0600

    clean up and add in providers and navigation

--- index.share.js
+++ index.share.js
@@ -1,34 +1,41 @@
-// import { AppRegistry, View, Text } from "react-native";
-//
-// function MyShareComponent() {
-//   return <View><Text>Rendered share component!</Text></View>
-// }
-//
-// AppRegistry.registerComponent(
-//   "ShareMenuModuleComponent",
-//   () => MyShareComponent
-// );
-import {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useEffect, useState} from 'react';
 import {AppRegistry, Pressable, ScrollView, Text, View} from 'react-native';
-import 'react-native-gesture-handler';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Onyx, {withOnyx} from 'react-native-onyx';
+// import OnyxProvider from './src/components/OnyxProvider';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
 import CONFIG from './src/CONFIG';
 import ONYXKEYS from './src/ONYXKEYS';
 import ExpensifyWordmark from './src/components/ExpensifyWordmark';
+// import SafeArea from './src/components/SafeArea';
+// import {KeyboardStateProvider} from './src/components/withKeyboardState';
+import ComposeProviders from './src/components/ComposeProviders';
+import CustomStatusBar from './src/components/CustomStatusBar';
+import HeaderWithCloseButton from './src/components/HeaderWithCloseButton';
+import OptionsSelector from './src/components/OptionsSelector';
+import withLocalize, {LocaleContextProvider} from './src/components/withLocalize';
+import {WindowDimensionsProvider} from './src/components/withWindowDimensions';
 import NetworkConnection from './src/libs/NetworkConnection';
 import * as Pusher from './src/libs/Pusher/pusher';
 import PusherConnectionManager from './src/libs/PusherConnectionManager';
 import * as App from './src/libs/actions/App';
 import * as User from './src/libs/actions/User';
+import compose from './src/libs/compose';
 import styles from './src/styles/styles';
 
-const config = {
-    keys: ONYXKEYS,
-};
+Onyx.init({keys: ONYXKEYS});
 
-Onyx.init(config);
-
-function BasicOnyxComponent(props) {
+const Home = compose(
+    withLocalize,
+    withOnyx({
+        account: {key: ONYXKEYS.ACCOUNT},
+        personalDetails: {key: ONYXKEYS.PERSONAL_DETAILS},
+        preferredLocale: {key: ONYXKEYS.NVP_PREFERRED_LOCALE},
+        session: {key: ONYXKEYS.SESSION},
+    }),
+)(function HomePage(props) {
     useEffect(() => {
         NetworkConnection.listenForReconnect();
         NetworkConnection.onReconnect(() => App.reconnectApp());
@@ -45,56 +52,115 @@ function BasicOnyxComponent(props) {
         App.setUpPoliciesAndNavigate(props.session);
     }, [props.session]);
 
+    const [searchValue, setSearchValue] = useState();
+
     return (
-        <ScrollView
-            contentContainerStyle={{padding: 24}}
-            style={{flex: 1}}
-        >
-            <Pressable onPress={() => App.setLocale(props.preferredLocale === 'en' ? 'es' : 'en')}>
-                <ExpensifyWordmark style={styles.anonymousRoomFooterLogo} />
-            </Pressable>
-            <View style={{padding: 24}} />
-            {Object.entries(props).map(([prop, json]) => (
-                <Text
-                    key={prop}
-                    style={{color: '#E7ECE9', fontWeight: 'bold'}}
-                >
-                    {prop.toUpperCase()}
-                    {'\n\n'}
-                    {typeof json === 'string' ? (
-                        <Text style={{color: '#E7ECE9', fontWeight: 'normal'}}>
-                            {json}
-                            {'\n\n'}
-                        </Text>
-                    ) : (
-                        Object.entries(json).map(([key, value]) => (
-                            <Text
-                                key={key}
-                                style={{color: '#E7ECE9', fontWeight: 'normal'}}
-                            >
-                                {key}: {JSON.stringify(value)}
-                                {'\n\n'}
+        <View style={{backgroundColor: '#07271F', flex: 1}}>
+            <HeaderWithCloseButton
+                title={props.translate('common.share')}
+                onCloseButtonPress={() => console.log('close share extension')}
+            />
+            <View style={[styles.flex1, styles.w100, styles.pRelative]}>
+                <OptionsSelector
+                    sections={[]}
+                    value={searchValue}
+                    // onSelectRow={this.selectReport}
+                    onChangeText={setSearchValue}
+                    // headerMessage={this.state.headerMessage}
+                    hideSectionHeaders
+                    showTitleTooltip
+                    // shouldShowOptions={didScreenTransitionEnd && isOptionsDataReady}
+                    textInputLabel={props.translate('optionsSelector.nameEmailOrPhoneNumber')}
+                    // onLayout={this.searchRendered}
+                    // safeAreaPaddingBottomStyle={safeAreaPaddingBottomStyle}
+                />
+            </View>
+            <View style={{padding: 48}} />
+            <ScrollView contentContainerStyle={{padding: 24}}>
+                <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
+                    <Pressable onPress={() => App.setLocale(props.preferredLocale === 'en' ? 'es' : 'en')}>
+                        <ExpensifyWordmark style={styles.anonymousRoomFooterLogo} />
+                    </Pressable>
+                    <Pressable onPress={() => props.navigation.navigate('Message')}>
+                        <Text style={{color: '#E7ECE9', fontWeight: 'bold'}}>{props.translate('common.next')}</Text>
+                    </Pressable>
+                </View>
+                <View style={{padding: 24}} />
+                {Object.entries(props).map(([prop, json]) => (
+                    <Text
+                        key={prop}
+                        style={{color: '#E7ECE9', fontWeight: 'bold'}}
+                    >
+                        {prop.toUpperCase()}
+                        {'\n\n'}
+                        {typeof json === 'string' ? (
+                            <Text style={{color: '#E7ECE9', fontWeight: 'normal'}}>
+                                {json}
+                                {'\n'}
                             </Text>
-                        ))
-                    )}
-                </Text>
-            ))}
-        </ScrollView>
-    );
-}
-
-const WithHOC = withOnyx({
-    account: {key: ONYXKEYS.ACCOUNT},
-    preferredLocale: {key: ONYXKEYS.NVP_PREFERRED_LOCALE},
-    session: {key: ONYXKEYS.SESSION},
-})(BasicOnyxComponent);
-
-function TestEntry() {
-    return (
-        <View style={{flex: 1, backgroundColor: '#07271F'}}>
-            <WithHOC />
+                        ) : (
+                            Object.entries(json).map(([key, value]) => (
+                                <Text
+                                    key={key}
+                                    style={{color: '#E7ECE9', fontWeight: 'normal'}}
+                                >
+                                    {key}: {JSON.stringify(value)}
+                                    {'\n'}
+                                </Text>
+                            ))
+                        )}
+                    </Text>
+                ))}
+            </ScrollView>
         </View>
     );
-}
+});
 
-AppRegistry.registerComponent('ShareMenuModuleComponent', () => TestEntry);
+const Message = compose(withLocalize)(function MessagePage(props) {
+    return (
+        <Pressable
+            onPress={props.navigation.goBack}
+            style={{alignItems: 'center', backgroundColor: '#07271F', flex: 1, justifyContent: 'center'}}
+        >
+            <Text style={{color: '#E7ECE9', fontWeight: 'bold'}}>{props.translate('common.goBack')}</Text>
+        </Pressable>
+    );
+});
+
+const Stack = createStackNavigator();
+
+const ShareExtension = () => (
+    <GestureHandlerRootView style={{flex: 1}}>
+        <ComposeProviders
+            components={[
+                // OnyxProvider,
+                // SafeAreaProvider,
+                // PortalProvider,
+                // SafeArea,
+                LocaleContextProvider,
+                WindowDimensionsProvider,
+                // KeyboardStateProvider,
+                // PickerStateProvider,
+            ]}
+        >
+            <CustomStatusBar />
+            {/* this appears to require firebase */}
+            {/* <ErrorBoundary errorMessage="NewExpensify crash caught by error boundary"> */}
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{headerShown: false}}>
+                    <Stack.Screen
+                        name="Home"
+                        component={Home}
+                    />
+                    <Stack.Screen
+                        name="Message"
+                        component={Message}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+            {/* </ErrorBoundary> */}
+        </ComposeProviders>
+    </GestureHandlerRootView>
+);
+
+AppRegistry.registerComponent('ShareMenuModuleComponent', () => ShareExtension);
