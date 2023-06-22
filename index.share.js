58f8c57dc416e47be265433e6187b979483b3f57
Ryan Linton <ryanlntn@gmail.com>
2023-06-21 19:37:03 -0600

    make linter happier

--- index.share.js
+++ index.share.js
@@ -1,9 +1,11 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import PropTypes from 'prop-types';
 import {useEffect, useState} from 'react';
 import {AppRegistry, Pressable, ScrollView, Text, View} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Onyx, {withOnyx} from 'react-native-onyx';
+import _ from 'underscore';
 // import OnyxProvider from './src/components/OnyxProvider';
 // import {SafeAreaProvider} from 'react-native-safe-area-context';
 import CONFIG from './src/CONFIG';
@@ -15,15 +17,55 @@ import ComposeProviders from './src/components/ComposeProviders';
 import CustomStatusBar from './src/components/CustomStatusBar';
 import HeaderWithCloseButton from './src/components/HeaderWithCloseButton';
 import OptionsSelector from './src/components/OptionsSelector';
-import withLocalize, {LocaleContextProvider} from './src/components/withLocalize';
-import {WindowDimensionsProvider} from './src/components/withWindowDimensions';
+import withLocalize, {LocaleContextProvider, withLocalizePropTypes} from './src/components/withLocalize';
+import {WindowDimensionsProvider, windowDimensionsPropTypes} from './src/components/withWindowDimensions';
 import NetworkConnection from './src/libs/NetworkConnection';
 import * as Pusher from './src/libs/Pusher/pusher';
 import PusherConnectionManager from './src/libs/PusherConnectionManager';
 import * as App from './src/libs/actions/App';
 import * as User from './src/libs/actions/User';
 import compose from './src/libs/compose';
+import personalDetailsPropType from './src/pages/personalDetailsPropType';
 import styles from './src/styles/styles';
+
+const propTypes = {
+    /* Onyx Props */
+
+    /** The details about the account that the user is signing in with */
+    account: PropTypes.shape({
+        /** An error message to display to the user */
+        errors: PropTypes.objectOf(PropTypes.string),
+
+        /** The message to be displayed when code requested */
+        message: PropTypes.string,
+    }),
+
+    /** All of the personal details for everyone */
+    personalDetails: personalDetailsPropType,
+
+    /** Indicates which locale the user currently has selected */
+    preferredLocale: PropTypes.string,
+
+    /** Session info for the currently logged-in user. */
+    session: PropTypes.shape({
+        /** Currently logged-in user email */
+        email: PropTypes.string,
+
+        /** Currently logged-in user authToken */
+        authToken: PropTypes.string,
+    }),
+
+    /** Window Dimensions Props */
+    ...windowDimensionsPropTypes,
+
+    ...withLocalizePropTypes,
+};
+const defaultProps = {
+    account: {},
+    personalDetails: {},
+    preferredLocale: 'en',
+    session: {},
+};
 
 Onyx.init({keys: ONYXKEYS});
 
@@ -35,7 +77,7 @@ const Home = compose(
         preferredLocale: {key: ONYXKEYS.NVP_PREFERRED_LOCALE},
         session: {key: ONYXKEYS.SESSION},
     }),
-)(function HomePage(props) {
+)((props) => {
     useEffect(() => {
         NetworkConnection.listenForReconnect();
         NetworkConnection.onReconnect(() => App.reconnectApp());
@@ -58,7 +100,7 @@ const Home = compose(
         <View style={{backgroundColor: '#07271F', flex: 1}}>
             <HeaderWithCloseButton
                 title={props.translate('common.share')}
-                onCloseButtonPress={() => console.log('close share extension')}
+                onCloseButtonPress={() => alert('close share extension')}
             />
             <View style={[styles.flex1, styles.w100, styles.pRelative]}>
                 <OptionsSelector
@@ -86,7 +128,7 @@ const Home = compose(
                     </Pressable>
                 </View>
                 <View style={{padding: 24}} />
-                {Object.entries(props).map(([prop, json]) => (
+                {_.map(Object.entries(props), ([prop, json]) => (
                     <Text
                         key={prop}
                         style={{color: '#E7ECE9', fontWeight: 'bold'}}
@@ -99,7 +141,7 @@ const Home = compose(
                                 {'\n'}
                             </Text>
                         ) : (
-                            Object.entries(json).map(([key, value]) => (
+                            _.map(Object.entries(json), ([key, value]) => (
                                 <Text
                                     key={key}
                                     style={{color: '#E7ECE9', fontWeight: 'normal'}}
@@ -116,16 +158,22 @@ const Home = compose(
     );
 });
 
-const Message = compose(withLocalize)(function MessagePage(props) {
-    return (
-        <Pressable
-            onPress={props.navigation.goBack}
-            style={{alignItems: 'center', backgroundColor: '#07271F', flex: 1, justifyContent: 'center'}}
-        >
-            <Text style={{color: '#E7ECE9', fontWeight: 'bold'}}>{props.translate('common.goBack')}</Text>
-        </Pressable>
-    );
-});
+Home.propTypes = propTypes;
+Home.defaultProps = defaultProps;
+
+const Message = withLocalize((props) => (
+    <Pressable
+        onPress={props.navigation.goBack}
+        style={{alignItems: 'center', backgroundColor: '#07271F', flex: 1, justifyContent: 'center'}}
+    >
+        <Text style={{color: '#E7ECE9', fontWeight: 'bold'}}>{props.translate('common.goBack')}</Text>
+    </Pressable>
+));
+
+Message.propTypes = {
+    ...withLocalizePropTypes,
+};
+Message.defaultProps = {};
 
 const Stack = createStackNavigator();
 
