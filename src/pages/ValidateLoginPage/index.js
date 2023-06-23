e5fa6f907191c48f65cea92b51ee937b1b44351d
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-06-23 19:06:48 +0200

    fix: revert removing false

--- src/pages/ValidateLoginPage/index.js
+++ src/pages/ValidateLoginPage/index.js
@@ -58,7 +58,7 @@ class ValidateLoginPage extends Component {
             if (lodashGet(this.props, 'session.authToken')) {
                 // If already signed in, do not show the validate code if not on web,
                 // because we don't want to block the user with the interstitial page.
-                Navigation.goBack();
+                Navigation.goBack(false);
             } else {
                 Session.signInWithValidateCodeAndNavigate(accountID, validateCode, this.props.preferredLocale);
             }
