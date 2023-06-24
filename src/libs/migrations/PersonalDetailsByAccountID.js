218920473e5753234408554cfd86517e4923ad84
Puneet Lath <puneet@expensify.com>
2023-06-24 19:52:27 -0400

    Use lodashHas for checking if properties exist in object

--- src/libs/migrations/PersonalDetailsByAccountID.js
+++ src/libs/migrations/PersonalDetailsByAccountID.js
@@ -1,4 +1,5 @@
 import _ from 'underscore';
+import lodashHas from 'lodash/has';
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 
@@ -44,131 +45,127 @@ function getDeprecatedPersonalDetailsFromOnyx() {
  * @returns {Promise<void>}
  */
 export default function () {
-    return Promise.all([getReportActionsFromOnyx(), getDeprecatedPersonalDetailsFromOnyx()]).then(
-        ([oldReportActions, oldPersonalDetails]) => {
-            const onyxData = {};
-            // originalMessage.oldLogin -> originalMessage.oldAccountID x
-            // originalMessage.newLogin -> originalMessage.newAccountID x
-            // actorEmail -> actorAccountID x
-            // childManagerEmail -> childManagerAccountID x
-            // whisperedTo -> whisperedToAccountIDs x
-            // childOldestFourEmails -> childOldestFourAccountIDs x
-            // participants -> participantAccountIDs ?
-            // accountEmail -> accountID ?
+    return Promise.all([getReportActionsFromOnyx(), getDeprecatedPersonalDetailsFromOnyx()]).then(([oldReportActions, oldPersonalDetails]) => {
+        const onyxData = {};
+        // originalMessage.oldLogin -> originalMessage.oldAccountID x
+        // originalMessage.newLogin -> originalMessage.newAccountID x
+        // actorEmail -> actorAccountID x
+        // childManagerEmail -> childManagerAccountID x
+        // whisperedTo -> whisperedToAccountIDs x
+        // childOldestFourEmails -> childOldestFourAccountIDs x
+        // participants -> participantAccountIDs ?
+        // accountEmail -> accountID ?
 
-            // We migrate reportActions to have the new accountID-based data if they don't already.
-            // If we are not able to get the accountID for some reason, we will just clear the reportAction
-            // and let it be fetched from the API next time they open the report and scroll to that action.
-            _.each(oldReportActions, (reportActionsForReport, onyxKey) => {
-                if (!reportActionsForReport) {
+        // We migrate reportActions to have the new accountID-based data if they don't already.
+        // If we are not able to get the accountID for some reason, we will just clear the reportAction
+        // and let it be fetched from the API next time they open the report and scroll to that action.
+        _.each(oldReportActions, (reportActionsForReport, onyxKey) => {
+            if (!reportActionsForReport) {
+                return;
+            }
+
+            const newReportActionsForReport = {};
+            _.each(reportActionsForReport, (reportAction) => {
+                if (!reportAction) {
                     return;
                 }
 
-                const newReportActionsForReport = {};
-                _.each(reportActionsForReport, (reportAction) => {
-                    if (!reportAction) {
+                const newReportAction = reportAction;
+
+                if (lodashHas(reportAction, ['originalMessage', 'oldLogin']) && !lodashHas(reportAction, ['originalMessage', 'oldAccountID'])) {
+                    const oldAccountID = _.get(oldPersonalDetails, [reportAction.originalMessage.oldLogin, 'accountID']);
+                    if (oldAccountID) {
+                        newReportAction.originalMessage.oldAccountID = oldAccountID;
+                    } else {
                         return;
                     }
+                }
 
-                    const newReportAction = reportAction;
-
-                    if (reportAction.originalMessage.oldLogin && !reportAction.originalMessage.oldAccountID) {
-                        const oldAccountID = _.get(oldPersonalDetails, [reportAction.originalMessage.oldLogin, 'accountID']);
-                        if (oldAccountID) {
-                            newReportAction.originalMessage.oldAccountID = oldAccountID;
-                        } else {
-                            return;
-                        }
+                if (lodashHas(reportAction, ['originalMessage', 'newLogin']) && !lodashHas(reportAction, ['originalMessage', 'newAccountID'])) {
+                    const newAccountID = _.get(oldPersonalDetails, [reportAction.originalMessage.newLogin, 'accountID']);
+                    if (newAccountID) {
+                        newReportAction.originalMessage.newAccountID = newAccountID;
+                    } else {
+                        return;
                     }
+                }
 
-                    if (reportAction.originalMessage.newLogin && !reportAction.originalMessage.newAccountID) {
-                        const newAccountID = _.get(oldPersonalDetails, [reportAction.originalMessage.newLogin, 'accountID']);
-                        if (newAccountID) {
-                            newReportAction.originalMessage.newAccountID = newAccountID;
-                        } else {
-                            return;
-                        }
+                if (lodashHas(reportAction, ['actorEmail']) && !lodashHas(reportAction, ['actorAccountID'])) {
+                    const actorAccountID = _.get(oldPersonalDetails, [reportAction.actorEmail, 'accountID']);
+                    if (actorAccountID) {
+                        newReportAction.actorAccountID = actorAccountID;
+                    } else {
+                        return;
                     }
+                }
 
-                    if (reportAction.actorEmail && !reportAction.actorAccountID) {
-                        const actorAccountID = _.get(oldPersonalDetails, [reportAction.actorEmail, 'accountID']);
-                        if (actorAccountID) {
-                            newReportAction.actorAccountID = actorAccountID;
-                        } else {
-                            return;
-                        }
+                if (lodashHas(reportAction, ['childManagerEmail']) && !lodashHas(reportAction, ['childManagerAccountID'])) {
+                    const childManagerAccountID = _.get(oldPersonalDetails, [reportAction.childManagerEmail, 'accountID']);
+                    if (childManagerAccountID) {
+                        newReportAction.childManagerAccountID = childManagerAccountID;
+                    } else {
+                        return;
                     }
+                }
 
-                    if (reportAction.childManagerEmail && !reportAction.childManagerAccountID) {
-                        const childManagerAccountID = _.get(oldPersonalDetails, [reportAction.childManagerEmail, 'accountID']);
-                        if (childManagerAccountID) {
-                            newReportAction.childManagerAccountID = childManagerAccountID;
-                        } else {
-                            return;
+                if (lodashHas(reportAction, ['whisperedTo']) && !lodashHas(reportAction, ['whisperedToAccountIDs'])) {
+                    const whisperedToAccountIDs = [];
+                    _.each(reportAction.whisperedTo, (whisperedToLogin) => {
+                        const whisperedToAccountID = _.get(oldPersonalDetails, [whisperedToLogin, 'accountID']);
+                        if (whisperedToAccountID) {
+                            whisperedToAccountIDs.push(whisperedToAccountID);
                         }
-                    }
-
-                    if (reportAction.whisperedTo && !reportAction.whisperedToAccountIDs) {
-                        const whisperedToAccountIDs = [];
-                        _.each(reportAction.whisperedTo, (whisperedToLogin) => {
-                            const whisperedToAccountID = _.get(oldPersonalDetails, [whisperedToLogin, 'accountID']);
-                            if (whisperedToAccountID) {
-                                whisperedToAccountIDs.push(whisperedToAccountID);
-                            }
-                        });
-
-                        if (whisperedToAccountIDs.length === reportAction.whisperedTo.length) {
-                            newReportAction.whisperedToAccountIDs = whisperedToAccountIDs;
-                        } else {
-                            return;
-                        }
-                    }
-
-                    if (reportAction.childOldestFourEmails && !reportAction.childOldestFourAccountIDs) {
-                        const childOldestFourEmails = reportAction.childOldestFourEmails.split(',');
-                        const childOldestFourAccountIDs = [];
-                        _.each(childOldestFourEmails, (login) => {
-                            const accountID = _.get(oldPersonalDetails, [login, 'accountID']);
-                            if (accountID) {
-                                childOldestFourAccountIDs.push(accountID);
-                            }
-                        });
-
-                        if (childOldestFourAccountIDs.length === childOldestFourEmails.length) {
-                            newReportAction.childOldestFourAccountIDs = childOldestFourAccountIDs.join(',');
-                        } else {
-                            return;
-                        }
-                    }
-
-
-                    if (reportAction.accountEmail && !reportAction.accountID) {
-                        const accountID = _.get(oldPersonalDetails, [reportAction.accountEmail, 'accountID']);
-                        if (accountID) {
-                            newReportAction.accountID = accountID;
-                        } else {
-                            return;
-                        }
-                    }
-
-                    const newOriginalMessage = _.omit(reportAction.originalMessage, ['participants']);
-                    const oldParticipants = _.get(reportAction, ['originalMessage', 'participants'], []);
-                    const newParticipants = [];
-                    _.each(oldParticipants, (login) => {
-                        const accountID = _.get(oldPersonalDetails, [login, 'accountID']);
-                        newParticipants.push(accountID);
                     });
-                    newOriginalMessage.participantAccountIDs = newParticipants;
-                    newReportAction.originalMessage = newOriginalMessage;
 
+                    if (whisperedToAccountIDs.length === reportAction.whisperedTo.length) {
+                        newReportAction.whisperedToAccountIDs = whisperedToAccountIDs;
+                    } else {
+                        return;
+                    }
+                }
 
-                    newReportActionsForReport[newReportAction.reportActionID] = newReportAction;
-                });
+                if (lodashHas(reportAction, ['childOldestFourEmails']) && !lodashHas(reportAction, ['childOldestFourAccountIDs'])) {
+                    const childOldestFourEmails = reportAction.childOldestFourEmails.split(',');
+                    const childOldestFourAccountIDs = [];
+                    _.each(childOldestFourEmails, (login) => {
+                        const accountID = _.get(oldPersonalDetails, [login, 'accountID']);
+                        if (accountID) {
+                            childOldestFourAccountIDs.push(accountID);
+                        }
+                    });
 
-                onyxData[onyxKey] = newReportActionsForReport;
+                    if (childOldestFourAccountIDs.length === childOldestFourEmails.length) {
+                        newReportAction.childOldestFourAccountIDs = childOldestFourAccountIDs.join(',');
+                    } else {
+                        return;
+                    }
+                }
+
+                // if (reportAction.accountEmail && !reportAction.accountID) {
+                //     const accountID = _.get(oldPersonalDetails, [reportAction.accountEmail, 'accountID']);
+                //     if (accountID) {
+                //         newReportAction.accountID = accountID;
+                //     } else {
+                //         return;
+                //     }
+                // }
+
+                // const newOriginalMessage = _.omit(reportAction.originalMessage, ['participants']);
+                // const oldParticipants = _.get(reportAction, ['originalMessage', 'participants'], []);
+                // const newParticipants = [];
+                // _.each(oldParticipants, (login) => {
+                //     const accountID = _.get(oldPersonalDetails, [login, 'accountID']);
+                //     newParticipants.push(accountID);
+                // });
+                // newOriginalMessage.participantAccountIDs = newParticipants;
+                // newReportAction.originalMessage = newOriginalMessage;
+
+                newReportActionsForReport[newReportAction.reportActionID] = newReportAction;
             });
 
-            return Onyx.multiSet(onyxData);
-        },
-    );
+            onyxData[onyxKey] = newReportActionsForReport;
+        });
+
+        return Onyx.multiSet(onyxData);
+    });
 }
