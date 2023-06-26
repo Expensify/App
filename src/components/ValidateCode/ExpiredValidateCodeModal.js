fbe83882cd340ad640b23df7db982cbb094d10b8
Cristi Paval <cristi@expensify.com>
2023-06-27 00:09:41 +0300

    Make expired validate code a function component.

--- src/components/ValidateCode/ExpiredValidateCodeModal.js
+++ src/components/ValidateCode/ExpiredValidateCodeModal.js
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import colors from '../../styles/colors';
 import styles from '../../styles/styles';
@@ -10,38 +10,35 @@ import * as Illustrations from '../Icon/Illustrations';
 import variables from '../../styles/variables';
 
 const propTypes = {
-
     ...withLocalizePropTypes,
 };
 
-class ExpiredValidateCodeModal extends PureComponent {
-    render() {
-        return (
-            <View style={styles.deeplinkWrapperContainer}>
-                <View style={styles.deeplinkWrapperMessage}>
-                    <View style={styles.mb2}>
-                        <Icon
-                            width={variables.modalTopIconWidth}
-                            height={variables.modalTopIconHeight}
-                            src={Illustrations.ToddBehindCloud}
-                        />
-                    </View>
-                    <Text style={[styles.textHeadline, styles.textXXLarge, styles.textAlignCenter]}>{this.props.translate('validateCodeModal.expiredCodeTitle')}</Text>
-                    <View style={[styles.mt2, styles.mb2]}>
-                        <Text style={[styles.fontSizeNormal, styles.textAlignCenter]}>{this.props.translate('validateCodeModal.expiredCodeDescription')}</Text>
-                    </View>
-                </View>
-                <View style={styles.deeplinkWrapperFooter}>
+function ExpiredValidateCodeModal(props) {
+    return (
+        <View style={styles.deeplinkWrapperContainer}>
+            <View style={styles.deeplinkWrapperMessage}>
+                <View style={styles.mb2}>
                     <Icon
-                        width={variables.modalWordmarkWidth}
-                        height={variables.modalWordmarkHeight}
-                        fill={colors.green}
-                        src={Expensicons.ExpensifyWordmark}
+                        width={variables.modalTopIconWidth}
+                        height={variables.modalTopIconHeight}
+                        src={Illustrations.ToddBehindCloud}
                     />
                 </View>
+                <Text style={[styles.textHeadline, styles.textXXLarge, styles.textAlignCenter]}>{props.translate('validateCodeModal.expiredCodeTitle')}</Text>
+                <View style={[styles.mt2, styles.mb2]}>
+                    <Text style={[styles.fontSizeNormal, styles.textAlignCenter]}>{props.translate('validateCodeModal.expiredCodeDescription')}</Text>
+                </View>
             </View>
-        );
-    }
+            <View style={styles.deeplinkWrapperFooter}>
+                <Icon
+                    width={variables.modalWordmarkWidth}
+                    height={variables.modalWordmarkHeight}
+                    fill={colors.green}
+                    src={Expensicons.ExpensifyWordmark}
+                />
+            </View>
+        </View>
+    );
 }
 
 ExpiredValidateCodeModal.propTypes = propTypes;
