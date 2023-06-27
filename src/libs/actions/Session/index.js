0ab8fde9f670a5882a9a62a3de5d66c514daff4f
VH <hoangzinhvn@gmail.com>
2023-06-27 23:20:34 +0700

    Fix unable to unlink due to translation error

--- src/libs/actions/Session/index.js
+++ src/libs/actions/Session/index.js
@@ -767,7 +767,7 @@ function unlinkLogin(accountID, validateCode) {
             key: ONYXKEYS.ACCOUNT,
             value: {
                 isLoading: false,
-                message: Localize.translateLocal('unlinkLoginForm.succesfullyUnlinkedLogin'),
+                message: 'unlinkLoginForm.succesfullyUnlinkedLogin',
             },
         },
         {
