3551658ff7d5d7703288c9e14b85eb655f941497
Eric Han <hanyl_fight@126.com>
2023-06-27 23:15:09 +0800

    fix clicking title of url preview don't open site

--- src/pages/home/report/LinkPreviewer.js
+++ src/pages/home/report/LinkPreviewer.js
@@ -5,6 +5,7 @@ import _ from 'underscore';
 import {uniqBy} from 'lodash';
 import useWindowDimensions from '../../../hooks/useWindowDimensions';
 import Text from '../../../components/Text';
+import TextLink from '../../../components/TextLink';
 import styles from '../../../styles/styles';
 import variables from '../../../styles/variables';
 import colors from '../../../styles/colors';
@@ -103,13 +104,14 @@ function LinkPreviewer(props) {
                             )}
                         </View>
                         {!_.isEmpty(title) && (
-                            <Text
+                            <TextLink
                                 fontSize={variables.fontSizeNormal}
                                 style={styles.pv2}
                                 color={colors.blueLinkPreview}
+                                href={url}
                             >
                                 {title}
-                            </Text>
+                            </TextLink>
                         )}
                         {!_.isEmpty(description) && <Text fontSize={variables.fontSizeNormal}>{description}</Text>}
                         {!_.isEmpty(image) && IMAGE_TYPES.includes(image.type) && (
