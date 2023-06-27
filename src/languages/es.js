c2c229c758e12c9c3127a94644f75757cb7b5f2a
Hezekiel Tamire <hezekiel.feyissa@gmail.com>
2023-06-27 21:22:37 +0300

    typo fixed

--- src/languages/es.js
+++ src/languages/es.js
@@ -706,7 +706,7 @@ export default {
         enterAuthenticatorCode: 'Por favor, introduce el código de autenticador',
         requiredWhen2FAEnabled: 'Obligatorio cuando A2F está habilitado',
         requestNewCode: 'Pedir un código nuevo en ',
-        requestNewCodeAfterErrorOccured: 'Solicitar un nuevo código',
+        requestNewCodeAfterErrorOccurred: 'Solicitar un nuevo código',
         error: {
             pleaseFillMagicCode: 'Por favor, introduce el código mágico',
             incorrectMagicCode: 'Código mágico incorrecto.',
