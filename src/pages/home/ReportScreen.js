89cedaf82c9e55cd9c06cbd35452d9c60df062d0
Pierre Michel <pmiche04@gmail.com>
2023-06-29 17:54:35 -0600

    Fixing the merge again
    Signed-off-by: Pierre Michel <pmiche04@gmail.com>

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -328,7 +328,7 @@ class ReportScreen extends React.Component {
                                     report={this.props.report}
                                     isComposerFullSize={this.props.isComposerFullSize}
                                     parentViewHeight={this.state.skeletonViewContainerHeight}
-                                    policies={this.props.policies}
+                                    policy={policy}
                                 />
                             )}
 
@@ -348,7 +348,7 @@ class ReportScreen extends React.Component {
                                         report={this.props.report}
                                         isComposerFullSize={this.props.isComposerFullSize}
                                         onSubmitComment={this.onSubmitComment}
-                                        policy={policy}
+                                        policies={this.props.policies}
                                     />
                                 </>
                             )}
