e6ad2d036aa52d561c6a1372a52f917a749e2ea6
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-06-29 15:18:24 +0800

    create common function for pending remove

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -390,7 +390,7 @@ function getLastMessageTextForReport(report) {
 
         // Yeah this is a bit ugly. If the latest report action that is not a whisper has been moderated as pending remove, then set the last message text to the text of the latest visible action that is not a whisper.
         const lastNonWhisper = _.find(allSortedReportActions[report.reportID], (action) => !ReportActionUtils.isWhisperAction(action)) || {};
-        if (lodashGet(lastNonWhisper, 'message[0].moderationDecisions[0].decision') === CONST.MODERATION.MODERATOR_DECISION_PENDING_REMOVE) {
+        if (ReportActionUtils.isPendingRemove(lastNonWhisper)) {
             const latestVisibleAction =
                 _.find(
                     allSortedReportActions[report.reportID],
