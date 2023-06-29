6d8223d454faae20a2e788f8115cf63192af849b
Blazej Kustra <blazej.kustra@swmansion.com>
2023-06-29 16:41:03 +0200

    Add type for ONYXKEYS.DEVICE_ID

--- src/types/onyx/OnyxValues.ts
+++ src/types/onyx/OnyxValues.ts
@@ -6,6 +6,7 @@ type OnyxValues = {
     [ONYXKEYS.ACCOUNT_MANAGER_REPORT_ID]?: string;
     [ONYXKEYS.NVP_IS_FIRST_TIME_NEW_EXPENSIFY_USER]?: boolean;
     [ONYXKEYS.ACTIVE_CLIENTS]: string[];
+    [ONYXKEYS.DEVICE_ID]: string;
 };
 
 export default OnyxValues;
