9fb8749d9489586d5dd102414a22012050d2d70a
dukenv0307 <dukenv0307@gmail.com>
2023-06-29 16:08:51 +0700

    fix: disable all-members button on admin channel

--- src/pages/settings/Report/ReportSettingsPage.js
+++ src/pages/settings/Report/ReportSettingsPage.js
@@ -97,9 +97,12 @@ class ReportSettingsPage extends Component {
         const shouldDisableRename = this.shouldDisableRename(linkedWorkspace) || ReportUtils.isThread(this.props.report);
         const notificationPreference = this.props.translate(`notificationPreferencesPage.notificationPreferences.${this.props.report.notificationPreference}`);
         const shouldDisableWelcomeMessage = this.shouldDisableWelcomeMessage(linkedWorkspace);
-        const writeCapability = this.props.report.writeCapability || CONST.REPORT.WRITE_CAPABILITIES.ALL;
+        const writeCapability = ReportUtils.isAdminRoom(this.props.report)
+            ? CONST.REPORT.WRITE_CAPABILITIES.ADMINS
+            : this.props.report.writeCapability || CONST.REPORT.WRITE_CAPABILITIES.ALL;
+
         const writeCapabilityText = this.props.translate(`writeCapabilityPage.writeCapability.${writeCapability}`);
-        const shouldAllowWriteCapabilityEditing = lodashGet(linkedWorkspace, 'role', '') === CONST.POLICY.ROLE.ADMIN;
+        const shouldAllowWriteCapabilityEditing = lodashGet(linkedWorkspace, 'role', '') === CONST.POLICY.ROLE.ADMIN && !ReportUtils.isAdminRoom(this.props.report);
 
         return (
             <ScreenWrapper>
@@ -148,7 +151,7 @@ class ReportSettingsPage extends Component {
                         )}
                         {shouldAllowWriteCapabilityEditing ? (
                             <MenuItemWithTopDescription
-                                shouldShowRightIcon
+                                shouldShowRightIcon={!ReportUtils.isAdminRoom(this.props.report)}
                                 title={writeCapabilityText}
                                 description={this.props.translate('writeCapabilityPage.label')}
                                 onPress={() => Navigation.navigate(ROUTES.getReportSettingsWriteCapabilityRoute(this.props.report.reportID))}
