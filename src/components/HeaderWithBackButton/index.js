0d94495032bf504710b6555988dfea19ffd0eb22
rory <rory@expensify.com>
2023-06-28 19:43:42 -0700

    Extract HeaderWithBackButton propTypes to separate file

--- src/components/HeaderWithBackButton/index.js
+++ src/components/HeaderWithBackButton/index.js
@@ -1,152 +1,29 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 import {View, Keyboard} from 'react-native';
-import _ from 'underscore';
-import styles from '../styles/styles';
-import Header from './Header';
-import Navigation from '../libs/Navigation/Navigation';
-import ROUTES from '../ROUTES';
-import Icon from './Icon';
-import * as Expensicons from './Icon/Expensicons';
-import Tooltip from './Tooltip';
-import getButtonState from '../libs/getButtonState';
-import * as StyleUtils from '../styles/StyleUtils';
-import compose from '../libs/compose';
-import ThreeDotsMenu, {ThreeDotsMenuItemPropTypes} from './ThreeDotsMenu';
-import withDelayToggleButtonState, {withDelayToggleButtonStatePropTypes} from './withDelayToggleButtonState';
-import withLocalize, {withLocalizePropTypes} from './withLocalize';
-import withKeyboardState, {keyboardStatePropTypes} from './withKeyboardState';
-import AvatarWithDisplayName from './AvatarWithDisplayName';
-import iouReportPropTypes from '../pages/iouReportPropTypes';
-import participantPropTypes from './participantPropTypes';
-import CONST from '../CONST';
-import PressableWithoutFeedback from './Pressable/PressableWithoutFeedback';
-import PinButton from './PinButton';
+import styles from '../../styles/styles';
+import Header from '../Header';
+import Navigation from '../../libs/Navigation/Navigation';
+import ROUTES from '../../ROUTES';
+import Icon from '../Icon';
+import * as Expensicons from '../Icon/Expensicons';
+import Tooltip from '../Tooltip';
+import getButtonState from '../../libs/getButtonState';
+import * as StyleUtils from '../../styles/StyleUtils';
+import compose from '../../libs/compose';
+import ThreeDotsMenu from '../ThreeDotsMenu';
+import withDelayToggleButtonState, {withDelayToggleButtonStatePropTypes} from '../withDelayToggleButtonState';
+import withLocalize, {withLocalizePropTypes} from '../withLocalize';
+import withKeyboardState, {keyboardStatePropTypes} from '../withKeyboardState';
+import AvatarWithDisplayName from '../AvatarWithDisplayName';
+import PressableWithoutFeedback from '../Pressable/PressableWithoutFeedback';
+import PinButton from '../PinButton';
+import {propTypes as headerWithBackButtonPropTypes, defaultProps as headerWithBackButtonDefaultProps} from './headerWithBackButtonPropTypes';
 
 const propTypes = {
-    /** Title of the Header */
-    title: PropTypes.string,
-
-    /** Subtitle of the header */
-    subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
-
-    /** Method to trigger when pressing download button of the header */
-    onDownloadButtonPress: PropTypes.func,
-
-    /** Method to trigger when pressing close button of the header */
-    onCloseButtonPress: PropTypes.func,
-
-    /** Method to trigger when pressing back button of the header */
-    onBackButtonPress: PropTypes.func,
-
-    /** Method to trigger when pressing more options button of the header */
-    onThreeDotsButtonPress: PropTypes.func,
-
-    /** Whether we should show a border on the bottom of the Header */
-    shouldShowBorderBottom: PropTypes.bool,
-
-    /** Whether we should show a download button */
-    shouldShowDownloadButton: PropTypes.bool,
-
-    /** Whether we should show a get assistance (question mark) button */
-    shouldShowGetAssistanceButton: PropTypes.bool,
-
-    /** Whether we should show a pin button */
-    shouldShowPinButton: PropTypes.bool,
-
-    /** Whether we should show a more options (threedots) button */
-    shouldShowThreeDotsButton: PropTypes.bool,
-
-    /** List of menu items for more(three dots) menu */
-    threeDotsMenuItems: ThreeDotsMenuItemPropTypes,
-
-    /** The anchor position of the menu */
-    threeDotsAnchorPosition: PropTypes.shape({
-        top: PropTypes.number,
-        right: PropTypes.number,
-        bottom: PropTypes.number,
-        left: PropTypes.number,
-    }),
-
-    /** The anchor alignment of the menu */
-    threeDotsAnchorAlignment: PropTypes.shape({
-        horizontal: PropTypes.oneOf(_.values(CONST.MODAL.ANCHOR_ORIGIN_HORIZONTAL)),
-        vertical: PropTypes.oneOf(_.values(CONST.MODAL.ANCHOR_ORIGIN_VERTICAL)),
-    }),
-
-    /** Whether we should show a close button */
-    shouldShowCloseButton: PropTypes.bool,
-
-    /** Whether we should show a back button */
-    shouldShowBackButton: PropTypes.bool,
-
-    /** Whether we should show the step counter */
-    shouldShowStepCounter: PropTypes.bool,
-
-    /** The guides call taskID to associate with the get assistance button, if we show it */
-    guidesCallTaskID: PropTypes.string,
-
-    /** Data to display a step counter in the header */
-    stepCounter: PropTypes.shape({
-        step: PropTypes.number,
-        total: PropTypes.number,
-        text: PropTypes.string,
-    }),
-
-    /** Whether we should show an avatar */
-    shouldShowAvatarWithDisplay: PropTypes.bool,
-
-    /** Parent report, if provided it will override props.report for AvatarWithDisplay */
-    parentReport: iouReportPropTypes,
-
-    /** Report, if we're showing the details for one and using AvatarWithDisplay */
-    report: iouReportPropTypes,
-
-    /** Policies, if we're showing the details for a report and need info about it for AvatarWithDisplay */
-    policies: PropTypes.shape({
-        /** Name of the policy */
-        name: PropTypes.string,
-    }),
-
-    /** Policies, if we're showing the details for a report and need participant details for AvatarWithDisplay */
-    personalDetails: PropTypes.objectOf(participantPropTypes),
-
+    ...headerWithBackButtonPropTypes,
     ...withLocalizePropTypes,
     ...withDelayToggleButtonStatePropTypes,
     ...keyboardStatePropTypes,
-};
-
-const defaultProps = {
-    title: '',
-    subtitle: '',
-    onDownloadButtonPress: () => {},
-    onBackButtonPress: () => Navigation.goBack(),
-    onCloseButtonPress: () => Navigation.dismissModal(),
-    onThreeDotsButtonPress: () => {},
-    shouldShowBorderBottom: false,
-    shouldShowDownloadButton: false,
-    shouldShowGetAssistanceButton: false,
-    shouldShowThreeDotsButton: false,
-    shouldShowPinButton: false,
-    shouldShowCloseButton: false,
-    shouldShowStepCounter: true,
-    shouldShowBackButton: true,
-    shouldShowAvatarWithDisplay: false,
-    report: null,
-    parentReport: null,
-    policies: {},
-    personalDetails: {},
-    guidesCallTaskID: '',
-    stepCounter: null,
-    threeDotsMenuItems: [],
-    threeDotsAnchorPosition: {
-        vertical: 0,
-        horizontal: 0,
-    },
-    threeDotsAnchorAlignment: {
-        horizontal: CONST.MODAL.ANCHOR_ORIGIN_HORIZONTAL.LEFT,
-        vertical: CONST.MODAL.ANCHOR_ORIGIN_VERTICAL.TOP,
-    },
 };
 
 class HeaderWithBackButton extends Component {
@@ -269,6 +146,6 @@ class HeaderWithBackButton extends Component {
 }
 
 HeaderWithBackButton.propTypes = propTypes;
-HeaderWithBackButton.defaultProps = defaultProps;
+HeaderWithBackButton.defaultProps = headerWithBackButtonDefaultProps;
 
 export default compose(withLocalize, withDelayToggleButtonState, withKeyboardState)(HeaderWithBackButton);
