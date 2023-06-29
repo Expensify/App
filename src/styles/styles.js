04fa5e9b2f042c3324786167f92c6426df7a93c1
Anjana Mendiratta <expertsquarrel@gmail.com>
2023-06-29 14:15:04 +0530

    fix: keyboard shortcuts resize sizing
    Signed-off-by: Anjana Mendiratta <expertsquarrel@gmail.com>

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2997,8 +2997,7 @@ const styles = {
 
     keyboardShortcutModalContainer: {
         maxHeight: '100%',
-        flexShrink: 0,
-        flexGrow: 0,
+        flex: 0,
         flexBasis: 'auto',
     },
 
