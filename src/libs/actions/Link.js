3e0c8d66fd60881f32e44fba08ef890525686491
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-06-30 13:03:47 +0800

    fix can't open link inside promise in safari

--- src/libs/actions/Link.js
+++ src/libs/actions/Link.js
@@ -67,17 +67,12 @@ function openOldDotLink(url) {
 
     // If shortLivedAuthToken is not accessible, fallback to opening the link without the token.
     // eslint-disable-next-line rulesdir/no-api-side-effects-method
-    API.makeRequestWithSideEffects('OpenOldDotLink', {}, {})
-        .then((response) => {
-            buildOldDotURL(url, response.shortLivedAuthToken).then((oldDotUrl) => {
-                Linking.openURL(oldDotUrl);
-            });
-        })
-        .catch(() => {
-            buildOldDotURL(url).then((oldDotUrl) => {
-                Linking.openURL(oldDotUrl);
-            });
-        });
+    asyncOpenURL(
+        API.makeRequestWithSideEffects('OpenOldDotLink', {}, {})
+            .then((response) => buildOldDotURL(url, response.shortLivedAuthToken))
+            .catch(() => buildOldDotURL(url)),
+        (url) => url,
+    );
 }
 
 /**
