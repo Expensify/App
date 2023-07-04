d0de724831f56ea42d8aece72b3f4209307143cb
payal-lathidadiya <130229379+payal-lathidadiya@users.noreply.github.com>
2023-07-04 20:43:29 +0530

    fix: emoji modal stops working in the chat when opening in the IOU report

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -33,7 +33,6 @@ import * as ReportActionsUtils from '../../libs/ReportActionsUtils';
 import personalDetailsPropType from '../personalDetailsPropType';
 import withNavigationFocus from '../../components/withNavigationFocus';
 import getIsReportFullyVisible from '../../libs/getIsReportFullyVisible';
-import EmojiPicker from '../../components/EmojiPicker/EmojiPicker';
 import * as EmojiPickerAction from '../../libs/actions/EmojiPickerAction';
 import TaskHeader from '../../components/TaskHeader';
 import MoneyRequestHeader from '../../components/MoneyRequestHeader';
@@ -363,7 +362,6 @@ class ReportScreen extends React.Component {
                                 />
                             )}
 
-                            <EmojiPicker ref={EmojiPickerAction.emojiPickerRef} />
                             <PortalHost name={CONST.REPORT.DROP_HOST_NAME} />
                         </View>
                     </FullPageNotFoundView>
