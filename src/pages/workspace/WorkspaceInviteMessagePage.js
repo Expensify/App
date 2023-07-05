631f7c3f757a89d91fc1356e513cd3d470d91532
Pujan <shahpujan1992@gmail.com>
2023-07-05 17:27:38 +0530

    restrict direct access for invite message without members

--- src/pages/workspace/WorkspaceInviteMessagePage.js
+++ src/pages/workspace/WorkspaceInviteMessagePage.js
@@ -81,6 +81,9 @@ class WorkspaceInviteMessagePage extends React.Component {
     }
 
     componentDidMount() {
+        if(_.isEmpty(this.props.invitedEmailsToAccountIDsDraft)) {
+            Navigation.goBack(ROUTES.getWorkspaceInviteRoute(this.props.route.params.policyID), true);
+        }
         this.focusWelcomeMessageInput();
     }
 
