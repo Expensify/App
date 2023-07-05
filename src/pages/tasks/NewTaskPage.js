ef26b0e4ca429c45547e351facbe12710c48d166
Jasper Huang <jasper@expensify.com>
2023-07-05 09:24:09 -0700

    fix merge conflicts

--- src/pages/tasks/NewTaskPage.js
+++ src/pages/tasks/NewTaskPage.js
@@ -118,7 +118,7 @@ function NewTaskPage(props) {
             return;
         }
 
-        TaskUtils.createTaskAndNavigate(parentReport.reportID, props.task.title, props.task.description, props.task.assignee, props.task.assigneeAccountID);
+        Task.createTaskAndNavigate(parentReport.reportID, props.task.title, props.task.description, props.task.assignee, props.task.assigneeAccountID);
     }
 
     if (!Permissions.canUseTasks(props.betas)) {
