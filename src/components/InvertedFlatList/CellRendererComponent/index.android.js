45914bbb23643a1190b5ae6b6c093752ffa9b278
petromoldovan <petro.moldovan@gmail.com>
2023-07-05 11:57:56 +0200

    Added comments for zIndex values

--- src/components/InvertedFlatList/CellRendererComponent/index.android.js
+++ src/components/InvertedFlatList/CellRendererComponent/index.android.js
@@ -13,7 +13,15 @@ function CellRendererComponent(props) {
         <View
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
-            style={[styles.invert, { zIndex: -props.index }]}
+            style={[
+                styles.invert,
+                // To achieve absolute positioning and handle overflows for list items,
+                // it is necessary to assign zIndex values. In the case of inverted lists,
+                // the lower list items will have higher zIndex values compared to the upper
+                // list items. Consequently, lower list items can overflow the upper list items.
+                // See: https://github.com/Expensify/App/issues/20451
+                { zIndex: -props.index }
+            ]}
         />
     );
 }
--- src/components/InvertedFlatList/CellRendererComponent/index.ios.js
+++ src/components/InvertedFlatList/CellRendererComponent/index.ios.js
@@ -19,7 +19,15 @@ function CellRendererComponent(props) {
         <View
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
-            style={[props.style, { zIndex: -props.index }]}
+            style={[
+                props.style,
+                // To achieve absolute positioning and handle overflows for list items,
+                // it is necessary to assign zIndex values. In the case of inverted lists,
+                // the lower list items will have higher zIndex values compared to the upper
+                // list items. Consequently, lower list items can overflow the upper list items.
+                // See: https://github.com/Expensify/App/issues/20451
+                { zIndex: -props.index }
+            ]}
         />
     );
 }
