0adc2884d1114920926f33c066867a0c13e47796
Huzaifa Rasheed <huzaifarasheed900@gmail.com>
2023-07-05 13:49:27 +0500

    show visible tooltip after animation cancel

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -45,6 +45,16 @@ class Tooltip extends PureComponent {
         this.showTooltip = this.showTooltip.bind(this);
         this.hideTooltip = this.hideTooltip.bind(this);
         this.updateBounds = this.updateBounds.bind(this);
+        this.isAnimationCanceled = React.createRef(false);
+    }
+
+    componentDidUpdate(prevProps) {
+        // if the tooltip text changed before the initial animation was finished, then the tooltip won't be shown
+        // we need to show the tooltip again
+        if (this.state.isVisible && this.isAnimationCanceled.current && this.props.text && prevProps.text !== this.props.text) {
+            this.isAnimationCanceled.current = false;
+            this.showTooltip();
+        }
     }
 
     /**
@@ -86,7 +96,9 @@ class Tooltip extends PureComponent {
                 duration: 140,
                 delay: 500,
                 useNativeDriver: false,
-            }).start();
+            }).start(({finished}) => {
+                this.isAnimationCanceled.current = !finished;
+            });
         }
         TooltipSense.activate();
     }
