220fc1eca1716422fc509787a2a00417c51e57fa
kadiealexander <59587260+kadiealexander@users.noreply.github.com>
2023-07-05 15:53:37 +1200

    Update src/components/ThumbnailImage.js
    Co-authored-by: Puneet Lath <puneet@expensify.com>

--- src/components/ThumbnailImage.js
+++ src/components/ThumbnailImage.js
@@ -85,7 +85,7 @@ function ThumbnailImage(props) {
             <View style={[StyleUtils.getWidthAndHeightStyle(imageWidth, imageHeight), styles.alignItemsCenter, styles.justifyContentCenter]}>
                 <ImageWithSizeCalculation
                     url={props.previewSourceURL}
-                    onMeasure={() => updateImageSize()}
+                    onMeasure={(width, height) => updateImageSize(width, height)}
                     isAuthTokenRequired={props.isAuthTokenRequired}
                 />
             </View>
