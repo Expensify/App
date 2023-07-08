fc4a0dc1bec916bc92a37f75e7e5e3fde720fcb6
Georgia Monahan <georgia@expensify.com>
2023-07-08 11:21:37 +0100

    Add src/components/MoneyReportHeader.js

---
+++ src/components/MoneyReportHeader.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import {withOnyx} from 'react-native-onyx';
+import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import lodashGet from 'lodash/get';
+import HeaderWithBackButton from './HeaderWithBackButton';
+import iouReportPropTypes from '../pages/iouReportPropTypes';
+import withLocalize, {withLocalizePropTypes} from './withLocalize';
+import * as ReportUtils from '../libs/ReportUtils';
+import * as Expensicons from './Icon/Expensicons';
+import Text from './Text';
+import participantPropTypes from './participantPropTypes';
+import Avatar from './Avatar';
+import styles from '../styles/styles';
+import themeColors from '../styles/themes/default';
+import CONST from '../CONST';
+import withWindowDimensions from './withWindowDimensions';
+import compose from '../libs/compose';
+import Navigation from '../libs/Navigation/Navigation';
+import ROUTES from '../ROUTES';
+import Icon from './Icon';
+import SettlementButton from './SettlementButton';
+import * as Policy from '../libs/actions/Policy';
+import ONYXKEYS from '../ONYXKEYS';
+import * as IOU from '../libs/actions/IOU';
+import * as CurrencyUtils from '../libs/CurrencyUtils';
+import MenuItemWithTopDescription from './MenuItemWithTopDescription';
+import DateUtils from '../libs/DateUtils';
+import reportPropTypes from '../pages/reportPropTypes';
+import * as UserUtils from '../libs/UserUtils';
+
+const propTypes = {
+    /** The report currently being looked at */
+    report: iouReportPropTypes.isRequired,
+
+    /** The expense report or iou report (only will have a value if this is a transaction thread) */
+    parentReport: iouReportPropTypes,
+
+    /** The policies which the user has access to and which the report could be tied to */
+    policies: PropTypes.shape({
+        /** Name of the policy */
+        name: PropTypes.string,
+    }).isRequired,
+
+    /** The chat report this report is linked to */
+    chatReport: reportPropTypes,
+
+    /** Personal details so we can get the ones for the report participants */
+    personalDetails: PropTypes.objectOf(participantPropTypes).isRequired,
+
+    /** Session info for the currently logged in user. */
+    session: PropTypes.shape({
+        /** Currently logged in user email */
+        email: PropTypes.string,
+    }),
+
+    ...withLocalizePropTypes,
+};
+
+const defaultProps = {
+    chatReport: {},
+    session: {
+        email: null,
+    },
+    parentReport: {},
+};
+
+function MoneyReportHeader(props) {
+    const moneyRequestReport = props.report;
+    const isSettled = ReportUtils.isSettled(moneyRequestReport.reportID);
+    const policy = props.policies[`${ONYXKEYS.COLLECTION.POLICY}${props.report.policyID}`];
+    const isPayer =
+        Policy.isAdminOfFreePolicy([policy]) || (ReportUtils.isMoneyRequestReport(moneyRequestReport) && lodashGet(props.session, 'accountID', null) === moneyRequestReport.managerID);
+    // const shouldShowSettlementButton = !isSettled && isPayer;
+    const shouldShowSettlementButton = true;
+    const bankAccountRoute = ReportUtils.getBankAccountRoute(props.chatReport);
+    const shouldShowPaypal = Boolean(lodashGet(props.personalDetails, [moneyRequestReport.managerID, 'payPalMeAddress']));
+    const report = props.report;
+    return (
+        <View style={[styles.pl0]}>
+            <HeaderWithBackButton
+                shouldShowAvatarWithDisplay
+                shouldShowPinButton={false}
+                shouldShowThreeDotsButton={false}
+                threeDotsMenuItems={[
+                    {
+                        icon: Expensicons.Trashcan,
+                        text: props.translate('common.delete'),
+                        onSelected: () => {},
+                    },
+                ]}
+                threeDotsAnchorPosition={styles.threeDotsPopoverOffsetNoCloseButton(props.windowWidth)}
+                report={report}
+                policies={props.policies}
+                personalDetails={props.personalDetails}
+                shouldShowBackButton={props.isSmallScreenWidth}
+                onBackButtonPress={() => Navigation.goBack(ROUTES.HOME, false, true)}
+                shouldShowBorderBottom
+            />
+            <View style={[styles.ph5, styles.pb2]}>
+                <View style={[styles.flexRow, styles.alignItemsCenter]}>
+                    {shouldShowSettlementButton && !props.isSmallScreenWidth && (
+                        <View style={[styles.ml4]}>
+                            <SettlementButton
+                                currency={props.report.currency}
+                                policyID={props.report.policyID}
+                                shouldShowPaypal={shouldShowPaypal}
+                                chatReportID={props.chatReport.reportID}
+                                iouReport={props.report}
+                                onPress={(paymentType) => IOU.payMoneyRequest(paymentType, props.chatReport, props.report)}
+                                enablePaymentsRoute={ROUTES.BANK_ACCOUNT_NEW}
+                                addBankAccountRoute={bankAccountRoute}
+                                shouldShowPaymentOptions
+                            />
+                        </View>
+                    )}
+                </View>
+                {shouldShowSettlementButton && props.isSmallScreenWidth && (
+                    <SettlementButton
+                        currency={props.report.currency}
+                        policyID={props.report.policyID}
+                        shouldShowPaypal={shouldShowPaypal}
+                        chatReportID={props.report.chatReportID}
+                        iouReport={props.report}
+                        onPress={(paymentType) => IOU.payMoneyRequest(paymentType, props.chatReport, props.report)}
+                        enablePaymentsRoute={ROUTES.BANK_ACCOUNT_NEW}
+                        addBankAccountRoute={bankAccountRoute}
+                        shouldShowPaymentOptions
+                    />
+                )}
+            </View>
+        </View>
+    );
+}
+
+MoneyReportHeader.displayName = 'MoneyReportHeader';
+MoneyReportHeader.propTypes = propTypes;
+MoneyReportHeader.defaultProps = defaultProps;
+
+export default compose(
+    withWindowDimensions,
+    withLocalize,
+    withOnyx({
+        chatReport: {
+            key: ({report}) => `${ONYXKEYS.COLLECTION.REPORT}${report.chatReportID}`,
+        },
+        session: {
+            key: ONYXKEYS.SESSION,
+        },
+        parentReport: {
+            key: (props) => `${ONYXKEYS.COLLECTION.REPORT}${props.report.parentReportID}`,
+        },
+    }),
+)(MoneyReportHeader);
