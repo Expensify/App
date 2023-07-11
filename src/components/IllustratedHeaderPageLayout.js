06285743f5f4149ca9ac1beb0702afc3d89bfbbb
rory <rory@expensify.com>
2023-07-11 15:00:50 -0700

    Remove unused import

--- src/components/IllustratedHeaderPageLayout.js
+++ src/components/IllustratedHeaderPageLayout.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {ScrollView, View} from 'react-native';
 import Lottie from 'lottie-react-native';
