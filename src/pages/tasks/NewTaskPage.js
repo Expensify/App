830446893d5a843737a0bbeed46c86ad5f302002
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-07-11 12:10:31 +0800

    disable share somewhere if initiated from a report

--- src/pages/tasks/NewTaskPage.js
+++ src/pages/tasks/NewTaskPage.js
@@ -169,7 +169,8 @@ function NewTaskPage(props) {
                             description={shareDestination.displayName ? shareDestination.subtitle : props.translate('newTaskPage.shareSomewhere')}
                             icon={shareDestination.icons}
                             onPress={() => Navigation.navigate(ROUTES.NEW_TASK_SHARE_DESTINATION)}
-                            shouldShowRightIcon
+                            interactive={!Boolean(props.task.parentReportID)}
+                            shouldShowRightIcon={!Boolean(props.task.parentReportID)}
                         />
                     </View>
                     <FormAlertWithSubmitButton
