deb4faf4cfe56757da4af4fe2f97d09f26dca717
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-07-12 12:41:35 +0200

    Fix validateGithubActions.test.js
    Fixes after merging with main
    
    See: https://github.com/Expensify/App/issues/13604

--- workflow_tests/mocks/validateGithubActionsMocks.js
+++ workflow_tests/mocks/validateGithubActionsMocks.js
@@ -1,7 +1,7 @@
 const utils = require('../utils/utils');
 
 // verify
-const VALIDATEGITHUBACTIONS__VERIFY__CHECKOUT__STEP_MOCK = utils.createMockStep('Checkout', 'Checkout', 'VERIFY', ['fetch-depth'], []);
+const VALIDATEGITHUBACTIONS__VERIFY__CHECKOUT__STEP_MOCK = utils.createMockStep('Checkout', 'Checkout', 'VERIFY');
 const VALIDATEGITHUBACTIONS__VERIFY__SETUP_NODE__STEP_MOCK = utils.createMockStep('Setup Node', 'Setup Node', 'VERIFY', [], []);
 const VALIDATEGITHUBACTIONS__VERIFY__VERIFY_JAVASCRIPT_ACTION_BUILDS__STEP_MOCK = utils.createMockStep(
     'Verify Javascript Action Builds',
