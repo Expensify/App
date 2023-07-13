9854cdfe3ab440da25a4329db08d64b4bb7cd25a
Blazej Kustra <blazej.kustra@swmansion.com>
2023-07-13 10:03:14 +0200

    Adjust IOU

--- src/types/onyx/IOU.ts
+++ src/types/onyx/IOU.ts
@@ -1,13 +1,14 @@
 type Participant = {
-    accountID?: number;
+    accountID: number;
     login?: string;
     isPolicyExpenseChat?: boolean;
     isOwnPolicyExpenseChat?: boolean;
     selected?: boolean;
+    reportID?: string;
 };
 
 type IOU = {
-    id?: string;
+    id: string;
     amount?: number;
     /** Selected Currency Code of the current IOU */
     currency?: string;
