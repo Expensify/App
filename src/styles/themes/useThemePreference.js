74ec10f0731e97980f80d76a27b3138112de3773
Christoph Pader <christoph.pader@outlook.com>
2023-07-13 08:36:10 +0200

    remove Onyx.connect and add comment

--- src/styles/themes/useThemePreference.js
+++ src/styles/themes/useThemePreference.js
@@ -13,27 +13,15 @@ function useThemePreference() {
     useEffect(() => {
         // This is used for getting the system theme, that can be set in the OS's theme settings. This will always return either "light" or "dark" and will update automatically if the OS theme changes.
         const systemThemeSubscription = Appearance.addChangeListener(({colorScheme}) => setSystemTheme(colorScheme));
-        return systemThemeSubscription.remove();
+        return systemThemeSubscription.remove;
     }, []);
 
     useEffect(() => {
         const theme = preferredThemeContext || CONST.THEME.DEFAULT;
 
+        // If the user chooses to use the device theme settings, we need to set the theme preference to the system theme
         if (theme === CONST.THEME.SYSTEM) setThemePreference(systemTheme);
         else setThemePreference(theme);
-
-        // // eslint-disable-next-line rulesdir/prefer-onyx-connect-in-libs
-        // const connectionId = Onyx.connect({
-        //     key: ONYXKEYS.PREFERRED_THEME,
-        //     callback: (newTheme) => {
-        //         const theme = newTheme || CONST.THEME.DEFAULT;
-
-        //         if (theme === CONST.THEME.SYSTEM) setThemePreference(systemTheme);
-        //         else setThemePreference(theme);
-        //     },
-        // });
-
-        // return () => Onyx.disconnect(connectionId);
     }, [preferredThemeContext, systemTheme]);
 
     return themePreference;
