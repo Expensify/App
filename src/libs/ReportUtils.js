5bc675758ac47cee2827b3395f7f8505447375d1
Jack Nam <jack@expensify.com>
2023-07-14 17:50:28 -0700

    prettier

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -2057,7 +2057,7 @@ function shouldReportBeInOptionList(report, currentReportId, isInGSDMode, iouRep
     if (
         !report ||
         !report.reportID ||
-        report.isHidden || 
+        report.isHidden ||
         (_.isEmpty(report.participantAccountIDs) && !isChatThread(report) && !isPublicRoom(report) && !isArchivedRoom(report) && !isMoneyRequestReport(report) && !isTaskReport(report))
     ) {
         return false;
