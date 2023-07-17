7b386bf83c0de3653a0c033cb908af953408fbe1
Georgia Monahan <georgia@expensify.com>
2023-07-17 07:54:51 +0100

    remove old colors

--- src/styles/StyleUtils.js
+++ src/styles/StyleUtils.js
@@ -602,38 +602,6 @@ function getEmojiPickerStyle(isSmallScreenWidth) {
 }
 
 /**
- * Get the random promo color and image for Login page
- *
- * @return {Object}
- */
-function getLoginPagePromoStyle() {
-    const promos = [
-        {
-            backgroundColor: colors.green,
-            backgroundImageUri: `${CONST.CLOUDFRONT_URL}/images/homepage/brand-stories/freeplan_green.svg`,
-        },
-        {
-            backgroundColor: colors.orange,
-            backgroundImageUri: `${CONST.CLOUDFRONT_URL}/images/homepage/brand-stories/freeplan_orange.svg`,
-        },
-        {
-            backgroundColor: colors.pink,
-            backgroundImageUri: `${CONST.CLOUDFRONT_URL}/images/homepage/brand-stories/freeplan_pink.svg`,
-        },
-        {
-            backgroundColor: colors.blue,
-            backgroundImageUri: `${CONST.CLOUDFRONT_URL}/images/homepage/brand-stories/freeplan_blue.svg`,
-        },
-        {
-            backgroundColor: colors.floralwhite,
-            backgroundImageUri: `${CONST.CLOUDFRONT_URL}/images/homepage/brand-stories/cpa-card.svg`,
-            redirectUri: `${CONST.USE_EXPENSIFY_URL}/accountants`,
-        },
-    ];
-    return promos[_.random(0, 4)];
-}
-
-/**
  * Generate the styles for the ReportActionItem wrapper view.
  *
  * @param {Boolean} [isHovered]
@@ -1319,7 +1287,6 @@ export {
     getModalPaddingStyles,
     getFontFamilyMonospace,
     getEmojiPickerStyle,
-    getLoginPagePromoStyle,
     getReportActionItemStyle,
     getMiniReportActionContextMenuWrapperStyle,
     getKeyboardShortcutsModalWidth,
