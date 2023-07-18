fb902fbd84f567f673f3d20a955fce6a347dd6d7
Jack Nam <jack@expensify.com>
2023-07-17 18:54:53 -0700

    Add animation asset

--- src/components/LottieAnimations.js
+++ src/components/LottieAnimations.js
@@ -2,5 +2,6 @@ import Fireworks from '../../assets/animations/Fireworks.json';
 import Hands from '../../assets/animations/Hands.json';
 import PreferencesDJ from '../../assets/animations/PreferencesDJ.json';
 import ReviewingBankInfo from '../../assets/animations/ReviewingBankInfo.json';
+import ExpensifyLounge from '../../assets/animations/ExpensifyLounge.json';
 
-export {Fireworks, Hands, PreferencesDJ, ReviewingBankInfo};
+export {Fireworks, Hands, PreferencesDJ, ReviewingBankInfo, ExpensifyLounge};
