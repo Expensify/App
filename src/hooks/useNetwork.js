6c43c44f104bd938652ef4e957a376d53087d3d2
rory <rory@expensify.com>
2023-07-17 20:31:35 -0700

    Rename useOnNetworkReconnect to useNetwork

--- src/hooks/useNetwork.js
+++ src/hooks/useNetwork.js
@@ -4,7 +4,7 @@ import {NetworkContext} from '../components/OnyxProvider';
 /**
  * @param {Function} onNetworkReconnect
  */
-export default function useOnNetworkReconnect(onNetworkReconnect) {
+export default function useNetwork(onNetworkReconnect) {
     const callback = useRef(onNetworkReconnect);
     callback.current = onNetworkReconnect;
 
