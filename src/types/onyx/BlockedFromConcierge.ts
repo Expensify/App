f81d0671cff5d423c6eb23841c7fc98e70f50d65
Blazej Kustra <blazej.kustra@swmansion.com>
2023-07-18 14:45:11 +0200

    Adjust BlockedFromConcierge

--- src/types/onyx/BlockedFromConcierge.ts
+++ src/types/onyx/BlockedFromConcierge.ts
@@ -2,6 +2,7 @@ type BlockedFromConcierge = {
     /** The date that the user will be unblocked */
     expiresAt: string;
 
+    /** Number of times the user has been blocked. */
     count: 1 | 2 | 3;
 };
 
