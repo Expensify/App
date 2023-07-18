6da3ffb38b5547f00f6a13d2583ca8baa3baf4ff
Blazej Kustra <blazej.kustra@swmansion.com>
2023-07-18 13:12:16 +0200

    Adjust BankAccount

--- src/types/onyx/BankAccount.ts
+++ src/types/onyx/BankAccount.ts
@@ -1,32 +1,72 @@
-type BankAccount = {
+type AdditionalData = {
+    isP2PDebitCard?: boolean;
+    beneficialOwners?: string[];
+    currency?: string;
+    bankName?: string;
+    fieldsType?: string;
+    country?: string;
+};
+
+type AccountData = {
+    /** The masked bank account number */
+    accountNumber?: string;
+
     /** The name of the institution (bank of america, etc */
     addressName?: string;
 
-    /** Alias for addressName */
-    title?: string;
+    /** Can we use this account to pay other people? */
+    allowDebit?: boolean;
 
-    /** The masked bank account number */
-    accountNumber?: string;
+    /** Can we use this account to receive money from other people? */
+    defaultCredit?: boolean;
+
+    /** Is a saving account */
+    isSavings?: boolean;
+
+    /** Return whether or not this bank account has been risk checked */
+    riskChecked?: boolean;
+
+    /** Account routing number */
+    routingNumber?: string;
+
+    /** The status of the bank account */
+    state?: string;
+
+    /** All user emails that have access to this bank account */
+    sharees?: string[];
+
+    processor?: string;
 
     /** The bankAccountID in the bankAccounts db */
     bankAccountID?: number;
 
-    /** Alias for bankAccountID */
-    methodID?: number;
+    /** All data related to the bank account */
+    additionalData?: AdditionalData;
 
     /** The bank account type */
     type?: string;
+};
 
-    isDefault?: boolean;
+type BankAccount = {
+    /** The bank account type */
+    accountType?: string;
 
     /** string like 'Account ending in XXXX' */
     description?: string;
 
+    isDefault?: boolean;
+
     /** string like 'bankAccount-{<bankAccountID>}' where <bankAccountID> is the bankAccountID */
     key?: string;
 
+    /** Alias for bankAccountID */
+    methodID?: number;
+
+    /** Alias for addressName */
+    title?: string;
+
     /** All data related to the bank account */
-    accountData?: Record<string, unknown>;
+    accountData?: AccountData;
 };
 
 export default BankAccount;
