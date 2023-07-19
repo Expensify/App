5891d85c734c25b4999820cfebfdd0c44321ea78
Andrew Rosiclair <arosiclair@expensify.com>
2023-07-19 16:48:49 -0400

    implement Parcelable for Message

--- android/app/src/main/java/com/expensify/chat/customairshipextender/NotificationCache.java
+++ android/app/src/main/java/com/expensify/chat/customairshipextender/NotificationCache.java
@@ -1,7 +1,13 @@
 package com.expensify.chat.customairshipextender;
 
 import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 import androidx.core.app.Person;
+import androidx.core.graphics.drawable.IconCompat;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -40,7 +46,7 @@ public class NotificationCache {
         public Map<String, Bitmap> bitmapIcons = new HashMap<>();
         public int prevNotificationID = -1;
 
-        public static class Message {
+        public static class Message implements Parcelable {
             public Person person;
             public String text;
             public long time;
@@ -50,6 +56,52 @@ public class NotificationCache {
                 this.text = text;
                 this.time = time;
             }
+
+            Message(Parcel parcel) {
+                this.person = convertToPerson(parcel.readBundle());
+                this.text = parcel.readString();
+                this.time = parcel.readLong();
+            }
+
+            @Override
+            public void writeToParcel(@NonNull Parcel parcel, int i) {
+                parcel.writeBundle(convertToBundle(person));
+                parcel.writeString(text);
+                parcel.writeLong(time);
+            }
+
+            @Override
+            public int describeContents() {
+                return 0;
+            }
+
+            public static final Parcelable.Creator CREATOR = new Parcelable.Creator()
+            {
+                public Message createFromParcel(Parcel in) {
+                    return new Message(in);
+                }
+
+                @Override
+                public Object[] newArray(int size) {
+                    return new Person[size];
+                }
+            };
         }
+    }
+
+    private static Bundle convertToBundle(Person p) {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("icon", (Parcelable) p.getIcon());
+        bundle.putString("key", p.getKey());
+        bundle.putString("name", p.getName().toString());
+        return bundle;
+    }
+
+    private static Person convertToPerson(Bundle b) {
+        return new Person.Builder()
+                .setIcon((IconCompat) b.getParcelable("icon"))
+                .setKey(b.getString("key"))
+                .setName(b.getString("name"))
+                .build();
     }
 }
