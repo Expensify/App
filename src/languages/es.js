53c6565741e0f4936e4dd4957f0c3ce60ae23c8c
charan h s <charan.hs@gmail.com>
2023-07-21 02:30:51 +0530

    update error message

--- src/languages/es.js
+++ src/languages/es.js
@@ -165,6 +165,7 @@ export default {
         sizeNotMet: 'El archivo adjunto debe ser mas grande que 240 bytes.',
         wrongFileType: 'El tipo del archivo adjunto es incorrecto',
         notAllowedExtension: 'Este tipo de archivo no está permitido',
+        folderNotAllowedMessage: 'No se permite subir carpetas.'
     },
     avatarCropModal: {
         title: 'Editar foto',
