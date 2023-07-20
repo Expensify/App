5ab332b9b0e9d60c91e5d13859975f10906cf7d1
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-07-19 15:23:04 -1000

    Refactor CollapsibleSection

--- src/components/CollapsibleSection/index.js
+++ src/components/CollapsibleSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import Collapsible from './Collapsible';
@@ -17,54 +17,41 @@ const propTypes = {
     children: PropTypes.node.isRequired,
 };
 
-class CollapsibleSection extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toggleSection = this.toggleSection.bind(this);
-        this.state = {
-            isExpanded: false,
-        };
+function CollapsibleSection() {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    function toggleSection() {
+        setIsExpanded((prevIsExpanded) => !prevIsExpanded);
     }
 
-    /**
-     * Expands/collapses the section
-     */
-    toggleSection() {
-        this.setState((prevState) => ({
-            isExpanded: !prevState.isExpanded,
-        }));
-    }
-
-    render() {
-        const src = this.state.isExpanded ? Expensicons.UpArrow : Expensicons.DownArrow;
-
-        return (
-            <View style={styles.mt4}>
-                <PressableWithFeedback
-                    onPress={this.toggleSection}
-                    style={[styles.pb4, styles.flexRow]}
-                    accessibilityRole={CONST.ACCESSIBILITY_ROLE.BUTTON}
-                    accessibilityLabel={this.props.title}
-                    hoverDimmingValue={1}
-                    pressDimmingValue={0.2}
+    const src = isExpanded ? Expensicons.UpArrow : Expensicons.DownArrow;
+    return (
+        <View style={styles.mt4}>
+            <PressableWithFeedback
+                onPress={toggleSection}
+                style={[styles.pb4, styles.flexRow]}
+                accessibilityRole={CONST.ACCESSIBILITY_ROLE.BUTTON}
+                accessibilityLabel={this.props.title}
+                hoverDimmingValue={1}
+                pressDimmingValue={0.2}
+            >
+                <Text
+                    selectable={false}
+                    style={[styles.flex1, styles.textStrong]}
                 >
-                    <Text
-                        selectable={false}
-                        style={[styles.flex1, styles.textStrong]}
-                    >
-                        {this.props.title}
-                    </Text>
-                    <Icon src={src} />
-                </PressableWithFeedback>
-                <View style={styles.collapsibleSectionBorder} />
+                    {this.props.title}
+                </Text>
+                <Icon src={src} />
+            </PressableWithFeedback>
+            <View style={styles.collapsibleSectionBorder} />
 
-                <Collapsible isOpened={this.state.isExpanded}>
-                    <View>{this.props.children}</View>
-                </Collapsible>
-            </View>
-        );
-    }
+            <Collapsible isOpened={.isExpanded}>
+                <View>{this.props.children}</View>
+            </Collapsible>
+        </View>
+    );
 }
 
+CollapsibleSection.displayName = 'CollapsibleSection';
 CollapsibleSection.propTypes = propTypes;
 export default CollapsibleSection;
