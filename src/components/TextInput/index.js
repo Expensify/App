232bf8dbf72b2a6e75babcb4869c62779682f4ae
Akinwale Ariwodola <akinwale@gmail.com>
2023-07-20 15:31:22 +0100

    add comments for explanation

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -17,6 +17,11 @@ class TextInput extends React.Component {
             this.textInput.setAttribute('name', this.props.name);
         }
 
+        /**
+         * A visibliity listener responsible for re-establishing focus on the TextInput in cases where a user
+         * switches away from an MWeb tab and back. This forces the soft keyboard to be activated on Android
+         * after switching back to the tab. See https://github.com/Expensify/App/issues/20690 for more details.
+         */
         this.unsubscribeVisibilityListener = Visibility.onVisibilityChange(() => {
             if (!Visibility.isVisible() || !this.textInput || DomUtils.getActiveElement() !== this.textInput) {
                 return;
