94683d9bdf0ce22c1cd7dc238d5e424b7722f8c0
Nam Le <yentth237@gmail.com>
2023-07-21 15:18:10 +0700

    add comment

--- src/libs/actions/PersistedRequests.js
+++ src/libs/actions/PersistedRequests.js
@@ -25,6 +25,10 @@ function save(requestsToPersist) {
  * @param {Object} requestToRemove
  */
 function remove(requestToRemove) {
+    /**
+     * Removes the first matching request, even if there are multiple matching requests.
+     * Because if multiple identical requests are queued. We want to keep the same requests then see more here: https://github.com/Expensify/App/issues/19640
+     */
     const index = _.findIndex(persistedRequests, (persistedRequest) => _.isEqual(persistedRequest, requestToRemove));
     if (index !== -1) {
         persistedRequests.splice(index, 1);
