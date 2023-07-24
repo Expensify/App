edc84532de63255c9a6e76be864132842683e2e1
s-alves10 <sangar.work1028@gmail.com>
2023-07-24 13:37:16 -0500

    fix: lint error

--- src/pages/workspace/WorkspaceInvitePage.js
+++ src/pages/workspace/WorkspaceInvitePage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef, useMemo} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import {View} from 'react-native';
 import {withOnyx} from 'react-native-onyx';
