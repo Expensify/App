77bd9aa14a0449bd845cdbb7089ffd4898304bbe
tienifr <christianwen18@gmail.com>
2023-07-24 12:31:49 +0700

    Revert "remove isDeletedAction check"
    This reverts commit 9bd655d21724ebbbbe812209534c52c159c506fe.

--- src/libs/ReportActionsUtils.js
+++ src/libs/ReportActionsUtils.js
@@ -346,7 +346,7 @@ function shouldReportActionBeVisible(reportAction, key) {
  * @returns {Boolean}
  */
 function shouldReportActionBeVisibleAsLastAction(reportAction) {
-    return shouldReportActionBeVisible(reportAction, reportAction.reportActionID) && !isWhisperAction(reportAction);
+    return shouldReportActionBeVisible(reportAction, reportAction.reportActionID) && !isWhisperAction(reportAction) && !isDeletedAction(reportAction);
 }
 
 /**
