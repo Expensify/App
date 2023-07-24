416b9701bef3fe222f87ffa214aea31804bd3d0a
Tim Golen <tgolen@expensify.com>
2023-07-24 14:29:33 -0600

    Fix expiration calculation, improve comments, document token shape

--- src/libs/MapboxAccessTokenManager.js
+++ src/libs/MapboxAccessTokenManager.js
@@ -8,7 +8,7 @@ let currentToken;
 let refreshTimeoutID;
 
 const refreshToken = () => {
-    // Cancel any previous timeouts so that there is only one request to get a token at a time
+    // Cancel any previous timeouts so that there is only one request to get a token at a time.
     if (refreshTimeoutID) {
         clearTimeout(refreshTimeoutID);
     }
@@ -18,12 +18,12 @@ const refreshToken = () => {
 const hasTokenExpired = () => {
     const now = moment();
     const expiration = moment(currentToken.expiration);
-    const minutesUntilTokenExpires = now.diff(expiration, 'minutes');
+    const minutesUntilTokenExpires = expiration.diff(now, 'minutes');
     return minutesUntilTokenExpires < 0;
 };
 
 const clearToken = () => {
-    // Use Onyx.set() to delete the key from Onyx, which will trigger the flow with an empty token (where a new token is retrieved from the API)
+    // Use Onyx.set() to delete the key from Onyx, which will trigger a new token to be retrieved from the API.
     Onyx.set(ONYXKEYS.MAPBOX_ACCESS_TOKEN, null);
 };
 
@@ -31,15 +31,21 @@ const init = () => {
     // When the token changes in Onyx, the expiration needs to be checked so a new token can be retrieved.
     Onyx.connect({
         key: ONYXKEYS.MAPBOX_ACCESS_TOKEN,
+        /**
+         * @param {Object} token
+         * @param {String} token.token
+         * @param {String} token.expiration
+         * @param {String[]} [token.errors]
+         */
         callback: (token) => {
-            // token is an object. If it is falsy or an empty object, the token needs to be retrieved from the API.
-            // The API sets a token in Onyx with a 30 minute expiration
+            // token is an object with. If it is falsy or an empty object, the token needs to be retrieved from the API.
+            // The API sets a token in Onyx with a 30 minute expiration.
             if (!token || _.size(token) === 0) {
                 // @TODO call the API GetMapboxAccessToken()
                 return;
             }
 
-            // Store the token in a place where the AppState callback can also access it
+            // Store the token in a place where the AppState callback can also access it.
             currentToken = token;
 
             if (hasTokenExpired()) {
@@ -52,7 +58,7 @@ const init = () => {
         },
     });
 
-    // WHen the app becomes active (eg. after being in the background), check if the token has expired
+    // When the app becomes active (eg. after being in the background), check if the token has expired.
     AppState.addEventListener('change', (nextAppState) => {
         if (nextAppState === CONST.APP_STATE.ACTIVE) {
             if (hasTokenExpired()) {
