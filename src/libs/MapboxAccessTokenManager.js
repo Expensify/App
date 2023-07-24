b0fed712a94f3ebfb8a43207975a4cd97e7ce4e9
Tim Golen <tgolen@expensify.com>
2023-07-24 14:05:41 -0600

    Handle expired tokens

--- src/libs/MapboxAccessTokenManager.js
+++ src/libs/MapboxAccessTokenManager.js
@@ -1,7 +1,7 @@
 import _ from 'underscore';
 import moment from 'moment';
 import Onyx from 'react-native-onyx';
-import ONYXKEYS from "../ONYXKEYS";
+import ONYXKEYS from '../ONYXKEYS';
 
 const init = () => {
     Onyx.connect({
@@ -12,7 +12,17 @@ const init = () => {
                 // @TODO call the API GetMapboxAccessToken()
                 return;
             }
-        }
+
+            const now = moment();
+            const expiration = moment(token.expiration);
+            const minutesUntilTokenExpires = now.diff(expiration, 'minutes');
+            const tokenHasExpired = minutesUntilTokenExpires < 0;
+            if (tokenHasExpired) {
+                // Use set() to delete the key from Onyx, which will trigger the flow above
+                Onyx.set(ONYXKEYS.MAPBOX_ACCESS_TOKEN, null);
+                return;
+            }
+        },
     });
 };
 
