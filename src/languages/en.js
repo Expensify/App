2b2d7360af156e501b34a20f852b4e03046b28d0
AkshatJoshi-18 <akshatjoshi8630@gmail.com>
2023-07-24 10:47:32 +0530

    Update en.js

--- src/languages/en.js
+++ src/languages/en.js
@@ -297,7 +297,7 @@ export default {
         beginningOfChatHistoryPolicyExpenseChatPartTwo: ' and ',
         beginningOfChatHistoryPolicyExpenseChatPartThree: ' starts here! 🎉 This is the place to chat, request money and settle up.',
         chatWithAccountManager: 'Chat with your account manager here',
-        sayHello: 'hi!',
+        sayHello: 'Say hello!',
         usePlusButton: '\n\nYou can also use the + button below to send or request money!',
     },
     reportAction: {
