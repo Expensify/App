8b401796a8577e705315962d278f2433ec7bacc1
Scott Deeter <deetergp@users.noreply.github.com>
2023-07-25 11:05:22 -0400

    Merge pull request #23551 from Expensify/revert-22727-fix-19640-notification-inconsistently
    [CP Staging] Revert "fix remove only first request"
    
    (cherry picked from commit 398f93d5779e6ea3f0db4806880632f20cd66d17)

--- src/libs/actions/PersistedRequests.js
+++ src/libs/actions/PersistedRequests.js
@@ -25,15 +25,7 @@ function save(requestsToPersist) {
  * @param {Object} requestToRemove
  */
 function remove(requestToRemove) {
-    /**
-     * We only remove the first matching request because the order of requests matters.
-     * If we were to remove all matching requests, we can end up with a final state that is different than what the user intended.
-     */
-    const index = _.findIndex(persistedRequests, (persistedRequest) => _.isEqual(persistedRequest, requestToRemove));
-    if (index !== -1) {
-        persistedRequests.splice(index, 1);
-    }
-
+    persistedRequests = _.reject(persistedRequests, (persistedRequest) => _.isEqual(persistedRequest, requestToRemove));
     Onyx.set(ONYXKEYS.PERSISTED_REQUESTS, persistedRequests);
 }
 
