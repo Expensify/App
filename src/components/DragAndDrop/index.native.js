9851ad1fb85a9e31570ad39c74b1ced4261aa9f1
rory <rory@expensify.com>
2023-07-25 13:28:45 -0700

    Add prop types on default components

--- src/components/DragAndDrop/index.native.js
+++ src/components/DragAndDrop/index.native.js
@@ -1,5 +1,8 @@
+import DragAndDropPropTypes from './dragAndDropPropTypes';
+
 const DragAndDrop = (props) => props.children;
 
+DragAndDrop.propTypes = DragAndDropPropTypes;
 DragAndDrop.displayName = 'DragAndDrop';
 
 export default DragAndDrop;
