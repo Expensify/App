ff091da26c1eb3149bdbf73147d2f1849bff5df5
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2023-07-25 11:27:34 +0530

    Remove focusAndUpdateMultilineInputRange

--- src/pages/tasks/TaskTitlePage.js
+++ src/pages/tasks/TaskTitlePage.js
@@ -13,7 +13,6 @@ import styles from '../../styles/styles';
 import reportPropTypes from '../reportPropTypes';
 import compose from '../../libs/compose';
 import * as Task from '../../libs/actions/Task';
-import focusAndUpdateMultilineInputRange from '../../libs/focusAndUpdateMultilineInputRange';
 import CONST from '../../CONST';
 
 const propTypes = {
@@ -64,7 +63,7 @@ function TaskTitlePage(props) {
     return (
         <ScreenWrapper
             includeSafeAreaPaddingBottom={false}
-            onEntryTransitionEnd={() => focusAndUpdateMultilineInputRange(inputRef.current)}
+            onEntryTransitionEnd={() => inputRef.current && inputRef.current.focus()}
             shouldEnableMaxHeight
         >
             <HeaderWithBackButton title={props.translate('task.task')} />
