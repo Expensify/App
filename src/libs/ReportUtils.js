ee5d96e43e1c0c7caea8519a29c51abb2c117a42
Carlos Martins <cmartins@expensify.com>
2023-07-26 16:21:45 -0600

    use stateNum to determine report state

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -236,7 +236,7 @@ function canEditReportAction(reportAction) {
  */
 function isSettled(reportID) {
     const report = lodashGet(allReports, `${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {});
-    return !_.isEmpty(report) && !report.hasOutstandingIOU && !report.isWaitingOnBankAccount;
+    return !_.isEmpty(report) && !report.isWaitingOnBankAccount && report.stateNum > CONST.REPORT.STATE_NUM.PROCESSING;
 }
 
 /**
