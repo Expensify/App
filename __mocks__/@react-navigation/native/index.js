4373de1518ab9ded86596152f21399571ba58074
rory <rory@expensify.com>
2023-07-27 16:03:28 -0700

    Fix tests

--- __mocks__/@react-navigation/native/index.js
+++ __mocks__/@react-navigation/native/index.js
@@ -1,6 +1,6 @@
-function useIsFocused() {
-    return true;
-}
+import {useIsFocused as realUseIsFocused} from '@react-navigation/native';
+
+const useIsFocused = process.env.NODE_ENV === 'test' ? realUseIsFocused : () => true;
 
 export * from '@react-navigation/core';
 export * from '@react-navigation/native';
