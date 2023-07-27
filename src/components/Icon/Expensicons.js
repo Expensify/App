7cef2e71a734f1028e4cf89d61c1b9f6e5fc930c
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2023-07-27 13:00:08 +0530

    Re-add Heart Icon with lint fix :(

--- src/components/Icon/Expensicons.js
+++ src/components/Icon/Expensicons.js
@@ -49,7 +49,7 @@ import Gallery from '../../../assets/images/gallery.svg';
 import Gear from '../../../assets/images/gear.svg';
 import Globe from '../../../assets/images/globe.svg';
 import Hashtag from '../../../assets/images/hashtag.svg';
-import Heart from '../../../assets/images/emojiCategoryIcons/heart.svg';
+import Heart from '../../../assets/images/heart.svg';
 import History from '../../../assets/images/history.svg';
 import Hourglass from '../../../assets/images/hourglass.svg';
 import ImageCropCircleMask from '../../../assets/images/image-crop-circle-mask.svg';
