11b8334824e5fe86c9069d916c15be4508d4e877
charan h s <charanengg08@gmail.com>
2023-07-27 23:41:48 +0530

    Error message update

--- src/languages/es.js
+++ src/languages/es.js
@@ -167,7 +167,7 @@ export default {
         sizeNotMet: 'El archivo adjunto debe ser mas grande que 240 bytes.',
         wrongFileType: 'El tipo del archivo adjunto es incorrecto',
         notAllowedExtension: 'Este tipo de archivo no está permitido',
-        folderNotAllowedMessage: 'No se permite subir carpetas.',
+        folderNotAllowedMessage: 'No se permite subir carpetas. Pruebe con un archivo diferente.',
     },
     avatarCropModal: {
         title: 'Editar foto',
