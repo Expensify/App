8a4dcf5b7a383c364c28c5df1ecd2104279c0e3e
rory <rory@expensify.com>
2023-07-27 16:17:32 -0700

    Add comment in mock

--- __mocks__/@react-navigation/native/index.js
+++ __mocks__/@react-navigation/native/index.js
@@ -1,5 +1,6 @@
 import {useIsFocused as realUseIsFocused} from '@react-navigation/native';
 
+// We only want this mocked for storybook, not jest
 const useIsFocused = process.env.NODE_ENV === 'test' ? realUseIsFocused : () => true;
 
 export * from '@react-navigation/core';
