578c22b43a8fbee1d1942f769ea22f71c6353941
rory <rory@expensify.com>
2023-07-27 17:05:51 -0700

    Remove subscriber optimization

--- src/components/DragAndDrop/Consumer/index.js
+++ src/components/DragAndDrop/Consumer/index.js
@@ -7,20 +7,10 @@ function DragAndDropConsumer({children, dropZoneID, onDrop}) {
     const DragAndDropContext = DragAndDropUtils.getDragAndDropContext(dropZoneID);
     const {isDraggingOver} = useContext(DragAndDropContext);
 
-    const onDropRef = useRef(onDrop);
-    onDropRef.current = onDrop;
     useEffect(() => {
-        // Internal function ensures that we only register the onDrop listener once for this consumer,
-        // even if the onDrop function passed in changes
-        const onDropCallback = (e) => {
-            if (!onDropRef.current) {
-                return;
-            }
-            onDropRef.current(e);
-        };
-        DragAndDropUtils.registerOnDropCallback(dropZoneID, onDropCallback);
-        return () => DragAndDropUtils.deregisterOnDropCallback(dropZoneID, onDropCallback);
-    }, [dropZoneID]);
+        DragAndDropUtils.registerOnDropCallback(dropZoneID, onDrop);
+        return () => DragAndDropUtils.deregisterOnDropCallback(dropZoneID, onDrop);
+    }, [dropZoneID, onDrop]);
 
     if (!isDraggingOver) {
         return null;
