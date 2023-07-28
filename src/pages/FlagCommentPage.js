24f5b11c559d22d77574fe08660033cbbc96fbda
dukenv0307 <dukenv0307@gmail.com>
2023-07-28 11:15:46 +0700

    clear condition

--- src/pages/FlagCommentPage.js
+++ src/pages/FlagCommentPage.js
@@ -50,9 +50,9 @@ const propTypes = {
 const defaultProps = {
     reportActions: {},
     report: {
-        isLoadingReportActions: true,
+        isLoadingReportActions: false,
     },
-    isLoadingReportData: true,
+    isLoadingReportData: false,
 };
 
 /**
@@ -159,8 +159,11 @@ function FlagCommentPage(props) {
         />
     ));
 
-    const shouldShowLoading =
-        (props.isLoadingReportData && _.isEmpty(props.report)) || _.isEmpty(props.reportActions) || (props.report.isLoadingReportActions && _.isEmpty(getActionToFlag()));
+    const isLoadingInitialReport = props.isLoadingReportData && _.isEmpty(props.report);
+    const isLoadingInitialReportActions = _.isEmpty(props.reportActions) || (props.report.isLoadingReportActions && _.isEmpty(getActionToFlag()));
+    const isExistReport = isLoadingInitialReport || (!_.isEmpty(props.report) && props.report.reportID);
+
+    const shouldShowLoading = (isLoadingInitialReport || isLoadingInitialReportActions) && isExistReport;
     if (shouldShowLoading) {
         return <FullscreenLoadingIndicator />;
     }
