cdb98217bae791078cedeb8dc9022496d557d6aa
staszekscp <mariusz.stanisz@swmansion.com>
2023-07-28 15:17:05 +0200

    partial fix

--- src/components/OptionsSelector/index.js
+++ src/components/OptionsSelector/index.js
@@ -1,14 +1,16 @@
 import React, {forwardRef} from 'react';
 import BaseOptionsSelector from './BaseOptionsSelector';
+import withWindowDimensions from '../withWindowDimensions';
 
 const OptionsSelector = forwardRef((props, ref) => (
     <BaseOptionsSelector
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
         ref={ref}
+        shouldDelayFocus={!props.isSmallScreenWidth}
     />
 ));
 
 OptionsSelector.displayName = 'OptionsSelector';
 
-export default OptionsSelector;
+export default withWindowDimensions(OptionsSelector);
