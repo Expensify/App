912356e9a1ba32223ef51e0e076e56a9afd8bf7a
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2023-07-30 19:43:54 +0100

    refactor useDelayToggleButtonState hook

--- src/hooks/useDelayToggleButtonState.js
+++ src/hooks/useDelayToggleButtonState.js
@@ -1,39 +1,19 @@
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 
-/**
- * Hook that manages a delayed toggle button state.
- *
- * @returns {Object} - An object containing isDelayButtonStateComplete and toggleDelayButtonState.
- */
 export default function useDelayToggleButtonState() {
-    // State to hold the button completion status
     const [isDelayButtonStateComplete, setIsDelayButtonStateComplete] = useState(false);
-    // Ref to hold the identifier of the setTimeout function
-    const resetButtonStateCompleteTimer = useRef(null);
+    const toggleDelayButtonState = useCallback(() => setIsDelayButtonStateComplete(true), []);
 
-    /**
-     * Function to toggle the button state.
-     * It sets the button state to complete and after 1.8s, reverts it to incomplete.
-     */
-    function toggleDelayButtonState() {
-        setIsDelayButtonStateComplete(true);
-        resetButtonStateCompleteTimer.current = setTimeout(() => {
+    useEffect(() => {
+        if (!isDelayButtonStateComplete) {
+            return;
+        }
+
+        const timer = setTimeout(() => {
             setIsDelayButtonStateComplete(false);
         }, 1800);
-    }
-
-    /**
-     * Effect to clean up the timer when the component unmounts.
-     */
-    // eslint-disable-next-line arrow-body-style
-    useEffect(() => {
-        return () => {
-            if (!resetButtonStateCompleteTimer.current) {
-                return;
-            }
-            clearTimeout(resetButtonStateCompleteTimer.current);
-        };
-    }, []);
+        return () => clearTimeout(timer);
+    }, [isDelayButtonStateComplete]);
 
     return {isDelayButtonStateComplete, toggleDelayButtonState};
 }
