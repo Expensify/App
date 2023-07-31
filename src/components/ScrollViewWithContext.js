fef0ab6f52e831ea19db8002d88e2804b984910a
Parshvi16 <91120214+Parshvi16@users.noreply.github.com>
2023-07-31 16:26:38 +0530

    fix: run prettier, add useRef

--- src/components/ScrollViewWithContext.js
+++ src/components/ScrollViewWithContext.js
@@ -1,54 +1,52 @@
-import React, { useState } from 'react';
-import { ScrollView } from 'react-native';
+import React, {useState, useRef} from 'react';
+import {ScrollView} from 'react-native';
 
 const MIN_SMOOTH_SCROLL_EVENT_THROTTLE = 16;
 
 const ScrollContext = React.createContext();
-const defaultScrollViewRef = React.createRef();
 
 // eslint-disable-next-line react/forbid-foreign-prop-types
 const propTypes = ScrollView.propTypes;
 
-const ScrollViewWithContext = React.forwardRef(
-  ({ onScroll, scrollEventThrottle, children, innerRef, ...restProps }, ref) => {
+function ScrollViewWithContext({onScroll, scrollEventThrottle, children, innerRef, ...restProps}, ref) {
     const [contentOffsetY, setContentOffsetY] = useState(0);
+    const defaultScrollViewRef = useRef();
     const scrollViewRef = innerRef || ref || defaultScrollViewRef;
 
     const setContextScrollPosition = (event) => {
-      if (onScroll) {
-        onScroll(event);
-      }
-      setContentOffsetY(event.nativeEvent.contentOffset.y);
+        if (onScroll) {
+            onScroll(event);
+        }
+        setContentOffsetY(event.nativeEvent.contentOffset.y);
     };
 
     return (
-      <ScrollView
-        ref={scrollViewRef}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...restProps}
-        onScroll={setContextScrollPosition}
-        scrollEventThrottle={scrollEventThrottle || MIN_SMOOTH_SCROLL_EVENT_THROTTLE}
-      >
-        <ScrollContext.Provider
-          value={{
-            scrollViewRef,
-            contentOffsetY,
-          }}
+        <ScrollView
+            ref={scrollViewRef}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...restProps}
+            onScroll={setContextScrollPosition}
+            scrollEventThrottle={scrollEventThrottle || MIN_SMOOTH_SCROLL_EVENT_THROTTLE}
         >
-          {children}
-        </ScrollContext.Provider>
-      </ScrollView>
+            <ScrollContext.Provider
+                value={{
+                    scrollViewRef,
+                    contentOffsetY,
+                }}
+            >
+                {children}
+            </ScrollContext.Provider>
+        </ScrollView>
     );
-  }
-);
+}
 
-ScrollViewWithContext.propTypes = propTypes
+ScrollViewWithContext.propTypes = propTypes;
 
 export default React.forwardRef((props, ref) => (
-  <ScrollViewWithContext
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...props}
-      innerRef={ref}
-  />
+    <ScrollViewWithContext
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        innerRef={ref}
+    />
 ));
-export { ScrollContext };
+export {ScrollContext};
