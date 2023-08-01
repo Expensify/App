51986858c3c381dd0052d755b8d7097532351cae
dhairyasenjaliya <dhairyasenjaliya@gmail.com>
2023-08-01 14:46:17 +0530

    Generate expensChat for principal intro

--- src/libs/actions/TeachersUnite.js
+++ src/libs/actions/TeachersUnite.js
@@ -1,28 +1,27 @@
-// import _ from 'underscore';
-// import Onyx from 'react-native-onyx';
-// import lodashGet from 'lodash/get';
-// import ONYXKEYS from '../../ONYXKEYS';
-// import * as API from '../API';
+import _ from 'underscore';
+import Onyx from 'react-native-onyx';
+import lodashGet from 'lodash/get';
+import ONYXKEYS from '../../ONYXKEYS';
+import * as API from '../API';
 import Navigation from '../Navigation/Navigation';
 import CONST from '../../CONST';
-// import ROUTES from '../../ROUTES';
-// import * as ReportUtils from '../ReportUtils';
+import * as ReportUtils from '../ReportUtils';
 
-// let sessionEmail = '';
-// let sessionAccountID = 0;
-// Onyx.connect({
-//     key: ONYXKEYS.SESSION,
-//     callback: (val) => {
-//         sessionEmail = lodashGet(val, 'email', '');
-//         sessionAccountID = lodashGet(val, 'accountID', 0);
-//     },
-// });
+let sessionEmail = '';
+let sessionAccountID = 0;
+Onyx.connect({
+    key: ONYXKEYS.SESSION,
+    callback: (val) => {
+        sessionEmail = lodashGet(val, 'email', '');
+        sessionAccountID = lodashGet(val, 'accountID', 0);
+    },
+});
 
-// let allPersonalDetails;
-// Onyx.connect({
-//     key: ONYXKEYS.PERSONAL_DETAILS_LIST,
-//     callback: (val) => (allPersonalDetails = val),
-// });
+let allPersonalDetails;
+Onyx.connect({
+    key: ONYXKEYS.PERSONAL_DETAILS_LIST,
+    callback: (val) => (allPersonalDetails = val),
+});
 
 /**
  * @param {String} reportID
@@ -66,108 +65,108 @@ function referTeachersUniteVolunteer(reportID, firstName, phoneOrEmail, lastName
 // Will be removed once we start using & remove this comment as well :)
 // eslint-disable-next-line no-unused-vars
 function createExpenseChatSchoolPrincipal(firstName, email, lastName = '') {
-    // const policyName = 'TeacherUniteSchoolPrincipal';
+    const policyName = 'TeacherUniteSchoolPrincipal';
     const policyID = CONST.TEACHER_UNITE.SCHOOL_PRINCIPAL_POLICY_ID;
 
-    // const {expenseChatReportID, expenseChatData, expenseReportActionData, expenseCreatedReportActionID} = ReportUtils.buildOptimisticExpenseChatForSchoolPrincipal(policyID, policyName);
+    const {expenseChatReportID, expenseChatData, expenseReportActionData, expenseCreatedReportActionID} = ReportUtils.buildOptimisticExpenseChatForSchoolPrincipal(policyID, policyName);
 
-    // API.write(
-    //     'AddSchoolPrincipal',
-    //     {
-    //         firstName,
-    //         lastName,
-    //         email,
-    //         expenseCreatedReportActionID,
-    //         policyExpenseChatReportID: expenseChatReportID,
-    //         policyExpenseCreatedReportActionID: expenseCreatedReportActionID,
-    //     },
-    //     {
-    //         optimisticData: [
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.POLICY}${policyID}`,
-    //                 value: {
-    //                     id: policyID,
-    //                     type: CONST.POLICY.TYPE.FREE,
-    //                     // name: workspaceName,
-    //                     role: CONST.POLICY.ROLE.ADMIN,
-    //                     owner: sessionEmail,
-    //                     outputCurrency: lodashGet(allPersonalDetails, [sessionAccountID, 'localCurrencyCode'], CONST.CURRENCY.USD),
-    //                     pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
-    //                 },
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.POLICY_MEMBERS}${policyID}`,
-    //                 value: {
-    //                     [sessionAccountID]: {
-    //                         role: CONST.POLICY.ROLE.ADMIN,
-    //                         errors: {},
-    //                     },
-    //                 },
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.REPORT}${expenseChatReportID}`,
-    //                 value: {
-    //                     pendingFields: {
-    //                         addWorkspaceRoom: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
-    //                     },
-    //                     ...expenseChatData,
-    //                 },
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${expenseChatReportID}`,
-    //                 value: expenseReportActionData,
-    //             },
-    //         ],
-    //         successData: [
-    //             {
-    //                 onyxMethod: Onyx.METHOD.MERGE,
-    //                 key: `${ONYXKEYS.COLLECTION.POLICY}${policyID}`,
-    //                 value: {pendingAction: null},
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.MERGE,
-    //                 key: `${ONYXKEYS.COLLECTION.REPORT}${expenseChatReportID}`,
-    //                 value: {
-    //                     pendingFields: {
-    //                         addWorkspaceRoom: null,
-    //                     },
-    //                     pendingAction: null,
-    //                 },
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.MERGE,
-    //                 key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${expenseChatReportID}`,
-    //                 value: {
-    //                     [_.keys(expenseChatData)[0]]: {
-    //                         pendingAction: null,
-    //                     },
-    //                 },
-    //             },
-    //         ],
-    //         failureData: [
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.POLICY_MEMBERS}${policyID}`,
-    //                 value: null,
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.REPORT}${expenseChatReportID}`,
-    //                 value: null,
-    //             },
-    //             {
-    //                 onyxMethod: Onyx.METHOD.SET,
-    //                 key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${expenseChatReportID}`,
-    //                 value: null,
-    //             },
-    //         ],
-    //     },
-    // );
-    Navigation.dismissModal(policyID);
+    API.write(
+        'AddSchoolPrincipal',
+        {
+            firstName,
+            lastName,
+            email,
+            expenseCreatedReportActionID,
+            policyExpenseChatReportID: expenseChatReportID,
+            policyExpenseCreatedReportActionID: expenseCreatedReportActionID,
+        },
+        {
+            optimisticData: [
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.POLICY}${policyID}`,
+                    value: {
+                        id: policyID,
+                        type: CONST.POLICY.TYPE.FREE,
+                        name: policyName,
+                        role: CONST.POLICY.ROLE.ADMIN,
+                        owner: sessionEmail,
+                        outputCurrency: lodashGet(allPersonalDetails, [sessionAccountID, 'localCurrencyCode'], CONST.CURRENCY.USD),
+                        pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
+                    },
+                },
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.POLICY_MEMBERS}${policyID}`,
+                    value: {
+                        [sessionAccountID]: {
+                            role: CONST.POLICY.ROLE.ADMIN,
+                            errors: {},
+                        },
+                    },
+                },
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.REPORT}${expenseChatReportID}`,
+                    value: {
+                        pendingFields: {
+                            addWorkspaceRoom: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
+                        },
+                        ...expenseChatData,
+                    },
+                },
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${expenseChatReportID}`,
+                    value: expenseReportActionData,
+                },
+            ],
+            successData: [
+                {
+                    onyxMethod: Onyx.METHOD.MERGE,
+                    key: `${ONYXKEYS.COLLECTION.POLICY}${policyID}`,
+                    value: {pendingAction: null},
+                },
+                {
+                    onyxMethod: Onyx.METHOD.MERGE,
+                    key: `${ONYXKEYS.COLLECTION.REPORT}${expenseChatReportID}`,
+                    value: {
+                        pendingFields: {
+                            addWorkspaceRoom: null,
+                        },
+                        pendingAction: null,
+                    },
+                },
+                {
+                    onyxMethod: Onyx.METHOD.MERGE,
+                    key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${expenseChatReportID}`,
+                    value: {
+                        [_.keys(expenseChatData)[0]]: {
+                            pendingAction: null,
+                        },
+                    },
+                },
+            ],
+            failureData: [
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.POLICY_MEMBERS}${policyID}`,
+                    value: null,
+                },
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.REPORT}${expenseChatReportID}`,
+                    value: null,
+                },
+                {
+                    onyxMethod: Onyx.METHOD.SET,
+                    key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${expenseChatReportID}`,
+                    value: null,
+                },
+            ],
+        },
+    );
+    Navigation.dismissModal(expenseChatReportID);
 }
 
 export default {referTeachersUniteVolunteer, createExpenseChatSchoolPrincipal};
