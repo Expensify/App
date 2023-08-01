86dfc42c07c3ceb6c52a03f07bf707e5f3e05688
Alex Beaman <alexbeaman@expensify.com>
2023-08-01 13:23:16 +0300

    Return when dismissing modal

--- src/components/AttachmentCarousel/createInitialState.js
+++ src/components/AttachmentCarousel/createInitialState.js
@@ -52,6 +52,10 @@ function createInitialState(props) {
     const page = _.findIndex(attachments, (a) => a.source === props.source);
     if (page === -1) {
         Navigation.dismissModal();
+        return {
+            page: 0,
+            attachments: [],
+        };
     }
 
     // Update the parent modal's state with the source and name from the mapped attachments
