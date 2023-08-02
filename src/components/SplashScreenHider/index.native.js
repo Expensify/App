aee92d45d315b53e1ff9122c6af3438478fe9540
Kamil Owczarz <kamil.owczarz@swmansion.com>
2023-08-02 09:51:39 +0200

    Revert different splash screens on Android

--- src/components/SplashScreenHider/index.native.js
+++ src/components/SplashScreenHider/index.native.js
@@ -2,11 +2,8 @@ import {useCallback, useRef} from 'react';
 import PropTypes from 'prop-types';
 import {StyleSheet} from 'react-native';
 import Reanimated, {useSharedValue, withTiming, Easing, useAnimatedStyle, runOnJS} from 'react-native-reanimated';
-import DeviceInfo from 'react-native-device-info';
 import BootSplash from '../../libs/BootSplash';
-import LogoProduction from '../../../assets/images/new-expensify-dark.svg';
-import LogoDev from '../../../assets/images/new-expensify-dev.svg';
-import LogoAdHoc from '../../../assets/images/new-expensify-adhoc.svg';
+import Logo from '../../../assets/images/new-expensify-dark.svg';
 import styles from '../../styles/styles';
 
 const propTypes = {
@@ -18,17 +15,6 @@ const defaultProps = {
     onHide: () => {},
 };
 
-const getLogo = () => {
-    const bundleId = DeviceInfo.getBundleId();
-    switch (bundleId) {
-        case 'com.expensify.chat.dev':
-            return LogoDev;
-        case 'com.expensify.chat.adhoc':
-            return LogoAdHoc;
-        default:
-            return LogoProduction;
-    }
-};
 
 function SplashScreenHider(props) {
     const {onHide} = props;
@@ -44,7 +30,6 @@ function SplashScreenHider(props) {
     }));
 
     const hideHasBeenCalled = useRef(false);
-    const Logo = getLogo();
 
     const hide = useCallback(() => {
         // hide can only be called once
