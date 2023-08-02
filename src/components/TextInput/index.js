3c83a9d011ff19965961f96f93fdbb5feb050c01
Huzaifa Rasheed <68777211+huzaifa-99@users.noreply.github.com>
2023-08-03 00:41:25 +0500

    Fix lint

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import _ from 'underscore';
 import styles from '../../styles/styles';
 import * as styleConst from './styleConst';
@@ -8,70 +8,70 @@ import DomUtils from '../../libs/DomUtils';
 import Visibility from '../../libs/Visibility';
 
 const TextInput = (props) => {
-  const textInputRef = useRef(null);
-  const removeVisibilityListenerRef = useRef(null);
+    const textInputRef = useRef(null);
+    const removeVisibilityListenerRef = useRef(null);
 
-  useEffect(() => {
-    if (props.disableKeyboard) {
-      textInputRef.current.setAttribute('inputmode', 'none');
-    }
+    useEffect(() => {
+        if (props.disableKeyboard) {
+            textInputRef.current.setAttribute('inputmode', 'none');
+        }
 
-    if (props.name) {
-      textInputRef.current.setAttribute('name', props.name);
-    }
+        if (props.name) {
+            textInputRef.current.setAttribute('name', props.name);
+        }
 
-    removeVisibilityListenerRef.current = Visibility.onVisibilityChange(() => {
-      if (!Visibility.isVisible() || !textInputRef.current || DomUtils.getActiveElement() !== textInputRef.current) {
-        return;
-      }
-      textInputRef.current.blur();
-      textInputRef.current.focus();
-    });
+        removeVisibilityListenerRef.current = Visibility.onVisibilityChange(() => {
+            if (!Visibility.isVisible() || !textInputRef.current || DomUtils.getActiveElement() !== textInputRef.current) {
+                return;
+            }
+            textInputRef.current.blur();
+            textInputRef.current.focus();
+        });
 
-    return () => {
-      if (removeVisibilityListenerRef.current) {
-        removeVisibilityListenerRef.current();
-      }
+        return () => {
+            if (removeVisibilityListenerRef.current) {
+                removeVisibilityListenerRef.current();
+            }
+        };
+    }, []);
+
+    const isLabeledMultiline = Boolean(props.label.length) && props.multiline;
+    const labelAnimationStyle = {
+        '--active-label-translate-y': `${styleConst.ACTIVE_LABEL_TRANSLATE_Y}px`,
+        '--active-label-scale': `${styleConst.ACTIVE_LABEL_SCALE}`,
+        '--label-transition-duration': `${styleConst.LABEL_ANIMATION_DURATION}ms`,
     };
-  }, []);
 
-  const isLabeledMultiline = Boolean(props.label.length) && props.multiline;
-  const labelAnimationStyle = {
-    '--active-label-translate-y': `${styleConst.ACTIVE_LABEL_TRANSLATE_Y}px`,
-    '--active-label-scale': `${styleConst.ACTIVE_LABEL_SCALE}`,
-    '--label-transition-duration': `${styleConst.LABEL_ANIMATION_DURATION}ms`,
-  };
+    return (
+        <BaseTextInput
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+            innerRef={(el) => {
+                textInputRef.current = el;
+                if (!props.innerRef) {
+                    return;
+                }
 
-  return (
-    <BaseTextInput
-    // eslint-disable-next-line react/jsx-props-no-spreading
-      {...props}
-      innerRef={(el) => {
-        textInputRef.current = el;
-        if (!props.innerRef) {
-          return;
-      }
+                if (_.isFunction(props.innerRef)) {
+                    props.innerRef(el);
+                    return;
+                }
 
-      if (_.isFunction(props.innerRef)) {
-          props.innerRef(el);
-          return;
-      }
-
-      props.innerRef.current = el;
-      }}
-      inputStyle={[styles.baseTextInput, styles.textInputDesktop, isLabeledMultiline ? styles.textInputMultiline : {}, ...props.inputStyle]}
-      textInputContainerStyles={[labelAnimationStyle, ...props.textInputContainerStyles]}
-    />
-  );
+                props.innerRef.current = el;
+            }}
+            inputStyle={[styles.baseTextInput, styles.textInputDesktop, isLabeledMultiline ? styles.textInputMultiline : {}, ...props.inputStyle]}
+            textInputContainerStyles={[labelAnimationStyle, ...props.textInputContainerStyles]}
+        />
+    );
 };
 
 TextInput.propTypes = baseTextInputPropTypes.propTypes;
 TextInput.defaultProps = baseTextInputPropTypes.defaultProps;
 
 export default React.forwardRef((props, ref) => (
-  <TextInput
-      // eslint-disable-next-line react/jsx-props-no-spreading
-      {...props}
-      innerRef={ref}
-  />
+    <TextInput
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        innerRef={ref}
+    />
 ));
