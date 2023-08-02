93f3a0b0054a46b604cbcfd0f16cefafeb8f8102
Michał Czernek <czernekmichal@gmail.com>
2023-08-02 10:59:27 +0200

    A few patches.

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     project: {
-        ios: {sourceDir: 'ios'},
-        android: {},
+        ios: {sourceDir: '../iOS'},
+        android: {sourceDir: '../Android'},
     },
     assets: ['./assets/fonts/native'],
+    dependencies: {
+        'react-native-flipper': {
+            platforms: {
+                ios: null
+            }
+        }
+    }
 };
