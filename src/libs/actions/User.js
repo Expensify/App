581ad3d8250cecc7d9c91e5a3ffdf5cd751a08af
Tim Golen <tgolen@expensify.com>
2023-08-02 11:16:12 -0600

    Cast to string and default to 0

--- src/libs/actions/User.js
+++ src/libs/actions/User.js
@@ -558,8 +558,8 @@ function subscribeToUserEvents() {
                 [ONYXKEYS.ONYX_UPDATES.PREVIOUS_UPDATE_ID]: pushJSON.previousUpdateID,
             });
             updates = pushJSON.updates;
-            console.debug('[OnyxUpdates] Received lastUpdateID from pusher', pushJSON.lastUpdateID);
-            console.debug('[OnyxUpdates] Received previousUpdateID from pusher', pushJSON.previousUpdateID);
+            console.debug('[OnyxUpdates] Received lastUpdateID from pusher', Number(pushJSON.lastUpdateID || 0));
+            console.debug('[OnyxUpdates] Received previousUpdateID from pusher', Number(pushJSON.previousUpdateID || 0));
         }
         _.each(updates, (multipleEvent) => {
             PusherUtils.triggerMultiEventHandler(multipleEvent.eventType, multipleEvent.data);
