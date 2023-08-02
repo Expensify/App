a6b973c7127d5d4745277ae098de149befcdc8db
Michał Skałka <michal.skalka@swmansion.com>
2023-08-02 11:53:59 +0200

    fixed undraggable desktop window bug

--- src/components/Pressable/GenericPressable/index.js
+++ src/components/Pressable/GenericPressable/index.js
@@ -14,7 +14,9 @@ const WebGenericPressable = forwardRef((props, ref) => (
         aria-label={props.accessibilityLabel}
         aria-labelledby={props.accessibilityLabelledBy}
         aria-valuenow={props.accessibilityValue}
-        nativeID={props.nativeID || 'no-drag-area'}
+        nativeID={props.nativeID}
+        dataSet={{id: 'pressable'}}
+        className="ASDASD"
     />
 ));
 
