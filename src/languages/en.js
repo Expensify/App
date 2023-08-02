4aa33f62d670a3d695b6e7c478329c4bf3e4ea74
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2023-08-03 02:48:20 +0530

    Lint code: Apply suggestions from code review
    Co-authored-by: Manan <manan.jadhav@gmail.com>

--- src/languages/en.js
+++ src/languages/en.js
@@ -1427,7 +1427,7 @@ export default {
         copy: 'Copy',
         paste: 'Paste',
         pasteAndMatchStyle: 'Paste and Match Style',
-        pasteAsPlainText:'Paste As Plain Text',
+        pasteAsPlainText:'Paste as Plain Text',
         delete: 'Delete',
         selectAll: 'Select All',
         speechSubmenu: 'Speech',
