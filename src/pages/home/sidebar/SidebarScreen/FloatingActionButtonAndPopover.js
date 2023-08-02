c1fb994cac0842037877bf2c18cd8430d614d1f4
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-08-02 14:43:43 +0200

    Drop splitBill option

--- src/pages/home/sidebar/SidebarScreen/FloatingActionButtonAndPopover.js
+++ src/pages/home/sidebar/SidebarScreen/FloatingActionButtonAndPopover.js
@@ -200,11 +200,6 @@ function FloatingActionButtonAndPopover(props) {
                         text: props.translate('iou.requestMoney'),
                         onSelected: () => interceptAnonymousUser(() => IOU.startMoneyRequest(CONST.IOU.MONEY_REQUEST_TYPE.REQUEST)),
                     },
-                    {
-                        icon: Expensicons.Receipt,
-                        text: props.translate('iou.splitBill'),
-                        onSelected: () => interceptAnonymousUser(() => IOU.startMoneyRequest(CONST.IOU.MONEY_REQUEST_TYPE.SPLIT)),
-                    },
                     ...(Permissions.canUseTasks(props.betas)
                         ? [
                               {
