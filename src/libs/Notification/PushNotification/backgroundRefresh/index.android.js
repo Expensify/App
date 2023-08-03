b38d73b38c22adb6210c074f058c8825d037731a
Andrew Rosiclair <arosiclair@expensify.com>
2023-08-03 12:57:03 -0400

    add background refresh for android

---
+++ src/libs/Notification/PushNotification/backgroundRefresh/index.android.js
@@ -0,0 +1,15 @@
+import * as App from '../../../actions/App';
+import Visibility from '../../../Visibility';
+/**
+ * Runs our reconnectApp action if the app is in the background.
+ *
+ * We use this to refresh the app in the background after receiving a push notification (native only). Since the full app
+ * wakes on iOS and by extension runs reconnectApp already, this is a no-op on everything but Android.
+ */
+export default function backgroundRefresh() {
+    if (Visibility.isVisible()) {
+        return;
+    }
+
+    App.reconnectApp();
+}
