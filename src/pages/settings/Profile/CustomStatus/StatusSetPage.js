c414d4972254672ad320ddd95d0975dd7b9816b4
Taras Perun <terry.perun@gmail.com>
2023-08-03 20:22:08 +0200

    create StatusSetPage

---
+++ src/pages/settings/Profile/CustomStatus/StatusSetPage.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import {withOnyx} from 'react-native-onyx';
+import Form from '../../../../components/Form';
+import HeaderWithBackButton from '../../../../components/HeaderWithBackButton';
+import ROUTES from '../../../../ROUTES';
+import ScreenWrapper from '../../../../components/ScreenWrapper';
+import Navigation from '../../../../libs/Navigation/Navigation';
+import compose from '../../../../libs/compose';
+import styles from '../../../../styles/styles';
+import useLocalize from '../../../../hooks/useLocalize';
+import CONST from '../../../../CONST';
+import EmojiPickerButtonDropdown from '../../../../components/EmojiPicker/EmojiPickerButtonDropdown';
+import ONYXKEYS from '../../../../ONYXKEYS';
+import * as User from '../../../../libs/actions/User';
+import withCurrentUserPersonalDetails, {withCurrentUserPersonalDetailsPropTypes} from '../../../../components/withCurrentUserPersonalDetails';
+import TextInput from '../../../../components/TextInput';
+
+const propTypes = {
+    /** The draft status of the user */
+    draftStatus: PropTypes.shape({
+        /** The emoji code of the draft status */
+        emojiCode: PropTypes.string,
+        /** The text of the draft status */
+        text: PropTypes.string,
+    }),
+
+    ...withCurrentUserPersonalDetailsPropTypes,
+};
+
+
+const INPUT_IDS = {
+  EMOJI_CODE: 'emojiCode',
+  STATUS_TEXT: 'statusText'
+};
+
+function StatusSetPage(props) {
+    const {translate} = useLocalize();
+    const defaultEmoji = props.draftStatus?.emojiCode || props.currentUserPersonalDetails?.status?.emojiCode || '💬';
+    const defaultText = props.draftStatus?.text || props.currentUserPersonalDetails?.status?.text || '';
+
+    const onSubmit = (v) => {
+        User.updateDraftCustomStatus({text: v.statusText, emojiCode: v.emojiCode});
+        Navigation.goBack(ROUTES.SETTINGS_STATUS);
+    };
+
+    return (
+        <ScreenWrapper>
+            <HeaderWithBackButton
+                title={translate('statusPage.status')}
+                onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_STATUS)}
+            />
+            <Form
+                formID={ONYXKEYS.FORMS.SETTINGS_STATUS_SET_FORM}
+                style={[styles.flexGrow1, styles.ph5]}
+                submitButtonText="Save"
+                onSubmit={onSubmit}
+                enabledWhenOffline
+            >
+                <View>
+                    <View style={[styles.mb4]}>
+                        <View style={[styles.mt1]}>
+                            <EmojiPickerButtonDropdown
+                                inputID={INPUT_IDS.EMOJI_CODE}
+                                accessibilityLabel={INPUT_IDS.EMOJI_CODE}
+                                defaultValue={defaultEmoji}
+                            />
+                        </View>
+                        <TextInput
+                            inputID={INPUT_IDS.STATUS_TEXT}
+                            label={translate('statusPage.message')}
+                            accessibilityLabel={INPUT_IDS.STATUS_TEXT}
+                            accessibilityRole={CONST.ACCESSIBILITY_ROLE.TEXT}
+                            defaultValue={defaultText}
+                            maxLength={100}
+                            autoFocus
+                        />
+                    </View>
+                </View>
+            </Form>
+        </ScreenWrapper>
+    );
+}
+
+StatusSetPage.displayName = 'StatusSetPage';
+StatusSetPage.propTypes = propTypes;
+
+export default compose(
+    withCurrentUserPersonalDetails,
+    withOnyx({
+        draftStatus: {
+            key: ONYXKEYS.CUSTOM_STATUS_DRAFT,
+        },
+    }),
+)(StatusSetPage);
