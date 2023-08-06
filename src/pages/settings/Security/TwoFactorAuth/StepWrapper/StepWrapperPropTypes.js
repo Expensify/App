49ab1836a5e5eead0bf3a915963e9d321b10e8ce
Agata Kosior <agata.kosior57@gmail.com>
2023-08-06 10:49:32 +0200

    fix: fix typo and add proptypes comments

--- src/pages/settings/Security/TwoFactorAuth/StepWrapper/StepWrapperPropTypes.js
+++ src/pages/settings/Security/TwoFactorAuth/StepWrapper/StepWrapperPropTypes.js
@@ -6,15 +6,18 @@ export default {
 
     /** Data to display a step counter in the header */
     stepCounter: PropTypes.shape({
+        /** Current step */
         step: PropTypes.number,
+        /** Total number of steps */
         total: PropTypes.number,
+        /** Text to display next to the step counter */
         text: PropTypes.string,
     }),
 
     /** Method to trigger when pressing back button of the header */
     onBackButtonPress: PropTypes.func,
 
-    /** Called when navigated Screen's transition is finished. It does not fire when user exit the page. */
+    /** Called when navigated Screen's transition is finished. It does not fire when user exits the page. */
     onEntryTransitionEnd: PropTypes.func,
 
     /** Children components */
