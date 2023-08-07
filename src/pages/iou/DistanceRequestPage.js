a8210419288cd948bc4322e19b4a3f00f36d01b8
neil-marcellini <neil@expensify.com>
2023-08-07 14:59:53 -0700

    Remove unused props

--- src/pages/iou/DistanceRequestPage.js
+++ src/pages/iou/DistanceRequestPage.js
@@ -4,61 +4,22 @@ import {withOnyx} from 'react-native-onyx';
 import * as IOU from '../../libs/actions/IOU';
 import ONYXKEYS from '../../ONYXKEYS';
 import DistanceRequest from '../../components/DistanceRequest';
-import CONST from '../../CONST';
 import reportPropTypes from '../reportPropTypes';
 
 const propTypes = {
     /** The transactionID of this request */
     transactionID: PropTypes.string,
 
-    /** Route params */
-    route: PropTypes.shape({
-        params: PropTypes.shape({
-            iouType: PropTypes.string,
-            reportID: PropTypes.string,
-        }),
-    }),
-
     /** The report on which the request is initiated on */
     report: reportPropTypes,
-
-    /** Holds data related to Money Request view state, rather than the underlying Money Request data. */
-    iou: PropTypes.shape({
-        id: PropTypes.string,
-        amount: PropTypes.number,
-        currency: PropTypes.string,
-        participants: PropTypes.arrayOf(
-            PropTypes.shape({
-                accountID: PropTypes.number,
-                login: PropTypes.string,
-                isPolicyExpenseChat: PropTypes.bool,
-                isOwnPolicyExpenseChat: PropTypes.bool,
-                selected: PropTypes.bool,
-            }),
-        ),
-        transactionID: PropTypes.string,
-    }),
 };
 
 const defaultProps = {
     transactionID: '',
-    route: {
-        params: {
-            iouType: '',
-            reportID: '',
-        },
-    },
     report: {},
-    iou: {
-        id: '',
-        amount: 0,
-        currency: CONST.CURRENCY.USD,
-        participants: [],
-        transactionID: '',
-    },
 };
 
-function DistanceRequestPage({route, report, iou, transactionID}) {
+function DistanceRequestPage({report, transactionID}) {
     useEffect(() => {
         if (transactionID) {
             return;
@@ -68,9 +29,7 @@ function DistanceRequestPage({route, report, iou, transactionID}) {
 
     return (
         <DistanceRequest
-            route={route}
             report={report}
-            iou={iou}
             transactionID={transactionID}
         />
     );
