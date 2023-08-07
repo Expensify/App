ee4be802325ff7a5769365fe7ee7831663de468b
Daniel Edwards <cdaniel.edwards@gmail.com>
2023-08-07 09:10:49 -0400

    Update src/components/SignInButtons/AppleAuthWrapper/index.ios.js
    Co-authored-by: Marco Chávez <marcochavezf@gmail.com>

--- src/components/SignInButtons/AppleAuthWrapper/index.ios.js
+++ src/components/SignInButtons/AppleAuthWrapper/index.ios.js
@@ -6,7 +6,6 @@ import * as Session from '../../../libs/actions/Session';
  * Apple Sign In wrapper for iOS
  * revokes the session if the credential is revoked.
  */
-
 function AppleAuthWrapper() {
     useEffect(() => {
         if (!appleAuth.isSupported) {
