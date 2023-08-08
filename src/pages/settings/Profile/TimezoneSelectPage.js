1bf3deee5f7e03a564ebeea1fd5d511a091b64bf
Michał Skałka <michal.skalka@swmansion.com>
2023-08-08 12:20:58 +0200

    migrated TimezoneSelectPage to functional component

--- src/pages/settings/Profile/TimezoneSelectPage.js
+++ src/pages/settings/Profile/TimezoneSelectPage.js
@@ -1,5 +1,5 @@
 import lodashGet from 'lodash/get';
-import React, {Component} from 'react';
+import React, {useState, useRef} from 'react';
 import _ from 'underscore';
 import moment from 'moment-timezone';
 import withCurrentUserPersonalDetails, {withCurrentUserPersonalDetailsPropTypes, withCurrentUserPersonalDetailsDefaultProps} from '../../../components/withCurrentUserPersonalDetails';
@@ -22,111 +22,66 @@ const defaultProps = {
     ...withCurrentUserPersonalDetailsDefaultProps,
 };
 
-class TimezoneSelectPage extends Component {
-    constructor(props) {
-        super(props);
+/**
+ * We add the current time to the key to fix a bug where the list options don't update unless the key is updated.
+ * @param {String} text
+ * @return {string} key for list item
+ */
+const getKey = (text) => `${text}-${new Date().getTime()}`;
 
-        this.saveSelectedTimezone = this.saveSelectedTimezone.bind(this);
-        this.filterShownTimezones = this.filterShownTimezones.bind(this);
-        this.getTimezoneOption = this.getTimezoneOption.bind(this);
+/**
+ * @param {Object} currentUserPersonalDetails
+ * @return {Object} user's timezone data
+ */
+const getUserTimezone = (currentUserPersonalDetails) => lodashGet(currentUserPersonalDetails, 'timezone', CONST.DEFAULT_TIME_ZONE);
 
-        this.timezone = this.getUserTimezone(props.currentUserPersonalDetails);
-        this.allTimezones = _.chain(moment.tz.names())
-            .filter((timezone) => !timezone.startsWith('Etc/GMT'))
-            .map(this.getTimezoneOption)
-            .value();
-
-        this.state = {
-            timezoneInputText: this.timezone.selected,
-            timezoneOptions: this.allTimezones,
-        };
-    }
-
-    componentDidUpdate() {
-        // componentDidUpdate is added in order to update the timezone options when automatic is toggled on/off as
-        // navigating back doesn't unmount the page, thus it won't update the timezone options & stay disabled without this.
-        const newTimezone = this.getUserTimezone(this.props.currentUserPersonalDetails);
-        if (_.isEqual(this.timezone, newTimezone)) {
-            return;
-        }
-        this.timezone = newTimezone;
-        this.allTimezones = _.map(this.allTimezones, (timezone) => {
-            const text = timezone.text.split('-')[0];
-            return this.getTimezoneOption(text);
-        });
-
-        this.setState({
-            timezoneInputText: this.timezone.selected,
-            timezoneOptions: this.allTimezones,
-        });
-    }
-
-    /**
-     * We add the current time to the key to fix a bug where the list options don't update unless the key is updated.
-     * @param {String} text
-     * @return {string} key for list item
-     */
-    getKey(text) {
-        return `${text}-${new Date().getTime()}`;
-    }
-
-    /**
-     * Get timezone option object for the list.
-     * @param {String} text
-     * @return {Object} Timezone list option
-     */
-    getTimezoneOption(text) {
-        return {
-            text,
-            keyForList: this.getKey(text),
-            isSelected: text === this.timezone.selected,
-        };
-    }
-
-    /**
-     * @param {Object} currentUserPersonalDetails
-     * @return {Object} user's timezone data
-     */
-    getUserTimezone(currentUserPersonalDetails) {
-        return lodashGet(currentUserPersonalDetails, 'timezone', CONST.DEFAULT_TIME_ZONE);
-    }
+function TimezoneSelectPage(props) {
+    const timezone = useRef(getUserTimezone(props.currentUserPersonalDetails));
+    const allTimezones = useRef(
+        _.chain(moment.tz.names())
+            .filter((tz) => !tz.startsWith('Etc/GMT'))
+            .map((text) => ({
+                text,
+                keyForList: getKey(text),
+                isSelected: text === timezone.current.selected,
+            }))
+            .value(),
+    );
+    const [timezoneInputText, setTimezoneInputText] = useState(timezone.current.selected);
+    const [timezoneOptions, setTimezoneOptions] = useState(allTimezones.current);
 
     /**
      * @param {Object} timezone
      * @param {String} timezone.text
      */
-    saveSelectedTimezone({text}) {
+    const saveSelectedTimezone = ({text}) => {
         PersonalDetails.updateSelectedTimezone(text);
-    }
+    };
 
     /**
      * @param {String} searchText
      */
-    filterShownTimezones(searchText) {
-        this.setState({
-            timezoneInputText: searchText,
-            timezoneOptions: _.filter(this.allTimezones, (tz) => tz.text.toLowerCase().includes(searchText.trim().toLowerCase())),
-        });
-    }
+    const filterShownTimezones = (searchText) => {
+        setTimezoneInputText(searchText);
+        setTimezoneOptions(_.filter(allTimezones.current, (tz) => tz.text.toLowerCase().includes(searchText.trim().toLowerCase())));
+    };
 
-    render() {
-        return (
-            <ScreenWrapper includeSafeAreaPaddingBottom={false}>
-                <HeaderWithBackButton
-                    title={this.props.translate('timezonePage.timezone')}
-                    onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_TIMEZONE)}
-                />
-                <SelectionListRadio
-                    textInputLabel={this.props.translate('timezonePage.timezone')}
-                    textInputValue={this.state.timezoneInputText}
-                    onChangeText={this.filterShownTimezones}
-                    onSelectRow={this.saveSelectedTimezone}
-                    sections={[{data: this.state.timezoneOptions, indexOffset: 0, isDisabled: this.timezone.automatic}]}
-                    initiallyFocusedOptionKey={_.get(_.filter(this.state.timezoneOptions, (tz) => tz.text === this.timezone.selected)[0], 'keyForList')}
-                />
-            </ScreenWrapper>
-        );
-    }
+    return (
+        <ScreenWrapper includeSafeAreaPaddingBottom={false}>
+            <HeaderWithBackButton
+                title={props.translate('timezonePage.timezone')}
+                onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_TIMEZONE)}
+            />
+            <SelectionListRadio
+                textInputLabel={props.translate('timezonePage.timezone')}
+                textInputValue={timezoneInputText}
+                onChangeText={filterShownTimezones}
+                onSelectRow={saveSelectedTimezone}
+                sections={[{data: timezoneOptions, indexOffset: 0, isDisabled: timezone.current.automatic}]}
+                initiallyFocusedOptionKey={_.get(_.filter(timezoneOptions, (tz) => tz.text === timezone.current.selected)[0], 'keyForList')}
+            />
+        </ScreenWrapper>
+    );
 }
 
 TimezoneSelectPage.propTypes = propTypes;
