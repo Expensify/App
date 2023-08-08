cc0352a39de64a54df4a12ce1ef38347495e3b0f
Jack Nam <jack@expensify.com>
2023-08-08 14:14:07 -0700

    Start DistanceRequest actions

---
+++ src/libs/actions/DistanceRequest.js
@@ -0,0 +1,14 @@
+import Onyx from 'react-native-onyx';
+import ONYXKEYS from '../../ONYXKEYS';
+import CONST from '../../CONST';
+
+
+function saveWaypoint(transactionID, index, waypoint)
+{
+    
+}
+
+
+export {
+
+};
