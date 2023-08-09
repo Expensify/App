d3f37594cc9b6bc7cff2aeebdeb79f38d6b2896e
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-08-09 11:14:46 +0200

    fix PDF extra updates

--- src/components/PDFView/index.js
+++ src/components/PDFView/index.js
@@ -178,13 +178,17 @@ class PDFView extends Component {
     }
 
     /**
-     * It is a currying method that returns a function that renders a specific page based on its index.
-     * The function includes a wrapper to apply virtualized styles.
-     * @param {Number} pageWidth
+     * Render a specific page based on its index.
+     * The method includes a wrapper to apply virtualized styles.
+     * @param {Object} page item object of the List
+     * @param {Number} page.index index of the page
+     * @param {Object} page.style virtualized styles
      * @returns {JSX.Element}
      */
-    renderPage(pageWidth) {
-        return ({index, style}) => (
+    renderPage({index, style}) {
+        const pageWidth = this.calculatePageWidth();
+
+        return (
             <View style={style}>
                 <Page
                     key={`page_${index}`}
@@ -240,7 +244,7 @@ class PDFView extends Component {
                                 itemCount={this.state.numPages}
                                 itemSize={this.calculatePageHeight}
                             >
-                                {this.renderPage(pageWidth)}
+                                {this.renderPage}
                             </List>
                         )}
                     </Document>
