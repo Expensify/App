fd26e252267bc08126695b49b215da9a0b6e3938
Daniel Ordóñez <dloz1996@gmail.com>
2023-08-09 15:30:44 -0500

    task: Dismiss modal if user is already logged in and access via url

--- src/pages/signin/SignInModal.js
+++ src/pages/signin/SignInModal.js
@@ -4,12 +4,17 @@ import ScreenWrapper from '../../components/ScreenWrapper';
 import HeaderWithBackButton from '../../components/HeaderWithBackButton';
 import Navigation from '../../libs/Navigation/Navigation';
 import styles from '../../styles/styles';
+import * as Session from '../../libs/actions/Session';
 
 const propTypes = {};
 
 const defaultProps = {};
 
 function SignInModal() {
+    if (!Session.isAnonymousUser()) {
+        // Sign in in RHP is only for anonymous users
+        Navigation.dismissModal();
+    }
     return (
         <ScreenWrapper
             style={[styles.highlightBG]}
