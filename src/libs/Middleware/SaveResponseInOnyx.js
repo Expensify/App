2eb7c879c934a248c5f48c195aa3223c7145ce7a
Tim Golen <tgolen@expensify.com>
2023-08-10 07:59:39 -0600

    Remove unnecessary method

--- src/libs/Middleware/SaveResponseInOnyx.js
+++ src/libs/Middleware/SaveResponseInOnyx.js
@@ -4,24 +4,6 @@ import * as QueuedOnyxUpdates from '../actions/QueuedOnyxUpdates';
 import * as OnyxUpdates from '../actions/OnyxUpdates';
 
 /**
- * @param {Object} updates
- * @param {Object[]} [updates.onyxData] an array of Onyx update instruction objects
- * @param {Number} [updates.previousUpdateID]
- * @param {Number} [updates.lastUpdateID]
- * @returns {Promise}
- */
-function updateOnyx(updates) {
-    // `updates.onyxData` only exists in the latest format of the OnyxUpdates provided by the RELIABLE_UPDATES beta.
-    // If the value doesn't exist, then onyx is updated with the old format of updates.
-    if (!updates.onyxData) {
-        return Onyx.update(updates);
-    }
-
-    OnyxUpdates.saveUpdateIDs(Number(updates.lastUpdateID || 0), Number(updates.previousUpdateID || 0));
-    return Onyx.update(updates.onyxData);
-}
-
-/**
  * @param {Promise} response
  * @param {Object} request
  * @returns {Promise}
@@ -33,9 +15,12 @@ function SaveResponseInOnyx(response, request) {
             return;
         }
 
+        // Save the update IDs to Onyx so they can be used to fetch incremental updates if the client gets out of sync from the server
+        OnyxUpdates.saveUpdateIDs(Number(responseData.lastUpdateID || 0), Number(responseData.previousUpdateID || 0));
+
         // For most requests we can immediately update Onyx. For write requests we queue the updates and apply them after the sequential queue has flushed to prevent a replay effect in
         // the UI. See https://github.com/Expensify/App/issues/12775 for more info.
-        const updateHandler = request.data.apiRequestType === CONST.API_REQUEST_TYPE.WRITE ? QueuedOnyxUpdates.queueOnyxUpdates : updateOnyx;
+        const updateHandler = request.data.apiRequestType === CONST.API_REQUEST_TYPE.WRITE ? QueuedOnyxUpdates.queueOnyxUpdates : Onyx.update;
 
         // First apply any onyx data updates that are being sent back from the API. We wait for this to complete and then
         // apply successData or failureData. This ensures that we do not update any pending, loading, or other UI states contained
