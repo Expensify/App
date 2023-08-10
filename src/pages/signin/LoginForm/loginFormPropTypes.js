0df77e93f032be66acf31bee03236518d61c4b38
Johannes Idarsson <138504087+joh42@users.noreply.github.com>
2023-08-10 21:56:06 +0200

    added isVisible propType (seemed to be missing)

--- src/pages/signin/LoginForm/loginFormPropTypes.js
+++ src/pages/signin/LoginForm/loginFormPropTypes.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 const propTypes = {
     /** Should we dismiss the keyboard when transitioning away from the page? */
     blurOnSubmit: PropTypes.bool,
+
+    /** Should the component be visible? */
+    isVisible: PropTypes.bool.isRequired,
 };
 
 const defaultProps = {
