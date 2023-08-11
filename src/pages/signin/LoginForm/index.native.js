cb6b2606769cab7011c45fc3f97742ce64b6f8b8
Johannes Idarsson <138504087+joh42@users.noreply.github.com>
2023-08-11 22:49:46 +0200

    implemented scroll to top on the login page when app regains focus

--- src/pages/signin/LoginForm/index.native.js
+++ src/pages/signin/LoginForm/index.native.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import BaseLoginForm from './BaseLoginForm';
 import {propTypes, defaultProps} from './loginFormPropTypes';
+import AppStateMonitor from '../../../libs/AppStateMonitor';
 
 function LoginForm(props) {
-    // TODO: Implement scrolling to top when AppState changes to visible
+    const loginFormRef = useRef();
+    const {scrollPageToTop} = props;
+
+    useEffect(() => {
+        if (scrollPageToTop) {
+            return;
+        }
+
+        const unsubscribeToBecameActiveListener = AppStateMonitor.addBecameActiveListener(() => {
+            const isInputFocused = loginFormRef.current && loginFormRef.current.isInputFocused();
+            if (!isInputFocused) {
+                return;
+            }
+
+            scrollPageToTop();
+        });
+
+        // Remove the subscription on cleanup
+        return unsubscribeToBecameActiveListener;
+    }, [scrollPageToTop]);
 
     return (
         <BaseLoginForm
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
+            ref={(ref) => (loginFormRef.current = ref)}
         />
     );
 }
