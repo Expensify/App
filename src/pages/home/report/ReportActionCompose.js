bddc3fb50301103aa6f976d7aa2c92972809ec5c
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-08-11 15:47:57 +0200

    reapply https://github.com/Expensify/App/pull/23258

--- src/pages/home/report/ReportActionCompose.js
+++ src/pages/home/report/ReportActionCompose.js
@@ -368,24 +368,8 @@ function ReportActionCompose({
      * @param {Boolean} [shouldDelay=false] Impose delay before focusing the composer
      * @memberof ReportActionCompose
      */
-    const focus = useCallback((shouldDelay = false) => {
-        // There could be other animations running while we trigger manual focus.
-        // This prevents focus from making those animations janky.
-        InteractionManager.runAfterInteractions(() => {
-            if (!textInputRef.current) {
-                return;
-            }
-
-            if (!shouldDelay) {
-                textInputRef.current.focus();
-            } else {
-                // Keyboard is not opened after Emoji Picker is closed
-                // SetTimeout is used as a workaround
-                // https://github.com/react-native-modal/react-native-modal/issues/114
-                // We carefully choose a delay. 100ms is found enough for keyboard to open.
-                setTimeout(() => textInputRef.current.focus(), 100);
-            }
-        });
+    const focus = useCallback((shouldDelay) => {
+        focusWithDelay(textInputRef.current)(shouldDelay);
     }, []);
 
     /**
