d3ec5e41ba662a3df289cc814a43b9e901502302
tienifr <christianwen18@gmail.com>
2023-08-13 00:56:15 +0700

    clear callback when exit draft mode

--- src/pages/home/report/ReportActionItemMessageEdit.js
+++ src/pages/home/report/ReportActionItemMessageEdit.js
@@ -204,6 +204,7 @@ function ReportActionItemMessageEdit(props) {
         debouncedSaveDraft.cancel();
         Report.saveReportActionDraft(props.reportID, props.action.reportActionID, '');
         ComposerActions.setShouldShowComposeInput(true);
+        ReportActionComposeFocusManager.clear();
         ReportActionComposeFocusManager.focus();
 
         // Scroll to the last comment after editing to make sure the whole comment is clearly visible in the report.
