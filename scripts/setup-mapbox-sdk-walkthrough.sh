50d34c8ebec706f613a61bd95cfc7e8618d810b4
Hayata Suenaga <hayata@expensify.com>
2023-08-14 12:47:19 -0700

    refactor: improve bash script and add comments

--- scripts/setup-mapbox-sdk-walkthrough.sh
+++ scripts/setup-mapbox-sdk-walkthrough.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
 
-echo -e "This script helps you set up credentials needed to use Mapbox SDKs for iOS and Android\n\nWe need two kinds of tokens: one for downloading Mapbox's closed-sourced SDKs, and the other for accessing Mapbox's API from these SDKs"
+# Mapbox SDK Credential Setup Script
+# ==================================
+#
+# Purpose:
+# --------
+# This script assists users in setting up the necessary credentials to utilize
+# Mapbox's closed-source SDKs for iOS and Android. It provides step-by-step
+# guidance for obtaining a secret token from Mapbox and subsequently invokes 
+# the "./scripts/configure-mapbox.sh" script to configure the development environment.
+#
+# Background:
+# -----------
+# To use the Mapbox SDKs for iOS and Android development, a secret token 
+# must be obtained from Mapbox's account page. This token is essential for
+# authenticating downloads of the closed-source SDKs during the build process.
+#
+# Usage:
+# ------
+# To configure Mapbox, invoke this script by running the following command from the project's root directory:
+# npm run configure-mapbox
 
-echo -e "\nStep 1: Go to https://account.mapbox.com/access-tokens/. If you don't have a Mapbox account yet, crease one. Copy the token named \"Default public token.\" This token is used to call Mapbox API to display maps and get directions. Paste the copied token below and hit enter (the token won't be displayed)\n"
+# Color definitions for better readability
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-read -s -p "Default public token: " PUBLIC_TOKEN
+# Intro message
+echo -e "${GREEN}This script helps you set up the credential needed to use Mapbox's closed-sourced SDKs for iOS and Android.${NC}\n"
 
-echo -e "\n\nStep 2: Next, we will create a secret token needed to download Mapbox SDKs.\nIf you haven't already, create a secret token by clicking "Create a token" button on "https://account.mapbox.com/access-tokens/". Give the token a useful name (ex. Token for SDK downloads) and tick the checkbox next to "Downloads:Read" under the "Secret scopes".\nAll checkboxes under the "Public scopes" should be ticked by default, and you can leave them as is. Then, click the "Create token" button at the bottom of the page.\nCopy the value of the newly created token. You only have one chance to copy it. Paste the value below.\n"
+echo -e "1. Visit: https://account.mapbox.com/access-tokens/\n"
+echo -e "2. If you don't have a Mapbox account, create one.\n"
+echo -e "3. Create a secret token needed to download Mapbox SDKs. If you haven't done this yet:"
+echo -e "   - Click the \"Create a token\" button."
+echo -e "   - Provide a descriptive name for the token (e.g., Token for SDK downloads)."
+echo -e "   - Ensure the checkbox next to \"Downloads:Read\" under \"Secret scopes\" is ticked."
+echo -e "   - All checkboxes under the \"Public scopes\" should be ticked by default. Leave them as they are."
+echo -e "   - Click the \"Create token\" button at the bottom of the page."
+echo -e "   - IMPORTANT: Copy the value of the newly created token. This is your only opportunity to do so."
+echo -e "\nOnce you've done the above steps, please paste the token value below.\n"
 
+# Reading the secret token
 read -s -p "Secret download token: " SECRET_TOKEN
+echo -e "\n"
 
-echo -e "\n\nThank you for providing necessary credentials. Setting these credentials in relevant files...\n"
+if [[ -z "$SECRET_TOKEN" ]]; then
+    echo "Token is empty. Please run the script again and provide a valid token."
+    exit 1
+fi
 
-./scripts/configure-mapbox.sh $SECRET_TOKEN
+echo -e "${GREEN}Thank you for providing the token. Setting these credentials in relevant files...${NC}\n"
+
+# Execute the configuration script
+./scripts/configure-mapbox.sh "$SECRET_TOKEN"
