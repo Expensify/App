828f6e46ca2971bd265427a9154f1e6a3b6db605
Hayata Suenaga <hayata@expensify.com>
2023-08-14 09:36:01 -0700

    style: fix typos in script comments
    Co-authored-by: Rajat Parashar <parasharrajat@users.noreply.github.com>

--- scripts/configure-mapbox-walkthrough.sh
+++ scripts/configure-mapbox-walkthrough.sh
@@ -6,7 +6,7 @@ echo -e "\nStep 1: Go to https://account.mapbox.com/access-tokens/. If you don't
 
 read -s -p "Default public token: " PUBLIC_TOKEN
 
-echo -e "\n\nStep 2: Next, we will create a secret token needed to download Mapbox SDKs.\nIf you haven't already, create a secret token by clicking "Create a token" button on "https://account.mapbox.com/access-tokens/". Give the token a useful name (ex. Token for SDK downloads) and tik the checkbox next to "Downloads:Read" under the "Secret scopes".\nAll checkboxes under the "Public scopes" should be tiked by default, and you can leave them as is. Then, click the "Create token" button at the bottom of the page.\nCopy the value of the newly created token. You only have once chance to copy it. Paste the value below.\n"
+echo -e "\n\nStep 2: Next, we will create a secret token needed to download Mapbox SDKs.\nIf you haven't already, create a secret token by clicking "Create a token" button on "https://account.mapbox.com/access-tokens/". Give the token a useful name (ex. Token for SDK downloads) and tick the checkbox next to "Downloads:Read" under the "Secret scopes".\nAll checkboxes under the "Public scopes" should be ticked by default, and you can leave them as is. Then, click the "Create token" button at the bottom of the page.\nCopy the value of the newly created token. You only have one chance to copy it. Paste the value below.\n"
 
 read -s -p "Secret download token: " SECRET_TOKEN
 
