95208037c77d8389e6e185a4c976331490bc7e9b
Artem Makushov <waterim3009@gmail.com>
2023-08-14 17:50:09 +0200

    popover changes

--- src/components/PopoverWithMeasuredContent.js
+++ src/components/PopoverWithMeasuredContent.js
@@ -131,7 +131,7 @@ function PopoverWithMeasuredContent(props) {
     const verticalShift = computeVerticalShift(adjustedAnchorPosition.top, popoverHeight, windowHeight);
     const shiftedAnchorPosition = {
         left: adjustedAnchorPosition.left + horizontalShift,
-        top: adjustedAnchorPosition.top + verticalShift,
+        bottom: windowHeight - (adjustedAnchorPosition.top + popoverHeight) - verticalShift,
     };
     return isContentMeasured ? (
         <Popover
@@ -139,7 +139,7 @@ function PopoverWithMeasuredContent(props) {
             {...props}
             anchorPosition={shiftedAnchorPosition}
         >
-            {props.children}
+            <View onLayout={measurePopover}>{props.children}</View>
         </Popover>
     ) : (
         /*
