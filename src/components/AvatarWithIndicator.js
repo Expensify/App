3719c5d20161c56d70188aed141d1b16f2820086
Taras Perun <terry.perun@gmail.com>
2023-08-15 12:15:59 +0200

    clean AvatarWithIndicator

--- src/components/AvatarWithIndicator.js
+++ src/components/AvatarWithIndicator.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import Avatar from './Avatar';
 import styles from '../styles/styles';
 import Tooltip from './Tooltip';
-import Text from './Text';
 import * as UserUtils from '../libs/UserUtils';
 import Indicator from './Indicator';
 
@@ -14,34 +13,18 @@ const propTypes = {
 
     /** To show a tooltip on hover */
     tooltipText: PropTypes.string,
-
-    /** Persons emojy status */
-    emojiStatus: PropTypes.string,
 };
 
 const defaultProps = {
     tooltipText: '',
-    emojiStatus: '',
 };
 
-function AvatarWithIndicator({tooltipText, emojiStatus, source}) {
+function AvatarWithIndicator(props) {
     return (
-        <Tooltip text={tooltipText}>
-            <View style={!!emojiStatus && styles.sidebarStatusAvatarContainer}>
-                {!!emojiStatus && (
-                    <View style={styles.sidebarStatusAvatar}>
-                        <Text
-                            style={styles.emojiStatusLHN}
-                            numberOfLines={1}
-                        >
-                            {emojiStatus}
-                        </Text>
-                    </View>
-                )}
-                <View style={[styles.sidebarAvatar]}>
-                    <Avatar source={UserUtils.getSmallSizeAvatar(source)} />
-                    <Indicator />
-                </View>
+        <Tooltip text={props.tooltipText}>
+            <View style={[styles.sidebarAvatar]}>
+                <Avatar source={UserUtils.getSmallSizeAvatar(props.source)} />
+                <Indicator />
             </View>
         </Tooltip>
     );
