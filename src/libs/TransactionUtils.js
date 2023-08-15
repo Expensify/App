c70f1933cd6194054773a9155d7939b1751b27df
Carlos Martins <cmartins@expensify.com>
2023-08-15 13:29:51 -0500

    fix tests

--- src/libs/TransactionUtils.js
+++ src/libs/TransactionUtils.js
@@ -169,7 +169,7 @@ function getLinkedTransaction(reportAction = {}) {
 }
 
 function getAllReportTransactions(reportID) {
-    _.filter(allTransactions, transaction => transaction.reportID === reportID);
+    return _.filter(allTransactions, transaction => transaction.reportID === reportID);
 }
 
 export {buildOptimisticTransaction, getUpdatedTransaction, getTransaction, getDescription, getAmount, getCurrency, getCreated, getLinkedTransaction, getAllReportTransactions};
--- tests/unit/IOUUtilsTest.js
+++ tests/unit/IOUUtilsTest.js
@@ -1,39 +1,10 @@
 import Onyx from 'react-native-onyx';
 import * as IOUUtils from '../../src/libs/IOUUtils';
 import * as ReportUtils from '../../src/libs/ReportUtils';
-import * as NumberUtils from '../../src/libs/NumberUtils';
-import CONST from '../../src/CONST';
 import ONYXKEYS from '../../src/ONYXKEYS';
 import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
 import currencyList from './currencyList.json';
-
-let iouReport;
-let reportActions;
-const ownerAccountID = 5;
-const managerEmail = 'manager@iou.com';
-const managerID = 10;
-
-function createIOUReportAction(type, amount, currency, isOffline = false, IOUTransactionID = NumberUtils.rand64()) {
-    const moneyRequestAction = ReportUtils.buildOptimisticIOUReportAction(type, amount, currency, 'Test comment', [managerEmail], IOUTransactionID, '', iouReport.reportID);
-
-    // Default is to create requests online, if `isOffline` is not specified then we need to remove the pendingAction
-    if (!isOffline) {
-        moneyRequestAction.pendingAction = null;
-    }
-
-    reportActions.push(moneyRequestAction);
-    return moneyRequestAction;
-}
-
-function deleteMoneyRequest(moneyRequestAction, isOffline = false) {
-    createIOUReportAction(
-        CONST.IOU.REPORT_ACTION_TYPE.DELETE,
-        moneyRequestAction.originalMessage.amount,
-        moneyRequestAction.originalMessage.currency,
-        isOffline,
-        moneyRequestAction.originalMessage.IOUTransactionID,
-    );
-}
+import * as TransactionUtils from '../../src/libs/TransactionUtils';
 
 function initCurrencyList() {
     Onyx.init({
@@ -47,90 +18,48 @@ function initCurrencyList() {
 
 describe('IOUUtils', () => {
     describe('isIOUReportPendingCurrencyConversion', () => {
-        beforeEach(() => {
-            reportActions = [];
-            const chatReportID = ReportUtils.generateReportID();
-            const amount = 1000;
-            const currency = 'USD';
-
-            iouReport = ReportUtils.buildOptimisticIOUReport(ownerAccountID, managerID, amount, chatReportID, currency);
-
-            // The starting point of all tests is the IOUReport containing a single non-pending transaction in USD
-            // All requests in the tests are assumed to be online, unless isOffline is specified
-            createIOUReportAction('create', amount, currency);
+        beforeAll(() => {
+            Onyx.init({
+                keys: ONYXKEYS,
+            });
         });
 
         test('Requesting money offline in a different currency will show the pending conversion message', () => {
-            // Request money offline in AED
-            createIOUReportAction('create', 100, 'AED', true);
+            const iouReport = ReportUtils.buildOptimisticIOUReport(1, 2, 100, 1, 'USD');
+            const usdPendingTransaction = TransactionUtils.buildOptimisticTransaction(100, 'USD', iouReport.reportID);
+            const aedPendingTransaction = TransactionUtils.buildOptimisticTransaction(100, 'AED', iouReport.reportID);
 
-            // We requested money offline in a different currency, we don't know the total of the iouReport until we're back online
-            expect(IOUUtils.isIOUReportPendingCurrencyConversion(reportActions, iouReport)).toBe(true);
+            return Onyx.mergeCollection(ONYXKEYS.COLLECTION.TRANSACTION, {
+                [`${ONYXKEYS.COLLECTION.TRANSACTION}${usdPendingTransaction.transactionID}`]: usdPendingTransaction,
+                [`${ONYXKEYS.COLLECTION.TRANSACTION}${aedPendingTransaction.transactionID}`]: aedPendingTransaction,
+            })
+                .then(() => {
+                    // We requested money offline in a different currency, we don't know the total of the iouReport until we're back online
+                    expect(IOUUtils.isIOUReportPendingCurrencyConversion(iouReport)).toBe(true);
+                });
         });
 
-        test('IOUReport is not pending conversion when all requests made offline have been deleted', () => {
-            // Create two requests offline
-            const moneyRequestA = createIOUReportAction('create', 1000, 'AED', true);
-            const moneyRequestB = createIOUReportAction('create', 1000, 'AED', true);
+        test('Requesting money online in a different currency will not show the pending conversion message', () => {
+            const iouReport = ReportUtils.buildOptimisticIOUReport(2, 3, 100, 1, 'USD');
+            const usdPendingTransaction = TransactionUtils.buildOptimisticTransaction(100, 'USD', iouReport.reportID);
+            const aedPendingTransaction = TransactionUtils.buildOptimisticTransaction(100, 'AED', iouReport.reportID);
 
-            // Delete both requests
-            deleteMoneyRequest(moneyRequestA, true);
-            deleteMoneyRequest(moneyRequestB, true);
-
-            // Both requests made offline have been deleted, total won't update so no need to show a pending conversion message
-            expect(IOUUtils.isIOUReportPendingCurrencyConversion(reportActions, iouReport)).toBe(false);
+            return Onyx.mergeCollection(ONYXKEYS.COLLECTION.TRANSACTION, {
+                [`${ONYXKEYS.COLLECTION.TRANSACTION}${usdPendingTransaction.transactionID}`]: {
+                    ...usdPendingTransaction,
+                    pendingAction: null,
+                },
+                [`${ONYXKEYS.COLLECTION.TRANSACTION}${aedPendingTransaction.transactionID}`]: {
+                    ...aedPendingTransaction,
+                    pendingAction: null,
+                },
+            })
+                .then(() => {
+                    // We requested money online in a different currency, we know the iouReport total and there's no need to show the pending conversion message
+                    expect(IOUUtils.isIOUReportPendingCurrencyConversion(iouReport)).toBe(false);
+                });
         });
-
-        test('Deleting a request made online shows the preview', () => {
-            // Request money online in AED
-            const moneyRequest = createIOUReportAction('create', 1000, 'AED');
-
-            // Delete it offline
-            deleteMoneyRequest(moneyRequest, true);
-
-            // We don't know what the total is because we need to subtract the converted amount of the offline request from the total
-            expect(IOUUtils.isIOUReportPendingCurrencyConversion(reportActions, iouReport)).toBe(true);
-        });
-
-        test("Deleting a request made offline while there's a previous one made online will not show the pending conversion message", () => {
-            // Request money online in AED
-            createIOUReportAction('create', 1000, 'AED');
-
-            // Another request offline
-            const moneyRequestOffline = createIOUReportAction('create', 1000, 'AED', true);
-
-            // Delete the request made offline
-            deleteMoneyRequest(moneyRequestOffline, true);
-
-            expect(IOUUtils.isIOUReportPendingCurrencyConversion(reportActions, iouReport)).toBe(false);
-        });
-
-        test('Deleting a request made online while we have one made offline will show the pending conversion message', () => {
-            // Request money online in AED
-            const moneyRequestOnline = createIOUReportAction('create', 1000, 'AED');
-
-            // Request money again but offline
-            createIOUReportAction('create', 1000, 'AED', true);
-
-            // Delete the request made online
-            deleteMoneyRequest(moneyRequestOnline, true);
-
-            // We don't know what the total is because we need to subtract the converted amount of the offline request from the total
-            expect(IOUUtils.isIOUReportPendingCurrencyConversion(reportActions, iouReport)).toBe(true);
-        });
-
-        test("Deleting a request offline in the report's currency when we have requests in a different currency does not show the pending conversion message", () => {
-            // Request money in the report's currency (USD)
-            const onlineMoneyRequestInUSD = createIOUReportAction('create', 1000, 'USD');
-
-            // Request money online in a different currency
-            createIOUReportAction('create', 2000, 'AED');
-
-            // Delete the USD request offline
-            deleteMoneyRequest(onlineMoneyRequestInUSD, true);
-
-            expect(IOUUtils.isIOUReportPendingCurrencyConversion(reportActions, iouReport)).toBe(false);
-        });
+    
     });
 
     describe('calculateAmount', () => {
