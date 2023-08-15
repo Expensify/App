0c12bc8f9f3b31f2160d1a7395af970e9f20effd
Vit Horacek <vit@expensify.com>
2023-08-16 00:22:45 +0100

    Refactor

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -1894,21 +1894,23 @@ function buildOptimisticIOUReportAction(
         type,
     };
 
-    // We store amount, comment, currency in IOUDetails when type = pay
-    if (type === CONST.IOU.REPORT_ACTION_TYPE.PAY && isSendMoneyFlow) {
-        _.each(['amount', 'comment', 'currency'], (key) => {
-            delete originalMessage[key];
-        });
-        originalMessage.IOUDetails = {amount, comment, currency};
-        originalMessage.paymentType = paymentType;
-    }
+    if (type === CONST.IOU.REPORT_ACTION_TYPE.PAY) {
+        // In send money flow, we store amount, comment, currency in IOUDetails when type = pay
+        if (isSendMoneyFlow) {
+            _.each(['amount', 'comment', 'currency'], (key) => {
+                delete originalMessage[key];
+            });
+            originalMessage.IOUDetails = {amount, comment, currency};
+            originalMessage.paymentType = paymentType;
+        }
 
-    // In case of pay money request action, we dont store the comment
-    // and there is no single transctionID to link the action to.
-    if (type === CONST.IOU.REPORT_ACTION_TYPE.PAY && !isSendMoneyFlow) {
-        delete originalMessage.IOUTransactionID;
-        delete originalMessage.comment;
-        originalMessage.paymentType = paymentType;
+        // In case of pay money request action, we dont store the comment
+        // and there is no single transctionID to link the action to.
+        if (!isSendMoneyFlow) {
+            delete originalMessage.IOUTransactionID;
+            delete originalMessage.comment;
+            originalMessage.paymentType = paymentType;
+        }
     }
 
     // IOUs of type split only exist in group DMs and those don't have an iouReport so we need to delete the IOUReportID key
