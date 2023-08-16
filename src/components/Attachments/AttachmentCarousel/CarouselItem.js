7dfcf176ed116ebffb6e955b5c4c861cf4a03221
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-08-16 21:28:07 +0800

    remove unused import

--- src/components/Attachments/AttachmentCarousel/CarouselItem.js
+++ src/components/Attachments/AttachmentCarousel/CarouselItem.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import CONST from '../../../CONST';
 import styles from '../../../styles/styles';
