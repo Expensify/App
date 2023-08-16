ad372bc3e885f98ffba2f63f00f34b03bee70ee6
Radoslaw Krzemien <radoslaw.krzemien@swmansion.com>
2023-08-16 11:36:06 +0200

    Merge from upstream
    Synced with upstream and updated testBuild tests
    
    See: https://github.com/Expensify/App/issues/13604

--- workflow_tests/testBuild.test.js
+++ workflow_tests/testBuild.test.js
@@ -232,7 +232,7 @@ describe('test workflow testBuild', () => {
                     web: mocks.TESTBUILD__WEB__STEP_MOCKS,
                     postGithubComment: mocks.TESTBUILD__POSTGITHUBCOMMENT__STEP_MOCKS,
                 };
-                testMockSteps.iOS[4] = utils.createMockStep('Install cocoapods', 'Install cocoapods', 'IOS', ['timeout_minutes', 'max_attempts', 'command'], [], {}, {}, false);
+                testMockSteps.iOS[5] = utils.createMockStep('Install cocoapods', 'Install cocoapods', 'IOS', ['timeout_minutes', 'max_attempts', 'command'], [], {}, {}, false);
                 const result = await act.runEvent(event, {
                     workflowFile: path.join(repoPath, '.github', 'workflows', 'testBuild.yml'),
                     mockSteps: testMockSteps,
@@ -243,7 +243,7 @@ describe('test workflow testBuild', () => {
                 assertions.assertValidateActorJobExecuted(result, '1234');
                 assertions.assertGetBranchRefJobExecuted(result);
                 assertions.assertAndroidJobExecuted(result, 'test-ref');
-                assertions.assertIOSJobExecuted(result, 'test-ref', true, 4);
+                assertions.assertIOSJobExecuted(result, 'test-ref', true, 5);
                 assertions.assertDesktopJobExecuted(result, 'test-ref');
                 assertions.assertWebJobExecuted(result, 'test-ref');
                 assertions.assertPostGithubCommentJobExecuted(result, 'test-ref', '1234', true, 'success', 'failure', 'success', 'success');
