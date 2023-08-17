2904336eed9be1c0ab83d2f0ad37e55158e36311
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-08-17 19:03:00 +0200

    integrate category picker to MoneyRequestCategoryPage

--- src/pages/iou/MoneyRequestCategoryPage.js
+++ src/pages/iou/MoneyRequestCategoryPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import lodashGet from 'lodash/get';
+import {withOnyx} from 'react-native-onyx';
 import ROUTES from '../../ROUTES';
 import Navigation from '../../libs/Navigation/Navigation';
 import useLocalize from '../../hooks/useLocalize';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import HeaderWithBackButton from '../../components/HeaderWithBackButton';
+import CategoryPicker from '../../components/CategoryPicker';
+import ONYXKEYS from '../../ONYXKEYS';
+import reportPropTypes from '../reportPropTypes';
 
 const propTypes = {
+    /** TODO: Comment */
     route: PropTypes.shape({
         /** Each parameter passed via the URL */
         params: PropTypes.shape({
@@ -18,14 +23,22 @@ const propTypes = {
             reportID: PropTypes.string,
         }),
     }).isRequired,
+
+    /** TODO: Comment */
+    report: reportPropTypes,
 };
 
-function MoneyRequestCategoryPage({route}) {
+const defaultProps = {
+    report: {},
+};
+
+function MoneyRequestCategoryPage({route, report}) {
     const {translate} = useLocalize();
+
+    const reportID = lodashGet(route, 'params.reportID', '');
 
     const navigateBack = () => {
         const iouType = lodashGet(route, 'params.iouType', '');
-        const reportID = lodashGet(route, 'params.reportID', '');
 
         Navigation.goBack(ROUTES.getMoneyRequestConfirmationRoute(iouType, reportID));
     };
@@ -39,11 +52,18 @@ function MoneyRequestCategoryPage({route}) {
                 title={translate('common.category')}
                 onBackButtonPress={navigateBack}
             />
+
+            <CategoryPicker policyID={report.policyID} />
         </ScreenWrapper>
     );
 }
 
 MoneyRequestCategoryPage.displayName = 'MoneyRequestCategoryPage';
 MoneyRequestCategoryPage.propTypes = propTypes;
+MoneyRequestCategoryPage.defaultProps = defaultProps;
 
-export default MoneyRequestCategoryPage;
+export default withOnyx({
+    report: {
+        key: ({route}) => `${ONYXKEYS.COLLECTION.REPORT}${lodashGet(route, 'params.reportID', '')}`,
+    },
+})(MoneyRequestCategoryPage);
