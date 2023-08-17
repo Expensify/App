545c533aaf908fcd54e9d753094b2128e0ad106e
Tim Golen <tgolen@expensify.com>
2023-08-17 08:32:30 -0600

    Add a method for stopping subscriptions

--- src/libs/actions/MapboxToken.js
+++ src/libs/actions/MapboxToken.js
@@ -15,10 +15,12 @@ Onyx.connect({
     },
 });
 
-let connectionID;
+let connectionIDForToken;
+let connectionIDForNetwork;
+let appStateSubscription;
 let currentToken;
 let refreshTimeoutID;
-const REFRESH_INTERVAL = 1000 * 60 * 25;
+const REFRESH_INTERVAL = 1000 * 60 * 1;
 
 const setExpirationTimer = () => {
     console.debug('[MapboxToken] refreshing token on an interval', REFRESH_INTERVAL, 'ms');
@@ -48,13 +50,13 @@ const clearToken = () => {
 };
 
 const init = () => {
-    if (connectionID) {
+    if (connectionIDForToken) {
         console.debug('[MapboxToken] init() is already listening to Onyx so returning early');
         return;
     }
 
     // When the token changes in Onyx, the expiration needs to be checked so a new token can be retrieved.
-    connectionID = Onyx.connect({
+    connectionIDForToken = Onyx.connect({
         key: ONYXKEYS.MAPBOX_ACCESS_TOKEN,
         /**
          * @param {Object} token
@@ -91,32 +93,50 @@ const init = () => {
         },
     });
 
-    AppState.addEventListener('change', (nextAppState) => {
-        // Skip getting a new token if:
-        // - The app state is not changing to active
-        // - There is no current token (which means it's not been fetch yet for the first time)
-        // - The token hasn't expired yet (this would just be a waste of an API call)
-        // - There is no authToken (which means the user has logged out)
-        if (nextAppState !== CONST.APP_STATE.ACTIVE || !currentToken || !hasTokenExpired() || !authToken) {
-            return;
-        }
-        console.debug('[MapboxToken] Token is expired after app became active');
-        clearToken();
-    });
-
-    let network;
-    Onyx.connect({
-        key: ONYXKEYS.NETWORK,
-        callback: (val) => {
-            // When the network reconnects, check if the token has expired. If it has, then clearing the token will
-            // trigger the fetch of a new one
-            if (network && network.isOffline && val && !val.isOffline) {
-                console.debug('[MapboxToken] Token is expired after network came online');
-                clearToken();
+    if (!appStateSubscription) {
+        appStateSubscription = AppState.addEventListener('change', (nextAppState) => {
+            // Skip getting a new token if:
+            // - The app state is not changing to active
+            // - There is no current token (which means it's not been fetch yet for the first time)
+            // - The token hasn't expired yet (this would just be a waste of an API call)
+            // - There is no authToken (which means the user has logged out)
+            if (nextAppState !== CONST.APP_STATE.ACTIVE || !currentToken || !hasTokenExpired() || !authToken) {
+                return;
             }
-            network = val;
-        },
-    });
+            console.debug('[MapboxToken] Token is expired after app became active');
+            clearToken();
+        });
+    }
+
+    if (!connectionIDForNetwork) {
+        let network;
+        connectionIDForNetwork = Onyx.connect({
+            key: ONYXKEYS.NETWORK,
+            callback: (val) => {
+                // When the network reconnects, check if the token has expired. If it has, then clearing the token will
+                // trigger the fetch of a new one
+                if (network && network.isOffline && val && !val.isOffline) {
+                    console.debug('[MapboxToken] Token is expired after network came online');
+                    clearToken();
+                }
+                network = val;
+            },
+        });
+    }
 };
 
-export default init;
+const stop = () => {
+    console.debug('[MapboxToken] Stopping all listeners and timers');
+    if (connectionIDForToken) {
+        Onyx.disconnect(connectionIDForToken);
+    }
+    if (connectionIDForNetwork) {
+        Onyx.disconnect(connectionIDForNetwork);
+    }
+    if (appStateSubscription) {
+        appStateSubscription.remove();
+    }
+    clearTimeout(refreshTimeoutID);
+};
+
+export {init, stop};
