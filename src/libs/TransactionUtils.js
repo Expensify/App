5d6e72aa53d4e1eb831287a5cd542477e5c6a017
Alberto <alberto@expensify.com>
2023-08-17 13:33:35 +0200

    only if there's a receipt

--- src/libs/TransactionUtils.js
+++ src/libs/TransactionUtils.js
@@ -86,7 +86,7 @@ function getUpdatedTransaction(transaction, transactionChanges, isFromExpenseRep
     if (_.has(transactionChanges, 'currency')) {
         updatedTransaction.modifiedCurrency = transactionChanges.currency;
     }
-    if(_.has(transaction, 'receipt') && lodashGet(transaction, 'receipt.state') !== CONST.IOU.RECEIPT_STATE.OPEN) {
+    if(_.has(transaction, 'receipt') && !_.isEmpty(transaction.receipt) && lodashGet(transaction, 'receipt.state') !== CONST.IOU.RECEIPT_STATE.OPEN) {
         updatedTransaction.receipt.state = CONST.IOU.RECEIPT_STATE.OPEN;
     }
     updatedTransaction.pendingAction = CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE;
