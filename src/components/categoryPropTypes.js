602aa40fcff636e01e894bbafe09b985d3e7d740
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-08-17 19:02:30 +0200

    create general category prop types

---
+++ src/components/categoryPropTypes.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export default PropTypes.shape({
+    /** TODO: Comment */
+    name: PropTypes.string.isRequired,
+
+    /** TODO: Comment */
+    enabled: PropTypes.bool.isRequired,
+});
