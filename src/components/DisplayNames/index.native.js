1cb14d0c454ad11d99ce76651e468cbba4e9f9e4
tienifr <christianwen18@gmail.com>
2023-08-18 15:00:15 +0700

    render markdown on native

--- src/components/DisplayNames/index.native.js
+++ src/components/DisplayNames/index.native.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {propTypes, defaultProps} from './displayNamesPropTypes';
 import Text from '../Text';
+import RenderHTML from "../RenderHTML";
 
 // As we don't have to show tooltips of the Native platform so we simply render the full display names list.
 function DisplayNames(props) {
+    const fullTitle = props.fullTitleHtml ? <RenderHTML html={props.fullTitleHtml} /> : props.fullTitle;
+
     return (
         <Text
             accessibilityLabel={props.accessibilityLabel}
             style={props.textStyles}
             numberOfLines={props.numberOfLines || undefined}
         >
-            {props.fullTitle}
+            {fullTitle}
         </Text>
     );
 }
