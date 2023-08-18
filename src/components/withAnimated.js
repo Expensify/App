16a2adfee112aaac2715976aa970099bf30072ec
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2023-08-18 02:35:57 +0000

    fix lint errors

--- src/components/withAnimated.js
+++ src/components/withAnimated.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Animated} from 'react-native';
 
-export function withAnimated(WrappedComponent) {
-    const displayName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
-
-    class WithAnimated extends React.Component {
-        static displayName = `WithAnimated(${displayName})`;
-
-        render() {
-            return <WrappedComponent {...this.props} />;
-        }
+// eslint-disable-next-line react/prefer-stateless-function
+class WithAnimated extends React.Component {
+    render() {
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        return <WrappedComponent {...this.props} />;
     }
+}
+
+function withAnimated(WrappedComponent) {
+    const displayName = WrappedComponent.displayName || WrappedComponent.name || 'Component';
+    WithAnimated.displayName = `WithAnimated(${displayName})`;
 
     return Animated.createAnimatedComponent(WithAnimated);
 }
+
+export default withAnimated;
