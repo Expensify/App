5b94b18aa1716246b613e6fe9fd1a583bc4b7303
Tim Golen <tgolen@expensify.com>
2023-08-18 10:27:50 -0600

    Change logging to use server logs

--- src/libs/actions/MemoryOnlyKeys/MemoryOnlyKeys.js
+++ src/libs/actions/MemoryOnlyKeys/MemoryOnlyKeys.js
@@ -1,16 +1,17 @@
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../../ONYXKEYS';
+import Log from '../../Log';
 
 const memoryOnlyKeys = [ONYXKEYS.COLLECTION.REPORT, ONYXKEYS.COLLECTION.POLICY, ONYXKEYS.PERSONAL_DETAILS_LIST];
 
 const enable = () => {
-    console.debug('[MemoryOnlyKeys] enabled');
+    Log.info('[MemoryOnlyKeys] enabled');
     Onyx.set(ONYXKEYS.IS_USING_MEMORY_ONLY_KEYS, true);
     Onyx.setMemoryOnlyKeys(memoryOnlyKeys);
 };
 
 const disable = () => {
-    console.debug('[MemoryOnlyKeys] disabled');
+    Log.info('[MemoryOnlyKeys] disabled');
     Onyx.set(ONYXKEYS.IS_USING_MEMORY_ONLY_KEYS, false);
     Onyx.setMemoryOnlyKeys([]);
 };
