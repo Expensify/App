9a241c408a22fd577682dddf9595eb95f05578ff
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-08-18 14:08:46 +0200

    clarify prop types

--- src/components/CategoryPicker/categoryPickerPropTypes.js
+++ src/components/CategoryPicker/categoryPickerPropTypes.js
@@ -3,13 +3,13 @@ import categoryPropTypes from '../categoryPropTypes';
 
 const propTypes = {
     /** The report ID of the IOU */
-    reportID: PropTypes.string,
+    reportID: PropTypes.string.isRequired,
 
     /** The policyID of we are getting categories */
     policyID: PropTypes.string,
 
     /** The type of IOU report, i.e. bill, request, send */
-    iouType: PropTypes.string,
+    iouType: PropTypes.string.isRequired,
 
     /** Collection of categories attached to a policy */
     policyCategories: PropTypes.objectOf(categoryPropTypes),
