560f56985940b8fbb586692da6c39b501ed1c4c4
Tim Golen <tgolen@expensify.com>
2023-08-18 10:25:53 -0600

    Improve comments, remove unnecessary code

--- src/libs/Middleware/SaveResponseInOnyx.js
+++ src/libs/Middleware/SaveResponseInOnyx.js
@@ -18,6 +18,15 @@ function SaveResponseInOnyx(response, request) {
             return;
         }
 
+        // The data for this response comes in two different formats:
+        // 1. Original format - this is what was sent before the RELIABLE_UPDATES project and will go away once RELIABLE_UPDATES is fully complete
+        //     - The data is an array of objects, where each object is an onyx update
+        //       Example: [{onyxMethod: 'whatever', key: 'foo', value: 'bar'}]
+        // 1. Reliable updates format - this is what was sent with the RELIABLE_UPDATES project and will be the format from now on
+        //     - The data is an object, containing updateIDs from the server and an array of onyx updates (this array is the same format as the original format above)
+        //       Example: {lastUpdateID: 1, previousUpdateID: 0, onyxData: [{onyxMethod: 'whatever', key: 'foo', value: 'bar'}]}
+        //       NOTE: This is slightly different than the format of the pusher event data, where pusher has "updates" and HTTPS responses have "onyxData" (long story)
+
         // Supports both the old format and the new format
         const onyxUpdates = _.isArray(responseData) ? responseData : responseData.onyxData;
         // If there is an OnyxUpdate for using memory only keys, enable them
