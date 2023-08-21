fec32bfa9d34b1b1993cbd21129ad1c4d855c42b
Andrew Li <andrewli@expensify.com>
2023-08-21 12:19:39 -0400

    Fix lint

--- src/components/Attachments/AttachmentCarousel/extractAttachmentsFromReport.js
+++ src/components/Attachments/AttachmentCarousel/extractAttachmentsFromReport.js
@@ -1,11 +1,11 @@
 import {Parser as HtmlParser} from 'htmlparser2';
 import _ from 'underscore';
+import lodashGet from 'lodash/get';
 import * as ReportActionsUtils from '../../../libs/ReportActionsUtils';
 import * as TransactionUtils from '../../../libs/TransactionUtils';
 import * as ReceiptUtils from '../../../libs/ReceiptUtils';
 import CONST from '../../../CONST';
 import tryResolveUrlFromApiRoot from '../../../libs/tryResolveUrlFromApiRoot';
-import lodashGet from 'lodash/get';
 
 /**
  * Constructs the initial component state from report actions
