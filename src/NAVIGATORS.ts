26ba0de104ac4fabea988b7cafea43795908a168
Parteek Goyal <parteekgoyal118@gmail.com>
2023-08-21 15:20:14 +0530

    migrating Navigator.js to Navigator.ts

--- src/NAVIGATORS.ts
+++ src/NAVIGATORS.ts
@@ -2,8 +2,17 @@
  * This is a file containing constants for navigators located directly in the RootStack in AuthScreens file
  * The ResponsiveStackNavigator displays stack differently based on these constants
  * */
-export default {
+
+type Navigator = {
+    CENTRAL_PANE_NAVIGATOR: string,
+    RIGHT_MODAL_NAVIGATOR: string,
+    FULL_SCREEN_NAVIGATOR: string,
+};
+
+const NAVIGATORS: Navigator =  {
     CENTRAL_PANE_NAVIGATOR: 'CentralPaneNavigator',
     RIGHT_MODAL_NAVIGATOR: 'RightModalNavigator',
     FULL_SCREEN_NAVIGATOR: 'FullScreenNavigator',
 };
+
+export default NAVIGATORS;
