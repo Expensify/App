f48525f3ee419c9303a3b9ec09f7984af2047a55
Hayata Suenaga <hayata@expensify.com>
2023-08-21 07:24:33 -0700

    style: simplify conditional check

--- scripts/setup-mapbox-sdk-ci.ts
+++ scripts/setup-mapbox-sdk-ci.ts
@@ -14,7 +14,7 @@ import {saveMapboxToken} from './setup-mapbox-sdk';
 const token = process.argv[2];
 
 // Check if the token is provided on the command argument
-if (token === undefined) {
+if (!token) {
     console.error('Error: An argument is required for this script.');
     console.error('Usage: ts-node ./scripts/setup-mapbox-sdk-ci.ts <YOUR_MAPBOX_TOKEN>');
     process.exit(1); // Exit with a failure status
