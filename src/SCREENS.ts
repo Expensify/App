fbc5d6014889e6a82c1bbeb33d1242183eedff28
Parteek Goyal <parteekgoyal118@gmail.com>
2023-08-21 14:40:27 +0530

    migrating SCREENS.js

--- src/SCREENS.ts
+++ src/SCREENS.ts
@@ -2,7 +2,23 @@
  * This is a file containing constants for all of the screen names. In most cases, we should use the routes for
  * navigation. But there are situations where we may need to access screen names directly.
  */
-export default {
+
+type Screen = {
+    HOME: string,
+    LOADING: string,
+    REPORT: string,
+    REPORT_ATTACHMENTS: string,
+    NOT_FOUND: string,
+    TRANSITION_BETWEEN_APPS: string,
+    SETTINGS: {
+        PREFERENCES: string,
+        WORKSPACES: string,
+    },
+    SIGN_IN_WITH_APPLE_DESKTOP: string,
+    SIGN_IN_WITH_GOOGLE_DESKTOP: string,
+}
+
+const SCREENS: Screen = {
     HOME: 'Home',
     LOADING: 'Loading',
     REPORT: 'Report',
@@ -16,3 +32,5 @@ export default {
     SIGN_IN_WITH_APPLE_DESKTOP: 'AppleSignInDesktop',
     SIGN_IN_WITH_GOOGLE_DESKTOP: 'GoogleSignInDesktop',
 };
+
+export default SCREENS;
