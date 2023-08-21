8ad5bd7bec5112bede14cdaf014cf33dafd7b570
Parteek Goyal <parteekgoyal118@gmail.com>
2023-08-21 16:30:03 +0530

    migrate roundToNearestMultipleOfFour.js to roundToNearestMultipleOfFour.ts

--- src/styles/roundToNearestMultipleOfFour.ts
+++ src/styles/roundToNearestMultipleOfFour.ts
@@ -2,11 +2,8 @@
  * This repo is very consistent about doing spacing in multiples of 4.
  * In an effort to maintain that consistency, we'll make sure that any distance we're shifting the components
  * are a multiple of 4.
- *
- * @param {Number} n
- * @returns {Number}
  */
-function roundToNearestMultipleOfFour(n) {
+function roundToNearestMultipleOfFour(n: number): number {
     if (n > 0) {
         return Math.ceil(n / 4.0) * 4;
     }
