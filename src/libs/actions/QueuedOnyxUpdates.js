1eb31bc8cbd51cb0edf4966dbba970e5570e4198
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-08-21 09:01:26 -1000

    Save queued updates to memory only

--- src/libs/actions/QueuedOnyxUpdates.js
+++ src/libs/actions/QueuedOnyxUpdates.js
@@ -1,24 +1,18 @@
-import Onyx from 'react-native-onyx';
-import ONYXKEYS from '../../ONYXKEYS';
-
-// In this file we manage a queue of Onyx updates while the SequentialQueue is processing. There are functions to get the updates and clear the queue after saving the updates in Onyx.
+// In this file we manage a queue of Onyx updates while the SequentialQueue is processing. There are functions to get the updates and clear the queue after saving the updates.
 
 let queuedOnyxUpdates = [];
-Onyx.connect({
-    key: ONYXKEYS.QUEUED_ONYX_UPDATES,
-    callback: (val) => (queuedOnyxUpdates = val || []),
-});
 
 /**
  * @param {Array<Object>} updates Onyx updates to queue for later
- * @returns {Promise}
+ * @returns {Promise<void>}
  */
 function queueOnyxUpdates(updates) {
-    return Onyx.set(ONYXKEYS.QUEUED_ONYX_UPDATES, [...queuedOnyxUpdates, ...updates]);
+    queuedOnyxUpdates.concat(updates);
+    return Promise.resolve();
 }
 
 function clear() {
-    Onyx.set(ONYXKEYS.QUEUED_ONYX_UPDATES, null);
+    queuedOnyxUpdates = [];
 }
 
 /**
