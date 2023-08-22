90b941e2445282e71b773c1f3746b7d522d097e5
Jack Nam <jack@expensify.com>
2023-08-22 11:30:52 -0700

    lint

--- src/components/DistanceRequest.js
+++ src/components/DistanceRequest.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, View} from 'react-native';
 import lodashGet from 'lodash/get';
 import _ from 'underscore';
