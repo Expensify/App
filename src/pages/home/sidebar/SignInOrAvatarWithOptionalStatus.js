5088e86f6302650f887d4b15a980ec03a4265f5a
Taras Perun <terry.perun@gmail.com>
2023-08-22 10:04:59 +0200

    a bit more optimizations

--- src/pages/home/sidebar/SignInOrAvatarWithOptionalStatus.js
+++ src/pages/home/sidebar/SignInOrAvatarWithOptionalStatus.js
@@ -1,24 +1,17 @@
 /* eslint-disable rulesdir/onyx-props-must-have-default */
-import React, {useCallback} from 'react';
-import {View} from 'react-native';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import lodashGet from 'lodash/get';
 import withCurrentUserPersonalDetails from '../../../components/withCurrentUserPersonalDetails';
-import PressableWithoutFeedback from '../../../components/Pressable/PressableWithoutFeedback';
 import personalDetailsPropType from '../../personalDetailsPropType';
 import PressableAvatarWithIndicator from './PressableAvatarWithIndicator';
-import Text from '../../../components/Text';
-import styles from '../../../styles/styles';
-import Navigation from '../../../libs/Navigation/Navigation';
+import AvatarWithOptionalStatus from './AvatarWithOptionalStatus';
+import SignInButton from './SignInButton';
 import * as Session from '../../../libs/actions/Session';
 import Permissions from '../../../libs/Permissions';
 import compose from '../../../libs/compose';
-import useLocalize from '../../../hooks/useLocalize';
 import ONYXKEYS from '../../../ONYXKEYS';
-import ROUTES from '../../../ROUTES';
-import CONST from '../../../CONST';
-import SignInButton from './SignInButton';
 
 const propTypes = {
     /** The personal details of the person who is logged in */
@@ -40,43 +33,18 @@ const defaultProps = {
 };
 
 function SignInOrAvatarWithOptionalStatus({currentUserPersonalDetails, isCreateMenuOpen, betas}) {
-    const {translate} = useLocalize();
     const statusEmojiCode = lodashGet(currentUserPersonalDetails, 'status.emojiCode', '');
     const emojiStatus = Permissions.canUseCustomStatus(betas) ? statusEmojiCode : '';
-
-    const showStatusPage = useCallback(() => {
-        if (isCreateMenuOpen) {
-            // Prevent opening Settings page when click profile avatar quickly after clicking FAB icon
-            return;
-        }
-
-        Navigation.setShouldPopAllStateOnUP();
-        Navigation.navigate(ROUTES.SETTINGS_STATUS);
-    }, [isCreateMenuOpen]);
 
     if (Session.isAnonymousUser()) {
         return <SignInButton />;
     }
     if (emojiStatus) {
         return (
-            <View style={styles.sidebarStatusAvatarContainer}>
-                <PressableWithoutFeedback
-                    accessibilityLabel={translate('sidebarScreen.buttonMySettings')}
-                    accessibilityRole={CONST.ACCESSIBILITY_ROLE.BUTTON}
-                    onPress={showStatusPage}
-                    style={styles.flex1}
-                >
-                    <View style={styles.sidebarStatusAvatar}>
-                        <Text
-                            style={styles.emojiStatusLHN}
-                            numberOfLines={1}
-                        >
-                            {emojiStatus}
-                        </Text>
-                    </View>
-                </PressableWithoutFeedback>
-                <PressableAvatarWithIndicator isCreateMenuOpen={isCreateMenuOpen} />
-            </View>
+            <AvatarWithOptionalStatus
+                emojiStatus={emojiStatus}
+                isCreateMenuOpen={isCreateMenuOpen}
+            />
         );
     }
     return <PressableAvatarWithIndicator isCreateMenuOpen={isCreateMenuOpen} />;
