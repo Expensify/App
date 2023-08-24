b63c0f91fdea7b733c8f560beda61c75268cac57
Rory Abraham <47436092+roryabraham@users.noreply.github.com>
2023-08-23 17:23:50 -0700

    Merge pull request #25799 from 0xmiroslav/fix/25797-google-icon-scroll
    [CP Staging] fix "Google icon on sign in page shows overflow horizontal scrolling bars"
    
    (cherry picked from commit 4fcc51f351509d1425ec8caa3f21c43cd4fcfcd9)

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3641,7 +3641,7 @@ const styles = {
         height: 40,
         marginLeft: 12,
         alignItems: 'center',
-        overflowY: 'hidden',
+        overflow: 'hidden',
     },
 
     googlePillButtonContainer: {
