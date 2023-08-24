4ddd9e32652e104f8b8bcc0d090cd5decc2b245e
Fábio Henriques <contact@fabiohenriques.dev>
2023-08-24 15:46:19 +0100

    Migrate file to TS

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -6,11 +6,8 @@ import {PixelRatio} from 'react-native';
  * This means that if you have text and its 'fontSize' is 19, the device font size changed to the 5th level on the iOS slider and the actual fontSize is 19 * PixelRatio.getFontScale()
  * = 19 * 1.11 = 21.09. Since we are disallowing font scaling we need to calculate it manually. We calculate it with: PixelRatio.getFontScale() * defaultValue > maxValue ? maxValue :
  * defaultValue * PixelRatio getFontScale() This means that the fontSize is increased/decreased when the device font size changes up to maxValue (second param)
- * @param {Number} defaultValue
- * @param {Number} maxValue
- * @returns {Number}
  */
-function getValueUsingPixelRatio(defaultValue, maxValue) {
+function getValueUsingPixelRatio(defaultValue: number, maxValue: number): number {
     return PixelRatio.getFontScale() * defaultValue > maxValue ? maxValue : defaultValue * PixelRatio.getFontScale();
 }
 
@@ -154,4 +151,4 @@ export default {
     qrShareHorizontalPadding: 32,
 
     baseMenuItemHeight: 64,
-};
+} as const;
