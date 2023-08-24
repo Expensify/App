a071a44940b9457593df829d8715c6b2e47b4cc5
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-08-24 16:22:56 +0200

    Cursor styles keys

--- src/styles/utilities/cursor/types.ts
+++ src/styles/utilities/cursor/types.ts
@@ -1,5 +1,18 @@
 import {CSSProperties} from 'react';
 
-type CursorStyles = Record<string, Partial<Pick<CSSProperties, 'cursor'>>>;
+type CursorStylesKeys =
+    | 'cursorDefault'
+    | 'cursorDisabled'
+    | 'cursorPointer'
+    | 'cursorMove'
+    | 'cursorUnset'
+    | 'cursorAuto'
+    | 'cursorZoomIn'
+    | 'cursorGrabbing'
+    | 'cursorZoomOut'
+    | 'cursorInitial'
+    | 'cursorText';
+
+type CursorStyles = Record<CursorStylesKeys, Partial<Pick<CSSProperties, 'cursor'>>>;
 
 export default CursorStyles;
