e1d0f9e55d06cd9e113955c5d8dc4ef34c38cb4d
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-08-25 10:16:28 +0200

    convert ThemeStylesContext to TypeScript

--- src/styles/ThemeStylesContext.ts
+++ src/styles/ThemeStylesContext.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles';
 
-const ThemeStylesContext = React.createContext(styles);
+const ThemeStylesContext = React.createContext<typeof styles>(styles);
 
 export default ThemeStylesContext;
