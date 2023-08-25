6b1a41e3d2c9d34cad7f779b34525f89b90bcdfd
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-08-25 15:32:30 +0200

    Simplify types

--- src/styles/getNavigationModalCardStyles/types.ts
+++ src/styles/getNavigationModalCardStyles/types.ts
@@ -3,8 +3,7 @@ import {ViewStyle} from 'react-native';
 import {Merge} from 'type-fest';
 
 type GetNavigationModalCardStylesParams = {isSmallScreenWidth: number};
-type GetNavigationModalCardStylesKeys = 'position' | 'top' | 'right' | 'width' | 'backgroundColor' | 'height';
 
-type GetNavigationModalCardStyles = ({isSmallScreenWidth}: GetNavigationModalCardStylesParams) => Merge<Pick<ViewStyle, GetNavigationModalCardStylesKeys>, Pick<CSSProperties, 'position'>>;
+type GetNavigationModalCardStyles = (params: GetNavigationModalCardStylesParams) => Merge<ViewStyle, Pick<CSSProperties, 'position'>>;
 
 export default GetNavigationModalCardStyles;
