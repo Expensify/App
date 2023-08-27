f371f95b89e8756bbd885ecc70bbfcaaeb5d8727
Huzaifa Rasheed <huzaifarasheed900@gmail.com>
2023-08-28 00:04:25 +0500

    Fixed type

--- src/components/LocationErrorMessage/index.js
+++ src/components/LocationErrorMessage/index.js
@@ -7,7 +7,7 @@ const propTypes = {};
 const defaultProps = {};
 
 function LocationErrorMessage() {
-    /** opens expensfiy help site in a new browser tab */
+    /** opens expensify help site in a new browser tab */
     const navigateToExpensifyHelpSite = () => {
         Linking.openURL(CONST.NEWHELP_URL);
     };
