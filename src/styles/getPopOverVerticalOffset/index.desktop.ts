dec8f403fd866701c1408fccb2c7289ec6d2f7da
Yauheni Pasiukevich <pasyukevich@live.com>
2023-08-28 15:35:56 +0200

    fix comment for getPopOverVerticalOffset

--- src/styles/getPopOverVerticalOffset/index.desktop.ts
+++ src/styles/getPopOverVerticalOffset/index.desktop.ts
@@ -1,7 +1,7 @@
 import CONST from '../../CONST';
 import GetPopOverVerticalOffset from './types';
 
-// Adds the header padding with vertical offset on desktop
+/** Adds the header padding with vertical offset on desktop */
 const getPopOverVerticalOffset: GetPopOverVerticalOffset = (vertical) => ({
     // We add CONST.DESKTOP_HEADER_GAP on desktop which we
     // need to add to vertical offset to have proper vertical
