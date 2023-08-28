52070c8a61a11f81206d13186a3ef8794d2a6f23
s-alves10 <sangar.work1028@gmail.com>
2023-08-28 18:37:14 -0500

    fix: lint error

--- src/pages/iou/WaypointEditor.js
+++ src/pages/iou/WaypointEditor.js
@@ -37,7 +37,7 @@ const propTypes = {
     }),
 
     /** Recent waypoints array */
-    recentWaypoints: PropTypes.array,
+    recentWaypoints: PropTypes.arrayOf(PropTypes.object),
 
     /** The optimistic transaction for this request */
     transaction: transactionPropTypes,
