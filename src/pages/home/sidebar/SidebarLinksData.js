2734584bfc8938f13d56291cfbe00673ee547dd1
s-alves10 <sangar.work1028@gmail.com>
2023-08-28 15:16:56 -0500

    fix: same behavior on all platforms

--- src/pages/home/sidebar/SidebarLinksData.js
+++ src/pages/home/sidebar/SidebarLinksData.js
@@ -75,8 +75,7 @@ function SidebarLinksData({isFocused, allReportActions, betas, chatReports, curr
         }
 
         // We need to update existing reports only once while loading because they are updated several times during loading and causes this reguression: https://github.com/Expensify/App/issues/24596#issuecomment-1681679531
-        // Also we should not update options list when currentReportID is not null: https://github.com/Expensify/App/issues/23735
-        if (isLoading && !reportIDsRef.current && !currentReportID) {
+        if ((isLoading && !reportIDsRef.current) || (_.isEmpty(reportIDsRef.current) && currentReportID)) {
             reportIDsRef.current = reportIDs;
         }
         if (!isLoading) {
