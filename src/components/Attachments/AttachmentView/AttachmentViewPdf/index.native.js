e381d0af43524a561fd0667b93dc6c2a95f4d5bc
pradeepkumar <kongupradeep@live.com>
2023-08-29 21:03:06 +0530

    update fix

--- src/components/Attachments/AttachmentView/AttachmentViewPdf/index.native.js
+++ src/components/Attachments/AttachmentView/AttachmentViewPdf/index.native.js
@@ -8,11 +8,11 @@ function AttachmentViewPdf({file, encryptedSourceUrl, isFocused, isUsedInCarouse
     const attachmentCarouselPagerContext = useContext(AttachmentCarouselPagerContext);
 
     useEffect(() => {
-        if(attachmentCarouselPagerContext) {
+        if (attachmentCarouselPagerContext) {
             attachmentCarouselPagerContext.onPinchGestureChange(false);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps -- we just want to call this function when component is mounted
-    }, [attachmentCarouselPagerContext]);
+    }, []);
 
     const onScaleChanged = useCallback(
         (scale) => {
