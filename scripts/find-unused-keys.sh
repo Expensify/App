fd39606656ba36778e293292ecbfd49a5feaa893
Edu <gedu07@gmail.com>
2023-08-29 16:30:12 +0200

    showing unused style keys

--- scripts/find-unused-keys.sh
+++ scripts/find-unused-keys.sh
@@ -11,24 +11,58 @@ REMOVAL_KEYS_FILE="scripts/removal_keys_list_temp.txt"
   if [ ! -f "$REMOVAL_KEYS_FILE" ]; then
       touch "$REMOVAL_KEYS_FILE"
   fi
+
+# Read the style file with unused keys
+show_unused_style_keywords() {
+  while IFS=: read -r key file line_number; do
+    line_count=$(wc -l < $STYLES_KEYS_FILE)
+    echo "Unused keys: $line_count"
+    echo "File: $file"
+    
+    # Get lines before and after the error line
+    lines_before=$((line_number - 3))
+    lines_after=$((line_number + 3))
+    
+    # Print context of the error line
+    echo "Context around line $line_number:"
+    sed -n "$lines_before,$lines_after p" "$file" | awk -v key="$key" '{gsub(key, "\033[1;31m"key"\033[0m"); print}'
+    
+    echo "Unused key: $key"
+    echo "--------------------------------"
+  done < "$STYLES_KEYS_FILE"
+}
+
 # Function to remove a keyword from the temp file
 remove_keyword() {
 
   keyword="$1"
   # echo "Removing $keyword"
   grep -v "$keyword" "$STYLES_KEYS_FILE" > "$REMOVAL_KEYS_FILE"
+  line_count=$(wc -l < $REMOVAL_KEYS_FILE)
+  # echo "$REMOVAL_KEYS_FILE lines in the file: $line_count"
   mv "$REMOVAL_KEYS_FILE" "$STYLES_KEYS_FILE"
+  # echo "$STYLES_KEYS_FILE UPDATED lines in the file: $line_count"
 }
 
 lookfor_unused_keywords() {
   # Loop through all files in the src folder
-  find src -type f -name "*.js" -o -name "*.jsx" -o -name "*.ts" -o -name "*.tsx" | while read -r file; do
+  find 'src' -type f -name "*.js" -o -name "*.jsx" -o -name "*.ts" -o -name "*.tsx" | while read -r file; do
       # echo "Checking $file"
       # Search for keywords starting with "styles"
-       grep -o '\bstyles\.[a-zA-Z0-9_.]*' "$file" | while IFS= read -r keyword; do
+      #  grep -o '\bstyles\.[a-zA-Z0-9_.]*' "$file" | while IFS= read -r keyword; do
+      grep -E -o '\bstyles\.[a-zA-Z0-9_.]*' "$file" | grep -v '\/\/' | grep -vE '\/\*.*\*\/' | while IFS= read -r keyword; do
+
         # Remove any [ ] characters from the keyword
+        # echo "File: $file"
         clean_keyword="${keyword//[\[\]]/}"
-        remove_keyword "$clean_keyword"
+        # skip styles. keyword that might be used in comments
+        if [[ "$clean_keyword" == "styles." ]]; then
+          continue
+        fi
+          # echo "Found $clean_keyword"
+          # Remove the keyword from the temp file
+          remove_keyword "$clean_keyword"
+        
     done
   done
 }
@@ -64,7 +98,12 @@ find_styles_and_store_keys() {
         key="$parent_key_trimmed.$key_trimmed"
       fi
     
-      echo "$key:$file:$line_number" >> "$STYLES_KEYS_FILE"
+      # echo "$key:$file:$line_number" >> "$STYLES_KEYS_FILE"
+      if [[ "$key" == "styles."* ]]; then
+        echo "$key:$file:$line_number" >> "$STYLES_KEYS_FILE"
+      else
+        echo "styles.$key:$file:$line_number" >> "$STYLES_KEYS_FILE"
+      fi
       parent_keys+=("$key")
     elif [[ "$line" =~ ^[[:space:]]*\} ]]; then
       # unset "parent_keys[${#parent_keys[@]}-1]"
@@ -140,3 +179,5 @@ echo "Unused keys are into to $STYLES_KEYS_FILE"
 line_count2=$(wc -l < $STYLES_KEYS_FILE)
 echo "Number of lines in the file: $line_count2"
 # cat "$STYLES_KEYS_FILE"
+
+show_unused_style_keywords
