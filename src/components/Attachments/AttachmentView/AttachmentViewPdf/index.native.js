cbfd403726d0add26607d4c47a6352157e9ff5db
Andrew Li <12980971+Li357@users.noreply.github.com>
2023-08-29 14:11:26 -0400

    Merge pull request #26218 from pradeepmdk/fix/26193-PDF-displays-error
    (cherry picked from commit a156ccfec755a57bee9d4bf435a4bbb0bbb8296e)

--- src/components/Attachments/AttachmentView/AttachmentViewPdf/index.native.js
+++ src/components/Attachments/AttachmentView/AttachmentViewPdf/index.native.js
@@ -8,6 +8,9 @@ function AttachmentViewPdf({file, encryptedSourceUrl, isFocused, isUsedInCarouse
     const attachmentCarouselPagerContext = useContext(AttachmentCarouselPagerContext);
 
     useEffect(() => {
+        if (!attachmentCarouselPagerContext) {
+            return;
+        }
         attachmentCarouselPagerContext.onPinchGestureChange(false);
         // eslint-disable-next-line react-hooks/exhaustive-deps -- we just want to call this function when component is mounted
     }, []);
