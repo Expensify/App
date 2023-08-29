b5b03db9dca346be3ae83d51b17ad00089d71221
Huzaifa Rasheed <huzaifarasheed900@gmail.com>
2023-08-29 12:33:53 +0500

    Copy email with text when iou message is copied

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -393,7 +393,7 @@ function getLastMessageTextForReport(report) {
         lastMessageTextFromReport = ReportUtils.getReportPreviewMessage(report, lastReportAction, true);
     } else if (ReportActionUtils.isReportPreviewAction(lastReportAction)) {
         const iouReport = ReportUtils.getReport(ReportActionUtils.getIOUReportIDFromReportActionPreview(lastReportAction));
-        lastMessageTextFromReport = ReportUtils.getReportPreviewMessage(iouReport, lastReportAction);
+        lastMessageTextFromReport = ReportUtils.getReportPreviewMessage(iouReport, lastReportAction, false);
     } else if (ReportActionUtils.isModifiedExpenseAction(lastReportAction)) {
         lastMessageTextFromReport = ReportUtils.getModifiedExpenseMessage(lastReportAction);
     } else {
