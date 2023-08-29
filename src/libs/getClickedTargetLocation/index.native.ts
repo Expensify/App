2c19b6c4a131c541fa4cb992f45ebf5c0b7ebb1e
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-08-29 12:29:48 +0200

    [TS migration] Migrate 'getClickedTargetLocation' lib to TypeScript

---
+++ src/libs/getClickedTargetLocation/index.native.ts
@@ -0,0 +1,9 @@
+/**
+ * We don't need to get the position of the element on native platforms because the popover will be bottom mounted
+ */
+
+import GetClickedTargetLocation from './types';
+
+const getClickedTargetLocation: GetClickedTargetLocation = () => ({top: 0, bottom: 0, left: 0, right: 0, height: 0, x: 0, y: 0});
+
+export default getClickedTargetLocation;
