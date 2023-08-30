7fe7fe5c9e297e500d087637472713c491f222cd
Blazej Kustra <blazej.kustra@swmansion.com>
2023-08-30 11:05:40 +0200

    Add comment

--- src/types/modules/react-native.d.ts
+++ src/types/modules/react-native.d.ts
@@ -3,6 +3,7 @@ import 'react-native';
 
 declare module 'react-native' {
     interface TextInput {
+        // Typescript type declaration is missing in React Native for setting text selection.
         setSelection: (start: number, end: number) => void;
     }
 }
