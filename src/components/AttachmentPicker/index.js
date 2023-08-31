08a94be8e60846fa6ca7f14c4bf00d919b462c1b
ntdiary <2471314@gmail.com>
2023-08-31 11:55:12 +0800

    add a comment for visibility listener

--- src/components/AttachmentPicker/index.js
+++ src/components/AttachmentPicker/index.js
@@ -57,6 +57,9 @@ function AttachmentPicker(props) {
                     fileInput.current.addEventListener(
                         'cancel',
                         () => {
+                            // For Android Chrome, the cancel event happens before the page is visible on physical devices,
+                            // which makes it unreliable for us to show the keyboard, while on emulators it happens after the page is visible.
+                            // So here we can delay calling the onCanceled.current function based on visibility in order to reliably show the keyboard. 
                             if (Visibility.isVisible()) {
                                 onCanceled.current();
                                 return;
