98c56276e5d4d6ebed387312ccfa93c7c73aa6ac
Carlos Martins <cmartins@expensify.com>
2023-08-31 15:24:33 -0600

    add onLinkPress to NotFoundPage

--- src/pages/ErrorPage/NotFoundPage.js
+++ src/pages/ErrorPage/NotFoundPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import FullPageNotFoundView from '../../components/BlockingViews/FullPageNotFoundView';
+import Navigation from '../../libs/Navigation/Navigation';
 
 // eslint-disable-next-line rulesdir/no-negated-variables
 function NotFoundPage() {
     return (
         <ScreenWrapper>
-            <FullPageNotFoundView shouldShow />
+            <FullPageNotFoundView
+                shouldShow 
+                onLinkPress={Navigation.navigate}
+            />
         </ScreenWrapper>
     );
 }
