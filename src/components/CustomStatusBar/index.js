9d281557e187a502e9c36c8a4212730fe0270301
Sibtain Ali <allroundexperts@gmail.com>
2023-08-31 15:49:36 +0500

    fix: add a comment

--- src/components/CustomStatusBar/index.js
+++ src/components/CustomStatusBar/index.js
@@ -6,6 +6,9 @@ import themeColors from '../../styles/themes/default';
 function CustomStatusBar() {
     useEffect(() => {
         Navigation.isNavigationReady().then(() => {
+            // Set the status bar colour depending on the current route.
+            // If we don't have any colour defined for a route, fall back to
+            // appBG color.
             const currentRoute = navigationRef.getCurrentRoute();
             let currentScreenBackgroundColor = themeColors.appBG;
             if (currentRoute && 'name' in currentRoute && currentRoute.name in themeColors.PAGE_BACKGROUND_COLORS) {
