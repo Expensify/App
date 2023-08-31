4857735eaaf2f5274c15e824f71b3851156d6e01
Hayata Suenaga <hayata@expensify.com>
2023-08-31 15:41:06 -0700

    Revert "fix: hidden button"
    This reverts commit df9e8c811b6b38a0d530c728641eebdd748c52a6.

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3874,11 +3874,6 @@ const styles = {
         overflow: 'hidden',
     },
 
-    distanceRequestNextButton: {
-        position: 'absolute',
-        bottom: 0,
-    },
-
     confirmationListMapItem: {
         ...spacing.m5,
         height: 200,
