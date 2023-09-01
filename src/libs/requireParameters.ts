b59c4df941b658697791a5746ea1b94ff97ad987
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-09-01 10:53:23 +0200

    [TS migration] Migrate 'requireParameters.js' lib to TypeScript

--- src/libs/requireParameters.ts
+++ src/libs/requireParameters.ts
@@ -1,24 +1,25 @@
-import _ from 'underscore';
-
 /**
  * @throws {Error} If the "parameters" object has a null or undefined value for any of the given parameterNames
  *
- * @param {String[]} parameterNames Array of the required parameter names
- * @param {Object} parameters A map from available parameter names to their values
- * @param {String} commandName The name of the API command
+ * @param parameterNames Array of the required parameter names
+ * @param parameters A map from available parameter names to their values
+ * @param commandName The name of the API command
  */
-export default function requireParameters(parameterNames, parameters, commandName) {
+export default function requireParameters(parameterNames: string[], parameters: Record<string, unknown>, commandName: string): void {
     parameterNames.forEach((parameterName) => {
-        if (_(parameters).has(parameterName) && parameters[parameterName] !== null && parameters[parameterName] !== undefined) {
+        if (parameterName in parameters && parameters[parameterName] !== null && parameters[parameterName] !== undefined) {
             return;
         }
 
         const propertiesToRedact = ['authToken', 'password', 'partnerUserSecret', 'twoFactorAuthCode'];
-        const parametersCopy = _.chain(parameters)
-            .clone()
-            .mapObject((val, key) => (_.contains(propertiesToRedact, key) ? '<redacted>' : val))
-            .value();
-        const keys = _(parametersCopy).keys().join(', ') || 'none';
+        const parametersCopy = {...parameters};
+        Object.keys(parametersCopy).forEach((key) => {
+            if (!propertiesToRedact.includes(key.toString())) return;
+
+            parametersCopy[key] = '<redacted>';
+        });
+
+        const keys = Object.keys(parametersCopy).join(', ') || 'none';
 
         let error = `Parameter ${parameterName} is required for "${commandName}". `;
         error += `Supplied parameters: ${keys}`;
