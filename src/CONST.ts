065a6dd0a9ee95ea7ff7928fdce527648bb6d6d0
Gegham Khachatryan <gegham.k90@gmail.com>
2023-09-01 21:07:12 +0400

    Map panning fix (#2)
    * Fix Map Panning behavior
    
    * Add screen focus state props to MapView
    
    Added new props to avoid introducing unnecessary dependencies ('@react-navigation/native') to the 'react-native-x-maps' library.
    
    * feat: Move code from react-native-x-maps to the App
    
    * fix: rename mapbox consts
    
    * fix: Fix app crashing on waypoint delete
    
    * Remove unnecessary re exports
    
    * Fix react duplicate keys #35
    
    * Fix Not Found Page appearing issue and add comment
    
    * Fix panning issue
    
    * fix: add PropTypes import which is mised during conflict resolution

--- src/CONST.ts
+++ src/CONST.ts
@@ -2604,7 +2604,6 @@ const CONST = {
 
     SF_COORDINATES: [-122.4194, 37.7749],
 
-    MAPBOX_STYLE_URL: 'mapbox://styles/expensify/cllcoiqds00cs01r80kp34tmq',
     NAVIGATION: {
         TYPE: {
             FORCED_UP: 'FORCED_UP',
@@ -2622,6 +2621,13 @@ const CONST = {
     DEMO_PAGES: {
         SAASTR: 'SaaStrDemoSetup',
         SBE: 'SbeDemoSetup',
+    },
+
+    MAPBOX: {
+        PADDING: 50,
+        DEFAULT_ZOOM: 10,
+        DEFAULT_COORDINATE: [-122.4021, 37.7911],
+        STYLE_URL: 'mapbox://styles/expensify/cllcoiqds00cs01r80kp34tmq',
     },
 } as const;
 
