d779eecd92326ab69a08bcaee194536412ba7b90
Eduardo <eduardo.graciano@callstack.com>
2023-09-01 08:05:43 +0200

    removed translation code

--- scripts/find-unused-keys.sh
+++ scripts/find-unused-keys.sh
@@ -3,14 +3,14 @@
 # Configurations
 SRC_DIR="src"
 STYLES_FILE="src/styles/styles.js"
-TRANSLATION_FILES=("src/languages/es.js" "src/languages/en.js")
 STYLES_KEYS_FILE="scripts/style_keys_list_temp.txt"
 TRANSLATION_KEYS_FILE="scripts/translations_keys_list_temp.txt"
 REMOVAL_KEYS_FILE="scripts/removal_keys_list_temp.txt"
-  # Create an empty temp file if it doesn't exist
-  if [ ! -f "$REMOVAL_KEYS_FILE" ]; then
-      touch "$REMOVAL_KEYS_FILE"
-  fi
+  
+# Create an empty temp file if it doesn't exist
+if [ ! -f "$REMOVAL_KEYS_FILE" ]; then
+    touch "$REMOVAL_KEYS_FILE"
+fi
 
 # Read the style file with unused keys
 show_unused_style_keywords() {
@@ -34,35 +34,28 @@ show_unused_style_keywords() {
 
 # Function to remove a keyword from the temp file
 remove_keyword() {
-
   keyword="$1"
-  # echo "Removing $keyword"
+  
   grep -v "$keyword" "$STYLES_KEYS_FILE" > "$REMOVAL_KEYS_FILE"
   line_count=$(wc -l < $REMOVAL_KEYS_FILE)
-  # echo "$REMOVAL_KEYS_FILE lines in the file: $line_count"
   mv "$REMOVAL_KEYS_FILE" "$STYLES_KEYS_FILE"
-  # echo "$STYLES_KEYS_FILE UPDATED lines in the file: $line_count"
 }
 
 lookfor_unused_keywords() {
   # Loop through all files in the src folder
   find 'src' -type f -name "*.js" -o -name "*.jsx" -o -name "*.ts" -o -name "*.tsx" | while read -r file; do
-      # echo "Checking $file"
+
       # Search for keywords starting with "styles"
-      #  grep -o '\bstyles\.[a-zA-Z0-9_.]*' "$file" | while IFS= read -r keyword; do
       grep -E -o '\bstyles\.[a-zA-Z0-9_.]*' "$file" | grep -v '\/\/' | grep -vE '\/\*.*\*\/' | while IFS= read -r keyword; do
 
         # Remove any [ ] characters from the keyword
-        # echo "File: $file"
         clean_keyword="${keyword//[\[\]]/}"
         # skip styles. keyword that might be used in comments
         if [[ "$clean_keyword" == "styles." ]]; then
           continue
         fi
-          # echo "Found $clean_keyword"
-          # Remove the keyword from the temp file
-          remove_keyword "$clean_keyword"
         
+        remove_keyword "$clean_keyword"
     done
   done
 }
@@ -87,6 +80,12 @@ find_styles_and_store_keys() {
     elif [[ "$line" =~ ^[[:space:]]*([a-zA-Z0-9_-]+\.)?[a-zA-Z0-9_-]+:[[:space:]]*\{ ]]; then
       local key=$(echo "$line" | sed -E "s/[:[:space:]]*\{.*//")
       # local line_number=$(echo "$line" | grep -n "$key:" | cut -d':' -f1)
+      echo "line $line"
+      # Handle keys defined in functions within objects
+       function_key_pattern="[a-zA-Z0-9_-]+:[[:space:]]*\\(.*\\)[[:space:]]*=>[[:space:]]*\\{"
+      if [[ "$line" =~ $function_key_pattern ]]; then
+        key="${BASH_REMATCH[0]%%:*}"
+      fi
       
       if [[ ${#parent_keys[@]} -gt 0 ]]; then
         parent_key_trimmed="${parent_keys[${#parent_keys[@]}-1]// /}"  # Trim spaces
@@ -113,58 +112,8 @@ find_styles_and_store_keys() {
   done < "$file"
 }
 
-find_translations_and_store_keys() {
-  local file="$1"
-  local parent_key=()
-  local current_key=""
-  local line_number=0  # Initialize the line number
-
-  while IFS= read -r line; do
-    ((line_number++))  # Increment the line number
-
-  # Skip lines that are not key-related
-    if [[ ! "$line" =~ ^[[:space:]]*([a-zA-Z0-9_-]+)[[:space:]]*:[[:space:]]*\{|^[[:space:]]*([a-zA-Z0-9_-]+)[[:space:]]*:[[:space:]]*(\'[^\']*\'|\{)|^[[:space:]]*\} ]]; then
-      continue
-    fi
-
-    
-    if [[ "$line" =~ ^[[:space:]]*([a-zA-Z0-9_-]+)[[:space:]]*:[[:space:]]*\{ ]]; then
-      local key="${BASH_REMATCH[1]}"
-      current_key="$key"
-
-      if [[ ${#parent_keys[@]} -gt 0 ]]; then
-        local parent_key="${parent_keys[*]}"
-        current_key="$parent_key.$key"
-      fi
-
-      parent_keys=("${parent_keys[@]}" "$current_key")
-    elif [[ "$line" =~ ^[[:space:]]*([a-zA-Z0-9_-]+)[[:space:]]*:[[:space:]]*(\'[^\']*\'|\{) ]]; then
-      local key="${BASH_REMATCH[1]}"
-      # local line_number=$(echo "$line" | grep -n "${BASH_REMATCH[1]}" | cut -d':' -f1)
-      
-      if [[ ${#parent_keys[@]} -gt 0 ]]; then
-        local lastItem="${#parent_keys[@]}-1"
-        local parent_key="${parent_keys[$lastItem]}"
-
-        echo "${parent_key}.${key}:${file}:${line_number}" >> "$TRANSLATION_KEYS_FILE"
-      else
-        echo "$key:${file}:${line_number}" >> "$TRANSLATION_KEYS_FILE"
-      fi
-    elif [[ "$line" =~ ^[[:space:]]*\} ]]; then
-      parent_keys=("${parent_keys[@]:0:${#parent_keys[@]}-1}")
-      current_key="${parent_keys[*]}"
-    fi
-  # done < <(grep -E "^[[:space:]]*([a-zA-Z0-9_-]+)[[:space:]]*:[[:space:]]*\{|^[[:space:]]*([a-zA-Z0-9_-]+)[[:space:]]*:[[:space:]]*(\'[^\']*\'|\{)|^[[:space:]]*\}" "$file")
-  done < "$file"
-}
-
 # Find and store keys from styles.js
 find_styles_and_store_keys "$STYLES_FILE"
-
-# Find and store keys from translation files
-# for translation_file in "${TRANSLATION_FILES[@]}"; do
-#   find_translations_and_store_keys "$translation_file"
-# done
 
 echo "Keys saved to $KEYS_FILE"
 echo "Now go through the list and remove the keys that are used."
@@ -172,12 +121,11 @@ echo "Now go through the list and remove the keys that are used."
 line_count=$(wc -l < $STYLES_KEYS_FILE)
 echo "Number of lines in the file: $line_count"
 
-lookfor_unused_keywords
+# lookfor_unused_keywords
 
 echo "Unused keys are into to $STYLES_KEYS_FILE"
 
 line_count2=$(wc -l < $STYLES_KEYS_FILE)
 echo "Number of lines in the file: $line_count2"
-# cat "$STYLES_KEYS_FILE"
 
-show_unused_style_keywords
+# show_unused_style_keywords
