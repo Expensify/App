78eaaab0dfe846d2bbef16ace7e417fc572f9f2a
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-09-01 10:40:54 +0200

    Add return types

--- src/libs/NumberFormatUtils.ts
+++ src/libs/NumberFormatUtils.ts
@@ -1,8 +1,8 @@
-function format(locale: string, number: number, options?: Intl.NumberFormatOptions) {
+function format(locale: string, number: number, options?: Intl.NumberFormatOptions): string {
     return new Intl.NumberFormat(locale, options).format(number);
 }
 
-function formatToParts(locale: string, number: number, options?: Intl.NumberFormatOptions) {
+function formatToParts(locale: string, number: number, options?: Intl.NumberFormatOptions): Intl.NumberFormatPart[] {
     return new Intl.NumberFormat(locale, options).formatToParts(number);
 }
 
