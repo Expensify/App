166c12dbc362f47c6abed4b5dc43cb0da5e53df3
Manan Jadhav <manan.jadhav@gmail.com>
2023-09-04 01:14:47 +0530

    fix: remove props

--- src/components/MoneyRequestSkeletonView.js
+++ src/components/MoneyRequestSkeletonView.js
@@ -2,24 +2,15 @@ import React from 'react';
 import {View} from 'react-native';
 import {Rect} from 'react-native-svg';
 import SkeletonViewContentLoader from 'react-content-loader/native';
-import PropTypes from 'prop-types';
 import styles from '../styles/styles';
 import variables from '../styles/variables';
 import themeColors from '../styles/themes/default';
 
-const propTypes = {
-    shouldAnimate: PropTypes.bool,
-};
-
-const defaultProps = {
-    shouldAnimate: true,
-};
-
-function MoneyRequestSkeletonView(props) {
+function MoneyRequestSkeletonView() {
     return (
         <View style={[styles.flex1, styles.overflowHidden]}>
             <SkeletonViewContentLoader
-                animate={props.shouldAnimate}
+                animate
                 height={variables.moneyRequestSkeletonHeight}
                 backgroundColor={themeColors.borderLighter}
                 foregroundColor={themeColors.border}
@@ -47,7 +38,5 @@ function MoneyRequestSkeletonView(props) {
     );
 }
 
-MoneyRequestSkeletonView.propTypes = propTypes;
-MoneyRequestSkeletonView.defaultProps = defaultProps;
 MoneyRequestSkeletonView.displayName = 'MoneyRequestSkeletonView';
 export default MoneyRequestSkeletonView;
