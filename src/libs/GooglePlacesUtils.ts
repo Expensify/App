6a13230f6a642186a6711ffa67924c7369a9a86d
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-09-04 11:59:56 +0200

    ts migration googleplacesutils

--- src/libs/GooglePlacesUtils.ts
+++ src/libs/GooglePlacesUtils.ts
@@ -1,5 +1,13 @@
 import _ from 'underscore';
 
+type AddressComponent = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    long_name: string;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    short_name: string;
+    types: string[];
+};
+
 /**
  * Finds an address component by type, and returns the value associated to key. Each address component object
  * inside the addressComponents array has the following structure:
@@ -8,16 +16,12 @@ import _ from 'underscore';
  *   short_name: "New York",
  *   types: [ "locality", "political" ]
  * }]
- *
- * @param {Array} addressComponents
- * @param {Object} fieldsToExtract – has shape: {addressType: 'keyToUse'}
- * @returns {Object}
  */
-function getAddressComponents(addressComponents, fieldsToExtract) {
+function getAddressComponents(addressComponents: AddressComponent[], fieldsToExtract: Record<string, keyof AddressComponent>) {
     const result = _.mapObject(fieldsToExtract, () => '');
-    _.each(addressComponents, (addressComponent) => {
-        _.each(addressComponent.types, (addressType) => {
-            if (!_.has(fieldsToExtract, addressType) || !_.isEmpty(result[addressType])) {
+    addressComponents.forEach((addressComponent) => {
+        addressComponent.types.forEach((addressType) => {
+            if (!(addressType in fieldsToExtract) || addressType in result) {
                 return;
             }
             const value = addressComponent[fieldsToExtract[addressType]] ? addressComponent[fieldsToExtract[addressType]] : '';
