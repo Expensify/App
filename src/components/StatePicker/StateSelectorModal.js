0088b39d496cddb72ad9b1e8a8d33f89e2c4f877
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-09-04 08:52:36 +0100

    feat: switch us states const to an array of ISOs

--- src/components/StatePicker/StateSelectorModal.js
+++ src/components/StatePicker/StateSelectorModal.js
@@ -46,7 +46,7 @@ function StateSelectorModal({currentState, isVisible, onClose, onStateSelected, 
 
     const countryStates = useMemo(
         () =>
-            _.map(_.keys(CONST.ALL_US_STATES), (state) => {
+            _.map(CONST.ALL_US_ISO_STATES, (state) => {
                 const stateName = translate(`allStates.${state}.stateName`);
                 const stateISO = translate(`allStates.${state}.stateISO`);
                 return {
