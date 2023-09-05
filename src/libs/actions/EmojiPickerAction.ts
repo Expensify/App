d3b1cf5962e638007755ec2f60fc0715345a5602
Blazej Kustra <blazej.kustra@swmansion.com>
2023-09-05 18:02:23 +0200

    [TS migration] Migrate 'EmojiPickerAction.js' lib to TypeScript

---
+++ src/libs/actions/EmojiPickerAction.ts
@@ -0,0 +1,77 @@
+import {ValueOf} from 'type-fest';
+import React from 'react';
+import {View} from 'react-native';
+import CONST from '../../CONST';
+
+type AnchorOrigin = {
+    horizontal: ValueOf<typeof CONST.MODAL.ANCHOR_ORIGIN_HORIZONTAL>;
+    vertical: ValueOf<typeof CONST.MODAL.ANCHOR_ORIGIN_VERTICAL>;
+};
+
+type EmojiPicker = {
+    showEmojiPicker: (onModalHideValue?: () => void, onEmojiSelectedValue?: () => void, emojiPopoverAnchor?: View, anchorOrigin?: AnchorOrigin, onWillShow?: () => void, id?: string) => void;
+    isActive: (id: string) => boolean;
+    hideEmojiPicker: (isNavigating: boolean) => void;
+    isEmojiPickerVisible: boolean;
+    resetEmojiPopoverAnchor: () => void;
+};
+
+const emojiPickerRef = React.createRef<EmojiPicker>();
+
+/**
+ * Show the EmojiPicker modal popover.
+ *
+ * @param onModalHide - Run a callback when Modal hides.
+ * @param onEmojiSelected - Run a callback when Emoji selected.
+ * @param emojiPopoverAnchor - Element on which EmojiPicker is anchored
+ * @param anchorOrigin - Anchor origin for Popover
+ * @param onWillShow - Run a callback when Popover will show
+ * @param id - Unique id for EmojiPicker
+ */
+function showEmojiPicker(onModalHide = () => {}, onEmojiSelected = () => {}, emojiPopoverAnchor = undefined, anchorOrigin = undefined, onWillShow = () => {}, id = undefined): void {
+    if (!emojiPickerRef.current) {
+        return;
+    }
+
+    emojiPickerRef.current.showEmojiPicker(onModalHide, onEmojiSelected, emojiPopoverAnchor, anchorOrigin, onWillShow, id);
+}
+
+/**
+ * Hide the Emoji Picker modal.
+ */
+function hideEmojiPicker(isNavigating: boolean): void {
+    if (!emojiPickerRef.current) {
+        return;
+    }
+
+    emojiPickerRef.current.hideEmojiPicker(isNavigating);
+}
+
+/**
+ * Whether Emoji Picker is active for the given id.
+ */
+function isActive(id: string): boolean {
+    if (!emojiPickerRef.current) {
+        return false;
+    }
+
+    return emojiPickerRef.current.isActive(id);
+}
+
+function isEmojiPickerVisible(): boolean {
+    if (!emojiPickerRef.current) {
+        return false;
+    }
+
+    return emojiPickerRef.current.isEmojiPickerVisible;
+}
+
+function resetEmojiPopoverAnchor(): void {
+    if (!emojiPickerRef.current) {
+        return;
+    }
+
+    emojiPickerRef.current.resetEmojiPopoverAnchor();
+}
+
+export {emojiPickerRef, showEmojiPicker, hideEmojiPicker, isActive, isEmojiPickerVisible, resetEmojiPopoverAnchor};
