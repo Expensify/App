8b12931e518b607af449f53d093442c75be56fe7
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-09-05 14:45:18 +0200

    add translations for CardDetails

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -802,6 +802,14 @@ export default {
             setDefaultFailure: 'No se ha podido configurar el método de pago.',
         },
         addBankAccountFailure: 'Ocurrió un error inesperado al intentar añadir la cuenta bancaria. Inténtalo de nuevo.',
+        cardDetails: {
+            cardNumber: "Número de tarjeta digital",
+            expiration: "Expiración",
+            cvv: "CVV",
+            address: "Dirección",
+            revealDetails: "Revelar detalles",
+            copyCardNumber: "Copiar número de la tarjeta"
+        }
     },
     cardPage: {
         expensifyCard: 'Tarjeta Expensify',
