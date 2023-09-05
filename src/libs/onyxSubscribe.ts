ee28c79dcb6e3fa94b3dc540b465e01d658e3bce
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-09-05 17:11:32 +0200

    modify OnyxKey import

--- src/libs/onyxSubscribe.ts
+++ src/libs/onyxSubscribe.ts
@@ -1,5 +1,5 @@
 import Onyx, {ConnectOptions} from 'react-native-onyx';
-import {OnyxKey} from 'react-native-onyx/lib/types';
+import {OnyxKey} from '../ONYXKEYS';
 
 /**
  * Connect to onyx data. Same params as Onyx.connect(), but returns a function to unsubscribe.
