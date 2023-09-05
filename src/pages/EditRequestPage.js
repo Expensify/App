945d500c073f639195880c3ea9cd7be8b51c2dd5
Carlos Martins <cmartins@expensify.com>
2023-09-05 16:10:27 -0600

    rm double onyx usage

--- src/pages/EditRequestPage.js
+++ src/pages/EditRequestPage.js
@@ -189,8 +189,6 @@ export default compose(
         report: {
             key: ({route}) => `${ONYXKEYS.COLLECTION.REPORT}${route.params.threadReportID}`,
         },
-    }),
-    withOnyx({
         parentReport: {
             key: ({report}) => `${ONYXKEYS.COLLECTION.REPORT}${report ? report.parentReportID : '0'}`,
         },
