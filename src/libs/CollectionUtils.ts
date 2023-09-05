5b3476448abd43c2250de49ee94a1eaf9ded9ebb
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-09-05 13:31:59 +0200

    ref: resolve issue after discusion

--- src/libs/CollectionUtils.ts
+++ src/libs/CollectionUtils.ts
@@ -1,9 +1,21 @@
-function lastItem<T>(object: Record<string, T> = {}): T {
-    const lastKey = Object.keys(object).pop() ?? 0;
+/* eslint-disable */
+import {OnyxCollectionKey} from '../ONYXKEYS';
+
+/**
+ * Return the highest item in a numbered collection
+ *
+ * e.g. {1: '1', 2: '2', 3: '3'} -> '3'
+ */
+function lastItem<T>(object: Record<number, T> = {}): T | undefined {
+    const lastKey = +(Object.keys(object).pop() ?? 0);
     return object[lastKey];
 }
 
-function extractCollectionItemID(key: string) {
+/**
+ * Used to grab the id for a particular collection item's key.
+ * e.g. reportActions_1 -> 1
+ */
+function extractCollectionItemID(key: `${OnyxCollectionKey}${string}`) {
     return key.split('_')[1];
 }
 
