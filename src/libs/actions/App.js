e4d3e4a0edcabe9e21482455dfde8c42f4337530
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-09-05 16:10:30 -1000

    Add more to the comment

--- src/libs/actions/App.js
+++ src/libs/actions/App.js
@@ -454,7 +454,7 @@ function beginDeepLinkRedirect(shouldAuthenticateWithCurrentAccount = true) {
     // eslint-disable-next-line rulesdir/no-api-side-effects-method
     API.makeRequestWithSideEffects('OpenOldDotLink', {shouldRetry: false}, {}).then((response) => {
         if (!response) {
-            Log.alert('Trying to redirect via deep link, but the response is empty', {response, shouldAuthenticateWithCurrentAccount, currentUserAccountID}, true);
+            Log.alert('Trying to redirect via deep link, but the response is empty. User likely not authenticated.', {response, shouldAuthenticateWithCurrentAccount, currentUserAccountID}, true);
             return;
         }
 
