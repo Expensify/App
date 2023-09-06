060d6c1193fefe54964ab508aeb65ecd8428987d
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-09-05 14:52:28 -1000

    Fix style

--- src/types/onyx/Card.ts
+++ src/types/onyx/Card.ts
@@ -1,16 +1,11 @@
 import {ValueOf} from 'type-fest';
 import CONST from '../../CONST';
 
-type State =
-3 /* OPEN */ |
-4 /* NOT_ACTIVATED */ |
-5 /* STATE_DEACTIVATED */ |
-6 /* CLOSED */ |
-7 /* STATE_SUSPENDED */;
+type State = 3 /* OPEN */ | 4 /* NOT_ACTIVATED */ | 5 /* STATE_DEACTIVATED */ | 6 /* CLOSED */ | 7 /* STATE_SUSPENDED */;
 
 type Card = {
     cardID: number;
-    state: State,
+    state: State;
     bank: string;
     availableSpend: number;
     domainName: string;
