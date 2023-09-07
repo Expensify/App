370edbdd4df7a9278b9fe6386c7b2bafbae4b34c
VH <hoangzinhvn@gmail.com>
2023-09-07 11:10:59 +0700

    Fix android background image is missing

--- src/pages/signin/SignInPageLayout/BackgroundImage/index.android.js
+++ src/pages/signin/SignInPageLayout/BackgroundImage/index.android.js
@@ -1,3 +1,24 @@
-import backgroundImage from '../../../../../assets/images/home-background--android.svg';
+import React from 'react';
+import PropTypes from 'prop-types';
+import AndroidBackgroundImage from '../../../../../assets/images/home-background--android.svg';
+import styles from '../../../../styles/styles';
 
-export default backgroundImage;
+const propTypes = {
+    pointerEvents: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+};
+
+function BackgroundImage(props) {
+    return (
+        <AndroidBackgroundImage
+            pointerEvents={props.pointerEvents}
+            width={props.width}
+            style={styles.signInBackground}
+        />
+    )
+}
+
+BackgroundImage.displayName = 'BackgroundImage';
+BackgroundImage.propTypes = propTypes;
+
+export default BackgroundImage;
