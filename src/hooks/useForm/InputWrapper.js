51b1ab3ec537d0586f22abbb4ba99e3187eb4db6
Kamil Owczarz <kamil.owczarz@swmansion.com>
2023-09-07 13:16:44 +0200

    Cleanup input wrapper

--- src/hooks/useForm/InputWrapper.js
+++ src/hooks/useForm/InputWrapper.js
@@ -1,11 +1,32 @@
 import React, {forwardRef, useContext} from 'react';
+import PropTypes from 'prop-types';
 import FormContext from './FormContext';
 
-const InputWrapper = forwardRef((props, ref) => {
-    const {RenderInput, inputID, ...rest} = props;
+const propTypes = {
+    RenderInput: PropTypes.node.isRequired,
+    inputID: PropTypes.string.isRequired,
+    forwardedRef: PropTypes.oneOfType([PropTypes.func, PropTypes.shape({current: PropTypes.instanceOf(React.Component)})]),
+};
+
+const defaultProps = {
+    forwardedRef: undefined,
+};
+
+function InputWrapper(props) {
+    const {RenderInput, inputID, forwardedRef, ...rest} = props;
     const {registerInput} = useContext(FormContext);
     // eslint-disable-next-line react/jsx-props-no-spreading
-    return <RenderInput {...registerInput(inputID, {ref, ...rest})} />;
-});
+    return <RenderInput {...registerInput(inputID, {ref: forwardedRef, ...rest})} />;
+}
 
-export default InputWrapper;
+InputWrapper.propTypes = propTypes;
+InputWrapper.defaultProps = defaultProps;
+InputWrapper.displayName = 'InputWrapper';
+
+export default forwardRef((props, ref) => (
+    <InputWrapper
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        forwardedRef={ref}
+    />
+));
