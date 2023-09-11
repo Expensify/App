0236648bb72d02b99bf13372a4bf62d89fac696e
Daniel Silva <dsilva@expensify.com>
2023-09-12 01:30:05 +0800

    adding comment

--- src/libs/actions/OnyxUpdateManager.js
+++ src/libs/actions/OnyxUpdateManager.js
@@ -34,7 +34,9 @@ export default () => {
             if (!val) {
                 return;
             }
-
+            
+            // Since we used the same key that used to store lastUpdateID, once we start watching this variable we need 
+            // to confirm that the value is what we expect and not a number.
             if (_.isNumber(val)) {
                 Onyx.set(ONYXKEYS.ONYX_UPDATES_FROM_SERVER, null);
                 return;
