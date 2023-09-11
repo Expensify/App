9780eb9162cca57fa602380139cf7e1146e833ab
DylanDylann <dylandylan9112@gmail.com>
2023-09-11 14:31:09 +0700

    fix remove unused variables

--- src/components/ReportActionItem/TaskAction.js
+++ src/components/ReportActionItem/TaskAction.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
 import PropTypes from 'prop-types';
-import {withOnyx} from 'react-native-onyx';
 import withLocalize, {withLocalizePropTypes} from '../withLocalize';
-import compose from '../../libs/compose';
-import ONYXKEYS from '../../ONYXKEYS';
 import Text from '../Text';
 import styles from '../../styles/styles';
 import * as ReportActionUtils from '../../libs/ReportActionsUtils';
@@ -17,24 +14,9 @@ const propTypes = {
     // eslint-disable-next-line react/no-unused-prop-types -- This is used in the withOnyx HOC
     taskReportID: PropTypes.string.isRequired,
 
-    /* Onyx Props */
-    taskReport: PropTypes.shape({
-        /** Title of the task */
-        reportName: PropTypes.string,
-
-        /** AccountID of the manager in this iou report */
-        managerID: PropTypes.number,
-
-        /** AccountID of the creator of this iou report */
-        ownerAccountID: PropTypes.number,
-    }),
-
     ...withLocalizePropTypes,
 };
 
-const defaultProps = {
-    taskReport: {},
-};
 function TaskAction(props) {
     return (
         <>
@@ -46,14 +28,6 @@ function TaskAction(props) {
 }
 
 TaskAction.propTypes = propTypes;
-TaskAction.defaultProps = defaultProps;
 TaskAction.displayName = 'TaskAction';
 
-export default compose(
-    withLocalize,
-    withOnyx({
-        taskReport: {
-            key: ({taskReportID}) => `${ONYXKEYS.COLLECTION.REPORT}${taskReportID}`,
-        },
-    }),
-)(TaskAction);
+export default withLocalize(TaskAction);
