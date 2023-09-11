606e939a165c03cbbe38ed82d8505fffc1a0e458
c3024 <chaitanya3024@gmail.com>
2023-09-11 19:28:47 +0530

    rename to shouldDisableResendValidateCode

--- src/pages/settings/Profile/Contacts/ValidateCodeForm/BaseValidateCodeForm.js
+++ src/pages/settings/Profile/Contacts/ValidateCodeForm/BaseValidateCodeForm.js
@@ -76,7 +76,7 @@ function BaseValidateCodeForm(props) {
     const loginData = props.loginList[props.contactMethod];
     const inputValidateCodeRef = useRef();
     const validateLoginError = ErrorUtils.getEarliestErrorField(loginData, 'validateLogin');
-    const shouldDisableDidntReceiveMagicCode = props.network.isOffline || props.account.isLoading;
+    const shouldDisableResendValidateCode = props.network.isOffline || props.account.isLoading;
 
     useImperativeHandle(props.innerRef, () => ({
         focus() {
@@ -171,7 +171,7 @@ function BaseValidateCodeForm(props) {
             >
                 <View style={[styles.mt2, styles.dFlex, styles.flexColumn, styles.alignItemsStart]}>
                     <PressableWithFeedback
-                        disabled={shouldDisableDidntReceiveMagicCode}
+                        disabled={shouldDisableResendValidateCode}
                         style={[styles.mr1]}
                         onPress={resendValidateCode}
                         underlayColor={themeColors.componentBG}
@@ -180,7 +180,7 @@ function BaseValidateCodeForm(props) {
                         accessibilityRole={CONST.ACCESSIBILITY_ROLE.BUTTON}
                         accessibilityLabel={props.translate('validateCodeForm.magicCodeNotReceived')}
                     >
-                        <Text style={[StyleUtils.getDisabledLinkStyles(shouldDisableDidntReceiveMagicCode)]}>{props.translate('validateCodeForm.magicCodeNotReceived')}</Text>
+                        <Text style={[StyleUtils.getDisabledLinkStyles(shouldDisableResendValidateCode)]}>{props.translate('validateCodeForm.magicCodeNotReceived')}</Text>
                     </PressableWithFeedback>
                     {props.hasMagicCodeBeenSent && (
                         <DotIndicatorMessage
