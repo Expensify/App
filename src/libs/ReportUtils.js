df153d18adb94c66a9758e9072498c34d7719d75
Huzaifa Rasheed <huzaifarasheed900@gmail.com>
2023-09-11 13:07:41 +0500

    Fix lint

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -1460,6 +1460,7 @@ function getTransactionReportName(reportAction) {
  * @param {Object} report
  * @param {Object} [reportAction={}] This can be either a report preview action or the IOU action
  * @param {Boolean} [shouldConsiderReceiptBeingScanned=false]
+ * @param {Boolean} [shouldShowEmailForSettledIOU=true]
  * @returns  {String}
  */
 function getReportPreviewMessage(report, reportAction = {}, shouldConsiderReceiptBeingScanned = false) {
