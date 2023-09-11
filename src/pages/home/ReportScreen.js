3de64e19414ae2fc7c07474adda4e3bf35eaf1cc
Jack Nam <jack@expensify.com>
2023-09-11 16:56:25 +0800

    component got converted

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -314,7 +314,7 @@ function ReportScreen({
                         needsOffscreenAlphaCompositing
                     >
                         {headerView}
-                        {ReportUtils.isTaskReport(report) && isSmallScreenWidth && ReportUtils.isOpenTaskReport(this.props.report, parentReportAction) && (
+                        {ReportUtils.isTaskReport(report) && isSmallScreenWidth && ReportUtils.isOpenTaskReport(report, parentReportAction) && (
                             <View style={[styles.borderBottom]}>
                                 <View style={[styles.appBG, styles.pl0]}>
                                     <View style={[styles.ph5, styles.pb3]}>
