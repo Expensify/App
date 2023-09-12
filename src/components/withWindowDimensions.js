e15c420419bf6de00a28e6bb419a30092eac531f
Tam Dao <daotam4536@gmail.com>
2023-09-12 09:05:50 +0900

    remove unused func

--- src/components/withWindowDimensions.js
+++ src/components/withWindowDimensions.js
@@ -1,4 +1,4 @@
-import React, {forwardRef, createContext, useState, useEffect, useCallback} from 'react';
+import React, {forwardRef, createContext, useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import lodashDebounce from 'lodash/debounce';
 import {Dimensions} from 'react-native';
