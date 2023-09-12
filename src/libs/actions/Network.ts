d68c576a2ca626e958c94fbc086095c53f5ab650
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-09-12 11:19:14 +0200

    Remove void returns

--- src/libs/actions/Network.ts
+++ src/libs/actions/Network.ts
@@ -1,18 +1,18 @@
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 
-function setIsOffline(isOffline: boolean): void {
+function setIsOffline(isOffline: boolean) {
     Onyx.merge(ONYXKEYS.NETWORK, {isOffline});
 }
 
-function setShouldForceOffline(shouldForceOffline: boolean): void {
+function setShouldForceOffline(shouldForceOffline: boolean) {
     Onyx.merge(ONYXKEYS.NETWORK, {shouldForceOffline});
 }
 
 /**
  * Test tool that will fail all network requests when enabled
  */
-function setShouldFailAllRequests(shouldFailAllRequests: boolean): void {
+function setShouldFailAllRequests(shouldFailAllRequests: boolean) {
     Onyx.merge(ONYXKEYS.NETWORK, {shouldFailAllRequests});
 }
 
