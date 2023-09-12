db6f0660308f06a1d0a46358a21e086babedfc07
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-09-12 15:32:05 +0200

    Convert type string[] to OnyxTypes.Beta[]

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -1,18 +1,19 @@
 import CONST from '../CONST';
+import * as OnyxTypes from '../types/onyx';
 
-function canUseAllBetas(betas: string[]): boolean {
+function canUseAllBetas(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.ALL);
 }
 
-function canUseChronos(betas: string[]): boolean {
+function canUseChronos(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.CHRONOS_IN_CASH) || canUseAllBetas(betas);
 }
 
-function canUsePayWithExpensify(betas: string[]): boolean {
+function canUsePayWithExpensify(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.PAY_WITH_EXPENSIFY) || canUseAllBetas(betas);
 }
 
-function canUseDefaultRooms(betas: string[]): boolean {
+function canUseDefaultRooms(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.DEFAULT_ROOMS) || canUseAllBetas(betas);
 }
 
@@ -23,11 +24,11 @@ function canUseIOUSend(): boolean {
     return false;
 }
 
-function canUseWallet(betas: string[]): boolean {
+function canUseWallet(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.BETA_EXPENSIFY_WALLET) || canUseAllBetas(betas);
 }
 
-function canUseCommentLinking(betas: string[]): boolean {
+function canUseCommentLinking(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.BETA_COMMENT_LINKING) || canUseAllBetas(betas);
 }
 
@@ -36,15 +37,15 @@ function canUseCommentLinking(betas: string[]): boolean {
  * since contributors have been reporting a number of false issues related to the feature being under development.
  * See https://expensify.slack.com/archives/C01GTK53T8Q/p1641921996319400?thread_ts=1641598356.166900&cid=C01GTK53T8Q
  */
-function canUsePolicyRooms(betas: string[]): boolean {
+function canUsePolicyRooms(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.POLICY_ROOMS) || canUseAllBetas(betas);
 }
 
-function canUseTasks(betas: string[]): boolean {
+function canUseTasks(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.TASKS) || canUseAllBetas(betas);
 }
 
-function canUseCustomStatus(betas: string[]): boolean {
+function canUseCustomStatus(betas: OnyxTypes.Beta[]): boolean {
     return betas.includes(CONST.BETAS.CUSTOM_STATUS) || canUseAllBetas(betas);
 }
 
