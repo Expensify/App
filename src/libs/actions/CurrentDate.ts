7064b80426de0906b45585f7d3a7ab4a25a87fab
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-09-12 11:14:54 +0200

    Remove void return

--- src/libs/actions/CurrentDate.ts
+++ src/libs/actions/CurrentDate.ts
@@ -1,7 +1,7 @@
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 
-function setCurrentDate(currentDate: string): void {
+function setCurrentDate(currentDate: string) {
     Onyx.set(ONYXKEYS.CURRENT_DATE, currentDate);
 }
 
