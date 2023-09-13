08ec6c24f78d0f158918e85134f562dce1e3f848
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-09-13 12:27:00 +0200

    add missing components to ReportCardLostPage

--- src/pages/settings/Wallet/ReportCardLostPage.js
+++ src/pages/settings/Wallet/ReportCardLostPage.js
@@ -1,26 +1,151 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {withOnyx} from 'react-native-onyx';
+import PropTypes from 'prop-types';
 import ScreenWrapper from '../../../components/ScreenWrapper';
 import Navigation from '../../../libs/Navigation/Navigation';
 import ROUTES from '../../../ROUTES';
 import HeaderWithBackButton from '../../../components/HeaderWithBackButton';
 import styles from '../../../styles/styles';
+import Form from '../../../components/Form';
+import ONYXKEYS from '../../../ONYXKEYS';
+import SingleOptionSelector from '../../../components/SingleOptionSelector';
+import useLocalize from '../../../hooks/useLocalize';
+import Text from '../../../components/Text';
+import MenuItemWithTopDescription from '../../../components/MenuItemWithTopDescription';
 
-function ReportCardLostPage() {
+const OPTIONS = [
+    {
+        key: 'damaged',
+        label: 'reportCardLostOrDamaged.cardDamaged',
+    },
+    {
+        key: 'lost_or_stolen',
+        label: 'reportCardLostOrDamaged.cardLostOrStolen',
+    },
+];
+
+const propTypes = {
+    /** User's private personal details */
+    privatePersonalDetails: PropTypes.shape({
+        /** User's home address */
+        address: PropTypes.shape({
+            street: PropTypes.string,
+            city: PropTypes.string,
+            state: PropTypes.string,
+            zip: PropTypes.string,
+            country: PropTypes.string,
+        }),
+    }),
+};
+
+const defaultProps = {
+    privatePersonalDetails: {
+        address: {
+            street: '',
+            street2: '',
+            city: '',
+            state: '',
+            zip: '',
+            country: '',
+        },
+    },
+};
+
+function ReportCardLostPage({privatePersonalDetails}) {
+    const privateDetails = privatePersonalDetails || {};
+    const address = privateDetails.address || {};
+
+    const {translate} = useLocalize();
+
+    const [reason, setReason] = useState(OPTIONS[0]);
+    const [isReasonConfirmed, setIsReasonConfirmed] = useState(false);
+
+    const validate = (values) => {
+        if (!isReasonConfirmed) {
+            return {};
+        }
+
+        return {};
+    };
+
+    const onSubmit = () => {
+        console.log('SUBMIT');
+        setIsReasonConfirmed(true);
+    };
+
+    const handleOptionSelect = (option) => {
+        setReason(option);
+    };
+
+    /**
+     * Applies common formatting to each piece of an address
+     *
+     * @param {String} piece
+     * @returns {String}
+     */
+    const formatPiece = (piece) => (piece ? `${piece}, ` : '');
+
+    /**
+     * Formats an address object into an easily readable string
+     *
+     * @returns {String}
+     */
+    const getFormattedAddress = () => {
+        const [street1, street2] = (address.street || '').split('\n');
+        const formattedAddress =
+            formatPiece(street1) + formatPiece(street2) + formatPiece(address.city) + formatPiece(address.state) + formatPiece(address.zip) + formatPiece(address.country);
+
+        // Remove the last comma of the address
+        return formattedAddress.trim().replace(/,$/, '');
+    };
+
     return (
         <ScreenWrapper>
             <HeaderWithBackButton
-                title="Report card lost or damaged"
+                title={translate('reportCardLostOrDamaged.screenTitle')}
                 onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_WALLET)}
             />
-            <Text
-                style={[styles.textHeadline, styles.pre]}
-                numberOfLines={1}
+            <Form
+                formID={ONYXKEYS.FORMS.REPORT_PHYSICAL_CARD_LOST}
+                validate={validate}
+                onSubmit={onSubmit}
+                submitButtonText={translate('reportCardLostOrDamaged.nextButtonLabel')}
+                style={[styles.mh5, styles.flexGrow1]}
             >
-                Why do you need a new card?
-            </Text>
+                {isReasonConfirmed ? (
+                    <>
+                        <Text style={[styles.textHeadline, styles.mb3]}>{translate('reportCardLostOrDamaged.confirmAddressTitle')}</Text>
+                        <MenuItemWithTopDescription
+                            title={getFormattedAddress()}
+                            description={translate('reportCardLostOrDamaged.address')}
+                            shouldShowRightIcon
+                            onPress={() => Navigation.navigate(ROUTES.SETTINGS_PERSONAL_DETAILS_ADDRESS)}
+                            style={[styles.popoverMenuItem, {paddingHorizontal: 0}]}
+                            numberOfLinesTitle={2}
+                        />
+                        <Text style={styles.mt3}>{translate('reportCardLostOrDamaged.currentCardInfo')}</Text>
+                    </>
+                ) : (
+                    <>
+                        <Text style={[styles.textHeadline, styles.pre]}>{translate('reportCardLostOrDamaged.reasonTitle')}</Text>
+                        <SingleOptionSelector
+                            options={OPTIONS}
+                            selectedOptionKey={reason.key}
+                            onSelectOption={handleOptionSelect}
+                        />
+                    </>
+                )}
+            </Form>
         </ScreenWrapper>
     );
 }
 
-export default ReportCardLostPage;
+ReportCardLostPage.propTypes = propTypes;
+ReportCardLostPage.defaultProps = defaultProps;
+ReportCardLostPage.displayName = 'ReportCardLostPage';
+
+export default withOnyx({
+    privatePersonalDetails: {
+        key: ONYXKEYS.PRIVATE_PERSONAL_DETAILS,
+    },
+})(ReportCardLostPage);
