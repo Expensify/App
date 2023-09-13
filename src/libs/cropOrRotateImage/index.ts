060876a144c10bdf31d4bad9dfd628a614bfe352
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-09-13 15:27:35 +0200

    ref: move cropOrRotateImage index to TS

--- src/libs/cropOrRotateImage/index.ts
+++ src/libs/cropOrRotateImage/index.ts
@@ -1,14 +1,30 @@
-import _ from 'underscore';
+type SizeFromAngle = {
+    width: number;
+    height: number;
+};
+type CropOrRotateImageOptions = {
+    type: string;
+    name: string;
+    compress: number;
+};
+type CropOptions = {
+    originX: number;
+    originY: number;
+    width: number;
+    height: number;
+};
+type Action = {
+    crop?: CropOptions;
+    rotate?: number;
+};
+type FileWithUri = File & {
+    uri: string;
+};
 
 /**
  * Calculates a size of canvas after rotation
- *
- * @param {Number} width
- * @param {Number} height
- * @param {Number} angle
- * @returns {Object} Returns width and height of new canvas
  */
-function sizeFromAngle(width, height, angle) {
+function sizeFromAngle(width: number, height: number, angle: number): SizeFromAngle {
     const radians = (angle * Math.PI) / 180;
     let sine = Math.cos(radians);
     let cosine = Math.sin(radians);
@@ -23,12 +39,8 @@ function sizeFromAngle(width, height, angle) {
 
 /**
  * Creates a new rotated canvas
- *
- * @param {Object} canvas
- * @param {Number} degrees
- * @returns {Object}
  */
-function rotateCanvas(canvas, degrees) {
+function rotateCanvas(canvas: HTMLCanvasElement, degrees: number): HTMLCanvasElement {
     const {width, height} = sizeFromAngle(canvas.width, canvas.height, degrees);
 
     // We have to create a new canvas because it is not possible to change already drawn
@@ -40,26 +52,22 @@ function rotateCanvas(canvas, degrees) {
     const context = result.getContext('2d');
 
     // In order to rotate image along its center we have to apply next transformation
-    context.translate(result.width / 2, result.height / 2);
+    context?.translate(result.width / 2, result.height / 2);
 
     const radians = (degrees * Math.PI) / 180;
-    context.rotate(radians);
+    context?.rotate(radians);
 
-    context.drawImage(canvas, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    context?.drawImage(canvas, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
 
     return result;
 }
 
 /**
  * Creates new cropped canvas and returns it
- *
- * @param {Object} canvas
- * @param {Object} options
- * @returns {Object}
  */
-function cropCanvas(canvas, options) {
+function cropCanvas(canvas: HTMLCanvasElement, options: CropOptions) {
     let {originX = 0, originY = 0, width = 0, height = 0} = options;
-    const clamp = (value, max) => Math.max(0, Math.min(max, value));
+    const clamp = (value: number, max: number) => Math.max(0, Math.min(max, value));
 
     width = clamp(width, canvas.width);
     height = clamp(height, canvas.height);
@@ -74,33 +82,28 @@ function cropCanvas(canvas, options) {
     result.height = height;
 
     const context = result.getContext('2d');
-    context.drawImage(canvas, originX, originY, width, height, 0, 0, width, height);
+    context?.drawImage(canvas, originX, originY, width, height, 0, 0, width, height);
 
     return result;
 }
 
-/**
- * @param {Object} canvas
- * @param {Object} options
- * @returns {Promise<File>}
- */
-function convertCanvasToFile(canvas, options = {}) {
+function convertCanvasToFile(canvas: HTMLCanvasElement, options: CropOrRotateImageOptions): Promise<FileWithUri> {
     return new Promise((resolve) => {
         canvas.toBlob((blob) => {
+            if (!blob) {
+                return;
+            }
             const file = new File([blob], options.name || 'fileName.jpeg', {type: options.type || 'image/jpeg'});
-            file.uri = URL.createObjectURL(file);
-            resolve(file);
+            const fileWithUri = Object.assign(file, {uri: URL.createObjectURL(file)});
+            resolve(fileWithUri);
         });
     });
 }
 
 /**
  * Loads image from specified url
- *
- * @param {String} uri
- * @returns {Promise<Object>}
  */
-function loadImageAsync(uri) {
+function loadImageAsync(uri: string): Promise<HTMLCanvasElement> {
     return new Promise((resolve, reject) => {
         const imageSource = new Image();
         imageSource.crossOrigin = 'anonymous';
@@ -110,7 +113,7 @@ function loadImageAsync(uri) {
             canvas.height = imageSource.naturalHeight;
 
             const context = canvas.getContext('2d');
-            context.drawImage(imageSource, 0, 0, imageSource.naturalWidth, imageSource.naturalHeight);
+            context?.drawImage(imageSource, 0, 0, imageSource.naturalWidth, imageSource.naturalHeight);
 
             resolve(canvas);
         };
@@ -121,27 +124,19 @@ function loadImageAsync(uri) {
 
 /**
  * Crops and rotates the image on web
- *
- * @param {String} uri
- * @param {Object} actions
- * @param {Object} options
- * @returns {Promise<Object>} Returns cropped and rotated image
  */
-function cropOrRotateImage(uri, actions, options) {
+
+function cropOrRotateImage(uri: string, actions: Action[], options: CropOrRotateImageOptions): Promise<FileWithUri> {
     return loadImageAsync(uri).then((originalCanvas) => {
-        const resultCanvas = _.reduce(
-            actions,
-            (canvas, action) => {
-                if ('crop' in action) {
-                    return cropCanvas(canvas, action.crop);
-                }
-                if ('rotate' in action) {
-                    return rotateCanvas(canvas, action.rotate);
-                }
-                return canvas;
-            },
-            originalCanvas,
-        );
+        const resultCanvas = actions.reduce((canvas, action) => {
+            if (action.crop) {
+                return cropCanvas(canvas, action.crop);
+            }
+            if (action.rotate) {
+                return rotateCanvas(canvas, action.rotate);
+            }
+            return canvas;
+        }, originalCanvas);
 
         return convertCanvasToFile(resultCanvas, options);
     });
