a83a05ab61e59e553ccf742aceb085a904f84ca6
Georgia Monahan <georgia@expensify.com>
2023-09-13 21:09:15 +0800

    Fix Lint, use themeColors

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1548,7 +1548,7 @@ const styles = {
         top: 0,
         bottom: 0,
         right: 0,
-        backgroundColor: Colors.black,
+        backgroundColor: themeColors.shadow,
         opacity: current.progress.interpolate({
             inputRange: [0, 1],
             outputRange: [0, variables.overlayOpacity],
