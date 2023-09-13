6593db0c090916d68af5bfcf837eabf7fd7f0a6d
Blazej Kustra <blazej.kustra@swmansion.com>
2023-09-13 15:55:28 +0200

    [TS migration] Migrate 'Download.js' lib to TypeScript

--- src/libs/actions/Download.ts
+++ src/libs/actions/Download.ts
@@ -1,27 +1,25 @@
-import _ from 'underscore';
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '../../ONYXKEYS';
 
 /**
  * Set whether an attachment is being downloaded so that a spinner can be shown.
- *
- * @param {String} sourceID
- * @param {Boolean} isDownloading
- * @returns {Promise}
  */
-function setDownload(sourceID, isDownloading) {
+function setDownload(sourceID: string, isDownloading: boolean): Promise<void> {
     return Onyx.merge(`${ONYXKEYS.COLLECTION.DOWNLOAD}${sourceID}`, {isDownloading});
 }
 
-function clearDownloads() {
+function clearDownloads(): void {
     const connectionID = Onyx.connect({
         key: ONYXKEYS.COLLECTION.DOWNLOAD,
         waitForCollectionCallback: true,
         callback: (records) => {
             Onyx.disconnect(connectionID);
-            const downloadsToDelete = {};
-            _.each(_.keys(records), (recordKey) => (downloadsToDelete[recordKey] = null));
-            if (!_.isEmpty(downloadsToDelete)) {
+            const downloadsToDelete: Record<string, null> = {};
+            Object.keys(records ?? {}).forEach((recordKey) => {
+                downloadsToDelete[recordKey] = null;
+            });
+
+            if (Object.keys(downloadsToDelete).length > 0) {
                 Onyx.multiSet(downloadsToDelete);
             }
         },
