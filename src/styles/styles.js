ddfab35d7c5ef0862696ac5f49328993d8581f43
Christoph Pader <christoph@margelo.io>
2023-09-13 14:54:56 +0200

    fix: add one more comment

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3964,7 +3964,7 @@ const styles = (theme) => ({
 // For now we need to export the styles function that takes the theme as an argument
 // as something named different than "styles", because a lot of files import the "defaultStyles"
 // as "styles", which causes ESLint to throw an error.
-// TODO: Remove this once the app is migrated to theme switching hooks and HOCs and "styles/theme/default.js" is not used anywhere anymore
+// TODO: Remove this once the app is migrated to theme switching hooks and HOCs and "styles/theme/default.js" is not used anywhere anymore (GH issue: https://github.com/Expensify/App/issues/27337)
 const stylesGenerator = styles;
 const defaultStyles = styles(defaultTheme);
 
