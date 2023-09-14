c8e42e75d49a917468d04dd5b634612cdf70ae76
Agata Kosior <agata.kosior57@gmail.com>
2023-09-14 08:36:35 +0200

    refactor: replace paticipants for participantAccountIDs in merchant page

--- src/pages/iou/MoneyRequestMerchantPage.js
+++ src/pages/iou/MoneyRequestMerchantPage.js
@@ -58,7 +58,7 @@ function MoneyRequestMerchantPage({iou, route}) {
             IOU.resetMoneyRequestInfo(moneyRequestId);
         }
 
-        if (_.isEmpty(iou.participants) || (iou.amount === 0 && !iou.receiptPath) || shouldReset) {
+        if (_.isEmpty(iou.participantAccountIDs) || (iou.amount === 0 && !iou.receiptPath) || shouldReset) {
             Navigation.goBack(ROUTES.getMoneyRequestRoute(iouType, reportID), true);
         }
     }, [iou.id, iou.participants, iou.amount, iou.receiptPath, iouType, reportID]);
