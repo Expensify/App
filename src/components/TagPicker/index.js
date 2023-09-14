4fdefef2c8b2d5e3014c5cd4e5265f63a5f80c95
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-09-14 10:42:10 +0100

    feat: build sections and show search input

--- src/components/TagPicker/index.js
+++ src/components/TagPicker/index.js
@@ -1,7 +1,8 @@
-import React, {useMemo} from 'react';
+import React, { useMemo, useState } from 'react';
 import _ from 'underscore';
 import lodashGet from 'lodash/get';
-import {withOnyx} from 'react-native-onyx';
+import { withOnyx } from 'react-native-onyx';
+import CONST from '../../CONST';
 import ONYXKEYS from '../../ONYXKEYS';
 import styles from '../../styles/styles';
 import Navigation from '../../libs/Navigation/Navigation';
@@ -9,10 +10,17 @@ import ROUTES from '../../ROUTES';
 import useLocalize from '../../hooks/useLocalize';
 import * as OptionsListUtils from '../../libs/OptionsListUtils';
 import OptionsSelector from '../OptionsSelector';
-import {propTypes, defaultProps} from './tagPickerPropTypes';
+import { propTypes, defaultProps } from './tagPickerPropTypes';
 
-function TagPicker({policyTags, reportID, tag, iouType, iou}) {
-    const {translate} = useLocalize();
+function TagPicker({ reportID, tag, iouType, policyTags, policyRecentlyUsedTags, iou }) {
+    const { translate } = useLocalize();
+    const [searchValue, setSearchValue] = useState('');
+
+    const policyTagList = lodashGet(policyTags, [tag, 'tags'], []);
+    const policyTagsCount = _.size(policyTagList);
+    const isTagsCountBelowThreshold = policyTagsCount < CONST.TAG_LIST_THRESHOLD;
+
+    const shouldShowTextInput = !isTagsCountBelowThreshold;
 
     const selectedOptions = useMemo(() => {
         if (!iou.tag) {
@@ -23,29 +31,31 @@ function TagPicker({policyTags, reportID, tag, iouType, iou}) {
             {
                 name: iou.tag,
                 enabled: true,
+                accountID: null,
             },
         ];
     }, [iou.tag]);
 
-    // Only shows one section, which will be the default behavior if there are
-    // less than 8 policy tags
-    // TODO: support sections with search
-    const sections = useMemo(() => {
-        const tagList = _.chain(lodashGet(policyTags, [tag, 'tags'], {}))
-            .values()
-            .map((t) => ({
-                text: t.name,
-                keyForList: t.name,
-                tooltipText: t.name,
-            }))
-            .value();
+    const initialFocusedIndex = useMemo(() => {
+        if (isTagsCountBelowThreshold && selectedOptions.length > 0) {
+            return _.chain(policyTagList)
+                .values()
+                .findIndex((tag) => tag.name === selectedOptions[0].name, true)
+                .value();
+        }
 
-        return [
-            {
-                data: tagList,
-            },
-        ];
-    }, [policyTags, tag]);
+        return 0;
+    }, [policyTagList, selectedOptions, isTagsCountBelowThreshold]);
+
+    const sections = useMemo(
+        () => OptionsListUtils.getNewChatOptions(
+            {},
+            {},
+            [], searchValue, selectedOptions, [], false, false, false, {}, [],
+            true, policyTagList, policyRecentlyUsedTags, false).tagOptions
+        ,
+        [searchValue, selectedOptions, policyTagList, policyRecentlyUsedTags]
+    );
 
     const headerMessage = OptionsListUtils.getHeaderMessage(lodashGet(sections, '[0].data.length', 0) > 0, false, '');
 
@@ -66,9 +76,13 @@ function TagPicker({policyTags, reportID, tag, iouType, iou}) {
             headerMessage={headerMessage}
             textInputLabel={translate('common.search')}
             boldStyle
-            value=""
+            highlightSelectedOptions
+            isRowMultilineSupported
+            shouldShowTextInput={shouldShowTextInput}
+            value={searchValue}
+            initialFocusedIndex={initialFocusedIndex}
+            onChangeText={setSearchValue}
             onSelectRow={updateTag}
-            shouldShowTextInput={false}
         />
     );
 }
@@ -79,10 +93,10 @@ TagPicker.defaultProps = defaultProps;
 
 export default withOnyx({
     policyTags: {
-        key: ({policyID}) => `${ONYXKEYS.COLLECTION.POLICY_TAGS}${policyID}`,
+        key: ({ policyID }) => `${ONYXKEYS.COLLECTION.POLICY_TAGS}${policyID}`,
     },
     policyRecentlyUsedTags: {
-        key: ({policyID}) => `${ONYXKEYS.COLLECTION.POLICY_RECENTLY_USED_TAGS}${policyID}`,
+        key: ({ policyID }) => `${ONYXKEYS.COLLECTION.POLICY_RECENTLY_USED_TAGS}${policyID}`,
     },
     iou: {
         key: ONYXKEYS.IOU,
