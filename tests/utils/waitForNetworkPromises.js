58f4bf5f180efe024718c14daa38d35a960841f2
Szymon Kapała <szymon20000@gmail.com>
2023-09-14 22:07:00 +0200

    cleanup

--- tests/utils/waitForNetworkPromises.js
+++ tests/utils/waitForNetworkPromises.js
@@ -2,7 +2,7 @@ import waitForBatchedUpdates from './waitForBatchedUpdates';
 
 /**
  * Method flushes microtasks and pending timers twice. Because we batch onyx updates
- * Some operations like for instance network requests takes 2 microtask cycles to resolve
+ * Network requests takes 2 microtask cycles to resolve
  * **Note:** It is recommended to wait for the Onyx operations, so in your tests its preferred to do:
  *  ✅  Onyx.merge(...).then(...)
  *  than to do
