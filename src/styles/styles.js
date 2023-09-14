5a60f1eef76f574f06906af84d478afab9a4ffa7
Aman Sheikh <aman.atg001@gmail.com>
2023-09-14 19:00:43 +0530

    avoid platform/comp specific spacing and use gap instead

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3676,27 +3676,26 @@ const styles = (theme) => ({
     },
 
     loginButtonRow: {
-        justifyContent: 'center',
         width: '100%',
+        gap: 12,
         ...flex.flexRow,
+        ...flex.justifyContentCenter,
     },
 
     loginButtonRowSmallScreen: {
-        justifyContent: 'center',
         width: '100%',
-        marginBottom: 10,
+        gap: 12,
         ...flex.flexRow,
+        ...flex.justifyContentCenter,
+        marginBottom: 10,
     },
 
-    appleButtonContainer: {
+    desktopSignInButtonContainer: {
         width: 40,
         height: 40,
-        marginRight: 20,
     },
 
     signInIconButton: {
-        margin: 10,
-        marginTop: 0,
         padding: 2,
     },
 
@@ -3704,7 +3703,6 @@ const styles = (theme) => ({
         colorScheme: 'light',
         width: 40,
         height: 40,
-        marginLeft: 12,
         alignItems: 'center',
         overflow: 'hidden',
     },
