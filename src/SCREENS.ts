e4a5f3696932aba962799e8faae2d52f195207e7
Sebastian Szewczyk <sebastian.szewczyk@swmansion.com>
2023-09-15 15:20:27 +0200

    Reverted testing changes

--- src/SCREENS.ts
+++ src/SCREENS.ts
@@ -19,5 +19,3 @@ export default {
     SIGN_IN_WITH_GOOGLE_DESKTOP: 'GoogleSignInDesktop',
     DESKTOP_SIGN_IN_REDIRECT: 'DesktopSignInRedirect',
 } as const;
-
-// TODO: remove this, testing GH actions
