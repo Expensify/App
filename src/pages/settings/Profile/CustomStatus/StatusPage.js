4c91e1f708d9b9986febe8e47394cf857a38d30b
Georgia Monahan <georgia@expensify.com>
2023-09-15 10:48:41 +0800

    use border color instead of old midtone (green700) for future theme support

--- src/pages/settings/Profile/CustomStatus/StatusPage.js
+++ src/pages/settings/Profile/CustomStatus/StatusPage.js
@@ -68,7 +68,7 @@ function StatusPage({draftStatus, currentUserPersonalDetails}) {
         <StaticHeaderPageLayout
             title={localize.translate('statusPage.status')}
             onBackButtonPress={navigateBackToSettingsPage}
-            backgroundColor={themeColors.midtone}
+            backgroundColor={themeColors.border}
             image={MobileBackgroundImage}
             footer={footerComponent}
         >
