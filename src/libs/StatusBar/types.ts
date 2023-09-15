94000917c762085f54e06f99f55df69b60914d4e
Blazej Kustra <blazej.kustra@swmansion.com>
2023-09-15 10:41:00 +0200

    Fix how StatuSBar type is augmented

--- src/libs/StatusBar/types.ts
+++ src/libs/StatusBar/types.ts
@@ -1,11 +1,10 @@
 // eslint-disable-next-line no-restricted-imports
 import {StatusBar as StatusBarRN} from 'react-native';
 
-class StatusBar extends StatusBarRN {
-    // Only has custom web implementation
-    static getBackgroundColor(): string | symbol | null {
-        return null;
-    }
-}
+type StatusBarExtended = typeof StatusBarRN & {
+    getBackgroundColor(): string | symbol | null;
+};
+
+const StatusBar = StatusBarRN as StatusBarExtended;
 
 export default StatusBar;
