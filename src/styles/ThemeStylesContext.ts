b7b1179f4bce3f0ca23ad471f14875d1f424a3a4
Christoph Pader <christoph@margelo.io>
2023-09-16 12:28:48 +0200

    fix: type remaining file with TODO

---
+++ src/styles/ThemeStylesContext.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './styles';
+
+// TODO: Change "uknown" once "styles.js" is typed
+const ThemeStylesContext = React.createContext<unknown>(styles);
+
+export default ThemeStylesContext;
