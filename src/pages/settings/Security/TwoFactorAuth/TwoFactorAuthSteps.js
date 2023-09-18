8f1c89252af275b33ca430cd205029e99f358ee9
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-09-18 09:15:18 +0200

    fix initial state in the TwoFactorAuthSteps

--- src/pages/settings/Security/TwoFactorAuth/TwoFactorAuthSteps.js
+++ src/pages/settings/Security/TwoFactorAuth/TwoFactorAuthSteps.js
@@ -13,22 +13,21 @@ import {defaultAccount, TwoFactorAuthPropTypes} from './TwoFactorAuthPropTypes';
 import useAnimatedStepContext from '../../../../components/AnimatedStep/useAnimatedStepContext';
 
 function TwoFactorAuthSteps({account = defaultAccount}) {
-    const [currentStep, setCurrentStep] = useState(CONST.TWO_FACTOR_AUTH_STEPS.CODES);
+    const calculateCurrentStep = () => {
+        if (account.twoFactorAuthStep) {
+            return account.twoFactorAuthStep;
+        }
+        return account.requiresTwoFactorAuth ? CONST.TWO_FACTOR_AUTH_STEPS.ENABLED : CONST.TWO_FACTOR_AUTH_STEPS.CODES;
+    };
+
+    const [currentStep, setCurrentStep] = useState(calculateCurrentStep);
 
     const {setAnimationDirection} = useAnimatedStepContext();
 
     useEffect(() => () => TwoFactorAuthActions.clearTwoFactorAuthData(), []);
 
     useEffect(() => {
-        if (account.twoFactorAuthStep) {
-            setCurrentStep(account.twoFactorAuthStep);
-            return;
-        }
-        if (account.requiresTwoFactorAuth) {
-            setCurrentStep(CONST.TWO_FACTOR_AUTH_STEPS.ENABLED);
-        } else {
-            setCurrentStep(CONST.TWO_FACTOR_AUTH_STEPS.CODES);
-        }
+        setCurrentStep(calculateCurrentStep);
         // we don't want to trigger the hook every time the step changes, only when the requiresTwoFactorAuth changes
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [account.requiresTwoFactorAuth]);
