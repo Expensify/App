b735c2addb176a155da0300f77327a0b38a79ce7
VickyStash <vikstash@gmail.com>
2023-09-18 14:13:15 +0200

    Update src/libs/getPlaidLinkTokenParameters/index.ts
    Co-authored-by: Carlos Alvarez <cead22@gmail.com>

--- src/libs/getPlaidLinkTokenParameters/index.ts
+++ src/libs/getPlaidLinkTokenParameters/index.ts
@@ -2,7 +2,7 @@ import ROUTES from '../../ROUTES';
 import CONFIG from '../../CONFIG';
 import GetPlaidLinkTokenParameters from './types';
 
-const getPlaidLinkTokenParameters: GetPlaidLinkTokenParameters = ()=> {
+const getPlaidLinkTokenParameters: GetPlaidLinkTokenParameters = () => {
     const bankAccountRoute = window.location.href.includes('personal') ? ROUTES.BANK_ACCOUNT_PERSONAL : ROUTES.BANK_ACCOUNT;
 
     // eslint-disable-next-line @typescript-eslint/naming-convention
