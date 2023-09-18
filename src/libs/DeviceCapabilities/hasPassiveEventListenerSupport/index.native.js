c6fd83e30562c2e5708679754935b5a448504c28
tienifr <christianwen18@gmail.com>
2023-09-18 12:17:53 +0700

    update comment

--- src/libs/DeviceCapabilities/hasPassiveEventListenerSupport/index.native.js
+++ src/libs/DeviceCapabilities/hasPassiveEventListenerSupport/index.native.js
@@ -1,6 +1,5 @@
 /**
  * Allows us to identify whether the browser supports passive event listener.
- * Because older browsers will interpret any object in the 3rd argument of an event listener as capture=true.
  *
  * @returns {Boolean}
  */
