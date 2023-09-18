15ee26f220ede610bd9d6ab5ed711f51b829fa5a
Ishpaul Singh <ishpaul207@gmail.com>
2023-09-18 14:02:02 +0530

    fixes the issue #27375

--- src/pages/home/sidebar/SidebarScreen/index.js
+++ src/pages/home/sidebar/SidebarScreen/index.js
@@ -24,17 +24,26 @@ function SidebarScreen(props) {
     );
 
     /**
+     * Method to hide popover when drag over.
+     */
+    const hidePopoverOnDragOver = useCallback(() => {
+        if (popoverModal.current !== null) {
+            popoverModal.current.hideCreateMenu();
+        }
+    }, []);
+
+    /**
      * Method create event listener
      */
     const createDragoverListener = () => {
-        document.addEventListener('dragover', () => popoverModal.current.hideCreateMenu());
+        document.addEventListener('dragover', hidePopoverOnDragOver);
     };
 
     /**
      * Method remove event listener.
      */
     const removeDragoverListener = () => {
-        document.removeEventListener('dragover', () => popoverModal.current.hideCreateMenu());
+        document.removeEventListener('dragover', hidePopoverOnDragOver);
     };
 
     return (
