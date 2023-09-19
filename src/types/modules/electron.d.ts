5a9f707198d51ddcb3928535467617ef7411acc5
Michał Skałka <michal.skalka@swmansion.com>
2023-09-19 12:55:29 +0200

    retype electron.d.ts

--- src/types/modules/electron.d.ts
+++ src/types/modules/electron.d.ts
@@ -1,7 +1,16 @@
+// TODO: Move this type to desktop/contextBridge.js once it is converted to TS
+type ContextBridgeApi = {
+    send: (channel: string, data?: unknown) => void;
+    sendSync: (channel: string, data?: unknown) => unknown;
+    invoke: (channel: string, ...args: unknown) => Promise<unknown>;
+    on: (channel: string, func: () => void) => void;
+    removeAllListeners: (channel: string) => void;
+};
+
 declare global {
     // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
     interface Window {
-        electron: Electron.IpcRenderer;
+        electron: ContextBridgeApi;
     }
 }
 
