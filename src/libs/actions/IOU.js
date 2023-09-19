32979b0df41f0164eac0be54682a39783a1af06f
Nam Le <yentth237@gmail.com>
2023-09-19 15:50:30 +0700

    add failure data when edit request money

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -1145,7 +1145,9 @@ function editMoneyRequest(transactionID, transactionThreadReportID, transactionC
             onyxMethod: Onyx.METHOD.MERGE,
             key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${transactionThread.reportID}`,
             value: {
-                [updatedReportAction.reportActionID]: updatedReportAction,
+                [updatedReportAction.reportActionID]: {
+                    errors: ErrorUtils.getMicroSecondOnyxError('iou.error.genericEditFailureMessage'),
+                },
             },
         },
         {
