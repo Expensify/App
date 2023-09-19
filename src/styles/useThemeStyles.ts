dba25a2a693737b6261cd6e73a46d07cdd9fc253
Christoph Pader <christoph@margelo.io>
2023-09-19 10:45:43 +0200

    fix: naming

--- src/styles/useThemeStyles.ts
+++ src/styles/useThemeStyles.ts
@@ -5,7 +5,7 @@ function useThemeStyles() {
     const themeStyles = useContext(ThemeStylesContext);
 
     if (!themeStyles) {
-        throw new Error('StylesContext was null! Are you sure that you wrapped the component under a <ThemeStylesProvider>?');
+        throw new Error('ThemeStylesContext was null! Are you sure that you wrapped the component under a <ThemeStylesProvider>?');
     }
 
     // TODO: Remove this "eslint-disable-next" once the theme switching migration is done and styles are fully typed (GH Issue: https://github.com/Expensify/App/issues/27337)
