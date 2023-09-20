ed0267c654674722b3cb363b5f8181b14cebc83e
Rushat Gabhane <rushatgabhane@gmail.com>
2023-09-21 05:01:24 +0530

    fix lint

--- .github/scripts/createDocsRoutes.js
+++ .github/scripts/createDocsRoutes.js
@@ -92,12 +92,12 @@ function run() {
 
     if (expensifyClassicArticleHubs.length !== expensifyClassicRoute.hubs.length) {
         console.error(warnMessage(platformNames.expensifyClassic));
-        exit(1);
+        process.exit(1);
     }
 
     if (newExpensifyArticleHubs.length !== newExpensifyRoute.hubs.length) {
         console.error(warnMessage(platformNames.newExpensify));
-        exit(1);
+        process.exit(1);
     }
 
     createHubsWithArticles(expensifyClassicArticleHubs, platformNames.expensifyClassic, expensifyClassicRoute.hubs);
@@ -113,5 +113,5 @@ try {
     run();
 } catch (error) {
     console.error('A problem occurred while trying to read the directories.', error);
-    exit(1);
+    process.exit(1);
 }
