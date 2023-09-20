bd716a534ba9a5a1eb3446b44c619899449ec1a8
Blazej Kustra <blazej.kustra@swmansion.com>
2023-09-20 15:52:29 +0200

    Fix prettier

--- src/libs/migrateOnyx.js
+++ src/libs/migrateOnyx.js
@@ -13,14 +13,7 @@ export default function () {
 
     return new Promise((resolve) => {
         // Add all migrations to an array so they are executed in order
-        const migrationPromises = [
-            RenameActiveClientsKey,
-            RenamePriorityModeKey,
-            AddEncryptedAuthToken,
-            RenameExpensifyNewsStatus,
-            AddLastVisibleActionCreated,
-            PersonalDetailsByAccountID,
-        ];
+        const migrationPromises = [RenameActiveClientsKey, RenamePriorityModeKey, AddEncryptedAuthToken, RenameExpensifyNewsStatus, AddLastVisibleActionCreated, PersonalDetailsByAccountID];
 
         // Reduce all promises down to a single promise. All promises run in a linear fashion, waiting for the
         // previous promise to finish before moving onto the next one.
