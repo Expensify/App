a187423e51e24f12ce9b0c0623796ef484236bea
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-09-20 16:37:36 +0800

    add jsdoc

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,6 +1,12 @@
 import {useEffect, useRef} from 'react';
 import _ from 'underscore';
 
+/**
+ * @param {*} func Function to debounce `waitMS` ms.
+ * @param {*} wait The number of milliseconds to wait before `func` can be invoked again.
+ * @param {*} immediate True if `func` should be invoked on the leading edge of `waitMS` instead of the trailing edge.
+ * @returns 
+ */
 export default function useDebounce(func, wait, immediate) {
     const debouncedFnRef = useRef();
 
