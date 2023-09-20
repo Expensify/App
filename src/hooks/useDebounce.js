76b9cc7cf578e263a82d27e860281e0a31c2c9b3
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-09-20 18:37:07 +0800

    prettier

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -3,9 +3,9 @@ import lodashDebounce from 'lodash/debounce';
 
 /**
  * Create and return a debounced function.
- * 
+ *
  * Make sure to pass a stable function reference to prevent recreating the debounced function on each render.
- * 
+ *
  * @param {Function} func The function to debounce.
  * @param {Number} wait The number of milliseconds to delay.
  * @param {Object} options The options object.
