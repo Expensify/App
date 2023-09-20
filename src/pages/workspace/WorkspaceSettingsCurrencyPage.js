f7ca60739a6a547a7b44533f768aec328faa9f7a
Thiago Brezinski <thiagobrez@gmail.com>
2023-09-20 15:46:56 +0100

    chore: cleanup

--- src/pages/workspace/WorkspaceSettingsCurrencyPage.js
+++ src/pages/workspace/WorkspaceSettingsCurrencyPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useMemo, useCallback} from 'react';
+import React, {useState, useCallback} from 'react';
 import _ from 'underscore';
 import {withOnyx} from 'react-native-onyx';
 import PropTypes from 'prop-types';
