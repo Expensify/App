7bd5be7df8da7107dabcab98f6043ac572a8626f
Etotaziba Olei <teneeto@gmail.com>
2023-09-20 13:47:58 +0100

    ref: move local file download to TS

--- src/libs/localFileDownload/index.ios.ts
+++ src/libs/localFileDownload/index.ios.ts
@@ -6,11 +6,8 @@ import * as FileUtils from '../fileDownload/FileUtils';
  * Writes a local file to the app's internal directory with the given fileName
  * and textContent, so we're able to share it using iOS' share API.
  * After the file is shared, it is removed from the internal dir.
- *
- * @param {String} fileName
- * @param {String} textContent
  */
-export default function localFileDownload(fileName, textContent) {
+export default function localFileDownload(fileName: string, textContent: string) {
     const newFileName = FileUtils.appendTimeToFileName(fileName);
     const dir = RNFetchBlob.fs.dirs.DocumentDir;
     const path = `${dir}/${newFileName}.txt`;
--- src/libs/localFileDownload/index.ts
+++ src/libs/localFileDownload/index.ts
@@ -4,11 +4,8 @@ import * as FileUtils from '../fileDownload/FileUtils';
  * Creates a Blob with the given fileName and textContent, then dynamically
  * creates a temporary anchor, just to programmatically click it, so the file
  * is downloaded by the browser.
- *
- * @param {String} fileName
- * @param {String} textContent
  */
-export default function localFileDownload(fileName, textContent) {
+export default function localFileDownload(fileName: string, textContent: string) {
     const blob = new Blob([textContent], {type: 'text/plain'});
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
