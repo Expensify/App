84669212fb272b7397486aa39ba256861d3ca46c
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-09-20 11:09:33 +0200

    chore: added TODO comment

--- src/libs/Accessibility/moveAccessibilityFocus/types.ts
+++ src/libs/Accessibility/moveAccessibilityFocus/types.ts
@@ -1,5 +1,6 @@
 import {HostComponent} from 'react-native';
 
+// TODO: Ask for help about how to type this
 type MoveAccessibilityFocus = (ref?: React.ElementRef<HostComponent<unknown>>) => void;
 
 export default MoveAccessibilityFocus;
