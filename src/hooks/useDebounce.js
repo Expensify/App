c212e9e2237377378c2c4172113ff3892ad0811a
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-09-20 16:42:18 +0800

    update params and jsdoc

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -2,21 +2,24 @@ import {useEffect, useRef} from 'react';
 import lodashDebounce from 'lodash/debounce';
 
 /**
- * @param {*} func Function to debounce `waitMS` ms.
- * @param {*} wait The number of milliseconds to wait before `func` can be invoked again.
- * @param {*} immediate True if `func` should be invoked on the leading edge of `waitMS` instead of the trailing edge.
- * @returns 
+ * @param {Function} func The function to debounce.
+ * @param {Number} wait The number of milliseconds to delay.
+ * @param {Object} options The options object.
+ * @param {Boolean} options.leading Specify invoking on the leading edge of the timeout.
+ * @param {Number} options.maxWait The maximum time func is allowed to be delayed before it’s invoked.
+ * @param {Boolean} options.trailing Specify invoking on the trailing edge of the timeout.
+ * @returns Returns the new debounced function.
  */
-export default function useDebounce(func, wait, immediate) {
+export default function useDebounce(func, wait, options) {
     const debouncedFnRef = useRef();
 
     useEffect(() => {
-        const debouncedFn = lodashDebounce(func, wait, immediate);
+        const debouncedFn = lodashDebounce(func, wait, options);
 
         debouncedFnRef.current = debouncedFn;
 
         return debouncedFn.cancel;
-    }, [func, wait, immediate]);
+    }, [func, wait, options]);
 
     return debouncedFnRef.current;
 }
