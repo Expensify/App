d9ddb6f3def934ab57c0a2803855212f8b9acedf
neil-marcellini <neil@expensify.com>
2023-09-21 15:40:40 +0800

    Display waypoints

--- src/components/EReceipt.js
+++ src/components/EReceipt.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {View} from 'react-native';
+import lodashGet from 'lodash/get';
+import _ from 'underscore';
 import Text from './Text';
 import styles from '../styles/styles';
 import transactionPropTypes from './transactionPropTypes';
 import * as ReceiptUtils from '../libs/ReceiptUtils';
 import * as ReportUtils from '../libs/ReportUtils';
 import * as CurrencyUtils from '../libs/CurrencyUtils';
+import * as TransactionUtils from '../libs/TransactionUtils';
 import tryResolveUrlFromApiRoot from '../libs/tryResolveUrlFromApiRoot';
 import ThumbnailImage from './ThumbnailImage';
+import useLocalize from '../hooks/useLocalize';
 
 const propTypes = {
     /** The transaction for the eReceipt */
@@ -19,10 +23,12 @@ const defaultProps = {
 };
 
 function EReceipt({transaction}) {
+    const {translate} = useLocalize();
     const {thumbnail} = ReceiptUtils.getThumbnailAndImageURIs(transaction.receipt.source, transaction.filename);
     const {amount: transactionAmount, currency: transactionCurrency, merchant: transactionMerchant} = ReportUtils.getTransactionDetails(transaction);
     const formattedTransactionAmount = CurrencyUtils.convertToDisplayString(transactionAmount, transactionCurrency);
     const thumbnailSource = tryResolveUrlFromApiRoot(thumbnail || '');
+    const waypoints = lodashGet(transaction, 'comment.waypoints', {});
     return (
         <>
             <View style={styles.moneyRequestViewImage}>
@@ -35,6 +41,25 @@ function EReceipt({transaction}) {
             </View>
             <Text style={styles.eReceiptAmount}>{formattedTransactionAmount}</Text>
             <Text style={styles.textHeadline}>{transactionMerchant}</Text>
+            <>
+                {_.map(waypoints, (waypoint, key) => {
+                    const index = TransactionUtils.getWaypointIndex(key);
+                    let descriptionKey = 'distance.waypointDescription.';
+                    if (index === 0) {
+                        descriptionKey += 'start';
+                    } else if (index === _.size(waypoints) - 1) {
+                        descriptionKey += 'finish';
+                    } else {
+                        descriptionKey += 'stop';
+                    }
+                    return (
+                        <View>
+                            <Text>{translate(descriptionKey)}</Text>
+                            <Text>{waypoint.address || ''}</Text>
+                        </View>
+                    );
+                })}
+            </>
         </>
     );
 }
