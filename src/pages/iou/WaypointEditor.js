6ec282dee0c0d549073c0621ce25bd4315815ba1
Yauheni <HorbachZhenja@yandex.by>
2023-09-21 08:50:06 +0200

    Fix App displays waypoint address page briefly on click on Go back to Home page

--- src/pages/iou/WaypointEditor.js
+++ src/pages/iou/WaypointEditor.js
@@ -4,7 +4,7 @@ import lodashGet from 'lodash/get';
 import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
-import {useIsFocused} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import AddressSearch from '../../components/AddressSearch';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import FullPageNotFoundView from '../../components/BlockingViews/FullPageNotFoundView';
@@ -76,7 +76,7 @@ const defaultProps = {
 function WaypointEditor({transactionID, route: {params: {iouType = '', waypointIndex = ''} = {}} = {}, transaction, recentWaypoints}) {
     const {windowWidth} = useWindowDimensions();
     const [isDeleteStopModalOpen, setIsDeleteStopModalOpen] = useState(false);
-    const isFocused = useIsFocused();
+    const isFocused = useNavigation().isFocused();
     const {translate} = useLocalize();
     const {isOffline} = useNetwork();
     const textInput = useRef(null);
