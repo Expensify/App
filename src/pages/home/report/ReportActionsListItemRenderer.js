2c539dea3b783266a68b389d9a60b93ca8a9637d
Janic Duplessis <janicduplessis@gmail.com>
2023-09-21 19:40:59 +0200

    Add prop types

--- src/pages/home/report/ReportActionsListItemRenderer.js
+++ src/pages/home/report/ReportActionsListItemRenderer.js
@@ -1,10 +1,44 @@
+import PropTypes from 'prop-types';
 import React, {memo} from 'react';
 import _ from 'underscore';
 import CONST from '../../../CONST';
 import * as ReportActionsUtils from '../../../libs/ReportActionsUtils';
 import * as ReportUtils from '../../../libs/ReportUtils';
+import reportPropTypes from '../../reportPropTypes';
 import ReportActionItem from './ReportActionItem';
 import ReportActionItemParentAction from './ReportActionItemParentAction';
+import reportActionPropTypes from './reportActionPropTypes';
+
+const propTypes = {
+    /** All the data of the action item */
+    reportAction: PropTypes.shape(reportActionPropTypes).isRequired,
+
+    /** Position index of the report action in the overall report FlatList view */
+    index: PropTypes.number.isRequired,
+
+    /** Report for this action */
+    report: reportPropTypes.isRequired,
+
+    /* Whether the option has an outstanding IOU */
+    hasOutstandingIOU: PropTypes.bool,
+
+    /** Sorted actions prepared for display */
+    sortedReportActions: PropTypes.arrayOf(PropTypes.shape(reportActionPropTypes)).isRequired,
+
+    /** The ID of the most recent IOU report action connected with the shown report */
+    mostRecentIOUReportActionID: PropTypes.string,
+
+    /** If the thread divider line should be hidden */
+    shouldHideThreadDividerLine: PropTypes.bool.isRequired,
+
+    /** Should we display the new marker on top of the comment? */
+    shouldDisplayNewMarker: PropTypes.bool.isRequired,
+};
+
+const defaultProps = {
+    mostRecentIOUReportActionID: '',
+    hasOutstandingIOU: false,
+};
 
 function ReportActionsListItemRenderer({
     reportAction,
@@ -45,5 +79,8 @@ function ReportActionsListItemRenderer({
         />
     );
 }
+
+ReportActionsListItemRenderer.propTypes = propTypes;
+ReportActionsListItemRenderer.defaultProps = defaultProps;
 
 export default memo(ReportActionsListItemRenderer);
