847ba127b829df6770a6b2b2f23f179e044aff0c
Pavlo Tsimura <paultsimura@gmail.com>
2023-09-21 08:48:47 +0200

    Formatting

--- src/components/LHNOptionsList/OptionRowLHN.js
+++ src/components/LHNOptionsList/OptionRowLHN.js
@@ -102,10 +102,7 @@ function OptionRowLHN(props) {
     const hasBrickError = optionItem.brickRoadIndicator === CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR;
     const defaultSubscriptSize = optionItem.isExpenseRequest ? CONST.AVATAR_SIZE.SMALL_NORMAL : CONST.AVATAR_SIZE.DEFAULT;
     const shouldShowGreenDotIndicator =
-        !hasBrickError &&
-        (optionItem.isUnreadWithMention ||
-            optionItem.isWaitingForTaskCompleteFromAssignee ||
-            ReportUtils.isWaitingForIOUActionFromCurrentUser(optionItem));
+        !hasBrickError && (optionItem.isUnreadWithMention || optionItem.isWaitingForTaskCompleteFromAssignee || ReportUtils.isWaitingForIOUActionFromCurrentUser(optionItem));
 
     /**
      * Show the ReportActionContextMenu modal popover.
