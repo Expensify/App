06a907cd51304c3bfbe15c2e4fec4207e82ed230
Kacper Falat <kacper.falat@callstack.com>
2023-09-21 10:58:43 +0200

    Fixed JSDoc removal.

--- src/libs/actions/Device/getDeviceInfo/index.ts
+++ src/libs/actions/Device/getDeviceInfo/index.ts
@@ -1,15 +1,6 @@
 import getBaseInfo, {DeviceInfo} from './getBaseInfo';
 import getOSAndName from './getOSAndName/index.native';
 
-/**
- * @typedef DeviceInfo
- * @type {object}
-
- */
-
-/**
- * @returns {DeviceInfo}
- */
 export default function getDeviceInfo(): DeviceInfo {
     return {
         ...getBaseInfo(),
