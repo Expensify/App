2c9910c4f297a1d19cf56206e7a7389b488b4afa
Carlos Martins <cmartins@expensify.com>
2023-09-21 11:16:19 +0800

    use receipt instead of transaction

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -2174,7 +2174,6 @@ function buildOptimisticIOUReportAction(
         }
     }
 
-    const transaction = TransactionUtils.getTransaction(transactionID);
     return {
         actionName: CONST.REPORT.ACTIONS.TYPE.IOU,
         actorAccountID: currentUserAccountID,
@@ -2195,7 +2194,7 @@ function buildOptimisticIOUReportAction(
         created: DateUtils.getDBTime(),
         pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
         receipt,
-        whisperedToAccountIDs: TransactionUtils.isReceiptBeingScanned(transaction) ? [currentUserAccountID] : [],
+        whisperedToAccountIDs: _.contains([CONST.IOU.RECEIPT_STATE.SCANREADY, CONST.IOU.RECEIPT_STATE.SCANNING], receipt.state) ? [currentUserAccountID] : [],
     };
 }
 /**
