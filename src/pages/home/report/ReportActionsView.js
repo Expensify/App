9eddbea83a437983a5f31b6027baab4f1af0a9bf
Oscar Franco <ospfranco@gmail.com>
2023-09-21 11:59:51 +0200

    Completely remove onLayout prop from ReportActionsView

--- src/pages/home/report/ReportActionsView.js
+++ src/pages/home/report/ReportActionsView.js
@@ -50,9 +50,6 @@ const propTypes = {
         avatar: PropTypes.string,
     }),
 
-    /** Used by parent component to tell Onyx when to hydrate data */
-    onLayout: PropTypes.func.isRequired,
-
     ...windowDimensionsPropTypes,
     ...withLocalizePropTypes,
 };
@@ -166,10 +163,6 @@ function ReportActionsView(props) {
      * Runs when the FlatList finishes laying out
      */
     const recordTimeToMeasureItemLayout = () => {
-        if (props.onLayout) {
-            props.onLayout();
-        }
-
         if (didLayout.current) {
             return;
         }
