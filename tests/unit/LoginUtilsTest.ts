104e1d0100ce707abe6cac0ed5c2ddcb2920afed
Kamil Owczarz <kamil.owczarz@swmansion.com>
2023-09-22 15:04:42 +0200

    Add login utils tests

---
+++ tests/unit/LoginUtilsTest.ts
@@ -0,0 +1,101 @@
+import Onyx from "react-native-onyx";
+import ONYXKEYS from '../../src/ONYXKEYS';
+import waitForPromisesToResolve from '../utils/waitForPromisesToResolve';
+import {
+  appendCountryCode, getPhoneLogin,
+  getPhoneNumberWithoutSpecialChars,
+  isEmailPublicDomain,
+  validateNumber
+} from '../../src/libs/LoginUtils'
+
+describe('LoginUtils', () => {
+  beforeAll(() => {
+    Onyx.init({
+      keys: ONYXKEYS,
+      initialKeyStates: {
+        [ONYXKEYS.COUNTRY_CODE]: 1,
+      },
+    });
+    return waitForPromisesToResolve();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+    Onyx.clear();
+  });
+  describe('getPhoneNumberWithoutSpecialChars', () => {
+    it('Should return valid phone number', () =>{
+      const givenPhone = '+12345678901'
+      const parsedPhone = getPhoneNumberWithoutSpecialChars(givenPhone)
+      expect(parsedPhone).toBe('+12345678901')
+    })
+    it('Should return valid phone number even if received special chars', () =>{
+      const givenPhone = '+1(234) 56-7\t8-9 01'
+      const parsedPhone = getPhoneNumberWithoutSpecialChars(givenPhone)
+      expect(parsedPhone).toBe('+12345678901')
+    })
+  })
+  describe('appendCountryCode', () => {
+    it('Should return valid phone number with country code when received a phone with country code', () =>{
+      const givenPhone = '+12345678901'
+      const parsedPhone = appendCountryCode(givenPhone)
+      expect(parsedPhone).toBe('+12345678901')
+    })
+    it('Should return valid phone number with country code when received a phone without country code', () =>{
+      const givenPhone = '2345678901'
+      const parsedPhone = appendCountryCode(givenPhone)
+      expect(parsedPhone).toBe('+12345678901')
+    })
+  })
+  describe('isEmailPublicDomain', () => {
+    it('Should return true if email is from public domain', () =>{
+      const givenEmail = 'test@gmail.com'
+      const parsedEmail = isEmailPublicDomain(givenEmail)
+      expect(parsedEmail).toBe(true)
+    })
+    it('Should return false if email is not from public domain', () =>{
+      const givenEmail = 'test@test.com'
+      const parsedEmail = isEmailPublicDomain(givenEmail)
+      expect(parsedEmail).toBe(false)
+    })
+    it("Should return false if provided string isn't email", () =>{
+      const givenEmail = 'test'
+      const parsedEmail = isEmailPublicDomain(givenEmail)
+      expect(parsedEmail).toBe(false)
+    })
+  })
+  describe('validateNumber', () => {
+    it("Should return valid phone number with '@expensify.sms' suffix if provided phone number is valid", () =>{
+      const givenPhone = '+12345678901'
+      const parsedPhone = validateNumber(givenPhone)
+      expect(parsedPhone).toBe('+12345678901@expensify.sms')
+    })
+    it('Should return empty string if provided phone number is not valid', () =>{
+      const givenPhone = '786'
+      const parsedPhone = validateNumber(givenPhone)
+      expect(parsedPhone).toBe('')
+    })
+    it('Should return empty string if provided phone number is empty', () =>{
+      const givenPhone = ''
+      const parsedPhone = validateNumber(givenPhone)
+      expect(parsedPhone).toBe('')
+    })
+  })
+  describe('getPhoneLogin', () => {
+    it("Should return valid phone number with country code if provided phone number is valid and with country code", () =>{
+      const givenPhone = '+12345678901'
+      const parsedPhone = getPhoneLogin(givenPhone)
+      expect(parsedPhone).toBe('+12345678901')
+    })
+    it('Should return valid phone number with country code if provided phone number is valid and without country code', () =>{
+      const givenPhone = '2345678901'
+      const parsedPhone = getPhoneLogin(givenPhone)
+      expect(parsedPhone).toBe('+12345678901')
+    })
+    it('Should return empty string if provided phone number is empty', () =>{
+      const givenPhone = ''
+      const parsedPhone = getPhoneLogin(givenPhone)
+      expect(parsedPhone).toBe('')
+    })
+  })
+})
