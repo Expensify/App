7786995f9a6390062b35cd3cff3f3f6fe25445ad
Kamil Owczarz <kamil.owczarz@swmansion.com>
2023-09-22 12:54:14 +0200

    Update comments

--- src/libs/actions/Device/getDeviceInfo/getOSAndName/index.native.ts
+++ src/libs/actions/Device/getDeviceInfo/getOSAndName/index.native.ts
@@ -6,8 +6,10 @@ const getOSAndName: GetOSAndName = () => {
     const deviceName = RNDeviceInfo.getDeviceNameSync();
     const prettyName = `${Str.UCFirst(RNDeviceInfo.getManufacturerSync() || '')} ${deviceName}`;
     return {
+        // Parameter names are predefined and we don't choose it here
         // eslint-disable-next-line @typescript-eslint/naming-convention
         device_name: RNDeviceInfo.isEmulatorSync() ? `Emulator - ${prettyName}` : prettyName,
+        // Parameter names are predefined and we don't choose it here
         // eslint-disable-next-line @typescript-eslint/naming-convention
         os_version: RNDeviceInfo.getSystemVersion(),
     };
--- src/libs/actions/Device/getDeviceInfo/getOSAndName/types.ts
+++ src/libs/actions/Device/getDeviceInfo/getOSAndName/types.ts
@@ -1,4 +1,4 @@
-// to keep same behavior from before migration
+// Parameter names are predefined and we don't choose it here
 // eslint-disable-next-line @typescript-eslint/naming-convention
 type GetOSAndName = () => {device_name: string | undefined; os_version: string | undefined};
 export default GetOSAndName;
