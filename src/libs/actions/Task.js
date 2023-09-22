b63b77a2181aa94325b8535957fb5f4b508224bb
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2023-09-22 11:31:51 +0000

    update task messages

--- src/libs/actions/Task.js
+++ src/libs/actions/Task.js
@@ -69,8 +69,7 @@ function createTaskAndNavigate(parentReportID, title, description, assigneeEmail
 
     // Parent ReportAction indicating that a task has been created
     const optimisticTaskCreatedAction = ReportUtils.buildOptimisticCreatedReportAction(currentUserEmail);
-    const displayName = lodashGet(allPersonalDetails, [currentUserAccountID, 'displayName']) || lodashGet(allPersonalDetails, [currentUserAccountID, 'login']);
-    const optimisticAddCommentReport = ReportUtils.buildOptimisticTaskCommentReportAction(taskReportID, title, assigneeEmail, assigneeAccountID, `${displayName}: task for ${title}`, parentReportID);
+    const optimisticAddCommentReport = ReportUtils.buildOptimisticTaskCommentReportAction(taskReportID, title, assigneeEmail, assigneeAccountID, `task for ${title}`, parentReportID);
     optimisticTaskReport.parentReportActionID = optimisticAddCommentReport.reportAction.reportActionID;
 
     const currentTime = DateUtils.getDBTime();
