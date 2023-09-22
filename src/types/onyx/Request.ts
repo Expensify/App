1719af4afbbffb8f4ba06d049966f135beb20e40
Blazej Kustra <blazej.kustra@swmansion.com>
2023-09-22 17:59:28 +0200

    Changes in onyx types

--- src/types/onyx/Request.ts
+++ src/types/onyx/Request.ts
@@ -1,12 +1,17 @@
 import {OnyxUpdate} from 'react-native-onyx';
+import Response from './Response';
 
 type Request = {
     command: string;
+    commandName?: string;
     data?: Record<string, unknown>;
     type?: string;
     shouldUseSecure?: boolean;
     successData?: OnyxUpdate[];
     failureData?: OnyxUpdate[];
+
+    resolve?: (value: Response) => void;
+    reject?: (value?: unknown) => void;
 };
 
 export default Request;
--- src/types/onyx/Session.ts
+++ src/types/onyx/Session.ts
@@ -5,6 +5,8 @@ type Session = {
     /** Currently logged in user authToken */
     authToken?: string;
 
+    supportAuthToken?: string;
+
     /** Currently logged in user encrypted authToken */
     encryptedAuthToken?: string;
 
