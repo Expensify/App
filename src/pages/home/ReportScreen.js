313c717d1d00321686eb9d8899612737b1322a9e
rory <rory@expensify.com>
2023-09-22 10:27:12 +0800

    Fix lint

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -88,7 +88,6 @@ const propTypes = {
     /** Whether user is leaving the current report */
     userLeavingStatus: PropTypes.bool,
 
-    ...windowDimensionsPropTypes,
     ...viewportOffsetTopPropTypes,
     ...withCurrentReportIDPropTypes,
 };
