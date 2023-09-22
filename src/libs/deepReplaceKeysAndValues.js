2f29985cdf3c1b580c691a2a5a9e63ad5b1d7a88
rory <rory@expensify.com>
2023-09-22 16:21:59 +0800

    Create deepReplaceKeysAndValues function

---
+++ src/libs/deepReplaceKeysAndValues.js
@@ -0,0 +1,51 @@
+import _ from 'underscore';
+
+/**
+ * @param {Object} obj the object to transform
+ * @param {String} oldVal the value to search for
+ * @param {String} newVal the replacement value
+ */
+function deepReplaceKeysAndValues(obj, oldVal, newVal) {
+    if (!obj) {
+        return obj;
+    }
+
+    if (typeof obj === 'string') {
+        return obj.replace(oldVal, newVal);
+    }
+
+    if (typeof obj !== 'object') {
+        return obj;
+    }
+
+    if (_.isArray(obj)) {
+        return _.map(obj, (item) => deepReplaceKeysAndValues(item, oldVal, newVal));
+    }
+
+    const newObj = {};
+    _.each(obj, (value, key) => {
+        // eslint-disable-next-line eqeqeq
+        let newKey = key == oldVal ? newVal : key;
+        if (_.isString(newKey)) {
+            newKey = newKey.replace(oldVal, newVal);
+        }
+
+        if (_.isObject(value)) {
+            newObj[newKey] = deepReplaceKeysAndValues(value, oldVal, newVal);
+            return;
+        }
+        // eslint-disable-next-line eqeqeq
+        if (value == oldVal) {
+            newObj[newKey] = newVal;
+            return;
+        }
+        if (_.isString(value)) {
+            newObj[newKey] = value.replace(oldVal, newVal);
+            return;
+        }
+        newObj[newKey] = value;
+    });
+    return newObj;
+}
+
+export default deepReplaceKeysAndValues;
