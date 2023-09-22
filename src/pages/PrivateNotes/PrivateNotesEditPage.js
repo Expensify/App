715a9f42363a13bae7160bd7a92b700b7d229813
Hans <visaocuoitroi193@gmail.com>
2023-09-22 13:21:49 +0700

    fix Save button is not at the bottom of the page

--- src/pages/PrivateNotes/PrivateNotesEditPage.js
+++ src/pages/PrivateNotes/PrivateNotesEditPage.js
@@ -92,47 +92,44 @@ function PrivateNotesEditPage({route, personalDetailsList, session, report}) {
                     shouldShowBackButton
                     onCloseButtonPress={() => Navigation.dismissModal()}
                 />
-                <View style={[styles.flexGrow1, styles.ph5]}>
-                    <View style={[styles.mb5]}>
-                        <Text>
-                            {translate(
-                                Str.extractEmailDomain(lodashGet(personalDetailsList, [route.params.accountID, 'login'], '')) === CONST.EMAIL.GUIDES_DOMAIN
-                                    ? 'privateNotes.sharedNoteMessage'
-                                    : 'privateNotes.personalNoteMessage',
-                            )}
-                        </Text>
-                    </View>
-                    <Form
-                        formID={ONYXKEYS.FORMS.PRIVATE_NOTES_FORM}
-                        onSubmit={savePrivateNote}
-                        submitButtonText={translate('common.save')}
-                        enabledWhenOffline
+                <Form
+                    formID={ONYXKEYS.FORMS.PRIVATE_NOTES_FORM}
+                    onSubmit={savePrivateNote}
+                    style={[styles.flexGrow1, styles.ph5]}
+                    submitButtonText={translate('common.save')}
+                    enabledWhenOffline
+                >
+                    <Text style={[styles.mb5]}>
+                        {translate(
+                            Str.extractEmailDomain(lodashGet(personalDetailsList, [route.params.accountID, 'login'], '')) === CONST.EMAIL.GUIDES_DOMAIN
+                                ? 'privateNotes.sharedNoteMessage'
+                                : 'privateNotes.personalNoteMessage',
+                        )}
+                    </Text>
+                    <OfflineWithFeedback
+                        errors={{
+                            ...lodashGet(report, ['privateNotes', route.params.accountID, 'errors'], ''),
+                        }}
+                        onClose={() => Report.clearPrivateNotesError(report.reportID, route.params.accountID)}
+                        style={[styles.mb3]}
                     >
-                        <OfflineWithFeedback
-                            errors={{
-                                ...lodashGet(report, ['privateNotes', route.params.accountID, 'errors'], ''),
-                            }}
-                            onClose={() => Report.clearPrivateNotesError(report.reportID, route.params.accountID)}
-                            style={[styles.mb3]}
-                        >
-                            <TextInput
-                                accessibilityRole={CONST.ACCESSIBILITY_ROLE.TEXT}
-                                inputID="privateNotes"
-                                label={translate('privateNotes.composerLabel')}
-                                accessibilityLabel={translate('privateNotes.title')}
-                                autoCompleteType="off"
-                                autoCorrect={false}
-                                autoGrowHeight
-                                textAlignVertical="top"
-                                containerStyles={[styles.autoGrowHeightMultilineInput]}
-                                defaultValue={privateNote}
-                                value={privateNote}
-                                onChangeText={(text) => setPrivateNote(text)}
-                                ref={(el) => (privateNotesInput.current = el)}
-                            />
-                        </OfflineWithFeedback>
-                    </Form>
-                </View>
+                        <TextInput
+                            accessibilityRole={CONST.ACCESSIBILITY_ROLE.TEXT}
+                            inputID="privateNotes"
+                            label={translate('privateNotes.composerLabel')}
+                            accessibilityLabel={translate('privateNotes.title')}
+                            autoCompleteType="off"
+                            autoCorrect={false}
+                            autoGrowHeight
+                            textAlignVertical="top"
+                            containerStyles={[styles.autoGrowHeightMultilineInput]}
+                            defaultValue={privateNote}
+                            value={privateNote}
+                            onChangeText={(text) => setPrivateNote(text)}
+                            ref={(el) => (privateNotesInput.current = el)}
+                        />
+                    </OfflineWithFeedback>
+                </Form>
             </FullPageNotFoundView>
         </ScreenWrapper>
     );
