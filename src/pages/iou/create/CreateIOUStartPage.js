deafd63a83cdcd746dafacbe5f080e89293b4503
Tim Golen <tgolen@expensify.com>
2023-09-25 16:23:35 +0800

    Create skeleton pages for tabs

--- src/pages/iou/create/CreateIOUStartPage.js
+++ src/pages/iou/create/CreateIOUStartPage.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import lodashGet from 'lodash/get';
 import {withOnyx} from 'react-native-onyx';
@@ -23,6 +24,17 @@ import * as CurrencyUtils from '../../../libs/CurrencyUtils';
 import EditRequestDistancePage from '../../EditRequestDistancePage';
 import FullPageNotFoundView from '../../../components/BlockingViews/FullPageNotFoundView';
 import EditRequestCategoryPage from '../../EditRequestCategoryPage';
+import ScreenWrapper from '../../../components/ScreenWrapper';
+import useLocalize from '../../../hooks/useLocalize';
+import styles from '../../../styles/styles';
+import themeColors from '../../../styles/themes/default';
+import DragAndDropProvider from '../../../components/DragAndDrop/Provider';
+import * as IOUUtils from '../../../libs/IOUUtils';
+import HeaderWithBackButton from '../../../components/HeaderWithBackButton';
+import TabSelector from '../../../components/TabSelector/TabSelector';
+import OnyxTabNavigator, {TopTab} from '../../../libs/Navigation/OnyxTabNavigator';
+import NewRequestAmountPage from '../steps/NewRequestAmountPage';
+import CreateIOUStartTabManual from './CreateIOUStartTabManual';
 
 const propTypes = {
     /** Route from navigation */
@@ -43,9 +55,89 @@ const propTypes = {
 
 const defaultProps = {};
 
-function CreateIOUStartPage() {
-    // Show the tab selector for switching transaction type
-    return <FullPageNotFoundView shouldShow />;
+function CreateIOUStartPage({
+    route: {
+        params: {iouType, reportID},
+    },
+}) {
+    const {translate} = useLocalize();
+    const [isDraggingOver, setIsDraggingOver] = useState(false);
+    const title = {
+        [CONST.IOU.MONEY_REQUEST_TYPE.REQUEST]: translate('iou.requestMoney'),
+        [CONST.IOU.MONEY_REQUEST_TYPE.SEND]: translate('iou.sendMoney'),
+        [CONST.IOU.MONEY_REQUEST_TYPE.SPLIT]: translate('iou.splitBill'),
+    };
+    // @TODO const isFromGlobalCreate = !reportID;
+    // @TODO const isExpenseRequest = ReportUtils.isPolicyExpenseChat(props.report);
+    // @TODO const shouldDisplayDistanceRequest = isExpenseRequest || isFromGlobalCreate;
+    const shouldDisplayDistanceRequest = true;
+
+    return (
+        <ScreenWrapper
+            includeSafeAreaPaddingBottom={false}
+            shouldEnableKeyboardAvoidingView={false}
+            headerGapStyles={
+                isDraggingOver
+                    ? [
+                          {
+                              backgroundColor: themeColors.receiptDropUIBG,
+                          },
+                      ]
+                    : []
+            }
+            testID={CreateIOUStartPage.displayName}
+        >
+            {({safeAreaPaddingBottomStyle}) => (
+                <FullPageNotFoundView shouldShow={!IOUUtils.isValidMoneyRequestType(iouType)}>
+                    <DragAndDropProvider
+                        isDisabled={props.selectedTab !== CONST.TAB.SCAN}
+                        setIsDraggingOver={setIsDraggingOver}
+                    >
+                        <View style={[styles.flex1, safeAreaPaddingBottomStyle]}>
+                            <HeaderWithBackButton
+                                title={title[iouType]}
+                                onBackButtonPress={Navigation.dismissModal}
+                            />
+                            {iouType === CONST.IOU.MONEY_REQUEST_TYPE.REQUEST ? (
+                                <OnyxTabNavigator
+                                    id={CONST.TAB.RECEIPT_TAB_ID}
+                                    selectedTab={props.selectedTab}
+                                    tabBar={({state, navigation, position}) => (
+                                        <TabSelector
+                                            state={state}
+                                            navigation={navigation}
+                                            onTabPress={resetMoneyRequestInfo}
+                                            position={position}
+                                        />
+                                    )}
+                                >
+                                    <TopTab.Screen
+                                        name={CONST.TAB.MANUAL}
+                                        component={CreateIOUStartTabManual}
+                                        initialParams={{reportID, iouType}}
+                                    />
+                                    <TopTab.Screen
+                                        name={CONST.TAB.SCAN}
+                                        component={ReceiptSelector}
+                                        initialParams={{reportID, iouType, pageIndex: 1}}
+                                    />
+                                    {shouldDisplayDistanceRequest && (
+                                        <TopTab.Screen
+                                            name={CONST.TAB.DISTANCE}
+                                            component={NewDistanceRequestPage}
+                                            initialParams={{reportID, iouType}}
+                                        />
+                                    )}
+                                </OnyxTabNavigator>
+                            ) : (
+                                <CreateIOUStartTabManual route={props.route} />
+                            )}
+                        </View>
+                    </DragAndDropProvider>
+                </FullPageNotFoundView>
+            )}
+        </ScreenWrapper>
+    );
 }
 
 CreateIOUStartPage.displayName = 'CreateIOUStartPage';
---
+++ src/pages/iou/create/CreateIOUStartTabDistance.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import {withOnyx} from 'react-native-onyx';
+import useLocalize from '../../../hooks/useLocalize';
+import * as IOUUtils from '../../../libs/IOUUtils';
+import FullPageNotFoundView from '../../../components/BlockingViews/FullPageNotFoundView';
+import styles from '../../../styles/styles';
+import HeaderWithBackButton from '../../../components/HeaderWithBackButton';
+import ScreenWrapper from '../../../components/ScreenWrapper';
+
+const propTypes = {
+    /** React Navigation route */
+    route: PropTypes.shape({
+        /** Params from the route */
+        params: PropTypes.shape({
+            /** The type of IOU report, i.e. bill, request, send */
+            iouType: PropTypes.string,
+
+            /** The optimistic ID of a new transaction that is being created */
+            transactionID: PropTypes.string.isRequired,
+
+            /** The report ID of the IOU */
+            reportID: PropTypes.string,
+        }),
+    }).isRequired,
+};
+
+const defaultProps = {};
+
+function CreateIOUStartTabDistance() {
+    const {translate} = useLocalize();
+
+    // @TODO const content = (
+    //     <MoneyRequestAmountForm
+    //         isEditing={isEditing}
+    //         currency={currency}
+    //         amount={iou.amount}
+    //         ref={(e) => (textInput.current = e)}
+    //         onCurrencyButtonPress={navigateToCurrencySelectionPage}
+    //         onSubmitButtonPress={navigateToNextPage}
+    //     />
+    // );
+    const content = null;
+
+    // ScreenWrapper is only needed in edit mode because we have a dedicated route for the edit amount page (MoneyRequestEditAmountPage).
+    // The rest of the cases this component is rendered through <MoneyRequestSelectorPage /> which has it's own ScreenWrapper
+    if (!isEditing) {
+        return content;
+    }
+
+    return (
+        <ScreenWrapper
+            includeSafeAreaPaddingBottom={false}
+            shouldEnableKeyboardAvoidingView={false}
+            onEntryTransitionEnd={focusTextInput}
+            testID={CreateIOUStartTabDistance.displayName}
+        >
+            {({safeAreaPaddingBottomStyle}) => (
+                <FullPageNotFoundView shouldShow={!IOUUtils.isValidMoneyRequestType(iouType)}>
+                    <View style={[styles.flex1, safeAreaPaddingBottomStyle]}>
+                        <HeaderWithBackButton
+                            title={translate('iou.amount')}
+                            onBackButtonPress={navigateBack}
+                        />
+                        {content}
+                    </View>
+                </FullPageNotFoundView>
+            )}
+        </ScreenWrapper>
+    );
+}
+
+CreateIOUStartTabDistance.propTypes = propTypes;
+CreateIOUStartTabDistance.defaultProps = defaultProps;
+CreateIOUStartTabDistance.displayName = 'CreateIOUStartTabDistance';
+
+export default withOnyx({})(CreateIOUStartTabDistance);
