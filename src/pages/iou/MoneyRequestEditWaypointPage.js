f84362e95f89b0b70c834d63bdcee7e504a34a9e
Tim Golen <tgolen@expensify.com>
2023-09-26 16:15:51 +0800

    Remove comment

--- src/pages/iou/MoneyRequestEditWaypointPage.js
+++ src/pages/iou/MoneyRequestEditWaypointPage.js
@@ -22,8 +22,6 @@ const defaultProps = {
     route: {},
 };
 
-// This component is responsible for grabbing the transactionID from the IOU key
-// You can't use Onyx props in the withOnyx mapping, so we need to set up and access the transactionID here, and then pass it down so that WaypointEditor can subscribe to the transaction.
 function MoneyRequestEditWaypointPage({route}) {
     return <WaypointEditor route={route} />;
 }
