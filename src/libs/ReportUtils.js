5dc91a13a198bd8e16c5f761b2188be25b3aeb2b
Jasper Huang <jasper@expensify.com>
2023-09-26 18:09:03 +0800

    show all types of UCRs in the LHN even if there aren't any participants

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -2923,7 +2923,7 @@ function shouldReportBeInOptionList(report, currentReportId, isInGSDMode, betas,
         (report.participantAccountIDs &&
             report.participantAccountIDs.length === 0 &&
             !isChatThread(report) &&
-            !isPublicRoom(report) &&
+            !isUserCreatedPolicyRoom(report) &&
             !isArchivedRoom(report) &&
             !isMoneyRequestReport(report) &&
             !isTaskReport(report))
