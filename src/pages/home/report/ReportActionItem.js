3d1a4725d264d4575bcbaeb8d9c6ac1078e80ba9
dukenv0307 <dukenv0307@gmail.com>
2023-09-27 11:41:09 +0700

    fix lint

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -557,7 +557,6 @@ function ReportActionItem(props) {
                     />
                 </OfflineWithFeedback>
             );
-            
         }
 
         const isNormalCreatedAction =
