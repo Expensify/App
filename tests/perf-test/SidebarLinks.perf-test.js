5f259e2eb36f941958013fbf1822cb415d528cbc
Adam Horodyski <ad.horodyski@gmail.com>
2023-09-28 17:13:45 +0200

    fix: ensure that the sidebar test renders a list of items

--- tests/perf-test/SidebarLinks.perf-test.js
+++ tests/perf-test/SidebarLinks.perf-test.js
@@ -1,57 +1,67 @@
 import {measurePerformance} from 'reassure';
 import Onyx from 'react-native-onyx';
+import {screen} from '@testing-library/react-native';
 import _ from 'underscore';
 import * as LHNTestUtils from '../utils/LHNTestUtils';
 import CONST from '../../src/CONST';
+import ONYXKEYS from '../../src/ONYXKEYS';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
+import wrapOnyxWithWaitForBatchedUpdates from '../utils/wrapOnyxWithWaitForBatchedUpdates';
 
 jest.mock('../../src/libs/Permissions');
 jest.mock('../../src/components/Icon/Expensicons');
 
-const ONYXKEYS = {
-    PERSONAL_DETAILS: 'personalDetails',
-    NVP_PRIORITY_MODE: 'nvp_priorityMode',
-    SESSION: 'session',
-    BETAS: 'betas',
-    COLLECTION: {
-        REPORT: 'report_',
-        REPORT_ACTIONS: 'reportActions_',
-    },
-    NETWORK: 'network',
-};
-
 beforeAll(() =>
     Onyx.init({
         keys: ONYXKEYS,
+        safeEvictionKeys: [ONYXKEYS.COLLECTION.REPORT_ACTIONS],
         registerStorageEventListener: () => {},
     }),
 );
 
 // Initialize the network key for OfflineWithFeedback
-beforeEach(() => Onyx.merge(ONYXKEYS.NETWORK, {isOffline: false}));
+beforeEach(() => {
+    wrapOnyxWithWaitForBatchedUpdates(Onyx);
+    return Onyx.merge(ONYXKEYS.NETWORK, {isOffline: false})
+});
 
 // Clear out Onyx after each test so that each test starts with a clean slate
 afterEach(() => {
     Onyx.clear();
 });
 
-test('simple Sidebar render with hundred of reports', () => {
-    const mockReports = Array.from({length: 100}, (__, i) => {
+const getMockedReportsMap = (length = 100) => {
+    const mockReports = Array.from({length}, (__, i) => {
         const reportID = i + 1;
-        const emails = [`email${reportID}@test.com`];
+        const participants = [1, 2];
         const reportKey = `${ONYXKEYS.COLLECTION.REPORT}${reportID}`;
-        const report = LHNTestUtils.getFakeReport(emails, 1, false);
+        const report = LHNTestUtils.getFakeReport(participants, 1, true);
+
         return {[reportKey]: report};
     });
-    const mockOnyxReports = _.assign({}, ...mockReports);
+    
+    return _.assign({}, ...mockReports);
+}
+
+test('should render Sidebar with 500 reports stored', () => {
+    const scenario = async () => {
+        /**
+         * Query for display names of participants [1, 2]. 
+         * This will ensure that the sidebar renders a list of items.
+         */
+        await screen.findAllByText('One, Two');
+    }
 
     return waitForBatchedUpdates()
         .then(() =>
             Onyx.multiSet({
                 [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.DEFAULT,
                 [ONYXKEYS.PERSONAL_DETAILS_LIST]: LHNTestUtils.fakePersonalDetails,
-                ...mockOnyxReports,
+                [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS, CONST.BETAS.POLICY_ROOMS],
+                [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
+                [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
+                ...getMockedReportsMap(500),
             }),
         )
-        .then(() => measurePerformance(<LHNTestUtils.MockedSidebarLinks />));
+        .then(() => measurePerformance(<LHNTestUtils.MockedSidebarLinks />, {scenario}));
 });
