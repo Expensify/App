31a94c9471a17a07ae1f6539e8e7d9a2e5e824ce
Aswin S <mr.aswin@gmail.com>
2023-09-28 09:15:53 +0530

    fix: add delay in setting opacity

--- src/components/EmptyStateBackground.js
+++ src/components/EmptyStateBackground.js
@@ -7,19 +7,23 @@ import * as StyleUtils from '../styles/StyleUtils';
 import variables from '../styles/variables';
 
 export default function EmptyStateBackground() {
-    const [visible, setVisible] = React.useState(false);
+    const [visible, setVisible] = React.useState(false);    
     const {isSmallScreenWidth, windowWidth} = useWindowDimensions();
-    const onImageLoaded = () => setVisible(true);
+    const onImageLoaded = () =>{
+        // Wait for background image size calculation to finish        
+        setTimeout(()=>setVisible(true), 100);
+    }
     // If window width is greater than the max background width, repeat the background image
     const maxBackgroundWidth = variables.sideBarWidth + CONST.EMPTY_STATE_BACKGROUND.ASPECT_RATIO * CONST.EMPTY_STATE_BACKGROUND.WIDE_SCREEN.IMAGE_HEIGHT;
+
 
     return (
         <Image
             pointerEvents="none"
-            onLoadEnd={onImageLoaded}
+            onLoad={onImageLoaded}
             source={EmptyStateBackgroundImage}
             style={[StyleUtils.getReportWelcomeBackgroundImageStyle(isSmallScreenWidth), {opacity: visible ? 1 : 0}]}
-            resizeMode={windowWidth > maxBackgroundWidth ? 'repeat' : 'cover'}
+            resizeMode={(windowWidth < maxBackgroundWidth) ? 'cover' : 'repeat'}
         />
     );
 }
