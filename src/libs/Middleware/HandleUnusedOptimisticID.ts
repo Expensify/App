15756baa8466632c2f52bfdce827e170c2728e91
rory <rory@expensify.com>
2023-09-28 14:27:16 +0800

    Get middleware working correctly

--- src/libs/Middleware/HandleUnusedOptimisticID.ts
+++ src/libs/Middleware/HandleUnusedOptimisticID.ts
@@ -1,17 +1,36 @@
+import ONYXKEYS from '../../ONYXKEYS';
+import Report from '../../types/onyx/Report';
 import {Middleware} from '../Request';
 import * as PersistedRequests from '../actions/PersistedRequests';
 import deepReplaceKeysAndValues from '../deepReplaceKeysAndValues';
 
 const handleUnusedOptimisticID: Middleware = (requestResponse, request) =>
     requestResponse.then((response) => {
-        if (response && typeof response === 'object' && 'preexistingReportID' in response) {
+        const responseOnyxData = response?.onyxData;
+        responseOnyxData.forEach((onyxData: {key: string; value: Record<string, unknown>}) => {
+            const key = onyxData.key;
+            if (!key.startsWith(ONYXKEYS.COLLECTION.REPORT)) {
+                return;
+            }
+
+            if (!onyxData.value) {
+                return;
+            }
+
+            const report: Report = onyxData.value as Report;
+            if (!report.preexistingReportID) {
+                return;
+            }
+            const preexistingReportID = report.preexistingReportID;
             const oldReportID = request.data?.reportID;
-            PersistedRequests.getAll().forEach((persistedRequest, index) => {
-                const oldRequest = persistedRequest;
-                oldRequest.data = deepReplaceKeysAndValues(oldRequest.data, oldReportID as string, response.preexistingReportID as string);
-                PersistedRequests.update(index, oldRequest);
-            });
-        }
+            PersistedRequests.getAll()
+                .slice(1)
+                .forEach((persistedRequest, index) => {
+                    const oldRequest = persistedRequest;
+                    oldRequest.data = deepReplaceKeysAndValues(oldRequest.data, oldReportID as string, preexistingReportID);
+                    PersistedRequests.update(index, oldRequest);
+                });
+        });
         return response;
     });
 
--- src/types/onyx/Report.ts
+++ src/types/onyx/Report.ts
@@ -55,7 +55,7 @@ type Report = {
     reportName?: string;
 
     /** ID of the report */
-    reportID?: string;
+    reportID: string;
 
     /** The state that the report is currently in */
     stateNum?: ValueOf<typeof CONST.REPORT.STATE_NUM>;
@@ -83,6 +83,7 @@ type Report = {
     participantAccountIDs?: number[];
     total?: number;
     currency?: string;
+    preexistingReportID?: string;
 };
 
 export default Report;
