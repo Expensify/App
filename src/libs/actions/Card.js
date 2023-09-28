68f2b5b235b794688fc68e6e20309012e7075d3a
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-09-28 15:12:44 +0200

    rename card actions file

---
+++ src/libs/actions/Card.js
@@ -0,0 +1,52 @@
+import Onyx from 'react-native-onyx';
+import ONYXKEYS from '../../ONYXKEYS';
+import * as API from '../API';
+
+/**
+ * Call the API to deactivate the card and request a new one
+ * @param {String} cardId - id of the card that is going to be replaced
+ * @param {String} reason - reason for replacement ('damaged' | 'stolen')
+ */
+function requestReplacementExpensifyCard(cardId, reason) {
+    API.write(
+        'RequestReplacementExpensifyCard',
+        {
+            cardId,
+            reason,
+        },
+        {
+            optimisticData: [
+                {
+                    onyxMethod: Onyx.METHOD.MERGE,
+                    key: ONYXKEYS.FORMS.REPORT_PHYSICAL_CARD_LOST_FORM,
+                    value: {
+                        isLoading: true,
+                    },
+                },
+            ],
+            successData: [
+                {
+                    onyxMethod: Onyx.METHOD.MERGE,
+                    key: ONYXKEYS.FORMS.REPORT_FRAUD_FORM,
+                    value: {
+                        isLoading: false,
+                    },
+                },
+            ],
+            failureData: [
+                {
+                    onyxMethod: Onyx.METHOD.MERGE,
+                    key: ONYXKEYS.FORMS.REPORT_PHYSICAL_CARD_LOST_FORM,
+                    value: {
+                        isLoading: false,
+                    },
+                },
+            ],
+        },
+    );
+}
+
+export {
+    // eslint-disable-next-line import/prefer-default-export
+    requestReplacementExpensifyCard,
+};
