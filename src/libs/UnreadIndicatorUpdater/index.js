268e57af777a3a61d9b0bb10a46596952e8d3f60
hurali97 <hurali97@gmail.com>
2023-09-29 17:12:27 +0500

    perf: call updateUnread method if count is changed

--- src/libs/UnreadIndicatorUpdater/index.js
+++ src/libs/UnreadIndicatorUpdater/index.js
@@ -1,14 +1,26 @@
 import _ from 'underscore';
 import Onyx from 'react-native-onyx';
+import {InteractionManager} from 'react-native';
 import ONYXKEYS from '../../ONYXKEYS';
 import updateUnread from './updateUnread/index';
 import * as ReportUtils from '../ReportUtils';
+
+let previousUnreadCount = 0;
 
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.REPORT,
     waitForCollectionCallback: true,
     callback: (reportsFromOnyx) => {
-        const unreadReports = _.filter(reportsFromOnyx, ReportUtils.isUnread);
-        updateUnread(_.size(unreadReports));
+        if (!reportsFromOnyx) {
+            return;
+        }
+
+        InteractionManager.runAfterInteractions(() => {
+            const unreadReportsCount = _.filter(reportsFromOnyx, ReportUtils.isUnread).length || 0;
+            if (previousUnreadCount !== unreadReportsCount) {
+                previousUnreadCount = unreadReportsCount;
+                updateUnread(unreadReportsCount);
+            }
+        });
     },
 });
