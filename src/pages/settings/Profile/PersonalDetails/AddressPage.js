6f1b1c00a53eb0b498fb58148109ac539b1da76b
Saransh Balyan <57568571+saranshbalyan-1234@users.noreply.github.com>
2023-09-29 15:01:52 +0530

    resolved comments

--- src/pages/settings/Profile/PersonalDetails/AddressPage.js
+++ src/pages/settings/Profile/PersonalDetails/AddressPage.js
@@ -71,16 +71,16 @@ function updateAddress(values) {
 function AddressPage({privatePersonalDetails, route}) {
     usePrivatePersonalDetails();
     const {translate} = useLocalize();
+    const [currentCountry, setCurrentCountry] = useState(PersonalDetails.getCountryISO(address.country));
     const countryFromUrl = lodashGet(route, 'params.country');
     const isUSAForm = currentCountry === CONST.COUNTRY.US;
     const zipSampleFormat = lodashGet(CONST.COUNTRY_ZIP_REGEX_DATA, [currentCountry, 'samples'], '');
     const zipFormat = translate('common.zipCodeExampleFormat', {zipSampleFormat});
-
+    
     const address = lodashGet(privatePersonalDetails, 'address') || {};
     const isLoadingPersonalDetails = lodashGet(privatePersonalDetails, 'isLoading', true);
     const [street1, street2] = (address.street || '').split('\n');
     const [state, setState] = useState(address.state);
-    const [currentCountry, setCurrentCountry] = useState(PersonalDetails.getCountryISO(address.country));
     /**
      * @param {Function} translate - translate function
      * @param {Boolean} isUSAForm - selected country ISO code is US
