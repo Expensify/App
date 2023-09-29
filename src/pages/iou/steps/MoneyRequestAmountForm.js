6ea45f7501f8ffbd2fd507a1056e2b6cb0d3411f
someone-here <eshgupta1217@gmail.com>
2023-09-29 18:33:46 +0530

    Removed useMemo

--- src/pages/iou/steps/MoneyRequestAmountForm.js
+++ src/pages/iou/steps/MoneyRequestAmountForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useCallback, useRef, useMemo} from 'react';
+import React, {useEffect, useState, useCallback, useRef} from 'react';
 import {ScrollView, View} from 'react-native';
 import PropTypes from 'prop-types';
 import lodashGet from 'lodash/get';
@@ -70,7 +70,7 @@ function MoneyRequestAmountForm({amount, currency, isEditing, forwardedRef, onCu
 
     const textInput = useRef(null);
 
-    const decimals = useMemo(() => CurrencyUtils.getCurrencyDecimals(currency), [currency]);
+    const decimals = CurrencyUtils.getCurrencyDecimals(currency);
     const selectedAmountAsString = amount ? CurrencyUtils.convertToFrontendAmount(amount).toString() : '';
 
     const [currentAmount, setCurrentAmount] = useState(selectedAmountAsString);
