e1f470f26ecb184f34dde67749a1441a19decade
rory <rory@expensify.com>
2023-09-29 15:06:41 +0800

    Renamed obj to target

--- src/libs/deepReplaceKeysAndValues.ts
+++ src/libs/deepReplaceKeysAndValues.ts
@@ -1,29 +1,29 @@
 type ReplaceableValue = Record<string, unknown> | unknown[] | string | number | boolean | undefined | null;
 
 /**
- * @param obj the object to transform
+ * @param target the object or value to transform
  * @param oldVal the value to search for
  * @param newVal the replacement value
  */
-function deepReplaceKeysAndValues<T extends ReplaceableValue>(obj: T, oldVal: string, newVal: string): T {
-    if (!obj) {
-        return obj;
+function deepReplaceKeysAndValues<T extends ReplaceableValue>(target: T, oldVal: string, newVal: string): T {
+    if (!target) {
+        return target;
     }
 
-    if (typeof obj === 'string') {
-        return obj.replace(oldVal, newVal) as T;
+    if (typeof target === 'string') {
+        return target.replace(oldVal, newVal) as T;
     }
 
-    if (typeof obj !== 'object') {
-        return obj;
+    if (typeof target !== 'object') {
+        return target;
     }
 
-    if (Array.isArray(obj)) {
-        return obj.map((item) => deepReplaceKeysAndValues(item as T, oldVal, newVal)) as T;
+    if (Array.isArray(target)) {
+        return target.map((item) => deepReplaceKeysAndValues(item as T, oldVal, newVal)) as T;
     }
 
     const newObj: Record<string, unknown> = {};
-    Object.entries(obj).forEach(([key, val]) => {
+    Object.entries(target).forEach(([key, val]) => {
         const newKey = key.replace(oldVal, newVal);
 
         if (typeof val === 'object') {
