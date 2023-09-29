87b7f76a9e8f8273e34f35fc189b66f47602c5fa
Michał Czernek <michal.czernek@swmansion.com>
2023-09-29 16:21:53 +0200

    Add expo-modules to application.

--- android/app/src/main/java/com/expensify/chat/MainActivity.java
+++ android/app/src/main/java/com/expensify/chat/MainActivity.java
@@ -1,4 +1,5 @@
 package com.expensify.chat;
+import expo.modules.ReactActivityDelegateWrapper;
 
 import android.os.Bundle;
 import android.content.pm.ActivityInfo;
@@ -31,11 +32,11 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+        DefaultNewArchitectureEntryPoint.getFabricEnabled()));
   }
 
   @Override
