37555f7aaad1c534eb34f53c8b9bc44fc8c9ae4a
Akinwale Ariwodola <akinwale@gmail.com>
2023-09-29 15:19:32 +0100

    on Android, do not intercept swipe in BaseOptionSelector

--- src/components/OptionsSelector/BaseOptionsSelector.js
+++ src/components/OptionsSelector/BaseOptionsSelector.js
@@ -364,7 +364,7 @@ class BaseOptionsSelector extends Component {
                 selectTextOnFocus
                 blurOnSubmit={Boolean(this.state.allOptions.length)}
                 spellCheck={false}
-                shouldInterceptSwipe
+                shouldInterceptSwipe={this.props.shouldTextInputInterceptSwipe}
             />
         );
         const optionsList = (
