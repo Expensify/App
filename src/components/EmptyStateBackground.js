0c1e67d14c526fae86d6ab6ece130568fa67e6eb
Aswin S <mr.aswin@gmail.com>
2023-09-30 14:42:49 +0530

    fix: revert  changes to prevent flicker

--- src/components/EmptyStateBackground.js
+++ src/components/EmptyStateBackground.js
@@ -7,22 +7,15 @@ import * as StyleUtils from '../styles/StyleUtils';
 import variables from '../styles/variables';
 
 export default function EmptyStateBackground() {
-    const [visible, setVisible] = React.useState(false);
     const {isSmallScreenWidth, windowWidth} = useWindowDimensions();
-    const onImageLoaded = () => {
-        // Wait for background image size calculation to finish
-        setTimeout(() => setVisible(true), 100);
-    };
-
     // If window width is greater than the max background width, repeat the background image
     const maxBackgroundWidth = variables.sideBarWidth + CONST.EMPTY_STATE_BACKGROUND.ASPECT_RATIO * CONST.EMPTY_STATE_BACKGROUND.WIDE_SCREEN.IMAGE_HEIGHT;
 
     return (
         <Image
             pointerEvents="none"
-            onLoad={onImageLoaded}
             source={EmptyStateBackgroundImage}
-            style={[StyleUtils.getReportWelcomeBackgroundImageStyle(isSmallScreenWidth), {opacity: visible ? 1 : 0}]}
+            style={StyleUtils.getReportWelcomeBackgroundImageStyle(isSmallScreenWidth)}
             resizeMode={windowWidth > maxBackgroundWidth ? 'repeat' : 'cover'}
         />
     );
