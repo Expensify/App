e38d43dec15ff5ed5479db606f508b1f1aaf0293
Michał Czernek <michal.czernek@swmansion.com>
2023-10-02 15:49:43 +0200

    Prettier on metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
+const {mergeConfig} = require('@react-native/metro-config');
 const defaultAssetExts = require('metro-config/src/defaults/defaults').assetExts;
 const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
 const _ = require('underscore');
