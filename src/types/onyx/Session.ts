d96b366abd17577ceb6fca9a0ad9e7803bf757f4
Blazej Kustra <blazej.kustra@swmansion.com>
2023-10-03 11:23:41 +0200

    [TS migration] Migrate 'Session' lib to TypeScript

--- src/types/onyx/Session.ts
+++ src/types/onyx/Session.ts
@@ -1,3 +1,8 @@
+import {ValueOf} from 'type-fest';
+import CONST from '../../CONST';
+
+type AutoAuthState = ValueOf<typeof CONST.AUTO_AUTH_STATE>;
+
 type Session = {
     /** The user's email for the current session */
     email?: string;
@@ -5,13 +10,19 @@ type Session = {
     /** Currently logged in user authToken */
     authToken?: string;
 
+    authTokenType?: string;
+
+    supportAuthToken?: string;
+
     /** Currently logged in user encrypted authToken */
     encryptedAuthToken?: string;
 
     /** Currently logged in user accountID */
     accountID?: number;
 
-    autoAuthState?: string;
+    autoAuthState?: AutoAuthState;
 };
 
 export default Session;
+
+export type {AutoAuthState};
