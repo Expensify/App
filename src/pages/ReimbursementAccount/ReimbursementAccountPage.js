401e05d46d76d21999447e14f59490c0c9ae49df
dukenv0307 <dukenv0307@gmail.com>
2023-10-03 18:20:10 +0700

    fix lint

--- src/pages/ReimbursementAccount/ReimbursementAccountPage.js
+++ src/pages/ReimbursementAccount/ReimbursementAccountPage.js
@@ -160,7 +160,7 @@ class ReimbursementAccountPage extends React.Component {
         // Update the data that is returned from back-end to draft value
         const stepToUpdateToDraft = this.props.reimbursementAccount.stepToUpdate;
         if (stepToUpdateToDraft) {
-            BankAccounts.updateReimbursementAccountDraft(this.getBankAccountFields(this.getFieldsOfCurrentStep(stepToUpdate)));
+            BankAccounts.updateReimbursementAccountDraft(this.getBankAccountFields(this.getFieldsOfCurrentStep(stepToUpdateToDraft)));
         }
 
         const currentStepRouteParam = this.getStepToOpenFromRouteParams();
