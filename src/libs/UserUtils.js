3069353acd37251b2574c47c33d6591aeb2bd0e1
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-10-02 14:33:46 -1000

    Revert "[No QA][TS migration] Migrate 'UserUtils.js' lib to TypeScript"

--- src/libs/UserUtils.js
+++ src/libs/UserUtils.js
@@ -1,14 +1,9 @@
-import {SvgProps} from 'react-native-svg';
-import {ValueOf} from 'type-fest';
+import _ from 'underscore';
+import lodashGet from 'lodash/get';
 import CONST from '../CONST';
 import hashCode from './hashCode';
-import {ConciergeAvatar, FallbackAvatar} from '../components/Icon/Expensicons';
+import * as Expensicons from '../components/Icon/Expensicons';
 import * as defaultAvatars from '../components/Icon/DefaultAvatars';
-import Login from '../types/onyx/Login';
-
-type AvatarRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24;
-
-type LoginListIndicator = ValueOf<typeof CONST.BRICK_ROAD_INDICATOR_STATUS> | '';
 
 /**
  * Searches through given loginList for any contact method / login with an error.
@@ -30,26 +25,36 @@ type LoginListIndicator = ValueOf<typeof CONST.BRICK_ROAD_INDICATOR_STATUS> | ''
  *          }
  *      }
  * }}
+ *
+ * @param {Object} loginList
+ * @param {Object} loginList.errorFields
+ * @returns {Boolean}
  */
-function hasLoginListError(loginList: Login): boolean {
-    const errorFields = loginList?.errorFields ?? {};
-    return Object.values(errorFields).some((field) => Object.keys(field ?? {}).length > 0);
+function hasLoginListError(loginList) {
+    return _.some(loginList, (login) => _.some(lodashGet(login, 'errorFields', {}), (field) => !_.isEmpty(field)));
 }
 
 /**
  * Searches through given loginList for any contact method / login that requires
  * an Info brick road status indicator. Currently this only applies if the user
  * has an unvalidated contact method.
+ *
+ * @param {Object} loginList
+ * @param {String} loginList.validatedDate
+ * @returns {Boolean}
  */
-function hasLoginListInfo(loginList: Login): boolean {
-    return !loginList.validatedDate;
+function hasLoginListInfo(loginList) {
+    return _.some(loginList, (login) => _.isEmpty(login.validatedDate));
 }
 
 /**
  * Gets the appropriate brick road indicator status for a given loginList.
  * Error status is higher priority, so we check for that first.
+ *
+ * @param {Object} loginList
+ * @returns {String}
  */
-function getLoginListBrickRoadIndicator(loginList: Login): LoginListIndicator {
+function getLoginListBrickRoadIndicator(loginList) {
     if (hasLoginListError(loginList)) {
         return CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR;
     }
@@ -61,35 +66,42 @@ function getLoginListBrickRoadIndicator(loginList: Login): LoginListIndicator {
 
 /**
  * Hashes provided string and returns a value between [0, range)
+ * @param {String} text
+ * @param {Number} range
+ * @returns {Number}
  */
-function hashText(text: string, range: number): number {
+function hashText(text, range) {
     return Math.abs(hashCode(text.toLowerCase())) % range;
 }
 
 /**
  * Helper method to return the default avatar associated with the given accountID
- * @param [accountID]
- * @returns
+ * @param {Number} [accountID]
+ * @returns {String}
  */
-function getDefaultAvatar(accountID = -1): React.FC<SvgProps> {
+function getDefaultAvatar(accountID = -1) {
     if (accountID <= 0) {
-        return FallbackAvatar;
+        return Expensicons.FallbackAvatar;
     }
     if (Number(accountID) === CONST.ACCOUNT_ID.CONCIERGE) {
-        return ConciergeAvatar;
+        return Expensicons.ConciergeAvatar;
     }
 
     // There are 24 possible default avatars, so we choose which one this user has based
     // on a simple modulo operation of their login number. Note that Avatar count starts at 1.
-    const accountIDHashBucket = ((accountID % CONST.DEFAULT_AVATAR_COUNT) + 1) as AvatarRange;
+    const accountIDHashBucket = (accountID % CONST.DEFAULT_AVATAR_COUNT) + 1;
 
     return defaultAvatars[`Avatar${accountIDHashBucket}`];
 }
 
 /**
  * Helper method to return default avatar URL associated with login
+ *
+ * @param {Number} [accountID]
+ * @param {Boolean} [isNewDot]
+ * @returns {String}
  */
-function getDefaultAvatarURL(accountID: string | number = '', isNewDot = false): string {
+function getDefaultAvatarURL(accountID = '', isNewDot = false) {
     if (Number(accountID) === CONST.ACCOUNT_ID.CONCIERGE) {
         return CONST.CONCIERGE_ICON_URL;
     }
@@ -103,25 +115,26 @@ function getDefaultAvatarURL(accountID: string | number = '', isNewDot = false):
 
 /**
  * Given a user's avatar path, returns true if user doesn't have an avatar or if URL points to a default avatar
- * @param [avatarURL] - the avatar source from user's personalDetails
+ * @param {String} [avatarURL] - the avatar source from user's personalDetails
+ * @returns {Boolean}
  */
-function isDefaultAvatar(avatarURL?: string): boolean {
-    if (typeof avatarURL === 'string') {
-        if (avatarURL.includes('images/avatars/avatar_') || avatarURL.includes('images/avatars/default-avatar_') || avatarURL.includes('images/avatars/user/default')) {
-            return true;
-        }
+function isDefaultAvatar(avatarURL) {
+    if (
+        _.isString(avatarURL) &&
+        (avatarURL.includes('images/avatars/avatar_') || avatarURL.includes('images/avatars/default-avatar_') || avatarURL.includes('images/avatars/user/default'))
+    ) {
+        return true;
+    }
 
-        // We use a hardcoded "default" Concierge avatar
-        if (avatarURL === CONST.CONCIERGE_ICON_URL_2021 || avatarURL === CONST.CONCIERGE_ICON_URL) {
-            return true;
-        }
+    // We use a hardcoded "default" Concierge avatar
+    if (_.isString(avatarURL) && (avatarURL === CONST.CONCIERGE_ICON_URL_2021 || avatarURL === CONST.CONCIERGE_ICON_URL)) {
+        return true;
     }
 
     if (!avatarURL) {
         // If null URL, we should also use a default avatar
         return true;
     }
-
     return false;
 }
 
@@ -129,10 +142,11 @@ function isDefaultAvatar(avatarURL?: string): boolean {
  * Provided a source URL, if source is a default avatar, return the associated SVG.
  * Otherwise, return the URL pointing to a user-uploaded avatar.
  *
- * @param avatarURL - the avatar source from user's personalDetails
- * @param accountID - the accountID of the user
+ * @param {String} avatarURL - the avatar source from user's personalDetails
+ * @param {Number} accountID - the accountID of the user
+ * @returns {String|Function}
  */
-function getAvatar(avatarURL: string, accountID: number): React.FC<SvgProps> | string {
+function getAvatar(avatarURL, accountID) {
     return isDefaultAvatar(avatarURL) ? getDefaultAvatar(accountID) : avatarURL;
 }
 
@@ -140,20 +154,25 @@ function getAvatar(avatarURL: string, accountID: number): React.FC<SvgProps> | s
  * Provided an avatar URL, if avatar is a default avatar, return NewDot default avatar URL.
  * Otherwise, return the URL pointing to a user-uploaded avatar.
  *
- * @param avatarURL - the avatar source from user's personalDetails
- * @param accountID - the accountID of the user
+ * @param {String} avatarURL - the avatar source from user's personalDetails
+ * @param {Number} accountID - the accountID of the user
+ * @returns {String}
  */
-function getAvatarUrl(avatarURL: string, accountID: number): string {
+function getAvatarUrl(avatarURL, accountID) {
     return isDefaultAvatar(avatarURL) ? getDefaultAvatarURL(accountID, true) : avatarURL;
 }
 
 /**
  * Avatars uploaded by users will have a _128 appended so that the asset server returns a small version.
  * This removes that part of the URL so the full version of the image can load.
+ *
+ * @param {String} [avatarURL]
+ * @param {Number} [accountID]
+ * @returns {String|Function}
  */
-function getFullSizeAvatar(avatarURL: string, accountID: number): React.FC<SvgProps> | string {
+function getFullSizeAvatar(avatarURL, accountID) {
     const source = getAvatar(avatarURL, accountID);
-    if (typeof source !== 'string') {
+    if (!_.isString(source)) {
         return source;
     }
     return source.replace('_128', '');
@@ -162,10 +181,14 @@ function getFullSizeAvatar(avatarURL: string, accountID: number): React.FC<SvgPr
 /**
  * Small sized avatars end with _128.<file-type>. This adds the _128 at the end of the
  * source URL (before the file type) if it doesn't exist there already.
+ *
+ * @param {String} avatarURL
+ * @param {Number} accountID
+ * @returns {String|Function}
  */
-function getSmallSizeAvatar(avatarURL: string, accountID: number): React.FC<SvgProps> | string {
+function getSmallSizeAvatar(avatarURL, accountID) {
     const source = getAvatar(avatarURL, accountID);
-    if (typeof source !== 'string') {
+    if (!_.isString(source)) {
         return source;
     }
 
@@ -184,8 +207,10 @@ function getSmallSizeAvatar(avatarURL: string, accountID: number): React.FC<SvgP
 
 /**
  * Generate a random accountID base on searchValue.
+ * @param {String} searchValue
+ * @returns {Number}
  */
-function generateAccountID(searchValue: string): number {
+function generateAccountID(searchValue) {
     return hashText(searchValue, 2 ** 32);
 }
 
