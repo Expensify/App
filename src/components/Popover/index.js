b7d6470719d1138eac687030cf9d928ed3c35eff
Shubham Agrawal <shubham.agra1206@gmail.com>
2023-10-04 12:49:29 +0530

    fix proper closing of popover context

--- src/components/Popover/index.js
+++ src/components/Popover/index.js
@@ -5,14 +5,16 @@ import CONST from '../../CONST';
 import Modal from '../Modal';
 import withWindowDimensions from '../withWindowDimensions';
 import PopoverWithoutOverlay from '../PopoverWithoutOverlay';
+import {PopoverContext} from '../PopoverProvider';
 
 /*
  * This is a convenience wrapper around the Modal component for a responsive Popover.
  * On small screen widths, it uses BottomDocked modal type, and a Popover type on wide screen widths.
  */
 function Popover(props) {
-    const {isVisible, onClose, isSmallScreenWidth, fullscreen, animationInTiming, onLayout, animationOutTiming, disableAnimation, withoutOverlay, anchorPosition} = props;
+    const {isVisible, onClose, isSmallScreenWidth, fullscreen, animationInTiming, onLayout, animationOutTiming, disableAnimation, withoutOverlay, anchorPosition, anchorRef} = props;
     const withoutOverlayRef = useRef(null);
+    const {close, popover} = React.useContext(PopoverContext);
 
     // Not adding this inside the PopoverProvider
     // because this is an issue on smaller screens as well.
@@ -30,11 +32,19 @@ function Popover(props) {
         };
     }, [onClose, isVisible]);
 
+    const onCloseWithPopoverContext = () => {
+        if (popover) {
+            close(anchorRef);
+        }
+        onClose();
+    };
+
     if (!fullscreen && !isSmallScreenWidth) {
         return createPortal(
             <Modal
                 // eslint-disable-next-line react/jsx-props-no-spreading
                 {...props}
+                onClose={onCloseWithPopoverContext}
                 type={CONST.MODAL.MODAL_TYPE.POPOVER}
                 popoverAnchorPosition={anchorPosition}
                 animationInTiming={disableAnimation ? 1 : animationInTiming}
@@ -61,6 +71,7 @@ function Popover(props) {
         <Modal
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
+            onClose={onCloseWithPopoverContext}
             type={isSmallScreenWidth ? CONST.MODAL.MODAL_TYPE.BOTTOM_DOCKED : CONST.MODAL.MODAL_TYPE.POPOVER}
             popoverAnchorPosition={isSmallScreenWidth ? undefined : anchorPosition}
             fullscreen={isSmallScreenWidth ? true : fullscreen}
