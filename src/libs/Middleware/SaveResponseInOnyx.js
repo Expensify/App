f201b448cb3abe02d5eba2bf7415d74ad41794c3
tienifr <christianwen18@gmail.com>
2023-10-04 18:15:29 +0700

    catch error

--- src/libs/Middleware/SaveResponseInOnyx.js
+++ src/libs/Middleware/SaveResponseInOnyx.js
@@ -14,51 +14,53 @@ const requestsToIgnoreLastUpdateID = ['OpenApp', 'ReconnectApp', 'GetMissingOnyx
  * @returns {Promise}
  */
 function SaveResponseInOnyx(requestResponse, request) {
-    return requestResponse.then((response) => {
-        // Make sure we have response data (i.e. response isn't a promise being passed down to us by a failed retry request and response undefined)
-        if (!response) {
-            return;
-        }
-        const onyxUpdates = response.onyxData;
+    return requestResponse
+        .then((response) => {
+            // Make sure we have response data (i.e. response isn't a promise being passed down to us by a failed retry request and response undefined)
+            if (!response) {
+                return;
+            }
+            const onyxUpdates = response.onyxData;
 
-        // Sometimes we call requests that are successfull but they don't have any response or any success/failure data to set. Let's return early since
-        // we don't need to store anything here.
-        if (!onyxUpdates && !request.successData && !request.failureData) {
-            return Promise.resolve(response);
-        }
-
-        // If there is an OnyxUpdate for using memory only keys, enable them
-        _.find(onyxUpdates, ({key, value}) => {
-            if (key !== ONYXKEYS.IS_USING_MEMORY_ONLY_KEYS || !value) {
-                return false;
+            // Sometimes we call requests that are successfull but they don't have any response or any success/failure data to set. Let's return early since
+            // we don't need to store anything here.
+            if (!onyxUpdates && !request.successData && !request.failureData) {
+                return Promise.resolve(response);
             }
 
-            MemoryOnlyKeys.enable();
-            return true;
-        });
+            // If there is an OnyxUpdate for using memory only keys, enable them
+            _.find(onyxUpdates, ({key, value}) => {
+                if (key !== ONYXKEYS.IS_USING_MEMORY_ONLY_KEYS || !value) {
+                    return false;
+                }
 
-        const responseToApply = {
-            type: CONST.ONYX_UPDATE_TYPES.HTTPS,
-            lastUpdateID: Number(response.lastUpdateID || 0),
-            previousUpdateID: Number(response.previousUpdateID || 0),
-            request,
-            response,
-        };
+                MemoryOnlyKeys.enable();
+                return true;
+            });
 
-        if (_.includes(requestsToIgnoreLastUpdateID, request.command) || !OnyxUpdates.doesClientNeedToBeUpdated(Number(response.previousUpdateID || 0))) {
-            return OnyxUpdates.apply(responseToApply);
-        }
+            const responseToApply = {
+                type: CONST.ONYX_UPDATE_TYPES.HTTPS,
+                lastUpdateID: Number(response.lastUpdateID || 0),
+                previousUpdateID: Number(response.previousUpdateID || 0),
+                request,
+                response,
+            };
 
-        // Save the update IDs to Onyx so they can be used to fetch incremental updates if the client gets out of sync from the server
-        OnyxUpdates.saveUpdateInformation(responseToApply);
+            if (_.includes(requestsToIgnoreLastUpdateID, request.command) || !OnyxUpdates.doesClientNeedToBeUpdated(Number(response.previousUpdateID || 0))) {
+                return OnyxUpdates.apply(responseToApply);
+            }
 
-        // Ensure the queue is paused while the client resolves the gap in onyx updates so that updates are guaranteed to happen in a specific order.
-        return Promise.resolve({
-            ...response,
-            shouldPauseQueue: true,
-        });
-    // # TODO: update with correct logic to log error to the console    
-    }).catch(error => console.log('error', error));
+            // Save the update IDs to Onyx so they can be used to fetch incremental updates if the client gets out of sync from the server
+            OnyxUpdates.saveUpdateInformation(responseToApply);
+
+            // Ensure the queue is paused while the client resolves the gap in onyx updates so that updates are guaranteed to happen in a specific order.
+            return Promise.resolve({
+                ...response,
+                shouldPauseQueue: true,
+            });
+            // # TODO: update with correct logic to log error to the console
+        })
+        .catch(console.error);
 }
 
 export default SaveResponseInOnyx;
