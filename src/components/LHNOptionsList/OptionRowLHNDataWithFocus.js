8a0abb639f6e16f1b07997829d1bca26044859e0
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2023-10-04 13:38:19 +0200

    add OptionRowLHNDataWithFocus wrapper

---
+++ src/components/LHNOptionsList/OptionRowLHNDataWithFocus.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withCurrentReportID, {withCurrentReportIDPropTypes, withCurrentReportIDDefaultProps} from '../withCurrentReportID';
+import OptionRowLHNData from './OptionRowLHNData';
+
+const propTypes = {
+    ...withCurrentReportIDPropTypes,
+    shouldDisableFocusOptions: PropTypes.bool,
+};
+
+const defaultProps = {
+    ...withCurrentReportIDDefaultProps,
+    shouldDisableFocusOptions: false,
+};
+
+function OptionRowLHNDataWithFocus({currentReportID, shouldDisableFocusOptions, ...props}) {
+    const isFocused = !shouldDisableFocusOptions && currentReportID === props.reportID;
+
+    return (
+        <OptionRowLHNData
+            {...props}
+            isFocused={isFocused}
+        />
+    );
+}
+
+OptionRowLHNDataWithFocus.defaultProps = defaultProps;
+OptionRowLHNDataWithFocus.propTypes = propTypes;
+OptionRowLHNDataWithFocus.displayName = 'OptionRowLHNDataWithFocus';
+
+export default withCurrentReportID(OptionRowLHNDataWithFocus);
