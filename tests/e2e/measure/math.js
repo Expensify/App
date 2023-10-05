08ecec25e28d0c0b49b816ac9a1fef2b9cd6658a
Oscar Franco <ospfranco@gmail.com>
2023-10-05 15:39:28 +0200

    Comment filteroutliersviaIQR function

--- tests/e2e/measure/math.js
+++ tests/e2e/measure/math.js
@@ -1,25 +1,25 @@
 const _ = require('underscore');
 
-const filterOutliersViaIQR = (data) => {
-    let q1;
-    let q3;
+// const filterOutliersViaIQR = (data) => {
+//     let q1;
+//     let q3;
 
-    const values = data.slice().sort((a, b) => a - b);
+//     const values = data.slice().sort((a, b) => a - b);
 
-    if ((values.length / 4) % 1 === 0) {
-        q1 = (1 / 2) * (values[values.length / 4] + values[values.length / 4 + 1]);
-        q3 = (1 / 2) * (values[values.length * (3 / 4)] + values[values.length * (3 / 4) + 1]);
-    } else {
-        q1 = values[Math.floor(values.length / 4 + 1)];
-        q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
-    }
+//     if ((values.length / 4) % 1 === 0) {
+//         q1 = (1 / 2) * (values[values.length / 4] + values[values.length / 4 + 1]);
+//         q3 = (1 / 2) * (values[values.length * (3 / 4)] + values[values.length * (3 / 4) + 1]);
+//     } else {
+//         q1 = values[Math.floor(values.length / 4 + 1)];
+//         q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
+//     }
 
-    const iqr = q3 - q1;
-    const maxValue = q3 + iqr * 1.5;
-    const minValue = q1 - iqr * 1.5;
+//     const iqr = q3 - q1;
+//     const maxValue = q3 + iqr * 1.5;
+//     const minValue = q1 - iqr * 1.5;
 
-    return _.filter(values, (x) => x >= minValue && x <= maxValue);
-};
+//     return _.filter(values, (x) => x >= minValue && x <= maxValue);
+// };
 
 const mean = (arr) => _.reduce(arr, (a, b) => a + b, 0) / arr.length;
 
