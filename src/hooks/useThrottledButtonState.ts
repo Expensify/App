68afc352640e7b3c9185bc1c0b97326b1170f318
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-10-05 16:34:41 +0200

    Extract returned value to type

--- src/hooks/useThrottledButtonState.ts
+++ src/hooks/useThrottledButtonState.ts
@@ -1,6 +1,8 @@
 import {useEffect, useState} from 'react';
 
-export default function useThrottledButtonState(): [boolean, () => void] {
+type ThrottledButtonState = [boolean, () => void];
+
+export default function useThrottledButtonState(): ThrottledButtonState {
     const [isButtonActive, setIsButtonActive] = useState(true);
 
     useEffect(() => {
