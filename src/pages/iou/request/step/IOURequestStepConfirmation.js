6589b05fc26a6973a3e05843a429758d9de9ba8a
Tim Golen <tgolen@expensify.com>
2023-10-06 13:18:47 -0600

    Fix logic

--- src/pages/iou/request/step/IOURequestStepConfirmation.js
+++ src/pages/iou/request/step/IOURequestStepConfirmation.js
@@ -340,7 +340,7 @@ function IOURequestStepConfirmation({
                                 // but not all of them (maybe someone skipped out on dinner). Then it's nice to be able to select/deselect people from the group chat bill
                                 // split rather than forcing the user to create a new group, just for that expense. The reportID is empty, when the action was initiated from
                                 // the floating-action-button (since it is something that exists outside the context of a report).
-                                canModifyParticipants={!report.reportID}
+                                canModifyParticipants={!_.isEmpty(report.reportID)}
                                 policyID={report.policyID}
                                 bankAccountRoute={ReportUtils.getBankAccountRoute(report)}
                                 iouMerchant={transaction.merchant}
