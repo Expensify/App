fea00f5f6a64bc1da45efc68f58f541d968b0c2c
Hayata Suenaga <hayata@expensify.com>
2023-10-06 17:29:52 +0900

    feat: convert NewDot URL to OldDot URL

--- src/components/IFrame.js
+++ src/components/IFrame.js
@@ -1,4 +1,6 @@
-import React, {useEffect} from 'react';
+/* eslint-disable es/no-nullish-coalescing-operators */
+import React, {useEffect, useState} from 'react';
+import Navigation from '../libs/Navigation/Navigation';
 
 function getNewDotURL(url) {
     const urlObj = new URL(url);
@@ -17,7 +19,7 @@ function getNewDotURL(url) {
     }
 
     if (pathname === 'expenses') {
-        return `${params.viewMode === 'charts' ? 'insights' : 'expenses'}/${paramString}`;
+        return `${params.viewMode === 'charts' ? 'insights' : 'expenses'}${paramString ? `/?param=${paramString}` : ''}`;
     }
 
     if (pathname === 'admin_policies') {
@@ -44,8 +46,46 @@ function getNewDotURL(url) {
     return pathname;
 }
 
+function getOldDotURL(url) {
+    const urlObj = new URL(url);
+    const pathname = urlObj.pathname;
+    const paths = pathname.slice(1).split('/');
+
+    if (pathname === 'home') {
+        return 'inbox';
+    }
+
+    if (pathname === 'expenses' ?? pathname === 'insights') {
+        return `expenses/${urlObj.search}`;
+    }
+
+    if (pathname === 'individual_workspaces' || pathname === 'group_workspaces') {
+        const param = {section: pathname === 'individual_workspaces' ? 'individual' : 'group'};
+        return `admin_policies?param=${JSON.stringify(param)}`;
+    }
+
+    if (pathname === 'workspace') {
+        const [, workspaceID, section] = paths;
+        const param = {policyID: workspaceID};
+        return `policy/?param${JSON.stringify(param)}#${section}`;
+    }
+
+    if (pathname === 'settings') {
+        const [, section] = paths;
+        const param = {section};
+        return `settings?param=${JSON.stringify(param)}`;
+    }
+
+    return pathname;
+}
+
 export default function ReportScreen() {
+    const [oldDotURL, setOldDotURL] = useState('https://expensify.com.dev');
+
     useEffect(() => {
+        const route = Navigation.getActiveRoute();
+        setOldDotURL(`https://expensify.com.dev/${getOldDotURL(route)}`);
+
         window.addEventListener('message', (event) => {
             const url = event.data;
             const newDotURL = getNewDotURL(url);
@@ -55,7 +95,7 @@ export default function ReportScreen() {
     return (
         <iframe
             style={{flex: 1}}
-            src="https://expensify.com.dev"
+            src={oldDotURL}
             title="OldDot"
         />
     );
