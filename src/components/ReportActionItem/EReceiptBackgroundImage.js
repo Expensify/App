b751a5f55157fa28c6c60fe23dfc415424fb81ea
Georgia Monahan <georgia@expensify.com>
2023-10-06 15:58:18 +0100

    prettier

--- src/components/ReportActionItem/EReceiptBackgroundImage.js
+++ src/components/ReportActionItem/EReceiptBackgroundImage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import EReceiptBGImage from '../../../assets/images/eReceipt-BGImage.svg';
 
-
 const propTypes = {
     /* Background color */
     primaryColor: PropTypes.string.isRequired,
@@ -12,13 +11,10 @@ const propTypes = {
 };
 
 function EReceiptBackgroundImage({primaryColor, secondaryColor}) {
-    return (
-        <EReceiptBGImage style={{backgroundColor: primaryColor, color: secondaryColor}}/>
-    );
+    return <EReceiptBGImage style={{backgroundColor: primaryColor, color: secondaryColor}} />;
 }
 
 EReceiptBackgroundImage.displayName = 'EReceiptBackgroundImage';
 EReceiptBackgroundImage.propTypes = propTypes;
 
 export default EReceiptBackgroundImage;
-
