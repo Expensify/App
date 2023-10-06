d48404f2eff42b383f5338a0194cfd68db7790d4
tienifr <christianwen18@gmail.com>
2023-10-06 13:47:51 +0700

    fix lint

--- src/components/KeyboardShortcutsModal.js
+++ src/components/KeyboardShortcutsModal.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import {View, ScrollView} from 'react-native';
 import {withOnyx} from 'react-native-onyx';
