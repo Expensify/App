ddb507dd5a882cbf9ae90ec3527533b2a6d23b6d
Agata Kosior <agata.kosior57@gmail.com>
2023-10-06 19:30:22 +0200

    feat: add test with scrolling and pressing

--- tests/perf-test/SidebarLinks.perf-test.js
+++ tests/perf-test/SidebarLinks.perf-test.js
@@ -1,15 +1,19 @@
 import {measurePerformance} from 'reassure';
 import Onyx from 'react-native-onyx';
-import {screen} from '@testing-library/react-native';
+import { fireEvent, screen } from '@testing-library/react-native';
 import _ from 'underscore';
 import * as LHNTestUtils from '../utils/LHNTestUtils';
 import CONST from '../../src/CONST';
 import ONYXKEYS from '../../src/ONYXKEYS';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
 import wrapOnyxWithWaitForBatchedUpdates from '../utils/wrapOnyxWithWaitForBatchedUpdates';
+import variables from "../../src/styles/variables";
 
 jest.mock('../../src/libs/Permissions');
+jest.mock('../../src/libs/Navigation/Navigation');
 jest.mock('../../src/components/Icon/Expensicons');
+
+jest.mock('@react-navigation/native')
 
 beforeAll(() =>
     Onyx.init({
@@ -43,8 +47,12 @@ const getMockedReportsMap = (length = 100) => {
     return _.assign({}, ...mockReports);
 };
 
+const mockedResponseMap = getMockedReportsMap(500);
+
 test('should render Sidebar with 500 reports stored', () => {
     const scenario = async () => {
+        // Query for the sidebar
+        await screen.findByTestId('lhn-options-list');
         /**
          * Query for display names of participants [1, 2].
          * This will ensure that the sidebar renders a list of items.
@@ -60,7 +68,52 @@ test('should render Sidebar with 500 reports stored', () => {
                 [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS, CONST.BETAS.POLICY_ROOMS],
                 [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
                 [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-                ...getMockedReportsMap(500),
+                ...mockedResponseMap,
+            }),
+        )
+        .then(() => measurePerformance(<LHNTestUtils.MockedSidebarLinks />, {scenario}));
+});
+
+test('should scroll and click some of the items', () => {
+    const scenario = async () => {
+        const eventData = {
+            nativeEvent: {
+                contentOffset: {
+                    y: variables.optionRowHeight * 5,
+                },
+                contentSize: {
+                    // Dimensions of the scrollable content
+                    height: variables.optionRowHeight * 10,
+                    width: 100,
+                },
+                layoutMeasurement: {
+                    // Dimensions of the device
+                    height: variables.optionRowHeight * 5,
+                    width: 100,
+                },
+            },
+        };
+
+        const lhnOptionsList =  await screen.findByTestId('lhn-options-list');
+        expect(lhnOptionsList).toBeDefined();
+
+        fireEvent.scroll(lhnOptionsList, eventData);
+
+        const button1 =  await screen.findByTestId('1');
+        const button2 =  await screen.findByTestId('2');
+        fireEvent.press(button1);
+        fireEvent.press(button2);
+    };
+
+    return waitForBatchedUpdates()
+        .then(() =>
+            Onyx.multiSet({
+                [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.DEFAULT,
+                [ONYXKEYS.PERSONAL_DETAILS_LIST]: LHNTestUtils.fakePersonalDetails,
+                [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS, CONST.BETAS.POLICY_ROOMS],
+                [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
+                [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
+                ...mockedResponseMap,
             }),
         )
         .then(() => measurePerformance(<LHNTestUtils.MockedSidebarLinks />, {scenario}));
