6396515c0265772d259a38b2d44e5df01dce09b9
Georgia Monahan <georgia@expensify.com>
2023-10-09 12:14:26 +0100

    Disable props spreading in stories

--- src/stories/EReceiptThumbail.stories.js
+++ src/stories/EReceiptThumbail.stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import {View} from 'react-native';
 import EReceiptThumbail from '../components/EReceiptThumbail';
@@ -13,7 +14,6 @@ const story = {
 };
 
 function Template(args) {
-    // eslint-disable-next-line react/jsx-props-no-spreading
     return (
         <>
             <View>
