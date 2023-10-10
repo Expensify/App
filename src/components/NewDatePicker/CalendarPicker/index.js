c3217c2658094f654a747995d982246a1ec8e777
c3024 <chaitanya3024@gmail.com>
2023-10-10 08:53:26 +0530

    fix check of available dates of next month

--- src/components/NewDatePicker/CalendarPicker/index.js
+++ src/components/NewDatePicker/CalendarPicker/index.js
@@ -125,7 +125,7 @@ class CalendarPicker extends React.PureComponent {
         const currentMonthView = this.state.currentDateView.getMonth();
         const currentYearView = this.state.currentDateView.getFullYear();
         const calendarDaysMatrix = generateMonthMatrix(currentYearView, currentMonthView);
-        const hasAvailableDatesNextMonth = moment(this.props.maxDate).endOf('month').startOf('day') > moment(this.state.currentDateView).add(1, 'months');
+        const hasAvailableDatesNextMonth = moment(this.props.maxDate).endOf('month').startOf('day') >= moment(this.state.currentDateView).add(1, 'months');
         const hasAvailableDatesPrevMonth = moment(this.props.minDate).startOf('day') < moment(this.state.currentDateView).subtract(1, 'months').endOf('month');
 
         return (
