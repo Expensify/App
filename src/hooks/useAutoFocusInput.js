dd859292248e7609b3bd82a4f8ac8bb9eccb43ca
tienifr <christianwen18@gmail.com>
2023-10-10 22:21:50 +0700

    fix: 22850 task title is not focused after refresh

---
+++ src/hooks/useAutoFocusInput.js
@@ -0,0 +1,40 @@
+import { useFocusEffect } from '@react-navigation/native';
+import {useState, useEffect, useRef, useCallback} from 'react';
+import CONST from '../CONST';
+
+export default function useAutoFocusInput(){
+    const [isInputInitialized, setIsInputInitialized] = useState(false);
+    const [screenTransitionEnd, setScreenTransitionEnd] = useState(false);
+
+    const inputRef = useRef(null);
+    const focusTimeoutRef = useRef(null);
+
+    useEffect(() => {
+        if (!screenTransitionEnd || !isInputInitialized || !inputRef.current) {
+            return;
+        }
+        inputRef.current.focus();
+    }, [screenTransitionEnd, isInputInitialized]);
+
+    useFocusEffect(
+        useCallback(() => {
+            focusTimeoutRef.current = setTimeout(() => {
+                setScreenTransitionEnd(true);
+            }, CONST.ANIMATED_TRANSITION);
+            return () => {
+                if (!focusTimeoutRef.current) {
+                    return;
+                }
+                clearTimeout(focusTimeoutRef.current);
+            };
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, []),
+    );
+
+    const inputCallbackRef = (ref) => {
+        inputRef.current = ref;
+        setIsInputInitialized(true);
+    };
+
+    return {inputCallbackRef};
+};
