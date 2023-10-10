6e1caf1243caf4b0c798e00b9b38a3168f7a22cf
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-10-10 15:48:00 +0800

    create a single and multi fallback font family file

---
+++ src/styles/fontFamily/singleFontFamily.ts
@@ -0,0 +1,17 @@
+import bold from './bold';
+import FontFamilyStyles from './types';
+
+const fontFamily: FontFamilyStyles = {
+    EXP_NEUE_ITALIC: 'ExpensifyNeue-Italic',
+    EXP_NEUE_BOLD: bold,
+    EXP_NEUE: 'ExpensifyNeue-Regular',
+    EXP_NEW_KANSAS_MEDIUM: 'ExpensifyNewKansas-Medium',
+    EXP_NEW_KANSAS_MEDIUM_ITALIC: 'ExpensifyNewKansas-MediumItalic',
+    SYSTEM: 'System',
+    MONOSPACE: 'ExpensifyMono-Regular',
+    MONOSPACE_ITALIC: 'ExpensifyMono-Regular',
+    MONOSPACE_BOLD: 'ExpensifyMono-Bold',
+    MONOSPACE_BOLD_ITALIC: 'ExpensifyMono-Bold',
+};
+
+export default fontFamily;
