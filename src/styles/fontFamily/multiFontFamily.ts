2ed8b64b48227a5e5e089c86565dd217095b4678
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-10-10 15:48:49 +0800

    create a single and multi fallback bold file

--- src/styles/fontFamily/multiFontFamily.ts
+++ src/styles/fontFamily/multiFontFamily.ts
@@ -1,11 +1,11 @@
-import bold from './bold';
+import {multiBold} from './bold';
 import FontFamilyStyles from './types';
 
 // In windows and ubuntu, we need some extra system fonts for emojis to work properly
 // otherwise few of them will appear as black and white
 const fontFamily: FontFamilyStyles = {
     EXP_NEUE_ITALIC: 'ExpensifyNeue-Italic, Segoe UI Emoji, Noto Color Emoji',
-    EXP_NEUE_BOLD: bold,
+    EXP_NEUE_BOLD: multiBold,
     EXP_NEUE: 'ExpensifyNeue-Regular, Segoe UI Emoji, Noto Color Emoji',
     EXP_NEW_KANSAS_MEDIUM: 'ExpensifyNewKansas-Medium, Segoe UI Emoji, Noto Color Emoji',
     EXP_NEW_KANSAS_MEDIUM_ITALIC: 'ExpensifyNewKansas-MediumItalic, Segoe UI Emoji, Noto Color Emoji',
