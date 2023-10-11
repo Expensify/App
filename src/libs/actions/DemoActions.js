18418968787edba18e87f14ad842433c189a5e2d
Alex Beaman <alexbeaman@expensify.com>
2023-10-11 17:13:45 +0300

    Fix up demo action

--- src/libs/actions/DemoActions.js
+++ src/libs/actions/DemoActions.js
@@ -1,15 +1,51 @@
+import Config from 'react-native-config';
 import Onyx from 'react-native-onyx';
 import _ from 'underscore';
 import lodashGet from 'lodash/get';
-import CONST from '../../CONST';
 import * as API from '../API';
 import * as ReportUtils from '../ReportUtils';
 import Navigation from '../Navigation/Navigation';
 import ROUTES from '../../ROUTES';
 import ONYXKEYS from '../../ONYXKEYS';
 
+let currentUserAccountID;
+let currentUserEmail;
+Onyx.connect({
+    key: ONYXKEYS.SESSION,
+    callback: (val) => {
+        currentUserAccountID = lodashGet(val, 'accountID', 0);
+        currentUserEmail = lodashGet(val, 'email', '');
+    },
+});
+
 function runMoney2020Demo() {
-    // createDemoWorkspaceAndNavigate(CONST.EMAIL.MONEY2020, 'CreateChatReport');
+    // Try to navigate to existing demo chat if it exists in Onyx
+    const money2020AccountID = Number(Config?.EXPENSIFY_ACCOUNT_ID_MONEY2020 ?? 15864555);
+    const chatReportID = ReportUtils.getChatByParticipants([money2020AccountID]);
+    if (chatReportID) {
+        // We must call goBack() to remove the demo route from nav history
+        Navigation.goBack();
+        Navigation.navigate(ROUTES.getReportRoute(chatReportID));
+        return;
+    }
+
+    API.makeRequestWithSideEffects('CreateChatReport', {
+        emailList: `${currentUserEmail},money2020@expensify.com`,
+        activationConference: 'money2020',
+    }).then((response) => {
+        // If there's no response or no reportID in the response, navigate the user home so user doesn't get stuck.
+        if (!response || !response.reportID) {
+            Navigation.goBack();
+            Navigation.navigate(ROUTES.HOME);
+            return;
+        }
+
+        // Get reportID & navigate to it
+        // Note: We must call goBack() to remove the demo route from history
+        const chatReportID = response.reportID;
+        Navigation.goBack();
+        Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(chatReportID));
+    });
 }
 
 /**
