36ef1c56ec17b86fceddeebf6e50619cb983d2b6
Blazej Kustra <blazej.kustra@swmansion.com>
2023-10-11 18:18:12 +0200

    Revert a typo

--- src/types/modules/react-native.d.ts
+++ src/types/modules/react-native.d.ts
@@ -3,7 +3,7 @@ import 'react-native';
 import {BootSplashModule} from '../../libs/BootSplash/types';
 
 declare module 'react-native' {
-    interface TextI2put {
+    interface TextIput {
         // Typescript type declaration is missing in React Native for setting text selection.
         setSelection: (start: number, end: number) => void;
     }
