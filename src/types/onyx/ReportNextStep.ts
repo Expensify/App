3355f18cd4d4cff339f835b905931b9e7dc7b475
Marco Chávez <marco@expensify.com>
2023-10-11 17:18:54 -0600

    create ts types for report next steps Onyx object

---
+++ src/types/onyx/ReportNextStep.ts
@@ -0,0 +1,15 @@
+type NextStepButton = {
+    text: string,
+    tooltip: string,
+    disabled: boolean,
+};
+
+type ReportNextStep = {
+    /** Buttons to display as the next step in the report in group policies */
+    buttons: {
+        approve: NextStepButton,
+        reimburse: NextStepButton,
+    }
+};
+
+export default ReportNextStep;
