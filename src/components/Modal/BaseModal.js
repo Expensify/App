85bb0bf2ff3a6715a4280c59d72301309b9a7ece
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-10-11 10:58:46 +0300

    fix delay of focus input after inserting emoji

--- src/components/Modal/BaseModal.js
+++ src/components/Modal/BaseModal.js
@@ -183,6 +183,7 @@ function BaseModal({
             onModalShow={handleShowModal}
             propagateSwipe={propagateSwipe}
             onModalHide={hideModal}
+            onModalWillShow={() => ComposerFocusManager.resetReadyToFocus()}
             onDismiss={handleDismissModal}
             onSwipeComplete={onClose}
             swipeDirection={swipeDirection}
--- src/libs/focusWithDelay.ts
+++ src/libs/focusWithDelay.ts
@@ -1,4 +1,4 @@
-import {InteractionManager, TextInput} from 'react-native';
+import {TextInput} from 'react-native';
 import ComposerFocusManager from './ComposerFocusManager';
 
 type FocusWithDelay = (shouldDelay?: boolean) => void;
@@ -13,21 +13,19 @@ function focusWithDelay(textInput: TextInput | null): FocusWithDelay {
     return (shouldDelay = false) => {
         // There could be other animations running while we trigger manual focus.
         // This prevents focus from making those animations janky.
-        InteractionManager.runAfterInteractions(() => {
+        if (!textInput) {
+            return;
+        }
+
+        if (!shouldDelay) {
+            textInput.focus();
+            return;
+        }
+        ComposerFocusManager.isReadyToFocus().then(() => {
             if (!textInput) {
                 return;
             }
-
-            if (!shouldDelay) {
-                textInput.focus();
-                return;
-            }
-            ComposerFocusManager.isReadyToFocus().then(() => {
-                if (!textInput) {
-                    return;
-                }
-                textInput.focus();
-            });
+            textInput.focus();
         });
     };
 }
