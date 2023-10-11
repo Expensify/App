39d43ad98446de5e5414b468e43aaee11981ecb0
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-10-11 15:07:56 +0200

    Linter

--- src/hooks/useCopySelectionHelper.ts
+++ src/hooks/useCopySelectionHelper.ts
@@ -29,6 +29,8 @@ export default function useCopySelectionHelper() {
             false,
         );
 
-        return () => { unsubscribeCopyShortcut() };
+        return () => {
+            unsubscribeCopyShortcut();
+        };
     }, []);
 }
