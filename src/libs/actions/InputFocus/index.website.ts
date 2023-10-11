3df292fc09fd60c849fef5e042ae31e5754794d0
Ayaz Alavi <ayaz.alavi@gmail.com>
2023-10-11 20:46:05 +0500

    removed mobile check
    Signed-off-by: Ayaz Alavi <ayaz.alavi@gmail.com>

--- src/libs/actions/InputFocus/index.website.ts
+++ src/libs/actions/InputFocus/index.website.ts
@@ -4,17 +4,11 @@ import * as Browser from '../../Browser';
 import ReportActionComposeFocusManager from '../../ReportActionComposeFocusManager';
 
 function inputFocusChange(focus: boolean) {
-    if (Browser.isMobile()) {
-        return;
-    }
     Onyx.set(ONYXKEYS.INPUT_FOCUSED, focus);
 }
 
 let refSave: HTMLElement | undefined;
 function composerFocusKeepFocusOn(ref: HTMLElement, isFocused: boolean, modal: {willAlertModalBecomeVisible: boolean; isVisible: boolean}, onyxFocused: boolean) {
-    if (Browser.isMobile()) {
-        return;
-    }
     if (isFocused && !onyxFocused) {
         inputFocusChange(true);
         ref.focus();
