aaab62502d46346af3dfcb06a41a0ab78b20fd44
Alex Beaman <alexbeaman@expensify.com>
2023-10-11 17:43:14 +0300

    Address lint issues

--- src/libs/actions/DemoActions.js
+++ src/libs/actions/DemoActions.js
@@ -1,6 +1,5 @@
 import Config from 'react-native-config';
 import Onyx from 'react-native-onyx';
-import _ from 'underscore';
 import lodashGet from 'lodash/get';
 import * as API from '../API';
 import * as ReportUtils from '../ReportUtils';
@@ -8,19 +7,17 @@ import Navigation from '../Navigation/Navigation';
 import ROUTES from '../../ROUTES';
 import ONYXKEYS from '../../ONYXKEYS';
 
-let currentUserAccountID;
 let currentUserEmail;
 Onyx.connect({
     key: ONYXKEYS.SESSION,
     callback: (val) => {
-        currentUserAccountID = lodashGet(val, 'accountID', 0);
         currentUserEmail = lodashGet(val, 'email', '');
     },
 });
 
 function runMoney2020Demo() {
     // Try to navigate to existing demo chat if it exists in Onyx
-    const money2020AccountID = Number(Config?.EXPENSIFY_ACCOUNT_ID_MONEY2020 ?? 15864555);
+    const money2020AccountID = Number(Config ? Config.EXPENSIFY_ACCOUNT_ID_MONEY2020 : 15864555);
     const existingChatReport = ReportUtils.getChatByParticipants([money2020AccountID]);
     if (existingChatReport) {
         // We must call goBack() to remove the demo route from nav history
@@ -29,6 +26,8 @@ function runMoney2020Demo() {
         return;
     }
 
+    // We use makeRequestWithSideEffects here because we need to get the chat report ID to navigate to it after it's created
+    // eslint-disable-next-line rulesdir/no-api-side-effects-method
     API.makeRequestWithSideEffects('CreateChatReport', {
         emailList: `${currentUserEmail},money2020@expensify.com`,
         activationConference: 'money2020',
