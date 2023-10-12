5595e791005fe04b388c701892903b04e1a82077
Marco Chávez <marco@expensify.com>
2023-10-11 19:45:02 -0600

    fix style for next step type

--- src/types/onyx/ReportNextStep.ts
+++ src/types/onyx/ReportNextStep.ts
@@ -1,15 +1,15 @@
 type NextStepButton = {
-    text: string,
-    tooltip: string,
-    disabled: boolean,
+    text: string;
+    tooltip: string;
+    disabled: boolean;
 };
 
 type ReportNextStep = {
     /** Buttons to display as the next step in the report in group policies */
     buttons: {
-        approve: NextStepButton,
-        reimburse: NextStepButton,
-    }
+        approve: NextStepButton;
+        reimburse: NextStepButton;
+    };
 };
 
 export default ReportNextStep;
