3e593c395455a0c2d8d287f302325b7e92bfc52f
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-10-12 14:20:05 +0200

    Rename generic

--- src/components/withNavigationFocus.tsx
+++ src/components/withNavigationFocus.tsx
@@ -11,13 +11,13 @@ type WithNavigationFocusProps = {
     isFocused: boolean;
 };
 
-export default function withNavigationFocus<T extends WithNavigationFocusProps, R>(WrappedComponent: ComponentType<T & RefAttributes<R>>) {
-    function WithNavigationFocus(props: Omit<T, keyof WithNavigationFocusProps>, ref: ForwardedRef<R>) {
+export default function withNavigationFocus<TProps extends WithNavigationFocusProps, TRef>(WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>) {
+    function WithNavigationFocus(props: Omit<TProps, keyof WithNavigationFocusProps>, ref: ForwardedRef<TRef>) {
         const isFocused = useIsFocused();
         return (
             <WrappedComponent
                 // eslint-disable-next-line react/jsx-props-no-spreading
-                {...(props as T)}
+                {...(props as TProps)}
                 ref={ref}
                 isFocused={isFocused}
             />
