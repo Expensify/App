e02245ceecadc1ef42b0711f6849596fa62786ac
Rodrigo Lino da Costa <rodrigo@expensify.com>
2023-10-12 17:42:48 -0300

    added manifest to web to prompt app install in chrome

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -83,6 +83,7 @@ const webpackConfig = ({envFile = '.env', platform = 'web'}) => ({
                 {from: 'web/favicon-unread.png'},
                 {from: 'web/og-preview-image.png'},
                 {from: 'web/apple-touch-icon.png'},
+                {from: 'web/manifest.json'},
                 {from: 'assets/css', to: 'css'},
                 {from: 'assets/fonts/web', to: 'fonts'},
                 {from: 'node_modules/react-pdf/dist/esm/Page/AnnotationLayer.css', to: 'css/AnnotationLayer.css'},
