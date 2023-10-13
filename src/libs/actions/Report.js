5b9c5649f8d293b68abcb617121c30a43d7102a4
Andrew Li <andrewli@expensify.com>
2023-10-12 22:31:34 -0400

    Update comment

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1040,7 +1040,8 @@ function deleteReportComment(reportID, reportAction) {
     };
 
     // If we are deleting the last visible message, let's find the previous visible one (or set an empty one if there are none) and update the lastMessageText in the LHN.
-    // Similarly, if we are deleting the last read comment we will want to update the lastVisibleActionCreated to use the previous visible message.
+    // Similarly, if we are deleting the last read comment we will want to update the lastVisibleActionCreated to use the previous visible message. If there is no
+    // previous comment, we don't reset lastVisibleActionCreated since we want the report to keep its position in the LHN.
     let optimisticReport = {
         lastMessageTranslationKey: '',
         lastMessageText: '',
