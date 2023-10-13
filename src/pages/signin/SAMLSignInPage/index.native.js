8991e7ef264595e150ed631c8e733291ec213be1
Nikki Wines <nikkiwines@expensify.com>
2023-10-13 20:18:43 +0900

    log in with shortlivedtoken when one is returned

--- src/pages/signin/SAMLSignInPage/index.native.js
+++ src/pages/signin/SAMLSignInPage/index.native.js
@@ -1,10 +1,11 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useRef} from 'react';
 import {withOnyx} from 'react-native-onyx';
 import PropTypes from 'prop-types';
 import ONYXKEYS from '../../../ONYXKEYS';
 import CONFIG from '../../../CONFIG';
 import WebView from 'react-native-webview';
 import FullScreenLoadingIndicator from '../../../components/FullscreenLoadingIndicator';
+import * as Session from '../../../libs/actions/Session';
 
 const propTypes = {
     /** The credentials of the logged in person */
@@ -23,6 +24,24 @@ const renderLoading = () => <FullScreenLoadingIndicator />;
 function SAMLSignInPage({credentials}) {
     const webViewRef = useRef();
     const samlLoginURL = `${CONFIG.EXPENSIFY.SAML_URL}?email=${credentials.login}&referer=${CONFIG.EXPENSIFY.EXPENSIFY_CASH_REFERER}`;
+
+    /**
+    * Handles in-app navigation once we get a response back from Expensify
+    *
+    * @param {String} params.type
+    * @param {String} params.url
+    */
+    const handleNavigationStateChange = useCallback(
+        ({type, url}) => {
+            const searchParams = new URLSearchParams(new URL(url).search);
+            if (searchParams.has('shortLivedAuthToken')) {
+                const shortLivedAuthToken = searchParams.get('shortLivedAuthToken');
+                Session.signInWithShortLivedAuthToken(credentials.login, shortLivedAuthToken);
+                return;
+            }
+        },
+        [webViewRef],
+    );
     return (
         <WebView
             ref={webViewRef}
@@ -31,7 +50,7 @@ function SAMLSignInPage({credentials}) {
             incognito // 'incognito' prop required for Android, issue here https://github.com/react-native-webview/react-native-webview/issues/1352
             startInLoadingState
             renderLoading={renderLoading}
-            onNavigationStateChange={(ref) => {console.log("meep meep navigation changed: " + JSON.stringify(ref))}}
+            onNavigationStateChange={handleNavigationStateChange}
         />
     );
 }
