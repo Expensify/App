83fa5fa7032084c06aa12cf0359db8c446276584
5war00p <maddisaiswaroop@gmail.com>
2023-10-14 16:49:25 +0530

    fix: changed bool condition with util function

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -340,12 +340,15 @@ function ReportScreen({
     }, [route, report, errors, fetchReportIfNeeded, prevReport.reportID, prevUserLeavingStatus, userLeavingStatus, prevReport.statusNum, prevReport.parentReportID]);
 
     useEffect(() => {
+        if (!ReportUtils.isValidReportIDFromPath(reportID)) {
+            return;
+        }
         // Ensures subscription event succeeds when the report/workspace room is created optimistically.
         // Check if the optimistic `OpenReport` or `AddWorkspaceRoom` has succeeded by confirming
         // any `pendingFields.createChat` or `pendingFields.addWorkspaceRoom` fields are set to null.
         // Existing reports created will have empty fields for `pendingFields`.
         const didCreateReportSuccessfully = !report.pendingFields || (!report.pendingFields.addWorkspaceRoom && !report.pendingFields.createChat);
-        if (!!reportID && !didSubscribeToReportLeavingEvents.current && didCreateReportSuccessfully) {
+        if (!didSubscribeToReportLeavingEvents.current && didCreateReportSuccessfully) {
             Report.subscribeToReportLeavingEvents(reportID);
             didSubscribeToReportLeavingEvents.current = true;
         }
