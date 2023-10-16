bc4f9c0dd24402a342895d3792550a425a583ca1
Huzaifa Rasheed <huzaifarasheed900@gmail.com>
2023-10-16 19:13:56 +0500

    Simplify branch case for isCurrentTargetInsideContainer

--- src/components/AddressSearch/isCurrentTargetInsideContainer.js
+++ src/components/AddressSearch/isCurrentTargetInsideContainer.js
@@ -2,10 +2,7 @@ function isCurrentTargetInsideContainer(event, containerRef) {
     // The related target check is required here
     // because without it when we select an option, the onBlur will still trigger setting displayListViewBorder to false
     // it will make the auto complete component re-render before onPress is called making selecting an option not working.
-    if (containerRef.current && event.target && containerRef.current.contains(event.relatedTarget)) {
-        return true;
-    }
-    return false;
+    return containerRef.current && event.target && containerRef.current.contains(event.relatedTarget);
 }
 
 export default isCurrentTargetInsideContainer;
