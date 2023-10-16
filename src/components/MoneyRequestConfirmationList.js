71064ed378342bd66f83f40b4e23fbd7acd86660
Youssef Lourayad <youssef@expensify.com>
2023-10-16 15:57:15 +0800

    Update src/components/MoneyRequestConfirmationList.js
    Co-authored-by: Situ Chandra Shil <108292595+situchan@users.noreply.github.com>

--- src/components/MoneyRequestConfirmationList.js
+++ src/components/MoneyRequestConfirmationList.js
@@ -558,7 +558,6 @@ function MoneyRequestConfirmationList(props) {
             footerContent={footerContent}
             listStyles={props.listStyles}
             shouldAllowScrollingChildren
-            bounces={false}
         >
             {props.isDistanceRequest && (
                 <View style={styles.confirmationListMapItem}>
