b3977ca8ac895af6351850bc7e4da40172982db4
Dylan <dylandylan9112@gmail.com>
2023-10-16 17:15:46 +0700

    fix lint

--- src/pages/iou/steps/MoneyRequestConfirmPage.js
+++ src/pages/iou/steps/MoneyRequestConfirmPage.js
@@ -1,5 +1,5 @@
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
-import {ScrollView, View} from 'react-native';
+import {View} from 'react-native';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
