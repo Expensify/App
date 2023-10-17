09089048a261f382e90073af8e61090f4d55c02a
OlimpiaZurek <olimpia.zurek@gmail.com>
2023-10-17 16:40:10 +0200

    feat: add perf tests to ReportActionsList

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -606,6 +606,7 @@ function ReportActionItem(props) {
             preventDefaultContextMenu={!props.draftMessage && !hasErrors}
             withoutFocusOnSecondaryInteraction
             accessibilityLabel={props.translate('accessibilityHints.chatMessage')}
+            testID={props.action.reportActionID}
         >
             <Hoverable
                 shouldHandleScroll
--- src/pages/home/report/ReportActionsList.js
+++ src/pages/home/report/ReportActionsList.js
@@ -379,6 +379,7 @@ function ReportActionsList({
                     onLayout={onLayoutInner}
                     onScroll={trackVerticalScrolling}
                     extraData={extraData}
+                    testID="report-actions-list"
                 />
             </Animated.View>
         </>
