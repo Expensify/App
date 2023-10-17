d94ed7396cf336ef4940ddccb83ff989fcb5deda
Jasper Huang <jasper@expensify.com>
2023-10-17 16:39:09 -0700

    Update src/libs/PolicyUtils.js
    Co-authored-by: Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>

--- src/libs/PolicyUtils.js
+++ src/libs/PolicyUtils.js
@@ -161,7 +161,7 @@ const isPolicyAdmin = (policy) => lodashGet(policy, 'role') === CONST.POLICY.ROL
  * @param {Object} policies
  * @returns {Boolean}
  */
-const isPolicyMember = (policyID, policies) => _.reduce(policies, (isMember, policy) => isMember || policy.id === policyID, false);
+const isPolicyMember = (policyID, policies) => _.some(policies, (policy) => policy.id === policyID);
 
 /**
  * @param {Object} policyMembers
