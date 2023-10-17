bd99ba5e57f2359bc1d3f710344c0eddbdc4d268
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-10-17 13:00:40 +0200

    Add eslint rule

--- src/components/CustomDevMenu/index.native.tsx
+++ src/components/CustomDevMenu/index.native.tsx
@@ -3,6 +3,7 @@ import DevMenu from 'react-native-dev-menu';
 import toggleTestToolsModal from '../../libs/actions/TestTool';
 import CustomDevMenuElement from './types';
 
+// eslint-disable-next-line react/function-component-definition
 const CustomDevMenu: CustomDevMenuElement = () => {
     useEffect(() => {
         DevMenu.addItem('Open Test Preferences', toggleTestToolsModal);
