2c5815fe3a3b493d52aca6430f6dd715199d527f
tienifr <christianwen18@gmail.com>
2023-10-18 14:20:12 +0700

    fix warning in PreRenderer

--- src/components/HTMLEngineProvider/HTMLRenderers/PreRenderer/index.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/PreRenderer/index.js
@@ -5,6 +5,9 @@ import ControlSelection from '../../../../libs/ControlSelection';
 import * as DeviceCapabilities from '../../../../libs/DeviceCapabilities';
 import htmlRendererPropTypes from '../htmlRendererPropTypes';
 import BasePreRenderer from './BasePreRenderer';
+import hasPassiveEventListenerSupport from '../../../../libs/DeviceCapabilities/hasPassiveEventListenerSupport';
+
+const supportsPassive = hasPassiveEventListenerSupport();
 
 const isScrollingVertically = (event) =>
     // Mark as vertical scrolling only when absolute value of deltaY is more than the double of absolute
@@ -42,7 +45,7 @@ function PreRenderer(props) {
         if (!eventListenerRefValue) {
             return;
         }
-        eventListenerRefValue.getScrollableNode().addEventListener('wheel', scrollNode);
+        eventListenerRefValue.getScrollableNode().addEventListener('wheel', scrollNode, supportsPassive ? {passive: true} : false);
 
         return () => {
             if (!eventListenerRefValue.getScrollableNode()) {
