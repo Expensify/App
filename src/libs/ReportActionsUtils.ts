a4363a1d9f10be7456801b3bd158d25454b60a07
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-10-19 20:48:29 +0200

    fix: removed uncecessary code

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -429,7 +429,7 @@ function filterOutDeprecatedReportActions(reportActions: ReportActions | null): 
  * to ensure they will always be displayed in the same order (in case multiple actions have the same timestamp).
  * This is all handled with getSortedReportActions() which is used by several other methods to keep the code DRY.
  */
-function getSortedReportActionsForDisplay(reportActions: ReportActions | null | undefined): ReportAction[] {
+function getSortedReportActionsForDisplay(reportActions: ReportActions | null): ReportAction[] {
     const filteredReportActions = Object.entries(reportActions ?? {})
         .filter(([key, reportAction]) => shouldReportActionBeVisible(reportAction, key))
         .map((entry) => entry[1]);
