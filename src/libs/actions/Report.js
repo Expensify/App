4a4ce7cfaad8d47a6b2975c611a1a6fa74cedf1f
Jasper Huang <jasper@expensify.com>
2023-10-19 14:01:44 -0700

    Update src/libs/actions/Report.js
    Co-authored-by: Francois Laithier <francois@expensify.com>

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -1992,7 +1992,7 @@ function leaveRoom(reportID, isWorkspaceMemberLeavingWorkspaceRoom = false) {
     if (isWorkspaceMemberLeavingWorkspaceRoom) {
         const participantAccountIDs = PersonalDetailsUtils.getAccountIDsByLogins([CONST.EMAIL.CONCIERGE]);
         const chat = ReportUtils.getChatByParticipants(participantAccountIDs);
-        Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(chat.reportID))
+        Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(chat.reportID));
     }
 }
 
