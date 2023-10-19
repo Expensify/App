d6e71ead00cd9638a8956df10aec1f9b883fe146
Sam Hariri <137707942+samh-nl@users.noreply.github.com>
2023-10-19 18:36:51 +0200

    fix: make not required

--- src/pages/EnablePayments/walletTermsPropTypes.js
+++ src/pages/EnablePayments/walletTermsPropTypes.js
@@ -8,7 +8,7 @@ export default PropTypes.shape({
     errors: PropTypes.objectOf(PropTypes.string),
 
     /** The source that triggered the KYC wall */
-    source: PropTypes.oneOf(_.values(CONST.KYC_WALL_SOURCE)).isRequired,
+    source: PropTypes.oneOf(_.values(CONST.KYC_WALL_SOURCE)),
 
     /** When the user accepts the Wallet's terms in order to pay an IOU, this is the ID of the chatReport the IOU is linked to */
     chatReportID: PropTypes.string,
