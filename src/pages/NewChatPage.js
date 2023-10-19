42afc185efb46af8152a5def93f43b4073aaab3d
Jack Nam <jack@expensify.com>
2023-10-19 13:34:32 -0700

    Replace new chat page with this method

--- src/pages/NewChatPage.js
+++ src/pages/NewChatPage.js
@@ -72,36 +72,7 @@ function NewChatPage({betas, isGroupChat, personalDetails, reports, translate, i
         const sectionsList = [];
         let indexOffset = 0;
 
-        // We show the selected participants at the top of the list when there is no search term
-        // However, if there is a search term we remove the selected participants from the top of the list unless they are part of the search results
-        // This clears up space on mobile views, where if you create a group with 4+ people you can't see the selected participants and the search results at the same time
-        if (searchTerm === '') {
-            sectionsList.push({
-                title: undefined,
-                data: selectedOptions,
-                shouldShow: !_.isEmpty(selectedOptions),
-                indexOffset,
-            });
-            indexOffset += selectedOptions.length;
-        } else {
-            // If you select a new user you don't have a contact for, they won't get returned as part of a recent report or personal details
-            // This will add them to the list of options, deduping them if they already exist in the other lists
-            const selectedParticipantsWithoutDetails = _.filter(selectedOptions, (participant) => {
-                const accountID = lodashGet(participant, 'accountID', null);
-                const isPartOfSearchTerm = participant.searchText.toLowerCase().includes(searchTerm.trim().toLowerCase());
-                const isReportInRecentReports = _.some(filteredRecentReports, (report) => report.accountID === accountID);
-                const isReportInPersonalDetails = _.some(filteredPersonalDetails, (personalDetail) => personalDetail.accountID === accountID);
-                return isPartOfSearchTerm && !isReportInRecentReports && !isReportInPersonalDetails;
-            });
-
-            sectionsList.push({
-                title: undefined,
-                data: selectedParticipantsWithoutDetails,
-                shouldShow: !_.isEmpty(selectedParticipantsWithoutDetails),
-                indexOffset,
-            });
-            indexOffset += selectedParticipantsWithoutDetails.length;
-        }
+        indexOffset = OptionsListUtils.formatSectionsFromSearchTerm(searchTerm, sectionsList, selectedOptions, filteredRecentReports, filteredPersonalDetails, {}, false, indexOffset);
 
         if (maxParticipantsReached) {
             return sectionsList;
