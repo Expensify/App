bd2274c63dd1c23c8bb509949a44ab6e72d7e75f
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-10-19 16:20:14 +0200

    WIP

---
+++ src/components/FullscreenLoadingIndicator.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import styles from '../styles/styles';
+import themeColors from '../styles/themes/default';
+
+type FullScreenLoadingIndicatorProps = {
+    style: Record<string, unknown> | Array<Record<string, unknown>> | (() => void);
+};
+
+function FullScreenLoadingIndicator({style = []}: FullScreenLoadingIndicatorProps) {
+    const additionalStyles = Array.isArray(style) ? style : [style];
+    return (
+        <View style={[StyleSheet.absoluteFillObject, styles.fullScreenLoading, ...additionalStyles]}>
+            <ActivityIndicator
+                color={themeColors.spinner}
+                size="large"
+            />
+        </View>
+    );
+}
+
+export default FullScreenLoadingIndicator;
