56172e9b4987d6d5328fe000b807f29f7236dcaa
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-10-20 14:57:38 +0200

    Migrate to TS

---
+++ src/components/SelectCircle.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View} from 'react-native';
+import globalStyles from '../styles/styles';
+import Icon from './Icon';
+import * as Expensicons from './Icon/Expensicons';
+import themeColors from '../styles/themes/default';
+
+type SelectCircleProps = {
+    /** Should we show the checkmark inside the circle */
+    isChecked: boolean;
+
+    /** Additional styles to pass to SelectCircle */
+    styles: Array<Record<string, unknown>>;
+};
+
+function SelectCircle({isChecked, styles}: SelectCircleProps) {
+    return (
+        <View style={[globalStyles.selectCircle, globalStyles.alignSelfCenter, ...styles]}>
+            {isChecked && (
+                <Icon
+                    src={Expensicons.Checkmark}
+                    fill={themeColors.iconSuccessFill}
+                />
+            )}
+        </View>
+    );
+}
+
+export default SelectCircle;
