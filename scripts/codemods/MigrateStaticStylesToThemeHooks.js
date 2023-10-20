21bf4809d46adc847a53c2e3d832c1dc51ad79a3
rory <rory@expensify.com>
2023-10-20 16:30:25 -0700

    Fix stripBlankLines

--- scripts/codemods/MigrateStaticStylesToThemeHooks.js
+++ scripts/codemods/MigrateStaticStylesToThemeHooks.js
@@ -233,101 +233,11 @@ async function migrateStaticStylesForDirectory(directoryPath) {
  *
  * @returns {Promise<void>}
  */
-// async function stripBlankLinesFromDiff() {
-//     try {
-//         const {stdout} = await exec("git diff --unified=0 -- ':!scripts/codemods/MigrateStaticStylesToThemeHooks.js'");
-//
-//         console.log('RORY_DEBUG diff', stdout);
-//
-//         // Process the diff output to remove blank lines from added content
-//         let currentFile = '';
-//         let lineNumber = 1; // the line number from the source file, not the diff
-//         const linesToDelete = new Map();
-//         for (const line of stdout.split('\n')) {
-//             // Get the filename from the diff line
-//             const currentFileMatch = line.match(/b\/(.+)/) || [];
-//             if (currentFileMatch && currentFileMatch.length) {
-//                 currentFile = currentFileMatch[1];
-//                 console.log('RORY_DEBUG setting currentFile', currentFile);
-//             }
-//
-//             const [, hunkStartNumber] = line.match(/@@ -\d+,\d+ \+(\d+),?/) || [];
-//
-//             if (hunkStartNumber) {
-//                 lineNumber = hunkStartNumber;
-//                 console.log('RORY_DEBUG setting lineNumber to', lineNumber);
-//             }
-//
-//             if (line.replace('+', '').trim() === '') {
-//                 console.log(`Adding currentFile ${currentFile} to linesToDelete map`, lineNumber);
-//                 const prevLinesToDelete = linesToDelete.get(currentFile) || new Set();
-//                 prevLinesToDelete.add(lineNumber);
-//                 linesToDelete.set(currentFile, prevLinesToDelete);
-//             }
-//
-//             if (line.startsWith('+')) {
-//                 lineNumber++;
-//                 console.log('RORY_DEBUG setting lineNumber to', lineNumber);
-//             }
-//         }
-//
-//         for (const [fileToModify] of linesToDelete) {
-//             // Read the content of the file
-//             console.log('RORY_DEBUG reading fileToModify', fileToModify);
-//             const fileContent = await fs.readFile(fileToModify, {encoding: 'utf8'});
-//             let modifiedContent = '';
-//             const lines = fileContent.split('\n');
-//             for (let lineNum = 1; lineNum < lines.length + 1; lineNum++) {
-//                 if (!linesToDelete.get(fileToModify).has(lineNum)) {
-//                     modifiedContent += lines[lineNum - 1];
-//                     modifiedContent += '\n';
-//                 }
-//             }
-//
-//             // Write the modified content back to the file
-//             await fs.writeFile(fileToModify, modifiedContent, 'utf8');
-//         }
-//
-//         console.log('Blank lines removed from added content.');
-//     } catch (error) {
-//         console.error(`Error running 'git diff' or processing files: ${error}`);
-//     }
-// }
-
-/**
- * If the diff includes any lines that are just whitespace, undo them.
- * Sometimes blank lines are added in weird places by babel's generator due to the retainLines flag,
- * which is necessary to keep the formatting from the original source as much as possible.
- *
- * @returns {Promise<void>}
- */
 async function stripBlankLinesFromDiff() {
     try {
-        const {stdout} = await exec("git diff --unified=0 -- ':!scripts/codemods/MigrateStaticStylesToThemeHooks.js'");
-        console.log('RORY_DEBUG original diff', stdout);
-        const modifiedDiff = [];
-        const lines = stdout.split('\n');
-        for (const line of lines) {
-            // A lonely plus in the diff means we added a blank line
-            if (line && line.replace('+', '').trim() === '') {
-                // Remove the previous line, which will be a hunk marker line, and don't include the lonely plus
-                modifiedDiff.pop();
-            } else {
-                modifiedDiff.push(line);
-            }
-        }
-        // for (let i = 0; i < lines.length; i++) {
-        //     const line = lines[i];
-        //     if (line && line.replace('+', '').trim() === '') {
-        //         // Remove the previous line, which will be a hunk marker line
-        //         modifiedDiff.pop();
-        //     } else {
-        //         modifiedDiff[i] = `${line}`;
-        //     }
-        // }
-        console.log('RORY_DEBUG have modifiedDiff', modifiedDiff.join('\n'));
+        const {stdout: diff} = await exec("git diff --ignore-blank-lines -- ':!scripts/codemods/MigrateStaticStylesToThemeHooks.js'");
         await exec('git restore src/components/Button/index.js');
-        await exec(`echo "${modifiedDiff.join('\n')}" | git apply --recount -`);
+        await exec(`echo "${diff}" | git apply -`);
     } catch (error) {
         console.error('Error stripping blank lines from diff', error);
     }
