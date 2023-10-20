05ef1b7c6900a3dcadacfae1deb2038e55544c51
rory <rory@expensify.com>
2023-10-20 16:22:26 -0700

    Save draft state

--- scripts/codemods/MigrateStaticStylesToThemeHooks.js
+++ scripts/codemods/MigrateStaticStylesToThemeHooks.js
@@ -21,15 +21,13 @@
 /* eslint-disable no-console */
 /* eslint-disable no-param-reassign */
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const {promisify} = require('util');
 const exec = promisify(require('child_process').exec);
 const parser = require('@babel/parser');
 const traverse = require('@babel/traverse');
 const generate = require('@babel/generator');
-
-const fsPromises = fs.promises;
 
 function isFunctionComponent(ast) {
     let hasReactImport = false;
@@ -80,7 +78,7 @@ async function writeASTToFile(filePath, fileContents, ast) {
     const modifiedCode = generate.default(ast, {compact: false, retainLines: true, retainFunctionParens: true}, fileContents).code;
 
     // Replace the contents of the file with the new code
-    await fs.writeFile(filePath, modifiedCode, {encoding: 'utf8'}, () => {});
+    await fs.writeFile(filePath, modifiedCode, {encoding: 'utf8'});
 }
 
 async function migrateStylesForClassComponent(filePath, fileContents, ast) {
@@ -198,7 +196,7 @@ async function migrateStylesForFunctionComponent(filePath, fileContents, ast) {
 }
 
 async function migrateStaticStylesForFile(filePath) {
-    const fileContents = await fsPromises.readFile(filePath, 'utf8');
+    const fileContents = await fs.readFile(filePath, 'utf8');
     const ast = parser.parse(fileContents, {
         sourceType: 'module',
         plugins: ['jsx', 'typescript'], // Enable JSX and TSX parsing
@@ -215,16 +213,123 @@ async function migrateStaticStylesForFile(filePath) {
 }
 
 async function migrateStaticStylesForDirectory(directoryPath) {
-    const files = await fsPromises.readdir(directoryPath);
+    const files = await fs.readdir(directoryPath);
     for (const file of files) {
         const filePath = path.join(directoryPath, file);
-        const stat = await fsPromises.stat(filePath);
+        const stat = await fs.stat(filePath);
 
         if (stat.isDirectory()) {
             await migrateStaticStylesForDirectory(filePath);
         } else if (stat.isFile() && (file.endsWith('.js') || file.endsWith('.jsx') || file.endsWith('.tsx'))) {
             await migrateStaticStylesForFile(filePath);
         }
+    }
+}
+
+/**
+ * If the diff includes any lines that are just whitespace, undo them.
+ * Sometimes blank lines are added in weird places by babel's generator due to the retainLines flag,
+ * which is necessary to keep the formatting from the original source as much as possible.
+ *
+ * @returns {Promise<void>}
+ */
+// async function stripBlankLinesFromDiff() {
+//     try {
+//         const {stdout} = await exec("git diff --unified=0 -- ':!scripts/codemods/MigrateStaticStylesToThemeHooks.js'");
+//
+//         console.log('RORY_DEBUG diff', stdout);
+//
+//         // Process the diff output to remove blank lines from added content
+//         let currentFile = '';
+//         let lineNumber = 1; // the line number from the source file, not the diff
+//         const linesToDelete = new Map();
+//         for (const line of stdout.split('\n')) {
+//             // Get the filename from the diff line
+//             const currentFileMatch = line.match(/b\/(.+)/) || [];
+//             if (currentFileMatch && currentFileMatch.length) {
+//                 currentFile = currentFileMatch[1];
+//                 console.log('RORY_DEBUG setting currentFile', currentFile);
+//             }
+//
+//             const [, hunkStartNumber] = line.match(/@@ -\d+,\d+ \+(\d+),?/) || [];
+//
+//             if (hunkStartNumber) {
+//                 lineNumber = hunkStartNumber;
+//                 console.log('RORY_DEBUG setting lineNumber to', lineNumber);
+//             }
+//
+//             if (line.replace('+', '').trim() === '') {
+//                 console.log(`Adding currentFile ${currentFile} to linesToDelete map`, lineNumber);
+//                 const prevLinesToDelete = linesToDelete.get(currentFile) || new Set();
+//                 prevLinesToDelete.add(lineNumber);
+//                 linesToDelete.set(currentFile, prevLinesToDelete);
+//             }
+//
+//             if (line.startsWith('+')) {
+//                 lineNumber++;
+//                 console.log('RORY_DEBUG setting lineNumber to', lineNumber);
+//             }
+//         }
+//
+//         for (const [fileToModify] of linesToDelete) {
+//             // Read the content of the file
+//             console.log('RORY_DEBUG reading fileToModify', fileToModify);
+//             const fileContent = await fs.readFile(fileToModify, {encoding: 'utf8'});
+//             let modifiedContent = '';
+//             const lines = fileContent.split('\n');
+//             for (let lineNum = 1; lineNum < lines.length + 1; lineNum++) {
+//                 if (!linesToDelete.get(fileToModify).has(lineNum)) {
+//                     modifiedContent += lines[lineNum - 1];
+//                     modifiedContent += '\n';
+//                 }
+//             }
+//
+//             // Write the modified content back to the file
+//             await fs.writeFile(fileToModify, modifiedContent, 'utf8');
+//         }
+//
+//         console.log('Blank lines removed from added content.');
+//     } catch (error) {
+//         console.error(`Error running 'git diff' or processing files: ${error}`);
+//     }
+// }
+
+/**
+ * If the diff includes any lines that are just whitespace, undo them.
+ * Sometimes blank lines are added in weird places by babel's generator due to the retainLines flag,
+ * which is necessary to keep the formatting from the original source as much as possible.
+ *
+ * @returns {Promise<void>}
+ */
+async function stripBlankLinesFromDiff() {
+    try {
+        const {stdout} = await exec("git diff --unified=0 -- ':!scripts/codemods/MigrateStaticStylesToThemeHooks.js'");
+        console.log('RORY_DEBUG original diff', stdout);
+        const modifiedDiff = [];
+        const lines = stdout.split('\n');
+        for (const line of lines) {
+            // A lonely plus in the diff means we added a blank line
+            if (line && line.replace('+', '').trim() === '') {
+                // Remove the previous line, which will be a hunk marker line, and don't include the lonely plus
+                modifiedDiff.pop();
+            } else {
+                modifiedDiff.push(line);
+            }
+        }
+        // for (let i = 0; i < lines.length; i++) {
+        //     const line = lines[i];
+        //     if (line && line.replace('+', '').trim() === '') {
+        //         // Remove the previous line, which will be a hunk marker line
+        //         modifiedDiff.pop();
+        //     } else {
+        //         modifiedDiff[i] = `${line}`;
+        //     }
+        // }
+        console.log('RORY_DEBUG have modifiedDiff', modifiedDiff.join('\n'));
+        await exec('git restore src/components/Button/index.js');
+        await exec(`echo "${modifiedDiff.join('\n')}" | git apply --recount -`);
+    } catch (error) {
+        console.error('Error stripping blank lines from diff', error);
     }
 }
 
@@ -237,7 +342,9 @@ async function run() {
     } else {
         try {
             await migrateStaticStylesForDirectory(directoryPath);
+            console.log('Running prettier...');
             await exec('npm run prettier');
+            await stripBlankLinesFromDiff();
         } catch (error) {
             console.error('Error:', error);
         }
