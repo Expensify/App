8213caad111c8c59a26ccf3f92d967dc9debe2ab
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-10-20 13:41:42 +0200

    create types file for Modal

---
+++ src/components/Modal/types.ts
@@ -0,0 +1,69 @@
+import {ValueOf} from 'type-fest';
+import {ModalProps} from 'react-native-modal';
+import ChildrenProps from '../../types/utils/ChildrenProps';
+import {WindowDimensionsProps} from '../withWindowDimensions/types';
+import CONST from '../../CONST';
+
+type BaseModalProps = WindowDimensionsProps &
+    ChildrenProps & {
+        /** Decides whether the modal should cover fullscreen. FullScreen modal has backdrop */
+        fullscreen?: boolean;
+
+        /** Should we close modal on outside click */
+        shouldCloseOnOutsideClick?: boolean;
+
+        /** Should we announce the Modal visibility changes? */
+        shouldSetModalVisibility?: boolean;
+
+        /** Callback method fired when the user requests to close the modal */
+        onClose: () => void;
+
+        /** State that determines whether to display the modal or not */
+        isVisible: boolean;
+
+        /** Callback method fired when the user requests to submit the modal content. */
+        onSubmit?: () => void;
+
+        /** Callback method fired when the modal is hidden */
+        onModalHide?: () => void;
+
+        /** Callback method fired when the modal is shown */
+        onModalShow?: () => void;
+
+        /** Style of modal to display */
+        // type: PropTypes.oneOf(_.values(CONST.MODAL.MODAL_TYPE)),
+        type?: ValueOf<typeof CONST.MODAL.MODAL_TYPE>;
+
+        /** A react-native-animatable animation definition for the modal display animation. */
+        animationIn?: Pick<ModalProps, 'animationIn'>;
+
+        /** A react-native-animatable animation definition for the modal hide animation. */
+        animationOut?: Pick<ModalProps, 'animationOut'>;
+
+        /** The anchor position of a popover modal. Has no effect on other modal types. */
+        popoverAnchorPosition?: {
+            top: number;
+            right: number;
+            bottom: number;
+            left: number;
+        };
+
+        /** Modal container styles  */
+        innerContainerStyle?: Pick<ModalProps, 'style'>;
+
+        /** Whether the modal should go under the system statusbar */
+        statusBarTranslucent?: boolean;
+
+        /** Whether the modal should avoid the keyboard */
+        avoidKeyboard?: boolean;
+
+        /**
+         * Whether the modal should hide its content while animating. On iOS, set to true
+         * if `useNativeDriver` is also true, to avoid flashes in the UI.
+         *
+         * See: https://github.com/react-native-modal/react-native-modal/pull/116
+         * */
+        hideModalContentWhileAnimating?: boolean;
+    };
+
+export default BaseModalProps;
