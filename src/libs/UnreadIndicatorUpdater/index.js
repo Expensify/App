15294bed03d9df1331630eaeb4cc02a6bbef2aaa
Sibtain Ali <allroundexperts@gmail.com>
2023-10-23 01:47:50 +0500

    feat: mimic lhn

--- src/libs/UnreadIndicatorUpdater/index.js
+++ src/libs/UnreadIndicatorUpdater/index.js
@@ -1,5 +1,6 @@
 import _ from 'underscore';
 import Onyx from 'react-native-onyx';
+import Navigation from '../Navigation/Navigation';
 import ONYXKEYS from '../../ONYXKEYS';
 import updateUnread from './updateUnread/index';
 import * as ReportUtils from '../ReportUtils';
@@ -8,7 +9,7 @@ Onyx.connect({
     key: ONYXKEYS.COLLECTION.REPORT,
     waitForCollectionCallback: true,
     callback: (reportsFromOnyx) => {
-        const unreadReports = _.filter(reportsFromOnyx, (report) => ReportUtils.isUnread(report) && !ReportUtils.isEmptyReport(report));
+        const unreadReports = _.filter(reportsFromOnyx, (report) => ReportUtils.isUnread(report) && ReportUtils.shouldReportBeInOptionList(report, Navigation.getTopmostReportId()));
         updateUnread(_.size(unreadReports));
     },
 });
