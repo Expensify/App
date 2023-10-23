5fffb48f2c18b47d2e2bda0075b7c0bd8bbb71b9
dukenv0307 <dukenv0307@gmail.com>
2023-10-23 11:52:49 +0700

    safely check

--- src/libs/actions/Policy.js
+++ src/libs/actions/Policy.js
@@ -585,7 +585,7 @@ function clearAvatarErrors(policyID) {
 function updateGeneralSettings(policyID, name, currency) {
     const policy = allPolicies[`${ONYXKEYS.COLLECTION.POLICY}${policyID}`];
     const distanceUnit = _.find(_.values(policy.customUnits), (unit) => unit.name === CONST.CUSTOM_UNITS.NAME_DISTANCE);
-    const distanceRate = _.find(_.values(distanceUnit.rates), (rate) => rate.name === CONST.CUSTOM_UNITS.DEFAULT_RATE);
+    const distanceRate = _.find(_.values(distanceUnit ? distanceUnit.rates : {}), (rate) => rate.name === CONST.CUSTOM_UNITS.DEFAULT_RATE);
     const optimisticData = [
         {
             // We use SET because it's faster than merge and avoids a race condition when setting the currency and navigating the user to the Bank account page in confirmCurrencyChangeAndHideModal
@@ -604,17 +604,21 @@ function updateGeneralSettings(policyID, name, currency) {
                 },
                 name,
                 outputCurrency: currency,
-                customUnits: {
-                    [distanceUnit.customUnitID]: {
-                        ...distanceUnit,
-                        rates: {
-                            [distanceRate.customUnitRateID]: {
-                                ...distanceRate,
-                                currency,
-                            },
-                        },
-                    },
-                },
+                ...(distanceUnit
+                    ? {
+                          customUnits: {
+                              [distanceUnit.customUnitID]: {
+                                  ...distanceUnit,
+                                  rates: {
+                                      [distanceRate.customUnitRateID]: {
+                                          ...distanceRate,
+                                          currency,
+                                      },
+                                  },
+                              },
+                          },
+                      }
+                    : {}),
             },
         },
     ];
@@ -640,9 +644,13 @@ function updateGeneralSettings(policyID, name, currency) {
                 errorFields: {
                     generalSettings: ErrorUtils.getMicroSecondOnyxError('workspace.editor.genericFailureMessage'),
                 },
-                customUnits: {
-                    [distanceUnit.customUnitID]: distanceUnit,
-                },
+                ...(distanceUnit
+                    ? {
+                          customUnits: {
+                              [distanceUnit.customUnitID]: distanceUnit,
+                          },
+                      }
+                    : {}),
             },
         },
     ];
