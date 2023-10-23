84c96f5a6de658f81f211a264c70079615fcd117
Vivek Kumar <vivek@expensify.com>
2023-10-23 10:34:53 +0530

    Update wallet translation

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -865,7 +865,7 @@ export default {
         assignedCards: 'Tarjetas asignadas',
         assignedCardsDescription: 'Son tarjetas asignadas por un administrador del Espacio de Trabajo para gestionar los gastos de la empresa.',
         expensifyCard: 'Tarjeta Expensify',
-        walletActivationPending: 'Estamos revisando la información y nos pondremos en contacto contigo en breve.',
+        walletActivationPending: 'Estamos revisando la información, por favor vuelva en unos minutos.',
     },
     cardPage: {
         expensifyCard: 'Tarjeta Expensify',
