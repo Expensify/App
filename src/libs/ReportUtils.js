5739b7854e6e7c0bf886cd922d2cb32ab9c42ece
dukenv0307 <dukenv0307@gmail.com>
2023-10-23 10:09:07 +0700

    Hide the thread that have not been commented

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -3195,6 +3195,11 @@ function shouldReportBeInOptionList(report, currentReportId, isInGSDMode, betas,
     const isEmptyChat = !report.lastMessageText && !report.lastMessageTranslationKey && !lastVisibleMessage.lastMessageText && !lastVisibleMessage.lastMessageTranslationKey;
     const canHideReport = shouldHideReport(report, currentReportId);
 
+    // Hide only chat threads that haven't been commented on (other threads are actionable)
+    if (isChatThread(report) && canHideReport && isEmptyChat) {
+        return false;
+    }
+
     // Include reports if they are pinned
     if (report.isPinned) {
         return true;
