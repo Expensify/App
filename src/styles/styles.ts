a15e7b9ce520c3cf5bd9b0b15375e51f3e4803f2
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-10-24 11:11:54 +0200

    Remove cardOverlay styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -2893,16 +2893,6 @@ const styles = (theme: ThemeDefault) =>
             width: variables.sideBarWidth - 68,
         },
 
-        cardOverlay: {
-            backgroundColor: theme.overlay,
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            opacity: variables.overlayOpacity,
-        },
-
         shortTermsBorder: {
             borderWidth: 1,
             borderColor: theme.border,
