fe8c19413bcba67a0b78c1c768ea59e88cf80ac1
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2023-10-25 17:45:58 +0200

    withCurrentUserPersonalDetails

--- src/components/withCurrentUserPersonalDetails.tsx
+++ src/components/withCurrentUserPersonalDetails.tsx
@@ -4,13 +4,12 @@ import getComponentDisplayName from '../libs/getComponentDisplayName';
 import ONYXKEYS from '../ONYXKEYS';
 import personalDetailsPropType from '../pages/personalDetailsPropType';
 import type {PersonalDetails, Session} from '../types/onyx';
+import {usePersonalDetails} from './OnyxProvider';
+import CONST from '../CONST';
 
 type CurrentUserPersonalDetails = PersonalDetails | Record<string, never>;
 
 type OnyxProps = {
-    /** Personal details of all the users, including current user */
-    personalDetails: OnyxEntry<Record<string, PersonalDetails>>;
-
     /** Session of the current user */
     session: OnyxEntry<Session>;
 };
@@ -34,8 +33,9 @@ export default function <TProps extends ComponentProps, TRef>(
     WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>,
 ): ComponentType<Omit<Omit<TProps, keyof HOCProps> & RefAttributes<TRef>, keyof OnyxProps>> {
     function WithCurrentUserPersonalDetails(props: Omit<TProps, keyof HOCProps>, ref: ForwardedRef<TRef>) {
+        const personalDetails = usePersonalDetails() || CONST.EMPTY_OBJECT;
         const accountID = props.session?.accountID ?? 0;
-        const accountPersonalDetails = props.personalDetails?.[accountID];
+        const accountPersonalDetails = personalDetails?.[accountID];
         const currentUserPersonalDetails: CurrentUserPersonalDetails = useMemo(
             () => (accountPersonalDetails ? {...accountPersonalDetails, accountID} : {}),
             [accountPersonalDetails, accountID],
@@ -55,9 +55,6 @@ export default function <TProps extends ComponentProps, TRef>(
     const withCurrentUserPersonalDetails = React.forwardRef(WithCurrentUserPersonalDetails);
 
     return withOnyx<Omit<TProps, keyof HOCProps> & RefAttributes<TRef>, OnyxProps>({
-        personalDetails: {
-            key: ONYXKEYS.PERSONAL_DETAILS_LIST,
-        },
         session: {
             key: ONYXKEYS.SESSION,
         },
