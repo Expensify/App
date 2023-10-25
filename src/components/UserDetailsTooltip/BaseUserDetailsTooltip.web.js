1b5ad384161bf7816fe656e6a28dee9bb6971784
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2023-10-25 17:45:34 +0200

    BaseUserDetailsTooltip

--- src/components/UserDetailsTooltip/BaseUserDetailsTooltip.web.js
+++ src/components/UserDetailsTooltip/BaseUserDetailsTooltip.web.js
@@ -1,6 +1,5 @@
 import React, {useCallback} from 'react';
 import {View, Text} from 'react-native';
-import {withOnyx} from 'react-native-onyx';
 import Str from 'expensify-common/lib/str';
 import lodashGet from 'lodash/get';
 import _ from 'underscore';
@@ -8,16 +7,17 @@ import Avatar from '../Avatar';
 import Tooltip from '../Tooltip';
 import {propTypes, defaultProps} from './userDetailsTooltipPropTypes';
 import styles from '../../styles/styles';
-import ONYXKEYS from '../../ONYXKEYS';
 import * as UserUtils from '../../libs/UserUtils';
 import CONST from '../../CONST';
 import * as LocalePhoneNumber from '../../libs/LocalePhoneNumber';
 import useLocalize from '../../hooks/useLocalize';
+import {usePersonalDetails} from '../OnyxProvider';
 
 function BaseUserDetailsTooltip(props) {
     const {translate} = useLocalize();
+    const personalDetails = usePersonalDetails() || CONST.EMPTY_OBJECT;
 
-    const userDetails = lodashGet(props.personalDetailsList, props.accountID, props.fallbackUserDetails);
+    const userDetails = lodashGet(personalDetails, props.accountID, props.fallbackUserDetails);
     let userDisplayName = userDetails.displayName ? userDetails.displayName.trim() : '';
     let userLogin = (userDetails.login || '').trim() && !_.isEqual(userDetails.login, userDetails.displayName) ? Str.removeSMSDomain(userDetails.login) : '';
     let userAvatar = userDetails.avatar;
@@ -26,7 +26,7 @@ function BaseUserDetailsTooltip(props) {
     // We replace the actor's email, name, and avatar with the Copilot manually for now. This will be improved upon when
     // the Copilot feature is implemented.
     if (props.delegateAccountID) {
-        const delegateUserDetails = lodashGet(props.personalDetailsList, props.delegateAccountID, {});
+        const delegateUserDetails = lodashGet(personalDetails, props.delegateAccountID, {});
         const delegateUserDisplayName = delegateUserDetails.displayName ? delegateUserDetails.displayName.trim() : '';
         userDisplayName = `${delegateUserDisplayName} (${translate('reportAction.asCopilot')} ${userDisplayName})`;
         userLogin = delegateUserDetails.login;
@@ -78,8 +78,4 @@ BaseUserDetailsTooltip.propTypes = propTypes;
 BaseUserDetailsTooltip.defaultProps = defaultProps;
 BaseUserDetailsTooltip.displayName = 'BaseUserDetailsTooltip';
 
-export default withOnyx({
-    personalDetailsList: {
-        key: ONYXKEYS.PERSONAL_DETAILS_LIST,
-    },
-})(BaseUserDetailsTooltip);
+export default BaseUserDetailsTooltip;
