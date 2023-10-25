70e00562acef33a84af3118477d9873488693307
Eduardo <eduardo.graciano@callstack.com>
2023-10-25 15:14:24 +0200

    Created Interactive Step Header

---
+++ src/components/InteractiveStepSubHeader.js
@@ -0,0 +1,100 @@
+import React, {forwardRef, useState, useImperativeHandle} from 'react';
+import PropTypes from 'prop-types';
+import map from 'lodash/map';
+import {View} from 'react-native';
+
+import CONST from '../CONST';
+import variables from '../styles/variables';
+import styles from '../styles/styles';
+import colors from '../styles/colors';
+import * as Expensicons from './Icon/Expensicons';
+import PressableWithFeedback from './Pressable/PressableWithFeedback';
+import Text from './Text';
+import Icon from './Icon';
+
+const propTypes = {
+    stepNames: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onStepSelected: PropTypes.func.isRequired,
+    startStep: PropTypes.number,
+};
+
+const defaultProps = {
+    startStep: 0,
+};
+
+const MIN_AMOUNT_FOR_EXPANDING = 3;
+const MIN_AMOUNT_OF_STEPS = 2;
+
+function InteractiveStepSubHeader({stepNames, startStep, onStepSelected}, ref) {
+    const [currentStep, setCurrentStep] = useState(startStep);
+    useImperativeHandle(
+        ref,
+        () => ({
+            moveNext: () => {
+                setCurrentStep((actualStep) => actualStep + 1);
+            },
+        }),
+        [],
+    );
+
+    if (stepNames.length < MIN_AMOUNT_OF_STEPS) {
+        console.warn('InteractiveStepSubHeader: stepNames prop must have at least 2 elements');
+        return null;
+    }
+
+    const amountOfUnions = stepNames.length - 1;
+
+    return (
+        <View style={[styles.interactiveStepHeaderContainer, {minWidth: stepNames.length < MIN_AMOUNT_FOR_EXPANDING ? '60%' : '100%'}]}>
+            {map(stepNames, (stepName, index) => {
+                const isCompletedStep = currentStep > index;
+                const isLockedStep = currentStep < index;
+                const isLockedLine = currentStep < index + 1;
+                const hasUnion = index < amountOfUnions;
+
+                const moveToStep = () => {
+                    if (isLockedStep) {
+                        return;
+                    }
+                    setCurrentStep(index);
+                    onStepSelected(stepNames[index]);
+                };
+                return (
+                    <View
+                        style={[styles.interactiveStepHeaderStepContainer, hasUnion && styles.flex1]}
+                        key={stepName}
+                    >
+                        <PressableWithFeedback
+                            style={[
+                                styles.interactiveStepHeaderStepButton,
+                                isLockedStep && styles.interactiveStepHeaderLockedStepButton,
+                                isCompletedStep && styles.interactiveStepHeaderCompletedStepButton,
+                            ]}
+                            disabled={isLockedStep}
+                            onPress={moveToStep}
+                            accessibilityRole={CONST.ACCESSIBILITY_ROLE.BUTTON}
+                        >
+                            {isCompletedStep ? (
+                                <Icon
+                                    src={Expensicons.Checkmark}
+                                    width={variables.iconSizeNormal}
+                                    height={variables.iconSizeNormal}
+                                    fill={colors.white}
+                                />
+                            ) : (
+                                <Text style={styles.interactiveStepHeaderStepText}>{index + 1}</Text>
+                            )}
+                        </PressableWithFeedback>
+                        {hasUnion ? <View style={[styles.interactiveStepHeaderStepLine, isLockedLine && styles.interactiveStepHeaderLockedStepLine]} /> : null}
+                    </View>
+                );
+            })}
+        </View>
+    );
+}
+
+InteractiveStepSubHeader.propTypes = propTypes;
+InteractiveStepSubHeader.defaultProps = defaultProps;
+InteractiveStepSubHeader.displayName = 'InteractiveStepSubHeader';
+
+export default forwardRef(InteractiveStepSubHeader);
