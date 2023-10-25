526a21a3f84c161c2e0451c8bef4eb211a230d0a
Jack Nam <30609178+thienlnam@users.noreply.github.com>
2023-10-25 16:39:57 -0700

    Merge pull request #30397 from ikevin127/fix/30373
    (cherry picked from commit 1ec799bd312c1a3e1facc2b0734ae90aec3a655e)

--- src/pages/iou/ReceiptSelector/NavigationAwareCamera.js
+++ src/pages/iou/ReceiptSelector/NavigationAwareCamera.js
@@ -72,14 +72,4 @@ NavigationAwareCamera.propTypes = propTypes;
 NavigationAwareCamera.displayName = 'NavigationAwareCamera';
 NavigationAwareCamera.defaultProps = defaultProps;
 
-const NavigationAwareCameraWithRef = React.forwardRef((props, ref) => (
-    <NavigationAwareCamera
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...props}
-        forwardedRef={ref}
-    />
-));
-
-NavigationAwareCameraWithRef.displayName = 'NavigationAwareCameraWithRef';
-
-export default NavigationAwareCameraWithRef;
+export default NavigationAwareCamera;
