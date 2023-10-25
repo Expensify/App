27e2efbfd7ba64e5ee81f11789f0615b540d7ec3
Tsaqif <tsaiinkwa@yahoo.com>
2023-10-25 12:30:13 +0700

    revert check condition of reportName
    Signed-off-by: Tsaqif <tsaiinkwa@yahoo.com>

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -3152,7 +3152,7 @@ function shouldReportBeInOptionList(report, currentReportId, isInGSDMode, betas,
         !report ||
         !report.reportID ||
         !report.type ||
-        !report.reportName ||
+        report.reportName === undefined ||
         report.isHidden ||
         (report.participantAccountIDs &&
             report.participantAccountIDs.length === 0 &&
