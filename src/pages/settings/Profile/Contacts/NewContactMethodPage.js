f996fe4756a17265037d417bf5e503a222d5f08f
Carlos Martins <luacmartins@gmail.com>
2023-10-27 12:35:08 -0600

    Revert "[Form Provider Refactor] NewContactMethodPage"

--- src/pages/settings/Profile/Contacts/NewContactMethodPage.js
+++ src/pages/settings/Profile/Contacts/NewContactMethodPage.js
@@ -18,9 +18,8 @@ import styles from '../../../../styles/styles';
 import * as User from '../../../../libs/actions/User';
 import * as LoginUtils from '../../../../libs/LoginUtils';
 import * as ErrorUtils from '../../../../libs/ErrorUtils';
+import Form from '../../../../components/Form';
 import CONST from '../../../../CONST';
-import FormProvider from '../../../../components/Form/FormProvider';
-import InputWrapper from '../../../../components/Form/InputWrapper';
 
 const propTypes = {
     /* Onyx Props */
@@ -105,7 +104,7 @@ function NewContactMethodPage(props) {
                 title={props.translate('contacts.newContactMethod')}
                 onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_CONTACT_METHODS)}
             />
-            <FormProvider
+            <Form
                 formID={ONYXKEYS.FORMS.NEW_CONTACT_METHOD_FORM}
                 validate={validate}
                 onSubmit={addNewContactMethod}
@@ -115,8 +114,7 @@ function NewContactMethodPage(props) {
             >
                 <Text style={[styles.mb5]}>{props.translate('common.pleaseEnterEmailOrPhoneNumber')}</Text>
                 <View style={[styles.mb6]}>
-                    <InputWrapper
-                        InputComponent={TextInput}
+                    <TextInput
                         label={`${props.translate('common.email')}/${props.translate('common.phoneNumber')}`}
                         accessibilityLabel={`${props.translate('common.email')}/${props.translate('common.phoneNumber')}`}
                         accessibilityRole={CONST.ACCESSIBILITY_ROLE.TEXT}
@@ -128,7 +126,7 @@ function NewContactMethodPage(props) {
                         maxLength={CONST.LOGIN_CHARACTER_LIMIT}
                     />
                 </View>
-            </FormProvider>
+            </Form>
         </ScreenWrapper>
     );
 }
