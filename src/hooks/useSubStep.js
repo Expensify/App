f76e933af1b60a51c8891c79f1c95ec13bf7b117
Marcin Swornowski <swor71@gmail.com>
2023-10-27 15:01:56 +0200

    add: prevScreen method and screenIndex to the return from the hook, added tests

--- src/hooks/useSubStep.js
+++ src/hooks/useSubStep.js
@@ -18,7 +18,9 @@ const propTypes = {
  * @typedef {Object} ReturnType
  * @property {React.ReactElement} componentToRender the component to be used in the body of substep scren
  * @property {boolean} isEditing a boolean for the substep to know if we moved back to it and it's being edited
+ * @property {boolean} screenIndex a boolean for the substep to know if we moved back to it and it's being edited
  * @property {Function} nextScreen a function to be called in order to navigate to the next substep screen
+ * @property {Function} prevScreen a function to be called in order to navigate to the previous substep screen if possible
  * @property {Function} moveTo a function to be called in order to navigate to a particular substep screen
  */
 
@@ -34,6 +36,16 @@ const propTypes = {
 export default function useSubStep({bodyContent, onFinished, startFrom = 0}) {
     const [screenIndex, setScreenIndex] = useState(startFrom);
     const isEditing = useRef(false);
+
+    const prevScreen = useCallback(() => {
+        const prevScreenIndex = screenIndex - 1;
+
+        if (prevScreenIndex < 0) {
+            return;
+        }
+
+        setScreenIndex(prevScreenIndex);
+    }, [screenIndex]);
 
     const nextScreen = useCallback(() => {
         if (isEditing.current) {
@@ -58,7 +70,7 @@ export default function useSubStep({bodyContent, onFinished, startFrom = 0}) {
         setScreenIndex(step);
     }, []);
 
-    return {componentToRender: bodyContent[screenIndex], isEditing: isEditing.current, nextScreen, moveTo};
+    return {componentToRender: bodyContent[screenIndex], isEditing: isEditing.current, screenIndex, prevScreen, nextScreen, moveTo};
 }
 
 useSubStep.propTypes = propTypes;
