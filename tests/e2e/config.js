c469811033001aa5f53efca024b81b013257c9d3
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-10-27 10:19:12 +0200

    increase test runs

--- tests/e2e/config.js
+++ tests/e2e/config.js
@@ -31,7 +31,7 @@ module.exports = {
     SERVER_PORT: 4723,
 
     // The amount of times a test should be executed for average performance metrics
-    RUNS: 60,
+    RUNS: 80,
 
     DEFAULT_BASELINE_BRANCH: 'main',
 
