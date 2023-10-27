f4ddc3008272c3d97e0923863dda9f7bb1f26c26
Marcin Swornowski <swor71@gmail.com>
2023-10-27 10:52:27 +0200

    add: descriptions for jsdoc properties

--- src/hooks/useSubStep.js
+++ src/hooks/useSubStep.js
@@ -1,30 +1,30 @@
 import {useState, useRef, useCallback} from 'react';
 import PropTypes from 'prop-types';
 
-const propTypes = PropTypes.shape({
+const propTypes = {
     bodyContent: PropTypes.arrayOf(PropTypes.element).isRequired,
     startFrom: PropTypes.number.isRequired,
     onFinished: PropTypes.func.isRequired,
-});
+};
 
 /**
+ * Return type for useSubStep hook
+ *
  * @typedef {Object} ReturnType
- * @property {React.ReactElement} componentToRender
- * @property {boolean} isEditing
- * @property {Function} nextScreen
- * @property {Function} moveTo
+ * @property {React.ReactElement} componentToRender the component to be used in the body of substep scren
+ * @property {boolean} isEditing a boolean for the substep to know if we moved back to it and it's being edited
+ * @property {Function} nextScreen a function to be called in order to navigate to the next substep screen
+ * @property {Function} moveTo a function to be called in order to navigate to a particular substep screen
  */
 
 /**
  * A hook which manages navigation between substeps
  *
- * @export
- * @param {[React.ReactElement]} bodyContent
- * @param {number} startFrom
- * @param {Function} onFinished
+ * @param {[React.ReactElement]} bodyContent an array of substep components
+ * @param {number} startFrom an index of the component from bodyContent array to start from
+ * @param {Function} onFinished a callback to be fired when pressing Confirm on the last substep screen
  * @return {ReturnType}
  */
-
 export default function useSubStep({bodyContent, startFrom = 0, onFinished}) {
     const [screenIndex, setScreenIndex] = useState(startFrom);
     const isEditing = useRef(false);
