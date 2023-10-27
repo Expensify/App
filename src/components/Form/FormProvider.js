08557e9a99b80d438ab8391fd2d89cd469d261c9
Ishpaul Singh <ishpaul207@gmail.com>
2023-10-28 00:53:13 +0530

    fix/focus error input on click of CTA message

--- src/components/Form/FormProvider.js
+++ src/components/Form/FormProvider.js
@@ -204,6 +204,7 @@ function FormProvider({validate, formID, shouldValidateOnBlur, shouldValidateOnC
 
     const registerInput = useCallback(
         (inputID, propsToParse = {}) => {
+
             const newRef = inputRefs.current[inputID] || propsToParse.ref || createRef();
             if (inputRefs.current[inputID] !== newRef) {
                 inputRefs.current[inputID] = newRef;
@@ -233,7 +234,10 @@ function FormProvider({validate, formID, shouldValidateOnBlur, shouldValidateOnC
 
             return {
                 ...propsToParse,
-                ref: newRef,
+                ref: typeof propsToParse.ref === 'function' ? (node) => {
+                    propsToParse.ref(node);
+                    newRef.current = node;
+                } : newRef,
                 inputID,
                 key: propsToParse.key || inputID,
                 errorText: errors[inputID] || fieldErrorMessage,
