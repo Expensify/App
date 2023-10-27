68d79d9b16cc4b0b6f84379b37f25dce3598844c
Christoph Pader <christoph@margelo.io>
2023-10-27 18:33:48 +0200

    add wasm load time metric

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import Config from './src/CONFIG';
 import * as Setup from './src/setup';
+import performance from 'react-native-performance';
+
+performance.mark('appStartup');
 
 enableLegacyWebImplementation(true);
 
 Setup.beforeAppLoad().then(() => {
+    performance.measure('Encryptify WASM load', 'appStartup');
+    const wasmLoadTime = performance.getEntriesByName('Encryptify WASM load')[0].duration;
+    console.log(`Encryptify WASM load time: ${wasmLoadTime}ms`);
+
     AppRegistry.registerComponent(Config.APP_NAME, () => App);
     Setup.afterAppLoad();
 });
