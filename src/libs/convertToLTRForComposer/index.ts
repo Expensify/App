6777fb5e78b22342a8c25b86c26eb8e6ca985227
Hardik Choudhary <hardik109bkp@gmail.com>
2023-10-27 20:40:02 +0530

    emoji/mention suggestion are displayed

--- src/libs/convertToLTRForComposer/index.ts
+++ src/libs/convertToLTRForComposer/index.ts
@@ -1,19 +1,27 @@
 import CONST from '../../CONST';
 import ConvertToLTRForComposer from './types';
 
-function hasLTRorRTLCharacters(text: string): boolean {
-    // Regular expressions to match LTR and RTL character ranges.
+function hasRTLCharacters(text: string): boolean {
+    // Regular expressions to match RTL character ranges.
+    const rtlPattern = /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/;
+    return rtlPattern.test(text);
+}
+function hasLTRCharacters(text: string): boolean {
+    // Regular expressions to match LTR character ranges.
     // eslint-disable-next-line no-control-regex
     const ltrPattern = /[\u0001-\u05FF\u0600-\u06FF\u0750-\u077F\uFB50-\uFDFF\uFE70-\uFEFF]/;
-    const rtlPattern = /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/;
 
-    return ltrPattern.test(text) || rtlPattern.test(text);
+    return ltrPattern.test(text);
 }
 
 // Converts a given text to ensure it starts with the LTR (Left-to-Right) marker.
 const convertToLTRForComposer: ConvertToLTRForComposer = (text) => {
-    // Ensure the text contains LTR or RTL characters to avoid an unwanted special character at the beginning, even after a backspace deletion.
-    if (!hasLTRorRTLCharacters(text)) {
+    // Ensure that the text starts with RTL characters if not we return the same text to avoid concatination with special character at the start which leads to unexpected behaviour for Emoji/Mention suggestions.
+    if (!hasRTLCharacters(text)) {
+        // If text contains LTR character return the same text otherwise return an empty string to avoid an empty draft due to special character.
+        if (hasLTRCharacters(text)) {
+            return text;
+        }
         return '';
     }
 
