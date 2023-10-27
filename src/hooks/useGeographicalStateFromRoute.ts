0f83cff67014fc53ffa0d005247a0cc29f1a8a3f
Yogesh Bhatt <ygshbht@gmail.com>
2023-10-28 02:35:21 +0530

    move getStateFromRoute to useGeographicalStateFromRoute

--- src/hooks/useGeographicalStateFromRoute.ts
+++ src/hooks/useGeographicalStateFromRoute.ts
@@ -1,7 +1,11 @@
 import {useRoute} from '@react-navigation/native';
-import getStateFromRoute from '../libs/getStateFromRoute';
+// eslint-disable-next-line you-dont-need-lodash-underscore/get
+import lodashGet from 'lodash/get';
+import {CONST as COMMON_CONST} from 'expensify-common/lib/CONST';
 
 export default function useGeographicalStateFromRoute(stateParamName = 'state') {
     const route = useRoute();
-    return getStateFromRoute(route, stateParamName);
+    const stateFromUrlTemp = lodashGet(route, `params.${stateParamName}`) as unknown as string;
+    // check if state is valid
+    return lodashGet(COMMON_CONST.STATES, stateFromUrlTemp) ? stateFromUrlTemp : '';
 }
