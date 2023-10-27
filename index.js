77594c8bb705587347219c972bc2e3bad88d5f62
Christoph Pader <christoph@margelo.io>
2023-10-27 18:35:02 +0200

    update app startup test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import App from './src/App';
 import Config from './src/CONFIG';
 import * as Setup from './src/setup';
 import performance from 'react-native-performance';
+import {PERFORMANCE_METRICS_DECIMAL_PLACES} from './src/testEncryptifyPerformance';
 
 performance.mark('appStartup');
 
@@ -16,7 +17,7 @@ enableLegacyWebImplementation(true);
 Setup.beforeAppLoad().then(() => {
     performance.measure('Encryptify WASM load', 'appStartup');
     const wasmLoadTime = performance.getEntriesByName('Encryptify WASM load')[0].duration;
-    console.log(`Encryptify WASM load time: ${wasmLoadTime}ms`);
+    console.log(`Encryptify WASM load time: ${wasmLoadTime.toFixed(PERFORMANCE_METRICS_DECIMAL_PLACES)}ms`);
 
     AppRegistry.registerComponent(Config.APP_NAME, () => App);
     Setup.afterAppLoad();
