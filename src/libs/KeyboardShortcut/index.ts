bf0a9128bcebed1c63123c8c5aff2f682846c3a9
Kailash Devrari <devrari@gmail.com>
2023-10-28 18:58:46 +0530

    updated code because of TS changes

--- src/libs/KeyboardShortcut/index.ts
+++ src/libs/KeyboardShortcut/index.ts
@@ -13,6 +13,7 @@ type EventHandler = {
     shouldPreventDefault: boolean;
     shouldBubble: boolean | (() => void);
     excludedNodes: string[];
+    shouldStopPropagation: boolean;
 };
 
 // Handlers for the various keyboard listeners we set up
@@ -135,6 +136,7 @@ function subscribe(
     priority = 0,
     shouldPreventDefault = true,
     excludedNodes = [],
+    shouldStopPropagation = false,
 ) {
     const platformAdjustedModifiers = getPlatformEquivalentForKeys(modifiers);
     const displayName = getDisplayName(key, platformAdjustedModifiers);
@@ -150,6 +152,7 @@ function subscribe(
         shouldPreventDefault,
         shouldBubble,
         excludedNodes,
+        shouldStopPropagation,
     });
 
     if (descriptionKey) {
