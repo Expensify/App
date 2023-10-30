fb274a90b68fb5a4f1cfb9ce345b25c2e9fcbce6
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-10-30 09:47:42 +0100

    fix: resolved review comment

--- src/types/modules/react-native-web-linear-gradient.d.ts
+++ src/types/modules/react-native-web-linear-gradient.d.ts
@@ -1,17 +1,6 @@
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 declare module 'react-native-web-linear-gradient' {
-    import type {ViewProps} from 'react-native';
-
-    interface LinearGradientProps extends ViewProps {
-        colors: string[];
-        start?: {x: number; y: number};
-        end?: {x: number; y: number};
-        locations?: number[];
-        useAngle?: boolean;
-        angle?: number;
-    }
-
-    class LinearGradient extends React.PureComponent<LinearGradientProps> {}
+    import LinearGradient from 'react-native-linear-gradient';
 
     export default LinearGradient;
 }
