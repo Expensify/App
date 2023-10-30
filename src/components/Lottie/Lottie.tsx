e20fa3b0782ca5508e82378387ae53b07b3fa4e0
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-10-30 10:58:39 +0100

    add offline support

--- src/components/Lottie/Lottie.tsx
+++ src/components/Lottie/Lottie.tsx
@@ -1,16 +1,28 @@
 import LottieView, {LottieViewProps} from 'lottie-react-native';
 import React, {CSSProperties, ForwardedRef, forwardRef} from 'react';
-import {ViewStyle} from 'react-native';
+import {ViewStyle, Text} from 'react-native';
 import DotLottieAnimation from '@components/LottieAnimations/types';
 import styles from '@styles/styles';
+import useNetwork from '@hooks/useNetwork';
 
 type Props = {
     animation: DotLottieAnimation;
 } & LottieViewProps;
 
 function Lottie({animation, ...props}: Props, ref: ForwardedRef<LottieView>) {
+    const [isError, setIsError] = React.useState(false);
+
     const source = animation.file;
     const style: ViewStyle = styles.aspectRatioLottie(animation);
+
+    useNetwork({onReconnect: () => {
+      console.log('reconnected, load image again');
+      setIsError(false);
+    }});
+
+    if (isError) {
+        return <Text>Animation not loaded. You are offline!</Text>;
+    }
 
     return (
         <LottieView
@@ -20,6 +32,10 @@ function Lottie({animation, ...props}: Props, ref: ForwardedRef<LottieView>) {
             ref={ref}
             style={[props.style, style]}
             webStyle={style as CSSProperties}
+            onAnimationFailure={(error) => {
+              console.log('animation failure', error);
+              setIsError(true);
+            }}
         />
     );
 }
