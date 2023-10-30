347d2a7bf17e42dddafced4c8efe9afd8f70f9c8
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-10-30 10:00:53 +0100

    fix eslint warnings

--- src/libs/E2E/tests/reportTypingTest.e2e.js
+++ src/libs/E2E/tests/reportTypingTest.e2e.js
@@ -1,12 +1,12 @@
-import E2ELogin from '../actions/e2eLogin';
-import Performance from '../../Performance';
-import E2EClient from '../client';
-import Navigation from '../../Navigation/Navigation';
-import ROUTES from '../../../ROUTES';
-import CONST from '../../../CONST';
+import E2ELogin from '@libs/E2E/actions/e2eLogin';
+import waitForKeyboard from '@libs/E2E/actions/waitForKeyboard';
+import E2EClient from '@libs/E2E/client';
+import Navigation from '@libs/Navigation/Navigation';
+import Performance from '@libs/Performance';
+import {getRerenderCount, resetRerenderCount} from '@pages/home/report/ReportActionCompose/ComposerWithSuggestions/index.e2e';
+import CONST from '@src/CONST';
+import ROUTES from '@src/ROUTES';
 import * as NativeCommands from '../../../../tests/e2e/nativeCommands/NativeCommandsAction';
-import waitForKeyboard from '../actions/waitForKeyboard';
-import {resetRerenderCount, getRerenderCount} from '../../../pages/home/report/ReportActionCompose/ComposerWithSuggestions/index.e2e';
 
 const test = () => {
     // check for login (if already logged in the action will simply resolve)
