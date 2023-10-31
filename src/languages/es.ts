41c05aea9ca3e8ccbd46d53998504c7ee0e0a753
VH <hoangzinhvn@gmail.com>
2023-10-31 23:20:24 +0700

    Fix lint

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -588,7 +588,8 @@ export default {
             genericSmartscanFailureMessage: 'La transacción tiene campos vacíos',
             duplicateWaypointsErrorMessage: 'Por favor elimina los puntos de ruta duplicados',
             emptyWaypointsErrorMessage: 'Por favor introduce al menos dos puntos de ruta',
-            splitBillMultipleParticipantsIncludingWorkspace: 'Solo puedes dividir una cuenta entre un único espacio de trabajo o con usuarios individuales. Por favor actualiza tu selección.',
+            splitBillMultipleParticipantsIncludingWorkspace:
+                'Solo puedes dividir una cuenta entre un único espacio de trabajo o con usuarios individuales. Por favor actualiza tu selección.',
         },
         waitingOnEnabledWallet: ({submitterDisplayName}: WaitingOnBankAccountParams) => `Inició el pago, pero no se procesará hasta que ${submitterDisplayName} active su Billetera`,
         enableWallet: 'Habilitar Billetera',
