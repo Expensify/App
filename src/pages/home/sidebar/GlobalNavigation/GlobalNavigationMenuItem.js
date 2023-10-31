382e43866fcdb6a8e7579818794235ff1175d778
rory <rory@expensify.com>
2023-10-31 14:21:45 -0700

    Migrate GlobalNavigationMenuItem

--- src/pages/home/sidebar/GlobalNavigation/GlobalNavigationMenuItem.js
+++ src/pages/home/sidebar/GlobalNavigation/GlobalNavigationMenuItem.js
@@ -4,8 +4,8 @@ import {View} from 'react-native';
 import Icon from '@components/Icon';
 import PressableWithFeedback from '@components/Pressable/PressableWithFeedback';
 import Text from '@components/Text';
-import styles from '@styles/styles';
 import * as StyleUtils from '@styles/StyleUtils';
+import useThemeStyles from '@styles/useThemeStyles';
 import variables from '@styles/variables';
 import CONST from '@src/CONST';
 
@@ -30,32 +30,35 @@ const defaultProps = {
     title: '',
 };
 
-const GlobalNavigationMenuItem = React.forwardRef(({icon, title, isFocused, onPress}, ref) => (
-    <PressableWithFeedback
-        onPress={() => !isFocused && onPress()}
-        style={styles.globalNavigationItemContainer}
-        ref={ref}
-        accessibilityRole={CONST.ACCESSIBILITY_ROLE.MENUITEM}
-        accessibilityLabel={title}
-    >
-        {({pressed}) => (
-            <View style={[styles.alignItemsCenter, styles.flexRow, styles.flex1]}>
-                <View style={styles.globalNavigationSelectionIndicator(isFocused)} />
-                <View style={[styles.flexColumn, styles.flex1, styles.alignItemsCenter, styles.mr1]}>
-                    <Icon
-                        additionalStyles={[styles.popoverMenuIcon]}
-                        pressed={pressed}
-                        src={icon}
-                        fill={isFocused ? StyleUtils.getIconFillColor(CONST.BUTTON_STATES.DEFAULT, true) : StyleUtils.getIconFillColor()}
-                    />
-                    <View style={[styles.mt1, styles.alignItemsCenter]}>
-                        <Text style={[StyleUtils.getFontSizeStyle(variables.fontSizeExtraSmall), styles.globalNavigationMenuItem(isFocused)]}>{title}</Text>
+const GlobalNavigationMenuItem = React.forwardRef(({icon, title, isFocused, onPress}, ref) => {
+    const styles = useThemeStyles();
+    return (
+        <PressableWithFeedback
+            onPress={() => !isFocused && onPress()}
+            style={styles.globalNavigationItemContainer}
+            ref={ref}
+            accessibilityRole={CONST.ACCESSIBILITY_ROLE.MENUITEM}
+            accessibilityLabel={title}
+        >
+            {({pressed}) => (
+                <View style={[styles.alignItemsCenter, styles.flexRow, styles.flex1]}>
+                    <View style={styles.globalNavigationSelectionIndicator(isFocused)} />
+                    <View style={[styles.flexColumn, styles.flex1, styles.alignItemsCenter, styles.mr1]}>
+                        <Icon
+                            additionalStyles={[styles.popoverMenuIcon]}
+                            pressed={pressed}
+                            src={icon}
+                            fill={isFocused ? StyleUtils.getIconFillColor(CONST.BUTTON_STATES.DEFAULT, true) : StyleUtils.getIconFillColor()}
+                        />
+                        <View style={[styles.mt1, styles.alignItemsCenter]}>
+                            <Text style={[StyleUtils.getFontSizeStyle(variables.fontSizeExtraSmall), styles.globalNavigationMenuItem(isFocused)]}>{title}</Text>
+                        </View>
                     </View>
                 </View>
-            </View>
-        )}
-    </PressableWithFeedback>
-));
+            )}
+        </PressableWithFeedback>
+    );
+});
 
 GlobalNavigationMenuItem.propTypes = propTypes;
 GlobalNavigationMenuItem.defaultProps = defaultProps;
