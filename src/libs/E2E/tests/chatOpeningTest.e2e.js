f919ede550fde5c2db36505ab40aad7514515347
Taras Perun <terry.perun@gmail.com>
2023-10-31 16:28:02 +0100

    get rid of E2ECleanReportActions

--- src/libs/E2E/tests/chatOpeningTest.e2e.js
+++ src/libs/E2E/tests/chatOpeningTest.e2e.js
@@ -1,4 +1,3 @@
-import E2ECleanReportActions from '@libs/E2E/actions/cleanReportActions';
 import E2ELogin from '@libs/E2E/actions/e2eLogin';
 import getReport from '@libs/E2E/apiMocks/openReport';
 import E2EClient from '@libs/E2E/client';
@@ -19,33 +18,30 @@ const test = () => {
             return E2EClient.submitTestDone();
         }
 
-        console.debug('[E2E] Logged in, cleaning report actions from onyx…');
-        E2ECleanReportActions().then(() => {
-            console.debug('[E2E] Logged in, getting chat opening metrics and submitting them…');
-            Performance.subscribeToMeasurements((entry) => {
-                if (entry.name === CONST.TIMING.SIDEBAR_LOADED) {
-                    console.debug(`[E2E] Sidebar loaded, navigating to search route…`);
-                    Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(reportID));
-                    return;
-                }
-                console.debug(`[E2E] Entry: ${JSON.stringify(entry)}`);
-                if (entry.name !== CONST.TIMING.CHAT_RENDER) {
-                    return;
-                }
+        console.debug('[E2E] Logged in, getting chat opening metrics and submitting them…');
+        Performance.subscribeToMeasurements((entry) => {
+            if (entry.name === CONST.TIMING.SIDEBAR_LOADED) {
+                console.debug(`[E2E] Sidebar loaded, navigating to search route…`);
+                Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(reportID));
+                return;
+            }
+            console.debug(`[E2E] Entry: ${JSON.stringify(entry)}`);
+            if (entry.name !== CONST.TIMING.CHAT_RENDER) {
+                return;
+            }
 
-                console.debug(`[E2E] Submitting!`);
-                E2EClient.submitTestResults({
-                    name: 'Chat opening',
-                    duration: entry.duration,
+            console.debug(`[E2E] Submitting!`);
+            E2EClient.submitTestResults({
+                name: 'Chat opening',
+                duration: entry.duration,
+            })
+                .then(() => {
+                    console.debug('[E2E] Done with search, exiting…');
+                    E2EClient.submitTestDone();
                 })
-                    .then(() => {
-                        console.debug('[E2E] Done with search, exiting…');
-                        E2EClient.submitTestDone();
-                    })
-                    .catch((err) => {
-                        console.debug('[E2E] Error while submitting test results:', err);
-                    });
-            });
+                .catch((err) => {
+                    console.debug('[E2E] Error while submitting test results:', err);
+                });
         });
     });
 };
