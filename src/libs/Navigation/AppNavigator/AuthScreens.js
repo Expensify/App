32270a39f3e634c687089d3fa1694074c6e658a5
rory <rory@expensify.com>
2023-10-31 14:20:13 -0700

    Fix getRootNavigatorScreenOptions

--- src/libs/Navigation/AppNavigator/AuthScreens.js
+++ src/libs/Navigation/AppNavigator/AuthScreens.js
@@ -1,6 +1,6 @@
 import lodashGet from 'lodash/get';
 import PropTypes from 'prop-types';
-import React, {memo, useEffect, useRef} from 'react';
+import React, {memo, useEffect, useMemo, useRef} from 'react';
 import {View} from 'react-native';
 import Onyx, {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
@@ -15,7 +15,9 @@ import * as SessionUtils from '@libs/SessionUtils';
 import DemoSetupPage from '@pages/DemoSetupPage';
 import NotFoundPage from '@pages/ErrorPage/NotFoundPage';
 import DesktopSignInRedirectPage from '@pages/signin/DesktopSignInRedirectPage';
+import getNavigationModalCardStyle from '@styles/getNavigationModalCardStyles';
 import useThemeStyles from '@styles/useThemeStyles';
+import variables from '@styles/variables';
 import * as App from '@userActions/App';
 import * as Download from '@userActions/Download';
 import * as Modal from '@userActions/Modal';
@@ -32,7 +34,7 @@ import ROUTES from '@src/ROUTES';
 import SCREENS from '@src/SCREENS';
 import createCustomStackNavigator from './createCustomStackNavigator';
 import defaultScreenOptions from './defaultScreenOptions';
-import getRootNavigatorScreenOptions from './getRootNavigatorScreenOptions';
+import modalCardStyleInterpolator from './modalCardStyleInterpolator';
 import CentralPaneNavigator from './Navigators/CentralPaneNavigator';
 import RightModalNavigator from './Navigators/RightModalNavigator';
 
@@ -141,10 +143,77 @@ const defaultProps = {
     demoInfo: {},
 };
 
+const commonScreenOptions = {
+    headerShown: false,
+    gestureDirection: 'horizontal',
+    animationEnabled: true,
+    cardOverlayEnabled: true,
+    animationTypeForReplace: 'push',
+};
+
 function AuthScreens({isUsingMemoryOnlyKeys, lastUpdateIDAppliedToClient, session, lastOpenedPublicRoomID, demoInfo}) {
     const styles = useThemeStyles();
     const {isSmallScreenWidth} = useWindowDimensions();
-    const screenOptions = getRootNavigatorScreenOptions(isSmallScreenWidth);
+    const screenOptions = useMemo(
+        () => ({
+            rightModalNavigator: {
+                ...commonScreenOptions,
+                cardStyleInterpolator: (props) => modalCardStyleInterpolator(isSmallScreenWidth, false, props),
+                presentation: 'transparentModal',
+
+                // We want pop in RHP since there are some flows that would work weird otherwise
+                animationTypeForReplace: 'pop',
+                cardStyle: {
+                    ...getNavigationModalCardStyle(),
+
+                    // This is necessary to cover translated sidebar with overlay.
+                    width: isSmallScreenWidth ? '100%' : '200%',
+                    // Excess space should be on the left so we need to position from right.
+                    right: 0,
+                },
+            },
+
+            homeScreen: {
+                title: CONFIG.SITE_TITLE,
+                ...commonScreenOptions,
+                cardStyleInterpolator: (props) => modalCardStyleInterpolator(isSmallScreenWidth, false, props),
+
+                cardStyle: {
+                    ...getNavigationModalCardStyle(),
+                    width: isSmallScreenWidth ? '100%' : variables.sideBarWidth,
+
+                    // We need to translate the sidebar to not be covered by the StackNavigator so it can be clickable.
+                    transform: [{translateX: isSmallScreenWidth ? 0 : -variables.sideBarWidth}],
+                    ...(isSmallScreenWidth ? {} : styles.borderRight),
+                },
+            },
+            // eslint-disable-next-line rulesdir/no-negated-variables
+            fullScreen: {
+                ...commonScreenOptions,
+                cardStyleInterpolator: (props) => modalCardStyleInterpolator(isSmallScreenWidth, true, props),
+                cardStyle: {
+                    ...getNavigationModalCardStyle(),
+
+                    // This is necessary to cover whole screen. Including translated sidebar.
+                    marginLeft: isSmallScreenWidth ? 0 : -variables.sideBarWidth,
+                },
+            },
+
+            centralPaneNavigator: {
+                title: CONFIG.SITE_TITLE,
+                ...commonScreenOptions,
+                animationEnabled: isSmallScreenWidth,
+                cardStyleInterpolator: (props) => modalCardStyleInterpolator(isSmallScreenWidth, true, props),
+
+                cardStyle: {
+                    ...getNavigationModalCardStyle(),
+                    paddingRight: isSmallScreenWidth ? 0 : variables.sideBarWidth,
+                },
+            },
+        }),
+        [isSmallScreenWidth, styles.borderRight],
+    );
+
     const isInitialRender = useRef(true);
 
     if (isInitialRender.current) {
