2f72f9eceb89fcc200a7208755cfbbfeaa5cf399
rory <rory@expensify.com>
2023-10-30 20:49:03 -0700

    Fix Terms

--- src/pages/signin/Terms.js
+++ src/pages/signin/Terms.js
@@ -1,38 +1,37 @@
 import React from 'react';
 import Text from '@components/Text';
 import TextLink from '@components/TextLink';
-import withLocalize, {withLocalizePropTypes} from '@components/withLocalize';
-import CONST from '@src/CONST';
+import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@styles/useThemeStyles';
+import CONST from '@src/CONST';
 
-const linkStyles = [styles.textExtraSmallSupporting, styles.link];
-
-function Terms(props) {
+function Terms() {
+    const {translate} = useLocalize();
     const styles = useThemeStyles();
+    const linkStyles = [styles.textExtraSmallSupporting, styles.link];
     return (
         <Text style={[styles.textExtraSmallSupporting, styles.mb4]}>
-            {props.translate('termsOfUse.phrase1')}
+            {translate('termsOfUse.phrase1')}
             <TextLink
                 style={linkStyles}
                 href={CONST.TERMS_URL}
             >
                 {' '}
-                {props.translate('termsOfUse.phrase2')}{' '}
+                {translate('termsOfUse.phrase2')}{' '}
             </TextLink>
-            {props.translate('termsOfUse.phrase3')}
+            {translate('termsOfUse.phrase3')}
             <TextLink
                 style={linkStyles}
                 href={CONST.PRIVACY_URL}
             >
                 {' '}
-                {props.translate('termsOfUse.phrase4')}
+                {translate('termsOfUse.phrase4')}
             </TextLink>
             {'. '}
         </Text>
     );
 }
 
-Terms.propTypes = {...withLocalizePropTypes};
 Terms.displayName = 'Terms';
 
-export default withLocalize(Terms);
+export default Terms;
