d6eff10375790ab10af0c86db33bc5b1ade8ab98
Puneet Lath <puneet@expensify.com>
2023-10-31 22:26:42 +0100

    Update src/libs/ReportUtils.js
    Co-authored-by: Marc Glasser <marc@expensify.com>

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -1327,7 +1327,7 @@ function getLastVisibleMessage(reportID, actionsToMerge = {}) {
  * Checks if a report is an open task report assigned to current user.
  *
  * @param {Object} report
- * @param {Object} parentReportAction - The parent report action of the report (Used to check if the task has been canceled)
+ * @param {Object} [parentReportAction] - The parent report action of the report (Used to check if the task has been canceled)
  * @returns {Boolean}
  */
 function isWaitingForAssigneeToCompleteTask(report, parentReportAction = {}) {
