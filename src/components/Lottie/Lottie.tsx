1851190fb1e7f5d4f0b32d7899f328f59a44d194
rory <rory@expensify.com>
2023-10-30 21:12:41 -0700

    Migrate Lottie

--- src/components/Lottie/Lottie.tsx
+++ src/components/Lottie/Lottie.tsx
@@ -1,14 +1,17 @@
 import LottieView, {LottieViewProps} from 'lottie-react-native';
 import React, {forwardRef} from 'react';
-import styles from '@styles/styles';
+import useThemeStyles from '@styles/useThemeStyles';
 
-const Lottie = forwardRef<LottieView, LottieViewProps>((props: LottieViewProps, ref) => (
-    <LottieView
-        // eslint-disable-next-line
-        {...props}
-        ref={ref}
-        style={[styles.aspectRatioLottie(props.source), props.style]}
-    />
-));
+const Lottie = forwardRef<LottieView, LottieViewProps>((props: LottieViewProps, ref) => {
+    const styles = useThemeStyles();
+    return (
+        <LottieView
+            // eslint-disable-next-line
+            {...props}
+            ref={ref}
+            style={[styles.aspectRatioLottie(props.source), props.style]}
+        />
+    );
+});
 
 export default Lottie;
