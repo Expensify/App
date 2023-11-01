0fecc01918ba3d821c39d21bd24ea359042e5f33
dukenv0307 <129500732+dukenv0307@users.noreply.github.com>
2023-11-01 16:46:39 +0700

    Update src/languages/en.ts
    Co-authored-by: wentao <ntdiary@163.com>

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -592,7 +592,6 @@ export default {
             genericDeleteFailureMessage: 'Unexpected error deleting the money request, please try again later',
             genericEditFailureMessage: 'Unexpected error editing the money request, please try again later',
             genericSmartscanFailureMessage: 'Transaction is missing fields',
-            duplicateWaypointsErrorMessage: 'Please remove duplicate waypoints',
             atLeastTwoDifferentWaypoints: 'Please enter at least two waypoints',
         },
         waitingOnEnabledWallet: ({submitterDisplayName}: WaitingOnBankAccountParams) => `Started settling up, payment is held until ${submitterDisplayName} enables their Wallet`,
