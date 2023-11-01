45b03b08f163b6cb0678d590fcf673d060bc5ac9
bartektomczyk <bartlomiejtomczyk@gmail.com>
2023-11-01 18:20:32 +0100

    refactor: change event handler method

--- src/libs/setShouldShowComposeInputKeyboardAware/index.native.ts
+++ src/libs/setShouldShowComposeInputKeyboardAware/index.native.ts
@@ -1,12 +1,18 @@
 import {EmitterSubscription, Keyboard, Platform} from 'react-native';
+import getOperatingSystem from '@libs/getOperatingSystem';
 import * as Composer from '@userActions/Composer';
+import CONST from '@src/CONST';
 import SetShouldShowComposeInputKeyboardAware from './types';
 
-let keyboardDidHideListener: EmitterSubscription | null = null;
+let keyboardEventListener: EmitterSubscription | null = null;
+// On iOS is visible delay with displaying input after keyboard has been closed with `keyboardDidHide` event
+// Because of that - on iOS we can use `keyboardWillHide` that is not available on android
+const keyboardEvent = getOperatingSystem() === CONST.OS.IOS ? 'keyboardWillHide' : 'keyboardDidHide';
+
 const setShouldShowComposeInputKeyboardAware: SetShouldShowComposeInputKeyboardAware = (shouldShow) => {
-    if (keyboardDidHideListener) {
-        keyboardDidHideListener.remove();
-        keyboardDidHideListener = null;
+    if (keyboardEventListener) {
+        keyboardEventListener.remove();
+        keyboardEventListener = null;
     }
 
     if (!shouldShow) {
@@ -20,9 +26,9 @@ const setShouldShowComposeInputKeyboardAware: SetShouldShowComposeInputKeyboardA
         return;
     }
 
-    keyboardDidHideListener = Keyboard.addListener(Platform.OS === 'ios' ? 'keyboardWillHide' : 'keyboardDidHide', () => {
+    keyboardEventListener = Keyboard.addListener(keyboardEvent, () => {
         Composer.setShouldShowComposeInput(true);
-        keyboardDidHideListener?.remove();
+        keyboardEventListener?.remove();
     });
 };
 
