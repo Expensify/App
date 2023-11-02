56ca43fcccecf0e14cf38c497261ca8ae853ee7f
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-02 10:31:01 +0100

    Migrate PressableWithoutFeedback

--- src/components/Pressable/PressableWithoutFeedback.tsx
+++ src/components/Pressable/PressableWithoutFeedback.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import _ from 'underscore';
+import React, {ForwardedRef} from 'react';
+import {View} from 'react-native';
 import GenericPressable from './GenericPressable';
-import GenericPressableProps from './GenericPressable/PropTypes';
+import PressableProps from './GenericPressable/types';
 
-const omittedProps = ['pressStyle', 'hoverStyle', 'focusStyle', 'activeStyle', 'disabledStyle', 'screenReaderActiveStyle', 'shouldUseHapticsOnPress', 'shouldUseHapticsOnLongPress'];
-
-const PressableWithoutFeedback = React.forwardRef((props, ref) => {
-    const propsWithoutStyling = _.omit(props, omittedProps);
+function PressableWithoutFeedback(
+    {pressStyle, hoverStyle, focusStyle, disabledStyle, screenReaderActiveStyle, shouldUseHapticsOnPress, shouldUseHapticsOnLongPress, ...rest}: PressableProps,
+    ref: ForwardedRef<View>,
+) {
     return (
         <GenericPressable
             // eslint-disable-next-line react/jsx-props-no-spreading
-            {...propsWithoutStyling}
+            {...rest}
             ref={ref}
         />
     );
-});
+}
 
 PressableWithoutFeedback.displayName = 'PressableWithoutFeedback';
-PressableWithoutFeedback.propTypes = _.omit(GenericPressableProps.pressablePropTypes, omittedProps);
-PressableWithoutFeedback.defaultProps = _.omit(GenericPressableProps.defaultProps, omittedProps);
 
-export default PressableWithoutFeedback;
+export default React.forwardRef(PressableWithoutFeedback);
