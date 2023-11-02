f0c5f7275a69ac6d2f7e9f740a98a5608f2b46da
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-11-02 10:52:22 -1000

    Revert "fix clipboard in report utils"

--- src/libs/ReportUtils.js
+++ src/libs/ReportUtils.js
@@ -4068,8 +4068,7 @@ function getIOUReportActionDisplayMessage(reportAction) {
     const originalMessage = _.get(reportAction, 'originalMessage', {});
     let displayMessage;
     if (originalMessage.type === CONST.IOU.REPORT_ACTION_TYPE.PAY) {
-        const {IOUReportID} = originalMessage;
-        const {amount, currency} = originalMessage.IOUDetails;
+        const {amount, currency, IOUReportID} = originalMessage;
         const formattedAmount = CurrencyUtils.convertToDisplayString(amount, currency);
         const iouReport = getReport(IOUReportID);
         const payerName = isExpenseReport(iouReport) ? getPolicyName(iouReport) : getDisplayNameForParticipant(iouReport.managerID, true);
