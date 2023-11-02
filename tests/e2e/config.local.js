1196a69d0e1b8a281881789de687baaed80d0221
Oscar Franco <ospfranco@gmail.com>
2023-11-02 11:33:30 +0100

    Fix local path

--- tests/e2e/config.local.js
+++ tests/e2e/config.local.js
@@ -1,6 +1,6 @@
 module.exports = {
     APP_PACKAGE: 'com.expensify.chat.adhoc',
     MAIN_APP_PATH: './android/app/build/outputs/apk/e2e/release/app-e2e-release.apk',
-    DELTA_APP_PATH: './android/app/build/outputs/apk/e2e/release/app-e2edelta-release.apk',
+    DELTA_APP_PATH: './android/app/build/outputs/apk/e2edelta/release/app-e2edelta-release.apk',
     RUNS: 8,
 };
