6ac09c50823b6b9de8f6f7b71b9ab1dea4b8e119
Yauheni Pasiukevich <pasyukevich@live.com>
2023-11-02 14:29:25 +0100

    remove optional for some props

--- src/components/AvatarIconType.ts
+++ src/components/AvatarIconType.ts
@@ -4,10 +4,10 @@ import CONST from '@src/CONST';
 type AvatarFunction = () => void;
 
 type AvatarIcon = {
-    source?: string | AvatarFunction;
-    type?: typeof CONST.ICON_TYPE_AVATAR | typeof CONST.ICON_TYPE_WORKSPACE;
-    name?: string;
-    id?: number | string;
+    source: string | AvatarFunction;
+    type: typeof CONST.ICON_TYPE_AVATAR | typeof CONST.ICON_TYPE_WORKSPACE;
+    name: string;
+    id: number | string;
     fallbackIcon?: string | AvatarFunction;
 };
 
