3a94b35a0ce3dbe05597dc0cb64cadf049407885
OlimpiaZurek <olimpia.zurek@gmail.com>
2023-11-02 09:27:00 +0100

    CR fixes

--- tests/perf-test/ReportActionCompose.perf-test.js
+++ tests/perf-test/ReportActionCompose.perf-test.js
@@ -102,7 +102,7 @@ test('should render Composer with text input interactions', async () => {
     return waitForBatchedUpdates().then(() => measurePerformance(<ReportActionComposeWrapper />, {scenario}));
 });
 
-test('should press composer buttons', async () => {
+test('should press add attachemnt button', async () => {
     const scenario = async () => {
         // Query for the attachment button
         const hintAttachmentButtonText = Localize.translateLocal('reportActionCompose.addAction');
@@ -110,14 +110,26 @@ test('should press composer buttons', async () => {
 
         expect(attachmentButton).toBeDefined();
         fireEvent.press(attachmentButton, mockEvent);
+    };
 
+    return waitForBatchedUpdates().then(() => measurePerformance(<ReportActionComposeWrapper />, {scenario}));
+});
+
+test('should press add emoji button', async () => {
+    const scenario = async () => {
         // Query for the emoji button
         const hintEmojiButtonText = Localize.translateLocal('reportActionCompose.emoji');
         const emojiButton = await screen.findByLabelText(hintEmojiButtonText);
 
         expect(emojiButton).toBeDefined();
         fireEvent.press(emojiButton);
+    };
 
+    return waitForBatchedUpdates().then(() => measurePerformance(<ReportActionComposeWrapper />, {scenario}));
+});
+
+test('should press send message button', async () => {
+    const scenario = async () => {
         // Query for the send button
         const hintSendButtonText = Localize.translateLocal('common.send');
         const sendButton = await screen.findByLabelText(hintSendButtonText);
