932267f3bfc699f68c18a7541a81d2908841ef47
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-11-03 18:12:20 +0100

    fix: rerun jobs

--- src/components/LinearGradient/types.ts
+++ src/components/LinearGradient/types.ts
@@ -1,5 +1,5 @@
 import LinearGradientNative from 'react-native-linear-gradient';
 
 type LinearGradient = typeof LinearGradientNative;
-
+console.log(LinearGradientNative);
 export default LinearGradient;
