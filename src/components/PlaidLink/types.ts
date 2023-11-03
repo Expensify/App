0205ca59215bbbbb36e85fbedff479aab95f3780
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-11-03 09:23:54 +0100

    migrate PlaidLinks native module to TypeScript, create a file for types

---
+++ src/components/PlaidLink/types.ts
@@ -0,0 +1,24 @@
+import {PlaidLinkOnEvent, PlaidLinkOnSuccessMetadata} from 'react-plaid-link';
+
+type PlaidLinkProps = {
+    // Plaid Link SDK public token used to initialize the Plaid SDK
+    token: string;
+
+    // Callback to execute once the user taps continue after successfully entering their account information
+    onSuccess?: (args: {publicToken?: string; metadata: PlaidLinkOnSuccessMetadata}) => void;
+
+    // Callback to execute when there is an error event emitted by the Plaid SDK
+    onError?: (error: ErrorEvent | null) => void;
+
+    // Callback to execute when the user leaves the Plaid widget flow without entering any information
+    onExit?: () => void;
+
+    // Callback to execute whenever a Plaid event occurs
+    onEvent?: PlaidLinkOnEvent;
+
+    // The redirect URI with an OAuth state ID. Needed to re-initialize the PlaidLink after directing the
+    // user to their respective bank platform
+    receivedRedirectURI?: string;
+};
+
+export default PlaidLinkProps;
