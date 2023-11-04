3fe32a314938a4752c680022f2d1d3a5284c5e16
Marc Glasser <marc.aaron.glasser@gmail.com>
2023-11-03 14:45:19 -1000

    Only filter global unreads when in #focus mode. Improve logic

--- src/libs/UnreadIndicatorUpdater/index.js
+++ src/libs/UnreadIndicatorUpdater/index.js
@@ -5,18 +5,36 @@ import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import updateUnread from './updateUnread/index';
 
+let priorityMode = CONST.PRIORITY_MODE.DEFAULT;
+Onyx.connect({
+    key: ONYXKEYS.NVP_PRIORITY_MODE,
+    callback: val => {
+        priorityMode = _.isString(val) ? val : 'default';
+
+        // eslint-disable-next-line no-use-before-define
+        calculate();
+    },
+});
+
+let reportsFromOnyx = {};
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.REPORT,
     waitForCollectionCallback: true,
-    callback: (reportsFromOnyx) => {
-        const unreadReports = _.filter(
-            reportsFromOnyx,
-            (report) =>
-                ReportUtils.isUnread(report) &&
-                // Reports that are archived or have a hidden preference are technically "unread", but should not be counted by the global indicator
-                report.notificationPreference !== CONST.REPORT.NOTIFICATION_PREFERENCE.HIDDEN &&
-                !ReportUtils.isArchivedRoom(report),
-        );
-        updateUnread(_.size(unreadReports));
+    callback: (val) => {
+        reportsFromOnyx = val;
+
+        // eslint-disable-next-line no-use-before-define
+        calculate();
     },
 });
+
+function calculate() {
+    let unreadReports = _.filter(reportsFromOnyx, (report) => ReportUtils.isUnread(report) && report.notificationPreference !== CONST.REPORT.NOTIFICATION_PREFERENCE.HIDDEN);
+
+    // If we are in #focus mode then we don't want to count any archived rooms since they will be hidden
+    if (priorityMode === CONST.PRIORITY_MODE.GSD) {
+        unreadReports = _.without(unreadReports, (report) => ReportUtils.isArchivedRoom(report));
+    }
+
+    updateUnread(_.size(unreadReports));
+}
