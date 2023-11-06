cda8b737fe3e8498b2b34a50b06f4db05d2e4325
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-11-06 12:43:20 +0100

    Relative imports; optional params; types

--- src/components/TextLink.tsx
+++ src/components/TextLink.tsx
@@ -1,10 +1,10 @@
-import _ from 'underscore';
-import React, { ForwardedRef, MouseEventHandler, ReactElement } from 'react';
+import React, { FC, ForwardedRef, KeyboardEventHandler, MouseEventHandler, ReactElement } from 'react';
+import _ from 'lodash';
 import Text from './Text';
-import styles from '../styles/styles';
-import CONST from '../CONST';
-import * as Link from '../libs/actions/Link';
-import { TextStyle, Text as RNText } from 'react-native';
+import styles from '@styles/styles';
+import CONST from '@src/CONST';
+import * as Link from '@userActions/Link';
+import { TextStyle, Text as RNText, GestureResponderEvent } from 'react-native';
 
 type TextLinkProps = {
     /** Link to open in new tab */
@@ -14,20 +14,20 @@ type TextLinkProps = {
     children: ReactElement;
 
     /** Additional style props */
-    style: TextStyle;
+    style?: TextStyle;
 
     /** Overwrites the default link behavior with a custom callback */
-    onPress: () => void;
+    onPress?: () => void;
 
     /** Callback that is called when mousedown is triggered */
-    onMouseDown: MouseEventHandler;
+    onMouseDown?: MouseEventHandler;
 
     /** A ref to forward to text */
     forwardedRef: ForwardedRef<RNText>,
 }
 
 function TextLink({href, children, style, onPress, onMouseDown = (event) => event.preventDefault(), forwardedRef, ...props}: TextLinkProps) {
-    const openLink = (event: Event) => {
+    const openLink = (event: GestureResponderEvent) => {
         event.preventDefault();
         if (onPress) {
             onPress();
@@ -37,11 +37,17 @@ function TextLink({href, children, style, onPress, onMouseDown = (event) => even
         Link.openExternalLink(href);
     };
 
-    const openLinkIfEnterKeyPressed = (event: KeyboardEvent) => {
+    const openLinkIfEnterKeyPressed: KeyboardEventHandler = (event) => {
+        event.preventDefault();
         if (event.key !== 'Enter') {
             return;
         }
-        openLink(event);
+        if (onPress) {
+            onPress();
+            return;
+        }
+
+        Link.openExternalLink(href);
     };
 
     return (
@@ -56,16 +62,20 @@ function TextLink({href, children, style, onPress, onMouseDown = (event) => even
             suppressHighlighting
             {...props}
         >
-            {/* {children} */}
+            {children}
         </Text>
     );
 }
 
 TextLink.displayName = 'TextLink';
 
-export default React.forwardRef<RNText, TextLinkProps>((props, ref) => (
+const TextLinkWithRef: FC<TextLinkProps> = React.forwardRef<RNText, TextLinkProps>((props, ref) => (
     <TextLink
         {...props}
         forwardedRef={ref}
     />
 ));
+
+TextLinkWithRef.displayName = 'TextLinkWithRef';
+
+export default TextLinkWithRef;
