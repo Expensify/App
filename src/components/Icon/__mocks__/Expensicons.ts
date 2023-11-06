38495812786f8c0b1d7460411187897246c2e597
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-11-06 09:40:41 +0100

    apply another fix to jest mock

--- src/components/Icon/__mocks__/Expensicons.ts
+++ src/components/Icon/__mocks__/Expensicons.ts
@@ -1,9 +1,7 @@
 const Expensicons = jest.requireActual('../Expensicons');
 
-module.exports = Object.entries(Expensicons).reduce((prev, curr, key) => {
-    // We set the name of the anonymous mock function here so we can dynamically build the list of mocks and access the
-    // "name" property to use in accessibility hints for element querying
+module.exports = Object.keys(Expensicons).reduce((prev, curr) => {
     const fn = () => '';
-    Object.defineProperty(fn, 'name', {value: key});
-    return {...prev, [key]: fn};
+    Object.defineProperty(fn, 'name', {value: curr});
+    return {...prev, [curr]: fn};
 }, {});
