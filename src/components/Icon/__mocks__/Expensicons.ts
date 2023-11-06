da1441a94ddba87727646c45fb8d1f8d78f1ae09
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-11-06 09:02:57 +0100

    fix jest mock

--- src/components/Icon/__mocks__/Expensicons.ts
+++ src/components/Icon/__mocks__/Expensicons.ts
@@ -1,9 +1,6 @@
-import React from 'react';
-import {SvgProps} from 'react-native-svg';
-
 const Expensicons = jest.requireActual('../Expensicons');
 
-module.exports = (Expensicons as Array<Record<string, React.FC<SvgProps>>>)?.reduce((prev, curr, key) => {
+module.exports = Object.entries(Expensicons).reduce((prev, curr, key) => {
     // We set the name of the anonymous mock function here so we can dynamically build the list of mocks and access the
     // "name" property to use in accessibility hints for element querying
     const fn = () => '';
