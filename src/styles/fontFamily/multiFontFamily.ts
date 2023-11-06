374e876d4a911baa8e695c8a02315c626c8f1eda
Alex Beaman <alexbeaman@expensify.com>
2023-11-06 08:55:43 +0200

    Prettier wanted import order changed?

--- src/styles/fontFamily/multiFontFamily.ts
+++ src/styles/fontFamily/multiFontFamily.ts
@@ -1,7 +1,7 @@
-import CONST from'@src/CONST'
-import getOperatingSystem from '@libs/getOperatingSystem'
-import FontFamilyStyles from './types';
+import getOperatingSystem from '@libs/getOperatingSystem';
+import CONST from '@src/CONST';
 import {multiBold} from './bold';
+import FontFamilyStyles from './types';
 
 // In windows and ubuntu, we need some extra system fonts for emojis to work properly
 // otherwise few of them will appear as black and white
