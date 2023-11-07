3bd396236cc84129b0157b32ca7e17bbbcbe6ca1
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-11-07 09:23:20 +0100

    fix web import

--- src/components/CurrentUserPersonalDetailsSkeletonView/index.tsx
+++ src/components/CurrentUserPersonalDetailsSkeletonView/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import SkeletonViewContentLoader from 'react-content-loader/native';
 import {View} from 'react-native';
 import {Circle, Rect} from 'react-native-svg';
 import {ValueOf} from 'type-fest';
+import SkeletonViewContentLoader from '@components/SkeletonViewContentLoader';
 import styles from '@styles/styles';
 import * as StyleUtils from '@styles/StyleUtils';
 import themeColors from '@styles/themes/default';
