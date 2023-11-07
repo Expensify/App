c97ff978ee258af698628b6a4632074dec451ddb
Getabalew Tesfaye <Getudesta4@gmail.com>
2023-11-07 15:36:17 +0300

    split useSignleExecution for web and native

--- src/hooks/useSingleExecution/index.ts
+++ src/hooks/useSingleExecution/index.ts
@@ -1,16 +1,16 @@
 import {useCallback} from 'react';
 
+type Action<T extends unknown[]> = (...params: T) => void | Promise<void>;
+
 /**
  * This hook was specifically written for native issue
  * more information: https://github.com/Expensify/App/pull/24614 https://github.com/Expensify/App/pull/24173
  * on web we don't need this mechanism so we just call the action directly.
- *
- * @returns {Object}
  */
 export default function useSingleExecution() {
     const singleExecution = useCallback(
-        (action) =>
-            (...params) => {
+        <T extends unknown[]>(action: Action<T>) =>
+            (...params: T) => {
                 action(...params);
             },
         [],
