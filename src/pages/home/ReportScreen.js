354a966aabeac4e1983847c1eb7dbcbeb10dca4d
Hans <visaocuoitroi193@gmail.com>
2023-11-07 23:14:14 +0700

    update function name

--- src/pages/home/ReportScreen.js
+++ src/pages/home/ReportScreen.js
@@ -409,7 +409,7 @@ function ReportScreen({
                                 shouldShowCloseButton
                             />
                         )}
-                        <DragAndDropProvider isDisabled={!isReportReadyForDisplay || ReportUtils.shouldDisableWriteActions(report)}>
+                        <DragAndDropProvider isDisabled={!isReportReadyForDisplay || !ReportUtils.canUserPerformWriteAction(report)}>
                             <View
                                 style={[styles.flex1, styles.justifyContentEnd, styles.overflowHidden]}
                                 onLayout={onListLayout}
