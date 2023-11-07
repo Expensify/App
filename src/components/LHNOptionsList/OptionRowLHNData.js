fac03c4749e47129889af629a7781a078c077d9c
Wojciech Boman <wojciechboman@gmail.com>
2023-11-07 15:26:47 +0100

    Add OptionRowLHNDataReport

--- src/components/LHNOptionsList/OptionRowLHNData.js
+++ src/components/LHNOptionsList/OptionRowLHNData.js
@@ -1,20 +1,13 @@
-import {deepEqual} from 'fast-equals';
-import lodashGet from 'lodash/get';
 import PropTypes from 'prop-types';
-import React, {useEffect, useMemo, useRef} from 'react';
+import React from 'react';
 import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
 import participantPropTypes from '@components/participantPropTypes';
-import compose from '@libs/compose';
-import * as ReportActionsUtils from '@libs/ReportActionsUtils';
-import SidebarUtils from '@libs/SidebarUtils';
-import * as TransactionUtils from '@libs/TransactionUtils';
 import * as UserUtils from '@libs/UserUtils';
-import reportActionPropTypes from '@pages/home/report/reportActionPropTypes';
-import * as Report from '@userActions/Report';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import OptionRowLHN, {defaultProps as baseDefaultProps, propTypes as basePropTypes} from './OptionRowLHN';
+import {defaultProps as baseDefaultProps, propTypes as basePropTypes} from './OptionRowLHN';
+import OptionRowLHNDataReport from './OptionRowLHNDataReport';
 
 const propTypes = {
     /** Whether row should be focused */
@@ -30,24 +23,6 @@ const propTypes = {
     // eslint-disable-next-line react/forbid-prop-types
     fullReport: PropTypes.object,
 
-    /** The policy which the user has access to and which the report could be tied to */
-    policy: PropTypes.shape({
-        /** The ID of the policy */
-        id: PropTypes.string,
-        /** Name of the policy */
-        name: PropTypes.string,
-        /** Avatar of the policy */
-        avatar: PropTypes.string,
-    }),
-
-    /** The actions from the parent report */
-    parentReportActions: PropTypes.objectOf(PropTypes.shape(reportActionPropTypes)),
-
-    /** The transaction from the parent report action */
-    transaction: PropTypes.shape({
-        /** The ID of the transaction */
-        transactionID: PropTypes.string,
-    }),
     ...basePropTypes,
 };
 
@@ -63,66 +38,20 @@ const defaultProps = {
 };
 
 /*
- * This component gets the data from onyx for the actual
- * OptionRowLHN component.
- * The OptionRowLHN component is memoized, so it will only
- * re-render if the data really changed.
+ * This component checks whether a fullReport object is defined for the provided reportID
  */
-function OptionRowLHNData({
-    isFocused,
-    fullReport,
-    reportActions,
-    personalDetails,
-    preferredLocale,
-    comment,
-    policy,
-    receiptTransactions,
-    parentReportActions,
-    transaction,
-    ...propsToForward
-}) {
-    const reportID = propsToForward.reportID;
-
-    const parentReportAction = parentReportActions[fullReport.parentReportActionID];
-
-    const optionItemRef = useRef();
-
-    const linkedTransaction = useMemo(() => {
-        const sortedReportActions = ReportActionsUtils.getSortedReportActionsForDisplay(reportActions);
-        const lastReportAction = _.first(sortedReportActions);
-        return TransactionUtils.getLinkedTransaction(lastReportAction);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [fullReport.reportID, receiptTransactions, reportActions]);
-
-    const optionItem = useMemo(() => {
-        // Note: ideally we'd have this as a dependent selector in onyx!
-        const item = SidebarUtils.getOptionData(fullReport, reportActions, personalDetails, preferredLocale, policy, parentReportAction);
-        if (deepEqual(item, optionItemRef.current)) {
-            return optionItemRef.current;
-        }
-        optionItemRef.current = item;
-        return item;
-        // Listen parentReportAction to update title of thread report when parentReportAction changed
-        // Listen to transaction to update title of transaction report when transaction changed
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [fullReport, linkedTransaction, reportActions, personalDetails, preferredLocale, policy, parentReportAction, transaction]);
-
-    useEffect(() => {
-        if (!optionItem || optionItem.hasDraftComment || !comment || comment.length <= 0 || isFocused) {
-            return;
-        }
-        Report.setReportWithDraft(reportID, true);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-        <OptionRowLHN
+function OptionRowLHNData({isFocused, fullReport, reportActions, personalDetails, preferredLocale, ...propsToForward}) {
+    return !_.isEmpty(fullReport) ? (
+        <OptionRowLHNDataReport
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...propsToForward}
             isFocused={isFocused}
-            optionItem={optionItem}
+            fullReport={fullReport}
+            reportActions={reportActions}
+            personalDetails={personalDetails}
+            preferredLocale={preferredLocale}
         />
-    );
+    ) : null;
 }
 
 OptionRowLHNData.propTypes = propTypes;
@@ -161,47 +90,23 @@ const personalDetailsSelector = (personalDetails) =>
  * use it to prevent re-renders from parent re-renders.
  */
 export default React.memo(
-    compose(
-        withOnyx({
-            comment: {
-                key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT_DRAFT_COMMENT}${reportID}`,
-            },
-            fullReport: {
-                key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT}${reportID}`,
-            },
-            reportActions: {
-                key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`,
-                canEvict: false,
-            },
-            personalDetails: {
-                key: ONYXKEYS.PERSONAL_DETAILS_LIST,
-                selector: personalDetailsSelector,
-            },
-            preferredLocale: {
-                key: ONYXKEYS.NVP_PREFERRED_LOCALE,
-            },
-        }),
-        // eslint-disable-next-line rulesdir/no-multiple-onyx-in-file
-        withOnyx({
-            parentReportActions: {
-                key: ({fullReport}) => `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${fullReport.parentReportID}`,
-                canEvict: false,
-            },
-            policy: {
-                key: ({fullReport}) => `${ONYXKEYS.COLLECTION.POLICY}${fullReport.policyID}`,
-            },
-            // Ideally, we aim to access only the last transaction for the current report by listening to changes in reportActions.
-            // In some scenarios, a transaction might be created after reportActions have been modified.
-            // This can lead to situations where `lastTransaction` doesn't update and retains the previous value.
-            // However, performance overhead of this is minimized by using memos inside the component.
-            receiptTransactions: {key: ONYXKEYS.COLLECTION.TRANSACTION},
-        }),
-        // eslint-disable-next-line rulesdir/no-multiple-onyx-in-file
-        withOnyx({
-            transaction: {
-                key: ({fullReport, parentReportActions}) =>
-                    `${ONYXKEYS.COLLECTION.TRANSACTION}${lodashGet(parentReportActions, [fullReport.parentReportActionID, 'originalMessage', 'IOUTransactionID'], '')}`,
-            },
-        }),
-    )(OptionRowLHNData),
+    withOnyx({
+        comment: {
+            key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT_DRAFT_COMMENT}${reportID}`,
+        },
+        fullReport: {
+            key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT}${reportID}`,
+        },
+        reportActions: {
+            key: ({reportID}) => `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`,
+            canEvict: false,
+        },
+        personalDetails: {
+            key: ONYXKEYS.PERSONAL_DETAILS_LIST,
+            selector: personalDetailsSelector,
+        },
+        preferredLocale: {
+            key: ONYXKEYS.NVP_PREFERRED_LOCALE,
+        },
+    })(OptionRowLHNData),
 );
