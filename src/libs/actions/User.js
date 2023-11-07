0244379fb3ff868a5079ed0be96d2c9474f756de
Carlos Barros <765936+barros001@users.noreply.github.com>
2023-11-06 21:14:28 -0500

    added missing ;

--- src/libs/actions/User.js
+++ src/libs/actions/User.js
@@ -526,7 +526,7 @@ function subscribeToUserEvents() {
         SequentialQueue.getCurrentRequest().then(() => {
             // If we don't have the currentUserAccountID (user is logged out) we don't want to update Onyx with data from Pusher
             if (!currentUserAccountID) {
-                return
+                return;
             }
 
             const onyxUpdatePromise = Onyx.update(pushJSON);
