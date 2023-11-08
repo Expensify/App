425268d9717463be835be76b650250b7a6dd866c
Jack Nam <jack@expensify.com>
2023-11-08 13:39:10 +0700

    Update index.js

--- src/components/InvertedFlatList/index.js
+++ src/components/InvertedFlatList/index.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, {forwardRef, useEffect, useRef} from 'react';
-import {DeviceEventEmitter, FlatList} from 'react-native';
+import {DeviceEventEmitter, FlatList, StyleSheet} from 'react-native';
+import styles from '@styles/styles';
 import CONST from '@src/CONST';
 import BaseInvertedFlatList from './BaseInvertedFlatList';
 
