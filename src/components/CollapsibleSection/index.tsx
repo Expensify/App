0f9ecf598c70763153918a53f445b22471150a56
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-11-08 09:26:56 +0100

    start migrating CollapsibleSection to TypeScript

---
+++ src/components/CollapsibleSection/index.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import Icon from '@components/Icon';
+import * as Expensicons from '@components/Icon/Expensicons';
+import PressableWithFeedback from '@components/Pressable/PressableWithFeedback';
+import Text from '@components/Text';
+import styles from '@styles/styles';
+import CONST from '@src/CONST';
+import ChildrenProps from '@src/types/utils/ChildrenProps';
+import Collapsible from './Collapsible';
+
+type CollapsibleSectionProps = ChildrenProps & {
+    /** Title of the Collapsible section */
+    title: string;
+};
+
+function CollapsibleSection(props: CollapsibleSectionProps) {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    /**
+     * Expands/collapses the section
+     */
+    const toggleSection = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    const src = isExpanded ? Expensicons.UpArrow : Expensicons.DownArrow;
+
+    return (
+        <View style={styles.mt4}>
+            <PressableWithFeedback
+                onPress={toggleSection}
+                style={[styles.pb4, styles.flexRow]}
+                accessibilityRole={CONST.ACCESSIBILITY_ROLE.BUTTON}
+                accessibilityLabel={props.title}
+                hoverDimmingValue={1}
+                pressDimmingValue={0.2}
+            >
+                <Text
+                    selectable={false}
+                    style={[styles.flex1, styles.textStrong]}
+                    dataSet={{[CONST.SELECTION_SCRAPER_HIDDEN_ELEMENT]: true}}
+                >
+                    {props.title}
+                </Text>
+                <Icon src={src} />
+            </PressableWithFeedback>
+            <View style={styles.collapsibleSectionBorder} />
+
+            <Collapsible isOpened={isExpanded}>
+                <View>{props.children}</View>
+            </Collapsible>
+        </View>
+    );
+}
+
+export default CollapsibleSection;
