15a6020f94848a9c49719b6ac2c9ad5b162df463
Trevor Coleman <trevor@trevorcoleman.design>
2023-11-08 14:55:13 -0500

    docs(react-native-share-menu): add JSDoc comments

--- src/hooks/useReactNativeShareMenuData.js
+++ src/hooks/useReactNativeShareMenuData.js
@@ -2,10 +2,21 @@ import {useEffect, useState, useCallback} from 'react';
 import ShareMenu from 'react-native-share-menu';
 import isEmpty from 'lodash/isEmpty';
 
+/**
+ * Custom hook to handle data shared via the React Native Share Menu.
+ * @returns {Object} An object containing the shared data and its MIME type.
+ */
 export default function useReactNativeShareMenuData() {
+    /** @type {null | string} Shared data */
     const [sharedData, setSharedData] = useState(null);
+
+    /** @type {null | string} MIME type of the shared data */
     const [sharedMimeType, setSharedMimeType] = useState(null);
 
+    /**
+     * Callback to handle received share data.
+     * @param {Array} item - The shared item.
+     */
     const handleShare = useCallback((item) => {
         console.log("RECEIVED:")
         console.log(item);
@@ -25,6 +36,7 @@ export default function useReactNativeShareMenuData() {
 
     }, []);
 
+    // Get initial share when component mounts
     useEffect(() => {
         ShareMenu.getInitialShare((...args)=>{
             console.log("GET INITIAL SHARE")
@@ -33,6 +45,7 @@ export default function useReactNativeShareMenuData() {
         });
     }, [handleShare]);
 
+    // Add a listener for new shares
     useEffect(() => {
         const listener = ShareMenu.addNewShareListener((...args) => {
             console.log("SHARE LISTENER")
@@ -40,11 +53,11 @@ export default function useReactNativeShareMenuData() {
             handleShare(args)
         });
 
+        // Clean up listener on unmount
         return () => {
             listener.remove();
         };
     }, [handleShare]);
 
     return {sharedData, sharedMimeType};
-
 }
