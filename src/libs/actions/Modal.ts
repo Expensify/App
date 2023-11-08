d81596b79dd8719bfee550fb5ff85dee738cf4d9
tienifr <christianwen18@gmail.com>
2023-11-08 11:33:59 +0700

    prevent duplicate

--- src/libs/actions/Modal.ts
+++ src/libs/actions/Modal.ts
@@ -9,7 +9,9 @@ let onModalClose: null | (() => void);
  * Allows other parts of the app to call modal close function
  */
 function setCloseModal(onClose: () => void) {
-    closeModals.push(onClose);
+    if (!closeModals.includes(onClose)) {
+        closeModals.push(onClose);
+    }
     return () => {
         const index = closeModals.indexOf(onClose);
         if (index === -1) {
