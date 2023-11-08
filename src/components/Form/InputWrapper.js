580c6895f5f12bf902059942591a1ef34a07baf4
Kamil Owczarz <kamil.owczarz@swmansion.com>
2023-11-08 11:12:57 +0100

    Fix lint

--- src/components/Form/InputWrapper.js
+++ src/components/Form/InputWrapper.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React, {forwardRef, useContext} from 'react';
 import refPropTypes from '@components/refPropTypes';
-import FormContext from "@components/Form/FormContext";
+import FormContext from './FormContext';
 
 const propTypes = {
     InputComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.elementType]).isRequired,
