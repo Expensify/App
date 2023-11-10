c5fee9791ad91365a8b85be26e8692e1e1c6d8fd
Alex Beaman <alexbeaman@expensify.com>
2023-11-10 14:00:10 +0200

    I mean.... fine

--- tests/utils/collections/reports.ts
+++ tests/utils/collections/reports.ts
@@ -1,4 +1,4 @@
-import {rand, randBoolean, randCurrencyCode, randEmail, randWord} from '@ngneat/falso';
+import {rand, randBoolean, randCurrencyCode, randWord} from '@ngneat/falso';
 import CONST from '@src/CONST';
 import type {Report} from '@src/types/onyx';
 
