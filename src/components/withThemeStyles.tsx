a32833d5ec1157bc8bb000890661c785a9569b84
rory <rory@expensify.com>
2023-11-10 10:28:50 -0800

    Migrate withThemeStyles to typescript

---
+++ src/components/withThemeStyles.tsx
@@ -0,0 +1,26 @@
+import React, {ComponentType, ForwardedRef, forwardRef, ReactElement, RefAttributes} from 'react';
+import getComponentDisplayName from '@libs/getComponentDisplayName';
+import type {Styles} from '@styles/styles';
+import useThemeStyles from '@styles/useThemeStyles';
+
+type ThemeStylesProps = {themeStyles: Styles};
+
+export default function withThemeStyles<TProps extends ThemeStylesProps, TRef>(
+    WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>,
+): (props: Omit<TProps, keyof ThemeStylesProps> & React.RefAttributes<TRef>) => ReactElement | null {
+    function WithThemeStyles(props: Omit<TProps, keyof ThemeStylesProps>, ref: ForwardedRef<TRef>): ReactElement {
+        const themeStyles = useThemeStyles();
+        return (
+            <WrappedComponent
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...(props as TProps)}
+                themeStyles={themeStyles}
+                ref={ref}
+            />
+        );
+    }
+
+    WithThemeStyles.displayName = `withThemeStyles(${getComponentDisplayName(WrappedComponent)})`;
+
+    return forwardRef(WithThemeStyles);
+}
