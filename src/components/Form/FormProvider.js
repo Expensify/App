ba8a7400a80af873f3775cc57eba73104e068c94
cdOut <88325488+cdOut@users.noreply.github.com>
2023-11-13 10:45:02 +0100

    pass inputValues through children in FormProvider

--- src/components/Form/FormProvider.js
+++ src/components/Form/FormProvider.js
@@ -320,7 +320,7 @@ function FormProvider({validate, formID, shouldValidateOnBlur, shouldValidateOnC
                 errors={errors}
                 enabledWhenOffline={enabledWhenOffline}
             >
-                {children}
+                {_.isFunction(children) ? children({inputValues}) : children}
             </FormWrapper>
         </FormContext.Provider>
     );
