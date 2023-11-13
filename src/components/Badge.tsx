fa04a3f7702418dc816397a78ba48c155cf0ac91
Fábio Henriques <contact@fabiohenriques.dev>
2023-11-13 09:33:41 -0300

    Migrate file to TS

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,79 +1,67 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import {View} from 'react-native';
+import {GestureResponderEvent, PressableStateCallbackType, StyleProp, TextStyle, View, ViewStyle} from 'react-native';
 import styles from '@styles/styles';
 import * as StyleUtils from '@styles/StyleUtils';
 import CONST from '@src/CONST';
 import PressableWithoutFeedback from './Pressable/PressableWithoutFeedback';
 import Text from './Text';
 
-const propTypes = {
+type BadgeProps = {
     /** Is Success type */
-    success: PropTypes.bool,
+    success?: boolean;
 
     /** Is Error type */
-    error: PropTypes.bool,
+    error?: boolean;
 
     /** Whether badge is clickable */
-    pressable: PropTypes.bool,
+    pressable?: boolean;
 
     /** Text to display in the Badge */
-    text: PropTypes.string.isRequired,
+    text: string;
 
     /** Text to display in the Badge */
-    environment: PropTypes.string,
+    environment?: string;
 
     /** Styles for Badge */
-    // eslint-disable-next-line react/forbid-prop-types
-    badgeStyles: PropTypes.arrayOf(PropTypes.object),
+    badgeStyles?: StyleProp<ViewStyle>;
 
     /** Styles for Badge Text */
-    // eslint-disable-next-line react/forbid-prop-types
-    textStyles: PropTypes.arrayOf(PropTypes.object),
+    textStyles?: StyleProp<TextStyle>;
 
     /** Callback to be called on onPress */
-    onPress: PropTypes.func,
+    onPress: (event?: GestureResponderEvent | KeyboardEvent) => void;
 };
 
-const defaultProps = {
-    success: false,
-    error: false,
-    pressable: false,
-    badgeStyles: [],
-    textStyles: [],
-    onPress: undefined,
-    environment: CONST.ENVIRONMENT.DEV,
-};
+function Badge({success = false, error = false, pressable = false, text, environment = CONST.ENVIRONMENT.DEV, badgeStyles, textStyles, onPress = () => {}}: BadgeProps) {
+    const textColorStyles = success || error ? styles.textWhite : undefined;
+    const Wrapper = pressable ? PressableWithoutFeedback : View;
 
-function Badge(props) {
-    const textStyles = props.success || props.error ? styles.textWhite : undefined;
-    const Wrapper = props.pressable ? PressableWithoutFeedback : View;
-    const wrapperStyles = ({pressed}) => [
+    const wrapperStyles: (state: PressableStateCallbackType) => StyleProp<ViewStyle> = ({pressed}) => [
         styles.badge,
         styles.ml2,
-        StyleUtils.getBadgeColorStyle(props.success, props.error, pressed, props.environment === CONST.ENVIRONMENT.ADHOC),
-        ...props.badgeStyles,
+        StyleUtils.getBadgeColorStyle(success, error, pressed, environment === CONST.ENVIRONMENT.ADHOC),
+        badgeStyles,
     ];
 
     return (
         <Wrapper
-            style={props.pressable ? wrapperStyles : wrapperStyles(false)}
-            onPress={props.onPress}
-            role={props.pressable ? CONST.ACCESSIBILITY_ROLE.BUTTON : CONST.ACCESSIBILITY_ROLE.TEXT}
-            accessibilityLabel={props.pressable ? props.text : undefined}
-            aria-label={!props.pressable ? props.text : undefined}
+            style={pressable ? wrapperStyles : wrapperStyles({focused: false, hovered: false, isDisabled: false, isScreenReaderActive: false, pressed: false})}
+            onPress={onPress}
+            role={pressable ? CONST.ACCESSIBILITY_ROLE.BUTTON : CONST.ACCESSIBILITY_ROLE.TEXT}
+            accessibilityLabel={pressable ? text : undefined}
+            aria-label={!pressable ? text : undefined}
+            accessible={false}
         >
             <Text
-                style={[styles.badgeText, textStyles, ...props.textStyles]}
+                style={[styles.badgeText, textColorStyles, textStyles]}
                 numberOfLines={1}
             >
-                {props.text}
+                {text}
             </Text>
         </Wrapper>
     );
 }
 
 Badge.displayName = 'Badge';
-Badge.propTypes = propTypes;
-Badge.defaultProps = defaultProps;
+
 export default Badge;
