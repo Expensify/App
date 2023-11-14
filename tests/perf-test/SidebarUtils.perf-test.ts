e58877e54f03e9ee1e8de6ceea6b8084d33b3536
OlimpiaZurek <olimpia.zurek@gmail.com>
2023-11-14 12:52:16 +0100

    cr fixes

--- tests/perf-test/SidebarUtils.perf-test.ts
+++ tests/perf-test/SidebarUtils.perf-test.ts
@@ -29,17 +29,17 @@ afterEach(() => {
 
 const getMockedReports = (length = 500) =>
     createCollection<Report>(
-        (item) => `report_${item.reportID}`,
+        (item) => `${ONYXKEYS.COLLECTION.REPORT}${item.reportID}`,
         (index) => createRandomReport(index),
         length,
-    ) as Partial<Report>;
+    );
 
 const reportActions = createCollection<ReportAction>(
     (item) => `${item.reportActionID}`,
     (index) => createRandomReportAction(index),
 );
 
-const mockedResponseMap = getMockedReports(10000);
+const mockedResponseMap: Partial<Report> = getMockedReports(10000);
 
 test('getOptionData on 10k reports', async () => {
     const report = createRandomReport(1);
@@ -53,34 +53,41 @@ test('getOptionData on 10k reports', async () => {
     });
 
     await waitForBatchedUpdates();
-    await measureFunction(() => SidebarUtils.getOptionData(report, reportActions, personalDetails, preferredLocale, policy, parentReportAction), {runs: 20});
+    await measureFunction(() => SidebarUtils.getOptionData(report, reportActions, personalDetails, preferredLocale, policy, parentReportAction));
 });
 
 test('getOrderedReportIDs on 10k reports', async () => {
     const currentReportId = '1';
-    const allReports = getMockedReports() as Record<string, Report>;
+    const allReports = getMockedReports();
     const betas = [CONST.BETAS.DEFAULT_ROOMS, CONST.BETAS.POLICY_ROOMS];
 
     const policies = createCollection<Policy>(
-        (item) => `policy_${item.id}`,
+        (item) => `${ONYXKEYS.COLLECTION.POLICY}${item.id}`,
         (index) => createRandomPolicy(index),
     );
 
-    const allReportActions = Object.values(reportActions).map((reportAction) => ({
-        errors: reportAction.errors ?? [],
-        message: [
-            {
-                moderationDecision: {
-                    decision: reportAction.message?.[0]?.moderationDecision?.decision,
+    const allReportActions = Object.fromEntries(
+        Object.keys(reportActions).map((key) => [
+            key,
+            [
+                {
+                    errors: reportActions[key].errors ?? [],
+                    message: [
+                        {
+                            moderationDecision: {
+                                decision: reportActions[key].message?.[0]?.moderationDecision?.decision,
+                            },
+                        },
+                    ],
                 },
-            },
-        ],
-    })) as unknown as Record<string, ReportAction[]>;
+            ],
+        ]),
+    ) as Record<string, ReportAction[]>;
 
     Onyx.multiSet({
         ...mockedResponseMap,
     });
 
     await waitForBatchedUpdates();
-    await measureFunction(() => SidebarUtils.getOrderedReportIDs(currentReportId, allReports, betas, policies, CONST.PRIORITY_MODE.DEFAULT, allReportActions), {runs: 20});
+    await measureFunction(() => SidebarUtils.getOrderedReportIDs(currentReportId, allReports, betas, policies, CONST.PRIORITY_MODE.DEFAULT, allReportActions));
 });
