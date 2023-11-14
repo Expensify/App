4f0b20e41e1eeebfe444a070efa30e63aec83f03
Andrew Rosiclair <arosiclair@expensify.com>
2023-11-14 18:27:02 -0500

    log BugBot alerts when errors are thrown

--- src/libs/Notification/PushNotification/backgroundRefresh/index.android.js
+++ src/libs/Notification/PushNotification/backgroundRefresh/index.android.js
@@ -1,6 +1,8 @@
 import Onyx from 'react-native-onyx';
+import Log from '@libs/Log';
 import Visibility from '@libs/Visibility';
 import * as App from '@userActions/App';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 
 function getLastOnyxUpdateID() {
@@ -26,15 +28,19 @@ export default function backgroundRefresh() {
         return;
     }
 
-    getLastOnyxUpdateID().then((lastUpdateID) => {
-        /**
-         * ReconnectApp waits on the isReadyToOpenApp promise to resolve and this normally only resolves when the LHN is rendered.
-         * However on Android, this callback is run in the background using a Headless JS task which does not render the React UI,
-         * so we must manually run confirmReadyToOpenApp here instead.
-         *
-         * See more here: https://reactnative.dev/docs/headless-js-android
-         */
-        App.confirmReadyToOpenApp();
-        App.reconnectApp(lastUpdateID);
-    });
+    getLastOnyxUpdateID()
+        .then((lastUpdateID) => {
+            /**
+             * ReconnectApp waits on the isReadyToOpenApp promise to resolve and this normally only resolves when the LHN is rendered.
+             * However on Android, this callback is run in the background using a Headless JS task which does not render the React UI,
+             * so we must manually run confirmReadyToOpenApp here instead.
+             *
+             * See more here: https://reactnative.dev/docs/headless-js-android
+             */
+            App.confirmReadyToOpenApp();
+            App.reconnectApp(lastUpdateID);
+        })
+        .catch((error) => {
+            Log.alert(`${CONST.ERROR.ENSURE_BUGBOT} [PushNotification] backgroundRefresh failed. This should never happen.`, {error});
+        });
 }
