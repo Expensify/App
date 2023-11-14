c83e4d06fe5b08886b6c8ce62a6a0902b92aa3ba
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-11-14 11:42:19 +0100

    fix: correct type in Permissions lib

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -1,28 +1,29 @@
+import {OnyxEntry} from 'react-native-onyx';
 import CONST from '@src/CONST';
 import Beta from '@src/types/onyx/Beta';
 
-function canUseAllBetas(betas: Beta[]): boolean {
-    return betas.includes(CONST.BETAS.ALL);
+function canUseAllBetas(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.ALL);
 }
 
-function canUseChronos(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.CHRONOS_IN_CASH) || canUseAllBetas(betas);
+function canUseChronos(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.CHRONOS_IN_CASH) || canUseAllBetas(betas);
 }
 
-function canUsePayWithExpensify(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.PAY_WITH_EXPENSIFY) || canUseAllBetas(betas);
+function canUsePayWithExpensify(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.PAY_WITH_EXPENSIFY) || canUseAllBetas(betas);
 }
 
-function canUseDefaultRooms(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.DEFAULT_ROOMS) || canUseAllBetas(betas);
+function canUseDefaultRooms(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.DEFAULT_ROOMS) || canUseAllBetas(betas);
 }
 
-function canUseWallet(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.BETA_EXPENSIFY_WALLET) || canUseAllBetas(betas);
+function canUseWallet(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.BETA_EXPENSIFY_WALLET) || canUseAllBetas(betas);
 }
 
-function canUseCommentLinking(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.BETA_COMMENT_LINKING) || canUseAllBetas(betas);
+function canUseCommentLinking(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.BETA_COMMENT_LINKING) || canUseAllBetas(betas);
 }
 
 /**
@@ -30,24 +31,24 @@ function canUseCommentLinking(betas: Beta[]): boolean {
  * since contributors have been reporting a number of false issues related to the feature being under development.
  * See https://expensify.slack.com/archives/C01GTK53T8Q/p1641921996319400?thread_ts=1641598356.166900&cid=C01GTK53T8Q
  */
-function canUsePolicyRooms(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.POLICY_ROOMS) || canUseAllBetas(betas);
+function canUsePolicyRooms(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.POLICY_ROOMS) || canUseAllBetas(betas);
 }
 
-function canUseTasks(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.TASKS) || canUseAllBetas(betas);
+function canUseTasks(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.TASKS) || canUseAllBetas(betas);
 }
 
-function canUseCustomStatus(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.CUSTOM_STATUS) || canUseAllBetas(betas);
+function canUseCustomStatus(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.CUSTOM_STATUS) || canUseAllBetas(betas);
 }
 
-function canUseTags(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.NEW_DOT_TAGS) || canUseAllBetas(betas);
+function canUseTags(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.NEW_DOT_TAGS) || canUseAllBetas(betas);
 }
 
-function canUseViolations(betas: Beta[]): boolean {
-    return betas?.includes(CONST.BETAS.VIOLATIONS) || canUseAllBetas(betas);
+function canUseViolations(betas: OnyxEntry<Beta[]>): boolean {
+    return !!betas?.includes(CONST.BETAS.VIOLATIONS) || canUseAllBetas(betas);
 }
 
 /**
