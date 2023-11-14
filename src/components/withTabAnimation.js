8353d56168808a55fe29a999ff3c4900f1378707
dukenv0307 <dukenv0307@gmail.com>
2023-11-14 11:22:00 +0700

    fix forwardedRef warning

--- src/components/withTabAnimation.js
+++ src/components/withTabAnimation.js
@@ -1,11 +1,20 @@
 import {useTabAnimation} from '@react-navigation/material-top-tabs';
 import PropTypes from 'prop-types';
 import * as React from 'react';
+import _ from 'underscore';
 import getComponentDisplayName from '@libs/getComponentDisplayName';
 
 const propTypes = {
+    /** The HOC takes an optional ref as a prop and passes it as a ref to the wrapped component.
+     * That way, if a ref is passed to a component wrapped in the HOC, the ref is a reference to the wrapped component, not the HOC. */
+    forwardedRef: PropTypes.func,
+
     /* Whether we're in a tab navigator */
     isInTabNavigator: PropTypes.bool.isRequired,
+};
+
+const defaultProps = {
+    forwardedRef: () => {},
 };
 
 export default function (WrappedComponent) {
@@ -23,27 +32,40 @@ export default function (WrappedComponent) {
 
     WrappedComponentWithTabAnimation.displayName = `withAnimation(${getComponentDisplayName(WrappedComponent)})`;
 
-    function WithTabAnimation(props, ref) {
+    function WithTabAnimation(props) {
+        const rest = _.omit(props, ['forwardedRef']);
         if (props.isInTabNavigator) {
             return (
                 <WrappedComponentWithTabAnimation
                     // eslint-disable-next-line react/jsx-props-no-spreading
-                    {...props}
-                    ref={ref}
+                    {...rest}
+                    ref={props.forwardedRef}
                 />
             );
         }
         return (
             <WrappedComponent
                 // eslint-disable-next-line react/jsx-props-no-spreading
-                {...props}
-                ref={ref}
+                {...rest}
+                ref={props.forwardedRef}
             />
         );
     }
 
     WithTabAnimation.propTypes = propTypes;
+    WithTabAnimation.defaultProps = defaultProps;
     WithTabAnimation.displayName = `withTabAnimation(${getComponentDisplayName(WrappedComponent)})`;
 
-    return React.forwardRef(WithTabAnimation);
+    // eslint-disable-next-line rulesdir/no-negated-variables
+    const WithTabAnimationWithRef = React.forwardRef((props, ref) => (
+        <WithTabAnimation
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+            forwardedRef={ref}
+        />
+    ));
+
+    WithTabAnimationWithRef.displayName = 'WithTabAnimationWithRef';
+
+    return WithTabAnimation;
 }
