627b85a92a1426f17f06a472d35d7bdb560aca8b
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-11-15 18:29:44 +0100

    fix: resolve comments

--- src/components/FormElement.tsx
+++ src/components/FormElement.tsx
@@ -2,11 +2,11 @@ import React, {ForwardedRef, forwardRef} from 'react';
 import {View, ViewProps} from 'react-native';
 import * as ComponentUtils from '@libs/ComponentUtils';
 
-function FormElement(props: ViewProps, ref: ForwardedRef<View | HTMLFormElement | null>) {
+function FormElement(props: ViewProps, ref: ForwardedRef<View>) {
     return (
         <View
             role={ComponentUtils.ACCESSIBILITY_ROLE_FORM}
-            ref={ref as ForwardedRef<View>}
+            ref={ref}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
         />
