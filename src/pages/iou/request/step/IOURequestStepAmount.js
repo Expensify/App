b1305b3027aaaf9fe20fdcb318b8417067121cda
Tim Golen <tgolen@expensify.com>
2023-11-15 10:57:20 -0700

    Create HOC to check permissions

--- src/pages/iou/request/step/IOURequestStepAmount.js
+++ src/pages/iou/request/step/IOURequestStepAmount.js
@@ -3,12 +3,11 @@ import lodashGet from 'lodash/get';
 import React, {useCallback, useRef} from 'react';
 import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
-import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import transactionPropTypes from '@components/transactionPropTypes';
 import useLocalize from '@hooks/useLocalize';
+import compose from '@libs/compose';
 import * as CurrencyUtils from '@libs/CurrencyUtils';
 import Navigation from '@libs/Navigation/Navigation';
-import * as ReportUtils from '@libs/ReportUtils';
 import MoneyRequestAmountForm from '@pages/iou/steps/MoneyRequestAmountForm';
 import reportPropTypes from '@pages/reportPropTypes';
 import * as IOU from '@userActions/IOU';
@@ -17,6 +16,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import IOURequestStepRoutePropTypes from './IOURequestStepRoutePropTypes';
 import StepScreenWrapper from './StepScreenWrapper';
+import withWritableReportOrNotFound from './withWritableReportOrNotFound';
 
 const propTypes = {
     /** Navigation route context info provided by react navigation */
@@ -58,12 +58,6 @@ function IOURequestStepAmount({
             };
         }, []),
     );
-
-    const iouTypeParamIsInvalid = !_.contains(_.values(CONST.IOU.TYPE), iouType);
-    const canUserPerformWriteAction = ReportUtils.canUserPerformWriteAction(report);
-    if (iouTypeParamIsInvalid || !canUserPerformWriteAction) {
-        return <FullPageNotFoundView shouldShow />;
-    }
 
     const navigateBack = () => {
         Navigation.goBack(backTo || ROUTES.HOME);
@@ -122,11 +116,14 @@ IOURequestStepAmount.propTypes = propTypes;
 IOURequestStepAmount.defaultProps = defaultProps;
 IOURequestStepAmount.displayName = 'IOURequestStepAmount';
 
-export default withOnyx({
-    report: {
-        key: ({route}) => `${ONYXKEYS.COLLECTION.REPORT}${lodashGet(route, 'params.reportID', '0')}`,
-    },
-    transaction: {
-        key: ({route}) => `${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${lodashGet(route, 'params.transactionID', '0')}`,
-    },
-})(IOURequestStepAmount);
+export default compose(
+    withWritableReportOrNotFound,
+    withOnyx({
+        report: {
+            key: ({route}) => `${ONYXKEYS.COLLECTION.REPORT}${lodashGet(route, 'params.reportID', '0')}`,
+        },
+        transaction: {
+            key: ({route}) => `${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${lodashGet(route, 'params.transactionID', '0')}`,
+        },
+    }),
+)(IOURequestStepAmount);
