479dbbf652c2f9c39749208eca6a4012d9d486fc
Christoph Pader <christoph@margelo.io>
2023-11-15 17:28:14 +0100

    fix: improve zoom range on Android

---
+++ src/components/Attachments/AttachmentView/AttachmentViewImage/Constants/index.android.js
@@ -0,0 +1,13 @@
+import {DEFAULT_MAX_ZOOM_SCALE, DEFAULT_MIN_ZOOM_SCALE} from '@components/MultiGestureCanvas/Constants';
+
+const modalZoomRange = {
+    min: DEFAULT_MIN_ZOOM_SCALE,
+    max: DEFAULT_MAX_ZOOM_SCALE * 1.135, // => to 22.7; tested to be the same perceived zoom scale compared to the attachment carousel on Android
+};
+
+const carouselZoomRange = {
+    min: DEFAULT_MIN_ZOOM_SCALE,
+    max: DEFAULT_MAX_ZOOM_SCALE,
+};
+
+export {modalZoomRange, carouselZoomRange};
