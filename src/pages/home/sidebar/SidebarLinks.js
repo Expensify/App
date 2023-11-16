d570eb110d327cef92fb344f37a1f158152ea38c
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-11-16 16:20:47 +0100

    add currentReportID to extraData

--- src/pages/home/sidebar/SidebarLinks.js
+++ src/pages/home/sidebar/SidebarLinks.js
@@ -1,7 +1,7 @@
 /* eslint-disable rulesdir/onyx-props-must-have-default */
 import PropTypes from 'prop-types';
 import React, {useCallback, useEffect, useRef} from 'react';
-import {InteractionManager, View, StyleSheet} from 'react-native';
+import {InteractionManager, StyleSheet, View} from 'react-native';
 import _ from 'underscore';
 import LogoComponent from '@assets/images/expensify-wordmark.svg';
 import Header from '@components/Header';
