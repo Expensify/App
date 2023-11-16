80afdf3bee727280d313dc9acc2545725b43dc00
Christoph Pader <christoph.pader@outlook.com>
2023-11-16 17:12:05 +0100

    feat: add useThemeIllustrations hook and context

---
+++ src/styles/illustrations/ThemeIllustrationsContext.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as DarkIllustrations from './dark';
+
+const ThemeIllustrationsContext = React.createContext(DarkIllustrations);
+
+export default ThemeIllustrationsContext;
