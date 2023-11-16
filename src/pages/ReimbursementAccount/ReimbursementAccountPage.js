868a1adc4a8a73e079f53b055d20c315fbee68f7
keisyrzk <krisstoferos@gmail.com>
2023-11-16 14:45:34 +0100

    visual updates

--- src/pages/ReimbursementAccount/ReimbursementAccountPage.js
+++ src/pages/ReimbursementAccount/ReimbursementAccountPage.js
@@ -138,11 +138,13 @@ function getStepToOpenFromRouteParams(route) {
 }
 
 function ReimbursementAccountPage({reimbursementAccount, route, onfidoToken, policy, account, isLoadingReportData, session, plaidLinkToken, plaidCurrentEvent, reimbursementAccountDraft}) {
-    // The SetupWithdrawalAccount flow allows us to continue the flow from various points depending on where the
-    // user left off. This view will refer to the achData as the single source of truth to determine which route to
-    // display. We can also specify a specific route to navigate to via route params when the component first
-    // mounts which will set the achData.currentStep after the account data is fetched and overwrite the logical
-    // next step.
+    /**  
+        The SetupWithdrawalAccount flow allows us to continue the flow from various points depending on where the
+        user left off. This view will refer to the achData as the single source of truth to determine which route to
+        display. We can also specify a specific route to navigate to via route params when the component first
+        mounts which will set the achData.currentStep after the account data is fetched and overwrite the logical
+        next step.
+    */
     const achData = lodashGet(reimbursementAccount, 'achData', {});
 
     /**
@@ -170,7 +172,7 @@ function ReimbursementAccountPage({reimbursementAccount, route, onfidoToken, pol
     /**
      * @param {Array} fieldNames
      *
-     * @returns {*}
+     * @returns {Object}
      */
     function getBankAccountFields(fieldNames) {
         return {
@@ -234,12 +236,14 @@ function ReimbursementAccountPage({reimbursementAccount, route, onfidoToken, pol
         return achData.state === BankAccount.STATE.PENDING || _.contains([CONST.BANK_ACCOUNT.STEP.BANK_ACCOUNT, ''], getStepToOpenFromRouteParams(route));
     }
 
-    // When this page is first opened, `reimbursementAccount` prop might not yet be fully loaded from Onyx
-    // or could be partially loaded such that `reimbursementAccount.achData.currentStep` is unavailable.
-    // Calculating `shouldShowContinueSetupButton` immediately on initial render doesn't make sense as
-    // it relies on complete data. Thus, we should wait to calculate it until we have received
-    // the full `reimbursementAccount` data from the server. This logic is handled within the useEffect hook,
-    // which acts similarly to `componentDidUpdate` when the `reimbursementAccount` dependency changes.
+    /** 
+        When this page is first opened, `reimbursementAccount` prop might not yet be fully loaded from Onyx
+        or could be partially loaded such that `reimbursementAccount.achData.currentStep` is unavailable.
+        Calculating `shouldShowContinueSetupButton` immediately on initial render doesn't make sense as
+        it relies on complete data. Thus, we should wait to calculate it until we have received
+        the full `reimbursementAccount` data from the server. This logic is handled within the useEffect hook,
+        which acts similarly to `componentDidUpdate` when the `reimbursementAccount` dependency changes.
+     */
     const [hasACHDataBeenLoaded, setHasACHDataBeenLoaded] = useState(
         reimbursementAccount !== ReimbursementAccountProps.reimbursementAccountDefaultProps && _.has(reimbursementAccount, 'achData.currentStep'),
     );
