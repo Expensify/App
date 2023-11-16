d5c96b3f54efa2882825fd59dc75a3f747cf8430
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-16 12:02:57 +0100

    [TS migration] Migrate 'withPolicyAndFullscreenLoading.js' HOC to TypeScript

---
+++ src/pages/workspace/withPolicyAndFullscreenLoading.tsx
@@ -0,0 +1,44 @@
+import isEmpty from 'lodash/isEmpty';
+import React, {ComponentType, ForwardedRef, forwardRef, RefAttributes} from 'react';
+import {OnyxEntry, withOnyx} from 'react-native-onyx';
+import FullscreenLoadingIndicator from '@components/FullscreenLoadingIndicator';
+import compose from '@libs/compose';
+import ONYXKEYS from '@src/ONYXKEYS';
+import withPolicy, {WithPolicyProps} from './withPolicy';
+
+type WithPolicyAndFullscreenLoadingOnyxProps = {
+    /** Indicated whether the report data is loading */
+    isLoadingReportData: OnyxEntry<boolean>;
+};
+
+type WithPolicyAndFullscreenLoadingProps = WithPolicyProps & WithPolicyAndFullscreenLoadingOnyxProps;
+
+// : React.ComponentType<Omit<TProps, keyof WithPolicyOnyxProps>>
+export default function withPolicyAndFullscreenLoading<TProps extends WithPolicyAndFullscreenLoadingProps, TRef>(WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>) {
+    function WithPolicyAndFullscreenLoading(props: TProps, ref: ForwardedRef<TRef>) {
+        const isLoadingReportData = props.isLoadingReportData ?? true;
+
+        if (isLoadingReportData && isEmpty(props.policy) && isEmpty(props.policyDraft)) {
+            return <FullscreenLoadingIndicator />;
+        }
+
+        return (
+            <WrappedComponent
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                {...props}
+                ref={ref}
+            />
+        );
+    }
+
+    WithPolicyAndFullscreenLoading.displayName = `WithPolicyAndFullscreenLoading`;
+
+    return compose(
+        withOnyx<TProps & RefAttributes<TRef>, WithPolicyAndFullscreenLoadingOnyxProps>({
+            isLoadingReportData: {
+                key: ONYXKEYS.IS_LOADING_REPORT_DATA,
+            },
+        }),
+        withPolicy,
+    )(forwardRef(WithPolicyAndFullscreenLoading));
+}
