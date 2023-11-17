e2aaa6a1707a114d2b0c075c09cf372f99c55a67
Trevor Coleman <trevor@trevorcoleman.design>
2023-11-17 16:35:16 -0500

    feat(Violations): create possibleViolationsByField lookup

---
+++ src/libs/Violations/possibleViolationsByField.ts
@@ -0,0 +1,52 @@
+import {ViolationName} from '@src/types/onyx';
+import invertBy from 'lodash/invertBy';
+
+
+/**
+ * Map from Violation Names to the field where that violation can occur
+ */
+const violationFields: Record<ViolationName, ViolationField> = {
+    perDayLimit: 'amount',
+    maxAge: 'date',
+    overLimit: 'amount',
+    overLimitAttendee: 'amount',
+    overCategoryLimit: 'amount',
+    receiptRequired: 'receipt',
+    missingCategory: 'category',
+    categoryOutOfPolicy: 'category',
+    missingTag: 'tag',
+    tagOutOfPolicy: 'tag',
+    missingComment: 'comment',
+    taxRequired: 'tax',
+    taxOutOfPolicy: 'tax',
+    billableExpense: 'billable',
+};
+
+/**
+ * Names of Fields where violations can occur
+ */
+type ViolationField =
+    | 'merchant'
+    | 'amount'
+    | 'category'
+    | 'date'
+    | 'tag'
+    | 'comment'
+    | 'billable'
+    | 'receipt'
+    | 'tax';
+
+/**
+ * Map from field name to array of violation types that can occur on that field.
+ * @example
+ * {
+ *   // ...
+ *   category: ['missingCategory', 'categoryOutOfPolicy']
+ *   // ...
+ * }
+ */
+const possibleViolationsByField = invertBy(violationFields, (value) => value) as Record<ViolationField, ViolationName[]>;
+
+
+export default possibleViolationsByField
+export type {ViolationField}
