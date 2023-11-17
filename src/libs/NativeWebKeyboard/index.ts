b7043422635b915c8172da633ec4cb4092ff9879
Lucas Mórawski <lukasz.morawski@callstack.com>
2023-11-17 15:55:37 +0100

    added comments to exported functions on NativeWebKeyboard

--- src/libs/NativeWebKeyboard/index.ts
+++ src/libs/NativeWebKeyboard/index.ts
@@ -86,9 +86,51 @@ const addListener = (eventName: 'keyboardDidShow' | 'keyboardDidHide', callbackF
 };
 
 export default {
+    /**
+     * Whether the keyboard is last known to be visible.
+     */
     isVisible,
+    /**
+     * Dismisses the active keyboard and removes focus.
+     */
     dismiss: Keyboard.dismiss,
+    /**
+     * The `addListener` function connects a JavaScript function to an identified native
+     * keyboard notification event.
+     *
+     * This function then returns the reference to the listener.
+     *
+     * {string} eventName The `nativeEvent` is the string that identifies the event you're listening for.  This
+     * can be any of the following:
+     *
+     * - `keyboardWillShow`
+     * - `keyboardDidShow`
+     * - `keyboardWillHide`
+     * - `keyboardDidHide`
+     * - `keyboardWillChangeFrame`
+     * - `keyboardDidChangeFrame`
+     *
+     * Note that if you set `android:windowSoftInputMode` to `adjustResize`  or `adjustNothing`,
+     * only `keyboardDidShow` and `keyboardDidHide` events will be available on Android.
+     * `keyboardWillShow` as well as `keyboardWillHide` are generally not available on Android
+     * since there is no native corresponding event.
+     *
+     * On Web only two events are available:
+     *
+     * - `keyboardDidShow`
+     * - `keyboardDidHide`
+     *
+     * {function} callback function to be called when the event fires.
+     */
     addListener,
+    /**
+     * Useful for syncing TextInput (or other keyboard accessory view) size of
+     * position changes with keyboard movements.
+     * Not working on web.
+     */
     scheduleLayoutAnimation: nullFn,
+    /**
+     * Return the metrics of the soft-keyboard if visible. Currently not working on web.
+     */
     metrics: nullFn,
 };
