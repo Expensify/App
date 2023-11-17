f0a8415df5c5708c41d54b9e24ff6414f9078300
Monil Bhavsar <monil@expensify.com>
2023-11-17 17:53:33 +0530

    Pass policyID for workspace avatar

--- src/components/MultipleAvatars.tsx
+++ src/components/MultipleAvatars.tsx
@@ -174,8 +174,7 @@ function MultipleAvatars({
                     >
                         {[...avatars].splice(0, maxAvatarsInRow).map((icon, index) => (
                             <UserDetailsTooltip
-                                /* eslint-disable-next-line react/no-array-index-key */
-                                key={`stackedAvatars-${index}`}
+                                key={`stackedAvatars-${icon.id}`}
                                 accountID={icon.id}
                                 icon={icon}
                                 fallbackUserDetails={{
