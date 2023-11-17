807ef899c7ffeb81aa3f7d11b2727593cb04cd56
dukenv0307 <dukenv0307@gmail.com>
2023-11-17 12:09:54 +0700

    fix lint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement } from 'react';
-import { StyleProp, TextStyle, View } from 'react-native';
+import React, {ReactElement} from 'react';
+import {StyleProp, TextStyle, View} from 'react-native';
 import useThemeStyles from '@styles/useThemeStyles';
 import EnvironmentBadge from './EnvironmentBadge';
 import Text from './Text';
@@ -18,31 +18,31 @@ type HeaderProps = {
     textStyles?: StyleProp<TextStyle>;
 };
 
-function Header({ title = '', subtitle = '', textStyles = [], shouldShowEnvironmentBadge = false }: HeaderProps) {
+function Header({title = '', subtitle = '', textStyles = [], shouldShowEnvironmentBadge = false}: HeaderProps) {
     const styles = useThemeStyles();
     return (
         <View style={[styles.flex1, styles.flexRow]}>
             <View style={styles.w100}>
                 {typeof title === 'string'
                     ? Boolean(title) && (
-                        <Text
-                            numberOfLines={2}
-                            style={[styles.headerText, styles.textLarge, textStyles]}
-                        >
-                            {title}
-                        </Text>
-                    )
+                          <Text
+                              numberOfLines={2}
+                              style={[styles.headerText, styles.textLarge, textStyles]}
+                          >
+                              {title}
+                          </Text>
+                      )
                     : title}
                 {/* If there's no subtitle then display a fragment to avoid an empty space which moves the main title */}
                 {typeof subtitle === 'string'
                     ? Boolean(subtitle) && (
-                        <Text
-                            style={[styles.mutedTextLabel, styles.pre]}
-                            numberOfLines={1}
-                        >
-                            {subtitle}
-                        </Text>
-                    )
+                          <Text
+                              style={[styles.mutedTextLabel, styles.pre]}
+                              numberOfLines={1}
+                          >
+                              {subtitle}
+                          </Text>
+                      )
                     : subtitle}
             </View>
             {shouldShowEnvironmentBadge && <EnvironmentBadge />}
