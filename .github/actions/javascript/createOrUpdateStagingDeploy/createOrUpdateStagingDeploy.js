cf71429aef87fa48d1a3fb82212604f46a5c4147
rory <rory@expensify.com>
2023-11-17 11:15:52 -0800

    Simplify didVersionChange

--- .github/actions/javascript/createOrUpdateStagingDeploy/createOrUpdateStagingDeploy.js
+++ .github/actions/javascript/createOrUpdateStagingDeploy/createOrUpdateStagingDeploy.js
@@ -93,7 +93,7 @@ async function run() {
                 });
             }
 
-            const didVersionChange = newVersionTag ? newVersionTag !== currentChecklistData.tag : false;
+            const didVersionChange = newVersionTag !== currentChecklistData.tag;
             checklistBody = await GithubUtils.generateStagingDeployCashBody(
                 newVersionTag,
                 _.pluck(PRList, 'url'),
