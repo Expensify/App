d8acbd9b11864de410df5772475af88cf79888bd
Viktoryia Kliushun <vikstash@gmail.com>
2023-11-17 14:44:31 +0100

    Fix TS issues

--- src/components/OfflineIndicator.tsx
+++ src/components/OfflineIndicator.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
 import {StyleProp, View, ViewStyle} from 'react-native';
 import {OnyxEntry} from 'react-native-onyx/lib/types';
-import compose from '@libs/compose';
 import styles from '@styles/styles';
 import variables from '@styles/variables';
 import type {Network} from '@src/types/onyx';
 import Icon from './Icon';
 import * as Expensicons from './Icon/Expensicons';
+import type {LocaleContextProps} from './LocaleContextProvider';
 import {withNetwork} from './OnyxProvider';
 import Text from './Text';
 import withLocalize from './withLocalize';
 import withWindowDimensions from './withWindowDimensions';
+import type {WindowDimensionsProps} from './withWindowDimensions/types';
 
-type OfflineIndicatorProps = {
-    /** Information about the network */
-    network: OnyxEntry<Network>;
+type OfflineIndicatorProps = LocaleContextProps &
+    WindowDimensionsProps & {
+        /** Information about the network */
+        network: OnyxEntry<Network>;
 
-    /** Optional styles for container element that will override the default styling for the offline indicator */
-    containerStyles?: StyleProp<ViewStyle>;
+        /** Optional styles for container element that will override the default styling for the offline indicator */
+        containerStyles?: StyleProp<ViewStyle>;
 
-    /** Optional styles for the container */
-    style?: StyleProp<ViewStyle>;
-
-    /** Is the window width narrow, like on a mobile device */
-    isSmallScreenWidth: boolean;
-
-    // TODO: remove after withLocalize migrated
-    /** Returns translated string for given locale and phrase */
-    translate: (value: string) => string;
-};
+        /** Optional styles for the container */
+        style?: StyleProp<ViewStyle>;
+    };
 
 const setStyles = (containerStyles: StyleProp<ViewStyle>, isSmallScreenWidth: boolean): StyleProp<ViewStyle> => {
     if (!!containerStyles && ((Array.isArray(containerStyles) && containerStyles.length) || Object.keys(containerStyles).length)) {
@@ -38,7 +33,7 @@ const setStyles = (containerStyles: StyleProp<ViewStyle>, isSmallScreenWidth: bo
     return isSmallScreenWidth ? styles.offlineIndicatorMobile : styles.offlineIndicator;
 };
 
-function OfflineIndicator({network, isSmallScreenWidth, translate, style = [], containerStyles = []}: OfflineIndicatorProps) {
+function OfflineIndicator({network, isSmallScreenWidth, translate, style, containerStyles}: OfflineIndicatorProps) {
     if (!network?.isOffline) {
         return null;
     }
@@ -57,7 +52,5 @@ function OfflineIndicator({network, isSmallScreenWidth, translate, style = [], c
 
 OfflineIndicator.displayName = 'OfflineIndicator';
 
-// TODO: remove when withWindowDimensions, withLocalize are migrated
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export default compose(withWindowDimensions, withLocalize, withNetwork())(OfflineIndicator);
+// TODO: use `compose` function for HOCs composing once TypeScript issues are resolved.
+export default withNetwork()(withLocalize(withWindowDimensions(OfflineIndicator)));
