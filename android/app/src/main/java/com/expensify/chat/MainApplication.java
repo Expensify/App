a1e3713e37cd3fd3c427acdeeaa2b798e56d5137
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-11-20 13:56:56 +0100

    feat: first look at bumping rn version

--- android/app/src/main/java/com/expensify/chat/MainApplication.java
+++ android/app/src/main/java/com/expensify/chat/MainApplication.java
@@ -75,7 +75,6 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
         // If you opted-in for the New Architecture, we load the native entry point for this app.
         DefaultNewArchitectureEntryPoint.load();
       }
-      ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
 
       if (BuildConfig.DEBUG) {
           FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false);
