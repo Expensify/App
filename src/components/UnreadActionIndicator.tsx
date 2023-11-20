4ed67b84987b241d5cd9a348f0ac148387d5c22f
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-11-20 08:58:27 +0100

    migrate UnreadActionIndicator.js to TypeScript

--- src/components/UnreadActionIndicator.tsx
+++ src/components/UnreadActionIndicator.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import {View} from 'react-native';
+import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@styles/useThemeStyles';
 import CONST from '@src/CONST';
 import Text from './Text';
-import withLocalize, {withLocalizePropTypes} from './withLocalize';
 
-function UnreadActionIndicator(props) {
+type UnreadActionIndicatorProps = {
+    reportActionID: string;
+};
+
+function UnreadActionIndicator(props: UnreadActionIndicatorProps) {
     const styles = useThemeStyles();
+    const {translate} = useLocalize();
+
     return (
         <View
-            accessibilityLabel={props.translate('accessibilityHints.newMessageLineIndicator')}
+            accessibilityLabel={translate('accessibilityHints.newMessageLineIndicator')}
             data-action-id={props.reportActionID}
             style={[styles.unreadIndicatorContainer, styles.userSelectNone, styles.pointerEventsNone]}
             dataSet={{[CONST.SELECTION_SCRAPER_HIDDEN_ELEMENT]: true}}
         >
             <View style={styles.unreadIndicatorLine} />
-            <Text style={styles.unreadIndicatorText}>{props.translate('common.new')}</Text>
+            <Text style={styles.unreadIndicatorText}>{translate('common.new')}</Text>
         </View>
     );
 }
 
-UnreadActionIndicator.propTypes = {...withLocalizePropTypes};
-
 UnreadActionIndicator.displayName = 'UnreadActionIndicator';
-export default withLocalize(UnreadActionIndicator);
+export default UnreadActionIndicator;
