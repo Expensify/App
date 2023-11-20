86699394ac42de84ec7ed7447108512d469d1505
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2023-11-20 16:19:43 +0100

    Add missing clarifying comment

--- src/components/Hoverable/index.tsx
+++ src/components/Hoverable/index.tsx
@@ -2,6 +2,11 @@ import React, {cloneElement, forwardRef, Ref} from 'react';
 import ActiveHoverable from './ActiveHoverable';
 import HoverableProps from './types';
 
+/**
+ * It is necessary to create a Hoverable component instead of relying solely on Pressable support for hover state,
+ * because nesting Pressables causes issues where the hovered state of the child cannot be easily propagated to the
+ * parent. https://github.com/necolas/react-native-web/issues/1875
+ */
 function Hoverable<R>({disabled, ...props}: HoverableProps, ref: Ref<R>) {
     // If Hoverable is disabled, just render the child without additional logic or event listeners.
     if (disabled) {
