667e9f96ad48e2f14bbf7582fb0d0fb311b16140
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-20 10:47:03 +0100

    Migrate useFlipper to TS

---
+++ src/hooks/useFlipper/types.ts
@@ -0,0 +1,6 @@
+import {NavigationContainerRefWithCurrent} from '@react-navigation/core';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type UseFlipper = (ref: NavigationContainerRefWithCurrent<ReactNavigation.RootParamList>) => void;
+
+export default UseFlipper;
