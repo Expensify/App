1a359807efbe26c8eee9bc537e92ce89d458cb44
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-11-21 16:14:55 +0100

    not allow for anonymous

--- src/pages/ShareCodePage.js
+++ src/pages/ShareCodePage.js
@@ -99,7 +99,6 @@ class ShareCodePage extends React.Component {
 
                     <View style={{marginTop: 36}}>
                         <MenuItem
-                            isAnonymousAction
                             title={this.props.translate(`referralProgram.${CONST.REFERRAL_PROGRAM.CONTENT_TYPES.SHARE_CODE}.buttonText1`)}
                             icon={Expensicons.Cash}
                             onPress={() => Navigation.navigate(ROUTES.REFERRAL_DETAILS_MODAL.getRoute(CONST.REFERRAL_PROGRAM.CONTENT_TYPES.SHARE_CODE))}
