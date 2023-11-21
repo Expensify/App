bf9d3a739e554fd38537d7cdd2953e42c5825d69
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-21 13:42:10 +0100

    Migrate shouldPreventDeeplinkPrompt

--- src/libs/Navigation/shouldPreventDeeplinkPrompt.ts
+++ src/libs/Navigation/shouldPreventDeeplinkPrompt.ts
@@ -2,12 +2,9 @@ import CONST from '@src/CONST';
 
 /**
  * Determines if the deeplink prompt should be shown on the current screen
- * @param {String} screenName
- * @param {Boolean} isAuthenticated
- * @returns {Boolean}
  */
-export default function shouldPreventDeeplinkPrompt(screenName) {
+export default function shouldPreventDeeplinkPrompt(screenName: string) {
     // We don't want to show the deeplink prompt on screens where a user is in the
     // authentication process, so we are blocking the prompt on the following screens (Denylist)
-    return CONST.DEEPLINK_PROMPT_DENYLIST.includes(screenName);
+    return (CONST.DEEPLINK_PROMPT_DENYLIST as readonly string[]).includes(screenName);
 }
