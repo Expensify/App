3949504d1345086fe5f1f8dbd73e310c60d5f767
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-21 10:16:30 +0100

    Add default values to prevent any regression

--- src/pages/workspace/withPolicyAndFullscreenLoading.tsx
+++ src/pages/workspace/withPolicyAndFullscreenLoading.tsx
@@ -4,7 +4,7 @@ import {OnyxEntry, withOnyx} from 'react-native-onyx';
 import FullscreenLoadingIndicator from '@components/FullscreenLoadingIndicator';
 import compose from '@libs/compose';
 import ONYXKEYS from '@src/ONYXKEYS';
-import withPolicy, {WithPolicyOnyxProps, WithPolicyProps} from './withPolicy';
+import withPolicy, {policyDefaultProps, WithPolicyOnyxProps, WithPolicyProps} from './withPolicy';
 
 type WithPolicyAndFullscreenLoadingOnyxProps = {
     /** Indicated whether the report data is loading */
@@ -20,7 +20,17 @@ type ComponentWithPolicyAndFullscreenLoading<TProps extends WithPolicyAndFullscr
 export default function withPolicyAndFullscreenLoading<TProps extends WithPolicyAndFullscreenLoadingProps, TRef>(
     WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>,
 ): ComponentWithPolicyAndFullscreenLoading<TProps, TRef> {
-    function WithPolicyAndFullscreenLoading({isLoadingReportData = true, policy, policyDraft, ...rest}: TProps, ref: ForwardedRef<TRef>) {
+    function WithPolicyAndFullscreenLoading(
+        {
+            isLoadingReportData = true,
+            policy = policyDefaultProps.policy,
+            policyDraft = policyDefaultProps.policyDraft,
+            policyMembers = policyDefaultProps.policyMembers,
+            policyMembersDraft = policyDefaultProps.policyMembersDraft,
+            ...rest
+        }: TProps,
+        ref: ForwardedRef<TRef>,
+    ) {
         if (isLoadingReportData && isEmpty(policy) && isEmpty(policyDraft)) {
             return <FullscreenLoadingIndicator />;
         }
@@ -32,6 +42,8 @@ export default function withPolicyAndFullscreenLoading<TProps extends WithPolicy
                 isLoadingReportData={isLoadingReportData}
                 policy={policy}
                 policyDraft={policyDraft}
+                policyMembers={policyMembers}
+                policyMembersDraft={policyMembersDraft}
                 ref={ref}
             />
         );
