ed9c97f5232255889b4e74d76e9c3b6a256e3abc
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-11-22 18:18:45 +0100

    use camelCase of getContextMenuItemStyles

--- src/styles/getContextMenuItemStyles/index.ts
+++ src/styles/getContextMenuItemStyles/index.ts
@@ -2,11 +2,11 @@ import styles from '@styles/styles';
 import variables from '@styles/variables';
 import GetContextMenuItemStyle from './types';
 
-const GetContextMenuItemStyles: GetContextMenuItemStyle = (windowWidth) => {
+const getContextMenuItemStyles: GetContextMenuItemStyle = (windowWidth) => {
     if (windowWidth && windowWidth > variables.mobileResponsiveWidthBreakpoint) {
         return [styles.popoverMenuItem, styles.contextMenuItemPopoverMaxWidth];
     }
     return [styles.popoverMenuItem];
 };
 
-export default GetContextMenuItemStyles;
+export default getContextMenuItemStyles;
