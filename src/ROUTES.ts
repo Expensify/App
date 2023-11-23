7b43b13219f5ee42037fe3be77e072d2ee6a5f57
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-23 14:32:05 +0100

    Fix gramma

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -399,7 +399,7 @@ type RouteIsPlainString = IsEqual<AllRoutes, string>;
 /**
  * Represents all routes in the app as a union of literal strings.
  *
- * If this type resolves to `never`, it implies that one or more routes defined within `ROUTES` have not correctly used
+ * If this type resolves to `never`, it implies that one or more routes defined within `ROUTES` have not been correctly used
  * `as const` in their `getRoute` function return value.
  */
 type Route = RouteIsPlainString extends true ? never : AllRoutes;
