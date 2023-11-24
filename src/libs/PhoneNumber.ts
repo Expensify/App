cfc9701919aecb7703fe563730509e57ff269660
Carlos Barros <765936+barros001@users.noreply.github.com>
2023-11-24 08:53:48 -0500

    refactored parsePhoneNumber function.

--- src/libs/PhoneNumber.ts
+++ src/libs/PhoneNumber.ts
@@ -9,31 +9,33 @@ import CONST from '@src/CONST';
  */
 function parsePhoneNumber(phoneNumber: string, options?: PhoneNumberParseOptions): ParsedPhoneNumber {
     const parsedPhoneNumber = originalParsePhoneNumber(phoneNumber, options);
-    const phoneNumberWithoutSpecialChars = phoneNumber.replace(CONST.REGEX.SPECIAL_CHARS_WITHOUT_NEWLINE, '');
-
-    if (parsedPhoneNumber.possible && phoneNumberWithoutSpecialChars.match(/^\+11[0-9]{10}$/)) {
-        // force number to be invalid
-        parsedPhoneNumber.valid = false;
-        parsedPhoneNumber.possible = false;
-
-        if (parsedPhoneNumber.number) {
-            const countryCode = phoneNumberWithoutSpecialChars.substring(0, 2);
-            const phoneNumberWithoutCountryCode = phoneNumberWithoutSpecialChars.substring(2);
-
-            parsedPhoneNumber.number = {
-                ...parsedPhoneNumber.number,
-
-                // mimic the behavior of awesome-phonenumber
-                e164: phoneNumberWithoutSpecialChars,
-                international: `${countryCode} ${phoneNumberWithoutCountryCode}`,
-                national: phoneNumberWithoutCountryCode,
-                rfc3966: `tel:${countryCode}-${phoneNumberWithoutCountryCode}`,
-                significant: phoneNumberWithoutCountryCode,
-            };
-        }
+    if (!parsedPhoneNumber.possible || !parsedPhoneNumber.valid) {
+        return parsedPhoneNumber;
     }
 
-    return parsedPhoneNumber;
+    const phoneNumberWithoutSpecialChars = phoneNumber.replace(CONST.REGEX.SPECIAL_CHARS_WITHOUT_NEWLINE, '');
+    if (!/^\+11[0-9]{10}$/.test(phoneNumberWithoutSpecialChars)) {
+        return parsedPhoneNumber;
+    }
+
+    const countryCode = phoneNumberWithoutSpecialChars.substring(0, 2);
+    const phoneNumberWithoutCountryCode = phoneNumberWithoutSpecialChars.substring(2);
+
+    return {
+        ...parsedPhoneNumber,
+        valid: false,
+        possible: false,
+        number: {
+            ...parsedPhoneNumber.number,
+
+            // mimic the behavior of awesome-phonenumber
+            e164: phoneNumberWithoutSpecialChars,
+            international: `${countryCode} ${phoneNumberWithoutCountryCode}`,
+            national: phoneNumberWithoutCountryCode,
+            rfc3966: `tel:${countryCode}-${phoneNumberWithoutCountryCode}`,
+            significant: phoneNumberWithoutCountryCode,
+        },
+    };
 }
 
 // eslint-disable-next-line no-restricted-imports
