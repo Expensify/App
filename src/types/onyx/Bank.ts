f72c49c2432d362ec9a96e3183fda5350dae3de0
Taras Perun <terry.perun@gmail.com>
2023-11-24 11:47:57 +0100

    create Bank type

---
+++ src/types/onyx/Bank.ts
@@ -0,0 +1,19 @@
+import {CSSProperties} from 'react';
+import {ViewStyle} from 'react-native';
+import {SvgProps} from 'react-native-svg';
+import {ValueOf} from 'type-fest';
+import CONST from '@src/CONST';
+
+type BankIcon = {
+    icon: React.FC<SvgProps>;
+    iconSize?: number;
+    iconHeight?: number;
+    iconWidth?: number;
+    iconStyles?: Array<ViewStyle | CSSProperties>;
+};
+
+type BankNameAndEmptyString = keyof typeof CONST.BANK_NAMES | '';
+type BankName = ValueOf<typeof CONST.BANK_NAMES>;
+type BankNameKey = keyof typeof CONST.BANK_NAMES;
+
+export type {BankIcon, BankNameAndEmptyString, BankName, BankNameKey};
