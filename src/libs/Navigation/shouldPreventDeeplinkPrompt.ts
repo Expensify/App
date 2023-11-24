89c8ac8a84f5ea5810b6c593b999663e852b30bb
Blazej Kustra <blazej.kustra@swmansion.com>
2023-11-24 10:54:11 +0100

    Add missing return type

--- src/libs/Navigation/shouldPreventDeeplinkPrompt.ts
+++ src/libs/Navigation/shouldPreventDeeplinkPrompt.ts
@@ -3,7 +3,7 @@ import CONST from '@src/CONST';
 /**
  * Determines if the deeplink prompt should be shown on the current screen
  */
-export default function shouldPreventDeeplinkPrompt(screenName: string) {
+export default function shouldPreventDeeplinkPrompt(screenName: string): boolean {
     // We don't want to show the deeplink prompt on screens where a user is in the
     // authentication process, so we are blocking the prompt on the following screens (Denylist)
     return (CONST.DEEPLINK_PROMPT_DENYLIST as readonly string[]).includes(screenName);
