81dc4ec8a8ef6feb66e8e685857ad738b1eab33f
Tsaqif <tsaiinkwa@yahoo.com>
2023-11-24 09:43:02 +0700

    Fix adding currency parameter to worksapace_settings get route calls
    Signed-off-by: Tsaqif <tsaiinkwa@yahoo.com>

--- src/pages/workspace/WorkspaceInitialPage.js
+++ src/pages/workspace/WorkspaceInitialPage.js
@@ -53,10 +53,10 @@ const defaultProps = {
 };
 
 /**
- * @param {string} policyID
+ * @param {Object} policy
  */
-function openEditor(policyID) {
-    Navigation.navigate(ROUTES.WORKSPACE_SETTINGS.getRoute(policyID));
+function openEditor(policy) {
+    Navigation.navigate(ROUTES.WORKSPACE_SETTINGS.getRoute(policy.id, policy.outputCurrency));
 }
 
 /**
@@ -152,7 +152,7 @@ function WorkspaceInitialPage(props) {
         {
             translationKey: 'workspace.common.settings',
             icon: Expensicons.Gear,
-            action: singleExecution(waitForNavigate(() => Navigation.navigate(ROUTES.WORKSPACE_SETTINGS.getRoute(policy.id)))),
+            action: singleExecution(waitForNavigate(() => Navigation.navigate(ROUTES.WORKSPACE_SETTINGS.getRoute(policy.id, policy.outputCurrency)))),
             brickRoadIndicator: hasGeneralSettingsError ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : '',
         },
         {
@@ -269,7 +269,7 @@ function WorkspaceInitialPage(props) {
                                             <PressableWithoutFeedback
                                                 disabled={hasPolicyCreationError || isExecuting}
                                                 style={[styles.pRelative, styles.avatarLarge]}
-                                                onPress={singleExecution(waitForNavigate(() => openEditor(policy.id)))}
+                                                onPress={singleExecution(waitForNavigate(() => openEditor(policy)))}
                                                 accessibilityLabel={translate('workspace.common.settings')}
                                                 role={CONST.ACCESSIBILITY_ROLE.BUTTON}
                                             >
@@ -289,7 +289,7 @@ function WorkspaceInitialPage(props) {
                                                 <PressableWithoutFeedback
                                                     disabled={hasPolicyCreationError || isExecuting}
                                                     style={[styles.alignSelfCenter, styles.mt4, styles.w100]}
-                                                    onPress={singleExecution(waitForNavigate(() => openEditor(policy.id)))}
+                                                    onPress={singleExecution(waitForNavigate(() => openEditor(policy)))}
                                                     accessibilityLabel={translate('workspace.common.settings')}
                                                     role={CONST.ACCESSIBILITY_ROLE.BUTTON}
                                                 >
