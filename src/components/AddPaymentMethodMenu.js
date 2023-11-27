65c73c1e696cb6e7f7d8d12695dc18b09e89696c
Vit Horacek <vit@expensify.com>
2023-11-27 12:16:36 +0000

    Show the business bank account as an option for workspace chats

--- src/components/AddPaymentMethodMenu.js
+++ src/components/AddPaymentMethodMenu.js
@@ -84,7 +84,11 @@ function AddPaymentMethodMenu({isVisible, onClose, anchorPosition, anchorAlignme
                           },
                       ]
                     : []),
-                ...(!ReportActionsUtils.hasRequestFromCurrentAccount(lodashGet(iouReport, 'reportID', 0), lodashGet(session, 'accountID', 0))
+
+                // Users can choose to pay with business bank account in case of Expense reports or in case of P2P IOU report
+                // which then starts a bottom up flow and creates a Collect workspace where the payer is an admin and payee is an employee.
+                ...(ReportUtils.isExpenseReport(iouReport) ||
+                (ReportUtils.isIOUReport(iouReport) && !ReportActionsUtils.hasRequestFromCurrentAccount(lodashGet(iouReport, 'reportID', 0), lodashGet(session, 'accountID', 0)))
                     ? [
                           {
                               text: translate('common.businessBankAccount'),
