4abcfd7c2786d617feb7018ba16e009b2ede6a91
Christoph Pader <christoph.pader@outlook.com>
2023-11-27 19:20:55 +0100

    add noop for native

---
+++ src/components/CustomScrollbarWrapper/CustomScrollbarWrapperProps.ts
@@ -0,0 +1,7 @@
+import {ThemePreferenceWithoutSystem} from '@styles/themes/types';
+
+type CustomScrollbarWrapperProps = React.PropsWithChildren & {
+    theme?: ThemePreferenceWithoutSystem;
+};
+
+export default CustomScrollbarWrapperProps;
---
+++ src/components/CustomScrollbarWrapper/index.native.tsx
@@ -0,0 +1,7 @@
+import CustomScrollbarWrapperProps from './CustomScrollbarWrapperProps';
+
+function CustomScrollbarWrapper({children}: CustomScrollbarWrapperProps) {
+    return <>{children}</>;
+}
+
+export default CustomScrollbarWrapper;
