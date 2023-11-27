32b1f25af3cd3774d90d3eff622cc936911de4b5
someone-here <eshgupta1217@gmail.com>
2023-11-27 16:53:08 +0530

    Correct translations

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -1189,7 +1189,7 @@ export default {
             noBankAccountAvailable: 'Sorry, no bank account is available',
             noBankAccountSelected: 'Please choose an account',
             taxID: 'Please enter a valid tax ID number',
-            website: 'Please enter a valid website. El sitio web debe estar en minúsculas.',
+            website: 'Please enter a valid website. The website should be in lowercase.',
             zipCode: `Incorrect zip code format. Acceptable format: ${CONST.COUNTRY_ZIP_REGEX_DATA.US.samples}`,
             phoneNumber: 'Please enter a valid phone number',
             companyName: 'Please enter a valid legal business name',
