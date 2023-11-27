3d553713607a5e8c8127b67b1ad12ae908ceff26
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-11-27 18:45:26 +0100

    Add stories for subscript icon

--- src/stories/SubscriptAvatar.stories.js
+++ src/stories/SubscriptAvatar.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as defaultAvatars from '@components/Icon/DefaultAvatars';
-import * as defaultWorkspaceAvatars from '@components/Icon/WorkspaceDefaultAvatars';
 import SubscriptAvatar from '@components/SubscriptAvatar';
+import * as Expensicons from '@components/Icon/Expensicons';
 import CONST from '@src/CONST';
 
 /**
@@ -14,7 +14,6 @@ export default {
     component: SubscriptAvatar,
     args: {
         mainAvatar: {source: defaultAvatars.Avatar5, name: '', type: CONST.ICON_TYPE_AVATAR},
-        secondaryAvatar: {source: defaultWorkspaceAvatars.WorkspaceE, name: '', type: CONST.ICON_TYPE_WORKSPACE},
         size: CONST.AVATAR_SIZE.DEFAULT,
     },
     argTypes: {
@@ -39,4 +38,15 @@ AvatarURLStory.args = {
     secondaryAvatar: {source: defaultAvatars.Avatar3, name: '', type: CONST.ICON_TYPE_AVATAR},
 };
 
-export {Default, AvatarURLStory};
+const SubscriptIcon = Template.bind({});
+SubscriptIcon.args = {
+    subscriptIcon: { source: Expensicons.DownArrow, width: 8, height: 8}
+};
+
+const WorkspaceSubscriptIcon = Template.bind({});
+WorkspaceSubscriptIcon.args = {
+    mainAvatar: {source: defaultAvatars.Avatar1, name: '', type: CONST.ICON_TYPE_WORKSPACE},
+    subscriptIcon: { source: Expensicons.DownArrow, width: 8, height: 8}
+};
+
+export {Default, AvatarURLStory, SubscriptIcon, WorkspaceSubscriptIcon};
