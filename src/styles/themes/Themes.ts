938bd28d2876288be52c7da8a4fdf2fe96212152
Christoph Pader <christoph.pader@outlook.com>
2023-11-27 19:14:11 +0100

    remove comment

--- src/styles/themes/Themes.ts
+++ src/styles/themes/Themes.ts
@@ -3,9 +3,6 @@ import darkTheme from './default';
 import lightTheme from './light';
 import {ThemeColors, ThemePreferenceWithoutSystem} from './types';
 
-// There might be more themes than just "dark" and "light".
-// Still, status bar, scrollbar and other components might need to adapt based on if the theme is overly light or dark
-// e.g. the StatusBar displays either "light-content" or "dark-content" based on the theme
 const Themes = {
     [CONST.THEME.LIGHT]: lightTheme,
     [CONST.THEME.DARK]: darkTheme,
