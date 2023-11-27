53cd80ed148e003aca822f621df4a91c87c3de14
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-11-27 10:08:30 +0100

    fix: resolve comment

--- src/types/utils/EmptyObject.ts
+++ src/types/utils/EmptyObject.ts
@@ -7,7 +7,7 @@ function isNotEmptyObject<T extends Record<string, unknown> | Falsy>(arg: T): ar
     return Object.keys(arg ?? {}).length > 0;
 }
 
-function isEmptyObject<T>(obj: T): boolean {
+function isEmptyObject<T extends Record<string, unknown> | Falsy>(obj: T): boolean {
     return Object.keys(obj ?? {}).length === 0;
 }
 
