e951bb7747210447028f350e5935f0bedb6e83e6
Viktoryia Kliushun <vikstash@gmail.com>
2023-11-28 17:24:39 +0100

    [TS migration] Migrate 'TransactionEdit.js' lib

--- src/libs/actions/TransactionEdit.ts
+++ src/libs/actions/TransactionEdit.ts
@@ -1,28 +1,31 @@
-import Onyx from 'react-native-onyx';
+import Onyx, {OnyxEntry} from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
+import {Transaction} from '@src/types/onyx';
 
 /**
  * Makes a backup copy of a transaction object that can be restored when the user cancels editing a transaction.
- *
- * @param {Object} transaction
  */
-function createBackupTransaction(transaction) {
+function createBackupTransaction(transaction: OnyxEntry<Transaction>) {
+    if (!transaction) {
+        return;
+    }
+
     const newTransaction = {
         ...transaction,
     };
+
     // Use set so that it will always fully overwrite any backup transaction that could have existed before
     Onyx.set(`${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${transaction.transactionID}`, newTransaction);
 }
 
 /**
  * Removes a transaction from Onyx that was only used temporary in the edit flow
- * @param {String} transactionID
  */
-function removeBackupTransaction(transactionID) {
+function removeBackupTransaction(transactionID: string) {
     Onyx.set(`${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${transactionID}`, null);
 }
 
-function restoreOriginalTransactionFromBackup(transactionID) {
+function restoreOriginalTransactionFromBackup(transactionID: string) {
     const connectionID = Onyx.connect({
         key: `${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${transactionID}`,
         callback: (backupTransaction) => {
