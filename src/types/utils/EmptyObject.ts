e8b91bdd1b5211328185237f288a9914bcb9f930
Yauheni Pasiukevich <pasyukevich@live.com>
2023-11-28 16:51:59 +0100

    revert EmptyObject

--- src/types/utils/EmptyObject.ts
+++ src/types/utils/EmptyObject.ts
@@ -1,7 +1,9 @@
 import Falsy from './Falsy';
 
+type EmptyObject = Record<string, never>;
+
 // eslint-disable-next-line rulesdir/no-negated-variables
-function isNotEmptyObject<T extends Record<string, unknown> | Falsy>(arg: T): arg is NonNullable<T> {
+function isNotEmptyObject<T extends Record<string, unknown> | Falsy>(arg: T | EmptyObject): arg is NonNullable<T> {
     return Object.keys(arg ?? {}).length > 0;
 }
 
@@ -10,3 +12,4 @@ function isEmptyObject<T>(obj: T): boolean {
 }
 
 export {isNotEmptyObject, isEmptyObject};
+export type {EmptyObject};
