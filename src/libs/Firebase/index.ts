0e3851d8083a39053877a36fd8c9dcb4cae50390
Daniel Silva <dsilva@expensify.com>
2023-11-29 21:39:29 +0100

    adding noop method on web for now

--- src/libs/Firebase/index.ts
+++ src/libs/Firebase/index.ts
@@ -1,10 +1,12 @@
-import {StartTrace, StopTrace} from './types';
+import {StartTrace, StopTrace, TrackScreen} from './types';
 
 /** Web does not use Firebase for performance tracing */
 const startTrace: StartTrace = () => {};
 const stopTrace: StopTrace = () => {};
+const trackScreen: TrackScreen = () => {};
 
 export default {
     startTrace,
     stopTrace,
+    trackScreen,
 };
