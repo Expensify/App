c8f48adac5389c719bdd95b364e1a93e92877268
Fábio Henriques <contact@fabiohenriques.dev>
2023-11-29 09:56:34 -0300

    Several improvements and PushNotification migration

--- src/libs/Notification/PushNotification/shouldShowPushNotification.ts
+++ src/libs/Notification/PushNotification/shouldShowPushNotification.ts
@@ -1,13 +1,8 @@
 import {PushPayload} from '@ua/react-native-airship';
-import {OnyxUpdate} from 'react-native-onyx';
 import Log from '@libs/Log';
 import * as ReportActionUtils from '@libs/ReportActionsUtils';
 import * as Report from '@userActions/Report';
-
-type PushData = {
-    onyxData: OnyxUpdate[];
-    reportID?: number;
-};
+import {NotificationData} from './NotificationType';
 
 /**
  * Returns whether the given Airship notification should be shown depending on the current state of the app
@@ -22,15 +17,15 @@ export default function shouldShowPushNotification(pushPayload: PushPayload): bo
         payload = JSON.parse(payload);
     }
 
-    const pushData = payload as PushData;
+    const data = payload as NotificationData;
 
-    if (!pushData.reportID) {
+    if (!data.reportID) {
         Log.info('[PushNotification] Not a report action notification. Showing notification');
         return true;
     }
 
-    const reportAction = ReportActionUtils.getLatestReportActionFromOnyxData(pushData.onyxData);
-    const shouldShow = Report.shouldShowReportActionNotification(String(pushData.reportID), reportAction, true);
+    const reportAction = ReportActionUtils.getLatestReportActionFromOnyxData(data.onyxData ?? null);
+    const shouldShow = Report.shouldShowReportActionNotification(String(data.reportID), reportAction, true);
     Log.info(`[PushNotification] ${shouldShow ? 'Showing' : 'Not showing'} notification`);
     return shouldShow;
 }
