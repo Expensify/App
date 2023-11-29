dbc794e417f1520f26a9321d9f9ef49ecd17333e
Daniel Silva <dsilva@expensify.com>
2023-11-29 22:23:38 +0100

    removing checks for useref

--- src/libs/ScreenTracker/ScreenTracker.ts
+++ src/libs/ScreenTracker/ScreenTracker.ts
@@ -2,21 +2,19 @@ import { useRef, useState } from "react";
 import Navigation, {navigationRef} from '@libs/Navigation/Navigation';
 import Firebase from '@libs/Firebase'
 
-const removeListener = useRef();
 
 export default function track(): void {
-    const [currentScreen, setCurrentScreen] = useState();
+    const [currentScreen, setCurrentScreen] = useState("");
     Navigation.isNavigationReady().then(() => {
-        // We already have an event listener, we don't need to set it up again.
-        if (removeListener.current !== undefined) {
-            return;
-        }
 
         // Get initial route
-        const initialRoute = navigationRef.current?.getCurrentRoute();
+        if (navigationRef.current === null) {
+            return;
+        }
+        const initialRoute = navigationRef.current.getCurrentRoute();
         setCurrentScreen(initialRoute.name);
         Firebase.trackScreen(initialRoute.name);
-        removeListener.current = navigationRef.current.addListener('state', (event) => {
+        navigationRef.current.addListener('state', (event) => {
             const newScreenName = Navigation.getRouteNameFromStateEvent(event);
             if (newScreenName === currentScreen) {
                 return;
