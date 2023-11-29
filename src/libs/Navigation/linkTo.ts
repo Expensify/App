a0806cb801220adfbcda76a2a6f62f9e7c203bfa
Wojciech Boman <wojciechboman@gmail.com>
2023-11-29 14:57:55 +0100

    Refactor linkTo method

--- src/libs/Navigation/linkTo.ts
+++ src/libs/Navigation/linkTo.ts
@@ -4,6 +4,7 @@ import {Writable} from 'type-fest';
 import CONST from '@src/CONST';
 import NAVIGATORS from '@src/NAVIGATORS';
 import {Route} from '@src/ROUTES';
+import dismissModal from './dismissModal';
 import getStateFromPath from './getStateFromPath';
 import getTopmostReportId from './getTopmostReportId';
 import linkingConfig from './linkingConfig';
@@ -55,6 +56,10 @@ function getMinimalAction(action: NavigationAction, state: NavigationState): Wri
     return currentAction;
 }
 
+function isModalNavigator(targetNavigator?: string) {
+    return targetNavigator === NAVIGATORS.LEFT_MODAL_NAVIGATOR || targetNavigator === NAVIGATORS.RIGHT_MODAL_NAVIGATOR;
+}
+
 export default function linkTo(navigation: NavigationContainerRef<RootStackParamList> | null, path: Route, type?: string, isActiveRoute?: boolean) {
     if (!navigation) {
         throw new Error("Couldn't find a navigation object. Is your component inside a screen in a navigator?");
@@ -75,8 +80,8 @@ export default function linkTo(navigation: NavigationContainerRef<RootStackParam
 
     // If action type is different than NAVIGATE we can't change it to the PUSH safely
     if (action?.type === CONST.NAVIGATION.ACTION_TYPE.NAVIGATE) {
-        const isTargetNavigatorModalNavigator = [NAVIGATORS.LEFT_MODAL_NAVIGATOR, NAVIGATORS.RIGHT_MODAL_NAVIGATOR].includes(action.payload.name);
-        const isTargetNavigatorOnTop = rootState.routes.at(-1)?.name === action.payload.name;
+        const topRouteName = rootState.routes.at(-1)?.name;
+        const isTargetNavigatorOnTop = topRouteName === action.payload.name;
 
         // In case if type is 'FORCED_UP' we replace current screen with the provided. This means the current screen no longer exists in the stack
         if (type === CONST.NAVIGATION.TYPE.FORCED_UP) {
@@ -92,12 +97,15 @@ export default function linkTo(navigation: NavigationContainerRef<RootStackParam
             action.type = CONST.NAVIGATION.ACTION_TYPE.REPLACE;
 
             // If this action is navigating to the RightModalNavigator and the last route on the root navigator is not RightModalNavigator then push
-        } else if (action.payload.name === NAVIGATORS.RIGHT_MODAL_NAVIGATOR && rootState.routes.at(-1)?.name !== NAVIGATORS.RIGHT_MODAL_NAVIGATOR) {
+        } else if (isModalNavigator(action.payload.name) && !isTargetNavigatorOnTop) {
+            if (isModalNavigator(topRouteName)) {
+                dismissModal();
+            }
             action.type = CONST.NAVIGATION.ACTION_TYPE.PUSH;
         }
     }
 
-    if (action && 'payload' in action && action.payload && 'name' in action.payload && action.payload.name === NAVIGATORS.RIGHT_MODAL_NAVIGATOR) {
+    if (action && 'payload' in action && action.payload && 'name' in action.payload && isModalNavigator(action.payload.name)) {
         const minimalAction = getMinimalAction(action, navigation.getRootState());
         if (minimalAction) {
             // There are situations where a route already exists on the current navigation stack
