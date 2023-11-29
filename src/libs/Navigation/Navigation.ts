e0cc1c70dcc1646447e9f2203f483f72fc0edf9e
Wojciech Boman <wojciechboman@gmail.com>
2023-11-29 12:20:12 +0100

    Remove dismissModal from Navigation.ts

--- src/libs/Navigation/Navigation.ts
+++ src/libs/Navigation/Navigation.ts
@@ -1,18 +1,17 @@
-import {findFocusedRoute, getActionFromState} from '@react-navigation/core';
+import {findFocusedRoute} from '@react-navigation/core';
 import {CommonActions, EventMapCore, getPathFromState, NavigationState, PartialState, StackActions} from '@react-navigation/native';
-import findLastIndex from 'lodash/findLastIndex';
 import Log from '@libs/Log';
 import CONST from '@src/CONST';
 import NAVIGATORS from '@src/NAVIGATORS';
 import ROUTES, {Route} from '@src/ROUTES';
-import SCREENS, {PROTECTED_SCREENS} from '@src/SCREENS';
-import getStateFromPath from './getStateFromPath';
+import {PROTECTED_SCREENS} from '@src/SCREENS';
+import dismissModal from './dismissModal';
 import originalGetTopmostReportActionId from './getTopmostReportActionID';
 import originalGetTopmostReportId from './getTopmostReportId';
 import linkingConfig from './linkingConfig';
 import linkTo from './linkTo';
 import navigationRef from './navigationRef';
-import {StackNavigationAction, StateOrRoute} from './types';
+import {StateOrRoute} from './types';
 
 let resolveNavigationIsReadyPromise: () => void;
 const navigationIsReadyPromise = new Promise<void>((resolve) => {
@@ -198,46 +197,6 @@ function setParams(params: Record<string, unknown>, routeKey: string) {
         ...CommonActions.setParams(params),
         source: routeKey,
     });
-}
-
-/**
- * Dismisses the last modal stack if there is any
- *
- * @param targetReportID - The reportID to navigate to after dismissing the modal
- */
-function dismissModal(targetReportID?: string) {
-    if (!canNavigate('dismissModal')) {
-        return;
-    }
-    const rootState = navigationRef.getRootState();
-    const lastRoute = rootState.routes.at(-1);
-    switch (lastRoute?.name) {
-        case NAVIGATORS.LEFT_MODAL_NAVIGATOR:
-        case NAVIGATORS.RIGHT_MODAL_NAVIGATOR:
-        case SCREENS.NOT_FOUND:
-        case SCREENS.REPORT_ATTACHMENTS:
-            // if we are not in the target report, we need to navigate to it after dismissing the modal
-            if (targetReportID && targetReportID !== getTopmostReportId(rootState)) {
-                const state = getStateFromPath(ROUTES.REPORT_WITH_ID.getRoute(targetReportID));
-
-                const action: StackNavigationAction = getActionFromState(state, linkingConfig.config);
-                if (action) {
-                    action.type = 'REPLACE';
-                    navigationRef.current?.dispatch(action);
-                }
-                // If not-found page is in the route stack, we need to close it
-            } else if (targetReportID && rootState.routes.some((route) => route.name === SCREENS.NOT_FOUND)) {
-                const lastRouteIndex = rootState.routes.length - 1;
-                const centralRouteIndex = findLastIndex(rootState.routes, (route) => route.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR);
-                navigationRef.current?.dispatch({...StackActions.pop(lastRouteIndex - centralRouteIndex), target: rootState.key});
-            } else {
-                navigationRef.current?.dispatch({...StackActions.pop(), target: rootState.key});
-            }
-            break;
-        default: {
-            Log.hmmm('[Navigation] dismissModal failed because there is no modal stack to dismiss');
-        }
-    }
 }
 
 /**
