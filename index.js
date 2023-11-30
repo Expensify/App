9b0a17211676b5f336451c3987704067e2a18a36
staszekscp <mariusz.stanisz@swmansion.com>
2023-11-30 15:50:08 +0100

    more cleanup

--- index.js
+++ index.js
@@ -4,10 +4,9 @@
 import {AppRegistry} from 'react-native';
 import {enableLegacyWebImplementation} from 'react-native-gesture-handler';
 import App from './src/App';
-
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
 
-// enableLegacyWebImplementation(true);
+enableLegacyWebImplementation(true);
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
 additionalAppSetup();
