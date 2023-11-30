cd3ae078927702e8b56f7d58cfa4d88d520f24d9
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2023-12-01 03:39:23 +0800

    always include owner account id

--- src/components/LHNOptionsList/LHNOptionsList.js
+++ src/components/LHNOptionsList/LHNOptionsList.js
@@ -117,11 +117,7 @@ function LHNOptionsList({
             const transactionID = lodashGet(itemParentReportAction, ['originalMessage', 'IOUTransactionID'], '');
             const itemTransaction = transactionID ? transactions[`${ONYXKEYS.COLLECTION.TRANSACTION}${transactionID}`] : {};
             const itemComment = draftComments[`${ONYXKEYS.COLLECTION.REPORT_DRAFT_COMMENT}${reportID}`] || '';
-            const participants = [...(itemFullReport.participantAccountIDs || [])];
-
-            if (itemFullReport.ownerAccountID > 0) {
-                participants.push(itemFullReport.ownerAccountID);
-            }
+            const participants = [...(itemFullReport.participantAccountIDs || []), itemFullReport.ownerAccountID];
 
             const participantsPersonalDetails = OptionsListUtils.getPersonalDetailsForAccountIDs(participants, personalDetails);
 
