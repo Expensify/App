1ef614bc9d36c812310290ca5630279183f51a65
Christoph Pader <christoph.pader@outlook.com>
2023-12-01 11:27:16 +0100

    fix: android status bar translucent

--- src/components/CustomStatusBar/updateStatusBarAppearance/index.android.ts
+++ src/components/CustomStatusBar/updateStatusBarAppearance/index.android.ts
@@ -3,6 +3,7 @@ import UpdateStatusBarAppearanceProps from './types';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default function updateStatusBarAppearance({statusBarStyle}: UpdateStatusBarAppearanceProps) {
+    StatusBar.setBackgroundColor('transparent');
     StatusBar.setTranslucent(true);
     if (statusBarStyle) {
         StatusBar.setBarStyle(statusBarStyle, true);
