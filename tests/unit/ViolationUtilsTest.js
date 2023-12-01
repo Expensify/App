4ab99316b24b7dcca332d58735f2ce32e46997dc
Trevor Coleman <trevor@trevorcoleman.design>
2023-12-01 13:01:13 -0500

    feat(Violations): refactor tests

--- tests/unit/ViolationUtilsTest.js
+++ tests/unit/ViolationUtilsTest.js
@@ -35,7 +35,7 @@ describe('getViolationsOnyxData', () => {
     let policyCategories;
 
     beforeEach(() => {
-        transaction = {category: 'Food', tag: 'Lunch', transactionID: '123'};
+        transaction = {transactionID: '123'};
         transactionViolations = [
             {name: 'duplicatedTransaction', type: 'violation', userMessage: ''},
             {name: 'receiptRequired', type: 'violation', userMessage: ''},
@@ -78,7 +78,6 @@ describe('getViolationsOnyxData', () => {
     it('should handle empty transaction', () => {
         transaction = {};
         transactionViolations = [];
-        policyRequiresCategories = undefined;
 
         const result = ViolationsUtils.getViolationsOnyxData(transaction, transactionViolations, policyRequiresTags, policyTags, policyRequiresCategories, policyCategories);
 
@@ -88,6 +87,7 @@ describe('getViolationsOnyxData', () => {
     describe('policyRequiresCategories', () => {
         beforeEach(() => {
             policyRequiresCategories = true;
+            transaction.category = 'Food';
         });
 
         it('should add missingCategory violation if no category is included', () => {
@@ -139,10 +139,11 @@ describe('getViolationsOnyxData', () => {
     describe('policyRequiresTags', () => {
         beforeEach(() => {
             policyRequiresTags = true;
-            policyTags = {Lunch: {enabled: true}};
+            policyTags = {Lunch: {enabled: true}, Dinner: {enabled: true}};
+            transaction.tag = 'Lunch';
         });
 
-        it('should handle policyRequiresTags correctly', () => {
+        it("shouldn't update the transactionViolations if the policy requires tags and the transaction has a tag from the policy", () => {
             const result = ViolationsUtils.getViolationsOnyxData(transaction, transactionViolations, policyRequiresTags, policyTags, policyRequiresCategories, policyCategories);
 
             expect(result.value).toEqual(transactionViolations);
@@ -156,7 +157,7 @@ describe('getViolationsOnyxData', () => {
             expect(result.value).toEqual(expect.arrayContaining([missingTagViolation]));
         });
 
-        it('should add a tagOutOfPolicy violation when tag does not exist and policy requires tags', () => {
+        it('should add a tagOutOfPolicy violation when policy requires tags and tag is not in the policy', () => {
             policyTags = {};
 
             const result = ViolationsUtils.getViolationsOnyxData(transaction, transactionViolations, policyRequiresTags, policyTags, policyRequiresCategories, policyCategories);
@@ -164,32 +165,13 @@ describe('getViolationsOnyxData', () => {
             expect(result.value).toEqual(expect.arrayContaining([tagOutOfPolicyViolation]));
         });
 
-        it('should add tagOutOfPolicy violation when tag is not enabled and policy requires tags', () => {
+        it('should add tagOutOfPolicy violation when policy requires tags and tag is not enabled in the policy', () => {
             policyRequiresTags = true;
             policyTags = {Lunch: {enabled: false}};
 
             const result = ViolationsUtils.getViolationsOnyxData(transaction, transactionViolations, policyRequiresTags, policyTags, policyRequiresCategories, policyCategories);
 
             expect(result.value).toEqual(expect.arrayContaining([tagOutOfPolicyViolation]));
-        });
-
-        it('should handle policy with multiple tags', () => {
-            policyRequiresTags = true;
-            policyTags = {Lunch: {enabled: true}, Dinner: {enabled: true}};
-            transaction.tag = 'Dinner';
-
-            const result = ViolationsUtils.getViolationsOnyxData(transaction, transactionViolations, policyRequiresTags, policyTags, policyRequiresCategories, policyCategories);
-
-            expect(result.value).toEqual(transactionViolations);
-        });
-
-        it('should not add a tagOutOfPolicy violation when tag is in policy', () => {
-            policyRequiresTags = true;
-            policyTags = {Lunch: {enabled: true}};
-
-            const result = ViolationsUtils.getViolationsOnyxData(transaction, transactionViolations, policyRequiresTags, policyTags, policyRequiresCategories, policyCategories);
-
-            expect(result.value).toEqual(transactionViolations);
         });
     });
 });
