483869fb12fbbe63d426f647ff6b12a5384ed2b4
Julian Kobrynski <julian.kobrynski@callstack.com>
2023-12-04 15:03:00 +0100

    start migrating RerportActionItemDate to TypeScript

--- src/pages/home/report/ReportActionItemDate.tsx
+++ src/pages/home/report/ReportActionItemDate.tsx
@@ -1,28 +1,25 @@
-import PropTypes from 'prop-types';
 import React, {memo} from 'react';
+import {OnyxEntry} from 'react-native-onyx';
 import {withCurrentDate} from '@components/OnyxProvider';
 import Text from '@components/Text';
-import withLocalize, {withLocalizePropTypes} from '@components/withLocalize';
+import useLocalize from '@hooks/useLocalize';
 import compose from '@libs/compose';
 import useThemeStyles from '@styles/useThemeStyles';
 
-const propTypes = {
-    /** UTC timestamp for when the action was created */
-    created: PropTypes.string.isRequired,
-    ...withLocalizePropTypes,
+type ReportActionItemDateOnyxProps = {
+    created: OnyxEntry<string>;
 };
 
-function ReportActionItemDate(props) {
+function ReportActionItemDate({created}: ReportActionItemDateOnyxProps) {
+    const {datetimeToCalendarTime} = useLocalize();
+
     const styles = useThemeStyles();
-    return <Text style={[styles.chatItemMessageHeaderTimestamp]}>{props.datetimeToCalendarTime(props.created)}</Text>;
+    return <Text style={[styles.chatItemMessageHeaderTimestamp]}>{datetimeToCalendarTime(created, false, false)}</Text>;
 }
 
-ReportActionItemDate.propTypes = propTypes;
 ReportActionItemDate.displayName = 'ReportActionItemDate';
 
 export default compose(
-    withLocalize,
-
     /** This component is hooked to the current date so that relative times can update when necessary
      * e.g. past midnight */
     withCurrentDate(),
