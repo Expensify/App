ea69b5c904ef9db83d695452d85dd7a31f5ba49b
Viktoryia Kliushun <vikstash@gmail.com>
2023-12-04 10:50:38 +0100

    TS updates after merging main

--- src/components/AvatarWithDisplayName.tsx
+++ src/components/AvatarWithDisplayName.tsx
@@ -1,9 +1,7 @@
 import React, {useCallback, useEffect, useRef} from 'react';
 import {View} from 'react-native';
-import {OnyxCollection, OnyxEntry} from 'react-native-onyx';
+import {OnyxCollection, OnyxEntry, withOnyx} from 'react-native-onyx';
 import {ValueOf} from 'type-fest';
-import {withOnyx} from 'react-native-onyx';
-import _ from 'underscore';
 import Navigation from '@libs/Navigation/Navigation';
 import * as OptionsListUtils from '@libs/OptionsListUtils';
 import * as ReportUtils from '@libs/ReportUtils';
@@ -13,7 +11,7 @@ import useThemeStyles from '@styles/useThemeStyles';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
-import {PersonalDetails, Policy, Report} from '@src/types/onyx';
+import {PersonalDetails, Policy, Report, ReportActions} from '@src/types/onyx';
 import DisplayNames from './DisplayNames';
 import MultipleAvatars from './MultipleAvatars';
 import ParentNavigationSubtitle from './ParentNavigationSubtitle';
@@ -21,7 +19,12 @@ import PressableWithoutFeedback from './Pressable/PressableWithoutFeedback';
 import SubscriptAvatar from './SubscriptAvatar';
 import Text from './Text';
 
-type AvatarWithDisplayNameProps = {
+type AvatarWithDisplayNamePropsWithOnyx = {
+    /** All of the actions of the report */
+    parentReportActions: OnyxEntry<ReportActions>;
+};
+
+type AvatarWithDisplayNameProps = AvatarWithDisplayNamePropsWithOnyx & {
     /** The report currently being looked at */
     report: OnyxEntry<Report>;
 
@@ -39,39 +42,35 @@ type AvatarWithDisplayNameProps = {
 
     /** Whether we should enable detail page navigation */
     shouldEnableDetailPageNavigation?: boolean;
-
-    /* Onyx Props */
-    /** All of the actions of the report */
-    parentReportActions: PropTypes.objectOf(PropTypes.shape(reportActionPropTypes)),
 };
 
 function AvatarWithDisplayName({
-                                   personalDetails,
-                                   policy,
-                                   report,
-                                   parentReportActions,
-                                   isAnonymous = false,
-                                   size = CONST.AVATAR_SIZE.DEFAULT,
-                                   shouldEnableDetailPageNavigation = false,
-                               }: AvatarWithDisplayNameProps) {
+    personalDetails,
+    policy,
+    report,
+    parentReportActions,
+    isAnonymous = false,
+    size = CONST.AVATAR_SIZE.DEFAULT,
+    shouldEnableDetailPageNavigation = false,
+}: AvatarWithDisplayNameProps) {
     const theme = useTheme();
     const styles = useThemeStyles();
     const title = ReportUtils.getReportName(report);
     const subtitle = ReportUtils.getChatRoomSubtitle(report);
     const parentNavigationSubtitleData = ReportUtils.getParentNavigationSubtitle(report);
     const isMoneyRequestOrReport = ReportUtils.isMoneyRequestReport(report) || ReportUtils.isMoneyRequest(report);
-    const icons = ReportUtils.getIcons(report, personalDetails, policy);
-    const ownerPersonalDetails = OptionsListUtils.getPersonalDetailsForAccountIDs([report.ownerAccountID], personalDetails);
-    const displayNamesWithTooltips = ReportUtils.getDisplayNamesWithTooltips(_.values(ownerPersonalDetails), false);
+    const icons = ReportUtils.getIcons(report, personalDetails, null, '', -1, policy);
+    const ownerPersonalDetails = OptionsListUtils.getPersonalDetailsForAccountIDs(report?.ownerAccountID ? [report.ownerAccountID] : [], personalDetails);
+    const displayNamesWithTooltips = ReportUtils.getDisplayNamesWithTooltips(Object.values(ownerPersonalDetails), false);
     const shouldShowSubscriptAvatar = ReportUtils.shouldReportShowSubscript(report);
     const isExpenseRequest = ReportUtils.isExpenseRequest(report);
     const defaultSubscriptSize = isExpenseRequest ? CONST.AVATAR_SIZE.SMALL_NORMAL : size;
     const avatarBorderColor = isAnonymous ? theme.highlightBG : theme.componentBG;
 
-    const actorAccountID = useRef(null);
+    const actorAccountID = useRef<number | null>(null);
     useEffect(() => {
-        const parentReportAction = lodashGet(parentReportActions, [report.parentReportActionID], {});
-        actorAccountID.current = lodashGet(parentReportAction, 'actorAccountID', -1);
+        const parentReportAction = parentReportActions?.[report?.parentReportActionID ?? ''];
+        actorAccountID.current = parentReportAction?.actorAccountID ?? -1;
     }, [parentReportActions, report]);
 
     const showActorDetails = useCallback(() => {
@@ -80,26 +79,28 @@ function AvatarWithDisplayName({
             return ReportUtils.navigateToDetailsPage(report);
         }
 
-        if (ReportUtils.isExpenseReport(report)) {
+        if (ReportUtils.isExpenseReport(report) && report?.ownerAccountID) {
             Navigation.navigate(ROUTES.PROFILE.getRoute(report.ownerAccountID));
             return;
         }
 
-        if (ReportUtils.isIOUReport(report)) {
+        if (ReportUtils.isIOUReport(report) && report?.reportID) {
             Navigation.navigate(ROUTES.REPORT_PARTICIPANTS.getRoute(report.reportID));
             return;
         }
 
         if (ReportUtils.isChatThread(report)) {
             // In an ideal situation account ID won't be 0
-            if (actorAccountID.current > 0) {
+            if (actorAccountID.current && actorAccountID.current > 0) {
                 Navigation.navigate(ROUTES.PROFILE.getRoute(actorAccountID.current));
                 return;
             }
         }
 
-        // Report detail route is added as fallback but based on the current implementation this route won't be executed
-        Navigation.navigate(ROUTES.REPORT_WITH_ID_DETAILS.getRoute(report.reportID));
+        if (report?.reportID) {
+            // Report detail route is added as fallback but based on the current implementation this route won't be executed
+            Navigation.navigate(ROUTES.REPORT_WITH_ID_DETAILS.getRoute(report.reportID));
+        }
     }, [report, shouldEnableDetailPageNavigation]);
 
     const headerView = (
@@ -173,7 +174,7 @@ function AvatarWithDisplayName({
 
 AvatarWithDisplayName.displayName = 'AvatarWithDisplayName';
 
-export default withOnyx({
+export default withOnyx<AvatarWithDisplayNameProps, AvatarWithDisplayNamePropsWithOnyx>({
     parentReportActions: {
         key: ({report}) => `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${report ? report.parentReportID : '0'}`,
         canEvict: false,
