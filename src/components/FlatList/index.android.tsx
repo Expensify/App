50c02ebdb20a589e64b0e33e13a7dfb2f70e2904
Kacper 'fvlvte' Fałat <me@falat.me>
2023-12-04 09:47:51 +0100

    Applied changes from main.

--- src/components/FlatList/index.android.tsx
+++ src/components/FlatList/index.android.tsx
@@ -1,25 +1,24 @@
 import {useFocusEffect} from '@react-navigation/native';
-import React, {ForwardedRef, forwardRef, useCallback, useState} from 'react';
+import React, {ForwardedRef, forwardRef, useCallback, useContext} from 'react';
 import {FlatList, FlatListProps} from 'react-native';
-
-type ScrollPosition = {offset?: number};
+import {ActionListContext} from '@pages/home/ReportScreenContext';
 
 // FlatList wrapped with the freeze component will lose its scroll state when frozen (only for Android).
 // CustomFlatList saves the offset and use it for scrollToOffset() when unfrozen.
 function CustomFlatList<T>(props: FlatListProps<T>, ref: ForwardedRef<FlatList>) {
-    const [scrollPosition, setScrollPosition] = useState<ScrollPosition>({});
+    const {scrollPosition, setScrollPosition} = useContext(ActionListContext);
 
     const onScreenFocus = useCallback(() => {
         if (typeof ref === 'function') {
             return;
         }
-        if (!ref?.current || !scrollPosition.offset) {
+        if (!ref?.current || !scrollPosition?.offset) {
             return;
         }
         if (ref.current && scrollPosition.offset) {
             ref.current.scrollToOffset({offset: scrollPosition.offset, animated: false});
         }
-    }, [scrollPosition.offset, ref]);
+    }, [scrollPosition?.offset, ref]);
 
     useFocusEffect(
         useCallback(() => {
