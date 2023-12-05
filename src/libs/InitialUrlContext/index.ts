0e228ca18b9ec48b9cf9d82c5a1847349e52fa96
staszekscp <mariusz.stanisz@swmansion.com>
2023-12-05 13:57:12 +0100

    add short explanation to InitialurlContext

--- src/libs/InitialUrlContext/index.ts
+++ src/libs/InitialUrlContext/index.ts
@@ -1,5 +1,6 @@
 import {createContext} from 'react';
 
+// Context to keep the initial url that will be opened when NewDot is embedded into Hybrid App
 const InitialUrlContext = createContext<{initialUrl: string | null}>({initialUrl: null});
 
 export default InitialUrlContext;
