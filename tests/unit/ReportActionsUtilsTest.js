e111654988b1f3848a779b40228614495fc04775
s-alves10 <sangar.work1028@gmail.com>
2023-12-05 07:07:24 -0600

    fix: update test case - we're hiding pending deleted ccomments in online mode

--- tests/unit/ReportActionsUtilsTest.js
+++ tests/unit/ReportActionsUtilsTest.js
@@ -227,7 +227,7 @@ describe('ReportActionsUtils', () => {
             expect(result).toStrictEqual(input);
         });
 
-        it('should filter out deleted, non-pending comments', () => {
+        it('should filter out deleted and delete-pending comments', () => {
             const input = [
                 {
                     created: '2022-11-13 22:27:01.825',
@@ -250,6 +250,7 @@ describe('ReportActionsUtils', () => {
                 },
             ];
             const result = ReportActionsUtils.getSortedReportActionsForDisplay(input);
+            input.pop();
             input.pop();
             expect(result).toStrictEqual(input);
         });
