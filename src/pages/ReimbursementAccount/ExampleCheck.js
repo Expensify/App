5af8847d7256b0eb408549158213c5f0d17a0e59
Georgia Monahan <georgia@expensify.com>
2023-12-06 12:57:16 -0500

    add themed Example Checks

---
+++ src/pages/ReimbursementAccount/ExampleCheck.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Image} from 'react-native';
+import useThemeIllustrations from '@styles/illustrations/useThemeIllustrations';
+import useThemeStyles from '@styles/useThemeStyles';
+import CONST from '@src/CONST';
+import useLocalize from '@hooks/useLocalize';
+
+function ExampleCheckImage() {
+    const styles = useThemeStyles();
+    const illustrations = useThemeIllustrations();
+    const {preferredLocale} = useLocalize();
+    const isSpanish = (preferredLocale || CONST.LOCALES.DEFAULT) === CONST.LOCALES.ES;
+
+    return (
+        <Image
+        resizeMode="contain"
+        style={[styles.exampleCheckImage, styles.mb5]}
+        source={isSpanish ? illustrations.ExampleCheckES : illustrations.ExampleCheckEN}
+        />
+    );
+}
+
+ExampleCheckImage.displayName = 'ExampleCheckImage';
+export default ExampleCheckImage;
+
