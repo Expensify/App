2dd986d508103b29274b16134732684ff41d20e7
Christoph Pader <christoph.pader@outlook.com>
2023-12-06 23:07:32 +0100

    fix

--- src/styles/ThemeStylesContext.ts
+++ src/styles/ThemeStylesContext.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles';
 
-const ThemeStylesContex./themes/themes/defaultContext(styles);
+const ThemeStylesContext = React.createContext(styles);
 
 export default ThemeStylesContext;
