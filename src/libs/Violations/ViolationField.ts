f757a27b77a33634abdd9fb9b16a766c22dbd240
Trevor Coleman <trevor@trevorcoleman.design>
2023-12-06 17:53:21 -0500

    feat(Violations): Move ViolationField into useViolations

--- src/libs/Violations/ViolationField.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Names of Fields where violations can occur
- */
-type ViolationField = 'amount' | 'billable' | 'category' | 'comment' | 'date' | 'merchant' | 'receipt' | 'tag' | 'tax';
-
-export default ViolationField;
