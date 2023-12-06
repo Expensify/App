431d7e2c5e56be98a5f68c757a622502b91230cc
Samil Abud <samilabud@gmail.com>
2023-12-06 11:33:54 -0400

    Fixed prettier errors

--- src/libs/convertToLTR/index.ts
+++ src/libs/convertToLTR/index.ts
@@ -2,12 +2,17 @@ import ConvertToLTR from './types';
 
 const convertToLTR: ConvertToLTR = (text) => text;
 
-const moveCursorToEndOfLine = (commentLength: number, setSelection:(value: React.SetStateAction<{
-    start: number;
-    end: number;
-}>)=>void) => setSelection
+const moveCursorToEndOfLine = (
+    commentLength: number,
+    setSelection: (
+        value: React.SetStateAction<{
+            start: number;
+            end: number;
+        }>,
+    ) => void,
+) => setSelection;
 
-const removeUnicodeLTRWhenEmpty = (newComment:string) => newComment;
+const removeUnicodeLTRWhenEmpty = (newComment: string) => newComment;
 
 export {moveCursorToEndOfLine, removeUnicodeLTRWhenEmpty};
 
