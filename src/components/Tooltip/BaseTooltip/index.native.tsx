203cd81b0dd64b1d81661590c4058d4ff66b8c8c
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-12-06 10:40:49 +0100

    fix warning on native

--- src/components/Tooltip/BaseTooltip/index.native.tsx
+++ src/components/Tooltip/BaseTooltip/index.native.tsx
@@ -1,10 +1,12 @@
-// We can't use the common component for the Tooltip as Web implementation uses DOM specific method to
-import TooltipProps from '@components/Tooltip/types';
+import {forwardRef} from 'react';
+import ChildrenProps from '@src/types/utils/ChildrenProps';
 
-function Tooltip({children}: TooltipProps) {
+// We can't use the common component for the Tooltip as Web implementation uses DOM specific method
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Tooltip({children}: ChildrenProps, ref: unknown) {
     return children;
 }
 
 Tooltip.displayName = 'Tooltip';
 
-export default Tooltip;
+export default forwardRef(Tooltip);
