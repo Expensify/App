c786e748a53a39812b5d00d9a745ed957f8323eb
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-12-06 13:18:54 +0100

    integrate a custom tag to style next step

--- src/libs/NextStepUtils.js
+++ src/libs/NextStepUtils.js
@@ -9,10 +9,12 @@ function parseMessage(messageToParse) {
         nextStepHTML += `<${tagType}>${Str.safeEscape(part.text)}</${tagType}>`;
     });
 
-    return nextStepHTML
+    const formattedHtml = nextStepHTML
         .replace(/%expenses/g, 'this expense')
         .replace(/%Expenses/g, 'This expense')
         .replace(/%tobe/g, 'is');
+
+    return `<next-steps>${formattedHtml}</next-steps>`;
 }
 
 // eslint-disable-next-line import/prefer-default-export
