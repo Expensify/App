da3441d6a49d7352e2bd77e01e6e853cad8f2907
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-12-07 11:20:40 +0100

    migrate custom full screen router

--- src/libs/Navigation/AppNavigator/createCustomStackNavigator/CustomFullScreenRouter.tsx
+++ src/libs/Navigation/AppNavigator/createCustomStackNavigator/CustomFullScreenRouter.tsx
@@ -1,14 +1,13 @@
-import {StackRouter} from '@react-navigation/native';
-import _ from 'underscore';
+import {NavigationState, ParamListBase, PartialState, RouterConfigOptions, StackNavigationState, StackRouter} from '@react-navigation/native';
 import SCREENS from '@src/SCREENS';
 import getIsSmallScreenWidth from '@libs/getIsSmallScreenWidth';
+import { ResponsiveStackNavigatorRouterOptions } from './types';
 
-/**
- * @param {Object} state - react-navigation state
- * @param {String} centralRoute - name of the central route
- * @returns {Boolean}
- */
-const isAtLeastOneCentralPaneNavigatorInState = (state) => _.find(state.routes, (r) => r.name === SCREENS.SETTINGS_CENTRAL_PANE);
+
+// TODO: export states to separate file
+type State = NavigationState | PartialState<NavigationState>;
+
+const isAtLeastOneInState = (state: State, screenName: string): boolean => !!state.routes.find((route) => route.name === screenName);
 
 /**
  * Adds report route without any specific reportID to the state.
@@ -16,7 +15,7 @@ const isAtLeastOneCentralPaneNavigatorInState = (state) => _.find(state.routes, 
  *
  * @param {Object} state - react-navigation state
  */
-const addCentralPaneNavigatorRoute = (state) => {
+const addCentralPaneNavigatorRoute = (state: State) => {
     const centralPaneNavigatorRoute = {
         name: SCREENS.SETTINGS_CENTRAL_PANE,
         state: {
@@ -28,33 +27,34 @@ const addCentralPaneNavigatorRoute = (state) => {
         },
     };
     state.routes.splice(1, 0, centralPaneNavigatorRoute);
-    // eslint-disable-next-line no-param-reassign
-    state.index = state.routes.length - 1;
+    // eslint-disable-next-line no-param-reassign, @typescript-eslint/non-nullable-type-assertion-style
+    (state.index as number) = state.routes.length - 1;
 };
 
-function CustomFullScreenRouter(options) {
+function CustomFullScreenRouter(options: ResponsiveStackNavigatorRouterOptions) {
     const stackRouter = StackRouter(options);
 
     return {
         ...stackRouter,
-        getInitialState({routeNames, routeParamList, routeGetIdList}) {
+        getInitialState({routeNames, routeParamList, routeGetIdList}: RouterConfigOptions) {
             const isSmallScreenWidth = getIsSmallScreenWidth();
             const initialState = stackRouter.getInitialState({routeNames, routeParamList, routeGetIdList});
-            if (!isAtLeastOneCentralPaneNavigatorInState(initialState) && !isSmallScreenWidth) {
+            if (!isAtLeastOneInState(initialState, SCREENS.SETTINGS_CENTRAL_PANE) && !isSmallScreenWidth) {
                 addCentralPaneNavigatorRoute(initialState);
             }
             return initialState;
         },
-        getRehydratedState(partialState, {routeNames, routeParamList}) {
+        getRehydratedState(partialState: StackNavigationState<ParamListBase>, {routeNames, routeParamList, routeGetIdList}: RouterConfigOptions): StackNavigationState<ParamListBase> {
             const isSmallScreenWidth = getIsSmallScreenWidth();
             // Make sure that there is at least one CentralPaneNavigator (ReportScreen by default) in the state if this is a wide layout
-            if (!isAtLeastOneCentralPaneNavigatorInState(partialState) && !isSmallScreenWidth) {
+            if (!isAtLeastOneInState(partialState, SCREENS.SETTINGS_CENTRAL_PANE) && !isSmallScreenWidth) {
                 // If we added a route we need to make sure that the state.stale is true to generate new key for this route
+
                 // eslint-disable-next-line no-param-reassign
-                partialState.stale = true;
+                (partialState.stale as boolean) = true;
                 addCentralPaneNavigatorRoute(partialState);
             }
-            const state = stackRouter.getRehydratedState(partialState, {routeNames, routeParamList});
+            const state = stackRouter.getRehydratedState(partialState, {routeNames, routeParamList, routeGetIdList});
             return state;
         },
     };
