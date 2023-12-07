93be15262e0ff77198aa9eb77daf778907387733
bartektomczyk <bartlomiejtomczyk@gmail.com>
2023-12-07 15:06:18 +0100

    fix: rollback import

--- src/pages/ReimbursementAccount/EnableStep.js
+++ src/pages/ReimbursementAccount/EnableStep.js
@@ -16,7 +16,6 @@ import Section from '@components/Section';
 import Text from '@components/Text';
 import withLocalize, {withLocalizePropTypes} from '@components/withLocalize';
 import compose from '@libs/compose';
-import * as ReimbursementAccountProps from '@pages/ReimbursementAccount/reimbursementAccountPropTypes';
 import userPropTypes from '@pages/settings/userPropTypes';
 import WorkspaceResetBankAccountModal from '@pages/workspace/WorkspaceResetBankAccountModal';
 import useThemeStyles from '@styles/useThemeStyles';
@@ -24,6 +23,7 @@ import * as Link from '@userActions/Link';
 import * as BankAccounts from '@userActions/ReimbursementAccount';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import * as ReimbursementAccountProps from './reimbursementAccountPropTypes';
 
 const propTypes = {
     /** Bank account currently in setup */
