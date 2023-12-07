d9146ee3ba410bc1c583782a66be0d2d35b1fee5
Jasper Huang <jasper@expensify.com>
2023-12-07 10:37:26 -0800

    update comments

--- src/libs/actions/Report.js
+++ src/libs/actions/Report.js
@@ -2004,7 +2004,8 @@ function openReportFromDeepLink(url, isAuthenticated) {
                 }
 
                 // We don't want to navigate to the exitTo route when creating a new workspace from a deep link,
-                // because we already handle creating the optimistic policy and navigating to it in App.setUpPoliciesAndNavigate.
+                // because we already handle creating the optimistic policy and navigating to it in App.setUpPoliciesAndNavigate,
+                // which is already called when AuthScreens mounts.
                 if (new URL(url).searchParams.get('exitTo') === ROUTES.WORKSPACE_NEW) {
                     return;
                 }
