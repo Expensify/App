eeab94cc63cd51fb9469809b14c8dee1d78f8d13
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-12-07 13:18:53 +0100

    remove hasOutstandingIOU from createOption

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -498,7 +498,6 @@ function createOption(accountIDs, personalDetails, report, reportActions = {}, {
         result.iouReportID = report.iouReportID;
         result.keyForList = String(report.reportID);
         result.tooltipText = ReportUtils.getReportParticipantsTitle(report.participantAccountIDs || []);
-        result.hasOutstandingIOU = report.hasOutstandingIOU;
         result.isWaitingOnBankAccount = report.isWaitingOnBankAccount;
         result.policyID = report.policyID;
 
