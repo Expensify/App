5be30740ec8123f66973908c7c36ee1570bbf5ca
Abdelrahman Khattab <abzokhattab@gmail.com>
2023-12-07 15:22:21 +0200

    Refactoring

--- src/components/SelectionList/BaseListItem.js
+++ src/components/SelectionList/BaseListItem.js
@@ -86,8 +86,7 @@ function BaseListItem({
                         textStyles={[
                             styles.optionDisplayName,
                             isFocused ? styles.sidebarLinkActiveText : styles.sidebarLinkText,
-                            item.isSelected && styles.sidebarLinkTextBold,
-                            isUserItem || item.isSelected ? styles.sidebarLinkTextBold : null,
+                            (isUserItem || item.isSelected) && styles.sidebarLinkTextBold,
                             styles.pre,
                         ]}
                         alternateTextStyles={[styles.optionAlternateText, styles.textLabelSupporting, isFocused ? styles.sidebarLinkActiveText : styles.sidebarLinkText, styles.pre]}
