3741ecf64429d71571f68b8f70373622125ae091
tienifr <christianwen18@gmail.com>
2023-12-08 14:04:43 +0700

    fix: inconsistency between autocomplete address and displayed address

--- src/components/AddressSearch/index.js
+++ src/components/AddressSearch/index.js
@@ -192,7 +192,7 @@ function AddressSearch({
             // amount of data massaging needs to happen for what the parent expects to get from this function.
             if (_.size(details)) {
                 onPress({
-                    address: lodashGet(details, 'description'),
+                    address: autocompleteData.description || lodashGet(details, 'formatted_address', ''),
                     lat: lodashGet(details, 'geometry.location.lat', 0),
                     lng: lodashGet(details, 'geometry.location.lng', 0),
                     name: lodashGet(details, 'name'),
@@ -261,7 +261,7 @@ function AddressSearch({
 
             lat: lodashGet(details, 'geometry.location.lat', 0),
             lng: lodashGet(details, 'geometry.location.lng', 0),
-            address: lodashGet(details, 'formatted_address', ''),
+            address: autocompleteData.description || lodashGet(details, 'formatted_address', ''),
         };
 
         // If the address is not in the US, use the full length state name since we're displaying the address's
