9647214369be222c97c98fc12f13ee54a93b1356
Wojciech Boman <wojciechboman@gmail.com>
2023-12-11 08:38:43 +0100

    Remove expo-asset and unnecessary comments

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 const testFileExtension = '[jt]s?(x)';
 module.exports = {
-    // TODO: change this back to preset: 'react-native' once we are able fix the tests on React Native >= 0.71.2
     preset: 'jest-expo',
     testMatch: [
         `<rootDir>/tests/ui/**/*.${testFileExtension}`,
