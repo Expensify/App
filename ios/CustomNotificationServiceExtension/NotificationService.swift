53272e9d053ba8f53c334a13aa484492e5ac4264
Andrew Rosiclair <arosiclair@expensify.com>
2023-12-11 14:27:37 -0500

    use airship's UANotificationServiceExtension

--- ios/CustomNotificationServiceExtension/NotificationService.swift
+++ ios/CustomNotificationServiceExtension/NotificationService.swift
@@ -5,31 +5,8 @@
 //  Created by Andrew Rosiclair on 12/8/23.
 //
 
-import UserNotifications
+import AirshipServiceExtension
 
-class NotificationService: UNNotificationServiceExtension {
-
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
-        }
-    }
-    
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
+class NotificationService: UANotificationServiceExtension {
 
 }
