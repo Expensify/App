bb7dda3fc498312826f8d42bf478f7d22e834d21
Maciej Dobosz <maciej.dobosz@swmansion.com>
2023-12-12 15:39:21 +0100

    Logo width exactly like in Figma

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -142,7 +142,7 @@ export default {
     signInLogoWidthPill: 132,
     tabSelectorButtonHeight: 40,
     tabSelectorButtonPadding: 12,
-    lhnLogoWidth: 95,
+    lhnLogoWidth: 95.09,
     lhnLogoHeight: 22.33,
     signInLogoWidthLargeScreenPill: 162,
     modalContentMaxWidth: 360,
