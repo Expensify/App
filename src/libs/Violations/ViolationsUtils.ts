a032ed237f1393e0aadfbd6f6d0b9589f193aebb
Trevor Coleman <trevor@trevorcoleman.design>
2023-12-12 15:49:11 -0500

    feat(Violations): fix comments, alphabetize cases

--- src/libs/Violations/ViolationsUtils.ts
+++ src/libs/Violations/ViolationsUtils.ts
@@ -83,129 +83,129 @@ const ViolationsUtils = {
         };
     },
     /**
-     * Gets the translated message for each violation type. Necessary because `translate` throws a type error if you
-     * attempt to pass it template strings, or members of a string literal union that contains both function and string
-     * members.
+     * Gets the translated message for each violation type.
+     *
+     * Necessary because `translate` throws a type error if you attempt to pass it a template strings, when the
+     * possible values could be either translation keys that resolve to  strings or translation keys that resolve to
+     * functions.
      *
      * The issue is with the type of {@link PhraseParameters} which is defined as
      *   `type PhraseParameters<T> = T extends (...args: infer A) => string ? A : never[];`
      *
-     *  When a templated key is passed that _may_ include a translation key that resolves to  a `string` but could also
-     * include  a translation key that resolves to  a `function`, the type returns `never`.
+     *  This says that if T is a function from some args to a string, then T is the type of the arguments. If T is
+     * anything else, then T should not be assigned a value.
+     *
+     * When a templated key is passed whose final value could be either, the type checker pessimistically assigns it
+     * never. There seems to be some block somewhere in the type system that prevents it from fully resolving it.
      *
      *  For instance:
      *   `translate('violations.missingCategory')` resolves to the  string 'Missing category',
      *   `translate('violations.overLimit') resolves to the function `({amount}: ViolationsOverLimitParams) => `Amount
      * over ${amount}/person limit`,
      *
-     *  So then:
+     *  But for some reason, typescript can't correctly infer the types when there is a union of multiple strings:
      *  ```
      *  const missingCategory = {key: 'missingCategory' as ViolationName , params:undefined};
      *  const overLimit = {key: }'overLimit' as ViolationName, params: {amount: 1};
      *
-     *  //this works fine
+     *  //this works fine:
      *  const missingCategoryTranslation = translate(`violations.${missingCategory.key}`)
      *  // this too
      *  const overLimitsTranslation = translate(`violations.${overLimit.key}`, overLimit.params)
      *
      * // but this will throw an error on the params arg  because the type checker can't sufficiently resolve the type.
-     *  [missingCategory, overLimit].map({key, params}}=>params ? translate(`violations.${key}`, params) :
-     * translate(`violations.$key}`)
-     *
+     * // Even though the type _should_ be sufficiently narrowed by the time it gets there.
+     *  [missingCategory, overLimit].map({key, params}} => params
+     *        ? translate(`violations.${key}`, params)
+     *        : translate(`violations.$key}`)
      * ```
-     *  If you have only switch cases with params, and use a default case to catch the strings, it will throw an error.
-     *
-     * @param violation
-     * @param translate
-     */
-    getViolationTranslation(violation: TransactionViolation, translate: <TKey extends TranslationPaths>(phraseKey: TKey, ...phraseParameters: PhraseParameters<Phrase<TKey>>) => string) {
+     */ getViolationTranslation(
+        violation: TransactionViolation,
+        translate: <TKey extends TranslationPaths>(phraseKey: TKey, ...phraseParameters: PhraseParameters<Phrase<TKey>>) => string,
+    ): string {
         switch (violation.name) {
-            case 'missingCategory':
-                return translate('violations.missingCategory');
-            case 'fieldRequired':
-                return translate('violations.fieldRequired');
-            case 'tagOutOfPolicy':
-                return translate('violations.tagOutOfPolicy', {tagName: violation.data?.tagName});
-            case 'missingTag':
-                return translate('violations.missingTag', {tagName: violation.data?.tagName});
-            case 'rter':
-                return translate('violations.rter', {
-                    brokenBankConnection: Boolean(violation.data?.brokenBankConnection),
-                    isAdmin: Boolean(violation.data?.isAdmin),
-                    email: violation.data?.email,
-                    isTransactionOlderThan7Days: Boolean(violation.data?.isTransactionOlderThan7Days),
-                    member: violation.data?.member,
-                });
-            case 'maxAge':
-                return translate('violations.maxAge', {maxAge: violation.data?.maxAge ?? 0});
-            case 'taxOutOfPolicy':
-                return translate('violations.taxOutOfPolicy', {taxName: violation.data?.taxName});
-            case 'billableExpense':
-                return translate('violations.billableExpense');
-            case 'futureDate':
-                return translate('violations.futureDate');
-            case 'modifiedAmount':
-                return translate('violations.modifiedAmount');
-            case 'conversionSurcharge':
-                return translate('violations.conversionSurcharge', {surcharge: violation.data?.surcharge});
-            case 'invoiceMarkup':
-                return translate('violations.invoiceMarkup', {invoiceMarkup: violation.data?.invoiceMarkup});
+            case 'allTagLevelsRequired':
+                return translate('violations.allTagLevelsRequired');
             case 'autoReportedRejectedExpense':
                 return translate('violations.autoReportedRejectedExpense', {
                     rejectedBy: violation.data?.rejectedBy ?? '',
                     rejectReason: violation.data?.rejectReason ?? '',
                 });
+            case 'billableExpense':
+                return translate('violations.billableExpense');
+            case 'cashExpenseWithNoReceipt':
+                return translate('violations.cashExpenseWithNoReceipt', {amount: violation.data?.amount ?? ''});
+            case 'categoryOutOfPolicy':
+                return translate('violations.categoryOutOfPolicy');
+            case 'conversionSurcharge':
+                return translate('violations.conversionSurcharge', {surcharge: violation.data?.surcharge});
+            case 'customUnitOutOfPolicy':
+                return translate('violations.customUnitOutOfPolicy');
+            case 'duplicatedTransaction':
+                return translate('violations.duplicatedTransaction');
+            case 'fieldRequired':
+                return translate('violations.fieldRequired');
+            case 'futureDate':
+                return translate('violations.futureDate');
+            case 'invoiceMarkup':
+                return translate('violations.invoiceMarkup', {invoiceMarkup: violation.data?.invoiceMarkup});
+            case 'maxAge':
+                return translate('violations.maxAge', {maxAge: violation.data?.maxAge ?? 0});
+            case 'missingCategory':
+                return translate('violations.missingCategory');
             case 'missingComment':
                 return translate('violations.missingComment');
-            case 'taxRequired':
-                return translate('violations.taxRequired');
-            case 'smartscanFailed':
-                return translate('violations.smartscanFailed');
-            case 'taxAmountChanged':
-                return translate('violations.taxAmountChanged');
+            case 'missingTag':
+                return translate('violations.missingTag', {tagName: violation.data?.tagName});
+            case 'modifiedAmount':
+                return translate('violations.modifiedAmount');
+            case 'modifiedDate':
+                return translate('violations.modifiedDate');
+            case 'nonExpensiworksExpense':
+                return translate('violations.nonExpensiworksExpense');
+            case 'overAutoApprovalLimit':
+                return translate('violations.overAutoApprovalLimit', {formattedLimitAmount: violation.data?.formattedLimitAmount ?? ''});
+            case 'overCategoryLimit':
+                return translate('violations.overCategoryLimit', {categoryLimit: violation.data?.categoryLimit ?? ''});
+            case 'overLimit':
+                return translate('violations.overLimit', {amount: violation.data?.amount ?? ''});
+            case 'overLimitAttendee':
+                return translate('violations.overLimitAttendee', {amount: violation.data?.amount ?? ''});
+            case 'perDayLimit':
+                return translate('violations.perDayLimit', {limit: violation.data?.limit ?? ''});
+            case 'receiptNotSmartScanned':
+                return translate('violations.receiptNotSmartScanned');
             case 'receiptRequired':
                 return translate('violations.receiptRequired', {
                     amount: violation.data?.amount ?? '0',
                     category: violation.data?.category ?? '',
                 });
-            case 'cashExpenseWithNoReceipt':
-                return translate('violations.cashExpenseWithNoReceipt', {amount: violation.data?.amount ?? ''});
-            case 'categoryOutOfPolicy':
-                return translate('violations.categoryOutOfPolicy');
-            case 'modifiedDate':
-                return translate('violations.modifiedDate');
-            case 'taxRateChanged':
-                return translate('violations.taxRateChanged');
-            case 'customUnitOutOfPolicy':
-                return translate('violations.customUnitOutOfPolicy');
-            case 'overLimit':
-                return translate('violations.overLimit', {amount: violation.data?.amount ?? ''});
-            case 'overLimitAttendee':
-                return translate('violations.overLimitAttendee', {amount: violation.data?.amount ?? ''});
-            case 'receiptNotSmartScanned':
-                return translate('violations.receiptNotSmartScanned');
-            case 'allTagLevelsRequired':
-                return translate('violations.allTagLevelsRequired');
-            case 'nonExpensiworksExpense':
-                return translate('violations.nonExpensiworksExpense');
-            case 'overAutoApprovalLimit':
-                return translate('violations.overAutoApprovalLimit', {formattedLimitAmount: violation.data?.formattedLimitAmount ?? ''});
-            case 'duplicatedTransaction':
-                return translate('violations.duplicatedTransaction');
+            case 'rter':
+                return translate('violations.rter', {
+                    brokenBankConnection: violation.data?.brokenBankConnection ?? false,
+                    isAdmin: violation.data?.isAdmin ?? false,
+                    email: violation.data?.email,
+                    isTransactionOlderThan7Days: Boolean(violation.data?.isTransactionOlderThan7Days),
+                    member: violation.data?.member,
+                });
+            case 'smartscanFailed':
+                return translate('violations.smartscanFailed');
             case 'someTagLevelsRequired':
                 return translate('violations.someTagLevelsRequired');
-            case 'overCategoryLimit':
-                return translate('violations.overCategoryLimit', {categoryLimit: violation.data?.categoryLimit ?? ''});
-            case 'perDayLimit':
-                return translate('violations.perDayLimit', {limit: violation.data?.limit ?? ''});
+            case 'tagOutOfPolicy':
+                return translate('violations.tagOutOfPolicy', {tagName: violation.data?.tagName});
+            case 'taxAmountChanged':
+                return translate('violations.taxAmountChanged');
+            case 'taxOutOfPolicy':
+                return translate('violations.taxOutOfPolicy', {taxName: violation.data?.taxName});
+            case 'taxRateChanged':
+                return translate('violations.taxRateChanged');
+            case 'taxRequired':
+                return translate('violations.taxRequired');
             default:
                 // The interpreter should never get here because the switch cases should be exhaustive.
-                //
                 // If typescript is showing an error on the assertion below it means the switch statement is out of
                 // sync with the `ViolationNames` type, and one or the other needs to be updated.
-                //
-                // The 'unnecessary' type assertion below is the guarantee of type safety for these translations.
-                //
                 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
                 return violation.name as never;
         }
