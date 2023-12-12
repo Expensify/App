be71e191d9a64ff328e2d941d3fbeffdf3e889f7
Carlos Martins <cmartins@expensify.com>
2023-12-12 13:23:30 -0700

    Merge pull request #32943 from dukenv0307/fix/32891
    Fix optimistic data for distance request
    
    (cherry picked from commit 380e6875331282b3dfcce27b5b9efafce4d11ee4)

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -54,6 +54,32 @@ Onyx.connect({
     },
 });
 
+<<<<<<< HEAD
+=======
+let allTransactionDrafts = {};
+Onyx.connect({
+    key: ONYXKEYS.COLLECTION.TRANSACTION_DRAFT,
+    waitForCollectionCallback: true,
+    callback: (val) => {
+        allTransactionDrafts = val || {};
+    },
+});
+
+let allTransactionViolations;
+Onyx.connect({
+    key: ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS,
+    waitForCollectionCallback: true,
+    callback: (val) => {
+        if (!val) {
+            allTransactionViolations = {};
+            return;
+        }
+
+        allTransactionViolations = val;
+    },
+});
+
+>>>>>>> 380e687 (Merge pull request #32943 from dukenv0307/fix/32891)
 let allDraftSplitTransactions;
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.SPLIT_TRANSACTION_DRAFT,
@@ -642,11 +668,12 @@ function getMoneyRequestInformation(
     // data. This is a big can of worms to change it to `Onyx.merge()` as explored in https://expensify.slack.com/archives/C05DWUDHVK7/p1692139468252109.
     // I want to clean this up at some point, but it's possible this will live in the code for a while so I've created https://github.com/Expensify/App/issues/25417
     // to remind me to do this.
-    const existingTransaction = existingTransactionID && TransactionUtils.getTransaction(existingTransactionID);
-    if (existingTransaction) {
+    const existingTransaction = allTransactionDrafts[`${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${CONST.IOU.OPTIMISTIC_TRANSACTION_ID}`];
+    if (existingTransaction && existingTransaction.iouRequestType === CONST.IOU.REQUEST_TYPE.DISTANCE) {
         optimisticTransaction = {
             ...optimisticTransaction,
             ...existingTransaction,
+            transactionID: optimisticTransaction.transactionID,
         };
     }
 
