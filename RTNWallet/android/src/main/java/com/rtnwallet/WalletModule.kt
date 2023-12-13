58d7c66300a3b06dfda5c8d16b80a741bcb937d4
Jack Nam <jack@expensify.com>
2023-12-13 12:03:00 -0800

    Finish implementing getDeviceDetails for android

--- RTNWallet/android/src/main/java/com/rtnwallet/WalletModule.kt
+++ RTNWallet/android/src/main/java/com/rtnwallet/WalletModule.kt
@@ -11,11 +11,37 @@ import com.rtnwallet.managers.WalletManager // Adjust with your actual manager
 class WalletModule(reactContext: ReactApplicationContext) : NativeWalletSpec(reactContext), TurboModule {
 
     override fun getDeviceDetails(promise: Promise) {
-        // Implementation to retrieve device-specific details
-        // ...
+        var walletIDError: Exception? = null
+        var hardwareIDError: Exception? = null
+        lateinit var walletID: String
+        lateinit var hardwareID: String
 
-        promise.resolve(/* Your result here */)
+        // Get Wallet ID
+        getWalletID { id, error ->
+            if (error != null) walletIDError = error
+            else walletID = id ?: ""
+        }
+
+        // Get Hardware ID
+        getHardwareID { id, error ->
+            if (error != null) hardwareIDError = error
+            else hardwareID = id ?: ""
+        }
+
+        // Check results and resolve/reject promise
+        when {
+            walletIDError != null -> promise.reject("WALLET_ID_ERROR", "Failed to retrieve Wallet ID", walletIDError)
+            hardwareIDError != null -> promise.reject("HARDWARE_ID_ERROR", "Failed to retrieve Hardware ID", hardwareIDError)
+            else -> {
+                val result = WritableNativeMap().apply {
+                    putString("walletID", walletID)
+                    putString("hardwareID", hardwareID)
+                }
+                promise.resolve(result)
+            }
+        }
     }
+
 
     override fun handleWalletCreationResponse(data: ReadableMap, promise: Promise) {
         // Convert ReadableMap to Kotlin Map and handle the wallet creation response
@@ -23,6 +49,27 @@ class WalletModule(reactContext: ReactApplicationContext) : NativeWalletSpec(rea
 
         promise.resolve(true) // Or handle with appropriate success/error response
     }
+
+    private fun getWalletID(completion: (String?, Exception?) -> Unit) {
+        tapAndPayClient.activewalletID.addOnCompleteListener { task ->
+            if (task.isSuccessful && task.result != null) {
+                completion(task.result, null)
+            } else {
+                completion(null, task.exception)
+            }
+        }
+    }
+
+    private fun getHardwareID(completion: (String?, Exception?) -> Unit) {
+        tapAndPayClient.stablehardwareID.addOnCompleteListener { task ->
+            if (task.isSuccessful && task.result != null) {
+                completion(task.result, null)
+            } else {
+                completion(null, task.exception)
+            }
+        }
+    }
+
 
     companion object {
         const val NAME = "RTNWallet"
