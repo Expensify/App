644c7708e52c278640bd4a40ee38ad806e47bed7
Adam Grzybowski <adam.grzybowski@swmansion.com>
2023-12-13 18:41:46 +0100

    modify custom router for the CustomStackNavigator

--- src/libs/Navigation/AppNavigator/createCustomStackNavigator/CustomRouter.ts
+++ src/libs/Navigation/AppNavigator/createCustomStackNavigator/CustomRouter.ts
@@ -1,61 +1,44 @@
-import {NavigationState, PartialState, RouterConfigOptions, StackNavigationState, StackRouter} from '@react-navigation/native';
+import {RouterConfigOptions, StackNavigationState, StackRouter} from '@react-navigation/native';
 import {ParamListBase} from '@react-navigation/routers';
 import getIsSmallScreenWidth from '@libs/getIsSmallScreenWidth';
+import getMatchingCentralPaneRouteForState from '@libs/Navigation/getMatchingCentralPaneRouteForState';
+import getTopmostBottomTabRoute from '@libs/Navigation/getTopmostBottomTabRoute';
+import getTopmostCentralPaneRoute from '@libs/Navigation/getTopmostCentralPaneRoute';
+import TAB_TO_CENTRAL_PANE_MAPPING from '@libs/Navigation/TAB_TO_CENTRAL_PANE_MAPPING';
+import {RootStackParamList, State} from '@libs/Navigation/types';
 import NAVIGATORS from '@src/NAVIGATORS';
-import SCREENS from '@src/SCREENS';
 import type {ResponsiveStackNavigatorRouterOptions} from './types';
 
-type State = NavigationState | PartialState<NavigationState>;
-
-const isAtLeastOneCentralPaneNavigatorInState = (state: State): boolean => !!state.routes.find((route) => route.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR);
-
-const getTopMostReportIDFromRHP = (state: State): string => {
-    if (!state) {
-        return '';
-    }
-
-    const topmostRightPane = state.routes.filter((route) => route.name === NAVIGATORS.RIGHT_MODAL_NAVIGATOR).at(-1);
-
-    if (topmostRightPane?.state) {
-        return getTopMostReportIDFromRHP(topmostRightPane.state);
-    }
-
-    const topmostRoute = state.routes.at(-1);
-
-    if (topmostRoute?.state) {
-        return getTopMostReportIDFromRHP(topmostRoute.state);
-    }
-
-    if (topmostRoute?.params && 'reportID' in topmostRoute.params && typeof topmostRoute.params.reportID === 'string' && topmostRoute.params.reportID) {
-        return topmostRoute.params.reportID;
-    }
-
-    return '';
-};
 /**
  * Adds report route without any specific reportID to the state.
  * The report screen will self set proper reportID param based on the helper function findLastAccessedReport (look at ReportScreenWrapper for more info)
  *
  * @param state - react-navigation state
  */
-const addCentralPaneNavigatorRoute = (state: State) => {
-    const reportID = getTopMostReportIDFromRHP(state);
+const addCentralPaneNavigatorRoute = (state: State<RootStackParamList>) => {
+    const matchingCentralPaneRoute = getMatchingCentralPaneRouteForState(state);
+
+    const bottomTabRoute = state.routes.filter((route) => route.name === NAVIGATORS.BOTTOM_TAB_NAVIGATOR);
+    const centralPaneRoutes = state.routes.filter((route) => route.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR);
+
+    // TODO-IDEAL Both RHP and LHP add condition for the LHP
+    const modalRoutes = state.routes.filter((route) => route.name === NAVIGATORS.RIGHT_MODAL_NAVIGATOR);
+
     const centralPaneNavigatorRoute = {
         name: NAVIGATORS.CENTRAL_PANE_NAVIGATOR,
-        state: {
-            routes: [
-                {
-                    name: SCREENS.REPORT,
-                    params: {
-                        reportID,
-                    },
-                },
-            ],
+        params: {
+            screen: matchingCentralPaneRoute.name,
+            params: matchingCentralPaneRoute.params,
         },
     };
-    state.routes.splice(1, 0, centralPaneNavigatorRoute);
-    // eslint-disable-next-line no-param-reassign, @typescript-eslint/non-nullable-type-assertion-style
-    (state.index as number) = state.routes.length - 1;
+
+    // @ts-expect-error Updating read only property
+    // noinspection JSConstantReassignment
+    state.routes = [...bottomTabRoute, ...centralPaneRoutes, centralPaneNavigatorRoute, ...modalRoutes]; // eslint-disable-line
+
+    // @ts-expect-error Updating read only property
+    // noinspection JSConstantReassignment
+    state.index = state.routes.length - 1; // eslint-disable-line
 };
 
 function CustomRouter(options: ResponsiveStackNavigatorRouterOptions) {
@@ -63,14 +46,19 @@ function CustomRouter(options: ResponsiveStackNavigatorRouterOptions) {
 
     return {
         ...stackRouter,
-        getRehydratedState(partialState: StackNavigationState<ParamListBase>, {routeNames, routeParamList, routeGetIdList}: RouterConfigOptions): StackNavigationState<ParamListBase> {
+        getRehydratedState(partialState: StackNavigationState<RootStackParamList>, {routeNames, routeParamList, routeGetIdList}: RouterConfigOptions): StackNavigationState<ParamListBase> {
             const isSmallScreenWidth = getIsSmallScreenWidth();
             // Make sure that there is at least one CentralPaneNavigator (ReportScreen by default) in the state if this is a wide layout
-            if (!isAtLeastOneCentralPaneNavigatorInState(partialState) && !isSmallScreenWidth) {
-                // If we added a route we need to make sure that the state.stale is true to generate new key for this route
+            const topmostCentralPaneRoute = getTopmostCentralPaneRoute(partialState);
+            const topmostBottomTabRoute = getTopmostBottomTabRoute(partialState);
 
-                // eslint-disable-next-line no-param-reassign
-                (partialState.stale as boolean) = true;
+            const isBottomTabMatchingCentralPane = topmostCentralPaneRoute && TAB_TO_CENTRAL_PANE_MAPPING[topmostBottomTabRoute.name].includes(topmostCentralPaneRoute.name);
+
+            if (!isSmallScreenWidth && !isBottomTabMatchingCentralPane) {
+                // If we added a route we need to make sure that the state.stale is true to generate new key for this route
+                // @ts-expect-error Updating read only property
+                // noinspection JSConstantReassignment
+                partialState.stale = true; // eslint-disable-line
                 addCentralPaneNavigatorRoute(partialState);
             }
             const state = stackRouter.getRehydratedState(partialState, {routeNames, routeParamList, routeGetIdList});
