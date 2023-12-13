ae0648c0b4a0565aecb7e8958859d4e1231f87e5
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2023-12-13 18:37:06 +0000

    refactor(typescript): migrate conciergepage

--- src/pages/ConciergePage.tsx
+++ src/pages/ConciergePage.tsx
@@ -1,36 +1,28 @@
 import {useFocusEffect} from '@react-navigation/native';
-import PropTypes from 'prop-types';
 import React from 'react';
-import {withOnyx} from 'react-native-onyx';
-import _ from 'underscore';
+import {OnyxEntry, withOnyx} from 'react-native-onyx';
 import FullScreenLoadingIndicator from '@components/FullscreenLoadingIndicator';
 import Navigation from '@libs/Navigation/Navigation';
 import * as Report from '@userActions/Report';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
+import {Session} from '@src/types/onyx';
 
-const propTypes = {
+type ConciergePageOnyxProps = {
     /** Session info for the currently logged in user. */
-    session: PropTypes.shape({
-        /** Currently logged in user authToken */
-        authToken: PropTypes.string,
-    }),
+    session: OnyxEntry<Session>;
 };
 
-const defaultProps = {
-    session: {
-        authToken: null,
-    },
-};
+type ConciergePageProps = ConciergePageOnyxProps;
 
 /*
  * This is a "utility page", that does this:
  *     - If the user is authenticated, find their concierge chat and re-route to it
  *     - Else re-route to the login page
  */
-function ConciergePage(props) {
+function ConciergePage({session}: ConciergePageProps) {
     useFocusEffect(() => {
-        if (_.has(props.session, 'authToken')) {
+        if (session?.authToken) {
             // Pop the concierge loading page before opening the concierge report.
             Navigation.isNavigationReady().then(() => {
                 Navigation.goBack(ROUTES.HOME);
@@ -43,12 +35,9 @@ function ConciergePage(props) {
 
     return <FullScreenLoadingIndicator />;
 }
-
-ConciergePage.propTypes = propTypes;
-ConciergePage.defaultProps = defaultProps;
 ConciergePage.displayName = 'ConciergePage';
 
-export default withOnyx({
+export default withOnyx<ConciergePageProps, ConciergePageOnyxProps>({
     session: {
         key: ONYXKEYS.SESSION,
     },
