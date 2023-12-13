29553bb0adac8db48165ea66ab3b1c8c6ffbe1ef
Blazej Kustra <blazej.kustra@swmansion.com>
2023-12-13 10:45:06 +0100

    Add new type to DeeplinkWrapperProps

--- src/components/DeeplinkWrapper/types.ts
+++ src/components/DeeplinkWrapper/types.ts
@@ -3,6 +3,9 @@ import ChildrenProps from '@src/types/utils/ChildrenProps';
 type DeeplinkWrapperProps = ChildrenProps & {
     /** User authentication status */
     isAuthenticated: boolean;
+
+    /** The auto authentication status */
+    autoAuthState?: string;
 };
 
 export default DeeplinkWrapperProps;
