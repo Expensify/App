cd6586a74297aab87b969e4a834566d3bd03e088
Daniel Edwards <cdaniel.edwards@gmail.com>
2023-12-13 12:22:50 -0500

    Trying this out

--- tests/perf-test/ReportScreen.perf-test.js
+++ tests/perf-test/ReportScreen.perf-test.js
@@ -55,6 +55,7 @@ jest.mock('../../src/hooks/useEnvironment', () =>
 
 jest.mock('../../src/libs/Permissions', () => ({
     canUseLinkPreviews: jest.fn(() => true),
+    canUseViolations: jest.fn(() => false),
 }));
 jest.mock('../../src/hooks/usePermissions.ts');
 
