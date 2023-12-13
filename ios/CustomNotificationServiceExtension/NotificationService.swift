83c0b93ef16cd50c6b462dcefa4e0d5a94258f5a
Andrew Rosiclair <arosiclair@expensify.com>
2023-12-13 14:18:09 -0500

    use os_log instead of print

--- ios/CustomNotificationServiceExtension/NotificationService.swift
+++ ios/CustomNotificationServiceExtension/NotificationService.swift
@@ -6,13 +6,17 @@
 //
 
 import AirshipServiceExtension
+import os.log
 
 class NotificationService: UANotificationServiceExtension {
   
   var contentHandler: ((UNNotificationContent) -> Void)?
   var bestAttemptContent: UNMutableNotificationContent?
+  let log = OSLog(subsystem: Bundle.main.bundleIdentifier ?? "com.expensify.chat.dev.CustomNotificationServiceExtension", category: "NotificationService")
   
   override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    os_log("[NotificationService] didReceive() - received notification", log: log)
+    
     self.contentHandler = contentHandler
     bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
     
@@ -21,21 +25,19 @@ class NotificationService: UANotificationServiceExtension {
       bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
       
       guard let payload = bestAttemptContent.userInfo["payload"] as? NSDictionary else {
-        print("[NotificationService] didReceive() - no payload in this notification")
+        os_log("[NotificationService] didReceive() - no payload in this notification", log: log, type: .error)
         return
       }
       
       guard let reportID = payload["reportID"] as? String else {
-        print("[NotificationService] didReceive() - no reportID in this notification")
+        os_log("[NotificationService] didReceive() - no reportID in this notification", log: log, type: .error)
         return
       }
       
       guard let onyxData = payload["onyxData"] as? NSArray else {
-        print("[NotificationService] didReceive() - no onyxData in this notification")
+        os_log("[NotificationService] didReceive() - no onyxData in this notification", log: log, type: .error)
         return
       }
-      
-      
       
       contentHandler(bestAttemptContent)
     }
