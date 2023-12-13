1ace7cc3131dee66a73e16f0ce04fd88ed63eba0
Jack Nam <jack@expensify.com>
2023-12-13 11:07:11 -0800

    Update WalletPackage.kt

--- RTNWallet/android/src/main/java/com/rtnwallet/WalletPackage.kt
+++ RTNWallet/android/src/main/java/com/rtnwallet/WalletPackage.kt
@@ -1,16 +1,32 @@
 package com.rtnwallet
 
-import com.facebook.react.ReactPackage
+import com.facebook.react.TurboReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ViewManager
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class WalletPackage : ReactPackage {
-    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return listOf(WalletModule(reactContext))
-    }
+class WalletPackage : TurboReactPackage() {
+    override fun getModule(name: String?, reactContext: ReactApplicationContext): NativeModule? =
+        if (name == WalletModule.NAME) {
+            WalletModule(reactContext)
+        } else {
+            null
+        }
 
-    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+        return ReactModuleInfoProvider {
+            mapOf(
+                WalletModule.NAME to ReactModuleInfo(
+                    WalletModule.NAME,
+                    WalletModule.NAME,
+                    false, // canOverrideExistingModule
+                    false, // needsEagerInit
+                    true,  // hasConstants
+                    false, // isCxxModule
+                    true   // isTurboModule
+                )
+            )
+        }
     }
 }
