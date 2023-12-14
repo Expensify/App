315cf256fa28c942e9f5ade22cd527a4a24cff04
Andrew Rosiclair <arosiclair@expensify.com>
2023-12-14 11:14:07 -0500

    Add ExpError for throwing runtime errors with messages

---
+++ ios/CustomNotificationServiceExtension/ExpError.swift
@@ -0,0 +1,12 @@
+//
+//  ExpError.swift
+//  CustomNotificationServiceExtension
+//
+//  Created by Andrew Rosiclair on 12/13/23.
+//
+
+import Foundation
+
+enum ExpError: Error {
+    case runtimeError(String)
+}
