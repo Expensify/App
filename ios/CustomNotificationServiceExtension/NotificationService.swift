75bdc80ae34dd0102a4c0aca7368edf379261ed9
Andrew Rosiclair <arosiclair@expensify.com>
2023-12-14 11:15:04 -0500

    parse notification data

--- ios/CustomNotificationServiceExtension/NotificationService.swift
+++ ios/CustomNotificationServiceExtension/NotificationService.swift
@@ -18,29 +18,65 @@ class NotificationService: UANotificationServiceExtension {
     os_log("[NotificationService] didReceive() - received notification", log: log)
     
     self.contentHandler = contentHandler
-    bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-    
-    if let bestAttemptContent = bestAttemptContent {
-      // Modify the notification content here...
+    guard let bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent) else {
+      contentHandler(request.content)
+      return
+    }
+    defer {
       bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-      
-      guard let payload = bestAttemptContent.userInfo["payload"] as? NSDictionary else {
-        os_log("[NotificationService] didReceive() - no payload in this notification", log: log, type: .error)
-        return
-      }
-      
-      guard let reportID = payload["reportID"] as? String else {
-        os_log("[NotificationService] didReceive() - no reportID in this notification", log: log, type: .error)
-        return
-      }
-      
-      guard let onyxData = payload["onyxData"] as? NSArray else {
-        os_log("[NotificationService] didReceive() - no onyxData in this notification", log: log, type: .error)
-        return
-      }
-      
       contentHandler(bestAttemptContent)
     }
+    
+    var notificationData: NotificationData
+    do {
+      notificationData = try parsePayload(notificationContent: bestAttemptContent)
+    } catch ExpError.runtimeError(let errorMessage) {
+      os_log("[NotificationService] didReceive() - couldn't parse the payload '%@'", log: log, type: .error, errorMessage)
+    } catch {
+      os_log("[NotificationService] didReceive() - unexpected error while parsing payload", log: log, type: .error)
+    }
+  }
+  
+  func parsePayload(notificationContent: UNMutableNotificationContent) throws -> NotificationData  {
+    guard let payload = notificationContent.userInfo["payload"] as? NSDictionary else {
+      throw ExpError.runtimeError("payload missing")
+    }
+    
+    guard let reportID = payload["reportID"] as? Int64 else {
+      throw ExpError.runtimeError("payload.reportID missing")
+    }
+    
+    guard let reportActionID = payload["reportActionID"] as? String else {
+      throw ExpError.runtimeError("payload.reportActionID missing")
+    }
+    
+    guard let onyxData = payload["onyxData"] as? NSArray else {
+      throw ExpError.runtimeError("payload.onyxData missing" + reportActionID)
+    }
+    
+    guard let reportActionOnyxUpdate = onyxData[1] as? NSDictionary else {
+      throw ExpError.runtimeError("payload.onyxData[1] missing" + reportActionID)
+    }
+    
+    guard let reportActionCollection = reportActionOnyxUpdate["value"] as? NSDictionary else {
+      throw ExpError.runtimeError("payload.onyxData[1].value (report action onyx update) missing" + reportActionID)
+    }
+    
+    guard let reportAction = reportActionCollection[reportActionID] as? NSDictionary else {
+      throw ExpError.runtimeError("payload.onyxData[1].value['\(reportActionID)'] (report action) is missing" + reportActionID)
+    }
+    
+    guard let avatarURL = reportAction["avatar"] as? String else {
+      throw ExpError.runtimeError("reportAction.avatar is missing" + reportActionID)
+    }
+    
+    return NotificationData(
+      reportID: reportID,
+      reportActionID: reportActionID,
+      onyxData: onyxData,
+      reportOnyxUpdate: reportActionOnyxUpdate,
+      avatarURL: avatarURL
+    )
   }
   
   override func serviceExtensionTimeWillExpire() {
@@ -51,4 +87,20 @@ class NotificationService: UANotificationServiceExtension {
     }
   }
   
+}
+
+class NotificationData {
+  public var reportID: Int64
+  public var reportActionID: String
+  public var onyxData: NSArray
+  public var reportOnyxUpdate: NSDictionary
+  public var avatarURL: String
+  
+  public init (reportID: Int64, reportActionID: String, onyxData: NSArray, reportOnyxUpdate: NSDictionary, avatarURL: String) {
+    self.reportID = reportID
+    self.reportActionID = reportActionID
+    self.onyxData = onyxData
+    self.reportOnyxUpdate = reportOnyxUpdate
+    self.avatarURL = avatarURL
+  }
 }
