195376981b1dc806b7c52ce02beb007b7af6905f
DylanDylann <dylandylan9112@gmail.com>
2023-12-14 21:10:55 +0700

    fix remove redundant code

--- src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.js
+++ src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.js
@@ -126,14 +126,13 @@ class ContactMethodDetailsPage extends Component {
 
         const loginData = lodashGet(this.props.loginList, contactMethod, {});
         const isDefaultContactMethod = this.props.session.email === loginData.partnerUserID;
-        const shouldSignOut = isDefaultContactMethod;
         // Navigate to methods page on successful magic code verification
         // validatedDate property is responsible to decide the status of the magic code verification
         if (!prevValidatedDate && validatedDate) {
             // If the selected contactMethod is the current session['login'] and the account is unvalidated,
             // the current authToken is invalid after the successful magic code verification.
             // So we need to sign out the user and redirect to the sign in page.
-            if (shouldSignOut) {
+            if (isDefaultContactMethod) {
                 Session.signOutAndRedirectToSignIn();
                 return;
             }
