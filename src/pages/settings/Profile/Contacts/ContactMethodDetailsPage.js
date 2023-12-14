a85a5c0792decc1522df37a8919373d49b4f250c
DylanDylann <dylandylan9112@gmail.com>
2023-12-14 20:52:06 +0700

    fix refactor comment

--- src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.js
+++ src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.js
@@ -130,8 +130,9 @@ class ContactMethodDetailsPage extends Component {
         // Navigate to methods page on successful magic code verification
         // validatedDate property is responsible to decide the status of the magic code verification
         if (!prevValidatedDate && validatedDate) {
-            // If the selected contactMethod is the current session.login and the account is unvalidated, the current authToken is invalid after the successful magic code verification
-            // So we need to sign out the user and redirect to the sign in page
+            // If the selected contactMethod is the current session['login'] and the account is unvalidated,
+            // the current authToken is invalid after the successful magic code verification.
+            // So we need to sign out the user and redirect to the sign in page.
             if (shouldSignOut) {
                 Session.signOutAndRedirectToSignIn();
                 return;
