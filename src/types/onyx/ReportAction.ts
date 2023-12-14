f34a723a5c345ca95d6cff4f3ed512dac73b94ed
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2023-12-14 20:03:26 +0100

    fix: linter

--- src/types/onyx/ReportAction.ts
+++ src/types/onyx/ReportAction.ts
@@ -7,48 +7,6 @@ import OriginalMessage, {Decision, Reaction} from './OriginalMessage';
 import {NotificationPreference} from './Report';
 import {Receipt} from './Transaction';
 
-type Image = {
-    /**  The height of the image. */
-    height?: number;
-
-    /**  The width of the image. */
-    width?: number;
-
-    /**  The URL of the image. */
-    url?: string;
-};
-
-type Logo = {
-    /**  The height of the logo. */
-    height?: number;
-
-    /**  The width of the logo. */
-    width?: number;
-
-    /**  The URL of the logo. */
-    url?: string;
-};
-
-type LinkMetaData = {
-    /**  The URL of the link. */
-    url?: string;
-
-    /**  A description of the link. */
-    description?: string;
-
-    /**  The title of the link. */
-    title?: string;
-
-    /**  The publisher of the link. */
-    publisher?: string;
-
-    /**  The image associated with the link. */
-    image: Image;
-
-    /**  The provider logo associated with the link. */
-    logo: Logo;
-};
-
 type Message = {
     /** The type of the action item fragment. Used to render a corresponding component */
     type: string;
