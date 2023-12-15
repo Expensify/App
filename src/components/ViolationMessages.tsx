44efe425a3bb4e909a0f48233fd53b25dfec15f5
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-12-15 13:38:42 -0800

    Fix hook import

--- src/components/ViolationMessages.tsx
+++ src/components/ViolationMessages.tsx
@@ -1,8 +1,8 @@
 import React, {useMemo} from 'react';
 import {View} from 'react-native';
 import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
 import ViolationsUtils from '@libs/ViolationsUtils';
-import useThemeStyles from '@styles/useThemeStyles';
 import {TransactionViolation} from '@src/types/onyx';
 import Text from './Text';
 
