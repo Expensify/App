bd72c6ce1e91ed6b6ed0e4cee28b218169da29fb
Etotaziba Olei <teneeto@gmail.com>
2023-12-18 11:51:42 +0100

    Update src/components/TaxPicker/taxPickerPropTypes.js
    Co-authored-by: Michael (Mykhailo) Kravchenko <rezkiy37@gmail.com>

--- src/components/TaxPicker/taxPickerPropTypes.js
+++ src/components/TaxPicker/taxPickerPropTypes.js
@@ -5,12 +5,12 @@ const propTypes = {
     /** The selected tax rate of an expense */
     selectedTaxRate: PropTypes.string,
 
+    /** Callback to fire when a tax is pressed */
+    onSubmit: PropTypes.func.isRequired,
+    
     /* Onyx Props */
     /** Collection of tax rates attached to a policy */
     policyTaxRates: taxPropTypes,
-
-    /** Callback to fire when a tax is pressed */
-    onSubmit: PropTypes.func.isRequired,
 };
 
 const defaultProps = {
