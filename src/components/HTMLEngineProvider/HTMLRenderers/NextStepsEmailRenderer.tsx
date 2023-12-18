858e6ed036c860896c1bc4bacb072a2073a47de1
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-12-18 11:45:29 +0100

    migrate to TS

--- src/components/HTMLEngineProvider/HTMLRenderers/NextStepsEmailRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/NextStepsEmailRenderer.tsx
@@ -1,21 +1,20 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import Text from '@components/Text';
 import useThemeStyles from '@hooks/useThemeStyles';
 
-const propTypes = {
-    tnode: PropTypes.shape({
-        data: PropTypes.string.isRequired,
-    }).isRequired,
+type NextStepsEmailRendererProps = {
+    tnode: {
+        data: string;
+    };
 };
 
-function NextStepsEmailRenderer({tnode}) {
+function NextStepsEmailRenderer({tnode}: NextStepsEmailRendererProps) {
     const styles = useThemeStyles();
 
     return <Text style={[styles.breakWord, styles.textLabelSupporting, styles.textStrong]}>{tnode.data}</Text>;
 }
 
-NextStepsEmailRenderer.propTypes = propTypes;
 NextStepsEmailRenderer.displayName = 'NextStepsEmailRenderer';
 
 export default NextStepsEmailRenderer;
+export type {NextStepsEmailRendererProps};
