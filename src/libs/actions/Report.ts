d31e907bd6070dd73881fe184c594c44c91ff6c3
Monil Bhavsar <monil@expensify.com>
2023-12-18 20:05:24 +0530

    Pass created time

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -337,6 +337,7 @@ function addActions(reportID: string, text = '', file?: File) {
         reportComment?: string;
         file?: File;
         timezone?: string;
+        createdDate?: string;
     };
 
     const parameters: AddCommentOrAttachementParameters = {
@@ -345,6 +346,7 @@ function addActions(reportID: string, text = '', file?: File) {
         commentReportActionID: file && reportCommentAction ? reportCommentAction.reportActionID : null,
         reportComment: reportCommentText,
         file,
+        createdDate: file ? attachmentAction?.created : reportCommentAction?.created,
     };
 
     const optimisticData: OnyxUpdate[] = [
