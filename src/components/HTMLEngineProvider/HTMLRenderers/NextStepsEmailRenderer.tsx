d601867eb7dfa49579a05accc853265a3ab2561f
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-12-18 14:05:48 +0100

    do not export type

--- src/components/HTMLEngineProvider/HTMLRenderers/NextStepsEmailRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/NextStepsEmailRenderer.tsx
@@ -17,4 +17,3 @@ function NextStepsEmailRenderer({tnode}: NextStepsEmailRendererProps) {
 NextStepsEmailRenderer.displayName = 'NextStepsEmailRenderer';
 
 export default NextStepsEmailRenderer;
-export type {NextStepsEmailRendererProps};
