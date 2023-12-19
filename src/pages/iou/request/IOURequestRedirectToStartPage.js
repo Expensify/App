eade9461dab92cc5e4bb8f4d782f48397cd4a949
Tim Golen <tgolen@expensify.com>
2023-12-19 08:23:39 -0800

    Add a route for easy redirects to the start request flow

---
+++ src/pages/iou/request/IOURequestRedirectToStartPage.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import _ from 'underscore';
+import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
+import Navigation from '@libs/Navigation/Navigation';
+import * as ReportUtils from '@libs/ReportUtils';
+import CONST from '@src/CONST';
+import ROUTES from '@src/ROUTES';
+
+const propTypes = {
+    /** Navigation route context info provided by react navigation */
+    route: PropTypes.shape({
+        /** Route specific parameters used on this screen */
+        params: PropTypes.shape({
+            /** The type of IOU report, i.e. bill, request, send */
+            iouType: PropTypes.oneOf(_.values(CONST.IOU.TYPE)).isRequired,
+
+            /** The type of IOU Request, i.e. manual, scan, distance */
+            iouRequestType: PropTypes.oneOf(_.values(CONST.IOU.REQUEST_TYPE)).isRequired,
+        }),
+    }).isRequired,
+};
+
+function IOURequestRedirectToStartPage({
+    route: {
+        params: {iouType, iouRequestType},
+    },
+}) {
+    // Redirect the person to the right start page using a rendom reportID
+    const optimisticReportID = ReportUtils.generateReportID();
+    if (iouRequestType === CONST.IOU.REQUEST_TYPE.DISTANCE) {
+        Navigation.navigate(ROUTES.MONEY_REQUEST_CREATE_TAB_DISTANCE.getRoute(iouType, CONST.IOU.OPTIMISTIC_TRANSACTION_ID, optimisticReportID));
+    } else if (iouRequestType === CONST.IOU.REQUEST_TYPE.MANUAL) {
+        Navigation.navigate(ROUTES.MONEY_REQUEST_CREATE_TAB_MANUAL.getRoute(iouType, CONST.IOU.OPTIMISTIC_TRANSACTION_ID, optimisticReportID));
+    } else if (iouRequestType === CONST.IOU.REQUEST_TYPE.SCAN) {
+        Navigation.navigate(ROUTES.MONEY_REQUEST_CREATE_TAB_SCAN.getRoute(iouType, CONST.IOU.OPTIMISTIC_TRANSACTION_ID, optimisticReportID));
+    }
+    return <FullPageNotFoundView />;
+}
+
+IOURequestRedirectToStartPage.displayName = 'IOURequestRedirectToStartPage';
+IOURequestRedirectToStartPage.propTypes = propTypes;
+
+export default IOURequestRedirectToStartPage;
