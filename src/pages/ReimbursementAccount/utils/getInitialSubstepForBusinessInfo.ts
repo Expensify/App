4e120c862bd4f5819eb32db7451fd366fd32d1a3
bartektomczyk <bartlomiejtomczyk@gmail.com>
2023-12-19 10:03:07 +0100

    fix: changed typing, added comment

--- src/pages/ReimbursementAccount/utils/getInitialSubstepForBusinessInfo.ts
+++ src/pages/ReimbursementAccount/utils/getInitialSubstepForBusinessInfo.ts
@@ -1,22 +1,13 @@
+import {ValueOf} from 'type-fest';
 import CONST from '@src/CONST';
 
 const businessInfoStepKeys = CONST.BANK_ACCOUNT.BUSINESS_INFO_STEP.INPUT_KEY;
 
-type BusinessInfoData = {
-    [businessInfoStepKeys.COMPANY_NAME]: string;
-    [businessInfoStepKeys.COMPANY_TAX_ID]: string;
-    [businessInfoStepKeys.COMPANY_WEBSITE]: string;
-    [businessInfoStepKeys.COMPANY_PHONE]: string;
-    [businessInfoStepKeys.COMPANY_NAME]: string;
-    [businessInfoStepKeys.STREET]: string;
-    [businessInfoStepKeys.CITY]: string;
-    [businessInfoStepKeys.STATE]: string;
-    [businessInfoStepKeys.ZIP_CODE]: string;
-    [businessInfoStepKeys.INCORPORATION_TYPE]: string;
-    [businessInfoStepKeys.INCORPORATION_DATE]: string;
-    [businessInfoStepKeys.INCORPORATION_STATE]: string;
-};
+type BusinessInfoData = Record<ValueOf<typeof businessInfoStepKeys>, string>;
 
+/**
+ * Returns the initial substep for the Business Info step based on already existing data
+ */
 function getInitialSubstepForBusinessInfo(data: BusinessInfoData): number {
     if (data[businessInfoStepKeys.COMPANY_NAME] === '') {
         return 0;
