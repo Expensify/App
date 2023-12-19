9ea140dda1d44076f5e10a818e89ec2b191502ff
Rocio Perez-Cano <pecanoro@users.noreply.github.com>
2023-12-19 16:59:35 +0100

    Merge pull request #33297 from Expensify/revert-32962-perf/debounce-search
    [CP Staging] Revert "(perf) Debounce search in OptionList"
    
    (cherry picked from commit 777a52f62dff7d6b58bf493cfda44414f5c417cb)

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -1,6 +1,7 @@
 import {format as timezoneFormat, utcToZonedTime} from 'date-fns-tz';
 import ExpensiMark from 'expensify-common/lib/ExpensiMark';
 import Str from 'expensify-common/lib/str';
+import lodashDebounce from 'lodash/debounce';
 import isEmpty from 'lodash/isEmpty';
 import {DeviceEventEmitter, InteractionManager} from 'react-native';
 import Onyx, {OnyxCollection, OnyxEntry, OnyxUpdate} from 'react-native-onyx';
@@ -2498,6 +2499,8 @@ function searchForReports(searchInput: string) {
     API.read('SearchForReports', parameters, {successData, failureData});
 }
 
+const debouncedSearchInServer = lodashDebounce(searchForReports, CONST.TIMING.SEARCH_FOR_REPORTS_DEBOUNCE_TIME, {leading: false});
+
 function searchInServer(searchInput: string) {
     if (isNetworkOffline || !searchInput.trim().length) {
         Onyx.set(ONYXKEYS.IS_SEARCHING_FOR_REPORTS, false);
@@ -2508,7 +2511,7 @@ function searchInServer(searchInput: string) {
     // we want to show the loading state right away. Otherwise, we will see a flashing UI where the client options are sorted and
     // tell the user there are no options, then we start searching, and tell them there are no options again.
     Onyx.set(ONYXKEYS.IS_SEARCHING_FOR_REPORTS, true);
-    searchForReports(searchInput);
+    debouncedSearchInServer(searchInput);
 }
 
 function clearNewRoomFormError() {
