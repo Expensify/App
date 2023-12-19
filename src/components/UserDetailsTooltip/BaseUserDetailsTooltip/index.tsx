631ff6f37ff21750a75aabc35311699987e7a505
Blazej Kustra <blazej.kustra@swmansion.com>
2023-12-19 10:46:36 +0100

    Fix typecheck

--- src/components/UserDetailsTooltip/BaseUserDetailsTooltip/index.tsx
+++ src/components/UserDetailsTooltip/BaseUserDetailsTooltip/index.tsx
@@ -27,11 +27,11 @@ function BaseUserDetailsTooltip({accountID, fallbackUserDetails, icon, delegateA
     // We replace the actor's email, name, and avatar with the Copilot manually for now. This will be improved upon when
     // the Copilot feature is implemented.
     if (delegateAccountID) {
-        const delegateUserDetails = personalDetails?.[delegateAccountID] ?? {};
+        const delegateUserDetails = personalDetails?.[delegateAccountID];
         const delegateUserDisplayName = ReportUtils.getDisplayNameForParticipant(delegateAccountID);
         userDisplayName = `${delegateUserDisplayName} (${translate('reportAction.asCopilot')} ${userDisplayName})`;
-        userLogin = delegateUserDetails.login ?? '';
-        userAvatar = delegateUserDetails.avatar;
+        userLogin = delegateUserDetails?.login ?? '';
+        userAvatar = delegateUserDetails?.avatar;
         userAccountID = delegateAccountID;
     }
 
@@ -46,9 +46,9 @@ function BaseUserDetailsTooltip({accountID, fallbackUserDetails, icon, delegateA
                 <View style={styles.emptyAvatar}>
                     <Avatar
                         containerStyles={[styles.actionAvatar]}
-                        source={icon ? icon.source : UserUtils.getAvatar(userAvatar, userAccountID)}
-                        type={icon ? icon.type : CONST.ICON_TYPE_AVATAR}
-                        name={icon ? icon.name : userLogin}
+                        source={icon?.source ?? UserUtils.getAvatar(userAvatar, userAccountID)}
+                        type={icon?.type ?? CONST.ICON_TYPE_AVATAR}
+                        name={icon?.name ?? userLogin}
                         fallbackIcon={icon?.fallbackIcon}
                     />
                 </View>
