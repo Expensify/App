354a2e3a5130ded89f11694e76a02a6bca34c49b
bartektomczyk <bartlomiejtomczyk@gmail.com>
2023-12-19 09:22:03 +0100

    refactor: migrated getInitialSubstepForBusinessInfo helper

--- src/pages/ReimbursementAccount/utils/getInitialSubstepForBusinessInfo.ts
+++ src/pages/ReimbursementAccount/utils/getInitialSubstepForBusinessInfo.ts
@@ -2,14 +2,22 @@ import CONST from '@src/CONST';
 
 const businessInfoStepKeys = CONST.BANK_ACCOUNT.BUSINESS_INFO_STEP.INPUT_KEY;
 
-/**
- * Returns the initial substep for the Business Info step based on already existing data
- *
- * @param {Object} data object that stores business info data
- *
- * @returns {number}
- */
-function getInitialSubstepForBusinessInfo(data) {
+type BusinessInfoData = {
+    [businessInfoStepKeys.COMPANY_NAME]: string;
+    [businessInfoStepKeys.COMPANY_TAX_ID]: string;
+    [businessInfoStepKeys.COMPANY_WEBSITE]: string;
+    [businessInfoStepKeys.COMPANY_PHONE]: string;
+    [businessInfoStepKeys.COMPANY_NAME]: string;
+    [businessInfoStepKeys.STREET]: string;
+    [businessInfoStepKeys.CITY]: string;
+    [businessInfoStepKeys.STATE]: string;
+    [businessInfoStepKeys.ZIP_CODE]: string;
+    [businessInfoStepKeys.INCORPORATION_TYPE]: string;
+    [businessInfoStepKeys.INCORPORATION_DATE]: string;
+    [businessInfoStepKeys.INCORPORATION_STATE]: string;
+};
+
+function getInitialSubstepForBusinessInfo(data: BusinessInfoData): number {
     if (data[businessInfoStepKeys.COMPANY_NAME] === '') {
         return 0;
     }
