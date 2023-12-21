f667699c57311f6110a0c28e7562526b4713132b
Georgia Monahan <georgia@expensify.com>
2023-12-20 18:02:28 -0800

    prettier

--- src/styles/theme/illustrations/themes/dark.ts
+++ src/styles/theme/illustrations/themes/dark.ts
@@ -1,9 +1,9 @@
+import FallbackAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-avatar-light.svg';
+import FallbackWorkspaceAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-workspace-avatar-light.svg';
 import EmptyStateBackgroundImage from '@assets/images/themeDependent/empty-state_background-fade-dark.png';
 import ExampleCheckEN from '@assets/images/themeDependent/example-check-image-dark-en.png';
 import ExampleCheckES from '@assets/images/themeDependent/example-check-image-dark-es.png';
 import {IllustrationsType} from '@styles/theme/illustrations/types';
-import FallbackAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-avatar-light.svg';
-import FallbackWorkspaceAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-workspace-avatar-light.svg';
 
 const illustrations = {
     EmptyStateBackgroundImage,
--- src/styles/theme/illustrations/themes/light.ts
+++ src/styles/theme/illustrations/themes/light.ts
@@ -1,9 +1,9 @@
-import {IllustrationsType} from '@styles/theme/illustrations/types';
+import FallbackAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-avatar-light.svg';
+import FallbackWorkspaceAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-workspace-avatar-light.svg';
 import EmptyStateBackgroundImage from '@assets/images/themeDependent/empty-state_background-fade-light.png';
 import ExampleCheckEN from '@assets/images/themeDependent/example-check-image-light-en.png';
 import ExampleCheckES from '@assets/images/themeDependent/example-check-image-light-es.png';
-import FallbackAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-avatar-light.svg';
-import FallbackWorkspaceAvatar from '@assets/images/avatars/fallback/themeDependent/fallback-workspace-avatar-light.svg';
+import {IllustrationsType} from '@styles/theme/illustrations/types';
 
 const illustrations = {
     EmptyStateBackgroundImage,
