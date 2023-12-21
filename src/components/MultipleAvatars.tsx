8f6f5fa2d0cad0a273ebb4500d104ddb9122744b
Georgia Monahan <georgia@expensify.com>
2023-12-21 11:08:52 -0800

    update bounding box

--- src/components/MultipleAvatars.tsx
+++ src/components/MultipleAvatars.tsx
@@ -12,6 +12,8 @@ import Avatar from './Avatar';
 import Text from './Text';
 import Tooltip from './Tooltip';
 import UserDetailsTooltip from './UserDetailsTooltip';
+import illustrations from '@styles/theme/illustrations/themes/dark';
+import useThemeIllustrations from '@hooks/useThemeIllustrations';
 
 type MultipleAvatarsProps = {
     /** Array of avatar URLs or icons */
@@ -81,6 +83,7 @@ function MultipleAvatars({
     const theme = useTheme();
     const styles = useThemeStyles();
     const StyleUtils = useStyleUtils();
+    const illustrations = useThemeIllustrations();
 
     const avatarSizeToStylesMap: AvatarSizeToStylesMap = useMemo(
         () => ({
