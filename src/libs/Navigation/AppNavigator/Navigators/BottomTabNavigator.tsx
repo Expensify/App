10c8b64a7eea91a75142afc63cd1375a271f2b03
Wojciech Boman <wojciechboman@gmail.com>
2023-12-22 16:26:59 +0100

    Add useActiveRoute

--- src/libs/Navigation/AppNavigator/Navigators/BottomTabNavigator.tsx
+++ src/libs/Navigation/AppNavigator/Navigators/BottomTabNavigator.tsx
@@ -1,10 +1,13 @@
+import {useNavigationState} from '@react-navigation/native';
 import {StackNavigationOptions} from '@react-navigation/stack';
 import React from 'react';
 import createCustomBottomTabNavigator from '@libs/Navigation/AppNavigator/createCustomBottomTabNavigator';
+import getTopmostCentralPaneRoute from '@libs/Navigation/getTopmostCentralPaneRoute';
 import {BottomTabNavigatorParamList} from '@libs/Navigation/types';
 import AllSettingsScreen from '@pages/home/sidebar/AllSettingsScreen';
 import SidebarScreen from '@pages/home/sidebar/SidebarScreen';
 import SCREENS from '@src/SCREENS';
+import ActiveRouteContext from './ActiveRouteContext';
 
 const loadWorkspaceInitialPage = () => require('../../../../pages/workspace/WorkspaceInitialPage').default as React.ComponentType;
 
@@ -15,21 +18,24 @@ const screenOptions: StackNavigationOptions = {
 };
 
 function BottomTabNavigator() {
+    const activeRoute = useNavigationState(getTopmostCentralPaneRoute);
     return (
-        <Tab.Navigator screenOptions={screenOptions}>
-            <Tab.Screen
-                name={SCREENS.HOME}
-                component={SidebarScreen}
-            />
-            <Tab.Screen
-                name={SCREENS.ALL_SETTINGS}
-                component={AllSettingsScreen}
-            />
-            <Tab.Screen
-                name={SCREENS.WORKSPACE.INITIAL}
-                getComponent={loadWorkspaceInitialPage}
-            />
-        </Tab.Navigator>
+        <ActiveRouteContext.Provider value={activeRoute?.name ?? ''}>
+            <Tab.Navigator screenOptions={screenOptions}>
+                <Tab.Screen
+                    name={SCREENS.HOME}
+                    component={SidebarScreen}
+                />
+                <Tab.Screen
+                    name={SCREENS.ALL_SETTINGS}
+                    component={AllSettingsScreen}
+                />
+                <Tab.Screen
+                    name={SCREENS.WORKSPACE.INITIAL}
+                    getComponent={loadWorkspaceInitialPage}
+                />
+            </Tab.Navigator>
+        </ActiveRouteContext.Provider>
     );
 }
 
