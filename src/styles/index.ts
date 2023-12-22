0c2d395dad365cf16a360c79cc74fcb7839713d9
Wojciech Boman <wojciechboman@gmail.com>
2023-12-22 16:58:35 +0100

    Revert "Merge pull request #33406 from Expensify/vit-revert32473"
    This reverts commit 5b91c04f59ee7aa5ec1c1ba313adc035b22f0208, reversing
    changes made to 880254c55ea2c210f11d77974cc29e4c446f47bd.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1415,6 +1415,12 @@ const styles = (theme: ThemeColors) =>
             height: variables.lineHeightSizeh1,
         },
 
+        LHPNavigatorContainer: (isSmallScreenWidth: boolean) =>
+            ({
+                ...modalNavigatorContainer(isSmallScreenWidth),
+                left: 0,
+            } satisfies ViewStyle),
+
         RHPNavigatorContainer: (isSmallScreenWidth: boolean) =>
             ({
                 ...modalNavigatorContainer(isSmallScreenWidth),
@@ -1634,14 +1640,15 @@ const styles = (theme: ThemeColors) =>
             marginBottom: 4,
         },
 
-        overlayStyles: (current: OverlayStylesParams) =>
+        overlayStyles: (current: OverlayStylesParams, isModalOnTheLeft: boolean) =>
             ({
                 ...positioning.pFixed,
                 // We need to stretch the overlay to cover the sidebar and the translate animation distance.
-                left: -2 * variables.sideBarWidth,
+                // The overlay must also cover borderRadius of the LHP component
+                left: isModalOnTheLeft ? 0 : -2 * variables.sideBarWidth,
                 top: 0,
                 bottom: 0,
-                right: 0,
+                right: isModalOnTheLeft ? -2 * variables.sideBarWidth : 0,
                 backgroundColor: theme.overlay,
                 opacity: current.progress.interpolate({
                     inputRange: [0, 1],
