4f25fa70385ad275a55d86f726a1cfca433a4b40
brunovjk <brunovjk@gmail.com>
2023-12-22 00:57:12 -0300

    Enhance 'OptionsListSkeletonView' for quicker display using Dimensions.

--- src/components/OptionsListSkeletonView.js
+++ src/components/OptionsListSkeletonView.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {View} from 'react-native';
+import {View, Dimensions} from 'react-native';
 import {Circle, Rect} from 'react-native-svg';
 import compose from '@libs/compose';
 import CONST from '@src/CONST';
@@ -9,64 +9,55 @@ import withTheme, {withThemePropTypes} from './withTheme';
 import withThemeStyles, {withThemeStylesPropTypes} from './withThemeStyles';
 
 const propTypes = {
-    /** Whether to animate the skeleton view */
-    shouldAnimate: PropTypes.bool,
-    ...withThemeStylesPropTypes,
-    ...withThemePropTypes,
+  /** Whether to animate the skeleton view */
+  shouldAnimate: PropTypes.bool,
+  ...withThemeStylesPropTypes,
+  ...withThemePropTypes,
 };
 
 const defaultTypes = {
-    shouldAnimate: true,
+  shouldAnimate: true,
 };
 
 class OptionsListSkeletonView extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            skeletonViewItems: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    const screenHeight = Dimensions.get('window').height;
+    const numItems = Math.ceil(screenHeight / CONST.LHN_SKELETON_VIEW_ITEM_HEIGHT);
+    this.state = {
+      skeletonViewItems: this.generateSkeletonViewItems(numItems),
+    };
+  }
 
-    /**
-     * Generate the skeleton view items.
-     *
-     * @param {Number} numItems
-     */
-    generateSkeletonViewItems(numItems) {
-        if (this.state.skeletonViewItems.length === numItems) {
-            return;
-        }
-
-        if (this.state.skeletonViewItems.length > numItems) {
-            this.setState((prevState) => ({
-                skeletonViewItems: prevState.skeletonViewItems.slice(0, numItems),
-            }));
-            return;
-        }
-
-        const skeletonViewItems = [];
-        for (let i = this.state.skeletonViewItems.length; i < numItems; i++) {
-            const step = i % 3;
-            let lineWidth;
-            switch (step) {
-                case 0:
-                    lineWidth = '100%';
-                    break;
-                case 1:
-                    lineWidth = '50%';
-                    break;
-                default:
-                    lineWidth = '25%';
-            }
-            skeletonViewItems.push(
-                <SkeletonViewContentLoader
-                    key={`skeletonViewItems${i}`}
-                    animate={this.props.shouldAnimate}
-                    height={CONST.LHN_SKELETON_VIEW_ITEM_HEIGHT}
-                    backgroundColor={this.props.theme.skeletonLHNIn}
-                    foregroundColor={this.props.theme.skeletonLHNOut}
-                    style={this.props.themeStyles.mr5}
-                >
+  /**
+   * Generate the skeleton view items.
+   *
+   * @param {Number} numItems
+   */
+  generateSkeletonViewItems(numItems) {
+    const skeletonViewItems = [];
+    for (let i = 0; i < numItems; i++) {
+      const step = i % 3;
+      let lineWidth;
+      switch (step) {
+        case 0:
+          lineWidth = '100%';
+          break;
+        case 1:
+          lineWidth = '50%';
+          break;
+        default:
+          lineWidth = '25%';
+      }
+      skeletonViewItems.push(
+        <SkeletonViewContentLoader
+          key={`skeletonViewItems${i}`}
+          animate={this.props.shouldAnimate}
+          height={CONST.LHN_SKELETON_VIEW_ITEM_HEIGHT}
+          backgroundColor={this.props.theme.skeletonLHNIn}
+          foregroundColor={this.props.theme.skeletonLHNOut}
+          style={this.props.themeStyles.mr5}
+        >
                     <Circle
                         cx="40"
                         cy="32"
@@ -84,31 +75,26 @@ class OptionsListSkeletonView extends React.Component {
                         width={lineWidth}
                         height="8"
                     />
-                </SkeletonViewContentLoader>,
-            );
-        }
-
-        this.setState((prevState) => ({
-            skeletonViewItems: [...prevState.skeletonViewItems, ...skeletonViewItems],
-        }));
+                    </SkeletonViewContentLoader>,
+      );
     }
 
-    render() {
-        return (
-            <View
-                style={[this.props.themeStyles.flex1, this.props.themeStyles.overflowHidden]}
-                onLayout={(event) => {
-                    const numItems = Math.ceil(event.nativeEvent.layout.height / CONST.LHN_SKELETON_VIEW_ITEM_HEIGHT);
-                    this.generateSkeletonViewItems(numItems);
-                }}
-            >
-                <View>{this.state.skeletonViewItems}</View>
-            </View>
-        );
-    }
+    return skeletonViewItems;
+  }
+
+  render() {
+    return (
+      <View
+        style={[this.props.themeStyles.flex1, this.props.themeStyles.overflowHidden]}
+      >
+        <View>{this.state.skeletonViewItems}</View>
+      </View>
+    );
+  }
 }
 
 OptionsListSkeletonView.propTypes = propTypes;
 OptionsListSkeletonView.defaultProps = defaultTypes;
 
 export default compose(withThemeStyles, withTheme)(OptionsListSkeletonView);
+
