7617e93405914e63a6488a63e82aebee64abcdb0
Nikki Wines <nikkiwines@expensify.com>
2023-12-22 21:29:30 -0500

    update getOnyxDataForOpenOrReconnect to use finallyData

--- src/libs/Middleware/SaveResponseInOnyx.ts
+++ src/libs/Middleware/SaveResponseInOnyx.ts
@@ -14,7 +14,7 @@ const SaveResponseInOnyx: Middleware = (requestResponse, request) =>
 
         // Sometimes we call requests that are successfull but they don't have any response or any success/failure data to set. Let's return early since
         // we don't need to store anything here.
-        if (!onyxUpdates && (!request.successData && !request.failureData || !request.finallyData)) {
+        if (!onyxUpdates && ((!request.successData && !request.failureData) || !request.finallyData)) {
             return Promise.resolve(response);
         }
 
