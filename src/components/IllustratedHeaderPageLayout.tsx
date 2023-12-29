4639673314ac8c322f220405a8931f6e11512e05
Filip Solecki <filip.solecki@swmansion.com>
2023-12-29 13:40:17 +0100

    [TS migration] Migrate 'IllustratedHeaderPageLayout.js' component to TypeScript

--- src/components/IllustratedHeaderPageLayout.tsx
+++ src/components/IllustratedHeaderPageLayout.tsx
@@ -1,43 +1,32 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
-import HeaderPageLayout from './HeaderPageLayout';
-import headerWithBackButtonPropTypes from './HeaderWithBackButton/headerWithBackButtonPropTypes';
+import HeaderPageLayout, {HeaderPageLayoutProps} from './HeaderPageLayout';
 import Lottie from './Lottie';
+import DotLottieAnimation from './LottieAnimations/types';
 
-const propTypes = {
-    ...headerWithBackButtonPropTypes,
-
+type IllustratedHeaderPageLayoutProps = {
     /** Children to display in the lower half of the page (below the header section w/ an animation) */
-    children: PropTypes.node.isRequired,
+    children?: React.ReactNode;
 
-    /** The illustration to display in the header. Can be either an SVG component or a JSON object representing a Lottie animation. */
-    illustration: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
+    /** The illustration to display in the header. Can be a JSON object representing a Lottie animation. */
+    illustration: DotLottieAnimation;
 
     /** The background color to apply in the upper half of the screen. */
-    backgroundColor: PropTypes.string,
-
-    /** A fixed footer to display at the bottom of the page. */
-    footer: PropTypes.node,
+    backgroundColor?: string;
 
     /** Overlay content to display on top of animation */
-    overlayContent: PropTypes.func,
+    overlayContent?: () => React.ReactNode;
+
+    propsToPassToHeader: HeaderPageLayoutProps;
 };
 
-const defaultProps = {
-    backgroundColor: undefined,
-    footer: null,
-    overlayContent: null,
-};
-
-function IllustratedHeaderPageLayout({backgroundColor, children, illustration, footer, overlayContent, ...propsToPassToHeader}) {
+function IllustratedHeaderPageLayout({backgroundColor, children, illustration, overlayContent, propsToPassToHeader}: IllustratedHeaderPageLayoutProps) {
     const theme = useTheme();
     const styles = useThemeStyles();
     return (
         <HeaderPageLayout
-            backgroundColor={backgroundColor || theme.appBG}
-            title={propsToPassToHeader.title}
+            backgroundColor={backgroundColor ?? theme.appBG}
             headerContent={
                 <>
                     <Lottie
@@ -47,11 +36,10 @@ function IllustratedHeaderPageLayout({backgroundColor, children, illustration, f
                         autoPlay
                         loop
                     />
-                    {overlayContent && overlayContent()}
+                    {overlayContent?.()}
                 </>
             }
             headerContainerStyles={[styles.justifyContentCenter, styles.w100]}
-            footer={footer}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...propsToPassToHeader}
         >
@@ -60,8 +48,6 @@ function IllustratedHeaderPageLayout({backgroundColor, children, illustration, f
     );
 }
 
-IllustratedHeaderPageLayout.propTypes = propTypes;
-IllustratedHeaderPageLayout.defaultProps = defaultProps;
 IllustratedHeaderPageLayout.displayName = 'IllustratedHeaderPageLayout';
 
 export default IllustratedHeaderPageLayout;
