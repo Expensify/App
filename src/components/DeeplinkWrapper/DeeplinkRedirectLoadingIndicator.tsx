24b7c4a16221d02a367669515c14d47524123d92
Shubham Agrawal <shubham.agra1206@gmail.com>
2023-12-30 20:32:38 +0530

    fixed illustrations

--- src/components/DeeplinkWrapper/DeeplinkRedirectLoadingIndicator.tsx
+++ src/components/DeeplinkWrapper/DeeplinkRedirectLoadingIndicator.tsx
@@ -36,7 +36,6 @@ function DeeplinkRedirectLoadingIndicator({openLinkInBrowser, session}: Deeplink
                     <Icon
                         width={200}
                         height={164}
-                        fill={theme.icon}
                         src={Illustrations.RocketBlue}
                     />
                 </View>
--- src/components/ValidateCode/ValidateCodeModal.tsx
+++ src/components/ValidateCode/ValidateCodeModal.tsx
@@ -39,7 +39,6 @@ function ValidateCodeModal({code, accountID, session = {}}: ValidateCodeModalPro
                     <Icon
                         width={variables.modalTopIconWidth}
                         height={variables.modalTopIconHeight}
-                        fill={theme.icon}
                         src={Illustrations.MagicCode}
                     />
                 </View>
