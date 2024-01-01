becc82a7986275b72ee731703986d7386bc5c399
Samil Abud <samilabud@gmail.com>
2024-01-01 13:32:00 -0400

    Added suggestion to redirect

--- src/libs/Navigation/AppNavigator/ReportScreenIDSetter.ts
+++ src/libs/Navigation/AppNavigator/ReportScreenIDSetter.ts
@@ -49,8 +49,7 @@ function ReportScreenIDSetter({route, reports, policies, navigation, isFirstTime
             const reportActionID = route?.params?.reportActionID;
             const regexValidReportActionID = new RegExp(/^\d*$/);
             if (reportActionID && !regexValidReportActionID.test(reportActionID)) {
-                Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(route?.params?.reportID));
-                return;
+                navigation.setParams({reportActionID: ''});
             }
             App.confirmReadyToOpenApp();
             return;
