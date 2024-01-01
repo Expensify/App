093e8a4b1a49397814d790b1221e7a9da4fdfd47
Pavlo Tsimura <paultsimura@gmail.com>
2024-01-01 13:03:01 +0100

    Allow missing smartscan fields on non-expense IOU requests

--- src/libs/TransactionUtils.ts
+++ src/libs/TransactionUtils.ts
@@ -3,7 +3,7 @@ import Onyx, {OnyxCollection, OnyxEntry} from 'react-native-onyx';
 import {ValueOf} from 'type-fest';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import {RecentWaypoint, ReportAction, Transaction} from '@src/types/onyx';
+import {RecentWaypoint, Report, ReportAction, Transaction} from '@src/types/onyx';
 import {Comment, Receipt, Waypoint, WaypointCollection} from '@src/types/onyx/Transaction';
 import {EmptyObject} from '@src/types/utils/EmptyObject';
 import {isCorporateCard, isExpensifyCard} from './CardUtils';
@@ -25,6 +25,13 @@ Onyx.connect({
         }
         allTransactions = Object.fromEntries(Object.entries(value).filter(([, transaction]) => !!transaction));
     },
+});
+
+let allReports: OnyxCollection<Report>;
+Onyx.connect({
+    key: ONYXKEYS.COLLECTION.REPORT,
+    waitForCollectionCallback: true,
+    callback: (value) => (allReports = value),
 });
 
 function isDistanceRequest(transaction: Transaction): boolean {
@@ -416,7 +423,11 @@ function isReceiptBeingScanned(transaction: OnyxEntry<Transaction>): boolean {
  * Check if the transaction has a non-smartscanning receipt and is missing required fields
  */
 function hasMissingSmartscanFields(transaction: OnyxEntry<Transaction>): boolean {
-    return Boolean(transaction && hasReceipt(transaction) && !isDistanceRequest(transaction) && !isReceiptBeingScanned(transaction) && areRequiredFieldsEmpty(transaction));
+    const parentReport = allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${transaction?.reportID}`] ?? null;
+    const isFromExpenseReport = parentReport?.type === CONST.REPORT.TYPE.EXPENSE;
+    return Boolean(
+        transaction && hasReceipt(transaction) && isFromExpenseReport && !isDistanceRequest(transaction) && !isReceiptBeingScanned(transaction) && areRequiredFieldsEmpty(transaction),
+    );
 }
 
 /**
