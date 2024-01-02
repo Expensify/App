1150212d7164a20ffd5f03c97f36266150c5c26d
Agata Kosior <agata.kosior57@gmail.com>
2024-01-02 10:43:07 +0100

    fix: minor fix

--- src/libs/OptionsListUtils.js
+++ src/libs/OptionsListUtils.js
@@ -519,7 +519,7 @@ function createOption(accountIDs, personalDetails, report, reportActions = {}, {
                 (lastReportActions[report.reportID] && lastReportActions[report.reportID].originalMessage && lastReportActions[report.reportID].originalMessage.reason) ||
                 CONST.REPORT.ARCHIVE_REASON.DEFAULT;
             lastMessageText = Localize.translate(preferredLocale, `reportArchiveReasons.${archiveReason}`, {
-                displayName: archiveReason.displayName || ReportUtils.getDisplayNameForParticipant(lastActorDetails?.accountID) || Localize.translateLocal('common.hidden'),
+                displayName: archiveReason.displayName || ReportUtils.getDisplayNameForParticipant(report.lastActorAccountID) || Localize.translateLocal('common.hidden'),
                 policyName: ReportUtils.getPolicyName(report),
             });
         }
