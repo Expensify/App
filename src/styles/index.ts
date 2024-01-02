e5db8866c7ddbdb9a4790f0a4025868f8b2bc053
Yauheni <HorbachZhenja@yandex.by>
2024-01-02 12:29:46 +0300

    Update colors

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2865,7 +2865,7 @@ const styles = (theme: ThemeColors) =>
 
         smallEditIcon: {
             alignItems: 'center',
-            backgroundColor: theme.buttonHoveredBG,
+            backgroundColor: theme.buttonDefaultBG,
             borderColor: theme.appBG,
             borderRadius: 14,
             borderWidth: 3,
