ffd4db3556702a4c5875d6c15231fd3754af9e63
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-01-05 00:23:25 +0800

    set mini context menu unselectable

--- src/styles/utils/index.ts
+++ src/styles/utils/index.ts
@@ -1345,6 +1345,7 @@ const createStyleUtils = (theme: ThemeColors, styles: ThemeStyles) => ({
             ...(isReportActionItemGrouped ? positioning.tn8 : positioning.tn4),
             ...positioning.r4,
             ...styles.cursorDefault,
+            ...styles.userSelectNone,
             position: 'absolute',
             zIndex: 8,
         }),
