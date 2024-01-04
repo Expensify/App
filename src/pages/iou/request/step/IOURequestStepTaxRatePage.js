f558497bacc30d13ae591bbc1667e6809b5b5419
Etotaziba Olei <teneeto@gmail.com>
2024-01-04 01:09:10 +0100

    use StepScreenWrapper and backTo for back navigation

--- src/pages/iou/request/step/IOURequestStepTaxRatePage.js
+++ src/pages/iou/request/step/IOURequestStepTaxRatePage.js
@@ -1,9 +1,6 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import {withOnyx} from 'react-native-onyx';
 import _ from 'underscore';
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
-import ScreenWrapper from '@components/ScreenWrapper';
 import TaxPicker from '@components/TaxPicker';
 import taxPropTypes from '@components/taxPropTypes';
 import transactionPropTypes from '@components/transactionPropTypes';
@@ -16,21 +13,14 @@ import * as TransactionUtils from '@libs/TransactionUtils';
 import * as IOU from '@userActions/IOU';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
+import IOURequestStepRoutePropTypes from './IOURequestStepRoutePropTypes';
+import StepScreenWrapper from './StepScreenWrapper';
 import withFullTransactionOrNotFound from './withFullTransactionOrNotFound';
 import withWritableReportOrNotFound from './withWritableReportOrNotFound';
 
 const propTypes = {
-    /** Route from navigation */
-    route: PropTypes.shape({
-        /** Params from the route */
-        params: PropTypes.shape({
-            /** The type of IOU report, i.e. bill, request, send */
-            iouType: PropTypes.string,
-
-            /** The report ID of the IOU */
-            reportID: PropTypes.string,
-        }),
-    }).isRequired,
+    /** Navigation route context info provided by react navigation */
+    route: IOURequestStepRoutePropTypes.isRequired,
 
     /* Onyx Props */
     /** Collection of tax rates attached to a policy */
@@ -52,16 +42,16 @@ const getTaxAmount = (taxRates, selectedTaxRate, amount) => {
 
 function IOURequestStepTaxRatePage({
     route: {
-        params: {iouType, reportID},
+        params: {iouType, reportID, backTo},
     },
     policyTaxRates,
     transaction,
 }) {
     const {translate} = useLocalize();
 
-    function navigateBack() {
-        Navigation.goBack(ROUTES.MONEY_REQUEST_CONFIRMATION.getRoute(iouType, reportID));
-    }
+    const navigateBack = () => {
+        Navigation.goBack(backTo || ROUTES.HOME);
+    };
 
     const defaultTaxKey = policyTaxRates.defaultExternalID;
     const defaultTaxName = (defaultTaxKey && `${policyTaxRates.taxes[defaultTaxKey].name} (${policyTaxRates.taxes[defaultTaxKey].value}) • ${translate('common.default')}`) || '';
@@ -77,26 +67,18 @@ function IOURequestStepTaxRatePage({
     };
 
     return (
-        <ScreenWrapper
-            includeSafeAreaPaddingBottom={false}
-            shouldEnableMaxHeight
+        <StepScreenWrapper
+            headerTitle={translate('iou.taxRate')}
+            onBackButtonPress={navigateBack}
+            shouldShowWrapper
             testID={IOURequestStepTaxRatePage.displayName}
         >
-            {({insets}) => (
-                <>
-                    <HeaderWithBackButton
-                        title={translate('iou.taxRate')}
-                        onBackButtonPress={() => navigateBack()}
-                    />
-                    <TaxPicker
-                        selectedTaxRate={selectedTaxRate}
-                        policyTaxRates={policyTaxRates}
-                        insets={insets}
-                        onSubmit={updateTaxRates}
-                    />
-                </>
-            )}
-        </ScreenWrapper>
+            <TaxPicker
+                selectedTaxRate={selectedTaxRate}
+                policyTaxRates={policyTaxRates}
+                onSubmit={updateTaxRates}
+            />
+        </StepScreenWrapper>
     );
 }
 
