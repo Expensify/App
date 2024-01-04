862f951c6fe16c71902ecba7cded92f5b15d59ae
bartektomczyk <bartlomiejtomczyk@gmail.com>
2024-01-04 14:34:04 +0100

    refactor: migrated LegalNameUBO

--- src/pages/ReimbursementAccount/BeneficialOwnerInfo/substeps/BeneficialOwnerDetailsFormSubsteps/LegalNameUBO.tsx
+++ src/pages/ReimbursementAccount/BeneficialOwnerInfo/substeps/BeneficialOwnerDetailsFormSubsteps/LegalNameUBO.tsx
@@ -1,48 +1,45 @@
 import lodashGet from 'lodash/get';
-import PropTypes from 'prop-types';
 import React from 'react';
 import {withOnyx} from 'react-native-onyx';
+import {OnyxEntry} from 'react-native-onyx/lib/types';
 import Form from '@components/Form';
+import InputWrapper from '@components/Form/InputWrapper';
 import Text from '@components/Text';
 import TextInput from '@components/TextInput';
 import useLocalize from '@hooks/useLocalize';
+import {SubStepProps} from '@hooks/useSubStep/types';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as ValidationUtils from '@libs/ValidationUtils';
-import reimbursementAccountDraftPropTypes from '@pages/ReimbursementAccount/ReimbursementAccountDraftPropTypes';
-import subStepPropTypes from '@pages/ReimbursementAccount/subStepPropTypes';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-
-const propTypes = {
-    /** The draft values of the bank account being setup */
-    reimbursementAccountDraft: reimbursementAccountDraftPropTypes,
-
-    /** ID of the beneficial owner that is being modified */
-    beneficialOwnerBeingModifiedID: PropTypes.string.isRequired,
-
-    ...subStepPropTypes,
-};
-
-const defaultProps = {
-    reimbursementAccountDraft: {},
-};
+import {ReimbursementAccountDraft} from '@src/types/onyx';
+import {FormValues} from '@src/types/onyx/Form';
+import {BeneficialOwnerDraftData} from '@src/types/onyx/ReimbursementAccountDraft';
 
 const {FIRST_NAME, LAST_NAME} = CONST.BANK_ACCOUNT.BENEFICIAL_OWNER_INFO_STEP.BENEFICIAL_OWNER_DATA;
 const BENEFICIAL_OWNER_PREFIX = CONST.BANK_ACCOUNT.BENEFICIAL_OWNER_INFO_STEP.BENEFICIAL_OWNER_DATA.PREFIX;
 
-function LegalNameUBO({reimbursementAccountDraft, onNext, isEditing, beneficialOwnerBeingModifiedID}) {
+type LegalNameUBOOnyxProps = {
+    /** The draft values of the bank account being setup */
+    reimbursementAccountDraft: OnyxEntry<ReimbursementAccountDraft>;
+};
+type LegalNameUBOProps = SubStepProps & LegalNameUBOOnyxProps & {beneficialOwnerBeingModifiedID: string};
+type FromValues = BeneficialOwnerDraftData;
+
+function LegalNameUBO({reimbursementAccountDraft, onNext, isEditing, beneficialOwnerBeingModifiedID}: LegalNameUBOProps) {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
 
-    const firstNameInputID = `${BENEFICIAL_OWNER_PREFIX}_${beneficialOwnerBeingModifiedID}_${FIRST_NAME}`;
-    const lastNameInputID = `${BENEFICIAL_OWNER_PREFIX}_${beneficialOwnerBeingModifiedID}_${LAST_NAME}`;
+    const firstNameInputID: keyof FromValues = `${BENEFICIAL_OWNER_PREFIX}_${beneficialOwnerBeingModifiedID}_${FIRST_NAME}`;
+    const lastNameInputID: keyof FormValues = `${BENEFICIAL_OWNER_PREFIX}_${beneficialOwnerBeingModifiedID}_${LAST_NAME}`;
 
     const defaultFirstName = lodashGet(reimbursementAccountDraft, firstNameInputID, '');
     const defaultLastName = lodashGet(reimbursementAccountDraft, lastNameInputID, '');
 
-    const validate = (values) => ValidationUtils.getFieldRequiredErrors(values, [firstNameInputID, lastNameInputID]);
+    const validate = (values: FromValues) => ValidationUtils.getFieldRequiredErrors(values, [firstNameInputID, lastNameInputID]);
 
     return (
+        // @ts-expect-error TODO: Remove this once Form (https://github.com/Expensify/App/issues/31972) is migrated to TypeScript
         <Form
             formID={ONYXKEYS.REIMBURSEMENT_ACCOUNT}
             submitButtonText={isEditing ? translate('common.confirm') : translate('common.next')}
@@ -52,7 +49,9 @@ function LegalNameUBO({reimbursementAccountDraft, onNext, isEditing, beneficialO
             submitButtonStyles={[styles.pb5, styles.mb0]}
         >
             <Text style={styles.textHeadline}>{translate('beneficialOwnerInfoStep.enterLegalFirstAndLastName')}</Text>
-            <TextInput
+            <InputWrapper
+                // @ts-expect-error TODO: Remove this once InputWrapper (https://github.com/Expensify/App/issues/31972) is migrated to TypeScript
+                InputComponent={TextInput}
                 label={translate('beneficialOwnerInfoStep.legalFirstName')}
                 aria-label={translate('beneficialOwnerInfoStep.legalFirstName')}
                 role={CONST.ROLE.PRESENTATION}
@@ -61,7 +60,9 @@ function LegalNameUBO({reimbursementAccountDraft, onNext, isEditing, beneficialO
                 defaultValue={defaultFirstName}
                 shouldSaveDraft
             />
-            <TextInput
+            <InputWrapper
+                // @ts-expect-error TODO: Remove this once InputWrapper (https://github.com/Expensify/App/issues/31972) is migrated to TypeScript
+                InputComponent={TextInput}
                 label={translate('beneficialOwnerInfoStep.legalLastName')}
                 aria-label={translate('beneficialOwnerInfoStep.legalLastName')}
                 role={CONST.ROLE.PRESENTATION}
@@ -74,11 +75,9 @@ function LegalNameUBO({reimbursementAccountDraft, onNext, isEditing, beneficialO
     );
 }
 
-LegalNameUBO.propTypes = propTypes;
-LegalNameUBO.defaultProps = defaultProps;
 LegalNameUBO.displayName = 'LegalNameUBO';
 
-export default withOnyx({
+export default withOnyx<LegalNameUBOProps, LegalNameUBOOnyxProps>({
     reimbursementAccountDraft: {
         key: ONYXKEYS.REIMBURSEMENT_ACCOUNT_DRAFT,
     },
