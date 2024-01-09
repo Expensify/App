6cc3f5c9f89c0058ab99adcef810b07dc34d52c7
Brandon Stites <bstites+1@expensify.com>
2024-01-09 11:08:51 -0700

    Move message copy outside of component lifecycle

--- src/components/PurposeForUsingExpensifyModal.js
+++ src/components/PurposeForUsingExpensifyModal.js
@@ -39,6 +39,57 @@ const defaultProps = {
     session: {},
 };
 
+// This is not translated because it is a message coming from concierge, which only supports english
+const messageCopy = {
+    track:
+        'Great! To track your expenses, I suggest you create a workspace to keep everything contained:\n' +
+        '\n' +
+        '1. Press your avatar icon\n' +
+        '2. Choose Workspaces\n' +
+        '3. Choose New Workspace\n' +
+        '4. Name your workspace something meaningful (eg, "My Business Expenses")\n' +
+        '\n' +
+        'Once you have your workspace set up, you can add expenses to it as follows:\n' +
+        '\n' +
+        '1. Choose My Business Expenses (or whatever you named it) in the list of chat rooms\n' +
+        '2. Choose the + button in the chat compose window\n' +
+        '3. Choose Request money\n' +
+        "4. Choose what kind of expense you'd like to log, whether a manual expense, scanned receipt, or tracked distance.\n" +
+        '\n' +
+        "That'll be stored in your My Business Expenses room for your later access. Thanks for asking, and let me know how it goes!",
+    submit:
+        'Hi there, to submit expenses for reimbursement, please:\n' +
+        '\n' +
+        '1. Press the big green + button\n' +
+        '2. Choose Request money\n' +
+        '3. Indicate how much to request, either manually, by scanning a receipt, or by tracking distance\n' +
+        '4. Enter the email address or phone number of your boss\n' +
+        '\n' +
+        "And we'll take it from there to get you paid back. Please give it a shot and let me know how it goes!",
+    business:
+        "Great! To manage your team's expenses, create a workspace to keep everything contained:\n" +
+        '\n' +
+        '1. Press your avatar icon\n' +
+        '2. Choose Workspaces\n' +
+        '3. Choose New Workspace\n' +
+        '4. Name your workspace something meaningful (eg, "Galaxy Food Inc.")\n' +
+        '\n' +
+        'Once you have your workspace set up, you can invite your team to it via the Members pane and connect a business bank account to reimburse them!',
+    chatSplit:
+        'Hi there, to split an expense such as with a friend, please:\n' +
+        '\n' +
+        'Press the big green + button\n' +
+        'Choose *Request money*\n' +
+        'Indicate how much was spent, either manually, by scanning a receipt, or by tracking distance\n' +
+        'Enter the email address or phone number of your friend\n' +
+        'Press *Split* next to their name\n' +
+        'Repeat as many times as you like for each of your friends\n' +
+        'Press *Add to split* when done adding friends\n' +
+        'Press Split to split the bill\n' +
+        '\n' +
+        "This will send a money request to each of your friends for however much they owe you, and we'll take care of getting you paid back. Thanks for asking, and let me know how it goes!",
+};
+
 function PurposeForUsingExpensifyModal(props) {
     const {translate} = useLocalize();
     const StyleUtils = useStyleUtils();
@@ -60,57 +111,6 @@ function PurposeForUsingExpensifyModal(props) {
         Welcome.show({routes, showEngagementModal: () => setIsModalOpen(true)});
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    // This is not translated because it is a message coming from concierge, which only supports english
-    const messageCopy = {
-        track:
-            'Great! To track your expenses, I suggest you create a workspace to keep everything contained:\n' +
-            '\n' +
-            '1. Press your avatar icon\n' +
-            '2. Choose Workspaces\n' +
-            '3. Choose New Workspace\n' +
-            '4. Name your workspace something meaningful (eg, "My Business Expenses")\n' +
-            '\n' +
-            'Once you have your workspace set up, you can add expenses to it as follows:\n' +
-            '\n' +
-            '1. Choose My Business Expenses (or whatever you named it) in the list of chat rooms\n' +
-            '2. Choose the + button in the chat compose window\n' +
-            '3. Choose Request money\n' +
-            "4. Choose what kind of expense you'd like to log, whether a manual expense, scanned receipt, or tracked distance.\n" +
-            '\n' +
-            "That'll be stored in your My Business Expenses room for your later access. Thanks for asking, and let me know how it goes!",
-        submit:
-            'Hi there, to submit expenses for reimbursement, please:\n' +
-            '\n' +
-            '1. Press the big green + button\n' +
-            '2. Choose Request money\n' +
-            '3. Indicate how much to request, either manually, by scanning a receipt, or by tracking distance\n' +
-            '4. Enter the email address or phone number of your boss\n' +
-            '\n' +
-            "And we'll take it from there to get you paid back. Please give it a shot and let me know how it goes!",
-        business:
-            "Great! To manage your team's expenses, create a workspace to keep everything contained:\n" +
-            '\n' +
-            '1. Press your avatar icon\n' +
-            '2. Choose Workspaces\n' +
-            '3. Choose New Workspace\n' +
-            '4. Name your workspace something meaningful (eg, "Galaxy Food Inc.")\n' +
-            '\n' +
-            'Once you have your workspace set up, you can invite your team to it via the Members pane and connect a business bank account to reimburse them!',
-        chatSplit:
-            'Hi there, to split an expense such as with a friend, please:\n' +
-            '\n' +
-            'Press the big green + button\n' +
-            'Choose *Request money*\n' +
-            'Indicate how much was spent, either manually, by scanning a receipt, or by tracking distance\n' +
-            'Enter the email address or phone number of your friend\n' +
-            'Press *Split* next to their name\n' +
-            'Repeat as many times as you like for each of your friends\n' +
-            'Press *Add to split* when done adding friends\n' +
-            'Press Split to split the bill\n' +
-            '\n' +
-            "This will send a money request to each of your friends for however much they owe you, and we'll take care of getting you paid back. Thanks for asking, and let me know how it goes!",
-    };
 
     const closeModal = useCallback(() => {
         Report.dismissEngagementModal();
