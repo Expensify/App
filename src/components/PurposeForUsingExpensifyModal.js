aa6398a4ba4f9525ba6e70b87baf13d8512947a3
Brandon Stites <bstites+1@expensify.com>
2024-01-09 17:20:01 -0700

    Add view for max height

--- src/components/PurposeForUsingExpensifyModal.js
+++ src/components/PurposeForUsingExpensifyModal.js
@@ -94,7 +94,7 @@ function PurposeForUsingExpensifyModal(props) {
     const {translate} = useLocalize();
     const StyleUtils = useStyleUtils();
     const styles = useThemeStyles();
-    const {isSmallScreenWidth} = useWindowDimensions();
+    const {isSmallScreenWidth, windowHeight} = useWindowDimensions();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const theme = useTheme();
 
@@ -163,45 +163,49 @@ function PurposeForUsingExpensifyModal(props) {
     ];
 
     return (
-        <Modal
-            type={isSmallScreenWidth ? CONST.MODAL.MODAL_TYPE.BOTTOM_DOCKED : CONST.MODAL.MODAL_TYPE.RIGHT_DOCKED}
-            isVisible={isModalOpen}
-            onClose={closeModal}
-            innerContainerStyle={styles.pt0}
-            shouldUseCustomBackdrop={!isSmallScreenWidth}
-        >
-            <ScrollView>
-                <View style={StyleUtils.getBackgroundColorStyle(theme.PAGE_THEMES[SCREENS.SETTINGS.WORKSPACES].backgroundColor)}>
-                    <Lottie
-                        source={LottieAnimations.Hands}
-                        style={styles.w100}
-                        webStyle={styles.w100}
-                        autoPlay
-                        loop
-                    />
-                    <HeaderWithBackButton
-                        shouldShowCloseButton
-                        shouldShowBackButton={false}
-                        onCloseButtonPress={closeModal}
-                        shouldOverlay
-                        iconFill={theme.iconColorfulBackground}
-                    />
+            <Modal
+                type={isSmallScreenWidth ? CONST.MODAL.MODAL_TYPE.BOTTOM_DOCKED : CONST.MODAL.MODAL_TYPE.RIGHT_DOCKED}
+                isVisible={isModalOpen}
+                onClose={closeModal}
+                innerContainerStyle={styles.pt0}
+                shouldUseCustomBackdrop={!isSmallScreenWidth}
+            >
+                <View
+                    style={{maxHeight: windowHeight}}
+                >
+                    <ScrollView>
+                        <View style={StyleUtils.getBackgroundColorStyle(theme.PAGE_THEMES[SCREENS.SETTINGS.WORKSPACES].backgroundColor)}>
+                            <Lottie
+                                source={LottieAnimations.Hands}
+                                style={styles.w100}
+                                webStyle={styles.w100}
+                                autoPlay
+                                loop
+                            />
+                            <HeaderWithBackButton
+                                shouldShowCloseButton
+                                shouldShowBackButton={false}
+                                onCloseButtonPress={closeModal}
+                                shouldOverlay
+                                iconFill={theme.iconColorfulBackground}
+                            />
+                        </View>
+                        <View style={[styles.w100, styles.ph5, styles.pv5]}>
+                            <Text
+                                style={[styles.textHeadline, styles.preWrap, styles.mb2]}
+                                numberOfLines={2}
+                            >
+                                {translate('purposeForExpensify.welcomeMessage')}
+                            </Text>
+                            <Text style={styles.baseFontStyle}>{translate('purposeForExpensify.welcomeSubtitle')}</Text>
+                        </View>
+                        <MenuItemList
+                            menuItems={menuItems}
+                            shouldUseSingleExecution
+                        />
+                    </ScrollView>
                 </View>
-                <View style={[styles.w100, styles.ph5, styles.pv5]}>
-                    <Text
-                        style={[styles.textHeadline, styles.preWrap, styles.mb2]}
-                        numberOfLines={2}
-                    >
-                        {translate('purposeForExpensify.welcomeMessage')}
-                    </Text>
-                    <Text style={styles.baseFontStyle}>{translate('purposeForExpensify.welcomeSubtitle')}</Text>
-                </View>
-                <MenuItemList
-                    menuItems={menuItems}
-                    shouldUseSingleExecution
-                />
-            </ScrollView>
-        </Modal>
+            </Modal>
     );
 }
 
