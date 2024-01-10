1064c3248a590dd44c11e6d3de27f73b8769d950
Cong Pham <ptcong411@gmail.com>
2024-01-10 23:23:20 +0700

    fix eslint

--- src/hooks/useWindowDimensions/index.ts
+++ src/hooks/useWindowDimensions/index.ts
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useRef, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 // eslint-disable-next-line no-restricted-imports
 import {Dimensions, useWindowDimensions} from 'react-native';
 import * as Browser from '@libs/Browser';
