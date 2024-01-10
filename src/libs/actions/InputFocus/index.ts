c8761cb65624c8c9f9ff4242aad56839797469d6
yh-0218 <appstar0112@gmail.com>
2024-01-10 18:15:52 +0200

    update to modal type

--- src/libs/actions/InputFocus/index.ts
+++ src/libs/actions/InputFocus/index.ts
@@ -1,9 +1,9 @@
 import type {Modal} from '@src/types/onyx';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-unused-vars
-function inputFocusChange(_focus: boolean) {}
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/naming-convention
-function composerFocusKeepFocusOn(_ref: HTMLElement, _isFocused: boolean, _modal: Modal, _onyxFocused: boolean) {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function inputFocusChange(focus: boolean) {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function composerFocusKeepFocusOn(ref: HTMLElement, isFocused: boolean, modal: Modal, onyxFocused: boolean) {}
 const callback = (method: () => void) => method();
 
 export {composerFocusKeepFocusOn, inputFocusChange, callback};
