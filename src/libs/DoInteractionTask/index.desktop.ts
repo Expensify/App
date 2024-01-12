31724643760b609d708384986f147784c64046fb
dukenv0307 <dukenv0307@gmail.com>
2024-01-12 23:07:07 +0700

    add comment

--- src/libs/DoInteractionTask/index.desktop.ts
+++ src/libs/DoInteractionTask/index.desktop.ts
@@ -1,5 +1,6 @@
 import {InteractionManager} from 'react-native';
 
+// For desktop, we should call the callback after all interactions to prevent freezing. See more detail in https://github.com/Expensify/App/issues/28916
 function doInteractionTask(callback: () => void) {
     return InteractionManager.runAfterInteractions(() => {
         callback();
