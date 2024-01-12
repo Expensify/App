e713030f8963c0324cfc0446a369487d4d283560
dukenv0307 <dukenv0307@gmail.com>
2024-01-12 10:59:58 +0700

    fix: disable eslint in import Text type

--- src/types/utils/textRef.ts
+++ src/types/utils/textRef.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-restricted-imports
 import type {Text} from 'react-native';
 
 const textRef = (ref: React.RefObject<Text | HTMLDivElement>) => ref as React.RefObject<Text>;
