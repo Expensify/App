48713bb8ba83c51cf1d3332b278aa249f66a251a
Vishrut Agarwalla <vishrutagarwalla19@gmail.com>
2024-01-13 22:56:22 +0530

    Migrated GetAssistancePage

--- src/pages/GetAssistancePage.tsx
+++ src/pages/GetAssistancePage.tsx
@@ -1,7 +1,7 @@
-import lodashGet from 'lodash/get';
-import PropTypes from 'prop-types';
+import type {RouteProp} from '@react-navigation/native';
 import React from 'react';
 import {ScrollView, View} from 'react-native';
+import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Expensicons from '@components/Icon/Expensicons';
@@ -9,53 +9,39 @@ import * as Illustrations from '@components/Icon/Illustrations';
 import ScreenWrapper from '@components/ScreenWrapper';
 import Section from '@components/Section';
 import Text from '@components/Text';
-import withLocalize, {withLocalizePropTypes} from '@components/withLocalize';
+import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
-import compose from '@libs/compose';
 import Navigation from '@libs/Navigation/Navigation';
 import * as Link from '@userActions/Link';
 import * as Report from '@userActions/Report';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
+import type {Account} from '@src/types/onyx';
 
-const propTypes = {
+type GetAssistanceOnyxProps = {
+    account: OnyxEntry<Account>;
+};
+
+type GetAssistancePageProps = GetAssistanceOnyxProps & {
     /** Route object from navigation */
-    route: PropTypes.shape({
-        params: PropTypes.shape({
-            /** The task ID to request the call for */
-            taskID: PropTypes.string,
-        }),
-    }).isRequired,
-
-    /** The details about the account that the user is signing in with */
-    account: PropTypes.shape({
-        /** URL to the assigned guide's appointment booking calendar */
-        guideCalendarLink: PropTypes.string,
-    }),
-
-    ...withLocalizePropTypes,
+    route: RouteProp<{params: {taskID: string; backTo: string}}>;
 };
 
-const defaultProps = {
-    account: {
-        guideCalendarLink: null,
-    },
-};
-
-function GetAssistancePage(props) {
+function GetAssistancePage({route, account}: GetAssistancePageProps) {
     const styles = useThemeStyles();
-    const navigateBackTo = lodashGet(props.route, 'params.backTo', ROUTES.SETTINGS_CONTACT_METHODS);
+    const navigateBackTo = route?.params.backTo ?? ROUTES.SETTINGS_CONTACT_METHODS;
+    const {translate} = useLocalize();
     const menuItems = [
         {
-            title: props.translate('getAssistancePage.chatWithConcierge'),
+            title: translate('getAssistancePage.chatWithConcierge'),
             onPress: () => Report.navigateToConciergeChat(),
             icon: Expensicons.ChatBubble,
             shouldShowRightIcon: true,
             wrapperStyle: [styles.cardMenuItem],
         },
         {
-            title: props.translate('getAssistancePage.exploreHelpDocs'),
+            title: translate('getAssistancePage.exploreHelpDocs'),
             onPress: () => Link.openExternalLink(CONST.NEWHELP_URL),
             icon: Expensicons.QuestionMark,
             shouldShowRightIcon: true,
@@ -66,10 +52,10 @@ function GetAssistancePage(props) {
     ];
 
     // If the user is eligible for calls with their Guide, add the 'Schedule a setup call' item at the second position in the list
-    const guideCalendarLink = lodashGet(props.account, 'guideCalendarLink');
+    const guideCalendarLink = account?.guideCalendarLink;
     if (guideCalendarLink) {
         menuItems.splice(1, 0, {
-            title: props.translate('getAssistancePage.scheduleSetupCall'),
+            title: translate('getAssistancePage.scheduleSetupCall'),
             onPress: () => Link.openExternalLink(guideCalendarLink),
             icon: Expensicons.Phone,
             shouldShowRightIcon: true,
@@ -82,17 +68,17 @@ function GetAssistancePage(props) {
     return (
         <ScreenWrapper testID={GetAssistancePage.displayName}>
             <HeaderWithBackButton
-                title={props.translate('getAssistancePage.title')}
+                title={translate('getAssistancePage.title')}
                 onBackButtonPress={() => Navigation.goBack(navigateBackTo)}
             />
             <ScrollView>
                 <Section
-                    title={props.translate('getAssistancePage.subtitle')}
+                    title={translate('getAssistancePage.subtitle')}
                     icon={Illustrations.ConciergeNew}
                     menuItems={menuItems}
                 >
                     <View style={styles.mv3}>
-                        <Text>{props.translate('getAssistancePage.description')}</Text>
+                        <Text>{translate('getAssistancePage.description')}</Text>
                     </View>
                 </Section>
             </ScrollView>
@@ -100,16 +86,11 @@ function GetAssistancePage(props) {
     );
 }
 
-GetAssistancePage.propTypes = propTypes;
-GetAssistancePage.defaultProps = defaultProps;
 GetAssistancePage.displayName = 'GetAssistancePage';
 
-export default compose(
-    withLocalize,
-    withOnyx({
-        account: {
-            key: ONYXKEYS.ACCOUNT,
-            selector: (account) => account && {guideCalendarLink: account.guideCalendarLink},
-        },
-    }),
-)(GetAssistancePage);
+export default withOnyx<GetAssistancePageProps, GetAssistanceOnyxProps>({
+    account: {
+        key: ONYXKEYS.ACCOUNT,
+        selector: (account) => account && {guideCalendarLink: account.guideCalendarLink},
+    },
+})(GetAssistancePage);
