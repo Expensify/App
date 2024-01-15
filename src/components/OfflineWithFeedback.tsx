b85408fd62052a8aa6e260acae57e1aecb6204d7
VickyStash <vikstash@gmail.com>
2024-01-15 16:50:47 +0100

    Code improvement

--- src/components/OfflineWithFeedback.tsx
+++ src/components/OfflineWithFeedback.tsx
@@ -78,8 +78,12 @@ function OfflineWithFeedback({
     const {isOffline} = useNetwork();
 
     const hasErrors = isNotEmptyObject(errors ?? {});
+
     // Some errors have a null message. This is used to apply opacity only and to avoid showing redundant messages.
-    const errorMessages = Object.fromEntries(Object.entries(errors ?? {}).filter((errorEntry): errorEntry is [string, string] => errorEntry[1] !== null));
+    const errorEntries = Object.entries(errors ?? {});
+    const filteredErrorEntries = errorEntries.filter((errorEntry): errorEntry is [string, string] => errorEntry[1] !== null);
+    const errorMessages = Object.fromEntries(filteredErrorEntries);
+
     const hasErrorMessages = isNotEmptyObject(errorMessages);
     const isOfflinePendingAction = !!isOffline && !!pendingAction;
     const isUpdateOrDeleteError = hasErrors && (pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE || pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE);
