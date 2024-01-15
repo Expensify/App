7f2ac3fac47e9181699bd7915556122c73375112
Filip Solecki <filip.solecki@swmansion.com>
2024-01-15 13:53:58 +0100

    Remove params with no descriptions

--- src/libs/SuggestionUtils.ts
+++ src/libs/SuggestionUtils.ts
@@ -1,10 +1,6 @@
 import CONST from '@src/CONST';
 
-/**
- * Return the max available index for arrow manager.
- * @param numRows
- * @param  isAutoSuggestionPickerLarge
- */
+/** Return the max available index for arrow manager. */
 function getMaxArrowIndex(numRows: number, isAutoSuggestionPickerLarge: boolean): number {
     // rowCount is number of emoji/mention suggestions. For small screen we can fit 3 items
     // and for large we show up to 20 items for mentions/emojis
@@ -16,20 +12,12 @@ function getMaxArrowIndex(numRows: number, isAutoSuggestionPickerLarge: boolean)
     return rowCount - 1;
 }
 
-/**
- * Trims first character of the string if it is a space
- * @param str
- */
+/** Trims first character of the string if it is a space */
 function trimLeadingSpace(str: string): string {
     return str.startsWith(' ') ? str.slice(1) : str;
 }
 
-/**
- * Checks if space is available to render large suggestion menu
- * @param listHeight
- * @param composerHeight
- * @param totalSuggestions
- */
+/** Checks if space is available to render large suggestion menu */
 function hasEnoughSpaceForLargeSuggestionMenu(listHeight: number, composerHeight: number, totalSuggestions: number): boolean {
     const maxSuggestions = CONST.AUTO_COMPLETE_SUGGESTER.MAX_AMOUNT_OF_VISIBLE_SUGGESTIONS_IN_CONTAINER;
     const chatFooterHeight = CONST.CHAT_FOOTER_SECONDARY_ROW_HEIGHT + 2 * CONST.CHAT_FOOTER_SECONDARY_ROW_PADDING;
