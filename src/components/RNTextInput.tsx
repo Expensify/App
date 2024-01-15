a2188fc7318da064f3fc78bf1485b52ea2257af2
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-01-15 16:19:07 +0100

    remove unnecessary eslint disable

--- src/components/RNTextInput.tsx
+++ src/components/RNTextInput.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import type {TextInputProps} from 'react-native';
 import {TextInput} from 'react-native';
 import Animated from 'react-native-reanimated';
-// eslint-disable-next-line no-restricted-imports
 import useTheme from '@hooks/useTheme';
 
 // Convert the underlying TextInput into an Animated component so that we can take an animated ref and pass it to a worklet
