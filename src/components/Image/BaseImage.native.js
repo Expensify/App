5d4609289db24ec11e12ee57e67aecfb2468ecf3
Peter Velkov <kidroca@gmail.com>
2024-01-15 16:31:26 +0200

    Refactor BaseImage.native.js to Align with `expo-image` Deprecation
    - Adapt to `expo-image` deprecation of `event.nativeEvent` usage.
    - Directly use the event object as recommended.
    - Ensure compatibility with components using the `onLoad` prop.

--- src/components/Image/BaseImage.native.js
+++ src/components/Image/BaseImage.native.js
@@ -1,4 +1,29 @@
 import {Image as ExpoImage} from 'expo-image';
+import React, {useCallback} from 'react';
+import {defaultProps, imagePropTypes} from './imagePropTypes';
 
+function BaseImage({onLoad, ...props}) {
+    const imageLoadedSuccessfully = useCallback(
+        (event) => {
+            // We override `onLoad`, so both web and native have the same signature
+            const {width, height} = event.source;
+            onLoad({nativeEvent: {width, height}});
+        },
+        [onLoad],
+    );
 
-export default ExpoImage;
+    return (
+        <ExpoImage
+            // Only subscribe to onLoad when a handler is provided to avoid unnecessary event registrations, optimizing performance.
+            onLoad={onLoad ? imageLoadedSuccessfully : undefined}
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+        />
+    );
+}
+
+BaseImage.propTypes = imagePropTypes;
+BaseImage.defaultProps = defaultProps;
+BaseImage.displayName = 'BaseImage';
+
+export default BaseImage;
