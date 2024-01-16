746fdda8c3c0eb90a5c4bbbf6579f3a31b0f2922
bartektomczyk <bartlomiejtomczyk@gmail.com>
2024-01-16 10:00:12 +0100

    fix: removed new js files

--- tests/unit/useSubStepTest.tsx
+++ tests/unit/useSubStepTest.tsx
@@ -1,5 +1,5 @@
 import {act, renderHook} from '@testing-library/react-native';
-import {Button, Text, View} from 'react-native';
+import {Text} from 'react-native';
 import useSubStep from '@hooks/useSubStep';
 import type {SubStepProps} from '@hooks/useSubStep/types';
 
