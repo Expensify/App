faca563b1f90300f0186bfad56ca5cebd8b57b5f
dukenv0307 <dukenv0307@gmail.com>
2024-01-17 14:17:17 +0700

    disable reply in thread for report preview when delete IOU report in offline

--- src/libs/actions/IOU.js
+++ src/libs/actions/IOU.js
@@ -2568,7 +2568,7 @@ function deleteMoneyRequest(transactionID, reportAction, isSingleTransactionView
         amount: CurrencyUtils.convertToDisplayString(updatedIOUReport.total, updatedIOUReport.currency),
     });
     updatedReportPreviewAction.message[0].text = messageText;
-    updatedReportPreviewAction.message[0].html = messageText;
+    updatedReportPreviewAction.message[0].html = shouldDeleteIOUReport ? '' : messageText;
 
     if (reportPreviewAction.childMoneyRequestCount > 0) {
         updatedReportPreviewAction.childMoneyRequestCount = reportPreviewAction.childMoneyRequestCount - 1;
