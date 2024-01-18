d19a95d4d0a52b2e41c3e949917ec03f3eb095f7
Ishpaul Singh <ishpaul207@gmail.com>
2024-01-18 20:50:11 +0530

    fix prettier diffs

--- src/components/CustomStatusBarAndBackground/index.tsx
+++ src/components/CustomStatusBarAndBackground/index.tsx
@@ -36,7 +36,7 @@ function CustomStatusBarAndBackground({isNested = false}: CustomStatusBarAndBack
         };
     }, [isNested, setRootStatusBarEnabled]);
     const os = getPlatform();
-    const initialBgColor = os === CONST.PLATFORM.WEB ? theme.splashScreen : theme.splashBG
+    const initialBgColor = os === CONST.PLATFORM.WEB ? theme.splashScreen : theme.splashBG;
     const prevStatusBarBackgroundColor = useRef(initialBgColor);
     const statusBarBackgroundColor = useRef(initialBgColor);
     const statusBarAnimation = useSharedValue(0);
