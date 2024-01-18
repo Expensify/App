ef01f3763274c676dc520dc1a3bd446c27e00ee9
tienifr <christianwen18@gmail.com>
2024-01-19 01:59:55 +0700

    remove redundant file

--- src/components/translatableTextPropTypes.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-/**
- * Traslatable text with phrase key and/or variables
- * Use Localize.MaybePhraseKey instead for Typescript
- *
- * E.g. ['common.error.characterLimitExceedCounter', {length: 5, limit: 20}]
- */
-export default PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.object]))]);
