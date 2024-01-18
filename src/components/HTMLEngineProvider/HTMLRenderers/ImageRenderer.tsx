2e0f786821d9557d3645dd0ae8fb6451c798bbc7
ruben-rebelo <ruben.rato96@gmail.com>
2024-01-18 09:53:23 +0000

    [TS migration] Lint fixes

--- src/components/HTMLEngineProvider/HTMLRenderers/ImageRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/ImageRenderer.tsx
@@ -76,16 +76,7 @@ function ImageRenderer({tnode}: ImageRendererProps) {
                         const route = ROUTES.REPORT_ATTACHMENTS.getRoute(report?.reportID ?? '', source);
                         Navigation.navigate(route);
                     }}
-                    onLongPress={(event) =>
-                        showContextMenuForReport(
-                            event,
-                            anchor,
-                            report?.reportID ?? '',
-                            action,
-                            checkIfContextMenuActive,
-                            ReportUtils.isArchivedRoom(report),
-                        )
-                    }
+                    onLongPress={(event) => showContextMenuForReport(event, anchor, report?.reportID ?? '', action, checkIfContextMenuActive, ReportUtils.isArchivedRoom(report))}
                     accessibilityRole={CONST.ACCESSIBILITY_ROLE.IMAGEBUTTON}
                     accessibilityLabel={translate('accessibilityHints.viewAttachment')}
                 >
