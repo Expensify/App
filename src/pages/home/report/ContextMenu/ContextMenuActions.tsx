c884245e7204abad57e01752ec54e4011c1e43e9
Vit Horacek <vit@expensify.com>
2024-01-18 01:29:20 +0000

    Revert "Merge pull request #33836 from Expensify/alberto-unpay2"
    This reverts commit 7bb7bac43204d7ff68da38c9ab3d741d2b3b4121, reversing
    changes made to a1c7f5645001919e6a204b6cf91e500164986170.

--- src/pages/home/report/ContextMenu/ContextMenuActions.tsx
+++ src/pages/home/report/ContextMenu/ContextMenuActions.tsx
@@ -307,11 +307,6 @@ const ContextMenuActions: ContextMenuAction[] = [
                 } else if (ReportActionsUtils.isModifiedExpenseAction(reportAction)) {
                     const modifyExpenseMessage = ModifiedExpenseMessage.getForReportAction(reportAction);
                     Clipboard.setString(modifyExpenseMessage);
-                } else if (ReportActionsUtils.isReimbursementDeQueuedAction(reportAction)) {
-                    const {expenseReportID} = reportAction.originalMessage;
-                    const expenseReport = ReportUtils.getReport(expenseReportID);
-                    const displayMessage = ReportUtils.getReimbursementDeQueuedActionMessage(reportAction, expenseReport);
-                    Clipboard.setString(displayMessage);
                 } else if (ReportActionsUtils.isMoneyRequestAction(reportAction)) {
                     const displayMessage = ReportUtils.getIOUReportActionDisplayMessage(reportAction);
                     Clipboard.setString(displayMessage);
