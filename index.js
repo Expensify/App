2e240a32c7ef5804f153594a964abb887478822b
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-01-22 21:13:56 +0100

    revert wip changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-require('./src/libs/E2E/reactNativeLaunchingTest');
+/**
+ * @format
+ */
+import {AppRegistry} from 'react-native';
+import {enableLegacyWebImplementation} from 'react-native-gesture-handler';
+import App from './src/App';
+import Config from './src/CONFIG';
+import additionalAppSetup from './src/setup';
+
+enableLegacyWebImplementation(true);
+AppRegistry.registerComponent(Config.APP_NAME, () => App);
+additionalAppSetup();
