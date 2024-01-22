b9bbccfde2f5fcbd4d101b08d96cd6ee329bcd0b
tienifr <christianwen18@gmail.com>
2024-01-23 03:11:06 +0700

    fix: code blocks flicker

--- src/pages/home/HeaderView.js
+++ src/pages/home/HeaderView.js
@@ -274,7 +274,7 @@ function HeaderView(props) {
                                             displayNamesWithTooltips={displayNamesWithTooltips}
                                             tooltipEnabled
                                             numberOfLines={1}
-                                            textStyles={[styles.headerText, styles.pre, styles.mw100]}
+                                            textStyles={[styles.headerText, styles.pre]}
                                             shouldUseFullTitle={isChatRoom || isPolicyExpenseChat || isChatThread || isTaskReport}
                                         />
                                         {!_.isEmpty(parentNavigationSubtitleData) && (
