0cc6502443d17483711b9a26bd010e878e944626
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-01-22 15:09:18 +0100

    fix: Migrate SilentCommentUpdater

---
+++ src/pages/home/report/ReportActionCompose/SilentCommentUpdater/types.ts
@@ -0,0 +1,29 @@
+import type {OnyxEntry} from 'react-native-onyx';
+import type {Report} from '@src/types/onyx';
+
+type SilentCommentUpdaterOnyxProps = {
+    /** The comment of the report */
+    comment: OnyxEntry<string>;
+};
+
+type SilentCommentUpdaterProps = {
+    /** Updates the comment */
+    updateComment: (comment: OnyxEntry<string>) => void;
+
+    /** The ID of the report associated with the comment */
+    reportID: string;
+
+    /** The report associated with the comment */
+    report: OnyxEntry<Report>;
+
+    /** The value of the comment */
+    value: string;
+
+    /** The ref of the comment */
+    commentRef: React.RefObject<string>;
+
+    /** The comment of the report */
+    commnet: string;
+} & SilentCommentUpdaterOnyxProps;
+
+export type {SilentCommentUpdaterProps, SilentCommentUpdaterOnyxProps};
