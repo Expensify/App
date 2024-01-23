35c6ca5c117122ad4d7ccee3472e544d7228b41f
ruben-rebelo <ruben.rato96@gmail.com>
2024-01-23 13:44:07 +0000

    [TS Migration] Migrate WorkspaceCard to Typescript

--- src/pages/workspace/card/WorkspaceCardVBANoECardView.tsx
+++ src/pages/workspace/card/WorkspaceCardVBANoECardView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
+import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
 import Button from '@components/Button';
 import * as Expensicons from '@components/Icon/Expensicons';
@@ -7,45 +8,37 @@ import * as Illustrations from '@components/Icon/Illustrations';
 import Section from '@components/Section';
 import Text from '@components/Text';
 import UnorderedList from '@components/UnorderedList';
-import withLocalize, {withLocalizePropTypes} from '@components/withLocalize';
+import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
-import compose from '@libs/compose';
-import userPropTypes from '@pages/settings/userPropTypes';
 import * as Link from '@userActions/Link';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {User} from '@src/types/onyx';
 
-const propTypes = {
+type WorkspaceCardVBANoECardViewOnyxProps = {
     /** Information about the logged in user's account */
-    user: userPropTypes,
-
-    ...withLocalizePropTypes,
+    user: OnyxEntry<User>;
 };
 
-const defaultProps = {
-    user: {},
-};
+type WorkspaceCardVBANoECardViewProps = WorkspaceCardVBANoECardViewOnyxProps;
 
-function WorkspaceCardVBANoECardView(props) {
+function WorkspaceCardVBANoECardView({user}: WorkspaceCardVBANoECardViewProps) {
     const styles = useThemeStyles();
+    const {translate} = useLocalize();
+
     return (
         <>
             <Section
-                title={props.translate('workspace.card.header')}
+                title={translate('workspace.card.header')}
                 icon={Illustrations.CreditCardsNew}
             >
                 <View style={[styles.mv3]}>
                     <UnorderedList
-                        items={[
-                            props.translate('workspace.card.benefit1'),
-                            props.translate('workspace.card.benefit2'),
-                            props.translate('workspace.card.benefit3'),
-                            props.translate('workspace.card.benefit4'),
-                        ]}
+                        items={[translate('workspace.card.benefit1'), translate('workspace.card.benefit2'), translate('workspace.card.benefit3'), translate('workspace.card.benefit4')]}
                     />
                 </View>
                 <Button
-                    text={props.translate('workspace.card.addWorkEmail')}
+                    text={translate('workspace.card.addWorkEmail')}
                     onPress={() => {
                         Link.openOldDotLink(CONST.ADD_SECONDARY_LOGIN_URL);
                     }}
@@ -57,20 +50,15 @@ function WorkspaceCardVBANoECardView(props) {
                     success
                 />
             </Section>
-            {Boolean(props.user.isCheckingDomain) && <Text style={[styles.m5, styles.formError]}>{props.translate('workspace.card.checkingDomain')}</Text>}
+            {Boolean(user?.isCheckingDomain) && <Text style={[styles.m5, styles.formError]}>{translate('workspace.card.checkingDomain')}</Text>}
         </>
     );
 }
 
-WorkspaceCardVBANoECardView.propTypes = propTypes;
-WorkspaceCardVBANoECardView.defaultProps = defaultProps;
 WorkspaceCardVBANoECardView.displayName = 'WorkspaceCardVBANoECardView';
 
-export default compose(
-    withLocalize,
-    withOnyx({
-        user: {
-            key: ONYXKEYS.USER,
-        },
-    }),
-)(WorkspaceCardVBANoECardView);
+export default withOnyx<WorkspaceCardVBANoECardViewProps, WorkspaceCardVBANoECardViewOnyxProps>({
+    user: {
+        key: ONYXKEYS.USER,
+    },
+})(WorkspaceCardVBANoECardView);
