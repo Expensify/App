94c115adec14ae658d098d2d4e0a794e7da411fd
Shubham Agrawal <shubham.agra1206@gmail.com>
2024-01-23 08:16:55 +0530

    fix lint

--- src/components/__mocks__/ConfirmedRoute.tsx
+++ src/components/__mocks__/ConfirmedRoute.tsx
@@ -1,8 +1,8 @@
-import {View} from "react-native";
+import {View} from 'react-native';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
-function ConfirmedRoute(props: any){
-    return <View />
+function ConfirmedRoute(props: any) {
+    return <View />;
 }
 
 export default ConfirmedRoute;
