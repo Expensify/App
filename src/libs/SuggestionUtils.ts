fe4fd495e59e083a6d42cca64e11f0a1de5dafa4
Filip Solecki <filip.solecki@swmansion.com>
2024-01-23 20:58:50 +0100

    Requested review changes

--- src/libs/SuggestionUtils.ts
+++ src/libs/SuggestionUtils.ts
@@ -1,10 +1,14 @@
 import CONST from '@src/CONST';
 
-/** Trims first character of the string if it is a space */
+/**
+ * Trims first character of the string if it is a space
+ */
 function trimLeadingSpace(str: string): string {
     return str.startsWith(' ') ? str.slice(1) : str;
 }
-/** Checks if space is available to render large suggestion menu */
+/**
+ * Checks if space is available to render large suggestion menu
+ */
 function hasEnoughSpaceForLargeSuggestionMenu(listHeight: number, composerHeight: number, totalSuggestions: number): boolean {
     const maxSuggestions = CONST.AUTO_COMPLETE_SUGGESTER.MAX_AMOUNT_OF_VISIBLE_SUGGESTIONS_IN_CONTAINER;
     const chatFooterHeight = CONST.CHAT_FOOTER_SECONDARY_ROW_HEIGHT + 2 * CONST.CHAT_FOOTER_SECONDARY_ROW_PADDING;
