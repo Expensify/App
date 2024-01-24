e66aeabcce1bb4af2082455f782f011bbc3ec4cb
Kaushik Kapadiya <kapadiyakaushik9@gmail.com>
2024-01-24 10:56:27 +0530

    cancel request where darkmode=true

--- src/hooks/useThemePreference.ts
+++ src/hooks/useThemePreference.ts
@@ -1,26 +1,25 @@
-import {useContext, useEffect, useState} from 'react';
+import {useContext, useState, useMemo} from 'react';
 import {useColorScheme} from 'react-native';
 import {PreferredThemeContext} from '@components/OnyxProvider';
 import type {ThemePreferenceWithoutSystem} from '@styles/theme/types';
 import CONST from '@src/CONST';
 
 function useThemePreference() {
-    const [themePreference, setThemePreference] = useState<ThemePreferenceWithoutSystem>(CONST.THEME.FALLBACK);
     const preferredThemeFromStorage = useContext(PreferredThemeContext);
     const systemTheme = useColorScheme();
-
-    useEffect(() => {
-        const theme = preferredThemeFromStorage ?? CONST.THEME.DEFAULT;
-
-        // If the user chooses to use the device theme settings, we need to set the theme preference to the system theme
-        if (theme === CONST.THEME.SYSTEM) {
-            setThemePreference(systemTheme ?? CONST.THEME.FALLBACK);
-        } else {
-            setThemePreference(theme);
-        }
+  
+    const themePreference = useMemo(() => {
+      const theme = preferredThemeFromStorage ?? CONST.THEME.DEFAULT;
+  
+      // If the user chooses to use the device theme settings, set the theme preference to the system theme
+      if (theme === CONST.THEME.SYSTEM) {
+        return systemTheme ?? CONST.THEME.FALLBACK;
+      } else {
+        return theme;
+      }
     }, [preferredThemeFromStorage, systemTheme]);
-
+  
     return themePreference;
-}
+  }
 
 export default useThemePreference;
