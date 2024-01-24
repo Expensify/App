ff81b72f7b3f8de625ca3eb29577249a95013e77
dukenv0307 <dukenv0307@gmail.com>
2024-01-24 15:26:31 +0700

    write new file in typescript

--- src/components/EmojiWithTooltip/index.tsx
+++ src/components/EmojiWithTooltip/index.tsx
@@ -1,23 +1,18 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import type {StyleProp, TextStyle} from 'react-native';
 import {View} from 'react-native';
 import Text from '@components/Text';
 import Tooltip from '@components/Tooltip';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as EmojiUtils from '@libs/EmojiUtils';
 
-const propTypes = {
-    emojiCode: PropTypes.string.isRequired,
-    style: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.object), PropTypes.object]),
+type EmojiWithTooltipProps = {
+    emojiCode: string;
+    style?: StyleProp<TextStyle>;
 };
 
-const defaultProps = {
-    style: undefined,
-};
-
-function EmojiWithTooltip(props) {
+function EmojiWithTooltip({emojiCode, style = undefined}: EmojiWithTooltipProps) {
     const styles = useThemeStyles();
-    const emojiCode = props.emojiCode;
     const emoji = EmojiUtils.findEmojiByCode(emojiCode);
     const emojiName = EmojiUtils.getEmojiName(emoji);
     return (
@@ -36,13 +31,11 @@ function EmojiWithTooltip(props) {
                 </View>
             )}
         >
-            <Text style={props.style}>{emojiCode}</Text>
+            <Text style={style}>{emojiCode}</Text>
         </Tooltip>
     );
 }
 
-EmojiWithTooltip.propTypes = propTypes;
-EmojiWithTooltip.defaultProps = defaultProps;
 EmojiWithTooltip.displayName = 'EmojiWithTooltip';
 
 export default EmojiWithTooltip;
