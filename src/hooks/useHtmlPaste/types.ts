4377ba80382d6839310378120ac0446110be44a3
Fitsum Abebe <engfitsum@gmail.com>
2024-01-25 18:06:25 +0300

    type changes

--- src/hooks/useHtmlPaste/types.ts
+++ src/hooks/useHtmlPaste/types.ts
@@ -1,6 +1,6 @@
 import type {MutableRefObject} from 'react';
 import type {TextInput} from 'react-native';
 
-type UseHtmlPaste = (textInputRef: MutableRefObject<(HTMLTextAreaElement & TextInput) | null>, checkComposerVisibility?: () => boolean, isUnmountedOnBlur?: boolean) => void;
+type UseHtmlPaste = (textInputRef: MutableRefObject<(HTMLTextAreaElement & TextInput) | TextInput | null>, checkComposerVisibility?: () => boolean, isUnmountedOnBlur?: boolean) => void;
 
 export default UseHtmlPaste;
