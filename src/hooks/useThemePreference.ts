2bb60b7de17430ea678466c835414c11efad6393
Kaushik Kapadiya <kapadiyakaushik9@gmail.com>
2024-01-25 10:23:59 +0530

    cancel request where darkmode=true

--- src/hooks/useThemePreference.ts
+++ src/hooks/useThemePreference.ts
@@ -4,17 +4,17 @@ import {PreferredThemeContext} from '@components/OnyxProvider';
 import CONST from '@src/CONST';
 
 function useThemePreference() {
-  const preferredThemeFromStorage = useContext(PreferredThemeContext);
-  const systemTheme = useColorScheme();
+    const preferredThemeFromStorage = useContext(PreferredThemeContext);
+    const systemTheme = useColorScheme();
 
-  const themePreference = useMemo(() => {
-    const theme = preferredThemeFromStorage ?? CONST.THEME.DEFAULT;
+    const themePreference = useMemo(() => {
+        const theme = preferredThemeFromStorage ?? CONST.THEME.DEFAULT;
 
-    // If the user chooses to use the device theme settings, set the theme preference to the system theme
-    return theme === CONST.THEME.SYSTEM ? (systemTheme ?? CONST.THEME.FALLBACK) : theme;
-  }, [preferredThemeFromStorage, systemTheme]);
+        // If the user chooses to use the device theme settings, set the theme preference to the system theme
+        return theme === CONST.THEME.SYSTEM ? systemTheme ?? CONST.THEME.FALLBACK : theme;
+    }, [preferredThemeFromStorage, systemTheme]);
 
-  return themePreference;
+    return themePreference;
 }
 
 export default useThemePreference;
