50600bc3a5b4153ce82fade2347d0fb791013eb2
Gray Lewis <grayjordann@gmail.com>
2024-01-25 17:05:39 +0100

    improved country search re-push

--- src/libs/searchCountryOptions.ts
+++ src/libs/searchCountryOptions.ts
@@ -14,26 +14,59 @@ type CountryData = {
  * @returns An array of countries/states sorted based on the search query
  */
 function searchCountryOptions(searchValue: string, countriesData: CountryData[]): CountryData[] {
-    if (!searchValue) {
-        return countriesData;
-    }
-
     const trimmedSearchValue = StringUtils.sanitizeString(searchValue);
     if (!trimmedSearchValue) {
         return [];
     }
 
-    const filteredData = countriesData.filter((country) => country.searchValue.includes(trimmedSearchValue));
+    const filteredData = countriesData.filter((country) => StringUtils.sanitizeString(country.searchValue).includes(trimmedSearchValue));
 
-    return filteredData.sort((a, b) => {
-        if (a.value.toLowerCase() === trimmedSearchValue) {
+    const halfSorted = filteredData.sort((a, b) => {
+        // Prioritize matches at the beginning of the string
+        // e.g. For the search term "Bar" "Barbados" should be prioritized over Antigua & Barbuda
+        // The first two characters are the country code, so we start at index 2
+        // and end at the length of the search term
+        const countryNameASubstring = a.searchValue.toLowerCase().substring(2, trimmedSearchValue.length + 2);
+        const countryNameBSubstring = b.searchValue.toLowerCase().substring(2, trimmedSearchValue.length + 2);
+        if (countryNameASubstring === trimmedSearchValue.toLowerCase()) {
             return -1;
         }
-        if (b.value.toLowerCase() === trimmedSearchValue) {
+        if (countryNameBSubstring === trimmedSearchValue.toLowerCase()) {
             return 1;
         }
         return 0;
     });
+
+    let fullSorted;
+
+    if (trimmedSearchValue !== searchValue.toLowerCase()) {
+        // Diacritic detected, prioritize diacritic matches
+        // We search for diacritic matches by using the unsanitized country name and search term
+        fullSorted = halfSorted.sort((a, b) => {
+            const unsanitizedCountryNameA = a.text.toLowerCase();
+            const unsanitizedCountryNameB = b.text.toLowerCase();
+            if (unsanitizedCountryNameA.includes(searchValue.toLowerCase())) {
+                return -1;
+            }
+            if (unsanitizedCountryNameB.includes(searchValue.toLowerCase())) {
+                return 1;
+            }
+            return 0;
+        });
+    } else {
+        // Diacritic not detected, prioritize country code matches (country codes can never contain diacritics)
+        // E.g. the search term 'US' should push 'United States' to the top
+        fullSorted = halfSorted.sort((a, b) => {
+            if (a.value.toLowerCase() === trimmedSearchValue) {
+                return -1;
+            }
+            if (b.value.toLowerCase() === trimmedSearchValue) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+    return fullSorted;
 }
 
 export default searchCountryOptions;
