454031be34c8817f25861d7adf0d4fa9b145c38f
Antony Kithinzi <kithinzi97@gmail.com>
2024-01-26 03:10:58 +0300

    Update ROUTES.ts

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -177,7 +177,7 @@ const ROUTES = {
     },
     REPORT_WITH_ID_DETAILS: {
         route: 'r/:reportID/details',
-        getRoute: (reportID: string, backTo?: string) => getUrlWithBackToParam(`r/${reportID}`, backTo),
+        getRoute: (reportID: string) => `r/${reportID}` as const,
     },
     REPORT_SETTINGS: {
         route: 'r/:reportID/settings',
