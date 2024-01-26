8cde6011031bc347f3d83204e100691ad8499a8a
Fedi Rajhi <fedirjh@gmail.com>
2024-01-26 21:06:47 +0100

    [Refactor] Convert Terms.js to TypeScript

--- src/pages/signin/Terms.tsx
+++ src/pages/signin/Terms.tsx
@@ -1,12 +1,13 @@
 import React, {useMemo} from 'react';
 import Text from '@components/Text';
 import TextLink from '@components/TextLink';
-import withLocalize, {withLocalizePropTypes} from '@components/withLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
+import useLocalize from "@hooks/useLocalize";
 
-function Terms(props) {
+function Terms() {
     const styles = useThemeStyles();
+    const {translate} = useLocalize();
     const [linkStyles, containerStyles] = useMemo(
         () => [
             [styles.textExtraSmallSupporting, styles.link],
@@ -17,28 +18,27 @@ function Terms(props) {
 
     return (
         <Text style={containerStyles}>
-            {props.translate('termsOfUse.phrase1')}
+            {translate('termsOfUse.phrase1')}
             <TextLink
                 style={linkStyles}
                 href={CONST.TERMS_URL}
             >
                 {' '}
-                {props.translate('termsOfUse.phrase2')}{' '}
+                {translate('termsOfUse.phrase2')}{' '}
             </TextLink>
-            {props.translate('termsOfUse.phrase3')}
+            {translate('termsOfUse.phrase3')}
             <TextLink
                 style={linkStyles}
                 href={CONST.PRIVACY_URL}
             >
                 {' '}
-                {props.translate('termsOfUse.phrase4')}
+                {translate('termsOfUse.phrase4')}
             </TextLink>
             {'. '}
         </Text>
     );
 }
 
-Terms.propTypes = {...withLocalizePropTypes};
 Terms.displayName = 'Terms';
 
-export default withLocalize(Terms);
+export default Terms;
