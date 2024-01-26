f4991ba4bdf13418dfa03d24d2dd3537b3414645
MrMuzyk <mimuzyk@gmail.com>
2024-01-26 15:51:03 +0100

    feat: Change business type selector from dropdown to list (#13)
    * feat: Change business type selector from dropdown to list
    
    * fix: typos
    
    * fix: remove unnecessary draft
    
    * fix: cr fixes

--- src/pages/ReimbursementAccount/BusinessInfo/substeps/TypeBusiness/TypeBusiness.tsx
+++ src/pages/ReimbursementAccount/BusinessInfo/substeps/TypeBusiness/TypeBusiness.tsx
@@ -3,7 +3,6 @@ import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
-import Picker from '@components/Picker';
 import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useReimbursementAccountStepFormSubmit from '@hooks/useReimbursementAccountStepFormSubmit';
@@ -15,6 +14,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 import type {ReimbursementAccount} from '@src/types/onyx';
 import type {FormValues} from '@src/types/onyx/Form';
 import type * as OnyxCommon from '@src/types/onyx/OnyxCommon';
+import BusinessTypePicker from './BusinessTypePicker';
 
 type TypeBusinessOnyxProps = {
     /** Reimbursement account from ONYX */
@@ -23,16 +23,14 @@ type TypeBusinessOnyxProps = {
 
 type TypeBusinessProps = TypeBusinessOnyxProps & SubStepProps;
 
-type IncorporationType = keyof typeof CONST.INCORPORATION_TYPES;
-
 const COMPANY_INCORPORATION_TYPE_KEY = CONST.BANK_ACCOUNT.BUSINESS_INFO_STEP.INPUT_KEY.INCORPORATION_TYPE;
 const STEP_FIELDS = [COMPANY_INCORPORATION_TYPE_KEY];
-
-const validate = (values: FormValues): OnyxCommon.Errors => ValidationUtils.getFieldRequiredErrors(values, STEP_FIELDS);
 
 function TypeBusiness({reimbursementAccount, onNext, isEditing}: TypeBusinessProps) {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
+    const validate = (values: FormValues): OnyxCommon.Errors => ValidationUtils.getFieldRequiredErrors(values, STEP_FIELDS);
+
     const defaultIncorporationType = reimbursementAccount?.achData?.incorporationType ?? '';
 
     const handleSubmit = useReimbursementAccountStepFormSubmit({
@@ -48,23 +46,19 @@ function TypeBusiness({reimbursementAccount, onNext, isEditing}: TypeBusinessPro
             submitButtonText={translate(isEditing ? 'common.confirm' : 'common.next')}
             validate={validate}
             onSubmit={handleSubmit}
-            style={[styles.mh5, styles.flexGrow1]}
-            submitButtonStyles={[styles.pb5, styles.mb0]}
+            style={[styles.flexGrow1]}
+            submitButtonStyles={[styles.p5, styles.mb0]}
         >
-            <Text style={[styles.textHeadline, styles.mb3]}>{translate('businessInfoStep.selectYourCompanysType')}</Text>
+            <Text style={[styles.textHeadline, styles.mb3, styles.ph5]}>{translate('businessInfoStep.selectYourCompanysType')}</Text>
             <InputWrapper
                 // @ts-expect-error TODO: Remove this once InputWrapper (https://github.com/Expensify/App/issues/31972) is migrated to TypeScript.
-                InputComponent={Picker}
+                InputComponent={BusinessTypePicker}
                 formID={ONYXKEYS.REIMBURSEMENT_ACCOUNT}
                 inputID={COMPANY_INCORPORATION_TYPE_KEY}
                 label={translate('businessInfoStep.companyType')}
-                items={Object.keys(CONST.INCORPORATION_TYPES).map((key) => ({
-                    value: key,
-                    label: translate(`businessInfoStep.incorporationType.${key as IncorporationType}`),
-                }))}
-                placeholder={{value: '', label: '-'}}
                 defaultValue={defaultIncorporationType}
                 shouldSaveDraft={!isEditing}
+                wrapperStyle={[styles.ph5, styles.mt4]}
             />
         </FormProvider>
     );
