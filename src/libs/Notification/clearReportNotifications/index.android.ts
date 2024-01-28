76ae394c2adc160365bc8cd7939689166cb13d03
Robert Kozik <robert.kozik@swmansion.com>
2024-01-29 00:07:10 +0100

    reintroduce removing notifications for android native

--- src/libs/Notification/clearReportNotifications/index.android.ts
+++
@@ -1,9 +0,0 @@
-import type ClearReportNotifications from './types';
-
-/**
- * This is a temporary fix for issues with our Notification Cache not being cleared in Android.
- * More info here: https://github.com/Expensify/App/issues/33367#issuecomment-1865196381
- */
-const clearReportNotifications: ClearReportNotifications = () => {};
-
-export default clearReportNotifications;
