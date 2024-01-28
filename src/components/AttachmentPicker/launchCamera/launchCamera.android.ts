b938c9a58fb20858ef6d14125b6fa813fc379ba7
Someshwar Tripathi <someshwartripathi8@gmail.com>
2024-01-29 04:21:26 +0530

    Migrate lauchCamera files

--- src/components/AttachmentPicker/launchCamera/launchCamera.android.ts
+++ src/components/AttachmentPicker/launchCamera/launchCamera.android.ts
@@ -1,19 +1,18 @@
+import {PermissionsAndroid} from 'react-native';
 import {launchCamera} from 'react-native-image-picker';
-import {PERMISSIONS, request, RESULTS} from 'react-native-permissions';
+import type {CameraOptions, Callback, ErrorCode} from './types';
 
 /**
- * Launching the camera for iOS involves checking for permissions
+ * Launching the camera for Android involves checking for permissions
  * And only then starting the camera
  * If the user deny permission the callback will be called with an error response
  * in the same format as the error returned by react-native-image-picker
- * @param {CameraOptions} options
- * @param {function} callback - callback called with the result
  */
-export default function launchCameraIOS(options, callback) {
+export default function launchCameraAndroid(options: CameraOptions, callback: Callback) {
     // Checks current camera permissions and prompts the user in case they aren't granted
-    request(PERMISSIONS.IOS.CAMERA)
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA)
         .then((permission) => {
-            if (permission !== RESULTS.GRANTED) {
+            if (permission !== PermissionsAndroid.RESULTS.GRANTED) {
                 const error = new Error('User did not grant permissions');
                 error.errorCode = 'permission';
                 throw error;
