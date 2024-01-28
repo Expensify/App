75f96a98288173a1060c2e752e68f147015231b4
Fedi Rajhi <fedirjh@gmail.com>
2024-01-28 16:03:58 +0100

    [Refactor] Convert SignInHeroCopy to TypeScript.

---
+++ src/pages/signin/SignInHeroCopy.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View} from 'react-native';
+import Text from '@components/Text';
+import useLocalize from '@hooks/useLocalize';
+import useStyleUtils from '@hooks/useStyleUtils';
+import useThemeStyles from '@hooks/useThemeStyles';
+import useWindowDimensions from '@hooks/useWindowDimensions';
+import variables from '@styles/variables';
+
+type SignInHeroCopyProps = {
+    /** Override the green headline copy */
+    customHeadline?: string;
+
+    /** Override the smaller hero body copy below the headline */
+    customHeroBody?: string;
+};
+
+function SignInHeroCopy({customHeadline = '', customHeroBody = ''}: SignInHeroCopyProps) {
+    const styles = useThemeStyles();
+    const StyleUtils = useStyleUtils();
+    const {isMediumScreenWidth, isLargeScreenWidth} = useWindowDimensions();
+    const {translate} = useLocalize();
+
+    return (
+        <View style={[styles.flex1, styles.alignSelfCenter, styles.gap7]}>
+            <Text
+                style={[
+                    styles.loginHeroHeader,
+                    isMediumScreenWidth && StyleUtils.getFontSizeStyle(variables.fontSizeSignInHeroMedium),
+                    isLargeScreenWidth && StyleUtils.getFontSizeStyle(variables.fontSizeSignInHeroLarge),
+                ]}
+            >
+                {customHeadline || translate('login.hero.header')}
+            </Text>
+            <Text style={[styles.loginHeroBody]}>{customHeroBody || translate('login.hero.body')}</Text>
+        </View>
+    );
+}
+
+SignInHeroCopy.displayName = 'SignInHeroCopy';
+
+export default SignInHeroCopy;
