f538d2c26a8b8a40620165d8bfaa4fa2ef082717
Fedi Rajhi <fedirjh@gmail.com>
2024-01-28 15:50:56 +0100

    [Refactor] Convert SignInHeroImage to TypeScript.

---
+++ src/pages/signin/SignInHeroImage.tsx
@@ -0,0 +1,42 @@
+import React, {useMemo} from 'react';
+import Lottie from '@components/Lottie';
+import LottieAnimations from '@components/LottieAnimations';
+import useThemeStyles from '@hooks/useThemeStyles';
+import useWindowDimensions from '@hooks/useWindowDimensions';
+import variables from '@styles/variables';
+
+type SignInHeroImageProps = {
+    shouldShowSmallScreen?: boolean;
+};
+
+function SignInHeroImage({shouldShowSmallScreen = false}: SignInHeroImageProps) {
+    const styles = useThemeStyles();
+    const {isSmallScreenWidth, isMediumScreenWidth} = useWindowDimensions();
+    const imageSize = useMemo(() => {
+        if (isSmallScreenWidth || shouldShowSmallScreen) {
+            return {
+                height: variables.signInHeroImageMobileHeight,
+                width: variables.signInHeroImageMobileWidth,
+            };
+        }
+
+        return {
+            height: isMediumScreenWidth ? variables.signInHeroImageTabletHeight : variables.signInHeroImageDesktopHeight,
+            width: isMediumScreenWidth ? variables.signInHeroImageTabletWidth : variables.signInHeroImageDesktopWidth,
+        };
+    }, [shouldShowSmallScreen, isMediumScreenWidth, isSmallScreenWidth]);
+
+    return (
+        <Lottie
+            source={LottieAnimations.Hands}
+            loop
+            autoPlay
+            style={[styles.alignSelfCenter, imageSize]}
+            webStyle={{...styles.alignSelfCenter, ...imageSize}}
+        />
+    );
+}
+
+SignInHeroImage.displayName = 'SignInHeroImage';
+
+export default SignInHeroImage;
