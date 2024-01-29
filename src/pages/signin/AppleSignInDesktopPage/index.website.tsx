77980b1c1d6246b37d2c7006c67a6ce26ef3d332
brunovjk <brunovjk@gmail.com>
2024-01-29 14:16:31 -0300

    Add // @ts-expect-error in pages using ThirdPartySignInPage

--- src/pages/signin/AppleSignInDesktopPage/index.website.tsx
+++ src/pages/signin/AppleSignInDesktopPage/index.website.tsx
@@ -3,7 +3,12 @@ import ThirdPartySignInPage from '@pages/signin/ThirdPartySignInPage';
 import CONST from '@src/CONST';
 
 function AppleSignInDesktopPage() {
-    return <ThirdPartySignInPage signInProvider={CONST.SIGN_IN_METHOD.APPLE} />;
+    return (
+        <ThirdPartySignInPage
+            // @ts-expect-error TODO: Remove this once SignIn (https://github.com/Expensify/App/issues/25224) is migrated to TypeScript.
+            signInProvider={CONST.SIGN_IN_METHOD.APPLE}
+        />
+    );
 }
 
 export default AppleSignInDesktopPage;
--- src/pages/signin/GoogleSignInDesktopPage/index.website.tsx
+++ src/pages/signin/GoogleSignInDesktopPage/index.website.tsx
@@ -3,7 +3,12 @@ import ThirdPartySignInPage from '@pages/signin/ThirdPartySignInPage';
 import CONST from '@src/CONST';
 
 function GoogleSignInDesktopPage() {
-    return <ThirdPartySignInPage signInProvider={CONST.SIGN_IN_METHOD.GOOGLE} />;
+    return (
+        <ThirdPartySignInPage
+            // @ts-expect-error TODO: Remove this once SignIn (https://github.com/Expensify/App/issues/25224) is migrated to TypeScript.
+            signInProvider={CONST.SIGN_IN_METHOD.GOOGLE}
+        />
+    );
 }
 
 export default GoogleSignInDesktopPage;
