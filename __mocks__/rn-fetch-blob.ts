67cd4c468bd94c99a61e611607d4010c70e1e6e7
ruben-rebelo <ruben.rato96@gmail.com>
2024-01-29 16:30:25 +0000

    [TS migration] Migrate rn-fetch-block mock

---
+++ __mocks__/rn-fetch-blob.ts
@@ -0,0 +1,11 @@
+type RnFetchBlob = {
+    DocumentDir: jest.Mock
+    ImageCache: jest.Mock
+}
+
+const RnFetchBlobMock: RnFetchBlob = {
+    DocumentDir: jest.fn(),
+    ImageCache: jest.fn(),
+}
+
+export default RnFetchBlobMock
