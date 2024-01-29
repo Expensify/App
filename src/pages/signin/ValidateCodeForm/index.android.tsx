fcbe68182932b953186cd7eb0243d9abbb193c78
Fedi Rajhi <fedirjh@gmail.com>
2024-01-29 01:15:24 +0100

    [Refactor] convert ValidateCodeForm to TypeScript

---
+++ src/pages/signin/ValidateCodeForm/index.android.tsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import BaseValidateCodeForm from './BaseValidateCodeForm';
+import type ValidateCodeFormProps from './types';
+
+function ValidateCodeForm(props: ValidateCodeFormProps) {
+    return (
+        <BaseValidateCodeForm
+            autoComplete="sms-otp"
+            /* eslint-disable-next-line react/jsx-props-no-spreading */
+            {...props}
+        />
+    );
+}
+
+ValidateCodeForm.displayName = 'ValidateCodeForm';
+
+export default ValidateCodeForm;
