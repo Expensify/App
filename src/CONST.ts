8ace304a4c7f0cd7c11754e85156b3690379b72f
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-01-29 19:53:27 +0200

    Added the ability to paste Google Docs images / charts into the Composer chat for web based platforms

--- src/CONST.ts
+++ src/CONST.ts
@@ -458,6 +458,8 @@ const CONST = {
     NEW_ZOOM_MEETING_URL: 'https://zoom.us/start/videomeeting',
     NEW_GOOGLE_MEET_MEETING_URL: 'https://meet.google.com/new',
     GOOGLE_MEET_URL_ANDROID: 'https://meet.google.com',
+    GOOGLE_DOC_IMAGE_LINK_MATCH: 'googleusercontent.com',
+    IMAGE_BASE64_MATCH: 'base64',
     DEEPLINK_BASE_URL: 'new-expensify://',
     PDF_VIEWER_URL: '/pdf/web/viewer.html',
     CLOUDFRONT_DOMAIN_REGEX: /^https:\/\/\w+\.cloudfront\.net/i,
