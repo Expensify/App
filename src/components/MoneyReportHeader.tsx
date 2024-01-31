c6a4cbf18a3a1e90d9f03b77ef3864103f7252c6
Alex Beaman <alexbeaman@expensify.com>
2024-01-31 14:53:51 +0200

    Hide Approve if on instant submit & submit & close

--- src/components/MoneyReportHeader.tsx
+++ src/components/MoneyReportHeader.tsx
@@ -11,6 +11,7 @@ import * as CurrencyUtils from '@libs/CurrencyUtils';
 import * as HeaderUtils from '@libs/HeaderUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import * as ReportUtils from '@libs/ReportUtils';
+import * as PolicyUtils from '@libs/PolicyUtils';
 import * as IOU from '@userActions/IOU';
 import * as Link from '@userActions/Link';
 import * as Session from '@userActions/Session';
@@ -65,6 +66,8 @@ function MoneyReportHeader({session, personalDetails, policy, chatReport, nextSt
           isPolicyAdmin && (isApproved || isManager)
         : isPolicyAdmin || (ReportUtils.isMoneyRequestReport(moneyRequestReport) && isManager);
     const isDraft = ReportUtils.isDraftExpenseReport(moneyRequestReport);
+    const isOnInstantSubmitPolicy = PolicyUtils.isInstantSubmitEnabled(policy);
+    const isOnSubmitAndClosePolicy = PolicyUtils.isSubmitAndClose(policy);
     const shouldShowPayButton = useMemo(
         () => isPayer && !isDraft && !isSettled && !moneyRequestReport.isWaitingOnBankAccount && reimbursableSpend !== 0 && !ReportUtils.isArchivedRoom(chatReport) && !isAutoReimbursable,
         [isPayer, isDraft, isSettled, moneyRequestReport, reimbursableSpend, chatReport, isAutoReimbursable],
@@ -73,8 +76,11 @@ function MoneyReportHeader({session, personalDetails, policy, chatReport, nextSt
         if (!isPaidGroupPolicy) {
             return false;
         }
+        if (isOnInstantSubmitPolicy && isOnSubmitAndClosePolicy) {
+            return false;
+        }
         return isManager && !isDraft && !isApproved && !isSettled;
-    }, [isPaidGroupPolicy, isManager, isDraft, isApproved, isSettled]);
+    }, [isPaidGroupPolicy, isManager, isDraft, isApproved, isSettled, isOnInstantSubmitPolicy, isOnSubmitAndClosePolicy]);
     const shouldShowSettlementButton = shouldShowPayButton || shouldShowApproveButton;
     const shouldShowSubmitButton = isDraft && reimbursableSpend !== 0;
     const isFromPaidPolicy = policyType === CONST.POLICY.TYPE.TEAM || policyType === CONST.POLICY.TYPE.CORPORATE;
