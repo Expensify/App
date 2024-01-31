77df109cdabe30afc19c9e510d326fd6a9b78eda
Wildan Muhlis <muhliswildan@gmail.com>
2024-01-31 08:27:26 +0700

    remove unused import and var

--- src/pages/signin/SignInPageLayout/BackgroundImage/index.android.js
+++ src/pages/signin/SignInPageLayout/BackgroundImage/index.android.js
@@ -1,13 +1,8 @@
 import {Image} from 'expo-image';
-import PropTypes from 'prop-types';
 import React from 'react';
 import AndroidBackgroundImage from '@assets/images/home-background--android.svg';
 import useThemeStyles from '@hooks/useThemeStyles';
 import defaultPropTypes from './propTypes';
-
-const defaultProps = {
-    transitionDuration: 0,
-};
 
 function BackgroundImage(props) {
     const styles = useThemeStyles();
