2aebbdc6160476809cf9f045a89efb0b55ec8953
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-01-31 14:57:38 +0100

    migrate react-freeze mock to TypeScript

---
+++ __mocks__/react-freeze.ts
@@ -0,0 +1,8 @@
+import type ChildrenProps from '@src/types/utils/ChildrenProps';
+
+const Freeze = (props: ChildrenProps) => props.children;
+
+export {
+    // eslint-disable-next-line import/prefer-default-export
+    Freeze,
+};
