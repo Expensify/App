9928820f9e3d0b0406028f5dd8fc6170e2da9523
ruben-rebelo <ruben.rato96@gmail.com>
2024-02-01 09:35:10 +0000

    [TS migration][crashlytics] Lint and prettier fixed

--- __mocks__/@react-native-firebase/crashlytics.ts
+++ __mocks__/@react-native-firebase/crashlytics.ts
@@ -1,4 +1,4 @@
-import type { FirebaseCrashlyticsTypes } from '@react-native-firebase/crashlytics';
+import type {FirebaseCrashlyticsTypes} from '@react-native-firebase/crashlytics';
 
 // <App> uses <ErrorBoundary> and we need to mock the imported crashlytics module
 // due to an error that happens otherwise https://github.com/invertase/react-native-firebase/issues/2475
@@ -7,7 +7,7 @@ type CrashlyticsModule = {
     log: FirebaseCrashlyticsTypes.Module['log'];
     recordError: FirebaseCrashlyticsTypes.Module['recordError'];
     setCrashlyticsCollectionEnabled: FirebaseCrashlyticsTypes.Module['setCrashlyticsCollectionEnabled'];
-}
+};
 
 type CrashlyticsMock = () => CrashlyticsModule;
 
@@ -17,4 +17,4 @@ const crashlyticsMock: CrashlyticsMock = () => ({
     setCrashlyticsCollectionEnabled: jest.fn(),
 });
 
-export default crashlyticsMock
+export default crashlyticsMock;
