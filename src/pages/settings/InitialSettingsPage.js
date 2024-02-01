4f122af7aa29e91b16ec5f10feded607813415f5
bartektomczyk <bartlomiejtomczyk@gmail.com>
2024-02-01 09:25:52 +0100

    fix: cretae seperated logo for icons

--- src/pages/settings/InitialSettingsPage.js
+++ src/pages/settings/InitialSettingsPage.js
@@ -167,7 +167,7 @@ function InitialSettingsPage(props) {
                 },
                 {
                     translationKey: 'initialSettingsPage.goToExpensifyClassic',
-                    icon: Expensicons.NewExpensify,
+                    icon: Expensicons.ExpensifyLogoNew,
                     action: () => {
                         Link.openOldDotLink(CONST.OLDDOT_URLS.INBOX);
                     },
