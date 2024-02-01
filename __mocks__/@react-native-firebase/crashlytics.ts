d673921fbb561ecea470ce219496ca4eccfd1f74
ruben-rebelo <ruben.rato96@gmail.com>
2024-02-01 09:19:51 +0000

    [TS migration] Migrate @react-native-firebase/crashlytics mock

---
+++ __mocks__/@react-native-firebase/crashlytics.ts
@@ -0,0 +1,20 @@
+import type { FirebaseCrashlyticsTypes } from '@react-native-firebase/crashlytics';
+
+// <App> uses <ErrorBoundary> and we need to mock the imported crashlytics module
+// due to an error that happens otherwise https://github.com/invertase/react-native-firebase/issues/2475
+
+type CrashlyticsModule = {
+    log: FirebaseCrashlyticsTypes.Module['log'];
+    recordError: FirebaseCrashlyticsTypes.Module['recordError'];
+    setCrashlyticsCollectionEnabled: FirebaseCrashlyticsTypes.Module['setCrashlyticsCollectionEnabled'];
+}
+
+type CrashlyticsMock = () => CrashlyticsModule;
+
+const crashlyticsMock: CrashlyticsMock = () => ({
+    log: jest.fn(),
+    recordError: jest.fn(),
+    setCrashlyticsCollectionEnabled: jest.fn(),
+});
+
+export default crashlyticsMock
