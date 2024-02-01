18654892791f9254d64c415b10b2ceed79562acc
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-01 18:46:20 +0100

    Move InputWrapperProps to InputWrapper file

--- src/components/Form/InputWrapper.tsx
+++ src/components/Form/InputWrapper.tsx
@@ -1,11 +1,20 @@
-import type {ForwardedRef} from 'react';
+import type {ComponentPropsWithoutRef, ForwardedRef} from 'react';
 import React, {forwardRef, useContext} from 'react';
 import type {AnimatedTextInputRef} from '@components/RNTextInput';
 import TextInput from '@components/TextInput';
 import FormContext from './FormContext';
-import type {InputWrapperProps, ValidInputs} from './types';
+import type {InputComponentBaseProps, InputComponentValueProps, ValidInputs, ValueTypeKey} from './types';
 
-function InputWrapper<TInput extends ValidInputs>({InputComponent, inputID, valueType = 'string', ...rest}: InputWrapperProps<TInput>, ref: ForwardedRef<AnimatedTextInputRef>) {
+type InputWrapperProps<TInput extends ValidInputs, TValue extends ValueTypeKey = ValueTypeKey> = ComponentPropsWithoutRef<TInput> &
+    InputComponentValueProps<TValue> & {
+        InputComponent: TInput;
+        inputID: string;
+    };
+
+function InputWrapper<TInput extends ValidInputs, TValue extends ValueTypeKey>(
+    {InputComponent, inputID, ...rest}: InputWrapperProps<TInput, TValue>,
+    ref: ForwardedRef<AnimatedTextInputRef>,
+) {
     const {registerInput} = useContext(FormContext);
     // There are inputs that don't have onBlur methods, to simulate the behavior of onBlur in e.g. checkbox, we had to
     // use different methods like onPress. This introduced a problem that inputs that have the onBlur method were
@@ -14,8 +23,10 @@ function InputWrapper<TInput extends ValidInputs>({InputComponent, inputID, valu
     const shouldSetTouchedOnBlurOnly = InputComponent === TextInput;
 
     // TODO: Sometimes we return too many props with register input, so we need to consider if it's better to make the returned type more general and disregard the issue, or we would like to omit the unused props somehow.
+    const inputProps = registerInput(inputID, {ref, valueType: 'string', ...rest, shouldSetTouchedOnBlurOnly} as InputComponentBaseProps);
+
     // eslint-disable-next-line react/jsx-props-no-spreading, @typescript-eslint/no-explicit-any
-    return <InputComponent {...(registerInput(inputID, {ref, valueType, ...rest, shouldSetTouchedOnBlurOnly}) as any)} />;
+    return <InputComponent {...(inputProps as any)} />;
 }
 
 InputWrapper.displayName = 'InputWrapper';
