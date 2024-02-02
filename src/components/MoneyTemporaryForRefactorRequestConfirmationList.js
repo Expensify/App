eecaf8402f4be0dcf3c9c2701e37cd2f4d7d64c3
dukenv0307 <dukenv0307@gmail.com>
2024-02-02 11:48:15 +0700

    fix lint

--- src/components/MoneyTemporaryForRefactorRequestConfirmationList.js
+++ src/components/MoneyTemporaryForRefactorRequestConfirmationList.js
@@ -876,7 +876,7 @@ function MoneyTemporaryForRefactorRequestConfirmationList({
                     />
                 )
             )}
-            {relevantMenuFields}
+            {relevantFields}
             {!shouldShowAllFields && (
                 <View style={[styles.flexRow, styles.justifyContentBetween, styles.mh3, styles.alignItemsCenter, styles.mb2, styles.mt1]}>
                     <View style={[styles.shortTermsHorizontalRule, styles.flex1, styles.mr0]} />
