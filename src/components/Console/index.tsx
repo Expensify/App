d040a4b30f98ebb1a13309e3e8c7baf4d6037da4
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-02-02 14:45:04 +0100

    cleanup console code

--- src/components/Console/index.tsx
+++ src/components/Console/index.tsx
@@ -2,59 +2,12 @@
 import React, {useEffect, useState} from 'react';
 import {Button, ScrollView, TextInput, View} from 'react-native';
 import Text from '@components/Text';
-
-type Log = {
-    time: Date;
-    level: string;
-    message: string;
-};
-
-type UpdateLogsFunction = (newLog: Log) => void;
-
-let updateLogs: UpdateLogsFunction | null = null;
-const capturedLogs: Log[] = [];
-const originalConsoleLog = console.log;
-
-console.log = (...args) => {
-    const message = args
-        .map((arg) => {
-            if (typeof arg === 'object') {
-                try {
-                    return JSON.stringify(arg, null, 2); // Indent for better readability
-                } catch (e) {
-                    return 'Unserializable Object';
-                }
-            }
-
-            return String(arg);
-        })
-        .join(' ');
-    const newLog = {time: new Date(), level: 'log', message};
-    capturedLogs.push(newLog);
-    if (updateLogs) {
-        updateLogs(newLog);
-    }
-    originalConsoleLog.apply(console, args);
-};
-
-function setUpdateLogsFunction(func: UpdateLogsFunction | null) {
-    updateLogs = func;
-}
-
-const charMap = {
-    '\u2018': "'",
-    '\u2019': "'",
-    '\u201C': '"',
-    '\u201D': '"',
-    '\u201E': '"',
-    '\u2026': '...',
-};
-
-const charsToSanitize = /[\u2018\u2019\u201C\u201D\u201E\u2026]/g;
+import {createLog, sanitizeConsoleInput, setUpdateLogsFunction} from '@libs/Console';
+import type {Log} from '@libs/Console';
 
 function Console() {
     const [input, setInput] = useState('');
-    const [logs, setLogs] = useState<Log[]>(capturedLogs);
+    const [logs, setLogs] = useState<Log[]>([]);
 
     useEffect(() => {
         const addLog = (newLog: Log) => {
@@ -68,21 +21,10 @@ function Console() {
     }, []);
 
     const handleExecute = () => {
-        const sanitizedInput = input.replace(charsToSanitize, (match) => charMap[match]);
+        const sanitizedInput = sanitizeConsoleInput(input);
 
-        try {
-            // @ts-expect-error Any code inside `sanitizedInput` that gets evaluated by `eval()` will be executed in the context of the current this value.
-            // eslint-disable-next-line no-eval
-            const result = eval.call(this, sanitizedInput);
-
-            if (result !== undefined) {
-                setLogs([...logs, {time: new Date(), level: 'info', message: `> ${input}`}, {time: new Date(), level: 'result', message: String(result)}]);
-            } else {
-                setLogs([...logs, {time: new Date(), level: 'info', message: `> ${input}`}]);
-            }
-        } catch (e) {
-            setLogs([...logs, {time: new Date(), level: 'error', message: `> ${sanitizedInput}`}, {time: new Date(), level: 'error', message: `Error: ${e.message}`}]);
-        }
+        const customLogs = createLog(sanitizedInput);
+        setLogs((prevLogs) => [...prevLogs, ...customLogs]);
         setInput('');
     };
 
---
+++ src/libs/Console/index.ts
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+type Log = {
+    time: Date;
+    level: string;
+    message: string;
+};
+
+type UpdateLogsFunction = (newLog: Log) => void;
+
+let updateLogs: UpdateLogsFunction | null = null;
+const capturedLogs: Log[] = [];
+// eslint-disable-next-line no-console
+const originalConsoleLog = console.log;
+
+function setUpdateLogsFunction(func: UpdateLogsFunction | null) {
+    updateLogs = func;
+}
+
+function logMessage(args: unknown[]) {
+    const message = args
+        .map((arg) => {
+            if (typeof arg === 'object') {
+                try {
+                    return JSON.stringify(arg, null, 2); // Indent for better readability
+                } catch (e) {
+                    return 'Unserializable Object';
+                }
+            }
+
+            return String(arg);
+        })
+        .join(' ');
+    const newLog = {time: new Date(), level: 'log', message};
+    capturedLogs.push(newLog);
+    if (updateLogs) {
+        updateLogs(newLog);
+    }
+}
+
+// eslint-disable-next-line no-console
+console.log = (...args) => {
+    logMessage(args);
+    originalConsoleLog.apply(console, args);
+};
+
+const charsToSanitize = /[\u2018\u2019\u201C\u201D\u201E\u2026]/g;
+
+const charMap: Record<string, string> = {
+    '\u2018': "'",
+    '\u2019': "'",
+    '\u201C': '"',
+    '\u201D': '"',
+    '\u201E': '"',
+    '\u2026': '...',
+};
+
+function sanitizeConsoleInput(text: string) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return text.replace(charsToSanitize, (match) => charMap[match]);
+}
+
+function createLog(text: string) {
+    const time = new Date();
+    try {
+        // @ts-expect-error Any code inside `sanitizedInput` that gets evaluated by `eval()` will be executed in the context of the current this value.
+        // eslint-disable-next-line no-eval, no-invalid-this
+        const result = eval.call(this, text);
+
+        if (result !== undefined) {
+            return [
+                {time, level: 'INFO', message: `> ${text}`},
+                {time, level: 'RESULT', message: String(result)},
+            ];
+        }
+        return [{time, level: 'INFO', message: `> ${text}`}];
+    } catch (error) {
+        return [
+            {time, level: 'ERROR', message: `> ${text}`},
+            {time, level: 'ERROR', message: `Error: ${(error as Error).message}`},
+        ];
+    }
+}
+
+export {sanitizeConsoleInput, createLog, capturedLogs, setUpdateLogsFunction};
+export type {Log};
