207385ad09ec9228e55a04f62654b47f57031548
mkhutornyi <mkhutorniy@gmail.com>
2024-02-02 10:17:27 +0100

    Merge branch 'main' of https://github.com/mkhutornyi/Expensify-Frontend into ts-migration/App

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,5 +1,6 @@
-import React, {useMemo} from 'react';
+import React, {useEffect, useMemo} from 'react';
 import useThemePreferenceWithStaticOverride from '@hooks/useThemePreferenceWithStaticOverride';
+import DomUtils from '@libs/DomUtils';
 // eslint-disable-next-line no-restricted-imports
 import themes from '@styles/theme';
 import ThemeContext from '@styles/theme/context/ThemeContext';
@@ -13,6 +14,10 @@ function ThemeProvider({children, theme: staticThemePreference}: ThemeProviderPr
     const themePreference = useThemePreferenceWithStaticOverride(staticThemePreference);
 
     const theme = useMemo(() => themes[themePreference], [themePreference]);
+
+    useEffect(() => {
+        DomUtils.addCSS(DomUtils.getAutofilledInputStyle(theme.text), 'autofill-input');
+    }, [theme.text]);
 
     return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
 }
---
+++ src/pages/home/sidebar/BottomTabBarFloatingActionButton/types.ts
@@ -0,0 +1,5 @@
+type FloatingActionButtonPopoverMenuRef = {
+    hideCreateMenu: () => void;
+};
+
+export default FloatingActionButtonPopoverMenuRef;
