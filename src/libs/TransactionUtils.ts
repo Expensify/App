b8b9f4e3b47e9591c7c863a6a9a18cfba47a32b4
Shubham Agrawal <shubham.agra1206@gmail.com>
2024-02-03 19:54:18 +0530

    merge main

--- src/libs/TransactionUtils.ts
+++ src/libs/TransactionUtils.ts
@@ -6,7 +6,7 @@ import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {RecentWaypoint, Report, ReportAction, Transaction, TransactionViolation} from '@src/types/onyx';
 import type {PolicyTaxRate, PolicyTaxRates} from '@src/types/onyx/PolicyTaxRates';
-import type {Comment, PendingFieldsCollection, Receipt, Waypoint, WaypointCollection} from '@src/types/onyx/Transaction';
+import type {Comment, TransactionPendingFieldsKey, Receipt, Waypoint, WaypointCollection} from '@src/types/onyx/Transaction';
 import type {EmptyObject} from '@src/types/utils/EmptyObject';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import {isCorporateCard, isExpensifyCard} from './CardUtils';
@@ -99,7 +99,7 @@ function buildOptimisticTransaction(
     category = '',
     tag = '',
     billable = false,
-    pendingFields: PendingFieldsCollection | undefined = undefined,
+    pendingFields: Partial<{[K in TransactionPendingFieldsKey]: ValueOf<typeof CONST.RED_BRICK_ROAD_PENDING_ACTION>}> | undefined = undefined,
 ): Transaction {
     // transactionIDs are random, positive, 64-bit numeric strings.
     // Because JS can only handle 53-bit numbers, transactionIDs are strings in the front-end (just like reportActionID)
