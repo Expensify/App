dd4cf14d7cafd0890c87bc681633d461ffe64dc2
Fedi Rajhi <fedirjh@gmail.com>
2024-02-04 14:56:03 +0100

    Revert "[Refactor] convert to TypeScript"
    This reverts commit eb862e5807d7241c3d01e795062e641345eebb9c.

--- src/pages/signin/SignInPageLayout/signInPageStyles/index.native.ts
+++ src/pages/signin/SignInPageLayout/signInPageStyles/index.native.ts
@@ -1,6 +1,6 @@
 // Using flexGrow on mobile allows the ScrollView container to grow to fit the content.
 // This is necessary because making the sign-in content fit exactly the viewheight causes the scroll to stop working on mobile.
-import type {ThemeStyles} from '@src/styles';
+import type {ThemeStyles} from "@src/styles";
 
 const scrollViewContentContainerStyles = (styles: ThemeStyles) => styles.flexGrow1;
 
