dc2cad8a27ef02f5e247494c6aa0bd48d88d676e
bartektomczyk <bartlomiejtomczyk@gmail.com>
2024-02-05 09:31:27 +0100

    fix: prettier

--- src/pages/ReimbursementAccount/RequestorOnfidoStep.js
+++ src/pages/ReimbursementAccount/RequestorOnfidoStep.js
@@ -25,7 +25,7 @@ const propTypes = {
     /** The token required to initialize the Onfido SDK */
     onfidoToken: PropTypes.string,
 
-     /** The application ID for our Onfido instance */
+    /** The application ID for our Onfido instance */
     onfidoApplicantID: PropTypes.string,
 };
 
