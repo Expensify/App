b4be6a8243682ffaeec8b4cda9eb1f0abcf7093c
tienifr <christianwen18@gmail.com>
2024-02-05 22:41:16 +0700

    promise all approach

--- src/pages/iou/request/step/IOURequestStepDistance.js
+++ src/pages/iou/request/step/IOURequestStepDistance.js
@@ -160,20 +160,8 @@ function IOURequestStepDistance({
             });
 
             setOptimisticWaypoints(newWaypoints);
-            // eslint-disable-next-line rulesdir/no-thenable-actions-in-views
-            Transaction.updateWaypoints(transactionID, newWaypoints, true).then(() => {
-                if (emptyWaypointIndex === -1) {
-                    setOptimisticWaypoints(null);
-                    return;
-                }
-                // This is a workaround because at this point, transaction data has not been updated yet
-                const updatedTransaction = {
-                    ...transaction,
-                    ...Transaction.getUpdatedWaypointsTransaction(newWaypoints),
-                };
-                Transaction.removeWaypoint(updatedTransaction, emptyWaypointIndex, true).then(() => {
-                    setOptimisticWaypoints(null);
-                });
+            Promise.all([Transaction.removeWaypoint(transaction, emptyWaypointIndex, true), Transaction.updateWaypoints(transactionID, newWaypoints, true)]).then(() => {
+                setOptimisticWaypoints(null);
             });
         },
         [transactionID, transaction, waypoints, waypointsList],
