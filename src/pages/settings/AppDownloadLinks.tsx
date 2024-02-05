73b15d0c8110140d68205904689803edc922839d
hkopser99 <144170694+hkopser99@users.noreply.github.com>
2024-02-05 22:52:41 +0100

    TS Migration #25175: Update and rename AppDownloadLinks.js to AppDownloadLinks.tsx
    Updates related to Typescript Migration #25175. 
    
    -Migrated component to Typescript
    -Extended MenuItemProps type as DownloadMenuItem type
    -Typed menu items as an array of DownloadMenuItems
    -Removed withWindowDimensions/withLocalize PropTypes and compose() from export per guidelines on migration
    -Used Hooks for localize instead for translation
    -Removed _ in JSX mapping of menu items. Saw other TSX files that removes and I was getting ESLint warnings. _ is noted as a best practice but given other TSX files have removed I followed suit. I also saw old ESlint ignores being used when removing _ but I didn't get flagged without this.
    -Removed onKeyDown attribute and blur() from MenuItem. I saw this nowhere else in the codebase and the removal had no impact on app use functionality (please advise if there's something I am missing but it appeared antiquated)
    -This component receives props from Navigation but I do not see elsewhere in the codebase that we are explicitly listing these as props via typescript (navigation props should inherintly apply to all pages)
    -TS warnings made me list translationKey as type TranslationPaths (warning only appears when not providing the translate from useLocalize() with a '' - others working on TS migrations may want to know this)
    -No ESlint warnings
    -No VSCode warnings or errors
    -No errors when testing
    -Only issue was on iOS simulator there's no App Store app so it doesn't know how to handle the download iOS click but the link is fine and it works everywhere else.
    -No other components or other files in codebase import this file so change from .js to .tsx should not require changes to imports anywhere. This component is routed to via its name as with other pages in codebase.

---
+++ src/pages/settings/AppDownloadLinks.tsx
@@ -0,0 +1,87 @@
+import React, {useRef} from 'react';
+import {ScrollView} from 'react-native';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
+import Navigation from '@libs/Navigation/Navigation';
+import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import * as Expensicons from '@components/Icon/Expensicons';
+import MenuItem from '@components/MenuItem';
+import ScreenWrapper from '@components/ScreenWrapper';
+import * as ReportActionContextMenu from '@pages/home/report/ContextMenu/ReportActionContextMenu';
+import * as Link from '@userActions/Link';
+import type { View } from 'react-native';
+import type { TranslationPaths } from '@src/languages/types';
+import type { MenuItemProps } from '@components/MenuItem';
+import CONST from '@src/CONST';
+import ROUTES from '@src/ROUTES';
+
+function AppDownloadLinksPage() {
+    const styles = useThemeStyles();
+    const { translate } = useLocalize();
+    const popoverAnchor = useRef<View>(null);
+
+    type DownloadMenuItem = MenuItemProps & {
+        translationKey: TranslationPaths,
+        openAppDownloadLink: () => void,
+        downloadLink: string,
+    };
+
+    const menuItems: DownloadMenuItem[] = [
+        {
+            translationKey: 'initialSettingsPage.appDownloadLinks.android.label',
+            openAppDownloadLink: () => {
+                Link.openExternalLink(CONST.APP_DOWNLOAD_LINKS.ANDROID);
+            },
+            downloadLink: CONST.APP_DOWNLOAD_LINKS.ANDROID,
+            icon: Expensicons.Android,
+            iconRight: Expensicons.NewWindow,
+        },
+        {
+            translationKey: 'initialSettingsPage.appDownloadLinks.ios.label',
+            openAppDownloadLink: () => {
+                Link.openExternalLink(CONST.APP_DOWNLOAD_LINKS.IOS, true);
+            },
+            downloadLink: CONST.APP_DOWNLOAD_LINKS.IOS,
+            icon: Expensicons.Apple,
+            iconRight: Expensicons.NewWindow,
+        },
+        {
+            translationKey: 'initialSettingsPage.appDownloadLinks.desktop.label',
+            openAppDownloadLink: () => {
+                Link.openExternalLink(CONST.APP_DOWNLOAD_LINKS.DESKTOP);
+            },
+            downloadLink: CONST.APP_DOWNLOAD_LINKS.DESKTOP,
+            icon: Expensicons.Monitor,
+            iconRight: Expensicons.NewWindow,
+        },
+    ];
+
+    return (
+        <ScreenWrapper testID={AppDownloadLinksPage.displayName}>
+            <HeaderWithBackButton
+                title={translate('initialSettingsPage.aboutPage.appDownloadLinks')}
+                onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_ABOUT)}
+            />
+            <ScrollView style={[styles.mt3]}>
+                {menuItems.map((item: DownloadMenuItem) => (
+                    <MenuItem
+                        key={item.translationKey}
+                        onPress={item.openAppDownloadLink}
+                        onSecondaryInteraction={(e) => ReportActionContextMenu.showContextMenu(CONST.CONTEXT_MENU_TYPES.LINK, e, item.downloadLink, popoverAnchor.current)}
+                        ref={popoverAnchor}
+                        title={translate(item.translationKey)}
+                        icon={item.icon}
+                        iconRight={item.iconRight} 
+                        shouldBlockSelection
+                        shouldShowRightIcon
+                    />      
+                ))}
+            </ScrollView>
+        </ScreenWrapper>
+    );
+}
+
+AppDownloadLinksPage.displayName = 'AppDownloadLinksPage';
+
+export default AppDownloadLinksPage;
+
