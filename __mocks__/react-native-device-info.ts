c889b30936dd6ed3b83a6cb71ff7c1026b29a2d5
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-02-05 13:55:31 +0100

    fix: created a react-native-device-info ts module migrate react-native-device-info to TS

---
+++ __mocks__/react-native-device-info.ts
@@ -0,0 +1,6 @@
+import type DeviceInfoModule from 'react-native-device-info';
+import MockDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
+
+const DevicoInfo: typeof DeviceInfoModule = MockDeviceInfo;
+
+export default DevicoInfo;
