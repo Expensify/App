534a3c72b6e094d8db70d48edbce8f2052af4e2b
rayane-djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-02-06 18:08:45 +0100

    run prettier

--- workflow_tests/mocks/failureNotifierMocks.js
+++ workflow_tests/mocks/failureNotifierMocks.js
@@ -2,20 +2,8 @@
 const utils = require('../utils/utils');
 
 // notifyfailure
-const FAILURENOTIFIER__NOTIFYFAILURE__FETCH_WORKFLOW_RUN_JOBS__STEP_MOCK = utils.createMockStep(
-    'Fetch Workflow Run Jobs',
-    'Fetch Workflow Run Jobs',
-    'NOTIFYFAILURE',
-    [],
-    [],
-);
-const FAILURENOTIFIER__NOTIFYFAILURE__PROCESS_EACH_FAILED_JOB__STEP_MOCK = utils.createMockStep(
-    'Process Each Failed Job',
-    'Process Each Failed Job',
-    'NOTIFYFAILURE',
-    [],
-    [],
-);
+const FAILURENOTIFIER__NOTIFYFAILURE__FETCH_WORKFLOW_RUN_JOBS__STEP_MOCK = utils.createMockStep('Fetch Workflow Run Jobs', 'Fetch Workflow Run Jobs', 'NOTIFYFAILURE', [], []);
+const FAILURENOTIFIER__NOTIFYFAILURE__PROCESS_EACH_FAILED_JOB__STEP_MOCK = utils.createMockStep('Process Each Failed Job', 'Process Each Failed Job', 'NOTIFYFAILURE', [], []);
 const FAILURENOTIFIER__NOTIFYFAILURE__STEP_MOCKS = [FAILURENOTIFIER__NOTIFYFAILURE__FETCH_WORKFLOW_RUN_JOBS__STEP_MOCK, FAILURENOTIFIER__NOTIFYFAILURE__PROCESS_EACH_FAILED_JOB__STEP_MOCK];
 
 module.exports = {
