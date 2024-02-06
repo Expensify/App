f1e15ca17856e23cd4ee212842db235e94ec5afa
Cong Pham <ptcong411@gmail.com>
2024-02-07 02:11:30 +0700

    fix typo

--- src/hooks/useTackInputFocus/index.native.ts
+++ src/hooks/useTackInputFocus/index.native.ts
@@ -1,5 +1,5 @@
 /**
- * This hook to detech input or text area focus on browser on native doesn't support DOM so default return false
+ * This hook to detect input or text area focus on browser, on native doesn't support DOM so default return false
  */
 export default function useTackInputFocus(): boolean {
     return false;
