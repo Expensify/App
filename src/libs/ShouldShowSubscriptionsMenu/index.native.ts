6afffdc31bd82cfa44a98bb278c6576cbfd14c71
Riya Shete <156463907+codinggeek2023@users.noreply.github.com>
2024-02-07 02:23:58 +0530

    Update src/libs/ShouldShowSubscriptionsMenu/index.native.ts
    Co-authored-by: Aimane Chnaif <96077027+aimane-chnaif@users.noreply.github.com>

--- src/libs/ShouldShowSubscriptionsMenu/index.native.ts
+++ src/libs/ShouldShowSubscriptionsMenu/index.native.ts
@@ -3,6 +3,6 @@ import type ShouldShowSubscriptionsMenu from './types';
 /**
  * Indicates whether the subscription menu should show in the all settings screen
  */
-const shouldShowSubscriptionsMenu: ShouldShowSubscriptionsMenu = () => false;
+const shouldShowSubscriptionsMenu: ShouldShowSubscriptionsMenu = false;
 
 export default shouldShowSubscriptionsMenu;
