6ddb061da03fcfc7e76612d926e6f343762b397a
Dylan <dylandylan9112@gmail.com>
2024-02-06 17:47:18 +0700

    update type

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -247,9 +247,9 @@ type MoneyRequestNavigatorParamList = {
     [SCREENS.MONEY_REQUEST.STEP_MERCHANT]: {
         action: ValueOf<typeof CONST.IOU.ACTION>;
         iouType: ValueOf<typeof CONST.IOU.TYPE>;
+        transactionID: string;
         reportID: string;
-        field: string;
-        threadReportID: string;
+        backTo: string;
     };
     [SCREENS.IOU_SEND.ENABLE_PAYMENTS]: undefined;
     [SCREENS.IOU_SEND.ADD_BANK_ACCOUNT]: undefined;
