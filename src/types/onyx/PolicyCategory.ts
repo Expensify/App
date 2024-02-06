0807737d069136bbb2b12a888957983e6326dd49
Carlos Martins <cmartins@expensify.com>
2024-02-06 11:31:47 -0500

    add errors to Category type

--- src/types/onyx/PolicyCategory.ts
+++ src/types/onyx/PolicyCategory.ts
@@ -1,3 +1,5 @@
+import type * as OnyxCommon from './OnyxCommon';
+
 type PolicyCategory = {
     /** Name of a category */
     name: string;
@@ -17,6 +19,8 @@ type PolicyCategory = {
 
     /** The external accounting service that this category comes from */
     origin: string;
+
+    errors?: OnyxCommon.Errors;
 };
 
 type PolicyCategories = Record<string, PolicyCategory>;
