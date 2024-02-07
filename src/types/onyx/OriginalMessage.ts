928ccd0a78339cbf88c813540ef42dc39bb8c0d4
Alberto <alberto@expensify.com>
2024-02-07 17:19:05 +0100

    more typescript

--- src/types/onyx/OriginalMessage.ts
+++ src/types/onyx/OriginalMessage.ts
@@ -22,6 +22,7 @@ type OriginalMessageActionName =
     | 'TASKEDITED'
     | 'TASKREOPENED'
     | 'ACTIONABLEMENTIONWHISPER'
+    | 'MARKEDREIMBURSED'
     | ValueOf<typeof CONST.REPORT.ACTIONS.TYPE.POLICYCHANGELOG>;
 type OriginalMessageApproved = {
     actionName: typeof CONST.REPORT.ACTIONS.TYPE.APPROVED;
