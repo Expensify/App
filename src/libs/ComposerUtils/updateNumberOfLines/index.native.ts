43733bff4020c162c19d98cbb5e9cec4efafad57
dukenv0307 <dukenv0307@gmail.com>
2024-02-07 18:28:04 +0700

    fix native

--- src/libs/ComposerUtils/updateNumberOfLines/index.native.ts
+++ src/libs/ComposerUtils/updateNumberOfLines/index.native.ts
@@ -15,7 +15,7 @@ const updateNumberOfLines: UpdateNumberOfLines = (props, event, styles) => {
         return;
     }
     const numberOfLines = getNumberOfLines(lineHeight, paddingTopAndBottom, inputHeight);
-    if (numberOfLines === 1 && ReportUtils.getCommentLength(props.value ?? '') === 0) {
+    if (numberOfLines === 1) {
         props.resetFullComposerSize?.();
     }
     updateIsFullComposerAvailable(props, numberOfLines);
