29c9d4e960a42c7b9224170b1b6ed118578dddaf
Filip Solecki <filip.solecki@swmansion.com>
2024-02-09 10:34:12 +0100

    Remove top margin

--- src/pages/ShareCodePage.tsx
+++ src/pages/ShareCodePage.tsx
@@ -83,7 +83,7 @@ function ShareCodePage({report, session, currentUserPersonalDetails}: ShareCodeP
                 shouldShowBackButton={isReport || isSmallScreenWidth}
                 icon={Illustrations.QrCode}
             />
-            <ScrollView style={[themeStyles.flex1, themeStyles.mt3]}>
+            <ScrollView style={[themeStyles.flex1]}>
                 <View style={[themeStyles.flex1, isSmallScreenWidth ? themeStyles.workspaceSectionMobile : themeStyles.workspaceSection]}>
                     <Section
                         title={translate('shareCodePage.title')}
