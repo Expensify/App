4227eafd1dc2cc3a2bf48cb3ca82bb291389988b
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-02-09 13:06:42 +0100

    cross-platform logs sharing

---
+++ src/pages/settings/AboutPage/ShareLogList/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as FileUtils from '@libs/fileDownload/FileUtils';
+import Navigation from '@libs/Navigation/Navigation';
+import * as Report from '@userActions/Report';
+import ROUTES from '@src/ROUTES';
+import BaseShareLogList from './BaseShareLogList';
+
+type ShareLogListProps = {
+    logSource: string;
+};
+
+function ShareLogList({logSource}: ShareLogListProps) {
+    const onAttachLogToReport = (reportID: string, filename: string) => {
+        FileUtils.readFileAsync(
+            logSource,
+            filename,
+            (file) => {
+                Report.addAttachment(reportID, file);
+
+                const routeToNavigate = ROUTES.REPORT_WITH_ID.getRoute(reportID);
+                Navigation.navigate(routeToNavigate);
+            },
+            () => {},
+        );
+    };
+    return <BaseShareLogList onAttachLogToReport={onAttachLogToReport} />;
+}
+
+export default ShareLogList;
