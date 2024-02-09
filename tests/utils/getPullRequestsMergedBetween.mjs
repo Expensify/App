ca7b5532d6a56ce3bcea979551be3f3352c8b528
Filip Solecki <filip.solecki@swmansion.com>
2024-02-09 12:55:14 +0100

    Fix file extension

--- tests/utils/getPullRequestsMergedBetween.mjs
+++ tests/utils/getPullRequestsMergedBetween.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import * as GitUtils from '../../.github/libs/GitUtils.js';
+import * as GitUtils from '../../.github/libs/GitUtils.ts';
 
 const fromRef = process.argv[2];
 const toRef = process.argv[3];
