7e3e9747853d5e17d0c8f40ae267a81283a82d62
brunovjk <brunovjk@gmail.com>
2024-02-10 10:49:20 -0300

    Update 'IOU.updateMoneyRequestDate' call to use 'policy, policyTags and policyCategories' params

--- src/pages/iou/request/step/IOURequestStepDate.js
+++ src/pages/iou/request/step/IOURequestStepDate.js
@@ -1,10 +1,13 @@
 import lodashGet from 'lodash/get';
 import lodashIsEmpty from 'lodash/isEmpty';
+import PropTypes from 'prop-types';
 import React from 'react';
 import {withOnyx} from 'react-native-onyx';
+import categoryPropTypes from '@components/categoryPropTypes';
 import DatePicker from '@components/DatePicker';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
+import tagPropTypes from '@components/tagPropTypes';
 import transactionPropTypes from '@components/transactionPropTypes';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -15,6 +18,7 @@ import * as TransactionUtils from '@libs/TransactionUtils';
 import * as IOU from '@userActions/IOU';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import {policyPropTypes} from '@src/pages/workspace/withPolicy';
 import IOURequestStepRoutePropTypes from './IOURequestStepRoutePropTypes';
 import StepScreenWrapper from './StepScreenWrapper';
 import withFullTransactionOrNotFound from './withFullTransactionOrNotFound';
@@ -30,11 +34,23 @@ const propTypes = {
 
     /** The draft transaction that holds data to be persisted on the current transaction */
     splitDraftTransaction: transactionPropTypes,
+
+    /** The policy of the report */
+    policy: policyPropTypes.policy,
+
+    /** Collection of categories attached to a policy */
+    policyCategories: PropTypes.objectOf(categoryPropTypes),
+
+    /** Collection of tags attached to a policy */
+    policyTags: tagPropTypes,
 };
 
 const defaultProps = {
     transaction: {},
     splitDraftTransaction: {},
+    policy: null,
+    policyTags: null,
+    policyCategories: null,
 };
 
 function IOURequestStepDate({
@@ -43,6 +59,9 @@ function IOURequestStepDate({
     },
     transaction,
     splitDraftTransaction,
+    policy,
+    policyTags,
+    policyCategories,
 }) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
@@ -78,7 +97,7 @@ function IOURequestStepDate({
         IOU.setMoneyRequestCreated(transaction.transactionID, newCreated, action === CONST.IOU.ACTION.CREATE);
 
         if (action === CONST.IOU.ACTION.EDIT) {
-            IOU.updateMoneyRequestDate(transaction.transactionID, reportID, newCreated);
+            IOU.updateMoneyRequestDate(transaction.transactionID, reportID, newCreated, policy, policyTags, policyCategories);
         }
 
         navigateBack();
@@ -125,6 +144,15 @@ export default compose(
                 const transactionID = lodashGet(route, 'params.transactionID', 0);
                 return `${ONYXKEYS.COLLECTION.SPLIT_TRANSACTION_DRAFT}${transactionID}`;
             },
+        },
+        policy: {
+            key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY}${report ? report.policyID : '0'}`,
+        },
+        policyCategories: {
+            key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY_CATEGORIES}${report ? report.policyID : '0'}`,
+        },
+        policyTags: {
+            key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY_TAGS}${report ? report.policyID : '0'}`,
         },
     }),
 )(IOURequestStepDate);
