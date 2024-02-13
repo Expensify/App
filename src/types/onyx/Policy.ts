374a1a077cb698a2c41d1266a30338701b560316
Vit Horacek <36083550+mountiny@users.noreply.github.com>
2024-02-13 17:05:13 +0000

    Merge pull request #36424 from Expensify/revert-35347-marco-displayPayButtonReimburser
    [CP Staging] Revert "Update logic to display pay button in paid policies"
    
    (cherry picked from commit 6d7d8c07b3fafd72b1231f9a28484f2815329829)

--- src/types/onyx/Policy.ts
+++ src/types/onyx/Policy.ts
@@ -162,9 +162,6 @@ type Policy = {
     /** When tax tracking is enabled */
     isTaxTrackingEnabled?: boolean;
 
-    /** The email of the reimburser set when reimbursement is direct */
-    reimburserEmail?: string;
-
     /** ReportID of the admins room for this workspace */
     chatReportIDAdmins?: number;
 
