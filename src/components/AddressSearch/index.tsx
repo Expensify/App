a825ab69b5bf72d536bcfb4489057024069e77f1
sourcecodedeveloper <sourcecoder@mailfence.com>
2024-02-13 00:46:02 -0800

    add useCallback hook to useKeyboardShortcut

--- src/components/AddressSearch/index.tsx
+++ src/components/AddressSearch/index.tsx
@@ -78,7 +78,7 @@ function AddressSearch(
     const containerRef = useRef<View>(null);
     const maxIndexRef = useRef(0);
     const resultRef = useRef();
-    const [focusedIndex, setFocusedIndex] = useArrowKeyFocusManager({
+    const [focusedIndex] = useArrowKeyFocusManager({
         initialFocusedIndex: -1,
         maxIndex: maxIndexRef.current - 1,
         isActive: true,
@@ -93,147 +93,150 @@ function AddressSearch(
         [preferredLocale, resultTypes, isLimitedToUSA, locationBias],
     );
     const shouldShowCurrentLocationButton = canUseCurrentLocation && searchValue.trim().length === 0 && isFocused;
-    const saveLocationDetails = (autocompleteData: GooglePlaceData, details: GooglePlaceDetail | null) => {
-        const addressComponents = details?.address_components;
-        if (!addressComponents) {
-            // When there are details, but no address_components, this indicates that some predefined options have been passed
-            // to this component which don't match the usual properties coming from auto-complete. In that case, only a limited
-            // amount of data massaging needs to happen for what the parent expects to get from this function.
-            if (details) {
-                onPress?.({
-                    address: autocompleteData.description ?? '',
-                    lat: details.geometry?.location.lat ?? 0,
-                    lng: details.geometry?.location.lng ?? 0,
-                    name: details?.name,
-                });
-            }
-            return;
-        }
-
-        // Gather the values from the Google details
-        const {
-            street_number: streetNumber,
-            route: streetName,
-            subpremise,
-            locality,
-            sublocality,
-            postal_town: postalTown,
-            postal_code: zipCode,
-            administrative_area_level_1: state,
-            administrative_area_level_2: stateFallback,
-            country: countryPrimary,
-        } = GooglePlacesUtils.getAddressComponents(addressComponents, {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            street_number: 'long_name',
-            route: 'long_name',
-            subpremise: 'long_name',
-            locality: 'long_name',
-            sublocality: 'long_name',
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            postal_town: 'long_name',
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            postal_code: 'long_name',
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            administrative_area_level_1: 'short_name',
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            administrative_area_level_2: 'long_name',
-            country: 'short_name',
-        });
-
-        // The state's iso code (short_name) is needed for the StatePicker component but we also
-        // need the state's full name (long_name) when we render the state in a TextInput.
-        const {administrative_area_level_1: longStateName} = GooglePlacesUtils.getAddressComponents(addressComponents, {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            administrative_area_level_1: 'long_name',
-        });
-
-        // Make sure that the order of keys remains such that the country is always set above the state.
-        // Refer to https://github.com/Expensify/App/issues/15633 for more information.
-        const {
-            country: countryFallbackLongName = '',
-            state: stateAutoCompleteFallback = '',
-            city: cityAutocompleteFallback = '',
-        } = GooglePlacesUtils.getPlaceAutocompleteTerms(autocompleteData?.terms ?? []);
-
-        const countryFallback = Object.keys(CONST.ALL_COUNTRIES).find((country) => country === countryFallbackLongName);
-
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        const country = countryPrimary || countryFallback || '';
-
-        const values = {
-            street: `${streetNumber} ${streetName}`.trim(),
-            name: details.name ?? '',
-            // Autocomplete returns any additional valid address fragments (e.g. Apt #) as subpremise.
-            street2: subpremise,
-            // Make sure country is updated first, since city and state will be reset if the country changes
-            country: '',
-            state: state || stateAutoCompleteFallback,
-            // When locality is not returned, many countries return the city as postalTown (e.g. 5 New Street
-            // Square, London), otherwise as sublocality (e.g. 384 Court Street Brooklyn). If postalTown is
-            // returned, the sublocality will be a city subdivision so shouldn't take precedence (e.g.
-            // Salagatan, Upssala, Sweden).
-            city: locality || postalTown || sublocality || cityAutocompleteFallback,
-            zipCode,
-
-            lat: details.geometry.location.lat ?? 0,
-            lng: details.geometry.location.lng ?? 0,
-            address: autocompleteData.description || details.formatted_address || '',
-        };
-
-        // If the address is not in the US, use the full length state name since we're displaying the address's
-        // state / province in a TextInput instead of in a picker.
-        if (country !== CONST.COUNTRY.US) {
-            values.state = longStateName;
-        }
-
-        // UK addresses return countries (e.g. England) in the state field (administrative_area_level_1)
-        // So we use a secondary field (administrative_area_level_2) as a fallback
-        if (country === CONST.COUNTRY.GB) {
-            values.state = stateFallback;
-        }
-
-        // Set the state to be the same as the city in case the state is empty.
-        if (!values.state) {
-            values.state = values.city;
-        }
-
-        // Some edge-case addresses may lack both street_number and route in the API response, resulting in an empty "values.street"
-        // We are setting up a fallback to ensure "values.street" is populated with a relevant value
-        if (!values.street && details.adr_address) {
-            const streetAddressRegex = /<span class="street-address">([^<]*)<\/span>/;
-            const adrAddress = details.adr_address.match(streetAddressRegex);
-            const streetAddressFallback = adrAddress ? adrAddress?.[1] : null;
-            if (streetAddressFallback) {
-                values.street = streetAddressFallback;
-            }
-        }
-
-        // Not all pages define the Address Line 2 field, so in that case we append any additional address details
-        // (e.g. Apt #) to Address Line 1
-        if (subpremise && typeof renamedInputKeys?.street2 === 'undefined') {
-            values.street += `, ${subpremise}`;
-        }
-
-        const isValidCountryCode = !!Object.keys(CONST.ALL_COUNTRIES).find((foundCountry) => foundCountry === country);
-        if (isValidCountryCode) {
-            values.country = country;
-        }
-
-        if (inputID) {
-            Object.entries(values).forEach(([key, inputValue]) => {
-                const inputKey = renamedInputKeys?.[key as keyof RenamedInputKeysProps] ?? key;
-                if (!inputKey) {
-                    return;
+    const saveLocationDetails = useCallback(
+        (autocompleteData: GooglePlaceData, details: GooglePlaceDetail | null) => {
+            const addressComponents = details?.address_components;
+            if (!addressComponents) {
+                // When there are details, but no address_components, this indicates that some predefined options have been passed
+                // to this component which don't match the usual properties coming from auto-complete. In that case, only a limited
+                // amount of data massaging needs to happen for what the parent expects to get from this function.
+                if (details) {
+                    onPress?.({
+                        address: autocompleteData.description ?? '',
+                        lat: details.geometry?.location.lat ?? 0,
+                        lng: details.geometry?.location.lng ?? 0,
+                        name: details?.name,
+                    });
                 }
-                onInputChange?.(inputValue, inputKey);
-            });
-        } else {
-            onInputChange?.(values);
-        }
+                return;
+            }
 
-        onPress?.(values);
-    };
-    const selectFocusedOption = () => {
+            // Gather the values from the Google details
+            const {
+                street_number: streetNumber,
+                route: streetName,
+                subpremise,
+                locality,
+                sublocality,
+                postal_town: postalTown,
+                postal_code: zipCode,
+                administrative_area_level_1: state,
+                administrative_area_level_2: stateFallback,
+                country: countryPrimary,
+            } = GooglePlacesUtils.getAddressComponents(addressComponents, {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                street_number: 'long_name',
+                route: 'long_name',
+                subpremise: 'long_name',
+                locality: 'long_name',
+                sublocality: 'long_name',
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                postal_town: 'long_name',
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                postal_code: 'long_name',
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                administrative_area_level_1: 'short_name',
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                administrative_area_level_2: 'long_name',
+                country: 'short_name',
+            });
+
+            // The state's iso code (short_name) is needed for the StatePicker component but we also
+            // need the state's full name (long_name) when we render the state in a TextInput.
+            const {administrative_area_level_1: longStateName} = GooglePlacesUtils.getAddressComponents(addressComponents, {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                administrative_area_level_1: 'long_name',
+            });
+
+            // Make sure that the order of keys remains such that the country is always set above the state.
+            // Refer to https://github.com/Expensify/App/issues/15633 for more information.
+            const {
+                country: countryFallbackLongName = '',
+                state: stateAutoCompleteFallback = '',
+                city: cityAutocompleteFallback = '',
+            } = GooglePlacesUtils.getPlaceAutocompleteTerms(autocompleteData?.terms ?? []);
+
+            const countryFallback = Object.keys(CONST.ALL_COUNTRIES).find((country) => country === countryFallbackLongName);
+
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            const country = countryPrimary || countryFallback || '';
+
+            const values = {
+                street: `${streetNumber} ${streetName}`.trim(),
+                name: details.name ?? '',
+                // Autocomplete returns any additional valid address fragments (e.g. Apt #) as subpremise.
+                street2: subpremise,
+                // Make sure country is updated first, since city and state will be reset if the country changes
+                country: '',
+                state: state || stateAutoCompleteFallback,
+                // When locality is not returned, many countries return the city as postalTown (e.g. 5 New Street
+                // Square, London), otherwise as sublocality (e.g. 384 Court Street Brooklyn). If postalTown is
+                // returned, the sublocality will be a city subdivision so shouldn't take precedence (e.g.
+                // Salagatan, Upssala, Sweden).
+                city: locality || postalTown || sublocality || cityAutocompleteFallback,
+                zipCode,
+
+                lat: details.geometry.location.lat ?? 0,
+                lng: details.geometry.location.lng ?? 0,
+                address: autocompleteData.description || details.formatted_address || '',
+            };
+
+            // If the address is not in the US, use the full length state name since we're displaying the address's
+            // state / province in a TextInput instead of in a picker.
+            if (country !== CONST.COUNTRY.US) {
+                values.state = longStateName;
+            }
+
+            // UK addresses return countries (e.g. England) in the state field (administrative_area_level_1)
+            // So we use a secondary field (administrative_area_level_2) as a fallback
+            if (country === CONST.COUNTRY.GB) {
+                values.state = stateFallback;
+            }
+
+            // Set the state to be the same as the city in case the state is empty.
+            if (!values.state) {
+                values.state = values.city;
+            }
+
+            // Some edge-case addresses may lack both street_number and route in the API response, resulting in an empty "values.street"
+            // We are setting up a fallback to ensure "values.street" is populated with a relevant value
+            if (!values.street && details.adr_address) {
+                const streetAddressRegex = /<span class="street-address">([^<]*)<\/span>/;
+                const adrAddress = details.adr_address.match(streetAddressRegex);
+                const streetAddressFallback = adrAddress ? adrAddress?.[1] : null;
+                if (streetAddressFallback) {
+                    values.street = streetAddressFallback;
+                }
+            }
+
+            // Not all pages define the Address Line 2 field, so in that case we append any additional address details
+            // (e.g. Apt #) to Address Line 1
+            if (subpremise && typeof renamedInputKeys?.street2 === 'undefined') {
+                values.street += `, ${subpremise}`;
+            }
+
+            const isValidCountryCode = !!Object.keys(CONST.ALL_COUNTRIES).find((foundCountry) => foundCountry === country);
+            if (isValidCountryCode) {
+                values.country = country;
+            }
+
+            if (inputID) {
+                Object.entries(values).forEach(([key, inputValue]) => {
+                    const inputKey = renamedInputKeys?.[key as keyof RenamedInputKeysProps] ?? key;
+                    if (!inputKey) {
+                        return;
+                    }
+                    onInputChange?.(inputValue, inputKey);
+                });
+            } else {
+                onInputChange?.(values);
+            }
+
+            onPress?.(values);
+        },
+        [inputID, onInputChange, onPress, renamedInputKeys],
+    );
+    const selectFocusedOption = useCallback(() => {
         if (!resultRef.current) {
             return;
         }
@@ -250,13 +253,8 @@ function AddressSearch(
 
         // Clear location error code after address is selected
         setLocationErrorCode(null);
-    };
-    useKeyboardShortcut(CONST.KEYBOARD_SHORTCUTS.ENTER, selectFocusedOption, {
-        captureOnInputs: true,
-        shouldBubble: true,
-        shouldStopPropagation: false,
-        isActive: true,
-    });
+    }, [focusedIndex, saveLocationDetails]);
+    useKeyboardShortcut(CONST.KEYBOARD_SHORTCUTS.ENTER, selectFocusedOption);
 
     /** Gets the user's current location and registers success/error callbacks */
     const getCurrentLocation = () => {
@@ -379,13 +377,10 @@ function AddressSearch(
                         renderRow={(data, index) => {
                             const title = data.isPredefinedPlace ? data.name : data.structured_formatting.main_text;
                             const subtitle = data.isPredefinedPlace ? data.description : data.structured_formatting.secondary_text;
-                            if (focusedIndex === index) {
-                                setFocusedIndex(index);
-                            }
                             return (
                                 <Hoverable>
-                                    {(hovered) => (
-                                        <View style={[styles.pv4, styles.ph3, StyleUtils.getBackgroundAndBorderStyle(hovered || focusedIndex === index ? theme.border : theme.appBG)]}>
+                                    {(isHovered) => (
+                                        <View style={[styles.pv4, styles.ph3, StyleUtils.getBackgroundAndBorderStyle(isHovered || focusedIndex === index ? theme.border : theme.appBG)]}>
                                             {title && <Text style={[styles.googleSearchText]}>{title}</Text>}
                                             <Text style={[styles.textLabelSupporting]}>{subtitle}</Text>
                                         </View>
