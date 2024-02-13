5e135e0554da91203724c4ebbb3fec09ceb76d1d
rayane-djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-02-13 21:42:58 +0100

    remove unused imports

--- src/pages/ValidateLoginPage/index.website.tsx
+++ src/pages/ValidateLoginPage/index.website.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import {InteractionManager, NativeModules} from 'react-native';
 import {withOnyx} from 'react-native-onyx';
 import FullScreenLoadingIndicator from '@components/FullscreenLoadingIndicator';
 import ExpiredValidateCodeModal from '@components/ValidateCode/ExpiredValidateCodeModal';
