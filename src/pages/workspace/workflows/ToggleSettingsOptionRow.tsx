6cc2a08cd5629aba19f932d7a242841977110168
Lucien Akchoté <lucien@expensify.com>
2024-02-13 16:34:28 +0100

    create ToggleSettingsOptionRow component

---
+++ src/pages/workspace/workflows/ToggleSettingsOptionRow.tsx
@@ -0,0 +1,51 @@
+import Switch from "@components/Switch";
+import Text from "@components/Text";
+import useLocalize from "@hooks/useLocalize";
+import useThemeStyles from "@hooks/useThemeStyles";
+import { TranslationPaths } from "@src/languages/types";
+import React, { useState } from "react";
+import { View } from "react-native";
+import { SvgProps } from "react-native-svg";
+
+export type OptionType = {
+    Illustration: React.ElementType<SvgProps>;
+    title: string;
+    subtitle: string;
+    onToggle: (isEnabled: boolean) => void;
+    subMenuItems?: React.ReactNode;
+};
+
+const ToggleSettingOptionRow = ({ Illustration, title, subtitle, onToggle, subMenuItems }: OptionType) => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const { translate } = useLocalize();
+    const styles = useThemeStyles();
+  
+    const toggleSwitch = () => {
+      setIsEnabled(previousState => !previousState);
+      onToggle(!isEnabled);
+    };
+  
+    return (
+    <View style={styles.workspaceWorkflowContainer}>        
+        <View style={styles.workspaceWorkflowContent}>
+            <View style={styles.workspaceWorkflowsIllustrationContainer}>
+                <Illustration style={styles.workspaceWorkflowsIcon} />
+            </View>
+            <View style={styles.workspaceWorkflowsWrapperText}>
+                <Text style={styles.workspaceWorkflowsHeading}>{title}</Text>
+                <Text style={styles.workspaceWorkflowsSubtitle}>{subtitle}</Text>
+            </View>
+        </View>
+        <View>
+            <Switch
+                accessibilityLabel={subtitle}
+                onToggle={toggleSwitch}
+                isOn={isEnabled}
+            />
+        </View>
+            {isEnabled && subMenuItems}
+    </View>
+    );
+  };
+
+  export default ToggleSettingOptionRow;
