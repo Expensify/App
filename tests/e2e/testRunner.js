78aa67e4b0b741eede868a756d2f9a8d8a7f1fe7
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-02-13 17:47:17 +0100

    fix: comments

--- tests/e2e/testRunner.js
+++ tests/e2e/testRunner.js
@@ -1,4 +1,3 @@
-// const launchApp = require('./utils/launchApp');
 import launchApp from './utils/launchApp';
 
 /**
--- tests/e2e/utils/launchApp.ts
+++ tests/e2e/utils/launchApp.ts
@@ -2,14 +2,13 @@
 import {ACTIVITY_PATH, MAIN_APP_PACKAGE} from '../config';
 import execAsync from './execAsync';
 
-const launchApp = (platform = 'android', packageName = MAIN_APP_PACKAGE, activityPath = ACTIVITY_PATH, launchArgs = {}) => {
+const launchApp = (platform = 'android', packageName = MAIN_APP_PACKAGE, activityPath = ACTIVITY_PATH, launchArgs: Record<string, boolean> = {}) => {
     if (platform !== 'android') {
         throw new Error(`launchApp() missing implementation for platform: ${platform}`);
     }
 
     // Use adb to start the app
     const launchArgsString = Object.keys(launchArgs)
-        // @ts-expect-error - yes
         .map((key) => `${typeof launchArgs[key] === 'boolean' ? '--ez' : '--es'} ${key} ${launchArgs[key]}`)
         .join(' ');
     return execAsync(`adb shell am start -n ${packageName}/${activityPath} ${launchArgsString}`);
