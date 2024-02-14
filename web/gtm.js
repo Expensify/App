0833ce98b886a91b411d5b79eb36fe92c1777c73
Daniel Silva <dsilva@expensify.com>
2024-02-14 11:50:03 +0100

    linter

--- web/gtm.js
+++ web/gtm.js
@@ -1,1 +1,2 @@
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N4M3FLJZ');
+/* eslint-disable no-param-reassign prefer-template */
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});let f=d.getElementsByTagName(s)[0];let j=d.createElement(s);let dl=l!=='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-N4M3FLJZ');
