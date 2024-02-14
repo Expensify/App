8b6fb5e1e3639f3532c1183e953f416417c6151c
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-02-14 15:58:46 +0100

    Add ErrorFields to PersonalBankAccount type

--- src/libs/actions/ReimbursementAccount/errors.ts
+++ src/libs/actions/ReimbursementAccount/errors.ts
@@ -1,24 +1,24 @@
 import Onyx from 'react-native-onyx';
 import * as ErrorUtils from '@libs/ErrorUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {Errors} from '@src/types/onyx/OnyxCommon';
+import type {ErrorFields} from '@src/types/onyx/OnyxCommon';
 
 /**
  * Set the current fields with errors.
  */
-function setPersonalBankAccountFormValidationErrorFields(errors: Errors) {
+function setPersonalBankAccountFormValidationErrorFields(errorFields: ErrorFields) {
     // We set 'errors' to null first because we don't have a way yet to replace a specific property without merging it
-    Onyx.merge(ONYXKEYS.PERSONAL_BANK_ACCOUNT, {errors: null});
-    Onyx.merge(ONYXKEYS.PERSONAL_BANK_ACCOUNT, {errors});
+    Onyx.merge(ONYXKEYS.PERSONAL_BANK_ACCOUNT, {errorFields: null});
+    Onyx.merge(ONYXKEYS.PERSONAL_BANK_ACCOUNT, {errorFields});
 }
 
 /**
  * Set the current fields with errors.
 
  */
-function setBankAccountFormValidationErrors(errors: Errors) {
-    Onyx.merge(ONYXKEYS.REIMBURSEMENT_ACCOUNT, {errors: null});
-    Onyx.merge(ONYXKEYS.REIMBURSEMENT_ACCOUNT, {errors});
+function setBankAccountFormValidationErrors(errorFields: ErrorFields) {
+    Onyx.merge(ONYXKEYS.REIMBURSEMENT_ACCOUNT, {errorFields: null});
+    Onyx.merge(ONYXKEYS.REIMBURSEMENT_ACCOUNT, {errorFields});
 }
 
 /**
