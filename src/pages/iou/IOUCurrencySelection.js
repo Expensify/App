53edc67a7c41c323060f7984471c983ff109a705
Krishna Gupta <belivethatkg@gmail.com>
2024-02-15 02:04:51 +0530

    minor fix.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/pages/iou/IOUCurrencySelection.js
+++ src/pages/iou/IOUCurrencySelection.js
@@ -17,7 +17,6 @@ import * as ReportActionsUtils from '@libs/ReportActionsUtils';
 import * as ReportUtils from '@libs/ReportUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import ROUTES from '@src/ROUTES';
 import {iouDefaultProps, iouPropTypes} from './propTypes';
 
 /**
@@ -72,6 +71,7 @@ function IOUCurrencySelection(props) {
     const optionsSelectorRef = useRef();
     const selectedCurrencyCode = (lodashGet(props.route, 'params.currency', props.iou.currency) || CONST.CURRENCY.USD).toUpperCase();
     const threadReportID = lodashGet(props.route, 'params.threadReportID', '');
+    const backTo = lodashGet(props.route, 'params.backTo', '');
 
     // Decides whether to allow or disallow editing a money request
     useEffect(() => {
@@ -96,7 +96,6 @@ function IOUCurrencySelection(props) {
 
     const confirmCurrencySelection = useCallback(
         (option) => {
-            const backTo = lodashGet(props.route, 'params.backTo', '');
             Keyboard.dismiss();
 
             // When we refresh the web, the money request route gets cleared from the navigation stack.
@@ -108,7 +107,7 @@ function IOUCurrencySelection(props) {
                 Navigation.navigate(`${props.route.params.backTo}?currency=${option.currencyCode}`);
             }
         },
-        [props.route, props.navigation],
+        [props.route, props.navigation, backTo],
     );
 
     const {translate, currencyList} = props;
@@ -159,7 +158,7 @@ function IOUCurrencySelection(props) {
                 <>
                     <HeaderWithBackButton
                         title={translate('common.selectCurrency')}
-                        onBackButtonPress={() => Navigation.goBack(ROUTES.EDIT_REQUEST.getRoute(threadReportID, CONST.EDIT_REQUEST_FIELD.AMOUNT))}
+                        onBackButtonPress={() => Navigation.goBack(backTo)}
                     />
                     <SelectionList
                         sections={sections}
