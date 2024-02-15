a33bca5d57fab862e3f432c6a0568a56435aaa4e
Shubham Agrawal <shubham.agra1206@gmail.com>
2024-02-15 19:02:37 +0530

    adjust code according to recommendations

--- src/components/FormElement.tsx
+++ src/components/FormElement.tsx
@@ -4,22 +4,19 @@ import type {ViewProps} from 'react-native';
 import {View} from 'react-native';
 import useFormSetup from '@hooks/useFormSetup';
 import * as ComponentUtils from '@libs/ComponentUtils';
+import mergeRefs from '@libs/mergeRefs';
 
 function FormElement(props: ViewProps, outerRef: ForwardedRef<View>) {
     const formRef = useRef<View | null>(null);
+
     useFormSetup(formRef.current);
+
+    const mergedRef = mergeRefs(formRef, outerRef);
+
     return (
         <View
             role={ComponentUtils.ACCESSIBILITY_ROLE_FORM}
-            ref={(ref) => {
-                formRef.current = ref;
-                if (typeof outerRef === 'function') {
-                    outerRef(ref);
-                } else if (outerRef && 'current' in outerRef) {
-                    // eslint-disable-next-line no-param-reassign
-                    outerRef.current = ref;
-                }
-            }}
+            ref={mergedRef}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
         />
--- src/hooks/useFormSetup/index.ts
+++ src/hooks/useFormSetup/index.ts
@@ -2,8 +2,8 @@ import {useEffect} from 'react';
 import type {View} from 'react-native';
 
 const preventFormDefault = (event: SubmitEvent) => {
-    // When enter is pressed form is submitted to action url (POST /).
-    // As we are using controlled component, we need to disable it here.
+    // When Enter is pressed, the form is submitted to the action URL (POST /).
+    // As we are using a controlled component, we need to disable this behavior here.
     event.preventDefault();
 };
 
@@ -13,19 +13,21 @@ const useFormSetup = (form: View | null) => {
             return;
         }
 
-        // This is a safe cast because we know that form is a HTMLFormElement.
-        const formElement = form as unknown as HTMLFormElement;
+        // Sanity check. The form should always be an HTMLFormElement.
+        if (!(form instanceof HTMLFormElement)) {
+            return;
+        }
 
         // Prevent the browser from applying its own validation, which affects the email input
-        formElement.setAttribute('novalidate', '');
+        form.setAttribute('novalidate', '');
 
         // Password Managers need these attributes to be able to identify the form elements properly.
-        formElement.setAttribute('method', 'post');
-        formElement.setAttribute('action', '/');
-        formElement.addEventListener('submit', preventFormDefault);
+        form.setAttribute('method', 'post');
+        form.setAttribute('action', '/');
+        form.addEventListener('submit', preventFormDefault);
 
         return () => {
-            formElement.removeEventListener('submit', preventFormDefault);
+            form.removeEventListener('submit', preventFormDefault);
         };
     }, [form]);
 };
