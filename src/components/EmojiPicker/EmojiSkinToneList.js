47737c48a94838f2db5bf50f3eab8b0a27ced3fe
tienifr <christianwen18@gmail.com>
2024-02-15 11:19:56 +0700

    fix lint

--- src/components/EmojiPicker/EmojiSkinToneList.js
+++ src/components/EmojiPicker/EmojiSkinToneList.js
@@ -36,7 +36,7 @@ function EmojiSkinToneList() {
             return;
         }
         toggleIsSkinToneListVisible();
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- only run when preferredSkinTone updates
+        // eslint-disable-next-line react-hooks/exhaustive-deps -- only run when preferredSkinTone updates
     }, [preferredSkinTone]);
 
     const currentSkinTone = getSkinToneEmojiFromIndex(preferredSkinTone);
