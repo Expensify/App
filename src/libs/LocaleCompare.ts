eff12f4ef2239384ba80202212b81fcdeb8b694a
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-02-20 15:00:47 +0100

    use default locale from CONST

--- src/libs/LocaleCompare.ts
+++ src/libs/LocaleCompare.ts
@@ -1,23 +1,22 @@
 import Onyx from 'react-native-onyx';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-
-const DEFAULT_LOCALE = 'en';
 
 const COLLATOR_OPTIONS: Intl.CollatorOptions = {usage: 'sort', sensitivity: 'base'};
 
-let collator = new Intl.Collator(DEFAULT_LOCALE, COLLATOR_OPTIONS);
+let collator = new Intl.Collator(CONST.LOCALES.DEFAULT, COLLATOR_OPTIONS);
 
 Onyx.connect({
     key: ONYXKEYS.NVP_PREFERRED_LOCALE,
     callback: (locale) => {
-        collator = new Intl.Collator(locale ?? DEFAULT_LOCALE, COLLATOR_OPTIONS);
+        collator = new Intl.Collator(locale ?? CONST.LOCALES.DEFAULT, COLLATOR_OPTIONS);
     },
 });
 
 /**
  * This is a wrapper around the localeCompare function that uses the preferred locale from the user's settings.
  *
- * It re-uses Intl.Collator for performance reasons. See https://github.com/facebook/hermes/issues/867 for more details.
+ * It re-uses Intl.Collator with static options for performance reasons. See https://github.com/facebook/hermes/issues/867 for more details.
  * @param a
  * @param b
  * @returns -1 if a < b, 1 if a > b, 0 if a === b
