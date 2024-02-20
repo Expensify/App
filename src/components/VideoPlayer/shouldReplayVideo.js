b6b77de1bfd8ba8a58b5f8eae257cca60fc58ab4
tienifr <christianwen18@gmail.com>
2024-02-20 22:46:57 +0700

    fix: On full screen mode, when the video ends, the play button does nothing

---
+++ src/components/VideoPlayer/shouldReplayVideo.js
@@ -0,0 +1,13 @@
+/**
+ * Whether to replay the video when users press play button
+ *
+ * @param {Object} e // playback status event
+ * @param {Boolean} isPlaying // current isPlaying status
+ * @param {Number} duration
+ * @param {Number} position
+ * @returns {Boolean}
+ */
+// eslint-disable-next-line no-unused-vars
+export default function shouldReplayVideo(e, isPlaying, duration, position) {
+    return false;
+}
