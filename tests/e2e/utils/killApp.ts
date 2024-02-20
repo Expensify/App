7ba8bee237734b78596121a049928e1f0257be24
ruben-rebelo <ruben.rato96@gmail.com>
2024-02-20 15:59:22 +0000

    [TS migration][killApp] Updated types

--- tests/e2e/utils/killApp.ts
+++ tests/e2e/utils/killApp.ts
@@ -1,9 +1,7 @@
 import {MAIN_APP_PACKAGE} from '../config';
 import execAsync from './execAsync';
 
-type KillApp = (platform: string, packageName: string) => Promise<void>;
-
-const killApp: KillApp = function (platform = 'android', packageName = MAIN_APP_PACKAGE) {
+const killApp = function (platform = 'android', packageName = MAIN_APP_PACKAGE): Promise<void> {
     if (platform !== 'android') {
         throw new Error(`killApp() missing implementation for platform: ${platform}`);
     }
