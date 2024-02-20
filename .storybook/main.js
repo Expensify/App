f3023d7562bbe37cda108d50c38fdb61beef9734
Rocio Perez-Cano <rocio@expensify.com>
2024-02-20 13:04:53 -0500

    Upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,18 @@ module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-react-native-web'],
     staticDirs: ['./public', {from: '../assets/css', to: 'css'}, {from: '../assets/fonts/web', to: 'fonts'}],
-    core: {
-        builder: 'webpack5',
-    },
+
     managerHead: (head) => `
         ${head}
         ${process.env.ENV === 'staging' ? '<meta name="robots" content="noindex">' : ''}
     `,
+
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {}
+    },
+
+    docs: {
+        autodocs: true
+    }
 };
