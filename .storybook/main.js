17d9e8448c1720cd133d7e1ffdb697210152dc07
Rocio Perez-Cano <rocio@expensify.com>
2024-02-21 14:07:04 -0500

    Önly down to 6 errors

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,26 @@
-const custom = require('./webpack.config');
-
 module.exports = {
     framework: '@storybook/react-webpack5',
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-react-native-web'],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y',
+        {
+            name: '@storybook/addon-react-native-web',
+            options: {
+                modulesToAlias: {
+                    'react-native': 'react-native-web',
+                },
+                modulesToTranspile: ['@onfido/react-native-sdk', '@rnmapbox/maps', 'lottie-react-native', '@ua/react-native-airship'],
+            },
+        }
+    ],
     staticDirs: ['./public', {from: '../assets/css', to: 'css'}, {from: '../assets/fonts/web', to: 'fonts'}],
     managerHead: (head) => `
         ${head}
         ${process.env.ENV === 'staging' ? '<meta name="robots" content="noindex">' : ''}
     `,
+    core: {
+        builder: 'webpack5',
+    },
     docs: {
         autodocs: true
-    },
-    webpackFinal: async (config) => {
-        console.log(custom);
     },
 };
