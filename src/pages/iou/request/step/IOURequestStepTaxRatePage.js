cd2883c47a696e5bdd3fbba36b343055c72adb58
Etotaziba Olei <teneeto@gmail.com>
2024-02-21 12:54:36 +0100

    add policy onyx props for IOURequestStepTaxRatePage

--- src/pages/iou/request/step/IOURequestStepTaxRatePage.js
+++ src/pages/iou/request/step/IOURequestStepTaxRatePage.js
@@ -96,5 +96,8 @@ export default compose(
         policyTaxRates: {
             key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY_TAX_RATE}${report ? report.policyID : '0'}`,
         },
+        policy: {
+            key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY}${report ? report.policyID : '0'}`,
+        },
     }),
 )(IOURequestStepTaxRatePage);
