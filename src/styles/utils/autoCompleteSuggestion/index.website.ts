972084ded272602bca2c4da8350e4a522702957d
MrRefactor <jakub.sys@syscrew.pl>
2024-02-21 15:25:55 +0100

    Implement shouldPreventScrollOnAutoCompleteSuggestion function

--- src/styles/utils/autoCompleteSuggestion/index.website.ts
+++ src/styles/utils/autoCompleteSuggestion/index.website.ts
@@ -1,30 +1,8 @@
-import type {ViewStyle} from 'react-native';
 import * as Browser from '@libs/Browser';
-import CONST from '@src/CONST';
+import type ShouldPreventScrollOnAutoCompleteSuggestion from './types';
 
 const isMobileSafari = Browser.isMobileSafari();
 
-function getAutoCompleteSuggestionContainerStyle(itemsHeight: number): ViewStyle {
-    'worklet';
+const shouldPreventScrollOnAutoCompleteSuggestion: ShouldPreventScrollOnAutoCompleteSuggestion = () => !isMobileSafari;
 
-    const borderWidth = 2;
-    let height = itemsHeight + 2 * CONST.AUTO_COMPLETE_SUGGESTER.SUGGESTER_INNER_PADDING;
-    let top = -(height + CONST.AUTO_COMPLETE_SUGGESTER.SUGGESTER_PADDING);
-
-    if (isMobileSafari) {
-        top += borderWidth;
-    } else {
-        height += borderWidth;
-    }
-
-    // The suggester is positioned absolutely within the component that includes the input and RecipientLocalTime view (for non-expanded mode only). To position it correctly,
-    // we need to shift it by the suggester's height plus its padding and, if applicable, the height of the RecipientLocalTime view.
-    return {
-        overflow: 'hidden',
-        top,
-        height,
-        minHeight: CONST.AUTO_COMPLETE_SUGGESTER.SUGGESTION_ROW_HEIGHT,
-    };
-}
-
-export default getAutoCompleteSuggestionContainerStyle;
+export default shouldPreventScrollOnAutoCompleteSuggestion;
