f930deb0f6022c1128f68c062168e91d05b5aa21
Rocio Perez-Cano <rocio@expensify.com>
2024-02-20 22:17:00 -0500

    Set custom one

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,18 @@
+const custom = require('./webpack.config');
+
 module.exports = {
+    framework: '@storybook/react-webpack5',
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-react-native-web'],
     staticDirs: ['./public', {from: '../assets/css', to: 'css'}, {from: '../assets/fonts/web', to: 'fonts'}],
-
     managerHead: (head) => `
         ${head}
         ${process.env.ENV === 'staging' ? '<meta name="robots" content="noindex">' : ''}
     `,
-
-    framework: {
-        name: '@storybook/react-webpack5',
-        options: {}
-    },
-
     docs: {
         autodocs: true
-    }
+    },
+    webpackFinal: async (config) => {
+        console.log(custom);
+    },
 };
