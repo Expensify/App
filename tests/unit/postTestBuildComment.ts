7064cfe6e4487972ff995c5886994cd45f80e5ea
ruben-rebelo <ruben.rato96@gmail.com>
2024-02-21 15:52:22 +0000

    [TS migration] Migrate postTestBuildComment to typescript

--- tests/unit/postTestBuildComment.ts
+++ tests/unit/postTestBuildComment.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core';
 import {when} from 'jest-when';
+import type {Writable} from 'type-fest';
 import ghAction from '../../.github/actions/javascript/postTestBuildComment/postTestBuildComment';
 import GithubUtils from '../../.github/libs/GithubUtils';
 
 const mockGetInput = jest.fn();
-const mockCreateComment = jest.fn();
+const createCommentMock = jest.spyOn(GithubUtils, 'createComment');
 const mockListComments = jest.fn();
 const mockGraphql = jest.fn();
 jest.spyOn(GithubUtils, 'octokit', 'get').mockReturnValue({
@@ -12,7 +13,11 @@ jest.spyOn(GithubUtils, 'octokit', 'get').mockReturnValue({
         listComments: mockListComments,
     },
 });
-jest.spyOn(GithubUtils, 'paginate', 'get').mockReturnValue((endpoint, params) => endpoint(params).then(({data}) => data));
+
+jest.spyOn(GithubUtils, 'paginate', 'get').mockReturnValue(<T, TData>(endpoint: (params: Record<string, T>) => Promise<{data: TData}>, params: Record<string, T>) =>
+    endpoint(params).then((response) => response.data),
+);
+
 jest.spyOn(GithubUtils, 'graphql', 'get').mockReturnValue(mockGraphql);
 
 jest.mock('@actions/github', () => ({
@@ -49,11 +54,12 @@ const message = `:test_tube::test_tube: Use the links below to test this adhoc b
 :eyes: [View the workflow run that generated this build](https://github.com/Expensify/App/actions/runs/1234) :eyes:
 `;
 
+const asMutable = <T>(value: T): Writable<T> => value as Writable<T>;
+
 describe('Post test build comments action tests', () => {
     beforeAll(() => {
         // Mock core module
-        core.getInput = mockGetInput;
-        GithubUtils.createComment = mockCreateComment;
+        asMutable(core).getInput = mockGetInput;
     });
 
     test('Test GH action', async () => {
@@ -66,11 +72,12 @@ describe('Post test build comments action tests', () => {
         when(core.getInput).calledWith('IOS_LINK').mockReturnValue('https://expensify.app/IOS_LINK');
         when(core.getInput).calledWith('WEB_LINK').mockReturnValue('https://expensify.app/WEB_LINK');
         when(core.getInput).calledWith('DESKTOP_LINK').mockReturnValue('https://expensify.app/DESKTOP_LINK');
-        GithubUtils.createComment.mockResolvedValue(true);
+        createCommentMock.mockResolvedValue(true);
         mockListComments.mockResolvedValue({
             data: [
                 {
                     body: ':test_tube::test_tube: Use the links below to test this adhoc build on Android, iOS, Desktop, and Web. Happy testing!',
+                    // eslint-disable-next-line @typescript-eslint/naming-convention
                     node_id: 'IC_abcd',
                 },
             ],
@@ -86,7 +93,7 @@ describe('Post test build comments action tests', () => {
               }
             }
         `);
-        expect(GithubUtils.createComment).toBeCalledTimes(1);
-        expect(GithubUtils.createComment).toBeCalledWith('App', 12, message);
+        expect(createCommentMock).toBeCalledTimes(1);
+        expect(createCommentMock).toBeCalledWith('App', 12, message);
     });
 });
