95f432f162593cae937504f15ebc9f925558b3e8
codinggeek2023 <codinggeek2023@gmail.com>
2024-02-23 00:20:36 +0530

    Change finish to stop to avoid confusion

--- src/components/DistanceEReceipt.tsx
+++ src/components/DistanceEReceipt.tsx
@@ -75,8 +75,6 @@ function DistanceEReceipt({transaction}: DistanceEReceiptProps) {
                             let descriptionKey: TranslationPaths = 'distance.waypointDescription.stop';
                             if (index === 0) {
                                 descriptionKey = 'distance.waypointDescription.start';
-                            } else if (index === Object.keys(waypoints).length - 1) {
-                                descriptionKey = 'distance.waypointDescription.finish';
                             }
 
                             return (
