3e42bb752bce83a2850a096d9c736f8164ccd85f
Jakub Trzebiatowski <cubuspl42@gmail.com>
2024-02-22 10:43:29 +0100

    Polyfill `Array.prototype.toSorted`

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
+import toSorted from 'array.prototype.tosorted';
 import App from './src/App';
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
+
+toSorted.shim();
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
 additionalAppSetup();
