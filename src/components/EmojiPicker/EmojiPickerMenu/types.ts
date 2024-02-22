efdb33c0c9a17f7a5503aa4536db7279b729bd25
Hezekiel Tamire <hezekiel.feyissa@gmail.com>
2024-02-22 12:19:43 +0300

    rename emojiPickerMenuPropTypes to types.ts

---
+++ src/components/EmojiPicker/EmojiPickerMenu/types.ts
@@ -0,0 +1,8 @@
+import type {Emoji} from "@assets/emojis/types";
+
+type EmojiPickerMenuProps = {
+    /** Function to add the selected emoji to the main compose text input */
+    onEmojiSelected: (emoji: string, emojiObject: Emoji) => void,
+};
+
+export default EmojiPickerMenuProps;
