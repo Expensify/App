2327eac1e92262c4039def1143de3c1de5c10c5e
Rocio Perez-Cano <rocio@expensify.com>
2024-02-22 12:45:56 -0500

    Move it to the srotybook file

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,13 +3,22 @@ module.exports = {
     core: {
         builder: {
             name: '@storybook/builder-webpack5',
-            options: {
-                lazyCompilation: true,
-            },
         },
     },
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-react-native-web'],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y',
+        {
+            name: '@storybook/addon-react-native-web',
+            options: {
+                modulesToTranspile: ['@onfido/react-native-sdk', '@rnmapbox/maps', '@ua/react-native-airship'],
+                babelPlugins: [
+                    "@babel/plugin-transform-class-properties",
+                    "@babel/plugin-transform-private-methods",
+                    "@babel/plugin-transform-private-property-in-object",
+                ],
+            },
+        }
+    ],
     staticDirs: ['./public', {from: '../assets/css', to: 'css'}, {from: '../assets/fonts/web', to: 'fonts'}],
     managerHead: (head) => `
         ${head}
