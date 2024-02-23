3573c2b908df161067f54ed2a67fd6d7cebe8090
Blazej Kustra <blazej.kustra@swmansion.com>
2024-02-23 11:31:53 +0100

    Address comments

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -3,16 +3,13 @@ import type Alert from './types';
 /** Shows an alert modal with ok and cancel options. */
 const alert: Alert = (title, description, options) => {
     const result = window.confirm([title, description].filter(Boolean).join('\n'));
+
     if (result) {
         const confirmOption = options?.find(({style}) => style !== 'cancel');
-        if (confirmOption?.onPress) {
-            confirmOption.onPress();
-        }
+        confirmOption?.onPress?.();
     } else {
         const cancelOption = options?.find(({style}) => style === 'cancel');
-        if (cancelOption?.onPress) {
-            cancelOption.onPress();
-        }
+        cancelOption?.onPress?.();
     }
 };
 
