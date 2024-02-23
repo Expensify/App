d6f8cb7df73a30b4060abd437275c497a4414a3f
Etotaziba Olei <teneeto@gmail.com>
2024-02-23 16:17:44 +0100

    add comments to ONYX Props

--- src/pages/iou/request/step/IOURequestStepTaxRatePage.js
+++ src/pages/iou/request/step/IOURequestStepTaxRatePage.js
@@ -27,6 +27,7 @@ const propTypes = {
     /** The transaction object being modified in Onyx */
     transaction: transactionPropTypes,
 
+    /* Onyx Props */
     /** The policy of the report */
     policy: PropTypes.shape({
         /** Collection of tax rates attached to a policy */
