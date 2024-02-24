c4441f0cd488287ae0b3b2ce066cf4c7ba236b23
Dylan <dylandylan9112@gmail.com>
2024-02-24 18:04:20 +0700

    fix ts fail

--- tests/e2e/utils/killApp.ts
+++ tests/e2e/utils/killApp.ts
@@ -1,9 +1,11 @@
 import config from '../config';
 import execAsync from './execAsync';
 
-const killApp = function (platform = 'android', packageName = config.MAIN_APP_PACKAGE): Promise<string | void> & {
+type PromiseWithAbort = Promise<string | void> & {
     abort?: () => void;
-} {
+};
+
+const killApp = function (platform = 'android', packageName = config.MAIN_APP_PACKAGE): PromiseWithAbort {
     if (platform !== 'android') {
         throw new Error(`killApp() missing implementation for platform: ${platform}`);
     }
