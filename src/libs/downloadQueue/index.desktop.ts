7dabcdbf5e90a9e51e73b6b04cc2dc7a26a3668d
Wildan Muhlis <muhliswildan@gmail.com>
2024-02-26 09:15:44 +0700

    add more comments for downloadQueue

--- src/libs/downloadQueue/index.desktop.ts
+++ src/libs/downloadQueue/index.desktop.ts
@@ -1,17 +1,36 @@
+
 import type {BrowserWindow} from 'electron';
 import {download as electronDownload} from './electronDownloadManager';
 import type {Options} from './electronDownloadManagerType';
 
 type DownloadItem = {
+    // The window where the download will be initiated
     win: BrowserWindow;
+    
+    // The URL of the file to be downloaded
     url: string;
+    
+    // The options for the download, such as save path, file name, etc.
     options: Options;
 };
 
+/**
+ * Represents a queue of download items.
+ */
 type DownloadQueue = DownloadItem[];
+
+/**
+ * Creates a download queue.
+ * @returns An object with methods to push and shift download items from the queue.
+ */
 const createDownloadQueue = () => {
     const queue: DownloadQueue = [];
 
+    /**
+     * Shifts and returns the first item from the download queue.
+     * If the queue is not empty, it triggers the download of the next item.
+     * @returns The shifted DownloadItem or undefined if the queue is empty.
+     */
     const shiftDownloadItem = (): DownloadItem | undefined => {
         const item = queue.shift();
         if (queue.length > 0) {
@@ -23,6 +42,10 @@ const createDownloadQueue = () => {
         return item;
     };
 
+    /**
+     * Downloads the specified item.
+     * @param item - The item to be downloaded.
+     */
     const downloadItem = (item: DownloadItem): void => {
         const newItem = {
             ...item,
@@ -40,6 +63,12 @@ const createDownloadQueue = () => {
         electronDownload(newItem.win, newItem.url, newItem.options);
     };
 
+    /**
+     * Pushes a download item to the queue and returns the new length of the queue.
+     * If the queue was empty before pushing the item, it will immediately start downloading the item.
+     * @param item The download item to be pushed to the queue.
+     * @returns The new length of the queue after pushing the item.
+     */
     const pushDownloadItem = (item: DownloadItem): number => {
         const len = queue.push(item);
         if (queue.length === 1) {
