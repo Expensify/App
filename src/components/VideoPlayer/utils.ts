fe9e2cf3811d0bd13e0cbb064b74ee138005a421
FitseTLT <engfitsum@gmail.com>
2024-02-26 23:42:15 +0300

    undo unnecessary change

--- src/components/VideoPlayer/utils.ts
+++ src/components/VideoPlayer/utils.ts
@@ -3,8 +3,7 @@ import {format} from 'date-fns';
 // Converts milliseconds to 'minutes:seconds' format
 const convertMillisecondsToTime = (milliseconds: number) => {
     const date = new Date(milliseconds);
-    return format(date, 'mm:ss', {locale: {}});
-    Locale;
+    return format(date, 'mm:ss');
 };
 
 export default convertMillisecondsToTime;
