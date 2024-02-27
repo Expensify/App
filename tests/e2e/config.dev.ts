d2c42bbff312098a12fc5a7cb78a0b31f4d1a616
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-02-27 10:49:43 +0100

    migrate config.dev.js to TypeScript

--- tests/e2e/config.dev.ts
+++ tests/e2e/config.dev.ts
@@ -1,7 +1,9 @@
 const packageName = 'com.expensify.chat.dev';
 const appPath = './android/app/build/outputs/apk/development/debug/app-development-debug.apk';
 
-export default {
+type Config = Record<string, string | number>;
+
+const config: Config = {
     MAIN_APP_PACKAGE: packageName,
     DELTA_APP_PACKAGE: packageName,
     MAIN_APP_PATH: appPath,
@@ -9,3 +11,5 @@ export default {
     RUNS: 8,
     BOOT_COOL_DOWN: 5 * 1000,
 };
+
+export default config;
