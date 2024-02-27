56ab70862ec2a6d2c3fcd11e188d7ef6266dd421
Riya Shete <156463907+codinggeek2023@users.noreply.github.com>
2024-02-28 00:57:58 +0530

    Update IOURequestStepWaypoint.tsx

--- src/pages/iou/request/step/IOURequestStepWaypoint.tsx
+++ src/pages/iou/request/step/IOURequestStepWaypoint.tsx
@@ -82,8 +82,6 @@ function IOURequestStepWaypoint({
         switch (parsedWaypointIndex) {
             case 0:
                 return 'distance.waypointDescription.start';
-            case waypointCount - 1:
-                return 'distance.waypointDescription.finish';
             default:
                 return 'distance.waypointDescription.stop';
         }
