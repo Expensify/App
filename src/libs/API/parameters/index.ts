d054157f93ee99a84964ddd995b2bbee3dd5b756
Lucien Akchoté <lucien@expensify.com>
2024-02-27 13:40:20 +0100

    add monthly offset api params command

--- src/libs/API/parameters/index.ts
+++ src/libs/API/parameters/index.ts
@@ -147,5 +147,6 @@ export type {default as AcceptACHContractForBankAccount} from './AcceptACHContra
 export type {default as UpdateWorkspaceDescriptionParams} from './UpdateWorkspaceDescriptionParams';
 export type {default as SetWorkspaceAutoReportingParams} from './SetWorkspaceAutoReportingParams';
 export type {default as SetWorkspaceAutoReportingFrequencyParams} from './SetWorkspaceAutoReportingFrequencyParams';
+export type {default as SetWorkspaceAutoReportingMonthlyOffsetParams} from './SetWorkspaceAutoReportingMonthlyOffsetParams';
 export type {default as SetWorkspaceApprovalModeParams} from './SetWorkspaceApprovalModeParams';
 export type {default as SwitchToOldDotParams} from './SwitchToOldDotParams';
