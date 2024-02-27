bdddc57dcdf09433296f9db95fa99ccf2a4e4614
Bartek Dybowski <b.dybowski@gmail.com>
2024-02-27 08:21:06 +0100

    building bulk actions options conditional fixed
    Co-authored-by: Carlos Martins <luacmartins@gmail.com>

--- src/pages/workspace/WorkspaceMembersPage.tsx
+++ src/pages/workspace/WorkspaceMembersPage.tsx
@@ -421,7 +421,8 @@ function WorkspaceMembersPage({policyMembers, personalDetails, route, policy, se
                 onSelected: () => handleBulkAction(CONST.POLICY.MEMBERS_BULK_ACTION_TYPES.MAKE_MEMBER),
                 ...iconSettings,
             });
-        } else if (selectedEmployees.find((employee) => policyMembers?.[employee].role === CONST.POLICY.ROLE.USER)) {
+        } 
+        if (selectedEmployees.find((employee) => policyMembers?.[employee].role === CONST.POLICY.ROLE.USER)) {
             options.push({
                 text: translate('workspace.people.makeAdmin'),
                 value: CONST.POLICY.MEMBERS_BULK_ACTION_TYPES.MAKE_ADMIN,
