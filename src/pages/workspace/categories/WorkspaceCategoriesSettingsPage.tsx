edb225169af739dc7a5392976e0ed97c32345bcd
Arkadiusz Chrabąszczewski <Arkadiuszchr@gmail.com>
2024-02-27 10:34:17 +0100

    Revert "fix: remove offline indicator"
    This reverts commit 3fb6e9874cac8f56a931f31ed2618db79ad54e1a.

--- src/pages/workspace/categories/WorkspaceCategoriesSettingsPage.tsx
+++ src/pages/workspace/categories/WorkspaceCategoriesSettingsPage.tsx
@@ -2,6 +2,7 @@ import type {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
 import {View} from 'react-native';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import OfflineWithFeedback from '@components/OfflineWithFeedback';
 import ScreenWrapper from '@components/ScreenWrapper';
 import Switch from '@components/Switch';
 import Text from '@components/Text';
@@ -12,6 +13,7 @@ import type {SettingsNavigatorParamList} from '@navigation/types';
 import AdminPolicyAccessOrNotFoundWrapper from '@pages/workspace/AdminPolicyAccessOrNotFoundWrapper';
 import PaidPolicyAccessOrNotFoundWrapper from '@pages/workspace/PaidPolicyAccessOrNotFoundWrapper';
 import type SCREENS from '@src/SCREENS';
+import type * as OnyxCommon from '@src/types/onyx/OnyxCommon';
 
 type WorkspaceCategoriesSettingsPageProps = StackScreenProps<SettingsNavigatorParamList, typeof SCREENS.WORKSPACE.CATEGORIES_SETTINGS>;
 
@@ -34,16 +36,22 @@ function WorkspaceCategoriesSettingsPage({route}: WorkspaceCategoriesSettingsPag
                         shouldShowOfflineIndicatorInWideScreen
                     >
                         <HeaderWithBackButton title={translate('common.settings')} />
-                        <View style={[styles.mt2, styles.mh4]}>
-                            <View style={[styles.flexRow, styles.mb5, styles.mr2, styles.alignItemsCenter, styles.justifyContentBetween]}>
-                                <Text style={[styles.textNormal, styles.colorMuted]}>{translate('workspace.categories.requiresCategory')}</Text>
-                                <Switch
-                                    isOn={policy?.requiresCategory ?? false}
-                                    accessibilityLabel={translate('workspace.categories.requiresCategory')}
-                                    onToggle={updateWorkspaceRequiresCategory}
-                                />
+                        <OfflineWithFeedback
+                            errors={policy?.errorFields?.requiresCategory}
+                            pendingAction={policy?.pendingFields?.requiresCategory as OnyxCommon.PendingAction}
+                            errorRowStyles={styles.mh5}
+                        >
+                            <View style={[styles.mt2, styles.mh4]}>
+                                <View style={[styles.flexRow, styles.mb5, styles.mr2, styles.alignItemsCenter, styles.justifyContentBetween]}>
+                                    <Text style={[styles.textNormal, styles.colorMuted]}>{translate('workspace.categories.requiresCategory')}</Text>
+                                    <Switch
+                                        isOn={policy?.requiresCategory ?? false}
+                                        accessibilityLabel={translate('workspace.categories.requiresCategory')}
+                                        onToggle={updateWorkspaceRequiresCategory}
+                                    />
+                                </View>
                             </View>
-                        </View>
+                        </OfflineWithFeedback>
                     </ScreenWrapper>
                 )}
             </PaidPolicyAccessOrNotFoundWrapper>
