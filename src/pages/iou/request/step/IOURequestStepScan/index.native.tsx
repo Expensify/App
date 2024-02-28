2f6ebf1a538412ff3e7b03e55a48cea43ce4354a
Riya Shete <156463907+codinggeek2023@users.noreply.github.com>
2024-02-29 00:33:51 +0530

    Update index.native.tsx

--- src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
@@ -193,9 +193,7 @@ function IOURequestStepScan({
         }
 
         // Store the receipt on the transaction object in Onyx
-        // On Android devices, fetching blob for a file with name containing spaces fails to retrieve the type of file.
-        // So, let us also save the file type in receipt for later use during blob fetch
-        IOU.setMoneyRequestReceipt(transactionID, file.uri, file.name, action !== CONST.IOU.ACTION.EDIT, file.type);
+        IOU.setMoneyRequestReceipt(transactionID, file.uri, file.name, action !== CONST.IOU.ACTION.EDIT);
 
         if (action === CONST.IOU.ACTION.EDIT) {
             updateScanAndNavigate(file, file.uri);
