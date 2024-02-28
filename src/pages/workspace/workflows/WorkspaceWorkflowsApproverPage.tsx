3f524bae29fa5c6e7c6e8c018d474b57dcc05596
Rushat Gabhane <rushatgabhane@gmail.com>
2024-02-28 06:35:11 +0300

    add check for approver

--- src/pages/workspace/workflows/WorkspaceWorkflowsApproverPage.tsx
+++ src/pages/workspace/workflows/WorkspaceWorkflowsApproverPage.tsx
@@ -1,31 +1,26 @@
-import Str from 'expensify-common/lib/str';
 import React, {useMemo, useState} from 'react';
-import {SectionListData} from 'react-native';
 import {OnyxEntry, withOnyx} from 'react-native-onyx';
+import Badge from '@components/Badge';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import SelectionList from '@components/SelectionList';
-import type {ListItem, Section} from '@components/SelectionList/types';
+import type {ListItem} from '@components/SelectionList/types';
 import UserListItem from '@components/SelectionList/UserListItem';
-import useCurrentUserPersonalDetails from '@hooks/useCurrentUserPersonalDetails';
 import useLocalize from '@hooks/useLocalize';
 import useNetwork from '@hooks/useNetwork';
+import useStyleUtils from '@hooks/useStyleUtils';
 import useThemeStyles from '@hooks/useThemeStyles';
 import compose from '@libs/compose';
-import * as DeviceCapabilities from '@libs/DeviceCapabilities';
-import * as LoginUtils from '@libs/LoginUtils';
+import {formatPhoneNumber} from '@libs/LocalePhoneNumber';
+import Log from '@libs/Log';
 import Navigation from '@libs/Navigation/Navigation';
 import * as OptionsListUtils from '@libs/OptionsListUtils';
-import type {MemberForList} from '@libs/OptionsListUtils';
 import * as PersonalDetailsUtils from '@libs/PersonalDetailsUtils';
-import {parsePhoneNumber} from '@libs/PhoneNumber';
-import * as PolicyUtils from '@libs/PolicyUtils';
-import type {OptionData} from '@libs/ReportUtils';
-import type {SettingsNavigatorParamList} from '@navigation/types';
-import withPolicy, {WithPolicyProps} from '@pages/workspace/withPolicy';
-import * as Policy from '@userActions/Policy';
+import * as UserUtils from '@libs/UserUtils';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import {Beta, InvitedEmailsToAccountIDs, PersonalDetails, PersonalDetailsList} from '@src/types/onyx';
+import {PersonalDetailsList, PolicyMember} from '@src/types/onyx';
+import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import withPolicyAndFullscreenLoading from '../withPolicyAndFullscreenLoading';
 import type {WithPolicyAndFullscreenLoadingProps} from '../withPolicyAndFullscreenLoading';
 
@@ -35,25 +30,74 @@ type WorkspaceWorkflowsApproverPageOnyxProps = {
 };
 
 type WorkspaceWorkflowsApproverPageProps = WorkspaceWorkflowsApproverPageOnyxProps & WithPolicyAndFullscreenLoadingProps;
-type MembersSection = SectionListData<MemberForList, Section<MemberForList>>;
+type MemberOption = Omit<ListItem, 'accountID'> & {accountID: number};
 
 function WorkspaceWorkflowsApproverPage({policy, policyMembers, personalDetails}: WorkspaceWorkflowsApproverPageProps) {
     const {translate} = useLocalize();
     const policyName = policy?.name ?? '';
     const [searchTerm, setSearchTerm] = useState('');
+    const {isOffline} = useNetwork();
+    const styles = useThemeStyles();
+    const StyleUtils = useStyleUtils();
+
     const headerMessage = useMemo(() => {
         const searchValue = searchTerm.trim().toLowerCase();
         return OptionsListUtils.getHeaderMessage(true, false, searchValue);
     }, [translate, searchTerm, policyName]);
 
-    const policyMemberAccountIDs = Object.keys(policyMembers ?? {}).map((accountId) => accountId);
-    const policyMemberDetails = policyMemberAccountIDs.filter((accountID) => !!personalDetails?.[accountID]).map((accountID) => personalDetails?.[accountID]);
+    const isDeletedPolicyMember = (policyMember: PolicyMember): boolean =>
+        !isOffline && policyMember.pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE && isEmptyObject(policyMember.errors);
 
-    const sections: MembersSection[] = useMemo(() => {
-        let result: MembersSection[] = [];
+    const policyUsers: MemberOption[] = useMemo(() => {
+        let result: MemberOption[] = [];
+        Object.entries(policyMembers ?? {}).forEach(([accountIDKey, policyMember]) => {
+            const accountID = Number(accountIDKey);
+            if (isDeletedPolicyMember(policyMember)) {
+                return;
+            }
 
+            const details = personalDetails?.[accountID];
+            if (!details) {
+                Log.hmmm(`[WorkspaceMembersPage] no personal details found for policy member with accountID: ${accountID}`);
+                return;
+            }
+
+            const isOwner = policy?.owner === details.login;
+            const isAdmin = policyMember.role === CONST.POLICY.ROLE.ADMIN;
+
+            let roleBadge = null;
+            if (isOwner || isAdmin) {
+                roleBadge = (
+                    <Badge
+                        text={isOwner ? translate('common.owner') : translate('common.admin')}
+                        textStyles={styles.textStrong}
+                        badgeStyles={[styles.justifyContentCenter, StyleUtils.getMinimumWidth(60), styles.badgeBordered]}
+                    />
+                );
+            }
+
+            result.push({
+                keyForList: accountIDKey,
+                accountID,
+                isSelected: policy?.approver === details.login,
+                isDisabled: policyMember.pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE || !isEmptyObject(policyMember.errors),
+                text: formatPhoneNumber(PersonalDetailsUtils.getDisplayNameOrDefault(details)),
+                alternateText: formatPhoneNumber(details?.login ?? ''),
+                rightElement: roleBadge,
+                icons: [
+                    {
+                        source: UserUtils.getAvatar(details.avatar, accountID),
+                        name: formatPhoneNumber(details?.login ?? ''),
+                        type: CONST.ICON_TYPE_AVATAR,
+                        id: accountID,
+                    },
+                ],
+                errors: policyMember.errors,
+                pendingAction: policyMember.pendingAction,
+            });
+        });
         return result;
-    }, [personalDetails, searchTerm, translate]);
+    }, [personalDetails, policyMembers, searchTerm, translate]);
 
     return (
         <ScreenWrapper
@@ -66,7 +110,7 @@ function WorkspaceWorkflowsApproverPage({policy, policyMembers, personalDetails}
                 onBackButtonPress={Navigation.goBack}
             />
             <SelectionList
-                sections={sections}
+                sections={[{data: policyUsers, indexOffset: 0}]}
                 textInputLabel={translate('optionsSelector.findMember')}
                 textInputValue={searchTerm}
                 onChangeText={setSearchTerm}
