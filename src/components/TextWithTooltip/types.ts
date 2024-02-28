c140118d793b8400ca9f12f29c1cf0f92d603b99
Someshwar Tripathi <someshwartripathi8@gmail.com>
2024-02-28 06:57:37 +0530

    Add the missing prop comments in TextWithTooltip Props

--- src/components/TextWithTooltip/types.ts
+++ src/components/TextWithTooltip/types.ts
@@ -1,8 +1,13 @@
 import type {StyleProp, TextStyle} from 'react-native';
 
 type TextWithTooltipProps = {
+    /** The text to display */
     text: string;
+
+    /** Whether to show the toolip text */
     shouldShowTooltip: boolean;
+    
+    /** Additional text styles */
     textStyles?: StyleProp<TextStyle>;
 
     /** Custom number of lines for Text */
