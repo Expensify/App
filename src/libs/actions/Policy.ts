a9fc40510dd634ec3838324e20fdf84f88a07566
Rushat Gabhane <rushatgabhane@gmail.com>
2024-02-28 08:02:01 +0300

    add set approver action

--- src/libs/actions/Policy.ts
+++ src/libs/actions/Policy.ts
@@ -22,6 +22,7 @@ import type {
     OpenWorkspaceReimburseViewParams,
     SetWorkspaceApprovalModeParams,
     SetWorkspaceAutoReportingParams,
+    SetWorkspaceWorkflowsApproverParams,
     UpdateWorkspaceAvatarParams,
     UpdateWorkspaceCustomUnitAndRateParams,
     UpdateWorkspaceDescriptionParams,
