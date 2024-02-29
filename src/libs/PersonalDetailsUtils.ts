e08f21791e7ea9004e9e7629fdb743ddbff29292
Rushat Gabhane <rushatgabhane@gmail.com>
2024-03-01 01:38:24 +0300

    fix func

--- src/libs/PersonalDetailsUtils.ts
+++ src/libs/PersonalDetailsUtils.ts
@@ -57,7 +57,7 @@ function getPersonalDetailsByIDs(accountIDs: number[], currentUserAccountID: num
 }
 
 function getPersonalDetailByEmail(email: string): PersonalDetails | undefined {
-    return Object.entries(allPersonalDetails ?? {}).find(([_, detail]) => detail?.login === email) as unknown as PersonalDetails;
+    return Object.values(allPersonalDetails ?? {}).find((detail) => detail?.login === email) as unknown as PersonalDetails;
 }
 
 /**
