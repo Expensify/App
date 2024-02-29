2df91801031effaccad339a4f2d6abd0b8478bfa
Daniel Silva <dsilva@expensify.com>
2024-02-29 15:25:07 +0100

    passing parameter to store stashed login data

--- src/pages/LogOutPreviousUserPage.tsx
+++ src/pages/LogOutPreviousUserPage.tsx
@@ -31,7 +31,7 @@ function LogOutPreviousUserPage({session, route}: LogOutPreviousUserPageProps) {
             const isSupportalLogin = route.params.authTokenType === CONST.AUTH_TOKEN_TYPES.SUPPORT;
 
             if (isLoggingInAsNewUser) {
-                SessionActions.signOutAndRedirectToSignIn();
+                SessionActions.signOutAndRedirectToSignIn(false, isSupportalLogin);
             }
 
             if (isSupportalLogin) {
