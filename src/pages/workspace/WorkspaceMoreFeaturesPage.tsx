a144cbe383e193bf22f1d06e4886d63ea09e6bc3
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-02-29 16:44:51 +0100

    build draft sections

--- src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
+++ src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
@@ -1,11 +1,11 @@
+/* eslint-disable no-console */
 import type {StackScreenProps} from '@react-navigation/stack';
 import React, {useMemo} from 'react';
-import {FlatList, View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Illustrations from '@components/Icon/Illustrations';
 import ScreenWrapper from '@components/ScreenWrapper';
-import Text from '@components/Text';
-import WorkspaceEmptyStateSection from '@components/WorkspaceEmptyStateSection';
+import Section from '@components/Section';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
@@ -13,32 +13,132 @@ import type {CentralPaneNavigatorParamList} from '@libs/Navigation/types';
 import type SCREENS from '@src/SCREENS';
 import AdminPolicyAccessOrNotFoundWrapper from './AdminPolicyAccessOrNotFoundWrapper';
 import PaidPolicyAccessOrNotFoundWrapper from './PaidPolicyAccessOrNotFoundWrapper';
+import type {ToggleSettingOptionRowProps} from './workflows/ToggleSettingsOptionRow';
 import ToggleSettingOptionRow from './workflows/ToggleSettingsOptionRow';
 
-type WorkspaceMoreFeaturesPageProps = StackScreenProps<CentralPaneNavigatorParamList, typeof SCREENS.WORKSPACE.CATEGORIES>;
+/**
+areCategoriesEnabled
+areTagsEnabled
+areDistanceRatesEnabled
+areWorkflowsEnabled
+areReportFieldsEnabled
+areConnectionsEnabled
+tax.trackingEnabled (see above)
+*/
 
-type FeatureForList = {
-    value: string;
-    text: string;
-    keyForList: string;
-    isSelected: boolean;
-};
+type WorkspaceMoreFeaturesPageProps = StackScreenProps<CentralPaneNavigatorParamList, typeof SCREENS.WORKSPACE.CATEGORIES>;
 
 function WorkspaceMoreFeaturesPage({route}: WorkspaceMoreFeaturesPageProps) {
     const styles = useThemeStyles();
     const {isSmallScreenWidth} = useWindowDimensions();
     const {translate} = useLocalize();
 
-    const featureList = useMemo<FeatureForList[]>(
-        () =>
-            [].map((name, index) => ({
-                // ['Feature 1', 'Feature 2', 'Feature 3'].map((name, index) => ({
-                value: name,
-                text: name,
-                keyForList: name,
-                isSelected: index % 2 === 0,
-            })),
+    const spendItems: ToggleSettingOptionRowProps[] = useMemo(
+        () => [
+            {
+                icon: Illustrations.Car,
+                title: 'Distance rates',
+                subtitle: 'Add, update and enforce rates',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+            {
+                icon: Illustrations.HandCard,
+                title: 'Expensify card',
+                subtitle: 'Gain insights and control over spend',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+            {
+                icon: Illustrations.Workflows,
+                title: 'Workflows',
+                subtitle: 'Configure how spend is approved and paid',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+        ],
         [],
+    );
+
+    const organizeItems: ToggleSettingOptionRowProps[] = useMemo(
+        () => [
+            {
+                icon: Illustrations.FolderOpen,
+                title: 'Categories',
+                subtitle: 'Track and organize spend',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+            {
+                icon: Illustrations.Tag,
+                title: 'Tags',
+                subtitle: 'Add additional ways to classify spend',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+            {
+                icon: Illustrations.Coins,
+                title: 'Taxes',
+                subtitle: 'Document and reclaim eligible taxes.',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+            {
+                icon: Illustrations.Pencil,
+                title: 'Report fields',
+                subtitle: 'Subtitle...',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+            {
+                icon: Illustrations.Accounting,
+                title: 'Connections',
+                subtitle: 'Subtitle...',
+                onToggle: (isEnabled: boolean) => {
+                    console.log('isEnabled', isEnabled);
+                },
+                isActive: false,
+                pendingAction: undefined,
+            },
+        ],
+        [],
+    );
+
+    const renderItem = (item: ToggleSettingOptionRowProps) => (
+        <View
+            key={item.title}
+            style={styles.mt7}
+        >
+            <ToggleSettingOptionRow
+                icon={item.icon}
+                title={item.title}
+                subtitle={item.subtitle}
+                onToggle={item.onToggle}
+                isActive={item.isActive}
+                pendingAction={item.pendingAction}
+            />
+        </View>
     );
 
     return (
@@ -51,38 +151,36 @@ function WorkspaceMoreFeaturesPage({route}: WorkspaceMoreFeaturesPageProps) {
                     shouldShowOfflineIndicatorInWideScreen
                 >
                     <HeaderWithBackButton
-                        icon={Illustrations.FolderOpen}
+                        icon={Illustrations.Gears}
                         title={translate('workspace.common.moreFeatures')}
                         shouldShowBackButton={isSmallScreenWidth}
                     />
 
-                    <View style={[styles.ph5, styles.pb5]}>
-                        <Text style={[styles.textNormal, styles.colorMuted]}>Page subtitle...</Text>
-                    </View>
+                    <ScrollView>
+                        <View style={[styles.mt3, isSmallScreenWidth ? styles.workspaceSectionMobile : styles.workspaceSection]}>
+                            <Section
+                                containerStyles={isSmallScreenWidth ? styles.p5 : styles.p8}
+                                title="Spend"
+                                titleStyles={styles.textStrong}
+                                subtitle="Enable optional functionality that helps you scale your team."
+                                subtitleMuted
+                            >
+                                {spendItems.map(renderItem)}
+                            </Section>
+                        </View>
 
-                    {featureList.length ? (
-                        <FlatList
-                            style={styles.flex1}
-                            contentContainerStyle={[styles.pl3, styles.pr9]}
-                            keyExtractor={(item) => item.keyForList}
-                            data={featureList}
-                            renderItem={({item}) => (
-                                <ToggleSettingOptionRow
-                                    icon={Illustrations.FolderOpen}
-                                    title={item.text}
-                                    subtitle={item.text}
-                                    onToggle={() => {}}
-                                    isActive={false}
-                                />
-                            )}
-                        />
-                    ) : (
-                        <WorkspaceEmptyStateSection
-                            title="Empty features title"
-                            icon={Illustrations.EmptyStateExpenses}
-                            subtitle="Empty features subtitle"
-                        />
-                    )}
+                        <View style={[styles.mt3, isSmallScreenWidth ? styles.workspaceSectionMobile : styles.workspaceSection]}>
+                            <Section
+                                containerStyles={isSmallScreenWidth ? styles.p5 : styles.p8}
+                                title="Organize"
+                                titleStyles={styles.textStrong}
+                                subtitle="Group and analyze spend, record every tax paid."
+                                subtitleMuted
+                            >
+                                {organizeItems.map(renderItem)}
+                            </Section>
+                        </View>
+                    </ScrollView>
                 </ScreenWrapper>
             </PaidPolicyAccessOrNotFoundWrapper>
         </AdminPolicyAccessOrNotFoundWrapper>
