580a53e0566f375e5ed0684a59912a14f0d50f1d
Krishna Gupta <belivethatkg@gmail.com>
2024-02-29 12:00:25 +0530

    Update as suggested.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -97,8 +97,6 @@ function Checkbox(
             aria-checked={isChecked}
             accessibilityLabel={accessibilityLabel}
             pressDimmingValue={1}
-            // Explicitly need to add this because getCheckboxPressableStyle returns `cursorPointer` which will override disabled cursor style determined by `disabled` prop
-            disabledStyle={styles.cursorDisabled}
         >
             {children ?? (
                 <View
--- src/components/SelectionList/BaseListItem.tsx
+++ src/components/SelectionList/BaseListItem.tsx
@@ -79,7 +79,7 @@ function BaseListItem<TItem extends ListItem>({
                                     accessibilityLabel={item.text}
                                     role={CONST.ROLE.BUTTON}
                                     onPress={handleCheckboxPress}
-                                    style={StyleUtils.getCheckboxPressableStyle()}
+                                    style={[styles.cursorUnset, StyleUtils.getCheckboxPressableStyle()]}
                                 >
                                     <View style={selectMultipleStyle}>
                                         {item.isSelected && (
