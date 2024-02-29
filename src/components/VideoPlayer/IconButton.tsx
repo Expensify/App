65e1faf057fdc95ffaf8f78d5248a66cfae9ff36
smelaa <aleksandra.smela@gmail.com>
2024-02-29 15:36:50 +0100

    IconButton moved to ts

--- src/components/VideoPlayer/IconButton.tsx
+++ src/components/VideoPlayer/IconButton.tsx
@@ -1,43 +1,24 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import type {GestureResponderEvent, StyleProp, ViewStyle} from 'react-native';
 import Hoverable from '@components/Hoverable';
 import Icon from '@components/Icon';
 import PressableWithoutFeedback from '@components/Pressable/PressableWithoutFeedback';
 import Tooltip from '@components/Tooltip';
 import useThemeStyles from '@hooks/useThemeStyles';
-import stylePropTypes from '@styles/stylePropTypes';
+import type IconAsset from '@src/types/utils/IconAsset';
 
-const propTypes = {
-    // use IconAsset as soon as it will be migrated to TS
-    // eslint-disable-next-line react/forbid-prop-types
-    src: PropTypes.any.isRequired,
-
-    onPress: PropTypes.func,
-
-    fill: PropTypes.string,
-
-    tooltipText: PropTypes.string,
-
-    style: stylePropTypes,
-
-    hoverStyle: stylePropTypes,
-
-    small: PropTypes.bool,
-
-    shouldForceRenderingTooltipBelow: PropTypes.bool,
+type IconButtonProps = {
+    src: IconAsset;
+    onPress?: (event?: GestureResponderEvent | KeyboardEvent) => void | Promise<void>;
+    fill?: string;
+    tooltipText?: string;
+    style?: StyleProp<ViewStyle>;
+    hoverStyle?: StyleProp<ViewStyle>;
+    small?: boolean;
+    shouldForceRenderingTooltipBelow?: boolean;
 };
 
-const defaultProps = {
-    fill: 'white',
-    onPress: () => {},
-    style: {},
-    hoverStyle: {},
-    small: false,
-    tooltipText: '',
-    shouldForceRenderingTooltipBelow: false,
-};
-
-function IconButton({src, fill, onPress, style, hoverStyle, tooltipText, small, shouldForceRenderingTooltipBelow}) {
+function IconButton({src, fill = 'white', onPress = () => {}, style = {}, hoverStyle = {}, tooltipText = '', small = false, shouldForceRenderingTooltipBelow = false}: IconButtonProps) {
     const styles = useThemeStyles();
     return (
         <Tooltip
@@ -63,8 +44,6 @@ function IconButton({src, fill, onPress, style, hoverStyle, tooltipText, small, 
     );
 }
 
-IconButton.propTypes = propTypes;
-IconButton.defaultProps = defaultProps;
 IconButton.displayName = 'IconButton';
 
 export default IconButton;
