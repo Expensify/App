14fb50d6e7900f0419b0a81a7355ef04135e2289
DylanDylann <141406735+DylanDylann@users.noreply.github.com>
2024-03-05 00:21:27 +0700

    Update src/pages/ReportParticipantsPage.tsx
    Co-authored-by: Getabalew Tesfaye <75031127+getusha@users.noreply.github.com>

--- src/pages/ReportParticipantsPage.tsx
+++ src/pages/ReportParticipantsPage.tsx
@@ -50,7 +50,7 @@ const getAllParticipants = (
                 !!userPersonalDetail?.login && !CONST.RESTRICTED_ACCOUNT_IDS.includes(accountID) ? LocalePhoneNumber.formatPhoneNumber(userPersonalDetail.login) : translate('common.hidden');
             const displayName = PersonalDetailsUtils.getDisplayNameOrDefault(userPersonalDetail);
 
-            const pendingVisibleChatMember = report?.pendingVisibleChatMembers ? report?.pendingVisibleChatMembers.find((member) => member.accountID === accountID.toString()) : undefined;
+            const pendingVisibleChatMember = report?.pendingVisibleChatMembers?.find((member) => member.accountID === accountID.toString());
             return {
                 alternateText: userLogin,
                 pendingAction: pendingVisibleChatMember?.pendingAction ?? undefined,
