21d3998cf8c22493d75a05c45dd686a0433e3a1c
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-03-04 09:39:19 +0100

    apply suggested changes to arrayDifference util funciton

--- src/utils/arrayDifference.ts
+++ src/utils/arrayDifference.ts
@@ -1,4 +1,4 @@
-function arrayDifference<T = string>(array1: T[], array2: T[]): T[] {
+function arrayDifference<TItem>(array1: TItem[], array2: TItem[]): TItem[] {
     return [array1, array2].reduce((a, b) => a.filter((c) => !b.includes(c)));
 }
 
