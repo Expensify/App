15a94f9135cafe17ea1cfa98d9dd4cb7db6f139f
Jasper Huang <jasper@expensify.com>
2024-03-04 19:05:35 -0800

    allow viewing members page in Concierge DM

--- src/pages/RoomMembersPage.tsx
+++ src/pages/RoomMembersPage.tsx
@@ -226,8 +226,7 @@ function RoomMembersPage({report, session, policies}: RoomMembersPageProps) {
                     isEmptyObject(report) ||
                     !isPolicyMember ||
                     (!ReportUtils.isChatThread(report) && ReportUtils.isUserCreatedPolicyRoom(report) && !isPolicyMember) ||
-                    (!ReportUtils.isChatThread(report) && ReportUtils.isDefaultRoom(report)) ||
-                    ReportUtils.isConciergeChatReport(report)
+                    (!ReportUtils.isChatThread(report) && ReportUtils.isDefaultRoom(report))
                 }
                 subtitleKey={isEmptyObject(report) ? undefined : 'roomMembersPage.notAuthorized'}
                 onBackButtonPress={() => {
