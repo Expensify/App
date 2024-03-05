79e97823dd6fbf088670eb97893f2e39f978ceac
VickyStash <vikstash@gmail.com>
2024-03-05 15:23:30 +0100

    [TS migration] Migrate 'Picker.stories.js' story

--- src/stories/Picker.stories.tsx
+++ src/stories/Picker.stories.tsx
@@ -1,25 +1,30 @@
+import type {ComponentMeta, ComponentStory} from '@storybook/react';
 import React, {useState} from 'react';
 import Picker from '@components/Picker';
+import type {BasePickerProps} from '@components/Picker/types';
+
+type PickerStory = ComponentStory<typeof Picker<string>>;
+
+type TemplateProps = Omit<BasePickerProps<string>, 'onInputChange'>;
 
 /**
  * We use the Component Story Format for writing stories. Follow the docs here:
  *
  * https://storybook.js.org/docs/react/writing-stories/introduction#component-story-format
  */
-const story = {
+const story: ComponentMeta<typeof Picker> = {
     title: 'Components/Picker',
     component: Picker,
 };
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-function Template(args) {
+function Template(props: TemplateProps) {
     const [value, setValue] = useState('');
     return (
         <Picker
             value={value}
             onInputChange={(e) => setValue(e)}
             // eslint-disable-next-line react/jsx-props-no-spreading
-            {...args}
+            {...props}
         />
     );
 }
@@ -27,10 +32,9 @@ function Template(args) {
 // Arguments can be passed to the component by binding
 // See: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 
-const Default = Template.bind({});
+const Default: PickerStory = Template.bind({});
 Default.args = {
     label: 'Default picker',
-    name: 'Default',
     hintText: 'Default hint text',
     items: [
         {
@@ -44,10 +48,9 @@ Default.args = {
     ],
 };
 
-const PickerWithValue = Template.bind({});
+const PickerWithValue: PickerStory = Template.bind({});
 PickerWithValue.args = {
     label: 'Picker with defined value',
-    name: 'Picker with defined value',
     value: 'apple',
     hintText: 'Picker with hint text',
     items: [
@@ -62,10 +65,9 @@ PickerWithValue.args = {
     ],
 };
 
-const ErrorStory = Template.bind({});
+const ErrorStory: PickerStory = Template.bind({});
 ErrorStory.args = {
     label: 'Picker with error',
-    name: 'PickerWithError',
     hintText: 'Picker hint text',
     errorText: 'This field has an error.',
     items: [
@@ -80,10 +82,9 @@ ErrorStory.args = {
     ],
 };
 
-const Disabled = Template.bind({});
+const Disabled: PickerStory = Template.bind({});
 Disabled.args = {
     label: 'Picker disabled',
-    name: 'Disabled',
     value: 'orange',
     isDisabled: true,
     hintText: 'Picker hint text',
