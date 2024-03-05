745523198797d784c0eeed828597e9e1c65ef80e
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-03-05 13:35:19 +0100

    improve perf. of WorkspaceMoreFeaturesPage

--- src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
+++ src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-console */
 import type {StackScreenProps} from '@react-navigation/stack';
-import React, {useMemo} from 'react';
+import React from 'react';
 import {ScrollView, View} from 'react-native';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Illustrations from '@components/Icon/Illustrations';
@@ -11,176 +10,161 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import type {CentralPaneNavigatorParamList} from '@libs/Navigation/types';
 import * as Policy from '@userActions/Policy';
+import type {TranslationPaths} from '@src/languages/types';
 import type SCREENS from '@src/SCREENS';
+import type {PendingAction} from '@src/types/onyx/OnyxCommon';
+import type IconAsset from '@src/types/utils/IconAsset';
 import AdminPolicyAccessOrNotFoundWrapper from './AdminPolicyAccessOrNotFoundWrapper';
 import PaidPolicyAccessOrNotFoundWrapper from './PaidPolicyAccessOrNotFoundWrapper';
 import type {WithPolicyAndFullscreenLoadingProps} from './withPolicyAndFullscreenLoading';
 import withPolicyAndFullscreenLoading from './withPolicyAndFullscreenLoading';
-import type {ToggleSettingOptionRowProps} from './workflows/ToggleSettingsOptionRow';
 import ToggleSettingOptionRow from './workflows/ToggleSettingsOptionRow';
 
 type WorkspaceMoreFeaturesPageProps = WithPolicyAndFullscreenLoadingProps & StackScreenProps<CentralPaneNavigatorParamList, typeof SCREENS.WORKSPACE.CATEGORIES>;
+
+type Item = {
+    icon: IconAsset;
+    titleTranslationKey: TranslationPaths;
+    subtitleTranslationKey: TranslationPaths;
+    isActive: boolean;
+    action: (isEnabled: boolean) => void;
+    pendingAction: PendingAction | undefined;
+};
+
+type SectionObject = {
+    titleTranslationKey: TranslationPaths;
+    subtitleTranslationKey: TranslationPaths;
+    items: Item[];
+};
 
 function WorkspaceMoreFeaturesPage({policy, route}: WorkspaceMoreFeaturesPageProps) {
     const styles = useThemeStyles();
     const {isSmallScreenWidth} = useWindowDimensions();
     const {translate} = useLocalize();
 
-    const spendItems: ToggleSettingOptionRowProps[] = useMemo(
-        () => [
-            {
-                icon: Illustrations.Car,
-                title: translate('workspace.moreFeatures.distanceRates.title'),
-                subtitle: translate('workspace.moreFeatures.distanceRates.subtitle'),
-                isActive: policy?.areDistanceRatesEnabled ?? false,
-                pendingAction: policy?.pendingFields?.areDistanceRatesEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyDistanceRates(policy?.id ?? '', isEnabled);
-                },
+    const spendItems: Item[] = [
+        {
+            icon: Illustrations.Car,
+            titleTranslationKey: 'workspace.moreFeatures.distanceRates.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.distanceRates.subtitle',
+            isActive: policy?.areDistanceRatesEnabled ?? false,
+            pendingAction: policy?.pendingFields?.areDistanceRatesEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyDistanceRates(policy?.id ?? '', isEnabled);
             },
-            {
-                icon: Illustrations.Workflows,
-                title: translate('workspace.moreFeatures.workflows.title'),
-                subtitle: translate('workspace.moreFeatures.workflows.subtitle'),
-                isActive: policy?.areWorkflowsEnabled ?? false,
-                pendingAction: policy?.pendingFields?.areWorkflowsEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyWorkflows(policy?.id ?? '', isEnabled);
-                },
+        },
+        {
+            icon: Illustrations.Workflows,
+            titleTranslationKey: 'workspace.moreFeatures.workflows.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.workflows.subtitle',
+            isActive: policy?.areWorkflowsEnabled ?? false,
+            pendingAction: policy?.pendingFields?.areWorkflowsEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyWorkflows(policy?.id ?? '', isEnabled);
             },
-        ],
-        [
-            policy?.areDistanceRatesEnabled,
-            policy?.areWorkflowsEnabled,
-            policy?.id,
-            policy?.pendingAction,
-            policy?.pendingFields?.areDistanceRatesEnabled,
-            policy?.pendingFields?.areWorkflowsEnabled,
-            translate,
-        ],
-    );
+        },
+    ];
 
-    const organizeItems: ToggleSettingOptionRowProps[] = useMemo(
-        () => [
-            {
-                icon: Illustrations.FolderOpen,
-                title: translate('workspace.moreFeatures.categories.title'),
-                subtitle: translate('workspace.moreFeatures.categories.subtitle'),
-                isActive: policy?.areCategoriesEnabled ?? false,
-                pendingAction: policy?.pendingFields?.areCategoriesEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyCategories(policy?.id ?? '', isEnabled);
-                },
+    const organizeItems: Item[] = [
+        {
+            icon: Illustrations.FolderOpen,
+            titleTranslationKey: 'workspace.moreFeatures.categories.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.categories.subtitle',
+            isActive: policy?.areCategoriesEnabled ?? false,
+            pendingAction: policy?.pendingFields?.areCategoriesEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyCategories(policy?.id ?? '', isEnabled);
             },
-            {
-                icon: Illustrations.Tag,
-                title: translate('workspace.moreFeatures.tags.title'),
-                subtitle: translate('workspace.moreFeatures.tags.subtitle'),
-                isActive: policy?.areTagsEnabled ?? false,
-                pendingAction: policy?.pendingFields?.areTagsEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyTags(policy?.id ?? '', isEnabled);
-                },
+        },
+        {
+            icon: Illustrations.Tag,
+            titleTranslationKey: 'workspace.moreFeatures.tags.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.tags.subtitle',
+            isActive: policy?.areTagsEnabled ?? false,
+            pendingAction: policy?.pendingFields?.areTagsEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyTags(policy?.id ?? '', isEnabled);
             },
-            {
-                icon: Illustrations.Coins,
-                title: translate('workspace.moreFeatures.taxes.title'),
-                subtitle: translate('workspace.moreFeatures.taxes.subtitle'),
-                isActive: policy?.tax?.trackingEnabled ?? false,
-                pendingAction: policy?.pendingFields?.isTaxTrackingEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyTaxes(policy?.id ?? '', isEnabled);
-                },
+        },
+        {
+            icon: Illustrations.Coins,
+            titleTranslationKey: 'workspace.moreFeatures.taxes.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.taxes.subtitle',
+            isActive: policy?.tax?.trackingEnabled ?? false,
+            pendingAction: policy?.pendingFields?.isTaxTrackingEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyTaxes(policy?.id ?? '', isEnabled);
             },
-            {
-                icon: Illustrations.Pencil,
-                title: translate('workspace.moreFeatures.reportFields.title'),
-                subtitle: translate('workspace.moreFeatures.reportFields.subtitle'),
-                isActive: policy?.areReportFieldsEnabled ?? false,
-                pendingAction: policy?.pendingFields?.areReportFieldsEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyReportFields(policy?.id ?? '', isEnabled);
-                },
+        },
+        {
+            icon: Illustrations.Pencil,
+            titleTranslationKey: 'workspace.moreFeatures.reportFields.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.reportFields.subtitle',
+            isActive: policy?.areReportFieldsEnabled ?? false,
+            pendingAction: policy?.pendingFields?.areReportFieldsEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyReportFields(policy?.id ?? '', isEnabled);
             },
-        ],
-        [
-            policy?.areCategoriesEnabled,
-            policy?.areReportFieldsEnabled,
-            policy?.areTagsEnabled,
-            policy?.id,
-            policy?.pendingAction,
-            policy?.pendingFields?.areCategoriesEnabled,
-            policy?.pendingFields?.areReportFieldsEnabled,
-            policy?.pendingFields?.areTagsEnabled,
-            policy?.pendingFields?.isTaxTrackingEnabled,
-            policy?.tax?.trackingEnabled,
-            translate,
-        ],
-    );
+        },
+    ];
 
-    const integrateItems: ToggleSettingOptionRowProps[] = useMemo(
-        () => [
-            {
-                icon: Illustrations.Accounting,
-                title: translate('workspace.moreFeatures.connections.title'),
-                subtitle: translate('workspace.moreFeatures.connections.subtitle'),
-                isActive: policy?.areConnectionsEnabled ?? false,
-                pendingAction: policy?.pendingFields?.areConnectionsEnabled && policy.pendingAction,
-                onToggle: (isEnabled: boolean) => {
-                    Policy.enablePolicyConnections(policy?.id ?? '', isEnabled);
-                },
+    const integrateItems: Item[] = [
+        {
+            icon: Illustrations.Accounting,
+            titleTranslationKey: 'workspace.moreFeatures.connections.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.connections.subtitle',
+            isActive: policy?.areConnectionsEnabled ?? false,
+            pendingAction: policy?.pendingFields?.areConnectionsEnabled && policy.pendingAction,
+            action: (isEnabled: boolean) => {
+                Policy.enablePolicyConnections(policy?.id ?? '', isEnabled);
             },
-        ],
-        [policy?.areConnectionsEnabled, policy?.id, policy?.pendingAction, policy?.pendingFields?.areConnectionsEnabled, translate],
-    );
+        },
+    ];
 
-    const sections = useMemo(
-        () => [
-            {
-                title: translate('workspace.moreFeatures.spendSection.title'),
-                subtitle: translate('workspace.moreFeatures.spendSection.subtitle'),
-                items: spendItems,
-            },
-            {
-                title: translate('workspace.moreFeatures.organizeSection.title'),
-                subtitle: translate('workspace.moreFeatures.organizeSection.subtitle'),
-                items: organizeItems,
-            },
-            {
-                title: translate('workspace.moreFeatures.integrateSection.title'),
-                subtitle: translate('workspace.moreFeatures.integrateSection.subtitle'),
-                items: integrateItems,
-            },
-        ],
-        [integrateItems, organizeItems, spendItems, translate],
-    );
+    const sections: SectionObject[] = [
+        {
+            titleTranslationKey: 'workspace.moreFeatures.spendSection.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.spendSection.subtitle',
+            items: spendItems,
+        },
+        {
+            titleTranslationKey: 'workspace.moreFeatures.organizeSection.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.organizeSection.subtitle',
+            items: organizeItems,
+        },
+        {
+            titleTranslationKey: 'workspace.moreFeatures.integrateSection.title',
+            subtitleTranslationKey: 'workspace.moreFeatures.integrateSection.subtitle',
+            items: integrateItems,
+        },
+    ];
 
-    const renderItem = (item: ToggleSettingOptionRowProps) => (
+    const renderItem = (item: Item) => (
         <View
-            key={item.title}
+            key={item.titleTranslationKey}
             style={styles.mt7}
         >
             <ToggleSettingOptionRow
                 icon={item.icon}
-                title={item.title}
-                subtitle={item.subtitle}
+                title={translate(item.titleTranslationKey)}
+                subtitle={translate(item.subtitleTranslationKey)}
                 isActive={item.isActive}
                 pendingAction={item.pendingAction}
-                onToggle={item.onToggle}
+                onToggle={item.action}
             />
         </View>
     );
 
     const renderSection = (section: (typeof sections)[0]) => (
         <View
-            key={section.title}
+            key={section.titleTranslationKey}
             style={[styles.mt3, isSmallScreenWidth ? styles.workspaceSectionMobile : styles.workspaceSection]}
         >
             <Section
                 containerStyles={isSmallScreenWidth ? styles.p5 : styles.p8}
-                title={section.title}
+                title={translate(section.titleTranslationKey)}
                 titleStyles={styles.textStrong}
-                subtitle={section.subtitle}
+                subtitle={translate(section.subtitleTranslationKey)}
                 subtitleMuted
             >
                 {section.items.map(renderItem)}
