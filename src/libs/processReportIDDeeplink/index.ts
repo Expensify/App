88b215a3fb82f5df46f620369973cce09eea1c66
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-03-05 15:57:11 +0100

    add return types

--- src/libs/processReportIDDeeplink/index.ts
+++ src/libs/processReportIDDeeplink/index.ts
@@ -1,5 +1,5 @@
 import getReportIDfromUrl from './getReportIDfromUrl';
 
-export default function processReportIDDeeplink(url: string) {
+export default function processReportIDDeeplink(url: string): string {
     return getReportIDfromUrl(url);
 }
--- src/libs/processReportIDDeeplink/index.website.ts
+++ src/libs/processReportIDDeeplink/index.website.ts
@@ -1,7 +1,7 @@
 import CONST from '@src/CONST';
 import getReportIDfromUrl from './getReportIDfromUrl';
 
-export default function processReportIDDeeplink(url: string) {
+export default function processReportIDDeeplink(url: string): string {
     const prevUrl = sessionStorage.getItem(CONST.SESSION_STORAGE_KEYS.INITIAL_URL);
     const prevReportID = getReportIDfromUrl(prevUrl ?? '');
     const currentReportID = getReportIDfromUrl(url);
