051ae6a9dd6c6696b70fe22162a35b9fa342bc82
Pujan <shahpujan1992@gmail.com>
2024-03-06 21:42:08 +0530

    added math floor for seconds

--- src/components/VideoPlayer/utils.ts
+++ src/components/VideoPlayer/utils.ts
@@ -2,7 +2,7 @@
 const convertMillisecondsToTime = (milliseconds: number) => {
     const hours = Math.floor(milliseconds / 3600000);
     const minutes = Math.floor((milliseconds % 3600000) / 60000);
-    const seconds = String(((milliseconds % 60000) / 1000).toFixed(0)).padStart(2, '0');
+    const seconds = String((Math.floor(milliseconds % 60000) / 1000).toFixed(0)).padStart(2, '0');
     return hours > 0 ? `${hours}:${String(minutes).padStart(2, '0')}:${seconds}` : `${minutes}:${seconds}`;
 };
 
