2a3aec3a586f6506cbeb5b94f0d5627fb16edf65
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-03-06 14:54:25 +0100

    remove release profiler

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
  * @format
  */
 import {AppRegistry} from 'react-native';
-import {startProfiling} from 'react-native-release-profiler';
 import App from './src/App';
 import Config from './src/CONFIG';
 import additionalAppSetup from './src/setup';
-
-startProfiling();
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
 additionalAppSetup();
