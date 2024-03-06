942e2960a726560e4723b8832ac50691ca3b3e53
Nicolay Arefyeu <n.arefyev91@gmail.com>
2024-03-06 17:18:26 +0200

    prettier

--- src/libs/navigateAfterJoinRequest/index.ts
+++ src/libs/navigateAfterJoinRequest/index.ts
@@ -1,8 +1,8 @@
-import Navigation from "@navigation/Navigation";
-import ROUTES from "@src/ROUTES";
+import Navigation from '@navigation/Navigation';
+import ROUTES from '@src/ROUTES';
 
 const navigateAfterJoinRequest = () => {
     Navigation.goBack(undefined, false, true);
     Navigation.navigate(ROUTES.ALL_SETTINGS);
-}
+};
 export default navigateAfterJoinRequest;
