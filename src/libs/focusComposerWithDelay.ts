69e6022b79b61e8c82c6d98d7581ff2ec5e807f7
FitseTLT <engfitsum@gmail.com>
2024-03-07 22:43:34 +0300

    revert ref variable change

--- src/libs/focusComposerWithDelay.ts
+++ src/libs/focusComposerWithDelay.ts
@@ -6,7 +6,7 @@ type FocusComposerWithDelay = (shouldDelay?: boolean) => void;
 /**
  * Create a function that focuses the composer.
  */
-function focusComposerWithDelay(textInputRef: TextInput | null): FocusComposerWithDelay {
+function focusComposerWithDelay(textInput: TextInput | null): FocusComposerWithDelay {
     /**
      * Focus the text input
      * @param [shouldDelay] Impose delay before focusing the text input
@@ -14,7 +14,6 @@ function focusComposerWithDelay(textInputRef: TextInput | null): FocusComposerWi
     return (shouldDelay = false) => {
         // There could be other animations running while we trigger manual focus.
         // This prevents focus from making those animations janky.
-        const textInput = textInputRef;
 
         if (!textInput || EmojiPickerAction.isEmojiPickerVisible()) {
             return;
