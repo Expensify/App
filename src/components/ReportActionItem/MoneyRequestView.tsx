86014ab56158310228a9f5cd20d21ae9bb593002
Krishna Gupta <belivethatkg@gmail.com>
2024-03-07 15:25:49 +0530

    hide ReceiptAudit when scan is in progress.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -129,6 +129,7 @@ function MoneyRequestView({
     const canEditDate = ReportUtils.canEditFieldOfMoneyRequest(parentReportAction, CONST.EDIT_REQUEST_FIELD.DATE);
     const canEditReceipt = ReportUtils.canEditFieldOfMoneyRequest(parentReportAction, CONST.EDIT_REQUEST_FIELD.RECEIPT);
     const canEditDistance = ReportUtils.canEditFieldOfMoneyRequest(parentReportAction, CONST.EDIT_REQUEST_FIELD.DISTANCE);
+    const isReceiptBeingScanned = TransactionUtils.hasReceipt(transaction) && !TransactionUtils.isReceiptBeingScanned(transaction);
 
     // A flag for verifying that the current report is a sub-report of a workspace chat
     // if the policy of the report is either Collect or Control, then this report must be tied to workspace chat
@@ -287,7 +288,7 @@ function MoneyRequestView({
                         }
                     />
                 )}
-                <ReceiptAudit notes={noteTypeViolations} />
+                {!isReceiptBeingScanned && canUseViolations && <ReceiptAudit notes={noteTypeViolations} />}
                 {canUseViolations && <ViolationMessages violations={getViolationsForField('receipt')} />}
                 <OfflineWithFeedback pendingAction={getPendingFieldAction('amount')}>
                     <MenuItemWithTopDescription
