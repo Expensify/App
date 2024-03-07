f5040bfccb2ea4f808a94fc721b6116affd98401
Andrew Rosiclair <arosiclair@expensify.com>
2024-03-07 14:49:42 -0500

    clear isOptimisticReport on success

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -592,6 +592,7 @@ function buildOnyxDataForMoneyRequest(
             value: {
                 pendingFields: null,
                 errorFields: null,
+                isOptimisticReport: false,
             },
         });
     }
