bb77b15c4833a6185d0b0c279ef45402b4360f66
smelaa <aleksandra.smela@gmail.com>
2024-03-07 14:04:36 +0100

    Add beta visibility

--- src/pages/Travel/MyTripsPage.tsx
+++ src/pages/Travel/MyTripsPage.tsx
@@ -1,20 +1,29 @@
 import type {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
+import {withOnyx} from 'react-native-onyx';
+import type {OnyxEntry} from 'react-native-onyx';
+import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Illustrations from '@components/Icon/Illustrations';
 import ScreenWrapper from '@components/ScreenWrapper';
 import useLocalize from '@hooks/useLocalize';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import Navigation from '@libs/Navigation/Navigation';
+import Permissions from '@libs/Permissions';
 import type {CentralPaneNavigatorParamList} from '@navigation/types';
+import ONYXKEYS from '@src/ONYXKEYS';
 import type SCREENS from '@src/SCREENS';
+import type {Beta} from '@src/types/onyx';
 import ManageTrips from './ManageTrips';
 
-type MyTripsPageProps = StackScreenProps<CentralPaneNavigatorParamList, typeof SCREENS.TRAVEL.MY_TRIPS>;
+type MyTripsPageOnyxProps = {betas: OnyxEntry<Beta[]>};
 
-function MyTripsPage({route}: MyTripsPageProps) {
+type MyTripsPageProps = StackScreenProps<CentralPaneNavigatorParamList, typeof SCREENS.TRAVEL.MY_TRIPS> & MyTripsPageOnyxProps;
+
+function MyTripsPage({betas}: MyTripsPageProps) {
     const {translate} = useLocalize();
     const {isSmallScreenWidth} = useWindowDimensions();
+    const canSeeTravelPage = Permissions.canSeeTravelPage(betas);
 
     return (
         <ScreenWrapper
@@ -30,11 +39,15 @@ function MyTripsPage({route}: MyTripsPageProps) {
                 shouldShowBackButton={isSmallScreenWidth}
                 onBackButtonPress={() => Navigation.goBack()}
             />
-            <ManageTrips />
+            {canSeeTravelPage ? <ManageTrips /> : <FullPageNotFoundView />}
         </ScreenWrapper>
     );
 }
 
 MyTripsPage.displayName = 'MyTripsPage';
 
-export default MyTripsPage;
+export default withOnyx<MyTripsPageProps, MyTripsPageOnyxProps>({
+    betas: {
+        key: ONYXKEYS.BETAS,
+    },
+})(MyTripsPage);
