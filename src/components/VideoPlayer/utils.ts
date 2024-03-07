3ce99c5e76f1daf7261d9ed970d5bb0e5dc9782a
Pujan <shahpujan1992@gmail.com>
2024-03-07 18:07:45 +0530

    fixed time conversion calculation

--- src/components/VideoPlayer/utils.ts
+++ src/components/VideoPlayer/utils.ts
@@ -1,8 +1,8 @@
 // Converts milliseconds to '[hours:]minutes:seconds' format
 const convertMillisecondsToTime = (milliseconds: number) => {
     const hours = Math.floor(milliseconds / 3600000);
-    const minutes = Math.floor((milliseconds % 3600000) / 60000);
-    const seconds = String((Math.floor(milliseconds % 60000) / 1000).toFixed(0)).padStart(2, '0');
+    const minutes = Math.floor((milliseconds / 60000) % 60);
+    const seconds = Math.floor((milliseconds / 1000) % 60).toFixed(0).padStart(2, '0');
     return hours > 0 ? `${hours}:${String(minutes).padStart(2, '0')}:${seconds}` : `${minutes}:${seconds}`;
 };
 
