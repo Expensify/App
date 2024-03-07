c0725644186075e13f7b23fec4abec420e17d1b3
tienifr <christianwen18@gmail.com>
2024-03-07 16:39:59 +0700

    refactor code

--- src/libs/actions/ReportActions.ts
+++ src/libs/actions/ReportActions.ts
@@ -8,36 +8,6 @@ import * as Report from './Report';
 
 type IgnoreDirection = 'parent' | 'child';
 
-/**
- * 
-ignore: undefined means we want to check both parent and children report actions
-ignore: parent | child means we want to ignore check parent | child report actions because we already check them before
- */
-function clearAllRelatedReportActionErrors(reportID: string, reportAction: ReportAction | null, ignore?: IgnoreDirection, keys?: string[]) {
-    const errorKeys = keys || Object.keys(reportAction?.errors ?? {});
-    if (!reportAction || errorKeys.length === 0) {
-        return;
-    }
-
-    clearReportActionErrors(reportID, reportAction, keys);
-
-    const report = ReportUtils.getReport(reportID);
-    if (report?.parentReportID && report?.parentReportActionID && ignore !== 'parent') {
-        const parentReportAction = ReportActionUtils.getReportAction(report.parentReportID, report.parentReportActionID);
-        const parentErrorKeys = Object.keys(parentReportAction?.errors ?? {}).filter((err) => errorKeys.includes(err));
-
-        clearAllRelatedReportActionErrors(report.parentReportID, parentReportAction, 'child', parentErrorKeys);
-    }
-
-    if (reportAction.childReportID && ignore !== 'child') {
-        const childActions = ReportActionUtils.getAllReportActions(reportAction.childReportID);
-        Object.values(childActions).forEach((action) => {
-            const childErrorKeys = Object.keys(action.errors ?? {}).filter((err) => errorKeys.includes(err));
-            clearAllRelatedReportActionErrors(reportAction.childReportID ?? '', action, 'parent', childErrorKeys);
-        });
-    }
-}
-
 function clearReportActionErrors(reportID: string, reportAction: ReportAction, keys?: string[]) {
     const originalReportID = ReportUtils.getOriginalReportID(reportID, reportAction);
 
@@ -86,8 +56,34 @@ function clearReportActionErrors(reportID: string, reportAction: ReportAction, k
     });
 }
 
-export {
-    // eslint-disable-next-line import/prefer-default-export
-    clearReportActionErrors,
-    clearAllRelatedReportActionErrors,
-};
+/**
+ * 
+ignore: undefined means we want to check both parent and children report actions
+ignore: parent or child means we want to ignore check parent or child report actions because we already check them before
+ */
+function clearAllRelatedReportActionErrors(reportID: string, reportAction: ReportAction | null, ignore?: IgnoreDirection, keys?: string[]) {
+    const errorKeys = keys ?? Object.keys(reportAction?.errors ?? {});
+    if (!reportAction || errorKeys.length === 0) {
+        return;
+    }
+
+    clearReportActionErrors(reportID, reportAction, keys);
+
+    const report = ReportUtils.getReport(reportID);
+    if (report?.parentReportID && report?.parentReportActionID && ignore !== 'parent') {
+        const parentReportAction = ReportActionUtils.getReportAction(report.parentReportID, report.parentReportActionID);
+        const parentErrorKeys = Object.keys(parentReportAction?.errors ?? {}).filter((err) => errorKeys.includes(err));
+
+        clearAllRelatedReportActionErrors(report.parentReportID, parentReportAction, 'child', parentErrorKeys);
+    }
+
+    if (reportAction.childReportID && ignore !== 'child') {
+        const childActions = ReportActionUtils.getAllReportActions(reportAction.childReportID);
+        Object.values(childActions).forEach((action) => {
+            const childErrorKeys = Object.keys(action.errors ?? {}).filter((err) => errorKeys.includes(err));
+            clearAllRelatedReportActionErrors(reportAction.childReportID ?? '', action, 'parent', childErrorKeys);
+        });
+    }
+}
+
+export {clearReportActionErrors, clearAllRelatedReportActionErrors};
