800c99cd40ccd9352cf193bfe8afe1c5f908e2b0
dukenv0307 <dukenv0307@gmail.com>
2024-03-07 17:02:44 +0700

    fix lint

--- src/pages/workspace/WorkspaceProfilePage.tsx
+++ src/pages/workspace/WorkspaceProfilePage.tsx
@@ -106,12 +106,7 @@ function WorkspaceProfilePage({policy, currencyList = {}, route}: WorkSpaceProfi
                                 DefaultAvatar={DefaultAvatar}
                                 type={CONST.ICON_TYPE_WORKSPACE}
                                 fallbackIcon={Expensicons.FallbackWorkspaceAvatar}
-                                style={[
-                                    styles.mb1,
-                                    isSmallScreenWidth ? styles.mtn17 : styles.mtn20,
-                                    styles.alignItemsStart,
-                                    styles.sectionMenuItemTopDescription,
-                                ]}
+                                style={[styles.mb1, isSmallScreenWidth ? styles.mtn17 : styles.mtn20, styles.alignItemsStart, styles.sectionMenuItemTopDescription]}
                                 isUsingDefaultAvatar={!policy?.avatar ?? null}
                                 onImageSelected={(file) => Policy.updateWorkspaceAvatar(policy?.id ?? '', file as File)}
                                 onImageRemoved={() => Policy.deleteWorkspaceAvatar(policy?.id ?? '')}
