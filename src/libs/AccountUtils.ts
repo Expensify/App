886123aec8a05ce752bf7a8be97638135d51fafa
tienifr <christianwen18@gmail.com>
2024-03-07 18:51:59 +0700

    conver undefined to boolean

--- src/libs/AccountUtils.ts
+++ src/libs/AccountUtils.ts
@@ -3,6 +3,6 @@ import CONST from '@src/CONST';
 import type {Account} from '@src/types/onyx';
 
 const isValidateCodeFormSubmitting = (account: OnyxEntry<Account>) =>
-    account?.isLoading && account.loadingForm === (account.requiresTwoFactorAuth ? CONST.FORMS.VALIDATE_TFA_CODE_FORM : CONST.FORMS.VALIDATE_CODE_FORM);
+    !!account?.isLoading && account.loadingForm === (account.requiresTwoFactorAuth ? CONST.FORMS.VALIDATE_TFA_CODE_FORM : CONST.FORMS.VALIDATE_CODE_FORM);
 
 export default {isValidateCodeFormSubmitting};
