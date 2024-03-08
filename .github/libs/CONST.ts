9cf90e2400a8e8c02bf9ee70705ee72938d2894b
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-03-08 16:39:49 +0100

    fix: tests

--- .github/libs/CONST.ts
+++ .github/libs/CONST.ts
@@ -29,4 +29,4 @@ const CONST: Const = {
 CONST.APP_REPO_URL = `https://github.com/${CONST.GITHUB_OWNER}/${CONST.APP_REPO}`;
 CONST.APP_REPO_GIT_URL = `git@github.com:${CONST.GITHUB_OWNER}/${CONST.APP_REPO}.git`;
 
-export default CONST;
+module.exports = CONST;
