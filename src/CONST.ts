838b20d03f1ca5450db01f11b10f03c0793bce7f
Sibtain Ali <allroundexperts@gmail.com>
2024-03-09 01:21:45 +0500

    feat: rename hubs

--- src/CONST.ts
+++ src/CONST.ts
@@ -3167,7 +3167,7 @@ const CONST = {
             SHARE_CODE: 'shareCode',
         },
         REVENUE: 250,
-        LEARN_MORE_LINK: 'https://help.expensify.com/articles/new-expensify/get-paid-back/Referral-Program',
+        LEARN_MORE_LINK: 'https://help.expensify.com/articles/new-expensify/expenses/Referral-Program',
         LINK: 'https://join.my.expensify.com',
     },
 
