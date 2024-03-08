8bffd0db79a7f2ec688257702b363ceb88b1eb8c
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-03-08 14:08:46 +0100

    ref: move sanitizeStringForJSONParse to TS

--- .github/libs/sanitizeStringForJSONParse.ts
+++ .github/libs/sanitizeStringForJSONParse.ts
@@ -1,4 +1,5 @@
-const replacer = (str) =>
+/* eslint-disable @typescript-eslint/naming-convention */
+const replacer = (str: string): string =>
     ({
         '\\': '\\\\',
         '\t': '\\t',
@@ -6,22 +7,19 @@ const replacer = (str) =>
         '\r': '\\r',
         '\f': '\\f',
         '"': '\\"',
-    }[str]);
+    }[str] ?? '');
 
 /**
  * Replace any characters in the string that will break JSON.parse for our Git Log output
  *
  * Solution partly taken from SO user Gabriel Rodríguez Flores 🙇
  * https://stackoverflow.com/questions/52789718/how-to-remove-special-characters-before-json-parse-while-file-reading
- *
- * @param {String} inputString
- * @returns {String}
  */
-module.exports = function (inputString) {
+export default function (inputString: string) {
     if (typeof inputString !== 'string') {
         throw new TypeError('Input must me of type String');
     }
 
     // Replace any newlines and escape backslashes
     return inputString.replace(/\\|\t|\n|\r|\f|"/g, replacer);
-};
+}
