4685b5a2933323f7a2f351185c22252deea32c55
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-03-08 17:57:03 +0100

    integrate getIsPad into getIsNarrowLayout

--- src/libs/getIsNarrowLayout/index.native.ts
+++ src/libs/getIsNarrowLayout/index.native.ts
@@ -1,3 +1,10 @@
+import getIsPad from '@libs/getIsPad';
+import getIsSmallScreenWidth from '@libs/getIsSmallScreenWidth';
+
 export default function getIsNarrowLayout() {
+    if (getIsPad()) {
+        return getIsSmallScreenWidth();
+    }
+
     return true;
 }
