8cc0c0349afda7772ca5b4db26874fce9fce4aa7
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-03-11 11:41:20 +0100

    fix: resolve comment

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -480,7 +480,7 @@ function getSearchText(
 /**
  * Get an object of error messages keyed by microtime by combining all error objects related to the report.
  */
-function getAllReportErrors(report: OnyxEntry<Report>, reportActions: OnyxEntry<ReportActions> | undefined, transactions: OnyxCollection<Transaction> = allTransactions): OnyxCommon.Errors {
+function getAllReportErrors(report: OnyxEntry<Report>, reportActions: OnyxEntry<ReportActions>, transactions: OnyxCollection<Transaction> = allTransactions): OnyxCommon.Errors {
     const reportErrors = report?.errors ?? {};
     const reportErrorFields = report?.errorFields ?? {};
     const reportActionErrors: OnyxCommon.ErrorFields = Object.values(reportActions ?? {}).reduce(
