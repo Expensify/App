dc35055073b34f237c9d4d6a3c589bf53ac8f892
Yogesh Bhatt <65986357+ygshbht@users.noreply.github.com>
2024-03-12 03:11:46 +0530

    Example update src/hooks/useGeographicalStateFromRoute.ts
    Co-authored-by: Vit Horacek <36083550+mountiny@users.noreply.github.com>

--- src/hooks/useGeographicalStateFromRoute.ts
+++ src/hooks/useGeographicalStateFromRoute.ts
@@ -12,10 +12,10 @@ const removeHash = (arg: string): string => arg.replace(/-hash-.*$/, '');
 
 /**
  * Extracts the 'state' (default) query parameter from the route/ url and validates it against COMMON_CONST.STATES, returning its ISO code or `undefined`.
- * Example 1: Url: https://dev.new.expensify.com:8082/settings/profile/address?state=MO Returns: MO
- * Example 2: Url: https://dev.new.expensify.com:8082/settings/profile/address?state=ASDF Returns: undefined
- * Example 3: Url: https://dev.new.expensify.com:8082/settings/profile/address Returns: undefined
- * Example 4: Url: https://dev.new.expensify.com:8082/settings/profile/address?state=MO-hash-a12341 Returns: MO
+ * Example 1: Url: https://new.expensify.com/settings/profile/address?state=MO Returns: MO
+ * Example 2: Url: https://new.expensify.com/settings/profile/address?state=ASDF Returns: undefined
+ * Example 3: Url: https://new.expensify.com/settings/profile/address Returns: undefined
+ * Example 4: Url: https://new.expensify.com/settings/profile/address?state=MO-hash-a12341 Returns: MO
  */
 export default function useGeographicalStateFromRoute(stateParamName = 'state'): State | undefined {
     const route = useRoute<RouteProp<CustomParamList, string>>();
