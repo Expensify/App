9c99afc6e9ee50c945a8b63d8588d85773bf81e0
John Lee <john@expensify.com>
2024-03-12 14:43:59 -0400

    Pass x-accountID' in requests

--- src/libs/HttpUtils.ts
+++ src/libs/HttpUtils.ts
@@ -42,20 +42,36 @@ const addSkewList: string[] = [SIDE_EFFECT_REQUEST_COMMANDS.OPEN_REPORT, SIDE_EF
 const APICommandRegex = /\/api\/([^&?]+)\??.*/;
 
 /**
+ * The accountID header value
+ */
+let accountIDHeaderValue = '';
+
+/**
  * Send an HTTP request, and attempt to resolve the json response.
  * If there is a network error, we'll set the application offline.
  */
 function processHTTPRequest(url: string, method: RequestType = 'get', body: FormData | null = null, canCancel = true): Promise<Response> {
     const startTime = new Date().valueOf();
+
+    const requestHeaders: HeadersInit = new Headers();
+    if (accountIDHeaderValue) {
+        requestHeaders.set('x-accountid', accountIDHeaderValue);
+    }
+
     return fetch(url, {
         // We hook requests to the same Controller signal, so we can cancel them all at once
         signal: canCancel ? cancellationController.signal : undefined,
         method,
         body,
+        headers: requestHeaders,
         // We want to include the cookie accountID that is returned form the API
         credentials: 'include',
     })
         .then((response) => {
+            if (response.headers.get('x-accountid') ?? false) {
+                accountIDHeaderValue = response.headers.get('x-accountid') ?? '';
+            }
+
             // We are calculating the skew to minimize the delay when posting the messages
             const match = url.match(APICommandRegex)?.[1];
             if (match && addSkewList.includes(match) && response.headers) {
