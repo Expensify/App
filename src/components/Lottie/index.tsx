365903d70b49f80d959c8c95095cc7b86bf4749c
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-03-12 16:43:20 +0200

    removed isFocused

--- src/components/Lottie/index.tsx
+++ src/components/Lottie/index.tsx
@@ -1,4 +1,3 @@
-import {useIsFocused} from '@react-navigation/native';
 import type {LottieViewProps} from 'lottie-react-native';
 import LottieView from 'lottie-react-native';
 import type {ForwardedRef} from 'react';
@@ -14,20 +13,18 @@ type Props = {
 } & Omit<LottieViewProps, 'source'>;
 
 function Lottie({source, webStyle, ...props}: Props, ref: ForwardedRef<LottieView>) {
+    const appState = useAppState();
     const styles = useThemeStyles();
     const [isError, setIsError] = React.useState(false);
-
-    const appState = useAppState();
-    const isFocused = useIsFocused();
 
     useNetwork({onReconnect: () => setIsError(false)});
 
     const aspectRatioStyle = styles.aspectRatioLottie(source);
 
     // If the image fails to load or app is in background state, we'll just render an empty view
-    // using the fallback in case of a Lottie error or (isFocused || isBackground) to prevent
-    // memory leaks, see issue: https://github.com/Expensify/App/issues/36645
-    if (isError || !isFocused || appState.isBackground) {
+    // using the fallback in case of a Lottie error or appState.isBackground to prevent
+    // memory leak, see issue: https://github.com/Expensify/App/issues/36645
+    if (isError || appState.isBackground) {
         return <View style={aspectRatioStyle} />;
     }
 
