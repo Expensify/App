1a13efea1464aea769ec47b841a4c35295571d7c
Nicolay Arefyeu <n.arefyev91@gmail.com>
2024-03-12 09:42:57 +0200

    update tags page

--- src/pages/workspace/tags/WorkspaceTagsPage.tsx
+++ src/pages/workspace/tags/WorkspaceTagsPage.tsx
@@ -123,7 +123,6 @@ function WorkspaceTagsPage({policyTags, route}: WorkspaceTagsPageProps) {
                 medium
                 onPress={navigateToTagsSettings}
                 icon={Expensicons.Gear}
-                iconStyles={[styles.mr2]}
                 text={translate('common.settings')}
                 style={[isSmallScreenWidth && styles.w50]}
             />
