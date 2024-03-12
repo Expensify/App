c4ee5d30a9e33921e745697fb96ffae4832d44ff
Jasper Huang <jasper@expensify.com>
2024-03-12 09:58:20 -0700

    Update ReportDetailsPage.tsx
    Co-authored-by: Aimane Chnaif <96077027+aimane-chnaif@users.noreply.github.com>

--- src/pages/ReportDetailsPage.tsx
+++ src/pages/ReportDetailsPage.tsx
@@ -179,7 +179,7 @@ function ReportDetailsPage({policies, report, session, personalDetails}: ReportD
         }
 
         return items;
-    }, [isArchivedRoom, participants.length, isChatThread, isMoneyRequestReport, report, isGroupDMChat, isPolicyMember, isUserCreatedPolicyRoom, session, isSelfDM, isChatThread, isDefaultRoom]);
+    }, [isArchivedRoom, participants.length, isChatThread, isMoneyRequestReport, report, isGroupDMChat, isPolicyMember, isUserCreatedPolicyRoom, session, isSelfDM, isDefaultRoom]);
 
     const displayNamesWithTooltips = useMemo(() => {
         const hasMultipleParticipants = participants.length > 1;
