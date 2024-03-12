c512812cc2f93dbdc4a02336c8f0191086aeb5bc
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-03-12 22:43:33 +0200

    added return AppStateType to useAppState web implementation

--- src/hooks/useAppState/index.ts
+++ src/hooks/useAppState/index.ts
@@ -1,4 +1,8 @@
-export default function useAppState() {
+import type AppStateType from './types';
+
+function useAppState(): AppStateType {
     // Since there's no AppState in web, we'll always return isForeground as true
     return {isForeground: true, isInactive: false, isBackground: false};
 }
+
+export default useAppState;
