05b73c6c7458af65296defda31ef007d13e67918
dukenv0307 <dukenv0307@gmail.com>
2024-03-13 16:16:22 +0700

    fix the case emoji inside codeblock

--- src/components/InlineCodeBlock/index.native.tsx
+++ src/components/InlineCodeBlock/index.native.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import useThemeStyles from '@hooks/useThemeStyles';
+import removeEmojiTag from './removeEmojiTag';
 import type InlineCodeBlockProps from './types';
 import type {TTextOrTPhrasing} from './types';
 import WrappedText from './WrappedText';
 
 function InlineCodeBlock<TComponent extends TTextOrTPhrasing>({TDefaultRenderer, defaultRendererProps, textStyle, boxModelStyle}: InlineCodeBlockProps<TComponent>) {
     const styles = useThemeStyles();
-
+    const data = removeEmojiTag(defaultRendererProps);
+    
     return (
         <TDefaultRenderer
             // eslint-disable-next-line react/jsx-props-no-spreading
