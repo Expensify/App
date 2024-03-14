a44a06460707f5d61950b1d3f491ac15b1944c4f
Michał Skałka <michal.skalka@swmansion.com>
2024-03-14 14:23:09 +0100

    Move skip tag adding to video utils

--- src/components/VideoPlayer/BaseVideoPlayer.js
+++ src/components/VideoPlayer/BaseVideoPlayer.js
@@ -15,6 +15,7 @@ import * as DeviceCapabilities from '@libs/DeviceCapabilities';
 import CONST from '@src/CONST';
 import {videoPlayerDefaultProps, videoPlayerPropTypes} from './propTypes';
 import shouldReplayVideo from './shouldReplayVideo';
+import * as VideoUtils from './utils';
 import VideoPlayerControls from './VideoPlayerControls';
 
 const isMobileSafari = Browser.isMobileSafari();
@@ -50,7 +51,7 @@ function BaseVideoPlayer({
     const [isLoading, setIsLoading] = useState(true);
     const [isBuffering, setIsBuffering] = useState(true);
     // we add "#t=0.001" at the end of the URL to skip first milisecond of the video and always be able to show proper video preview when video is paused at the beginning
-    const [sourceURL] = useState(`${url.includes('blob:') || url.includes('file:///') ? url : addEncryptedAuthTokenToURL(url)}#t=0.001`);
+    const [sourceURL] = useState(VideoUtils.addSkipTimeTagToURL(url.includes('blob:') || url.includes('file:///') ? url : addEncryptedAuthTokenToURL(url), 0.001));
     const [isPopoverVisible, setIsPopoverVisible] = useState(false);
     const [popoverAnchorPosition, setPopoverAnchorPosition] = useState({horizontal: 0, vertical: 0});
     const videoPlayerRef = useRef(null);
