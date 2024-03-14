f77b6eb5c96074b49eedf2abbe024dbdc05da85d
Shridhar Goel <35566748+ShridharGoel@users.noreply.github.com>
2024-03-14 22:52:24 +0530

    Add policyMembers.ts in utils

---
+++ tests/utils/collections/policyMembers.ts
@@ -0,0 +1,9 @@
+import {randWord} from '@ngneat/falso';
+import PolicyMember from '@src/types/onyx/PolicyMember';
+
+export default function createRandomPolicyMember(): PolicyMember {
+    return {
+        role: randWord(),
+        errors: {},
+    };
+}
