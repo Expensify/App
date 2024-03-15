669b49063eac5f67623aa76cd07403692cc3dc11
Arkadiusz Chrabąszczewski <Arkadiuszchr@gmail.com>
2024-03-15 16:41:19 +0100

    fix: routing for edit category

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -570,8 +570,8 @@ const ROUTES = {
         getRoute: (policyID: string) => `settings/workspaces/${policyID}/categories/new` as const,
     },
     WORKSPACE_CATEGORY_EDIT: {
-        route: 'workspace/:policyID/categories/:categoryName/edit',
-        getRoute: (policyID: string, categoryName: string) => `workspace/${policyID}/categories/${encodeURI(categoryName)}/edit` as const,
+        route: 'settings/workspaces/:policyID/categories/:categoryName/edit',
+        getRoute: (policyID: string, categoryName: string) => `settings/workspaces/${policyID}/categories/${encodeURI(categoryName)}/edit` as const,
     },
     WORKSPACE_TAGS: {
         route: 'settings/workspaces/:policyID/tags',
