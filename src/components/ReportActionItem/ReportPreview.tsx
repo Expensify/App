cdf5dbafd7f1540a9ebbbb8f1375813e26034809
rayane-djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-03-15 23:50:47 +0100

    fix lint error

--- src/components/ReportActionItem/ReportPreview.tsx
+++ src/components/ReportActionItem/ReportPreview.tsx
@@ -133,6 +133,7 @@ function ReportPreview({
 
     const hasReceipts = transactionsWithReceipts.length > 0;
     const isScanning = hasReceipts && areAllRequestsBeingSmartScanned;
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
     const hasErrors = hasMissingSmartscanFields || (canUseViolations && ReportUtils.hasViolations(iouReportID, transactionViolations)) || ReportUtils.hasActionsWithErrors(iouReportID);
     const lastThreeTransactionsWithReceipts = transactionsWithReceipts.slice(-3);
     const lastThreeReceipts = lastThreeTransactionsWithReceipts.map((transaction) => ReceiptUtils.getThumbnailAndImageURIs(transaction));
