ffa510b2947b71e569dc58c142a3ea4377ad055e
kirillzyusko <kiryl@margelo.io>
2024-03-15 12:54:40 +0100

    fix: incorrect automatic types conversion

--- src/libs/Navigation/AppNavigator/leftModalNavigatorOptions/index.ts
+++ src/libs/Navigation/AppNavigator/leftModalNavigatorOptions/index.ts
@@ -1,4 +1,6 @@
-const leftModalNavigatorOptions = {
+import type {StackNavigationOptions} from '@react-navigation/stack';
+
+const leftModalNavigatorOptions: StackNavigationOptions = {
     presentation: 'transparentModal',
     gestureDirection: 'horizontal-inverted',
 };
