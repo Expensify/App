9a4429365c3f2ef59f9e778d267d215d097afc0f
Wildan M <muhliswildan@gmail.com>
2024-03-16 09:39:16 +0700

    fix on event args usage

--- desktop/main.ts
+++ desktop/main.ts
@@ -14,6 +14,7 @@ import type {TranslationPaths} from '@src/languages/types';
 import type PlatformSpecificUpdater from '@src/setup/platformSetup/types';
 import type {Locale} from '@src/types/onyx';
 import ELECTRON_EVENTS from './ELECTRON_EVENTS';
+
 const createDownloadQueue = require('./createDownloadQueue').default;
 
 const port = process.env.PORT ?? 8082;
--- src/libs/fileDownload/index.desktop.ts
+++ src/libs/fileDownload/index.desktop.ts
@@ -13,10 +13,17 @@ const fileDownload: FileDownload = (url, fileName) => {
     window.electron.send(ELECTRON_EVENTS.DOWNLOAD, {url, options});
 
     return new Promise((resolve) => {
-        window.electron.on(ELECTRON_EVENTS.DOWNLOAD_STARTED, (args) => {
-            if (args.url !== url) {
+        window.electron.on(ELECTRON_EVENTS.DOWNLOAD_STARTED, (...args: unknown[]) => {
+            if (!Array.isArray(args) || args[0] === null || typeof args[0] !== 'object' || !('url' in args[0])) {
                 return;
             }
+
+            const {url: eventUrl} = args[0] as {url: string};
+
+            if (eventUrl !== url) {
+                return;
+            }
+
             resolve();
         });
     });
