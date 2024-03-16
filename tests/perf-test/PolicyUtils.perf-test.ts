b6343bcf591b78a225058abd10fa1137a8e3f774
ShridharGoel <35566748+ShridharGoel@users.noreply.github.com>
2024-03-16 19:44:39 +0530

    Add performance tests for getMemberAccountIDsForWorkspace

--- tests/perf-test/PolicyUtils.perf-test.ts
+++ tests/perf-test/PolicyUtils.perf-test.ts
@@ -5,60 +5,62 @@ import createCollection from '../utils/collections/createCollection';
 import createPersonalDetails from '../utils/collections/personalDetails';
 import createRandomPolicyMember from '../utils/collections/policyMembers';
 
-describe('[PolicyUtils] Performance tests for getMemberAccountIDsForWorkspace', () => {
-    test('With multiple members with personal details and policy members', async () => {
-        const policyMembers = createCollection<PolicyMember>(
-            (_, index) => `policyMembers_${index}`,
-            () => createRandomPolicyMember(),
-        );
-        const personalDetails = createCollection<PersonalDetails>((_, index) => `personalDetails_${index}`, createPersonalDetails);
+describe('PolicyUtils', () => {
+    describe('getMemberAccountIDsForWorkspace', () => {
+        test('500 policy members and full personal details', async () => {
+            const policyMembers = createCollection<PolicyMember>(
+                (_, index) => index,
+                () => createRandomPolicyMember(),
+            );
+            const personalDetails = createCollection<PersonalDetails>((_, index) => index, createPersonalDetails);
 
-        await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
-    });
+            await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
+        });
 
-    test('With multiple members with empty personal details and with errors in policy members', async () => {
-        const policyMembers = createCollection<PolicyMember>(
-            (_, index) => `policyMembers_${index}`,
-            () => ({
-                ...createRandomPolicyMember(),
-                errors: {error: 'Error message'},
-            }),
-        );
-        const personalDetails = createCollection<PersonalDetails>(
-            (_, index) => `personalDetails_${index}`,
-            (index) => ({
-                accountID: index,
-            }),
-        );
+        test('500 policy members and empty personal details', async () => {
+            const policyMembers = createCollection<PolicyMember>(
+                (_, index) => index,
+                () => createRandomPolicyMember(),
+            );
+            const personalDetails = createCollection<PersonalDetails>(
+                (_, index) => index,
+                (index) => ({
+                    accountID: index,
+                }),
+            );
 
-        await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
-    });
+            await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
+        });
 
-    test('With multiple members with personal details and with errors in policy members', async () => {
-        const policyMembers = createCollection<PolicyMember>(
-            (_, index) => `policyMembers_${index}`,
-            () => ({
-                ...createRandomPolicyMember(),
-                errors: {error: 'Error message'},
-            }),
-        );
-        const personalDetails = createCollection<PersonalDetails>((_, index) => `personalDetails_${index}`, createPersonalDetails);
+        test('500 policy members with errors and full personal details', async () => {
+            const policyMembers = createCollection<PolicyMember>(
+                (_, index) => index,
+                () => ({
+                    ...createRandomPolicyMember(),
+                    errors: {error: 'Error message'},
+                }),
+            );
+            const personalDetails = createCollection<PersonalDetails>((_, index) => index, createPersonalDetails);
 
-        await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
-    });
+            await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
+        });
 
-    test('With multiple members with empty personal details and with policy members', async () => {
-        const policyMembers = createCollection<PolicyMember>(
-            (_, index) => `policyMembers_${index}`,
-            () => createRandomPolicyMember(),
-        );
-        const personalDetails = createCollection<PersonalDetails>(
-            (_, index) => `personalDetails_${index}`,
-            (index) => ({
-                accountID: index,
-            }),
-        );
+        test('500 policy members with errors and empty personal details', async () => {
+            const policyMembers = createCollection<PolicyMember>(
+                (_, index) => index,
+                () => ({
+                    ...createRandomPolicyMember(),
+                    errors: {error: 'Error message'},
+                }),
+            );
+            const personalDetails = createCollection<PersonalDetails>(
+                (_, index) => index,
+                (index) => ({
+                    accountID: index,
+                }),
+            );
 
-        await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
+            await measureFunction(() => getMemberAccountIDsForWorkspace(policyMembers, personalDetails));
+        });
     });
 });
