c8b2b17174e1d817e535abf3767d2ff1efefe4bb
Filip Solecki <filip.solecki@swmansion.com>
2024-03-18 16:37:37 +0100

    Fix failing TS

--- src/CONST.ts
+++ src/CONST.ts
@@ -4068,10 +4068,6 @@ const CONST = {
     SESSION_STORAGE_KEYS: {
         INITIAL_URL: 'INITIAL_URL',
     },
-
-    TAX_RATES: {
-        NAME_MAX_LENGTH: 50,
-    },
 } as const;
 
 type Country = keyof typeof CONST.ALL_COUNTRIES;
