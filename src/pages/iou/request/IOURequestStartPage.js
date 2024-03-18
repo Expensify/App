d63b371f17f42bbe6cefe00d52afda75cb2e4d56
Shubham Agrawal <shubham.agra1206@gmail.com>
2024-03-18 09:54:53 +0530

    Disabled distance track expense as it is not implemented yet

--- src/pages/iou/request/IOURequestStartPage.js
+++ src/pages/iou/request/IOURequestStartPage.js
@@ -110,7 +110,7 @@ function IOURequestStartPage({
 
     const isExpenseChat = ReportUtils.isPolicyExpenseChat(report);
     const isExpenseReport = ReportUtils.isExpenseReport(report);
-    const shouldDisplayDistanceRequest = canUseP2PDistanceRequests || isExpenseChat || isExpenseReport || isFromGlobalCreate;
+    const shouldDisplayDistanceRequest = iouType !== CONST.IOU.TYPE.TRACK_EXPENSE && (canUseP2PDistanceRequests || isExpenseChat || isExpenseReport || isFromGlobalCreate);
 
     // Allow the user to create the request if we are creating the request in global menu or the report can create the request
     const isAllowedToCreateRequest = _.isEmpty(report.reportID) || ReportUtils.canCreateRequest(report, policy, iouType);
