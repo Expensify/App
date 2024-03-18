cfc67146623d405ca9f253539b335047b15fd954
Shubham Agrawal <shubham.agra1206@gmail.com>
2024-03-18 17:19:51 +0530

    Removed unecessary checkmark from RadioListItem

--- src/components/SelectionList/RadioListItem.tsx
+++ src/components/SelectionList/RadioListItem.tsx
@@ -1,13 +1,7 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import {View} from 'react-native';
-import Icon from '@components/Icon';
-import * as Expensicons from '@components/Icon/Expensicons';
-import PressableWithFeedback from '@components/Pressable/PressableWithFeedback';
 import TextWithTooltip from '@components/TextWithTooltip';
-import useStyleUtils from '@hooks/useStyleUtils';
-import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
-import CONST from '@src/CONST';
 import BaseListItem from './BaseListItem';
 import type {RadioListItemProps} from './types';
 
@@ -16,25 +10,13 @@ function RadioListItem({
     isFocused,
     showTooltip,
     isDisabled,
-    canSelectMultiple,
     onSelectRow,
-    onCheckboxPress,
     onDismissError,
     shouldPreventDefaultFocusOnSelectRow,
     rightHandSideComponent,
     isMultilineSupported = false,
 }: RadioListItemProps) {
     const styles = useThemeStyles();
-    const theme = useTheme();
-    const StyleUtils = useStyleUtils();
-
-    const handleCheckboxPress = useCallback(() => {
-        if (onCheckboxPress) {
-            onCheckboxPress(item);
-        } else {
-            onSelectRow(item);
-        }
-    }, [item, onCheckboxPress, onSelectRow]);
 
     return (
         <BaseListItem
@@ -43,7 +25,6 @@ function RadioListItem({
             isFocused={isFocused}
             isDisabled={isDisabled}
             showTooltip={showTooltip}
-            canSelectMultiple={canSelectMultiple}
             onSelectRow={onSelectRow}
             onDismissError={onDismissError}
             shouldPreventDefaultFocusOnSelectRow={shouldPreventDefaultFocusOnSelectRow}
@@ -51,27 +32,6 @@ function RadioListItem({
             keyForList={item.keyForList}
         >
             <>
-                {canSelectMultiple && (
-                    <PressableWithFeedback
-                        accessibilityLabel={item.text ?? ''}
-                        role={CONST.ROLE.BUTTON}
-                        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-                        disabled={isDisabled || item.isDisabledCheckbox}
-                        onPress={handleCheckboxPress}
-                        style={[styles.cursorUnset, StyleUtils.getCheckboxPressableStyle(), item.isDisabledCheckbox && styles.cursorDisabled]}
-                    >
-                        <View style={[StyleUtils.getCheckboxContainerStyle(20), StyleUtils.getMultiselectListStyles(!!item.isSelected, !!item.isDisabled)]}>
-                            {item.isSelected && (
-                                <Icon
-                                    src={Expensicons.Checkmark}
-                                    fill={theme.textLight}
-                                    height={14}
-                                    width={14}
-                                />
-                            )}
-                        </View>
-                    </PressableWithFeedback>
-                )}
                 <View style={[styles.flex1, styles.alignItemsStart]}>
                     <TextWithTooltip
                         shouldShowTooltip={showTooltip}
