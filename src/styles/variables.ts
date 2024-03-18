a78f71309914a106db224b2e0fb309188511ba68
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-03-18 13:04:51 +0100

    move the variable next to iconBottomBar

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -83,6 +83,7 @@ export default {
     iconSizeSuperLarge: 60,
     iconSizeUltraLarge: 120,
     iconBottomBar: 24,
+    sidebarAvatarSize: 28,
     iconHeader: 48,
     emojiSize: 20,
     emojiLineHeight: 28,
@@ -199,7 +200,6 @@ export default {
     oldDotWireframeIconHeight: 143.28,
     sectionIllustrationHeight: 220,
     photoUploadPopoverWidth: 335,
-    sidebarAvatarSize: 28,
 
     // The height of the empty list is 14px (2px for borders and 12px for vertical padding)
     // This is calculated based on the values specified in the 'getGoogleListViewStyle' function of the 'StyleUtils' utility
