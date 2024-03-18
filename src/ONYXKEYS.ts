df55f3c76bdf7994ce8c360d278364adac0c1a93
Ionatan Wiznia <ionatan@expensify.com>
2024-03-18 12:32:09 -0300

    Change NVP name

--- src/ONYXKEYS.ts
+++ src/ONYXKEYS.ts
@@ -298,7 +298,7 @@ const ONYXKEYS = {
         POLICY_CATEGORIES: 'policyCategories_',
         POLICY_RECENTLY_USED_CATEGORIES: 'policyRecentlyUsedCategories_',
         POLICY_TAGS: 'policyTags_',
-        POLICY_RECENTLY_USED_TAGS: 'nvp_policyRecentlyUsedTags_',
+        POLICY_RECENTLY_USED_TAGS: 'nvp_recentlyUsedTags_',
         OLD_POLICY_RECENTLY_USED_TAGS: 'policyRecentlyUsedTags_',
         POLICY_REPORT_FIELDS: 'policyReportFields_',
         WORKSPACE_INVITE_MEMBERS_DRAFT: 'workspaceInviteMembersDraft_',
