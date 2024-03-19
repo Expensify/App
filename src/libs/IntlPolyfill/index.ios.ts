5ae33e4ef520ebf2f63e49eee2042889ff465ead
hurali97 <hurali97@gmail.com>
2024-03-19 15:16:01 +0500

    perf: remove not needed Intl polyfills by separating iOS and android

---
+++ src/libs/IntlPolyfill/index.ios.ts
@@ -0,0 +1,23 @@
+import polyfillListFormat from './polyfillListFormat';
+import polyfillNumberFormat from './polyfillNumberFormat';
+import type IntlPolyfill from './types';
+
+/**
+ * Polyfill the Intl API, always performed for native devices.
+ */
+const intlPolyfill: IntlPolyfill = () => {
+    // Native devices require extra polyfills which are
+    // not yet implemented in hermes.
+    // see support: https://hermesengine.dev/docs/intl/
+
+    require('@formatjs/intl-locale/polyfill');
+
+    // Required to polyfill NumberFormat on iOS
+    // see: https://github.com/facebook/hermes/issues/1172#issuecomment-1776156538
+    require('@formatjs/intl-pluralrules/polyfill');
+    polyfillNumberFormat();
+
+    polyfillListFormat();
+};
+
+export default intlPolyfill;
