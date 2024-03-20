d16f27a039976cab00e3e35ae45214748da37f19
BrtqKr <bartlomiej.krason@swmansion.com>
2024-03-20 12:49:20 +0100

    adjust templates for pre-generate

--- workflow_tests/utils/preGenerateTest.ts
+++ workflow_tests/utils/preGenerateTest.ts
@@ -124,7 +124,7 @@ const jobMocksTemplate = (jobMocksName: string, stepMocks: string[]): string => 
 };
 
 const jobAssertionTemplate = (jobAssertionName: string, stepAssertionsContent: string) => `
-const ${jobAssertionName} = (workflowResult: Step[], didExecute = true) => {
+function ${jobAssertionName}(workflowResult: Step[], didExecute = true) {
     const steps = [\n${stepAssertionsContent}\n];
 
     for (const expectedStep of steps) {
@@ -226,7 +226,7 @@ const getMockFileContent = (workflowName: string, jobs: Record<string, YamlMockJ
             stepMocks.push(stepMockName);
             mockStepsContent += mockStepTemplate(stepMockName, step, jobId);
         });
-        
+
         const jobMocksName = `${workflowName.toUpperCase()}__${jobId.toUpperCase()}__STEP_MOCKS`;
         jobMocks.push(jobMocksName);
         mockStepsContent += jobMocksTemplate(jobMocksName, stepMocks);
