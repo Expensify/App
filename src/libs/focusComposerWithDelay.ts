4c3483018962f4509294a616df823c5a6872c015
FitseTLT <engfitsum@gmail.com>
2024-03-20 23:59:24 +0300

    minor fix

--- src/libs/focusComposerWithDelay.ts
+++ src/libs/focusComposerWithDelay.ts
@@ -14,10 +14,10 @@ function focusComposerWithDelay(textInput: TextInput | null): FocusComposerWithD
     return (shouldDelay = false) => {
         // There could be other animations running while we trigger manual focus.
         // This prevents focus from making those animations janky.
-
         if (!textInput || EmojiPickerAction.isEmojiPickerVisible()) {
             return;
         }
+
         if (!shouldDelay) {
             textInput.focus();
             return;
