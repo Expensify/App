eabe6262507ddcda20b5e0c52cc7ab7acd5dc72f
hurali97 <hurali97@gmail.com>
2024-03-20 17:27:14 +0500

    fix: allow truthy values to be returned correctly

--- src/libs/Localize/index.ts
+++ src/libs/Localize/index.ts
@@ -152,7 +152,7 @@ function translate<TKey extends TranslationPaths>(desiredLanguage: 'en' | 'es' |
     const languageAbbreviation = desiredLanguage.substring(0, 2) as 'en' | 'es';
 
     const translatedPhrase = getTranslatedPhrase(language, phraseKey, languageAbbreviation, ...phraseParameters);
-    if (translatedPhrase) {
+    if (translatedPhrase !== null && translatedPhrase !== undefined) {
         return translatedPhrase;
     }
 
