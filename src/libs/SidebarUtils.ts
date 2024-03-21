81f4e1009e3e0d2c6173593eb327914ccce847fc
aimane-chnaif <aiman.chnaif@gmail.com>
2024-03-21 21:47:13 +1000

    revert using allReportActions

--- src/libs/SidebarUtils.ts
+++ src/libs/SidebarUtils.ts
@@ -83,8 +83,7 @@ function getOrderedReportIDs(
 
         const parentReportActionsKey = `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${report.parentReportID}`;
         const parentReportActions = allReportActions?.[parentReportActionsKey];
-        const reportActionsKey = `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${report.reportID}`;
-        const reportActions = allReportActions?.[reportActionsKey];
+        const reportActions = ReportActionsUtils.getAllReportActions(report.reportID);
         const parentReportAction = parentReportActions?.find((action) => action && action?.reportActionID === report.parentReportActionID);
         const doesReportHaveViolations =
             betas.includes(CONST.BETAS.VIOLATIONS) && !!parentReportAction && ReportUtils.doesTransactionThreadHaveViolations(report, transactionViolations, parentReportAction);
