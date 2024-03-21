d149231d5fbbcf4732c9e18a89bae58cb84ae777
Taras Perun <terry.perun@gmail.com>
2024-03-21 11:02:30 +0100

    undo wrapping

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -2,16 +2,18 @@ import React, {forwardRef} from 'react';
 import BaseVideoPlayer from './BaseVideoPlayer';
 import {videoPlayerDefaultProps, videoPlayerPropTypes} from './propTypes';
 
-const VideoPlayer = forwardRef((props, ref) => (
-    <BaseVideoPlayer
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...props}
-        ref={ref}
-    />
-));
+function VideoPlayer(props, ref) {
+    return (
+        <BaseVideoPlayer
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+            ref={ref}
+        />
+    );
+}
 
 VideoPlayer.displayName = 'VideoPlayer';
 VideoPlayer.propTypes = videoPlayerPropTypes;
 VideoPlayer.defaultProps = videoPlayerDefaultProps;
 
-export default VideoPlayer;
+export default forwardRef(VideoPlayer);
--- src/components/VideoPlayer/index.native.js
+++ src/components/VideoPlayer/index.native.js
@@ -3,19 +3,21 @@ import CONST from '@src/CONST';
 import BaseVideoPlayer from './BaseVideoPlayer';
 import {videoPlayerDefaultProps, videoPlayerPropTypes} from './propTypes';
 
-const VideoPlayer = forwardRef(({videoControlsStyle, ...props}, ref) => (
-    <BaseVideoPlayer
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        {...props}
-        isVideoHovered
-        shouldUseSharedVideoElement={false}
-        videoControlsStyle={[{bottom: CONST.VIDEO_PLAYER.CONTROLS_POSITION.NATIVE}, videoControlsStyle]}
-        ref={ref}
-    />
-));
+function VideoPlayer({videoControlsStyle, ...props}, ref) {
+    return (
+        <BaseVideoPlayer
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+            isVideoHovered
+            shouldUseSharedVideoElement={false}
+            videoControlsStyle={[{bottom: CONST.VIDEO_PLAYER.CONTROLS_POSITION.NATIVE}, videoControlsStyle]}
+            ref={ref}
+        />
+    );
+}
 
 VideoPlayer.displayName = 'VideoPlayer';
 VideoPlayer.propTypes = videoPlayerPropTypes;
 VideoPlayer.defaultProps = videoPlayerDefaultProps;
 
-export default VideoPlayer;
+export default forwardRef(VideoPlayer);
