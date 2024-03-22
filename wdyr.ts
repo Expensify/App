5e919ec96d7228a90b7c6ceccbea31048b1bb640
Mateusz Titz <titzmateusz@gmail.com>
2024-03-22 09:14:36 +0100

    improve types in wdyr.ts

--- wdyr.ts
+++ wdyr.ts
@@ -1,12 +1,12 @@
-/// <reference types="@welldone-software/why-did-you-render" />
-// Implements Why Did You Render (WDYR) in Dev, setup according to: https://github.com/welldone-software/why-did-you-render?tab=readme-ov-file#setup
+// Implements Why Did You Render (WDYR) in Dev
+import type WhyDidYouRender from '@welldone-software/why-did-you-render';
 import React from 'react';
 import Config from 'react-native-config';
 
 const useWDYR = Config?.USE_WDYR === 'true';
 
 if (useWDYR) {
-    const whyDidYouRender = require('@welldone-software/why-did-you-render');
+    const whyDidYouRender = require<typeof WhyDidYouRender>('@welldone-software/why-did-you-render');
     whyDidYouRender(React, {
         // Enable tracking in all pure components by default
         trackAllPureComponents: true,
