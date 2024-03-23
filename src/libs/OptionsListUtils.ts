a592191e050e0cee05481d800ccab8ca31d4962d
Krishna Gupta <belivethatkg@gmail.com>
2024-03-23 09:02:00 +0530

    minor naming change.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -960,11 +960,11 @@ function getCategoryListSections(
     const enabledCategories = Object.values(sortedCategories).filter((category) => category.enabled);
 
     const categorySections: CategoryTreeSection[] = [];
-    const numberOfCategories = enabledCategories.length;
+    const numberOfEnabledCategories = enabledCategories.length;
 
     let indexOffset = 0;
 
-    if (numberOfCategories === 0 && selectedOptions.length > 0) {
+    if (numberOfEnabledCategories === 0 && selectedOptions.length > 0) {
         categorySections.push({
             // "Selected" section
             title: '',
@@ -1015,7 +1015,7 @@ function getCategoryListSections(
     const selectedOptionNames = selectedOptions.map((selectedOption) => selectedOption.name);
     const enabledWithoutSelectedCategories = sortedCategories.filter((category) => category.enabled && !selectedOptionNames.includes(category.name));
 
-    if (numberOfCategories < CONST.CATEGORY_LIST_THRESHOLD) {
+    if (numberOfEnabledCategories < CONST.CATEGORY_LIST_THRESHOLD) {
         categorySections.push({
             // "All" section when items amount less than the threshold
             title: '',
