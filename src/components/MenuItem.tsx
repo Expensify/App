b65f6526ae044efa302e0fef433ac9698b8b3314
Brandon Henry <itsbhenry@gmail.com>
2024-03-23 18:14:36 -0500

    fix by truncation

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -240,6 +240,177 @@ type MenuItemBaseProps = {
 };
 
 type MenuItemProps = (IconProps | AvatarProps | NoIcon) & MenuItemBaseProps;
+const ASTERISK_ITALIC = '*';
+const UNDERSCORE_ITALIC = '_';
+const ASTERISK_BOLD = '**';
+const UNDERSCORE_BOLD = '__';
+const ESCAPED_UNDERSCORE = '\\_';
+const ESCAPED_ASTERISK = '\\*'
+const ASTERISK_PLACEHOLDER_REGEXP = /ASTERISKPLACEHOLDER/gm;
+const UNDERSCORE_PLACEHOLDER_REGEXP = /UNDERSCOREPLACEHOLDER/gm;
+const UNDERSCORE_BOLD_PLACEHOLDER_REGEXP = /UNDERSCOREBOLDPLACEHOLDER/gm;
+const ASTERISK_BOLD_PLACEHOLDER_REGEXP = /ASTERISKBOLDPLACEHOLDER/gm;
+const UNDERSCORE_ITALIC_PLACEHOLDER_REGEXP = /UNDERSCOREITALICPLACEHOLDER/gm;
+const ASTERISK_ITALIC_PLACEHOLDER_REGEXP = /ASTERISKITALICPLACEHOLDER/gm;
+
+
+const ESCAPED_ASTERISK_REGEXP = /\\\*/g;
+
+const ESCAPED_UNDERSCORE_REGEXP = /\\_/g;
+const UNDERSCORE_BOLD_REGEXP = /(__)(.*?)(__)/g;
+const ASTERISK_BOLD_REGEXP = /(\*\*)(.*?)(\*\*)/g;
+const UNDERSCORE_ITALIC_REGEXP = /(_)(.*?)(_)/g;
+const ASTERISK_ITALIC_REGEXP = /(\*)(.*?)(\*)/g;
+
+const HYPERLINK = /^\[([^[]+)\]\(([^)]+)\)/;
+
+const formatMarkers = [
+  ASTERISK_BOLD_PLACEHOLDER_REGEXP.source,
+  UNDERSCORE_BOLD_PLACEHOLDER_REGEXP.source,
+  ASTERISK_ITALIC_PLACEHOLDER_REGEXP.source,
+  UNDERSCORE_ITALIC_PLACEHOLDER_REGEXP.source,
+];
+
+const formatPlaceholdersMap = {
+  [UNDERSCORE_PLACEHOLDER_REGEXP.source]: ESCAPED_UNDERSCORE.length,
+  [ASTERISK_PLACEHOLDER_REGEXP.source]: ESCAPED_ASTERISK.length,
+}
+
+const findFormatPlaceholderAhead = (text: string) => {
+  const formatPlaceholders = Object.keys(formatPlaceholdersMap);
+
+  for (let i = 0, l = formatPlaceholders.length; i < l; i++) {
+    if (text.startsWith(formatPlaceholders[i])) {
+      return formatPlaceholders[i];
+    }
+  }
+
+  return null;
+}
+
+const findFormatMarkerAhead = (text: string, formatStack: string[]): string | null => {
+  for (let i = 0, l = formatMarkers.length; i < l; i++) {
+    if (text.startsWith(formatMarkers[i])) {
+      if (formatStack[formatStack.length - 1] === formatMarkers[i]) {
+        formatStack.pop();
+      } else {
+        formatStack.push(formatMarkers[i]);
+      }
+      return formatMarkers[i];
+    }
+  }
+
+  return null;
+};
+
+const replaceFormatMarkersWithPlaceholders = (text: string): string =>
+  text
+    .replace(ESCAPED_UNDERSCORE_REGEXP, UNDERSCORE_PLACEHOLDER_REGEXP.source)
+    .replace(ESCAPED_ASTERISK_REGEXP, ASTERISK_PLACEHOLDER_REGEXP.source)
+    .replace(
+      UNDERSCORE_BOLD_REGEXP,
+      `${UNDERSCORE_BOLD_PLACEHOLDER_REGEXP.source}$2${UNDERSCORE_BOLD_PLACEHOLDER_REGEXP.source}`
+    )
+    .replace(
+      ASTERISK_BOLD_REGEXP,
+      `${ASTERISK_BOLD_PLACEHOLDER_REGEXP.source}$2${ASTERISK_BOLD_PLACEHOLDER_REGEXP.source}`
+    )
+    .replace(
+      UNDERSCORE_ITALIC_REGEXP,
+      `${UNDERSCORE_ITALIC_PLACEHOLDER_REGEXP.source}$2${UNDERSCORE_ITALIC_PLACEHOLDER_REGEXP.source}`
+    )
+    .replace(
+      ASTERISK_ITALIC_REGEXP,
+      `${ASTERISK_ITALIC_PLACEHOLDER_REGEXP.source}$2${ASTERISK_ITALIC_PLACEHOLDER_REGEXP.source}`
+    );
+
+const replaceFormatPlaceholdersWithMarkers = (text: string): string =>
+  text
+    .replace(UNDERSCORE_PLACEHOLDER_REGEXP, ESCAPED_UNDERSCORE)
+    .replace(ASTERISK_PLACEHOLDER_REGEXP, ESCAPED_ASTERISK)
+    .replace(UNDERSCORE_BOLD_PLACEHOLDER_REGEXP, UNDERSCORE_BOLD)
+    .replace(ASTERISK_BOLD_PLACEHOLDER_REGEXP, ASTERISK_BOLD)
+    .replace(UNDERSCORE_ITALIC_PLACEHOLDER_REGEXP, UNDERSCORE_ITALIC)
+    .replace(ASTERISK_ITALIC_PLACEHOLDER_REGEXP, ASTERISK_ITALIC);
+
+const truncate = (text: string, limit: number, ellipsis: boolean): string => {
+    let count = 0;
+
+  const truncateString = (tText:string) => {
+    const formatStack = [] as string[];
+    let skipCountIncrement = false;
+    let outputText = '';
+    let index = 0;
+
+    while (count < limit && index < tText.length) {
+      const formatMarker = findFormatMarkerAhead(tText.substring(index), formatStack);
+      if (formatMarker) {
+        outputText += formatMarker;
+        index += formatMarker.length;
+        skipCountIncrement = true;
+      }
+
+      const formatPlaceholder = findFormatPlaceholderAhead(tText.substring(index));
+      if (formatPlaceholder) {
+        outputText += formatPlaceholder;
+        index += formatPlaceholder.length;
+        skipCountIncrement = true;
+        count += formatPlaceholdersMap[formatPlaceholder];
+      }
+
+      const hyperlinkAheadRegexp = new RegExp(HYPERLINK);
+      const hyperlinkMatch = hyperlinkAheadRegexp.exec(tText.substring(index));
+      if (hyperlinkMatch) {
+        const hyperlinkText = hyperlinkMatch[1];
+        const hyperlinkUrl = hyperlinkMatch[2];
+
+        outputText += `[${truncateString(hyperlinkText)}](${hyperlinkUrl})`;
+        index += hyperlinkMatch[0].length;
+        skipCountIncrement = true;
+      }
+
+      if (!formatMarker && !hyperlinkMatch) {
+        outputText += tText[index];
+        index++;
+      }
+
+      if (!skipCountIncrement) {
+        count++;
+      }
+
+      skipCountIncrement = false;
+    }
+
+    outputText = outputText.trimEnd();
+
+    while (formatStack.length > 0) {
+      outputText += formatStack.pop();
+    }
+
+    return outputText;
+  };
+
+  let outputText = truncateString(text);
+
+  if (ellipsis && outputText.length < text.length) {
+    outputText += '...';
+  }
+
+  return outputText;
+};
+
+function truncateMarkdown(text = '', options: { limit?: number; ellipsis?: boolean } = {}): string {
+  const { limit, ellipsis } = options || {};
+
+  if (Number.isNaN(parseInt(limit as unknown as string, 10)) || text.length <= (limit ?? 0)) {
+    return text;
+  }
+
+  let outputText = replaceFormatMarkersWithPlaceholders(text);
+  outputText = truncate(outputText, limit ?? 0, ellipsis ?? false);
+  outputText = replaceFormatPlaceholdersWithMarkers(outputText);
+  return outputText;
+}
 
 function MenuItem(
     {
@@ -514,7 +685,7 @@ function MenuItem(
                                         <View style={[styles.flexRow, styles.alignItemsCenter]}>
                                             {!!title && (shouldRenderAsHTML || (shouldParseTitle && !!html.length)) && (
                                                 <View style={styles.renderHTMLTitle}>
-                                                    <RenderHTML html={getProcessedTitle} />
+                                                    <RenderHTML html={truncateMarkdown(getProcessedTitle, { limit: 100, ellipsis: true })} />
                                                 </View>
                                             )}
                                             {!shouldRenderAsHTML && !shouldParseTitle && !!title && (
