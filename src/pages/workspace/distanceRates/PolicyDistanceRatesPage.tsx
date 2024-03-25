c34a731bccca03c82738e01697c628ceff951cd1
Michal Muzyk <mimuzyk@gmail.com>
2024-03-25 12:59:30 +0100

    fix: delete case fix

--- src/pages/workspace/distanceRates/PolicyDistanceRatesPage.tsx
+++ src/pages/workspace/distanceRates/PolicyDistanceRatesPage.tsx
@@ -60,10 +60,11 @@ function PolicyDistanceRatesPage({policy, route}: PolicyDistanceRatesPageProps) 
         [policy?.customUnits],
     );
     const customUnitRates: Record<string, Rate> = useMemo(() => customUnit?.rates ?? {}, [customUnit]);
-    // Filter out rates that are pending deletion
+    // Filter out rates that will be deleted
     const allSelectableRates = Object.values(customUnitRates).filter((rate) => rate.pendingAction !== CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE);
-    const canDeleteSelectedRates = selectedDistanceRates.length !== allSelectableRates.length;
-    const canDisableSelectedRates = allSelectableRates.filter((rate: Rate) => !selectedDistanceRates.includes(rate)).some((rate) => rate.enabled);
+    const canDisableOrDeleteSelectedRates = allSelectableRates
+        .filter((rate: Rate) => !selectedDistanceRates.some((selectedRate) => selectedRate.customUnitRateID === rate.customUnitRateID))
+        .some((rate) => rate.enabled === true);
 
     function fetchDistanceRates() {
         Policy.openPolicyDistanceRatesPage(policyID);
@@ -199,7 +200,7 @@ function PolicyDistanceRatesPage({policy, route}: PolicyDistanceRatesPageProps) 
                 text: translate('workspace.distanceRates.deleteRates', {count: selectedDistanceRates.length}),
                 value: CONST.POLICY.DISTANCE_RATES_BULK_ACTION_TYPES.DELETE,
                 icon: Expensicons.Trashcan,
-                onSelected: () => (canDeleteSelectedRates ? setIsDeleteModalVisible(true) : setIsWarningModalVisible(true)),
+                onSelected: () => (canDisableOrDeleteSelectedRates ? setIsDeleteModalVisible(true) : setIsWarningModalVisible(true)),
             },
         ];
 
@@ -209,7 +210,7 @@ function PolicyDistanceRatesPage({policy, route}: PolicyDistanceRatesPageProps) 
                 text: translate('workspace.distanceRates.disableRates', {count: enabledRates.length}),
                 value: CONST.POLICY.DISTANCE_RATES_BULK_ACTION_TYPES.DISABLE,
                 icon: Expensicons.DocumentSlash,
-                onSelected: () => (canDisableSelectedRates ? disableRates() : setIsWarningModalVisible(true)),
+                onSelected: () => (canDisableOrDeleteSelectedRates ? disableRates() : setIsWarningModalVisible(true)),
             });
         }
 
