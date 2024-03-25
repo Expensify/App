c5c1413b685188c0f168d75ee91e8ad3c2999605
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-03-25 12:34:01 +0100

    decimals support for creating new taxes

--- src/pages/workspace/taxes/WorkspaceCreateTaxPage.tsx
+++ src/pages/workspace/taxes/WorkspaceCreateTaxPage.tsx
@@ -106,7 +106,10 @@ function WorkspaceCreateTaxPage({
                                         description={translate('workspace.taxes.value')}
                                         rightLabel={translate('common.required')}
                                         hideCurrencySymbol
-                                        extraDecimals={6}
+                                        // The default currency uses 2 decimal places, so we substract it
+                                        extraDecimals={CONST.MAX_TAX_RATE_DECIMAL_PLACES - 2}
+                                        // We increase the amount max length. We have to add 2 places for one digit and comma.
+                                        amountMaxLength={CONST.MAX_TAX_RATE_DECIMAL_PLACES + 2}
                                         extraSymbol={<Text style={styles.iouAmountText}>%</Text>}
                                     />
                                 </View>
