eada5836332fd242310952f4b604ad8f275f1a78
VickyStash <vikstash@gmail.com>
2024-03-25 18:50:20 +0100

    Fix ts issues

--- workflow_tests/utils/utils.ts
+++ workflow_tests/utils/utils.ts
@@ -1,26 +1,9 @@
+import type {EventJSON} from '@kie/act-js/build/src/action-event/action-event.types';
 import type {StepIdentifier} from '@kie/act-js/build/src/step-mocker/step-mocker.types';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'yaml';
 import type ExtendedAct from './ExtendedAct';
-
-type Label = {
-    name: string;
-};
-
-type EventOptions = {
-    action?: string;
-    ref?: string;
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    ref_type?: string;
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    ref_name?: string;
-    type?: string;
-    label?: Label;
-    issue?: {
-        labels: Label[];
-    };
-};
 
 type StepAssertionInputEntry = {key: string; value: string};
 
@@ -33,7 +16,7 @@ type StepAssertion = {
 function setUpActParams(
     act: ExtendedAct,
     event: string | null = null,
-    eventOptions: EventOptions | null = null,
+    eventOptions: EventJSON | null = null,
     secrets: Record<string, string> | null = null,
     githubToken: string | null = null,
     envVars: Record<string, string> | null = null,
