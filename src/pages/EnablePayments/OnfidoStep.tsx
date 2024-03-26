4bd8986678302c6e5a6a942a5fd3e7d5baea5fbe
tienifr <christianwen18@gmail.com>
2024-03-27 03:02:02 +0700

    run prettier

--- src/pages/EnablePayments/OnfidoStep.tsx
+++ src/pages/EnablePayments/OnfidoStep.tsx
@@ -40,10 +40,13 @@ function OnfidoStep({walletOnfidoData = DEFAULT_WALLET_ONFIDO_DATA}: OnfidoStepP
         Wallet.updateCurrentStep(CONST.WALLET.STEP.ADDITIONAL_DETAILS);
     }, []);
 
-    const reportError = useCallback((error?: string) => {
-        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing -- error can be empty
-        Growl.error(error || translate('onfidoStep.genericError'), 10000);
-    }, [translate]);
+    const reportError = useCallback(
+        (error?: string) => {
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing -- error can be empty
+            Growl.error(error || translate('onfidoStep.genericError'), 10000);
+        },
+        [translate],
+    );
 
     const verifyIdentity = useCallback(
         (data: OnfidoData) => {
