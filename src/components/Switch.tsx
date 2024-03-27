4c44b31cb7a2a829431d53b7fe36c137db56fa69
Brandon Henry <itsbhenry@gmail.com>
2024-03-26 20:38:08 -0500

    added default margin to switch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -39,7 +39,7 @@ function Switch({isOn, onToggle, accessibilityLabel, disabled}: SwitchProps) {
     return (
         <PressableWithFeedback
             disabled={disabled}
-            style={[styles.switchTrack, !isOn && styles.switchInactive]}
+            style={[styles.switchTrack, !isOn && styles.switchInactive, styles.ml2]}
             onPress={() => onToggle(!isOn)}
             onLongPress={() => onToggle(!isOn)}
             role={CONST.ROLE.SWITCH}
