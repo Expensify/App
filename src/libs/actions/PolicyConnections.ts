e0532d5aaceb560bb1648dae198129656c6dfd08
Hayata Suenaga <hayata@expensify.com>
2024-03-27 16:19:04 -0700

    chore: add new action func to call web-e command

---
+++ src/libs/actions/PolicyConnections.ts
@@ -0,0 +1,15 @@
+import * as API from '@libs/API';
+import type {OpenPolicyAccountingPageParams} from '@libs/API/parameters';
+import {READ_COMMANDS} from '@libs/API/types';
+
+function openPolicyAccountingPage({policyID}: {policyID: string}) {
+    const parameters: OpenPolicyAccountingPageParams = {
+        policyID,
+    };
+
+    API.read(READ_COMMANDS.OPEN_POLICY_ACCOUNTING_PAGE, parameters);
+}
+
+// More action functions will be added later
+// eslint-disable-next-line import/prefer-default-export
+export {openPolicyAccountingPage};
