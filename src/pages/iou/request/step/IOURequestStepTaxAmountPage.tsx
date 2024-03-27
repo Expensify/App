4afc0284736502f29b8cd3ab428f68e55819bdd7
Jakub Szymczak <szymczak.jakub12@gmail.com>
2024-03-27 14:20:19 +0100

    migrate IOURequestStepTaxAmountPage

--- src/pages/iou/request/step/IOURequestStepTaxAmountPage.tsx
+++ src/pages/iou/request/step/IOURequestStepTaxAmountPage.tsx
@@ -1,59 +1,43 @@
 import {useFocusEffect} from '@react-navigation/native';
-import lodashGet from 'lodash/get';
-import PropTypes from 'prop-types';
 import React, {useCallback, useEffect, useRef} from 'react';
 import {View} from 'react-native';
+import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
-import taxPropTypes from '@components/taxPropTypes';
-import transactionPropTypes from '@components/transactionPropTypes';
+import type {BaseTextInputRef} from '@components/TextInput/BaseTextInput/types';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
-import compose from '@libs/compose';
 import * as CurrencyUtils from '@libs/CurrencyUtils';
 import * as IOUUtils from '@libs/IOUUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import * as TransactionUtils from '@libs/TransactionUtils';
 import MoneyRequestAmountForm from '@pages/iou/steps/MoneyRequestAmountForm';
-import reportPropTypes from '@pages/reportPropTypes';
 import * as IOU from '@userActions/IOU';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
-import IOURequestStepRoutePropTypes from './IOURequestStepRoutePropTypes';
+import type {Policy, Transaction} from '@src/types/onyx';
 import withFullTransactionOrNotFound from './withFullTransactionOrNotFound';
+import type {WithWritableReportOrNotFoundProps} from './withWritableReportOrNotFound';
 import withWritableReportOrNotFound from './withWritableReportOrNotFound';
 
-const propTypes = {
-    /** Navigation route context info provided by react navigation */
-    route: IOURequestStepRoutePropTypes.isRequired,
-
-    /* Onyx Props */
-    /** The report that the transaction belongs to */
-    report: reportPropTypes,
-
-    /** The transaction object being modified in Onyx */
-    transaction: transactionPropTypes,
-
-    /* Onyx Props */
-    /** The policy of the report */
-    policy: PropTypes.shape({
-        /** Collection of tax rates attached to a policy */
-        taxRates: taxPropTypes,
-    }),
+type IOURequestStepTaxAmountPageOnyxProps = {
+    policy: OnyxEntry<Policy>;
 };
 
-const defaultProps = {
-    report: {},
-    transaction: {},
-    policy: {},
-};
+type IOURequestStepTaxAmountPageProps = {
+    transaction: OnyxEntry<Transaction>;
+} & IOURequestStepTaxAmountPageOnyxProps &
+    WithWritableReportOrNotFoundProps;
 
-const getTaxAmount = (transaction, defaultTaxValue) => {
-    const percentage = (transaction.taxRate ? transaction.taxRate.data.value : defaultTaxValue) || '';
-    return CurrencyUtils.convertToBackendAmount(Number.parseFloat(TransactionUtils.calculateTaxAmount(percentage, transaction.amount)));
+const getTaxAmount = (transaction: OnyxEntry<Transaction>, defaultTaxValue: string | undefined) => {
+    if (!transaction?.amount) {
+        return;
+    }
+    const percentage = (transaction?.taxRate ? transaction?.taxRate?.data?.value : defaultTaxValue) ?? '';
+    return CurrencyUtils.convertToBackendAmount(TransactionUtils.calculateTaxAmount(percentage, transaction?.amount));
 };
 
 function IOURequestStepTaxAmountPage({
@@ -61,30 +45,29 @@ function IOURequestStepTaxAmountPage({
         params: {iouType, reportID, transactionID, backTo},
     },
     transaction,
-    transaction: {currency},
     report,
     policy,
-}) {
+}: IOURequestStepTaxAmountPageProps) {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
-    const textInput = useRef(null);
+    const textInput = useRef<BaseTextInputRef | null>();
     const isEditing = Navigation.getActiveRoute().includes('taxAmount');
 
-    const focusTimeoutRef = useRef(null);
+    const focusTimeoutRef = useRef<NodeJS.Timeout>();
 
     const isSaveButtonPressed = useRef(false);
-    const originalCurrency = useRef(null);
-    const taxRates = lodashGet(policy, 'taxRates', {});
+    const originalCurrency = useRef<string>();
+    const taxRates = policy?.taxRates;
 
     useEffect(() => {
-        if (transaction.originalCurrency) {
+        if (transaction?.originalCurrency) {
             originalCurrency.current = transaction.originalCurrency;
-        } else {
-            originalCurrency.current = currency;
-            IOU.setMoneyRequestOriginalCurrency_temporaryForRefactor(transactionID, currency);
+        } else if (transaction?.currency) {
+            originalCurrency.current = transaction.currency;
+            IOU.setMoneyRequestOriginalCurrency_temporaryForRefactor(transactionID, transaction?.currency);
         }
         return () => {
-            if (isSaveButtonPressed.current) {
+            if (isSaveButtonPressed.current || !originalCurrency.current) {
                 return;
             }
             IOU.setMoneyRequestCurrency_temporaryForRefactor(transactionID, originalCurrency.current, true);
@@ -94,7 +77,7 @@ function IOURequestStepTaxAmountPage({
 
     useFocusEffect(
         useCallback(() => {
-            focusTimeoutRef.current = setTimeout(() => textInput.current && textInput.current.focus(), CONST.ANIMATED_TRANSITION);
+            focusTimeoutRef.current = setTimeout(() => textInput.current?.focus(), CONST.ANIMATED_TRANSITION);
             return () => {
                 if (!focusTimeoutRef.current) {
                     return;
@@ -115,12 +98,13 @@ function IOURequestStepTaxAmountPage({
         Navigation.navigate(ROUTES.MONEY_REQUEST_STEP_CURRENCY.getRoute(iouType, transactionID, reportID, backTo ? 'confirm' : '', Navigation.getActiveRouteWithoutParams()));
     };
 
-    const updateTaxAmount = (currentAmount) => {
+    const updateTaxAmount = (currentAmount: {amount: string}) => {
         isSaveButtonPressed.current = true;
         const amountInSmallestCurrencyUnits = CurrencyUtils.convertToBackendAmount(Number.parseFloat(currentAmount.amount));
         IOU.setMoneyRequestTaxAmount(transactionID, amountInSmallestCurrencyUnits);
 
-        IOU.setMoneyRequestCurrency_temporaryForRefactor(transactionID, currency || CONST.CURRENCY.USD, true);
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+        IOU.setMoneyRequestCurrency_temporaryForRefactor(transactionID, transaction?.currency || CONST.CURRENCY.USD, true);
 
         if (backTo) {
             Navigation.goBack(backTo);
@@ -130,7 +114,7 @@ function IOURequestStepTaxAmountPage({
         // If a reportID exists in the report object, it's because the user started this flow from using the + button in the composer
         // inside a report. In this case, the participants can be automatically assigned from the report and the user can skip the participants step and go straight
         // to the confirm step.
-        if (report.reportID) {
+        if (report?.reportID) {
             // TODO: Is this really needed at all?
             IOU.setMoneyRequestParticipantsFromReport(transactionID, report);
             Navigation.navigate(ROUTES.MONEY_REQUEST_STEP_CONFIRMATION.getRoute(iouType, transactionID, reportID));
@@ -145,10 +129,9 @@ function IOURequestStepTaxAmountPage({
     const content = (
         <MoneyRequestAmountForm
             isEditing={isEditing}
-            currency={currency}
-            amount={transaction.taxAmount}
-            taxAmount={getTaxAmount(transaction, taxRates.defaultValue)}
-            transaction={transaction}
+            currency={transaction?.currency}
+            amount={transaction?.taxAmount}
+            taxAmount={getTaxAmount(transaction, taxRates?.defaultValue)}
             ref={(e) => (textInput.current = e)}
             onCurrencyButtonPress={navigateToCurrencySelectionPage}
             onSubmitButtonPress={updateTaxAmount}
@@ -176,16 +159,16 @@ function IOURequestStepTaxAmountPage({
     );
 }
 
-IOURequestStepTaxAmountPage.propTypes = propTypes;
-IOURequestStepTaxAmountPage.defaultProps = defaultProps;
 IOURequestStepTaxAmountPage.displayName = 'IOURequestStepTaxAmountPage';
 
-export default compose(
-    withWritableReportOrNotFound,
-    withFullTransactionOrNotFound,
-    withOnyx({
-        policy: {
-            key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY}${report ? report.policyID : '0'}`,
-        },
-    }),
-)(IOURequestStepTaxAmountPage);
+// eslint-disable-next-line rulesdir/no-negated-variables
+const IOURequestStepTaxAmountPageWithWritableReportOrNotFound = withWritableReportOrNotFound(IOURequestStepTaxAmountPage);
+// eslint-disable-next-line rulesdir/no-negated-variables
+const IOURequestStepTaxAmountPageWithFullTransactionOrNotFound = withFullTransactionOrNotFound(IOURequestStepTaxAmountPageWithWritableReportOrNotFound);
+
+export default withOnyx<IOURequestStepTaxAmountPageProps, IOURequestStepTaxAmountPageOnyxProps>({
+    policy: {
+        key: ({report}) => `${ONYXKEYS.COLLECTION.POLICY}${report ? report.policyID : '0'}`,
+    },
+    // @ts-expect-error TODO: Remove this once withFullTransactionOrNotFound (https://github.com/Expensify/App/issues/36123) is migrated to TypeScript.
+})(IOURequestStepTaxAmountPageWithFullTransactionOrNotFound);
