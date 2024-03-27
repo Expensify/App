b68937c6c1877e5592542e4651e525aff3d2d9dd
usman-ghani564 <usmanghani564.ug9@gmail.com>
2024-03-28 02:42:52 +0500

    use key to get report

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -4449,12 +4449,8 @@ function getChatByParticipants(newParticipantList: number[], reports: OnyxCollec
     );
 }
 
-function getChatByReportID(reportID: string, reports: OnyxCollection<Report> = allReports) {
-    return (
-        Object.values(reports ?? {}).find((report) => {
-            return report?.reportID === reportID;
-        }) ?? null
-    );
+function getChatByReportID(reportID: string) {
+    return allReports[`${ONYXKEYS.COLLECTION.REPORT}${reportID}`]
 }
 
 /**
