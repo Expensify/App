d5b38947dea86c1e972a79de8a90dd2a3a187f11
Carlos Martins <cmartins@expensify.com>
2024-03-26 18:02:34 -0600

    rm success data

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -4398,15 +4398,6 @@ function getPayMoneyRequestParams(chatReport: OnyxTypes.Report, iouReport: OnyxT
     const successData: OnyxUpdate[] = [
         {
             onyxMethod: Onyx.METHOD.MERGE,
-            key: `${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${iouReport.reportID}`,
-            value: {
-                [optimisticIOUReportAction.reportActionID]: {
-                    pendingAction: null,
-                },
-            },
-        },
-        {
-            onyxMethod: Onyx.METHOD.MERGE,
             key: `${ONYXKEYS.COLLECTION.REPORT}${iouReport.reportID}`,
             value: {
                 pendingFields: {
