878ba7431ba6f9f8d60ccbc6d18bc31d4ace4967
neil-marcellini <neil@expensify.com>
2024-03-28 14:44:09 -0700

    If distance field is shown and writeable it should be editable

--- src/components/MoneyTemporaryForRefactorRequestConfirmationList.js
+++ src/components/MoneyTemporaryForRefactorRequestConfirmationList.js
@@ -246,13 +246,12 @@ function MoneyTemporaryForRefactorRequestConfirmationList({
     const theme = useTheme();
     const styles = useThemeStyles();
     const {translate, toLocaleDigit} = useLocalize();
-    const {canUseP2PDistanceRequests, canUseViolations} = usePermissions(iouType);
+    const {canUseViolations} = usePermissions();
 
     const isTypeRequest = iouType === CONST.IOU.TYPE.REQUEST;
     const isTypeSplit = iouType === CONST.IOU.TYPE.SPLIT;
     const isTypeSend = iouType === CONST.IOU.TYPE.SEND;
     const isTypeTrackExpense = iouType === CONST.IOU.TYPE.TRACK_EXPENSE;
-    const canEditDistance = isTypeRequest || (canUseP2PDistanceRequests && isTypeSplit);
 
     const {unit, rate, currency} = mileageRate;
     const distance = lodashGet(transaction, 'routes.route0.distance', 0);
@@ -723,7 +722,7 @@ function MoneyTemporaryForRefactorRequestConfirmationList({
             item: (
                 <MenuItemWithTopDescription
                     key={translate('common.distance')}
-                    shouldShowRightIcon={!isReadOnly && canEditDistance}
+                    shouldShowRightIcon={!isReadOnly}
                     title={isMerchantEmpty ? '' : iouMerchant}
                     description={translate('common.distance')}
                     style={[styles.moneyRequestMenuItem]}
@@ -734,7 +733,7 @@ function MoneyTemporaryForRefactorRequestConfirmationList({
                         )
                     }
                     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-                    disabled={didConfirm || !canEditDistance}
+                    disabled={didConfirm}
                     interactive={!isReadOnly}
                 />
             ),
