2819878caf552b80d426d34e442bbf60f07cae70
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-03-28 12:14:03 +0100

    fix: run gh-actions-build

--- .github/actions/javascript/verifySignedCommits/index.js
+++ .github/actions/javascript/verifySignedCommits/index.js
@@ -4,6 +4,9 @@
 /**
  * NOTE: This is a compiled file. DO NOT directly edit this file.
  */
+/**
+ * NOTE: This is a compiled file. DO NOT directly edit this file.
+ */
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
