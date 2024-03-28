1c814c51a14085a59de008c08db62912a7fe9476
Ishpaul Singh <ishpaul207@gmail.com>
2024-03-29 02:09:57 +0530

    fix lint

--- src/components/Attachments/AttachmentView/AttachmentViewPdf/index.tsx
+++ src/components/Attachments/AttachmentView/AttachmentViewPdf/index.tsx
@@ -2,7 +2,7 @@ import React, {memo} from 'react';
 import PDFView from '@components/PDFView';
 import type AttachmentViewPdfProps from './types';
 
-function AttachmentViewPdf({file, encryptedSourceUrl, isFocused, onPress, onToggleKeyboard, onLoadComplete, style, onError, isUsedAsChatAttachment} : AttachmentViewPdfProps) {
+function AttachmentViewPdf({file, encryptedSourceUrl, isFocused, onPress, onToggleKeyboard, onLoadComplete, style, onError, isUsedAsChatAttachment}: AttachmentViewPdfProps) {
     return (
         <PDFView
             // @ts-expect-error waiting for https://github.com/Expensify/App/issues/16186 merge
