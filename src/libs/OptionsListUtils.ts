82445012d0fb8f0a695b13df50409e5361dbdb2f
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-03-28 18:41:14 +0000

    Reapply "[No QA] Migrate 'OptionsListUtilsTest.js', 'DateUtilsTest.js', 'SidebarLinks.perf-test.js', 'markdown.js' and 'ReportUtilsTest.js' to Typescript"
    This reverts commit 39aaa567d352e8a43538886ea614c745d1b8c03a.

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -2095,4 +2095,4 @@ export {
     getTaxRatesSection,
 };
 
-export type {MemberForList, CategorySection, GetOptions, PayeePersonalDetails, Category};
+export type {MemberForList, CategorySection, CategoryTreeSection, GetOptions, PayeePersonalDetails, Category, Tag};
