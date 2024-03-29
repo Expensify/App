d8139a8c5fe43e9d0cd562cbfe2dda09d599f2f1
Hayata Suenaga <hayata@expensify.com>
2024-03-29 10:14:53 -0700

    doc: add and improve comments

--- src/pages/workspace/withPolicyConnections.tsx
+++ src/pages/workspace/withPolicyConnections.tsx
@@ -8,17 +8,27 @@ import type {WithPolicyProps} from './withPolicy';
 
 type WithPolicyConnectionsProps = WithPolicyProps;
 
+/**
+ * Higher-order component that fetches the connections data and populates
+ * the corresponding field of the policy object if the field is empty. It then passes the policy object
+ * to the wrapped component.
+ *
+ * Use this HOC when you need the policy object with its connections field populated.
+ *
+ * Only the active policy gets the complete policy data upon app start that includes the connections data.
+ * For other policies, the connections data needs to be fetched when it's needed.
+ */
 function withPolicyConnections(WrappedComponent: ComponentType<WithPolicyConnectionsProps>) {
     function WithPolicyConnections({policy, policyMembers, policyDraft, policyMembersDraft, route}: WithPolicyConnectionsProps) {
         const {isOffline} = useNetwork();
 
         useEffect(() => {
+            // When the accounting feature is not enabled, or if the connections data already exists,
+            // there is no need to fetch the connections data.
             if (!policy?.areConnectionsEnabled || !!policy?.connections || !policy?.id) {
                 return;
             }
 
-            // If the policy doesn't have the connections data when it has the accounting connections feature enabled,
-            // call the API to get the data
             openPolicyAccountingPage(policy.id);
         }, [policy]);
 
