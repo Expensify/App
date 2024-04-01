fea5996e82377084ba8954fe64915454ec452f3a
Alberto <alberto@expensify.com>
2024-04-01 13:26:30 +0200

    remove console

--- src/pages/iou/steps/NewRequestAmountPage.tsx
+++ src/pages/iou/steps/NewRequestAmountPage.tsx
@@ -137,8 +137,6 @@ function NewRequestAmountPage({route, iou, report, selectedTab}: NewRequestAmoun
             selectedTab={selectedTab ?? CONST.TAB_REQUEST.MANUAL}
         />
     );
-    console.log(iou);
-    console.log(iouType);
 
     // ScreenWrapper is only needed in edit mode because we have a dedicated route for the edit amount page (MoneyRequestEditAmountPage).
     // The rest of the cases this component is rendered through <MoneyRequestSelectorPage /> which has it's own ScreenWrapper
