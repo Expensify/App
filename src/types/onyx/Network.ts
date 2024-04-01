4213ccf99135100c4963a4e127dd7689efa3797b
tienifr <christianwen18@gmail.com>
2024-04-01 17:10:30 +0700

    fix typecheck

--- src/types/onyx/Network.ts
+++ src/types/onyx/Network.ts
@@ -2,6 +2,7 @@ type Network = {
     /** Is the network currently offline or not */
     isOffline: boolean;
 
+    /** Is the backend reachable when online */
     isBackendReachable: boolean;
 
     /** Should the network be forced offline */
