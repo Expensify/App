4860d792d5ee863fef8e8ca053261eec7049dfdf
rayane-djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-04-02 18:34:48 +0100

    prevent Thread and Replies words from being selected and copied

--- src/pages/home/report/ThreadDivider.tsx
+++ src/pages/home/report/ThreadDivider.tsx
@@ -27,7 +27,10 @@ function ThreadDivider({ancestor, isDisabled}: ThreadDividerProps) {
     const {translate} = useLocalize();
 
     return (
-        <View style={[styles.flexRow, styles.alignItemsCenter, styles.ml5, styles.mt3, styles.mb1, styles.userSelectNone]}>
+        <View
+            style={[styles.flexRow, styles.alignItemsCenter, styles.ml5, styles.mt3, styles.mb1, styles.userSelectNone]}
+            dataSet={{[CONST.SELECTION_SCRAPER_HIDDEN_ELEMENT]: true}}
+        >
             <PressableWithoutFeedback
                 onPress={() => Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(ancestor?.report?.parentReportID ?? ''))}
                 accessibilityLabel={translate('threads.thread')}
