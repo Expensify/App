e45193c81de6fb3b01b854a76b51e6f045ff2e20
Wildan Muhlis <muhliswildan@gmail.com>
2024-04-02 08:47:54 +0700

    optimize desktop fileDownload

--- src/libs/fileDownload/index.desktop.ts
+++ src/libs/fileDownload/index.desktop.ts
@@ -11,13 +11,16 @@ const fileDownload: FileDownload = (url, fileName) => {
         saveAs: true,
     };
     window.electron.send(ELECTRON_EVENTS.DOWNLOAD, {url, options});
-
     return new Promise((resolve) => {
-        window.electron.on(ELECTRON_EVENTS.DOWNLOAD_STARTED, (args) => {
-            if (args.url !== url) {
-                return;
+        window.electron.on(ELECTRON_EVENTS.DOWNLOAD_STARTED, (...args: unknown[]) => {
+            const arg = Array.isArray(args) ? args[0] : null;
+            const eventUrl = arg && typeof arg === 'object' && 'url' in arg ? arg.url : null;
+
+            // This event is triggered for all active download instances. We intentionally keep other promises waiting.
+            // Early resolution or rejection of other promises could prematurely stop the loading spinner or prevent the promise from being resolved.
+            if (eventUrl === url) {
+                resolve();
             }
-            resolve();
         });
     });
 };
