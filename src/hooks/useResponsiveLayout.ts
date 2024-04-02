15372b72c5d0f709ac0c9785022bb0707d240ca6
rayane-djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-04-02 20:53:22 +0100

    Revert "optimize useResponsive hook by removing unnecessary state updates" made changes by error
    This reverts commit c694a2d311f6c8c1c1f2797dc37043b0148d554e.

--- src/hooks/useResponsiveLayout.ts
+++ src/hooks/useResponsiveLayout.ts
@@ -6,25 +6,29 @@ type ResponsiveLayoutResult = {
     shouldUseNarrowLayout: boolean;
     isSmallScreenWidth: boolean;
     isInModal: boolean;
+    isExtraSmallScreenHeight: boolean;
+    isMediumScreenWidth: boolean;
+    isLargeScreenWidth: boolean;
+    isExtraSmallScreenWidth: boolean;
+    isSmallScreen: boolean;
 };
 
 /**
- * Hook to determine if we are on mobile devices or in the Modal Navigator
+ * Hook to determine if we are on mobile devices or in the Modal Navigator.
+ * Use "shouldUseNarrowLayout" for "on mobile or in RHP/LHP", "isSmallScreenWidth" for "on mobile", "isInModal" for "in RHP/LHP".
+ * Note: Don't use "shouldUseNarrowLayout" in popovers and "alert-style" modals.
  */
 export default function useResponsiveLayout(): ResponsiveLayoutResult {
-    const {isSmallScreenWidth} = useWindowDimensions();
+    const {isSmallScreenWidth, isExtraSmallScreenHeight, isExtraSmallScreenWidth, isMediumScreenWidth, isLargeScreenWidth, isSmallScreen} = useWindowDimensions();
 
     const [isInModal, setIsInModal] = useState(Navigation.isDisplayedInModal());
 
     useEffect(() => {
         Navigation.isNavigationReady().then(() => {
-            const modalStatus = Navigation.isDisplayedInModal();
-            if (modalStatus !== isInModal) {
-                setIsInModal(modalStatus);
-            }
+            setIsInModal(Navigation.isDisplayedInModal());
         });
     }, []);
 
     const shouldUseNarrowLayout = isSmallScreenWidth || isInModal;
-    return {shouldUseNarrowLayout, isSmallScreenWidth, isInModal};
+    return {shouldUseNarrowLayout, isSmallScreenWidth, isInModal, isExtraSmallScreenHeight, isExtraSmallScreenWidth, isMediumScreenWidth, isLargeScreenWidth, isSmallScreen};
 }
