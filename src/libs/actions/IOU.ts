b88efc8f52a81744ecfde08a9a2b637675ee5d99
rory <rory@expensify.com>
2024-04-03 14:19:25 -0700

    Add comment explaining policyAdmin exception

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -467,6 +467,9 @@ function hasOutstandingChildRequest(report: OnyxTypes.Report, policy: OnyxEntry<
         return true;
     }
 
+    // If the user is a policy admin, they can approve and pay the report themselves.
+    // So even if the report will be automatically submitted, the next step would need to be done by them anyway.
+    // In this case, we show a green dot because they don't need to wait for harvesting to submit.
     if (ReportUtils.isExpenseReport(report) && PolicyUtils.isPolicyAdmin(policy)) {
         return true;
     }
@@ -5335,4 +5338,5 @@ export {
     canIOUBePaid,
     canApproveIOU,
     needsToBeManuallySubmitted,
+    hasOutstandingChildRequest,
 };
