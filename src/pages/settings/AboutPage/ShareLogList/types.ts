5194168e0c1ff0abe553ac5b18924e6a5631c16a
Lucas Mórawski <lukasz.morawski@callstack.com>
2024-04-04 16:55:52 +0200

    type fixes

--- src/pages/settings/AboutPage/ShareLogList/types.ts
+++ src/pages/settings/AboutPage/ShareLogList/types.ts
@@ -1,18 +1,10 @@
-import type {OnyxCollection} from 'react-native-onyx';
-import type {Report} from '@src/types/onyx';
-
-type BaseShareLogListOnyxProps = {
-    /** All reports shared with the user */
-    reports: OnyxCollection<Report>;
-};
-
 type ShareLogListProps = {
     /** The source of the log file to share */
     logSource: string;
 };
 
-type BaseShareLogListProps = BaseShareLogListOnyxProps & {
+type BaseShareLogListProps = {
     onAttachLogToReport: (reportID: string, filename: string) => void;
 };
 
-export type {BaseShareLogListOnyxProps, BaseShareLogListProps, ShareLogListProps};
+export type {BaseShareLogListProps, ShareLogListProps};
