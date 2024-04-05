efd0f793b1c34ce82f6c5ab169256c4d5800dd3c
Tomasz Lesniakiewicz <tlesniakiewicz@gmail.com>
2024-04-05 14:22:58 +0200

    chore: build gh actions

--- .github/actions/javascript/getGraphiteString/index.js
+++ .github/actions/javascript/getGraphiteString/index.js
@@ -2745,7 +2745,7 @@ const run = () => {
             const current = JSON.parse(entry);
             // Extract timestamp, Graphite accepts timestamp in seconds
             const timestamp = current.metadata?.creationDate ? Math.floor(new Date(current.metadata.creationDate).getTime() / 1000) : '';
-            if (current.name && current.meanDuration && current.meanCount) {
+            if (current.name && current.meanDuration && current.meanCount && timestamp) {
                 const formattedName = current.name.split(' ').join('-');
                 const renderDurationString = `${GRAPHITE_PATH}.${formattedName}.renderDuration ${current.meanDuration} ${timestamp}`;
                 const renderCountString = `${GRAPHITE_PATH}.${formattedName}.renderCount ${current.meanCount} ${timestamp}`;
