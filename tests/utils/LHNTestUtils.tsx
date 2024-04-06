a9c77e9e1f6b10333683addf134376f88e433f3c
Yauheni <HorbachZhenja@yandex.by>
2024-04-06 22:27:41 +0200

    Fix ts issue

--- tests/utils/LHNTestUtils.tsx
+++ tests/utils/LHNTestUtils.tsx
@@ -248,7 +248,7 @@ function getFakePolicy(id = '1', name = 'Workspace-Test-001'): Policy {
         owner: 'myuser@gmail.com',
         outputCurrency: 'BRL',
         avatar: '',
-        employeeList: [],
+        employeeList: {},
         isPolicyExpenseChatEnabled: true,
         lastModified: '1697323926777105',
         autoReporting: true,
