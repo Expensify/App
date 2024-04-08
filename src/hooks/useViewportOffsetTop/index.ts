57afe613dc4327c8a6db56168448a2a974b1b344
Cong Pham <ptcong411@gmail.com>
2024-04-09 01:18:34 +0700

    remove set timeout

--- src/hooks/useViewportOffsetTop/index.ts
+++ src/hooks/useViewportOffsetTop/index.ts
@@ -1,13 +1,11 @@
 import {useCallback, useEffect, useRef, useState} from 'react';
 import addViewportResizeListener from '@libs/VisualViewport';
-import CONST from '@src/CONST';
 
 /**
  * A hook that returns the offset of the top edge of the visual viewport
  */
 export default function useViewportOffsetTop(shouldAdjustScrollView = false): number {
     const [viewportOffsetTop, setViewportOffsetTop] = useState(0);
-    const initialHeight = useRef(0);
     const cachedDefaultOffsetTop = useRef<number>(0);
 
     const updateOffsetTop = useCallback(
@@ -16,9 +14,10 @@ export default function useViewportOffsetTop(shouldAdjustScrollView = false): nu
             if (event?.target instanceof VisualViewport) {
                 targetOffsetTop = event.target.offsetTop;
             }
-
-            if (shouldAdjustScrollView && window.visualViewport && initialHeight.current) {
-                const adjustScrollY = Math.round(initialHeight.current - window.visualViewport.height);
+            
+            if (shouldAdjustScrollView && window.visualViewport) {
+                const clientHeight = document.body.clientHeight;
+                const adjustScrollY = Math.round(clientHeight - window.visualViewport.height);
                 if (cachedDefaultOffsetTop.current === 0) {
                     cachedDefaultOffsetTop.current = targetOffsetTop;
                 }
@@ -31,20 +30,13 @@ export default function useViewportOffsetTop(shouldAdjustScrollView = false): nu
                     setViewportOffsetTop(targetOffsetTop);
                 }
             } else {
-                setViewportOffsetTop(0);
+                setViewportOffsetTop(targetOffsetTop);
             }
         },
         [shouldAdjustScrollView],
     );
 
     useEffect(() => addViewportResizeListener(updateOffsetTop), [shouldAdjustScrollView, updateOffsetTop]);
-
-    useEffect(() => {
-        setTimeout(() => {
-            initialHeight.current = window.visualViewport?.height ?? window.innerHeight;
-            updateOffsetTop();
-        }, CONST.TIMING.RESIZE_DEBOUNCE_TIME);
-    }, [updateOffsetTop]);
 
     useEffect(() => {
         if (!shouldAdjustScrollView) {
