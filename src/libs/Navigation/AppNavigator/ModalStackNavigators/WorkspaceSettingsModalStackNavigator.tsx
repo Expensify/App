69f1ccfcc5d925864c840c30c09920eab6ee600d
Etotaziba Olei <teneeto@gmail.com>
2024-04-09 22:48:56 +0100

    put back accounting stack

--- src/libs/Navigation/AppNavigator/ModalStackNavigators/WorkspaceSettingsModalStackNavigator.tsx
+++ src/libs/Navigation/AppNavigator/ModalStackNavigators/WorkspaceSettingsModalStackNavigator.tsx
@@ -52,6 +52,12 @@ function WorkspaceSettingsModalStackNavigator() {
             />
 
             <StackNavigator.Screen
+                key={SCREENS.WORKSPACE.ACCOUNTING}
+                name={SCREENS.WORKSPACE.ACCOUNTING}
+                getComponent={() => require('@pages/workspace/accounting/WorkspaceAccountingPage').default as React.ComponentType}
+            />
+
+            <StackNavigator.Screen
                 key={SCREENS.WORKSPACE.CATEGORIES}
                 name={SCREENS.WORKSPACE.CATEGORIES}
                 getComponent={() => require('@pages/workspace/categories/WorkspaceCategoriesPage').default as React.ComponentType}
