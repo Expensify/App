5756241f20290ba7ca215d9219500af642e5d29e
Yauheni <HorbachZhenja@yandex.by>
2024-04-10 12:08:46 +0200

    Remove SCREENS.MONEY_REQUEST.CURRENCY type

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -360,12 +360,6 @@ type MoneyRequestNavigatorParamList = {
         iouType: string;
         reportID: string;
     };
-    [SCREENS.MONEY_REQUEST.CURRENCY]: {
-        iouType: string;
-        reportID: string;
-        currency: string;
-        backTo: Routes;
-    };
     [SCREENS.MONEY_REQUEST.STEP_DATE]: {
         action: ValueOf<typeof CONST.IOU.ACTION>;
         iouType: ValueOf<typeof CONST.IOU.TYPE>;
