633d881e2a06f0cd0ae57648709ad54b6c7e2b23
Adam Grzybowski <adam.grzybowski@swmansion.com>
2024-04-10 18:28:54 +0200

    remove waitForNavigation and OnyxTabNavigator from SearchPageBottomTab

--- src/pages/Search/SearchPageBottomTab.tsx
+++ src/pages/Search/SearchPageBottomTab.tsx
@@ -34,54 +34,38 @@ function SearchPageBottomTab() {
         {
             title: 'All',
             icon: Expensicons.ExpensifyLogoNew,
-            action: singleExecution(waitForNavigate(() => Navigation.navigate(ROUTES.SEARCH.getRoute('all')))),
+            action: singleExecution(() => Navigation.navigate(ROUTES.SEARCH.getRoute('all'))),
         },
         {
             title: 'Sent',
             icon: Expensicons.ExpensifyLogoNew,
-            action: singleExecution(waitForNavigate(() => Navigation.navigate(ROUTES.SEARCH.getRoute('sent')))),
+            action: singleExecution(() => Navigation.navigate(ROUTES.SEARCH.getRoute('sent'))),
         },
         {
             title: 'Drafts',
             icon: Expensicons.ExpensifyLogoNew,
-            action: singleExecution(waitForNavigate(() => Navigation.navigate(ROUTES.SEARCH.getRoute('drafts')))),
+            action: singleExecution(() => Navigation.navigate(ROUTES.SEARCH.getRoute('drafts'))),
         },
     ];
 
     return (
         <ScreenWrapper testID="testPage">
-            {isSmallScreenWidth ? (
-                <OnyxTabNavigator
-                    id={CONST.TAB.NEW_CHAT_TAB_ID}
-                    tabBar={TabSelector}
-                    onTabSelected={(tab: string) => Navigation.navigate(ROUTES.SEARCH.getRoute(tab))}
-                >
-                    <TopTab.Screen name={CONST.TAB_SEARCH.ALL}>{() => <SearchResults filter="all" />}</TopTab.Screen>
-                    <TopTab.Screen name={CONST.TAB_SEARCH.SENT}>{() => <SearchResults filter="sent" />}</TopTab.Screen>
-                    <TopTab.Screen name={CONST.TAB_SEARCH.DRAFTS}>{() => <SearchResults filter="drafts" />}</TopTab.Screen>
-                </OnyxTabNavigator>
-            ) : (
-                <View style={[styles.pb4, styles.mh3, styles.mt3]}>
-                    {/*
-                                Ideally we should use MenuList component for MenuItems with singleExecution/Navigation actions.
-                                In this case where user can click on workspace avatar or menu items, we need to have a check for `isExecuting`. So, we are directly mapping menuItems.
-                            */}
-                    {searchMenuItems.map((item) => (
-                        <MenuItem
-                            key={item.title}
-                            disabled={false}
-                            interactive
-                            title={item.title}
-                            icon={item.icon}
-                            onPress={item.action}
-                            wrapperStyle={styles.sectionMenuItem}
-                            focused={item.title.toLowerCase() === currentQuery}
-                            hoverAndPressStyle={styles.hoveredComponentBG}
-                            isPaneMenu
-                        />
-                    ))}
-                </View>
-            )}
+            <View style={[styles.pb4, styles.mh3, styles.mt3]}>
+                {searchMenuItems.map((item) => (
+                    <MenuItem
+                        key={item.title}
+                        disabled={false}
+                        interactive
+                        title={item.title}
+                        icon={item.icon}
+                        onPress={item.action}
+                        wrapperStyle={styles.sectionMenuItem}
+                        focused={item.title.toLowerCase() === currentQuery}
+                        hoverAndPressStyle={styles.hoveredComponentBG}
+                        isPaneMenu
+                    />
+                ))}
+            </View>
         </ScreenWrapper>
     );
 }
