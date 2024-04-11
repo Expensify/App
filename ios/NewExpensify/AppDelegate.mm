ba2f7796dec8925d9de74e8a63a00221bc174a5f
MrRefactor <jakub.sys@syscrew.pl>
2024-04-11 15:33:06 +0800

    Initial upgrade

--- ios/NewExpensify/AppDelegate.mm
+++ ios/NewExpensify/AppDelegate.mm
@@ -69,10 +69,10 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
+  return [self bundleURL];
 }
 
-- (NSURL *)getBundleURL
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return
--- metro.config.js
+++ metro.config.js
@@ -11,7 +11,7 @@ const e2eSourceExts = ['e2e.js', 'e2e.ts'];
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
