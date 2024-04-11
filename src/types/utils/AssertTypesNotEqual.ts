0becc8aa333680d33c58529423e2e1a4e1b3f834
smelaa <aleksandra.smela@gmail.com>
2024-04-11 13:04:05 +0200

    Update comment

--- src/types/utils/AssertTypesNotEqual.ts
+++ src/types/utils/AssertTypesNotEqual.ts
@@ -4,7 +4,7 @@ type MatchError = 'Error: Types do match';
 
 /**
  * The 'AssertTypesNotEqual' type here enforces that `T1` and `T2` do not match.
- * If `T1` or `T2` are different this type will cause a compile-time error.
+ * If `T1` or `T2` are the same this type will cause a compile-time error.
  */
 type AssertTypesNotEqual<T1, T2 extends IsEqual<T1, T2> extends false ? T1 : TMatchError, TMatchError = MatchError> = T1 & T2;
 
