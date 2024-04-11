f8deb981da2798f05b8d848d6d122a16df78380a
Lucien Akchoté <lucien@expensify.com>
2024-04-11 15:35:00 +0200

    add GH to comment to add back the download button as soon as it's possible

--- src/pages/ShareCodePage.tsx
+++ src/pages/ShareCodePage.tsx
@@ -79,7 +79,8 @@ function ShareCodePage({report}: ShareCodePageProps) {
                     {/* 
                     This is a temporary measure because right now it's broken because of the Fabric update.
                     We need to wait for react-native v0.74 to be released so react-native-view-shot gets fixed.
-                    
+                    Please see https://github.com/Expensify/App/issues/40110 to see if it can be re-enabled.
+
                     <QRShareWithDownload
                         ref={qrCodeRef}
                         url={url}
--- src/pages/workspace/WorkspaceProfileSharePage.tsx
+++ src/pages/workspace/WorkspaceProfileSharePage.tsx
@@ -54,6 +54,7 @@ function WorkspaceProfileSharePage({policy}: WithPolicyProps) {
                         {/* 
                             This is a temporary measure because right now it's broken because of the Fabric update.
                             We need to wait for react-native v0.74 to be released so react-native-view-shot gets fixed.
+                            Please see https://github.com/Expensify/App/issues/40110 to see if it can be re-enabled.
 
                             <QRShareWithDownload
                             ref={qrCodeRef}
