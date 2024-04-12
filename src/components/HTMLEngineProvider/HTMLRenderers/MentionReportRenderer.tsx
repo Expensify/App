19b670aefa45ba2a49d07a19d6521462ff525619
war-in <war-in@users.noreply.github.com>
2024-04-12 12:39:58 +0200

    disable linking in non-policy chats

--- src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer.tsx
@@ -1,5 +1,5 @@
 import isEmpty from 'lodash/isEmpty';
-import React from 'react';
+import React, {useMemo} from 'react';
 import type {TextStyle} from 'react-native';
 import {StyleSheet} from 'react-native';
 import type {OnyxCollection, OnyxEntry} from 'react-native-onyx';
@@ -10,11 +10,14 @@ import Text from '@components/Text';
 import useCurrentReportID from '@hooks/useCurrentReportID';
 import useStyleUtils from '@hooks/useStyleUtils';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {getReport} from '@libs/ReportUtils';
+import * as ReportUtils from '@libs/ReportUtils';
 import Navigation from '@navigation/Navigation';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type {Report} from '@src/types/onyx';
+import type {EmptyObject} from '@src/types/utils/EmptyObject';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
 
 type RoomMentionOnyxProps = {
@@ -26,13 +29,13 @@ type MentionRoomRendererProps = RoomMentionOnyxProps & CustomRendererProps<TText
 
 const removeLeadingLTRAndHash = (value: string) => value.replace(CONST.UNICODE.LTR, '').slice(1);
 
-const getMentionDetails = (htmlAttributeReportID: string, currentReportID: string, reports: OnyxCollection<Report>, tnode: TText | TPhrasing) => {
+const getMentionDetails = (htmlAttributeReportID: string, currentReport: OnyxEntry<Report> | EmptyObject, reports: OnyxCollection<Report>, tnode: TText | TPhrasing) => {
     let reportID: string | undefined;
     let mentionDisplayText: string;
 
     // get mention details based on reportID from tag attribute
     if (!isEmpty(htmlAttributeReportID)) {
-        const report = reports?.['report_'.concat(htmlAttributeReportID)];
+        const report = getReport(htmlAttributeReportID);
 
         reportID = report?.reportID ?? undefined;
         mentionDisplayText = report?.reportName ?? report?.displayName ?? htmlAttributeReportID;
@@ -40,7 +43,6 @@ const getMentionDetails = (htmlAttributeReportID: string, currentReportID: strin
     } else if ('data' in tnode && !isEmptyObject(tnode.data)) {
         mentionDisplayText = removeLeadingLTRAndHash(tnode.data);
 
-        const currentReport = reports?.['report_'.concat(currentReportID)];
         // eslint-disable-next-line rulesdir/prefer-early-return
         Object.values(reports ?? {}).forEach((report) => {
             if (report?.policyID === currentReport?.policyID && removeLeadingLTRAndHash(report?.reportName ?? '') === mentionDisplayText) {
@@ -58,16 +60,19 @@ function MentionReportRenderer({style, tnode, TDefaultRenderer, reports, ...defa
     const styles = useThemeStyles();
     const StyleUtils = useStyleUtils();
     const htmlAttributeReportID = tnode.attributes.reportid;
-    const currentReportID = useCurrentReportID();
 
-    const mentionDetails = getMentionDetails(htmlAttributeReportID, currentReportID?.currentReportID ?? '', reports, tnode);
+    const currentReportID = useCurrentReportID();
+    const currentReport = getReport(currentReportID?.currentReportID);
+    const isGroupPolicyReport = useMemo(() => (currentReport && !isEmptyObject(currentReport) ? ReportUtils.isGroupPolicy(currentReport) : false), [currentReport]);
+
+    const mentionDetails = getMentionDetails(htmlAttributeReportID, currentReport, reports, tnode);
     if (!mentionDetails) {
         return null;
     }
-
     const {reportID, mentionDisplayText} = mentionDetails;
-    const navigationRoute = reportID ? ROUTES.REPORT_WITH_ID.getRoute(String(reportID)) : undefined;
-    const isCurrentRoomMention = String(reportID) === currentReportID?.currentReportID;
+
+    const navigationRoute = reportID ? ROUTES.REPORT_WITH_ID.getRoute(reportID) : undefined;
+    const isCurrentRoomMention = reportID === currentReportID?.currentReportID;
 
     const flattenStyle = StyleSheet.flatten(style as TextStyle);
     const {color, ...styleWithoutColor} = flattenStyle;
@@ -78,18 +83,22 @@ function MentionReportRenderer({style, tnode, TDefaultRenderer, reports, ...defa
                 <Text
                     // eslint-disable-next-line react/jsx-props-no-spreading
                     {...defaultRendererProps}
-                    style={[styles.link, styleWithoutColor, StyleUtils.getMentionStyle(isCurrentRoomMention), {color: StyleUtils.getMentionTextColor(isCurrentRoomMention)}]}
+                    style={
+                        isGroupPolicyReport
+                            ? [styles.link, styleWithoutColor, StyleUtils.getMentionStyle(isCurrentRoomMention), {color: StyleUtils.getMentionTextColor(isCurrentRoomMention)}]
+                            : []
+                    }
                     suppressHighlighting
                     onPress={
-                        navigationRoute
+                        navigationRoute && isGroupPolicyReport
                             ? (event) => {
                                   event.preventDefault();
                                   Navigation.navigate(navigationRoute);
                               }
                             : undefined
                     }
-                    role={CONST.ROLE.LINK}
-                    accessibilityLabel={`/${navigationRoute}`}
+                    role={isGroupPolicyReport ? CONST.ROLE.LINK : undefined}
+                    accessibilityLabel={isGroupPolicyReport ? `/${navigationRoute}` : undefined}
                 >
                     #{mentionDisplayText}
                 </Text>
