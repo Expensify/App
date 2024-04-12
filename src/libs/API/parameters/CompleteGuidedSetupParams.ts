4f291f065706f049c42249fc9458ecd7703cd8d7
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-04-12 12:21:47 +0200

    simplify CompleteGuidedSetupParams

--- src/libs/API/parameters/CompleteGuidedSetupParams.ts
+++ src/libs/API/parameters/CompleteGuidedSetupParams.ts
@@ -1,11 +1,8 @@
-import type {ValueOf} from 'type-fest';
-import type CONST from '@src/CONST';
-
 type CompleteGuidedSetupParams = {
     firstName: string;
     lastName: string;
     guidedSetupData: string;
-    engagementChoice: ValueOf<typeof CONST.ONBOARDING_CHOICES>;
+    engagementChoice: string;
 };
 
 export default CompleteGuidedSetupParams;
