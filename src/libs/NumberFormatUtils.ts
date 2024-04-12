609d0bdc01f5db0185c4bce2d7c0bfa2fd761885
Janic Duplessis <janicduplessis@gmail.com>
2024-04-11 23:44:31 -0400

    Cache NumberFormat instances

--- src/libs/NumberFormatUtils.ts
+++ src/libs/NumberFormatUtils.ts
@@ -1,12 +1,25 @@
+import stableStringify from 'fast-json-stable-stringify';
 import type {ValueOf} from 'type-fest';
 import type CONST from '@src/CONST';
 
+const cache = new Map<string, Intl.NumberFormat>();
+
+function getNumberFormat(locale: ValueOf<typeof CONST.LOCALES>, options?: Intl.NumberFormatOptions): Intl.NumberFormat {
+    const key = `${locale}-${stableStringify(options)}`;
+    let numberFormat = cache.get(key);
+    if (!numberFormat) {
+        numberFormat = new Intl.NumberFormat(locale, options);
+        cache.set(key, numberFormat);
+    }
+    return numberFormat;
+}
+
 function format(locale: ValueOf<typeof CONST.LOCALES>, number: number, options?: Intl.NumberFormatOptions): string {
-    return new Intl.NumberFormat(locale, options).format(number);
+    return getNumberFormat(locale, options).format(number);
 }
 
 function formatToParts(locale: ValueOf<typeof CONST.LOCALES>, number: number, options?: Intl.NumberFormatOptions): Intl.NumberFormatPart[] {
-    return new Intl.NumberFormat(locale, options).formatToParts(number);
+    return getNumberFormat(locale, options).formatToParts(number);
 }
 
 export {format, formatToParts};
