9a2d4e14f61f83a041997435a090a1a70fbd4a27
Lucien Akchoté <lucien@expensify.com>
2024-04-12 12:21:55 +0200

    Revert "Merge pull request #40109 from Expensify/lucien/temp-fix-qrCode-downloadBtn"
    This reverts commit 4e4f55ab148e09f08a41381575c536e630614f59, reversing
    changes made to bbc676100f7309a4495307885d0c6823b2825d47.

--- src/pages/workspace/WorkspaceProfileSharePage.tsx
+++ src/pages/workspace/WorkspaceProfileSharePage.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {View} from 'react-native';
+import type {ImageSourcePropType} from 'react-native';
+import expensifyLogo from '@assets/images/expensify-logo-round-transparent.png';
 import ContextMenuItem from '@components/ContextMenuItem';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Expensicons from '@components/Icon/Expensicons';
+import MenuItem from '@components/MenuItem';
 import {useSession} from '@components/OnyxProvider';
+import QRShareWithDownload from '@components/QRShare/QRShareWithDownload';
+import type QRShareWithDownloadHandle from '@components/QRShare/QRShareWithDownload/types';
 import ScreenWrapper from '@components/ScreenWrapper';
 import ScrollView from '@components/ScrollView';
 import useEnvironment from '@hooks/useEnvironment';
@@ -12,7 +17,9 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import Clipboard from '@libs/Clipboard';
 import Navigation from '@libs/Navigation/Navigation';
+import shouldAllowDownloadQRCode from '@libs/shouldAllowDownloadQRCode';
 import * as Url from '@libs/Url';
+import CONST from '@src/CONST';
 import ROUTES from '@src/ROUTES';
 import withPolicy from './withPolicy';
 import type {WithPolicyProps} from './withPolicy';
@@ -21,9 +28,11 @@ function WorkspaceProfileSharePage({policy}: WithPolicyProps) {
     const themeStyles = useThemeStyles();
     const {translate} = useLocalize();
     const {environmentURL} = useEnvironment();
+    const qrCodeRef = useRef<QRShareWithDownloadHandle>(null);
     const {isSmallScreenWidth} = useWindowDimensions();
     const session = useSession();
 
+    const policyName = policy?.name ?? '';
     const id = policy?.id ?? '';
     const adminEmail = session?.email ?? '';
     const urlWithTrailingSlash = Url.addTrailingForwardSlash(environmentURL);
@@ -41,13 +50,16 @@ function WorkspaceProfileSharePage({policy}: WithPolicyProps) {
             />
             <ScrollView style={[themeStyles.flex1, themeStyles.pt2]}>
                 <View style={[themeStyles.flex1, isSmallScreenWidth ? themeStyles.workspaceSectionMobile : themeStyles.workspaceSection]}>
-                    {/* 
-                        Right now QR code download button is not shown anymore
-                        This is a temporary measure because right now it's broken because of the Fabric update.
-                        We need to wait for react-native v0.74 to be released so react-native-view-shot gets fixed.
-                        
-                        Please see https://github.com/Expensify/App/issues/40110 to see if it can be re-enabled.
-                    */}
+                    <View style={[themeStyles.workspaceSectionMobile, themeStyles.ph9]}>
+                        <QRShareWithDownload
+                            ref={qrCodeRef}
+                            url={url}
+                            title={policyName}
+                            logo={(policy?.avatar ? policy.avatar : expensifyLogo) as ImageSourcePropType}
+                            logoRatio={CONST.QR.DEFAULT_LOGO_SIZE_RATIO}
+                            logoMarginRatio={CONST.QR.DEFAULT_LOGO_MARGIN_RATIO}
+                        />
+                    </View>
 
                     <View style={[themeStyles.mt3, themeStyles.ph4]}>
                         <ContextMenuItem
@@ -60,6 +72,15 @@ function WorkspaceProfileSharePage({policy}: WithPolicyProps) {
                             shouldLimitWidth={false}
                             wrapperStyle={themeStyles.sectionMenuItemTopDescription}
                         />
+                        {shouldAllowDownloadQRCode && (
+                            <MenuItem
+                                isAnonymousAction
+                                title={translate('common.download')}
+                                icon={Expensicons.Download}
+                                onPress={() => qrCodeRef.current?.download?.()}
+                                wrapperStyle={themeStyles.sectionMenuItemTopDescription}
+                            />
+                        )}
                     </View>
                 </View>
             </ScrollView>
