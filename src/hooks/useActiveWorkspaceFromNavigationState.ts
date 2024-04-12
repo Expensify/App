4b1681c76db57613511ac7bbe4be900bbe7954fd
Wojciech Boman <wojciechboman@gmail.com>
2024-04-12 08:44:20 +0200

    Add a comment to useActiveWorkspaceFromNavigationState

--- src/hooks/useActiveWorkspaceFromNavigationState.ts
+++ src/hooks/useActiveWorkspaceFromNavigationState.ts
@@ -5,6 +5,7 @@ import type {BottomTabNavigatorParamList} from '@libs/Navigation/types';
  *  Get the currently selected policy ID stored in the navigation state. This hook should only be called only from screens in BottomTab.
  */
 function useActiveWorkspaceFromNavigationState() {
+    // The last policyID value is always stored in the last route in BottomTabNavigator.
     const activeWorkpsaceID = useNavigationState<BottomTabNavigatorParamList, string | undefined>((state) => state.routes.at(-1)?.params?.policyID);
 
     return activeWorkpsaceID;
