36ccc936df58fd041f097c2e2fe4290ab339d0af
FitseTLT <engfitsum@gmail.com>
2024-04-15 14:09:27 +0300

    minor fix

--- src/components/Lightbox/index.tsx
+++ src/components/Lightbox/index.tsx
@@ -168,7 +168,7 @@ function Lightbox({isAuthTokenRequired = false, uri, onScaleChanged: onScaleChan
             return;
         }
         isLightboxImageLoaded.current = false;
-        setLightboxImageLoading(false);
+        setLightboxImageLoading(true);
         setContentSize(undefined);
     }, [isLightboxVisible, setContentSize]);
 
