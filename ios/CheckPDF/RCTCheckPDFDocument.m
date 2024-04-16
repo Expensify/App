f7ded6e251d5110ccc9319d123c487dffacc5b70
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-04-16 16:26:02 +0200

    fix: added comments

--- ios/CheckPDF/RCTCheckPDFDocument.m
+++ ios/CheckPDF/RCTCheckPDFDocument.m
@@ -14,19 +14,33 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(checkPdf:(NSString *)fileurl callback: (RCTResponseSenderBlock)callback)
 {
+  // Convert the file URL from a string to an NSURL object
   NSURL *url = [NSURL URLWithString:fileurl];
+
   NSFileManager *fileManager = [NSFileManager defaultManager];
+
+  // Read the contents of the file at the URL into an NSData object
   NSData *ns = [NSData dataWithContentsOfURL:url];
-      if ([ns length] >= 1024) {
-          uint8_t pdfBytes[] = { 0x25, 0x50, 0x44, 0x46 };
-          NSData *pdfHeader = [NSData dataWithBytes:pdfBytes length:4];
-          NSRange range = [ns rangeOfData:pdfHeader options:NSDataSearchAnchored range:NSMakeRange(0, 1024)];
-          if (range.length > 0) {
-            callback(@[@YES]);
-          }
-          else {
-            callback(@[@NO]);
-          }
-      }
+
+  // Check if the file is at least 1024 bytes
+  if ([ns length] >= 1024) {
+    // Define the first four bytes of a PDF file
+    uint8_t pdfBytes[] = { 0x25, 0x50, 0x44, 0x46 };
+
+    // Create an NSData object with the PDF bytes
+    NSData *pdfHeader = [NSData dataWithBytes:pdfBytes length:4];
+
+    // Check if the first 1024 bytes of the file contain the PDF header
+    NSRange range = [ns rangeOfData:pdfHeader options:NSDataSearchAnchored range:NSMakeRange(0, 1024)];
+
+    // If the PDF header is found, call the callback with YES
+    if (range.length > 0) {
+      callback(@[@YES]);
+    }
+    // If the PDF header is not found, call the callback with NO
+    else {
+      callback(@[@NO]);
+    }
+  }
 }
 @end
