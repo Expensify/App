0c77c4fefd232c429e25767d5accdf2a832ec317
Aldo Canepa <aldo@expensify.com>
2024-04-16 16:06:35 -0700

    Style

--- src/types/onyx/Request.ts
+++ src/types/onyx/Request.ts
@@ -21,7 +21,7 @@ type RequestData = {
     finallyData?: OnyxUpdate[];
     resolve?: (value: Response) => void;
     reject?: (value?: unknown) => void;
-    skipWebProxy?: boolean,
+    skipWebProxy?: boolean;
 };
 
 type Request = RequestData & OnyxData;
