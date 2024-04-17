11647a33a96b3fa5a2976f814da51257126eee7f
Jack Nam <jack@expensify.com>
2024-04-17 16:42:52 -0700

    Use FlatList in PaymentMethodList

--- src/pages/settings/Wallet/PaymentMethodList.tsx
+++ src/pages/settings/Wallet/PaymentMethodList.tsx
@@ -1,9 +1,8 @@
-import {FlashList} from '@shopify/flash-list';
 import lodashSortBy from 'lodash/sortBy';
 import type {ReactElement, Ref} from 'react';
 import React, {useCallback, useMemo} from 'react';
 import type {GestureResponderEvent, StyleProp, ViewStyle} from 'react-native';
-import {View} from 'react-native';
+import {FlatList, View} from 'react-native';
 import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
 import type {SvgProps} from 'react-native-svg/lib/typescript/ReactNativeSVG';
@@ -336,7 +335,7 @@ function PaymentMethodList({
     return (
         <>
             <View style={[style, {minHeight: (filteredPaymentMethods.length + (shouldShowAddBankAccount ? 1 : 0)) * variables.optionRowHeight}]}>
-                <FlashList
+                <FlatList
                     estimatedItemSize={variables.optionRowHeight}
                     data={filteredPaymentMethods}
                     renderItem={renderItem}
