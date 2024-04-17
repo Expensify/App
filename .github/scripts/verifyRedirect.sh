5363920f85cd7c7bc99caeb27e7cc9578112920a
Rushat Gabhane <rushatgabhane@gmail.com>
2024-04-18 01:31:31 +0530

    add workflow to validate redirects.csv

---
+++ .github/scripts/verifyRedirect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# HelpDot - Verifies that redirects.csv does not have any duplicates
+# Duplicate sourceURLs break redirection on cloudflare pages
+
+duplicates=$(awk -F, 'a[$1]++{print "duplicate redirects are not allowed: " $1}' ../../docs/redirects.csv)
+
+if [[ -z "$duplicates" ]]; then
+    exit 0
+fi
+
+echo $duplicates
+exit 1
