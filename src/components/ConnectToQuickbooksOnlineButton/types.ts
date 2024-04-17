d1f621244317d0d8dcaa8aa943fa89b9bcba7702
Aldo Canepa Garay <87341702+aldo-expensify@users.noreply.github.com>
2024-04-17 11:31:55 -0700

    Remove comment
    Co-authored-by: Marc Glasser <marc@expensify.com>

--- src/components/ConnectToQuickbooksOnlineButton/types.ts
+++ src/components/ConnectToQuickbooksOnlineButton/types.ts
@@ -2,6 +2,5 @@ type ConnectToQuickbooksOnlineButtonProps = {
     policyID: string;
 };
 
-// More types will be added later
 // eslint-disable-next-line import/prefer-default-export
 export type {ConnectToQuickbooksOnlineButtonProps};
