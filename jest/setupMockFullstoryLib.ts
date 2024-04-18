93ff19f5b3a519a464bf5960c72f510aa22781e4
Oleksii Vagin <oleksii@leanconvert.com>
2024-04-18 14:39:12 +0200

    Fullstory integration. - React-Native Android - React-Native IOS - Web
    Functionality included:
    - Session recording
    - Essential Identity resolution
    
    Functionality added:
    - FSPage - Mobile Pages API
    
    Platforms affected:
    - Android
    - IOs
    
    Typescript types definitions added.
    Unit test fixed, fixed eslint errors.
    
    Signed-off-by: Oleksii Vagin <oleksii@leanconvert.com>

---
+++ jest/setupMockFullstoryLib.ts
@@ -0,0 +1,18 @@
+export default function mockFSLibrary() {
+    jest.mock('@fullstory/react-native', () => {
+        class Fullstory {
+            consent = jest.fn();
+
+            anonymize = jest.fn();
+
+            identify = jest.fn();
+        }
+
+        return {
+            FSPage(){
+                this.start = jest.fn()
+            },
+            default: Fullstory,
+        };
+    });
+}
