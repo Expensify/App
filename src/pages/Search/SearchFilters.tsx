a00ef2c85a74671c1361873d9c3da6be3d0ba5fc
Mateusz Titz <titzmateusz@gmail.com>
2024-04-18 12:18:13 +0200

    Add first version of SearchFilters component

---
+++ src/pages/Search/SearchFilters.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {View} from 'react-native';
+import MenuItem from '@components/MenuItem';
+import TabSelectorItem from '@components/TabSelector/TabSelectorItem';
+import useActiveRoute from '@hooks/useActiveRoute';
+import useSingleExecution from '@hooks/useSingleExecution';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
+import useWindowDimensions from '@hooks/useWindowDimensions';
+import Navigation from '@libs/Navigation/Navigation';
+import * as Expensicons from '@src/components/Icon/Expensicons';
+import CONST from '@src/CONST';
+import type {Route} from '@src/ROUTES';
+import ROUTES from '@src/ROUTES';
+import type IconAsset from '@src/types/utils/IconAsset';
+
+type SearchMenuItem = {
+    title: string;
+    icon: IconAsset;
+    route: Route;
+};
+
+const searchMenuItems: SearchMenuItem[] = [
+    {
+        title: 'All',
+        icon: Expensicons.All,
+        route: ROUTES.SEARCH.getRoute(CONST.TAB_SEARCH.ALL),
+    },
+    // More tabs prepared for final version but in v1 we support only "All"
+    // {
+    //     title: 'Sent',
+    //     icon: Expensicons.Send,
+    //     route: ROUTES.SEARCH.getRoute(CONST.TAB_SEARCH.SENT),
+    // },
+    // {
+    //     title: 'Drafts',
+    //     icon: Expensicons.Pencil,
+    //     route: ROUTES.SEARCH.getRoute(CONST.TAB_SEARCH.DRAFTS),
+    // },
+];
+
+function SearchFilters() {
+    const theme = useTheme();
+    const styles = useThemeStyles();
+    const {singleExecution} = useSingleExecution();
+    const activeRoute = useActiveRoute();
+    const {isSmallScreenWidth} = useWindowDimensions();
+
+    const currentQuery = activeRoute?.params && 'query' in activeRoute.params ? activeRoute?.params?.query : '';
+    const flexDirection = isSmallScreenWidth && styles.flexRow;
+
+    return (
+        <View style={[styles.pb4, styles.mh3, styles.mt3, flexDirection]}>
+            {searchMenuItems.map((item) => {
+                const isActive = item.title.toLowerCase() === currentQuery;
+                const onPress = singleExecution(() => Navigation.navigate(item.route));
+
+                if (isSmallScreenWidth) {
+                    return (
+                        <TabSelectorItem
+                            key={item.title}
+                            icon={item.icon}
+                            title={item.title}
+                            isActive={isActive}
+                            activeOpacity={isActive ? 1 : 0}
+                            backgroundColor={isActive ? theme.border : theme.appBG}
+                            onPress={onPress}
+                        />
+                    );
+                }
+
+                return (
+                    <MenuItem
+                        key={item.title}
+                        disabled={false}
+                        interactive
+                        title={item.title}
+                        icon={item.icon}
+                        wrapperStyle={styles.sectionMenuItem}
+                        focused={isActive}
+                        hoverAndPressStyle={styles.hoveredComponentBG}
+                        onPress={onPress}
+                        isPaneMenu
+                    />
+                );
+            })}
+        </View>
+    );
+}
+
+SearchFilters.displayName = 'SearchFilters';
+
+export default SearchFilters;
--- src/pages/Search/SearchPageBottomTab.tsx
+++ src/pages/Search/SearchPageBottomTab.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import {View} from 'react-native';
-import MenuItem from '@components/MenuItem';
 import ScreenWrapper from '@components/ScreenWrapper';
-import useActiveRoute from '@hooks/useActiveRoute';
-import useSingleExecution from '@hooks/useSingleExecution';
-import useThemeStyles from '@hooks/useThemeStyles';
-import Navigation from '@libs/Navigation/Navigation';
-import * as Expensicons from '@src/components/Icon/Expensicons';
-import CONST from '@src/CONST';
-import ROUTES from '@src/ROUTES';
+import SearchFilters from './SearchFilters';
 import type IconAsset from '@src/types/utils/IconAsset';
 
 // import EmptySearchView from './EmptySearchView';
@@ -20,48 +12,11 @@ type SearchMenuItem = {
 };
 
 function SearchPageBottomTab() {
-    const styles = useThemeStyles();
-    const {singleExecution} = useSingleExecution();
-    const activeRoute = useActiveRoute();
-    const currentQuery = activeRoute?.params && 'query' in activeRoute.params ? activeRoute?.params?.query : '';
-
-    const searchMenuItems: SearchMenuItem[] = [
-        {
-            title: 'All',
-            icon: Expensicons.ExpensifyLogoNew,
-            action: singleExecution(() => Navigation.navigate(ROUTES.SEARCH.getRoute(CONST.TAB_SEARCH.ALL))),
-        },
-        {
-            title: 'Sent',
-            icon: Expensicons.ExpensifyLogoNew,
-            action: singleExecution(() => Navigation.navigate(ROUTES.SEARCH.getRoute(CONST.TAB_SEARCH.SENT))),
-        },
-        {
-            title: 'Drafts',
-            icon: Expensicons.ExpensifyLogoNew,
-            action: singleExecution(() => Navigation.navigate(ROUTES.SEARCH.getRoute(CONST.TAB_SEARCH.DRAFTS))),
-        },
-    ];
-
     return (
         <ScreenWrapper testID={SearchPageBottomTab.displayName}>
-            <View style={[styles.pb4, styles.mh3, styles.mt3]}>
-                {searchMenuItems.map((item) => (
-                    <MenuItem
-                        key={item.title}
-                        disabled={false}
-                        interactive
-                        title={item.title}
-                        icon={item.icon}
-                        onPress={item.action}
-                        wrapperStyle={styles.sectionMenuItem}
-                        focused={item.title.toLowerCase() === currentQuery}
-                        hoverAndPressStyle={styles.hoveredComponentBG}
-                        isPaneMenu
-                    />
-                ))}
-            </View>
             {/* <EmptySearchView /> */}
+            <SearchFilters />
+            {/*  Search results list goes here  */}
         </ScreenWrapper>
     );
 }
