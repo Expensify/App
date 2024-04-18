695720dc7ce99ef7b8af8bfbdf84220b26ebcfef
nkdengineer <nkdengineer@outlook.com>
2024-04-18 14:37:35 +0700

    revert mistake change

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -2318,7 +2318,7 @@ export default {
     roomMembersPage: {
         memberNotFound: 'Member not found. To invite a new member to the room, please use the Invite button above.',
         notAuthorized: `You do not have access to this page. Are you trying to join the room? Please reach out to a member of this room so they can add you as a member! Something else? Reach out to ${CONST.EMAIL.CONCIERGE}`,
-        removeMembersPrompt: 'Are you sure you want to remove the selecmoneyRequestConfirmationListted members from the room?',
+        removeMembersPrompt: 'Are you sure you want to remove the selected members from the room?',
     },
     newTaskPage: {
         assignTask: 'Assign task',
