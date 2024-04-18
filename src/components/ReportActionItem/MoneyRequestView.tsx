f4c533792f0b6c62612e9d05981a4b813084f7ac
Kaushik Kapadiya <kapadiyakaushik9@gmail.com>
2024-04-18 18:14:58 +0530

    Show error message on transaction thread

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -255,7 +255,7 @@ function MoneyRequestView({
         [transactionAmount, isSettled, isCancelled, isPolicyExpenseChat, isEmptyMerchant, transactionDate, hasErrors, canUseViolations, hasViolations, translate, getViolationsForField],
     );
 
-    let errors = transaction?.errors ?? {};
+    let errors: Errors | ReceiptErrors = transaction?.errors ?? {};
     if (parentReportAction?.errors) {
         errors = {
             ...errors,
