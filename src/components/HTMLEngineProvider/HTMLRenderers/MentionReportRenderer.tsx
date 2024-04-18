d99c451ec0034cae9a80057af97b1f394df65e3c
Rodrigo Lino da Costa <rodrigo@expensify.com>
2024-04-18 18:11:45 -0300

    better comment in rendere

--- src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer.tsx
@@ -63,8 +63,8 @@ function MentionReportRenderer({style, tnode, TDefaultRenderer, reports, ...defa
 
     const currentReportID = useCurrentReportID();
     const currentReport = getReport(currentReportID?.currentReportID);
-    
-    // When we invite someone to a room they don't have the policy type, but we still want them to be able to mention other reports they are members of, so we also check the reportType in case the policyType was empty.
+
+    // When we invite someone to a room they don't have the policy type, but we still want them to be able see and click on report mentions, so we also check the reportType in case the policyType was empty.
     const isGroupPolicyReport = useMemo(() => (currentReport && !isEmptyObject(currentReport) ? (ReportUtils.isGroupPolicy(currentReport) || ReportUtils.isChatRoom(currentReport)) : false), [currentReport]);
 
     const mentionDetails = getMentionDetails(htmlAttributeReportID, currentReport, reports, tnode);
