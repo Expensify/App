2a01d46114f9abf2a32d03b02db4e9b180401936
Tim Golen <tgolen@expensify.com>
2024-04-18 10:01:33 -0600

    Use simplified type

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -2489,7 +2489,7 @@ const getConvertTrackedExpenseInformation = (
     linkedTrackedExpenseReportAction: OnyxTypes.ReportAction,
     linkedTrackedExpenseReportID: string,
     transactionThreadReportID: string,
-    resolution: Omit<ValueOf<typeof CONST.IOU.ACTION>, 'edit' | 'create'>,
+    resolution: IOUAction,
 ) => {
     const optimisticData: OnyxUpdate[] = [];
     const successData: OnyxUpdate[] = [];
