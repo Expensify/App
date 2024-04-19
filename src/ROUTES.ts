7e90c81446e3f1032da29857c8a07d2adc485ee5
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-04-19 12:57:29 +0200

    feat: added new routes

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -707,6 +707,30 @@ const ROUTES = {
         route: 'r/:reportID/transaction/:transactionID/receipt',
         getRoute: (reportID: string, transactionID: string) => `r/${reportID}/transaction/${transactionID}/receipt` as const,
     },
+    TRANSACTION_DUPLICATE_REVIEW_PAGE: {
+        route: 'r/:threadReportID/duplicates/review',
+        getRoute: (threadReportID: string) => `/r/${threadReportID}/duplicates/review`,
+    },
+    TRANSACTION_DUPLICATE_REVIEW_MERCHANT_PAGE: {
+        route: 'r/:threadReportID/duplicates/review/merchant',
+        getRoute: (threadReportID: string) => `/r/${threadReportID}/duplicates/review/merchant`,
+    },
+    TRANSACTION_DUPLICATE_REVIEW_CATEGORY_PAGE: {
+        route: 'r/:threadReportID/duplicates/review/category',
+        getRoute: (threadReportID: string) => `/r/${threadReportID}/duplicates/review/category`,
+    },
+    TRANSACTION_DUPLICATE_REVIEW_TAG_PAGE: {
+        route: 'r/:threadReportID/duplicates/review/tag',
+        getRoute: (threadReportID: string) => `/r/${threadReportID}/duplicates/review/tag`,
+    },
+    TRANSACTION_DUPLICATE_REVIEW_DESCRIPTION_PAGE: {
+        route: 'r/:threadReportID/duplicates/confirm',
+        getRoute: (threadReportID: string) => `/r/${threadReportID}/duplicates/confirm`,
+    },
+    TRANSACTION_DUPLICATE_CONFIRM: {
+        route: 'r/:threadReportID/duplicates/review/description',
+        getRoute: (threadReportID: string) => `/r/${threadReportID}/duplicates/review/description`,
+    },
     POLICY_ACCOUNTING_QUICKBOOKS_ONLINE_IMPORT: {
         route: 'settings/workspaces/:policyID/accounting/quickbooks-online/import',
         getRoute: (policyID: string) => `settings/workspaces/${policyID}/accounting/quickbooks-online/import` as const,
