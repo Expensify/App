73ea3bd7e1905e7fbdfaf0c6f9ee116285c4c48f
Jack Nam <30609178+thienlnam@users.noreply.github.com>
2024-04-19 10:35:13 -0700

    Merge pull request #40471 from Krishna2323/krishna2323/follow-up/37435
    fix: Submit Expense - Error message about corrupted file when dragging and dropping image receipt
    (cherry picked from commit 86993153490d91ffa4b6ed19d8323339452d2d8d)

--- src/pages/iou/request/step/IOURequestStepScan/index.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.tsx
@@ -481,6 +481,7 @@ function IOURequestStepScan({
                     onDrop={(e) => {
                         const file = e?.dataTransfer?.files[0];
                         if (file) {
+                            file.uri = URL.createObjectURL(file);
                             setReceiptAndNavigate(file);
                         }
                     }}
