422e872b926603c65bf568c5b20869a97eca9444
Blazej Kustra <blazej.kustra@swmansion.com>
2024-04-23 11:03:48 +0200

    Update tests eslint config

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
     rules: {
         'no-import-assign': 'off',
+        '@lwc/lwc/no-async-await': 'off',
+        'no-await-in-loop': 'off',
+        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+
+        // This helps disable the `prefer-alias` rule for tests
+        '@dword-design/import-alias/prefer-alias': ['off'],
     },
 };
