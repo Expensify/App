a8949461763278351f058552578b10e8a5711c8d
Hayata Suenaga <hayata@expensify.com>
2024-04-24 17:03:58 -0400

    feat: add spanish translation

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -1980,6 +1980,7 @@ export default {
             deleteFailureMessage: 'Se ha producido un error al intentar eliminar la categoría. Por favor, inténtalo más tarde.',
             categoryName: 'Nombre de la categoría',
             requiresCategory: 'Los miembros deben categorizar todos los gastos',
+            needCategoryForExportToIntegration: 'Se requiere una categoría en cada gasto para poder exportarlo a',
             subtitle: 'Obtén una visión general de dónde te gastas el dinero. Utiliza las categorías predeterminadas o añade las tuyas propias.',
             emptyCategories: {
                 title: 'No has creado ninguna categoría',
