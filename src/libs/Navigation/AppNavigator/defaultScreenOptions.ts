a2f16c3f55bd3cb3e170ea9e3675b6733b19a165
Christoph Pader <chris@margelo.io>
2024-04-24 19:10:59 +0200

    fix: defaultScreenOptions

--- src/libs/Navigation/AppNavigator/defaultScreenOptions.ts
+++ src/libs/Navigation/AppNavigator/defaultScreenOptions.ts
@@ -1,12 +1,14 @@
 import type {PlatformStackNavigationOptions} from '@libs/Navigation/PlatformStackNavigation/types';
 
 const defaultScreenOptions: PlatformStackNavigationOptions = {
-    cardStyle: {
-        overflow: 'visible',
-        flex: 1,
-    },
     headerShown: false,
     animationTypeForReplace: 'push',
+    webOnly: {
+        cardStyle: {
+            overflow: 'visible',
+            flex: 1,
+        },
+    },
 };
 
 export default defaultScreenOptions;
