2e551c7613f64717aeb00ccf50fa77288c8c1f0a
dragnoir <mosaixel.org@gmail.com>
2024-04-24 21:31:57 +0100

    undo MoneyRequestHeaderStatusBar

--- src/components/MoneyRequestHeaderStatusBar.tsx
+++ src/components/MoneyRequestHeaderStatusBar.tsx
@@ -12,21 +12,15 @@ type MoneyRequestHeaderStatusBarProps = {
 
     /** Whether we show the border bottom */
     shouldShowBorderBottom: boolean;
-
-    /** Red Badge background */
-    danger?: boolean;
 };
 
-function MoneyRequestHeaderStatusBar({title, description, shouldShowBorderBottom, danger}: MoneyRequestHeaderStatusBarProps) {
+function MoneyRequestHeaderStatusBar({title, description, shouldShowBorderBottom}: MoneyRequestHeaderStatusBarProps) {
     const styles = useThemeStyles();
     const borderBottomStyle = shouldShowBorderBottom ? styles.borderBottom : {};
-    const badgeBackgroundColorStyle = danger ? styles.moneyRequestHeaderStatusBarBadgeDangerBackground : styles.moneyRequestHeaderStatusBarBadgeBackground;
-    const badgeTextColorStyle = danger ? styles.textMicroBoldDangerColor : styles.textMicroBoldColor;
-
     return (
         <View style={[styles.dFlex, styles.flexRow, styles.alignItemsCenter, styles.flexGrow1, styles.overflowHidden, styles.ph5, styles.pb3, borderBottomStyle]}>
-            <View style={[styles.moneyRequestHeaderStatusBarBadge, badgeBackgroundColorStyle]}>
-                <Text style={[styles.textStrong, styles.textMicroBold, badgeTextColorStyle]}>{title}</Text>
+            <View style={[styles.moneyRequestHeaderStatusBarBadge]}>
+                <Text style={[styles.textStrong, styles.textMicroBold]}>{title}</Text>
             </View>
             <View style={[styles.flexShrink1]}>
                 <Text style={[styles.textLabelSupporting]}>{description}</Text>
