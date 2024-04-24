2d472a66f0ab753adbecfd60b96f71dc2b02d2a2
Viktor Kenyz <skakruk15@gmail.com>
2024-04-24 19:12:24 +0300

    Add tracking of closed clients

--- src/libs/ActiveClientManager/index.ts
+++ src/libs/ActiveClientManager/index.ts
@@ -7,10 +7,11 @@ import Str from 'expensify-common/lib/str';
 import Onyx from 'react-native-onyx';
 import * as ActiveClients from '@userActions/ActiveClients';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {Init, IsClientTheLeader, IsReady} from './types';
+import type { Init, IsClientTheLeader, IsReady } from './types';
 
 const clientID = Str.guid();
 const maxClients = 20;
+const ACTIVE_CLIENT_LEFT_KEY = 'activeClientLeft';
 let activeClients: string[] = [];
 let resolveSavedSelfPromise: () => void;
 const savedSelfPromise = new Promise<void>((resolve) => {
@@ -45,6 +46,46 @@ Onyx.connect({
     },
 });
 
+const cleanUpOnPageHide = () => {
+    // notify other open tabs that this client is closed
+    localStorage.setItem(ACTIVE_CLIENT_LEFT_KEY, clientID);
+};
+
+const syncLocal = ({ key, newValue: clientLeftID }: StorageEvent) => {
+    if (key !== ACTIVE_CLIENT_LEFT_KEY) {
+        return;
+    }
+
+    // clean clientID of recently closed tab
+    // since it's not possible to write to IDB on closing stage
+    if (clientLeftID && activeClients.includes(clientLeftID) && clientLeftID !== clientID) {
+        activeClients = activeClients.filter(id => id !== clientLeftID);
+        ActiveClients.setActiveClients(activeClients);
+    }
+    localStorage.removeItem(ACTIVE_CLIENT_LEFT_KEY);
+};
+
+const setupCleanUp = () => {
+    const previousClientID = localStorage.getItem(ACTIVE_CLIENT_LEFT_KEY);
+
+    // cleanup of last closed client
+    if (previousClientID) {
+        activeClients = activeClients.filter((id) => id !== previousClientID);
+        ActiveClients.setActiveClients(activeClients);
+        localStorage.removeItem(ACTIVE_CLIENT_LEFT_KEY);
+    }
+
+    // use onpagehide event since onbeforeunload in not recommended. keeping beforeunload for legacy browsers
+    // https://developer.chrome.com/docs/web-platform/page-lifecycle-api#the_beforeunload_event
+    const terminationEvent = 'onpagehide' in window ? 'pagehide' : 'beforeunload';
+
+    // for tracking tab close
+    window.addEventListener(terminationEvent, cleanUpOnPageHide);
+
+    // listen to localStorage change
+    window.addEventListener('storage', syncLocal);
+};
+
 /**
  * Add our client ID to the list of active IDs.
  * We want to ensure we have no duplicates and that the activeClient gets added at the end of the array (see isClientTheLeader)
@@ -53,6 +94,8 @@ const init: Init = () => {
     activeClients = activeClients.filter((id) => id !== clientID);
     activeClients.push(clientID);
     ActiveClients.setActiveClients(activeClients).then(resolveSavedSelfPromise);
+
+    setupCleanUp();
 };
 
 /**
@@ -64,4 +107,4 @@ const isClientTheLeader: IsClientTheLeader = () => {
     return lastActiveClient === clientID;
 };
 
-export {init, isClientTheLeader, isReady};
+export { init, isClientTheLeader, isReady };
