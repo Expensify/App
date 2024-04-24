b031b0ad30033ba62a2087e311a957646ee68a2b
Jan Nowakowski <janeknowakowski9@gmail.com>
2024-04-24 13:05:44 +0200

    Further improvements

--- src/types/utils/viewRef.ts
+++ src/types/utils/viewRef.ts
@@ -1,5 +1,5 @@
 import type {View} from 'react-native';
 
-const viewRef = (ref: React.RefObject<View | HTMLDivElement>) => ref as React.RefObject<View>;
+const viewRef = (ref: React.RefObject<View | HTMLElement>) => ref as React.RefObject<View>;
 
 export default viewRef;
