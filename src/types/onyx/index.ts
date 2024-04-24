1e5deec895491957d816396aadf1d6995b555c2d
Daniel Silva <danieldoglas@users.noreply.github.com>
2024-04-24 19:15:35 +0200

    Merge pull request #40894 from mananjadhav/revert-full-story
    [CP Staging] Revert Fullstory lib changes
    
    (cherry picked from commit f0c6589bf40e095a50cf389d99da3c0ef5b84ebf)

--- src/types/onyx/index.ts
+++ src/types/onyx/index.ts
@@ -72,7 +72,6 @@ import type {TransactionViolation, ViolationName} from './TransactionViolation';
 import type TransactionViolations from './TransactionViolation';
 import type User from './User';
 import type UserLocation from './UserLocation';
-import type UserMetadata from './UserMetadata';
 import type UserWallet from './UserWallet';
 import type WalletAdditionalDetails from './WalletAdditionalDetails';
 import type {WalletAdditionalQuestionDetails} from './WalletAdditionalDetails';
@@ -156,7 +155,6 @@ export type {
     TransactionViolations,
     User,
     UserLocation,
-    UserMetadata,
     UserWallet,
     ViolationName,
     WalletAdditionalDetails,
