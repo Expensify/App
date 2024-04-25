c079b78db5a83f87b9364b6885a4ddccc2d7908a
nkdengineer <nkdengineer@outlook.com>
2024-04-25 12:13:20 +0700

    fix: es languages

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -228,7 +228,7 @@ export default {
             enterMerchant: 'Introduce un comerciante',
             enterAmount: 'Introduce un importe',
             enterDate: 'Introduce una fecha',
-            invalidTimeRange: 'Introduzca una hora utilizando el formato de reloj de 12 horas (por ejemplo, 2:30PM).',
+            invalidTimeRange: 'Por favor, introduce una hora entre 1 y 12 (por ejemplo, 2:30 PM).',
         },
         comma: 'la coma',
         semicolon: 'el punto y coma',
