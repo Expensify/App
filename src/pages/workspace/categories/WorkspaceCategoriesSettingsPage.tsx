c9cec18f4ce7753f42a05b058dcd139abbb7728c
Hayata Suenaga <hayata@expensify.com>
2024-04-25 10:52:13 -0400

    fix: use ToggleSettingOptionRow and use correct subtitle

--- src/pages/workspace/categories/WorkspaceCategoriesSettingsPage.tsx
+++ src/pages/workspace/categories/WorkspaceCategoriesSettingsPage.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
-import OfflineWithFeedback from '@components/OfflineWithFeedback';
 import ScreenWrapper from '@components/ScreenWrapper';
-import Switch from '@components/Switch';
-import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {setWorkspaceRequiresCategory} from '@libs/actions/Policy';
@@ -15,6 +12,7 @@ import FeatureEnabledAccessOrNotFoundWrapper from '@pages/workspace/FeatureEnabl
 import PaidPolicyAccessOrNotFoundWrapper from '@pages/workspace/PaidPolicyAccessOrNotFoundWrapper';
 import type {WithPolicyConnectionsProps} from '@pages/workspace/withPolicyConnections';
 import withPolicyConnections from '@pages/workspace/withPolicyConnections';
+import ToggleSettingOptionRow from '@pages/workspace/workflows/ToggleSettingsOptionRow';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 
@@ -46,28 +44,16 @@ function WorkspaceCategoriesSettingsPage({policy, route}: WorkspaceCategoriesSet
                     >
                         <HeaderWithBackButton title={translate('common.settings')} />
                         <View style={styles.flexGrow1}>
-                            <OfflineWithFeedback
-                                errors={policy?.errorFields?.requiresCategory}
+                            <ToggleSettingOptionRow
+                                title={translate('workspace.categories.requiresCategory')}
+                                subtitle={isConnectedToAccounting ? `${translate('workspace.categories.needCategoryForExportToIntegration')} ${translate('workspace.accounting.qbo')}` : ''}
+                                isActive={policy?.requiresCategory ?? false}
+                                onToggle={updateWorkspaceRequiresCategory}
                                 pendingAction={policy?.pendingFields?.requiresCategory}
-                                errorRowStyles={styles.mh5}
-                            >
-                                <View style={[styles.mt2, styles.mh4]}>
-                                    <View style={[styles.flexRow, styles.mb5, styles.mr2, styles.alignItemsCenter, styles.justifyContentBetween]}>
-                                        <Text style={[styles.textNormal, styles.colorMuted, styles.flexShrink1, styles.mr2]}>{translate('workspace.categories.requiresCategory')}</Text>
-                                        <Switch
-                                            isOn={policy?.requiresCategory ?? false}
-                                            accessibilityLabel={translate('workspace.categories.requiresCategory')}
-                                            onToggle={updateWorkspaceRequiresCategory}
-                                            disabled={!policy?.areCategoriesEnabled || !hasEnabledOptions}
-                                        />
-                                    </View>
-                                </View>
-                                {isConnectedToAccounting && (
-                                    <Text style={[styles.textNormal, styles.colorMuted]}>{`${translate('workspace.categories.importedFromAccountingSoftware')}${translate(
-                                        'workspace.accounting.qbo',
-                                    )}`}</Text>
-                                )}
-                            </OfflineWithFeedback>
+                                disabled={!policy?.areCategoriesEnabled || !hasEnabledOptions}
+                                wrapperStyle={[styles.mt2, styles.mh4]}
+                                errors={policy?.errorFields?.requiresCategory ?? undefined}
+                            />
                         </View>
                     </ScreenWrapper>
                 </FeatureEnabledAccessOrNotFoundWrapper>
