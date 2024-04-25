09a39b96c3fcb7056a241e34f9bcf4c70ffd7e03
Christoph Pader <chris@margelo.io>
2024-04-26 00:34:55 +0200

    fix: bottom tab navigator transformState on native

--- src/libs/Navigation/AppNavigator/createCustomBottomTabNavigator/index.tsx
+++ src/libs/Navigation/AppNavigator/createCustomBottomTabNavigator/index.tsx
@@ -1,34 +1,11 @@
-import type {EventMapBase, ParamListBase} from '@react-navigation/native';
+import type {ParamListBase} from '@react-navigation/native';
 import {createNavigatorFactory} from '@react-navigation/native';
 import React from 'react';
 import createPlatformStackNavigatorComponent from '@libs/Navigation/PlatformStackNavigation/createPlatformStackNavigatorComponent';
-import type {
-    PlatformSpecificEventMap,
-    PlatformSpecificNavigationOptions,
-    PlatformStackNavigationEventMap,
-    PlatformStackNavigationOptions,
-    PlatformStackNavigationState,
-} from '@libs/Navigation/PlatformStackNavigation/types';
-import type {TransformStateProps} from '@libs/Navigation/PlatformStackNavigation/types/NavigatorComponent';
-import type {NavigationStateRoute} from '@libs/Navigation/types';
-import SCREENS from '@src/SCREENS';
+import type {PlatformStackNavigationEventMap, PlatformStackNavigationOptions, PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
 import BottomTabBar from './BottomTabBar';
 import BottomTabNavigationContentWrapper from './BottomTabNavigationContentWrapper';
-
-function transformState({state}: TransformStateProps<PlatformSpecificNavigationOptions, PlatformSpecificEventMap & EventMapBase, ParamListBase>) {
-    const routesToRender = [state.routes.at(-1)] as NavigationStateRoute[];
-
-    // We need to render at least one HOME screen to make sure everything load properly. This may be not necessary after changing how IS_SIDEBAR_LOADED is handled.
-    // Currently this value will be switched only after the first HOME screen is rendered.
-    if (routesToRender[0].name !== SCREENS.HOME) {
-        const routeToRender = state.routes.find((route) => route.name === SCREENS.HOME);
-        if (routeToRender) {
-            routesToRender.unshift(routeToRender);
-        }
-    }
-
-    return {stateToRender: {...state, routes: routesToRender, index: routesToRender.length - 1}};
-}
+import transformState from './transformState';
 
 const defaultScreenOptions: PlatformStackNavigationOptions = {
     animation: 'none',
