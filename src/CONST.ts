5762ea4ff34059fe656feeea8e6090f1b3405183
Yauheni <HorbachZhenja@yandex.by>
2024-04-25 14:45:56 +0200

    Move Leave button into a row of the Report Details page

--- src/CONST.ts
+++ src/CONST.ts
@@ -1936,7 +1936,6 @@ const CONST = {
         INFO: 'info',
     },
     REPORT_DETAILS_MENU_ITEM: {
-        SHARE_CODE: 'shareCode',
         MEMBERS: 'member',
         INVITE: 'invite',
         SETTINGS: 'settings',
