3dffd6404e2166aae51fcffd10853936583509a6
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-04-26 13:16:58 +0800

    remove console log

--- src/pages/ReimbursementAccount/ReimbursementAccountPage.tsx
+++ src/pages/ReimbursementAccount/ReimbursementAccountPage.tsx
@@ -389,7 +389,6 @@ function ReimbursementAccountPage({
         ].some((value) => value === currentStep)
     );
 
-    console.log('is loading?', {isLoading, isReimbursementAccountLoading, hasACHDataBeenLoaded, plaidCurrentEvent, substep: achData?.subStep})
     // Show loading indicator when page is first time being opened and props.reimbursementAccount yet to be loaded from the server
     // or when data is being loaded. Don't show the loading indicator if we're offline and restarted the bank account setup process
     // On Android, when we open the app from the background, Onfido activity gets destroyed, so we need to reopen it.
