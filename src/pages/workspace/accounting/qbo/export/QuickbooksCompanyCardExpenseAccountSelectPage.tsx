a6cd09d27411c8feefb0cf71e3ba9998d3920efe
Hayata Suenaga <hayata@expensify.com>
2024-04-29 15:53:13 -0700

    fix: translation key

--- src/pages/workspace/accounting/qbo/export/QuickbooksCompanyCardExpenseAccountSelectPage.tsx
+++ src/pages/workspace/accounting/qbo/export/QuickbooksCompanyCardExpenseAccountSelectPage.tsx
@@ -19,6 +19,11 @@ import type {Account} from '@src/types/onyx/Policy';
 type CardListItem = ListItem & {
     value: string;
 };
+const AccountTypeTranslationKeyMapping = {
+    [CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.VENDOR_BILL]: 'vendorBill',
+    [CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.CREDIT_CARD]: 'creditCard',
+    [CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.DEBIT_CARD]: 'debitCard',
+} as const;
 
 function QuickbooksCompanyCardExpenseAccountSelectPage({policy}: WithPolicyConnectionsProps) {
     const {translate} = useLocalize();
@@ -73,7 +78,11 @@ function QuickbooksCompanyCardExpenseAccountSelectPage({policy}: WithPolicyConne
                     title={exportCompanyCard === CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.VENDOR_BILL ? translate('workspace.qbo.vendor') : translate('workspace.qbo.account')}
                 />
                 <SelectionList
-                    headerContent={exportCompanyCard ? <Text style={[styles.ph5, styles.pb5]}>{translate(`workspace.qbo.${exportCompanyCard}AccountDescription`)}</Text> : null}
+                    headerContent={
+                        exportCompanyCard ? (
+                            <Text style={[styles.ph5, styles.pb5]}>{translate(`workspace.qbo.${AccountTypeTranslationKeyMapping[exportCompanyCard]}AccountDescription`)}</Text>
+                        ) : null
+                    }
                     sections={[{data}]}
                     ListItem={RadioListItem}
                     onSelectRow={selectExportAccount}
