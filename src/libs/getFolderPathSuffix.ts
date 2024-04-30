dac895c0ff04139b5732e3881c3c0344d1bf1447
ShridharGoel <35566748+ShridharGoel@users.noreply.github.com>
2024-04-30 13:26:23 +0530

    Update

---
+++ src/libs/getFolderPathSuffix.ts
@@ -0,0 +1,24 @@
+import CONST from '@src/CONST';
+
+function getFolderPathSuffix(environment) {
+    let folderSuffix = '';
+
+    switch (environment) {
+        case CONST.ENVIRONMENT.PRODUCTION:
+            folderSuffix = '';
+            break;
+        case CONST.ENVIRONMENT.ADHOC:
+            folderSuffix = ' Adhoc';
+            break;
+        case CONST.ENVIRONMENT.DEV:
+            folderSuffix = ' Dev';
+            break;
+        default:
+            folderSuffix = '';
+            break;
+    }
+
+    return folderSuffix;
+}
+
+export default getFolderPathSuffix;
