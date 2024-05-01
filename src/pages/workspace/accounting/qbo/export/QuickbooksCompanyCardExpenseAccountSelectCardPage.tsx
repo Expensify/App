d0029f643bc86a0eda4f302b18ef32bc3d253f79
Hayata Suenaga <hayata@expensify.com>
2024-05-01 10:08:38 -0700

    chore: translation key

--- src/pages/workspace/accounting/qbo/export/QuickbooksCompanyCardExpenseAccountSelectCardPage.tsx
+++ src/pages/workspace/accounting/qbo/export/QuickbooksCompanyCardExpenseAccountSelectCardPage.tsx
@@ -34,15 +34,15 @@ function QuickbooksCompanyCardExpenseAccountSelectCardPage({policy}: WithPolicyC
     const defaultCards = useMemo<Card[]>(
         () => [
             {
-                name: translate(`workspace.qbo.exportCompanyCardAccountType.creditCard`),
+                name: translate(`workspace.qbo.exportCompanyCardAccountType.credit_card`),
                 id: CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.CREDIT_CARD,
             },
             {
-                name: translate(`workspace.qbo.exportCompanyCardAccountType.debitCard`),
+                name: translate(`workspace.qbo.exportCompanyCardAccountType.debit_card`),
                 id: CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.DEBIT_CARD,
             },
             {
-                name: translate(`workspace.qbo.exportCompanyCardAccountType.vendorBill`),
+                name: translate(`workspace.qbo.exportCompanyCardAccountType.bill`),
                 id: CONST.QUICKBOOKS_EXPORT_COMPANY_CARD_ACCOUNT_TYPE.VENDOR_BILL,
             },
         ],
