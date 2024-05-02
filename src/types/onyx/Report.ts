1c245829b6b3d1d4f2f13c36737a130da736c1ed
usman-ghani564 <usmanghani564.ug9@gmail.com>
2024-05-02 19:58:20 +0500

    use OnyxValueWithOfflineFeedback

--- src/types/onyx/Report.ts
+++ src/types/onyx/Report.ts
@@ -23,11 +23,10 @@ type PendingChatMember = {
     pendingAction: OnyxCommon.PendingAction;
 };
 
-type Participant = {
+type Participant = OnyxCommon.OnyxValueWithOfflineFeedback<{
     hidden?: boolean;
     role?: 'admin' | 'member';
-    pendingFields?: Record<string, OnyxCommon.PendingAction>;
-};
+}>;
 
 type InvoiceReceiver =
     | {
