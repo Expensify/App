460effc410a65f565112269c2f8767c534bf6dac
Rushat Gabhane <rushatgabhane@gmail.com>
2024-05-03 11:50:46 +0530

    add helper text to menu item

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -141,6 +141,11 @@ type MenuItemBaseProps = {
     /** A description text to show under the title */
     description?: string;
 
+    /** Text to show below menu item. This text is not interactive */
+    helperText?: string;
+
+    helperTextStyle?: StyleProp<TextStyle>;
+
     /** Should the description be shown above the title (instead of the other way around) */
     shouldShowDescriptionOnTop?: boolean;
 
@@ -296,6 +301,8 @@ function MenuItem(
         furtherDetailsIcon,
         furtherDetails,
         description,
+        helperText,
+        helperTextStyle,
         error,
         errorText,
         success = false,
@@ -679,6 +686,7 @@ function MenuItem(
                     </PressableWithSecondaryInteraction>
                 )}
             </Hoverable>
+            {!!helperText && <Text style={[styles.mutedNormalTextLabel, styles.ph5, styles.pb5, helperTextStyle]}>{helperText}</Text>}
         </View>
     );
 }
