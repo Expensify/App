5129cbd0e6beb55b0effe1c1001bde5c9beb6efa
rory <rory@expensify.com>
2024-05-02 22:15:17 -0700

    Implement Troubleshooting lib

--- src/libs/actions/Troubleshooting.ts
+++ src/libs/actions/Troubleshooting.ts
@@ -1,7 +1,14 @@
 import Onyx from 'react-native-onyx';
+import FileTypes from '@libs/FileTypes';
+import localFileDownload from '@libs/localFileDownload';
 
+/**
+ * Export all data from Onyx to a file, then download that file.
+ */
 function exportOnyxDataToFile() {
-    Onyx.getAllEntries().then((entries) => JSON.stringify(entries));
+    Onyx.getAllEntries()
+        .then((entries) => JSON.stringify(entries))
+        .then((json) => localFileDownload('NewExpensifyOnyxExport', json, FileTypes.JSON));
 }
 
 export default {
