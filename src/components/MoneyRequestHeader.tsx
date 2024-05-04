28cff71e64e880935f0241583dd96c02929536c7
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-05-04 22:07:47 +0800

    enable navigate to detail page for transaction thread

--- src/components/MoneyRequestHeader.tsx
+++ src/components/MoneyRequestHeader.tsx
@@ -178,6 +178,7 @@ function MoneyRequestHeader({
                 <HeaderWithBackButton
                     shouldShowBorderBottom={!isScanning && !isPending && !isOnHold}
                     shouldShowReportAvatarWithDisplay
+                    shouldEnableDetailPageNavigation
                     shouldShowPinButton={false}
                     shouldShowThreeDotsButton
                     threeDotsMenuItems={threeDotsMenuItems}
