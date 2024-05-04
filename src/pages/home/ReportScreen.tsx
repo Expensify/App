4724283a8d4aed2064b208b3a800e396cd1c2b5c
cretadn22 <cretadn22@gmail.com>
2024-05-04 20:08:45 +0700

    format

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -322,9 +322,7 @@ function ReportScreen({
     }, [shouldHideReport, report]);
 
     const onBackButtonPress = useCallback(() => {
-        isReportOpenInRHP
-            ? Navigation.dismissModal()
-            : Navigation.goBack(undefined, false, true);
+        isReportOpenInRHP ? Navigation.dismissModal() : Navigation.goBack(undefined, false, true);
     }, [isReportOpenInRHP]);
 
     let headerView = (
