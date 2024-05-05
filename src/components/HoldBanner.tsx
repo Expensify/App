22b6cd9c0af4047124041478f099cbb241b7f2ea
gijoe0295 <gijoe0295@gmail.com>
2024-05-05 21:10:06 +0700

    revert HoldBanner changes

--- src/components/HoldBanner.tsx
+++ src/components/HoldBanner.tsx
@@ -5,19 +5,14 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import Text from './Text';
 import TextPill from './TextPill';
 
-type HoldBannerProps = {
-    isRequestDuplicate?: boolean;
-    shouldShowBorderBottom?: boolean;
-};
-
-function HoldBanner({isRequestDuplicate = false, shouldShowBorderBottom = false}: HoldBannerProps) {
+function HoldBanner() {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
 
     return (
-        <View style={[styles.dFlex, styles.flexRow, styles.alignItemsCenter, styles.pb3, styles.ph5, shouldShowBorderBottom ? styles.borderBottom : {}]}>
+        <View style={[styles.dFlex, styles.flexRow, styles.alignItemsCenter, styles.pb3, styles.ph5, styles.borderBottom]}>
             <TextPill>{translate('iou.hold')}</TextPill>
-            <Text style={[styles.textLabel, styles.pl3, styles.mw100, styles.flexShrink1]}>{isRequestDuplicate ? translate('iou.expenseDuplicate') : translate('iou.expenseOnHold')}</Text>
+            <Text style={[styles.textLabel, styles.pl3, styles.mw100, styles.flexShrink1]}>{translate('iou.expenseOnHold')}</Text>
         </View>
     );
 }
