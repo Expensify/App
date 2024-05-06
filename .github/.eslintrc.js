081d52a490eef6855df4715dc726fee106dd946b
Blazej Kustra <blazej.kustra@swmansion.com>
2024-05-06 17:38:13 +0200

    Test

--- .github/.eslintrc.js
+++ .github/.eslintrc.js
@@ -1,4 +1,4 @@
-// For all these Node.js scripts, we do not want to disable `console` statements
+// For all these Node.js scripts, we do not want to disable `console` statements3
 module.exports = {
     rules: {
         'no-console': 'off',
