8625cf3dc0f9657586924f34bc96992325f2e9a0
Wildan Muhlis <muhliswildan@gmail.com>
2024-05-06 23:40:06 +0700

    fix bug in file name

--- desktop/createDownloadQueue.ts
+++ desktop/createDownloadQueue.ts
@@ -1,5 +1,6 @@
 import type {BrowserWindow} from 'electron';
 import {app} from 'electron';
+import * as path from 'path';
 import createQueue from '@libs/Queue/Queue';
 import ELECTRON_EVENTS from './ELECTRON_EVENTS';
 import type {Options} from './electronDownloadManagerType';
@@ -15,26 +16,60 @@ type DownloadItem = {
     options: Options;
 };
 
+/**
+ * Returns the filename with extension based on the given name and MIME type.
+ * @param name - The name of the file.
+ * @param mime - The MIME type of the file.
+ * @returns The filename with extension.
+ */
+const getFilenameFromMime = (name: string, mime: string): string => {
+    const extensions = mime.split('/').pop();
+    return `${name}.${extensions}`;
+};
+
 const createDownloadQueue = () => {
     const downloadItemProcessor = (item: DownloadItem): Promise<void> =>
         new Promise((resolve, reject) => {
             item.win.webContents.downloadURL(item.url);
 
-            const listener = (event: Electron.Event, downloadItem: Electron.DownloadItem) => {
+            const listener = (event: Electron.Event, electronDownloadItem: Electron.DownloadItem) => {
+                const options = item.options;
                 const cleanup = () => item.win.webContents.session.removeListener('will-download', listener);
-                const errorMessage = `The download of ${downloadItem.getFilename()} was interrupted`;
+                const errorMessage = `The download of ${electronDownloadItem.getFilename()} was interrupted`;
 
-                downloadItem.on('updated', (_, state) => {
+                if (options.directory && !path.isAbsolute(options.directory)) {
+                    throw new Error('The `directory` option must be an absolute path');
+                }
+
+                const directory = options.directory ?? app.getPath('downloads');
+
+                let filePath: string;
+                if (options.filename) {
+                    filePath = path.join(directory, options.filename);
+                } else {
+                    const filename = electronDownloadItem.getFilename();
+                    const name = path.extname(filename) ? filename : getFilenameFromMime(filename, electronDownloadItem.getMimeType());
+
+                    filePath = options.overwrite ? path.join(directory, name) : path.join(directory, name);
+                }
+
+                if (options.saveAs) {
+                    electronDownloadItem.setSaveDialogOptions({defaultPath: filePath, ...options.dialogOptions});
+                } else {
+                    electronDownloadItem.setSavePath(filePath);
+                }
+
+                electronDownloadItem.on('updated', (_, state) => {
                     if (state !== 'interrupted') {
                         return;
                     }
 
                     cleanup();
                     reject(new Error(errorMessage));
-                    downloadItem.cancel();
+                    electronDownloadItem.cancel();
                 });
 
-                downloadItem.on('done', (_, state) => {
+                electronDownloadItem.on('done', (_, state) => {
                     cleanup();
                     if (state === 'cancelled') {
                         resolve();
@@ -42,7 +77,7 @@ const createDownloadQueue = () => {
                         reject(new Error(errorMessage));
                     } else if (state === 'completed') {
                         if (process.platform === 'darwin') {
-                            const savePath = downloadItem.getSavePath();
+                            const savePath = electronDownloadItem.getSavePath();
                             app.dock.downloadFinished(savePath);
                         }
                         resolve();
