42e2aa925dc255c9aef879b70c981e6e4c9507d9
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-05-06 10:50:32 +0200

    update to max recent reports to show

--- src/pages/EditReportFieldDropdown.tsx
+++ src/pages/EditReportFieldDropdown.tsx
@@ -10,6 +10,7 @@ import useDebouncedState from '@hooks/useDebouncedState';
 import useLocalize from '@hooks/useLocalize';
 import useTheme from '@hooks/useTheme';
 import * as OptionsListUtils from '@libs/OptionsListUtils';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {RecentlyUsedReportFields} from '@src/types/onyx';
 
@@ -86,7 +87,7 @@ function EditReportFieldDropdownPage({onSubmit, fieldKey, fieldValue, fieldOptio
             false,
             false,
             undefined,
-            5,
+            CONST.IOU.MAX_RECENT_REPORTS_TO_SHOW,
             undefined,
             undefined,
             true,
