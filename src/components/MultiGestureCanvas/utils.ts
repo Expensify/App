99e0265e3e2381456c42a5d367f998549d7d3ca1
Samil Abud <samilabud@gmail.com>
2024-05-07 09:30:42 -0400

    Fixed image attachment fit to scale even when not needed

--- src/components/MultiGestureCanvas/utils.ts
+++ src/components/MultiGestureCanvas/utils.ts
@@ -1,12 +1,23 @@
 import type {CanvasSize, ContentSize} from './types';
 
+type ShouldResizeToFit = (canvasSize: CanvasSize, contentSize: ContentSize) => boolean;
 type GetCanvasFitScale = (props: {canvasSize: CanvasSize; contentSize: ContentSize}) => {scaleX: number; scaleY: number; minScale: number; maxScale: number};
 
+const shouldResizeToFit: ShouldResizeToFit = (canvasSize, contentSize) => {
+    // If the image is smaller than canvas should no fit to canvas scale
+    if (canvasSize && contentSize) {
+        return canvasSize.width < contentSize.width || canvasSize.height < contentSize.height;
+    }
+    return false;
+};
+
 const getCanvasFitScale: GetCanvasFitScale = ({canvasSize, contentSize}) => {
+    const shouldResize = shouldResizeToFit(canvasSize, contentSize);
+
     const scaleX = canvasSize.width / contentSize.width;
     const scaleY = canvasSize.height / contentSize.height;
 
-    const minScale = Math.min(scaleX, scaleY);
+    const minScale = !shouldResize ? 1 : Math.min(scaleX, scaleY);
     const maxScale = Math.max(scaleX, scaleY);
 
     return {scaleX, scaleY, minScale, maxScale};
