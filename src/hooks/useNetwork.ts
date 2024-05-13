0fecf4c797a3aa1ea4014ecf2222ae419a24fc08
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-05-13 11:42:37 +0200

    useNetwork default values

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -1,3 +1,4 @@
+import {noop} from 'lodash';
 import {useContext, useEffect, useRef} from 'react';
 import {NetworkContext} from '@components/OnyxProvider';
 import CONST from '@src/CONST';
@@ -8,11 +9,13 @@ type UseNetworkProps = {
 
 type UseNetwork = {isOffline: boolean};
 
-export default function useNetwork({onReconnect = () => {}}: UseNetworkProps = {}): UseNetwork {
+const DEFAULT_NETWORK_CONTEXT_DATA = {...CONST.DEFAULT_NETWORK_DATA, networkStatus: CONST.NETWORK.NETWORK_STATUS.UNKNOWN};
+
+export default function useNetwork({onReconnect = noop}: UseNetworkProps = {}): UseNetwork {
     const callback = useRef(onReconnect);
     callback.current = onReconnect;
 
-    const {isOffline, networkStatus} = useContext(NetworkContext) ?? {...CONST.DEFAULT_NETWORK_DATA, networkStatus: CONST.NETWORK.NETWORK_STATUS.UNKNOWN};
+    const {isOffline, networkStatus} = useContext(NetworkContext) ?? DEFAULT_NETWORK_CONTEXT_DATA;
     const prevOfflineStatusRef = useRef(isOffline);
     useEffect(() => {
         // If we were offline before and now we are not offline then we just reconnected
