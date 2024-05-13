64544aa29efaa50ba97ec27518e2b89cb282d186
Srikar Parsi <srikar.parsi@expensify.com>
2024-05-13 02:14:38 -0400

    style

--- src/types/onyx/Report.ts
+++ src/types/onyx/Report.ts
@@ -202,7 +202,7 @@ type Report = OnyxCommon.OnyxValueWithOfflineFeedback<
         transactionThreadReportID?: string;
 
         fieldList?: Record<string, PolicyReportField>;
-        
+
         permissions?: Array<ValueOf<typeof CONST.REPORT.PERMISSIONS>>;
     },
     PolicyReportField['fieldID']
