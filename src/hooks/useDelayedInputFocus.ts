26efda685a2d226cb1d3f8fcc8d1120b59662ef6
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-05-13 10:15:31 +0200

    useDelayedInputFocus remove unnecessary ref

--- src/hooks/useDelayedInputFocus.ts
+++ src/hooks/useDelayedInputFocus.ts
@@ -1,6 +1,6 @@
 import {useFocusEffect} from '@react-navigation/native';
 import type {MutableRefObject} from 'react';
-import {useCallback, useRef} from 'react';
+import {useCallback} from 'react';
 import type {TextInput} from 'react-native';
 import CONST from '@src/CONST';
 
@@ -8,17 +8,10 @@ import CONST from '@src/CONST';
  * Focus a text input when a screen is navigated to, after the specified time delay has elapsed.
  */
 export default function useDelayedInputFocus(inputRef: MutableRefObject<TextInput>, delay: number = CONST.ANIMATED_TRANSITION) {
-    const timeoutRef = useRef<NodeJS.Timeout | null>(null);
-
     useFocusEffect(
         useCallback(() => {
-            timeoutRef.current = setTimeout(() => inputRef.current?.focus(), delay);
-            return () => {
-                if (!timeoutRef.current) {
-                    return;
-                }
-                clearTimeout(timeoutRef.current);
-            };
+            const timeoutRef = setTimeout(() => inputRef.current?.focus(), delay);
+            return () => clearTimeout(timeoutRef);
         }, [delay, inputRef]),
     );
 }
