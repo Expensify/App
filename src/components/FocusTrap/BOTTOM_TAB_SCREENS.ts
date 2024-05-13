6e3ba8ca2e731198963df2f7c31a1603f70574af
Jan Nowakowski <janeknowakowski9@gmail.com>
2024-05-13 14:15:07 +0200

    Fix prettier

--- src/components/FocusTrap/BOTTOM_TAB_SCREENS.ts
+++ src/components/FocusTrap/BOTTOM_TAB_SCREENS.ts
@@ -1,4 +1,4 @@
-import type { BottomTabName } from '@libs/Navigation/types';
+import type {BottomTabName} from '@libs/Navigation/types';
 import SCREENS from '@src/SCREENS';
 
 const BOTTOM_TAB_SCREENS: BottomTabName[] = [SCREENS.HOME, SCREENS.SETTINGS.ROOT];
