a1bd7f159a14590cf89f594a3c7f7ed9bde190db
nkdengineer <nkdengineer@outlook.com>
2024-05-13 12:00:45 +0700

    Fix member option appear in transaction thread report detail page

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -1052,6 +1052,14 @@ function isSystemChat(report: OnyxEntry<Report>): boolean {
 }
 
 /**
+ * Checks if a report is an IOU or expense report.
+ */
+function isMoneyRequestReport(reportOrID: OnyxEntry<Report> | EmptyObject | string): boolean {
+    const report = typeof reportOrID === 'object' ? reportOrID : allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportOrID}`] ?? null;
+    return isIOUReport(report) || isExpenseReport(report);
+}
+
+/**
  * Only returns true if this is our main 1:1 DM report with Concierge
  */
 function isConciergeChatReport(report: OnyxEntry<Report>): boolean {
@@ -1217,7 +1225,7 @@ function hasExpenses(reportID?: string): boolean {
  * Whether the provided report is a closed expense report with no expenses
  */
 function isClosedExpenseReportWithNoExpenses(report: OnyxEntry<Report>): boolean {
-    return report?.statusNum === CONST.REPORT.STATUS_NUM.CLOSED && isExpenseReport(report) && !hasExpenses(report.reportID);
+    return report?.statusNum === CONST.REPORT.STATUS_NUM.CLOSED && isMoneyRequestReport(report) && !hasExpenses(report.reportID);
 }
 
 /**
@@ -1395,14 +1403,6 @@ function isMoneyRequest(reportOrID: OnyxEntry<Report> | string): boolean {
 }
 
 /**
- * Checks if a report is an IOU or expense report.
- */
-function isMoneyRequestReport(reportOrID: OnyxEntry<Report> | EmptyObject | string): boolean {
-    const report = typeof reportOrID === 'object' ? reportOrID : allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportOrID}`] ?? null;
-    return isIOUReport(report) || isExpenseReport(report);
-}
-
-/**
  * Checks if a report has only one transaction associated with it
  */
 function isOneTransactionReport(reportID: string): boolean {
@@ -1496,6 +1496,10 @@ function getChildReportNotificationPreference(reportAction: OnyxEntry<ReportActi
  */
 function canAddOrDeleteTransactions(moneyRequestReport: OnyxEntry<Report>): boolean {
     if (!isMoneyRequestReport(moneyRequestReport)) {
+        return false;
+    }
+
+    if (moneyRequestReport?.statusNum === CONST.REPORT.STATUS_NUM.CLOSED) {
         return false;
     }
 
@@ -4309,10 +4313,11 @@ function buildOptimisticChatReport(
     description = '',
     avatarUrl = '',
     optimisticReportID = '',
+    shouldShowParticipants = true,
 ): OptimisticChatReport {
     const participants = participantList.reduce((reportParticipants: Participants, accountID: number) => {
         const participant: ReportParticipant = {
-            hidden: false,
+            hidden: !shouldShowParticipants,
             role: accountID === currentUserAccountID ? CONST.REPORT.ROLE.ADMIN : CONST.REPORT.ROLE.MEMBER,
         };
         // eslint-disable-next-line no-param-reassign
@@ -4859,6 +4864,8 @@ function buildTransactionThread(
         moneyRequestReport?.reportID,
         '',
         '',
+        '',
+        false,
     );
 }
 
