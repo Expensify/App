4243779bc6db4590c2eb893f51812a956a7b1670
smelaa <aleksandra.smela@gmail.com>
2024-05-13 14:31:02 +0200

    Update JSDoc

--- src/hooks/useGeographicalStateAndCountryFromRoute.ts
+++ src/hooks/useGeographicalStateAndCountryFromRoute.ts
@@ -7,11 +7,12 @@ type Country = keyof typeof CONST.ALL_COUNTRIES;
 type StateAndCountry = {state?: State; country?: Country};
 
 /**
- * Extracts the 'state' (default) query parameter from the route/ url and validates it against COMMON_CONST.STATES, returning its ISO code or `undefined`.
- * Example 1: Url: https://new.expensify.com/settings/profile/address?state=MO Returns: MO
- * Example 2: Url: https://new.expensify.com/settings/profile/address?state=ASDF Returns: undefined
- * Example 3: Url: https://new.expensify.com/settings/profile/address Returns: undefined
- * Example 4: Url: https://new.expensify.com/settings/profile/address?state=MO-hash-a12341 Returns: MO
+ * Extracts the 'state' and 'country' query parameters from the route/ url and validates it against COMMON_CONST.STATES and CONST.ALL_COUNTRIES.
+ * Example 1: Url: https://new.expensify.com/settings/profile/address?state=MO Returns: state=MO
+ * Example 2: Url: https://new.expensify.com/settings/profile/address?state=ASDF Returns: state=undefined
+ * Example 3: Url: https://new.expensify.com/settings/profile/address Returns: state=undefined
+ * Example 4: Url: https://new.expensify.com/settings/profile/address?state=MO-hash-a12341 Returns: state=MO
+ * Similarly for country parameter.
  */
 export default function useGeographicalStateAndCountryFromRoute(stateParamName = 'state', countryParamName = 'country'): StateAndCountry {
     const routeParams = useRoute().params as Record<string, string>;
