e11b521326b533aa3c2282091863845622f08065
Christoph Pader <chris@margelo.io>
2024-05-14 13:06:28 +0200

    fix lint

--- src/libs/actions/OnyxUpdateManager/utils/GetMissingOnyxUpdatesPromise.ts
+++ src/libs/actions/OnyxUpdateManager/utils/GetMissingOnyxUpdatesPromise.ts
@@ -1,4 +1,4 @@
-import {Response} from '@src/types/onyx';
+import type {Response} from '@src/types/onyx';
 import createProxyForObject from '@src/utils/createProxyForObject';
 
 const GetMissingOnyxUpdatesPromiseValue = {GetMissingOnyxUpdatesPromise: undefined as Promise<Response | Response[] | void> | undefined};
