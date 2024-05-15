f51d40a39721d92d853891752968309db3017eca
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2024-05-15 09:56:36 +0200

    refactor: caret wrapper

--- src/components/DisplayNames/index.tsx
+++ src/components/DisplayNames/index.tsx
@@ -10,60 +10,74 @@ import DisplayNamesWithoutTooltip from './DisplayNamesWithoutTooltip';
 import DisplayNamesWithToolTip from './DisplayNamesWithTooltip';
 import type DisplayNamesProps from './types';
 
-function DisplayNames({fullTitle, tooltipEnabled, textStyles, numberOfLines, shouldUseFullTitle, displayNamesWithTooltips, renderAdditionalText, shouldShowCaret}: DisplayNamesProps) {
-    const {translate} = useLocalize();
-    const title = fullTitle || translate('common.hidden');
+type CaretWrapperProps = {
+    children: React.ReactNode;
+    shouldShowCaret?: boolean;
+};
+
+function CaretWrapper({children, shouldShowCaret}: CaretWrapperProps) {
     const theme = useTheme();
     const styles = useThemeStyles();
 
-    let result = (
-        <DisplayNamesWithToolTip
-            fullTitle={title}
-            displayNamesWithTooltips={displayNamesWithTooltips}
-            textStyles={textStyles}
-            numberOfLines={numberOfLines}
-            renderAdditionalText={renderAdditionalText}
-        />
+    if (!shouldShowCaret) {
+        return children;
+    }
+
+    return (
+        <View style={[styles.flex1, styles.flexRow, styles.gap1, styles.alignItemsCenter]}>
+            {children}
+            <Icon
+                src={Expensicons.DownArrow}
+                fill={theme.icon}
+                width={variables.iconSizeExtraSmall}
+                height={variables.iconSizeExtraSmall}
+            />
+        </View>
     );
+}
+
+function DisplayNames({fullTitle, tooltipEnabled, textStyles, numberOfLines, shouldUseFullTitle, displayNamesWithTooltips, renderAdditionalText, shouldShowCaret}: DisplayNamesProps) {
+    const {translate} = useLocalize();
+    const title = fullTitle || translate('common.hidden');
 
     if (!tooltipEnabled) {
-        result = (
-            <DisplayNamesWithoutTooltip
-                textStyles={textStyles}
-                numberOfLines={numberOfLines}
-                fullTitle={title}
-                renderAdditionalText={renderAdditionalText}
-            />
+        return (
+            <CaretWrapper shouldShowCaret={shouldShowCaret}>
+                <DisplayNamesWithoutTooltip
+                    textStyles={textStyles}
+                    numberOfLines={numberOfLines}
+                    fullTitle={title}
+                    renderAdditionalText={renderAdditionalText}
+                />
+            </CaretWrapper>
         );
     }
 
     if (shouldUseFullTitle) {
-        result = (
+        return (
+            <CaretWrapper shouldShowCaret={shouldShowCaret}>
+                <DisplayNamesWithToolTip
+                    shouldUseFullTitle
+                    fullTitle={title}
+                    textStyles={textStyles}
+                    numberOfLines={numberOfLines}
+                    renderAdditionalText={renderAdditionalText}
+                />
+            </CaretWrapper>
+        );
+    }
+
+    return (
+        <CaretWrapper shouldShowCaret={shouldShowCaret}>
             <DisplayNamesWithToolTip
-                shouldUseFullTitle
                 fullTitle={title}
+                displayNamesWithTooltips={displayNamesWithTooltips}
                 textStyles={textStyles}
                 numberOfLines={numberOfLines}
                 renderAdditionalText={renderAdditionalText}
             />
-        );
-    }
-
-    if (shouldShowCaret) {
-        return (
-            <View style={[styles.flex1, styles.flexRow, styles.gap1, styles.alignItemsCenter]}>
-                {result}
-                <Icon
-                    src={Expensicons.DownArrow}
-                    fill={theme.icon}
-                    width={variables.iconSizeExtraSmall}
-                    height={variables.iconSizeExtraSmall}
-                />
-            </View>
-        );
-    }
-
-    return result;
+        </CaretWrapper>
+    );
 }
 
 DisplayNames.displayName = 'DisplayNames';
