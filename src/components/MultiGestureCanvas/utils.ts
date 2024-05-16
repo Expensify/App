0bfd04e120d1c757090c94a646f886886ca69780
Samil Abud <samilabud@gmail.com>
2024-05-16 09:57:58 -0400

    Removing function resizeToFit and using clamp to calculate the new scale

--- src/components/MultiGestureCanvas/utils.ts
+++ src/components/MultiGestureCanvas/utils.ts
@@ -1,27 +1,6 @@
 import type {CanvasSize, ContentSize} from './types';
 
-type ShouldResizeToFit = (canvasSize: CanvasSize, contentSize: ContentSize) => boolean;
 type GetCanvasFitScale = (props: {canvasSize: CanvasSize; contentSize: ContentSize}) => {scaleX: number; scaleY: number; minScale: number; maxScale: number};
-
-const shouldResizeToFit: ShouldResizeToFit = (canvasSize, contentSize) => {
-    // If the image is smaller than canvas should no fit to canvas scale
-    if (canvasSize && contentSize) {
-        return canvasSize.width < contentSize.width || canvasSize.height < contentSize.height;
-    }
-    return false;
-};
-
-const getCanvasFitScale: GetCanvasFitScale = ({canvasSize, contentSize}) => {
-    const shouldResize = shouldResizeToFit(canvasSize, contentSize);
-
-    const scaleX = canvasSize.width / contentSize.width;
-    const scaleY = canvasSize.height / contentSize.height;
-
-    const minScale = !shouldResize ? 1 : Math.min(scaleX, scaleY);
-    const maxScale = Math.max(scaleX, scaleY);
-
-    return {scaleX, scaleY, minScale, maxScale};
-};
 
 /** Clamps a value between a lower and upper bound */
 function clamp(value: number, lowerBound: number, upperBound: number) {
@@ -29,5 +8,17 @@ function clamp(value: number, lowerBound: number, upperBound: number) {
 
     return Math.min(Math.max(lowerBound, value), upperBound);
 }
+
+const getCanvasFitScale: GetCanvasFitScale = ({canvasSize, contentSize}) => {
+    const trueScaleX = canvasSize.width / contentSize.width;
+    const trueScaleY = canvasSize.height / contentSize.height;
+
+    const scaleX = clamp(trueScaleX, 0, 1);
+    const scaleY = clamp(trueScaleY, 0, 1);
+    const minScale = Math.min(scaleX, scaleY);
+    const maxScale = Math.max(scaleX, scaleY);
+
+    return {scaleX, scaleY, minScale, maxScale};
+};
 
 export {getCanvasFitScale, clamp};
