acbeb1b60b2fcdc9f16a5197044af7f03ff71a91
Manan Jadhav <manan.jadhav@gmail.com>
2024-05-17 23:10:28 +0530

    fix: select first item in list

--- src/pages/workspace/accounting/xero/export/XeroExportConfigurationPage.tsx
+++ src/pages/workspace/accounting/xero/export/XeroExportConfigurationPage.tsx
@@ -26,7 +26,7 @@ function XeroExportConfigurationPage({policy}: WithPolicyConnectionsProps) {
     const {bankAccounts} = policy?.connections?.xero?.data ?? {};
     const selectedBankAccountName = useMemo(() => {
         const selectedAccount = (bankAccounts ?? []).find((bank) => bank.id === exportConfiguration?.nonReimbursableAccount);
-        return selectedAccount?.name ?? '';
+        return selectedAccount?.name ?? bankAccounts?.[0]?.name ?? '';
     }, [bankAccounts, exportConfiguration?.nonReimbursableAccount]);
 
     const currentXeroOrganizationName = useMemo(() => getCurrentXeroOrganizationName(policy ?? undefined), [policy]);
