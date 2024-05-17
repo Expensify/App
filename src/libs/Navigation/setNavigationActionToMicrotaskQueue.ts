49b050f3426380dd85b89457ac98a258fb5ef038
Yauheni <HorbachZhenja@yandex.by>
2024-05-17 20:16:43 +0200

    rename param for setNavigationActionToMicrotaskQueue

--- src/libs/Navigation/setNavigationActionToMicrotaskQueue.ts
+++ src/libs/Navigation/setNavigationActionToMicrotaskQueue.ts
@@ -1,4 +1,4 @@
-export default function setNavigationActionToMicrotaskQueue(nabigationAction: () => void) {
+export default function setNavigationActionToMicrotaskQueue(navigationAction: () => void) {
     /**
      * The function is used when the app needs to set a navigation action to the microtask queue, it guarantees to execute Onyx.update first, then the navigation action.
      * More details - https://github.com/Expensify/App/issues/37785#issuecomment-1989056726.
@@ -7,7 +7,7 @@ export default function setNavigationActionToMicrotaskQueue(nabigationAction: ()
         resolve();
     }).then(() => {
         requestAnimationFrame(() => {
-            nabigationAction();
+            navigationAction();
         });
     });
 }
