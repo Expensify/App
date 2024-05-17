e8afcb5026c5d5fbf43e01c028e335867e6c9af6
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-05-17 15:23:05 +0200

    fix

--- src/pages/TransactionDuplicate/DuplicateTransactionItem.tsx
+++ src/pages/TransactionDuplicate/DuplicateTransactionItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type {OnyxEntry} from 'react-native-onyx';
 import {useOnyx} from 'react-native-onyx';
+import Text from '@components/Text';
 import * as ReportActionsUtils from '@libs/ReportActionsUtils';
 import ReportActionItem from '@pages/home/report/ReportActionItem';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -17,23 +18,26 @@ function DuplicateTransactionItem(props: DuplicateTransactionItemProps) {
     const parentReportAction = ReportActionsUtils.getReportAction(report?.parentReportID ?? '', report?.parentReportActionID ?? '');
 
     return (
-        <ReportActionItem
-            action={
+        <>
+            <Text>{props.transaction?.transactionID}</Text>
+            <ReportActionItem
+                action={
+                    // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+                    Object.values(reportActions ?? {})?.find(
+                        (reportAction) => reportAction.actionName === 'IOU' && reportAction.originalMessage.IOUTransactionID === props.transaction?.transactionID,
+                    ) as ReportAction
+                }
                 // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-                Object.values(reportActions ?? {})?.find(
-                    (reportAction) => reportAction.actionName === 'IOU' && reportAction.originalMessage.IOUTransactionID === props.transaction?.transactionID,
-                ) as ReportAction
-            }
-            // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
-            report={report as Report}
-            parentReportAction={parentReportAction}
-            index={props.index}
-            reportActions={Object.values(reportActions ?? {})}
-            displayAsGroup={false}
-            shouldDisplayNewMarker={false}
-            isMostRecentIOUReportAction={false}
-            isFirstVisibleReportAction={false}
-        />
+                report={report as Report}
+                parentReportAction={parentReportAction}
+                index={props.index}
+                reportActions={Object.values(reportActions ?? {})}
+                displayAsGroup={false}
+                shouldDisplayNewMarker={false}
+                isMostRecentIOUReportAction={false}
+                isFirstVisibleReportAction={false}
+            />
+        </>
     );
 }
 
