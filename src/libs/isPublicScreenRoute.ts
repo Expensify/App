5153699f6503b009225d5f7208b8bc7a5ec2fbeb
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-05-18 12:26:45 +0800

    dry-ing

--- src/libs/isPublicScreenRoute.ts
+++ src/libs/isPublicScreenRoute.ts
@@ -1,18 +1,7 @@
-import ROUTES from '@src/ROUTES';
-
-const publicScreenRoutes: string[] = [
-    ROUTES.ROOT,
-    ROUTES.TRANSITION_BETWEEN_APPS,
-    ROUTES.VALIDATE_LOGIN,
-    ROUTES.CONNECTION_COMPLETE,
-    ROUTES.UNLINK_LOGIN,
-    ROUTES.APPLE_SIGN_IN,
-    ROUTES.GOOGLE_SIGN_IN,
-    ROUTES.SAML_SIGN_IN,
-];
+import {PUBLIC_SCREENS_ROUTES} from '@src/ROUTES';
 
 export default function isPublicScreenRoute(route: string) {
-    return publicScreenRoutes.some((screenRoute) => {
+    return Object.values(PUBLIC_SCREENS_ROUTES).some((screenRoute) => {
         const routeRegex = new RegExp(`^${screenRoute.replace(/:\w+/g, '\\w+')}$`);
         return routeRegex.test(route);
     });
