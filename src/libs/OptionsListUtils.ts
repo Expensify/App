c36f29b16a99ecd6abe09d1af34a279370681211
gijoe0295 <gijoe0295@gmail.com>
2024-05-18 22:34:25 +0700

    use report instead of result

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -724,7 +724,7 @@ function createOption(
         result.tooltipText = ReportUtils.getReportParticipantsTitle(visibleParticipantAccountIDs);
         result.isOneOnOneChat = isOneOnOneChat;
 
-        hasMultipleParticipants = personalDetailList.length > 1 || result.isChatRoom || result.isPolicyExpenseChat || ReportUtils.isGroupChat(result);
+        hasMultipleParticipants = personalDetailList.length > 1 || result.isChatRoom || result.isPolicyExpenseChat || ReportUtils.isGroupChat(report);
         subtitle = ReportUtils.getChatRoomSubtitle(report);
 
         const lastActorDetails = personalDetailMap[report.lastActorAccountID ?? 0] ?? null;
@@ -1878,6 +1878,7 @@ function getOptions(
 
             // Skip if we aren't including multiple participant reports and this report has multiple participants
             if (!isCurrentUserOwnedPolicyExpenseChatThatCouldShow && !includeMultipleParticipantReports && !reportOption.login) {
+                console.log(reportOption);
                 continue;
             }
 
