555fd586457a161e1807467d3d535080b767225c
Hayata Suenaga <hayata@expensify.com>
2024-05-22 13:31:43 -0700

    pass default index manually

--- tests/actions/PolicyTagTest.ts
+++ tests/actions/PolicyTagTest.ts
@@ -395,7 +395,7 @@ describe('actions/Policy', () => {
                     Onyx.set(`${ONYXKEYS.COLLECTION.POLICY_TAGS}${fakePolicy.id}`, fakePolicyTags);
                 })
                 .then(() => {
-                    Policy.setWorkspaceTagEnabled(fakePolicy.id, tagsToUpdate, fakePolicyTags[0].orderWeight);
+                    Policy.setWorkspaceTagEnabled(fakePolicy.id, tagsToUpdate, 0);
                     return waitForBatchedUpdates();
                 })
                 .then(
@@ -468,7 +468,7 @@ describe('actions/Policy', () => {
                 .then(() => {
                     mockFetch?.fail?.();
 
-                    Policy.setWorkspaceTagEnabled(fakePolicy.id, tagsToUpdate, fakePolicyTags[0].orderWeight);
+                    Policy.setWorkspaceTagEnabled(fakePolicy.id, tagsToUpdate, 0);
                     return waitForBatchedUpdates();
                 })
                 .then(mockFetch?.resume)
