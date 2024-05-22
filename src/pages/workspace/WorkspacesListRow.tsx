1103541facbd21ffa52520358b37e0f2af60061b
Krishna Gupta <belivethatkg@gmail.com>
2024-05-22 14:25:54 +0530

    fix: workspace list row padding.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/pages/workspace/WorkspacesListRow.tsx
+++ src/pages/workspace/WorkspacesListRow.tsx
@@ -142,9 +142,9 @@ function WorkspacesListRow({
     const isDeleted = style && Array.isArray(style) ? style.includes(styles.offlineFeedback.deleted) : false;
 
     const ThreeDotMenuOrPendingIcon = (
-        <View style={[styles.flexRow, isNarrow && styles.mr5]}>
+        <View style={[styles.flexRow]}>
             {isJoinRequestPending && (
-                <View style={[styles.flexRow, styles.gap2, styles.alignItemsCenter, styles.flex1, styles.justifyContentEnd, !isNarrow && styles.pr4, isNarrow && styles.workspaceListBadge]}>
+                <View style={[styles.flexRow, styles.gap2, styles.alignItemsCenter, styles.flex1, styles.justifyContentEnd, isNarrow && styles.workspaceListBadge]}>
                     <Badge
                         text={translate('workspace.common.requested')}
                         textStyles={styles.textStrong}
@@ -154,14 +154,11 @@ function WorkspacesListRow({
                 </View>
             )}
             {!isJoinRequestPending && (
-                <>
-                    <View style={[styles.flexRow, styles.gap2, isNarrow && styles.mr5, styles.alignItemsCenter, isNarrow && styles.workspaceListRBR]}>
+                <View style={[styles.flexRow, styles.ml5, styles.gap1, !isSmallScreenWidth && styles.workspaceThreeDotMenu]}>
+                    <View style={[styles.flexRow, styles.gap2, styles.alignItemsCenter, isNarrow && styles.workspaceListRBR]}>
                         <BrickRoadIndicatorIcon brickRoadIndicator={brickRoadIndicator} />
                     </View>
-                    <View
-                        ref={threeDotsMenuContainerRef}
-                        style={[!isSmallScreenWidth && styles.workspaceThreeDotMenu]}
-                    >
+                    <View ref={threeDotsMenuContainerRef}>
                         <ThreeDotsMenu
                             onIconPress={() => {
                                 if (isSmallScreenWidth) {
@@ -181,13 +178,13 @@ function WorkspacesListRow({
                             disabled={shouldDisableThreeDotsMenu}
                         />
                     </View>
-                </>
+                </View>
             )}
         </View>
     );
 
     return (
-        <View style={[styles.flexRow, styles.highlightBG, rowStyles, style, isWide && styles.gap5, styles.br3, styles.pv5, styles.pl5]}>
+        <View style={[styles.flexRow, styles.highlightBG, rowStyles, style, isWide && styles.gap5, styles.br3, styles.p5]}>
             <View style={[isWide ? styles.flexRow : styles.flexColumn, styles.flex1, isWide && styles.gap5]}>
                 <View style={[styles.flexRow, styles.justifyContentBetween, styles.flex1, isNarrow && styles.mb3, styles.alignItemsCenter]}>
                     <View style={[styles.flexRow, styles.gap3, styles.flex1, styles.alignItemsCenter]}>
