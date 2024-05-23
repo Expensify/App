5d98734518f3e4a5c9631731cb73595fa5740605
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-05-23 14:09:59 +0200

    add screen title to translations

--- src/pages/settings/Subscription/SubscriptionSettingsPage.tsx
+++ src/pages/settings/Subscription/SubscriptionSettingsPage.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Illustrations from '@components/Icon/Illustrations';
 import ScreenWrapper from '@components/ScreenWrapper';
+import useLocalize from '@hooks/useLocalize';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import Navigation from '@libs/Navigation/Navigation';
 
 function SubscriptionSettingsPage() {
     const {isSmallScreenWidth} = useWindowDimensions();
+    const {translate} = useLocalize();
 
     return (
         <ScreenWrapper testID={SubscriptionSettingsPage.displayName}>
             <HeaderWithBackButton
-                title="Subscription"
+                title={translate('workspace.common.subscription')}
                 onBackButtonPress={() => Navigation.goBack()}
                 shouldShowBackButton={isSmallScreenWidth}
                 icon={Illustrations.CreditCardsNew}
