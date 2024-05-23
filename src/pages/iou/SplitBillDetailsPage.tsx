ccec593e427b45bfec1291a68dc1519aabc825c6
Krishna Gupta <belivethatkg@gmail.com>
2024-05-23 23:36:24 +0530

    minor fix.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/pages/iou/SplitBillDetailsPage.tsx
+++ src/pages/iou/SplitBillDetailsPage.tsx
@@ -113,6 +113,7 @@ function SplitBillDetailsPage({personalDetails, report, route, reportActions, tr
                             iouCurrencyCode={splitCurrency}
                             iouComment={splitComment}
                             iouCreated={splitCreated}
+                            isSplitBillDetailsPage
                             iouMerchant={splitMerchant}
                             iouCategory={splitCategory}
                             iouIsBillable={splitBillable}
@@ -122,7 +123,6 @@ function SplitBillDetailsPage({personalDetails, report, route, reportActions, tr
                             receiptPath={transaction?.receipt?.source}
                             receiptFilename={transaction?.filename}
                             isEditingSplitBill={isEditingSplitBill}
-                            isSplitBillDetailsPage
                             hasSmartScanFailed={hasSmartScanFailed}
                             reportID={reportID}
                             reportActionID={reportAction?.reportActionID}
