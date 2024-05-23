2f008b5e343a1bee3e3053356fed85732edbb8b4
Adam Grzybowski <adam.grzybowski@swmansion.com>
2024-05-23 13:02:00 +0200

    fix problem with deeplinks and policyIDs v2

--- src/libs/Navigation/linkTo/index.ts
+++ src/libs/Navigation/linkTo/index.ts
@@ -90,7 +90,7 @@ export default function linkTo(navigation: NavigationContainerRef<RootStackParam
             // We need to push a tab if the tab doesn't match the central pane route that we are going to push.
             const topmostBottomTabRoute = getTopmostBottomTabRoute(rootState);
             const policyIDsFromState = extractPolicyIDsFromState(stateFromPath);
-            const matchingBottomTabRoute = getMatchingBottomTabRouteForState(stateFromPath, policyID !== '' ? policyID : policyIDsFromState);
+            const matchingBottomTabRoute = getMatchingBottomTabRouteForState(stateFromPath, policyID ? policyID : policyIDsFromState);
             const isNewPolicyID =
                 (topmostBottomTabRoute?.params as Record<string, string | undefined>)?.policyID !== (matchingBottomTabRoute?.params as Record<string, string | undefined>)?.policyID;
             if (topmostBottomTabRoute && (topmostBottomTabRoute.name !== matchingBottomTabRoute.name || isNewPolicyID)) {
