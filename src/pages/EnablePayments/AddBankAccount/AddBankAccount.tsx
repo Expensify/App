c1bea82678c9a16796fdef26b914c48a2795691f
Agata Kosior <agata.kosior57@gmail.com>
2024-05-24 15:02:55 +0200

    fix: remove unnecessary comment

--- src/pages/EnablePayments/AddBankAccount/AddBankAccount.tsx
+++ src/pages/EnablePayments/AddBankAccount/AddBankAccount.tsx
@@ -54,7 +54,6 @@ function AddBankAccount({personalBankAccount, plaidData, personalBankAccountDraf
 
     const exitFlow = (shouldContinue = false) => {
         const exitReportID = personalBankAccount?.exitReportID;
-        // TODO: https://github.com/Expensify/App/issues/36648 This should be updated to the correct route once the refactor is complete
         const onSuccessFallbackRoute = personalBankAccount?.onSuccessFallbackRoute ?? '';
 
         if (exitReportID) {
