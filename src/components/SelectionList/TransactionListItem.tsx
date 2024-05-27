447acc4714083c68c01d0584a5127eb21c7084f0
Carlos Martins <cmartins@expensify.com>
2024-05-27 16:21:23 -0600

    create areReceiptPropsEqual

--- src/components/SelectionList/TransactionListItem.tsx
+++ src/components/SelectionList/TransactionListItem.tsx
@@ -54,31 +54,32 @@ function arePropsEqual(prevProps: CellProps, nextProps: CellProps) {
     return prevProps.keyForList === nextProps.keyForList;
 }
 
-const ReceiptCell = memo(
-    ({transactionItem, isHovered = false}: ReceiptCellProps) => {
-        const theme = useTheme();
-        const styles = useThemeStyles();
-        const StyleUtils = useStyleUtils();
+function areReceiptPropsEqual(prevProps: ReceiptCellProps, nextProps: ReceiptCellProps) {
+    return prevProps.keyForList === nextProps.keyForList && prevProps.isHovered === nextProps.isHovered;
+}
 
-        return (
-            <View style={[StyleUtils.getWidthAndHeightStyle(variables.h36, variables.w40), StyleUtils.getBorderRadiusStyle(variables.componentBorderRadiusSmall), styles.overflowHidden]}>
-                <ReceiptImage
-                    source={tryResolveUrlFromApiRoot(transactionItem?.receipt?.source ?? '')}
-                    isEReceipt={transactionItem.hasEReceipt}
-                    transactionID={transactionItem.transactionID}
-                    shouldUseThumbnailImage={!transactionItem?.receipt?.source}
-                    isAuthTokenRequired
-                    fallbackIcon={Expensicons.ReceiptPlus}
-                    fallbackIconSize={20}
-                    fallbackIconColor={theme.icon}
-                    iconSize="x-small"
-                    isHovered={isHovered}
-                />
-            </View>
-        );
-    },
-    (prevProps, nextProps) => arePropsEqual(prevProps, nextProps) && prevProps.isHovered === nextProps.isHovered,
-);
+const ReceiptCell = memo(({transactionItem, isHovered = false}: ReceiptCellProps) => {
+    const theme = useTheme();
+    const styles = useThemeStyles();
+    const StyleUtils = useStyleUtils();
+
+    return (
+        <View style={[StyleUtils.getWidthAndHeightStyle(variables.h36, variables.w40), StyleUtils.getBorderRadiusStyle(variables.componentBorderRadiusSmall), styles.overflowHidden]}>
+            <ReceiptImage
+                source={tryResolveUrlFromApiRoot(transactionItem?.receipt?.source ?? '')}
+                isEReceipt={transactionItem.hasEReceipt}
+                transactionID={transactionItem.transactionID}
+                shouldUseThumbnailImage={!transactionItem?.receipt?.source}
+                isAuthTokenRequired
+                fallbackIcon={Expensicons.ReceiptPlus}
+                fallbackIconSize={20}
+                fallbackIconColor={theme.icon}
+                iconSize="x-small"
+                isHovered={isHovered}
+            />
+        </View>
+    );
+}, areReceiptPropsEqual);
 
 const DateCell = memo(({showTooltip, date, isLargeScreenWidth}: DateCellProps) => {
     const styles = useThemeStyles();
