369f92ec67b45f744ad633fa62d3ac0652a83d74
Manan Jadhav <manan.jadhav@gmail.com>
2024-05-28 14:14:47 +0530

    fix: added dependency

--- src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
+++ src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
@@ -258,7 +258,7 @@ function WorkspaceMoreFeaturesPage({policy, route}: WorkspaceMoreFeaturesPagePro
             default:
                 return undefined;
         }
-    }, [connectionWarningModalState.itemType]);
+    }, [connectionWarningModalState.itemType, connectionName, translate]);
 
     return (
         <AccessOrNotFoundWrapper
