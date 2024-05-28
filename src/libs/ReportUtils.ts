4bdd2808c3955676de458a3b8d3081a4834f20ea
Tsaqif <tsaiinkwa@yahoo.com>
2024-05-28 08:57:08 +0700

    Change variable names, add comment and move cache check inside parse function
    Signed-off-by: Tsaqif <tsaiinkwa@yahoo.com>

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -487,7 +487,11 @@ let currentUserEmail: string | undefined;
 let currentUserPrivateDomain: string | undefined;
 let currentUserAccountID: number | undefined;
 let isAnonymousUser = false;
-const reportActionMessageCache: Record<string, string> = {};
+
+// This cache is used to save parse result of report action html message into text
+// to prevent unnecessary parsing when the report action is not changed/modified.
+// Example case: when we need to get a report name of a thread which is dependent on a report action message.
+const parsedReportActionMessageCache: Record<string, string> = {};
 
 const defaultAvatarBuildingIconTestID = 'SvgDefaultAvatarBuilding Icon';
 
@@ -3069,7 +3073,16 @@ function getInvoicePayerName(report: OnyxEntry<Report>): string {
 /**
  * Parse html of reportAction into text
  */
-function parseReportActionHtmlToText(reportAction: OnyxEntry<ReportAction>, reportID: string): string {
+function parseReportActionHtmlToText(reportAction: OnyxEntry<ReportAction>, reportID: string, childReportID?: string): string {
+    if (!reportAction) {
+        return '';
+    }
+    const key = `${reportID}_${reportAction.reportActionID}_${reportAction.lastModified}`;
+    const cachedText = parsedReportActionMessageCache[key];
+    if (cachedText !== undefined) {
+        return cachedText;
+    }
+
     const {html, text} = reportAction?.message?.[0] ?? {};
 
     if (!html) {
@@ -3079,28 +3092,31 @@ function parseReportActionHtmlToText(reportAction: OnyxEntry<ReportAction>, repo
     const mentionReportRegex = /<mention-report reportID="(\d+)" *\/>/gi;
     const matches = html.matchAll(mentionReportRegex);
 
-    const reportIdToName: Record<string, string> = {};
+    const reportIDToName: Record<string, string> = {};
     for (const match of matches) {
-        if (match[1] !== reportID) {
+        if (match[1] !== childReportID) {
             // eslint-disable-next-line @typescript-eslint/no-use-before-define
-            reportIdToName[match[1]] = getReportName(getReport(match[1])) ?? '';
+            reportIDToName[match[1]] = getReportName(getReport(match[1])) ?? '';
         }
     }
 
     const mentionUserRegex = /<mention-user accountID="(\d+)" *\/>/gi;
-    const accountIdToName: Record<string, string> = {};
-    const accountIds = Array.from(html.matchAll(mentionUserRegex), (m) => Number(m[1]));
-    const logins = PersonalDetailsUtils.getLoginsByAccountIDs(accountIds);
-    accountIds.forEach((id, index) => (accountIdToName[id] = logins[index]));
+    const accountIDToName: Record<string, string> = {};
+    const accountIDs = Array.from(html.matchAll(mentionUserRegex), (mention) => Number(mention[1]));
+    const logins = PersonalDetailsUtils.getLoginsByAccountIDs(accountIDs);
+    accountIDs.forEach((id, index) => (accountIDToName[id] = logins[index]));
 
     const parser = new ExpensiMark();
-    return parser.htmlToText(html, {reportIdToName});
+    const textMessage = Str.removeSMSDomain(parser.htmlToText(html, {reportIDToName, accountIDToName}));
+    parsedReportActionMessageCache[key] = textMessage;
+
+    return textMessage;
 }
 
 /**
  * Get the report action message for a report action.
  */
-function getReportActionMessage(reportAction: ReportAction | EmptyObject, reportID: string, parentReportID?: string) {
+function getReportActionMessage(reportAction: ReportAction | EmptyObject, reportID?: string, childReportID?: string) {
     if (isEmptyObject(reportAction)) {
         return '';
     }
@@ -3108,17 +3124,10 @@ function getReportActionMessage(reportAction: ReportAction | EmptyObject, report
         return ReportActionsUtils.getReportActionMessageText(reportAction);
     }
     if (ReportActionsUtils.isReimbursementQueuedAction(reportAction)) {
-        return getReimbursementQueuedActionMessage(reportAction, getReport(parentReportID), false);
+        return getReimbursementQueuedActionMessage(reportAction, getReport(reportID), false);
     }
 
-    const key = `${reportAction.reportActionID}_${reportAction.lastModified}`;
-    const cachedMessage = reportActionMessageCache[key];
-    if (cachedMessage !== undefined) {
-        return cachedMessage;
-    }
-    const message = Str.removeSMSDomain(parseReportActionHtmlToText(reportAction, reportID));
-    reportActionMessageCache[key] = message;
-    return message;
+    return parseReportActionHtmlToText(reportAction, reportID, childReportID);
 }
 
 /**
@@ -3164,7 +3173,7 @@ function getReportName(report: OnyxEntry<Report>, policy: OnyxEntry<Policy> = nu
         }
 
         const isAttachment = ReportActionsUtils.isReportActionAttachment(!isEmptyObject(parentReportAction) ? parentReportAction : null);
-        const parentReportActionMessage = getReportActionMessage(parentReportAction, report?.reportID ?? '', report?.parentReportID).replace(/(\r\n|\n|\r)/gm, ' ');
+        const parentReportActionMessage = getReportActionMessage(parentReportAction, report?.parentReportID, report?.reportID ?? '').replace(/(\r\n|\n|\r)/gm, ' ');
         if (isAttachment && parentReportActionMessage) {
             return `[${Localize.translateLocal('common.attachment')}]`;
         }
