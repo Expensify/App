d6e9e0e7f54fb3a25fb5d66ec0fe29ab76e74ecc
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-05-29 16:03:21 +0200

    add heads up section

--- src/pages/settings/Subscription/SubscriptionDetails/index.tsx
+++ src/pages/settings/Subscription/SubscriptionDetails/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as Expensicons from '@components/Icon/Expensicons';
 import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
 import Section from '@components/Section';
 import Text from '@components/Text';
@@ -16,13 +15,16 @@ function SubscriptionDetails() {
             isCentralPane
             titleStyles={styles.textStrong}
         >
-            <Text>Test</Text>
             <MenuItemWithTopDescription
                 description={translate('subscription.details.subscriptionSize')}
-                // title='20'
-                iconRight={Expensicons.ArrowRight}
                 shouldShowRightIcon
+                wrapperStyle={styles.sectionMenuItemTopDescription}
+                style={styles.mt5}
             />
+            <Text style={styles.mt2}>
+                <Text style={styles.h4}>{translate('subscription.details.headsUpTitle')}</Text>
+                <Text style={styles.textLabelSupporting}>{translate('subscription.details.headsUpBody')}</Text>
+            </Text>
         </Section>
     );
 }
