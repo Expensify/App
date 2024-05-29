ad202800542b480ab212b5acabe88b73c2834a7f
Andrew Rosiclair <arosiclair@expensify.com>
2024-05-29 12:02:07 -0400

    Revert "Merge pull request #35045 from callstack-internal/issues/30123"
    This reverts commit 849a0d40cc21aa9f2ee85b0c77902de78c77395f, reversing
    changes made to 611b86f7a1755d2e678fbb271cd101fc966073a9.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3481,6 +3481,7 @@ const styles = (theme: ThemeColors) =>
             fontSize: variables.fontSizeNormal,
             lineHeight: variables.fontSizeNormalHeight,
             fontFamily: FontUtils.fontFamily.platform.EXP_NEUE,
+            flex: 1,
         },
 
         searchPressable: {
