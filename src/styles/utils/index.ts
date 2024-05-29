2b25e7f2118e2418dd81c284a594513859ee2fc7
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-05-29 14:27:59 +0800

    remove unused import

--- src/styles/utils/index.ts
+++ src/styles/utils/index.ts
@@ -5,7 +5,6 @@ import type {EdgeInsets} from 'react-native-safe-area-context';
 import type {ValueOf} from 'type-fest';
 import type ImageSVGProps from '@components/ImageSVG/types';
 import * as Browser from '@libs/Browser';
-import * as EmojiUtils from '@libs/EmojiUtils';
 import * as UserUtils from '@libs/UserUtils';
 // eslint-disable-next-line no-restricted-imports
 import {defaultTheme} from '@styles/theme';
