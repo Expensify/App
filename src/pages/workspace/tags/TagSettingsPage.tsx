46eb25b2b4ae570a40ffa508b6594f453d2662e4
cdOut <88325488+cdOut@users.noreply.github.com>
2024-05-29 18:39:01 +0200

    fix prettier

--- src/pages/workspace/tags/TagSettingsPage.tsx
+++ src/pages/workspace/tags/TagSettingsPage.tsx
@@ -14,13 +14,13 @@ import Switch from '@components/Switch';
 import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {setWorkspaceTagEnabled} from '@userActions/Policy/Policy'
 import * as ErrorUtils from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import * as PolicyUtils from '@libs/PolicyUtils';
 import type {SettingsNavigatorParamList} from '@navigation/types';
 import NotFoundPage from '@pages/ErrorPage/NotFoundPage';
 import AccessOrNotFoundWrapper from '@pages/workspace/AccessOrNotFoundWrapper';
+import {setWorkspaceTagEnabled} from '@userActions/Policy/Policy';
 import * as Policy from '@userActions/Policy/Policy';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
