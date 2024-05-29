f345f49d04231156302827aaafe9942b1b0be78d
Krishna Gupta <belivethatkg@gmail.com>
2024-05-30 03:53:29 +0530

    fix lint issue.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -81,7 +81,7 @@ import * as ReportActionsUtils from './ReportActionsUtils';
 import StringUtils from './StringUtils';
 import * as TransactionUtils from './TransactionUtils';
 import * as Url from './Url';
-import type * as UserUtils from './UserUtils';
+import * as UserUtils from './UserUtils';
 
 type AvatarRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18;
 
