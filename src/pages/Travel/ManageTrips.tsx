9b5a83708c064603867f95fd3223c688a58d4ae6
Rushat Gabhane <rushatgabhane@gmail.com>
2024-05-31 00:50:57 +0530

    add secondary button to book travel demo on trips page

--- src/pages/Travel/ManageTrips.tsx
+++ src/pages/Travel/ManageTrips.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Linking, View} from 'react-native';
 import type {FeatureListItem} from '@components/FeatureList';
 import FeatureList from '@components/FeatureList';
 import * as Illustrations from '@components/Icon/Illustrations';
@@ -10,6 +10,7 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import Navigation from '@libs/Navigation/Navigation';
 import colors from '@styles/theme/colors';
+import CONST from '@src/CONST';
 import ROUTES from '@src/ROUTES';
 import getTripIllustrationStyle from './getTripIllustrationStyle';
 
@@ -30,6 +31,10 @@ function ManageTrips() {
     const {translate} = useLocalize();
     const illustrationStyle = getTripIllustrationStyle();
 
+    const navigateToBookTravelDemo = () => {
+        Linking.openURL(CONST.BOOK_TRAVEL_DEMO_URL);
+    };
+
     return (
         <ScrollView contentContainerStyle={styles.pt3}>
             <View style={[styles.flex1, isSmallScreenWidth ? styles.workspaceSectionMobile : styles.workspaceSection]}>
@@ -42,6 +47,9 @@ function ManageTrips() {
                     onCtaPress={() => {
                         Navigation.navigate(ROUTES.TRAVEL_TCS);
                     }}
+                    secondaryButtonText={translate('travel.bookDemo')}
+                    secondaryButtonAccessibilityLabel={translate('travel.bookDemo')}
+                    onSecondaryButtonPress={navigateToBookTravelDemo}
                     illustration={LottieAnimations.Plane}
                     illustrationStyle={illustrationStyle}
                     illustrationBackgroundColor={colors.blue600}
