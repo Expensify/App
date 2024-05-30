851f4d8cdfda3bb8c4ccdbb7cc7f495ca1d63925
Carlos Martins <cmartins@expensify.com>
2024-05-30 09:28:30 -0600

    Merge pull request #42805 from Krishna2323/krishna2323/issue/42751
    [CP Staging] fix: Chat - Display Name Appears Black and Members Not Ticked in Group Preview
    
    (cherry picked from commit 99b0563aab8bcaad8f8b12db57f6dbe413965a2b)

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -81,7 +81,12 @@ import * as ReportActionsUtils from './ReportActionsUtils';
 import StringUtils from './StringUtils';
 import * as TransactionUtils from './TransactionUtils';
 import * as Url from './Url';
+<<<<<<< HEAD
 import type * as UserUtils from './UserUtils';
+=======
+import type {AvatarSource} from './UserUtils';
+import * as UserUtils from './UserUtils';
+>>>>>>> 99b0563 (Merge pull request #42805 from Krishna2323/krishna2323/issue/42751)
 
 type AvatarRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18;
 
@@ -117,7 +122,7 @@ type SpendBreakdown = {
     totalDisplaySpend: number;
 };
 
-type ParticipantDetails = [number, string, UserUtils.AvatarSource, UserUtils.AvatarSource];
+type ParticipantDetails = [number, string, AvatarSource, AvatarSource];
 
 type OptimisticAddCommentReportAction = Pick<
     ReportAction,
@@ -587,7 +592,7 @@ function getLastUpdatedReport(): OnyxEntry<Report> {
     return lastUpdatedReport;
 }
 
-function getCurrentUserAvatar(): UserUtils.AvatarSource | undefined {
+function getCurrentUserAvatar(): AvatarSource | undefined {
     return currentUserPersonalDetails?.avatar;
 }
 
@@ -1714,7 +1719,7 @@ function getDefaultWorkspaceAvatarTestID(workspaceName: string): string {
     return !alphaNumeric ? defaultAvatarBuildingIconTestID : `SvgDefaultAvatar_${alphaNumeric[0]} Icon`;
 }
 
-function getWorkspaceAvatar(report: OnyxEntry<Report>): UserUtils.AvatarSource {
+function getWorkspaceAvatar(report: OnyxEntry<Report>): AvatarSource {
     const workspaceName = getPolicyName(report, false, allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${report?.policyID}`]);
     const avatar = allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${report?.policyID}`]?.avatarURL ?? '';
     return !isEmpty(avatar) ? avatar : getDefaultWorkspaceAvatar(workspaceName);
@@ -1921,7 +1926,7 @@ function getParticipants(reportID: string) {
 function getIcons(
     report: OnyxEntry<Report>,
     personalDetails: OnyxCollection<PersonalDetails>,
-    defaultIcon: UserUtils.AvatarSource | null = null,
+    defaultIcon: AvatarSource | null = null,
     defaultName = '',
     defaultAccountID = -1,
     policy: OnyxEntry<Policy> = null,
