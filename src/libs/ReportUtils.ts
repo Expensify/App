3f22789dc18ab91bc12eedb645b8ff9ce272a3db
Carlos Martins <cmartins@expensify.com>
2024-05-30 14:49:41 -0600

    resolve conflicts

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -81,12 +81,7 @@ import * as ReportActionsUtils from './ReportActionsUtils';
 import StringUtils from './StringUtils';
 import * as TransactionUtils from './TransactionUtils';
 import * as Url from './Url';
-<<<<<<< HEAD
-import type * as UserUtils from './UserUtils';
-=======
 import type {AvatarSource} from './UserUtils';
-import * as UserUtils from './UserUtils';
->>>>>>> 99b0563 (Merge pull request #42805 from Krishna2323/krishna2323/issue/42751)
 
 type AvatarRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18;
 
