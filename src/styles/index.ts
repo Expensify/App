1e6ff814b07b699a51e4b144bd3ee5034b465513
Carlos Martins <cmartins@expensify.com>
2024-05-31 08:59:54 -0600

    fix report total and alignment

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4939,10 +4939,6 @@ const styles = (theme: ThemeColors) =>
             fontSize: variables.fontSizeNormal,
             fontWeight: FontUtils.fontWeight.bold,
         },
-
-        reportListItemActionButtonMargin: {
-            marginLeft: variables.searchTypeColumnWidth,
-        },
     } satisfies Styles);
 
 type ThemeStyles = ReturnType<typeof styles>;
