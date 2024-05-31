074ad978d465312da275fc59a9f68791b09b8bd0
Rushat Gabhane <rushatgabhane@gmail.com>
2024-05-31 15:35:40 +0530

    add articles dir

--- .github/scripts/enforceRedirect.sh
+++ .github/scripts/enforceRedirect.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# HelpDot - Whenever an article is moved/deleted we should verify that
+# HelpDot - Whenever an article is moved/renamed/deleted we should verify that
 # we have added a redirect link for it in redirects.csv. This ensures that we don't have broken links.
 
 declare -r REDIRECTS_FILE="docs/redirects.csv"
+declare -r ARTICLES_DIRECTORY="docs/articles"
 
+# shouldAddRedirect=git
 hasAddedRedirect=git diff $REDIRECTS_FILE
+
+
 echo $hasAddedRedirect
