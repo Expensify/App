300f23ee1bec8c48795d171739a577ad7976fe3f
Vivek Kumar <vivek@expensify.com>
2024-05-31 08:09:05 +0530

    fix type

--- src/libs/API/parameters/SignUpUserParams.ts
+++ src/libs/API/parameters/SignUpUserParams.ts
@@ -3,7 +3,7 @@ import type CONST from '@src/CONST';
 
 type SignUpUserParams = {
     email?: string;
-    preferredLocale: ValueOf<typeof CONST.LOCALES>
+    preferredLocale: ValueOf<typeof CONST.LOCALES> | null;
 };
 
 export default SignUpUserParams;
