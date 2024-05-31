a403ac13101a1e6bc08dd90ff213818b35499d1e
Rushat Gabhane <rushatgabhane@gmail.com>
2024-05-31 19:18:24 +0530

    working grep to detect rename / delete / modified

--- .github/scripts/enforceRedirect.sh
+++ .github/scripts/enforceRedirect.sh
@@ -6,9 +6,18 @@
 declare -r REDIRECTS_FILE="docs/redirects.csv"
 declare -r ARTICLES_DIRECTORY="docs/articles"
 
-hasMovedOrDeletedArticle=$(git log main.. --name-status --pretty=format: docs/articles | grep -E "^(R|D)")
-hasModifiedRedirect=$(git log main.. --name-status --pretty=format: docs/redirects.csv | grep "^M")
+hasMovedOrDeletedArticle=false
+hasModifiedRedirect=false
+
+if git log main.. --name-status --pretty=format: docs/articles | grep -q -E "^(R|D)"
+then
+    hasMovedOrDeletedArticle=true
+fi
+
+if git log main.. --name-status --pretty=format: docs/redirects.csv | grep -E "^(M)"
+then
+    hasModifiedRedirect=true
+fi
 
 echo $hasMovedOrDeletedArticle
-
 echo $hasModifiedRedirect
