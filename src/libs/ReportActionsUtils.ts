e73fb372346be1cdc6ae7cf352eb2dd92328e8cc
nkdengineer <161821005+nkdengineer@users.noreply.github.com>
2024-06-04 12:27:05 +0700

    Update src/libs/ReportActionsUtils.ts
    Co-authored-by: ahmedGaber93 <41129870+ahmedGaber93@users.noreply.github.com>

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -320,7 +320,7 @@ function getCombinedReportActions(reportActions: ReportAction[], transactionThre
 
     const report = allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportID}`];
     const isSelfDM = report?.chatType === CONST.REPORT.CHAT_TYPE.SELF_DM;
-    // Filter out request actions because we don't want to show any preview actions for one transaction reports
+    // Filter out request money actions because we don't want to show any preview actions for one transaction reports
     const filteredReportActions = [...reportActions, ...filteredTransactionThreadReportActions].filter((action) => {
         const actionType = (action as OriginalMessageIOU).originalMessage?.type ?? '';
         if (isSelfDM) {
