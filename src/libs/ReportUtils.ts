5b0fd9266bd20c0649c0121e805dbfd5412b7ce4
Rushat Gabhane <rushatgabhane@gmail.com>
2024-06-05 01:19:01 +0300

    get trip id from transaction

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -6638,6 +6638,10 @@ function getTripTransactions(tripRoomReportID: string | undefined): Transaction[
     return tripTransactionReportIDs.flatMap((reportID) => TransactionUtils.getAllReportTransactions(reportID));
 }
 
+function getTripIDFromTransactionParentReport(transactionParentReport: OnyxEntry<Report> | undefined | null): string | undefined {
+    return getReport(transactionParentReport?.parentReportID)?.tripData?.tripId;
+}
+
 /**
  * Checks if report contains actions with errors
  */
@@ -7075,6 +7079,7 @@ export {
     updateReportPreview,
     temporary_getMoneyRequestOptions,
     getTripTransactions,
+    getTripIDFromTransactionParentReport,
     buildOptimisticInvoiceReport,
     getInvoiceChatByParticipants,
     shouldShowMerchantColumn,
