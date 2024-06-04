09ba2713897fc11e8a05f877344212b9bf498137
Wojciech Boman <wojciechboman@gmail.com>
2024-06-04 14:08:57 +0200

    Add docs for withPrepareCentralPaneScreen

--- src/components/withPrepareCentralPaneScreen/index.native.tsx
+++ src/components/withPrepareCentralPaneScreen/index.native.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import getComponentDisplayName from '@libs/getComponentDisplayName';
 import FreezeWrapper from '@libs/Navigation/FreezeWrapper';
 
+/**
+ * This HOC is dependent on platform. On native platforms screens that aren't already dipslayed in the navigation stack should be freezed to prevent from unnecessary rendering.
+ * It's handled this way only on mobile platforms, because on web more than one screen is displayed in a wide layout, so these screens shouldn't be freezed.
+ */
 export default function withPrepareCentralPaneScreen<TProps, TRef>(
     WrappedComponent: ComponentType<TProps & RefAttributes<TRef>>,
 ): (props: TProps & React.RefAttributes<TRef>) => React.ReactElement | null {
--- src/components/withPrepareCentralPaneScreen/index.tsx
+++ src/components/withPrepareCentralPaneScreen/index.tsx
@@ -1,5 +1,9 @@
 import type {ComponentType} from 'react';
 
+/**
+ * This HOC is dependent on platform. On native platforms screens that aren't already dipslayed in the navigation stack should be freezed to prevent from unnecessary rendering.
+ * It's handled this way only on mobile platforms, because on web more than one screen is displayed in a wide layout, so these screens shouldn't be freezed.
+ */
 export default function withPrepareCentralPaneScreen(WrappedComponent: ComponentType) {
     return WrappedComponent;
 }
