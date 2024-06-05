78cb66102120c9525d4e91e1fdd52736a740855d
Rushat Gabhane <rushatgabhane@gmail.com>
2024-06-06 02:46:31 +0300

    add nav type

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -200,6 +200,10 @@ type SettingsNavigatorParamList = {
         categoryName: string;
         backTo?: Routes;
     };
+    [SCREENS.WORKSPACE.CATEGORY_PAYROLL_CODE]: {
+        policyID: string;
+        categoryName: string;
+    };
     [SCREENS.WORKSPACE.CATEGORY_SETTINGS]: {
         policyID: string;
         categoryName: string;
