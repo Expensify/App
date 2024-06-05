2cec124f843f43816a9322de70b9644ac85565b1
Rushat Gabhane <rushatgabhane@gmail.com>
2024-06-06 02:51:11 +0300

    add payroll code to category type

--- src/types/onyx/PolicyCategory.ts
+++ src/types/onyx/PolicyCategory.ts
@@ -17,6 +17,9 @@ type PolicyCategory = OnyxCommon.OnyxValueWithOfflineFeedback<{
     // eslint-disable-next-line @typescript-eslint/naming-convention
     'GL Code'?: string;
 
+    /** Payroll code is used to keep track of taxes, deductions, and an employee’s earnings */
+    payrollCode?: string;
+
     /** An ID for this category from an external accounting system */
     externalID?: string;
 
