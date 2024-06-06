ec885801cfdb0bf54df34308bdfffd7fb693e7c9
Yauheni Pasiukevich <pasyukevich@live.com>
2024-06-06 09:32:09 +0200

    revert default card

--- src/pages/settings/Subscription/CardSection/CardSection.tsx
+++ src/pages/settings/Subscription/CardSection/CardSection.tsx
@@ -20,7 +20,7 @@ function CardSection() {
     const theme = useTheme();
     const [fundList] = useOnyx(ONYXKEYS.FUND_LIST);
 
-    const defaultCard = useMemo(() => Object.values(fundList ?? {}).find((card) => card.accountData?.additionalData?.isBillingCard), [fundList]);
+    const defaultCard = useMemo(() => Object.values(fundList ?? {}).find((card) => card.isDefault), [fundList]);
 
     const cardMonth = useMemo(() => DateUtils.getMonthNames(preferredLocale)[(defaultCard?.accountData?.cardMonth ?? 1) - 1], [defaultCard?.accountData?.cardMonth, preferredLocale]);
 
