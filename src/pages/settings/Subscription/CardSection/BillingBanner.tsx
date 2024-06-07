bdd0adc6e2b86741f8d8294b835003839df53c04
Yauheni Pasiukevich <pasyukevich@live.com>
2024-06-07 14:03:37 +0200

    implement billing banner

---
+++ src/pages/settings/Subscription/CardSection/BillingBanner.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View} from 'react-native';
+import Icon from '@components/Icon';
+import * as Expensicons from '@components/Icon/Expensicons';
+import * as Illustrations from '@components/Icon/Illustrations';
+import Text from '@components/Text';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
+
+type BillingBannerProps = {
+    title: string;
+    subtitle: string;
+    isError?: boolean;
+    shouldShowRedDotIndicator?: boolean;
+};
+
+function BillingBanner({title, subtitle, isError, shouldShowRedDotIndicator}: BillingBannerProps) {
+    const styles = useThemeStyles();
+    const theme = useTheme();
+
+    return (
+        <View style={[styles.pt4, styles.pb3, styles.ph5, styles.flexRow, styles.gap3, styles.w100, styles.alignItemsCenter, styles.hoveredComponentBG]}>
+            <Icon
+                src={isError ? Illustrations.CreditCardEyes : Illustrations.CheckmarkCircle}
+                width={48}
+                height={48}
+            />
+            <View style={[styles.flex1, styles.justifyContentCenter]}>
+                <Text style={[styles.textStrong, styles.textLarge]}>{title}</Text>
+                <Text style={styles.colorMuted}>{subtitle}</Text>
+            </View>
+            {isError && shouldShowRedDotIndicator && (
+                <Icon
+                    src={Expensicons.DotIndicator}
+                    fill={theme.danger}
+                />
+            )}
+        </View>
+    );
+}
+
+BillingBanner.displayName = 'BillingBanner';
+
+export default BillingBanner;
