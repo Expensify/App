c2c8f73bdc1dfb74bfd0c799dfd0f977fd218221
cdOut <88325488+cdOut@users.noreply.github.com>
2024-06-07 12:37:10 +0200

    add initialValue to omit null

--- src/components/MoneyRequestHeader.tsx
+++ src/components/MoneyRequestHeader.tsx
@@ -52,7 +52,7 @@ function MoneyRequestHeader({report, parentReportAction, policy, shouldUseNarrow
     const [transaction] = useOnyx(`${ONYXKEYS.COLLECTION.TRANSACTION}${(parentReportAction as ReportAction & OriginalMessageIOU)?.originalMessage?.IOUTransactionID ?? 0}`);
     const [transactionViolations] = useOnyx(ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS);
     const [session] = useOnyx(ONYXKEYS.SESSION);
-    const [dismissedHoldUseExplanation] = useOnyx(ONYXKEYS.NVP_DISMISSED_HOLD_USE_EXPLANATION);
+    const [dismissedHoldUseExplanation] = useOnyx(ONYXKEYS.NVP_DISMISSED_HOLD_USE_EXPLANATION, {initialValue: true});
 
     const styles = useThemeStyles();
     const theme = useTheme();
@@ -172,7 +172,7 @@ function MoneyRequestHeader({report, parentReportAction, policy, shouldUseNarrow
 
     useEffect(() => {
         setShouldShowHoldMenu(isOnHold && !dismissedHoldUseExplanation);
-    }, [dismissedHoldUseExplanation, isOnHold]);
+    }, [dismissedHoldUseExplanation, isOnHold, status]);
 
     useEffect(() => {
         if (!shouldShowHoldMenu) {
