6115929b6179f44ffd780c9e8a4677d88d68d472
tienifr <christianwen18@gmail.com>
2024-06-08 17:16:48 +0700

    fix lint

--- src/libs/isPdfFilePasswordProtected/index.tsx
+++ src/libs/isPdfFilePasswordProtected/index.tsx
@@ -1,37 +1,37 @@
 import * as pdfjsLib from 'pdfjs-dist';
+import type {FileObject} from '@components/AttachmentModal';
 
-const isPdfFilePasswordProtected = (file) => {
-    return new Promise((resolve, reject) => {
+const isPdfFilePasswordProtected = (file: FileObject): Promise<boolean> => {
+    return new Promise((resolve) => {
         const reader = new FileReader();
 
-        reader.onload = async (event) => {
+        reader.onload = (event) => {
             const arrayBuffer = event.target?.result;
             if (!arrayBuffer) {
                 resolve(false);
-                return;
-            }
-
-            try {
-                const loadingTask = pdfjsLib.getDocument({data: arrayBuffer});
-                loadingTask.promise.then(
-                    () => {
-                        resolve(false);
-                    },
-                    (error) => {
-                        if (error.name === 'PasswordException') {
-                            resolve(true);
-                        } else {
-                            reject(error);
-                        }
-                    },
-                );
-            } catch (error) {
-                reject(error);
+            } else {
+                try {
+                    const loadingTask = pdfjsLib.getDocument({data: arrayBuffer});
+                    loadingTask.promise.then(
+                        () => {
+                            resolve(false);
+                        },
+                        (error) => {
+                            if (error.name === 'PasswordException') {
+                                resolve(true);
+                            } else {
+                                resolve(false);
+                            }
+                        },
+                    );
+                } catch (error) {
+                    resolve(false);
+                }
             }
         };
 
         reader.onerror = (error) => {
-            reject(error);
+            resolve(false);
         };
 
         reader.readAsArrayBuffer(file);
