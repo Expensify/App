a2b43174cdeba3775cdc4b7be7dc61b7fc7b0629
dominictb <tb-dominic@outlook.com>
2024-06-10 14:38:32 +0700

    fix: update the tz backward map polyfill to avoid iOS crash
    Signed-off-by: dominictb <tb-dominic@outlook.com>

--- src/libs/IntlPolyfill/polyfillDateTimeFormat.ts
+++ src/libs/IntlPolyfill/polyfillDateTimeFormat.ts
@@ -1,33 +1,16 @@
 import type {DateTimeFormatConstructor} from '@formatjs/intl-datetimeformat';
 import DateUtils from '@libs/DateUtils';
+import {timezoneBackwardMap} from '@src/TIMEZONES';
 
-/* eslint-disable @typescript-eslint/naming-convention */
-const tzLinks: Record<string, string> = {
-    'Africa/Abidjan': 'Africa/Accra',
-    CET: 'Europe/Paris',
-    CST6CDT: 'America/Chicago',
-    EET: 'Europe/Sofia',
-    EST: 'America/Cancun',
-    EST5EDT: 'America/New_York',
-    'Etc/GMT': 'UTC',
-    'Etc/UTC': 'UTC',
-    Factory: 'UTC',
-    GMT: 'UTC',
-    HST: 'Pacific/Honolulu',
-    MET: 'Europe/Paris',
-    MST: 'America/Phoenix',
-    MST7MDT: 'America/Denver',
-    PST8PDT: 'America/Los_Angeles',
-    WET: 'Europe/Lisbon',
-};
 /* eslint-enable @typescript-eslint/naming-convention */
 
 export default function () {
     // Because JS Engines do not expose default timezone, the polyfill cannot detect local timezone that a browser is in.
     // We must manually do this by getting the local timezone before adding polyfill.
     let currentTimezone = DateUtils.getCurrentTimezone().selected as string;
-    if (currentTimezone in tzLinks) {
-        currentTimezone = tzLinks[currentTimezone];
+    
+    if (currentTimezone in timezoneBackwardMap) {
+        currentTimezone = timezoneBackwardMap[currentTimezone];
     }
 
     require('@formatjs/intl-datetimeformat/polyfill');
