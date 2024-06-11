b3c337a4028353d441ba1fd07504f7d193191ad1
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-06-11 19:01:10 +0200

    add map cache

--- src/libs/memoize/memoize.ts
+++ src/libs/memoize/memoize.ts
@@ -1,11 +1,11 @@
 import moize from 'moize';
 import type {Memoize, MemoizeConfig, MemoizeInstance} from './types';
 
-const DEFAULT_CONFIG: MemoizeConfig = {
+const DEFAULT_CONFIG = {
     cacheMode: 'array',
     equalityCheck: 'shallow',
     maxSize: 1,
-};
+} satisfies MemoizeConfig;
 
 const buildArrayCache: Memoize = (f, c) => {
     // If cacheMode is array we proceed with moize
@@ -21,8 +21,40 @@ const buildArrayCache: Memoize = (f, c) => {
 };
 
 const buildMapCache: Memoize = (f, c) => {
-    // If cacheMode is map we need to implement it
-    throw new Error('Map cache mode is not implemented yet');
+    const cache = new Map<string, ReturnType<typeof f>>();
+
+    // Declaring here to not close over the config;
+    const maxCacheSize = c.maxSize ?? DEFAULT_CONFIG.maxSize;
+
+    function memoized(...params: Parameters<typeof f>) {
+        const key = JSON.stringify(params);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = f(...params) as ReturnType<typeof f>;
+        cache.set(key, result);
+
+        if (cache.size > maxCacheSize) {
+            // Maps should keep insertion order, so we can safely delete the first key
+            cache.delete(cache.keys().next().value as string);
+        }
+
+        return result;
+    }
+
+    memoized.set = (key: string, value: ReturnType<typeof f>) => {
+        cache.set(key, value);
+    };
+
+    memoized.get = (key: string) => cache.get(key);
+
+    memoized.clear = () => {
+        cache.clear();
+    };
+
+    return memoized as MemoizeInstance<typeof f>;
 };
 
 const memoize: Memoize = (f, config = DEFAULT_CONFIG) => {
