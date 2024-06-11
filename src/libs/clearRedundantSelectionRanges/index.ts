bc62a7e0faa6c0420dfe154f15dafa37b87f023d
dominictb <tb-dominic@outlook.com>
2024-06-11 14:51:49 +0700

    chore: update lint and add comments

--- src/libs/clearRedundantSelectionRanges/index.ts
+++ src/libs/clearRedundantSelectionRanges/index.ts
@@ -1,17 +1,26 @@
-const isNodeEditable = (node: HTMLElement) => {
-    let ancestorNodeToCheck = node.nodeType === Node.ELEMENT_NODE ? node : node.parentElement;
+/**
+ * determine if node is editable
+ */
+
+const isNodeEditable = (node: HTMLElement | Node) => {
+    // the node here could be text node, so we should try finding the nearest ancestor node of type: Element
+    let ancestorNodeToCheck: HTMLElement | Node | null = node.nodeType === Node.ELEMENT_NODE ? node : node.parentElement;
     while (ancestorNodeToCheck && ancestorNodeToCheck.nodeType !== Node.ELEMENT_NODE) {
         ancestorNodeToCheck = ancestorNodeToCheck.parentElement;
     }
 
     // couldn't find closest ancestor node that is an element node
     if (!ancestorNodeToCheck) {
+        // if the design mode is on, we should consider the node as editable
         return document.designMode.toLowerCase() === 'on';
     }
 
+    // check the tag name (input, textarea,...)
     const nodeName = ancestorNodeToCheck.nodeName.toLowerCase();
 
-    if (nodeName === 'input' || nodeName === 'textarea' || ancestorNodeToCheck.isContentEditable) {
+    // in case of input/textarea or contenteditable element, we should consider it as editable
+    // since contenteditable is inherit, isContentEditable always returns true if the element is a child of a contenteditable element
+    if (nodeName === 'input' || nodeName === 'textarea' || (ancestorNodeToCheck as HTMLElement).isContentEditable) {
         return true;
     }
 };
@@ -19,17 +28,17 @@ const isNodeEditable = (node: HTMLElement) => {
 const clearRedundantSelectionRanges = () => {
     const selection = window.getSelection();
 
-    if (!selection || !selection.rangeCount) {
+    if (!selection?.rangeCount) {
         return;
     }
 
-    let rangesNeedToBeRemove: Range[] = [];
+    const rangesNeedToBeRemove: Range[] = [];
     for (let i = 0; i < selection.rangeCount; ++i) {
         const range = selection.getRangeAt(i);
-        if (isNodeEditable(range.commonAncestorContainer as HTMLElement)) {
-            continue;
+        // if a selection range is not editable, we should remove it as it's likely for the tap-to-search feature on Chrome
+        if (!isNodeEditable(range.commonAncestorContainer as HTMLElement)) {
+            rangesNeedToBeRemove.push(range);
         }
-        rangesNeedToBeRemove.push(range);
     }
 
     rangesNeedToBeRemove.forEach((range) => {
