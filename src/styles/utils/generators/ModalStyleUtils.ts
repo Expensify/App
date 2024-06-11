7f1d69b4de6388ce9a10d2b19349f57b74fe422b
dragnoir <mosaixel.org@gmail.com>
2024-06-11 11:19:11 +0100

    use componentBorderRadiusLarge

--- src/styles/utils/generators/ModalStyleUtils.ts
+++ src/styles/utils/generators/ModalStyleUtils.ts
@@ -197,7 +197,7 @@ const createModalStyleUtils: StyleUtilGenerator<GetModalStylesStyleUtil> = ({the
                     },
                 };
                 modalContainerStyle = {
-                    borderRadius: 16,
+                    borderRadius: variables.componentBorderRadiusLarge,
                     borderWidth: 1,
                     borderColor: theme.border,
                     justifyContent: 'center',
