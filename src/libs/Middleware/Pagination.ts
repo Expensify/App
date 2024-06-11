4161096b4de10c56e271ca8a705519b6d8be32a5
Janic Duplessis <janicduplessis@gmail.com>
2024-06-10 22:15:51 -0400

    Use Onyx.connect instead of OnyxCache

--- src/libs/Middleware/Pagination.ts
+++ src/libs/Middleware/Pagination.ts
@@ -1,13 +1,12 @@
 // TODO: Is this a legit use case for exposing `OnyxCache`, or should we use `Onyx.connect`?
 import fastMerge from 'expensify-common/dist/fastMerge';
 import Onyx from 'react-native-onyx';
-import OnyxCache from 'react-native-onyx/dist/OnyxCache';
 import type {ApiCommand} from '@libs/API/types';
 import Log from '@libs/Log';
 import PaginationUtils from '@libs/PaginationUtils';
 import CONST from '@src/CONST';
 import type {OnyxCollectionKey, OnyxPagesKey, OnyxValues} from '@src/ONYXKEYS';
-import type {Pages, Request} from '@src/types/onyx';
+import type {Request} from '@src/types/onyx';
 import type {PaginatedRequest} from '@src/types/onyx/Request';
 import type Middleware from './types';
 
@@ -26,6 +25,8 @@ type PaginationConfigMapValue = Omit<PaginationConfig<OnyxCollectionKey, OnyxPag
 };
 
 const paginationConfigs = new Map<string, PaginationConfigMapValue>();
+const ressources = new Map<OnyxCollectionKey, OnyxValues[OnyxCollectionKey] | null>();
+const pages = new Map<OnyxPagesKey, OnyxValues[OnyxPagesKey] | null>();
 
 function registerPaginationConfig<TResourceKey extends OnyxCollectionKey, TPageKey extends OnyxPagesKey>({
     initialCommand,
@@ -37,6 +38,18 @@ function registerPaginationConfig<TResourceKey extends OnyxCollectionKey, TPageK
     paginationConfigs.set(initialCommand, {...config, type: 'initial'} as unknown as PaginationConfigMapValue);
     paginationConfigs.set(previousCommand, {...config, type: 'previous'} as unknown as PaginationConfigMapValue);
     paginationConfigs.set(nextCommand, {...config, type: 'next'} as unknown as PaginationConfigMapValue);
+    Onyx.connect({
+        key: config.resourceCollectionKey,
+        callback: (data) => {
+            ressources.set(config.resourceCollectionKey, data);
+        },
+    });
+    Onyx.connect({
+        key: config.pageCollectionKey,
+        callback: (data) => {
+            pages.set(config.pageCollectionKey, data);
+        },
+    });
 }
 
 function isPaginatedRequest(request: Request | PaginatedRequest): request is PaginatedRequest {
@@ -87,11 +100,11 @@ const Pagination: Middleware = (requestResponse, request) => {
             newPage.unshift(CONST.PAGINATION_START_ID);
         }
 
-        const existingItems = (OnyxCache.getValue(resourceKey) ?? {}) as OnyxValues[typeof resourceCollectionKey];
+        const existingItems = ressources.get(resourceCollectionKey) ?? {};
         const allItems = fastMerge(existingItems, pageItems, true);
         const sortedAllItems = sortItems(allItems);
 
-        const existingPages = (OnyxCache.getValue(pageKey) ?? []) as Pages;
+        const existingPages = pages.get(pageCollectionKey) ?? [];
         const mergedPages = PaginationUtils.mergeContinuousPages(sortedAllItems, [...existingPages, newPage], getItemID);
 
         response.onyxData.push({
