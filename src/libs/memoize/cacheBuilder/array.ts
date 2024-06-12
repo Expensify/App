8bce219fcf329d1b922fbdd2787c5b218d9ea9f1
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-06-12 18:09:08 +0200

    fix arrayCache snapshot method

--- src/libs/memoize/cacheBuilder/array.ts
+++ src/libs/memoize/cacheBuilder/array.ts
@@ -17,7 +17,11 @@ function arrayCacheBuilder<Args, RT>(f: (...args: Args[]) => RT, c: ArrayCacheCo
     const moized = moize(f, moizeConfig);
 
     moized.snapshot = () => {
-        const {keys, values} = moized.cacheSnapshot.snapshot;
+        const {keys, values} = moized.cache.snapshot;
+
+        if (!keys?.length) {
+            return [];
+        }
 
         return keys.map((k, i) => [k, values[i]] as [Args[], RT[]]);
     };
