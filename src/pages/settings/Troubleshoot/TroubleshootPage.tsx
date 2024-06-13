95e118b39567ad95b4dd664778483029b36738c5
burczu <b.dybowski@gmail.com>
2024-06-13 15:37:15 +0200

    direct access to indexedDB instance possibility added

--- src/pages/settings/Troubleshoot/TroubleshootPage.tsx
+++ src/pages/settings/Troubleshoot/TroubleshootPage.tsx
@@ -95,7 +95,39 @@ function TroubleshootPage({shouldStoreLogs}: TroubleshootPageProps) {
         return Promise.all(promises);
     };
 
+    const readFromIndexedDB = () => new Promise((resolve) => {
+        let db: IDBDatabase;
+        const openRequest = indexedDB.open('OnyxDB', 1);
+        openRequest.onsuccess = () => {
+            db = openRequest.result;
+            const transaction = db.transaction('keyvaluepairs');
+            const objectStore = transaction.objectStore('keyvaluepairs');
+            const cursor = objectStore.openCursor();
+
+            const queryResult: Record<string, unknown> = {};
+
+            cursor.onerror = () => {
+                console.error('Error reading cursor');
+            }
+
+            cursor.onsuccess = (event) => {
+                const { result } = event.target as IDBRequest<IDBCursorWithValue>;
+                if (result) {
+                    queryResult[result.primaryKey as string] = result.value;
+                    result.continue();
+                }
+                else {
+                    resolve(queryResult);
+                }
+            }
+        };
+    });
+
     const exportOnyxState = () => {
+        readFromIndexedDB().then((value) => {
+            console.log('exported indexedDB state: ', value);
+        });
+
         getOnyxValues().then((value) => {
             console.log('exported onyx state: ', value.filter(Boolean));
         });
