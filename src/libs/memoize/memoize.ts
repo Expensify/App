6af5d573a265c1fbf2b0cc364816ecdaae138aa7
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-06-13 10:29:47 +0200

    memoize description

--- src/libs/memoize/memoize.ts
+++ src/libs/memoize/memoize.ts
@@ -7,6 +7,16 @@ const DEFAULT_CONFIG = {
     maxSize: Infinity,
 } as const;
 
+/**
+ * Utility which returns a memoized version of a function. If same arguments are passed to the memoized function, its return value will be retrieved from cache instead of calculating it from scratch.
+ *
+ * Possible options for cache structure:
+ * - `map` - uses `Map` primitive under the hood (only `deep` comparison of keys possible)
+ * - `array` - uses `Array` primitive under the hood (slower lookup but enables `shallow` comparison)
+ * @param f - Function you want to memoize
+ * @param config - See `MemoizeConfig` type for more details
+ * @returns Memoized function. Just use it and your function input should be memoized. Static methods added for direct cache manipulation. See `MemoizedInterface` type for more details.
+ */
 function memoize<Fn extends MemoizeFnPredicate>(f: Fn, config: ExternalMemoizeConfig = DEFAULT_CONFIG): MemoizedInterface<Fn> {
     // Make sure default values are provided
     const preparedConfig = (config !== DEFAULT_CONFIG ? {...DEFAULT_CONFIG, ...config} : config) as MemoizeConfig;
