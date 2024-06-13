038fead0166f34763e4499ae949e990787e31be7
truph01 <truphd01@gmail.com>
2024-06-13 19:04:53 +0700

    Fix: Lint

--- src/components/Skeletons/ItemListSkeletonView.tsx
+++ src/components/Skeletons/ItemListSkeletonView.tsx
@@ -1,5 +1,6 @@
 import React, {useMemo, useState} from 'react';
-import {StyleProp, View, ViewStyle} from 'react-native';
+import {View} from 'react-native';
+import type {StyleProp, ViewStyle} from 'react-native';
 import SkeletonViewContentLoader from '@components/SkeletonViewContentLoader';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
