dd2d72c6820ad8273f73adbd9456d330712c8002
Krishna Gupta <belivethatkg@gmail.com>
2024-06-16 16:29:35 +0530

    fix: QBO - Required remains enabled when all tags are disabled
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/libs/actions/Policy/Tag.ts
+++ src/libs/actions/Policy/Tag.ts
@@ -6,6 +6,7 @@ import {READ_COMMANDS, WRITE_COMMANDS} from '@libs/API/types';
 import * as ErrorUtils from '@libs/ErrorUtils';
 import getIsNarrowLayout from '@libs/getIsNarrowLayout';
 import Log from '@libs/Log';
+import * as OptionsListUtils from '@libs/OptionsListUtils';
 import * as PolicyUtils from '@libs/PolicyUtils';
 import {navigateWhenEnableFeature} from '@libs/PolicyUtils';
 import * as ReportUtils from '@libs/ReportUtils';
@@ -181,6 +182,23 @@ function createPolicyTag(policyID: string, tagName: string) {
 function setWorkspaceTagEnabled(policyID: string, tagsToUpdate: Record<string, {name: string; enabled: boolean}>, tagListIndex: number) {
     const policyTag = PolicyUtils.getTagLists(allPolicyTags?.[`${ONYXKEYS.COLLECTION.POLICY_TAGS}${policyID}`] ?? {})?.[tagListIndex] ?? {};
 
+    const optimisticPolicyTagsData = {
+        ...Object.keys(tagsToUpdate).reduce<PolicyTags>((acc, key) => {
+            acc[key] = {
+                ...policyTag.tags[key],
+                ...tagsToUpdate[key],
+                errors: null,
+                pendingFields: {
+                    enabled: CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE,
+                },
+                pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE,
+            };
+
+            return acc;
+        }, {}),
+    };
+    const shouldDisableRequiredTag = !OptionsListUtils.hasEnabledOptions({...policyTag.tags, ...optimisticPolicyTagsData});
+
     const onyxData: OnyxData = {
         optimisticData: [
             {
@@ -188,21 +206,8 @@ function setWorkspaceTagEnabled(policyID: string, tagsToUpdate: Record<string, {
                 key: `${ONYXKEYS.COLLECTION.POLICY_TAGS}${policyID}`,
                 value: {
                     [policyTag.name]: {
-                        tags: {
-                            ...Object.keys(tagsToUpdate).reduce<PolicyTags>((acc, key) => {
-                                acc[key] = {
-                                    ...policyTag.tags[key],
-                                    ...tagsToUpdate[key],
-                                    errors: null,
-                                    pendingFields: {
-                                        enabled: CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE,
-                                    },
-                                    pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE,
-                                };
-
-                                return acc;
-                            }, {}),
-                        },
+                        ...(shouldDisableRequiredTag ? {required: false, pendingFields: {required: CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE}} : {}),
+                        tags: optimisticPolicyTagsData,
                     },
                 },
             },
@@ -213,6 +218,7 @@ function setWorkspaceTagEnabled(policyID: string, tagsToUpdate: Record<string, {
                 key: `${ONYXKEYS.COLLECTION.POLICY_TAGS}${policyID}`,
                 value: {
                     [policyTag.name]: {
+                        ...(shouldDisableRequiredTag ? {pendingFields: {required: null}} : {}),
                         tags: {
                             ...Object.keys(tagsToUpdate).reduce<PolicyTags>((acc, key) => {
                                 acc[key] = {
@@ -238,6 +244,7 @@ function setWorkspaceTagEnabled(policyID: string, tagsToUpdate: Record<string, {
                 key: `${ONYXKEYS.COLLECTION.POLICY_TAGS}${policyID}`,
                 value: {
                     [policyTag.name]: {
+                        ...(shouldDisableRequiredTag ? {pendingFields: {required: null}, required: policyTag.required} : {}),
                         tags: {
                             ...Object.keys(tagsToUpdate).reduce<PolicyTags>((acc, key) => {
                                 acc[key] = {
