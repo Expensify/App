ecd069697fcd3d31ae23d233f9b85d9d22864edb
Dominic <165644294+dominictb@users.noreply.github.com>
2024-06-17 16:46:20 +0700

    fix: link to the correct issue comment ID
    Co-authored-by: Jayesh Mangwani <35371050+jayeshmangwani@users.noreply.github.com>

--- src/components/ClientSideLoggingToolMenu/index.android.tsx
+++ src/components/ClientSideLoggingToolMenu/index.android.tsx
@@ -1,7 +1,7 @@
 /**
  * Since client-side logging is currently supported on web and desktop natively right now,
  * this menu will be hidden in iOS and Android.
- * See comment here: https://github.com/Expensify/App/issues/43256#issuecomment-2154609239
+ * See comment here: https://github.com/Expensify/App/issues/43256#issuecomment-2154610196
  */
 function ClientSideLoggingToolMenu() {
     return null;
