c97a94430a4e6951eb8847d1266395e7e4298194
truph01 <truphd01@gmail.com>
2024-06-18 17:40:19 +0700

    Fix: Type

--- src/components/SelectionList/types.ts
+++ src/components/SelectionList/types.ts
@@ -271,7 +271,7 @@ type BaseSelectionListProps<TItem extends ListItem> = Partial<ChildrenProps> & {
     /** Default renderer for every item in the list */
     ListItem: ValidListItem;
 
-    SkeletonView: React.FC<SkeletonViewProps>;
+    SkeletonView?: React.FC<SkeletonViewProps>;
 
     /** Whether this is a multi-select list */
     canSelectMultiple?: boolean;
