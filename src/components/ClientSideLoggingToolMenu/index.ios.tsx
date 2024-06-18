662551739f412862cbd20fd50554058e7b7717dc
Jayesh Mangwani <jayeshmangwani@gmail.com>
2024-06-18 23:00:30 +0530

    reverted the ClientSideLoggingToolMenu Android iOS files

--- src/components/ClientSideLoggingToolMenu/index.ios.tsx
+++ src/components/ClientSideLoggingToolMenu/index.ios.tsx
@@ -1,10 +1,37 @@
-/**
- * Since client-side logging is currently supported on web and desktop natively right now,
- * this menu will be hidden in iOS and Android.
- * See comment here: https://github.com/Expensify/App/issues/43256#issuecomment-2154610196
- */
+import React, {useState} from 'react';
+import Share from 'react-native-share';
+import type {Log} from '@libs/Console';
+import localFileCreate from '@libs/localFileCreate';
+import CONST from '@src/CONST';
+import BaseClientSideLoggingToolMenu from './BaseClientSideLoggingToolMenu';
+
 function ClientSideLoggingToolMenu() {
-    return null;
+    const [file, setFile] = useState<{path: string; newFileName: string; size: number}>();
+
+    const createFile = (logs: Log[]) => {
+        localFileCreate('logs', JSON.stringify(logs, null, 2)).then((localFile) => {
+            setFile(localFile);
+        });
+    };
+
+    const shareLogs = () => {
+        if (!file) {
+            return;
+        }
+        Share.open({
+            url: `file://${file.path}`,
+        });
+    };
+
+    return (
+        <BaseClientSideLoggingToolMenu
+            file={file}
+            onEnableLogging={() => setFile(undefined)}
+            onDisableLogging={createFile}
+            onShareLogs={shareLogs}
+            displayPath={`${CONST.NEW_EXPENSIFY_PATH}/${file?.newFileName ?? ''}`}
+        />
+    );
 }
 
 ClientSideLoggingToolMenu.displayName = 'ClientSideLoggingToolMenu';
