eca4735958d2fc0f5933cf29ccc3e3b9212822df
Mateusz Titz <titzmateusz@gmail.com>
2024-06-18 09:30:39 +0200

    Remove extra filtering of Search ReportItems

--- src/libs/SearchUtils.ts
+++ src/libs/SearchUtils.ts
@@ -199,7 +199,7 @@ function getReportSections(data: OnyxTypes.SearchResults['data']): ReportListIte
         }
     }
 
-    return Object.values(reportIDToTransactions).filter((item) => item.transactions?.length !== 0);
+    return Object.values(reportIDToTransactions);
 }
 
 const searchTypeToItemMap: SearchTypeToItemMap = {
