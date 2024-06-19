340176980f773372aeb341011d52219ce851d689
rory <rory@expensify.com>
2024-06-19 14:48:23 -0700

    Fix ts errors

--- tests/utils/TestHelper.ts
+++ tests/utils/TestHelper.ts
@@ -312,11 +312,37 @@ function assertFormDataMatchesObject(formData: FormData, obj: Report) {
     ).toEqual(expect.objectContaining(obj));
 }
 
+/**
+ * This is a helper function to create a mock for the addListener function of the react-navigation library.
+ * The reason we need this is because we need to trigger the transitionEnd event in our tests to simulate
+ * the transitionEnd event that is triggered when the screen transition animation is completed.
+ *
+ * @returns An object with two functions: triggerTransitionEnd and addListener
+ */
+const createAddListenerMock = () => {
+    const transitionEndListeners: Listener[] = [];
+    const triggerTransitionEnd = () => {
+        transitionEndListeners.forEach((transitionEndListener) => transitionEndListener());
+    };
+
+    const addListener = jest.fn().mockImplementation((listener, callback: Listener) => {
+        if (listener === 'transitionEnd') {
+            transitionEndListeners.push(callback);
+        }
+        return () => {
+            transitionEndListeners.filter((cb) => cb !== callback);
+        };
+    });
+
+    return {triggerTransitionEnd, addListener};
+};
+
 export type {MockFetch, FormData, NativeNavigationMock};
 export {
     assertFormDataMatchesObject,
     buildPersonalDetails,
     buildTestReportComment,
+    createAddListenerMock,
     getGlobalFetchMock,
     setupApp,
     setupGlobalFetchMock,
