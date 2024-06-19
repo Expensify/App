5985107cc05d1f98bddd3edc0e660a87fb97679b
cdOut <88325488+cdOut@users.noreply.github.com>
2024-06-19 15:32:50 +0200

    add conditional for Hold PromotedAction display

--- src/pages/ReportDetailsPage.tsx
+++ src/pages/ReportDetailsPage.tsx
@@ -439,7 +439,7 @@ function ReportDetailsPage({policies, report, session, personalDetails}: ReportD
     const isSettled = ReportUtils.isSettled(moneyRequestReport?.reportID);
     const isApproved = ReportUtils.isReportApproved(moneyRequestReport);
 
-    const shouldShowHoldAction = !isSettled && !isApproved && !isDeletedParentAction && !ReportUtils.isArchivedRoom(parentReport);
+    const shouldShowHoldAction = caseID !== CASES.MONEY_REPORT && !isSettled && !isApproved && !isDeletedParentAction && !ReportUtils.isArchivedRoom(parentReport);
     const canHoldUnholdReportAction = ReportUtils.canHoldUnholdReportAction(parentReportAction);
 
     const canJoin = !isExpenseReport && ReportUtils.canJoinChat(report, parentReportAction, policy);
