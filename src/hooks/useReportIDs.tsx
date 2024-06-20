36cb17160219025f251de2c51b7d39a869386f71
hurali97 <hurali97@gmail.com>
2024-06-20 14:04:30 +0500

    perf: reduce re-renders by memoizing policyMemberAccountIDs and explicitly checking whether derivedCurrentReportID is not -1

--- src/hooks/useReportIDs.tsx
+++ src/hooks/useReportIDs.tsx
@@ -2,7 +2,6 @@ import React, {createContext, useCallback, useContext, useMemo} from 'react';
 import type {OnyxEntry} from 'react-native-onyx';
 import {useOnyx} from 'react-native-onyx';
 import {getPolicyEmployeeListByIdWithoutCurrentUser} from '@libs/PolicyUtils';
-import * as ReportUtils from '@libs/ReportUtils';
 import SidebarUtils from '@libs/SidebarUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -12,7 +11,6 @@ import useActiveWorkspace from './useActiveWorkspace';
 import useCurrentReportID from './useCurrentReportID';
 import useCurrentUserPersonalDetails from './useCurrentUserPersonalDetails';
 
-type ChatReportSelector = OnyxTypes.Report & {isUnreadWithMention: boolean};
 type PolicySelector = Pick<OnyxTypes.Policy, 'type' | 'name' | 'avatarURL' | 'employeeList'>;
 type ReportActionsSelector = Array<Pick<OnyxTypes.ReportAction, 'reportActionID' | 'actionName' | 'errors' | 'message' | 'originalMessage'>>;
 
@@ -24,56 +22,19 @@ type ReportIDsContextProviderProps = {
 type ReportIDsContextValue = {
     orderedReportIDs: string[];
     currentReportID: string;
+    policyMemberAccountIDs: number[];
 };
 
 const ReportIDsContext = createContext<ReportIDsContextValue>({
     orderedReportIDs: [],
     currentReportID: '',
+    policyMemberAccountIDs: [],
 });
 
 /**
  * This function (and the few below it), narrow down the data from Onyx to just the properties that we want to trigger a re-render of the component. This helps minimize re-rendering
  * and makes the entire component more performant because it's not re-rendering when a bunch of properties change which aren't ever used in the UI.
  */
-const chatReportSelector = (report: OnyxEntry<OnyxTypes.Report>): ChatReportSelector =>
-    (report && {
-        reportID: report.reportID,
-        participants: report.participants,
-        isPinned: report.isPinned,
-        isHidden: report.isHidden,
-        notificationPreference: report.notificationPreference,
-        errorFields: {
-            addWorkspaceRoom: report.errorFields?.addWorkspaceRoom,
-        },
-        lastMessageText: report.lastMessageText,
-        lastVisibleActionCreated: report.lastVisibleActionCreated,
-        iouReportID: report.iouReportID,
-        total: report.total,
-        nonReimbursableTotal: report.nonReimbursableTotal,
-        hasOutstandingChildRequest: report.hasOutstandingChildRequest,
-        isWaitingOnBankAccount: report.isWaitingOnBankAccount,
-        statusNum: report.statusNum,
-        stateNum: report.stateNum,
-        chatType: report.chatType,
-        type: report.type,
-        policyID: report.policyID,
-        visibility: report.visibility,
-        lastReadTime: report.lastReadTime,
-        // Needed for name sorting:
-        reportName: report.reportName,
-        policyName: report.policyName,
-        oldPolicyName: report.oldPolicyName,
-        // Other less obvious properites considered for sorting:
-        ownerAccountID: report.ownerAccountID,
-        currency: report.currency,
-        managerID: report.managerID,
-        // Other important less obivous properties for filtering:
-        parentReportActionID: report.parentReportActionID,
-        parentReportID: report.parentReportID,
-        isDeletedParentAction: report.isDeletedParentAction,
-        isUnreadWithMention: ReportUtils.isUnreadWithMention(report),
-    }) as ChatReportSelector;
-
 const reportActionsSelector = (reportActions: OnyxEntry<OnyxTypes.ReportActions>): ReportActionsSelector =>
     (reportActions &&
         Object.values(reportActions)
@@ -116,7 +77,7 @@ function ReportIDsContextProvider({
     currentReportIDForTests,
 }: ReportIDsContextProviderProps) {
     const [priorityMode] = useOnyx(ONYXKEYS.NVP_PRIORITY_MODE, {initialValue: CONST.PRIORITY_MODE.DEFAULT});
-    const [chatReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: chatReportSelector});
+    const [chatReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT);
     const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: policySelector});
     const [allReportActions] = useOnyx(ONYXKEYS.COLLECTION.REPORT_ACTIONS, {selector: reportActionsSelector});
     const [transactionViolations] = useOnyx(ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS);
@@ -128,7 +89,7 @@ function ReportIDsContextProvider({
     const derivedCurrentReportID = currentReportIDForTests ?? currentReportIDValue?.currentReportID;
     const {activeWorkspaceID} = useActiveWorkspace();
 
-    const policyMemberAccountIDs = getPolicyEmployeeListByIdWithoutCurrentUser(policies, activeWorkspaceID, accountID);
+    const policyMemberAccountIDs = useMemo(() => getPolicyEmployeeListByIdWithoutCurrentUser(policies, activeWorkspaceID, accountID), [policies, activeWorkspaceID, accountID]);
 
     const getOrderedReportIDs = useCallback(
         (currentReportID?: string) =>
@@ -155,15 +116,16 @@ function ReportIDsContextProvider({
         // we first generate the list as if there was no current report, then we check if
         // the current report is missing from the list, which should very rarely happen. In this
         // case we re-generate the list a 2nd time with the current report included.
-        if (derivedCurrentReportID && !orderedReportIDs.includes(derivedCurrentReportID)) {
-            return {orderedReportIDs: getOrderedReportIDs(derivedCurrentReportID), currentReportID: derivedCurrentReportID ?? '-1'};
+        if (derivedCurrentReportID && derivedCurrentReportID !== '-1' && orderedReportIDs.indexOf(derivedCurrentReportID) === -1) {
+            return {orderedReportIDs: getOrderedReportIDs(derivedCurrentReportID), currentReportID: derivedCurrentReportID ?? '-1', policyMemberAccountIDs};
         }
 
         return {
             orderedReportIDs,
             currentReportID: derivedCurrentReportID ?? '-1',
+            policyMemberAccountIDs,
         };
-    }, [getOrderedReportIDs, orderedReportIDs, derivedCurrentReportID]);
+    }, [getOrderedReportIDs, orderedReportIDs, derivedCurrentReportID, policyMemberAccountIDs]);
 
     return <ReportIDsContext.Provider value={contextValue}>{children}</ReportIDsContext.Provider>;
 }
@@ -173,4 +135,4 @@ function useReportIDs() {
 }
 
 export {ReportIDsContext, ReportIDsContextProvider, policySelector, useReportIDs};
-export type {ChatReportSelector, PolicySelector, ReportActionsSelector};
+export type {PolicySelector, ReportActionsSelector};
