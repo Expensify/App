dc8ad8b4c72b551bc123f73707c03435b25a9987
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-06-20 15:54:43 +0200

    use memoize for NumberFormatUtils

--- src/libs/NumberFormatUtils.ts
+++ src/libs/NumberFormatUtils.ts
@@ -1,12 +1,15 @@
 import type {ValueOf} from 'type-fest';
 import type CONST from '@src/CONST';
+import memoize from './memoize';
+
+const numberFormatter = memoize(Intl.NumberFormat, {maxSize: 10});
 
 function format(locale: ValueOf<typeof CONST.LOCALES>, number: number, options?: Intl.NumberFormatOptions): string {
-    return new Intl.NumberFormat(locale, options).format(number);
+    return numberFormatter(locale, options).format(number);
 }
 
 function formatToParts(locale: ValueOf<typeof CONST.LOCALES>, number: number, options?: Intl.NumberFormatOptions): Intl.NumberFormatPart[] {
-    return new Intl.NumberFormat(locale, options).formatToParts(number);
+    return numberFormatter(locale, options).formatToParts(number);
 }
 
 export {format, formatToParts};
