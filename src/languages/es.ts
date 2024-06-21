ffccc5ec55266e2ad8a4b79137b035c281273a5f
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-06-21 08:59:35 +0100

    fix: spanish translation not fitting in the UI

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -2730,7 +2730,7 @@ export default {
             workspaceOwnerWillNeedToAddOrUpdatePaymentCard: ({workspaceOwnerName}) =>
                 `El propietario del espacio de trabajo, ${workspaceOwnerName} tendrá que añadir o actualizar la tarjeta de pago registrada para desbloquear nueva actividad en el espacio de trabajo.`,
             youWillNeedToAddOrUpdatePaymentCard: 'Debes añadir o actualizar la tarjeta de pago registrada para desbloquear nueva actividad en el espacio de trabajo.',
-            addPaymentCardToUnlock: 'Añade una tarjeta de pago para desbloquearlo!',
+            addPaymentCardToUnlock: 'Añade una tarjeta para desbloquearlo!',
             addPaymentCardToContinueUsingWorkspace: 'Añade una tarjeta de pago para seguir utilizando este espacio de trabajo',
             pleaseReachOutToYourWorkspaceAdmin: 'Si tienes alguna pregunta, ponte en contacto con el administrador de su espacio de trabajo.',
             chatWithYourAdmin: 'Chatea con tu administrador',
