e2851c4dfc5aa46e7175de76b0350b5dbe6d0402
Krishna Gupta <belivethatkg@gmail.com>
2024-06-24 00:31:55 +0530

    fix lint issue.
    Signed-off-by: Krishna Gupta <belivethatkg@gmail.com>

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -347,7 +347,7 @@ function MoneyRequestView({
     ];
     const receiptViolations =
         transactionViolations?.filter((violation) => receiptViolationNames.includes(violation.name)).map((violation) => ViolationsUtils.getViolationTranslation(violation, translate)) ?? [];
-    const shouldShowNotesViolations = !isReceiptBeingScanned && canUseViolations && ReportUtils.isPaidGroupPolicy(report);
+    const shouldShowAuditMessage = !isReceiptBeingScanned && canUseViolations && ReportUtils.isPaidGroupPolicy(report);
     const shouldShowReceiptHeader = isReceiptAllowed && (shouldShowReceiptEmptyState || hasReceipt);
 
     const errors = {
