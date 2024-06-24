8071028df1538ffba30681c628dac748639aaf69
Dysto coder <dystovskycoder@gmail.com>
2024-06-24 12:32:42 +0300

    Import utils and components required for the component

---
+++ src/components/ValidateAccountMessage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View} from 'react-native';
+import Icon from '@components/Icon';
+import * as Expensicons from '@components/Icon/Expensicons';
+import Text from '@components/Text';
+import TextLink from '@components/TextLink';
+import useLocalize from '@hooks/useLocalize';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
+import variables from '@styles/variables';
+import * as Session from '@userActions/Session';
+function ValidateAccountMessage () {
+
+}
+
+export default ValidateAccountMessage;
