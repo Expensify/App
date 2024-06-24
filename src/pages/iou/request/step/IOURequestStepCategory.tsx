4add17c46158454efee979b9d2011db3efc58823
daledah <daleda261@outlook.com>
2024-06-24 14:57:05 +0700

    revert link ask to admin

--- src/pages/iou/request/step/IOURequestStepCategory.tsx
+++ src/pages/iou/request/step/IOURequestStepCategory.tsx
@@ -91,6 +91,7 @@ function IOURequestStepCategory({
     const isAdmin = policy?.role === CONST.POLICY.ROLE.ADMIN;
     const {shouldUseNarrowLayout} = useResponsiveLayout();
     const transactionCategory = ReportUtils.getTransactionDetails(isEditingSplitBill && !lodashIsEmpty(splitDraftTransaction) ? splitDraftTransaction : transaction)?.category;
+
     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
     const reportAction = reportActions?.[report?.parentReportActionID || reportActionID] ?? null;
 
@@ -176,13 +177,7 @@ function IOURequestStepCategory({
                     ) : (
                         <Text style={[styles.textNormal, styles.emptyCardSectionSubtitle]}>
                             {translate('workspace.categories.emptyCategories.otherWorkspace')}
-                            <TextLink
-                                onPress={() => {
-                                    Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(report?.reportID ?? '-1'));
-                                }}
-                            >
-                                {translate('workspace.categories.emptyCategories.askYourAdmin')}
-                            </TextLink>
+                            <TextLink onPress={() => {}}>{translate('workspace.categories.emptyCategories.askYourAdmin')}</TextLink>
                             {translate('workspace.categories.emptyCategories.enableForThisWorkspace')}
                         </Text>
                     )}
