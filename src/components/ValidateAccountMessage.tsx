99fc4ec17881bc798f656c4111fcb1ba63a3d004
Dysto coder <dystovskycoder@gmail.com>
2024-06-24 12:57:26 +0300

    Add BankAccountStep Error message style with SignOut functionality

--- src/components/ValidateAccountMessage.tsx
+++ src/components/ValidateAccountMessage.tsx
@@ -9,12 +9,41 @@ import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import variables from '@styles/variables';
 import * as Session from '@userActions/Session';
-function ValidateAccountMessage () {
 
+function ValidateAccountMessage() {
     const theme = useTheme();
     const styles = useThemeStyles();
     const {translate} = useLocalize();
 
+    return (
+        <View style={[styles.flexRow, styles.alignItemsCenter, styles.m4]}>
+            <Icon
+                src={Expensicons.Exclamation}
+                fill={theme.danger}
+            />
+
+            <Text style={[styles.mutedTextLabel, styles.ml4, styles.flex1]}>
+                {translate('bankAccount.validateAccountError.phrase1')}
+                <TextLink
+                    fontSize={variables.fontSizeLabel}
+                    onPress={() => Session.signOutAndRedirectToSignIn()}
+                >
+                    {translate('bankAccount.validateAccountError.phrase2')}
+                </TextLink>
+                {translate('bankAccount.validateAccountError.phrase3')}
+                <TextLink
+                    fontSize={variables.fontSizeLabel}
+                    onPress={() => {
+                        // const login = loginList?.[loginNames?.[0]] ?? {};
+                        // Navigation.navigate(ROUTES.SETTINGS_CONTACT_METHOD_DETAILS.getRoute(login?.partnerUserID ?? loginNames?.[0]));
+                    }}
+                >
+                    {translate('bankAccount.validateAccountError.phrase4')}
+                </TextLink>
+                .
+            </Text>
+        </View>
+    );
 }
 
 export default ValidateAccountMessage;
