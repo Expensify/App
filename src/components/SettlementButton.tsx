cbe89b400f3277bb81271786a2b711d7d13242be
Brandon Henry <itsbhenry@gmail.com>
2024-06-25 01:24:40 -0500

    match staging

--- src/components/SettlementButton.tsx
+++ src/components/SettlementButton.tsx
@@ -186,7 +186,7 @@ function SettlementButton({
             value: CONST.IOU.REPORT_ACTION_TYPE.APPROVE,
             disabled: !!shouldDisableApproveButton,
         };
-        const canUseWallet = !isExpenseReport && !isInvoiceReport && currency === CONST.CURRENCY.USD;
+        const canUseWallet = !isExpenseReport && currency === CONST.CURRENCY.USD;
 
         // Only show the Approve button if the user cannot pay the expense
         if (shouldHidePaymentOptions && shouldShowApproveButton) {
