7e8c2e484f1fd8a5254695418c80d137d31f07f4
Yuwen Memon <yuwen@expensify.com>
2024-06-24 21:32:41 -0700

    Merge pull request #44300 from ishpaul777/fix/44194
    fixes the "Room chat LHN preview displays "removed 0 user" after editing room description "
    
    (cherry picked from commit 9ee8806a4011174bd215e0f950a547bf2a2c5243)

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -3327,4 +3327,7 @@ export default {
         additionalInfoTitle: 'What software are you moving to and why?',
         additionalInfoInputLabel: 'Your response',
     },
+    roomChangeLog: {
+        updateRoomDescription: 'set the room description to:',
+    },
 } satisfies TranslationBase;
