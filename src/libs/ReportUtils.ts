92dac2c4ea0c151014ebd292801f77b7791d1055
Carlos Martins <cmartins@expensify.com>
2024-06-24 18:21:23 -0600

    Merge pull request #44357 from Expensify/aldo_fix-infinite-loading-bug
    Remove checks for IOU
    
    (cherry picked from commit 522eab780c7b2c4eef862090f33a1b3d54d6bb1a)

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -2170,10 +2170,11 @@ function getDeletedParentActionMessageForChatReport(reportAction: OnyxEntry<Repo
 /**
  * Returns the preview message for `REIMBURSEMENT_QUEUED` action
  */
-function getReimbursementQueuedActionMessage(reportAction: OnyxEntry<ReportAction>, reportOrID: OnyxEntry<Report> | string, shouldUseShortDisplayName = true): string {
-    if (!ReportActionsUtils.isMoneyRequestAction(reportAction)) {
-        return '';
-    }
+function getReimbursementQueuedActionMessage(
+    reportAction: OnyxEntry<ReportAction<typeof CONST.REPORT.ACTIONS.TYPE.REIMBURSEMENT_QUEUED>>,
+    reportOrID: OnyxEntry<Report> | string,
+    shouldUseShortDisplayName = true,
+): string {
     const report = typeof reportOrID === 'string' ? allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportOrID}`] : reportOrID;
     const submitterDisplayName = getDisplayNameForParticipant(report?.ownerAccountID, shouldUseShortDisplayName) ?? '';
     const originalMessage = ReportActionsUtils.getOriginalMessage(reportAction);
@@ -2195,9 +2196,6 @@ function getReimbursementDeQueuedActionMessage(
     reportOrID: OnyxEntry<Report> | EmptyObject | string,
     isLHNPreview = false,
 ): string {
-    if (!ReportActionsUtils.isReimbursementDeQueuedAction(reportAction)) {
-        return '';
-    }
     const report = typeof reportOrID === 'string' ? allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportOrID}`] : reportOrID;
     const originalMessage = ReportActionsUtils.getOriginalMessage(reportAction);
     const amount = originalMessage?.amount;
