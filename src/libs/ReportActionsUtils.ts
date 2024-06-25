b08e4aa05d2201695e2463e91d44d4f01b447ce2
Wildan Muhlis <muhliswildan@gmail.com>
2024-06-25 11:01:54 +0700

    Revert unnecessary change

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -383,7 +383,7 @@ function getCombinedReportActions(reportActions: ReportAction[], transactionThre
     }
 
     // Filter out the created action from the transaction thread report actions, since we already have the parent report's created action in `reportActions`
-    const filteredTransactionThreadReportActions = transactionThreadReportActions?.filter((action) => action.actionName !== CONST.REPORT.ACTIONS.TYPE.CREATED) ?? [];
+    const filteredTransactionThreadReportActions = transactionThreadReportActions?.filter((action) => action.actionName !== CONST.REPORT.ACTIONS.TYPE.CREATED);
     const report = allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportID}`];
     const isSelfDM = report?.chatType === CONST.REPORT.CHAT_TYPE.SELF_DM;
     // Filter out request and send money request actions because we don't want to show any preview actions for one transaction reports
