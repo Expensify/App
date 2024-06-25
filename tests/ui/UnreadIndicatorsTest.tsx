14610afd86ace7d244ca8a4628f0cf1bb5e11e5b
rory <rory@expensify.com>
2024-06-25 10:52:53 -0700

    Remove unused import

--- tests/ui/UnreadIndicatorsTest.tsx
+++ tests/ui/UnreadIndicatorsTest.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as NativeNavigation from '@react-navigation/native';
-import {act, fireEvent, render, screen, waitFor, within} from '@testing-library/react-native';
+import {act, fireEvent, render, screen, waitFor} from '@testing-library/react-native';
 import {addSeconds, format, subMinutes, subSeconds} from 'date-fns';
 import {utcToZonedTime} from 'date-fns-tz';
 import React from 'react';
