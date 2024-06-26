d4927cb09e88b17d1075ed19adcfa60c5cb91f4d
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-06-26 18:56:00 +0200

    fix: remove unused setValue props from Suggestions

--- src/pages/home/report/ReportActionCompose/ComposerWithSuggestions/ComposerWithSuggestions.tsx
+++ src/pages/home/report/ReportActionCompose/ComposerWithSuggestions/ComposerWithSuggestions.tsx
@@ -785,7 +785,6 @@ function ComposerWithSuggestions(
                 policyID={policyID}
                 // Input
                 value={value}
-                setValue={setValue}
                 selection={selection}
                 setSelection={setSelection}
                 resetKeyboardInput={resetKeyboardInput}
