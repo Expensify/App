6bc845eba8ef37eea8783a37dd530cef0c973d4b
Justin Persaud <jpersaud@expensify.com>
2024-06-26 09:56:13 -0400

    Remove more unused imports

--- src/pages/settings/Profile/ProfilePage.tsx
+++ src/pages/settings/Profile/ProfilePage.tsx
@@ -1,4 +1,3 @@
-import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
@@ -21,7 +20,6 @@ import * as LocalePhoneNumber from '@libs/LocalePhoneNumber';
 import Navigation from '@libs/Navigation/Navigation';
 import * as PersonalDetailsUtils from '@libs/PersonalDetailsUtils';
 import * as UserUtils from '@libs/UserUtils';
-import * as App from '@userActions/App';
 import CONST from '@src/CONST';
 import type {TranslationPaths} from '@src/languages/types';
 import ONYXKEYS from '@src/ONYXKEYS';
