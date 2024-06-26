63bb1bce5d19c2bf0e7b7547ab95af9e6b151b87
Carlos Martins <cmartins@expensify.com>
2024-06-26 10:39:12 -0600

    fix gh actions again

--- .github/actions/javascript/createOrUpdateStagingDeploy/index.js
+++ .github/actions/javascript/createOrUpdateStagingDeploy/index.js
@@ -15061,7 +15061,7 @@ const replacer = (str) => ({
     '\r': '\\r',
     '\f': '\\f',
     '"': '\\"',
-})[str] ?? '';
+}[str] ?? '');
 /**
  * Replace any characters in the string that will break JSON.parse for our Git Log output
  *
