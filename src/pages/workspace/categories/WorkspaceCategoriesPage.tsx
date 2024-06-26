1110e86197b2404ae9886836a2e9585869865975
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-06-26 01:42:00 +0100

    make the condition consistent

--- src/pages/workspace/categories/WorkspaceCategoriesPage.tsx
+++ src/pages/workspace/categories/WorkspaceCategoriesPage.tsx
@@ -309,7 +309,7 @@ function WorkspaceCategoriesPage({route}: WorkspaceCategoriesPageProps) {
                     danger
                 />
                 {shouldUseNarrowLayout && <View style={[styles.pl5, styles.pr5]}>{getHeaderButtons()}</View>}
-                {(!shouldUseNarrowLayout || (!hasVisibleCategories && !isLoading) || isLoading) && getHeaderText()}
+                {(!shouldUseNarrowLayout || categoryList.length === 0 || isLoading) && getHeaderText()}
                 {isLoading && (
                     <ActivityIndicator
                         size={CONST.ACTIVITY_INDICATOR_SIZE.LARGE}
