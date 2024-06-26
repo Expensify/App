93de31b774002237d41a64050f901076d40a0751
Agata Kosior <agata.kosior57@gmail.com>
2024-06-26 10:54:26 +0200

    feat: UI for card name step

--- src/pages/workspace/card/issueNew/CardNameStep.tsx
+++ src/pages/workspace/card/issueNew/CardNameStep.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import {View} from 'react-native';
 import FormProvider from '@components/Form/FormProvider';
+import InputWrapper from '@components/Form/InputWrapper';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import InteractiveStepSubHeader from '@components/InteractiveStepSubHeader';
 import ScreenWrapper from '@components/ScreenWrapper';
 import Text from '@components/Text';
+import TextInput from '@components/TextInput';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as Card from '@userActions/Card';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import INPUT_IDS from '@src/types/form/IssueNewExpensifyCardForm';
 
 function CardNameStep() {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
+
+    // TODO: validation
 
     const submit = () => {
         // TODO: the logic will be created in https://github.com/Expensify/App/issues/44309
@@ -48,8 +53,16 @@ function CardNameStep() {
                 onSubmit={submit}
                 style={[styles.mh5, styles.flexGrow1]}
             >
-                {/* TODO: the content will be created in https://github.com/Expensify/App/issues/44309 */}
-                <View />
+                <InputWrapper
+                    InputComponent={TextInput}
+                    inputID={INPUT_IDS.CARD_NAME}
+                    label={translate('workspace.card.issueNewCard.cardName')}
+                    hint={translate('workspace.card.issueNewCard.giveItNameInstruction')}
+                    aria-label={translate('workspace.card.issueNewCard.cardName')}
+                    role={CONST.ROLE.PRESENTATION}
+                    defaultValue=""
+                    containerStyles={[styles.mb6]}
+                />
             </FormProvider>
         </ScreenWrapper>
     );
