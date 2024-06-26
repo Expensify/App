cd6e8f1ea624daaa8327f2cf6bae40cadb7d1bd9
Rushat Gabhane <rushatgabhane@gmail.com>
2024-06-26 04:17:27 +0300

    revert: rm unused lang

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -790,6 +790,7 @@ export default {
         chooseARate: ({unit}: ReimbursementRateParams) => `Select a workspace reimbursement rate per ${unit}`,
         unapprove: 'Unapprove',
         unapproveReport: 'Unapprove report',
+        headsUp: 'Heads up!',
         unapproveWithIntegrationWarning: (accountingIntegration: string) =>
             `This report has already been exported to ${accountingIntegration}. Changes to this report in Expensify may lead to data discrepancies and Expensify Card reconciliation issues. Are you sure you want to unapprove this report?`,
     },
