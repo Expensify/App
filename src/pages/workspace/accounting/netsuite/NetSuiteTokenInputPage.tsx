8287fd511dbf596935270a3d51ba6e266262aa1e
Manan Jadhav <manan.jadhav@gmail.com>
2024-06-27 16:30:17 +0530

    fix: remove unwanted file

--- src/pages/workspace/accounting/netsuite/NetSuiteTokenInputPage.tsx
+++
@@ -1,7 +0,0 @@
-function NetSuiteTokenInputPage() {
-    return <>NetSuite Token Input</>;
-}
-
-NetSuiteTokenInputPage.displayName = 'NetSuiteTokenInputPage';
-
-export default NetSuiteTokenInputPage;
