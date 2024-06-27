8f37a0d03768866ed8fcb5d5be6d2aaf2ee9334e
Monil Bhavsar <monilbhavsar25@gmail.com>
2024-06-27 19:03:07 +0530

    Revert "Add small improvements related to introducing React strict mode"

--- src/setup/platformSetup/index.website.ts
+++ src/setup/platformSetup/index.website.ts
@@ -1,4 +1,6 @@
 import {AppRegistry} from 'react-native';
+// This is a polyfill for InternetExplorer to support the modern KeyboardEvent.key and KeyboardEvent.code instead of KeyboardEvent.keyCode
+import 'shim-keyboard-event-key';
 import checkForUpdates from '@libs/checkForUpdates';
 import DateUtils from '@libs/DateUtils';
 import Visibility from '@libs/Visibility';
