0904ac649d5a890301b8e39f64db575f75012783
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-06-28 09:34:12 +0200

    fix: pressing send not working on web

--- src/pages/home/report/ReportActionCompose/SendButton.tsx
+++ src/pages/home/report/ReportActionCompose/SendButton.tsx
@@ -1,16 +1,13 @@
 import React, {memo} from 'react';
 import {View} from 'react-native';
-import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 import Icon from '@components/Icon';
 import * as Expensicons from '@components/Icon/Expensicons';
 import PressableWithFeedback from '@components/Pressable/PressableWithFeedback';
 import Tooltip from '@components/Tooltip';
 import useLocalize from '@hooks/useLocalize';
-import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
-import { runOnJS } from 'react-native-reanimated';
 
 type SendButtonProps = {
     /** Whether the button is disabled */
@@ -24,10 +21,6 @@ function SendButton({isDisabled: isDisabledProp, handleSendMessage}: SendButtonP
     const theme = useTheme();
     const styles = useThemeStyles();
     const {translate} = useLocalize();
-    const {isSmallScreenWidth} = useResponsiveLayout();
-    const Tap = Gesture.Tap().onEnd(() => {
-        runOnJS(handleSendMessage)();
-    });
 
     return (
         <View
@@ -35,31 +28,25 @@ function SendButton({isDisabled: isDisabledProp, handleSendMessage}: SendButtonP
             // Keep focus on the composer when Send message is clicked.
             onMouseDown={(e) => e.preventDefault()}
         >
-            <GestureDetector
-                // A new GestureDetector instance must be created when switching from a large screen to a small screen
-                // if not, the GestureDetector may not function correctly.
-                key={`send-button-${isSmallScreenWidth ? 'small-screen' : 'normal-screen'}`}
-                gesture={Tap}
-            >
-                <Tooltip text={translate('common.send')}>
-                    <PressableWithFeedback
-                        style={({pressed, isDisabled}) => [
-                            styles.chatItemSubmitButton,
-                            isDisabledProp || pressed || isDisabled ? undefined : styles.buttonSuccess,
-                            isDisabledProp ? styles.cursorDisabled : undefined,
-                        ]}
-                        role={CONST.ROLE.BUTTON}
-                        accessibilityLabel={translate('common.send')}
-                    >
-                        {({pressed}) => (
-                            <Icon
-                                src={Expensicons.Send}
-                                fill={isDisabledProp || pressed ? theme.icon : theme.textLight}
-                            />
-                        )}
-                    </PressableWithFeedback>
-                </Tooltip>
-            </GestureDetector>
+            <Tooltip text={translate('common.send')}>
+                <PressableWithFeedback
+                    onPress={handleSendMessage}
+                    style={({pressed, isDisabled}) => [
+                        styles.chatItemSubmitButton,
+                        isDisabledProp || pressed || isDisabled ? undefined : styles.buttonSuccess,
+                        isDisabledProp ? styles.cursorDisabled : undefined,
+                    ]}
+                    role={CONST.ROLE.BUTTON}
+                    accessibilityLabel={translate('common.send')}
+                >
+                    {({pressed}) => (
+                        <Icon
+                            src={Expensicons.Send}
+                            fill={isDisabledProp || pressed ? theme.icon : theme.textLight}
+                        />
+                    )}
+                </PressableWithFeedback>
+            </Tooltip>
         </View>
     );
 }
