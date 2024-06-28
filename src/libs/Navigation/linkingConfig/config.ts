c06284189b1f0d48af3eb95cc67e6f67c11bbe71
Nicolay Arefyeu <n.arefyev91@gmail.com>
2024-06-28 15:40:57 +0300

    fix ts

--- src/libs/Navigation/linkingConfig/config.ts
+++ src/libs/Navigation/linkingConfig/config.ts
@@ -366,10 +366,6 @@ const config: LinkingOptions<RootStackParamList>['config'] = {
                         [SCREENS.WORKSPACE.SHARE]: {
                             path: ROUTES.WORKSPACE_PROFILE_SHARE.route,
                         },
-                        // TODO: uncomment after development
-                        // [SCREENS.WORKSPACE.EXPENSIFY_CARD]: {
-                        //     path: ROUTES.WORKSPACE_EXPENSIFY_CARD,
-                        // },
                         [SCREENS.WORKSPACE.EXPENSIFY_CARD_ISSUE_NEW]: {
                             path: ROUTES.WORKSPACE_EXPENSIFY_CARD_ISSUE_NEW,
                         },
