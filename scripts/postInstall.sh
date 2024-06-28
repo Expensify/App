607c23165e42ace6399aa2710f76cb0c0911753b
Fábio Henriques <contact@fabiohenriques.dev>
2024-06-28 11:19:10 +0100

    Make postInstall.sh script fail if any of its commands exits with a non-zero status

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Exit immediately if any command exits with a non-zero status
+set -e
+
 # Go to project root
 ROOT_DIR=$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)")
-cd "$ROOT_DIR" || exit 1
+cd "$ROOT_DIR"
 
 # Run patch-package
-npx patch-package
+npx patch-package --error-on-fail
 
 # Install node_modules in subpackages, unless we're in a CI/CD environment,
 # where the node_modules for subpackages are cached separately.
 # See `.github/actions/composite/setupNode/action.yml` for more context.
 if [[ -z ${CI+x} ]]; then
-  cd desktop || exit 1
+  cd desktop
   npm install
 fi
