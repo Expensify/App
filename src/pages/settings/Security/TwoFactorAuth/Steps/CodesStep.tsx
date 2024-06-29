bc5eb7dec68950985f3000b440f33486395ae55a
Dysto coder <dystovskycoder@gmail.com>
2024-06-29 19:09:15 +0300

    When user is not validated, hide 2FA section except header and next button

--- src/pages/settings/Security/TwoFactorAuth/Steps/CodesStep.tsx
+++ src/pages/settings/Security/TwoFactorAuth/Steps/CodesStep.tsx
@@ -33,6 +33,7 @@ function CodesStep({account, user, backTo}: CodesStepProps) {
     const {translate} = useLocalize();
     const {isExtraSmallScreenWidth, isSmallScreenWidth} = useResponsiveLayout();
     const [error, setError] = useState('');
+    const isUserValidated = user?.validated;
 
     const {setStep} = useTwoFactorAuthContext();
 
@@ -57,70 +58,72 @@ function CodesStep({account, user, backTo}: CodesStepProps) {
             onBackButtonPress={() => TwoFactorAuthActions.quitAndNavigateBack(backTo)}
         >
             <ScrollView contentContainerStyle={styles.flexGrow1}>
-                <Section
-                    title={translate('twoFactorAuth.keepCodesSafe')}
-                    icon={Illustrations.ShieldYellow}
-                    containerStyles={[styles.twoFactorAuthSection]}
-                    iconContainerStyles={[styles.ml6]}
-                >
-                    <View style={styles.mv3}>
-                        <Text>{translate('twoFactorAuth.codesLoseAccess')}</Text>
-                    </View>
-                    <View style={styles.twoFactorAuthCodesBox({isExtraSmallScreenWidth, isSmallScreenWidth})}>
-                        {account?.isLoading ? (
-                            <View style={styles.twoFactorLoadingContainer}>
-                                <ActivityIndicator color={theme.spinner} />
-                            </View>
-                        ) : (
-                            <>
-                                <View style={styles.twoFactorAuthCodesContainer}>
-                                    {!!account?.recoveryCodes &&
-                                        account?.recoveryCodes?.split(', ').map((code) => (
-                                            <Text
-                                                style={styles.twoFactorAuthCode}
-                                                key={code}
-                                            >
-                                                {code}
-                                            </Text>
-                                        ))}
+                {isUserValidated && (
+                    <Section
+                        title={translate('twoFactorAuth.keepCodesSafe')}
+                        icon={Illustrations.ShieldYellow}
+                        containerStyles={[styles.twoFactorAuthSection]}
+                        iconContainerStyles={[styles.ml6]}
+                    >
+                        <View style={styles.mv3}>
+                            <Text>{translate('twoFactorAuth.codesLoseAccess')}</Text>
+                        </View>
+                        <View style={styles.twoFactorAuthCodesBox({isExtraSmallScreenWidth, isSmallScreenWidth})}>
+                            {account?.isLoading ? (
+                                <View style={styles.twoFactorLoadingContainer}>
+                                    <ActivityIndicator color={theme.spinner} />
                                 </View>
-                                <View style={styles.twoFactorAuthCodesButtonsContainer}>
-                                    <PressableWithDelayToggle
-                                        text={translate('twoFactorAuth.copy')}
-                                        textChecked={translate('common.copied')}
-                                        icon={Expensicons.Copy}
-                                        inline={false}
-                                        onPress={() => {
-                                            Clipboard.setString(account?.recoveryCodes ?? '');
-                                            setError('');
-                                            TwoFactorAuthActions.setCodesAreCopied();
-                                        }}
-                                        styles={[styles.button, styles.buttonMedium, styles.twoFactorAuthCodesButton]}
-                                        textStyles={[styles.buttonMediumText]}
-                                        accessible={false}
-                                        tooltipText=""
-                                        tooltipTextChecked=""
-                                    />
-                                    <PressableWithDelayToggle
-                                        text={translate('common.download')}
-                                        icon={Expensicons.Download}
-                                        onPress={() => {
-                                            localFileDownload('two-factor-auth-codes', account?.recoveryCodes ?? '');
-                                            setError('');
-                                            TwoFactorAuthActions.setCodesAreCopied();
-                                        }}
-                                        inline={false}
-                                        styles={[styles.button, styles.buttonMedium, styles.twoFactorAuthCodesButton]}
-                                        textStyles={[styles.buttonMediumText]}
-                                        accessible={false}
-                                        tooltipText=""
-                                        tooltipTextChecked=""
-                                    />
-                                </View>
-                            </>
-                        )}
-                    </View>
-                </Section>
+                            ) : (
+                                <>
+                                    <View style={styles.twoFactorAuthCodesContainer}>
+                                        {!!account?.recoveryCodes &&
+                                            account?.recoveryCodes?.split(', ').map((code) => (
+                                                <Text
+                                                    style={styles.twoFactorAuthCode}
+                                                    key={code}
+                                                >
+                                                    {code}
+                                                </Text>
+                                            ))}
+                                    </View>
+                                    <View style={styles.twoFactorAuthCodesButtonsContainer}>
+                                        <PressableWithDelayToggle
+                                            text={translate('twoFactorAuth.copy')}
+                                            textChecked={translate('common.copied')}
+                                            icon={Expensicons.Copy}
+                                            inline={false}
+                                            onPress={() => {
+                                                Clipboard.setString(account?.recoveryCodes ?? '');
+                                                setError('');
+                                                TwoFactorAuthActions.setCodesAreCopied();
+                                            }}
+                                            styles={[styles.button, styles.buttonMedium, styles.twoFactorAuthCodesButton]}
+                                            textStyles={[styles.buttonMediumText]}
+                                            accessible={false}
+                                            tooltipText=""
+                                            tooltipTextChecked=""
+                                        />
+                                        <PressableWithDelayToggle
+                                            text={translate('common.download')}
+                                            icon={Expensicons.Download}
+                                            onPress={() => {
+                                                localFileDownload('two-factor-auth-codes', account?.recoveryCodes ?? '');
+                                                setError('');
+                                                TwoFactorAuthActions.setCodesAreCopied();
+                                            }}
+                                            inline={false}
+                                            styles={[styles.button, styles.buttonMedium, styles.twoFactorAuthCodesButton]}
+                                            textStyles={[styles.buttonMediumText]}
+                                            accessible={false}
+                                            tooltipText=""
+                                            tooltipTextChecked=""
+                                        />
+                                    </View>
+                                </>
+                            )}
+                        </View>
+                    </Section>
+                )}
                 <FixedFooter style={[styles.mtAuto, styles.pt5]}>
                     {!!error && (
                         <FormHelpMessage
