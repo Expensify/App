9f82a3b289d6d9a0a037f0d50c803271a7faed85
Manan Jadhav <manan.jadhav@gmail.com>
2024-06-30 15:55:24 +0530

    fix: import field param

--- src/pages/workspace/accounting/netsuite/import/NetSuiteImportMappingPage.tsx
+++ src/pages/workspace/accounting/netsuite/import/NetSuiteImportMappingPage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import withPolicyConnections from '@pages/workspace/withPolicyConnections';
 import type {WithPolicyConnectionsProps} from '@pages/workspace/withPolicyConnections';
+import type {StackScreenProps} from '@react-navigation/stack';
+import type SCREENS from '@src/SCREENS';
+import type { SettingsNavigatorParamList } from '@libs/Navigation/types';
 
+type NetSuiteImportMappingPageProps = WithPolicyConnectionsProps & StackScreenProps<SettingsNavigatorParamList, typeof SCREENS.WORKSPACE.ACCOUNTING.NETSUITE_IMPORT_MAPPING>;
 
-function NetSuiteImportMappingPage({policy, importField}: WithPolicyConnectionsProps & { importField: string}) {
+function NetSuiteImportMappingPage({policy, route: {
+  params: {
+    importField
+  }
+}}: NetSuiteImportMappingPageProps) {
   const policyID = policy?.id ?? '-1';
   return <>Import Mapping Page {policyID} {importField}</>
 }
