026bdd5b72fe509892bab47b5bae66494e03d0ad
Marco Chávez <marco@expensify.com>
2024-07-01 14:49:17 -0600

    Remove CurrencyUtils

--- src/pages/home/report/ReportActionItem.tsx
+++ src/pages/home/report/ReportActionItem.tsx
@@ -39,7 +39,6 @@ import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import ControlSelection from '@libs/ControlSelection';
-import * as CurrencyUtils from '@libs/CurrencyUtils';
 import * as DeviceCapabilities from '@libs/DeviceCapabilities';
 import * as ErrorUtils from '@libs/ErrorUtils';
 import focusTextInputAfterAnimation from '@libs/focusTextInputAfterAnimation';
