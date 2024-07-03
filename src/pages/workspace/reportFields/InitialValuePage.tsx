cd47a6e57f3e043c18c725dbb8b50225521052d6
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-07-03 15:28:30 +0200

    integrate InitialValuePage

--- src/pages/workspace/reportFields/InitialValuePage.tsx
+++ src/pages/workspace/reportFields/InitialValuePage.tsx
@@ -1,8 +1,138 @@
-import React from 'react';
-import Text from '@components/Text';
+import type {StackScreenProps} from '@react-navigation/stack';
+import React, {useCallback} from 'react';
+import FormProvider from '@components/Form/FormProvider';
+import InputWrapper from '@components/Form/InputWrapper';
+import type {FormInputErrors, FormOnyxValues} from '@components/Form/types';
+import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import ReportFieldsInitialListValuePicker from '@components/ReportFieldsInitialListValuePicker';
+import ScreenWrapper from '@components/ScreenWrapper';
+import TextInput from '@components/TextInput';
+import useAutoFocusInput from '@hooks/useAutoFocusInput';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
+import Navigation from '@libs/Navigation/Navigation';
+import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
+import * as ReportUtils from '@libs/ReportUtils';
+import NotFoundPage from '@pages/ErrorPage/NotFoundPage';
+import AccessOrNotFoundWrapper from '@pages/workspace/AccessOrNotFoundWrapper';
+import type {WithPolicyAndFullscreenLoadingProps} from '@pages/workspace/withPolicyAndFullscreenLoading';
+import withPolicyAndFullscreenLoading from '@pages/workspace/withPolicyAndFullscreenLoading';
+import CONST from '@src/CONST';
+import * as ErrorUtils from '@src/libs/ErrorUtils';
+import * as ValidationUtils from '@src/libs/ValidationUtils';
+import ONYXKEYS from '@src/ONYXKEYS';
+import type SCREENS from '@src/SCREENS';
+import INPUT_IDS from '@src/types/form/WorkspaceReportFieldsForm';
 
-function InitialValuePage() {
-    return <Text>Coming soon...</Text>;
+type InitialValuePagePageProps = WithPolicyAndFullscreenLoadingProps & StackScreenProps<SettingsNavigatorParamList, typeof SCREENS.WORKSPACE.REPORT_FIELDS_EDIT_INITIAL_VALUE>;
+function InitialValuePage({
+    policy,
+    route: {
+        params: {policyID, reportFieldID},
+    },
+}: InitialValuePagePageProps) {
+    const styles = useThemeStyles();
+    const {translate} = useLocalize();
+    const {inputCallbackRef} = useAutoFocusInput();
+
+    const reportField = policy?.fieldList?.[ReportUtils.getReportFieldKey(reportFieldID)] ?? null;
+    const availableListValuesLength = (reportField?.disabledOptions ?? []).filter((disabledListValue) => !disabledListValue).length;
+
+    const submitForm = useCallback(
+        (values: FormOnyxValues<typeof ONYXKEYS.FORMS.WORKSPACE_REPORT_FIELDS_FORM>) => {
+            console.debug('submitForm', policyID, values);
+            Navigation.goBack();
+        },
+        [policyID],
+    );
+
+    const validateForm = useCallback(
+        (values: FormOnyxValues<typeof ONYXKEYS.FORMS.WORKSPACE_REPORT_FIELDS_FORM>): FormInputErrors<typeof ONYXKEYS.FORMS.WORKSPACE_REPORT_FIELDS_FORM> => {
+            const {name, type, initialValue} = values;
+            const errors: FormInputErrors<typeof ONYXKEYS.FORMS.WORKSPACE_REPORT_FIELDS_FORM> = {};
+
+            if (!ValidationUtils.isRequiredFulfilled(name)) {
+                errors[INPUT_IDS.NAME] = translate('workspace.reportFields.reportFieldNameRequiredError');
+            } else if (Object.values(policy?.fieldList ?? {}).some((currentReportField) => currentReportField.name === name)) {
+                errors[INPUT_IDS.NAME] = translate('workspace.reportFields.existingReportFieldNameError');
+            } else if ([...name].length > CONST.WORKSPACE_REPORT_FIELD_POLICY_MAX_LENGTH) {
+                // Uses the spread syntax to count the number of Unicode code points instead of the number of UTF-16 code units.
+                ErrorUtils.addErrorMessage(
+                    errors,
+                    INPUT_IDS.NAME,
+                    translate('common.error.characterLimitExceedCounter', {length: [...name].length, limit: CONST.WORKSPACE_REPORT_FIELD_POLICY_MAX_LENGTH}),
+                );
+            }
+
+            if (type === CONST.REPORT_FIELD_TYPES.LIST && availableListValuesLength > 0 && !ValidationUtils.isRequiredFulfilled(initialValue)) {
+                errors[INPUT_IDS.INITIAL_VALUE] = translate('workspace.reportFields.reportFieldInitialValueRequiredError');
+            }
+
+            return errors;
+        },
+        [availableListValuesLength, policy?.fieldList, translate],
+    );
+
+    if (!reportField) {
+        return <NotFoundPage />;
+    }
+
+    const isTextFieldType = reportField.type === CONST.REPORT_FIELD_TYPES.TEXT;
+    const isListFieldType = reportField.type === CONST.REPORT_FIELD_TYPES.LIST;
+
+    return (
+        <AccessOrNotFoundWrapper
+            accessVariants={[CONST.POLICY.ACCESS_VARIANTS.ADMIN, CONST.POLICY.ACCESS_VARIANTS.PAID]}
+            policyID={policyID}
+            featureName={CONST.POLICY.MORE_FEATURES.ARE_REPORT_FIELDS_ENABLED}
+        >
+            <ScreenWrapper
+                includeSafeAreaPaddingBottom={false}
+                style={styles.defaultModalContainer}
+                testID={InitialValuePage.displayName}
+                shouldEnableMaxHeight
+            >
+                <HeaderWithBackButton
+                    title={reportField.name}
+                    onBackButtonPress={Navigation.goBack}
+                />
+                <FormProvider
+                    formID={ONYXKEYS.FORMS.WORKSPACE_REPORT_FIELDS_FORM}
+                    onSubmit={submitForm}
+                    submitButtonText={translate('common.save')}
+                    validate={validateForm}
+                    style={styles.flex1}
+                    enabledWhenOffline
+                    submitButtonStyles={styles.mh5}
+                >
+                    {isTextFieldType && (
+                        <InputWrapper
+                            InputComponent={TextInput}
+                            inputID={INPUT_IDS.INITIAL_VALUE}
+                            label={translate('common.initialValue')}
+                            accessibilityLabel={translate('workspace.editor.initialValueInputLabel')}
+                            maxLength={CONST.WORKSPACE_REPORT_FIELD_POLICY_MAX_LENGTH}
+                            multiline={false}
+                            containerStyles={styles.mh5}
+                            role={CONST.ROLE.PRESENTATION}
+                            ref={inputCallbackRef}
+                        />
+                    )}
+                    {isListFieldType && (
+                        <InputWrapper
+                            InputComponent={ReportFieldsInitialListValuePicker}
+                            inputID={INPUT_IDS.INITIAL_VALUE}
+                            listValues={reportField.values}
+                            disabledOptions={reportField.disabledOptions}
+                            value={reportField.defaultValue}
+                        />
+                    )}
+                </FormProvider>
+            </ScreenWrapper>
+        </AccessOrNotFoundWrapper>
+    );
 }
 
-export default InitialValuePage;
+InitialValuePage.displayName = 'InitialValuePage';
+
+export default withPolicyAndFullscreenLoading(InitialValuePage);
