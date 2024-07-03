55feafb2c173693b11c2143c8b5d6112dfc53066
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-07-03 11:20:52 +0200

    render mapview when access token is set in a library

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -37,6 +37,7 @@ const MapView = forwardRef<MapViewHandle, ComponentProps>(
         const currentPosition = userLocation ?? initialLocation;
         const [userInteractedWithMap, setUserInteractedWithMap] = useState(false);
         const shouldInitializeCurrentPosition = useRef(true);
+        const [isAccessTokenSet, setIsAccessTokenSet] = useState(false);
 
         // Determines if map can be panned to user's detected
         // location without bothering the user. It will return
@@ -138,7 +139,12 @@ const MapView = forwardRef<MapViewHandle, ComponentProps>(
         }, [navigation]);
 
         useEffect(() => {
-            setAccessToken(accessToken);
+            setAccessToken(accessToken).then((token) => {
+                if(!token) {
+                    return;
+                }
+                setIsAccessTokenSet(true);
+            });
         }, [accessToken]);
 
         const setMapIdle = (e: MapState) => {
@@ -198,7 +204,7 @@ const MapView = forwardRef<MapViewHandle, ComponentProps>(
         const initCenterCoordinate = useMemo(() => (interactive ? centerCoordinate : undefined), [interactive, centerCoordinate]);
         const initBounds = useMemo(() => (interactive ? undefined : waypointsBounds), [interactive, waypointsBounds]);
 
-        return !isOffline && !!accessToken && !!defaultSettings ? (
+        return !isOffline && isAccessTokenSet && !!defaultSettings ? (
             <View style={[style, !interactive ? styles.pointerEventsNone : {}]}>
                 <Mapbox.MapView
                     style={{flex: 1}}
