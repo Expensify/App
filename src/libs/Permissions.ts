6f12b112b9c5ed7efe708412a67e34bf6db7f53a
Agata Kosior <agata.kosior57@gmail.com>
2024-07-04 10:17:39 +0200

    fix: change icon

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -53,6 +53,7 @@ function canUseReportFieldsFeature(betas: OnyxEntry<Beta[]>): boolean {
 }
 
 function canUseWorkspaceFeeds(betas: OnyxEntry<Beta[]>): boolean {
+    return true;
     return !!betas?.includes(CONST.BETAS.WORKSPACE_FEEDS) || canUseAllBetas(betas);
 }
 
