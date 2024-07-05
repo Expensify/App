60835a06dcaf6c950f4562b0dc1312bea2d75730
Mateusz Titz <titzmateusz@gmail.com>
2024-07-05 13:01:05 +0200

    use custom interpolator also for Modal Stack navigators

--- src/libs/Navigation/AppNavigator/ModalStackNavigators/useModalScreenOptions.ts
+++ src/libs/Navigation/AppNavigator/ModalStackNavigators/useModalScreenOptions.ts
@@ -1,19 +1,25 @@
-import type {StackNavigationOptions} from '@react-navigation/stack';
-import {CardStyleInterpolators} from '@react-navigation/stack';
+import type {StackCardInterpolationProps, StackNavigationOptions} from '@react-navigation/stack';
 import {useMemo} from 'react';
+import useStyleUtils from '@hooks/useStyleUtils';
 import useThemeStyles from '@hooks/useThemeStyles';
+import useWindowDimensions from '@hooks/useWindowDimensions';
+import createModalCardStyleInterpolator from '@navigation/AppNavigator/createModalCardStyleInterpolator';
 import type {ThemeStyles} from '@src/styles';
 
 function useModalScreenOptions(getScreenOptions?: (styles: ThemeStyles) => StackNavigationOptions) {
     const styles = useThemeStyles();
+    const styleUtils = useStyleUtils();
+    const {isSmallScreenWidth} = useWindowDimensions();
+
+    const customInterpolator = createModalCardStyleInterpolator(styleUtils);
 
     const defaultSubRouteOptions = useMemo(
         (): StackNavigationOptions => ({
             cardStyle: styles.navigationScreenCardStyle,
             headerShown: false,
-            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+            cardStyleInterpolator: (props: StackCardInterpolationProps) => customInterpolator(isSmallScreenWidth, false, false, props),
         }),
-        [styles],
+        [styles, customInterpolator, isSmallScreenWidth],
     );
 
     return getScreenOptions?.(styles) ?? defaultSubRouteOptions;
