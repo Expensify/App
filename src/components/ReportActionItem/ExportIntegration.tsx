9235c987b0acb6145193e3d1520b0a84d2ab74f6
Andrew Rosiclair <arosiclair@expensify.com>
2024-07-05 17:06:04 -0400

    handle multiple non-reimbursable urls

--- src/components/ReportActionItem/ExportIntegration.tsx
+++ src/components/ReportActionItem/ExportIntegration.tsx
@@ -6,6 +6,7 @@ import TextLink from '@components/TextLink';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as ReportActionUtils from '@libs/ReportActionsUtils';
+import CONST from '@src/CONST';
 import type {ReportAction} from '@src/types/onyx';
 import type {OriginalMessageExportIntegration} from '@src/types/onyx/OriginalMessage';
 
@@ -17,6 +18,9 @@ function ExportIntegration({action}: ExportIntegrationProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
     const {label, markedManually, reimbursableUrls, nonReimbursableUrls} = (ReportActionUtils.getOriginalMessage(action) ?? {}) as OriginalMessageExportIntegration;
+    const wasExportedAfterBase62 = (action?.created ?? '') > '2022-11-14';
+    const reportID = action?.reportID ?? '';
+    const base62ReportID = 'BASE62REPORTID';
 
     const exportText = markedManually ? translate('report.actions.type.exportedToIntegration.manual', {label}) : translate('report.actions.type.exportedToIntegration.automatic', {label});
 
@@ -25,9 +29,27 @@ function ExportIntegration({action}: ExportIntegrationProps) {
     if (reimbursableUrls.length === 1) {
         linkText = translate('report.actions.type.exportedToIntegration.reimburseableLink');
         linkURL = reimbursableUrls[0];
-    } else if (nonReimbursableUrls.length >= 1) {
+    } else if (nonReimbursableUrls.length) {
         linkText = translate('report.actions.type.exportedToIntegration.nonReimbursableLink');
-        linkURL = nonReimbursableUrls[0];
+
+        if (nonReimbursableUrls.length === 1) {
+            linkURL = nonReimbursableUrls[0];
+        } else {
+            switch (label) {
+                case CONST.POLICY.CONNECTIONS.NAME_USER_FRIENDLY.xero:
+                    linkURL = 'https://go.xero.com/Bank/BankAccounts.aspx';
+                    break;
+                case CONST.POLICY.CONNECTIONS.NAME_USER_FRIENDLY.netsuite:
+                    linkURL = 'https://system.netsuite.com/app/common/search/ubersearchresults.nl?quicksearch=T&searchtype=Uber&frame=be&Uber_NAMEtype=KEYWORDSTARTSWITH&Uber_NAME=';
+                    linkURL += wasExportedAfterBase62 ? base62ReportID : reportID;
+                    break;
+                case CONST.POLICY.CONNECTIONS.NAME_USER_FRIENDLY.financialForce:
+                    linkURL = '';
+                    break;
+                default:
+                    linkURL = 'https://qbo.intuit.com/app/expenses';
+            }
+        }
     }
 
     return (
