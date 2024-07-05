221cf36dc2ebdaaafa51f77d81d2d45d4fd252c7
Artem Makushov <waterim3009@gmail.com>
2024-07-05 14:42:48 +0200

    fix comments

--- src/libs/actions/Policy/ReportField.ts
+++ src/libs/actions/Policy/ReportField.ts
@@ -31,11 +31,11 @@ Onyx.connect({
 const allPolicies: OnyxCollection<Policy> = {};
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.POLICY,
-    callback: (val, key) => {
+    callback: (value, key) => {
         if (!key) {
             return;
         }
-        if (val === null || val === undefined) {
+        if (value === null || value === undefined) {
             // If we are deleting a policy, we have to check every report linked to that policy
             // and unset the draft indicator (pencil icon) alongside removing any draft comments. Clearing these values will keep the newly archived chats from being displayed in the LHN.
             // More info: https://github.com/Expensify/App/issues/14260
@@ -57,7 +57,7 @@ Onyx.connect({
             return;
         }
 
-        allPolicies[key] = val;
+        allPolicies[key] = value;
     },
 });
 
