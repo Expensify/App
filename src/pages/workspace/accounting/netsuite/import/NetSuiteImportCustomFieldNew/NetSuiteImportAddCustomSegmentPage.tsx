91eb969fc89638a297dbaed1c622a987f931a62d
Manan Jadhav <manan.jadhav@gmail.com>
2024-07-08 03:02:44 +0530

    feat: missing temp keys

--- src/pages/workspace/accounting/netsuite/import/NetSuiteImportCustomFieldNew/NetSuiteImportAddCustomSegmentPage.tsx
+++ src/pages/workspace/accounting/netsuite/import/NetSuiteImportCustomFieldNew/NetSuiteImportAddCustomSegmentPage.tsx
@@ -14,11 +14,11 @@ import type {WithPolicyConnectionsProps} from '@pages/workspace/withPolicyConnec
 import withPolicyConnections from '@pages/workspace/withPolicyConnections';
 import * as FormActions from '@userActions/FormActions';
 import CONST from '@src/CONST';
+import type {TranslationPaths} from '@src/languages/types';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import INPUT_IDS from '@src/types/form/NetSuiteCustomFieldForm';
 import type {NetSuiteCustomFieldMapping} from '@src/types/onyx/Policy';
-import type {TranslationPaths} from '@src/languages/types';
 import ChooseSegmentTypeStep from './substeps/ChooseSegmentTypeStep';
 import ConfirmCustomSegmentList from './substeps/ConfirmCustomSegmentList';
 import CustomSegmentInternalIdStep from './substeps/CustomSegmentInternalIdStep';
@@ -74,13 +74,13 @@ function NetSuiteImportAddCustomSegmentPage({policy}: WithPolicyConnectionsProps
         ref.current?.moveNext();
         nextScreen();
     };
-    
+
     const segmentType = addCustomSegmentFormDraft?.[INPUT_IDS.CUSTOM_SEGMENT_TYPE];
 
     return (
         <ConnectionLayout
             displayName={NetSuiteImportAddCustomSegmentPage.displayName}
-            headerTitle={`workspace.netsuite.import.importCustomFields.customSegments.${segmentType ? `addForm.${segmentType}AddTitle`: 'addText'}` as TranslationPaths}
+            headerTitle={`workspace.netsuite.import.importCustomFields.customSegments.${segmentType ? `addForm.${segmentType}AddTitle` : 'addText'}` as TranslationPaths}
             accessVariants={[CONST.POLICY.ACCESS_VARIANTS.ADMIN, CONST.POLICY.ACCESS_VARIANTS.PAID]}
             policyID={policyID}
             featureName={CONST.POLICY.MORE_FEATURES.ARE_CONNECTIONS_ENABLED}
