e8441ef9cafb5b04da8a96fb5ec32067375a6206
cdOut <88325488+cdOut@users.noreply.github.com>
2024-07-08 13:18:06 +0200

    correct imports and prettier

--- src/components/InlineCodeBlock/WrappedText.tsx
+++ src/components/InlineCodeBlock/WrappedText.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
-import type {NativeSyntheticEvent, TextLayoutEventData, StyleProp, TextStyle, ViewStyle} from 'react-native';
+import type {NativeSyntheticEvent, StyleProp, TextLayoutEventData, TextStyle, ViewStyle} from 'react-native';
 import Text from '@components/Text';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {containsOnlyEmojis} from '@libs/EmojiUtils';
@@ -50,33 +50,35 @@ function WrappedText({children, wordStyles, textStyles}: WrappedTextProps) {
         );
     }
 
-    return (<>
-        {lines.map((line, index) => (
-            <View
-                // eslint-disable-next-line react/no-array-index-key
-                key={index}
-                style={styles.codeWordWrapper}
-            >
-                <View style={[wordStyles, index === 0 && styles.codeFirstWordStyle, index === lines.length - 1 && styles.codeLastWordStyle]}>
-                    <Text style={[textStyles, !containsEmoji(line) && styles.codePlainTextStyle]}>
-                        {Array.from(line).map((char, charIndex) =>
-                            containsOnlyEmojis(char) ? (
-                                <Text
-                                    // eslint-disable-next-line react/no-array-index-key
-                                    key={`${index}-${charIndex}`}
-                                    style={[textStyles, styles.emojiDefaultStyles]}
-                                >
-                                    {char}
-                                </Text>
-                            ) : (
-                                char
-                            ),
-                        )}
-                    </Text>
+    return (
+        <>
+            {lines.map((line, index) => (
+                <View
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={index}
+                    style={styles.codeWordWrapper}
+                >
+                    <View style={[wordStyles, index === 0 && styles.codeFirstWordStyle, index === lines.length - 1 && styles.codeLastWordStyle]}>
+                        <Text style={[textStyles, !containsEmoji(line) && styles.codePlainTextStyle]}>
+                            {Array.from(line).map((char, charIndex) =>
+                                containsOnlyEmojis(char) ? (
+                                    <Text
+                                        // eslint-disable-next-line react/no-array-index-key
+                                        key={`${index}-${charIndex}`}
+                                        style={[textStyles, styles.emojiDefaultStyles]}
+                                    >
+                                        {char}
+                                    </Text>
+                                ) : (
+                                    char
+                                ),
+                            )}
+                        </Text>
+                    </View>
                 </View>
-            </View>
-        ))}
-    </>);
+            ))}
+        </>
+    );
 }
 
 WrappedText.displayName = 'WrappedText';
