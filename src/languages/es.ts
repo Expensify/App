5695c02046c37bb7b39e7447803d626fa4a72f97
Manan Jadhav <manan.jadhav@gmail.com>
2024-07-08 09:55:42 +0530

    fix: added spanish keys

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -2444,7 +2444,6 @@ export default {
                 importCustomFields: {
                     chooseOptionBelow: 'Choose an option below:',
                     requiredFieldError: (fieldName: string) => `Please enter the ${fieldName}`,
-                    uniqueTransactionFieldIdError: `A custom list with this transaction field ID already exists.`,
                     customSegments: {
                         title: 'Segmentos/registros personalizados',
                         addText: 'Añadir segmento/registro personalizado',
@@ -2484,6 +2483,9 @@ export default {
                             customSegmentMappingTitle: 'How should this custom segment be displayed in Expensify?',
                             customRecordMappingTitle: 'How should this custom segment record be displayed in Expensify?',
                         },
+                        errors: {
+                            uniqueFieldError: (fieldName: string) => `A custom segment/record with this ${fieldName?.toLowerCase()} already exists.`,
+                        },
                     },
                     customLists: {
                         title: 'Listas personalizados',
@@ -2506,6 +2508,9 @@ export default {
                             transactionFieldIDTitle: "What's the transaction field ID?",
                             transactionFieldIDFooter: `You can find transaction field IDs in NetSuite by following these steps:\n\n1. Enter "Transaction Line Fields" in global search.\n2. Click into a custom list.\n3. Find the transaction field ID on the left-hand side.\n\n_For more detailed instructions, [visit our help site](${CONST.NETSUITE_IMPORT.HELP_LINKS.CUSTOM_LISTS})_.`,
                             mappingTitle: 'How should this custom list be displayed in Expensify?',
+                        },
+                        errors: {
+                            uniqueTransactionFieldIDError: `A custom list with this transaction field ID already exists.`,
                         },
                     },
                 },
