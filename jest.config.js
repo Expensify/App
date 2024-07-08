992a12d7b8de49c186ee49a114ef90cf50293a4f
kirillzyusko <kiryl@margelo.io>
2024-07-08 15:34:11 +0200

    chore: return original path back

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const testFileExtension = '[jt]s?(x)';
 module.exports = {
     preset: 'jest-expo',
     testMatch: [
-        `<rootDir>/tests/e2e/**/spec/*.spec.${testFileExtension}`,
+        `<rootDir>/tests/e2e/**/*.spec.${testFileExtension}`,
         `<rootDir>/tests/ui/**/*.${testFileExtension}`,
         `<rootDir>/tests/unit/**/*.${testFileExtension}`,
         `<rootDir>/tests/actions/**/*.${testFileExtension}`,
