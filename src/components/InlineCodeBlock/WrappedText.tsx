92d3147f02d9b8a8f1e82951bc8742f021c080f7
cdOut <88325488+cdOut@users.noreply.github.com>
2024-07-08 12:59:36 +0200

    add handling for wrapped code text for native devices

--- src/components/InlineCodeBlock/WrappedText.tsx
+++ src/components/InlineCodeBlock/WrappedText.tsx
@@ -1,6 +1,6 @@
-import React, {Fragment} from 'react';
-import type {StyleProp, TextStyle, ViewStyle} from 'react-native';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {TextLayoutEventData, View} from 'react-native';
+import type {NativeSyntheticEvent, StyleProp, TextStyle, ViewStyle} from 'react-native';
 import Text from '@components/Text';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {containsOnlyEmojis} from '@libs/EmojiUtils';
@@ -11,77 +11,71 @@ type WrappedTextProps = ChildrenProps & {
     /** Style to be applied to Text */
     textStyles?: StyleProp<TextStyle>;
 
-    /**
-     * Style for each individual word (token) in the text. Note that a token can also include whitespace characters between words.
-     */
+    /** Style for each individual word (token) in the text */
     wordStyles?: StyleProp<ViewStyle>;
 };
 
-/**
- * Breaks the text into matrix
- *
- * @example
- * const text = "My Name is Rajat";
- * const resultMatrix = getTextMatrix(text);
- * console.log(resultMatrix);
- * // Output:
- * // [
- * //   ['My', ' ', 'Name', ' ', 'is', ' ', 'Rajat'],
- * // ]
- */
-function getTextMatrix(text: string): string[][] {
-    return text.split('\n').map((row) => row.split(CONST.REGEX.SPACE_OR_EMOJI).filter((value) => value !== ''));
-}
-
-/**
- * Validates if the text contains any emoji
- */
+/** Validates if the text contains any emoji */
 function containsEmoji(text: string): boolean {
     return CONST.REGEX.EMOJIS.test(text);
 }
 
 function WrappedText({children, wordStyles, textStyles}: WrappedTextProps) {
+    const [lines, setLines] = useState<string[]>([]);
     const styles = useThemeStyles();
 
     if (typeof children !== 'string') {
         return null;
     }
 
-    const textMatrix = getTextMatrix(children);
+    type TextLayoutEvent = NativeSyntheticEvent<TextLayoutEventData>;
 
-    return textMatrix.map((rowText, rowIndex) => (
-        <Fragment
-            // eslint-disable-next-line react/no-array-index-key
-            key={`${rowText[0]}-${rowIndex}`}
-        >
-            {rowText.map((colText, colIndex) => (
-                // Outer View is important to vertically center the Text
-                <View
-                    // eslint-disable-next-line react/no-array-index-key
-                    key={`${colText}-${colIndex}`}
-                    style={styles.codeWordWrapper}
-                >
-                    <View style={[wordStyles, colIndex === 0 && styles.codeFirstWordStyle, colIndex === rowText.length - 1 && styles.codeLastWordStyle]}>
-                        <Text style={[textStyles, !containsEmoji(colText) && styles.codePlainTextStyle]}>
-                            {Array.from(colText).map((char, charIndex) =>
-                                containsOnlyEmojis(char) ? (
-                                    <Text
-                                        // eslint-disable-next-line react/no-array-index-key
-                                        key={`${colIndex}-${charIndex}`}
-                                        style={[textStyles, styles.emojiDefaultStyles]}
-                                    >
-                                        {char}
-                                    </Text>
-                                ) : (
-                                    char
-                                ),
-                            )}
-                        </Text>
+    const handleTextLayout = (event: TextLayoutEvent) => {
+        const {
+            nativeEvent: {lines: textLines},
+        } = event;
+        setLines(textLines.map((line: {text: string}) => line.text));
+    };
+
+    return (
+        <>
+            {!!lines.length ? (
+                lines.map((line, index) => (
+                    <View
+                        key={index}
+                        style={styles.codeWordWrapper}
+                    >
+                        <View style={[wordStyles, index === 0 && styles.codeFirstWordStyle, index === lines.length - 1 && styles.codeLastWordStyle]}>
+                            <Text style={[textStyles, !containsEmoji(line) && styles.codePlainTextStyle]}>
+                                {Array.from(line).map((char, charIndex) =>
+                                    containsOnlyEmojis(char) ? (
+                                        <Text
+                                            // eslint-disable-next-line react/no-array-index-key
+                                            key={`${index}-${charIndex}`}
+                                            style={[textStyles, styles.emojiDefaultStyles]}
+                                        >
+                                            {char}
+                                        </Text>
+                                    ) : (
+                                        char
+                                    ),
+                                )}
+                            </Text>
+                        </View>
                     </View>
+                ))
+            ) : (
+                <View style={{position: 'absolute', opacity: 0, width: '100%'}}>
+                    <Text
+                        style={[textStyles]}
+                        onTextLayout={handleTextLayout}
+                    >
+                        {children}
+                    </Text>
                 </View>
-            ))}
-        </Fragment>
-    ));
+            )}
+        </>
+    );
 }
 
 WrappedText.displayName = 'WrappedText';
