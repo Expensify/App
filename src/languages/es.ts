208180d995a6aac45761b009f2000863d485307b
Wildan Muhlis <muhliswildan@gmail.com>
2024-07-08 10:45:11 +0700

    Revise attachmentImageTooLarge spanish translation

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -384,7 +384,7 @@ export default {
         folderNotAllowedMessage: 'Subir una carpeta no está permitido. Prueba con otro archivo.',
         protectedPDFNotSupported: 'Los PDFs con contraseña no son compatibles',
         attachmentImageResized: 'Se ha cambiado el tamaño de esta imagen para obtener una vista previa. Descargar para resolución completa.',
-        attachmentImageTooLarge: 'Esta imagen es demasiado grande para obtener una vista previa antes de cargarla.',
+        attachmentImageTooLarge: 'Esta imagen es demasiado grande para obtener una vista previa antes de subirla.',
     },
     avatarCropModal: {
         title: 'Editar foto',
