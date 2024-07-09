d0ce3ee3b3e83701fa53812a2758fa9545a6ccc0
Manan Jadhav <manan.jadhav@gmail.com>
2024-07-09 15:36:27 +0530

    fix: added draft for transaction field id

--- src/pages/workspace/accounting/netsuite/import/NetSuiteImportCustomFieldNew/substeps/TransactionFieldIDStep.tsx
+++ src/pages/workspace/accounting/netsuite/import/NetSuiteImportCustomFieldNew/substeps/TransactionFieldIDStep.tsx
@@ -28,6 +28,7 @@ function TransactionFieldIDStep() {
                 aria-label={fieldLabel}
                 role={CONST.ROLE.PRESENTATION}
                 spellCheck={false}
+                shouldSaveDraft
             />
             <View style={[styles.flex1, styles.mv3, styles.renderHTML, styles.textDecorationSkipInkNone]}>
                 <RenderHTML html={`<comment>${parser.replace(translate(`workspace.netsuite.import.importCustomFields.customLists.addForm.transactionFieldIDFooter`))}</comment>`} />
