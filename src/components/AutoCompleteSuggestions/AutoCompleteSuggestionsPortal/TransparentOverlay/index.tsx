d00604b0fc3568e5046ae70941e40a7909aa1553
Taras Perun <terry.perun@gmail.com>
2024-07-09 15:56:25 +0200

    TransparentOverlay

---
+++ src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/TransparentOverlay/index.tsx
@@ -0,0 +1,40 @@
+import React, {useCallback} from 'react';
+import {View} from 'react-native';
+import type {GestureResponderEvent} from 'react-native';
+import PressableWithoutFeedback from '@components/Pressable/PressableWithoutFeedback';
+import useThemeStyles from '@hooks/useThemeStyles';
+import CONST from '@src/CONST';
+
+type TransparentOverlayProps = {
+    resetSuggestions: () => void;
+};
+type OnPressHandler = (event: GestureResponderEvent) => void;
+
+function TransparentOverlay({resetSuggestions}: TransparentOverlayProps) {
+    const styles = useThemeStyles();
+
+    const onResetSuggestions: OnPressHandler = useCallback(
+        (event: GestureResponderEvent) => {
+            event?.preventDefault();
+            resetSuggestions();
+        },
+        [resetSuggestions],
+    );
+    return (
+        <View
+            onPointerDown={(e: GestureResponderEvent) => {
+                e?.preventDefault();
+            }}
+            style={styles.fullScreen}
+        >
+            <PressableWithoutFeedback
+                onPress={onResetSuggestions}
+                style={styles.flex1}
+                accessibilityLabel={CONST.ROLE.NONE}
+                role={CONST.ROLE.NONE}
+            />
+        </View>
+    );
+}
+
+export default TransparentOverlay;
