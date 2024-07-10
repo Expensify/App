e70ae1e092a457c2797f6908dfd6227e6f2297d7
tienifr <christianwen18@gmail.com>
2024-07-10 18:49:43 +0700

    add explanative comments

--- src/components/Form/SafariFormWrapper.tsx
+++ src/components/Form/SafariFormWrapper.tsx
@@ -4,6 +4,9 @@ import type ChildrenProps from '@src/types/utils/ChildrenProps';
 
 type SafariFormWrapperProps = ChildrenProps;
 
+/**
+ * If we used any <input> without <form> wrapper, Safari 11+ would show the auto-fill suggestion popup.
+ */
 function SafariFormWrapper({children}: SafariFormWrapperProps) {
     if (isSafari()) {
         return <form>{children}</form>;
