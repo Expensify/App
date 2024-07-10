7cc38dec35821780782d38e8a4c50532cee2ef20
dominictb <tb-dominic@outlook.com>
2024-07-10 14:44:48 +0700

    Revert "fix: update explanation comment"
    This reverts commit 5cd9f11a5623df8ad537161467d63728c3f3cd25.

--- src/pages/iou/request/IOURequestStartPage.tsx
+++ src/pages/iou/request/IOURequestStartPage.tsx
@@ -96,10 +96,9 @@ function IOURequestStartPage({
         if (transaction?.reportID === reportID) {
             return;
         }
-        // if transaction is from global create, the original behavior is that the user can change the recipient reportID
-        // without having to reset the whole transaction, hence we don't need to re-init the transaction in this case
+        // if transaction is from global create, reset the reportID & participant
         if (transaction?.isFromGlobalCreate && transaction.transactionID) {
-            IOU.resetMoneyRequestReportID(transaction.transactionID, reportID, isFromGlobalCreate);
+            IOU.resetMoneyRequest(transaction.transactionID, reportID, isFromGlobalCreate);
             return;
         }
         IOU.initMoneyRequest(reportID, policy, isFromGlobalCreate, transactionRequestType.current);
