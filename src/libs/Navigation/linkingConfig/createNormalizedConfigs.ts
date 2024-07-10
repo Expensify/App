d3b58bc64adc79346506cadc1edec107b9d23138
Jan Nowakowski <janeknowakowski9@gmail.com>
2024-07-10 11:56:54 +0200

    fix linter

--- src/libs/Navigation/linkingConfig/createNormalizedConfigs.ts
+++ src/libs/Navigation/linkingConfig/createNormalizedConfigs.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/default-param-last */
 
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
