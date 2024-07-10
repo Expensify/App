fba3ffa7dd17744b9fbfeca91d87537859086ded
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-07-10 10:57:09 +0200

    translation into Spanish

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -2790,7 +2790,7 @@ export default {
             subtitle: 'Los campos de informe se aplican a todos los gastos y pueden ser útiles cuando quieras solicitar información adicional.',
             disableReportFields: 'Desactivar campos de informe',
             disableReportFieldsConfirmation: 'Estás seguro? Se eliminarán los campos de texto y fecha y se desactivarán las listas.',
-            importedFromAccountingSoftware: 'The report fields below are imported from your',
+            importedFromAccountingSoftware: 'Campos de informes importadas desde',
             textType: 'Texto',
             dateType: 'Fecha',
             dropdownType: 'Lista',
