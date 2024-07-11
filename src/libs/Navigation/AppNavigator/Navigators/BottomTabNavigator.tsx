f306ab25b35d5d90fb7a1ea08b2f8eef4be9cbe1
truph01 <truphd01@gmail.com>
2024-07-11 22:38:06 +0700

    Fix: Move effect to BottomTabNavigator

--- src/libs/Navigation/AppNavigator/Navigators/BottomTabNavigator.tsx
+++ src/libs/Navigation/AppNavigator/Navigators/BottomTabNavigator.tsx
@@ -1,11 +1,17 @@
 import {useNavigationState} from '@react-navigation/native';
 import type {StackNavigationOptions} from '@react-navigation/stack';
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useOnyx} from 'react-native-onyx';
+import useActiveWorkspace from '@hooks/useActiveWorkspace';
+import usePrevious from '@hooks/usePrevious';
+import {updateLastAccessedWorkspace} from '@libs/actions/Policy/Policy';
 import createCustomBottomTabNavigator from '@libs/Navigation/AppNavigator/createCustomBottomTabNavigator';
 import getTopmostCentralPaneRoute from '@libs/Navigation/getTopmostCentralPaneRoute';
+import Navigation from '@libs/Navigation/Navigation';
 import type {BottomTabNavigatorParamList, CentralPaneName, NavigationPartialRoute, RootStackParamList} from '@libs/Navigation/types';
 import SidebarScreen from '@pages/home/sidebar/SidebarScreen';
 import SearchPageBottomTab from '@pages/Search/SearchPageBottomTab';
+import ONYXKEYS from '@src/ONYXKEYS';
 import SCREENS from '@src/SCREENS';
 import type ReactComponentModule from '@src/types/utils/ReactComponentModule';
 import ActiveRouteContext from './ActiveRouteContext';
@@ -20,6 +26,19 @@ const screenOptions: StackNavigationOptions = {
 
 function BottomTabNavigator() {
     const activeRoute = useNavigationState<RootStackParamList, NavigationPartialRoute<CentralPaneName> | undefined>(getTopmostCentralPaneRoute);
+    const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
+    const {activeWorkspaceID} = useActiveWorkspace();
+    const selectedPolicy = Object.values(policies ?? {}).find((o) => o?.id === activeWorkspaceID);
+    const prevSelectedPolicy = usePrevious(selectedPolicy);
+    useEffect(() => {
+        if (!selectedPolicy && !prevSelectedPolicy) {
+            return;
+        }
+        if (prevSelectedPolicy && !selectedPolicy && activeWorkspaceID !== undefined) {
+            Navigation.navigateWithSwitchPolicyID({policyID: undefined});
+            updateLastAccessedWorkspace(undefined);
+        }
+    }, [prevSelectedPolicy, selectedPolicy, activeWorkspaceID]);
 
     return (
         <ActiveRouteContext.Provider value={activeRoute}>
--- src/pages/home/sidebar/SidebarScreen/BaseSidebarScreen.tsx
+++ src/pages/home/sidebar/SidebarScreen/BaseSidebarScreen.tsx
@@ -1,20 +1,15 @@
 import React, {useEffect} from 'react';
 import {View} from 'react-native';
-import {useOnyx} from 'react-native-onyx';
 import ScreenWrapper from '@components/ScreenWrapper';
 import useActiveWorkspaceFromNavigationState from '@hooks/useActiveWorkspaceFromNavigationState';
 import useLocalize from '@hooks/useLocalize';
-import usePrevious from '@hooks/usePrevious';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {updateLastAccessedWorkspace} from '@libs/actions/Policy/Policy';
 import * as Browser from '@libs/Browser';
 import TopBar from '@libs/Navigation/AppNavigator/createCustomBottomTabNavigator/TopBar';
-import Navigation from '@libs/Navigation/Navigation';
 import Performance from '@libs/Performance';
 import SidebarLinksData from '@pages/home/sidebar/SidebarLinksData';
 import Timing from '@userActions/Timing';
 import CONST from '@src/CONST';
-import ONYXKEYS from '@src/ONYXKEYS';
 
 /**
  * Function called when a pinned chat is selected.
@@ -33,19 +28,6 @@ function BaseSidebarScreen() {
         Performance.markStart(CONST.TIMING.SIDEBAR_LOADED);
         Timing.start(CONST.TIMING.SIDEBAR_LOADED, true);
     }, []);
-
-    const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
-    const selectedPolicy = Object.values(policies ?? {}).find((o) => o?.id === activeWorkspaceID);
-    const prevSelectedPolicy = usePrevious(selectedPolicy);
-    useEffect(() => {
-        if (!selectedPolicy && !prevSelectedPolicy) {
-            return;
-        }
-        if (prevSelectedPolicy && !selectedPolicy && activeWorkspaceID !== undefined) {
-            Navigation.navigateWithSwitchPolicyID({policyID: undefined});
-            updateLastAccessedWorkspace(undefined);
-        }
-    }, [prevSelectedPolicy, selectedPolicy, activeWorkspaceID]);
 
     return (
         <ScreenWrapper
