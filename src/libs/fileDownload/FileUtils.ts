78ac528d1c0699aa5cf58652973d3ac52e993b33
Wildan Muhlis <muhliswildan@gmail.com>
2024-07-11 11:31:38 +0700

    fix isHighResolutionImage

--- src/libs/fileDownload/FileUtils.ts
+++ src/libs/fileDownload/FileUtils.ts
@@ -282,7 +282,7 @@ function getFileResolution(targetFile: FileObject | undefined): Promise<{width: 
 }
 
 function isHighResolutionImage(resolution: {width: number; height: number} | null): boolean {
-    return resolution !== null && resolution.width > CONST.IMAGE_HIGH_RESOLUTION_THRESHOLD && resolution.height > CONST.IMAGE_HIGH_RESOLUTION_THRESHOLD;
+    return resolution !== null && (resolution.width > CONST.IMAGE_HIGH_RESOLUTION_THRESHOLD || resolution.height > CONST.IMAGE_HIGH_RESOLUTION_THRESHOLD);
 }
 
 export {
