3f7cd2a6800454185c825bb78dc5450bbfab3799
kirillzyusko <kiryl@margelo.io>
2024-07-12 17:18:52 +0200

    fix: re-create GestureDetector after freeze being enabled/disabled

--- src/pages/home/report/ReportActionCompose/SendButton.tsx
+++ src/pages/home/report/ReportActionCompose/SendButton.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React, {memo, useCallback, useState} from 'react';
 import {View} from 'react-native';
 import {Gesture, GestureDetector} from 'react-native-gesture-handler';
 import Icon from '@components/Icon';
@@ -10,6 +10,7 @@ import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
+import {useFocusEffect} from '@react-navigation/native';
 
 type SendButtonProps = {
     /** Whether the button is disabled */
@@ -24,9 +25,17 @@ function SendButton({isDisabled: isDisabledProp, handleSendMessage}: SendButtonP
     const styles = useThemeStyles();
     const {translate} = useLocalize();
     const {isSmallScreenWidth} = useResponsiveLayout();
+    const [uniqueRenderId, setUniqueRenderId] = useState(0);
+
     const Tap = Gesture.Tap().onEnd(() => {
         handleSendMessage();
     });
+
+    useFocusEffect(
+        useCallback(() => {
+            setUniqueRenderId((c) => c + 1);
+        }, []),
+    );
 
     return (
         <View
@@ -37,7 +46,9 @@ function SendButton({isDisabled: isDisabledProp, handleSendMessage}: SendButtonP
             <GestureDetector
                 // A new GestureDetector instance must be created when switching from a large screen to a small screen
                 // if not, the GestureDetector may not function correctly.
-                key={`send-button-${isSmallScreenWidth ? 'small-screen' : 'normal-screen'}`}
+                // The same is applicable when parent enables/disables freeze - after that we need to re-create
+                // the gesture detector. Otherwise `onEnd` will not be
+                key={`send-button-${isSmallScreenWidth ? 'small-screen' : 'normal-screen'}-${uniqueRenderId}`}
                 gesture={Tap}
             >
                 <Tooltip text={translate('common.send')}>
