01a753573f272e062da368e1e9ebb59de68a1081
Kaushik Kapadiya <kapadiyakaushik9@gmail.com>
2024-07-13 17:40:20 +0530

    Reset amount on reset button click

--- src/components/MoneyRequestAmountInput.tsx
+++ src/components/MoneyRequestAmountInput.tsx
@@ -88,6 +88,18 @@ type MoneyRequestAmountInputProps = {
      * Autogrow input container length based on the entered text.
      */
     autoGrow?: boolean;
+
+    /**
+     * Determines whether the amount should be reset.
+     */
+    shouldResetAmount?: boolean;
+
+    /**
+     * Callback function triggered when the amount is reset.
+     *
+     * @param resetValue - A boolean indicating whether the amount should be reset.
+     */
+    onResetAmount?: (resetValue: boolean) => void;
 };
 
 type Selection = {
@@ -103,7 +115,7 @@ const getNewSelection = (oldSelection: Selection, prevLength: number, newLength:
     return {start: cursorPosition, end: cursorPosition};
 };
 
-const defaultOnFormatAmount = (amount: number, currency?: string) => CurrencyUtils.convertToFrontendAmountAsString(amount, currency ?? CONST.CURRENCY.USD);
+const defaultOnFormatAmount = useCallback((amount: number, currency?: string) => CurrencyUtils.convertToFrontendAmountAsString(amount, currency?? CONST.CURRENCY.USD), []);
 
 function MoneyRequestAmountInput(
     {
@@ -123,6 +135,8 @@ function MoneyRequestAmountInput(
         hideFocusedState = true,
         shouldKeepUserInput = false,
         autoGrow = true,
+        shouldResetAmount,
+        onResetAmount,
         ...props
     }: MoneyRequestAmountInputProps,
     forwardedRef: ForwardedRef<BaseTextInputRef>,
@@ -202,10 +216,21 @@ function MoneyRequestAmountInput(
     }));
 
     useEffect(() => {
+        const frontendAmount = onFormatAmount(amount, currency);
+        setCurrentAmount(frontendAmount);
+        if (shouldResetAmount) {
+            setSelection({
+                start: frontendAmount.length,
+                end: frontendAmount.length,
+            });
+            onResetAmount?.(false);
+            return;
+        }
+
         if ((!currency || typeof amount !== 'number' || (formatAmountOnBlur && isTextInputFocused(textInput))) ?? shouldKeepUserInput) {
             return;
         }
-        const frontendAmount = onFormatAmount(amount, currency);
+
         setCurrentAmount(frontendAmount);
 
         // Only update selection if the amount prop was changed from the outside and is not the same as the current amount we just computed
@@ -216,7 +241,7 @@ function MoneyRequestAmountInput(
                 end: frontendAmount.length,
             });
         }
-    }, [amount, shouldKeepUserInput]);
+    }, [amount, shouldKeepUserInput,shouldResetAmount]);
 
     // Modifies the amount to match the decimals for changed currency.
     useEffect(() => {
