ace0862561c0b434b4462c2bc48d20f516e0f0b8
krishna2323 <belivethatkg@gmail.com>
2024-07-15 12:36:13 +0530

    add comment.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
@@ -389,6 +389,7 @@ function IOURequestStepScan({
             return;
         }
 
+        // If we have a pdf file and if it is not validated then set the pdf file for validation and return
         if (Str.isPDF(file.name ?? '') && !isPdfValidated) {
             setPdfFile(file);
             return;
