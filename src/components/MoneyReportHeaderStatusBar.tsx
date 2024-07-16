50d9e47618e9badaa8b1bb968994b4af5bfff2df
Lucien Akchoté <lucien@expensify.com>
2024-07-16 16:16:33 +0200

    fix prettier

--- src/components/MoneyReportHeaderStatusBar.tsx
+++ src/components/MoneyReportHeaderStatusBar.tsx
@@ -1,16 +1,16 @@
 import React, {useMemo} from 'react';
 import {View} from 'react-native';
 import useLocalize from '@hooks/useLocalize';
-import useThemeStyles from '@hooks/useThemeStyles';
 import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
 import * as NextStepUtils from '@libs/NextStepUtils';
-import CONST from '@src/CONST';
 import variables from '@styles/variables';
+import CONST from '@src/CONST';
 import type ReportNextStep from '@src/types/onyx/ReportNextStep';
 import Badge from './Badge';
-import RenderHTML from './RenderHTML';
 import Icon from './Icon';
 import * as Expensicons from './Icon/Expensicons';
+import RenderHTML from './RenderHTML';
 
 type MoneyReportHeaderStatusBarProps = {
     /** The next step for the report */
@@ -32,19 +32,19 @@ function MoneyReportHeaderStatusBar({nextStep}: MoneyReportHeaderStatusBarProps)
     return (
         <View style={[styles.dFlex, styles.flexRow, styles.alignItemsCenter, styles.overflowHidden, styles.w100, styles.headerStatusBarContainer]}>
             <View style={[styles.mr3]}>
-            {nextStep?.icon && nextStep.icon === HOURGLASS_ICON ? (
-                <Icon
-                    src={Expensicons.Hourglass}
-                    height={variables.iconSizeSmall}
-                    width={variables.iconSizeSmall}
-                    fill={theme.icon}
-                />
-            ) : (
-                <Badge
-                    text={translate(nextStep.title === CONST.NEXT_STEP.FINISHED ? 'iou.finished' : 'iou.nextStep')}
-                    badgeStyles={styles.ml0}
-                />
-            )}
+                {nextStep?.icon && nextStep.icon === HOURGLASS_ICON ? (
+                    <Icon
+                        src={Expensicons.Hourglass}
+                        height={variables.iconSizeSmall}
+                        width={variables.iconSizeSmall}
+                        fill={theme.icon}
+                    />
+                ) : (
+                    <Badge
+                        text={translate(nextStep.title === CONST.NEXT_STEP.FINISHED ? 'iou.finished' : 'iou.nextStep')}
+                        badgeStyles={styles.ml0}
+                    />
+                )}
             </View>
             <View style={[styles.dFlex, styles.flexRow, styles.flexShrink1]}>
                 <RenderHTML html={messageContent} />
