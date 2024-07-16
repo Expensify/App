b36baa7d3d7f64909243dc19194563704315ad26
Lucien Akchoté <lucien@expensify.com>
2024-07-16 15:34:28 +0200

    display hourglass icon when available

--- src/components/MoneyReportHeaderStatusBar.tsx
+++ src/components/MoneyReportHeaderStatusBar.tsx
@@ -7,6 +7,10 @@ import CONST from '@src/CONST';
 import type ReportNextStep from '@src/types/onyx/ReportNextStep';
 import Badge from './Badge';
 import RenderHTML from './RenderHTML';
+import variables from '@styles/variables';
+import Icon from './Icon';
+import useTheme from '@hooks/useTheme';
+import * as Expensicons from '@components/Icon/Expensicons';
 
 type MoneyReportHeaderStatusBarProps = {
     /** The next step for the report */
@@ -16,19 +20,31 @@ type MoneyReportHeaderStatusBarProps = {
 function MoneyReportHeaderStatusBar({nextStep}: MoneyReportHeaderStatusBarProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
+    const theme = useTheme();
 
     const messageContent = useMemo(() => {
         const messageArray = nextStep.message;
         return NextStepUtils.parseMessage(messageArray);
     }, [nextStep.message]);
 
+    const HOURGLASS_ICON = 'hourglass';
+
     return (
         <View style={[styles.dFlex, styles.flexRow, styles.alignItemsCenter, styles.overflowHidden, styles.w100, styles.headerStatusBarContainer]}>
             <View style={[styles.mr3]}>
+            {nextStep?.icon && nextStep.icon === HOURGLASS_ICON ? (
+                <Icon
+                    src={Expensicons.Hourglass}
+                    height={variables.iconSizeSmall}
+                    width={variables.iconSizeSmall}
+                    fill={theme.icon}
+                />
+            ) : (
                 <Badge
                     text={translate(nextStep.title === CONST.NEXT_STEP.FINISHED ? 'iou.finished' : 'iou.nextStep')}
                     badgeStyles={styles.ml0}
                 />
+            )}
             </View>
             <View style={[styles.dFlex, styles.flexRow, styles.flexShrink1]}>
                 <RenderHTML html={messageContent} />
