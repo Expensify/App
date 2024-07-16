44dccfcfd26e3644a32be6a36d193fea73f8db61
Mateusz Titz <titzmateusz@gmail.com>
2024-07-16 15:20:42 +0200

    Add AdvancedSearchFilters component with sample static filters

---
+++ src/pages/Search/AdvancedSearchFilters.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View} from 'react-native';
+import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
+import useLocalize from '@hooks/useLocalize';
+import useSingleExecution from '@hooks/useSingleExecution';
+import Navigation from '@libs/Navigation/Navigation';
+import type {Route} from '@src/ROUTES';
+
+const advancedFilters = [
+    {
+        fieldName: 'type',
+        description: 'common.type' as const,
+        route: '/search/filter/type' as Route,
+    },
+    {
+        fieldName: 'date',
+        description: 'common.date' as const,
+        route: '/search/filter/date' as Route,
+    },
+];
+
+function getFilterDisplayTitle(filters: Record<string, string>, fieldName: string) {
+    // This is temporary because the full parsing of search query is not yet done
+    // The actual value will be `filters[fieldName].value` to be updated later
+    return fieldName;
+}
+
+function AdvancedSearchFilters() {
+    const {singleExecution} = useSingleExecution();
+    const {translate} = useLocalize();
+
+    return (
+        <View style={[]}>
+            {advancedFilters.map((item) => {
+                const filterValue = getFilterDisplayTitle({}, item.fieldName);
+                const onPress = singleExecution(() => Navigation.navigate(item.route));
+
+                return (
+                    <MenuItemWithTopDescription
+                        title={filterValue}
+                        description={translate(item.description)}
+                        shouldShowRightIcon
+                        onPress={onPress}
+                    />
+                );
+            })}
+        </View>
+    );
+}
+
+AdvancedSearchFilters.displayName = 'AdvancedSearchFilters';
+
+export default AdvancedSearchFilters;
--- src/pages/Search/SearchAdvancedFiltersPage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage.tsx
@@ -1,22 +1,14 @@
-import type {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Text, View} from 'react-native';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
-import type {AuthScreensParamList} from '@libs/Navigation/types';
-import type SCREENS from '@src/SCREENS';
+import AdvancedSearchFilters from './AdvancedSearchFilters';
 
-type SearchPageProps = StackScreenProps<AuthScreensParamList, typeof SCREENS.SEARCH.CENTRAL_PANE>;
-
-// Todo finish this page, fix route params and add Form provider
 function SearchAdvancedFiltersPage() {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
-
-    // add advanced filters
 
     return (
         <ScreenWrapper
@@ -26,9 +18,7 @@ function SearchAdvancedFiltersPage() {
         >
             <FullPageNotFoundView shouldShow={false}>
                 <HeaderWithBackButton title={translate('search.filtersHeader')} />
-                <View style={[styles.flex1]}>
-                    <Text>Search advanced filters</Text>
-                </View>
+                <AdvancedSearchFilters />
             </FullPageNotFoundView>
         </ScreenWrapper>
     );
