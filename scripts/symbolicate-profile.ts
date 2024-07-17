59c72216e82516fc5ec5942392f28dc79a4d384d
neil-marcellini <neil@expensify.com>
2024-07-16 17:14:36 -0700

    Update other ghToken arg prompt

--- scripts/symbolicate-profile.ts
+++ scripts/symbolicate-profile.ts
@@ -32,7 +32,7 @@ if (Object.keys(argsMap).length === 0 || argsMap.help !== undefined) {
     Logger.log('Options:');
     Logger.log('  --profile=<filename>          The .cpuprofile file to symbolicate');
     Logger.log('  --platform=<ios|android>      The platform for which the source map was uploaded');
-    Logger.log('  --gh-token                    Token to use for requests send to the GitHub API. By default tries to pick up from the environment variable GITHUB_TOKEN');
+    Logger.log('  --ghToken                     Token to use for requests send to the GitHub API. By default tries to pick up from the environment variable GITHUB_TOKEN');
     Logger.log('  --help                        Display this help message');
     process.exit(0);
 }
