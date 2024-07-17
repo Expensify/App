0815754c9fc704ad199c0758b94628fcfcddfb98
Rodrigo Lino da Costa <5201282+rlinoz@users.noreply.github.com>
2024-07-17 16:12:03 -0300

    Update src/languages/en.ts
    Co-authored-by: Carlos Martins <cmartins@expensify.com>

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -3530,7 +3530,7 @@ export default {
             unhold: 'Unhold',
             noOptionsAvailable: 'No options available for the selected group of expenses.',
         },
-        offlinePrompt: 'You can’t take this action right now.',
+        offlinePrompt: 'You can't take this action right now.',
     },
     genericErrorPage: {
         title: 'Uh-oh, something went wrong!',
