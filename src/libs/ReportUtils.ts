3624a4a6b6f76440f7d5cbd558c8d47ef5c27599
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-07-19 15:47:10 +0100

    fix: multiple chat reports picked for onboarding

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -7089,8 +7089,8 @@ function shouldShowMerchantColumn(transactions: Transaction[]) {
  */
 function isChatUsedForOnboarding(optionOrReport: OnyxEntry<Report> | OptionData): boolean {
     // onboarding can be an array for old accounts and accounts created from olddot
-    if (!Array.isArray(onboarding) && onboarding?.chatReportID === optionOrReport?.reportID) {
-        return true;
+    if (onboarding && !Array.isArray(onboarding) && onboarding.chatReportID && optionOrReport?.chatReportID) {
+        return onboarding.chatReportID === optionOrReport.chatReportID;
     }
 
     return AccountUtils.isAccountIDOddNumber(currentUserAccountID ?? -1)
