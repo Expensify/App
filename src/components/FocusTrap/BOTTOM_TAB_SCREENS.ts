1e49eb04aa855a2c6b3ca4fac38590be2a1af43c
Tsaqif <tsaiinkwa@yahoo.com>
2024-07-19 10:00:51 +0700

    Fix blud border appears on workspace switcher button after login
    Signed-off-by: Tsaqif <tsaiinkwa@yahoo.com>

--- src/components/FocusTrap/BOTTOM_TAB_SCREENS.ts
+++ src/components/FocusTrap/BOTTOM_TAB_SCREENS.ts
@@ -1,6 +1,6 @@
-import type {BottomTabName} from '@libs/Navigation/types';
+import NAVIGATORS from '@src/NAVIGATORS';
 import SCREENS from '@src/SCREENS';
 
-const BOTTOM_TAB_SCREENS: BottomTabName[] = [SCREENS.HOME, SCREENS.SETTINGS.ROOT];
+const BOTTOM_TAB_SCREENS = [SCREENS.HOME, SCREENS.SETTINGS.ROOT, NAVIGATORS.BOTTOM_TAB_NAVIGATOR];
 
 export default BOTTOM_TAB_SCREENS;
