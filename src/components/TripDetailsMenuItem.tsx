28a443c522a7ac690a480cc6e00ad1f142ad5e63
cdOut <88325488+cdOut@users.noreply.github.com>
2024-07-19 21:50:55 +0200

    add dummy data to wip component to omit typescript errors without ignore

--- src/components/TripDetailsMenuItem.tsx
+++ src/components/TripDetailsMenuItem.tsx
@@ -11,15 +11,10 @@ import Text from './Text';
 /**
  * This component is going to be used in TravelDetails when we resolve
  * the backends and send over detailed info about reservations.
- * 
- * Due to it not being currently used and there being no data passed to this
- * as I am unaware of the data structure, I have disabled eslint rules
- * for this component.
  *
  * Please refer to the conversation below for more details:
  * https://swmansion.slack.com/archives/C05S5EV2JTX/p1721062807563259
  */
-/* eslint-disable */
 function TripDetailsMenuItem() {
     const theme = useTheme();
     const styles = useThemeStyles();
@@ -37,34 +32,34 @@ function TripDetailsMenuItem() {
                     />
                 </View>
                 <View>
-                    <Text style={styles.textBold}>{}</Text>
-                    <Text style={styles.textLabelSupporting}>{}</Text>
+                    <Text style={styles.textBold}>{'data'}</Text>
+                    <Text style={styles.textLabelSupporting}>{'data'}</Text>
                 </View>
             </View>
 
             <View style={[styles.tripDetailsContainer, styles.pt3]}>
-                <Text style={[styles.textBold, styles.textLarge]}>{}</Text>
+                <Text style={[styles.textBold, styles.textLarge]}>{'data'}</Text>
                 <View style={[styles.tripDetailsContainer, styles.flex1, styles.justifyContentBetween]}>
                     <View style={styles.tripDetailsLine} />
                     <View style={styles.tripDetailsLineTextContainer}>
-                        <Text style={styles.textLabelSupporting}>{}</Text>
-                        <Text style={[styles.textLabelSupporting, styles.tripDetailsLineSubtext]}>{}</Text>
+                        <Text style={styles.textLabelSupporting}>{'data'}</Text>
+                        <Text style={[styles.textLabelSupporting, styles.tripDetailsLineSubtext]}>{'data'}</Text>
                     </View>
                     <View style={styles.tripDetailsLine} />
                 </View>
-                <Text style={[styles.textBold, styles.textLarge]}>{}</Text>
+                <Text style={[styles.textBold, styles.textLarge]}>{'data'}</Text>
             </View>
 
             <View style={[styles.tripDetailsContainer, styles.pt3]}>
                 <View style={{flex: 1, paddingRight: 24}}>
-                    <Text style={[styles.labelStrong, styles.pb1]}>{}</Text>
-                    <Text style={[styles.textLabelSupporting, styles.pb1]}>{}</Text>
-                    <Text style={styles.textLabelSupporting}>{}</Text>
+                    <Text style={[styles.labelStrong, styles.pb1]}>{'data'}</Text>
+                    <Text style={[styles.textLabelSupporting, styles.pb1]}>{'data'}</Text>
+                    <Text style={styles.textLabelSupporting}>{'data'}</Text>
                 </View>
                 <View style={{flex: 1, paddingLeft: 24, alignItems: 'flex-end'}}>
-                    <Text style={[styles.labelStrong, styles.pb1]}>{}</Text>
-                    <Text style={[styles.textLabelSupporting, styles.textAlignRight, styles.pb1]}>{}</Text>
-                    <Text style={[styles.textLabelSupporting, styles.textAlignRight]}>{}</Text>
+                    <Text style={[styles.labelStrong, styles.pb1]}>{'data'}</Text>
+                    <Text style={[styles.textLabelSupporting, styles.textAlignRight, styles.pb1]}>{'data'}</Text>
+                    <Text style={[styles.textLabelSupporting, styles.textAlignRight]}>{'data'}</Text>
                 </View>
             </View>
         </View>
