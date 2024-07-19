38e45de114be2f2ab9602b33e2320cd0142086bc
Michał Skałka <michal.skalka@swmansion.com>
2024-07-19 16:37:21 +0200

    Fix pasting performance

--- src/hooks/useHtmlPaste/index.ts
+++ src/hooks/useHtmlPaste/index.ts
@@ -7,6 +7,26 @@ const insertByCommand = (text: string) => {
     document.execCommand('insertText', false, text);
 };
 
+const insertAtCaret = (target: HTMLElement, text: string) => {
+    const selection = window.getSelection();
+    if (selection?.rangeCount) {
+        const range = selection.getRangeAt(0);
+        range.deleteContents();
+        const node = document.createTextNode(text);
+        range.insertNode(node);
+
+        // Move caret to the end of the newly inserted text node.
+        range.setStart(node, node.length);
+        range.setEnd(node, node.length);
+        selection.setBaseAndExtent(range.startContainer, range.startOffset, range.endContainer, range.endOffset);
+
+        // Dispatch input event to trigger Markdown Input to parse the new text
+        target.dispatchEvent(new Event('input', {bubbles: true}));
+    } else {
+        insertByCommand(text);
+    }
+};
+
 const useHtmlPaste: UseHtmlPaste = (textInputRef, preHtmlPasteCallback, removeListenerOnScreenBlur = false) => {
     const navigation = useNavigation();
 
@@ -17,14 +37,10 @@ const useHtmlPaste: UseHtmlPaste = (textInputRef, preHtmlPasteCallback, removeLi
     const paste = useCallback((text: string) => {
         try {
             const textInputHTMLElement = textInputRef.current as HTMLElement;
-            insertByCommand(text);
             if (textInputHTMLElement?.hasAttribute('contenteditable')) {
-                return;
-            }
-
-            if (!textInputRef.current?.isFocused()) {
-                textInputRef.current?.focus();
-                return;
+                insertAtCaret(textInputHTMLElement, text);
+            } else {
+                insertByCommand(text);
             }
 
             // Pointer will go out of sight when a large paragraph is pasted on the web. Refocusing the input keeps the cursor in view.
