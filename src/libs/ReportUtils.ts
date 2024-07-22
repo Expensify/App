f29352cedec620aeb09acdafdc05bbc97797a7d9
Github <olimpia.zurek@gmail.com>
2024-07-22 15:11:27 +0200

    fix: improve getDisplayNameForParticipant performance

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -1859,6 +1859,9 @@ function getPersonalDetailsForAccountID(accountID: number): Partial<PersonalDeta
 }
 
 const hiddenTranslation = Localize.translateLocal('common.hidden');
+
+const phoneNumberCache: {[key: string]: string} = {};
+
 /**
  * Get the displayName for a single report participant.
  */
@@ -1868,8 +1871,18 @@ function getDisplayNameForParticipant(accountID?: number, shouldUseShortForm = f
     }
 
     const personalDetails = getPersonalDetailsForAccountID(accountID);
-    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    const formattedLogin = LocalePhoneNumber.formatPhoneNumber(personalDetails.login || '');
+    if (!personalDetails) {
+        return '';
+    }
+
+    // Check if the phone number is already cached
+    let formattedLogin = phoneNumberCache[personalDetails.login || ''];
+    if (!formattedLogin) {
+        formattedLogin = LocalePhoneNumber.formatPhoneNumber(personalDetails.login || '');
+        // Store the formatted phone number in the cache
+        phoneNumberCache[personalDetails.login || ''] = formattedLogin;
+    }
+
     // This is to check if account is an invite/optimistically created one
     // and prevent from falling back to 'Hidden', so a correct value is shown
     // when searching for a new user
