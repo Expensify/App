e96a0a09585ba2a998db6e004ca17b1750c83e07
BrtqKr <bartlomiej.krason@swmansion.com>
2024-07-22 10:34:29 +0200

    prettier

--- src/components/SelectionList/types.ts
+++ src/components/SelectionList/types.ts
@@ -1,12 +1,12 @@
-import type { MutableRefObject, ReactElement, ReactNode } from 'react';
-import type { GestureResponderEvent, InputModeOptions, LayoutChangeEvent, SectionListData, StyleProp, TextInput, TextStyle, ViewStyle } from 'react-native';
-import type { BrickRoad } from '@libs/WorkspacesSettingsUtils';
+import type {MutableRefObject, ReactElement, ReactNode} from 'react';
+import type {GestureResponderEvent, InputModeOptions, LayoutChangeEvent, SectionListData, StyleProp, TextInput, TextStyle, ViewStyle} from 'react-native';
+import type {BrickRoad} from '@libs/WorkspacesSettingsUtils';
 // eslint-disable-next-line no-restricted-imports
 import type CursorStyles from '@styles/utils/cursor/types';
 import type CONST from '@src/CONST';
-import type { Errors, Icon, PendingAction } from '@src/types/onyx/OnyxCommon';
-import type { SearchAccountDetails, SearchReport, SearchTransaction } from '@src/types/onyx/SearchResults';
-import type { ReceiptErrors } from '@src/types/onyx/Transaction';
+import type {Errors, Icon, PendingAction} from '@src/types/onyx/OnyxCommon';
+import type {SearchAccountDetails, SearchReport, SearchTransaction} from '@src/types/onyx/SearchResults';
+import type {ReceiptErrors} from '@src/types/onyx/Transaction';
 import type ChildrenProps from '@src/types/utils/ChildrenProps';
 import type IconAsset from '@src/types/utils/IconAsset';
 import type InviteMemberListItem from './InviteMemberListItem';
@@ -15,7 +15,6 @@ import type ReportListItem from './Search/ReportListItem';
 import type TransactionListItem from './Search/TransactionListItem';
 import type TableListItem from './TableListItem';
 import type UserListItem from './UserListItem';
-
 
 type TRightHandSideComponent<TItem extends ListItem> = {
     /** Component to display on the right side */
