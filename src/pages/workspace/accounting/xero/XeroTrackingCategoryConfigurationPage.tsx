e880a1ddc4e52a7603f6dd22a5c9c8e7a0114693
war-in <war-in@users.noreply.github.com>
2024-07-22 16:43:24 +0200

    use default tracking categories options value

--- src/pages/workspace/accounting/xero/XeroTrackingCategoryConfigurationPage.tsx
+++ src/pages/workspace/accounting/xero/XeroTrackingCategoryConfigurationPage.tsx
@@ -32,7 +32,7 @@ function XeroTrackingCategoryConfigurationPage({policy}: WithPolicyProps) {
             id: category.id,
             description: translate('workspace.xero.mapTrackingCategoryTo', {categoryName: category.name}) as TranslationPaths,
             onPress: () => Navigation.navigate(ROUTES.POLICY_ACCOUNTING_XERO_TRACKING_CATEGORIES_MAP.getRoute(policyID, category.id, category.name)),
-            title: translate(`workspace.xero.trackingCategoriesOptions.${category.value.toLowerCase()}` as TranslationPaths),
+            title: translate(`workspace.xero.trackingCategoriesOptions.${!!category.value ? category.value.toLowerCase() : 'default'}` as TranslationPaths),
         }));
     }, [translate, policy, policyID]);
 
