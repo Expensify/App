c3671691a41d1bf34fa1c5c80ba064a5f890f736
jaydamani <jaydamani567@gmail.com>
2024-07-24 05:21:31 +0530

    lint: code cleaup

--- tests/unit/SubscriptionUtilsTest.ts
+++ tests/unit/SubscriptionUtilsTest.ts
@@ -13,7 +13,7 @@ const billingGraceEndPeriod: BillingGraceEndPeriod = {
     value: 0,
 };
 
-const GRACE_PERIOD_DATE = new Date().getTime() + 1000*3600;
+const GRACE_PERIOD_DATE = new Date().getTime() + 1000 * 3600;
 const GRACE_PERIOD_DATE_OVERDUE = new Date().getTime() - 1000;
 
 const AMOUNT_OWED = 100;
