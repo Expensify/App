51eb4c444f556261fffccc1829c05e0327a7a2fe
Blazej Kustra <blazej.kustra@swmansion.com>
2024-07-23 13:20:31 +0200

    Add more descriptive comment

--- src/components/FocusTrap/WIDE_LAYOUT_INACTIVE_SCREENS.ts
+++ src/components/FocusTrap/WIDE_LAYOUT_INACTIVE_SCREENS.ts
@@ -1,7 +1,10 @@
 import NAVIGATORS from '@src/NAVIGATORS';
 import SCREENS from '@src/SCREENS';
 
-// Screens that should not have active focus trap when rendered on wide screen in order to allow Tab navigation in LHP and RHP
+/**
+ * Screens displayed in the BottomTab and CentralPane displayed side by side that should not have active
+ * focus trap when rendered on a wide screen to allow navigation between them using the keyboard
+ */
 const WIDE_LAYOUT_INACTIVE_SCREENS: string[] = [
     NAVIGATORS.BOTTOM_TAB_NAVIGATOR,
     SCREENS.HOME,
