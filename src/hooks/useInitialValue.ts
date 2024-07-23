df0850a04f5b688fadb999c5be5dcf6539308365
kirillzyusko <kiryl@margelo.io>
2024-07-23 18:31:20 +0200

    Revert "check: add intentionally uncompilable code to see whether action works or not"
    This reverts commit 122c876e9c72e33e76cb761a43b21440940fb9ff.

--- src/hooks/useInitialValue.ts
+++ src/hooks/useInitialValue.ts
@@ -1,17 +1,9 @@
-import {useEffect, useState} from 'react';
-import {useSharedValue} from 'react-native-reanimated';
+import {useState} from 'react';
 
 // In some places we set initial value on first render, but we don't want to re-run the function
 // This hook will memoize the initial value and return that without setter, so it's never changed
 // https://github.com/Expensify/App/pull/29643#issuecomment-1765894078
 export default function useInitialValue<T>(initialStateFunc: () => T) {
-    const a = useSharedValue(0);
     const [initialValue] = useState(initialStateFunc);
-
-    useEffect(() => {
-        // eslint-disable-next-line react-compiler/react-compiler
-        a.value += 1;
-    }, []);
-
     return initialValue;
 }
