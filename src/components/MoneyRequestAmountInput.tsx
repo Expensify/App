de4ebbfc0f9d8ab0bdd1bb3c49e1f116018961b7
Kaushik Kapadiya <kapadiyakaushik9@gmail.com>
2024-07-23 19:30:03 +0530

    Reset amount on reset button click

--- src/components/MoneyRequestAmountInput.tsx
+++ src/components/MoneyRequestAmountInput.tsx
@@ -247,7 +247,7 @@ function MoneyRequestAmountInput(
 
         // we want to re-initialize the state only when the amount changes
         // eslint-disable-next-line react-compiler/react-compiler, react-hooks/exhaustive-deps
-    }, [amount, formatAmountOnBlur, shouldKeepUserInput, onFormatAmount, shouldResetAmount, onResetAmount]);
+    }, [amount, shouldKeepUserInput]);
 
     // Modifies the amount to match the decimals for changed currency.
     useEffect(() => {
