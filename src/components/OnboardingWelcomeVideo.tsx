d81c68eb39e461dea0a4f411d17d86af6b5f12ef
daledah <daleda261@outlook.com>
2024-07-24 10:58:43 +0700

    fix: revert OnboardingWelcomeVideo

--- src/components/OnboardingWelcomeVideo.tsx
+++ src/components/OnboardingWelcomeVideo.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import {useOnyx} from 'react-native-onyx';
 import useLocalize from '@hooks/useLocalize';
-import useWindowDimensions from '@hooks/useWindowDimensions';
-import Navigation from '@libs/Navigation/Navigation';
 import CONST from '@src/CONST';
-import ONYXKEYS from '@src/ONYXKEYS';
-import ROUTES from '@src/ROUTES';
 import FeatureTrainingModal from './FeatureTrainingModal';
 
 function OnboardingWelcomeVideo() {
     const {translate} = useLocalize();
-    const [onboardingPurposeSelected] = useOnyx(ONYXKEYS.ONBOARDING_PURPOSE_SELECTED);
-    const {isSmallScreenWidth} = useWindowDimensions();
 
     return (
         <FeatureTrainingModal
@@ -19,12 +12,6 @@ function OnboardingWelcomeVideo() {
             description={translate('onboarding.welcomeVideo.description')}
             confirmText={translate('onboarding.getStarted')}
             videoURL={CONST.WELCOME_VIDEO_URL}
-            onConfirm={() => {
-                if (onboardingPurposeSelected !== CONST.ONBOARDING_CHOICES.EMPLOYER || !isSmallScreenWidth) {
-                    return;
-                }
-                Navigation.goBack(ROUTES.HOME, true, true);
-            }}
         />
     );
 }
