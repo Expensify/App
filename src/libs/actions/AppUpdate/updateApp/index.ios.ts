5dc24eca0656bb4ff60eed3b8030ec582a27e67d
staszekscp <mariusz.stanisz@swmansion.com>
2024-07-25 14:07:28 +0200

    fix list for updating app in Hybrid

--- src/libs/actions/AppUpdate/updateApp/index.ios.ts
+++ src/libs/actions/AppUpdate/updateApp/index.ios.ts
@@ -1,6 +1,7 @@
 import {Linking} from 'react-native';
 import CONST from '@src/CONST';
+import { NativeModules } from 'react-native';
 
 export default function updateApp() {
-    Linking.openURL(CONST.APP_DOWNLOAD_LINKS.IOS);
+    Linking.openURL(NativeModules.HybridAppModule ? CONST.APP_DOWNLOAD_LINKS.OLD_DOT_IOS : CONST.APP_DOWNLOAD_LINKS.IOS);
 }
