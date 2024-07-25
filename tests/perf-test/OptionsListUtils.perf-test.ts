aa78d9e5eb9ab86a60c789d439104d74b03b3b00
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-07-25 12:35:53 +0200

    Revert "mock timing on perf tests"
    This reverts commit 04a99966b576c1c017def0b34f6332723409a842.

--- tests/perf-test/OptionsListUtils.perf-test.ts
+++ tests/perf-test/OptionsListUtils.perf-test.ts
@@ -73,13 +73,6 @@ jest.mock('@react-navigation/native', () => {
     };
 });
 
-jest.mock('@src/libs/actions/Timing', () => {
-    return {
-        start: jest.fn(),
-        end: jest.fn(),
-    };
-});
-
 const options = OptionsListUtils.createOptionList(personalDetails, reports);
 
 /* GetOption is the private function and is never called directly, we are testing the functions which call getOption with different params */
