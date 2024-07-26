a52a9975698ca43d3ad8c06bead7b4700f8280c8
jacobkim9881 <jacobkim9881@gmail.com>
2024-07-27 03:03:59 +0900

    canceled null check

--- src/components/PopoverMenu.tsx
+++ src/components/PopoverMenu.tsx
@@ -61,7 +61,7 @@ type PopoverMenuProps = Partial<PopoverModalProps> & {
     anchorPosition: AnchorPosition;
 
     /** Ref of the anchor */
-    anchorRef: RefObject<View | HTMLDivElement | null>;
+    anchorRef: RefObject<View | HTMLDivElement>;
 
     /** Where the popover should be positioned relative to the anchor points. */
     anchorAlignment?: AnchorAlignment;
