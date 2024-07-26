a23e4fe0a5b622a9ab1fdc61811d1865d0f541fb
Blazej Kustra <blazej.kustra@swmansion.com>
2024-07-26 08:39:19 +0200

    Update the comments according to feedback

--- src/types/onyx/ApprovalWorkflow.ts
+++ src/types/onyx/ApprovalWorkflow.ts
@@ -25,18 +25,21 @@ type Approver = {
     displayName?: string;
 
     /**
-     * Is this approver in more than one workflow
+     * Is this user used as an approver in more than one workflow (used to show a warning)
      */
     isInMultipleWorkflows: boolean;
 
     /**
-     * Is this approver in a circular reference
+     * Is this approver in a circular reference (approver forwards to themselves, or a cycle of forwards)
+     *
+     * example: A -> A (self forwards)
+     * example: A -> B -> C -> A (cycle)
      */
     isCircularReference?: boolean;
 };
 
 /**
- *
+ * Member in the approval workflow
  */
 type Member = {
     /**
@@ -65,12 +68,14 @@ type ApprovalWorkflow = {
     members: Member[];
 
     /**
-     * List of approvers in the workflow
+     * List of approvers in the workflow (the order of approvers in this array is important)
+     *
+     * The first approver in the array is the first approver in the workflow, next approver is the one they forward to, etc.
      */
     approvers: Approver[];
 
     /**
-     * Is this the default workflow
+     * Is this the default workflow for the policy (first approver of this workflow is the same as the policy's default approver)
      */
     isDefault: boolean;
 
