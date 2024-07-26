d65d7f990339043f8c7d3c7e6500b5e0944b3ad8
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-07-26 13:34:42 +0100

    fix: modal stack navigators import paths

--- src/libs/Navigation/AppNavigator/ModalStackNavigators/index.tsx
+++ src/libs/Navigation/AppNavigator/ModalStackNavigators/index.tsx
@@ -516,9 +516,9 @@ const RestrictedActionModalStackNavigator = createModalStackNavigator<SearchRepo
 });
 
 const DebugModalStackNavigator = createModalStackNavigator<DebugParamList>({
-    [SCREENS.DEBUG.REPORT]: () => require<ReactComponentModule>('../../../../pages/debug/Report/DebugReportPage').default,
-    [SCREENS.DEBUG.REPORT_ACTION]: () => require<ReactComponentModule>('../../../../pages/debug/Report/DebugReportActionPage').default,
-    [SCREENS.DEBUG.REPORT_ACTION_CREATE]: () => require<ReactComponentModule>('../../../../pages/debug/Report/DebugReportActionCreatePage').default,
+    [SCREENS.DEBUG.REPORT]: () => require<ReactComponentModule>('../../../../pages/Debug/Report/DebugReportPage').default,
+    [SCREENS.DEBUG.REPORT_ACTION]: () => require<ReactComponentModule>('../../../../pages/Debug/Report/DebugReportActionPage').default,
+    [SCREENS.DEBUG.REPORT_ACTION_CREATE]: () => require<ReactComponentModule>('../../../../pages/Debug/Report/DebugReportActionCreatePage').default,
 });
 
 export {
