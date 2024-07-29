4008f07965faf09006edc3e910b63a0a7d69f50b
Peter Velkov <kidroca@gmail.com>
2024-07-29 09:19:08 +0300

    🐛 fix(components/AnchorForAttachmentsOnly): add sourceID check to download logic
    Update the download logic to include a check for the presence of
    sourceID alongside existing conditions for isDownloading and
    isOffline, preventing potential errors when attempting to download
    attachments without a valid source ID.

--- src/components/AnchorForAttachmentsOnly/BaseAnchorForAttachmentsOnly.tsx
+++ src/components/AnchorForAttachmentsOnly/BaseAnchorForAttachmentsOnly.tsx
@@ -45,7 +45,7 @@ function BaseAnchorForAttachmentsOnly({style, source = '', displayName = '', dow
                 <PressableWithoutFeedback
                     style={[style, isOffline && styles.cursorDefault]}
                     onPress={() => {
-                        if (isDownloading || isOffline) {
+                        if (isDownloading || isOffline || !sourceID) {
                             return;
                         }
                         Download.setDownload(sourceID, true);
