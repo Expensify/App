04f18c4cc8011ad103a1a1a5cd23f7c85a88e9e9
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-07-30 13:28:04 +0200

    typo

--- src/types/utils/TupleOperations.ts
+++ src/types/utils/TupleOperations.ts
@@ -16,7 +16,7 @@ type TupleSplit<T, N extends number, O extends any[] = []> = O['length'] extends
 
 /**
  * Get the first `N` elements of a tuple. If `N` is not provided, it defaults to the length of the tuple.
- * `number extends N ? PositiveInfinity : N` is a hack to accomodate for rest parameters.
+ * `number extends N ? PositiveInfinity : N` is a hack to accommodate for rest parameters.
  */
 type TakeFirst<T extends any[], N extends number = T['length']> = TupleSplit<NonPartial<T>, number extends N ? PositiveInfinity : N>[0];
 
