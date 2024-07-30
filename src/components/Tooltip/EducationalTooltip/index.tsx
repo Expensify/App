2bd231707c457016750080f08e620ca340e1d86e
tienifr <christianwen18@gmail.com>
2024-07-30 16:42:28 +0700

    hide tooltip on navigating

--- src/components/Tooltip/EducationalTooltip/index.tsx
+++ src/components/Tooltip/EducationalTooltip/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
 import type {TooltipExtendedProps} from '@components/Tooltip/types';
 import BaseEducationalTooltip from './BaseEducationalTooltip';
 
 function EducationalTooltip({shouldRender = true, children, ...props}: TooltipExtendedProps) {
-    if (!shouldRender) {
+    const navigation = useNavigation();
+    const [shouldHide, setShouldHide] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('blur', () => {
+            setShouldHide(true);
+        });
+        return unsubscribe;
+    }, [navigation]);
+
+    if (!shouldRender || shouldHide) {
         return children;
     }
 
