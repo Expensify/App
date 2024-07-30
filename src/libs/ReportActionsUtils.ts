de372e1af1227dbfb4d0ab9b6b85c2e0963753c2
Adam Horodyski <ad.horodyski@gmail.com>
2024-07-30 14:52:57 +0200

    fix: improve perf of isActionOfType by limiting calls to the includes

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -189,7 +189,14 @@ function isActionOfType<T extends ReportActionName[]>(
 ): action is {
     [K in keyof T]: ReportAction<T[K]>;
 }[number] {
-    return actionNames.includes(action?.actionName as T[number]);
+    const actionName = action?.actionName as T[number];
+
+    // This is purely a performance optimization to limit the 'includes()' calls on Hermes
+    if (actionNames.length === 1) {
+        return actionNames[0] === actionName;
+    }
+
+    return actionNames.includes(actionName);
 }
 
 function getOriginalMessage<T extends ReportActionName>(reportAction: OnyxInputOrEntry<ReportAction<T>>): OriginalMessage<T> | undefined {
