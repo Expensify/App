83c085c5c90c3b09545d5924fc1698398fb2f2be
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2024-07-30 21:18:57 +0200

    fix TupleSplit number as length

--- src/types/utils/TupleOperations.ts
+++ src/types/utils/TupleOperations.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type {PositiveInfinity} from 'type-fest';
 import type NonPartial from './NonPartial';
 
 // Based on: https://stackoverflow.com/questions/67605122/obtain-a-slice-of-a-typescript-parameters-tuple
 /**
  * Split a tuple into two parts: the first `N` elements and the rest.
  */
-type TupleSplit<T, N extends number, O extends any[] = []> = O['length'] extends N
+type TupleSplit<T, N extends number, O extends any[] = []> = number extends N
+    ? [T, O]
+    : O['length'] extends N
     ? [O, T]
     : T extends [infer F, ...infer R]
     ? TupleSplit<[...R], N, [...O, F]>
@@ -18,6 +19,6 @@ type TupleSplit<T, N extends number, O extends any[] = []> = O['length'] extends
  * Get the first `N` elements of a tuple. If `N` is not provided, it defaults to the length of the tuple.
  * `number extends N ? PositiveInfinity : N` is a hack to accommodate for rest parameters.
  */
-type TakeFirst<T extends any[], N extends number = T['length']> = TupleSplit<NonPartial<T>, number extends N ? PositiveInfinity : N>[0];
+type TakeFirst<T extends any[], N extends number = T['length']> = TupleSplit<NonPartial<T>, N>[0];
 
 export type {TupleSplit, TakeFirst};
