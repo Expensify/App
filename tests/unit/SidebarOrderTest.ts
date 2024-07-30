d11779abc19baf8414ed6f2c6117adffd8156ec6
ShridharGoel <35566748+ShridharGoel@users.noreply.github.com>
2024-07-30 17:12:18 +0530

    Update

--- tests/unit/SidebarOrderTest.ts
+++ tests/unit/SidebarOrderTest.ts
@@ -10,7 +10,7 @@ import * as LHNTestUtils from '../utils/LHNTestUtils';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
 import wrapOnyxWithWaitForBatchedUpdates from '../utils/wrapOnyxWithWaitForBatchedUpdates';
 
-// Be sure to include the mocked Permissions and Expensicons libraries as well as the usePermissions hook or else the beta tests won't work
+// Be sure to include the mocked Permissions and Expensicons libraries or else the beta tests won't work
 jest.mock('@libs/Permissions');
 jest.mock('@components/Icon/Expensicons');
 jest.mock('@src/hooks/useActiveWorkspaceFromNavigationState');
