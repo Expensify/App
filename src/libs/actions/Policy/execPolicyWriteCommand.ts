f5c8e0783ebfd99aa5ba453fbaed6d61fc8d0ce2
dominictb <tb-dominic@outlook.com>
2024-07-30 09:19:45 +0700

    fix: test new approach

--- src/libs/actions/Policy/execPolicyWriteCommand.ts
+++ src/libs/actions/Policy/execPolicyWriteCommand.ts
@@ -17,13 +17,37 @@ Onyx.connect({
 
 function execPolicyWriteCommand<TCommand extends WriteCommand>(commandName: TCommand, params: ApiRequestCommandParameters[TCommand], onyxData: OnyxData = {}, policyID?: string) {
     const extendedParams: (typeof params & {lastModified?: string | number}) | null = params && {...params};
-    if (NetworkStore.isOffline() && extendedParams) {
-        const respectivePolicyID = policyID ?? (params as {policyID?: string})?.policyID;
-        const lastModified = allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${respectivePolicyID}`]?.lastModified ?? '1';
-        extendedParams.lastModified = lastModified;
+    const currentTs = new Date().getTime().toString();
+    const respectivePolicyID = policyID ?? (params as {policyID?: string})?.policyID;
+    const currentPolicyLastModified = allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${respectivePolicyID}`]?.lastModified;
+
+    const optimisticData = onyxData.optimisticData ?? [];
+    const failureData = onyxData.failureData ?? [];
+
+
+    if(currentPolicyLastModified) {
+        extendedParams.lastModified = currentTs;
+        optimisticData.push({
+            key: `${ONYXKEYS.COLLECTION.POLICY}${respectivePolicyID}`,
+            onyxMethod: Onyx.METHOD.MERGE,
+            value: {
+                lastModified: currentTs.toString()
+            }
+        })
+        failureData.push({
+            key: `${ONYXKEYS.COLLECTION.POLICY}${respectivePolicyID}`,
+            onyxMethod: Onyx.METHOD.MERGE,
+            value: {
+                lastModified: currentPolicyLastModified,
+            }
+        })
     }
 
-    API.write(commandName, extendedParams, onyxData);
+    API.write(commandName, extendedParams, {
+        ...onyxData,
+        optimisticData,
+        failureData,
+    });
 }
 
 // eslint-disable-next-line import/prefer-default-export
