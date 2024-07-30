04ad2261ac8057fb03bac777e8ce2606e704b49a
Filip Solecki <filip.solecki@swmansion.com>
2024-07-30 11:31:42 +0200

    Add new empty state component to add report field list type page

--- src/pages/workspace/reportFields/ReportFieldsListValuesPage.tsx
+++ src/pages/workspace/reportFields/ReportFieldsListValuesPage.tsx
@@ -6,6 +6,7 @@ import Button from '@components/Button';
 import ButtonWithDropdownMenu from '@components/ButtonWithDropdownMenu';
 import type {DropdownOption} from '@components/ButtonWithDropdownMenu/types';
 import ConfirmModal from '@components/ConfirmModal';
+import EmptyStateComponent from '@components/EmptyStateComponent';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import * as Expensicons from '@components/Icon/Expensicons';
 import * as Illustrations from '@components/Icon/Illustrations';
@@ -14,8 +15,8 @@ import SelectionList from '@components/SelectionList';
 import ListItemRightCaretWithLabel from '@components/SelectionList/ListItemRightCaretWithLabel';
 import TableListItem from '@components/SelectionList/TableListItem';
 import type {ListItem} from '@components/SelectionList/types';
+import TableListItemSkeleton from '@components/Skeletons/TableRowSkeleton';
 import Text from '@components/Text';
-import WorkspaceEmptyStateSection from '@components/WorkspaceEmptyStateSection';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
@@ -99,7 +100,7 @@ function ReportFieldsListValuesPage({
     }, [disabledListValues, listValues, policy?.fieldList, reportFieldID, selectedValues, translate]);
 
     const hasAccountingConnections = PolicyUtils.hasAccountingConnections(policy);
-    const shouldShowEmptyState = Object.values(listValues ?? {}).length <= 0;
+    const shouldShowEmptyState = true;
     const selectedValuesArray = Object.keys(selectedValues).filter((key) => selectedValues[key]);
 
     const toggleValue = (valueItem: ValueListItem) => {
@@ -292,11 +293,14 @@ function ReportFieldsListValuesPage({
                     <Text style={[styles.sidebarLinkText, styles.optionAlternateText]}>{translate('workspace.reportFields.listInputSubtitle')}</Text>
                 </View>
                 {shouldShowEmptyState && (
-                    <WorkspaceEmptyStateSection
-                        containerStyle={[styles.bgTransparent, styles.mtAuto, styles.mbAuto]}
+                    <EmptyStateComponent
                         title={translate('workspace.reportFields.emptyReportFieldsValues.title')}
-                        icon={Illustrations.EmptyStateExpenses}
                         subtitle={translate('workspace.reportFields.emptyReportFieldsValues.subtitle')}
+                        SkeletonComponent={TableListItemSkeleton}
+                        headerMediaType={CONST.EMPTY_STATE_MEDIA.ILLUSTRATION}
+                        headerMedia={Illustrations.EmptyStateExpenses}
+                        headerStyles={styles.emptyFolderBG}
+                        headerContentStyles={styles.emptyStateFolderIconSize}
                     />
                 )}
                 {!shouldShowEmptyState && (
