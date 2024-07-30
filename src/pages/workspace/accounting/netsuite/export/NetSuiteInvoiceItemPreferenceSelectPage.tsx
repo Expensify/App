d11260058a19f43f3c72371608bb9a298dd8cbf2
war-in <war-in@users.noreply.github.com>
2024-07-30 18:30:34 +0200

    update invoice config pages

--- src/pages/workspace/accounting/netsuite/export/NetSuiteInvoiceItemPreferenceSelectPage.tsx
+++ src/pages/workspace/accounting/netsuite/export/NetSuiteInvoiceItemPreferenceSelectPage.tsx
@@ -1,13 +1,13 @@
 import React, {useCallback, useMemo} from 'react';
 import {View} from 'react-native';
 import type {ValueOf} from 'type-fest';
+import ConnectionLayout from '@components/ConnectionLayout';
 import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
 import OfflineWithFeedback from '@components/OfflineWithFeedback';
+import SelectionList from '@components/SelectionList';
 import RadioListItem from '@components/SelectionList/RadioListItem';
 import type {ListItem} from '@components/SelectionList/types';
-import SelectionScreen from '@components/SelectionScreen';
 import type {SelectorType} from '@components/SelectionScreen';
-import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as Connections from '@libs/actions/connections/NetSuiteCommands';
@@ -54,39 +54,42 @@ function NetSuiteInvoiceItemPreferenceSelectPage({policy}: WithPolicyConnections
         [config?.invoiceItemPreference, policyID],
     );
 
-    const headerContent = useMemo(
-        () => (
-            <View style={[styles.pb2, styles.ph5]}>
-                <Text style={[styles.pb2, styles.textNormal]}>
-                    {translate(`workspace.netsuite.invoiceItem.values.${config?.invoiceItemPreference ?? CONST.NETSUITE_INVOICE_ITEM_PREFERENCE.CREATE}.description`)}
-                </Text>
-            </View>
-        ),
-        [styles.pb2, styles.ph5, styles.textNormal, translate, config?.invoiceItemPreference],
-    );
-
     return (
-        <SelectionScreen
-            displayName={NetSuiteInvoiceItemPreferenceSelectPage.displayName}
-            title="workspace.netsuite.invoiceItem.label"
-            sections={[{data}]}
-            listItem={RadioListItem}
-            headerContent={headerContent}
-            onSelectRow={(selection: SelectorType) => selectInvoicePreference(selection as MenuListItem)}
-            initiallyFocusedOptionKey={data.find((mode) => mode.isSelected)?.keyForList}
-            policyID={policyID}
+        <ConnectionLayout
+            headerTitle="workspace.netsuite.invoiceItem.label"
+            title={`workspace.netsuite.invoiceItem.values.${config?.invoiceItemPreference ?? CONST.NETSUITE_INVOICE_ITEM_PREFERENCE.CREATE}.description`}
+            titleStyle={[styles.ph5, styles.pb5]}
+            onBackButtonPress={() => Navigation.goBack(ROUTES.POLICY_ACCOUNTING_NETSUITE_EXPORT.getRoute(policyID))}
             accessVariants={[CONST.POLICY.ACCESS_VARIANTS.ADMIN]}
             featureName={CONST.POLICY.MORE_FEATURES.ARE_CONNECTIONS_ENABLED}
-            onBackButtonPress={() => Navigation.goBack(ROUTES.POLICY_ACCOUNTING_NETSUITE_EXPORT.getRoute(policyID))}
+            displayName={NetSuiteInvoiceItemPreferenceSelectPage.displayName}
+            policyID={policyID}
             connectionName={CONST.POLICY.CONNECTIONS.NAME.NETSUITE}
-            listFooterContent={
-                config?.invoiceItemPreference === CONST.NETSUITE_INVOICE_ITEM_PREFERENCE.SELECT ? (
+            shouldUseScrollView={false}
+            shouldIncludeSafeAreaPaddingBottom
+        >
+            <OfflineWithFeedback
+                pendingAction={config?.pendingFields?.[CONST.NETSUITE_CONFIG.INVOICE_ITEM_PREFERENCE]}
+                errors={ErrorUtils.getLatestErrorField(config, CONST.NETSUITE_CONFIG.INVOICE_ITEM_PREFERENCE)}
+                errorRowStyles={[styles.ph5, styles.pv3]}
+                onClose={() => Policy.clearSageIntacctErrorField(policyID, CONST.NETSUITE_CONFIG.INVOICE_ITEM_PREFERENCE)}
+                style={[styles.flexGrow1, styles.flexShrink1]}
+                contentContainerStyle={[styles.flexGrow1, styles.flexShrink1]}
+            >
+                <SelectionList
+                    onSelectRow={(selection: SelectorType) => selectInvoicePreference(selection as MenuListItem)}
+                    sections={[{data}]}
+                    ListItem={RadioListItem}
+                    showScrollIndicator
+                    initiallyFocusedOptionKey={data.find((mode) => mode.isSelected)?.keyForList}
+                    containerStyle={[styles.flexReset, styles.flexGrow1, styles.flexShrink1, styles.pb0]}
+                />
+            </OfflineWithFeedback>
+            {config?.invoiceItemPreference === CONST.NETSUITE_INVOICE_ITEM_PREFERENCE.SELECT && (
+                <View style={[styles.flexGrow1, styles.flexShrink1]}>
                     <OfflineWithFeedback
                         key={translate('workspace.netsuite.invoiceItem.label')}
                         pendingAction={config?.pendingFields?.invoiceItem}
-                        errors={ErrorUtils.getLatestErrorField(config, CONST.NETSUITE_CONFIG.INVOICE_ITEM)}
-                        errorRowStyles={[styles.ph5]}
-                        onClose={() => Policy.clearNetSuiteErrorField(policyID, CONST.NETSUITE_CONFIG.INVOICE_ITEM)}
                     >
                         <MenuItemWithTopDescription
                             description={translate('workspace.netsuite.invoiceItem.label')}
@@ -97,9 +100,9 @@ function NetSuiteInvoiceItemPreferenceSelectPage({policy}: WithPolicyConnections
                             brickRoadIndicator={config?.errorFields?.invoiceItem ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : undefined}
                         />
                     </OfflineWithFeedback>
-                ) : null
-            }
-        />
+                </View>
+            )}
+        </ConnectionLayout>
     );
 }
 
