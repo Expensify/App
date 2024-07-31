312f925942756c17322573e5daeccddd9dcdb5d4
war-in <war-in@users.noreply.github.com>
2024-07-31 13:02:49 +0200

    Revert "set active integration to undefined when disconnecting from another integration"
    This reverts commit 4d97f6e7e1f4b8f82b058714316694da47d5d365.

--- src/pages/workspace/accounting/AccountingContext.tsx
+++ src/pages/workspace/accounting/AccountingContext.tsx
@@ -71,6 +71,19 @@ function AccountingContextProvider({children, policy}: AccountingContextProvider
         [policy, policyID, translate],
     );
 
+    const closeConfirmationModal = () => {
+        setActiveIntegration((prev) => {
+            if (prev) {
+                return {
+                    ...prev,
+                    shouldDisconnectIntegrationBeforeConnecting: false,
+                    integrationToDisconnect: undefined,
+                };
+            }
+            return undefined;
+        });
+    };
+
     const accountingContext = useMemo(
         () => ({
             activeIntegration,
@@ -101,7 +114,7 @@ function AccountingContextProvider({children, policy}: AccountingContextProvider
                             return;
                         }
                         removePolicyConnection(policyID, activeIntegration?.integrationToDisconnect);
-                        setActiveIntegration(undefined);
+                        closeConfirmationModal();
                     }}
                     integrationToConnect={activeIntegration?.integrationToDisconnect}
                     onCancel={() => {
