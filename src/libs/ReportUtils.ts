1bb96d1ac93bb16f7c1e0ac23d7d68c8a9d46354
daledah <daleda261@outlook.com>
2024-08-01 22:34:50 +0700

    feat: add isBold property to OptionData

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -451,6 +451,7 @@ type OptionData = {
     amountInputProps?: MoneyRequestAmountInputProps;
     tabIndex?: 0 | -1;
     isConciergeChat?: boolean;
+    isBold?: boolean;
 } & Report;
 
 type OnyxDataTaskAssigneeChat = {
