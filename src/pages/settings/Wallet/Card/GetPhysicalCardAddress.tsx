ee67c38a368f7ed74765ef591cde9d58950b75c5
nkdengineer <nkdengineer@outlook.com>
2024-08-01 11:30:24 +0700

    fix: State is not auto filled after selecting the address

--- src/pages/settings/Wallet/Card/GetPhysicalCardAddress.tsx
+++ src/pages/settings/Wallet/Card/GetPhysicalCardAddress.tsx
@@ -1,15 +1,17 @@
 import type {StackScreenProps} from '@react-navigation/stack';
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {withOnyx} from 'react-native-onyx';
 import type {OnyxEntry} from 'react-native-onyx';
 import AddressForm from '@components/AddressForm';
 import useLocalize from '@hooks/useLocalize';
 import * as FormActions from '@libs/actions/FormActions';
 import type {SettingsNavigatorParamList} from '@navigation/types';
+import type {Country} from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
 import type {GetPhysicalCardForm} from '@src/types/form';
+import type {Address} from '@src/types/onyx/PrivatePersonalDetails';
 import BaseGetPhysicalCard from './BaseGetPhysicalCard';
 import type {RenderContentProps} from './BaseGetPhysicalCard';
 
@@ -28,7 +30,23 @@ function GetPhysicalCardAddress({
 }: GetPhysicalCardAddressProps) {
     const {translate} = useLocalize();
 
-    const {addressLine1 = '', addressLine2 = '', city = '', state = '', zipPostCode = '', country = ''} = draftValues ?? {};
+    // Check if country is valid
+    const {addressLine1, addressLine2} = draftValues ?? {};
+    const [currentCountry, setCurrentCountry] = useState(draftValues?.country);
+    const [state, setState] = useState(draftValues?.state);
+    const [city, setCity] = useState(draftValues?.city);
+    const [zipPostCode, setZipPostCode] = useState(draftValues?.zipPostCode);
+
+    useEffect(() => {
+        if (!draftValues) {
+            return;
+        }
+        setState(draftValues.state);
+        setCurrentCountry(draftValues.country);
+        setCity(draftValues.city);
+        setZipPostCode(draftValues.zipPostCode);
+        // eslint-disable-next-line react-compiler/react-compiler, react-hooks/exhaustive-deps
+    }, [draftValues?.state, draftValues?.country, draftValues?.city, draftValues?.zipPostCode]);
 
     useEffect(() => {
         if (!countryFromUrl) {
@@ -37,14 +55,43 @@ function GetPhysicalCardAddress({
         FormActions.setDraftValues(ONYXKEYS.FORMS.GET_PHYSICAL_CARD_FORM, {country: countryFromUrl});
     }, [countryFromUrl]);
 
+    const handleAddressChange = useCallback((value: unknown, key: unknown) => {
+        const addressPart = value as string;
+        const addressPartKey = key as keyof Address;
+
+        if (addressPartKey !== 'country' && addressPartKey !== 'state' && addressPartKey !== 'city' && addressPartKey !== 'zipPostCode') {
+            return;
+        }
+        if (addressPartKey === 'country') {
+            setCurrentCountry(addressPart as Country | '');
+            setState('');
+            setCity('');
+            setZipPostCode('');
+            return;
+        }
+        if (addressPartKey === 'state') {
+            setState(addressPart);
+            setCity('');
+            setZipPostCode('');
+            return;
+        }
+        if (addressPartKey === 'city') {
+            setCity(addressPart);
+            setZipPostCode('');
+            return;
+        }
+        setZipPostCode(addressPart);
+    }, []);
+
     const renderContent = useCallback(
         ({onSubmit, submitButtonText}: RenderContentProps) => (
             <AddressForm
                 formID={ONYXKEYS.FORMS.GET_PHYSICAL_CARD_FORM}
                 onSubmit={onSubmit}
+                onAddressChanged={handleAddressChange}
                 submitButtonText={submitButtonText}
                 city={city}
-                country={country}
+                country={currentCountry}
                 shouldSaveDraft
                 state={state}
                 street1={addressLine1}
@@ -52,7 +99,7 @@ function GetPhysicalCardAddress({
                 zip={zipPostCode}
             />
         ),
-        [addressLine1, addressLine2, city, country, state, zipPostCode],
+        [addressLine1, addressLine2, city, currentCountry, handleAddressChange, state, zipPostCode],
     );
 
     return (
