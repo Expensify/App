c7cb1199219ded7c82ee1cce6627818602cdec2b
Georgia Monahan <38015950+grgia@users.noreply.github.com>
2024-08-01 19:02:54 +0100

    Update src/components/ReportActionItem/ReportPreview.tsx
    Co-authored-by: Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>

--- src/components/ReportActionItem/ReportPreview.tsx
+++ src/components/ReportActionItem/ReportPreview.tsx
@@ -122,7 +122,6 @@ function ReportPreview({
     const {translate} = useLocalize();
     const {canUseViolations} = usePermissions();
     const {isOffline} = useNetwork();
-    // eslint-disable-next-line react-compiler/react-compiler, react-hooks/exhaustive-deps
 const allTransactions = useMemo(() => TransactionUtils.getAllReportTransactions(iouReportID, transactions), [iouReportID, transactions]);
 
     const {hasMissingSmartscanFields, areAllRequestsBeingSmartScanned, hasOnlyTransactionsWithPendingRoutes, hasNonReimbursableTransactions} = useMemo(
