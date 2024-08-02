4f10c9ff08fc2a78241bfe993a594c823e9e2b52
Filip Solecki <filip.solecki@swmansion.com>
2024-08-02 08:28:14 +0200

    Clear selectedTags and turn off selection mode on page leave WorkspaceViewTagsPage

--- src/pages/workspace/tags/WorkspaceViewTagsPage.tsx
+++ src/pages/workspace/tags/WorkspaceViewTagsPage.tsx
@@ -69,6 +69,11 @@ function WorkspaceViewTagsPage({route}: WorkspaceViewTagsProps) {
             return;
         }
         setSelectedTags({});
+
+        return () => {
+            setSelectedTags({});
+            turnOffMobileSelectionMode();
+        };
     }, [isFocused]);
 
     const tagList = useMemo<TagListItem[]>(
