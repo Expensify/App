826c5c8d5b08faf5c4d71449ca326944e31827b2
krishna2323 <belivethatkg@gmail.com>
2024-08-05 15:24:12 +0530

    fix: merge conflicts.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/pages/workspace/reportFields/ReportFieldsSettingsPage.tsx
+++ src/pages/workspace/reportFields/ReportFieldsSettingsPage.tsx
@@ -70,51 +70,6 @@ function ReportFieldsSettingsPage({
                     title={reportField.name}
                     shouldSetModalVisibility={false}
                 />
-                <MenuItemWithTopDescription
-                    style={[styles.moneyRequestMenuItem]}
-                    titleStyle={styles.flex1}
-                    title={reportField.name}
-                    description={translate('common.name')}
-                    interactive={false}
-                />
-                <MenuItemWithTopDescription
-                    style={[styles.moneyRequestMenuItem]}
-                    titleStyle={styles.flex1}
-                    title={Str.recapitalize(translate(WorkspaceReportFieldUtils.getReportFieldTypeTranslationKey(reportField.type)))}
-                    description={translate('common.type')}
-                    interactive={false}
-                />
-                {isListFieldType && (
-                    <MenuItemWithTopDescription
-                        style={[styles.moneyRequestMenuItem]}
-                        titleStyle={styles.flex1}
-                        description={translate('workspace.reportFields.listValues')}
-                        shouldShowRightIcon
-                        onPress={() => Navigation.navigate(ROUTES.WORKSPACE_REPORT_FIELDS_LIST_VALUES.getRoute(policyID, reportFieldID))}
-                        title={listValues.join(', ')}
-                        numberOfLinesTitle={5}
-                    />
-                )}
-                {!isListFieldEmpty && (
-                    <MenuItemWithTopDescription
-                        style={[styles.moneyRequestMenuItem]}
-                        titleStyle={styles.flex1}
-                        title={WorkspaceReportFieldUtils.getReportFieldInitialValue(reportField)}
-                        description={translate('common.initialValue')}
-                        shouldShowRightIcon={!isDateFieldType && !hasAccountingConnections}
-                        interactive={!isDateFieldType && !hasAccountingConnections}
-                        onPress={() => Navigation.navigate(ROUTES.WORKSPACE_EDIT_REPORT_FIELDS_INITIAL_VALUE.getRoute(policyID, reportFieldID))}
-                    />
-                )}
-                {!hasAccountingConnections && (
-                    <View style={styles.flexGrow1}>
-                        <MenuItem
-                            icon={Expensicons.Trashcan}
-                            title={translate('common.delete')}
-                            onPress={() => setIsDeleteModalVisible(true)}
-                        />
-                    </View>
-                )}
                 <ConfirmModal
                     title={translate('workspace.reportFields.delete')}
                     isVisible={isDeleteModalVisible && !hasAccountingConnections}
@@ -126,6 +81,53 @@ function ReportFieldsSettingsPage({
                     cancelText={translate('common.cancel')}
                     danger
                 />
+                <View style={styles.flexGrow1}>
+                    <MenuItemWithTopDescription
+                        style={[styles.moneyRequestMenuItem]}
+                        titleStyle={styles.flex1}
+                        title={reportField.name}
+                        description={translate('common.name')}
+                        interactive={false}
+                    />
+                    <MenuItemWithTopDescription
+                        style={[styles.moneyRequestMenuItem]}
+                        titleStyle={styles.flex1}
+                        title={Str.recapitalize(translate(WorkspaceReportFieldUtils.getReportFieldTypeTranslationKey(reportField.type)))}
+                        description={translate('common.type')}
+                        interactive={false}
+                    />
+                    {isListFieldType && (
+                        <MenuItemWithTopDescription
+                            style={[styles.moneyRequestMenuItem]}
+                            titleStyle={styles.flex1}
+                            description={translate('workspace.reportFields.listValues')}
+                            shouldShowRightIcon
+                            onPress={() => Navigation.navigate(ROUTES.WORKSPACE_REPORT_FIELDS_LIST_VALUES.getRoute(policyID, reportFieldID))}
+                            title={listValues.join(', ')}
+                            numberOfLinesTitle={5}
+                        />
+                    )}
+                    {!isListFieldEmpty && (
+                        <MenuItemWithTopDescription
+                            style={[styles.moneyRequestMenuItem]}
+                            titleStyle={styles.flex1}
+                            title={WorkspaceReportFieldUtils.getReportFieldInitialValue(reportField)}
+                            description={translate('common.initialValue')}
+                            shouldShowRightIcon={!isDateFieldType && !hasAccountingConnections}
+                            interactive={!isDateFieldType && !hasAccountingConnections}
+                            onPress={() => Navigation.navigate(ROUTES.WORKSPACE_EDIT_REPORT_FIELDS_INITIAL_VALUE.getRoute(policyID, reportFieldID))}
+                        />
+                    )}
+                    {!hasAccountingConnections && (
+                        <View style={styles.flexGrow1}>
+                            <MenuItem
+                                icon={Expensicons.Trashcan}
+                                title={translate('common.delete')}
+                                onPress={() => setIsDeleteModalVisible(true)}
+                            />
+                        </View>
+                    )}
+                </View>
             </ScreenWrapper>
         </AccessOrNotFoundWrapper>
     );
