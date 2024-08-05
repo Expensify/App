98eceb02ef4305cff9e17195c72bbf10fcd08f91
Daniel Gale-Rosen <dgalerosen@expensify.com>
2024-08-05 15:26:25 -0400

    add new types in list

--- src/libs/API/parameters/index.ts
+++ src/libs/API/parameters/index.ts
@@ -220,6 +220,8 @@ export type {default as SetPolicyCustomTaxNameParams} from './SetPolicyCustomTax
 export type {default as SetPolicyForeignCurrencyDefaultParams} from './SetPolicyForeignCurrencyDefaultParams';
 export type {default as SetPolicyCurrencyDefaultParams} from './SetPolicyCurrencyDefaultParams';
 export type {default as UpdateQuickbooksOnlineEnableNewCategoriesParams} from './UpdateQuickbooksOnlineEnableNewCategoriesParams';
+export type {default as UpdateQuickbooksOnlineAutoCreateVendorParams} from './UpdateQuickbooksOnlineAutoCreateVendorParams';
+export type {default as UpdateQuickbooksOnlineReimbursableExpensesAccountParams} from './UpdateQuickbooksOnlineReimbursableExpensesAccountParams';
 export type {default as UpdatePolicyConnectionConfigParams} from './UpdatePolicyConnectionConfigParams';
 export type {default as UpdateManyPolicyConnectionConfigurationsParams} from './UpdateManyPolicyConnectionConfigurationsParams';
 export type {default as RemovePolicyConnectionParams} from './RemovePolicyConnectionParams';
