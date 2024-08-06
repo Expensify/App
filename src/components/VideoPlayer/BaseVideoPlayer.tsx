b26c1efe5de4d6b2230bedc4bbe1d94678f5121c
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-08-06 11:44:07 +0800

    fix neseted button error

--- src/components/VideoPlayer/BaseVideoPlayer.tsx
+++ src/components/VideoPlayer/BaseVideoPlayer.tsx
@@ -297,7 +297,6 @@ function BaseVideoPlayer({
             {/* We need to wrap the video component in a component that will catch unhandled pointer events. Otherwise, these
             events will bubble up the tree, and it will cause unexpected press behavior. */}
             <PressableWithoutFeedback
-                accessibilityRole="button"
                 accessible={false}
                 style={[styles.cursorDefault, style]}
             >
