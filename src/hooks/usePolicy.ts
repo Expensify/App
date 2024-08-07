5b0d5a371c15a1d59dd0a86e0ff1eca263043a95
daledah <daleda261@outlook.com>
2024-08-08 00:22:45 +0700

    fix: move getPolicyId to outside of usePolicy

--- src/hooks/usePolicy.ts
+++ src/hooks/usePolicy.ts
@@ -2,15 +2,15 @@ import {useOnyx} from 'react-native-onyx';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 
-function usePolicy(policyID?: string) {
-    const getPolicyID = () => {
-        if (!policyID || policyID === CONST.POLICY.OWNER_EMAIL_FAKE || policyID === '-1') {
-            return '-1';
-        }
-        return policyID;
-    };
+function getPolicyIDOrDefault(policyID?: string) {
+    if (!policyID || policyID === CONST.POLICY.OWNER_EMAIL_FAKE) {
+        return '-1';
+    }
+    return policyID;
+}
 
-    const [policy] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${getPolicyID()}`);
+function usePolicy(policyID?: string) {
+    const [policy] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${getPolicyIDOrDefault(policyID)}`);
     return policy;
 }
 
