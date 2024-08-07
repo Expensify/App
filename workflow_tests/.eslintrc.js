341aba5370e25ac3b02195732b89939844afe67d
Github <andrew@expensify.com>
2024-08-07 17:42:52 -0600

    Delete workflow tests

--- workflow_tests/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rules: {
-        '@lwc/lwc/no-async-await': 'off',
-        'no-await-in-loop': 'off',
-        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
-    },
-};
