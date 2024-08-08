4d85ff75ccac4c4b50ddc31128a0d351389eb9f4
Carlos Martins <cmartins@expensify.com>
2024-08-09 00:09:13 +0300

    refactor empty view

--- src/pages/Search/EmptySearchView.tsx
+++ src/pages/Search/EmptySearchView.tsx
@@ -1,41 +1,15 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import EmptyStateComponent from '@components/EmptyStateComponent';
 import * as Illustrations from '@components/Icon/Illustrations';
 import SearchRowSkeleton from '@components/Skeletons/SearchRowSkeleton';
+import useLocalize from '@hooks/useLocalize';
+import useStyleUtils from '@hooks/useStyleUtils';
+import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
-import * as Localize from '@libs/Localize';
 import Navigation from '@libs/Navigation/Navigation';
 import CONST from '@src/CONST';
 import ROUTES from '@src/ROUTES';
 import type {SearchDataTypes} from '@src/types/onyx/SearchResults';
-import colors from '@styles/theme/colors';
-
-function getContent(type: SearchDataTypes) {
-    switch (type) {
-        case CONST.SEARCH.DATA_TYPES.TRIP:
-            return {
-                headerMedia: Illustrations.EmptyStateTravel,
-                headerStyles: {backgroundColor: colors.blue600},
-                title: Localize.translateLocal('search.searchResults.emptyTripResults.title'),
-                subtitle: Localize.translateLocal('search.searchResults.emptyTripResults.subtitle'),
-                buttonText: Localize.translateLocal('search.searchResults.emptyTripResults.buttonText'),
-                buttonAction: () => Navigation.navigate(ROUTES.TRAVEL_MY_TRIPS),
-            };
-        case CONST.SEARCH.DATA_TYPES.TRANSACTION:
-        case CONST.SEARCH.DATA_TYPES.REPORT:
-        case CONST.SEARCH.DATA_TYPES.EXPENSE:
-        case CONST.SEARCH.DATA_TYPES.INVOICE:
-        default:
-            return {
-                headerMedia: Illustrations.EmptyState,
-                headerStyles: {backgroundColor: colors.yellow600},
-                title: Localize.translateLocal('search.searchResults.emptyResults.title'),
-                subtitle: Localize.translateLocal('search.searchResults.emptyResults.subtitle'),
-                buttonText: undefined,
-                buttonAction: undefined,
-            };
-    }
-}
 
 type EmptySearchViewProps = {
     type: SearchDataTypes;
@@ -43,7 +17,36 @@ type EmptySearchViewProps = {
 
 function EmptySearchView({type}: EmptySearchViewProps) {
     const styles = useThemeStyles();
-    const content = getContent(type, styles);
+    const theme = useTheme();
+    const StyleUtils = useStyleUtils();
+    const {translate} = useLocalize();
+
+    const content = useMemo(() => {
+        switch (type) {
+            case CONST.SEARCH.DATA_TYPES.TRIP:
+                return {
+                    headerMedia: Illustrations.EmptyStateTravel,
+                    headerStyles: StyleUtils.getBackgroundColorStyle(theme.mentionBG),
+                    title: translate('search.searchResults.emptyTripResults.title'),
+                    subtitle: translate('search.searchResults.emptyTripResults.subtitle'),
+                    buttonText: translate('search.searchResults.emptyTripResults.buttonText'),
+                    buttonAction: () => Navigation.navigate(ROUTES.TRAVEL_MY_TRIPS),
+                };
+            case CONST.SEARCH.DATA_TYPES.TRANSACTION:
+            case CONST.SEARCH.DATA_TYPES.REPORT:
+            case CONST.SEARCH.DATA_TYPES.EXPENSE:
+            case CONST.SEARCH.DATA_TYPES.INVOICE:
+            default:
+                return {
+                    headerMedia: Illustrations.EmptyState,
+                    headerStyles: StyleUtils.getBackgroundColorStyle(theme.emptyFolderBG),
+                    title: translate('search.searchResults.emptyResults.title'),
+                    subtitle: translate('search.searchResults.emptyResults.subtitle'),
+                    buttonText: undefined,
+                    buttonAction: undefined,
+                };
+        }
+    }, [type, StyleUtils, translate, theme]);
 
     return (
         <EmptyStateComponent
