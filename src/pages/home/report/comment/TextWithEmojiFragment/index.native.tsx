8fdd879c32d4d2a493d6ea72951d645e1805fa27
VickyStash <vikstash@gmail.com>
2024-08-08 11:56:33 +0200

    Add more options for testing

--- src/pages/home/report/comment/TextWithEmojiFragment/index.native.tsx
+++ src/pages/home/report/comment/TextWithEmojiFragment/index.native.tsx
@@ -1,5 +1,6 @@
 import React, {useMemo} from 'react';
-import {StyleProp, TextStyle, View} from 'react-native';
+import {View} from 'react-native';
+import type {StyleProp, TextStyle} from 'react-native';
 import Text from '@components/Text';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -41,15 +42,21 @@ function TextWithEmojiFragment({message, passedStyles, styleAsDeleted, styleAsMu
                 !DeviceCapabilities.canUseTouchScreen() || !shouldUseNarrowLayout ? styles.userSelectText : styles.userSelectNone,
             ]}
         >
-            {processedTextArray.map(({text: textik, isEmoji}) =>
+            {processedTextArray.map(({text: textItem, isEmoji}) =>
                 isEmoji ? (
                     <View>
-                        <Text style={{fontSize: 19, fontFamily: 'System', marginBottom: -5}}>{textik}</Text>
+                        <Text style={{fontSize: 19, fontFamily: 'System', marginBottom: -5}}>{textItem}</Text>
                     </View>
                 ) : (
-                    convertToLTR(textik ?? '')
+                    convertToLTR(textItem ?? '')
                 ),
             )}
+
+            {/* Option 2 */}
+            {/* {processedTextArray.map(({text: textItem, isEmoji}) => (isEmoji ? <Text style={{fontSize: 19, fontFamily: 'System'}}>{textItem}</Text> : convertToLTR(textItem ?? '')))} */}
+
+            {/* Option 3 - with 15 font size */}
+            {/* {processedTextArray.map(({text: textItem, isEmoji}) => (isEmoji ? <Text style={{fontFamily: 'System'}}>{textItem}</Text> : convertToLTR(textItem ?? '')))} */}
         </Text>
     );
 }
