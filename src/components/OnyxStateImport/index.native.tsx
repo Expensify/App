caa2573c0f0a7bd09568c43676b1df6f20867395
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-08-09 14:48:27 +0200

    fix: make the android component more performant to not crash the app when importing state

--- src/components/OnyxStateImport/index.native.tsx
+++ src/components/OnyxStateImport/index.native.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RNFS from 'react-native-fs';
 import Onyx from 'react-native-onyx';
 import AttachmentPicker from '@components/AttachmentPicker';
@@ -16,27 +16,131 @@ function transformNumericKeysToArray(obj) {
         return obj;
     }
 
-    // Check if all keys are numeric and sequential starting from 0
-    const keys = Object.keys(obj);
-    const allKeysAreNumeric = keys.every((key) => !isNaN(key));
-    const keysAreSequential = keys.every((key, index) => parseInt(key) === index);
+    if (Array.isArray(obj)) {
+        return obj.map(transformNumericKeysToArray);
+    }
 
-    if (allKeysAreNumeric && keysAreSequential) {
-        // Convert object to array
+    const keys = Object.keys(obj);
+    const length = keys.length;
+    let allKeysAreNumericAndSequential = true;
+
+    for (let i = 0; i < length; i++) {
+        const key = keys[i];
+        if (parseInt(key, 10) !== i) {
+            allKeysAreNumericAndSequential = false;
+            break;
+        }
+    }
+
+    if (allKeysAreNumericAndSequential) {
         return keys.map((key) => transformNumericKeysToArray(obj[key]));
     }
 
-    // Recursively apply transformation to each property
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            obj[key] = transformNumericKeysToArray(obj[key]);
-        }
+    for (const key of keys) {
+        obj[key] = transformNumericKeysToArray(obj[key]);
     }
 
     return obj;
 }
+
+function readFileInChunks(fileUri, chunkSize = 1024 * 1024) {
+    console.log('File URI:', fileUri); // Log the file URI
+    const filePath = decodeURIComponent(fileUri.replace('file://', ''));
+    // Check if the file exists
+    return RNFS.exists(filePath)
+        .then((exists) => {
+            if (!exists) {
+                throw new Error('File does not exist');
+            }
+            console.log('File exists:', exists);
+            return RNFS.stat(filePath);
+        })
+        .then((fileStats) => {
+            console.log('File stats:', fileStats);
+            const fileSize = fileStats.size;
+            let fileContent = '';
+            const promises = [];
+
+            console.log('Start chunking');
+            for (let i = 0; i < fileSize; i += chunkSize) {
+                promises.push(
+                    RNFS.read(filePath, chunkSize, i, 'utf8').then((chunk) => {
+                        fileContent += chunk;
+                    }),
+                );
+            }
+            console.log('end chunking');
+
+            return Promise.all(promises).then(() => fileContent);
+        })
+        .catch((error) => {
+            console.error('Error reading file:', error);
+            throw error;
+        });
+}
+
+const CHUNK_SIZE = 100; // Adjust the chunk size based on your needs
+
+function chunkArray(array, size) {
+    const result = [];
+    for (let i = 0; i < array.length; i += size) {
+        result.push(array.slice(i, i + size));
+    }
+    return result;
+}
+
+function applyStateInChunks(state) {
+    const entries = Object.entries(state);
+    const chunks = chunkArray(entries, CHUNK_SIZE);
+
+    let promise = Promise.resolve();
+    chunks.forEach((chunk) => {
+        promise = promise.then(() => Onyx.multiSet(Object.fromEntries(chunk)));
+    });
+
+    return promise;
+}
+
 export default function OnyxStateImport({setIsLoading}: {setIsLoading: (isLoading: boolean) => void}) {
     const {translate} = useLocalize();
+    const [error, setError] = useState(null);
+
+    const handleFileRead = (fileUri) => {
+        console.log('File URI:', fileUri); // Log the file URI
+        setIsLoading(true);
+        readFileInChunks(fileUri)
+            .then((fileContent) => {
+                const importedState = JSON.parse(fileContent);
+                console.log('Parse imported state');
+                const transformedState = transformNumericKeysToArray(importedState);
+                console.log('Transformed state:');
+                // Only keep the keys that we're interested in
+                Object.keys(transformedState).forEach((key) => {
+                    const shouldOmit = keysToOmit.some((onyxKey) => key.startsWith(onyxKey));
+                    if (shouldOmit) {
+                        delete transformedState[key];
+                    }
+                });
+                console.log('Omitted keys');
+
+                Onyx.merge(ONYXKEYS.NETWORK, {shouldForceOffline: true}).then(() => {
+                    // Onyx.clear(App.KEYS_TO_PRESERVE).then(() => {
+
+                    // 4. Apply the new state from the file
+                    applyStateInChunks(transformedState).then(() => {
+                        console.log('Applied imported state.');
+                        Navigation.navigate(ROUTES.HOME);
+                    });
+                });
+                // Handle the transformed state as needed
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                setError(err.message);
+                setIsLoading(false);
+            });
+    };
+
     return (
         <AttachmentPicker>
             {({openPicker}) => {
@@ -51,39 +155,7 @@ export default function OnyxStateImport({setIsLoading}: {setIsLoading: (isLoadin
                                         return;
                                     }
 
-                                    // 1. Read the file content
-                                    RNFS.readFile(file.uri).then((fileContent) => {
-                                        setIsLoading(true);
-                                        const importedState = JSON.parse(fileContent);
-                                        // TODO: this needs to be exactly the same as we get from running the App.openApp command?
-                                        const parsedState = {...importedState};
-
-                                        // 2. Only keep the keys that we're interested in
-                                        Object.keys(parsedState).forEach((key) => {
-                                            const shouldOmit = keysToOmit.some((onyxKey) => key.startsWith(onyxKey));
-
-                                            if (shouldOmit) {
-                                                delete parsedState[key];
-                                            }
-                                        });
-
-                                        const t = transformNumericKeysToArray(parsedState);
-                                        // 3. Go offline
-                                        Onyx.merge(ONYXKEYS.NETWORK, {shouldForceOffline: true}).then(() => {
-                                            // Onyx.clear(App.KEYS_TO_PRESERVE).then(() => {
-
-                                            // 4. Apply the new state from the file
-                                            Onyx.multiSet(t)
-                                                .then(() => {
-                                                    console.log('Applied imported state.');
-                                                    Navigation.navigate(ROUTES.HOME);
-                                                })
-                                                .finally(() => {
-                                                    setIsLoading(false);
-                                                });
-                                            // });
-                                        });
-                                    });
+                                    handleFileRead(file.uri);
                                 },
                             });
                         }}
