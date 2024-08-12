e99bf8855eca6db8e47034aca0d2a26b98322c4a
BrtqKr <bartlomiej.krason@swmansion.com>
2024-08-12 12:08:00 +0200

    add custom report names section content wip

--- src/pages/workspace/rules/ExpenseReportRulesSection.tsx
+++ src/pages/workspace/rules/ExpenseReportRulesSection.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
 import MenuItem from '@components/MenuItem';
 import Section from '@components/Section';
-import Text from '@components/Text';
+import Switch from '@components/Switch';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import ToggleSettingOptionRow from '@pages/workspace/workflows/ToggleSettingsOptionRow';
-
-const EXPENSE_REPORT_RULE_TYPES = [
-    {title: 'workspace.rules.expenseReportRules.customReportNamesTitle', description: 'workspace.rules.expenseReportRules.customReportNamesDescription'},
-    {title: 'workspace.rules.expenseReportRules.preventSelfApprovalsTitle', description: 'workspace.rules.expenseReportRules.preventSelfApprovalsDescription'},
-    {title: 'workspace.rules.expenseReportRules.autoApproveCompliantReportsTitle', description: 'workspace.rules.expenseReportRules.autoApproveCompliantReportsDescription'},
-    {title: 'workspace.rules.expenseReportRules.autoPayApprovedReportsTitle', description: 'workspace.rules.expenseReportRules.autoPayApprovedReportsDescription'},
-] as const;
 
 type ExpenseReportRulesSectionProps = {
     policyID: string;
@@ -21,6 +13,61 @@ type ExpenseReportRulesSectionProps = {
 function ExpenseReportRulesSection({policyID}: ExpenseReportRulesSectionProps) {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
+
+    const optionItems = [
+        {
+            title: translate('workspace.rules.expenseReportRules.customReportNamesTitle'),
+            subtitle: translate('workspace.rules.expenseReportRules.customReportNamesSubtitle'),
+            switchAccessibilityLabel: translate('workspace.rules.expenseReportRules.customReportNamesTitle'),
+            isActive: true,
+            onToggle: (isEnabled: boolean) => {},
+            subMenuItems: [
+                <MenuItem
+                    title="Custom name"
+                    titleStyle={styles.textLabelSupportingEmptyValue}
+                    shouldShowRightIcon
+                    style={[styles.sectionMenuItemTopDescription, styles.mt6, styles.mbn3]}
+                    onPress={() => {}}
+                />,
+                <MenuItem
+                    title="Prevent members from changing custom report names"
+                    titleStyle={{...styles.textLabelSupportingEmptyValue, ...styles.textWhite}}
+                    onPress={() => {}}
+                    rightComponent={
+                        <Switch
+                            accessibilityLabel="Prevent members from changing custom report names"
+                            onToggle={() => {}}
+                            isOn={false}
+                        />
+                    }
+                    shouldShowRightComponent
+                    style={[styles.sectionMenuItemTopDescription, styles.mt6, styles.mbn3]}
+                    // wrapperStyle={[styles.sectionMenuItemTopDescription, styles.mt3, styles.mbn3]}
+                    // brickRoadIndicator={hasReimburserError ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : undefined}
+                />,
+            ],
+        },
+        {
+            title: translate('workspace.rules.expenseReportRules.preventSelfApprovalsTitle'),
+            subtitle: translate('workspace.rules.expenseReportRules.preventSelfApprovalsSubtitle'),
+            switchAccessibilityLabel: translate('workspace.rules.expenseReportRules.preventSelfApprovalsTitle'),
+            onToggle: (isEnabled: boolean) => {},
+        },
+        {
+            title: translate('workspace.rules.expenseReportRules.autoApproveCompliantReportsTitle'),
+            subtitle: translate('workspace.rules.expenseReportRules.autoApproveCompliantReportsSubtitle'),
+            switchAccessibilityLabel: translate('workspace.rules.expenseReportRules.autoApproveCompliantReportsTitle'),
+            onToggle: (isEnabled: boolean) => {},
+        },
+
+        {
+            title: translate('workspace.rules.expenseReportRules.autoPayApprovedReportsTitle'),
+            subtitle: translate('workspace.rules.expenseReportRules.autoPayApprovedReportsSubtitle'),
+            switchAccessibilityLabel: translate('workspace.rules.expenseReportRules.autoPayApprovedReportsTitle'),
+            onToggle: (isEnabled: boolean) => {},
+        },
+    ];
+
     return (
         <Section
             isCentralPane
@@ -29,50 +76,22 @@ function ExpenseReportRulesSection({policyID}: ExpenseReportRulesSectionProps) {
             titleStyles={styles.accountSettingsSectionTitle}
             subtitleMuted
         >
-            {EXPENSE_REPORT_RULE_TYPES.map(({title, description}, index) => {
-                const showBorderBottom = index !== EXPENSE_REPORT_RULE_TYPES.length - 1;
+            {optionItems.map(({title, subtitle, isActive, subMenuItems}, index) => {
+                const showBorderBottom = index !== optionItems.length - 1;
 
                 return (
                     <ToggleSettingOptionRow
-                        title={translate(title)}
-                        subtitle={translate(description)}
+                        title={title}
+                        subtitle={subtitle}
+                        switchAccessibilityLabel={title}
                         wrapperStyle={[styles.pv6, showBorderBottom && styles.borderBottom]}
                         shouldPlaceSubtitleBelowSwitch
                         titleStyle={styles.pv2}
                         subtitleStyle={styles.pt1}
-                        switchAccessibilityLabel={translate(title)}
-                        isActive={false}
+                        isActive={!!isActive}
+                        subMenuItems={subMenuItems}
                         onToggle={() => {}}
                     />
-
-                    // <MenuItem
-                    //     description={translate(description)}
-                    //     onPress={() => {}}
-                    //     style={[{paddingHorizontal: 0}, showBorderBottom && styles.borderBottom]}
-                    //     titleContainerStyle={styles.pv6}
-                    //     descriptionTextStyle={styles.pt1}
-                    //     isLabelHoverable={false}
-                    //     shouldShowRightComponent
-                    //     interactive={false}
-                    //     titleComponent={
-                    //         <View style={[styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
-                    //             <View style={styles.pv3}>
-                    //                 <Text
-                    //                     style={styles.popoverMenuText}
-                    //                     numberOfLines={1}
-                    //                 >
-                    //                     {translate(title)}
-                    //                 </Text>
-                    //             </View>
-                    //             <ToggleSettingOptionRow
-                    //                 switchAccessibilityLabel={translate(title)}
-                    //                 onToggle={() => {}}
-                    //                 isActive={false}
-                    //             />
-                    //         </View>
-                    //     }
-                    //     shouldShowDescriptionOnTop={false}
-                    // />
                 );
             })}
         </Section>
