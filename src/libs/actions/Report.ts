a65773a5e4305908a8ba08f89856677ac9b83c6c
brunovjk <brunovjk@gmail.com>
2024-08-12 16:49:03 -0300

    fix test

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -439,9 +439,9 @@ function notifyNewAction(reportID: string, accountID?: number, reportActionID?: 
 /** Clears the highlight of a report action if it’s from the current user */
 function clearHighlightIfCurrentUserAction(accountID?: number) {
     const isCurrentUserAction = accountID === currentUserAccountID;
-    const reportActionID = Navigation.getTopmostReportActionId();
+    const reportActionID = Navigation.getTopmostReportActionId() || '-1';
 
-    if (!isCurrentUserAction || !reportActionID) {
+    if (!isCurrentUserAction || !reportActionID || reportActionID === '-1') {
         return;
     }
 
