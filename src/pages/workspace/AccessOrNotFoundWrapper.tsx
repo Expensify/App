3e622bf6f774e858531f49c7ec9b8e7e1c6b6a1f
BrtqKr <bartlomiej.krason@swmansion.com>
2024-08-12 10:27:57 +0200

    replace items with ToggleSettingOptionRow

--- src/pages/workspace/AccessOrNotFoundWrapper.tsx
+++ src/pages/workspace/AccessOrNotFoundWrapper.tsx
@@ -122,7 +122,7 @@ function AccessOrNotFoundWrapper({accessVariants = [], fullPageNotFoundViewProps
 
     const shouldShowFullScreenLoadingIndicator = !isMoneyRequest && isLoadingReportData !== false && (!Object.entries(policy ?? {}).length || !policy?.id);
 
-    const isFeatureEnabled = featureName ? PolicyUtils.isPolicyFeatureEnabled(policy, featureName) : true;
+    const isFeatureEnabled = true;
 
     const [isPolicyFeatureEnabled, setIsPolicyFeatureEnabled] = useState(isFeatureEnabled);
     const {isOffline} = useNetwork();
--- src/pages/workspace/rules/ExpenseReportRulesSection.tsx
+++ src/pages/workspace/rules/ExpenseReportRulesSection.tsx
@@ -33,34 +33,46 @@ function ExpenseReportRulesSection({policyID}: ExpenseReportRulesSectionProps) {
                 const showBorderBottom = index !== EXPENSE_REPORT_RULE_TYPES.length - 1;
 
                 return (
-                    <MenuItem
-                        description={translate(description)}
-                        onPress={() => {}}
-                        style={[{paddingHorizontal: 0}, showBorderBottom && styles.borderBottom]}
-                        titleContainerStyle={styles.pv6}
-                        descriptionTextStyle={styles.pt1}
-                        isLabelHoverable={false}
-                        shouldShowRightComponent
-                        interactive={false}
-                        titleComponent={
-                            <View style={[styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
-                                <View style={styles.pv3}>
-                                    <Text
-                                        style={styles.popoverMenuText}
-                                        numberOfLines={1}
-                                    >
-                                        {translate(title)}
-                                    </Text>
-                                </View>
-                                <ToggleSettingOptionRow
-                                    switchAccessibilityLabel={translate(title)}
-                                    onToggle={() => {}}
-                                    isActive={false}
-                                />
-                            </View>
-                        }
-                        shouldShowDescriptionOnTop={false}
+                    <ToggleSettingOptionRow
+                        title={translate(title)}
+                        subtitle={translate(description)}
+                        wrapperStyle={[styles.pv6, showBorderBottom && styles.borderBottom]}
+                        shouldPlaceSubtitleBelowSwitch
+                        titleStyle={styles.pv2}
+                        subtitleStyle={styles.pt1}
+                        switchAccessibilityLabel={translate(title)}
+                        isActive={false}
+                        onToggle={() => {}}
                     />
+
+                    // <MenuItem
+                    //     description={translate(description)}
+                    //     onPress={() => {}}
+                    //     style={[{paddingHorizontal: 0}, showBorderBottom && styles.borderBottom]}
+                    //     titleContainerStyle={styles.pv6}
+                    //     descriptionTextStyle={styles.pt1}
+                    //     isLabelHoverable={false}
+                    //     shouldShowRightComponent
+                    //     interactive={false}
+                    //     titleComponent={
+                    //         <View style={[styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
+                    //             <View style={styles.pv3}>
+                    //                 <Text
+                    //                     style={styles.popoverMenuText}
+                    //                     numberOfLines={1}
+                    //                 >
+                    //                     {translate(title)}
+                    //                 </Text>
+                    //             </View>
+                    //             <ToggleSettingOptionRow
+                    //                 switchAccessibilityLabel={translate(title)}
+                    //                 onToggle={() => {}}
+                    //                 isActive={false}
+                    //             />
+                    //         </View>
+                    //     }
+                    //     shouldShowDescriptionOnTop={false}
+                    // />
                 );
             })}
         </Section>
