ba25ad5254ae679bd2e5baae705bf03e67c5aa9a
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-08-13 21:07:53 +0800

    lint and ts

--- src/libs/BankAccountUtils.ts
+++ src/libs/BankAccountUtils.ts
@@ -1,4 +1,9 @@
-function getDefaultCompanyWebsite(): string {
+// import {Str} from 'expensify-common';
+import type {OnyxEntry} from 'react-native-onyx';
+import type * as OnyxTypes from '@src/types/onyx';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getDefaultCompanyWebsite(session: OnyxEntry<OnyxTypes.Session>, user: OnyxEntry<OnyxTypes.User>): string {
     // return user?.isFromPublicDomain ? 'https://' : `https://www.${Str.extractEmailDomain(session?.email ?? '')}`;
     // temporarily always returns https:// to fix https://github.com/Expensify/App/issues/47227 until https://github.com/Expensify/App/issues/45278 is resolved.
     return 'https://';
