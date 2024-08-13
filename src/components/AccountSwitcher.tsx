28dd9088ff264b451e851aeb45ce757f582565a8
Rushat Gabhane <rushatgabhane@gmail.com>
2024-08-13 08:34:40 +0300

    adjust styles to be responsive and text overflow

--- src/components/AccountSwitcher.tsx
+++ src/components/AccountSwitcher.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import {View} from 'react-native';
 import useCurrentUserPersonalDetails from '@hooks/useCurrentUserPersonalDetails';
 import useLocalize from '@hooks/useLocalize';
+import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
+import variables from '@styles/variables';
 import CONST from '@src/CONST';
 import Avatar from './Avatar';
+import Icon from './Icon';
+import * as Expensicons from './Icon/Expensicons';
 import {PressableWithFeedback} from './Pressable';
 import Text from './Text';
 
@@ -14,11 +18,14 @@ function AccountSwitcher() {
     const accountID = currentUserPersonalDetails?.accountID ?? -1;
     const styles = useThemeStyles();
     const {translate} = useLocalize();
+    const theme = useTheme();
 
     return (
         <PressableWithFeedback
             accessible
             accessibilityLabel={translate('common.profile')}
+            onPress={() => {}}
+            wrapperStyle={[styles.flexGrow1, styles.accountSwitcherWrapper, styles.justifyContentCenter]}
         >
             <View style={[styles.flexRow, styles.gap3]}>
                 <Avatar
@@ -28,13 +35,23 @@ function AccountSwitcher() {
                     source={avatarUrl}
                     fallbackIcon={currentUserPersonalDetails.fallbackIcon}
                 />
-                <View style={[styles.justifyContentCenter, styles.gap1]}>
-                    <Text
-                        numberOfLines={1}
-                        style={[styles.cardSectionTitle, styles.textBold]}
-                    >
-                        {currentUserPersonalDetails?.displayName}
-                    </Text>
+                <View style={[styles.flex1, styles.flexShrink1, styles.flexBasis0, styles.justifyContentCenter]}>
+                    <View style={[styles.flexRow, styles.gap1]}>
+                        <Text
+                            numberOfLines={1}
+                            style={[styles.textBold, styles.optionDisplayNameCompact, styles.pre]}
+                        >
+                            {currentUserPersonalDetails?.displayName}
+                        </Text>
+                        <View style={styles.justifyContentCenter}>
+                            <Icon
+                                fill={theme.icon}
+                                src={Expensicons.CaretUpDown}
+                                height={variables.iconSizeSmall}
+                                width={variables.iconSizeSmall}
+                            />
+                        </View>
+                    </View>
                     <Text
                         numberOfLines={1}
                         style={styles.colorMuted}
