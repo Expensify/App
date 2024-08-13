79adc548ac06e00e4044c2b069c0a823fb3cb686
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-08-13 20:41:18 +0800

    temporarily always returns https://

--- src/libs/BankAccountUtils.ts
+++ src/libs/BankAccountUtils.ts
@@ -1,9 +1,11 @@
-import {Str} from 'expensify-common';
+// import {Str} from 'expensify-common';
 import type {OnyxEntry} from 'react-native-onyx';
 import type * as OnyxTypes from '@src/types/onyx';
 
-function getDefaultCompanyWebsite(session: OnyxEntry<OnyxTypes.Session>, user: OnyxEntry<OnyxTypes.User>): string {
-    return user?.isFromPublicDomain ? 'https://' : `https://www.${Str.extractEmailDomain(session?.email ?? '')}`;
+function getDefaultCompanyWebsite(_session: OnyxEntry<OnyxTypes.Session>, _user: OnyxEntry<OnyxTypes.User>): string {
+    // return user?.isFromPublicDomain ? 'https://' : `https://www.${Str.extractEmailDomain(session?.email ?? '')}`;
+    // temporarily always returns https:// to fix https://github.com/Expensify/App/issues/47227 until https://github.com/Expensify/App/issues/45278 is resolved.
+    return 'https://';
 }
 
 function getLastFourDigits(bankAccountNumber: string): string {
