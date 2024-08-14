d93f16c61a2e04fa2188e7db1ca0410aaf362a7e
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-08-14 17:15:25 +0200

    restore idnex setup

--- src/libs/worker/exampleWorker.ts
+++ src/libs/worker/exampleWorker.ts
@@ -1,15 +1,10 @@
 import * as FilterListUtils from '@libs/FilterListUtils';
 import type {FilterOptionsConfig, Options} from '@libs/OptionsListUtils';
-import initOnyx from '@src/setup/initOnyx';
 import {expose} from './index.web';
-
-// TODO: do this automatically per worker? But not every worker needs onyx - hm.
-initOnyx();
 
 const worker = expose((options: Options, searchInputValue: string, config?: FilterOptionsConfig) => {
     const performanceStart = performance.now();
 
-    // inlinerequire
     const res = FilterListUtils.filterOptions(options, searchInputValue, config);
     const performanceEnd = performance.now();
     console.log(`[hanno] worker finished filtering in ${performanceEnd - performanceStart}ms`);
