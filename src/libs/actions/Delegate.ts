97e581d886b92a1a10bd970f124a3b1d2c078a22
Rushat Gabhane <rushatgabhane@gmail.com>
2024-08-14 15:52:18 +0300

    add onyx data to handle errors

--- src/libs/actions/Delegate.ts
+++ src/libs/actions/Delegate.ts
@@ -1,18 +1,70 @@
 import Onyx from 'react-native-onyx';
+import type {OnyxUpdate} from 'react-native-onyx';
 import * as API from '@libs/API';
 import {SIDE_EFFECT_REQUEST_COMMANDS} from '@libs/API/types';
+import Log from '@libs/Log';
 import * as NetworkStore from '@libs/Network/NetworkStore';
 import * as SequentialQueue from '@libs/Network/SequentialQueue';
+import ONYXKEYS from '@src/ONYXKEYS';
+import type {DelegatedAccess, DelegateRole} from '@src/types/onyx/Account';
 import {openApp} from './App';
 import updateSessionAuthTokens from './Session/updateSessionAuthTokens';
 
-function connect(email: string) {
+let delegatedAccess: DelegatedAccess;
+Onyx.connect({
+    key: ONYXKEYS.ACCOUNT,
+    callback: (val) => {
+        delegatedAccess = val?.delegatedAccess ?? {};
+    },
+});
+
+function connect(email: string, role: DelegateRole) {
+    if (!delegatedAccess?.delegators) {
+        return;
+    }
+
+    const optimisticData: OnyxUpdate[] = [
+        {
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: ONYXKEYS.ACCOUNT,
+            value: {
+                delegatedAccess: {
+                    delegators: delegatedAccess.delegators.map((delegator) => (delegator.email === email ? {email, role, error: undefined} : delegator)),
+                },
+            },
+        },
+    ];
+
+    const successData: OnyxUpdate[] = [
+        {
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: ONYXKEYS.ACCOUNT,
+            value: {
+                delegatedAccess: {
+                    delegators: delegatedAccess.delegators.map((delegator) => (delegator.email === email ? {email, role, error: undefined} : delegator)),
+                },
+            },
+        },
+    ];
+
+    const failureData: OnyxUpdate[] = [
+        {
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: ONYXKEYS.ACCOUNT,
+            value: {
+                delegatedAccess: {
+                    delegators: delegatedAccess.delegators.map((delegator) => (delegator.email === email ? {email, role, error: 'delegate.genericError'} : delegator)),
+                },
+            },
+        },
+    ];
+
     // eslint-disable-next-line rulesdir/no-api-side-effects-method
-    API.makeRequestWithSideEffects(SIDE_EFFECT_REQUEST_COMMANDS.CONNECT_AS_DELEGATE, {to: email}, {})
+    API.makeRequestWithSideEffects(SIDE_EFFECT_REQUEST_COMMANDS.CONNECT_AS_DELEGATE, {to: email}, {optimisticData, successData, failureData})
         .then((response) => {
             if (!response?.restrictedToken || !response?.encryptedAuthToken) {
-                // TODO: Show an error message to the user
-                console.error('Error during connect: No authToken returned');
+                Log.alert('[Delegate] No auth token returned while connecting as a delegate');
+                Onyx.update(failureData);
                 return;
             }
             return SequentialQueue.waitForIdle()
@@ -26,8 +78,8 @@ function connect(email: string) {
                 });
         })
         .catch((error) => {
-            // TODO: Show an error message to the user
-            console.error('Error during connect:', error);
+            Log.alert('[Delegate] Error connecting as delegate', {error});
+            Onyx.update(failureData);
         });
 }
 
