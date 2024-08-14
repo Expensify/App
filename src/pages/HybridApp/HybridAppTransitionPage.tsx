5006922fe6c856444caa1f6241aad982c542b617
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-08-14 15:33:20 +0200

    Refactor params

--- src/pages/HybridApp/HybridAppTransitionPage.tsx
+++ src/pages/HybridApp/HybridAppTransitionPage.tsx
@@ -1,17 +1,14 @@
 import type {StackScreenProps} from '@react-navigation/stack';
 import {useEffect} from 'react';
-import {useOnyx} from 'react-native-onyx';
 import FullScreenLoadingIndicator from '@components/FullscreenLoadingIndicator';
 import {signInOnHybridApp} from '@libs/actions/Session';
 import type {HybridAppPublicScreensParamList} from '@navigation/types';
-import ONYXKEYS from '@src/ONYXKEYS';
 import type SCREENS from '@src/SCREENS';
 
 type HybridAppTransitionPageProps = StackScreenProps<HybridAppPublicScreensParamList, typeof SCREENS.TRANSITION_BETWEEN_APPS>;
 
 function HybridAppTransitionPage({route}: HybridAppTransitionPageProps) {
-    const [account] = useOnyx(ONYXKEYS.ACCOUNT);
-    const {shortLivedAuthToken, email, accountID, exitTo} = route?.params ?? {};
+    const {shortLivedAuthToken, email, accountID} = route?.params ?? {};
 
     useEffect(() => {
         if (!shortLivedAuthToken || !email || !accountID) {
