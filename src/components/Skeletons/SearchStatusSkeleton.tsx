e7ef910917a56ba9f06b7c3b26683e1cef0ea510
Carlos Martins <cmartins@expensify.com>
2024-08-15 13:09:25 -0600

    fix style

--- src/components/Skeletons/SearchStatusSkeleton.tsx
+++ src/components/Skeletons/SearchStatusSkeleton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import {Path, Rect} from 'react-native-svg';
+import {Rect} from 'react-native-svg';
 import SkeletonViewContentLoader from '@components/SkeletonViewContentLoader';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -21,20 +21,13 @@ function SearchStatusSkeleton({shouldAnimate = true}: SearchStatusSkeletonProps)
                 backgroundColor={theme.skeletonLHNIn}
                 foregroundColor={theme.skeletonLHNOut}
             >
-                <Path d="M0 20C0 8.95431 8.95431 0 20 0H48C59.0457 0 68 8.95431 68 20C68 31.0457 59.0457 40 48 40H20C8.9543 40 0 31.0457 0 20Z" />
                 <Rect
-                    x={12}
-                    y={14}
-                    width={12}
-                    height={12}
-                    fill={theme.hoverComponentBG}
-                />
-                <Rect
-                    x={32}
-                    y={16}
-                    width={24}
-                    height={8}
-                    fill={theme.hoverComponentBG}
+                    x={0}
+                    y={0}
+                    rx={20}
+                    ry={20}
+                    width={68}
+                    height={40}
                 />
                 <Rect
                     x={80}
@@ -73,6 +66,27 @@ function SearchStatusSkeleton({shouldAnimate = true}: SearchStatusSkeletonProps)
                     height={8}
                 />
             </SkeletonViewContentLoader>
+            <View style={[styles.pAbsolute]}>
+                <SkeletonViewContentLoader
+                    animate={shouldAnimate}
+                    height={40}
+                    backgroundColor={theme.hoverComponentBG}
+                    foregroundColor={theme.buttonHoveredBG}
+                >
+                    <Rect
+                        x={12}
+                        y={14}
+                        width={12}
+                        height={12}
+                    />
+                    <Rect
+                        x={32}
+                        y={16}
+                        width={24}
+                        height={8}
+                    />
+                </SkeletonViewContentLoader>
+            </View>
         </View>
     );
 }
