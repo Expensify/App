3f574fbc9ccbeb100019a527c8918281224d0fdc
Wojciech Boman <wojciechboman@gmail.com>
2024-08-19 15:57:27 +0200

    Remove unnecessary check from toggling Rules in More Features

--- src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
+++ src/pages/workspace/WorkspaceMoreFeaturesPage.tsx
@@ -134,10 +134,6 @@ function WorkspaceMoreFeaturesPage({policy, route}: WorkspaceMoreFeaturesPagePro
             isActive: policy?.areRulesEnabled ?? false,
             pendingAction: policy?.pendingFields?.areRulesEnabled,
             action: (isEnabled: boolean) => {
-                if (!policyID) {
-                    return;
-                }
-
                 if (isEnabled && !isControlPolicy(policy)) {
                     Navigation.navigate(ROUTES.WORKSPACE_UPGRADE.getRoute(policyID, CONST.UPGRADE_FEATURE_INTRO_MAPPING.rules.alias, ROUTES.WORKSPACE_MORE_FEATURES.getRoute(policyID)));
                     return;
