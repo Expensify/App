24fe82850deb9887a952fb45230c6595943d5c88
Shubham Agrawal <58412969+shubham1206agra@users.noreply.github.com>
2024-08-19 10:28:58 +0530

    Apply suggestions from code review
    Co-authored-by: Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -102,7 +102,7 @@ import type {
 } from '@src/types/onyx';
 import type {Decision} from '@src/types/onyx/OriginalMessage';
 import type {ConnectionName} from '@src/types/onyx/Policy';
-import type {NotificationPreference, Participant, Participants, Participant as ReportParticipant, RoomVisibility, WriteCapability} from '@src/types/onyx/Report';
+import type {NotificationPreference, Participants, Participant as ReportParticipant, RoomVisibility, WriteCapability} from '@src/types/onyx/Report';
 import type Report from '@src/types/onyx/Report';
 import type {Message, ReportActions} from '@src/types/onyx/ReportAction';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
