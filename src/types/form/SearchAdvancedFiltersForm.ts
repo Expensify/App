eed752aa07ecaa7cbf56e7c678aae9a402575e3c
289Adam289 <adam.szwaja@swmansion.com>
2024-08-19 13:21:08 +0200

    fix merge

--- src/types/form/SearchAdvancedFiltersForm.ts
+++ src/types/form/SearchAdvancedFiltersForm.ts
@@ -26,21 +26,21 @@ type InputID = ValueOf<typeof FILTER_KEYS>;
 type SearchAdvancedFiltersForm = Form<
     InputID,
     {
-        [INPUT_IDS.DATE_AFTER]: string;
-        [INPUT_IDS.DATE_BEFORE]: string;
-        [INPUT_IDS.CURRENCY]: string[];
-        [INPUT_IDS.CATEGORY]: string[];
-        [INPUT_IDS.POLICY_ID]: string;
-        [INPUT_IDS.CARD_ID]: string[];
-        [INPUT_IDS.MERCHANT]: string;
-        [INPUT_IDS.DESCRIPTION]: string;
-        [INPUT_IDS.REPORT_ID]: string;
-        [INPUT_IDS.LESS_THAN]: string;
-        [INPUT_IDS.GREATER_THAN]: string;
-        [INPUT_IDS.KEYWORD]: string;
-        [INPUT_IDS.TAX_RATE]: string[];
-        [INPUT_IDS.EXPENSE_TYPE]: string[];
-        [INPUT_IDS.TAG]: string[];
+        [FILTER_KEYS.DATE_AFTER]: string;
+        [FILTER_KEYS.DATE_BEFORE]: string;
+        [FILTER_KEYS.CURRENCY]: string[];
+        [FILTER_KEYS.CATEGORY]: string[];
+        [FILTER_KEYS.POLICY_ID]: string;
+        [FILTER_KEYS.CARD_ID]: string[];
+        [FILTER_KEYS.MERCHANT]: string;
+        [FILTER_KEYS.DESCRIPTION]: string;
+        [FILTER_KEYS.REPORT_ID]: string;
+        [FILTER_KEYS.LESS_THAN]: string;
+        [FILTER_KEYS.GREATER_THAN]: string;
+        [FILTER_KEYS.KEYWORD]: string;
+        [FILTER_KEYS.TAX_RATE]: string[];
+        [FILTER_KEYS.EXPENSE_TYPE]: string[];
+        [FILTER_KEYS.TAG]: string[];
         [FILTER_KEYS.FROM]: string[];
         [FILTER_KEYS.TO]: string[];
     }
