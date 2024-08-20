d1d07dd9b4c17ed23694a2f8ca593ce92251d9c9
Artem Makushov <waterim3009@gmail.com>
2024-08-20 17:06:21 +0200

    add new illustration

--- src/components/Icon/Illustrations.ts
+++ src/components/Icon/Illustrations.ts
@@ -1,6 +1,7 @@
 import EmptyCardState from '@assets/images/emptystate__expensifycard.svg';
 import ExpensifyCardIllustration from '@assets/images/expensifyCard/cardIllustration.svg';
 import LaptopwithSecondScreenandHourglass from '@assets/images/LaptopwithSecondScreenandHourglass.svg';
+import MagnifyingGlassMoney from '@assets/images/magnifyingglass-money.svg';
 import Abracadabra from '@assets/images/product-illustrations/abracadabra.svg';
 import BankArrowPink from '@assets/images/product-illustrations/bank-arrow--pink.svg';
 import BankMouseGreen from '@assets/images/product-illustrations/bank-mouse--green.svg';
@@ -218,4 +219,5 @@ export {
     Tire,
     BigVault,
     Filters,
+    MagnifyingGlassMoney,
 };
