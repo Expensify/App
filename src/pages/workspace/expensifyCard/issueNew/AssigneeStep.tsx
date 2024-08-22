7a3d786d9ad3109e81dd36d72234f400e6f0b4a1
Agata Kosior <agata.kosior57@gmail.com>
2024-08-22 10:18:54 +0200

    fix: lint

--- src/pages/workspace/expensifyCard/issueNew/AssigneeStep.tsx
+++ src/pages/workspace/expensifyCard/issueNew/AssigneeStep.tsx
@@ -22,7 +22,6 @@ import Navigation from '@navigation/Navigation';
 import * as Card from '@userActions/Card';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import ROUTES from '@src/ROUTES';
 import type * as OnyxTypes from '@src/types/onyx';
 
 const MINIMUM_MEMBER_TO_SHOW_SEARCH = 8;
