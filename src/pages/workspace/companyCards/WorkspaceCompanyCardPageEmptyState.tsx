172be7331b2a44839e9ffd26d79e0ba4e0ea375a
Artem Makushov <waterim3009@gmail.com>
2024-08-23 19:43:23 +0200

    update empty state, remove unused component and props

--- src/pages/workspace/companyCards/WorkspaceCompanyCardPageEmptyState.tsx
+++ src/pages/workspace/companyCards/WorkspaceCompanyCardPageEmptyState.tsx
@@ -1,4 +1,3 @@
-import type {StackScreenProps} from '@react-navigation/stack';
 import React, {useCallback} from 'react';
 import {View} from 'react-native';
 import FeatureList from '@components/FeatureList';
@@ -8,12 +7,7 @@ import useLocalize from '@hooks/useLocalize';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
-import type {FullScreenNavigatorParamList} from '@libs/Navigation/types';
-import type {WithPolicyAndFullscreenLoadingProps} from '@pages/workspace/withPolicyAndFullscreenLoading';
 import withPolicyAndFullscreenLoading from '@pages/workspace/withPolicyAndFullscreenLoading';
-import WorkspacePageWithSections from '@pages/workspace/WorkspacePageWithSections';
-import CONST from '@src/CONST';
-import type SCREENS from '@src/SCREENS';
 
 const companyCardFeatures: FeatureListItem[] = [
     {
@@ -29,11 +23,8 @@ const companyCardFeatures: FeatureListItem[] = [
         translationKey: 'workspace.moreFeatures.companyCards.feed.features.automaticImport',
     },
 ];
-type WorkspaceCompanyCardPageEmptyStateProps = {
-    route: StackScreenProps<FullScreenNavigatorParamList, typeof SCREENS.WORKSPACE.COMPANY_CARDS>['route'];
-} & WithPolicyAndFullscreenLoadingProps;
 
-function WorkspaceCompanyCardPageEmptyState({route}: WorkspaceCompanyCardPageEmptyStateProps) {
+function WorkspaceCompanyCardPageEmptyState() {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
     const theme = useTheme();
@@ -44,29 +35,20 @@ function WorkspaceCompanyCardPageEmptyState({route}: WorkspaceCompanyCardPageEmp
     }, []);
 
     return (
-        <WorkspacePageWithSections
-            shouldUseScrollView
-            icon={Illustrations.HandCard}
-            headerText={translate('workspace.common.companyCards')}
-            route={route}
-            guidesCallTaskID={CONST.GUIDES_CALL_TASK_IDS.WORKSPACE_EXPENSIFY_CARD}
-            shouldShowOfflineIndicatorInWideScreen
-        >
-            <View style={[styles.mt3, shouldUseNarrowLayout ? styles.workspaceSectionMobile : styles.workspaceSection]}>
-                <FeatureList
-                    menuItems={companyCardFeatures}
-                    title={translate('workspace.moreFeatures.companyCards.feed.title')}
-                    subtitle={translate('workspace.moreFeatures.companyCards.subtitle')}
-                    ctaText={translate('workspace.moreFeatures.companyCards.feed.ctaTitle')}
-                    ctaAccessibilityLabel={translate('workspace.moreFeatures.companyCards.feed.ctaTitle')}
-                    onCtaPress={startFlow}
-                    illustrationBackgroundColor={theme.fallbackIconColor}
-                    illustration={Illustrations.ExpensifyCardIllustration}
-                    illustrationStyle={styles.expensifyCardIllustrationContainer}
-                    titleStyles={styles.textHeadlineH1}
-                />
-            </View>
-        </WorkspacePageWithSections>
+        <View style={[styles.mt3, shouldUseNarrowLayout ? styles.workspaceSectionMobile : styles.workspaceSection]}>
+            <FeatureList
+                menuItems={companyCardFeatures}
+                title={translate('workspace.moreFeatures.companyCards.feed.title')}
+                subtitle={translate('workspace.moreFeatures.companyCards.subtitle')}
+                ctaText={translate('workspace.moreFeatures.companyCards.feed.ctaTitle')}
+                ctaAccessibilityLabel={translate('workspace.moreFeatures.companyCards.feed.ctaTitle')}
+                onCtaPress={startFlow}
+                illustrationBackgroundColor={theme.fallbackIconColor}
+                illustration={Illustrations.CompanyCardsIllustration}
+                illustrationStyle={styles.expensifyCardIllustrationContainer}
+                titleStyles={styles.textHeadlineH1}
+            />
+        </View>
     );
 }
 
