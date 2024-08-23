871933cdaf9f010b37cbc2efc2d2965818597d56
Agata Kosior <agata.kosior57@gmail.com>
2024-08-23 19:27:25 +0200

    feat: add translations

--- src/languages/types.ts
+++ src/languages/types.ts
@@ -358,6 +358,11 @@ type ApprovalWorkflowErrorParams = {
     name2: string;
 };
 
+type AssignCardParams = {
+    assignee: string;
+    feed: string;
+};
+
 export type {
     AddressLineParams,
     AdminCanceledRequestParams,
@@ -481,4 +486,5 @@ export type {
     RemoveMembersWarningPrompt,
     DeleteExpenseTranslationParams,
     ApprovalWorkflowErrorParams,
+    AssignCardParams,
 };
