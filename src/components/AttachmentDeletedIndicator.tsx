01003146a058a2a1048164f67889947ec1a14ba6
krishna2323 <belivethatkg@gmail.com>
2024-08-25 03:10:05 +0530

    add AttachmentDeletedIndicator.tsx
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

---
+++ src/components/AttachmentDeletedIndicator.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {View} from 'react-native';
+import type {StyleProp, ViewStyle} from 'react-native';
+import useNetwork from '@hooks/useNetwork';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
+import variables from '@styles/variables';
+import Icon from './Icon';
+import * as Expensicons from './Icon/Expensicons';
+
+type AttachmentOfflineIndicatorProps = {
+    /** Any additional styles to apply */
+    containerStyles?: StyleProp<ViewStyle>;
+};
+
+function AttachmentDeletedIndicator({containerStyles}: AttachmentOfflineIndicatorProps) {
+    const theme = useTheme();
+    const styles = useThemeStyles();
+    const {isOffline} = useNetwork();
+
+    if (!isOffline) {
+        return null;
+    }
+
+    return (
+        <>
+            <View
+                style={[styles.pAbsolute, styles.alignItemsCenter, styles.justifyContentCenter, styles.highlightBG, styles.deletedIndicatorOverlay, styles.deletedIndicator, containerStyles]}
+            />
+            <View style={[styles.pAbsolute, styles.deletedIndicator, styles.alignItemsCenter, styles.justifyContentCenter, containerStyles]}>
+                <Icon
+                    fill={theme.iconHovered}
+                    src={Expensicons.Trashcan}
+                    width={variables.iconSizeSuperLarge}
+                    height={variables.iconSizeSuperLarge}
+                />
+            </View>
+        </>
+    );
+}
+
+AttachmentDeletedIndicator.displayName = 'AttachmentDeletedIndicator';
+
+export default AttachmentDeletedIndicator;
