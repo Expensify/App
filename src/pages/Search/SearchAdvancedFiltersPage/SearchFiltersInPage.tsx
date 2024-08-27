2499a0d60d4b597c88147b07a143891592e3a55f
Wiktor Gut <wiktor.gut@swmansion.com>
2024-08-27 16:05:00 +0200

    fix Navigation.goBack in filters

--- src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersInPage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersInPage.tsx
@@ -6,8 +6,10 @@ import ScreenWrapper from '@components/ScreenWrapper';
 import SearchFiltersChatsSelector from '@components/Search/SearchFiltersChatsSelector';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
+import Navigation from '@libs/Navigation/Navigation';
 import * as SearchActions from '@userActions/Search';
 import ONYXKEYS from '@src/ONYXKEYS';
+import ROUTES from '@src/ROUTES';
 
 function SearchFiltersInPage() {
     const styles = useThemeStyles();
@@ -28,7 +30,12 @@ function SearchFiltersInPage() {
             onEntryTransitionEnd={handleScreenTransitionEnd}
             shouldEnableMaxHeight
         >
-            <HeaderWithBackButton title={translate('common.in')} />
+            <HeaderWithBackButton
+                title={translate('common.in')}
+                onBackButtonPress={() => {
+                    Navigation.goBack(ROUTES.SEARCH_ADVANCED_FILTERS);
+                }}
+            />
             <View style={[styles.flex1]}>
                 <SearchFiltersChatsSelector
                     isScreenTransitionEnd={isScreenTransitionEnd}
