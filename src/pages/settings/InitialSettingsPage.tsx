9b548fa432969a8e77f74970ae7a681349303274
war-in <war-in@users.noreply.github.com>
2024-08-27 15:36:05 +0200

    add isSwitchingToOldDot param

--- src/pages/settings/InitialSettingsPage.tsx
+++ src/pages/settings/InitialSettingsPage.tsx
@@ -2,7 +2,7 @@ import {useRoute} from '@react-navigation/native';
 import React, {useCallback, useContext, useEffect, useLayoutEffect, useMemo, useRef, useState} from 'react';
 // eslint-disable-next-line no-restricted-imports
 import type {GestureResponderEvent, ScrollView as RNScrollView, ScrollViewProps, StyleProp, ViewStyle} from 'react-native';
-import {View} from 'react-native';
+import {NativeModules, View} from 'react-native';
 import type {OnyxCollection, OnyxEntry} from 'react-native-onyx';
 import {useOnyx, withOnyx} from 'react-native-onyx';
 import type {ValueOf} from 'type-fest';
@@ -151,8 +151,7 @@ function InitialSettingsPage({session, userWallet, bankAccountList, fundList, wa
                     ...(NativeModules.HybridAppModule
                         ? {
                               action: () => {
-                                  Navigation.resetToHome();
-                                  NativeModules.HybridAppModule.closeReactNativeApp(false);
+                                  NativeModules.HybridAppModule.closeReactNativeApp(false, true);
                               },
                           }
                         : {
