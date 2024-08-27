95d60be42befa1d2bbb7ef0f0ee449f028e87030
daledah <daleda261@outlook.com>
2024-08-27 13:59:11 +0700

    fix: remove transaction after navigate

--- src/pages/ReportDetailsPage.tsx
+++ src/pages/ReportDetailsPage.tsx
@@ -697,7 +697,8 @@ function ReportDetailsPage({policies, report, session, personalDetails}: ReportD
         if (ReportActionsUtils.isTrackExpenseAction(requestParentReportAction)) {
             navigateBackToAfterDelete.current = IOU.deleteTrackExpense(moneyRequestReport?.reportID ?? '', iouTransactionID, requestParentReportAction, isSingleTransactionView);
         } else {
-            navigateBackToAfterDelete.current = IOU.deleteMoneyRequest(iouTransactionID, requestParentReportAction, isSingleTransactionView);
+            const {urlToNavigateBack} = IOU.prepareToCleanUpMoneyRequest(iouTransactionID, requestParentReportAction, true);
+            navigateBackToAfterDelete.current = urlToNavigateBack;
         }
 
         isTransactionDeleted.current = true;
@@ -805,6 +806,9 @@ function ReportDetailsPage({policies, report, session, personalDetails}: ReportD
                             Navigation.dismissModal();
                         } else {
                             ReportUtils.navigateBackAfterDeleteTransaction(navigateBackToAfterDelete.current, true);
+                            setTimeout(() => {
+                                IOU.deleteMoneyRequest(iouTransactionID, requestParentReportAction, isSingleTransactionView);
+                            }, CONST.ANIMATED_TRANSITION);
                         }
                     }}
                     prompt={caseID === CASES.DEFAULT ? translate('task.deleteConfirmation') : translate('iou.deleteConfirmation')}
