1dae4ddbc28752b8a5dc0a8ea39c23675e7768b2
Christoph Pader <chris@margelo.io>
2024-08-30 11:11:05 +0200

    use ios like animations on android

--- src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/slideFromRight/index.android.ts
+++ src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/slideFromRight/index.android.ts
@@ -1,6 +1,5 @@
 import type {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 
-// `slide_from_right` is resolved to `default` transition on iOS, but this transition causes issues on iOS
-const transition: NativeStackNavigationOptions = {animation: 'slide_from_right'};
+const transition: NativeStackNavigationOptions = {animation: 'ios_from_right'};
 
 export default transition;
