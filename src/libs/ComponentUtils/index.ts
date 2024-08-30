a7ba9164675f72d2477c74c28863e161bbcdecbe
Taras Perun <terry.perun@gmail.com>
2024-08-30 14:28:28 +0200

    update forceClearInput for web

--- src/libs/ComponentUtils/index.ts
+++ src/libs/ComponentUtils/index.ts
@@ -1,4 +1,3 @@
-import type {Component} from 'react';
 import type {TextInput} from 'react-native';
 import type {AnimatedRef} from 'react-native-reanimated';
 import type {AccessibilityRoleForm, NewPasswordAutocompleteType, PasswordAutocompleteType} from './types';
@@ -10,10 +9,10 @@ const PASSWORD_AUTOCOMPLETE_TYPE: PasswordAutocompleteType = 'current-password';
 const NEW_PASSWORD_AUTOCOMPLETE_TYPE: NewPasswordAutocompleteType = 'new-password';
 const ACCESSIBILITY_ROLE_FORM: AccessibilityRoleForm = 'form';
 
-function forceClearInput(_: AnimatedRef<Component>, textInputRef: React.RefObject<TextInput>) {
+function forceClearInput(animatedInputRef: AnimatedRef<TextInput>) {
     'worklet';
 
-    textInputRef.current?.clear();
+    animatedInputRef.current?.clear();
 }
 
 export {PASSWORD_AUTOCOMPLETE_TYPE, ACCESSIBILITY_ROLE_FORM, NEW_PASSWORD_AUTOCOMPLETE_TYPE, forceClearInput};
