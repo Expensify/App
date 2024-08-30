cda29f7153d3c66618c7875190680d137682b098
Taras Perun <terry.perun@gmail.com>
2024-08-30 14:28:48 +0200

    type

--- src/libs/ComponentUtils/index.ts
+++ src/libs/ComponentUtils/index.ts
@@ -1,4 +1,4 @@
-import type {TextInput} from 'react-native';
+import type {Component} from 'react';
 import type {AnimatedRef} from 'react-native-reanimated';
 import type {AccessibilityRoleForm, NewPasswordAutocompleteType, PasswordAutocompleteType} from './types';
 
@@ -9,10 +9,13 @@ const PASSWORD_AUTOCOMPLETE_TYPE: PasswordAutocompleteType = 'current-password';
 const NEW_PASSWORD_AUTOCOMPLETE_TYPE: NewPasswordAutocompleteType = 'new-password';
 const ACCESSIBILITY_ROLE_FORM: AccessibilityRoleForm = 'form';
 
-function forceClearInput(animatedInputRef: AnimatedRef<TextInput>) {
+function forceClearInput(animatedInputRef: AnimatedRef<Component>) {
     'worklet';
 
-    animatedInputRef.current?.clear();
+    const input = animatedInputRef.current;
+    if (input && 'clear' in input && typeof input.clear === 'function') {
+        input.clear();
+    }
 }
 
 export {PASSWORD_AUTOCOMPLETE_TYPE, ACCESSIBILITY_ROLE_FORM, NEW_PASSWORD_AUTOCOMPLETE_TYPE, forceClearInput};
