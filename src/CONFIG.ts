8650119a8f68c10c34dcd0679c959d9031b9cbc3
Tomasz Lesniakiewicz <tlesniakiewicz@gmail.com>
2024-08-30 17:20:15 +0200

    add fb config

--- src/CONFIG.ts
+++ src/CONFIG.ts
@@ -97,4 +97,14 @@ export default {
     },
     GCP_GEOLOCATION_API_KEY: googleGeolocationAPIKey,
     USE_REACT_STRICT_MODE: true,
+    // TODO add newly created Firebase web project details before merging
+    FIREBASE_WEB_CONFIG: {
+        apiKey: 'YOUR_API_KEY',
+        authDomain: 'YOUR_AUTH_DOMAIN',
+        projectId: 'YOUR_PROJECT_ID',
+        storageBucket: 'YOUR_STORAGE_BUCKET',
+        messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+        appId: 'YOUR_APP_ID',
+        measurementId: 'YOUR_MEASUREMENT_ID',
+    },
 } as const;
--- src/libs/Firebase/firebaseWebConfig.ts
+++ src/libs/Firebase/firebaseWebConfig.ts
@@ -1,17 +1,8 @@
 import {initializeApp} from '@firebase/app';
 import {getPerformance, initializePerformance} from '@firebase/performance';
+import Config from '@src/CONFIG';
 
-// TODO add newly created Firebase web project details before merging
-
-const firebaseConfig = {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID',
-    measurementId: 'YOUR_MEASUREMENT_ID',
-};
+const firebaseConfig = Config.FIREBASE_WEB_CONFIG;
 const firebaseApp = initializeApp(firebaseConfig);
 
 initializePerformance(firebaseApp, {dataCollectionEnabled: true});
