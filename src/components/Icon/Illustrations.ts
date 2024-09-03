eb1a18d1fff2efac4db67ef099a3f0199d23b471
Artem Makushov <waterim3009@gmail.com>
2024-09-03 17:42:29 +0200

    new asset

--- src/components/Icon/Illustrations.ts
+++ src/components/Icon/Illustrations.ts
@@ -1,6 +1,7 @@
 import AmexCompanyCards from '@assets/images/companyCards/amex.svg';
 import CompanyCardsEmptyState from '@assets/images/companyCards/emptystate__card-pos.svg';
 import MasterCardCompanyCards from '@assets/images/companyCards/mastercard.svg';
+import CompanyCardsPendingState from '@assets/images/companyCards/pendingstate_laptop-with-hourglass-and-cards.svg';
 import VisaCompanyCards from '@assets/images/companyCards/visa.svg';
 import EmptyCardState from '@assets/images/emptystate__expensifycard.svg';
 import ExpensifyCardIllustration from '@assets/images/expensifyCard/cardIllustration.svg';
@@ -230,4 +231,5 @@ export {
     AmexCompanyCards,
     MasterCardCompanyCards,
     VisaCompanyCards,
+    CompanyCardsPendingState,
 };
