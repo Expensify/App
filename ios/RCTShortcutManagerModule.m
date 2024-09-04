f7c30b069c8eab1945877ee549f308fe74170693
Michał Skałka <michal.skalka@swmansion.com>
2024-09-04 10:32:18 +0200

    Add iOS native module

---
+++ ios/RCTShortcutManagerModule.m
@@ -0,0 +1,10 @@
+// RCTCalendarModule.m
+#import "RCTShortcutManagerModule.h"
+
+@implementation RCTShortcutManagerModule
+
+RCT_EXPORT_METHOD(removeAllDynamicShortcuts){}
+
+RCT_EXPORT_MODULE(ShortcutManager);
+
+@end
