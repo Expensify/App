79192b27d15e72f0c643650eb5f52ef9bd177769
Gandalf <whogandalf@gmail.com>
2024-09-06 03:28:34 +0530

    Update Permissions.ts

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -4,7 +4,7 @@ import type {IOUType} from '@src/CONST';
 import type Beta from '@src/types/onyx/Beta';
 
 function canUseAllBetas(betas: OnyxEntry<Beta[]>): boolean {
-    return true;
+    return !!betas?.includes(CONST.BETAS.ALL);
 }
 
 function canUseDefaultRooms(betas: OnyxEntry<Beta[]>): boolean {
