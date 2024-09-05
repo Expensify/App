a37a6e42a52b636fc0e4f8fd6d1bbe2e70d78f9e
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-09-05 23:17:01 +0700

    undo unintentional change

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -5757,7 +5757,6 @@ function isUnread(report: OnyxEntry<Report>): boolean {
     if (isEmptyReport(report) && !isSelfDM(report)) {
         return false;
     }
-
     // lastVisibleActionCreated and lastReadTime are both datetime strings and can be compared directly
     const lastVisibleActionCreated = report.lastVisibleActionCreated ?? '';
     const lastReadTime = report.lastReadTime ?? '';
