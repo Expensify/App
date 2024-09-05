db25e268bba13cf6f530f6eddb7da24191a60ff5
Wildan Muhlis <muhliswildan@gmail.com>
2024-09-05 13:27:21 +0700

    Fix getPolicyName when report / policies not ready

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -739,13 +739,17 @@ const unavailableTranslation = Localize.translateLocal('workspace.common.unavail
  */
 function getPolicyName(report: OnyxInputOrEntry<Report>, returnEmptyIfNotFound = false, policy?: OnyxInputOrEntry<Policy>): string {
     const noPolicyFound = returnEmptyIfNotFound ? '' : unavailableTranslation;
-    if (isEmptyObject(report)) {
+    
+    // Ensure policies and report are fully loaded before proceeding.
+    // On initial login, they may be partially loaded, so additional checks are necessary to prevent unexpected behavior.
+    if (isEmptyObject(report) || !report.reportID || isEmptyObject(allPolicies)) {
         return noPolicyFound;
     }
-
-    if ((!allPolicies || Object.keys(allPolicies).length === 0) && !report?.policyName) {
+    
+    if (isEmptyObject(allPolicies) && !report?.policyName) {
         return unavailableTranslation;
     }
+
     const finalPolicy = policy ?? allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${report?.policyID}`];
 
     const parentReport = getRootParentReport(report);
