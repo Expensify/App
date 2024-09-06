ec6691152b3e3795b786d1a58f9cdbc6d423ea54
rory <rory@expensify.com>
2024-09-05 17:24:46 -0700

    Apply GitHub Actions build diff

--- .github/actions/javascript/createOrUpdateStagingDeploy/index.js
+++ .github/actions/javascript/createOrUpdateStagingDeploy/index.js
@@ -15385,11 +15385,11 @@ module.exports = _interopRequireDefault, module.exports.__esModule = true, modul
 function _typeof(o) {
   "@babel/helpers - typeof";
 
-  return (module.exports = _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) {
+  return module.exports = _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) {
     return typeof o;
   } : function (o) {
     return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o;
-  }, module.exports.__esModule = true, module.exports["default"] = module.exports), _typeof(o);
+  }, module.exports.__esModule = true, module.exports["default"] = module.exports, _typeof(o);
 }
 module.exports = _typeof, module.exports.__esModule = true, module.exports["default"] = module.exports;
 
