b9d18659ec3cd3b99de220f42164f339d2905298
tienifr <christianwen18@gmail.com>
2024-09-06 18:11:34 +0700

    lint fix

--- src/components/Tooltip/EducationalTooltip/measureTooltipCoordinate/index.android.ts
+++ src/components/Tooltip/EducationalTooltip/measureTooltipCoordinate/index.android.ts
@@ -1,11 +1,7 @@
 import type React from 'react';
 import type {LayoutRectangle, NativeMethods} from 'react-native';
 
-export default function measureTooltipCoordinate(
-    target: React.Component<unknown, {}, any> & Readonly<NativeMethods>,
-    updateTargetBounds: (rect: LayoutRectangle) => void,
-    showTooltip: () => void,
-) {
+export default function measureTooltipCoordinate(target: React.Component & Readonly<NativeMethods>, updateTargetBounds: (rect: LayoutRectangle) => void, showTooltip: () => void) {
     return target?.measure((x, y, width, height, px, py) => {
         updateTargetBounds({height, width, x: px, y: py});
         showTooltip();
--- src/components/Tooltip/EducationalTooltip/measureTooltipCoordinate/index.ts
+++ src/components/Tooltip/EducationalTooltip/measureTooltipCoordinate/index.ts
@@ -1,11 +1,7 @@
 import type React from 'react';
 import type {LayoutRectangle, NativeMethods} from 'react-native';
 
-export default function measureTooltipCoordinate(
-    target: React.Component<unknown, {}, any> & Readonly<NativeMethods>,
-    updateTargetBounds: (rect: LayoutRectangle) => void,
-    showTooltip: () => void,
-) {
+export default function measureTooltipCoordinate(target: React.Component & Readonly<NativeMethods>, updateTargetBounds: (rect: LayoutRectangle) => void, showTooltip: () => void) {
     return target?.measureInWindow((x, y, width, height) => {
         updateTargetBounds({height, width, x, y});
         showTooltip();
