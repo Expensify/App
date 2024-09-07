5109afa0d9f42657f544e9acbf0157e6196c70fc
Hans <visaocuoitroi193@gmail.com>
2024-09-07 21:02:42 +0700

    address comment

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -1395,7 +1395,6 @@ function isJoinRequestInAdminRoom(report: OnyxEntry<Report>): boolean {
 /**
  * Checks if the user has auditor permission in the provided report
  */
-
 function isAuditor(report: OnyxEntry<Report>): boolean {
     if (Array.isArray(report?.permissions) && report?.permissions.length > 0) {
         return report?.permissions?.includes(CONST.REPORT.PERMISSIONS.AUDITOR);
