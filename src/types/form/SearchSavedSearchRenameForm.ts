cebf87d070d03bf8b12e2fd74af6426aba165d4c
Lucien Akchoté <lucien@expensify.com>
2024-09-10 10:30:51 +0200

    add distinct name

--- src/types/form/SearchSavedSearchRenameForm.ts
+++ src/types/form/SearchSavedSearchRenameForm.ts
@@ -2,7 +2,7 @@ import type {ValueOf} from 'type-fest';
 import type Form from './Form';
 
 const INPUT_IDS = {
-    NAME: 'name',
+    NAME: 'savedSearchNewName',
 } as const;
 
 type InputID = ValueOf<typeof INPUT_IDS>;
