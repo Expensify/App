2ffd307c91561e233267d55e304f9b8a78ce94e4
Blazej Kustra <blazej.kustra@swmansion.com>
2024-09-10 14:13:22 +0200

    Create a file with withOnyx

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,30 @@
 /* eslint-disable no-console */
 console.log('CONST');
+
+export default withOnyx<ExpensifyProps, ExpensifyOnyxProps>({
+    isCheckingPublicRoom: {
+        key: ONYXKEYS.IS_CHECKING_PUBLIC_ROOM,
+        initWithStoredValues: false,
+    },
+    updateAvailable: {
+        key: ONYXKEYS.UPDATE_AVAILABLE,
+        initWithStoredValues: false,
+    },
+    updateRequired: {
+        key: ONYXKEYS.UPDATE_REQUIRED,
+        initWithStoredValues: false,
+    },
+    isSidebarLoaded: {
+        key: ONYXKEYS.IS_SIDEBAR_LOADED,
+    },
+    screenShareRequest: {
+        key: ONYXKEYS.SCREEN_SHARE_REQUEST,
+    },
+    focusModeNotification: {
+        key: ONYXKEYS.FOCUS_MODE_NOTIFICATION,
+        initWithStoredValues: false,
+    },
+    lastVisitedPath: {
+        key: ONYXKEYS.LAST_VISITED_PATH,
+    },
+})(Expensify);
