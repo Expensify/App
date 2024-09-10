901483c94185965542ca49896cd85c03c67ce16d
Manan Jadhav <manan.jadhav@gmail.com>
2024-09-11 00:57:26 +0530

    fix: condition when connection is empty

--- src/pages/workspace/accounting/netsuite/NetSuiteTokenInput/NetSuiteTokenInputPage.tsx
+++ src/pages/workspace/accounting/netsuite/NetSuiteTokenInput/NetSuiteTokenInputPage.tsx
@@ -67,6 +67,7 @@ function NetSuiteTokenInputPage({policy}: WithPolicyConnectionsProps) {
             connectionName={CONST.POLICY.CONNECTIONS.NAME.NETSUITE}
             onBackButtonPress={handleBackButtonPress}
             shouldIncludeSafeAreaPaddingBottom
+            shouldLoadForEmptyConnection
             shouldBeBlocked={shouldPageBeBlocked}
         >
             <View style={[styles.ph5, styles.mb3, styles.mt3, {height: CONST.BANK_ACCOUNT.STEPS_HEADER_HEIGHT}]}>
