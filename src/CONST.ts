fb791eb9513ceb14d7ea5262e99fdf20a186ea62
Marc Glasser <marc.aaron.glasser@gmail.com>
2024-09-13 10:56:19 -1000

    Move so that tags is next to categories task

--- src/CONST.ts
+++ src/CONST.ts
@@ -4616,6 +4616,25 @@ const CONST = {
                         `[Take me to workspace category settings](${workspaceCategoriesLink}).`,
                 },
                 {
+                    type: 'setupTags',
+                    autoCompleted: false,
+                    title: 'Set up tags (optional)',
+                    description: ({workspaceMoreFeaturesLink}) =>
+                        'Tags can be used if you want more details with every expense. Use tags for projects, clients, locations, departments, and more. If you need multiple levels of tags you can upgrade to a control plan.\n' +
+                        '\n' +
+                        '*Here’s how to setup tags:*\n' +
+                        '\n' +
+                        '1. Click your profile picture.\n' +
+                        '2. Go to Workspaces.\n' +
+                        '3. Select your workspace.\n' +
+                        '4. Click More features.\n' +
+                        '5. Enable tags.\n' +
+                        '6. Navigate to Tags in the workspace editor.\n' +
+                        '7. In Tags, click + Add tag to make your own.\n' +
+                        '\n' +
+                        `*[Take me to more features](${workspaceMoreFeaturesLink})*`,
+                },
+                {
                     type: 'addExpenseApprovals',
                     autoCompleted: false,
                     title: 'Add expense approvals',
@@ -4651,25 +4670,6 @@ const CONST = {
                         '6. Add an invite message if you want.\n' +
                         '\n' +
                         `[Take me to workspace members](${workspaceMembersLink}). That’s it, happy expensing! :)`,
-                },
-                {
-                    type: 'setupTags',
-                    autoCompleted: false,
-                    title: 'Set up tags (optional)',
-                    description: ({workspaceMoreFeaturesLink}) =>
-                        'Tags can be used if you want more details with every expense. Use tags for projects, clients, locations, departments, and more. If you need multiple levels of tags you can upgrade to a control plan.\n' +
-                        '\n' +
-                        '*Here’s how to setup tags:*\n' +
-                        '\n' +
-                        '1. Click your profile picture.\n' +
-                        '2. Go to Workspaces.\n' +
-                        '3. Select your workspace.\n' +
-                        '4. Click More features.\n' +
-                        '5. Enable tags.\n' +
-                        '6. Navigate to Tags in the workspace editor.\n' +
-                        '7. In Tags, click + Add tag to make your own.\n' +
-                        '\n' +
-                        `*[Take me to more features](${workspaceMoreFeaturesLink})*`,
                 },
             ],
         },
