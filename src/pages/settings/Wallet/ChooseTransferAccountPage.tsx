0439b299d4b96df9e0abcc964667ba02a221ab36
Gandalf <whogandalf@gmail.com>
2024-09-17 19:04:29 +0530

    refactor wallet bank transfer page to use SelectionList

--- src/pages/settings/Wallet/ChooseTransferAccountPage.tsx
+++ src/pages/settings/Wallet/ChooseTransferAccountPage.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import type {GestureResponderEvent} from 'react-native';
 import {View} from 'react-native';
 import type {OnyxEntry} from 'react-native-onyx';
-import {withOnyx} from 'react-native-onyx';
+import {useOnyx, withOnyx} from 'react-native-onyx';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import Icon from '@components/Icon';
+import getBankIcon from '@components/Icon/BankIcons';
 import * as Expensicons from '@components/Icon/Expensicons';
 import MenuItem from '@components/MenuItem';
 import ScreenWrapper from '@components/ScreenWrapper';
+import SelectionList from '@components/SelectionList';
+import RadioListItem from '@components/SelectionList/RadioListItem';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {getLastFourDigits} from '@libs/BankAccountUtils';
+import * as CardUtils from '@libs/CardUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import * as BankAccounts from '@userActions/BankAccounts';
 import * as PaymentMethods from '@userActions/PaymentMethods';
@@ -16,6 +22,7 @@ import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type {AccountData, WalletTransfer} from '@src/types/onyx';
+import type {BankName} from '@src/types/onyx/Bank';
 import PaymentMethodList from './PaymentMethodList';
 
 type ChooseTransferAccountPageOnyxProps = {
@@ -50,6 +57,36 @@ function ChooseTransferAccountPage({walletTransfer = {}}: ChooseTransferAccountP
         BankAccounts.openPersonalBankAccountSetupView();
     };
 
+    const [bankAccountsList] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST);
+
+    const data = useMemo(() => {
+        const options = Object.values(bankAccountsList || {}).map((bankAccount) => {
+            const bankName = (bankAccount.accountData?.additionalData?.bankName ?? '') as BankName;
+            const bankAccountNumber = bankAccount.accountData?.accountNumber ?? '';
+            const bankAccountID = bankAccount.accountData?.bankAccountID ?? bankAccount.methodID;
+            const {icon, iconSize, iconStyles} = getBankIcon({bankName, styles});
+            return {
+                value: bankAccountID,
+                text: bankAccount.title,
+                leftElement: icon && (
+                    <View style={[styles.flexRow, styles.alignItemsCenter, styles.mr3]}>
+                        <Icon
+                            src={icon}
+                            width={iconSize}
+                            height={iconSize}
+                            additionalStyles={iconStyles}
+                        />
+                    </View>
+                ),
+                alternateText: `${translate('workspace.expensifyCard.accountEndingIn')} ${getLastFourDigits(bankAccountNumber)}`,
+                keyForList: bankAccountID?.toString(),
+                isSelected: bankAccountID?.toString() === walletTransfer?.selectedAccountID,
+                bankAccount: bankAccount,
+            };
+        });
+        return options;
+    }, [bankAccountsList, walletTransfer?.selectedAccountID, styles, translate]);
+
     return (
         <ScreenWrapper testID={ChooseTransferAccountPage.displayName}>
             <HeaderWithBackButton
@@ -57,14 +94,16 @@ function ChooseTransferAccountPage({walletTransfer = {}}: ChooseTransferAccountP
                 onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_WALLET_TRANSFER_BALANCE)}
             />
             <View style={[styles.mt3, styles.flexShrink1, styles.flexBasisAuto]}>
-                <PaymentMethodList
-                    onPress={selectAccountAndNavigateBack}
-                    shouldShowSelectedState
-                    filterType={walletTransfer?.filterPaymentMethodType}
-                    selectedMethodID={walletTransfer?.selectedAccountID}
-                    shouldShowAddPaymentMethodButton={false}
-                    shouldShowAddBankAccount={false}
-                    shouldShowRightIcon={false}
+                <SelectionList
+                    sections={[{data}]}
+                    ListItem={RadioListItem}
+                    onSelectRow={(value) => {
+                        const accountType = value?.bankAccount?.accountType;
+                        const accountData = value?.bankAccount?.accountData;
+                        selectAccountAndNavigateBack(undefined, accountType, accountData);
+                    }}
+                    shouldSingleExecuteRowSelect
+                    initiallyFocusedOptionKey={walletTransfer?.selectedAccountID?.toString()}
                 />
             </View>
             <MenuItem
