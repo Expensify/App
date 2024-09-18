afdffbb5a692ef5dd65def996b82c1f58850a9f0
cretadn22 <cretadn22@gmail.com>
2024-09-18 21:13:10 +0700

    Fix eslint

--- src/libs/actions/Policy/Policy.ts
+++ src/libs/actions/Policy/Policy.ts
@@ -88,7 +88,7 @@ import type {
     Transaction,
 } from '@src/types/onyx';
 import type {Errors} from '@src/types/onyx/OnyxCommon';
-import type {Attributes, CompanyAddress, CustomUnit, NetSuiteCustomList, NetSuiteCustomSegment, Rate, TaxRate, Unit} from '@src/types/onyx/Policy';
+import type {Attributes, CompanyAddress, CustomUnit, NetSuiteCustomList, NetSuiteCustomSegment, Rate, TaxRate} from '@src/types/onyx/Policy';
 import type {ReportActions} from '@src/types/onyx/ReportAction';
 import type {OnyxData} from '@src/types/onyx/Request';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
