075745198479378a9cdff4988a1958f3867446ea
Srikar Parsi <srikar.parsi@expensify.com>
2024-09-18 01:16:45 -0400

    use message instead of originalMessage

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -4258,7 +4258,7 @@ function buildOptimisticTaskCommentReportAction(
 
     // These parameters are not saved on the reportAction, but are used to display the task in the UI
     // Added when we fetch the reportActions on a report
-    reportAction.reportAction.originalMessage = {
+    reportAction.reportAction.message = {
         html: ReportActionsUtils.getReportActionHtml(reportAction.reportAction),
         taskReportID: ReportActionsUtils.getReportActionMessage(reportAction.reportAction)?.taskReportID,
         whisperedTo: [],
