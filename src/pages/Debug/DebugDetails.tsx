eb30a1ac61f038757a78d661d94407c2ea055b95
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-09-20 16:08:41 +0100

    refactor(debug): reduce type cast usage

--- src/pages/Debug/DebugDetails.tsx
+++ src/pages/Debug/DebugDetails.tsx
@@ -12,7 +12,7 @@ import Text from '@components/Text';
 import TextInput from '@components/TextInput';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
-import type {OnyxDataType} from '@libs/DebugUtils';
+import type {ObjectType, OnyxDataType} from '@libs/DebugUtils';
 import DebugUtils from '@libs/DebugUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import Debug from '@userActions/Debug';
@@ -28,14 +28,17 @@ type DebugDetailsProps = {
     /** The report or report action data to be displayed and editted. */
     data: OnyxEntry<Report> | OnyxEntry<ReportAction>;
 
+    children?: React.ReactNode;
+
     /** Callback to be called when user saves the debug data. */
-    onSave: (values: FormOnyxValues<typeof ONYXKEYS.FORMS.DEBUG_DETAILS_FORM>) => void;
+    onSave: (values: Record<string, unknown>) => void;
 
     /** Callback to be called when user deletes the debug data. */
     onDelete: () => void;
 
     /** Callback to be called every time the debug data form is validated. */
-    validate: (key: never, value: string) => void;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    validate: (key: any, value: string) => void;
 };
 
 function DebugDetails({data, children, onSave, onDelete, validate}: DebugDetailsProps) {
@@ -52,39 +55,41 @@ function DebugDetails({data, children, onSave, onDelete, validate}: DebugDetails
     const constantFields = useMemo(
         () =>
             Object.entries(data ?? {})
-                .filter(([key]) => DETAILS_CONSTANT_FIELDS.includes(key as DetailsConstantFieldsKeys))
-                .sort((a, b) => a[0].localeCompare(b[0])) as Array<[string, string]>,
+                .filter((entry): entry is [string, string] => DETAILS_CONSTANT_FIELDS.includes(entry[0] as DetailsConstantFieldsKeys))
+                .sort((a, b) => a[0].localeCompare(b[0])),
         [data],
     );
     const numberFields = useMemo(
         () =>
             Object.entries(data ?? {})
-                .filter(([, value]) => typeof value === 'number')
-                .sort((a, b) => a[0].localeCompare(b[0])) as Array<[string, number]>,
+                .filter((entry): entry is [string, number] => typeof entry[1] === 'number')
+                .sort((a, b) => a[0].localeCompare(b[0])),
         [data],
     );
     const textFields = useMemo(
         () =>
             Object.entries(data ?? {})
                 .filter(
-                    ([key, value]) =>
-                        (typeof value === 'string' || typeof value === 'object') &&
-                        !DETAILS_CONSTANT_FIELDS.includes(key as DetailsConstantFieldsKeys) &&
-                        !DETAILS_DATETIME_FIELDS.includes(key as DetailsDatetimeFieldsKeys),
+                    (entry): entry is [string, string | ObjectType] =>
+                        (typeof entry[1] === 'string' || typeof entry[1] === 'object') &&
+                        !DETAILS_CONSTANT_FIELDS.includes(entry[0] as DetailsConstantFieldsKeys) &&
+                        !DETAILS_DATETIME_FIELDS.includes(entry[0] as DetailsDatetimeFieldsKeys),
                 )
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
                 .map(([key, value]) => [key, DebugUtils.onyxDataToString(value)])
                 .sort((a, b) => a[0].localeCompare(b[0])),
         [data],
     );
-    const dateTimeFields = useMemo(() => Object.entries(data ?? {}).filter(([key]) => DETAILS_DATETIME_FIELDS.includes(key as DetailsDatetimeFieldsKeys)) as Array<[string, string]>, [data]);
+    const dateTimeFields = useMemo(
+        () => Object.entries(data ?? {}).filter((entry): entry is [string, string] => DETAILS_DATETIME_FIELDS.includes(entry[0] as DetailsDatetimeFieldsKeys)),
+        [data],
+    );
 
     const validator = useCallback(
         (values: FormOnyxValues<typeof ONYXKEYS.FORMS.DEBUG_DETAILS_FORM>): FormInputErrors<typeof ONYXKEYS.FORMS.DEBUG_DETAILS_FORM> => {
             const newErrors: Record<string, string | undefined> = {};
             Object.entries(values).forEach(([key, value]) => {
                 try {
-                    validate(key as never, DebugUtils.onyxDataToString(value));
+                    validate(key, DebugUtils.onyxDataToString(value));
                 } catch (e) {
                     const {cause, message} = e as SyntaxError;
                     newErrors[key] = cause || message === 'debug.missingValue' ? translate(message as TranslationPaths, cause as never) : message;
@@ -102,11 +107,11 @@ function DebugDetails({data, children, onSave, onDelete, validate}: DebugDetails
 
     const handleSubmit = useCallback(
         (values: FormOnyxValues<typeof ONYXKEYS.FORMS.DEBUG_DETAILS_FORM>) => {
-            const dataPreparedToSave = Object.entries(values).reduce((acc: FormOnyxValues<typeof ONYXKEYS.FORMS.DEBUG_DETAILS_FORM>, [key, value]) => {
+            const dataPreparedToSave = Object.entries<string | boolean>(values).reduce((acc: Record<string, unknown>, [key, value]) => {
                 if (typeof value === 'boolean') {
                     acc[key] = value;
                 } else {
-                    acc[key] = DebugUtils.stringToOnyxData(value as string, typeof data?.[key as keyof Report & keyof ReportAction] as OnyxDataType);
+                    acc[key] = DebugUtils.stringToOnyxData(value, typeof data?.[key as keyof typeof data] as OnyxDataType);
                 }
                 return acc;
             }, {});
@@ -145,7 +150,7 @@ function DebugDetails({data, children, onSave, onDelete, validate}: DebugDetails
                 <Text style={[styles.headerText, styles.ph5, styles.mb3]}>{translate('debug.textFields')}</Text>
                 <View style={[styles.mb5, styles.ph5, styles.gap5]}>
                     {textFields.map(([key, value]) => {
-                        const numberOfLines = DebugUtils.getNumberOfLinesFromString((formDraftData?.[key] as string) ?? value);
+                        const numberOfLines = DebugUtils.getNumberOfLinesFromString((formDraftData?.[key as keyof typeof formDraftData] as string) ?? value);
                         return (
                             <InputWrapper
                                 InputComponent={TextInput}
