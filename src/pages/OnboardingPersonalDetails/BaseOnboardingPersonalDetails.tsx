ced0874da930158101cb9d8a7a070f29b00ea594
nkdengineer <nkdengineer@outlook.com>
2024-09-20 14:51:49 +0700

    remove withOnyx

--- src/pages/OnboardingPersonalDetails/BaseOnboardingPersonalDetails.tsx
+++ src/pages/OnboardingPersonalDetails/BaseOnboardingPersonalDetails.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {View} from 'react-native';
-import {useOnyx, withOnyx} from 'react-native-onyx';
+import {useOnyx} from 'react-native-onyx';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
 import type {FormOnyxValues} from '@components/Form/types';
