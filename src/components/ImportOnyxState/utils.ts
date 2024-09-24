c3853c529d2510059fbe4c675e58f327f4f5ec2e
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-09-24 14:37:41 +0200

    fix displaying messages and composer

--- src/components/ImportOnyxState/utils.ts
+++ src/components/ImportOnyxState/utils.ts
@@ -11,28 +11,21 @@ function isRecord(value: unknown): value is Record<string, unknown> {
 
 function transformNumericKeysToArray(data: UnknownRecord): UnknownRecord | unknown[] {
     const dataCopy = cloneDeep(data);
-    if (typeof dataCopy !== 'object' || dataCopy === null) {
-        return dataCopy;
+    if (!isRecord(dataCopy)) {
+        return Array.isArray(dataCopy) ? (dataCopy as UnknownRecord[]).map(transformNumericKeysToArray) : (dataCopy as UnknownRecord);
     }
 
     const keys = Object.keys(dataCopy);
-    const allKeysAreNumeric = keys.every((key) => !Number.isNaN(key));
+    const allKeysAreNumeric = keys.every((key) => !Number.isNaN(Number(key)));
     const keysAreSequential = keys.every((key, index) => parseInt(key, 10) === index);
 
     if (allKeysAreNumeric && keysAreSequential) {
-        return keys.map((key) => {
-            if (isRecord(dataCopy)) {
-                return transformNumericKeysToArray(dataCopy[key] as UnknownRecord);
-            }
-            return dataCopy;
-        });
+        return keys.map((key) => transformNumericKeysToArray(dataCopy[key] as UnknownRecord));
     }
 
-    if (isRecord(dataCopy)) {
-        for (const key in dataCopy) {
-            if (key in dataCopy) {
-                dataCopy[key] = transformNumericKeysToArray(dataCopy[key] as UnknownRecord);
-            }
+    for (const key in dataCopy) {
+        if (key in dataCopy) {
+            dataCopy[key] = transformNumericKeysToArray(dataCopy[key] as UnknownRecord);
         }
     }
 
