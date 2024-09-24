18055aefcc94f1a327c2b998864ae3a8e1d6524f
christianwen <tienanhnguyen0108@gmail.com>
2024-09-24 16:44:16 +0700

    remove CONST

--- src/components/Composer/index.native.tsx
+++ src/components/Composer/index.native.tsx
@@ -17,7 +17,6 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import updateIsFullComposerAvailable from '@libs/ComposerUtils/updateIsFullComposerAvailable';
 import * as EmojiUtils from '@libs/EmojiUtils';
 import * as FileUtils from '@libs/fileDownload/FileUtils';
-import CONST from '@src/CONST';
 import type {ComposerProps} from './types';
 
 const excludeNoStyles: Array<keyof MarkdownStyle> = [];
