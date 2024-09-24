b742ddd2750a93f6cba8bada5c9ac7f2e631eff0
nkdengineer <nkdengineer@outlook.com>
2024-09-24 17:01:28 +0700

    fix lint

--- src/pages/home/report/ReportActionsView.tsx
+++ src/pages/home/report/ReportActionsView.tsx
@@ -202,7 +202,7 @@ function ReportActionsView({
     // Get a sorted array of reportActions for both the current report and the transaction thread report associated with this report (if there is one)
     // so that we display transaction-level and report-level report actions in order in the one-transaction view
     const combinedReportActions = useMemo(
-        () => ReportActionsUtils.getCombinedReportActions(reportActionsToDisplay, transactionThreadReportID ?? null, transactionThreadReportActions as OnyxTypes.ReportAction[]),
+        () => ReportActionsUtils.getCombinedReportActions(reportActionsToDisplay, transactionThreadReportID ?? null, transactionThreadReportActions! as OnyxTypes.ReportAction[]),
         [reportActionsToDisplay, transactionThreadReportActions, transactionThreadReportID],
     );
 
