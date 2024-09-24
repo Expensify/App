2bad56336e9b5f3ef8b1708ec815c6b944a916e5
Blazej Kustra <blazej.kustra@swmansion.com>
2024-09-24 18:00:53 +0200

    Add comments for selectors

--- src/libs/onboardingSelectors.ts
+++ src/libs/onboardingSelectors.ts
@@ -1,8 +1,15 @@
 import {type OnyxValue} from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 
+/**
+ * Selector to get the value of hasCompletedGuidedSetupFlow from the Onyx store
+ *
+ * `undefined` means the value is not loaded yet
+ * `true` means the user has completed the NewDot onboarding flow
+ * `false` means the user has not completed the NewDot onboarding flow
+ */
 function hasCompletedGuidedSetupFlowSelector(onboarding: OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>): boolean | undefined {
-    // onboarding is an array for old accounts and accounts created from olddot
+    // Onboarding is an array for old accounts and accounts created from OldDot
     if (Array.isArray(onboarding)) {
         return true;
     }
@@ -10,8 +17,16 @@ function hasCompletedGuidedSetupFlowSelector(onboarding: OnyxValue<typeof ONYXKE
     return onboarding?.hasCompletedGuidedSetupFlow;
 }
 
-function hasCompletedHybridAppSetupFlowSelector(tryNewDotData: OnyxValue<typeof ONYXKEYS.NVP_TRYNEWDOT>): boolean | undefined {
+/**
+ * Selector to get the value of completedHybridAppOnboarding from the Onyx store
+ *
+ * `undefined` means the value is not loaded yet
+ * `true` means the user has completed the hybrid app onboarding flow
+ * `false` means the user has not completed the hybrid app onboarding flow
+ */
+function hasCompletedHybridAppOnboardingFlowSelector(tryNewDotData: OnyxValue<typeof ONYXKEYS.NVP_TRYNEWDOT>): boolean | undefined {
     let completedHybridAppOnboarding = tryNewDotData?.classicRedirect?.completedHybridAppOnboarding;
+
     // Backend might return strings instead of booleans
     if (typeof completedHybridAppOnboarding === 'string') {
         completedHybridAppOnboarding = completedHybridAppOnboarding === 'true';
@@ -20,4 +35,4 @@ function hasCompletedHybridAppSetupFlowSelector(tryNewDotData: OnyxValue<typeof 
     return completedHybridAppOnboarding;
 }
 
-export {hasCompletedGuidedSetupFlowSelector, hasCompletedHybridAppSetupFlowSelector};
+export {hasCompletedGuidedSetupFlowSelector, hasCompletedHybridAppOnboardingFlowSelector};
