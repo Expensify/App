778934ad9aa452bec04b5dde1be8de8a11de776e
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-09-24 11:33:06 +0200

    fix lint

--- src/pages/settings/Troubleshoot/TroubleshootPage.tsx
+++ src/pages/settings/Troubleshoot/TroubleshootPage.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useMemo, useState} from 'react';
 import {View} from 'react-native';
-import Onyx, {useOnyx} from 'react-native-onyx';
+import {useOnyx} from 'react-native-onyx';
 import type {SvgProps} from 'react-native-svg';
 import ClientSideLoggingToolMenu from '@components/ClientSideLoggingToolMenu';
 import ConfirmModal from '@components/ConfirmModal';
