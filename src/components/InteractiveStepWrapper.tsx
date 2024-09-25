e058da375a61a96746c6ddc672e9c26ba1f20b3f
burczu <b.dybowski@gmail.com>
2024-09-25 10:26:34 +0200

    interactive step wrapper extended to match all use cases

--- src/components/InteractiveStepWrapper.tsx
+++ src/components/InteractiveStepWrapper.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {forwardRef} from 'react';
+import {StyleProp, View, ViewStyle} from 'react-native';
 import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
 import HeaderWithBackButton from './HeaderWithBackButton';
@@ -24,21 +24,55 @@ type InteractiveStepWrapperProps = {
 
     // Array of step names
     stepNames?: readonly string[];
+
+    // Should enable max height
+    shouldEnableMaxHeight?: boolean;
+
+    // Should show offline indicator
+    shouldShowOfflineIndicator?: boolean;
+
+    // Should enable picker avoiding
+    shouldEnablePickerAvoiding?: boolean;
+
+    // Call task ID for the guides
+    guidesCallTaskID?: string;
+
+    // Offline indicator style
+    offlineIndicatorStyle?: StyleProp<ViewStyle>;
 };
 
-function InteractiveStepWrapper({children, wrapperID, handleBackButtonPress, headerTitle, startStepIndex, stepNames}: InteractiveStepWrapperProps) {
+function InteractiveStepWrapper(
+    {
+        children,
+        wrapperID,
+        handleBackButtonPress,
+        headerTitle,
+        startStepIndex,
+        stepNames,
+        shouldEnableMaxHeight,
+        shouldShowOfflineIndicator,
+        shouldEnablePickerAvoiding = false,
+        guidesCallTaskID,
+        offlineIndicatorStyle,
+    }: InteractiveStepWrapperProps,
+    ref: React.ForwardedRef<View>,
+) {
     const styles = useThemeStyles();
 
     return (
         <ScreenWrapper
+            ref={ref}
             testID={wrapperID}
             includeSafeAreaPaddingBottom={false}
-            shouldEnablePickerAvoiding={false}
-            shouldEnableMaxHeight
+            shouldEnablePickerAvoiding={shouldEnablePickerAvoiding}
+            shouldEnableMaxHeight={shouldEnableMaxHeight}
+            shouldShowOfflineIndicator={shouldShowOfflineIndicator}
+            offlineIndicatorStyle={offlineIndicatorStyle}
         >
             <HeaderWithBackButton
                 title={headerTitle}
                 onBackButtonPress={handleBackButtonPress}
+                guidesCallTaskID={guidesCallTaskID}
             />
             {stepNames && (
                 <View style={[styles.ph5, styles.mb5, styles.mt3, {height: CONST.BANK_ACCOUNT.STEPS_HEADER_HEIGHT}]}>
@@ -55,4 +89,4 @@ function InteractiveStepWrapper({children, wrapperID, handleBackButtonPress, hea
 
 InteractiveStepWrapper.displayName = 'InteractiveStepWrapper';
 
-export default InteractiveStepWrapper;
+export default forwardRef(InteractiveStepWrapper);
