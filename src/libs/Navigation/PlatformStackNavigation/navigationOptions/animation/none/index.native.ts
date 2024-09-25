234e91e61213d7ad7aec639195ccdeba8cf3ec9b
Christoph Pader <chris@margelo.com>
2024-09-25 13:30:24 +0200

    disable gesture for no animation screens

--- src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/none/index.native.ts
+++ src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/none/index.native.ts
@@ -1,5 +1,5 @@
 import type {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 
-const none: NativeStackNavigationOptions = {animation: 'none'};
+const none: NativeStackNavigationOptions = {animation: 'none', gestureEnabled: false};
 
 export default none;
--- src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/none/index.ts
+++ src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/none/index.ts
@@ -1,5 +1,5 @@
 import type {StackNavigationOptions} from '@react-navigation/stack';
 
-const none: StackNavigationOptions = {animationEnabled: false};
+const none: StackNavigationOptions = {animationEnabled: false, gestureEnabled: false};
 
 export default none;
