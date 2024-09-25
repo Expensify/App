a6b7939fc4813639af7c27232cc8b93835ad1a96
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-09-25 16:33:07 +0200

    add test verifying identical words work in suffix tree

--- tests/unit/SuffixUkkonenTree.ts
+++ tests/unit/SuffixUkkonenTree.ts
@@ -4,6 +4,12 @@ describe('SuffixUkkonenTree', () => {
     it('should insert, build, and find all occurrences', () => {
         const tree = makeTree([...stringToNumeric('banana'), DELIMITER_CHAR_CODE, ...stringToNumeric('pancake'), END_CHAR_CODE]);
         tree.build();
-        expect(tree.findSubstring(stringToNumeric('an'))).toEqual([1, 3, 8]);
+        expect(tree.findSubstring(stringToNumeric('an'))).toEqual(expect.arrayContaining([1, 3, 8]));
+    });
+
+    it('should handle identical words', () => {
+        const tree = makeTree([...stringToNumeric('banana'), DELIMITER_CHAR_CODE, ...stringToNumeric('banana'), END_CHAR_CODE]);
+        tree.build();
+        expect(tree.findSubstring(stringToNumeric('an'))).toEqual(expect.arrayContaining([1, 3, 8, 10]));
     });
 });
