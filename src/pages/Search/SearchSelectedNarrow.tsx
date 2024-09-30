029f277b168a26de24175325a1019bcf3b0c93cc
Jakub Szymczak <szymczak.jakub12@gmail.com>
2024-09-30 13:11:06 +0200

    fix styles

--- src/pages/Search/SearchSelectedNarrow.tsx
+++ src/pages/Search/SearchSelectedNarrow.tsx
@@ -45,7 +45,7 @@ function SearchSelectedNarrow({options, itemsLength}: SearchSelectedNarrowProps)
     };
 
     return (
-        <View>
+        <View style={[styles.pb3]}>
             <Button
                 onPress={openMenu}
                 ref={buttonRef}
