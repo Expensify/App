e49490334dc1a3e4e191d3c406262f99121c1029
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2024-10-01 21:00:49 +0100

    chore(debug): add missing translations and routes for settings debugging UI

--- src/hooks/useSettingsStatus.ts
+++ src/hooks/useSettingsStatus.ts
@@ -1,67 +1,74 @@
-import { useOnyx } from 'react-native-onyx';
-import ONYXKEYS from '@src/ONYXKEYS';
+import {useOnyx} from 'react-native-onyx';
+import {ValueOf} from 'type-fest';
+import {isConnectionInProgress} from '@libs/actions/connections';
 import * as PolicyUtils from '@libs/PolicyUtils';
 import * as SubscriptionUtils from '@libs/SubscriptionUtils';
 import * as UserUtils from '@libs/UserUtils';
 import * as PaymentMethods from '@userActions/PaymentMethods';
-import { isConnectionInProgress } from '@libs/actions/connections';
-import useTheme from './useTheme';
 import CONST from '@src/CONST';
-import { ValueOf } from 'type-fest';
+import ONYXKEYS from '@src/ONYXKEYS';
+import useTheme from './useTheme';
 
 type SettingsStatus = {
-  indicatorColor: string;
-  status: ValueOf<typeof CONST.SETTINGS_STATUS> | undefined
-}
+    indicatorColor: string;
+    status: ValueOf<typeof CONST.SETTINGS_STATUS> | undefined;
+    idOfPolicyWithErrors: string | undefined;
+};
 
 function useSettingsStatus(): SettingsStatus {
-  const theme = useTheme();
-  const [allConnectionSyncProgresses] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS}`);
-  const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
-  const [bankAccountList] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST);
-  const [reimbursementAccount] = useOnyx(ONYXKEYS.REIMBURSEMENT_ACCOUNT);
-  const [fundList] = useOnyx(ONYXKEYS.FUND_LIST);
-  const [userWallet] = useOnyx(ONYXKEYS.USER_WALLET);
-  const [walletTerms] = useOnyx(ONYXKEYS.WALLET_TERMS);
-  const [loginList] = useOnyx(ONYXKEYS.LOGIN_LIST);
+    const theme = useTheme();
+    const [allConnectionSyncProgresses] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS}`);
+    const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
+    const [bankAccountList] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST);
+    const [reimbursementAccount] = useOnyx(ONYXKEYS.REIMBURSEMENT_ACCOUNT);
+    const [fundList] = useOnyx(ONYXKEYS.FUND_LIST);
+    const [userWallet] = useOnyx(ONYXKEYS.USER_WALLET);
+    const [walletTerms] = useOnyx(ONYXKEYS.WALLET_TERMS);
+    const [loginList] = useOnyx(ONYXKEYS.LOGIN_LIST);
 
-  // If a policy was just deleted from Onyx, then Onyx will pass a null value to the props, and
-  // those should be cleaned out before doing any error checking
-  const cleanPolicies = Object.fromEntries(Object.entries(policies ?? {}).filter(([, policy]) => policy?.id));
+    // If a policy was just deleted from Onyx, then Onyx will pass a null value to the props, and
+    // those should be cleaned out before doing any error checking
+    const cleanPolicies = Object.fromEntries(Object.entries(policies ?? {}).filter(([, policy]) => policy?.id));
 
-  // All of the error & info-checking methods are put into an array. This is so that using _.some() will return
-  // early as soon as the first error / info condition is returned. This makes the checks very efficient since
-  // we only care if a single error / info condition exists anywhere.
-  const errorChecking = {
-    [CONST.SETTINGS_STATUS.HAS_USER_WALLET_ERRORS]: Object.keys(userWallet?.errors ?? {}).length > 0,
-    [CONST.SETTINGS_STATUS.HAS_PAYMENT_METHOD_ERROR]: PaymentMethods.hasPaymentMethodError(bankAccountList, fundList),
-    [CONST.SETTINGS_STATUS.HAS_POLICY_ERRORS]: Object.values(cleanPolicies).some(PolicyUtils.hasPolicyError),
-    [CONST.SETTINGS_STATUS.HAS_CUSTOM_UNITS_ERROR]: Object.values(cleanPolicies).some(PolicyUtils.hasCustomUnitsError),
-    [CONST.SETTINGS_STATUS.HAS_EMPLOYEE_LIST_ERROR]: Object.values(cleanPolicies).some(PolicyUtils.hasEmployeeListError),
-    [CONST.SETTINGS_STATUS.HAS_SYNC_ERRORS]: Object.values(cleanPolicies).some((cleanPolicy) =>
-      PolicyUtils.hasSyncError(
-        cleanPolicy,
-        isConnectionInProgress(allConnectionSyncProgresses?.[`${ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS}${cleanPolicy?.id}`], cleanPolicy),
-      )),
-    [CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_ERRORS]: SubscriptionUtils.hasSubscriptionRedDotError(),
-    [CONST.SETTINGS_STATUS.HAS_REIMBURSEMENT_ACCOUNT_ERRORS]: Object.keys(reimbursementAccount?.errors ?? {}).length > 0,
-    [CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_ERROR]: !!loginList && UserUtils.hasLoginListError(loginList),
-    // Wallet term errors that are not caused by an IOU (we show the red brick indicator for those in the LHN instead)
-    [CONST.SETTINGS_STATUS.HAS_WALLET_TERMS_ERRORS]: Object.keys(walletTerms?.errors ?? {}).length > 0 && !walletTerms?.chatReportID,
-  }
+    const policyErrors = {
+        [CONST.SETTINGS_STATUS.HAS_POLICY_ERRORS]: Object.values(cleanPolicies).find(PolicyUtils.hasPolicyError),
+        [CONST.SETTINGS_STATUS.HAS_CUSTOM_UNITS_ERROR]: Object.values(cleanPolicies).find(PolicyUtils.hasCustomUnitsError),
+        [CONST.SETTINGS_STATUS.HAS_EMPLOYEE_LIST_ERROR]: Object.values(cleanPolicies).find(PolicyUtils.hasEmployeeListError),
+        [CONST.SETTINGS_STATUS.HAS_SYNC_ERRORS]: Object.values(cleanPolicies).find((cleanPolicy) =>
+            PolicyUtils.hasSyncError(
+                cleanPolicy,
+                isConnectionInProgress(allConnectionSyncProgresses?.[`${ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS}${cleanPolicy?.id}`], cleanPolicy),
+            ),
+        ),
+    };
 
-  const infoChecking = {
-    [CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_INFO]: !!loginList && UserUtils.hasLoginListInfo(loginList),
-    [CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO]: SubscriptionUtils.hasSubscriptionGreenDotInfo()
-  };
+    // All of the error & info-checking methods are put into an array. This is so that using _.some() will return
+    // early as soon as the first error / info condition is returned. This makes the checks very efficient since
+    // we only care if a single error / info condition exists anywhere.
+    const errorChecking = {
+        [CONST.SETTINGS_STATUS.HAS_USER_WALLET_ERRORS]: Object.keys(userWallet?.errors ?? {}).length > 0,
+        [CONST.SETTINGS_STATUS.HAS_PAYMENT_METHOD_ERROR]: PaymentMethods.hasPaymentMethodError(bankAccountList, fundList),
+        ...(Object.fromEntries(Object.entries(policyErrors).map(([error, policy]) => [error, !!policy])) as Record<keyof typeof policyErrors, boolean>),
+        [CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_ERRORS]: SubscriptionUtils.hasSubscriptionRedDotError(),
+        [CONST.SETTINGS_STATUS.HAS_REIMBURSEMENT_ACCOUNT_ERRORS]: Object.keys(reimbursementAccount?.errors ?? {}).length > 0,
+        [CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_ERROR]: !!loginList && UserUtils.hasLoginListError(loginList),
+        // Wallet term errors that are not caused by an IOU (we show the red brick indicator for those in the LHN instead)
+        [CONST.SETTINGS_STATUS.HAS_WALLET_TERMS_ERRORS]: Object.keys(walletTerms?.errors ?? {}).length > 0 && !walletTerms?.chatReportID,
+    };
 
-  const [error] = Object.entries(errorChecking).find(([, value]) => value) || []
-  const [info] = Object.entries(infoChecking).find(([, value]) => value) || []
+    const infoChecking = {
+        [CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_INFO]: !!loginList && UserUtils.hasLoginListInfo(loginList),
+        [CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO]: SubscriptionUtils.hasSubscriptionGreenDotInfo(),
+    };
 
-  const status = (error || info) as ValueOf<typeof CONST.SETTINGS_STATUS> | undefined;
-  const indicatorColor = error ? theme.danger : theme.success;
+    const [error] = Object.entries(errorChecking).find(([, value]) => value) || [];
+    const [info] = Object.entries(infoChecking).find(([, value]) => value) || [];
 
-  return { indicatorColor, status };
+    const status = (error || info) as ValueOf<typeof CONST.SETTINGS_STATUS> | undefined;
+    const idOfPolicyWithErrors = Object.values(policyErrors).find(Boolean)?.id;
+    const indicatorColor = error ? theme.danger : theme.success;
+
+    return {indicatorColor, status: CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO, idOfPolicyWithErrors};
 }
 
 export default useSettingsStatus;
--- src/libs/Navigation/AppNavigator/createCustomBottomTabNavigator/DebugTabView.tsx
+++ src/libs/Navigation/AppNavigator/createCustomBottomTabNavigator/DebugTabView.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useMemo } from 'react';
-import { Text, View } from 'react-native';
-import { ValueOf } from 'type-fest';
+import React, {useCallback, useMemo} from 'react';
+import {Text, View} from 'react-native';
+import {ValueOf} from 'type-fest';
 import Button from '@components/Button';
 import Icon from '@components/Icon';
 import * as Expensicons from '@components/Icon/Expensicons';
@@ -10,167 +10,153 @@ import useStyleUtils from '@hooks/useStyleUtils';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
-import { BrickRoad, getChatTabBrickRoadReport } from '@libs/WorkspacesSettingsUtils';
+import {BrickRoad, getChatTabBrickRoadReport} from '@libs/WorkspacesSettingsUtils';
 import CONST from '@src/CONST';
-import { TranslationPaths } from '@src/languages/types';
-import ROUTES, { Route } from '@src/ROUTES';
+import {TranslationPaths} from '@src/languages/types';
+import ROUTES, {Route} from '@src/ROUTES';
 import SCREENS from '@src/SCREENS';
 
 type DebugTabViewProps = {
-  selectedTab?: string;
-  chatTabBrickRoad: BrickRoad;
-  activeWorkspaceID?: string;
+    selectedTab?: string;
+    chatTabBrickRoad: BrickRoad;
+    activeWorkspaceID?: string;
 };
 
-function getSettingsMessage(status: ValueOf<typeof CONST.SETTINGS_STATUS> | undefined): TranslationPaths {
-  switch (status) {
-    case CONST.SETTINGS_STATUS.HAS_CUSTOM_UNITS_ERROR:
-      return 'debug.settingsStatus.workspaceHasCustomUnitsErrors';
-    case CONST.SETTINGS_STATUS.HAS_EMPLOYEE_LIST_ERROR:
-      return 'debug.settingsStatus.workspaceHasEmployeeListErrors';
-    case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_ERROR:
-      return 'debug.settingsStatus.profileHasErrors';
-    case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_INFO:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_PAYMENT_METHOD_ERROR:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_POLICY_ERRORS:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_REIMBURSEMENT_ACCOUNT_ERRORS:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_ERRORS:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_SYNC_ERRORS:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_USER_WALLET_ERRORS:
-      //TODO: CREATE TRANSLATION
-      return '';
-    case CONST.SETTINGS_STATUS.HAS_WALLET_TERMS_ERRORS:
-      //TODO: CREATE TRANSLATION
-      return '';
-    default:
-      //TODO: CREATE TRANSLATION
-      return '';
-  }
+function getSettingsMessage(status: ValueOf<typeof CONST.SETTINGS_STATUS> | undefined): TranslationPaths | undefined {
+    switch (status) {
+        case CONST.SETTINGS_STATUS.HAS_CUSTOM_UNITS_ERROR:
+            return 'debug.settingsStatus.workspaceHasCustomUnitsErrors';
+        case CONST.SETTINGS_STATUS.HAS_EMPLOYEE_LIST_ERROR:
+            return 'debug.settingsStatus.workspaceHasEmployeeListErrors';
+        case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_ERROR:
+            return 'debug.settingsStatus.profileHasErrors';
+        case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_INFO:
+            return 'debug.settingsStatus.profileRequiresAttention';
+        case CONST.SETTINGS_STATUS.HAS_PAYMENT_METHOD_ERROR:
+            return 'debug.settingsStatus.walletHasPaymentMethodError';
+        case CONST.SETTINGS_STATUS.HAS_POLICY_ERRORS:
+            return 'debug.settingsStatus.workspaceHasErrors';
+        case CONST.SETTINGS_STATUS.HAS_REIMBURSEMENT_ACCOUNT_ERRORS:
+            return 'debug.settingsStatus.workspaceHasReimbursementAccountError';
+        case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_ERRORS:
+            return 'debug.settingsStatus.subscriptionHasErrors';
+        case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO:
+            return 'debug.settingsStatus.subscriptionRequiresAttention';
+        case CONST.SETTINGS_STATUS.HAS_SYNC_ERRORS:
+            return 'debug.settingsStatus.policyAccountingHasSyncErrors';
+        case CONST.SETTINGS_STATUS.HAS_USER_WALLET_ERRORS:
+            return 'debug.settingsStatus.walletHasErrors';
+        case CONST.SETTINGS_STATUS.HAS_WALLET_TERMS_ERRORS:
+            return 'debug.settingsStatus.walletHasTermsErrors';
+        default:
+            return undefined;
+    }
 }
 
-function getSettingsRoute(status: ValueOf<typeof CONST.SETTINGS_STATUS> | undefined): Route | undefined {
-  switch (status) {
-    case CONST.SETTINGS_STATUS.HAS_CUSTOM_UNITS_ERROR:
-      return ROUTES.SETTINGS_SUBSCRIPTION;
-    case CONST.SETTINGS_STATUS.HAS_EMPLOYEE_LIST_ERROR:
-      //TODO: USE POLICY ID
-      return ROUTES.WORKSPACE_MEMBERS.route;
-    case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_ERROR:
-      return ROUTES.SETTINGS_PROFILE;
-    case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_INFO:
-      return ROUTES.SETTINGS_PROFILE;
-    case CONST.SETTINGS_STATUS.HAS_PAYMENT_METHOD_ERROR:
-      return ROUTES.SETTINGS_WALLET;
-    case CONST.SETTINGS_STATUS.HAS_POLICY_ERRORS:
-      //TODO: USE POLICY ID
-      return ROUTES.SETTINGS_WORKSPACES;
-    case CONST.SETTINGS_STATUS.HAS_REIMBURSEMENT_ACCOUNT_ERRORS:
-      //TODO: USE POLICY ID
-      return ROUTES.BANK_ACCOUNT_WITH_STEP_TO_OPEN.route as Route;
-    case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_ERRORS:
-      return ROUTES.SETTINGS_SUBSCRIPTION;
-    case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO:
-      return ROUTES.SETTINGS_SUBSCRIPTION;
-    case CONST.SETTINGS_STATUS.HAS_SYNC_ERRORS:
-      //TODO: USE POLICY ID
-      return ROUTES.SETTINGS_WORKSPACES;
-    case CONST.SETTINGS_STATUS.HAS_USER_WALLET_ERRORS:
-      return ROUTES.SETTINGS_WALLET;
-    case CONST.SETTINGS_STATUS.HAS_WALLET_TERMS_ERRORS:
-      return ROUTES.SETTINGS_WALLET;
-    default:
-      return undefined;
-  }
+function getSettingsRoute(status: ValueOf<typeof CONST.SETTINGS_STATUS> | undefined, idOfPolicyWithErrors = ''): Route | undefined {
+    switch (status) {
+        case CONST.SETTINGS_STATUS.HAS_CUSTOM_UNITS_ERROR:
+            return ROUTES.SETTINGS_SUBSCRIPTION;
+        case CONST.SETTINGS_STATUS.HAS_EMPLOYEE_LIST_ERROR:
+            return ROUTES.WORKSPACE_MEMBERS.getRoute(idOfPolicyWithErrors);
+        case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_ERROR:
+            return ROUTES.SETTINGS_PROFILE;
+        case CONST.SETTINGS_STATUS.HAS_LOGIN_LIST_INFO:
+            return ROUTES.SETTINGS_PROFILE;
+        case CONST.SETTINGS_STATUS.HAS_PAYMENT_METHOD_ERROR:
+            return ROUTES.SETTINGS_WALLET;
+        case CONST.SETTINGS_STATUS.HAS_POLICY_ERRORS:
+            return ROUTES.WORKSPACE_INITIAL.getRoute(idOfPolicyWithErrors);
+        case CONST.SETTINGS_STATUS.HAS_REIMBURSEMENT_ACCOUNT_ERRORS:
+            return ROUTES.BANK_ACCOUNT_WITH_STEP_TO_OPEN.getRoute('', idOfPolicyWithErrors);
+        case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_ERRORS:
+            return ROUTES.SETTINGS_SUBSCRIPTION;
+        case CONST.SETTINGS_STATUS.HAS_SUBSCRIPTION_INFO:
+            return ROUTES.SETTINGS_SUBSCRIPTION;
+        case CONST.SETTINGS_STATUS.HAS_SYNC_ERRORS:
+            return ROUTES.WORKSPACE_ACCOUNTING.getRoute(idOfPolicyWithErrors);
+        case CONST.SETTINGS_STATUS.HAS_USER_WALLET_ERRORS:
+            return ROUTES.SETTINGS_WALLET;
+        case CONST.SETTINGS_STATUS.HAS_WALLET_TERMS_ERRORS:
+            return ROUTES.SETTINGS_WALLET;
+        default:
+            return undefined;
+    }
 }
 
-const DebugTabView = ({ selectedTab = '', chatTabBrickRoad, activeWorkspaceID }: DebugTabViewProps) => {
-  const StyleUtils = useStyleUtils();
-  const theme = useTheme();
-  const styles = useThemeStyles();
-  const { translate } = useLocalize();
-  const { status, indicatorColor } = useSettingsStatus();
+const DebugTabView = ({selectedTab = '', chatTabBrickRoad, activeWorkspaceID}: DebugTabViewProps) => {
+    const StyleUtils = useStyleUtils();
+    const theme = useTheme();
+    const styles = useThemeStyles();
+    const {translate} = useLocalize();
+    const {status, indicatorColor, idOfPolicyWithErrors} = useSettingsStatus();
 
-  const message = useMemo((): TranslationPaths | undefined => {
-    if (selectedTab === SCREENS.HOME) {
-      if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.INFO) {
-        return 'debug.settingsStatus.theresAReportAwaitingAction';
-      }
-      if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR) {
-        return 'debug.settingsStatus.theresAReportWithErrors';
-      }
+    const message = useMemo((): TranslationPaths | undefined => {
+        if (selectedTab === SCREENS.HOME) {
+            if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.INFO) {
+                return 'debug.settingsStatus.theresAReportAwaitingAction';
+            }
+            if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR) {
+                return 'debug.settingsStatus.theresAReportWithErrors';
+            }
+        }
+        if (selectedTab === SCREENS.SETTINGS.ROOT) {
+            return getSettingsMessage(status);
+        }
+    }, [selectedTab, chatTabBrickRoad]);
+
+    const indicator = useMemo(() => {
+        if (selectedTab === SCREENS.HOME) {
+            if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.INFO) {
+                return theme.success;
+            }
+            if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR) {
+                return theme.danger;
+            }
+        }
+        if (selectedTab === SCREENS.SETTINGS.ROOT) {
+            if (status) {
+                return indicatorColor;
+            }
+        }
+    }, [selectedTab, chatTabBrickRoad]);
+
+    const navigateTo = useCallback(() => {
+        if (selectedTab === SCREENS.HOME && !!chatTabBrickRoad) {
+            const report = getChatTabBrickRoadReport(activeWorkspaceID);
+
+            if (report) {
+                Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(report.reportID));
+            }
+        }
+        if (selectedTab === SCREENS.SETTINGS.ROOT) {
+            const route = getSettingsRoute(status, idOfPolicyWithErrors);
+
+            if (route) {
+                Navigation.navigate(route);
+            }
+        }
+    }, [selectedTab, chatTabBrickRoad]);
+
+    if (!([SCREENS.HOME, SCREENS.SETTINGS.ROOT] as string[]).includes(selectedTab) || !indicator) {
+        return null;
     }
-    if (selectedTab === SCREENS.SETTINGS.ROOT) {
-      return getSettingsMessage(status);
-    }
-  }, [selectedTab, chatTabBrickRoad]);
 
-  const indicator = useMemo(() => {
-    if (selectedTab === SCREENS.HOME) {
-      if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.INFO) {
-        return theme.success;
-      }
-      if (chatTabBrickRoad === CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR) {
-        return theme.danger;
-      }
-    }
-    if (selectedTab === SCREENS.SETTINGS.ROOT) {
-      if (status) {
-        return indicatorColor;
-      }
-    }
-  }, [selectedTab, chatTabBrickRoad]);
-
-  const navigateTo = useCallback(() => {
-    if (selectedTab === SCREENS.HOME && !!chatTabBrickRoad) {
-      const report = getChatTabBrickRoadReport(activeWorkspaceID);
-
-      if (report) {
-        Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(report.reportID));
-      }
-    }
-    if (selectedTab === SCREENS.SETTINGS.ROOT) {
-      const route = getSettingsRoute(status);
-
-      if (route) {
-        Navigation.navigate(route);
-      }
-    }
-  }, [selectedTab, chatTabBrickRoad]);
-
-  if (!([SCREENS.HOME, SCREENS.SETTINGS.ROOT] as string[]).includes(selectedTab) || !indicator) {
-    return null;
-  }
-
-  return (
-    <View style={[StyleUtils.getBackgroundColorStyle(theme.cardBG), styles.p3, styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
-      <View style={[styles.flexRow, styles.gap2]}>
-        <Icon
-          src={Expensicons.DotIndicator}
-          fill={indicator}
-        />
-        <Text style={[styles.textWhite, styles.lh20]}>{message && translate(message)}</Text>
-      </View>
-      <Button
-        text="View"
-        onPress={navigateTo}
-      />
-    </View>
-  );
+    return (
+        <View style={[StyleUtils.getBackgroundColorStyle(theme.cardBG), styles.p3, styles.flexRow, styles.justifyContentBetween, styles.alignItemsCenter]}>
+            <View style={[styles.flexRow, styles.gap2]}>
+                <Icon
+                    src={Expensicons.DotIndicator}
+                    fill={indicator}
+                />
+                <Text style={[styles.textWhite, styles.lh20]}>{message && translate(message)}</Text>
+            </View>
+            <Button
+                text="View"
+                onPress={navigateTo}
+            />
+        </View>
+    );
 };
 
 export default DebugTabView;
