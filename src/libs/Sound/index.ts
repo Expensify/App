d646ce928f9d2b020a852f4f862528e4084721e5
Wildan Muhlis <muhliswildan@gmail.com>
2024-10-03 10:03:19 +0700

    change to early return style

--- src/libs/Sound/index.ts
+++ src/libs/Sound/index.ts
@@ -5,21 +5,23 @@ import { SOUNDS, isMuted, withMinimalExecutionTime } from './BaseSound'
 import Log from '@libs/Log';
 
 function cacheSoundAssets() {
-    if ('caches' in window) {
-        caches.open('sound-assets').then(cache => {
-            const soundFiles = Object.values(SOUNDS).map(sound => `${config.prefix}${sound}.mp3`);
-            
-            const cachePromises = soundFiles.map(soundFile => {
-                return cache.match(soundFile).then(response => {
-                    if (!response) {
-                        return cache.add(soundFile);
-                    }
-                });
-            });
-
-            return Promise.all(cachePromises);
-        });
+    if (!('caches' in window)) {
+        return;
     }
+
+    caches.open('sound-assets').then(cache => {
+        const soundFiles = Object.values(SOUNDS).map(sound => `${config.prefix}${sound}.mp3`);
+        
+        const cachePromises = soundFiles.map(soundFile => {
+            return cache.match(soundFile).then(response => {
+                if (!response) {
+                    return cache.add(soundFile);
+                }
+            });
+        });
+
+        return Promise.all(cachePromises);
+    });
 }
 
 const initializeAndPlaySound = (src: string) => {
@@ -44,24 +46,26 @@ const playSound = (soundFile: ValueOf<typeof SOUNDS>) => {
 
     const soundSrc = `${config.prefix}${soundFile}.mp3`;
 
-    if ('caches' in window) {
-        caches.open('sound-assets').then(cache => {
-            cache.match(soundSrc).then(response => {
-                if (response) {
-                    response.blob().then(soundBlob => {
-                        const soundUrl = URL.createObjectURL(soundBlob);
-                        initializeAndPlaySound(soundUrl);
-                    });
-                } else {
-                    initializeAndPlaySound(soundSrc);
-                }
-            });
-        });
-    } else {
+    if (!('caches' in window)) {
         // Fallback to fetching from network if not in cache
         initializeAndPlaySound(soundSrc);
+        return;
     }
+
+    caches.open('sound-assets').then(cache => {
+        cache.match(soundSrc).then(response => {
+            if (response) {
+                response.blob().then(soundBlob => {
+                    const soundUrl = URL.createObjectURL(soundBlob);
+                    initializeAndPlaySound(soundUrl);
+                });
+                return;
+            }
+            initializeAndPlaySound(soundSrc);
+        });
+    });
 };
+
 // Cache sound assets on load
 cacheSoundAssets();
 
