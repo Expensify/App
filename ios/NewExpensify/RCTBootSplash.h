23984a616372da9889b07e8a2ed008fbbd29c321
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-10-04 11:55:40 +0200

    fix: make ios work properly

--- ios/NewExpensify/RCTBootSplash.h
+++ ios/NewExpensify/RCTBootSplash.h
@@ -1,16 +1,7 @@
-//
-//  RCTBootSplash.h
-//  NewExpensify
-//
-//  Created by Mathieu Acthernoene on 07/01/2022.
-//
-
 #import <React/RCTBridgeModule.h>
-#import <React/RCTRootView.h>
 
 @interface RCTBootSplash : NSObject <RCTBridgeModule>
 
-+ (void)invalidateBootSplash;
 + (void)initWithStoryboard:(NSString * _Nonnull)storyboardName
                   rootView:(UIView * _Nullable)rootView;
 
