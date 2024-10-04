5e484340deae19d14a4a6a98f4c83f51bc26b32b
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2024-10-04 22:08:07 +0530

    Revert "Fix task title is overflown and align the checkbox and arrow correctly"

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4151,6 +4151,11 @@ const styles = (theme: ThemeColors) =>
             width: 1,
         },
 
+        taskCheckboxWrapper: {
+            height: variables.fontSizeNormalHeight,
+            ...flex.justifyContentCenter,
+        },
+
         taskTitleMenuItem: {
             ...writingDirection.ltr,
             ...headlineFont,
