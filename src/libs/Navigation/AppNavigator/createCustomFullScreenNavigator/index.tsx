a7293da050229bb24daa0e6031114980a9270135
Christoph Pader <chris@margelo.com>
2024-10-06 17:35:20 +0200

    fix: remove header in FullScreenNavigator

--- src/libs/Navigation/AppNavigator/createCustomFullScreenNavigator/index.tsx
+++ src/libs/Navigation/AppNavigator/createCustomFullScreenNavigator/index.tsx
@@ -2,12 +2,14 @@ import type {ParamListBase} from '@react-navigation/native';
 import {createNavigatorFactory} from '@react-navigation/native';
 import useNavigationResetOnLayoutChange from '@libs/Navigation/AppNavigator/useNavigationResetOnLayoutChange';
 import createPlatformStackNavigatorComponent from '@libs/Navigation/PlatformStackNavigation/createPlatformStackNavigatorComponent';
+import defaultPlatformStackScreenOptions from '@libs/Navigation/PlatformStackNavigation/defaultPlatformStackScreenOptions';
 import type {PlatformStackNavigationEventMap, PlatformStackNavigationOptions, PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
 import CustomFullScreenRouter from './CustomFullScreenRouter';
 
 const CustomFullScreenNavigatorComponent = createPlatformStackNavigatorComponent('CustomFullScreenNavigator', {
     createRouter: CustomFullScreenRouter,
     useCustomEffects: useNavigationResetOnLayoutChange,
+    defaultScreenOptions: defaultPlatformStackScreenOptions,
 });
 
 function createCustomFullScreenNavigator<ParamList extends ParamListBase>() {
