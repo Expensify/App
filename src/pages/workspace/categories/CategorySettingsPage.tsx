b515fe0e99858fde04d91396fa2ae447094dbff7
cretadn22 <cretadn22@gmail.com>
2024-10-07 22:43:43 +0700

    display-name-instead-of-email

--- src/pages/workspace/categories/CategorySettingsPage.tsx
+++ src/pages/workspace/categories/CategorySettingsPage.tsx
@@ -20,6 +20,7 @@ import * as CategoryUtils from '@libs/CategoryUtils';
 import * as CurrencyUtils from '@libs/CurrencyUtils';
 import * as ErrorUtils from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
+import * as PersonalDetailsUtils from '@libs/PersonalDetailsUtils';
 import {isControlPolicy} from '@libs/PolicyUtils';
 import * as PolicyUtils from '@libs/PolicyUtils';
 import type {SettingsNavigatorParamList} from '@navigation/types';
@@ -77,8 +78,9 @@ function CategorySettingsPage({
     }, [policyCategory?.maxExpenseAmount, policyCategoryExpenseLimitType, policyCurrency, translate]);
 
     const approverText = useMemo(() => {
-        const categoryApprover = CategoryUtils.getCategoryApproverRule(policy?.rules?.approvalRules ?? [], categoryName)?.approver;
-        return categoryApprover ?? '';
+        const categoryApprover = CategoryUtils.getCategoryApproverRule(policy?.rules?.approvalRules ?? [], categoryName)?.approver ?? '';
+        const approver = PersonalDetailsUtils.getPersonalDetailByEmail(categoryApprover);
+        return approver?.displayName || categoryApprover;
     }, [categoryName, policy?.rules?.approvalRules]);
 
     const defaultTaxRateText = useMemo(() => {
