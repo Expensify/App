2ffd9d4ceda076c0e1923e07aa8900f6c846de65
Alex Beaman <alexbeaman@expensify.com>
2024-10-08 10:49:15 -0600

    Update related actions

--- src/pages/home/report/ContextMenu/ContextMenuActions.tsx
+++ src/pages/home/report/ContextMenu/ContextMenuActions.tsx
@@ -444,31 +444,25 @@ const ContextMenuActions: ContextMenuAction[] = [
                     ReportActionsUtils.isActionOfType(reportAction, CONST.REPORT.ACTIONS.TYPE.SUBMITTED_AND_CLOSED)
                 ) {
                     const {harvesting} = ReportActionsUtils.getOriginalMessage(reportAction) ?? {};
-                    let displayMessage = '';
                     if (harvesting) {
-                        displayMessage = ReportUtils.getReportAutomaticallySubmittedMessage(reportAction);
+                        setClipboardMessage(ReportUtils.getReportAutomaticallySubmittedMessage(reportAction));
                     } else {
-                        displayMessage = ReportUtils.getIOUSubmittedMessage(reportAction);
+                        Clipboard.setString(ReportUtils.getIOUSubmittedMessage(reportAction));
                     }
-                    Clipboard.setString(displayMessage);
                 } else if (ReportActionsUtils.isActionOfType(reportAction, CONST.REPORT.ACTIONS.TYPE.APPROVED)) {
                     const {automaticAction} = ReportActionsUtils.getOriginalMessage(reportAction) ?? {};
-                    let displayMessage = '';
                     if (automaticAction) {
-                        displayMessage = ReportUtils.getReportAutomaticallyApprovedMessage(reportAction);
+                        setClipboardMessage(ReportUtils.getReportAutomaticallyApprovedMessage(reportAction));
                     } else {
-                        displayMessage = ReportUtils.getIOUApprovedMessage(reportAction);
+                        Clipboard.setString(ReportUtils.getIOUApprovedMessage(reportAction));
                     }
-                    Clipboard.setString(displayMessage);
                 } else if (ReportActionsUtils.isActionOfType(reportAction, CONST.REPORT.ACTIONS.TYPE.FORWARDED)) {
                     const {automaticAction} = ReportActionsUtils.getOriginalMessage(reportAction) ?? {};
-                    let displayMessage = '';
                     if (automaticAction) {
-                        displayMessage = ReportUtils.getReportAutomaticallyForwardedMessage(reportAction, reportID);
+                        setClipboardMessage(ReportUtils.getReportAutomaticallyForwardedMessage(reportAction, reportID));
                     } else {
-                        displayMessage = ReportUtils.getIOUForwardedMessage(reportAction, reportID);
+                        Clipboard.setString(ReportUtils.getIOUForwardedMessage(reportAction, reportID));
                     }
-                    Clipboard.setString(displayMessage);
                 } else if (reportAction?.actionName === CONST.REPORT.ACTIONS.TYPE.REJECTED) {
                     const displayMessage = ReportUtils.getRejectedReportMessage();
                     Clipboard.setString(displayMessage);
