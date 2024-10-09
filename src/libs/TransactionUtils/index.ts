da47219c6cb80b45223024827b9632d831bfd96e
neil-marcellini <neil@expensify.com>
2024-10-09 15:00:26 -0700

    Set distance pending when converted to new rate unit

--- src/libs/TransactionUtils/index.ts
+++ src/libs/TransactionUtils/index.ts
@@ -293,6 +293,7 @@ function getUpdatedTransaction(transaction: Transaction, transactionChanges: Tra
             const conversionFactor = existingDistanceUnit === CONST.CUSTOM_UNITS.DISTANCE_UNIT_MILES ? CONST.CUSTOM_UNITS.MILES_TO_KILOMETERS : CONST.CUSTOM_UNITS.KILOMETERS_TO_MILES;
             const distance = Math.round((updatedTransaction?.comment?.customUnit?.quantity ?? 0) * conversionFactor * 100) / 100;
             lodashSet(updatedTransaction, 'comment.customUnit.quantity', distance);
+            lodashSet(updatedTransaction, 'pendingFields.waypoints', CONST.RED_BRICK_ROAD_PENDING_ACTION.UPDATE);
         }
     }
 
