93eb244bf0651bab58854a16d661720085b53ae4
Adam Grzybowski <adam.grzybowski@swmansion.com>
2024-10-09 17:28:15 +0200

    remove FreezeWrapper

--- src/pages/home/sidebar/SidebarScreen/index.tsx
+++ src/pages/home/sidebar/SidebarScreen/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-// import FreezeWrapper from '@libs/Navigation/FreezeWrapper';
 import BaseSidebarScreen from './BaseSidebarScreen';
 
-// TODO-SPLITS: Figure out how to handle the FreezeWrapper component.
 function SidebarScreen() {
-    return (
-        // <FreezeWrapper>
-        <BaseSidebarScreen />
-        // </FreezeWrapper>
-    );
+    return <BaseSidebarScreen />;
 }
 
 SidebarScreen.displayName = 'SidebarScreen';
