e8fe3c3b7946322702cbb3d6176e42825b859397
truph01 <truphd01@gmail.com>
2024-10-09 14:39:37 +0700

    fix: type

--- src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer/MentionReportContext.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer/MentionReportContext.tsx
@@ -2,10 +2,12 @@ import {createContext} from 'react';
 
 type MentionReportContextProps = {
     currentReportID: string;
+    exactlyMatch: boolean;
 };
 
 const MentionReportContext = createContext<MentionReportContextProps>({
     currentReportID: '',
+    exactlyMatch: false,
 });
 
 export default MentionReportContext;
