f60884a56fe78b011afdf486614e8848b5a7aefd
Abdelrahman Khattab <59809993+abzokhattab@users.noreply.github.com>
2024-10-10 20:26:46 +0200

    Fix eslint

--- src/pages/workspace/members/WorkspaceMemberNewCardPage.tsx
+++ src/pages/workspace/members/WorkspaceMemberNewCardPage.tsx
@@ -72,8 +72,6 @@ function WorkspaceMemberNewCardPage({route, personalDetails}: WorkspaceMemberNew
             return;
         }
         if (selectedFeed === CONST.EXPENSIFY_CARD.NAME) {
-            const activeRoute = Navigation.getActiveRoute();
-
             Card.setIssueNewCardStepAndData({
                 step: CONST.EXPENSIFY_CARD.STEP.CARD_TYPE,
                 data: {
@@ -81,7 +79,7 @@ function WorkspaceMemberNewCardPage({route, personalDetails}: WorkspaceMemberNew
                 },
                 isEditing: false,
             });
-            Navigation.navigate(ROUTES.WORKSPACE_EXPENSIFY_CARD_ISSUE_NEW.getRoute(policyID, ROUTES.WORKSPACE_MEMBER_DETAILS.getRoute(policyID, accountID)));        
+            Navigation.navigate(ROUTES.WORKSPACE_EXPENSIFY_CARD_ISSUE_NEW.getRoute(policyID, ROUTES.WORKSPACE_MEMBER_DETAILS.getRoute(policyID, accountID)));
         } else {
             CompanyCards.setAssignCardStepAndData({
                 currentStep: CONST.COMPANY_CARD.STEP.CARD,
