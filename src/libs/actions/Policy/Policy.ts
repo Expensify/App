7b55a821d6a3921152baf7bee2ccd1211877e4ad
truph01 <truphd01@gmail.com>
2024-10-11 14:14:57 +0700

    fix: 'Pay elsewhere' button is present by default

--- src/libs/actions/Policy/Policy.ts
+++ src/libs/actions/Policy/Policy.ts
@@ -1627,6 +1627,7 @@ function buildPolicyData(policyOwnerEmail = '', makeMeAdmin = false, policyName 
                     enabled: true,
                 },
                 customUnits,
+                reimbursementChoice: CONST.POLICY.REIMBURSEMENT_CHOICES.REIMBURSEMENT_NO,
                 areCategoriesEnabled: true,
                 areTagsEnabled: false,
                 areDistanceRatesEnabled: false,
