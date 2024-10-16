6720b4182449fd13ed4a3588adc67ab9ab12f74c
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-10-17 00:31:54 +0100

    Fix Changed files ESLint check deprecation warning

--- tests/perf-test/ReportScreen.perf-test.tsx
+++ tests/perf-test/ReportScreen.perf-test.tsx
@@ -6,7 +6,7 @@ import type ReactNative from 'react-native';
 import {Dimensions, InteractionManager} from 'react-native';
 import Onyx from 'react-native-onyx';
 import type Animated from 'react-native-reanimated';
-import {measurePerformance} from 'reassure';
+import {measureRenders} from 'reassure';
 import type {WithNavigationFocusProps} from '@components/withNavigationFocus';
 import type Navigation from '@libs/Navigation/Navigation';
 import type {AuthScreensParamList} from '@libs/Navigation/types';
@@ -229,7 +229,7 @@ test('[ReportScreen] should render ReportScreen', async () => {
         ...reportCollectionDataSet,
         ...reportActionsCollectionDataSet,
     });
-    await measurePerformance(
+    await measureRenders(
         <ReportScreenWrapper
             navigation={navigation}
             route={mockRoute}
@@ -264,7 +264,7 @@ test('[ReportScreen] should render composer', async () => {
         ...reportCollectionDataSet,
         ...reportActionsCollectionDataSet,
     });
-    await measurePerformance(
+    await measureRenders(
         <ReportScreenWrapper
             navigation={navigation}
             route={mockRoute}
@@ -304,7 +304,7 @@ test('[ReportScreen] should render report list', async () => {
         ...reportCollectionDataSet,
         ...reportActionsCollectionDataSet,
     });
-    await measurePerformance(
+    await measureRenders(
         <ReportScreenWrapper
             navigation={navigation}
             route={mockRoute}
