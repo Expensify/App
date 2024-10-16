0054c861f88386c4f4aef2be8aabedf5a6483864
Yauheni <HorbachZhenja@yandex.by>
2024-10-16 19:05:20 +0200

    refactor screen

--- src/pages/workspace/accounting/qbd/QuickBooksDesktopSetupPage.tsx
+++ src/pages/workspace/accounting/qbd/QuickBooksDesktopSetupPage.tsx
@@ -1,5 +1,5 @@
 import type {StackScreenProps} from '@react-navigation/stack';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {View} from 'react-native';
 import Computer from '@assets/images/laptop-with-second-screen-sync.svg';
 import FullPageOfflineBlockingView from '@components/BlockingViews/FullPageOfflineBlockingView';
@@ -29,16 +29,16 @@ function RequireQuickBooksDesktopModal({route}: RequireQuickBooksDesktopModalPro
     const [isLoading, setIsLoading] = useState(true);
     const [codatSetupLink, setCodatSetupLink] = useState<string>('');
 
-    const FullPageView = codatSetupLink ? ({children}: React.PropsWithChildren) => <View style={[styles.flex1, styles.ph5]}>{children}</View> : FullPageOfflineBlockingView;
+    const ContentWrapper = codatSetupLink ? ({children}: React.PropsWithChildren) => children : FullPageOfflineBlockingView;
 
-    const fetchSetupLink = () => {
+    const fetchSetupLink = useCallback(() => {
         setIsLoading(true);
         // eslint-disable-next-line rulesdir/no-thenable-actions-in-views
         QuickbooksDesktop.getQuickbooksDesktopCodatSetupLink(policyID).then((response) => {
             setCodatSetupLink(String(response?.setupUrl ?? ''));
             setIsLoading(false);
         });
-    };
+    }, [policyID]);
 
     useEffect(() => {
         fetchSetupLink();
@@ -70,29 +70,31 @@ function RequireQuickBooksDesktopModal({route}: RequireQuickBooksDesktopModalPro
                 shouldShowBackButton
                 onBackButtonPress={() => Navigation.dismissModal()}
             />
-            <FullPageView>
-                <View style={[styles.alignSelfCenter, styles.computerIllustrationContainer, styles.pv6]}>
-                    <ImageSVG src={Computer} />
-                </View>
+            <ContentWrapper>
+                <View style={[styles.flex1, styles.ph5]}>
+                    <View style={[styles.alignSelfCenter, styles.computerIllustrationContainer, styles.pv6]}>
+                        <ImageSVG src={Computer} />
+                    </View>
 
-                <Text style={[styles.textHeadlineH1, styles.pt5]}>{translate('workspace.qbd.setupPage.title')}</Text>
-                <Text style={[styles.textSupporting, styles.textNormal, styles.pt4]}>{translate('workspace.qbd.setupPage.body')}</Text>
-                <View style={[styles.qbdSetupLinkBox, styles.mt5]}>
-                    <CopyTextToClipboard
-                        text={codatSetupLink}
-                        textStyles={[styles.textSupporting]}
-                    />
+                    <Text style={[styles.textHeadlineH1, styles.pt5]}>{translate('workspace.qbd.setupPage.title')}</Text>
+                    <Text style={[styles.textSupporting, styles.textNormal, styles.pt4]}>{translate('workspace.qbd.setupPage.body')}</Text>
+                    <View style={[styles.qbdSetupLinkBox, styles.mt5]}>
+                        <CopyTextToClipboard
+                            text={codatSetupLink}
+                            textStyles={[styles.textSupporting]}
+                        />
+                    </View>
+                    <FixedFooter style={[styles.mtAuto, styles.ph0]}>
+                        <Button
+                            success
+                            text={translate('common.done')}
+                            onPress={() => Navigation.navigate(ROUTES.POLICY_ACCOUNTING_QUICKBOOKS_DESKTOP_TRIGGER_FIRST_SYNC.getRoute(policyID))}
+                            pressOnEnter
+                            large
+                        />
+                    </FixedFooter>
                 </View>
-                <FixedFooter style={[styles.mtAuto, styles.ph0]}>
-                    <Button
-                        success
-                        text={translate('common.done')}
-                        onPress={() => Navigation.navigate(ROUTES.POLICY_ACCOUNTING_QUICKBOOKS_DESKTOP_TRIGGER_FIRST_SYNC.getRoute(policyID))}
-                        pressOnEnter
-                        large
-                    />
-                </FixedFooter>
-            </FullPageView>
+            </ContentWrapper>
         </ScreenWrapper>
     );
 }
