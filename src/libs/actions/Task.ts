c4bec93603fcd1bc43b5a703edf959ef80cb54b6
mkzie2 <mkzie2@gmail.com>
2024-10-16 07:01:53 +0700

    fix: chat with only task is hidden from lhn

--- src/libs/actions/Task.ts
+++ src/libs/actions/Task.ts
@@ -232,6 +232,19 @@ function createTaskAndNavigate(
         },
     );
 
+    const shouldUpdateNotificationPrefernece = !isEmptyObject(parentReport) && ReportUtils.getReportNotificationPreference(parentReport) === CONST.REPORT.NOTIFICATION_PREFERENCE.HIDDEN;
+    if (shouldUpdateNotificationPrefernece) {
+        optimisticData.push({
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: `${ONYXKEYS.COLLECTION.REPORT}${parentReportID}`,
+            value: {
+                participants: {
+                    [currentUserAccountID]: {notificationPreference: CONST.REPORT.NOTIFICATION_PREFERENCE.ALWAYS},
+                },
+            },
+        });
+    }
+
     // FOR QUICK ACTION NVP
     optimisticData.push({
         onyxMethod: Onyx.METHOD.SET,
