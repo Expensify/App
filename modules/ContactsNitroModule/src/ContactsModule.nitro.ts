fdc5ba6962fc1104d0ded5320bfd88165023a574
Taras Perun <terry.perun@gmail.com>
2024-10-17 17:19:32 +0200

    ts spec for ContactsModule

--- modules/ContactsNitroModule/src/ContactsModule.nitro.ts
+++ modules/ContactsNitroModule/src/ContactsModule.nitro.ts
@@ -1,6 +1,29 @@
 import type { HybridObject } from 'react-native-nitro-modules'
 
+interface StringHolder {
+  value: string
+}
+
+interface Contact {
+  firstName?: string
+  lastName?: string
+  middleName?: string
+  phoneNumbers?: StringHolder[]
+  emailAddresses?: StringHolder[]
+  imageData?: string
+  thumbnailImageData?: string
+}
+export type ContactFields =
+  | 'FIRST_NAME'
+  | 'LAST_NAME'
+  | 'MIDDLE_NAME'
+  | 'PHONE_NUMBERS'
+  | 'EMAIL_ADDRESSES'
+  | 'IMAGE_DATA'
+  | 'THUMBNAIL_IMAGE_DATA'
+  | 'GIVEN_NAME_KEY'
+
 export interface ContactsModule
   extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
-  getAll(): void
+  getAll(keys: ContactFields[]): Promise<Contact[]>
 }
