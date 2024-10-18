d725a59f58b895778529111aeef49d12fdd5b8ef
Nicolay Arefyeu <n.arefyev91@gmail.com>
2024-10-18 13:26:25 +0300

    Add constants

--- src/pages/workspace/companyCards/addNew/BankConnection/openBankConnection/index.website.tsx
+++ src/pages/workspace/companyCards/addNew/BankConnection/openBankConnection/index.website.tsx
@@ -1,9 +1,12 @@
+const WINDOW_WIDTH = 700;
+const WINDOW_HEIGHT = 600;
+
 const handleOpenBankConnectionFlow = (url: string) => {
     const screenWidth = window.screen.width;
     const screenHeight = window.screen.height;
-    const left = (screenWidth - 700) / 2;
-    const top = (screenHeight - 600) / 2;
-    const popupFeatures = `width=${700},height=${600},left=${left},top=${top},scrollbars=yes,resizable=yes`;
+    const left = (screenWidth - WINDOW_WIDTH) / 2;
+    const top = (screenHeight - WINDOW_HEIGHT) / 2;
+    const popupFeatures = `width=${WINDOW_WIDTH},height=${WINDOW_HEIGHT},left=${left},top=${top},scrollbars=yes,resizable=yes`;
 
     return window.open(url, 'popupWindow', popupFeatures);
 };
