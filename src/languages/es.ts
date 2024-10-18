c853780931389b68b2ad666613f3f67101852229
VH <hoangzinhvn@gmail.com>
2024-10-18 18:56:26 +0700

    Update translation

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -2488,7 +2488,7 @@ const translations = {
                 createEntities: 'Crear entidades automáticamente',
                 createEntitiesDescription: 'Expensify creará automáticamente proveedores en QuickBooks Desktop si aún no existen, y creará automáticamente clientes al exportar facturas.',
             },
-            itemsDescription: 'Elige cómo gestionar los artículos de QuickBooks Desktop en Expensify.',
+            itemsDescription: 'Elige cómo gestionar los elementos de QuickBooks Desktop en Expensify.',
         },
         qbo: {
             importDescription: 'Elige que configuraciónes de codificación son importadas desde QuickBooks Online a Expensify.',
