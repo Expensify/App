481f5916fee85952034b8109a90a5c857f0fb267
Wojciech Boman <wojciechboman@gmail.com>
2024-10-18 13:09:16 +0200

    Refactor variables names in customGetPathFromState

--- src/libs/Navigation/linkingConfig/customGetPathFromState.ts
+++ src/libs/Navigation/linkingConfig/customGetPathFromState.ts
@@ -7,15 +7,15 @@ const customGetPathFromState: typeof getPathFromState = (state, options) => {
     // For the Home page we should remove policyID from the params, because on small screens it's displayed twice in the URL
     const stateWithoutPolicyID = removePolicyIDParamFromState(state as State<RootStackParamList>);
     const path = getPathFromState(stateWithoutPolicyID, options);
-    const topmostSplitOrSearchRoute = state.routes.findLast((route) => isFullScreenRoute(route as NavigationPartialRoute));
+    const fullScreenRoute = state.routes.findLast((route) => isFullScreenRoute(route as NavigationPartialRoute));
 
-    const shouldAddPolicyID = topmostSplitOrSearchRoute?.name === NAVIGATORS.REPORTS_SPLIT_NAVIGATOR;
+    const shouldAddPolicyID = fullScreenRoute?.name === NAVIGATORS.REPORTS_SPLIT_NAVIGATOR;
 
     if (!shouldAddPolicyID) {
         return path;
     }
 
-    const policyID = topmostSplitOrSearchRoute.params && `policyID` in topmostSplitOrSearchRoute.params ? (topmostSplitOrSearchRoute.params.policyID as string) : undefined;
+    const policyID = fullScreenRoute.params && `policyID` in fullScreenRoute.params ? (fullScreenRoute.params.policyID as string) : undefined;
 
     return `${policyID ? `/w/${policyID}` : ''}${path}`;
 };
