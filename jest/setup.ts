52a039f96c7ea0e1031df29b919395bc94ef59f5
kirillzyusko <kiryl@margelo.io>
2024-10-18 13:47:37 +0200

    feat: react-native-app-logs integration

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,5 +1,6 @@
 /* eslint-disable max-classes-per-file */
 import '@shopify/flash-list/jestSetup';
+import type * as RNAppLogs from 'react-native-app-logs';
 import 'react-native-gesture-handler/jestSetup';
 import type * as RNKeyboardController from 'react-native-keyboard-controller';
 import mockStorage from 'react-native-onyx/dist/storage/__mocks__';
@@ -74,6 +75,8 @@ jest.mock('react-native-reanimated', () => ({
 }));
 
 jest.mock('react-native-keyboard-controller', () => require<typeof RNKeyboardController>('react-native-keyboard-controller/jest'));
+
+jest.mock('react-native-app-logs', () => require<typeof RNAppLogs>('react-native-app-logs/jest'));
 
 jest.mock('@src/libs/actions/Timing', () => ({
     start: jest.fn(),
