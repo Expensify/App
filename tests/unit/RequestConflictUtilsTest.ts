e42d272bff0734442e2d4654c6de83c87587aaab
Povilas Zirgulis <pzirgulis@gmail.com>
2024-10-18 17:21:09 +0300

    add filterUpdateCommentRequest and filterRequestByCommand utils

--- tests/unit/RequestConflictUtilsTest.ts
+++ tests/unit/RequestConflictUtilsTest.ts
@@ -1,11 +1,11 @@
-import resolveDuplicationConflictAction from '@libs/actions/RequestConflictUtils';
+import {filterRequestByCommand, resolveDuplicationConflictAction} from '@libs/actions/RequestConflictUtils';
 import type {WriteCommand} from '@libs/API/types';
 
 describe('RequestConflictUtils', () => {
     it.each([['OpenApp'], ['ReconnectApp']])('resolveDuplicationConflictAction when %s do not exist in the queue should push %i', (command) => {
         const persistedRequests = [{command: 'OpenReport'}, {command: 'AddComment'}, {command: 'CloseAccount'}];
         const commandToFind = command as WriteCommand;
-        const result = resolveDuplicationConflictAction(persistedRequests, commandToFind);
+        const result = resolveDuplicationConflictAction(persistedRequests, filterRequestByCommand(commandToFind));
         expect(result).toEqual({conflictAction: {type: 'push'}});
     });
 
@@ -15,7 +15,7 @@ describe('RequestConflictUtils', () => {
     ])('resolveDuplicationConflictAction when %s exist in the queue should replace at index %i', (command, index) => {
         const persistedRequests = [{command: 'OpenApp'}, {command: 'AddComment'}, {command: 'ReconnectApp'}];
         const commandToFind = command as WriteCommand;
-        const result = resolveDuplicationConflictAction(persistedRequests, commandToFind);
+        const result = resolveDuplicationConflictAction(persistedRequests, filterRequestByCommand(commandToFind));
         expect(result).toEqual({conflictAction: {type: 'replace', index}});
     });
 });
