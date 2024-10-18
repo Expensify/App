62e72643d9e99213767f6ff17924f91bc14b6d76
Eduardo <eduardo.graciano@callstack.com>
2024-10-18 15:40:35 +0200

    Replace duplicated openReports

--- src/libs/actions/RequestConflictUtils.ts
+++ src/libs/actions/RequestConflictUtils.ts
@@ -1,6 +1,7 @@
-import type {WriteCommand} from '@libs/API/types';
 import type OnyxRequest from '@src/types/onyx/Request';
 import type {ConflictActionData} from '@src/types/onyx/Request';
+
+type RequestMatcher = (request: OnyxRequest) => boolean;
 
 /**
  * Resolves duplication conflicts between persisted requests and a given command.
@@ -9,8 +10,8 @@ import type {ConflictActionData} from '@src/types/onyx/Request';
  * - If the command is not found, it suggests adding the command to the list, indicating a 'push' action.
  * - If the command is found, it suggests updating the existing entry, indicating a 'replace' action at the found index.
  */
-function resolveDuplicationConflictAction(persistedRequests: OnyxRequest[], commandToFind: WriteCommand): ConflictActionData {
-    const index = persistedRequests.findIndex((request) => request.command === commandToFind);
+function resolveDuplicationConflictAction(persistedRequests: OnyxRequest[], requestMatcher: RequestMatcher): ConflictActionData {
+    const index = persistedRequests.findIndex(requestMatcher);
     if (index === -1) {
         return {
             conflictAction: {
