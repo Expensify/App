5025a7ba753f73546b4fa7f13623a81549cd62e8
Andrew Rosiclair <arosiclair@expensify.com>
2024-10-21 15:31:50 -0400

    add doc

--- src/libs/GoogleTagManager/types.ts
+++ src/libs/GoogleTagManager/types.ts
@@ -1,3 +1,7 @@
+/**
+ * An event that can be published to Google Tag Manager. New events must be configured in GTM before they can be used
+ * in the app.
+ */
 type GoogleTagManagerEvent = 'sign_up' | 'workspace_created' | 'paid_adoption';
 
 // eslint-disable-next-line import/prefer-default-export
