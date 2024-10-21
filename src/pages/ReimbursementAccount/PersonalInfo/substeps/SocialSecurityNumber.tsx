ea4686234c1c90f6dc27750c21293cb0ec3042ab
burczu <b.dybowski@gmail.com>
2024-10-21 10:08:43 +0200

    common single field step used for personal info page

--- src/pages/ReimbursementAccount/PersonalInfo/substeps/SocialSecurityNumber.tsx
+++ src/pages/ReimbursementAccount/PersonalInfo/substeps/SocialSecurityNumber.tsx
@@ -1,19 +1,12 @@
 import React, {useCallback} from 'react';
-import {View} from 'react-native';
 import type {OnyxEntry} from 'react-native-onyx';
 import {withOnyx} from 'react-native-onyx';
-import FormProvider from '@components/Form/FormProvider';
-import InputWrapper from '@components/Form/InputWrapper';
 import type {FormInputErrors, FormOnyxValues} from '@components/Form/types';
-import Text from '@components/Text';
-import TextInput from '@components/TextInput';
+import SingleFieldStep from '@components/SubStepForms/SingleFieldStep';
 import useLocalize from '@hooks/useLocalize';
 import useReimbursementAccountStepFormSubmit from '@hooks/useReimbursementAccountStepFormSubmit';
 import type {SubStepProps} from '@hooks/useSubStep/types';
-import useThemeStyles from '@hooks/useThemeStyles';
 import * as ValidationUtils from '@libs/ValidationUtils';
-import HelpLinks from '@pages/ReimbursementAccount/PersonalInfo/HelpLinks';
-import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import INPUT_IDS from '@src/types/form/ReimbursementAccountForm';
 import type {ReimbursementAccount} from '@src/types/onyx';
@@ -28,9 +21,8 @@ type SocialSecurityNumberProps = SocialSecurityNumberOnyxProps & SubStepProps;
 const PERSONAL_INFO_STEP_KEY = INPUT_IDS.PERSONAL_INFO_STEP;
 const STEP_FIELDS = [PERSONAL_INFO_STEP_KEY.SSN_LAST_4];
 
-function SocialSecurityNumber({reimbursementAccount, onNext, isEditing}: SocialSecurityNumberProps) {
+function SocialSecurityNumber({reimbursementAccount, onNext, onMove, isEditing}: SocialSecurityNumberProps) {
     const {translate} = useLocalize();
-    const styles = useThemeStyles();
 
     const defaultSsnLast4 = reimbursementAccount?.achData?.[PERSONAL_INFO_STEP_KEY.SSN_LAST_4] ?? '';
 
@@ -54,38 +46,23 @@ function SocialSecurityNumber({reimbursementAccount, onNext, isEditing}: SocialS
     });
 
     return (
-        <FormProvider
+        <SingleFieldStep<typeof ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM>
+            isEditing={isEditing}
+            onNext={onNext}
+            onMove={onMove}
             formID={ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM}
-            submitButtonText={translate(isEditing ? 'common.confirm' : 'common.next')}
+            formTitle={translate('personalInfoStep.enterTheLast4')}
+            formDisclaimer={translate('personalInfoStep.dontWorry')}
             validate={validate}
             onSubmit={handleSubmit}
-            style={[styles.mh5, styles.flexGrow1]}
-            submitButtonStyles={[styles.mb0]}
-        >
-            <View>
-                <Text style={[styles.textHeadlineLineHeightXXL, styles.mb3]}>{translate('personalInfoStep.enterTheLast4')}</Text>
-                <Text style={[styles.textSupporting]}>{translate('personalInfoStep.dontWorry')}</Text>
-                <View style={[styles.flex1]}>
-                    <InputWrapper
-                        InputComponent={TextInput}
-                        inputID={PERSONAL_INFO_STEP_KEY.SSN_LAST_4}
-                        label={translate('personalInfoStep.last4SSN')}
-                        aria-label={translate('personalInfoStep.last4SSN')}
-                        role={CONST.ROLE.PRESENTATION}
-                        containerStyles={[styles.mt6]}
-                        inputMode={CONST.INPUT_MODE.NUMERIC}
-                        defaultValue={defaultSsnLast4}
-                        maxLength={CONST.BANK_ACCOUNT.MAX_LENGTH.SSN}
-                        shouldSaveDraft={!isEditing}
-                    />
-                </View>
-                <HelpLinks containerStyles={[styles.mt5]} />
-            </View>
-        </FormProvider>
+            inputId={PERSONAL_INFO_STEP_KEY.SSN_LAST_4}
+            inputLabel={translate('personalInfoStep.last4SSN')}
+            defaultValue={defaultSsnLast4}
+        />
     );
 }
 
-SocialSecurityNumber.displayName = 'SocialSecurityNumber';
+SocialSecurityNumber.defaultName = 'SocialSecurityNumber';
 
 export default withOnyx<SocialSecurityNumberProps, SocialSecurityNumberOnyxProps>({
     // @ts-expect-error: ONYXKEYS.REIMBURSEMENT_ACCOUNT is conflicting with ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM
