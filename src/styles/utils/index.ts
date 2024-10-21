ec1b3de1f10e77b695112a8a8de434128b14324a
nkdengineer <nkdengineer@outlook.com>
2024-10-21 15:12:25 +0700

    change background color of focus element

--- src/styles/utils/index.ts
+++ src/styles/utils/index.ts
@@ -1119,6 +1119,21 @@ function getAmountWidth(amount: string): number {
     return width;
 }
 
+function getItemBackgroundColorStyle(isSelected: boolean, isFocused: boolean, selectedBG: string, focusedBG: string): ViewStyle {
+    let backgroundColor;
+    if (isSelected) {
+        backgroundColor = selectedBG;
+    }
+
+    if (isFocused) {
+        backgroundColor = focusedBG;
+    }
+
+    return {
+        backgroundColor,
+    };
+}
+
 const staticStyleUtils = {
     positioning,
     combineStyles,
@@ -1193,6 +1208,7 @@ const staticStyleUtils = {
     getAmountWidth,
     getBorderRadiusStyle,
     getHighResolutionInfoWrapperStyle,
+    getItemBackgroundColorStyle,
 };
 
 const createStyleUtils = (theme: ThemeColors, styles: ThemeStyles) => ({
@@ -1211,9 +1227,7 @@ const createStyleUtils = (theme: ThemeColors, styles: ThemeStyles) => ({
     getAutoCompleteSuggestionItemStyle: (highlightedEmojiIndex: number, rowHeight: number, isHovered: boolean, currentEmojiIndex: number): ViewStyle[] => {
         let backgroundColor;
 
-        if (currentEmojiIndex === highlightedEmojiIndex) {
-            backgroundColor = theme.activeComponentBG;
-        } else if (isHovered) {
+        if (isHovered || currentEmojiIndex === highlightedEmojiIndex) {
             backgroundColor = theme.hoverComponentBG;
         }
 
