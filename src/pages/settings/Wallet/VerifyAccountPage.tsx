412bc42588525300ce568dd8d1eedc56b3ca20fb
Getabalew <getudesta4@gmail.com>
2024-10-22 19:01:47 +0300

    fix: reuse validate-code-action-moda in wallet settings page

--- src/pages/settings/Wallet/VerifyAccountPage.tsx
+++ src/pages/settings/Wallet/VerifyAccountPage.tsx
@@ -1,15 +1,8 @@
 import type {StackScreenProps} from '@react-navigation/stack';
-import React, {useCallback, useEffect, useRef} from 'react';
-import {View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useOnyx} from 'react-native-onyx';
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
-import type {AnimatedTextInputRef} from '@components/RNTextInput';
-import ScreenWrapper from '@components/ScreenWrapper';
-import Text from '@components/Text';
-import ValidateCodeForm from '@components/ValidateCodeActionModal/ValidateCodeForm';
 import useLocalize from '@hooks/useLocalize';
 import useSafePaddingBottomStyle from '@hooks/useSafePaddingBottomStyle';
-import useThemeStyles from '@hooks/useThemeStyles';
 import * as ErrorUtils from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
@@ -17,6 +10,7 @@ import * as User from '@userActions/User';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
+import ValidateCodeActionModal from "@components/ValidateCodeActionModal";
 
 type VerifyAccountPageProps = StackScreenProps<SettingsNavigatorParamList, typeof SCREENS.SETTINGS.PROFILE.NEW_CONTACT_METHOD>;
 
@@ -24,16 +18,11 @@ function VerifyAccountPage({route}: VerifyAccountPageProps) {
     const [account] = useOnyx(ONYXKEYS.ACCOUNT);
     const [loginList] = useOnyx(ONYXKEYS.LOGIN_LIST);
     const contactMethod = account?.primaryLogin ?? '';
-    const themeStyles = useThemeStyles();
     const {translate} = useLocalize();
-    const safePaddingBottomStyle = useSafePaddingBottomStyle();
-    const loginInputRef = useRef<AnimatedTextInputRef>(null);
     const loginData = loginList?.[contactMethod];
-    const styles = useThemeStyles();
     const validateLoginError = ErrorUtils.getEarliestErrorField(loginData, 'validateLogin');
     const [isUserValidated] = useOnyx(ONYXKEYS.USER, {selector: (user) => !!user?.validated});
-
-    const [validateCodeAction] = useOnyx(ONYXKEYS.VALIDATE_ACTION_CODE);
+    const [isValidateCodeActionModalVisible, setIsValidateCodeActionModalVisible] = useState(true);
 
     const navigateBackTo = route?.params?.backTo ?? ROUTES.SETTINGS_WALLET;
 
@@ -55,31 +44,25 @@ function VerifyAccountPage({route}: VerifyAccountPageProps) {
             return;
         }
         Navigation.navigate(navigateBackTo);
+        setIsValidateCodeActionModalVisible(false);
     }, [isUserValidated, navigateBackTo]);
 
     return (
-        <ScreenWrapper
-            onEntryTransitionEnd={() => loginInputRef.current?.focus()}
-            includeSafeAreaPaddingBottom={false}
-            shouldEnableMaxHeight
-            testID={VerifyAccountPage.displayName}
-        >
-            <HeaderWithBackButton
-                title={translate('contacts.validateAccount')}
-                onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS_WALLET)}
-            />
-            <View style={[themeStyles.ph5, themeStyles.mt3, themeStyles.mb7, styles.flex1]}>
-                <Text style={[themeStyles.mb3]}>{translate('contacts.featureRequiresValidate')}</Text>
-                <ValidateCodeForm
-                    sendValidateCode={() => User.requestValidateCodeAction()}
-                    validateCodeAction={validateCodeAction}
-                    validateError={validateLoginError}
-                    handleSubmitForm={handleSubmitForm}
-                    clearError={clearError}
-                    buttonStyles={[styles.justifyContentEnd, styles.flex1, safePaddingBottomStyle]}
-                />
-            </View>
-        </ScreenWrapper>
+        <ValidateCodeActionModal
+            sendValidateCode={() => User.requestValidateCodeAction()}
+            handleSubmitForm={handleSubmitForm}
+            validateError={validateLoginError}
+            isVisible={isValidateCodeActionModalVisible}
+            title={translate('contacts.validateAccount')}
+            description={translate('contacts.featureRequiresValidate')}
+            onClose={() => {
+                setIsValidateCodeActionModalVisible(false)
+                if (!isUserValidated) {
+                    Navigation.navigate(ROUTES.SETTINGS_WALLET);
+                }
+            }}
+            clearError={clearError}
+        />
     );
 }
 
