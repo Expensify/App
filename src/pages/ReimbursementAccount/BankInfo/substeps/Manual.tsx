ebba9671a2ad2684ad19e7c9f9b43350c0cdd056
cretadn22 <cretadn22@gmail.com>
2024-10-22 18:08:35 +0700

    adjust the condition

--- src/pages/ReimbursementAccount/BankInfo/substeps/Manual.tsx
+++ src/pages/ReimbursementAccount/BankInfo/substeps/Manual.tsx
@@ -34,7 +34,6 @@ function Manual({onNext}: ManualProps) {
     };
 
     const hasBankAccountData = !!(reimbursementAccount?.achData?.bankAccountID ?? '');
-    const shouldBeReadOnlyInput = reimbursementAccount?.achData?.setupType === CONST.BANK_ACCOUNT.SETUP_TYPE.PLAID || hasBankAccountData;
 
     const validate = useCallback(
         (values: FormOnyxValues<typeof ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM>): FormInputErrors<typeof ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM> => {
@@ -87,7 +86,7 @@ function Manual({onNext}: ManualProps) {
                 inputMode={CONST.INPUT_MODE.NUMERIC}
                 shouldSaveDraft
                 shouldUseDefaultValue={hasBankAccountData}
-                disabled={shouldBeReadOnlyInput}
+                disabled={hasBankAccountData}
             />
             <InputWrapper
                 InputComponent={TextInput}
@@ -100,7 +99,7 @@ function Manual({onNext}: ManualProps) {
                 inputMode={CONST.INPUT_MODE.NUMERIC}
                 shouldSaveDraft
                 shouldUseDefaultValue={hasBankAccountData}
-                disabled={shouldBeReadOnlyInput}
+                disabled={hasBankAccountData}
             />
         </FormProvider>
     );
