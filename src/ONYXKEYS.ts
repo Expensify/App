4a721c48505ad0725d7aff8155b13b9f29c0e4b2
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-10-23 11:40:38 +0200

    Revert "Revert "[HybridApp] Allow classic experience users to use NewDot travel page""
    This reverts commit 3d342ee74e418637451323a17cc54170aafbe659.

--- src/ONYXKEYS.ts
+++ src/ONYXKEYS.ts
@@ -441,6 +441,9 @@ const ONYXKEYS = {
     /** Stores recently used currencies */
     RECENTLY_USED_CURRENCIES: 'nvp_recentlyUsedCurrencies',
 
+    /** States whether we transitioned from OldDot to show only certain group of screens. It should be undefined on pure NewDot. */
+    IS_SINGLE_NEW_DOT_ENTRY: 'isSingleNewDotEntry',
+
     /** Company cards custom names */
     NVP_EXPENSIFY_COMPANY_CARDS_CUSTOM_NAMES: 'nvp_expensify_ccCustomNames',
 
@@ -1004,6 +1007,7 @@ type OnyxValuesMapping = {
     [ONYXKEYS.APPROVAL_WORKFLOW]: OnyxTypes.ApprovalWorkflowOnyx;
     [ONYXKEYS.IMPORTED_SPREADSHEET]: OnyxTypes.ImportedSpreadsheet;
     [ONYXKEYS.LAST_ROUTE]: string;
+    [ONYXKEYS.IS_SINGLE_NEW_DOT_ENTRY]: boolean | undefined;
     [ONYXKEYS.IS_USING_IMPORTED_STATE]: boolean;
     [ONYXKEYS.SHOULD_SHOW_SAVED_SEARCH_RENAME_TOOLTIP]: boolean;
     [ONYXKEYS.NVP_EXPENSIFY_COMPANY_CARDS_CUSTOM_NAMES]: Record<string, string>;
