0136b74fa99527288179c993b5b904efaee60ce1
burczu <b.dybowski@gmail.com>
2024-10-23 07:35:09 +0200

    unused form action methods removed

--- src/libs/actions/FormActions.ts
+++ src/libs/actions/FormActions.ts
@@ -31,12 +31,4 @@ function clearDraftValues(formID: OnyxFormKey) {
     Onyx.set(`${formID}Draft`, null);
 }
 
-function setFormValues(formId: OnyxFormKey, values: NullishDeep<OnyxValue<OnyxFormDraftKey>>) {
-    Onyx.merge(formId, values ?? null);
-}
-
-function clearFormValues(formId: OnyxFormKey) {
-    Onyx.set(formId, null);
-}
-
-export {clearDraftValues, clearErrorFields, clearErrors, setDraftValues, setErrorFields, setErrors, setIsLoading, setFormValues, clearFormValues};
+export {clearDraftValues, clearErrorFields, clearErrors, setDraftValues, setErrorFields, setErrors, setIsLoading};
