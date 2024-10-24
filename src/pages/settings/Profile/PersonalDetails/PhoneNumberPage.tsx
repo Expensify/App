89b1f44d52d2cd408712a292a62a585dd515246e
Gandalf <whogandalf@gmail.com>
2024-10-24 11:57:36 +0530

    fix lint

--- src/pages/settings/Profile/PersonalDetails/PhoneNumberPage.tsx
+++ src/pages/settings/Profile/PersonalDetails/PhoneNumberPage.tsx
@@ -100,6 +100,7 @@ function PhoneNumberPage() {
                             onBlur={() => {
                                 if (validateLoginError) {
                                     PersonalDetails.clearPhoneNumberError();
+                                    return;
                                 }
                             }}
                         />
