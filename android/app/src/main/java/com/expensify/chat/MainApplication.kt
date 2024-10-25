180a8b22a04199573ab4a087025438b76011e2f3
Tim Golen <tgolen@gmail.com>
2024-10-25 11:23:49 -0600

    Merge pull request #51489 from software-mansion-labs/@wolewicki/revert-bridgeless
    fix: revert bridgeless on Android
    (cherry picked from commit 2db49d4759d0008e2fb177523cd574290513e3c8)
    
    (CP triggered by Beamanator)

--- android/app/src/main/java/com/expensify/chat/MainApplication.kt
+++ android/app/src/main/java/com/expensify/chat/MainApplication.kt
@@ -64,7 +64,7 @@ class MainApplication : MultiDexApplication(), ReactApplication {
         SoLoader.init(this,  /* native exopackage */false)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load()
+            load(bridgelessEnabled = false)
         }
         if (BuildConfig.DEBUG) {
             FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
