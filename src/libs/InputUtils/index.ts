283c00ca7bb6981045ce07418f6c727d398af73e
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-10-26 13:49:09 +0800

    lint

--- src/libs/InputUtils/index.ts
+++ src/libs/InputUtils/index.ts
@@ -1,6 +1,8 @@
 import type {TextInput} from 'react-native';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function scrollToBottom(_input: TextInput) {}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function moveSelectionToEnd(_input: TextInput) {}
 
 export {scrollToBottom, moveSelectionToEnd};
