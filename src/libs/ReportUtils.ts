ffa5ed86dcaf03f550193fefa3355fc1bfc4a1b2
NJ-2020 <ntt.london@gmail.com>
2024-10-26 23:15:45 -0700

    fix eslint error

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -5046,6 +5046,7 @@ function buildOptimisticReportPreview(
     const hasReceipt = TransactionUtils.hasReceipt(transaction);
     const message = getReportPreviewMessage(iouReport);
     const created = DateUtils.getDBTime();
+    const reportActorAccountID = isInvoiceReport(iouReport) ? iouReport.ownerAccountID : iouReport?.managerID ?? -1;
     return {
         reportActionID: reportActionID ?? NumberUtils.rand64(),
         reportID: chatReport?.reportID,
@@ -5065,7 +5066,7 @@ function buildOptimisticReportPreview(
         created,
         accountID: iouReport?.managerID ?? -1,
         // The preview is initially whispered if created with a receipt, so the actor is the current user as well
-        actorAccountID: hasReceipt ? currentUserAccountID : isInvoiceReport(iouReport) ? iouReport.ownerAccountID : iouReport?.managerID ?? -1,
+        actorAccountID: hasReceipt ? currentUserAccountID : reportActorAccountID,
         childReportID: childReportID ?? iouReport?.reportID,
         childMoneyRequestCount: 1,
         childLastMoneyRequestComment: comment,
