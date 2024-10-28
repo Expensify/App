2c477ec061f5ebee1bd3e5cb5ed0d1f5df9ac6bb
Jakub Butkiewicz <jakub.butkiewicz94@gmail.com>
2024-10-28 13:18:28 +0100

    fix: Warning: Function components cannot be given refs.

--- src/components/LHNOptionsList/OptionRowRendererComponent/index.native.tsx
+++ src/components/LHNOptionsList/OptionRowRendererComponent/index.native.tsx
@@ -1,11 +1,14 @@
 import {CellContainer} from '@shopify/flash-list';
 import type {CellContainerProps} from '@shopify/flash-list/dist/native/cell-container/CellContainer';
+import type {ForwardedRef} from 'react';
+import {forwardRef} from 'react';
 
-function OptionRowRendererComponent(props: CellContainerProps) {
+function OptionRowRendererComponent(props: CellContainerProps, ref: ForwardedRef<unknown>) {
     return (
         <CellContainer
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
+            ref={ref}
             style={[props.style, {zIndex: -props.index}]}
         />
     );
@@ -13,4 +16,4 @@ function OptionRowRendererComponent(props: CellContainerProps) {
 
 OptionRowRendererComponent.displayName = 'OptionRowRendererComponent';
 
-export default OptionRowRendererComponent;
+export default forwardRef(OptionRowRendererComponent);
