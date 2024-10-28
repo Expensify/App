5a8cdbc78930a7a809d971a1712e45c00be8dcba
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-10-28 17:57:39 +0100

    fix: enable bridgeless again

--- android/app/src/main/java/com/expensify/chat/MainApplication.kt
+++ android/app/src/main/java/com/expensify/chat/MainApplication.kt
@@ -65,7 +65,7 @@ class MainApplication : MultiDexApplication(), ReactApplication {
         SoLoader.init(this, OpenSourceMergedSoMapping)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load(bridgelessEnabled = false)
+            load()
         }
         if (BuildConfig.DEBUG) {
             FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
