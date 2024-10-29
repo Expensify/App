eb4ab896c942c75175f3be7e15f87df5003799ce
Anusha <anusharukhsar1@gmail.com>
2024-10-29 19:22:31 +0500

    fix navigation

--- src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer/index.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/MentionReportRenderer/index.tsx
@@ -10,10 +10,13 @@ import Text from '@components/Text';
 import useCurrentReportID from '@hooks/useCurrentReportID';
 import useStyleUtils from '@hooks/useStyleUtils';
 import useThemeStyles from '@hooks/useThemeStyles';
-import Navigation from '@navigation/Navigation';
+import getTopmostCentralPaneRoute from '@libs/Navigation/getTopmostCentralPaneRoute';
+import {RootStackParamList, State} from '@libs/Navigation/types';
+import Navigation, {navigationRef} from '@navigation/Navigation';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
+import SCREENS from '@src/SCREENS';
 import type {Report} from '@src/types/onyx';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import MentionReportContext from './MentionReportContext';
@@ -69,7 +72,12 @@ function MentionReportRenderer({style, tnode, TDefaultRenderer, ...defaultRender
     }
     const {reportID, mentionDisplayText} = mentionDetails;
 
-    const navigationRoute = reportID ? ROUTES.REPORT_WITH_ID.getRoute(reportID) : undefined;
+    let navigationRoute: any = reportID ? ROUTES.REPORT_WITH_ID.getRoute(reportID) : undefined;
+    const topmostCentralPaneRoute = getTopmostCentralPaneRoute(navigationRef.getRootState() as State<RootStackParamList>);
+    const backTo = Navigation.getActiveRoute();
+    if (topmostCentralPaneRoute?.name === SCREENS.SEARCH.CENTRAL_PANE) {
+        navigationRoute = reportID ? ROUTES.SEARCH_REPORT.getRoute({reportID, backTo}) : undefined;
+    }
     const isCurrentRoomMention = reportID === currentReportIDValue;
 
     const flattenStyle = StyleSheet.flatten(style as TextStyle);
