c856af356d4e6e450cf7021ace103bf0f1cec6ce
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-10-29 15:06:40 +0100

    fix: patch devmenu on bridgeless

--- android/app/src/main/java/com/expensify/chat/MainApplication.kt
+++ android/app/src/main/java/com/expensify/chat/MainApplication.kt
@@ -64,7 +64,7 @@ class MainApplication : MultiDexApplication(), ReactApplication {
         SoLoader.init(this,  /* native exopackage */false)
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
-            load(bridgelessEnabled = false)
+            load()
         }
         if (BuildConfig.DEBUG) {
             FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(false)
