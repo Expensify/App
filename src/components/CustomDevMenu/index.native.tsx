c5c37e1abccb6fbf640f5e011d6b99410fe79f86
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-10-29 16:51:57 +0100

    fix: use DevSettings instead of lib

--- src/components/CustomDevMenu/index.native.tsx
+++ src/components/CustomDevMenu/index.native.tsx
@@ -1,4 +1,5 @@
 import {useEffect} from 'react';
+import {DevSettings} from 'react-native';
 import DevMenu from 'react-native-dev-menu';
 import toggleTestToolsModal from '@userActions/TestTool';
 import type CustomDevMenuElement from './types';
@@ -6,7 +7,7 @@ import type CustomDevMenuElement from './types';
 const CustomDevMenu: CustomDevMenuElement = Object.assign(
     () => {
         useEffect(() => {
-            DevMenu.addItem('Open Test Preferences', toggleTestToolsModal);
+            DevSettings.addMenuItem('Open Test Preferences', toggleTestToolsModal);
         }, []);
         // eslint-disable-next-line react/jsx-no-useless-fragment
         return <></>;
