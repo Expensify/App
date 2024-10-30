acf8c268abfd92d90aee854b678cbc53a88dac84
Lucien Akchoté <lucien@expensify.com>
2024-10-30 19:45:23 +0100

    add comment

--- src/libs/API/parameters/PayMoneyRequestOnSearchParams.ts
+++ src/libs/API/parameters/PayMoneyRequestOnSearchParams.ts
@@ -1,6 +1,10 @@
 type PayMoneyRequestOnSearchParams = {
     hash: number;
     paymentType: string;
+    /**
+     * Stringified JSON object with type of following structure:
+     * Array<{reportID: string, amount: number}>
+     */
     reportsAndAmounts: string;
 };
 
