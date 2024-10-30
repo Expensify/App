23e436b0988c544c8c37bd16e61c7aaefeb504a8
cdOut <88325488+cdOut@users.noreply.github.com>
2024-10-30 23:50:15 +0100

    add longer explaination comment

--- src/components/InlineCodeBlock/WrappedText.tsx
+++ src/components/InlineCodeBlock/WrappedText.tsx
@@ -43,7 +43,20 @@ function containsEmoji(text: string): boolean {
 }
 
 /**
- * Splits long words into multiple strings
+ * Takes a long word and splits it into an array of sub-strings.
+ * 
+ * The function tests whether the length of the provided word exceeds the provided maximum length.
+ * If the word's length is less than or equal to `maxLength`, it returns an array with the original word.
+ * If the word's length exceeds 'maxLength', it utilizes a regular expression to split the word into
+ * substrings with a specified 'maxLength' and returns them as an array of strings.
+ *
+ * @param {string} word - The original word to be split.
+ * @param {number} maxLength - The maximum length of each substring.
+ * @return {string[]} An array of substrings derived from the original word.
+ * 
+ * @example
+ * splitLongWord('longteststring', 4);
+ * // Output: ['long', 'test', 'stri', 'ng']
  */
 function splitLongWord(word: string, maxLength: number): string[] {
     if (word.length <= maxLength) {
