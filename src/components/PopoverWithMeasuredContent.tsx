3ef967ffbeaf08b124ad7a8d9aebb8a41aabad4d
kirillzyusko <kiryl@margelo.io>
2024-10-31 14:21:22 +0100

    fix: bottom sheet avoidance when keyboard is hidden

--- src/components/PopoverWithMeasuredContent.tsx
+++ src/components/PopoverWithMeasuredContent.tsx
@@ -98,7 +98,7 @@ function PopoverWithMeasuredContent({
         // it handles the case when `measurePopover` is called with values like: 192, 192.00003051757812, 192
         // if we update it, then animation in `ActionSheetAwareScrollView` may be re-running
         // and we'll see unsynchronized and junky animation
-        if (actionSheetAwareScrollViewContext.currentActionSheetState.value.current.payload?.popoverHeight !== Math.floor(popoverHeight)) {
+        if (actionSheetAwareScrollViewContext.currentActionSheetState.value.current.payload?.popoverHeight !== Math.floor(height) && height !== 0) {
             actionSheetAwareScrollViewContext.transitionActionSheetState({
                 type: ActionSheetAwareScrollView.Actions.MEASURE_POPOVER,
                 payload: {
