ec6315eb5631c3ff6a34aa5eb9e403a029763d22
war-in <war-in@users.noreply.github.com>
2024-10-31 14:16:00 +0100

    reset onyx values on start

--- src/libs/actions/Session/index.ts
+++ src/libs/actions/Session/index.ts
@@ -517,6 +517,11 @@ function signInAfterTransitionFromOldDot(transitionURL: string) {
                 Onyx.multiSet({
                     [ONYXKEYS.USE_NEWDOT_SIGN_IN_PAGE]: useNewDotSignInPage === 'true',
                     [ONYXKEYS.NVP_TRYNEWDOT]: {classicRedirect: {dismissed}}, // This data is mocked and should be returned by BeginSignUp/SignInUser API commands
+                    [ONYXKEYS.HYBRID_APP]: {
+                        readyToSwitchToClassicExperience: false,
+                        readyToShowAuthScreens: useNewDotSignInPage !== 'true',
+                        isSigningIn: false,
+                    },
                 });
             })
             .then(initAppAfterTransition)
