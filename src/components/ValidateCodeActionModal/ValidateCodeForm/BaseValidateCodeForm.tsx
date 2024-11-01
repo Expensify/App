a4e8b0eda6dcb5e83d907bcfbe7783ff06b4c273
shahinyan11 <shahe.shahinyan@gmail.com>
2024-11-01 18:03:34 +0400

    Fix the Verify button position

--- src/components/ValidateCodeActionModal/ValidateCodeForm/BaseValidateCodeForm.tsx
+++ src/components/ValidateCodeActionModal/ValidateCodeForm/BaseValidateCodeForm.tsx
@@ -253,6 +253,7 @@ function BaseValidateCodeForm({
                 />
             )}
             <OfflineWithFeedback
+                shouldDisplayErrorAbove
                 pendingAction={validatePendingAction}
                 errors={validateError}
                 errorRowStyles={[styles.mt2]}
