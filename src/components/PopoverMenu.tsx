64a39d69d1c6cd0d6478ba9d41eab05077777ce8
Carlos Martins <cmartins@expensify.com>
2024-11-04 13:25:35 -0700

    handle disabled case

--- src/components/PopoverMenu.tsx
+++ src/components/PopoverMenu.tsx
@@ -264,7 +264,7 @@ function PopoverMenu({
                         }
                         setFocusedIndex(menuIndex);
                     }}
-                    wrapperStyle={StyleUtils.getItemBackgroundColorStyle(!!item.isSelected, focusedIndex === menuIndex, theme.activeComponentBG, theme.hoverComponentBG)}
+                    wrapperStyle={StyleUtils.getItemBackgroundColorStyle(!!item.isSelected, focusedIndex === menuIndex, item.disabled ?? false, theme.activeComponentBG, theme.hoverComponentBG)}
                     shouldRemoveHoverBackground={item.isSelected}
                     titleStyle={StyleSheet.flatten([styles.flex1, item.titleStyle])}
                     // Spread other props dynamically
