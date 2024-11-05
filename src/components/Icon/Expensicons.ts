163be2279e7ec522a55610c4fd80635a29ca2d68
Klajdi Paja <klajdipaja@gmail.com>
2024-11-05 09:05:57 +0100

    Revert "remove unused DocumentSlash icon"
    This reverts commit f1585a2918230ba2329ff57d3cc493ec5b5f55c8.

--- src/components/Icon/Expensicons.ts
+++ src/components/Icon/Expensicons.ts
@@ -60,6 +60,7 @@ import CreditCardHourglass from '@assets/images/credit-card-hourglass.svg';
 import CreditCard from '@assets/images/creditcard.svg';
 import Crosshair from '@assets/images/crosshair.svg';
 import DocumentPlus from '@assets/images/document-plus.svg';
+import DocumentSlash from '@assets/images/document-slash.svg';
 import Document from '@assets/images/document.svg';
 import DotIndicatorUnfilled from '@assets/images/dot-indicator-unfilled.svg';
 import DotIndicator from '@assets/images/dot-indicator.svg';
@@ -250,6 +251,7 @@ export {
     CreditCardExclamation,
     DeletedRoomAvatar,
     Document,
+    DocumentSlash,
     DomainRoomAvatar,
     DotIndicator,
     DotIndicatorUnfilled,
