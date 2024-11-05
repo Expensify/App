6f85fe37fd59d27848459b25c91dc03dd31b84ff
Blazej Kustra <blazej.kustra@swmansion.com>
2024-11-05 11:16:30 +0100

    Add explanation to useEffectOnce

--- src/hooks/useEffectOnce.ts
+++ src/hooks/useEffectOnce.ts
@@ -1,6 +1,12 @@
 import type {EffectCallback} from 'react';
 import {useEffect, useRef} from 'react';
 
+/**
+ * This hook is used to run an effect only once, usually on the first render (on mount).
+ *
+ * @param callback The side effect to run only once.
+ * @param condition Run the callback if this condition is true. Defaults to true.
+ */
 function useEffectOnce(callback: EffectCallback, condition = true) {
     const hasRunCallbackBefore = useRef(false);
 
