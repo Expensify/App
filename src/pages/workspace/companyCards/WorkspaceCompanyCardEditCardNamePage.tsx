b14b70429807c48313a6b29cb8ea54de071562f1
VickyStash <vikstash@gmail.com>
2024-11-05 14:42:25 +0100

    Minor improvement

--- src/pages/workspace/companyCards/WorkspaceCompanyCardEditCardNamePage.tsx
+++ src/pages/workspace/companyCards/WorkspaceCompanyCardEditCardNamePage.tsx
@@ -64,9 +64,7 @@ function WorkspaceCompanyCardEditCardNamePage({route}: WorkspaceCompanyCardEditC
                     title={translate('workspace.moreFeatures.companyCards.cardName')}
                     onBackButtonPress={() => Navigation.goBack(ROUTES.WORKSPACE_COMPANY_CARD_DETAILS.getRoute(policyID, cardID, bank))}
                 />
-                <Text style={[styles.mt3, styles.mh5, styles.mb5]}>
-                    <Text style={styles.textNormal}>{translate('workspace.moreFeatures.companyCards.giveItNameInstruction')}</Text>
-                </Text>
+                <Text style={[styles.mh5, styles.mt3, styles.mb5]}>{translate('workspace.moreFeatures.companyCards.giveItNameInstruction')}</Text>
                 <FormProvider
                     formID={ONYXKEYS.FORMS.EDIT_WORKSPACE_COMPANY_CARD_NAME_FORM}
                     submitButtonText={translate('common.save')}
--- src/pages/workspace/companyCards/assignCard/CardNameStep.tsx
+++ src/pages/workspace/companyCards/assignCard/CardNameStep.tsx
@@ -66,9 +66,7 @@ function CardNameStep({policyID}: CardNameStepProps) {
                     title={translate('workspace.moreFeatures.companyCards.cardName')}
                     onBackButtonPress={() => CompanyCards.setAssignCardStepAndData({currentStep: CONST.COMPANY_CARD.STEP.CONFIRMATION, isEditing: false})}
                 />
-                <Text style={[styles.mt3, styles.mh5, styles.mb5]}>
-                    <Text style={styles.textNormal}>{translate('workspace.moreFeatures.companyCards.giveItNameInstruction')}</Text>
-                </Text>
+                <Text style={[styles.mh5, styles.mt3, styles.mb5]}>{translate('workspace.moreFeatures.companyCards.giveItNameInstruction')}</Text>
                 <FormProvider
                     formID={ONYXKEYS.FORMS.EDIT_WORKSPACE_COMPANY_CARD_NAME_FORM}
                     submitButtonText={translate('common.confirm')}
