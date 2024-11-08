bb2af17b839b7b63ee819e9c9b2b490ea457d6e6
nkdengineer <nkdengineer@outlook.com>
2024-11-08 10:38:10 +0700

    change translation

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -3930,7 +3930,7 @@ const translations = {
             personalMessagePrompt: 'Mensaje',
             inviteNoMembersError: 'Por favor, selecciona al menos un miembro a invitar.',
             genericFailureMessage: 'Se ha producido un error al invitar al miembro al espacio de trabajo. Por favor, vuelva a intentarlo.',
-            joinRequest: ({user}: {user: string}) => `${user} solicita unirse al espacio de trabajo`,
+            joinRequest: ({user}: {user: string}) => `${user} solicitó unirse al espacio de trabajo`,
         },
         distanceRates: {
             oopsNotSoFast: 'Ups! No tan rápido...',
