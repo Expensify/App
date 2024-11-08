d42e264564a476786262ac6e29957d50912d5821
truph01 <truphd01@gmail.com>
2024-11-08 16:32:21 +0700

    fix: add 2nd test case

--- tests/unit/WorkspaceSettingsUtilsTest.ts
+++ tests/unit/WorkspaceSettingsUtilsTest.ts
@@ -43,5 +43,28 @@ describe('WorkspacesSettingsUtils', () => {
             // Then the result should be 'error'
             expect(result).toBe('error');
         });
+
+        it('Should return "undefined"', async () => {
+            // Given mock data for reports, transaction violations, sessions, and report actions
+            const report = Object.values(mockData.reports)?.at(0);
+            const transactionViolations = mockData.transactionViolations;
+            const reports = mockData.reports;
+            const session = mockData.session;
+            const reportActions = mockData.reportActions;
+
+            await Onyx.multiSet({
+                ...(reports as ReportCollectionDataSet),
+                ...(reportActions as OnyxCollection<ReportActions>),
+                session,
+            });
+
+            await waitForBatchedUpdates();
+
+            // When calling getBrickRoadForPolicy with a report and report actions
+            const result = getBrickRoadForPolicy(report as Report, reportActions as OnyxCollection<ReportActions>);
+
+            // Then the result should be 'error'
+            expect(result).toBe(undefined);
+        });
     });
 });
