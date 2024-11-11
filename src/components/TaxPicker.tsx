5d3b94063d52bf7b01531ffdd7618dedd38809e8
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-11-11 14:21:35 +0100

    refactor to use useOnyx

--- src/components/TaxPicker.tsx
+++ src/components/TaxPicker.tsx
@@ -1,6 +1,5 @@
 import React, {useCallback, useMemo, useState} from 'react';
-import {withOnyx} from 'react-native-onyx';
-import type {OnyxEntry} from 'react-native-onyx';
+import {useOnyx} from 'react-native-onyx';
 import type {EdgeInsets} from 'react-native-safe-area-context';
 import type {ValueOf} from 'type-fest';
 import useLocalize from '@hooks/useLocalize';
@@ -12,32 +11,18 @@ import * as TransactionUtils from '@libs/TransactionUtils';
 import CONST from '@src/CONST';
 import type {IOUAction} from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {Policy, Transaction} from '@src/types/onyx';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import SelectionList from './SelectionList';
 import RadioListItem from './SelectionList/RadioListItem';
 
-type TaxPickerOnyxProps = {
-    /** The policy which the user has access to and which the report is tied to */
-    policy: OnyxEntry<Policy>;
-
-    /** All the data for the transaction */
-    transaction: OnyxEntry<Transaction>;
-
-    /** The draft transaction that holds data to be persisted on the current split transaction */
-    splitDraftTransaction: OnyxEntry<Transaction>;
-};
-
-type TaxPickerProps = TaxPickerOnyxProps & {
+type TaxPickerProps = {
     /** The selected tax rate of an expense */
     selectedTaxRate?: string;
 
     /** ID of the policy */
-    // eslint-disable-next-line react/no-unused-prop-types
     policyID?: string;
 
     /** ID of the transaction */
-    // eslint-disable-next-line react/no-unused-prop-types
     transactionID?: string;
 
     /**
@@ -50,7 +35,6 @@ type TaxPickerProps = TaxPickerOnyxProps & {
     onSubmit: (tax: TaxOptionsListUtils.TaxRatesOption) => void;
 
     /** The action to take */
-    // eslint-disable-next-line react/no-unused-prop-types
     action?: IOUAction;
 
     /** The type of IOU */
@@ -59,10 +43,21 @@ type TaxPickerProps = TaxPickerOnyxProps & {
     onDismiss: () => void;
 };
 
-function TaxPicker({selectedTaxRate = '', policy, transaction, insets, onSubmit, action, splitDraftTransaction, iouType, onDismiss}: TaxPickerProps) {
+function TaxPicker({selectedTaxRate = '', policyID, transactionID, insets, onSubmit, action, iouType, onDismiss}: TaxPickerProps) {
     const StyleUtils = useStyleUtils();
     const {translate} = useLocalize();
     const [searchValue, setSearchValue] = useState('');
+
+    const [policy] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${policyID}`);
+    const [transaction] = useOnyx(
+        (() => {
+            if (IOUUtils.shouldUseTransactionDraft(action)) {
+                return `${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${transactionID}` as `${typeof ONYXKEYS.COLLECTION.TRANSACTION}${string}`;
+            }
+            return `${ONYXKEYS.COLLECTION.TRANSACTION}${transactionID}`;
+        })(),
+    );
+    const [splitDraftTransaction] = useOnyx(`${ONYXKEYS.COLLECTION.SPLIT_TRANSACTION_DRAFT}${transactionID}`);
 
     const isEditing = action === CONST.IOU.ACTION.EDIT;
     const isEditingSplitBill = isEditing && iouType === CONST.IOU.TYPE.SPLIT;
@@ -131,20 +126,4 @@ function TaxPicker({selectedTaxRate = '', policy, transaction, insets, onSubmit,
 }
 
 TaxPicker.displayName = 'TaxPicker';
-
-export default withOnyx<TaxPickerProps, TaxPickerOnyxProps>({
-    policy: {
-        key: ({policyID}) => `${ONYXKEYS.COLLECTION.POLICY}${policyID}`,
-    },
-    transaction: {
-        key: ({transactionID, action}) => {
-            if (IOUUtils.shouldUseTransactionDraft(action)) {
-                return `${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${transactionID}` as `${typeof ONYXKEYS.COLLECTION.TRANSACTION}${string}`;
-            }
-            return `${ONYXKEYS.COLLECTION.TRANSACTION}${transactionID}`;
-        },
-    },
-    splitDraftTransaction: {
-        key: ({transactionID}) => `${ONYXKEYS.COLLECTION.SPLIT_TRANSACTION_DRAFT}${transactionID}`,
-    },
-})(TaxPicker);
+export default TaxPicker;
