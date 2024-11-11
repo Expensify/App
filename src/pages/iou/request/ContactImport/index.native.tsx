0475d046e0852774acb6fb45e4fc4868ffd9b6dd
Taras Perun <terry.perun@gmail.com>
2024-11-11 19:14:32 +0100

    clean contactImport

--- src/pages/iou/request/ContactImport/index.native.tsx
+++ src/pages/iou/request/ContactImport/index.native.tsx
@@ -3,16 +3,19 @@ import {requestContactPermission} from '@pages/iou/request/ContactPermission';
 import type {ContactImportResult, DeviceContact} from './types';
 
 function contactImport(): Promise<ContactImportResult> {
+    let isPermissionGranted = false;
     return requestContactPermission()
         .then((response) => {
             if (response === 'granted') {
+                isPermissionGranted = true;
                 return ContactsNitroModule.getAll(['FIRST_NAME', 'LAST_NAME', 'PHONE_NUMBERS', 'EMAIL_ADDRESSES', 'IMAGE_DATA']);
             }
+            isPermissionGranted = false;
             return [] as DeviceContact[];
         })
         .then((deviceContacts) => ({
             contactList: Array.isArray(deviceContacts) ? deviceContacts : [],
-            isPermissionBlocked: false,
+            isPermissionBlocked: !isPermissionGranted,
         }));
 }
 
