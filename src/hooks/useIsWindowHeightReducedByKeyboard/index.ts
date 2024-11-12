9c4e6625d04582d3404c806b328064ce02aa0976
truph01 <truphd01@gmail.com>
2024-11-12 22:27:07 +0700

    add comment to useEffect

--- src/hooks/useIsWindowHeightReducedByKeyboard/index.ts
+++ src/hooks/useIsWindowHeightReducedByKeyboard/index.ts
@@ -18,6 +18,12 @@ const useIsWindowHeightReducedByKeyboard = () => {
         [shouldUseNarrowLayout],
     );
     useEffect(() => {
+        // Use window height changes to toggle the keyboard. To maintain keyboard state
+        // on all platforms we also use focus/blur events. So we need to make sure here
+        // that we avoid redundant keyboard toggling.
+        // Minus 100px is needed to make sure that when the internet connection is
+        // disabled in android chrome and a small 'No internet connection' text box appears,
+        // we do not take it as a sign to open the keyboard
         if (!isWindowHeightReducedByKeyboard && windowHeight < prevWindowHeight - 100) {
             toggleKeyboardOnSmallScreens(true);
         } else if (isWindowHeightReducedByKeyboard && windowHeight > prevWindowHeight) {
