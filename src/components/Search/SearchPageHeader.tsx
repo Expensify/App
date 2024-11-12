997f26e80a295d44428c4243da6006753576a22e
krishna2323 <belivethatkg@gmail.com>
2024-11-12 16:41:08 +0530

    fix: Search - Workspace switcher resets to Expensify after manually searching via search input.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/components/Search/SearchPageHeader.tsx
+++ src/components/Search/SearchPageHeader.tsx
@@ -344,6 +344,9 @@ function SearchPageHeader({queryJSON, hash}: SearchPageHeaderProps) {
             // After https://github.com/Expensify/App/pull/51633 is merged, autocomplete functionality will be included into this component, and `getFindIDFromDisplayValue` can be removed
             const computeNodeValueFn = SearchQueryUtils.getFindIDFromDisplayValue(cardList, taxRates);
             const standardizedQuery = SearchQueryUtils.traverseAndUpdatedQuery(inputQueryJSON, computeNodeValueFn);
+            if (queryJSON.policyID) {
+                standardizedQuery.policyID = queryJSON.policyID;
+            }
             const query = SearchQueryUtils.buildSearchQueryString(standardizedQuery);
             SearchActions.clearAllFilters();
             Navigation.navigate(ROUTES.SEARCH_CENTRAL_PANE.getRoute({query}));
