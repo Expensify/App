390641938a6eb1f025805f0831c2c73d9446f256
burczu <b.dybowski@gmail.com>
2024-11-13 11:39:33 +0100

    yes or no step component extracted

--- src/pages/ReimbursementAccount/NonUSD/SignerInfo/DirectorCheck.tsx
+++ src/pages/ReimbursementAccount/NonUSD/SignerInfo/DirectorCheck.tsx
@@ -1,11 +1,6 @@
-import React, {useMemo, useState} from 'react';
-import FormProvider from '@components/Form/FormProvider';
-import type {Choice} from '@components/RadioButtons';
-import RadioButtons from '@components/RadioButtons';
-import Text from '@components/Text';
+import React from 'react';
+import YesNoStep from '@components/SubStepForms/YesNoStep';
 import useLocalize from '@hooks/useLocalize';
-import useThemeStyles from '@hooks/useThemeStyles';
-import ONYXKEYS from '@src/ONYXKEYS';
 
 type DirectorCheckProps = {
     /** The title of the question */
@@ -20,43 +15,14 @@ type DirectorCheckProps = {
 
 function DirectorCheck({title, onSelectedValue, defaultValue}: DirectorCheckProps) {
     const {translate} = useLocalize();
-    const styles = useThemeStyles();
-    const [value, setValue] = useState(defaultValue);
-
-    const handleSubmit = () => {
-        onSelectedValue(value);
-    };
-    const handleSelectValue = (newValue: string) => setValue(newValue === 'true');
-    const options = useMemo<Choice[]>(
-        () => [
-            {
-                label: translate('common.yes'),
-                value: 'true',
-            },
-            {
-                label: translate('common.no'),
-                value: 'false',
-            },
-        ],
-        [translate],
-    );
 
     return (
-        <FormProvider
-            formID={ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM}
-            submitButtonText={translate('common.confirm')}
-            onSubmit={handleSubmit}
-            style={[styles.mh5, styles.flexGrow1]}
-        >
-            <Text style={[styles.textHeadlineLineHeightXXL]}>{title}</Text>
-            <Text style={[styles.pv3, styles.textSupporting]}>{translate('signerInfoStep.regulationRequiresUs')}</Text>
-            <RadioButtons
-                items={options}
-                onPress={handleSelectValue}
-                defaultCheckedValue={defaultValue.toString()}
-                radioButtonStyle={[styles.mb6]}
-            />
-        </FormProvider>
+        <YesNoStep
+            title={title}
+            description={translate('signerInfoStep.regulationRequiresUs')}
+            defaultValue={defaultValue}
+            onSelectedValue={onSelectedValue}
+        />
     );
 }
 
