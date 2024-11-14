022cbdf7294bc5a337c27faf3eb19a72682335ba
mkzie2 <mkzie2@gmail.com>
2024-11-14 23:55:57 +0700

    use FullPageNotFoundView

--- src/components/ValidateCode/ValidateCodeModal.tsx
+++ src/components/ValidateCode/ValidateCodeModal.tsx
@@ -1,6 +1,7 @@
 import React, {useCallback} from 'react';
 import {View} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
+import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import Icon from '@components/Icon';
 import * as Expensicons from '@components/Icon/Expensicons';
 import * as Illustrations from '@components/Icon/Illustrations';
@@ -11,7 +12,6 @@ import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
 import * as ValidationUtils from '@libs/ValidationUtils';
-import NotFoundPage from '@pages/ErrorPage/NotFoundPage';
 import variables from '@styles/variables';
 import * as Session from '@userActions/Session';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -30,53 +30,48 @@ function ValidateCodeModal({code, accountID}: ValidateCodeModalProps) {
     const signInHere = useCallback(() => Session.signInWithValidateCode(accountID, code), [accountID, code]);
     const {translate} = useLocalize();
 
-    const isInvalidValidateCode = !ValidationUtils.isValidValidateCode(code);
-
-    if (isInvalidValidateCode) {
-        return (
-            <NotFoundPage
-                shouldShow
-                onLinkPress={() => {
-                    Navigation.goBack();
-                }}
-            />
-        );
-    }
     return (
-        <View style={styles.deeplinkWrapperContainer}>
-            <View style={styles.deeplinkWrapperMessage}>
-                <View style={styles.mb2}>
+        <FullPageNotFoundView
+            shouldShow={!ValidationUtils.isValidValidateCode(code)}
+            onLinkPress={() => {
+                Navigation.goBack();
+            }}
+        >
+            <View style={styles.deeplinkWrapperContainer}>
+                <View style={styles.deeplinkWrapperMessage}>
+                    <View style={styles.mb2}>
+                        <Icon
+                            width={variables.modalTopIconWidth}
+                            height={variables.modalTopIconHeight}
+                            src={Illustrations.MagicCode}
+                        />
+                    </View>
+                    <Text style={[styles.textHeadline, styles.textXXLarge, styles.textAlignCenter]}>{translate('validateCodeModal.title')}</Text>
+                    <View style={[styles.mt2, styles.mb2]}>
+                        <Text style={styles.textAlignCenter}>
+                            {translate('validateCodeModal.description')}
+                            {!session?.authToken && (
+                                <>
+                                    {translate('validateCodeModal.or')} <TextLink onPress={signInHere}>{translate('validateCodeModal.signInHere')}</TextLink>
+                                </>
+                            )}
+                            .
+                        </Text>
+                    </View>
+                    <View style={styles.mt6}>
+                        <Text style={styles.validateCodeDigits}>{code}</Text>
+                    </View>
+                </View>
+                <View style={styles.deeplinkWrapperFooter}>
                     <Icon
-                        width={variables.modalTopIconWidth}
-                        height={variables.modalTopIconHeight}
-                        src={Illustrations.MagicCode}
+                        width={variables.modalWordmarkWidth}
+                        height={variables.modalWordmarkHeight}
+                        fill={theme.success}
+                        src={Expensicons.ExpensifyWordmark}
                     />
                 </View>
-                <Text style={[styles.textHeadline, styles.textXXLarge, styles.textAlignCenter]}>{translate('validateCodeModal.title')}</Text>
-                <View style={[styles.mt2, styles.mb2]}>
-                    <Text style={styles.textAlignCenter}>
-                        {translate('validateCodeModal.description')}
-                        {!session?.authToken && (
-                            <>
-                                {translate('validateCodeModal.or')} <TextLink onPress={signInHere}>{translate('validateCodeModal.signInHere')}</TextLink>
-                            </>
-                        )}
-                        .
-                    </Text>
-                </View>
-                <View style={styles.mt6}>
-                    <Text style={styles.validateCodeDigits}>{code}</Text>
-                </View>
             </View>
-            <View style={styles.deeplinkWrapperFooter}>
-                <Icon
-                    width={variables.modalWordmarkWidth}
-                    height={variables.modalWordmarkHeight}
-                    fill={theme.success}
-                    src={Expensicons.ExpensifyWordmark}
-                />
-            </View>
-        </View>
+        </FullPageNotFoundView>
     );
 }
 
