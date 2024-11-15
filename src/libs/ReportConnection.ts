8dc09af7d651cd66f2415a731297a41fd2919ae9
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-11-15 14:11:28 +0100

    fix import cycles starting with (and containing) ReportConnection.ts -> Report.ts -> ReportConnection.ts

--- src/libs/ReportConnection.ts
+++ src/libs/ReportConnection.ts
@@ -3,10 +3,10 @@ import Onyx from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Report} from '@src/types/onyx';
 import * as PriorityModeActions from './actions/PriorityMode';
-import * as ReportHelperActions from './actions/Report';
 
-// Dynamic Import to avoid circular dependency
+// Dynamic Imports to avoid circular dependencies
 const UnreadIndicatorUpdaterHelper = () => import('./UnreadIndicatorUpdater');
+const ReportHelperActions = () => import('./actions/Report');
 
 const reportIDToNameMap: Record<string, string> = {};
 let allReports: OnyxCollection<Report>;
@@ -29,7 +29,9 @@ Onyx.connect({
                 return;
             }
             reportIDToNameMap[report.reportID] = report.reportName ?? report.displayName ?? report.reportID;
-            ReportHelperActions.handleReportChanged(report);
+            ReportHelperActions().then((module) => {
+                module.handleReportChanged(report);
+            });
         });
     },
 });
