b9571c4f41839727a8af46f48722deae0d61ce76
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-11-16 22:37:41 +0800

    add comment

--- src/components/Search/SearchRouter/SearchRouterModal.tsx
+++ src/components/Search/SearchRouter/SearchRouterModal.tsx
@@ -12,6 +12,7 @@ const isMobileSafari = Browser.isMobileSafari();
 function SearchRouterModal() {
     const {shouldUseNarrowLayout} = useResponsiveLayout();
     const {isSearchRouterDisplayed, closeSearchRouter} = useSearchRouterContext();
+    // On mWeb Safari, the input caret stuck for a moment while the modal is animating. So, we hide the caret until the animation is done.
     const [shouldHideInputCaret, setShouldHideInputCaret] = useState(isMobileSafari);
 
     const modalType = shouldUseNarrowLayout ? CONST.MODAL.MODAL_TYPE.CENTERED_SWIPABLE_TO_RIGHT : CONST.MODAL.MODAL_TYPE.POPOVER;
