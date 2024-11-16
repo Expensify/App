f5ed2423e103ab8cb9be5ff7eda218dcf9805043
NikkiWines <nikkiwines@expensify.com>
2024-11-15 21:28:07 -0500

    use fetch instead of simply window.location

--- src/pages/signin/SAMLSignInPage/index.tsx
+++ src/pages/signin/SAMLSignInPage/index.tsx
@@ -3,11 +3,26 @@ import {withOnyx} from 'react-native-onyx';
 import SAMLLoadingIndicator from '@components/SAMLLoadingIndicator';
 import CONFIG from '@src/CONFIG';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {SAMLSignInPageOnyxProps, SAMLSignInPageProps} from './types';
+import {getApiRoot} from '@libs/ApiUtils';
+import CONST from '@src/CONST';
 
 function SAMLSignInPage({credentials}: SAMLSignInPageProps) {
     useEffect(() => {
-        window.location.replace(`${CONFIG.EXPENSIFY.SAML_URL}?email=${credentials?.login}&referer=${CONFIG.EXPENSIFY.EXPENSIFY_CASH_REFERER}`);
+        const body = new FormData();
+        body.append('email', credentials?.login ?? '');
+        body.append('referer', CONFIG.EXPENSIFY.EXPENSIFY_CASH_REFERER)
+
+        const url = `${getApiRoot()}authentication/saml/login`;
+        fetch(url, {
+            method: CONST.NETWORK.METHOD.POST,
+            body,
+            credentials: 'omit',
+        }).then((response) => response.json() as Promise<Response>
+        ).then((response) => {
+            if (response.url) {
+                window.location.replace(response.url);
+            }
+        });
     }, [credentials?.login]);
 
     return <SAMLLoadingIndicator />;
