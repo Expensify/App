8f3b3ada1b5faf76a8ecd66f2968cdb6b206b04b
Shubham Agrawal <shubham.agra1206@gmail.com>
2024-11-16 22:46:40 +0530

    Fix confirm page

--- src/pages/settings/Wallet/InternationalDepositAccount/substeps/Confirmation.tsx
+++ src/pages/settings/Wallet/InternationalDepositAccount/substeps/Confirmation.tsx
@@ -1,16 +1,35 @@
-import React, {useState} from 'react';
-import ConfirmationStep from '@components/SubStepForms/ConfirmationStep';
+import React, {useCallback, useState} from 'react';
+import CheckboxWithLabel from '@components/CheckboxWithLabel';
+import FormProvider from '@components/Form/FormProvider';
+import InputWrapper from '@components/Form/InputWrapper';
+import type {FormInputErrors, FormOnyxValues} from '@components/Form/types';
+import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
+import ScrollView from '@components/ScrollView';
+import Text from '@components/Text';
+import TextLink from '@components/TextLink';
 import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
 import type {CustomSubStepProps} from '@pages/settings/Wallet/InternationalDepositAccount/types';
 import * as BankAccounts from '@userActions/BankAccounts';
 import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
 
 const STEP_INDEXES = CONST.CORPAY_FIELDS.INDEXES.MAPPING;
 
-function Confirmation({onNext, onMove, isEditing, formValues, fieldsMap}: CustomSubStepProps) {
+function TermsAndConditionsLabel() {
     const {translate} = useLocalize();
+    return (
+        <Text>
+            {translate('common.iAcceptThe')}
+            <TextLink href={CONST.TERMS_URL}>{`${translate('common.addCardTermsOfService')}`}</TextLink>
+        </Text>
+    );
+}
+
+function Confirmation({onNext, onMove, formValues, fieldsMap}: CustomSubStepProps) {
+    const {translate} = useLocalize();
+    const styles = useThemeStyles();
     const [isSubmitting, setIsSubmitting] = useState(false);
-    const [hasError, setHasError] = useState(false);
 
     const getDataAndGoToNextStep = () => {
         setIsSubmitting(true);
@@ -19,8 +38,6 @@ function Confirmation({onNext, onMove, isEditing, formValues, fieldsMap}: Custom
             if (response?.jsonCode) {
                 if (response.jsonCode === CONST.JSON_CODE.SUCCESS) {
                     onNext();
-                } else {
-                    setHasError(true);
                 }
             }
         });
@@ -101,17 +118,48 @@ function Confirmation({onNext, onMove, isEditing, formValues, fieldsMap}: Custom
         });
     }
 
+    const validate = useCallback(
+        (values: FormOnyxValues<typeof ONYXKEYS.FORMS.INTERNATIONAL_BANK_ACCOUNT_FORM>): FormInputErrors<typeof ONYXKEYS.FORMS.INTERNATIONAL_BANK_ACCOUNT_FORM> => {
+            const errors: FormInputErrors<typeof ONYXKEYS.FORMS.INTERNATIONAL_BANK_ACCOUNT_FORM> = {};
+            if (!values.acceptTerms) {
+                errors.acceptTerms = translate('common.error.acceptTerms');
+            }
+            return errors;
+        },
+        [translate],
+    );
+
     return (
-        <ConfirmationStep
-            isEditing={isEditing}
-            onNext={getDataAndGoToNextStep}
-            onMove={onMove}
-            pageTitle={translate('personalInfoStep.letsDoubleCheck')}
-            summaryItems={summaryItems}
-            showOnfidoLinks={false}
-            isLoading={isSubmitting}
-            error={hasError ? translate('common.genericErrorMessage') : ''}
-        />
+        <ScrollView contentContainerStyle={styles.flexGrow1}>
+            <Text style={[styles.textHeadlineLineHeightXXL, styles.ph5, styles.mb3]}>{translate('personalInfoStep.letsDoubleCheck')}</Text>
+            {summaryItems.map(({description, title, shouldShowRightIcon, onPress}) => (
+                <MenuItemWithTopDescription
+                    key={`${title}_${description}`}
+                    description={description}
+                    title={title}
+                    shouldShowRightIcon={shouldShowRightIcon}
+                    onPress={onPress}
+                />
+            ))}
+            <FormProvider
+                formID={ONYXKEYS.FORMS.INTERNATIONAL_BANK_ACCOUNT_FORM}
+                validate={validate}
+                onSubmit={getDataAndGoToNextStep}
+                scrollContextEnabled
+                submitButtonText={translate('common.confirm')}
+                style={[styles.mh5, styles.flexGrow1]}
+                enabledWhenOffline={false}
+                isSubmitDisabled={isSubmitting}
+            >
+                <InputWrapper
+                    InputComponent={CheckboxWithLabel}
+                    aria-label={`${translate('common.iAcceptThe')} ${translate('common.addCardTermsOfService')}`}
+                    inputID="acceptTerms"
+                    LabelComponent={TermsAndConditionsLabel}
+                    style={[styles.mt3]}
+                />
+            </FormProvider>
+        </ScrollView>
     );
 }
 
