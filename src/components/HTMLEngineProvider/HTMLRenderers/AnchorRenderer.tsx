a1ef32e8e580dfa81b41540de98f7473ff78c9a6
mkzie2 <mkzie2@gmail.com>
2024-11-18 01:35:42 +0700

    fix text link style is not applied in code block

--- src/components/HTMLEngineProvider/HTMLRenderers/AnchorRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/AnchorRenderer.tsx
@@ -80,7 +80,7 @@ function AnchorRenderer({tnode, style, key}: AnchorRendererProps) {
                     if (props.childTnode.tagName === 'br') {
                         return <Text key={props.key}>{'\n'}</Text>;
                     }
-                    if (props.childTnode.type === 'text') {
+                    if (props.childTnode.type === 'text' && props.childTnode.tagName !== 'code') {
                         return (
                             <Text
                                 key={props.key}
