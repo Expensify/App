6da0b81e9bea7929914aaaeb5ca72ab20aeca513
truph01 <truphd01@gmail.com>
2024-11-19 01:09:02 +0700

    fix: update comments

--- tests/unit/OnboardingSelectorsTest.ts
+++ tests/unit/OnboardingSelectorsTest.ts
@@ -4,28 +4,31 @@ import CONST from '@src/CONST';
 import type ONYXKEYS from '@src/ONYXKEYS';
 
 describe('onboardingSelectors', () => {
+    // Not all users have this NVP defined as we did not run a migration to backfill it for existing accounts, hence we need to make sure
+    // the onboarding flow is only showed to the users with `hasCompletedGuidedSetupFlow` set to false
     describe('hasCompletedGuidedSetupFlowSelector', () => {
-        it('Should return true if onboarding nvp is array ', () => {
+        // It might be the case that backend returns an empty array if the NVP is not defined on this particular account
+        it('Should return true if onboarding NVP is an array', () => {
             const onboarding = [] as OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>;
             expect(hasCompletedGuidedSetupFlowSelector(onboarding)).toBe(true);
         });
-        it('Should return true if onboarding nvp is {}', () => {
+        it('Should return true if onboarding NVP is an empty object', () => {
             const onboarding = {} as OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>;
             expect(hasCompletedGuidedSetupFlowSelector(onboarding)).toBe(true);
         });
-        it('Should return true if onboarding nvp contains only signupQualifier', () => {
+        it('Should return true if onboarding NVP contains only signupQualifier', () => {
             const onboarding = {signupQualifier: CONST.ONBOARDING_SIGNUP_QUALIFIERS.VSB} as OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>;
             expect(hasCompletedGuidedSetupFlowSelector(onboarding)).toBe(true);
         });
-        it('Should return true if onboarding nvp contains hasCompletedGuidedSetupFlow = true', () => {
+        it('Should return true if onboarding NVP contains hasCompletedGuidedSetupFlow = true', () => {
             const onboarding = {hasCompletedGuidedSetupFlow: true} as OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>;
             expect(hasCompletedGuidedSetupFlowSelector(onboarding)).toBe(true);
         });
-        it('Should return false if onboarding nvp contains hasCompletedGuidedSetupFlow = false', () => {
+        it('Should return false if onboarding NVP contains hasCompletedGuidedSetupFlow = false', () => {
             const onboarding = {hasCompletedGuidedSetupFlow: false} as OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>;
             expect(hasCompletedGuidedSetupFlowSelector(onboarding)).toBe(false);
         });
-        it('Should return true if onboarding nvp contains only selfTourViewed', () => {
+        it('Should return true if onboarding NVP contains only selfTourViewed', () => {
             const onboarding = {selfTourViewed: true} as OnyxValue<typeof ONYXKEYS.NVP_ONBOARDING>;
             expect(hasCompletedGuidedSetupFlowSelector(onboarding)).toBe(true);
         });
