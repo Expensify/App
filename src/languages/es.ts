9672a303f4182e1c8add9e316f7cd8fdf3c8fbb2
James Dean <james@expensify.com>
2024-11-19 14:52:04 -0800

    Update src/languages/es.ts
    Co-authored-by: Ionatan Wiznia <ionatan@expensify.com>

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -1412,7 +1412,6 @@ const translations = {
         enableWalletToSendAndReceiveMoney: 'Habilita tu Billetera Expensify para comenzar a enviar y recibir dinero con amigos.',
         walletEnabledToSendAndReceiveMoney: 'Tu billetera ha sido habilitada para enviar y recibir dinero con amigos.',
         enableWallet: 'Habilitar billetera',
-        addBankAccountToSendAndReceive: 'Añadir una cuenta bancaria te permite recibir reembolsos por los gastos que envíes a un espacio de trabajo.',
         addBankAccountToSendAndReceive: 'Añade una cuenta bancaria para recibir reembolsos por los gastos que envíes a un espacio de trabajo.',
         addBankAccount: 'Añadir cuenta bancaria',
         assignedCards: 'Tarjetas asignadas',
