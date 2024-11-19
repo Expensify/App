b6f73fe2eb2cc9a8e18822826de2b95b3c55c3f3
Ugo Giordano <144606237+ugogiordano@users.noreply.github.com>
2024-11-20 00:52:20 +0100

    Update ConciergePage.tsx
    Migrating from `withOnyx`  to `useOnyx`.

--- src/pages/ConciergePage.tsx
+++ src/pages/ConciergePage.tsx
@@ -1,38 +1,27 @@
 import {useFocusEffect} from '@react-navigation/native';
-import type {StackScreenProps} from '@react-navigation/stack';
 import React, {useCallback, useEffect, useRef} from 'react';
 import {View} from 'react-native';
-import type {OnyxEntry} from 'react-native-onyx';
-import {useOnyx, withOnyx} from 'react-native-onyx';
+import {useOnyx} from 'react-native-onyx';
 import ReportActionsSkeletonView from '@components/ReportActionsSkeletonView';
 import ReportHeaderSkeletonView from '@components/ReportHeaderSkeletonView';
 import ScreenWrapper from '@components/ScreenWrapper';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
-import type {AuthScreensParamList} from '@libs/Navigation/types';
 import * as App from '@userActions/App';
 import * as Report from '@userActions/Report';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type SCREENS from '@src/SCREENS';
-import type {Session} from '@src/types/onyx';
-
-type ConciergePageOnyxProps = {
-    /** Session info for the currently logged in user. */
-    session: OnyxEntry<Session>;
-};
-
-type ConciergePageProps = ConciergePageOnyxProps & StackScreenProps<AuthScreensParamList, typeof SCREENS.CONCIERGE>;
 
 /*
  * This is a "utility page", that does this:
  *     - If the user is authenticated, find their concierge chat and re-route to it
  *     - Else re-route to the login page
  */
-function ConciergePage({session}: ConciergePageProps) {
+function ConciergePage() {
     const styles = useThemeStyles();
     const isUnmounted = useRef(false);
     const {shouldUseNarrowLayout} = useResponsiveLayout();
+    const [session] = useOnyx(ONYXKEYS.SESSION);
     const [isLoadingReportData] = useOnyx(ONYXKEYS.IS_LOADING_REPORT_DATA, {initialValue: true});
 
     useFocusEffect(
@@ -70,8 +59,4 @@ function ConciergePage({session}: ConciergePageProps) {
 
 ConciergePage.displayName = 'ConciergePage';
 
-export default withOnyx<ConciergePageProps, ConciergePageOnyxProps>({
-    session: {
-        key: ONYXKEYS.SESSION,
-    },
-})(ConciergePage);
+export default ConciergePage;
