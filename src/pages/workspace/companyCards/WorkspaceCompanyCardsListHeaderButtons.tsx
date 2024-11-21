d71e7f1cd473598d5470fda5d9b1d12e335ae15e
Agata Kosior <agata.kosior57@gmail.com>
2024-11-21 17:22:47 +0700

    fix: minor fix

--- src/pages/workspace/companyCards/WorkspaceCompanyCardsListHeaderButtons.tsx
+++ src/pages/workspace/companyCards/WorkspaceCompanyCardsListHeaderButtons.tsx
@@ -59,12 +59,12 @@ function WorkspaceCompanyCardsListHeaderButtons({policyID, selectedFeed}: Worksp
         if (Object.keys(policy?.employeeList ?? {}).length === 1) {
             data.email = Object.keys(policy?.employeeList ?? {}).at(0);
             currentStep = CONST.COMPANY_CARD.STEP.CARD;
-        }
 
-        if (CardUtils.hasOnlyOneCardToAssign(filteredCardList)) {
-            currentStep = CONST.COMPANY_CARD.STEP.TRANSACTION_START_DATE;
-            data.cardNumber = Object.keys(filteredCardList).at(0);
-            data.encryptedCardNumber = Object.values(filteredCardList).at(0);
+            if (CardUtils.hasOnlyOneCardToAssign(filteredCardList)) {
+                currentStep = CONST.COMPANY_CARD.STEP.TRANSACTION_START_DATE;
+                data.cardNumber = Object.keys(filteredCardList).at(0);
+                data.encryptedCardNumber = Object.values(filteredCardList).at(0);
+            }
         }
 
         CompanyCards.setAssignCardStepAndData({data, currentStep});
