e99ede55a720b8bbb749912a01ade6f8ebe0cc77
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-11-21 13:57:02 +0800

    clears the draft when opening the invite page

--- src/pages/workspace/WorkspaceMembersPage.tsx
+++ src/pages/workspace/WorkspaceMembersPage.tsx
@@ -195,10 +195,16 @@ function WorkspaceMembersPage({personalDetails, route, policy, currentUserPerson
         getWorkspaceMembers();
     }, [isOffline, prevIsOffline, getWorkspaceMembers]);
 
+    const clearInviteDraft = useCallback(() => {
+        Member.setWorkspaceInviteMembersDraft(route.params.policyID, {});
+        FormActions.clearDraftValues(ONYXKEYS.FORMS.WORKSPACE_INVITE_MESSAGE_FORM);
+    }, [route.params.policyID]);
+
     /**
      * Open the modal to invite a user
      */
     const inviteUser = () => {
+        clearInviteDraft();
         Navigation.navigate(ROUTES.WORKSPACE_INVITE.getRoute(route.params.policyID));
     };
 
@@ -417,9 +423,8 @@ function WorkspaceMembersPage({personalDetails, route, policy, currentUserPerson
         }
         const invitedEmails = Object.values(invitedEmailsToAccountIDsDraft).map(String);
         selectionListRef.current?.scrollAndHighlightItem?.(invitedEmails);
-        Member.setWorkspaceInviteMembersDraft(route.params.policyID, {});
-        FormActions.clearDraftValues(ONYXKEYS.FORMS.WORKSPACE_INVITE_MESSAGE_FORM);
-    }, [invitedEmailsToAccountIDsDraft, route.params.policyID, isFocused, accountIDs, prevAccountIDs]);
+        clearInviteDraft();
+    }, [invitedEmailsToAccountIDsDraft, isFocused, accountIDs, prevAccountIDs, clearInviteDraft]);
 
     const getHeaderMessage = () => {
         if (isOfflineAndNoMemberDataAvailable) {
