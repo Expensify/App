7fa7e1f00f5602d1ebed96a651ecc02043744b38
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2024-11-21 15:20:16 +0100

    override session of the user

--- src/ONYXKEYS.ts
+++ src/ONYXKEYS.ts
@@ -458,6 +458,9 @@ const ONYXKEYS = {
     /** The user's Concierge reportID */
     CONCIERGE_REPORT_ID: 'conciergeReportID',
 
+    /** The user's session that will be preserved when using imported state */
+    PRESERVED_USER_SESSION: 'preservedUserSession',
+
     /** Collection Keys */
     COLLECTION: {
         DOWNLOAD: 'download_',
@@ -1029,6 +1032,7 @@ type OnyxValuesMapping = {
     [ONYXKEYS.SHOULD_SHOW_SAVED_SEARCH_RENAME_TOOLTIP]: boolean;
     [ONYXKEYS.NVP_EXPENSIFY_COMPANY_CARDS_CUSTOM_NAMES]: Record<string, string>;
     [ONYXKEYS.CONCIERGE_REPORT_ID]: string;
+    [ONYXKEYS.PRESERVED_USER_SESSION]: OnyxTypes.Session;
 };
 type OnyxValues = OnyxValuesMapping & OnyxCollectionValuesMapping & OnyxFormValuesMapping & OnyxFormDraftValuesMapping;
 
--- src/components/ImportOnyxState/index.tsx
+++ src/components/ImportOnyxState/index.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
-import Onyx from 'react-native-onyx';
+import Onyx, {useOnyx} from 'react-native-onyx';
 import type {FileObject} from '@components/AttachmentModal';
-import {KEYS_TO_PRESERVE, setIsUsingImportedState} from '@libs/actions/App';
+import {KEYS_TO_PRESERVE, setIsUsingImportedState, setPreservedUserSession} from '@libs/actions/App';
 import {setShouldForceOffline} from '@libs/actions/Network';
 import Navigation from '@libs/Navigation/Navigation';
 import type {OnyxValues} from '@src/ONYXKEYS';
+import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import BaseImportOnyxState from './BaseImportOnyxState';
 import type ImportOnyxStateProps from './types';
@@ -12,6 +13,7 @@ import {cleanAndTransformState} from './utils';
 
 export default function ImportOnyxState({setIsLoading, isLoading}: ImportOnyxStateProps) {
     const [isErrorModalVisible, setIsErrorModalVisible] = useState(false);
+    const [session] = useOnyx(ONYXKEYS.SESSION);
 
     const handleFileRead = (file: FileObject) => {
         if (!file.uri) {
@@ -27,6 +29,8 @@ export default function ImportOnyxState({setIsLoading, isLoading}: ImportOnyxSta
             .then((text) => {
                 const fileContent = text;
                 const transformedState = cleanAndTransformState<OnyxValues>(fileContent);
+                const currentUserSessionCopy = {...session};
+                setPreservedUserSession(currentUserSessionCopy);
                 setShouldForceOffline(true);
                 Onyx.clear(KEYS_TO_PRESERVE).then(() => {
                     Onyx.multiSet(transformedState)
