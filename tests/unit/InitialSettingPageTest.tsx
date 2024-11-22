279ea761b07bc445759fefd9a5cef81dbbb96be9
Hans <visaocuoitroi193@gmail.com>
2024-11-22 15:48:47 +0700

    make tests running

--- tests/unit/InitialSettingPageTest.tsx
+++ tests/unit/InitialSettingPageTest.tsx
@@ -1,32 +1,91 @@
-import Navigation from '@libs/Navigation/Navigation';
-import ROUTES from '@src/ROUTES';
+import * as NativeNavigation from '@react-navigation/native';
+import {fireEvent, render, screen} from '@testing-library/react-native';
+import Onyx from 'react-native-onyx';
+import {act} from 'react-test-renderer';
+import * as Localize from '@libs/Localize';
+import App from '@src/App';
+import OnyxUpdateManager from '@src/libs/actions/OnyxUpdateManager';
+import ONYXKEYS from '@src/ONYXKEYS';
+import type {NativeNavigationMock} from '../../__mocks__/@react-navigation/native';
+import PusherHelper from '../utils/PusherHelper';
+import * as TestHelper from '../utils/TestHelper';
+import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
+import waitForBatchedUpdatesWithAct from '../utils/waitForBatchedUpdatesWithAct';
 
-jest.mock('@src/libs/Navigation/Navigation', () => ({
-    navigate: jest.fn(),
-}));
+const USER_A_ACCOUNT_ID = 1;
+const USER_A_EMAIL = 'user_a@test.com';
 
-const switchToExpensifyClassic = (shouldOpenBookACall: boolean) => {
-    if (shouldOpenBookACall) {
-        Navigation.navigate(ROUTES.SETTINGS_EXIT_SURVERY_BOOK_CALL.route);
-        return;
+jest.setTimeout(60000);
+
+jest.mock('@react-navigation/native');
+
+TestHelper.setupApp();
+TestHelper.setupGlobalFetchMock();
+
+function navigateToSetting() {
+    const hintText = Localize.translateLocal('sidebarScreen.buttonMySettings');
+    const reportHeaderBackButton = screen.queryByAccessibilityHint(hintText);
+    if (reportHeaderBackButton) {
+        fireEvent(reportHeaderBackButton, 'press');
     }
-    Navigation.navigate(ROUTES.SETTINGS_EXIT_SURVEY_CONFIRM.route);
-};
+    return waitForBatchedUpdatesWithAct();
+}
 
-describe('Test switch to Expensify Classic flow', () => {
-    afterEach(() => {
+function navigateToExpensifyClassicFlow() {
+    const hintText = Localize.translateLocal('exitSurvey.goToExpensifyClassic');
+    const switchToExpensifyClassicBtn = screen.queryByAccessibilityHint(hintText);
+    if (switchToExpensifyClassicBtn) {
+        fireEvent(switchToExpensifyClassicBtn, 'press');
+    }
+    return waitForBatchedUpdatesWithAct();
+}
+
+function signInAppAndEnterTestFlow(dismissedValue?: boolean): Promise<void> {
+    render(<App />);
+    return waitForBatchedUpdatesWithAct()
+        .then(async () => {
+            await waitForBatchedUpdatesWithAct();
+            const hintText = Localize.translateLocal('loginForm.loginForm');
+            const loginForm = screen.queryAllByLabelText(hintText);
+            expect(loginForm).toHaveLength(1);
+
+            await act(async () => {
+                await TestHelper.signInWithTestUser(USER_A_ACCOUNT_ID, USER_A_EMAIL, undefined, undefined, 'A');
+            });
+            await Onyx.set(ONYXKEYS.NVP_TRYNEWDOT, {
+                classicRedirect: {
+                    dismissed: dismissedValue,
+                },
+            });
+            await waitForBatchedUpdates();
+            return navigateToSetting();
+        })
+        .then(async () => {
+            await act(() => (NativeNavigation as NativeNavigationMock).triggerTransitionEnd());
+            return navigateToExpensifyClassicFlow();
+        });
+}
+
+OnyxUpdateManager();
+
+describe('Switch to Expensify Classic flow', () => {
+    beforeEach(() => {
         jest.clearAllMocks();
+        Onyx.clear();
+
+        // Unsubscribe to pusher channels
+        PusherHelper.teardown();
     });
 
-    it('Should navigate to BookACall page if shouldOpenBookACall is true', () => {
-        switchToExpensifyClassic(true);
-        expect(Navigation.navigate).toHaveBeenCalledWith(ROUTES.SETTINGS_EXIT_SURVERY_BOOK_CALL.route);
-        expect(Navigation.navigate).toHaveBeenCalledTimes(1);
+    test('Should navigate to BookACall when dismissed is false', () => {
+        signInAppAndEnterTestFlow(false).then(() => {
+            expect(screen.getAllByText(Localize.translateLocal('exitSurvey.bookACallTitle')).at(0)).toBeOnTheScreen();
+        });
     });
 
-    it('Should navigate to Confirm page if shouldOpenBookACall is false', () => {
-        switchToExpensifyClassic(false);
-        expect(Navigation.navigate).toHaveBeenCalledWith(ROUTES.SETTINGS_EXIT_SURVEY_CONFIRM.route);
-        expect(Navigation.navigate).toHaveBeenCalledTimes(1);
+    test('Should navigate to ConfirmPage when dismissed is true', () => {
+        signInAppAndEnterTestFlow(true).then(() => {
+            expect(screen.getAllByText(Localize.translateLocal('exitSurvey.goToExpensifyClassic')).at(0)).toBeOnTheScreen();
+        });
     });
 });
