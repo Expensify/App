53ff59332bbf2b961846c1b84d8616f151111326
Artem Makushov <waterim3009@gmail.com>
2024-11-22 10:19:46 +0100

    update isLoading both for expensify and company cards

--- src/pages/workspace/expensifyCard/WorkspaceExpensifyCardPage.tsx
+++ src/pages/workspace/expensifyCard/WorkspaceExpensifyCardPage.tsx
@@ -36,7 +36,7 @@ function WorkspaceExpensifyCardPage({route}: WorkspaceExpensifyCardPageProps) {
     useFocusEffect(fetchExpensifyCards);
 
     const paymentBankAccountID = cardSettings?.paymentBankAccountID ?? 0;
-    const isLoading = !isOffline && (!cardSettings || (cardSettings.isLoading && !cardsList));
+    const isLoading = !isOffline && !cardSettings;
 
     return (
         <AccessOrNotFoundWrapper
