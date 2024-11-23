e68f3881d3c9afc42147f8e94c85a5ec952a2f04
Sachin Chavda <sachin.chavda.1024@gmail.com>
2024-11-23 06:35:53 +0530

    Cleanup

--- src/pages/settings/Wallet/WalletPage/WalletPage.tsx
+++ src/pages/settings/Wallet/WalletPage/WalletPage.tsx
@@ -3,7 +3,7 @@ import isEmpty from 'lodash/isEmpty';
 import type {ForwardedRef, RefObject} from 'react';
 import React, {useCallback, useEffect, useLayoutEffect, useRef, useState} from 'react';
 import type {GestureResponderEvent} from 'react-native';
-import {ActivityIndicator, Dimensions, InteractionManager, View} from 'react-native';
+import {ActivityIndicator, Dimensions, View} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
 import AddPaymentMethodMenu from '@components/AddPaymentMethodMenu';
 import ConfirmModal from '@components/ConfirmModal';
