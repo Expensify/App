1b89a1f6f1e89dce8d7dbdc468bf89ec7d36a231
Huu Le <20178761+huult@users.noreply.github.com>
2024-11-25 16:53:42 +0700

    Remove unused imports

--- src/libs/actions/Policy/Category.ts
+++ src/libs/actions/Policy/Category.ts
@@ -33,7 +33,7 @@ import * as ReportUtils from '@libs/ReportUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Policy, PolicyCategories, PolicyCategory, RecentlyUsedCategories, Report} from '@src/types/onyx';
-import type {ApprovalRule, CustomUnit, ExpenseRule, MccGroup} from '@src/types/onyx/Policy';
+import type {ApprovalRule, ExpenseRule, MccGroup} from '@src/types/onyx/Policy';
 import type {PolicyCategoryExpenseLimitType} from '@src/types/onyx/PolicyCategory';
 import type {OnyxData} from '@src/types/onyx/Request';
 
