f3cfec5cb4d3e0f66984c49b202110d20866b5c9
Carlos Martins <cmartins@expensify.com>
2024-11-26 13:09:48 -0700

    Merge pull request #53165 from Expensify/vit-53150
    [CP Staging] Make sure to check the navBarManager is defined
    
    (cherry picked from commit d69518cb52ed3a5f88372468ad47c264087213c6)
    
    (CP triggered by mountiny)

--- src/libs/NavBarManager/index.android.ts
+++ src/libs/NavBarManager/index.android.ts
@@ -1,9 +1,15 @@
 import {NativeModules} from 'react-native';
+import Log from '@libs/Log';
 import type StartupTimer from './types';
 import type {NavBarButtonStyle} from './types';
 
 const navBarManager: StartupTimer = {
     setButtonStyle: (style: NavBarButtonStyle) => {
+        if (!NativeModules.RNNavBarManager) {
+            Log.hmmm('RNNavBarManager not found');
+            return;
+        }
+
         NativeModules.RNNavBarManager.setButtonStyle(style);
     },
 };
--- src/libs/Navigation/AppNavigator/AuthScreens.tsx
+++ src/libs/Navigation/AppNavigator/AuthScreens.tsx
@@ -261,6 +261,10 @@ function AuthScreens({session, lastOpenedPublicRoomID, initialLastUpdateIDApplie
     });
 
     useEffect(() => {
+        if (!NavBarManager) {
+            Log.hmmm('RNNavBarManager not found in AuthScreens');
+            return;
+        }
         NavBarManager.setButtonStyle(theme.navigationBarButtonsStyle);
 
         return () => {
