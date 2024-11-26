c781e696533d30f89c0c4f8bd09a3d70aa65d921
FitseTLT <engfitsum@gmail.com>
2024-11-27 01:47:50 +0300

    remove unnecessary import

--- src/pages/OnboardingAccounting/BaseOnboardingAccounting.tsx
+++ src/pages/OnboardingAccounting/BaseOnboardingAccounting.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {InteractionManager} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
 import Button from '@components/Button';
