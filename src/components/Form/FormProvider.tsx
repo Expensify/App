731fa283891ae27544c744de6b6b76ae3183b872
Sachin Chavda <sachin.chavda.1024@gmail.com>
2024-11-27 22:05:16 +0530

    lint

--- src/components/Form/FormProvider.tsx
+++ src/components/Form/FormProvider.tsx
@@ -216,8 +216,7 @@ function FormProvider(
         isSubmittingRef.current = true;
         KeyboardUtils.dismiss().then(() => onSubmit(trimmedStringValues));
 
-        // eslint-disable-next-line no-unused-expressions
-        () => (isSubmittingRef.current = false);
+        return () => (isSubmittingRef.current = false);
     }, [enabledWhenOffline, formState?.isLoading, inputValues, network?.isOffline, onSubmit, onValidate, shouldTrimValues]);
 
     // Keep track of the focus state of the current screen.
