d5f53d10184b3be7ed2a026d6779681d56556524
Christoph Pader <chris@margelo.com>
2024-11-27 19:36:54 +0100

    fix: useSafeAreaInsets hook

--- src/hooks/useSafeAreaInsets.ts
+++ src/hooks/useSafeAreaInsets.ts
@@ -1,4 +1,13 @@
-/* eslint-disable no-restricted-imports */
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import type {EdgeInsets} from 'react-native-safe-area-context';
+// eslint-disable-next-line no-restricted-imports
+import {useSafeAreaInsets as useSafeAreaInsetsInternal} from 'react-native-safe-area-context';
+import useStyleUtils from './useStyleUtils';
+
+function useSafeAreaInsets(): EdgeInsets {
+    const StyleUtils = useStyleUtils();
+    const insets = useSafeAreaInsetsInternal();
+    const adjustedInsets = StyleUtils.getSafeAreaInsets(insets);
+    return adjustedInsets;
+}
 
 export default useSafeAreaInsets;
