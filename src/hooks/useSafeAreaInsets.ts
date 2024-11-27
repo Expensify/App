33c2afadbe9248f1a2bcd9fcc12c3f282a84b48f
Christoph Pader <chris@margelo.com>
2024-11-27 23:53:18 +0100

    fix: set inset bottom to 0 if keyboard is open

--- src/hooks/useSafeAreaInsets.ts
+++ src/hooks/useSafeAreaInsets.ts
@@ -1,13 +1,19 @@
 import type {EdgeInsets} from 'react-native-safe-area-context';
 // eslint-disable-next-line no-restricted-imports
 import {useSafeAreaInsets as useSafeAreaInsetsInternal} from 'react-native-safe-area-context';
+import useKeyboardState from './useKeyboardState';
 import useStyleUtils from './useStyleUtils';
 
 function useSafeAreaInsets(): EdgeInsets {
     const StyleUtils = useStyleUtils();
     const insets = useSafeAreaInsetsInternal();
+    const {isKeyboardShown} = useKeyboardState();
     const adjustedInsets = StyleUtils.getSafeAreaInsets(insets);
-    return adjustedInsets;
+
+    return {
+        ...adjustedInsets,
+        bottom: isKeyboardShown ? 0 : insets.bottom,
+    };
 }
 
 export default useSafeAreaInsets;
