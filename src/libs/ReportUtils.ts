551fac9d3d86d0d744b711f6050ed26374f3a224
abzokhattab <abzokhattab@gmail.com>
2024-11-27 12:20:39 +0100

    cleanup

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -1,5 +1,5 @@
-import { format } from 'date-fns';
-import { Str } from 'expensify-common';
+import {format} from 'date-fns';
+import {Str} from 'expensify-common';
 import lodashEscape from 'lodash/escape';
 import lodashFindLastIndex from 'lodash/findLastIndex';
 import lodashIntersection from 'lodash/intersection';
@@ -7,7 +7,6 @@ import isEmpty from 'lodash/isEmpty';
 import lodashIsEqual from 'lodash/isEqual';
 import isNumber from 'lodash/isNumber';
 import lodashMaxBy from 'lodash/maxBy';
-
 import type {OnyxCollection, OnyxEntry, OnyxUpdate} from 'react-native-onyx';
 import Onyx from 'react-native-onyx';
 import type {SvgProps} from 'react-native-svg';
@@ -6357,7 +6356,6 @@ function hasViolations(reportID: string, transactionViolations: OnyxCollection<T
     return transactions.some((transaction) => TransactionUtils.hasViolation(transaction.transactionID, transactionViolations, shouldShowInReview));
 }
 
-
 /**
  * Checks to see if a report contains a violation of type `warning`
  */
@@ -8579,7 +8577,6 @@ export {
     hasSmartscanError,
     hasUpdatedTotal,
     hasViolations,
-    hasNonHoldViolations,
     hasWarningTypeViolations,
     hasNoticeTypeViolations,
     isActionCreator,
