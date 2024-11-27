b61410e61e9c24116bc17c7af11939b890514162
Carlos Martins <cmartins@expensify.com>
2024-11-27 14:20:08 -0700

    refactor all pages

--- src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersExportedPage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersExportedPage.tsx
@@ -1,64 +1,13 @@
-import {format} from 'date-fns';
 import React from 'react';
-import {useOnyx} from 'react-native-onyx';
-import DatePicker from '@components/DatePicker';
-import FormProvider from '@components/Form/FormProvider';
-import InputWrapper from '@components/Form/InputWrapper';
-import type {FormOnyxValues} from '@components/Form/types';
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
-import ScreenWrapper from '@components/ScreenWrapper';
-import useLocalize from '@hooks/useLocalize';
-import useThemeStyles from '@hooks/useThemeStyles';
-import {updateAdvancedFilters} from '@libs/actions/Search';
-import Navigation from '@libs/Navigation/Navigation';
+import SearchDateFilterBase from '@components/Search/SearchDateFilterBase';
 import CONST from '@src/CONST';
-import ONYXKEYS from '@src/ONYXKEYS';
-import ROUTES from '@src/ROUTES';
-import FILTER_KEYS from '@src/types/form/SearchAdvancedFiltersForm';
 
 function SearchFiltersExportedPage() {
-    const styles = useThemeStyles();
-    const {translate} = useLocalize();
-
-    const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM);
-    const exportedDate = searchAdvancedFiltersForm?.[FILTER_KEYS.EXPORTED] ? format(searchAdvancedFiltersForm?.[FILTER_KEYS.EXPORTED], 'yyyy-MM-dd') : undefined;
-
-    const updateExportedFilter = (values: FormOnyxValues<typeof ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM>) => {
-        updateAdvancedFilters(values);
-        Navigation.goBack(ROUTES.SEARCH_ADVANCED_FILTERS);
-    };
-
     return (
-        <ScreenWrapper
-            testID={SearchFiltersExportedPage.displayName}
-            shouldShowOfflineIndicatorInWideScreen
-            offlineIndicatorStyle={styles.mtAuto}
-            includeSafeAreaPaddingBottom={false}
-            shouldEnableMaxHeight
-        >
-            <HeaderWithBackButton
-                title={translate('search.filters.exported')}
-                onBackButtonPress={() => {
-                    Navigation.goBack(ROUTES.SEARCH_ADVANCED_FILTERS);
-                }}
-            />
-            <FormProvider
-                style={[styles.flex1, styles.ph5]}
-                formID={ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM}
-                onSubmit={updateExportedFilter}
-                submitButtonText={translate('common.save')}
-                enabledWhenOffline
-            >
-                <InputWrapper
-                    InputComponent={DatePicker}
-                    inputID={FILTER_KEYS.EXPORTED}
-                    label={translate('search.filters.exported')}
-                    defaultValue={exportedDate}
-                    maxDate={CONST.CALENDAR_PICKER.MAX_DATE}
-                    minDate={CONST.CALENDAR_PICKER.MIN_DATE}
-                />
-            </FormProvider>
-        </ScreenWrapper>
+        <SearchDateFilterBase
+            dateKey={CONST.SEARCH.SYNTAX_FILTER_KEYS.EXPORTED}
+            titleKey="search.filters.exported"
+        />
     );
 }
 
--- src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersPostedPage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersPostedPage.tsx
@@ -1,64 +1,13 @@
-import {format} from 'date-fns';
 import React from 'react';
-import {useOnyx} from 'react-native-onyx';
-import DatePicker from '@components/DatePicker';
-import FormProvider from '@components/Form/FormProvider';
-import InputWrapper from '@components/Form/InputWrapper';
-import type {FormOnyxValues} from '@components/Form/types';
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
-import ScreenWrapper from '@components/ScreenWrapper';
-import useLocalize from '@hooks/useLocalize';
-import useThemeStyles from '@hooks/useThemeStyles';
-import {updateAdvancedFilters} from '@libs/actions/Search';
-import Navigation from '@libs/Navigation/Navigation';
+import SearchDateFilterBase from '@components/Search/SearchDateFilterBase';
 import CONST from '@src/CONST';
-import ONYXKEYS from '@src/ONYXKEYS';
-import ROUTES from '@src/ROUTES';
-import FILTER_KEYS from '@src/types/form/SearchAdvancedFiltersForm';
 
 function SearchFiltersPostedPage() {
-    const styles = useThemeStyles();
-    const {translate} = useLocalize();
-
-    const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM);
-    const postedDate = searchAdvancedFiltersForm?.[FILTER_KEYS.POSTED] ? format(searchAdvancedFiltersForm?.[FILTER_KEYS.POSTED], 'yyyy-MM-dd') : undefined;
-
-    const updatePostedFilter = (values: FormOnyxValues<typeof ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM>) => {
-        updateAdvancedFilters(values);
-        Navigation.goBack(ROUTES.SEARCH_ADVANCED_FILTERS);
-    };
-
     return (
-        <ScreenWrapper
-            testID={SearchFiltersPostedPage.displayName}
-            shouldShowOfflineIndicatorInWideScreen
-            offlineIndicatorStyle={styles.mtAuto}
-            includeSafeAreaPaddingBottom={false}
-            shouldEnableMaxHeight
-        >
-            <HeaderWithBackButton
-                title={translate('search.filters.posted')}
-                onBackButtonPress={() => {
-                    Navigation.goBack(ROUTES.SEARCH_ADVANCED_FILTERS);
-                }}
-            />
-            <FormProvider
-                style={[styles.flex1, styles.ph5]}
-                formID={ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM}
-                onSubmit={updatePostedFilter}
-                submitButtonText={translate('common.save')}
-                enabledWhenOffline
-            >
-                <InputWrapper
-                    InputComponent={DatePicker}
-                    inputID={FILTER_KEYS.POSTED}
-                    label={translate('search.filters.posted')}
-                    defaultValue={postedDate}
-                    maxDate={CONST.CALENDAR_PICKER.MAX_DATE}
-                    minDate={CONST.CALENDAR_PICKER.MIN_DATE}
-                />
-            </FormProvider>
-        </ScreenWrapper>
+        <SearchDateFilterBase
+            dateKey={CONST.SEARCH.SYNTAX_FILTER_KEYS.POSTED}
+            titleKey="search.filters.posted"
+        />
     );
 }
 
