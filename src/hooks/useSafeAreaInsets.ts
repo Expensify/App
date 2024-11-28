8748e7930c3226a8d500ff60bde787d6e65ad733
Christoph Pader <chris@margelo.com>
2024-11-28 01:25:04 +0100

    fix: remove useKeyboardState hook usage

--- src/hooks/useSafeAreaInsets.ts
+++ src/hooks/useSafeAreaInsets.ts
@@ -1,19 +1,14 @@
 import type {EdgeInsets} from 'react-native-safe-area-context';
 // eslint-disable-next-line no-restricted-imports
 import {useSafeAreaInsets as useSafeAreaInsetsInternal} from 'react-native-safe-area-context';
-import useKeyboardState from './useKeyboardState';
 import useStyleUtils from './useStyleUtils';
 
 function useSafeAreaInsets(): EdgeInsets {
     const StyleUtils = useStyleUtils();
     const insets = useSafeAreaInsetsInternal();
-    const {isKeyboardShown} = useKeyboardState();
     const adjustedInsets = StyleUtils.getSafeAreaInsets(insets);
 
-    return {
-        ...adjustedInsets,
-        bottom: isKeyboardShown ? 0 : insets.bottom,
-    };
+    return adjustedInsets;
 }
 
 export default useSafeAreaInsets;
