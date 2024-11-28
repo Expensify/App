6a9854f089bd30c86d6b26d34055e77c1a7f95b0
jayeshmangwani <jayeshmangwani@gmail.com>
2024-11-28 21:56:35 +0530

    rever PR 52117

--- src/pages/workspace/upgrade/UpgradeIntro.tsx
+++ src/pages/workspace/upgrade/UpgradeIntro.tsx
@@ -15,12 +15,11 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
 import CONST from '@src/CONST';
 import ROUTES from '@src/ROUTES';
-import GenericFeaturesView from './GenericFeaturesView';
 
 type Props = {
     buttonDisabled?: boolean;
     loading?: boolean;
-    feature?: ValueOf<typeof CONST.UPGRADE_FEATURE_INTRO_MAPPING>;
+    feature: ValueOf<typeof CONST.UPGRADE_FEATURE_INTRO_MAPPING>;
     onUpgrade: () => void;
     isCategorizing?: boolean;
 };
@@ -30,15 +29,6 @@ function UpgradeIntro({feature, onUpgrade, buttonDisabled, loading, isCategorizi
     const {isExtraSmallScreenWidth} = useResponsiveLayout();
     const {translate} = useLocalize();
 
-    if (!feature) {
-        return (
-            <GenericFeaturesView
-                onUpgrade={onUpgrade}
-                buttonDisabled={buttonDisabled}
-                loading={loading}
-            />
-        );
-    }
     const isIllustration = feature.icon in Illustrations;
     const iconSrc = isIllustration ? Illustrations[feature.icon as keyof typeof Illustrations] : Expensicon[feature.icon as keyof typeof Expensicon];
     const iconAdditionalStyles = feature.id === CONST.UPGRADE_FEATURE_INTRO_MAPPING.approvals.id ? styles.br0 : undefined;
