a093da35e761932cdf9aa03de2e9cac528fdd690
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-11-29 13:52:41 +0100

    cleanup fixed footer

--- src/components/FixedFooter.tsx
+++ src/components/FixedFooter.tsx
@@ -2,8 +2,6 @@ import type {ReactNode} from 'react';
 import React from 'react';
 import type {StyleProp, ViewStyle} from 'react-native';
 import {View} from 'react-native';
-import useEdgeSpacing from '@hooks/useEdgeSpacing';
-import useSafeAreaInsets from '@hooks/useSafeAreaInsets';
 import useThemeStyles from '@hooks/useThemeStyles';
 
 type FixedFooterProps = {
@@ -14,19 +12,12 @@ type FixedFooterProps = {
     style?: StyleProp<ViewStyle>;
 };
 
-// ScreenWrapper (Web: 0, Mobile: 24/variable)
-//  Component (All platforms: bottomPadding: 20) ?
-
 function FixedFooter({style, children}: FixedFooterProps) {
-    // const insets = useSafeAreaInsets();
-    const {bottom} = useEdgeSpacing();
     const styles = useThemeStyles();
 
     if (!children) {
         return null;
     }
-
-    // const shouldAddBottomPadding = !insets.bottom;
 
     return <View style={[styles.ph5, styles.pb5, styles.flexShrink0, style]}>{children}</View>;
 }
