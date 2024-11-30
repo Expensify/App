01dd6fffba6f362ceb72caa2769c171a126bc521
NikkiWines <nikkiwines@expensify.com>
2024-11-30 00:09:54 +0000

    add handleError function

--- src/pages/signin/SAMLSignInPage/index.native.tsx
+++ src/pages/signin/SAMLSignInPage/index.native.tsx
@@ -28,9 +28,7 @@ function SAMLSignInPage() {
     useEffect(() => {
         // If we don't have a valid login to pass here, direct the user back to a clean sign in state to try again
         if (!credentials?.login) {
-            Session.clearSignInData();
-            Session.setAccountError(translate('common.error.email'));
-            Navigation.goBack(ROUTES.HOME);
+            handleError(translate('common.error.email'), true);
             return;
         }
 
@@ -45,9 +43,14 @@ function SAMLSignInPage() {
         body.append('platform', getPlatform());
         fetchSAMLUrl(body).then((response) => {
             if (!response || !response.url) {
+                handleError(translate('common.error.login'));
                 return;
             }
             setSAMLUrl(response.url);
+        })
+        .catch((response) => {
+            handleError(response.message ?? translate('common.error.login'));
+            return;
         });
     }, [credentials?.login, SAMLUrl]);
 
@@ -82,6 +85,15 @@ function SAMLSignInPage() {
         },
         [credentials?.login, shouldShowNavigation, account?.isLoading],
     );
+
+    function handleError(errorMessage: string, cleanSignInData: boolean = false) {
+        if (cleanSignInData) {
+            Session.clearSignInData();
+        }
+
+        Session.setAccountError(errorMessage);
+        Navigation.goBack(ROUTES.HOME);
+    }
 
     return (
         <ScreenWrapper
