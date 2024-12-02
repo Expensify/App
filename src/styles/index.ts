59e3e757a296228a3936aaab3acbdbff5e41aa0d
sumo_slonik <jakub.nowakowski@swmansion.com>
2024-12-02 11:31:55 +0100

    remove unnecessary import after merge

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,7 +2,7 @@
 import type {LineLayerStyleProps} from '@rnmapbox/maps/src/utils/MapboxStyles';
 import lodashClamp from 'lodash/clamp';
 import type {LineLayer} from 'react-map-gl';
-import type {AnimatableNumericValue, Animated, ImageStyle, TextStyle, ViewStyle} from 'react-native';
+import type {Animated, ImageStyle, TextStyle, ViewStyle} from 'react-native';
 import {Platform, StyleSheet} from 'react-native';
 import type {CustomAnimation} from 'react-native-animatable';
 import type {PickerStyle} from 'react-native-picker-select';
