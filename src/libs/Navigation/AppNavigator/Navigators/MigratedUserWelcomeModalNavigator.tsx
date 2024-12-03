b9cac988e08c36380460e52ada868078dcceb114
Ishpaul Singh <ishpaul207@gmail.com>
2024-12-04 00:42:03 +0530

    resolve conflict after merge

--- src/libs/Navigation/AppNavigator/Navigators/MigratedUserWelcomeModalNavigator.tsx
+++ src/libs/Navigation/AppNavigator/Navigators/MigratedUserWelcomeModalNavigator.tsx
@@ -1,18 +1,19 @@
-import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import {View} from 'react-native';
 import NoDropZone from '@components/DragAndDrop/NoDropZone';
 import MigratedUserWelcomeModal from '@components/MigratedUserWelcomeModal';
+import createPlatformStackNavigator from '@libs/Navigation/PlatformStackNavigation/createPlatformStackNavigator';
+import Animations from '@libs/Navigation/PlatformStackNavigation/navigationOptions/animation';
 import type {MigratedUserModalNavigatorParamList} from '@libs/Navigation/types';
 import SCREENS from '@src/SCREENS';
 
-const Stack = createStackNavigator<MigratedUserModalNavigatorParamList>();
+const Stack = createPlatformStackNavigator<MigratedUserModalNavigatorParamList>();
 
 function MigratedUserWelcomeModalNavigator() {
     return (
         <NoDropZone>
             <View>
-                <Stack.Navigator screenOptions={{headerShown: false, animationEnabled: true}}>
+                <Stack.Navigator screenOptions={{headerShown: false, animation: Animations.MODAL}}>
                     <Stack.Screen
                         name={SCREENS.MIGRATED_USER_WELCOME_MODAL.ROOT}
                         component={MigratedUserWelcomeModal}
