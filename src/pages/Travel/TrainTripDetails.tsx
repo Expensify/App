6466aec45a505d3b2a1a84a12a39a31d46bda151
Blazej Kustra <blazej.kustra@swmansion.com>
2024-12-03 13:24:26 +0100

    Refactor TrainTripDetails component to use reservation object for rendering details

--- src/pages/Travel/TrainTripDetails.tsx
+++ src/pages/Travel/TrainTripDetails.tsx
@@ -4,39 +4,92 @@ import type {OnyxEntry} from 'react-native-onyx';
 import * as Expensicons from '@components/Icon/Expensicons';
 import MenuItem from '@components/MenuItem';
 import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
+import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
-import useStyleUtils from '@hooks/useStyleUtils';
-import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import DateUtils from '@libs/DateUtils';
-import * as TripReservationUtils from '@libs/TripReservationUtils';
 import CONST from '@src/CONST';
-import type {PersonalDetails, Transaction} from '@src/types/onyx';
+import type {PersonalDetails} from '@src/types/onyx';
+import type {Reservation} from '@src/types/onyx/Transaction';
 
 type TrainTripDetailsProps = {
-    transaction: OnyxEntry<Transaction>;
+    reservation: Reservation;
     personalDetails: OnyxEntry<PersonalDetails>;
-    reservationIndex: number;
 };
 
-function TrainTripDetails({transaction, personalDetails, reservationIndex}: TrainTripDetailsProps) {
+function TrainTripDetails({reservation, personalDetails}: TrainTripDetailsProps) {
     const styles = useThemeStyles();
-    const theme = useTheme();
-    const StyleUtils = useStyleUtils();
     const {translate} = useLocalize();
 
-    const trainReservation = transaction?.receipt?.reservationList?.at(reservationIndex);
-
-    if (!trainReservation) {
-        return null;
-    }
-
-    const reservationIcon = TripReservationUtils.getTripReservationIcon(trainReservation.type);
-    const startDate = DateUtils.getFormattedTransportDateAndHour(new Date(trainReservation.start.date));
-    const endDate = DateUtils.getFormattedTransportDateAndHour(new Date(trainReservation.end.date));
+    const startDate = DateUtils.getFormattedTransportDateAndHour(new Date(reservation.start.date));
+    const endDate = DateUtils.getFormattedTransportDateAndHour(new Date(reservation.end.date));
+    const trainRouteDescription = `${reservation.start.longName} (${reservation.start.shortName}) ${translate('common.conjunctionTo')} ${reservation.end.longName} (${
+        reservation.end.shortName
+    })`;
+    const trainDuration = reservation.duration && DateUtils.getFormattedDuration(translate, reservation.duration);
 
     return (
         <>
+            <Text style={[styles.textHeadlineH1, styles.mh5, styles.mv3]}>{trainRouteDescription}</Text>
+
+            <MenuItemWithTopDescription
+                description={`${translate('travel.train')} ${trainDuration ? `${CONST.DOT_SEPARATOR} ${trainDuration}` : ''}`}
+                title={reservation.route?.name}
+                interactive={false}
+            />
+            <MenuItemWithTopDescription
+                description={translate('common.date')}
+                title={startDate.date}
+                interactive={false}
+            />
+
+            <MenuItemWithTopDescription
+                description={translate('travel.trainDetails.departs')}
+                descriptionTextStyle={[styles.textLabelSupporting, styles.mb1]}
+                titleComponent={<Text style={[styles.textLarge, styles.textHeadlineH2]}>{startDate.hour}</Text>}
+                helperText={`${reservation.start.longName} (${reservation.start.shortName})`}
+                helperTextStyle={[styles.pb3, styles.mtn2]}
+                interactive={false}
+            />
+            <MenuItemWithTopDescription
+                description={translate('travel.trainDetails.arrives')}
+                descriptionTextStyle={[styles.textLabelSupporting, styles.mb1]}
+                titleComponent={<Text style={[styles.textLarge, styles.textHeadlineH2]}>{endDate.hour}</Text>}
+                helperText={`${reservation.end.longName} (${reservation.end.shortName})`}
+                helperTextStyle={[styles.pb3, styles.mtn2]}
+                interactive={false}
+            />
+
+            <View style={[styles.flexRow, styles.flexWrap]}>
+                {!!reservation.coachNumber && (
+                    <View style={styles.w50}>
+                        <MenuItemWithTopDescription
+                            description={translate('travel.trainDetails.coachNumber')}
+                            title={reservation.coachNumber}
+                            interactive={false}
+                        />
+                    </View>
+                )}
+                {!!reservation.seatNumber && (
+                    <View style={styles.w50}>
+                        <MenuItemWithTopDescription
+                            description={translate('travel.trainDetails.seat')}
+                            title={reservation.seatNumber}
+                            interactive={false}
+                        />
+                    </View>
+                )}
+                {!!reservation.confirmations?.at(0)?.value && (
+                    <View style={styles.w50}>
+                        <MenuItemWithTopDescription
+                            description={translate('travel.trainDetails.confirmation')}
+                            title={reservation.confirmations?.at(0)?.value}
+                            interactive={false}
+                        />
+                    </View>
+                )}
+            </View>
+
             <MenuItem
                 label={translate('travel.trainDetails.passenger')}
                 title={personalDetails?.displayName}
@@ -46,65 +99,6 @@ function TrainTripDetails({transaction, personalDetails, reservationIndex}: Trai
                 interactive={false}
                 wrapperStyle={styles.pb3}
             />
-
-            <MenuItem
-                title={`${trainReservation.start.longName} (${trainReservation.start.shortName}) ${translate('common.conjunctionTo')} ${trainReservation.end.longName} (${
-                    trainReservation.end.shortName
-                })`}
-                description={`${trainReservation.company?.longName} ${CONST.DOT_SEPARATOR} ${trainReservation.route?.name}`}
-                descriptionTextStyle={[styles.textLabelSupporting, styles.lh16]}
-                secondaryIcon={reservationIcon}
-                wrapperStyle={[styles.taskDescriptionMenuItem]}
-                numberOfLinesDescription={2}
-                iconHeight={20}
-                iconWidth={20}
-                iconStyles={[StyleUtils.getTripReservationIconContainer(false), styles.mr3]}
-                secondaryIconFill={theme.icon}
-                interactive={false}
-            />
-            <MenuItemWithTopDescription
-                description={`${translate('travel.trainDetails.departs')} ${CONST.DOT_SEPARATOR} ${startDate.date}`}
-                title={startDate.hour}
-                helperText={`${trainReservation.start.longName} (${trainReservation.start.shortName})`}
-                helperTextStyle={[styles.pb3, styles.mtn2]}
-                interactive={false}
-            />
-            <MenuItemWithTopDescription
-                description={`${translate('travel.trainDetails.arrives')} ${CONST.DOT_SEPARATOR} ${endDate.date}`}
-                title={endDate.hour}
-                helperText={`${trainReservation.end.longName} (${trainReservation.end.shortName})`}
-                helperTextStyle={[styles.pb3, styles.mtn2]}
-                interactive={false}
-            />
-
-            <View style={[styles.flexRow, styles.flexWrap]}>
-                {!!trainReservation.coachNumber && (
-                    <View style={styles.w50}>
-                        <MenuItemWithTopDescription
-                            description={translate('travel.trainDetails.coachNumber')}
-                            title={trainReservation.coachNumber}
-                            interactive={false}
-                        />
-                    </View>
-                )}
-                {!!trainReservation.seatNumber && (
-                    <View style={styles.w50}>
-                        <MenuItemWithTopDescription
-                            description={translate('travel.trainDetails.seat')}
-                            title={trainReservation.seatNumber}
-                            interactive={false}
-                        />
-                    </View>
-                )}
-            </View>
-
-            {!!trainReservation.confirmations?.at(0)?.value && (
-                <MenuItemWithTopDescription
-                    description={translate('travel.trainDetails.confirmation')}
-                    title={trainReservation.confirmations?.at(0)?.value}
-                    interactive={false}
-                />
-            )}
         </>
     );
 }
