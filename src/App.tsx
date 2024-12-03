be2197f6bc4d7904ec87118c674ac7a24c9ac4f4
szymonrybczak <szymon.rybczak@gmail.com>
2024-12-03 17:45:44 +0100

    chore: small code cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {PortalProvider} from '@gorhom/portal';
-import React, {useEffect} from 'react';
+import React from 'react';
 import {LogBox} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {PickerStateProvider} from 'react-native-picker-select';
