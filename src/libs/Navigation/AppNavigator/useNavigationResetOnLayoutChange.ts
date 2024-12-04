5b46385713b541e7cae0f4c956d586b76f370202
Wojciech Boman <wojciechboman@gmail.com>
2024-12-04 11:01:17 +0100

    Add comments to useNavigationResetOnLayoutChange hooks

--- src/libs/Navigation/AppNavigator/useNavigationResetOnLayoutChange.ts
+++ src/libs/Navigation/AppNavigator/useNavigationResetOnLayoutChange.ts
@@ -4,6 +4,11 @@ import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import navigationRef from '@libs/Navigation/navigationRef';
 import type {CustomEffectsHookProps} from '@libs/Navigation/PlatformStackNavigation/types';
 
+/**
+ * This hook resets the navigation root state when changing the layout size, resetting the state calls the getRehydredState method in CustomFullScreenRouter.tsx.
+ * When the screen size is changed, it is necessary to check whether the application displays the content correctly.
+ * When the app is opened on a small layout and the user resizes it to wide, a second screen has to be present in the navigation state to fill the space.
+ */
 function useNavigationResetOnLayoutChange({navigation}: CustomEffectsHookProps<ParamListBase>) {
     const {shouldUseNarrowLayout} = useResponsiveLayout();
 
