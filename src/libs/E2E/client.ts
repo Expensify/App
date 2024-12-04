4ae6548a800ab497373290c56f11602f8d8f41b2
Tim Golen <tgolen@expensify.com>
2024-12-04 14:22:22 -0700

    Skip calling specific APIs in the E2E tests

--- src/libs/E2E/client.ts
+++ src/libs/E2E/client.ts
@@ -23,8 +23,13 @@ const defaultRequestInit: RequestInit = {
     headers: defaultHeaders,
 };
 
-const sendRequest = (url: string, data: Record<string, unknown>): Promise<Response> =>
-    fetch(url, {
+const sendRequest = (url: string, data: Record<string, unknown>): Promise<Response> => {
+    // Don't process these specific API commands because running them over and over again in the tests hammers the server in a bad way.
+    if (url.indexOf('command=OptInToPushNotifications') > -1 || url.indexOf('command=OptOutOfPushNotifications') > -1) {
+        return Promise.resolve(new Response());
+    }
+
+    return fetch(url, {
         method: 'POST',
         headers: {
             // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -46,6 +51,7 @@ const sendRequest = (url: string, data: Record<string, unknown>): Promise<Respon
                 throw new Error(errorMsg);
             });
     });
+};
 
 /**
  * Submits a test result to the server.
