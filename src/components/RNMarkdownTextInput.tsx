a9b0d1aebbfb58b25bb9bc8b296af8d6ab54bdfb
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2024-12-05 13:14:34 +0100

    Bump react-native-live-markdown to 0.1.192

--- src/components/RNMarkdownTextInput.tsx
+++ src/components/RNMarkdownTextInput.tsx
@@ -1,5 +1,5 @@
 import type {MarkdownTextInputProps} from '@expensify/react-native-live-markdown';
-import {MarkdownTextInput} from '@expensify/react-native-live-markdown';
+import {MarkdownTextInput, parseExpensiMark} from '@expensify/react-native-live-markdown';
 import type {ForwardedRef} from 'react';
 import React from 'react';
 import type {TextInput} from 'react-native';
@@ -9,9 +9,11 @@ import useTheme from '@hooks/useTheme';
 // Convert the underlying TextInput into an Animated component so that we can take an animated ref and pass it to a worklet
 const AnimatedMarkdownTextInput = Animated.createAnimatedComponent(MarkdownTextInput);
 
-type AnimatedMarkdownTextInputRef = typeof AnimatedMarkdownTextInput & TextInput & HTMLInputElement;
+type AnimatedMarkdownTextInputRef = typeof AnimatedMarkdownTextInput & MarkdownTextInput & HTMLInputElement;
 
-function RNMarkdownTextInputWithRef(props: MarkdownTextInputProps, ref: ForwardedRef<AnimatedMarkdownTextInputRef>) {
+type RNMarkdownTextInputProps = Omit<MarkdownTextInputProps, 'parser'>;
+
+function RNMarkdownTextInputWithRef(props: RNMarkdownTextInputProps, ref: ForwardedRef<AnimatedMarkdownTextInputRef>) {
     const theme = useTheme();
 
     return (
@@ -19,6 +21,7 @@ function RNMarkdownTextInputWithRef(props: MarkdownTextInputProps, ref: Forwarde
             allowFontScaling={false}
             textBreakStrategy="simple"
             keyboardAppearance={theme.colorScheme}
+            parser={parseExpensiMark}
             ref={(refHandle) => {
                 if (typeof ref !== 'function') {
                     return;
