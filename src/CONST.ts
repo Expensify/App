aae6ef7ef2bb1bb93a098607d3e1357a9c74fdfd
Adam Horodyski <ad.horodyski@gmail.com>
2024-12-05 20:38:12 +0100

    feat: mark start/end getOrderedReportIDs

--- src/CONST.ts
+++ src/CONST.ts
@@ -1301,6 +1301,7 @@ const CONST = {
         },
     },
     TIMING: {
+        GET_ORDERED_REPORT_IDS: 'get_ordered_report_ids',
         CALCULATE_MOST_RECENT_LAST_MODIFIED_ACTION: 'calc_most_recent_last_modified_action',
         OPEN_SEARCH: 'open_search',
         OPEN_REPORT: 'open_report',
