0cb9ab96e1d0b0935eb420dc968e04816acde394
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-12-06 13:14:03 +0800

    fix can't press pay button if edit composer is focused

--- src/libs/setShouldShowComposeInputKeyboardAware/index.ts
+++ src/libs/setShouldShowComposeInputKeyboardAware/index.ts
@@ -2,7 +2,12 @@ import * as Composer from '@userActions/Composer';
 import type SetShouldShowComposeInputKeyboardAware from './types';
 
 const setShouldShowComposeInputKeyboardAware: SetShouldShowComposeInputKeyboardAware = (shouldShow) => {
-    Composer.setShouldShowComposeInput(shouldShow);
+    // When the edit composer loses focus, we want to show the main composer. 
+    // If it loses focus due to a pressable being pressed, the press event might not be captured. 
+    // To address this, we delay showing the main composer to allow the press event to complete. 
+    setTimeout(() => {
+        Composer.setShouldShowComposeInput(shouldShow);
+    }, 0);
 };
 
 export default setShouldShowComposeInputKeyboardAware;
