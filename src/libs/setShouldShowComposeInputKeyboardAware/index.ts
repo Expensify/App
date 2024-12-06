4e1cc0810a1b41ce1f836abea3e829233f130d0a
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-12-06 18:50:00 +0800

    prettier

--- src/libs/setShouldShowComposeInputKeyboardAware/index.ts
+++ src/libs/setShouldShowComposeInputKeyboardAware/index.ts
@@ -2,9 +2,9 @@ import * as Composer from '@userActions/Composer';
 import type SetShouldShowComposeInputKeyboardAware from './types';
 
 const setShouldShowComposeInputKeyboardAware: SetShouldShowComposeInputKeyboardAware = (shouldShow) => {
-    // When the edit composer loses focus, we want to show the main composer. 
-    // If it loses focus due to a pressable being pressed, the press event might not be captured. 
-    // To address this, we delay showing the main composer to allow the press event to complete. 
+    // When the edit composer loses focus, we want to show the main composer.
+    // If it loses focus due to a pressable being pressed, the press event might not be captured.
+    // To address this, we delay showing the main composer to allow the press event to complete.
     setTimeout(() => {
         Composer.setShouldShowComposeInput(shouldShow);
     }, 0);
