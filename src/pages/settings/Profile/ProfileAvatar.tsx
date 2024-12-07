b05d47b541d3627451378dfc33de94da54fc88c0
Kalydosos <rnel2@hotmail.com>
2024-12-07 01:01:00 +0100

    resolve conflict on profileavatar

--- src/pages/settings/Profile/ProfileAvatar.tsx
+++ src/pages/settings/Profile/ProfileAvatar.tsx
@@ -19,7 +19,7 @@ function ProfileAvatar({route}: ProfileAvatarProps) {
     const [personalDetails] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST);
     const [personalDetailsMetadata] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_METADATA);
     const [isLoadingApp] = useOnyx(ONYXKEYS.IS_LOADING_APP, {initialValue: true});
-  
+
     const personalDetail = personalDetails?.[route.params.accountID];
     const avatarURL = personalDetail?.avatar ?? '';
     const accountID = Number(route.params.accountID ?? '-1');
@@ -37,7 +37,6 @@ function ProfileAvatar({route}: ProfileAvatarProps) {
         <AttachmentModal
             headerTitle={LocalePhoneNumber.formatPhoneNumber(displayName)}
             defaultOpen
-            headerTitle={displayName}
             source={UserUtils.getFullSizeAvatar(avatarURL, accountID)}
             onModalClose={() => {
                 setTimeout(() => {
