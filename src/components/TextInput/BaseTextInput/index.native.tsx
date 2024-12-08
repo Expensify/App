b8fdae85195777ee2e5cf324bd5641ae74946495
QichenZhu <57348009+QichenZhu@users.noreply.github.com>
2024-12-09 12:55:12 +1300

    Remove outdated comments

--- src/components/TextInput/BaseTextInput/index.native.tsx
+++ src/components/TextInput/BaseTextInput/index.native.tsx
@@ -451,14 +451,10 @@ function BaseTextInput(
             )}
             {/*
                  Text input component doesn't support auto grow by default.
-                 We're using a hidden text input to achieve that.
                  This text view is used to calculate width or height of the input value given textStyle in this component.
                  This Text component is intentionally positioned out of the screen.
              */}
             {(!!autoGrow || autoGrowHeight) && !isAutoGrowHeightMarkdown && (
-                // Add +2 to width on Safari browsers so that text is not cut off due to the cursor or when changing the value
-                // https://github.com/Expensify/App/issues/8158
-                // https://github.com/Expensify/App/issues/26628
                 <Text
                     style={[
                         inputStyle,
