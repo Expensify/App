cfb2417e088631f39e5d70f1a69a0292d1f7c011
Jakub Szymczak <szymczak.jakub12@gmail.com>
2024-12-09 11:42:10 +0100

    fix PR comments

--- src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersCardPage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersCardPage.tsx
@@ -60,13 +60,7 @@ function buildCardFeedItem(
     bank: CompanyCardFeed,
     iconStyles: StyleProp<ViewStyle>,
 ): CardFilterItem {
-    let isSelected = true;
-    correspondingCardIDs.forEach((card) => {
-        if (selectedCards.includes(card)) {
-            return;
-        }
-        isSelected = false;
-    });
+    const isSelected = correspondingCardIDs.every((card) => selectedCards.includes(card));
 
     const icon = CardUtils.getCardFeedIcon(bank);
     return {
@@ -84,6 +78,31 @@ function buildCardFeedItem(
     };
 }
 
+function getReapeatingBanks(filteredWorkspaceCardFeedsKeys: string[], domainFeedsData: Record<string, DomainFeedData>) {
+    const repeatingBanks: string[] = [];
+    const banks: string[] = [];
+    const handleRepeatingBankNames = (bankName: string) => {
+        if (banks.includes(bankName)) {
+            repeatingBanks.push(bankName);
+        } else {
+            banks.push(bankName);
+        }
+    };
+
+    filteredWorkspaceCardFeedsKeys.forEach((cardFeedKey) => {
+        const bankName = cardFeedKey.split('_').at(2);
+        if (!bankName) {
+            return;
+        }
+
+        handleRepeatingBankNames(bankName);
+    });
+    Object.values(domainFeedsData).forEach((domainFeed) => {
+        handleRepeatingBankNames(domainFeed.bank);
+    });
+    return repeatingBanks;
+}
+
 function SearchFiltersCardPage() {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
@@ -95,18 +114,12 @@ function SearchFiltersCardPage() {
     const [searchTerm, debouncedSearchTerm, setSearchTerm] = useDebouncedState('');
     const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM);
     const initiallySelectedCards = searchAdvancedFiltersForm?.cardID;
-    const [newSelectedCards, setNewSelectedCards] = useState(initiallySelectedCards ?? []);
+    const [selectedCards, setSelectedCards] = useState(initiallySelectedCards ?? []);
 
-    const {invidualCardsSectionData, domainFeedsData} = useMemo(() => {
-        const individualCards: CardFilterItem[] = [];
+    const {individualCardsSectionData, domainFeedsData} = useMemo(() => {
         const domainFeeds: Record<string, DomainFeedData> = {};
 
-        Object.values(userCardList ?? {}).forEach((card) => {
-            const isSelected = newSelectedCards.includes(card.cardID.toString());
-            const cardData = buildIndividualCardItem(card, isSelected, styles.cardIcon);
-
-            individualCards.push(cardData);
-
+        const userAssignedCards = Object.values(userCardList ?? {}).map((card) => {
             // Cards in cardList can also be domain cards, we use them to compute domain feed
             if (!card.domainName.match(CONST.REGEX.EXPENSIFY_POLICY_DOMAIN_NAME)) {
                 if (domainFeeds[card.domainName]) {
@@ -115,100 +128,84 @@ function SearchFiltersCardPage() {
                     domainFeeds[card.domainName] = {domainName: card.domainName, bank: card.bank, correspospondingCardIDs: [card.cardID.toString()]};
                 }
             }
+            const isSelected = selectedCards.includes(card.cardID.toString());
+            const cardData = buildIndividualCardItem(card, isSelected, styles.cardIcon);
+
+            return cardData;
         });
 
         // When user is admin of a workspace he sees all the cards of workspace under cards_ Onyx key
-        filteredWorkspaceCardFeeds.forEach(([, cardFeed]) => {
-            Object.values(cardFeed ?? {}).forEach((card) => {
-                if (!card || !isCard(card) || userCardList?.[card.cardID]) {
-                    return;
-                }
-                const isSelected = newSelectedCards.includes(card.cardID.toString());
-                const cardData = buildIndividualCardItem(card, isSelected, styles.cardIcon);
+        const allWorkspaceCards = filteredWorkspaceCardFeeds.flatMap(([, cardFeed]) => {
+            return Object.values(cardFeed ?? {})
+                .filter((card) => card && isCard(card) && !userCardList?.[card.cardID])
+                .map((card) => {
+                    const isSelected = selectedCards.includes(card.cardID.toString());
+                    const cardData = buildIndividualCardItem(card as Card, isSelected, styles.cardIcon);
 
-                individualCards.push(cardData);
-            });
+                    return cardData;
+                });
         });
-        return {invidualCardsSectionData: individualCards, domainFeedsData: domainFeeds};
-    }, [filteredWorkspaceCardFeeds, newSelectedCards, styles.cardIcon, userCardList]);
+        return {individualCardsSectionData: [...userAssignedCards, ...allWorkspaceCards], domainFeedsData: domainFeeds};
+    }, [filteredWorkspaceCardFeeds, selectedCards, styles.cardIcon, userCardList]);
 
     const cardFeedsSectionData = useMemo(() => {
-        const repeatingBanks: string[] = [];
-        const banks: string[] = [];
-        const handleRepeatingBankNames = (bankName: string) => {
-            if (banks.includes(bankName)) {
-                repeatingBanks.push(bankName);
-            } else {
-                banks.push(bankName);
-            }
-        };
+        const repeatingBanks = getReapeatingBanks(Object.keys(filteredWorkspaceCardFeeds), domainFeedsData);
 
-        filteredWorkspaceCardFeeds.forEach(([cardFeedKey]) => {
-            const bankName = cardFeedKey.split('_').at(2);
-            if (!bankName) {
-                return;
-            }
-
-            handleRepeatingBankNames(bankName);
-        });
-        Object.values(domainFeedsData).forEach((domainFeed) => {
-            handleRepeatingBankNames(domainFeed.bank);
-        });
-
-        const cardFeedsData: CardFilterItem[] = [];
-
-        filteredWorkspaceCardFeeds.forEach(([, cardFeed]) => {
-            const representativeCard = Object.values(cardFeed ?? {}).find((cardFeedItem) => isCard(cardFeedItem));
-            if (!representativeCard) {
-                return;
-            }
-            const {domainName, bank} = representativeCard;
-            const isBankRepeating = repeatingBanks.includes(bank);
-            const cardFeedBankName = bank === CONST.EXPENSIFY_CARD.BANK ? translate('search.filters.card.expensify') : CardUtils.getCardFeedName(bank as CompanyCardFeed);
-            const policyID = domainName.match(CONST.REGEX.EXPENSIFY_POLICY_DOMAIN_NAME)?.[1] ?? '';
-            const correspondingPolicy = PolicyUtils.getPolicy(policyID?.toUpperCase());
-            const text = translate('search.filters.card.cardFeedName', {cardFeedBankName, cardFeedLabel: isBankRepeating ? correspondingPolicy?.name : undefined});
-            const correspondingCards = Object.keys(cardFeed ?? {}).filter((cardFeedKey) => cardFeedKey !== 'cardList');
-
-            cardFeedsData.push(buildCardFeedItem(text, policyID, correspondingCards, newSelectedCards, bank as CompanyCardFeed, styles.cardIcon));
-        });
-
-        Object.values(domainFeedsData).forEach((domainFeed) => {
+        const domainFeeds = Object.values(domainFeedsData).map((domainFeed) => {
             const {domainName, bank, correspospondingCardIDs} = domainFeed;
             const isBankRepeating = repeatingBanks.includes(bank);
             const cardFeedBankName = bank === CONST.EXPENSIFY_CARD.BANK ? translate('search.filters.card.expensify') : CardUtils.getCardFeedName(bank as CompanyCardFeed);
             const text = translate('search.filters.card.cardFeedName', {cardFeedBankName, cardFeedLabel: isBankRepeating ? domainName : undefined});
 
-            cardFeedsData.push(buildCardFeedItem(text, domainName, correspospondingCardIDs, newSelectedCards, bank as CompanyCardFeed, styles.cardIcon));
+            return buildCardFeedItem(text, domainName, correspospondingCardIDs, selectedCards, bank as CompanyCardFeed, styles.cardIcon);
         });
-        return cardFeedsData;
-    }, [domainFeedsData, filteredWorkspaceCardFeeds, newSelectedCards, styles.cardIcon, translate]);
 
-    const shouldShowSearchInput = cardFeedsSectionData.length + invidualCardsSectionData.length > CONST.COMPANY_CARDS.CARD_LIST_THRESHOLD;
+        const workspaceFeeds = filteredWorkspaceCardFeeds
+            .map(([, cardFeed]) => {
+                const representativeCard = Object.values(cardFeed ?? {}).find((cardFeedItem) => isCard(cardFeedItem));
+                if (!representativeCard) {
+                    return;
+                }
+                const {domainName, bank} = representativeCard;
+                const isBankRepeating = repeatingBanks.includes(bank);
+                const cardFeedBankName = bank === CONST.EXPENSIFY_CARD.BANK ? translate('search.filters.card.expensify') : CardUtils.getCardFeedName(bank as CompanyCardFeed);
+                const policyID = domainName.match(CONST.REGEX.EXPENSIFY_POLICY_DOMAIN_NAME)?.[1] ?? '';
+                const correspondingPolicy = PolicyUtils.getPolicy(policyID?.toUpperCase());
+                const text = translate('search.filters.card.cardFeedName', {cardFeedBankName, cardFeedLabel: isBankRepeating ? correspondingPolicy?.name : undefined});
+                const correspondingCards = Object.keys(cardFeed ?? {}).filter((cardFeedKey) => cardFeedKey !== 'cardList');
+
+                return buildCardFeedItem(text, policyID, correspondingCards, selectedCards, bank as CompanyCardFeed, styles.cardIcon);
+            })
+            .filter((feed) => feed) as CardFilterItem[];
+
+        return [...domainFeeds, ...workspaceFeeds];
+    }, [domainFeedsData, filteredWorkspaceCardFeeds, selectedCards, styles.cardIcon, translate]);
+
+    const shouldShowSearchInput = cardFeedsSectionData.length + individualCardsSectionData.length > CONST.COMPANY_CARDS.CARD_LIST_THRESHOLD;
 
     const sections = useMemo(() => {
         const newSections = [];
 
         newSections.push({
             title: translate('search.filters.card.cardFeeds'),
-            data: cardFeedsSectionData.filter((item) => item.text?.toLocaleLowerCase().includes(debouncedSearchTerm.toLocaleLowerCase())),
+            data: cardFeedsSectionData.filter((item) => item && item.text?.toLocaleLowerCase().includes(debouncedSearchTerm.toLocaleLowerCase())),
             shouldShow: cardFeedsSectionData.length > 0,
         });
         newSections.push({
             title: translate('search.filters.card.individualCards'),
-            data: invidualCardsSectionData.filter((item) => item.text?.toLocaleLowerCase().includes(debouncedSearchTerm.toLocaleLowerCase())),
-            shouldShow: invidualCardsSectionData.length > 0,
+            data: individualCardsSectionData.filter((item) => item.text?.toLocaleLowerCase().includes(debouncedSearchTerm.toLocaleLowerCase())),
+            shouldShow: individualCardsSectionData.length > 0,
         });
         return newSections;
-    }, [translate, cardFeedsSectionData, invidualCardsSectionData, debouncedSearchTerm]);
+    }, [translate, cardFeedsSectionData, individualCardsSectionData, debouncedSearchTerm]);
 
     const handleConfirmSelection = useCallback(() => {
         SearchActions.updateAdvancedFilters({
-            cardID: newSelectedCards,
+            cardID: selectedCards,
         });
 
         Navigation.goBack(ROUTES.SEARCH_ADVANCED_FILTERS);
-    }, [newSelectedCards]);
+    }, [selectedCards]);
 
     const updateNewCards = useCallback(
         (item: CardFilterItem) => {
@@ -219,14 +216,14 @@ function SearchFiltersCardPage() {
             const isCardFeed = item?.isCardFeed && item?.correspondingCards;
 
             if (item.isSelected) {
-                const newCardsObject = newSelectedCards.filter((card) => (isCardFeed ? !item.correspondingCards?.includes(card) : card !== item.keyForList));
-                setNewSelectedCards(newCardsObject);
+                const newCardsObject = selectedCards.filter((card) => (isCardFeed ? !item.correspondingCards?.includes(card) : card !== item.keyForList));
+                setSelectedCards(newCardsObject);
             } else {
-                const newCardsObject = isCardFeed ? [...newSelectedCards, ...(item?.correspondingCards ?? [])] : [...newSelectedCards, item.keyForList];
-                setNewSelectedCards(newCardsObject);
+                const newCardsObject = isCardFeed ? [...selectedCards, ...(item?.correspondingCards ?? [])] : [...selectedCards, item.keyForList];
+                setSelectedCards(newCardsObject);
             }
         },
-        [newSelectedCards],
+        [selectedCards],
     );
 
     const footerContent = useMemo(
