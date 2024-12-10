04dec5f09a35417a403cdeb858532303a8bf8a7c
Huu Le <20178761+huult@users.noreply.github.com>
2024-12-11 00:37:29 +0700

    Add comment to explain shouldCallDirectly

--- src/components/AttachmentModal.tsx
+++ src/components/AttachmentModal.tsx
@@ -379,7 +379,11 @@ function AttachmentModal({
     );
 
     /**
-     * close the modal
+     * Closes the modal.
+     * @param {boolean} [shouldCallDirectly] If true, directly calls `onModalClose`.
+     * This is useful when you plan to continue navigating to another page after closing the modal, to avoid freezing the app due to navigating to another page first and dismissing the modal later.
+     * If `shouldCallDirectly` is false or undefined, it calls `attachmentModalHandler.handleModalClose` to close the modal.
+     * This ensures smooth modal closing behavior without causing delays in closing.
      */
     const closeModal = useCallback(
         (shouldCallDirectly?: boolean) => {
