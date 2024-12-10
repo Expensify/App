2692235ba89faa36d0c21f99e1d1b40816169761
szymonrybczak <szymon.rybczak@gmail.com>
2024-12-10 11:24:48 +0100

    feat!: use new EventEmitter API

--- modules/background-task/src/NativeReactNativeBackgroundTask.ts
+++ modules/background-task/src/NativeReactNativeBackgroundTask.ts
@@ -1,8 +1,10 @@
 import type {TurboModule} from 'react-native';
 import {TurboModuleRegistry} from 'react-native';
+import type {EventEmitter} from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface Spec extends TurboModule {
     defineTask(taskName: string, taskExecutor: (data: unknown) => void | Promise<void>): Promise<void>;
+    readonly onBackgroundTaskExecution: EventEmitter<string>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('ReactNativeBackgroundTask');
