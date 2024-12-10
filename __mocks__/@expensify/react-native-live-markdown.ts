02d1d08d927519c1a8b8019c0c08c7b3393932fb
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2024-12-10 03:32:41 +0100

    removed duplicated line

--- __mocks__/@expensify/react-native-live-markdown.ts
+++ __mocks__/@expensify/react-native-live-markdown.ts
@@ -1,7 +1,6 @@
 import {MarkdownTextInput} from '@expensify/react-native-live-markdown';
 import type {parseExpensiMark} from '@expensify/react-native-live-markdown';
 
-global.jsi_registerMarkdownWorklet = jest.fn();
 global.jsi_setMarkdownRuntime = jest.fn();
 global.jsi_registerMarkdownWorklet = jest.fn();
 global.jsi_unregisterMarkdownWorklet = jest.fn();
