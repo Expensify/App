8d89ff5891450b008e1a6cbbff1329b23223de7e
szymonrybczak <szymon.rybczak@gmail.com>
2024-12-10 22:26:11 +0100

    fix: share code between iOS and Android

--- src/setup/backgroundTask/index.ts
+++
@@ -1,9 +0,0 @@
-// FIXME: .android.ts doesn't seem to work, .native.ts neither.
-import TaskManager from '@expensify/react-native-background-task';
-import * as SequentialQueue from '@libs/Network/SequentialQueue';
-
-const BACKGROUND_FETCH_TASK = 'FLUSH-SEQUENTIAL-QUEUE-BACKGROUND-FETCH';
-
-TaskManager.defineTask(BACKGROUND_FETCH_TASK, () => {
-    SequentialQueue.flush();
-});
