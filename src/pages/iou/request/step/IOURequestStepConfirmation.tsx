4465d4e6edb9f0f9c0d0789228f53834c57e205e
Cristi Paval <cristi@expensify.com>
2024-12-10 23:24:07 +0200

    Allow adding receipt to invoices

--- src/pages/iou/request/step/IOURequestStepConfirmation.tsx
+++ src/pages/iou/request/step/IOURequestStepConfirmation.tsx
@@ -610,7 +610,7 @@ function IOURequestStepConfirmation({
                     title={headerTitle}
                     onBackButtonPress={navigateBack}
                     shouldShowThreeDotsButton={
-                        requestType === CONST.IOU.REQUEST_TYPE.MANUAL && (iouType === CONST.IOU.TYPE.SUBMIT || iouType === CONST.IOU.TYPE.TRACK) && !isMovingTransactionFromTrackExpense
+                        requestType === CONST.IOU.REQUEST_TYPE.MANUAL && (iouType === CONST.IOU.TYPE.SUBMIT || iouType === CONST.IOU.TYPE.TRACK || iouType === CONST.IOU.TYPE.INVOICE) && !isMovingTransactionFromTrackExpense
                     }
                     threeDotsAnchorPosition={styles.threeDotsPopoverOffsetNoCloseButton(windowWidth)}
                     threeDotsMenuItems={[
