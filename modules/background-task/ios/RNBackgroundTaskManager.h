99bf5576edcc24b57c99a6b18cc1f7543c1a4da4
szymonrybczak <szymon.rybczak@gmail.com>
2024-12-10 12:16:12 +0100

    fix: make 0 warnings

--- modules/background-task/ios/RNBackgroundTaskManager.h
+++ modules/background-task/ios/RNBackgroundTaskManager.h
@@ -4,12 +4,12 @@
 
 @interface RNBackgroundTaskManager : NSObject
 
-@property (nonatomic, copy) void (^taskHandler)(BGTask * _Nonnull);
+@property (nonatomic, copy) void (^ _Nullable taskHandler)(BGTask * _Nonnull);
 
-+ (instancetype)shared;
++ (instancetype _Nullable )shared;
 + (void)setup;
-- (void)setHandlerForIdentifier:(NSString *)identifier
-                     completion:(void (^)(BGTask * _Nonnull))handler;
-- (void (^)(BGTask * _Nonnull))handlerForIdentifier:(NSString *)identifier;
+- (void)setHandlerForIdentifier:(NSString *_Nullable)identifier
+                     completion:(void (^_Nullable)(BGTask * _Nonnull))handler;
+- (void (^_Nullable)(BGTask * _Nonnull))handlerForIdentifier:(NSString *_Nullable)identifier;
 
 @end
