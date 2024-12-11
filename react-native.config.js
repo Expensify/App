4f01ab0dafc3c636b265807b1324d3bb84216e86
Jules <julianrosser91@gmail.com>
2024-12-11 17:29:55 +0000

    Revert "Contacts import"

--- react-native.config.js
+++ react-native.config.js
@@ -1,15 +1,7 @@
-const path = require('path');
-const pak = require('./modules/ContactsNitroModule/package.json');
-
 module.exports = {
     project: {
         ios: {sourceDir: process.env.PROJECT_ROOT_PATH + 'ios'},
         android: {sourceDir: process.env.PROJECT_ROOT_PATH + 'android'},
     },
     assets: ['./assets/fonts/native'],
-    dependencies: {
-        [pak.name]: {
-            root: path.join(__dirname, 'modules', 'ContactsNitroModule'),
-        },
-    },
 };
