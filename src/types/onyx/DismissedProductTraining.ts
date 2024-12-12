a8ab5c645fbab74ce63fbceea3474ec7c7bfcf09
Ishpaul Singh <ishpaul207@gmail.com>
2024-12-12 16:36:35 +0530

    changes as per review

--- src/types/onyx/DismissedProductTraining.ts
+++ src/types/onyx/DismissedProductTraining.ts
@@ -1,3 +1,6 @@
+import CONST from '@src/CONST';
+
+const {CONCEIRGE_LHN_GBR, RENAME_SAVED_SEARCH, WORKSAPCE_CHAT_CREATE, QUICK_ACTION_BUTTON} = CONST.PRODUCT_TRAINING_TOOLTIP_NAMES;
 /**
  * This type is used to store the timestamp of when the user dismisses a product training ui elements.
  */
@@ -5,27 +8,27 @@ type DismissedProductTraining = {
     /**
      * When user dismisses the nudgeMigration Welcome Modal, we store the timestamp here.
      */
-    migratedUserWelcomeModal: Date;
+    [CONST.MIGRATED_USER_WELCOME_MODAL]: Date;
 
     /**
      * When user dismisses the conciergeLHNGBR product training tooltip, we store the timestamp here.
      */
-    conciergeLHNGBR: Date;
+    [CONCEIRGE_LHN_GBR]: Date;
 
     /**
      * When user dismisses the renameSavedSearch product training tooltip, we store the timestamp here.
      */
-    renameSavedSearch: Date;
+    [RENAME_SAVED_SEARCH]: Date;
 
     /**
      * When user dismisses the workspaceChatCreate product training tooltip, we store the timestamp here.
      */
-    workspaceChatCreate: Date;
+    [WORKSAPCE_CHAT_CREATE]: Date;
 
     /**
      * When user dismisses the quickActionButton product training tooltip, we store the timestamp here.
      */
-    quickActionButton: Date;
+    [QUICK_ACTION_BUTTON]: Date;
 };
 
 export default DismissedProductTraining;
