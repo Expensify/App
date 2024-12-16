5514599bd6dda118a6ef6a990e77470e253e84b9
nkdengineer <nkdengineer@outlook.com>
2024-12-16 14:13:35 +0700

    revert hardcode

--- src/libs/SubscriptionUtils.ts
+++ src/libs/SubscriptionUtils.ts
@@ -442,7 +442,6 @@ function doesUserHavePaymentCardAdded(): boolean {
  * Whether the user's billable actions should be restricted.
  */
 function shouldRestrictUserBillableActions(policyID: string): boolean {
-    return false;
     const currentDate = new Date();
 
     const policy = allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${policyID}`];
