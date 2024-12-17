6ad917088616335b3683381adaaee557bda2ee4a
kubabutkiewicz <jakub.butkiewicz94@gmail.com>
2024-12-17 12:31:19 +0100

    run prettier

--- .eslintrc.changed.js
+++ .eslintrc.changed.js
@@ -9,14 +9,14 @@ module.exports = {
         'no-restricted-syntax': [
             'error',
             {
-                selector: "ImportNamespaceSpecifier[parent.source.value=/^@libs/]",
-                message: 'Namespace imports from @libs are not allowed. Use named imports instead. Example: import { method } from "@libs/module"'
+                selector: 'ImportNamespaceSpecifier[parent.source.value=/^@libs/]',
+                message: 'Namespace imports from @libs are not allowed. Use named imports instead. Example: import { method } from "@libs/module"',
             },
             {
-                selector: "ImportNamespaceSpecifier[parent.source.value=/^@userActions/]",
-                message: 'Namespace imports from @userActions are not allowed. Use named imports instead. Example: import { action } from "@userActions/module"'
-            }
-        ]
+                selector: 'ImportNamespaceSpecifier[parent.source.value=/^@userActions/]',
+                message: 'Namespace imports from @userActions are not allowed. Use named imports instead. Example: import { action } from "@userActions/module"',
+            },
+        ],
     },
     overrides: [
         {
@@ -25,16 +25,15 @@ module.exports = {
                 'no-restricted-syntax': [
                     'error',
                     {
-                        selector: "ImportNamespaceSpecifier[parent.source.value=/^\\.\\./]",
-                        message: 'Namespace imports are not allowed. Use named imports instead. Example: import { method } from "../libs/module"'
+                        selector: 'ImportNamespaceSpecifier[parent.source.value=/^\\.\\./]',
+                        message: 'Namespace imports are not allowed. Use named imports instead. Example: import { method } from "../libs/module"',
                     },
                     {
-                        selector: "ImportNamespaceSpecifier[parent.source.value=/^\\./]", 
-                        message: 'Namespace imports are not allowed. Use named imports instead. Example: import { method } from "./libs/module"'
+                        selector: 'ImportNamespaceSpecifier[parent.source.value=/^\\./]',
+                        message: 'Namespace imports are not allowed. Use named imports instead. Example: import { method } from "./libs/module"',
                     },
-
-                ]
-            }
-        }
-    ]
+                ],
+            },
+        },
+    ],
 };
