ddc354ffd2731675eaf0c6f18a1c97576196a930
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-12-17 12:51:00 +0100

    add to large files

--- .eslintrc.changed.js
+++ .eslintrc.changed.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     overrides: [
         {
-            files: ['src/libs/ReportUtils.ts', 'src/libs/actions/IOU.ts', 'src/libs/actions/Report.ts', 'src/libs/actions/Task.ts'],
+            files: ['src/libs/ReportUtils.ts', 'src/libs/actions/IOU.ts', 'src/libs/actions/Report.ts', 'src/libs/actions/Task.ts', 'src/libs/OptionsListUtils.ts'],
             rules: {
                 'rulesdir/no-default-id-values': 'off',
             },
