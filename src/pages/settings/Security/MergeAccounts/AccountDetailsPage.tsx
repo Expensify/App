ec445e1889e078530d7ebd32bbe0dbce71e5497e
Sibtain Ali <allroundexperts@gmail.com>
2024-12-19 03:41:30 +0500

    more files

--- src/pages/settings/Security/MergeAccounts/AccountDetailsPage.tsx
+++ src/pages/settings/Security/MergeAccounts/AccountDetailsPage.tsx
@@ -1,75 +1,63 @@
 import {Str} from 'expensify-common';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
-import ConfirmModal from '@components/ConfirmModal';
+import type {ValueOf} from 'type-fest';
+import CheckboxWithLabel from '@components/CheckboxWithLabel';
 import DelegateNoAccessWrapper from '@components/DelegateNoAccessWrapper';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
-import type {FormInputErrors, FormOnyxValues} from '@components/Form/types';
+import type {FormOnyxValues} from '@components/Form/types';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import Text from '@components/Text';
 import TextInput from '@components/TextInput';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
+import * as LoginUtils from '@libs/LoginUtils';
 import Navigation from '@libs/Navigation/Navigation';
+import {parsePhoneNumber} from '@libs/PhoneNumber';
 import * as ValidationUtils from '@libs/ValidationUtils';
-import variables from '@styles/variables';
-import * as CloseAccount from '@userActions/CloseAccount';
-import * as User from '@userActions/User';
+import * as MergeAccounts from '@userActions/MergeAccounts';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import INPUT_IDS from '@src/types/form/MergeAccountDetailsForm';
-import CheckboxWithLabel from '@components/CheckboxWithLabel';
 
 function AccountDetailsPage() {
-    const [session] = useOnyx(ONYXKEYS.SESSION);
+    const [userEmailOrPhone] = useOnyx(ONYXKEYS.SESSION, {selector: (session) => session?.email});
+    const [mergeAccountData] = useOnyx(ONYXKEYS.ACCOUNT, {selector: (account) => account?.mergeAccount});
 
     const styles = useThemeStyles();
-    const {translate, formatPhoneNumber} = useLocalize();
+    const {translate} = useLocalize();
 
-    const [isConfirmModalVisible, setConfirmModalVisibility] = useState(false);
-    const [reasonForLeaving, setReasonForLeaving] = useState('');
+    const validate = (values: FormOnyxValues<typeof ONYXKEYS.FORMS.MERGE_ACCOUNT_DETAILS_FORM>) => {
+        const errors: Record<ValueOf<typeof INPUT_IDS>, string | undefined> = {
+            [INPUT_IDS.PHONE_OR_EMAIL]: undefined,
+            [INPUT_IDS.CONSENT]: undefined,
+        };
 
-    // If you are new to hooks this might look weird but basically it is something that only runs when the component unmounts
-    // nothing runs on mount and we pass empty dependencies to prevent this from running on every re-render.
-    // TODO: We should refactor this so that the data in instead passed directly as a prop instead of "side loading" the data
-    // here, we left this as is during refactor to limit the breaking changes.
-    useEffect(() => () => CloseAccount.clearError(), []);
+        const login = values[INPUT_IDS.PHONE_OR_EMAIL];
 
-    const hideConfirmModal = () => {
-        setConfirmModalVisibility(false);
-    };
+        if (!login) {
+            errors[INPUT_IDS.PHONE_OR_EMAIL] = translate('common.pleaseEnterEmailOrPhoneNumber');
+        } else {
+            const phoneLogin = LoginUtils.appendCountryCode(LoginUtils.getPhoneNumberWithoutSpecialChars(login));
+            const parsedPhoneNumber = parsePhoneNumber(phoneLogin);
 
-    const onConfirm = () => {
-        User.closeAccount(reasonForLeaving);
-        hideConfirmModal();
-    };
+            if (!Str.isValidEmail(login) && !parsedPhoneNumber.possible) {
+                if (ValidationUtils.isNumericWithSpecialChars(login)) {
+                    errors[INPUT_IDS.PHONE_OR_EMAIL] = translate('common.error.phoneNumber');
+                } else {
+                    errors[INPUT_IDS.PHONE_OR_EMAIL] = translate('loginForm.error.invalidFormatEmailLogin');
+                }
+            }
+        }
 
-    const showConfirmModal = (values: FormOnyxValues<typeof ONYXKEYS.FORMS.CLOSE_ACCOUNT_FORM>) => {
-        setConfirmModalVisibility(true);
-        setReasonForLeaving(values.reasonForLeaving);
-    };
-
-    /**
-     * Removes spaces and transform the input string to lowercase.
-     * @param phoneOrEmail - The input string to be sanitized.
-     * @returns The sanitized string
-     */
-    const sanitizePhoneOrEmail = (phoneOrEmail: string): string => phoneOrEmail.replace(/\s+/g, '').toLowerCase();
-
-    const validate = (values: FormOnyxValues<typeof ONYXKEYS.FORMS.CLOSE_ACCOUNT_FORM>): FormInputErrors<typeof ONYXKEYS.FORMS.CLOSE_ACCOUNT_FORM> => {
-        const userEmailOrPhone = session?.email ? formatPhoneNumber(session.email) : null;
-        const errors = ValidationUtils.getFieldRequiredErrors(values, ['phoneOrEmail']);
-
-        if (values.phoneOrEmail && userEmailOrPhone && sanitizePhoneOrEmail(userEmailOrPhone) !== sanitizePhoneOrEmail(values.phoneOrEmail)) {
-            errors.phoneOrEmail = translate('closeAccountPage.enterYourDefaultContactMethod');
+        if (values[INPUT_IDS.CONSENT] === undefined) {
+            errors[INPUT_IDS.CONSENT] = translate('common.error.fieldRequired');
         }
         return errors;
     };
-
-    const userEmailOrPhone = session?.email ? formatPhoneNumber(session.email) : null;
 
     return (
         <ScreenWrapper
@@ -83,10 +71,13 @@ function AccountDetailsPage() {
                 />
                 <FormProvider
                     formID={ONYXKEYS.FORMS.MERGE_ACCOUNT_DETAILS_FORM}
-                    validate={validate}
-                    onSubmit={showConfirmModal}
+                    onSubmit={(values) => MergeAccounts.requestValidationCodeForAccountMerge(values[INPUT_IDS.PHONE_OR_EMAIL])}
                     submitButtonText={translate('common.next')}
                     style={[styles.flexGrow1, styles.mh5]}
+                    shouldTrimValues
+                    validate={validate}
+                    isSubmitDisabled={!!mergeAccountData?.getValidateCodeForAccountMerge?.isLoading}
+                    enabledWhenOffline={false}
                 >
                     <View style={[styles.flexGrow1]}>
                         <Text style={[styles.mt5]}>{translate('mergeAccountsPage.accountDetails.accountToMergeInto', userEmailOrPhone ?? '')}</Text>
@@ -94,8 +85,8 @@ function AccountDetailsPage() {
                             InputComponent={TextInput}
                             inputID={INPUT_IDS.PHONE_OR_EMAIL}
                             autoCapitalize="none"
-                            label={translate('closeAccountPage.enterDefaultContact')}
-                            aria-label={translate('closeAccountPage.enterDefaultContact')}
+                            label={translate('loginForm.phoneOrEmail')}
+                            aria-label={translate('loginForm.phoneOrEmail')}
                             role={CONST.ROLE.PRESENTATION}
                             containerStyles={[styles.mt5]}
                             autoCorrect={false}
@@ -103,8 +94,8 @@ function AccountDetailsPage() {
                         <InputWrapper
                             InputComponent={CheckboxWithLabel}
                             inputID={INPUT_IDS.CONSENT}
-                            label={translate('closeAccountPage.enterDefaultContact')}
-                            aria-label={translate('closeAccountPage.enterDefaultContact')}
+                            label={translate('mergeAccountsPage.accountDetails.notReversibleConsent')}
+                            aria-label={translate('mergeAccountsPage.accountDetails.notReversibleConsent')}
                         />
                     </View>
                 </FormProvider>
