0e1f3b186ad287754d30c0f5dff6ffc526a63c2c
VickyStash <vikstash@gmail.com>
2024-12-18 12:25:48 +0100

    Add ReportScreen to ignored files cause it updates caused tests failing

--- .eslintrc.changed.js
+++ .eslintrc.changed.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     overrides: [
         {
-            files: ['src/libs/ReportUtils.ts', 'src/libs/actions/IOU.ts', 'src/libs/actions/Report.ts', 'src/libs/actions/Task.ts', 'src/libs/OptionsListUtils.ts', 'src/libs/ReportActionsUtils.ts', 'src/libs/TransactionUtils/index.ts'],
+            files: ['src/libs/ReportUtils.ts', 'src/libs/actions/IOU.ts', 'src/libs/actions/Report.ts', 'src/libs/actions/Task.ts', 'src/libs/OptionsListUtils.ts', 'src/libs/ReportActionsUtils.ts', 'src/libs/TransactionUtils/index.ts', 'src/pages/home/ReportScreen.tsx'],
             rules: {
                 'rulesdir/no-default-id-values': 'off',
             },
