a115ffe79b29d2d430bd42e06ae17f43d31bf805
Antony M. Kithinzi <kithinzi97@gmail.com>
2024-12-19 04:47:39 +0300

    refactoring useSubscriptionPrice.ts

--- src/hooks/useSubscriptionPrice.ts
+++ src/hooks/useSubscriptionPrice.ts
@@ -4,59 +4,18 @@ import ONYXKEYS from '@src/ONYXKEYS';
 import usePreferredCurrency from './usePreferredCurrency';
 import useSubscriptionPlan from './useSubscriptionPlan';
 
-const SUBSCRIPTION_PRICES = {
-    [CONST.PAYMENT_CARD_CURRENCY.USD]: {
-        [CONST.POLICY.TYPE.CORPORATE]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 900,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1800,
-        },
-        [CONST.POLICY.TYPE.TEAM]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 500,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1000,
-        },
-    },
-    [CONST.PAYMENT_CARD_CURRENCY.AUD]: {
-        [CONST.POLICY.TYPE.CORPORATE]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 1500,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 3000,
-        },
-        [CONST.POLICY.TYPE.TEAM]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 700,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1400,
-        },
-    },
-    [CONST.PAYMENT_CARD_CURRENCY.GBP]: {
-        [CONST.POLICY.TYPE.CORPORATE]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 700,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1400,
-        },
-        [CONST.POLICY.TYPE.TEAM]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 400,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 800,
-        },
-    },
-    [CONST.PAYMENT_CARD_CURRENCY.NZD]: {
-        [CONST.POLICY.TYPE.CORPORATE]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 1600,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 3200,
-        },
-        [CONST.POLICY.TYPE.TEAM]: {
-            [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 800,
-            [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1600,
-        },
-    },
-} as const;
-
-function useSubscriptionPrice(): number {
+function useSubscriptionPrice(upgradePlan?: 'corporate' | 'team' | null): number {
     const preferredCurrency = usePreferredCurrency();
-    const subscriptionPlan = useSubscriptionPlan();
+    const currentSubscriptionPlan = useSubscriptionPlan();
     const [privateSubscription] = useOnyx(ONYXKEYS.NVP_PRIVATE_SUBSCRIPTION);
 
-    if (!subscriptionPlan || !privateSubscription?.type) {
+    if ((!currentSubscriptionPlan && !upgradePlan) || (currentSubscriptionPlan && !upgradePlan && !privateSubscription?.type)) {
         return 0;
     }
 
-    return SUBSCRIPTION_PRICES[preferredCurrency][subscriptionPlan][privateSubscription.type];
+    return CONST.SUBSCRIPTION.PRICES[preferredCurrency][
+        currentSubscriptionPlan === CONST.POLICY.TYPE.CORPORATE || upgradePlan === CONST.POLICY.TYPE.CORPORATE ? CONST.POLICY.TYPE.CORPORATE : CONST.POLICY.TYPE.TEAM
+    ][privateSubscription?.type ?? CONST.SUBSCRIPTION.TYPE.ANNUAL];
 }
 
 export default useSubscriptionPrice;
