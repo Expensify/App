51d701dac9a15bfb912a2c5e65cbb5b2a77ef0e9
Antony M. Kithinzi <kithinzi97@gmail.com>
2024-12-19 04:49:03 +0300

    moving SUBSCRIPTION_PRICS to CONST.ts

--- src/CONST.ts
+++ src/CONST.ts
@@ -2972,6 +2972,50 @@ const CONST = {
             ANNUAL: 'yearly2018',
             PAYPERUSE: 'monthly2018',
         },
+        get PRICES() {
+            return {
+                [CONST.PAYMENT_CARD_CURRENCY.USD]: {
+                    [CONST.POLICY.TYPE.CORPORATE]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 900,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1800,
+                    },
+                    [CONST.POLICY.TYPE.TEAM]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 500,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1000,
+                    },
+                },
+                [CONST.PAYMENT_CARD_CURRENCY.AUD]: {
+                    [CONST.POLICY.TYPE.CORPORATE]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 1500,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 3000,
+                    },
+                    [CONST.POLICY.TYPE.TEAM]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 700,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1400,
+                    },
+                },
+                [CONST.PAYMENT_CARD_CURRENCY.GBP]: {
+                    [CONST.POLICY.TYPE.CORPORATE]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 700,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1400,
+                    },
+                    [CONST.POLICY.TYPE.TEAM]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 400,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 800,
+                    },
+                },
+                [CONST.PAYMENT_CARD_CURRENCY.NZD]: {
+                    [CONST.POLICY.TYPE.CORPORATE]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 1600,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 3200,
+                    },
+                    [CONST.POLICY.TYPE.TEAM]: {
+                        [CONST.SUBSCRIPTION.TYPE.ANNUAL]: 800,
+                        [CONST.SUBSCRIPTION.TYPE.PAYPERUSE]: 1600,
+                    },
+                },
+            };
+        },
     },
     REGEX: {
         SPECIAL_CHARS_WITHOUT_NEWLINE: /((?!\n)[()-\s\t])/g,
