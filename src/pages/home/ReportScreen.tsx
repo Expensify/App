8c7ba127558f4ccdaa69f31b9da5a945397eda68
Artem Makushov <waterim3009@gmail.com>
2024-12-19 02:52:46 +0100

    update focusing for reportScreen

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -760,6 +760,10 @@ function ReportScreen({route, currentReportID = '', navigation}: ReportScreenPro
         return null;
     }
 
+    if (!isFocused) {
+        return null;
+    }
+
     return (
         <ActionListContext.Provider value={actionListValue}>
             <ReactionListContext.Provider value={reactionListRef}>
