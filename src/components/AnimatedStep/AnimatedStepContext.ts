bb4b1865f8e9dd6e02b165868a917d5712bebfdf
Blazej Kustra <blazej.kustra@swmansion.com>
2024-12-20 11:55:18 +0100

    refactor AnimatedStepProvider: enhance step management and animation handling

--- src/components/AnimatedStep/AnimatedStepContext.ts
+++ src/components/AnimatedStep/AnimatedStepContext.ts
@@ -1,13 +1,17 @@
-import type React from 'react';
 import {createContext} from 'react';
+import type {StyleProp, ViewStyle} from 'react-native';
 import type {ValueOf} from 'type-fest';
 import type CONST from '@src/CONST';
 
 type AnimationDirection = ValueOf<typeof CONST.ANIMATION_DIRECTION>;
 
 type StepContext = {
-    animationDirection: AnimationDirection;
-    setAnimationDirection: React.Dispatch<React.SetStateAction<AnimationDirection>>;
+    currentStep: string;
+    previousStep: string | null;
+    setStep: (newStep: string, direction: AnimationDirection) => void;
+    renderStep: (stepName: string) => React.ReactNode;
+    currentScreenAnimatedStyle: StyleProp<ViewStyle>;
+    previousScreenAnimatedStyle: StyleProp<ViewStyle>;
 };
 
 const AnimatedStepContext = createContext<StepContext | null>(null);
--- src/components/AnimatedStep/AnimatedStepProvider.tsx
+++ src/components/AnimatedStep/AnimatedStepProvider.tsx
@@ -1,12 +1,64 @@
-import React, {useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
+import {Easing, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
+import variables from '@styles/variables';
 import CONST from '@src/CONST';
 import type ChildrenProps from '@src/types/utils/ChildrenProps';
 import type {AnimationDirection} from './AnimatedStepContext';
 import AnimatedStepContext from './AnimatedStepContext';
 
-function AnimatedStepProvider({children}: ChildrenProps): React.ReactNode {
+const ANIMATED_SCREEN_TRANSITION = 400;
+
+type AnimatedStepProviderProps = ChildrenProps & {
+    initialStep: string;
+    renderStep: (name: string) => React.ReactNode;
+};
+
+function AnimatedStepProvider({children, renderStep, initialStep}: AnimatedStepProviderProps): React.ReactNode {
     const [animationDirection, setAnimationDirection] = useState<AnimationDirection>(CONST.ANIMATION_DIRECTION.IN);
-    const contextValue = useMemo(() => ({animationDirection, setAnimationDirection}), [animationDirection, setAnimationDirection]);
+    const [currentStep, setCurrentStep] = useState<string>(initialStep);
+    const [previousStep, setPreviousStep] = useState<string | null>(null);
+
+    const currentTranslateX = useSharedValue(0);
+    const prevTranslateX = useSharedValue(0);
+
+    const setStep = useCallback(
+        (newStep: string, direction: AnimationDirection) => {
+            setAnimationDirection(direction);
+            setPreviousStep(currentStep);
+            setCurrentStep(newStep);
+
+            const currentStepPosition = direction === 'in' ? variables.sideBarWidth : -CONST.ANIMATED_TRANSITION_FROM_VALUE;
+            const previousStepPosition = direction === 'in' ? -CONST.ANIMATED_TRANSITION_FROM_VALUE : variables.sideBarWidth;
+
+            currentTranslateX.set(currentStepPosition);
+            currentTranslateX.set(withTiming(0, {duration: ANIMATED_SCREEN_TRANSITION, easing: Easing.inOut(Easing.cubic)}, () => setPreviousStep(null)));
+
+            prevTranslateX.set(0);
+            prevTranslateX.set(withTiming(previousStepPosition, {duration: ANIMATED_SCREEN_TRANSITION, easing: Easing.inOut(Easing.cubic)}));
+        },
+        [currentStep, currentTranslateX, prevTranslateX],
+    );
+
+    const currentScreenAnimatedStyle = useAnimatedStyle(() => ({
+        transform: [{translateX: currentTranslateX.get()}],
+    }));
+
+    const previousScreenAnimatedStyle = useAnimatedStyle(() => ({
+        transform: [{translateX: prevTranslateX.get()}],
+        zIndex: animationDirection === 'in' ? -1 : 1,
+    }));
+
+    const contextValue = useMemo(
+        () => ({
+            currentStep,
+            previousStep,
+            setStep,
+            currentScreenAnimatedStyle,
+            previousScreenAnimatedStyle,
+            renderStep,
+        }),
+        [currentStep, previousStep, setStep, currentScreenAnimatedStyle, previousScreenAnimatedStyle, renderStep],
+    );
 
     return <AnimatedStepContext.Provider value={contextValue}>{children}</AnimatedStepContext.Provider>;
 }
