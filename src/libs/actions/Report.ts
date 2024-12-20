4132cab45aba46c1f9f8fbf647f4ef900777bb99
Filip Solecki <filip.solecki@swmansion.com>
2024-12-20 12:12:40 +0100

    Fix TS in Report file

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -92,6 +92,7 @@ import CONFIG from '@src/CONFIG';
 import type {OnboardingAccounting, OnboardingCompanySize} from '@src/CONST';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {Route} from '@src/ROUTES';
 import ROUTES from '@src/ROUTES';
 import INPUT_IDS from '@src/types/form/NewRoomForm';
 import type {
@@ -2884,7 +2885,7 @@ function openReportFromDeepLink(url: string) {
                                 return;
                             }
 
-                            Navigation.navigate(route, CONST.NAVIGATION.ACTION_TYPE.PUSH);
+                            Navigation.navigate(route as Route, CONST.NAVIGATION.ACTION_TYPE.PUSH);
                         };
 
                         // We need skip deeplinking if the user hasn't completed the guided setup flow.
