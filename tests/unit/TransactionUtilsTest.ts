99ef4c1e7afe7082cafce076d54a55d1479e1ad4
FitseTLT <engfitsum@gmail.com>
2024-12-23 19:10:53 +0300

    Added tests

--- tests/unit/TransactionUtilsTest.ts
+++ tests/unit/TransactionUtilsTest.ts
@@ -1,6 +1,11 @@
+import Onyx from 'react-native-onyx';
+import * as ReportUtils from '@libs/ReportUtils';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
 import type {Attendee} from '@src/types/onyx/IOU';
 import * as TransactionUtils from '../../src/libs/TransactionUtils';
 import type {Transaction} from '../../src/types/onyx';
+import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
 
 function generateTransaction(values: Partial<Transaction> = {}): Transaction {
     const reportID = '1';
@@ -13,6 +18,8 @@ function generateTransaction(values: Partial<Transaction> = {}): Transaction {
 
     return {...baseValues, ...values};
 }
+
+Onyx.init({keys: ONYXKEYS});
 
 describe('TransactionUtils', () => {
     describe('getCreated', () => {
@@ -89,6 +96,68 @@ describe('TransactionUtils', () => {
                 const result = TransactionUtils.getFormattedPostedDate(transaction);
 
                 expect(result).toEqual(expectedResult);
+            });
+        });
+
+        describe('shouldShowViolations', () => {
+            it('returns true if there are violations and only if the user can edit the request', async () => {
+                // Given violations exist for a transaction
+                await Onyx.merge(ONYXKEYS.SESSION, {
+                    accountID: 1234,
+                });
+                const expenseReport = ReportUtils.buildOptimisticExpenseReport('212', '123', 100, 122, 'USD');
+                const expenseTransaction = TransactionUtils.buildOptimisticTransaction(100, 'USD', expenseReport.reportID);
+                const transactionViolations = {
+                    [`${ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS}${expenseTransaction.transactionID}`]: [
+                        {type: CONST.VIOLATION_TYPES.NOTICE, name: CONST.VIOLATIONS.TAX_REQUIRED},
+                        {type: CONST.VIOLATION_TYPES.VIOLATION, name: CONST.VIOLATIONS.TAX_REQUIRED},
+                        {type: CONST.VIOLATION_TYPES.WARNING, name: CONST.VIOLATIONS.TAX_REQUIRED},
+                    ],
+                };
+
+                const parentIOUAction = ReportUtils.buildOptimisticIOUReportAction(
+                    'create',
+                    100,
+                    'USD',
+                    '',
+                    [],
+                    expenseTransaction.transactionID,
+                    undefined,
+                    expenseReport.reportID,
+                    undefined,
+                    false,
+                    false,
+                    undefined,
+                    undefined,
+                );
+                const transactionThreadReport = ReportUtils.buildTransactionThread(parentIOUAction, expenseReport);
+                parentIOUAction.childReportID = transactionThreadReport.reportID;
+
+                await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${expenseReport.reportID}`, expenseReport);
+                await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${transactionThreadReport.reportID}`, transactionThreadReport);
+                await Onyx.merge(`${ONYXKEYS.COLLECTION.TRANSACTION}${expenseTransaction.transactionID}`, expenseTransaction);
+                // When the current user can edit the request then we should show violations
+                expect(TransactionUtils.shouldShowViolation({transactionID: expenseTransaction.transactionID, transactionViolations, parentReportAction: parentIOUAction})).toBe(true);
+                expect(TransactionUtils.shouldShowNoticeTypeViolation({transactionID: expenseTransaction.transactionID, transactionViolations, parentReportAction: parentIOUAction})).toBe(
+                    true,
+                );
+                expect(TransactionUtils.shouldShowWarningTypeViolation({transactionID: expenseTransaction.transactionID, transactionViolations, parentReportAction: parentIOUAction})).toBe(
+                    true,
+                );
+                expect(TransactionUtils.shouldShowMissingSmartscanFieldsError({...expenseTransaction, amount: 0}, parentIOUAction)).toBe(true);
+
+                // When the current user can't edit the request
+                parentIOUAction.actorAccountID = 4321;
+
+                // We should not show violations
+                expect(TransactionUtils.shouldShowViolation({transactionID: expenseTransaction.transactionID, transactionViolations, parentReportAction: parentIOUAction})).toBe(false);
+                expect(TransactionUtils.shouldShowNoticeTypeViolation({transactionID: expenseTransaction.transactionID, transactionViolations, parentReportAction: parentIOUAction})).toBe(
+                    false,
+                );
+                expect(TransactionUtils.shouldShowWarningTypeViolation({transactionID: expenseTransaction.transactionID, transactionViolations, parentReportAction: parentIOUAction})).toBe(
+                    false,
+                );
+                expect(TransactionUtils.shouldShowMissingSmartscanFieldsError({...expenseTransaction, amount: 0}, parentIOUAction)).toBe(false);
             });
         });
     });
