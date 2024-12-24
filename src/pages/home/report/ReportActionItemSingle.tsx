f55cff99e35c7c541a225d85ac8544151cdbec26
Wildan Muhlis <muhliswildan@gmail.com>
2024-12-24 14:59:14 +0700

    Remove unnecessary code

--- src/pages/home/report/ReportActionItemSingle.tsx
+++ src/pages/home/report/ReportActionItemSingle.tsx
@@ -91,8 +91,7 @@ function ReportActionItemSingle({
     const delegatePersonalDetails = personalDetails?.[action?.delegateAccountID ?? ''];
     const ownerAccountID = iouReport?.ownerAccountID ?? action?.childOwnerAccountID;
     const isReportPreviewAction = action?.actionName === CONST.REPORT.ACTIONS.TYPE.REPORT_PREVIEW;
-    // fallback to action?.accountID to handle search result chat item
-    const actorAccountID = ReportUtils.getReportActionActorAccountID(action, iouReport, report) ?? action?.accountID;
+    const actorAccountID = ReportUtils.getReportActionActorAccountID(action, iouReport, report);
     const [invoiceReceiverPolicy] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${report?.invoiceReceiver && 'policyID' in report.invoiceReceiver ? report.invoiceReceiver.policyID : -1}`);
 
     let displayName = ReportUtils.getDisplayNameForParticipant(actorAccountID);
