93328ea8b0dc1880041b6277c219c0c4379cad35
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2024-12-24 12:58:38 +0700

    add comment

--- src/hooks/useBeforeRemove.tsx
+++ src/hooks/useBeforeRemove.tsx
@@ -2,6 +2,7 @@ import {useNavigation} from '@react-navigation/native';
 import type {EventListenerCallback, EventMapCore, NavigationState} from '@react-navigation/native';
 import {useEffect} from 'react';
 
+// beforeRemove have some limitations. When the react-navigation is upgraded to 7.x, update this to use usePreventRemove hook.
 const useBeforeRemove = (onBeforeRemove: EventListenerCallback<EventMapCore<NavigationState>, 'beforeRemove'>) => {
     const navigation = useNavigation();
 
