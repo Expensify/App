02ae8cd4e8a48b300151e8ff7073a3019ec34142
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-12-24 17:58:20 +0100

    Add initiatedOffline property to persistedRequest in SequentialQueueTest

--- tests/unit/SequentialQueueTest.ts
+++ tests/unit/SequentialQueueTest.ts
@@ -248,7 +248,7 @@ describe('SequentialQueue', () => {
     });
 
     it('should get the ongoing request from onyx and start processing it', async () => {
-        const persistedRequest = {...request, persistWhenOngoing: true};
+        const persistedRequest = {...request, persistWhenOngoing: true, initiatedOffline: false};
         Onyx.set(ONYXKEYS.PERSISTED_ONGOING_REQUESTS, persistedRequest);
         SequentialQueue.push({command: 'OpenReport'});
 
