b27b051ba9a0d5854747f0caa1626c6308962c9e
Novell <76243292+NJ-2020@users.noreply.github.com>
2024-12-27 20:25:16 -0800

    update report id comment
    Co-authored-by: Monil Bhavsar <monilbhavsar25@gmail.com>

--- tests/unit/Search/SearchUIUtilsTest.ts
+++ tests/unit/Search/SearchUIUtilsTest.ts
@@ -5,7 +5,6 @@ import type * as OnyxTypes from '@src/types/onyx';
 
 const accountID = 18439984;
 const policyID = 'A1B2C3';
-// Given the report id of the report transaction
 const reportID = '123456789';
 // Transaction ID of the transaction
 const transactionID = '1';
