df6460ed79f4d135b2205b451d6a3735270efa43
Chavda Sachin <81917707+ChavdaSachin@users.noreply.github.com>
2024-12-30 04:18:42 +0530

    Delete accidentally added file

--- modules/ContactsNitroModule/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type {ContactsModule} from './ContactsModule.nitro';
-import type {Contact} from './ContactsModule.nitro';
-
-export declare const ContactsNitroModule: ContactsModule;
-export type {Contact};
