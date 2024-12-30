f124c97ac923075912cf0023c28fe6e855b8a9f8
Kevin Brian Bader <kevin.bader96@gmail.com>
2024-12-30 12:08:07 -0800

    Fix composer highlight flickers after closing attachment view

--- src/hooks/useResetComposerFocus.ts
+++ src/hooks/useResetComposerFocus.ts
@@ -2,18 +2,23 @@ import {useIsFocused} from '@react-navigation/native';
 import type {MutableRefObject} from 'react';
 import {useEffect, useRef} from 'react';
 import type {TextInput} from 'react-native';
+import {useOnyx} from 'react-native-onyx';
+import ONYXKEYS from '@src/ONYXKEYS';
+import usePrevious from './usePrevious';
 
 export default function useResetComposerFocus(inputRef: MutableRefObject<TextInput | null>) {
     const isFocused = useIsFocused();
     const shouldResetFocusRef = useRef(false);
+    const [modal] = useOnyx(ONYXKEYS.MODAL);
+    const prevIsModalVisible = usePrevious(modal?.isVisible);
 
     useEffect(() => {
-        if (!isFocused || !shouldResetFocusRef.current) {
+        if (!isFocused || !shouldResetFocusRef.current || prevIsModalVisible) {
             return;
         }
         inputRef.current?.focus(); // focus input again
         shouldResetFocusRef.current = false;
-    }, [isFocused, inputRef]);
+    }, [isFocused, inputRef, prevIsModalVisible]);
 
     return {isFocused, shouldResetFocusRef};
 }
