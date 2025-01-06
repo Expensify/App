2ccb1aac920bbf3bd6b17a9d89ed1952486fb18e
FitseTLT <engfitsum@gmail.com>
2025-01-06 17:14:34 +0300

    minor fix

--- src/libs/actions/Task.ts
+++ src/libs/actions/Task.ts
@@ -1256,7 +1256,7 @@ function canActionTask(taskReport: OnyxEntry<OnyxTypes.Report>, sessionAccountID
 
     const parentReport = getParentReport(taskReport);
     const reportNameValuePairs = ReportUtils.getReportNameValuePairs(parentReport?.reportID);
-    if (ReportUtils.isArchivedRoom(parentReport, reportNameValuePairs)) {
+    if (ReportUtils.isArchivedNonExpenseReport(parentReport, reportNameValuePairs)) {
         return false;
     }
 
