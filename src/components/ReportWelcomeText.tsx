02e102a4fcd1e5cb5c187cf92ef906c11f817811
FitseTLT <engfitsum@gmail.com>
2025-01-06 23:48:15 +0300

    lint fix

--- src/components/ReportWelcomeText.tsx
+++ src/components/ReportWelcomeText.tsx
@@ -34,7 +34,7 @@ function ReportWelcomeText({report, policy}: ReportWelcomeTextProps) {
     const [personalDetails] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST);
     const isPolicyExpenseChat = ReportUtils.isPolicyExpenseChat(report);
     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${report?.reportID || -1}`);
+    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${report?.reportID || undefined}`);
     const isArchivedRoom = ReportUtils.isArchivedNonExpenseReport(report, reportNameValuePairs);
     const isChatRoom = ReportUtils.isChatRoom(report);
     const isSelfDM = ReportUtils.isSelfDM(report);
