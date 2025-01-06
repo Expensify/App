697f96a4f5fe5a44622a91ed0e39f6fac30c9466
FitseTLT <engfitsum@gmail.com>
2025-01-06 23:21:07 +0300

    update tests

--- tests/unit/PolicyDistanceRatesUtilsTest.ts
+++ tests/unit/PolicyDistanceRatesUtilsTest.ts
@@ -4,10 +4,21 @@ describe('PolicyDistanceRatesUtils', () => {
     describe('validateTaxClaimableValue', () => {
         it('should return an error when taxClaimableValue is equal to tax rate', () => {
             // Given a tax claimable value inserted for a distance rate
+
             // When the taxClaimableValue is equal to the tax rate
             const validate = validateTaxClaimableValue({taxClaimableValue: '0.67'}, {rate: 67, customUnitRateID: ''});
             // Then validateTaxClaimableValue will return an error.
             expect(validate.taxClaimableValue).toBeDefined();
+
+            // When the taxClaimableValue is greater than the tax rate
+            const validate2 = validateTaxClaimableValue({taxClaimableValue: '0.69'}, {rate: 67, customUnitRateID: ''});
+            // Then validateTaxClaimableValue will return an error.
+            expect(validate2.taxClaimableValue).toBeDefined();
+
+            // When the taxClaimableValue is less than the tax rate
+            const validate3 = validateTaxClaimableValue({taxClaimableValue: '0.65'}, {rate: 67, customUnitRateID: ''});
+            // Then validateTaxClaimableValue will not return an error.
+            expect(validate3.taxClaimableValue).toBeUndefined();
         });
     });
 });
