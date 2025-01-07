531f434368bcf27976cee06543181d0f7a6144bd
Michal Muzyk <mimuzyk@gmail.com>
2025-01-07 12:21:57 +0100

    fix: linter

--- src/pages/ReimbursementAccount/NonUSD/BusinessInfo/substeps/PaymentVolume.tsx
+++ src/pages/ReimbursementAccount/NonUSD/BusinessInfo/substeps/PaymentVolume.tsx
@@ -24,7 +24,7 @@ function PaymentVolume({onNext, isEditing}: PaymentVolumeProps) {
     const [reimbursementAccount] = useOnyx(ONYXKEYS.REIMBURSEMENT_ACCOUNT);
     const [reimbursementAccountDraft] = useOnyx(ONYXKEYS.FORMS.REIMBURSEMENT_ACCOUNT_FORM_DRAFT);
     const [corpayOnboardingFields] = useOnyx(ONYXKEYS.CORPAY_ONBOARDING_FIELDS);
-    const policyID = reimbursementAccount?.achData?.policyID ?? '-1';
+    const policyID = reimbursementAccount?.achData?.policyID;
     const [policy] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${policyID}`);
     const currency = policy?.outputCurrency ?? '';
 
