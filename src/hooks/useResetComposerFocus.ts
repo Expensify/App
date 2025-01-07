8fce2ab9730ff999fac2b910c2b46594787262f0
Kevin Brian Bader <kevin.bader96@gmail.com>
2025-01-07 01:18:10 -0800

    adjusted solution to fix additional issue

--- src/hooks/useResetComposerFocus.ts
+++ src/hooks/useResetComposerFocus.ts
@@ -1,24 +1,25 @@
 import {useIsFocused} from '@react-navigation/native';
 import type {MutableRefObject} from 'react';
 import {useEffect, useRef} from 'react';
+import {InteractionManager} from 'react-native';
 import type {TextInput} from 'react-native';
-import {useOnyx} from 'react-native-onyx';
-import ONYXKEYS from '@src/ONYXKEYS';
-import usePrevious from './usePrevious';
 
 export default function useResetComposerFocus(inputRef: MutableRefObject<TextInput | null>) {
     const isFocused = useIsFocused();
     const shouldResetFocusRef = useRef(false);
-    const [modal] = useOnyx(ONYXKEYS.MODAL);
-    const prevIsModalVisible = usePrevious(modal?.isVisible);
 
     useEffect(() => {
-        if (!isFocused || !shouldResetFocusRef.current || prevIsModalVisible) {
+        if (!isFocused || !shouldResetFocusRef.current) {
             return;
         }
-        inputRef.current?.focus(); // focus input again
-        shouldResetFocusRef.current = false;
-    }, [isFocused, inputRef, prevIsModalVisible]);
+        const interactionTask = InteractionManager.runAfterInteractions(() => {
+            inputRef.current?.focus(); // focus input again
+            shouldResetFocusRef.current = false;
+        });
+        return () => {
+            interactionTask.cancel();
+        };
+    }, [isFocused, inputRef]);
 
     return {isFocused, shouldResetFocusRef};
 }
