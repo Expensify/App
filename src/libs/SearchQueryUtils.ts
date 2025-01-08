86acfed3d135a5b45f76e561364cc28b02934903
289Adam289 <adam.szwaja@swmansion.com>
2025-01-08 14:08:00 +0100

    fix comment

--- src/libs/SearchQueryUtils.ts
+++ src/libs/SearchQueryUtils.ts
@@ -67,7 +67,7 @@ const UserFriendlyKeyMap: Record<SearchFilterKey | typeof CONST.SEARCH.SYNTAX_RO
 
 /**
  * @private
- * Returns string value wrapped in quotes "", if the value contains special characters.
+ * Returns string value wrapped in quotes "", if the value contains space.
  */
 function sanitizeSearchValue(str: string) {
     if (str.includes(' ')) {
