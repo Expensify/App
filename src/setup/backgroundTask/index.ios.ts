3bd5984c7d2e4f6ebeb8ddb929ccea33d00b9b71
szymonrybczak <szymon.rybczak@gmail.com>
2025-01-08 09:52:30 +0100

    feat: add a log when executing background task

--- src/setup/backgroundTask/index.ios.ts
+++ src/setup/backgroundTask/index.ios.ts
@@ -1,8 +1,10 @@
 import TaskManager from '@expensify/react-native-background-task';
+import Log from '@libs/Log';
 import * as SequentialQueue from '@libs/Network/SequentialQueue';
 
 const BACKGROUND_FETCH_TASK = 'FLUSH-SEQUENTIAL-QUEUE-BACKGROUND-FETCH';
 
 TaskManager.defineTask(BACKGROUND_FETCH_TASK, () => {
+    Log.info('BackgroundTask', true, `Executing ${BACKGROUND_FETCH_TASK} background task at ${new Date().toISOString()}`);
     SequentialQueue.flush();
 });
