12d4d7b7ff4fc3ed28ab64560b0c676c9e6f7a6b
Filip Solecki <filip.solecki@swmansion.com>
2025-01-08 11:50:24 +0100

    Fix changed files eslint

--- src/pages/iou/request/MoneyRequestParticipantsSelector.tsx
+++ src/pages/iou/request/MoneyRequestParticipantsSelector.tsx
@@ -169,7 +169,7 @@ function MoneyRequestParticipantsSelector({
 
         const formatResults = OptionsListUtils.formatSectionsFromSearchTerm(
             debouncedSearchTerm,
-            participants.map((participant) => ({...participant, reportID: participant.reportID ?? '-1'})),
+            participants.map((participant) => ({...participant, reportID: participant.reportID})) as ReportUtils.OptionData[],
             chatOptions.recentReports,
             chatOptions.personalDetails,
             personalDetails,
@@ -192,7 +192,11 @@ function MoneyRequestParticipantsSelector({
 
         if (
             chatOptions.userToInvite &&
-            !OptionsListUtils.isCurrentUser({...chatOptions.userToInvite, accountID: chatOptions.userToInvite?.accountID ?? -1, status: chatOptions.userToInvite?.status ?? undefined})
+            !OptionsListUtils.isCurrentUser({
+                ...chatOptions.userToInvite,
+                accountID: chatOptions.userToInvite?.accountID ?? CONST.DEFAULT_NUMBER_ID,
+                status: chatOptions.userToInvite?.status ?? undefined,
+            })
         ) {
             newSections.push({
                 title: undefined,
