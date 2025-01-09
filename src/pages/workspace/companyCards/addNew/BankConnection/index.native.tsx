baefa79f73fc1fd2f0089fdf54dd732bba930829
VickyStash <vikstash@gmail.com>
2025-01-09 12:06:30 +0100

    Show loading before navigation when bank is connected on mobile

--- src/pages/workspace/companyCards/addNew/BankConnection/index.native.tsx
+++ src/pages/workspace/companyCards/addNew/BankConnection/index.native.tsx
@@ -1,5 +1,7 @@
-import React, {useEffect, useMemo, useRef} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import {ActivityIndicator} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
+import type {WebViewNavigation} from 'react-native-webview';
 import {WebView} from 'react-native-webview';
 import type {ValueOf} from 'type-fest';
 import FullPageOfflineBlockingView from '@components/BlockingViews/FullPageOfflineBlockingView';
@@ -8,6 +10,8 @@ import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import useLocalize from '@hooks/useLocalize';
 import usePrevious from '@hooks/usePrevious';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
 import * as CardUtils from '@libs/CardUtils';
 import getUAForWebView from '@libs/getUAForWebView';
 import Navigation from '@libs/Navigation/Navigation';
@@ -25,6 +29,8 @@ type BankConnectionStepProps = {
 
 function BankConnection({policyID}: BankConnectionStepProps) {
     const {translate} = useLocalize();
+    const theme = useTheme();
+    const styles = useThemeStyles();
     const webViewRef = useRef<WebView>(null);
     const [session] = useOnyx(ONYXKEYS.SESSION);
     const authToken = session?.authToken ?? null;
@@ -33,6 +39,7 @@ function BankConnection({policyID}: BankConnectionStepProps) {
     const url = getCompanyCardBankConnection(policyID, bankName);
     const workspaceAccountID = PolicyUtils.getWorkspaceAccountID(policyID ?? '-1');
     const [cardFeeds] = useOnyx(`${ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER}${workspaceAccountID}`);
+    const [isConnectionCompleted, setConnectionCompleted] = useState(false);
     const prevFeedsData = usePrevious(cardFeeds?.settings?.oAuthAccountDetails);
     const {isNewFeedConnected, newFeed} = useMemo(() => CardUtils.checkIfNewFeedConnected(prevFeedsData ?? {}, cardFeeds?.settings?.oAuthAccountDetails ?? {}), [cardFeeds, prevFeedsData]);
 
@@ -62,6 +69,13 @@ function BankConnection({policyID}: BankConnectionStepProps) {
         }
     }, [isNewFeedConnected, newFeed, policyID, url]);
 
+    const checkIfConnectionCompleted = (navState: WebViewNavigation) => {
+        if (!navState.url.includes(ROUTES.BANK_CONNECTION_COMPLETE)) {
+            return;
+        }
+        setConnectionCompleted(true);
+    };
+
     return (
         <ScreenWrapper
             testID={BankConnection.displayName}
@@ -75,7 +89,7 @@ function BankConnection({policyID}: BankConnectionStepProps) {
                 onBackButtonPress={handleBackButtonPress}
             />
             <FullPageOfflineBlockingView>
-                {!!url && (
+                {!!url && !isConnectionCompleted && (
                     <WebView
                         ref={webViewRef}
                         source={{
@@ -86,8 +100,16 @@ function BankConnection({policyID}: BankConnectionStepProps) {
                         }}
                         userAgent={getUAForWebView()}
                         incognito
+                        onNavigationStateChange={checkIfConnectionCompleted}
                         startInLoadingState
                         renderLoading={renderLoading}
+                    />
+                )}
+                {isConnectionCompleted && (
+                    <ActivityIndicator
+                        size={CONST.ACTIVITY_INDICATOR_SIZE.LARGE}
+                        style={styles.flex1}
+                        color={theme.spinner}
                     />
                 )}
             </FullPageOfflineBlockingView>
