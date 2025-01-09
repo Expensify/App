508c4a38d0f4f364211771cff6e5fd775e7c61ed
Mateusz Titz <titzmateusz@gmail.com>
2025-01-09 13:41:02 +0100

    Fix short mentions handling and bump live-markdown

--- src/components/RNMarkdownTextInput.tsx
+++ src/components/RNMarkdownTextInput.tsx
@@ -1,10 +1,11 @@
-import type {MarkdownRange, MarkdownTextInputProps} from '@expensify/react-native-live-markdown';
+import type {MarkdownTextInputProps} from '@expensify/react-native-live-markdown';
 import {MarkdownTextInput, parseExpensiMark} from '@expensify/react-native-live-markdown';
 import type {ForwardedRef} from 'react';
-import React, {forwardRef, useCallback} from 'react';
-import Animated from 'react-native-reanimated';
-import useCurrentUserPersonalDetails from '@hooks/useCurrentUserPersonalDetails';
+import React, {forwardRef, useCallback, useEffect} from 'react';
+import Animated, {useSharedValue} from 'react-native-reanimated';
+import useShortMentionsList from '@hooks/useShortMentionsList';
 import useTheme from '@hooks/useTheme';
+import {decorateRangesWithShortMentions} from '@libs/ParsingUtils';
 import CONST from '@src/CONST';
 
 // Convert the underlying TextInput into an Animated component so that we can take an animated ref and pass it to a worklet
@@ -17,30 +18,15 @@ type RNMarkdownTextInputWithRefProps = Omit<MarkdownTextInputProps, 'parser'> & 
     parser?: MarkdownTextInputProps['parser'];
 };
 
-function decorateRangesWithCurrentUser(ranges: MarkdownRange[], text: string, currentUser: string): MarkdownRange[] {
-    'worklet';
-
-    return ranges.map((range) => {
-        if (range.type === 'mention-user') {
-            const mentionText = text.slice(range.start, range.start + range.length);
-            const isCurrentUser = mentionText === `@${currentUser}`;
-            if (isCurrentUser) {
-                return {
-                    ...range,
-                    type: 'mention-here',
-                };
-            }
-        }
-
-        return range;
-    });
-}
-
 function RNMarkdownTextInputWithRef({maxLength, parser, ...props}: RNMarkdownTextInputWithRefProps, ref: ForwardedRef<AnimatedMarkdownTextInputRef>) {
     const theme = useTheme();
-    const currentUserPersonalDetails = useCurrentUserPersonalDetails();
 
-    const currentUserLogin = currentUserPersonalDetails.login;
+    const {mentionsList, currentUserMention} = useShortMentionsList();
+    const mentionsSharedVal = useSharedValue<string[]>([]);
+
+    useEffect(() => {
+        mentionsSharedVal.set(mentionsList);
+    }, [mentionsList, mentionsSharedVal]);
 
     // We accept parser passed down as a prop or use ExpensiMark if parser is not defined
     const parserWorklet = useCallback(
@@ -52,13 +38,14 @@ function RNMarkdownTextInputWithRef({maxLength, parser, ...props}: RNMarkdownTex
             }
 
             const parsedMentions = parseExpensiMark(text);
-            if (!currentUserLogin) {
+            const availableMentions = mentionsSharedVal.get();
+            if (availableMentions.length === 0) {
                 return parsedMentions;
             }
 
-            return decorateRangesWithCurrentUser(parsedMentions, text, currentUserLogin);
+            return decorateRangesWithShortMentions(parsedMentions, text, mentionsSharedVal.get(), currentUserMention);
         },
-        [currentUserLogin, parser],
+        [currentUserMention, mentionsSharedVal, parser],
     );
 
     return (
@@ -86,5 +73,4 @@ function RNMarkdownTextInputWithRef({maxLength, parser, ...props}: RNMarkdownTex
 RNMarkdownTextInputWithRef.displayName = 'RNTextInputWithRef';
 
 export default forwardRef(RNMarkdownTextInputWithRef);
-export {decorateRangesWithCurrentUser};
 export type {AnimatedMarkdownTextInputRef};
