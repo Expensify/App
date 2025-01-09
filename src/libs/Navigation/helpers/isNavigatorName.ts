2d6951a03843d37c1893061ea266bcf8cd506fa4
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-01-09 16:43:58 +0100

    remove isSplitNavigatorName

--- src/libs/Navigation/helpers/isNavigatorName.ts
+++ src/libs/Navigation/helpers/isNavigatorName.ts
@@ -1,5 +1,5 @@
 import {SIDEBAR_TO_SPLIT, SPLIT_TO_SIDEBAR} from '@libs/Navigation/linkingConfig/RELATIONS';
-import type {FullScreenName, OnboardingFlowName, SplitNavigatorName, SplitNavigatorSidebarScreen} from '@libs/Navigation/types';
+import type {FullScreenName, OnboardingFlowName, SplitNavigatorSidebarScreen} from '@libs/Navigation/types';
 import SCREENS from '@src/SCREENS';
 
 const ONBOARDING_SCREENS = [
@@ -12,7 +12,6 @@ const ONBOARDING_SCREENS = [
     SCREENS.ONBOARDING.WORKSPACES,
 ];
 
-const SPLIT_NAVIGATORS_SET = new Set(Object.values(SIDEBAR_TO_SPLIT));
 const FULL_SCREENS_SET = new Set([...Object.values(SIDEBAR_TO_SPLIT), SCREENS.SEARCH.ROOT]);
 const SIDEBARS_SET = new Set(Object.values(SPLIT_TO_SIDEBAR));
 const ONBOARDING_SCREENS_SET = new Set(ONBOARDING_SCREENS);
@@ -33,10 +32,6 @@ function isOnboardingFlowName(screen: string | undefined) {
     return checkIfScreenHasMatchingNameToSetValues<OnboardingFlowName>(screen, ONBOARDING_SCREENS_SET);
 }
 
-function isSplitNavigatorName(screen: string | undefined) {
-    return checkIfScreenHasMatchingNameToSetValues<SplitNavigatorName>(screen, SPLIT_NAVIGATORS_SET);
-}
-
 function isFullScreenName(screen: string | undefined) {
     return checkIfScreenHasMatchingNameToSetValues<FullScreenName>(screen, FULL_SCREENS_SET);
 }
@@ -45,4 +40,4 @@ function isSidebarScreenName(screen: string | undefined) {
     return checkIfScreenHasMatchingNameToSetValues<SplitNavigatorSidebarScreen>(screen, SIDEBARS_SET);
 }
 
-export {isFullScreenName, isOnboardingFlowName, isSidebarScreenName, isSplitNavigatorName};
+export {isFullScreenName, isOnboardingFlowName, isSidebarScreenName};
