c9b2f4f5808482757af489611e5cf2e6a66c91d3
Ishpaul Singh <ishpaul207@gmail.com>
2025-01-11 02:19:37 +0530

    replace meetSetupSpecialist task with meetGuideTask and fix guidedSetupData being empty

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -4144,6 +4144,8 @@ function prepareOnboardingOnyxData(
         }
     }
 
+    guidedSetupData.push(...tasksForParameters);
+
     if (!introSelected?.choice) {
         optimisticData.push({
             onyxMethod: Onyx.METHOD.MERGE,
@@ -4170,7 +4172,7 @@ function prepareOnboardingOnyxData(
                 } as ReportAction,
             },
         });
-        guidedSetupData.push(...tasksForParameters, {type: 'message', ...welcomeSignOffMessage});
+        guidedSetupData.push({type: 'message', ...welcomeSignOffMessage});
     }
 
     return {optimisticData, successData, failureData, guidedSetupData, actorAccountID, selfDMParameters};
