8f8e9d4864d4e153bffac207828e1848c677dc24
jay <jaydamani567@gmail.com>
2025-01-12 22:13:50 +0100

    lint code

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -609,7 +609,7 @@ function getLastMessageTextForReport(report: OnyxEntry<Report>, lastActorDetails
 
     // we do not want to show report closed in LHN for non archived report so use getReportLastMessage as fallback instead of lastMessageText from report
     if (reportID && !report.private_isArchived && report.lastActionType === CONST.REPORT.ACTIONS.TYPE.CLOSED) {
-        return lastMessageTextFromReport || (ReportUtils.getReportLastMessage(reportID).lastMessageText ?? '')
+        return lastMessageTextFromReport || (ReportUtils.getReportLastMessage(reportID).lastMessageText ?? '');
     }
     return lastMessageTextFromReport || (report?.lastMessageText ?? '');
 }
