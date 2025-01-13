1d94320cab5aa5c0dbe18d82b97aaed70fd2c7af
nkdengineer <nkdengineer@outlook.com>
2025-01-13 16:09:03 +0700

    fix lint

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -1387,7 +1387,7 @@ function markCommentAsUnread(reportID: string | undefined, reportActionCreated: 
     // If no action created date is provided, use the last action's from other user
     const actionCreationTime =
         // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-        reportActionCreated || latestReportActionFromOtherUsers?.created ?? allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportID}`]?.lastVisibleActionCreated ?? DateUtils.getDBTime(0);
+        reportActionCreated || (latestReportActionFromOtherUsers?.created ?? allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${reportID}`]?.lastVisibleActionCreated ?? DateUtils.getDBTime(0));
 
     // We subtract 1 millisecond so that the lastReadTime is updated to just before a given reportAction's created date
     // For example, if we want to mark a report action with ID 100 and created date '2014-04-01 16:07:02.999' unread, we set the lastReadTime to '2014-04-01 16:07:02.998'
