367f95c06f71090cb6ee5c6c62a2c1d51bbbcbd2
szymonrybczak <szymon.rybczak@gmail.com>
2025-01-13 20:56:03 +0100

    fix: move mocks to one file

--- tests/unit/loginTest.tsx
+++ tests/unit/loginTest.tsx
@@ -4,11 +4,6 @@ import 'react-native';
 import renderer from 'react-test-renderer';
 import App from '@src/App';
 
-jest.mock('../../modules/background-task/src/NativeReactNativeBackgroundTask', () => ({
-    defineTask: jest.fn(),
-    onBackgroundTaskExecution: jest.fn(),
-}));
-
 // Needed for: https://stackoverflow.com/questions/76903168/mocking-libraries-in-jest
 jest.mock('react-native/Libraries/LogBox/LogBox', () => ({
     // eslint-disable-next-line @typescript-eslint/naming-convention
