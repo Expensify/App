0da271f3f451cdeea4aacd07c144be1f15f3b49f
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-01-17 10:51:22 +0100

    remove unnecessary line

--- src/components/Navigation/TopBar.tsx
+++ src/components/Navigation/TopBar.tsx
@@ -27,7 +27,6 @@ type TopBarProps = {
 function TopBar({breadcrumbLabel, activeWorkspaceID, shouldDisplaySearch = true, shouldDisplayCancelSearch = false}: TopBarProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
-
     const policy = usePolicy(activeWorkspaceID);
     const [session] = useOnyx(ONYXKEYS.SESSION, {selector: (sessionValue) => sessionValue && {authTokenType: sessionValue.authTokenType}});
     const isAnonymousUser = Session.isAnonymousUser(session);
