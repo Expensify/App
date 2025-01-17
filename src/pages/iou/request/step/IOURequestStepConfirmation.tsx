a7ff3403f9e0caa2833c445fabbc4f5069f0788d
cretadn22 <cretadn22@gmail.com>
2025-01-17 22:35:38 +0700

    adjust style

--- src/pages/iou/request/step/IOURequestStepConfirmation.tsx
+++ src/pages/iou/request/step/IOURequestStepConfirmation.tsx
@@ -23,7 +23,7 @@ import {isMovingTransactionFromTrackExpense as isMovingTransactionFromTrackExpen
 import Log from '@libs/Log';
 import Navigation from '@libs/Navigation/Navigation';
 import {getParticipantsOption, getReportOption} from '@libs/OptionsListUtils';
-import {getBankAccountRoute} from '@libs/ReportUtils';
+import {getBankAccountRoute, generateReportID} from '@libs/ReportUtils';
 import playSound, {SOUNDS} from '@libs/Sound';
 import {getDefaultTaxCode, getRateID, getRequestType, getValidWaypoints} from '@libs/TransactionUtils';
 import type {GpsPoint} from '@userActions/IOU';
@@ -39,6 +39,7 @@ import {
     setMoneyRequestCategory,
     splitBill,
     splitBillAndOpenReport,
+    startMoneyRequest,
     startSplitBill,
     submitPerDiemExpense as submitPerDiemExpenseIOUActions,
     trackExpense as trackExpenseIOUActions,
@@ -171,13 +172,16 @@ function IOURequestStepConfirmation({
     }, [transactionID, defaultBillable]);
 
     useEffect(() => {
-        if (transaction?.transactionID) return;
+        if (transaction?.transactionID) {
+            return;
+        }
         startMoneyRequest(
             CONST.IOU.TYPE.CREATE,
             // When starting to create an expense from the global FAB, there is not an existing report yet. A random optimistic reportID is generated and used
             // for all of the routes in the creation flow.
             generateReportID(),
         );
+        // eslint-disable-next-line react-compiler/react-compiler, react-hooks/exhaustive-deps -- we don't want this effect to run again
     }, []);
 
     useEffect(() => {
