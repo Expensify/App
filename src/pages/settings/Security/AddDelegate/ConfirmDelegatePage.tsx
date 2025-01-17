728ac399fdcbce0b9f4bcfa297b1f5262a75453b
truph01 <truphd01@gmail.com>
2025-01-17 10:32:18 +0700

    fix: lint

--- src/pages/settings/Security/AddDelegate/ConfirmDelegatePage.tsx
+++ src/pages/settings/Security/AddDelegate/ConfirmDelegatePage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import type {ValueOf} from 'type-fest';
 import Button from '@components/Button';
 import DelegateNoAccessWrapper from '@components/DelegateNoAccessWrapper';
