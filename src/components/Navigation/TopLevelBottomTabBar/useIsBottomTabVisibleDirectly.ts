133e7d2aa5d7813dcad43dd6d227df34d5345693
Wojciech Boman <wojciechboman@gmail.com>
2025-01-17 08:38:58 +0100

    Add optional chaining to useIsBottomTabVisibleDirectly

--- src/components/Navigation/TopLevelBottomTabBar/useIsBottomTabVisibleDirectly.ts
+++ src/components/Navigation/TopLevelBottomTabBar/useIsBottomTabVisibleDirectly.ts
@@ -3,7 +3,7 @@ import {isFullScreenName} from '@libs/Navigation/helpers/isNavigatorName';
 
 // Visible directly means not through the overlay. So the full screen (split navigator or search) has to be the last route on the root stack.
 function useIsBottomTabVisibleDirectly() {
-    const isBottomTabVisibleDirectly = useNavigationState((state) => isFullScreenName(state.routes.at(-1)?.name));
+    const isBottomTabVisibleDirectly = useNavigationState((state) => isFullScreenName(state?.routes.at(-1)?.name));
     return isBottomTabVisibleDirectly;
 }
 
