69e06879d2d0bcf5e51ca6dd978d18a61eabf567
Cristi Paval <cristi@expensify.com>
2025-01-17 15:03:23 +0200

    Add confirmation button in the PublicDomainErrorPage

--- src/pages/Travel/PublicDomainErrorPage.tsx
+++ src/pages/Travel/PublicDomainErrorPage.tsx
@@ -6,6 +6,9 @@ import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
+import FixedFooter from '@components/FixedFooter';
+import Button from '@components/Button';
+import style from '@components/Icon/IconWrapperStyles';
 
 function PublicDomainErrorPage() {
     const styles = useThemeStyles();
@@ -18,12 +21,23 @@ function PublicDomainErrorPage() {
             testID={PublicDomainErrorPage.displayName}
         >
             <HeaderWithBackButton onBackButtonPress={() => Navigation.goBack()} />
-            <View style={[styles.mt3, styles.mr5, styles.ml5]}>
-                <Text style={styles.headerAnonymousFooter}>{`${translate('travel.publicDomainError.title')}`}</Text>
+            <View style={[styles.flex1]}>
+                <View style={[styles.mt3, styles.mr5, styles.ml5]}>
+                    <Text style={styles.headerAnonymousFooter}>{`${translate('travel.publicDomainError.title')}`}</Text>
+                </View>
+                <View style={[styles.mt3, styles.mr5, styles.mb5, styles.ml5]}>
+                    <Text>{translate('travel.publicDomainError.message')}</Text>
+                </View>
             </View>
-            <View style={[styles.mt3, styles.mr5, styles.mb5, styles.ml5]}>
-                <Text>{translate('travel.publicDomainError.message')}</Text>
-            </View>
+            <FixedFooter>
+                <Button
+                    success
+                    large
+                    style={[styles.w100]}
+                    onPress={() => Navigation.goBack()}
+                    text={translate('common.buttonConfirm')}
+                />
+            </FixedFooter>
         </ScreenWrapper>
     );
 }
