3dfd5fd71109294624043723269ff5cfee1743bb
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-01-18 10:50:17 +0800

    only check using >= if both action created and lastVisibleActionCreated exists

--- src/pages/home/report/ReportActionsView.tsx
+++ src/pages/home/report/ReportActionsView.tsx
@@ -243,8 +243,10 @@ function ReportActionsView({
 
     const newestReportAction = useMemo(() => reportActions?.at(0), [reportActions]);
     const mostRecentIOUReportActionID = useMemo(() => getMostRecentIOURequestActionID(reportActions), [reportActions]);
-    const lastActionCreated = visibleReportActions.at(0)?.created ?? '';
-    const hasNewestReportAction = lastActionCreated >= (report.lastVisibleActionCreated ?? '') || lastActionCreated >= (transactionThreadReport?.lastVisibleActionCreated ?? '');
+    const lastActionCreated = visibleReportActions.at(0)?.created;
+    const isNewestAction = (actionCreated: string | undefined, lastVisibleActionCreated: string | undefined) =>
+        actionCreated && (lastVisibleActionCreated ? actionCreated >= lastVisibleActionCreated : actionCreated === lastVisibleActionCreated);
+    const hasNewestReportAction = isNewestAction(lastActionCreated, report.lastVisibleActionCreated) || isNewestAction(lastActionCreated, transactionThreadReport?.lastVisibleActionCreated);
     const oldestReportAction = useMemo(() => reportActions?.at(-1), [reportActions]);
 
     useEffect(() => {
