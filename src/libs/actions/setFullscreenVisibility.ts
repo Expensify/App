98b21803ea1d40e60abfd6d92898d0c54b0105f0
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-01-22 16:13:42 +0800

    prefer default export

--- src/libs/actions/setFullscreenVisibility.ts
+++ src/libs/actions/setFullscreenVisibility.ts
@@ -1,8 +1,6 @@
 import Onyx from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 
-function setFullscreenVisibility(isVisible: boolean) {
+export default function setFullscreenVisibility(isVisible: boolean) {
     Onyx.merge(ONYXKEYS.FULLSCREEN_VISIBILITY, isVisible);
 }
-
-export {setFullscreenVisibility};
