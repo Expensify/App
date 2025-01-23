0511174ab4395a0cb45d4ffcb10eea95ff35f435
kubabutkiewicz <jakub.butkiewicz94@gmail.com>
2025-01-23 17:54:19 +0100

    fix: tests

--- src/components/Button/utils/index.ts
+++ src/components/Button/utils/index.ts
@@ -1,5 +1,6 @@
+import CONST from '@src/CONST';
 import type {GetButtonRole, GetNestedButtonStyle} from './types';
 
 const getNestedButtonStyle: GetNestedButtonStyle = () => undefined;
-const getNestedButtonRole: GetButtonRole = () => undefined;
+const getNestedButtonRole: GetButtonRole = () => CONST.ROLE.BUTTON;
 export {getNestedButtonStyle, getNestedButtonRole};
