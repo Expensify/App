5493c6b5d4483f7023218c6c4607170202af83bf
jacobkim9881 <jacobkim9881@gmail.com>
2025-01-23 11:40:40 +0900

    comment about focus trap for animation

--- src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.tsx
+++ src/pages/settings/Profile/Contacts/ContactMethodDetailsPage.tsx
@@ -269,6 +269,8 @@ function ContactMethodDetailsPage({route}: ContactMethodDetailsPageProps) {
             testID={ContactMethodDetailsPage.displayName}
             focusTrapSettings={{
                 focusTrapOptions: {
+                    //It is added because input form's focusing bothers transition animation:
+                    // https://github.com/Expensify/App/issues/53884#issuecomment-2594568960
                     checkCanFocusTrap: (trapContainers: Array<HTMLElement | SVGElement>) => {
                         return new Promise((resolve) => {
                             const interval = setInterval(() => {
