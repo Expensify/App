c168d4e7bade9084e71b962ca137585f8a851ba9
nkdengineer <nkdengineer@outlook.com>
2025-01-23 10:03:23 +0700

    remove unchange

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -645,7 +645,7 @@ function ReportScreen({route, navigation}: ReportScreenProps) {
         // If you already have a report open and are deeplinking to a new report on native,
         // the ReportScreen never actually unmounts and the reportID in the route also doesn't change.
         // Therefore, we need to compare if the existing reportID is the same as the one in the route
-        // before deciding that we shouldn't call Open
+        // before deciding that we shouldn't call OpenReport.
         if (reportIDFromRoute === lastReportIDFromRoute && (!onyxReportID || onyxReportID === reportIDFromRoute)) {
             return;
         }
