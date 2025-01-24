0101f1c6fc762b8f0f1a0eb0c6c6ee9ea862259a
Vit Horacek <vit@expensify.com>
2025-01-24 17:25:41 +0100

    Update the isCustom param type and add more tests

--- src/libs/CardUtils.ts
+++ src/libs/CardUtils.ts
@@ -287,7 +287,7 @@ function getCardFeedIcon(cardFeed: CompanyCardFeed | typeof CONST.EXPENSIFY_CARD
 /**
  * Verify if the feed is a custom feed. Those are also refered to as commercial feeds.
  */
-function isCustomFeed(feed: CompanyCardFeed): boolean {
+function isCustomFeed(feed: CompanyCardFeedWithNumber): boolean {
     return [CONST.COMPANY_CARD.FEED_BANK_NAME.MASTER_CARD, CONST.COMPANY_CARD.FEED_BANK_NAME.VISA, CONST.COMPANY_CARD.FEED_BANK_NAME.AMEX].some((value) => feed.startsWith(value));
 }
 
