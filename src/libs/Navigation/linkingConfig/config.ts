217baad91f8600d2f22e4e207a8701d62480f914
Wojciech Boman <wojciechboman@gmail.com>
2025-01-24 13:09:53 +0100

    Remove CONST.EMPTY_STRING

--- src/libs/Navigation/linkingConfig/config.ts
+++ src/libs/Navigation/linkingConfig/config.ts
@@ -36,10 +36,12 @@ const config: LinkingOptions<RootStackParamList>['config'] = {
             // If params are defined, but reportID is explicitly undefined, we will get the url /r/undefined.
             // We want to avoid that situation, so we will return an empty string instead.
             parse: {
-                reportID: (reportID: string | undefined) => reportID ?? CONST.EMPTY_STRING,
+                // eslint-disable-next-line
+                reportID: (reportID: string | undefined) => reportID ?? '',
             },
             stringify: {
-                reportID: (reportID: string | undefined) => reportID ?? CONST.EMPTY_STRING,
+                // eslint-disable-next-line
+                reportID: (reportID: string | undefined) => reportID ?? '',
             },
         },
         [SCREENS.SETTINGS.PROFILE.ROOT]: {
