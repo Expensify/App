3dfac09f975a54ef7a28a599503717dd409bf6b5
nkdengineer <nkdengineer@outlook.com>
2025-01-24 11:28:25 +0700

    update type

--- src/components/Search/types.ts
+++ src/components/Search/types.ts
@@ -39,7 +39,7 @@ type SelectedTransactions = Record<string, SelectedTransactionInfo>;
 /** Model of selected reports */
 type SelectedReports = {
     reportID: string;
-    policyID: string;
+    policyID: string | undefined;
     action: ValueOf<typeof CONST.SEARCH.ACTION_TYPES>;
     total: number;
 };
