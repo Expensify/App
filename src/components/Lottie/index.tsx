4cf0923cd3523ecf792750afb2f686501c4af760
Linh Vo <vovanlinh29546@gmail.com>
2025-01-28 08:11:30 +0700

    fix: pause lottie animation on mweb to prevent memory leak

--- src/components/Lottie/index.tsx
+++ src/components/Lottie/index.tsx
@@ -9,6 +9,7 @@ import useAppState from '@hooks/useAppState';
 import useNetwork from '@hooks/useNetwork';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as Browser from '@libs/Browser';
+import {isMobile} from '@libs/Browser';
 import isSideModalNavigator from '@libs/Navigation/isSideModalNavigator';
 import CONST from '@src/CONST';
 import {useSplashScreenStateContext} from '@src/SplashScreenStateContext';
@@ -74,7 +75,7 @@ function Lottie({source, webStyle, shouldLoadAfterInteractions, ...props}: Props
         const unsubscribeNavigationBlur = navigator.addListener('blur', () => {
             const state = navigationContainerRef.getRootState();
             const targetRouteName = state?.routes?.[state?.index ?? 0]?.name;
-            if (!isSideModalNavigator(targetRouteName)) {
+            if (!isSideModalNavigator(targetRouteName) || isMobile()) {
                 setHasNavigatedAway(true);
             }
         });
