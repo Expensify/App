fa0ddbb4a7b0076eca748c76ec4cdfa9322da18e
Cristi Paval <cristi@expensify.com>
2025-01-29 00:58:26 +0200

    Put BookTravelButton in the body of the EmptyStateComponent

--- src/pages/Search/EmptySearchView.tsx
+++ src/pages/Search/EmptySearchView.tsx
@@ -2,8 +2,8 @@ import React, {useMemo, useState} from 'react';
 import {Linking, View} from 'react-native';
 import {useOnyx} from 'react-native-onyx';
 import type {OnyxCollection} from 'react-native-onyx';
+import BookTravelButton from '@components/BookTravelButton';
 import ConfirmModal from '@components/ConfirmModal';
-import DotIndicatorMessage from '@components/DotIndicatorMessage';
 import EmptyStateComponent from '@components/EmptyStateComponent';
 import type {FeatureListItem} from '@components/FeatureList';
 import {Alert, PiggyBank} from '@components/Icon/Illustrations';
@@ -21,7 +21,6 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import {startMoneyRequest} from '@libs/actions/IOU';
 import {openExternalLink, openOldDotLink} from '@libs/actions/Link';
 import {canActionTask, canModifyTask, completeTask} from '@libs/actions/Task';
-import {bookATrip} from '@libs/actions/Travel';
 import {setSelfTourViewed} from '@libs/actions/Welcome';
 import interceptAnonymousUser from '@libs/interceptAnonymousUser';
 import {hasSeenTourSelector} from '@libs/onboardingSelectors';
@@ -60,11 +59,8 @@ function EmptySearchView({type, hasResults}: EmptySearchViewProps) {
     const shouldRedirectToExpensifyClassic = useMemo(() => {
         return areAllGroupPoliciesExpenseChatDisabled((allPolicies as OnyxCollection<Policy>) ?? {});
     }, [allPolicies]);
-    const [activePolicyID] = useOnyx(ONYXKEYS.NVP_ACTIVE_POLICY_ID);
 
-    const [ctaErrorMessage, setCtaErrorMessage] = useState('');
-
-    const subtitleComponent = useMemo(() => {
+    const contentBody = useMemo(() => {
         return (
             <>
                 <Text style={[styles.textSupporting, styles.textNormal]}>
@@ -78,7 +74,7 @@ function EmptySearchView({type, hasResults}: EmptySearchViewProps) {
                     </TextLink>
                     {translate('travel.toLearnMore')}
                 </Text>
-                <View style={[styles.flex1, styles.flexRow, styles.flexWrap, styles.rowGap4, styles.pt4, styles.pl1]}>
+                <View style={[styles.flex1, styles.flexRow, styles.flexWrap, styles.rowGap4, styles.pt4, styles.pl1, styles.mb5]}>
                     {tripsFeatures.map((tripsFeature) => (
                         <View
                             key={tripsFeature.translationKey}
@@ -98,16 +94,10 @@ function EmptySearchView({type, hasResults}: EmptySearchViewProps) {
                         </View>
                     ))}
                 </View>
-                {!!ctaErrorMessage && (
-                    <DotIndicatorMessage
-                        style={styles.mt1}
-                        messages={{error: ctaErrorMessage}}
-                        type="error"
-                    />
-                )}
+                <BookTravelButton text={translate('search.searchResults.emptyTripResults.buttonText')} />
             </>
         );
-    }, [styles, translate, ctaErrorMessage]);
+    }, [styles, translate]);
 
     const [introSelected] = useOnyx(ONYXKEYS.NVP_INTRO_SELECTED);
     const onboardingPurpose = introSelected?.choice;
@@ -130,20 +120,7 @@ function EmptySearchView({type, hasResults}: EmptySearchViewProps) {
                     headerContentStyles: [StyleUtils.getWidthAndHeightStyle(375, 240), StyleUtils.getBackgroundColorStyle(theme.travelBG)],
                     title: translate('travel.title'),
                     titleStyles: {...styles.textAlignLeft},
-                    body: subtitleComponent,
-                    buttons: [
-                        {
-                            buttonText: translate('search.searchResults.emptyTripResults.buttonText'),
-                            buttonAction: () => {
-                                const activePolicy = allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${activePolicyID}`];
-                                if (!activePolicy) {
-                                    return;
-                                }
-                                bookATrip(activePolicy, translate, setCtaErrorMessage, ctaErrorMessage);
-                            },
-                            success: true,
-                        },
-                    ],
+                    body: contentBody,
                     lottieWebViewStyles: {backgroundColor: theme.travelBG, ...styles.emptyStateFolderWebStyles},
                 };
             case CONST.SEARCH.DATA_TYPES.EXPENSE:
@@ -243,17 +220,14 @@ function EmptySearchView({type, hasResults}: EmptySearchViewProps) {
         translate,
         styles.textAlignLeft,
         styles.emptyStateFolderWebStyles,
-        subtitleComponent,
+        contentBody,
         hasSeenTour,
-        ctaErrorMessage,
         navatticURL,
         shouldRedirectToExpensifyClassic,
         hasResults,
         viewTourTaskReport,
         canModifyTheTask,
         canActionTheTask,
-        allPolicies,
-        activePolicyID,
     ]);
 
     return (
