f3533e448ca1583cfb98db0ef6c4edbd1766c576
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-01-29 14:53:07 +0100

    chore: repack-init

--- react-native.config.js
+++ react-native.config.js
@@ -2,6 +2,7 @@ const iosSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PA
 const androidSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'android' : 'android';
 
 module.exports = {
+    commands: require('@callstack/repack/commands/rspack'),
     project: {
         ios: {sourceDir: iosSourceDir},
         android: {sourceDir: androidSourceDir},
