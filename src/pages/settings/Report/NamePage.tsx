99d1068481a71b83c9fba5bb5b67d3f4a8db1e91
Blazej Kustra <blazej.kustra@swmansion.com>
2025-01-29 13:40:46 +0100

    Add TripChatNameEditPage for editing trip chat names

--- src/pages/settings/Report/NamePage.tsx
+++ src/pages/settings/Report/NamePage.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import * as ReportUtils from '@libs/ReportUtils';
 import type {ReportSettingsNavigatorParamList} from '@navigation/types';
-import ChatNameEditPage from '@pages/ChatNameEditPage';
+import GroupChatNameEditPage from '@pages/GroupChatNameEditPage';
 import withReportOrNotFound from '@pages/home/report/withReportOrNotFound';
 import type {WithReportOrNotFoundProps} from '@pages/home/report/withReportOrNotFound';
+import TripChatNameEditPage from '@pages/TripChatNameEditPage';
 import type SCREENS from '@src/SCREENS';
 import RoomNamePage from './RoomNamePage';
 
 type NamePageProps = WithReportOrNotFoundProps & PlatformStackScreenProps<ReportSettingsNavigatorParamList, typeof SCREENS.REPORT_SETTINGS.NAME>;
 
 function NamePage({report}: NamePageProps) {
-    if (ReportUtils.isGroupChat(report) || ReportUtils.isTripRoom(report)) {
-        return <ChatNameEditPage report={report} />;
+    if (ReportUtils.isTripRoom(report)) {
+        return <TripChatNameEditPage report={report} />;
+    }
+
+    if (ReportUtils.isGroupChat(report)) {
+        return <GroupChatNameEditPage report={report} />;
     }
 
     return <RoomNamePage report={report} />;
