10465955c551bc841b0bb99600a4d7605625cc31
sumo_slonik <jakub.nowakowski@swmansion.com>
2025-01-29 15:35:53 +0100

    adding comments to script

--- .github/scripts/checkParser.sh
+++ .github/scripts/checkParser.sh
@@ -2,23 +2,25 @@
 
 set -e
 
-START_DIR=$(pwd)
 ROOT_DIR=$(dirname "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")
 cd "$ROOT_DIR" || exit 1
 
 autocomplete_parser_backup="src/libs/SearchParser/autocompleteParser.js.bak"
 search_parser_backup="src/libs/SearchParser/searchParser.js.bak"
 
+#Copying the current .js parser files
 cp src/libs/SearchParser/autocompleteParser.js "$autocomplete_parser_backup" 2>/dev/null
 cp src/libs/SearchParser/searchParser.js "$search_parser_backup" 2>/dev/null
 
+#Running the scripts that generate the .js parser files
 npm run generate-search-parser
 npm run generate-autocomplete-parser
 
-
+#Checking if the saved files differ from the newly generated
 if ! diff -q "$autocomplete_parser_backup" src/libs/SearchParser/autocompleteParser.js >/dev/null ||
    ! diff -q "$search_parser_backup" src/libs/SearchParser/searchParser.js >/dev/null; then
     echo "The files generated from the .peggy files using the commands: generate-search-parser and generate-autocomplete-parser are not identical to those currently on this branch."
+    echo "If it didn’t work, make sure you’ve run locally: npm run generate-search-parser and npm run generate-autocomplete-parser, and committed those changes."
     exit 1
 else
     echo "The files generated from the .peggy files using the commands: generate-search-parser and generate-autocomplete-parser are identical to those currently on this branch."
