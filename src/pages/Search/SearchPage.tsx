0145b82efdc9e4e820369b8ee6967d9f80d90d9b
Wildan Muhlis <muhliswildan@gmail.com>
2025-01-30 14:36:13 +0700

    skip compiler warning

--- src/pages/Search/SearchPage.tsx
+++ src/pages/Search/SearchPage.tsx
@@ -33,6 +33,7 @@ function SearchPage({route}: SearchPageProps) {
         return () => {
             resetVideoPlayerData();
         };
+        // eslint-disable-next-line react-compiler/react-compiler
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
