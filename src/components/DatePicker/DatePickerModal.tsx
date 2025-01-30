85530c4274edf9d39b145dc13735c4cad81528b1
Taras Perun <terry.perun@gmail.com>
2025-01-30 13:56:19 +0100

    use separate file for type

--- src/components/DatePicker/DatePickerModal.tsx
+++ src/components/DatePicker/DatePickerModal.tsx
@@ -2,63 +2,12 @@ import {setYear} from 'date-fns';
 import React, {useEffect, useRef, useState} from 'react';
 import type {View} from 'react-native';
 import PopoverWithMeasuredContent from '@components/PopoverWithMeasuredContent';
-import type {PopoverWithMeasuredContentProps} from '@components/PopoverWithMeasuredContent';
-import type {BaseTextInputProps} from '@components/TextInput/BaseTextInput/types';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as FormActions from '@userActions/FormActions';
 import CONST from '@src/CONST';
-import type {OnyxFormValuesMapping} from '@src/ONYXKEYS';
 import CalendarPicker from './CalendarPicker';
-
-type DatePickerProps = {
-    /**
-     * The datepicker supports any value that `new Date()` can parse.
-     * `onInputChange` would always be called with a Date (or null)
-     */
-    value?: string;
-
-    /**
-     * The datepicker supports any defaultValue that `new Date()` can parse.
-     * `onInputChange` would always be called with a Date (or null)
-     */
-    defaultValue?: string;
-
-    inputID: string;
-
-    /** A minimum date of calendar to select */
-    minDate?: Date;
-
-    /** A maximum date of calendar to select */
-    maxDate?: Date;
-
-    /** A function that is passed by FormWrapper */
-    onInputChange?: (value: string) => void;
-
-    /** A function that is passed by FormWrapper */
-    onTouched?: () => void;
-
-    /** Saves a draft of the input value when used in a form */
-    shouldSaveDraft?: boolean;
-
-    /** ID of the wrapping form */
-    formID?: keyof OnyxFormValuesMapping;
-
-    /** Whether the modal is visible */
-    isVisible: boolean;
-
-    /** Callback to close the modal */
-    onClose: () => void;
-
-    /** Callback when date is selected */
-    onSelected?: (value: string) => void;
-
-    /** Whether to close the modal when browser navigation changes */
-    shouldCloseWhenBrowserNavigationChanged?: boolean;
-
-    /** If the popover will be positioned from the top */
-    shouldPositionFromTop?: boolean;
-} & Omit<BaseTextInputProps & PopoverWithMeasuredContentProps, 'anchorRef' | 'children'>;
+import type {DatePickerProps} from './types';
 
 const DEFAULT_ANCHOR_ORIGIN = {
     horizontal: CONST.MODAL.ANCHOR_ORIGIN_HORIZONTAL.RIGHT,
