689c8d859d8d75b561bd2e9f62711e8b9dfafca8
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-01-31 01:17:15 +0800

    always scroll to top when selectin item

--- __mocks__/@react-navigation/native/index.ts
+++ __mocks__/@react-navigation/native/index.ts
@@ -42,8 +42,9 @@ const useLinkBuilder = isJestEnv ? realReactNavigation.useLinkBuilder : () => nu
 const useLinkProps = isJestEnv ? realReactNavigation.useLinkProps : () => null;
 const useLinkTo = isJestEnv ? realReactNavigation.useLinkTo : () => null;
 const useScrollToTop = isJestEnv ? realReactNavigation.useScrollToTop : () => null;
-const useRoute = isJestEnv ? realReactNavigation.useRoute : () => ({params: {}});
+const useRoute = isJestEnv ? jest.fn() : () => ({params: {}});
 const useFocusEffect = isJestEnv ? realReactNavigation.useFocusEffect : (callback: () => void) => callback();
+const UNSTABLE_usePreventRemove = isJestEnv ? jest.fn() : () => {};
 
 export {
     // Overriden modules
@@ -66,6 +67,7 @@ export {
     useScrollToTop,
     useRoute,
     useFocusEffect,
+    UNSTABLE_usePreventRemove,
 };
 
 export type {NativeNavigationMock};
