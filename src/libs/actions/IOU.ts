60025b53429c53ab83fd6aa0162af67ddc03fe87
Yauheni Pasiukevich <pasyukevich@live.com>
2025-01-31 19:13:42 +0100

    Update getTransactionDetails call to include additional parameters for improved transaction handling

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -3278,7 +3278,7 @@ function getUpdateMoneyRequestParams(
               policy,
           })
         : undefined;
-    const transactionDetails = getTransactionDetails(updatedTransaction);
+    const transactionDetails = getTransactionDetails(updatedTransaction, undefined, true);
 
     if (transactionDetails?.waypoints) {
         // This needs to be a JSON string since we're sending this to the MapBox API
