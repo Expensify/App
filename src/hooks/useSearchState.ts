0a78c2e2d20dc6a69a838a78347097ebc73c9af8
Wildan Muhlis <muhliswildan@gmail.com>
2025-02-03 10:03:59 +0700

    isOnSearch true for attachment modal case

--- src/hooks/useSearchState.ts
+++ src/hooks/useSearchState.ts
@@ -4,20 +4,24 @@ import type {PlatformStackRouteProp} from '@libs/Navigation/PlatformStackNavigat
 import type {AuthScreensParamList} from '@libs/Navigation/types';
 import {buildSearchQueryJSON} from '@libs/SearchQueryUtils';
 import SCREENS from '@src/SCREENS';
+import CONST from '@src/CONST';
 
-type SearchResult = {
+type SearchStateResult = {
     isOnSearch: boolean;
     hashKey?: string;
+    isSearchAttachmentModal?: boolean;
 };
 
 /**
  * Hook to manage search state based on route parameters
  * Returns search status and hash for query tracking
  */
-const useSearchState = (): SearchResult => {
+const useSearchState = (): SearchStateResult => {
     // We are using these contexts directly instead of useRoute, because those will throw an error if used outside a navigator.
-    const route = useContext(NavigationRouteContext) as PlatformStackRouteProp<AuthScreensParamList, typeof SCREENS.SEARCH.CENTRAL_PANE>;
-    const {q} = route?.params ?? {};
+    // const route = useContext(NavigationRouteContext) as PlatformStackRouteProp<AuthScreensParamList, typeof SCREENS.SEARCH.CENTRAL_PANE>;
+    const route = useContext(NavigationRouteContext);
+    const {q, type} = (route?.params as { q?: string; type?: string }) ?? { q: undefined, type: undefined };
+    const isSearchAttachmentModal = route?.name === SCREENS.ATTACHMENTS && type === CONST.ATTACHMENT_TYPE.SEARCH ;
 
     return useMemo(() => {
         if (!route) {
@@ -26,7 +30,7 @@ const useSearchState = (): SearchResult => {
 
         const queryJSON = q ? buildSearchQueryJSON(q) : ({} as {hash?: string});
         const hashKey = queryJSON?.hash ? String(queryJSON.hash) : undefined;
-        const isOnSearch = (route?.name === SCREENS.SEARCH.CENTRAL_PANE || route?.name === SCREENS.SEARCH.BOTTOM_TAB) && !!hashKey;
+        const isOnSearch = ((route?.name === SCREENS.SEARCH.CENTRAL_PANE || route?.name === SCREENS.SEARCH.BOTTOM_TAB) && !!hashKey) || isSearchAttachmentModal;
 
         return {hashKey, isOnSearch};
     }, [q, route]);
