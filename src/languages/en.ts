df566ce08519f9e5d58a849cfc2036c2f6369e38
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-02-03 16:01:55 +0100

    add an option to display action buttons in tooltips

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -5731,6 +5731,8 @@ const translations = {
             part6: 'Now,',
             part7: ' submit your expense',
             part8: ' and watch the magic happen!',
+            tryItOut: 'Try it out',
+            noThanks: 'No thanks',
         },
     },
     discardChangesConfirmation: {
--- src/languages/es.ts
+++ src/languages/es.ts
@@ -6251,6 +6251,8 @@ const translations = {
             part6: 'Ahora,',
             part7: ' envía tu gasto y',
             part8: ' ¡observa cómo ocurre la magia!',
+            tryItOut: 'Prueba esto',
+            noThanks: 'No, gracias',
         },
     },
     discardChangesConfirmation: {
