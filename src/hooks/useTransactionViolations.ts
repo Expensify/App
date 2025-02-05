083bf1e8b64c3d0924e8d0e84e31b7f4ab2bc58d
krishna2323 <belivethatkg@gmail.com>
2025-02-06 04:06:15 +0530

    update as per requested changes.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/hooks/useTransactionViolations.ts
+++ src/hooks/useTransactionViolations.ts
@@ -1,19 +1,13 @@
+import {useMemo} from 'react';
 import {useOnyx} from 'react-native-onyx';
-import {getTransaction, isViolationDismissed} from '@libs/TransactionUtils';
+import {isViolationDismissed} from '@libs/TransactionUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {TransactionViolations} from '@src/types/onyx';
 
-function useTransactionViolations(transactionID?: string): TransactionViolations | undefined {
-    const [transactionViolations] = useOnyx(`${ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS}${transactionID}`, {
-        selector: (violations) => {
-            const transaction = getTransaction(transactionID);
-            if (!transaction) {
-                return [];
-            }
-            return (violations ?? []).filter((violation) => !isViolationDismissed(transaction, violation));
-        },
-    });
-    return transactionViolations;
+function useTransactionViolations(transactionID?: string): TransactionViolations {
+    const [transaction] = useOnyx(`${ONYXKEYS.COLLECTION.TRANSACTION}${transactionID}`);
+    const [transactionViolations = []] = useOnyx(`${ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS}${transactionID}`);
+    return useMemo(() => transactionViolations.filter((violation) => !isViolationDismissed(transaction, violation)), [transaction, transactionViolations]);
 }
 
 export default useTransactionViolations;
