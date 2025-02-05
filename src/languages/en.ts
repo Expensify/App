749dc5fb37256dffff678bfc84f4d486149a40e2
rory <rory@expensify.com>
2025-02-05 08:39:09 -0800

    Remove accidentally committed change

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -216,13 +216,6 @@ type States = Record<keyof typeof COMMON_CONST.STATES, StateValue>;
 
 type AllCountries = Record<Country, string>;
 
-const language = {
-    language: 'English',
-    translations: {
-        ...
-    },
-};
-
 /* eslint-disable max-len */
 const translations = {
     common: {
