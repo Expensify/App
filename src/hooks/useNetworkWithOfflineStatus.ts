ed37d21e5f1a53f7ae2761185082e6541b117440
nkdengineer <nkdengineer@outlook.com>
2025-02-06 15:27:53 +0700

    fix test

--- src/hooks/useNetworkWithOfflineStatus.ts
+++ src/hooks/useNetworkWithOfflineStatus.ts
@@ -1,8 +1,6 @@
 import type {MutableRefObject} from 'react';
 import {useEffect, useRef} from 'react';
-import {useOnyx} from 'react-native-onyx';
 import DateUtils from '@libs/DateUtils';
-import ONYXKEYS from '@src/ONYXKEYS';
 import useLocalize from './useLocalize';
 import useNetwork from './useNetwork';
 import usePrevious from './usePrevious';
@@ -10,14 +8,12 @@ import usePrevious from './usePrevious';
 type UseNetworkWithOfflineStatus = {isOffline: boolean; lastOfflineAt: MutableRefObject<Date | undefined>; lastOnlineAt: MutableRefObject<Date | undefined>};
 
 export default function useNetworkWithOfflineStatus(): UseNetworkWithOfflineStatus {
-    const {isOffline} = useNetwork();
+    const {isOffline, lastOfflineAt: lastOfflineAtFromOnyx} = useNetwork();
     const prevIsOffline = usePrevious(isOffline);
     const {preferredLocale} = useLocalize();
 
-    const [network] = useOnyx(ONYXKEYS.NETWORK);
-
     // The last time/date the user went/was offline. If the user was never offline, it is set to undefined.
-    const lastOfflineAt = useRef(isOffline ? network?.lastOfflineAt : undefined);
+    const lastOfflineAt = useRef(isOffline ? lastOfflineAtFromOnyx : undefined);
 
     // The last time/date the user went/was online. If the user was never online, it is set to undefined.
     const lastOnlineAt = useRef(isOffline ? undefined : DateUtils.getLocalDateFromDatetime(preferredLocale));
