30f5ed33651188f429032e3e9efe10183adc1eae
VickyStash <vikstash@gmail.com>
2025-02-06 12:44:08 +0100

    Use SectionList to display debug report actions. Add search and amount showing

--- src/pages/Debug/Report/DebugReportActions.tsx
+++ src/pages/Debug/Report/DebugReportActions.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useMemo, useState} from 'react';
 import {useOnyx} from 'react-native-onyx';
 import Button from '@components/Button';
-import {PressableWithFeedback} from '@components/Pressable';
 import ScrollView from '@components/ScrollView';
+import SelectionList from '@components/SelectionList';
+import RadioListItem from '@components/SelectionList/RadioListItem';
 import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -11,7 +12,6 @@ import * as ReportActionsUtils from '@libs/ReportActionsUtils';
 import * as ReportUtils from '@libs/ReportUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
-import type {ReportAction} from '@src/types/onyx';
 
 type DebugReportActionsProps = {
     reportID: string;
@@ -20,6 +20,7 @@ type DebugReportActionsProps = {
 function DebugReportActions({reportID}: DebugReportActionsProps) {
     const {translate, datetimeToCalendarTime} = useLocalize();
     const styles = useThemeStyles();
+    const [searchValue, setSearchValue] = useState('');
     const [report] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`);
     const canUserPerformWriteAction = ReportUtils.canUserPerformWriteAction(report);
     const [sortedAllReportActions] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`, {
@@ -27,31 +28,34 @@ function DebugReportActions({reportID}: DebugReportActionsProps) {
         selector: (allReportActions) => ReportActionsUtils.getSortedReportActionsForDisplay(allReportActions, canUserPerformWriteAction, true),
     });
 
-    const renderItem = (item: ReportAction, index: number) => (
-        <PressableWithFeedback
-            accessibilityLabel={translate('common.details')}
-            onPress={() => Navigation.navigate(ROUTES.DEBUG_REPORT_ACTION.getRoute(reportID, item.reportActionID))}
-            style={({pressed}) => [styles.flexRow, styles.justifyContentBetween, pressed && styles.hoveredComponentBG, styles.p4]}
-            hoverStyle={styles.hoveredComponentBG}
-            key={index}
-        >
-            <Text>{item.reportActionID}</Text>
-            <Text style={styles.textLabelSupporting}>{datetimeToCalendarTime(item.created, false, false)}</Text>
-        </PressableWithFeedback>
-    );
+    const searchedReportActions = useMemo(() => {
+        return (sortedAllReportActions ?? [])
+            .filter((reportAction) => reportAction.reportActionID.includes(searchValue))
+            .map((reportAction) => ({
+                reportActionID: reportAction.reportActionID,
+                text: reportAction.reportActionID,
+                rightElement: <Text style={styles.textLabelSupporting}>{datetimeToCalendarTime(reportAction.created, false, false)}</Text>,
+            }));
+    }, [sortedAllReportActions, searchValue, datetimeToCalendarTime, styles.textLabelSupporting]);
 
     return (
-        <ScrollView style={styles.mv5}>
+        <ScrollView style={styles.mv3}>
             <Button
                 success
                 large
                 text={translate('common.create')}
                 onPress={() => Navigation.navigate(ROUTES.DEBUG_REPORT_ACTION_CREATE.getRoute(reportID))}
-                style={[styles.pb5, styles.ph3]}
+                style={[styles.pb3, styles.ph3]}
             />
-            {/* This list was previously rendered as a FlatList, but it turned out that it caused the component to flash in some cases,
-            so it was replaced by this solution. */}
-            {sortedAllReportActions?.map((item, index) => renderItem(item, index))}
+            <SelectionList
+                sections={[{title: `Total Amount: ${searchedReportActions.length}`, data: searchedReportActions}]} // TODO: add translation for the Total Amount: text
+                listItemTitleStyles={styles.fontWeightNormal}
+                textInputValue={searchValue}
+                textInputLabel={translate('common.search')}
+                onChangeText={setSearchValue}
+                onSelectRow={(item) => Navigation.navigate(ROUTES.DEBUG_REPORT_ACTION.getRoute(reportID, item.reportActionID))}
+                ListItem={RadioListItem}
+            />
         </ScrollView>
     );
 }
