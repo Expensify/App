f4a8f96c2673f4ad315834cfd11a3bb5fdeb2b13
Mykhailo Kravchenko <rezkiy37@gmail.com>
2025-02-06 16:28:20 +0100

    revert useLHNEstimatedListSize

--- src/hooks/useLHNEstimatedListSize/index.ts
+++ src/hooks/useLHNEstimatedListSize/index.ts
@@ -1,18 +1,8 @@
-import useWindowDimensions from '@hooks/useWindowDimensions';
-import variables from '@styles/variables';
 import type UseLHNEstimatedListSize from './types';
 
 /**
- * This a native specific implementation for FlashList of LHNOptionsList. It calculates estimated visible height and width of the list. It is not the scroll content size. Defining this prop will enable the list to be rendered immediately. Without it, the list first needs to measure its size, leading to a small delay during the first render.
+ * This is the web implementation. It is intentionally left unimplemented because it does not function correctly on the web.
  */
-const useLHNEstimatedListSize: UseLHNEstimatedListSize = () => {
-    const {windowHeight, windowWidth} = useWindowDimensions();
-    const listHeight = windowHeight - variables.bottomTabHeight;
-
-    return {
-        height: listHeight,
-        width: windowWidth,
-    };
-};
+const useLHNEstimatedListSize: UseLHNEstimatedListSize = () => undefined;
 
 export default useLHNEstimatedListSize;
