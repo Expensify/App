1930d6f878f17454c9a3db4daa93d2f11292363a
FitseTLT <engfitsum@gmail.com>
2025-02-06 17:16:03 +0300

    minor fix

--- src/pages/workspace/WorkspaceUpdateToUSDModal.tsx
+++ src/pages/workspace/WorkspaceUpdateToUSDModal.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import React, {useEffect} from 'react';
 import type {ConfirmModalProps} from '@components/ConfirmModal';
 import ConfirmModal from '@components/ConfirmModal';
 import CONST from '@src/CONST';
