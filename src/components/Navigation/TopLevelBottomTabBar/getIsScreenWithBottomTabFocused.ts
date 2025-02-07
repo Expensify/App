7afde6e76a42c9f52593f0123c15615c4cdeeede
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-02-07 16:25:54 +0100

    fix commnet

--- src/components/Navigation/TopLevelBottomTabBar/getIsScreenWithBottomTabFocused.ts
+++ src/components/Navigation/TopLevelBottomTabBar/getIsScreenWithBottomTabFocused.ts
@@ -6,7 +6,7 @@ import SCREENS_WITH_BOTTOM_TAB_BAR from './SCREENS_WITH_BOTTOM_TAB_BAR';
 function getIsBottomTabVisibleDirectly(state: NavigationState) {
     const focusedRoute = findFocusedRoute(state);
 
-    // We are checking if the focused route is a split navigator because there may be a brief moment where the navigator don't have state yet.
+    // We are checking if the focused route is a split navigator because there may be a brief moment where the navigator doesn't have state yet.
     // That mens we don't have screen with bottom tab focused. This caused glitching.
     return SCREENS_WITH_BOTTOM_TAB_BAR.includes(focusedRoute?.name ?? '') || isSplitNavigatorName(focusedRoute?.name);
 }
