1a64acabfdd9d4bc6bcd27b663c43f818626c116
Carlos Martins <cmartins@expensify.com>
2025-02-07 08:55:36 -0800

    update get language

--- src/libs/LocaleUtils.ts
+++ src/libs/LocaleUtils.ts
@@ -9,6 +9,28 @@ function getLanguageFromLocale(locale: ValueOf<typeof CONST.LOCALES>): TupleToUn
             return CONST.LOCALES.ES;
         case CONST.LOCALES.EN:
             return CONST.LOCALES.EN;
+        case CONST.LOCALES.PT:
+            return CONST.LOCALES.PT;
+        case CONST.LOCALES.IT:
+            return CONST.LOCALES.IT;
+        case CONST.LOCALES.DE:
+            return CONST.LOCALES.DE;
+        case CONST.LOCALES.FR:
+            return CONST.LOCALES.FR;
+        case CONST.LOCALES.NL:
+            return CONST.LOCALES.NL;
+        case CONST.LOCALES.PL:
+            return CONST.LOCALES.PL;
+        case CONST.LOCALES.RU:
+            return CONST.LOCALES.RU;
+        case CONST.LOCALES.TR:
+            return CONST.LOCALES.TR;
+        case CONST.LOCALES.KO:
+            return CONST.LOCALES.KO;
+        case CONST.LOCALES.CH:
+            return CONST.LOCALES.CH;
+        case CONST.LOCALES.JA:
+            return CONST.LOCALES.JA;
         default:
             return CONST.LOCALES.DEFAULT;
     }
