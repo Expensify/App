670bbb22b4aa46d5f4dc5d9838fdf580db2beab6
burczu <b.dybowski@gmail.com>
2025-02-07 14:34:42 +0100

    new version of signer params

--- src/libs/API/parameters/SaveCorpayOnboardingDirectorInformationParams.ts
+++ src/libs/API/parameters/SaveCorpayOnboardingDirectorInformationParams.ts
@@ -1,30 +1,37 @@
 import type {FileObject} from '@components/AttachmentModal';
 
-type SaveCorpayOnboardingCompanyDirector = {
-    signerFullName: string;
-    signerDateOfBirth: string;
-    signerJobTitle: string;
-    signerEmail: string;
-    signerCompleteResidentialAddress: string;
-};
-
-type SaveCorpayOnboardingDirectorInformation = {
-    companyDirectors: SaveCorpayOnboardingCompanyDirector[];
-    proofOfDirectors?: FileObject[] | string;
-    copyOfID?: FileObject[] | string;
-    addressProof?: FileObject[] | string;
-    codiceProof?: FileObject[] | string;
-    pdsAndFSG?: FileObject[] | string;
-};
+// type SaveCorpayOnboardingCompanyDirector = {
+//     signerFullName: string;
+//     signerDateOfBirth: string;
+//     signerJobTitle: string;
+//     signerEmail: string;
+//     signerCompleteResidentialAddress: string;
+// };
+//
+// type SaveCorpayOnboardingDirectorInformation = {
+//     companyDirectors: SaveCorpayOnboardingCompanyDirector[];
+//     proofOfDirectors?: FileObject[] | string;
+//     copyOfID?: FileObject[] | string;
+//     addressProof?: FileObject[] | string;
+//     codiceProof?: FileObject[] | string;
+//     pdsAndFSG?: FileObject[] | string;
+// };
+//
+// type SaveCorpayOnboardingDirectorInformationParams = {
+//     inputs: string;
+//     bankAccountID: number;
+//     proofOfDirectors?: FileObject[] | string;
+//     copyOfID?: FileObject[] | string;
+//     addressProof?: FileObject[] | string;
+//     codiceProof?: FileObject[] | string;
+//     pdsAndFSG?: FileObject[] | string;
+// };
 
 type SaveCorpayOnboardingDirectorInformationParams = {
     inputs: string;
+    directorIDs?: string;
+    proofOfDirectors?: FileObject[];
     bankAccountID: number;
-    proofOfDirectors?: FileObject[] | string;
-    copyOfID?: FileObject[] | string;
-    addressProof?: FileObject[] | string;
-    codiceProof?: FileObject[] | string;
-    pdsAndFSG?: FileObject[] | string;
 };
 
-export type {SaveCorpayOnboardingDirectorInformation, SaveCorpayOnboardingDirectorInformationParams};
+export default SaveCorpayOnboardingDirectorInformationParams;
