6106e7779bd7fb89e90e12302edd0a64d982af93
dominictb <tb-dominic@outlook.com>
2025-02-10 03:18:55 +0700

    fix: optimistic transaction violations are cleared when fixing a violation if they had receipt required violation

--- src/libs/Violations/ViolationsUtils.ts
+++ src/libs/Violations/ViolationsUtils.ts
@@ -230,7 +230,7 @@ const ViolationsUtils = {
         const shouldDisplayFutureDateViolation = !isInvoiceTransaction && DateUtils.isFutureDay(inputDate) && isControlPolicy;
         const hasReceiptRequiredViolation = transactionViolations.some((violation) => violation.name === 'receiptRequired');
         const hasOverLimitViolation = transactionViolations.some((violation) => violation.name === 'overLimit');
-        const amount = updatedTransaction.modifiedAmount ?? updatedTransaction.amount;
+        const amount = updatedTransaction.modifiedAmount || updatedTransaction.amount;
         const shouldShowReceiptRequiredViolation =
             !isInvoiceTransaction &&
             policy.maxExpenseAmountNoReceipt &&
