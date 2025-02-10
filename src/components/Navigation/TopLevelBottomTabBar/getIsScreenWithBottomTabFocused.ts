6a5bb49d52f078b878e66c17a8765ad1b165b94d
Wojciech Boman <wojciechboman@gmail.com>
2025-02-10 11:15:51 +0100

    Fix function name in getIsScreenWithBottomTabFocused

--- src/components/Navigation/TopLevelBottomTabBar/getIsScreenWithBottomTabFocused.ts
+++ src/components/Navigation/TopLevelBottomTabBar/getIsScreenWithBottomTabFocused.ts
@@ -3,7 +3,7 @@ import type {NavigationState} from '@react-navigation/native';
 import {isSplitNavigatorName} from '@libs/Navigation/helpers/isNavigatorName';
 import SCREENS_WITH_BOTTOM_TAB_BAR from './SCREENS_WITH_BOTTOM_TAB_BAR';
 
-function getIsBottomTabVisibleDirectly(state: NavigationState) {
+function getIsScreenWithBottomTabFocused(state: NavigationState) {
     const focusedRoute = findFocusedRoute(state);
 
     // We are checking if the focused route is a split navigator because there may be a brief moment where the navigator doesn't have state yet.
@@ -11,4 +11,4 @@ function getIsBottomTabVisibleDirectly(state: NavigationState) {
     return SCREENS_WITH_BOTTOM_TAB_BAR.includes(focusedRoute?.name ?? '') || isSplitNavigatorName(focusedRoute?.name);
 }
 
-export default getIsBottomTabVisibleDirectly;
+export default getIsScreenWithBottomTabFocused;
