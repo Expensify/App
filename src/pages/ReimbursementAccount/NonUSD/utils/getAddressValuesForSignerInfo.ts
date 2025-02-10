45c65cb4ba08f335fbe3f76cd30275c7ff93ad1a
burczu <b.dybowski@gmail.com>
2025-02-10 14:21:22 +0100

    getting address values fixed

--- src/pages/ReimbursementAccount/NonUSD/utils/getAddressValuesForSignerInfo.ts
+++ src/pages/ReimbursementAccount/NonUSD/utils/getAddressValuesForSignerInfo.ts
@@ -1,4 +1,5 @@
 import type {OnyxEntry} from 'react-native-onyx';
+import CONST from '@src/CONST';
 import type {ReimbursementAccountForm} from '@src/types/form';
 
 type SignerInfoAddressValues = {
@@ -7,6 +8,8 @@ type SignerInfoAddressValues = {
     state: string;
     zipCode: string;
 };
+
+const {STREET, CITY, STATE, ZIP_CODE} = CONST.NON_USD_BANK_ACCOUNT.SIGNER_INFO_STEP.SIGNER_INFO_DATA;
 
 function getAddressValuesForSignerInfo(prefix: string, reimbursementAccountDraft: OnyxEntry<ReimbursementAccountForm>): SignerInfoAddressValues {
     if (!reimbursementAccountDraft) {
@@ -19,10 +22,10 @@ function getAddressValuesForSignerInfo(prefix: string, reimbursementAccountDraft
     }
 
     return {
-        street: reimbursementAccountDraft[`${prefix}_street`],
-        city: reimbursementAccountDraft[`${prefix}_city`],
-        state: reimbursementAccountDraft[`${prefix}_state`],
-        zipCode: reimbursementAccountDraft[`${prefix}_zipCode`],
+        street: reimbursementAccountDraft[STREET],
+        city: reimbursementAccountDraft[CITY],
+        state: reimbursementAccountDraft[STATE],
+        zipCode: reimbursementAccountDraft[ZIP_CODE],
     };
 }
 
