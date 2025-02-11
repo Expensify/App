1e4620fbba55b1ccfc6da9616ff1b9a72fa52bff
Blazej Kustra <blazej.kustra@swmansion.com>
2025-02-11 22:00:03 +0100

    Fix trip summary for nested threads

--- src/pages/home/report/PureReportActionItem.tsx
+++ src/pages/home/report/PureReportActionItem.tsx
@@ -139,6 +139,7 @@ import ReportActionItemMessageEdit from './ReportActionItemMessageEdit';
 import ReportActionItemSingle from './ReportActionItemSingle';
 import ReportActionItemThread from './ReportActionItemThread';
 import ReportAttachmentsContext from './ReportAttachmentsContext';
+import TripSummary from './TripSummary';
 
 type PureReportActionItemProps = {
     /** Report for this action */
@@ -1072,6 +1073,11 @@ function PureReportActionItem({
             />
         );
     }
+
+    if (isTripPreview(action) && isThreadReportParentAction) {
+        return <TripSummary reportID={getOriginalMessage(action)?.linkedReportID} />;
+    }
+
     if (isChronosOOOListAction(action)) {
         return (
             <ChronosOOOListActions
