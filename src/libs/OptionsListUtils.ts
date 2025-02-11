9c409e0f946fac5afb1123056044960aee9cb053
nkdengineer <nkdengineer@outlook.com>
2025-02-11 14:19:11 +0700

    fix create expense flow regression

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -163,6 +163,7 @@ type PayeePersonalDetails = {
     login: string;
     accountID: number;
     keyForList: string;
+    isInteractive: boolean;
 };
 
 type SectionBase = {
@@ -1627,6 +1628,7 @@ function getIOUConfirmationOptionsFromPayeePersonalDetail(personalDetail: OnyxEn
         login: personalDetail?.login ?? '',
         accountID: personalDetail?.accountID ?? CONST.DEFAULT_NUMBER_ID,
         keyForList: String(personalDetail?.accountID ?? CONST.DEFAULT_NUMBER_ID),
+        isInteractive: false,
     };
 }
 
