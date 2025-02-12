e754b44cb426aa84a4a088e60ea7f26cf6748a68
Huu Le <20178761+huult@users.noreply.github.com>
2025-02-12 21:43:36 +0700

    Revert PR #56316

--- src/hooks/useResetComposerFocus.ts
+++ src/hooks/useResetComposerFocus.ts
@@ -1,8 +1,8 @@
 import {useIsFocused} from '@react-navigation/native';
 import type {MutableRefObject} from 'react';
 import {useEffect, useRef} from 'react';
+import {InteractionManager} from 'react-native';
 import type {TextInput} from 'react-native';
-import inputFocusResetHandler from '@libs/InputFocusResetHandler';
 
 export default function useResetComposerFocus(inputRef: MutableRefObject<TextInput | null>) {
     const isFocused = useIsFocused();
@@ -12,8 +12,13 @@ export default function useResetComposerFocus(inputRef: MutableRefObject<TextInp
         if (!isFocused || !shouldResetFocusRef.current) {
             return;
         }
-
-        inputFocusResetHandler.handleInputFocusReset(inputRef, shouldResetFocusRef);
+        const interactionTask = InteractionManager.runAfterInteractions(() => {
+            inputRef.current?.focus(); // focus input again
+            shouldResetFocusRef.current = false;
+        });
+        return () => {
+            interactionTask.cancel();
+        };
     }, [isFocused, inputRef]);
 
     return {isFocused, shouldResetFocusRef};
