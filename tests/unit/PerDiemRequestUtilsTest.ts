e0ec1a47d5290e3de2b03bb5de33f347da643fd8
Ni Wayan Devi <wayandevi813@gmail.com>
2025-02-13 17:58:47 +0800

    getDestinationListSections unit test

---
+++ tests/unit/PerDiemRequestUtilsTest.ts
@@ -0,0 +1,50 @@
+import {DestinationTreeSection, getDestinationListSections} from '@libs/PerDiemRequestUtils';
+import {Rate} from '@src/types/onyx/Policy';
+
+describe('PerDiemRequestUtils', () => {
+    it('getDestinationListSections()', () => {
+        const tokenizeSearch = 'Antigua Barbuda';
+
+        const destinations: Rate[] = [
+            {
+                currency: 'EUR',
+                customUnitRateID: 'Afghanistan',
+                enabled: true,
+                name: 'Afghanistan',
+                rate: 0,
+            },
+            {
+                currency: 'EUR',
+                customUnitRateID: 'Antigua and Barbuda',
+                enabled: true,
+                name: 'Antigua and Barbuda',
+                rate: 0,
+            },
+        ];
+
+        const searchResultList: DestinationTreeSection[] = [
+            {
+                data: [
+                    {
+                        currency: 'EUR',
+                        isDisabled: false,
+                        isSelected: false,
+                        keyForList: 'Antigua and Barbuda',
+                        searchText: 'Antigua and Barbuda',
+                        text: 'Antigua and Barbuda',
+                        tooltipText: 'Antigua and Barbuda',
+                    },
+                ],
+                indexOffset: 1,
+                shouldShow: true,
+                title: '',
+            },
+        ];
+
+        const tokenizeSearchResult = getDestinationListSections({
+            destinations: destinations,
+            searchValue: tokenizeSearch,
+        });
+        expect(tokenizeSearchResult).toStrictEqual(searchResultList);
+    });
+});
