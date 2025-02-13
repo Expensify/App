1b0dead83dfbda8afd8fdd2d2ed0da2947eb99e2
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-02-13 15:45:15 +0800

    fix missing top inset

--- src/components/Search/SearchRouter/SearchRouterModal.tsx
+++ src/components/Search/SearchRouter/SearchRouterModal.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import FocusTrapForModal from '@components/FocusTrap/FocusTrapForModal';
 import Modal from '@components/Modal';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
+import useSafeAreaInsets from '@hooks/useSafeAreaInsets';
 import useViewportOffsetTop from '@hooks/useViewportOffsetTop';
 import {isMobileChrome, isMobileSafari} from '@libs/Browser';
 import CONST from '@src/CONST';
@@ -14,6 +15,7 @@ function SearchRouterModal() {
     const {shouldUseNarrowLayout} = useResponsiveLayout();
     const {isSearchRouterDisplayed, closeSearchRouter} = useSearchRouterContext();
     const viewportOffsetTop = useViewportOffsetTop();
+    const safeAreaInsets = useSafeAreaInsets();
 
     // On mWeb Safari, the input caret stuck for a moment while the modal is animating. So, we hide the caret until the animation is done.
     const [shouldHideInputCaret, setShouldHideInputCaret] = useState(isMobileWebSafari);
@@ -24,7 +26,7 @@ function SearchRouterModal() {
         <Modal
             type={modalType}
             isVisible={isSearchRouterDisplayed}
-            innerContainerStyle={{paddingTop: viewportOffsetTop}}
+            innerContainerStyle={{paddingTop: safeAreaInsets.top + viewportOffsetTop}}
             popoverAnchorPosition={{right: 6, top: 6}}
             fullscreen
             propagateSwipe
