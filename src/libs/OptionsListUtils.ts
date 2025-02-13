a4f6257108758a64ce819e731977ca3f3c067645
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-02-13 10:31:00 +0100

    revert subtitle logic in getReportOption

--- src/libs/OptionsListUtils.ts
+++ src/libs/OptionsListUtils.ts
@@ -40,7 +40,7 @@ import Parser from './Parser';
 import Performance from './Performance';
 import {getDisplayNameOrDefault} from './PersonalDetailsUtils';
 import {addSMSDomainIfPhoneNumber, parsePhoneNumber} from './PhoneNumber';
-import {canSendInvoiceFromWorkspace} from './PolicyUtils';
+import {canSendInvoiceFromWorkspace, getSubmitToAccountID} from './PolicyUtils';
 import {
     getCombinedReportActions,
     getExportIntegrationLastMessageText,
@@ -884,7 +884,10 @@ function getReportOption(participant: Participant): OptionData {
         option.alternateText = translateLocal('workspace.common.workspace');
 
         if (report?.policyID) {
-            const subtitle = getChatRoomSubtitle(report, {isCreateExpenseFlow: true});
+            const policy = allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${report.policyID}`];
+            const submitToAccountID = getSubmitToAccountID(policy, report);
+            const submitsToAccountDetails = allPersonalDetails?.[submitToAccountID];
+            const subtitle = submitsToAccountDetails?.displayName ?? submitsToAccountDetails?.login;
 
             if (subtitle) {
                 option.alternateText = translateLocal('iou.submitsTo', {name: subtitle ?? ''});
