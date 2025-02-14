f345fc149f8ae79813a31a12e35bd5f8dd7a1b1e
nkdengineer <161821005+nkdengineer@users.noreply.github.com>
2025-02-14 08:39:08 +0700

    Update src/libs/Navigation/navigateAfterInteraction/index.ios.ts
    Co-authored-by: Vit Horacek <36083550+mountiny@users.noreply.github.com>

--- src/libs/Navigation/navigateAfterInteraction/index.ios.ts
+++ src/libs/Navigation/navigateAfterInteraction/index.ios.ts
@@ -2,7 +2,7 @@ import {InteractionManager} from 'react-native';
 import Navigation from '@libs/Navigation/Navigation';
 
 /**
- * On IOS, the navigation transition can sometimes break other animations, such as the closing modal.
+ * On iOS, the navigation transition can sometimes break other animations, such as the closing modal.
  * In this case we need to wait for the animation to be complete before executing the navigation
  */
 function navigateAfterInteraction(callback: () => void) {
