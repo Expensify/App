821856e7371c5559cbdb58acaa0a026ad05290ab
Taras Perun <terry.perun@gmail.com>
2025-02-14 16:52:44 +0100

    memo web SelectionList

--- src/components/SelectionList/index.tsx
+++ src/components/SelectionList/index.tsx
@@ -1,6 +1,7 @@
-import React, {forwardRef, useEffect, useState} from 'react';
+import React, {forwardRef, useCallback, useEffect, useRef, useState} from 'react';
 import type {ForwardedRef} from 'react';
 import {Keyboard} from 'react-native';
+import type {NativeScrollEvent, NativeSyntheticEvent} from 'react-native';
 import * as Browser from '@libs/Browser';
 import * as DeviceCapabilities from '@libs/DeviceCapabilities';
 import CONST from '@src/CONST';
@@ -9,10 +10,10 @@ import type {BaseSelectionListProps, ListItem, SelectionListHandle} from './type
 
 function SelectionList<TItem extends ListItem>({onScroll, ...props}: BaseSelectionListProps<TItem>, ref: ForwardedRef<SelectionListHandle>) {
     const [isScreenTouched, setIsScreenTouched] = useState(false);
+    const [shouldDebounceScrolling, setShouldDebounceScrolling] = useState(false);
 
-    const touchStart = () => setIsScreenTouched(true);
-    const touchEnd = () => setIsScreenTouched(false);
-
+    const touchStart = useCallback(() => setIsScreenTouched(true), []);
+    const touchEnd = useCallback(() => setIsScreenTouched(false), []);
     useEffect(() => {
         if (!DeviceCapabilities.canUseTouchScreen()) {
             return;
@@ -20,18 +21,16 @@ function SelectionList<TItem extends ListItem>({onScroll, ...props}: BaseSelecti
 
         // We're setting `isScreenTouched` in this listener only for web platforms with touchscreen (mWeb) where
         // we want to dismiss the keyboard only when the list is scrolled by the user and not when it's scrolled programmatically.
-        document.addEventListener('touchstart', touchStart);
-        document.addEventListener('touchend', touchEnd);
+        document.addEventListener('touchstart', touchStart, {passive: true});
+        document.addEventListener('touchend', touchEnd, {passive: true});
 
         return () => {
             document.removeEventListener('touchstart', touchStart);
             document.removeEventListener('touchend', touchEnd);
         };
-    }, []);
+    }, [touchStart, touchEnd]);
 
-    const [shouldDebounceScrolling, setShouldDebounceScrolling] = useState(false);
-
-    const checkShouldDebounceScrolling = (event: KeyboardEvent) => {
+    const checkShouldDebounceScrolling = useCallback((event: KeyboardEvent) => {
         if (!event) {
             return;
         }
@@ -44,7 +43,7 @@ function SelectionList<TItem extends ListItem>({onScroll, ...props}: BaseSelecti
         ) {
             setShouldDebounceScrolling(event.type === 'keydown');
         }
-    };
+    }, []);
 
     useEffect(() => {
         document.addEventListener('keydown', checkShouldDebounceScrolling, {passive: true});
@@ -54,23 +53,29 @@ function SelectionList<TItem extends ListItem>({onScroll, ...props}: BaseSelecti
             document.removeEventListener('keydown', checkShouldDebounceScrolling);
             document.removeEventListener('keyup', checkShouldDebounceScrolling);
         };
-    }, []);
+    }, [checkShouldDebounceScrolling]);
 
     // In SearchPageBottomTab we use useAnimatedScrollHandler from reanimated(for performance reasons) and it returns object instead of function. In that case we cannot change it to a function call, that's why we have to choose between onScroll and defaultOnScroll.
-    const defaultOnScroll = () => {
-        // Only dismiss the keyboard whenever the user scrolls the screen
-        if (!isScreenTouched) {
-            return;
-        }
-        Keyboard.dismiss();
-    };
+    const defaultOnScroll = useCallback(
+        (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+            if (onScroll) {
+                return onScroll(e);
+            }
+            // Only dismiss the keyboard whenever the user scrolls the screen
+            if (!isScreenTouched) {
+                return;
+            }
+            Keyboard.dismiss();
+        },
+        [isScreenTouched, onScroll],
+    );
 
     return (
         <BaseSelectionList
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
             ref={ref}
-            onScroll={onScroll ?? defaultOnScroll}
+            onScroll={defaultOnScroll}
             // Ignore the focus if it's caused by a touch event on mobile chrome.
             // For example, a long press will trigger a focus event on mobile chrome.
             shouldIgnoreFocus={Browser.isMobileChrome() && isScreenTouched}
