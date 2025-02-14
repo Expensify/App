91758837e8e72a6c178eeabdf7fb38f8269651f7
Taras Perun <terry.perun@gmail.com>
2025-02-14 17:05:21 +0100

    highlightBackgroundStyle

--- src/hooks/useAnimatedHighlightStyle/index.ts
+++ src/hooks/useAnimatedHighlightStyle/index.ts
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {InteractionManager} from 'react-native';
-import {Easing, interpolate, interpolateColor, runOnJS, useAnimatedStyle, useSharedValue, withDelay, withSequence, withTiming} from 'react-native-reanimated';
+import {cancelAnimation, Easing, interpolate, interpolateColor, runOnJS, useAnimatedStyle, useSharedValue, withDelay, withSequence, withTiming} from 'react-native-reanimated';
 import useScreenWrapperTranstionStatus from '@hooks/useScreenWrapperTransitionStatus';
 import useTheme from '@hooks/useTheme';
 import CONST from '@src/CONST';
@@ -88,6 +88,9 @@ export default function useAnimatedHighlightStyle({
             return;
         }
         setStartHighlight(false);
+        // nonRepeatableProgress.set(0);
+        // repeatableProgress.set(0);
+
         InteractionManager.runAfterInteractions(() => {
             runOnJS(() => {
                 nonRepeatableProgress.set(
@@ -109,6 +112,11 @@ export default function useAnimatedHighlightStyle({
                 );
             })();
         });
+
+        return () => {
+            cancelAnimation(nonRepeatableProgress);
+            cancelAnimation(repeatableProgress);
+        };
     }, [
         didScreenTransitionEnd,
         startHighlight,
