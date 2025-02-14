c30bcf494d406a606b9f3f9c0138184e4e169c64
Taras Perun <terry.perun@gmail.com>
2025-02-14 18:10:17 +0100

    BaseSelectionListItemRenderer

--- src/components/SelectionList/BaseSelectionListItemRenderer.tsx
+++ src/components/SelectionList/BaseSelectionListItemRenderer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import type {NativeSyntheticEvent, StyleProp, TextStyle, ViewStyle} from 'react-native';
 import type useArrowKeyFocusManager from '@hooks/useArrowKeyFocusManager';
 import type useSingleExecution from '@hooks/useSingleExecution';
@@ -45,12 +45,45 @@ function BaseSelectionListItemRenderer<TItem extends ListItem>({
     singleExecution,
     titleContainerStyles,
 }: BaseSelectionListItemRendererProps<TItem>) {
-    const handleOnCheckboxPress = () => {
+    const handleOnCheckboxPress = useCallback(() => {
         if (isReportListItemType(item)) {
             return onCheckboxPress;
         }
         return onCheckboxPress ? () => onCheckboxPress(item) : undefined;
-    };
+    }, [item, onCheckboxPress]);
+
+    // Memoized row selection handler
+    const handleSelectRow = useCallback(() => {
+        if (shouldSingleExecuteRowSelect) {
+            singleExecution(() => selectRow(item, index))();
+        } else {
+            selectRow(item, index);
+        }
+    }, [shouldSingleExecuteRowSelect, singleExecution, selectRow, item, index]);
+
+    // Memoized error dismiss handler
+    const handleDismissError = useCallback(() => {
+        onDismissError?.(item);
+    }, [onDismissError, item]);
+    const onHandleLongPressRow = useCallback(() => {
+        onLongPressRow?.(item);
+    }, [onLongPressRow, item]);
+
+    // Memoized focus handler
+    const handleFocus = useCallback(
+        (event: NativeSyntheticEvent<ExtendedTargetedEvent>) => {
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            if (shouldIgnoreFocus || isDisabled) {
+                return;
+            }
+            // Prevent unexpected scrolling on mobile Chrome after the context menu closes by ignoring programmatic focus not triggered by direct user interaction.
+            if (isMobileChrome() && event.nativeEvent && !event.nativeEvent.sourceCapabilities) {
+                return;
+            }
+            setFocusedIndex(normalizedIndex);
+        },
+        [shouldIgnoreFocus, isDisabled, normalizedIndex, setFocusedIndex],
+    );
 
     return (
         <>
@@ -60,46 +93,29 @@ function BaseSelectionListItemRenderer<TItem extends ListItem>({
                 isDisabled={isDisabled}
                 showTooltip={showTooltip}
                 canSelectMultiple={canSelectMultiple}
-                onLongPressRow={onLongPressRow}
-                onSelectRow={() => {
-                    if (shouldSingleExecuteRowSelect) {
-                        singleExecution(() => selectRow(item, index))();
-                    } else {
-                        selectRow(item, index);
-                    }
-                }}
+                onLongPressRow={onHandleLongPressRow}
+                onSelectRow={handleSelectRow}
                 onCheckboxPress={handleOnCheckboxPress()}
-                onDismissError={() => onDismissError?.(item)}
+                onDismissError={handleDismissError}
                 shouldPreventDefaultFocusOnSelectRow={shouldPreventDefaultFocusOnSelectRow}
-                // We're already handling the Enter key press in the useKeyboardShortcut hook, so we don't want the list item to submit the form
                 shouldPreventEnterKeySubmit
                 rightHandSideComponent={rightHandSideComponent}
                 keyForList={item.keyForList ?? ''}
                 isMultilineSupported={isMultilineSupported}
                 isAlternateTextMultilineSupported={isAlternateTextMultilineSupported}
                 alternateTextNumberOfLines={alternateTextNumberOfLines}
-                onFocus={(event: NativeSyntheticEvent<ExtendedTargetedEvent>) => {
-                    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-                    if (shouldIgnoreFocus || isDisabled) {
-                        return;
-                    }
-                    // Prevent unexpected scrolling on mobile Chrome after the context menu closes by ignoring programmatic focus not triggered by direct user interaction.
-                    if (isMobileChrome() && event.nativeEvent && !event.nativeEvent.sourceCapabilities) {
-                        return;
-                    }
-                    setFocusedIndex(normalizedIndex);
-                }}
+                onFocus={handleFocus}
                 shouldSyncFocus={shouldSyncFocus}
                 shouldHighlightSelectedItem={shouldHighlightSelectedItem}
                 wrapperStyle={wrapperStyle}
                 titleStyles={titleStyles}
                 titleContainerStyles={titleContainerStyles}
             />
-            {item.footerContent && item.footerContent}
+            {!!item.footerContent && item.footerContent}
         </>
     );
 }
 
 BaseSelectionListItemRenderer.displayName = 'BaseSelectionListItemRenderer';
 
-export default BaseSelectionListItemRenderer;
+export default React.memo(BaseSelectionListItemRenderer);
