7954cbbf0964f60cd8872015dcdec7a9a50696e5
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-02-14 14:58:57 +0100

    fix goBack on per diem flow

--- src/pages/iou/request/step/IOURequestStepTime.tsx
+++ src/pages/iou/request/step/IOURequestStepTime.tsx
@@ -62,7 +62,13 @@ function IOURequestStepTime({
             Navigation.goBack(backTo);
             return;
         }
-        Navigation.goBack(ROUTES.MONEY_REQUEST_STEP_DESTINATION.getRoute(action, iouType, transactionID, reportID));
+
+        if (transaction?.isFromGlobalCreate) {
+            Navigation.goBack(ROUTES.MONEY_REQUEST_STEP_DESTINATION.getRoute(action, iouType, transactionID, reportID));
+            return;
+        }
+
+        Navigation.goBack(ROUTES.MONEY_REQUEST_CREATE_TAB_PER_DIEM.getRoute(action, iouType, transactionID, reportID));
     };
 
     const validate = (value: FormOnyxValues<typeof ONYXKEYS.FORMS.MONEY_REQUEST_TIME_FORM>) => {
