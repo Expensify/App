7e7ab312634034900d5c32134f92b291dc1db53b
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-02-18 18:52:10 +0530

    Changed code according to suggestion

--- src/components/Onfido/types.ts
+++ src/components/Onfido/types.ts
@@ -1,8 +1,7 @@
 import type {OnfidoResult} from '@onfido/react-native-sdk';
 import type {Handle} from 'onfido-sdk-ui/types/Onfido';
+import type {CompleteData} from 'onfido-sdk-ui/types/shared/SdkParameters';
 import type {OnyxEntry} from 'react-native-onyx';
-
-type CompleteData = Record<string, unknown>;
 
 type OnfidoData = CompleteData | OnfidoResult;
 
