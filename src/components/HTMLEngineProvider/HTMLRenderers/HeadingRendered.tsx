556f1956576e38fa63df927529546923e85ce9a3
krishna2323 <belivethatkg@gmail.com>
2025-02-20 03:37:16 +0530

    add Heading renderer.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

---
+++ src/components/HTMLEngineProvider/HTMLRenderers/HeadingRendered.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import {View} from 'react-native';
+import type {CustomRendererProps, TPhrasing, TText} from 'react-native-render-html';
+import {TNodeChildrenRenderer} from 'react-native-render-html';
+import * as HTMLEngineUtils from '@components/HTMLEngineProvider/htmlEngineUtils';
+import Text from '@components/Text';
+import useThemeStyles from '@hooks/useThemeStyles';
+
+function HeadingRendered({tnode}: CustomRendererProps<TText | TPhrasing>) {
+    const styles = useThemeStyles();
+    const isChildOfTaskTitle = HTMLEngineUtils.isChildOfTaskTitle(tnode);
+
+    return (
+        <TNodeChildrenRenderer
+            tnode={tnode}
+            renderChild={(props) => {
+                return (
+                    <Text
+                        style={[styles.webViewStyles.baseFontStyle, styles.h1, isChildOfTaskTitle && styles.taskTitleMenuItem]}
+                        key={props.key}
+                    >
+                        {props.childElement}
+                    </Text>
+                );
+            }}
+        />
+    );
+}
+
+HeadingRendered.displayName = 'HeadingRendered';
+
+export default HeadingRendered;
