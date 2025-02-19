79d4fd2b474f5b341bf974c379dbbc62187479d0
Antony M. Kithinzi <kithinzi97@gmail.com>
2025-02-19 14:43:18 +0000

    added tests

--- tests/ui/WorkspaceUpgradeTest.tsx
+++ tests/ui/WorkspaceUpgradeTest.tsx
@@ -4,10 +4,12 @@ import {act, fireEvent, render, screen} from '@testing-library/react-native';
 import React from 'react';
 import Onyx from 'react-native-onyx';
 import {WRITE_COMMANDS} from '@libs/API/types';
+import {convertToShortDisplayString} from '@libs/CurrencyUtils';
 import * as SequentialQueue from '@libs/Network/SequentialQueue';
 import createResponsiveStackNavigator from '@navigation/AppNavigator/createResponsiveStackNavigator';
 import type {SettingsNavigatorParamList} from '@navigation/types';
 import WorkspaceUpgradePage from '@pages/workspace/upgrade/WorkspaceUpgradePage';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import SCREENS from '@src/SCREENS';
 import type {Policy} from '@src/types/onyx';
@@ -72,5 +74,49 @@ describe('WorkspaceUpgrade', () => {
 
         // Then "Set policy rules enabled" API request should be made
         TestHelper.expectAPICommandToHaveBeenCalled(WRITE_COMMANDS.SET_POLICY_RULES_ENABLED, 1);
+    });
+
+    it("should show the upgrade corporate plan price is in the user's local currency", async () => {
+        // Team policy which the user can upgrade to corporate
+        const policy = LHNTestUtils.getFakePolicy();
+
+        // Given that a policy is initialized in Onyx
+        await Onyx.merge(`${ONYXKEYS.COLLECTION.POLICY}${policy.id}`, policy);
+
+        // Render the WorkspaceUpgradePage without initializing user's preferred currency
+        renderPage(SCREENS.WORKSPACE.UPGRADE, {policyID: policy.id});
+
+        // Expect the price to be in USD, as the user's preferred currency is not initialized
+        expect(
+            screen.getByText(
+                `${convertToShortDisplayString(
+                    CONST.SUBSCRIPTION_PRICES[CONST.PAYMENT_CARD_CURRENCY.USD][CONST.POLICY.TYPE.CORPORATE][CONST.SUBSCRIPTION.TYPE.ANNUAL],
+                    CONST.PAYMENT_CARD_CURRENCY.USD,
+                )}`,
+            ),
+        ).toBeTruthy();
+
+        // Iterate through all payment card currencies
+        for (const currency of Object.values(CONST.PAYMENT_CARD_CURRENCY)) {
+            // Format the price in the user's preferred currency
+            const price = `${convertToShortDisplayString(CONST.SUBSCRIPTION_PRICES[currency][CONST.POLICY.TYPE.CORPORATE][CONST.SUBSCRIPTION.TYPE.ANNUAL], currency)}`;
+
+            // Initialized the user's preferred currency to another payment card currency
+            await Onyx.merge(ONYXKEYS.PERSONAL_DETAILS_LIST, {'-1': {localCurrencyCode: currency}});
+
+            // Render the WorkspaceUpgradePage without a feature to render GenericFeaturesView
+            renderPage(SCREENS.WORKSPACE.UPGRADE, {policyID: policy.id});
+
+            expect(screen.getByText(price)).toBeTruthy();
+
+            // Render the WorkspaceUpgradePage with rules as a feature to render UpgradeIntro
+            const {unmount} = renderPage(SCREENS.WORKSPACE.UPGRADE, {policyID: policy.id, featureName: 'rules'});
+
+            expect(screen.getByText(price)).toBeTruthy();
+
+            unmount();
+        }
+
+        await waitForBatchedUpdates();
     });
 });
