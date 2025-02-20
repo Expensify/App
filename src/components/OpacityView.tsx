7f09e7b27b7a6685723462b227dad36abfaca180
mkzie2 <mkzie2@gmail.com>
2025-02-20 21:29:21 +0700

    add comment

--- src/components/OpacityView.tsx
+++ src/components/OpacityView.tsx
@@ -8,6 +8,17 @@ import variables from '@styles/variables';
 
 const easing = Easing.bezier(0.76, 0.0, 0.24, 1.0);
 
+/**
+ * Due to issues with react-native-reanimated Keyframes the easing type doesn't account for bezier functions
+ * and we also need to use internal .build() function to make the easing apply on each mount.
+ *
+ * This causes problems with both eslint & Typescript and is going to be fixed in react-native-reanimated 3.17 with these PRs merged:
+ * https://github.com/software-mansion/react-native-reanimated/pull/6960
+ * https://github.com/software-mansion/react-native-reanimated/pull/6958
+ *
+ * Once that's added we can apply our changes to files in BottomDockedModal/Backdrop/*.tsx and BottomDockedModal/Container/*.tsx
+ */
+
 type OpacityViewProps = {
     /** Should we dim the view */
     shouldDim: boolean;
