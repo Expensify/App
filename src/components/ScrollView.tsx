3789062a7fa62940083ed3b87463a5ee370abd05
Christoph Pader <christoph.pader@outlook.com>
2025-02-21 16:16:25 +0100

    feat: implement `addBottomSafeAreaPaddingToContent`

--- src/components/ScrollView.tsx
+++ src/components/ScrollView.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import type {ForwardedRef} from 'react';
 // eslint-disable-next-line no-restricted-imports
-import {ScrollView as RNScrollView} from 'react-native';
-import type {ScrollViewProps} from 'react-native';
+import {ScrollView as RNScrollView, StyleSheet} from 'react-native';
+import type {ScrollViewProps as RNScrollViewProps, StyleProp, ViewStyle} from 'react-native';
+import useStyledSafeAreaInsets from '@hooks/useStyledSafeAreaInsets';
 
-function ScrollView({children, scrollIndicatorInsets, ...props}: ScrollViewProps, ref: ForwardedRef<RNScrollView>) {
+type ScrollViewProps = RNScrollViewProps & {
+    /**
+     * If enabled, the content will have a bottom padding equal to account for the safe bottom area inset.
+     */
+    addBottomSafeAreaPaddingToContent?: boolean;
+};
+
+function ScrollView(
+    {children, scrollIndicatorInsets, contentContainerStyle: contentContainerStyleProp, addBottomSafeAreaPaddingToContent = false, ...props}: ScrollViewProps,
+    ref: ForwardedRef<RNScrollView>,
+) {
+    const {paddingBottom: safeAreaPaddingBottom} = useStyledSafeAreaInsets();
+
+    const contentContainerStyle: StyleProp<ViewStyle> = useMemo(() => {
+        if (addBottomSafeAreaPaddingToContent) {
+            const contentContainerStyleFlattened = StyleSheet.flatten(contentContainerStyleProp);
+            const paddingBottom = contentContainerStyleFlattened.paddingBottom ?? 0;
+            return [contentContainerStyleProp, {paddingBottom: typeof paddingBottom === 'number' ? safeAreaPaddingBottom + paddingBottom : safeAreaPaddingBottom}];
+        }
+        return contentContainerStyleProp;
+    }, [addBottomSafeAreaPaddingToContent, contentContainerStyleProp, safeAreaPaddingBottom]);
+
     return (
         <RNScrollView
             ref={ref}
@@ -12,6 +34,7 @@ function ScrollView({children, scrollIndicatorInsets, ...props}: ScrollViewProps
             // on middle/left side of scrollview. scrollIndicatorInsets with right
             // to closest value to 0 fixes this issue, 0 (default) doesn't work
             // See: https://github.com/Expensify/App/issues/31441
+            contentContainerStyle={contentContainerStyle}
             scrollIndicatorInsets={scrollIndicatorInsets ?? {right: Number.MIN_VALUE}}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
