c9b15c86c5f0debfcfb2f509879bbfb848d076e2
Christoph Pader <chris@margelo.com>
2025-02-23 23:48:32 +0100

    Update useBottomSafeSafeAreaPaddingStyle.ts

--- src/hooks/useBottomSafeSafeAreaPaddingStyle.ts
+++ src/hooks/useBottomSafeSafeAreaPaddingStyle.ts
@@ -6,30 +6,46 @@ import useStyledSafeAreaInsets from './useStyledSafeAreaInsets';
 type UseBottomSafeAreaPaddingStyleParams = {
     addBottomSafeAreaPadding?: boolean;
     style?: StyleProp<ViewStyle>;
-    paddingBottom?: number;
+    additionalPaddingBottom?: number;
 };
 
 function useBottomSafeSafeAreaPaddingStyle(params?: UseBottomSafeAreaPaddingStyleParams) {
     const {paddingBottom: safeAreaPaddingBottom} = useStyledSafeAreaInsets(true);
 
-    const {addBottomSafeAreaPadding, style, paddingBottom} = params ?? {};
+    const {addBottomSafeAreaPadding, style, additionalPaddingBottom} = params ?? {};
 
     return useMemo<StyleProp<ViewStyle>>(() => {
-        let totalPaddingBottom = paddingBottom ?? 0;
+        let totalPaddingBottom: number | string = additionalPaddingBottom ?? 0;
+
+        // Add the safe area padding to the total padding if the flag is enabled
         if (addBottomSafeAreaPadding) {
             totalPaddingBottom += safeAreaPaddingBottom;
         }
 
+        // If there is no bottom safe area or additional padding, return the style as is
+        if (totalPaddingBottom === 0) {
+            return style;
+        }
+
+        // If a style is provided, flatten the style and add the padding to it
         if (style) {
-            const contentContainerStyleFlattened = style === undefined ? undefined : StyleSheet.flatten(style);
+            const contentContainerStyleFlattened = StyleSheet.flatten(style);
             const stylePaddingBottom = contentContainerStyleFlattened?.paddingBottom;
-            totalPaddingBottom += typeof stylePaddingBottom === 'number' ? stylePaddingBottom : 0;
+
+            if (typeof stylePaddingBottom === 'number') {
+                totalPaddingBottom += stylePaddingBottom;
+            } else if (typeof stylePaddingBottom === 'string') {
+                totalPaddingBottom = `calc(${totalPaddingBottom}px + ${stylePaddingBottom})`;
+            } else if (stylePaddingBottom !== undefined) {
+                return style;
+            }
 
             return [style, {paddingBottom: totalPaddingBottom}];
         }
 
+        // If no style is provided, return the padding as an object
         return {paddingBottom: totalPaddingBottom};
-    }, [addBottomSafeAreaPadding, style, paddingBottom, safeAreaPaddingBottom]);
+    }, [addBottomSafeAreaPadding, style, additionalPaddingBottom, safeAreaPaddingBottom]);
 }
 
 export default useBottomSafeSafeAreaPaddingStyle;
