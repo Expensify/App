fbf3886f8de969c9e2a111c2114cef9f239c9ceb
rory <rory@expensify.com>
2025-02-24 11:48:30 -0800

    Make errors work with live reload

--- emails/server.ts
+++ emails/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import open from 'open';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import SampleEmail from './components/SampleEmail';
@@ -9,6 +8,7 @@ import LiveReloadServer from './LiveReloadServer';
 const app = express();
 const url = `http://localhost:${CONFIG.EXPRESS_PORT}`;
 
+// TODO: only send live reload connection snippet for dev bundle. For production bundle don't include it
 app.get('/', (req, res) => {
     const emailContent = ReactDOMServer.renderToStaticMarkup(React.createElement(SampleEmail));
     res.send(`<!DOCTYPE html>
@@ -22,6 +22,37 @@ app.get('/', (req, res) => {
     ${emailContent}
   </body>
 </html>`);
+});
+
+// Custom error handler works with live reload server
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(err.status || 500);
+
+    // Check if the request accepts HTML
+    if (req.accepts('html')) {
+        res.send(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Error</title>
+            </head>
+            <body>
+                <h1>Something went wrong :(</h1>
+                <pre>${err.stack}</pre>
+                ${LiveReloadServer.clientConnectionScript}
+            </body>
+            </html>
+        `);
+    } else if (req.accepts('json')) {
+        res.json({error: 'Something went wrong!', details: err.message});
+    } else {
+        res.type('txt').send('Something went wrong!');
+    }
 });
 
 app.listen(CONFIG.EXPRESS_PORT, () => {
