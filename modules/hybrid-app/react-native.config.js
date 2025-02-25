91a78a2097e3efd9656765098f8f5cdf6ae1e4ad
war-in <war-in@users.noreply.github.com>
2025-02-25 16:04:54 +0100

    fix prettier

--- modules/hybrid-app/react-native.config.js
+++ modules/hybrid-app/react-native.config.js
@@ -2,11 +2,11 @@
  * @type {import('@react-native-community/cli-types').UserDependencyConfig}
  */
 module.exports = {
-  dependency: {
-    platforms: {
-      android: {
-        cmakeListsPath: 'build/generated/source/codegen/jni/CMakeLists.txt',
-      },
+    dependency: {
+        platforms: {
+            android: {
+                cmakeListsPath: 'build/generated/source/codegen/jni/CMakeLists.txt',
+            },
+        },
     },
-  },
 };
