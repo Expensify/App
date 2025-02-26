cd2941b496d924714f7b8106d27cd2e6ea80fa5f
kubabutkiewicz <jakub.butkiewicz94@gmail.com>
2025-02-26 15:46:56 +0100

    adjust styles

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -5946,7 +5946,7 @@ const translations = {
         },
         scanTestTooltip: {
             part1: 'Want to see how Scan works?',
-            part2: ' Try a test receipt!',
+            part2: ' Try a test \nreceipt!',
             part3: 'Choose our',
             part4: ' test manager',
             part5: ' to try it out!',
