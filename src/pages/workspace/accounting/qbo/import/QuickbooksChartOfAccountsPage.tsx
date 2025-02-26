7543e5421d5d48000f2a3d7c005b1194d2ef8b6d
Christoph Pader <chris@margelo.com>
2025-02-26 17:41:27 +0100

    fix: more fixes

--- src/pages/workspace/accounting/qbo/import/QuickbooksChartOfAccountsPage.tsx
+++ src/pages/workspace/accounting/qbo/import/QuickbooksChartOfAccountsPage.tsx
@@ -32,6 +32,7 @@ function QuickbooksChartOfAccountsPage({policy}: WithPolicyProps) {
             contentContainerStyle={[styles.pb2, styles.ph5]}
             connectionName={CONST.POLICY.CONNECTIONS.NAME.QBO}
             onBackButtonPress={() => Navigation.goBack(ROUTES.POLICY_ACCOUNTING_QUICKBOOKS_ONLINE_IMPORT.getRoute(policyID))}
+            enableEdgeToEdgeBottomSafeAreaPadding
         >
             <ToggleSettingOptionRow
                 title={translate('workspace.accounting.import')}
