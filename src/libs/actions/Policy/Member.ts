ca420829523076161264c4bbef17303531cfc2b2
Daniel Silva <dsilva@expensify.com>
2025-02-27 14:09:02 -0300

    adding using accountID from message if present

--- src/libs/actions/Policy/Member.ts
+++ src/libs/actions/Policy/Member.ts
@@ -994,11 +994,11 @@ function acceptJoinRequest(reportID: string | undefined, reportAction: OnyxEntry
             },
         },
     ];
-
+    const accountIDToApprove = ReportActionsUtils.getOriginalMessage(reportAction)?.accountID ?? reportAction?.actorAccountID;
     const parameters = {
         requests: JSON.stringify({
             [ReportActionsUtils.isActionableJoinRequest(reportAction) ? ReportActionsUtils.getOriginalMessage(reportAction)?.policyID ?? CONST.DEFAULT_NUMBER_ID : CONST.DEFAULT_NUMBER_ID]: {
-                requests: [{accountID: reportAction?.actorAccountID, adminsRoomMessageReportActionID: reportAction.reportActionID}],
+                requests: [{accountID: accountIDToApprove, adminsRoomMessageReportActionID: reportAction.reportActionID}],
             },
         }),
     };
@@ -1053,11 +1053,11 @@ function declineJoinRequest(reportID: string | undefined, reportAction: OnyxEntr
             },
         },
     ];
-
+    const accountIDToApprove = ReportActionsUtils.getOriginalMessage(reportAction)?.accountID ?? reportAction?.actorAccountID;
     const parameters = {
         requests: JSON.stringify({
             [ReportActionsUtils.isActionableJoinRequest(reportAction) ? ReportActionsUtils.getOriginalMessage(reportAction)?.policyID ?? CONST.DEFAULT_NUMBER_ID : CONST.DEFAULT_NUMBER_ID]: {
-                requests: [{accountID: reportAction?.actorAccountID, adminsRoomMessageReportActionID: reportAction.reportActionID}],
+                requests: [{accountID: accountIDToApprove, adminsRoomMessageReportActionID: reportAction.reportActionID}],
             },
         }),
     };
