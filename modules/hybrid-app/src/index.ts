2e52297c6cd670b7117a4d4d5914090d7f7f4039
war-in <war-in@users.noreply.github.com>
2025-02-28 15:29:13 +0100

    fix broken web

--- modules/hybrid-app/src/index.ts
+++ modules/hybrid-app/src/index.ts
@@ -1,5 +1,11 @@
-import ReactNativeHybridApp from './NativeReactNativeHybridApp';
-
-const HybridAppModule = ReactNativeHybridApp;
+const HybridAppModule = {
+    isHybridApp() {
+        return false;
+    },
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    closeReactNativeApp(shouldSignOut: boolean, shouldSetNVP: boolean) {},
+    completeOnboarding(status: boolean) {},
+    switchAccount(newDotCurrentAccountEmail: string, authToken: string, policyID: string, accountID: string) {},
+};
 
 export default HybridAppModule;
