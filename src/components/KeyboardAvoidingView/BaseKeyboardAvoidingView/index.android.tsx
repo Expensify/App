2b10eb5a1959ba56db7495ca449fc880f042f271
Christoph Pader <chris@margelo.com>
2025-03-03 19:06:55 +0100

    fix: address more issues

--- src/components/KeyboardAvoidingView/BaseKeyboardAvoidingView/index.android.tsx
+++ src/components/KeyboardAvoidingView/BaseKeyboardAvoidingView/index.android.tsx
@@ -1,15 +1,12 @@
-/*
- * The KeyboardAvoidingView is only used on ios
- */
 import type React from 'react';
 import type {KeyboardAvoidingViewProps} from 'react-native-keyboard-controller';
 import {KeyboardAvoidingView as KeyboardAvoidingViewComponent} from 'react-native-keyboard-controller';
 
-function KeyboardAvoidingView(props: KeyboardAvoidingViewProps) {
+function BaseKeyboardAvoidingView(props: KeyboardAvoidingViewProps) {
     // eslint-disable-next-line react/jsx-props-no-spreading
     return <KeyboardAvoidingViewComponent {...props} />;
 }
 
 BaseKeyboardAvoidingView.displayName = 'BaseKeyboardAvoidingView';
 
-export default KeyboardAvoidingView;
+export default BaseKeyboardAvoidingView;
