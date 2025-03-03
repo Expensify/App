d000d19c2a948449339eceee14b45a12f36b07df
nkdengineer <nkdengineer@outlook.com>
2025-03-03 15:19:39 +0700

    fix prettier

--- src/libs/actions/OnyxUpdates.ts
+++ src/libs/actions/OnyxUpdates.ts
@@ -10,7 +10,7 @@ import type {OnyxUpdateEvent, OnyxUpdatesFromServer, Request} from '@src/types/o
 import type Response from '@src/types/onyx/Response';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import {queueOnyxUpdates} from './QueuedOnyxUpdates';
-import { signOutAndRedirectToSignIn } from './Session';
+import {signOutAndRedirectToSignIn} from './Session';
 
 // This key needs to be separate from ONYXKEYS.ONYX_UPDATES_FROM_SERVER so that it can be updated without triggering the callback when the server IDs are updated. If that
 // callback were triggered it would lead to duplicate processing of server updates.
@@ -44,7 +44,7 @@ function applyHTTPSOnyxUpdates(request: Request, response: Response) {
                 return updateHandler(request.successData);
             }
 
-            if(response.jsonCode === 408 && response.message?.includes('The account you are trying to use is deleted.')){
+            if (response.jsonCode === 408 && response.message?.includes('The account you are trying to use is deleted.')) {
                 signOutAndRedirectToSignIn(true);
                 return Promise.resolve();
             }
