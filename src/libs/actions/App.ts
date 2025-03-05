bb6f96a3f7e44bb2ee8d62129c66e64f87256898
Francois Laithier <francois@expensify.com>
2025-03-05 11:15:39 -0800

    Merge pull request #57831 from software-mansion-labs/fix/scroll-reports-screen
    [CP Staging] Fix: Scroll Issue on Reports Page, side pane appearing after language change
    
    (cherry picked from commit a99a5fe9a754394f2173fd9115e6d00897792859)
    
    (CP triggered by francoisl)

--- src/libs/actions/App.ts
+++ src/libs/actions/App.ts
@@ -31,7 +31,6 @@ import {getAll, rollbackOngoingRequest, save} from './PersistedRequests';
 import {createDraftInitialWorkspace, createWorkspace, generatePolicyID} from './Policy/Policy';
 import {resolveDuplicationConflictAction} from './RequestConflictUtils';
 import {isAnonymousUser} from './Session';
-import {triggerSidePane} from './SidePane';
 import Timing from './Timing';
 
 type PolicyParamsForOpenOrReconnect = {
@@ -197,7 +196,6 @@ function setLocale(locale: Locale) {
 
 function setLocaleAndNavigate(locale: Locale) {
     setLocale(locale);
-    triggerSidePane(false, {shouldUpdateNarrowLayout: true});
     Navigation.goBack();
 }
 
