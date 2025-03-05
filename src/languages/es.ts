46b921fe164cab7799742962b4bff9008048ec0d
Yauheni Pasiukevich <pasyukevich@live.com>
2025-03-05 21:31:05 +0100

    fix: update Spanish translation for 'flip' to improve clarity

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -968,7 +968,7 @@ const translations = {
         payElsewhere: ({formattedAmount}: SettleExpensifyCardParams) => (formattedAmount ? `Pagar ${formattedAmount} de otra forma` : `Pagar de otra forma`),
         nextStep: 'Pasos siguientes',
         finished: 'Finalizado',
-        flip: 'Voltear',
+        flip: 'Cambiar',
         sendInvoice: ({amount}: RequestAmountParams) => `Enviar factura de ${amount}`,
         submitAmount: ({amount}: RequestAmountParams) => `solicitar ${amount}`,
         submittedAmount: ({formattedAmount, comment}: RequestedAmountMessageParams) => `solicitó ${formattedAmount}${comment ? ` para ${comment}` : ''}`,
