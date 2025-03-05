e47328cee20a170d0d3b528f7e77977b3fde6bd9
nkdengineer <nkdengineer@outlook.com>
2025-03-05 22:09:07 +0700

    fix: Attachments-Swiping to the left through attachment

--- src/components/AttachmentModal.tsx
+++ src/components/AttachmentModal.tsx
@@ -514,7 +514,6 @@ function AttachmentModal({
                     }
                 }}
                 propagateSwipe
-                swipeDirection={shouldUseNarrowLayout ? CONST.SWIPE_DIRECTION.RIGHT : undefined}
                 initialFocus={() => {
                     if (!submitRef.current) {
                         return false;
