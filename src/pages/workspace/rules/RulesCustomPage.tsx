c18871cf5ed50961309ab85c682c916e9dfb1c74
nkdengineer <nkdengineer@outlook.com>
2025-03-06 10:55:38 +0700

    fix: custom rule description has no description text limit

--- src/pages/workspace/rules/RulesCustomPage.tsx
+++ src/pages/workspace/rules/RulesCustomPage.tsx
@@ -2,6 +2,7 @@ import React, {useCallback, useState} from 'react';
 import {View} from 'react-native';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
+import type {FormOnyxValues} from '@components/Form/types';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import Text from '@components/Text';
@@ -10,6 +11,7 @@ import useAutoFocusInput from '@hooks/useAutoFocusInput';
 import useLocalize from '@hooks/useLocalize';
 import usePolicy from '@hooks/usePolicy';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {addErrorMessage} from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
@@ -40,6 +42,22 @@ function RulesCustomPage({
         setCustomRulesValue(newValue);
     }, []);
 
+    const validate = useCallback(
+        (values: FormOnyxValues<typeof ONYXKEYS.FORMS.RULES_CUSTOM_FORM>) => {
+            const errors = {};
+
+            if (values[INPUT_IDS.CUSTOM_RULES].length > CONST.DESCRIPTION_LIMIT) {
+                addErrorMessage(
+                    errors,
+                    INPUT_IDS.CUSTOM_RULES,
+                    translate('common.error.characterLimitExceedCounter', {length: values[INPUT_IDS.CUSTOM_RULES].length, limit: CONST.DESCRIPTION_LIMIT}),
+                );
+            }
+            return errors;
+        },
+        [translate],
+    );
+
     return (
         <AccessOrNotFoundWrapper
             policyID={policyID}
@@ -64,6 +82,7 @@ function RulesCustomPage({
                     }}
                     submitButtonText={translate('workspace.editor.save')}
                     enabledWhenOffline
+                    validate={validate}
                 >
                     <View style={styles.mb4}>
                         <InputWrapper
