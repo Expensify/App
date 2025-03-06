a2aef3cb469eeb432a1f302182046b72a99586ed
Wildan Muhlis <muhliswildan@gmail.com>
2025-03-06 14:37:51 +0700

    Resolve https://github.com/Expensify/App/issues/57357

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -34,7 +34,7 @@ import DateUtils from './DateUtils';
 import {translateLocal} from './Localize';
 import Navigation from './Navigation/Navigation';
 import {canSendInvoice} from './PolicyUtils';
-import {isDeletedAction, isResolvedActionableWhisper} from './ReportActionsUtils';
+import {isCreatedAction, isDeletedAction, isResolvedActionableWhisper} from './ReportActionsUtils';
 import {
     getSearchReportName,
     hasInvoiceReports,
@@ -431,16 +431,19 @@ function getReportActionsSections(data: OnyxTypes.SearchResults['data']): Report
         .filter(isPolicyEntry)
         .map((key) => data[key]);
 
+        
     for (const key in data) {
         if (isReportActionEntry(key)) {
             const reportActions = data[key];
             for (const reportAction of Object.values(reportActions)) {
+            
+    
                 const from = data.personalDetailsList?.[reportAction.accountID];
                 const report = data[`${ONYXKEYS.COLLECTION.REPORT}${reportAction.reportID}`] ?? {};
                 const policy = data[`${ONYXKEYS.COLLECTION.POLICY}${report.policyID}`] ?? {};
                 const invoiceReceiverPolicy: SearchPolicy | undefined =
                     report?.invoiceReceiver?.type === CONST.REPORT.INVOICE_RECEIVER_TYPE.BUSINESS ? data[`${ONYXKEYS.COLLECTION.POLICY}${report.invoiceReceiver.policyID}`] : undefined;
-                if (isDeletedAction(reportAction) || isResolvedActionableWhisper(reportAction)) {
+                if (isDeletedAction(reportAction) || isResolvedActionableWhisper(reportAction) || reportAction.actionName === CONST.REPORT.ACTIONS.TYPE.CLOSED || isCreatedAction(reportAction)) {
                     // eslint-disable-next-line no-continue
                     continue;
                 }
