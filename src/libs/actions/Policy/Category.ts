4f80cd22c635e2d8754f04a80e090ce21934b379
nkdengineer <nkdengineer@outlook.com>
2025-03-06 10:39:18 +0700

    fix: Not here page opens after renaming category twice

--- src/libs/actions/Policy/Category.ts
+++ src/libs/actions/Policy/Category.ts
@@ -704,7 +704,6 @@ function renamePolicyCategory(policyID: string, policyCategory: {oldName: string
                 value: {
                     [policyCategory.oldName]: null,
                     [policyCategory.newName]: {
-                        ...policyCategoryToUpdate,
                         name: policyCategory.newName,
                         errors: null,
                         pendingAction: null,
