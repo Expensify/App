3c0f398d7c738c5ffff393fb415d95b001251759
Blazej Kustra <blazej.kustra@swmansion.com>
2025-03-06 13:39:51 +0100

    Remove ScreenWrapper from the sidepane

--- src/components/SidePane/index.tsx
+++ src/components/SidePane/index.tsx
@@ -5,15 +5,16 @@ import React, {useCallback, useEffect, useMemo, useRef} from 'react';
 import {Animated, View} from 'react-native';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import Backdrop from '@components/Modal/BottomDockedModal/Backdrop';
-import ScreenWrapper from '@components/ScreenWrapper';
 import useLocalize from '@hooks/useLocalize';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useSidePane from '@hooks/useSidePane';
+import useStyledSafeAreaInsets from '@hooks/useStyledSafeAreaInsets';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {triggerSidePane} from '@libs/actions/SidePane';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
 import {substituteRouteParameters} from '@libs/SidePaneUtils';
+import NAVIGATORS from '@src/NAVIGATORS';
 import getHelpContent from './getHelpContent';
 
 type SidePaneProps = {
@@ -24,13 +25,18 @@ function SidePane({state}: SidePaneProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
 
-    const {isExtraLargeScreenWidth, shouldUseNarrowLayout, isInNarrowPaneModal} = useResponsiveLayout();
+    const {isExtraLargeScreenWidth, shouldUseNarrowLayout} = useResponsiveLayout();
     const {sidePaneTranslateX, shouldHideSidePane, shouldHideSidePaneBackdrop} = useSidePane();
+    const {paddingTop} = useStyledSafeAreaInsets();
 
-    const route = useMemo(() => {
+    const {route, isInNarrowPaneModal} = useMemo(() => {
         const params = (findFocusedRoute(state)?.params as Record<string, string>) ?? {};
         const activeRoute = Navigation.getActiveRouteWithoutParams();
-        return substituteRouteParameters(activeRoute, params);
+
+        return {
+            route: substituteRouteParameters(activeRoute, params),
+            isInNarrowPaneModal: state?.routes?.some((r) => r.name === NAVIGATORS.RIGHT_MODAL_NAVIGATOR),
+        };
     }, [state]);
 
     const onClose = useCallback(
@@ -68,19 +74,17 @@ function SidePane({state}: SidePaneProps) {
                     />
                 )}
             </View>
-            <Animated.View style={[styles.sidePaneContainer(shouldUseNarrowLayout, isExtraLargeScreenWidth), {transform: [{translateX: sidePaneTranslateX.current}]}]}>
-                <ScreenWrapper testID={SidePane.displayName}>
-                    <HeaderWithBackButton
-                        title={translate('common.help')}
-                        style={styles.headerBarDesktopHeight}
-                        onBackButtonPress={() => onClose(false)}
-                        onCloseButtonPress={() => onClose(false)}
-                        shouldShowBackButton={!isExtraLargeScreenWidth}
-                        shouldShowCloseButton={isExtraLargeScreenWidth}
-                        shouldDisplayHelpButton={false}
-                    />
-                    {getHelpContent(styles, route)}
-                </ScreenWrapper>
+            <Animated.View style={[styles.sidePaneContainer(shouldUseNarrowLayout, isExtraLargeScreenWidth), {transform: [{translateX: sidePaneTranslateX.current}], paddingTop}]}>
+                <HeaderWithBackButton
+                    title={translate('common.help')}
+                    style={styles.headerBarDesktopHeight}
+                    onBackButtonPress={() => onClose(false)}
+                    onCloseButtonPress={() => onClose(false)}
+                    shouldShowBackButton={!isExtraLargeScreenWidth}
+                    shouldShowCloseButton={isExtraLargeScreenWidth}
+                    shouldDisplayHelpButton={false}
+                />
+                {getHelpContent(styles, route)}
             </Animated.View>
         </>
     );
