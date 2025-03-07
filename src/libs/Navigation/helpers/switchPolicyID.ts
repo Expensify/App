28e0864075a7b11eb860f44e8edad91bfee2e937
nkdengineer <nkdengineer@outlook.com>
2025-03-07 22:31:48 +0700

    remove log

--- src/libs/Navigation/helpers/switchPolicyID.ts
+++ src/libs/Navigation/helpers/switchPolicyID.ts
@@ -2,6 +2,5 @@ import navigationRef from '@libs/Navigation/navigationRef';
 import CONST from '@src/CONST';
 
 export default function switchPolicyID(newPolicyID: string | undefined) {
-    console.trace();
     navigationRef.dispatch({type: CONST.NAVIGATION.ACTION_TYPE.SWITCH_POLICY_ID, payload: {policyID: newPolicyID}});
 }
