b2650bbe7f2e38826f5f8879bb047f41dbc1bba0
Alberto <alberto@expensify.com>
2025-03-07 12:06:34 +0000

    test with token

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -175,6 +175,7 @@ import {
 import {canAnonymousUserAccessRoute, hasAuthToken, isAnonymousUser, signOutAndRedirectToSignIn, waitForUserSignIn} from './Session';
 import {isOnboardingFlowCompleted, onServerDataReady, setOnboardingErrorMessage} from './Welcome';
 import {startOnboardingFlow} from './Welcome/OnboardingFlow';
+import addEncryptedAuthTokenToURL from "@libs/addEncryptedAuthTokenToURL";
 
 type SubscriberCallback = (isFromCurrentUser: boolean, reportActionID: string | undefined) => void;
 
@@ -4685,9 +4686,9 @@ function exportReportToPDF({reportID}: ExportReportPDFParams) {
 function downloadReportPDF(fileName: string, reportName: string) {
     const baseURL = addTrailingForwardSlash(getOldDotURLFromEnvironment(environment));
     const downloadFileName = `${reportName}.pdf`;
-    const pdfURL = `https://www.expensify.com/secure?secureType=pdfreport&filename=${fileName}&downloadName=${downloadFileName}`;
+    const pdfURL = `https://staging.expensify.com/secure?secureType=pdfreport&filename=${fileName}&downloadName=${downloadFileName}`;
     setDownload(downloadFileName, true);
-    fileDownload(pdfURL, downloadFileName, '', Browser.isMobileSafari()).then(() => setDownload(downloadFileName, false));
+    fileDownload(addEncryptedAuthTokenToURL(pdfURL), downloadFileName, '', Browser.isMobileSafari()).then(() => setDownload(downloadFileName, false));
 }
 
 function setDeleteTransactionNavigateBackUrl(url: string) {
