025b2a0eb9cc3cadf880a98b53e33e38dc34e0f5
Sibtain Ali <allroundexperts@gmail.com>
2025-03-07 19:10:53 +0500

    change order of menu

--- src/pages/settings/Security/SecuritySettingsPage.tsx
+++ src/pages/settings/Security/SecuritySettingsPage.tsx
@@ -118,15 +118,15 @@ function SecuritySettingsPage() {
                 action: isActingAsDelegate ? showDelegateNoAccessMenu : waitForNavigate(() => Navigation.navigate(ROUTES.SETTINGS_2FA_ROOT.getRoute())),
             },
             {
-                translationKey: 'closeAccountPage.closeAccount',
-                icon: Expensicons.ClosedSign,
-                action: isActingAsDelegate ? showDelegateNoAccessMenu : waitForNavigate(() => Navigation.navigate(ROUTES.SETTINGS_CLOSE)),
-            },
-            {
                 translationKey: 'mergeAccountsPage.mergeAccount',
                 icon: Expensicons.ArrowCollapse,
                 action: waitForNavigate(() => Navigation.navigate(ROUTES.SETTINGS_MERGE_ACCOUNTS)),
             },
+            {
+                translationKey: 'closeAccountPage.closeAccount',
+                icon: Expensicons.ClosedSign,
+                action: isActingAsDelegate ? showDelegateNoAccessMenu : waitForNavigate(() => Navigation.navigate(ROUTES.SETTINGS_CLOSE)),
+            }
         ];
 
         return baseMenuItems.map((item) => ({
