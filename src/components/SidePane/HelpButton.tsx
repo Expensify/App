793979de5e791a3be1643ffbbf0fbf87584561cc
Blazej Kustra <blazej.kustra@swmansion.com>
2025-03-07 10:58:44 +0100

    Centralize helpButton logic in useSidePane

--- src/components/SidePane/HelpButton.tsx
+++ src/components/SidePane/HelpButton.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import type {StyleProp, ViewStyle} from 'react-native';
-import {useOnyx} from 'react-native-onyx';
+import Animated, {FadeIn, FadeOut} from 'react-native-reanimated';
 import Icon from '@components/Icon';
 import * as Expensicons from '@components/Icon/Expensicons';
 import {PressableWithoutFeedback} from '@components/Pressable';
 import Tooltip from '@components/Tooltip';
-import useEnvironment from '@hooks/useEnvironment';
 import useLocalize from '@hooks/useLocalize';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
+import useSidePane from '@hooks/useSidePane';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {triggerSidePane} from '@libs/actions/SidePane';
-import CONST from '@src/CONST';
-import ONYXKEYS from '@src/ONYXKEYS';
 
 type HelpButtonProps = {
     style?: StyleProp<ViewStyle>;
@@ -22,29 +20,31 @@ function HelpButton({style}: HelpButtonProps) {
     const styles = useThemeStyles();
     const theme = useTheme();
     const {translate} = useLocalize();
-    const [sidePane] = useOnyx(ONYXKEYS.NVP_SIDE_PANE);
-    const [language] = useOnyx(ONYXKEYS.NVP_PREFERRED_LOCALE);
     const {isExtraLargeScreenWidth} = useResponsiveLayout();
-    const {isProduction} = useEnvironment();
+    const {sidePane, shouldHideHelpButton} = useSidePane();
 
-    const shouldHideHelpButton = !sidePane || language !== CONST.LOCALES.EN;
-    if (shouldHideHelpButton && isProduction) {
+    if (shouldHideHelpButton) {
         return null;
     }
 
     return (
-        <Tooltip text={translate('common.help')}>
-            <PressableWithoutFeedback
-                accessibilityLabel={translate('common.help')}
-                style={[styles.flexRow, styles.touchableButtonImage, styles.mr2, style]}
-                onPress={() => triggerSidePane(isExtraLargeScreenWidth ? !sidePane?.open : !sidePane?.openNarrowScreen, {shouldUpdateNarrowLayout: !isExtraLargeScreenWidth})}
-            >
-                <Icon
-                    src={Expensicons.QuestionMark}
-                    fill={theme.icon}
-                />
-            </PressableWithoutFeedback>
-        </Tooltip>
+        <Animated.View
+            entering={FadeIn}
+            exiting={FadeOut}
+        >
+            <Tooltip text={translate('common.help')}>
+                <PressableWithoutFeedback
+                    accessibilityLabel={translate('common.help')}
+                    style={[styles.flexRow, styles.touchableButtonImage, styles.mr2, style]}
+                    onPress={() => triggerSidePane(isExtraLargeScreenWidth ? !sidePane?.open : !sidePane?.openNarrowScreen, {shouldUpdateNarrowLayout: !isExtraLargeScreenWidth})}
+                >
+                    <Icon
+                        src={Expensicons.QuestionMark}
+                        fill={theme.icon}
+                    />
+                </PressableWithoutFeedback>
+            </Tooltip>
+        </Animated.View>
     );
 }
 
