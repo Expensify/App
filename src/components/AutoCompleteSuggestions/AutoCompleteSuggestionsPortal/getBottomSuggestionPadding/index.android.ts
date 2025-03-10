c4a6cb9260c7382243df6a6e0e559bfad17a8cbe
Qichen Zhu <57348009+QichenZhu@users.noreply.github.com>
2025-03-11 08:49:38 +1300

    Add a comment

--- src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.android.ts
+++ src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.android.ts
@@ -1,3 +1,4 @@
+// Selection position returned from react-native-keyboard-controller is platform-dependent, so set different paddings for Android and iOS.
 function getBottomSuggestionPadding(): number {
     return 30;
 }
