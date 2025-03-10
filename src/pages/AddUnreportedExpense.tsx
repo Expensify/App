a6b97aa4ec9f124955247e9008ded980513b03f9
sumo_slonik <jakub.nowakowski@swmansion.com>
2025-03-10 09:59:38 +0100

    work in progress

--- src/pages/AddUnreportedExpense.tsx
+++ src/pages/AddUnreportedExpense.tsx
@@ -1,19 +1,70 @@
-import React, { useMemo, useRef, useState } from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
+import { Text, View } from 'react-native';
 import Button from '@components/Button';
 import FocusTrapContainerElement from '@components/FocusTrap/FocusTrapContainerElement';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
-import type {ListItem, SelectionListHandle} from '@components/SelectionList/types';
-import navigation from '@navigation/Navigation';
-import styles from '@src/styles';
 import SelectionList from '@components/SelectionList';
-import type {Option} from '@libs/OptionsListUtils';
-import {getAllTransactions} from '@userActions/Transaction';
-import RadioListItem from '@components/SelectionList/RadioListItem';
-import TransactionItemComponent from '@components/TransactionItemComponent';
-import NewChatSelectorPage from './NewChatSelectorPage';
+import BaseListItem from '@components/SelectionList/BaseListItem';
+import type { ListItem, SelectionListHandle, UserListItemProps } from '@components/SelectionList/types';
 import useThemeStyles from '@hooks/useThemeStyles';
+import navigation from '@navigation/Navigation';
+import { getAllTransactions } from '@userActions/Transaction';
+import type {Option, Section} from '@libs/OptionsListUtils';
+import UserListItem from '@components/SelectionList/UserListItem';
+import TransactionItemComponent from '@components/TransactionItemComponent';
+import ScrollView from '@components/ScrollView';
+import SelectCircle from '@components/SelectCircle';
+import NewChatSelectorPage from './NewChatSelectorPage';
+
+
+function unreportedExpenseListItem<TItem extends ListItem>({
+                                                  item,
+                                                  isFocused,
+                                                  showTooltip,
+                                                  isDisabled,
+                                                  canSelectMultiple,
+                                                  onSelectRow,
+                                                  onCheckboxPress,
+                                                  onDismissError,
+                                                  shouldPreventEnterKeySubmit,
+                                                  rightHandSideComponent,
+                                                  onFocus,
+                                                  shouldSyncFocus,
+                                                  wrapperStyle,
+                                                  pressableStyle,
+                                              }: UserListItemProps<TItem>) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const styles = useThemeStyles();
+    const [isSelected,setIsSelected] = useState<boolean>(false);
+    return (
+        <BaseListItem
+            item={item}
+            wrapperStyle={[styles.m5]}
+            isFocused={isFocused}
+            isDisabled={isDisabled}
+            showTooltip={showTooltip}
+            canSelectMultiple={canSelectMultiple}
+            onSelectRow={onSelectRow}
+            onDismissError={onDismissError}
+            shouldPreventEnterKeySubmit={shouldPreventEnterKeySubmit}
+            rightHandSideComponent={rightHandSideComponent}
+            errors={item.errors}
+            pendingAction={item.pendingAction}
+            pressableStyle={pressableStyle}
+            keyForList={item.keyForList}
+            onFocus={onFocus}
+            shouldSyncFocus={shouldSyncFocus}
+        >
+            <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
+                <TransactionItemComponent transactionItem={item} isLargeScreenWidth={false} isSelected={false}/>
+                <SelectCircle isChecked={isSelected}/>
+            </View>
+        </BaseListItem>
+    );
+}
+
+unreportedExpenseListItem.displayName = 'unreportedExpenseListItem';
 
 
 function AddUnreportedExpense() {
@@ -24,6 +75,15 @@ function AddUnreportedExpense() {
     }, [headerWithBackBtnContainerElement]);
     const selectionListRef = useRef<SelectionListHandle>(null);
     const unreportedExpensesList = Object.values(getAllTransactions()).filter((item)=>item.reportID==='0')
+    const sections: Section[] = [
+        {
+            title:"expense",
+            shouldShow:true,
+            data: unreportedExpensesList
+        }
+    ]
+
+
     return (
         <ScreenWrapper
             shouldEnableKeyboardAvoidingView={false}
@@ -43,29 +103,33 @@ function AddUnreportedExpense() {
                 />
             </FocusTrapContainerElement>
             <View>
+                <ScrollView>
                 <SelectionList<Option & ListItem>
                     ref={selectionListRef}
                     onSelectRow={() => {}}
                     shouldShowTextInput={false}
                     canSelectMultiple
-                    sections={[]}
-                    ListItem={RadioListItem}
-                    items={unreportedExpensesList}/>
+                    sections={sections}
+                    ListItem={unreportedExpenseListItem}
+                    onConfirm={()=>{console.log(unreportedExpensesList)}}
+                    confirmButtonStyles={styles.mb5}
+                    />
+                </ScrollView>
             </View>
-            {unreportedExpensesList.map((item) => (
-                <View style={styles.m5}>
-                    <TransactionItemComponent transactionItem={item} isLargeScreenWidth={false} isSelected={false}/>
-                </View>
-            ))}
-            <Button
-                text="Add to report"
-                onPress={()=>{
-                    console.log(unreportedExpensesList)
-                }}
-                style={styles.w100}
-                success
-                large
-            />
+            {/* {unreportedExpensesList.map((item) => ( */}
+            {/*     <View style={styles.m5}> */}
+            {/*         <TransactionItemComponent transactionItem={item} isLargeScreenWidth={false} isSelected={false}/> */}
+            {/*     </View> */}
+            {/* ))} */}
+            {/* <Button */}
+            {/*     text="Add to report" */}
+            {/*     onPress={()=>{ */}
+            {/*         console.log(unreportedExpensesList) */}
+            {/*     }} */}
+            {/*     style={styles.w100} */}
+            {/*     success */}
+            {/*     large */}
+            {/* /> */}
         </ScreenWrapper>
 
     )
