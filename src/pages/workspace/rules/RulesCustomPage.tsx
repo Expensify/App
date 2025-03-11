3e708295e53103053e9260f5c04614c643cb79aa
nkdengineer <nkdengineer@outlook.com>
2025-03-11 14:08:33 +0700

    fix add maxLength to rules description

--- src/pages/workspace/rules/RulesCustomPage.tsx
+++ src/pages/workspace/rules/RulesCustomPage.tsx
@@ -2,7 +2,6 @@ import React, {useCallback, useState} from 'react';
 import {View} from 'react-native';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
-import type {FormOnyxValues} from '@components/Form/types';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import Text from '@components/Text';
@@ -11,7 +10,6 @@ import useAutoFocusInput from '@hooks/useAutoFocusInput';
 import useLocalize from '@hooks/useLocalize';
 import usePolicy from '@hooks/usePolicy';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {addErrorMessage} from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
@@ -42,22 +40,6 @@ function RulesCustomPage({
         setCustomRulesValue(newValue);
     }, []);
 
-    const validate = useCallback(
-        (values: FormOnyxValues<typeof ONYXKEYS.FORMS.RULES_CUSTOM_FORM>) => {
-            const errors = {};
-
-            if (values[INPUT_IDS.CUSTOM_RULES].length > CONST.DESCRIPTION_LIMIT) {
-                addErrorMessage(
-                    errors,
-                    INPUT_IDS.CUSTOM_RULES,
-                    translate('common.error.characterLimitExceedCounter', {length: values[INPUT_IDS.CUSTOM_RULES].length, limit: CONST.DESCRIPTION_LIMIT}),
-                );
-            }
-            return errors;
-        },
-        [translate],
-    );
-
     return (
         <AccessOrNotFoundWrapper
             policyID={policyID}
@@ -82,7 +64,6 @@ function RulesCustomPage({
                     }}
                     submitButtonText={translate('workspace.editor.save')}
                     enabledWhenOffline
-                    validate={validate}
                 >
                     <View style={styles.mb4}>
                         <InputWrapper
@@ -95,6 +76,7 @@ function RulesCustomPage({
                             ref={inputCallbackRef}
                             type="markdown"
                             autoGrowHeight
+                            maxLength={CONST.DESCRIPTION_LIMIT}
                         />
                         <Text style={[styles.mutedTextLabel, styles.mt2]}>{translate('workspace.rules.customRules.description')}</Text>
                     </View>
