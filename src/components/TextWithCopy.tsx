5390816201dce02b51363070930e2765e13bf31e
mkzie2 <mkzie2@gmail.com>
2025-03-12 00:45:33 +0700

    disable long press to copy

--- src/components/TextWithCopy.tsx
+++ src/components/TextWithCopy.tsx
@@ -2,8 +2,6 @@ import React, {useRef} from 'react';
 // eslint-disable-next-line no-restricted-imports
 import type {Text as RNText} from 'react-native';
 import Clipboard from '@libs/Clipboard';
-import {showContextMenu} from '@pages/home/report/ContextMenu/ReportActionContextMenu';
-import CONST from '@src/CONST';
 import Text from './Text';
 import type {TextProps} from './Text';
 
@@ -19,9 +17,6 @@ function TextWithCopy({children, copyText, ...rest}: TextWithCopyProps) {
             ref={textRef}
             onPress={() => {
                 Clipboard.setString(copyText);
-            }}
-            onLongPress={(event) => {
-                showContextMenu(CONST.CONTEXT_MENU_TYPES.LINK, event, copyText, textRef.current);
             }}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...rest}
