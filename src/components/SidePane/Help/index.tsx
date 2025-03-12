7429818d479a59d88ab0564ee99fb3bcb93e40e4
Blazej Kustra <blazej.kustra@swmansion.com>
2025-03-12 12:28:33 +0100

    Move animated stylings deeper into the component tree

--- src/components/SidePane/Help/index.tsx
+++ src/components/SidePane/Help/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
+// eslint-disable-next-line no-restricted-imports
+import {Animated} from 'react-native';
 import useKeyboardShortcut from '@hooks/useKeyboardShortcut';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useSidePane from '@hooks/useSidePane';
+import useStyledSafeAreaInsets from '@hooks/useStyledSafeAreaInsets';
+import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
 import HelpContent from './HelpContent';
 
 function Help() {
-    const {isExtraLargeScreenWidth} = useResponsiveLayout();
-    const {closeSidePane} = useSidePane();
+    const styles = useThemeStyles();
+    const {sidePaneTranslateX, closeSidePane} = useSidePane();
+    const {isExtraLargeScreenWidth, shouldUseNarrowLayout} = useResponsiveLayout();
+    const {paddingTop, paddingBottom} = useStyledSafeAreaInsets();
 
     useKeyboardShortcut(CONST.KEYBOARD_SHORTCUTS.ESCAPE, () => closeSidePane(), {isActive: !isExtraLargeScreenWidth});
 
-    return <HelpContent />;
+    return (
+        <Animated.View style={[styles.sidePaneContainer(shouldUseNarrowLayout, isExtraLargeScreenWidth), {transform: [{translateX: sidePaneTranslateX.current}], paddingTop, paddingBottom}]}>
+            <HelpContent />
+        </Animated.View>
+    );
 }
 
 Help.displayName = 'Help';
--- src/components/SidePane/index.tsx
+++ src/components/SidePane/index.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
-// eslint-disable-next-line no-restricted-imports
-import {Animated, View} from 'react-native';
-import useResponsiveLayout from '@hooks/useResponsiveLayout';
+import {View} from 'react-native';
 import useRootNavigationState from '@hooks/useRootNavigationState';
 import useSidePane from '@hooks/useSidePane';
-import useStyledSafeAreaInsets from '@hooks/useStyledSafeAreaInsets';
-import useThemeStyles from '@hooks/useThemeStyles';
 import NAVIGATORS from '@src/NAVIGATORS';
 import Help from './Help';
 import SidePaneOverlay from './SidePaneOverlay';
 
 function SidePane() {
-    const styles = useThemeStyles();
-    const {isExtraLargeScreenWidth, shouldUseNarrowLayout} = useResponsiveLayout();
-    const {sidePaneTranslateX, shouldHideSidePane, shouldHideSidePaneBackdrop, closeSidePane} = useSidePane();
-    const {paddingTop} = useStyledSafeAreaInsets();
+    const {shouldHideSidePane, shouldHideSidePaneBackdrop, closeSidePane} = useSidePane();
     const isInNarrowPaneModal = useRootNavigationState((state) => state?.routes.at(-1)?.name === NAVIGATORS.RIGHT_MODAL_NAVIGATOR);
 
     if (shouldHideSidePane) {
@@ -31,9 +24,7 @@ function SidePane() {
                     />
                 )}
             </View>
-            <Animated.View style={[styles.sidePaneContainer(shouldUseNarrowLayout, isExtraLargeScreenWidth), {transform: [{translateX: sidePaneTranslateX.current}], paddingTop}]}>
-                <Help />
-            </Animated.View>
+            <Help />
         </>
     );
 }
