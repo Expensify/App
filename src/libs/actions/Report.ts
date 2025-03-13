ec7a3cd85eb0ecad9ed46f6122b7f44e4b35555f
Alberto <alberto@expensify.com>
2025-03-13 16:39:32 +0100

    prettier

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -140,6 +140,7 @@ import {getNavatticURL} from '@libs/TourUtils';
 import {addTrailingForwardSlash} from '@libs/Url';
 import {generateAccountID} from '@libs/UserUtils';
 import Visibility from '@libs/Visibility';
+import {setDownload} from '@userActions/Download';
 import CONFIG from '@src/CONFIG';
 import type {OnboardingAccounting, OnboardingCompanySize} from '@src/CONST';
 import CONST from '@src/CONST';
@@ -182,7 +183,6 @@ import {
 import {canAnonymousUserAccessRoute, hasAuthToken, isAnonymousUser, signOutAndRedirectToSignIn, waitForUserSignIn} from './Session';
 import {isOnboardingFlowCompleted, onServerDataReady, setOnboardingErrorMessage} from './Welcome';
 import {startOnboardingFlow} from './Welcome/OnboardingFlow';
-import {setDownload} from "@userActions/Download";
 
 type SubscriberCallback = (isFromCurrentUser: boolean, reportActionID: string | undefined) => void;
 
