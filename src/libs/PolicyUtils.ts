07236c8c33fd0d06b6e6b52e3a3daf4984ecf36d
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-03-13 15:54:57 +0100

    change navigate to goBack

--- src/libs/PolicyUtils.ts
+++ src/libs/PolicyUtils.ts
@@ -520,7 +520,7 @@ function getPolicyEmployeeListByIdWithoutCurrentUser(policies: OnyxCollection<Pi
 }
 
 function goBackFromInvalidPolicy() {
-    Navigation.navigate(ROUTES.SETTINGS_WORKSPACES.route);
+    Navigation.goBack(ROUTES.SETTINGS_WORKSPACES.route);
 }
 
 /** Get a tax with given ID from policy */
