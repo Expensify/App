9f38875b3b2066ddc7db2fe94fa7b20145dbf207
krishna2323 <belivethatkg@gmail.com>
2025-03-14 02:11:39 +0530

    fix blockquoted rendering.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/components/HTMLEngineProvider/HTMLRenderers/BlockQuotesRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/BlockQuotesRenderer.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import type {CustomRendererProps, TPhrasing, TText} from 'react-native-render-html';
+import type {StyleProp, TextStyle} from 'react-native';
 import {TNodeChildrenRenderer} from 'react-native-render-html';
+import type {CustomRendererProps, TPhrasing, TText} from 'react-native-render-html';
 import * as HTMLEngineUtils from '@components/HTMLEngineProvider/htmlEngineUtils';
 import Text from '@components/Text';
 import useThemeStyles from '@hooks/useThemeStyles';
 
-function BlockQuoteRenderer({tnode}: CustomRendererProps<TText | TPhrasing>) {
+function BlockQuoteRenderer({TDefaultRenderer, style, tnode, ...props}: CustomRendererProps<TText | TPhrasing>) {
     const styles = useThemeStyles();
     const isChildOfTaskTitle = HTMLEngineUtils.isChildOfTaskTitle(tnode);
 
-    return 'data' in tnode ? (
-        <Text style={[styles.webViewStyles.baseFontStyle, !isChildOfTaskTitle && styles.webViewStyles.tagStyles.blockquote]}>{tnode.data}</Text>
-    ) : (
+    return isChildOfTaskTitle ? (
         <TNodeChildrenRenderer
             tnode={tnode}
-            renderChild={(props) => {
+            renderChild={(props2) => {
                 return (
                     <Text
-                        style={[styles.webViewStyles.baseFontStyle, !isChildOfTaskTitle && styles.blockquote]}
-                        key={props.key}
+                        style={[styles.webViewStyles.baseFontStyle]}
+                        key={props2.key}
                     >
-                        {props.childElement}
+                        {props2.childElement}
                     </Text>
                 );
             }}
+        />
+    ) : (
+        <TDefaultRenderer
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+            tnode={tnode}
+            style={[style as StyleProp<TextStyle>]}
         />
     );
 }
