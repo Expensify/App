c62bec10e77062b10e4264f6072d311a2a9ffc1b
thelullabyy <the.lullaby2@hotmail.com>
2025-03-15 15:42:03 +0700

    fix: add more details over voice content

--- src/components/LHNOptionsList/OptionRowLHN.tsx
+++ src/components/LHNOptionsList/OptionRowLHN.tsx
@@ -237,7 +237,9 @@ function OptionRowLHN({reportID, isFocused = false, onSelectRow = () => {}, opti
                                     (hovered || isContextMenuActive) && !isFocused ? styles.sidebarLinkHover : null,
                                 ]}
                                 role={CONST.ROLE.BUTTON}
-                                accessibilityLabel={`${translate('accessibilityHints.navigatesToChat')} ${optionItem.text}`}
+                                accessibilityLabel={`${translate('accessibilityHints.navigatesToChat')} ${optionItem.text}. ${optionItem.isUnread ? `${translate('common.unread')}.` : ``} ${
+                                    optionItem.alternateText
+                                }`}
                                 onLayout={onLayout}
                                 needsOffscreenAlphaCompositing={(optionItem?.icons?.length ?? 0) >= 2}
                             >
