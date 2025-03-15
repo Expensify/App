9ca462994b7ded5dde43b224c75f4fdaca14c52f
mkzie2 <mkzie2@gmail.com>
2025-03-15 20:06:07 +0700

    remove confirm button when editing the account type step

--- src/pages/settings/Wallet/InternationalDepositAccount/substeps/AccountType.tsx
+++ src/pages/settings/Wallet/InternationalDepositAccount/substeps/AccountType.tsx
@@ -1,12 +1,13 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useRef} from 'react';
 import {View} from 'react-native';
 import FormProvider from '@components/Form/FormProvider';
 import InputWrapper from '@components/Form/InputWrapper';
-import type {FormInputErrors, FormOnyxValues} from '@components/Form/types';
+import type {FormInputErrors, FormOnyxValues, FormRef} from '@components/Form/types';
 import ValuePicker from '@components/ValuePicker';
 import useInternationalBankAccountFormSubmit from '@hooks/useInternationalBankAccountFormSubmit';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {setDraftValues} from '@libs/actions/FormActions';
 import type {CustomSubStepProps} from '@pages/settings/Wallet/InternationalDepositAccount/types';
 import Text from '@src/components/Text';
 import CONST from '@src/CONST';
@@ -15,6 +16,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 function AccountType({isEditing, onNext, fieldsMap}: CustomSubStepProps) {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
+    const formRef = useRef<FormRef>(null);
 
     const fieldData = fieldsMap[CONST.CORPAY_FIELDS.STEPS_NAME.ACCOUNT_TYPE]?.[CONST.CORPAY_FIELDS.ACCOUNT_TYPE_KEY] ?? {};
 
@@ -48,12 +50,14 @@ function AccountType({isEditing, onNext, fieldsMap}: CustomSubStepProps) {
     return (
         <FormProvider
             formID={ONYXKEYS.FORMS.INTERNATIONAL_BANK_ACCOUNT_FORM}
-            submitButtonText={translate(isEditing ? 'common.confirm' : 'common.next')}
+            submitButtonText={translate('common.confirm')}
             onSubmit={handleSubmit}
             validate={validate}
             style={[styles.flexGrow1, styles.mt3]}
             submitButtonStyles={[styles.ph5, styles.mb0]}
             enabledWhenOffline
+            ref={formRef}
+            isSubmitButtonVisible={!isEditing}
         >
             <View style={styles.ph5}>
                 <Text style={[styles.textHeadlineLineHeightXXL, styles.mb6]}>{translate('addPersonalBankAccount.accountTypeStepHeader')}</Text>
@@ -65,6 +69,14 @@ function AccountType({isEditing, onNext, fieldsMap}: CustomSubStepProps) {
                 items={options}
                 shouldSaveDraft={!isEditing}
                 shouldShowModal={false}
+                onValueChange={(value) => {
+                    if (!isEditing) {
+                        return;
+                    }
+                    setDraftValues(ONYXKEYS.FORMS.INTERNATIONAL_BANK_ACCOUNT_FORM, {[CONST.CORPAY_FIELDS.ACCOUNT_TYPE_KEY]: value}).then(() => {
+                        onNext();
+                    });
+                }}
             />
         </FormProvider>
     );
