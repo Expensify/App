77b92320ec6cdbb71e68ef011db92b59c20fd945
Ionatan Wiznia <ionatan@expensify.com>
2025-03-19 16:39:31 -0300

    Fix tests

--- tests/actions/ReportTest.ts
+++ tests/actions/ReportTest.ts
@@ -443,13 +443,13 @@ describe('actions/Report', () => {
                 expect(ReportUtils.isUnread(report, undefined)).toBe(false);
 
                 // When the user manually marks a message as "unread"
-                Report.markCommentAsUnread(REPORT_ID, reportActions['1']);
+                Report.markCommentAsUnread(REPORT_ID, reportActions[400]);
                 return waitForBatchedUpdates();
             })
             .then(() => {
                 // Then we should expect the report to be to be unread
                 expect(ReportUtils.isUnread(report, undefined)).toBe(true);
-                expect(report?.lastReadTime).toBe(DateUtils.subtractMillisecondsFromDateTime(reportActionCreatedDate, 1));
+                expect(report?.lastReadTime).toBe(DateUtils.subtractMillisecondsFromDateTime(reportActions[400].created, 1));
 
                 // If the user deletes the last comment after the lastReadTime the lastMessageText will reflect the new last comment
                 Report.deleteReportComment(REPORT_ID, {...reportActions[400]});
