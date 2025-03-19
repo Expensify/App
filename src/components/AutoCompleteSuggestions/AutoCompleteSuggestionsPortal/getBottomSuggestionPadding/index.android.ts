033c9028b3c2c449e7f951ea866632b282ae469d
Povilas Zirgulis <pzirgulis@gmail.com>
2025-03-19 11:57:11 +0200

    fix prettier

--- src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.android.ts
+++ src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.android.ts
@@ -6,16 +6,16 @@ function getBottomSuggestionPadding(): number {
     const referenceHeightMin = 900;
     const referencePaddingMin = -10;
     const referenceHeightMax = 1000;
-    const referencePaddingMax = -44;  
-    
+    const referencePaddingMax = -44;
+
     if (height >= referenceHeightMin && height < referenceHeightMax) {
         // Calculate the padding adjustment rate per pixel of height
         const paddingRate = (referencePaddingMax - referencePaddingMin) / (referenceHeightMax - referenceHeightMin);
-        
+
         const padding = referencePaddingMin + (height - referenceHeightMin) * paddingRate;
         return Math.round(Math.max(-60, Math.min(-5, padding)));
     }
-    
+
     return basePadding;
 }
 
