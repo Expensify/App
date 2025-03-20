b40f7d132ca9ada56786802448b775529d19cc88
Samran <samranahmed03@gmail.com>
2025-03-21 00:42:51 +0500

    fix typecheck error

--- src/languages/params.ts
+++ src/languages/params.ts
@@ -212,7 +212,7 @@ type NewWorkspaceNameParams = {userName: string; workspaceNumber?: number};
 
 type RoomNameReservedErrorParams = {reservedName: string};
 
-type RenamedRoomActionParams = {memberName: string; oldName: string; newName: string};
+type RenamedRoomActionParams = {memberName?: string; oldName: string; newName: string};
 
 type RoomRenamedToParams = {newName: string};
 
