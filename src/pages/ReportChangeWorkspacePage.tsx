3af6cd2f7aecf13457f69e30ae1f556aa5dbdffc
Artem Makushov <waterim3009@gmail.com>
2025-03-20 23:01:06 +0100

    update ReportChangeWorkspacePage with moveIOUReportToPolicy

--- src/pages/ReportChangeWorkspacePage.tsx
+++ src/pages/ReportChangeWorkspacePage.tsx
@@ -11,11 +11,12 @@ import useNetwork from '@hooks/useNetwork';
 import useThemeStyles from '@hooks/useThemeStyles';
 import type {WorkspaceListItem} from '@hooks/useWorkspaceList';
 import useWorkspaceList from '@hooks/useWorkspaceList';
-import {changeReportPolicy} from '@libs/actions/Report';
+import {changeReportPolicy, moveIOUReportToPolicy} from '@libs/actions/Report';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {ReportChangeWorkspaceNavigatorParamList} from '@libs/Navigation/types';
-import {isWorkspaceEligibleForReportChange} from '@libs/PolicyUtils';
+import {isPolicyMember, isWorkspaceEligibleForReportChange} from '@libs/PolicyUtils';
+import {isIOUReport} from '@libs/ReportUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
@@ -43,9 +44,13 @@ function ReportChangeWorkspacePage({report}: ReportChangeWorkspacePageProps) {
                 return;
             }
             Navigation.goBack(ROUTES.REPORT_WITH_ID.getRoute(reportID));
-            changeReportPolicy(reportID, policyID);
+            if (isIOUReport(reportID) && isPolicyMember(currentUserLogin, policyID)) {
+                moveIOUReportToPolicy(reportID, policyID);
+            } else {
+                changeReportPolicy(reportID, policyID);
+            }
         },
-        [reportID],
+        [currentUserLogin, reportID],
     );
 
     const {sections, shouldShowNoResultsFoundMessage, shouldShowSearchInput} = useWorkspaceList({
