f32629955b664e3a780d27af571f1f35956debdb
Andrew Gable <andrew@expensify.com>
2025-03-20 09:56:55 -0600

    Run `npm run prettier`

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -1085,7 +1085,7 @@ const translations = {
         holdEducationalText: 'hold',
         whatIsHoldExplain: 'Hold is like hitting “pause” on an expense to ask for more details before approval or payment.',
         holdIsLeftBehind: 'Held expenses are left behind on report approval or payment.',
-        unholdWhenReady: "Approvers can unhold expenses when they’re ready for approval or payment.",
+        unholdWhenReady: 'Approvers can unhold expenses when they’re ready for approval or payment.',
         changePolicyEducational: {
             title: 'You moved this report!',
             description: 'Double-check these items, which tend to change when moving reports to a new workspace.',
