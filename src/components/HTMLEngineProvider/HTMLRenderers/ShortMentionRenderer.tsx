16af8ec1c9195db7f2d93f0d6876a48317af53bc
Andrii Vitiv <cyberandrii@gmail.com>
2025-03-21 22:18:29 +0200

    Remove LTR character from mention
    It is added on Android Native and causes equality checks to fail. The same fix is applied in `MentionUserRenderer` and `MentionReportRenderer` files.

--- src/components/HTMLEngineProvider/HTMLRenderers/ShortMentionRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/ShortMentionRenderer.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {TNodeChildrenRenderer} from 'react-native-render-html';
 import type {CustomRendererProps, TPhrasing, TText} from 'react-native-render-html';
 import useShortMentionsList from '@hooks/useShortMentionsList';
+import CONST from '@src/CONST';
 import MentionHereRenderer from './MentionHereRenderer';
 import MentionUserRenderer from './MentionUserRenderer';
 
 function ShortMentionRenderer(props: CustomRendererProps<TText | TPhrasing>) {
     const {mentionsList, currentUserMentions} = useShortMentionsList();
 
-    const mentionValue = 'data' in props.tnode ? props.tnode.data : '';
+    const mentionValue = 'data' in props.tnode ? props.tnode.data.replace(CONST.UNICODE.LTR, '') : '';
 
     if (currentUserMentions?.includes(mentionValue)) {
         // eslint-disable-next-line react/jsx-props-no-spreading
