09a1c73a11ccd4cc68c8b5642e390fd4b78fd402
nkdengineer <nkdengineer@outlook.com>
2025-03-21 14:39:34 +0700

    remove unrelated change

--- src/pages/workspace/members/WorkspaceOwnerChangeWrapperPage.tsx
+++ src/pages/workspace/members/WorkspaceOwnerChangeWrapperPage.tsx
@@ -13,7 +13,7 @@ import CardAuthenticationModal from '@pages/settings/Subscription/CardAuthentica
 import AccessOrNotFoundWrapper from '@pages/workspace/AccessOrNotFoundWrapper';
 import withPolicy from '@pages/workspace/withPolicy';
 import type {WithPolicyOnyxProps} from '@pages/workspace/withPolicy';
-import {clearWorkspaceOwnerChangeFlow} from '@userActions/Policy/Member';
+import * as MemberActions from '@userActions/Policy/Member';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
@@ -68,7 +68,7 @@ function WorkspaceOwnerChangeWrapperPage({route, policy}: WorkspaceOwnerChangeWr
                 <HeaderWithBackButton
                     title={translate('workspace.changeOwner.changeOwnerPageTitle')}
                     onBackButtonPress={() => {
-                        clearWorkspaceOwnerChangeFlow(policyID);
+                        MemberActions.clearWorkspaceOwnerChangeFlow(policyID);
                         Navigation.goBack();
                         Navigation.navigate(ROUTES.WORKSPACE_MEMBER_DETAILS.getRoute(policyID, accountID));
                     }}
