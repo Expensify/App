8fc5c0a0d702e47bd459b23b87af7d485c1f9fe1
Adam Horodyski <ad.horodyski@gmail.com>
2025-03-21 10:21:12 +0100

    feat: setup rnef for ios and android

---
+++ rnef.config.mjs
@@ -0,0 +1,13 @@
+import {pluginMetro} from '@rnef/plugin-metro';
+import {platformAndroid} from '@rnef/platform-android';
+import {platformIOS} from '@rnef/platform-ios';
+
+export default {
+    remoteCacheProvider: 'github-actions',
+    plugins: [pluginMetro()],
+    platforms: {
+        ios: platformIOS(),
+        android: platformAndroid(),
+    },
+    root: process.env.PROJECT_ROOT_PATH ?? './',
+};
