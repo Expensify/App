951143a1818b71b9b1bba4b1953fd750ad11cb0e
Povilas Zirgulis <pzirgulis@gmail.com>
2025-03-24 18:21:51 +0200

    add check for bottom percentage to height

--- src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.android.ts
+++ src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.android.ts
@@ -1,17 +1,16 @@
 import {Dimensions} from 'react-native';
 
-function getBottomSuggestionPadding(): number {
+function getBottomSuggestionPadding(bottom: number): number {    
     const {height} = Dimensions.get('window');
+
+    const bottomPercentageToHeight = (bottom / height) * 100;
     const basePadding = 30;
     const referenceHeightMin = 900;
     const referencePaddingMin = -10;
     const referenceHeightMax = 1000;
     const referencePaddingMax = -44;
 
-    console.log('getBottomSuggestionPadding height', height);
-
-    if (height >= referenceHeightMin && height < referenceHeightMax) {
-        // Calculate the padding adjustment rate per pixel of height
+    if (bottomPercentageToHeight > 7) {
         const paddingRate = (referencePaddingMax - referencePaddingMin) / (referenceHeightMax - referenceHeightMin);
 
         const padding = referencePaddingMin + (height - referenceHeightMin) * paddingRate;
--- src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/index.native.tsx
+++ src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/index.native.tsx
@@ -1,18 +1,21 @@
 import {Portal} from '@gorhom/portal';
 import React, {useMemo} from 'react';
-import {View} from 'react-native';
+import {Platform, View} from 'react-native';
 import BaseAutoCompleteSuggestions from '@components/AutoCompleteSuggestions/BaseAutoCompleteSuggestions';
 import useStyleUtils from '@hooks/useStyleUtils';
 import getBottomSuggestionPadding from './getBottomSuggestionPadding';
 import TransparentOverlay from './TransparentOverlay/TransparentOverlay';
 import type {AutoCompleteSuggestionsPortalProps} from './types';
 
+type getBottomSuggestionPaddingAndroid = (bottom: number) => number;
+
 function AutoCompleteSuggestionsPortal<TSuggestion>({left = 0, width = 0, bottom = 0, resetSuggestions = () => {}, ...props}: AutoCompleteSuggestionsPortalProps<TSuggestion>) {
     const StyleUtils = useStyleUtils();
-    const bottomPadding = getBottomSuggestionPadding();
+    const bottomPadding = Platform.OS === 'android' ? (getBottomSuggestionPadding as getBottomSuggestionPaddingAndroid)(bottom) : getBottomSuggestionPadding();
 
+    console.log('AutoCompleteSuggestionsPortal bottom', bottom);
     console.log('AutoCompleteSuggestionsPortal bottomPadding', bottomPadding);
-    console.log('AutoCompleteSuggestionsPortal bottom + bottomPadding', bottom + bottomPadding);
+    console.log('AutoCompleteSuggestionsPortal bottom + bottomPadding', bottom);
     const styles = useMemo(() => StyleUtils.getBaseAutoCompleteSuggestionContainerStyle({left, width, bottom: bottom + bottomPadding}), [StyleUtils, left, width, bottom, bottomPadding]);
 
     if (!width) {
