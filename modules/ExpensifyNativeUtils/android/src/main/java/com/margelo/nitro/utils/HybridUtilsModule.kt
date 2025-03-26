3bd8809293e0c527f18e6d421c093b7262503371
Taras Perun <terry.perun@gmail.com>
2025-03-26 17:38:46 +0100

    specs + add empty module

---
+++ modules/ExpensifyNativeUtils/android/src/main/java/com/margelo/nitro/utils/HybridUtilsModule.kt
@@ -0,0 +1,14 @@
+package com.margelo.nitro.utils
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.provider.ContactsContract
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ThemedReactContext
+import com.margelo.nitro.NitroModules
+import com.margelo.nitro.core.Promise
+
+class HybridUtilsModule(val context: ThemedReactContext) : HybridUtilsModuleSpec() {
+}
