55703d19a220d31460c59bc951cfa2f398d78477
nkdengineer <nkdengineer@outlook.com>
2025-03-26 22:48:20 +0700

    add comment

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -514,6 +514,7 @@ function getReportSections(data: OnyxTypes.SearchResults['data'], metadata: Onyx
         }
     }
 
+    // Filter out reports with no transactions to prevent the wrong number of the selected options
     return Object.values(reportIDToTransactions).filter((report) => report.transactions.length);
 }
 
