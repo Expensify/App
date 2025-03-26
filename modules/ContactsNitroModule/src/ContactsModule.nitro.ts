defe8e9a3a7e8ccbfc80917a259f04d382c95be5
Taras Perun <terry.perun@gmail.com>
2025-03-26 12:02:13 +0100

    move from interface to type

--- modules/ContactsNitroModule/src/ContactsModule.nitro.ts
+++ modules/ContactsNitroModule/src/ContactsModule.nitro.ts
@@ -1,29 +1,20 @@
-import type { HybridObject } from 'react-native-nitro-modules'
+import type {HybridObject} from 'react-native-nitro-modules';
 
-interface StringHolder {
-  value: string
-}
+type StringHolder = {
+    value: string;
+};
 
-export interface Contact {
-  firstName?: string
-  lastName?: string
-  middleName?: string
-  phoneNumbers?: StringHolder[]
-  emailAddresses?: StringHolder[]
-  imageData?: string
-  thumbnailImageData?: string
-}
-export type ContactFields =
-  | 'FIRST_NAME'
-  | 'LAST_NAME'
-  | 'MIDDLE_NAME'
-  | 'PHONE_NUMBERS'
-  | 'EMAIL_ADDRESSES'
-  | 'IMAGE_DATA'
-  | 'THUMBNAIL_IMAGE_DATA'
-  | 'GIVEN_NAME_KEY'
+export type Contact = {
+    firstName?: string;
+    lastName?: string;
+    middleName?: string;
+    phoneNumbers?: StringHolder[];
+    emailAddresses?: StringHolder[];
+    imageData?: string;
+    thumbnailImageData?: string;
+};
+export type ContactFields = 'FIRST_NAME' | 'LAST_NAME' | 'MIDDLE_NAME' | 'PHONE_NUMBERS' | 'EMAIL_ADDRESSES' | 'IMAGE_DATA' | 'THUMBNAIL_IMAGE_DATA' | 'GIVEN_NAME_KEY';
 
-export interface ContactsModule
-  extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
-  getAll(keys: ContactFields[]): Promise<Contact[]>
+export interface ContactsModule extends HybridObject<{ios: 'swift'; android: 'kotlin'}> {
+    getAll(keys: ContactFields[]): Promise<Contact[]>;
 }
