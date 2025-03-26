112fc0c80a2fe87e5571d1d70fe8d1c4caecc6d1
Cristi Paval <cristi@expensify.com>
2025-03-26 15:57:27 +0200

    Merge pull request #59152 from Expensify/revert-58725-fix-update-receipt-expense-flow
    [CP Staging] Revert "Fix - Update the scanned expense creation flow to allow user to see full receipt"
    
    (cherry picked from commit 7efe082d790bd14c00b697eaaafbadaff3468208)
    
    (CP triggered by cristipaval)

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -4570,8 +4570,6 @@ const styles = (theme: ThemeColors) =>
             borderRadius: 16,
             margin: 20,
             overflow: 'hidden',
-            borderWidth: 1,
-            borderColor: theme.border,
         },
 
         reportPreviewBox: {
@@ -4744,8 +4742,8 @@ const styles = (theme: ThemeColors) =>
             ...spacing.mh5,
             ...spacing.mv3,
             overflow: 'hidden',
-            borderWidth: 1,
-            borderColor: theme.border,
+            borderWidth: 2,
+            borderColor: theme.cardBG,
             borderRadius: variables.componentBorderRadiusLarge,
             height: 180,
             maxWidth: 360,
