c542c3f75d75163cd667169a4769e1abd9c19a41
FitseTLT <engfitsum@gmail.com>
2025-03-28 18:06:52 +0300

    avoid empty policy chats on LHN

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -7472,7 +7472,7 @@ function reasonForReportToBeInOptionList({
     }
 
     // Hide chats between two users that haven't been commented on from the LNH
-    if (excludeEmptyChats && isEmptyChat && isChatReport(report) && !isChatRoom(report) && !isPolicyExpenseChat(report) && !isSystemChat(report) && !isGroupChat(report) && canHideReport) {
+    if (excludeEmptyChats && isEmptyChat && isChatReport(report) && !isChatRoom(report) && !isSystemChat(report) && !isGroupChat(report) && canHideReport) {
         return null;
     }
 
