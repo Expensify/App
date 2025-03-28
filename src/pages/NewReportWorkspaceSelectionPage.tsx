575f1d467f730332a716e2cd0c076c28a5cc0a7b
Jakub Szymczak <szymczak.jakub12@gmail.com>
2025-03-28 14:21:49 +0100

    fix wrong navigation

--- src/pages/NewReportWorkspaceSelectionPage.tsx
+++ src/pages/NewReportWorkspaceSelectionPage.tsx
@@ -47,7 +47,7 @@ function NewReportWorkspaceSelectionPage() {
             }
             const optimisticReportID = createNewReport(currentUserPersonalDetails, policyID);
             Navigation.setNavigationActionToMicrotaskQueue(() => {
-                Navigation.navigate(ROUTES.SEARCH_MONEY_REQUEST_REPORT.getRoute({reportID: optimisticReportID, backTo: Navigation.getActiveRoute()}), {forceReplace: true});
+                Navigation.navigate(ROUTES.SEARCH_MONEY_REQUEST_REPORT.getRoute({reportID: optimisticReportID}), {forceReplace: true});
             });
         },
         [currentUserPersonalDetails],
