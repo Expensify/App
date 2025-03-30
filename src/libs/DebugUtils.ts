15fe3344b02b9bf4e2f897feff10574e27cc7bc1
Hans <visaocuoitroi193@gmail.com>
2025-03-30 14:55:40 +0700

    fix tests

--- src/libs/DebugUtils.ts
+++ src/libs/DebugUtils.ts
@@ -798,6 +798,8 @@ function validateReportActionDraftProperty(key: keyof ReportAction, value: strin
                             ...CONST.REPORT.ACTIONABLE_REPORT_MENTION_WHISPER_RESOLUTION,
                         },
                         deleted: 'string',
+                        bankAccountID: 'string',
+                        payAsBusiness: 'string'
                     }),
                 () =>
                     validateObject<ObjectElement<ReportAction, 'message'>>(value, {
@@ -881,6 +883,8 @@ function validateReportActionDraftProperty(key: keyof ReportAction, value: strin
                         expenseReportID: 'string',
                         resolution: 'string',
                         deleted: 'string',
+                        bankAccountID: 'string',
+                        payAsBusiness: 'string'
                     }),
             );
     }
