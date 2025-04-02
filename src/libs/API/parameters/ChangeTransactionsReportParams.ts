2a56d0726051197056fe31848009985dbdc4925c
Artem Makushov <waterim3009@gmail.com>
2025-04-02 17:29:43 +0200

    update params

--- src/libs/API/parameters/ChangeTransactionsReportParams.ts
+++ src/libs/API/parameters/ChangeTransactionsReportParams.ts
@@ -1,6 +1,17 @@
+/**
+ * A map linking the optimistic MOVEDTRANSACTION or UNREPORTEDTRANSACTION reportActionID to the transactionID.
+ * If we're creating the transactionThread as part of moving the transaction, we should also send the optimistic
+ * transactionThreadReportID and transactionThreadCreatedReportActionID
+ */
+type TransactionThreadInfo = {
+    transactionThreadReportID?: string;
+    transactionThreadCreatedReportActionID?: string;
+    movedReportActionID: string;
+};
 type ChangeTransactionsReportParams = {
     transactionList: string;
     reportID: string;
-    reportActionIDToThreadReportIDMap: Record<string, string>; // A map linking the optimistic MOVEDTRANSACTION or UNREPORTEDTRANSACTION reportActionID to the transaction thread reportID.
+    transactionIDToReportActionAndThreadData: Record<string, TransactionThreadInfo>; // A map of transactionID to TransactionThreadInfo
 };
+
 export default ChangeTransactionsReportParams;
