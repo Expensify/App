ebb56f05bc2b50a92ee00e3533774e7073fdb3df
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-04-03 10:46:24 +0200

    fix GroupChatNameTest

--- src/libs/actions/OnyxDerived/configs/reportAttributes.ts
+++ src/libs/actions/OnyxDerived/configs/reportAttributes.ts
@@ -5,7 +5,7 @@ import type {ReportAttributes} from '@src/types/onyx';
 
 export default createOnyxDerivedValueConfig({
     key: ONYXKEYS.DERIVED.REPORT_ATTRIBUTES,
-    dependencies: [ONYXKEYS.COLLECTION.REPORT],
+    dependencies: [ONYXKEYS.COLLECTION.REPORT, ONYXKEYS.PERSONAL_DETAILS_LIST],
     compute: ([reports], {currentValue, sourceValues}) => {
         const reportUpdates = sourceValues?.[ONYXKEYS.COLLECTION.REPORT];
         if (!reports) {
