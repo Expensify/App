661c6dfa08ffe0b4d2296a6077da661eff5bdd9d
Mateusz Titz <titzmateusz@gmail.com>
2025-04-03 16:15:15 +0200

    Improve onPress handling on report item row

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -291,14 +291,16 @@ function Search({queryJSON, currentSearchResults, lastNonEmptySearchResults, onS
     const openReport = useCallback(
         (item: TransactionListItemType | ReportListItemType | ReportActionListItemType, isOpenedAsReport?: boolean) => {
             const isFromSelfDM = item.reportID === CONST.REPORT.UNREPORTED_REPORTID;
-            let reportID = isTransactionListItemType(item) && (!item.isFromOneTransactionReport || isFromSelfDM) ? item.transactionThreadReportID : item.reportID;
+            const isTransactionItem = isTransactionListItemType(item);
+
+            let reportID = isTransactionItem && (!item.isFromOneTransactionReport || isFromSelfDM) ? item.transactionThreadReportID : item.reportID;
 
             if (!reportID) {
                 return;
             }
 
             const backTo = Navigation.getActiveRoute();
-            const shouldHandleTransactionAsReport = isReportListItemType(item) || isOpenedAsReport;
+            const shouldHandleTransactionAsReport = isReportListItemType(item) || (isTransactionItem && isOpenedAsReport);
 
             if (canUseTableReportView && shouldHandleTransactionAsReport) {
                 Navigation.navigate(ROUTES.SEARCH_MONEY_REQUEST_REPORT.getRoute({reportID, backTo}));
@@ -306,7 +308,7 @@ function Search({queryJSON, currentSearchResults, lastNonEmptySearchResults, onS
             }
 
             // If we're trying to open a legacy transaction without a transaction thread, let's create the thread and navigate the user
-            if (isTransactionListItemType(item) && reportID === '0' && item.moneyRequestReportActionID) {
+            if (isTransactionItem && reportID === '0' && item.moneyRequestReportActionID) {
                 reportID = generateReportID();
                 updateSearchResultsWithTransactionThreadReportID(hash, item.transactionID, reportID);
                 Navigation.navigate(
