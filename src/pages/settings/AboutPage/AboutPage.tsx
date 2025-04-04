9843e13cf293b89370dcd7a7090f76d4367c3634
Wojciech Boman <wojciechboman@gmail.com>
2025-04-04 18:52:45 +0200

    Set shouldPopToTop to true in Navigation.goBack calls in settings central screens

--- src/pages/settings/AboutPage/AboutPage.tsx
+++ src/pages/settings/AboutPage/AboutPage.tsx
@@ -142,7 +142,7 @@ function AboutPage() {
                 title={translate('initialSettingsPage.about')}
                 shouldShowBackButton={shouldUseNarrowLayout}
                 shouldDisplaySearchRouter
-                onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS)}
+                onBackButtonPress={() => Navigation.goBack(ROUTES.SETTINGS, {shouldPopToTop: true})}
                 icon={Illustrations.PalmTree}
                 shouldUseHeadlineHeader
             />
