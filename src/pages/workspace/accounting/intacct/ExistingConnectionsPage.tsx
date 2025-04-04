5b3e2c4a997fb0f72c715fd221e1a0892d619117
nkdengineer <nkdengineer@outlook.com>
2025-04-04 15:00:59 +0700

    remove unused View

--- src/pages/workspace/accounting/intacct/ExistingConnectionsPage.tsx
+++ src/pages/workspace/accounting/intacct/ExistingConnectionsPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {View} from 'react-native';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import {LinkCopy} from '@components/Icon/Expensicons';
 import MenuItem from '@components/MenuItem';
