6a7f9c6c9e3e1659b010748ff604bb5f0995180f
Michał Skałka <michal.skalka@swmansion.com>
2025-04-04 11:23:35 +0200

    Add In-App Provisioning util functions

--- src/libs/Wallet/index.ios.ts
+++ src/libs/Wallet/index.ios.ts
@@ -1,9 +1,14 @@
-import {addCardToAppleWallet} from '@expensify/react-native-wallet';
+import {addCardToAppleWallet, checkWalletAvailability, getCardStatus} from '@expensify/react-native-wallet';
 import type {IOSCardData} from '@expensify/react-native-wallet/lib/typescript/src/NativeWallet';
+import {Alert} from 'react-native';
 import {issuerEncryptPayloadCallback} from '@libs/actions/Wallet';
 import type {Card} from '@src/types/onyx';
 
-export default function handleAddCardToWallet(card: Card, cardHolderName: string) {
+function checkIfWalletIsAvailable(): Promise<boolean> {
+    return checkWalletAvailability();
+}
+
+function handleAddCardToWallet(card: Card, cardHolderName: string) {
     const data = {
         network: 'VISA',
         lastDigits: card.lastFourPAN,
@@ -12,8 +17,27 @@ export default function handleAddCardToWallet(card: Card, cardHolderName: string
     } as IOSCardData;
 
     addCardToAppleWallet(data, issuerEncryptPayloadCallback)
-        .then(() => console.log('DONE'))
+        .then(() => {
+            Alert.alert('Card added to wallet successfully');
+        })
         .catch((e) => {
             console.log('ADD ERROR: ', e);
+            Alert.alert('Failed to add card to wallet', 'Please try again later.');
         });
 }
+
+function isCardInWallet(card: Card): Promise<boolean> {
+    if (card.lastFourPAN) {
+        return getCardStatus(card.lastFourPAN)
+            .then((status) => {
+                return status === 'active';
+            })
+            .catch((e) => {
+                console.log('STATUS ERROR', e);
+                return Promise.resolve(card.state === 6);
+            });
+    }
+    return Promise.resolve(card.state === 6);
+}
+
+export {handleAddCardToWallet, isCardInWallet, checkIfWalletIsAvailable};
