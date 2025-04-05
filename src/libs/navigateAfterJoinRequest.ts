f0af22eef370194fc817c811b124c33002c9cbcf
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-04-05 12:09:12 +0800

    fix dupe settings screen on the stack

--- src/libs/navigateAfterJoinRequest.ts
+++ src/libs/navigateAfterJoinRequest.ts
@@ -1,12 +1,10 @@
-import getIsSmallScreenWidth from '@libs/getIsSmallScreenWidth';
 import Navigation from '@navigation/Navigation';
 import ROUTES from '@src/ROUTES';
 
 const navigateAfterJoinRequest = () => {
     Navigation.goBack(undefined, {shouldPopToTop: true});
-    if (getIsSmallScreenWidth()) {
-        Navigation.navigate(ROUTES.SETTINGS);
-    }
-    Navigation.navigate(ROUTES.SETTINGS_WORKSPACES.route);
+    Navigation.setNavigationActionToMicrotaskQueue(() => {
+        Navigation.navigate(ROUTES.SETTINGS_WORKSPACES.route);
+    });
 };
 export default navigateAfterJoinRequest;
