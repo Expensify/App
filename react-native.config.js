e542e3b0b51c9f943802356397bdd7234260b4ed
c3024 <chaitanya3024@gmail.com>
2025-04-06 18:35:16 +0530

    remove linking changes in react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,5 @@
 const iosSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'ios' : 'ios';
 const androidSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'android' : 'android';
-const linkAssets = require('@react-native-community/cli-link-assets');
 
 module.exports = {
     project: {
@@ -8,5 +7,4 @@ module.exports = {
         android: {sourceDir: androidSourceDir},
     },
     assets: ['./assets/fonts/native'],
-    commands: [linkAssets.commands.linkAssets],
 };
