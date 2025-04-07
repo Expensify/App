172138c33b276e1d46d52c52303eef93510be8c8
Hubert Sosinski <hubert.sosinski@callstack.com>
2025-04-07 14:14:04 +0200

    Add long press to copy in MoneyRequest view

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -380,6 +380,7 @@ function MoneyRequestView({report, shouldShowAnimatedBackground, readonly = fals
                             ROUTES.MONEY_REQUEST_STEP_DISTANCE.getRoute(CONST.IOU.ACTION.EDIT, iouType, transaction.transactionID, report.reportID, getReportRHPActiveRoute()),
                         );
                     }}
+                    copyValue={!canEditDistance ? distanceToDisplay : undefined}
                 />
             </OfflineWithFeedback>
             <OfflineWithFeedback pendingAction={getPendingFieldAction('customUnitRateID')}>
@@ -647,6 +648,7 @@ function MoneyRequestView({report, shouldShowAnimatedBackground, readonly = fals
                             brickRoadIndicator={getErrorForField('merchant') ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : undefined}
                             errorText={getErrorForField('merchant')}
                             numberOfLinesTitle={0}
+                            copyValue={!canEditMerchant ? updatedMerchantTitle : undefined}
                         />
                     </OfflineWithFeedback>
                 )}
