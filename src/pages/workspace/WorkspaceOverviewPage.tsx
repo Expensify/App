d7eb6cb5f0fb2d1948825f8cd9ec3649abc6050f
Hubert Sosinski <hubert.sosinski@callstack.com>
2025-04-07 14:24:45 +0200

    Add long press to copy to workspace overview

--- src/pages/workspace/WorkspaceOverviewPage.tsx
+++ src/pages/workspace/WorkspaceOverviewPage.tsx
@@ -329,6 +329,7 @@ function WorkspaceOverviewPage({policyDraft, policy: policyProp, route}: Workspa
                                         interactive={!readOnly}
                                         wrapperStyle={styles.sectionMenuItemTopDescription}
                                         onPress={onPressAddress}
+                                        copyValue={formattedAddress}
                                     />
                                 </View>
                             </OfflineWithFeedback>
