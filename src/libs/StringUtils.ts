6bcc151af985ea6ea417c654127ae4912d3a28a5
c3024 <chaitanya3024@gmail.com>
2025-04-07 20:32:29 +0530

    fix isEmptyString function

--- src/libs/StringUtils.ts
+++ src/libs/StringUtils.ts
@@ -24,7 +24,7 @@ function isEmptyString(value: string): boolean {
     // \p{C} matches all 'Other' characters
     // \p{Z} matches all separators (spaces etc.)
     // Source: http://www.unicode.org/reports/tr18/#General_Category_Property
-    transformed = value.replace(CONST.REGEX.INVISIBLE_CHARACTERS_GROUPS, '');
+    transformed = transformed.replace(CONST.REGEX.INVISIBLE_CHARACTERS_GROUPS, '');
 
     // Remove other invisible characters that are not in the above unicode categories
     transformed = transformed.replace(CONST.REGEX.OTHER_INVISIBLE_CHARACTERS, '');
