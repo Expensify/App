120b01a0b2655124c0ad8201cddd1cca696d24a2
Zuzanna Furtak <zuzkafurtak@gmail.com>
2025-04-07 11:39:12 +0200

    Fix test

--- config/webpack/webpack.common.ts
+++ config/webpack/webpack.common.ts
@@ -41,6 +41,7 @@ const includeModules = [
     '@react-native/assets',
     'expo',
     'expo-av',
+    'expo-image',
     'expo-image-manipulator',
     'expo-modules-core',
     'react-native-webrtc-web-shim',
@@ -203,12 +204,16 @@ const getCommonConfiguration = ({file = '.env', platform = 'web'}: Environment):
                 test: /\.(png|jpe?g|gif)$/i,
                 type: 'asset',
             },
-
-            // Load svg images
+            // Load svg images, include images from react-native-wallet
             {
                 test: /\.svg$/,
                 resourceQuery: {not: [/raw/]},
-                exclude: /node_modules/,
+                exclude: (p) => {
+                    if (/node_modules/.test(p)) {
+                        return !/node_modules\/@expensify\/react-native-wallet/.test(p);
+                    }
+                    return false;
+                },
                 use: [
                     {
                         loader: '@svgr/webpack',
