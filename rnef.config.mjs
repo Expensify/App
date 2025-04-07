2bcdcaa360025038e355e4e9c2912c2ca6564681
Adam Horodyski <ad.horodyski@gmail.com>
2025-04-07 18:33:38 +0200

    chore: test project_root_path env var

--- rnef.config.mjs
+++ rnef.config.mjs
@@ -2,6 +2,8 @@ import {platformAndroid} from '@rnef/platform-android';
 import {platformIOS} from '@rnef/platform-ios';
 import {pluginMetro} from '@rnef/plugin-metro';
 
+console.log({ rnefRoot: process.env.PROJECT_ROOT_PATH });
+
 export default {
     remoteCacheProvider: null,
     bundler: pluginMetro(),
