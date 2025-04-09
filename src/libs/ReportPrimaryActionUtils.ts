97861c5aae0501f8e0dff86856f03263cbceb819
nkdengineer <nkdengineer@outlook.com>
2025-04-09 20:17:23 +0700

    fix: approve button still appears after deleting all transactions

--- src/libs/ReportPrimaryActionUtils.ts
+++ src/libs/ReportPrimaryActionUtils.ts
@@ -51,7 +51,7 @@ function isApproveAction(report: Report, reportTransactions: Transaction[], poli
     const isReportApprover = isApproverUtils(policy, getCurrentUserAccountID());
     const isApprovalEnabled = policy?.approvalMode && policy.approvalMode !== CONST.POLICY.APPROVAL_MODE.OPTIONAL;
 
-    if (!isExpenseReport || !isReportApprover || !isApprovalEnabled) {
+    if (!isExpenseReport || !isReportApprover || !isApprovalEnabled || reportTransactions.length === 0) {
         return false;
     }
 
