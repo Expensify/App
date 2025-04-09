3dab9c58d0ecbe3aa03dac1585051c3fdbc02c44
daledah <daleda261@outlook.com>
2025-04-09 13:44:55 +0700

    fix: correct go back behaviour for workspace list page

--- src/pages/workspace/WorkspaceNewRoomPage.tsx
+++ src/pages/workspace/WorkspaceNewRoomPage.tsx
@@ -228,7 +228,7 @@ function WorkspaceNewRoomPage() {
                 success
                 large
                 text={translate('footer.learnMore')}
-                onPress={() => Navigation.navigate(ROUTES.SETTINGS_WORKSPACES.route)}
+                onPress={() => Navigation.navigate(ROUTES.SETTINGS_WORKSPACES.getRoute(Navigation.getActiveRoute()))}
                 style={[styles.mh5, styles.mb5]}
             />
             {isSmallScreenWidth && (
