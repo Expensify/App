3fae7b866587e4228c46bccfb7061deeec997b0d
daledah <daleda261@outlook.com>
2025-04-09 16:40:09 +0700

    fix: lint

--- src/pages/TransactionDuplicate/ReviewDescription.tsx
+++ src/pages/TransactionDuplicate/ReviewDescription.tsx
@@ -9,7 +9,7 @@ import {setReviewDuplicatesKey} from '@libs/actions/Transaction';
 import type {PlatformStackRouteProp} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {TransactionDuplicateNavigatorParamList} from '@libs/Navigation/types';
 import Parser from '@libs/Parser';
-import * as TransactionUtils from '@libs/TransactionUtils';
+import {compareDuplicateTransactionFields, getTransactionID} from '@libs/TransactionUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type SCREENS from '@src/SCREENS';
 import type {FieldItemType} from './ReviewFields';
@@ -18,9 +18,9 @@ import ReviewFields from './ReviewFields';
 function ReviewDescription() {
     const route = useRoute<PlatformStackRouteProp<TransactionDuplicateNavigatorParamList, typeof SCREENS.TRANSACTION_DUPLICATE.DESCRIPTION>>();
     const {translate} = useLocalize();
-    const transactionID = TransactionUtils.getTransactionID(route.params.threadReportID ?? '');
+    const transactionID = getTransactionID(route.params.threadReportID ?? '');
     const [reviewDuplicates] = useOnyx(ONYXKEYS.REVIEW_DUPLICATES);
-    const compareResult = TransactionUtils.compareDuplicateTransactionFields(transactionID, reviewDuplicates?.reportID ?? '-1');
+    const compareResult = compareDuplicateTransactionFields(transactionID, reviewDuplicates?.reportID ?? '-1');
     const stepNames = Object.keys(compareResult.change ?? {}).map((key, index) => (index + 1).toString());
     const {currentScreenIndex, goBack, navigateToNextScreen} = useReviewDuplicatesNavigation(
         Object.keys(compareResult.change ?? {}),
