083846e8b36012242b0d1461174f95eabc71378a
rory <rory@expensify.com>
2025-04-10 09:27:28 -0700

    Disallow labeled statements, with, and for...in loops

--- .eslintrc.js
+++ .eslintrc.js
@@ -237,6 +237,22 @@ module.exports = {
                 selector: 'TSEnumDeclaration',
                 message: "Please don't declare enums, use union types instead.",
             },
+
+            // These are the original rules from AirBnB's style guide, modified to allow for...of loops
+            {
+                selector: 'ForInStatement',
+                message:
+                    'for..in loops iterate over the entire prototype chain, which is virtually never what you want. ' +
+                    'Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'LabeledStatement',
+                message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize. It is also deprecated',
+            },
         ],
         'no-restricted-properties': [
             'error',
