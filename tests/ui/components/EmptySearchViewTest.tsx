ab11090585ebabc683e9d69ef8d1f2fcc2acf68d
VH <hoangzinhvn@gmail.com>
2025-04-13 15:35:09 +0700

    fix prettier

--- tests/ui/components/EmptySearchViewTest.tsx
+++ tests/ui/components/EmptySearchViewTest.tsx
@@ -1,8 +1,8 @@
 import {render, screen} from '@testing-library/react-native';
 import React from 'react';
 import Onyx from 'react-native-onyx';
-import OnyxProvider from '@components/OnyxProvider';
 import {LocaleContextProvider} from '@components/LocaleContextProvider';
+import OnyxProvider from '@components/OnyxProvider';
 import EmptySearchView from '@pages/Search/EmptySearchView';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -13,9 +13,7 @@ jest.mock('@components/ConfirmedRoute.tsx');
 function Wrapper({children}: {children: React.ReactNode}) {
     return (
         <OnyxProvider>
-            <LocaleContextProvider>
-                {children}
-            </LocaleContextProvider>
+            <LocaleContextProvider>{children}</LocaleContextProvider>
         </OnyxProvider>
     );
 }
@@ -35,40 +33,39 @@ describe('EmptySearchView', () => {
         it('should display correct buttons and subtitle when user has not clicked on "Take a tour"', async () => {
             // Given user hasn't clicked on "Take a tour" yet
             await Onyx.merge(ONYXKEYS.NVP_ONBOARDING, {selfTourViewed: false});
-    
+
             // Render component
             render(
                 <Wrapper>
-                    <EmptySearchView 
-                        type={dataType} 
-                        hasResults={false} 
+                    <EmptySearchView
+                        type={dataType}
+                        hasResults={false}
                     />
-                </Wrapper>
+                </Wrapper>,
             );
-    
+
             // Then it should display create expenses and take a tour buttons
             expect(await screen.findByText('Create expense')).toBeVisible();
             expect(await screen.findByText('Take a tour')).toBeVisible();
 
             // And correct modal subtitle
             expect(screen.getByText('Create an expense or take a tour of Expensify to learn more.')).toBeVisible();
-
         });
-    
+
         it('should display correct buttons and subtitle when user already did "Take a tour"', async () => {
             // Given user clicked on "Take a tour"
             await Onyx.merge(ONYXKEYS.NVP_ONBOARDING, {selfTourViewed: true});
-    
+
             // Render component
             render(
                 <Wrapper>
-                    <EmptySearchView 
-                        type={dataType} 
-                        hasResults={false} 
+                    <EmptySearchView
+                        type={dataType}
+                        hasResults={false}
                     />
-                </Wrapper>
+                </Wrapper>,
             );
-    
+
             // Then it should display create expenses button
             expect(await screen.findByText('Create expense')).toBeVisible();
             expect(screen.queryByText('Take a tour')).not.toBeOnTheScreen();
@@ -76,7 +73,7 @@ describe('EmptySearchView', () => {
             // And correct modal subtitle
             expect(screen.getByText('Use the green button below to create an expense.')).toBeVisible();
         });
-    })
+    });
 
     describe('type is Invoice', () => {
         const dataType = CONST.SEARCH.DATA_TYPES.INVOICE;
@@ -84,40 +81,39 @@ describe('EmptySearchView', () => {
         it('should display correct buttons and subtitle when user has not clicked on "Take a tour"', async () => {
             // Given user hasn't clicked on "Take a tour" yet
             await Onyx.merge(ONYXKEYS.NVP_ONBOARDING, {selfTourViewed: false});
-    
+
             // Render component
             render(
                 <Wrapper>
-                    <EmptySearchView 
-                        type={dataType} 
-                        hasResults={false} 
+                    <EmptySearchView
+                        type={dataType}
+                        hasResults={false}
                     />
-                </Wrapper>
+                </Wrapper>,
             );
-    
+
             // Then it should display send invoice and take a tour buttons
             expect(await screen.findByText('Send invoice')).toBeVisible();
             expect(await screen.findByText('Take a tour')).toBeVisible();
 
             // And correct modal subtitle
             expect(screen.getByText('Send an invoice or take a tour of Expensify to learn more.')).toBeVisible();
-
         });
-    
+
         it('should display correct buttons and subtitle when user already did "Take a tour"', async () => {
             // Given user clicked on "Take a tour"
             await Onyx.merge(ONYXKEYS.NVP_ONBOARDING, {selfTourViewed: true});
-    
+
             // Render component
             render(
                 <Wrapper>
-                    <EmptySearchView 
-                        type={dataType} 
-                        hasResults={false} 
+                    <EmptySearchView
+                        type={dataType}
+                        hasResults={false}
                     />
-                </Wrapper>
+                </Wrapper>,
             );
-    
+
             // Then it should display Send invoice button
             expect(await screen.findByText('Send invoice')).toBeVisible();
             expect(screen.queryByText('Take a tour')).not.toBeOnTheScreen();
@@ -125,5 +121,5 @@ describe('EmptySearchView', () => {
             // And correct modal subtitle
             expect(screen.getByText('Use the green button below to send an invoice.')).toBeVisible();
         });
-    })
+    });
 });
