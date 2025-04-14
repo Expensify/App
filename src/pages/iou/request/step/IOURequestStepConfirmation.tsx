6ec8f3b1d8a1e2a64cb67b9d84d1f41d8410dcd7
Artem Makushov <39777589+waterim@users.noreply.github.com>
2025-04-14 23:30:55 +0200

    Update IOURequestStepConfirmation.tsx
    Co-authored-by: rayane-d <77965000+rayane-d@users.noreply.github.com>

--- src/pages/iou/request/step/IOURequestStepConfirmation.tsx
+++ src/pages/iou/request/step/IOURequestStepConfirmation.tsx
@@ -92,7 +92,7 @@ function IOURequestStepConfirmation({
     const [userLocation] = useOnyx(ONYXKEYS.USER_LOCATION);
 
     /*
-     * We want to use a repport from the transaction if it exists
+     * We want to use a report from the transaction if it exists
      * Also if the report was submitted and delayed submittion is on, then we should use an initial report
      */
     const transactionReport = getReportOrDraftReport(transaction?.reportID);
