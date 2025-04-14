5490a990c8f176831d84c643429dc15eb210ebd5
krishna2323 <belivethatkg@gmail.com>
2025-04-14 21:17:53 +0530

    remove userSelect.userSelectNone.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/styles/utils/editedLabelStyles/index.ts
+++ src/styles/utils/editedLabelStyles/index.ts
@@ -2,14 +2,11 @@
 import display from '@styles/utils/display';
 // eslint-disable-next-line no-restricted-imports
 import flex from '@styles/utils/flex';
-// eslint-disable-next-line no-restricted-imports
-import userSelect from '@styles/utils/userSelect';
 import type EditedLabelStyles from './types';
 
 const editedLabelStyles: EditedLabelStyles = {
     ...display.dInlineFlex,
     ...flex.alignItemsBaseline,
-    ...userSelect.userSelectNone,
 };
 
 export default editedLabelStyles;
