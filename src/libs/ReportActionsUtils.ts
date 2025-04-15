aae406d9c34284b43d82b721eb5c91695ab4c821
Carlos Martins <cmartins@expensify.com>
2025-04-15 17:48:49 -0600

    deprecate actions

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -745,6 +745,8 @@ function isReportActionDeprecated(reportAction: OnyxEntry<ReportAction>, key: st
         CONST.REPORT.ACTIONS.TYPE.REIMBURSEMENT_SETUP_REQUESTED,
         CONST.REPORT.ACTIONS.TYPE.DONATION,
         CONST.REPORT.ACTIONS.TYPE.REIMBURSED,
+        CONST.REPORT.ACTIONS.TYPE.UNREPORTED_TRANSACTION,
+        CONST.REPORT.ACTIONS.TYPE.MOVED_TRANSACTION,
     ];
     if (deprecatedOldDotReportActions.includes(reportAction.actionName)) {
         Log.info('Front end filtered out reportAction for being an older, deprecated report action', false, reportAction);
