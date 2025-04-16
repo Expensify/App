92df591066a469bb2098b9bca876130d92ff0f00
VickyStash <vikstash@gmail.com>
2025-04-16 10:56:46 +0200

    Update useCardFeed hook

--- src/hooks/useCardFeeds.tsx
+++ src/hooks/useCardFeeds.tsx
@@ -2,34 +2,47 @@ import type {ResultMetadata} from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {CardFeeds, CompanyCardFeed} from '@src/types/onyx';
 import useOnyx from './useOnyx';
+import useWorkspaceAccountID from './useWorkspaceAccountID';
 
 const useCardFeeds = (policyID: string | undefined): [CardFeeds | undefined, ResultMetadata] => {
+    const workspaceAccountID = useWorkspaceAccountID(policyID);
     const [allFeeds, allFeedsResult] = useOnyx(ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER);
+    const defaultFeed = allFeeds?.[`${ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER}${workspaceAccountID}`];
+    const {companyCards = {}, companyCardNicknames = {}, oAuthAccountDetails = {}} = defaultFeed?.settings ?? {};
 
     if (!policyID || !allFeeds) {
         return [undefined, allFeedsResult];
     }
 
-    const workspaceFeeds: {settings: Required<CardFeeds['settings']>} = {settings: {companyCards: {}, companyCardNicknames: {}, oAuthAccountDetails: {}}};
-
-    Object.values(allFeeds).forEach((feed) => {
-        if (!feed) {
-            return;
-        }
-        const companyCards = feed.settings.companyCards;
-        Object.entries(companyCards ?? {}).forEach(([key, feedSettings]) => {
-            const feedName = key as CompanyCardFeed;
-            if (feedSettings.preferredPolicy === policyID && !workspaceFeeds.settings.companyCards[feedName]) {
-                workspaceFeeds.settings.companyCards[feedName] = feedSettings;
-                if (feed.settings.oAuthAccountDetails?.[feedName]) {
-                    workspaceFeeds.settings.oAuthAccountDetails[feedName] = feed.settings.oAuthAccountDetails[feedName];
-                }
-                if (feed.settings.companyCardNicknames?.[feedName]) {
-                    workspaceFeeds.settings.companyCardNicknames[feedName] = feed.settings.companyCardNicknames[feedName];
-                }
+    const workspaceFeeds = Object.values(allFeeds).reduce<CardFeeds & {settings: Required<CardFeeds['settings']>}>(
+        (acc, feed) => {
+            if (!feed?.settings?.companyCards) {
+                return acc;
             }
-        });
-    });
+
+            Object.entries(feed.settings.companyCards).forEach(([key, feedSettings]) => {
+                const feedName = key as CompanyCardFeed;
+                const feedOAuthAccountDetails = feed.settings.oAuthAccountDetails?.[feedName];
+                const feedCompanyCardNicknames = feed.settings.companyCardNicknames?.[feedName];
+
+                if (feedSettings.preferredPolicy !== policyID || acc.settings.companyCards[feedName]) {
+                    return;
+                }
+
+                acc.settings.companyCards[feedName] = feedSettings;
+
+                if (feedOAuthAccountDetails) {
+                    acc.settings.oAuthAccountDetails[feedName] = feedOAuthAccountDetails;
+                }
+                if (feedCompanyCardNicknames) {
+                    acc.settings.companyCardNicknames[feedName] = feedCompanyCardNicknames;
+                }
+            });
+
+            return acc;
+        },
+        {settings: {companyCards, companyCardNicknames, oAuthAccountDetails}},
+    );
 
     return [workspaceFeeds, allFeedsResult];
 };
