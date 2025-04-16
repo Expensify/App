a34ff7803818d37e560c91505aec75ca6372c40d
Rushat Gabhane <rushatgabhane@gmail.com>
2025-04-16 23:09:33 +0200

    move to shouldShowBanner util function

--- src/pages/settings/Subscription/CardSection/CardSection.tsx
+++ src/pages/settings/Subscription/CardSection/CardSection.tsx
@@ -8,6 +8,7 @@ import * as Expensicons from '@components/Icon/Expensicons';
 import MenuItem from '@components/MenuItem';
 import Section from '@components/Section';
 import Text from '@components/Text';
+import useHasTeam2025Pricing from '@hooks/useHasTeam2025Pricing';
 import useLocalize from '@hooks/useLocalize';
 import useNetwork from '@hooks/useNetwork';
 import useSubscriptionPlan from '@hooks/useSubscriptionPlan';
@@ -48,6 +49,7 @@ function CardSection() {
     const [session] = useOnyx(ONYXKEYS.SESSION);
     const [fundList] = useOnyx(ONYXKEYS.FUND_LIST);
     const [purchaseList] = useOnyx(ONYXKEYS.PURCHASE_LIST);
+    const hasTeam2025Pricing = useHasTeam2025Pricing();
     const subscriptionPlan = useSubscriptionPlan();
     const [subscriptionRetryBillingStatusPending] = useOnyx(ONYXKEYS.SUBSCRIPTION_RETRY_BILLING_STATUS_PENDING);
     const [subscriptionRetryBillingStatusSuccessful] = useOnyx(ONYXKEYS.SUBSCRIPTION_RETRY_BILLING_STATUS_SUCCESSFUL);
@@ -113,7 +115,7 @@ function CardSection() {
     };
 
     let BillingBanner: React.ReactNode | undefined;
-    if (shouldShowDiscountBanner()) {
+    if (shouldShowDiscountBanner(hasTeam2025Pricing, subscriptionPlan)) {
         BillingBanner = <EarlyDiscountBanner isSubscriptionPage />;
     } else if (shouldShowPreTrialBillingBanner()) {
         BillingBanner = <PreTrialBillingBanner />;
