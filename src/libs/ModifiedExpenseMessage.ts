bbc5f078aa5b2b1ecc1b74c2c12724cc47f98933
FitseTLT <engfitsum@gmail.com>
2025-04-16 18:26:44 +0300

    fix lint

--- src/libs/ModifiedExpenseMessage.ts
+++ src/libs/ModifiedExpenseMessage.ts
@@ -11,6 +11,7 @@ import {translateLocal} from './Localize';
 import Log from './Log';
 import Parser from './Parser';
 import {getCleanedTagName, getSortedTagKeys} from './PolicyUtils';
+// eslint-disable-next-line import/no-cycle
 import {getOriginalMessage, isModifiedExpenseAction} from './ReportActionsUtils';
 // eslint-disable-next-line import/no-cycle
 import {buildReportNameFromParticipantNames, getPolicyExpenseChatName, getPolicyName, getRootParentReport, isPolicyExpenseChat, isSelfDM} from './ReportUtils';
