596d3265777ba66a86558ea8ecf82e6127b02fce
Amy Evans <amy@expensify.com>
2025-04-16 15:34:32 -0400

    Actually these keys are often undefined

--- src/hooks/useHasTeam2025Pricing.tsx
+++ src/hooks/useHasTeam2025Pricing.tsx
@@ -4,8 +4,8 @@ import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 
 function useHasTeam2025Pricing(): boolean {
-    const [firstPolicyDate] = useOnyx(ONYXKEYS.NVP_PRIVATE_FIRST_POLICY_CREATED_DATE, {canBeMissing: false});
-    const [hasManualTeam2025Pricing] = useOnyx(ONYXKEYS.NVP_PRIVATE_MANUAL_TEAM_2025_PRICING, {canBeMissing: false});
+    const [firstPolicyDate] = useOnyx(ONYXKEYS.NVP_PRIVATE_FIRST_POLICY_CREATED_DATE, {canBeMissing: true});
+    const [hasManualTeam2025Pricing] = useOnyx(ONYXKEYS.NVP_PRIVATE_MANUAL_TEAM_2025_PRICING, {canBeMissing: true});
 
     if (hasManualTeam2025Pricing) {
         return true;
