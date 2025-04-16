b4bd67b724ffff80c97aa0b69787c7e66992cbde
Issa Nimaga <inimaga@expensify.com>
2025-04-16 19:27:52 +0100

    Add missing fields from user model in the account model

--- src/types/onyx/Account.ts
+++ src/types/onyx/Account.ts
@@ -198,6 +198,33 @@ type Account = {
         /** Errors while merging the account */
         errors: OnyxCommon.Errors;
     };
+
+    /** Whether or not the user is subscribed to news updates */
+    isSubscribedToNewsletter?: boolean;
+
+    /** Whether we should use the staging version of the secure API server */
+    shouldUseStagingServer?: boolean;
+
+    /** Whether or not the user is on a public domain email account or not */
+    isFromPublicDomain?: boolean;
+
+    /** Whether or not the user uses expensify card */
+    isUsingExpensifyCard?: boolean;
+
+    /** Whether Expensify Card approval flow is ongoing - checking loginList for private domains */
+    isCheckingDomain?: boolean;
+
+    /** Whether or not the user has lounge access */
+    hasLoungeAccess?: boolean;
+
+    /** error associated with adding a secondary login */
+    error?: string;
+
+    /** Whether the user is an Expensify Guide */
+    isGuide?: boolean;
+
+    /** Whether the debug mode is currently enabled */
+    isDebugModeEnabled?: boolean;
 };
 
 export default Account;
