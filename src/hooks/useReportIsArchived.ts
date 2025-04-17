d9ede178cd63a925453cfa139f5ab4c54b49c3c9
Tim Golen <tgolen@expensify.com>
2025-04-17 10:17:21 -0600

    Add hook

---
+++ src/hooks/useReportIsArchived.ts
@@ -0,0 +1,12 @@
+import {isArchivedReport} from '@libs/ReportUtils';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import useOnyx from './useOnyx';
+
+function useReportIsArchived(reportID?: string): [boolean] {
+    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${reportID ?? CONST.DEFAULT_NUMBER_ID}`);
+    const isReportArchived = isArchivedReport(reportNameValuePairs);
+    return [isReportArchived];
+}
+
+export default useReportIsArchived;
