c1aa0e33e2399423a6e17381160b37438e8bd8d6
Tim Golen <tgolen@expensify.com>
2025-04-17 11:31:48 -0600

    Use hook to detect if report is archived or not

--- tests/actions/TaskTest.ts
+++ tests/actions/TaskTest.ts
@@ -1,8 +1,12 @@
+import {renderHook} from '@testing-library/react-native';
 import Onyx from 'react-native-onyx';
-import {getFakeReport} from 'tests/utils/LHNTestUtils';
+import useReportIsArchived from '@hooks/useReportIsArchived';
 import {canModifyTask} from '@libs/actions/Task';
 import OnyxUpdateManager from '@src/libs/actions/OnyxUpdateManager';
 import ONYXKEYS from '@src/ONYXKEYS';
+import * as LHNTestUtils from '../utils/LHNTestUtils';
+
+jest.mock('@components/ConfirmedRoute.tsx');
 
 OnyxUpdateManager();
 describe('actions/Task', () => {
@@ -15,26 +19,46 @@ describe('actions/Task', () => {
     describe('canModifyTask', () => {
         const managerAccountID = 1;
         const employeeAccountID = 2;
-        const report = getFakeReport([managerAccountID, employeeAccountID]);
-        const archivedReport = getFakeReport([managerAccountID, employeeAccountID]);
-        const cancelledTaskReport = getFakeReport([managerAccountID, employeeAccountID]);
+        const report = LHNTestUtils.getFakeReport([managerAccountID, employeeAccountID]);
+        const archivedReport = LHNTestUtils.getFakeReport([managerAccountID, employeeAccountID]);
+        const cancelledTaskReport = LHNTestUtils.getFakeReport([managerAccountID, employeeAccountID]);
+
+        // Set the manager as the owner of each report
+        report.ownerAccountID = managerAccountID;
+        archivedReport.ownerAccountID = managerAccountID;
+        cancelledTaskReport.ownerAccountID = managerAccountID;
+
+        // This is what indicates that the report is a cancelled task report (see ReportUtils.isCanceledTaskReport())
+        cancelledTaskReport.isDeletedParentAction = true;
 
         beforeAll(async () => {
+            // Store all the necessary data in Onyx
             await Onyx.set(`${ONYXKEYS.COLLECTION.REPORT}${report.reportID}`, report);
             await Onyx.set(`${ONYXKEYS.COLLECTION.REPORT}${archivedReport.reportID}`, archivedReport);
             await Onyx.set(`${ONYXKEYS.COLLECTION.REPORT}${cancelledTaskReport.reportID}`, cancelledTaskReport);
 
-            // Set First conciergeChatReport1 to archived state
+            // This is what indicates that a report is archived (see ReportUtils.isArchivedReport())
             await Onyx.set(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${archivedReport.reportID}`, {
                 private_isArchived: new Date().toString(),
             });
         });
 
         it('returns false if the user modifying the task is not the author', () => {
-            canModifyTask();
+            // Simulate how components call canModifyTask() by using the hook useReportIsArchived() to see if the report is archived
+            const {result: isReportArchived} = renderHook(() => useReportIsArchived(report?.reportID));
+            expect(canModifyTask(report, employeeAccountID, undefined, isReportArchived.current)).toBe(false);
         });
-        it('returns false if the parent report is archived', () => {});
-        it('returns false if the report is a cancelled task report', () => {});
-        it('returns true if the user modifying the task is the author and the parent report is not archived or cancelled', () => {});
+        it('returns false if the parent report is archived', () => {
+            const {result: isReportArchived} = renderHook(() => useReportIsArchived(archivedReport?.reportID));
+            expect(canModifyTask(archivedReport, managerAccountID, undefined, isReportArchived.current)).toBe(false);
+        });
+        it('returns false if the report is a cancelled task report', () => {
+            const {result: isReportArchived} = renderHook(() => useReportIsArchived(cancelledTaskReport?.reportID));
+            expect(canModifyTask(cancelledTaskReport, managerAccountID, undefined, isReportArchived.current)).toBe(false);
+        });
+        it('returns true if the user modifying the task is the author and the parent report is not archived or cancelled', () => {
+            const {result: isReportArchived} = renderHook(() => useReportIsArchived(report?.reportID));
+            expect(canModifyTask(report, managerAccountID, undefined, isReportArchived.current)).toBe(true);
+        });
     });
 });
