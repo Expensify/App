2b0c298b17eec5f5bf1d808469c92bf7981ecb1e
VickyStash <vikstash@gmail.com>
2025-04-17 10:45:38 +0200

    Apply reviewer feedback

--- src/hooks/useCardFeeds.tsx
+++ src/hooks/useCardFeeds.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from 'react';
 import type {OnyxCollection, ResultMetadata} from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {CardFeeds, CompanyCardFeed} from '@src/types/onyx';
@@ -7,42 +8,45 @@ import useWorkspaceAccountID from './useWorkspaceAccountID';
 const useCardFeeds = (policyID: string | undefined): [CardFeeds | undefined, ResultMetadata<OnyxCollection<CardFeeds>>] => {
     const workspaceAccountID = useWorkspaceAccountID(policyID);
     const [allFeeds, allFeedsResult] = useOnyx(ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER);
-    const defaultFeed = allFeeds?.[`${ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER}${workspaceAccountID}`];
-    const {companyCards = {}, companyCardNicknames = {}, oAuthAccountDetails = {}} = defaultFeed?.settings ?? {};
 
-    if (!policyID || !allFeeds) {
-        return [undefined, allFeedsResult];
-    }
+    const workspaceFeeds = useMemo(() => {
+        if (!policyID || !allFeeds) {
+            return undefined;
+        }
 
-    const workspaceFeeds = Object.values(allFeeds).reduce<CardFeeds & {settings: Required<CardFeeds['settings']>}>(
-        (acc, feed) => {
-            if (!feed?.settings?.companyCards) {
+        const defaultFeed = allFeeds?.[`${ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER}${workspaceAccountID}`];
+        const {companyCards = {}, companyCardNicknames = {}, oAuthAccountDetails = {}} = defaultFeed?.settings ?? {};
+
+        return Object.values(allFeeds).reduce<CardFeeds & {settings: Required<CardFeeds['settings']>}>(
+            (acc, feed) => {
+                if (!feed?.settings?.companyCards) {
+                    return acc;
+                }
+
+                Object.entries(feed.settings.companyCards).forEach(([key, feedSettings]) => {
+                    const feedName = key as CompanyCardFeed;
+                    const feedOAuthAccountDetails = feed.settings.oAuthAccountDetails?.[feedName];
+                    const feedCompanyCardNicknames = feed.settings.companyCardNicknames?.[feedName];
+
+                    if (feedSettings.preferredPolicy !== policyID || acc.settings.companyCards[feedName]) {
+                        return;
+                    }
+
+                    acc.settings.companyCards[feedName] = feedSettings;
+
+                    if (feedOAuthAccountDetails) {
+                        acc.settings.oAuthAccountDetails[feedName] = feedOAuthAccountDetails;
+                    }
+                    if (feedCompanyCardNicknames) {
+                        acc.settings.companyCardNicknames[feedName] = feedCompanyCardNicknames;
+                    }
+                });
+
                 return acc;
-            }
-
-            Object.entries(feed.settings.companyCards).forEach(([key, feedSettings]) => {
-                const feedName = key as CompanyCardFeed;
-                const feedOAuthAccountDetails = feed.settings.oAuthAccountDetails?.[feedName];
-                const feedCompanyCardNicknames = feed.settings.companyCardNicknames?.[feedName];
-
-                if (feedSettings.preferredPolicy !== policyID || acc.settings.companyCards[feedName]) {
-                    return;
-                }
-
-                acc.settings.companyCards[feedName] = feedSettings;
-
-                if (feedOAuthAccountDetails) {
-                    acc.settings.oAuthAccountDetails[feedName] = feedOAuthAccountDetails;
-                }
-                if (feedCompanyCardNicknames) {
-                    acc.settings.companyCardNicknames[feedName] = feedCompanyCardNicknames;
-                }
-            });
-
-            return acc;
-        },
-        {settings: {companyCards, companyCardNicknames, oAuthAccountDetails}},
-    );
+            },
+            {settings: {companyCards, companyCardNicknames, oAuthAccountDetails}},
+        );
+    }, [allFeeds, policyID, workspaceAccountID]);
 
     return [workspaceFeeds, allFeedsResult];
 };
