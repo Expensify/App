2c3e18fccad2b6b6c55790de952bda1e095d8e5e
Marc Glasser <marc@expensify.com>
2025-04-17 12:36:05 -1000

    Merge pull request #60462 from Expensify/revert-60078-pac-guerreiro/feature/test-drive-admin-modal
    Revert "[Test Drive][Phase 1][FE] Implement the Test Drive modal"
    
    (cherry picked from commit 9b4d99d36b377044a17d06953b421f75ccf0a6b9)
    
    (CP triggered by roryabraham)

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -6227,14 +6227,6 @@ const translations = {
         talkToConcierge: 'Talk to Concierge',
         hangUp: 'Hang up',
     },
-    testDrive: {
-        modal: {
-            title: 'Take us for a test drive',
-            description: 'Take a quick product tour to get up to speed fast. No pit stops required!',
-            confirmText: 'Start test drive',
-            helpText: 'Skip',
-        },
-    },
 };
 
 export default translations satisfies TranslationDeepObject<typeof translations>;
