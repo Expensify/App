c816514dd9ecfe31ee4e36bee9b60a81aa01b425
Tim Golen <tgolen@expensify.com>
2025-04-18 09:33:58 -0600

    Lint

--- src/hooks/useReportIsArchived.ts
+++ src/hooks/useReportIsArchived.ts
@@ -4,7 +4,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 import useOnyx from './useOnyx';
 
 function useReportIsArchived(reportID?: string): boolean {
-    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${reportID ?? CONST.DEFAULT_NUMBER_ID}`);
+    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${reportID ?? CONST.DEFAULT_NUMBER_ID}`, {canBeMissing: true});
     const isReportArchived = isArchivedReport(reportNameValuePairs);
     return isReportArchived;
 }
