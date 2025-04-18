a631035450f35c57f84ec5087f189db2ec1a2726
Tim Golen <tgolen@expensify.com>
2025-04-18 09:49:02 -0600

    Remove default value

--- src/hooks/useReportIsArchived.ts
+++ src/hooks/useReportIsArchived.ts
@@ -1,10 +1,9 @@
 import {isArchivedReport} from '@libs/ReportUtils';
-import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import useOnyx from './useOnyx';
 
 function useReportIsArchived(reportID?: string): boolean {
-    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${reportID ?? CONST.DEFAULT_NUMBER_ID}`, {canBeMissing: true});
+    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${reportID}`, {canBeMissing: true});
     const isReportArchived = isArchivedReport(reportNameValuePairs);
     return isReportArchived;
 }
