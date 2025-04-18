4d7212cc6d0045b7ba0d149cb9918c54903247e3
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-04-18 14:05:06 +0200

    generate report attributes when all required dependencies are available

--- src/libs/actions/OnyxDerived/configs/reportAttributes.ts
+++ src/libs/actions/OnyxDerived/configs/reportAttributes.ts
@@ -5,32 +5,39 @@ import type {ReportAttributes} from '@src/types/onyx';
 
 /**
  * This derived value is used to get the report attributes for the report.
- * Dependency on ONYXKEYS.PERSONAL_DETAILS_LIST is to ensure that the report attributes are generated after the personal details are available.
+ * It's dependent on all collections that are used to generate the report attributes under the hood.
  */
 
 export default createOnyxDerivedValueConfig({
     key: ONYXKEYS.DERIVED.REPORT_ATTRIBUTES,
-    dependencies: [ONYXKEYS.COLLECTION.REPORT, ONYXKEYS.PERSONAL_DETAILS_LIST, ONYXKEYS.NVP_PREFERRED_LOCALE],
-    compute: ([reports, personalDetails, preferredLocale], {currentValue, sourceValues}) => {
-        if (!reports || !personalDetails || !preferredLocale) {
+    dependencies: [
+        ONYXKEYS.COLLECTION.REPORT,
+        ONYXKEYS.PERSONAL_DETAILS_LIST,
+        ONYXKEYS.COLLECTION.TRANSACTION,
+        ONYXKEYS.COLLECTION.REPORT_ACTIONS,
+        ONYXKEYS.COLLECTION.POLICY,
+        ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS,
+        ONYXKEYS.NVP_PREFERRED_LOCALE,
+    ],
+    compute: (dependencies, {currentValue, sourceValues}) => {
+        const areAllDependenciesSet = dependencies.every((dependency) => dependency !== undefined);
+        if (!areAllDependenciesSet) {
             return {};
         }
 
+        const [reports] = dependencies;
         const reportUpdates = sourceValues?.[ONYXKEYS.COLLECTION.REPORT];
 
-        return Object.values(reportUpdates ?? reports).reduce<Record<string, ReportAttributes>>(
-            (acc, report) => {
-                if (!report) {
-                    return acc;
-                }
-
-                acc[report.reportID] = {
-                    reportName: generateReportName(report),
-                };
-
+        return Object.values(reportUpdates ?? reports ?? {}).reduce<Record<string, ReportAttributes>>((acc, report) => {
+            if (!report) {
                 return acc;
-            },
-            reportUpdates && currentValue ? currentValue : {},
-        );
+            }
+
+            acc[report.reportID] = {
+                reportName: generateReportName(report),
+            };
+
+            return acc;
+        }, currentValue ?? {});
     },
 });
