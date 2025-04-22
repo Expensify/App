9614c3f809be02e72c5c2eb2538f9336942dd4f1
krishna2323 <belivethatkg@gmail.com>
2025-04-23 01:55:40 +0530

    fix: report page input border color.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1314,7 +1314,6 @@ const styles = (theme: ThemeColors) =>
             return {
                 transform: [{translateY: translateY.get()}],
                 fontSize: interpolate(scale.get(), [0, ACTIVE_LABEL_SCALE], [0, variables.fontSizeLabel]),
-                transition: 'ease',
             } satisfies TextStyle;
         },
 
