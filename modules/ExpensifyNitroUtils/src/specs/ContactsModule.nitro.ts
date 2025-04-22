c827792f8eba620d91967febbe7767042ddeb5e5
Taras Perun <terry.perun@gmail.com>
2025-04-22 09:37:14 +0200

    lint: export types

--- modules/ExpensifyNitroUtils/src/specs/ContactsModule.nitro.ts
+++ modules/ExpensifyNitroUtils/src/specs/ContactsModule.nitro.ts
@@ -4,15 +4,15 @@ type StringHolder = {
     value: string;
 };
 
-export const CONTACT_FIELDS = {
-  FIRST_NAME: 'FIRST_NAME',
-  LAST_NAME: 'LAST_NAME',
-  PHONE_NUMBERS: 'PHONE_NUMBERS',
-  EMAIL_ADDRESSES: 'EMAIL_ADDRESSES',
-  IMAGE_DATA: 'IMAGE_DATA',
+const CONTACT_FIELDS = {
+    FIRST_NAME: 'FIRST_NAME',
+    LAST_NAME: 'LAST_NAME',
+    PHONE_NUMBERS: 'PHONE_NUMBERS',
+    EMAIL_ADDRESSES: 'EMAIL_ADDRESSES',
+    IMAGE_DATA: 'IMAGE_DATA',
 } as const;
 
-export type Contact = {
+type Contact = {
     firstName?: string;
     lastName?: string;
     middleName?: string;
@@ -21,8 +21,13 @@ export type Contact = {
     imageData?: string;
     thumbnailImageData?: string;
 };
-export type ContactFields = 'FIRST_NAME' | 'LAST_NAME' | 'MIDDLE_NAME' | 'PHONE_NUMBERS' | 'EMAIL_ADDRESSES' | 'IMAGE_DATA' | 'THUMBNAIL_IMAGE_DATA' | 'GIVEN_NAME_KEY';
 
-export interface ContactsModule extends HybridObject<{ios: 'swift'; android: 'kotlin'}> {
+type ContactFields = 'FIRST_NAME' | 'LAST_NAME' | 'MIDDLE_NAME' | 'PHONE_NUMBERS' | 'EMAIL_ADDRESSES' | 'IMAGE_DATA' | 'THUMBNAIL_IMAGE_DATA' | 'GIVEN_NAME_KEY';
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+interface ContactsModule extends HybridObject<{ios: 'swift'; android: 'kotlin'}> {
     getAll(keys: ContactFields[]): Promise<Contact[]>;
 }
+
+export {CONTACT_FIELDS};
+export type {Contact, ContactFields, ContactsModule};
