8581bd79abbf07a2b381fbe70cf005a2a5b9d1a3
daledah <daleda261@outlook.com>
2025-04-23 18:04:03 +0700

    fix: spelling

--- src/components/ReportActionItem/MoneyRequestReportPreview/types.ts
+++ src/components/ReportActionItem/MoneyRequestReportPreview/types.ts
@@ -56,7 +56,7 @@ type MoneyRequestReportPreviewProps = {
     /** Whether the report is an invoice preview */
     isInvoice?: boolean;
 
-    /** Whether to sho a border to separate Reports Chat Item and Money Request Report Preview */
+    /** Whether to show a border to separate Reports Chat Item and Money Request Report Preview */
     shouldShowBorder?: boolean;
 };
 
