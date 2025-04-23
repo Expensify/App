e7d613b7547b9329d474a7bab2cebcb11895d785
Linh <vovanlinh29546@gmail.com>
2025-04-23 15:08:26 +0700

    fix: eslint fail

--- src/pages/iou/request/IOURequestStartPage.tsx
+++ src/pages/iou/request/IOURequestStartPage.tsx
@@ -50,7 +50,7 @@ function IOURequestStartPage({
     const [session] = useOnyx(ONYXKEYS.SESSION, {canBeMissing: false});
     const isLoadingSelectedTab = shouldUseTab ? isLoadingOnyxValue(selectedTabResult) : false;
     // eslint-disable-next-line  @typescript-eslint/prefer-nullish-coalescing
-    const [transaction] = useOnyx(`${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${route?.params.transactionID || CONST.DEFAULT_NUMBER_ID}`, {canBeMissing: true});
+    const [transaction] = useOnyx(`${ONYXKEYS.COLLECTION.TRANSACTION_DRAFT}${route?.params.transactionID}`, {canBeMissing: true});
     const [allPolicies] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {canBeMissing: true});
 
     const tabTitles = {
