a76700e322e7e818995da490a6bd4cf2dd780598
nkdengineer <nkdengineer@outlook.com>
2025-04-23 11:30:50 +0700

    fix:  Amount is not updating optimistically in the Expense Report

--- src/components/TransactionItemRow/DataCells/TotalCell.tsx
+++ src/components/TransactionItemRow/DataCells/TotalCell.tsx
@@ -4,23 +4,23 @@ import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {convertToDisplayString} from '@libs/CurrencyUtils';
 import {getCurrency as getTransactionCurrency, hasReceipt, isReceiptBeingScanned} from '@libs/TransactionUtils';
+import {getTransactionDetails} from '@libs/ReportUtils';
 import type TransactionDataCellProps from './TransactionDataCellProps';
 
 function TotalCell({shouldShowTooltip, transactionItem}: TransactionDataCellProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
     const currency = getTransactionCurrency(transactionItem);
-
-    let amount = convertToDisplayString(Math.abs(transactionItem.amount), currency);
-
+    const amount =  getTransactionDetails(transactionItem)?.amount;
+    let amountToDisplay = convertToDisplayString(amount, currency);
     if (hasReceipt(transactionItem) && isReceiptBeingScanned(transactionItem)) {
-        amount = translate('iou.receiptStatusTitle');
+        amountToDisplay = translate('iou.receiptStatusTitle');
     }
 
     return (
         <TextWithTooltip
             shouldShowTooltip={shouldShowTooltip}
-            text={amount}
+            text={amountToDisplay}
             style={[styles.optionDisplayName, styles.justifyContentCenter]}
         />
     );
