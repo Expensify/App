e84492aa81d82454ffe4ba90ab0b60cd43e34339
nkdengineer <nkdengineer@outlook.com>
2025-04-23 11:43:18 +0700

    fix prettier

--- src/components/TransactionItemRow/DataCells/TotalCell.tsx
+++ src/components/TransactionItemRow/DataCells/TotalCell.tsx
@@ -3,15 +3,15 @@ import TextWithTooltip from '@components/TextWithTooltip';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {convertToDisplayString} from '@libs/CurrencyUtils';
-import {getCurrency as getTransactionCurrency, hasReceipt, isReceiptBeingScanned} from '@libs/TransactionUtils';
 import {getTransactionDetails} from '@libs/ReportUtils';
+import {getCurrency as getTransactionCurrency, hasReceipt, isReceiptBeingScanned} from '@libs/TransactionUtils';
 import type TransactionDataCellProps from './TransactionDataCellProps';
 
 function TotalCell({shouldShowTooltip, transactionItem}: TransactionDataCellProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
     const currency = getTransactionCurrency(transactionItem);
-    const amount =  getTransactionDetails(transactionItem)?.amount;
+    const amount = getTransactionDetails(transactionItem)?.amount;
     let amountToDisplay = convertToDisplayString(amount, currency);
     if (hasReceipt(transactionItem) && isReceiptBeingScanned(transactionItem)) {
         amountToDisplay = translate('iou.receiptStatusTitle');
