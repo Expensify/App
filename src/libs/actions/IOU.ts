8a9af90914dd34fec296f9d6d3ac3ad7c8262f26
war-in <war-in@users.noreply.github.com>
2025-04-24 14:16:10 +0200

    fix other lint issues

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -4372,7 +4372,7 @@ function updateMoneyRequestTag(
 /** Updates the created tax amount of an expense */
 function updateMoneyRequestTaxAmount(
     transactionID: string,
-    optimisticReportActionID: string,
+    optimisticReportActionID: string | undefined,
     taxAmount: number,
     policy: OnyxEntry<OnyxTypes.Policy>,
     policyTagList: OnyxEntry<OnyxTypes.PolicyTagLists>,
