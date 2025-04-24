1565bb4fe8c972bbe22273883d17b39a34259122
mkzie2 <mkzie2@gmail.com>
2025-04-25 00:36:26 +0700

    add explanative comments

--- src/pages/iou/request/step/DiscardChangesConfirmation/index.tsx
+++ src/pages/iou/request/step/DiscardChangesConfirmation/index.tsx
@@ -31,13 +31,21 @@ function DiscardChangesConfirmation({getHasUnsavedChanges}: DiscardChangesConfir
         ),
     );
 
+    /**
+     * We cannot programmatically stop the browser's back navigation like react-navigation's beforeRemove
+     * Events like popstate and transitionStart are triggered AFTER the back navigation has already completed
+     * So we need to go forward to get back to the current page
+     */
     useEffect(() => {
+        // transitionStart is triggered before the previous page is fully loaded so RHP sliding animation
+        // could be less "glitchy" when going back and forth between the previous and current pages
         const unsubscribe = navigation.addListener('transitionStart', () => {
             // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
             if (!getHasUnsavedChanges() || blockedNavigationAction.current || shouldNavigateBack.current) {
                 return;
             }
 
+            // Navigation.navigate() rerenders the current page and resets its states
             window.history.go(1);
             setIsVisible(true);
             shouldNavigateBack.current = true;
