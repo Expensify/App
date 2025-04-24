16fc9c6063af7859862c2851c65a715301c7920f
Tim Golen <tgolen@expensify.com>
2025-04-24 13:26:53 -0700

    Add new hook

---
+++ src/hooks/useParentReport.ts
@@ -0,0 +1,13 @@
+import type {OnyxEntry} from 'react-native-onyx';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import type {Report} from '@src/types/onyx';
+import useOnyx from './useOnyx';
+
+function useParentReport(reportID?: string): OnyxEntry<Report> {
+    const [report] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {canBeMissing: true});
+    const [parentReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${report?.parentReportID ?? CONST.DEFAULT_NUMBER_ID}`, {canBeMissing: true});
+    return parentReport;
+}
+
+export default useParentReport;
