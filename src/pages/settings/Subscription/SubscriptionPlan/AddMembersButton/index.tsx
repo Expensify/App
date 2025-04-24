aa9c0431726ac3175c918388dae4121fa09c354b
VickyStash <vikstash@gmail.com>
2025-04-24 08:51:03 +0200

    Hide Add Members button if there's no default workspace set

--- src/pages/settings/Subscription/SubscriptionPlan/AddMembersButton/index.tsx
+++ src/pages/settings/Subscription/SubscriptionPlan/AddMembersButton/index.tsx
@@ -3,9 +3,11 @@ import {useOnyx} from 'react-native-onyx';
 import Button from '@components/Button';
 import * as Expensicons from '@components/Icon/Expensicons';
 import useLocalize from '@hooks/useLocalize';
+import usePolicy from '@hooks/usePolicy';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@navigation/Navigation';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 
@@ -13,7 +15,12 @@ function AddMembersButton() {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
     const [activePolicyID] = useOnyx(ONYXKEYS.NVP_ACTIVE_POLICY_ID);
+    const activePolicy = usePolicy(activePolicyID);
     const {shouldUseNarrowLayout} = useResponsiveLayout();
+
+    if (!activePolicy || activePolicy.type === CONST.POLICY.TYPE.PERSONAL) {
+        return null;
+    }
 
     return (
         <Button
