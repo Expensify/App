487670828577a7371b5654943869f09e517bedf6
Tim Golen <tgolen@expensify.com>
2025-04-25 14:18:08 -0700

    Refactor task description page

--- src/pages/tasks/TaskDescriptionPage.tsx
+++ src/pages/tasks/TaskDescriptionPage.tsx
@@ -73,8 +73,8 @@ function TaskDescriptionPage({report, currentUserPersonalDetails}: TaskDescripti
     const focusTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
     const isOpen = isOpenTaskReport(report);
-    const isReportArchived = useReportIsArchived(report?.parentReportID);
-    const canActuallyModifyTask = canModifyTask(report, currentUserPersonalDetails.accountID, isReportArchived);
+    const isParentReportArchived = useReportIsArchived(report?.parentReportID);
+    const canActuallyModifyTask = canModifyTask(report, currentUserPersonalDetails.accountID, isParentReportArchived);
     const isTaskNonEditable = isTaskReport(report) && (!canActuallyModifyTask || !isOpen);
 
     useFocusEffect(
