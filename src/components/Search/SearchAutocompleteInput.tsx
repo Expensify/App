d88f979504e7a3fa8ce3fd531845f88ede2cf87c
krishna2323 <belivethatkg@gmail.com>
2025-04-25 14:10:35 +0530

    remove extra top padding from search page input.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/components/Search/SearchAutocompleteInput.tsx
+++ src/components/Search/SearchAutocompleteInput.tsx
@@ -208,7 +208,7 @@ function SearchAutocompleteInput(
                         maxLength={CONST.SEARCH_QUERY_LIMIT}
                         onSubmitEditing={onSubmit}
                         shouldUseDisabledStyles={false}
-                        textInputContainerStyles={[styles.borderNone, styles.pb0, styles.pl3, styles.pt1]}
+                        textInputContainerStyles={[styles.borderNone, styles.pb0, styles.pl3, styles.pt0]}
                         inputStyle={[inputWidth, {lineHeight: undefined}]}
                         placeholderTextColor={theme.textSupporting}
                         onFocus={() => {
