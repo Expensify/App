0e66333929c87de6bc2c654df7648593cd912610
Zuzanna Furtak <zuzkafurtak@gmail.com>
2025-04-25 17:01:59 +0200

    Adjust config

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,20 @@
 const iosSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'ios' : 'ios';
 const androidSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'android' : 'android';
+const isHybrid = process.env.IS_HYBRID_APP === 'true' ? true : false;
 
-module.exports = {
+const config = {
     project: {
         ios: {sourceDir: iosSourceDir},
         android: {sourceDir: androidSourceDir},
     },
     assets: ['./assets/fonts/native'],
+    dependencies: {},
 };
+
+if (!isHybrid) {
+    config.dependencies['@expensify/react-native-wallet'] = {
+        platforms: {
+            android: null,
+        },
+    };
+}
