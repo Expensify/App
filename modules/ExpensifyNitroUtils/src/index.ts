5582bd8c01c1d9c694c52724f3a0a40b7eeeee2c
Taras Perun <terry.perun@gmail.com>
2025-04-25 12:28:40 +0200

    fix type

--- modules/ExpensifyNitroUtils/src/index.ts
+++ modules/ExpensifyNitroUtils/src/index.ts
@@ -1,9 +1,7 @@
 import {NitroModules} from 'react-native-nitro-modules';
-import type {Contact} from './specs/ContactsModule.nitro';
-import * as ContactsModuleSpec from './specs/ContactsModule.nitro';
+import type * as ContactsModuleSpec from './specs/ContactsModule.nitro';
 
 const ContactsNitroModule = NitroModules.createHybridObject<ContactsModuleSpec.ContactsModule>('ContactsModule');
 
 export {ContactsNitroModule};
-export type {Contact};
 export * from './specs/ContactsModule.nitro';
