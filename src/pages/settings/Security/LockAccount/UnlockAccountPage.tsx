849f9de2bb037f687f6537453e3b3a5e88d9ca96
Rushat Gabhane <rushatgabhane@gmail.com>
2025-04-28 17:21:08 +0200

    rm unused imports

--- src/pages/settings/Security/LockAccount/UnlockAccountPage.tsx
+++ src/pages/settings/Security/LockAccount/UnlockAccountPage.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import {View} from 'react-native';
-import Button from '@components/Button';
 import ConfirmationPage from '@components/ConfirmationPage';
-import HeaderPageLayout from '@components/HeaderPageLayout';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
-import Icon from '@components/Icon';
 import * as Expensicons from '@components/Icon/Expensicons';
 import ScrollView from '@components/ScrollView';
-import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
-import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
-import variables from '@styles/variables';
 import ROUTES from '@src/ROUTES';
 
 function UnlockAccountPage() {
