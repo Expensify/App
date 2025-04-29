a5f23385abb73ea6d0c12dac653b0420b8848d79
Christoph Pader <chris@margelo.com>
2025-04-29 23:36:53 +0200

    fix: RenderTaskQueue mock

--- jest/setup.ts
+++ jest/setup.ts
@@ -131,7 +131,9 @@ jest.mock(
     '@components/InvertedFlatList/BaseInvertedFlatList/RenderTaskQueue',
     () =>
         class SyncRenderTaskQueue {
-            private handler: (info: unknown) => void = () => {};
+            private handler: ((info: unknown) => void) | undefined = undefined;
+
+            private onEndReached: (() => void) | undefined = undefined;
 
             add(info: unknown) {
                 this.handler(info);
@@ -139,8 +141,12 @@ jest.mock(
 
             start() {}
 
-            setHandler(handler: () => void) {
+            setHandler(handler: (info: unknown) => void) {
                 this.handler = handler;
+            }
+
+            setOnEndReached(onEndReached: (() => void) | undefined) {
+                this.onEndReached = onEndReached;
             }
 
             cancel() {}
