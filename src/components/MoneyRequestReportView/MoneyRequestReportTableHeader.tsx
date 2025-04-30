ca16d77c761b14372b2db285060a64aa0b831b7b
nkdengineer <nkdengineer@outlook.com>
2025-04-30 10:22:37 +0700

    run prettier

--- src/components/MoneyRequestReportView/MoneyRequestReportTableHeader.tsx
+++ src/components/MoneyRequestReportView/MoneyRequestReportTableHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, {useCallback} from 'react';
 import {View} from 'react-native';
 import type {SortOrder, TableColumnSize} from '@components/Search/types';
 import SortableTableHeader from '@components/SelectionList/SortableTableHeader';
