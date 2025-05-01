833cb050910be8ffcb55b9ac7fe0a65dddd0e30b
Rutika Pawar <183392827+twilight2294@users.noreply.github.com>
2025-05-01 19:33:47 +0530

    fix styles

--- src/components/MoneyReportHeader.tsx
+++ src/components/MoneyReportHeader.tsx
@@ -559,6 +559,7 @@ function MoneyReportHeader({policy, report: moneyRequestReport, transactionThrea
 
                 exportToIntegration(moneyRequestReport.reportID, connectedIntegration);
             },
+            additionalIconStyles: styles.integrationIcon,
             displayInDefaultIconColor: true,
         },
         [CONST.REPORT.SECONDARY_ACTIONS.MARK_AS_EXPORTED]: {
