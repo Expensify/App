224f7b3c6081efb3760b8979a5f576e3421b283f
ahmdshrif <a.s1171111@gmail.com>
2025-05-01 11:21:59 +0300

    refactor(GroupChatNameTests): improve sidebar rendering test with async wait

--- tests/ui/GroupChatNameTests.tsx
+++ tests/ui/GroupChatNameTests.tsx
@@ -220,23 +220,26 @@ describe('Tests for group chat name', () => {
 
     it('Should show only 5 names with ellipsis when there are 8 participants in the report header', () =>
         signInAndGetApp('', participantAccountIDs8)
-            .then(() => {
+            .then(async () => {
+                // Wait for sidebar to be rendered to ensure all asynchronous updates are completed
+                await waitForBatchedUpdatesWithAct();
+                
                 // Verify the sidebar links are rendered
                 const sidebarLinksHintText = translateLocal('sidebarScreen.listOfChats');
-                const sidebarLinks = screen.queryAllByLabelText(sidebarLinksHintText);
-                expect(sidebarLinks).toHaveLength(1);
+                await waitFor(() => {
+                    const sidebarLinks = screen.queryAllByLabelText(sidebarLinksHintText);
+                    expect(sidebarLinks).toHaveLength(1);
 
-                // Verify there is only one option in the sidebar
-                const optionRows = screen.queryAllByAccessibilityHint(TestHelper.getNavigateToChatHintRegex());
-                expect(optionRows).toHaveLength(1);
+                    // Verify there is only one option in the sidebar
+                    const optionRows = screen.queryAllByAccessibilityHint(TestHelper.getNavigateToChatHintRegex());
+                    expect(optionRows).toHaveLength(1);
 
-                const displayNameHintText = translateLocal('accessibilityHints.chatUserDisplayNames');
-                const displayNameText = screen.queryByLabelText(displayNameHintText);
-
-                expect(displayNameText?.props?.children?.[0]).toBe('A, B, C, D, E...');
-
-                return navigateToSidebarOption(0);
+                    const displayNameHintText = translateLocal('accessibilityHints.chatUserDisplayNames');
+                    const displayNameText = screen.queryByLabelText(displayNameHintText);
+                    expect(displayNameText?.props?.children?.[0]).toBe('A, B, C, D, E...');
+                });
             })
+            .then(() => navigateToSidebarOption(0))
             .then(waitForBatchedUpdates)
             .then(async () => {
                 await act(() => transitionEndCB?.());
