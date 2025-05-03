d0f01c41d3942ce46f50677f78c3519cec9a8f39
mkzie2 <mkzie2@gmail.com>
2025-05-04 01:03:12 +0700

    fix: amount in thread header does not match invoice amount when edit offline

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -2963,7 +2963,7 @@ function getSendInvoiceInformation(
     }
     const optimisticTransaction = buildOptimisticTransaction({
         transactionParams: {
-            amount,
+            amount: amount * -1,
             currency,
             reportID: optimisticInvoiceReport.reportID,
             comment: trimmedComment,
