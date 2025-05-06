dc5fa4c05b9feda9637daf922d97649d9700618a
Marcin Warchoł <61014013+war-in@users.noreply.github.com>
2025-05-06 18:11:07 +0200

    Revert "[No QA] Simplify hybrid and standalone setup"

--- rnef.config.mjs
+++ rnef.config.mjs
@@ -2,14 +2,13 @@ import {platformAndroid} from '@rnef/platform-android';
 import {platformIOS} from '@rnef/platform-ios';
 import {pluginMetro} from '@rnef/plugin-metro';
 
-const isHybrid = process.env.IS_HYBRID_APP === 'true';
-
 /** @type {import('@rnef/config').Config} */
 export default {
     remoteCacheProvider: null,
     bundler: pluginMetro(),
     platforms: {
-        ios: platformIOS({sourceDir: isHybrid ? './Mobile-Expensify/iOS' : './ios'}),
-        android: platformAndroid({sourceDir: isHybrid ? './Mobile-Expensify/Android' : './android'}),
+        ios: platformIOS(),
+        android: platformAndroid(),
     },
+    root: process.env.PROJECT_ROOT_PATH ?? './',
 };
