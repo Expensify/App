0b7d53c10a9f7209a1ce10b3b993d9c44fcacd8d
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-05-06 12:26:18 +0800

    prettier

--- src/components/TextInput/TextInputMeasurement/index.ios.tsx
+++ src/components/TextInput/TextInputMeasurement/index.ios.tsx
@@ -18,21 +18,22 @@ function TextInputMeasurement({
     const styles = useThemeStyles();
 
     // On iOS, we don't need to measure for the auto grow size since iOS supports auto grow
-    return !!contentWidth && isPrefixCharacterPaddingCalculated && (
-        <View
-            style={[inputStyle as ViewStyle, styles.hiddenElementOutsideOfWindow, styles.visibilityHidden, styles.wAuto, inputPaddingLeft]}
-            onLayout={(e) => {
-                if (e.nativeEvent.layout.width === 0 && e.nativeEvent.layout.height === 0) {
-                    return;
-                }
-                onSetTextInputWidth(e.nativeEvent.layout.width);
-                onSetTextInputHeight(e.nativeEvent.layout.height);
-            }}
-        >
-            <Text style={[inputStyle, {width: contentWidth}]}>
-                {value ? `${value}${value.endsWith('\n') ? '\u200B' : ''}` : placeholder}
-            </Text>
-        </View>
+    return (
+        !!contentWidth &&
+        isPrefixCharacterPaddingCalculated && (
+            <View
+                style={[inputStyle as ViewStyle, styles.hiddenElementOutsideOfWindow, styles.visibilityHidden, styles.wAuto, inputPaddingLeft]}
+                onLayout={(e) => {
+                    if (e.nativeEvent.layout.width === 0 && e.nativeEvent.layout.height === 0) {
+                        return;
+                    }
+                    onSetTextInputWidth(e.nativeEvent.layout.width);
+                    onSetTextInputHeight(e.nativeEvent.layout.height);
+                }}
+            >
+                <Text style={[inputStyle, {width: contentWidth}]}>{value ? `${value}${value.endsWith('\n') ? '\u200B' : ''}` : placeholder}</Text>
+            </View>
+        )
     );
 }
 
