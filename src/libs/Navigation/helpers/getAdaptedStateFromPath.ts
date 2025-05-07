b9aed27e468a9319f7a8b3a880ab90366ac96f22
Wojciech Boman <wojciechboman@gmail.com>
2025-05-07 09:09:10 +0200

    Adjust getMatchingFullScreenRoute to handle WorkspaceHubSplitNavigator

--- src/libs/Navigation/helpers/getAdaptedStateFromPath.ts
+++ src/libs/Navigation/helpers/getAdaptedStateFromPath.ts
@@ -1,12 +1,12 @@
 import type {NavigationState, PartialState, Route} from '@react-navigation/native';
 import {findFocusedRoute, getStateFromPath} from '@react-navigation/native';
 import pick from 'lodash/pick';
-import Onyx from 'react-native-onyx';
 import type {OnyxCollection} from 'react-native-onyx';
+import Onyx from 'react-native-onyx';
 import {isAnonymousUser} from '@libs/actions/Session';
 import getInitialSplitNavigatorState from '@libs/Navigation/AppNavigator/createSplitNavigator/getInitialSplitNavigatorState';
 import {config} from '@libs/Navigation/linkingConfig/config';
-import {RHP_TO_SEARCH, RHP_TO_SETTINGS, RHP_TO_SIDEBAR, RHP_TO_WORKSPACE} from '@libs/Navigation/linkingConfig/RELATIONS';
+import {RHP_TO_SEARCH, RHP_TO_SETTINGS, RHP_TO_SIDEBAR, RHP_TO_WORKSPACE, RHP_TO_WORKSPACE_HUB} from '@libs/Navigation/linkingConfig/RELATIONS';
 import type {NavigationPartialRoute, RootNavigatorParamList} from '@libs/Navigation/types';
 import {extractPolicyIDFromPath, getPathWithoutPolicyID} from '@libs/PolicyUtils';
 import NAVIGATORS from '@src/NAVIGATORS';
@@ -101,6 +101,21 @@ function getMatchingFullScreenRoute(route: NavigationPartialRoute, policyID?: st
             },
             {
                 name: RHP_TO_WORKSPACE[route.name],
+                params: paramsFromRoute.length > 0 ? pick(route.params, paramsFromRoute) : undefined,
+            },
+        );
+    }
+
+    if (RHP_TO_WORKSPACE_HUB[route.name]) {
+        const paramsFromRoute = getParamsFromRoute(RHP_TO_WORKSPACE_HUB[route.name]);
+
+        return getInitialSplitNavigatorState(
+            {
+                name: SCREENS.WORKSPACE_HUB.ROOT,
+                params: paramsFromRoute.length > 0 ? pick(route.params, paramsFromRoute) : undefined,
+            },
+            {
+                name: RHP_TO_WORKSPACE_HUB[route.name],
                 params: paramsFromRoute.length > 0 ? pick(route.params, paramsFromRoute) : undefined,
             },
         );
