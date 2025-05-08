10c0a43e6b87c183151bd379be14e96703f7c9fe
Taras Perun <terry.perun@gmail.com>
2025-05-08 11:51:25 +0200

    getPlatform

--- src/components/Search/getPlatformHeight.ts
+++ src/components/Search/getPlatformHeight.ts
@@ -1,4 +1,5 @@
 import {Platform} from 'react-native';
+import getPlatform from '@libs/getPlatform';
 import variables from '@styles/variables';
 
 type PlatformHeights = {
@@ -27,7 +28,7 @@ export const ITEM_HEIGHTS = {
  * @returns The appropriate height for the current platform.
  */
 function getPlatformHeight(heights: PlatformHeights, fallbackHeight: number): number {
-    const currentPlatform = Platform.OS;
+    const currentPlatform = getPlatform();
     const height = heights[currentPlatform] ?? fallbackHeight;
     return Math.max(height, 1);
 }
