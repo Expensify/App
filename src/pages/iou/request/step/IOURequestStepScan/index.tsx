230c680b9f7b2d6a2aa08b6addbb630f716f2cb5
VickyStash <vikstash@gmail.com>
2025-05-08 14:37:40 +0200

    Code clean-up pt2

--- src/pages/iou/request/step/IOURequestStepScan/index.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.tsx
@@ -64,7 +64,7 @@ import {
     trackExpense,
     updateLastLocationPermissionPrompt,
 } from '@userActions/IOU';
-import {generatePolicyID} from '@userActions/Policy/Policy';
+import {generateTransactionID} from '@userActions/Transaction';
 import {createDraftTransaction} from '@userActions/TransactionEdit';
 import CONST from '@src/CONST';
 import type {TranslationPaths} from '@src/languages/types';
@@ -308,12 +308,12 @@ function IOURequestStepScan({
     );
 
     const buildOptimisticTransaction = useCallback((): Transaction => {
-        const newTransactionID = generatePolicyID(); // TODO: replace generatePolicyID
+        const newTransactionID = generateTransactionID();
         const {comment, currency, category, iouRequestType, isFromGlobalCreate, splitPayerAccountIDs} = initialTransaction ?? {};
         const newTransaction = {
             amount: 0,
             comment,
-            created: format(new Date(), 'yyyy-MM-dd'), // TODO: change
+            created: format(new Date(), 'yyyy-MM-dd'),
             currency,
             category,
             iouRequestType,
