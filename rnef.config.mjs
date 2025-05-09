6bf4f63a0f9599ff38e20d100424cf7851f43a85
Adam Horodyski <ad.horodyski@gmail.com>
2025-05-09 15:58:25 +0200

    fix: revert back config changes for rnef extraSources

--- rnef.config.mjs
+++ rnef.config.mjs
@@ -3,17 +3,13 @@ import {platformIOS} from '@rnef/platform-ios';
 import {pluginMetro} from '@rnef/plugin-metro';
 
 const isHybrid = process.env.IS_HYBRID_APP === 'true';
-const hybridRoot = './Mobile-Expensify';
 
 /** @type {import('@rnef/config').Config} */
 export default {
     remoteCacheProvider: null,
     bundler: pluginMetro(),
     platforms: {
-        ios: platformIOS({sourceDir: isHybrid ? `${hybridRoot}/iOS` : './ios'}),
-        android: platformAndroid({sourceDir: isHybrid ? `${hybridRoot}/Android` : './android'}),
-    },
-    fingerprint: {
-        extraSources: isHybrid ? [`${hybridRoot}/iOS`, `${hybridRoot}/Android`, `${hybridRoot}/patches`] : [],
+        ios: platformIOS({sourceDir: isHybrid ? './Mobile-Expensify/iOS' : './ios'}),
+        android: platformAndroid({sourceDir: isHybrid ? './Mobile-Expensify/Android' : './android'}),
     },
 };
