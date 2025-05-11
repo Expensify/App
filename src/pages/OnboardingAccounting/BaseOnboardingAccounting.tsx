0fe32dc2dcfaf1687434423dcd9a4f099634f83d
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-05-11 15:47:09 +0200

    fixing the last item cut off on android

--- src/pages/OnboardingAccounting/BaseOnboardingAccounting.tsx
+++ src/pages/OnboardingAccounting/BaseOnboardingAccounting.tsx
@@ -310,7 +310,7 @@ function BaseOnboardingAccounting({shouldUseNativeStyles}: BaseOnboardingAccount
                 <Text style={[styles.textHeadlineH1, styles.mb5]}>{translate('onboarding.accounting.title')}</Text>
             </View>
             <ScrollView style={[onboardingIsMediumOrLargerScreenWidth ? styles.mh8 : styles.mh5, styles.pt3, styles.pb8]}>
-                <View style={[styles.flexRow, styles.flexWrap, styles.gap3]}>{accountingOptions.map(renderOption)}</View>
+                <View style={[styles.flexRow, styles.flexWrap, styles.gap3, styles.mb3]}>{accountingOptions.map(renderOption)}</View>
             </ScrollView>
             <FixedFooter style={[styles.pt3, styles.ph5]}>
                 {!!error && (
