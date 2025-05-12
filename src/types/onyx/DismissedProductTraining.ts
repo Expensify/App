27dba432fdad8d9a7b3ae32e3200970d544cb74f
Wojciech Boman <wojciechboman@gmail.com>
2025-05-12 17:39:05 +0200

    Add WORKSPACES_TAB to DismissedProductTraining type

--- src/types/onyx/DismissedProductTraining.ts
+++ src/types/onyx/DismissedProductTraining.ts
@@ -11,6 +11,7 @@ const {
     SCAN_TEST_CONFIRMATION,
     OUTSANDING_FILTER,
     SETTINGS_TAB,
+    WORKSPACES_TAB,
     WORKSPACES_SETTINGS,
     ACCOUNT_SWITCHER,
     GBR_RBR_CHAT,
@@ -87,6 +88,11 @@ type DismissedProductTraining = {
      * When user dismisses the settings tab product training tooltip, we store the timestamp here.
      */
     [SETTINGS_TAB]: DismissedProductTrainingElement;
+
+    /**
+     * When user dismisses the workspaces tab product training tooltip, we store the timestamp here.
+     */
+    [WORKSPACES_TAB]: DismissedProductTrainingElement;
 
     /**
      * When user dismisses the workspaces settings product training tooltip, we store the timestamp here.
