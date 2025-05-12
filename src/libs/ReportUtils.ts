960277e83f6856a8d488207a973f59c96e00f8f7
nkdengineer <161821005+nkdengineer@users.noreply.github.com>
2025-05-12 10:31:09 +0700

    Update src/libs/ReportUtils.ts
    Co-authored-by: Lucien Akchoté <lucien@expensify.com>

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -6082,7 +6082,7 @@ function buildOptimisticMovedReportAction(
         {
             html: isIouReport
                 ? `moved this <a href='${CONST.NEW_EXPENSIFY_URL}r/${reportID}/${reportActionID}' target='_blank' rel='noreferrer noopener'>report</a> to the ${policyName} workspace`
-                : `moved the report to the <a href='${CONST.NEW_EXPENSIFY_URL}r/${newParentReportID}' target='_blank' rel='noreferrer noopener'>${policyName}</a> workspace`,
+                : `moved this report to the <a href='${CONST.NEW_EXPENSIFY_URL}r/${newParentReportID}' target='_blank' rel='noreferrer noopener'>${policyName}</a> workspace`,
             text: `moved the report to the ${policyName} workspace`,
             type: CONST.REPORT.MESSAGE.TYPE.COMMENT,
         },
