5fd9bcaf00562925b1bd2ac9f2faa6db832195d4
Adam Horodyski <ad.horodyski@gmail.com>
2025-05-12 14:20:22 +0200

    feat: session tracking based on session.id

--- src/libs/Telemetry.ts
+++ src/libs/Telemetry.ts
@@ -1,11 +1,20 @@
 import { openTelemetrySDK } from 'react-native-open-telemetry';
 import pkg from '../../package.json';
+import { getSession } from './SessionUtils';
 
 const sdk = openTelemetrySDK({
   debug: true,
   url: "http://localhost:4318",
   name: pkg.name,
   version: pkg.version,
+  features: {
+    session: {
+      getSessionId: () => {
+        const session = getSession();
+        return session?.authToken ? `${session.accountID}-${session.creationDate} : null;
+      }
+    }
+  }
 });
 
 const tracer = sdk.trace.getTracer(pkg.name, pkg.version);
