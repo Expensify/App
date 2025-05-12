b5ddbfdf0aedfad28b152a3e10e70bea25767256
NJ-2020 <ntt.london@gmail.com>
2025-05-12 08:06:04 +0700

    fix: infinite loading spinner when uploading pdf file while offline

--- src/components/AnchorForAttachmentsOnly/BaseAnchorForAttachmentsOnly.tsx
+++ src/components/AnchorForAttachmentsOnly/BaseAnchorForAttachmentsOnly.tsx
@@ -9,7 +9,7 @@ import * as Attachment from '@libs/actions/Attachment';
 import addEncryptedAuthTokenToURL from '@libs/addEncryptedAuthTokenToURL';
 import * as Browser from '@libs/Browser';
 import fileDownload from '@libs/fileDownload';
-import * as FileUtils from '@libs/fileDownload/FileUtils';
+import {isLocalFile} from '@libs/fileDownload/FileUtils';
 import * as ReportUtils from '@libs/ReportUtils';
 import * as Download from '@userActions/Download';
 import CONST from '@src/CONST';
@@ -25,24 +25,20 @@ type BaseAnchorForAttachmentsOnlyProps = AnchorForAttachmentsOnlyProps & {
 };
 
 function BaseAnchorForAttachmentsOnly({style, attachmentID, source = '', displayName = '', onPressIn, onPressOut, isDeleted = false}: BaseAnchorForAttachmentsOnlyProps) {
-    const finalSourceURL = FileUtils.isLocalFile(source) ? source : addEncryptedAuthTokenToURL(source);
-    const sourceID = (source.match(CONST.REGEX.ATTACHMENT_ID) ?? [])[1];
+    const finalSourceURL = isLocalFile(source) ? source : addEncryptedAuthTokenToURL(source);
 
-    const [download] = useOnyx(`${ONYXKEYS.COLLECTION.DOWNLOAD}${sourceID}`);
-
+    const [download] = useOnyx(`${ONYXKEYS.COLLECTION.DOWNLOAD}${attachmentID}`);
     const {isOffline} = useNetwork();
     const styles = useThemeStyles();
 
     const isDownloading = download?.isDownloading ?? false;
+    const isAuthTokenRequired = isLocalFile(source) ? false : true;
 
     const cacheAttachmentFile = () => {
-        if (FileUtils.isLocalFile(source) || !attachmentID) {
+        if (isLocalFile(source) || !attachmentID) {
             return;
         }
-        Attachment.cacheAttachment({
-            attachmentID,
-            url: finalSourceURL,
-        });
+        Attachment.cacheAttachment(attachmentID, source);
     };
 
     useEffect(() => {
@@ -54,13 +50,13 @@ function BaseAnchorForAttachmentsOnly({style, attachmentID, source = '', display
         <ShowContextMenuContext.Consumer>
             {({anchor, report, reportNameValuePairs, action, checkIfContextMenuActive, isDisabled, shouldDisplayContextMenu}) => (
                 <PressableWithoutFeedback
-                    style={[style, (isOffline || !sourceID) && styles.cursorDefault]}
+                    style={[style, (isOffline || !attachmentID) && styles.cursorDefault]}
                     onPress={() => {
-                        if (isDownloading || isOffline || !sourceID) {
+                        if (isDownloading || isOffline || !attachmentID) {
                             return;
                         }
-                        Download.setDownload(sourceID, true);
-                        fileDownload(finalSourceURL, displayName, '', Browser.isMobileSafari()).then(() => Download.setDownload(sourceID, false));
+                        Download.setDownload(attachmentID, true);
+                        fileDownload(finalSourceURL, displayName, '', Browser.isMobileSafari()).then(() => Download.setDownload(attachmentID, false));
                     }}
                     onPressIn={onPressIn}
                     onPressOut={onPressOut}
@@ -77,12 +73,12 @@ function BaseAnchorForAttachmentsOnly({style, attachmentID, source = '', display
                     <AttachmentView
                         source={source}
                         file={{name: displayName}}
-                        isAuthTokenRequired={true}
-                        shouldShowDownloadIcon={!!sourceID && !isOffline}
+                        isAuthTokenRequired={isAuthTokenRequired}
+                        shouldShowDownloadIcon={!!attachmentID && !isOffline}
                         shouldShowLoadingSpinnerIcon={isDownloading}
                         isUsedAsChatAttachment
                         isDeleted={!!isDeleted}
-                        isUploading={!sourceID}
+                        isUploading={!attachmentID}
                     />
                 </PressableWithoutFeedback>
             )}
--- src/libs/actions/Attachment/index.native.ts
+++ src/libs/actions/Attachment/index.native.ts
@@ -3,7 +3,6 @@ import {isLocalFile} from '@libs/fileDownload/FileUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Attachment} from '@src/types/onyx';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
-import {CacheAttachmentProps, FetchFileProps, GetAttachmentSourceProps, UploadAttachmentProps} from './types';
 
 let attachments: OnyxCollection<Attachment> | undefined;
 Onyx.connect({
@@ -11,20 +10,26 @@ Onyx.connect({
     waitForCollectionCallback: true,
     callback: (value) => (attachments = value),
 });
-
 function uploadAttachment(attachmentID: string, url: string) {
     if (!attachmentID || !url) {
         return;
     }
-    Onyx.set(`${ONYXKEYS.COLLECTION.ATTACHMENT}${attachmentID}`, {
-        source: url,
+    fetchFile(url)?.then((file) => {
+        if (!file) {
+            return;
+        }
+        Onyx.set(`${ONYXKEYS.COLLECTION.ATTACHMENT}${attachmentID}`, {
+            source: file,
+        });
     });
 }
 
 function getAttachmentSource(attachmentID: string, currentSource: string) {
     const attachment: Attachment | undefined = attachments?.[`${ONYXKEYS.COLLECTION.ATTACHMENT}${attachmentID}`];
+    console.log('get attachment souce');
     if (attachment && attachment.source) {
-        if (attachment.source instanceof Blob) {
+        if (typeof attachment.source !== 'string') {
+            console.log('get attachment souce blob');
             const imageUrl = URL.createObjectURL(attachment.source);
             return imageUrl;
         } else {
@@ -34,48 +39,21 @@ function getAttachmentSource(attachmentID: string, currentSource: string) {
     return attachment?.remoteSource || currentSource;
 }
 
-function fetchFile(url?: string, file?: File): Promise<ArrayBuffer | undefined> {
+function fetchFile(url: string): Promise<Blob | undefined> {
     return new Promise((resolve) => {
-        if (!url && !file?.uri) {
-            resolve(undefined);
-            return;
-        }
-
-        const isLocal = isLocalFile(file?.uri);
-        const hasFileAndIsLocal = !isEmptyObject(file) && file.uri && isLocal;
-
-        if (hasFileAndIsLocal) {
-            const reader = new FileReader();
-
-            reader.onload = (e) => {
-                if (e.target && e.target.result instanceof ArrayBuffer) {
-                    resolve(e.target.result);
-                }
-                resolve(undefined);
-            };
-
-            reader.onerror = (err) => {
-                console.error('Failed to read file', err);
-                resolve(undefined);
-            };
-
-            // Use array buffer for better perfomance i.e large attachment
-            reader.readAsArrayBuffer(file);
-
-            return;
-        }
         if (!url) {
             resolve(undefined);
             return;
         }
+
         fetch(url)
             .then((res) => {
                 if (!res.ok) {
                     console.error('Failed to fetch file');
                     resolve(undefined);
                 }
-                const fileBuffer = res.arrayBuffer();
-                resolve(fileBuffer);
+                const fileData = res.blob();
+                resolve(fileData);
             })
             .catch((err) => {
                 console.error('Failed to fetch file', err);
@@ -85,18 +63,15 @@ function fetchFile(url?: string, file?: File): Promise<ArrayBuffer | undefined> 
 }
 
 function cacheAttachment(attachmentID: string, url: string, file?: File) {
-    const attachment = attachments?.[attachmentID];
-
-    // Exit from the function if the image is not changed or the image has already been cached
-    // if (attachment && attachment.remoteSource === src && attachment.localSource) {
-    //     return;
-    // }
+    const attachment: Attachment | undefined = attachments?.[`${ONYXKEYS.COLLECTION.ATTACHMENT}${attachmentID}`];
     const attachmentUrl = url ?? file?.uri ?? '';
-    fetchFile(attachmentUrl)?.then((arrayBuffer) => {
-        const uint8Array = new Uint8Array(arrayBuffer ?? []);
-        const fileData = new Blob([uint8Array], {type: file?.type ?? ''});
+    if (attachment && attachment.remoteSource === attachmentUrl && attachment.source) {
+        return;
+    }
+
+    fetchFile(attachmentUrl)?.then((file) => {
         Onyx.merge(`${ONYXKEYS.COLLECTION.ATTACHMENT}${attachmentID}`, {
-            source: fileData,
+            source: file,
         });
     });
 }
