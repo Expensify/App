c45c340f63ffb965459c0eecd848abeb664c57bb
Rushat Gabhane <rushatgabhane@gmail.com>
2025-05-13 20:08:02 +0200

    fix merge

--- src/libs/API/types.ts
+++ src/libs/API/types.ts
@@ -822,11 +822,8 @@ type WriteCommandParameters = {
     [WRITE_COMMANDS.SET_POLICY_PROHIBITED_EXPENSES]: Parameters.SetPolicyProhibitedExpensesParams;
     [WRITE_COMMANDS.COMPLETE_CONCIERGE_CALL]: Parameters.CompleteConciergeCallParams;
     [WRITE_COMMANDS.FINISH_CORPAY_BANK_ACCOUNT_ONBOARDING]: Parameters.FinishCorpayBankAccountOnboardingParams;
-<<<<<<< HEAD
     [WRITE_COMMANDS.LOCK_ACCOUNT]: Parameters.LockAccountParams;
-=======
     [WRITE_COMMANDS.REOPEN_REPORT]: Parameters.ReopenReportParams;
->>>>>>> origin
 
     [WRITE_COMMANDS.DELETE_MONEY_REQUEST_ON_SEARCH]: Parameters.DeleteMoneyRequestOnSearchParams;
     [WRITE_COMMANDS.HOLD_MONEY_REQUEST_ON_SEARCH]: Parameters.HoldMoneyRequestOnSearchParams;
