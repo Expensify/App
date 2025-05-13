3847dd1ff7d0925e134488d4cececaaad5c0b746
Huu Le <20178761+huult@users.noreply.github.com>
2025-05-13 12:27:36 +0700

    add new color for thumnail image

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -5800,8 +5800,11 @@ const styles = (theme: ThemeColors) =>
             // Choosing a lowest value just above the threshold for the items to adjust width against the various screens. Only 2 items are shown 35 * 2 = 70 thus third item of 35% width can't fit forcing a two column layout.
             flexBasis: '35%',
         },
-        thumbnailImageContainer: {
-            backgroundColor: theme.activeComponentBG,
+        thumbnailImageContainerHover: {
+            backgroundColor: theme.hoverComponentBG,
+        },
+        thumbnailImageContainerHighlight: {
+            backgroundColor: theme.highlightBG,
         },
     } satisfies Styles);
 
