68e04343bd6b891a10083a782c98df97eb14883e
gijoe0295 <153004152+gijoe0295@users.noreply.github.com>
2025-05-15 05:24:16 +0700

    update unit test

--- tests/unit/postTestBuildComment.ts
+++ tests/unit/postTestBuildComment.ts
@@ -130,7 +130,7 @@ describe('Post test build comments action tests', () => {
     test('Test GH action when input is not complete', async () => {
         when(core.getInput).calledWith('REPO', {required: true}).mockReturnValue(CONST.APP_REPO);
         when(core.getInput).calledWith('PR_NUMBER', {required: true}).mockReturnValue('12');
-        when(core.getInput).calledWith('HYBRID_APP_PR_NUMBER', {required: false}).mockReturnValue('13');
+        when(core.getInput).calledWith('HYBRID_APP_PR_NUMBER', {required: false}).mockReturnValue('');
         when(core.getInput).calledWith('ANDROID', {required: false}).mockReturnValue('');
         when(core.getInput).calledWith('IOS', {required: false}).mockReturnValue('');
         when(core.getInput).calledWith('WEB', {required: false}).mockReturnValue('');
