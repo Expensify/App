2d41b34ba707d070c0ca2ba5b722ba188c05401f
Francois Laithier <francois@expensify.com>
2025-05-13 21:25:46 -0700

    Revert "Merge pull request #60505 from abzokhattab/update-onboarding-integrations"
    This reverts commit 6bda07451d4d4fc47836ef4140fa28e330c0cb9a, reversing
    changes made to 1ad9d6a655fb2e6b41466941379f676835726c9d.

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -2054,7 +2054,7 @@ const translations = {
         },
         accounting: {
             title: 'Do you use any accounting software?',
-            none: 'None',
+            noneOfAbove: 'None of the above',
         },
         error: {
             requiredFirstName: 'Please input your first name to continue',
@@ -4355,9 +4355,6 @@ const translations = {
             xero: 'Xero',
             netsuite: 'NetSuite',
             intacct: 'Sage Intacct',
-            sap: 'SAP',
-            oracle: 'Oracle',
-            microsoftDynamics: 'Microsoft Dynamics',
             talkYourOnboardingSpecialist: 'Chat with your setup specialist.',
             talkYourAccountManager: 'Chat with your account manager.',
             talkToConcierge: 'Chat with Concierge.',
@@ -4386,7 +4383,7 @@ const translations = {
             import: 'Import',
             export: 'Export',
             advanced: 'Advanced',
-            other: 'Other',
+            other: 'Other integrations',
             syncNow: 'Sync now',
             disconnect: 'Disconnect',
             reinstall: 'Reinstall connector',
