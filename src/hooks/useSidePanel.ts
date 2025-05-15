7bd7be15a2cce8c0021f18517cdbeaebdf95ad73
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-05-15 13:18:21 +0800

    fix side panel transition state is stuck at false initially

--- src/hooks/useSidePanel.ts
+++ src/hooks/useSidePanel.ts
@@ -64,7 +64,7 @@ function useSidePanel() {
     const {windowWidth} = useWindowDimensions();
     const sidePanelWidth = shouldUseNarrowLayout ? windowWidth : variables.sideBarWidth;
 
-    const [isSidePanelTransitionEnded, setIsSidePanelTransitionEnded] = useState(false);
+    const [isSidePanelTransitionEnded, setIsSidePanelTransitionEnded] = useState(true);
     const {shouldHideSidePanel, shouldHideSidePanelBackdrop, shouldHideHelpButton, sidePanelNVP} = useSidePanelDisplayStatus();
     const shouldHideToolTip = isExtraLargeScreenWidth ? !isSidePanelTransitionEnded : !shouldHideSidePanel;
 
