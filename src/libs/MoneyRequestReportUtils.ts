b0f959d2c4e458ea4a55417d06aeae38caea627e
Jakub Korytko <jakub@korytko.me>
2025-05-16 10:16:19 +0200

    Remove getIOUReportPreviewButtonType

--- src/libs/MoneyRequestReportUtils.ts
+++ src/libs/MoneyRequestReportUtils.ts
@@ -91,49 +91,6 @@ function shouldDisplayReportTableView(report: OnyxEntry<Report>, transactions: T
     return !isReportTransactionThread(report) && !isSingleTransactionReport(report, transactions);
 }
 
-/* This function is a legacy used for old version of MoneyReportHeader & ReportPreview, do not use it in new versions or anywhere else */
-/**
- * Determines the appropriate button type for the IOU Report Preview based on the given flags.
- *
- * @param flags - An object containing boolean flags indicating button visibility options.
- * @param flags.shouldShowSubmitButton - Flag indicating if the submit button should be shown.
- * @param flags.shouldShowExportIntegrationButton - Flag indicating if the export integration button should be shown.
- * @param flags.shouldShowPayButton - Flag indicating if the pay button should be shown.
- * @param flags.shouldShowApproveButton - Flag indicating if the approve button should be shown.
- * @returns - Returns the type of button that should be displayed based on the input flags.
- */
-const getIOUReportPreviewButtonType = ({
-    shouldShowSubmitButton,
-    shouldShowExportIntegrationButton,
-    shouldShowApproveButton,
-    shouldShowPayButton,
-}: {
-    shouldShowSubmitButton: boolean;
-    shouldShowExportIntegrationButton: boolean;
-    shouldShowRBR: boolean;
-    shouldShowSettlementButton: boolean;
-    shouldShowPayButton: boolean;
-    shouldShowApproveButton: boolean;
-}): ValueOf<typeof CONST.REPORT.REPORT_PREVIEW_ACTIONS> => {
-    if (shouldShowPayButton) {
-        return CONST.REPORT.REPORT_PREVIEW_ACTIONS.PAY;
-    }
-
-    if (shouldShowApproveButton) {
-        return CONST.REPORT.REPORT_PREVIEW_ACTIONS.APPROVE;
-    }
-
-    if (shouldShowExportIntegrationButton) {
-        return CONST.REPORT.REPORT_PREVIEW_ACTIONS.EXPORT_TO_ACCOUNTING;
-    }
-
-    if (shouldShowSubmitButton) {
-        return CONST.REPORT.REPORT_PREVIEW_ACTIONS.SUBMIT;
-    }
-
-    return CONST.REPORT.REPORT_PREVIEW_ACTIONS.REVIEW;
-};
-
 /**
  * Determines the total amount to be displayed based on the selected button type in the IOU Report Preview.
  *
@@ -175,7 +132,6 @@ export {
     getThreadReportIDsForTransactions,
     getTotalAmountForIOUReportPreviewButton,
     selectAllTransactionsForReport,
-    getIOUReportPreviewButtonType,
     isSingleTransactionReport,
     shouldDisplayReportTableView,
 };
