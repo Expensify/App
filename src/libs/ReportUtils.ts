7c89a5126bb6e906befab3a07de26edf85fd9651
Mitrovic Djordje <mitrovicdjordje00@gmail.com>
2025-05-17 09:52:54 +0200

    New branch

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -177,6 +177,7 @@ import {
     isExportIntegrationAction,
     isIntegrationMessageAction,
     isMarkAsClosedAction,
+    isMessageDeleted,
     isModifiedExpenseAction,
     isMoneyRequestAction,
     isOldDotReportAction,
@@ -4777,7 +4778,8 @@ function getReportName(
     const canUseDerivedValue = report && policy === undefined && parentReportActionParam === undefined && personalDetails === undefined && invoiceReceiverPolicy === undefined;
     const attributes = reportAttributes ?? reportAttributesDerivedValue;
     const derivedNameExists = report && !!attributes?.[report.reportID]?.reportName;
-    if (canUseDerivedValue && derivedNameExists) {
+    // This doesn't apply to chat reports because last message (report name) can be changed and/or edited
+    if (canUseDerivedValue && derivedNameExists && !isChatReport(report)) {
         return attributes[report.reportID].reportName;
     }
     return getReportNameInternal({report, policy, parentReportActionParam, personalDetails, invoiceReceiverPolicy});
@@ -4789,6 +4791,20 @@ function getSearchReportName(props: GetReportNameParams): string {
         return policy.name;
     }
     return getReportNameInternal(props);
+}
+
+function isChatThreadDeleted(report: OnyxInputOrEntry<Report>, reportActionParam: OnyxInputOrEntry<ReportAction>): boolean
+{
+    if (!isChatThread(report)) {
+        return false
+    }
+
+    let reportAction = reportActionParam as OnyxEntry<ReportAction>
+    if (!reportAction) {
+        reportAction = getReportAction(report?.parentReportID, report?.parentReportActionID);
+    }
+
+    return isMessageDeleted(reportAction)
 }
 
 function getInvoiceReportName(report: OnyxEntry<Report>, policy?: OnyxEntry<Policy | SearchPolicy>, invoiceReceiverPolicy?: OnyxEntry<Policy | SearchPolicy>): string {
@@ -4956,7 +4972,7 @@ function getReportNameInternal({
             return getRenamedAction(parentReportAction, isExpenseReport(getReport(report.parentReportID, allReports)));
         }
 
-        if (parentReportActionMessage?.isDeletedParentAction) {
+        if (isChatThreadDeleted(report, parentReportActionParam)) {
             return translateLocal('parentReportAction.deletedMessage');
         }
 
@@ -11043,6 +11059,7 @@ export {
     isChatRoom,
     isTripRoom,
     isChatThread,
+    isChatThreadDeleted,
     isChildReport,
     isClosedExpenseReportWithNoExpenses,
     isCompletedTaskReport,
