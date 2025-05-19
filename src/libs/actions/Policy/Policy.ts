171fed19294deb0ce3831a75de3d34580d1b82f7
Carlos Martins <cmartins@expensify.com>
2025-05-19 12:34:46 -0600

    add comment

--- src/libs/actions/Policy/Policy.ts
+++ src/libs/actions/Policy/Policy.ts
@@ -1820,6 +1820,8 @@ function buildPolicyData(
         introSelected.choice === CONST.ONBOARDING_CHOICES.PERSONAL_SPEND;
     const shouldSetCreatedWorkspaceAsActivePolicy = !!activePolicyID && allPolicies?.[`${ONYXKEYS.COLLECTION.POLICY}${activePolicyID}`]?.type === CONST.POLICY.TYPE.PERSONAL;
 
+    // WARNING: The data below should be kept in sync with the API so we create the policy with the correct configuration.
+    // 
     const optimisticData: OnyxUpdate[] = [
         {
             onyxMethod: Onyx.METHOD.SET,
