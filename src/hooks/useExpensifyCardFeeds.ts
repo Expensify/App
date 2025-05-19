f30a9fb76fb2d8c9480140b7b8d81ebd4f3a2240
VickyStash <vikstash@gmail.com>
2025-05-19 10:04:16 +0200

    Remove unused eslint disable

--- src/hooks/useExpensifyCardFeeds.ts
+++ src/hooks/useExpensifyCardFeeds.ts
@@ -6,14 +6,11 @@ function useExpensifyCardFeeds(policyID: string | undefined) {
     const workspaceAccountID = useWorkspaceAccountID(policyID);
     const [allExpensifyCardFeeds] = useOnyx(ONYXKEYS.COLLECTION.PRIVATE_EXPENSIFY_CARD_SETTINGS, {
         selector: (cardSettings) => {
-            const matchingEntries = Object.entries(cardSettings ?? {}).filter(
-                // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                ([key, settings]) => {
-                    const isDomainFeed = !!(settings?.preferredPolicy && settings.preferredPolicy === policyID);
-                    const isWorkspaceFeed = key.includes(workspaceAccountID.toString()) && settings && Object.keys(settings).length > 1;
-                    return isDomainFeed || isWorkspaceFeed;
-                },
-            );
+            const matchingEntries = Object.entries(cardSettings ?? {}).filter(([key, settings]) => {
+                const isDomainFeed = !!(settings?.preferredPolicy && settings.preferredPolicy === policyID);
+                const isWorkspaceFeed = key.includes(workspaceAccountID.toString()) && settings && Object.keys(settings).length > 1;
+                return isDomainFeed || isWorkspaceFeed;
+            });
 
             return Object.fromEntries(matchingEntries);
         },
