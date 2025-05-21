969046b6cc100e491924123ee82e3dbde14cd62d
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-05-21 11:22:00 +0800

    fix can't select item

--- src/pages/workspace/tags/WorkspaceViewTagsPage.tsx
+++ src/pages/workspace/tags/WorkspaceViewTagsPage.tsx
@@ -378,6 +378,7 @@ function WorkspaceViewTagsPage({route}: WorkspaceViewTagsProps) {
                         turnOnSelectionModeOnLongPress
                         onTurnOnSelectionMode={(item) => item && toggleTag(item)}
                         sections={sections}
+                        selectedItemKeys={selectedTags}
                         shouldUseDefaultRightHandSideCheckmark={false}
                         onCheckboxPress={toggleTag}
                         onSelectRow={navigateToTagSettings}
