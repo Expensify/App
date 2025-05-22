21964e82183d3315cde229931b7c5463bea8d84b
daledah <daleda261@outlook.com>
2025-05-22 22:17:39 +0700

    fix: correct logics

--- src/components/MoneyRequestConfirmationListFooter.tsx
+++ src/components/MoneyRequestConfirmationListFooter.tsx
@@ -288,8 +288,7 @@ function MoneyRequestConfirmationListFooter({
     const policyID = selectedParticipants?.at(0)?.policyID;
     const reportOwnerAccountID = selectedParticipants?.at(0)?.ownerAccountID;
     const shouldUseTransactionReport = !!transactionReport && isReportOutstanding(transactionReport, policyID);
-    const shouldAllowProcessingReport = !isPolicyExpenseChat || action !== CONST.IOU.ACTION.CREATE;
-    const firstOutstandingReport = getOutstandingReportsForUser(policyID, reportOwnerAccountID, allReports ?? {}, shouldAllowProcessingReport).at(0);
+    const firstOutstandingReport = getOutstandingReportsForUser(policyID, reportOwnerAccountID, allReports ?? {}, false).at(0);
     let reportName: string | undefined;
     if (shouldUseTransactionReport) {
         reportName = transactionReport.reportName;
