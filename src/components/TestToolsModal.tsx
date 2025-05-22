5c98ee323a50c7f4acef4cc6918a6f6ea834e0bf
daledah <daleda261@outlook.com>
2025-05-22 23:56:49 +0700

    Revert "fix: keep validate code modal when open test tool menu"

--- src/components/TestToolsModal.tsx
+++ src/components/TestToolsModal.tsx
@@ -38,10 +38,6 @@ function TestToolsModal() {
     const isAuthenticated = useIsAuthenticated();
     const route = getRouteBasedOnAuthStatus(isAuthenticated, activeRoute);
 
-    if (!isTestToolsModalOpen) {
-        return null;
-    }
-
     return (
         <Modal
             isVisible={!!isTestToolsModalOpen}
