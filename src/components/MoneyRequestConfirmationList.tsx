c9b9d68a182f63a45c48025a07b5daf450d089b2
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2025-05-22 11:12:49 +0800

    TypeScript fix

--- src/components/MoneyRequestConfirmationList.tsx
+++ src/components/MoneyRequestConfirmationList.tsx
@@ -1049,7 +1049,6 @@ function MoneyRequestConfirmationList({
             isCategoryRequired={isCategoryRequired}
             isDistanceRequest={isDistanceRequest}
             isPerDiemRequest={isPerDiemRequest}
-            isEditingSplitBill={isEditingSplitBill}
             isMerchantEmpty={isMerchantEmpty}
             isMerchantRequired={isMerchantRequired}
             isPolicyExpenseChat={isPolicyExpenseChat}
