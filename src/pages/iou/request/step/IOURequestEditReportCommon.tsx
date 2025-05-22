4017afed2dbbc4893194ddb23130e18a08eb4624
Carlos Martins <cmartins@expensify.com>
2025-05-22 16:42:45 -0600

    revert breaking changes

--- src/pages/iou/request/step/IOURequestEditReportCommon.tsx
+++ src/pages/iou/request/step/IOURequestEditReportCommon.tsx
@@ -45,28 +45,27 @@ type Props = {
 function IOURequestEditReportCommon({backTo, transactionsReports, selectReport}: Props) {
     const {translate} = useLocalize();
     const [allReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: (reports) => mapOnyxCollectionItems(reports, reportSelector), canBeMissing: true});
+    const [allPoliciesID] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: (policies) => mapOnyxCollectionItems(policies, (policy) => policy?.id), canBeMissing: false});
 
     const currentUserPersonalDetails = useCurrentUserPersonalDetails();
     const [searchValue, debouncedSearchValue, setSearchValue] = useDebouncedState('');
 
     const expenseReports = useMemo(
         () =>
-            Object.values(transactionsReports ?? {}).flatMap((transactionsReport) => {
-                if (!transactionsReport.policyID) {
+            Object.values(allPoliciesID ?? {}).flatMap((policyID) => {
+                if (!policyID) {
                     return [];
                 }
-                const reports = getOutstandingReportsForUser(transactionsReport.policyID, transactionsReport?.ownerAccountID ?? currentUserPersonalDetails.accountID, allReports ?? {});
+                const reports = getOutstandingReportsForUser(policyID, transactionsReports.at(0)?.ownerAccountID ?? currentUserPersonalDetails.accountID, allReports ?? {});
                 return reports;
             }),
-        [allReports, currentUserPersonalDetails.accountID, transactionsReports],
+        [allReports, currentUserPersonalDetails.accountID, transactionsReports, allPoliciesID],
     );
 
     const reportOptions: ReportListItem[] = useMemo(() => {
         if (!allReports) {
             return [];
         }
-
-        const onlyReport = transactionsReports.length === 1 ? transactionsReports.at(0) : undefined;
 
         return expenseReports
             .sort((a, b) => a?.reportName?.localeCompare(b?.reportName?.toLowerCase() ?? '') ?? 0)
@@ -76,7 +75,7 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport}:
                 text: report.reportName,
                 value: report.reportID,
                 keyForList: report.reportID,
-                isSelected: onlyReport && report.reportID === onlyReport?.reportID,
+                isSelected: transactionsReports.at(0)?.reportID === report.reportID,
             }));
     }, [allReports, debouncedSearchValue, expenseReports, transactionsReports]);
 
