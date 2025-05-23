3755185d2f6ac0d82b22516ca763c2aa7ff39906
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2025-05-23 12:15:21 +0800

    remove setTimeout

--- src/hooks/useHasInstalledApp.ts
+++ src/hooks/useHasInstalledApp.ts
@@ -1,19 +1,11 @@
-import {useEffect, useState} from 'react';
 import {useOnyx} from 'react-native-onyx';
 import ONYXKEYS from '@src/ONYXKEYS';
 
 const useHasInstalledApp = () => {
-    const [lastIOSLogin] = useOnyx(ONYXKEYS.NVP_LAST_ECASH_IOS_LOGIN, {canBeMissing: true});
-    const [lastAndroidLogin] = useOnyx(ONYXKEYS.NVP_LAST_ECASH_ANDROID_LOGIN, {canBeMissing: true});
-    const [isLastAppLoginLoaded, setIsLastAppLoginLoaded] = useState(false);
+    const [lastIOSLogin, lastIOSLoginResult] = useOnyx(ONYXKEYS.NVP_LAST_ECASH_IOS_LOGIN, {canBeMissing: true});
+    const [lastAndroidLogin, lastAndroidLoginResult] = useOnyx(ONYXKEYS.NVP_LAST_ECASH_ANDROID_LOGIN, {canBeMissing: true});
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setIsLastAppLoginLoaded(true);
-        }, 200);
-
-        return () => clearTimeout(timer);
-    }, []);
+    const isLastAppLoginLoaded = lastIOSLoginResult.status !== 'loading' && lastAndroidLoginResult.status !== 'loading';
 
     const hasInstalledApp = !!lastIOSLogin || !!lastAndroidLogin;
 
