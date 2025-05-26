19986ee04b7e5dac42c336b226db22ff011a182b
Hubert Sosinski <hubert.sosinski@callstack.com>
2025-05-26 10:52:12 +0200

    Remove input value bliking while typing

--- src/components/Search/SearchRouter/SearchRouter.tsx
+++ src/components/Search/SearchRouter/SearchRouter.tsx
@@ -182,7 +182,6 @@ function SearchRouter({onRouterClose, shouldHideInputCaret, isSearchRouterDispla
         if (!textInputRef.current || !shouldScrollRef.current) {
             return;
         }
-
         scrollToRight(textInputRef.current);
         shouldScrollRef.current = false;
     }, [textInputValue]);
@@ -194,7 +193,6 @@ function SearchRouter({onRouterClose, shouldHideInputCaret, isSearchRouterDispla
             }
             const singleLineUserQuery = StringUtils.lineBreaksToSpaces(userQuery, true);
             const updatedUserQuery = getAutocompleteQueryWithComma(textInputValue, singleLineUserQuery);
-            setTextInputValue(updatedUserQuery);
             setAutocompleteQueryValue(updatedUserQuery);
 
             const updatedSubstitutionsMap = getUpdatedSubstitutionsMap(singleLineUserQuery, autocompleteSubstitutions);
@@ -234,7 +232,10 @@ function SearchRouter({onRouterClose, shouldHideInputCaret, isSearchRouterDispla
         (text: string) => {
             setTextInputValue(text);
             shouldScrollRef.current = true;
-            setSelection({start: text.length, end: text.length});
+
+            InteractionManager.runAfterInteractions(() => {
+                setSelection({start: text.length, end: text.length});
+            });
         },
         [setSelection, setTextInputValue],
     );
@@ -259,8 +260,8 @@ function SearchRouter({onRouterClose, shouldHideInputCaret, isSearchRouterDispla
                 if (item.searchItemType === CONST.SEARCH.SEARCH_ROUTER_ITEM_TYPE.CONTEXTUAL_SUGGESTION) {
                     const searchQuery = getContextualSearchQuery(item);
                     const newSearchQuery = `${searchQuery}\u00A0`;
+                    setTextAndUpdateSelection(newSearchQuery);
                     onSearchQueryChange(newSearchQuery, true);
-                    setSelection({start: newSearchQuery.length, end: newSearchQuery.length});
 
                     const autocompleteKey = getContextualSearchAutocompleteKey(item);
                     if (autocompleteKey && item.autocompleteID) {
@@ -272,8 +273,8 @@ function SearchRouter({onRouterClose, shouldHideInputCaret, isSearchRouterDispla
                 } else if (item.searchItemType === CONST.SEARCH.SEARCH_ROUTER_ITEM_TYPE.AUTOCOMPLETE_SUGGESTION && textInputValue) {
                     const trimmedUserSearchQuery = getQueryWithoutAutocompletedPart(textInputValue);
                     const newSearchQuery = `${trimmedUserSearchQuery}${sanitizeSearchValue(item.searchQuery)}\u00A0`;
+                    setTextAndUpdateSelection(newSearchQuery);
                     onSearchQueryChange(newSearchQuery, true);
-                    setSelection({start: newSearchQuery.length, end: newSearchQuery.length});
 
                     if (item.mapKey && item.autocompleteID) {
                         const substitutions = {...autocompleteSubstitutions, [item.mapKey]: item.autocompleteID};
