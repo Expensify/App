b493ca7d54b3194e325273d0dc67cf575e87baaa
Pedro Guerreiro <pedro.a.c.guerreiro@gmail.com>
2025-05-27 00:05:06 +0100

    chore: second attempt at fixing infinite loader

--- src/libs/actions/setTestReceipt/getFileURL/index.ts
+++ src/libs/actions/setTestReceipt/getFileURL/index.ts
@@ -1,12 +1,11 @@
 import {Image} from 'react-native';
-import ReactNativeBlobUtil from 'react-native-blob-util';
 import type {Asset, AssetExtension} from '@userActions/setTestReceipt/types';
 import CONFIG from '@src/CONFIG';
 import CONST from '@src/CONST';
 
 function getFileURL(asset: Asset, assetExtension: AssetExtension) {
     const source = Image.resolveAssetSource(asset).uri;
-    return CONFIG.ENVIRONMENT === CONST.ENVIRONMENT.DEV ? source : ReactNativeBlobUtil.fs.asset(`${source}.${assetExtension}`);
+    return CONFIG.ENVIRONMENT === CONST.ENVIRONMENT.DEV ? source : `file:///android_res/drawable/${source}.${assetExtension}`;
 }
 
 export default getFileURL;
