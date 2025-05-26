f1e18a0fecf43abae87ebf680a9339c3374ca0f1
FitseTLT <engfitsum@gmail.com>
2025-05-27 00:42:12 +0300

    fix test

--- tests/actions/ReportPreviewActionUtilsTest.ts
+++ tests/actions/ReportPreviewActionUtilsTest.ts
@@ -230,8 +230,11 @@ describe('getReportPreviewAction', () => {
 
     it('canReview should return true for reports where there are violations, user is submitter or approver and Workflows are enabled', async () => {
         (ReportUtils.hasViolations as jest.Mock).mockReturnValue(true);
-        const report = {
+        const report: Report = {
             ...createRandomReport(REPORT_ID),
+            statusNum: 0,
+            stateNum: 0,
+            type: CONST.REPORT.TYPE.EXPENSE,
             ownerAccountID: CURRENT_USER_ACCOUNT_ID,
             isWaitingOnBankAccount: false,
         };
@@ -261,8 +264,11 @@ describe('getReportPreviewAction', () => {
 
     it('canReview should return true for reports with RTER violations regardless of workspace workflow configuration', async () => {
         (ReportUtils.hasViolations as jest.Mock).mockReturnValue(true);
-        const report = {
+        const report: Report = {
             ...createRandomReport(REPORT_ID),
+            statusNum: 0,
+            stateNum: 0,
+            type: CONST.REPORT.TYPE.EXPENSE,
             ownerAccountID: CURRENT_USER_ACCOUNT_ID,
             isWaitingOnBankAccount: false,
         };
