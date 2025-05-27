7de203f670391f1feead76c2f4577479a2a0a18b
FitseTLT <engfitsum@gmail.com>
2025-05-27 18:49:27 +0300

    revert unnecessary change

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -2359,9 +2359,8 @@ function getReportActionsLength() {
     return Object.keys(allReportActions ?? {}).length;
 }
 
-function getReportActions(reportOrID: Report | string) {
-    const reportID = typeof reportOrID === 'string' ? reportOrID : reportOrID.reportID;
-    return allReportActions?.[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`];
+function getReportActions(report: Report) {
+    return allReportActions?.[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${report.reportID}`];
 }
 
 /**
