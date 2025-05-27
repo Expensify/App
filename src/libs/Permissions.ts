a1e66481aa68315d8c0ada8880e60e2be65c58a6
Sibtain Ali <allroundexperts@gmail.com>
2025-05-28 02:21:00 +0500

    feat: remove the beta

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -70,10 +70,6 @@ function canUseGlobalReimbursementsOnND(betas: OnyxEntry<Beta[]>): boolean {
     return !!betas?.includes(CONST.BETAS.GLOBAL_REIMBURSEMENTS_ON_ND) || canUseAllBetas(betas);
 }
 
-function canUsePrivateDomainOnboarding(betas: OnyxEntry<Beta[]>): boolean {
-    return !!betas?.includes(CONST.BETAS.PRIVATE_DOMAIN_ONBOARDING) || canUseAllBetas(betas);
-}
-
 function canUsePlaidCompanyCards(betas: OnyxEntry<Beta[]>): boolean {
     return !!betas?.includes(CONST.BETAS.PLAID_COMPANY_CARDS) || canUseAllBetas(betas);
 }
@@ -105,7 +101,6 @@ export default {
     canUseProhibitedExpenses,
     canUseInAppProvisioning,
     canUseGlobalReimbursementsOnND,
-    canUsePrivateDomainOnboarding,
     canUseRetractNewDot,
     canUseMultiLevelTags,
     canUseMultiFilesDragAndDrop,
