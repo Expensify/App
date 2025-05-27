dfc3fcfaf2fdc8ffe53997a9ce09bc3d6d7d8102
Adam Horodyski <ad.horodyski@gmail.com>
2025-05-27 15:35:42 +0200

    fix: run prettier on the rnef config file

--- rnef.config.mjs
+++ rnef.config.mjs
@@ -8,9 +8,9 @@ const isHybrid = process.env.IS_HYBRID_APP === 'true';
 /** @type {import('@rnef/config').Config} */
 export default {
     remoteCacheProvider: providerGitHub({
-      owner: "Expensify",
-      repository: "App",
-      token: process.env.GITHUB_TOKEN,
+        owner: 'Expensify',
+        repository: 'App',
+        token: process.env.GITHUB_TOKEN,
     }),
     bundler: pluginMetro(),
     platforms: {
