f7d6bd5fdbf5d5d6ba4fffa9f7abe7a1ffd0a85a
nkdengineer <nkdengineer@outlook.com>
2025-05-30 00:53:21 +0700

    prettier

--- src/libs/getMatchScore.ts
+++ src/libs/getMatchScore.ts
@@ -2,8 +2,14 @@ export default function getMatchScore(str: string, query: string): number {
     const lowerStr = str.toLowerCase();
     const lowerQuery = query.toLowerCase();
 
-    if (lowerStr === lowerQuery) {return 3;}
-    if (lowerStr.startsWith(lowerQuery)) {return 2;}
-    if (lowerStr.includes(lowerQuery)) {return 1;}
+    if (lowerStr === lowerQuery) {
+        return 3;
+    }
+    if (lowerStr.startsWith(lowerQuery)) {
+        return 2;
+    }
+    if (lowerStr.includes(lowerQuery)) {
+        return 1;
+    }
     return 0;
 }
