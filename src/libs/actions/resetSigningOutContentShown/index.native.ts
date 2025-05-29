efef9024c4bee9988a3f73b55bbaa57208cf9b5f
neil-marcellini <neil@expensify.com>
2025-05-29 12:49:33 -0700

    Clean up noop to match others

--- src/libs/actions/resetSigningOutContentShown/index.native.ts
+++ src/libs/actions/resetSigningOutContentShown/index.native.ts
@@ -1,10 +1,5 @@
 import type ResetSigningOutContentShown from './types';
 
-/**
- * No-op implementation for native platforms
- */
-const resetSigningOutContentShown: ResetSigningOutContentShown = () => {
-    // No-op
-};
+const resetSigningOutContentShown: ResetSigningOutContentShown = () => {};
 
 export default resetSigningOutContentShown;
