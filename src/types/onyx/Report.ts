45c922e023def89e22bb6229e84f1ebd32b0c92c
Jasper Huang <jasper@expensify.com>
2025-05-29 16:20:05 -0700

    display thinking indicator

--- src/types/onyx/Report.ts
+++ src/types/onyx/Report.ts
@@ -225,6 +225,9 @@ type Report = OnyxCommon.OnyxValueWithOfflineFeedback<
 
         /** The report's welcome message */
         welcomeMessage?: string;
+
+        /** Agent Zero processing request indicator message */
+        agentZeroProcessingRequestIndicator?: string;
     },
     'addWorkspaceRoom' | 'avatar' | 'createChat' | 'partial' | 'reimbursed' | 'preview' | 'createReport'
 >;
