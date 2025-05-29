d15728dd1b9a8192be5b1540c5299490f9a376c5
Artem Makushov <waterim3009@gmail.com>
2025-05-29 14:23:44 +0200

    fix admin can remove transaction

--- src/pages/iou/request/step/IOURequestEditReportCommon.tsx
+++ src/pages/iou/request/step/IOURequestEditReportCommon.tsx
@@ -53,9 +53,12 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport, 
     const [allReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: (reports) => mapOnyxCollectionItems(reports, reportSelector), canBeMissing: true});
     const [reportNameValuePairs] = useOnyx(ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS, {canBeMissing: true});
     const [allPoliciesID] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: (policies) => mapOnyxCollectionItems(policies, (policy) => policy?.id), canBeMissing: false});
+    const onlyReport = transactionsReports.length === 1 ? transactionsReports.at(0) : undefined;
 
     const currentUserPersonalDetails = useCurrentUserPersonalDetails();
     const [searchValue, debouncedSearchValue, setSearchValue] = useDebouncedState('');
+
+    const isOwner = onlyReport ? onlyReport.ownerAccountID === currentUserPersonalDetails.accountID : false;
 
     const expenseReports = useMemo(
         () =>
@@ -79,8 +82,6 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport, 
             return [];
         }
 
-        const onlyReport = transactionsReports.length === 1 ? transactionsReports.at(0) : undefined;
-
         return expenseReports
             .sort((a, b) => a?.reportName?.localeCompare(b?.reportName?.toLowerCase() ?? '') ?? 0)
             .filter((report) => !debouncedSearchValue || report?.reportName?.toLowerCase().includes(debouncedSearchValue.toLowerCase()))
@@ -94,7 +95,7 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport, 
                     isSelected: onlyReport && report.reportID === onlyReport?.reportID,
                 };
             });
-    }, [allReports, transactionsReports, expenseReports, debouncedSearchValue, options.reports]);
+    }, [allReports, expenseReports, debouncedSearchValue, options.reports, onlyReport]);
 
     const navigateBack = () => {
         Navigation.goBack(backTo);
@@ -122,7 +123,7 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport, 
                 initiallyFocusedOptionKey={transactionsReports.length === 1 ? transactionsReports.at(0)?.reportID : undefined}
                 ListItem={InviteMemberListItem}
                 listFooterContent={
-                    isEditing ? (
+                    isEditing && !isOwner ? (
                         <MenuItem
                             onPress={removeFromReport}
                             title={translate('iou.removeFromReport')}
