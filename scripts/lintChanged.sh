4f9bc6791188ac518fc1727ab21da5b3f817972f
Dylan Le <54580990+MacroLens@users.noreply.github.com>
2025-05-29 11:29:08 -0400

    scripts: lintChanged.sh add CI/local checks
    Move the NPM checking logic to top of script.
    Add CI variable checks to detect if local or on CI.
    Clean some comments.
    
    This commit is a stopgap using depth=30000 until a better
    solution is implemented.

--- scripts/lintChanged.sh
+++ scripts/lintChanged.sh
@@ -8,6 +8,21 @@ TOP="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 readonly TOP
 source "${TOP}/scripts/shellUtils.sh"
 
+# Ensure this script is running inside an npm run context
+if [[ -z "${npm_lifecycle_event:-}" ]]; then
+    info "Re-executing this script from an npm context"
+    exec npx -c "NODE_OPTIONS=--max_old_space_size=8192 ${BASH_SOURCE[0]} $*"
+fi
+
+# Fetch the commit history to include the merge-base commit
+if [[ -z "${CI:-}" ]]; then
+    info "Running lintChanged locally."
+    git fetch origin main --no-tags
+else
+    info "Running lintChanged on CI."
+    git fetch origin main --no-tags --depth=30000
+fi
+
 MERGE_BASE_SHA_HASH="$(git merge-base origin/main HEAD)"
 # Check if output is empty or malformed
 if [[ -z "$MERGE_BASE_SHA_HASH" ]] || ! [[ "$MERGE_BASE_SHA_HASH" =~ ^[a-fA-F0-9]{40}$ ]]; then
@@ -16,17 +31,11 @@ if [[ -z "$MERGE_BASE_SHA_HASH" ]] || ! [[ "$MERGE_BASE_SHA_HASH" =~ ^[a-fA-F0-9
 fi
 
 DIFF_OUTPUT="$(git diff --diff-filter=AMR --name-only "$MERGE_BASE_SHA_HASH" HEAD -- '*.ts' '*.tsx')"
-# Check if there were any changes.
+# Check if there were any changes
 # shellcheck disable=SC2181 # Long command
 if [[ $? -ne 0 ]]; then
     error "git diff failed"
     exit 1
 fi
 
-# Ensure this script is running inside an npm run context
-IS_NPM_CONTEXT="${npm_lifecycle_event:-}"
-if [[ -z $IS_NPM_CONTEXT ]]; then
-    info "Re-executing this script from an npm context"
-    exec npx -c "NODE_OPTIONS=--max_old_space_size=8192 ${BASH_SOURCE[0]} $*"
-fi
 eslint --max-warnings=0 --config ./.eslintrc.changed.js "$DIFF_OUTPUT"
