97005d07b5d99d6406f369a145784f685420d0ef
Dylan Le <54580990+MacroLens@users.noreply.github.com>
2025-05-29 11:02:42 -0400

    scripts: lintChanged.sh unify style
    Unify style on curly braces and semicolons. Remove
    the no-pager option from the diff output

--- scripts/lintChanged.sh
+++ scripts/lintChanged.sh
@@ -10,15 +10,15 @@ source "${TOP}/scripts/shellUtils.sh"
 
 MERGE_BASE_SHA_HASH="$(git merge-base origin/main HEAD)"
 # Check if output is empty or malformed
-if [[ -z "${MERGE_BASE_SHA_HASH}" ]] || ! [[ "${MERGE_BASE_SHA_HASH}" =~ ^[a-fA-F0-9]{40}$ ]]; then
+if [[ -z "$MERGE_BASE_SHA_HASH" ]] || ! [[ "$MERGE_BASE_SHA_HASH" =~ ^[a-fA-F0-9]{40}$ ]]; then
     error "git merge-base returned unexpected output"
     exit 1
 fi
 
-DIFF_OUTPUT="$(git --no-pager diff --diff-filter=AMR --name-only "$MERGE_BASE_SHA_HASH" HEAD -- '*.ts' '*.tsx')"
+DIFF_OUTPUT="$(git diff --diff-filter=AMR --name-only "$MERGE_BASE_SHA_HASH" HEAD -- '*.ts' '*.tsx')"
 # Check if there were any changes.
 # shellcheck disable=SC2181 # Long command
-if [[ $? -ne 0 ]] ; then
+if [[ $? -ne 0 ]]; then
     error "git diff failed"
     exit 1
 fi
@@ -29,4 +29,4 @@ if [[ -z $IS_NPM_CONTEXT ]]; then
     info "Re-executing this script from an npm context"
     exec npx -c "NODE_OPTIONS=--max_old_space_size=8192 ${BASH_SOURCE[0]} $*"
 fi
-eslint --max-warnings=0 --config ./.eslintrc.changed.js "${DIFF_OUTPUT}"
+eslint --max-warnings=0 --config ./.eslintrc.changed.js "$DIFF_OUTPUT"
