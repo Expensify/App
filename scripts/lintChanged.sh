c8987b37cea6a635afecac06ced957e2540ae043
Dylan Le <54580990+MacroLens@users.noreply.github.com>
2025-05-29 10:53:13 -0400

    scripts: lintChanged.sh fix style and clean logic
    Fix style and add additional bash safety to variable
    assignments.
    Add new rerun in NPM context checks.
    Combine error cases for merge-base.

--- scripts/lintChanged.sh
+++ scripts/lintChanged.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-# Checks for changes between the common ancestor of origin/main and the
-# current HEAD. 
+# Lints .ts and .tsx files that have changed in this branch
 
-source scripts/shellUtils.sh
+set -eu
 
-MERGE_BASE_SHA_HASH=$(git merge-base origin/main HEAD)
-# Check if output is empty.
-if [ -z "${MERGE_BASE_SHA_HASH}" ]; then
-    error "git merge-base did not return any output"
-    exit 1
-fi
+TOP="$(realpath "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/..")"
+readonly TOP
+source "${TOP}/scripts/shellUtils.sh"
 
-# Check if the output of merge-base is a single SHA-1 commit hash.
-if ! [[ "${MERGE_BASE_SHA_HASH}" =~ ^[a-fA-F0-9]{40}$ ]]; then
+MERGE_BASE_SHA_HASH="$(git merge-base origin/main HEAD)"
+# Check if output is empty or malformed
+if [[ -z "${MERGE_BASE_SHA_HASH}" ]] || ! [[ "${MERGE_BASE_SHA_HASH}" =~ ^[a-fA-F0-9]{40}$ ]]; then
     error "git merge-base returned unexpected output"
     exit 1
 fi
 
-DIFF_OUTPUT=$(git --no-pager diff --diff-filter=AMR --name-only "${MERGE_BASE_SHA_HASH}" HEAD -- '*.ts' '*.tsx')
-
+DIFF_OUTPUT="$(git --no-pager diff --diff-filter=AMR --name-only "$MERGE_BASE_SHA_HASH" HEAD -- '*.ts' '*.tsx')"
 # Check if there were any changes.
-if [ -z "${MERGE_BASE_SHA_HASH}" ]; then
-    error "git diff returned no changed files"
+# shellcheck disable=SC2181 # Long command
+if [[ $? -ne 0 ]] ; then
+    error "git diff failed"
     exit 1
 fi
 
 # Ensure this script is running inside an npm run context
-if [[ -n "$npm_lifecycle_event" ]]; then
-    eslint --max-warnings=0 --config ./.eslintrc.changed.js "${DIFF_OUTPUT}"
-else
-    error "This script is meant to run by using npm run lint-changed"
+IS_NPM_CONTEXT="${npm_lifecycle_event:-}"
+if [[ -z $IS_NPM_CONTEXT ]]; then
+    info "Re-executing this script from an npm context"
+    exec npx -c "NODE_OPTIONS=--max_old_space_size=8192 ${BASH_SOURCE[0]} $*"
 fi
+eslint --max-warnings=0 --config ./.eslintrc.changed.js "${DIFF_OUTPUT}"
