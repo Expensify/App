f41dcff608ccde1a1ca422e86f388909d6230d53
Github <olimpia.zurek@gmail.com>
2025-05-29 10:19:22 +0200

    fix typo

--- src/hooks/useIsScreenFocus.ts
+++ src/hooks/useIsScreenFocus.ts
@@ -1,7 +1,7 @@
 import {useFocusEffect} from '@react-navigation/native';
 import {useCallback, useState} from 'react';
 
-export default function useIsScreenFocusedRef() {
+export default function useIsScreenFocused() {
     const [isScreenFocused, setIsScreenFocused] = useState(false);
 
     useFocusEffect(
