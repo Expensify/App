7ef2f14c99bf91d7b6fa5c1fa62bf723f8227a96
truph01 <truphd01@gmail.com>
2025-05-30 15:53:28 +0700

    fix: lint

--- src/components/Form/FormWrapper.tsx
+++ src/components/Form/FormWrapper.tsx
@@ -211,6 +211,7 @@ function FormWrapper({
             submitButtonText,
             submitFlexEnabled,
             shouldRenderFooterAboveSubmit,
+            shouldPreventDefaultFocusOnPressSubmit,
         ],
     );
 
