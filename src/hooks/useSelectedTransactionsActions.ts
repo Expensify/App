6ff9644816cfc18c15a0ebda058c9216bd64d3b8
Artem Makushov <waterim3009@gmail.com>
2025-06-02 21:59:55 +0200

    update hook for multiple transactions

--- src/hooks/useSelectedTransactionsActions.ts
+++ src/hooks/useSelectedTransactionsActions.ts
@@ -158,18 +158,26 @@ function useSelectedTransactionsActions({
         }
 
         options.push({
-            value: CONST.REPORT.EXPORT_OPTIONS.DOWNLOAD_CSV,
-            text: translate('common.downloadAsCSV'),
-            icon: Expensicons.Download,
-            onSelected: () => {
-                if (!report) {
-                    return;
-                }
-                exportReportToCSV({reportID: report.reportID, transactionIDList: selectedTransactionsID}, () => {
-                    onExportFailed?.();
-                });
-                setSelectedTransactionsID([]);
-            },
+            value: CONST.REPORT.SECONDARY_ACTIONS.EXPORT,
+            text: translate('common.export'),
+            backButtonText: translate('common.export'),
+            icon: Expensicons.Export,
+            subMenuItems: [
+                {
+                    text: translate('common.basicExport'),
+                    icon: Expensicons.Table,
+                    value: CONST.REPORT.EXPORT_OPTIONS.DOWNLOAD_CSV,
+                    onSelected: () => {
+                        if (!report) {
+                            return;
+                        }
+                        exportReportToCSV({reportID: report.reportID, transactionIDList: selectedTransactionsID}, () => {
+                            onExportFailed?.();
+                        });
+                        setSelectedTransactionsID([]);
+                    },
+                },
+            ],
         });
 
         const canSelectedExpensesBeMoved = selectedTransactions.every((transaction) => {
