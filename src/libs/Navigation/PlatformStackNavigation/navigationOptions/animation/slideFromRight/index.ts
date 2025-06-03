0891793d8d853eb8e4e5f905080eaedc61467a5c
staszekscp <mariusz.stanisz@swmansion.com>
2025-06-03 16:17:49 +0200

    Fix  option value for

--- src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/slideFromRight/index.ts
+++ src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/slideFromRight/index.ts
@@ -1,6 +1,7 @@
 import type {StackNavigationOptions} from '@react-navigation/stack';
 import type {PlatformSpecificNavigationOptions} from '@libs/Navigation/PlatformStackNavigation/types';
+import {InternalPlatformAnimations} from '..';
 
-const slideFromRight: PlatformSpecificNavigationOptions = {animation: 'none', gestureDirection: 'horizontal'} satisfies StackNavigationOptions;
+const slideFromRight: PlatformSpecificNavigationOptions = {animation: InternalPlatformAnimations.SLIDE_FROM_RIGHT, gestureDirection: 'horizontal'} satisfies StackNavigationOptions;
 
 export default slideFromRight;
