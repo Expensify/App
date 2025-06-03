18e7db1f248e50b9c6049aa6559189b9788cbe9b
Agata Kosior <agata.kosior57@gmail.com>
2025-06-03 18:39:34 +0200

    fix: minor translation improvement

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -612,7 +612,7 @@ const translations = {
         someFilesCantBeUploaded: "Algunos archivos no se pueden subir",
         sizeLimitExceeded: "Los archivos deben ser menores a 10 MB. Los archivos más grandes no se subirán.",
         maxFileLimitExceeded: "Puedes subir hasta 30 recibos a la vez. Los extras no se subirán.",
-        unsupportedFileType: "archivos no son compatibles. Solo se subirán los tipos de archivo compatibles. Obtén más información sobre los formatos compatibles."
+        unsupportedFileType: "archivos no son compatibles. Solo se subirán los archivos compatibles. Obtén más información sobre los formatos compatibles."
     },
     dropzone: {
         addAttachments: 'Añadir archivos adjuntos',
