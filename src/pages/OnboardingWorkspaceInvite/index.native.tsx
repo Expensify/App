4405a1ab3ad39ec3ecf8038e80f29b96d0f0a42b
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-06-04 22:09:29 +0530

    Fix back handler code

--- src/pages/OnboardingWorkspaceInvite/index.native.tsx
+++ src/pages/OnboardingWorkspaceInvite/index.native.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import {useFocusEffect} from '@react-navigation/native';
+import React, {useCallback} from 'react';
+import {BackHandler} from 'react-native';
 import BaseOnboardingWorkspaceInvite from './BaseOnboardingWorkspaceInvite';
 import type {OnboardingWorkspaceInviteProps} from './types';
 
-function OnboardingWorkspaceInvite({...rest}: OnboardingWorkspaceInviteProps) {
+function OnboardingWorkspaceInvite(props: OnboardingWorkspaceInviteProps) {
+    // To block android native back button behavior
+    useFocusEffect(
+        useCallback(() => {
+            // Return true to indicate that the back button press is handled here
+            const backAction = () => true;
+
+            const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+            return () => backHandler.remove();
+        }, []),
+    );
     return (
         <BaseOnboardingWorkspaceInvite
             shouldUseNativeStyles
             // eslint-disable-next-line react/jsx-props-no-spreading
-            {...rest}
+            {...props}
         />
     );
 }
--- src/pages/OnboardingWorkspaceOptional/index.native.tsx
+++ src/pages/OnboardingWorkspaceOptional/index.native.tsx
@@ -1,21 +1,8 @@
-import {useFocusEffect} from '@react-navigation/native';
-import React, {useCallback} from 'react';
-import {BackHandler} from 'react-native';
+import React from 'react';
 import BaseOnboardingWorkspaceOptional from './BaseOnboardingWorkspaceOptional';
 import type {OnboardingWorkspaceOptionalProps} from './types';
 
 function OnboardingWorkspaceOptional(props: OnboardingWorkspaceOptionalProps) {
-    // To block android native back button behavior
-    useFocusEffect(
-        useCallback(() => {
-            // Return true to indicate that the back button press is handled here
-            const backAction = () => true;
-
-            const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
-
-            return () => backHandler.remove();
-        }, []),
-    );
     return (
         <BaseOnboardingWorkspaceOptional
             shouldUseNativeStyles
