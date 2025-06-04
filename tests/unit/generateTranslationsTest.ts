598a98c32ee6f7ceccd84cf83ae676beae3ec37f
rory <rory@expensify.com>
2025-06-03 19:10:46 -0700

    Create failing test for recursive templates

--- tests/unit/generateTranslationsTest.ts
+++ tests/unit/generateTranslationsTest.ts
@@ -79,5 +79,81 @@ describe('generateTranslations', () => {
             `),
             );
         });
+
+        it('handles nested template expressions', async () => {
+            fs.writeFileSync(
+                EN_PATH,
+                StringUtils.dedent(`
+                    const strings = {
+                        simple: (name: string, greeting: string) => \`\${greeting} good sir \${name}!\`,
+                        simpleWithDotNotation: (myParams: {name: string; greeting: string}) => \`\${myParams.greeting} good sir \${myParams.greeting}!\`,
+                        simpleWithNullishCoalesce: (name: string) => \`Pay \${name ?? 'someone'}\`,
+                        simpleWithFalsyCoalesce: (name: string) => \`Pay \${name || 'someone'}\`,
+                        complex: (action: {actionName: string}) => \`Edit \${action.actionName === 'shouldNotBeTranslated' ? 'expense' : 'comment'}\`,
+                        extraComplex: (payer: string) => \`\${payer ? \`\${payer} \` : ''}paid elsewhere\`,
+                        evenMoreComplex: (someBool: boolean, someOtherBool: boolean) => \`\${someBool ? \`\${someOtherBool ? 'Hello' : 'Goodbye'} moon\` : 'Goodnight, moon' }, kupo\`,
+                        tooComplex: (numScanning: number, numPending: number) => {
+                            const statusText: string[] = [];
+                            if (numScanning > 0) {
+                                statusText.push(\`\${numScanning} scanning\`);
+                            }
+                            if (numPending > 0) {
+                                statusText.push(\`\${numPending} pending\`);
+                            }
+                            return statusText.length > 0 ? \`1 expense (\${statusText.join(', ')})\` : \`1 expense\`;
+                        },
+                        unrealisticallyComplex: (numScanning: number, numPending: number) => \`\${(() => {
+                            const statusText: string[] = [];
+                            if (numScanning > 0) {
+                                statusText.push(\`\${numScanning} scanning\`);
+                            }
+                            if (numPending > 0) {
+                                statusText.push(\`\${numPending} pending\`);
+                            }
+                            return statusText.length > 0 ? \`1 expense (\${statusText.join(', ')})\` : \`1 expense\`;
+                        })()} If someone really uses an IIFE in here, then we've got bigger problems.\`,
+                    };
+                    export default strings;
+            `),
+                'utf8',
+            );
+            await generateTranslations();
+            const itContent = fs.readFileSync(IT_PATH, 'utf8');
+            expect(itContent).toStrictEqual(
+                StringUtils.dedent(`
+                    const strings = {
+                        simple: (name: string, greeting: string) => \`[it] \${greeting} good sir \${name}!\`,
+                        simpleWithDotNotation: (myParams: {name: string; greeting: string}) => \`[it] \${myParams.greeting} good sir \${myParams.greeting}!\`,
+                        simpleWithNullishCoalesce: (name: string) => \`[it] Pay \${name ?? '[it] someone'}\`,
+                        simpleWithFalsyCoalesce: (name: string) => \`[it] Pay \${name || '[it] someone'}\`,
+                        complex: (action: {actionName: string}) => \`[it] Edit \${action.actionName === 'expense' ? '[it] expense' : '[it] comment'}\`,
+                        extraComplex: (payer: string) => \`[it] \${payer ? \`[it] \${payer} as payer \` : ''}paid elsewhere\`\`,
+                        extraComplexButJustWhitespace: (payer: string) => \`[it] \${payer ? \`\${payer} \` : ''}paid elsewhere\`\`,
+                        evenMoreComplex: (someBool: boolean, someOtherBool: boolean) => \`[it] \${someBool ? \`[it] \${someOtherBool ? '[it] Hello' : '[it] Goodbye'} moon\` : '[it] Goodnight, moon' }, kupo\`,
+                        tooComplex: (numScanning: number, numPending: number) => {
+                            const statusText: string[] = [];
+                            if (numScanning > 0) {
+                                statusText.push(\`[it] \${numScanning} scanning\`);
+                            }
+                            if (numPending > 0) {
+                                statusText.push(\`[it] \${numPending} pending\`);
+                            }
+                            return statusText.length > 0 ? \`[it] 1 expense (\${statusText.join(', ')})\` : \`[it] 1 expense\`;
+                        },
+                        unrealisticallyComplex: (numScanning: number, numPending: number) => \`[it] \${(() => {
+                            const statusText: string[] = [];
+                            if (numScanning > 0) {
+                                statusText.push(\`[it] \${numScanning} scanning\`);
+                            }
+                            if (numPending > 0) {
+                                statusText.push(\`[it] \${numPending} pending\`);
+                            }
+                            return statusText.length > 0 ? \`[it] 1 expense (\${statusText.join(', ')})\` : \`[it] 1 expense\`;
+                        })()} If someone really uses an IIFE in here, then we've got bigger problems.\`,
+                    };
+                    export default strings;
+            `),
+            );
+        });
     });
 });
