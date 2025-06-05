947e6d2f32b2346dbd31a4ac2fcba985de9fdd69
Carlos Martins <cmartins@expensify.com>
2025-06-05 16:16:58 -0600

    use add transactions

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -9767,7 +9767,7 @@ function getOutstandingReportsForUser(
         return [];
     }
     return Object.values(reports)
-        .filter((report) => isReportOutstanding(report, policyID, reportNameValuePairs) && report?.ownerAccountID === reportOwnerAccountID)
+        .filter((report) => report?.ownerAccountID === reportOwnerAccountID && report?.policyID === policyID && canAddTransaction(report, !!reportNameValuePairs?.[`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${report?.reportID}`]?.private_isArchived))
         .sort((a, b) => a?.reportName?.localeCompare(b?.reportName?.toLowerCase() ?? '') ?? 0);
 }
 
