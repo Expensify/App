fa770416a44481823ede9332449e97327ec3dc33
Christoph Pader <chris@margelo.com>
2025-06-06 12:26:54 +0200

    fix: delete updates to deprecated ImageManipulator

--- src/libs/fileDownload/getImageManipulator/index.native.ts
+++ src/libs/fileDownload/getImageManipulator/index.native.ts
@@ -1,17 +1,13 @@
-import {ImageManipulator} from 'expo-image-manipulator';
-import type {FileObject} from '@pages/media/AttachmentModalScreen/types';
+import {manipulateAsync} from 'expo-image-manipulator';
+import type {FileObject} from '@components/AttachmentModal';
 import type ImageManipulatorConfig from './type';
 
 export default function getImageManipulator({fileUri, width, height, type, fileName}: ImageManipulatorConfig): Promise<FileObject> {
-    return ImageManipulator.manipulate(fileUri ?? '')
-        .resize({width, height})
-        .renderAsync()
-        .then((image) => image.saveAsync())
-        .then((result) => ({
-            uri: result.uri,
-            width: result.width,
-            height: result.height,
-            type,
-            name: fileName,
-        }));
+    return manipulateAsync(fileUri ?? '', [{resize: {width, height}}]).then((result) => ({
+        uri: result.uri,
+        width: result.width,
+        height: result.height,
+        type,
+        name: fileName,
+    }));
 }
