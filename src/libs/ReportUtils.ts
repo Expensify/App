24a54b888c6948f741f0731311529df3508f583e
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-06-09 10:12:59 +0200

    bring back report utils change

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -953,22 +953,29 @@ Onyx.connect({
 });
 
 let allTransactions: OnyxCollection<Transaction> = {};
+let reportsTransactions: Record<string, Transaction[]> = {};
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.TRANSACTION,
     waitForCollectionCallback: true,
     callback: (value) => {
-        allTransactions = Object.fromEntries(Object.entries(value).filter(([, transaction]) => transaction));
-    },
-});
-
-let reportsTransactions: Record<string, Transaction[]> = {};
-Onyx.connect({
-    key: ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS,
-    callback: (value) => {
         if (!value) {
             return;
         }
-        reportsTransactions = Object.fromEntries(Object.entries(value).map(([reportID, {transactions}]) => [reportID, Object.values(transactions ?? {})]));
+        allTransactions = Object.fromEntries(Object.entries(value).filter(([, transaction]) => transaction));
+
+        reportsTransactions = Object.values(value).reduce<Record<string, Transaction[]>>((all, transaction) => {
+            const reportsMap = all;
+            if (!transaction?.reportID) {
+                return reportsMap;
+            }
+
+            if (!reportsMap[transaction.reportID]) {
+                reportsMap[transaction.reportID] = [];
+            }
+            reportsMap[transaction.reportID].push(transaction);
+
+            return all;
+        }, {});
     },
 });
 
