ac069d77a6b7c33738b26a9375d2b461ad7d6d13
Marcin Swornowski <swor71@gmail.com>
2025-06-09 17:14:27 +0200

    add: check for localSource in AttachmentView

--- src/components/Attachments/AttachmentView/index.tsx
+++ src/components/Attachments/AttachmentView/index.tsx
@@ -88,7 +88,9 @@ type AttachmentViewProps = Attachment & {
 };
 
 function checkIsFileImage(source: string | number | ImageURISource | ImageURISource[], fileName: string | undefined) {
-    const isSourceImage = typeof source === 'number' || typeof source === 'string';
+    const isLocalSource = typeof source === 'string' && /^file:|^blob:/.test(source);
+
+    const isSourceImage = typeof source === 'number' || (typeof source === 'string' && Str.isImage(source)) || isLocalSource;
 
     const isFileNameImage = fileName && Str.isImage(fileName);
 
