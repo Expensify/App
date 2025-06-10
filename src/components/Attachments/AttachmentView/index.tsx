b395fdae6e276c4d84d9a842161303d70cd8195c
Marcin Swornowski <swor71@gmail.com>
2025-06-10 15:38:10 +0200

    chore: moved check for local source next to if block responsible for returning an image

--- src/components/Attachments/AttachmentView/index.tsx
+++ src/components/Attachments/AttachmentView/index.tsx
@@ -88,9 +88,7 @@ type AttachmentViewProps = Attachment & {
 };
 
 function checkIsFileImage(source: string | number | ImageURISource | ImageURISource[], fileName: string | undefined) {
-    const isLocalSource = typeof source === 'string' && /^file:|^blob:/.test(source);
-
-    const isSourceImage = typeof source === 'number' || (typeof source === 'string' && Str.isImage(source)) || isLocalSource;
+    const isSourceImage = typeof source === 'number' || (typeof source === 'string' && Str.isImage(source));
 
     const isFileNameImage = fileName && Str.isImage(fileName);
 
@@ -252,9 +250,14 @@ function AttachmentView({
     // For this check we use both source and file.name since temporary file source is a blob
     // both PDFs and images will appear as images when pasted into the text field.
     // We also check for numeric source since this is how static images (used for preview) are represented in RN.
-    const isFileImage = checkIsFileImage(source, file?.name);
 
-    if (isFileImage) {
+    // isLocalSource checks if the source is blob as that's the type of the temp image coming from mobile web
+    const isFileImage = checkIsFileImage(source, file?.name);
+    const isLocalSourceImage = typeof source === 'string' && source.startsWith('blob:');
+
+    const isImage = isFileImage ?? isLocalSourceImage;
+
+    if (isImage) {
         if (imageError && (typeof fallbackSource === 'number' || typeof fallbackSource === 'function')) {
             return (
                 <View style={[styles.flexColumn, styles.alignItemsCenter, styles.justifyContentCenter]}>
@@ -301,7 +304,7 @@ function AttachmentView({
                         file={file}
                         isAuthTokenRequired={isAuthTokenRequired}
                         loadComplete={loadComplete}
-                        isImage={isFileImage}
+                        isImage={isImage}
                         onPress={onPress}
                         onError={() => {
                             if (isOffline) {
