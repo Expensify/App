60ed01900d7355329241826ffee0404a9c79ee3e
NJ-2020 <ntt.london@gmail.com>
2025-06-10 11:05:41 +0700

    add missing comments

--- src/types/onyx/Attachment.tsx
+++ src/types/onyx/Attachment.tsx
@@ -1,8 +1,11 @@
 type Attachment = {
+    /** Attachment ID of the attachment */
     attachmentID: string;
 
+    /** Source url of the attachment either can be local or remote url */
     source?: string;
 
+    /** Remote source url of the attachment */
     remoteSource?: string;
 };
 
