dfb768d570eadf61e5368cc88b7bb0afa2677f68
NikkiWines <nikkiwines@expensify.com>
2025-06-11 22:04:40 +0100

    add DelegateNoAccessModalProvider

---
+++ src/components/DelegateNoAccessModalProvider.tsx
@@ -0,0 +1,62 @@
+import React, {createContext, useMemo, useState} from 'react';
+import useLocalize from '@hooks/useLocalize';
+import useOnyx from '@hooks/useOnyx';
+import ONYXKEYS from '@src/ONYXKEYS';
+import ConfirmModal from './ConfirmModal';
+import CONST from '@src/CONST';
+import TextLink from './TextLink';
+import useDelegateUserDetails from '@hooks/useDelegateUserDetails';
+import Text from './Text';
+
+type DelegateNoAccessContextType = {
+    showDelegateNoAccessModal: () => void;
+    isActingAsDelegate: boolean;
+};
+
+const DelegateNoAccessContext = createContext<DelegateNoAccessContextType>({
+    showDelegateNoAccessModal: () => {},
+    isActingAsDelegate: false,
+});
+
+function DelegateNoAccessModalProvider({children}: React.PropsWithChildren) {
+    const {translate} = useLocalize();
+    const {delegatorEmail} = useDelegateUserDetails();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [account] = useOnyx(ONYXKEYS.ACCOUNT, {canBeMissing: true});
+    const isActingAsDelegate = !!account?.delegatedAccess?.delegate;
+
+    const delegateNoAccessPrompt = (
+        <Text>
+            {translate('delegate.notAllowedMessageStart')}
+            <TextLink href={CONST.DELEGATE_ROLE_HELP_DOT_ARTICLE_LINK}>{translate('delegate.notAllowedMessageHyperLinked')}</TextLink>
+            {translate('delegate.notAllowedMessageEnd', {accountOwnerEmail: delegatorEmail ?? ''})}
+        </Text>
+    );
+    const contextValue = useMemo(
+        () => ({
+            isActingAsDelegate,
+            showDelegateNoAccessModal: () => setIsModalOpen(true),
+        }),
+        [isActingAsDelegate],
+    );
+
+    return (
+        <DelegateNoAccessContext.Provider value={contextValue}>
+            {children}
+            <ConfirmModal
+                isVisible={isModalOpen}
+                onConfirm={() => setIsModalOpen(false)}
+                onCancel={() => setIsModalOpen(false)}
+                title={translate('delegate.notAllowed')}
+                prompt={delegateNoAccessPrompt}
+                confirmText={translate('common.buttonConfirm')}
+                shouldShowCancelButton={false}
+            />
+        </DelegateNoAccessContext.Provider>
+    );
+}
+
+DelegateNoAccessModalProvider.displayName = 'DelegateNoAccessModal';
+
+export default DelegateNoAccessModalProvider;
+export {DelegateNoAccessContext};
