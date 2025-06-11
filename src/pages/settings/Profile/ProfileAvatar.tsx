2e0df3311a48ea1409bb3d98a4efbf4085dfc704
Linh <vovanlinh29546@gmail.com>
2025-06-11 15:35:46 +0700

    fix: eslint fail

--- src/pages/settings/Profile/ProfileAvatar.tsx
+++ src/pages/settings/Profile/ProfileAvatar.tsx
@@ -16,9 +16,9 @@ import type SCREENS from '@src/SCREENS';
 type ProfileAvatarProps = PlatformStackScreenProps<AuthScreensParamList, typeof SCREENS.PROFILE_AVATAR>;
 
 function ProfileAvatar({route}: ProfileAvatarProps) {
-    const [personalDetails] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST);
-    const [personalDetailsMetadata] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_METADATA);
-    const [isLoadingApp] = useOnyx(ONYXKEYS.IS_LOADING_APP, {initialValue: true});
+    const [personalDetails] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {canBeMissing: true});
+    const [personalDetailsMetadata] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_METADATA, {canBeMissing: true});
+    const [isLoadingApp] = useOnyx(ONYXKEYS.IS_LOADING_APP, {initialValue: true, canBeMissing: true});
     const personalDetail = personalDetails?.[route.params.accountID];
     const avatarURL = personalDetail?.avatar ?? '';
     const accountID = Number(route.params.accountID ?? CONST.DEFAULT_NUMBER_ID);
