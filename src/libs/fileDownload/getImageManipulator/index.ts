2f7ac48067898c27010ed3c12cde144900845716
Christoph Pader <chris@margelo.com>
2025-06-12 15:22:45 +0200

    revert: image manipulator changes

--- src/libs/fileDownload/getImageManipulator/index.ts
+++ src/libs/fileDownload/getImageManipulator/index.ts
@@ -1,18 +1,14 @@
-import {ImageManipulator} from 'expo-image-manipulator';
+import {manipulateAsync} from 'expo-image-manipulator';
 import type ImageManipulatorConfig from './type';
 
 export default function getImageManipulator({fileUri, width, height, fileName}: ImageManipulatorConfig): Promise<File> {
-    return ImageManipulator.manipulate(fileUri ?? '')
-        .resize({width, height})
-        .renderAsync()
-        .then((image) => image.saveAsync())
-        .then((result) =>
-            fetch(result.uri)
-                .then((res) => res.blob())
-                .then((blob) => {
-                    const resizedFile = new File([blob], `${fileName}.jpeg`, {type: 'image/jpeg'});
-                    resizedFile.uri = URL.createObjectURL(resizedFile);
-                    return resizedFile;
-                }),
-        );
+    return manipulateAsync(fileUri ?? '', [{resize: {width, height}}]).then((result) =>
+        fetch(result.uri)
+            .then((res) => res.blob())
+            .then((blob) => {
+                const resizedFile = new File([blob], `${fileName}.jpeg`, {type: 'image/jpeg'});
+                resizedFile.uri = URL.createObjectURL(resizedFile);
+                return resizedFile;
+            }),
+    );
 }
