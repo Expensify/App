9a0579d411e65cf3845f689476db5a58896cd0d8
Piyush <chandra.piyush17@gmail.com>
2025-06-12 17:09:29 +0530

    Fix eslint error

--- src/components/ComposeProviders.tsx
+++ src/components/ComposeProviders.tsx
@@ -8,7 +8,7 @@ type ComposeProvidersProps = ChildrenProps & {
 };
 
 function ComposeProviders(props: ComposeProvidersProps): ReactNode {
-    return props.components.reduceRight((memo, Component, idx) => <Component key={idx}>{memo}</Component>, props.children);
+    return props.components.reduceRight((memo, Component) => <Component>{memo}</Component>, props.children);
 }
 
 ComposeProviders.displayName = 'ComposeProviders';
