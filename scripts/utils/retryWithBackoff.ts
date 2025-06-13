e6fba10321769ddbd1d388022ed9e2f971c13a30
rory <rory@expensify.com>
2025-06-13 10:59:16 -0700

    Add comments in retryWithBackoff

--- scripts/utils/retryWithBackoff.ts
+++ scripts/utils/retryWithBackoff.ts
@@ -5,12 +5,18 @@ type RetryOptions = {
     isRetryable?: (err: unknown) => boolean;
 };
 
+/**
+ * Return a promise that resolves after the given number of ms.
+ */
 function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     });
 }
 
+/**
+ * Attempt an asynchronous operation (such as a network request) with exponential backoff, up to a certain number of retries.
+ */
 async function retryWithBackoff<T>(fn: () => Promise<T>, {maxRetries = 5, initialDelayMs = 1000, factor = 2, isRetryable = () => true}: RetryOptions = {}): Promise<T> {
     let attempt = 0;
     let delay = initialDelayMs;
