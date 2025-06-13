c9ec61f9eab3b62d295318023647335a5a50b53e
rory <rory@expensify.com>
2025-06-13 11:15:16 -0700

    Rebuild gh actions

--- .github/actions/javascript/proposalPoliceComment/index.js
+++ .github/actions/javascript/proposalPoliceComment/index.js
@@ -18761,11 +18761,17 @@ exports["default"] = OpenAIUtils;
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+/**
+ * Return a promise that resolves after the given number of ms.
+ */
 function sleep(ms) {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     });
 }
+/**
+ * Attempt an asynchronous operation (such as a network request) with exponential backoff, up to a certain number of retries.
+ */
 async function retryWithBackoff(fn, { maxRetries = 5, initialDelayMs = 1000, factor = 2, isRetryable = () => true } = {}) {
     let attempt = 0;
     let delay = initialDelayMs;
