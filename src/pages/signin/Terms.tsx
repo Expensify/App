23c7b9076527f81693c87f986201948964966709
Piyush <chandra.piyush17@gmail.com>
2025-06-15 23:40:24 +0530

    added styling part in RenderHTML

--- src/pages/signin/Terms.tsx
+++ src/pages/signin/Terms.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import type {StyleProp, TextStyle} from 'react-native';
 import RenderHTML from '@components/RenderHTML';
+import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
 
 function Terms() {
+    const styles = useThemeStyles();
     const {translate} = useLocalize();
+    const [containerStyles] = useMemo<Array<StyleProp<TextStyle>>>(
+        () => [
+            [styles.textExtraSmallSupporting, styles.link],
+            [styles.textExtraSmallSupporting, styles.mb4],
+        ],
+        [styles],
+    );
 
-    return <RenderHTML html={`<muted-text>${translate('termsOfUse.terms')}</muted-text>`} />;
+    return (
+        <Text style={containerStyles}>
+            <RenderHTML html={`<muted-text>${translate('termsOfUse.terms')}</muted-text>`} />
+        </Text>
+    );
 }
 
 Terms.displayName = 'Terms';
