847e4749c6b7067849aa6a2d47a8d470a09ccc71
truph01 <truphd01@gmail.com>
2025-06-16 18:02:25 +0700

    fix: prettier

--- tests/unit/Search/SearchUIUtilsTest.ts
+++ tests/unit/Search/SearchUIUtilsTest.ts
@@ -1253,11 +1253,7 @@ describe('SearchUIUtils', () => {
                 .map((section) => section.menuItems)
                 .flat();
 
-            const expectedQueries = [
-                'type:expense sortBy:date sortOrder:desc',
-                'type:expense sortBy:date sortOrder:desc groupBy:reports',
-                'type:chat sortBy:date sortOrder:desc',
-            ];
+            const expectedQueries = ['type:expense sortBy:date sortOrder:desc', 'type:expense sortBy:date sortOrder:desc groupBy:reports', 'type:chat sortBy:date sortOrder:desc'];
 
             menuItems.forEach((item, index) => {
                 expect(item.getSearchQuery()).toStrictEqual(expectedQueries.at(index));
