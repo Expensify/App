3ad6e0f728f35029304f11e903c6bbdfec03cc02
VickyStash <vikstash@gmail.com>
2025-06-17 11:16:07 +0200

    Revert "Update canUseMultiScan for UI testing"
    This reverts commit 0b99fda5813eaca02964125470bd7b42580e1089.

--- src/pages/iou/request/step/IOURequestStepScan/index.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.tsx
@@ -135,7 +135,7 @@ function IOURequestStepScan({
     const isEditing = action === CONST.IOU.ACTION.EDIT;
     // TODO: use correct canUseMultiScan value when all multi-scan functionality is implemented
     // const canUseMultiScan = isBetaEnabled(CONST.BETAS.NEWDOT_MULTI_SCAN) && !isEditing && iouType !== CONST.IOU.TYPE.SPLIT && !backTo && !backToReport;
-    const canUseMultiScan = true && !isEditing && iouType !== CONST.IOU.TYPE.SPLIT && !backTo && !backToReport;
+    const canUseMultiScan = false;
 
     const [optimisticTransactions] = useOnyx(ONYXKEYS.COLLECTION.TRANSACTION_DRAFT, {
         selector: (items) => Object.values(items ?? {}),
