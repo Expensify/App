3a121af4f00493f82e851145975cc27ffe62f659
Georgia Monahan <georgia@expensify.com>
2025-06-18 15:47:23 +0100

    prettier

--- src/pages/home/report/ReportActionItemMessageHeaderSender/index.tsx
+++ src/pages/home/report/ReportActionItemMessageHeaderSender/index.tsx
@@ -2,7 +2,7 @@ import React, {useMemo} from 'react';
 import Text from '@components/Text';
 import UserDetailsTooltip from '@components/UserDetailsTooltip';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {splitTextWithEmojis, getProcessedText} from '@libs/EmojiUtils';
+import {getProcessedText, splitTextWithEmojis} from '@libs/EmojiUtils';
 import type ReportActionItemMessageHeaderSenderProps from './types';
 
 function ReportActionItemMessageHeaderSender({fragmentText, accountID, delegateAccountID, actorIcon, isSingleLine, shouldShowTooltip}: ReportActionItemMessageHeaderSenderProps) {
