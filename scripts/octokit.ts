8744713c5e18365e4ca41bb65c2aa9a56dafeadc
rory <rory@expensify.com>
2025-06-19 14:41:26 -0700

    Put token validation in GithubUtils

--- scripts/octokit.ts
+++ scripts/octokit.ts
@@ -2,11 +2,6 @@ if (!process.env.GITHUB_REPOSITORY) {
     process.env.GITHUB_REPOSITORY = 'Expensify/App';
 }
 
-if (!process.env.GITHUB_TOKEN) {
-    console.error('GITHUB_TOKEN env var not found');
-    process.exit(1);
-}
-
 // eslint-disable-next-line
 const {default: GitHubUtils} = require('@github/libs/GithubUtils');
 
