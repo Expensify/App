35ab31e094215667e04ae0547e1e949ff6b80d42
rory <rory@expensify.com>
2025-06-19 14:38:57 -0700

    Use require instead of import

--- scripts/octokit.ts
+++ scripts/octokit.ts
@@ -1,10 +1,14 @@
-/* eslint-disable */
-
 if (!process.env.GITHUB_REPOSITORY) {
     process.env.GITHUB_REPOSITORY = 'Expensify/App';
 }
 
-import GitHubUtils from '@github/libs/GithubUtils';
+if (!process.env.GITHUB_TOKEN) {
+    console.error('GITHUB_TOKEN env var not found');
+    process.exit(1);
+}
+
+// eslint-disable-next-line
+const {default: GitHubUtils} = require('@github/libs/GithubUtils');
 
 console.log(`Working directory: ${process.cwd()}`);
 console.log('🐙 GitHubUtils has been imported to the repl.\n');
