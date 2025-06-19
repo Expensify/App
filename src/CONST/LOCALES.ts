4f3d3dcc1720c0e4e603bd9be66809be822bfc14
ganzz4 <arrvvtd@gmail.com>
2025-06-19 16:14:30 +0300

    style: run prettier to fix formatting #64420

--- src/CONST/LOCALES.ts
+++ src/CONST/LOCALES.ts
@@ -68,12 +68,12 @@ type TranslationTargetLocale = ValueOf<typeof TRANSLATION_TARGET_LOCALES>;
 
 // Sort all locales alphabetically by their display names
 const SORTED_LOCALES = Object.values({...FULLY_SUPPORTED_LOCALES, ...BETA_LOCALES})
-    .map(locale => ({
+    .map((locale) => ({
         locale,
-        displayName: LOCALE_TO_LANGUAGE_STRING[locale]
+        displayName: LOCALE_TO_LANGUAGE_STRING[locale],
     }))
     .sort((a, b) => a.displayName.localeCompare(b.displayName))
-    .map(item => item.locale);
+    .map((item) => item.locale);
 
 function isSupportedLocale(locale: string): locale is Locale {
     return (Object.values(LOCALES) as readonly string[]).includes(locale);
