4da86a146ced4cda6fe99d7f17d679c05faaa5df
neil-marcellini <neil@expensify.com>
2025-06-20 06:47:29 -0700

    Update comment

--- src/libs/actions/Session/afterSignOutRedirect/index.native.ts
+++ src/libs/actions/Session/afterSignOutRedirect/index.native.ts
@@ -1,9 +1,10 @@
 import type AfterSignOutRedirect from './types';
 
+
 /**
  * Native implementation of afterSignOutRedirect is a no-op.
  *
- * For Hybrid App where there are two platforms, we handle the sign out of Classic elsewhere, with HybridAppModule.closeReactNativeApp.
+ * For Hybrid App where there are two platforms, we handle the sign out of Classic elsewhere, with HybridAppModule.signOutFromOldDot.
  * For standalone New Expensify, we don't need to redirect to Classic because it's a separate app.
  */
 const afterSignOutRedirect: AfterSignOutRedirect = () => {
