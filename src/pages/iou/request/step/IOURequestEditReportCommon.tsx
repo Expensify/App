a85a2ddfecb50f7d13cdd2a55dda5b6b6f6da83c
Linh <vovanlinh29546@gmail.com>
2025-06-24 11:00:41 +0700

    fix: handlle change report in creation flow

--- src/pages/iou/request/step/IOURequestEditReportCommon.tsx
+++ src/pages/iou/request/step/IOURequestEditReportCommon.tsx
@@ -41,9 +41,10 @@ type Props = {
     backTo: Route | undefined;
     transactionsReports: Report[];
     selectReport: (item: ReportListItem) => void;
+    policyID?: string;
 };
 
-function IOURequestEditReportCommon({backTo, transactionsReports, selectReport}: Props) {
+function IOURequestEditReportCommon({backTo, transactionsReports, policyID: policyIDFromProps, selectReport}: Props) {
     const {translate} = useLocalize();
     const {options} = useOptionsList();
     const [allReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: (reports) => mapOnyxCollectionItems(reports, reportSelector), canBeMissing: true});
@@ -56,7 +57,7 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport}:
     const expenseReports = useMemo(
         () =>
             Object.values(allPoliciesID ?? {}).flatMap((policyID) => {
-                if (!policyID) {
+                if (!policyID || (policyIDFromProps && policyID !== policyIDFromProps)) {
                     return [];
                 }
                 const reports = getOutstandingReportsForUser(
@@ -67,7 +68,7 @@ function IOURequestEditReportCommon({backTo, transactionsReports, selectReport}:
                 );
                 return reports;
             }),
-        [allReports, currentUserPersonalDetails.accountID, transactionsReports, allPoliciesID, reportNameValuePairs],
+        [allReports, currentUserPersonalDetails.accountID, transactionsReports, allPoliciesID, reportNameValuePairs, policyIDFromProps],
     );
 
     const reportOptions: ReportListItem[] = useMemo(() => {
