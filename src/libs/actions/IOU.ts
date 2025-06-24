8107215e4ba826c9b27a97975457a076e76af390
FitseTLT <engfitsum@gmail.com>
2025-06-24 20:18:54 +0300

    update to navigate to money request report only if it is the top most report

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -5464,7 +5464,7 @@ function submitPerDiemExpense(submitPerDiemExpenseInformation: PerDiemExpenseInf
         transactionParams,
         moneyRequestReportID,
     });
-    const activeReportID = isMoneyRequestReport ? report?.reportID : chatReport.reportID;
+    const activeReportID = isMoneyRequestReport && Navigation.getTopmostReportId() === report?.reportID ? report?.reportID : chatReport.reportID;
 
     const customUnitRate = getPerDiemRateCustomUnitRate(policyParams.policy, customUnit.customUnitRateID);
 
