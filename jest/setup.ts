8d99e23c9b3605b1bb770fdf7ef07f1a7810a3e8
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-06-25 10:26:09 +0200

    move react-native-nitro-sqlite mock to jest/setup.ts

--- jest/setup.ts
+++ jest/setup.ts
@@ -175,3 +175,7 @@ jest.mock('@src/hooks/useWorkletStateMachine/executeOnUIRuntimeSync', () => ({
     __esModule: true,
     default: jest.fn(() => jest.fn()), // Return a function that returns a function
 }));
+
+jest.mock('react-native-nitro-sqlite', () => ({
+    open: jest.fn(),
+}));
