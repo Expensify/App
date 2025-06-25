c201f4e7120497361b7a180161f3ed4f1fc52a34
thelullabyy <the.lullaby2@hotmail.com>
2025-06-25 11:36:56 +0700

    fix: Open reports show receiver in narrow screen

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -910,12 +910,13 @@ function getReportSections(data: OnyxTypes.SearchResults['data'], metadata: Onyx
             const isIOUReport = reportItem.type === CONST.REPORT.TYPE.IOU;
 
             const reportPendingAction = reportItem?.pendingAction ?? reportItem?.pendingFields?.preview;
+            const shouldShowBlankTo = !reportItem || isOpenExpenseReport(reportItem);
             reportIDToTransactions[reportKey] = {
                 ...reportItem,
                 action: getAction(data, allViolations, key),
                 keyForList: reportItem.reportID,
                 from: data.personalDetailsList?.[reportItem.accountID ?? CONST.DEFAULT_NUMBER_ID],
-                to: reportItem.managerID ? data.personalDetailsList?.[reportItem.managerID] : emptyPersonalDetails,
+                to: !shouldShowBlankTo && reportItem.managerID ? data.personalDetailsList?.[reportItem.managerID] : emptyPersonalDetails,
                 transactions,
                 ...(reportPendingAction ? {pendingAction: reportPendingAction} : {}),
             };
