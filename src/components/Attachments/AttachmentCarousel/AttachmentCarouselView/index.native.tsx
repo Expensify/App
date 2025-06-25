053056a1846b0f7a9a6f494ef97348416d93808b
Marcin Swornowski <swor71@gmail.com>
2025-06-25 09:35:55 +0200

    fix: incorrect spacing for photos on ios

--- src/components/Attachments/AttachmentCarousel/AttachmentCarouselView/index.native.tsx
+++ src/components/Attachments/AttachmentCarousel/AttachmentCarouselView/index.native.tsx
@@ -9,7 +9,9 @@ import * as Illustrations from '@components/Icon/Illustrations';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {canUseTouchScreen as canUseTouchScreenUtil} from '@libs/DeviceCapabilities';
+import getPlatform from '@libs/getPlatform';
 import variables from '@styles/variables';
+import CONST from '@src/CONST';
 import type AttachmentCarouselViewProps from './types';
 
 function AttachmentCarouselView({
@@ -31,6 +33,7 @@ function AttachmentCarouselView({
     const canUseTouchScreen = canUseTouchScreenUtil();
     const styles = useThemeStyles();
     const [activeAttachmentID, setActiveAttachmentID] = useState<AttachmentSource>(attachmentID ?? source);
+    const isIOSNative = getPlatform() === CONST.PLATFORM.IOS;
 
     const pagerRef = useRef<AttachmentCarouselPagerHandle>(null);
 
@@ -74,7 +77,7 @@ function AttachmentCarouselView({
 
     return (
         <View
-            style={[styles.flex1, styles.attachmentCarouselContainer]}
+            style={[styles.flex1, styles.attachmentCarouselContainer, isIOSNative && styles.mhn2]}
             onMouseEnter={() => !canUseTouchScreen && setShouldShowArrows(true)}
             onMouseLeave={() => !canUseTouchScreen && setShouldShowArrows(false)}
         >
