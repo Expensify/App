a99bd4e9e305e29985cc02bc8ddcddd1ab56fd60
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-06-25 15:49:27 +0530

    Fix warnings

--- src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersTaxRatePage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersTaxRatePage.tsx
@@ -9,7 +9,7 @@ import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
 import {getAllTaxRates} from '@libs/PolicyUtils';
-import * as SearchActions from '@userActions/Search';
+import {updateAdvancedFilters} from '@userActions/Search';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 
@@ -17,7 +17,7 @@ function SearchFiltersTaxRatePage() {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
 
-    const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM);
+    const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM, {canBeMissing: true});
     const allTaxRates = getAllTaxRates();
     const selectedTaxesItems: SearchMultipleSelectionPickerItem[] = [];
     Object.entries(allTaxRates).forEach(([taxRateName, taxRateKeys]) => {
@@ -31,6 +31,7 @@ function SearchFiltersTaxRatePage() {
     const policyIDs = searchAdvancedFiltersForm?.policyID ?? [];
     const [policies] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}`, {
         selector: (allPolicies) => allPolicies && Object.values(allPolicies).filter((policy) => policy && policyIDs.includes(policy.id)),
+        canBeMissing: true,
     });
     const selectedPoliciesTaxRates = policies?.map((policy) => policy?.taxRates?.taxes).filter((taxRates) => !!taxRates);
 
@@ -41,7 +42,7 @@ function SearchFiltersTaxRatePage() {
         return selectedPoliciesTaxRates.map((taxRates) => Object.entries(taxRates).map(([taxRateKey, taxRate]) => ({name: taxRate.name, value: [taxRateKey]}))).flat();
     }, [allTaxRates, selectedPoliciesTaxRates]);
 
-    const updateTaxRateFilters = useCallback((values: string[]) => SearchActions.updateAdvancedFilters({taxRate: values}), []);
+    const updateTaxRateFilters = useCallback((values: string[]) => updateAdvancedFilters({taxRate: values}), []);
 
     return (
         <ScreenWrapper
