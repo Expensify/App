5196a54ccd49f6aa7e01e8a1fd32f0857800e4a1
Linh <vovanlinh29546@gmail.com>
2025-06-26 13:57:58 +0700

    fix: remove hasError prop

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -568,7 +568,6 @@ function MoneyRequestView({report, shouldShowAnimatedBackground, readonly = fals
                         style={styles.mv3}
                     >
                         <ReceiptEmptyState
-                            hasError={hasErrors}
                             disabled={!canEditReceipt}
                             onPress={() => {
                                 if (!transaction?.transactionID || !report?.reportID) {
