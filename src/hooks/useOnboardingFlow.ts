6b1848fa0fbb37e86eea40826314abcc4afd40d4
Neil Marcellini <neil@expensify.com>
2025-06-26 13:23:40 -0700

    Revert "Remove the onboarding flow"

--- src/hooks/useOnboardingFlow.ts
+++ src/hooks/useOnboardingFlow.ts
@@ -60,6 +60,40 @@ function useOnboardingFlowRouter() {
                 return;
             }
 
+            const isOnboardingCompleted = hasCompletedGuidedSetupFlowSelector(onboardingValues);
+
+            if (CONFIG.IS_HYBRID_APP) {
+                // For single entries, such as using the Travel feature from OldDot, we don't want to show onboarding
+                if (isSingleNewDotEntry) {
+                    return;
+                }
+
+                // When user is transitioning from OldDot to NewDot, we usually show the explanation modal
+                if (isHybridAppOnboardingCompleted === false) {
+                    Navigation.navigate(ROUTES.EXPLANATION_MODAL_ROOT);
+                }
+
+                // But if the hybrid app onboarding is completed, but NewDot onboarding is not completed, we start NewDot onboarding flow
+                // This is a special case when user created an account from NewDot without finishing the onboarding flow and then logged in from OldDot
+                if (isHybridAppOnboardingCompleted === true && isOnboardingCompleted === false && !startedOnboardingFlowRef.current) {
+                    startedOnboardingFlowRef.current = true;
+                    startOnboardingFlow({
+                        onboardingValuesParam: onboardingValues,
+                        isUserFromPublicDomain: !!account?.isFromPublicDomain,
+                        hasAccessiblePolicies: !!account?.hasAccessibleDomainPolicies,
+                    });
+                }
+            }
+
+            // If the user is not transitioning from OldDot to NewDot, we should start NewDot onboarding flow if it's not completed yet
+            if (!CONFIG.IS_HYBRID_APP && isOnboardingCompleted === false && !startedOnboardingFlowRef.current) {
+                startedOnboardingFlowRef.current = true;
+                startOnboardingFlow({
+                    onboardingValuesParam: onboardingValues,
+                    isUserFromPublicDomain: !!account?.isFromPublicDomain,
+                    hasAccessiblePolicies: !!account?.hasAccessibleDomainPolicies,
+                });
+            }
         });
     }, [
         isLoadingApp,
@@ -77,8 +111,7 @@ function useOnboardingFlowRouter() {
         account?.hasAccessibleDomainPolicies,
     ]);
 
-    // Always return that onboarding is completed to skip any onboarding-related UI
-    return {isOnboardingCompleted: true, isHybridAppOnboardingCompleted};
+    return {isOnboardingCompleted: hasCompletedGuidedSetupFlowSelector(onboardingValues), isHybridAppOnboardingCompleted};
 }
 
 export default useOnboardingFlowRouter;
