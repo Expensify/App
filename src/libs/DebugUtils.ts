61fe4f8a8aba7b6d744ec57216926937baf624d5
DylanDylann <dylandylan9112@gmail.com>
2025-06-26 13:54:48 +0700

    lint fix

--- src/libs/DebugUtils.ts
+++ src/libs/DebugUtils.ts
@@ -1110,6 +1110,7 @@ function validateTransactionDraftProperty(key: keyof Transaction, value: string)
                 splits: 'array',
                 dismissedViolations: 'object',
                 splitExpenses: 'array',
+                isDemoTransaction: 'boolean',
             });
         case 'accountant':
             return validateObject<ObjectElement<Transaction, 'accountant'>>(value, {
