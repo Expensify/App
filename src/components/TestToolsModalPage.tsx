788c8c7dde9d667b4828725f9a677a59d5b3f30a
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2025-06-26 16:19:33 +0800

    fix: console navigation issue

--- src/components/TestToolsModalPage.tsx
+++ src/components/TestToolsModalPage.tsx
@@ -1,3 +1,4 @@
+import {useRoute} from '@react-navigation/native';
 import React from 'react';
 import {View} from 'react-native';
 import useIsAuthenticated from '@hooks/useIsAuthenticated';
@@ -5,9 +6,12 @@ import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import useWindowDimensions from '@hooks/useWindowDimensions';
+import type {PlatformStackRouteProp} from '@libs/Navigation/PlatformStackNavigation/types';
 import Navigation from '@navigation/Navigation';
+import type {TestToolsModalModalNavigatorParamList} from '@navigation/types';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
+import type SCREENS from '@src/SCREENS';
 import Button from './Button';
 import PressableWithoutFeedback from './Pressable/PressableWithoutFeedback';
 import RecordTroubleshootDataToolMenu from './RecordTroubleshootDataToolMenu';
@@ -17,7 +21,7 @@ import TestToolMenu from './TestToolMenu';
 import TestToolRow from './TestToolRow';
 import Text from './Text';
 
-function getRouteBasedOnAuthStatus(isAuthenticated: boolean, activeRoute: string) {
+function getRouteBasedOnAuthStatus(isAuthenticated: boolean, activeRoute?: string) {
     return isAuthenticated ? ROUTES.SETTINGS_CONSOLE.getRoute(activeRoute) : ROUTES.PUBLIC_CONSOLE_DEBUG.getRoute(activeRoute);
 }
 
@@ -25,10 +29,11 @@ function TestToolsModalPage() {
     const {windowHeight} = useWindowDimensions();
     const styles = useThemeStyles();
     const {translate} = useLocalize();
-    const activeRoute = Navigation.getActiveRoute();
+    const route = useRoute<PlatformStackRouteProp<TestToolsModalModalNavigatorParamList, typeof SCREENS.TEST_TOOLS_MODAL.ROOT>>();
+    const backTo = route.params?.backTo;
     const [shouldStoreLogs = false] = useOnyx(ONYXKEYS.SHOULD_STORE_LOGS, {canBeMissing: true});
     const isAuthenticated = useIsAuthenticated();
-    const route = getRouteBasedOnAuthStatus(isAuthenticated, activeRoute);
+    const consoleRoute = getRouteBasedOnAuthStatus(isAuthenticated, backTo);
 
     const maxHeight = windowHeight;
 
@@ -57,7 +62,7 @@ function TestToolsModalPage() {
                                         small
                                         text={translate('initialSettingsPage.debugConsole.viewConsole')}
                                         onPress={() => {
-                                            Navigation.navigate(route);
+                                            Navigation.goBack(consoleRoute);
                                         }}
                                     />
                                 </TestToolRow>
