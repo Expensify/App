6a427bb9848ca9db39f178a37e8d16beb1857c60
Fábio Henriques <contact@fabiohenriques.dev>
2025-06-27 17:39:33 +0100

    Migrate more files to useOnyx wrapper

--- src/components/Search/SearchDatePresetFilterBase.tsx
+++ src/components/Search/SearchDatePresetFilterBase.tsx
@@ -1,5 +1,4 @@
 import React, {useCallback, useMemo, useState} from 'react';
-import {useOnyx} from 'react-native-onyx';
 import Button from '@components/Button';
 import CalendarPicker from '@components/DatePicker/CalendarPicker';
 import FormAlertWithSubmitButton from '@components/FormAlertWithSubmitButton';
@@ -9,6 +8,7 @@ import ScreenWrapper from '@components/ScreenWrapper';
 import ScrollView from '@components/ScrollView';
 import SingleSelectListItem from '@components/SelectionList/SingleSelectListItem';
 import useLocalize from '@hooks/useLocalize';
+import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {updateAdvancedFilters} from '@libs/actions/Search';
 import Navigation from '@libs/Navigation/Navigation';
