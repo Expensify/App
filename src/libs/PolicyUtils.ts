09cb5792da56acf914f1b2ed6bb3589c25a1f5f4
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-06-29 10:37:40 +0530

    Fixed per-diem workspace selection

--- src/libs/PolicyUtils.ts
+++ src/libs/PolicyUtils.ts
@@ -88,9 +88,26 @@ function getActivePolicies(policies: OnyxCollection<Policy> | null, currentUserL
     );
 }
 
+/**
+ * Filter out the active policies, which will exclude policies with pending deletion
+ * and policies the current user doesn't belong to.
+ * These are policies that we can use to create reports with in NewDot.
+ */
+function getActivePoliciesWithExpenseChat(policies: OnyxCollection<Policy> | null, currentUserLogin: string | undefined): Policy[] {
+    return Object.values(policies ?? {}).filter<Policy>(
+        (policy): policy is Policy =>
+            !!policy &&
+            policy.pendingAction !== CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE &&
+            !!policy.name &&
+            !!policy.id &&
+            !!getPolicyRole(policy, currentUserLogin) &&
+            policy.isPolicyExpenseChatEnabled,
+    );
+}
+
 function getPerDiemCustomUnits(policies: OnyxCollection<Policy> | null, email: string | undefined): Array<{policyID: string; customUnit: CustomUnit}> {
     return (
-        getActivePolicies(policies, email)
+        getActivePoliciesWithExpenseChat(policies, email)
             .map((mappedPolicy) => ({policyID: mappedPolicy.id, customUnit: getPerDiemCustomUnit(mappedPolicy)}))
             // We filter out custom units that are undefine but ts cant' figure it out.
             .filter(({customUnit}) => !isEmptyObject(customUnit) && !!customUnit.enabled) as Array<{policyID: string; customUnit: CustomUnit}>
@@ -752,7 +769,7 @@ function canSendInvoiceFromWorkspace(policyID: string | undefined): boolean {
 /** Whether the user can submit per diem expense from the workspace */
 function canSubmitPerDiemExpenseFromWorkspace(policy: OnyxEntry<Policy>): boolean {
     const perDiemCustomUnit = getPerDiemCustomUnit(policy);
-    return !isEmptyObject(perDiemCustomUnit) && !!perDiemCustomUnit?.enabled;
+    return !!policy?.isPolicyExpenseChatEnabled && !isEmptyObject(perDiemCustomUnit) && !!perDiemCustomUnit?.enabled;
 }
 
 /** Whether the user can send invoice */
