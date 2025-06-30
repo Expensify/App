8c8095e732b2e1110126a014d2cbcce976e18559
Puneet Lath <puneet@expensify.com>
2025-06-30 14:43:01 -0400

    Merge pull request #65200 from Expensify/revert-62340-feature/download-option
    Revert "Feat:  Updated the "Download" option to be a push row in both the bulk actions menu and in the More secondary action menu"
    
    (cherry picked from commit 712c6a2a8812f337a6b586bb7df06c07b20e616d)
    
    (cherry-picked to staging by puneetlath)

--- src/languages/zh-hans.ts
+++ src/languages/zh-hans.ts
@@ -549,7 +549,6 @@ const translations = {
         userID: '用户 ID',
         disable: '禁用',
         export: '导出',
-        basicExport: '基本导出',
         initialValue: '初始值',
         currentDate: '当前日期',
         value: '值',
