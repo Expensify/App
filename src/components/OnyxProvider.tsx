7ca886eb5c32aab6a64afb611d8777483135d9c9
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-07-01 11:10:50 +0200

    create hook for getting single item data

--- src/components/OnyxProvider.tsx
+++ src/components/OnyxProvider.tsx
@@ -13,7 +13,7 @@ const [PreferredThemeProvider, PreferredThemeContext] = createOnyxContext(ONYXKE
 const [FrequentlyUsedEmojisProvider, , useFrequentlyUsedEmojis] = createOnyxContext(ONYXKEYS.FREQUENTLY_USED_EMOJIS);
 const [PreferredEmojiSkinToneProvider, PreferredEmojiSkinToneContext] = createOnyxContext(ONYXKEYS.PREFERRED_EMOJI_SKIN_TONE);
 const [SessionProvider, , useSession] = createOnyxContext(ONYXKEYS.SESSION);
-const [ReportTransactionsAndViolationsProvider, , useReportTransactionsAndViolations] = createOnyxContext(ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS);
+const [ReportTransactionsAndViolationsProvider, , useAllReportsTransactionsAndViolations] = createOnyxContext(ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS);
 
 type OnyxProviderProps = {
     /** Rendered child component */
@@ -54,5 +54,5 @@ export {
     PreferredEmojiSkinToneContext,
     useBlockedFromConcierge,
     useSession,
-    useReportTransactionsAndViolations,
+    useAllReportsTransactionsAndViolations,
 };
