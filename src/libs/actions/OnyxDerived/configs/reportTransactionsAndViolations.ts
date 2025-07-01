04a9012e54812d2aac463bb9e82703110506cae8
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-07-01 11:14:12 +0200

    atomic updates on derived value

--- src/libs/actions/OnyxDerived/configs/reportTransactionsAndViolations.ts
+++ src/libs/actions/OnyxDerived/configs/reportTransactionsAndViolations.ts
@@ -5,12 +5,24 @@ import type {ReportTransactionsAndViolationsDerivedValue} from '@src/types/onyx'
 export default createOnyxDerivedValueConfig({
     key: ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS,
     dependencies: [ONYXKEYS.COLLECTION.TRANSACTION, ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS],
-    compute: ([transactions, violations]) => {
+    compute: ([transactions, violations], {sourceValues, currentValue}) => {
         if (!transactions) {
             return {};
         }
 
-        return Object.entries(transactions).reduce<ReportTransactionsAndViolationsDerivedValue>((acc, [transactionKey, transaction]) => {
+        const transactionsUpdates = sourceValues?.[ONYXKEYS.COLLECTION.TRANSACTION];
+        const transactionViolationsUpdates = sourceValues?.[ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS];
+        let transactionsToProcess = Object.keys(transactions);
+        if (transactionsUpdates) {
+            transactionsToProcess = Object.keys(transactionsUpdates);
+        } else if (transactionViolationsUpdates) {
+            transactionsToProcess = Object.keys(transactionViolationsUpdates).map((transactionViolation) =>
+                transactionViolation.replace(ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS, ONYXKEYS.COLLECTION.TRANSACTION),
+            );
+        }
+
+        return transactionsToProcess.reduce<ReportTransactionsAndViolationsDerivedValue>((acc, transactionKey) => {
+            const transaction = transactions[transactionKey];
             const reportID = transaction?.reportID;
             if (!reportID) {
                 return acc;
@@ -24,18 +36,14 @@ export default createOnyxDerivedValueConfig({
             }
             const transactionID = transaction.transactionID;
             const transactionViolations = violations?.[`${ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS}${transactionID}`];
+
             if (transactionViolations && transactionViolations.length > 0) {
-                acc[reportID].violations = {
-                    ...acc[reportID].violations,
-                    [`${ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS}${transactionID}`]: transactionViolations,
-                };
+                acc[reportID].violations[`${ONYXKEYS.COLLECTION.TRANSACTION_VIOLATIONS}${transactionID}`] = transactionViolations;
             }
-            acc[reportID].transactions = {
-                ...acc[reportID].transactions,
-                [transactionKey]: transaction,
-            };
+
+            acc[reportID].transactions[transactionKey] = transaction;
 
             return acc;
-        }, {});
+        }, currentValue ?? {});
     },
 });
