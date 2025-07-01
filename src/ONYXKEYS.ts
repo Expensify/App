7e6c8780780286a35b063be4dc7da58515fd2ae6
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2025-07-01 10:25:44 +0200

    rename derived value

--- src/ONYXKEYS.ts
+++ src/ONYXKEYS.ts
@@ -849,7 +849,7 @@ const ONYXKEYS = {
     },
     DERIVED: {
         REPORT_ATTRIBUTES: 'reportAttributes',
-        REPORT_TRANSACTIONS: 'reportTransactions',
+        REPORT_TRANSACTIONS_AND_VIOLATIONS: 'reportTransactionsAndViolations',
     },
 } as const;
 
@@ -1203,7 +1203,7 @@ type OnyxValuesMapping = {
 
 type OnyxDerivedValuesMapping = {
     [ONYXKEYS.DERIVED.REPORT_ATTRIBUTES]: OnyxTypes.ReportAttributesDerivedValue;
-    [ONYXKEYS.DERIVED.REPORT_TRANSACTIONS]: OnyxTypes.ReportTransactionsDerivedValue;
+    [ONYXKEYS.DERIVED.REPORT_TRANSACTIONS_AND_VIOLATIONS]: OnyxTypes.ReportTransactionsAndViolationsDerivedValue;
 };
 
 type OnyxValues = OnyxValuesMapping & OnyxCollectionValuesMapping & OnyxFormValuesMapping & OnyxFormDraftValuesMapping & OnyxDerivedValuesMapping;
