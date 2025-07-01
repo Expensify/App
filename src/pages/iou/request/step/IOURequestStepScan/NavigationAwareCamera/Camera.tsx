93960b85f7eab0c797a9d1d8abd29dbc32875649
war-in <war-in@users.noreply.github.com>
2025-07-01 13:41:30 +0200

    fix prettier

--- src/pages/iou/request/step/IOURequestStepScan/NavigationAwareCamera/Camera.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/NavigationAwareCamera/Camera.tsx
@@ -1,12 +1,12 @@
+import {useIsFocused} from '@react-navigation/native';
 import React from 'react';
 import type {ForwardedRef} from 'react';
 import {Camera as VisionCamera} from 'react-native-vision-camera';
-import {useIsFocused} from '@react-navigation/native';
 import type {NavigationAwareCameraNativeProps} from './types';
 
 // Wraps a camera that will only be active when the tab is focused or as soon as it starts to become focused.
 function Camera({cameraTabIndex, ...props}: NavigationAwareCameraNativeProps, ref: ForwardedRef<VisionCamera>) {
-    const isCameraActive = useIsFocused()
+    const isCameraActive = useIsFocused();
 
     return (
         <VisionCamera
