f90e4dfb673ae4a5d470cdbd90b681582bcd2aad
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-07-01 12:45:30 +0700

    lint

--- src/libs/ReportActionComposeFocusManager.ts
+++ src/libs/ReportActionComposeFocusManager.ts
@@ -1,6 +1,6 @@
 import {findFocusedRoute} from '@react-navigation/native';
 import React from 'react';
-import type {MutableRefObject} from 'react';
+import type {RefObject} from 'react';
 import type {TextInput} from 'react-native';
 import SCREENS from '@src/SCREENS';
 import isReportOpenInRHP from './Navigation/helpers/isReportOpenInRHP';
@@ -8,11 +8,11 @@ import navigationRef from './Navigation/navigationRef';
 
 type FocusCallback = (shouldFocusForNonBlurInputOnTapOutside?: boolean) => void;
 
-const composerRef: MutableRefObject<TextInput | null> = React.createRef<TextInput>();
+const composerRef: RefObject<TextInput | null> = React.createRef<TextInput>();
 
 // There are two types of composer: general composer (edit composer) and main composer.
 // The general composer callback will take priority if it exists.
-const editComposerRef: MutableRefObject<TextInput | null> = React.createRef<TextInput>();
+const editComposerRef: RefObject<TextInput | null> = React.createRef<TextInput>();
 // There are two types of focus callbacks: priority and general
 // Priority callback would take priority if it existed
 let priorityFocusCallback: FocusCallback | null = null;
