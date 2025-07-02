a902e62afc1451b2949680ed9768290995c8d497
Jasper Huang <jasper@expensify.com>
2025-07-02 11:31:41 -0700

    Revert "Merge pull request #64056 from software-mansion-labs/war-in/unify-hybrid-and-standalone-deeplinks"
    This reverts commit 5a66345bb2d02695a899a8a173b65683bf20c1e7, reversing
    changes made to a8246a843d5f4347bbc6e1e4e39ddc51fe44707d.

--- src/setup/hybridApp/index.native.ts
+++ src/setup/hybridApp/index.native.ts
@@ -1,12 +1,28 @@
+import {findFocusedRoute} from '@react-navigation/native';
 import {Linking} from 'react-native';
-import Navigation from '@navigation/Navigation';
-import {openReportFromDeepLink} from '@userActions/Report';
+import Navigation, {navigationRef} from '@navigation/Navigation';
 import CONFIG from '@src/CONFIG';
 import type {Route} from '@src/ROUTES';
+import ROUTES from '@src/ROUTES';
+import SCREENS from '@src/SCREENS';
 
 if (CONFIG.IS_HYBRID_APP) {
     Linking.addEventListener('url', (state) => {
-        const parsedUrl = Navigation.parseHybridAppUrl(state.url as Route);
-        openReportFromDeepLink(parsedUrl);
+        handleHybridUrlNavigation(state.url as Route);
+    });
+}
+
+function handleHybridUrlNavigation(url: Route) {
+    const parsedUrl = Navigation.parseHybridAppUrl(url);
+
+    Navigation.isNavigationReady().then(() => {
+        if (parsedUrl.startsWith(`/${ROUTES.SHARE_ROOT}`)) {
+            const focusRoute = findFocusedRoute(navigationRef.getRootState());
+            if (focusRoute?.name === SCREENS.SHARE.SHARE_DETAILS || focusRoute?.name === SCREENS.SHARE.SUBMIT_DETAILS) {
+                Navigation.goBack(ROUTES.SHARE_ROOT);
+                return;
+            }
+        }
+        Navigation.navigate(parsedUrl);
     });
 }
