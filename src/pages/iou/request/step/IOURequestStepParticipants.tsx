2e742ff716e10e48768aeb57e8560cdf1d02e0b6
Yauheni Horbach <HorbachZhenja@yandex.by>
2025-07-02 17:49:10 +0200

    Fix prettier issue

--- src/pages/iou/request/step/IOURequestStepParticipants.tsx
+++ src/pages/iou/request/step/IOURequestStepParticipants.tsx
@@ -199,7 +199,7 @@ function IOURequestStepParticipants({
             }
 
             // When a participant is selected, the reportID needs to be saved because that's the reportID that will be used in the confirmation step.
-            // We use || to be sure that if the first participant doesn't have a reportID, we generate a new one. 
+            // We use || to be sure that if the first participant doesn't have a reportID, we generate a new one.
             // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
             selectedReportID.current = firstParticipantReportID || generateReportID();
         },
