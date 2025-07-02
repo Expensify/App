def8188dc8b6754059394867fb091c0d01a603a7
Chirag Chandrakant Salian <chirag@expensify.com>
2025-07-02 11:32:17 -0700

    Revert "Bring back new sign-in page"

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -6723,12 +6723,6 @@ const CONST = {
         HIDDEN: `hidden`,
     },
 
-    HYBRID_APP_SIGN_IN_STATE: {
-        NOT_STARTED: 'notStarted',
-        STARTED: 'started',
-        FINISHED: 'finished',
-    },
-
     CSV_IMPORT_COLUMNS: {
         EMAIL: 'email',
         NAME: 'name',
