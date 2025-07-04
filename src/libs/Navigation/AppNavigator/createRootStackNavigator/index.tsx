11c3fb9662403e3f9013f232fc41363d04e71271
Wojciech Boman <wojciechboman@gmail.com>
2025-07-04 12:23:11 +0200

    Render only last fullscreen navigator in RootStackNavigator

--- src/libs/Navigation/AppNavigator/createRootStackNavigator/index.tsx
+++ src/libs/Navigation/AppNavigator/createRootStackNavigator/index.tsx
@@ -11,7 +11,7 @@ import RootStackRouter from './RootStackRouter';
 // This is an optimization to keep mounted only last few screens in the stack.
 function useCustomRootStackNavigatorState({state}: CustomStateHookProps) {
     const lastSplitIndex = state.routes.findLastIndex((route) => isFullScreenName(route.name));
-    const routesToRender = state.routes.slice(Math.max(0, lastSplitIndex - 1), state.routes.length);
+    const routesToRender = state.routes.slice(Math.max(0, lastSplitIndex), state.routes.length);
 
     return {...state, routes: routesToRender, index: routesToRender.length - 1};
 }
