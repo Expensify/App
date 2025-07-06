eede9a3ae4505befec073451736813b83a2fac09
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-07-06 19:08:10 +0100

    update route

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -1767,8 +1767,8 @@ const ROUTES = {
         getRoute: (policyID: string) => `workspaces/${policyID}/company-cards/settings/feed-name` as const,
     },
     WORKSPACE_COMPANY_CARDS_SETTINGS_STATEMENT_CLOSE_DATE: {
-        route: 'settings/workspaces/:policyID/company-cards/settings/statement-close-date',
-        getRoute: (policyID: string) => `settings/workspaces/${policyID}/company-cards/settings/statement-close-date` as const,
+        route: 'workspaces/:policyID/company-cards/settings/statement-close-date',
+        getRoute: (policyID: string) => `workspaces/${policyID}/company-cards/settings/statement-close-date` as const,
     },
     WORKSPACE_RULES: {
         route: 'workspaces/:policyID/rules',
