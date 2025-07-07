e11f79d9c5e84918f86f30242351a155ba7bc7f4
Michał Muzyk <mimuzyk@gmail.com>
2025-07-07 11:16:16 +0200

    cr fixes

--- src/pages/ReimbursementAccount/NonUSD/utils/requiresDocusignStep.ts
+++ src/pages/ReimbursementAccount/NonUSD/utils/requiresDocusignStep.ts
@@ -7,15 +7,9 @@ import CONST from '@src/CONST';
 function requiresDocusignStep(currency: string | undefined): boolean {
     switch (currency) {
         case CONST.CURRENCY.USD:
-            return true;
         case CONST.CURRENCY.CAD:
-            return true;
         case CONST.CURRENCY.AUD:
             return true;
-        case CONST.CURRENCY.GBP:
-            return false;
-        case CONST.CURRENCY.EUR:
-            return false;
         default:
             return false;
     }
