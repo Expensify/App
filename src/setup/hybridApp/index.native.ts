ab1291e57fa18b9cb0f995af2fcfabdbdc88bc4a
Mateusz Rajski <mateusz.rajski@swmansion.com>
2025-07-07 15:33:45 +0200

    Update comments

--- src/setup/hybridApp/index.native.ts
+++ src/setup/hybridApp/index.native.ts
@@ -6,6 +6,8 @@ import CONST from '@src/CONST';
 import type {HybridAppRoute, Route} from '@src/ROUTES';
 import ROUTES, {HYBRID_APP_ROUTES} from '@src/ROUTES';
 
+
+// In HybridApp, URLs might come from OldDot in an unsupported format, so we transform them into a format that is supported by NewDot.
 function parseHybridAppUrl(url: HybridAppRoute | Route): Route {
     switch (url) {
         case HYBRID_APP_ROUTES.MONEY_REQUEST_CREATE_TAB_MANUAL:
@@ -20,22 +22,21 @@ function parseHybridAppUrl(url: HybridAppRoute | Route): Route {
     }
 }
 
-// On HybridApp we need to shadow official implementation of Linking.getInitialURL on NewDot side with our custom implementation.
-// Main benefit from this approach is that our deeplink-related code can be implemented the same way for both standalone NewDot and HybridApp.
 if(CONFIG.IS_HYBRID_APP) {
+    // On HybridApp we need to shadow official implementation of Linking.getInitialURL on NewDot side with our custom implementation.
+    // Main benefit from this approach is that our deeplink-related code can be implemented the same way for both standalone NewDot and HybridApp.
+    // It's not possible to use the official implementation from the Linking module because the way OldDot handles deeplinks is significantly different from a standard React Native app.
     Linking.getInitialURL = () => HybridAppModule.getInitialURL().then((url) => parseHybridAppUrl(url as HybridAppRoute | Route));
 
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const originalAddEventListener = Linking.addEventListener;
     
     Linking.addEventListener = (type: 'url', handler: (event: {url: string}) => void) => {
-        // For hybrid app, we need to transform the URL through parseHybridAppUrl before calling the handler
         const handlerWithParsedHybridAppUrl = (event: {url: string}) => {
             const transformedUrl = parseHybridAppUrl(event.url as HybridAppRoute | Route);
             handler({url: transformedUrl});
         };
         
-        // Call the original addEventListener with our transformed handler
         return originalAddEventListener.call(Linking, type, handlerWithParsedHybridAppUrl);
     };
 }
