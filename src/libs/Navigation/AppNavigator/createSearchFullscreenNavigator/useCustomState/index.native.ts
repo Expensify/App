5fe71a5240abafb1127110c99e14c7715fe5d0df
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2025-07-08 10:18:20 +0200

    add usage remarks

--- src/libs/Navigation/AppNavigator/createSearchFullscreenNavigator/useCustomState/index.native.ts
+++ src/libs/Navigation/AppNavigator/createSearchFullscreenNavigator/useCustomState/index.native.ts
@@ -2,8 +2,9 @@ import type {CustomStateHookProps} from '@libs/Navigation/PlatformStackNavigatio
 import SCREENS from '@src/SCREENS';
 
 /**
- * This is a custom state hook that is used to render the last two search routes in the stack.
+ * This is a custom state hook for SearchFullscreenNavigator that is used to render the last two search routes in the stack.
  * For native platforms, we render the last two routes to allow users to return by swiping left.
+ * @see SearchFullscreenNavigator use only!
  */
 export default function useCustomState({state}: CustomStateHookProps) {
     const lastSearchNavigatorIndex = state.routes.findLastIndex((route) => route.name === SCREENS.SEARCH.ROOT);
