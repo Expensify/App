2b7d724f47df7af63218ffa7f1f19c47f51f6705
thelullabyy <the.lullaby2@hotmail.com>
2025-07-08 18:21:41 +0800

    fix: onboarding issue

--- src/pages/OnboardingWorkEmail/BaseOnboardingWorkEmail.tsx
+++ src/pages/OnboardingWorkEmail/BaseOnboardingWorkEmail.tsx
@@ -71,10 +71,9 @@ function BaseOnboardingWorkEmail({shouldUseNativeStyles}: BaseOnboardingWorkEmai
             Navigation.navigate(ROUTES.ONBOARDING_WORK_EMAIL_VALIDATION.getRoute());
             return;
         }
-        // Once we verify that shouldValidate is false, we need to force replace the screen
-        // so that we don't navigate back on back button press
+
         if (isVsb) {
-            Navigation.navigate(ROUTES.ONBOARDING_ACCOUNTING.getRoute(), {forceReplace: true});
+            Navigation.navigate(ROUTES.ONBOARDING_ACCOUNTING.getRoute());
             return;
         }
 
