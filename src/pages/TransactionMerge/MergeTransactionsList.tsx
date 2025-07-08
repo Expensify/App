a7f98ef9660257757e669ae778f9ec16ae16627b
VH <hoangzinhvn@gmail.com>
2025-07-08 22:45:34 +0700

    Display no eligible expenses view

--- src/pages/TransactionMerge/MergeTransactionsList.tsx
+++ src/pages/TransactionMerge/MergeTransactionsList.tsx
@@ -1,14 +1,18 @@
 import React, {useCallback, useEffect} from 'react';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
+import EmptyStateComponent from '@components/EmptyStateComponent';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import LottieAnimations from '@components/LottieAnimations';
 import ScreenWrapper from '@components/ScreenWrapper';
 import MergeExpensesSkeleton from '@components/Skeletons/MergeExpensesSkeleton';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
+import useThemeStyles from '@hooks/useThemeStyles';
 import {getTransactionsForMerging} from '@libs/actions/Transaction';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {MergeTransactionNavigatorParamList} from '@libs/Navigation/types';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type SCREENS from '@src/SCREENS';
 
@@ -17,6 +21,7 @@ type MergeTransactionsListProps = PlatformStackScreenProps<MergeTransactionNavig
 function MergeTransactionsList({route}: MergeTransactionsListProps) {
     const {translate} = useLocalize();
     const {transactionID, backTo} = route.params;
+    const styles = useThemeStyles();
 
     const [mergeTransaction] = useOnyx(`${ONYXKEYS.COLLECTION.MERGE_TRANSACTION}${transactionID}`, {canBeMissing: false});
 
@@ -59,6 +64,17 @@ function MergeTransactionsList({route}: MergeTransactionsListProps) {
                 <HeaderWithBackButton
                     title={translate('transactionMerge.listPage.header')}
                     onBackButtonPress={goBack}
+                />
+                <EmptyStateComponent
+                    cardStyles={[styles.appBG]}
+                    cardContentStyles={[styles.pt5, styles.pb0]}
+                    headerMediaType={CONST.EMPTY_STATE_MEDIA.ANIMATION}
+                    headerMedia={LottieAnimations.GenericEmptyState}
+                    title={translate('transactionMerge.listPage.noEligibleExpenseFound')}
+                    subtitle={translate('transactionMerge.listPage.noEligibleExpenseFoundSubtitle')}
+                    headerStyles={[styles.emptyStateMoneyRequestReport]}
+                    lottieWebViewStyles={styles.emptyStateFolderWebStyles}
+                    headerContentStyles={styles.emptyStateFolderWebStyles}
                 />
             </ScreenWrapper>
         );
