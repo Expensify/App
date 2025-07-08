aa6b52ef65e566ad078f30bbf7f3392e742ef1b0
dmkt9 <demoskratos9x@gmail.com>
2025-07-08 08:08:13 +0700

    Fix - "New Messages" badge isn't seen if scroll up the conversation and other user send message

--- src/pages/home/report/ReportActionsList.tsx
+++ src/pages/home/report/ReportActionsList.tsx
@@ -412,7 +412,6 @@ function ReportActionsList({
     const scrollToBottomForCurrentUserAction = useCallback(
         (isFromCurrentUser: boolean) => {
             InteractionManager.runAfterInteractions(() => {
-                setIsFloatingMessageCounterVisible(false);
                 // If a new comment is added and it's from the current user scroll to the bottom otherwise leave the user positioned where
                 // they are now in the list.
                 if (!isFromCurrentUser || (!isReportTopmostSplitNavigator() && !Navigation.getReportRHPActiveRoute())) {
@@ -428,6 +427,7 @@ function ReportActionsList({
                     return;
                 }
 
+                setIsFloatingMessageCounterVisible(false);
                 reportScrollManager.scrollToBottom();
                 setIsScrollToBottomEnabled(true);
             });
