17e2cb7e05f0e3c08d2ab2bc6e07ef31e17efcbf
mhawryluk <marcinhawrylukxx@gmail.com>
2025-07-09 13:02:55 +0200

    Change cursor pointer/auto logic in non-custom backdrop

--- src/components/Modal/ReanimatedModal/Backdrop/index.web.tsx
+++ src/components/Modal/ReanimatedModal/Backdrop/index.web.tsx
@@ -1,3 +1,4 @@
+import noop from 'lodash/noop';
 import React, {useMemo} from 'react';
 import {View} from 'react-native';
 import Animated, {Easing, Keyframe} from 'react-native-reanimated';
@@ -59,7 +60,7 @@ function Backdrop({
                 accessible
                 accessibilityLabel={translate('modal.backdropLabel')}
                 onPress={onBackdropPress}
-                style={[styles.userSelectNone, styles.cursorAuto]}
+                style={[styles.userSelectNone, onBackdropPress && onBackdropPress !== noop ? styles.cursorPointer : styles.cursorAuto]}
                 dataSet={{[CONST.SELECTION_SCRAPER_HIDDEN_ELEMENT]: true}}
             >
                 {isBackdropVisible && (
