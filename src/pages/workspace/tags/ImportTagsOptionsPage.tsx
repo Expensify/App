f2f272a9349eedb5bfb175ca4d2cc05dbb0ce735
Yuwen Memon <yuwen@expensify.com>
2025-07-09 12:21:33 -0700

    Fix links for multi tags feature

--- src/pages/workspace/tags/ImportTagsOptionsPage.tsx
+++ src/pages/workspace/tags/ImportTagsOptionsPage.tsx
@@ -174,12 +174,7 @@ function ImportTagsOptionsPage({route}: ImportTagsOptionsPageProps) {
                                     {translate('workspace.tags.switchSingleToMultiLevelTagWarning.prompt3')}
                                 </TextLink>
                                 {translate('workspace.tags.switchSingleToMultiLevelTagWarning.prompt4')}
-                                <TextLink
-                                    onPress={() => {
-                                        // TODO: Add link to tag levels documentation
-                                        return null;
-                                    }}
-                                >
+                                <TextLink href={CONST.IMPORT_SPREADSHEET.TAGS_ARTICLE_LINK}>
                                     {translate('workspace.tags.switchSingleToMultiLevelTagWarning.prompt5')}
                                 </TextLink>
                                 {translate('workspace.tags.switchSingleToMultiLevelTagWarning.prompt6')}
