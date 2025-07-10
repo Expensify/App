360c6cf256d64426e90a8066c2caab6aa58b49ba
William Quayson <wgsquayson@gmail.com>
2025-07-10 16:54:10 -0300

    fix: scroll offset logic

--- src/pages/home/report/ReportFooter.tsx
+++ src/pages/home/report/ReportFooter.tsx
@@ -205,15 +205,14 @@ function ReportFooter({
         setDidHideComposerInput(true);
     }, [shouldShowComposeInput, didHideComposerInput]);
 
-    const animatedStyle = useAnimatedStyle(
-        () => ({
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
             position: 'absolute',
             bottom: 0,
             width: '100%',
-            transform: [{translateY: -keyboardHeight.value}],
-        }),
-        [],
-    );
+            transform: [{translateY: -keyboardHeight.get()}],
+        };
+    });
 
     return (
         <>
@@ -248,7 +247,7 @@ function ReportFooter({
                 </View>
             )}
             {!shouldHideComposer && (!!shouldShowComposeInput || !shouldUseNarrowLayout) && (
-                <Animated.View style={[chatFooterStyles, isComposerFullSize && styles.chatFooterFullCompose, animatedStyle]}>
+                <Animated.View style={[chatFooterStyles, isComposerFullSize && styles.chatFooterFullCompose, !isComposerFullSize && animatedStyle]}>
                     <ReportActionCompose
                         onSubmit={onSubmitComment}
                         onComposerFocus={onComposerFocus}
