32c34ac9b15f0960303465136a7fdff1f505801e
William Quayson <wgsquayson@gmail.com>
2025-07-10 16:54:10 -0300

    fix: scroll offset logic

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -141,56 +141,46 @@ function useKeyboardAnimation() {
     const inset = useSharedValue(0);
     const offset = useSharedValue(0);
     const scrollY = useSharedValue(0);
-    const shouldUseOnMoveHandler = useSharedValue(false);
 
     useKeyboardHandler({
         onStart: (e) => {
             'worklet';
 
-            // i. e. the keyboard was under interactive gesture, and will be showed
-            // again. Since iOS will not schedule layout animation for that we can't
-            // simply update `height` to destination and we need to listen to `onMove`
-            // handler to have a smooth animation
-            if (progress.value !== 1 && progress.value !== 0 && e.height !== 0) {
-                // eslint-disable-next-line react-compiler/react-compiler
-                shouldUseOnMoveHandler.value = true;
-
-                return;
-            }
-
-            progress.value = e.progress;
-            height.value = e.height;
-
-            inset.value = e.height;
-            // Math.max is needed to prevent overscroll when keyboard hides (and user scrolled to the top, for example)
-            offset.value = Math.max(e.height - scrollY.value, 0);
+            progress.set(e.progress);
+            height.set(e.height);
         },
         onInteractive: (e) => {
             'worklet';
 
-            progress.value = e.progress;
-            height.value = e.height;
+            progress.set(e.progress);
+            height.set(e.height);
         },
         onMove: (e) => {
             'worklet';
 
-            if (shouldUseOnMoveHandler.value) {
-                progress.value = e.progress;
-                height.value = e.height;
+            const isAtBottom = scrollY.get() <= 10;
+
+            if (isAtBottom) {
+                offset.set(e.height);
             }
+
+            inset.set(e.height);
+            progress.set(e.progress);
+            height.set(e.height);
         },
         onEnd: (e) => {
             'worklet';
 
-            height.value = e.height;
-            progress.value = e.progress;
-            shouldUseOnMoveHandler.value = false;
+            height.set(e.height);
+            progress.set(e.progress);
         },
     });
 
     const onScroll = useAnimatedScrollHandler({
         onScroll: (e) => {
-            scrollY.set(e.contentOffset.y);
+            'worklet';
+
+            scrollY.set(e.contentOffset.y + inset.get());
         },
     });
 
--- src/pages/home/report/ReportFooter.tsx
+++ src/pages/home/report/ReportFooter.tsx
@@ -193,15 +193,14 @@ function ReportFooter({
         setDidHideComposerInput(true);
     }, [shouldShowComposeInput, didHideComposerInput]);
 
-    const animatedStyle = useAnimatedStyle(
-        () => ({
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
             position: 'absolute',
             bottom: 0,
             width: '100%',
-            transform: [{translateY: -keyboardHeight.value}],
-        }),
-        [],
-    );
+            transform: [{translateY: -keyboardHeight.get()}],
+        };
+    });
 
     return (
         <>
@@ -236,7 +235,7 @@ function ReportFooter({
                 </View>
             )}
             {!shouldHideComposer && (!!shouldShowComposeInput || !shouldUseNarrowLayout) && (
-                <Animated.View style={[chatFooterStyles, isComposerFullSize && styles.chatFooterFullCompose, animatedStyle]}>
+                <Animated.View style={[chatFooterStyles, isComposerFullSize && styles.chatFooterFullCompose, !isComposerFullSize && animatedStyle]}>
                     <ReportActionCompose
                         onSubmit={onSubmitComment}
                         onComposerFocus={onComposerFocus}
