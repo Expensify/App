a3081befcbb837b71fb59f34d46e8f0ac5749a62
ahmed <ahmed@mvst.co>
2025-07-13 15:28:43 +0100

    change navigation to handle firefox issues

--- src/libs/Navigation/Navigation.ts
+++ src/libs/Navigation/Navigation.ts
@@ -561,6 +561,11 @@ function getReportRouteByID(reportID?: string, routes: NavigationRoute[] = navig
 const dismissModal = (ref = navigationRef) => {
     isNavigationReady().then(() => {
         ref.dispatch({type: CONST.NAVIGATION.ACTION_TYPE.DISMISS_MODAL});
+
+        // Let React Navigation finish modal transition
+        InteractionManager.runAfterInteractions(() => {
+            fireModalDismissed();
+        });
     });
 };
 
@@ -646,6 +651,20 @@ function removeScreenByKey(key: string) {
     });
 }
 
+const modalDismissedListeners: Array<() => void> = [];
+
+function onModalDismissedOnce(callback: () => void) {
+    modalDismissedListeners.push(callback);
+}
+
+// Wrap modal dismissal so listeners get called
+function fireModalDismissed() {
+    while (modalDismissedListeners.length) {
+        const cb = modalDismissedListeners.pop();
+        cb?.();
+    }
+}
+
 function isOnboardingFlow() {
     const state = navigationRef.getRootState();
     const currentFocusedRoute = findFocusedRoute(state);
@@ -685,6 +704,8 @@ export default {
     replaceWithSplitNavigator,
     isTopmostRouteModalScreen,
     isOnboardingFlow,
+    onModalDismissedOnce,
+    fireModalDismissed,
 };
 
 export {navigationRef};
