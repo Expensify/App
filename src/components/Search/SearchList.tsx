3257d6bded7c80ba96c5641585fa63a9657041ab
daledah <daleda261@outlook.com>
2025-07-13 22:04:05 +0700

    fix: prevent open report when filter modal is opened

--- src/components/Search/SearchList.tsx
+++ src/components/Search/SearchList.tsx
@@ -224,7 +224,7 @@ function SearchList(
         captureOnInputs: true,
         shouldBubble: false,
         shouldPreventDefault: false,
-        isActive: isFocused,
+        isActive: isFocused && focusedIndex >= 0,
         shouldStopPropagation: true,
     });
 
