80861599f55bcc7232401b1dad856c9aade5fa60
Getabalew <getudesta4@gmail.com>
2025-07-14 22:57:40 +0300

    Revert 'Merge pull request #66059 from getusha/fix-prevent-snapshot-use-in-settlement-button'

--- src/libs/PolicyUtils.ts
+++ src/libs/PolicyUtils.ts
@@ -755,8 +755,8 @@ function getPolicy(policyID: string | undefined, policies: OnyxCollection<Policy
 }
 
 /** Return active policies where current user is an admin */
-function getActiveAdminWorkspaces(policies: OnyxCollection<Policy> | null | undefined, currentUserLogin: string | undefined): Policy[] {
-    const activePolicies = getActivePolicies(policies === undefined ? allPolicies : policies, currentUserLogin);
+function getActiveAdminWorkspaces(policies: OnyxCollection<Policy> | null, currentUserLogin: string | undefined): Policy[] {
+    const activePolicies = getActivePolicies(policies, currentUserLogin);
     return activePolicies.filter((policy) => shouldShowPolicy(policy, isOfflineNetworkStore(), currentUserLogin) && isPolicyAdmin(policy, currentUserLogin));
 }
 
