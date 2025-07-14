242e30075f96640381b7b0819fce6be6b977b778
Wojciech Boman <wojciechboman@gmail.com>
2025-07-14 15:41:35 +0200

    Fix isPrevFullScreen value in useCustomRootStackNavigatorState

--- src/libs/Navigation/AppNavigator/createRootStackNavigator/useCustomRootStackNavigatorState/index.ts
+++ src/libs/Navigation/AppNavigator/createRootStackNavigator/useCustomRootStackNavigatorState/index.ts
@@ -6,7 +6,7 @@ export default function useCustomRootStackNavigatorState({state}: CustomStateHoo
     const lastSplitIndex = state.routes.findLastIndex((route) => isFullScreenName(route.name));
     let indexToSlice = Math.max(0, lastSplitIndex);
     const hasPrevRoute = lastSplitIndex > 0;
-    const isPrevFullScreen = !isFullScreenName(state.routes.at(lastSplitIndex - 1)?.name);
+    const isPrevFullScreen = isFullScreenName(state.routes.at(lastSplitIndex - 1)?.name);
     // If the route before the last fullscreen is e.g. RHP, we should leave it to avoid bugs when going back to this screen.
     // This solves the issue: https://github.com/Expensify/App/pull/65500#issuecomment-3061323128
     if (hasPrevRoute && !isPrevFullScreen) {
