3e9fcaef25cffbbc8c66ba1bf438acdcdcab41c9
Wojciech Boman <wojciechboman@gmail.com>
2025-07-14 12:52:07 +0200

    Adjust useCustomRootStackNavigatorState to store previous RHP route

--- src/libs/Navigation/AppNavigator/createRootStackNavigator/useCustomRootStackNavigatorState/index.ts
+++ src/libs/Navigation/AppNavigator/createRootStackNavigator/useCustomRootStackNavigatorState/index.ts
@@ -4,7 +4,14 @@ import type {CustomStateHookProps} from '@libs/Navigation/PlatformStackNavigatio
 // This is an optimization to keep mounted only last few screens in the stack.
 export default function useCustomRootStackNavigatorState({state}: CustomStateHookProps) {
     const lastSplitIndex = state.routes.findLastIndex((route) => isFullScreenName(route.name));
-    const routesToRender = state.routes.slice(Math.max(0, lastSplitIndex), state.routes.length);
-
+    let indexToSlice = Math.max(0, lastSplitIndex);
+    const hasPrevRoute = lastSplitIndex > 0;
+    const isPrevFullScreen = !isFullScreenName(state.routes.at(lastSplitIndex - 1)?.name);
+    // If the route before the last fullscreen is e.g. RHP, we should leave it to avoid bugs when going back to this screen.
+    // This solves the issue: https://github.com/Expensify/App/pull/65500#issuecomment-3061323128
+    if (hasPrevRoute && !isPrevFullScreen) {
+        indexToSlice = lastSplitIndex - 1;
+    }
+    const routesToRender = state.routes.slice(indexToSlice, state.routes.length);
     return {...state, routes: routesToRender, index: routesToRender.length - 1};
 }
