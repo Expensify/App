c34bce1ee50dc30de6b0ebf1f227adcf072172c9
FitseTLT <engfitsum@gmail.com>
2025-07-15 17:08:29 +0300

    disable auto focus on mobile on 2fa page

--- src/pages/settings/Security/TwoFactorAuth/TwoFactorAuthForm/types.ts
+++ src/pages/settings/Security/TwoFactorAuth/TwoFactorAuthForm/types.ts
@@ -7,6 +7,7 @@ type BaseTwoFactorAuthFormRef = {
 
 type TwoFactorAuthFormProps = {
     innerRef: ForwardedRef<BaseTwoFactorAuthFormRef>;
+    shouldAutoFocusOnMobile?: boolean;
 
     // Set this to true in order to call the validateTwoFactorAuth action which is used when setting up 2FA for the first time.
     // Set this to false in order to disable 2FA when a valid code is entered.
