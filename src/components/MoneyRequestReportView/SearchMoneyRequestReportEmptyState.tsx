69bdb0f8d40df9d6121a9268a6d04f84efaa3fd3
sumo_slonik <jakub.nowakowski@swmansion.com>
2025-07-15 10:43:53 +0200

    check if report is archived

--- src/components/MoneyRequestReportView/SearchMoneyRequestReportEmptyState.tsx
+++ src/components/MoneyRequestReportView/SearchMoneyRequestReportEmptyState.tsx
@@ -4,23 +4,27 @@ import EmptyStateComponent from '@components/EmptyStateComponent';
 import * as Expensicons from '@components/Icon/Expensicons';
 import LottieAnimations from '@components/LottieAnimations';
 import useLocalize from '@hooks/useLocalize';
+import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {canAddTransaction} from '@libs/ReportUtils';
+import {canAddTransaction, isArchivedReport} from '@libs/ReportUtils';
 import {shouldRestrictUserBillableActions} from '@libs/SubscriptionUtils';
 import Navigation from '@navigation/Navigation';
 import {startMoneyRequest} from '@userActions/IOU';
 import {openUnreportedExpense} from '@userActions/Report';
 import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type * as OnyxTypes from '@src/types/onyx';
 
 const minModalHeight = 380;
 
 function SearchMoneyRequestReportEmptyState({report, policy}: {report: OnyxTypes.Report; policy?: OnyxTypes.Policy}) {
+    const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${report.reportID}`, {canBeMissing: true});
     const {translate} = useLocalize();
     const styles = useThemeStyles();
     const reportId = report.reportID;
-    const canAddTransactionToReport = canAddTransaction(report);
+    const isReportArchived = isArchivedReport(reportNameValuePairs);
+    const canAddTransactionToReport = canAddTransaction(report, isReportArchived);
     const addExpenseDropdownOptions = [
         {
             value: CONST.REPORT.ADD_EXPENSE_OPTIONS.CREATE_NEW_EXPENSE,
