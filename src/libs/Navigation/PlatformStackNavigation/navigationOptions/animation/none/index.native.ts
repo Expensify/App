9fdf21bc9dbcbea40725f118aeebbe080837b246
Christoph Pader <chris@margelo.com>
2025-07-15 12:01:25 +0700

    Revert "refactor: improve none animation in platform stack navigation"
    This reverts commit 51c207d8ba7775b0bcb6e897d3199f1eb1a06ecc.

--- src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/none/index.native.ts
+++ src/libs/Navigation/PlatformStackNavigation/navigationOptions/animation/none/index.native.ts
@@ -1,7 +1,8 @@
 import type {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 import {InternalPlatformAnimations} from '..';
+import type NoneTransitionNavigationOptions from './types';
 
-const none = {
+const none: NoneTransitionNavigationOptions = {
     animation: InternalPlatformAnimations.NONE,
     gestureEnabled: false,
 } satisfies NativeStackNavigationOptions;
