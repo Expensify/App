a9df595239dbd641c3eeeca001efd6a421ae5cc4
Olgierd <olgierd.smyka@swmansion.com>
2025-07-16 12:58:40 +0200

    Remove redundant lines

--- src/components/AddPaymentCard/PaymentCardCurrencyModal.tsx
+++ src/components/AddPaymentCard/PaymentCardCurrencyModal.tsx
@@ -6,9 +6,7 @@ import ScreenWrapper from '@components/ScreenWrapper';
 import SelectionList from '@components/SelectionList';
 import RadioListItem from '@components/SelectionList/RadioListItem';
 import useLocalize from '@hooks/useLocalize';
-import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
-import Navigation from '@libs/Navigation/Navigation';
 import CONST from '@src/CONST';
 
 type PaymentCardCurrencyModalProps = {
@@ -29,7 +27,6 @@ type PaymentCardCurrencyModalProps = {
 };
 
 function PaymentCardCurrencyModal({isVisible, currencies, currentCurrency = CONST.PAYMENT_CARD_CURRENCY.USD, onCurrencyChange, onClose}: PaymentCardCurrencyModalProps) {
-    const {shouldUseNarrowLayout} = useResponsiveLayout();
     const styles = useThemeStyles();
     const {translate} = useLocalize();
     const {sections} = useMemo(
@@ -55,8 +52,6 @@ function PaymentCardCurrencyModal({isVisible, currencies, currentCurrency = CONS
             onClose={() => onClose?.()}
             onModalHide={onClose}
             hideModalContentWhileAnimating
-            innerContainerStyle={styles.RHPNavigatorContainer(shouldUseNarrowLayout)}
-            onBackdropPress={Navigation.dismissModal}
             useNativeDriver
             shouldUseReanimatedModal
         >
