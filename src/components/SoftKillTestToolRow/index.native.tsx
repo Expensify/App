409e5d7fd57fdbae30518589f400d0ffba6bcb4a
Andrew Rosiclair <arosiclair@expensify.com>
2025-07-17 16:20:15 -0400

    use translation

--- src/components/SoftKillTestToolRow/index.native.tsx
+++ src/components/SoftKillTestToolRow/index.native.tsx
@@ -1,13 +1,16 @@
 import {NativeModules} from 'react-native';
 import Button from '@components/Button';
 import TestToolRow from '@components/TestToolRow';
+import useLocalize from '@hooks/useLocalize';
 
 export default function SoftKillTestToolRow() {
+    const {translate} = useLocalize();
+
     return (
-        <TestToolRow title="Soft kill the app">
+        <TestToolRow title={translate('initialSettingsPage.troubleshoot.softKillTheApp')}>
             <Button
                 small
-                text="Kill"
+                text={translate('initialSettingsPage.troubleshoot.kill')}
                 onPress={() => NativeModules.TestToolsBridge.softKillApp()}
             />
         </TestToolRow>
--- src/languages/nl.ts
+++ src/languages/nl.ts
@@ -1525,6 +1525,8 @@ const translations = {
             invalidFileDescription: 'Het bestand dat je probeert te importeren is niet geldig. Probeer het opnieuw.',
             invalidateWithDelay: 'Ongeldig maken met vertraging',
             recordTroubleshootData: 'Probleemoplossingsgegevens opnemen',
+            softKillTheApp: 'Soft kill de app',
+            kill: 'Dood',
         },
         debugConsole: {
             saveLog: 'Log opslaan',
