3b45d03284be22cc5b54d02c3f270a7744067b9f
Jack Senyitko <jsenyitko@expensify.com>
2025-07-17 08:17:32 -0400

    update optimistic. comments

--- src/libs/actions/Search.ts
+++ src/libs/actions/Search.ts
@@ -324,7 +324,7 @@ function submitMoneyRequestOnSearch(hash: number, reportList: SearchReport[], po
 
     const optimisticData: OnyxUpdate[] = createOnyxData({isActionLoading: true});
     const failureData: OnyxUpdate[] = createOnyxData({isActionLoading: false});
-    // If we are on the 'Submit' suggested search, optimistically remove the report from the view once the action is taken
+    // If we are on the 'Submit' suggested search, remove the report from the view once the action is taken, don't wait for the view to be re-fetched via Search
     const successData: OnyxUpdate[] = currentSearchKey === CONST.SEARCH.SUGGESTED_SEARCH_KEYS.SUBMIT ? createOnyxData(null) : createOnyxData({isActionLoading: false});
 
     const report = (reportList.at(0) ?? {}) as SearchReport;
@@ -354,6 +354,7 @@ function approveMoneyRequestOnSearch(hash: number, reportIDList: string[], trans
 
     const optimisticData: OnyxUpdate[] = createOnyxData({isActionLoading: true});
     const failureData: OnyxUpdate[] = createOnyxData({isActionLoading: false, errors: getMicroSecondOnyxErrorWithTranslationKey('common.genericErrorMessage')});
+    // If we are on the 'Approve' suggested search, remove the report from the view once the action is taken, don't wait for the view to be re-fetched via Search
     const successData: OnyxUpdate[] = currentSearchKey === CONST.SEARCH.SUGGESTED_SEARCH_KEYS.APPROVE ? createOnyxData(null) : createOnyxData({isActionLoading: false});
 
     playSound(SOUNDS.SUCCESS);
@@ -385,6 +386,7 @@ function exportToIntegrationOnSearch(hash: number, reportID: string, connectionN
 
     const optimisticData: OnyxUpdate[] = createOnyxData({isActionLoading: true});
     const failureData: OnyxUpdate[] = createOnyxData({errors: getMicroSecondOnyxErrorWithTranslationKey('common.genericErrorMessage')});
+    // If we are on the 'Export' suggested search, remove the report from the view once the action is taken, don't wait for the view to be re-fetched via Search
     const successData: OnyxUpdate[] = currentSearchKey === CONST.SEARCH.SUGGESTED_SEARCH_KEYS.EXPORT ? createOnyxData(null) : createOnyxData({isActionLoading: false});
 
     const params = {
@@ -414,6 +416,7 @@ function payMoneyRequestOnSearch(hash: number, paymentData: PaymentData[], trans
 
     const optimisticData: OnyxUpdate[] = createOnyxData({isActionLoading: true});
     const failureData: OnyxUpdate[] = createOnyxData({isActionLoading: false, errors: getMicroSecondOnyxErrorWithTranslationKey('common.genericErrorMessage')});
+    // If we are on the 'Pay' suggested search, remove the report from the view once the action is taken, don't wait for the view to be re-fetched via Search
     const successData: OnyxUpdate[] = currentSearchKey === CONST.SEARCH.SUGGESTED_SEARCH_KEYS.PAY ? createOnyxData(null) : createOnyxData({isActionLoading: false});
 
     // eslint-disable-next-line rulesdir/no-api-side-effects-method
