9d95409cdec8103b0b6cbbffb03be5d175146fec
Agata Kosior <agata.kosior57@gmail.com>
2025-07-17 16:32:57 +0200

    fix: pasting from clipboard on ios

--- src/libs/fileDownload/FileUtils.ts
+++ src/libs/fileDownload/FileUtils.ts
@@ -1,18 +1,19 @@
-import {Str} from 'expensify-common';
-import {Alert, Linking, Platform} from 'react-native';
+import { Str } from 'expensify-common';
+import { Alert, Linking, Platform } from 'react-native';
 import ImageSize from 'react-native-image-size';
-import type {TupleToUnion, ValueOf} from 'type-fest';
+import type { TupleToUnion, ValueOf } from 'type-fest';
 import DateUtils from '@libs/DateUtils';
 import getPlatform from '@libs/getPlatform';
-import {translateLocal} from '@libs/Localize';
+import { translateLocal } from '@libs/Localize';
 import Log from '@libs/Log';
 import saveLastRoute from '@libs/saveLastRoute';
-import type {FileObject} from '@pages/media/AttachmentModalScreen/types';
+import type { FileObject } from '@pages/media/AttachmentModalScreen/types';
 import CONST from '@src/CONST';
-import type {TranslationPaths} from '@src/languages/types';
+import type { TranslationPaths } from '@src/languages/types';
 import getImageManipulator from './getImageManipulator';
 import getImageResolution from './getImageResolution';
-import type {ReadFileAsync, SplitExtensionFromFileName} from './types';
+import type { ReadFileAsync, SplitExtensionFromFileName } from './types';
+
 
 /**
  * Show alert on successful attachment download
@@ -406,8 +407,10 @@ const normalizeFileObject = (file: FileObject): Promise<FileObject> => {
     }
 
     const isAndroidNative = getPlatform() === CONST.PLATFORM.ANDROID;
+    const isIOSNative = getPlatform() === CONST.PLATFORM.IOS;
+    const isNativePlatform = isAndroidNative || isIOSNative;
 
-    if (!isAndroidNative || 'size' in file) {
+    if (!isNativePlatform || 'size' in file) {
         return Promise.resolve(file);
     }
 
