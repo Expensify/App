d51b542ec36e768d6f8dae65b771cf77a5f09221
Andrew Rosiclair <arosiclair@expensify.com>
2025-07-17 11:25:55 -0400

    add soft kill button

---
+++ src/components/SoftKillTestToolRow/index.native.tsx
@@ -0,0 +1,14 @@
+import Button from '@components/Button';
+import TestToolRow from '@components/TestToolRow';
+
+export default function SoftKillTestToolRow() {
+    return (
+        <TestToolRow title="Soft kill the app">
+            <Button
+                small
+                text="Kill"
+                onPress={() => {}}
+            />
+        </TestToolRow>
+    );
+}
