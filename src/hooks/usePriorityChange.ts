749c82ea4163ae802b1236040ae9fd2ca190a1e0
thelullabyy <the.lullaby2@hotmail.com>
2025-07-17 23:07:18 +0700

    fix lint

--- src/hooks/usePriorityChange.ts
+++ src/hooks/usePriorityChange.ts
@@ -8,13 +8,13 @@ import ONYXKEYS from '@src/ONYXKEYS';
 function usePriorityChange() {
     const [priorityMode] = useOnyx(ONYXKEYS.NVP_PRIORITY_MODE, {canBeMissing: true});
     const [allReportsDraftComment] = useOnyx(ONYXKEYS.COLLECTION.REPORT_DRAFT_COMMENT, {canBeMissing: true});
-    // Track the previous value of priorityMode to detect when it changes from GSD to DEFAULT
     const prevPriorityMode = usePrevious(priorityMode);
 
     useEffect(() => {
-        if (prevPriorityMode === CONST.PRIORITY_MODE.GSD && priorityMode === CONST.PRIORITY_MODE.DEFAULT) {
-            openApp(false, allReportsDraftComment);
+        if (!(prevPriorityMode === CONST.PRIORITY_MODE.GSD && priorityMode === CONST.PRIORITY_MODE.DEFAULT)) {
+            return;
         }
+        openApp(false, allReportsDraftComment);
     }, [priorityMode, allReportsDraftComment, prevPriorityMode]);
 }
 
