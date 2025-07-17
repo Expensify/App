a3cbb33a5c84b184f329182025f559cf4c52dbe5
dmkt9 <demoskratos9x@gmail.com>
2025-07-17 21:06:59 +0700

    Fix - Report field shows a broken icon instead of the custom global create emoji

--- src/components/RenderHTML.tsx
+++ src/components/RenderHTML.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {RenderHTMLSource} from 'react-native-render-html';
 import useWindowDimensions from '@hooks/useWindowDimensions';
+import Parser from '@libs/Parser';
 
 type RenderHTMLProps = {
     /** HTML string to render */
@@ -11,8 +12,13 @@ type RenderHTMLProps = {
 // Configuration for RenderHTML is handled in a top-level component providing
 // context to RenderHTMLSource components. See https://git.io/JRcZb
 // The provider is available at src/components/HTMLEngineProvider/
-function RenderHTML({html}: RenderHTMLProps) {
+function RenderHTML({html: htmlParam}: RenderHTMLProps) {
     const {windowWidth} = useWindowDimensions();
+    const html = useMemo(() => {
+        // Sanitize emoji characters already wrapped in <emoji> tags to prevent double-tagging.
+        const sanitizedHtml = htmlParam.replaceAll(/<\/?emoji>/g, '');
+        return Parser.replace(sanitizedHtml, {shouldEscapeText: false, filterRules: ['emoji']});
+    }, [htmlParam]);
     return (
         <RenderHTMLSource
             contentWidth={windowWidth * 0.8}
