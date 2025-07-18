267bbb77f0ec3e5d3f3de5b77c828aa54459a85f
Fábio Henriques <contact@fabiohenriques.dev>
2025-07-18 15:41:28 +0100

    Mask reimbursement bank account flow

--- src/pages/ReimbursementAccount/NonUSD/BankInfo/BankInfo.tsx
+++ src/pages/ReimbursementAccount/NonUSD/BankInfo/BankInfo.tsx
@@ -129,6 +129,7 @@ function BankInfo({onBackButtonPress, onSubmit, policyID, stepNames}: BankInfoPr
             headerTitle={translate('bankAccount.bankInfo')}
             stepNames={stepNames}
             startStepIndex={1}
+            fsClass={CONST.FULL_STORY.MASK}
         >
             <SubStep
                 isEditing={isEditing}
