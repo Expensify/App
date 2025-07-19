dc1a650e4afd805a7ee026543786edab2e9af429
allgandalf <whogandalf@gmail.com>
2025-07-19 16:51:33 +0530

    fix typecheck

--- src/pages/iou/request/step/IOURequestStepConfirmation.tsx
+++ src/pages/iou/request/step/IOURequestStepConfirmation.tsx
@@ -492,6 +492,7 @@ function IOURequestStepConfirmation({
                     },
                     shouldHandleNavigation: index === transactions.length - 1,
                     backToReport,
+                    countryCodeByIP,
                 });
             });
         },
@@ -511,6 +512,7 @@ function IOURequestStepConfirmation({
             backToReport,
             viewTourReport,
             viewTourReportID,
+            countryCodeByIP,
         ],
     );
 
@@ -546,9 +548,10 @@ function IOURequestStepConfirmation({
                     billable: transaction.billable,
                     attendees: transaction.comment?.attendees,
                 },
+                countryCodeByIP,
             });
         },
-        [report, transaction, currentUserPersonalDetails.login, currentUserPersonalDetails.accountID, policy, policyTags, policyCategories],
+        [report, transaction, currentUserPersonalDetails.login, currentUserPersonalDetails.accountID, policy, policyTags, policyCategories, countryCodeByIP],
     );
 
     const trackExpense = useCallback(
@@ -653,6 +656,7 @@ function IOURequestStepConfirmation({
                     attendees: transaction.comment?.attendees,
                 },
                 backToReport,
+                countryCodeByIP,
             });
         },
         [
@@ -668,6 +672,7 @@ function IOURequestStepConfirmation({
             transactionTaxAmount,
             customUnitRateID,
             backToReport,
+            countryCodeByIP,
         ],
     );
 
@@ -719,6 +724,7 @@ function IOURequestStepConfirmation({
                         currency: transaction.currency,
                         taxCode: transactionTaxCode,
                         taxAmount: transactionTaxAmount,
+                        countryCodeByIP,
                     });
                 }
                 return;
@@ -746,6 +752,7 @@ function IOURequestStepConfirmation({
                         splitPayerAccountIDs: transaction.splitPayerAccountIDs ?? [],
                         taxCode: transactionTaxCode,
                         taxAmount: transactionTaxAmount,
+                        countryCodeByIP,
                     });
                 }
                 return;
@@ -771,6 +778,7 @@ function IOURequestStepConfirmation({
                         splitPayerAccountIDs: transaction.splitPayerAccountIDs,
                         taxCode: transactionTaxCode,
                         taxAmount: transactionTaxAmount,
+                        countryCodeByIP,
                     });
                 }
                 return;
@@ -892,6 +900,7 @@ function IOURequestStepConfirmation({
             trackExpense,
             submitPerDiemExpense,
             userLocation,
+            countryCodeByIP,
         ],
     );
 
