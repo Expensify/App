1fb4579bf40076c37379ed53fa266455b58d7d19
Eskalifer1 <artemkryt1800@gmail.com>
2025-07-21 11:05:11 +0300

    fix:65746: added Keyboard.dismiss on tab change in Create Expense

--- src/components/SelectionList/index.tsx
+++ src/components/SelectionList/index.tsx
@@ -58,12 +58,8 @@ function SelectionList<TItem extends ListItem>({onScroll, shouldHideKeyboardOnSc
 
     // In SearchPageBottomTab we use useAnimatedScrollHandler from reanimated(for performance reasons) and it returns object instead of function. In that case we cannot change it to a function call, that's why we have to choose between onScroll and defaultOnScroll.
     const defaultOnScroll = () => {
-        // Only dismiss the keyboard whenever the user scrolls the screen
-        if (!isScreenTouched) {
-            return;
-        }
-
-        if (!shouldHideKeyboardOnScroll) {
+        // Only dismiss the keyboard whenever the user scrolls the screen or `shouldHideKeyboardOnScroll` is true
+        if (!isScreenTouched || !shouldHideKeyboardOnScroll) {
             return;
         }
         Keyboard.dismiss();
