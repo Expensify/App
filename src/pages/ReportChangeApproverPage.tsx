5cd6f09c5a0f5816b02dd7650c862401a48d03e8
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-07-22 21:44:42 +0530

    Revert Add approver changes

--- src/pages/ReportChangeApproverPage.tsx
+++ src/pages/ReportChangeApproverPage.tsx
@@ -12,9 +12,8 @@ import {changeReportApprover} from '@libs/actions/Report';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {ReportChangeApproverParamList} from '@libs/Navigation/types';
-import {getManagerAccountEmail, isControlPolicy, isPolicyAdmin, isUserPolicyAdmin} from '@libs/PolicyUtils';
+import {getManagerAccountEmail, isPolicyAdmin, isUserPolicyAdmin} from '@libs/PolicyUtils';
 import {isMoneyRequestReport, isMoneyRequestReportPendingDeletion} from '@libs/ReportUtils';
-import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
@@ -25,45 +24,30 @@ import withReportOrNotFound from './home/report/withReportOrNotFound';
 
 type ReportChangeApproverPageProps = WithReportOrNotFoundProps & PlatformStackScreenProps<ReportChangeApproverParamList, typeof SCREENS.REPORT_CHANGE_APPROVER.ROOT>;
 
-function ReportChangeApproverPage({report, policies, isLoadingReportData}: ReportChangeApproverPageProps) {
+function ReportChangeApproverPage({report, policy, isLoadingReportData}: ReportChangeApproverPageProps) {
     const reportID = report?.reportID;
     const {translate} = useLocalize();
     const styles = useThemeStyles();
 
     const [session] = useOnyx(ONYXKEYS.SESSION, {canBeMissing: false});
-    const reportPolicy = policies && Object.values(policies).find((policy) => policy?.id === report?.policyID);
     const [selectedApproverType, setSelectedApproverType] = useState<string>();
 
     const changeApprover = useCallback(() => {
         if (!selectedApproverType) {
             return;
         }
-        if (selectedApproverType === 'addApprover') {
-            if (reportPolicy && !isControlPolicy(reportPolicy)) {
-                Navigation.navigate(
-                    ROUTES.WORKSPACE_UPGRADE.getRoute(
-                        reportPolicy.id,
-                        CONST.UPGRADE_FEATURE_INTRO_MAPPING.multiApprovalLevels.alias,
-                        ROUTES.REPORT_CHANGE_APPROVER_ADD_APPROVER.getRoute(report.reportID),
-                    ),
-                );
-                return;
-            }
-            Navigation.navigate(ROUTES.REPORT_CHANGE_APPROVER_ADD_APPROVER.getRoute(report.reportID));
-            return;
-        }
 
-        if (!isPolicyAdmin(reportPolicy) || !reportPolicy || !session?.accountID) {
+        if (!isPolicyAdmin(policy) || !policy || !session?.accountID) {
             return;
         }
-        changeReportApprover(reportID, reportPolicy.id, session.accountID);
+        changeReportApprover(reportID, policy.id, session.accountID);
         Navigation.goBack(ROUTES.REPORT_WITH_ID.getRoute(reportID));
-    }, [selectedApproverType, reportPolicy, reportID, session?.accountID, report.reportID]);
+    }, [selectedApproverType, policy, reportID, session?.accountID]);
 
     const sections = useMemo(() => {
         const data = [];
 
-        if (!isUserPolicyAdmin(reportPolicy, getManagerAccountEmail(reportPolicy, report))) {
+        if (!isUserPolicyAdmin(policy, getManagerAccountEmail(policy, report))) {
             data.push({
                 text: translate('iou.changeApprover.actions.bypassApprovers'),
                 keyForList: 'bypassApprover',
@@ -74,11 +58,10 @@ function ReportChangeApproverPage({report, policies, isLoadingReportData}: Repor
         }
 
         return [{data}];
-    }, [report, reportPolicy, selectedApproverType, translate]);
+    }, [report, policy, selectedApproverType, translate]);
 
     // eslint-disable-next-line rulesdir/no-negated-variables
-    const shouldShowNotFoundView =
-        (isEmptyObject(reportPolicy) && !isLoadingReportData) || !isPolicyAdmin(reportPolicy) || !isMoneyRequestReport(report) || isMoneyRequestReportPendingDeletion(report);
+    const shouldShowNotFoundView = (isEmptyObject(policy) && !isLoadingReportData) || !isPolicyAdmin(policy) || !isMoneyRequestReport(report) || isMoneyRequestReportPendingDeletion(report);
 
     if (shouldShowNotFoundView) {
         return <NotFoundPage />;
@@ -107,8 +90,7 @@ function ReportChangeApproverPage({report, policies, isLoadingReportData}: Repor
                         <Text style={[styles.ph5, styles.mb5]}>{translate('iou.changeApprover.subtitle')}</Text>
                         <Text style={[styles.ph5, styles.mb5]}>
                             {translate('iou.changeApprover.description')}
-                            <TextLink onPress={() => Navigation.navigate(ROUTES.WORKSPACE_WORKFLOWS.getRoute(reportPolicy?.id))}>{translate('iou.changeApprover.workflowSettings')}</TextLink>
-                            .
+                            <TextLink onPress={() => Navigation.navigate(ROUTES.WORKSPACE_WORKFLOWS.getRoute(policy?.id))}>{translate('iou.changeApprover.workflowSettings')}</TextLink>.
                         </Text>
                     </>
                 }
