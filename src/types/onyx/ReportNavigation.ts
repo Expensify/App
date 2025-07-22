2b9157204b3f04c244b5129c26b4c2047294563f
sumo_slonik <jakub.nowakowski@swmansion.com>
2025-07-22 14:34:56 +0200

    add comment to onyx type

--- src/types/onyx/ReportNavigation.ts
+++ src/types/onyx/ReportNavigation.ts
@@ -1,24 +1,26 @@
 import type {SearchQueryJSON} from '@components/Search/types';
 
 /**
- *hehhehehe
+ * Represents the parameters from the previous search invocation.
+ * This is used to persist search arguments between navigations within reports,
+ * and allows loading more search results as the user continues navigating.
  */
 type LastSearchParams = {
     /**
-     *
+     * The number of results returned in the previous search.
      */
     previousLengthOfResults?: number;
     /**
-     *
+     * Indicates whether there are more results available beyond the last search.
      */
     hasMoreResults?: boolean;
 
     /**
-     *
+     * The full query JSON object that was used in the last search.
      */
     queryJSON: SearchQueryJSON;
     /**
-     *
+     * The current offset used in pagination for fetching the previous set of results.
      */
     offset?: number;
 };
