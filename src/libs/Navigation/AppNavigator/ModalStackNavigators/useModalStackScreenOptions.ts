6e9b284556c861aa0c6dd7c99264eb3bc1218e82
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-07-23 17:11:14 +0200

    implement wide rhp

--- src/libs/Navigation/AppNavigator/ModalStackNavigators/useModalStackScreenOptions.ts
+++ src/libs/Navigation/AppNavigator/ModalStackNavigators/useModalStackScreenOptions.ts
@@ -1,9 +1,11 @@
+import type {ParamListBase} from '@react-navigation/native';
 import {CardStyleInterpolators} from '@react-navigation/stack';
-import {useMemo} from 'react';
+import {useCallback, useContext} from 'react';
+import {WideRHPContext} from '@components/WideRHPContextProvider';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import hideKeyboardOnSwipe from '@libs/Navigation/AppNavigator/hideKeyboardOnSwipe';
-import type {PlatformStackNavigationOptions} from '@libs/Navigation/PlatformStackNavigation/types';
+import type {PlatformStackNavigationOptions, PlatformStackRouteProp} from '@libs/Navigation/PlatformStackNavigation/types';
 import variables from '@styles/variables';
 
 function useModalStackScreenOptions() {
@@ -14,26 +16,41 @@ function useModalStackScreenOptions() {
     // https://github.com/Expensify/App/issues/63747
     // eslint-disable-next-line rulesdir/prefer-shouldUseNarrowLayout-instead-of-isSmallScreenWidth
     const {isSmallScreenWidth} = useResponsiveLayout();
-
+    const {wideRHPRouteKeys} = useContext(WideRHPContext);
     const cardStyleInterpolator = CardStyleInterpolators.forHorizontalIOS;
 
-    return useMemo(
-        (): PlatformStackNavigationOptions => ({
-            ...hideKeyboardOnSwipe,
-            headerShown: false,
-            animationTypeForReplace: 'pop',
-            native: {
-                contentStyle: styles.navigationScreenCardStyle,
-            },
-            web: {
-                cardStyle: {
-                    ...styles.navigationScreenCardStyle,
-                    width: isSmallScreenWidth ? '100%' : variables.sideBarWidth,
+    return useCallback<({route}: {route: PlatformStackRouteProp<ParamListBase, string>}) => PlatformStackNavigationOptions>(
+        ({route}) => {
+            let width;
+
+            if (isSmallScreenWidth) {
+                width = '100%';
+            } else if (wideRHPRouteKeys.includes(route.key)) {
+                // For the wide rhp screen
+                width = variables.sideBarWidth * 2;
+            } else {
+                width = variables.sideBarWidth;
+            }
+
+            return {
+                ...hideKeyboardOnSwipe,
+                headerShown: false,
+                animationTypeForReplace: 'pop',
+                native: {
+                    contentStyle: styles.navigationScreenCardStyle,
                 },
-                cardStyleInterpolator,
-            },
-        }),
-        [cardStyleInterpolator, isSmallScreenWidth, styles.navigationScreenCardStyle],
+                web: {
+                    cardStyle: {
+                        ...styles.navigationScreenCardStyle,
+                        width,
+                        position: 'fixed',
+                        right: 0,
+                    },
+                    cardStyleInterpolator,
+                },
+            };
+        },
+        [cardStyleInterpolator, isSmallScreenWidth, styles.navigationScreenCardStyle, wideRHPRouteKeys],
     );
 }
 
