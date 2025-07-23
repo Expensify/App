47f0e72027f25c59fb2b085c3275e6e274874f8c
allgandalf <whogandalf@gmail.com>
2025-07-23 22:24:20 +0530

    fix comments

--- tests/unit/libs/PersonalDetailsUtilsTest.ts
+++ tests/unit/libs/PersonalDetailsUtilsTest.ts
@@ -7,7 +7,7 @@ describe('PersonalDetailsUtils', () => {
     beforeAll(async () => {
         await Onyx.merge(ONYXKEYS.COUNTRY_CODE, 1);
     });
-    
+
     test('getPersonalDetailsOnyxDataForOptimisticUsers should return correct optimistic and finally data', () => {
         const newLogins = ['3322076524', 'test2@test.com'];
         const newAccountIDs = [1, 2];
@@ -19,21 +19,21 @@ describe('PersonalDetailsUtils', () => {
         const expected = {
             optimisticData: [
                 {
-                    key: ONYXKEYS.PERSONAL_DETAILS_LIST, // Expecting 'personalDetailsList'
-                    onyxMethod: 'merge', // Use string literal as expected
+                    key: ONYXKEYS.PERSONAL_DETAILS_LIST,
+                    onyxMethod: 'merge',
                     value: {
                         // eslint-disable-next-line @typescript-eslint/naming-convention
                         '1': {
-                            // Use numeric key as per lint rule (no quotes)
                             accountID: 1,
-                            displayName: '3322076524', // This comes from mockFormatPhoneNumber('test1@test.com')
+                            displayName: '3322076524',
                             isOptimisticPersonalDetail: true,
                             login: '3322076524',
                         },
                         // eslint-disable-next-line @typescript-eslint/naming-convention
                         '2': {
                             accountID: 2,
-                            displayName: 'test2@test.com', // This comes from mockFormatPhoneNumber('test2@test.com')
+                            // This comes from mockFormatPhoneNumber('test2@test.com')
+                            displayName: 'test2@test.com',
                             isOptimisticPersonalDetail: true,
                             login: 'test2@test.com',
                         },
@@ -42,13 +42,13 @@ describe('PersonalDetailsUtils', () => {
             ],
             finallyData: [
                 {
-                    key: ONYXKEYS.PERSONAL_DETAILS_LIST, // Expecting 'personalDetailsList'
-                    onyxMethod: Onyx.METHOD.MERGE, // Expecting 'merge'
+                    key: ONYXKEYS.PERSONAL_DETAILS_LIST, 
+                    onyxMethod: Onyx.METHOD.MERGE, 
                     value: {
                         // eslint-disable-next-line @typescript-eslint/naming-convention
-                        '1': null, // Cleanup for accountID 1
+                        '1': null,
                         // eslint-disable-next-line @typescript-eslint/naming-convention
-                        '2': null, // Cleanup for accountID 2
+                        '2': null, 
                     },
                 },
             ],
