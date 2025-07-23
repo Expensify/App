4508434b263745e74af191e879e4eaf9b5e9df00
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-07-23 15:47:10 +0200

    fix PopoverMenu subMenuItems bug

--- src/components/ButtonWithDropdownMenu/index.tsx
+++ src/components/ButtonWithDropdownMenu/index.tsx
@@ -264,6 +264,7 @@ function ButtonWithDropdownMenuInner<IValueType>(props: ButtonWithDropdownMenuPr
                                   setSelectedItemIndex(index);
                               },
                         shouldCallAfterModalHide: true,
+                        subMenuItems: item.subMenuItems?.map((subItem) => ({...subItem, shouldCallAfterModalHide: true})),
                     }))}
                 />
             )}
