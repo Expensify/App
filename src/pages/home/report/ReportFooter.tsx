090fda84f8541d696dd0d995c025370d97c4d499
William Quayson <wgsquayson@gmail.com>
2025-07-23 12:35:42 -0300

    chore: ensure the full height composer does not overlap the chat header

--- src/pages/home/report/ReportFooter.tsx
+++ src/pages/home/report/ReportFooter.tsx
@@ -84,6 +84,8 @@ type ReportFooterProps = {
     keyboardHeight: SharedValue<number>;
 
     onLayout: (height: number) => void;
+
+    headerHeight: number;
 };
 
 function ReportFooter({
@@ -101,6 +103,7 @@ function ReportFooter({
     nativeID,
     keyboardHeight,
     onLayout,
+    headerHeight,
 }: ReportFooterProps) {
     const styles = useThemeStyles();
     const {isOffline} = useNetwork();
@@ -221,7 +224,8 @@ function ReportFooter({
 
         return {
             position: 'absolute',
-            bottom: 0,
+            // Ensures the composer does not overlap the header
+            bottom: isComposerFullSize ? -headerHeight : 0,
             width: '100%',
             transform,
         };
