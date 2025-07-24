36aee14aeac3984f890ae4ba14a84fe88ad968d7
allgandalf <whogandalf@gmail.com>
2025-07-24 22:57:28 +0530

    fix prettier and failing test

--- src/pages/tasks/NewTaskPage.tsx
+++ src/pages/tasks/NewTaskPage.tsx
@@ -34,7 +34,10 @@ function NewTaskPage({route}: NewTaskPageProps) {
     const [personalDetails] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {canBeMissing: false});
     const styles = useThemeStyles();
     const {translate, formatPhoneNumber} = useLocalize();
-    const assignee = useMemo(() => getAssignee(task?.assigneeAccountID ?? CONST.DEFAULT_NUMBER_ID, personalDetails, formatPhoneNumber), [task?.assigneeAccountID, personalDetails, formatPhoneNumber]);
+    const assignee = useMemo(
+        () => getAssignee(task?.assigneeAccountID ?? CONST.DEFAULT_NUMBER_ID, personalDetails, formatPhoneNumber),
+        [task?.assigneeAccountID, personalDetails, formatPhoneNumber],
+    );
     const assigneeTooltipDetails = getDisplayNamesWithTooltips(getPersonalDetailsForAccountIDs(task?.assigneeAccountID ? [task.assigneeAccountID] : [], personalDetails), false);
     const shareDestination = useMemo(
         () => (task?.shareDestination ? getShareDestination(task.shareDestination, reports, personalDetails, formatPhoneNumber) : undefined),
