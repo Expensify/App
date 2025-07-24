2b3221c871fdeebd788474223b412c631eb2df5e
DylanDylann <dylandylan9112@gmail.com>
2025-07-24 22:20:10 +0700

    create new log

---
+++ src/libs/LogNew.ts
@@ -0,0 +1,95 @@
+// Making an exception to this rule here since we don't need an "action" for Log and Log should just be used directly. Creating a Log
+// action would likely cause confusion about which one to use. But most other API methods should happen inside an action file.
+
+/* eslint-disable rulesdir/no-api-in-views */
+import {Logger} from 'expensify-common';
+import AppLogs from 'react-native-app-logs';
+import useOnyx from '@hooks/useOnyx';
+import type {Merge} from 'type-fest';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import pkg from '../../package.json';
+import {addLog, flushAllLogsOnAppLaunch} from './actions/Console';
+import {shouldAttachLog} from './Console';
+import getPlatform from './getPlatform';
+import {post} from './Network';
+import requireParameters from './requireParameters';
+
+type LogCommandParameters = {
+    expensifyCashAppVersion: string;
+    logPacket: string;
+};
+
+function LogCommand(parameters: LogCommandParameters): Promise<{requestID: string}> {
+    const commandName = 'Log';
+    requireParameters(['logPacket', 'expensifyCashAppVersion'], parameters, commandName);
+
+    // Note: We are forcing Log to run since it requires no authToken and should only be queued when we are offline.
+    // Non-cancellable request: during logout, when requests are cancelled, we don't want to cancel any remaining logs
+    return post(commandName, {...parameters, forceNetworkRequest: true, canCancel: false}) as Promise<{requestID: string}>;
+}
+
+// eslint-disable-next-line
+type ServerLoggingCallbackOptions = {api_setCookie: boolean; logPacket: string};
+type RequestParams = Merge<ServerLoggingCallbackOptions, {shouldProcessImmediately: boolean; shouldRetry: boolean; expensifyCashAppVersion: string; parameters: string}>;
+
+/**
+ * Network interface for logger.
+ */
+function serverLoggingCallback(logger: Logger, params: ServerLoggingCallbackOptions): Promise<{requestID: string}> {
+    const requestParams = params as RequestParams;
+    requestParams.shouldProcessImmediately = false;
+    requestParams.shouldRetry = false;
+    requestParams.expensifyCashAppVersion = `expensifyCash[${getPlatform()}]${pkg.version}`;
+    if (requestParams.parameters) {
+        requestParams.parameters = JSON.stringify(requestParams.parameters);
+    }
+    setTimeout(() => logger.info('Flushing logs older than 10 minutes', true, {}, true), 10 * 60 * 1000);
+    return LogCommand(requestParams);
+}
+
+// Note: We are importing Logger from expensify-common because it is used by other platforms. The server and client logging
+// callback methods are passed in here so we can decouple the logging library from the logging methods.
+const createLogger = (shouldCollectLogs: boolean) =>
+    new Logger({
+        serverLoggingCallback,
+        clientLoggingCallback: (message: string, extraData: string | Record<string, unknown> | Error | Array<Record<string, unknown>>) => {
+            if (!shouldAttachLog(message)) {
+                return;
+            }
+            flushAllLogsOnAppLaunch().then(() => {
+                console.debug(message, extraData);
+                if (shouldCollectLogs) {
+                    addLog({time: new Date(), level: CONST.DEBUG_CONSOLE.LEVELS.DEBUG, message, extraData});
+                }
+            });
+        },
+        maxLogLinesBeforeFlush: 150,
+        isDebug: true,
+    });
+
+function useLog() {
+    const [shouldStoreLogs = false] = useOnyx(ONYXKEYS.SHOULD_STORE_LOGS);
+
+    const Log = createLogger(shouldStoreLogs);
+    setTimeout(() => Log.info('Flushing logs older than 10 minutes', true, {}, true), 10 * 60 * 1000);
+
+    AppLogs.configure({appGroupName: 'group.com.expensify.new', interval: -1});
+    AppLogs.registerHandler({
+        filter: '[NotificationService]',
+        handler: ({filter, logs}) => {
+            logs.forEach((log) => {
+                // Both native and JS logs are captured by the filter so we replace the filter before logging to avoid an infinite loop
+                const message = `[PushNotification] ${log.message.replace(filter, 'NotificationService -')}`;
+
+                if (log.level === 'error') {
+                    Log.hmmm(message);
+                } else {
+                    Log.info(message);
+                }
+            });
+        },
+    });
+}
+
+export default useLog;
