0c9f4d2f7c989e0e51b0320e5c214ff1b8e1e052
Yauheni Pasiukevich <pasyukevich@live.com>
2025-07-24 13:29:50 +0200

    fix: negative value for the moved transaction in offline mode

--- src/libs/actions/Transaction.ts
+++ src/libs/actions/Transaction.ts
@@ -20,6 +20,7 @@ import {
     buildOptimisticUnreportedTransactionAction,
     buildTransactionThread,
     findSelfDMReportID,
+    shouldEnableNegative,
 } from '@libs/ReportUtils';
 import {getAmount, waypointHasValidAddress} from '@libs/TransactionUtils';
 import ViolationsUtils from '@libs/Violations/ViolationsUtils';
@@ -679,8 +680,11 @@ function changeTransactionsReport(transactionIDs: string[], reportID: string, po
             });
         }
 
+        const allowNegative = shouldEnableNegative(newReport);
+
         // 3. Keep track of the new report totals
-        const transactionAmount = getAmount(transaction);
+        const transactionAmount = getAmount(transaction, undefined, undefined, allowNegative);
+
         if (oldReport) {
             updatedReportTotals[oldReportID] = (updatedReportTotals[oldReportID] ? updatedReportTotals[oldReportID] : (oldReport?.total ?? 0)) + transactionAmount;
         }
