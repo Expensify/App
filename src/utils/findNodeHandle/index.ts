f21660483cd2229a9bc1e1d3e54c5bf1b8e5493b
war-in <war-in@users.noreply.github.com>
2025-07-24 15:58:28 +0200

    fix comment

--- src/utils/findNodeHandle/index.ts
+++ src/utils/findNodeHandle/index.ts
@@ -4,7 +4,7 @@ import type { NodeHandle } from 'react-native/Libraries/ReactNative/RendererProx
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 function findNodeHandle(componentOrHandle: null | number | Component<any, any> | ComponentClass<any>): null | NodeHandle {
     // eslint-disable-next-line no-console
-    console.warn('findNodeHandle is not supported on web. Use the ref property on the component instead.');
+    console.warn('findNodeHandle is not supported on web. Use the ref property of the component instead.');
     return null;
 }
 
