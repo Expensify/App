fd032bfa09e470155dd57681f22356ecd5b2becc
William Quayson <wgsquayson@gmail.com>
2025-07-24 11:27:21 -0300

    chore: add description to some props and adjust the ActionSheet to be an animated component

--- src/components/ActionSheetAwareScrollView/index.tsx
+++ src/components/ActionSheetAwareScrollView/index.tsx
@@ -1,33 +1,39 @@
 // this whole file is just for other platforms
 // iOS version has everything implemented
-import type {PropsWithChildren} from 'react';
+import type {ReactNode} from 'react';
 import React, {forwardRef} from 'react';
-import type {ScrollViewProps} from 'react-native';
 // eslint-disable-next-line no-restricted-imports
-import {ScrollView} from 'react-native';
+import type {AnimatedScrollViewProps, SharedValue} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
+import type {AnimatedScrollView} from 'react-native-reanimated/lib/typescript/component/ScrollView';
 import {Actions, ActionSheetAwareScrollViewContext, ActionSheetAwareScrollViewProvider} from './ActionSheetAwareScrollViewContext';
 
-const ActionSheetAwareScrollView = forwardRef<ScrollView, PropsWithChildren<ScrollViewProps>>((props, ref) => (
-    <ScrollView
+type PropsWithAnimatedChildren = AnimatedScrollViewProps & {
+    children?: ReactNode | SharedValue<ReactNode>;
+};
+
+const ActionSheetAwareScrollView = forwardRef<AnimatedScrollView, PropsWithAnimatedChildren>((props, ref) => (
+    <Animated.ScrollView
         ref={ref}
         // eslint-disable-next-line react/jsx-props-no-spreading
         {...props}
     >
         {props.children}
-    </ScrollView>
+    </Animated.ScrollView>
 ));
 
 export default ActionSheetAwareScrollView;
 
 /**
- * This is only used on iOS and Android.
+ * The bottom spacing config for this action sheet is only used on Android and iOS. On other platforms,
+ * this component will be a default Animated.Scrollview, because the onScroll handler used is from Reanimated.
  *
  * This function should be used as renderScrollComponent prop for FlatList
  * @param {Object} props - props that will be passed to the ScrollView from FlatList
  * @returns {React.ReactElement} - ActionSheetAwareScrollView
  */
 
-function renderScrollComponent(props: ScrollViewProps) {
+function renderScrollComponent(props: PropsWithAnimatedChildren) {
     // eslint-disable-next-line react/jsx-props-no-spreading
     return <ActionSheetAwareScrollView {...props} />;
 }
