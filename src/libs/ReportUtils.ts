23ee449fe020117c5402a271c4d23c326709eecb
Hubert Sosinski <hubert.sosinski@callstack.com>
2025-07-25 13:44:38 +0200

    Add method getTransactionDetailsAmount

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -3989,6 +3989,14 @@ function getTransactionDetails(
     };
 }
 
+function getTransactionDetailsAmount(transaction: OnyxInputOrEntry<Transaction>) {
+    if (!transaction) {
+        return 0;
+    }
+    const report = getReportOrDraftReport(transaction?.reportID);
+    return getTransactionAmount(transaction, !isEmptyObject(report) && isExpenseReport(report), transaction?.reportID === CONST.REPORT.UNREPORTED_REPORT_ID);
+}
+
 function getTransactionCommentObject(transaction: OnyxEntry<Transaction>): Comment {
     return {
         ...transaction?.comment,
@@ -11267,6 +11275,7 @@ export {
     canDeleteCardTransactionByLiabilityType,
     getTaskAssigneeChatOnyxData,
     getTransactionDetails,
+    getTransactionDetailsAmount,
     getTransactionReportName,
     getDisplayedReportID,
     getTransactionsWithReceipts,
