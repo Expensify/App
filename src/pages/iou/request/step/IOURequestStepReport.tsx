b8c889a397577589043020115e1acde64754691a
Ryan Teguh <ryanteguh123@gmail.com>
2025-07-28 19:55:57 +0800

    ESLint fix

--- src/pages/iou/request/step/IOURequestStepReport.tsx
+++ src/pages/iou/request/step/IOURequestStepReport.tsx
@@ -31,7 +31,7 @@ function IOURequestStepReport({route, transaction}: IOURequestStepReportProps) {
     const isFromGlobalCreate = !!transaction?.isFromGlobalCreate;
     const reportOrDraftReport = getReportOrDraftReport(reportIDFromRoute);
 
-    const handleGoBackWithReportID = (id: string) => {
+    const handleGoBackWithReportID = () => {
         if (isEditing) {
             Navigation.dismissModal();
         } else {
@@ -88,7 +88,7 @@ function IOURequestStepReport({route, transaction}: IOURequestStepReportProps) {
             changeTransactionsReport([transaction.transactionID], item.value);
         }
 
-        handleGoBackWithReportID(item.value);
+        handleGoBackWithReportID();
     };
 
     const selectReport = (item: TransactionGroupListItem) => {
@@ -100,7 +100,7 @@ function IOURequestStepReport({route, transaction}: IOURequestStepReportProps) {
 
         // Early return for same report selection
         if (isSameReport) {
-            handleGoBackWithReportID(item.value);
+            handleGoBackWithReportID();
             return;
         }
 
