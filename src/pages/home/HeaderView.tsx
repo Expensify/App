0fa17143982e804f0df79db785819fa025c053a9
krishna2323 <belivethatkg@gmail.com>
2025-07-28 10:11:32 +0530

    minor update.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/pages/home/HeaderView.tsx
+++ src/pages/home/HeaderView.tsx
@@ -209,7 +209,7 @@ function HeaderView({report, parentReportAction, onNavigationMenuButtonClicked, 
     // If the onboarding report is directly loaded, shouldShowDiscountBanner can return wrong value as it is not
     // linked to the react lifecycle directly. Wait for trial dates to load, before calculating.
     const shouldShowDiscount = useMemo(
-        () => shouldShowDiscountBanner(hasTeam2025Pricing, subscriptionPlan) && !isArchivedReport(reportNameValuePairs),
+        () => shouldShowDiscountBanner(hasTeam2025Pricing, subscriptionPlan) && !isReportArchived,
         // eslint-disable-next-line react-compiler/react-compiler
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [firstDayFreeTrial, lastDayFreeTrial, hasTeam2025Pricing, reportNameValuePairs, subscriptionPlan],
