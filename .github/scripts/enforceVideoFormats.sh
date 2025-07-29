34dc4e9e8564e172fed5508d314dd66129a2adfe
Dylan Le <54580990+MacroLens@users.noreply.github.com>
2025-07-29 18:24:50 -0400

    github: scripts: add error flag and and exit codes

--- .github/scripts/enforceVideoFormats.sh
+++ .github/scripts/enforceVideoFormats.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 # Checks for usage of iframe and non-cloudflare URL for videos in documentation.
+set -eu
 
 source ./scripts/shellUtils.sh
 title 'Enforce no iframe usage for videos and cloudflare CDN links'
+hasViolation=false
 folder="./docs"
 regex="(?:<iframe[^>]*?)(?:\s*width=[\"\"'](?<width>[^\"\"']+)[\"\"']|\s*height=[\"\"'](?<height>[^'\"\"]+)[\"\"']|\s*src=[\"\"'](?<src>[^'\"\"]+[\"\"']))+[^>]*?>"
-find "$folder" -type f -name "*.md" ! -iname "README.md" | while read -r file; do
-    pcregrep -n "$regex" "$file" | while read -r match; do
+while IFS= read -r file; do
+    while IFS= read -r match; do
         error "Do not use iframes for video embeds: $file:$match"
-    done
-done
+        hasViolation=true
+    done < <(pcregrep -n "$regex" "$file")
+done < <(find "$folder" -type f -name "*.md" ! -iname "README.md")
+
 
 regex="{%\s*include\s+video\.html\s+(?:\s*thumbnail=[\"\"'](?<thumbnail>[^'\"\"]+)[\"\"']|\s*src=[\"\"'](?<src>[^'\"\"]+[\"\"']))+\s*%}"
 cdn_regex="https:\/\/(?:\S+)\.cloudflarestream.com\/(?:\S*)"
-find "$folder" -type f -name "*.md" ! -iname "README.md" | while read -r file; do
-    pcregrep -n "$regex" "$file" | while read -r match; do
-    if ! echo "$match" | pcregrep -q "$cdn_regex"; then
-        error "Video URL must be from Cloudflare CDN: $match"
-    fi
-    done
-done
+while IFS= read -r file; do
+    while IFS= read -r match; do
+        if ! echo "$match" | pcregrep -q "$cdn_regex"; then
+            error "Video URL must be from Cloudflare CDN: $match"
+            hasViolation=true
+        fi
+    done < <(pcregrep -n "$regex" "$file")
+done < <(find "$folder" -type f -name "*.md" ! -iname "README.md")
+
+if [[ $hasViolation == true ]]; then
+    error "Documentation has video violations"
+    exit 1
+fi
+
+success "No violations."
+exit 0
+
