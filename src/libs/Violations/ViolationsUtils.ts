8789166e493d2de544e19bb15fdbc36c7cc92d3e
nkdengineer <nkdengineer@outlook.com>
2025-07-30 16:23:52 +0700

    update isPolicyTrackTaxEnabled

--- src/libs/Violations/ViolationsUtils.ts
+++ src/libs/Violations/ViolationsUtils.ts
@@ -287,6 +287,7 @@ const ViolationsUtils = {
         const hasCategoryOverLimitViolation = transactionViolations.some((violation) => violation.name === CONST.VIOLATIONS.OVER_CATEGORY_LIMIT);
         const hasMissingCommentViolation = transactionViolations.some((violation) => violation.name === CONST.VIOLATIONS.MISSING_COMMENT);
         const hasTaxOutOfPolicyViolation = transactionViolations.some((violation) => violation.name === CONST.VIOLATIONS.TAX_OUT_OF_POLICY);
+        const isPolicyTrackTaxEnabled = !!policy?.tax?.trackingEnabled;
         const isTaxInPolicy = Object.keys(policy.taxRates?.taxes ?? {}).some((key) => key === updatedTransaction.taxCode);
 
         // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
@@ -394,11 +395,11 @@ const ViolationsUtils = {
             newTransactionViolations = reject(newTransactionViolations, {name: CONST.VIOLATIONS.MISSING_COMMENT});
         }
 
-        if (!hasTaxOutOfPolicyViolation && !isTaxInPolicy) {
+        if (isPolicyTrackTaxEnabled && !hasTaxOutOfPolicyViolation && !isTaxInPolicy) {
             newTransactionViolations.push({name: CONST.VIOLATIONS.TAX_OUT_OF_POLICY, type: CONST.VIOLATION_TYPES.VIOLATION});
         }
 
-        if (hasTaxOutOfPolicyViolation && isTaxInPolicy) {
+        if (isPolicyTrackTaxEnabled && hasTaxOutOfPolicyViolation && isTaxInPolicy) {
             newTransactionViolations = reject(newTransactionViolations, {name: CONST.VIOLATIONS.TAX_OUT_OF_POLICY});
         }
         return {
