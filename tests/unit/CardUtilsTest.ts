c193e586bd0395969ba585ba39c51560fab42807
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-07-31 23:46:18 +0530

    Add some tests

--- tests/unit/CardUtilsTest.ts
+++ tests/unit/CardUtilsTest.ts
@@ -10,6 +10,7 @@ import {
     formatCardExpiration,
     getBankCardDetailsImage,
     getBankName,
+    getCardDescription,
     getCardFeedIcon,
     getCardsByCardholderName,
     getCompanyFeeds,
@@ -26,7 +27,7 @@ import {
     maskCardNumber,
     sortCardsByCardholderName,
 } from '@src/libs/CardUtils';
-import type {CardFeeds, CardList, CompanyCardFeed, ExpensifyCardSettings, PersonalDetailsList, Policy, WorkspaceCardsList} from '@src/types/onyx';
+import type {Card, CardFeeds, CardList, CompanyCardFeed, ExpensifyCardSettings, PersonalDetailsList, Policy, WorkspaceCardsList} from '@src/types/onyx';
 import type {CompanyCardFeedWithNumber} from '@src/types/onyx/CardFeeds';
 import {localeCompare} from '../utils/TestHelper';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
@@ -1003,6 +1004,43 @@ describe('CardUtils', () => {
 
             expect(sortedCards).toHaveLength(1);
             expect(sortedCards.at(0)?.cardID).toBe(1);
+        });
+    });
+
+    describe('getCardDescription', () => {
+        it('should return the correct card description for company card', () => {
+            const card: Card = {
+                accountID: 18439984,
+                bank: CONST.COMPANY_CARD.FEED_BANK_NAME.VISA,
+                cardID: 21310091,
+                cardName: '480801XXXXXX2554',
+                domainName: 'expensify-policy41314f4dc5ce25af.exfy',
+                fraud: 'none',
+                lastFourPAN: '2554',
+                lastUpdated: '',
+                lastScrape: '2024-11-27 11:00:53',
+                scrapeMinDate: '2024-10-17',
+                state: 3,
+            };
+            const description = getCardDescription(card);
+            expect(description).toBe('Visa - 2554');
+        });
+
+        it('should return the correct card description for Expensify card', () => {
+            const card: Card = {
+                accountID: 18439984,
+                bank: CONST.EXPENSIFY_CARD.BANK,
+                cardID: 21570657,
+                cardName: 'CREDIT CARD...5644',
+                domainName: 'expensify-policy17f617b9fe23d2f1.exfy',
+                fraud: 'none',
+                lastFourPAN: '',
+                lastScrape: '',
+                lastUpdated: '',
+                state: 2,
+            };
+            const description = getCardDescription(card);
+            expect(description).toBe('Expensify Card');
         });
     });
 });
