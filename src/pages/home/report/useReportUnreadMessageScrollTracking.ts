160a4b52377cc6dec65dbc9c1451860aee2ab04a
FitseTLT <engfitsum@gmail.com>
2025-07-31 16:04:32 +0300

    fix ts

--- src/pages/home/report/useReportUnreadMessageScrollTracking.ts
+++ src/pages/home/report/useReportUnreadMessageScrollTracking.ts
@@ -61,7 +61,7 @@ export default function useReportUnreadMessageScrollTracking({reportID, currentV
     };
 
     const onViewableItemsChanged = useCallback(({viewableItems}: {viewableItems: ViewToken[]; changed: ViewToken[]}) => {
-        const viewableIndexes = viewableItems.map((viewableItem) => viewableItem.index).filter((value) => typeof value === 'number');
+        const viewableIndexes = viewableItems.map((viewableItem) => viewableItem.index).filter((value) => typeof value === 'number') as number[];
         const maxIndex = Math.max(...viewableIndexes);
         const minIndex = Math.min(...viewableIndexes);
         const unreadActionIndex = ref.current.unreadMarkerReportActionIndex;
