263f1723473132573fd9ed14c0b186774c51c1ef
allgandalf <whogandalf@gmail.com>
2025-07-31 16:00:31 +0530

    remove session onyx.connect

--- src/libs/actions/Delegate.ts
+++ src/libs/actions/Delegate.ts
@@ -20,11 +20,6 @@ import {getCurrentUserAccountID} from './Report';
 import updateSessionAuthTokens from './Session/updateSessionAuthTokens';
 import updateSessionUser from './Session/updateSessionUser';
 
-let credentials: Credentials = {};
-Onyx.connect({
-    key: ONYXKEYS.CREDENTIALS,
-    callback: (value) => (credentials = value ?? {}),
-});
 
 let stashedCredentials: Credentials = {};
 Onyx.connect({
@@ -73,12 +68,12 @@ const KEYS_TO_PRESERVE_DELEGATE_ACCESS = [
  * Connects the user as a delegate to another account.
  * Returns a Promise that resolves to true on success, false on failure, or undefined if not applicable.
  */
-function connect(email: string, delegatedAccess: OnyxEntry<DelegatedAccess>, isFromOldDot = false) {
+function connect(email: string, delegatedAccess: OnyxEntry<DelegatedAccess>, credentials: OnyxEntry<Credentials>, isFromOldDot = false) {
     if (!delegatedAccess?.delegators && !isFromOldDot) {
         return;
     }
 
-    Onyx.set(ONYXKEYS.STASHED_CREDENTIALS, credentials);
+    Onyx.set(ONYXKEYS.STASHED_CREDENTIALS, credentials ?? {});
     Onyx.set(ONYXKEYS.STASHED_SESSION, session);
 
     const previousAccountID = getCurrentUserAccountID();
