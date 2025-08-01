3a1af397352a6e6a8150627721f1f0874f2655a9
Gandalf <whogandalf@gmail.com>
2025-08-01 11:51:38 +0530

    add comment

--- src/libs/E2E/actions/waitForAppLoaded.ts
+++ src/libs/E2E/actions/waitForAppLoaded.ts
@@ -4,6 +4,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 // Once we get the sidebar loaded end mark we know that the app is ready to be used:
 export default function waitForAppLoaded(): Promise<void> {
     return new Promise((resolve) => {
+        // We have used `connectWithoutView` here because OnyxUpdates is not connected to any UI
         const connection = Onyx.connectWithoutView({
             key: ONYXKEYS.IS_SIDEBAR_LOADED,
             callback: (isSidebarLoaded) => {
