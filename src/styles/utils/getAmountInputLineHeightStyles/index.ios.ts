3fcbb594c14f1d8ff7a267f17caebe05669a7813
krishna2323 <belivethatkg@gmail.com>
2025-08-01 14:34:41 +0530

    fix: Android - Expense-Amount and currency are not on the same line when device font size is minimum.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

---
+++ src/styles/utils/getAmountInputLineHeightStyles/index.ios.ts
@@ -0,0 +1,7 @@
+import type AmountInputLineHeightStyle from './type';
+
+function getAmountInputLineHeightStyles(): AmountInputLineHeightStyle {
+    return {lineHeight: undefined};
+}
+
+export default getAmountInputLineHeightStyles;
