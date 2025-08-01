99162cdcf4160605c3326cfc92cdf1195d521f9e
Tomasz Lesniakiewicz <tlesniakiewicz@gmail.com>
2025-08-01 14:56:46 +0200

    remove remoteCacheProvider

--- rnef.config.mjs
+++ rnef.config.mjs
@@ -7,11 +7,12 @@ const isHybrid = process.env.IS_HYBRID_APP === 'true';
 
 /** @type {import('@rnef/config').Config} */
 export default {
-    remoteCacheProvider: providerGitHub({
-        owner: 'Expensify',
-        repository: 'App',
-        token: process.env.GITHUB_TOKEN,
-    }),
+    remoteCacheProvider: null,
+    // remoteCacheProvider: providerGitHub({
+    //     owner: 'Expensify',
+    //     repository: 'App',
+    //     token: process.env.GITHUB_TOKEN,
+    // }),
     bundler: pluginMetro(),
     platforms: {
         ios: platformIOS({sourceDir: isHybrid ? './Mobile-Expensify/iOS' : './ios'}),
