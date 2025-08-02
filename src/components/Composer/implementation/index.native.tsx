993ee291020a273c913d8df2d19f74df8ac26b00
Lorenzo Bloedow <dev@lorenzobloedow.com>
2025-08-02 12:22:49 -0300

    Remove outdated comment

--- src/components/Composer/implementation/index.native.tsx
+++ src/components/Composer/implementation/index.native.tsx
@@ -31,7 +31,6 @@ function Composer(
         style,
         // On native layers we like to have the Text Input not focused so the
         // user can read new chats without the keyboard in the way of the view.
-        // On Android the selection prop is required on the TextInput but this prop has issues on IOS
         selection,
         value,
         isGroupPolicyReport = false,
