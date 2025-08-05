36d488080f60ce00e248069ad2521c81de261ef5
William Quayson <wgsquayson@gmail.com>
2025-08-05 18:58:49 -0300

    chore: remove unecessary onScroll usage

--- src/components/FlatList/index.ios.tsx
+++ src/components/FlatList/index.ios.tsx
@@ -3,14 +3,12 @@ import React, {forwardRef, useCallback, useState} from 'react';
 import type {FlatListProps} from 'react-native';
 import {FlatList} from 'react-native';
 import KeyboardDismissableFlatList from '@components/KeyboardDismissableFlatList';
-import {useKeyboardDismissableFlatListContext} from '@components/KeyboardDismissableFlatList/KeyboardDismissableFlatListContext';
 import type {AdditionalFlatListProps} from '.';
 
 // On iOS, we have to unset maintainVisibleContentPosition while the user is scrolling to prevent jumping to the beginning issue
 function CustomFlatList<T>(props: FlatListProps<T> & AdditionalFlatListProps, ref: ForwardedRef<FlatList>) {
     const {maintainVisibleContentPosition: originalMaintainVisibleContentPosition, withAnimatedKeyboardHandler, ...rest} = props;
     const [isScrolling, setIsScrolling] = useState(false);
-    const {onScroll} = useKeyboardDismissableFlatListContext();
 
     const handleScrollBegin = useCallback(() => {
         setIsScrolling(true);
@@ -27,7 +25,6 @@ function CustomFlatList<T>(props: FlatListProps<T> & AdditionalFlatListProps, re
             <KeyboardDismissableFlatList
                 // eslint-disable-next-line react/jsx-props-no-spreading
                 {...rest}
-                onScroll={onScroll}
                 ref={ref}
                 maintainVisibleContentPosition={maintainVisibleContentPosition}
                 onMomentumScrollBegin={handleScrollBegin}
