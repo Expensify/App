56fd68c0dc3e0ed0727e2205afa80335eb585210
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-08-05 02:18:00 +0100

    close RHP before navigating to statement close date page

--- src/pages/workspace/companyCards/BankConnection/index.native.tsx
+++ src/pages/workspace/companyCards/BankConnection/index.native.tsx
@@ -118,6 +118,7 @@ function BankConnection({policyID: policyIDFromProps, feed, route}: BankConnecti
             // Direct feeds (except those added via Plaid) are created with default statement period end date.
             // Redirect the user to set a custom date.
             if (policyID && !isPlaid) {
+                Navigation.closeRHPFlow();
                 Navigation.navigate(ROUTES.WORKSPACE_COMPANY_CARDS_SETTINGS_STATEMENT_CLOSE_DATE.getRoute(policyID));
             } else {
                 Navigation.goBack(ROUTES.WORKSPACE_COMPANY_CARDS.getRoute(policyID));
--- src/pages/workspace/companyCards/BankConnection/index.tsx
+++ src/pages/workspace/companyCards/BankConnection/index.tsx
@@ -145,13 +145,14 @@ function BankConnection({policyID: policyIDFromProps, feed, route}: BankConnecti
             if (newFeed) {
                 updateSelectedFeed(newFeed, policyID);
             }
-            Navigation.closeRHPFlow();
 
             // Direct feeds (except those added via Plaid) are created with default statement period end date.
             // Redirect the user to set a custom date.
             if (policyID && !isPlaid) {
+                Navigation.closeRHPFlow();
                 Navigation.navigate(ROUTES.WORKSPACE_COMPANY_CARDS_SETTINGS_STATEMENT_CLOSE_DATE.getRoute(policyID));
             } else {
+                Navigation.closeRHPFlow();
                 Navigation.navigate(ROUTES.WORKSPACE_COMPANY_CARDS.getRoute(policyID), {forceReplace: true});
             }
             return;
