61ada5ab60482827cb89fb0b6d4cae0ec58f4fb1
William Quayson <wgsquayson@gmail.com>
2025-08-04 21:12:54 -0300

    chore: remove unecessary onScroll

--- src/pages/home/report/ReportActionsList.tsx
+++ src/pages/home/report/ReportActionsList.tsx
@@ -807,7 +807,6 @@ function ReportActionsList({
                     onEndReached={onEndReached}
                     onStartReachedThreshold={ON_SCROLL_TO_LIMITS_THRESHOLD}
                     onEndReachedThreshold={ON_SCROLL_TO_LIMITS_THRESHOLD}
-                    onScroll={onScroll}
                     renderScrollComponent={renderScrollComponent}
                     contentContainerStyle={styles.chatContentScrollView}
                     keyExtractor={keyExtractor}
