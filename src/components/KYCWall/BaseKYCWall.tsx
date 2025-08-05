aff8da3e159cdd8cfd42f7ba0c82b1eeab85d0bb
Getabalew <getudesta4@gmail.com>
2025-08-05 19:39:10 +0300

    fix: lint

--- src/components/KYCWall/BaseKYCWall.tsx
+++ src/components/KYCWall/BaseKYCWall.tsx
@@ -158,7 +158,7 @@ function KYCWall({
                 Navigation.navigate(bankAccountRoute);
             }
         },
-        [addBankAccountRoute, addDebitCardRoute, chatReport, iouReport, onSelectPaymentMethod, formatPhoneNumber, lastPaymentMethod],
+        [addBankAccountRoute, addDebitCardRoute, chatReport, iouReport, onSelectPaymentMethod, formatPhoneNumber, lastPaymentMethod, allReports],
     );
 
     /**
