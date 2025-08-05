902dd88fdd090d4cee967e5d3511ee800af614b5
Tomasz Lesniakiewicz <tlesniakiewicz@gmail.com>
2025-08-05 14:19:11 +0200

    add expensify repo details

--- rnef.config.mjs
+++ rnef.config.mjs
@@ -8,8 +8,8 @@ const isHybrid = process.env.IS_HYBRID_APP === 'true';
 /** @type {import('@rnef/config').Config} */
 export default {
     remoteCacheProvider: providerGitHub({
-        owner: 'callstack-internal',
-        repository: 'Expensify-App',
+        owner: 'Expensify',
+        repository: 'App',
         token: process.env.GITHUB_TOKEN,
     }),
     bundler: pluginMetro(),
