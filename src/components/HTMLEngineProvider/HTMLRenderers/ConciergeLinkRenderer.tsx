dd46bc624ee934f9fd8325024207da9e90ffc223
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-08-05 10:42:55 +0530

    Stable ref

--- src/components/HTMLEngineProvider/HTMLRenderers/ConciergeLinkRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/ConciergeLinkRenderer.tsx
@@ -5,9 +5,16 @@ import {TNodeChildrenRenderer} from 'react-native-render-html';
 import * as HTMLEngineUtils from '@components/HTMLEngineProvider/htmlEngineUtils';
 import Text from '@components/Text';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {navigateToConciergeChat} from '@userActions/Report';
+import {navigateToConciergeChat as navigateToConciergeChatAction} from '@userActions/Report';
 
 type ConciergeLinkRendererProps = CustomRendererProps<TText | TPhrasing>;
+
+/**
+ * Simple wrapper to create a stable reference without passing event args to navigation function.
+ */
+function navigateToConciergeChat() {
+    navigateToConciergeChatAction();
+}
 
 function ConciergeLinkRenderer({tnode}: ConciergeLinkRendererProps) {
     const styles = useThemeStyles();
@@ -28,7 +35,7 @@ function ConciergeLinkRenderer({tnode}: ConciergeLinkRendererProps) {
     return (
         <Text
             style={linkStyle}
-            onPress={() => navigateToConciergeChat()}
+            onPress={navigateToConciergeChat}
             suppressHighlighting
         >
             <TNodeChildrenRenderer tnode={tnode} />
