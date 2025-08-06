8cb4d16591bb181fd9bd15ab5bb8f16b57b96ace
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-08-06 14:51:35 +0200

    make size of receipt images flexible

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -5754,6 +5754,26 @@ const styles = (theme: ThemeColors) =>
             zIndex: 15,
         },
 
+        fullHeight: {
+            height: '100%',
+        },
+
+        heightAuto: {
+            height: 'auto',
+        },
+
+        wideRHPMoneyRequestReceiptViewContainer: {
+            backgroundColor: theme.appBG,
+            width: variables.sideBarWidth,
+            height: '100%',
+            borderRightWidth: 1,
+            borderColor: theme.border,
+        },
+        wideRHPMoneyRequestReceiptViewScrollView: {
+            paddingTop: 12,
+            minHeight: '100%',
+        },
+
         smallMoreButton: {
             marginRight: 12,
         },
