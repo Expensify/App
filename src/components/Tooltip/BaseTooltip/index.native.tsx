5fda30adcc9acd8058c3a37306668fecc3a04c1e
Jakub Kaliński <kubakalinski2@gmail.com>
2025-08-06 11:40:23 +0200

    Small change to ChildrenProps

--- src/components/Tooltip/BaseTooltip/index.native.tsx
+++ src/components/Tooltip/BaseTooltip/index.native.tsx
@@ -2,7 +2,7 @@ import type ChildrenProps from '@src/types/utils/ChildrenProps';
 
 // We can't use the common component for the Tooltip as Web implementation uses DOM specific method
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function Tooltip({children, ref}: ChildrenProps) {
+function Tooltip({children, ref}: ChildrenProps & {ref?: unknown}) {
     return children;
 }
 
--- src/types/utils/ChildrenProps.ts
+++ src/types/utils/ChildrenProps.ts
@@ -3,9 +3,6 @@ import type {ReactNode} from 'react';
 type ChildrenProps = {
     /** Rendered child component */
     children: ReactNode;
-
-    /** Reference to the outer element */
-    ref?: unknown
 };
 
 export default ChildrenProps;
