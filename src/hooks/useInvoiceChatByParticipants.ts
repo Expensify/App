b1040b54791695a426917b3f41ad72b5e8538e34
Dysto coder <dystovskycoder@gmail.com>
2025-08-07 18:11:24 +0300

    use useMemo instead of useEffect

--- src/hooks/useInvoiceChatByParticipants.ts
+++ src/hooks/useInvoiceChatByParticipants.ts
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useMemo} from 'react';
 import type {OnyxEntry} from 'react-native-onyx';
 import {isArchivedNonExpenseReport, isArchivedReport, isInvoiceRoom} from '@libs/ReportUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -15,15 +15,11 @@ const reportNameValuePairsSelector = (reportNameValuePairs: OnyxEntry<ReportName
     }) as ReportNameValuePairsSelector;
 
 function useInvoiceChatByParticipants(receiverID: string | number | undefined, receiverType: InvoiceReceiverType, policyID?: string): OnyxEntry<Report> {
-    const [invoiceReport, setInvoiceReport] = useState<OnyxEntry<Report>>(undefined);
     const [allReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {canBeMissing: true});
-    const [aaallReportNameValuePair] = useOnyx(ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS, {canBeMissing: true});
     const [allReportNameValuePair] = useOnyx(ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS, {canBeMissing: true, selector: (c) => mapOnyxCollectionItems(c, reportNameValuePairsSelector)});
 
-    console.log(Object.values(aaallReportNameValuePair ?? {})?.length, Object.values(allReportNameValuePair ?? {})?.length);
-
-    useEffect(() => {
-        const existingInvoiceReport = Object.values(allReports ?? {}).find((report) => {
+    const invoiceReport = useMemo(() => {
+        return Object.values(allReports ?? {}).find((report) => {
             const reportNameValuePairs = allReportNameValuePair?.[`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${report?.reportID}`];
             const isReportArchived = isArchivedReport(reportNameValuePairs);
             if (!report || !isInvoiceRoom(report) || isArchivedNonExpenseReport(report, isReportArchived)) {
@@ -38,8 +34,6 @@ function useInvoiceChatByParticipants(receiverID: string | number | undefined, r
 
             return report.policyID === policyID && isSameReceiver;
         });
-
-        setInvoiceReport(existingInvoiceReport);
     }, [receiverID, receiverType, policyID, allReports, allReportNameValuePair]);
 
     return invoiceReport;
