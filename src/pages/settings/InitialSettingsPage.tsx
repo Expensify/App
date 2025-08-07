190d600d1bd6868ce927c83bd8d9f9f667c40fb6
DylanDylann <dylandylan9112@gmail.com>
2025-08-07 11:14:31 +0700

    chore

--- src/pages/settings/InitialSettingsPage.tsx
+++ src/pages/settings/InitialSettingsPage.tsx
@@ -199,7 +199,20 @@ function InitialSettingsPage({currentUserPersonalDetails}: InitialSettingsPagePr
             sectionTranslationKey: 'initialSettingsPage.account',
             items,
         };
-    }, [loginList, privatePersonalDetails, vacationDelegate, session?.email, walletBrickRoadIndicator, hasActivatedWallet, userWallet?.currentBalance, subscriptionPlan, styles.accountSettingsSectionContainer, styles.badgeSuccess, privateSubscription?.errors, freeTrialText]);
+    }, [
+        loginList,
+        privatePersonalDetails,
+        vacationDelegate,
+        session?.email,
+        walletBrickRoadIndicator,
+        hasActivatedWallet,
+        userWallet?.currentBalance,
+        subscriptionPlan,
+        styles.accountSettingsSectionContainer,
+        styles.badgeSuccess,
+        privateSubscription?.errors,
+        freeTrialText,
+    ]);
 
     /**
      * Return a list of menu items data for general section
