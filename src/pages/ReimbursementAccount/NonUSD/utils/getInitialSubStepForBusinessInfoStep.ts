da992322cc3c9a591bb9ccac1ad87127a484a4b3
Michał Muzyk <mimuzyk@gmail.com>
2025-08-08 13:46:26 +0200

    fix: Validation loophole in global reimbursements

--- src/pages/ReimbursementAccount/NonUSD/utils/getInitialSubStepForBusinessInfoStep.ts
+++ src/pages/ReimbursementAccount/NonUSD/utils/getInitialSubStepForBusinessInfoStep.ts
@@ -1,41 +1,86 @@
+import {Str} from 'expensify-common';
+import {
+    isValidAddress,
+    isValidCompanyName,
+    isValidEmail,
+    isValidPhoneInternational,
+    isValidRegistrationNumber,
+    isValidTaxIDEINNumber,
+    isValidWebsite,
+    isValidZipCode,
+    isValidZipCodeInternational,
+} from '@libs/ValidationUtils';
 import CONST from '@src/CONST';
 import INPUT_IDS from '@src/types/form/ReimbursementAccountForm';
 
 const BUSINESS_INFO_STEP_KEYS = INPUT_IDS.ADDITIONAL_DATA.CORPAY;
 
+function isMissingValidCompanyName(companyName: string) {
+    return companyName === '' || !isValidCompanyName(companyName);
+}
+
+function isMissingValidWebsite(website: string) {
+    return website === '' || !isValidWebsite(Str.sanitizeURL(website, CONST.COMPANY_WEBSITE_DEFAULT_SCHEME));
+}
+
+function isMissingValidAddress(street: string, city: string, postalCode: string, state: string, country: string) {
+    return (
+        street === '' ||
+        city === '' ||
+        postalCode === '' ||
+        country === '' ||
+        ((country === CONST.COUNTRY.US || country === CONST.COUNTRY.CA) && state === '') ||
+        (country === '' && state === '') ||
+        !isValidAddress(street) ||
+        (country === CONST.COUNTRY.US && !isValidZipCodeInternational(postalCode))
+    );
+}
+
+function isMissingValidBusinessContactInformation(contactNumber: string, contactEmail: string) {
+    return contactNumber === '' || contactEmail === '' || !isValidPhoneInternational(contactNumber) || !isValidEmail(contactEmail);
+}
+
+function isMissingValidRegistrationNumber(registrationNumber: string, country: string) {
+    return registrationNumber === '' || !isValidRegistrationNumber(registrationNumber, country as keyof typeof CONST.COUNTRY);
+}
+
+function isMissingValidTaxIDEINNumber(taxIDEINNumber: string, country: string) {
+    return taxIDEINNumber === '' || !isValidTaxIDEINNumber(taxIDEINNumber, country as keyof typeof CONST.COUNTRY);
+}
+
 /**
  * Returns the initial subStep for the Business info step based on already existing data
  */
 function getInitialSubStepForBusinessInfoStep(data: Record<string, string>): number {
-    if (data[BUSINESS_INFO_STEP_KEYS.COMPANY_NAME] === '') {
+    if (isMissingValidCompanyName(data[BUSINESS_INFO_STEP_KEYS.COMPANY_NAME])) {
         return 0;
     }
 
-    if (data[BUSINESS_INFO_STEP_KEYS.COMPANY_WEBSITE] === '') {
+    if (isMissingValidWebsite(data[BUSINESS_INFO_STEP_KEYS.COMPANY_WEBSITE])) {
         return 1;
     }
 
     if (
-        data[BUSINESS_INFO_STEP_KEYS.COMPANY_STREET] === '' ||
-        data[BUSINESS_INFO_STEP_KEYS.COMPANY_CITY] === '' ||
-        data[BUSINESS_INFO_STEP_KEYS.COMPANY_POSTAL_CODE] === '' ||
-        data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE] === '' ||
-        ((data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE] === CONST.COUNTRY.US || data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE] === CONST.COUNTRY.CA) &&
-            data[BUSINESS_INFO_STEP_KEYS.COMPANY_STATE] === '') ||
-        (data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE] === '' && data[BUSINESS_INFO_STEP_KEYS.COMPANY_STATE] === '')
+        isMissingValidAddress(
+            data[BUSINESS_INFO_STEP_KEYS.COMPANY_STREET],
+            data[BUSINESS_INFO_STEP_KEYS.COMPANY_CITY],
+            data[BUSINESS_INFO_STEP_KEYS.COMPANY_POSTAL_CODE],
+            data[BUSINESS_INFO_STEP_KEYS.COMPANY_STATE],
+            data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE],
+        )
     ) {
         return 2;
     }
 
-    if (data[BUSINESS_INFO_STEP_KEYS.BUSINESS_CONTACT_NUMBER] === '' || data[BUSINESS_INFO_STEP_KEYS.BUSINESS_CONFIRMATION_EMAIL] === '') {
+    if (isMissingValidBusinessContactInformation(data[BUSINESS_INFO_STEP_KEYS.BUSINESS_CONTACT_NUMBER], data[BUSINESS_INFO_STEP_KEYS.BUSINESS_CONFIRMATION_EMAIL])) {
         return 3;
     }
 
-    if (data[BUSINESS_INFO_STEP_KEYS.BUSINESS_REGISTRATION_INCORPORATION_NUMBER] === '') {
+    if (isMissingValidRegistrationNumber(data[BUSINESS_INFO_STEP_KEYS.BUSINESS_REGISTRATION_INCORPORATION_NUMBER], data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE])) {
         return 4;
     }
 
-    if (data[BUSINESS_INFO_STEP_KEYS.TAX_ID_EIN_NUMBER] === '') {
+    if (isMissingValidTaxIDEINNumber(data[BUSINESS_INFO_STEP_KEYS.TAX_ID_EIN_NUMBER], data[BUSINESS_INFO_STEP_KEYS.COMPANY_COUNTRY_CODE])) {
         return 5;
     }
 
