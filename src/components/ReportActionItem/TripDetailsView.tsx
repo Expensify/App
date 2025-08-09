3ed43a5e2d547c813aa6d329fb8095de12f70c11
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-08-09 15:03:55 +0530

    flight details update

--- src/components/ReportActionItem/TripDetailsView.tsx
+++ src/components/ReportActionItem/TripDetailsView.tsx
@@ -194,7 +194,7 @@ function TripDetailsView({tripRoomReport, shouldShowHorizontalRule, tripTransact
 
             switch (firstReservation?.type) {
                 case CONST.RESERVATION_TYPE.FLIGHT: {
-                    const destinationReservation = reservations.filter((reservation) => reservation.reservation.legId === 0).at(-1);
+                    const destinationReservation = reservations.filter((reservation) => reservation.reservation.legId === firstReservation.legId).at(-1);
                     if (!destinationReservation) {
                         return '';
                     }
