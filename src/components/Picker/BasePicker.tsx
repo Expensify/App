915eaa59b4b4bbe78715716c13c5eb1db8531d4d
Jakub Kaliński <kubakalinski2@gmail.com>
2025-08-11 18:14:12 +0200

    Removed forwardRef from BasePicker.tsx

--- src/components/Picker/BasePicker.tsx
+++ src/components/Picker/BasePicker.tsx
@@ -1,6 +1,6 @@
 import lodashDefer from 'lodash/defer';
-import type {ForwardedRef, ReactElement, ReactNode, RefObject} from 'react';
-import React, {forwardRef, useEffect, useImperativeHandle, useMemo, useRef, useState} from 'react';
+import type {ReactElement, ReactNode, RefObject} from 'react';
+import React, {useEffect, useImperativeHandle, useMemo, useRef, useState} from 'react';
 // eslint-disable-next-line no-restricted-imports
 import type {ScrollView} from 'react-native';
 import {View} from 'react-native';
@@ -14,7 +14,7 @@ import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import getOperatingSystem from '@libs/getOperatingSystem';
 import CONST from '@src/CONST';
-import type {BasePickerHandle, BasePickerProps} from './types';
+import type {BasePickerProps} from './types';
 
 type IconToRender = () => ReactElement;
 
@@ -38,8 +38,8 @@ function BasePicker<TPickerValue>(
         shouldShowOnlyTextWhenDisabled = true,
         onBlur = () => {},
         additionalPickerEvents = () => {},
+        ref,
     }: BasePickerProps<TPickerValue>,
-    ref: ForwardedRef<BasePickerHandle>,
 ) {
     const theme = useTheme();
     const styles = useThemeStyles();
@@ -231,4 +231,4 @@ function BasePicker<TPickerValue>(
 
 BasePicker.displayName = 'BasePicker';
 
-export default forwardRef(BasePicker);
+export default BasePicker;
