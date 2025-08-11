581f7a2cc965b3be9a3c67066f0718b54b932aca
nkdengineer <nkdengineer@outlook.com>
2025-08-11 15:37:36 +0700

    fix fail test

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -2516,6 +2516,7 @@ function buildOnyxDataForTrackExpense({
                 onyxMethod: Onyx.METHOD.MERGE,
                 key: `${ONYXKEYS.COLLECTION.REPORT}${chatReport.reportID}`,
                 value: {
+                    lastReadTime: actionableTrackExpenseWhisper.created,
                     lastVisibleActionCreated: actionableTrackExpenseWhisper.created,
                     lastMessageText: CONST.ACTIONABLE_TRACK_EXPENSE_WHISPER_MESSAGE,
                 },
