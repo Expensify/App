9388bf48063ff1c6c3cee4812ba3d26719884f69
Blazej Kustra <blazej.kustra@swmansion.com>
2025-08-12 16:34:54 +0200

    fix: prevent side panel actions when side panel NVP is undefined

--- src/components/SidePanel/index.tsx
+++ src/components/SidePanel/index.tsx
@@ -3,8 +3,14 @@ import useSidePanel from '@hooks/useSidePanel';
 import Help from './HelpModal';
 
 function SidePanel() {
-    const {isSidePanelTransitionEnded, shouldHideSidePanel, sidePanelTranslateX, shouldHideSidePanelBackdrop, closeSidePanel} = useSidePanel();
+    const {sidePanelNVP, isSidePanelTransitionEnded, shouldHideSidePanel, sidePanelTranslateX, shouldHideSidePanelBackdrop, closeSidePanel} = useSidePanel();
 
+    // Side panel can't be displayed if NVP is undefined
+    if (!sidePanelNVP) {
+        return null;
+    }
+
+    // Hide side panel once animation ends
     if (isSidePanelTransitionEnded && shouldHideSidePanel) {
         return null;
     }
