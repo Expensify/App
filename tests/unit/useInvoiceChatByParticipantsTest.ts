4de59fd0d3145fb66422c24ba481f4bef6cd5786
Dysto coder <dystovskycoder@gmail.com>
2025-08-13 14:15:47 +0300

    rename invoiceReceiver

--- tests/unit/useInvoiceChatByParticipantsTest.ts
+++ tests/unit/useInvoiceChatByParticipantsTest.ts
@@ -10,7 +10,7 @@ const accountID = 12345;
 const mockPolicyID = '123';
 const activeReportID = 12;
 const archivedReportID = 233;
-const invoiceReceiver: InvoiceReceiver = {
+const activeIndividualInvoiceReceiver: InvoiceReceiver = {
     accountID,
     type: CONST.REPORT.INVOICE_RECEIVER_TYPE.INDIVIDUAL,
 };
@@ -23,7 +23,7 @@ const archivedReportNameValuePairs = {
     private_isArchived: '12-3-2024',
 };
 
-const mockActiveIndividualInvoiceReport = {...createInvoiceRoom(activeReportID), invoiceReceiver, policyID: mockPolicyID};
+const mockActiveIndividualInvoiceReport = {...createInvoiceRoom(activeReportID), invoiceReceiver: activeIndividualInvoiceReceiver, policyID: mockPolicyID};
 const mockArchivedIndividualInvoiceReport = {...createInvoiceRoom(archivedReportID), invoiceReceiver: archivedIndividualReportInvoiceReceiver, policyID: mockPolicyID};
 
 describe('useInvoiceChatByParticipants', () => {
@@ -42,7 +42,7 @@ describe('useInvoiceChatByParticipants', () => {
 
             // When sending invoice to the same receiver from FAB flow (outside of invoice room)
             const {result} = renderHook(({receiverID, receiverType, policyID}) => useInvoiceChatByParticipants(receiverID, receiverType, policyID), {
-                initialProps: {receiverID: accountID, receiverType: invoiceReceiver.type, policyID: mockPolicyID},
+                initialProps: {receiverID: accountID, receiverType: activeIndividualInvoiceReceiver.type, policyID: mockPolicyID},
             });
 
             // Then invoice should be sent in the same invoice room
