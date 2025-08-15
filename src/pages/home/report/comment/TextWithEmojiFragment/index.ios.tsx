8c722fd8356f0b62222ff0e13d2dee595a2b1b4e
Samran Ahmed <samranahmed03@gmail.com>
2025-08-16 00:16:51 +0500

    revert TextWithEmojiFragment changes for ios

--- src/pages/home/report/comment/TextWithEmojiFragment/index.ios.tsx
+++ src/pages/home/report/comment/TextWithEmojiFragment/index.ios.tsx
@@ -3,45 +3,27 @@ import {View} from 'react-native';
 import Text from '@components/Text';
 import useThemeStyles from '@hooks/useThemeStyles';
 import convertToLTR from '@libs/convertToLTR';
-import {containsCustomEmoji, containsOnlyCustomEmoji, splitTextWithEmojis} from '@libs/EmojiUtils';
+import {containsCustomEmoji, splitTextWithEmojis} from '@libs/EmojiUtils';
 import type TextWithEmojiFragmentProps from './types';
 
-function TextWithEmojiFragment({message = '', style, alignCustomEmoji = false}: TextWithEmojiFragmentProps) {
+function TextWithEmojiFragment({message = '', style}: TextWithEmojiFragmentProps) {
     const styles = useThemeStyles();
     const processedTextArray = useMemo(() => splitTextWithEmojis(message), [message]);
 
     return (
         <Text style={style}>
-            {processedTextArray.map(({text, isEmoji}, index) => {
-                const charsBeforeEmoji = processedTextArray.slice(0, index).reduce((sum, part) => sum + part.text.length, 0);
-
-                if (isEmoji) {
-                    if (alignCustomEmoji && containsOnlyCustomEmoji(text)) {
-                        return (
-                            <Text
-                                // eslint-disable-next-line react/no-array-index-key
-                                key={index}
-                                style={[styles.emojisWithTextFontSizeAligned, styles.customEmojiFontAlignment]}
-                            >
-                                {text}
-                            </Text>
-                        );
-                    }
-                    if (alignCustomEmoji && charsBeforeEmoji > 50) {
-                        return null;
-                    }
-
-                    return (
-                        <View
-                            // eslint-disable-next-line react/no-array-index-key
-                            key={index}
-                        >
-                            <Text style={[styles.emojisWithTextFontSizeAligned, containsCustomEmoji(text) && styles.customEmojiFontAlignment]}>{text}</Text>
-                        </View>
-                    );
-                }
-                return convertToLTR(text);
-            })}
+            {processedTextArray.map(({text, isEmoji}, index) =>
+                isEmoji ? (
+                    <View
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={index}
+                    >
+                        <Text style={[styles.emojisWithTextFontSizeAligned, containsCustomEmoji(text) && styles.customEmojiFontAlignment]}>{text}</Text>
+                    </View>
+                ) : (
+                    convertToLTR(text)
+                ),
+            )}
         </Text>
     );
 }
