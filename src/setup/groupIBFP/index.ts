93246cc25babd2986f2d16da21a6a3a44ecb599b
Cristi Paval <cristi@expensify.com>
2025-08-16 01:54:35 +0300

    Simplify Group IB FP initialization

--- src/setup/groupIBFP/index.ts
+++ src/setup/groupIBFP/index.ts
@@ -1,7 +1,3 @@
 import GroupIBFPUtils from '@libs/GroupIBFPUtils';
 
-export default function initGroupIB(): Promise<any | undefined> {
-    return GroupIBFPUtils.init();
-}
-
-initGroupIB();
+GroupIBFPUtils.init();
