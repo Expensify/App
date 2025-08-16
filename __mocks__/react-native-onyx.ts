cd043101da1227c3a1865dd0cae97f0b1b5e6c39
DylanDylann <dylandylan9112@gmail.com>
2025-08-16 11:17:31 +0700

    remove withOnyx

--- __mocks__/react-native-onyx.ts
+++ __mocks__/react-native-onyx.ts
@@ -6,7 +6,7 @@
 /* eslint-disable rulesdir/prefer-onyx-connect-in-libs */
 import type {ConnectOptions, OnyxKey} from 'react-native-onyx';
 // eslint-disable-next-line no-restricted-imports
-import Onyx, {useOnyx, withOnyx} from 'react-native-onyx';
+import Onyx, {useOnyx} from 'react-native-onyx';
 
 let connectCallbackDelay = 0;
 function addDelayToConnectCallback(delay: number) {
@@ -41,4 +41,4 @@ const reactNativeOnyxMock: ReactNativeOnyxMock = {
 };
 
 export default reactNativeOnyxMock;
-export {withOnyx, useOnyx};
+export {useOnyx};
