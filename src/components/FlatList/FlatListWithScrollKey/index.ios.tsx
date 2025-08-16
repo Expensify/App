fab51da93bbc069c3963fe67d5cdc6b5fd580dd6
dmkt9 <demoskratos9x@gmail.com>
2025-08-16 16:25:25 +0700

    fix import

--- src/components/FlatList/FlatListWithScrollKey/index.ios.tsx
+++ src/components/FlatList/FlatListWithScrollKey/index.ios.tsx
@@ -3,7 +3,7 @@ import React, {forwardRef, useCallback, useEffect, useRef, useState} from 'react
 import type {FlatListProps, LayoutChangeEvent, ListRenderItem, ListRenderItemInfo, FlatList as RNFlatList} from 'react-native';
 import {InteractionManager} from 'react-native';
 import useFlatListScrollKey from '@hooks/useFlatListScrollKey';
-import FlatList from '.';
+import FlatList from '..';
 
 type FlatListWithScrollKeyProps<T> = Omit<FlatListProps<T>, 'data' | 'initialScrollIndex'> & {
     data: T[];
@@ -72,6 +72,9 @@ function FlatListWithScrollKey<T>(props: FlatListWithScrollKeyProps<T>, ref: For
         (w: number, h: number) => {
             onContentSizeChange?.(w, h);
 
+            if (!initialScrollKey) {
+                return;
+            }
             // Since the ListHeaderComponent is only rendered after the data has finished rendering, iOS locks the entire current viewport.
             // As a result, the viewport does not automatically scroll down to fill the gap at the bottom.
             // We will check during the initial render (isInitialData === true). If the content height is less than the layout height,
@@ -87,7 +90,7 @@ function FlatListWithScrollKey<T>(props: FlatListWithScrollKeyProps<T>, ref: For
                 shouldScrollToEndRef.current = true;
             }
         },
-        [onContentSizeChange, isInitialData],
+        [onContentSizeChange, isInitialData, initialScrollKey],
     );
 
     return (
