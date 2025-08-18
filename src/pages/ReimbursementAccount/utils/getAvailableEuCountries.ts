accbeca3585f3d23eb3b540ec88c0b13772d4ef4
narefyev91 <n.arefyev91@gmail.com>
2025-08-18 12:32:37 +0300

    Update src/pages/ReimbursementAccount/utils/getAvailableEuCountries.ts
    Co-authored-by: Maria D'Costa <mariahcdcosta@gmail.com>

--- src/pages/ReimbursementAccount/utils/getAvailableEuCountries.ts
+++ src/pages/ReimbursementAccount/utils/getAvailableEuCountries.ts
@@ -1,6 +1,6 @@
 import CONST from '@src/CONST';
 
-const specificSet = new Set<string>(CONST.EXPENSIFY_EU_UK_SUPPORT_COUNTRIES);
+const euUkSupportedCountrySet = new Set<string>(CONST.EXPENSIFY_EU_UK_SUPPORT_COUNTRIES);
 const europeanCountries = Object.entries(CONST.EUROPEAN_UNION_COUNTRIES_WITH_GB);
 
 export default function getAvailableEuCountries(shouldAllowChange?: boolean, isEuCurrencySupported?: boolean): Record<string, string> {
