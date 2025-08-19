0eb3a204d8fbc7093b81c563ba0d6776faabc96b
Jan Musiał <jan.musial@swmansion.com>
2025-08-19 16:20:35 +0200

    prettier fix

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -346,7 +346,10 @@ const ROUTES = {
     SETTINGS_2FA_VERIFY_ACCOUNT: {
         route: 'settings/security/two-factor-auth/verify-account',
         getRoute: (backTo?: string, forwardTo?: string) =>
-            getUrlWithBackToParam(forwardTo ? `settings/security/two-factor-auth/verify-account?forwardTo=${encodeURIComponent(forwardTo)}` : 'settings/security/two-factor-auth/verify-account', backTo),
+            getUrlWithBackToParam(
+                forwardTo ? `settings/security/two-factor-auth/verify-account?forwardTo=${encodeURIComponent(forwardTo)}` : 'settings/security/two-factor-auth/verify-account',
+                backTo,
+            ),
     },
     SETTINGS_2FA_VERIFY: {
         route: 'settings/security/two-factor-auth/verify',
