a71c0acc2cc9b4637e53efa712c78606b046ce9a
Sachin Chavda <Sachin.chavda.1024@gmail.com>
2025-08-20 17:43:10 +0530

    stop separating selected and non selected options

--- src/hooks/useWorkspaceList.ts
+++ src/hooks/useWorkspaceList.ts
@@ -62,7 +62,7 @@ function useWorkspaceList({policies, currentUserLogin, selectedPolicyIDs, search
 
     const filteredAndSortedUserWorkspaces = useMemo<WorkspaceListItem[]>(
         () => tokenizedSearch(usersWorkspaces, searchTerm, (policy) => [policy.text]).sort((policy1, policy2) => localeCompare(policy1.text, policy2.text)),
-        [searchTerm, usersWorkspaces, selectedPolicyIDs, localeCompare],
+        [searchTerm, usersWorkspaces, localeCompare],
     );
 
     const sections = useMemo(() => {
