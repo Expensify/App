cb773de02230677d4fbd926708a09550c0e6eba2
Adam Grzybowski <adam.grzybowski@swmansion.com>
2025-08-20 19:03:17 +0200

    fix wrong report for receipt view

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -875,7 +875,7 @@ function ReportScreen({route, navigation}: ReportScreenProps) {
                                         <ScrollView contentContainerStyle={styles.wideRHPMoneyRequestReceiptViewScrollView}>
                                             <MoneyRequestReceiptView
                                                 allReports={allReports}
-                                                report={report}
+                                                report={transactionThreadReport ?? report}
                                                 fillSpace
                                             />
                                         </ScrollView>
