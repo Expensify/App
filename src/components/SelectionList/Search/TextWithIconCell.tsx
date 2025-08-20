9f561d47615ad1ac4227aa0cc5c66c9fefb28d87
thelullabyy <the.lullaby2@hotmail.com>
2025-08-20 18:13:36 +0700

    fix lint

--- src/components/SelectionList/Search/TextWithIconCell.tsx
+++ src/components/SelectionList/Search/TextWithIconCell.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import {View} from 'react-native';
 import type {StyleProp, TextStyle} from 'react-native';
 import Icon from '@components/Icon';
-import Text from '@components/Text';
-import Tooltip from '@components/Tooltip';
+import TextWithTooltip from '@components/TextWithTooltip';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import type IconAsset from '@src/types/utils/IconAsset';
-import TextWithTooltip from '@components/TextWithTooltip';
 
 type TextWithIconCellProps = {
     icon: IconAsset;
@@ -26,17 +24,17 @@ export default function TextWithIconCell({icon, text, showTooltip, textStyle}: T
 
     return (
         <View style={[styles.flexRow, styles.flexShrink1, styles.gap1]}>
-        <Icon
-            src={icon}
-            fill={theme.icon}
-            height={12}
-            width={12}
-        />
-        <TextWithTooltip
-            text={text}
-            shouldShowTooltip={showTooltip}
-            style={[styles.optionDisplayName, styles.label, styles.pre, styles.justifyContentCenter, styles.textMicro, styles.textSupporting, styles.flexShrink1, textStyle]}
-        />
-    </View>
+            <Icon
+                src={icon}
+                fill={theme.icon}
+                height={12}
+                width={12}
+            />
+            <TextWithTooltip
+                text={text}
+                shouldShowTooltip={showTooltip}
+                style={[styles.optionDisplayName, styles.label, styles.pre, styles.justifyContentCenter, styles.textMicro, styles.textSupporting, styles.flexShrink1, textStyle]}
+            />
+        </View>
     );
 }
