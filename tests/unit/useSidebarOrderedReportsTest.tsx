cb1fe71b9c5dc1b44b11eff6baf1bf16fa8fc91f
Github <olimpia.zurek@gmail.com>
2025-08-20 12:56:13 +0200

    fix prettier and tests

--- tests/unit/useSidebarOrderedReportsTest.tsx
+++ tests/unit/useSidebarOrderedReportsTest.tsx
@@ -184,6 +184,7 @@ describe('useSidebarOrderedReports', () => {
             expect.any(Function), // localeCompare
             expect.any(Object), // reportNameValuePairs
             expect.any(Object), // reportAttributes
+            expect.any(Object), // drafts
         );
     });
 
