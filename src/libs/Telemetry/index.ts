92cfb6bd8e612f3fdb77efa78d1ee71ec0afd6f2
Adam Horodyski <ad.horodyski@gmail.com>
2025-08-20 09:55:40 +0200

    chore: formatting

--- src/libs/Telemetry/index.ts
+++ src/libs/Telemetry/index.ts
@@ -1,20 +1,10 @@
-import { Platform } from "react-native";
-import * as api from "@opentelemetry/api";
-import {
-  defaultResource,
-  resourceFromAttributes,
-} from "@opentelemetry/resources";
-import {
-  MeterProvider,
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from "@opentelemetry/sdk-metrics";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
-import {
-  ATTR_SERVICE_NAME,
-  ATTR_SERVICE_VERSION,
-} from "@opentelemetry/semantic-conventions";
-import { isDevelopment } from "@libs/Environment/Environment";
+import * as api from '@opentelemetry/api';
+import {OTLPMetricExporter} from '@opentelemetry/exporter-metrics-otlp-http';
+import {defaultResource, resourceFromAttributes} from '@opentelemetry/resources';
+import {ConsoleMetricExporter, MeterProvider, PeriodicExportingMetricReader} from '@opentelemetry/sdk-metrics';
+import {ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION} from '@opentelemetry/semantic-conventions';
+import {Platform} from 'react-native';
+import {isDevelopment} from '@libs/Environment/Environment';
 import pkg from '../../../package.json';
 
 // Resource
@@ -22,37 +12,34 @@ import pkg from '../../../package.json';
 /* eslint-disable @typescript-eslint/naming-convention */
 const resource = defaultResource().merge(
     resourceFromAttributes({
-      [ATTR_SERVICE_NAME]: pkg.name,
-      [ATTR_SERVICE_VERSION]: pkg.version,
-      "deployment.environment.name":
-        process.env.NODE_ENV,
-      "os.name": Platform.OS, // ATTR_OS_NAME
-      "os.version": Platform.Version, // ATTR_OS_VERSION
-    })
-  );
+        [ATTR_SERVICE_NAME]: pkg.name,
+        [ATTR_SERVICE_VERSION]: pkg.version,
+        'deployment.environment.name': process.env.NODE_ENV,
+        'os.name': Platform.OS, // ATTR_OS_NAME
+        'os.version': Platform.Version, // ATTR_OS_VERSION
+    }),
+);
 
 // Metrics
 
 const logMetricReader = isDevelopment()
     ? new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
+          exporter: new ConsoleMetricExporter(),
       })
     : null;
 
 const otlpMetricReader = new PeriodicExportingMetricReader({
-  exporter: new OTLPMetricExporter({
-    url: `http://localhost:4318/v1/metrics`,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }),
+    exporter: new OTLPMetricExporter({
+        url: `http://localhost:4318/v1/metrics`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }),
 });
 
 const meterProvider = new MeterProvider({
-  resource,
-  readers: [logMetricReader, otlpMetricReader].filter(
-    (reader) => reader !== null
-  ),
+    resource,
+    readers: [logMetricReader, otlpMetricReader].filter((reader) => reader !== null),
 });
 
 // SDK
@@ -67,4 +54,7 @@ const sdk = openTelemetrySDK();
 
 const meter = sdk.metrics.getMeter(pkg.name, pkg.version);
 
-export default { sdk, meter };
+export default {
+    sdk,
+    meter,
+};
