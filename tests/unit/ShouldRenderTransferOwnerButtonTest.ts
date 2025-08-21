a0232a4f85d314f14e75d1f7eeaa66bdd6bcae0a
allgandalf <whogandalf@gmail.com>
2025-08-21 23:22:31 +0530

    fix prettier

--- tests/unit/ShouldRenderTransferOwnerButtonTest.ts
+++ tests/unit/ShouldRenderTransferOwnerButtonTest.ts
@@ -1,6 +1,6 @@
+import type {OnyxEntry} from 'react-native-onyx';
 import shouldRenderTransferOwnerButton from '@libs/shouldRenderTransferOwnerButton';
 import type {FundList} from '@src/types/onyx';
-import type {OnyxEntry} from 'react-native-onyx';
 
 describe('shouldRenderTransferOwnerButton', () => {
     it('should return true if the user has debit card funds', () => {
@@ -16,7 +16,7 @@ describe('shouldRenderTransferOwnerButton', () => {
                 },
             },
         };
-        
+
         // eslint-disable-next-line testing-library/render-result-naming-convention
         const result = shouldRenderTransferOwnerButton(FUND_LIST);
         expect(result).toBe(true);
