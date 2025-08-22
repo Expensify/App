a3c52269890f36b55482c3e1aeab3605434ae79f
DylanDylann <dylandylan9112@gmail.com>
2025-08-22 12:17:40 +0700

    remove connect method

--- src/libs/Navigation/AppNavigator/AuthScreens.tsx
+++ src/libs/Navigation/AppNavigator/AuthScreens.tsx
@@ -121,7 +121,6 @@ function initializePusher() {
 let timezone: Timezone | null;
 let currentAccountID = -1;
 let isLoadingApp = false;
-let lastUpdateIDAppliedToClient: OnyxEntry<number>;
 
 Onyx.connect({
     key: ONYXKEYS.SESSION,
@@ -170,14 +169,7 @@ Onyx.connect({
     },
 });
 
-Onyx.connect({
-    key: ONYXKEYS.ONYX_UPDATES_LAST_UPDATE_ID_APPLIED_TO_CLIENT,
-    callback: (value) => {
-        lastUpdateIDAppliedToClient = value;
-    },
-});
-
-function handleNetworkReconnect() {
+function handleNetworkReconnect(lastUpdateIDAppliedToClient: number | undefined) {
     if (isLoadingApp) {
         App.openApp();
     } else {
@@ -229,6 +221,7 @@ function AuthScreens({session, lastOpenedPublicRoomID, initialLastUpdateIDApplie
     const {shouldUseNarrowLayout} = useResponsiveLayout();
     const rootNavigatorScreenOptions = useRootNavigatorScreenOptions();
     const currentUserPersonalDetails = useCurrentUserPersonalDetails();
+    const [lastUpdateIDAppliedToClient] = useOnyx(ONYXKEYS.ONYX_UPDATES_LAST_UPDATE_ID_APPLIED_TO_CLIENT, {canBeMissing: true});
     const {toggleSearch} = useSearchRouterContext();
     const currentUrl = getCurrentUrl();
     const delegatorEmail = getSearchParamFromUrl(currentUrl, 'delegatorEmail');
@@ -301,7 +294,7 @@ function AuthScreens({session, lastOpenedPublicRoomID, initialLastUpdateIDApplie
         }
 
         NetworkConnection.listenForReconnect();
-        NetworkConnection.onReconnect(handleNetworkReconnect);
+        NetworkConnection.onReconnect(() => handleNetworkReconnect(lastUpdateIDAppliedToClient));
         PusherConnectionManager.init();
         initializePusher();
         // Sometimes when we transition from old dot to new dot, the client is not the leader
