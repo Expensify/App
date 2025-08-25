fb9515011af7f3ceff7f886666e29acf8b1421a4
NJ-2020 <ntt.london@gmail.com>
2025-08-25 12:13:38 +0700

    fix attachment test eslint errror

--- tests/actions/AttachmentTest.ts
+++ tests/actions/AttachmentTest.ts
@@ -29,7 +29,7 @@ describe('AttachmentStorage', () => {
         // Mock global fetch and response for attachment
         global.fetch = TestHelper.getGlobalFetchMock({
             headers: new Headers({
-                'Content-Type': 'image/jpeg',
+                ['Content-Type']: 'image/jpeg',
             }),
             arrayBuffer: () => Promise.resolve(new ArrayBuffer(0)),
         });
