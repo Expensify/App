17e2e1ad35924d89a68535d333f4ed753d5be7e1
Chuck Dries <chuck@expensify.com>
2025-08-25 15:18:32 -0700

    Apply prettier-changed

--- src/libs/migrateOnyx.ts
+++ src/libs/migrateOnyx.ts
@@ -11,13 +11,7 @@ export default function () {
 
     return new Promise<void>((resolve) => {
         // Add all migrations to an array so they are executed in order
-        const migrationPromises = [
-            RenameCardIsVirtual,
-            RenameReceiptFilename,
-            KeyReportActionsDraftByReportActionID,
-            MoveIsOptimisticReportToMetadata,
-            PendingMembersToMetadata,
-        ];
+        const migrationPromises = [RenameCardIsVirtual, RenameReceiptFilename, KeyReportActionsDraftByReportActionID, MoveIsOptimisticReportToMetadata, PendingMembersToMetadata];
 
         // Reduce all promises down to a single promise. All promises run in a linear fashion, waiting for the
         // previous promise to finish before moving onto the next one.
