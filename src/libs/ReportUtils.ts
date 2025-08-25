1d66a22082a6fd022a1227d5fb24a1f56a7089a1
alberto <alberto@expensify.com>
2025-08-25 13:34:52 +0200

    Do not hide empty trip rooms in most recent mode

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -8468,7 +8468,7 @@ function reasonForReportToBeInOptionList({
     }
 
     // Hide chats between two users that haven't been commented on from the LNH
-    if (excludeEmptyChats && isEmptyChat && isChatReport(report) && !isPolicyExpenseChat(report) && !isSystemChat(report) && canHideReport) {
+    if (excludeEmptyChats && isEmptyChat && isChatReport(report) && !isPolicyExpenseChat(report) && !isTripRoom(report) && !isSystemChat(report) && canHideReport) {
         return null;
     }
 
