d17837d8f0aa9c190558a7e09cfa18124575d316
Zuzanna Furtak <zuzkafurtak@gmail.com>
2025-08-25 11:09:44 +0200

    Add comments to type

--- src/components/LHNOptionsList/OptionRowRendererComponent/index.native.tsx
+++ src/components/LHNOptionsList/OptionRowRendererComponent/index.native.tsx
@@ -4,8 +4,13 @@ import type {StyleProp, ViewStyle} from 'react-native';
 import {View} from 'react-native';
 
 type OptionRowRendererComponentProps = {
+    /** The index position of this option row in the list */
     index: number;
+
+    /** Callback function called when the component layout changes */
     onLayout?: () => void;
+
+    /** Style prop for customizing the option row */
     style?: StyleProp<ViewStyle>;
 };
 
