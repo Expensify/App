be895d1445b956f5039e4b7164ee63c581be8b19
Michał Muzyk <mimuzyk@gmail.com>
2025-08-26 15:32:41 +0200

    fix: all betas

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -4,7 +4,6 @@ import type Beta from '@src/types/onyx/Beta';
 
 // eslint-disable-next-line rulesdir/no-beta-handler
 function canUseAllBetas(betas: OnyxEntry<Beta[]>): boolean {
-    return true;
     return !!betas?.includes(CONST.BETAS.ALL);
 }
 
