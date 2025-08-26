c8456569ea7ee450fdbdc8e34617d4cd9a7d5c9a
Tomasz Lesniakiewicz <tlesniakiewicz@gmail.com>
2025-08-26 16:55:37 +0200

    refactor: migrate rnef to rock

---
+++ rock.config.mjs
@@ -0,0 +1,16 @@
+import {platformAndroid} from '@rock-js/platform-android';
+import {platformIOS} from '@rock-js/platform-ios';
+import {pluginMetro} from '@rock-js/plugin-metro';
+import {providerGitHub} from '@rock-js/provider-github';
+
+const isHybrid = process.env.IS_HYBRID_APP === 'true';
+
+/** @type {import('@rock-js/config').Config} */
+export default {
+    remoteCacheProvider: 'github-actions',
+    bundler: pluginMetro(),
+    platforms: {
+        ios: platformIOS({sourceDir: isHybrid ? './Mobile-Expensify/iOS' : './ios'}),
+        android: platformAndroid({sourceDir: isHybrid ? './Mobile-Expensify/Android' : './android'}),
+    },
+};
