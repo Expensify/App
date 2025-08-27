e6c4343b2b9eb62ac6086b25c850a3e7a4aa2380
neil-marcellini <neil@expensify.com>
2025-08-27 13:49:25 -0700

    Fix types

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -5,7 +5,7 @@ import type Beta from '@src/types/onyx/Beta';
 // A list of betas that require being explicitly enabled, and will not be enabled merely by the 'all' beta.
 // QA accounts have the 'all' beta enabled, so this allows us to deploy incomplete pieces of features under a beta without QA testing running and creating a bunch of deploy blocker issues.
 // When the feature is ready for QA, the beta can be removed from the list, and the feature can be QAed before being released to all users.
-const EXPLICIT_ONLY_BETAS = new Set([
+const EXPLICIT_ONLY_BETAS = new Set<Beta>([
     CONST.BETAS.AUTH_AUTO_REPORT_TITLE,
     CONST.BETAS.MANUAL_DISTANCE,
 ]);
@@ -13,7 +13,7 @@ const EXPLICIT_ONLY_BETAS = new Set([
 // These betas exclude or prevent accounts from accessing a specific feature. They are useful for disabling features for entire domains.
 // Similar to the EXPLICIT_ONLY_BETAS, these betas are not enabled by the 'all' beta.
 // The 'all' beta adds users to all features, so if it were to also add users to exclusion betas it would be contradicting its purpose.
-const EXCLUSION_BETAS = new Set([
+const EXCLUSION_BETAS = new Set<Beta>([
     CONST.BETAS.NO_OPTIMISTIC_TRANSACTION_THREADS,
 ]);
 
