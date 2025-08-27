bd136c8382d6c300f37a462d6e26871c035984fe
James Dean <james@expensify.com>
2025-08-27 12:01:44 -0700

    [NO QA] Update en.ts
    Coming from here: https://expensify.slack.com/archives/C07NMDKEFMH/p1756240425803979

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -6592,7 +6592,7 @@ const translations = {
         isTransactionBillable: 'Choose if transaction is billable',
         keepThisOne: 'Keep this one',
         confirmDetails: `Confirm the details you're keeping`,
-        confirmDuplicatesInfo: `The duplicate requests you don't keep will be held for the member to delete`,
+        confirmDuplicatesInfo: `The duplicate expenses you don't keep will be held for the submitter to delete.`,
         hold: 'This expense was put on hold',
         resolvedDuplicates: 'resolved the duplicate',
     },
