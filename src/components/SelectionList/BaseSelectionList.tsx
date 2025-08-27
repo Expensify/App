c0bf6c294c2d53eaf1eaed3b9d62aa5b6a52d188
Sachin Chavda <Sachin.chavda.1024@gmail.com>
2025-08-27 20:28:02 +0530

    avoid clearing focus index for initial render

--- src/components/SelectionList/BaseSelectionList.tsx
+++ src/components/SelectionList/BaseSelectionList.tsx
@@ -830,6 +830,9 @@ function BaseSelectionList<TItem extends ListItem>(
             }
         }
 
+        // Avoid clearing focus on initial render
+        if (isInitialSectionListRender) return;
+
         // Remove the focus if the search input is empty and prev search input not empty or selected options length is changed (and allOptions length remains the same)
         // else focus on the first non disabled item
         const newSelectedIndex =
