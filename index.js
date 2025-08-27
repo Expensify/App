897df80917b74be3cd86531542cd60f5a66debb6
Jakub Szymczak <szymczak.jakub12@gmail.com>
2025-08-27 10:22:40 +0200

    add a polyfill of PromiseWithResolvers

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import Config from './src/CONFIG';
+import './src/polyfills/PromiseWithResolvers';
 import additionalAppSetup from './src/setup';
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
