a6e76c6c70e1d9497f9806e53d81ce1acf60dfae
marta.sudol <msudol8@gmail.com>
2025-08-28 10:32:19 +0200

    ESLint

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -304,7 +304,6 @@ function ReportScreen({route, navigation}: ReportScreenProps) {
 
     const reportTransactions = useMemo(() => {
         const nonDeletedTransactions = getAllNonDeletedTransactions(allReportTransactions, reportActions);
-
         if (!nonDeletedTransactions) {
             return nonDeletedTransactions;
         }
