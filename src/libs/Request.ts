a0c4824abfa12498af2200c225147800779b1e3e
Brandon Stites <bstites+1@expensify.com>
2025-08-28 13:51:36 -0400

    Remove isSupportRequest

--- src/libs/Request.ts
+++ src/libs/Request.ts
@@ -4,22 +4,13 @@ import HttpUtils from './HttpUtils';
 import Log from './Log';
 import type Middleware from './Middleware/types';
 import enhanceParameters from './Network/enhanceParameters';
-import {hasReadRequiredDataFromStorage, isSupportAuthToken, isSupportRequest} from './Network/NetworkStore';
+import {hasReadRequiredDataFromStorage, isSupportAuthToken} from './Network/NetworkStore';
 
 let middlewares: Middleware[] = [];
 
 function makeXHR(request: Request): Promise<Response | void> {
     const finalParameters = enhanceParameters(request.command, request?.data ?? {});
     return hasReadRequiredDataFromStorage().then((): Promise<Response | void> => {
-        // If we're using the Supportal token and this is not a Supportal request
-        // let's just return a promise that will resolve itself.
-        if (isSupportAuthToken() && !isSupportRequest(request.command)) {
-            Log.info(`[API] The ${request.command} API call is skipped because user is using support token.`);
-            return new Promise<void>((resolve) => {
-                resolve();
-            });
-        }
-
         return HttpUtils.xhr(request.command, finalParameters, request.type, request.shouldUseSecure, request.initiatedOffline);
     });
 }
