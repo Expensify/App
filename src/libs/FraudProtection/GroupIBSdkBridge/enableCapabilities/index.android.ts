f18d847e5ca328f64f47ecabb9a896492a8ab1bb
Cristi Paval <cristi@expensify.com>
2025-08-29 03:00:45 +0300

    Remove console.log

--- src/libs/FraudProtection/GroupIBSdkBridge/enableCapabilities/index.android.ts
+++ src/libs/FraudProtection/GroupIBSdkBridge/enableCapabilities/index.android.ts
@@ -1,77 +1,33 @@
+import Log from "@libs/Log";
 import {FP, AndroidCapability} from "group-ib-fp";
 
 function enableCapabilities(fp: FP) {
     fp.enableAndroidCapability(AndroidCapability.CellsCollection, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability CellsCollection error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability CellsCollection isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(CellsCollection)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.AccessPointsCollection, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability AccessPointsCollection error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability AccessPointsCollection isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(AccessPointsCollection)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.Location, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability Location error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability Location isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(Location)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.GlobalIdentification, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability GlobalIdentification error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability GlobalIdentification isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(GlobalIdentification)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.CloudIdentification, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability CloudIdentification error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability CloudIdentification isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(CloudIdentification)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.CallIdentification, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability ActivityCollection error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability ActivityCollection isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(CallIdentification)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.ActivityCollection, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability ActivityCollection error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability ActivityCollection isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(ActivityCollection)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.MotionCollection, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability MotionCollection error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability MotionCollection isRun: ${isRun}`);
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(MotionCollection)', {error: e, isRun});
     });
     fp.enableAndroidCapability(AndroidCapability.PackageCollection, (e: any, isRun: boolean) => {
-        if (e) {
-            console.log(`[Fraud Protection] enableAndroidCapability PackageCollection error: ${e}`);
-        }
-        if (isRun) {
-            console.log(`[Fraud Protection] enableAndroidCapability PackageCollection isRun: ${isRun}`); 
-        }
+        Log.warn('[Fraud Protection] enableAndroidCapability(PackageCollection)', {error: e, isRun});
     });
 }
 
--- src/libs/FraudProtection/GroupIBSdkBridge/index.native.ts
+++ src/libs/FraudProtection/GroupIBSdkBridge/index.native.ts
@@ -3,6 +3,7 @@ import {getEnvironment, getOldDotEnvironmentURL} from '@libs/Environment/Environ
 import CONST from '@src/CONST';
 import enableCapabilities from './enableCapabilities/index';
 import CONFIG from '@src/CONFIG';
+import Log from '@libs/Log';
 
 const cidIOSMap: Record<string, string> = {
     [CONST.ENVIRONMENT.PRODUCTION]: 'gib-i-expensify',
@@ -29,36 +30,28 @@ function init(): Promise<void> {
         const androidCustomerID = cidAndroidMap[env] ?? cidAndroidMap[CONST.ENVIRONMENT.DEV];
         
         const fp = FP.getInstance();
-        console.log(`[Fraud Protection] FP.getInstance()`);
         
         if (env === CONST.ENVIRONMENT.DEV) {
-            console.log(`[Fraud Protection] FP.enableDebugLogs()`);
             oldDotURL = CONFIG.EXPENSIFY.DEFAULT_API_ROOT;
             fp.enableDebugLogs();
         }
         
-        console.log(`[Fraud Protection] FP.setCustomerId(${iOSCustomerID}, ${androidCustomerID})`);
         fp.setCustomerId(iOSCustomerID, androidCustomerID, (error: string) => {
-            console.log(`[Fraud Protection] setCustomerId error: ${error}`);
+            Log.warn(`[Fraud Protection] setCustomerId error: ${error}`);
         });
         
         oldDotURL = oldDotURL.endsWith('/') ? oldDotURL.slice(0, -1) : oldDotURL;
-        console.log(`[Fraud Protection] FP.setTargetURL(${oldDotURL}/api/fl)`);
         fp.setTargetURL(`${oldDotURL}/api/fl`, (error: string) => {
-            console.log(`[Fraud Protection] setTargetURL error: ${error}`);
+            Log.warn(`[Fraud Protection] setTargetURL error: ${error}`);
         });
-
-        console.log(`[Fraud Protection] FP.setGlobalIdURL(${oldDotURL}/api/fl/id.html)`);
         fp.setGlobalIdURL(`${oldDotURL}/api/fl/id.html`, (error: string) => {
-            console.log(`[Fraud Protection] setGlobalIdURL error: ${error}`);
+            Log.warn(`[Fraud Protection] setGlobalIdURL error: ${error}`);
         });
         
-        console.log(`[Fraud Protection] enableCapabilities()`);
         enableCapabilities(fp);
         
-        console.log(`[Fraud Protection] FP.run()`);
         fp.run((error: string) => {
-            console.log(`[Fraud Protection] run error: ${error}`);
+            Log.warn(`[Fraud Protection] run error: ${error}`);
         });
         
         resolveFpInstancePromise(fp);
@@ -67,13 +60,11 @@ function init(): Promise<void> {
 
 function setAuthenticationData(identity: string, sessionID: string): void {
     fpInstancePromise.then((fp) => {
-        console.log(`[Fraud Protection] setAttributeTitle('user_id', ${identity}, FPAttributeFormat.ClearText)`);
         fp.setAttributeTitle('user_id', identity, FPAttributeFormat.ClearText, (e: any) => { 
-            console.log(`[Fraud Protection] setAttributeTitle error: ${e}`);
+            Log.warn(`[Fraud Protection] setAttributeTitle error: ${e}`);
         });
-        console.log(`[Fraud Protection] FP.setSessionId(${sessionID})`);
         fp.setSessionId(sessionID, (e: any) => {
-            console.log(`[Fraud Protection] setSessionId error: ${e}`);
+            Log.warn(`[Fraud Protection] setSessionId error: ${e}`);
         });
     });
 }
