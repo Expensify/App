615690048c9899d1b6f79f760eb2d2e02f6694c2
Jakub Szymczak <szymczak.jakub12@gmail.com>
2025-08-29 08:31:38 +0200

    change position of polyfill import

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /**
  * @format
  */
+// import of polyfills should always be first
+import './src/polyfills/PromiseWithResolvers';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import Config from './src/CONFIG';
-import './src/polyfills/PromiseWithResolvers';
 import additionalAppSetup from './src/setup';
 
 AppRegistry.registerComponent(Config.APP_NAME, () => App);
