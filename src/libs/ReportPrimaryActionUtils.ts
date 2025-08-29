79237e5193f4ea006718af83f55e508aa00f59f6
Rayane <77965000+rayane-d@users.noreply.github.com>
2025-08-29 23:22:26 +0100

    Reapply "Display submit button when the report is open in an instant submit policy with no approvers"
    This reverts commit 998b01916c2c331059ea1223e438d53600d07459.

--- src/libs/ReportPrimaryActionUtils.ts
+++ src/libs/ReportPrimaryActionUtils.ts
@@ -10,7 +10,9 @@ import {
     getSubmitToAccountID,
     getValidConnectedIntegration,
     hasIntegrationAutoSync,
+    isInstantSubmitEnabled,
     isPreferredExporter,
+    isSubmitAndClose,
 } from './PolicyUtils';
 import {getAllReportActions, getOneTransactionThreadReportID, isMoneyRequestAction} from './ReportActionsUtils';
 import {
@@ -105,7 +107,9 @@ function isSubmitAction(report: Report, reportTransactions: Transaction[], polic
         return true;
     }
 
-    return isManualSubmitEnabled && baseIsSubmit;
+    // The report need manual submission if manual submit is enabled in the policy or the report is open in a Submit & Close policy with no approvers
+    const needsManualSubmit = isManualSubmitEnabled || (isOpenReport && isInstantSubmitEnabled(policy) && isSubmitAndClose(policy));
+    return needsManualSubmit && baseIsSubmit;
 }
 
 function isApproveAction(report: Report, reportTransactions: Transaction[], policy?: Policy) {
