06ac2e4991bdc3c91cba635f451de481f560b344
Alexey Kureev <a.g.kureev@gmail.com>
2025-08-29 16:15:15 +0200

    fix(63871): use shellUtils for formatting check-android-16kb script

--- scripts/check-android-16kb-memory-page-size-compatibility.sh
+++ scripts/check-android-16kb-memory-page-size-compatibility.sh
@@ -5,13 +5,15 @@ progname="${0##*/}"
 progname="${progname%.sh}"
 
 # usage: check_elf_alignment.sh [path to *.so files|path to *.apk]
-
 cleanup_trap() {
   if [ -n "$tmp" ] && [ -d "$tmp" ]; then
     rm -rf "$tmp"
   fi
   exit "${1:-0}"
 }
+
+# shellcheck disable=SC1090
+source "$(dirname "$0")/shellUtils.sh"
 
 usage() {
   echo "Host side script to check the ELF alignment of shared libraries."
@@ -37,7 +39,7 @@ case "$1" in
 esac
 
 if ! [ -f "$dir" ] && ! [ -d "$dir" ]; then
-  echo "Invalid file: $dir" >&2
+  error "Invalid file: $dir"
   exit 1
 fi
 
@@ -45,20 +47,19 @@ if [[ "$dir" == *.apk ]]; then
   trap 'cleanup_trap' EXIT
 
   echo
-  echo "Recursively analyzing $dir"
+  title "Recursively analyzing $dir"
   echo
 
   if zipalign --help 2>&1 | grep -q "\-P <pagesize_kb>"; then
-    echo "=== APK zip-alignment ==="
-    # SC2196: use grep -E instead of egrep
+    title "APK zip-alignment"
     zipalign -v -c -P 16 4 "$dir" | grep -E 'lib/arm64-v8a|lib/x86_64|Verification'
     echo "========================="
   else
-    echo "NOTICE: Zip alignment check requires build-tools version 35.0.0-rc3 or higher."
-    echo "  You can install the latest build-tools by running the below command"
-    echo "  and updating your \$PATH:"
+    info "NOTICE: Zip alignment check requires build-tools version 35.0.0-rc3 or higher."
+    info "  You can install the latest build-tools by running the below command"
+    info "  and updating your \$PATH:"
     echo
-    echo "    sdkmanager \"build-tools;35.0.0-rc3\""
+    info '    sdkmanager "build-tools;35.0.0-rc3"'
   fi
 
   dir_filename=$(basename "$dir")
@@ -71,47 +72,43 @@ if [[ "$dir" == *.apex ]]; then
   trap 'cleanup_trap' EXIT
 
   echo
-  echo "Recursively analyzing $dir"
+  title "Recursively analyzing $dir"
   echo
 
   dir_filename=$(basename "$dir")
   tmp=$(mktemp -d -t "${dir_filename%.apex}_out_XXXXX")
   if ! deapexer extract "$dir" "$tmp"; then
-    echo "Failed to deapex."
+    error "Failed to deapex."
     exit 1
   fi
   dir="$tmp"
 fi
 
-RED="\e[31m"
-GREEN="\e[32m"
-ENDCOLOR="\e[0m"
-
 unaligned_libs=()
 
 echo
-echo "=== ELF alignment ==="
+title "ELF alignment"
 
 matches="$(find "$dir" -type f)"
 IFS=$'\n'
 for match in $matches; do
-  [[ "$match" == *".apk" ]] && echo "WARNING: doesn't recursively inspect .apk file: $match"
-  [[ "$match" == *".apex" ]] && echo "WARNING: doesn't recursively inspect .apex file: $match"
+  [[ "$match" == *".apk"  ]] && warn "doesn't recursively inspect .apk file: $match"
+  [[ "$match" == *".apex" ]] && warn "doesn't recursively inspect .apex file: $match"
 
   [[ $(file "$match") == *"ELF"* ]] || continue
 
   res="$(objdump -p "$match" | grep LOAD | awk '{ print $NF }' | head -1)"
   if [[ $res =~ 2\*\*(1[4-9]|[2-9][0-9]|[1-9][0-9]{2,}) ]]; then
-    echo -e "$match: ${GREEN}ALIGNED${ENDCOLOR} ($res)"
+    printf "%s: %sALIGNED%s (%s)\n" "$match" "$GREEN" "$RESET" "$res"
   else
-    echo -e "$match: ${RED}UNALIGNED${ENDCOLOR} ($res)"
+    printf "%s: %sUNALIGNED%s (%s)\n" "$match" "$RED" "$RESET" "$res"
     unaligned_libs+=("$match")
   fi
 done
 
 if [ "${#unaligned_libs[@]}" -gt 0 ]; then
-  echo -e "${RED}Found ${#unaligned_libs[@]} unaligned libs (only arm64-v8a/x86_64 libs need to be aligned).${ENDCOLOR}"
+  printf "%sFound %d unaligned libs (only arm64-v8a/x86_64 libs need to be aligned).%s\n" "$RED" "${#unaligned_libs[@]}" "$RESET"
 elif [ -n "${dir_filename:-}" ]; then
-  echo -e "ELF Verification Successful"
+  success "ELF Verification Successful"
 fi
 echo "====================="
