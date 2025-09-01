f9e0806cb7fb76cc0dde66dc98384bb58a14c5a4
Nicolay Arefyeu <n.arefyev91@gmail.com>
2025-09-01 19:26:46 +0300

    comments

--- src/hooks/useIsUberConnected.ts
+++ src/hooks/useIsUberConnected.ts
@@ -3,6 +3,5 @@ import usePolicy from './usePolicy';
 export default function useIsUberConnected({policyID}: {policyID?: string}) {
     const policy = usePolicy(policyID);
 
-    const integrations = policy?.receiptPartners;
-    return !!integrations?.uber?.organizationID;
+    return !!policy?.receiptPartners?.uber?.organizationID;
 }
