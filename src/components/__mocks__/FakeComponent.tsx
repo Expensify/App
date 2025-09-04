7229c1bf9e0d8435606a83dd338ad40f40c29042
Github <olimpia.zurek@gmail.com>
2025-09-04 16:56:31 +0200

    test changes 2

--- src/components/__mocks__/FakeComponent.tsx
+++ src/components/__mocks__/FakeComponent.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
-import {getCrashlytics} from '@react-native-firebase/crashlytics';
 
 function FakeComponent() {
-    React.useEffect(() => {
-        const crashlytics = getCrashlytics();
-        crashlytics.log('Fake component loaded');
-    }, []);
-    console.log('Fake component loaded');
-
 
     return <View testID="fake-component">This component is designed to fail tests</View>;
 }
