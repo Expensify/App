fbca9580beceacc0e1b78707ace4645e3fe9bf7f
Github <olimpia.zurek@gmail.com>
2025-09-04 12:57:27 +0200

    run test again

--- src/components/__mocks__/FakeComponent.tsx
+++ src/components/__mocks__/FakeComponent.tsx
@@ -7,8 +7,6 @@ function FakeComponent() {
         const crashlytics = getCrashlytics();
         crashlytics.log('Fake component loaded');
     }, []);
-    console.log('Fake component loaded');
-    
 
     return <View testID="fake-component">This component is designed to fail tests</View>;
 }
