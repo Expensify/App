cc1dbc18dd3514f5ae14a7c0e0b5b27217eb5b5f
Github <olimpia.zurek@gmail.com>
2025-09-04 10:20:53 +0200

    revert tests

---
+++ tests/perf-test/SideBarLinks.perf-test.tsx
@@ -0,0 +1,117 @@
+import {fireEvent, screen, waitFor} from '@testing-library/react-native';
+import Onyx from 'react-native-onyx';
+import {measureRenders} from 'reassure';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import * as LHNTestUtils from '../utils/LHNTestUtils';
+import * as TestHelper from '../utils/TestHelper';
+import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
+import wrapInAct from '../utils/wrapInActHelper';
+import wrapOnyxWithWaitForBatchedUpdates from '../utils/wrapOnyxWithWaitForBatchedUpdates';
+
+jest.mock('@libs/Permissions');
+jest.mock('../../src/libs/Navigation/Navigation', () => ({
+    navigate: jest.fn(),
+    isActiveRoute: jest.fn(),
+    getTopmostReportId: jest.fn(),
+    getActiveRoute: jest.fn(),
+    getTopmostReportActionId: jest.fn(),
+    isNavigationReady: jest.fn(() => Promise.resolve()),
+    isDisplayedInModal: jest.fn(() => false),
+}));
+jest.mock('../../src/libs/Navigation/navigationRef', () => ({
+    getState: () => ({
+        routes: [{name: 'Report'}],
+    }),
+    getRootState: () => ({
+        routes: [],
+    }),
+    addListener: () => () => {},
+}));
+jest.mock('@components/Icon/Expensicons');
+
+jest.mock('@react-navigation/native');
+jest.mock('@src/hooks/useLHNEstimatedListSize/index.native.ts');
+
+const getMockedReportsMap = (length = 100) => {
+    const mockReports = Object.fromEntries(
+        Array.from({length}, (value, index) => {
+            const reportID = index + 1;
+            const participants = [1, 2];
+            const reportKey = `${ONYXKEYS.COLLECTION.REPORT}${reportID}`;
+            const report = {...LHNTestUtils.getFakeReport(participants, 1, true), lastMessageText: 'hey'};
+
+            return [reportKey, report];
+        }),
+    );
+
+    return mockReports;
+};
+
+const mockedResponseMap = getMockedReportsMap(500);
+
+describe('SidebarLinks', () => {
+    beforeAll(() => {
+        Onyx.init({
+            keys: ONYXKEYS,
+            evictableKeys: [ONYXKEYS.COLLECTION.REPORT_ACTIONS],
+        });
+    });
+
+    beforeEach(() => {
+        global.fetch = TestHelper.getGlobalFetchMock();
+        wrapOnyxWithWaitForBatchedUpdates(Onyx);
+
+        // Initialize the network key for OfflineWithFeedback
+        Onyx.merge(ONYXKEYS.NETWORK, {isOffline: false});
+        TestHelper.signInWithTestUser(1, 'email1@test.com', undefined, undefined, 'One').then(waitForBatchedUpdates);
+    });
+
+    afterEach(() => {
+        Onyx.clear();
+    });
+
+    test('[SidebarLinks] should render Sidebar with 500 reports stored', async () => {
+        const scenario = async () => {
+            await waitFor(async () => {
+                // Query for the sidebar
+                await screen.findByTestId('lhn-options-list');
+            });
+        };
+
+        await waitForBatchedUpdates();
+
+        Onyx.multiSet({
+            [ONYXKEYS.PERSONAL_DETAILS_LIST]: LHNTestUtils.fakePersonalDetails,
+            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
+            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
+            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
+            ...mockedResponseMap,
+        });
+
+        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
+    });
+
+    test('[SidebarLinks] should click on list item', async () => {
+        const scenario = async () => {
+            await waitFor(async () => {
+                const button = await screen.findByTestId('1');
+                await wrapInAct(() => {
+                    fireEvent.press(button);
+                });
+            });
+        };
+
+        await waitForBatchedUpdates();
+
+        Onyx.multiSet({
+            [ONYXKEYS.PERSONAL_DETAILS_LIST]: LHNTestUtils.fakePersonalDetails,
+            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
+            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
+            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
+            ...mockedResponseMap,
+        });
+
+        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
+    });
+});
