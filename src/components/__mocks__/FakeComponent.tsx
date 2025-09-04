47604f84cb1eb53bbf890ecfc0ad69e9b3ed7247
Github <olimpia.zurek@gmail.com>
2025-09-04 12:09:52 +0200

    make the test fail

--- src/components/__mocks__/FakeComponent.tsx
+++ src/components/__mocks__/FakeComponent.tsx
@@ -3,12 +3,12 @@ import {View} from 'react-native';
 import {getCrashlytics} from '@react-native-firebase/crashlytics';
 
 function FakeComponent() {
-    // This will fail because Firebase Crashlytics is not properly mocked in perf tests
     React.useEffect(() => {
         const crashlytics = getCrashlytics();
         crashlytics.log('Fake component loaded');
     }, []);
     
+
     return <View testID="fake-component">This component is designed to fail tests</View>;
 }
 
