c729235d58f7d60baf793701897d1ad8cfadeb0d
war-in <war-in@users.noreply.github.com>
2025-09-04 14:25:51 +0200

    fix android crash

--- src/components/RNMaskedTextInput.tsx
+++ src/components/RNMaskedTextInput.tsx
@@ -1,13 +1,13 @@
 import type {ForwardedRef} from 'react';
 import React from 'react';
-import type {TextInput} from 'react-native';
+import {TextInput} from 'react-native';
 import type {MaskedTextInputProps} from 'react-native-advanced-input-mask';
 import {MaskedTextInput} from 'react-native-advanced-input-mask';
 import Animated from 'react-native-reanimated';
 import useTheme from '@hooks/useTheme';
 
 // Convert the underlying TextInput into an Animated component so that we can take an animated ref and pass it to a worklet
-const AnimatedTextInput = Animated.createAnimatedComponent(MaskedTextInput);
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 type AnimatedTextInputRef = typeof AnimatedTextInput & TextInput & HTMLInputElement;
 
@@ -19,7 +19,8 @@ function RNMaskedTextInputWithRef({ref, ...props}: RNMaskedTextInputWithRefProps
     const theme = useTheme();
 
     return (
-        <AnimatedTextInput
+        <MaskedTextInput
+            renderTextInputComponent={AnimatedTextInput}
             // disable autocomplete to prevent part of mask to be present on Android when value is empty
             autocomplete={false}
             allowFontScaling={false}
