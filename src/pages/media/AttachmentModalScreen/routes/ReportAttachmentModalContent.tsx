d8b1bb99b5a65e54b8738e4293235e79383c1262
Christoph Pader <chris@margelo.com>
2025-09-04 13:54:10 +0200

    fix: wrong screen name

--- src/pages/media/AttachmentModalScreen/routes/ReportAttachmentModalContent.tsx
+++ src/pages/media/AttachmentModalScreen/routes/ReportAttachmentModalContent.tsx
@@ -9,17 +9,17 @@ import {isReportNotFound} from '@libs/ReportUtils';
 import tryResolveUrlFromApiRoot from '@libs/tryResolveUrlFromApiRoot';
 import type {AttachmentModalBaseContentProps} from '@pages/media/AttachmentModalScreen/AttachmentModalBaseContent/types';
 import AttachmentModalContainer from '@pages/media/AttachmentModalScreen/AttachmentModalContainer';
-import useDownloadAttachment from '@pages/media/AttachmentModalScreen/routes/hooks/useDownloadAttachment';
 import type {AttachmentModalScreenProps} from '@pages/media/AttachmentModalScreen/types';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
+import useDownloadAttachment from './hooks/useDownloadAttachment';
 import useNavigateToReportOnRefresh from './hooks/useNavigateToReportOnRefresh';
 import useReportAttachmentModalType from './hooks/useReportAttachmentModalType';
 
-function ReportAttachmentModalContent({route, navigation}: AttachmentModalScreenProps<typeof SCREENS.REPORT_ATTACHMENTS>) {
+function ReportAttachmentModalContent({route, navigation}: AttachmentModalScreenProps<typeof SCREENS.ATTACHMENTS>) {
     const {attachmentID, type, source: sourceParam, isAuthTokenRequired, attachmentLink, originalFileName, accountID, reportID, hashKey, headerTitle, onShow, onClose} = route.params;
 
     const [report] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {canBeMissing: false});
@@ -67,7 +67,7 @@ function ReportAttachmentModalContent({route, navigation}: AttachmentModalScreen
 
     const onCarouselAttachmentChange = useCallback(
         (attachment: Attachment) => {
-            const routeToNavigate = ROUTES.REPORT_ATTACHMENTS.getRoute({
+            const routeToNavigate = ROUTES.ATTACHMENTS.getRoute({
                 reportID,
                 attachmentID: attachment.attachmentID,
                 type,
@@ -112,7 +112,7 @@ function ReportAttachmentModalContent({route, navigation}: AttachmentModalScreen
     );
 
     return (
-        <AttachmentModalContainer<typeof SCREENS.REPORT_ATTACHMENTS>
+        <AttachmentModalContainer<typeof SCREENS.ATTACHMENTS>
             navigation={navigation}
             contentProps={contentProps}
             modalType={modalType}
