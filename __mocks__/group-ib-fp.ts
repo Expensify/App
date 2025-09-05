e6a27ab40eacd1aded3b8aed983d9c949775bfe8
Cristi Paval <cristi@expensify.com>
2025-09-05 16:27:26 +0200

    Try fix jest

--- __mocks__/group-ib-fp.ts
+++ __mocks__/group-ib-fp.ts
@@ -1,5 +1,45 @@
-type FPAttributeFormat = 1;
-const FPAttributeFormatMap = {ClearText: 1 as FPAttributeFormat};
+const Capability = {
+    BatteryStatus: 0,
+    Cellular: 1,
+    Call: 2,
+    Passcode: 3,
+    WebView: 4,
+    Network: 5,
+    Motion: 6,
+    Swizzle: 7,
+    Location: 8,
+    Audio: 9,
+    CloudIdentifier: 10,
+    DeviceStatus: 11,
+    Capture: 12,
+    Apps: 13,
+    Proxy: 14,
+    Keyboard: 15,
+    Behavior: 16,
+    PreventScreenshots: 17,
+    Security: 18,
+    Advertise: 19,
+    PortScan: 20,
+    GlobalId: 21,
+};
+
+const FPAttributeFormat = {
+    ClearText: 0,
+    Hashed: 1,
+    Encrypted: 2,
+};
+
+const AndroidCapability = {
+    CellsCollection: 0,
+    AccessPointsCollection: 1,
+    Location: 2,
+    GlobalIdentification: 3,
+    CloudIdentification: 4,
+    CallIdentification: 5,
+    ActivityCollection: 6,
+    MotionCollection: 7,
+    PackageCollection: 8,
+};
 
 class FP {
     static getInstance() {
@@ -39,4 +79,4 @@ class FP {
     run(..._a: unknown[]) {}
 }
 
-export {FP, FPAttributeFormatMap as FPAttributeFormat};
+export {AndroidCapability, Capability, FP, FPAttributeFormat};
