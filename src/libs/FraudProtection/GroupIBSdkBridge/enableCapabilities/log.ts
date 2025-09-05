5746bbc4cf9588d7022af5ecdcb1b18757034980
Cristi Paval <cristi@expensify.com>
2025-09-05 23:39:16 +0200

    Improve logging

--- src/libs/FraudProtection/GroupIBSdkBridge/enableCapabilities/log.ts
+++ src/libs/FraudProtection/GroupIBSdkBridge/enableCapabilities/log.ts
@@ -1,7 +1,11 @@
 import Log from '@libs/Log';
 
 function logCapability(capabilityName: string, e: string, isRun: boolean) {
-    Log.warn(`[Fraud Protection] enableCapability(${capabilityName})`, {error: e, isRun});
+    if (e !== null && e !== undefined && e !== '') {
+        Log.warn(`[Fraud Protection] ${capabilityName} capability error: ${e}`);
+    } else {
+        Log.info(`[Fraud Protection] ${capabilityName} capability status: ${isRun ? 'Enabled' : 'Disabled'}`);
+    }
 }
 
 export default logCapability;
