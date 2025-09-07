36525def22142ac34063d57f019022470cac8cc0
allgandalf <whogandalf@gmail.com>
2025-09-07 16:05:08 +0530

    pass accountID from Expensify.tsx

--- src/Expensify.tsx
+++ src/Expensify.tsx
@@ -168,7 +168,7 @@ function Expensify() {
         ActiveClientManager.init();
 
         // Used for the offline indicator appearing when someone is offline
-        const unsubscribeNetInfo = NetworkConnection.subscribeToNetInfo();
+        const unsubscribeNetInfo = NetworkConnection.subscribeToNetInfo(session?.accountID);
 
         return unsubscribeNetInfo;
     }, []);
