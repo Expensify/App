b53c0083f69c122eaac3413efc6a6b2c2e6fd30b
Eliran Goshen <elirangoshen@gmail.com>
2025-09-08 15:11:59 +0200

    clearing logs

--- jest/setupAfterEnv.ts
+++ jest/setupAfterEnv.ts
@@ -1,3 +1,11 @@
 import '@testing-library/react-native';
 
+beforeEach(() => {
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+    jest.spyOn(console, 'info').mockImplementation(() => {});
+    jest.spyOn(console, 'debug').mockImplementation(() => {});
+    jest.spyOn(console, 'warn').mockImplementation(() => {});
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+});
+
 jest.useRealTimers();
