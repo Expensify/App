7c39b7929242a21169b11b26a40291e51e07e9fa
Eliran Goshen <elirangoshen@gmail.com>
2025-09-08 19:02:20 +0200

    reverting error logs visibility

--- jest/setupAfterEnv.ts
+++ jest/setupAfterEnv.ts
@@ -5,7 +5,6 @@ beforeEach(() => {
     jest.spyOn(console, 'info').mockImplementation(() => {});
     jest.spyOn(console, 'debug').mockImplementation(() => {});
     jest.spyOn(console, 'warn').mockImplementation(() => {});
-    jest.spyOn(console, 'error').mockImplementation(() => {});
 });
 
 jest.useRealTimers();
