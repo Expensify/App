8d7ad2487e7bb290f99223ba8a5eba58bddc1b59
Eliran Goshen <elirangoshen@gmail.com>
2025-09-10 12:56:51 +0200

    added verbose to show all logs

--- jest/setupAfterEnv.ts
+++ jest/setupAfterEnv.ts
@@ -1,6 +1,13 @@
 import '@testing-library/react-native';
 
+const isVerbose = process.argv.includes('--verbose') || process.env.JEST_VERBOSE === 'true';
+
+
 beforeEach(() => {
+    if (isVerbose) {
+        return;
+    }
+
     jest.spyOn(console, 'log').mockImplementation(() => {});
     jest.spyOn(console, 'info').mockImplementation(() => {});
     jest.spyOn(console, 'debug').mockImplementation(() => {});
