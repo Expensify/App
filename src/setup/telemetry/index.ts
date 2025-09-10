90f1ff158a04a9247fc919e31acb1852096fc23e
Adam Horodyski <ad.horodyski@gmail.com>
2025-09-10 20:09:32 +0200

    feat: setup react navigation instrumentation

--- src/setup/telemetry/index.ts
+++ src/setup/telemetry/index.ts
@@ -2,15 +2,21 @@ import * as Sentry from '@sentry/react-native';
 import CONFIG from '@src/CONFIG';
 import pkg from '../../../package.json';
 
-export default function(): void {
-  Sentry.init({
-      dsn: 'https://c30560649568161d294f43893a3d0f5e@o4509989802344448.ingest.us.sentry.io/4509990212730880',
-      tracesSampleRate: 1.0,
-      profilesSampleRate: 1.0,
-      replaysSessionSampleRate: 0.1,
-      replaysOnErrorSampleRate: 1.0,
-      integrations: [Sentry.mobileReplayIntegration()],
-      environment: CONFIG.ENVIRONMENT,
-      release: `${pkg.name}@${pkg.version}`
-  });
+const navigationIntegration = Sentry.reactNavigationIntegration({
+    enableTimeToInitialDisplay: true,
+});
+
+export default function (): void {
+    Sentry.init({
+        dsn: 'https://c30560649568161d294f43893a3d0f5e@o4509989802344448.ingest.us.sentry.io/4509990212730880',
+        tracesSampleRate: 1.0,
+        profilesSampleRate: 1.0,
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
+        integrations: [Sentry.mobileReplayIntegration(), navigationIntegration],
+        environment: CONFIG.ENVIRONMENT,
+        release: `${pkg.name}@${pkg.version}`,
+    });
 }
+
+export {navigationIntegration};
