93c298a9fc255ca5d6ea6b18378cdcbe1ae1893f
Christoph Pader <chris@margelo.com>
2025-09-11 13:12:00 +0200

    fix: typo

--- src/hooks/useWithFallbackRef.ts
+++ src/hooks/useWithFallbackRef.ts
@@ -1,7 +1,7 @@
 import {useRef} from 'react';
 import type {ForwardedRef} from 'react';
 
-function useWithFallbackRef<RefType, ForwaredRefType = RefType>(ref: ForwardedRef<ForwaredRefType>) {
+function useWithFallbackRef<RefType, ForwardedRefType = RefType>(ref: ForwardedRef<ForwardedRefType>) {
     const fallbackRef = useRef<RefType>(null);
     const combinedRef = (ref as React.RefObject<RefType>) ?? fallbackRef;
 
