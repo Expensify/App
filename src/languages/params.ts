ee0e1f0b597551513b120908e71f113ff7a897c7
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-09-13 21:53:00 +0530

    Merge termsStep translations

--- src/languages/params.ts
+++ src/languages/params.ts
@@ -922,6 +922,10 @@ type PayAndDowngradeDescriptionParams = {
     formattedAmount: string;
 };
 
+type WalletAgreementParams = {
+    walletAgreementUrl: string;
+};
+
 export type {
     ContactMethodsRouteParams,
     ContactMethodParams,
@@ -1228,4 +1232,5 @@ export type {
     BusinessRegistrationNumberParams,
     DependentMultiLevelTagsSubtitleParams,
     PayAndDowngradeDescriptionParams,
+    WalletAgreementParams,
 };
