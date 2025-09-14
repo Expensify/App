a9ae8336961dd3f093e990fbba35dd91719cbc69
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-09-14 17:16:30 +0100

    allow choosing no report if default option is 'New report'

--- src/pages/iou/request/step/IOURequestEditReportCommon.tsx
+++ src/pages/iou/request/step/IOURequestEditReportCommon.tsx
@@ -74,7 +74,7 @@ function IOURequestEditReportCommon({
             .some((transaction) => transaction?.comment?.liabilityType === CONST.TRANSACTION.LIABILITY_TYPE.RESTRICT);
     }, [transactionIDs, selectedReport, reportTransactions]);
 
-    const shouldShowRemoveFromReport = isOwner && !isReportIOU && !isUnreported && !isCardTransaction;
+    const shouldShowRemoveFromReport = (isOwner || !selectedReport) && !isReportIOU && !isUnreported && !isCardTransaction;
 
     const expenseReports = useMemo(() => {
         // Early return if no reports are available to prevent useless loop
