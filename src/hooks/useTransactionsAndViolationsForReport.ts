a1bdfc35eb1c4277a93d46cb7711db33eb85d26b
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-09-15 22:46:18 +0100

    filter out dismissed violations

--- src/hooks/useTransactionsAndViolationsForReport.ts
+++ src/hooks/useTransactionsAndViolationsForReport.ts
@@ -1,5 +1,8 @@
+import {useMemo} from 'react';
 import {useAllReportsTransactionsAndViolations} from '@components/OnyxListItemProvider';
+import {getTransactionViolations} from '@libs/TransactionUtils';
 import CONST from '@src/CONST';
+import type {TransactionViolations} from '@src/types/onyx';
 import type {ReportTransactionsAndViolations} from '@src/types/onyx/DerivedValues';
 
 const DEFAULT_RETURN_VALUE: ReportTransactionsAndViolations = {transactions: {}, violations: {}};
@@ -11,7 +14,23 @@ function useTransactionsAndViolationsForReport(reportID?: string) {
         return DEFAULT_RETURN_VALUE;
     }
 
-    return allReportsTransactionsAndViolations?.[reportID ?? CONST.DEFAULT_NUMBER_ID] ?? DEFAULT_RETURN_VALUE;
+    const {transactions, violations} = allReportsTransactionsAndViolations?.[reportID ?? CONST.DEFAULT_NUMBER_ID] ?? DEFAULT_RETURN_VALUE;
+
+    const transactionsAndViolations = useMemo<ReportTransactionsAndViolations>(() => {
+        const filteredViolations = Object.keys(violations).reduce(
+            (filteredTransactionViolations, transactionViolationKey) => {
+                const transactionID = transactionViolationKey.split('_').at(1) ?? '';
+                const transaction = transactions[transactionID];
+                filteredTransactionViolations[transactionID] = getTransactionViolations(transaction, violations) ?? [];
+                return filteredTransactionViolations;
+            },
+            {} as Record<string, TransactionViolations>,
+        );
+
+        return {transactions, violations: filteredViolations};
+    }, [transactions, violations]);
+
+    return transactionsAndViolations;
 }
 
 export default useTransactionsAndViolationsForReport;
