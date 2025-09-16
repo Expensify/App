d8131f287d81a778d9ad2868bb1445e2342b4d10
lorretheboy <lorretheboy@outlook.com>
2025-09-16 21:04:06 +0800

    fix: Approve filter missing in To-Do section after approver changed

--- src/hooks/useSearchTypeMenuSections.ts
+++ src/hooks/useSearchTypeMenuSections.ts
@@ -44,6 +44,7 @@ const useSearchTypeMenuSections = () => {
     const [currentUserLoginAndAccountID] = useOnyx(ONYXKEYS.SESSION, {selector: currentUserLoginAndAccountIDSelector, canBeMissing: false});
     const [activePolicyID] = useOnyx(ONYXKEYS.NVP_ACTIVE_POLICY_ID, {canBeMissing: true});
     const [savedSearches] = useOnyx(ONYXKEYS.SAVED_SEARCHES, {canBeMissing: true});
+    const [reports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {canBeMissing: true});
 
     const typeMenuSections = useMemo(
         () =>
@@ -56,8 +57,9 @@ const useSearchTypeMenuSections = () => {
                 activePolicyID,
                 savedSearches,
                 isOffline,
+                reports,
             ),
-        [currentUserLoginAndAccountID?.email, currentUserLoginAndAccountID?.accountID, cardFeedsByPolicy, defaultCardFeed, allPolicies, activePolicyID, savedSearches, isOffline],
+        [currentUserLoginAndAccountID?.email, currentUserLoginAndAccountID?.accountID, cardFeedsByPolicy, defaultCardFeed, allPolicies, activePolicyID, savedSearches, isOffline, reports],
     );
 
     return {typeMenuSections};
