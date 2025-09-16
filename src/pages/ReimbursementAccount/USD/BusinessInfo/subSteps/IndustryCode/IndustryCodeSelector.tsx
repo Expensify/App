4a8a8c5efc370723846c115b14d73dfc30d0a160
Jakub Kaliński <kubakalinski2@gmail.com>
2025-09-16 13:40:39 +0200

    Removed forwardRef from IndustryCodeSelector.tsx, PersonalInfo.tsx and RequestorStep.tsx

--- src/pages/ReimbursementAccount/USD/BusinessInfo/subSteps/IndustryCode/IndustryCodeSelector.tsx
+++ src/pages/ReimbursementAccount/USD/BusinessInfo/subSteps/IndustryCode/IndustryCodeSelector.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef, useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {View} from 'react-native';
 import SelectionList from '@components/SelectionList';
 import RadioListItem from '@components/SelectionList/RadioListItem';
@@ -91,4 +91,4 @@ function IndustryCodeSelector({onInputChange, value, errorText}: IndustryCodeSel
 
 IndustryCodeSelector.displayName = 'IndustryCodeSelector';
 
-export default forwardRef(IndustryCodeSelector);
+export default IndustryCodeSelector;
