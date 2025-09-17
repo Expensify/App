acfd89c7675c30fc8a1a169c0d3c68eb03ce0160
war-in <war-in@users.noreply.github.com>
2025-09-17 14:35:01 +0200

    revert NavigationBarModule migration (standalone)

---
+++ android/app/src/main/java/com/expensify/chat/navbar/NavBarManagerModule.kt
@@ -0,0 +1,43 @@
+package com.expensify.chat.navbar
+
+import android.content.res.Resources
+import androidx.core.view.WindowInsetsControllerCompat
+import com.expensify.chat.R
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.UiThreadUtil
+
+class NavBarManagerModule(
+    private val mReactContext: ReactApplicationContext,
+) : ReactContextBaseJavaModule(mReactContext) {
+    override fun getName(): String = "RNNavBarManager"
+
+    @ReactMethod
+    fun setButtonStyle(style: String) {
+        UiThreadUtil.runOnUiThread {
+            mReactContext.currentActivity?.window?.let {
+                WindowInsetsControllerCompat(it, it.decorView).let { controller ->
+                    when (style) {
+                        "light" -> controller.isAppearanceLightNavigationBars = false
+                        "dark" -> controller.isAppearanceLightNavigationBars = true
+                    }
+                }
+            }
+        }
+    }
+
+    @ReactMethod
+    fun getType(promise: Promise) {
+        val resources = mReactContext.resources
+        val resourceId = resources.getIdentifier("config_navBarInteractionMode", "integer", "android");
+        if (resourceId > 0) {
+            val navBarInteractionMode = resources.getInteger(resourceId)
+            when (navBarInteractionMode) {
+                0, 1 -> promise.resolve("soft-keys")
+                2 -> promise.resolve("gesture-bar")
+            }
+        }
+        promise.resolve("soft-keys")
+    }
+}
