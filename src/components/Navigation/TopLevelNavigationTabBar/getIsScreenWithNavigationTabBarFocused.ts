8c548a8ae8ee678eaed2fcf393fd76f022c14c52
dmkt9 <demoskratos9x@gmail.com>
2025-09-17 16:11:23 +0700

    Fix - Bottom navigation overlaps room content

--- src/components/Navigation/TopLevelNavigationTabBar/getIsScreenWithNavigationTabBarFocused.ts
+++ src/components/Navigation/TopLevelNavigationTabBar/getIsScreenWithNavigationTabBarFocused.ts
@@ -6,9 +6,10 @@ import SCREENS_WITH_NAVIGATION_TAB_BAR from './SCREENS_WITH_NAVIGATION_TAB_BAR';
 function getIsScreenWithNavigationTabBarFocused(state: NavigationState) {
     const focusedRoute = findFocusedRoute(state);
 
+    const routeName = focusedRoute?.params && 'screen' in focusedRoute.params ? (focusedRoute.params.screen as string) : focusedRoute?.name;
     // We are checking if the focused route is a split navigator because there may be a brief moment where the navigator doesn't have state yet.
     // That mens we don't have screen with bottom tab focused. This caused glitching.
-    return SCREENS_WITH_NAVIGATION_TAB_BAR.includes(focusedRoute?.name ?? '') || isSplitNavigatorName(focusedRoute?.name);
+    return SCREENS_WITH_NAVIGATION_TAB_BAR.includes(routeName ?? '') || isSplitNavigatorName(routeName);
 }
 
 export default getIsScreenWithNavigationTabBarFocused;
