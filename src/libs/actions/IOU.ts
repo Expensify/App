5cb05a5bb623b1a6541f0ac77e75db7819e2bf7c
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-09-18 00:20:44 +0800

    only update nonReimbursableTotal if the expense is not reimbursable

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -3458,7 +3458,10 @@ function getMoneyRequestInformation(moneyRequestInformation: MoneyRequestInforma
                 if (!Number.isNaN(iouReport.total) && iouReport.total !== undefined) {
                     iouReport.total -= amount;
                 }
-                iouReport.nonReimbursableTotal = (iouReport.nonReimbursableTotal ?? 0) - amount;
+
+                if (!reimbursable) {
+                    iouReport.nonReimbursableTotal = (iouReport.nonReimbursableTotal ?? 0) - amount;
+                }
 
                 if (typeof iouReport.unheldTotal === 'number') {
                     iouReport.unheldTotal -= amount;
