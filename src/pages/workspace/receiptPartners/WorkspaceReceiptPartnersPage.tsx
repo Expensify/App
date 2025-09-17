de8dec1d7e20b0ff8ceb44c4e25787a18ecb3c2c
Nicolay Arefyeu <n.arefyev91@gmail.com>
2025-09-17 13:24:30 +0300

    Use loading for Connect button until Uber data will be ready to use

--- src/pages/workspace/receiptPartners/WorkspaceReceiptPartnersPage.tsx
+++ src/pages/workspace/receiptPartners/WorkspaceReceiptPartnersPage.tsx
@@ -187,6 +187,7 @@ function WorkspaceReceiptPartnersPage({route}: WorkspaceReceiptPartnersPageProps
                                 text={translate('workspace.accounting.setup')}
                                 style={styles.justifyContentCenter}
                                 small
+                                isLoading={!policy?.receiptPartners?.uber}
                                 isDisabled={isOffline}
                             />
                         ),
@@ -204,7 +205,7 @@ function WorkspaceReceiptPartnersPage({route}: WorkspaceReceiptPartnersPageProps
         styles.sectionMenuItemTopDescription,
         styles.pr2,
         styles.justifyContentCenter,
-        policy?.receiptPartners?.uber?.errorFields,
+        policy?.receiptPartners?.uber,
         isUberConnected,
         calculateAndSetThreeDotsMenuPosition,
         isOffline,
