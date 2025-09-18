d945dfaef688d8d3aebbb2c6fd318e82c8ed3e43
nkdengineer <nkdengineer@outlook.com>
2025-09-18 22:27:48 +0700

    fix: Submit option is missing in More if retract and hold expense

--- tests/unit/ReportSecondaryActionUtilsTest.ts
+++ tests/unit/ReportSecondaryActionUtilsTest.ts
@@ -30,6 +30,7 @@ const PERSONAL_DETAILS = {
 const REPORT_ID = 1;
 const POLICY_ID = 'POLICY_ID';
 const OLD_POLICY_ID = 'OLD_POLICY_ID';
+
 describe('getSecondaryAction', () => {
     beforeAll(() => {
         Onyx.init({
@@ -115,6 +116,43 @@ describe('getSecondaryAction', () => {
         await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${REPORT_ID}`, report);
 
         const result = getSecondaryReportActions({report, chatReport, reportTransactions: [], violations: {}, policy});
+        expect(result.includes(CONST.REPORT.SECONDARY_ACTIONS.SUBMIT)).toBe(true);
+    });
+
+    it('include SUBMIT option if the report is retracted', async () => {
+        const report = {
+            reportID: REPORT_ID,
+            type: CONST.REPORT.TYPE.EXPENSE,
+            stateNum: CONST.REPORT.STATE_NUM.OPEN,
+            statusNum: CONST.REPORT.STATUS_NUM.OPEN,
+            total: 10,
+            ownerAccountID: EMPLOYEE_ACCOUNT_ID,
+            hasReportBeenReopened: true,
+        } as unknown as Report;
+        const policy = {
+            autoReportingFrequency: CONST.POLICY.AUTO_REPORTING_FREQUENCIES.MANUAL,
+            harvesting: {
+                enabled: true,
+            },
+            role: CONST.POLICY.ROLE.USER,
+        } as unknown as Policy;
+
+        const transaction = {
+            transactionID: 'TRANSACTION_ID',
+            amount: 10,
+            merchant: 'Merchant',
+            date: '2025-01-01',
+            comment: {
+                hold: 'hold',
+            },
+        } as unknown as Transaction;
+
+        await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT}${REPORT_ID}`, report);
+
+        jest.spyOn(ReportActionsUtils, 'getOneTransactionThreadReportID').mockReturnValue('12345');
+        jest.spyOn(ReportUtils, 'isHoldCreator').mockReturnValue(true);
+
+        const result = getSecondaryReportActions({report, chatReport, reportTransactions: [transaction], violations: {}, policy});
         expect(result.includes(CONST.REPORT.SECONDARY_ACTIONS.SUBMIT)).toBe(true);
     });
 
