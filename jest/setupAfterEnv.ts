98e2e430019bc54a9e840b86f4112a1f423ec949
eliran goshen <elirangoshen@gmail.com>
2025-09-19 11:52:07 +0200

    moving log mock to setup,ts

--- jest/setupAfterEnv.ts
+++ jest/setupAfterEnv.ts
@@ -1,21 +1,3 @@
 import '@testing-library/react-native';
-import * as Log from '../scripts/utils/Logger';
-
-const isVerbose = process.argv.includes('--verbose') || process.env.JEST_VERBOSE === 'true';
-
-beforeEach(() => {
-    if (isVerbose) {
-        return;
-    }
-
-    jest.spyOn(console, 'log').mockImplementation(() => {});
-    jest.spyOn(console, 'info').mockImplementation(() => {});
-    jest.spyOn(console, 'debug').mockImplementation(() => {});
-    jest.spyOn(console, 'warn').mockImplementation(() => {});
-    jest.spyOn(Log, 'log').mockImplementation(() => {});
-    jest.spyOn(Log, 'info').mockImplementation(() => {});
-    jest.spyOn(Log, 'success').mockImplementation(() => {});
-    jest.spyOn(Log, 'warn').mockImplementation(() => {});
-});
 
 jest.useRealTimers();
