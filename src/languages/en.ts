e7a02a8d1d6826fc87c2aff53107ac1df2f54cdf
Rutika Pawar <183392827+twilight2294@users.noreply.github.com>
2025-09-22 11:37:42 +0000

    fix white spaces

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -4852,7 +4852,7 @@ const translations = {
             invalidTagNameError: 'Tag name cannot be 0. Please choose a different value.',
             genericFailureMessage: 'An error occurred while updating the tag, please try again',
             importedFromAccountingSoftware: 'Tags are managed in your',
-            employeesSeeTagsAs: ({customTagName}: EmployeesSeeTagsAsParams) => ` <muted-text> Employees see tags as <strong>${customTagName}</strong>.</muted-text>`,
+            employeesSeeTagsAs: ({customTagName}: EmployeesSeeTagsAsParams) => `<muted-text>Employees see tags as <strong>${customTagName}</strong>.</muted-text>`,
             glCode: 'GL code',
             updateGLCodeFailureMessage: 'An error occurred while updating the GL code, please try again',
             tagRules: 'Tag rules',
