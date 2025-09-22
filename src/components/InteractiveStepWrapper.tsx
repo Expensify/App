498ec89cc0ffc9f5df5a3ef1acd1f1e8e25a03f3
Jakub Kaliński <kubakalinski2@gmail.com>
2025-09-22 10:35:43 +0200

    Removed forwardRef from InteractiveStepWrapper.tsx

--- src/components/InteractiveStepWrapper.tsx
+++ src/components/InteractiveStepWrapper.tsx
@@ -1,4 +1,5 @@
-import React, {forwardRef} from 'react';
+import type { ForwardedRef } from 'react';
+import React from 'react';
 import type {StyleProp, ViewStyle} from 'react-native';
 import {View} from 'react-native';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -53,6 +54,9 @@ type InteractiveStepWrapperProps = {
      * This flag can be removed, once all components/screens have switched to edge-to-edge safe area handling.
      */
     enableEdgeToEdgeBottomSafeAreaPadding?: boolean;
+
+    // Reference to the outer element
+    ref?: ForwardedRef<View>;
 };
 
 function InteractiveStepWrapper(
@@ -70,8 +74,8 @@ function InteractiveStepWrapper(
         offlineIndicatorStyle,
         shouldKeyboardOffsetBottomSafeAreaPadding,
         enableEdgeToEdgeBottomSafeAreaPadding,
+        ref,
     }: InteractiveStepWrapperProps,
-    ref: React.ForwardedRef<View>,
 ) {
     const styles = useThemeStyles();
 
@@ -107,4 +111,4 @@ function InteractiveStepWrapper(
 
 InteractiveStepWrapper.displayName = 'InteractiveStepWrapper';
 
-export default forwardRef(InteractiveStepWrapper);
+export default InteractiveStepWrapper;
