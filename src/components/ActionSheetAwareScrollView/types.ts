d95407b0096c57643b95491760ae8db1b0f5d40d
Jakub Kaliński <kubakalinski2@gmail.com>
2025-09-22 15:19:10 +0200

    Removed forwardRef from ActionSheetAwareScrollView component

--- src/components/ActionSheetAwareScrollView/types.ts
+++ src/components/ActionSheetAwareScrollView/types.ts
@@ -1,9 +1,11 @@
-import type {PropsWithChildren} from 'react';
-import type {ScrollViewProps} from 'react-native';
+import type {PropsWithChildren, Ref} from 'react';
+// eslint-disable-next-line no-restricted-imports
+import type {ScrollView, ScrollViewProps} from 'react-native';
 
 type ActionSheetAwareScrollViewAnimationProps = {
     /** Whether the child ScrollView is inverted */
     isInvertedScrollView?: boolean;
+    ref?: Ref<ScrollView>;
 };
 type ActionSheetAwareScrollViewProps = PropsWithChildren<ScrollViewProps> & ActionSheetAwareScrollViewAnimationProps;
 type RenderActionSheetAwareScrollViewComponent = ((props: ActionSheetAwareScrollViewProps) => React.ReactElement<ScrollViewProps>) | undefined;
