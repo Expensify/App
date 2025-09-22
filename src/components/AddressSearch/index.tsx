ce775040e9ae6c70647d7b5ba98876fa237a1323
Jakub Kaliński <kubakalinski2@gmail.com>
2025-09-22 15:24:55 +0200

    Removed forwardRef from AddressSearch component

--- src/components/AddressSearch/index.tsx
+++ src/components/AddressSearch/index.tsx
@@ -1,5 +1,4 @@
-import React, {forwardRef, useEffect, useMemo, useRef, useState} from 'react';
-import type {ForwardedRef} from 'react';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
 import {Keyboard, LogBox, View} from 'react-native';
 import type {LayoutChangeEvent} from 'react-native';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
@@ -80,8 +79,8 @@ function AddressSearch(
         value,
         locationBias,
         caretHidden,
+        ref,
     }: AddressSearchProps,
-    ref: ForwardedRef<HTMLElement>,
 ) {
     const theme = useTheme();
     const styles = useThemeStyles();
@@ -490,6 +489,6 @@ function AddressSearch(
 
 AddressSearch.displayName = 'AddressSearchWithRef';
 
-export default forwardRef(AddressSearch);
+export default AddressSearch;
 
 export type {AddressSearchProps};
