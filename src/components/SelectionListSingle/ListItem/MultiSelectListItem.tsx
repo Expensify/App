5dd862bae88475b0ff6bd50ad2cd4ecd250f7e96
Zuzanna Furtak <zuzkafurtak@gmail.com>
2025-09-22 19:59:22 +0200

    Change SelectionList in MultiSelectPopup component

---
+++ src/components/SelectionListSingle/ListItem/MultiSelectListItem.tsx
@@ -0,0 +1,67 @@
+import React, {useCallback} from 'react';
+import Checkbox from '@components/Checkbox';
+import useThemeStyles from '@hooks/useThemeStyles';
+import RadioListItem from './RadioListItem';
+import type {ListItem, MultiSelectListItemProps} from './types';
+
+/**
+ * MultiSelectListItem mirrors the behavior of a default RadioListItem, but adds support
+ * for the new style of multi selection lists.
+ */
+function MultiSelectListItem<TItem extends ListItem>({
+    item,
+    isFocused,
+    showTooltip,
+    isDisabled,
+    onSelectRow,
+    onDismissError,
+    shouldPreventEnterKeySubmit,
+    isMultilineSupported = false,
+    isAlternateTextMultilineSupported = false,
+    alternateTextNumberOfLines = 2,
+    onFocus,
+    shouldSyncFocus,
+    wrapperStyle,
+    titleStyles,
+    accessibilityState,
+}: MultiSelectListItemProps<TItem>) {
+    const styles = useThemeStyles();
+    const isSelected = item.isSelected;
+
+    const checkboxComponent = useCallback(() => {
+        return (
+            <Checkbox
+                shouldSelectOnPressEnter
+                isChecked={isSelected}
+                accessibilityLabel={item.text ?? ''}
+                onPress={() => onSelectRow(item)}
+            />
+        );
+    }, [isSelected, item, onSelectRow]);
+
+    return (
+        <RadioListItem
+            item={item}
+            keyForList={item.keyForList}
+            isFocused={isFocused}
+            showTooltip={showTooltip}
+            isDisabled={isDisabled}
+            rightHandSideComponent={checkboxComponent}
+            onSelectRow={onSelectRow}
+            onDismissError={onDismissError}
+            shouldPreventEnterKeySubmit={shouldPreventEnterKeySubmit}
+            isMultilineSupported={isMultilineSupported}
+            isAlternateTextMultilineSupported={isAlternateTextMultilineSupported}
+            alternateTextNumberOfLines={alternateTextNumberOfLines}
+            onFocus={onFocus}
+            shouldSyncFocus={shouldSyncFocus}
+            wrapperStyle={[wrapperStyle, styles.optionRowCompact]}
+            titleStyles={titleStyles}
+            accessibilityState={accessibilityState}
+        />
+    );
+}
+
+MultiSelectListItem.displayName = 'MultiSelectListItem';
+
+export default MultiSelectListItem;
--- src/components/SelectionListSingle/ListItem/types.ts
+++ src/components/SelectionListSingle/ListItem/types.ts
@@ -254,4 +254,16 @@ type RadioListItemProps<TItem extends ListItem> = ListItemProps<TItem>;
 
 type SingleSelectListItemProps<TItem extends ListItem> = ListItemProps<TItem>;
 
-export type {BaseListItemProps, ExtendedTargetedEvent, ListItem, ListItemProps, ListItemFocusEventHandler, RadioListItemProps, SingleSelectListItemProps, ValidListItem};
+type MultiSelectListItemProps<TItem extends ListItem> = ListItemProps<TItem>;
+
+export type {
+    BaseListItemProps,
+    ExtendedTargetedEvent,
+    ListItem,
+    ListItemProps,
+    ListItemFocusEventHandler,
+    MultiSelectListItemProps,
+    RadioListItemProps,
+    SingleSelectListItemProps,
+    ValidListItem,
+};
