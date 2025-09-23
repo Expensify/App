766153a1864f4ded454275bf48e3d20e345b197f
Eskalifer1 <artemkryt1800@gmail.com>
2025-09-23 12:37:27 +0300

    chore: design team comment

--- src/pages/settings/Profile/CustomStatus/StatusPage.tsx
+++ src/pages/settings/Profile/CustomStatus/StatusPage.tsx
@@ -202,7 +202,7 @@ function StatusPage() {
                 <View style={[styles.mh5, styles.mv1]}>
                     <Text style={[styles.textNormal, styles.mt2]}>{translate('statusPage.statusExplanation')}</Text>
                 </View>
-                <View style={[styles.mb2, styles.mt4]}>
+                <View style={[styles.mt4]}>
                     <View style={[styles.mb4, styles.ph5]}>
                         <InputWrapper
                             InputComponent={EmojiPickerButtonDropdown}
