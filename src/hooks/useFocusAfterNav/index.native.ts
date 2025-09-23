b06c7054a0e45b1e0596cb1630d0d93fb53633e3
krishna2323 <belivethatkg@gmail.com>
2025-09-23 17:53:37 +0530

    use requestAnimationFrame.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/hooks/useFocusAfterNav/index.native.ts
+++ src/hooks/useFocusAfterNav/index.native.ts
@@ -2,7 +2,6 @@ import {useFocusEffect} from '@react-navigation/native';
 import {useCallback} from 'react';
 import type {RefObject} from 'react';
 import type {AnimatedTextInputRef} from '@components/RNTextInput';
-import CONST from '@src/CONST';
 
 /** We added a delay to focus on text input to allow navigation/modal animations to get completed,
  *see issue https://github.com/Expensify/App/issues/65855 for more details
@@ -10,16 +9,12 @@ import CONST from '@src/CONST';
 const useFocusAfterNav = (ref: RefObject<AnimatedTextInputRef | null>) => {
     useFocusEffect(
         useCallback(() => {
-            const timeoutId = setTimeout(() => {
+            requestAnimationFrame(() => {
                 if (!ref.current) {
                     return;
                 }
                 ref.current.focus();
-            }, CONST.ANIMATED_TRANSITION);
-
-            return () => {
-                clearTimeout(timeoutId);
-            };
+            });
         }, [ref]),
     );
     return false;
