bbeff65c4cddb497837c87bdd90d443ac821c853
Yehor Kharchenko <yehor.kharchenko@swmansion.com>
2025-09-23 13:48:57 +0200

    added possible names for screen access object

--- src/libs/Navigation/VerifyAccountMappings.ts
+++ src/libs/Navigation/VerifyAccountMappings.ts
@@ -1,5 +1,10 @@
 import SCREENS from '@src/SCREENS';
 
+// possible names for this new functionality
+// INCOMING_SCREENS
+// SCREEN_ENTRY_RULES
+// SCREEN_ACCESS_MAP
+
 const MAPPINGS = {
     VERIFY_ACCOUNT: [SCREENS.REPORT, SCREENS.SEARCH.REPORT_RHP],
 };
