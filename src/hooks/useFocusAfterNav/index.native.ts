e94ca353ee6412f10c780a9b0c56b57f8818e5af
krishna2323 <belivethatkg@gmail.com>
2025-09-23 17:51:41 +0530

    update hook to use useFocusEffect.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/hooks/useFocusAfterNav/index.native.ts
+++ src/hooks/useFocusAfterNav/index.native.ts
@@ -1,17 +1,27 @@
+import {useFocusEffect} from '@react-navigation/native';
 import {useCallback} from 'react';
 import type {RefObject} from 'react';
 import type {AnimatedTextInputRef} from '@components/RNTextInput';
+import CONST from '@src/CONST';
 
 /** We added a delay to focus on text input to allow navigation/modal animations to get completed,
  *see issue https://github.com/Expensify/App/issues/65855 for more details
  */
 const useFocusAfterNav = (ref: RefObject<AnimatedTextInputRef | null>) => {
-    useCallback(() => {
-        // Focus on the next frame so navigation/animation completes first
-        requestAnimationFrame(() => {
-            ref.current?.focus();
-        });
-    }, [ref]);
+    useFocusEffect(
+        useCallback(() => {
+            const timeoutId = setTimeout(() => {
+                if (!ref.current) {
+                    return;
+                }
+                ref.current.focus();
+            }, CONST.ANIMATED_TRANSITION);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }, [ref]),
+    );
     return false;
 };
 
