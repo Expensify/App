18ecdcdf0b51538995b6219feca86d5f0584c478
krishna2323 <belivethatkg@gmail.com>
2025-09-23 17:57:09 +0530

    revert back to setTimeout.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/hooks/useFocusAfterNav/index.native.ts
+++ src/hooks/useFocusAfterNav/index.native.ts
@@ -2,6 +2,7 @@ import {useFocusEffect} from '@react-navigation/native';
 import {useCallback} from 'react';
 import type {RefObject} from 'react';
 import type {AnimatedTextInputRef} from '@components/RNTextInput';
+import CONST from '@src/CONST';
 
 /** We added a delay to focus on text input to allow navigation/modal animations to get completed,
  *see issue https://github.com/Expensify/App/issues/65855 for more details
@@ -9,12 +10,16 @@ import type {AnimatedTextInputRef} from '@components/RNTextInput';
 const useFocusAfterNav = (ref: RefObject<AnimatedTextInputRef | null>) => {
     useFocusEffect(
         useCallback(() => {
-            requestAnimationFrame(() => {
+            const timeoutId = setTimeout(() => {
                 if (!ref.current) {
                     return;
                 }
                 ref.current.focus();
-            });
+            }, CONST.ANIMATED_TRANSITION);
+
+            return () => {
+                clearTimeout(timeoutId);
+            };
         }, [ref]),
     );
     return false;
