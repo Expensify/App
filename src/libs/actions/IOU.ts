28a14df330a11302b9148d5115cded7b772b38ef
dariusz-biela <dariusz.biela.dev@gmail.com>
2025-09-23 17:00:24 +0200

    refactor: adds comment to allRecentlyUsedTags in src/libs/actions/IOU.ts

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -816,6 +816,9 @@ Onyx.connect({
     },
 });
 
+// TODO: remove `allRecentlyUsedTags` from this file
+// `allRecentlyUsedTags` was moved here temporarily from `src/libs/actions/Policy/Tag.ts` during the `Deprecate Onyx.connect` refactor.
+// All uses of this variable should be replaced with `useOnyx`.
 let allRecentlyUsedTags: OnyxCollection<RecentlyUsedTags> = {};
 Onyx.connect({
     key: ONYXKEYS.COLLECTION.POLICY_RECENTLY_USED_TAGS,
