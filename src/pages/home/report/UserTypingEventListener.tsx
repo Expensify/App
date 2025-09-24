93b6a25b03c7aea24dda84d6851dcae89ef0c951
Novell <76243292+NJ-2020@users.noreply.github.com>
2025-09-24 11:02:43 +0700

    fallback lastVisitedPath to empty string
    Co-authored-by: dukenv0307 <129500732+dukenv0307@users.noreply.github.com>

--- src/pages/home/report/UserTypingEventListener.tsx
+++ src/pages/home/report/UserTypingEventListener.tsx
@@ -16,7 +16,7 @@ type UserTypingEventListenerProps = {
     report: OnyxTypes.Report;
 };
 function UserTypingEventListener({report}: UserTypingEventListenerProps) {
-    const [lastVisitedPath] = useOnyx(ONYXKEYS.LAST_VISITED_PATH, {canBeMissing: true, selector: lastVisitedPathSelector});
+    const [lastVisitedPath = ''] = useOnyx(ONYXKEYS.LAST_VISITED_PATH, {canBeMissing: true});
     const didSubscribeToReportTypingEvents = useRef(false);
     const reportID = report.reportID;
     const isFocused = useIsFocused();
