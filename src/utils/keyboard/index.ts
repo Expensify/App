dada05e782195adb6c5ef1af0ca4cac147e5c2ef
Jan Musiał <jan.musial@swmansion.com>
2025-09-25 17:59:17 +0200

    Delay emoji modal opening

--- src/utils/keyboard/index.ts
+++ src/utils/keyboard/index.ts
@@ -1,4 +1,5 @@
 import {Keyboard} from 'react-native';
+import {KeyboardEvents} from 'react-native-keyboard-controller';
 
 let isVisible = false;
 
@@ -27,6 +28,26 @@ const dismiss = (): Promise<void> => {
     });
 };
 
-const utils = {dismiss};
+const executeAfterKeyboardDidHide = (cb: () => void): Promise<void> => {
+    if (!isVisible) {
+        cb();
+    }
+    return new Promise((resolve) => {
+        const check = (e: {height: number}) => {
+            if (e.height !== 0) {
+                return;
+            }
+            cb();
+            resolve();
+            // eslint-disable-next-line @typescript-eslint/no-use-before-define
+            test.remove();
+        };
+
+        const test = KeyboardEvents.addListener('keyboardDidHide', check);
+        Keyboard.dismiss();
+    });
+};
+
+const utils = {dismiss, executeAfterKeyboardDidHide};
 
 export default utils;
