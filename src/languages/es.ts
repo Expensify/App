03fe9d407af9b8ae7e906dd564001a283b0ec1f9
Rory Abraham <47436092+roryabraham@users.noreply.github.com>
2025-09-24 19:19:07 -0700

    Merge pull request #71257 from Expensify/francois-revert-70821
    [CP Staging] Revert #70821 "Add active-workspace-specific fields to the Unreported Expenses"
    
    (cherry picked from commit 3ea59461359568914418d39c8aff5756b9fb9848)
    
    (cherry-picked to staging by roryabraham)

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -5547,11 +5547,6 @@ const translations = {
                     'Expensify Travel es una nueva plataforma corporativa de reserva y gestión de viajes que permite a los miembros reservar alojamientos, vuelos, transporte y mucho más.',
                 onlyAvailableOnPlan: 'Los viajes están disponibles en el plan Recopilar, a partir de ',
             },
-            reports: {
-                title: 'Informes',
-                description: 'Crea informes de gastos organizados para hacer seguimiento de tus gastos comerciales, enviarlos para aprobación y optimizar tu proceso de reembolso.',
-                onlyAvailableOnPlan: 'Los informes están disponibles en el plan Recopilar, a partir de ',
-            },
             multiLevelTags: {
                 title: 'Etiquetas multinivel',
                 description:
