7787465e6dc32e44a4dad86d6ac71b0dd4870e21
Olgierd <olgierd.smyka@swmansion.com>
2025-10-01 11:07:59 +0200

    Name props type correctly

--- src/pages/Search/SearchReportVerifyAccountPage.tsx
+++ src/pages/Search/SearchReportVerifyAccountPage.tsx
@@ -5,9 +5,9 @@ import VerifyAccountPageBase from '@pages/settings/VerifyAccountPageBase';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
 
-type SearchReportVerifyAccountPageParamList = PlatformStackScreenProps<SearchReportParamList, typeof SCREENS.SEARCH.REPORT_VERIFY_ACCOUNT>;
+type SearchReportVerifyAccountPageProps = PlatformStackScreenProps<SearchReportParamList, typeof SCREENS.SEARCH.REPORT_VERIFY_ACCOUNT>;
 
-function SearchReportVerifyAccountPage({route}: SearchReportVerifyAccountPageParamList) {
+function SearchReportVerifyAccountPage({route}: SearchReportVerifyAccountPageProps) {
     return <VerifyAccountPageBase navigateBackTo={ROUTES.SEARCH_REPORT.getRoute({reportID: route.params.reportID})} />;
 }
 
