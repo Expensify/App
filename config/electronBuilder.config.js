ff9bc65c477c929302c62ea4e3ab020c35b11b48
Michał Jasikowski <michal@expensify.com>
2025-10-01 15:53:02 +0200

    afterPack typescript workflow changes

--- config/electronBuilder.config.js
+++ config/electronBuilder.config.js
@@ -86,5 +86,5 @@ module.exports = {
         name: 'New Expensify',
         schemes: ['new-expensify'],
     },
-    afterPack: 'desktop/afterPack.ts',
+    afterPack: 'desktop/dist/afterPack.js',
 };
