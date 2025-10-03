df90bcafe555c4224ba852b694541aad187a1abb
Tyler Karaszewski <tyler@tylerkaraszewski.com>
2025-10-03 10:45:18 -0700

    Revert "Back-navigation animation appears when loading a new chat from scratch without Onyx data v2"

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -1886,7 +1886,7 @@ function handleReportChanged(report: OnyxEntry<Report>) {
             const currCallback = callback;
             callback = () => {
                 currCallback();
-                Navigation.setParams({reportID: preexistingReportID.toString()});
+                Navigation.navigate(ROUTES.REPORT_WITH_ID.getRoute(preexistingReportID), {forceReplace: true});
             };
 
             // The report screen will listen to this event and transfer the draft comment to the existing report
