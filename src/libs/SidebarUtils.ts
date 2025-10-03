c688d8fb5197fbc994205a1cbfbf2c542da44f7e
mkzie2 <mkzie2@gmail.com>
2025-10-04 01:58:23 +0800

    fix: pinned room shows no acitivity after workspace is deleted

--- src/libs/SidebarUtils.ts
+++ src/libs/SidebarUtils.ts
@@ -231,7 +231,7 @@ function getReportsToDisplayInLHN(
     const reportsToDisplay: ReportsToDisplayInLHN = {};
 
     Object.entries(allReportsDictValues).forEach(([reportID, report]) => {
-        if (!report) {
+        if (!report || !report.reportID) {
             return;
         }
 
@@ -269,7 +269,7 @@ function updateReportsToDisplayInLHN(
     const displayedReportsCopy = {...displayedReports};
     updatedReportsKeys.forEach((reportID) => {
         const report = reports?.[reportID];
-        if (!report) {
+        if (!report || !report.reportID) {
             return;
         }
 
