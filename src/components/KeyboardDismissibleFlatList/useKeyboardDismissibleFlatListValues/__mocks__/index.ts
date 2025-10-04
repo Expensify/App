2efbe9bdee06a7ea7a9b81d369ce9f24fbfd59f1
Christoph Pader <chris@margelo.com>
2025-10-04 16:03:08 +0100

    fix: useKeyboardDismissableFlatListValues mock

--- src/components/KeyboardDismissibleFlatList/useKeyboardDismissibleFlatListValues/__mocks__/index.ts
+++ src/components/KeyboardDismissibleFlatList/useKeyboardDismissibleFlatListValues/__mocks__/index.ts
@@ -1,29 +1,25 @@
-function useKeyboardDismissibleFlatListValues() {
-    const createMockSharedValue = (initialValue = 0) => ({
-        value: initialValue,
-        get: jest.fn().mockReturnValue(initialValue),
-        set: jest.fn(),
-        addListener: jest.fn().mockReturnValue(0),
-        removeListener: jest.fn(),
-        modify: jest.fn(),
-    });
+const createMockSharedValue = (initialValue = 0) => ({
+    value: initialValue,
+    get: jest.fn().mockReturnValue(initialValue),
+    set: jest.fn(),
+    addListener: jest.fn().mockReturnValue(0),
+    removeListener: jest.fn(),
+    modify: jest.fn(),
+});
 
-    const mockScrollHandler = jest.fn();
-    const mockSetListBehavior = jest.fn();
+const mockScrollHandler = jest.fn();
+const mockSetListBehavior = jest.fn();
 
-    return {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        __esModule: true,
-        default: jest.fn().mockReturnValue({
-            keyboardHeight: createMockSharedValue(0),
-            keyboardOffset: createMockSharedValue(0),
-            scrollY: createMockSharedValue(0),
-            onScroll: mockScrollHandler,
-            contentSizeHeight: createMockSharedValue(0),
-            layoutMeasurementHeight: createMockSharedValue(0),
-            setListBehavior: mockSetListBehavior,
-        }),
-    };
-}
+const mockContextValue = {
+    keyboardHeight: createMockSharedValue(0),
+    keyboardOffset: createMockSharedValue(0),
+    scrollY: createMockSharedValue(0),
+    onScroll: mockScrollHandler,
+    contentSizeHeight: createMockSharedValue(0),
+    layoutMeasurementHeight: createMockSharedValue(0),
+    setListBehavior: mockSetListBehavior,
+};
 
-export default useKeyboardDismissibleFlatListValues;
+const mockUseKeyboardDismissibleFlatListValues = jest.fn().mockReturnValue(mockContextValue);
+
+export default mockUseKeyboardDismissibleFlatListValues;
