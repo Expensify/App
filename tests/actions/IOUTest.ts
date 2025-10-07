d1e12e0d87284c57f30a76a7d2e7648a24271383
DylanDylann <dylandylan9112@gmail.com>
2025-10-07 16:37:19 +0700

    update UTs

--- tests/actions/IOUTest.ts
+++ tests/actions/IOUTest.ts
@@ -4816,7 +4816,15 @@ describe('actions/IOU', () => {
                 )
                 .then(() => {
                     if (policy) {
-                        deleteWorkspace(policy.id, policy.name, undefined, undefined, [expenseReport as Report, chatReport as Report], undefined, undefined);
+                        const reportToArchive = [];
+                        if (expenseReport) {
+                            reportToArchive.push(expenseReport);
+                        }
+                        if (chatReport) {
+                            reportToArchive.push(chatReport);
+                        }
+                        // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+                        deleteWorkspace(policy.id, policy.name, undefined, undefined, reportToArchive, undefined, undefined);
                     }
                     return waitForBatchedUpdates();
                 })
