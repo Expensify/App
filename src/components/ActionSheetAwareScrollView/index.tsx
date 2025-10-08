b4d773e9eb9aa955976f584704a6f5547f97eaca
Christoph Pader <christoph.pader@outlook.com>
2025-10-08 21:36:10 +0100

    Update src/components/ActionSheetAwareScrollView/index.tsx
    Co-authored-by: Rajat <parasharrajat@users.noreply.github.com>

--- src/components/ActionSheetAwareScrollView/index.tsx
+++ src/components/ActionSheetAwareScrollView/index.tsx
@@ -31,7 +31,7 @@ export default ActionSheetAwareScrollView;
 
 /**
  * The bottom spacing config for this action sheet is only used on Android and iOS. On other platforms,
- * this component will be a default Animated.ScrollView, because the onScroll handler used is from Reanimated.
+ * this component will be a default Reanimated.ScrollView, because the onScroll handler used is from Reanimated.
  *
  * This function should be used as renderScrollComponent prop for FlatList
  * @param {Object} props - props that will be passed to the ScrollView from FlatList
