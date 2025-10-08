d180fc3bc9ab82aff869921fb92b5c838d9dec42
Jan Musiał <jan.musial@swmansion.com>
2025-10-08 16:53:06 +0200

    update prompt

--- src/pages/workspace/invoices/WorkspaceInvoiceVBASection.tsx
+++ src/pages/workspace/invoices/WorkspaceInvoiceVBASection.tsx
@@ -176,7 +176,7 @@ function WorkspaceInvoiceVBASection({policyID}: WorkspaceInvoiceVBASectionProps)
                             !shouldUseNarrowLayout
                                 ? {
                                       ...styles.sidebarPopover,
-                                      ...styles.pv6
+                                      ...styles.pv6,
                                       ...styles.pv4,
                                   }
                                 : styles.pt5,
