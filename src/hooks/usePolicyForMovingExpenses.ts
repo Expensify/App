5e3a865cbe3de6b475efd5684c18a80a4fd39f2e
Agata Kosior <agata.kosior57@gmail.com>
2025-10-08 12:43:07 +0200

    fix: filter policies when no employeeList

--- src/hooks/usePolicyForMovingExpenses.ts
+++ src/hooks/usePolicyForMovingExpenses.ts
@@ -1,8 +1,20 @@
 import {activePolicySelector} from '@selectors/Policy';
+import type {OnyxEntry} from 'react-native-onyx';
 import {useSession} from '@components/OnyxListItemProvider';
-import {isPaidGroupPolicy, isPolicyMemberWithoutPendingDelete} from '@libs/PolicyUtils';
+import {isPaidGroupPolicy, isPolicyMemberWithoutPendingDelete, isPolicyUser, isUserPolicyAdmin} from '@libs/PolicyUtils';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {Policy} from '@src/types/onyx';
+import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import useOnyx from './useOnyx';
+
+// TODO: temporary util - if we don't have employeeList object we don't check for the pending delete
+function checkForPendingDelete(login: string, policy: OnyxEntry<Policy>) {
+    if (isEmptyObject(policy?.employeeList)) {
+        return true;
+    }
+    return isPolicyMemberWithoutPendingDelete(login, policy);
+}
 
 function usePolicyForMovingExpenses() {
     const [allPolicies] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {canBeMissing: true});
@@ -13,7 +25,14 @@ function usePolicyForMovingExpenses() {
     });
 
     const session = useSession();
-    const userPolicies = Object.values(allPolicies ?? {}).filter((policy) => isPolicyMemberWithoutPendingDelete(session?.email, policy) && isPaidGroupPolicy(policy));
+    const login = session?.email ?? '';
+    const userPolicies = Object.values(allPolicies ?? {}).filter(
+        (policy) =>
+            checkForPendingDelete(login, policy) &&
+            (isUserPolicyAdmin(policy, login) || isPolicyUser(policy, login)) &&
+            isPaidGroupPolicy(policy) &&
+            policy?.pendingAction !== CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE,
+    );
     const isMemberOfMoreThanOnePolicy = userPolicies.length > 1;
 
     if (activePolicy) {
