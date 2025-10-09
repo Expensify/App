099723b61992c0c866391ba4f8d82743d5c297bc
Sachin Chavda <Sachin.chavda.1024@gmail.com>
2025-10-09 22:41:19 +0530

    Refactor RadioButton and OptionItem component to use new style consistently

--- src/components/OptionsPicker/OptionItem.tsx
+++ src/components/OptionsPicker/OptionItem.tsx
@@ -3,7 +3,6 @@ import type {StyleProp, ViewStyle} from 'react-native';
 import {View} from 'react-native';
 import Icon from '@components/Icon';
 import {PressableWithFeedback} from '@components/Pressable';
-import SelectCircle from '@components/SelectCircle';
 import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -11,6 +10,7 @@ import variables from '@styles/variables';
 import CONST from '@src/CONST';
 import type {TranslationPaths} from '@src/languages/types';
 import type IconAsset from '@src/types/utils/IconAsset';
+import RadioButton from '@components/RadioButton';
 
 type OptionItemProps = {
     /** Text to be rendered */
@@ -54,9 +54,11 @@ function OptionItem({title, icon, onPress, isSelected = false, isDisabled, style
                         />
                         {!isDisabled && (
                             <View>
-                                <SelectCircle
+                                <RadioButton
                                     isChecked={isSelected}
-                                    selectCircleStyles={styles.sectionSelectCircle}
+                                    onPress={() => onPress?.()}
+                                    accessibilityLabel={CONST.ROLE.RADIO}
+                                    shouldUseNewStyle
                                 />
                             </View>
                         )}
