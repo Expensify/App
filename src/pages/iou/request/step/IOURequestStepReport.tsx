6a25493e4e3b1a7b9177c0d5ebefba8d702ecb8b
Cristi Paval <cristi@expensify.com>
2025-10-09 13:03:49 +0300

    Merge pull request #72171 from callstack-internal/fix/72148-show-paywall-on-create-report
    [CP Staging] Show paywall when moving from the reports
    
    (cherry picked from commit bad2b721961d6171a2ab74c8696b21599bdd6385)
    
    (cherry-picked to staging by lakchote)

--- src/pages/iou/request/step/IOURequestStepReport.tsx
+++ src/pages/iou/request/step/IOURequestStepReport.tsx
@@ -12,6 +12,7 @@ import {changeTransactionsReport, setTransactionReport} from '@libs/actions/Tran
 import Navigation from '@libs/Navigation/Navigation';
 import Permissions from '@libs/Permissions';
 import {getReportOrDraftReport, hasViolations as hasViolationsReportUtils, isPolicyExpenseChat, isReportOutstanding} from '@libs/ReportUtils';
+import {shouldRestrictUserBillableActions} from '@libs/SubscriptionUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
@@ -171,9 +172,16 @@ function IOURequestStepReport({route, transaction}: IOURequestStepReportProps) {
     const shouldShowNotFoundPage = useShowNotFoundPageInIOUStep(action, iouType, reportActionID, reportOrDraftReport, transaction);
 
     const createReport = () => {
+        if (!policyForMovingExpensesID && !shouldSelectPolicy) {
+            return;
+        }
         if (shouldSelectPolicy) {
             setSelectedTransactions([transactionID]);
             Navigation.navigate(ROUTES.NEW_REPORT_WORKSPACE_SELECTION.getRoute(true, backTo));
+            return;
+        }
+        if (policyForMovingExpensesID && shouldRestrictUserBillableActions(policyForMovingExpensesID)) {
+            Navigation.navigate(ROUTES.RESTRICTED_ACTION.getRoute(policyForMovingExpensesID));
             return;
         }
         const createdReportID = createNewReport(currentUserPersonalDetails, hasViolations, isASAPSubmitBetaEnabled, policyForMovingExpensesID);
