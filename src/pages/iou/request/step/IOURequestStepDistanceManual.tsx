ee57f720b4da14cf7cc225bada5c5fab36f05c94
Lucien Akchoté <lucien@expensify.com>
2025-10-10 10:02:15 +0200

    Merge pull request #72252 from s77rt/per-diem-transaction
    [CP Staging] Fix per diem transaction report id
    
    (cherry picked from commit 37af2065625680fd1ce009e562879c0c1d8cc640)
    
    (cherry-picked to staging by lakchote)

--- src/pages/iou/request/step/IOURequestStepDistanceManual.tsx
+++ src/pages/iou/request/step/IOURequestStepDistanceManual.tsx
@@ -244,14 +244,14 @@ function IOURequestStepDistanceManual({
                 !shouldRestrictUserBillableActions(defaultExpensePolicy.id)
             ) {
                 const activePolicyExpenseChat = getPolicyExpenseChat(currentUserPersonalDetails.accountID, defaultExpensePolicy?.id);
+                const shouldAutoReport = !!defaultExpensePolicy?.autoReporting || !!personalPolicy?.autoReporting;
+                const transactionReportID = shouldAutoReport ? activePolicyExpenseChat?.reportID : CONST.REPORT.UNREPORTED_REPORT_ID;
                 const rateID = DistanceRequestUtils.getCustomUnitRateID({
-                    reportID: activePolicyExpenseChat?.reportID,
+                    reportID: transactionReportID,
                     isPolicyExpenseChat: true,
                     policy: defaultExpensePolicy,
                     lastSelectedDistanceRates,
                 });
-                const shouldAutoReport = !!defaultExpensePolicy?.autoReporting || !!personalPolicy?.autoReporting;
-                const transactionReportID = shouldAutoReport ? activePolicyExpenseChat?.reportID : CONST.REPORT.UNREPORTED_REPORT_ID;
                 setTransactionReport(transactionID, {reportID: transactionReportID}, true);
                 setCustomUnitRateID(transactionID, rateID);
                 setMoneyRequestParticipantsFromReport(transactionID, activePolicyExpenseChat).then(() => {
