b8f5df40c70f1ecc9d09e3a2a1fc9dbbc6ddcc7c
Georgia Monahan <georgia@expensify.com>
2025-10-10 14:56:02 +0100

    clean up

--- src/libs/Avatars/CustomAvatarCatalog.ts
+++ src/libs/Avatars/CustomAvatarCatalog.ts
@@ -172,17 +172,19 @@ const ALL_CUSTOM_AVATARS: Record<CustomAvatarID, AvatarEntry> = {
     ...SEASON_F1,
 };
 
-const ALL_CUSTOM_AVATARS_ORDERED: AvatarEntry[] = (() => {
-    const allIds = Object.keys(ALL_CUSTOM_AVATARS) as CustomAvatarID[];
-    const explicit = DISPLAY_ORDER.filter((id) => id in ALL_CUSTOM_AVATARS) as CustomAvatarID[];
-    const explicitSet = new Set(explicit);
-    const leftovers = allIds.filter((id) => !explicitSet.has(id)).sort();
-    const finalOrder = [...explicit, ...leftovers];
-    return finalOrder.map((id) => ALL_CUSTOM_AVATARS[id]);
-})();
+const buildOrderedAvatars = (): AvatarEntry[] => {
+    const allIDS = Object.keys(ALL_CUSTOM_AVATARS) as CustomAvatarID[];
+    const orderedIDS = DISPLAY_ORDER.filter((id) => id in ALL_CUSTOM_AVATARS);
+    const orderedIDSet = new Set(orderedIDS);
+    const leftoverIDS = allIDS.filter((id) => !orderedIDSet.has(id)).sort();
+    const finalIDOrder = [...orderedIDS, ...leftoverIDS];
+    return finalIDOrder.map((id) => ALL_CUSTOM_AVATARS[id]);
+};
+
+const CUSTOM_AVATAR_CATALOG: AvatarEntry[] = buildOrderedAvatars();
 
 const getAvatarLocal = (id: CustomAvatarID) => ALL_CUSTOM_AVATARS[id].local;
 const getAvatarURL = (id: CustomAvatarID) => ALL_CUSTOM_AVATARS[id].url;
 
-export {ALL_CUSTOM_AVATARS, ALL_CUSTOM_AVATARS_ORDERED, getAvatarLocal, getAvatarURL};
+export {CUSTOM_AVATAR_CATALOG, getAvatarLocal, getAvatarURL};
 export type {DefaultAvatarIDs, SeasonF1AvatarIDs, CustomAvatarID};
