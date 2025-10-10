ae7a016d6ce386ac8a2c4ffd8cfa116aca467fa9
Georgia Monahan <georgia@expensify.com>
2025-10-10 15:25:38 +0100

    use onyx instead

--- src/components/Share/ShareTabParticipantsSelector.tsx
+++ src/components/Share/ShareTabParticipantsSelector.tsx
@@ -36,7 +36,6 @@ function ShareTabParticipantsSelectorComponent({detailsPageRouteObject}: ShareTa
                 }
             }}
             action="create"
-            isFromShareExtension
         />
     );
 }
