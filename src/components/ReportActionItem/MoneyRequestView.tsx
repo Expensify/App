b23aea92f739fde54b5940e3daa7ea6743954a1d
alberto <alberto@expensify.com>
2025-10-10 14:00:30 +0200

    lint

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -67,7 +67,6 @@ import {
     getTagForDisplay,
     getTaxName,
     hasMissingSmartscanFields,
-    hasReceipt,
     hasReservationList,
     hasRoute as hasRouteTransactionUtils,
     isCardTransaction as isCardTransactionTransactionUtils,
