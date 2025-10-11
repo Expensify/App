66a1e979f88d6fc0f91a387425d78987b7976e46
Sachin Chavda <Sachin.chavda.1024@gmail.com>
2025-10-11 17:31:09 +0530

    improve getDefaultItemHeight function

--- src/components/SelectionListWithSections/BaseSelectionListWithSections.tsx
+++ src/components/SelectionListWithSections/BaseSelectionListWithSections.tsx
@@ -43,8 +43,21 @@ import BaseSelectionListItemRenderer from './BaseSelectionListItemRenderer';
 import FocusAwareCellRendererComponent from './FocusAwareCellRendererComponent';
 import type {ButtonOrCheckBoxRoles, FlattenedSectionsReturn, ListItem, SectionListDataType, SectionWithIndexOffset, SelectionListProps} from './types';
 
-const getDefaultItemHeight = () => variables.optionRowHeight;
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getDefaultItemHeight = (ListItem: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    switch (ListItem?.displayName) {
+        case 'SingleSelectListItem':
+        case 'MultiSelectListItem':
+        case 'UserSelectionListItem':
+            return variables.optionRowHeightCompact;
+        case 'UserListItem':
+        case 'NewChatListItem':
+        case 'InviteMemberListItem':
+        default:
+            return variables.optionRowHeight;
+    }
+};
 function BaseSelectionListWithSections<TItem extends ListItem>({
     sections,
     ListItem,
@@ -55,7 +68,7 @@ function BaseSelectionListWithSections<TItem extends ListItem>({
     onCheckboxPress,
     onSelectAll,
     onDismissError,
-    getItemHeight = getDefaultItemHeight,
+    getItemHeight,
     textInputLabel = '',
     textInputPlaceholder = '',
     textInputValue = '',
@@ -256,8 +269,9 @@ function BaseSelectionListWithSections<TItem extends ListItem>({
                 }
                 disabledIndex += 1;
 
+                const defaultItemHeight = getItemHeight ? getItemHeight(item) : getDefaultItemHeight(ListItem);
                 // Account for the height of the item in getItemLayout
-                const fullItemHeight = item?.keyForList && itemHeights.current[item.keyForList] ? itemHeights.current[item.keyForList] : getItemHeight(item);
+                const fullItemHeight = item?.keyForList && itemHeights.current[item.keyForList] ? itemHeights.current[item.keyForList] : defaultItemHeight;
                 itemLayouts.push({length: fullItemHeight, offset});
                 offset += fullItemHeight;
 
