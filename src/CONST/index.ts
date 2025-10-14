f0fcc5fb0a2aa82fcdb8fd5ccd0d41e678e5fea5
NJ-2020 <ntt.london@gmail.com>
2025-10-14 10:40:41 +0700

    fix: update SEARCH_SELECTOR list

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -6763,8 +6763,8 @@ const CONST = {
         SEARCH_CONTEXT_GENERAL: 'general',
         SEARCH_CONTEXT_SEARCH: 'search',
         SEARCH_CONTEXT_MEMBER_INVITE: 'memberInvite',
-        SEARCH_CONTEXT_ATTENDEES: 'attendees',
         SEARCH_CONTEXT_SHARE_DESTINATION: 'shareDestination',
+        SEARCH_CONTEXT_ATTENDEES: 'attendees',
     },
     EXPENSE: {
         TYPE: {
