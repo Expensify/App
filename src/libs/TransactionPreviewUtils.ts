043d88424694a3baf7c3df182fb38e2ea9176333
alberto <alberto@expensify.com>
2025-10-15 12:42:20 +0200

    remove uneeded

--- src/libs/TransactionPreviewUtils.ts
+++ src/libs/TransactionPreviewUtils.ts
@@ -31,7 +31,6 @@ import {
     hasMissingSmartscanFields,
     hasNoticeTypeViolation,
     hasPendingRTERViolation,
-    hasReceipt,
     hasViolation,
     hasWarningTypeViolation,
     isCardTransaction,
