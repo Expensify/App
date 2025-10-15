4b6dc18c65eb8b3b721a63ffcee386c96dd144ba
fahimj <fahim.jatmiko@gmail.com>
2025-10-15 07:32:11 +0700

    Remove unused personal details entry from ReportUtilsTest

--- tests/unit/ReportUtilsTest.ts
+++ tests/unit/ReportUtilsTest.ts
@@ -269,10 +269,6 @@ const personalDetails: PersonalDetailsList = {
         accountID: 8,
         login: CONST.EMAIL.GUIDES_DOMAIN,
     },
-    '9': {
-        accountID: 9,
-        login: 'manager@test.com',
-    },
 };
 
 const rules = {
