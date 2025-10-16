d257e10a2641c7c3f7237c8f1ae53832490d090e
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2025-10-16 09:51:55 +0200

    add // eslint-disable-next-line @typescript-eslint/no-deprecated for expo-image-manipulator

--- src/libs/fileDownload/getImageManipulator/index.native.ts
+++ src/libs/fileDownload/getImageManipulator/index.native.ts
@@ -4,6 +4,7 @@ import type {FileObject} from '@src/types/utils/Attachment';
 import type ImageManipulatorConfig from './type';
 
 export default function getImageManipulator({fileUri, width, height, type, fileName}: ImageManipulatorConfig): Promise<FileObject> {
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
     return manipulateAsync(fileUri ?? '', [{resize: {width, height}}]).then((result) => ({
         uri: result.uri,
         width: result.width,
--- src/libs/fileDownload/getImageManipulator/index.ts
+++ src/libs/fileDownload/getImageManipulator/index.ts
@@ -3,6 +3,7 @@ import {manipulateAsync} from 'expo-image-manipulator';
 import type ImageManipulatorConfig from './type';
 
 export default function getImageManipulator({fileUri, width, height, fileName}: ImageManipulatorConfig): Promise<File> {
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
     return manipulateAsync(fileUri ?? '', [{resize: {width, height}}]).then((result) =>
         fetch(result.uri)
             .then((res) => res.blob())
