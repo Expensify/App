9a06668bb66d85b33b0bea004d796b13dc613495
Kamil Stróż <kstroz24@gmail.com>
2025-10-16 13:33:01 +0200

    fix: RenderTaskQueue test

--- tests/unit/RenderTaskQueueTest.ts
+++ tests/unit/RenderTaskQueueTest.ts
@@ -1,5 +1,7 @@
 import RenderTaskQueue from '../../src/components/InvertedFlatList/BaseInvertedFlatList/RenderTaskQueue';
 
+jest.unmock('../../src/components/InvertedFlatList/BaseInvertedFlatList/RenderTaskQueue');
+
 describe('RenderTaskQueue', () => {
     beforeEach(() => {
         jest.useFakeTimers();
@@ -11,23 +13,26 @@ describe('RenderTaskQueue', () => {
     });
 
     describe('notifyRenderingStateChange callback', () => {
-        it('should call callback with true when rendering starts', () => {
-            const mockCallback = jest.fn();
-            const queue = new RenderTaskQueue(mockCallback);
+        it('should call notifyRenderingStateChange callback 2 times durning render process', () => {
+            const mockStateChangeCallback = jest.fn();
+            const queue = new RenderTaskQueue(mockStateChangeCallback);
 
             queue.add({distanceFromStart: 100});
 
-            expect(mockCallback).toHaveBeenCalledWith(true);
-            expect(mockCallback).toHaveBeenCalledTimes(1);
+            expect(mockStateChangeCallback).toHaveBeenCalledWith(true);
+            jest.advanceTimersByTime(500);
+            expect(mockStateChangeCallback).toHaveBeenCalledTimes(2);
+            expect(mockStateChangeCallback).toHaveBeenCalledWith(false);
         });
 
-        it('should call callback with false when rendering finishes', () => {
+        it('should call notifyRenderingStateChange callback 2 times when canceling render', () => {
             const mockCallback = jest.fn();
             const queue = new RenderTaskQueue(mockCallback);
 
             queue.add({distanceFromStart: 100});
 
             expect(mockCallback).toHaveBeenCalledWith(true);
+            queue.cancel();
             jest.advanceTimersByTime(500);
             expect(mockCallback).toHaveBeenCalledTimes(2);
             expect(mockCallback).toHaveBeenCalledWith(false);
