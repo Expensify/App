9cc068328522f357f917b8bbf9a4f4c6d06661e1
FitseTLT <engfitsum@gmail.com>
2025-10-16 18:03:53 +0300

    removed other draft transaction before create test receipt transaction

--- src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
@@ -551,6 +551,7 @@ function IOURequestStepScan({
             }
 
             setMoneyRequestReceipt(initialTransactionID, source, filename, !isEditing, undefined, true);
+            removeDraftTransactions(true);
             navigateToConfirmationStep([{file, source: file.uri, transactionID: initialTransactionID}], false, true);
         });
     }, [initialTransactionID, isEditing, navigateToConfirmationStep]);
