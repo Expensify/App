f7216c39c4fb117c90a1f33625aa0bf42e38b140
Samran Ahmed <samranahmed03@gmail.com>
2025-10-17 17:10:47 +0500

    refactor: remove no-op functions from HTMLClickableActions

--- src/libs/HTMLClickableActionsUtils.ts
+++ src/libs/HTMLClickableActionsUtils.ts
@@ -2,12 +2,9 @@
  * A collection of HTML click actions that can be triggered from rendered <clickable> tags.
  *
  * Each key represents the id attribute of a <clickable> tag in the rendered HTML.
- * When the user taps that tag, the corresponding function is executed.
+ * When the user click that tag, the corresponding function is executed.
  */
 
-const HTMLClickableActions: Record<string, () => void | Promise<void>> = {
-    retry: () => {},
-    download: () => {},
-};
+const HTMLClickableActions: Record<string, () => void | Promise<void>> = {};
 
 export default HTMLClickableActions;
