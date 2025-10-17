6622395f33cf242007aa869af6062ccba5b4e7d4
Lucien Akchoté <lucien@expensify.com>
2025-10-17 11:19:41 +0200

    add reject translations for all languages

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -6168,6 +6168,7 @@ const translations = {
             delete: 'Delete',
             hold: 'Hold',
             unhold: 'Remove hold',
+            reject: 'Reject',
             noOptionsAvailable: 'No options available for the selected group of expenses.',
         },
         filtersHeader: 'Filters',
