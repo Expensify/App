74ff5252f4197802a598f36cf9b9c868bc099b63
Monil Bhavsar <monil@expensify.com>
2025-10-17 20:08:48 +0530

    Run prettier

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -1994,7 +1994,8 @@ const translations = {
         validateCardTitle: 'Asegurémonos de que eres tú',
         enterMagicCode: ({contactMethod}: EnterMagicCodeParams) =>
             `Introduzca el código mágico enviado a ${contactMethod} para ver los datos de su tarjeta. Debería llegar en un par de minutos.`,
-        missingPrivateDetails: ({profileSettingsLink}: {profileSettingsLink: string}) => `Agrega tus datos personales en <a href="${profileSettingsLink}">Cuenta > Perfil > Privado</a> y vuelve a intentarlo.`,
+        missingPrivateDetails: ({profileSettingsLink}: {profileSettingsLink: string}) =>
+            `Agrega tus datos personales en <a href="${profileSettingsLink}">Cuenta > Perfil > Privado</a> y vuelve a intentarlo.`,
         unexpectedError: 'Se produjo un error al intentar obtener los detalles de tu tarjeta Expensify. Vuelve a intentarlo.',
         cardFraudAlert: {
             confirmButtonText: 'Sí, lo hago',
