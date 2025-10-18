ceaa89ded9cff7939b73bdf1087bc48b666f9787
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-10-19 01:13:22 +0200

    fix eslint

--- src/components/MoneyReportHeaderStatusBar.tsx
+++ src/components/MoneyReportHeaderStatusBar.tsx
@@ -37,7 +37,7 @@ function MoneyReportHeaderStatusBar({nextStep}: MoneyReportHeaderStatusBarProps)
         <View style={[styles.dFlex, styles.flexRow, styles.alignItemsCenter, styles.overflowHidden, styles.w100, styles.headerStatusBarContainer]}>
             <View style={[styles.mr3]}>
                 <Icon
-                    src={iconMap[nextStep?.icon] || Expensicons.Hourglass}
+                    src={(nextStep?.icon && iconMap?.[nextStep.icon]) || Expensicons.Hourglass}
                     height={variables.iconSizeSmall}
                     width={variables.iconSizeSmall}
                     fill={theme.icon}
