065f81a02e12d190adc8efc3a22181a99c15efee
daledah <daleda261@outlook.com>
2025-10-20 15:54:42 +0700

    fix: show Mark As Resolved as primary action

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -1137,6 +1137,7 @@ const CONST = {
             REMOVE_HOLD: 'removeHold',
             REVIEW_DUPLICATES: 'reviewDuplicates',
             MARK_AS_CASH: 'markAsCash',
+            MARK_AS_RESOLVED: 'markAsResolved',
         },
         TRANSACTION_PRIMARY_ACTIONS: {
             REMOVE_HOLD: 'removeHold',
