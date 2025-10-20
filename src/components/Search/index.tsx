5a22c8580db44ca048ef29478eb417f79648c1f1
Michał Muzyk <mimuzyk@gmail.com>
2025-10-20 14:17:31 +0200

    fix: prettier

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -829,7 +829,17 @@ function Search({queryJSON, searchResults, onSearchListScroll, contentContainerS
             ),
             data,
         );
-    }, [validGroupBy, isExpenseReportType, selectedTransactions, setSelectedTransactions, data, clearSelectedTransactions, reportActionsArray, outstandingReportsByPolicyID, isUserWorkspaceMember]);
+    }, [
+        validGroupBy,
+        isExpenseReportType,
+        selectedTransactions,
+        setSelectedTransactions,
+        data,
+        clearSelectedTransactions,
+        reportActionsArray,
+        outstandingReportsByPolicyID,
+        isUserWorkspaceMember,
+    ]);
 
     const onLayout = useCallback(() => handleSelectionListScroll(sortedSelectedData, searchListRef.current), [handleSelectionListScroll, sortedSelectedData]);
 
