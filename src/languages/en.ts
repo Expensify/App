8465235c0468360ca0f702cce778fc33ebbb1768
eliran goshen <elirangoshen@gmail.com>
2025-10-20 16:45:22 +0200

    adding card text

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -4576,6 +4576,7 @@ const translations = {
                 monthly: 'Monthly',
             },
             cardDetails: 'Card details',
+            cardPending: ({name}: {name: string}) => `Card is currently pending and will be issued once ${name}'s account is validated.`,
             virtual: 'Virtual',
             physical: 'Physical',
             deactivate: 'Deactivate card',
