81e537105376f11972a58a711ebc625511b99742
DylanDylann <dylandylan9112@gmail.com>
2025-10-21 10:59:25 +0700

    fix deps

--- src/hooks/useTransactionsAndViolationsForReport.ts
+++ src/hooks/useTransactionsAndViolationsForReport.ts
@@ -29,7 +29,7 @@ function useTransactionsAndViolationsForReport(reportID?: string) {
         );
 
         return {transactions, violations: filteredViolations};
-    }, [transactions, violations, currentUserDetails.email]);
+    }, [transactions, violations, currentUserDetails?.email]);
 
     return transactionsAndViolations;
 }
