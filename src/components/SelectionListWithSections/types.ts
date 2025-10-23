733b1f8ad77f92f49d654e419ad9ccb0fb6523ee
Zuzanna Furtak <zuzkafurtak@gmail.com>
2025-10-23 18:39:16 +0200

    Remove TravelDomainListItem from SelectionListWithSections

--- src/components/SelectionListWithSections/types.ts
+++ src/components/SelectionListWithSections/types.ts
@@ -49,7 +49,6 @@ import type SearchQueryListItem from './Search/SearchQueryListItem';
 import type TransactionGroupListItem from './Search/TransactionGroupListItem';
 import type TransactionListItem from './Search/TransactionListItem';
 import type TableListItem from './TableListItem';
-import type TravelDomainListItem from './TravelDomainListItem';
 import type UserListItem from './UserListItem';
 
 type TRightHandSideComponent<TItem extends ListItem> = {
@@ -579,7 +578,6 @@ type ValidListItem =
     | typeof ChatListItem
     | typeof SearchQueryListItem
     | typeof SearchRouterItem
-    | typeof TravelDomainListItem
     | typeof UnreportedExpenseListItem;
 
 type Section<TItem extends ListItem> = {
