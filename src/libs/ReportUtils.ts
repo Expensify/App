17fff44b4e45e0632da98cd4dbf4427dfe2294cb
Lucien Akchoté <lucien@expensify.com>
2025-10-23 07:47:28 +0200

    Merge pull request #73041 from Expensify/chuckdries/buildOptimisticIOUReportAction-improperly-reuses-reportActionID
    [CP Staging/Production] Fix buildOptimisticIOUReportAction returning an existing reportActionID when it should generate a new one
    
    (cherry picked from commit 173dee902f1283344d913c0c857a3a4ef4bc11ac)
    
    (cherry-picked to staging by mountiny)

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -6809,7 +6809,7 @@ function buildOptimisticIOUReportAction(params: BuildOptimisticIOUReportActionPa
         automatic: false,
         isAttachmentOnly: false,
         originalMessage,
-        reportActionID: linkedExpenseReportAction?.reportActionID ?? reportActionID ?? rand64(),
+        reportActionID: reportActionID ?? rand64(),
         shouldShow: true,
         created,
         pendingAction: CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD,
