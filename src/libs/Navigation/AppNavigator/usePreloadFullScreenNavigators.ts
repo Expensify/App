5c71d1cceb3c95124ccb53f7f53dd99a5af498ae
sumo_slonik <jakub.nowakowski@swmansion.com>
2025-10-24 12:18:39 +0200

    fix comment

--- src/libs/Navigation/AppNavigator/usePreloadFullScreenNavigators.ts
+++ src/libs/Navigation/AppNavigator/usePreloadFullScreenNavigators.ts
@@ -24,7 +24,7 @@ import {getPreservedNavigatorState} from './createSplitNavigator/usePreserveNavi
 // This timing is used to call the preload function after a tab change, when the initial tab screen has already been rendered.
 const TIMING_TO_CALL_PRELOAD = 1000;
 
-// Currently, only the Account and Workspaces tabs are preloaded. The remaining tabs will be supported soon.
+// Currently, only the Account, Inbox and Workspaces tabs are preloaded. The remaining tabs will be supported soon.
 const TABS_TO_PRELOAD = [NAVIGATION_TABS.SETTINGS, NAVIGATION_TABS.WORKSPACES, NAVIGATION_TABS.HOME];
 
 function preloadWorkspacesTab(navigation: PlatformStackNavigationProp<AuthScreensParamList>) {
