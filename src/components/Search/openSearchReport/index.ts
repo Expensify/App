dc0bad4f261892ced23aa37c3350fc58f65af3ae
Wojciech Boman <wojciechboman@gmail.com>
2025-10-27 15:41:46 +0100

    Add comment to openSearchReport

--- src/components/Search/openSearchReport/index.ts
+++ src/components/Search/openSearchReport/index.ts
@@ -4,6 +4,7 @@ import NAVIGATORS from '@src/NAVIGATORS';
 import ROUTES from '@src/ROUTES';
 import SCREENS from '@src/SCREENS';
 
+// The search report screen is preloaded before navigating to avoid lag when opening the page. If the content is rendered in the background and then navigated, the opening experience is smoother.
 export default function openSearchReport(reportID: string, backTo: string) {
     navigationRef.dispatch({
         ...CommonActions.preload(NAVIGATORS.RIGHT_MODAL_NAVIGATOR, {
