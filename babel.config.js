49f4fe9a936b65736bcbd3328494ad0baf3bd92f
Christoph Pader <chris@margelo.com>
2025-10-27 20:43:29 +0000

    refactor: add back `target` property in React Compiler babel config

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const IS_E2E_TESTING = process.env.E2E_TESTING === 'true';
 
 const ReactCompilerConfig = {
+    target: '19',
     environment: {
         enableTreatRefLikeIdentifiersAsRefs: true,
     },
