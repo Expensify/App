97637ec89233ad66c80103e597bf2382e5c81760
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-10-27 14:00:08 +0100

    change icon to camera on mobile web

--- src/components/FloatingCameraButton/index.tsx
+++ src/components/FloatingCameraButton/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import {ReceiptPlus} from '@components/Icon/Expensicons';
+import {Camera, ReceiptPlus} from '@components/Icon/Expensicons';
+import getPlatform from '@libs/getPlatform';
+import CONST from '@src/CONST';
 import BaseFloatingCameraButton from './BaseFloatingCameraButton';
 
 function FloatingCameraButton() {
-    return <BaseFloatingCameraButton icon={ReceiptPlus} />;
+    const platform = getPlatform(true);
+    const icon = platform === CONST.PLATFORM.MOBILE_WEB ? Camera : ReceiptPlus;
+
+    return <BaseFloatingCameraButton icon={icon} />;
 }
 
 FloatingCameraButton.displayName = 'FloatingCameraButton';
