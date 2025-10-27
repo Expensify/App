fb0d4acceb53d4a746ee652ad8fa5729011c9dcc
Cristi Paval <onisio.software@gmail.com>
2025-10-27 13:26:37 +0200

    Revert "fix: Delete option is missing in More menu of an unpaid invoice report"

--- tests/unit/ReportUtilsTest.ts
+++ tests/unit/ReportUtilsTest.ts
@@ -26,7 +26,6 @@ import {
     buildTransactionThread,
     canAddTransaction,
     canCreateRequest,
-    canDeleteMoneyRequestReport,
     canDeleteReportAction,
     canDeleteTransaction,
     canEditMoneyRequest,
@@ -3050,13 +3049,6 @@ describe('ReportUtils', () => {
             ];
             const latestReport: OnyxEntry<Report> = {reportID: '1', lastReadTime: '2023-07-08 07:15:44.030'};
             expect(getMostRecentlyVisitedReport(reports, undefined)).toEqual(latestReport);
-        });
-    });
-
-    describe('canDeleteMoneyRequestReport', () => {
-        it('should allow deletion if the report is open invoice report', () => {
-            const invoiceReport = {...createInvoiceReport(343), ownerAccountID: currentUserAccountID, stateNum: CONST.REPORT.STATE_NUM.OPEN, statusNum: CONST.REPORT.STATUS_NUM.OPEN};
-            expect(canDeleteMoneyRequestReport(invoiceReport, [], [])).toBe(true);
         });
     });
 
