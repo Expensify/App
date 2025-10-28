9f31629df33f015153beab00437b9471b217d057
Christoph Pader <chris@margelo.com>
2025-10-28 17:57:38 +0000

    chore: `npm run gh-actions-build`

--- .github/actions/javascript/getPullRequestIncrementalChanges/index.js
+++ .github/actions/javascript/getPullRequestIncrementalChanges/index.js
@@ -12796,7 +12796,7 @@ class Git {
         }
     }
     static async getMainBranchCommitHash(remote) {
-        const baseRefName = GITHUB_BASE_REF ?? CONST_1.default.DEFAULT_BASE_REF;
+        const baseRefName = GITHUB_BASE_REF ?? 'main';
         // Fetch the main branch from the specified remote (or locally) to ensure it's available
         if (IS_CI || remote) {
             await exec(`git fetch ${remote ?? 'origin'} ${baseRefName} --no-tags --depth=1`);
