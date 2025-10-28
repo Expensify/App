686411344175001e86907e49a4824e07b0204d45
rory <rory@expensify.com>
2025-10-27 22:18:58 -0700

    Manually add dedent import to target files

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -1,5 +1,6 @@
 import {CONST as COMMON_CONST} from 'expensify-common';
 import type {OnboardingTask} from '@libs/actions/Welcome/OnboardingFlow';
+import dedent from '@libs/StringUtils/dedent';
 import CONST from '@src/CONST';
 import type OriginalMessage from '@src/types/onyx/OriginalMessage';
 import type en from './en';
