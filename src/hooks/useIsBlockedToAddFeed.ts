8d57ece7eae0fb9732cccfabf57a7e1ef9e3d64e
Nicolay Arefyeu <n.arefyev91@gmail.com>
2025-10-31 15:40:36 +0200

    migrate to useState and add tests

--- src/hooks/useIsBlockedToAddFeed.ts
+++ src/hooks/useIsBlockedToAddFeed.ts
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react';
 import {checkIfNewFeedConnected, getCompanyFeeds, getSelectedFeed} from '@libs/CardUtils';
 import {isCollectPolicy} from '@libs/PolicyUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -7,11 +8,9 @@ import useCardFeeds from './useCardFeeds';
 import useCardsList from './useCardsList';
 import useOnyx from './useOnyx';
 import usePolicy from './usePolicy';
-import usePrevious from './usePrevious';
 
 function useIsBlockedToAddFeed(policyID?: string) {
     const policy = usePolicy(policyID);
-
     const [cardFeeds, allFeedsResult, defaultFeed] = useCardFeeds(policyID);
     const [addNewCard] = useOnyx(ONYXKEYS.ADD_NEW_COMPANY_CARD, {canBeMissing: true});
     const [lastSelectedFeed] = useOnyx(`${ONYXKEYS.COLLECTION.LAST_SELECTED_FEED}${policyID}`, {canBeMissing: true});
@@ -20,10 +19,19 @@ function useIsBlockedToAddFeed(policyID?: string) {
     const isAllFeedsResultLoading = isLoadingOnyxValue(allFeedsResult);
     const selectedFeed = getSelectedFeed(lastSelectedFeed, cardFeeds);
     const [cardsList] = useCardsList(policyID, selectedFeed);
-    const prevFeedsData = usePrevious(cardFeeds?.settings?.oAuthAccountDetails);
-    const {isNewFeedConnected} = checkIfNewFeedConnected(prevFeedsData ?? {}, cardFeeds?.settings?.oAuthAccountDetails ?? {}, addNewCard?.data?.plaidConnectedFeed);
+    const [prevOAuthDetails, setPrevOAuthDetails] = useState(cardFeeds?.settings?.oAuthAccountDetails);
+    const [isNewFeedConnected, setIsNewFeedConnected] = useState(false);
 
     const isLoading = !cardFeeds || (!!cardFeeds.isLoading && isEmptyObject(cardsList)) || !!defaultFeed?.isLoading;
+
+    useEffect(() => {
+        const currentOAuthDetails = cardFeeds?.settings?.oAuthAccountDetails ?? {};
+        const plaidConnectedFeed = addNewCard?.data?.plaidConnectedFeed;
+
+        const {isNewFeedConnected: newFeedConnected} = checkIfNewFeedConnected(prevOAuthDetails ?? {}, currentOAuthDetails, plaidConnectedFeed);
+        setIsNewFeedConnected(!!newFeedConnected);
+        setPrevOAuthDetails(currentOAuthDetails);
+    }, [cardFeeds?.settings?.oAuthAccountDetails, addNewCard?.data?.plaidConnectedFeed, prevOAuthDetails]);
 
     return {
         isBlockedToAddNewFeeds: isCollect && Object.entries(companyFeeds)?.length >= 1 && !isNewFeedConnected,
