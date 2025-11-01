01834f6e5fe0faf2acbabd4a91e54427595cf7b9
Hubert Sosinski <hubert.sosinski@callstack.com>
2025-11-01 10:26:09 +0100

    Fix Fullstory connection causing crash on Dev

--- src/libs/TelemetrySynchronizer.ts
+++ src/libs/TelemetrySynchronizer.ts
@@ -3,17 +3,5 @@
  *
  * It uses Onyx.connectWithoutView as nothing here is related to the UI. We only send data to external provider and want to keep this outside of the render loop.
  */
-import * as Sentry from '@sentry/react-native';
-import CONST from '@src/CONST';
-import FS from './Fullstory';
 
-/**
- * Connect to FullStory to retrieve session id from it. We want to link FullStory with Sentry for easier debugging.
- */
-FS.onReady().then(async () => {
-    const sessionId = await FS.getSessionId();
-    if (!sessionId) {
-        return;
-    }
-    Sentry.setContext(CONST.TELEMETRY.CONTEXT_FULLSTORY, {sessionId});
-});
+// We moved Fullstory connection to Expensify.tsx but we want to keep the file as it will be needed in https://github.com/Expensify/App/pull/73848 which will be merged asap
