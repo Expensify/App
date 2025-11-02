4ef57cd43f428c66b1e3be7582155f92c5dc8f5d
Manan Jadhav <manan.jadhav@gmail.com>
2025-11-03 01:00:48 +0530

    fix: update hold modal content

--- src/components/ProcessMoneyRequestHoldMenu.tsx
+++ src/components/ProcessMoneyRequestHoldMenu.tsx
@@ -1,16 +1,12 @@
-import React, {useMemo} from 'react';
-import {View} from 'react-native';
+import React from 'react';
 import useBeforeRemove from '@hooks/useBeforeRemove';
 import useLocalize from '@hooks/useLocalize';
-import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import variables from '@styles/variables';
 import CONST from '@src/CONST';
 import FeatureTrainingModal from './FeatureTrainingModal';
 import HoldMenuSectionList from './HoldMenuSectionList';
 import * as Illustrations from './Icon/Illustrations';
-import Text from './Text';
-import TextPill from './TextPill';
 
 type ProcessMoneyRequestHoldMenuProps = {
     /** Method to trigger when pressing outside of the popover menu to close it */
@@ -23,23 +19,12 @@ type ProcessMoneyRequestHoldMenuProps = {
 function ProcessMoneyRequestHoldMenu({onClose, onConfirm}: ProcessMoneyRequestHoldMenuProps) {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
-    const {onboardingIsMediumOrLargerScreenWidth} = useResponsiveLayout();
 
     useBeforeRemove(onClose);
 
-    const title = useMemo(
-        () => (
-            <View style={[styles.flexRow, styles.alignItemsCenter, onboardingIsMediumOrLargerScreenWidth ? styles.mb1 : styles.mb2]}>
-                <Text style={[styles.textHeadline, styles.mr2]}>{translate('iou.holdEducationalTitle')}</Text>
-                <TextPill textStyles={styles.holdRequestInline}>{translate('iou.holdEducationalText')}</TextPill>
-            </View>
-        ),
-        [onboardingIsMediumOrLargerScreenWidth, styles.flexRow, styles.alignItemsCenter, styles.mb1, styles.mb2, styles.textHeadline, styles.mr2, styles.holdRequestInline, translate],
-    );
-
     return (
         <FeatureTrainingModal
-            title={title}
+            title={translate('iou.holdEducationalTitle')}
             description={translate('iou.whatIsHoldExplain')}
             confirmText={translate('common.buttonConfirm')}
             image={Illustrations.HoldExpense}
