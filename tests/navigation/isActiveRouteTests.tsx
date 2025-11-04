8067d7108db6851b2550c46c3662d64349a11dcc
Monil Bhavsar <monil@expensify.com>
2025-11-04 13:23:11 +0530

    Fix lint

--- tests/navigation/isActiveRouteTests.tsx
+++ tests/navigation/isActiveRouteTests.tsx
@@ -1,4 +1,4 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect} from '@jest/globals';
 import {render} from '@testing-library/react-native';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import getIsNarrowLayout from '@libs/getIsNarrowLayout';
