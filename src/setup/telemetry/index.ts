2609591e9f388a9270af51709cb940539b955437
Rodrigo Lino da Costa <rodrigo@expensify.com>
2025-11-05 12:22:22 -0300

    dont send applause events to sentry

--- src/setup/telemetry/index.ts
+++ src/setup/telemetry/index.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/react-native';
 import {isDevelopment} from '@libs/Environment/Environment';
 import CONFIG from '@src/CONFIG';
+import CONST from '@src/CONST';
 import pkg from '../../../package.json';
 
 const navigationIntegration = Sentry.reactNavigationIntegration({
@@ -20,6 +21,14 @@ export default function (): void {
         integrations: [navigationIntegration],
         environment: CONFIG.ENVIRONMENT,
         release: `${pkg.name}@${pkg.version}`,
+        beforeSend: (event) => {
+            const email = event.user?.email;
+            const lowerEmail = typeof email === 'string' ? email.toLowerCase() : '';
+            if (lowerEmail.endsWith(CONST.EMAIL.QA_DOMAIN) || lowerEmail.endsWith('applauseauto.com')) {
+                return null;
+            }
+            return event;
+        },
     });
 }
 
