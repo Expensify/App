89d4b6bd4207625d77137b1ed4a8f24cca11b5c3
Mukhriddin Shakhriyorov <mshakhriyorov8@gmail.com>
2025-11-05 22:42:46 +0500

    prettier fix

--- tests/actions/IOUTest.ts
+++ tests/actions/IOUTest.ts
@@ -6489,7 +6489,7 @@ describe('actions/IOU', () => {
                 statusNum: CONST.REPORT.STATUS_NUM.SUBMITTED,
                 ownerAccountID: CARLOS_ACCOUNT_ID,
                 managerID: RORY_ACCOUNT_ID,
-                isWaitingOnBankAccount: false, 
+                isWaitingOnBankAccount: false,
                 total: 100, // positive amount in the DB means negative amount in the UI
             };
 
