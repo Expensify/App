9aa9d0ab974c876ee3a3578fcba2c474152a1b13
Youssef Lourayad <youssef@expensify.com>
2025-11-05 20:29:43 +0100

    Lint

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -2023,6 +2023,7 @@ type MergeTransactionNavigatorParamList = {
         transactionID: string;
         // eslint-disable-next-line no-restricted-syntax -- `backTo` usages in this file are legacy. Do not add new `backTo` params to screens. See contributingGuides/NAVIGATION.md
         backTo?: Routes;
+        hash?: number;
     };
     [SCREENS.MERGE_TRANSACTION.RECEIPT_PAGE]: {
         transactionID: string;
