64e873ca9c255cc6f3e951be36b7cef0ee6173eb
Rodrigo Lino da Costa <rodrigo@expensify.com>
2025-11-06 09:25:17 -0300

    allow specific email events

--- src/libs/Telemetry/middlewares/emailDomainFilter.ts
+++ src/libs/Telemetry/middlewares/emailDomainFilter.ts
@@ -5,8 +5,8 @@ import type {TelemetryBeforeSend} from './index';
 const emailDomainFilter: TelemetryBeforeSend = (event: TransactionEvent): TransactionEvent | null => {
     const email = event.user?.email;
     const lowerEmail = typeof email === 'string' ? email.toLowerCase() : '';
-
-    if (lowerEmail.endsWith(CONST.EMAIL.QA_DOMAIN) || lowerEmail.endsWith('applauseauto.com')) {
+ 
+    if (lowerEmail !== 'applausetester@applause.expensifail.com' && (lowerEmail.endsWith(CONST.EMAIL.QA_DOMAIN) || lowerEmail.endsWith('applauseauto.com'))) {
         return null;
     }
 
