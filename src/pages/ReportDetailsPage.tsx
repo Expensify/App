3905bf74ba48c362ed3c1ac20e2f4f442ec9403d
Trace Harris <traceh38@gmail.com>
2025-11-05 18:59:09 -0800

    Update src/pages/ReportDetailsPage.tsx
    refactoring based upon correct pattern usage
    
    Co-authored-by: Qichen Zhu <zhuqc9@gmail.com>

--- src/pages/ReportDetailsPage.tsx
+++ src/pages/ReportDetailsPage.tsx
@@ -331,7 +331,7 @@ function ReportDetailsPage({policy, report, route, reportMetadata}: ReportDetail
 
     const shouldShowLeaveButton = canLeaveChat(report, policy, !!reportNameValuePairs?.private_isArchived);
     const shouldShowGoToWorkspace = shouldShowPolicy(policy, false, currentUserPersonalDetails?.email) && !policy?.isJoinRequestPending;
-    const reportName = getReportName(report, undefined, undefined, undefined, reportAttributes);
+    const reportName = getReportName(report, undefined, undefined, undefined, undefined, reportAttributes);
 
     const additionalRoomDetails =
         (isPolicyExpenseChat && !!report?.isOwnPolicyExpenseChat) || isExpenseReportUtil(report) || isPolicyExpenseChat || isInvoiceRoom
