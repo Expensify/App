e3111699fe40a7e6de2bcf6cdf6600f8fb5804aa
Christoph Pader <chris@margelo.com>
2025-11-06 17:31:45 +0000

    fix: report loading indicator on every open

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -897,15 +897,16 @@ function ReportScreen({route, navigation}: ReportScreenProps) {
 
     useShowWideRHPVersion(shouldShowWideRHP);
 
+    const isReportUnread = isUnread(report, transactionThreadReport, isReportArchived);
+
     // When opening an unread report, it is very likely that the message we will open to is not the latest,
     // which is the only one we will have in cache.
-    const isInitiallyLoadingReport =
-        isUnread(report, transactionThreadReport, isReportArchived) && !!reportMetadata.isLoadingInitialReportActions && (isOffline || reportActions.length <= 1);
+    const isInitiallyLoadingReport = isReportUnread && !!reportMetadata.isLoadingInitialReportActions && (isOffline || reportActions.length <= 1);
 
     // When we open a report, we have to wait for the oldest unread report action ID to be set and
     // retrieved from Onyx, in order to get the correct initial report action page from store,
     // except for when the user is offline.
-    const isLoadingOldestUnreadReportActionWhileOnline = !isOffline && isLoadingOldestUnreadReportActionID;
+    const isLoadingOldestUnreadReportActionWhileOnline = !isOffline && isReportUnread && isLoadingOldestUnreadReportActionID;
 
     // Once all the above conditions are met, we can consider the report ready.
     const isReportReady = !isInitiallyLoadingReport && !isLoadingOldestUnreadReportActionWhileOnline;
