2e56857afb51bfc979880f495c6ab080f94de775
huutech <20178761+huult@users.noreply.github.com>
2025-11-08 09:24:03 +0700

    Fix issue where the login page does not scroll to the top when focused

--- src/libs/AppStateMonitor/index.ts
+++ src/libs/AppStateMonitor/index.ts
@@ -3,13 +3,13 @@ import {AppState} from 'react-native';
 import CONST from '@src/CONST';
 import shouldReportActivity from './shouldReportActivity';
 
-let appState: AppStateStatus = CONST.APP_STATE.ACTIVE;
-
 /**
  * Listener that will only fire the callback when the user has become active.
  * @returns callback to unsubscribe
  */
 function addBecameActiveListener(callback: () => void): () => void {
+    let appState: AppStateStatus = CONST.APP_STATE.ACTIVE;
+
     function appStateChangeCallback(state: AppStateStatus) {
         if (shouldReportActivity && (appState === CONST.APP_STATE.INACTIVE || appState === CONST.APP_STATE.BACKGROUND) && state === CONST.APP_STATE.ACTIVE) {
             callback();
