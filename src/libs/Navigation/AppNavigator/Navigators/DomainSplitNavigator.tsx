316fdbd906d1c59c57f4b2d1cd70174558a8b57c
mhawryluk <marcinhawrylukxx@gmail.com>
2025-11-10 18:16:14 +0100

    Add handling workspaceOrDomainSplitsWithoutEnteringAnimation in DomainSplitNavigator

--- src/libs/Navigation/AppNavigator/Navigators/DomainSplitNavigator.tsx
+++ src/libs/Navigation/AppNavigator/Navigators/DomainSplitNavigator.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import FocusTrapForScreens from '@components/FocusTrap/FocusTrapForScreen';
+import {workspaceOrDomainSplitsWithoutEnteringAnimation} from '@libs/Navigation/AppNavigator/createRootStackNavigator/GetStateForActionHandlers';
 import createSplitNavigator from '@libs/Navigation/AppNavigator/createSplitNavigator';
 import useSplitNavigatorScreenOptions from '@libs/Navigation/AppNavigator/useSplitNavigatorScreenOptions';
+import Animations from '@libs/Navigation/PlatformStackNavigation/navigationOptions/animation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {AuthScreensParamList, DomainSplitNavigatorParamList} from '@libs/Navigation/types';
 import type NAVIGATORS from '@src/NAVIGATORS';
@@ -14,8 +16,25 @@ const loadDomainSamlPage = () => require<ReactComponentModule>('../../../../page
 
 const Split = createSplitNavigator<DomainSplitNavigatorParamList>();
 
-function DomainSplitNavigator({route}: PlatformStackScreenProps<AuthScreensParamList, typeof NAVIGATORS.DOMAIN_SPLIT_NAVIGATOR>) {
+function DomainSplitNavigator({route, navigation}: PlatformStackScreenProps<AuthScreensParamList, typeof NAVIGATORS.DOMAIN_SPLIT_NAVIGATOR>) {
     const splitNavigatorScreenOptions = useSplitNavigatorScreenOptions();
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('transitionEnd', () => {
+            // We want to call this function only once.
+            unsubscribe();
+
+            // If we open this screen from a different tab, then it won't have animation.
+            if (!workspaceOrDomainSplitsWithoutEnteringAnimation.has(route.key)) {
+                return;
+            }
+
+            // We want to set animation after mounting so it will animate on going UP to the settings split.
+            navigation.setOptions({animation: Animations.SLIDE_FROM_RIGHT});
+        });
+
+        return unsubscribe;
+    }, [navigation, route.key]);
 
     return (
         <FocusTrapForScreens>
