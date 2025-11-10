f718a37c9dc36385e3db8be0a885609f234ded67
daledah <daleda261@outlook.com>
2025-11-10 23:58:30 +0700

    fix app crashes when update member role

--- src/libs/actions/Policy/Member.ts
+++ src/libs/actions/Policy/Member.ts
@@ -340,7 +340,7 @@ function resetAccountingPreferredExporter(policyID: string, loginList: string[])
     }
 
     connections.forEach((connection) => {
-        const exporter = policy?.connections?.[connection]?.config.export.exporter;
+        const exporter = policy?.connections?.[connection]?.config?.export?.exporter;
         if (!exporter || !adminLoginList.includes(exporter)) {
             return;
         }
@@ -374,7 +374,7 @@ function resetAccountingPreferredExporter(policyID: string, loginList: string[])
                 connections: {
                     [connection]: {
                         config: {
-                            export: {exporter: policy?.connections?.[connection]?.config.export.exporter},
+                            export: {exporter},
                             pendingFields: {[pendingFieldKey]: null},
                         },
                     },
@@ -383,7 +383,7 @@ function resetAccountingPreferredExporter(policyID: string, loginList: string[])
         });
     });
 
-    const exporter = policy?.connections?.netsuite?.options.config.exporter;
+    const exporter = policy?.connections?.netsuite?.options?.config?.exporter;
     if (exporter && adminLoginList.includes(exporter)) {
         optimisticData.push({
             onyxMethod: Onyx.METHOD.MERGE,
@@ -402,7 +402,7 @@ function resetAccountingPreferredExporter(policyID: string, loginList: string[])
         failureData.push({
             onyxMethod: Onyx.METHOD.MERGE,
             key: policyKey,
-            value: {connections: {netsuite: {options: {config: {exporter: policy?.connections?.netsuite?.options.config.exporter, pendingFields: {exporter: null}}}}}},
+            value: {connections: {netsuite: {options: {config: {exporter, pendingFields: {exporter: null}}}}}},
         });
     }
 
