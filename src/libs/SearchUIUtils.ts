2e14e32ebbec5630f2682e9762cf2d36a110b0aa
Mohammad Jafarinejad <71210799+mohammadjafarinejad@users.noreply.github.com>
2025-11-10 14:31:01 -0800

    Fix transaction fromDetails lookup
    Co-authored-by: Carlos Martins <luacmartins@gmail.com>

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -1470,7 +1470,7 @@ function getReportSections(
                 const shouldShowBlankTo = !reportItem || isOpenExpenseReport(reportItem);
                 const allActions = getActions(data, allViolations, key, currentSearch, currentAccountID, currentUserEmail, actions);
 
-                const fromDetails = transactions.length > 0 ? data.personalDetailsList[data?.[reportKey as ReportKey]?.ownerAccountID ?? CONST.DEFAULT_NUMBER_ID] : emptyPersonalDetails;
+                const fromDetails = data.personalDetailsList[reportItem.ownerAccountID ?? CONST.DEFAULT_NUMBER_ID];
 
                 const toDetails = !shouldShowBlankTo && reportItem.managerID ? data.personalDetailsList?.[reportItem.managerID] : emptyPersonalDetails;
                 const formattedFrom = fromDetails === emptyPersonalDetails ? '' : formatPhoneNumber(getDisplayNameOrDefault(fromDetails));
