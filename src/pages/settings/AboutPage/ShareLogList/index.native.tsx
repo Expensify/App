5698023ba0fe2a92b5e3968287fa33812ebb59e4
Antony M. Kithinzi <kithinzi97@gmail.com>
2025-11-11 23:51:14 +0000

    refactor: enhance attachment logic by incorporating ancestors in addAttachmentWithComment

--- src/pages/settings/AboutPage/ShareLogList/index.native.tsx
+++ src/pages/settings/AboutPage/ShareLogList/index.native.tsx
@@ -5,15 +5,24 @@ import {addAttachmentWithComment} from '@userActions/Report';
 import ROUTES from '@src/ROUTES';
 import BaseShareLogList from './BaseShareLogList';
 import type {ShareLogListProps} from './types';
+import useAncestors from '@hooks/useAncestors';
+import ONYXKEYS from '@src/ONYXKEYS';
+import useOnyx from '@hooks/useOnyx';
+import { getAncestors } from '@libs/ReportUtils';
 
 function ShareLogList({logSource}: ShareLogListProps) {
     const personalDetail = useCurrentUserPersonalDetails();
+    const [reportCollection] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {canBeMissing: false});
+    const [reportDraftCollection] = useOnyx(ONYXKEYS.COLLECTION.REPORT_DRAFT, {canBeMissing: true});
+    const [reportActionsCollection] = useOnyx(ONYXKEYS.COLLECTION.REPORT_ACTIONS, {canBeMissing: false});
     const onAttachLogToReport = (reportID: string, filename: string) => {
         if (!reportID || !logSource) {
             return;
         }
+        const report = reportCollection?.[`${ONYXKEYS.COLLECTION.REPORT}${reportID}`];
+        const ancestors = getAncestors(report, reportCollection, reportDraftCollection, reportActionsCollection);
         const src = `file://${logSource}`;
-        addAttachmentWithComment(reportID, reportID, {name: filename, source: src, uri: src, type: 'text/plain'} as File, undefined, personalDetail.timezone);
+        addAttachmentWithComment(reportID, reportID, ancestors, {name: filename, source: src, uri: src, type: 'text/plain'} as File, undefined, personalDetail.timezone);
 
         const routeToNavigate = ROUTES.REPORT_WITH_ID.getRoute(reportID);
         Navigation.navigate(routeToNavigate);
