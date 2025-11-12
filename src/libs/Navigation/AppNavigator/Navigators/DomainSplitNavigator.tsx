9debb8390bfe93fa60a87b2e950201e9ef50435e
mhawryluk <marcinhawrylukxx@gmail.com>
2025-11-12 10:36:42 +0100

    Extract common split navigator logic to a new hook

--- src/libs/Navigation/AppNavigator/Navigators/DomainSplitNavigator.tsx
+++ src/libs/Navigation/AppNavigator/Navigators/DomainSplitNavigator.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import FocusTrapForScreens from '@components/FocusTrap/FocusTrapForScreen';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {workspaceOrDomainSplitsWithoutEnteringAnimation} from '@libs/Navigation/AppNavigator/createRootStackNavigator/GetStateForActionHandlers';
 import createSplitNavigator from '@libs/Navigation/AppNavigator/createSplitNavigator';
 import useSplitNavigatorScreenOptions from '@libs/Navigation/AppNavigator/useSplitNavigatorScreenOptions';
-import Animations from '@libs/Navigation/PlatformStackNavigation/navigationOptions/animation';
+import useNoAnimationWhenOpenedFromTabBar from '@libs/Navigation/helpers/useNoAnimationWhenOpenedFromTabBar';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {AuthScreensParamList, DomainSplitNavigatorParamList} from '@libs/Navigation/types';
 import type NAVIGATORS from '@src/NAVIGATORS';
@@ -21,22 +20,7 @@ function DomainSplitNavigator({route, navigation}: PlatformStackScreenProps<Auth
     const splitNavigatorScreenOptions = useSplitNavigatorScreenOptions();
     const styles = useThemeStyles();
 
-    useEffect(() => {
-        const unsubscribe = navigation.addListener('transitionEnd', () => {
-            // We want to call this function only once.
-            unsubscribe();
-
-            // If we open this screen from a different tab, then it won't have animation.
-            if (!workspaceOrDomainSplitsWithoutEnteringAnimation.has(route.key)) {
-                return;
-            }
-
-            // We want to set animation after mounting so it will animate on going UP to the settings split.
-            navigation.setOptions({animation: Animations.SLIDE_FROM_RIGHT});
-        });
-
-        return unsubscribe;
-    }, [navigation, route.key]);
+    useNoAnimationWhenOpenedFromTabBar(navigation, route.key);
 
     return (
         <FocusTrapForScreens>
