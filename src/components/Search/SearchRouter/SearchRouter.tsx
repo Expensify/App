a4491358251af572de925d8610ebbdddf15757b5
Dysto coder <dystovskycoder@gmail.com>
2025-11-12 16:29:01 +0300

    pass showPersonalDetails as true from other components that use createOptionFromReport

--- src/components/Search/SearchRouter/SearchRouter.tsx
+++ src/components/Search/SearchRouter/SearchRouter.tsx
@@ -153,7 +153,7 @@ function SearchRouter({onRouterClose, shouldHideInputCaret, isSearchRouterDispla
                     return undefined;
                 }
 
-                const option = createOptionFromReport(report, personalDetails);
+                const option = createOptionFromReport(report, personalDetails, undefined, {showPersonalDetails: true});
                 reportForContextualSearch = option;
             }
 
