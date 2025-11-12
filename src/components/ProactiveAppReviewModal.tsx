7d7ba48176ec6204dc3c82ab968269fc81f66728
Marc Glasser <marc.aaron.glasser@gmail.com>
2025-11-11 14:18:50 -1000

    Improve design

--- src/components/ProactiveAppReviewModal.tsx
+++ src/components/ProactiveAppReviewModal.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import {View} from 'react-native';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
-import DecisionModal from './DecisionModal';
+import useThemeStyles from '@hooks/useThemeStyles';
+import CONST from '@src/CONST';
+import Button from './Button';
+import ImageSVG from './ImageSVG';
+import Modal from './Modal';
+import Text from './Text';
+import * as Illustrations from './Icon/Illustrations';
 
 type ProactiveAppReviewModalProps = {
     /** Whether modal is visible */
@@ -18,19 +25,53 @@ type ProactiveAppReviewModalProps = {
 
 function ProactiveAppReviewModal({isVisible, onPositive, onNegative, onSkip}: ProactiveAppReviewModalProps) {
     const {shouldUseNarrowLayout} = useResponsiveLayout();
+    const styles = useThemeStyles();
 
     return (
-        <DecisionModal
-            title="Enjoying New Expensify?"
-            prompt="Let us know so we can help make your expensing experience even better."
-            firstOptionText="Yeah!"
-            secondOptionText="Not really."
-            onFirstOptionSubmit={onPositive}
-            onSecondOptionSubmit={onNegative}
-            isSmallScreenWidth={shouldUseNarrowLayout}
+        <Modal
             onClose={onSkip}
             isVisible={isVisible}
-        />
+            type={shouldUseNarrowLayout ? CONST.MODAL.MODAL_TYPE.BOTTOM_DOCKED : CONST.MODAL.MODAL_TYPE.CONFIRM}
+            innerContainerStyle={styles.pv0}
+        >
+            <View style={[styles.m5]}>
+                {/* Todd with phones illustration */}
+                <View style={[styles.alignItemsCenter, styles.mb3]}>
+                    <ImageSVG
+                        src={Illustrations.ToddWithPhones}
+                        contentFit="contain"
+                        width={200}
+                        height={200}
+                    />
+                </View>
+
+                {/* Title and prompt */}
+                <View>
+                    <Text style={[styles.textHeadlineH1, styles.textAlignCenter, styles.mb3]}>
+                        Enjoying New Expensify?
+                    </Text>
+                    <Text style={[styles.textAlignCenter, styles.textSupporting]}>
+                        {'Let us know so we can help make your\nexpensing experience even better.'}
+                    </Text>
+                </View>
+
+                {/* Buttons */}
+                <Button
+                    success
+                    style={[styles.mt5]}
+                    onPress={onPositive}
+                    pressOnEnter
+                    text="Yeah!"
+                    large
+                />
+                <Button
+                    style={[styles.mt3, styles.noSelect]}
+                    onPress={onNegative}
+                    text="Not really"
+                    large
+                />
+            </View>
+        </Modal>
     );
 }
 
