3e7e802e7a85dbf3bd6b3df7d2de84608e135665
Jan Musiał <jan.musial@swmansion.com>
2025-11-12 11:40:00 +0100

    add arePersonalDetailsMissing util

--- src/pages/settings/Wallet/ExpensifyCardPage/ExpensifyCardMissingDetailsMagicCodePage.tsx
+++ src/pages/settings/Wallet/ExpensifyCardPage/ExpensifyCardMissingDetailsMagicCodePage.tsx
@@ -3,44 +3,53 @@ import ValidateCodeActionContent from '@components/ValidateCodeActionModal/Valid
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import {clearDraftValues} from '@libs/actions/FormActions';
-import {clearPersonalDetailsErrors, updatePersonalDetailsAndShipExpensifyCards} from '@libs/actions/PersonalDetails';
+import {clearPersonalDetailsErrors, setPersonalDetailsAndRevealExpensifyCard} from '@libs/actions/PersonalDetails';
 import {requestValidateCodeAction, resetValidateActionCodeSent} from '@libs/actions/User';
 import {normalizeCountryCode} from '@libs/CountryUtils';
 import {getLatestError} from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
+import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
+import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
 import {arePersonalDetailsMissing} from '@libs/PersonalDetailsUtils';
+import {getSubstepValues} from '@pages/MissingPersonalDetails/utils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
+import type SCREENS from '@src/SCREENS';
 import type {PersonalDetailsForm} from '@src/types/form';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
-import {getSubstepValues} from './utils';
+import useExpensifyCardContext from './useExpensifyCardContext';
 
-function MissingPersonalDetailsMagicCodePage() {
+type ExpensifyCardMissingDetailsMagicCodePageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.SETTINGS.WALLET.CARD_MISSING_DETAILS_CONFIRM_MAGIC_CODE>;
+
+function ExpensifyCardMissingDetailsMagicCodePage({
+    route: {
+        params: {cardID = ''},
+    },
+}: ExpensifyCardMissingDetailsMagicCodePageProps) {
     const {translate} = useLocalize();
     const [privatePersonalDetails] = useOnyx(ONYXKEYS.PRIVATE_PERSONAL_DETAILS, {canBeMissing: false});
     const [draftValues] = useOnyx(ONYXKEYS.FORMS.PERSONAL_DETAILS_FORM_DRAFT, {canBeMissing: false});
     const [countryCode = CONST.DEFAULT_COUNTRY_CODE] = useOnyx(ONYXKEYS.COUNTRY_CODE, {canBeMissing: false});
 
-    const [cardList] = useOnyx(ONYXKEYS.CARD_LIST, {canBeMissing: true});
     const [account] = useOnyx(ONYXKEYS.ACCOUNT, {canBeMissing: true});
 
     const [validateCodeAction] = useOnyx(ONYXKEYS.VALIDATE_ACTION_CODE, {canBeMissing: true});
     const privateDetailsErrors = privatePersonalDetails?.errors ?? undefined;
     const validateLoginError = getLatestError(privateDetailsErrors);
     const primaryLogin = account?.primaryLogin ?? '';
-    const areAllCardsShipped = Object.values(cardList ?? {})?.every((card) => card?.state !== CONST.EXPENSIFY_CARD.STATE.STATE_NOT_ISSUED);
+    const {setIsCardDetailsLoading, setCardsDetails, setCardsDetailsErrors} = useExpensifyCardContext();
 
     const missingDetails = arePersonalDetailsMissing(privatePersonalDetails);
 
     useEffect(() => {
-        if (missingDetails || !!privateDetailsErrors || !areAllCardsShipped) {
+        if (missingDetails || !!privateDetailsErrors) {
             return;
         }
 
         clearDraftValues(ONYXKEYS.FORMS.PERSONAL_DETAILS_FORM);
         Navigation.dismissModal();
-    }, [missingDetails, privateDetailsErrors, areAllCardsShipped]);
+    }, [missingDetails, privateDetailsErrors]);
 
     const clearError = () => {
         if (isEmptyObject(validateLoginError) && isEmptyObject(validateCodeAction?.errorFields)) {
@@ -53,9 +62,32 @@ function MissingPersonalDetailsMagicCodePage() {
 
     const handleSubmitForm = useCallback(
         (validateCode: string) => {
-            updatePersonalDetailsAndShipExpensifyCards(values, validateCode, countryCode);
+            setIsCardDetailsLoading((prevState: Record<number, boolean>) => ({
+                ...prevState,
+                [cardID]: true,
+            }));
+
+            setPersonalDetailsAndRevealExpensifyCard(values, validateCode, countryCode, Number.parseInt(cardID, 10))
+                .then((value) => {
+                    setCardsDetails((prevState) => ({...prevState, [cardID]: value}));
+                    setCardsDetailsErrors((prevState) => ({
+                        ...prevState,
+                        [cardID]: '',
+                    }));
+                    Navigation.goBack(ROUTES.SETTINGS_WALLET_DOMAIN_CARD.getRoute(cardID));
+                })
+                .catch((error: string) => {
+                    setCardsDetailsErrors((prevState) => ({
+                        ...prevState,
+                        [cardID]: error,
+                    }));
+                    Navigation.goBack(ROUTES.SETTINGS_WALLET_DOMAIN_CARD.getRoute(cardID));
+                })
+                .finally(() => {
+                    setIsCardDetailsLoading((prevState: Record<number, boolean>) => ({...prevState, [cardID]: false}));
+                });
         },
-        [countryCode, values],
+        [cardID, countryCode, setCardsDetails, setCardsDetailsErrors, setIsCardDetailsLoading, values],
     );
 
     return (
@@ -69,13 +101,13 @@ function MissingPersonalDetailsMagicCodePage() {
             clearError={clearError}
             onClose={() => {
                 resetValidateActionCodeSent();
-                Navigation.goBack(ROUTES.MISSING_PERSONAL_DETAILS);
+                Navigation.goBack(ROUTES.SETTINGS_WALLET_CARD_MISSING_DETAILS.getRoute(cardID));
             }}
             isLoading={privatePersonalDetails?.isLoading}
         />
     );
 }
 
-MissingPersonalDetailsMagicCodePage.displayName = 'MissingPersonalDetailsMagicCodePage';
+ExpensifyCardMissingDetailsMagicCodePage.displayName = 'ExpensifyCardMissingDetailsMagicCodePage';
 
-export default MissingPersonalDetailsMagicCodePage;
+export default ExpensifyCardMissingDetailsMagicCodePage;
