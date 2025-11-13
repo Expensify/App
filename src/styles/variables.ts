ba8a8b55ee15782c70cf26c84f41a6977b37a0dd
Jan Musiał <jan.musial@swmansion.com>
2025-11-13 10:40:41 +0100

    Increase letter avatars size

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -35,6 +35,7 @@ export default {
     buttonBorderRadius: 100,
     avatarSizeLargeBordered: 88,
     avatarSizeXLarge: 100,
+    avatarSizeXXLarge: 720,
     avatarSizeLarge: 80,
     avatarSizeMediumLarge: 60,
     avatarSizeMedium: 52,
