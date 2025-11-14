89d3ca3850f11707b95d114a41cf2493e08a136e
Samran Ahmed <samranahmed03@gmail.com>
2025-11-14 23:46:25 +0500

    fix: lint

--- src/components/DotIndicatorMessage.tsx
+++ src/components/DotIndicatorMessage.tsx
@@ -4,6 +4,7 @@ import type {ReactElement} from 'react';
 import React, {useState} from 'react';
 import type {StyleProp, TextStyle, ViewStyle} from 'react-native';
 import {View} from 'react-native';
+import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useStyleUtils from '@hooks/useStyleUtils';
 import useTheme from '@hooks/useTheme';
@@ -15,7 +16,6 @@ import type {TranslationKeyError} from '@src/types/onyx/OnyxCommon';
 import type {ReceiptError} from '@src/types/onyx/Transaction';
 import ConfirmModal from './ConfirmModal';
 import Icon from './Icon';
-import * as Expensicons from './Icon/Expensicons';
 import RenderHTML from './RenderHTML';
 import Text from './Text';
 
@@ -47,6 +47,7 @@ function DotIndicatorMessage({messages = {}, style, type, textStyles, dismissErr
     const styles = useThemeStyles();
     const StyleUtils = useStyleUtils();
     const {translate} = useLocalize();
+    const expensifyIcons = useMemoizedLazyExpensifyIcons(['DotIndicator'] as const);
 
     const [shouldShowErrorModal, setShouldShowErrorModal] = useState(false);
 
@@ -119,7 +120,7 @@ function DotIndicatorMessage({messages = {}, style, type, textStyles, dismissErr
         <View style={[styles.dotIndicatorMessage, style]}>
             <View style={styles.offlineFeedbackErrorDot}>
                 <Icon
-                    src={Expensicons.DotIndicator}
+                    src={expensifyIcons.DotIndicator}
                     fill={isErrorMessage ? theme.danger : theme.success}
                 />
             </View>
