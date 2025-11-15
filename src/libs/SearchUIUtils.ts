60fbe2c5fb03d0bda67c2546a31b88947369bfed
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2025-11-15 21:19:58 +0800

    Fix: App crashes when switching group-by from 'From' to 'Withdrawal ID'

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -1708,6 +1708,10 @@ function getWithdrawalIDSections(data: OnyxTypes.SearchResults['data'], queryJSO
                 transactionsQueryJSON = buildSearchQueryJSON(newQuery);
             }
 
+            if (!withdrawalIDGroup.accountNumber) {
+                continue;
+            }
+
             withdrawalIDSections[key] = {
                 groupedBy: CONST.SEARCH.GROUP_BY.WITHDRAWAL_ID,
                 transactions: [],
