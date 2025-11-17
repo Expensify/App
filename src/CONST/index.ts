c35c6204ca512c2d4b3fe0a38107c9994b398b3c
Puneet Lath <puneet@expensify.com>
2025-11-17 16:34:59 -0500

    Revert "Merge pull request #74356 from callstack-internal/VickyStash/refactor/73380-support-domain-workspace-cards-same-name"
    This reverts commit f62e324d2f9ed7dfb90ecfc7090d0a0766a2ac6c, reversing
    changes made to 8bf8f0ac8447d271fb5f8a4df93a461b35872af2.

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -3350,7 +3350,6 @@ const CONST = {
             AMEX_FILE_DOWNLOAD: 'americanexpressfd.us',
             CSV: 'ccupload',
         },
-        FEED_KEY_SEPARATOR: '#',
         STEP_NAMES: ['1', '2', '3', '4'],
         STEP: {
             BANK_CONNECTION: 'BankConnection',
