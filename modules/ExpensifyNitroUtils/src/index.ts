6eda14ba96c03bd8f19e1c0fc0fe88803acb8262
Christoph Pader <chris@margelo.com>
2025-11-19 15:54:12 +0000

    fix: invalid exports in `TtiMeasurementView`

--- modules/ExpensifyNitroUtils/src/index.ts
+++ modules/ExpensifyNitroUtils/src/index.ts
@@ -1,7 +1,7 @@
 import {getHostComponent, NitroModules} from 'react-native-nitro-modules';
 import type * as ContactsModuleSpec from './specs/ContactsModule.nitro';
 import {TtiMeasurementViewConfig} from './specs/TtiMeasurementView.nitro';
-import type {TtiMeasurementViewMethods, TtiMeasurementViewProps} from './specs/TtiMeasurementView.nitro';
+import type {OnTtiMeasurement, TtiMeasurementValue, TtiMeasurementViewMethods, TtiMeasurementViewProps, TtiMeasurementView as TtiMeasurementViewType} from './specs/TtiMeasurementView.nitro';
 
 const ContactsNitroModule = NitroModules.createHybridObject<ContactsModuleSpec.ContactsModule>('ContactsModule');
 
@@ -9,4 +9,5 @@ const TtiMeasurementView = getHostComponent<TtiMeasurementViewProps, TtiMeasurem
 
 export {ContactsNitroModule, TtiMeasurementView};
 export * from './specs/ContactsModule.nitro';
-export type {TtiMeasurementView as TtiMeasurementViewType} from './specs/TtiMeasurementView.nitro';
+
+export type {TtiMeasurementViewType, TtiMeasurementViewMethods, TtiMeasurementViewProps, TtiMeasurementValue, OnTtiMeasurement};
