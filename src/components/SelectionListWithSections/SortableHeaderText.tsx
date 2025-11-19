3aeaaeda6d36251c052bd9d7a58c107a49e2cbe6
Mohammad Jafarinejad <71210799+mohammadjafarinejad@users.noreply.github.com>
2025-11-19 21:29:41 +0400

    chore: remove unused eslint-disable comment

--- src/components/SelectionListWithSections/SortableHeaderText.tsx
+++ src/components/SelectionListWithSections/SortableHeaderText.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {View} from 'react-native';
 import type {StyleProp, TextStyle, ViewStyle} from 'react-native';
 import Icon from '@components/Icon';
-// eslint-disable-next-line no-restricted-imports
 import * as Expensicons from '@components/Icon/Expensicons';
 import PressableWithFeedback from '@components/Pressable/PressableWithFeedback';
 import type {SortOrder} from '@components/Search/types';
