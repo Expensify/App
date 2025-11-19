5297d91c0838a412e1b397ca26556ebc79562937
eliran goshen <elirangoshen@gmail.com>
2025-11-19 13:45:01 +0100

    adding span for od nd transition

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -1655,6 +1655,7 @@ const CONST = {
         TAG_ACTIVE_POLICY: 'active_policy_id',
         SPAN_OPEN_REPORT: 'ManualOpenReport',
         SPAN_APP_STARTUP: 'ManualAppStartup',
+        SPAN_OD_ND_TRANSITION: 'ManualOdNdTransition',
     },
     PRIORITY_MODE: {
         GSD: 'gsd',
