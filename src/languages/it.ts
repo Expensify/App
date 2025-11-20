eb8424635c37f23d990e3ea876b72291e824ac6a
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-11-21 00:18:20 +0100

    minor fixes

--- src/languages/it.ts
+++ src/languages/it.ts
@@ -1292,7 +1292,7 @@ const translations: TranslationDeepObject<typeof en> = {
         expenseAmount: ({formattedAmount, comment}: RequestedAmountMessageParams) => `${formattedAmount}${comment ? `per ${comment}` : ''}`,
         submitted: ({memo}: SubmittedWithMemoParams) => `inviato${memo ? `, dicendo: ${memo}` : ''}`,
         automaticallySubmitted: `inviato tramite <a href="${CONST.SELECT_WORKFLOWS_HELP_URL}">invio ritardato</a>`,
-        queuedToSubmitViaDEW: 'in coda per l\'invio tramite flusso di approvazione personalizzato',
+        queuedToSubmitViaDEW: "in coda per l'invio tramite flusso di approvazione personalizzato",
         dynamicExternalWorkflowCannotSubmit: 'Questo rapporto non può essere inviato. Si prega di rivedere i commenti per risolvere.',
         trackedAmount: ({formattedAmount, comment}: RequestedAmountMessageParams) => `tracking ${formattedAmount}${comment ? `per ${comment}` : ''}`,
         splitAmount: ({amount}: SplitAmountParams) => `dividi ${amount}`,
