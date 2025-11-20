b5990c37bc9b43de8e094f0720edcff3d6ed0046
Mohammad Jafarinejad <71210799+mohammadjafarinejad@users.noreply.github.com>
2025-11-20 19:14:26 +0400

    fix: replace direct illustration imports with useMemoizedLazyIllustrations in billing banners

--- src/pages/settings/Subscription/CardSection/BillingBanner/PreTrialBillingBanner.tsx
+++ src/pages/settings/Subscription/CardSection/BillingBanner/PreTrialBillingBanner.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import * as Illustrations from '@components/Icon/Illustrations';
 import Text from '@components/Text';
 import TextLink from '@components/TextLink';
+import {useMemoizedLazyIllustrations} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useThemeStyles from '@hooks/useThemeStyles';
 import * as Report from '@libs/actions/Report';
@@ -13,6 +13,7 @@ import BillingBanner from './BillingBanner';
 function PreTrialBillingBanner() {
     const {translate} = useLocalize();
     const styles = useThemeStyles();
+    const illustrations = useMemoizedLazyIllustrations(['TreasureChest'] as const);
 
     const navigateToChat = () => {
         const reportUsedForOnboarding = ReportUtils.getChatUsedForOnboarding();
@@ -40,7 +41,7 @@ function PreTrialBillingBanner() {
                     {translate('subscription.billingBanner.preTrial.subtitleEnd')}
                 </Text>
             }
-            icon={Illustrations.TreasureChest}
+            icon={illustrations.TreasureChest}
         />
     );
 }
