60acefada3e1f45eb47f9651609b308681f82369
DylanDylann <dylandylan9112@gmail.com>
2025-11-20 18:42:07 +0700

    prettier

--- tests/unit/MoneyRequestReportUtilsTest.ts
+++ tests/unit/MoneyRequestReportUtilsTest.ts
@@ -10,14 +10,14 @@ describe('MoneyRequestReportUtils', () => {
     describe('getReportIDForTransaction', () => {
         it('returns transaction thread ID if its not from one transaction report', () => {
             const transactionItem = {...transactionItemBaseMock};
-            const resultID = getReportIDForTransaction(transactionItem, "thread123");
+            const resultID = getReportIDForTransaction(transactionItem, 'thread123');
 
             expect(resultID).toBe('thread123');
         });
 
         it('returns transaction thread ID if its from self DM', () => {
             const transactionItem = {...transactionItemBaseMock, reportID: CONST.REPORT.UNREPORTED_REPORT_ID};
-            const resultID = getReportIDForTransaction(transactionItem, "thread123");
+            const resultID = getReportIDForTransaction(transactionItem, 'thread123');
 
             expect(resultID).toBe('thread123');
         });
