a5dc803edb3ba980201dfd8ff77f71a960123259
dmkt9 <demoskratos9x@gmail.com>
2025-11-20 22:21:38 +0700

    Fix - The bottom menu bar overlaps with the keyboard instead of minimizing

--- src/components/Navigation/NavigationTabBar/index.tsx
+++ src/components/Navigation/NavigationTabBar/index.tsx
@@ -226,7 +226,10 @@ function NavigationTabBar({selectedTab, isTopLevelBar = false, shouldShowFloatin
                         chatTabBrickRoad={chatTabBrickRoad}
                     />
                 )}
-                <View style={styles.leftNavigationTabBarContainer}>
+                <View
+                    style={styles.leftNavigationTabBarContainer}
+                    testID={NavigationTabBar.displayName}
+                >
                     <HeaderGap />
                     <View style={styles.flex1}>
                         <PressableWithFeedback
@@ -375,7 +378,10 @@ function NavigationTabBar({selectedTab, isTopLevelBar = false, shouldShowFloatin
                     chatTabBrickRoad={chatTabBrickRoad}
                 />
             )}
-            <View style={styles.navigationTabBarContainer}>
+            <View
+                style={styles.navigationTabBarContainer}
+                testID={NavigationTabBar.displayName}
+            >
                 <PressableWithFeedback
                     onPress={navigateToChats}
                     role={CONST.ROLE.BUTTON}
