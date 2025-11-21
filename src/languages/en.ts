470afcf6e5631e5953410e67ee98ced892e6afeb
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-11-21 02:54:40 +0100

    covering more tests

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -7463,6 +7463,7 @@ const translations = {
             hasChildReportAwaitingAction: 'Has child report awaiting action',
             hasMissingInvoiceBankAccount: 'Has missing invoice bank account',
             hasUnresolvedCardFraudAlert: 'Has unresolved card fraud alert',
+            hasDEWError: 'Has DEW error',
         },
         reasonRBR: {
             hasErrors: 'Has errors in report or report actions data',
