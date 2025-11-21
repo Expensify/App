a710abc5c481379176402250b15a85df7c172017
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-11-21 01:50:21 +0100

    reverting permissions change

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -5,7 +5,7 @@ import type BetaConfiguration from '@src/types/onyx/BetaConfiguration';
 
 // eslint-disable-next-line rulesdir/no-beta-handler
 function canUseAllBetas(betas: OnyxEntry<Beta[]>): boolean {
-    return true;
+    return !!betas?.includes(CONST.BETAS.ALL);
 }
 
 /**
