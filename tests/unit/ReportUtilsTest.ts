8bb5b47b585fa5cfd5675686fdf2dcef001eaf73
Rocio Perez-Cano <rocio@expensify.com>
2025-11-21 13:53:03 -0500

    Prettier

--- tests/unit/ReportUtilsTest.ts
+++ tests/unit/ReportUtilsTest.ts
@@ -1521,7 +1521,9 @@ describe('ReportUtils', () => {
                                 personalDetails: participantsPersonalDetails,
                             });
 
-                            expect(reportName).toBe(`This workspace has been upgraded to the Control plan. Click <a href="${CONST.PLAN_TYPES_AND_PRICING_HELP_URL}">here</a> for more information.`);
+                            expect(reportName).toBe(
+                                `This workspace has been upgraded to the Control plan. Click <a href="${CONST.PLAN_TYPES_AND_PRICING_HELP_URL}">here</a> for more information.`,
+                            );
                         });
                     });
                 });
