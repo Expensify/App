e1975aec97bc8a99f532becf3bd8d758812a20af
TaduJR <nattysec26@gmail.com>
2025-11-22 15:27:52 +0300

    refactor: simplify ReportLayoutPage by removing unused reportID parameter

--- src/pages/settings/Report/ReportLayoutPage.tsx
+++ src/pages/settings/Report/ReportLayoutPage.tsx
@@ -10,31 +10,23 @@ import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {getReportLayoutGroupBy, setReportLayoutGroupBy} from '@libs/actions/ReportLayout';
 import Navigation from '@libs/Navigation/Navigation';
-import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
-import type {ReportSettingsNavigatorParamList} from '@libs/Navigation/types';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import ROUTES from '@src/ROUTES';
-import type SCREENS from '@src/SCREENS';
 import type {ReportLayoutGroupBy} from '@src/types/onyx';
 
 type ReportLayoutItem = ListItem & {
     value: ReportLayoutGroupBy;
 };
 
-type ReportLayoutPageProps = PlatformStackScreenProps<ReportSettingsNavigatorParamList, typeof SCREENS.REPORT_SETTINGS.REPORT_LAYOUT>;
-
-function ReportLayoutPage({route}: ReportLayoutPageProps) {
+function ReportLayoutPage() {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
     const [reportLayoutGroupByNVP] = useOnyx(ONYXKEYS.NVP_REPORT_LAYOUT_GROUP_BY, {canBeMissing: true});
-
-    const {reportID} = route.params;
     const currentGroupBy = getReportLayoutGroupBy(reportLayoutGroupByNVP);
 
     const goBack = useCallback(() => {
-        Navigation.goBack(ROUTES.REPORT_SETTINGS.getRoute(reportID));
-    }, [reportID]);
+        Navigation.dismissModal();
+    }, []);
 
     const onSelectGroupBy = useCallback(
         (item: ReportLayoutItem) => {
@@ -62,10 +54,6 @@ function ReportLayoutPage({route}: ReportLayoutPageProps) {
             isSelected: currentGroupBy === CONST.REPORT_LAYOUT.GROUP_BY.TAG,
         },
     ];
-
-    if (!reportID) {
-        return null;
-    }
 
     return (
         <ScreenWrapper
