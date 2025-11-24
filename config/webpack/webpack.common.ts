d4ba87b3fb8cf0f6e4ba686964f64ace262f649b
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-11-24 14:46:40 +0100

    remove pdf worker module rule

--- config/webpack/webpack.common.ts
+++ config/webpack/webpack.common.ts
@@ -225,12 +225,6 @@ const getCommonConfiguration = ({file = '.env', platform = 'web'}: Environment):
                      */
                     exclude: [new RegExp(`node_modules/(?!(${includeModules})/).*|.native.js$`)],
                 },
-                // We are importing this worker as a string by using asset/source otherwise it will default to loading via an HTTPS request later.
-                // This causes issues if we have gone offline before the pdfjs web worker is set up as we won't be able to load it from the server.
-                {
-                    test: new RegExp('node_modules/pdfjs-dist/build/pdf.worker.min.mjs'),
-                    type: 'asset/source',
-                },
 
                 // Rule for react-native-web-webview
                 {
