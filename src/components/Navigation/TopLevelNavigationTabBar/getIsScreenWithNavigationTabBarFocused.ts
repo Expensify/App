faefc85248020d121c7e4d26c0aaf6589888d7e7
paulnjs <paulqn45@outlook.com>
2025-11-24 14:07:51 +0700

    fix: Scan icon and bottom navigation button shown briefly in concierge chat

--- src/components/Navigation/TopLevelNavigationTabBar/getIsScreenWithNavigationTabBarFocused.ts
+++ src/components/Navigation/TopLevelNavigationTabBar/getIsScreenWithNavigationTabBarFocused.ts
@@ -1,15 +1,19 @@
 import {findFocusedRoute} from '@react-navigation/native';
 import type {NavigationState} from '@react-navigation/native';
-import {isSplitNavigatorName} from '@libs/Navigation/helpers/isNavigatorName';
+import NAVIGATORS from '@src/NAVIGATORS';
 import SCREENS_WITH_NAVIGATION_TAB_BAR from './SCREENS_WITH_NAVIGATION_TAB_BAR';
 
 function getIsScreenWithNavigationTabBarFocused(state: NavigationState) {
     const focusedRoute = findFocusedRoute(state);
 
     const routeName = focusedRoute?.params && 'screen' in focusedRoute.params ? (focusedRoute.params.screen as string) : focusedRoute?.name;
+
+    if (routeName == NAVIGATORS.REPORTS_SPLIT_NAVIGATOR) {
+        return false;
+    }
     // We are checking if the focused route is a split navigator because there may be a brief moment where the navigator doesn't have state yet.
     // That mens we don't have screen with bottom tab focused. This caused glitching.
-    return SCREENS_WITH_NAVIGATION_TAB_BAR.includes(routeName ?? '') || isSplitNavigatorName(routeName);
+    return SCREENS_WITH_NAVIGATION_TAB_BAR.includes(routeName ?? '');
 }
 
 export default getIsScreenWithNavigationTabBarFocused;
