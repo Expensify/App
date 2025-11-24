0f04e8e64879a1673d5d7f106970bf57055da2b5
war-in <war-in@users.noreply.github.com>
2025-11-24 17:28:23 +0100

    chore: remove double `//`

--- src/libs/actions/Session/clearCache/index.native.ts
+++ src/libs/actions/Session/clearCache/index.native.ts
@@ -3,7 +3,7 @@ import type ClearCache from './types';
 
 // `unlink` is used to delete contents of the caches directory
 const clearStorage: ClearCache = async () => {
-    // // iOS restricts deletion of the entire caches directory, requiring us to individually remove its contents
+    // iOS restricts deletion of the entire caches directory, requiring us to individually remove its contents
     // For additional context, see: https://github.com/Expensify/App/pull/75894
     const files = await readDir(CachesDirectoryPath);
     const deletionPromises = files.map((file) => unlink(file.path));
