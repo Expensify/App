fc6e6114f3671f18a2f4d544e735ec0c663ce373
war-in <war-in@users.noreply.github.com>
2025-11-24 14:23:49 +0100

    fix: iOS - remove all files inside caches directory, not the directory itself

--- src/libs/actions/Session/clearCache/index.native.ts
+++ src/libs/actions/Session/clearCache/index.native.ts
@@ -1,7 +1,12 @@
-import {CachesDirectoryPath, unlink} from 'react-native-fs';
+import {CachesDirectoryPath, readDir, unlink} from 'react-native-fs';
 import type ClearCache from './types';
 
-// `unlink` is used to delete the caches directory
-const clearStorage: ClearCache = () => unlink(CachesDirectoryPath);
+// `unlink` is used to delete contents of the caches directory
+const clearStorage: ClearCache = async () => {
+    const files = await readDir(CachesDirectoryPath);
+    const deletionPromises = files.map((file) => unlink(file.path));
+
+    await Promise.all(deletionPromises);
+};
 
 export default clearStorage;
