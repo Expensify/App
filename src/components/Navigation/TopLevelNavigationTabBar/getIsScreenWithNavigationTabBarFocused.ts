578406da50f846324ff24a4989db11ef7adcce59
paulnjs <paulqn45@outlook.com>
2025-11-24 22:11:03 +0700

    update code

--- src/components/Navigation/TopLevelNavigationTabBar/getIsScreenWithNavigationTabBarFocused.ts
+++ src/components/Navigation/TopLevelNavigationTabBar/getIsScreenWithNavigationTabBarFocused.ts
@@ -1,6 +1,5 @@
 import {findFocusedRoute} from '@react-navigation/native';
 import type {NavigationState} from '@react-navigation/native';
-import NAVIGATORS from '@src/NAVIGATORS';
 import SCREENS_WITH_NAVIGATION_TAB_BAR from './SCREENS_WITH_NAVIGATION_TAB_BAR';
 
 function getIsScreenWithNavigationTabBarFocused(state: NavigationState) {
@@ -8,9 +7,6 @@ function getIsScreenWithNavigationTabBarFocused(state: NavigationState) {
 
     const routeName = focusedRoute?.params && 'screen' in focusedRoute.params ? (focusedRoute.params.screen as string) : focusedRoute?.name;
 
-    if (routeName === NAVIGATORS.REPORTS_SPLIT_NAVIGATOR) {
-        return false;
-    }
     // We are checking if the focused route is a split navigator because there may be a brief moment where the navigator doesn't have state yet.
     // That mens we don't have screen with bottom tab focused. This caused glitching.
     return SCREENS_WITH_NAVIGATION_TAB_BAR.includes(routeName ?? '');
