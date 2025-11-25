c60e1bcc17c8049f9924e9b1c4274660528a7555
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2025-11-25 23:54:26 +0100

    fix other linter errors

--- src/hooks/useIsAuthenticated.tsx
+++ src/hooks/useIsAuthenticated.tsx
@@ -3,7 +3,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 import useOnyx from './useOnyx';
 
 function useIsAuthenticated() {
-    const [session] = useOnyx(ONYXKEYS.SESSION);
+    const [session] = useOnyx(ONYXKEYS.SESSION, {canBeMissing: true});
     const isAuthenticated = useMemo(() => !!(session?.authToken ?? null), [session?.authToken]);
     return isAuthenticated;
 }
