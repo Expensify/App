8ea7d7eb7ad22b0ea80eefcd2a63aacff81a8d15
alberto <alberto@expensify.com>
2025-11-26 12:41:15 +0100

    Add beta

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -710,6 +710,7 @@ const CONST = {
         NO_OPTIMISTIC_TRANSACTION_THREADS: 'noOptimisticTransactionThreads',
         UBER_FOR_BUSINESS: 'uberForBusiness',
         CUSTOM_REPORT_NAMES: 'newExpensifyCustomReportNames',
+        ZERO_EXPENSES: 'zeroExpenses',
     },
     BUTTON_STATES: {
         DEFAULT: 'default',
