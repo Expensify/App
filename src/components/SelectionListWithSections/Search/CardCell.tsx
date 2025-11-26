14a9be1c5d4f281f2e25787ef3b152776f6f3180
Mohammad Jafarinejad <71210799+mohammadjafarinejad@users.noreply.github.com>
2025-11-27 03:26:06 +0400

    chore: fix prettier

--- src/components/SelectionListWithSections/Search/CardCell.tsx
+++ src/components/SelectionListWithSections/Search/CardCell.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { ColorValue} from 'react-native';
+import type {ColorValue} from 'react-native';
 import {View} from 'react-native';
 import ReportActionAvatars from '@components/ReportActionAvatars';
 import TextWithTooltip from '@components/TextWithTooltip';
