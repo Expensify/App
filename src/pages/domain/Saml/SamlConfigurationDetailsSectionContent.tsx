a4c6ae28909821a8b90150a8f24677a43bf03050
mhawryluk <marcinhawrylukxx@gmail.com>
2025-11-26 12:10:24 +0100

    Set metaIdentity form input as required

--- src/pages/domain/Saml/SamlConfigurationDetailsSectionContent.tsx
+++ src/pages/domain/Saml/SamlConfigurationDetailsSectionContent.tsx
@@ -71,7 +71,7 @@ function SamlConfigurationDetailsSectionContent({accountID, domainName, shouldSh
                 autoGrowHeight
                 maxAutoGrowHeight={variables.textInputAutoGrowMaxHeight}
                 onValueCommitted={(metaIdentity) => {
-                    if (StringUtils.isEmptyString(metaIdentity) || metaIdentity === StringUtils.normalizeCRLF(samlMetadata.metaIdentity)) {
+                    if (metaIdentity === StringUtils.normalizeCRLF(samlMetadata.metaIdentity)) {
                         return;
                     }
                     setSamlIdentity(accountID, domainName, metaIdentity);
@@ -79,6 +79,7 @@ function SamlConfigurationDetailsSectionContent({accountID, domainName, shouldSh
                 errorText={getLatestErrorMessage({errors: samlMetadata.samlMetadataError})}
                 maxLength={Infinity}
                 enabledWhenOffline={false}
+                required
             />
 
             <MenuItemWithTopDescription
