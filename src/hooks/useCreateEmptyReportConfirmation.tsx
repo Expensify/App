064ec7b0a20284221d3ac4ff5a6ba482085b375b
ShridharGoel <35566748+ShridharGoel@users.noreply.github.com>
2025-11-26 09:56:10 +0530

    Lint fix

--- src/hooks/useCreateEmptyReportConfirmation.tsx
+++ src/hooks/useCreateEmptyReportConfirmation.tsx
@@ -5,11 +5,11 @@ import CheckboxWithLabel from '@components/CheckboxWithLabel';
 import ConfirmModal from '@components/ConfirmModal';
 import Text from '@components/Text';
 import TextLink from '@components/TextLink';
-import useThemeStyles from './useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
 import {buildCannedSearchQuery} from '@libs/SearchQueryUtils';
 import CONST from '@src/CONST';
 import ROUTES from '@src/ROUTES';
+import useThemeStyles from './useThemeStyles';
 import useLocalize from './useLocalize';
 
 type UseCreateEmptyReportConfirmationParams = {
