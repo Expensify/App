c96450163e757b4d4ede98eae8ead906882183ff
TaduJR <nattysec26@gmail.com>
2025-11-26 22:57:37 +0300

    fix: useOnyx import in ReportLayoutPage

--- src/pages/settings/Report/ReportLayoutPage.tsx
+++ src/pages/settings/Report/ReportLayoutPage.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback} from 'react';
-import {useOnyx} from 'react-native-onyx';
+import useOnyx from '@hooks/useOnyx';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
