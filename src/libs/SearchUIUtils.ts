9ea500aa8877062d68b710db09c39c37318dd7b0
Mohammad Jafarinejad <71210799+mohammadjafarinejad@users.noreply.github.com>
2025-11-27 14:24:21 +0400

    fix: hide Action column from nested rows

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -2640,7 +2640,7 @@ function getColumnsToShow(
               [CONST.SEARCH.TABLE_COLUMNS.TAG]: false,
               [CONST.SEARCH.TABLE_COLUMNS.TAX_AMOUNT]: false,
               [CONST.SEARCH.TABLE_COLUMNS.TOTAL_AMOUNT]: true,
-              [CONST.SEARCH.TABLE_COLUMNS.ACTION]: true,
+              [CONST.SEARCH.TABLE_COLUMNS.ACTION]: false,
               [CONST.SEARCH.TABLE_COLUMNS.TITLE]: true,
           };
 
