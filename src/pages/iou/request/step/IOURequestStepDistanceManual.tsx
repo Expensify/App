5b757d0b8069ef814baa768889c41041accccecd
alberto <alberto@expensify.com>
2025-11-27 09:28:55 +0100

    prevent zero distances on beta

--- src/pages/iou/request/step/IOURequestStepDistanceManual.tsx
+++ src/pages/iou/request/step/IOURequestStepDistanceManual.tsx
@@ -307,7 +307,13 @@ function IOURequestStepDistanceManual({
         const value = numberFormRef.current?.getNumber() ?? '';
         const isP2P = isParticipantP2P(getMoneyRequestParticipantsFromReport(report).at(0));
 
-        if (!value.length || parseFloat(value) < 0) {
+        if (isBetaEnabled(CONST.BETAS.ZERO_EXPENSES)) {
+            if (!value.length || parseFloat(value) < 0) {
+                setFormError(translate('iou.error.invalidDistance'));
+                return;
+            }
+        }
+        if (!value.length || parseFloat(value) <= 0) {
             setFormError(translate('iou.error.invalidDistance'));
             return;
         }
