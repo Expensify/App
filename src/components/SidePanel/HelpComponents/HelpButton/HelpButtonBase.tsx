460015d2b955f5169a823cc7a317bdc0c9937759
Blazej Kustra <blazej.kustra@swmansion.com>
2025-11-28 17:14:33 +0100

    Bring back help button

--- src/components/SidePanel/HelpComponents/HelpButton/HelpButtonBase.tsx
+++ src/components/SidePanel/HelpComponents/HelpButton/HelpButtonBase.tsx
@@ -1,10 +1,40 @@
+import React from 'react';
+import Icon from '@components/Icon';
+import * as Expensicons from '@components/Icon/Expensicons';
+import {PressableWithoutFeedback} from '@components/Pressable';
+import Tooltip from '@components/Tooltip';
+import useLocalize from '@hooks/useLocalize';
+import useSidePanel from '@hooks/useSidePanel';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
 import type HelpButtonProps from './types';
 
-// Help panel is currently disabled. To open it manually, run:
-// Onyx.set('nvp_sidePanel', { open: true }) in the console.
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function HelpButton({style}: HelpButtonProps) {
-    return null;
+    const styles = useThemeStyles();
+    const theme = useTheme();
+    const {translate} = useLocalize();
+    const {openSidePanel, shouldHideHelpButton} = useSidePanel();
+
+    if (shouldHideHelpButton) {
+        return null;
+    }
+
+    return (
+        <Tooltip text={translate('common.help')}>
+            <PressableWithoutFeedback
+                accessibilityLabel={translate('common.help')}
+                style={[styles.flexRow, styles.touchableButtonImage, style]}
+                onPress={openSidePanel}
+            >
+                <Icon
+                    src={Expensicons.ConciergeAvatar}
+                    fill={theme.icon}
+                    width={28}
+                    height={28}
+                />
+            </PressableWithoutFeedback>
+        </Tooltip>
+    );
 }
 
 HelpButton.displayName = 'HelpButtonBase';
