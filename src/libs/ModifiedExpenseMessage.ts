d3b2dbbbc7d97f021129d3eabfdcbbb3201e5069
Krishna Chaitanya <chaitanya3024@gmail.com>
2025-11-28 12:35:01 +0530

    move lint override to correct place

--- src/libs/ModifiedExpenseMessage.ts
+++ src/libs/ModifiedExpenseMessage.ts
@@ -47,8 +47,8 @@ function buildMessageFragmentForValue(
     const newValueToDisplay = valueInQuotes ? `"${newValue}"` : newValue;
 
     // If the valueName is category and the old value was Uncategorized, show it in lowercase without quotes
-    // eslint-disable-next-line @typescript-eslint/no-deprecated
     let oldValueToDisplay;
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
     if (valueName.includes(translateLocal('common.category')) && isCategoryMissing(oldValue)) {
         oldValueToDisplay = oldValue.toLowerCase();
     } else if (valueInQuotes) {
