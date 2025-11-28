39a86a90515271bc1062e91ca7eafbd356650396
Krishna Chaitanya <chaitanya3024@gmail.com>
2025-11-28 12:31:52 +0530

    remove nested ternary as lint complains

--- src/libs/ModifiedExpenseMessage.ts
+++ src/libs/ModifiedExpenseMessage.ts
@@ -48,7 +48,14 @@ function buildMessageFragmentForValue(
 
     // If the valueName is category and the old value was Uncategorized, show it in lowercase without quotes
     // eslint-disable-next-line @typescript-eslint/no-deprecated
-    const oldValueToDisplay = valueName.includes(translateLocal('common.category')) && isCategoryMissing(oldValue) ? oldValue.toLowerCase() : valueInQuotes ? `"${oldValue}"` : oldValue;
+    let oldValueToDisplay;
+    if (valueName.includes(translateLocal('common.category')) && isCategoryMissing(oldValue)) {
+        oldValueToDisplay = oldValue.toLowerCase();
+    } else if (valueInQuotes) {
+        oldValueToDisplay = `"${oldValue}"`;
+    } else {
+        oldValueToDisplay = oldValue;
+    }
 
     const displayValueName = shouldConvertToLowercase ? valueName.toLowerCase() : valueName;
     // eslint-disable-next-line @typescript-eslint/no-deprecated
