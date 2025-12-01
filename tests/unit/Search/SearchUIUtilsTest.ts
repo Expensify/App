a5541b24e2114ef9474f01eb3025519a0556422a
Shubham Agrawal <shubham.agra1206@gmail.com>
2025-12-01 22:10:23 +0530

    Fixed test

--- tests/unit/Search/SearchUIUtilsTest.ts
+++ tests/unit/Search/SearchUIUtilsTest.ts
@@ -1790,7 +1790,7 @@ describe('SearchUIUtils', () => {
             expect(distanceTransaction).toBeDefined();
             expect(distanceTransaction?.iouRequestType).toBe(CONST.IOU.REQUEST_TYPE.DISTANCE);
 
-            const expectedPropertyCount = 49;
+            const expectedPropertyCount = 48;
             expect(Object.keys(distanceTransaction ?? {}).length).toBe(expectedPropertyCount);
         });
 
@@ -1823,7 +1823,7 @@ describe('SearchUIUtils', () => {
             expect(distanceTransaction).toBeDefined();
             expect(distanceTransaction?.iouRequestType).toBe(CONST.IOU.REQUEST_TYPE.DISTANCE);
 
-            const expectedPropertyCount = 49;
+            const expectedPropertyCount = 48;
             expect(Object.keys(distanceTransaction ?? {}).length).toBe(expectedPropertyCount);
         });
 
