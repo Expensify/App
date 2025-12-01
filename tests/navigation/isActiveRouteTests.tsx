71fe82af22eea646e20c5fbccbd4ad2e75d9b504
ShridharGoel <35566748+ShridharGoel@users.noreply.github.com>
2025-12-01 10:21:27 +0530

    Fix lint issue of _esModule

--- tests/navigation/isActiveRouteTests.tsx
+++ tests/navigation/isActiveRouteTests.tsx
@@ -12,6 +12,7 @@ jest.mock('@libs/Navigation/navigationRef', () => {
     };
 
     return {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
         __esModule: true,
         default: navigationRefMock,
     };
