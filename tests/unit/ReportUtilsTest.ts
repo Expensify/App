0d1e7078140d743afedc261124e139d4a48f59ad
allgandaf <whogandalf@gmail.com>
2025-12-02 09:32:43 +0530

    fix

--- tests/unit/ReportUtilsTest.ts
+++ tests/unit/ReportUtilsTest.ts
@@ -55,7 +55,6 @@ import {
     getDefaultWorkspaceAvatar,
     getDisplayNameForParticipant,
     getDisplayNamesWithTooltips,
-    getGroupChatName,
     getIconsForParticipants,
     getIOUReportActionDisplayMessage,
     getMoneyReportPreviewName,
