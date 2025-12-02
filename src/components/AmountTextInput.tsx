6a31c2e559446a4d875ab6f43bbb240e502ae8aa
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2025-12-02 09:42:21 +0800

    disable the input if the split is not editable

--- src/components/AmountTextInput.tsx
+++ src/components/AmountTextInput.tsx
@@ -41,7 +41,7 @@ type AmountTextInputProps = {
 
     /** Hide the focus styles on TextInput */
     hideFocusedState?: boolean;
-} & Pick<BaseTextInputProps, 'autoFocus' | 'autoGrowExtraSpace' | 'submitBehavior' | 'ref' | 'onFocus' | 'onBlur'>;
+} & Pick<BaseTextInputProps, 'autoFocus' | 'autoGrowExtraSpace' | 'submitBehavior' | 'ref' | 'onFocus' | 'onBlur' | 'disabled'>;
 
 function AmountTextInput({
     formattedAmount,
@@ -57,6 +57,7 @@ function AmountTextInput({
     hideFocusedState = true,
     shouldApplyPaddingToContainer = false,
     ref,
+    disabled,
     ...rest
 }: AmountTextInputProps) {
     return (
@@ -69,6 +70,7 @@ function AmountTextInput({
             textInputContainerStyles={containerStyle}
             onChangeText={onChangeAmount}
             ref={ref}
+            disabled={disabled}
             value={formattedAmount}
             placeholder={placeholder}
             inputMode={CONST.INPUT_MODE.DECIMAL}
