ff8fca277a8e99fbd0dd04577bba28470f5c9b02
Blazej Kustra <blazej.kustra@swmansion.com>
2025-12-03 17:03:17 +0100

    Hide side panel and button on production

--- src/hooks/useSidePanelDisplayStatus.tsx
+++ src/hooks/useSidePanelDisplayStatus.tsx
@@ -1,4 +1,5 @@
 import ONYXKEYS from '@src/ONYXKEYS';
+import useEnvironment from './useEnvironment';
 import useOnyx from './useOnyx';
 import useResponsiveLayout from './useResponsiveLayout';
 
@@ -8,17 +9,20 @@ import useResponsiveLayout from './useResponsiveLayout';
 function useSidePanelDisplayStatus() {
     const {isExtraLargeScreenWidth, shouldUseNarrowLayout} = useResponsiveLayout();
     const [sidePanelNVP] = useOnyx(ONYXKEYS.NVP_SIDE_PANEL, {canBeMissing: true});
+    const {isProduction} = useEnvironment();
 
     const isSidePanelVisible = isExtraLargeScreenWidth ? sidePanelNVP?.open : sidePanelNVP?.openNarrowScreen;
 
     // The Side Panel is hidden when:
     // - NVP is not set or it is false
-    const shouldHideSidePanel = !isSidePanelVisible || !sidePanelNVP;
-    const isSidePanelHiddenOrLargeScreen = !isSidePanelVisible || isExtraLargeScreenWidth || !sidePanelNVP;
+    // - Production environment
+    const shouldHideSidePanel = !isSidePanelVisible || !sidePanelNVP || isProduction;
+    const isSidePanelHiddenOrLargeScreen = !isSidePanelVisible || isExtraLargeScreenWidth || isProduction || !sidePanelNVP;
 
     // The help button is hidden when:
     // - Side Panel is displayed currently
-    const shouldHideHelpButton = !shouldHideSidePanel;
+    // - Production environment
+    const shouldHideHelpButton = !shouldHideSidePanel || isProduction;
     const shouldHideSidePanelBackdrop = shouldHideSidePanel || isExtraLargeScreenWidth || shouldUseNarrowLayout;
 
     return {
