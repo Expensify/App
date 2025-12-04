8195f07374f8d8413b58989742d979c4f3ad37df
Julian Kobrynski <julian.kobrynski@callstack.com>
2025-12-04 08:30:34 +0100

    load react-pdf libary in the native apps

--- src/components/PDF/setup/index.native.ts
+++ src/components/PDF/setup/index.native.ts
@@ -1,3 +1,7 @@
+// This import is necesssary as the native apps use some files from the react-pdf library
+// and won't work without it
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {Document} from 'react-pdf';
 import type {PDFSetupPromise, UsePreloadLazyModules} from './types';
 
 function ensurePdfJsInitialized(): PDFSetupPromise {
