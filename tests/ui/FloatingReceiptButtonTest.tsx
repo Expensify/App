f22e3c6b4ca07cfb161418ba9514e26136104189
dmkt9 <demoskratos9x@gmail.com>
2025-12-04 22:31:56 +0700

    Next button on Initial page reopens same page when Workflows settings are disabled

--- tests/ui/FloatingReceiptButtonTest.tsx
+++ tests/ui/FloatingReceiptButtonTest.tsx
@@ -8,6 +8,7 @@ import {LocaleContextProvider} from '@components/LocaleContextProvider';
 import NavigationTabBar from '@components/Navigation/NavigationTabBar';
 import NAVIGATION_TABS from '@components/Navigation/NavigationTabBar/NAVIGATION_TABS';
 import OnyxListItemProvider from '@components/OnyxListItemProvider';
+import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import {SidebarOrderedReportsContextProvider} from '@hooks/useSidebarOrderedReports';
 import {startMoneyRequest} from '@libs/actions/IOU';
 import navigationRef from '@libs/Navigation/navigationRef';
@@ -22,10 +23,12 @@ jest.mock('@libs/actions/IOU', () => ({
     startMoneyRequest: jest.fn(),
 }));
 
-jest.mock('@hooks/useResponsiveLayout', () => (): {shouldUseNarrowLayout: boolean} => ({shouldUseNarrowLayout: false}));
+jest.mock('@hooks/useResponsiveLayout', () => jest.fn());
 
 const SS_EMAIL = 'testing@gmail.com';
 const SS_ACCOUNT_ID = 1;
+
+const mockedUseResponsiveLayout = useResponsiveLayout as jest.MockedFunction<typeof useResponsiveLayout>;
 
 describe('FloatingReceiptButton hover', () => {
     const onPress = jest.fn();
@@ -79,7 +82,9 @@ describe('FloatingReceiptButton onPress', () => {
     });
 
     it('should call startMoneyRequest with CREATE iouType when there is no active policy', () => {
-        render(
+        mockedUseResponsiveLayout.mockReturnValue({...CONST.NAVIGATION_TESTS.DEFAULT_USE_RESPONSIVE_LAYOUT_VALUE, shouldUseNarrowLayout: false});
+
+        const {rerender} = render(
             <ComposeProviders components={[OnyxListItemProvider, LocaleContextProvider, SidebarOrderedReportsContextProvider]}>
                 <NavigationContainer
                     ref={navigationRef}
@@ -98,9 +103,33 @@ describe('FloatingReceiptButton onPress', () => {
         expect(startMoneyRequest).toHaveBeenCalled();
         const [[iouType]] = (startMoneyRequest as jest.MockedFunction<typeof startMoneyRequest>).mock.calls;
         expect(iouType).toEqual(CONST.IOU.TYPE.CREATE);
+
+        // render FloatingCameraButton component
+        mockedUseResponsiveLayout.mockReturnValue({...CONST.NAVIGATION_TESTS.DEFAULT_USE_RESPONSIVE_LAYOUT_VALUE, shouldUseNarrowLayout: true});
+        rerender(
+            <ComposeProviders components={[OnyxListItemProvider, LocaleContextProvider, SidebarOrderedReportsContextProvider]}>
+                <NavigationContainer
+                    ref={navigationRef}
+                    onReady={() => {}}
+                >
+                    <NavigationTabBar selectedTab={NAVIGATION_TABS.HOME} />
+                </NavigationContainer>
+            </ComposeProviders>,
+        );
+
+        const floatingCameraButton = screen.getByTestId('floating-camera-button');
+        expect(floatingCameraButton).toBeVisible();
+
+        fireEvent.press(floatingCameraButton);
+
+        expect(startMoneyRequest).toHaveBeenCalled();
+        const [, [iouType1]] = (startMoneyRequest as jest.MockedFunction<typeof startMoneyRequest>).mock.calls;
+        expect(iouType1).toEqual(CONST.IOU.TYPE.CREATE);
     });
 
     it('should call startMoneyRequest with SUBMIT iouType when there is an active policy', async () => {
+        mockedUseResponsiveLayout.mockReturnValue({...CONST.NAVIGATION_TESTS.DEFAULT_USE_RESPONSIVE_LAYOUT_VALUE, shouldUseNarrowLayout: false});
+
         const fakePolicy = {...createRandomPolicy(0, CONST.POLICY.TYPE.TEAM), isPolicyExpenseChatEnabled: true};
         const fakeReport = {...createPolicyExpenseChat(0), ownerAccountID: SS_ACCOUNT_ID};
         await Onyx.set(ONYXKEYS.SESSION, {email: SS_EMAIL, accountID: SS_ACCOUNT_ID});
@@ -110,7 +139,7 @@ describe('FloatingReceiptButton onPress', () => {
 
         await waitForBatchedUpdates();
 
-        render(
+        const {rerender} = render(
             <ComposeProviders components={[OnyxListItemProvider, LocaleContextProvider, SidebarOrderedReportsContextProvider]}>
                 <NavigationContainer
                     ref={navigationRef}
@@ -132,5 +161,28 @@ describe('FloatingReceiptButton onPress', () => {
         const [[iouType, reportID]] = (startMoneyRequest as jest.MockedFunction<typeof startMoneyRequest>).mock.calls;
         expect(iouType).toEqual(CONST.IOU.TYPE.SUBMIT);
         expect(reportID).toEqual(fakeReport.reportID);
+
+        // render FloatingCameraButton component
+        mockedUseResponsiveLayout.mockReturnValue({...CONST.NAVIGATION_TESTS.DEFAULT_USE_RESPONSIVE_LAYOUT_VALUE, shouldUseNarrowLayout: true});
+        rerender(
+            <ComposeProviders components={[OnyxListItemProvider, LocaleContextProvider, SidebarOrderedReportsContextProvider]}>
+                <NavigationContainer
+                    ref={navigationRef}
+                    onReady={() => {}}
+                >
+                    <NavigationTabBar selectedTab={NAVIGATION_TABS.HOME} />
+                </NavigationContainer>
+            </ComposeProviders>,
+        );
+
+        const floatingCameraButton = screen.getByTestId('floating-camera-button');
+        expect(floatingCameraButton).toBeVisible();
+
+        fireEvent.press(floatingCameraButton);
+
+        expect(startMoneyRequest).toHaveBeenCalled();
+        const [, [iouType1, reportID1]] = (startMoneyRequest as jest.MockedFunction<typeof startMoneyRequest>).mock.calls;
+        expect(iouType1).toEqual(CONST.IOU.TYPE.SUBMIT);
+        expect(reportID1).toEqual(fakeReport.reportID);
     });
 });
