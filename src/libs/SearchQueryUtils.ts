5191fd5575d01fef47e3cd9948403a6579e6bf6d
dmkt9 <demoskratos9x@gmail.com>
2025-12-04 20:21:55 +0700

    fix lint

--- src/libs/SearchQueryUtils.ts
+++ src/libs/SearchQueryUtils.ts
@@ -1000,6 +1000,7 @@ function getFilterDisplayValue(
         return getCardDescription(cardList?.[cardID]) || filterValue;
     }
     if (filterName === CONST.SEARCH.SYNTAX_FILTER_KEYS.IN) {
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         return getReportName(reports?.[`${ONYXKEYS.COLLECTION.REPORT}${filterValue}`]) || filterValue;
     }
     if (filterName === CONST.SEARCH.SYNTAX_FILTER_KEYS.AMOUNT || filterName === CONST.SEARCH.SYNTAX_FILTER_KEYS.TOTAL || filterName === CONST.SEARCH.SYNTAX_FILTER_KEYS.PURCHASE_AMOUNT) {
