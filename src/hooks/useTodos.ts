2fafb2a426eaae220cc866c35dcd3fe535a41400
Carlos Martins <cmartins@expensify.com>
2025-12-04 14:28:37 -0700

    update return response

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -103,7 +103,6 @@ export default function useTodos() {
             continue;
         }
     });
-    
 
-    return {};
+    return {reportsToSubmit, reportsToApprove, reportsToPay, reportsToExport};
 }
