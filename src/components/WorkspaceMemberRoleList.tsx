b184de4cafbefb14b0ec7e5e7e626f7749c4013e
mkzie2 <mkzie2@gmail.com>
2025-12-05 17:11:16 +0700

    refactor: dry workspace role list

---
+++ src/components/WorkspaceMemberRoleList.tsx
@@ -0,0 +1,90 @@
+import React, {useMemo} from 'react';
+import {View} from 'react-native';
+import {OnyxEntry} from 'react-native-onyx';
+import {ValueOf} from 'type-fest';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
+import Navigation from '@libs/Navigation/Navigation';
+import {isControlPolicy} from '@libs/PolicyUtils';
+import CONST from '@src/CONST';
+import {Route} from '@src/ROUTES';
+import {Policy} from '@src/types/onyx';
+import HeaderWithBackButton from './HeaderWithBackButton';
+import SelectionList from './SelectionList';
+import RadioListItem from './SelectionList/ListItem/RadioListItem';
+import {ListItem} from './SelectionList/types';
+
+type ListItemType = ListItem<ValueOf<typeof CONST.POLICY.ROLE>> & {
+    value: ValueOf<typeof CONST.POLICY.ROLE>;
+    text: string;
+    alternateText: string;
+    isSelected: boolean;
+};
+
+type WorkspaceMemberRoleListProps = {
+    role: string | undefined;
+    policy: OnyxEntry<Policy>;
+    navigateBackTo?: Route;
+    isLoading?: boolean;
+    onSelectRole?: (value: ListItemType) => void;
+};
+
+function WorkspaceMemberRoleList({role, policy, navigateBackTo = undefined, isLoading = false, onSelectRole = () => {}}: WorkspaceMemberRoleListProps) {
+    const {translate} = useLocalize();
+    const styles = useThemeStyles();
+
+    const roleItems: ListItemType[] = useMemo(() => {
+        const items: ListItemType[] = [
+            {
+                value: CONST.POLICY.ROLE.ADMIN,
+                text: translate('common.admin'),
+                alternateText: translate('workspace.common.adminAlternateText'),
+                isSelected: role === CONST.POLICY.ROLE.ADMIN,
+                keyForList: CONST.POLICY.ROLE.ADMIN,
+            },
+            {
+                value: CONST.POLICY.ROLE.AUDITOR,
+                text: translate('common.auditor'),
+                alternateText: translate('workspace.common.auditorAlternateText'),
+                isSelected: role === CONST.POLICY.ROLE.AUDITOR,
+                keyForList: CONST.POLICY.ROLE.AUDITOR,
+            },
+            {
+                value: CONST.POLICY.ROLE.USER,
+                text: translate('common.member'),
+                alternateText: translate('workspace.common.memberAlternateText'),
+                isSelected: role === CONST.POLICY.ROLE.USER,
+                keyForList: CONST.POLICY.ROLE.USER,
+            },
+        ];
+
+        if (!isControlPolicy(policy)) {
+            return items.filter((item) => item.value !== CONST.POLICY.ROLE.AUDITOR);
+        }
+        return items;
+    }, [role, translate, policy]);
+
+    return (
+        <>
+            <HeaderWithBackButton
+                title={translate('common.role')}
+                onBackButtonPress={() => Navigation.goBack(navigateBackTo)}
+            />
+            {!isLoading && (
+                <View style={[styles.containerWithSpaceBetween, styles.pointerEventsBoxNone]}>
+                    <SelectionList
+                        data={roleItems}
+                        ListItem={RadioListItem}
+                        onSelectRow={onSelectRole}
+                        shouldSingleExecuteRowSelect
+                        initiallyFocusedItemKey={roleItems.find((item) => item.isSelected)?.keyForList}
+                        addBottomSafeAreaPadding
+                    />
+                </View>
+            )}
+        </>
+    );
+}
+
+WorkspaceMemberRoleList.displayName = 'WorkspaceMemberRoleList';
+export default WorkspaceMemberRoleList;
--- src/pages/workspace/members/WorkspaceMemberDetailsRolePage.tsx
+++ src/pages/workspace/members/WorkspaceMemberDetailsRolePage.tsx
@@ -1,25 +1,19 @@
-import React, {useCallback, useMemo} from 'react';
-import {View} from 'react-native';
+import React, {useCallback} from 'react';
 import type {OnyxEntry} from 'react-native-onyx';
 import type {ValueOf} from 'type-fest';
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
-import SelectionList from '@components/SelectionList';
-import RadioListItem from '@components/SelectionList/ListItem/RadioListItem';
 import type {ListItem} from '@components/SelectionList/types';
-import useLocalize from '@hooks/useLocalize';
-import useThemeStyles from '@hooks/useThemeStyles';
+import WorkspaceMemberRoleList from '@components/WorkspaceMemberRoleList';
 import {updateWorkspaceMembersRole} from '@libs/actions/Policy/Member';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
-import {isControlPolicy} from '@libs/PolicyUtils';
+import AccessOrNotFoundWrapper from '@pages/workspace/AccessOrNotFoundWrapper';
 import withPolicyAndFullscreenLoading from '@pages/workspace/withPolicyAndFullscreenLoading';
 import type {WithPolicyAndFullscreenLoadingProps} from '@pages/workspace/withPolicyAndFullscreenLoading';
 import CONST from '@src/CONST';
 import type SCREENS from '@src/SCREENS';
 import type {PersonalDetailsList} from '@src/types/onyx';
-import AccessOrNotFoundWrapper from '../AccessOrNotFoundWrapper';
 
 type ListItemType = ListItem<ValueOf<typeof CONST.POLICY.ROLE>> & {
     value: ValueOf<typeof CONST.POLICY.ROLE>;
@@ -35,43 +29,10 @@ type WorkspaceMemberDetailsRolePageProps = Omit<WithPolicyAndFullscreenLoadingPr
     };
 
 function WorkspaceMemberDetailsRolePage({policy, personalDetails, route}: WorkspaceMemberDetailsRolePageProps) {
-    const {translate} = useLocalize();
-    const styles = useThemeStyles();
     const accountID = Number(route.params.accountID);
     const policyID = route.params.policyID;
     const memberLogin = personalDetails?.[accountID]?.login ?? '';
     const member = policy?.employeeList?.[memberLogin];
-
-    const roleItems: ListItemType[] = useMemo(() => {
-        const items: ListItemType[] = [
-            {
-                value: CONST.POLICY.ROLE.ADMIN,
-                text: translate('common.admin'),
-                alternateText: translate('workspace.common.adminAlternateText'),
-                isSelected: member?.role === CONST.POLICY.ROLE.ADMIN,
-                keyForList: CONST.POLICY.ROLE.ADMIN,
-            },
-            {
-                value: CONST.POLICY.ROLE.AUDITOR,
-                text: translate('common.auditor'),
-                alternateText: translate('workspace.common.auditorAlternateText'),
-                isSelected: member?.role === CONST.POLICY.ROLE.AUDITOR,
-                keyForList: CONST.POLICY.ROLE.AUDITOR,
-            },
-            {
-                value: CONST.POLICY.ROLE.USER,
-                text: translate('common.member'),
-                alternateText: translate('workspace.common.memberAlternateText'),
-                isSelected: member?.role === CONST.POLICY.ROLE.USER,
-                keyForList: CONST.POLICY.ROLE.USER,
-            },
-        ];
-
-        if (isControlPolicy(policy)) {
-            return items;
-        }
-        return member?.role === CONST.POLICY.ROLE.AUDITOR ? items : items.filter((item) => item.value !== CONST.POLICY.ROLE.AUDITOR);
-    }, [member?.role, translate, policy]);
 
     const changeRole = useCallback(
         ({value}: ListItemType) => {
@@ -94,17 +55,11 @@ function WorkspaceMemberDetailsRolePage({policy, personalDetails, route}: Worksp
                 includePaddingTop={false}
                 enableEdgeToEdgeBottomSafeAreaPadding
             >
-                <HeaderWithBackButton title={translate('common.role')} />
-                <View style={[styles.containerWithSpaceBetween, styles.pointerEventsBoxNone]}>
-                    <SelectionList
-                        data={roleItems}
-                        ListItem={RadioListItem}
-                        onSelectRow={changeRole}
-                        shouldSingleExecuteRowSelect
-                        initiallyFocusedItemKey={roleItems.find((item) => item.isSelected)?.keyForList}
-                        addBottomSafeAreaPadding
-                    />
-                </View>
+                <WorkspaceMemberRoleList
+                    role={member?.role}
+                    policy={policy}
+                    onSelectRole={changeRole}
+                />
             </ScreenWrapper>
         </AccessOrNotFoundWrapper>
     );
