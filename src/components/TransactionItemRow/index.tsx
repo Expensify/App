94887afd3a6e0bcda8ddab89c52f1f3c178a5c80
krishna2323 <belivethatkg@gmail.com>
2025-12-06 01:30:29 +0530

    fix warning.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/components/TransactionItemRow/index.tsx
+++ src/components/TransactionItemRow/index.tsx
@@ -4,7 +4,6 @@ import type {StyleProp, ViewStyle} from 'react-native';
 import type {ValueOf} from 'type-fest';
 import Checkbox from '@components/Checkbox';
 import Icon from '@components/Icon';
-import * as Expensicons from '@components/Icon/Expensicons';
 import type {TransactionWithOptionalHighlight} from '@components/MoneyRequestReportView/MoneyRequestReportTransactionList';
 import {PressableWithFeedback} from '@components/Pressable';
 import RadioButton from '@components/RadioButton';
@@ -13,6 +12,7 @@ import ActionCell from '@components/SelectionListWithSections/Search/ActionCell'
 import DateCell from '@components/SelectionListWithSections/Search/DateCell';
 import UserInfoCell from '@components/SelectionListWithSections/Search/UserInfoCell';
 import Text from '@components/Text';
+import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useStyleUtils from '@hooks/useStyleUtils';
@@ -162,6 +162,7 @@ function TransactionItemRow({
     const {isLargeScreenWidth} = useResponsiveLayout();
     const hasCategoryOrTag = !isCategoryMissing(transactionItem?.category) || !!transactionItem.tag;
     const createdAt = getTransactionCreated(transactionItem);
+    const expensicons = useMemoizedLazyExpensifyIcons(['ArrowRight']);
 
     const isDateColumnWide = dateColumnSize === CONST.SEARCH.TABLE_COLUMN_SIZES.WIDE;
     const isAmountColumnWide = amountColumnSize === CONST.SEARCH.TABLE_COLUMN_SIZES.WIDE;
@@ -472,7 +473,7 @@ function TransactionItemRow({
                         {!!shouldShowArrowRightOnNarrowLayout && (
                             <View style={[styles.justifyContentEnd, styles.alignItemsEnd, styles.mbHalf, styles.ml1]}>
                                 <Icon
-                                    src={Expensicons.ArrowRight}
+                                    src={expensicons.ArrowRight}
                                     fill={theme.icon}
                                     additionalStyles={styles.opacitySemiTransparent}
                                     small
@@ -577,7 +578,7 @@ function TransactionItemRow({
                             {({hovered}) => {
                                 return (
                                     <Icon
-                                        src={Expensicons.ArrowRight}
+                                        src={expensicons.ArrowRight}
                                         fill={theme.icon}
                                         additionalStyles={!hovered && styles.opacitySemiTransparent}
                                         small
