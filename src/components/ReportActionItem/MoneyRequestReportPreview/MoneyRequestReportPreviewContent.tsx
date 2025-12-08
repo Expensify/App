8f636e7af948f5646dced5a187ce7bcaed952424
thelullabyy <the.lullaby2@hotmail.com>
2025-12-08 19:32:42 +0800

    fix: revert the revert of 74676

--- src/components/ReportActionItem/MoneyRequestReportPreview/MoneyRequestReportPreviewContent.tsx
+++ src/components/ReportActionItem/MoneyRequestReportPreview/MoneyRequestReportPreviewContent.tsx
@@ -703,7 +703,7 @@ function MoneyRequestReportPreviewContent({
                                                                     <Text style={[styles.reportStatusText, {color: reportStatusColorStyle?.textColor}]}>{reportStatus}</Text>
                                                                 </View>
                                                             )}
-                                                            <Text style={[styles.textLabelSupporting, styles.lh16]}>{expenseCount}</Text>
+                                                            <Text style={[styles.textLabelSupporting]}>{expenseCount}</Text>
                                                         </View>
                                                     )
                                                 )}
