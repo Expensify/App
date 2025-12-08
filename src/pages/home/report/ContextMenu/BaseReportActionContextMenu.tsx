65ca1c659293c8a2519b38aa68405ddcc5357405
VH <hoangzinhvn@gmail.com>
2025-12-08 22:39:19 +0700

    Run prettier

--- src/pages/home/report/ContextMenu/BaseReportActionContextMenu.tsx
+++ src/pages/home/report/ContextMenu/BaseReportActionContextMenu.tsx
@@ -161,7 +161,9 @@ function BaseReportActionContextMenu({
     const [isDebugModeEnabled] = useOnyx(ONYXKEYS.IS_DEBUG_MODE_ENABLED, {canBeMissing: true});
     const [report] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`, {canBeMissing: true});
     const [reportNameValuePairs] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${reportID}`, {canBeMissing: true});
-    const [harvestReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${getNonEmptyStringOnyxID(getHarvestOriginalReportID(reportNameValuePairs?.origin, reportNameValuePairs?.originalID))}`, {canBeMissing: true});
+    const [harvestReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${getNonEmptyStringOnyxID(getHarvestOriginalReportID(reportNameValuePairs?.origin, reportNameValuePairs?.originalID))}`, {
+        canBeMissing: true,
+    });
     const [originalReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${originalReportID}`, {canBeMissing: true});
     const isOriginalReportArchived = useReportIsArchived(originalReportID);
     const policyID = report?.policyID;
@@ -228,7 +230,10 @@ function BaseReportActionContextMenu({
         !isArchivedNonExpenseReport(transactionThreadReportID ? childReport : parentReport, transactionThreadReportID ? isChildReportArchived : isParentReportArchived);
 
     const shouldEnableArrowNavigation = !isMini && (isVisible || shouldKeepOpen);
-    const isHarvestReport = useMemo(() => isHarvestCreatedExpenseReport(reportNameValuePairs?.origin, reportNameValuePairs?.originalID), [reportNameValuePairs?.origin, reportNameValuePairs?.originalID]);
+    const isHarvestReport = useMemo(
+        () => isHarvestCreatedExpenseReport(reportNameValuePairs?.origin, reportNameValuePairs?.originalID),
+        [reportNameValuePairs?.origin, reportNameValuePairs?.originalID],
+    );
     let filteredContextMenuActions = ContextMenuActions.filter(
         (contextAction) =>
             !disabledActions.includes(contextAction) &&
