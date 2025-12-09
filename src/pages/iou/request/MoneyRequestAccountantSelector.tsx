378cec09f1a321222ea465fc4028b14476c58469
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-12-09 18:08:53 +0530

    Refactor IsCurrentUser functions

--- src/pages/iou/request/MoneyRequestAccountantSelector.tsx
+++ src/pages/iou/request/MoneyRequestAccountantSelector.tsx
@@ -64,6 +64,7 @@ function MoneyRequestAccountantSelector({onFinish, onAccountantSelected, iouType
     const [reportAttributesDerived] = useOnyx(ONYXKEYS.DERIVED.REPORT_ATTRIBUTES, {canBeMissing: true, selector: reportsSelector});
     const [draftComments] = useOnyx(ONYXKEYS.COLLECTION.REPORT_DRAFT_COMMENT, {canBeMissing: true});
     const [nvpDismissedProductTraining] = useOnyx(ONYXKEYS.NVP_DISMISSED_PRODUCT_TRAINING, {canBeMissing: true});
+    const [loginList] = useOnyx(ONYXKEYS.LOGIN_LIST, {canBeMissing: true});
 
     useEffect(() => {
         searchInServer(debouncedSearchTerm.trim());
@@ -81,6 +82,7 @@ function MoneyRequestAccountantSelector({onFinish, onAccountantSelected, iouType
             },
             draftComments,
             nvpDismissedProductTraining,
+            loginList,
             {
                 betas,
                 excludeLogins: CONST.EXPENSIFY_EMAILS_OBJECT,
@@ -95,7 +97,7 @@ function MoneyRequestAccountantSelector({onFinish, onAccountantSelected, iouType
             ...optionList,
             ...orderedOptions,
         };
-    }, [action, areOptionsInitialized, betas, didScreenTransitionEnd, draftComments, nvpDismissedProductTraining, options.personalDetails, options.reports, countryCode]);
+    }, [areOptionsInitialized, didScreenTransitionEnd, options.reports, options.personalDetails, draftComments, nvpDismissedProductTraining, loginList, betas, action, countryCode]);
 
     const chatOptions = useMemo(() => {
         if (!areOptionsInitialized) {
@@ -107,12 +109,12 @@ function MoneyRequestAccountantSelector({onFinish, onAccountantSelected, iouType
                 headerMessage: '',
             };
         }
-        const newOptions = filterAndOrderOptions(defaultOptions, debouncedSearchTerm, countryCode, {
+        const newOptions = filterAndOrderOptions(defaultOptions, debouncedSearchTerm, countryCode, loginList, {
             excludeLogins: CONST.EXPENSIFY_EMAILS_OBJECT,
             maxRecentReportsToShow: CONST.IOU.MAX_RECENT_REPORTS_TO_SHOW,
         });
         return newOptions;
-    }, [areOptionsInitialized, defaultOptions, debouncedSearchTerm, countryCode]);
+    }, [areOptionsInitialized, defaultOptions, debouncedSearchTerm, countryCode, loginList]);
 
     /**
      * Returns the sections needed for the OptionsSelector
@@ -152,7 +154,10 @@ function MoneyRequestAccountantSelector({onFinish, onAccountantSelected, iouType
 
         if (
             chatOptions.userToInvite &&
-            !isCurrentUser({...chatOptions.userToInvite, accountID: chatOptions.userToInvite?.accountID ?? CONST.DEFAULT_NUMBER_ID, status: chatOptions.userToInvite?.status ?? undefined})
+            !isCurrentUser(
+                {...chatOptions.userToInvite, accountID: chatOptions.userToInvite?.accountID ?? CONST.DEFAULT_NUMBER_ID, status: chatOptions.userToInvite?.status ?? undefined},
+                loginList,
+            )
         ) {
             newSections.push({
                 title: undefined,
@@ -183,6 +188,7 @@ function MoneyRequestAccountantSelector({onFinish, onAccountantSelected, iouType
         personalDetails,
         reportAttributesDerived,
         translate,
+        loginList,
         countryCode,
     ]);
 
