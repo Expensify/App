7a3bdfcc8c7f77146502ebd114a71f7a72cdcde9
cretadn22 <cretadn22@gmail.com>
2025-12-10 00:49:25 +0700

    fix the comment

--- src/pages/iou/request/step/IOURequestStepConfirmation.tsx
+++ src/pages/iou/request/step/IOURequestStepConfirmation.tsx
@@ -350,8 +350,8 @@ function IOURequestStepConfirmation({
 
         startMoneyRequest(
             iouType ?? CONST.IOU.TYPE.CREATE,
-            // When starting to create an expense from the global FAB, there is not an existing report yet.
-            // A random optimistic reportID is generated and used for all routes in the creation flow.
+            // When starting to create an expense from the global FAB, If there is not an existing report yet, a random optimistic reportID is generated and used
+            // for all of the routes in the creation flow.
             reportID ?? generateReportID(),
         );
         // eslint-disable-next-line react-compiler/react-compiler, react-hooks/exhaustive-deps -- we don't want this effect to run again
