641112bed27548aee05d890113673aab5b19d27e
Rajat Parashar <parasharrajat@users.noreply.github.com>
2025-12-09 18:41:27 +0530

    Fix issues

--- src/libs/ContactUtils.ts
+++ src/libs/ContactUtils.ts
@@ -1,4 +1,4 @@
-import {OnyxEntry} from 'react-native-onyx';
+import type {OnyxEntry} from 'react-native-onyx';
 import type {LocaleContextProps} from '@components/LocaleContextProvider';
 import CONST from '@src/CONST';
 import type {Login, PersonalDetails} from '@src/types/onyx';
