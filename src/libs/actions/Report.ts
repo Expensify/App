b2195b66f088af26ac32735e35a62243ebacda76
paulnjs <paulqn45@outlook.com>
2025-12-10 00:14:44 +0700

    fix: Header does not update after changing to workspace with rules not enabled

--- src/libs/actions/Report.ts
+++ src/libs/actions/Report.ts
@@ -5735,7 +5735,7 @@ function buildOptimisticChangePolicyData(
         });
     }
 
-    if (newStatusNum) {
+    if (newStatusNum != null && newStatusNum !== undefined) {
         // buildOptimisticNextStep is used in parallel
         // eslint-disable-next-line @typescript-eslint/no-deprecated
         const optimisticNextStepDeprecated = buildNextStepNew({
