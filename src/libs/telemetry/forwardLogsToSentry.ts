ec7789a96e4b7c14f06d64df813fb77137a4b3e1
Hubert Sosinski <hubert.sosinski@callstack.com>
2025-12-09 12:06:12 +0100

    Add comment

--- src/libs/telemetry/forwardLogsToSentry.ts
+++ src/libs/telemetry/forwardLogsToSentry.ts
@@ -7,7 +7,7 @@ type SentryLogLevel = 'debug' | 'info' | 'warn' | 'error';
  *
  * ATTENTION!
  * Currently, this always returns false because we want to deliberately decide what is being forwarded.
- * There is no redaction / filtering of sensitive data implemented yet. When you'll implement any log forwarding logic, make sure that you do not leak any sensitive data.
+ * There is no redaction / filtering of sensitive data implemented yet. When you implement any log forwarding logic, make sure that you do not leak any sensitive data.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function shouldForwardLog(log: {message?: string; parameters?: Record<string, unknown> | undefined}) {
