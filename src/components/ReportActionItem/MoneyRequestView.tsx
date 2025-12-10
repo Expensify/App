966d72bf0c9cab8b08003c71924930364a3533e3
Agata Kosior <agata.kosior57@gmail.com>
2025-12-10 09:52:43 +0100

    fix: prettier

--- src/components/ReportActionItem/MoneyRequestView.tsx
+++ src/components/ReportActionItem/MoneyRequestView.tsx
@@ -45,8 +45,7 @@ import type {TransactionDetails} from '@libs/ReportUtils';
 import {
     canEditFieldOfMoneyRequest,
     canEditMoneyRequest,
-    canUserPerformWriteAction as canUserPerformWriteActionReportUtils,
-    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    canUserPerformWriteAction as canUserPerformWriteActionReportUtils, // eslint-disable-next-line @typescript-eslint/no-deprecated
     getReportName,
     getTransactionDetails,
     getTripIDFromTransactionParentReportID,
