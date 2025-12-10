fbbe6e1e3e690aaa2747713c73ad8bb58a489b02
Kacper Mikołajczak <kacpermikolajczak10@gmail.com>
2025-12-10 23:51:30 +0100

    suppress deprecation

--- src/components/MoneyRequestConfirmationListFooter.tsx
+++ src/components/MoneyRequestConfirmationListFooter.tsx
@@ -346,6 +346,7 @@ function MoneyRequestConfirmationListFooter({
     }, [allReports, shouldUseTransactionReport, transaction?.reportID, outstandingReportID]);
 
     const reportName = useMemo(() => {
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         const name = getReportName(selectedReport, selectedPolicy);
         if (!name) {
             return isUnreported ? translate('common.none') : translate('iou.newReport');
