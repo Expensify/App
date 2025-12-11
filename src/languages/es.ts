21f3e314a1b2df24982c60e183a33d8a82d42cba
nkdengineer <nkdengineer@outlook.com>
2025-12-12 00:29:07 +0700

    fix lint

--- src/languages/es.ts
+++ src/languages/es.ts
@@ -5947,7 +5947,7 @@ ${amount} para ${merchant} - ${date}`,
             return `cambió el índice de la tarifa de ${customUnitName} "${customUnitRateName}" a "${newValue}" ${oldValue ? `(previamente "${oldValue}")` : ''}`;
         },
         updatedCustomUnitRateEnabled: ({customUnitName, customUnitRateName, newValue}) => {
-            return `${newValue ? "habilitó" : "deshabilitó"} la tarifa de ${customUnitName} "${customUnitRateName}"`;
+            return `${newValue ? 'habilitó' : 'deshabilitó'} la tarifa de ${customUnitName} "${customUnitRateName}"`;
         },
         deleteCustomUnitRate: ({customUnitName, rateName}) => `eliminó la tasa "${rateName}" de "${customUnitName}"`,
         addedReportField: ({fieldType, fieldName}) => `añadió el campo de informe ${fieldType} "${fieldName}"`,
