ba22adb208ee491eaff710141fd53c578ee0d462
eliran goshen <elirangoshen@gmail.com>
2025-12-11 10:06:28 +0100

    error test

--- src/setup/telemetry/index.ts
+++ src/setup/telemetry/index.ts
@@ -14,6 +14,8 @@ export default function (): void {
         return;
     }
 
+    badcode();
+    this.anothrtBadCode();
     const integrations = [navigationIntegration, tracingIntegration, browserProfilingIntegration, browserTracingIntegration].filter((integration) => !!integration);
 
     Sentry.init({
