0a42a2971ff982c59b68e8f6f09097f1a59f3c72
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-12-15 22:28:09 +0100

    fix ts errors

--- src/components/MoneyReportHeaderStatusBar.tsx
+++ src/components/MoneyReportHeaderStatusBar.tsx
@@ -24,12 +24,13 @@ type IconMap = Record<IconName, IconAsset>;
 function MoneyReportHeaderStatusBar({nextStep}: MoneyReportHeaderStatusBarProps) {
     const styles = useThemeStyles();
     const theme = useTheme();
-    const icons = useMemoizedLazyExpensifyIcons(['Hourglass', 'Checkmark', 'Stopwatch'] as const);
+    const icons = useMemoizedLazyExpensifyIcons(['Hourglass', 'Checkmark', 'Stopwatch', 'DotIndicator'] as const);
     const iconMap: IconMap = useMemo(
         () => ({
             [CONST.NEXT_STEP.ICONS.HOURGLASS]: icons.Hourglass,
             [CONST.NEXT_STEP.ICONS.CHECKMARK]: icons.Checkmark,
             [CONST.NEXT_STEP.ICONS.STOPWATCH]: icons.Stopwatch,
+            [CONST.NEXT_STEP.ICONS.DOT_INDICATOR]: icons.DotIndicator,
         }),
         [icons],
     );
