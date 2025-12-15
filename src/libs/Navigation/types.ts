64c2fda2fda76c78cd34ce5ab8df49eb7115bf28
Francois Laithier <francois@expensify.com>
2025-12-15 15:00:47 -0800

    Merge pull request #77691 from Expensify/revert-71659
    Revert "Merge pull request #71659 from callstack-internal/feat/65912-reapply-followup"
    
    (cherry picked from commit 6bf6a8ea06acbb70a67d7d727ddcc1409ad29ad4)
    
    (cherry-picked to staging by AndrewGable)

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -1977,12 +1977,6 @@ type ReimbursementAccountEnterSignerInfoNavigatorParamList = {
     };
 };
 
-type ConnectExistingBankAccountNavigatorParamList = {
-    [SCREENS.CONNECT_EXISTING_BUSINESS_BANK_ACCOUNT_ROOT]: {
-        policyID: string;
-    };
-};
-
 type WalletStatementNavigatorParamList = {
     [SCREENS.WALLET_STATEMENT_ROOT]: {
         /** The statement year and month as one string, i.e. 202110 */
@@ -2331,6 +2325,9 @@ type WorkspaceSplitNavigatorParamList = {
         policyID: string;
     };
     [SCREENS.WORKSPACE.WORKFLOWS_AUTO_REPORTING_MONTHLY_OFFSET]: {
+        policyID: string;
+    };
+    [SCREENS.WORKSPACE.WORKFLOWS_CONNECT_EXISTING_BANK_ACCOUNT]: {
         policyID: string;
     };
     [SCREENS.WORKSPACE.INVOICES]: {
@@ -2858,7 +2855,6 @@ export type {
     ReportVerifyAccountNavigatorParamList,
     ReimbursementAccountNavigatorParamList,
     ReimbursementAccountEnterSignerInfoNavigatorParamList,
-    ConnectExistingBankAccountNavigatorParamList,
     NewReportWorkspaceSelectionNavigatorParamList,
     ReportDescriptionNavigatorParamList,
     ReportDetailsNavigatorParamList,
