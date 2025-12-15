819ee6a6513323e2eb028ecfa59243d430f8c5ca
Vit Horacek <36083550+mountiny@users.noreply.github.com>
2025-12-15 21:11:36 +0100

    Revert "Refactor: Deprecate getPolicy (part 2)"

--- src/libs/actions/Policy/Tag.ts
+++ src/libs/actions/Policy/Tag.ts
@@ -1164,11 +1164,10 @@ function setPolicyTagGLCode({policyID, tagName, tagListIndex, glCode, policyTags
     API.write(WRITE_COMMANDS.UPDATE_POLICY_TAG_GL_CODE, parameters, onyxData);
 }
 
-function setPolicyTagApprover(policy: OnyxEntry<Policy>, tag: string, approver: string) {
-    if (!policy?.id) {
-        return;
-    }
-    const policyID = policy.id;
+function setPolicyTagApprover(policyID: string, tag: string, approver: string) {
+    // This will be fixed as part of https://github.com/Expensify/Expensify/issues/507850
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    const policy = PolicyUtils.getPolicy(policyID);
     const prevApprovalRules = policy?.rules?.approvalRules ?? [];
     const approverRuleToUpdate = PolicyUtils.getTagApproverRule(policyID, tag);
     const filteredApprovalRules = approverRuleToUpdate ? prevApprovalRules.filter((rule) => rule.id !== approverRuleToUpdate.id) : prevApprovalRules;
