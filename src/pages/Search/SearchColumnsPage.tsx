a39ee548db2d9a1e6d0da2af77c15bbf9277b79e
Jack Senyitko <jsenyitko@expensify.com>
2025-12-15 09:18:48 -0500

    create columns page

---
+++ src/pages/Search/SearchColumnsPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import ScreenWrapper from '@components/ScreenWrapper';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
+
+function SearchColumnsPage() {
+    const styles = useThemeStyles();
+    const {translate} = useLocalize();
+
+    return (
+        <ScreenWrapper
+            testID={SearchColumnsPage.displayName}
+            shouldShowOfflineIndicatorInWideScreen
+            offlineIndicatorStyle={styles.mtAuto}
+            includeSafeAreaPaddingBottom
+        >
+            <HeaderWithBackButton title={translate('search.columns')} />
+        </ScreenWrapper>
+    );
+}
+
+SearchColumnsPage.displayName = 'SearchColumnsPage';
+
+export default SearchColumnsPage;
