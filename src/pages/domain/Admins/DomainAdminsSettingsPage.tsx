28497e4065444dac21ae781a29898ed2d532c588
war-in <war-in@users.noreply.github.com>
2025-12-15 18:07:34 +0100

    chore: post-merge fixes

--- src/pages/domain/Admins/DomainAdminsSettingsPage.tsx
+++ src/pages/domain/Admins/DomainAdminsSettingsPage.tsx
@@ -1,6 +1,5 @@
 import {Str} from 'expensify-common';
 import React from 'react';
-import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
 import OfflineWithFeedback from '@components/OfflineWithFeedback';
@@ -8,20 +7,16 @@ import ScreenWrapper from '@components/ScreenWrapper';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {selectAdminIDs} from '@libs/DomainUtils';
 import {getLatestError} from '@libs/ErrorUtils';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@navigation/types';
 import DomainNotFoundPageWrapper from '@pages/domain/DomainNotFoundPageWrapper';
-import {clearChoosePrimaryContactError} from '@userActions/Domain';
 import ToggleSettingOptionRow from '@pages/workspace/workflows/ToggleSettingsOptionRow';
 import {clearChoosePrimaryContactError, clearToggleConsolidatedDomainBillingErrors, toggleConsolidatedDomainBilling} from '@userActions/Domain';
-import {getCurrentUserAccountID} from '@userActions/Report';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
-import isLoadingOnyxValue from '@src/types/utils/isLoadingOnyxValue';
 
 type DomainAdminsSettingsPageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.DOMAIN.ADMINS_SETTINGS>;
 
@@ -40,15 +35,8 @@ function DomainAdminsSettingsPage({route}: DomainAdminsSettingsPageProps) {
     const [domainSettings] = useOnyx(`${ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER}${domainAccountID}`, {
         canBeMissing: false,
     });
-    const [domain, domainMetadata] = useOnyx(`${ONYXKEYS.COLLECTION.DOMAIN}${domainAccountID}`, {canBeMissing: true});
+    const [domain] = useOnyx(`${ONYXKEYS.COLLECTION.DOMAIN}${domainAccountID}`, {canBeMissing: true});
     const currentlySelectedUser = domainSettings?.settings?.technicalContactEmail;
-
-    const [adminIDs] = useOnyx(`${ONYXKEYS.COLLECTION.DOMAIN}${domainAccountID}`, {
-        canBeMissing: true,
-        selector: selectAdminIDs,
-    });
-    const currentUserAccountID = getCurrentUserAccountID();
-    const isAdmin = adminIDs?.includes(currentUserAccountID) ?? false;
 
     return (
         <DomainNotFoundPageWrapper domainAccountID={domainAccountID}>
@@ -82,13 +70,18 @@ function DomainAdminsSettingsPage({route}: DomainAdminsSettingsPageProps) {
                     isActive={!!domainSettings?.settings?.technicalContactEmail && !!domainSettings?.settings?.useTechnicalContactBillingCard}
                     disabled={!domainSettings?.settings?.technicalContactEmail}
                     showLockIcon={!domainSettings?.settings?.technicalContactEmail}
-                    onToggle={(value) => toggleConsolidatedDomainBilling(domainAccountID, Str.extractEmailDomain(domain?.email ?? ''), value)}
+                    onToggle={(value) => {
+                        if (!domain?.email) {
+                            return;
+                        }
+                        toggleConsolidatedDomainBilling(domainAccountID, Str.extractEmailDomain(domain.email), value);
+                    }}
                     subtitle={translate('domain.admins.consolidatedDomainBillingDescription')}
                     title={translate('domain.admins.consolidatedDomainBilling')}
                     shouldPlaceSubtitleBelowSwitch
                     pendingAction={domainPendingActions?.useTechnicalContactBillingCard}
                     errors={getLatestError(domainErrors?.useTechnicalContactBillingCardErrors)}
-                    onCloseError={() => clearToggleConsolidatedDomainBillingErrors(route.params.accountID)}
+                    onCloseError={() => clearToggleConsolidatedDomainBillingErrors(domainAccountID)}
                 />
             </ScreenWrapper>
         </DomainNotFoundPageWrapper>
