be1c751179021a114d92f40436e022a7b367f119
Jan Musiał <jan.musial@swmansion.com>
2025-12-15 13:18:03 +0100

    make sure search router address cannot be navigated to manually

--- src/components/Search/SearchRouter/SearchRouterPage.tsx
+++ src/components/Search/SearchRouter/SearchRouterPage.tsx
@@ -1,8 +1,11 @@
+import Navigation from '@libs/Navigation/Navigation';
+import ROUTES from '@src/ROUTES';
+
 /**
  * On native devices SearchRouter is served from SearchRouterPage, on web from SearchRouterModal.
  */
 function SearchRouterPage() {
-    return null;
+    return Navigation.navigate(ROUTES.HOME);
 }
 
 SearchRouterPage.displayName = 'SearchRouterPage';
