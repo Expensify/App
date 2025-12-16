13d875eeff3708d9a9a2532199873369ce7d16ef
Blazej Kustra <blazej.kustra@swmansion.com>
2025-12-16 10:36:03 +0100

    Refactor Concierge component to use a constant for report key and remove unnecessary useMemo hook

--- src/components/SidePanel/Concierge/index.tsx
+++ src/components/SidePanel/Concierge/index.tsx
@@ -1,5 +1,5 @@
 import {NavigationRouteContext} from '@react-navigation/native';
-import React, {useMemo} from 'react';
+import React from 'react';
 import useOnyx from '@hooks/useOnyx';
 import type {ExtraContentProps, PlatformStackNavigationProp} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {ReportsSplitNavigatorParamList} from '@libs/Navigation/types';
@@ -7,9 +7,12 @@ import ReportScreen from '@pages/home/ReportScreen';
 import ONYXKEYS from '@src/ONYXKEYS';
 import SCREENS from '@src/SCREENS';
 
+const CONCIERGE_REPORT_KEY = 'Report-Concierge-Key';
+
 function Concierge({navigation}: Pick<ExtraContentProps, 'navigation'>) {
     const [conciergeReportID] = useOnyx(ONYXKEYS.CONCIERGE_REPORT_ID, {canBeMissing: true});
-    const route = useMemo(() => !!conciergeReportID && ({name: SCREENS.REPORT, params: {reportID: conciergeReportID}, key: 'Report-Concierge-Key'} as const), [conciergeReportID]);
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    const route = !!conciergeReportID && ({name: SCREENS.REPORT, params: {reportID: conciergeReportID}, key: CONCIERGE_REPORT_KEY} as const);
 
     if (!route) {
         return null;
