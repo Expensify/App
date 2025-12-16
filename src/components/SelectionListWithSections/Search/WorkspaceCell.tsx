475571d8d2a512eb9ce5d89f0b58a6ec06fc1be1
Jack Senyitko <jsenyitko@expensify.com>
2025-12-16 17:32:28 -0500

    update expense policy avatar

--- src/components/SelectionListWithSections/Search/WorkspaceCell.tsx
+++ src/components/SelectionListWithSections/Search/WorkspaceCell.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import {View} from 'react-native';
 import Avatar from '@components/Avatar';
 import TextWithTooltip from '@components/TextWithTooltip';
+import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
 
 type WorkspaceCellProps = {
     policyID?: string;
@@ -13,19 +15,25 @@ type WorkspaceCellProps = {
 
 function WorkspaceCell({policyName, avatarURL, policyID}: WorkspaceCellProps) {
     const styles = useThemeStyles();
+    const [policy] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${policyID}`, {canBeMissing: true});
+
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+    const name = policyName || policy?.name;
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+    const avatar = avatarURL || policy?.avatarURL;
 
     return (
         <View style={[styles.flexRow, styles.gap3, styles.flex1, styles.alignItemsCenter]}>
             <Avatar
                 imageStyles={[styles.alignSelfCenter]}
                 size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
-                source={avatarURL}
+                source={avatar}
                 avatarID={policyID}
-                name={policyName}
+                name={name ?? ''}
                 type={CONST.ICON_TYPE_WORKSPACE}
             />
             <TextWithTooltip
-                text={policyName ?? ''}
+                text={name ?? ''}
                 shouldShowTooltip
                 style={[styles.flex1, styles.flexGrow1]}
             />
