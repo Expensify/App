c64389f28e729442f41209b7c8a8d43968540716
Abdelrahman Khattab <abzokhattab@gmail.com>
2025-12-16 02:21:47 +0100

    fixing eslint

--- src/libs/actions/IOU.ts
+++ src/libs/actions/IOU.ts
@@ -91,7 +91,6 @@ import {
     getSubmitToAccountID,
     hasDependentTags,
     hasDynamicExternalWorkflow,
-    hasOnlyPersonalPolicies,
     isControlPolicy,
     isDelayedSubmissionEnabled,
     isPaidGroupPolicy,
