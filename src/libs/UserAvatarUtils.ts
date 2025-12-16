52779574131cf901b5cb987afcf6f4a665352748
Nodebrute <anusharukhsar1@gmail.com>
2025-12-16 05:26:16 +0500

    fix different avatar in offline mode

--- src/libs/UserAvatarUtils.ts
+++ src/libs/UserAvatarUtils.ts
@@ -298,6 +298,7 @@ function getSmallSizeAvatar(args: GetAvatarArgsType): AvatarSource | undefined {
 export {
     getAvatar,
     getAvatarURL,
+    getDefaultAvatar,
     getDefaultAvatarName,
     getDefaultAvatarURL,
     getPresetAvatarNameFromURL,
