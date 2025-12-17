9c2eeb4198bfd392bb903d93d3456d352931e816
Christoph Pader <chris@margelo.com>
2025-12-17 10:04:54 -0500

    fix: update log for manual memo errors

--- scripts/react-compiler-compliance-check.ts
+++ scripts/react-compiler-compliance-check.ts
@@ -650,7 +650,7 @@ class ResultsPrinter {
         }
 
         log();
-        logError(`The following newly added components should be auto memoized by the React Compiler (manual memoization is not allowed):`);
+        logError(`The following components should be auto memoized by the React Compiler and therefore manual memoization should be removed:`);
 
         for (const [filePath, manualMemoizationMatches] of manualMemoFailures) {
             log();
