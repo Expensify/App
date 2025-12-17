9624b4818782f31cbf38432f69d1b979df890ee1
truph01 <truphd01@gmail.com>
2025-12-17 09:59:31 +0700

    fix: Infinite loading when delete message in reply

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -722,6 +722,10 @@ function ReportScreen({route, navigation}: ReportScreenProps) {
             // Early return if the report we're passing isn't in a focused state. We only want to navigate to Concierge if the user leaves the room from another device or gets removed from the room while the report is in a focused state.
             // Prevent auto navigation for report in RHP
             if ((!isFocused && !isReportDetailOpenInRHP) || isInNarrowPaneModal) {
+                if (isInNarrowPaneModal) {
+                    Navigation.goBack(backTo as Route);
+                    return;
+                }
                 return;
             }
             Navigation.dismissModal();
