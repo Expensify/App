94664169b3ece8749c89139e3fac207bf304a855
Jack Senyitko <jsenyitko@expensify.com>
2025-12-17 17:33:06 -0500

    add logic for the icons

--- src/components/SelectionListWithSections/Search/ExportedIconCell.tsx
+++ src/components/SelectionListWithSections/Search/ExportedIconCell.tsx
@@ -6,6 +6,7 @@ import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useOnyx from '@hooks/useOnyx';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {getOriginalMessage, isExportedToIntegrationAction} from '@libs/ReportActionsUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 
@@ -19,7 +20,15 @@ function ExportedIconCell({reportID}: ExportedIconCellProps) {
     const reportActions = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`, {canBeMissing: true});
     const icons = useMemoizedLazyExpensifyIcons(['Document', 'NetSuiteSquare', 'XeroSquare', 'IntacctSquare', 'QBOSquare']);
 
-    console.log(reportActions);
+    const actions = Object.values(reportActions[0] ?? {});
+    const actionNames = actions.map((action) => action.actionName);
+
+    const isExportedToCsv = actionNames.includes(CONST.REPORT.ACTIONS.TYPE.EXPORTED_TO_CSV);
+    const isExportedToXero = actions.find((action) => isExportedToIntegrationAction(action) && getOriginalMessage(action)?.label === CONST.EXPORT_LABELS.XERO);
+    const isExportedToIntacct = actions.find((action) => isExportedToIntegrationAction(action) && getOriginalMessage(action)?.label === CONST.EXPORT_LABELS.INTACCT);
+    const isExportedToNetsuite = actions.find((action) => isExportedToIntegrationAction(action) && getOriginalMessage(action)?.label === CONST.EXPORT_LABELS.NETSUITE);
+    const isExportedToQuickbooksOnline = actions.find((action) => isExportedToIntegrationAction(action) && getOriginalMessage(action)?.label === CONST.EXPORT_LABELS.QBO);
+    const isExportedToQuickbooksDesktop = actions.find((action) => isExportedToIntegrationAction(action) && getOriginalMessage(action)?.label === CONST.EXPORT_LABELS.QBD);
 
     if (!reportID) {
         return null;
@@ -27,31 +36,41 @@ function ExportedIconCell({reportID}: ExportedIconCellProps) {
 
     return (
         <View style={[styles.flexRow, styles.gap2]}>
-            <Icon
-                src={icons.Document}
-                fill={theme.icon}
-                small
-            />
-            <Avatar
-                source={icons.NetSuiteSquare}
-                type={CONST.ICON_TYPE_AVATAR}
-                size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
-            />
-            <Avatar
-                source={icons.XeroSquare}
-                type={CONST.ICON_TYPE_AVATAR}
-                size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
-            />
-            <Avatar
-                source={icons.IntacctSquare}
-                type={CONST.ICON_TYPE_AVATAR}
-                size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
-            />
-            <Avatar
-                source={icons.QBOSquare}
-                type={CONST.ICON_TYPE_AVATAR}
-                size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
-            />
+            {isExportedToCsv && (
+                <Icon
+                    src={icons.Document}
+                    fill={theme.icon}
+                    small
+                />
+            )}
+            {isExportedToNetsuite && (
+                <Avatar
+                    source={icons.NetSuiteSquare}
+                    type={CONST.ICON_TYPE_AVATAR}
+                    size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
+                />
+            )}
+            {isExportedToXero && (
+                <Avatar
+                    source={icons.XeroSquare}
+                    type={CONST.ICON_TYPE_AVATAR}
+                    size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
+                />
+            )}
+            {isExportedToIntacct && (
+                <Avatar
+                    source={icons.IntacctSquare}
+                    type={CONST.ICON_TYPE_AVATAR}
+                    size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
+                />
+            )}
+            {(isExportedToQuickbooksOnline || isExportedToQuickbooksDesktop) && (
+                <Avatar
+                    source={icons.QBOSquare}
+                    type={CONST.ICON_TYPE_AVATAR}
+                    size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
+                />
+            )}
         </View>
     );
 }
