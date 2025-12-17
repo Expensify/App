fab329f76ab5604bdc764257a9e05146925c8af0
Christoph Pader <chris@margelo.com>
2025-12-17 12:43:49 -0500

    fix: Expensicons error

--- src/components/CaretWrapper.tsx
+++ src/components/CaretWrapper.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import type {StyleProp, ViewStyle} from 'react-native';
 import {View} from 'react-native';
+import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import variables from '@styles/variables';
 import type ChildrenProps from '@src/types/utils/ChildrenProps';
 import Icon from './Icon';
-import * as Expensicons from './Icon/Expensicons';
 
 type CaretWrapperProps = ChildrenProps & {
     style?: StyleProp<ViewStyle>;
@@ -17,12 +17,13 @@ type CaretWrapperProps = ChildrenProps & {
 function CaretWrapper({children, style, carretWidth, carretHeight}: CaretWrapperProps) {
     const theme = useTheme();
     const styles = useThemeStyles();
+    const expensifyIcons = useMemoizedLazyExpensifyIcons(['DownArrow'] as const);
 
     return (
         <View style={[styles.flexRow, styles.gap1, styles.alignItemsCenter, style]}>
             {children}
             <Icon
-                src={Expensicons.DownArrow}
+                src={expensifyIcons.DownArrow}
                 fill={theme.icon}
                 width={carretWidth ?? variables.iconSizeExtraSmall}
                 height={carretHeight ?? variables.iconSizeExtraSmall}
