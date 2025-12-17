217baa7e2dba168f2455aa79cabe1ced8382c5f0
Jack Senyitko <jsenyitko@expensify.com>
2025-12-17 14:52:21 -0500

    add exported icon

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -6712,6 +6712,7 @@ const CONST = {
                     TAX_AMOUNT: this.TABLE_COLUMNS.TAX_AMOUNT,
                     STATUS: this.TABLE_COLUMNS.STATUS,
                     TITLE: this.TABLE_COLUMNS.TITLE,
+                    EXPORTED_ICON: this.TABLE_COLUMNS.EXPORTED_ICON,
                     ACTION: this.TABLE_COLUMNS.ACTION,
                 },
                 EXPENSE_REPORT: {
@@ -6724,6 +6725,7 @@ const CONST = {
                     TO: this.TABLE_COLUMNS.TO,
                     REPORT_ID: this.TABLE_COLUMNS.REPORT_ID,
                     BASE_62_REPORT_ID: this.TABLE_COLUMNS.BASE_62_REPORT_ID,
+                    EXPORTED_ICON: this.TABLE_COLUMNS.EXPORTED_ICON,
                     ACTION: this.TABLE_COLUMNS.ACTION,
                 },
                 INVOICE: {},
@@ -6826,6 +6828,7 @@ const CONST = {
             REPORT_ID: 'reportID',
             BASE_62_REPORT_ID: 'base62ReportID',
             TAX: 'tax',
+            EXPORTED_ICON: 'exportedIcon',
         },
         SYNTAX_OPERATORS: {
             AND: 'and',
