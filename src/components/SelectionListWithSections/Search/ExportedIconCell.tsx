5be35b0341e9df03452325a2fbda43870e966295
Jack Senyitko <jsenyitko@expensify.com>
2025-12-17 15:51:10 -0500

    add icons

--- src/components/SelectionListWithSections/Search/ExportedIconCell.tsx
+++ src/components/SelectionListWithSections/Search/ExportedIconCell.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {View} from 'react-native';
+import Avatar from '@components/Avatar';
 import Icon from '@components/Icon';
 import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
+import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
+import CONST from '@src/CONST';
 
 type ExportedIconCellProps = {
     reportID?: string;
 };
 
 function ExportedIconCell({reportID}: ExportedIconCellProps) {
+    const theme = useTheme();
     const styles = useThemeStyles();
-    const icons = useMemoizedLazyExpensifyIcons(['Table']);
+    const icons = useMemoizedLazyExpensifyIcons(['Table', 'NetSuiteSquare', 'XeroSquare', 'IntacctSquare', 'QBOSquare']);
 
     if (!reportID) {
         return null;
@@ -20,23 +24,28 @@ function ExportedIconCell({reportID}: ExportedIconCellProps) {
         <View style={[styles.flexRow, styles.gap2]}>
             <Icon
                 src={icons.Table}
-                small
+                fill={theme.icon}
+                height={16}
             />
-            <Icon
-                src={icons.Table}
-                small
+            <Avatar
+                source={icons.NetSuiteSquare}
+                type={CONST.ICON_TYPE_AVATAR}
+                size={CONST.AVATAR_SIZE.SMALLER}
             />
-            <Icon
-                src={icons.Table}
-                small
+            <Avatar
+                source={icons.XeroSquare}
+                type={CONST.ICON_TYPE_AVATAR}
+                size={CONST.AVATAR_SIZE.SMALLER}
             />
-            <Icon
-                src={icons.Table}
-                small
+            <Avatar
+                source={icons.IntacctSquare}
+                type={CONST.ICON_TYPE_AVATAR}
+                size={CONST.AVATAR_SIZE.SMALLER}
             />
-            <Icon
-                src={icons.Table}
-                small
+            <Avatar
+                source={icons.QBOSquare}
+                type={CONST.ICON_TYPE_AVATAR}
+                size={CONST.AVATAR_SIZE.SMALLER}
             />
         </View>
     );
