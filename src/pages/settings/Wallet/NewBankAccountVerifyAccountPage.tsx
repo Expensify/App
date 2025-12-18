f1ea4f7b48c842f55379276a28ba7307d810c392
Agata Kosior <agata.kosior57@gmail.com>
2025-12-18 15:49:46 +0100

    fix: forwardTo route for a member

--- src/pages/settings/Wallet/NewBankAccountVerifyAccountPage.tsx
+++ src/pages/settings/Wallet/NewBankAccountVerifyAccountPage.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
 import VerifyAccountPageBase from '@pages/settings/VerifyAccountPageBase';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
+import {hasActiveAdminWorkspaces} from '@libs/PolicyUtils';
+import {getCurrentUserEmail} from '@userActions/Report';
+import useOnyx from '@hooks/useOnyx';
+import ONYXKEYS from '@src/ONYXKEYS';
 
 type NewBankAccountVerifyAccountPageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.SETTINGS.ADD_BANK_ACCOUNT_VERIFY_ACCOUNT>;
 
 function NewBankAccountVerifyAccountPage({route}: NewBankAccountVerifyAccountPageProps) {
+    const [allPolicies] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {canBeMissing: true});
+
+    const currentUserEmail = getCurrentUserEmail();
+    const isAdmin = useMemo(() => hasActiveAdminWorkspaces(currentUserEmail, allPolicies), [currentUserEmail, allPolicies]);
+    const navigateForwardTo = isAdmin ? ROUTES.SETTINGS_BANK_ACCOUNT_PURPOSE : ROUTES.SETTINGS_ADD_BANK_ACCOUNT.route;
     const navigateBackTo = route.params?.backTo ?? ROUTES.SETTINGS_WALLET;
 
     return (
         <VerifyAccountPageBase
             navigateBackTo={navigateBackTo}
-            navigateForwardTo={ROUTES.SETTINGS_BANK_ACCOUNT_PURPOSE}
+            navigateForwardTo={navigateForwardTo}
         />
     );
 }
