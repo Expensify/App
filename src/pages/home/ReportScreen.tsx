b2f0f6b46e51563b47cc226d86961e1820f7236f
Yehor Kharchenko <yehor.kharchenko@swmansion.com>
2025-12-18 12:25:40 +0100

    remove dead code

--- src/pages/home/ReportScreen.tsx
+++ src/pages/home/ReportScreen.tsx
@@ -1066,5 +1066,4 @@ function ReportScreen({route, navigation, isInSidePanel = false}: ReportScreenPr
     );
 }
 
-// eslint-disable-next-line rulesdir/no-deep-equal-in-memo
 export default memo(ReportScreen, (prevProps, nextProps) => deepEqual(prevProps.route, nextProps.route));
