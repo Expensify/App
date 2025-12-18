02a41ca9ff377ffb3a6028ee9a499f6cf1cefaff
GCyganek <gabrielcyganek@gmail.com>
2025-12-18 16:30:58 +0100

    Add Precise location request modal on Android

--- src/pages/iou/request/step/IOURequestStepDistanceGPS/BackgroundLocationPermissionsFlow/index.android.tsx
+++ src/pages/iou/request/step/IOURequestStepDistanceGPS/BackgroundLocationPermissionsFlow/index.android.tsx
@@ -1,17 +1,30 @@
 import {getBackgroundPermissionsAsync, getForegroundPermissionsAsync, PermissionStatus, requestBackgroundPermissionsAsync, requestForegroundPermissionsAsync} from 'expo-location';
 import React, {useCallback, useEffect, useState} from 'react';
+import {Linking} from 'react-native';
 import ConfirmModal from '@components/ConfirmModal';
 import {loadIllustration} from '@components/Icon/IllustrationLoader';
 import {useMemoizedLazyAsset} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import type BackgroundLocationPermissionsFlowProps from './types';
 
-async function requestForegroundPermissions(onSuccess: () => Promise<void>, onError: () => void) {
+async function requestForegroundPermissions({
+    onSuccess,
+    onError,
+    onPreciseLocationNotGranted,
+}: {
+    onSuccess: () => Promise<void>;
+    onPreciseLocationNotGranted: () => void;
+    onError: () => void;
+}) {
     try {
         const {status, android} = await requestForegroundPermissionsAsync();
 
         if (status === PermissionStatus.GRANTED && android?.accuracy === 'fine') {
             await onSuccess();
+        }
+
+        if (android?.accuracy !== 'fine') {
+            onPreciseLocationNotGranted();
         }
     } catch (e) {
         console.error('[GPS distance request] Failed to request foreground location permissions: ', e);
@@ -68,6 +81,7 @@ async function checkPermissions({
 function BackgroundLocationPermissionsFlow({startPermissionsFlow, setStartPermissionsFlow, onGrant, onDeny, setShouldShowPermissionsError}: BackgroundLocationPermissionsFlowProps) {
     const [showFirstAskModal, setShowFirstAskModal] = useState(false);
     const [showBgPermissionsModal, setShowBgPermissionsModal] = useState(false);
+    const [showPreciseLocationModal, setShowPreciseLocationModal] = useState(false);
     const {asset: ReceiptLocationMarker} = useMemoizedLazyAsset(() => loadIllustration('ReceiptLocationMarker'));
     const {translate} = useLocalize();
 
@@ -76,7 +90,7 @@ function BackgroundLocationPermissionsFlow({startPermissionsFlow, setStartPermis
     const onForegroundPermissionsGranted = async () => {
         const {granted} = await getBackgroundPermissionsAsync();
 
-        // possible when foreground permissions request was to grant precise location and
+        // possible when foreground location permissions request was to grant precise location and
         // bg permissions were already granted
         if (granted) {
             onGrant();
@@ -103,7 +117,7 @@ function BackgroundLocationPermissionsFlow({startPermissionsFlow, setStartPermis
                 onCancel={() => setShowFirstAskModal(false)}
                 onConfirm={() => {
                     setShowFirstAskModal(false);
-                    requestForegroundPermissions(onForegroundPermissionsGranted, onError);
+                    requestForegroundPermissions({onSuccess: onForegroundPermissionsGranted, onError, onPreciseLocationNotGranted: () => setShowPreciseLocationModal(true)});
                 }}
                 confirmText={translate('gps.locationRequiredModal.allow')}
                 cancelText={translate('common.dismiss')}
@@ -126,6 +140,24 @@ function BackgroundLocationPermissionsFlow({startPermissionsFlow, setStartPermis
                 confirmText={translate('common.settings')}
                 cancelText={translate('common.dismiss')}
                 prompt={translate('gps.androidBackgroundLocationRequiredModal.prompt')}
+                iconSource={ReceiptLocationMarker}
+                iconFill={false}
+                iconWidth={140}
+                iconHeight={120}
+                shouldCenterIcon
+                shouldReverseStackedButtons
+            />
+            <ConfirmModal
+                title={translate('gps.preciseLocationRequiredModal.title')}
+                isVisible={showPreciseLocationModal}
+                onConfirm={() => {
+                    setShowPreciseLocationModal(false);
+                    Linking.openSettings();
+                }}
+                onCancel={() => setShowPreciseLocationModal(false)}
+                confirmText={translate('common.settings')}
+                cancelText={translate('common.dismiss')}
+                prompt={translate('gps.preciseLocationRequiredModal.prompt')}
                 iconSource={ReceiptLocationMarker}
                 iconFill={false}
                 iconWidth={140}
