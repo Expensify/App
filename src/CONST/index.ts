43822fa9819052377278730c0f1d5da7bee9028d
Michał Muzyk <mimuzyk@gmail.com>
2025-12-18 16:54:22 +0100

    fix: 77979 Continue button directs to proper step

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -670,6 +670,15 @@ const CONST = {
             },
         },
         BANK_INFO_STEP_ACCOUNT_HOLDER_KEY_PREFIX: 'accountHolder',
+        STATE: {
+            VERIFYING: 'VERIFYING',
+            VALIDATING: 'VALIDATING',
+            SETUP: 'SETUP',
+            PENDING: 'PENDING',
+            OPEN: 'OPEN',
+            DELETED: 'DELETED',
+            LOCKED: 'LOCKED',
+        },
     },
     ENABLE_GLOBAL_REIMBURSEMENTS: {
         STEP_NAMES: ['1', '2', '3'],
