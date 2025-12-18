56f969ec7d996cd71d31832a3bbcfe2125fe0bd1
Eskalifer1 <artemkryt1800@gmail.com>
2025-12-18 12:14:41 +0200

    Updated SubscriptionPlanDowngradeBlocked to support invoicing users

--- src/languages/ja.ts
+++ src/languages/ja.ts
@@ -3872,6 +3872,8 @@ ${
             viewTransactions: '取引を表示',
             policyExpenseChatName: ({displayName}: PolicyExpenseChatNameParams) => `${displayName} の経費`,
             deepDiveExpensifyCard: `<muted-text-label>Expensify Card の取引は、<a href="${CONST.DEEP_DIVE_EXPENSIFY_CARD}">当社のインテグレーション</a>によって作成される「Expensify Card 負債勘定」に自動的にエクスポートされます。</muted-text-label>`,
+            youCantDowngradeInvoicing:
+                '請求書払いのサブスクリプションでは、プランをダウングレードできません。サブスクリプションについて相談したり変更したりする場合は、アカウントマネージャーまたはConciergeまでお問い合わせください。',
         },
         receiptPartners: {
             connect: '今すぐ接続',
