0596d0b37914a428de6b21a7514574c885ee1489
Carlos Miceli <carlosmiceli@expensify.com>
2025-12-19 11:20:27 -0500

    Update src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
    Co-authored-by: Tim Golen <tgolen@gmail.com>

--- src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
+++ src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
@@ -97,10 +97,7 @@ function WorkspaceCompanyCardTableItem({
     const companyCardFeedIcons = useCompanyCardFeedIcons();
     const Expensicons = useMemoizedLazyExpensifyIcons(['ArrowRight']);
 
-    let cardFeedIcon = null;
-    if (!plaidIconUrl) {
-        cardFeedIcon = getCardFeedIcon(selectedFeed as CompanyCardFeed, illustrations, companyCardFeedIcons);
-    }
+    const cardFeedIcon = !plaidIconUrl && getCardFeedIcon(selectedFeed as CompanyCardFeed, illustrations, companyCardFeedIcons) : null;
 
     const {failedCompanyCardAssignment} = item;
     let {cardName, customCardName, cardholder, assignedCard, isAssigned, isCardDeleted} = item;
