f674f376fd41e1a4d33610ff7d531546395c499a
eliran goshen <elirangoshen@gmail.com>
2025-12-19 11:13:13 +0100

    prettier fix

--- src/hooks/useDeepLink/index.native.ts
+++ src/hooks/useDeepLink/index.native.ts
@@ -20,4 +20,3 @@ export default function useDeepLink(): UseDeepLinkResult {
         deepLinkUrl,
     };
 }
-
--- src/hooks/useDeepLink/index.ts
+++ src/hooks/useDeepLink/index.ts
@@ -19,4 +19,3 @@ export default function useDeepLink(): UseDeepLinkResult {
         deepLinkUrl,
     };
 }
-
