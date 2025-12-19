dc0039accab781dfd21d0313d66344595b774656
Tim Golen <tgolen@expensify.com>
2025-12-19 12:49:25 -0500

    Add a space

--- src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
+++ src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
@@ -234,7 +234,7 @@ function WorkspaceCompanyCardTableItem({
                             </View>
                         )}
 
-                        <View style={[shouldUseNarrowTableRowLayout ? styles.flex0 :styles.flex1, styles.alignItemsEnd]}>
+                        <View style={[shouldUseNarrowTableRowLayout ? styles.flex0 : styles.flex1, styles.alignItemsEnd]}>
                             {isAssigned && (
                                 <View style={[styles.justifyContentEnd, styles.w100, styles.flexRow, styles.ml2, styles.gap3]}>
                                     {!shouldUseNarrowTableRowLayout && (
