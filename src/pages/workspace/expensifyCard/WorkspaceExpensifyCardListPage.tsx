6f9b8944c49d9c2b068431aba8625ebe6c0eff9f
Christoph Pader <chris@margelo.com>
2025-12-19 11:51:52 -0500

    fix: do not inline cardFeedIcon element

--- src/pages/workspace/expensifyCard/WorkspaceExpensifyCardListPage.tsx
+++ src/pages/workspace/expensifyCard/WorkspaceExpensifyCardListPage.tsx
@@ -88,6 +88,8 @@ function WorkspaceExpensifyCardListPage({route, cardsList, fundID}: WorkspaceExp
     const headerHeight = useEmptyViewHeaderHeight(shouldUseNarrowLayout, isBankAccountVerified);
     const [footerHeight, setFooterHeight] = useState(0);
 
+    const cardFeedIcon = useMemo(() => <CardFeedIcon selectedFeed={undefined} />, []);
+
     const settlementCurrency = useCurrencyForExpensifyCard({policyID});
 
     const allCards = useMemo(() => {
@@ -234,7 +236,7 @@ function WorkspaceExpensifyCardListPage({route, cardsList, fundID}: WorkspaceExp
                 <View style={[styles.w100, styles.ph5, styles.pb3, !shouldChangeLayout && [styles.flexRow, styles.alignItemsCenter, styles.justifyContentBetween]]}>
                     <FeedSelector
                         onFeedSelect={() => Navigation.navigate(ROUTES.WORKSPACE_EXPENSIFY_CARD_SELECT_FEED.getRoute(policyID))}
-                        CardFeedIcon={<CardFeedIcon selectedFeed={undefined} />}
+                        CardFeedIcon={cardFeedIcon}
                         feedName={translate('workspace.common.expensifyCard')}
                         supportingText={getDescriptionForPolicyDomainCard(cardSettings?.domainName ?? '')}
                     />
