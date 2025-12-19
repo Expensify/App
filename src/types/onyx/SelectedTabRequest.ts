751b5f624d3662b019864924ae455a511a756c06
Yauheni Horbach <HorbachZhenja@yandex.by>
2025-12-19 19:01:05 +0100

    Fix ts issues

--- src/types/onyx/SelectedTabRequest.ts
+++ src/types/onyx/SelectedTabRequest.ts
@@ -2,6 +2,6 @@ import type {ValueOf} from 'type-fest';
 import type CONST from '@src/CONST';
 
 /** Selectable IOU request tabs */
-type SelectedTabRequest = ValueOf<typeof CONST.TAB_REQUEST> | ValueOf<typeof CONST.TAB.SPLIT>;
+type SelectedTabRequest = ValueOf<typeof CONST.TAB_REQUEST>;
 
 export default SelectedTabRequest;
