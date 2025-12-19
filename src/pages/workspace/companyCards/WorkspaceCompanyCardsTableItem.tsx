a34a88b2af9018a1bd43beb290b34992bf778fa5
Carlos Miceli <carlosmiceli@expensify.com>
2025-12-18 21:58:36 -0500

    fix button color

--- src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
+++ src/pages/workspace/companyCards/WorkspaceCompanyCardsTableItem.tsx
@@ -212,6 +212,7 @@ function WorkspaceCompanyCardTableItem({
                             )}
                             {!isAssigned && (
                                 <Button
+                                    success
                                     text={shouldUseNarrowTableRowLayout ? translate('workspace.companyCards.assign') : translate('workspace.companyCards.assignCard')}
                                     onPress={onAssignCard}
                                     isDisabled={isAssigningCardDisabled}
