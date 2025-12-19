1ff742c550b65b138da128e83519a884d60bb18a
Carlos Miceli <carlosmiceli@expensify.com>
2025-12-18 22:00:28 -0500

    start cleanup of Assign Card Feed Page

--- src/pages/workspace/companyCards/assignCard/AssignCardFeedPage.tsx
+++ src/pages/workspace/companyCards/assignCard/AssignCardFeedPage.tsx
@@ -2,41 +2,29 @@ import {isActingAsDelegateSelector} from '@selectors/Account';
 import React, {useEffect} from 'react';
 import DelegateNoAccessWrapper from '@components/DelegateNoAccessWrapper';
 import ScreenWrapper from '@components/ScreenWrapper';
-import useInitial from '@hooks/useInitial';
 import useOnyx from '@hooks/useOnyx';
 import {getCompanyCardFeed} from '@libs/CardUtils';
+import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@navigation/types';
-import PlaidConnectionStep from '@pages/workspace/companyCards/addNew/PlaidConnectionStep';
-import BankConnection from '@pages/workspace/companyCards/BankConnection';
 import type {WithPolicyAndFullscreenLoadingProps} from '@pages/workspace/withPolicyAndFullscreenLoading';
 import withPolicyAndFullscreenLoading from '@pages/workspace/withPolicyAndFullscreenLoading';
 import {clearAssignCardStepAndData, setAssignCardStepAndData} from '@userActions/CompanyCards';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type SCREENS from '@src/SCREENS';
+import ROUTES from '@src/ROUTES';
+import SCREENS from '@src/SCREENS';
 import type {CompanyCardFeedWithDomainID} from '@src/types/onyx';
-import AssigneeStep from './AssigneeStep';
-import CardNameStep from './CardNameStep';
-import CardSelectionStep from './CardSelectionStep';
-import ConfirmationStep from './ConfirmationStep';
-import InviteNewMemberStep from './InviteNewMemberStep';
-import TransactionStartDateStep from './TransactionStartDateStep';
 
 type AssignCardFeedPageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.WORKSPACE.COMPANY_CARDS_ASSIGN_CARD> & WithPolicyAndFullscreenLoadingProps;
 
 function AssignCardFeedPage({route, policy}: AssignCardFeedPageProps) {
     const feed = decodeURIComponent(route.params?.feed) as CompanyCardFeedWithDomainID;
-    const cardID = route.params?.cardID ? decodeURIComponent(route.params?.cardID) : undefined;
-
-    const [assignCard] = useOnyx(ONYXKEYS.ASSIGN_CARD, {canBeMissing: true});
-    const currentStep = assignCard?.currentStep;
+    const cardID = route.params?.cardID ? decodeURIComponent(route.params?.cardID) : '';
 
     const backTo = route.params?.backTo;
     const policyID = policy?.id;
     const [isActingAsDelegate] = useOnyx(ONYXKEYS.ACCOUNT, {selector: isActingAsDelegateSelector, canBeMissing: true});
-    const firstAssigneeEmail = useInitial(assignCard?.data?.email);
-    const shouldUseBackToParam = !firstAssigneeEmail || firstAssigneeEmail === assignCard?.data?.email;
 
     useEffect(() => {
         return () => {
