260bdbfb8d92a0e8f88f6d29b83dbe23baaff5ec
Wojciech Boman <wojciechboman@gmail.com>
2025-12-22 09:11:58 +0100

    Fix SearchReport animations

--- src/components/WideRHPOverlayWrapper/index.tsx
+++ src/components/WideRHPOverlayWrapper/index.tsx
@@ -7,7 +7,6 @@ import {
     secondOverlayRHPOnSuperWideRHPProgress,
     secondOverlayRHPOnWideRHPProgress,
     secondOverlayWideRHPProgress,
-    thirdOverlayProgress,
     WideRHPContext,
 } from '@components/WideRHPContextProvider';
 import Overlay from '@libs/Navigation/AppNavigator/Navigators/Overlay';
@@ -70,26 +69,6 @@ function SecondaryOverlay() {
     return null;
 }
 
-function TertiaryOverlay() {
-    const {shouldRenderTertiaryOverlay, wideRHPRouteKeys} = useContext(WideRHPContext);
-    const route = useRoute();
-
-    const isWide = route?.key && wideRHPRouteKeys.includes(route.key);
-
-    // This overlay is used to cover the space under the narrower RHP screen when more than one RHP width is displayed on the screen
-    // There is a special case where three different RHP widths are displayed at the same time. In this case, an overlay under RHP should be rendered from Wide RHP.
-    if (isWide && shouldRenderTertiaryOverlay) {
-        return (
-            <Overlay
-                progress={thirdOverlayProgress}
-                positionLeftValue={modalStackOverlaySuperWideRHPPositionLeft}
-            />
-        );
-    }
-
-    return null;
-}
-
 type WideRHPOverlayWrapperProps = {
     children: React.ReactNode;
     shouldWrap?: boolean;
@@ -128,7 +107,6 @@ export default function WideRHPOverlayWrapper({children, shouldWrap = true}: Wid
         <>
             {children}
             <SecondaryOverlay />
-            <TertiaryOverlay />
         </>
     );
 }
