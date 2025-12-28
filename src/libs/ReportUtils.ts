ce6fe10b446d92531c1387c9cbce3db609c11900
Rushat Gabhane <rushatgabhane@gmail.com>
2025-12-28 19:21:10 +0300

    use explicit modified amount in iou check

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -12468,8 +12468,7 @@ function hasExportError(reportActions: OnyxEntry<ReportActions> | ReportAction[]
 
 function doesReportContainRequestsFromMultipleUsers(iouReport: OnyxEntry<Report>): boolean {
     const transactions = getReportTransactions(iouReport?.reportID);
-    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    return isIOUReport(iouReport) && transactions.some((transaction) => (transaction?.modifiedAmount || transaction?.amount) < 0);
+    return isIOUReport(iouReport) && transactions.some((transaction) => (isNumber(transaction?.modifiedAmount) ? transaction.modifiedAmount : transaction?.amount) < 0);
 }
 
 /**
