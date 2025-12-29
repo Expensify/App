c55f57ee9e6552b5d0e339b8acee173c18a50ab2
bartlomiej obudzinski <bartlomiej.obudzinski@callstack.com>
2025-12-29 10:21:56 +0100

    feat: initialize IntlStore with English translations for testing

--- jest/setupAfterEnv.ts
+++ jest/setupAfterEnv.ts
@@ -1,3 +1,8 @@
 import '@testing-library/react-native';
+import IntlStore from '@src/languages/IntlStore';
+
+// Initialize IntlStore with EN translations for tests
+IntlStore.init();
+IntlStore.load('en');
 
 jest.useRealTimers();
