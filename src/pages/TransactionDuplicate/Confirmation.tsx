49ac8603f56f074ba3949164863dec9769e8d012
Vit Horacek <36083550+mountiny@users.noreply.github.com>
2025-12-30 14:42:49 +0100

    Merge pull request #78580 from Expensify/youssef_fix_mistake_param_removed
    [CP Staging] Bring back param removed by mistake
    
    (cherry picked from commit 2e1da9f86e77775ec47a629e6a47a4b3797cfc7b)
    
    (cherry-picked to staging by mountiny)

--- src/pages/TransactionDuplicate/Confirmation.tsx
+++ src/pages/TransactionDuplicate/Confirmation.tsx
@@ -148,6 +148,7 @@ function Confirmation() {
                                 shouldShowAnimatedBackground={false}
                                 readonly
                                 updatedTransaction={newTransaction as OnyxEntry<Transaction>}
+                                isFromReviewDuplicates
                             />
                         </ShowContextMenuContext.Provider>
                     </ScrollView>
