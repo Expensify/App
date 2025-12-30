f95ec5b50cb83f2f0efd095a3f4c6ca99c895b5c
Rutika Pawar <183392827+twilight2294@users.noreply.github.com>
2025-12-31 02:10:09 +0530

    Update InviteNewMemberStep.tsx

--- src/pages/workspace/expensifyCard/issueNew/InviteNewMemberStep.tsx
+++ src/pages/workspace/expensifyCard/issueNew/InviteNewMemberStep.tsx
@@ -5,7 +5,6 @@ import withCurrentUserPersonalDetails from '@components/withCurrentUserPersonalD
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import {setDraftInviteAccountID, setIssueNewCardStepAndData} from '@libs/actions/Card';
-import {clearInviteDraft} from '@libs/actions/Policy/Member';
 import type {PlatformStackRouteProp} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
 import WorkspaceInviteMessageComponent from '@pages/workspace/members/WorkspaceInviteMessageComponent';
@@ -36,7 +35,6 @@ function InviteNewMemberStep({policy, route, currentUserPersonalDetails}: Invite
             return;
         }
 
-        clearInviteDraft(policyID);
         setIssueNewCardStepAndData({
             step: CONST.EXPENSIFY_CARD.STEP.ASSIGNEE,
             data: {...issueNewCard?.data, invitingMemberEmail: undefined},
@@ -75,7 +73,7 @@ function InviteNewMemberStep({policy, route, currentUserPersonalDetails}: Invite
 
     return (
         <InteractiveStepWrapper
-            wrapperID="InviteNewMemberStep"
+            wrapperID={InviteNewMemberStep.displayName}
             shouldEnablePickerAvoiding={false}
             shouldEnableMaxHeight
             headerTitle={translate('workspace.card.issueCard')}
@@ -98,5 +96,7 @@ function InviteNewMemberStep({policy, route, currentUserPersonalDetails}: Invite
         </InteractiveStepWrapper>
     );
 }
+
+InviteNewMemberStep.displayName = 'InviteNewMemberStep';
 
 export default withPolicyAndFullscreenLoading(withCurrentUserPersonalDetails(InviteNewMemberStep));
