b580071d274003b45a47c4d103d948d78a069dc3
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-12-30 07:05:05 +0100

    simplify useSelfDMReport

--- src/hooks/useSelfDMReport.ts
+++ src/hooks/useSelfDMReport.ts
@@ -1,12 +1,10 @@
-import {useMemo} from 'react';
 import DateUtils from '@libs/DateUtils';
 import {buildOptimisticSelfDMReport, findSelfDMReportID} from '@libs/ReportUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import useOnyx from './useOnyx';
 
 function useSelfDMReport() {
-    const [allReports] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {canBeMissing: false});
-    const selfDMReportID = useMemo(() => findSelfDMReportID(allReports), [allReports]);
+    const [selfDMReportID] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {canBeMissing: false, selector: findSelfDMReportID});
     const [selfDMReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${selfDMReportID}`, {canBeMissing: true});
     return selfDMReport ?? buildOptimisticSelfDMReport(DateUtils.getDBTime());
 }
