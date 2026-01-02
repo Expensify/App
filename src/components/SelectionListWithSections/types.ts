f2a5b4a44ff693fcae494cbf27cbc97e7b8bd19f
nkdengineer <nkdengineer@outlook.com>
2026-01-02 12:55:02 +0700

    fix: Card column changes to card number and reverts to card name after editing expense

--- src/components/SelectionListWithSections/types.ts
+++ src/components/SelectionListWithSections/types.ts
@@ -633,6 +633,7 @@ type TransactionListItemProps<TItem extends ListItem> = ListItemProps<TItem> & {
     isLoading?: boolean;
     columns?: SearchColumnType[];
     violations?: Record<string, TransactionViolations | undefined> | undefined;
+    customCardNames?: Record<number, string>;
     /** Callback to fire when DEW modal should be opened */
     onDEWModalOpen?: () => void;
 };
