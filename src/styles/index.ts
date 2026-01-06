021b8eac1c435ed274d6b18c2a2434ec6ab3a705
Andrew Rosiclair <arosiclair@expensify.com>
2026-01-06 14:31:58 -0500

    Merge pull request #78903 from situchan/revert-77324-fix/76785
    Revert "Report field - Multiline input for name field in report field does not scroll"
    
    (cherry picked from commit 0a2c7945d72c8e059c7b15564778ceff3ddf1077)
    
    (cherry-picked to staging by arosiclair)

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1251,12 +1251,6 @@ const staticStyles = (theme: ThemeColors) =>
             borderColor: theme.border,
         },
 
-        hiddenTextInputContainer: {
-            paddingLeft: 8,
-            paddingRight: 8,
-            borderWidth: 1,
-        },
-
         cannotBeEditedSplitInputContainer: {
             flexDirection: 'row',
             alignItems: 'center',
