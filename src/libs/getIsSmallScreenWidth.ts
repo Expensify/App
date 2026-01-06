c3fa2b75c382b0ad474229fafcf985c35344cc11
Andrew Gable <andrew@expensify.com>
2026-01-06 12:12:50 -0700

    Test: Expect to retry and pass (with a diff)

--- src/libs/getIsSmallScreenWidth.ts
+++ src/libs/getIsSmallScreenWidth.ts
@@ -1,6 +1,7 @@
 import {Dimensions} from 'react-native';
 import variables from '@styles/variables';
 
+// comment
 export default function getIsSmallScreenWidth(windowWidth = Dimensions.get('window').width) {
     return windowWidth <= variables.mobileResponsiveWidthBreakpoint;
 }
