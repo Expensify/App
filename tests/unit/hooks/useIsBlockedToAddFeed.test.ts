a785764236f07459c8c752be3c21c0029bba5f8d
Gandalf <whogandalf@gmail.com>
2026-01-06 10:38:43 +0530

    Update useIsBlockedToAddFeed.test.ts

--- tests/unit/hooks/useIsBlockedToAddFeed.test.ts
+++ tests/unit/hooks/useIsBlockedToAddFeed.test.ts
@@ -70,6 +70,7 @@ describe('useIsBlockedToAddFeed', () => {
             {status: 'loaded'},
         ]);
         const {result} = renderHook(() => useIsBlockedToAddFeed(mockPolicyID));
+        
         // CSV feeds don't count toward the limit, so user can add another feed
         expect(result.current.isBlockedToAddNewFeeds).toBe(false);
     });
