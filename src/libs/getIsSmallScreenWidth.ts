7e04d8c5370f9d5aca26405de802944332a5255a
Andrew Gable <andrew@expensify.com>
2026-01-06 12:54:19 -0700

    Add retry to lint changed as well

--- src/libs/getIsSmallScreenWidth.ts
+++ src/libs/getIsSmallScreenWidth.ts
@@ -1,7 +1,6 @@
 import {Dimensions} from 'react-native';
 import variables from '@styles/variables';
 
-// comment
 export default function getIsSmallScreenWidth(windowWidth = Dimensions.get('window').width) {
     return windowWidth <= variables.mobileResponsiveWidthBreakpoint;
 }
