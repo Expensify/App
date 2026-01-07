8e5952785b8786e986f19f9c43cd7272719960a8
mhawryluk <marcinhawrylukxx@gmail.com>
2026-01-07 16:02:43 +0100

    Change computeTimeAmount to treat rate as being a formatted number in currency units

--- src/libs/TimeTrackingUtils.ts
+++ src/libs/TimeTrackingUtils.ts
@@ -1,10 +1,16 @@
 import type {LocalizedTranslate} from '@components/LocaleContextProvider';
 import {convertToDisplayString} from './CurrencyUtils';
 
-function computeTimeAmount(rateInCents: number, count: number): number {
-    return Math.round(rateInCents * count);
+/**
+ * Computes the transaction amount for given hourly rate (as a formatted number in currency units) and hour count.
+ */
+function computeTimeAmount(rate: number, count: number): number {
+    return Math.round(rate * 100 * count);
 }
 
+/**
+ * Creates an automatic merchant value for time requests.
+ */
 function formatTimeMerchant(hours: number, rate: number, currency: string, translate: LocalizedTranslate): string {
     return translate('iou.timeTracking.hoursAt', hours, convertToDisplayString(rate, currency));
 }
