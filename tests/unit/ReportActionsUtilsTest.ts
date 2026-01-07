de32ef0ce0c8089e8b08994b9ace8caa76fcb3d8
Rayane <77965000+rayane-d@users.noreply.github.com>
2026-01-07 21:52:54 +0100

    Remove duplicated imports

--- tests/unit/ReportActionsUtilsTest.ts
+++ tests/unit/ReportActionsUtilsTest.ts
@@ -14,8 +14,6 @@ import {
     getCompanyAddressUpdateMessage,
     getOneTransactionThreadReportID,
     getOriginalMessage,
-    getOneTransactionThreadReportID,
-    getOriginalMessage,
     getReportActionActorAccountID,
     getSendMoneyFlowAction,
     isIOUActionMatchingTransactionList,
