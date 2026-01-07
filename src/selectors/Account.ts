8fb5a5eacf97299c70caf9f92293dab376b2926f
Rushat Gabhane <rushatgabhane@gmail.com>
2026-01-07 14:39:01 +0300

    add isdelegateaccessrestrictedselector export

--- src/selectors/Account.ts
+++ src/selectors/Account.ts
@@ -1,7 +1,11 @@
 import type {OnyxEntry} from 'react-native-onyx';
+import AccountUtils from '@libs/AccountUtils';
 import type {Account} from '@src/types/onyx';
 
 const isActingAsDelegateSelector = (account: OnyxEntry<Account>) => !!account?.delegatedAccess?.delegate;
+
+const isDelegateAccessRestrictedSelector = (account: OnyxEntry<Account>) =>
+    !!account?.delegatedAccess?.delegate && AccountUtils.isDelegateOnlySubmitter(account);
 
 const isUserValidatedSelector = (account: OnyxEntry<Account>) => account?.validated;
 
@@ -9,4 +13,4 @@ const primaryLoginSelector = (account: OnyxEntry<Account>) => account?.primaryLo
 
 const delegatesSelector = (account: OnyxEntry<Account>) => account?.delegatedAccess?.delegates;
 
-export {isActingAsDelegateSelector, isUserValidatedSelector, primaryLoginSelector, delegatesSelector};
+export {isActingAsDelegateSelector, isDelegateAccessRestrictedSelector, isUserValidatedSelector, primaryLoginSelector, delegatesSelector};
