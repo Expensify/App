91084daacd3c12dd5c2c2f057fead9c489f4a203
Zuzanna Furtak <zuzkafurtak@gmail.com>
2026-01-08 15:55:26 +0100

    revert unnecessary changes

--- src/components/SelectionListWithSections/BaseSelectionListWithSections.tsx
+++ src/components/SelectionListWithSections/BaseSelectionListWithSections.tsx
@@ -787,16 +787,16 @@ function BaseSelectionListWithSections<TItem extends ListItem>({
                 const itemHeight = nativeEvent.nativeEvent.layout.y;
                 setMaxToRenderPerBatch((Math.ceil(listHeight / itemHeight) || 0) + CONST.MAX_TO_RENDER_PER_BATCH.DEFAULT);
             }
+
             if (!isInitialSectionListRender) {
                 return;
             }
-
             if (shouldScrollToFocusedIndex) {
                 scrollToIndex(focusedIndex, false);
             }
             setIsInitialSectionListRender(false);
         },
-        [shouldUseDynamicMaxToRenderPerBatch, isInitialSectionListRender, shouldScrollToFocusedIndex, scrollToIndex, focusedIndex],
+        [focusedIndex, isInitialSectionListRender, scrollToIndex, shouldUseDynamicMaxToRenderPerBatch, shouldScrollToFocusedIndex],
     );
 
     const onSectionListLayout = useCallback(
