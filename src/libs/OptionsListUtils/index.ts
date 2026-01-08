4b0572218679444e0a1632672a67a419be34acb8
Szymon Zalarski <szymonzalarski98@gmail.com>
2026-01-08 15:52:24 +0100

    Use onyx.connectWithoutView instead of onyx.connect

--- src/libs/OptionsListUtils/index.ts
+++ src/libs/OptionsListUtils/index.ts
@@ -244,7 +244,7 @@ Onyx.connect({
 });
 
 let reportAttributesDerivedGlobal: ReportAttributesDerivedValue['reports'];
-Onyx.connect({
+Onyx.connectWithoutView({
     key: ONYXKEYS.DERIVED.REPORT_ATTRIBUTES,
     callback: (value) => {
         reportAttributesDerivedGlobal = value?.reports ?? {};
