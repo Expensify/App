549067ca3c497a26425b0c9a2da3172615641e77
Faizan Shoukat Abbasi <abbasi.faizan1997@gmail.com>
2026-01-08 20:41:47 +0500

    reverted the navigation code

--- src/libs/Navigation/helpers/isSearchTopmostFullScreenRoute.ts
+++ src/libs/Navigation/helpers/isSearchTopmostFullScreenRoute.ts
@@ -4,11 +4,6 @@ import NAVIGATORS from '@src/NAVIGATORS';
 import {isFullScreenName} from './isNavigatorName';
 
 const isSearchTopmostFullScreenRoute = (): boolean => {
-    // Check if navigation is ready before accessing state to avoid "navigation not initialized" errors
-    if (!navigationRef.isReady()) {
-        return false;
-    }
-
     const rootState = navigationRef.getRootState() as State<RootNavigatorParamList>;
 
     if (!rootState) {
