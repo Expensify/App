b155ac0680248ba3a781e274bd05c513ec1d815b
cretadn22 <168617111+cretadn22@users.noreply.github.com>
2026-01-11 01:20:19 +0700

    Revert "Revert "Fix: incorrect recipient after switching invoice rooms""

--- src/pages/iou/request/step/IOURequestStepConfirmation.tsx
+++ src/pages/iou/request/step/IOURequestStepConfirmation.tsx
@@ -1020,12 +1020,14 @@ function IOURequestStepConfirmation({
             if (iouType === CONST.IOU.TYPE.INVOICE) {
                 const invoiceChatReport =
                     !isEmptyObject(report) && report?.reportID && doesReportReceiverMatchParticipant(report, receiverParticipantAccountID) ? report : existingInvoiceReport;
+                const invoiceChatReportID = invoiceChatReport ? undefined : reportID;
 
                 sendInvoice({
                     currentUserAccountID: currentUserPersonalDetails.accountID,
                     transaction,
                     policyRecentlyUsedCurrencies: policyRecentlyUsedCurrencies ?? [],
                     invoiceChatReport,
+                    invoiceChatReportID,
                     receiptFile: currentTransactionReceiptFile,
                     policy,
                     policyTagList: policyTags,
@@ -1149,6 +1151,7 @@ function IOURequestStepConfirmation({
             userLocation,
             submitPerDiemExpense,
             policyRecentlyUsedCurrencies,
+            reportID,
         ],
     );
 
