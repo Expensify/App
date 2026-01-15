386db737cfe0ea5780ab0b1e30cf383cb74bdce7
Adam Grzybowski <adam.grzybowski@swmansion.com>
2026-01-15 16:59:49 +0100

    fix types

--- src/libs/Navigation/PlatformStackNavigation/navigationOptions/convertToWebNavigationOptions.ts
+++ src/libs/Navigation/PlatformStackNavigation/navigationOptions/convertToWebNavigationOptions.ts
@@ -1,8 +1,14 @@
-import type {ParamListBase, ScreenOptionsOrCallback} from '@react-navigation/native';
+import type {ParamListBase, RouteProp, ScreenOptionsOrCallback} from '@react-navigation/native';
 import type {StackNavigationOptions} from '@react-navigation/stack';
-import type {PlatformStackNavigationOptions, PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
+import type {PlatformStackNavigationOptions} from '@libs/Navigation/PlatformStackNavigation/types';
 import {isRouteBasedScreenOptions} from '@libs/Navigation/PlatformStackNavigation/types';
 import buildPlatformSpecificNavigationOptions from './buildPlatformSpecificNavigationOptions';
+
+type ScreenOptionsProps = {
+    route: RouteProp<ParamListBase, string>;
+    navigation: unknown;
+    theme: ReactNavigation.Theme;
+};
 
 function convertToWebNavigationOptions(screenOptions: ScreenOptionsOrCallback<PlatformStackNavigationOptions> | undefined): ScreenOptionsOrCallback<StackNavigationOptions> | undefined {
     if (!screenOptions) {
