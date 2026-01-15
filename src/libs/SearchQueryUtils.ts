93948b2cef3d41950686207a08b72eb664a62345
Rajat Parashar <parasharrajat@users.noreply.github.com>
2026-01-15 19:13:50 +0530

    Fix import

--- src/libs/SearchQueryUtils.ts
+++ src/libs/SearchQueryUtils.ts
@@ -1,7 +1,5 @@
 import cloneDeep from 'lodash/cloneDeep';
 import type {OnyxCollection} from 'react-native-onyx';
-// eslint-disable-next-line @typescript-eslint/no-deprecated
-import {translateLocal} from 'tests/utils/TestHelper';
 import type {ValueOf} from 'type-fest';
 import type {LocaleContextProps} from '@components/LocaleContextProvider';
 import type {
@@ -37,6 +35,8 @@ import arraysEqual from '@src/utils/arraysEqual';
 import {getCardFeedsForDisplay} from './CardFeedUtils';
 import {getCardDescription} from './CardUtils';
 import {convertToBackendAmount, convertToFrontendAmountAsInteger} from './CurrencyUtils';
+// eslint-disable-next-line @typescript-eslint/no-deprecated
+import {translateLocal} from './Localize';
 import Log from './Log';
 import {validateAmount} from './MoneyRequestUtils';
 import {getPreservedNavigatorState} from './Navigation/AppNavigator/createSplitNavigator/usePreserveNavigatorState';
