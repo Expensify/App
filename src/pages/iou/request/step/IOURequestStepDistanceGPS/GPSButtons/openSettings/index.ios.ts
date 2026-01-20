6611bdf55945c28db0185554ef4579a838a5a7d1
GCyganek <gabrielcyganek@gmail.com>
2026-01-20 15:31:51 +0100

    fix spell check

--- src/pages/iou/request/step/IOURequestStepDistanceGPS/GPSButtons/openSettings/index.ios.ts
+++ src/pages/iou/request/step/IOURequestStepDistanceGPS/GPSButtons/openSettings/index.ios.ts
@@ -1,9 +1,9 @@
 import {Linking} from 'react-native';
 
+// cspell:disable-next-line
 const IOS_SETTINGS_URL = 'App-Prefs:General';
 
 function openSettings() {
-    // cspell:disable-next-line
     Linking.openURL(IOS_SETTINGS_URL);
 }
 
