f23e863e724a466661895aa77afa0204fa6601fd
Wojciech Boman <wojciechboman@gmail.com>
2026-01-20 11:55:15 +0100

    Add buttons navigating to search sections

--- src/pages/NewDotHomePage.tsx
+++ src/pages/NewDotHomePage.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
 import {View} from 'react-native';
+import Button from '@components/Button';
 import NavigationTabBar from '@components/Navigation/NavigationTabBar';
 import NAVIGATION_TABS from '@components/Navigation/NavigationTabBar/NAVIGATION_TABS';
 import ScreenWrapper from '@components/ScreenWrapper';
+import useOnyx from '@hooks/useOnyx';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
+import Navigation from '@libs/Navigation/Navigation';
+import {buildQueryStringFromFilterFormValues} from '@libs/SearchQueryUtils';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import ROUTES from '@src/ROUTES';
+import {accountIDSelector} from '@src/selectors/Session';
 
 function NewDotHomePage() {
     const styles = useThemeStyles();
     const {shouldUseNarrowLayout} = useResponsiveLayout();
+    const [accountID] = useOnyx(ONYXKEYS.SESSION, {canBeMissing: false, selector: accountIDSelector});
+
+    const handleGoToSearch = () => {
+        Navigation.navigate(
+            ROUTES.SEARCH_ROOT.getRoute({
+                query: buildQueryStringFromFilterFormValues({
+                    type: CONST.SEARCH.DATA_TYPES.EXPENSE_REPORT,
+                    action: CONST.SEARCH.ACTION_FILTERS.SUBMIT,
+                    from: [`${accountID}`],
+                }),
+            }),
+        );
+    };
+
+    const handleGoToApproveSearch = () => {
+        Navigation.navigate(
+            ROUTES.SEARCH_ROOT.getRoute({
+                query: buildQueryStringFromFilterFormValues({
+                    type: CONST.SEARCH.DATA_TYPES.EXPENSE_REPORT,
+                    action: CONST.SEARCH.ACTION_FILTERS.APPROVE,
+                    to: [`${accountID}`],
+                }),
+            }),
+        );
+    };
+
+    const handleGoToPaySearch = () => {
+        Navigation.navigate(
+            ROUTES.SEARCH_ROOT.getRoute({
+                query: buildQueryStringFromFilterFormValues({
+                    type: CONST.SEARCH.DATA_TYPES.EXPENSE_REPORT,
+                    action: CONST.SEARCH.ACTION_FILTERS.PAY,
+                    reimbursable: CONST.SEARCH.BOOLEAN.YES,
+                    payer: accountID?.toString(),
+                }),
+            }),
+        );
+    };
+
+    const handleGoToExportSearch = () => {
+        Navigation.navigate(
+            ROUTES.SEARCH_ROOT.getRoute({
+                query: buildQueryStringFromFilterFormValues({
+                    type: CONST.SEARCH.DATA_TYPES.EXPENSE_REPORT,
+                    action: CONST.SEARCH.ACTION_FILTERS.EXPORT,
+                    exporter: [`${accountID}`],
+                    exportedOn: CONST.SEARCH.DATE_PRESETS.NEVER,
+                }),
+            }),
+        );
+    };
 
     return (
         <ScreenWrapper
@@ -25,7 +84,30 @@ function NewDotHomePage() {
                 )
             }
         >
-            <View style={styles.flex1} />
+            <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
+                <Button
+                    text="Go to submitted expense reports"
+                    onPress={handleGoToSearch}
+                />
+                <View style={styles.mt3}>
+                    <Button
+                        text="Go to expense reports to approve"
+                        onPress={handleGoToApproveSearch}
+                    />
+                </View>
+                <View style={styles.mt3}>
+                    <Button
+                        text="Go to expense reports to pay"
+                        onPress={handleGoToPaySearch}
+                    />
+                </View>
+                <View style={styles.mt3}>
+                    <Button
+                        text="Go to expense reports to export"
+                        onPress={handleGoToExportSearch}
+                    />
+                </View>
+            </View>
         </ScreenWrapper>
     );
 }
