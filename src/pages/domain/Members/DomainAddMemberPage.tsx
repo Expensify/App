a37da53e264f3e1c7d320df8d5deb2b709d99d7d
war-in <war-in@users.noreply.github.com>
2026-01-21 15:42:53 +0100

    feat: don't allow inviting a member

--- src/pages/domain/Members/DomainAddMemberPage.tsx
+++ src/pages/domain/Members/DomainAddMemberPage.tsx
@@ -9,6 +9,7 @@ import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import Navigation from '@libs/Navigation/Navigation';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
+import {getPersonalDetailsByIDs} from '@libs/PersonalDetailsUtils';
 import {isValidEmail} from '@libs/ValidationUtils';
 import type {SettingsNavigatorParamList} from '@navigation/types';
 import DomainNotFoundPageWrapper from '@pages/domain/DomainNotFoundPageWrapper';
@@ -17,7 +18,8 @@ import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
-import {domainNameSelector} from '@src/selectors/Domain';
+import {domainNameSelector, memberAccountIDsSelector} from '@src/selectors/Domain';
+import getEmptyArray from '@src/types/utils/getEmptyArray';
 
 type DomainAddMemberProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.DOMAIN.ADD_MEMBER>;
 
@@ -27,19 +29,38 @@ function DomainAddMemberPage({route}: DomainAddMemberProps) {
 
     const {domainAccountID} = route.params;
 
+    const [memberIDs = getEmptyArray<number>()] = useOnyx(`${ONYXKEYS.COLLECTION.DOMAIN}${domainAccountID}`, {
+        canBeMissing: true,
+        selector: memberAccountIDsSelector,
+    });
+    const personalDetails = getPersonalDetailsByIDs({accountIDs: memberIDs});
     const [domainName] = useOnyx(`${ONYXKEYS.COLLECTION.DOMAIN}${domainAccountID}`, {canBeMissing: false, selector: domainNameSelector});
-    const [email, setEmail] = useState<string | undefined>();
 
-    const isEmailInvalid = !!domainName && !!email && !isValidEmail(`${email}@${domainName}`);
-    const isSubmitDisabled = !domainName || !email || isEmailInvalid;
+    const [email, setEmail] = useState<string | undefined>();
+    const fullEmail = `${email}@${domainName}`;
+
+    const isUserAlreadyAMember = !!email && personalDetails.some(({login}) => login?.toLowerCase() === fullEmail.toLowerCase());
+    const isEmailInvalid = !!domainName && !!email && !isValidEmail(fullEmail);
+    const isSubmitDisabled = !domainName || !email || isEmailInvalid || isUserAlreadyAMember;
 
     const handleSubmit = () => {
         if (isSubmitDisabled) {
             return;
         }
 
-        addMemberToDomain(domainAccountID, `${email}@${domainName}`);
+        addMemberToDomain(domainAccountID, fullEmail);
         Navigation.dismissModal();
+    };
+
+    const getErrorText = () => {
+        if (isEmailInvalid) {
+            return translate('messages.errorMessageInvalidEmail');
+        }
+        if (isUserAlreadyAMember && !!domainName) {
+            return translate('messages.userIsAlreadyMember', {login: fullEmail, name: domainName});
+        }
+
+        return undefined;
     };
 
     return (
@@ -67,8 +88,8 @@ function DomainAddMemberPage({route}: DomainAddMemberProps) {
                         autoFocus
                         suffixCharacter={domainName ? `@${domainName}` : undefined}
                         suffixStyle={styles.colorMuted}
-                        hasError={isEmailInvalid}
-                        errorText={isEmailInvalid ? translate('messages.errorMessageInvalidEmail') : undefined}
+                        hasError={isEmailInvalid || isUserAlreadyAMember}
+                        errorText={getErrorText()}
                     />
                 </View>
 
