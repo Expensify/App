0352121449928740f22a882fed7a0ef635218df7
Faizan Shoukat Abbasi <abbasi.faizan1997@gmail.com>
2026-01-22 02:29:52 +0500

    fixed eslintissues

--- src/pages/ReportParticipantDetailsPage.tsx
+++ src/pages/ReportParticipantDetailsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {View} from 'react-native';
 import Avatar from '@components/Avatar';
 import Button from '@components/Button';
