32d868ad1a09d3f575a99b7da919aaa358911ef7
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-01-21 10:37:00 +0800

    fallback to child report name

--- src/components/ReportActionItem/MoneyRequestReportPreview/MoneyRequestReportPreviewContent.tsx
+++ src/components/ReportActionItem/MoneyRequestReportPreview/MoneyRequestReportPreviewContent.tsx
@@ -732,7 +732,7 @@ function MoneyRequestReportPreviewContent({
                                                         >
                                                             {/* This will be fixed as follow up https://github.com/Expensify/App/pull/75357 */}
                                                             {/* eslint-disable-next-line @typescript-eslint/no-deprecated */}
-                                                            {getReportName(iouReport, undefined, undefined, undefined, undefined, reportAttributes)}
+                                                            {getReportName(iouReport, undefined, undefined, undefined, undefined, reportAttributes) || action.childReportName}
                                                         </Text>
                                                     </Animated.View>
                                                 </View>
