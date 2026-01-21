e22baf704791d332ea22fcd7e7e216212df97519
Zuzanna Furtak <zuzkafurtak@gmail.com>
2026-01-21 10:54:13 +0100

    Extract common behaviour to useWebSelectionListBehaviour

--- src/components/SelectionList/SelectionListWithSections/index.tsx
+++ src/components/SelectionList/SelectionListWithSections/index.tsx
@@ -1,55 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {isMobileChrome} from '@libs/Browser';
-import {canUseTouchScreen} from '@libs/DeviceCapabilities';
-import CONST from '@src/CONST';
+import React from 'react';
+import useWebSelectionListBehavior from '@components/SelectionList/hooks/useWebSelectionListBehavior';
 import BaseSelectionList from './BaseSelectionListWithSections';
 import type {ListItem, SelectionListWithSectionsProps} from './types';
 
 function SelectionList<TItem extends ListItem>({ref, ...props}: SelectionListWithSectionsProps<TItem>) {
-    const [isScreenTouched, setIsScreenTouched] = useState(false);
-    const [shouldDebounceScrolling, setShouldDebounceScrolling] = useState(false);
-
-    const touchStart = () => setIsScreenTouched(true);
-    const touchEnd = () => setIsScreenTouched(false);
-
-    useEffect(() => {
-        if (!canUseTouchScreen()) {
-            return;
-        }
-        // We're setting `isScreenTouched` in this listener only for web platforms with touchscreen (mWeb) where
-        // we want to dismiss the keyboard only when the list is scrolled by the user and not when it's scrolled programmatically.
-        document.addEventListener('touchstart', touchStart);
-        document.addEventListener('touchend', touchEnd);
-
-        return () => {
-            document.removeEventListener('touchstart', touchStart);
-            document.removeEventListener('touchend', touchEnd);
-        };
-    }, []);
-
-    const handleKeyboardScrollDebounce = (event: KeyboardEvent) => {
-        if (!event) {
-            return;
-        }
-        // Moving through items using the keyboard triggers scrolling by the browser, so we debounce programmatic scrolling to prevent jittering.
-        if (
-            event.key === CONST.KEYBOARD_SHORTCUTS.ARROW_DOWN.shortcutKey ||
-            event.key === CONST.KEYBOARD_SHORTCUTS.ARROW_UP.shortcutKey ||
-            event.key === CONST.KEYBOARD_SHORTCUTS.TAB.shortcutKey
-        ) {
-            setShouldDebounceScrolling(event.type === 'keydown');
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('keydown', handleKeyboardScrollDebounce, {passive: true});
-        document.addEventListener('keyup', handleKeyboardScrollDebounce, {passive: true});
-
-        return () => {
-            document.removeEventListener('keydown', handleKeyboardScrollDebounce);
-            document.removeEventListener('keyup', handleKeyboardScrollDebounce);
-        };
-    }, []);
+    const {shouldIgnoreFocus, shouldDebounceScrolling} = useWebSelectionListBehavior();
 
     return (
         <BaseSelectionList
@@ -58,7 +13,7 @@ function SelectionList<TItem extends ListItem>({ref, ...props}: SelectionListWit
             ref={ref}
             // Ignore the focus if it's caused by a touch event on mobile chrome.
             // For example, a long press will trigger a focus event on mobile chrome.
-            shouldIgnoreFocus={isMobileChrome() && isScreenTouched}
+            shouldIgnoreFocus={shouldIgnoreFocus}
             shouldDebounceScrolling={shouldDebounceScrolling}
         />
     );
