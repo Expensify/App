1d7df7d96c9d48852135825a46a6c41823e277d6
Zuzanna Furtak <zuzkafurtak@gmail.com>
2026-01-22 11:20:12 +0100

    Check if section is disabled

--- src/components/SelectionList/hooks/useFlattenedSections.ts
+++ src/components/SelectionList/hooks/useFlattenedSections.ts
@@ -62,6 +62,7 @@ function useFlattenedSections<TItem extends ListItem>(sections: Array<Section<TI
                 const itemData = {
                     ...item,
                     type: CONST.SECTION_LIST_ITEM_TYPE.ROW,
+                    isDisabled: section.isDisabled === true || item.isDisabled === true ,
                 } as SectionListItem<TItem>;
                 data.push(itemData);
 
