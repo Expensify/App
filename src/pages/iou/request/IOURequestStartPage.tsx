ff57717190bd29877ff0be4937b9f7623937a870
paulnjs <paulqn45@outlook.com>
2026-01-23 00:38:03 +0700

    fix: Tooltip Try it out doesn't appeared

--- src/pages/iou/request/IOURequestStartPage.tsx
+++ src/pages/iou/request/IOURequestStartPage.tsx
@@ -166,7 +166,7 @@ function IOURequestStartPage({
         setSelectedTab(lastSelectedTab);
         // We only want to set the selected tab when selectedTab is not set yet, don't want to run this effect again when lastSelectedTab changes
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isLoadingSelectedTab, selectedTab]);
+    }, [isLoadingSelectedTab, selectedTab, lastSelectedTab]);
 
     const navigateBack = () => {
         Navigation.closeRHPFlow();
