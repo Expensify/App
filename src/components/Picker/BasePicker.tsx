0c558b6dd7cf9c48f2d3fb1b0a3c2d07f03257b2
TaduJR <nattysec26@gmail.com>
2026-01-22 16:07:41 +0300

    chore: use isMobile() for picker tabIndex to correctly detect iPad

--- src/components/Picker/BasePicker.tsx
+++ src/components/Picker/BasePicker.tsx
@@ -12,6 +12,7 @@ import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useScrollContext from '@hooks/useScrollContext';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {isMobile} from '@libs/Browser';
 import getOperatingSystem from '@libs/getOperatingSystem';
 import CONST from '@src/CONST';
 import type {BasePickerProps} from './types';
@@ -159,9 +160,7 @@ function BasePicker<TPickerValue>({
 
     const hasError = !!errorText;
 
-    const currentOS = getOperatingSystem();
-    const isMobileOS = currentOS === CONST.OS.IOS || currentOS === CONST.OS.ANDROID;
-    const pickerTabIndex = isMobileOS ? -1 : 0;
+    const pickerTabIndex = isMobile() ? -1 : 0;
 
     if (isDisabled && shouldShowOnlyTextWhenDisabled) {
         return (
