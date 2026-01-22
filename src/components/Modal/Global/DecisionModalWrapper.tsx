c5b1537d70446c48b4b113ff6e3aae9e1131a546
rory <rory@expensify.com>
2026-01-22 15:40:18 -0800

    Add DecisionModalWrapper and useDecisionModal

---
+++ src/components/Modal/Global/DecisionModalWrapper.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import type {ValueOf} from 'type-fest';
+import DecisionModal from '@components/DecisionModal';
+import type {ModalProps} from './ModalContext';
+
+const DecisionModalActions = {
+    FIRST_OPTION: 'FIRST_OPTION',
+    SECOND_OPTION: 'SECOND_OPTION',
+    CLOSE: 'CLOSE',
+} as const;
+
+type DecisionModalAction = ValueOf<typeof DecisionModalActions>;
+
+type DecisionModalWrapperProps = ModalProps<DecisionModalAction> & Omit<React.ComponentProps<typeof DecisionModal>, 'isVisible' | 'onClose' | 'onModalHide'>;
+
+function DecisionModalWrapper({closeModal, ...props}: DecisionModalWrapperProps) {
+    const [isVisible, setIsVisible] = useState(true);
+    const [closeAction, setCloseAction] = useState<DecisionModalAction>(DecisionModalActions.CLOSE);
+
+    const handleFirstOption = () => {
+        setCloseAction(DecisionModalActions.FIRST_OPTION);
+        setIsVisible(false);
+    };
+
+    const handleSecondOption = () => {
+        setCloseAction(DecisionModalActions.SECOND_OPTION);
+        setIsVisible(false);
+    };
+
+    const handleClose = () => {
+        setCloseAction(DecisionModalActions.CLOSE);
+        setIsVisible(false);
+    };
+
+    const handleModalHide = () => {
+        if (isVisible) {
+            return;
+        }
+        closeModal({action: closeAction});
+    };
+
+    return (
+        <DecisionModal
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            {...props}
+            isVisible={isVisible}
+            onFirstOptionSubmit={props.firstOptionText ? handleFirstOption : undefined}
+            onSecondOptionSubmit={handleSecondOption}
+            onClose={handleClose}
+            onModalHide={handleModalHide}
+        />
+    );
+}
+
+export default DecisionModalWrapper;
+export {DecisionModalActions};
+export type {DecisionModalAction};
