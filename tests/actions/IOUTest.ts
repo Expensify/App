e6e6274d9c42481062e0ed8859301d5441f54ee5
Wiktor Gut <wiktor.gut@swmansion.com>
2026-01-24 14:21:09 +0100

    fix spell check 2

--- tests/actions/IOUTest.ts
+++ tests/actions/IOUTest.ts
@@ -1633,9 +1633,9 @@ describe('actions/IOU', () => {
                     const connection = Onyx.connect({
                         key: ONYXKEYS.COLLECTION.REPORT,
                         waitForCollectionCallback: true,
-                        callback: (rpts) => {
+                        callback: (reports) => {
                             Onyx.disconnect(connection);
-                            resolve(rpts);
+                            resolve(reports);
                         },
                     });
                 });
