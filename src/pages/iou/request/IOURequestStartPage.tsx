a54cf092f2b03c161e4f8bd2a6a316d518e19280
paulnjs <paulqn45@outlook.com>
2026-01-26 17:22:00 +0700

    update code

--- src/pages/iou/request/IOURequestStartPage.tsx
+++ src/pages/iou/request/IOURequestStartPage.tsx
@@ -164,8 +164,6 @@ function IOURequestStartPage({
             return;
         }
         setSelectedTab(lastSelectedTab);
-        // We only want to set the selected tab when selectedTab is not set yet, don't want to run this effect again when lastSelectedTab changes
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isLoadingSelectedTab, selectedTab, lastSelectedTab]);
 
     const navigateBack = () => {
