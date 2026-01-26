41c338c0e5e2fdc1aeb6ff93deadedca48b66af0
Mateusz Rajski <mateusz.rajski@swmansion.com>
2026-01-26 13:55:51 -0800

    Add view parameter to search query grammar

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -7063,6 +7063,13 @@ const CONST = {
             SORT_ORDER: 'sortOrder',
             GROUP_BY: 'groupBy',
             COLUMNS: 'columns',
+            VIEW: 'view',
+        },
+        VIEW: {
+            TABLE: 'table',
+            BAR: 'bar',
+            LINE: 'line',
+            PIE: 'pie',
         },
         SYNTAX_FILTER_KEYS: {
             TYPE: 'type',
