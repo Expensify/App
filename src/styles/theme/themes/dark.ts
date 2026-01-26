121d40f50cb9a030c97ab2665b98990f0cdbd534
Adam Grzybowski <adam.grzybowski@swmansion.com>
2026-01-26 11:31:37 -0800

    add home in lights and darks

--- src/styles/theme/themes/dark.ts
+++ src/styles/theme/themes/dark.ts
@@ -112,6 +112,10 @@ const darkTheme = {
     // The route urls from ROUTES.ts are only used for deep linking and configuring URLs on web.
     // The screen name (see SCREENS.ts) is the name of the screen as far as react-navigation is concerned, and the linkingConfig maps screen names to URLs
     PAGE_THEMES: {
+        [SCREENS.HOME]: {
+            backgroundColor: colors.productDark100,
+            statusBarStyle: CONST.STATUS_BAR_STYLE.LIGHT_CONTENT,
+        },
         [SCREENS.INBOX]: {
             backgroundColor: colors.productDark100,
             statusBarStyle: CONST.STATUS_BAR_STYLE.LIGHT_CONTENT,
