0bdb25dd2ba531c6e3d8104001a7b13b55ef82c7
Adam Horodyski <ad.horodyski@gmail.com>
2026-01-27 12:05:44 -0800

    cleanup all firebase perf deps and utilities no longer in use

--- src/libs/API/types.ts
+++ src/libs/API/types.ts
@@ -1133,7 +1133,6 @@ const READ_COMMANDS = {
     OPEN_ROOM_MEMBERS_PAGE: 'OpenRoomMembersPage',
     SEARCH_FOR_REPORTS: 'SearchForReports',
     SEARCH_FOR_ROOMS_TO_MENTION: 'SearchForRoomsToMention',
-    SEND_PERFORMANCE_TIMING: 'SendPerformanceTiming',
     GRAPHITE: 'Graphite',
     GET_ROUTE: 'GetRoute',
     GET_ROUTE_FOR_DRAFT: 'GetRouteForDraft',
@@ -1216,7 +1215,6 @@ type ReadCommandParameters = {
     [READ_COMMANDS.OPEN_ROOM_MEMBERS_PAGE]: Parameters.OpenRoomMembersPageParams;
     [READ_COMMANDS.SEARCH_FOR_REPORTS]: Parameters.SearchForReportsParams;
     [READ_COMMANDS.SEARCH_FOR_ROOMS_TO_MENTION]: Parameters.SearchForRoomsToMentionParams;
-    [READ_COMMANDS.SEND_PERFORMANCE_TIMING]: Parameters.SendPerformanceTimingParams;
     [READ_COMMANDS.GRAPHITE]: Parameters.GraphiteParams;
     [READ_COMMANDS.GET_ROUTE]: Parameters.GetRouteParams;
     [READ_COMMANDS.GET_ROUTE_FOR_DRAFT]: Parameters.GetRouteParams;
--- src/libs/Firebase/types.ts
+++ src/libs/Firebase/types.ts
@@ -1,25 +1,3 @@
-import type {PerformanceTrace} from '@firebase/performance';
-import type {FirebasePerformanceTypes} from '@react-native-firebase/perf';
-
-type Trace = {
-    trace: FirebasePerformanceTypes.Trace | PerformanceTrace;
-    start: number;
-};
-type TraceMap = Record<string, Trace>;
-type StartTrace = (customEventName: string) => void;
-type StopTrace = (customEventName: string) => void;
 type Log = (action: string) => void;
-type PerfAttributes = {
-    accountId: string;
-    personalDetailsLength: string;
-    reportsLength: string;
-    reportActionsLength: string;
-    transactionViolationsLength: string;
-    policiesLength: string;
-    transactionsLength: string;
-};
 
-// TODO confirm which attributes are required for Firebase
-type FirebaseAttributes = Pick<PerfAttributes, 'accountId' | 'personalDetailsLength' | 'reportActionsLength' | 'reportsLength' | 'policiesLength'>;
-
-export type {StartTrace, StopTrace, TraceMap, Log, PerfAttributes, FirebaseAttributes};
+export type {Log};
