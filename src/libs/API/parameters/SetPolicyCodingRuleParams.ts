4d0957724542a0cfdd14a605f76b48962d0f30ac
Carlos Martins <cmartins@expensify.com>
2026-01-28 10:40:46 -0800

    Update src/libs/API/parameters/SetPolicyCodingRuleParams.ts
    Co-authored-by: Carlos Alvarez <carlos@expensify.com>

--- src/libs/API/parameters/SetPolicyCodingRuleParams.ts
+++ src/libs/API/parameters/SetPolicyCodingRuleParams.ts
@@ -2,7 +2,7 @@ type SetPolicyCodingRuleParams = {
     /** The policy ID that the rule will be created or updated for */
     policyID: string;
 
-    /** The existing ruleID, or an optimistic one to create the rule */
+    /** The existing codingRuleID, or an optimistic one to create the rule */
     codingRuleID: string;
 
     /** The JSON value of the merchant rule, stringified */
