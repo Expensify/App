e94bafd33811678faf56893a0b1df0eae74bb1b4
Jack Senyitko <jsenyitko@expensify.com>
2026-01-28 08:19:24 -0800

    add page

--- src/pages/workspace/rules/MerchantRules/PreviewMatchesPage.tsx
+++ src/pages/workspace/rules/MerchantRules/PreviewMatchesPage.tsx
@@ -1,6 +1,20 @@
-type PreviewMatchesPageProps = {};
+import {useEffect} from 'react';
+import {OnyxEntry} from 'react-native-onyx';
+import useOnyx from '@hooks/useOnyx';
+import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
+import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
+import ONYXKEYS from '@src/ONYXKEYS';
+import type SCREENS from '@src/SCREENS';
+import {MerchantRuleForm} from '@src/types/form';
 
-function PreviewMatchesPage({}: PreviewMatchesPageProps) {
+type PreviewMatchesPageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.WORKSPACE.RULES_MERCHANT_PREVIEW_MATCHES>;
+
+const merchantRuleFormSelector = (form: OnyxEntry<MerchantRuleForm>) => form?.merchantToMatch ?? '';
+
+function PreviewMatchesPage({route}: PreviewMatchesPageProps) {
+    const policyID = route.params.policyID;
+    const [merchant = ''] = useOnyx(ONYXKEYS.FORMS.MERCHANT_RULE_FORM, {canBeMissing: true, selector: merchantRuleFormSelector});
+
     return <></>;
 }
 
