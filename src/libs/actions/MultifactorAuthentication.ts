da873a99ed28768b73895aeb52c2a5c4e4b6b0d9
Chuck Dries <chuck@expensify.com>
2026-01-28 08:56:24 -0800

    Implement requested changes on MFA Revoke flow

--- src/libs/actions/MultifactorAuthentication.ts
+++ src/libs/actions/MultifactorAuthentication.ts
@@ -1,7 +1,7 @@
 /* eslint-disable rulesdir/no-api-side-effects-method */
 // These functions use makeRequestWithSideEffects because challenge data must be returned immediately
 // for security and timing requirements (see detailed explanation below)
-import Onyx from 'react-native-onyx';
+import Onyx, { OnyxUpdate } from 'react-native-onyx';
 import type {MultifactorAuthenticationScenarioParameters} from '@components/MultifactorAuthentication/config/types';
 import {makeRequestWithSideEffects} from '@libs/API';
 import {SIDE_EFFECT_REQUEST_COMMANDS} from '@libs/API/types';
@@ -73,17 +73,41 @@ async function troubleshootMultifactorAuthentication({signedChallenge}: Multifac
 }
 
 async function revokeMultifactorAuthenticationCredentials() {
+    const optimisticData: Array<OnyxUpdate<typeof ONYXKEYS.ACCOUNT>> = [
+        {
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: ONYXKEYS.ACCOUNT,
+            value: {
+                isLoading: true
+            }
+        }
+    ]
+    const successData: Array<OnyxUpdate<typeof ONYXKEYS.ACCOUNT>> = [
+        {
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: ONYXKEYS.ACCOUNT,
+            value: {
+                isLoading: false
+            }
+        }
+    ]
+    const failureData: Array<OnyxUpdate<typeof ONYXKEYS.ACCOUNT>> = [
+        {
+            onyxMethod: Onyx.METHOD.MERGE,
+            key: ONYXKEYS.ACCOUNT,
+            value: {
+                isLoading: false
+            }
+        }
+    ]
     try {
-        Onyx.merge(ONYXKEYS.ACCOUNT, {isLoading: true});
-        const response = await makeRequestWithSideEffects(SIDE_EFFECT_REQUEST_COMMANDS.REVOKE_MULTIFACTOR_AUTHENTICATION_CREDENTIALS, {});
-        Onyx.merge(ONYXKEYS.ACCOUNT, {isLoading: false});
+        const response = await makeRequestWithSideEffects(SIDE_EFFECT_REQUEST_COMMANDS.REVOKE_MULTIFACTOR_AUTHENTICATION_CREDENTIALS, {}, { optimisticData, successData, failureData });
 
         const {jsonCode, message} = response ?? {};
 
         return parseHttpRequest(jsonCode, CONST.MULTIFACTOR_AUTHENTICATION.API_RESPONSE_MAP.REVOKE_MULTIFACTOR_AUTHENTICATION_SETUP, message);
     } catch (error) {
         Log.hmmm('[MultifactorAuthentication] Failed to revoke multifactor authentication credentials', {error});
-        Onyx.merge(ONYXKEYS.ACCOUNT, {isLoading: false});
         return parseHttpRequest(undefined, CONST.MULTIFACTOR_AUTHENTICATION.API_RESPONSE_MAP.REVOKE_MULTIFACTOR_AUTHENTICATION_SETUP, undefined);
     }
 }
