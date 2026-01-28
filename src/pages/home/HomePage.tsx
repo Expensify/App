3e250e8b19e4d5720430c94e9bab077bd67bfadf
Adam Grzybowski <adam.grzybowski@swmansion.com>
2026-01-28 13:28:38 -0800

    fix home page loading

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View} from 'react-native';
 import NavigationTabBar from '@components/Navigation/NavigationTabBar';
 import NAVIGATION_TABS from '@components/Navigation/NavigationTabBar/NAVIGATION_TABS';
@@ -6,6 +6,8 @@ import ScreenWrapper from '@components/ScreenWrapper';
 import ScrollView from '@components/ScrollView';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
+import {confirmReadyToOpenApp} from '@libs/actions/App';
+import usePreloadFullScreenNavigators from '@libs/Navigation/AppNavigator/usePreloadFullScreenNavigators';
 import TopBar from '@components/Navigation/TopBar';
 import useLocalize from '@hooks/useLocalize';
 import AnnouncementsSection from './AnnouncementsSection';
@@ -17,6 +19,13 @@ function HomePage() {
     const shouldDisplayLHB = !shouldUseNarrowLayout;
     const styles = useThemeStyles();
     const {translate} = useLocalize();
+
+    useEffect(() => {
+        confirmReadyToOpenApp();
+    }, []);
+
+    // This hook preloads the screens of adjacent tabs to make changing tabs faster.
+    usePreloadFullScreenNavigators();
 
     return (
         <ScreenWrapper
