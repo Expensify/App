eabf74fe4f20e41b879bf2e1f89e88d04b7c546e
Mateusz Rajski <mateusz.rajski@swmansion.com>
2026-01-29 07:31:37 -0800

    Fix font path to use alias

--- src/components/Charts/font/index.native.ts
+++ src/components/Charts/font/index.native.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const fontSource = require('../../../../assets/fonts/native/ExpensifyNeue-Regular.otf');
+const fontSource = require('@assets/fonts/native/ExpensifyNeue-Regular.otf');
 
 export default fontSource;
