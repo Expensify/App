5bbdbc53f0bb54d620202ca06c60c691098ba230
Artem Makushov <waterim3009@gmail.com>
2026-01-29 18:54:36 +0100

    split personalDetails

--- src/pages/settings/Wallet/PersonalCardDetailsPage.tsx
+++ src/pages/settings/Wallet/PersonalCardDetailsPage.tsx
@@ -1,15 +1,9 @@
-import {format, parseISO} from 'date-fns';
+import {format} from 'date-fns';
 import React, {useState} from 'react';
 import {View} from 'react-native';
-import ActivityIndicator from '@components/ActivityIndicator';
 import ConfirmModal from '@components/ConfirmModal';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
-// eslint-disable-next-line no-restricted-imports
-import * as Expensicons from '@components/Icon/Expensicons';
 import ImageSVG from '@components/ImageSVG';
-import MenuItem from '@components/MenuItem';
-import MenuItemWithTopDescription from '@components/MenuItemWithTopDescription';
-import OfflineWithFeedback from '@components/OfflineWithFeedback';
 import ScreenWrapper from '@components/ScreenWrapper';
 import ScrollView from '@components/ScrollView';
 import {useCompanyCardFeedIcons} from '@hooks/useCompanyCardIcons';
@@ -19,23 +13,21 @@ import useNetwork from '@hooks/useNetwork';
 import useOnyx from '@hooks/useOnyx';
 import useThemeIllustrations from '@hooks/useThemeIllustrations';
 import useThemeStyles from '@hooks/useThemeStyles';
-import {getCardFeedIcon, getDefaultCardName, isPersonalCard} from '@libs/CardUtils';
-import {getLatestErrorField} from '@libs/ErrorUtils';
+import {getCardFeedIcon, isPersonalCard} from '@libs/CardUtils';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
 import {getDisplayNameOrDefault} from '@libs/PersonalDetailsUtils';
-import {buildCannedSearchQuery} from '@libs/SearchQueryUtils';
 import Navigation from '@navigation/Navigation';
 import NotFoundPage from '@pages/ErrorPage/NotFoundPage';
-import ToggleSettingOptionRow from '@pages/workspace/workflows/ToggleSettingsOptionRow';
 import variables from '@styles/variables';
-import {clearCardErrorField, clearCardNameValuePairsErrorField, setPersonalCardReimbursable, syncCard, unassignCard} from '@userActions/Card';
+import {syncCard, unassignCard} from '@userActions/Card';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
 import type {CompanyCardFeed} from '@src/types/onyx';
 import isLoadingOnyxValue from '@src/types/utils/isLoadingOnyxValue';
+import PersonalCardDetailsHeaderMenu from './PersonalCardDetailsHeaderMenu';
 
 type PersonalCardDetailsPageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.SETTINGS.WALLET.PERSONAL_CARD_DETAILS>;
 
@@ -116,126 +108,22 @@ function PersonalCardDetailsPage({route}: PersonalCardDetailsPageProps) {
                         width={variables.cardPreviewWidth}
                     />
                 </View>
-                {!cardholder?.validated && (
-                    <MenuItem
-                        icon={Expensicons.Hourglass}
-                        iconStyles={styles.mln2}
-                        description={translate('workspace.expensifyCard.cardPending', {name: displayName})}
-                        numberOfLinesDescription={0}
-                        interactive={false}
+                {!!card && (
+                    <PersonalCardDetailsHeaderMenu
+                        card={card}
+                        cardID={cardID}
+                        cardholder={cardholder}
+                        displayName={displayName}
+                        customCardNames={customCardNames}
+                        expensifyIcons={expensifyIcons}
+                        isCSVImportedPersonalCard={isCSVImportedPersonalCard}
+                        reimbursableSetting={reimbursableSetting}
+                        lastScrape={lastScrape}
+                        isOffline={isOffline}
+                        onUpdateCard={updateCard}
+                        onUnassignCard={() => setIsUnassignModalVisible(true)}
                     />
                 )}
-
-                <MenuItem
-                    label={translate('workspace.moreFeatures.companyCards.cardholder')}
-                    title={displayName}
-                    titleStyle={styles.mt1}
-                    iconStyles={styles.mt1}
-                    icon={cardholder?.avatar ?? expensifyIcons.FallbackAvatar}
-                    iconType={CONST.ICON_TYPE_AVATAR}
-                    description={cardholder?.login ?? ''}
-                    interactive={false}
-                />
-                <OfflineWithFeedback
-                    pendingAction={card?.nameValuePairs?.pendingFields?.cardTitle}
-                    errorRowStyles={[styles.ph5, styles.mb3]}
-                    errors={getLatestErrorField(card?.nameValuePairs ?? {}, 'cardTitle')}
-                    onClose={() => {
-                        if (!card) {
-                            return;
-                        }
-                        clearCardNameValuePairsErrorField(card.cardID, 'cardTitle');
-                    }}
-                >
-                    <MenuItemWithTopDescription
-                        description={translate('workspace.moreFeatures.companyCards.cardNumber')}
-                        title={customCardNames?.[cardID] ?? getDefaultCardName(cardholder?.firstName)}
-                        shouldShowRightIcon
-                        brickRoadIndicator={card?.nameValuePairs?.errorFields?.cardTitle ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : undefined}
-                        onPress={() => Navigation.navigate(ROUTES.SETTINGS_WALLET_PERSONAL_CARD_EDIT_NAME.getRoute(cardID))}
-                    />
-                </OfflineWithFeedback>
-
-                <ToggleSettingOptionRow
-                    title={translate('cardPage.markTransactionsAsReimbursable')}
-                    subtitle={translate('cardPage.markTransactionsDescription')}
-                    shouldPlaceSubtitleBelowSwitch
-                    switchAccessibilityLabel={translate('cardPage.markTransactionsAsReimbursable')}
-                    isActive={reimbursableSetting}
-                    onToggle={(isOn) => card && setPersonalCardReimbursable(card.cardID, isOn, reimbursableSetting)}
-                    pendingAction={card?.pendingFields?.reimbursable}
-                    errors={card?.errorFields?.reimbursable ?? undefined}
-                    onCloseError={() => card && clearCardErrorField(card.cardID, 'markTransactionsAsReimbursable')}
-                    wrapperStyle={[styles.ph5, styles.mb3]}
-                />
-
-                <MenuItemWithTopDescription
-                    shouldShowRightComponent={card?.isLoadingLastUpdated}
-                    rightComponent={<ActivityIndicator style={[styles.popoverMenuIcon]} />}
-                    description={translate('workspace.moreFeatures.companyCards.lastUpdated')}
-                    title={card?.isLoadingLastUpdated ? translate('workspace.moreFeatures.companyCards.updating') : lastScrape}
-                    interactive={false}
-                />
-                {!isCSVImportedPersonalCard && (
-                    <OfflineWithFeedback
-                        pendingAction={card?.pendingFields?.scrapeMinDate}
-                        errorRowStyles={[styles.ph5, styles.mb3]}
-                        errors={getLatestErrorField(card ?? {}, 'scrapeMinDate')}
-                        onClose={() => {
-                            if (!card) {
-                                return;
-                            }
-                            clearCardErrorField(card.cardID, 'scrapeMinDate');
-                        }}
-                    >
-                        <MenuItemWithTopDescription
-                            description={translate('workspace.moreFeatures.companyCards.transactionStartDate')}
-                            title={card?.scrapeMinDate ? format(parseISO(card.scrapeMinDate), CONST.DATE.FNS_FORMAT_STRING) : ''}
-                            shouldShowRightIcon
-                            brickRoadIndicator={card?.errorFields?.scrapeMinDate ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : undefined}
-                            onPress={() => Navigation.navigate(ROUTES.SETTINGS_WALLET_PERSONAL_CARD_EDIT_TRANSACTION_START_DATE.getRoute(cardID))}
-                        />
-                    </OfflineWithFeedback>
-                )}
-                <MenuItem
-                    icon={expensifyIcons.MoneySearch}
-                    title={translate('workspace.common.viewTransactions')}
-                    style={styles.mt3}
-                    onPress={() => {
-                        Navigation.navigate(
-                            ROUTES.SEARCH_ROOT.getRoute({
-                                query: buildCannedSearchQuery({type: CONST.SEARCH.DATA_TYPES.EXPENSE, status: CONST.SEARCH.STATUS.EXPENSE.ALL, cardID}),
-                            }),
-                        );
-                    }}
-                />
-                {!isCSVImportedPersonalCard && (
-                    <OfflineWithFeedback
-                        pendingAction={card?.pendingFields?.lastScrape}
-                        errorRowStyles={[styles.ph5, styles.mb3]}
-                        errors={getLatestErrorField(card ?? {}, 'lastScrape')}
-                        onClose={() => {
-                            if (!card) {
-                                return;
-                            }
-                            clearCardErrorField(card.cardID, 'lastScrape');
-                        }}
-                    >
-                        <MenuItem
-                            icon={expensifyIcons.Sync}
-                            disabled={isOffline || card?.isLoadingLastUpdated}
-                            title={translate('workspace.moreFeatures.companyCards.updateCard')}
-                            brickRoadIndicator={card?.errorFields?.lastScrape ? CONST.BRICK_ROAD_INDICATOR_STATUS.ERROR : undefined}
-                            onPress={updateCard}
-                        />
-                    </OfflineWithFeedback>
-                )}
-                <MenuItem
-                    icon={expensifyIcons.RemoveMembers}
-                    title={translate('workspace.moreFeatures.companyCards.unassignCard')}
-                    style={styles.mb1}
-                    onPress={() => setIsUnassignModalVisible(true)}
-                />
                 <ConfirmModal
                     title={translate('workspace.moreFeatures.companyCards.unassignCard')}
                     isVisible={isUnassignModalVisible}
