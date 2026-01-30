b9f06cdab02055cf6706ce071b6e7b0e2a94ff14
borys3kk <szymon.banys01@wp.pl>
2026-01-30 15:59:47 +0100

    make pieChart/index.tsx react compiler compliant

--- src/components/Charts/PieChart/index.tsx
+++ src/components/Charts/PieChart/index.tsx
@@ -1,28 +1,23 @@
-import {WithSkiaWeb} from '@shopify/react-native-skia/lib/module/web';
+import { WithSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import ActivityIndicator from '@components/ActivityIndicator';
-import type {PieChartProps} from '@components/Charts/types';
-import colors from '@styles/theme/colors';
+import type { PieChartProps } from '@components/Charts/types';
+import useThemeStyles from '@hooks/useThemeStyles';
 
 const getPieChartContent = () => import('./PieChartContent');
 
 function PieChart(props: PieChartProps) {
+    const styles = useThemeStyles();
+
     return (
         <WithSkiaWeb
-            opts={{locateFile: (file: string) => `/${file}`}}
+            opts={{ locateFile: (file: string) => `/${file}` }}
             getComponent={getPieChartContent}
             componentProps={props}
             fallback={
                 <View
-                    style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: colors.productLight200,
-                        borderRadius: 16,
-                        padding: 20,
-                    }}
+                    style={[styles.flex1, styles.justifyContentCenter, styles.alignItemsCenter, styles.highlightBG, styles.br4, styles.p5]}
                 >
                     <ActivityIndicator size="large" />
                 </View>
