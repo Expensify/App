7488015032b7123819d98d69260757c26f847077
alberto <alberto@expensify.com>
2026-01-29 17:45:12 -0800

    new pattern

--- src/pages/settings/Wallet/WalletPage/index.tsx
+++ src/pages/settings/Wallet/WalletPage/index.tsx
@@ -466,7 +466,7 @@ function WalletPage() {
                       {
                           text: translate('common.delete'),
                           icon: icons.Trashcan,
-                          onSelected: confirmDeleteCard,
+                          onSelected: () => closeModal(confirmDeleteCard),
                       },
                   ]
                 : []),
