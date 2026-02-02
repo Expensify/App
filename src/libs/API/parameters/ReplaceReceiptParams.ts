15187475dd40b15cc8afaccae39183851d61e54b
aimane-chnaif <aiman.chnaif@gmail.com>
2026-02-02 11:37:49 +0100

    fix lint

--- src/libs/API/parameters/ReplaceReceiptParams.ts
+++ src/libs/API/parameters/ReplaceReceiptParams.ts
@@ -1,5 +1,5 @@
-import {ValueOf} from 'type-fest';
-import CONST from '@src/CONST';
+import type {ValueOf} from 'type-fest';
+import type CONST from '@src/CONST';
 
 type ReplaceReceiptParams = {
     transactionID: string;
