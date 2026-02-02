0996881e51f1c80cdca2965d552204fa01d99571
Zuzanna Furtak <zuzkafurtak@gmail.com>
2026-02-02 20:47:29 +0100

    Remove manual memoization

--- src/components/CurrencySelectionList/index.tsx
+++ src/components/CurrencySelectionList/index.tsx
@@ -1,5 +1,5 @@
 import {Str} from 'expensify-common';
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useState} from 'react';
 import RadioListItem from '@components/SelectionList/ListItem/RadioListItem';
 import SelectionListWithSections from '@components/SelectionList/SelectionListWithSections';
 import useCurrencyList from '@hooks/useCurrencyList';
@@ -21,82 +21,79 @@ function CurrencySelectionList({
     const {currencyList, getCurrencySymbol} = useCurrencyList();
     const [searchValue, setSearchValue] = useState('');
     const {translate} = useLocalize();
-    const getUnselectedOptions = useCallback((options: CurrencyListItem[]) => options.filter((option) => !option.isSelected), []);
-    const {sections, headerMessage} = useMemo(() => {
-        const currencyOptions: CurrencyListItem[] = Object.entries(currencyList).reduce((acc, [currencyCode, currencyInfo]) => {
-            const isSelectedCurrency = currencyCode === initiallySelectedCurrencyCode || selectedCurrencies.includes(currencyCode);
-            if (!excludedCurrencies.includes(currencyCode) && (isSelectedCurrency || !currencyInfo?.retired)) {
-                acc.push({
-                    currencyName: currencyInfo?.name ?? '',
-                    text: `${currencyCode} - ${getCurrencySymbol(currencyCode)}`,
-                    currencyCode,
-                    keyForList: currencyCode,
-                    isSelected: isSelectedCurrency,
-                });
-            }
-            return acc;
-        }, [] as CurrencyListItem[]);
+    const getUnselectedOptions = (options: CurrencyListItem[]) => options.filter((option) => !option.isSelected);
 
-        const recentlyUsedCurrencyOptions: CurrencyListItem[] = Array.isArray(recentlyUsedCurrencies)
-            ? recentlyUsedCurrencies?.map((currencyCode) => {
-                  const currencyInfo = currencyList?.[currencyCode];
-                  const isSelectedCurrency = currencyCode === initiallySelectedCurrencyCode;
-                  return {
-                      currencyName: currencyInfo?.name ?? '',
-                      text: `${currencyCode} - ${getCurrencySymbol(currencyCode)}`,
-                      currencyCode,
-                      keyForList: currencyCode,
-                      isSelected: isSelectedCurrency,
-                  };
-              })
-            : [];
 
-        const searchRegex = new RegExp(Str.escapeForRegExp(searchValue.trim()), 'i');
-        const filteredCurrencies = currencyOptions
-            .filter((currencyOption) => searchRegex.test(currencyOption.text ?? '') || searchRegex.test(currencyOption.currencyName))
-            .sort((currency1, currency2) => getMatchScore(currency2.text ?? '', searchValue) - getMatchScore(currency1.text ?? '', searchValue));
-
-        const isEmpty = searchValue.trim() && !filteredCurrencies.length;
-        const shouldDisplayRecentlyOptions = !isEmptyObject(recentlyUsedCurrencyOptions) && !searchValue;
-        const selectedOptions = filteredCurrencies.filter((option) => option.isSelected);
-        const shouldDisplaySelectedOptionOnTop = selectedOptions.length > 0;
-        const unselectedOptions = getUnselectedOptions(filteredCurrencies);
-        const result = [];
-
-        if (shouldDisplaySelectedOptionOnTop) {
-            result.push({
-                title: undefined,
-                data: selectedOptions,
-                sectionIndex: 0,
+    const currencyOptions: CurrencyListItem[] = Object.entries(currencyList).reduce((acc, [currencyCode, currencyInfo]) => {
+        const isSelectedCurrency = currencyCode === initiallySelectedCurrencyCode || selectedCurrencies.includes(currencyCode);
+        if (!excludedCurrencies.includes(currencyCode) && (isSelectedCurrency || !currencyInfo?.retired)) {
+            acc.push({
+                currencyName: currencyInfo?.name ?? '',
+                text: `${currencyCode} - ${getCurrencySymbol(currencyCode)}`,
+                currencyCode,
+                keyForList: currencyCode,
+                isSelected: isSelectedCurrency,
             });
         }
+        return acc;
+    }, [] as CurrencyListItem[]);
 
-        if (shouldDisplayRecentlyOptions) {
-            if (!isEmpty) {
-                result.push(
-                    {
-                        title: translate('common.recents'),
-                        data: shouldDisplaySelectedOptionOnTop ? getUnselectedOptions(recentlyUsedCurrencyOptions) : recentlyUsedCurrencyOptions,
-                        sectionIndex: 1,
-                    },
-                    {title: translate('common.all'), data: shouldDisplayRecentlyOptions ? unselectedOptions : filteredCurrencies, sectionIndex: 2},
-                );
-            }
-        } else if (!isEmpty) {
-            result.push({
-                data: shouldDisplaySelectedOptionOnTop ? unselectedOptions : filteredCurrencies,
-                sectionIndex: 3,
-            });
+    const recentlyUsedCurrencyOptions: CurrencyListItem[] = Array.isArray(recentlyUsedCurrencies)
+        ? recentlyUsedCurrencies?.map((currencyCode) => {
+              const currencyInfo = currencyList?.[currencyCode];
+              const isSelectedCurrency = currencyCode === initiallySelectedCurrencyCode;
+              return {
+                  currencyName: currencyInfo?.name ?? '',
+                  text: `${currencyCode} - ${getCurrencySymbol(currencyCode)}`,
+                  currencyCode,
+                  keyForList: currencyCode,
+                  isSelected: isSelectedCurrency,
+              };
+          })
+        : [];
+
+    const searchRegex = new RegExp(Str.escapeForRegExp(searchValue.trim()), 'i');
+    const filteredCurrencies = currencyOptions
+        .filter((currencyOption) => searchRegex.test(currencyOption.text ?? '') || searchRegex.test(currencyOption.currencyName))
+        .sort((currency1, currency2) => getMatchScore(currency2.text ?? '', searchValue) - getMatchScore(currency1.text ?? '', searchValue));
+
+    const isEmpty = searchValue.trim() && !filteredCurrencies.length;
+    const shouldDisplayRecentlyOptions = !isEmptyObject(recentlyUsedCurrencyOptions) && !searchValue;
+    const selectedOptions = filteredCurrencies.filter((option) => option.isSelected);
+    const shouldDisplaySelectedOptionOnTop = selectedOptions.length > 0;
+    const unselectedOptions = getUnselectedOptions(filteredCurrencies);
+    const sections = [];
+
+    if (shouldDisplaySelectedOptionOnTop) {
+        sections.push({
+            title: undefined,
+            data: selectedOptions,
+            sectionIndex: 0,
+        });
+    }
+
+    if (shouldDisplayRecentlyOptions) {
+        if (!isEmpty) {
+            sections.push(
+                {
+                    title: translate('common.recents'),
+                    data: shouldDisplaySelectedOptionOnTop ? getUnselectedOptions(recentlyUsedCurrencyOptions) : recentlyUsedCurrencyOptions,
+                    sectionIndex: 1,
+                },
+                {title: translate('common.all'), data: shouldDisplayRecentlyOptions ? unselectedOptions : filteredCurrencies, sectionIndex: 2},
+            );
         }
-
-        return {sections: result, headerMessage: isEmpty ? translate('common.noResultsFound') : ''};
-    }, [currencyList, recentlyUsedCurrencies, searchValue, getUnselectedOptions, translate, initiallySelectedCurrencyCode, selectedCurrencies, excludedCurrencies, getCurrencySymbol]);
-
+    } else if (!isEmpty) {
+        sections.push({
+            data: shouldDisplaySelectedOptionOnTop ? unselectedOptions : filteredCurrencies,
+            sectionIndex: 3,
+        });
+    }
     const textInputOptions = {
         label: searchInputLabel,
         value: searchValue,
         onChangeText: setSearchValue,
-        headerMessage,
+        headerMessage: isEmpty ? translate('common.noResultsFound') : undefined,
     };
 
     return (
