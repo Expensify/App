36b405b52a15e32888b17af821eaacdfcc77884d
Sergei Sharabai <105950333+sharabai@users.noreply.github.com>
2026-02-03 19:21:03 +0100

    Fix failing lint CI

--- src/components/Search/SearchFiltersParticipantsSelector.tsx
+++ src/components/Search/SearchFiltersParticipantsSelector.tsx
@@ -52,6 +52,7 @@ function getOptionDataFromAttendee(attendee: Attendee): OptionData {
         accountID: attendee.accountID ?? CONST.DEFAULT_NUMBER_ID,
         // eslint-disable-next-line rulesdir/no-default-id-values
         reportID: '-1',
+        keyForList: attendee.displayName,
         selected: true,
         icons: attendee.avatarUrl
             ? [
