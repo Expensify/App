5869f4aa5386715d457a462a021fa76af9b3dbf9
eliran goshen <elirangoshen@gmail.com>
2026-02-03 12:14:17 +0100

    merge fixes

--- src/libs/DeepLinkHandler/index.ts
+++ src/libs/DeepLinkHandler/index.ts
@@ -2,17 +2,13 @@ import HybridAppModule from '@expensify/react-native-hybrid-app';
 import type {NativeEventSubscription} from 'react-native';
 import {Linking} from 'react-native';
 import Onyx from 'react-native-onyx';
-import type {OnyxCollection, OnyxEntry} from 'react-native-onyx';
+import type {OnyxCollection} from 'react-native-onyx';
 import {openReportFromDeepLink} from '@libs/actions/Link';
 import {hasAuthToken} from '@libs/actions/Session';
-import type {OnboardingCompanySize, OnboardingPurpose} from '@libs/actions/Welcome/OnboardingFlow';
 import CONFIG from '@src/CONFIG';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Report} from '@src/types/onyx';
 
-let currentOnboardingPurposeSelected: OnyxEntry<OnboardingPurpose>;
-let currentOnboardingCompanySize: OnyxEntry<OnboardingCompanySize>;
-let onboardingInitialPath: OnyxEntry<string>;
 let allReports: OnyxCollection<Report> = {};
 let currentConciergeReportID: string | undefined;
 let areSubscriptionsSetUp = false;
@@ -20,9 +16,6 @@ let isInitialized = false;
 let pendingDeepLinkUrl: string | null = null;
 
 let hasReportsReceived = false;
-let hasOnboardingPurposeSelectedReceived = false;
-let hasOnboardingCompanySizeReceived = false;
-let hasOnboardingInitialPathReceived = false;
 
 let linkinEventSubscription: NativeEventSubscription | null = null;
 
@@ -55,7 +48,7 @@ function processPendingDeepLinkIfReady() {
         return;
     }
 
-    if (!hasReportsReceived || !hasOnboardingPurposeSelectedReceived || !hasOnboardingCompanySizeReceived || !hasOnboardingInitialPathReceived) {
+    if (!hasReportsReceived) {
         return;
     }
 
@@ -63,51 +56,13 @@ function processPendingDeepLinkIfReady() {
     pendingDeepLinkUrl = null;
 
     const isCurrentlyAuthenticated = hasAuthToken();
-    openReportFromDeepLink(
-        urlToProcess,
-        currentOnboardingPurposeSelected,
-        currentOnboardingCompanySize,
-        onboardingInitialPath,
-        allReports,
-        isCurrentlyAuthenticated,
-        currentConciergeReportID,
-    );
+    openReportFromDeepLink(urlToProcess, allReports, isCurrentlyAuthenticated, currentConciergeReportID);
 }
 
 function setUpOnyxSubscriptions() {
     if (areSubscriptionsSetUp) {
         return;
     }
-
-    Onyx.connectWithoutView({
-        key: ONYXKEYS.ONBOARDING_PURPOSE_SELECTED,
-        callback: (value) => {
-            currentOnboardingPurposeSelected = value;
-            hasOnboardingPurposeSelectedReceived = true;
-
-            processPendingDeepLinkIfReady();
-        },
-    });
-
-    Onyx.connectWithoutView({
-        key: ONYXKEYS.ONBOARDING_COMPANY_SIZE,
-        callback: (value) => {
-            currentOnboardingCompanySize = value;
-            hasOnboardingCompanySizeReceived = true;
-
-            processPendingDeepLinkIfReady();
-        },
-    });
-
-    Onyx.connectWithoutView({
-        key: ONYXKEYS.ONBOARDING_LAST_VISITED_PATH,
-        callback: (value) => {
-            onboardingInitialPath = value;
-            hasOnboardingInitialPathReceived = true;
-
-            processPendingDeepLinkIfReady();
-        },
-    });
 
     Onyx.connectWithoutView({
         key: ONYXKEYS.COLLECTION.REPORT,
@@ -129,16 +84,13 @@ function handleDeepLink(url: string | null, fromUrlChangeEvent: boolean) {
 
     if (fromUrlChangeEvent) {
         const isCurrentlyAuthenticated = hasAuthToken();
-        openReportFromDeepLink(url, currentOnboardingPurposeSelected, currentOnboardingCompanySize, onboardingInitialPath, allReports, isCurrentlyAuthenticated, currentConciergeReportID);
+        openReportFromDeepLink(url, allReports, isCurrentlyAuthenticated, currentConciergeReportID);
         return;
     }
 
     setUpOnyxSubscriptions();
 
     hasReportsReceived = false;
-    hasOnboardingPurposeSelectedReceived = false;
-    hasOnboardingCompanySizeReceived = false;
-    hasOnboardingInitialPathReceived = false;
 
     pendingDeepLinkUrl = url;
 
