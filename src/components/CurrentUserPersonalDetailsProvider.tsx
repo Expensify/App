1a64c8a33b4152f2ad84cea1e4f582e54cc5b936
Maciej Leśniewski <maciej.lesniewski@callstack.com>
2026-02-04 23:56:36 +0100

    refactor CurrentUserPersonalDetailsContext to utilize useMemo

--- src/components/CurrentUserPersonalDetailsProvider.tsx
+++ src/components/CurrentUserPersonalDetailsProvider.tsx
@@ -1,9 +1,7 @@
-import React, {createContext, useCallback, useEffect, useRef} from 'react';
-import type {OnyxEntry} from 'react-native-onyx';
+import React, {createContext, useMemo} from 'react';
 import useOnyx from '@hooks/useOnyx';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {PersonalDetailsList} from '@src/types/onyx';
 import type {CurrentUserPersonalDetails} from '@src/types/onyx/PersonalDetails';
 import {useSession} from './OnyxListItemProvider';
 
@@ -15,22 +13,11 @@ const CurrentUserPersonalDetailsContext = createContext<CurrentUserPersonalDetai
 
 function CurrentUserPersonalDetailsProvider({children}: {children: React.ReactNode}) {
     const session = useSession();
-    const sessionRef = useRef(session);
-
-    // Keep a ref of session to make the selector perfectly synchronized
-    // with the current Onyx state during the same render phase.
-    useEffect(() => {
-        sessionRef.current = session;
-    }, [session?.accountID]);
-
-    const userAccountSelector = useCallback(
-        (allPersonalDetails: OnyxEntry<PersonalDetailsList>): CurrentUserPersonalDetails => {
-            const userAccountID = sessionRef.current?.accountID ?? CONST.DEFAULT_NUMBER_ID;
-            return allPersonalDetails?.[userAccountID] as CurrentUserPersonalDetails;
-        },
-        [],
-    );
-    const [currentUserPersonalDetails = defaultCurrentUserPersonalDetails] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {selector: userAccountSelector, canBeMissing: true}, [session?.accountID]);
+    const accountID = session?.accountID ?? CONST.DEFAULT_NUMBER_ID;
+    const [personalDetailsList] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {canBeMissing: true});
+    const currentUserPersonalDetails = useMemo(() => {
+        return personalDetailsList?.[accountID] ?? defaultCurrentUserPersonalDetails;
+    }, [personalDetailsList, accountID]);
 
     return <CurrentUserPersonalDetailsContext.Provider value={currentUserPersonalDetails}>{children}</CurrentUserPersonalDetailsContext.Provider>;
 }
