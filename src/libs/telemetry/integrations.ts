8ac0e261971cba8f6b036233219752265e3ec5f1
Hubert Sosinski <hubert.sosinski@callstack.com>
2026-02-05 09:24:21 +0100

    Browser tracing integration crashes on mobile in release builds

--- src/libs/telemetry/integrations.ts
+++ src/libs/telemetry/integrations.ts
@@ -1,5 +1,6 @@
 import * as SentryReact from '@sentry/react';
 import * as Sentry from '@sentry/react-native';
+import {Platform} from 'react-native';
 
 const navigationIntegration = Sentry.reactNavigationIntegration({
     enableTimeToInitialDisplay: true,
@@ -14,9 +15,13 @@ const tracingIntegration = Sentry.reactNativeTracingIntegration({
     shouldCreateSpanForRequest,
 });
 
-const browserTracingIntegration = SentryReact.browserTracingIntegration({
-    shouldCreateSpanForRequest: () => false,
-});
+// Browser tracing integration crashes on mobile in release builds
+const browserTracingIntegration =
+    Platform.OS === 'android' || Platform.OS === 'ios'
+        ? null
+        : SentryReact.browserTracingIntegration({
+            shouldCreateSpanForRequest: () => false,
+        });
 
 const browserProfilingIntegration = SentryReact.browserProfilingIntegration();
 
