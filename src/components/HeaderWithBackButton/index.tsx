4a1bfaee6599f92862e2ae3fbc1fdb3808d8cec1
TaduJR <nattysec26@gmail.com>
2026-02-05 15:36:03 +0300

    fix: use string literal for sentryLabel instead of undefined constant

--- src/components/HeaderWithBackButton/index.tsx
+++ src/components/HeaderWithBackButton/index.tsx
@@ -296,7 +296,7 @@ function HeaderWithBackButton({
                                         style={[styles.touchableButtonImage]}
                                         role="button"
                                         accessibilityLabel={translate('common.rotate')}
-                                        sentryLabel={CONST.SENTRY_LABEL.HEADER.ROTATE_BUTTON}
+                                        sentryLabel="Header-RotateButton"
                                     >
                                         <Icon
                                             src={icons.Rotate}
