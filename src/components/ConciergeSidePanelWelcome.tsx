e521788ca677982078cb71c27178b4c5810d91a3
krishna2323 <belivethatkg@gmail.com>
2026-02-09 01:22:46 +0530

    Hide the chat history when Concierge is opened up in the right hand pane.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

---
+++ src/components/ConciergeSidePanelWelcome.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View} from 'react-native';
+import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
+import CONST from '@src/CONST';
+import Avatar from './Avatar';
+import Text from './Text';
+
+function ConciergeSidePanelWelcome() {
+    const styles = useThemeStyles();
+    const {translate} = useLocalize();
+    const {ConciergeAvatar} = useMemoizedLazyExpensifyIcons(['ConciergeAvatar']);
+
+    return (
+        <View style={[styles.flex1, styles.justifyContentCenter, styles.alignItemsCenter, styles.ph5]}>
+            <Avatar
+                source={ConciergeAvatar}
+                size={CONST.AVATAR_SIZE.LARGE}
+                type={CONST.ICON_TYPE_AVATAR}
+            />
+            <Text style={[styles.textHeadlineH1, styles.mt3, styles.textAlignCenter]}>
+                {translate('common.concierge.sidePanelGreeting')}
+            </Text>
+        </View>
+    );
+}
+
+ConciergeSidePanelWelcome.displayName = 'ConciergeSidePanelWelcome';
+
+export default ConciergeSidePanelWelcome;
