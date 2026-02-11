4149f87a7f915d625e8fc953ae7a7823d90e2895
Bartosz Maziarka <37315072+Bar-Maz@users.noreply.github.com>
2026-02-11 10:19:56 +0100

    Delete unnecessary SidebarLinks tests

--- tests/perf-test/SidebarLinks.perf-test.tsx
+++ tests/perf-test/SidebarLinks.perf-test.tsx
@@ -1,17 +1,11 @@
-import type * as Navigation from '@react-navigation/native';
 import {fireEvent, screen, waitFor} from '@testing-library/react-native';
 import Onyx from 'react-native-onyx';
 import {measureRenders} from 'reassure';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {OnyxValues} from '@src/ONYXKEYS';
-import type {ReportActions} from '@src/types/onyx';
-import createRandomReportAction from '../utils/collections/reportActions';
-import {createSidebarReportsWithActions as createReportsWithActions} from '../utils/collections/sidebarReports';
 import * as LHNTestUtils from '../utils/LHNTestUtils';
 import * as TestHelper from '../utils/TestHelper';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
-import waitForBatchedUpdatesWithAct from '../utils/waitForBatchedUpdatesWithAct';
 import wrapOnyxWithWaitForBatchedUpdates from '../utils/wrapOnyxWithWaitForBatchedUpdates';
 
 jest.mock('@libs/Permissions');
@@ -35,22 +29,8 @@ jest.mock('../../src/libs/Navigation/navigationRef', () => ({
     isReady: () => true,
 }));
 jest.mock('@components/Icon/Expensicons');
-jest.mock('@react-navigation/native', () => {
-    const actualNav = jest.requireActual<typeof Navigation>('@react-navigation/native');
 
-    return {
-        ...actualNav,
-        useNavigationState: () => true,
-        useRoute: jest.fn(),
-        useFocusEffect: jest.fn(),
-        useIsFocused: () => true,
-        useNavigation: () => ({
-            navigate: jest.fn(),
-            addListener: jest.fn(),
-        }),
-        createNavigationContainerRef: jest.fn(),
-    };
-});
+jest.mock('@react-navigation/native');
 jest.mock('@src/hooks/useLHNEstimatedListSize/index.native.ts');
 
 const getMockedReportsMap = (length = 100) => {
@@ -69,8 +49,6 @@ const getMockedReportsMap = (length = 100) => {
 };
 
 const mockedResponseMap = getMockedReportsMap(500);
-
-const REPORTS_COUNT = 150;
 
 describe('SidebarLinks', () => {
     beforeAll(() => {
@@ -132,189 +110,6 @@ describe('SidebarLinks', () => {
 
         // Wait for Onyx to process the data
         await waitForBatchedUpdates();
-
-        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
-    });
-
-    test('[SidebarLinks LHN] initial render with 150 reports and actions', async () => {
-        const {reports, reportActions, reportNameValuePairs, policies, personalDetails, reportMetadata} = createReportsWithActions(REPORTS_COUNT);
-
-        const scenario = async () => {
-            await screen.findByTestId('lhn-options-list');
-        };
-
-        await Onyx.multiSet({
-            [ONYXKEYS.PERSONAL_DETAILS_LIST]: personalDetails,
-            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
-            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
-            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-            ...reports,
-            ...reportActions,
-            ...reportNameValuePairs,
-            ...policies,
-            ...reportMetadata,
-        } as Partial<OnyxValues>);
-
-        await waitForBatchedUpdatesWithAct();
-
-        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
-    });
-
-    test('[SidebarLinks LHN] re-render when single report action changes', async () => {
-        const {reports, reportActions, reportNameValuePairs, policies, personalDetails, reportMetadata} = createReportsWithActions(REPORTS_COUNT);
-
-        await Onyx.multiSet({
-            [ONYXKEYS.PERSONAL_DETAILS_LIST]: personalDetails,
-            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
-            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
-            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-            ...reports,
-            ...reportActions,
-            ...reportNameValuePairs,
-            ...policies,
-            ...reportMetadata,
-        } as Partial<OnyxValues>);
-
-        await waitForBatchedUpdates();
-
-        const scenario = async () => {
-            await screen.findByTestId('lhn-options-list');
-            const firstReportID = '1';
-            const actionsCollection = reportActions ?? {};
-            const firstReportActions = actionsCollection[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${firstReportID}`];
-            if (firstReportActions) {
-                const newAction = createRandomReportAction(999);
-                const updatedActions = {
-                    ...firstReportActions,
-                    [`${firstReportID}_999`]: newAction,
-                };
-                await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${firstReportID}`, updatedActions);
-                await waitForBatchedUpdatesWithAct();
-            }
-        };
-
-        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
-    });
-
-    test('[SidebarLinks LHN] re-render when multiple reports change', async () => {
-        const {reports, reportActions, reportNameValuePairs, policies, personalDetails, reportMetadata} = createReportsWithActions(REPORTS_COUNT);
-
-        await Onyx.multiSet({
-            [ONYXKEYS.PERSONAL_DETAILS_LIST]: personalDetails,
-            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
-            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
-            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-            ...reports,
-            ...reportActions,
-            ...reportNameValuePairs,
-            ...policies,
-            ...reportMetadata,
-        } as Partial<OnyxValues>);
-
-        await waitForBatchedUpdates();
-
-        const scenario = async () => {
-            await screen.findByTestId('lhn-options-list');
-            const updates: Record<string, ReportActions> = {};
-            const actionsCollection = reportActions ?? {};
-            for (let i = 1; i <= 10; i++) {
-                const reportID = String(i);
-                const existingActions = actionsCollection[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`];
-                if (existingActions) {
-                    const newAction = createRandomReportAction(1000 + i);
-                    updates[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${reportID}`] = {
-                        ...existingActions,
-                        [`${reportID}_new`]: newAction,
-                    };
-                }
-            }
-            await Onyx.multiSet(updates as Partial<OnyxValues>);
-            await waitForBatchedUpdatesWithAct();
-        };
-
-        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
-    });
-
-    test('[SidebarLinks LHN] re-render when report metadata changes', async () => {
-        const {reports, reportActions, reportNameValuePairs, policies, personalDetails, reportMetadata} = createReportsWithActions(REPORTS_COUNT);
-
-        await Onyx.multiSet({
-            [ONYXKEYS.PERSONAL_DETAILS_LIST]: personalDetails,
-            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
-            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
-            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-            ...reports,
-            ...reportActions,
-            ...reportNameValuePairs,
-            ...policies,
-            ...reportMetadata,
-        } as Partial<OnyxValues>);
-
-        await waitForBatchedUpdates();
-
-        const scenario = async () => {
-            await screen.findByTestId('lhn-options-list');
-            const firstReportID = '1';
-            await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT_METADATA}${firstReportID}`, {
-                lastVisitTime: new Date().toISOString(),
-            });
-            await waitForBatchedUpdatesWithAct();
-        };
-
-        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
-    });
-
-    test('[SidebarLinks LHN] re-render when report archived status changes', async () => {
-        const {reports, reportActions, reportNameValuePairs, policies, personalDetails, reportMetadata} = createReportsWithActions(REPORTS_COUNT);
-
-        await Onyx.multiSet({
-            [ONYXKEYS.PERSONAL_DETAILS_LIST]: personalDetails,
-            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
-            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
-            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-            ...reports,
-            ...reportActions,
-            ...reportNameValuePairs,
-            ...policies,
-            ...reportMetadata,
-        } as Partial<OnyxValues>);
-
-        await waitForBatchedUpdates();
-
-        const scenario = async () => {
-            await screen.findByTestId('lhn-options-list');
-            const firstReportID = '1';
-            const nameValuePairsCollection = reportNameValuePairs ?? {};
-            const currentArchivedStatus = nameValuePairsCollection[`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${firstReportID}`]?.private_isArchived;
-            await Onyx.merge(`${ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS}${firstReportID}`, {
-                private_isArchived: currentArchivedStatus === 'true' ? 'false' : 'true',
-            });
-            await waitForBatchedUpdatesWithAct();
-        };
-
-        await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
-    });
-
-    test('[SidebarLinks LHN] scaling test – initial render with 500 reports', async () => {
-        const {reports, reportActions, reportNameValuePairs, policies, personalDetails, reportMetadata} = createReportsWithActions(500);
-
-        const scenario = async () => {
-            await screen.findByTestId('lhn-options-list');
-        };
-
-        await Onyx.multiSet({
-            [ONYXKEYS.PERSONAL_DETAILS_LIST]: personalDetails,
-            [ONYXKEYS.BETAS]: [CONST.BETAS.DEFAULT_ROOMS],
-            [ONYXKEYS.NVP_PRIORITY_MODE]: CONST.PRIORITY_MODE.GSD,
-            [ONYXKEYS.IS_LOADING_REPORT_DATA]: false,
-            ...reports,
-            ...reportActions,
-            ...reportNameValuePairs,
-            ...policies,
-            ...reportMetadata,
-        } as Partial<OnyxValues>);
-
-        await waitForBatchedUpdatesWithAct();
 
         await measureRenders(<LHNTestUtils.MockedSidebarLinks />, {scenario});
     });
