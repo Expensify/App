5c4a7e061aaeba095202dd280d239afe91d6b864
dmkt9 <demoskratos9x@gmail.com>
2026-02-16 08:02:26 +0700

    Fix - Self DM is marked as selected in recipient list when submitting track distance from self DM

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -11110,6 +11110,7 @@ function createDraftTransactionAndNavigateToParticipantSelector(
         modifiedMerchant: '',
         modifiedAttendees: undefined,
         mccGroup,
+        participants: undefined
     } as Transaction);
 
     let firstPolicy: Policy | undefined;
