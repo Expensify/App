c8b83a7aba71ff414338f29e9358ff738fc2990a
Rushat Gabhane <rushatgabhane@gmail.com>
2026-02-21 13:12:08 +0100

    update search and ai api urls to localhost proxy

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -75,8 +75,8 @@ function injectFooterCopyright() {
     footer.innerHTML = `&copy;2008-${new Date().getFullYear()} Expensify, Inc.`;
 }
 
-const SEARCH_API_URL = 'https://www.expensify.com/api/SearchHelpsite';
-const ASK_AI_API_URL = 'https://www.expensify.com/api/AskHelpsiteAI';
+const SEARCH_API_URL = 'http://localhost:8010/proxy/api/SearchHelpsite';
+const ASK_AI_API_URL = 'http://localhost:8010/proxy/api/AskHelpsiteAI';
 
 let allowedDomains = [];
 
