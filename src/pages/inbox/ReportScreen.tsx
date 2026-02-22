e3cf6b40093fc36a6e9f379901ec3f84420e49d6
Maruf Sharifi <maruf.sharifi.work@gmail.com>
2026-02-22 14:58:13 +0430

    fix: resolve unending skeleton after deleting expense and navigating back to self DM

--- src/pages/inbox/ReportScreen.tsx
+++ src/pages/inbox/ReportScreen.tsx
@@ -828,6 +828,10 @@ function ReportScreen({route, navigation, isInSidePanel = false}: ReportScreenPr
             return;
         }
 
+        if (reportIDFromRoute) {
+            Navigation.removeReportScreen(new Set([reportIDFromRoute]));
+        }
+
         // Only redirect if focused
         if (!isFocused) {
             return;
@@ -845,7 +849,7 @@ function ReportScreen({route, navigation, isInSidePanel = false}: ReportScreenPr
         Navigation.isNavigationReady().then(() => {
             navigateToConciergeChat(conciergeReportID);
         });
-    }, [reportWasDeleted, isFocused, deletedReportParentID, conciergeReportID]);
+    }, [reportWasDeleted, isFocused, deletedReportParentID, conciergeReportID, reportIDFromRoute]);
 
     useEffect(() => {
         if (!isValidReportIDFromPath(reportIDFromRoute)) {
