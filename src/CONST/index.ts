0d0f1cdf21dbf02a89092bb1f9a870ce00d85523
Imran Jamil Malik <ijmalik@gmail.com>
2026-02-23 16:01:36 +0500

    Multi-level categories with colons are shown with hierarchical indentation

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -6327,7 +6327,7 @@ const CONST = {
         PM: 'PM',
     },
     INDENTS: '    ',
-    PARENT_CHILD_SEPARATOR: ': ',
+    PARENT_CHILD_SEPARATOR: ':',
     DISTANCE_MERCHANT_SEPARATOR: '@',
     COLON: ':',
     MAPBOX: {
