f808afac56320233f322df33d79e3aa884dbe5bf
Jan Musiał <jan.musial@swmansion.com>
2026-02-24 12:57:25 +0100

    use CONST for bullet

--- src/components/HTMLEngineProvider/HTMLRenderers/BulletItemRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/BulletItemRenderer.tsx
@@ -6,6 +6,7 @@ import Text from '@components/Text';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 import variables from '@styles/variables';
+import CONST from '@src/CONST';
 
 function BulletItemRenderer({tnode}: CustomRendererProps<TBlock>) {
     const styles = useThemeStyles();
@@ -13,7 +14,7 @@ function BulletItemRenderer({tnode}: CustomRendererProps<TBlock>) {
 
     return (
         <View style={styles.flexRow}>
-            <Text style={{color: theme.text, fontSize: variables.fontSizeNormal, lineHeight: variables.fontSizeNormalHeight, paddingHorizontal: 8}}>•</Text>
+            <Text style={{color: theme.text, fontSize: variables.fontSizeNormal, lineHeight: variables.fontSizeNormalHeight, paddingHorizontal: 8}}>{CONST.DOT_SEPARATOR}</Text>
             <View style={styles.flex1}>
                 <TNodeChildrenRenderer tnode={tnode} />
             </View>
