f50a20f5f8a87097b0cc98b8cdc793c9e7cd0f05
allgandaf <whogandalf@gmail.com>
2026-02-25 00:55:45 +0530

    Remove auto-toggle from settlement account selection

--- src/pages/workspace/travel/WorkspaceTravelInvoicingSettlementAccountPage.tsx
+++ src/pages/workspace/travel/WorkspaceTravelInvoicingSettlementAccountPage.tsx
@@ -8,12 +8,12 @@ import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import useWorkspaceAccountID from '@hooks/useWorkspaceAccountID';
-import {setTravelInvoicingSettlementAccount, toggleTravelInvoicing} from '@libs/actions/TravelInvoicing';
+import {setTravelInvoicingSettlementAccount} from '@libs/actions/TravelInvoicing';
 import {getLastFourDigits} from '@libs/BankAccountUtils';
 import {getEligibleBankAccountsForCard} from '@libs/CardUtils';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import {REIMBURSEMENT_ACCOUNT_ROUTE_NAMES} from '@libs/ReimbursementAccountUtils';
-import {getIsTravelInvoicingEnabled, getTravelInvoicingCardSettingsKey} from '@libs/TravelInvoicingUtils';
+import {getTravelInvoicingCardSettingsKey} from '@libs/TravelInvoicingUtils';
 import Navigation from '@navigation/Navigation';
 import type {SettingsNavigatorParamList} from '@navigation/types';
 import AccessOrNotFoundWrapper from '@pages/workspace/AccessOrNotFoundWrapper';
@@ -67,12 +67,6 @@ function WorkspaceTravelInvoicingSettlementAccountPage({route}: WorkspaceTravelI
 
         const previousPaymentBankAccountID = cardSettings?.previousPaymentBankAccountID ?? cardSettings?.paymentBankAccountID;
         setTravelInvoicingSettlementAccount(policyID, workspaceAccountID, value, previousPaymentBankAccountID);
-
-        // If Travel Invoicing is not yet enabled, enable it after setting the settlement account
-        // Backend requires settlement account to be configured before enabling
-        if (!getIsTravelInvoicingEnabled(cardSettings)) {
-            toggleTravelInvoicing(policyID, workspaceAccountID, true);
-        }
 
         Navigation.goBack();
     };
