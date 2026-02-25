708f524ab7ac406aa7a6379c3027e3ea6a38401c
nkdengineer <nkdengineer@outlook.com>
2026-02-25 14:22:20 +0700

    fix: Travel opens travel.expensify.com in external browser instead of in-app webview

--- src/libs/openTravelDotLink/index.ts
+++ src/libs/openTravelDotLink/index.ts
@@ -1,8 +1,13 @@
-import {openTravelDotLink as openTravelDotLinkWeb} from '@libs/actions/Link';
+import {buildTravelDotURL, openTravelDotLink as openTravelDotLinkWeb} from '@libs/actions/Link';
+import asyncOpenURL from '@libs/asyncOpenURL';
 import Navigation from '@libs/Navigation/Navigation';
 import ROUTES from '@src/ROUTES';
 
-const openTravelDotLink = (activePolicyID?: string, postLoginPath?: string) => {
+const openTravelDotLink = (activePolicyID?: string, postLoginPath?: string, spotnanaToken?: string, isTestAccount?: boolean) => {
+    if (spotnanaToken) {
+        asyncOpenURL(Promise.resolve(), () => buildTravelDotURL(spotnanaToken, isTestAccount ?? false, postLoginPath));
+        return;
+    }
     openTravelDotLinkWeb(activePolicyID, postLoginPath)
         ?.then(() => {})
         ?.catch(() => {
