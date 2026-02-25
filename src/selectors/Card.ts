b7d993d1bb3998c07f925d9755201e2d036174cb
DylanDylann <dylandylan9112@gmail.com>
2026-02-25 22:50:24 +0700

    AI'comment

--- src/selectors/Card.ts
+++ src/selectors/Card.ts
@@ -79,8 +79,8 @@ const areAllExpensifyCardsShipped = (cardList: OnyxEntry<CardList>): boolean =>
         .filter((card) => isCard(card) && isExpensifyCard(card))
         .every((card) => card.state !== CONST.EXPENSIFY_CARD.STATE.STATE_NOT_ISSUED);
 
-const isExpensifyCardUkEuSupportedSelector = (cardList: OnyxEntry<CardList>, cardID?: string): boolean =>
-    !!cardID && Object.values(cardList ?? {}).filter((card) => card.cardID === Number(cardID) && isExpensifyCardUkEuSupported(card)).length > 0;
+const isExpensifyCardUkEuSupportedSelector = (cardList: OnyxEntry<CardList>, cardID: string): boolean =>
+    !!cardID && Object.values(cardList ?? {}).filter((card) => isCard(card) && card.cardID === Number(cardID) && isExpensifyCardUkEuSupported(card)).length > 0;
 
 export {
     filterCardsHiddenFromSearch,
