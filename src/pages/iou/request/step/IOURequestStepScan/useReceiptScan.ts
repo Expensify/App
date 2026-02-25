4c5d2cd4793f707dc350e7222447b4a64ce99760
Marc Glasser <marc.aaron.glasser@gmail.com>
2026-02-25 10:13:12 -1000

    Revert blink animation to original subtle values
    Restore 0.4 opacity / 10ms+50ms timing instead of the more pronounced 1.0 / 50ms+150ms version.

--- src/pages/iou/request/step/IOURequestStepScan/useReceiptScan.ts
+++ src/pages/iou/request/step/IOURequestStepScan/useReceiptScan.ts
@@ -117,8 +117,8 @@ function useReceiptScan({
 
     function showBlink() {
         blinkOpacity.set(
-            withTiming(1, {duration: 50}, () => {
-                blinkOpacity.set(withTiming(0, {duration: 150}));
+            withTiming(0.4, {duration: 10}, () => {
+                blinkOpacity.set(withTiming(0, {duration: 50}));
             }),
         );
         HapticFeedback.press();
