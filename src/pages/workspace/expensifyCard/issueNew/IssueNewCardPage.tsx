6e7a1c79cc688b23fb5680febba89fd0f8ecef60
Julian Kobrynski <julian.kobrynski@callstack.com>
2026-02-25 15:37:21 +0100

    remove initWithStoredValues from ISSUE_NEW_EXPENSIFY_CARD

--- src/pages/workspace/expensifyCard/issueNew/IssueNewCardPage.tsx
+++ src/pages/workspace/expensifyCard/issueNew/IssueNewCardPage.tsx
@@ -47,7 +47,7 @@ function getStartStepIndex(issueNewCard: OnyxEntry<IssueNewCard>, isSingleUseEna
 
 function IssueNewCardPage({policy, route}: IssueNewCardPageProps) {
     const policyID = policy?.id;
-    const [issueNewCard] = useOnyx(`${ONYXKEYS.COLLECTION.ISSUE_NEW_EXPENSIFY_CARD}${policyID}`, {initWithStoredValues: false});
+    const [issueNewCard] = useOnyx(`${ONYXKEYS.COLLECTION.ISSUE_NEW_EXPENSIFY_CARD}${policyID}`);
     const {currentStep} = issueNewCard ?? {};
     const backTo = route?.params?.backTo;
     const {isDelegateAccessRestricted} = useDelegateNoAccessState();
