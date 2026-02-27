bf33da107ac514b14e4feb9af7f49e2d30581446
sumo_slonik <jakub.nowakowski@swmansion.com>
2026-02-27 10:52:29 +0100

    migrate state editing only to place where it is rendering

--- src/libs/Navigation/AppNavigator/createRootStackNavigator/useCustomRootStackNavigatorState/index.ts
+++ src/libs/Navigation/AppNavigator/createRootStackNavigator/useCustomRootStackNavigatorState/index.ts
@@ -1,18 +1,39 @@
 import {isFullScreenName} from '@libs/Navigation/helpers/isNavigatorName';
 import type {CustomStateHookProps} from '@libs/Navigation/PlatformStackNavigation/types';
 
-// This is an optimization to keep mounted only last few screens in the stack.
 export default function useCustomRootStackNavigatorState({state}: CustomStateHookProps) {
     const lastSplitIndex = state.routes.findLastIndex((route) => isFullScreenName(route.name));
     let indexToSlice = Math.max(0, lastSplitIndex);
     const hasPrevRoute = lastSplitIndex > 0;
     const isPrevFullScreen = isFullScreenName(state.routes.at(lastSplitIndex - 1)?.name);
 
-    // If the route before the last full screen is e.g. RHP, we should leave it in the rendered routes,
-    // as there may be display issues (blank screen) when navigating back and recreating that route to render.
     if (hasPrevRoute && !isPrevFullScreen) {
         indexToSlice = lastSplitIndex - 1;
     }
-    const routesToRender = state.routes.slice(indexToSlice, state.routes.length);
-    return {...state, routes: routesToRender, index: routesToRender.length - 1};
+
+
+    const firstKeyMap = new Map<string, string>();
+    state.routes.forEach((route) => {
+        if (!firstKeyMap.has(route.name)) {
+            firstKeyMap.set(route.name, route.key);
+        }
+    });
+
+    const routesToRender = state.routes.slice(indexToSlice).map((route) => {
+        const firstKey = firstKeyMap.get(route.name);
+
+        if (firstKey && route.key !== firstKey) {
+            return {
+                ...route,
+                key: firstKey,
+            };
+        }
+        return route;
+    });
+
+    return {
+        ...state,
+        routes: routesToRender,
+        index: routesToRender.length - 1
+    };
 }
