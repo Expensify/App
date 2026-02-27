b9e9fa4702fb830d6318c29efd254125d00372a7
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-02-27 22:54:04 +0800

    change the animation type

--- src/libs/Navigation/AppNavigator/ModalStackNavigators/index.tsx
+++ src/libs/Navigation/AppNavigator/ModalStackNavigators/index.tsx
@@ -91,7 +91,9 @@ const OPTIONS_PER_SCREEN: Partial<Record<Screen, PlatformStackNavigationOptions>
     [SCREENS.MISSING_PERSONAL_DETAILS]: {
         animationTypeForReplace: 'push',
     },
-
+    [SCREENS.SETTINGS.ADD_BANK_ACCOUNT]: {
+        animationTypeForReplace: 'push',
+    },
     [SCREENS.MULTIFACTOR_AUTHENTICATION.MAGIC_CODE]: {
         animationTypeForReplace: 'push',
     },
