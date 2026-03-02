4c27251bea9cd93c62f64003025d637b712e2589
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-02 12:59:38 -0500

    fix: apply solution for issue #83898

--- src/components/ReportActionItem/TaskView.js
+++ src/components/ReportActionItem/TaskView.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {View} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {Text as RNText} from 'react-native';
 import PropTypes from 'prop-types';
 import {withOnyx} from 'react-native-onyx';
 import Text from '../Text';
     }
 
     return (
-        <ScrollView style={[styles.p5, styles.borderRadiusNormal]} nestedScrollEnabled>
-            <View style={[styles.overflowHidden]}>
-                <Text style={[styles.textSupporting, styles.mb1]} selectable>
-                    {props.taskReport.description}
-                </Text>
-            </View>
-        </ScrollView>
+        <View style={[styles.p5, styles.borderRadiusNormal]}>
+            <RNText style={[styles.textSupporting, styles.mb1]} numberOfLines={0}>
+                {props.taskReport.description}
+            </RNText>
+        </View>
     );
 };
