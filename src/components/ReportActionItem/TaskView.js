0e0f22e95fc0000597e8adbd66899183258d0f20
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-02 12:59:43 -0500

    fix: apply solution for issue #83898

--- src/components/ReportActionItem/TaskView.js
+++ src/components/ReportActionItem/TaskView.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import {View} from 'react-native';
-import {Text as RNText} from 'react-native';
-import PropTypes from 'prop-types';
-import {withOnyx} from 'react-native-onyx';
-import Text from '../Text';
     }
 
     return (
         <View style={[styles.p5, styles.borderRadiusNormal]}>
-            <RNText style={[styles.textSupporting, styles.mb1]} numberOfLines={0}>
+            <Text style={[styles.textSupporting, styles.mb1]} numberOfLines={0}>
                 {props.taskReport.description}
-            </RNText>
+            </Text>
         </View>
     );
 };
