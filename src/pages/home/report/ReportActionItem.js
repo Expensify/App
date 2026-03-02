e4bfab463bb900350c9a8ab44b9e8a290a066328
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-02 12:59:42 -0500

    fix: apply solution for issue #83898

--- src/pages/home/report/ReportActionItem.js
+++ src/pages/home/report/ReportActionItem.js
@@ -1,43 +1,13 @@
-import _ from 'underscore';
-import React, {useState, useEffect, useRef} from 'react';
-import {View, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View} from 'react-native';
+import {Text as RNText} from 'react-native';
 import PropTypes from 'prop-types';
-import * as ReportUtils from '../../libs/ReportUtils';
-import * as StyleUtils from '../../styles/StyleUtils';
-import * as TaskUtils from '../../libs/TaskUtils';
-import RenderHTML from '../../components/RenderHTML';
-
-const propTypes = {
-    /** All the data of the action item */
-    const [isTaskReport, setIsTaskReport] = useState(false);
-    const [taskReport, setTaskReport] = useState({});
-    const [taskAssignee, setTaskAssignee] = useState('');
-    const [taskDescription, setTaskDescription] = useState('');
-
-    useEffect(() => {
-        if (ReportActionsUtils.isTaskAction(props.action)) {
-            if (taskReportID) {
-                setIsTaskReport(true);
-                setTaskReport(ReportUtils.getReport(taskReportID) || {});
-                setTaskDescription(TaskUtils.getTaskDescription(ReportUtils.getReport(taskReportID) || {}));
-            }
-        }
-    }, [props.action]);
-    useEffect(() => {
-        if (isTaskReport && taskReport) {
-            setTaskAssignee(TaskUtils.getTaskAssignee(taskReport));
-            setTaskDescription(TaskUtils.getTaskDescription(taskReport));
-        }
-    }, [isTaskReport, taskReport]);
-
+import Text from '../../components/Text';
+import styles from '../../styles/styles';
             return (
                 <View style={[styles.p5, styles.borderRadiusNormal, styles.overflowHidden]}>
-                    <Text style={[styles.textSupporting, styles.mb1]}>Task: {TaskUtils.getTaskReportName(taskReport)}</Text>
-                    {taskDescription && (
-                        <View style={[styles.mt2]}>
-                            <RenderHTML html={`<div>${taskDescription.replace(/\n/g, '<br/>')}</div>`} />
-                        </View>
-                    )}
-                    {taskAssignee && (
-                        <Text style={[styles.textSupporting, styles.mt1]}>
-                            Assignee: {taskAssignee}
+                    <Text style={[styles.textSupporting, styles.mb1]}>Task: {taskReportName}</Text>
+                    <RNText style={[styles.textSupporting]} numberOfLines={0}>{taskDescription}</RNText>
+                </View>
+            );
+        }
