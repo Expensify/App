dd074e5bb8d9b34398fd5561cea853a5872c7b6a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-02 11:37:33 -0500

    fix: ShanaBoo autonomous solution (update)

--- shanaboo/a/src/components/ReportActionItem/TaskView.tsx
+++ shanaboo/a/src/components/ReportActionItem/TaskView.tsx
@@ -1,7 +1,15 @@
-                <RenderHTML
-                    html={description}
-                    debug={false}
-                    style={[styles.textSupporting, styles.textNormal, styles.pre, {maxWidth: '100%'}]}
-                />
-            </View>
-            {shouldShowHorizontalRule && <View style={styles.taskHorizontalRule} />}
+import React from 'react';
+import {View} from 'react-native';
+import RenderHTML from 'react-native-render-html';
+import {parse} from 'expensify-common';
+import useThemeStyles from '@hooks/useThemeStyles';
+import useWindowDimensions from '@hooks/useWindowDimensions';
+import * as ReportUtils from '@libs/ReportUtils';
+    const isCanceled = ReportUtils.isCanceledTaskReport(taskReport);
+    const isOpen = ReportUtils.isOpenTaskReport(taskReport);
+    const isCompleted = ReportUtils.isCompletedTaskReport(taskReport);
+    const description = taskReport?.description ?? '';
+    const html = description ? parse(description, {textRepresentation: 'html'}) : '';
+
+    const renderAvatar = () => {
+        if (!ReportUtils.isTaskReport(taskReport)) {
