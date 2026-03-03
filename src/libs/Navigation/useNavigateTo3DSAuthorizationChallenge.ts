JakubKorytko/Expensify-App

feat: add simulate pending transaction screen

src/libs/Navigation/useNavigateTo3DSAuthorizationChallenge.ts
@@ -82,7 +82,8 @@ function useNavigateTo3DSAuthorizationChallenge() {
             }
 
             // Make an API call to double check that the challenge is still valid
-            const challengeStillValid = await isTransactionStillPending3DSReview(transactionPending3DSReview.transactionID);
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            const challengeStillValid = transactionPending3DSReview?.simulated || (await isTransactionStillPending3DSReview(transactionPending3DSReview.transactionID));
 
             // If we know that a challenge isn't valid anymore, better to bail out of navigating to the flow rather than showing the user the "already reviewed" outcome screen
             if (!challengeStillValid || cancel) {
@@ -98,7 +99,7 @@ function useNavigateTo3DSAuthorizationChallenge() {
         return () => {
             cancel = true;
         };
-    }, [transactionPending3DSReview?.transactionID, doesDeviceSupportBiometrics]);
+    }, [transactionPending3DSReview?.transactionID, doesDeviceSupportBiometrics, transactionPending3DSReview?.simulated]);
 }
 
 export default useNavigateTo3DSAuthorizationChallenge;

Expensify/App

src/libs/Navigation/useNavigateTo3DSAuthorizationChallenge.ts
@@ -133,7 +133,8 @@ function useNavigateTo3DSAuthorizationChallenge() {
             }
 
             // Make an API call to double check that the challenge is still eligible for review (i.e. has not been reviewed on another device)
-            const challengeStillPendingReview = await isTransactionStillPending3DSReview(transactionPending3DSReview.transactionID);
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            const challengeStillPendingReview = transactionPending3DSReview?.simulated || (await isTransactionStillPending3DSReview(transactionPending3DSReview.transactionID));
 
             // If we know that a challenge is no longer pending review, bail rather than showing the user the "already reviewed" outcome screen
             if (!challengeStillPendingReview) {
@@ -157,7 +158,7 @@ function useNavigateTo3DSAuthorizationChallenge() {
         return () => {
             cancel = true;
         };
-    }, [transactionPending3DSReview?.transactionID, doesDeviceSupportBiometrics, isCurrentlyActingOn3DSChallenge]);
+    }, [transactionPending3DSReview?.transactionID, doesDeviceSupportBiometrics, isCurrentlyActingOn3DSChallenge, transactionPending3DSReview?.simulated]);
 }
 
 export default useNavigateTo3DSAuthorizationChallenge;