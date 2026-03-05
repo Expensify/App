0f69484c32958c2e587e8bb6a181f94b1f54536b
Carlos Martins <cmartins@expensify.com>
2026-03-05 15:17:23 -0700

    Stabilize useIsAnonymousUser hook

--- src/hooks/useIsAnonymousUser.ts
+++ src/hooks/useIsAnonymousUser.ts
@@ -1,20 +1,18 @@
-import {useMemo} from 'react';
+import type {OnyxEntry} from 'react-native-onyx';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {Session} from '@src/types/onyx';
 import useOnyx from './useOnyx';
+
+const authTokenTypeSelector = (session: OnyxEntry<Session>) => session?.authTokenType;
 
 /**
  * Hook to check if the current user is anonymous
  * Returns true if the user's auth token type is ANONYMOUS, false otherwise
  */
 function useIsAnonymousUser() {
-    const [session] = useOnyx(ONYXKEYS.SESSION);
-
-    const isAnonymousUser = useMemo(() => {
-        return session?.authTokenType === CONST.AUTH_TOKEN_TYPES.ANONYMOUS;
-    }, [session?.authTokenType]);
-
-    return isAnonymousUser ?? false;
+    const [authTokenType = CONST.AUTH_TOKEN_TYPES.ANONYMOUS] = useOnyx(ONYXKEYS.SESSION, {selector: authTokenTypeSelector});
+    return authTokenType === CONST.AUTH_TOKEN_TYPES.ANONYMOUS;
 }
 
 export default useIsAnonymousUser;
