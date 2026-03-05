84cc7948c3944efa30b790b8a62fc5ab71bd2cc8
war-in <war-in@users.noreply.github.com>
2026-03-05 14:46:14 +0100

    chore: fix typecheck

--- src/components/Image/BaseImage.tsx
+++ src/components/Image/BaseImage.tsx
@@ -1,12 +1,12 @@
 import {Image as ExpoImage} from 'expo-image';
-import type {ImageLoadEventData} from 'expo-image';
+import type {ImageProps as ExpoImageProps, ImageLoadEventData} from 'expo-image';
 import React, {useCallback, useContext, useEffect} from 'react';
 import type {AttachmentSource} from '@components/Attachments/types';
 import getImageRecyclingKey from '@libs/getImageRecyclingKey';
 import {AttachmentStateContext} from '@pages/media/AttachmentModalScreen/AttachmentModalBaseContent/AttachmentStateContextProvider';
 import type {BaseImageProps} from './types';
 
-function BaseImage({onLoad, onLoadStart, source, ...props}: BaseImageProps) {
+function BaseImage({onLoad, onLoadStart, source, style, ...props}: BaseImageProps) {
     const {setAttachmentLoaded, isAttachmentLoaded} = useContext(AttachmentStateContext);
     useEffect(() => {
         if (isAttachmentLoaded?.(source as AttachmentSource)) {
@@ -45,6 +45,7 @@ function BaseImage({onLoad, onLoadStart, source, ...props}: BaseImageProps) {
             onLoad={onLoad ? imageLoadedSuccessfully : undefined}
             source={source}
             recyclingKey={getImageRecyclingKey(source)}
+            style={style as ExpoImageProps['style']}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...props}
         />
