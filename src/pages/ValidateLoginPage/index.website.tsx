511c5beaea060939954b7e922dbfed134e1a72cb
MobileMage <aidelojejoshua@gmail.com>
2026-03-05 23:17:03 +0100

    Show ValidateCodeModal instead of empty page when 2FA fails

--- src/pages/ValidateLoginPage/index.website.tsx
+++ src/pages/ValidateLoginPage/index.website.tsx
@@ -80,7 +80,10 @@ function ValidateLoginPage({
             {autoAuthStateWithDefault === CONST.AUTO_AUTH_STATE.JUST_SIGNED_IN && is2FARequired && !isSignedIn && !!login && <JustSignedInModal is2FARequired />}
             {autoAuthStateWithDefault === CONST.AUTO_AUTH_STATE.JUST_SIGNED_IN && isSignedIn && !exitTo && !!login && <JustSignedInModal is2FARequired={false} />}
             {/* If session.autoAuthState isn't available yet, we use shouldStartSignInWithValidateCode to conditionally render the component instead of local autoAuthState which contains a default value of NOT_STARTED */}
-            {(!autoAuthState ? !shouldStartSignInWithValidateCode : autoAuthStateWithDefault === CONST.AUTO_AUTH_STATE.NOT_STARTED && !isNavigatingToExitTo) && (
+            {(!autoAuthState
+                ? !shouldStartSignInWithValidateCode
+                : (autoAuthStateWithDefault === CONST.AUTO_AUTH_STATE.NOT_STARTED && !isNavigatingToExitTo) ||
+                  (autoAuthStateWithDefault === CONST.AUTO_AUTH_STATE.FAILED && is2FARequired)) && (
                 <ValidateCodeModal
                     accountID={Number(accountID)}
                     code={validateCode}
