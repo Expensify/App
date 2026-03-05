b80afacd9e65dafc7ae056a3e4164dbc741f9c52
Carlos Martins <cmartins@expensify.com>
2026-03-05 15:20:31 -0700

    stabilize selfDMReportIDSelector

--- src/hooks/useSelfDMReport.ts
+++ src/hooks/useSelfDMReport.ts
@@ -1,11 +1,18 @@
 import DateUtils from '@libs/DateUtils';
-import {buildOptimisticSelfDMReport, findSelfDMReportID} from '@libs/ReportUtils';
+import {buildOptimisticSelfDMReport, isSelfDM, isThread} from '@libs/ReportUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {OnyxCollection} from 'react-native-onyx';
+import type {Report} from '@src/types/onyx';
 import useOnyx from './useOnyx';
 
+const selfDMReportIDSelector = (reports: OnyxCollection<Report>) => {
+    return Object.values(reports ?? {}).find((report) => isSelfDM(report) && !isThread(report))?.reportID;
+};
+
 function useSelfDMReport() {
-    const [selfDMReportID] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: findSelfDMReportID});
-    const [selfDMReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${selfDMReportID}`);
+    const [cachedSelfDMReportID] = useOnyx(ONYXKEYS.SELF_DM_REPORT_ID);
+    const [selfDMReportID] = useOnyx(ONYXKEYS.COLLECTION.REPORT, {selector: selfDMReportIDSelector});
+    const [selfDMReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${cachedSelfDMReportID ?? selfDMReportID}`);
     return selfDMReport ?? buildOptimisticSelfDMReport(DateUtils.getDBTime());
 }
 
