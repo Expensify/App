56cb2bae575cdb7d013b2cf93c0898fdcb56757f
Rushat Gabhane <rushatgabhane@gmail.com>
2026-03-05 20:04:16 +0100

    add reassign_approver to action type check

--- src/pages/inbox/report/PureReportActionItem.tsx
+++ src/pages/inbox/report/PureReportActionItem.tsx
@@ -1744,7 +1744,11 @@ function PureReportActionItem({
                     )}
                 </ReportActionItemBasicMessage>
             );
-        } else if (isActionOfType(action, CONST.REPORT.ACTIONS.TYPE.TAKE_CONTROL) || isActionOfType(action, CONST.REPORT.ACTIONS.TYPE.REROUTE)) {
+        } else if (
+            isActionOfType(action, CONST.REPORT.ACTIONS.TYPE.TAKE_CONTROL) ||
+            isActionOfType(action, CONST.REPORT.ACTIONS.TYPE.REROUTE) ||
+            isActionOfType(action, CONST.REPORT.ACTIONS.TYPE.REASSIGN_APPROVER)
+        ) {
             children = (
                 <ReportActionItemBasicMessage>
                     <RenderHTML html={`<comment><muted-text>${getChangedApproverActionMessage(translate, action)}</muted-text></comment>`} />
