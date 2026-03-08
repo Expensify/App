ae46a1074da33705ab235c074fd36283332da36d
krishna2323 <belivethatkg@gmail.com>
2026-03-09 02:45:45 +0530

    fix navigation bug after account validation.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/libs/PaymentUtils.ts
+++ src/libs/PaymentUtils.ts
@@ -155,6 +155,8 @@ const handleUnvalidatedAccount = (iouReport: OnyxEntry<Report>) => {
         Navigation.navigate(ROUTES.SEARCH_MONEY_REQUEST_REPORT_VERIFY_ACCOUNT.getRoute(reportID));
     } else if (activeRoute.includes(ROUTES.SEARCH_REPORT.getRoute({reportID}))) {
         Navigation.navigate(ROUTES.SEARCH_REPORT_VERIFY_ACCOUNT.getRoute(reportID));
+    } else if (activeRoute.includes(ROUTES.EXPENSE_REPORT_RHP.getRoute({reportID}))) {
+        Navigation.navigate(ROUTES.EXPENSE_REPORT_VERIFY_ACCOUNT.getRoute(reportID));
     } else {
         Navigation.navigate(ROUTES.REPORT_VERIFY_ACCOUNT.getRoute(reportID));
     }
