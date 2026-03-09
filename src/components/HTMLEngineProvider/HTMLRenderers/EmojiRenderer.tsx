0e879129b8812202363e810e0c1fdd3b995a1dde
paulnjs <paulqn45@outlook.com>
2026-03-09 14:46:39 +0700

    update code

--- src/components/HTMLEngineProvider/HTMLRenderers/EmojiRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/EmojiRenderer.tsx
@@ -18,7 +18,7 @@ function EmojiRenderer({tnode, style: styleProp}: CustomRendererProps<TText | TP
 
     return (
         <EmojiWithTooltip
-            isOnSeparateLine={'isOnSeparateLine' in tnode.attributes}
+            isOnSeparateLine={'oneline' in tnode.attributes}
             style={[style, styles.cursorDefault, styles.emojiDefaultStyles]}
             emojiCode={'data' in tnode ? tnode.data : ''}
             isMedium={'ismedium' in tnode.attributes}
