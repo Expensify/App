880cd54eef2a4386fdb2ca9aa81907c147a4658a
bartlomiej obudzinski <bartlomiej.obudzinski@callstack.com>
2026-03-09 15:57:53 +0100

    Revert "fix: remove unused imports from setupSentry"
    This reverts commit 4340f8d4b60732ec88a5dac47930e357e4edc8d7.

--- src/setup/telemetry/setupSentry.ts
+++ src/setup/telemetry/setupSentry.ts
@@ -1,23 +1,25 @@
 import * as Sentry from '@sentry/react-native';
 import {Platform} from 'react-native';
+import {isDevelopment} from '@libs/Environment/Environment';
 import {breadcrumbsIntegration, browserProfilingIntegration, consoleIntegration, navigationIntegration, tracingIntegration} from '@libs/telemetry/integrations';
 import {processBeforeSendLogs, processBeforeSendTransactions} from '@libs/telemetry/middlewares';
 import CONFIG from '@src/CONFIG';
 import CONST from '@src/CONST';
 import pkg from '../../../package.json';
+import makeDebugTransport from './debugTransport';
 
 function setupSentry(): void {
     // With Sentry enabled in dev mode, profiling on iOS and Android does not work
     // If you want to enable Sentry in dev, set ENABLE_SENTRY_ON_DEV=true in .env
-    // if (isDevelopment() && !CONFIG.ENABLE_SENTRY_ON_DEV) {
-    //     return;
-    // }
+    if (isDevelopment() && !CONFIG.ENABLE_SENTRY_ON_DEV) {
+        return;
+    }
 
     const integrations = [navigationIntegration, tracingIntegration, browserProfilingIntegration, breadcrumbsIntegration, consoleIntegration].filter((integration) => !!integration);
 
     Sentry.init({
         dsn: CONFIG.SENTRY_DSN,
-        // transport: isDevelopment() ? makeDebugTransport : undefined,
+        transport: isDevelopment() ? makeDebugTransport : undefined,
         tracesSampleRate: 1.0,
         // 1. Profiling for Android is currently disabled because it causes crashes sometimes.
         // 2. When updating the profile sample rate, make sure it will not blow up our current limit in Sentry.
