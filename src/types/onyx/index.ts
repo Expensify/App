8b0d2c921c7fbee69b35c6bac3d5bd93fe7a6226
Hans <visaocuoitroi193@gmail.com>
2026-03-10 11:19:17 +0700

    Update tests

--- src/types/onyx/index.ts
+++ src/types/onyx/index.ts
@@ -133,7 +133,7 @@ import type ReportNameValuePairs from './ReportNameValuePairs';
 import type LastSearchParams from './ReportNavigation';
 import type ReportNextStepDeprecated from './ReportNextStepDeprecated';
 import type ReportUserIsTyping from './ReportUserIsTyping';
-import type {ReportViolationName} from './ReportViolation';
+import type ReportViolationName from './ReportViolationName';
 import type Request from './Request';
 import type {AnyRequest} from './Request';
 import type Response from './Response';
