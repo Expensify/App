18566c02619d6878b584076c05a0d2f18d556494
GCyganek <gabrielcyganek@gmail.com>
2026-03-10 12:04:28 +0100

    Fix import

--- src/components/GPSTripStateChecker/index.native.tsx
+++ src/components/GPSTripStateChecker/index.native.tsx
@@ -14,7 +14,7 @@ import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import {useSplashScreenState} from '@src/SplashScreenStateContext';
-import useUpdateGpsNotification from './useUpdateGpsNotification/index.ios';
+import useUpdateGpsNotification from './useUpdateGpsNotification';
 import useUpdateGpsTripOnReconnect from './useUpdateGpsTripOnReconnect';
 
 function GPSTripStateChecker() {
