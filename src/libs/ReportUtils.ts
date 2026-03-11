38db48c699ed80005bf8c68ad88aea6c5effdf2c
Jack Senyitko <jsenyitko@expensify.com>
2026-03-11 14:34:55 -0400

    remove another o(n)

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -3493,6 +3493,10 @@ function getParticipantsAccountIDsForDisplay(
     const reportParticipants = report?.participants ?? {};
     const reportMetadata = reportMetadataParam ?? getReportMetadata(report?.reportID);
 
+    // For 1:1 chat, we don't want to include the current user as a participant in order to not mark 1:1 chats as having multiple participants
+    // For system chat, we want to display Expensify as the only participant
+    const shouldExcludeCurrentUser = isOneOnOneChat(report) || isSystemChat(report) || shouldForceExcludeCurrentUser;
+
     let participantsAccountIDs: number[] = [];
 
     for (const entry of Object.entries(reportParticipants)) {
@@ -3510,16 +3514,27 @@ function getParticipantsAccountIDsForDisplay(
             continue;
         }
 
+        if (shouldExcludeCurrentUser && accountIDNumber === currentUserAccountID) {
+            continue;
+        }
+
+        if (shouldExcludeHidden && isHiddenForCurrentUser(reportParticipants[accountIDNumber]?.notificationPreference)) {
+            continue;
+        }
+
+        if (
+            shouldExcludeDeleted &&
+            reportMetadata?.pendingChatMembers?.findLast((member) => Number(member.accountID) === accountIDNumber)?.pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE
+        ) {
+            continue;
+        }
+
         participantsAccountIDs.push(Number(accountID));
     }
 
-    // For 1:1 chat, we don't want to include the current user as a participant in order to not mark 1:1 chats as having multiple participants
-    // For system chat, we want to display Expensify as the only participant
-    const shouldExcludeCurrentUser = isOneOnOneChat(report) || isSystemChat(report) || shouldForceExcludeCurrentUser;
-
-    if (shouldExcludeCurrentUser || shouldExcludeHidden || shouldExcludeDeleted) {
-        participantsIds = excludeParticipantsForDisplay(participantsIds, reportParticipants, reportMetadata, {shouldExcludeHidden, shouldExcludeDeleted, shouldExcludeCurrentUser});
-    }
+    // if (shouldExcludeCurrentUser || shouldExcludeHidden || shouldExcludeDeleted) {
+    //     participantsIds = excludeParticipantsForDisplay(participantsIds, reportParticipants, reportMetadata, {shouldExcludeHidden, shouldExcludeDeleted, shouldExcludeCurrentUser});
+    // }
 
     return participantsAccountIDs;
 }
