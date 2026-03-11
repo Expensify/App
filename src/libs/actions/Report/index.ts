7f6e8a6dfdae5c13015584cda6f2cf83d2ccb813
NJ-2020 <ntt.london@gmail.com>
2026-03-11 23:54:54 +0700

    fix: add comment explanation

--- src/libs/actions/Report/index.ts
+++ src/libs/actions/Report/index.ts
@@ -416,6 +416,7 @@ Onyx.connect({
     },
 });
 
+// We use connectWithoutView because `allAttachments` doesn't affect the UI rendering, it's only used to retireve atttachment local source when deleting a comment
 let allAttachments: OnyxCollection<Attachment> = {};
 Onyx.connectWithoutView({
     key: ONYXKEYS.COLLECTION.ATTACHMENT,
