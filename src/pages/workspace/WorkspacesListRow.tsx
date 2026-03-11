600e857612fb471e4030f2edf4ada9fb5b2c01ac
Shawn Borton (via MelvinBot) <infra+melvinbot@expensify.com>
2026-03-11 11:17:39 +0000

    Fix: Prettier formatting in WorkspacesListRow.tsx
    Co-authored-by: Shawn Borton <shawnborton@users.noreply.github.com>

--- src/pages/workspace/WorkspacesListRow.tsx
+++ src/pages/workspace/WorkspacesListRow.tsx
@@ -262,7 +262,9 @@ function WorkspacesListRow({
 
     return (
         <View style={[styles.flexRow, styles.highlightBG, rowStyles, style, styles.br3]}>
-            <Animated.View style={[styles.flex1, styles.flexRow, styles.bgTransparent, isWide ? styles.gap5 : styles.gap2, styles.pt3, styles.pr2, styles.pb3, styles.pl4, animatedHighlightStyle]}>
+            <Animated.View
+                style={[styles.flex1, styles.flexRow, styles.bgTransparent, isWide ? styles.gap5 : styles.gap2, styles.pt3, styles.pr2, styles.pb3, styles.pl4, animatedHighlightStyle]}
+            >
                 <PressableWithoutFeedback
                     accessible
                     accessibilityLabel={accessibilityLabel}
