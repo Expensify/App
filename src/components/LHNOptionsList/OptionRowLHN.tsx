d8c356995bba89343fd99f5b579b7cecec18a8e2
aimane-chnaif <aiman.chnaif@gmail.com>
2026-03-12 14:54:44 +0000

    update badge style

--- src/components/LHNOptionsList/OptionRowLHN.tsx
+++ src/components/LHNOptionsList/OptionRowLHN.tsx
@@ -445,6 +445,7 @@ function OptionRowLHN({
                                                 <Badge
                                                     icon={expensifyIcons.Pin}
                                                     text={translate('common.pinned')}
+                                                    badgeStyles={isOptionFocused && styles.badgeDefaultActive}
                                                     isCondensed
                                                     isStrong
                                                 />
