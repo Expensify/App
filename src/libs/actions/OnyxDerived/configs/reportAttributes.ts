65df1a62c87cf22ce31d14b4b6341775ae57d0be
Hans <visaocuoitroi193@gmail.com>
2026-03-12 10:46:30 +0700

    prettier

--- src/libs/actions/OnyxDerived/configs/reportAttributes.ts
+++ src/libs/actions/OnyxDerived/configs/reportAttributes.ts
@@ -19,7 +19,7 @@ const prepareReportKeys = (keys: string[]) => {
                 key
                     .replace(ONYXKEYS.COLLECTION.REPORT_METADATA, ONYXKEYS.COLLECTION.REPORT)
                     .replace(ONYXKEYS.COLLECTION.REPORT_ACTIONS, ONYXKEYS.COLLECTION.REPORT)
-                    .replace(ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS, ONYXKEYS.COLLECTION.REPORT)
+                    .replace(ONYXKEYS.COLLECTION.REPORT_NAME_VALUE_PAIRS, ONYXKEYS.COLLECTION.REPORT),
             ),
         ),
     ];
