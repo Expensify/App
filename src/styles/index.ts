2ca1e0a698bad74cc3244cea6b8e9293f50893e6
Rushat Gabhane (via MelvinBot) <infra+melvinbot@expensify.com>
2026-03-12 00:59:04 +0000

    Add textDarkColorfulBackground theme variable instead of using colors directly
    Co-authored-by: Rushat Gabhane <rushatgabhane@users.noreply.github.com>

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -607,7 +607,7 @@ const staticStyles = (theme: ThemeColors) =>
         },
 
         textVersion: {
-            color: theme.iconColorfulBackground,
+            color: theme.textDarkColorfulBackground,
             fontSize: variables.fontSizeNormal,
             lineHeight: variables.lineHeightNormal,
             ...FontUtils.fontFamily.platform.MONOSPACE,
