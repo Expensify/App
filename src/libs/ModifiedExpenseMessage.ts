5605785be6073026782d98dea80908422cae84e7
jakubstec <jakub.stec@swmansion.com>
2026-03-12 11:40:27 +0100

    Revert changes to MovedFromPersonalSpaceParams type part2

--- src/libs/ModifiedExpenseMessage.ts
+++ src/libs/ModifiedExpenseMessage.ts
@@ -178,10 +178,7 @@ function getForExpenseMovedFromSelfDM(translate: LocalizedTranslate, destination
     if (isEmpty(policyName) && !reportName) {
         return translate('iou.changedTheExpense');
     }
-    return translate('iou.movedFromPersonalSpace', {
-        workspaceName: reportName,
-        reportName: !isEmpty(policyName) ? policyName : undefined,
-    });
+    return translate('iou.movedFromPersonalSpace', {reportName, workspaceName: !isEmpty(policyName) ? policyName : undefined});
 }
 
 function getMovedReportID(reportAction: OnyxEntry<ReportAction>, type: ValueOf<typeof CONST.REPORT.MOVE_TYPE>): string | undefined {
