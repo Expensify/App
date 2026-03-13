664ea451c86c8f0afb81883bdbdc914f4122e7bc
dukenv0307 <dukenv0307@gmail.com>
2026-03-13 09:56:12 +0700

    lint gic

--- src/libs/interceptAnonymousUser.ts
+++ src/libs/interceptAnonymousUser.ts
@@ -1,14 +1,13 @@
 import type {OnyxEntry} from 'react-native-onyx';
-import * as Session from './actions/Session';
+import {isAnonymousUser, signOutAndRedirectToSignIn} from './actions/Session';
 
 /**
  * Checks if user is anonymous. If true, shows the sign in modal, else,
  * executes the callback.
  */
 const interceptAnonymousUser = (callback: () => void, activePolicyID?: OnyxEntry<string>) => {
-    const isAnonymousUser = Session.isAnonymousUser();
-    if (isAnonymousUser) {
-        Session.signOutAndRedirectToSignIn(undefined, undefined, undefined, undefined, activePolicyID);
+    if (isAnonymousUser()) {
+        signOutAndRedirectToSignIn(undefined, undefined, undefined, undefined, activePolicyID);
     } else {
         callback();
     }
