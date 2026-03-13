1237374714c85bda25bb7cd84a3d3438f3b6343c
Shridhar Goel (via MelvinBot) <infra+melvinbot@expensify.com>
2026-03-13 18:37:45 +0000

    Move sandbox attribute from EmbeddedDemo to TestDriveDemo via iframeProps
    Co-authored-by: Shridhar Goel <ShridharGoel@users.noreply.github.com>

--- src/components/EmbeddedDemo/index.tsx
+++ src/components/EmbeddedDemo/index.tsx
@@ -9,7 +9,6 @@ function EmbeddedDemo({url, iframeTitle, iframeProps}: EmbeddedDemoProps) {
         <iframe
             title={iframeTitle}
             src={url}
-            sandbox="allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
             style={styles.embeddedDemoIframe}
             // eslint-disable-next-line react/jsx-props-no-spreading
             {...iframeProps}
