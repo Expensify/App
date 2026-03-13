7f1258dcba87ff07c6107e167c14c9286f937a7f
Eskalifer1 <artemkryt1800@gmail.com>
2026-03-13 14:16:44 +0200

    review comments

--- src/libs/clearSelectedTextIfComposerBlurred/clearSelectedTextIfComposerBlurred.website.ts
+++ src/libs/clearSelectedTextIfComposerBlurred/clearSelectedTextIfComposerBlurred.website.ts
@@ -1,3 +1,6 @@
+import clearSelectedText from '@libs/clearSelectedText/clearSelectedText';
+import CONST from '@src/CONST';
+
 /**
  * This function checks if the currently focused element is the composer element
  * (with id 'composer'). If the composer is focused, the function returns early
@@ -7,13 +10,10 @@
  */
 function clearSelectedTextIfComposerBlurred() {
     const activeElement = document.activeElement as HTMLElement | null;
-    const isComposerFocused = activeElement?.id === 'composer';
-
-    if (isComposerFocused) {
+    if (activeElement?.id === CONST.COMPOSER.NATIVE_ID) {
         return;
     }
-
-    window.getSelection()?.removeAllRanges();
+    clearSelectedText();
 }
 
 export default clearSelectedTextIfComposerBlurred;
