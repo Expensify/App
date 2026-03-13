9e8761b4b3c68516342f2bd70573db5909cd4f47
Abdelrahman Khattab <abzokhattab@gmail.com>
2026-03-13 02:04:40 +0100

    Remove unused getCombinedReportActions import

--- src/libs/OptionsListUtils/index.ts
+++ src/libs/OptionsListUtils/index.ts
@@ -39,7 +39,6 @@ import {
     getAutoPayApprovedReportsEnabledMessage,
     getAutoReimbursementMessage,
     getChangedApproverActionMessage,
-    getCombinedReportActions,
     getCurrencyDefaultTaxUpdateMessage,
     getCustomTaxNameUpdateMessage,
     getDynamicExternalWorkflowRoutedMessage,
