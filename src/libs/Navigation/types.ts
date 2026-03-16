baf65c534f99a0c506c27bedacb4b4f2382fc349
huutech <20178761+huult@users.noreply.github.com>
2026-03-16 09:14:16 +0700

    update import

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -1688,13 +1688,13 @@ type ReportSettingsNavigatorParamList = {
         // eslint-disable-next-line no-restricted-syntax -- `backTo` usages in this file are legacy. Do not add new `backTo` params to screens. See contributingGuides/NAVIGATION.md
         backTo?: Routes;
     };
-    [SCREENS.REPORT_SETTINGS.DYNAMIC_SETTINGS_WRITE_CAPABILITY]: {
-        reportID: string;
-    };
     [SCREENS.REPORT_SETTINGS.NOTIFICATION_PREFERENCES]: {
         reportID: string;
         // eslint-disable-next-line no-restricted-syntax -- `backTo` usages in this file are legacy. Do not add new `backTo` params to screens. See contributingGuides/NAVIGATION.md
         backTo?: Routes;
+    };
+    [SCREENS.REPORT_SETTINGS.DYNAMIC_SETTINGS_WRITE_CAPABILITY]: {
+        reportID: string;
     };
     [SCREENS.REPORT_SETTINGS.VISIBILITY]: {
         reportID: string;
