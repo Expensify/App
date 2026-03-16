fc7c72669e32e19004bf24bc6dbe7980769b4243
Yuwen Memon <yuwen@expensify.com>
2026-03-16 09:16:26 -0700

    Revert "Multi-level categories with colons are shown with hierarchical indent…"

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -6051,7 +6051,7 @@ const CONST = {
         PM: 'PM',
     },
     INDENTS: '    ',
-    PARENT_CHILD_SEPARATOR: ':',
+    PARENT_CHILD_SEPARATOR: ': ',
     DISTANCE_MERCHANT_SEPARATOR: '@',
     COLON: ':',
     MAPBOX: {
