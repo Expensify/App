9fb598e655121f93439865caf3db36fba27fb858
I Nyoman Jyotisa <jyotisa1616@gmail.com>
2026-03-18 01:42:09 +0800

    Fix: Expense auto enters selection mode after clicking Move to report

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -1779,6 +1779,7 @@ type MoneyRequestNavigatorParamList = {
         // eslint-disable-next-line no-restricted-syntax -- `backTo` usages in this file are legacy. Do not add new `backTo` params to screens. See contributingGuides/NAVIGATION.md
         backTo: Routes;
         shouldTurnOffSelectionMode?: boolean;
+        transactionID?: string;
     };
     [SCREENS.MONEY_REQUEST.STEP_REPORT]: {
         action: IOUAction;
