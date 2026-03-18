a2dd375d0a78cd55f93283af745ac4c4ac4a1649
TaduJR <nattysec26@gmail.com>
2026-03-18 21:34:50 +0300

    refactor: remove redundant picker a11y props handled by fork

--- src/components/Picker/BasePicker.tsx
+++ src/components/Picker/BasePicker.tsx
@@ -9,7 +9,6 @@ import FormHelpMessage from '@components/FormHelpMessage';
 import Icon from '@components/Icon';
 import Text from '@components/Text';
 import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
-import useLocalize from '@hooks/useLocalize';
 import useScrollContext from '@hooks/useScrollContext';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
@@ -43,7 +42,6 @@ function BasePicker<TPickerValue>({
     ref,
 }: BasePickerProps<TPickerValue>) {
     const icons = useMemoizedLazyExpensifyIcons(['DownArrow']);
-    const {translate} = useLocalize();
     const theme = useTheme();
     const styles = useThemeStyles();
 
@@ -211,10 +209,6 @@ function BasePicker<TPickerValue>({
                     onClose={disableHighlight}
                     textInputProps={{
                         allowFontScaling: false,
-                    }}
-                    touchableDoneProps={{
-                        accessibilityRole: CONST.ROLE.BUTTON,
-                        accessibilityLabel: translate('common.dismiss'),
                     }}
                     pickerProps={{
                         ref: picker,
