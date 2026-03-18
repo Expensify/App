b50e1d5e41a55d413a3badc26baca63385a18400
Rutika Pawar <183392827+twilight2294@users.noreply.github.com>
2026-03-18 21:26:36 +0530

    Update SortableTableHeader.tsx

--- src/components/SelectionListWithSections/SortableTableHeader.tsx
+++ src/components/SelectionListWithSections/SortableTableHeader.tsx
@@ -90,7 +90,6 @@ function SortableTableHeader({
                                     postedColumnSize === CONST.SEARCH.TABLE_COLUMN_SIZES.WIDE,
                                     exportedColumnSize === CONST.SEARCH.TABLE_COLUMN_SIZES.WIDE,
                                     shouldRemoveTotalColumnFlex,
-                                    isActive,
                                 ),
                             ]}
                             isSortable={isSortable}
