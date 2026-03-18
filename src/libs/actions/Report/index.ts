a6b928bc0c9695c8dca2292d3eabfd4e798c8716
Srikar Parsi <srikar.parsi@expensify.com>
2026-03-18 15:37:29 -0400

    Revert "Merge pull request #83899 from software-mansion-labs/collectioneur/transition-tracker-v2"
    This reverts commit c4d00e12ad75da9e939b82c281e7441ed96674a7, reversing
    changes made to fd3c052a2b5388b866632ebb1582674ba59554a3.

--- src/libs/actions/Report/index.ts
+++ src/libs/actions/Report/index.ts
@@ -1988,7 +1988,7 @@ function createTransactionThreadReport(
 function navigateToReport(reportID: string | undefined, shouldDismissModal = true) {
     if (shouldDismissModal) {
         Navigation.dismissModal({
-            afterTransition: () => {
+            callback: () => {
                 if (!reportID) {
                     return;
                 }
