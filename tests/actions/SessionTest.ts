8b9992a01e7ef1e9317c2ce293c33ca2c16a12ec
Vit Horacek <36083550+mountiny@users.noreply.github.com>
2026-03-19 13:29:37 +0100

    Merge pull request #85767 from margelo/@chrispader/dowgrade-onyx-to-v3.0.45
    [CP Staging] fix: Downgrade Onyx to v3.0.45
    
    (cherry picked from commit 5277b58aa75817c9959381de7cd01be0260712c0)
    
    (cherry-picked to staging by mountiny)

--- tests/actions/SessionTest.ts
+++ tests/actions/SessionTest.ts
@@ -189,8 +189,6 @@ describe('Session', () => {
 
         await Onyx.set(ONYXKEYS.NETWORK, {isOffline: false});
 
-        await waitForBatchedUpdates();
-
         expect(getAllPersistedRequests().length).toBe(0);
     });
 
@@ -227,8 +225,6 @@ describe('Session', () => {
         expect(getAllPersistedRequests().at(0)?.command).toBe(WRITE_COMMANDS.OPEN_APP);
 
         await Onyx.set(ONYXKEYS.NETWORK, {isOffline: false});
-
-        await waitForBatchedUpdates();
 
         expect(getAllPersistedRequests().length).toBe(0);
     });
