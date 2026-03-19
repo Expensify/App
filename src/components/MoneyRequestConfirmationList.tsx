6baa39b1c4f6c228b0011592c4edd8fa33e66a84
Marc Glasser <marc.aaron.glasser@gmail.com>
2026-03-19 13:30:19 -1000

    Revert "fix: [Web] Focus restoration mechanism on back navigation (#76921)"

--- src/components/MoneyRequestConfirmationList.tsx
+++ src/components/MoneyRequestConfirmationList.tsx
@@ -15,7 +15,7 @@ import usePolicyForTransaction from '@hooks/usePolicyForTransaction';
 import usePreferredPolicy from '@hooks/usePreferredPolicy';
 import usePrevious from '@hooks/usePrevious';
 import useThemeStyles from '@hooks/useThemeStyles';
-import blurActiveInputElement from '@libs/Accessibility/blurActiveInputElement';
+import blurActiveElement from '@libs/Accessibility/blurActiveElement';
 import {
     setCustomUnitRateID,
     setMoneyRequestAmount,
@@ -1153,9 +1153,7 @@ function MoneyRequestConfirmationList({
             focusTimeoutRef.current = setTimeout(() => {
                 // eslint-disable-next-line @typescript-eslint/no-deprecated
                 InteractionManager.runAfterInteractions(() => {
-                    // Only blur input elements to dismiss keyboard.
-                    // Don't blur other elements to preserve focus restoration on back navigation.
-                    blurActiveInputElement();
+                    blurActiveElement();
                 });
             }, CONST.ANIMATED_TRANSITION);
             return () => focusTimeoutRef.current && clearTimeout(focusTimeoutRef.current);
