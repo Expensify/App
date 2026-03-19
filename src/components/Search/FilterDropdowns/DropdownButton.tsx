89125e0f16d33301cf7adcbff2e1067459227538
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-03-19 21:40:20 +0800

    remove unused var

--- src/components/Search/FilterDropdowns/DropdownButton.tsx
+++ src/components/Search/FilterDropdowns/DropdownButton.tsx
@@ -52,8 +52,6 @@ type DropdownButtonProps = WithSentryLabel & {
     wrapperStyle?: StyleProp<ViewStyle>;
 };
 
-const PADDING_MODAL = 8;
-
 const ANCHOR_ORIGIN = {
     horizontal: CONST.MODAL.ANCHOR_ORIGIN_HORIZONTAL.LEFT,
     vertical: CONST.MODAL.ANCHOR_ORIGIN_VERTICAL.TOP,
