6e6912a8dfad22f2f3205a7bfa463eb948ecda69
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2026-03-19 11:45:25 +0100

    Reapply "Merge pull request #83899 from software-mansion-labs/collectioneur/transition-tracker-v2"
    This reverts commit a6b928bc0c9695c8dca2292d3eabfd4e798c8716.

--- src/components/Modal/BaseModal.tsx
+++ src/components/Modal/BaseModal.tsx
@@ -2,7 +2,7 @@ import React, {useCallback, useContext, useEffect, useMemo, useRef, useState} fr
 import type {LayoutChangeEvent} from 'react-native';
 // Animated required for side panel navigation
 // eslint-disable-next-line no-restricted-imports
-import {Animated, DeviceEventEmitter, View} from 'react-native';
+import {Animated, View} from 'react-native';
 import ColorSchemeWrapper from '@components/ColorSchemeWrapper';
 import NavigationBar from '@components/NavigationBar';
 import ScreenWrapperOfflineIndicatorContext from '@components/ScreenWrapper/ScreenWrapperOfflineIndicatorContext';
@@ -167,8 +167,6 @@ function BaseModal({
         // eslint-disable-next-line react-hooks/exhaustive-deps
         [],
     );
-
-    useEffect(() => () => DeviceEventEmitter.emit(CONST.MODAL_EVENTS.CLOSED), []);
 
     const handleShowModal = useCallback(() => {
         if (shouldSetModalVisibility) {
