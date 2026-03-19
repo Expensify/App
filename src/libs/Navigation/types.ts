3e6c0ee3bddf5dde403eba0c77a05041d30c87dc
Christoph Pader <chris@margelo.com>
2026-03-19 10:41:42 +0100

    refactor: remove unnecessary duplicate `validateAttachmentFile` check

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -2889,8 +2889,7 @@ type AttachmentModalScreensParamList = {
         accountID?: number;
         attachmentID?: string;
         source?: AvatarSource;
-        file?: FileObject | FileObject[];
-        dataTransferItems?: DataTransferItem[];
+        file: FileObject | FileObject[];
         type?: ValueOf<typeof CONST.ATTACHMENT_TYPE>;
         isAuthTokenRequired?: boolean;
         originalFileName?: string;
