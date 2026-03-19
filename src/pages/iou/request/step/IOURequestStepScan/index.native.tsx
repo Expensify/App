2f5e405d9b33147e43373d0a979b6a988639ca38
Marc Glasser <marc.aaron.glasser@gmail.com>
2026-03-19 13:36:34 -1000

    Fix deprecated absoluteFillObject usage

--- src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/index.native.tsx
@@ -526,7 +526,7 @@ function IOURequestStepScan({
                                     <Animated.View style={[styles.cameraFocusIndicator, cameraFocusIndicatorAnimatedStyle]} />
                                     <Animated.View
                                         pointerEvents="none"
-                                        style={[StyleSheet.absoluteFillObject, StyleUtils.getBackgroundColorStyle(theme.appBG), blinkStyle, styles.zIndex10]}
+                                        style={[StyleSheet.absoluteFill, StyleUtils.getBackgroundColorStyle(theme.appBG), blinkStyle, styles.zIndex10]}
                                     />
                                 </View>
                             </GestureDetector>
