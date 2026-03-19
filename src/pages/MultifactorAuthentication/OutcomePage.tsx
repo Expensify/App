bb59a166509de0e94fc655f831dd35709c570bfc
DylanDylann <dylandylan9112@gmail.com>
2026-03-19 16:19:50 +0700

    revert outcome changes

--- src/pages/MultifactorAuthentication/OutcomePage.tsx
+++ src/pages/MultifactorAuthentication/OutcomePage.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import {DefaultClientFailureScreen} from '@components/MultifactorAuthentication/components/OutcomeScreen';
 import {useMultifactorAuthenticationState} from '@components/MultifactorAuthentication/Context';
 import type {ErrorState} from '@components/MultifactorAuthentication/Context/State';
-import CONST from '@src/CONST';
 
 /** Server failure screen generally represents "unknown error" so also show when status is unknown (e.g. network/parse error). */
 function isServerError(error: ErrorState): boolean {
-    return !!error.httpStatusCode && error.httpStatusCode >= 500;
+    return error.httpStatusCode === undefined || error.httpStatusCode >= 500;
 }
 
 function MultifactorAuthenticationOutcomePage() {
@@ -21,17 +20,15 @@ function MultifactorAuthenticationOutcomePage() {
     }
 
     const reasonScreen = scenario.failureScreens?.[error.reason];
-
-    const isPriorityReason =
-        error.reason === CONST.MULTIFACTOR_AUTHENTICATION.REASON.GENERIC.NO_ELIGIBLE_METHODS || error.reason === CONST.MULTIFACTOR_AUTHENTICATION.REASON.GENERIC.UNSUPPORTED_DEVICE;
-
-    if (reasonScreen && isPriorityReason) {
+    if (reasonScreen) {
         return reasonScreen;
     }
 
-    const defaultScreen = isServerError(error) ? scenario.defaultServerFailureScreen : scenario.defaultClientFailureScreen;
+    if (isServerError(error)) {
+        return scenario.defaultServerFailureScreen;
+    }
 
-    return defaultScreen ?? reasonScreen;
+    return scenario.defaultClientFailureScreen;
 }
 
 MultifactorAuthenticationOutcomePage.displayName = 'MultifactorAuthenticationOutcomePage';
