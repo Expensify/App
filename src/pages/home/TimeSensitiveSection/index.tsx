5477eea31bdd0057e4ecca1c05d2a988cfabf96a
Adam Grzybowski <adam.grzybowski@swmansion.com>
2026-03-19 13:42:38 +0100

    Use useIsAnonymousUser hook instead of manual session check

--- src/pages/home/TimeSensitiveSection/index.tsx
+++ src/pages/home/TimeSensitiveSection/index.tsx
@@ -6,13 +6,13 @@ import type {OnyxCollection} from 'react-native-onyx';
 import WidgetContainer from '@components/WidgetContainer';
 import useCardFeedErrors from '@hooks/useCardFeedErrors';
 import useCurrentUserPersonalDetails from '@hooks/useCurrentUserPersonalDetails';
+import useIsAnonymousUser from '@hooks/useIsAnonymousUser';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {hasSynchronizationErrorMessage, isConnectionInProgress} from '@libs/actions/connections';
 import {isCurrentUserValidated} from '@libs/UserUtils';
-import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Policy} from '@src/types/onyx';
 import type {ConnectionName, PolicyConnectionName} from '@src/types/onyx/Policy';
@@ -61,6 +61,7 @@ function TimeSensitiveSection() {
     const {translate} = useLocalize();
     const {shouldUseNarrowLayout} = useResponsiveLayout();
     const {login} = useCurrentUserPersonalDetails();
+    const isAnonymous = useIsAnonymousUser();
 
     // Use custom hooks for offers and cards (Release 3)
     const {shouldShow50off, shouldShow25off, shouldShowAddPaymentCard, firstDayFreeTrial, discountInfo} = useTimeSensitiveOffers();
@@ -143,7 +144,6 @@ function TimeSensitiveSection() {
     const hasBrokenCompanyCards = brokenCompanyCardConnections.length > 0;
     const hasBrokenPersonalCards = brokenPersonalCardConnections.length > 0;
     const hasBrokenAccountingConnections = brokenAccountingConnections.length > 0;
-    const isAnonymous = session?.authTokenType === CONST.AUTH_TOKEN_TYPES.ANONYMOUS;
     const isCurrentLoginValidated = isCurrentUserValidated(loginList, session?.email ?? login);
     const shouldShowValidateAccount = isUserValidated === false && !isAnonymous && !isCurrentLoginValidated;
     // This guard must exactly match the conditions used to render each widget below.
