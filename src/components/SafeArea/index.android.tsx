f9e16c8d9b1c15fe38db8a2c0e88114da601ee95
GCyganek <gabrielcyganek@gmail.com>
2026-03-19 16:13:17 +0100

    SafeArea Android update

--- src/components/SafeArea/index.android.tsx
+++ src/components/SafeArea/index.android.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import {Keyboard} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import PressableWithoutFeedback from '@components/Pressable/PressableWithoutFeedback';
 import useIsInLandscapeMode from '@hooks/useIsInLandscapeMode';
-import useSafeAreaInsets from '@hooks/useSafeAreaInsets';
 import useThemeStyles from '@hooks/useThemeStyles';
 import type SafeAreaProps from './types';
 
 function SafeArea({children}: SafeAreaProps) {
     const styles = useThemeStyles();
     const isInLandscapeMode = useIsInLandscapeMode();
-    const {left, right} = useSafeAreaInsets();
-
-    console.log('SafeAreaAndroid2');
 
     if (!isInLandscapeMode) {
         return children;
@@ -24,22 +18,6 @@ function SafeArea({children}: SafeAreaProps) {
             edges={['left', 'right']}
         >
             {children}
-            {left > 0 && (
-                <PressableWithoutFeedback
-                    style={{position: 'absolute', left: 0, top: 0, bottom: 0, width: left}}
-                    onPress={() => Keyboard.dismiss()}
-                    role="none"
-                    accessibilityLabel=""
-                />
-            )}
-            {right > 0 && (
-                <PressableWithoutFeedback
-                    style={{position: 'absolute', right: 0, top: 0, bottom: 0, width: right}}
-                    onPress={() => Keyboard.dismiss()}
-                    role="none"
-                    accessibilityLabel=""
-                />
-            )}
         </SafeAreaView>
     );
 }
