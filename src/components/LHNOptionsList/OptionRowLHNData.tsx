9b3f3cc581ba6605538f4aafb724979e22f99212
bartlomiej obudzinski <bartlomiej.obudzinski@callstack.com>
2026-03-20 10:03:45 +0100

    perf: remove leftover blank line in useMemo deps

--- src/components/LHNOptionsList/OptionRowLHNData.tsx
+++ src/components/LHNOptionsList/OptionRowLHNData.tsx
@@ -126,7 +126,6 @@ function OptionRowLHNData({
         conciergeReportID,
         iouReportReportActions,
         transaction,
-
         invoiceReceiverPolicy,
         lastMessageTextFromReport,
         card,
