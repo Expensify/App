5e7646567764ac6f8a8b634bc7174823ea456436
truph01 <truphd01@gmail.com>
2026-03-22 19:35:23 +0700

    fix: lint

--- src/components/Search/SearchFiltersParticipantsSelector.tsx
+++ src/components/Search/SearchFiltersParticipantsSelector.tsx
@@ -69,7 +69,6 @@ function SearchFiltersParticipantsSelector({initialAccountIDs, onFiltersUpdate, 
     const currentUserEmail = currentUserPersonalDetails.email ?? '';
     const [recentAttendees] = useOnyx(ONYXKEYS.NVP_RECENT_ATTENDEES);
     const [sortedReportActionsData] = useOnyx(ONYXKEYS.DERIVED.SORTED_REPORT_ACTIONS);
-    const sortedActions = sortedReportActionsData?.sortedActions;
 
     // Transform raw recentAttendees into Option[] format for use with getValidOptions (only for attendee filter)
     const recentAttendeeLists = useMemo(
