fdd6fe583ef2c48ee58aa4a113914088c2fa3577
GCyganek <gabrielcyganek@gmail.com>
2026-03-23 15:46:41 +0100

    Delete not used import

--- src/pages/settings/Profile/CustomStatus/SetTimePage.tsx
+++ src/pages/settings/Profile/CustomStatus/SetTimePage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {View} from 'react-native';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import ScreenWrapper from '@components/ScreenWrapper';
 import ScrollView from '@components/ScrollView';
