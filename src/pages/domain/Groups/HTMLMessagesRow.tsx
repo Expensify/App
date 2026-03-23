3786e9548c7676ac9b8bfc4c589f2065e4e53893
war-in <war-in@users.noreply.github.com>
2026-03-23 12:48:30 +0100

    feat: support regular text in HTMLMessagesRow

--- src/pages/domain/Groups/HTMLMessagesRow.tsx
+++ src/pages/domain/Groups/HTMLMessagesRow.tsx
@@ -4,6 +4,7 @@ import MessagesRow from '@components/MessagesRow';
 import RenderHTML from '@components/RenderHTML';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {getLatestErrorMessage} from '@libs/ErrorUtils';
+import Parser from '@libs/Parser';
 import type {Errors} from '@src/types/onyx/OnyxCommon';
 import {isEmptyObject} from '@src/types/utils/EmptyObject';
 
@@ -15,22 +16,28 @@ type HTMLMessagesRowProps = {
 function HTMLMessagesRow({errors, onDismiss}: HTMLMessagesRowProps) {
     const styles = useThemeStyles();
 
+    if (!errors || isEmptyObject(errors)) {
+        return null;
+    }
+
+    const latestErrorMessage = getLatestErrorMessage({errors});
+    const messages = {
+        error: Parser.isHTML(latestErrorMessage) ? (
+            <View style={[styles.renderHTML, styles.flexRow]}>
+                <RenderHTML html={latestErrorMessage} />
+            </View>
+        ) : (
+            latestErrorMessage
+        ),
+    };
+
     return (
-        !!errors &&
-        !isEmptyObject(errors) && (
-            <MessagesRow
-                type="error"
-                messages={{
-                    error: (
-                        <View style={[styles.renderHTML, styles.flexRow]}>
-                            <RenderHTML html={getLatestErrorMessage({errors})} />
-                        </View>
-                    ),
-                }}
-                onDismiss={onDismiss}
-                containerStyles={[styles.mh5, styles.mt3]}
-            />
-        )
+        <MessagesRow
+            type="error"
+            messages={messages}
+            onDismiss={onDismiss}
+            containerStyles={[styles.mh5, styles.mt3]}
+        />
     );
 }
 
