efbb5ddc8cf4c370614efc03cfb5844210d0a0a5
Jack Senyitko <jsenyitko@expensify.com>
2026-03-25 09:52:40 -0400

    add a loading state to the component

--- src/components/Search/SearchPageHeader/FeedFilterPopup.tsx
+++ src/components/Search/SearchPageHeader/FeedFilterPopup.tsx
@@ -1,23 +1,38 @@
-
-import { MultiSelectItem, MultiSelectItem } from '@components/Search/FilterDropdowns/MultiSelectPopup';
+import {useEffect} from 'react';
+import type {MultiSelectItem} from '@components/Search/FilterDropdowns/MultiSelectPopup';
+import useNetwork from '@hooks/useNetwork';
+import useOnyx from '@hooks/useOnyx';
+import {openSearchCardFiltersPage} from '@libs/actions/Search';
+import ONYXKEYS from '@src/ONYXKEYS';
 import MultiSelectFilterPopup from './MultiSelectFilterPopup';
 
 type FeedFilterPopupProps = {
-            items:  Array<MultiSelectItem<string>>
-            value:  Array<MultiSelectItem<string>>
-   closeOverlay: () => void;
-            onChangeCallback: (items: Array<MultiSelectItem<string>>) => void
-}
+    items: Array<MultiSelectItem<string>>;
+    value: Array<MultiSelectItem<string>>;
+    closeOverlay: () => void;
+    onChangeCallback: (items: Array<MultiSelectItem<string>>) => void;
+};
 
-function FeedFilterPopup({ closeOverlay,  items, value, onChangeCallback }: FeedFilterPopupProps) {
+function FeedFilterPopup({closeOverlay, items, value, onChangeCallback}: FeedFilterPopupProps) {
+    const {isOffline} = useNetwork();
+    const [areCardsLoaded] = useOnyx(ONYXKEYS.IS_SEARCH_FILTERS_CARD_DATA_LOADED);
 
+    useEffect(() => {
+        if (isOffline) {
+            return;
+        }
+        openSearchCardFiltersPage();
+    }, [isOffline]);
+
+    const shouldShowLoadingState = !areCardsLoaded && !isOffline;
 
     return (
         <MultiSelectFilterPopup
-            closeOverlay={closeOverlay} 
-            translationKey="search.filters.feed"
             items={items}
             value={value}
+            loading={shouldShowLoadingState}
+            translationKey="search.filters.feed"
+            closeOverlay={closeOverlay}
             onChangeCallback={onChangeCallback}
         />
     );
