ce1490a21922c766118c83768f1910e6e7338df0
Jack Senyitko <jsenyitko@expensify.com>
2026-03-25 09:47:10 -0400

    add back imports

--- src/components/Search/SearchPageHeader/FeedFilterPopup.tsx
+++ src/components/Search/SearchPageHeader/FeedFilterPopup.tsx
@@ -1,10 +1,23 @@
-import {PopoverComponentProps} from '../FilterDropdowns/DropdownButton';
+import {PopoverComponentProps} from '@components/Search/FilterDropdowns/DropdownButton';
+import useFeedKeysWithAssignedCards from '@hooks/useFeedKeysWithAssignedCards';
+import useLocalize from '@hooks/useLocalize';
+import useOnyx from '@hooks/useOnyx';
+import {getFeedOptions} from '@libs/SearchUIUtils';
+import ONYXKEYS from '@src/ONYXKEYS';
 import MultiSelectFilterPopup from './MultiSelectFilterPopup';
 
-function FeedFilterPopup({}: PopoverComponentProps) {
+function FeedFilterPopup({closeOverlay}: PopoverComponentProps) {
+    const {translate, localeCompare} = useLocalize();
+    const feedKeysWithCards = useFeedKeysWithAssignedCards();
+
+    const [allFeeds] = useOnyx(ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER);
+    const [personalAndWorkspaceCards] = useOnyx(ONYXKEYS.DERIVED.PERSONAL_AND_WORKSPACE_CARD_LIST);
+
+    const feedOptions = getFeedOptions(allFeeds, personalAndWorkspaceCards, translate, localeCompare, feedKeysWithCards);
+
     return (
         <MultiSelectFilterPopup
-            closeOverlay={props.closeOverlay}
+            closeOverlay={closeOverlay}
             translationKey="search.filters.feed"
             items={feedOptions}
             value={feed}
