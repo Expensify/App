91af34f169cbe0611ec167470979b358d317a056
dmkt9 <demoskratos9x@gmail.com>
2026-03-26 18:48:04 +0700

    fix: add date of birth validation

--- src/pages/MissingPersonalDetails/subPages/DateOfBirth.tsx
+++ src/pages/MissingPersonalDetails/subPages/DateOfBirth.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DateOfBirthStep from '@components/SubStepForms/DateOfBirthStep';
 import useLocalize from '@hooks/useLocalize';
 import usePersonalDetailsFormSubmit from '@hooks/usePersonalDetailsFormSubmit';
-import {isValidDate} from '@libs/ValidationUtils';
+import {isValidPastDate, meetsMaximumAgeRequirement, meetsMinimumAgeRequirement} from '@libs/ValidationUtils';
 import type {CustomSubPageProps} from '@pages/MissingPersonalDetails/types';
 import ONYXKEYS from '@src/ONYXKEYS';
 import INPUT_IDS from '@src/types/form/PersonalDetailsForm';
@@ -18,6 +18,9 @@ function DateOfBirth({isEditing, onNext, onMove, personalDetailsValues}: CustomS
         shouldSaveDraft: true,
     });
 
+    const dobValue = personalDetailsValues[INPUT_IDS.DATE_OF_BIRTH];
+    const isValidDOB = dobValue !== '' && isValidPastDate(dobValue) && meetsMaximumAgeRequirement(dobValue) && meetsMinimumAgeRequirement(dobValue);
+
     return (
         <DateOfBirthStep<typeof ONYXKEYS.FORMS.PERSONAL_DETAILS_FORM>
             isEditing={isEditing}
@@ -28,7 +31,7 @@ function DateOfBirth({isEditing, onNext, onMove, personalDetailsValues}: CustomS
             onSubmit={handleSubmit}
             stepFields={STEP_FIELDS}
             dobInputID={INPUT_IDS.DATE_OF_BIRTH}
-            dobDefaultValue={isValidDate(personalDetailsValues[INPUT_IDS.DATE_OF_BIRTH]) ? personalDetailsValues[INPUT_IDS.DATE_OF_BIRTH] : ''}
+            dobDefaultValue={isValidDOB ? dobValue : ''}
         />
     );
 }
