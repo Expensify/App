08a02f229b420712828d0ac01e772c038080d2c9
Jan Nowakowski <janeknowakowski9@gmail.com>
2026-03-27 12:42:47 +0100

    fix linter

--- src/components/Search/SearchList/ListItem/StatusCell.tsx
+++ src/components/Search/SearchList/ListItem/StatusCell.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import StatusBadge from '@components/StatusBadge';
 import useLocalize from '@hooks/useLocalize';
