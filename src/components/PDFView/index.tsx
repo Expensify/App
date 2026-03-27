8898ac86a1686ee93c670da09de4548a668a5024
Shubham Agrawal <shubham.agra1206@gmail.com>
2026-03-27 11:33:10 +0530

    Fix ESLint

--- src/components/PDFView/index.tsx
+++ src/components/PDFView/index.tsx
@@ -137,6 +137,7 @@ function PDFView({onToggleKeyboard, fileName, onPress, isFocused, sourceURL, sty
             accessibilityRole={CONST.ROLE.BUTTON}
             // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
             accessibilityLabel={fileName || translate('attachmentView.unknownFilename')}
+            sentryLabel={CONST.SENTRY_LABEL.PDF_VIEW.DOCUMENT}
         >
             {renderPDFView()}
         </PressableWithoutFeedback>
