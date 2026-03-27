2d2fcc997563584644845cfd8927af6444446e20
Pujan <shahpujan1992@gmail.com>
2026-03-28 01:19:47 +0530

    prettier fix

--- src/pages/settings/HelpPage/HelpPage.tsx
+++ src/pages/settings/HelpPage/HelpPage.tsx
@@ -12,12 +12,12 @@ import useOnyx from '@hooks/useOnyx';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {openExternalLink} from '@libs/actions/Link';
+import {navigateToConciergeChat} from '@libs/actions/Report';
 import Navigation from '@libs/Navigation/Navigation';
 import colors from '@styles/theme/colors';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import {hasSeenTourSelector} from '@src/selectors/Onboarding';
-import { navigateToConciergeChat } from '@libs/actions/Report';
 
 function HelpPage() {
     const icons = useMemoizedLazyExpensifyIcons(['ConciergeAvatar', 'NewWindow', 'Monitor']);
