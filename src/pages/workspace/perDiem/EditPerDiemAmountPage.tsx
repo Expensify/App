a3de2037546db203e911545317b51027ec40f9ce
Shubham Agrawal <shubham.agra1206@gmail.com>
2026-03-29 15:39:30 +0530

    Fix bug

--- src/pages/workspace/perDiem/EditPerDiemAmountPage.tsx
+++ src/pages/workspace/perDiem/EditPerDiemAmountPage.tsx
@@ -38,7 +38,8 @@ function EditPerDiemAmountPage({route}: EditPerDiemAmountPageProps) {
     const selectedRate = customUnit?.rates?.[rateID];
     const selectedSubrate = selectedRate?.subRates?.find((subRate) => subRate.id === subRateID);
 
-    const defaultAmount = selectedSubrate?.rate ? convertToFrontendAmountAsString(Number(selectedSubrate.rate), CONST.POLICY.CUSTOM_UNIT_RATE_BASE_OFFSET) : undefined;
+    // We default to 2 decimals for per diem rates since we always store amounts to include 2 decimal places.
+    const defaultAmount = selectedSubrate?.rate ? convertToFrontendAmountAsString(Number(selectedSubrate.rate), 2) : undefined;
 
     const {inputCallbackRef} = useAutoFocusInput();
 
