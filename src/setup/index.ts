24c2b718971a5280310c30bc2de865374bae5fd7
VickyStash <vikstash@gmail.com>
2026-03-30 16:16:34 +0200

    Lint fix

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -2,7 +2,6 @@ import toSortedPolyfill from 'array.prototype.tosorted';
 import {I18nManager} from 'react-native';
 import Config from 'react-native-config';
 import Onyx from 'react-native-onyx';
-import {enableFreeze} from 'react-native-screens';
 import intlPolyfill from '@libs/IntlPolyfill';
 import {setDeviceID} from '@userActions/Device';
 import initOnyxDerivedValues from '@userActions/OnyxDerived';
