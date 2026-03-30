1ebaf61360dc1e463f04e97fd131be457a6fc0aa
neil-marcellini <neil@expensify.com>
2026-03-30 15:50:25 -0700

    Fix company size options on employees page

--- src/pages/OnboardingEmployees/BaseOnboardingEmployees.tsx
+++ src/pages/OnboardingEmployees/BaseOnboardingEmployees.tsx
@@ -38,7 +38,7 @@ function BaseOnboardingEmployees({shouldUseNativeStyles, route}: BaseOnboardingE
     const companySizeOptions: OnboardingListItem[] = useMemo(() => {
         const isSmb = onboardingValues?.signupQualifier === CONST.ONBOARDING_SIGNUP_QUALIFIERS.SMB;
         return Object.values(CONST.ONBOARDING_COMPANY_SIZE)
-            .filter((size) => size !== CONST.ONBOARDING_COMPANY_SIZE.MICRO && (!isSmb || size !== CONST.ONBOARDING_COMPANY_SIZE.MICRO_SMALL))
+            .filter((size) => !isSmb || (size !== CONST.ONBOARDING_COMPANY_SIZE.MICRO && size !== CONST.ONBOARDING_COMPANY_SIZE.MICRO_SMALL && size !== CONST.ONBOARDING_COMPANY_SIZE.MICRO_MEDIUM))
             .map((companySize): OnboardingListItem => {
                 return {
                     text: translate(`onboarding.employees.${companySize}`),
