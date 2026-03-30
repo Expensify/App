507fc44bd2d1205258453fd03f777c1bdf5a797e
Agata Kosior <agata.kosior57@gmail.com>
2026-03-30 17:21:32 +0200

    fix: remove unused import

--- src/libs/ReimbursementAccountUtils.ts
+++ src/libs/ReimbursementAccountUtils.ts
@@ -1,6 +1,6 @@
 import type {ValueOf} from 'type-fest';
 import CONST from '@src/CONST';
-import type {ACHDataReimbursementAccount, ReimbursementAccountStep} from '@src/types/onyx/ReimbursementAccount';
+import type {ACHDataReimbursementAccount} from '@src/types/onyx/ReimbursementAccount';
 
 type ReimbursementAccountStepToOpen = ValueOf<typeof REIMBURSEMENT_ACCOUNT_ROUTE_NAMES> | '';
 
