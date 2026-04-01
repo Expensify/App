915e25a7daf8e7941d7319da407653ed41085ed0
truph01 <truphd01@gmail.com>
2026-04-01 14:52:20 +0700

    fix: update hasPreviousApprover

--- src/pages/RejectExpenseReportPage.tsx
+++ src/pages/RejectExpenseReportPage.tsx
@@ -82,8 +82,7 @@ function RejectExpenseReportPage({route}: RejectExpenseReportPageProps) {
     }, [reportActions]);
 
     const submitterAccountID = report?.ownerAccountID ?? CONST.DEFAULT_NUMBER_ID;
-    const hasPreviousApprover = previousApprover !== null && previousApprover.accountID !== currentUserPersonalDetails?.accountID;
-    const isSubmitterSameAsPreviousApprover = hasPreviousApprover && previousApprover.accountID === submitterAccountID;
+    const hasPreviousApprover = previousApprover !== null && previousApprover.accountID !== currentUserPersonalDetails?.accountID && previousApprover.accountID === submitterAccountID;
 
     const options = [];
 
