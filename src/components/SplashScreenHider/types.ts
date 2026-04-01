490a3c5828d4897dd362f8cfe1c9c8d489704f4c
war-in <war-in@users.noreply.github.com>
2026-04-01 14:45:46 +0200

    fix: trigger hide on `shouldHideSplash` change

--- src/components/SplashScreenHider/types.ts
+++ src/components/SplashScreenHider/types.ts
@@ -3,6 +3,9 @@ import type {ReactNode} from 'react';
 type SplashScreenHiderProps = {
     /** Splash screen has been hidden */
     onHide: () => void;
+
+    /** Whether the splash screen should be hidden */
+    shouldHideSplash: boolean;
 };
 
 type SplashScreenHiderReturnType = ReactNode;
