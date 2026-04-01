eb6f6b249797e892457278fdb48c841c47442940
DylanDylann <dylandylan9112@gmail.com>
2026-04-01 13:55:02 +0700

    resolve comment

--- src/pages/iou/request/step/IOURequestStepOdometerImage/index.tsx
+++ src/pages/iou/request/step/IOURequestStepOdometerImage/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
 import {PanResponder, View} from 'react-native';
 import ActivityIndicator from '@components/ActivityIndicator';
 import AttachmentPicker from '@components/AttachmentPicker';
