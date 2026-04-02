3e2e8fdb576e54bbf8e4cb82aaa6a20c5f834ec0
Lucien Akchoté <lucien@expensify.com>
2026-04-02 09:19:55 +0200

    use onyx selector in `useHasPoliciesConnectedToQBD`

--- src/hooks/useHasPoliciesConnectedToQBD.ts
+++ src/hooks/useHasPoliciesConnectedToQBD.ts
@@ -1,10 +1,10 @@
 import ONYXKEYS from '@src/ONYXKEYS';
-import {adminPoliciesConnectedToQBDSelector} from '@src/selectors/Policy';
+import {hasPoliciesConnectedToQBDSelector} from '@src/selectors/Policy';
 import useOnyx from './useOnyx';
 
 function useHasPoliciesConnectedToQBD() {
-    const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
-    return !!adminPoliciesConnectedToQBDSelector(policies).length;
+    const [hasPolicies] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: hasPoliciesConnectedToQBDSelector});
+    return hasPolicies ?? false;
 }
 
 export default useHasPoliciesConnectedToQBD;
