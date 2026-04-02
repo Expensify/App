06b9ecdcad7ccb1e1cf25815aee681d8a2b46399
Rodrigo Lino da Costa <rodrigo@expensify.com>
2026-04-02 13:46:04 -0300

    prettier

--- src/components/MoneyReportHeaderEducationalModals.tsx
+++ src/components/MoneyReportHeaderEducationalModals.tsx
@@ -1,5 +1,5 @@
+import {shouldFailAllRequestsSelector} from '@selectors/Network';
 import React from 'react';
-import { shouldFailAllRequestsSelector } from '@selectors/Network';
 import type {OnyxEntry} from 'react-native-onyx';
 import type {ValueOf} from 'type-fest';
 import useNetwork from '@hooks/useNetwork';
