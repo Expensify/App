e1d028ce7feda615ebf8ba79398e7259ae8da485
daledah <daleda261@outlook.com>
2026-04-03 10:44:36 +0700

    add missing files

---
+++ src/components/SelectionList/utils/getContainerRole/index.ts
@@ -0,0 +1,6 @@
+import type {GetContainerRole} from './types';
+
+const getContainerRole: GetContainerRole = () => undefined;
+
+// eslint-disable-next-line import/prefer-default-export
+export {getContainerRole};
---
+++ src/components/SelectionList/utils/getContainerRole/types.ts
@@ -0,0 +1,6 @@
+import type {Role} from 'react-native';
+
+type GetContainerRole = (canSelectMultiple: boolean) => Role | undefined;
+
+// eslint-disable-next-line import/prefer-default-export
+export type {GetContainerRole};
