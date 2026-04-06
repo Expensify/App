4c5590b9f9fea554273bb4136a8a32bfbc7d82e0
Milos Prodanovic <milosh.prodanovic@gmail.com>
2026-04-05 17:10:27 -0700

    Add fallback to workspaceAccountID for backward compatibility with cached data

--- src/hooks/useWorkspaceAccountID.ts
+++ src/hooks/useWorkspaceAccountID.ts
@@ -4,7 +4,7 @@ import ONYXKEYS from '@src/ONYXKEYS';
 import type {Policy} from '@src/types/onyx';
 import useOnyx from './useOnyx';
 
-const policyWorkspaceAccountIDSelector = (policy: OnyxEntry<Policy>) => policy?.policyAccountID;
+const policyWorkspaceAccountIDSelector = (policy: OnyxEntry<Policy>) => policy?.policyAccountID ?? policy?.workspaceAccountID;
 
 function useWorkspaceAccountID(policyID: string | undefined) {
     const [workspaceAccountID = CONST.DEFAULT_NUMBER_ID] = useOnyx(`${ONYXKEYS.COLLECTION.POLICY}${policyID}`, {selector: policyWorkspaceAccountIDSelector});
