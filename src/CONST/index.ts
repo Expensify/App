27b78876b85a8aee8a59ab80ac91ae8604d4d72e
Jack Senyitko <49812749+JS00001@users.noreply.github.com>
2026-04-07 10:08:33 -0400

    Merge pull request #87210 from nkdengineer/fix/87208
    [CP Staging] fix analytics event
    
    (cherry picked from commit 7b8fde7d1dac1f48a49e7344cd04446f54576d51)
    
    (cherry-picked to staging by roryabraham)

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -8433,9 +8433,9 @@ const CONST = {
             SIGN_UP: 'sign_up',
             WORKSPACE_CREATED: 'workspace_created',
             PAID_ADOPTION: 'paid_adoption',
-            PRODUCT_TRAINING_SCAN_TEST_TOOLTIP_SHOWN: 'prod_training_scan_test_tooltip_shown',
-            PRODUCT_TRAINING_SCAN_TEST_TOOLTIP_DISMISSED: 'prod_training_scan_test_tooltip_dismissed',
-            PRODUCT_TRAINING_SCAN_TEST_TOOLTIP_CONFIRMED: 'prod_training_scan_test_tooltip_confirmed',
+            PRODUCT_TRAINING_SCAN_TEST_TOOLTIP_SHOWN: 'training_scan_test_tooltip_shown',
+            PRODUCT_TRAINING_SCAN_TEST_TOOLTIP_DISMISSED: 'training_scan_test_tooltip_dismissed',
+            PRODUCT_TRAINING_SCAN_TEST_TOOLTIP_CONFIRMED: 'training_scan_test_tooltip_confirmed',
         },
     },
 
