b7b1bad13a058da699c9c33c48dec965378a6329
DylanDylann <dylandylan9112@gmail.com>
2026-04-08 00:17:12 +0700

    prettier

--- src/pages/iou/request/step/IOURequestStepMerchant.tsx
+++ src/pages/iou/request/step/IOURequestStepMerchant.tsx
@@ -20,8 +20,8 @@ import {getTransactionDetails, isExpenseRequest, isPolicyExpenseChat} from '@lib
 import {hasReceipt} from '@libs/TransactionUtils';
 import {isInvalidMerchantValue, isValidInputLength} from '@libs/ValidationUtils';
 import {setMoneyRequestMerchant} from '@userActions/IOU';
-import {updateMoneyRequestMerchant} from '@userActions/IOU/UpdateMoneyRequest';
 import {setDraftSplitTransaction} from '@userActions/IOU/Split';
+import {updateMoneyRequestMerchant} from '@userActions/IOU/UpdateMoneyRequest';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type SCREENS from '@src/SCREENS';
