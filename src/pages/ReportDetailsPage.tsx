77a6bbf91864809a8289b35c52c492281456aa84
daledah <daleda261@outlook.com>
2026-04-07 16:38:19 +0700

    fix prettier

--- src/pages/ReportDetailsPage.tsx
+++ src/pages/ReportDetailsPage.tsx
@@ -1,7 +1,7 @@
 import {StackActions} from '@react-navigation/native';
 import {validTransactionDraftIDsSelector} from '@selectors/TransactionDraft';
 import React, {useCallback, useEffect, useMemo} from 'react';
-import type { StyleProp, TextStyle} from 'react-native';
+import type {StyleProp, TextStyle} from 'react-native';
 import {InteractionManager, View} from 'react-native';
 import type {OnyxEntry} from 'react-native-onyx';
 import type {ValueOf} from 'type-fest';
