60cb0d336cbfea51814bebcbaf520480f1b19489
GCyganek <gabrielcyganek@gmail.com>
2026-04-08 14:53:06 +0200

    bottomSuggestionPadding 16 -> 30 on ios

--- src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.ios.ts
+++ src/components/AutoCompleteSuggestions/AutoCompleteSuggestionsPortal/getBottomSuggestionPadding/index.ios.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function getBottomSuggestionPadding(bottom?: number, isInLandscapeMode?: boolean): number {
-    return 16;
+    return 30;
 }
 
 export default getBottomSuggestionPadding;
