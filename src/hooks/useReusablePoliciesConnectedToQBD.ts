289ae49483d962d1776042f41300ace0dada1844
Lucien Akchoté <lucien@expensify.com>
2026-04-09 08:58:19 +0200

    add  hook

---
+++ src/hooks/useReusablePoliciesConnectedToQBD.ts
@@ -0,0 +1,21 @@
+import ONYXKEYS from '@src/ONYXKEYS';
+import {reusablePoliciesConnectedToQBDSelector} from '@src/selectors/Policy';
+import useOnyx from './useOnyx';
+
+function useReusablePoliciesConnectedToQBD(policyID: string | undefined) {
+    const [policies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
+    const [connectionSyncProgressCollection] = useOnyx(ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS);
+    const reusablePoliciesConnectedToQBD = reusablePoliciesConnectedToQBDSelector(
+        policies,
+        connectionSyncProgressCollection,
+        policyID,
+    );
+
+    return {
+        connectionSyncProgressCollection,
+        hasReusablePoliciesConnectedToQBD: reusablePoliciesConnectedToQBD.length > 0,
+        reusablePoliciesConnectedToQBD,
+    };
+}
+
+export default useReusablePoliciesConnectedToQBD;
