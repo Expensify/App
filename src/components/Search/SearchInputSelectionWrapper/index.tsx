2abce15b8156925d8160ac55a45ab4d869b86b57
staszekscp <mariusz.stanisz@swmansion.com>
2026-04-10 09:05:53 +0200

    Remove web improvements

--- src/components/Search/SearchInputSelectionWrapper/index.tsx
+++ src/components/Search/SearchInputSelectionWrapper/index.tsx
@@ -1,28 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import SearchAutocompleteInput from '@components/Search/SearchAutocompleteInput';
 import type {SearchAutocompleteInputProps} from '@components/Search/SearchAutocompleteInput';
-import SearchInputSelectionSkeleton from '@components/Skeletons/SearchInputSelectionSkeleton';
-
-let isAutocompleteInputInitialized = false;
 
 function SearchInputSelectionWrapper({selection, ref, ...props}: SearchAutocompleteInputProps) {
-    const [showSkeleton, setShowSkeleton] = useState(!isAutocompleteInputInitialized);
-
-    useEffect(() => {
-        if (isAutocompleteInputInitialized) {
-            return;
-        }
-        isAutocompleteInputInitialized = true;
-        const id = requestAnimationFrame(() => {
-            setShowSkeleton(false);
-        });
-        return () => cancelAnimationFrame(id);
-    }, []);
-
-    if (showSkeleton) {
-        return <SearchInputSelectionSkeleton reasonAttributes={{context: 'SearchInputSelectionWrapper'}} />;
-    }
-
     return (
         <SearchAutocompleteInput
             selection={selection}
