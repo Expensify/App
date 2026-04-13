06b061a9171216038a4a166de55af069f07ea0b2
staszekscp <mariusz.stanisz@swmansion.com>
2026-04-13 10:18:37 +0200

    Fix TS and Lint

--- src/components/Search/SearchAutocompleteList.tsx
+++ src/components/Search/SearchAutocompleteList.tsx
@@ -209,8 +209,7 @@ function SearchAutocompleteList({
             personalDetails,
         });
     }, [
-        areOptionsInitialized,
-        options,
+        listOptions,
         draftComments,
         nvpDismissedProductTraining,
         betas,
