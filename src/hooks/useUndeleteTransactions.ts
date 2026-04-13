ed46281acfe76fe5df0f7cfc8f8fd64ecf5f8c8f
Jan Nowakowski <janeknowakowski9@gmail.com>
2026-04-13 14:03:02 +0200

    Fix typescript

--- src/hooks/useUndeleteTransactions.ts
+++ src/hooks/useUndeleteTransactions.ts
@@ -26,8 +26,6 @@ function useUndeleteTransactions() {
             email: currentUserPersonalDetails.email ?? '',
             policy,
             allTransactions,
-            translate,
-            toLocaleDigit,
         });
     };
 }
