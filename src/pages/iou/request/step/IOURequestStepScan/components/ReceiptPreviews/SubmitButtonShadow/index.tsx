095c8a0cbeba15248e5aec838b2627dc462875b2
GCyganek <gabrielcyganek@gmail.com>
2026-04-14 12:31:13 +0200

    New layout for Scan screen in landscape mode

--- src/pages/iou/request/step/IOURequestStepScan/components/ReceiptPreviews/SubmitButtonShadow/index.tsx
+++ src/pages/iou/request/step/IOURequestStepScan/components/ReceiptPreviews/SubmitButtonShadow/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
 import useThemeStyles from '@hooks/useThemeStyles';
-import type ChildrenProps from '@src/types/utils/ChildrenProps';
+import type {SubmitButtonShadowProps} from './types';
 
-function SubmitButtonShadow({children}: ChildrenProps) {
+// isInLandscapeMode is only used on native platforms.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function SubmitButtonShadow({children, isInLandscapeMode}: SubmitButtonShadowProps) {
     const styles = useThemeStyles();
 
     return <View style={[styles.receiptsSubmitButton, styles.buttonShadowContainer, styles.webButtonShadow]}>{children}</View>;
