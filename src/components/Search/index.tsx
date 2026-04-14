ad4b6ef78be189cab5f9f3e654d8f910f998b0de
Jack Senyitko <jsenyitko@expensify.com>
2026-04-14 08:54:30 -0400

    fix bug

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1074,7 +1074,7 @@ function Search({
             if (isTransactionGroupListItemType(item) && !isTransactionReportGroupListItemType(item) && item.transactionsQueryJSON) {
                 handleSearch({
                     queryJSON: item.transactionsQueryJSON,
-                    searchKey: currentSearchKey,
+                    searchKey: undefined,
                     offset: 0,
                     shouldCalculateTotals: false,
                     isLoading: false,
