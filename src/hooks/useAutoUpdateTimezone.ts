71eb62ea8204cad1b4c0358caf4306849c560fea
Shubham Agrawal <shubham.agra1206@gmail.com>
2026-04-15 12:58:13 +0530

    Fixed new login case

--- src/hooks/useAutoUpdateTimezone.ts
+++ src/hooks/useAutoUpdateTimezone.ts
@@ -4,6 +4,7 @@ import {updateAutomaticTimezone} from '@userActions/PersonalDetails';
 import ONYXKEYS from '@src/ONYXKEYS';
 import {isActingAsDelegateSelector} from '@src/selectors/Account';
 import type {SelectedTimezone} from '@src/types/onyx/PersonalDetails';
+import {isEmptyObject} from '@src/types/utils/EmptyObject';
 import useAppFocusEvent from './useAppFocusEvent';
 import useCurrentUserPersonalDetails from './useCurrentUserPersonalDetails';
 import useOnyx from './useOnyx';
@@ -12,6 +13,7 @@ const useAutoUpdateTimezone = () => {
     const [isActingAsDelegate] = useOnyx(ONYXKEYS.ACCOUNT, {selector: isActingAsDelegateSelector});
     const currentUserPersonalDetails = useCurrentUserPersonalDetails();
     const timezone = currentUserPersonalDetails?.timezone ?? {};
+    const isTimezonePresent = !isEmptyObject(timezone);
     const updateTimezone = () => {
         if (isActingAsDelegate) {
             // Do not update timezone if user is acting as a delegate, as the timezone should be based on the primary account holder, not the delegate.
@@ -31,12 +33,12 @@ const useAutoUpdateTimezone = () => {
     const updateTimezoneEvent = useEffectEvent(updateTimezone);
     useAppFocusEvent(updateTimezone);
     useEffect(() => {
-        if (!currentUserPersonalDetails.accountID) {
-            // If we don't have an accountID, we can't update the timezone, so we should wait until we do before attempting to update it.
+        if (!isTimezonePresent) {
+            // If we don't have a timezone, we can't update it, so we should wait until we do before attempting to update it.
             return;
         }
         updateTimezoneEvent();
-    }, [currentUserPersonalDetails.accountID]);
+    }, [isTimezonePresent]);
 };
 
 export default useAutoUpdateTimezone;
