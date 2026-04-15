b969d3eecf2e16985057dfd89722c12b8c31ebfc
truph01 <truphd01@gmail.com>
2026-04-16 06:23:25 +0700

    fix: revert change

--- src/libs/actions/IOU/ReportWorkflow.ts
+++ src/libs/actions/IOU/ReportWorkflow.ts
@@ -1239,7 +1239,7 @@ function submitReport({
     if (!expenseReport) {
         return;
     }
-    if (expenseReport.policyID && shouldRestrictUserBillableActions(policy, ownerBillingGracePeriodEnd, userBillingGracePeriodEnds, amountOwed)) {
+    if (expenseReport.policyID && shouldRestrictUserBillableActions(expenseReport.policyID, ownerBillingGracePeriodEnd, userBillingGracePeriodEnds, amountOwed)) {
         Navigation.navigate(ROUTES.RESTRICTED_ACTION.getRoute(expenseReport.policyID));
         return;
     }
