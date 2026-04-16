45b415ad0b0b4be4f85530f267ac2f18e3a3e3b6
Jack Senyitko <jsenyitko@expensify.com>
2026-04-16 12:40:10 -0400

    add state

--- src/components/Search/SearchPageHeader/FeedFilterPopup.tsx
+++ src/components/Search/SearchPageHeader/FeedFilterPopup.tsx
@@ -1,8 +1,11 @@
 import React, {useEffect} from 'react';
 import type {MultiSelectItem} from '@components/Search/FilterDropdowns/MultiSelectPopup';
+import useFeedKeysWithAssignedCards from '@hooks/useFeedKeysWithAssignedCards';
+import useLocalize from '@hooks/useLocalize';
 import useNetwork from '@hooks/useNetwork';
 import useOnyx from '@hooks/useOnyx';
 import {openSearchCardFiltersPage} from '@libs/actions/Search';
+import {getFeedOptions} from '@libs/SearchUIUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 import MultiSelectFilterPopup from './MultiSelectFilterPopup';
 
@@ -16,7 +19,14 @@ type FeedFilterPopupProps = {
 
 function FeedFilterPopup({closeOverlay, items, value, isExpanded, onChangeCallback}: FeedFilterPopupProps) {
     const {isOffline} = useNetwork();
+    const {translate, localeCompare} = useLocalize();
+    const feedKeysWithCards = useFeedKeysWithAssignedCards();
+
     const [areCardsLoaded] = useOnyx(ONYXKEYS.IS_SEARCH_FILTERS_CARD_DATA_LOADED);
+    const [allFeeds] = useOnyx(ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER);
+    const [personalAndWorkspaceCards] = useOnyx(ONYXKEYS.DERIVED.PERSONAL_AND_WORKSPACE_CARD_LIST);
+
+    const feedOptions = getFeedOptions(allFeeds, personalAndWorkspaceCards, translate, localeCompare, feedKeysWithCards);
 
     useEffect(() => {
         if (isOffline || !isExpanded) {
