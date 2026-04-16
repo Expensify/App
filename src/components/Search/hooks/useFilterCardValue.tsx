7c760bfe7de55937fa1440fb286c18fa8370ace3
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-04-17 01:51:18 +0800

    use different name for the feeds

--- src/components/Search/hooks/useFilterCardValue.tsx
+++ src/components/Search/hooks/useFilterCardValue.tsx
@@ -1,48 +1,31 @@
 import useAdvancedSearchFilters from '@hooks/useAdvancedSearchFilters';
+import useFeedKeysWithAssignedCards from '@hooks/useFeedKeysWithAssignedCards';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
-import {createCardFeedKey, getCardFeedKey, getCardFeedNamesWithType, getWorkspaceCardFeedKey} from '@libs/CardFeedUtils';
+import {createCardFeedKey, getCardFeedsForDisplay} from '@libs/CardFeedUtils';
 import {getCardDescription} from '@libs/CardUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {WorkspaceCardsList} from '@src/types/onyx';
 
 function useFilterCardValue(): string {
     const {translate} = useLocalize();
-    const {policies, searchCards} = useAdvancedSearchFilters();
+    const {searchCards} = useAdvancedSearchFilters();
+    const feedKeysWithCards = useFeedKeysWithAssignedCards();
 
     const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM);
+    const [allFeeds] = useOnyx(ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER);
+
     const cardIdsFilter = searchAdvancedFiltersForm?.[CONST.SEARCH.SYNTAX_FILTER_KEYS.CARD_ID] ?? [];
     const feedFilter = searchAdvancedFiltersForm?.[CONST.SEARCH.SYNTAX_FILTER_KEYS.FEED] ?? [];
-    const workspaceCardFeeds = Object.entries(searchCards ?? {}).reduce<Record<string, WorkspaceCardsList>>((acc, [cardID, card]) => {
-        const feedKey = `${createCardFeedKey(card.fundID, card.bank)}`;
-        const workspaceFeedKey = getWorkspaceCardFeedKey(feedKey);
-        if (!acc[workspaceFeedKey]) {
-            acc[workspaceFeedKey] = {};
-        }
-
-        acc[workspaceFeedKey][cardID] = card;
-        return acc;
-    }, {});
-
-    const cardFeedNamesWithType = getCardFeedNamesWithType({
-        workspaceCardFeeds,
-        policies,
-        translate,
-    });
 
     const cardNames = Object.values(searchCards ?? {})
         .filter((card) => cardIdsFilter.includes(card.cardID.toString()) && !feedFilter.includes(createCardFeedKey(card.fundID, card.bank)))
         .map((card) => getCardDescription(card, translate));
 
-    const feedNames = Object.keys(cardFeedNamesWithType)
-        .filter((workspaceCardFeedKey) => {
-            const feedKey = getCardFeedKey(workspaceCardFeeds, workspaceCardFeedKey);
-            return !!feedKey && feedFilter.includes(feedKey);
-        })
-        .map((cardFeedKey) => cardFeedNamesWithType[cardFeedKey].name);
+    const feedAutoCompleteList = Object.values(getCardFeedsForDisplay(allFeeds, {}, translate, feedKeysWithCards));
+    const filteredFeeds = feedAutoCompleteList.filter((feed) => feedFilter.includes(feed.id)).map((feed) => feed.name);
 
-    return [...feedNames, ...cardNames].join(', ');
+    return [...filteredFeeds, ...cardNames].join(', ');
 }
 
 export default useFilterCardValue;
