238434ce0193d6fb22a30a9970a23a064dc5ea6e
Jack Senyitko <jsenyitko@expensify.com>
2026-04-16 12:47:09 -0400

    refactor/move to new file

--- src/components/Search/FilterDropdowns/FeedSelectPopup.tsx
+++ src/components/Search/FilterDropdowns/FeedSelectPopup.tsx
@@ -6,27 +6,29 @@ import useNetwork from '@hooks/useNetwork';
 import useOnyx from '@hooks/useOnyx';
 import {openSearchCardFiltersPage} from '@libs/actions/Search';
 import {getFeedOptions} from '@libs/SearchUIUtils';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import MultiSelectFilterPopup from './MultiSelectFilterPopup';
+import MultiSelectFilterPopup from '../SearchPageHeader/MultiSelectFilterPopup';
 
 type FeedFilterPopupProps = {
     isExpanded: boolean;
-    items: Array<MultiSelectItem<string>>;
-    value: Array<MultiSelectItem<string>>;
     closeOverlay: () => void;
     onChangeCallback: (items: Array<MultiSelectItem<string>>) => void;
 };
 
-function FeedFilterPopup({closeOverlay, items, value, isExpanded, onChangeCallback}: FeedFilterPopupProps) {
+function FeedSelectPopup({closeOverlay, isExpanded, onChangeCallback}: FeedFilterPopupProps) {
     const {isOffline} = useNetwork();
     const {translate, localeCompare} = useLocalize();
     const feedKeysWithCards = useFeedKeysWithAssignedCards();
 
     const [areCardsLoaded] = useOnyx(ONYXKEYS.IS_SEARCH_FILTERS_CARD_DATA_LOADED);
     const [allFeeds] = useOnyx(ONYXKEYS.COLLECTION.SHARED_NVP_PRIVATE_DOMAIN_MEMBER);
+    const [searchAdvancedFiltersForm] = useOnyx(ONYXKEYS.FORMS.SEARCH_ADVANCED_FILTERS_FORM);
     const [personalAndWorkspaceCards] = useOnyx(ONYXKEYS.DERIVED.PERSONAL_AND_WORKSPACE_CARD_LIST);
 
+    const feedFilterValues = searchAdvancedFiltersForm?.[CONST.SEARCH.SYNTAX_FILTER_KEYS.FEED] ?? [];
     const feedOptions = getFeedOptions(allFeeds, personalAndWorkspaceCards, translate, localeCompare, feedKeysWithCards);
+    const feed = feedFilterValues ? feedOptions.filter((option) => feedFilterValues.includes(option.value)) : [];
 
     useEffect(() => {
         if (isOffline || !isExpanded) {
@@ -49,4 +51,4 @@ function FeedFilterPopup({closeOverlay, items, value, isExpanded, onChangeCallba
     );
 }
 
-export default FeedFilterPopup;
+export default FeedSelectPopup;
