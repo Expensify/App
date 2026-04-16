2945128d37efcf6fe689481f3cd917059ceee899
Samran Ahmed <samranahmed03@gmail.com>
2026-04-17 02:21:02 +0500

    Update export action hooks to use TablePencil for custom templates

--- src/hooks/useSearchBulkActions.ts
+++ src/hooks/useSearchBulkActions.ts
@@ -257,6 +257,7 @@ function useSearchBulkActions({queryJSON}: UseSearchBulkActionsParams) {
     const expensifyIcons = useMemoizedLazyExpensifyIcons([
         'Export',
         'Table',
+        'TablePencil',
         'DocumentMerge',
         'Send',
         'Trashcan',
@@ -1019,9 +1020,11 @@ function useSearchBulkActions({queryJSON}: UseSearchBulkActionsParams) {
             });
 
             for (const template of exportTemplates) {
+                const isStandardTemplate =
+                    template.templateName === CONST.REPORT.EXPORT_OPTIONS.EXPENSE_LEVEL_EXPORT || template.templateName === CONST.REPORT.EXPORT_OPTIONS.REPORT_LEVEL_EXPORT;
                 exportOptions.push({
                     text: template.name,
-                    icon: expensifyIcons.Table,
+                    icon: isStandardTemplate ? expensifyIcons.Table : expensifyIcons.TablePencil,
                     description: template.description,
                     onSelected: () => {
                         beginExportWithTemplate(template.templateName, template.type, template.policyID);
