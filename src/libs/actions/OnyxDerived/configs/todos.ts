ec8a94d0179a68d03fbde5beb4e054a72dd27fb8
annaweber <annaweber830@outlook.com>
2026-04-17 15:04:19 -0700

    fix: remove unnessary code

--- src/libs/actions/OnyxDerived/configs/todos.ts
+++ src/libs/actions/OnyxDerived/configs/todos.ts
@@ -1,6 +1,6 @@
 import type {OnyxCollection, OnyxEntry} from 'react-native-onyx';
-import {isApproveAction, isExportAction, isPrimaryPayAction} from '@libs/ReportPrimaryActionUtils';
-import {hasOnlyNonReimbursableTransactions, isArchivedReport, isCurrentUserSubmitter, isOpenExpenseReport} from '@libs/ReportUtils';
+import {isApproveAction, isExportAction, isPrimaryPayAction, isSubmitAction} from '@libs/ReportPrimaryActionUtils';
+import {hasOnlyNonReimbursableTransactions} from '@libs/ReportUtils';
 import createOnyxDerivedValueConfig from '@userActions/OnyxDerived/createOnyxDerivedValueConfig';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
@@ -58,8 +58,7 @@ const createTodosReportsAndTransactions = ({
         const reportActions = Object.values(allReportActions?.[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${report.reportID}`] ?? []);
         const reportTransactions = transactionsByReportID[report.reportID] ?? [];
         const reportMetadata = allReportMetadata?.[`${ONYXKEYS.COLLECTION.REPORT_METADATA}${report.reportID}`];
-        const shouldShowInSubmitTodo = isOpenExpenseReport(report) && isCurrentUserSubmitter(report) && reportTransactions.length > 0 && !isArchivedReport(reportNameValuePair);
-        if (shouldShowInSubmitTodo) {
+        if (isSubmitAction(report, reportTransactions, reportMetadata, policy, reportNameValuePair, undefined, login, currentUserAccountID)) {
             reportsToSubmit.push(report);
         }
         if (isApproveAction(report, reportTransactions, currentUserAccountID, reportMetadata, policy)) {
