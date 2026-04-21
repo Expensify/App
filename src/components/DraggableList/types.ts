29ad30583abebf6fbba89503552cc86393fc2f0c
MobileMage <aidelojejoshua@gmail.com>
2026-04-21 11:51:18 +0100

    Revert "Add keyboard reorder mode for native DraggableList"
    This reverts commit c51ebb389484d5fe5f5da3b388bd5c952ff2f7b5.

--- src/components/DraggableList/types.ts
+++ src/components/DraggableList/types.ts
@@ -3,8 +3,6 @@ import type {RenderItemParams} from 'react-native-draggable-flatlist';
 
 type DraggableListRenderItemParams<T> = RenderItemParams<T> & {
     isFocused?: boolean;
-    /** Whether the list is currently in keyboard reorder mode (native only) */
-    isKeyboardMoving?: boolean;
 };
 
 type DraggableListData<T> = {
