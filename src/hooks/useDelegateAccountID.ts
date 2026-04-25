744ea26c9bc31d17f687e813efdae9b6b7f52891
krishna2323 <belivethatkg@gmail.com>
2026-04-26 03:34:36 +0530

    Optimize useDelegateAccountID to use a targeted selector
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/hooks/useDelegateAccountID.ts
+++ src/hooks/useDelegateAccountID.ts
@@ -1,13 +1,26 @@
 import {delegateEmailSelector} from '@selectors/Account';
+import {useCallback} from 'react';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {PersonalDetailsList} from '@src/types/onyx';
 import useOnyx from './useOnyx';
-import usePersonalDetailsByLogin from './usePersonalDetailsByLogin';
 
 function useDelegateAccountID(): number | undefined {
     const [delegateEmail] = useOnyx(ONYXKEYS.ACCOUNT, {selector: delegateEmailSelector});
-    const personalDetailsByLogin = usePersonalDetailsByLogin();
+    const normalizedEmail = delegateEmail?.toLowerCase();
 
-    return delegateEmail ? personalDetailsByLogin[delegateEmail.toLowerCase()]?.accountID : undefined;
+    const selector = useCallback(
+        (personalDetails: PersonalDetailsList | null | undefined): number | undefined => {
+            if (!normalizedEmail || !personalDetails) {
+                return undefined;
+            }
+            return Object.values(personalDetails).find((detail) => detail?.login?.toLowerCase() === normalizedEmail)?.accountID;
+        },
+        [normalizedEmail],
+    );
+
+    const [delegateAccountID] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {selector});
+
+    return delegateAccountID;
 }
 
 export default useDelegateAccountID;
