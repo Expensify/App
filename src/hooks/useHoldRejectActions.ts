156890e0831ec04433f5e887a5a7d4244e17f156
Manan Jadhav <manan.jadhav@gmail.com>
2026-04-27 09:24:05 +0530

    fix: remove isdm check

--- src/hooks/useHoldRejectActions.ts
+++ src/hooks/useHoldRejectActions.ts
@@ -5,7 +5,7 @@ import type {RejectModalAction} from '@components/MoneyReportHeaderEducationalMo
 import getNonEmptyStringOnyxID from '@libs/getNonEmptyStringOnyxID';
 import Navigation from '@libs/Navigation/Navigation';
 import {getOriginalMessage, isMoneyRequestAction} from '@libs/ReportActionsUtils';
-import {changeMoneyRequestHoldStatus, isCurrentUserSubmitter, isDM} from '@libs/ReportUtils';
+import {changeMoneyRequestHoldStatus, isCurrentUserSubmitter} from '@libs/ReportUtils';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
@@ -36,7 +36,6 @@ function useHoldRejectActions({reportID, onHoldEducationalOpen, onRejectModalOpe
     const expensifyIcons = useMemoizedLazyExpensifyIcons(['Stopwatch', 'ThumbsDown'] as const);
 
     const [moneyRequestReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${reportID}`);
-    const [chatReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${getNonEmptyStringOnyxID(moneyRequestReport?.chatReportID)}`);
     const {transactionThreadReport} = useTransactionThreadReport(reportID);
 
     const [reportActionsForParent] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${getNonEmptyStringOnyxID(moneyRequestReport?.reportID)}`);
@@ -53,7 +52,6 @@ function useHoldRejectActions({reportID, onHoldEducationalOpen, onRejectModalOpe
     const [dismissedHoldUseExplanation] = useOnyx(ONYXKEYS.NVP_DISMISSED_HOLD_USE_EXPLANATION);
 
     const isReportSubmitter = isCurrentUserSubmitter(chatIOUReport);
-    const isChatReportDM = isDM(chatReport);
 
     return {
         [CONST.REPORT.SECONDARY_ACTIONS.HOLD]: {
@@ -73,7 +71,7 @@ function useHoldRejectActions({reportID, onHoldEducationalOpen, onRejectModalOpe
 
                 const isDismissed = isReportSubmitter ? dismissedHoldUseExplanation : dismissedRejectUseExplanation;
 
-                if (isDismissed || isChatReportDM) {
+                if (isDismissed) {
                     changeMoneyRequestHoldStatus(requestParentReportAction, transaction, isOffline);
                 } else if (isReportSubmitter) {
                     onHoldEducationalOpen();
