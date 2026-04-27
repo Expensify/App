f5e21e38cd5b123a115e1b53b1ad9e6f3023e914
Muhammet Mihri Sümer <swe@mmsumer.dev>
2026-04-27 20:06:52 +0300

    test: add openApp test cases and improve structure for consistency

--- tests/actions/AppTest.ts
+++ tests/actions/AppTest.ts
@@ -1,5 +1,6 @@
 import type {OnyxCollection} from 'react-native-onyx';
 import Onyx from 'react-native-onyx';
+import * as ActiveClientManager from '@libs/ActiveClientManager';
 import DateUtils from '@libs/DateUtils';
 import '@libs/Navigation/AppNavigator/AuthScreens';
 import OnyxUpdateManager from '@src/libs/actions/OnyxUpdateManager';
@@ -23,6 +24,7 @@ describe('actions/App', () => {
 
     beforeEach(() => {
         global.fetch = TestHelper.getGlobalFetchMock();
+        jest.spyOn(ActiveClientManager, 'isClientTheLeader').mockReturnValue(true);
         return Onyx.clear().then(waitForBatchedUpdates);
     });
 
@@ -30,77 +32,107 @@ describe('actions/App', () => {
         jest.restoreAllMocks();
     });
 
-    test('lastFullReconnectTime - openApp', async () => {
-        // When Open App runs
-        App.openApp();
-        App.confirmReadyToOpenApp();
-        await waitForBatchedUpdates();
+    describe('openApp', () => {
+        it('should not make api call when client is not leader', async () => {
+            // When Open App runs
+            jest.spyOn(ActiveClientManager, 'isClientTheLeader').mockReturnValue(false);
+            App.openApp();
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
 
-        // The lastFullReconnectTime should be updated
-        expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
+            // The lastFullReconnectTime should NOT be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeUndefined();
+        });
+
+        it('should not make api call when using imported state', async () => {
+            // When Open App runs
+            await Onyx.set(ONYXKEYS.IS_USING_IMPORTED_STATE, true);
+            App.openApp();
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
+
+            // The app should be marked as loaded without an API call
+            expect(await getOnyxValue(ONYXKEYS.IS_LOADING_APP)).toBeFalsy();
+            expect(await getOnyxValue(ONYXKEYS.HAS_LOADED_APP)).toBeTruthy();
+
+            // The lastFullReconnectTime should NOT be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeUndefined();
+        });
+
+        it('should make an api call successfully and update lastFullReconnectTime', async () => {
+            // When Open App runs
+            App.openApp();
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
+
+            // The lastFullReconnectTime should be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
+        });
     });
 
-    test('lastFullReconnectTime - full reconnectApp', async () => {
-        // When a full ReconnectApp runs
-        await Onyx.set(ONYXKEYS.HAS_LOADED_APP, true);
-        App.reconnectApp();
-        App.confirmReadyToOpenApp();
-        await waitForBatchedUpdates();
+    describe('reconnectApp', () => {
+        it('should update lastFullReconnectTime on full reconnect', async () => {
+            // When a full ReconnectApp runs
+            await Onyx.set(ONYXKEYS.HAS_LOADED_APP, true);
+            App.reconnectApp();
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
 
-        // The lastFullReconnectTime should be updated
-        expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
-    });
+            // The lastFullReconnectTime should be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
+        });
 
-    test('lastFullReconnectTime - incremental reconnectApp', async () => {
-        // When an incremental ReconnectApp runs
-        await Onyx.set(ONYXKEYS.HAS_LOADED_APP, true);
-        App.reconnectApp(123);
-        App.confirmReadyToOpenApp();
-        await waitForBatchedUpdates();
+        it('should not update lastFullReconnectTime on incremental reconnect', async () => {
+            // When an incremental ReconnectApp runs
+            await Onyx.set(ONYXKEYS.HAS_LOADED_APP, true);
+            App.reconnectApp(123);
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
 
-        // The lastFullReconnectTime should NOT be updated
-        expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeUndefined();
-    });
+            // The lastFullReconnectTime should NOT be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeUndefined();
+        });
 
-    test('trigger full reconnect', async () => {
-        const reconnectApp = jest.spyOn(App, 'reconnectApp');
+        it('should trigger full reconnect', async () => {
+            const reconnectApp = jest.spyOn(App, 'reconnectApp');
 
-        // When OpenApp runs
-        App.openApp();
-        App.confirmReadyToOpenApp();
-        await waitForBatchedUpdates();
+            // When OpenApp runs
+            App.openApp();
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
 
-        // The lastFullReconnectTime should be updated
-        expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
+            // The lastFullReconnectTime should be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
 
-        // And when a new reconnectAppIfFullReconnectBefore is received
-        Onyx.set(ONYXKEYS.NVP_RECONNECT_APP_IF_FULL_RECONNECT_BEFORE, DateUtils.getDBTime());
-        await waitForBatchedUpdates();
+            // And when a new reconnectAppIfFullReconnectBefore is received
+            Onyx.set(ONYXKEYS.NVP_RECONNECT_APP_IF_FULL_RECONNECT_BEFORE, DateUtils.getDBTime());
+            await waitForBatchedUpdates();
 
-        // Then ReconnectApp should get called with no updateIDFrom to perform a full reconnect
-        expect(reconnectApp).toHaveBeenCalledTimes(1);
-        expect(reconnectApp).toHaveBeenCalledWith();
-    });
+            // Then ReconnectApp should get called with no updateIDFrom to perform a full reconnect
+            expect(reconnectApp).toHaveBeenCalledTimes(1);
+            expect(reconnectApp).toHaveBeenCalledWith();
+        });
 
-    test("don't trigger full reconnect", async () => {
-        const reconnectApp = jest.spyOn(App, 'reconnectApp');
+        it('should not trigger full reconnect', async () => {
+            const reconnectApp = jest.spyOn(App, 'reconnectApp');
 
-        // When OpenApp runs
-        App.openApp();
-        App.confirmReadyToOpenApp();
-        await waitForBatchedUpdates();
+            // When OpenApp runs
+            App.openApp();
+            App.confirmReadyToOpenApp();
+            await waitForBatchedUpdates();
 
-        // The lastFullReconnectTime should be updated
-        expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
+            // The lastFullReconnectTime should be updated
+            expect(await getOnyxValue(ONYXKEYS.LAST_FULL_RECONNECT_TIME)).toBeTruthy();
 
-        // And when a reconnectAppIfFullReconnectBefore is received with a timestamp in the past
-        const yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - 1);
-        Onyx.set(ONYXKEYS.NVP_RECONNECT_APP_IF_FULL_RECONNECT_BEFORE, DateUtils.getDBTime(yesterday.toISOString()));
-        await waitForBatchedUpdates();
+            // And when a reconnectAppIfFullReconnectBefore is received with a timestamp in the past
+            const yesterday = new Date();
+            yesterday.setDate(yesterday.getDate() - 1);
+            Onyx.set(ONYXKEYS.NVP_RECONNECT_APP_IF_FULL_RECONNECT_BEFORE, DateUtils.getDBTime(yesterday.toISOString()));
+            await waitForBatchedUpdates();
 
-        // Then ReconnectApp should NOT get called
-        expect(reconnectApp).toHaveBeenCalledTimes(0);
+            // Then ReconnectApp should NOT get called
+            expect(reconnectApp).toHaveBeenCalledTimes(0);
+        });
     });
 
     describe('getNonOptimisticPolicyIDs', () => {
