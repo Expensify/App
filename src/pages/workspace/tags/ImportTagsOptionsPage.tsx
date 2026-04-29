df1c3fda6ba69626c52473adc0d925ecb5aa3fb9
Nabi <mnabijan049@gmail.com>
2026-04-30 02:21:37 +0430

    fixed prettier failer

--- src/pages/workspace/tags/ImportTagsOptionsPage.tsx
+++ src/pages/workspace/tags/ImportTagsOptionsPage.tsx
@@ -97,9 +97,7 @@ function ImportTagsOptionsPage({route}: ImportTagsOptionsPageProps) {
         cleanPolicyTags(policyID);
         Navigation.setNavigationActionToMicrotaskQueue(() => {
             Navigation.navigate(
-                isQuickSettingsFlow
-                    ? ROUTES.SETTINGS_TAGS_IMPORT.getRoute(policyID, ROUTES.SETTINGS_TAGS_ROOT.getRoute(policyID, backTo))
-                    : ROUTES.WORKSPACE_TAGS_IMPORT.getRoute(policyID),
+                isQuickSettingsFlow ? ROUTES.SETTINGS_TAGS_IMPORT.getRoute(policyID, ROUTES.SETTINGS_TAGS_ROOT.getRoute(policyID, backTo)) : ROUTES.WORKSPACE_TAGS_IMPORT.getRoute(policyID),
             );
         });
     };
