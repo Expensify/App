93a1c54e101a100688c0c934c656216788572fcb
X Developer <ex.dev.work@gmail.com>
2026-05-02 14:22:57 +0430

    fix: allow expenses to be marked as billable

--- src/libs/API/parameters/SetPolicyRulesEnabledParams.ts
+++ src/libs/API/parameters/SetPolicyRulesEnabledParams.ts
@@ -1,6 +1,13 @@
 type SetPolicyRulesEnabledParams = {
     policyID: string;
     enabled: boolean;
+    /**
+     * Stringified JSON object with type of following structure:
+     *  disabledFields: {
+     *      defaultBillable: boolean;
+     *  };
+     */
+    disabledFields?: string;
 };
 
 export default SetPolicyRulesEnabledParams;
