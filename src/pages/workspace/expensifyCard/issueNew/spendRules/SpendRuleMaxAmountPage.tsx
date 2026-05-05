565c6f2fabc636a699aebb2b3ebe4e0ec1af5c93
Jack Senyitko <jsenyitko@expensify.com>
2026-05-05 10:29:29 -0400

    add max amount screen

--- src/pages/workspace/expensifyCard/issueNew/spendRules/SpendRuleMaxAmountPage.tsx
+++ src/pages/workspace/expensifyCard/issueNew/spendRules/SpendRuleMaxAmountPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import SpendRuleMaxAmountBase from '@components/SpendRules/configuration/SpendRuleMaxAmountBase';
 import useOnyx from '@hooks/useOnyx';
+import {setIssueNewCardData} from '@libs/actions/Card';
 import {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import {SettingsNavigatorParamList} from '@libs/Navigation/types';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import SCREENS from '@src/SCREENS';
 
@@ -10,5 +13,20 @@ type SpendRuleMaxAmountPageProps = PlatformStackScreenProps<SettingsNavigatorPar
 export default function SpendRuleMaxAmountPage({route}: SpendRuleMaxAmountPageProps) {
     const {policyID} = route.params;
     const [issueNewCardForm] = useOnyx(`${ONYXKEYS.COLLECTION.RAM_ONLY_ISSUE_NEW_EXPENSIFY_CARD}${policyID}`);
-    return <></>;
+
+    const defaultValue = issueNewCardForm?.data.cardRuleValue?.maxAmount ?? '';
+    const selectedCurrency = issueNewCardForm?.data.currency ?? CONST.CURRENCY.USD;
+
+    const handleMaxAmountChange = (maxAmount: string) => {
+        setIssueNewCardData(policyID, {cardRuleValue: {maxAmount}});
+    };
+
+    return (
+        <SpendRuleMaxAmountBase
+            policyID={policyID}
+            maxAmount={defaultValue}
+            currencyCode={selectedCurrency}
+            onMaxAmountChange={handleMaxAmountChange}
+        />
+    );
 }
