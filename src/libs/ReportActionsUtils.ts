66d00a55230c5b25716a468d58a51b894fff11a2
Carlos Martins <cmartins@expensify.com>
2026-05-07 14:47:56 -0300

    rm unused import

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -34,7 +34,6 @@ import type {
     OriginalMessageChangeLog,
     OriginalMessageExportIntegration,
     OriginalMessageMarkedReimbursed,
-    OriginalMessagePolicyChangeLog,
     OriginalMessageReimbursed,
     OriginalMessageUnreportedTransaction,
     PolicyBudgetFrequency,
