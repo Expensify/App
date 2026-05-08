6069f88df063e9a4db83eb5bc36cf676e3e9a71e
Jack Senyitko <jsenyitko@expensify.com>
2026-05-08 11:10:42 -0400

    update table to start

--- src/components/Tables/WorkspaceListTable/index.tsx
+++ src/components/Tables/WorkspaceListTable/index.tsx
@@ -1,5 +1,55 @@
+import React, {useRef} from 'react';
+import Table, {TableColumn, TableHandle} from '@components/Table';
+import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
+
+type WorkspaceTableColumnKey = 'workspaces' | 'owner' | 'type' | 'actions';
+
+type WorkspaceRowData = {};
+
 type WorkspaceListTableProps = {};
 
 export default function WorkspaceListTable({}: WorkspaceListTableProps) {
-    return <></>;
+    const styles = useThemeStyles();
+    const {translate} = useLocalize();
+    const tableRef = useRef<TableHandle<WorkspaceRowData, WorkspaceTableColumnKey>>(null);
+
+    const columns: Array<TableColumn<WorkspaceTableColumnKey>> = [
+        {
+            key: 'workspaces',
+            label: translate('common.workspaces'),
+        },
+        {
+            key: 'owner',
+            label: translate('common.owner'),
+        },
+        {
+            key: 'type',
+            label: translate('workspace.common.workspaceType'),
+        },
+        {
+            key: 'actions',
+            label: '',
+            styling: {
+                containerStyles: [styles.justifyContentEnd, styles.pr3],
+            },
+        },
+    ];
+
+    return (
+        <Table
+            data={[]}
+            columns={columns}
+            renderItem={() => <></>}
+            keyExtractor={() => <></>}
+            compareItems={() => {}}
+            isItemInSearch={() => {}}
+            initialSortColumn="workspaces"
+        >
+            <>
+                <Table.Header />
+                <Table.Body />
+            </>
+        </Table>
+    );
 }
