f7fecec6dfb28f94198bb9258b622a81b62c8f7f
Michał Muzyk <mimuzyk@gmail.com>
2026-05-12 12:29:46 +0200

    fix: hide date picker on navigating forward via hardware button

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -172,6 +172,7 @@ function DatePicker({
                 anchorPosition={popoverPosition}
                 shouldPositionFromTop={!isInverted}
                 forwardedFSClass={forwardedFSClass}
+                shouldCloseWhenBrowserNavigationChanged
             />
         </>
     );
