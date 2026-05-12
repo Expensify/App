0a3c573f4212c7c6b29b91d6f1f02954017e2957
Adam Horodyski <ad.horodyski@gmail.com>
2026-05-12 15:33:59 +0200

    Tune RHP open to 60px slide over 100ms

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -1934,10 +1934,10 @@ const CONST = {
             DEFAULT_RIGHT_DOCKED_IOS_OUT: Platform.OS === 'web' ? 1 : 400,
             FAB_IN: Platform.OS === 'web' ? 1 : 350,
             FAB_OUT: Platform.OS === 'web' ? 1 : 200,
-            RHP_OPEN_WEB: 150,
+            RHP_OPEN_WEB: 100,
             RHP_CLOSE_WEB: 0,
         },
-        RHP_ENTER_OFFSET_PX_WEB: 100,
+        RHP_ENTER_OFFSET_PX_WEB: 60,
     },
     FAB_MENU_ITEM_IDS: {
         QUICK_ACTION: 'quick-action',
