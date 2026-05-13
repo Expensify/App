03f33e3a54776e0804bc842bd24dd8511d3c1ce2
Nabi <mnabijan049@gmail.com>
2026-05-13 16:38:14 +0430

    chore: run prettier

--- src/pages/inbox/report/ReportActionsView.tsx
+++ src/pages/inbox/report/ReportActionsView.tsx
@@ -140,12 +140,12 @@ function ReportActionsView({reportID, onLayout}: ReportActionsViewProps) {
         [canPerformWriteAction, transactionThreadReportID],
     );
     const [transactionThreadReport] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${transactionThreadReportID}`);
-    
+
     const [isLoadingApp] = useOnyx(ONYXKEYS.IS_LOADING_APP);
     const [visibleReportActionsData] = useOnyx(ONYXKEYS.DERIVED.VISIBLE_REPORT_ACTIONS);
 
     const [reportPaginationState] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT_PAGINATION_STATE}${reportID}`);
-    
+
     const reportPreviewAction = useMemo(() => getReportPreviewAction(report?.chatReportID, report?.reportID), [report?.chatReportID, report?.reportID]);
     const didLayout = useRef(false);
 
