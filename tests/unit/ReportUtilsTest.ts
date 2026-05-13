45c594c13ef07385ae00a9e8609d8bca4b4d94b3
Nabi <mnabijan049@gmail.com>
2026-05-13 07:10:39 +0100

    fixed EsLint

--- tests/unit/ReportUtilsTest.ts
+++ tests/unit/ReportUtilsTest.ts
@@ -187,8 +187,7 @@ import type {
 import type {ErrorFields, Errors, OnyxValueWithOfflineFeedback} from '@src/types/onyx/OnyxCommon';
 import type {JoinWorkspaceResolution} from '@src/types/onyx/OriginalMessage';
 import type {ACHAccount, PolicyReportField} from '@src/types/onyx/Policy';
-import type {Participant, Participants} from '@src/types/onyx/Report';
-import type {ReportCollectionDataSet} from '@src/types/onyx/Report';
+import type {Participant, Participants, ReportCollectionDataSet} from '@src/types/onyx/Report';
 import type {ReportActionsCollectionDataSet} from '@src/types/onyx/ReportAction';
 import type {TransactionCollectionDataSet} from '@src/types/onyx/Transaction';
 import type CollectionDataSet from '@src/types/utils/CollectionDataSet';
