57d87273b322bde9777ff3c4da522c3b30d7c25d
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2026-05-13 12:58:44 +0200

    refactor static and dynamic success page

--- src/pages/settings/Security/TwoFactorAuth/DynamicSuccessPage.tsx
+++ src/pages/settings/Security/TwoFactorAuth/DynamicSuccessPage.tsx
@@ -1,12 +1,8 @@
 import React, {useCallback} from 'react';
-import ConfirmationPage from '@components/ConfirmationPage';
-import LottieAnimations from '@components/LottieAnimations';
 import useDynamicBackPath from '@hooks/useDynamicBackPath';
 import useDynamicForwardPath from '@hooks/useDynamicForwardPath';
 import useEnvironment from '@hooks/useEnvironment';
-import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
-import useThemeStyles from '@hooks/useThemeStyles';
 import {getXeroSetupLink} from '@libs/actions/connections/Xero';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {TwoFactorAuthNavigatorParamList} from '@libs/Navigation/types';
@@ -15,19 +11,16 @@ import {closeReactNativeApp} from '@userActions/HybridApp';
 import {openLink} from '@userActions/Link';
 import {quitAndNavigateBack} from '@userActions/TwoFactorAuthActions';
 import CONFIG from '@src/CONFIG';
-import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import {DYNAMIC_ROUTES} from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
 import isLoadingOnyxValue from '@src/types/utils/isLoadingOnyxValue';
-import TwoFactorAuthWrapper from './TwoFactorAuthWrapper';
+import SuccessPageBase from './SuccessPageBase';
 
 type DynamicSuccessPageProps = PlatformStackScreenProps<TwoFactorAuthNavigatorParamList, typeof SCREENS.TWO_FACTOR_AUTH.DYNAMIC_SUCCESS>;
 
 function DynamicSuccessPage({route}: DynamicSuccessPageProps) {
-    const {translate} = useLocalize();
     const {environmentURL} = useEnvironment();
-    const styles = useThemeStyles();
     const dynamicBackPath = useDynamicBackPath(DYNAMIC_ROUTES.TWO_FACTOR_AUTH_SUCCESS.path);
     const dynamicForwardPath = useDynamicForwardPath(DYNAMIC_ROUTES.TWO_FACTOR_AUTH_SUCCESS.path);
 
@@ -40,38 +33,25 @@ function DynamicSuccessPage({route}: DynamicSuccessPageProps) {
         quitAndNavigateBack(dynamicBackPath);
     }, [dynamicBackPath]);
 
+    const onButtonPress = useCallback(() => {
+        if (CONFIG.IS_HYBRID_APP && isClassicRedirectDismissed && !isClassicRedirectBlocked) {
+            closeReactNativeApp({shouldSetNVP: false, isTrackingGPS: false});
+            return;
+        }
+        goBack();
+        if (dynamicForwardPath) {
+            const policyID = route.params?.policyID;
+            if (policyID) {
+                openLink(getXeroSetupLink(policyID), environmentURL);
+            }
+        }
+    }, [isClassicRedirectDismissed, isClassicRedirectBlocked, goBack, dynamicForwardPath, route.params?.policyID, environmentURL]);
+
     return (
-        <TwoFactorAuthWrapper
-            stepName={CONST.TWO_FACTOR_AUTH_STEPS.SUCCESS}
-            title={translate('twoFactorAuth.headerTitle')}
-            stepCounter={{
-                step: 3,
-                text: translate('twoFactorAuth.stepSuccess'),
-            }}
+        <SuccessPageBase
+            onButtonPress={onButtonPress}
             onBackButtonPress={goBack}
-        >
-            <ConfirmationPage
-                illustration={LottieAnimations.Fireworks}
-                heading={translate('twoFactorAuth.enabled')}
-                description={translate('twoFactorAuth.congrats')}
-                shouldShowButton
-                buttonText={translate('common.buttonConfirm')}
-                onButtonPress={() => {
-                    if (CONFIG.IS_HYBRID_APP && isClassicRedirectDismissed && !isClassicRedirectBlocked) {
-                        closeReactNativeApp({shouldSetNVP: false, isTrackingGPS: false});
-                        return;
-                    }
-                    goBack();
-                    if (dynamicForwardPath) {
-                        const policyID = route.params?.policyID;
-                        if (policyID) {
-                            openLink(getXeroSetupLink(policyID), environmentURL);
-                        }
-                    }
-                }}
-                containerStyle={styles.flex1}
-            />
-        </TwoFactorAuthWrapper>
+        />
     );
 }
 
--- src/pages/settings/Security/TwoFactorAuth/SuccessPage.tsx
+++ src/pages/settings/Security/TwoFactorAuth/SuccessPage.tsx
@@ -1,23 +1,15 @@
 import React, {useCallback} from 'react';
-import ConfirmationPage from '@components/ConfirmationPage';
-import LottieAnimations from '@components/LottieAnimations';
-import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
-import useThemeStyles from '@hooks/useThemeStyles';
 import {shouldHideOldAppRedirect} from '@libs/TryNewDotUtils';
 import {closeReactNativeApp} from '@userActions/HybridApp';
 import {quitAndNavigateBack} from '@userActions/TwoFactorAuthActions';
 import CONFIG from '@src/CONFIG';
-import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import isLoadingOnyxValue from '@src/types/utils/isLoadingOnyxValue';
-import TwoFactorAuthWrapper from './TwoFactorAuthWrapper';
+import SuccessPageBase from './SuccessPageBase';
 
 function SuccessPage() {
-    const {translate} = useLocalize();
-    const styles = useThemeStyles();
-
     const [tryNewDot, tryNewDotMetadata] = useOnyx(ONYXKEYS.NVP_TRY_NEW_DOT);
     const isLoadingTryNewDot = isLoadingOnyxValue(tryNewDotMetadata);
     const isClassicRedirectBlocked = shouldHideOldAppRedirect(tryNewDot, isLoadingTryNewDot, CONFIG.IS_HYBRID_APP);
@@ -27,32 +19,19 @@ function SuccessPage() {
         quitAndNavigateBack(ROUTES.SETTINGS_SECURITY);
     }, []);
 
+    const onButtonPress = useCallback(() => {
+        if (CONFIG.IS_HYBRID_APP && isClassicRedirectDismissed && !isClassicRedirectBlocked) {
+            closeReactNativeApp({shouldSetNVP: false, isTrackingGPS: false});
+            return;
+        }
+        goBack();
+    }, [isClassicRedirectDismissed, isClassicRedirectBlocked, goBack]);
+
     return (
-        <TwoFactorAuthWrapper
-            stepName={CONST.TWO_FACTOR_AUTH_STEPS.SUCCESS}
-            title={translate('twoFactorAuth.headerTitle')}
-            stepCounter={{
-                step: 3,
-                text: translate('twoFactorAuth.stepSuccess'),
-            }}
+        <SuccessPageBase
+            onButtonPress={onButtonPress}
             onBackButtonPress={goBack}
-        >
-            <ConfirmationPage
-                illustration={LottieAnimations.Fireworks}
-                heading={translate('twoFactorAuth.enabled')}
-                description={translate('twoFactorAuth.congrats')}
-                shouldShowButton
-                buttonText={translate('common.buttonConfirm')}
-                onButtonPress={() => {
-                    if (CONFIG.IS_HYBRID_APP && isClassicRedirectDismissed && !isClassicRedirectBlocked) {
-                        closeReactNativeApp({shouldSetNVP: false, isTrackingGPS: false});
-                        return;
-                    }
-                    goBack();
-                }}
-                containerStyle={styles.flex1}
-            />
-        </TwoFactorAuthWrapper>
+        />
     );
 }
 
