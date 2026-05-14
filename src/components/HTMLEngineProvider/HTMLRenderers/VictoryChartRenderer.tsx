f2090cef775897c598e1259264bdc516392e1940
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-14 23:42:30 +0100

    parse styles

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer.tsx
@@ -1,9 +1,12 @@
+import {topLeft} from '@shopify/react-native-skia';
 import JSON5 from 'json5';
+import lodashIsObject from 'lodash/isObject';
 import lodashMerge from 'lodash/merge';
 import React, {useCallback, useMemo} from 'react';
 import {View} from 'react-native';
+import type {ColorValue, ViewStyle} from 'react-native';
 import {type CustomRendererProps, type TBlock, TNode, TNodeChildrenRenderer} from 'react-native-render-html';
-import type {CartesianChartRenderArg, ChartBounds, PointsArray} from 'victory-native';
+import type {CartesianChartRenderArg, ChartBounds, PointsArray, RoundedCorners} from 'victory-native';
 import {Bar, CartesianChart, Line} from 'victory-native';
 import {BAR_INNER_PADDING} from '@components/Charts/BarChart/BarChartContent';
 import {DEFAULT_CHART_COLOR} from '@components/Charts/utils';
@@ -38,31 +41,31 @@ function getHierarchyID(tnode: TNode): string {
 /**
  * Traverse node and extract all points from `data` attributes
  */
-function extractData(tnode: TNode): Record<string, RawData> {
-    const rawData: Record<string, RawData> = {};
+function extractData(tnode: TNode) {
+    const data: Record<string, RawData> = {};
+    const xKey: string = X_KEY;
+    const yKeys: string[] = [];
+
     if (tnode.attributes?.data) {
         const parsedData = parseAttribute(tnode.attributes.data);
         if (Array.isArray(parsedData)) {
-            const xKey = X_KEY;
             const yKey = Y_KEY_PREFIX + getHierarchyID(tnode);
+            yKeys.push(yKey);
             (parsedData as RawData[]).forEach((point) => {
-                rawData[point.x] = {
+                data[point.x] = {
                     [xKey]: point.x,
                     [yKey]: point.y,
                 };
             });
         }
     }
-    return lodashMerge(rawData, ...tnode.children.map((child) => extractData(child)));
-}
 
-/**
- * Returns required props for `<CartesianChart />`
- */
-function prepareDataForCartesianChart(rawData: Record<string, RawData>) {
-    const data = Object.values(rawData);
-    const xKey = X_KEY;
-    const yKeys = data.length > 0 ? Object.keys(data[0]).filter((key) => key !== xKey) : [];
+    tnode.children.forEach((child) => {
+        const {data: childData, yKeys: childYkeys} = extractData(child);
+        lodashMerge(data, childData);
+        yKeys.push(...childYkeys);
+    });
+
     return {
         data,
         xKey,
@@ -87,15 +90,69 @@ function parseAttribute(attribute: string): any {
     }
 }
 
-function VictoryChartRenderer({tnode}: CustomRendererProps<TBlock>) {
-    const rawData = useMemo(() => extractData(tnode), [tnode]);
-    const isPolarChart = useMemo(() => false, [rawData]);
-    const {data, xKey, yKeys} = useMemo(() => prepareDataForCartesianChart(rawData), [rawData, isPolarChart]);
+/**
+ * Helper to parse VC's `cornerRadius` into VN's `roundedCorners`
+ */
+function parseCornerRadius(attribute: string): RoundedCorners | undefined {
+    const cornerRadius = parseAttribute(attribute);
+    if (typeof cornerRadius === 'number') {
+        return {
+            topLeft: cornerRadius,
+            topRight: cornerRadius,
+            bottomLeft: cornerRadius,
+            bottomRight: cornerRadius,
+        };
+    }
+    if (lodashIsObject(cornerRadius)) {
+        return {
+            topLeft: 'topLeft' in cornerRadius ? (cornerRadius.topLeft as number) : 'top' in cornerRadius ? (cornerRadius.top as number) : undefined,
+            topRight: 'topRight' in cornerRadius ? (cornerRadius.topRight as number) : 'top' in cornerRadius ? (cornerRadius.top as number) : undefined,
+            bottomLeft: 'bottomLeft' in cornerRadius ? (cornerRadius.bottomLeft as number) : 'bottom' in cornerRadius ? (cornerRadius.bottom as number) : undefined,
+            bottomRight: 'bottomRight' in cornerRadius ? (cornerRadius.bottomRight as number) : 'bottom' in cornerRadius ? (cornerRadius.bottom as number) : undefined,
+        };
+    }
+    return undefined;
+}
 
-    const renderCartesianChartChild = useCallback((tnode: TNode, index: Number, renderArgs: CartesianChartRenderArg<any, any>) => {
+/**
+ * Helper to parse common styles
+ */
+function parseStyles(tnode: TNode) {
+    const nodeStyles: ViewStyle = {};
+    const parentNodeStyles: ViewStyle = {};
+
+    const parsedHeight = parseAttribute(tnode.attributes.height);
+    if (typeof parsedHeight === 'number') {
+        nodeStyles.height = parsedHeight;
+    }
+    const parsedWidth = parseAttribute(tnode.attributes.width);
+    if (typeof parsedWidth === 'number') {
+        nodeStyles.width = parsedWidth;
+    }
+
+    const parsedStyle = parseAttribute(tnode.attributes.style);
+    if (lodashIsObject(parsedStyle)) {
+        if ('parent' in parsedStyle && lodashIsObject(parsedStyle.parent)) {
+            Object.assign(parentNodeStyles, parsedStyle.parent);
+        }
+        if ('data' in parsedStyle && lodashIsObject(parsedStyle.data)) {
+            Object.assign(nodeStyles, parsedStyle.data);
+        }
+    }
+
+    return {nodeStyles, parentNodeStyles};
+}
+
+function VictoryChartRenderer({tnode}: CustomRendererProps<TBlock>) {
+    const styles = useThemeStyles();
+    const {data, xKey, yKeys} = useMemo(() => extractData(tnode), [tnode]);
+    const {nodeStyles, parentNodeStyles} = useMemo(() => parseStyles(tnode), [tnode]);
+
+    const renderCartesianChartChild = useCallback((tnode: TNode, index: Number, renderArgs: CartesianChartRenderArg<RawData, string>) => {
         const key = `${tnode.tagName ?? 'node'}-${index}`;
         const yKey = Y_KEY_PREFIX + getHierarchyID(tnode);
         const {points, chartBounds} = renderArgs;
+        const {nodeStyles} = parseStyles(tnode);
         switch (tnode.tagName) {
             case 'victorybar':
                 return (
@@ -103,9 +160,9 @@ function VictoryChartRenderer({tnode}: CustomRendererProps<TBlock>) {
                         key={key}
                         points={points[yKey]}
                         chartBounds={chartBounds}
-                        color={DEFAULT_CHART_COLOR}
+                        color={nodeStyles.fill ?? DEFAULT_CHART_COLOR}
                         innerPadding={BAR_INNER_PADDING}
-                        roundedCorners={parseAttribute(tnode.attributes.cornerradius)}
+                        roundedCorners={parseCornerRadius(tnode.attributes.cornerradius)}
                         barWidth={parseAttribute(tnode.attributes.barwidth)}
                         labels={parseAttribute(tnode.attributes.labels)}
                     >
@@ -117,7 +174,7 @@ function VictoryChartRenderer({tnode}: CustomRendererProps<TBlock>) {
                     <Line
                         key={key}
                         points={points[yKey]}
-                        color={DEFAULT_CHART_COLOR}
+                        color={nodeStyles.fill ?? DEFAULT_CHART_COLOR}
                         strokeWidth={2}
                         curveType="linear"
                     >
@@ -130,17 +187,19 @@ function VictoryChartRenderer({tnode}: CustomRendererProps<TBlock>) {
     }, []);
 
     return (
-        <View style={{height: 250, width: '100%'}}>
-            <CartesianChart
-                data={data}
-                xKey={xKey}
-                yKeys={yKeys}
-                domain={parseAttribute(tnode.attributes.domain)}
-                domainPadding={parseAttribute(tnode.attributes.domainpadding)}
-                padding={parseAttribute(tnode.attributes.padding)}
-            >
-                {(renderArgs) => tnode.children.map((child, childIndex) => renderCartesianChartChild(child, childIndex, renderArgs))}
-            </CartesianChart>
+        <View style={[styles.chartContainer, styles.mw100, parentNodeStyles]}>
+            <View style={[styles.chartContent, styles.mw100, nodeStyles]}>
+                <CartesianChart
+                    data={Object.values(data)}
+                    xKey={xKey}
+                    yKeys={yKeys}
+                    domain={parseAttribute(tnode.attributes.domain)}
+                    domainPadding={parseAttribute(tnode.attributes.domainpadding)}
+                    padding={parseAttribute(tnode.attributes.padding)}
+                >
+                    {(renderArgs) => tnode.children.map((child, childIndex) => renderCartesianChartChild(child, childIndex, renderArgs))}
+                </CartesianChart>
+            </View>
         </View>
     );
 }
