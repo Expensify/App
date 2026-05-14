ba4bfcdac6f62ada1588b4092b19ce618e6ed4e3
Christoph Pader <chris@margelo.com>
2026-05-14 23:52:06 +0100

    revert: changes needed to make NitroFetch work with Storybook

--- .storybook/webpackMockPaths.ts
+++ .storybook/webpackMockPaths.ts
@@ -10,9 +10,5 @@ export default {
     'react-native$': 'react-native-web',
     '@react-native-community/netinfo': path.resolve(dirname, '../__mocks__/@react-native-community/netinfo.ts'),
     '@react-navigation/native': path.resolve(dirname, '../__mocks__/@react-navigation/native'),
-
-    // These are needed because react-native-nitro-fetch references `react-native-nitro-text-decoder` package without actually importing it
-    // This causes Webpack and therefore Storybook scripts to fail, because it tries to import the package and fails.
-    'react-native-nitro-text-decoder': path.resolve(dirname, '../__mocks__/react-native-nitro-text-decoder.ts'),
 };
 /* eslint-enable @typescript-eslint/naming-convention */
--- __mocks__/react-native-nitro-text-decoder.ts
+++
@@ -1,6 +0,0 @@
-// These are needed because react-native-nitro-fetch references `react-native-nitro-text-decoder` package without actually importing it
-// This causes Webpack and therefore Storybook scripts to fail, because it tries to import the package and fails.
-const TextDecoder = globalThis.TextDecoder;
-const TextEncoder = globalThis.TextEncoder;
-
-export {TextDecoder, TextEncoder};
