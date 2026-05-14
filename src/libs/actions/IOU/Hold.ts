370b544f9332d4f827af1233bc598f2aad61cdaa
allgandaf <whogandalf@gmail.com>
2026-05-14 14:49:00 +0530

    Mirror unheldReimbursableTotal on optimistic unhold update

--- src/libs/actions/IOU/Hold.ts
+++ src/libs/actions/IOU/Hold.ts
@@ -423,6 +423,7 @@ function unholdRequest(transactionID: string, reportID: string, policy: OnyxEntr
             value: {
                 unheldTotal: (iouReport.unheldTotal ?? 0) + transactionAmount,
                 unheldNonReimbursableTotal: !transaction?.reimbursable ? (iouReport.unheldNonReimbursableTotal ?? 0) + transactionAmount : iouReport.unheldNonReimbursableTotal,
+                unheldReimbursableTotal: transaction?.reimbursable ? (iouReport.unheldReimbursableTotal ?? 0) + transactionAmount : iouReport.unheldReimbursableTotal,
             },
         });
     }
