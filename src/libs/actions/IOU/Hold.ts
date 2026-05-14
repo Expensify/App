83a21c6927f8cf91783519c82ae0bd0547fffe1b
allgandaf <whogandalf@gmail.com>
2026-05-14 15:36:40 +0530

    Revert "Mirror unheldReimbursableTotal on optimistic unhold update"
    This reverts commit 370b544f9332d4f827af1233bc598f2aad61cdaa.

--- src/libs/actions/IOU/Hold.ts
+++ src/libs/actions/IOU/Hold.ts
@@ -423,7 +423,6 @@ function unholdRequest(transactionID: string, reportID: string, policy: OnyxEntr
             value: {
                 unheldTotal: (iouReport.unheldTotal ?? 0) + transactionAmount,
                 unheldNonReimbursableTotal: !transaction?.reimbursable ? (iouReport.unheldNonReimbursableTotal ?? 0) + transactionAmount : iouReport.unheldNonReimbursableTotal,
-                unheldReimbursableTotal: transaction?.reimbursable ? (iouReport.unheldReimbursableTotal ?? 0) + transactionAmount : iouReport.unheldReimbursableTotal,
             },
         });
     }
