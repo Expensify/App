196aeac91ee7274394125a7acb699b4a40407f5b
Jan Musiał <jan.musial@swmansion.com>
2026-05-14 16:31:22 +0200

    move styles to style file

--- src/components/HTMLEngineProvider/HTMLRenderers/NumberedItemRenderer.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/NumberedItemRenderer.tsx
@@ -1,29 +1,18 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {View} from 'react-native';
 import type {TNode} from 'react-native-render-html';
 import {TNodeChildrenRenderer} from 'react-native-render-html';
 import Text from '@components/Text';
-import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
-import variables from '@styles/variables';
 
-const markerStyles = StyleSheet.create({
-    marker: {
-        fontSize: variables.fontSizeNormal,
-        lineHeight: variables.fontSizeNormalHeight,
-        minWidth: 32,
-        textAlign: 'right',
-        paddingHorizontal: 8,
-    },
-});
+type NumberedItemRendererProps = {tnode: TNode; index: number};
 
-function NumberedItemRenderer({tnode, index}: {tnode: TNode; index: number}) {
+function NumberedItemRenderer({tnode, index}: NumberedItemRendererProps) {
     const styles = useThemeStyles();
-    const theme = useTheme();
 
     return (
         <View style={[styles.flexRow, styles.w100]}>
-            <Text style={[markerStyles.marker, {color: theme.text}]}>{`${index}.`}</Text>
+            <Text style={styles.numberedListItemMarker}>{`${index}.`}</Text>
             <View style={styles.flex1}>
                 <TNodeChildrenRenderer tnode={tnode} />
             </View>
