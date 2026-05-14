74ba7ebef32674f39573c95aad502c0e9f836d64
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-05-14 19:26:53 +0800

    fix pressed state stuck issue

--- src/components/Search/FilterDropdowns/AdvancedFilters/FilterItem.tsx
+++ src/components/Search/FilterDropdowns/AdvancedFilters/FilterItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import Icon from '@components/Icon';
 import {PressableWithFeedback} from '@components/Pressable';
 import Text from '@components/Text';
@@ -26,13 +26,14 @@ function FilterItem({filterKey, isSelected, onPress, onHoverIn, onFocus}: Filter
     const StyleUtils = useStyleUtils();
     const theme = useTheme();
 
-    const [isPressed, setIsPressed] = useState(false);
+    const shouldCallPressOnPressOutRef = useRef(false);
+    const shouldCallPressOnPressRef = useRef(false);
 
     const {labelKey, icon} = FILTER_VIEW_MAP[filterKey];
     const icons = useMemoizedLazyExpensifyIcons(['ArrowRight', icon]);
 
-    const getPressableBackgroundStyle = () => {
-        if (isPressed) {
+    const getPressableBackgroundStyle = (pressed: boolean) => {
+        if (pressed) {
             return styles.buttonHoveredBG;
         }
 
@@ -45,32 +46,57 @@ function FilterItem({filterKey, isSelected, onPress, onHoverIn, onFocus}: Filter
 
     return (
         <PressableWithFeedback
-            style={[styles.typeFilterMenu, getPressableBackgroundStyle()]}
+            style={({pressed}) => [styles.typeFilterMenu, getPressableBackgroundStyle(pressed)]}
             accessible
             accessibilityLabel={filterKey}
             onHoverIn={onHoverIn}
             onFocus={onFocus}
             onPress={() => {
-                onPress?.();
-                setIsPressed(false);
+                // Pressable has an issue (https://github.com/facebook/react-native/issues/29714) where onPress
+                // could be triggered earlier before onPressOut when the press happens quickly.
+                // Normal interaction: onPressIn -> onPressOut -> onPress
+                // Buggy interaction: onPressIn -> onPress -> onPressOut
+                // onPress is called immediately if we are going through the normal interaction (onPress is triggered after onPressOut).
+                // Otherwise, the onPress will be triggered later in onPressOut.
+                if (shouldCallPressOnPressRef.current) {
+                    onPress?.();
+                    shouldCallPressOnPressRef.current = false;
+                    return;
+                }
+
+                shouldCallPressOnPressOutRef.current = true;
             }}
-            onPressIn={() => setIsPressed(true)}
-            onPressOut={() => setIsPressed(false)}
+            onPressOut={() => {
+                if (!shouldCallPressOnPressOutRef.current) {
+                    shouldCallPressOnPressRef.current = true;
+                    return;
+                }
+                // If shouldCallPressOnPressOutRef.current is true, then it means onPress is triggered earlier before onPressOut
+                // and we only want to trigger the onPress after onPressOut, because when this component is hidden by Activity,
+                // onPressOut won't be triggered anymore. This fix make sure onPressOut will always be triggered.
+                // We could probably fix this in GenericPressable, but since the bug only affect this component, I'll keep the fix scope small.
+                onPress?.();
+                shouldCallPressOnPressOutRef.current = false;
+            }}
             sentryLabel={`Search-Advanced-Filter-${filterKey}`}
         >
-            <Icon
-                src={icons[icon]}
-                fill={theme.icon}
-                width={variables.iconSizeSmall}
-                height={variables.iconSizeSmall}
-            />
-            <Text style={[styles.flex1]}>{translate(labelKey)}</Text>
-            <Icon
-                src={icons.ArrowRight}
-                fill={StyleUtils.getIconFillColor(getButtonState(isSelected, isPressed))}
-                width={variables.iconSizeNormal}
-                height={variables.iconSizeNormal}
-            />
+            {({pressed}) => (
+                <>
+                    <Icon
+                        src={icons[icon]}
+                        fill={theme.icon}
+                        width={variables.iconSizeSmall}
+                        height={variables.iconSizeSmall}
+                    />
+                    <Text style={[styles.flex1]}>{translate(labelKey)}</Text>
+                    <Icon
+                        src={icons.ArrowRight}
+                        fill={StyleUtils.getIconFillColor(getButtonState(isSelected, pressed))}
+                        width={variables.iconSizeNormal}
+                        height={variables.iconSizeNormal}
+                    />
+                </>
+            )}
         </PressableWithFeedback>
     );
 }
