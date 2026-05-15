5c261cf130a634f76768ec6e862e08446ea83bc1
allgandaf <whogandalf@gmail.com>
2026-05-15 21:12:31 +0530

    Render bank account chip on search filter bar

---
+++ src/components/Search/hooks/useFilterBankAccountValue.tsx
@@ -0,0 +1,22 @@
+import useOnyx from '@hooks/useOnyx';
+import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
+import type {BankName} from '@src/types/onyx/Bank';
+
+function useFilterBankAccountValue(value: string[]): string {
+    const [bankAccountList] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST);
+
+    const bankAccountLabels = Object.values(bankAccountList ?? {})
+        .filter((bankAccount) => value.includes(bankAccount?.accountData?.bankAccountID?.toString() ?? ''))
+        .map((bankAccount) => {
+            const bankName = (bankAccount?.accountData?.additionalData?.bankName ?? '') as BankName;
+            const accountNumber = bankAccount?.accountData?.accountNumber ?? '';
+            const formattedBankName = CONST.BANK_NAMES_USER_FRIENDLY[bankName] ?? CONST.BANK_NAMES_USER_FRIENDLY[CONST.BANK_NAMES.GENERIC_BANK];
+            const maskedNumber = accountNumber ? `xx${accountNumber.slice(-4)}` : '';
+            return maskedNumber ? `${formattedBankName} ${maskedNumber}` : formattedBankName;
+        });
+
+    return bankAccountLabels.join(', ');
+}
+
+export default useFilterBankAccountValue;
