fd3c153a06a00d6446ecbcb18faf43f44ddcbc82
X Developer <ex.dev.work@gmail.com>
2026-05-16 14:48:45 +0430

    refactor: remove unnecessary comments from SetPolicyRulesEnabledParams type

--- src/libs/API/parameters/SetPolicyRulesEnabledParams.ts
+++ src/libs/API/parameters/SetPolicyRulesEnabledParams.ts
@@ -1,12 +1,6 @@
 type SetPolicyRulesEnabledParams = {
     policyID: string;
     enabled: boolean;
-    /**
-     * Stringified JSON object with type of following structure:
-     *  disabledFields: {
-     *      defaultBillable: boolean;
-     *  };
-     */
     disabledFields?: string;
 };
 
