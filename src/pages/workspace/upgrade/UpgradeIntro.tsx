49d992c88bf31cace79ce8898f5a9e4979603da6
Hans (via MelvinBot) <infra+melvinbot@expensify.com>
2026-05-18 15:27:03 +0000

    Fix: run prettier on UpgradeIntro.tsx
    Co-authored-by: Hans <hungvu193@users.noreply.github.com>

--- src/pages/workspace/upgrade/UpgradeIntro.tsx
+++ src/pages/workspace/upgrade/UpgradeIntro.tsx
@@ -168,7 +168,11 @@ function UpgradeIntro({feature, onUpgrade, buttonDisabled, loading, isCategorizi
                 </View>
                 <Button
                     isLoading={loading}
-                    text={feature.id === CONST.UPGRADE_FEATURE_INTRO_MAPPING.expensifyCard.id ? translate('workspace.upgrade.expensifyCard.upgradeButton') : translate('common.upgrade')}
+                    text={
+                        feature.id === CONST.UPGRADE_FEATURE_INTRO_MAPPING.expensifyCard.id
+                            ? translate('workspace.upgrade.expensifyCard.upgradeButton')
+                            : translate('common.upgrade')
+                    }
                     testID="upgrade-button"
                     success
                     onPress={onUpgrade}
