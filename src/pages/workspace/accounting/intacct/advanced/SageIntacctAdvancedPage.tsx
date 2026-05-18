0e53b844fcf9e79291e17225f72c0bc48146734e
Rodrigo Lino da Costa <rodrigo@expensify.com>
2026-05-18 19:26:56 -0300

    Add travel invoicing reconciliation to Intacct

--- src/pages/workspace/accounting/intacct/advanced/SageIntacctAdvancedPage.tsx
+++ src/pages/workspace/accounting/intacct/advanced/SageIntacctAdvancedPage.tsx
@@ -11,6 +11,7 @@ import {getLatestErrorField} from '@libs/ErrorUtils';
 import {areSettingsInErrorFields, getCurrentSageIntacctEntityName, settingsPendingAction} from '@libs/PolicyUtils';
 import createDynamicRoute from '@navigation/helpers/dynamicRoutesUtils/createDynamicRoute';
 import Navigation from '@navigation/Navigation';
+import TravelInvoicingContinuousReconciliationSection from '@pages/workspace/accounting/common/TravelInvoicingContinuousReconciliationSection';
 import type {WithPolicyProps} from '@pages/workspace/withPolicy';
 import withPolicy from '@pages/workspace/withPolicy';
 import ToggleSettingOptionRow from '@pages/workspace/workflows/ToggleSettingsOptionRow';
@@ -152,6 +153,12 @@ function SageIntacctAdvancedPage({policy}: WithPolicyProps) {
                     />
                 </OfflineWithFeedback>
             </Accordion>
+            <TravelInvoicingContinuousReconciliationSection
+                policy={policy}
+                connectionName={CONST.POLICY.CONNECTIONS.NAME.SAGE_INTACCT}
+                isAutoSyncEnabled={!!config?.autoSync?.enabled}
+                toggleWrapperStyle={[styles.ph5, styles.pv3]}
+            />
         </ConnectionLayout>
     );
 }
