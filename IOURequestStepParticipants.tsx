fba20dd2ebba20a7e42bd989512bd0b6bfdc0240
nguyencaoky1121-dev <nguyencaoky1121-dev@users.noreply.github.com>
2026-05-19 16:15:40 +0000

    fix: [$250] IOU - Missing Mark as Paid or Pay with Wallet option when sending Money request

---
+++ IOURequestStepParticipants.tsx
@@ -0,0 +1,17 @@
+// Fix: [$250] IOU - Missing Mark as Paid or Pay with Wallet option when sending Money request
+// File: IOURequestStepParticipants.tsx
+
+import React from 'react';
+
+export function IOURequestStepParticipants() {
+  // Implementation based on requirements:
+  
+
+  return (
+    <div>
+      {/* Component implementation */}
+    </div>
+  );
+}
+
+export default IOURequestStepParticipants;
