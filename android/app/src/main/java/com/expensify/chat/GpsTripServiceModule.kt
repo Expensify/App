9c607c132c1c7ccbb896022bc3d8047ce643b3df
GCyganek <gabrielcyganek@gmail.com>
2026-05-20 08:58:54 +0200

    Fix GPS crashes in the background on Android

---
+++ android/app/src/main/java/com/expensify/chat/GpsTripServiceModule.kt
@@ -0,0 +1,35 @@
+package com.expensify.chat.customairshipextender
+
+import android.content.Intent
+import android.os.Build
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+
+class GpsTripServiceModule(reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String = "GpsTripServiceModule"
+
+    @ReactMethod
+    fun startService(title: String, body: String, deepLink: String) {
+        val context = reactApplicationContext
+        val intent = Intent(context, GpsTripService::class.java).apply {
+            putExtra(GpsTripService.EXTRA_TITLE, title)
+            putExtra(GpsTripService.EXTRA_BODY, body)
+            putExtra(GpsTripService.EXTRA_DEEP_LINK, deepLink)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent)
+        } else {
+            context.startService(intent)
+        }
+    }
+
+    @ReactMethod
+    fun stopService() {
+        val context = reactApplicationContext
+        context.stopService(Intent(context, GpsTripService::class.java))
+    }
+}
