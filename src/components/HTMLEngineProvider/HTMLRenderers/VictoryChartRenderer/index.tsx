8d166b7a1c0268d7f19705dfa31a2d020983bfe9
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-21 16:48:32 +0100

    fix wasm resolution

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/index.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/index.tsx
@@ -1,13 +1,31 @@
 import {WithSkiaWeb} from '@shopify/react-native-skia/lib/module/web';
 import React from 'react';
+import {View} from 'react-native';
+import ActivityIndicator from '@components/ActivityIndicator';
+import useThemeStyles from '@hooks/useThemeStyles';
+import type {SkeletonSpanReasonAttributes} from '@libs/telemetry/useSkeletonSpan';
 import type {VictoryChartRendererProps} from './types';
 
+const getBaseVictoryChartRenderer = () => import('./BaseVictoryChartRenderer');
+
 function VictoryChartRenderer(props: VictoryChartRendererProps) {
+    const styles = useThemeStyles();
+    const reasonAttributes: SkeletonSpanReasonAttributes = {context: 'VictoryChartRenderer.SkiaWebLoading'};
+
     // Victory Chart uses Skia internally and it uses a WASM module that must be loaded before rendering any Skia-based component.
     return (
         <WithSkiaWeb
-            getComponent={() => import('./BaseVictoryChartRenderer')}
+            opts={{locateFile: (file: string) => `/${file}`}}
+            getComponent={getBaseVictoryChartRenderer}
             componentProps={props}
+            fallback={
+                <View style={styles.chartWebFallback}>
+                    <ActivityIndicator
+                        size="large"
+                        reasonAttributes={reasonAttributes}
+                    />
+                </View>
+            }
         />
     );
 }
