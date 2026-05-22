a6d3fa1f4e1ea98b4f1dd132651cf6db2d5e4b30
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2026-05-22 12:26:19 +0200

    route useOnyx context import via SearchContext shim

--- src/hooks/useOnyx.ts
+++ src/hooks/useOnyx.ts
@@ -3,7 +3,7 @@ import type {DependencyList} from 'react';
 // eslint-disable-next-line no-restricted-imports
 import {useOnyx as originalUseOnyx} from 'react-native-onyx';
 import type {OnyxCollection, OnyxEntry, OnyxKey, OnyxValue, UseOnyxOptions, UseOnyxResult} from 'react-native-onyx';
-import {SearchQueryContext, SearchResultsContext} from '@components/Search/SearchContextDefinitions';
+import {SearchQueryContext, SearchResultsContext} from '@components/Search/SearchContext';
 import {useIsOnSearch} from '@components/Search/SearchScopeProvider';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
