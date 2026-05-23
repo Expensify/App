bc07bcd4d13b6c5f95257f9eb6b3c30376040252
allgandaf <whogandalf@gmail.com>
2026-05-23 12:17:04 +0530

    Switch SearchFiltersParticipantsSelector to getExpensifyTeamExclusions

--- src/components/Search/SearchFiltersParticipantsSelector.tsx
+++ src/components/Search/SearchFiltersParticipantsSelector.tsx
@@ -12,7 +12,7 @@ import useSearchSelector from '@hooks/useSearchSelector';
 import {canUseTouchScreen} from '@libs/DeviceCapabilities';
 import {formatSectionsFromSearchTerm, getFilteredRecentAttendees, getParticipantsOption} from '@libs/OptionsListUtils';
 import {doesPersonalDetailMatchSearchTerm} from '@libs/OptionsListUtils/searchMatchUtils';
-import {getNonWorkspaceMemberExclusions} from '@libs/PolicyUtils';
+import {getExpensifyTeamExclusions} from '@libs/PolicyUtils';
 import type {OptionData} from '@libs/ReportUtils';
 import {getDisplayNameForParticipant} from '@libs/ReportUtils';
 import Navigation from '@navigation/Navigation';
@@ -78,7 +78,7 @@ function SearchFiltersParticipantsSelector({initialAccountIDs, onFiltersUpdate, 
         [personalDetails, recentAttendees, currentUserEmail, currentUserAccountID, shouldAllowNameOnlyOptions],
     );
 
-    const nonMemberExclusions = getNonWorkspaceMemberExclusions(personalDetails, allPolicies, currentUserEmail);
+    const expensifyTeamExclusions = getExpensifyTeamExclusions(personalDetails, allPolicies, currentUserEmail);
 
     const {searchTerm, debouncedSearchTerm, setSearchTerm, availableOptions, selectedOptions, setSelectedOptions, toggleSelection, areOptionsInitialized, onListEndReached} =
         useSearchSelector({
@@ -87,7 +87,7 @@ function SearchFiltersParticipantsSelector({initialAccountIDs, onFiltersUpdate, 
             maxRecentReportsToShow: CONST.IOU.MAX_RECENT_REPORTS_TO_SHOW,
             includeUserToInvite: true,
             excludeLogins: CONST.EXPENSIFY_EMAILS_OBJECT,
-            excludeFromSuggestionsOnly: nonMemberExclusions,
+            excludeFromSuggestionsOnly: expensifyTeamExclusions,
             includeRecentReports: true,
             shouldInitialize: didScreenTransitionEnd,
             includeCurrentUser: true,
