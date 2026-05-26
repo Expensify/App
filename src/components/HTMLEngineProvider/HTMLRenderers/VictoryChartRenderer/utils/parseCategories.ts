ce14f7c54c1014b4daefe57d6c99f53b9f50ab33
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-26 05:00:03 +0100

    lint

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseCategories.ts
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseCategories.ts
@@ -8,7 +8,7 @@ function parseCategories(attribute: string): string[] | undefined {
     const categories = parseAttribute(attribute);
     if (lodashIsObject(categories)) {
         if ('x' in categories && Array.isArray(categories.x)) {
-            return categories.x;
+            return categories.x.map(String);
         }
     }
     return undefined;
