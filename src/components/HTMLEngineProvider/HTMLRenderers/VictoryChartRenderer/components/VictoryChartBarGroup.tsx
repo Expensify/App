905073792804706cefc9488eda48cdd51c784b1e
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-26 23:30:49 +0100

    fallback if barWidth is not set

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartBarGroup.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartBarGroup.tsx
@@ -25,8 +25,8 @@ function VictoryChartBarGroup({tnode, isHorizontal}: VictoryChartBarGroupProps) 
     }
 
     const roundedCorners = parseCornerRadius(firstBarChild?.attributes?.cornerradius ?? '');
-    const barWidth = Number(parseAttribute(firstBarChild.attributes?.barwidth ?? ''));
-    const betweenGroupPadding = parseOffset(tnode.attributes.offset, chartBounds, barChildren.length, barWidth, points[getYKey(firstBarChild)].length, isHorizontal ?? false);
+    const barWidth = firstBarChild.attributes?.barwidth ? Number(parseAttribute(firstBarChild.attributes.barwidth)) : undefined;
+    const betweenGroupPadding = parseOffset(tnode.attributes.offset, chartBounds, barChildren.length, barWidth ?? 0, points[getYKey(firstBarChild)].length, isHorizontal ?? false);
 
     return (
         <BarGroup
