fa80ff890765f0365ceb077cbed66d93f0cb782d
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-28 23:00:16 +0100

    use custom label is available

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPieLabel.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPieLabel.tsx
@@ -7,6 +7,7 @@ import type {PieSliceData} from 'victory-native';
 import {useChartDefaultTypeface} from '@components/Charts/hooks';
 import {COLOR_KEY, LABEL_KEY, VALUE_KEY, X_KEY} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/constants';
 import {useVictoryChartContext} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/context/VictoryChartContext';
+import type {ChartType, PolarChartData, ProcessNodeResult} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/types';
 import parseAttribute from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseAttribute';
 import parseComponent from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseComponent';
 import parseVictoryLabelNode from '../parsers/victoryLabelParser';
@@ -20,6 +21,7 @@ type VictoryChartPieLabelProps = {
 const RADIAN = Math.PI / 180;
 
 function VictoryChartPieLabel({tnode, slice}: VictoryChartPieLabelProps) {
+    const {data} = useVictoryChartContext();
     const renderEngine = useAmbientTRenderEngine();
     const labelComponentNode = parseComponent(tnode.attributes.labelcomponent, renderEngine, 'victorylabel');
     const labelItem = labelComponentNode ? parseVictoryLabelNode(labelComponentNode).labelItems?.at(0) : undefined;
@@ -28,7 +30,10 @@ function VictoryChartPieLabel({tnode, slice}: VictoryChartPieLabelProps) {
         return null;
     }
 
-    const text = slice.label;
+    const dataLabels = Object.values(data).map((entry) => (entry as PolarChartData).label);
+    const labels = parseAttribute<string[]>(tnode.attributes.labels);
+    const text = labels?.at(dataLabels.indexOf(slice.label)) ?? slice.label;
+
     const labelRadius = tnode.attributes.labelradius !== undefined ? Number(parseAttribute(tnode.attributes.labelradius)) : slice.radius;
     const midAngle = (slice.startAngle + slice.endAngle) / 2;
     const x = slice.center.x + labelRadius * Math.cos(-midAngle * RADIAN);
