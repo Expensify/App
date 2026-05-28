a31e1f570b920e22dace947a6b1c4a54dae16745
Shawn Borton <shawn@expensify.com>
2026-05-28 14:22:04 +0200

    Widen Cmd+K search router to 600px

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -224,7 +224,7 @@ export default {
     popoverWidth: 375,
     compactPopoverMenuWidth: 300,
     compactPopoverMenuVerticalMargin: 100,
-    searchRouterPopoverWidth: 512,
+    searchRouterPopoverWidth: 600,
     bankAccountActionPopoverRightSpacing: 32,
     bankAccountActionPopoverTopSpacing: 14,
     addPaymentPopoverRightSpacing: 23,
