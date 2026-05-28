6d7cf8e88d20e7dfdb1bbcfec8f6747e6cfe0fc6
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-28 01:22:37 +0100

    get correct radius and innerRadius

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPie.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPie.tsx
@@ -5,19 +5,28 @@ import {Pie, PolarChart} from 'victory-native';
 import {useChartDefaultTypeface} from '@components/Charts/hooks';
 import {COLOR_KEY, LABEL_KEY, VALUE_KEY, X_KEY} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/constants';
 import {useVictoryChartContext} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/context/VictoryChartContext';
+import parseAttribute from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseAttribute';
 
 type VictoryChartPieProps = {tnode: TNode};
+
+// Victory Chart's 0° angle is equivalent to 270° in Victory Native
+const START_ANGLE = 270;
 
 function VictoryChartPie({tnode}: VictoryChartPieProps) {
     const {data} = useVictoryChartContext();
     const {regular: regularTypeface, bold: boldTypeface} = useChartDefaultTypeface();
+    const innerRadius = tnode.attributes.innerradius !== undefined ? Number(parseAttribute(tnode.attributes.innerradius)) : undefined;
+    const radius = tnode.attributes.radius !== undefined ? Number(parseAttribute(tnode.attributes.radius)) : undefined;
+    const size = radius ? radius * 2 : undefined;
 
     return (
         <Pie.Chart
-            startAngle={270}
-            innerRadius={30}
+            startAngle={START_ANGLE}
+            innerRadius={innerRadius}
+            size={size}
+            circleSweepDegrees={360}
         >
-            {(slice) => (
+            {() => (
                 <>
                     <Pie.Slice>
                         <Pie.Label
