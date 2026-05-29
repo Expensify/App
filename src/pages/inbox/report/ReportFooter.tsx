9740b80dcaf9f4c4cef0d9fbb4f7db16b071ba19
Nicolas Bonet <nicolas@expensify.com>
2026-05-29 09:31:14 -0500

    Enhance ReportFooter: Add flex styling to composer overlap view when notifications banner is shown

--- src/pages/inbox/report/ReportFooter.tsx
+++ src/pages/inbox/report/ReportFooter.tsx
@@ -96,7 +96,7 @@ function ReportFooter() {
         return (
             <View style={[chatFooterStyles, isComposerFullSize && styles.chatFooterFullCompose]}>
                 {shouldShowEnableNotificationsBanner && <EnableNotificationsBanner />}
-                <View style={shouldShowEnableNotificationsBanner ? composerOverlapStyle : undefined}>
+                <View style={[shouldShowEnableNotificationsBanner ? composerOverlapStyle : undefined, styles.flex1]}>
                     <SwipeableView onSwipeDown={Keyboard.dismiss}>
                         <ReportActionCompose reportID={reportIDFromRoute} />
                     </SwipeableView>
