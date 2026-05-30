8c6dbf79fe6682af019cd722c2a3f533a314b003
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-05-30 04:40:45 +0100

    fix color and size of pad angle

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPie.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPie.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type {TNode} from 'react-native-render-html';
 import {useAmbientTRenderEngine} from 'react-native-render-html';
 import {Pie} from 'victory-native';
+import VictoryTheme from '@components/Charts/VictoryTheme';
 import {useVictoryChartContext} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/context/VictoryChartContext';
 import parseVictoryLabelNode from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/parsers/victoryLabelParser';
 import type {PolarChartData} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/types';
@@ -15,11 +16,8 @@ type VictoryChartPieProps = {tnode: TNode};
 // Victory Chart's 0° angle is equivalent to 270° in Victory Native
 const START_ANGLE = 270;
 
-// Victory Chart's padAngle do not support fill color so it's always transparent
-const ANGULAR_STROKE_COLOR = 'transparent';
-
 function VictoryChartPie({tnode}: VictoryChartPieProps) {
-    const {data} = useVictoryChartContext();
+    const {data, chartContainerStyles} = useVictoryChartContext();
     const renderEngine = useAmbientTRenderEngine();
     const labelComponentNode = parseComponent(tnode.attributes.labelcomponent, renderEngine, 'victorylabel');
     const baseLabelItem = labelComponentNode ? parseVictoryLabelNode(labelComponentNode).labelItems?.at(0) : undefined;
@@ -30,7 +28,8 @@ function VictoryChartPie({tnode}: VictoryChartPieProps) {
     const padAngle = tnode.attributes.padangle !== undefined ? Number(parseAttribute(tnode.attributes.padangle)) : undefined;
     const radius = tnode.attributes.radius !== undefined ? Number(parseAttribute(tnode.attributes.radius)) : undefined;
     const size = radius ? radius * 2 : undefined;
-    const angularStrokeWidth = padAngle && radius ? convertAngleToArcLength(padAngle, radius) : 0;
+    const angularStrokeWidth = padAngle && radius ? 2 * convertAngleToArcLength(padAngle, radius) : 0;
+    const angularStrokeColor = typeof chartContainerStyles.backgroundColor === 'string' ? chartContainerStyles.backgroundColor : VictoryTheme.colors.default;
 
     return (
         <Pie.Chart
@@ -53,7 +52,7 @@ function VictoryChartPie({tnode}: VictoryChartPieProps) {
                     <Pie.SliceAngularInset
                         angularInset={{
                             angularStrokeWidth: angularStrokeWidth,
-                            angularStrokeColor: ANGULAR_STROKE_COLOR,
+                            angularStrokeColor: angularStrokeColor,
                         }}
                     />
                 </>
