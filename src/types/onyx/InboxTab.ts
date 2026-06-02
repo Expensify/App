f2c14045c2756bb55fd880efa6d0cb5f4c4c6466
Youssef Lourayad <youssef@expensify.com>
2026-06-02 14:17:20 +0000

    Remove unused type

--- src/types/onyx/InboxTab.ts
+++
@@ -1,7 +0,0 @@
-import type {ValueOf} from 'type-fest';
-import type CONST from '@src/CONST';
-
-/** The active Inbox tab filter (All, To-do, or Unread) */
-type InboxTab = ValueOf<typeof CONST.INBOX_TAB>;
-
-export default InboxTab;
