ac21d8cd6a54964b5536a4620a4fc632987bd39c
Jack Senyitko <jsenyitko@expensify.com>
2026-06-02 08:15:48 -0400

    remove statuses

--- src/pages/settings/Subscription/CardSection/CardSection.tsx
+++ src/pages/settings/Subscription/CardSection/CardSection.tsx
@@ -100,15 +100,6 @@ function CardSection() {
     const viewPurchases = () => {
         const query = buildQueryStringFromFilterFormValues({
             type: CONST.SEARCH.DATA_TYPES.EXPENSE,
-            status: [
-                CONST.SEARCH.STATUS.EXPENSE.UNREPORTED,
-                CONST.SEARCH.STATUS.EXPENSE.DRAFTS,
-                CONST.SEARCH.STATUS.EXPENSE.OUTSTANDING,
-                CONST.SEARCH.STATUS.EXPENSE.APPROVED,
-                CONST.SEARCH.STATUS.EXPENSE.DONE,
-                CONST.SEARCH.STATUS.EXPENSE.PAID,
-                CONST.SEARCH.STATUS.EXPENSE.DELETED,
-            ],
             merchant: CONST.EXPENSIFY_MERCHANT,
             from: session?.accountID ? [session.accountID.toString()] : undefined,
         });
