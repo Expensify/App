e4483b0a0dfceef3b70da7560df2934a045f8674
Ionatan Wiznia <ionatan@expensify.com>
2026-06-03 11:36:55 +0200

    Fix parens

--- src/libs/Navigation/AppNavigator/KeyboardShortcutsHandler/GoToWorkspaceHandler.tsx
+++ src/libs/Navigation/AppNavigator/KeyboardShortcutsHandler/GoToWorkspaceHandler.tsx
@@ -19,26 +19,24 @@ function GoToWorkspaceHandler() {
         const shortcutConfig = CONST.KEYBOARD_SHORTCUTS.GO_TO_WORKSPACE;
         const unsubscribe = KeyboardShortcut.subscribe(
             shortcutConfig.shortcutKey,
-            callFunctionIfActionIsAllowed(
-                () => {
-                    const reportID = Navigation.getTopmostReportId();
-                    if (!reportID) {
-                        return;
-                    }
+            callFunctionIfActionIsAllowed(() => {
+                const reportID = Navigation.getTopmostReportId();
+                if (!reportID) {
+                    return;
+                }
 
-                    const report = getReportOrDraftReport(reportID);
-                    const policyID = report?.policyID ?? (report?.parentReportID ? getReportOrDraftReport(report.parentReportID)?.policyID : undefined);
-                    if (!policyID || policyID === CONST.POLICY.ID_FAKE) {
-                        return;
-                    }
+                const report = getReportOrDraftReport(reportID);
+                const policyID = report?.policyID ?? (report?.parentReportID ? getReportOrDraftReport(report.parentReportID)?.policyID : undefined);
+                if (!policyID || policyID === CONST.POLICY.ID_FAKE) {
+                    return;
+                }
 
-                    const route = shouldUseNarrowLayoutRef.current ? ROUTES.WORKSPACE_INITIAL.getRoute(policyID, Navigation.getActiveRoute()) : ROUTES.WORKSPACE_OVERVIEW.getRoute(policyID);
-                    Navigation.navigate(route);
-                },
-                shortcutConfig.descriptionKey,
-                shortcutConfig.modifiers,
-                true,
-            ),
+                const route = shouldUseNarrowLayoutRef.current ? ROUTES.WORKSPACE_INITIAL.getRoute(policyID, Navigation.getActiveRoute()) : ROUTES.WORKSPACE_OVERVIEW.getRoute(policyID);
+                Navigation.navigate(route);
+            }),
+            shortcutConfig.descriptionKey,
+            shortcutConfig.modifiers,
+            true,
         );
 
         return () => unsubscribe();
