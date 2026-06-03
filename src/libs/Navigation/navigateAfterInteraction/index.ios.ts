2651a93983a396ffa697065ebb9f80f7cfb585b0
Michał Rejdak <michal.rejdak@swmansion.com>
2026-06-03 15:29:47 +0200

    migrate navigateAfterInteraction

--- src/libs/Navigation/navigateAfterInteraction/index.ios.ts
+++ src/libs/Navigation/navigateAfterInteraction/index.ios.ts
@@ -1,14 +1,15 @@
-// eslint-disable-next-line no-restricted-imports
-import {InteractionManager} from 'react-native';
 import Navigation from '@libs/Navigation/Navigation';
+import TransitionTracker from '@libs/Navigation/TransitionTracker';
 
 /**
  * On iOS, the navigation transition can sometimes break other animations, such as the closing modal.
  * In this case we need to wait for the animation to be complete before executing the navigation
  */
 function navigateAfterInteraction(callback: () => void) {
-    InteractionManager.runAfterInteractions(() => {
-        Navigation.setNavigationActionToMicrotaskQueue(callback);
+    TransitionTracker.runAfterTransitions({
+        callback: () => {
+            Navigation.setNavigationActionToMicrotaskQueue(callback);
+        },
     });
 }
 
