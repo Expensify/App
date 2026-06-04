e59f24c1cf50af36bcf0642fdb827e5cb1e00c0a
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-06-04 02:54:29 +0100

    move aiRules inside rules object

--- src/pages/workspace/rules/AIRulesSection.tsx
+++ src/pages/workspace/rules/AIRulesSection.tsx
@@ -34,7 +34,7 @@ function AIRulesSection({policyID}: AIRulesSectionProps) {
     const policy = usePolicy(policyID);
     const expensifyIcons = useMemoizedLazyExpensifyIcons(['Plus']);
     const {isBetaEnabled} = usePermissions();
-    const aiRules = policy?.aiRules;
+    const aiRules = policy?.rules?.aiRules;
     const hasRules = !isEmptyObject(aiRules);
     const isCustomAgentEnabled = isBetaEnabled(CONST.BETAS.CUSTOM_AGENT);
 
