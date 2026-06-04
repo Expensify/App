40b85e3f5735fa65626c47a490871312d7803cfd
aswin-s <mr.aswin@gmail.com>
2026-06-04 14:48:03 +0530

    style: remove useless Fragment wrapper in RootNavigatorExtraContent native

--- src/components/Navigation/RootNavigatorExtraContent/index.native.tsx
+++ src/components/Navigation/RootNavigatorExtraContent/index.native.tsx
@@ -3,11 +3,7 @@ import SidePanel from '@components/SidePanel';
 import type {ExtraContentProps} from '@libs/Navigation/PlatformStackNavigation/types';
 
 function RootNavigatorExtraContent({navigation}: ExtraContentProps) {
-    return (
-        <>
-            <SidePanel navigation={navigation} />
-        </>
-    );
+    return <SidePanel navigation={navigation} />;
 }
 
 export default RootNavigatorExtraContent;
