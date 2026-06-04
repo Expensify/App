8f186bf1c23c4be4f61ae80b2e25751b1b304bb7
Carlos Martins <cmartins@expensify.com>
2026-06-04 14:18:27 -0600

    offset labels

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPie.tsx
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/components/VictoryChartPie.tsx
@@ -12,6 +12,19 @@ type VictoryChartPieProps = {tnode: TNode};
 // Victory Chart's 0° angle is equivalent to 270° in Victory Native
 const START_ANGLE = 270;
 
+/** Alternating multipliers for pie slice label distance (base `labelradius` from HTML). */
+const EVEN_SLICE_LABEL_RADIUS_FACTOR = 1;
+const ODD_SLICE_LABEL_RADIUS_FACTOR = 0.8;
+
+function getSliceLabelRadius(baseLabelRadius: number | undefined, sliceIndex: number): number | undefined {
+    if (baseLabelRadius === undefined || sliceIndex < 0) {
+        return baseLabelRadius;
+    }
+
+    const factor = sliceIndex % 2 === 0 ? EVEN_SLICE_LABEL_RADIUS_FACTOR : ODD_SLICE_LABEL_RADIUS_FACTOR;
+    return baseLabelRadius * factor;
+}
+
 function VictoryChartPie({tnode}: VictoryChartPieProps) {
     const renderEngine = useAmbientTRenderEngine();
     const labelComponentNode = parseComponent(tnode.attributes.labelcomponent, renderEngine, 'victorylabel');
@@ -27,6 +40,7 @@ function VictoryChartPie({tnode}: VictoryChartPieProps) {
             innerRadius={innerRadius}
             size={size}
         >
+<<<<<<< Updated upstream
             {({slice}) => (
                 <Pie.Slice>
                     <VictoryChartPieLabel
@@ -36,6 +50,39 @@ function VictoryChartPie({tnode}: VictoryChartPieProps) {
                     />
                 </Pie.Slice>
             )}
+=======
+            {({slice}) => {
+                const sliceIndex = dataLabels.indexOf(slice.label);
+                const sliceLabelRadius = getSliceLabelRadius(labelRadius, sliceIndex);
+
+                return (
+                <>
+                    <Pie.Slice>
+                        {!!baseLabelItem && (
+                            <VictoryChartPieLabel
+                                slice={slice}
+                                baseLabelItem={baseLabelItem}
+                                label={pieLabels?.[sliceIndex]}
+                                labelRadius={sliceLabelRadius}
+                                labelIndicatorXShift={labelIndicatorXShift}
+                                labelIndicatorYShift={labelIndicatorYShift}
+                                labelIndicatorStroke={labelIndicatorStroke}
+                                labelIndicatorStrokeWidth={labelIndicatorStrokeWidth}
+                                labelIndicatorInnerOffset={labelIndicatorInnerOffset}
+                                labelIndicatorOuterOffset={labelIndicatorOuterOffset}
+                            />
+                        )}
+                    </Pie.Slice>
+                    <Pie.SliceAngularInset
+                        angularInset={{
+                            angularStrokeWidth,
+                            angularStrokeColor,
+                        }}
+                    />
+                </>
+                );
+            }}
+>>>>>>> Stashed changes
         </Pie.Chart>
     );
 }
