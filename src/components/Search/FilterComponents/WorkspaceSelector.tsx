86cbbccb92cb7cf51e41ce80899da0ecac6c9462
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-06-05 11:05:49 +0800

    customize workspace selector

--- src/components/Search/FilterComponents/WorkspaceSelector.tsx
+++ src/components/Search/FilterComponents/WorkspaceSelector.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import type {OnyxCollection} from 'react-native-onyx';
+import ActivityIndicator from '@components/ActivityIndicator';
 import type {SearchFilterCommonProps} from '@components/Search/types';
+import SelectionList from '@components/SelectionList';
+import MultiSelectListItem from '@components/SelectionList/ListItem/MultiSelectListItem';
+import type {ListItem, TextInputOptions} from '@components/SelectionList/types';
 import {advancedSearchPoliciesSelector, useAdvancedSearchFiltersWorkspaces} from '@hooks/useAdvancedSearchFilters';
 import useDebouncedState from '@hooks/useDebouncedState';
+import useLocalize from '@hooks/useLocalize';
 import useNetwork from '@hooks/useNetwork';
 import useOnyx from '@hooks/useOnyx';
+import useTheme from '@hooks/useTheme';
+import useThemeStyles from '@hooks/useThemeStyles';
+import type {SkeletonSpanReasonAttributes} from '@libs/telemetry/useSkeletonSpan';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
 import type {Policy} from '@src/types/onyx';
 import type {Icon} from '@src/types/onyx/OnyxCommon';
 import {getEmptyObject} from '@src/types/utils/EmptyObject';
 import isLoadingOnyxValue from '@src/types/utils/isLoadingOnyxValue';
+import ListFilterView from './ListFilterViewWrapper';
 import type {MultiSelectItem} from './MultiSelect';
-import MultiSelect from './MultiSelect';
 
 type WorkspaceSelectorProps = SearchFilterCommonProps & {
     policyIDQuery: string[] | undefined;
@@ -21,9 +31,12 @@ type WorkspaceSelectorProps = SearchFilterCommonProps & {
 
 function WorkspaceSelector({policyIDQuery, value, selectionListTextInputStyle, selectionListStyle, autoFocus, ready = true, footer, onChange}: WorkspaceSelectorProps) {
     const {isOffline} = useNetwork();
+    const {translate} = useLocalize();
+    const theme = useTheme();
+    const styles = useThemeStyles();
     const [isLoadingApp] = useOnyx(ONYXKEYS.IS_LOADING_APP);
     const [policies = getEmptyObject<NonNullable<OnyxCollection<Policy>>>(), policiesResult] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: advancedSearchPoliciesSelector});
-    const [, debouncedSearchTerm, setSearchTerm] = useDebouncedState('');
+    const [searchTerm, debouncedSearchTerm, setSearchTerm] = useDebouncedState('');
     const {workspaces, shouldShowWorkspaceSearchInput} = useAdvancedSearchFiltersWorkspaces(policies, debouncedSearchTerm);
     const workspaceOptions: Array<MultiSelectItem<string>> = workspaces
         .flatMap((section) => section.data)
@@ -34,23 +47,70 @@ function WorkspaceSelector({policyIDQuery, value, selectionListTextInputStyle, s
             icons: workspace.icons,
         }));
 
-    const policyID = value ?? policyIDQuery ?? [];
-    const selectedWorkspaceOptions = workspaceOptions.filter((option) => (Array.isArray(policyID) ? policyID : [policyID]).includes(option.value));
+    const [selectedItems, setSelectedItems] = useState(() => {
+        const policyID = value ?? policyIDQuery ?? [];
+        return workspaceOptions.filter((option) => (Array.isArray(policyID) ? policyID : [policyID]).includes(option.value));
+    });
+
+    const updateSelectedItems = (item: ListItem) => {
+        if (item.isSelected) {
+            setSelectedItems((prevSelectedItems) => prevSelectedItems.filter((i) => i.value !== item.keyForList));
+            return;
+        }
+
+        const newItem = workspaceOptions.find((i) => i.value === item.keyForList);
+        if (!newItem) {
+            return;
+        }
+        setSelectedItems((prevSelectedItems) => [...prevSelectedItems, newItem]);
+    };
+
+    const listData: ListItem[] = workspaceOptions.map((item) => ({
+        text: item.text,
+        keyForList: item.value,
+        isSelected: !!selectedItems.find((i) => i.value === item.value),
+        icons: item.icons,
+    }));
+
+    const textInputOptions: TextInputOptions = {
+        value: searchTerm,
+        label: shouldShowWorkspaceSearchInput ? translate('common.search') : undefined,
+        onChangeText: setSearchTerm,
+        headerMessage: shouldShowWorkspaceSearchInput && listData.length === 0 ? translate('common.noResultsFound') : undefined,
+        style: {
+            containerStyle: selectionListTextInputStyle,
+        },
+        disableAutoFocus: !autoFocus,
+    };
+
+    const reasonAttributes: SkeletonSpanReasonAttributes = {context: 'MultiSelectDataLoading'};
 
     return (
-        <MultiSelect
-            items={workspaceOptions}
-            value={selectedWorkspaceOptions}
-            autoFocus={autoFocus}
+        <ListFilterView
+            itemCount={listData.length}
             isSearchable={shouldShowWorkspaceSearchInput}
-            selectionListTextInputStyle={selectionListTextInputStyle}
-            selectionListStyle={selectionListStyle}
-            footer={footer}
-            loading={isLoadingApp && !isOffline}
-            shouldShowLoadingPlaceholder={isLoadingOnyxValue(policiesResult) || !ready}
-            onChange={(policyIDs) => onChange(policyIDs.map((id) => id.value))}
-            onSearch={setSearchTerm}
-        />
+        >
+            {isLoadingApp && !isOffline ? (
+                <View style={[styles.flex1, styles.justifyContentCenter, styles.alignItemsCenter]}>
+                    <ActivityIndicator
+                        size={CONST.ACTIVITY_INDICATOR_SIZE.SMALL}
+                        color={theme.spinner}
+                        reasonAttributes={reasonAttributes}
+                    />
+                </View>
+            ) : (
+                <SelectionList
+                    shouldSingleExecuteRowSelect
+                    shouldShowLoadingPlaceholder={isLoadingOnyxValue(policiesResult) || !ready}
+                    data={listData}
+                    ListItem={MultiSelectListItem}
+                    onSelectRow={updateSelectedItems}
+                    textInputOptions={textInputOptions}
+                    style={{contentContainerStyle: [styles.pb0], ...selectionListStyle}}
+                    footerContent={footer}
+                />
+            )}
+        </ListFilterView>
     );
 }
 
