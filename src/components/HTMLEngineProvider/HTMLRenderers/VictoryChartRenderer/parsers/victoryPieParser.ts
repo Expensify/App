ad4c32278f89a6c6db5b481d1d5d3ca2b400e7f6
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-06-08 18:43:48 +0100

    add parseRawChartData

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/parsers/victoryPieParser.ts
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/parsers/victoryPieParser.ts
@@ -2,15 +2,14 @@ import type {Color} from '@shopify/react-native-skia';
 import type {TNode} from 'react-native-render-html';
 import VictoryTheme from '@components/Charts/VictoryTheme';
 import {COLOR_KEY, LABEL_KEY, VALUE_KEY} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/constants';
-import type {PartialProcessNodeResult, PolarChartData, RawChartData} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/types';
-import isNonNullObject from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/isNonNullObject';
-import parseArrayAttribute from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseArrayAttribute';
+import type {PartialProcessNodeResult, PolarChartData} from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/types';
+import parseRawChartData from '@components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseRawChartData';
 
 /**
  * Parse data categories from a `<victorypie>` node.
  */
 function parseVictoryPieNode(tnode: TNode): PartialProcessNodeResult {
-    const categories = parseArrayAttribute<RawChartData>(tnode.attributes.data).filter(isNonNullObject<RawChartData>);
+    const categories = parseRawChartData(tnode.attributes.data);
     const colorScale = parseArrayAttribute<Color>(tnode.attributes.colorscale);
     const data: Record<string, PolarChartData> = {};
     for (const [index, category] of categories.entries()) {
