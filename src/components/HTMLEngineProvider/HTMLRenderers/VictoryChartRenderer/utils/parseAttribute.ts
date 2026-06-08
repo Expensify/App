96059d69fe54568a1853d7c8691d4111e47f5dc5
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-06-08 16:43:53 +0100

    remove type argument and always return unknown type in parseAttribute

--- src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseAttribute.ts
+++ src/components/HTMLEngineProvider/HTMLRenderers/VictoryChartRenderer/utils/parseAttribute.ts
@@ -6,15 +6,15 @@ import JSON5 from 'json5';
  *        : "[ {x: 'Jan', y: 3} ]" -> `[{"x": "Jan", "y": 3}]` (Valid RFC 8259)
  *        : "Green" -> "Green"
  */
-function parseAttribute<T>(attribute: string): T | undefined {
+function parseAttribute(attribute: string): unknown {
     if (!attribute) {
         return undefined;
     }
     try {
         // Using JSON5 instead of JSON because the former is not as strict as the later e.g. can parse objects with non-stringified fields `'{x: 100}'`
-        return JSON5.parse<T>(attribute);
+        return JSON5.parse(attribute);
     } catch {
-        return attribute as T;
+        return attribute;
     }
 }
 
