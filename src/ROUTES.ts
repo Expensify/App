53f52d81130689765f73012eeb35e46167c2e60c
Vit Horacek (via MelvinBot) <infra+melvinbot@expensify.com>
2026-06-10 14:54:01 +0000

    Revert "Merge pull request #91627 from Expensify/claude-migrateInitialListValueSelectorModal"
    This reverts commit 0f5f69c3d666c83e84f2a1e39eb15b4c2e7bf7b7, reversing
    changes made to ba47db3ae02dfa36b7e0025225cd21f2561298f0.
    
    Co-authored-by: Vit Horacek <mountiny@users.noreply.github.com>

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -968,10 +968,6 @@ const DYNAMIC_ROUTES = {
         entryScreens: [SCREENS.REPORT, SCREENS.RIGHT_MODAL.SEARCH_REPORT, SCREENS.RIGHT_MODAL.EXPENSE_REPORT, SCREENS.RIGHT_MODAL.SEARCH_MONEY_REQUEST_REPORT],
         getRoute: (reportID: string, reportActionID: string) => `flag/${reportID}/${reportActionID}`,
     },
-    WORKSPACE_REPORT_FIELDS_INITIAL_LIST_VALUE: {
-        path: 'initial-list-value',
-        entryScreens: [SCREENS.WORKSPACE.REPORT_FIELDS_CREATE],
-    },
 } as const satisfies DynamicRoutes;
 
 const ROUTES = {
