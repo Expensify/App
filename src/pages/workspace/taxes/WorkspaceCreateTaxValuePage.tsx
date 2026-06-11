80cab79ffba3be1bee29aa7708bf465c0a24e3f6
Nabi <nabi.zarin.work@gmail.com>
2026-06-11 09:46:16 +0430

    fix: prevent Enter save from reopening tax rate value field

--- src/pages/workspace/taxes/WorkspaceCreateTaxValuePage.tsx
+++ src/pages/workspace/taxes/WorkspaceCreateTaxValuePage.tsx
@@ -12,6 +12,7 @@ import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {setDraftValues} from '@libs/actions/FormActions';
 import Navigation from '@libs/Navigation/Navigation';
+import {skipNextFocusRestore} from '@libs/NavigationFocusReturn';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 // eslint-disable-next-line no-restricted-imports -- The input ref doesn't exist at the navigate() call site, so we can't use Navigation's afterTransition callback
 import TransitionTracker from '@libs/Navigation/TransitionTracker';
@@ -41,6 +42,7 @@ function WorkspaceCreateTaxValuePage({
     const save = () => {
         const normalizedValue = currentValue !== undefined ? String(Number(currentValue)) : currentValue;
         setDraftValues(ONYXKEYS.FORMS.WORKSPACE_NEW_TAX_FORM, {[INPUT_IDS.VALUE]: normalizedValue});
+        skipNextFocusRestore();
         goBack();
     };
 
