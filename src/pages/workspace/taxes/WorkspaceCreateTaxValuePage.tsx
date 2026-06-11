d9759444e7070bdd39fccc288ea3918ba0e8cedb
Nabi <nabi.zarin.work@gmail.com>
2026-06-11 10:35:24 +0430

    format tax value page imports

--- src/pages/workspace/taxes/WorkspaceCreateTaxValuePage.tsx
+++ src/pages/workspace/taxes/WorkspaceCreateTaxValuePage.tsx
@@ -12,11 +12,11 @@ import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {setDraftValues} from '@libs/actions/FormActions';
 import Navigation from '@libs/Navigation/Navigation';
-import {skipNextFocusRestore} from '@libs/NavigationFocusReturn';
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 // eslint-disable-next-line no-restricted-imports -- The input ref doesn't exist at the navigate() call site, so we can't use Navigation's afterTransition callback
 import TransitionTracker from '@libs/Navigation/TransitionTracker';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
+import {skipNextFocusRestore} from '@libs/NavigationFocusReturn';
 import variables from '@styles/variables';
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
