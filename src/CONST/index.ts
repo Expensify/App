6194df0c6068b0fdfe090c941785b84584902184
mkhutornyi <mkhutorniy@gmail.com>
2026-06-12 14:52:06 +0100

    Revert "Merge pull request #92683 from mkhutornyi/revert-92365-revert-92345-fix-90023"
    This reverts commit 07cab47e955622bc575d6a249a1097ce4d823503, reversing
    changes made to 9ab43e4315f5cfd6585792602159fd365b50d3ae.

--- src/CONST/index.ts
+++ src/CONST/index.ts
@@ -2438,6 +2438,9 @@ const CONST = {
         GATEWAY_TIMEOUT: 504,
         UNKNOWN_ERROR: 520,
     },
+    HTTP_HEADER_NAMES: {
+        AUTH_TOKEN: 'authToken',
+    },
     ERROR: {
         XHR_FAILED: 'xhrFailed',
         THROTTLED: 'throttled',
