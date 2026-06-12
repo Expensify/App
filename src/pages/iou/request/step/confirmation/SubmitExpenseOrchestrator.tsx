ffb56134fcc742161156be67639f72a6702dc630
Michał Rejdak <michal.rejdak@swmansion.com>
2026-06-12 13:30:43 +0200

    remove last usage and handle itself

--- src/pages/iou/request/step/confirmation/SubmitExpenseOrchestrator.tsx
+++ src/pages/iou/request/step/confirmation/SubmitExpenseOrchestrator.tsx
@@ -8,7 +8,6 @@ import isReportOpenInRHP from '@libs/Navigation/helpers/isReportOpenInRHP';
 import isReportOpenInSuperWideRHP from '@libs/Navigation/helpers/isReportOpenInSuperWideRHP';
 import isReportTopmostSplitNavigator from '@libs/Navigation/helpers/isReportTopmostSplitNavigator';
 import isSearchTopmostFullScreenRoute from '@libs/Navigation/helpers/isSearchTopmostFullScreenRoute';
-import navigateAfterInteraction from '@libs/Navigation/navigateAfterInteraction';
 import Navigation, {navigationRef} from '@libs/Navigation/Navigation';
 import {getReportOrDraftReport, isMoneyRequestReport} from '@libs/ReportUtils';
 import {buildCannedSearchQuery, getCurrentSearchQueryJSON} from '@libs/SearchQueryUtils';
@@ -408,9 +407,7 @@ function SubmitExpenseOrchestrator({
                         startSubmitSpans();
                         setFastPath(CONST.TELEMETRY.FAST_PATH_HANDLER.DEFAULT);
                         reserveSearchChannelIfGlobalCreate();
-                        navigateAfterInteraction(() => {
-                            createTransaction(true);
-                        });
+                        createTransaction(true);
                     }}
                     onDeny={(wasUserInitiated) => {
                         startSubmitSpans();
@@ -419,9 +416,7 @@ function SubmitExpenseOrchestrator({
                             updateLastLocationPermissionPrompt();
                         }
                         reserveSearchChannelIfGlobalCreate();
-                        navigateAfterInteraction(() => {
-                            createTransaction(false);
-                        });
+                        createTransaction(false);
                     }}
                     onInitialGetLocationCompleted={() => {
                         setIsConfirming(false);
