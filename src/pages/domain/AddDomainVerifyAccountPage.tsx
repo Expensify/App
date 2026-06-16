3fd933598443740c314115f21d7cfb05346800f6
autodev-botbit <hutianci20@outlook.com>
2026-06-16 16:18:47 +0800

    fix: [$250] Required 2FA setup screen is unresponsive, blocking new-domain lo
    Fixes #91985

--- src/pages/domain/AddDomainVerifyAccountPage.tsx
+++ src/pages/domain/AddDomainVerifyAccountPage.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
-import VerifyAccountPageBase from '@pages/settings/VerifyAccountPageBase';
-import ROUTES from '@src/ROUTES';
-
-function AddDomainVerifyAccountPage() {
-    return <VerifyAccountPageBase navigateBackTo={ROUTES.WORKSPACES_ADD_DOMAIN} />;
+{
+  "file": "src/pages/domain/AddDomainVerifyAccountPage.tsx",
+  "content": "import React from 'react';\nimport VerifyAccountPageBase from '@pages/settings/VerifyAccountPageBase';\nimport ROUTES from '@src/ROUTES';\n\nfunction AddDomainVerifyAccountPage() {\n    return <VerifyAccountPageBase navigateBackTo={ROUTES.WORKSPACES_ADD_DOMAIN} shouldForceEnableTwoFactorAuth />;\n}\n\nexport default AddDomainVerifyAccountPage;"
 }
-
-export default AddDomainVerifyAccountPage;
