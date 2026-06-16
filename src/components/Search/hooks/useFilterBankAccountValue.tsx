e8c320f8544759fadd01f21526c27b6f1671cfbd
allgandaf <whogandalf@gmail.com>
2026-06-16 19:40:53 +0530

    Resolve filter-bar pill label from full bankAccountList so closed selections render

--- src/components/Search/hooks/useFilterBankAccountValue.tsx
+++ src/components/Search/hooks/useFilterBankAccountValue.tsx
@@ -1,11 +1,11 @@
 import useOnyx from '@hooks/useOnyx';
-import {getBankAccountSearchLabel, getSearchEligibleBankAccounts} from '@libs/BankAccountUtils';
+import {getBankAccountSearchLabel} from '@libs/BankAccountUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
 
 function useFilterBankAccountValue(value: string[]): string {
     const [bankAccountList] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST);
 
-    const bankAccountLabels = Object.values(getSearchEligibleBankAccounts(bankAccountList))
+    const bankAccountLabels = Object.values(bankAccountList ?? {})
         .filter((bankAccount) => value.includes(bankAccount?.accountData?.bankAccountID?.toString() ?? ''))
         .map((bankAccount) => getBankAccountSearchLabel(bankAccount));
 
