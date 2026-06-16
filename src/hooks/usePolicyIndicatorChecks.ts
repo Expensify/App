1ebd2560f5961eac57c03cdfbacb22cd877e4f6c
mhawryluk <marcinhawrylukxx@gmail.com>
2026-06-16 15:18:49 +0200

    Refactor usePolicyIndicatorChecks

--- src/hooks/usePolicyIndicatorChecks.ts
+++ src/hooks/usePolicyIndicatorChecks.ts
@@ -30,40 +30,41 @@ function usePolicyIndicatorChecks(): PolicyIndicatorChecksResult {
 
     const {cleanPolicies, policiesWithCardFeedErrors, isPolicyAdmin: isAdminOfPolicyWithCardFeedErrors} = usePoliciesWithCardFeedErrors();
 
-    const policyErrorChecks: Partial<Record<IndicatorStatus, Policy | undefined>> = {
-        [CONST.INDICATOR_STATUS.HAS_POLICY_ERRORS]: cleanPolicies.find(shouldShowPolicyError),
-        [CONST.INDICATOR_STATUS.HAS_CUSTOM_UNITS_ERROR]: cleanPolicies.find(shouldShowCustomUnitsError),
-        [CONST.INDICATOR_STATUS.HAS_EMPLOYEE_LIST_ERROR]: cleanPolicies.find(shouldShowEmployeeListError),
-        [CONST.INDICATOR_STATUS.HAS_SYNC_ERRORS]: cleanPolicies.find((cleanPolicy) =>
-            shouldShowSyncError(
-                cleanPolicy,
-                isConnectionInProgress(allConnectionSyncProgresses?.[`${ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS}${cleanPolicy?.id}`], cleanPolicy),
-                CONST.POLICY.CONNECTIONS.ACCOUNTING_CONNECTION_NAMES,
+    const policyErrorChecks: Array<[IndicatorStatus, Policy | undefined]> = [
+        [CONST.INDICATOR_STATUS.HAS_POLICY_ERRORS, cleanPolicies.find(shouldShowPolicyError)],
+        [CONST.INDICATOR_STATUS.HAS_CUSTOM_UNITS_ERROR, cleanPolicies.find(shouldShowCustomUnitsError)],
+        [CONST.INDICATOR_STATUS.HAS_EMPLOYEE_LIST_ERROR, cleanPolicies.find(shouldShowEmployeeListError)],
+        [
+            CONST.INDICATOR_STATUS.HAS_SYNC_ERRORS,
+            cleanPolicies.find((cleanPolicy) =>
+                shouldShowSyncError(
+                    cleanPolicy,
+                    isConnectionInProgress(allConnectionSyncProgresses?.[`${ONYXKEYS.COLLECTION.POLICY_CONNECTION_SYNC_PROGRESS}${cleanPolicy?.id}`], cleanPolicy),
+                    CONST.POLICY.CONNECTIONS.ACCOUNTING_CONNECTION_NAMES,
+                ),
             ),
-        ),
-        [CONST.INDICATOR_STATUS.HAS_QBO_EXPORT_ERROR]: cleanPolicies.find(shouldShowQBOReimbursableExportDestinationAccountError),
-        [CONST.INDICATOR_STATUS.HAS_UBER_CREDENTIALS_ERROR]: cleanPolicies.find(getUberConnectionErrorDirectlyFromPolicy),
-        [CONST.INDICATOR_STATUS.HAS_POLICY_ADMIN_CARD_FEED_ERRORS]: isAdminOfPolicyWithCardFeedErrors ? policiesWithCardFeedErrors.at(0) : undefined,
-    };
+        ],
+        [CONST.INDICATOR_STATUS.HAS_QBO_EXPORT_ERROR, cleanPolicies.find(shouldShowQBOReimbursableExportDestinationAccountError)],
+        [CONST.INDICATOR_STATUS.HAS_UBER_CREDENTIALS_ERROR, cleanPolicies.find(getUberConnectionErrorDirectlyFromPolicy)],
+        [CONST.INDICATOR_STATUS.HAS_POLICY_ADMIN_CARD_FEED_ERRORS, isAdminOfPolicyWithCardFeedErrors ? policiesWithCardFeedErrors.at(0) : undefined],
+    ];
+    const policyInfoChecks: Array<[IndicatorStatus, Policy | undefined]> = [
+        [CONST.INDICATOR_STATUS.HAS_MERGE_HR_SETUP_NEEDED, cleanPolicies.find((policy) => isPolicyAdmin(policy) && isMergeHRCompleteSetupNeeded(policy))],
+    ];
+    const domainChecks: Array<[IndicatorStatus, boolean]> = [
+        [CONST.INDICATOR_STATUS.HAS_DOMAIN_ERRORS, Object.values(allDomainErrors ?? {}).some((domainErrors) => hasDomainErrors(domainErrors))],
+    ];
 
-    const policyInfoChecks: Partial<Record<IndicatorStatus, Policy | undefined>> = {
-        [CONST.INDICATOR_STATUS.HAS_MERGE_HR_SETUP_NEEDED]: cleanPolicies.find((policy) => isPolicyAdmin(policy) && isMergeHRCompleteSetupNeeded(policy)),
-    };
+    const [policyErrorStatus] = policyErrorChecks.find(([, value]) => value) ?? [];
+    const [policyInfoStatus] = policyInfoChecks.find(([, value]) => value) ?? [];
+    const [domainStatus] = domainChecks.find(([, value]) => value) ?? [];
 
-    const domainChecks: Partial<Record<IndicatorStatus, boolean>> = {
-        [CONST.INDICATOR_STATUS.HAS_DOMAIN_ERRORS]: Object.values(allDomainErrors ?? {}).some((domainErrors) => hasDomainErrors(domainErrors)),
-    };
-
-    const [policyErrorStatus] = Object.entries(policyErrorChecks).find(([, value]) => value) ?? [];
-    const [policyInfoStatus] = Object.entries(policyInfoChecks).find(([, value]) => value) ?? [];
-    const [domainStatus] = Object.entries(domainChecks).find(([, value]) => value) ?? [];
-
-    const policyIDWithErrors = Object.values(policyErrorChecks).find(Boolean)?.id;
+    const policyIDWithErrors = policyErrorChecks.find(([, value]) => value)?.[1]?.id;
 
     return {
-        policyErrorStatus: policyErrorStatus as IndicatorStatus | undefined,
-        policyInfoStatus: policyInfoStatus as IndicatorStatus | undefined,
-        domainStatus: domainStatus as IndicatorStatus | undefined,
+        policyErrorStatus,
+        policyInfoStatus,
+        domainStatus,
         policyIDWithErrors,
     };
 }
