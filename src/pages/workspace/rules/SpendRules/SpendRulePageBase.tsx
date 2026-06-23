9430809f5054eec145aa4eaf252eb49f9669b28b
Jack Senyitko <jsenyitko@expensify.com>
2026-06-23 11:57:11 -0400

    remove dev lock

--- src/pages/workspace/rules/SpendRules/SpendRulePageBase.tsx
+++ src/pages/workspace/rules/SpendRules/SpendRulePageBase.tsx
@@ -15,7 +15,6 @@ import useCanWriteCardSpendRules from '@hooks/useCanWriteCardSpendRules';
 import useConfirmModal from '@hooks/useConfirmModal';
 import {useCurrencyListActions} from '@hooks/useCurrencyList';
 import useDefaultFundID from '@hooks/useDefaultFundID';
-import useEnvironment from '@hooks/useEnvironment';
 import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
@@ -68,7 +67,6 @@ function SpendRulePageBase({policyID, ruleID, titleKey, testID}: SpendRulePageBa
     const {translate} = useLocalize();
     const {showConfirmModal} = useConfirmModal();
     const policy = usePolicy(policyID);
-    const {isDevelopment} = useEnvironment();
 
     const {showReadOnlyModal} = usePolicyFeatureWriteAccess(policy, CONST.POLICY.POLICY_FEATURE.RULES);
     const canWriteSpendRules = useCanWriteCardSpendRules(policyID);
@@ -394,15 +392,13 @@ function SpendRulePageBase({policyID, ruleID, titleKey, testID}: SpendRulePageBa
         icon: isRulesRevampEnabled ? icons.CoinsButton : undefined,
     });
 
-    const currenciesMenuItem = isDevelopment
-        ? renderEditableMenuItem({
-              description: translate('workspace.rules.spendRules.permittedCurrencies'),
-              title: currenciesMenuTitle,
-              onPress: chooseCurrencies,
-              sentryLabel: CONST.SENTRY_LABEL.WORKSPACE.RULES.CURRENCY_SELECTOR,
-              icon: isRulesRevampEnabled ? icons.MoneyCircle : undefined,
-          })
-        : null;
+    const currenciesMenuItem = renderEditableMenuItem({
+        description: translate('workspace.rules.spendRules.permittedCurrencies'),
+        title: currenciesMenuTitle,
+        onPress: chooseCurrencies,
+        sentryLabel: CONST.SENTRY_LABEL.WORKSPACE.RULES.CURRENCY_SELECTOR,
+        icon: isRulesRevampEnabled ? icons.MoneyCircle : undefined,
+    });
 
     const revampFormContent = (
         <>
