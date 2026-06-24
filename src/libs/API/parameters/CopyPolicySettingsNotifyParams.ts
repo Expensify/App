0019d724e220661a1e52b434e30daa51d53bbf12
Fedi Rajhi <fedirjh@gmail.com>
2026-06-24 13:41:09 +0000

    Rename param to shouldOnlyNotifyOnFailure for CopyPolicySettingsNotify

--- src/libs/API/parameters/CopyPolicySettingsNotifyParams.ts
+++ src/libs/API/parameters/CopyPolicySettingsNotifyParams.ts
@@ -1,6 +1,6 @@
 type CopyPolicySettingsNotifyParams = {
     /** When true, only notify via Concierge if the copy fails (set when the user dismisses the in-progress modal) */
-    shouldNotifyOnFailure: boolean;
+    shouldOnlyNotifyOnFailure: boolean;
 };
 
 export default CopyPolicySettingsNotifyParams;
--- src/libs/actions/Policy/CopyPolicySettings.ts
+++ src/libs/actions/Policy/CopyPolicySettings.ts
@@ -75,8 +75,8 @@ function clearCopyPolicySettings(): void {
     Onyx.set(ONYXKEYS.COPY_POLICY_SETTINGS, {});
 }
 
-function requestCopyPolicySettingsNotification(shouldNotifyOnFailure = false): void {
-    write(WRITE_COMMANDS.COPY_POLICY_SETTINGS_NOTIFY, {shouldNotifyOnFailure});
+function requestCopyPolicySettingsNotification(shouldOnlyNotifyOnFailure = false): void {
+    write(WRITE_COMMANDS.COPY_POLICY_SETTINGS_NOTIFY, {shouldOnlyNotifyOnFailure});
 }
 
 function findCustomUnitByName(policy: Policy | undefined, unitName: string): CustomUnit | undefined {
