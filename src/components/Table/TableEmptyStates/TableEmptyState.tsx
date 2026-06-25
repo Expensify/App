e0d0d44d8254c07bf70ae8820710268cc028e2aa
Jack Senyitko <jsenyitko@expensify.com>
2026-06-25 13:18:47 -0400

    add the component logic

--- src/components/Table/TableEmptyStates/TableEmptyState.tsx
+++ src/components/Table/TableEmptyStates/TableEmptyState.tsx
@@ -1,5 +1,20 @@
-type TableEmptyStateProps = {};
+import GenericEmptyStateComponent from '@components/EmptyStateComponent/GenericEmptyStateComponent';
+import type {GenericEmptyStateComponentProps} from '@components/EmptyStateComponent/types';
+import ScrollView from '@components/ScrollView';
+import {useTableContext} from '@components/Table/TableContext';
+import useThemeStyles from '@hooks/useThemeStyles';
 
-export default function TableEmptyState({}: TableEmptyStateProps) {
-    return <></>;
+export default function TableEmptyState(emptyStateProps: GenericEmptyStateComponentProps) {
+    const styles = useThemeStyles();
+    const {originalDataLength} = useTableContext();
+
+    if (originalDataLength) {
+        return null;
+    }
+
+    return (
+        <ScrollView contentContainerStyle={[styles.flexGrow1, styles.flexShrink0]}>
+            <GenericEmptyStateComponent {...emptyStateProps} />
+        </ScrollView>
+    );
 }
--- src/components/Table/TableEmptyStates/TableNoResultsState.tsx
+++ src/components/Table/TableEmptyStates/TableNoResultsState.tsx
@@ -1,5 +1,20 @@
-type TableNoResultsStateProps = {};
+import GenericEmptyStateComponent from '@components/EmptyStateComponent/GenericEmptyStateComponent';
+import type {GenericEmptyStateComponentProps} from '@components/EmptyStateComponent/types';
+import ScrollView from '@components/ScrollView';
+import {useTableContext} from '@components/Table/TableContext';
+import useThemeStyles from '@hooks/useThemeStyles';
 
-export default function TableNoResultsState({}: TableNoResultsStateProps) {
-    return <></>;
+export default function TableNoResultsState(emptyStateProps: GenericEmptyStateComponentProps) {
+    const styles = useThemeStyles();
+    const {processedData, originalDataLength} = useTableContext();
+
+    if (!originalDataLength || processedData.length) {
+        return null;
+    }
+
+    return (
+        <ScrollView contentContainerStyle={[styles.flexGrow1, styles.flexShrink0]}>
+            <GenericEmptyStateComponent {...emptyStateProps} />
+        </ScrollView>
+    );
 }
