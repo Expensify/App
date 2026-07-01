f9f500fea1ba0ef4cff3b84c78af833e355f2652
borys3kk <szymon.banys01@wp.pl>
2026-07-01 12:49:55 +0200

    make the api symmetric for error growls, add duration resolving

--- src/libs/Growl.ts
+++ src/libs/Growl.ts
@@ -29,26 +29,28 @@ function setIsReady() {
 /**
  * Show the growl notification
  */
-function show(bodyText: string, type: GrowlType, duration: number = CONST.GROWL.DURATION, action?: GrowlAction) {
+function show(bodyText: string, type: GrowlType, duration?: number, action?: GrowlAction) {
+    // Default to a longer duration when there's an action button so users have time to tap it.
+    const resolvedDuration = duration ?? (action ? CONST.GROWL.DURATION_WITH_ACTION : CONST.GROWL.DURATION);
     isReadyPromise.then(() => {
         if (!growlRef?.current?.show) {
             return;
         }
-        growlRef.current.show(bodyText, type, duration, action);
+        growlRef.current.show(bodyText, type, resolvedDuration, action);
     });
 }
 
 /**
  * Show error growl
  */
-function error(bodyText: string, duration: number = CONST.GROWL.DURATION) {
-    show(bodyText, CONST.GROWL.ERROR, duration);
+function error(bodyText: string, duration?: number, action?: GrowlAction) {
+    show(bodyText, CONST.GROWL.ERROR, duration, action);
 }
 
 /**
  * Show success growl
  */
-function success(bodyText: string, duration: number = CONST.GROWL.DURATION, action?: GrowlAction) {
+function success(bodyText: string, duration?: number, action?: GrowlAction) {
     show(bodyText, CONST.GROWL.SUCCESS, duration, action);
 }
 
