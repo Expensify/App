988525bbb8cb9a717962086cfc5c9b1a986974b4
jayeshmangwani <jayeshmangwani@gmail.com>
2026-07-01 12:27:20 +0530

    fixed failing tests

--- tests/unit/hooks/useSelectedTransactionsActions.test.ts
+++ tests/unit/hooks/useSelectedTransactionsActions.test.ts
@@ -691,7 +691,7 @@ describe('useSelectedTransactionsActions', () => {
 
         unholdOption?.onSelected?.();
 
-        expect(unholdRequest).toHaveBeenCalledWith(transactionID, 'child123', undefined, false, CURRENT_USER_LOGIN, CURRENT_USER_ACCOUNT_ID, undefined);
+        expect(unholdRequest).toHaveBeenCalledWith(transactionID, 'child123', undefined, false, CURRENT_USER_LOGIN, CURRENT_USER_ACCOUNT_ID, undefined, false);
         expect(mockClearSelectedTransactions).toHaveBeenCalledWith(true);
     });
 
@@ -745,7 +745,7 @@ describe('useSelectedTransactionsActions', () => {
 
         unholdOption?.onSelected?.();
 
-        expect(unholdRequest).toHaveBeenCalledWith(transactionID, 'child123', undefined, true, CURRENT_USER_LOGIN, CURRENT_USER_ACCOUNT_ID, undefined);
+        expect(unholdRequest).toHaveBeenCalledWith(transactionID, 'child123', undefined, true, CURRENT_USER_LOGIN, CURRENT_USER_ACCOUNT_ID, undefined, false);
         expect(mockClearSelectedTransactions).toHaveBeenCalledWith(true);
     });
 
