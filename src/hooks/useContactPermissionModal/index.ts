f48ccaa8dbec4cd9ca53d732fe66f6023fe6a3ac
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2026-07-01 12:12:45 +0200

    fix ts

--- src/hooks/useContactPermissionModal/index.ts
+++ src/hooks/useContactPermissionModal/index.ts
@@ -1,2 +1,5 @@
+import type UseContactPermissionModalParams from './types';
+
 // Contact import is only available on native platforms, so this hook is a no-op on web.
-export default () => {};
+const useContactPermissionModal: (params: UseContactPermissionModalParams) => void = () => {};
+export default useContactPermissionModal;
