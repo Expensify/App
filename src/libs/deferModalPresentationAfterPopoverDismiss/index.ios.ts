787022ac1af22a8a97bfbc85a622c8610463e91c
krishna2323 <belivethatkg@gmail.com>
2026-07-02 20:06:53 +0530

    Split deferModal helper into platform-specific files
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/libs/deferModalPresentationAfterPopoverDismiss/index.ios.ts
+++ src/libs/deferModalPresentationAfterPopoverDismiss/index.ios.ts
@@ -1,17 +1,10 @@
-import CONST from '@src/CONST';
-import getPlatform from './getPlatform';
-import TransitionTracker from './Navigation/TransitionTracker';
+import TransitionTracker from '@libs/Navigation/TransitionTracker';
 
 /**
  * Presents a blocking modal after the payment popover dismisses.
  * On iOS, presenting a modal while another modal is dismissing freezes the app.
  */
 function deferModalPresentationAfterPopoverDismiss(presentModal: () => void) {
-    if (getPlatform() !== CONST.PLATFORM.IOS) {
-        presentModal();
-        return;
-    }
-
     TransitionTracker.runAfterTransitions({
         callback: presentModal,
         waitForUpcomingTransition: true,
