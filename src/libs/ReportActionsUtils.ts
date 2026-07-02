a5da7f1a9d563a2f1cc7de603dff6a510c1fb9ff
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-07-02 23:28:01 +0100

    update comment

--- src/libs/ReportActionsUtils.ts
+++ src/libs/ReportActionsUtils.ts
@@ -2796,7 +2796,7 @@ function getExportIntegrationActionFragments(translate: LocalizedTranslate, repo
                     url = nonReimbursableUrls.at(0)?.substring(0, SALESFORCE_EXPENSES_URL_PREFIX.length + 3) ?? '';
                     break;
                 case CONST.EXPORT_LABELS.RILLET:
-                    // TODO Test in R3 https://github.com/Expensify/App/issues/94848
+                    // s77rt TODO Test in R3 https://github.com/Expensify/App/issues/94848
                     url = '';
                     break;
                 default:
