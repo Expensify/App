929b6a81c86018d7a190e0b3a0e2a00438938a14
allgandaf <whogandalf@gmail.com>
2026-07-04 14:48:17 +0530

    Key the travel card icon on the transaction feedCountry

--- src/components/TransactionItemRow/DataCells/TypeCell.tsx
+++ src/components/TransactionItemRow/DataCells/TypeCell.tsx
@@ -8,7 +8,7 @@ import useOnyx from '@hooks/useOnyx';
 import useTheme from '@hooks/useTheme';
 import useThemeStyles from '@hooks/useThemeStyles';
 
-import {isTravelCard} from '@libs/CardUtils';
+import {isTravelCardTransaction} from '@libs/CardUtils';
 import {getExpenseTypeTranslationKey, getTransactionType, isExpensifyCardTransaction, isManagedCardTransaction, isPending} from '@libs/TransactionUtils';
 
 import variables from '@styles/variables';
@@ -73,7 +73,7 @@ function TypeCell({transactionItem, shouldUseNarrowLayout, shouldShowTooltip}: T
     const type = getTransactionType(transactionItem, card);
     const isExpensifyCard = isExpensifyCardTransaction(transactionItem);
     const isManagedCard = isManagedCardTransaction(transactionItem);
-    const isTravelInvoicingCard = isTravelCard(card);
+    const isTravelInvoicingCard = isTravelCardTransaction(transactionItem.feedCountry, card);
     const isPendingExpensifyCardTransaction = isExpensifyCard && isPending(transactionItem);
     const pendingIcon = isTravelInvoicingCard ? expensifyIcons.CreditCardWithPlaneHourglass : expensifyIcons.ExpensifyCardHourglass;
     const typeIcon = isPendingExpensifyCardTransaction ? pendingIcon : getTypeIcon(expensifyIcons, type, isExpensifyCard, isManagedCard, isTravelInvoicingCard);
