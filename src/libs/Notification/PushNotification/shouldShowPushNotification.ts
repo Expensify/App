59ef2814e1bf1180a922de5b0fa524a3437d5219
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-07-06 13:07:47 +0800

    pass the topmost one-transaction thread report id

--- src/libs/Notification/PushNotification/shouldShowPushNotification.ts
+++ src/libs/Notification/PushNotification/shouldShowPushNotification.ts
@@ -1,11 +1,13 @@
 import Log from '@libs/Log';
+import Navigation from '@libs/Navigation/Navigation';
+import {getIsOffline} from '@libs/NetworkState';
 import * as ReportActionUtils from '@libs/ReportActionsUtils';
 
 import * as Report from '@userActions/Report';
 
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type {ReportActions} from '@src/types/onyx';
+import type {Report as ReportType, ReportActions} from '@src/types/onyx';
 
 import type {PushPayload} from '@ua/react-native-airship';
 import type {OnyxCollection} from 'react-native-onyx';
@@ -31,6 +33,14 @@ Onyx.connectWithoutView({
     callback: (value) => (allReportActions = value),
 });
 
+// We do not depend on updates on the UI for notifications, so we can use `connectWithoutView` here.
+let allReports: OnyxCollection<ReportType>;
+Onyx.connectWithoutView({
+    key: ONYXKEYS.COLLECTION.REPORT,
+    waitForCollectionCallback: true,
+    callback: (value) => (allReports = value),
+});
+
 /**
  * Returns whether the given Airship notification should be shown depending on the current state of the app
  */
@@ -47,7 +57,12 @@ export default function shouldShowPushNotification(pushPayload: PushPayload): bo
         shouldShow = true;
     } else {
         const reportAction = ReportActionUtils.getLatestReportActionFromOnyxData(data.onyxData ?? null);
-        shouldShow = Report.shouldShowReportActionNotification(String(data.reportID), allReportActions, currentUserAccountID, reportAction, true);
+        const topmostReportID = Navigation.getTopmostReportId();
+        const topmostReport = allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${topmostReportID}`];
+        const topmostChatReport = allReports?.[`${ONYXKEYS.COLLECTION.REPORT}${topmostReport?.chatReportID}`];
+        const topmostReportActions = allReportActions?.[`${ONYXKEYS.COLLECTION.REPORT_ACTIONS}${topmostReportID}`];
+        const topmostOneTransactionThreadReportID = ReportActionUtils.getOneTransactionThreadReportID(topmostReport, topmostChatReport, topmostReportActions, getIsOffline());
+        shouldShow = Report.shouldShowReportActionNotification(String(data.reportID), topmostOneTransactionThreadReportID, currentUserAccountID, reportAction, true);
     }
 
     Log.info(`[PushNotification] ${shouldShow ? 'Showing' : 'Not showing'} notification`);
