626fb1afffc3c901d7e91fa6e68d4955f92bcd99
nkdengineer <nkdengineer@outlook.com>
2026-07-09 11:45:07 +0700

    update style JSDoc

--- src/pages/inbox/report/useDebouncedSaveDraft.ts
+++ src/pages/inbox/report/useDebouncedSaveDraft.ts
@@ -6,10 +6,10 @@ import {useRef} from 'react';
 
 /**
  * Debounces a function to save a draft for a report comment or report action draft.
- * @param saveDraft - The function to save the draft. It will be called with the arguments passed to the triggerSaveDraft function.
- * @returns An object containing the debounced save draft function, the trigger save draft function, and the is save pending ref.
- * @property {Function} saveDraft - The debounced save draft function.
- * @property {Ref<boolean>} isSavePending - The ref to check whether the save is pending.
+ * @param saveDraftFn - The function to save the draft. It will be called with the arguments passed to saveDraft.
+ * @returns An object containing the debounced save draft function and the is-save-pending ref.
+ * @property saveDraft - The debounced save draft function.
+ * @property isSavePending - The ref to check whether the save is pending.
  */
 function useDebouncedSaveDraft<SaveDraftArgs extends unknown[]>(saveDraftFn: (...args: SaveDraftArgs) => void, wait = CONST.TIMING.DRAFT_SAVE_DEBOUNCE_TIME) {
     const isSavePending = useRef(false);
