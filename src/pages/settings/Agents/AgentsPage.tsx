4e2e89fd311247eb6421eed8578fc02b05875c2a
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-07-13 11:03:50 +0100

    should always display errors if we have errors

--- src/pages/settings/Agents/AgentsPage.tsx
+++ src/pages/settings/Agents/AgentsPage.tsx
@@ -65,9 +65,6 @@ function AgentsPage() {
         }
     };
 
-    const shouldShowErrors = (pendingAction: PendingAction | null | undefined) =>
-        pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.ADD || pendingAction === CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE;
-
     const agents: AgentRowData[] = Object.entries(agentPrompts ?? {}).flatMap(([key, agentPrompt]) => {
         const accountID = Number(key.slice(ONYXKEYS.COLLECTION.SHARED_NVP_AGENT_PROMPT.length));
         const details = personalDetailsList?.[accountID];
@@ -82,7 +79,7 @@ function AgentsPage() {
         }
 
         const mergedErrors = {
-            ...(shouldShowErrors(pendingAction) ? getLatestError(agentPrompt?.errors ?? undefined) : {}),
+            ...getLatestError(agentPrompt?.errors ?? undefined),
             ...getLatestError(agentPrompt?.nameErrors ?? undefined),
             ...getLatestError(agentPrompt?.promptErrors ?? undefined),
             ...getLatestError(agentPrompt?.avatarErrors ?? undefined),
