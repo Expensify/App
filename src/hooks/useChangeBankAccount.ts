481ad632a221aed94cffaabb4b162e75a542f0fd
VickyStash <vikstash@gmail.com>
2026-07-14 11:26:06 +0200

    Use Onyx selector for hasOtherEligibleAccountsToConnect

--- src/hooks/useChangeBankAccount.ts
+++ src/hooks/useChangeBankAccount.ts
@@ -16,14 +16,15 @@ import useOnyx from './useOnyx';
  * Shared by ConnectedVerifiedBankAccount and VerifiedBankAccountFlowEntryPoint.
  */
 function useChangeBankAccount(policyID: string | undefined, currency: string | undefined, bankAccountID: number | undefined) {
-    const [bankAccountList] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST);
+    const [hasOtherEligibleAccountsToConnect = false] = useOnyx(ONYXKEYS.BANK_ACCOUNT_LIST, {
+        selector: (bankAccountList) => getEligibleExistingBusinessBankAccounts(bankAccountList, currency, true, bankAccountID).length > 0,
+    });
 
     return () => {
         if (!policyID || !currency) {
             return;
         }
 
-        const hasOtherEligibleAccountsToConnect = getEligibleExistingBusinessBankAccounts(bankAccountList, currency, true, bankAccountID).length > 0;
         if (hasOtherEligibleAccountsToConnect) {
             Navigation.navigate(ROUTES.BANK_ACCOUNT_CONNECT_EXISTING_BUSINESS_BANK_ACCOUNT.getRoute(policyID, undefined, CONST.BANK_ACCOUNT.CONNECT_EXISTING_SOURCE.CHANGE_BANK_ACCOUNT));
             return;
