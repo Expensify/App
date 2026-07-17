bae20ea37f9f00dd5c666cb639d0b6194c5c94b8
Shubham Agrawal <shubham.agra1206@gmail.com>
2026-07-17 18:57:39 +0530

    Fix ts

--- src/libs/actions/Report/index.ts
+++ src/libs/actions/Report/index.ts
@@ -444,6 +444,7 @@ type MergeReportsProps = {
     bankAccountList: OnyxEntry<BankAccountList>;
     isTrackIntentUser: boolean | undefined;
     personalPolicyOutputCurrency: string | undefined;
+    selfDMReportActions: OnyxEntry<ReportActions>;
 };
 
 const addNewMessageWithText = new Set<string>([WRITE_COMMANDS.ADD_COMMENT, WRITE_COMMANDS.ADD_TEXT_AND_ATTACHMENT]);
@@ -8096,6 +8097,7 @@ function mergeReports({
     allReports: allReportsParam,
     isTrackIntentUser,
     personalPolicyOutputCurrency,
+    selfDMReportActions,
 }: MergeReportsProps) {
     const reports = allReportsParam ?? allReports;
     const destinationReport = reports?.[`${ONYXKEYS.COLLECTION.REPORT}${destinationReportID}`];
@@ -8124,6 +8126,7 @@ function mergeReports({
         skippedReportIDs: sourceReportIDs,
         isTrackIntentUser,
         personalPolicyOutputCurrency,
+        selfDMReportActions,
     });
 
     const {
