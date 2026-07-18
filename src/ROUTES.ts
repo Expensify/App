3919fa6aa8e4ab9765242c453c69b820410ed145
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-07-18 15:11:05 +0100

    make cardID param optional

--- src/ROUTES.ts
+++ src/ROUTES.ts
@@ -804,9 +804,12 @@ const DYNAMIC_ROUTES = {
         getRoute: (feed: CompanyCardFeedWithDomainID, cardID: string) => `company-card-details/${encodeURIComponent(feed)}/${encodeURIComponent(cardID)}` as const,
     },
     WORKSPACE_COMPANY_CARD_EXPORT: {
-        path: ':cardID/edit/export',
+        path: 'edit/export/:cardID?',
         entryScreens: [SCREENS.WORKSPACE.DYNAMIC_COMPANY_CARD_DETAILS, SCREENS.WORKSPACE.ACCOUNTING.RILLET_CARD_ACCOUNT_CARD_LIST],
-        getRoute: (cardID: string) => `${encodeURIComponent(cardID)}/edit/export` as const,
+
+        // The cardID param is optional because some entry screens e.g. `SCREENS.WORKSPACE.DYNAMIC_COMPANY_CARD_DETAILS`
+        // already has the cardID in their path and we don't want to double pass the cardID.
+        getRoute: (cardID?: string) => `edit/export${cardID ? `/${encodeURIComponent(cardID)}` : ''}` as const,
     },
     WORKSPACE_COMPANY_CARDS_ASSIGN_CARD_ASSIGNEE: {
         path: 'assign-card/:feed/:cardID/assignee',
