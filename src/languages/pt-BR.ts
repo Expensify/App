1d1ff25fa068a03b4e130220d0ff39cd23212606
Jack Senyitko <jsenyitko@expensify.com>
2026-07-21 08:13:49 -0400

    Revert "Merge pull request #90403 from thelullabyy/fix-lullabyy/90366"
    This reverts commit 4c9698b213713e8d6d30ac2d9d727de471a32999, reversing
    changes made to acedb0276b7a7b86f93968d6233389e5a6c0960b.

--- src/languages/pt-BR.ts
+++ src/languages/pt-BR.ts
@@ -517,7 +517,6 @@ const translations: TranslationDeepObject<typeof en> = {
         previousYear: 'Ano anterior',
         nextYear: 'Ano que vem',
         avatar: 'Avatar',
-        currentOfTotal: ({current, total}: {current: number; total: number}) => `${current} de ${total}`,
         editor: 'Editor',
         restrictions: 'Restrições',
         tryAgain: 'Tentar novamente',
