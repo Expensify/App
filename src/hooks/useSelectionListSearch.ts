9340d3e1918ed31aadf085ffa21140573a12a723
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-07-23 04:27:30 +0100

    fix compiler error

--- src/hooks/useSelectionListSearch.ts
+++ src/hooks/useSelectionListSearch.ts
@@ -9,8 +9,7 @@ import useSearchResults from './useSearchResults';
 
 function useSelectionListSearch<T extends ListItem & {value: string}>(data: T[]) {
     const {translate} = useLocalize();
-    const filterData = (item: T, searchInput: string) => tokenizedSearch([item], searchInput, () => [item.text ?? '', item.value]).length > 0;
-    const [searchValue, setSearchValue, filteredData] = useSearchResults(data, filterData);
+    const [searchValue, setSearchValue, filteredData] = useSearchResults(data, (item, searchInput) => tokenizedSearch([item], searchInput, () => [item.text ?? '', item.value]).length > 0);
     const textInputOptions = {
         label: data.length >= CONST.STANDARD_LIST_ITEM_LIMIT ? translate('common.search') : undefined,
         value: searchValue,
