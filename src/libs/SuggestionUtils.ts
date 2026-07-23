2219d4a01188f480a203bd490f31997c3321c4f6
Maruf Sharifi <maruf.sharifi.work@gmail.com>
2026-07-23 18:42:56 +0430

    Refactor mention suggestion phone formatter

--- src/libs/SuggestionUtils.ts
+++ src/libs/SuggestionUtils.ts
@@ -2,7 +2,6 @@ import type {LocaleContextProps} from '@components/LocaleContextProvider';
 
 import type {PersonalDetails} from '@src/types/onyx';
 
-import {formatPhoneNumber as formatPhoneNumberPhoneUtils} from './LocalePhoneNumber';
 import {getDisplayNameForParticipant} from './ReportUtils';
 
 /**
@@ -12,8 +11,8 @@ function trimLeadingSpace(str: string): string {
     return str.startsWith(' ') ? str.slice(1) : str;
 }
 
-function getDisplayName(details: PersonalDetails) {
-    const displayNameFromAccountID = getDisplayNameForParticipant({accountID: details.accountID, formatPhoneNumber: formatPhoneNumberPhoneUtils});
+function getDisplayName(details: PersonalDetails, formatPhoneNumber: LocaleContextProps['formatPhoneNumber']) {
+    const displayNameFromAccountID = getDisplayNameForParticipant({accountID: details.accountID, formatPhoneNumber});
     if (!displayNameFromAccountID) {
         return details.login?.length ? details.login : '';
     }
@@ -23,13 +22,17 @@ function getDisplayName(details: PersonalDetails) {
 /**
  * Function to sort users. It compares weights, display names, and accountIDs in that order
  */
-function getSortedPersonalDetails(personalDetails: Array<PersonalDetails & {weight: number}>, localeCompare: LocaleContextProps['localeCompare']) {
+function getSortedPersonalDetails(
+    personalDetails: Array<PersonalDetails & {weight: number}>,
+    localeCompare: LocaleContextProps['localeCompare'],
+    formatPhoneNumber: LocaleContextProps['formatPhoneNumber'],
+) {
     return personalDetails.sort((first, second) => {
         if (first.weight !== second.weight) {
             return first.weight - second.weight;
         }
 
-        const displayNameLoginOrder = localeCompare(getDisplayName(first), getDisplayName(second));
+        const displayNameLoginOrder = localeCompare(getDisplayName(first, formatPhoneNumber), getDisplayName(second, formatPhoneNumber));
         if (displayNameLoginOrder !== 0) {
             return displayNameLoginOrder;
         }
