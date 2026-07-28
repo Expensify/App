9006953a25dd188d9ade8eb59d66319cf85a8789
gijoe0295 <153004152+gijoe0295@users.noreply.github.com>
2026-07-28 18:01:29 +0700

    Extract types and export components

---
+++ src/components/FeatureTraining/index.tsx
@@ -0,0 +1,31 @@
+import FeatureTrainingBase from './FeatureTraining';
+import FeatureTrainingCarousel from './FeatureTrainingCarousel';
+import BackButton from './primitives/BackButton';
+import Body from './primitives/Body';
+import ButtonRow from './primitives/ButtonRow';
+import ConfirmButton from './primitives/ConfirmButton';
+import Description from './primitives/Description';
+import DismissOption from './primitives/DismissOption';
+import HelpButton from './primitives/HelpButton';
+import Illustration from './primitives/Illustration';
+import Page from './primitives/Page';
+import Subtitle from './primitives/Subtitle';
+import Title from './primitives/Title';
+
+const FeatureTraining = Object.assign(FeatureTrainingBase, {
+    Carousel: FeatureTrainingCarousel,
+    Page,
+    Illustration,
+    Body,
+    Title,
+    Subtitle,
+    Description,
+    DismissOption,
+    HelpButton,
+    ConfirmButton,
+    BackButton,
+    ButtonRow,
+});
+
+export default FeatureTraining;
+export type {FeatureTrainingProps, FeatureTrainingCarouselProps, FeatureTrainingModalProps} from './types';
