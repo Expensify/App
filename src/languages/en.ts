82bf645f0f59b3d50fc1fbe9b6f4bfb817544007
Aldo Canepa <aldo@expensify.com>
2026-07-29 13:33:36 -0700

    Fix english translation again

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -9925,7 +9925,7 @@ const translations = {
         keepThisOne: 'Keep this one',
         confirmDetails: `Confirm the details you're keeping`,
         confirmDuplicatesInfo: `The duplicates you don't keep will be held for the submitter to delete.`,
-        cannotMergeDuplicates: 'You can only merge expenses on reports that are open or pending first approval. You may need to retract the report, merge the expenses, then resubmit.',
+        cannotMergeDuplicates: 'You can only merge expenses on draft or outstanding reports. Retract it and try again.',
         hold: 'This expense was put on hold',
         resolvedDuplicates: 'resolved the duplicate',
         companyCardRequired: 'Company card purchases required',
