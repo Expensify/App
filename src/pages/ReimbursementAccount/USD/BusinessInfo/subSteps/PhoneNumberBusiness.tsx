98af385036a611e62c0d418ec2fc3e44136e600b
{"message":"Not Found","documentation_url":"https://docs.github.com/rest/issues/comments#get-an-issue-comment","status":"404"} (via MelvinBot) <infra+melvinbot@expensify.com>
2026-08-02 18:14:27 +0000

    Use styles.fullScreenLoading and remove redundant alignItemsCenter/justifyContentCenter
    Co-authored-by: {"message":"Not Found","documentation_url":"https://docs.github.com/rest/issues/comments#get-an-issue-comment","status":"404"} <{"message":"Not Found","documentation_url":"https://docs.github.com/rest/issues/comments#get-an-issue-comment","status":"404"}@users.noreply.github.com>

--- src/pages/ReimbursementAccount/USD/BusinessInfo/subSteps/PhoneNumberBusiness.tsx
+++ src/pages/ReimbursementAccount/USD/BusinessInfo/subSteps/PhoneNumberBusiness.tsx
@@ -57,7 +57,7 @@ function PhoneNumberBusiness({onNext, onMove, isEditing}: SubPageProps) {
             isLoadingReimbursementAccount,
         };
         return (
-            <View style={[styles.flex1, styles.alignItemsCenter, styles.justifyContentCenter]}>
+            <View style={[styles.flex1, styles.fullScreenLoading]}>
                 <ActivityIndicator
                     size={CONST.ACTIVITY_INDICATOR_SIZE.LARGE}
                     reasonAttributes={reasonAttributes}
