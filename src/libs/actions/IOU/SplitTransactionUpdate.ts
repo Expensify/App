5b7076242902e96fae28d709e98cc0861913c39c
yauhenihorbach <HorbachZhenja@yandex.by>
2026-08-03 18:01:01 +0200

    Pass distanceUnit to BE

--- src/libs/actions/IOU/SplitTransactionUpdate.ts
+++ src/libs/actions/IOU/SplitTransactionUpdate.ts
@@ -345,6 +345,7 @@ function updateSplitTransactions({
                 billable: split?.billable,
                 quantity: split.customUnit?.quantity ?? undefined,
                 customUnitRateID: split.customUnit?.customUnitRateID,
+                distanceUnit: split.customUnit?.distanceUnit,
                 odometerStart: split.odometerStart,
                 odometerEnd: split.odometerEnd,
                 waypoints: split.waypoints,
@@ -802,6 +803,7 @@ function updateSplitTransactions({
             const oldTransactionChanges = {
                 ...existing,
                 quantity: splitTransaction.comment?.customUnit?.quantity ?? existing?.distance,
+                distanceUnit: splitTransaction.comment?.customUnit?.distanceUnit,
             } as TransactionChanges;
 
             if (currentSplit) {
