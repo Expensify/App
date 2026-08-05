2d252a3e912b27b0cefaefcd9cbda51167c7f990
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-08-05 01:40:04 +0100

    lint

--- src/components/Search/SearchList/ListItem/ExportedIconCell.tsx
+++ src/components/Search/SearchList/ListItem/ExportedIconCell.tsx
@@ -144,7 +144,7 @@ function ExportedIconCell({reportActions}: ExportedIconCellProps) {
                 <Avatar
                     source={icons.DualEntrySquare}
                     type={CONST.ICON_TYPE_AVATAR}
-                    size={CONST.AVATAR_SIZE.MID_SUBSCRIPT}
+                    size={CONST.AVATAR_SIZE.XXX_SMALL}
                 />
             )}
             {isExportedToBillCom && (
