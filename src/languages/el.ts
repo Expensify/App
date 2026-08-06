977a743e1db7e3098b0e480dd4ddaef30fe05a15
lorretheboy <lorretheboy@outlook.com>
2026-08-06 15:21:49 +0700

    fix: copy

--- src/languages/el.ts
+++ src/languages/el.ts
@@ -9371,7 +9371,6 @@ ${reportName}`,
         exportedTo: 'Εξήχθη σε',
         exportAll: {
             selectAllMatchingItems: 'Επιλέξτε όλα τα στοιχεία που ταιριάζουν',
-            allMatchingItemsSelected: 'Έχουν επιλεγεί όλα τα στοιχεία που ταιριάζουν',
             selectAllOnThisPage: 'Επιλέξτε όλα σε αυτή τη σελίδα',
         },
         errors: {
