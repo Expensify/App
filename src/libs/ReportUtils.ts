2ea5fe712b6f5c2bbdc804faee0f0fab8db8f93b
Krishna (via MelvinBot) <infra+melvinbot@expensify.com>
2026-08-06 17:27:09 +0000

    Fix invoice 'marked as paid' copy diverging from displayed message
    Co-authored-by: Krishna <Krishna2323@users.noreply.github.com>

--- src/libs/ReportUtils.ts
+++ src/libs/ReportUtils.ts
@@ -11027,7 +11027,7 @@ function getIOUReportActionDisplayMessage(
 
         switch (originalMessage.paymentType) {
             case CONST.IOU.PAYMENT_TYPE.ELSEWHERE:
-                translationKey = hasMissingInvoiceBankAccount(IOUReportID) ? 'iou.payerSettledWithMissingBankAccount' : 'iou.paidElsewhere';
+                translationKey = 'iou.paidElsewhere';
                 break;
             case CONST.IOU.PAYMENT_TYPE.EXPENSIFY:
             case CONST.IOU.PAYMENT_TYPE.VBBA:
@@ -11054,9 +11054,6 @@ function getIOUReportActionDisplayMessage(
         }
         if (translationKey === 'iou.paidElsewhere') {
             return getElsewherePaymentReportActionMessage(translate, originalMessage);
-        }
-        if (translationKey === 'iou.payerSettledWithMissingBankAccount') {
-            return translate(translationKey, '');
         }
     }
 
