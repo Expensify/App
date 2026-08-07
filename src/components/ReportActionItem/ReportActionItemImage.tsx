4322983a6e30e8a38f374c4dc487e4137b616066
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-08-07 16:52:10 +0800

    only use original image when shouldUseThumbnailImage is false

--- src/components/ReportActionItem/ReportActionItemImage.tsx
+++ src/components/ReportActionItem/ReportActionItemImage.tsx
@@ -191,7 +191,7 @@ function ReportActionItemImage({
         propsObj = {
             shouldUseThumbnailImage: shouldUseThumbnailImage ?? true,
 
-            source: originalImageSource,
+            source: shouldUseThumbnailImage ? thumbnailSource : originalImageSource,
             fallbackIcon: icons.Receipt,
             fallbackIconSize: isSingleImage ? variables.iconSizeSuperLarge : variables.iconSizeExtraLarge,
             isAuthTokenRequired: true,
