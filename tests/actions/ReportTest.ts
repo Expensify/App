1f1e0f6b4a6f31648a8becee80a478e227b2be83
Maruf Sharifi <maruf.sharifi.work@gmail.com>
2026-08-08 16:29:30 +0430

    fixed lint failure action

--- tests/actions/ReportTest.ts
+++ tests/actions/ReportTest.ts
@@ -45,6 +45,8 @@ import {toZonedTime} from 'date-fns-tz';
 import Onyx from 'react-native-onyx';
 import OnyxUtils from 'react-native-onyx/dist/OnyxUtils';
 
+import type {MockFetch} from '../utils/TestHelper';
+
 import createCollection from '../utils/collections/createCollection';
 import createRandomPolicy from '../utils/collections/policies';
 import createRandomReportAction from '../utils/collections/reportActions';
@@ -54,7 +56,7 @@ import createMock from '../utils/createMock';
 import getIsUsingFakeTimers from '../utils/getIsUsingFakeTimers';
 import getOnyxValue from '../utils/getOnyxValue';
 import PusherHelper from '../utils/PusherHelper';
-import {MockFetch, formatPhoneNumber} from '../utils/TestHelper';
+import {formatPhoneNumber} from '../utils/TestHelper';
 import * as TestHelper from '../utils/TestHelper';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
 import waitForNetworkPromises from '../utils/waitForNetworkPromises';
