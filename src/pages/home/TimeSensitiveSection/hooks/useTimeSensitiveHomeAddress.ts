a045ce32f89e360171423e17061bf2d093935de6
alberto <alberto@expensify.com>
2026-08-10 19:27:28 +0200

    better performance

--- src/pages/home/TimeSensitiveSection/hooks/useTimeSensitiveHomeAddress.ts
+++ src/pages/home/TimeSensitiveSection/hooks/useTimeSensitiveHomeAddress.ts
@@ -2,6 +2,16 @@ import useOnyx from '@hooks/useOnyx';
 
 import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {Policy, PrivatePersonalDetails} from '@src/types/onyx';
+
+import type {OnyxCollection, OnyxEntry} from 'react-native-onyx';
+
+const hasHomeAndOfficeWorkspaceSelector = (policies: OnyxCollection<Policy>) =>
+    Object.values(policies ?? {}).some(
+        (policy) => policy?.commuterExclusions?.method === CONST.POLICY.COMMUTER_EXCLUSION_METHOD.HOME_AND_OFFICE && policy?.pendingAction !== CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE,
+    );
+
+const hasHomeAddressSelector = (privatePersonalDetails: OnyxEntry<PrivatePersonalDetails>) => (privatePersonalDetails?.addresses ?? []).some((address) => !!address?.street?.trim());
 
 /**
  * Surfaces the "Add a home address" item under Home > Time sensitive when the user belongs to at
@@ -9,17 +19,11 @@ import ONYXKEYS from '@src/ONYXKEYS';
  * no home address recorded in their private personal details.
  */
 function useTimeSensitiveHomeAddress() {
-    const [allPolicies] = useOnyx(ONYXKEYS.COLLECTION.POLICY);
-    const [privatePersonalDetails] = useOnyx(ONYXKEYS.PRIVATE_PERSONAL_DETAILS);
-
-    const hasHomeAndOfficeWorkspace = Object.values(allPolicies ?? {}).some(
-        (policy) => policy?.commuterExclusions?.method === CONST.POLICY.COMMUTER_EXCLUSION_METHOD.HOME_AND_OFFICE && policy?.pendingAction !== CONST.RED_BRICK_ROAD_PENDING_ACTION.DELETE,
-    );
-
-    const hasHomeAddress = (privatePersonalDetails?.addresses ?? []).some((address) => !!address?.street?.trim());
+    const [hasHomeAndOfficeWorkspace] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: hasHomeAndOfficeWorkspaceSelector});
+    const [hasHomeAddress] = useOnyx(ONYXKEYS.PRIVATE_PERSONAL_DETAILS, {selector: hasHomeAddressSelector});
 
     return {
-        shouldShowAddHomeAddress: hasHomeAndOfficeWorkspace && !hasHomeAddress,
+        shouldShowAddHomeAddress: !!hasHomeAndOfficeWorkspace && !hasHomeAddress,
     };
 }
 
