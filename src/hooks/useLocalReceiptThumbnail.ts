a586d88134f129bb8f9486ffeee5ef4362564292
VickyStash <vikstash@gmail.com>
2026-08-12 09:52:15 +0200

    Clean up

--- src/hooks/useLocalReceiptThumbnail.ts
+++ src/hooks/useLocalReceiptThumbnail.ts
@@ -1,8 +1,4 @@
-import {endSpan, getSpan, startSpan} from '@libs/telemetry/activeSpans';
-
 import {generateThumbnail} from '@pages/iou/request/step/IOURequestStepScan/cropImageToAspectRatio';
-
-import CONST from '@src/CONST';
 
 import {useEffect, useRef, useState, useTransition} from 'react';
 
