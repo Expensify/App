da3048f8ccee8b658b19ed52e8e5468eed16057e
krishna2323 <belivethatkg@gmail.com>
2026-08-12 16:48:23 +0530

    add margin bottom to custom tag name row.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/pages/workspace/tags/DynamicWorkspaceViewTagsPage.tsx
+++ src/pages/workspace/tags/DynamicWorkspaceViewTagsPage.tsx
@@ -425,6 +425,7 @@ function DynamicWorkspaceViewTagsPage({route}: DynamicWorkspaceViewTagsProps) {
                         onPress={navigateToEditTag}
                         shouldShowRightIcon={canWriteTags}
                         interactive={canWriteTags}
+                        wrapperStyle={styles.mb5}
                     />
                 </OfflineWithFeedback>
                 {isLoading && (
