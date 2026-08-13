c68accbb4e7d4b1b219c9661227ce52d2d7e6de3
dominictb <tb-dominic@outlook.com>
2026-08-13 19:03:46 +0700

    Handle success case

--- src/pages/settings/Wallet/LinkPlaidToBankAccount/LinkPlaidToBankAccountPage.tsx
+++ src/pages/settings/Wallet/LinkPlaidToBankAccount/LinkPlaidToBankAccountPage.tsx
@@ -1,18 +1,26 @@
 import ActivityIndicator from '@components/ActivityIndicator';
 import FullPageOfflineBlockingView from '@components/BlockingViews/FullPageOfflineBlockingView';
+import Button from '@components/Button';
+import ConfirmationPage from '@components/ConfirmationPage';
+import FixedFooter from '@components/FixedFooter';
 import HeaderWithBackButton from '@components/HeaderWithBackButton';
 import PlaidLink from '@components/PlaidLink';
+import RadioButtons from '@components/RadioButtons';
+import RenderHTML from '@components/RenderHTML';
 import ScreenWrapper from '@components/ScreenWrapper';
+import ScrollView from '@components/ScrollView';
 import Text from '@components/Text';
 
 import useBeforeRemove from '@hooks/useBeforeRemove';
 import useDynamicBackPath from '@hooks/useDynamicBackPath';
+import {useMemoizedLazyIllustrations} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 
 import {clearLinkPlaidBankAccountErrors, clearPlaid, linkPlaidToBankAccount} from '@libs/actions/BankAccounts';
 import {openPlaidBankLogin} from '@libs/actions/Plaid';
+import {hasBrokenPlaidConnection, isConnectedViaPlaid} from '@libs/BankAccountUtils';
 import Log from '@libs/Log';
 
 import Navigation from '@navigation/Navigation';
@@ -28,7 +36,7 @@ import type SCREENS from '@src/SCREENS';
 import type {LinkAccount} from 'react-native-plaid-link-sdk';
 import type {PlaidAccount} from 'react-plaid-link';
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 
 type LinkPlaidToBankAccountPageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.SETTINGS.WALLET.DYNAMIC_BANK_ACCOUNT_LINK_PLAID>;
@@ -46,6 +54,7 @@ type LinkPlaidToBankAccountInnerProps = {
 function LinkPlaidToBankAccountInner({bankAccountID, backPath}: LinkPlaidToBankAccountInnerProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
+    const illustrations = useMemoizedLazyIllustrations(['Fireworks', 'BrokenMagnifyingGlass']);
 
     const [plaidLinkToken] = useOnyx(ONYXKEYS.RAM_ONLY_PLAID_LINK_TOKEN);
     const [isPlaidDisabled] = useOnyx(ONYXKEYS.IS_PLAID_DISABLED);
@@ -53,6 +62,16 @@ function LinkPlaidToBankAccountInner({bankAccountID, backPath}: LinkPlaidToBankA
 
     const policyID = bankAccountList?.[bankAccountID]?.accountData?.additionalData?.policyID;
     const isLoading = !!bankAccountList?.[bankAccountID]?.isLoading;
+    const bankAccountErrorMessages = Object.values(bankAccountList?.[bankAccountID]?.errors ?? {}).filter((message): message is string => !!message);
+    const hasError = bankAccountErrorMessages.length > 0;
+
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const isSuccess =
+        hasSubmitted &&
+        !isLoading &&
+        !hasError &&
+        isConnectedViaPlaid(bankAccountList?.[bankAccountID]?.accountData) &&
+        !hasBrokenPlaidConnection(bankAccountList?.[bankAccountID]?.accountData);
 
     useEffect(() => {
         openPlaidBankLogin(false, bankAccountID);
@@ -64,12 +83,29 @@ function LinkPlaidToBankAccountInner({bankAccountID, backPath}: LinkPlaidToBankA
     });
 
     const submit = (publicToken: string, account: PlaidLinkAccount | undefined) => {
+        setHasSubmitted(true);
         linkPlaidToBankAccount(bankAccountID, publicToken, account?.mask, policyID);
     };
 
     const handlePlaidSuccess = ({publicToken, accounts}: {publicToken: string; accounts: PlaidLinkAccount[]}) => {
         submit(publicToken, accounts.at(0));
     };
+
+    if (isSuccess) {
+        return (
+            <ScrollView contentContainerStyle={styles.flexGrow1}>
+                <ConfirmationPage
+                    heading={translate('walletPage.linkPlaid.successHeading')}
+                    description={translate('walletPage.linkPlaid.successDescription')}
+                    illustration={illustrations.Fireworks}
+                    shouldShowButton
+                    onButtonPress={() => Navigation.goBack(backPath)}
+                    buttonText={translate('common.buttonConfirm')}
+                    containerStyle={styles.h100}
+                />
+            </ScrollView>
+        );
+    }
 
     if (isPlaidDisabled) {
         return (
