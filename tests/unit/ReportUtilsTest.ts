91fe1fd874cf785f202a95a04e38ab3fc1f36081
ShridharGoel <35566748+ShridharGoel@users.noreply.github.com>
2026-08-16 12:32:05 +0530

    Remove report outstanding NVP fallback

--- tests/unit/ReportUtilsTest.ts
+++ tests/unit/ReportUtilsTest.ts
@@ -10215,7 +10215,7 @@ describe('ReportUtils', () => {
                     login: currentUserEmail,
                 },
             });
-            expect(isReportOutstanding(report, policy.id)).toBe(true);
+            expect(isReportOutstanding(report, policy.id, undefined)).toBe(true);
         });
         it('should return false for submitted reports if we specify it', () => {
             const report: Report = {
@@ -10254,7 +10254,7 @@ describe('ReportUtils', () => {
                 },
             });
             await Onyx.set(`${ONYXKEYS.COLLECTION.REPORT}${activeReport.reportID}`, activeReport);
-            expect(isReportOutstanding(report, policy.id)).toBe(true);
+            expect(isReportOutstanding(report, policy.id, undefined)).toBe(true);
         });
         it('should return false for archived report', async () => {
             const report: Report = {
