fe2c572ecadc58ab756ce3a3a56ec9228278b3dd
Chuck Dries <chuck@expensify.com>
2026-08-17 10:27:50 -0700

    add period at end of second sentence

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -7407,7 +7407,7 @@ const translations = {
             workspaceCurrency: 'Workspace currency',
             updateCurrencyPrompt: 'It looks like your workspace is currently set to a different currency than USD. Please click the button below to update your currency to USD now.',
             updateCurrencyForExpensifyCard:
-                'The Expensify Card is available to issue in USD, GBP, and EUR. Please update this workspace to a supported currency or use a different workspace',
+                'The Expensify Card is available to issue in USD, GBP, and EUR. Please update this workspace to a supported currency or use a different workspace.',
             updateCurrencyForExpensifyCardTitle: 'Get the Expensify Card',
             updateToUSD: 'Update to USD',
             updateWorkspaceCurrency: 'Update workspace currency',
