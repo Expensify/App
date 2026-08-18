1c43df2cf0023a0cab0119225bd9447c52260d7d
KJ21-ENG <140263938+KJ21-ENG@users.noreply.github.com>
2026-08-18 14:53:40 +0530

    Remove partitioned BaseTextInput cleanup

--- src/components/TextInput/BaseTextInput/implementations.ts
+++ src/components/TextInput/BaseTextInput/implementations.ts
@@ -2,14 +2,14 @@ import RNMarkdownTextInput from '@components/RNMarkdownTextInput';
 import RNMaskedTextInput from '@components/RNMaskedTextInput';
 import RNTextInput from '@components/RNTextInput';
 
-import type {InputType} from './types';
+import type {BaseTextInputProps, InputType} from './types';
 
-type InputComponentType = typeof RNTextInput | typeof RNMaskedTextInput | typeof RNMarkdownTextInput;
+type InputComponentType = React.ComponentType<BaseTextInputProps>;
 
 const InputComponentMap = new Map<InputType, InputComponentType>([
-    ['default', RNTextInput],
-    ['mask', RNMaskedTextInput],
-    ['markdown', RNMarkdownTextInput],
+    ['default', RNTextInput as InputComponentType],
+    ['mask', RNMaskedTextInput as InputComponentType],
+    ['markdown', RNMarkdownTextInput as InputComponentType],
 ]);
 
 export default InputComponentMap;
