f8ea16d3690a27ca579adf114b797b2a21daead9
Wojciech Boman <wojciechboman@gmail.com>
2026-08-21 10:16:39 +0200

    chore: temporary src touch to trigger Reassure workflow

--- src/types/utils/Nullable.ts
+++ src/types/utils/Nullable.ts
@@ -1,3 +1,5 @@
 type Nullable<T> = {[K in keyof T]: T[K] | null};
 
 export default Nullable;
+
+// TEMPORARY: touches src/ so the Reassure workflow is not skipped by paths-ignore. Revert before review.
