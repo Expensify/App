83ef10b43b8baca4e713810ba5755561e80b64bd
GCyganek <gabrielcyganek@gmail.com>
2026-08-25 16:11:17 +0200

    Fix spell check

--- src/pages/settings/Agents/const/index.ios.ts
+++ src/pages/settings/Agents/const/index.ios.ts
@@ -1,6 +1,6 @@
 import getPromptMaxHeightOnKeyboardOpenLandscapeMode from './getPromptMaxHeightOnKeyboardOpenLandscapeMode';
 
-// The iOS keyboard is smaller than the Android keyboard, so we want to show one more line comapred to the Android version.
+// The iOS keyboard is smaller than the Android keyboard, so we want to show one more line compared to the Android version.
 const MAX_PROMPT_LINES_WITH_KEYBOARD = 3;
 const PROMPT_MAX_HEIGHT_ON_KEYBOARD_OPEN_LANDSCAPE_MODE = getPromptMaxHeightOnKeyboardOpenLandscapeMode(MAX_PROMPT_LINES_WITH_KEYBOARD);
 
