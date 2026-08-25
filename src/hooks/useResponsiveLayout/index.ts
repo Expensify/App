a48c9777efac1c9bbf173081fc075f923e9a812e
GCyganek <gabrielcyganek@gmail.com>
2026-08-25 14:03:55 +0200

    Fix isMediumScreenWidth check in landscapem ode

--- src/hooks/useResponsiveLayout/index.ts
+++ src/hooks/useResponsiveLayout/index.ts
@@ -39,7 +39,8 @@ export default function useResponsiveLayout(): ResponsiveLayoutResult {
     const screenHeight = Dimensions.get('screen').height;
     const isExtraSmallScreenHeight = screenHeight <= variables.extraSmallMobileResponsiveHeightBreakpoint;
     const isSmallScreenWidth = windowWidth <= variables.mobileResponsiveWidthBreakpoint || isInLandscapeMode;
-    const isMediumScreenWidth = windowWidth > variables.mobileResponsiveWidthBreakpoint && windowWidth <= variables.tabletResponsiveWidthBreakpoint;
+    const isMediumScreenWidth = windowWidth > variables.mobileResponsiveWidthBreakpoint && windowWidth <= variables.tabletResponsiveWidthBreakpoint && !isInLandscapeMode;
+    console.log({isMediumScreenWidth, isSmallScreenWidth, isInLandscapeMode});
     const onboardingIsMediumOrLargerScreenWidth = !isInLandscapeMode && windowWidth > variables.mobileResponsiveWidthBreakpoint;
     const isLargeScreenWidth = windowWidth > variables.tabletResponsiveWidthBreakpoint;
     const isExtraLargeScreenWidth = windowWidth > variables.sidePanelResponsiveWidthBreakpoint;
