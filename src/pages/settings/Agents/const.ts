1c4fecfe610b6f802c4565b0f185eecafef31db8
GCyganek <gabrielcyganek@gmail.com>
2026-08-25 16:01:43 +0200

    Show 3 lines on iOS

--- src/pages/settings/Agents/const.ts
+++
@@ -1,7 +0,0 @@
-import variables from '@styles/variables';
-
-const MAX_PROMPT_LINES_WITH_KEYBOARD = 2;
-const PROMPT_MAX_HEIGHT_ON_KEYBOARD_OPEN_LANDSCAPE_MODE = variables.componentSizeLarge + variables.lineHeightXLarge * (MAX_PROMPT_LINES_WITH_KEYBOARD - 1);
-
-// eslint-disable-next-line import/prefer-default-export
-export {PROMPT_MAX_HEIGHT_ON_KEYBOARD_OPEN_LANDSCAPE_MODE};
---
+++ src/pages/settings/Agents/const/index.ios.ts
@@ -0,0 +1,8 @@
+import getPromptMaxHeightOnKeyboardOpenLandscapeMode from './getPromptMaxHeightOnKeyboardOpenLandscapeMode';
+
+// The iOS keyboard is smaller than the Android keyboard, so we want to show one more line comapred to the Android version.
+const MAX_PROMPT_LINES_WITH_KEYBOARD = 3;
+const PROMPT_MAX_HEIGHT_ON_KEYBOARD_OPEN_LANDSCAPE_MODE = getPromptMaxHeightOnKeyboardOpenLandscapeMode(MAX_PROMPT_LINES_WITH_KEYBOARD);
+
+// eslint-disable-next-line import/prefer-default-export
+export {PROMPT_MAX_HEIGHT_ON_KEYBOARD_OPEN_LANDSCAPE_MODE};
