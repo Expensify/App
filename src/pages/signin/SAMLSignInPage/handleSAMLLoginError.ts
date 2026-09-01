144d6f8dfb7de3b3540bc2220ed49cd48da53480
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-09-01 14:08:00 +0200

    Move handleSAMLLoginError out of LoginUtils to break import cycles

---
+++ src/pages/signin/SAMLSignInPage/handleSAMLLoginError.ts
@@ -0,0 +1,19 @@
+import Navigation from "@libs/Navigation/Navigation";
+
+import { clearSignInData, setAccountError } from "@userActions/Session";
+
+import ROUTES from "@src/ROUTES";
+
+function handleSAMLLoginError(
+  errorMessage: string,
+  shouldClearSignInData: boolean,
+) {
+  if (shouldClearSignInData) {
+    clearSignInData();
+  }
+
+  setAccountError(errorMessage);
+  Navigation.goBack(ROUTES.INBOX);
+}
+
+export default handleSAMLLoginError;
