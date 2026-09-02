ed346a3ab3bd88861f8118f0f4e30f6e3d4072c0
“arekm213” <arek.mrowczyk@gmail.com>
2026-09-02 14:55:01 +0200

    Drop the second production gate in usePermissions

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -3,13 +3,8 @@ import {BetaConfigurationContext, BetasContext, BetaOverridesContext} from '@com
 import Permissions from '@libs/Permissions';
 
 import type Beta from '@src/types/onyx/Beta';
-import type BetaOverrides from '@src/types/onyx/BetaOverrides';
 
 import {useContext, useMemo} from 'react';
-
-import useEnvironment from './useEnvironment';
-
-const NO_BETA_OVERRIDES: BetaOverrides = {};
 
 type PermissionKey = keyof typeof Permissions;
 type UsePermissions = Partial<Record<Exclude<PermissionKey, 'isBetaEnabled'>, boolean>> & {isBetaEnabled: (beta: Beta) => boolean};
@@ -19,10 +14,9 @@ export default function usePermissions(): UsePermissions {
     const betas = useContext(BetasContext);
     const betaConfiguration = useContext(BetaConfigurationContext);
     const betaOverrides = useContext(BetaOverridesContext);
-    const {isProduction} = useEnvironment();
     return useMemo(() => {
         const permissions: UsePermissions = {
-            isBetaEnabled: (beta: Beta) => Permissions.isBetaEnabled(beta, betas, betaConfiguration, isProduction ? NO_BETA_OVERRIDES : betaOverrides),
+            isBetaEnabled: (beta: Beta) => Permissions.isBetaEnabled(beta, betas, betaConfiguration, betaOverrides),
         };
 
         for (permissionKey in Permissions) {
@@ -33,5 +27,5 @@ export default function usePermissions(): UsePermissions {
         }
 
         return permissions;
-    }, [betas, betaConfiguration, betaOverrides, isProduction]);
+    }, [betas, betaConfiguration, betaOverrides]);
 }
