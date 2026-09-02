eac073d37da3fc3ed72dd3a799dafcd12932c8a3
Youssef Lourayad <youssef@expensify.com>
2026-09-02 17:32:21 +0000

    Render the Spend loading skeleton as a plain View

--- src/components/Search/SearchLoadingSkeleton.tsx
+++ src/components/Search/SearchLoadingSkeleton.tsx
@@ -1,7 +1,5 @@
 import SearchRowSkeleton from '@components/Skeletons/SearchRowSkeleton';
 
-import useThemeStyles from '@hooks/useThemeStyles';
-
 import {endSpanWithAttributes} from '@libs/telemetry/activeSpans';
 import {endNavigateToReportsFirstPaint} from '@libs/telemetry/navigateToReportsSpans';
 
@@ -10,21 +8,18 @@ import CONST from '@src/CONST';
 import type {StyleProp, ViewStyle} from 'react-native';
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import Animated from 'react-native-reanimated';
+import {StyleSheet, View} from 'react-native';
 
 type SearchLoadingSkeletonProps = {
     containerStyle?: StyleProp<ViewStyle>;
 };
 
 function SearchLoadingSkeleton({containerStyle}: SearchLoadingSkeletonProps) {
-    const styles = useThemeStyles();
-
     return (
         // Absolutely filled so it never shares the parent's column layout with the incoming results, which would
         // halve both heights for a frame.
-        <Animated.View
-            style={[styles.flex1, StyleSheet.absoluteFill]}
+        <View
+            style={StyleSheet.absoluteFill}
             onLayout={() => {
                 endSpanWithAttributes(CONST.TELEMETRY.SPAN_NAVIGATE_TO_REPORTS, {[CONST.TELEMETRY.ATTRIBUTE_IS_WARM]: false});
                 endNavigateToReportsFirstPaint(CONST.TELEMETRY.NAVIGATE_TO_REPORTS_START_TYPE.COLD);
@@ -34,7 +29,7 @@ function SearchLoadingSkeleton({containerStyle}: SearchLoadingSkeletonProps) {
                 shouldAnimate
                 containerStyle={containerStyle}
             />
-        </Animated.View>
+        </View>
     );
 }
 
