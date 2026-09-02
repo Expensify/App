d0d776ba4c58eb984098c2e381df8040f746de1a
Wiktor Gut <wiktor.gut@swmansion.com>
2026-09-02 17:43:08 +0200

    remove redundant prop from ButtonDisabledWhenOffline

--- src/components/ButtonComposed/composed/ButtonDisabledWhenOffline.tsx
+++ src/components/ButtonComposed/composed/ButtonDisabledWhenOffline.tsx
@@ -1,21 +1,15 @@
 import useNetwork from '@hooks/useNetwork';
 
-import React from 'react';
-
 import type {ButtonProps} from '..';
 
 import Button from '..';
 
-type ButtonDisabledWhenOfflineProps = ButtonProps & {
-    disabledWhenOffline?: boolean;
-};
-
-function ButtonDisabledWhenOffline({disabledWhenOffline = true, ...props}: ButtonDisabledWhenOfflineProps) {
+function ButtonDisabledWhenOffline({...props}: ButtonProps) {
     const {isOffline} = useNetwork();
     return (
         <Button
             {...props}
-            isDisabled={disabledWhenOffline && isOffline}
+            isDisabled={isOffline}
         />
     );
 }
