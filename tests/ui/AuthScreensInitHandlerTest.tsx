f6d339313b2722ed51ce811d56779bbbf7f801bc
Tomasz Lesniakiewicz <tlesniakiewicz@gmail.com>
2026-09-04 18:35:08 +0200

    test: fix tests asserts

--- tests/ui/AuthScreensInitHandlerTest.tsx
+++ tests/ui/AuthScreensInitHandlerTest.tsx
@@ -226,7 +226,7 @@ describe('AuthScreensInitHandler', () => {
         renderAuthScreensInitHandler();
         await waitForBatchedUpdatesWithAct();
 
-        expect(signOutAndRedirectToSignIn).toHaveBeenCalledWith(false, false);
+        expect(signOutAndRedirectToSignIn).toHaveBeenCalledWith(false, false, true, undefined, CONST.SIGN_OUT_REASON.LOGIN_AS_NEW_USER);
     });
 
     it('calls openApp when didUserLogInDuringSession returns true', async () => {
