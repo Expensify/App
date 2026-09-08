fbf685dfd86bd8e13fcc461777e62dec83aaa046
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2026-09-08 12:26:21 +0100

    add CampfireDefaultCompanyCardVendorPage

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -6135,6 +6135,8 @@ const translations = {
                 label: 'Company card account',
                 description: 'Choose where to export company card transactions.',
             },
+            noVendorsFound: 'No vendors found',
+            noVendorsFoundDescription: 'Please add vendors in Campfire and sync the connection again',
         },
         type: {
             free: 'Free',
