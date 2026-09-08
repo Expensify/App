8e0b01faab1ef0cbaffed6fa3e3aac448f2cbb75
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-09-08 17:05:20 +0800

    oops, revert unintended change

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -17,7 +17,6 @@ function canUseLinkPreviews(): boolean {
 }
 
 function isBetaEnabled(beta: Beta, betas: OnyxEntry<Beta[]>, betaConfiguration?: OnyxEntry<BetaConfiguration>): boolean {
-    return true;
     const hasAllBetasEnabled = canUseAllBetas(betas);
     const isFeatureEnabled = !!betas?.includes(beta);
 
