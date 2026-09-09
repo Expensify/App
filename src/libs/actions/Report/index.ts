c64fd18fe0a9bf37deaa48453f87feb3e56b1eae
daledah <daleda261@outlook.com>
2026-09-09 13:55:38 +0700

    address comment

--- src/libs/actions/Report/index.ts
+++ src/libs/actions/Report/index.ts
@@ -4324,6 +4324,7 @@ function updateWriteCapability(report: Report, newValue: WriteCapability) {
  * Navigates to the 1:1 report with Concierge
  */
 // eslint-disable-next-line @typescript-eslint/max-params
+// TODO: Refactor to use options object parameter to reduce parameter count https://github.com/Expensify/App/issues/66421
 function navigateToConciergeChat(
     conciergeReportID: string | undefined,
     introSelected: OnyxEntry<IntroSelected>,
