fa0a4326d3823c004aee4a2fcce6d51479df35d4
krishna2323 <belivethatkg@gmail.com>
2026-09-09 23:35:33 +0530

    update translation.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -8315,8 +8315,7 @@ const translations = {
                 thenApplyFollowingDefaults: 'Then apply the following defaults:',
                 confirmErrorCategory: 'Please select a category',
                 confirmErrorCategoryTax: 'Please select a tax rate',
-                confirmErrorCategoryTaxMoveIsWorkspaceDefault:
-                    "The selected tax rate is now your workspace default, so this rule can't move to another category. Choose a different tax rate.",
+                confirmErrorCategoryTaxMoveIsWorkspaceDefault: 'The selected tax rate is now your workspace default, so this rule is no longer valid. Choose a different tax rate.',
                 confirmErrorCondition: 'Please enter a merchant',
                 confirmErrorConditionAndDefault: 'Enter a merchant, and apply at least one default',
                 turnOnTaxesFirstTitle: 'Turn on taxes first',
