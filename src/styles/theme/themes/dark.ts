dac0ceb9e55cdd729f17cd83d715b94424dfeee0
Shawn Borton <shawn@expensify.com>
2026-09-09 07:51:24 +0200

    Soften the bottom tab bar shadow

--- src/styles/theme/themes/dark.ts
+++ src/styles/theme/themes/dark.ts
@@ -51,7 +51,7 @@ const darkTheme = {
     overlay: colors.productDark300,
     inverse: colors.productDark900,
     shadow: '0px 4px 12px 0px rgba(2,18,4,0.24)',
-    shadowTop: '0px -6px 12px -2px rgba(2,18,4,0.24)',
+    shadowTop: '0px -6px 12px -2px rgba(2,18,4,0.16)',
     componentBG: colors.productDark100,
     hoverComponentBG: colors.productDark300,
     messageHighlightBG: colors.messageHighlightDark,
