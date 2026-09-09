0a2e60bf503d3e75f4f7dcc641a77c20c5fea153
dominictb <tb-dominic@outlook.com>
2026-09-10 02:32:28 +0700

    Add None option to NetSuite default vendor selector

--- src/pages/workspace/accounting/netsuite/export/DynamicNetSuiteExportExpensesVendorSelectPage.tsx
+++ src/pages/workspace/accounting/netsuite/export/DynamicNetSuiteExportExpensesVendorSelectPage.tsx
@@ -41,7 +41,21 @@ function DynamicNetSuiteExportExpensesVendorSelectPage({policy}: WithPolicyConne
     const backPath = useDynamicBackPath(DYNAMIC_ROUTES.POLICY_ACCOUNTING_NETSUITE_EXPORT_EXPENSES_VENDOR_SELECT.path);
 
     const config = policy?.connections?.netsuite?.options.config;
-    const netsuiteVendorOptions = useMemo<SelectorType[]>(() => getNetSuiteVendorOptions(policy ?? undefined, config?.defaultVendor), [config?.defaultVendor, policy]);
+    const vendorOptions = useMemo<SelectorType[]>(() => getNetSuiteVendorOptions(policy ?? undefined, config?.defaultVendor), [config?.defaultVendor, policy]);
+    const clearOption: SelectorType = useMemo(
+        () => ({
+            value: '',
+            text: translate('common.none'),
+            keyForList: '',
+            isSelected: !config?.defaultVendor,
+        }),
+        [translate, config?.defaultVendor],
+    );
+    const shouldShowClearOption = !!config?.defaultVendor || vendorOptions.length > 0;
+    const netsuiteVendorOptions = useMemo<SelectorType[]>(
+        () => (shouldShowClearOption ? [clearOption, ...vendorOptions] : vendorOptions),
+        [shouldShowClearOption, clearOption, vendorOptions],
+    );
 
     const initiallyFocusedOptionKey = useMemo(() => netsuiteVendorOptions?.find((mode) => mode.isSelected)?.keyForList, [netsuiteVendorOptions]);
 
