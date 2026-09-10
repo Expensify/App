7054410567b9826010f3152d4874d5ebf2bd3ee1
AlexMartin503 <alexmartinyt92@gmail.com>
2026-09-09 19:14:14 -0600

    fix: remove unused import

--- src/pages/workspace/members/WorkspaceInviteMessageComponent.tsx
+++ src/pages/workspace/members/WorkspaceInviteMessageComponent.tsx
@@ -31,7 +31,6 @@ import {
     getDefaultApprover,
     getMemberAccountIDsForWorkspace,
     goBackFromInvalidPolicy,
-    hasDynamicExternalWorkflow,
     isControlPolicy,
     isSubmitPolicy,
     shouldHideDynamicExternalWorkflowPeople,
