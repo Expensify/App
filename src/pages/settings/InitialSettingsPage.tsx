52311aa79bd79064021db9a7086a182fe413aa65
mhawryluk <marcinhawrylukxx@gmail.com>
2026-09-10 18:20:50 +0200

    Don't show the Account button on the Account initial page on mobile

--- src/pages/settings/InitialSettingsPage.tsx
+++ src/pages/settings/InitialSettingsPage.tsx
@@ -147,7 +147,7 @@ function InitialSettingsPage({currentUserPersonalDetails}: InitialSettingsPagePr
                 breadcrumbLabel={translate('initialSettingsPage.account')}
                 shouldDisplaySearch={shouldUseNarrowLayout}
                 shouldDisplayHelpButton={shouldUseNarrowLayout}
-                shouldDisplayAccountButton={shouldUseNarrowLayout}
+                shouldDisplayAccountButton={false}
             />
             <ScrollView
                 ref={scrollViewRef}
