eef64e0f65324c92b65af7b0fa56f381e9ae1ff2
krishna2323 <belivethatkg@gmail.com>
2026-09-10 13:08:24 +0530

    fix failing tests.
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/libs/SearchUIUtils.ts
+++ src/libs/SearchUIUtils.ts
@@ -129,6 +129,7 @@ import {
     getSubmitToAccountID,
     getTagGLCode,
     isArchivedOrPendingDeletePolicy,
+    isControlPolicy,
     isGroupPolicy,
     isPaidGroupPolicy,
     isPolicyAdmin,
@@ -1215,7 +1216,7 @@ function getSuggestedSearchesVisibility(
         const isEligibleForTopCategoriesSuggestion = isGroupPolicyEligible && policy.areCategoriesEnabled === true;
         const isEligibleForTopMerchantsSuggestion = isGroupPolicyEligible;
         const isEligibleForViolationsBySubmitterSuggestion =
-            isGroupPolicyEligible &&
+            isControlPolicy(policy) &&
             (isAdmin || isAuditor) &&
             arePolicyRulesEnabled(policy, policy.id ? policyCategories?.[`${ONYXKEYS.COLLECTION.POLICY_CATEGORIES}${policy.id}`] : undefined) &&
             memberCount >= 2;
