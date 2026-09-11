2967435416fdcdf2d8cc00bc8ab068f0071e86e6
mhawryluk <marcinhawrylukxx@gmail.com>
2026-09-11 10:56:51 +0200

    Revert changes to the concierge anywhere icon

--- src/components/SidePanel/SidePanelButton/index.native.tsx
+++ src/components/SidePanel/SidePanelButton/index.native.tsx
@@ -3,14 +3,11 @@ import {PressableWithoutFeedback} from '@components/Pressable';
 import Tooltip from '@components/Tooltip';
 
 import useCurrentUserPersonalDetails from '@hooks/useCurrentUserPersonalDetails';
-import {useMemoizedLazyExpensifyIcons, useMemoizedLazyIllustrations} from '@hooks/useLazyAsset';
+import {useMemoizedLazyExpensifyIcons} from '@hooks/useLazyAsset';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
-import usePermissions from '@hooks/usePermissions';
 import useSidePanelState from '@hooks/useSidePanelState';
 import useThemeStyles from '@hooks/useThemeStyles';
-
-import variables from '@styles/variables';
 
 import {navigateToConciergeChat} from '@userActions/Report';
 
@@ -28,14 +25,10 @@ function SidePanelButton({style}: SidePanelButtonProps) {
     const {shouldHideHelpButton} = useSidePanelState();
     const {accountID: currentUserAccountID = CONST.DEFAULT_NUMBER_ID} = useCurrentUserPersonalDetails();
     const {ConciergeAvatar} = useMemoizedLazyExpensifyIcons(['ConciergeAvatar']);
-    const {Concierge} = useMemoizedLazyIllustrations(['Concierge']);
     const [conciergeReportID] = useOnyx(ONYXKEYS.CONCIERGE_REPORT_ID);
     const [introSelected] = useOnyx(ONYXKEYS.NVP_INTRO_SELECTED);
     const [isSelfTourViewed] = useOnyx(ONYXKEYS.NVP_ONBOARDING, {selector: hasSeenTourSelector});
     const [betas] = useOnyx(ONYXKEYS.BETAS);
-    const {isBetaEnabled} = usePermissions();
-
-    const shouldUseConciergeIllustration = isBetaEnabled(CONST.BETAS.INSIGHTS_PAGE);
 
     if (shouldHideHelpButton) {
         return null;
@@ -49,19 +42,11 @@ function SidePanelButton({style}: SidePanelButtonProps) {
                 style={[styles.flexRow, styles.touchableButtonImage, style]}
                 onPress={() => navigateToConciergeChat(conciergeReportID, introSelected, currentUserAccountID, isSelfTourViewed, betas)}
             >
-                {shouldUseConciergeIllustration ? (
-                    <Icon
-                        src={Concierge}
-                        width={variables.iconSizeExtraLarge}
-                        height={variables.iconSizeExtraLarge}
-                    />
-                ) : (
-                    <Icon
-                        src={ConciergeAvatar}
-                        width={variables.iconSizeXLarge}
-                        height={variables.iconSizeXLarge}
-                    />
-                )}
+                <Icon
+                    src={ConciergeAvatar}
+                    width={28}
+                    height={28}
+                />
             </PressableWithoutFeedback>
         </Tooltip>
     );
