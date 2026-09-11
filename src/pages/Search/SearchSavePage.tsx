025a7ba5aea8428845a99755363c241d35cd6a7f
Nabi <nabi.zarin.work@gmail.com>
2026-09-11 11:03:56 +0430

    fix/search-group-by-reports-error

--- src/pages/Search/SearchSavePage.tsx
+++ src/pages/Search/SearchSavePage.tsx
@@ -25,7 +25,7 @@ import useThemeStyles from '@hooks/useThemeStyles';
 import {saveSearch} from '@libs/actions/Search';
 import Navigation from '@libs/Navigation/Navigation';
 import {rand64} from '@libs/NumberUtils';
-import {getCustomColumnDefault, getSearchColumnTranslationKey, mapFiltersFormToLabelValueList, savedSearchIDToSearchKey} from '@libs/SearchUIUtils';
+import {getCustomColumnDefault, getSearchColumnTranslationKey, getValidGroupBy, mapFiltersFormToLabelValueList, savedSearchIDToSearchKey} from '@libs/SearchUIUtils';
 import type {SearchFilter} from '@libs/SearchUIUtils';
 import {getFieldRequiredErrors} from '@libs/ValidationUtils';
 
