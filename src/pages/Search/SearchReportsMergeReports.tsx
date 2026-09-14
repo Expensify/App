071de85bf03d7df5335546240de5b9b7b85134b4
dmkt9 <demoskratos9x@gmail.com>
2026-09-14 10:32:25 +0700

    rm temp test

--- src/pages/Search/SearchReportsMergeReports.tsx
+++ src/pages/Search/SearchReportsMergeReports.tsx
@@ -221,7 +221,7 @@ function SearchMergeReports() {
             testID="SearchMergeReports"
             includeSafeAreaPaddingBottom
         >
-            {(false && reportItems.length > 0) || isLoadingAllReports ? (
+            {reportItems.length > 0 || isLoadingAllReports ? (
                 <>
                     {!isLoadingAllReports && <Text style={[styles.ph5, styles.pb5, styles.textLabelSupporting]}>{translate('search.mergeReports.description')}</Text>}
                     <SelectionList
