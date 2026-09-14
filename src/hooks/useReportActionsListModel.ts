56522fb3e4e52b8a74850e74801ce3b200b5556f
Rushat Gabhane <rushatgabhane@gmail.com>
2026-09-14 06:05:48 +0200

    Unlock history toggle

--- src/hooks/useReportActionsListModel.ts
+++ src/hooks/useReportActionsListModel.ts
@@ -11,6 +11,7 @@ import {reportActionsListLoadingStateSelector} from '@src/selectors/ReportMetaDa
 
 import {useRoute} from '@react-navigation/native';
 
+import useConciergeAskState from './useConciergeAskState';
 import {useIsAppLoadPending} from './useInFlightRequests';
 import useLoadReportActions from './useLoadReportActions';
 import useNetworkWithOfflineStatus from './useNetworkWithOfflineStatus';
@@ -60,6 +61,7 @@ function useReportActionsListModel(reportID: string, isReportLoadPending: boolea
     const hasLoadingOlderReportActionsError = reportLoadingState?.hasLoadingOlderReportActionsError;
 
     const {sessionStartTime, showFullHistory: conciergeShowFullHistory, hadMessagesAtSessionStart: conciergeHadMessagesAtSessionStart} = useConciergeSessionState();
+    const {isAskConciergeChat} = useConciergeAskState(reportID);
     const {setShowFullHistory: setConciergeShowFullHistory, setHadMessagesAtSessionStart: setConciergeHadMessagesAtSessionStart} = useConciergeSessionActions();
     const isReportTransactionThread = isReportTransactionThreadUtil(report);
     const shouldBeAlignedToTop = shouldReportAlignToTop(report, parentReportAction);
@@ -98,7 +100,7 @@ function useReportActionsListModel(reportID: string, isReportLoadPending: boolea
         hasOlderActions,
         loadOlderChats,
         mainDMSessionStartTime: sessionStartTime,
-        conciergeShowFullHistory: conciergeShowFullHistory || !!reportActionIDFromRoute || !!report?.hasOutstandingChildTask,
+        conciergeShowFullHistory: conciergeShowFullHistory || !!reportActionIDFromRoute || (!isAskConciergeChat && !!report?.hasOutstandingChildTask),
         setConciergeShowFullHistory,
         conciergeHadMessagesAtSessionStart,
         setConciergeHadMessagesAtSessionStart,
