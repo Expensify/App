645cb8e6304f8b05a74083481da89c192247a315
Carlos Miceli <carlosmiceli@expensify.com>
2026-09-14 17:50:58 -0300

    Complete validation task before workspace lookup

--- src/pages/settings/DynamicVerifyAccountPage.tsx
+++ src/pages/settings/DynamicVerifyAccountPage.tsx
@@ -1,16 +1,19 @@
 import useDynamicBackPath from '@hooks/useDynamicBackPath';
 import useDynamicForwardPath from '@hooks/useDynamicForwardPath';
+import useOnboardingTaskInformation from '@hooks/useOnboardingTaskInformation';
 import useReturnToOriginReport from '@hooks/useReturnToOriginReport';
 
 import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
 import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
 
 import {getAccessiblePolicies} from '@userActions/Policy/Policy';
+import {completeTask} from '@userActions/Task';
 
+import CONST from '@src/CONST';
 import ROUTES, {DYNAMIC_ROUTES} from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
 
-import React from 'react';
+import React, {useCallback} from 'react';
 
 import VerifyAccountPageBase from './VerifyAccountPageBase';
 
@@ -21,6 +24,26 @@ function DynamicVerifyAccountPage({route}: DynamicVerifyAccountPageProps) {
     let forwardPath = useDynamicForwardPath(DYNAMIC_ROUTES.VERIFY_ACCOUNT.path);
     const isJoinWorkspaceTask = route.params?.isJoinWorkspaceTask === 'true';
     const returnToOriginReport = useReturnToOriginReport();
+    const {
+        taskReport: validateEmailTaskReport,
+        taskParentReport: validateEmailTaskParentReport,
+        parentReportAction: validateEmailTaskParentReportAction,
+    } = useOnboardingTaskInformation(CONST.ONBOARDING_TASK_TYPE.VALIDATE_EMAIL);
+
+    const handleJoinWorkspaceValidationSuccess = useCallback(() => {
+        completeTask(
+            validateEmailTaskReport,
+            validateEmailTaskParentReport?.hasOutstandingChildTask ?? false,
+            false,
+            validateEmailTaskParentReportAction,
+            undefined,
+            undefined,
+            true,
+            true,
+            CONST.ACCOUNT_ID.CONCIERGE,
+        );
+        getAccessiblePolicies();
+    }, [validateEmailTaskParentReport?.hasOutstandingChildTask, validateEmailTaskParentReportAction, validateEmailTaskReport]);
 
     if (backPath === ROUTES.SETTINGS_WALLET) {
         forwardPath = ROUTES.SETTINGS_ENABLE_PAYMENTS.getRoute();
@@ -35,7 +58,7 @@ function DynamicVerifyAccountPage({route}: DynamicVerifyAccountPageProps) {
             navigateBackTo={backPath}
             navigateForwardTo={forwardPath}
             handleClose={isJoinWorkspaceTask ? returnToOriginReport : undefined}
-            onValidationSuccess={isJoinWorkspaceTask ? getAccessiblePolicies : undefined}
+            onValidationSuccess={isJoinWorkspaceTask ? handleJoinWorkspaceValidationSuccess : undefined}
             shouldShowCloseButton={isJoinWorkspaceTask}
         />
     );
