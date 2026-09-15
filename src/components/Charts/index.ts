32de2e0e9f9ba705d8c2238f0b82dff50663e0d3
Olgierd <olgierd.smyka@swmansion.com>
2026-09-15 15:54:35 +0200

    Remove unused exports from src/components/Charts/index.ts in order to fix knip check

--- src/components/Charts/index.ts
+++ src/components/Charts/index.ts
@@ -4,4 +4,4 @@ import LineChart from './LineChart';
 import PieChart from './PieChart';
 
 export {BarChart, ChartHeader, LineChart, PieChart};
-export type {CartesianChartProps, ChartDataPoint, UnitPosition, UnitWithFallback} from './types';
+export type {CartesianChartProps, ChartDataPoint} from './types';
