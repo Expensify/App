960149857cbf2da4c901b32e09307a3613022a32
dukenv0307 <dukenv0307@gmail.com>
2026-09-16 17:09:47 +0700

    clear the assign card error when leaving the confirmation step

--- src/pages/workspace/companyCards/assignCard/ConfirmationStep.tsx
+++ src/pages/workspace/companyCards/assignCard/ConfirmationStep.tsx
@@ -95,6 +95,12 @@ function ConfirmationStep({route}: ConfirmationStepProps) {
         });
     }, [assignCard?.isAssignmentFinished]);
 
+    useEffect(() => {
+        return () => {
+            clearAssignCardErrorsAction();
+        };
+    }, []);
+
     const submit = () => {
         if (!policyID) {
             return;
