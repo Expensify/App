dc70458d97bba464b2b504593afda0322bd5e44d
dominictb <tb-dominic@outlook.com>
2026-09-17 00:28:19 +0700

    Revert initial focus on None option in Sage Intacct

--- src/pages/workspace/accounting/intacct/export/DynamicSageIntacctDefaultVendorPage.tsx
+++ src/pages/workspace/accounting/intacct/export/DynamicSageIntacctDefaultVendorPage.tsx
@@ -130,7 +130,7 @@ function DynamicSageIntacctDefaultVendorPage() {
             displayName="SageIntacctDefaultVendorPage"
             data={vendorSelectorOptions ?? []}
             onSelectRow={updateDefaultVendor}
-            initiallyFocusedOptionKey={shouldShowClearOption ? clearOption.keyForList : vendorSelectorOptions.find((mode) => mode.isSelected)?.keyForList}
+            initiallyFocusedOptionKey={vendorSelectorOptions.find((mode) => mode.isSelected)?.keyForList}
             headerContent={listHeaderComponent}
             onBackButtonPress={goBack}
             title="workspace.sageIntacct.defaultVendor"
