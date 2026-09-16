5f7d1bb0e50170135358a907b5298bcd9240e0ec
jakubstec <jakub.stec@swmansion.com>
2026-09-16 13:07:51 +0200

    fix: handle missing search clear buttons

--- src/pages/workspace/rules/AgentRules/AddAgentRuleSuggestionsTab.tsx
+++ src/pages/workspace/rules/AgentRules/AddAgentRuleSuggestionsTab.tsx
@@ -111,6 +111,7 @@ function AddAgentRuleSuggestionsTab({onSelectSuggestion}: AddAgentRuleSuggestion
                     accessibilityLabel={translate('workspace.rules.agentRules.findSuggestion')}
                     value={searchValue}
                     onChangeText={setSearchValue}
+                    shouldHideClearButton={!searchValue}
                     autoGrowHeight={false}
                     role={CONST.ROLE.SEARCHBOX}
                 />
