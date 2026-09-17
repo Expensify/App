7892d1477e327ce0057f3f248b348d1e933480f2
Michał Jasikowski <michal@expensify.com>
2026-09-17 11:44:19 +0200

    Remove cache test success color change

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -16,7 +16,6 @@ const colors: Record<string, Color> = {
     red: '#F25730',
     redHover: '#DE4822',
     redPressed: '#F57959',
-    bloodRed: '#880808',
     transparent: 'transparent',
 
     // Dark Mode Theme Colors
