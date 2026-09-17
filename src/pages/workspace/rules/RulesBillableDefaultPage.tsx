3c9ff33f211ef4a8fc45e9796d394a780fcc5737
Wiktor Gut <wiktor.gut@swmansion.com>
2026-09-18 00:35:10 +0200

    migrate 2 files from batch 1 with different methods

--- src/pages/workspace/rules/RulesBillableDefaultPage.tsx
+++ src/pages/workspace/rules/RulesBillableDefaultPage.tsx
@@ -1,4 +1,4 @@
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import HeaderWithBackButtonAndTitle from '@components/Header/composed/HeaderWithBackButtonAndTitle';
 import ScreenWrapper from '@components/ScreenWrapper';
 import SelectionList from '@components/SelectionList';
 import SingleSelectListItem from '@components/SelectionList/ListItem/SingleSelectListItem';
@@ -105,7 +105,7 @@ function RulesBillableDefaultPage({
                 shouldEnableMaxHeight
                 testID="RulesBillableDefaultPage"
             >
-                <HeaderWithBackButton
+                <HeaderWithBackButtonAndTitle
                     title={translate('workspace.rules.generalTab.billableExpenses')}
                     onBackButtonPress={() => Navigation.goBack()}
                 />
--- src/pages/workspace/rules/RulesProhibitedDefaultPage.tsx
+++ src/pages/workspace/rules/RulesProhibitedDefaultPage.tsx
@@ -1,6 +1,6 @@
 import Button from '@components/Button';
 import FixedFooter from '@components/FixedFooter';
-import HeaderWithBackButton from '@components/HeaderWithBackButton';
+import Header from '@components/Header';
 import OfflineWithFeedback from '@components/OfflineWithFeedback';
 import ScreenWrapper from '@components/ScreenWrapper';
 import ScrollView from '@components/ScrollView';
@@ -116,10 +116,10 @@ function RulesProhibitedDefaultPage({
                 shouldEnableMaxHeight
                 testID="RulesProhibitedDefaultPage"
             >
-                <HeaderWithBackButton
-                    title={translate('workspace.rules.generalTab.flagReceiptLineItems')}
-                    onBackButtonPress={() => Navigation.goBack()}
-                />
+                <Header>
+                    <Header.BackButton onPress={() => Navigation.goBack()} />
+                    <Header.Title title={translate('workspace.rules.generalTab.flagReceiptLineItems')} />
+                </Header>
                 <ScrollView
                     style={[styles.flexGrow1]}
                     contentContainerStyle={[styles.ph5, styles.pb5]}
