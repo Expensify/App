f7e02bbdbb80217bf441a4658b75429e230c2cc6
Rayane <77965000+rayane-d@users.noreply.github.com>
2026-09-18 11:28:46 +0100

    fix tests

--- tests/ui/SAMLSignInPageTest.tsx
+++ tests/ui/SAMLSignInPageTest.tsx
@@ -108,7 +108,7 @@ describe('SAMLSignInPage', () => {
         renderPage();
         await waitForBatchedUpdatesWithAct();
 
-        expect(signInWithShortLivedAuthToken).toHaveBeenCalledWith('token', true, '/search?q=status:outstanding');
+        expect(signInWithShortLivedAuthToken).toHaveBeenCalledWith('token', undefined, true, '/search?q=status:outstanding');
         expect(clearSignInData).not.toHaveBeenCalled();
     });
 
@@ -120,6 +120,6 @@ describe('SAMLSignInPage', () => {
         renderPage();
         await waitForBatchedUpdatesWithAct();
 
-        expect(signInWithShortLivedAuthToken).toHaveBeenCalledWith('token', true, '/search?q=status:outstanding');
+        expect(signInWithShortLivedAuthToken).toHaveBeenCalledWith('token', undefined, true, '/search?q=status:outstanding');
     });
 });
