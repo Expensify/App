571a66c9d97ade1ea442f6acb26aa8b8150fdfea
nkdengineer <nkdengineer@outlook.com>
2026-09-18 14:52:48 +0700

    update comment

--- src/libs/actions/Link.ts
+++ src/libs/actions/Link.ts
@@ -622,7 +622,7 @@ function openReportFromDeepLink(
                         }
 
                         const navigateHandler = (reportParam?: OnyxEntry<Report>) => {
-                            // Skip if the user already left the deeplinked route.
+                            // Skip if the user already is in the deeplinked route.
                             const deeplinkRoute = route as Route;
                             if (deeplinkRoute && Navigation.isActiveRoute(deeplinkRoute)) {
                                 return;
