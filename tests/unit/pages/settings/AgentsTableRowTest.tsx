5333f58f36d5a99451ae1364a6b9afa3d4b3bdcd
war-in <war-in@users.noreply.github.com>
2026-09-18 14:17:21 +0200

    test: adapt test to header-placement changes

--- tests/unit/pages/settings/AgentsTableRowTest.tsx
+++ tests/unit/pages/settings/AgentsTableRowTest.tsx
@@ -65,7 +65,7 @@ jest.mock('@components/Table/TableContext', () => ({
         isMobileSelectionEnabled: false,
         tableListMetadata: {
             hasPageHeader: false,
-            shouldRenderColumnHeaderAsStickyRow: false,
+            columnHeaderPlacement: 'none',
         },
     })),
     useTableRowSemanticID: jest.fn(() => undefined),
