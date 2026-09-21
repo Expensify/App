b66c3e1ab7a5af46f9e5adaa4354c4eb5aa49372
Carlos Miceli <carlosmiceli@expensify.com>
2026-09-21 17:27:22 -0300

    fix: require onboarding close handler

--- src/components/OnboardingHeader.tsx
+++ src/components/OnboardingHeader.tsx
@@ -54,7 +54,7 @@ function OnboardingHeader({onBackButtonPress, onCloseButtonPress, shouldShowBack
                     <Text style={styles.createMenuHeaderText}>{translate('common.back')}</Text>
                 </PressableWithoutFeedback>
             ) : null}
-            {shouldShowCloseButton ? (
+            {shouldShowCloseButton && onCloseButtonPress ? (
                 <View style={styles.mlAuto}>
                     <HeaderCloseButton onPress={onCloseButtonPress} />
                 </View>
