06aaeb5a082b4a35265230a531605d0bc2d3a44f
truph01 <truphd01@gmail.com>
2026-09-23 09:45:48 +0700

    fix: remove mock

--- src/libs/Permissions.ts
+++ src/libs/Permissions.ts
@@ -19,7 +19,6 @@ isProduction()
 
 // eslint-disable-next-line rulesdir/no-beta-handler
 function canUseAllBetas(betas: OnyxEntry<Beta[]>): boolean {
-    return true;
     return !!betas?.includes(CONST.BETAS.ALL);
 }
 
