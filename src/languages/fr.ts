df4d7816a828aecc052fe03225d551509b013222
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-09-23 09:49:39 +0800

    update translations

--- src/languages/fr.ts
+++ src/languages/fr.ts
@@ -8704,13 +8704,27 @@ Ajoutez davantage de règles de dépenses pour protéger la trésorerie de l’e
             approverFields: {recruiter: 'Recruteur', recruitingCoordinator: 'Coordinateur recrutement'},
             filters: {
                 description: (providerName: string) =>
-                    `Sélectionnez quels membres sont importés depuis ${providerName}. Vous pouvez choisir parmi les étapes de poste, les tags et les bureaux.`,
+                    `Sélectionnez quels membres sont importés depuis ${providerName}. Vous pouvez choisir selon les étapes de poste, les tags et les bureaux.`,
                 chooseAtLeastOneItem: 'Choisissez au moins un élément à importer',
-                stages: {title: 'Étape du poste', description: 'Choisissez l’étape de recrutement des candidats que vous souhaitez synchroniser avec cet espace de travail'},
-                tags: {title: 'Tag', description: 'Choisissez les tags des candidats que vous souhaitez synchroniser avec cet espace de travail'},
+                stages: {
+                    title: 'Étape du poste',
+                    description: 'Choisissez l’étape de recrutement des candidats que vous souhaitez synchroniser avec cet espace de travail',
+                    toggleTitle: 'Étapes de la tâche d’importation',
+                    allSelected: 'Tous les stades du poste',
+                    emptySelectionError: 'Veuillez sélectionner au moins une étape de travail',
+                },
+                tags: {
+                    title: 'Tag',
+                    description: 'Choisissez les tags de candidats que vous souhaitez synchroniser avec cet espace de travail',
+                    toggleTitle: 'Importer des tags',
+                    allSelected: 'Tous les tags',
+                    emptySelectionError: 'Veuillez sélectionner au moins un tag',
+                },
                 offices: {
                     title: 'Bureau',
                     description: 'Choisissez les bureaux des candidats que vous souhaitez synchroniser avec cet espace de travail',
+                    toggleTitle: 'Importer des bureaux (facultatif)',
+                    allSelected: 'Tous les bureaux',
                 },
             },
             subtitle: 'Connectez les outils de recrutement et synchronisez les validations de candidats.',
