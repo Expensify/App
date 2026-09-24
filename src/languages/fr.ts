315d9eaa1cd73055edc9aedb62083ad10025b246
allgandaf <whogandalf@gmail.com>
2026-09-24 14:07:56 +0530

    match the french display name wording

--- src/languages/fr.ts
+++ src/languages/fr.ts
@@ -2284,7 +2284,7 @@ const translations: TranslationDeepObject<typeof en> = {
     displayNamePage: {
         headerTitle: 'Nom d’affichage',
         isShownOnProfile: 'Votre nom d’affichage est affiché sur votre profil.',
-        isShownOnMemberProfile: 'Le nom affiché apparaîtra sur le profil du membre. Une fois enregistré, seul le membre pourra le modifier.',
+        isShownOnMemberProfile: 'Le nom d’affichage apparaîtra sur le profil du membre. Une fois enregistré, seul le membre pourra le modifier.',
     },
     timezonePage: {
         timezone: 'Fuseau horaire',
