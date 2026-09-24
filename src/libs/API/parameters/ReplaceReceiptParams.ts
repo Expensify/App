67e4dc621ad02d0b5a9540b7a1e45a651b2d40d2
VickyStash <vikstash@gmail.com>
2026-09-24 20:20:24 +0200

    Fix receipt being lost after app upgrade for ReplaceReceipt api command

--- src/libs/API/parameters/ReplaceReceiptParams.ts
+++ src/libs/API/parameters/ReplaceReceiptParams.ts
@@ -1,10 +1,11 @@
 import type CONST from '@src/CONST';
+import type {Receipt} from '@src/types/onyx/Transaction';
 
 import type {ValueOf} from 'type-fest';
 
 type ReplaceReceiptParams = {
     transactionID: string;
-    receipt: File;
+    receipt: Receipt;
     receiptState?: ValueOf<typeof CONST.IOU.RECEIPT_STATE>;
     isSameReceipt?: boolean;
 };
