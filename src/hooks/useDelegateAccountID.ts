a4833e240d80879563fc92edc673657e8d508e38
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2026-09-24 12:05:01 +0200

    Move useDelegateAccountID selector out of the hook

--- src/hooks/useDelegateAccountID.ts
+++ src/hooks/useDelegateAccountID.ts
@@ -1,25 +1,28 @@
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {PersonalDetailsList} from '@src/types/onyx';
+
+import type {OnyxEntry} from 'react-native-onyx';
 
 import {delegateEmailSelector} from '@selectors/Account';
 
 import useOnyx from './useOnyx';
 import {useAllPersonalDetails} from './usePersonalDetails';
 
+const accountIDByLoginSelector = (lowerEmail: string | undefined) => (personalDetails: OnyxEntry<PersonalDetailsList>) => {
+    if (!lowerEmail || !personalDetails) {
+        return undefined;
+    }
+    for (const detail of Object.values(personalDetails)) {
+        if (detail?.login?.toLowerCase() === lowerEmail) {
+            return detail.accountID;
+        }
+    }
+    return undefined;
+};
+
 function useDelegateAccountID(): number | undefined {
     const [delegateEmail] = useOnyx(ONYXKEYS.ACCOUNT, {selector: delegateEmailSelector});
-    const lowerEmail = delegateEmail?.toLowerCase();
-
-    const [accountID] = useAllPersonalDetails((personalDetails) => {
-        if (!lowerEmail || !personalDetails) {
-            return undefined;
-        }
-        for (const detail of Object.values(personalDetails)) {
-            if (detail?.login?.toLowerCase() === lowerEmail) {
-                return detail.accountID;
-            }
-        }
-        return undefined;
-    });
+    const [accountID] = useAllPersonalDetails(accountIDByLoginSelector(delegateEmail?.toLowerCase()));
 
     return accountID;
 }
