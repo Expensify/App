3171daafbbd50c934c117b7124f85865f79eabb2
Rayane <77965000+rayane-d@users.noreply.github.com>
2026-09-25 12:14:07 +0100

    update switchAccount modal copy

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -677,7 +677,7 @@ const translations = {
         sessionMismatch: "The link you opened isn't valid for your current session.",
         switchAccount: {
             title: 'Switch accounts?',
-            prompt: ({newEmail, currentEmail}: {newEmail: string; currentEmail: string}) => `You're signed in as ${currentEmail}. This sign-in link is for ${newEmail}.`,
+            prompt: ({newEmail, currentEmail}: {newEmail: string; currentEmail: string}) => `You're signed in to ${currentEmail}. This sign-in link is for ${newEmail}.`,
             confirm: 'Switch accounts',
             staySignedIn: 'Stay signed-in',
         },
