1677c50b6ae0d2c675416c929700b111a5a88c39
mhawryluk <marcinhawrylukxx@gmail.com>
2026-09-25 10:05:50 +0200

    Show the Insights widget for all users with workspaces (behind beta), hide Top categories when all workspaces have categories disabled

--- src/pages/home/InsightsSection/index.tsx
+++ src/pages/home/InsightsSection/index.tsx
@@ -1,22 +1,31 @@
 import useCurrentUserPersonalDetails from '@hooks/useCurrentUserPersonalDetails';
 import useOnyx from '@hooks/useOnyx';
+import usePermissions from '@hooks/usePermissions';
 
+import {isGroupPolicy} from '@libs/PolicyUtils';
 import {isPolicyEligibleForSpendOverTime} from '@libs/SearchUIUtils';
 
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
+import type {Policy} from '@src/types/onyx';
+
+import type {OnyxCollection} from 'react-native-onyx';
 
 import React from 'react';
 
 import InsightsSectionContent from './InsightsSectionContent';
 
+const hasGroupPolicySelector = (policies: OnyxCollection<Policy>) => Object.values(policies ?? {}).some((policy) => isGroupPolicy(policy));
+
 function InsightsSection() {
     const {login} = useCurrentUserPersonalDetails();
-    const [isAnyPolicyEligibleForInsights] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {
+    const {isBetaEnabled} = usePermissions();
+    const [hasGroupPolicy] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {selector: hasGroupPolicySelector});
+    const [isPolicyAdminAuditorOrApprover] = useOnyx(ONYXKEYS.COLLECTION.POLICY, {
         selector: (policies) => Object.values(policies ?? {}).some((policy) => !!policy && isPolicyEligibleForSpendOverTime(policy, login)),
     });
 
-    // The widget is only shown for workspace admins/auditors/approvers.
-    if (!isAnyPolicyEligibleForInsights) {
+    if (isBetaEnabled(CONST.BETAS.INSIGHTS_PAGE) ? !hasGroupPolicy : !isPolicyAdminAuditorOrApprover) {
         return null;
     }
 
